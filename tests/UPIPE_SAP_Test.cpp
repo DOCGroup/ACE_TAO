29d9b203280d9d41d405d7049eb05b723b105768
pjain <pjain@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1996-11-24 22:11:02 +0000

    Made small modifications to get it to work on NT

--- tests/UPIPE_SAP_Test.cpp
+++ tests/UPIPE_SAP_Test.cpp
@@ -29,34 +29,33 @@
 // Global pattern
 static ACE_UPIPE_Addr addr ("pattern");
 
-// peer1 thread.
+// client thread.
 
 static void *
-peer1 (void *)
+client (void *)
 {
   // Insert thread into thr_mgr.
-  ACE_Thread_Control thread_control (ACE_Service_Config::thr_mgr ());
   ACE_NEW_THREAD;
 
   ACE_UPIPE_Stream c_stream;
 
-  ACE_DEBUG ((LM_DEBUG, "(%t) peer1 starting connect\n"));
+  ACE_DEBUG ((LM_DEBUG, "(%t) client starting connect\n"));
   ACE_UPIPE_Connector con;
 
   if (con.connect (c_stream, addr) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) peer1 ACE_UPIPE_Connector failed\n"));
+    ACE_DEBUG ((LM_DEBUG, "(%t) client ACE_UPIPE_Connector failed\n"));
 
   ACE_Message_Block *mb;
 
-  ACE_NEW_RETURN (mb, ACE_Message_Block (20), 0);
+  ACE_NEW_RETURN (mb, ACE_Message_Block (sizeof "hello thanks"), 0);
 
-  mb->copy ("hello", 6);
+  mb->copy ("hello", sizeof "hello");
 
   if (c_stream.send (mb) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) error peer1 send\n"));
+    ACE_DEBUG ((LM_DEBUG, "(%t) error client send\n"));
 
   if (c_stream.recv (mb) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) error peer1 recv\n"));
+    ACE_DEBUG ((LM_DEBUG, "(%t) error client recv\n"));
 
   ACE_ASSERT (ACE_OS::strcmp (mb->rd_ptr (), "thanks") == 0);
 
@@ -64,14 +63,14 @@ peer1 (void *)
   delete mb;
 
   // Now try the send()/recv() interface.
-  char mytext[] = "This string is sent by peer1 as buffer";
+  char mytext[] = "This string is sent by client as a buffer";
 
-  ACE_DEBUG ((LM_DEBUG, "(%t) peer1 sending text\n"));
+  ACE_DEBUG ((LM_DEBUG, "(%t) client sending text\n"));
   if (c_stream.send (mytext, sizeof mytext) == -1)
     ACE_DEBUG ((LM_DEBUG, 
-		"(%t) buffer send from peer1 failed\n"));
+		"(%t) buffer send from client failed\n"));
 
-  char conbuf[30];  // Buffer to receive response.
+  char conbuf[BUFSIZ];  // Buffer to receive response.
 
   int i = 0;
 
@@ -79,68 +78,77 @@ peer1 (void *)
     {
       if (c_stream.recv (&c, 1) == -1)
 	ACE_DEBUG ((LM_DEBUG, 
-		    "(%t) buffer recv from peer1 failed\n"));
+		    "(%t) buffer recv from client failed\n"));
       else
 	conbuf[i] = c;
     }
 
   conbuf[i] = '\0';
   ACE_DEBUG ((LM_DEBUG, "(%t) conbuf = %s", conbuf));
-  ACE_ASSERT (ACE_OS::strcmp (conbuf, "this is the peer2 response!") == 0);
+  ACE_ASSERT (ACE_OS::strcmp (conbuf, "this is the server response!") == 0);
+
   c_stream.close ();
+  ACE_DEBUG ((LM_DEBUG, "\n(%t) exiting thread\n"));
   return 0;
 }
 
 static void *
-peer2 (void *)
+server (void *args)
 {
   // Insert thread into thr_mgr.
-  ACE_Thread_Control thread_control (ACE_Service_Config::thr_mgr ());
   ACE_NEW_THREAD;
 
-  ACE_UPIPE_Acceptor acc (addr);
+  ACE_UPIPE_Acceptor *acceptor = (ACE_UPIPE_Acceptor *) args;
   ACE_UPIPE_Stream s_stream;
+  ACE_hthread_t thr_handle;
 
-  // Spawn a peer1 thread.
-  if (ACE_Service_Config::thr_mgr ()->spawn (ACE_THR_FUNC (peer1), 
-					     (void *) 0,
-					     THR_NEW_LWP | THR_DETACHED) == -1)
+  // Spawn a client thread.
+  if (ACE_Thread::spawn (ACE_THR_FUNC (client), 
+			 (void *) 0,
+			 THR_NEW_LWP | THR_DETACHED,
+			 0,
+			 &thr_handle) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "spawn"), 0);
 
-  ACE_DEBUG ((LM_DEBUG, "(%t) peer2 starting accept\n"));
+  ACE_DEBUG ((LM_DEBUG, "(%t) server starting accept\n"));
 
-  if (acc.accept (s_stream) == -1)
+  if (acceptor->accept (s_stream) == -1)
     ACE_DEBUG ((LM_DEBUG, 
 		"(%t) ACE_UPIPE_Acceptor.accept failed\n"));
 
   ACE_Message_Block *mb = 0;
 
   if (s_stream.recv (mb) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) peer2 recv failed\n"));
+    ACE_DEBUG ((LM_DEBUG, "(%t) server recv failed\n"));
 
   ACE_ASSERT (ACE_OS::strcmp (mb->rd_ptr (), "hello") == 0);
 
   mb->wr_ptr (mb->rd_ptr ());
-  mb->copy ("thanks", 7);
+  mb->copy ("thanks", sizeof "thanks");
 
   if (s_stream.send (mb) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) peer2 send failed\n"));
+    ACE_DEBUG ((LM_DEBUG, "(%t) server send failed\n"));
 
-  char s_buf[42];
-  ACE_DEBUG ((LM_DEBUG, "(%t) peer2 sleeping on recv\n"));
+  char s_buf[BUFSIZ];
+  ACE_DEBUG ((LM_DEBUG, "(%t) server sleeping on recv\n"));
 
   if (s_stream.recv (s_buf, sizeof s_buf) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) peer2 recv failed\n"));
+    ACE_DEBUG ((LM_DEBUG, "(%t) server recv failed\n"));
   else
     ACE_ASSERT (ACE_OS::strcmp (s_buf, 
-				"This string is sent by peer1 as buffer") == 0);
+				"This string is sent by client as a buffer") == 0);
 
-  ACE_OS::strcpy (s_buf, "this is the peer2 response!");
+  const char svr_response[] = "this is the server response!";
+  ACE_OS::strcpy (s_buf, svr_response);
 
-  if (s_stream.send (s_buf, 30) == -1)
-    ACE_DEBUG ((LM_DEBUG, "(%t) peer2 send failed\n"));
+  if (s_stream.send (s_buf, sizeof svr_response) == -1)
+    ACE_DEBUG ((LM_DEBUG, "(%t) server send failed\n"));
 
+  // Wait until the client shuts down and the shutdown.
+  if (ACE_Thread::join (thr_handle) == -1)
+    ACE_ERROR ((LM_ERROR, "%p\n", "join"));
   s_stream.close ();
+  ACE_DEBUG ((LM_DEBUG, "(%t) exiting thread\n"));
   return 0;
 }
 
@@ -152,14 +160,19 @@ main (int, char *argv[])
   ACE_START_TEST ("UPIPE_SAP_Test.cpp");
 
 #if defined (ACE_HAS_THREADS) && (defined (ACE_HAS_STREAM_PIPES) || defined (ACE_WIN32))
-  // Spawn a peer2 thread.
-  if (ACE_Service_Config::thr_mgr ()->spawn (ACE_THR_FUNC (peer2), 
-					     (void *) 0,
-					     THR_NEW_LWP | THR_DETACHED) == -1)
+  // Spawn a server thread.
+  ACE_hthread_t thr_handle;
+  ACE_UPIPE_Acceptor acceptor (addr);
+
+  if (ACE_Thread::spawn (ACE_THR_FUNC (server), 
+			 (void *) &acceptor,
+			 THR_NEW_LWP | THR_DETACHED,
+			 0,
+			 &thr_handle) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "spawn"), 1);
 
-  // Wait for peer2 and peer1 threads to exit.
-  ACE_Service_Config::thr_mgr ()->wait ();
+  // Wait for server thread to exit.
+  ACE_Thread::join (thr_handle);
 #else
   ACE_ERROR ((LM_ERROR, "threads and/or UPIPE not supported on this platform\n"));
 #endif /* ACE_HAS_THREADS */
