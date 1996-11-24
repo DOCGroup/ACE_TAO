fb5d10449d5ed4dc1c0156f9709b3981eb79623a
pjain <pjain@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1996-11-24 23:09:35 +0000

    Cleaned up the code a bit. Moved the spawning of both client and server threads into main.

--- trunk/tests/UPIPE_SAP_Test.cpp
+++ trunk/tests/UPIPE_SAP_Test.cpp
@@ -49,7 +49,7 @@ client (void *)
 
   ACE_NEW_RETURN (mb, ACE_Message_Block (sizeof "hello thanks"), 0);
 
-  mb->copy ("hello", sizeof "hello");
+  mb->copy ("hello");
 
   if (c_stream.send (mb) == -1)
     ACE_DEBUG ((LM_DEBUG, "(%t) error client send\n"));
@@ -100,15 +100,6 @@ server (void *args)
 
   ACE_UPIPE_Acceptor *acceptor = (ACE_UPIPE_Acceptor *) args;
   ACE_UPIPE_Stream s_stream;
-  ACE_hthread_t thr_handle;
-
-  // Spawn a client thread.
-  if (ACE_Thread::spawn (ACE_THR_FUNC (client), 
-			 (void *) 0,
-			 THR_NEW_LWP | THR_DETACHED,
-			 0,
-			 &thr_handle) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "spawn"), 0);
 
   ACE_DEBUG ((LM_DEBUG, "(%t) server starting accept\n"));
 
@@ -124,7 +115,7 @@ server (void *args)
   ACE_ASSERT (ACE_OS::strcmp (mb->rd_ptr (), "hello") == 0);
 
   mb->wr_ptr (mb->rd_ptr ());
-  mb->copy ("thanks", sizeof "thanks");
+  mb->copy ("thanks");
 
   if (s_stream.send (mb) == -1)
     ACE_DEBUG ((LM_DEBUG, "(%t) server send failed\n"));
@@ -144,9 +135,6 @@ server (void *args)
   if (s_stream.send (s_buf, sizeof svr_response) == -1)
     ACE_DEBUG ((LM_DEBUG, "(%t) server send failed\n"));
 
-  // Wait until the client shuts down and the shutdown.
-  if (ACE_Thread::join (thr_handle) == -1)
-    ACE_ERROR ((LM_ERROR, "%p\n", "join"));
   s_stream.close ();
   ACE_DEBUG ((LM_DEBUG, "(%t) exiting thread\n"));
   return 0;
@@ -160,19 +148,33 @@ main (int, char *argv[])
   ACE_START_TEST ("UPIPE_SAP_Test.cpp");
 
 #if defined (ACE_HAS_THREADS) && (defined (ACE_HAS_STREAM_PIPES) || defined (ACE_WIN32))
-  // Spawn a server thread.
-  ACE_hthread_t thr_handle;
+  
+  ACE_hthread_t thr_handle_server;
+  ACE_hthread_t thr_handle_client;
+
   ACE_UPIPE_Acceptor acceptor (addr);
 
+  // Spawn a server thread.
   if (ACE_Thread::spawn (ACE_THR_FUNC (server), 
 			 (void *) &acceptor,
 			 THR_NEW_LWP | THR_DETACHED,
 			 0,
-			 &thr_handle) == -1)
+			 &thr_handle_server) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "spawn"), 1);
 
-  // Wait for server thread to exit.
-  ACE_Thread::join (thr_handle);
+  // Spawn a client thread.
+  if (ACE_Thread::spawn (ACE_THR_FUNC (client), 
+			 (void *) 0,
+			 THR_NEW_LWP | THR_DETACHED,
+			 0,
+			 &thr_handle_client) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "spawn"), 1);
+
+
+  // Wait for server and client threads to exit.
+  ACE_Thread::join (thr_handle_client);
+  ACE_Thread::join (thr_handle_server);
+  
 #else
   ACE_ERROR ((LM_ERROR, "threads and/or UPIPE not supported on this platform\n"));
 #endif /* ACE_HAS_THREADS */
