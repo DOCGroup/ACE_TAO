8e3acc45283e292ed0dbcb8b5f09e14362da78b3
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1996-11-28 09:26:10 +0000

    *** empty log message ***

--- tests/Pipe_Test.cpp
+++ tests/Pipe_Test.cpp
@@ -18,13 +18,15 @@
 #include "test_config.h"
 
 void
-open (ACE_Pipe &pipe, const char *name)
+open (ACE_Pipe &pipe, 
+      const char *name, 
+      int close_pipe = 1)
 {
   if (pipe.open () == 0)
     ACE_DEBUG ((LM_DEBUG, "Pipe %s: successful open\n", name));
   else
     {
-      ACE_DEBUG ((LM_DEBUG, "Pipe %s: UNSUCCESSFUL open\n", name));
+      ACE_DEBUG ((LM_DEBUG, "%p: UNSUCCESSFUL open for pipe %s\n", "pipe.open", name));
       if (pipe.read_handle () == ACE_INVALID_HANDLE)
 	ACE_DEBUG ((LM_DEBUG, "Reader is invalid\n"));
       else
@@ -36,6 +38,8 @@ open (ACE_Pipe &pipe, const char *name)
 
       ACE_OS::exit (-1);
     }     
+  if (close_pipe)
+    pipe.close ();
 }
 
 int 
@@ -43,13 +47,18 @@ main (int, char *argv[])
 {
   ACE_START_TEST ("Pipe_Test.cpp");
 
-  ACE_Pipe a, b, c, d, e;
+  ACE_Pipe a, b, c, d, e, f, g, h, i, j,;
 
   ::open (a, "a");
   ::open (b, "b");
   ::open (c, "c");
   ::open (d, "d");
   ::open (e, "e");
+  ::open (f, "f");
+  ::open (g, "g");
+  ::open (h, "h");
+  ::open (i, "i");
+  ::open (j, "j");
 
   ACE_END_TEST;
   return 0;
