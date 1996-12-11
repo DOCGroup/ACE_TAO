cd0d4e0e686bd285e3da4fed0404a509850a899f
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1996-12-11 02:55:19 +0000

    foo

--- trunk/tests/Service_Config_Test.cpp
+++ trunk/tests/Service_Config_Test.cpp
@@ -20,17 +20,25 @@
 #include "ace/Service_Config.h"
 #include "test_config.h"
 
-int 
-main (int argc, char *argv[])
+static void
+run_test (int argc, char *argv[])
 {
-  ACE_START_TEST ("Service_Config_Test");
-  ACE_Time_Value tv (argc > 1 ? atoi (argv[1]) : 2);
-
+  // We need this scope to make sure
   ACE_Service_Config daemon;
   
   daemon.open (argc, argv);
 
+  ACE_Time_Value tv (argc > 1 ? atoi (argv[1]) : 2);
+
   ACE_ASSERT (daemon.run_reactor_event_loop (tv) == 0);
+}
+
+int 
+main (int argc, char *argv[])
+{
+  ACE_START_TEST ("Service_Config_Test");
+
+  run_test (argc, argv);
 
   ACE_END_TEST;
   return 0;
