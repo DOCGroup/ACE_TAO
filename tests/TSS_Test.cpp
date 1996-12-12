cb40666f255f983d24bc99d602de544da3f9cf07
schmidt <douglascraigschmidt@users.noreply.github.com>
1996-12-12 08:03:36 +0000

    foo

--- tests/TSS_Test.cpp
+++ tests/TSS_Test.cpp
@@ -22,49 +22,13 @@
 
 #include "ace/Service_Config.h"
 #include "ace/Synch.h"
+#include "TSS_Test_Errno.h"
 #include "test_config.h"
 
 #if defined (ACE_HAS_THREADS) 
 #if !defined (ACE_TEMPLATES_REQUIRE_PRAGMA) // AIX is evil
 
 static const int ITERATIONS = 100;
-
-class Errno
-{
-public:
-  int error (void) { return this->errno_; }
-  void error (int i) { this->errno_ = i; }
-  
-  int line (void) { return this->lineno_; }
-  void line (int l) { this->lineno_ = l; }
-
-  // Errno::flags_ is a static variable, so we've got to protect it
-  // with a mutex since it isn't kept in thread-specific storage.
-  int flags (void) { 
-    ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, ace_Mon, Errno::lock_, -1));
-
-    return Errno::flags_;
-  }
-  int flags (int f)
-  {
-    ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, ace_mon, Errno::lock_, -1));
-
-    Errno::flags_ = f;
-    return 0;
-  }
-
-private:
-  // = errno_ and lineno_ will be thread-specific data so they don't
-  // need a lock.  
-  int errno_;
-  int lineno_;
-
-  static int flags_;
-#if defined (ACE_HAS_THREADS)
-  // flags_ needs a lock. 
-  static ACE_Thread_Mutex lock_;
-#endif /* ACE_HAS_THREADS */
-};
 
 // Static variables.
 ACE_MT (ACE_Thread_Mutex Errno::lock_);
---
+++ tests/TSS_Test_Errno.h
@@ -0,0 +1,46 @@
+class Errno
+// ============================================================================
+// = TITLE
+//    Define a simple Errno abstraction
+//
+// = DESCRIPTION
+//    This class gets its own header file to work around AIX C++
+//    compiler "features" related to template instantiation...  It is
+//    only used by TSS_Test.cpp  
+// ============================================================================
+{
+public:
+  int error (void) { return this->errno_; }
+  void error (int i) { this->errno_ = i; }
+  
+  int line (void) { return this->lineno_; }
+  void line (int l) { this->lineno_ = l; }
+
+  // Errno::flags_ is a static variable, so we've got to protect it
+  // with a mutex since it isn't kept in thread-specific storage.
+  int flags (void) { 
+    ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, ace_Mon, Errno::lock_, -1));
+
+    return Errno::flags_;
+  }
+  int flags (int f)
+  {
+    ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, ace_mon, Errno::lock_, -1));
+
+    Errno::flags_ = f;
+    return 0;
+  }
+
+private:
+  // = errno_ and lineno_ will be thread-specific data so they don't
+  // need a lock.  
+  int errno_;
+  int lineno_;
+
+  static int flags_;
+#if defined (ACE_HAS_THREADS)
+  // flags_ needs a lock. 
+  static ACE_Thread_Mutex lock_;
+#endif /* ACE_HAS_THREADS */
+};
+
