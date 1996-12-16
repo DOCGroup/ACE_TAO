053e0ab4e155ba099db848be4d6999fc725a177c
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1996-12-16 19:15:29 +0000

    #ifdef'd out compiles of ace_yyunput () and ace_yy_{push,pop,top}_state () because they're not used, and commented out a few breaks after return statements to prevent compiler warnings

--- ace/Svc_Conf_l.cpp
+++ ace/Svc_Conf_l.cpp
@@ -138,7 +138,9 @@ extern "C" {
 		} \
 	while ( 0 )
 
+#if 0
 #define unput(c) ace_yyunput( c, ace_yytext_ptr )
+#endif /* 0 */
 
 
 struct ace_yy_buffer_state
@@ -213,7 +215,9 @@ static int ace_yy_start = 0;	/* start state number */
  */
 static int ace_yy_did_buffer_switch_on_eof;
 
+#if 0
 static void ace_yyunput YY_PROTO(( int c, char *buf_ptr ));
+#endif /* 0 */
 void ace_yyrestart YY_PROTO(( FILE *input_file ));
 void ace_yy_switch_to_buffer YY_PROTO(( YY_BUFFER_STATE new_buffer ));
 void ace_yy_load_buffer_state YY_PROTO(( void ));
@@ -224,9 +228,11 @@ void ace_yy_init_buffer YY_PROTO(( YY_BUFFER_STATE b, FILE *file ));
 static int ace_yy_start_stack_ptr = 0;
 static int ace_yy_start_stack_depth = 0;
 static int *ace_yy_start_stack = 0;
+#if 0
 static void ace_yy_push_state YY_PROTO(( int new_state ));
 static void ace_yy_pop_state YY_PROTO(( void ));
 static int ace_yy_top_state YY_PROTO(( void ));
+#endif /* 0 */
 
 static void *ace_yy_flex_alloc YY_PROTO(( unsigned int ));
 static void *ace_yy_flex_realloc YY_PROTO(( void *, unsigned int ));
@@ -658,87 +664,87 @@ case 2:
 YY_USER_ACTION
 # line 35 "Svc_Conf.l"
 { return token (ACE_DYNAMIC); }
-	YY_BREAK
+	/* YY_BREAK */
 case 3:
 YY_USER_ACTION
 # line 36 "Svc_Conf.l"
 { return token (ACE_STATIC); }
-	YY_BREAK
+	/* YY_BREAK */
 case 4:
 YY_USER_ACTION
 # line 37 "Svc_Conf.l"
 { return token (ACE_SUSPEND); }
-	YY_BREAK
+	/* YY_BREAK */
 case 5:
 YY_USER_ACTION
 # line 38 "Svc_Conf.l"
 { return token (ACE_RESUME); }
-	YY_BREAK
+	/* YY_BREAK */
 case 6:
 YY_USER_ACTION
 # line 39 "Svc_Conf.l"
 { return token (ACE_REMOVE); }
-	YY_BREAK
+	/* YY_BREAK */
 case 7:
 YY_USER_ACTION
 # line 40 "Svc_Conf.l"
 { return token (ACE_USTREAM); }
-	YY_BREAK
+	/* YY_BREAK */
 case 8:
 YY_USER_ACTION
 # line 41 "Svc_Conf.l"
 { return token (ACE_MODULE_T); }
-	YY_BREAK
+	/* YY_BREAK */
 case 9:
 YY_USER_ACTION
 # line 42 "Svc_Conf.l"
 { return token (ACE_SVC_OBJ_T); }
-	YY_BREAK
+	/* YY_BREAK */
 case 10:
 YY_USER_ACTION
 # line 43 "Svc_Conf.l"
 { return token (ACE_STREAM_T); }
-	YY_BREAK
+	/* YY_BREAK */
 case 11:
 YY_USER_ACTION
 # line 44 "Svc_Conf.l"
 { return token (ACE_ACTIVE); }
-	YY_BREAK
+	/* YY_BREAK */
 case 12:
 YY_USER_ACTION
 # line 45 "Svc_Conf.l"
 { return token (ACE_INACTIVE); }
-	YY_BREAK
+	/* YY_BREAK */
 case 13:
 YY_USER_ACTION
 # line 46 "Svc_Conf.l"
 { return token (ACE_COLON); }
-	YY_BREAK
+	/* YY_BREAK */
 case 14:
 YY_USER_ACTION
 # line 47 "Svc_Conf.l"
 { return token (ACE_STAR); }
-	YY_BREAK
+	/* YY_BREAK */
 case 15:
 YY_USER_ACTION
 # line 48 "Svc_Conf.l"
 { return token (ACE_LPAREN); }
-	YY_BREAK
+	/* YY_BREAK */
 case 16:
 YY_USER_ACTION
 # line 49 "Svc_Conf.l"
 { return token (ACE_RPAREN); }
-	YY_BREAK
+	/* YY_BREAK */
 case 17:
 YY_USER_ACTION
 # line 50 "Svc_Conf.l"
 { return token (ACE_LBRACE); }
-	YY_BREAK
+	/* YY_BREAK */
 case 18:
 YY_USER_ACTION
 # line 51 "Svc_Conf.l"
 { return token (ACE_RBRACE); }
-	YY_BREAK
+	/* YY_BREAK */
 case 19:
 YY_USER_ACTION
 # line 52 "Svc_Conf.l"
@@ -747,7 +753,7 @@ YY_USER_ACTION
 			ace_yyleng -= 1;
                         ace_yylval.ident_ = ace_obstack->copy (ace_yytext + 1, ace_yyleng); 
 			return token (ACE_STRING); }
-	YY_BREAK
+	/* YY_BREAK */
 case 20:
 YY_USER_ACTION
 # line 57 "Svc_Conf.l"
@@ -755,7 +761,7 @@ YY_USER_ACTION
 		        ace_yylval.ident_ = ace_obstack->copy (ace_yytext, ace_yyleng); 
 			return token (ACE_IDENT); 
 		      }
-	YY_BREAK
+	/* YY_BREAK */
 case 21:
 YY_USER_ACTION
 # line 61 "Svc_Conf.l"
@@ -763,7 +769,7 @@ YY_USER_ACTION
 		        ace_yylval.ident_ = ace_obstack->copy (ace_yytext, ace_yyleng); 
 			return token (ACE_PATHNAME); 
 		      }
-	YY_BREAK
+	/* YY_BREAK */
 case 22:
 YY_USER_ACTION
 # line 65 "Svc_Conf.l"
@@ -1117,6 +1123,7 @@ ace_yy_state_type ace_yy_current_state;
 	}
 
 
+#if 0
 #ifdef YY_USE_PROTOS
 static void ace_yyunput( int c, register char *ace_yy_bp )
 #else
@@ -1161,6 +1168,7 @@ register char *ace_yy_bp;
 	 */
 	YY_DO_BEFORE_ACTION; /* set up ace_yytext again */
 	}
+#endif /* 0 */
 
 
 #ifdef __cplusplus
@@ -1369,6 +1377,7 @@ FILE *file;
 	}
 
 
+#if 0
 #ifdef YY_USE_PROTOS
 static void ace_yy_push_state( int new_state )
 #else
@@ -1414,6 +1423,7 @@ static int ace_yy_top_state()
 	{
 	return ace_yy_start_stack[ace_yy_start_stack_ptr - 1];
 	}
+#endif /* 0 */
 
 
 #ifdef YY_USE_PROTOS
