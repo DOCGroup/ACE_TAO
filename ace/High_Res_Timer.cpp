9ecbbb1e8ad58855047d744bd7a25dfb9c0586d6
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1996-12-31 21:04:53 +0000

    added comment to print_total () saying it should only be used for incremental timings; added defaults to a few print_ave/print_total args; trimmed output if count is 1; rounded instead of truncated usecs portion of time

--- ace/High_Res_Timer.cpp
+++ ace/High_Res_Timer.cpp
@@ -32,42 +32,58 @@ ACE_High_Res_Timer::reset (void)
   (void) ACE_OS::memset (&this->temp_, 0, sizeof this->temp_);
 }
 
-ACE_High_Res_Timer::ACE_High_Res_Timer (void)
-{
-  ACE_TRACE ("ACE_High_Res_Timer::ACE_High_Res_Timer");
-  this->reset ();
-}
-
 void 
-ACE_High_Res_Timer::print_ave (char *str, int count, ACE_HANDLE handle)
+ACE_High_Res_Timer::print_ave (const char *str, const int count, ACE_HANDLE handle)
 {
   ACE_TRACE ("ACE_High_Res_Timer::print_ave");
 #if defined (ACE_HAS_LONGLONG_T)
   hrtime_t total       = this->end_ - this->start_;
-  hrtime_t avg_nsecs   = total / count;
   hrtime_t total_secs  = total / (1000 * 1000 * 1000);
-  hrtime_t extra_nsecs = total - (total_secs * (1000 * 1000 * 1000));
-  char	   buf[100];
+  u_long extra_nsecs = total - (total_secs * (1000 * 1000 * 1000));
 
-  ACE_OS::sprintf (buf, "%s count = %d, total (secs %lld, usecs %lld), avg usecs = %lld\n",
-             str, count, total_secs, extra_nsecs / 1000, avg_nsecs / 1000);
+  char buf[100];
+  if (count > 1)
+    {
+      hrtime_t avg_nsecs = total / count;
+      ACE_OS::sprintf (buf, " count = %d, total (secs %lld, usecs %lu), avg usecs = %lld\n",
+             count, total_secs, (extra_nsecs + 500) / 1000,
+             (avg_nsecs + 500) / 1000);
+    }
+  else
+    ACE_OS::sprintf (buf, " total %3lld.%06lu secs\n",
+             total_secs, (extra_nsecs + 500) / 1000);
+
+  ACE_OS::write (handle, str, strlen (str));
   ACE_OS::write (handle, buf, strlen (buf));
+#else
+# error must have ACE_HAS_LONGLONG_T with ACE_HAS_HI_RES_TIMER
 #endif /* ACE_HAS_LONGLONG_T */
 }
 
 void 
-ACE_High_Res_Timer::print_total (char *str, int count, ACE_HANDLE handle)
+ACE_High_Res_Timer::print_total (const char *str, const int count, ACE_HANDLE handle)
 {
   ACE_TRACE ("ACE_High_Res_Timer::print_total");
 #if defined (ACE_HAS_LONGLONG_T)
-  hrtime_t avg_nsecs   = this->total_ / count;
   hrtime_t total_secs  = this->total_ / (1000 * 1000 * 1000);
-  hrtime_t extra_nsecs = this->total_ - (total_secs * (1000 * 1000 * 1000));
-  char buf[100];
+  u_long extra_nsecs = this->total_ - (total_secs * (1000 * 1000 * 1000));
 
-  ACE_OS::sprintf (buf, "%s count = %d, total (secs %lld, usecs %lld), avg usecs = %lld\n",
-             str, count, total_secs, extra_nsecs / 1000, avg_nsecs / 1000);
+  char buf[100];
+  if (count > 1)
+    {
+      hrtime_t avg_nsecs   = this->total_ / count;
+      ACE_OS::sprintf (buf, " count = %d, total (secs %lld, usecs %lu), avg usecs = %lld\n",
+             count, total_secs, (extra_nsecs + 500) / 1000,
+             (avg_nsecs + 500) / 1000);
+    }
+  else
+    ACE_OS::sprintf (buf, " total %3lld.%06lu secs\n",
+             total_secs, (extra_nsecs + 500) / 1000);
+
+  ACE_OS::write (handle, str, strlen (str));
   ACE_OS::write (handle, buf, strlen (buf));
+#else
+# error must have ACE_HAS_LONGLONG_T with ACE_HAS_HI_RES_TIMER
 #endif /* ACE_HAS_LONGLONG_T */
 }
 #endif /* ACE_HAS_HI_RES_TIMER */
