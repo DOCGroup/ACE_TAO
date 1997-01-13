8876995b79464592cc44b9e574bb80eca67c0aa4
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-01-13 07:14:30 +0000

    foo

--- trunk/examples/Reactor/Misc/test_timer_queue.cpp
+++ trunk/examples/Reactor/Misc/test_timer_queue.cpp
@@ -22,27 +22,54 @@ private:
   int count_;
 };
 
-int
-main (int, char *[])
+static void
+test_functionality (ACE_Timer_Queue *tq)
 {
-  ACE_Timer_Queue tq;
   Example_Handler eh;
 
-  ACE_ASSERT (tq.is_empty ());
+  ACE_ASSERT (tq->is_empty ());
   ACE_ASSERT (ACE_Time_Value::zero == ACE_Time_Value (0));
   int timer_id; 
   
-  timer_id = tq.schedule (&eh, (const void *) 1, ACE_OS::gettimeofday ());
-  tq.schedule (&eh, (const void *) 2, ACE_OS::gettimeofday ());
-  tq.schedule (&eh, (const void *) 3, ACE_OS::gettimeofday ());
-  tq.cancel (timer_id);
-  ACE_ASSERT (!tq.is_empty ());
-  tq.expire (ACE_OS::gettimeofday ());
-  tq.schedule (&eh, (const void *) 4, ACE_OS::gettimeofday ());
-  tq.schedule (&eh, (const void *) 5, ACE_OS::gettimeofday ());
-  tq.cancel (&eh);
-  ACE_ASSERT (tq.is_empty ());
-  tq.expire (ACE_OS::gettimeofday ());
-  return 0;
+  timer_id = tq->schedule (&eh, (const void *) 1, ACE_OS::gettimeofday ());
+  ACE_ASSERT (timer_id != -1);
+
+  ACE_ASSERT (tq->schedule (&eh, (const void *) 42,
+			   ACE_OS::gettimeofday ()) != -1);
+  ACE_ASSERT (tq->schedule (&eh, (const void *) 42,
+			   ACE_OS::gettimeofday ()) != -1);
+  ACE_ASSERT (tq->cancel (timer_id) == 1);
+  ACE_ASSERT (!tq->is_empty ());
+
+  ACE_ASSERT (tq->expire () == 2);
+
+  ACE_ASSERT (tq->schedule (&eh, (const void *) 4, ACE_OS::gettimeofday
+			   ()) != -1);
+  ACE_ASSERT (tq->schedule (&eh, (const void *) 5, ACE_OS::gettimeofday
+			   ()) != -1);
+  ACE_ASSERT (tq->cancel (&eh) == 2);
+  ACE_ASSERT (tq->is_empty ());
+  ACE_ASSERT (tq->expire () == 0);
 }
 
+static Timer_Queues timer_queues[] =
+{
+  { new ACE_Timer_List, "ACE_Timer_List" },
+  { new ACE_Timer_Heap, "ACE_Timer_Heap" },
+  { 0, 0 },
+};
+
+int
+main (int, char *[])
+{
+  ACE_START_TEST ("Timer_Queue_Test");
+
+  for (int i = 0; timer_queues[i].name_ != 0; i++)
+    {
+      test_functionality (timer_queues[i].queue_);
+      delete timer_queues[i].queue_;
+    }
+
+  ACE_END_TEST;
+  return 0;
+}
