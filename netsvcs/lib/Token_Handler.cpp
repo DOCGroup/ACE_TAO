721d8ee01737482b11f2600aaf757af550a2ddfc
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-01-20 15:02:24 +0000

    added ACE_UNUSED_ARG (sig);

--- netsvcs/lib/Token_Handler.cpp
+++ netsvcs/lib/Token_Handler.cpp
@@ -296,6 +296,7 @@ ACE_Token_Acceptor::init (int argc, char *argv[])
   // Ignore SIGPIPE so that each <SVC_HANDLER> can handle this on its
   // own.
   ACE_Sig_Action sig (ACE_SignalHandler (SIG_IGN), SIGPIPE);
+  ACE_UNUSED_ARG (sig);
 
   ACE_INET_Addr server_addr;
 
