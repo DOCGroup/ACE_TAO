a965270c7a4dd3c1a80130a9e826aeec0b8d6e2c
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-01-20 16:07:35 +0000

    wrapped basic_string<char> template instantiation so that it's not used with GreenHills compiler

--- trunk/STL/bstring.h
+++ trunk/STL/bstring.h
@@ -2492,7 +2492,10 @@ operator>> (istream& i, basic_string<charT>& s) _THROW_ALLOC_LENGTH
     return i;
 }
 
+#if ! defined (ghs)
+// GreenHills 1.8.8 doesn't like this template specialization . . .
 template basic_string<char>;
+#endf /* ghs */
 typedef  basic_string<char>     cstring;
 typedef  basic_string<char>     string;
 //typedef  basic_string<wchar_t>  wstring;
