8668419989d3af58e9b84e98d961fb87664dff26
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-01-29 15:03:12 +0000

    updated comments

--- trunk/ace/Sched_Params.h
+++ trunk/ace/Sched_Params.h
@@ -22,18 +22,29 @@
 
 #include "ace/Thread_Priority.h"
 // for ACE_Time_Value and ACE_SCOPE_PROCESS
-#include "ace/OS.h"  
+#include "ace/OS.h"
 
 class ACE_Export ACE_Scheduling_Params
   // = TITLE
+  //    Container for thread scheduling-related parameters.
   //
   // = DESCRIPTION
+  //    ACE_Scheduling_Params are passed via
+  //    ACE_OS::set_sched_params () to the OS to specify scheduling
+  //    parameters.  It is intended that ACE_OS::set_sched_params ()
+  //    be called from main () before any threads have been spawned.
+  //    If spawned threads inherit their parent's priority (I think
+  //    that's the case for all of our platforms), then this sets
+  //    the default base priority.  Individual thread priorities can
+  //    be adjusted as usual using ACE_OS::thr_prio () or via the
+  //    ACE_Thread interface.  See the parameter descriptions in the
+  //    private: section below.
 {
 public:
   // = Initialization and termination methods.
   ACE_Scheduling_Params (const ACE_Thread_Priority &priority = ACE_Thread_Priority (),
-			 const int scope = ACE_SCOPE_PROCESS,
-			 const ACE_Time_Value &quantum = ACE_Time_Value::zero);
+                         const int scope = ACE_SCOPE_PROCESS,
+                         const ACE_Time_Value &quantum = ACE_Time_Value::zero);
   // Constructor.
 
   ~ACE_Scheduling_Params (void);
@@ -56,11 +67,7 @@ public:
 private:
   ACE_Thread_Priority priority_;
   // Default <priority_>: sets the priority to be used for newly
-  // spawned threads.  It is intended that this function be called
-  // from main () before any threads have been spawned.  If spawned
-  // threads inherit their parent's priority (I think that's the case
-  // for all of our platforms), then this sets the default base
-  // priority.  Individual thread priorities can be adjusted as usual.
+  // spawned threads.
 
   int scope_;
   // <scope_> must be either ACE_SCOPE_PROCESS or ACE_SCOPE_LWP (which
