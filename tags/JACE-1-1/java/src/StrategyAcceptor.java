1e44ffd24ed33c1e45cfb7517ab4c7a347f6d7a1
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-01-30 04:32:59 +0000

    This commit was manufactured by cvs2svn to create tag 'JACE-1-1'.

---
+++ tags/JACE-1-1/java/src/StrategyAcceptor.java
@@ -0,0 +1,148 @@
+/*************************************************
+ *
+ * = PACKAGE
+ *    ACE.Connection
+ *
+ * = FILENAME
+ *    StrategyAcceptor.java
+ *
+ *@author Prashant Jain
+ *
+ *************************************************/
+package ACE.Connection;
+
+import java.io.*;
+import java.net.*;
+import ACE.OS.*;
+import ACE.SOCK_SAP.*;
+
+public class StrategyAcceptor
+{
+  /**
+   * Create an instance of StrategyAcceptor.
+   *@param handlerFactory Svc Handler factory that is used to create
+   * an instance of a Svc Handler when a connection is accepted.
+   */
+  public StrategyAcceptor (Class handlerFactory)
+    {
+      this (handlerFactory, null, null, null);
+    }
+
+  /**
+   * Create an instance of StrategyAcceptor. Use the creation
+   * strategy and the handlerFactory passed in to creae a new instance
+   * of the Svc Handler.
+   *@param handlerFactory Svc Handler factory that is used to create
+   * an instance of a Svc Handler when a connection is accepted.
+   *@param creStrategy Creation strategy to use to create a new
+   * instance of the Svc Handler.
+   *@param acceptStrategy Accept strategy to use to accept a new
+   * connection into the Svc Handler.
+   *@param activateStrategy Activate strategy to use to activate the
+   * instance of the Svc Handler.
+   */
+  public StrategyAcceptor (Class handlerFactory,
+			    CreationStrategy creStrategy,
+			    AcceptStrategy acceptStrategy,
+			    ActivateStrategy activateStrategy)
+    {
+      // Cache everything
+      this.handlerFactory_ = handlerFactory;
+      this.creStrategy_ = creStrategy;
+      this.acceptStrategy_ = acceptStrategy;
+      this.activateStrategy_ = activateStrategy;
+    }
+
+  /**
+   * Initialize the Strategy Acceptor. The method creates the
+   * appropriate strategies as needed.
+   *@param port port number where the server will listen for connections
+   */
+  public void open (int port) throws IOException
+    {
+      if (this.creStrategy_ == null)
+	this.creStrategy_ = new CreationStrategy (this.handlerFactory_);
+      if (this.acceptStrategy_ == null)
+	this.acceptStrategy_ = new AcceptStrategy (port);
+      else
+	this.acceptStrategy_.open (port);
+      if (this.activateStrategy_ == null)
+	this.activateStrategy_ = new ActivateStrategy ();
+    }
+
+  /**
+   * Accept a connection using the appropriate strategies.
+   */
+  public void accept () throws SocketException, 
+      InstantiationException, 
+      IllegalAccessException,
+      IOException
+  {
+    // Create a Svc_Handler using the appropriate Creation_Strategy
+    SvcHandler sh = this.makeSvcHandler ();
+
+    // Accept a connection into the Svc_Handler
+    this.acceptSvcHandler (sh);
+
+    // Activate the Svc_Handler
+    this.activateSvcHandler (sh);
+  }
+
+  /**
+   * Bridge method for creating a SvcHandler.  The strategy for
+   * creating a SvcHandler is configured into the Acceptor via it's
+   * creStrategy_.  If no strategy is passed in, the default behavior
+   * of this method is to use the default CreationStrategy.
+   *@return a new instance of the Svc Handler
+   */
+  protected SvcHandler makeSvcHandler () throws InstantiationException, 
+    IllegalAccessException
+    {
+      // Create a new handler for the connection
+      return this.creStrategy_.makeSvcHandler ();
+    }
+
+
+  /**
+   * Bridge method for accepting the new connection into the
+   * <SvcHandler>.  The strategy for accepting into a SvcHandler is
+   * configured into the Acceptor via it's acceptStrategy_.  If no
+   * strategy is passed in, the default behavior of this method is to
+   * use the default AcceptStrategy. 
+   *@param sh Svc Handler in which to accept the connection
+   *@return result of accepting a connection using the accept strategy
+   */
+  protected int acceptSvcHandler (SvcHandler sh) throws
+  SocketException, IOException
+  {
+    // Delegate responsibility to the appropriate strategy
+    return this.acceptStrategy_.acceptSvcHandler (sh);
+  }
+  
+  /**
+   * Bridge method for activating a <SvcHandler>.  The strategy for
+   * activating a SvcHandler is configured into the Acceptor via it's
+   * activateStrategy_. If no strategy is passed in, the default
+   * behavior of this method is to use the default ActivateStrategy.
+   *@param sh Svc Handler to activate
+   *@return result of activating the Svc Handler
+   */
+  protected int activateSvcHandler (SvcHandler sh)
+    {
+      // Delegate responsibility to the appropriate strategy
+      return this.activateStrategy_.activateSvcHandler (sh);
+    }
+
+  // Handler class that should be instantiated when a connection is
+  // made with a client
+  private Class handlerFactory_;
+
+  // Creation Strategy
+  private CreationStrategy creStrategy_;
+
+  // Accept Strategy
+  private AcceptStrategy acceptStrategy_;
+
+  // Activation Strategy
+  private ActivateStrategy activateStrategy_;
+}
---
+++ tags/JACE-1-1/java/src/TimeoutException.java
@@ -0,0 +1,34 @@
+/*************************************************
+ *
+ * = PACKAGE
+ *    ACE.ASX
+ *
+ * = FILENAME
+ *    TimeoutException.java
+ *
+ *@author Prashant Jain and Doug Schmidt
+ *
+ *************************************************/
+package ACE.ASX;
+
+public class TimeoutException extends Exception
+{ 
+  /**
+   * Default Constructor.
+   */
+  public TimeoutException ()
+  {
+    super ("Timed Out");
+  }
+
+  /**
+   * Constructor.
+   *@param timeout The timeout value which expired.
+   *@param desc Textual description of the exception
+   */
+  public TimeoutException (TimeValue timeout, String desc)
+  {
+    super ("Timed Out in " + timeout + ": " + desc);
+  }
+
+}
