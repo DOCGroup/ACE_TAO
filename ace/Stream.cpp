dc4385a2abeb4788ea89cc15eb51871409b2e997
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-02-22 19:42:50 +0000

    *** empty log message ***

--- ace/Stream.cpp
+++ ace/Stream.cpp
@@ -265,7 +265,8 @@ ACE_Stream<ACE_SYNCH_2>::open (void *a,
       h2 = new ACE_Stream_Head<ACE_SYNCH_2>;
       head = new ACE_Module<ACE_SYNCH_2> ("ACE_Stream_Head", 
 					  h1, h2, 
-					  a | M_DELETE);
+					  a,
+					  M_DELETE);
     }
 
   if (tail == 0)
@@ -274,7 +275,8 @@ ACE_Stream<ACE_SYNCH_2>::open (void *a,
       t2 = new ACE_Stream_Tail<ACE_SYNCH_2>;
       tail = new ACE_Module<ACE_SYNCH_2> ("ACE_Stream_Tail", 
 					  t1, t2, 
-					  a | M_DELETE);
+					  a,
+					  M_DELETE);
     }
 
   // Make sure *all* the allocation succeeded!
