5e2d8ebf5dcaf77f173b67cfc707cf43bc0e300b
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-02-25 02:23:04 +0000

    *** empty log message ***

---
+++ trunk/apps/JAWS/clients/cp.h
@@ -0,0 +1,18 @@
+#include "util.h"
+#include "stats.h"
+
+class Client_Parameters {
+public:
+  Client_Parameters(int);
+  URL *url;
+  static Stats *stats;
+  static int tcp_nodelay;
+  static int sockbufsiz;
+  int id;
+};
+
+Client_Parameters::Client_Parameters(int i) {
+
+  id = i;
+
+}
---
+++ trunk/apps/JAWS/clients/global.h
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <string.h>
+#include <iostream.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#include <ace/OS.h>
+#include <ace/SOCK_Connector.h>
+#include <ace/SOCK_Stream.h>
+#include <ace/INET_Addr.h>
+#include <ace/Profile_Timer.h>
+#include <ace/Thread.h>
+#include <ace/Thread_Manager.h >
+#include <ace/Service_Config.h>
+
+#include <math.h>
+
+
+#ifndef extern_c_symbols
+#define extern_c_symbols
+extern "C" {
+
+       int usleep(unsigned int useconds);
+}
+#endif
+
+
+
+
+
+
+
+
+
+
