e417164c1b571e8e0a585e878fca78bc15d586e2
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-02-26 03:35:52 +0000

    This commit was manufactured by cvs2svn to create branch 'posix_thread_priority'.

---
+++ branches/posix_thread_priority/ace/Sched_Params.cpp
@@ -0,0 +1,27 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    ACE
+//
+// = FILENAME
+//    Scheduling_Params.cpp
+//
+// = CREATION DATE
+//    28 January 1997
+//
+// = AUTHOR
+//    David Levine
+//
+// ============================================================================
+
+#define ACE_BUILD_DLL
+
+#include "ace/ACE.h"
+#include "ace/Scheduling_Params.h"
+
+#if !defined (__ACE_INLINE__)
+#include "ace/Scheduling_Params.i"
+#endif /* __ACE_INLINE__ */
+
