8eaeaf116186284cab824e92b651724456109ffe
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-02-27 00:13:49 +0000

    Used ACE_High_Res_Timer

--- trunk/apps/JAWS/clients/global.h
+++ trunk/apps/JAWS/clients/global.h
@@ -8,13 +8,16 @@
 #include <fcntl.h>
 #include <signal.h>
 
+#include <ace/ACE.h>
 #include <ace/OS.h>
+#include <ace/SOCK_Acceptor.h>
 #include <ace/SOCK_Connector.h>
+#include <ace/Pipe.h>
 #include <ace/SOCK_Stream.h>
 #include <ace/INET_Addr.h>
 #include <ace/Profile_Timer.h>
 #include <ace/Thread.h>
-#include <ace/Thread_Manager.h >
+#include <ace/Thread_Manager.h>
 #include <ace/Service_Config.h>
 
 #include <math.h>
