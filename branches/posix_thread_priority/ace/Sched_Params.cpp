11b363a93899fccab268ca61cf1e5bca337b4a70
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-03-05 14:54:11 +0000

    added scope param to priority_min/max () and next/previous_priority () static member functions

--- branches/posix_thread_priority/ace/Sched_Params.cpp
+++ branches/posix_thread_priority/ace/Sched_Params.cpp
@@ -31,9 +31,12 @@
 #endif /* ACE_HAS_STHREADS */
 
 int
-ACE_Sched_Params::priority_min (const Policy policy)
+ACE_Sched_Params::priority_min (const Policy policy,
+                                const int scope)
 {
 #if defined (ACE_HAS_STHREADS)
+  ACE_UNUSED_ARG (scope);
+
   // Assume that ACE_SCHED_OTHER indicates Solaris TS class, and that
   // other policies indicate Solaris RT class.
 
@@ -67,20 +70,54 @@ ACE_Sched_Params::priority_min (const Policy policy)
       return 0;
     }
 #elif defined (ACE_HAS_DCETHREADS) || defined(ACE_HAS_PTHREADS)
+
+#if defined (PRI_FIFO_MIN) && defined (PRI_RR_MIN) && defined (PRI_OTHER_MIN)
+  if (scope == ACE_SCOPE_THREAD)
+    {
+      // return the minimal priority of the given policy;
+      // default to ACE_SCHED_OTHER in case of invalid policy value
+      switch (policy)
+        {
+          case ACE_SCHED_FIFO:
+            return PRI_FIFO_MIN;
+          case ACE_SCHED_RR:
+            return PRI_RR_MIN;
+          case ACE_SCHED_OTHER:
+          default:
+            return PRI_OTHER_MIN;
+        }
+    }
+  else
+    {
+      return ::sched_get_priority_min (policy);
+    }
+#else
+  ACE_UNUSED_ARG (scope);
   return ::sched_get_priority_min (policy);
+#endif /* (PRI_FIFO_MIN && PRI_RR_MIN && PRI_OTHER_MIN */
+
 #elif defined (ACE_HAS_WTHREADS)
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   return THREAD_PRIORITY_IDLE;
 #elif defined (VXWORKS)
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   return 255;
 #elif
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   ACE_NOTSUP_RETURN (-1);
 #endif /* ACE_HAS_STHREADS */
 }
 
 int
-ACE_Sched_Params::priority_max (const Policy policy)
+ACE_Sched_Params::priority_max (const Policy policy,
+                                const int scope)
 {
 #if defined (ACE_HAS_STHREADS)
+  ACE_UNUSED_ARG (scope);
+
   // Assume that ACE_SCHED_OTHER indicates Solaris TS class, and that
   // other policies indicate Solaris RT class.
 
@@ -108,25 +145,60 @@ ACE_Sched_Params::priority_max (const Policy policy)
       ? ((tsinfo_t *) pcinfo.pc_clinfo)->ts_maxupri
       : ((rtinfo_t *) pcinfo.pc_clinfo)->rt_maxpri;
 #elif defined (ACE_HAS_DCETHREADS) || defined(ACE_HAS_PTHREADS)
+
+#if defined (PRI_FIFO_MAX) && defined (PRI_RR_MAX) && defined (PRI_OTHER_MAX)
+  if (scope == ACE_SCOPE_THREAD)
+    {
+      // return the maximal priority of the given policy;
+      // default to ACE_SCHED_OTHER in case of invalid policy value
+      switch (policy)
+        {
+          case ACE_SCHED_FIFO:
+            return PRI_FIFO_MAX;
+          case ACE_SCHED_RR:
+            return PRI_RR_MAX;
+          case ACE_SCHED_OTHER:
+          default:
+            return PRI_OTHER_MAX;
+        }
+    }
+  else
+    {
+      return ::sched_get_priority_max (policy);
+    }
+#else
+  ACE_UNUSED_ARG (scope);
   return ::sched_get_priority_max (policy);
+#endif /* (PRI_FIFO_MAX && PRI_RR_MAX && PRI_OTHER_MAX */
+
 #elif defined (ACE_HAS_WTHREADS)
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   return THREAD_PRIORITY_TIME_CRITICAL;
 #elif defined (VXWORKS)
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   return 0;
 #elif
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   ACE_NOTSUP_RETURN (-1);
 #endif /* ACE_HAS_STHREADS */
 }
 
 int
-ACE_Sched_Params::next_priority (const Policy policy, const int priority)
+ACE_Sched_Params::next_priority (const Policy policy,
+                                 const int priority,
+                                 const int scope)
 {
 #if defined (VXWORKS)
   ACE_UNUSED_ARG (policy);
-  return priority > priority_max (policy)  ?  priority - 1
-                                           :  priority_max (policy);
+  return priority > priority_max (policy, scope)
+           ?  priority - 1
+           :  priority_max (policy, scope);
 #elif defined (ACE_HAS_WTHREADS)
   ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   switch (priority)
     {
       case THREAD_PRIORITY_IDLE:
@@ -146,23 +218,28 @@ ACE_Sched_Params::next_priority (const Policy policy, const int priority)
     }
 #elif defined (ACE_HAS_THREADS)
   // including STHREADS, DCETHREADS, and PTHREADS
-  const int max = priority_max (policy);
+  const int max = priority_max (policy, scope);
   return priority < max  ?  priority + 1  :  max;
 #elif
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   ACE_NOTSUP_RETURN (-1);
 #endif /* ACE_HAS_THREADS */
 }
 
 int
 ACE_Sched_Params::previous_priority (const Policy policy,
-                                          const int priority)
+                                     const int priority,
+                                     const int scope)
 {
 #if defined (VXWORKS)
   ACE_UNUSED_ARG (policy);
-  return priority < priority_min (policy)  ?  priority + 1
-                                           :  priority_min (policy);
+  return priority < priority_min (policy, scope)
+           ?  priority + 1
+           :  priority_min (policy, scope);
 #elif defined (ACE_HAS_WTHREADS)
   ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   switch (priority)
     {
       case THREAD_PRIORITY_IDLE:
@@ -182,9 +259,11 @@ ACE_Sched_Params::previous_priority (const Policy policy,
     }
 #elif defined (ACE_HAS_THREADS)
   // including STHREADS, DCETHREADS, and PTHREADS
-  const int min = priority_min (policy);
+  const int min = priority_min (policy, scope);
   return priority > min  ?  priority - 1  :  min;
 #elif
+  ACE_UNUSED_ARG (policy);
+  ACE_UNUSED_ARG (scope);
   ACE_NOTSUP_RETURN (-1);
 #endif /* ACE_HAS_THREADS */
 }
