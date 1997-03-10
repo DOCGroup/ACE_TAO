38690e70dcf5f6452e502215b3273d4ca66d40fe
pjain <pjain@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-03-10 21:35:51 +0000

    New class added

---
+++ java/src/ThruTask.java
@@ -0,0 +1,48 @@
+/*************************************************
+ *
+ * = PACKAGE
+ *    ACE.ASX
+ *
+ * = FILENAME
+ *    ThruTask.java
+ *
+ *@author Prashant Jain
+ *
+ *************************************************/
+package ACE.ASX;
+    
+/**
+ * <hr>
+ * <h2>SYNOPSIS</h2>
+ *<blockquote>
+ *     Standard module that acts as a "no op", simply passing on all
+ *     data to its adjacent neighbor.
+ *</blockquote>
+ */
+public class ThruTask extends Task
+{
+  public int open (Object obj)
+  {
+    return 0;
+  }
+
+  public int close (long flags)
+  {
+    return 0;
+  }
+  
+  public int put (MessageBlock msg, TimeValue tv)
+  {
+    return this.putNext (msg, tv);
+  }
+
+  public int svc ()
+  {
+    return -1;
+  }
+
+  public int handleTimeout (TimeValue tv, Object obj)
+  {
+    return 0;
+  }
+}
