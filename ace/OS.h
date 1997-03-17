810923b7e8e943832c34e929a428d7d0d0e90041
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-03-17 18:59:35 +0000

    added nanosleep (), currently supported only on POSIX platforms

--- ace/OS.h
+++ ace/OS.h
@@ -2423,6 +2423,8 @@ public:
   static int setrlimit (int resource, ACE_SETRLIMIT_TYPE *rl);
   static int sleep (u_int seconds);
   static int sleep (const ACE_Time_Value &tv);
+  static int nanosleep (const struct timespec *requested,
+                        struct timespec *remaining);
 
 #if defined (ACE_HAS_BROKEN_R_ROUTINES)
 #undef ctime_r
