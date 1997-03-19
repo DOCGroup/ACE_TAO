94bb7751883887563d5f136f61b9a5b2fe80be11
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-03-19 04:10:30 +0000

    foo

--- trunk/ace/SOCK_Dgram_Bcast.h
+++ trunk/ace/SOCK_Dgram_Bcast.h
@@ -1,9 +1,7 @@
 /* -*- C++ -*- */
 // $Id$
 
-
 // ============================================================================
-
 // = LIBRARY
 //    ace
 // 
@@ -35,7 +33,7 @@ struct ACE_Bcast_Node
 class ACE_Export ACE_SOCK_Dgram_Bcast : public ACE_SOCK_Dgram
   // = TITLE
   //     Defines the member functions for the ACE_SOCK datagram
-  //	 abstraction. 
+  //     abstraction. 
 {
 public:
   // = Initialization and termination methods.
@@ -43,45 +41,47 @@ public:
   // Default constructor.
 
   ACE_SOCK_Dgram_Bcast (const ACE_Addr &local, 
-			int protocol_family = PF_INET, 
-			int protocol = 0,
-			int reuse_addr = 0);
+                        int protocol_family = PF_INET, 
+                        int protocol = 0,
+                        int reuse_addr = 0,
+                        const char *host_name = 0);
   // Initiate a connectionless datagram broadcast endpoint.
 
   int open (const ACE_Addr &local, 
-	    int protocol_family = PF_INET, 
-	    int protocol = 0,
-	    int reuse_addr = 0);
+            int protocol_family = PF_INET, 
+            int protocol = 0,
+            int reuse_addr = 0,
+            const char *host_name = 0);
   // Initiate a connectionless datagram broadcast endpoint.
 
   int close (void);
   // Close up and release dynamically allocated resources.
 
   ssize_t send (const void *buf, 
-		size_t n, 
-		u_short portnum, 
-		int flags = 0) const;
+                size_t n, 
+                u_short portnum, 
+                int flags = 0) const;
   // Broadcast the datagram to every interface.  Returns the average
   // number of bytes sent.
 
   ssize_t send (const iovec iov[], 
-		size_t n, 
-		u_short portnum, 
-		int flags = 0) const;
+                size_t n, 
+                u_short portnum, 
+                int flags = 0) const;
   // Broadcast the <iovec> datagrams to every interface.  Returns the
   // average number of bytes sent.
 
   ssize_t send (const void *buf, 
-		size_t n, 
-		const ACE_Addr &addr, 
-		int flags = 0) const;
+                size_t n, 
+                const ACE_Addr &addr, 
+                int flags = 0) const;
   // Broadcast an N byte datagram to ADDR (note that addr must be
   // preassigned to the broadcast address of the subnet...).
 
   ssize_t send (const iovec iov[], 
-		size_t n, 
-		const ACE_Addr &addr, 
-		int flags = 0) const;
+                size_t n, 
+                const ACE_Addr &addr, 
+                int flags = 0) const;
   // Broadcast an <iovec> of size <n> to <addr> as a datagram (note
   // that addr must be preassigned to the broadcast address of the
   // subnet...) */
@@ -93,7 +93,7 @@ public:
   // Declare the dynamic allocation hooks.
 
 private:
-  int mk_broadcast (void);
+  int mk_broadcast (const char *host_name);
   // Make broadcast available for Datagram socket.
 
   ACE_Bcast_Node *if_list_;
