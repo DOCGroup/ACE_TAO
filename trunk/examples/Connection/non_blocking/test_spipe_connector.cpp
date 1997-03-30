951d22a1cfa3340cdd5dca88ed2f308aabae53e3
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-03-30 16:19:30 +0000

    added more template instantiations

--- trunk/examples/Connection/non_blocking/test_spipe_connector.cpp
+++ trunk/examples/Connection/non_blocking/test_spipe_connector.cpp
@@ -24,7 +24,17 @@ main (int argc, char *argv[])
 }                                                       
 
 #if defined (ACE_TEMPLATES_REQUIRE_SPECIALIZATION)
+template class ACE_Connector<PEER_HANDLER, ACE_SPIPE_CONNECTOR>;
+template class ACE_Guard<ACE_RW_Mutex>;
+template class ACE_Map_Iterator<int, ACE_Svc_Tuple<PEER_HANDLER> *, ACE_RW_Mutex>;
+template class ACE_Map_Manager<int, ACE_Svc_Tuple<PEER_HANDLER> *, ACE_RW_Mutex>;
+template class ACE_Read_Guard<ACE_RW_Mutex>;
+template class ACE_Svc_Handler<ACE_SPIPE_STREAM, ACE_SYNCH>;
+template class ACE_Svc_Tuple<PEER_HANDLER>;
+template class ACE_Write_Guard<ACE_RW_Mutex>;
+template class ACE_TSS<ACE_Dynamic>;
 template class IPC_Client<PEER_HANDLER, ACE_SPIPE_CONNECTOR>;
+template class Peer_Handler<ACE_SPIPE_STREAM>;
 #endif /* ACE_TEMPLATES_REQUIRE_SPECIALIZATION */
 
 #else
