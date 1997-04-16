36ae50515d451ed82b2609c0806262305e8ca5ac
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-04-16 00:47:42 +0000

    *** empty log message ***

--- netsvcs/lib/Client_Logging_Handler.cpp
+++ netsvcs/lib/Client_Logging_Handler.cpp
@@ -3,63 +3,8 @@
 // Client_Logging_Handler.cpp
 
 #define ACE_BUILD_SVC_DLL
-#include "ace/Service_Config.h"
-#include "ace/Connector.h"
 #include "ace/Get_Opt.h"
-#include "ace/SOCK_Connector.h"
-#include "ace/SOCK_Stream.h"
-#include "ace/FIFO_Recv_Msg.h"
 #include "Client_Logging_Handler.h"
-
-class ACE_Svc_Export ACE_Client_Logging_Handler : public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH>
-  // = TITLE
-  //    This client logging daemon is a mediator that receives logging
-  //    records from local applications processes and forwards them to
-  //    the server logging daemon running on another host. 
-{
-public:
-  // = Initialization and termination.
-
-  ACE_Client_Logging_Handler (const char rendezvous[] = 0);
-  // Default constructor.
-
-  virtual int open (void * = 0);
-  // Activate this instance of the <ACE_Client_Logging_Handler>
-  // (called by the <ACE_Client_Logging_Connector>).
-
-  virtual ACE_HANDLE get_handle (void) const;
-  // Return the handle of the message_fifo_;
-
-  virtual int close (u_long);
-  // Called when object is removed from the ACE_Reactor.
-
-protected:
-  virtual int handle_signal (int signum, siginfo_t *, ucontext_t *);
-  // Handle SIGPIPE.
-
-  virtual int handle_input (ACE_HANDLE);
-  // Receive logging records from applications.
-
-  virtual int handle_exception (ACE_HANDLE);
-  // Receive logging records from applications.  This is necessary to handle
-  // madness with UNIX select, which can't deal with MSG_BAND data easily due
-  // to its overly simple interface...  This just calls <handle_input>.
-
-  virtual int handle_output (ACE_HANDLE);
-  // Called back when it's ok to send.
-  
-  int send (ACE_Log_Record &log_record);
-  // Send the <log_record> to the logging server.
-
-  ACE_FIFO_Recv_Msg message_fifo_;
-  // Message queue we use to receive logging records from clients.
-  
-  ACE_HANDLE logging_output_;
-  // This is either a SOCKET (if we're connected to a logging server)
-  // or ACE_STDOUT.
-
-  static void stderr_output (int = 0);
-};
 
 ACE_Client_Logging_Handler::ACE_Client_Logging_Handler (const char rendezvous[])
   : logging_output_ (ACE_STDOUT)
--- netsvcs/lib/Server_Logging_Handler.h
+++ netsvcs/lib/Server_Logging_Handler.h
@@ -18,7 +18,103 @@
 #if !defined (ACE_SERVER_LOGGING_HANDLER_H)
 #define ACE_SERVER_LOGGING_HANDLER_H
 
-#include "ace/OS.h"
+#include "ace/Synch.h"
+#include "ace/TLI_Acceptor.h"
+#include "ace/SOCK_Acceptor.h"
+#include "ace/Acceptor.h"
+
+template <ACE_PEER_STREAM_1, class COUNTER, ACE_SYNCH_1>
+class ACE_Server_Logging_Handler : public ACE_Svc_Handler<ACE_PEER_STREAM_2, ACE_SYNCH_2>
+{
+  // = TITLE
+  //    Product object created by an <ACE_Server_Logging_Acceptor>.  An
+  //    <ACE_Server_Logging_Handler> receives, frames, and processes logging
+  //    records.
+  // 
+  // = DESCRIPTION
+  //     Defines the classes that perform server logging daemon
+  //     functionality.
+public:
+  ACE_Server_Logging_Handler (ACE_Thread_Manager * = 0);
+
+  virtual int open (void * = 0);
+  // Hook called by <Server_Logging_Acceptor> when connection is
+  // established.
+
+  virtual int handle_input (ACE_HANDLE = ACE_INVALID_HANDLE);
+  // Process remote logging records. 
+
+protected:
+  int handle_logging_record (void);
+  // Receive the logging record from a client.
+  
+#if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  static COUNTER request_count_;
+  // Count the number of logging records that arrive.
+#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+
+  char host_name_[MAXHOSTNAMELEN + 1];
+  // Name of the host we are connected to.
+};
+
+#if !defined (ACE_HAS_TLI)
+#define LOGGING_PEER_ACCEPTOR ACE_SOCK_ACCEPTOR
+#define LOGGING_PEER_STREAM ACE_SOCK_STREAM
+#else /* use sockets */
+#define LOGGING_PEER_ACCEPTOR ACE_TLI_ACCEPTOR
+#define LOGGING_PEER_STREAM ACE_TLI_STREAM
+#endif /* ACE_HAS_TLI */
+
+typedef ACE_Server_Logging_Handler<LOGGING_PEER_STREAM, u_long, ACE_NULL_SYNCH>
+	SERVER_LOGGING_HANDLER;
+
+class ACE_Server_Logging_Acceptor : public ACE_Strategy_Acceptor<SERVER_LOGGING_HANDLER, LOGGING_PEER_ACCEPTOR>
+  // = TITLE
+  //     This class implements the ACE single-threaded logging service.
+  //
+  // = DESCRIPTION
+  //     This class contains the service-specific methods that can't
+  //     easily be factored into the <ACE_Strategy_Acceptor>.
+{
+public:
+  virtual int init (int argc, char *argv[]);
+  // Dynamic linking hook.
+
+  int parse_args (int argc, char *argv[]);
+  // Parse svc.conf arguments.
+
+private:
+  ACE_Schedule_All_Reactive_Strategy<SERVER_LOGGING_HANDLER> scheduling_strategy_;
+  // The scheduling strategy is designed for Reactive services.
+};
+
+#if !defined (ACE_HAS_THREADS) 
+typedef u_long COUNTER;
+#define ACE_LOGGER_SYNCH ACE_NULL_SYNCH 
+#else
+typedef ACE_Atomic_Op <ACE_Thread_Mutex, u_long> COUNTER;
+#define ACE_LOGGER_SYNCH ACE_MT_SYNCH 
+#endif /* ACE_HAS_THREADS */
+
+class ACE_Svc_Export ACE_Thr_Server_Logging_Handler : public ACE_Server_Logging_Handler<LOGGING_PEER_STREAM, COUNTER, ACE_LOGGER_SYNCH> 
+  // = TITLE
+  //    Product object created by a <ACE_Thr_Server_Logging_Acceptor>.  An
+  //    <ACE_Thr_Server_Logging_Handler> receives, frames, and processes
+  //    logging records.
+  //
+  // = DESCRIPTION
+  //     Each client is handled in its own separate thread.
+{
+public:
+  ACE_Thr_Server_Logging_Handler (ACE_Thread_Manager * = 0);
+
+  virtual int open (void * = 0);
+  // Override activation definition in the ACE_Svc_Handler class (will
+  // spawn a new thread if we've got threads).
+
+  virtual int svc (void);
+  // Process remote logging records. 
+};
 
 ACE_SVC_FACTORY_DECLARE (ACE_Server_Logging_Acceptor)
 ACE_SVC_FACTORY_DECLARE (ACE_Thr_Server_Logging_Acceptor)
