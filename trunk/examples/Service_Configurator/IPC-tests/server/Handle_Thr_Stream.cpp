280492629c683615377f282cd0db7cea20fac75b
cleeland <cleeland@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-04-21 16:34:45 +0000

    * examples/Service_Configurator/IPC-tests/server/Handle_Thr_Stream.cpp: Removed some unnecessary explicit template instantiations.
    * examples/Threads/barrier1.cpp (main): Explicitly converted
    n_threads to int.

--- trunk/examples/Service_Configurator/IPC-tests/server/Handle_Thr_Stream.cpp
+++ trunk/examples/Service_Configurator/IPC-tests/server/Handle_Thr_Stream.cpp
@@ -5,6 +5,7 @@
 
 #include "ace/Get_Opt.h"
 #include "ace/INET_Addr.h"
+#include "ace/TLI_Acceptor.h"
 #include "Handle_Thr_Stream.h"
 
 #if defined (ACE_HAS_THREADS)
@@ -188,13 +189,15 @@ HANDLE_THR_ACCEPTOR remote_thr_stream;
 ACE_Service_Object_Type rts (&remote_thr_stream, "Remote_Thr_Stream");
 
 #if defined (ACE_TEMPLATES_REQUIRE_SPECIALIZATION)
+#  if defined(ACE_HAS_TLI)
 template class ACE_Acceptor<CLI_STREAM, ACE_TLI_ACCEPTOR>;
 template class ACE_Accept_Strategy<CLI_STREAM, ACE_TLI_ACCEPTOR>;
+template class ACE_Strategy_Acceptor<CLI_STREAM, ACE_TLI_ACCEPTOR>;
+template class ACE_Svc_Handler<ACE_TLI_STREAM, ACE_MT_SYNCH>;
+#  endif /* ACE_HAS_TLI */
 template class ACE_Concurrency_Strategy<CLI_STREAM>;
 template class ACE_Creation_Strategy<CLI_STREAM>;
 template class ACE_Scheduling_Strategy<CLI_STREAM>;
-template class ACE_Strategy_Acceptor<CLI_STREAM, ACE_TLI_ACCEPTOR>;
-template class ACE_Svc_Handler<ACE_TLI_STREAM, ACE_MT_SYNCH>;
 template class ACE_TSS<ACE_Dynamic>;
 template class ACE_Thread_Strategy<CLI_STREAM>;
 template class CLI_Stream <THR_STREAM>;
