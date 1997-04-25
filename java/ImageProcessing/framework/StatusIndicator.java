946497744df95dfebce2ab67d7d6590213d19df5
pjain <pjain@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-04-25 19:55:36 +0000

    Small changes

--- java/ImageProcessing/framework/StatusIndicator.java
+++ java/ImageProcessing/framework/StatusIndicator.java
@@ -5,43 +5,66 @@ import java.awt.*;
 
 public class StatusIndicator extends Frame
 {
-  public StatusIndicator (String title)
+  public StatusIndicator (String title, Image image)
   {
     super (title);
-
+    this.image_ = image;
+    //    statusCanvas_ = new CounterCanvas (image);
     this.setLayout (new BorderLayout ());
-    this.add ("Center", textCanvas_);
+    this.text_ = new TextField ();
+    this.label_ = new TextField ("Generating");
+
+    this.add ("North", this.label_);
+    this.add ("Center", text_);
+    //    this.add ("Center", statusCanvas_);
     //    this.add ("Center", new Button ("Hello"));
-    this.add ("South", new Button ("Hello"));
+    //    this.add ("South", new Button ("Hello"));
     this.resize (300,200);
     this.show ();
   }
 
   public void update (int count)
   {
-    textCanvas_.setCount (count);
+    //    this.label_.setText ("Generating...");
+    this.text_.setText ((new Integer (count++)).toString ());
+    //    statusCanvas_.update ();
+    //    textCanvas_.setCount (count);
   }
 
-  CounterCanvas textCanvas_ = new CounterCanvas ();
+  TextField text_;
+  TextField label_;
+  Image image_;
+  //  CounterCanvas statusCanvas_ = null;
 }
 
 class CounterCanvas extends Canvas
 {
+  public CounterCanvas (Image image)
+  {
+    this.image_ = image;
+  }
+
   public void paint (Graphics g)
   {
     g.clearRect (0, 0, this.size ().width, this.size ().height);
-    g.setFont (new Font ("TimesRoman", Font.PLAIN, 18));
     this.setBackground (Color.white);
-    g.drawString ("Generating GIF format: ", 20, 20);
-    g.drawString ((new Integer (count_)).toString (), 200, 20);
+
+    if (this.image_ != null)
+      g.drawImage(this.image_, 
+		  40, 40,
+		  this.image_.getWidth (this),
+		  this.image_.getHeight (this),
+		  this);
+
+    g.setFont (new Font ("TimesRoman", Font.PLAIN, 18));
+    g.drawString ("Generating GIF format: ", 120, 120);
   }
 
-  public void setCount (int count)
+  public void update ()
   {
-    count_ = count;
     repaint ();
   }
 
-  int count_ = 0;
+  Image image_ = null;
 }
 
