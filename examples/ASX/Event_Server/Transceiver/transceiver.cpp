b0309804a618e8db3a5d091e0d2b001c24371623
Chris Cleeland <chris.cleeland@gmail.com>
1997-04-28 22:24:35 +0000

    * examples/ASX/Event_Server/Event_Server/Peer_Router.cpp: Added   template specializations.
    * examples/ASX/Event_Server/Transceiver/transceiver.cpp: Added
      template specializations.

--- examples/ASX/Event_Server/Transceiver/transceiver.cpp
+++ examples/ASX/Event_Server/Transceiver/transceiver.cpp
@@ -210,4 +210,7 @@ template class ACE_Connector<Event_Transceiver, ACE_SOCK_CONNECTOR>;
 template class ACE_Svc_Tuple<Event_Transceiver>;
 template class ACE_Map_Iterator<ACE_HANDLE, ACE_Svc_Tuple<Event_Transceiver> *, ACE_SYNCH_RW_MUTEX>;
 template class ACE_Map_Manager<ACE_HANDLE, ACE_Svc_Tuple<Event_Transceiver> *, ACE_SYNCH_RW_MUTEX>;
+template class ACE_Write_Guard<ACE_RW_Thread_Mutex>;
+template class ACE_Read_Guard<ACE_RW_Thread_Mutex>;
+template class ACE_Guard<ACE_RW_Thread_Mutex>;
 #endif /* ACE_TEMPLATES_REQUIRE_SPECIALIZATION */
