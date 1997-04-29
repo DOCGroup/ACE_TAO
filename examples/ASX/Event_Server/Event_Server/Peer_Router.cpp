6f9319329ebe2ca5a0227b05e94c1a0c80545ffd
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-04-29 00:53:11 +0000

    protected the ACE_*Guard template instantiations with #ifdef ACE_HAS_THREADS

--- examples/ASX/Event_Server/Event_Server/Peer_Router.cpp
+++ examples/ASX/Event_Server/Event_Server/Peer_Router.cpp
@@ -350,7 +350,12 @@ Peer_Router::control (ACE_Message_Block *mb)
 template class ACE_Acceptor<Peer_Handler, ACE_SOCK_ACCEPTOR>;
 template class ACE_Map_Iterator<ROUTING_KEY, Peer_Handler *, ACE_SYNCH_RW_MUTEX>;
 template class ACE_Map_Manager<ROUTING_KEY, Peer_Handler *, ACE_SYNCH_RW_MUTEX>;
-template class ACE_Write_Guard<ACE_SYNCH_RW_MUTEX>;
-template class ACE_Read_Guard<ACE_SYNCH_RW_MUTEX>;
-template class ACE_Guard<ACE_SYNCH_RW_MUTEX>;
+
+#if defined (ACE_HAS_THREADS)
+  template class ACE_Guard<ACE_SYNCH_RW_MUTEX>;
+  template class ACE_Read_Guard<ACE_SYNCH_RW_MUTEX>;
+  template class ACE_Write_Guard<ACE_SYNCH_RW_MUTEX>;
+#else
+  // These are specialized in libACE if ACE doesn't have threads.
+#endif /* ACE_HAS_THREADS */
 #endif /* ACE_TEMPLATES_REQUIRE_SPECIALIZATION */
