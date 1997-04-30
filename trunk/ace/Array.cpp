0b4e7199b8ec93202c39ea0459db0b6ef6c89e43
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-04-30 16:10:02 +0000

    they'll compile now :-)

--- trunk/ace/Array.cpp
+++ trunk/ace/Array.cpp
@@ -7,14 +7,13 @@
 #define ACE_BUILD_DLL
 #include "Array.h"
 
-#if !defined (__INLINE__)
-#define INLINE 
+#if !defined (__ACE_INLINE__)
 #include "Array.i"
-#endif /* __INLINE__ */
+#endif /* __ACE_INLINE__ */
 
 // Dynamically initialize an array.
 
-template <class T> 
+template <class T>
 ACE_Array<T>::ACE_Array (size_t size)
   : cur_size_ (size),
     max_size_ (size)
@@ -22,8 +21,8 @@ ACE_Array<T>::ACE_Array (size_t size)
   ACE_NEW (this->array_, T[size]);
 }
 
-template <class T> 
-ACE_Array<T>::ACE_Array (size_t size, 
+template <class T>
+ACE_Array<T>::ACE_Array (size_t size,
 		 const T &default_value)
   : cur_size_ (size),
     max_size_ (size)
@@ -36,20 +35,20 @@ ACE_Array<T>::ACE_Array (size_t size,
 
 // The copy constructor (performs initialization).
 
-template <class T> 
+template <class T>
 ACE_Array<T>::ACE_Array (const ACE_Array<T> &s)
    : cur_size_ (s.size ()),
      max_size_ (s.size ())
 {
-  ACE_NEW (this->array_ T[s.size ()]);
+  ACE_NEW (this->array_, T[s.size ()]);
 
   for (size_t i = 0; i < this->size (); i++)
     this->array_[i] = s.array_[i];
 }
 
-// Assignment operator (performs assignment). 
+// Assignment operator (performs assignment).
 
-template <class T> void 
+template <class T> void
 ACE_Array<T>::operator= (const ACE_Array<T> &s)
 {
   // Check for "self-assignment".
@@ -60,7 +59,7 @@ ACE_Array<T>::operator= (const ACE_Array<T> &s)
     {
       delete [] this->array_;
 
-      ACE_NEW (this->array_ T[s.size ()]);
+      ACE_NEW (this->array_, T[s.size ()]);
 
       this->max_size_ = s.size ();
     }
@@ -71,23 +70,7 @@ ACE_Array<T>::operator= (const ACE_Array<T> &s)
     this->array_[i] = s.array_[i];
 }
 
-// Clean up the array (e.g., delete dynamically allocated memory).
-
-template <class T> 
-ACE_Array<T>::~ACE_Array (void)
-{
-   delete [] this->array_;
-}
-
-// = Set/get methods.
-
-template <class T> T &
-ACE_Array<T>::operator[] (size_t index)
-{
-  return this->array_[index];
-}
-
-// Set an item in the array at location index.  
+// Set an item in the array at location index.
 
 template <class T> int
 ACE_Array<T>::set (const T &new_item, size_t index)
@@ -108,7 +91,10 @@ ACE_Array<T>::get (T &item, size_t index) const
 {
    if (this->in_range (index))
      {
-       item = this->array_[index];
+       item = this->array_[index];  // Copies the item.  If you don't
+                                    // want to copy, use operator [] instead
+                                    // (but then you'll be responsible for
+                                    // range checking).
        return 0;
      }
    else
@@ -124,7 +110,7 @@ ACE_Array<T>::operator== (const ACE_Array<T> &s) const
     return 1;
   else if (this->cur_size_ != s.cur_size_)
     return 0;
-  
+
   for (size_t index = 0; index < s.cur_size_; index++)
     if (this->array_[index] != s.array_[index])
       return 0;
