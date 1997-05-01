6a0a4443d2b146381bbfd9c3fc315a1bbdbaa8b9
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-01 03:39:21 +0000

    *** empty log message ***

--- trunk/examples/Service_Configurator/IPC-tests/server/Handle_Thr_Stream.cpp
+++ trunk/examples/Service_Configurator/IPC-tests/server/Handle_Thr_Stream.cpp
@@ -189,12 +189,12 @@ HANDLE_THR_ACCEPTOR remote_thr_stream;
 ACE_Service_Object_Type rts (&remote_thr_stream, "Remote_Thr_Stream");
 
 #if defined (ACE_TEMPLATES_REQUIRE_SPECIALIZATION)
-#  if defined(ACE_HAS_TLI)
+#if defined (ACE_HAS_TLI)
 template class ACE_Acceptor<CLI_STREAM, ACE_TLI_ACCEPTOR>;
 template class ACE_Accept_Strategy<CLI_STREAM, ACE_TLI_ACCEPTOR>;
 template class ACE_Strategy_Acceptor<CLI_STREAM, ACE_TLI_ACCEPTOR>;
 template class ACE_Svc_Handler<ACE_TLI_STREAM, ACE_MT_SYNCH>;
-#  endif /* ACE_HAS_TLI */
+#endif /* ACE_HAS_TLI */
 template class ACE_Concurrency_Strategy<CLI_STREAM>;
 template class ACE_Creation_Strategy<CLI_STREAM>;
 template class ACE_Scheduling_Strategy<CLI_STREAM>;
