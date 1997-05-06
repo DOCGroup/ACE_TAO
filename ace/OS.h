0c05ff0b69c76529260edf94a6ce478a1b36965a
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-06 22:02:07 +0000

    Added strftime.

--- ace/OS.h
+++ ace/OS.h
@@ -2714,6 +2714,8 @@ public:
   static char *asctime_r (const struct tm *tm, char *buf, int buflen);
   static char *ctime (const time_t *t);
   static char *ctime_r (const time_t *clock, char *buf, int buflen);
+  static size_t strftime (const char *s, size_t maxsize, const char *format,
+                          const struct tm *timeptr);
 
   // = A set of wrappers for memory managment.
   static void *sbrk (int brk);
