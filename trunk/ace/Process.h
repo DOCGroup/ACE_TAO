d9e4d9eb2c0adad4bb9feb0acc3040107e3461f0
harrison <harrison@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-08 05:08:05 +0000

    This contains the first pass at the new ACE_Process class.  It is now called ACE_ProcessEx.  When it is completely ported to Solaris, we'll replace the current ACE_Process with ACE_ProcessEx.

--- trunk/ace/Process.h
+++ trunk/ace/Process.h
@@ -19,6 +19,252 @@
 
 #include "ace/OS.h"
 
+class ACE_Export ACE_Process_Options
+// = TITLE
+//    Process Options
+//
+// = DESCRIPTION
+//    This class controls the options passed to CreateProcess (or fork
+//    and exec).
+{
+public:
+  ACE_Process_Options (int inherit_environment = 1,
+		       int cl_options_buf_len=1024);
+  // If <inherit_environment> == 1, the new process will inherit the
+  // environment of the current process.  <cl_options_buf_len> is the
+  // max strlen for command-line arguments.
+
+  ~ACE_Process_Options (void);
+  // Death.
+
+  // ************************************************************
+  // = These operations are used by applications to portably set
+  // process creation options.
+  // ************************************************************
+
+  int set_handles (ACE_HANDLE std_in,
+		   ACE_HANDLE std_out = ACE_INVALID_HANDLE,
+		   ACE_HANDLE std_err = ACE_INVALID_HANDLE);
+  // Set the standard handles of the new process to the respective
+  // handles.  If you want to affect a subset of the handles, make
+  // sure to set the others to ACE_INVALID_HANDLE.  Returns 0 on
+  // success, -1 on failure.
+
+  int setenv (const char *variable_name, const char *format, ...);
+  // Set a single environment variable, <variable_name>.  Since
+  // different platforms separate each environment variable
+  // differently, you must call this method once for each variable.
+  // <format> can be any printf format string.  
+  // So options->setenv ("FOO","one + two = %s", "three") will result
+  // in "FOO=one + two = three".
+
+  void working_directory (const TCHAR *wd);
+  // Set the working directory for the process.  strlen of <wd> must
+  // be <= MAXPATHLEN.
+
+  void path (LPCTSTR p);
+  // Set the path to the application.  This can either be a full path,
+  // relative path, or just an executable name.  If an executable name
+  // is used, we rely on the platforms support for searching paths.
+  // strlen of <p> must be <= MAXPATHLEN.
+
+  void cl_options (const char *format, ...);
+  // Set the command-line arguments.  <format> can use any printf
+  // formats.  Check this->cl_options_buf and this->argv_cl_options
+  // for accessors.
+
+  // ************************************************************
+  // = These operations are used by ACE_Process to retrieve options
+  // values.
+  // ************************************************************
+
+  LPCTSTR path (void) const;
+  // The full path to the application.
+
+  LPTSTR working_directory (void);
+  // Current working directory.  Returns "" if nothing has been set.
+
+  LPTSTR cl_options_buf (void);
+  // Buffer of command-line options.  Returns exactly what was passed
+  // to this->cl_options.
+
+  char **cl_options_argv (void);
+  // argv-style command-line options.  Parses and modifies the string
+  // created from this->cl_options.  All spaces not in quotes ("" or
+  // '') are replaced with null (\0) bytes.  An argv array is built
+  // and returned with each entry pointing to the start of
+  // null-terminated string.  Returns { 0 } if nothing has been set.
+
+  LPTSTR env_buf (void);
+  // Null-terminated buffer of null terminated strings.  Each string
+  // is an environment assignment "VARIABLE=value".  This buffer
+  // should end with two null characters.
+
+  char **env_argv (void);
+  // argv-style array of environment settings.
+
+#if defined (ACE_WIN32)
+  // = Non-portable accessors for when you "just have to use them."
+
+  BOOL new_console (void) const;
+  // Get.
+  void new_console (BOOL);
+  // Set.
+
+  STARTUPINFO *startup_info (void);
+  // Used for setting and getting.
+
+  const LPSECURITY_ATTRIBUTES get_process_attributes (void) const;
+  // Get the process_attributes.  Returns NULL if
+  // set_process_attributes has not been set.
+  LPSECURITY_ATTRIBUTES set_process_attributes (void);
+  // If this is called, a non-null process attributes is sent to
+  // CreateProcess.
+  const LPSECURITY_ATTRIBUTES get_thread_attributes (void) const;
+  // Get the thread_attributes.  Returns NULL if set_thread_attributes
+  // has not been set.
+  LPSECURITY_ATTRIBUTES set_thread_attributes (void);
+  // If this is called, a non-null thread attributes is sent to
+  // CreateProcess.
+
+  BOOL handle_inheritence (void);
+  // Default is TRUE.
+  void handle_inheritence (BOOL);
+  // Allows disabling of handle inheritence.
+#else /* All things not WIN32 */
+
+  ACE_HANDLE stdin (void);
+  ACE_HANDLE stdout (void);
+  ACE_HANDLE stderr (void);
+
+#endif /* ACE_WIN32 */
+
+protected:
+
+  int setenv_i (LPTSTR assignment, int len);
+  // Add <assignment> to environment_buf_ and adjust
+  // environment_argv_.  <len> is the strlen of <assignment>.
+
+  enum { 
+    MAX_COMMAND_LINE_OPTIONS = 128,
+    ENVIRONMENT_BUFFER = 8192,
+    MAX_ENVIRONMENT_ARGS = 128
+  };
+
+#if defined (ACE_WIN32)
+  void inherit_environment (void);
+  // Helper function to grab win32 environment and stick it in
+  // environment_buf_ using this->setenv_i.
+
+  int inherit_environment_;
+  // Whether the child process inherits the current process
+  // environment.
+
+  int environment_inherited_;
+  // Ensures once only call to inherit environment.
+#endif /* ACE_WIN32 */
+
+  int environment_buf_index_;
+  // Pointer into environment_buf_.  This should point to the next
+  // free spot.
+
+  TCHAR environment_buf_[ENVIRONMENT_BUFFER];
+  // Buffer containing all environment strings.
+
+  int environment_argv_index_;
+  // Pointer to environment_argv_.
+
+  char *environment_argv_[MAX_ENVIRONMENT_ARGS];
+  // Pointers into environment_buf_.
+
+  LPTSTR cl_options_buf_;
+  // Buffer of command-line arguments.  E.g., "-f foo -b bar".
+
+  char *cl_options_;
+  // Buffer containing command line options.
+
+  const char *cl_options_argv_[MAX_COMMAND_LINE_OPTIONS];
+  // Are 64 command-line arguments enough?
+
+  TCHAR working_directory_[MAXPATHLEN + 1];
+  // The current working directory.
+
+  TCHAR path_[MAXPATHLEN + 1];
+  // The application path.
+#if defined (ACE_WIN32)
+  STARTUPINFO startup_info_;
+  int set_handles_called_;
+  // Is 1 if stdhandles was called.
+
+  BOOL handle_inheritence_;
+  // Default TRUE.
+
+  BOOL new_console_;
+  // Default FALSE.
+
+  LPSECURITY_ATTRIBUTES process_attributes_;
+  // Pointer to security_buf1_.
+  LPSECURITY_ATTRIBUTES thread_attributes_;
+  // Pointer to security_buf2_.
+  SECURITY_ATTRIBUTES security_buf1_;
+  // Data for process_attributes_.
+  SECURITY_ATTRIBUTES security_buf2_;
+  // Data for thread_attributes_.
+
+#else /* ACE_WIN32 */
+  ACE_HANDLE stdin_;
+  ACE_HANDLE stdout_;
+  ACE_HANDLE stderr_;
+#endif /* ACE_WIN32 */
+};
+
+// ************************************************************
+
+class ACE_Export ACE_ProcessEx
+// = TITLE
+//     Process
+//
+// = DESCRIPTION
+//     A Portable encapsulation for creating new processes.
+{
+public:
+  ACE_ProcessEx (void);
+  // Default construction.  Must use ACE_Process::start.
+
+  ~ACE_ProcessEx (void);
+  // Destructor.
+
+  pid_t start (ACE_Process_Options &options);
+  // Launch the process described by <options>.
+
+  int wait (void);
+  // Wait for the process we just created to exit.
+
+  int wait (const ACE_Time_Value &tv);
+  // Timed wait for the process we just created to exit.
+
+  int kill (int signum = SIGINT);
+  // Send the process a signal.
+
+  pid_t getpid (void);
+  // Return the pid of the new process.
+
+#if defined (ACE_WIN32)
+  PROCESS_INFORMATION process_info (void);
+#endif /* ACE_WIN32 */
+
+protected:
+#if defined (ACE_WIN32)
+  PROCESS_INFORMATION process_info_;
+#else /* ACE_WIN32 */
+  pid_t child_id_;
+  // Process id of the child.
+#endif /* ACE_WIN32 */
+  
+};
+
+// ************************************************************
+
 class ACE_Export ACE_Process
 // = TITLE
 //     A Portable encapsulation for creating new processes and
@@ -40,10 +286,10 @@ public:
   // Default construction.
 
   ACE_Process (char *argv[],
-	       ACE_HANDLE std_in,
-	       ACE_HANDLE std_out = ACE_INVALID_HANDLE,
-	       ACE_HANDLE std_err = ACE_INVALID_HANDLE,
-	       char *envp[] = 0);
+		      ACE_HANDLE std_in,
+		      ACE_HANDLE std_out = ACE_INVALID_HANDLE,
+		      ACE_HANDLE std_err = ACE_INVALID_HANDLE,
+		      char *envp[] = 0);
   // Set the standard handles of the new process to the respective
   // handles and start the new process (using <execvp>/<execve> on
   // UNIX and <CreateProcess> on Win32>).  If <argv> is non-NULL it
@@ -107,6 +353,78 @@ private:
 
   TCHAR cwd_[MAXPATHLEN + 1];
   // The current working directory.
+};
+
+// ************************************************************
+
+class ACE_Export ACE_Tokenizer
+// = TITLE
+//    Tokenizer
+//
+// = DESCRIPTION
+//    Tokenizes a buffer.  Allows application to set delimiters and
+//    preserve designators.  Does not allow special characters, yet
+//    (e.g., printf ("\"like a quoted string\"").
+{
+public:
+  ACE_Tokenizer (char *buffer);
+  // <buffer> will be parsed.
+
+  int delimiter (char d);
+  // <d> is a delimiter.  Returns 0 on success, -1 if there is no
+  // memory left.
+
+  int delimiter_replace (char d, char replacement);
+  // <d> is a delimiter and, when found, will be replaced by
+  // <replacement>.  Returns 0 on success, -1 if there is no memory
+  // left.
+
+  int preserve_designators (char start, char stop, int strip=1);
+  // For instance, quotes, or '(' and ')'.  Returns 0 on success, -1
+  // if there is no memory left.  If <strip> == 1, then the preserve
+  // designators will be stripped from the tokens returned by next.
+
+  const char *next (void);
+  // Returns the next token.
+
+  enum { 
+    MAX_DELIMITERS=16,
+    MAX_PRESERVES=16
+  };
+
+protected:
+  int is_delimiter (char d, int &replace, char &r);
+  // Returns 1 if <d> is a delimiter, 0 otherwise.  If <d> should be
+  // replaced with <r>, <replace> is set to 1, otherwise 0.
+
+  int is_preserve_designator (char start, char &stop, int &strip);
+  // If <start> is a start preserve designator, returns 1 and sets
+  // <stop> to the stop designator.  Returns 0 if <start> is not a
+  // preserve designator.
+ 
+private:
+  char *buffer_;
+  int index_;
+
+  struct Preserve_Entry
+  {
+    char start_;
+    char stop_;
+    int strip_;
+  };
+
+  Preserve_Entry preserves_[MAX_PRESERVES];
+  int preserves_index_;
+
+  struct Delimiter_Entry
+  {
+    char delimiter_;
+    char replacement_;
+    int replace_;
+  };
+
+  Delimiter_Entry delimiters_[MAX_DELIMITERS];
+  int delimiter_index_;
 };
 
 #if defined (__ACE_INLINE__)
--- trunk/ace/Process.i
+++ trunk/ace/Process.i
@@ -1,6 +1,32 @@
 /* -*- C++ -*- */
 // $Id$
 
+#if defined (ACE_WIN32)
+ACE_INLINE PROCESS_INFORMATION 
+ACE_ProcessEx::process_info (void)
+{
+  return process_info_;
+}
+#endif /* ACE_WIN32 */
+
+
+ACE_INLINE pid_t
+ACE_ProcessEx::getpid (void)
+{
+#if defined (ACE_WIN32)
+  return process_info_.dwProcessId;
+#else /* ACE_WIN32 */
+  return child_id_;
+#endif /* ACE_WIN32 */
+}
+
+ACE_INLINE int
+ACE_ProcessEx::kill (int signum)
+{
+  return ACE_OS::kill (this->getpid (), signum);
+}
+
+// ************************************************************
 
 ACE_INLINE pid_t
 ACE_Process::getpid (void)
@@ -16,4 +42,119 @@ ACE_INLINE int
 ACE_Process::kill (int signum)
 {
   return ACE_OS::kill (this->getpid (), signum);
+}
+
+// ************************************************************
+
+#if defined (ACE_WIN32)
+
+ACE_INLINE BOOL
+ACE_Process_Options::new_console (void) const
+{
+  return new_console_;
+}
+
+ACE_INLINE void
+ACE_Process_Options::new_console (BOOL nc)
+{
+  new_console_ = nc;
+}
+
+ACE_INLINE STARTUPINFO *
+ACE_Process_Options::startup_info (void)
+{
+  return &startup_info_;
+}
+
+ACE_INLINE const LPSECURITY_ATTRIBUTES 
+ACE_Process_Options::get_process_attributes (void) const
+{  
+  return process_attributes_;
+}
+ 
+ACE_INLINE LPSECURITY_ATTRIBUTES 
+ACE_Process_Options::set_process_attributes (void)
+{
+  process_attributes_ = &security_buf1_;
+  return process_attributes_;
+}
+ 
+ACE_INLINE const LPSECURITY_ATTRIBUTES 
+ACE_Process_Options::get_thread_attributes (void) const
+{
+  return thread_attributes_;
+}
+
+ACE_INLINE LPSECURITY_ATTRIBUTES 
+ACE_Process_Options::set_thread_attributes (void)
+{
+  thread_attributes_ = &security_buf2_;
+  return thread_attributes_;
+}
+
+#else /* !defined (ACE_WIN32) */
+
+ACE_INLINE ACE_HANDLE
+ACE_Process_Options::stdin (void)
+{
+  return stdin_;
+}
+
+ACE_INLINE ACE_HANDLE 
+ACE_Process_Options::stdout (void)
+{
+  return stdout_;
+}
+
+ACE_INLINE ACE_HANDLE 
+ACE_Process_Options::stderr (void)
+{
+  return stderr_;
+}
+
+#endif /* ACE_WIN32 */
+
+ACE_INLINE LPTSTR
+ACE_Process_Options::cl_options_buf (void)
+{
+  return cl_options_;
+}
+
+ACE_INLINE LPTSTR 
+ACE_Process_Options::working_directory (void)
+{
+  if (working_directory_[0] == '\0')
+    return 0;
+  else
+    return working_directory_;
+}
+
+ACE_INLINE void
+ACE_Process_Options::working_directory (const TCHAR *wd)
+{
+  ACE_OS::strcpy (working_directory_, wd);
+}
+
+ACE_INLINE LPCTSTR
+ACE_Process_Options::path (void) const
+{
+  return path_;
+}
+
+ACE_INLINE void
+ACE_Process_Options::path (LPCTSTR p)
+{
+  ACE_OS::strcpy (path_, p);
+}
+
+ACE_INLINE BOOL
+ACE_Process_Options::handle_inheritence (void)
+{
+  return handle_inheritence_;
+}
+
+ACE_INLINE void
+ACE_Process_Options::handle_inheritence (BOOL hi)
+{
+  handle_inheritence_ = hi;
 }
