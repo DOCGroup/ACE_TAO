59db3262fd406fa73141a44aeb22cdf0567f8e83
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-05-09 05:58:59 +0000

    *** empty log message ***

--- ace/Acceptor.cpp
+++ ace/Acceptor.cpp
@@ -731,7 +731,7 @@ ACE_Oneshot_Acceptor<SVC_HANDLER, ACE_PEER_ACCEPTOR_2>::handle_timeout
   errno = ETIMEDOUT;
 
   if (this->svc_handler_->handle_timeout (tv, arg) == -1)
-    this->svc_handler_->handle_close (sh->get_handle (), 
+    this->svc_handler_->handle_close (this->svc_handler_->get_handle (), 
 				      ACE_Event_Handler::TIMER_MASK);;
 
   // Since we aren't necessarily registered with the Reactor, don't
