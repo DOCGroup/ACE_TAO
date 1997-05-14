7f112ff6968e4fee2a38bcca1a6c1f6630d75e37
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-05-14 22:48:54 +0000

    *** empty log message ***

--- ace/Singleton.cpp
+++ ace/Singleton.cpp
@@ -18,49 +18,72 @@ ACE_Singleton<TYPE, LOCK>::dump (void)
   ACE_TRACE ("ACE_Singleton<TYPE, LOCK>::dump");
 
 #if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
-  ACE_DEBUG ((LM_DEBUG, "instance_ = %x", instance_));
-  ace_singleton_lock_.dump ();
+  ACE_DEBUG ((LM_DEBUG, "instance_ = %x", 
+	      ACE_Singleton<TYPE, LOCK>::instance_i ()));
+  ACE_Singleton<TYPE, LOCK>::singleton_lock_i ().dump ();
   ACE_DEBUG ((LM_DEBUG, ACE_END_DUMP));
+#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+}
+
+template <class TYPE, class LOCK> TYPE *&
+ACE_Singleton<TYPE, LOCK>::instance_i (void)
+{
+#if defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  // Pointer to the Singleton instance.  This works around a bug with
+  // G++...
+  static TYPE *instance_ = 0;
+
+  return instance_;
+#else
+  return ACE_Singleton<TYPE, LOCK>::instance_;
+#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+}
+
+template <class TYPE, class LOCK> LOCK &
+ACE_Singleton<TYPE, LOCK>::singleton_lock_i (void)
+{
+#if defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  // Lock the creation of the singleton.  This works around a
+  // "feature" of G++... ;-)
+  static LOCK ace_singleton_lock_;
+
+  return ace_singleton_lock_;
+#else
+  return ACE_Singleton<TYPE, LOCK>::ace_singleton_lock_;
 #endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
 }
 
 template <class TYPE, class LOCK> TYPE *
 ACE_Singleton<TYPE, LOCK>::instance (void)
 {
-  ACE_TRACE ("ACE_Singleton::instance");
+  ACE_TRACE ("ACE_Singleton<TYPE, LOCK>::instance");
 
-#if defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
-  // Pointer to the Singleton instance.  This works around a bug with
-  // G++...
-  static TYPE *instance_ = 0;
-
-  // Lock the creation of the singleton.  This works around a
-  // "feature" of G++... ;-)
-  static LOCK ace_singleton_lock_;
-  // Perform the Double-Check pattern...
-  if (instance_ == 0)
-    {
-      ACE_GUARD_RETURN (LOCK, ace_mon, ace_singleton_lock_, 0);
-
-      if (instance_ == 0)
-	ACE_NEW_RETURN (instance_, TYPE, 0);
-    }
-
-  return instance_;
-#else
+  TYPE *&singleton = ACE_Singleton<TYPE, LOCK>::instance_i ();
 
   // Perform the Double-Check pattern...
-  if (ACE_Singleton<TYPE, LOCK>::instance_ == 0)
+  if (singleton == 0)
     {
-      ACE_GUARD_RETURN (LOCK, ace_mon, (ACE_Singleton<TYPE, LOCK>::ace_singleton_lock_), 0);
+      ACE_GUARD_RETURN (LOCK, ace_mon, ACE_Singleton<TYPE, LOCK>::singleton_lock_i (), 0);
 
-      if (ACE_Singleton<TYPE, LOCK>::instance_ == 0)
-	ACE_NEW_RETURN ((ACE_Singleton<TYPE, LOCK>::instance_), TYPE, 0);
+      if (singleton == 0)
+	ACE_NEW_RETURN (singleton, TYPE, 0);
     }
 
-  return ACE_Singleton<TYPE, LOCK>::instance_;
+  return singleton;
+}
 
-#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+template <class TYPE, class LOCK> TYPE *
+ACE_Singleton<TYPE, LOCK>::instance (TYPE *new_instance)
+{
+  ACE_TRACE ("ACE_Singleton::set_instance");
+
+  TYPE *&singleton = ACE_Singleton<TYPE, LOCK>::instance_i ();
+  ACE_GUARD_RETURN (LOCK, ace_mon, ACE_Singleton<TYPE, LOCK>::singleton_lock_i (), 0);
+
+  TYPE *old_instance = singleton;
+  singleton = new_instance;
+
+  return singleton;
 }
 
 #if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
@@ -71,6 +94,79 @@ ACE_Singleton<TYPE, LOCK>::instance_ = 0;
 // Lock the creation of the singleton.  
 template <class TYPE, class LOCK> LOCK
 ACE_Singleton<TYPE, LOCK>::ace_singleton_lock_;
+#endif /* !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES) */
+
+template <class TYPE, class LOCK, ACE_Singleton_Strategy MEMORY> void
+ACE_SingletonEx<TYPE, LOCK, MEMORY>::dump (void)
+{
+  ACE_TRACE ("ACE_SingletonEx<TYPE, LOCK, MEMORY>::dump");
+
+#if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  ACE_DEBUG ((LM_DEBUG, "instance_ = %x", 
+	      ACE_Singleton<TYPE, LOCK>::instance_i ()));
+  ACE_Singleton<TYPE, LOCK>::singleton_lock_i ().dump ();
+  ACE_DEBUG ((LM_DEBUG, ACE_END_DUMP));
+#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+}
+
+template <class TYPE, class LOCK, ACE_Singleton_Strategy MEMORY> TYPE *&
+ACE_SingletonEx<TYPE, LOCK, MEMORY>::instance_i (void)
+{
+#if defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  // Pointer to the Singleton instance.  This works around a bug with
+  // G++...
+  static TYPE *instance_ = 0;
+
+  return instance_;
+#else
+  return ACE_SingletonEx<TYPE, LOCK, MEMORY>::instance_;
+#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+}
+
+template <class TYPE, class LOCK, ACE_Singleton_Strategy MEMORY> LOCK &
+ACE_SingletonEx<TYPE, LOCK, MEMORY>::singleton_lock_i (void)
+{
+#if defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  // Lock the creation of the singleton.  This works around a
+  // "feature" of G++... ;-)
+  static LOCK ace_singletonex_lock_;
+
+  return ace_singletonex_lock_;
+#else
+  return ACE_SingletonEx<TYPE, LOCK, MEMORY>::ace_singletonex_lock_;
+#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+}
+
+template <class TYPE, class LOCK, ACE_Singleton_Strategy MEMORY> TYPE *
+ACE_SingletonEx<TYPE, LOCK, MEMORY>::instance (void)
+{
+  ACE_TRACE ("ACE_SingletonEx<TYPE, LOCK, MEMORY>::instance");
+
+  TYPE *&singleton = ACE_SingletonEx<TYPE, LOCK>::instance_i ();
+
+  // Perform the Double-Check pattern...
+  if (singleton == 0)
+    {
+      ACE_GUARD_RETURN (LOCK, ace_mon, ACE_SingletonEx<TYPE, LOCK>::singleton_lock_i (), 0);
+
+      if (singleton == 0)
+	ACE_NEW_RETURN (singleton, TYPE, 0);
+    }
+
+  if (MEMORY == ACE_SINGLETON_HEAP)
+    return singleton;
+  else
+    return ACE_TSS_GET (singleton, TYPE);
+}
+
+#if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+// Pointer to the Singleton instance.
+template <class TYPE, class LOCK, ACE_Singleton_Strategy MEMORY> TYPE *
+ACE_SingletonEx<TYPE, LOCK, MEMORY>::instance_ = 0;
+
+// Lock the creation of the singleton.  
+template <class TYPE, class LOCK, ACE_Singleton_Strategy MEMORY> LOCK
+ACE_SingletonEx<TYPE, LOCK, MEMORY>::ace_singletonex_lock_;
 #endif /* !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES) */
 
 #endif /* ACE_SINGLETON_C */
