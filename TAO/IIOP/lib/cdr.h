97d5bb291d35a313e612e787f6b3de256580ef2d
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-05-14 02:53:31 +0000

    *** empty log message ***

--- TAO/IIOP/lib/cdr.h
+++ TAO/IIOP/lib/cdr.h
@@ -1,31 +1,46 @@
 // This may look like C, but it's really -*- C++ -*-
-//
-// @(#) $Id$
+
 // Copyright 1994-1995 by Sun Microsystems Inc.
 // All Rights Reserved
 //
-// CDR:		 Common Data Representation (CDR) marshaling streams.
+// ============================================================================
 //
-// This implementation assumes that the native numeric representation
-// is two's complement for integers, IEEE single/double for floats.  Also
-// that characters are in ISO Latin/1.
+// = LIBRARY
+//    TAO
+// 
+// = FILENAME
+//    cdr.h
 //
-// Note that CDR itself makes no such assumptions, but this implementation
-// makes such assumptions for reasons of efficiency.  Careful enhancements
-// could preserve that efficiency where the assumptions are true, yet
-// still allow the code to work when they aren't true.
+// = DESCRIPTION
+//     Common Data Representation (CDR) marshaling streams.
 //
-// The implementation expects that buffers are aligned according to the
-// strongest CDR alignment restriction.
+//     This implementation assumes that the native numeric
+//     representation is two's complement for integers, IEEE
+//     single/double for floats.  Also that characters are in ISO
+//     Latin/1.
 //
-// NOTE: this does everything "CDR 1.1" does ... that is, it supports
-// the five extended OMG-IDL data types in UNO Appendix A, which provide
-// richer arithmetic types (64 bit integers, "quad precision" FP) and
-// UNICODE-based characters and strings.  Those types are not standard
-// parts of OMG-IDL at this time.
+//     Note that CDR itself makes no such assumptions, but this
+//     implementation makes such assumptions for reasons of
+//     efficiency.  Careful enhancements could preserve that
+//     efficiency where the assumptions are true, yet still allow the
+//     code to work when they aren't true.
 //
-// THREADING NOTE:  CDR data structures must be protected against
-// concurrent access by their owning thread.
+//     The implementation expects that buffers are aligned according
+//     to the strongest CDR alignment restriction.
+//
+//     NOTE: this does everything "CDR 1.1" does ... that is, it
+//     supports the five extended OMG-IDL data types in UNO Appendix
+//     A, which provide richer arithmetic types (64 bit integers,
+//     "quad precision" FP) and UNICODE-based characters and strings.
+//     Those types are not standard parts of OMG-IDL at this time.
+//
+//     THREADING NOTE: CDR data structures must be protected against
+//     concurrent access by their owning thread.
+//
+// = AUTHOR
+//     Copyright 1994-1995 by Sun Microsystems, Inc.
+// 
+// ============================================================================
 
 #if !defined (TAO_CDR_H)
 #define	TAO_CDR_H
@@ -41,24 +56,25 @@
 
 #include	<align.h>
 
-
+// Identify byte order ... this is basically dependent on processor,
+// but some processors support different byte orders (e.g. MIPS,
+// UltraSPARC, PowerPC) as required for different software
+// environments.
 //
-// Identify byte order ... this is basically dependent on processor, but some
-// processors support different byte orders (e.g. MIPS, UltraSPARC, PowerPC)
-// as required for different software environments.
-//
-// We currently get this information through the "configure" script, which
-// must (!!) be run before source code can be correctly compiled; too bad
-// we have no way to ensure that the machine on which "configure" ran is
-// the same as the one on which "make" will be run.
+// We currently get this information through the "configure" script,
+// which must (!!) be run before source code can be correctly
+// compiled; too bad we have no way to ensure that the machine on
+// which "configure" ran is the same as the one on which "make" will
+// be run.
 
 #undef	LITTLE_ENDIAN
 #undef	BIG_ENDIAN
 #undef	MY_BYTE_SEX
 
 // PC compilers normally don't assume they'll be given code that runs
-// on other architectures; cope with that limitation once here, so that
-// only truly compiler-specific code needs to use their predefines.
+// on other architectures; cope with that limitation once here, so
+// that only truly compiler-specific code needs to use their
+// predefines.
 //
 // NOTE:  should really be using "__i386" to be strictly ANSI compliant.
 //
@@ -69,10 +85,9 @@
 #	endif
 #endif
 
-//
 // Yes, we assume no bizarre mixed-endian "nuxi" machine, just as we
 // assume only IEEE arithmetic.
-//
+
 #if defined (WORDS_BIGENDIAN)
 #	define	BIG_ENDIAN
 #	define	MY_BYTE_SEX	0
@@ -82,29 +97,30 @@
 #endif
 
 struct ACE_Svc_Export CDR
-// = TITLE
-// The core marshaling primitive: a memory buffer, into which all the
-// basic OMG-IDL datatypes can be placed ... or from which they can be
-// retreived.
-//
-// = DESCRIPTION
-// A particularly useful static member function for this buffer is an
-// interpretive encoding routine, usable as a typecode interpreter
-// callback.  Ditto for decoding.  These are used to support all
-// OMG-IDL datatypes, even those not supported directly by put/get
-// primitives.
-//
-// Struct members are intentionally exposed; the functionality of this
-// class, and hence the appropriate abstactions for them, hasn't quite
-// settled down enough to settle on fast abstractions that let data be
-// hidden without pointlessly sacrificing speed.
+  // = TITLE
+  // The core marshaling primitive: a memory buffer, into which all the
+  // basic OMG-IDL datatypes can be placed ... or from which they can be
+  // retreived.
+  //
+  // = DESCRIPTION
+  // A particularly useful static member function for this buffer is an
+  // interpretive encoding routine, usable as a typecode interpreter
+  // callback.  Ditto for decoding.  These are used to support all
+  // OMG-IDL datatypes, even those not supported directly by put/get
+  // primitives.
+  //
+  // Struct members are intentionally exposed; the functionality of this
+  // class, and hence the appropriate abstactions for them, hasn't quite
+  // settled down enough to settle on fast abstractions that let data be
+  // hidden without pointlessly sacrificing speed.
 {
-  // Define these constants as enums to ensure they get inlined
-  // and to avoid pointless static memory allocations.
+  // = Constants defined by the CDR protocol.  
+
+  // By defining these constants as enums we ensure they get inlined
+  // and avoid pointless static memory allocations.
+
   enum 
   {
-    // = Constants defined by the CDR protocol.  
-
     // Note that some of these get reused as part of the standard
     // binary format: unsigned is the same size as its signed cousin,
     // float is CDR_LONG_SIZE, and double is CDR_LONGLONG_SIZE.
@@ -115,16 +131,17 @@ struct ACE_Svc_Export CDR
     LONGDOUBLE_SIZE = 16,
 
     MAX_ALIGNMENT = 16,
-    // maximal CDR 1.1 alignment:   "quad precision" FP
-    // (i.e. "long double", size as above)
+    // Maximal CDR 1.1 alignment: "quad precision" FP (i.e. "long
+    // double", size as above).
 
-    DEFAULT_BUFSIZE = 1430		// Ethernet MTU, less headers
-    // Default buffer size for request/response messages.  These are
-    // normally stack-allocated, and tuning may cause you to want to
-    // change this value.  The best value depends on your particular
-    // application mix; you can also change how buffers grow().  Most
-    // remote invocations (statistically) are "small", and the default
-    // used here is perhaps larger than most such messages.
+    DEFAULT_BUFSIZE = 1430 
+    // Ethernet MTU, less headers.  Default buffer size for
+    // request/response messages.  These are normally stack-allocated,
+    // and tuning may cause you to want to change this value.  The
+    // best value depends on your particular application mix; you can
+    // also change how buffers grow().  Most remote invocations
+    // (statistically) are "small", and the default used here is
+    // perhaps larger than most such messages.
     // 
     // If this size is "too small" you need to heap-allocate buffers too
     // often.  "Too large" is mostly a waste of stackspace, but stack
@@ -140,28 +157,26 @@ struct ACE_Svc_Export CDR
   // (4 bytes), doubleword (8 bytes) and quadword (16 byte) entities,
   // plus the interpretive encoder.
 
-  CORBA_Boolean		put_byte (char c);
-  CORBA_Boolean		put_short (CORBA_Short s);
-  CORBA_Boolean		put_long (CORBA_Long l);
-  CORBA_Boolean		put_longlong (const CORBA_LongLong &ll);
+  CORBA_Boolean put_byte (char c);
+  CORBA_Boolean put_short (CORBA_Short s);
+  CORBA_Boolean put_long (CORBA_Long l);
+  CORBA_Boolean put_longlong (const CORBA_LongLong &ll);
 
-  inline CORBA_Boolean	put_char (CORBA_Char c)
+  // @@ Please put this into the *.i file.
+  inline CORBA_Boolean put_char (CORBA_Char c)
     { return put_byte ((char) c); }
 
-  inline CORBA_Boolean	put_wchar (CORBA_WChar wc)
+  inline CORBA_Boolean put_wchar (CORBA_WChar wc)
     {
-      //
-      // "wchar_t" isn't always 2 bytes, such
-      // systems might need further conversion
-      // (e.g. hosts with multibyte characters
+      // "wchar_t" isn't always 2 bytes, such systems might need
+      // further conversion (e.g. hosts with multibyte characters
       // native, rather than UNICODE)
-      //
-      return put_short ((short)wc);
+
+      return put_short ((short) wc);
     }
     
-  inline CORBA_Boolean	put_boolean (CORBA_Boolean b)
-    { return put_byte ((char)
-		       (b != CORBA_B_FALSE)); }
+  inline CORBA_Boolean put_boolean (CORBA_Boolean b)
+    { return put_byte ((char) (b != CORBA_B_FALSE)); }
 
   inline CORBA_Boolean	put_octet (CORBA_Octet o)
     { return put_byte ((char) o); }
@@ -179,18 +194,17 @@ struct ACE_Svc_Export CDR
     { return put_long (*(CORBA_Long *) &f); }
 
   inline CORBA_Boolean	put_double (const double &d)
-    { return
-	put_longlong (*(CORBA_LongLong *) &d); }
+    { return put_longlong (*(CORBA_LongLong *) &d); }
 
-  CORBA_Boolean		put_longdouble (CORBA_LongDouble &ld);
+  CORBA_Boolean put_longdouble (CORBA_LongDouble &ld);
 
   static CORBA_TypeCode::traverse_status
     encoder (CORBA_TypeCode_ptr	tc,
-	     const void		*data,
-	     const void     		*,
-	     void			*context,
-	     CORBA_Environment	&env);
-  // Marshaling interpreter ... 'context' really points to a CDR.
+	     const void *data,
+	     const void *,
+	     void *context,
+	     CORBA_Environment &env);
+  // Marshaling interpreter ... <context> really points to a <CDR>.
 
   // = DECODING SUPPORT 
 
@@ -198,68 +212,70 @@ struct ACE_Svc_Export CDR
   // determine whether decode should byteswap or not.  It's cheaper to
   // do it that way than to use virtual functions.
 
-  CORBA_Boolean		get_byte (char &c);
-  CORBA_Boolean		get_short (CORBA_Short &s);
-  CORBA_Boolean		get_long (CORBA_Long &l);
-  CORBA_Boolean		get_longlong (CORBA_LongLong &ll);
+  CORBA_Boolean get_byte (char &c);
+  CORBA_Boolean get_short (CORBA_Short &s);
+  CORBA_Boolean get_long (CORBA_Long &l);
+  CORBA_Boolean get_longlong (CORBA_LongLong &ll);
 
-  inline CORBA_Boolean	get_char (CORBA_Char &o)
-    { return get_byte ((char &) o); }
+  // @@ These methods should be moved into the *.i file.
 
-  inline CORBA_Boolean	get_wchar (CORBA_WChar &wc)
+  inline CORBA_Boolean get_char (CORBA_Char &o)
+    { return this->get_byte ((char &) o); }
+
+  inline CORBA_Boolean get_wchar (CORBA_WChar &wc)
     {
       short s;
 
-      //
       // wchar_t isn't always "short"
-      //
-      CORBA_Boolean retval = get_short (s);
+
+      CORBA_Boolean retval = this->get_short (s);
       wc = s;
       return retval;
     }
 
-  inline CORBA_Boolean	get_boolean (CORBA_Boolean &b)
+  inline CORBA_Boolean get_boolean (CORBA_Boolean &b)
     {
       CORBA_Char c;
 
       //
       // CORBA_Boolean is rarely 'char'
       //
-      CORBA_Boolean retval = get_char (c);
+      CORBA_Boolean retval = this->get_char (c);
       b = (c == 1);
       return retval;
     }
 
   inline CORBA_Boolean	get_octet (CORBA_Octet &o)
-    { return get_byte ((char &) o); }
+    { return this->get_byte ((char &) o); }
 
   inline CORBA_Boolean	get_ushort (CORBA_UShort &s)
-    { return get_short ((short&) s); }
+    { return this->get_short ((short&) s); }
 
   inline CORBA_Boolean	get_ulong (CORBA_ULong &l)
-    { return get_long ((CORBA_Long &) l); }
+    { return this->get_long ((CORBA_Long &) l); }
 
   inline CORBA_Boolean	get_ulonglong (const CORBA_ULongLong &ull)
-    { return get_longlong ((CORBA_LongLong &) ull); }
+    { return this->get_longlong ((CORBA_LongLong &) ull); }
 
   inline CORBA_Boolean	get_float (float &f)
-    { return get_long ((CORBA_Long &) f); }
+    { return this->get_long ((CORBA_Long &) f); }
 
   inline CORBA_Boolean	get_double (double &d)
-    { return get_longlong ((CORBA_LongLong &) d); }
+    { return this->get_longlong ((CORBA_LongLong &) d); }
 
   CORBA_Boolean get_longdouble (CORBA_LongDouble &ld);
 
   static CORBA_TypeCode::traverse_status
     decoder (CORBA_TypeCode_ptr	tc,
-	     const void		*data,
-	     const void		*,
-	     void			*context,
-	     CORBA_Environment	&env);
-  // Unmarshaling interpreter ... 'context' really points to a CDR.
+	     const void *data,
+	     const void *,
+	     void *context,
+	     CORBA_Environment &env);
+  // Unmarshaling interpreter ... <context> really points to a <CDR>.
 
+  // @@ Please put this into the *.i file.
   CDR (u_char *buf = 0,
-       unsigned len = 0,
+       u_int len = 0,
        int byte_order = MY_BYTE_SEX,
        int consume_buf = 0) 
   // Constructor ... buffer must be aligned for the strictest CDR
@@ -277,8 +293,8 @@ struct ACE_Svc_Export CDR
       temp &= ~((ptr_arith_t) MAX_ALIGNMENT - 1);
       buffer = next = (u_char *) temp;
 
-      length = remaining
-	= len - (unsigned) (buffer - real_buffer);
+      remaining = len - (u_int) (buffer - real_buffer);
+      length = remaining;
     }
 
   ~CDR (void)
@@ -296,8 +312,8 @@ struct ACE_Svc_Export CDR
   // These may change the state of a CDR buffer even when errors are
   // reported.
 
-  CORBA_Boolean		skip_string ();
-  CORBA_Boolean		skip_bytes (u_int nbytes)
+  CORBA_Boolean skip_string (void);
+  CORBA_Boolean skip_bytes (u_int nbytes)
     {
       if (remaining < nbytes)
 	return CORBA_B_FALSE;
@@ -324,7 +340,7 @@ struct ACE_Svc_Export CDR
   // grow it by a standard quantum (e.g. when encoding we can't know
   // in advance how big it will need to become).
 
-  size_t bytes_remaining () { return remaining; }
+  size_t bytes_remaining (void) { return remaining; }
   // Some code needs to know how much is left on encode or decode
 
   // private:
@@ -340,12 +356,16 @@ struct ACE_Svc_Export CDR
   // maybe not aligned
 
   int do_free;
+  // Shall we free the memory?
 
   u_char *buffer;
+  // Points to the dynamically allocated memory.
+
   size_t length;
+  // Length of the dynamically allocated memory.
 
   int do_byteswap;	
-  // decode ONLY
+  // Decode ONLY.
 };
 
-#endif	/* TAO_CDR_H */
+#endif /* TAO_CDR_H */
--- TAO/IIOP/lib/corbacom.h
+++ TAO/IIOP/lib/corbacom.h
@@ -5,7 +5,6 @@
 // All Rights Reserved
 //
 // CORBA C/C++/COM mapping for Win32
-//
 
 #if !defined (TAO_CORBACOM_H)
 #define TAO_CORBACOM_H
@@ -17,51 +16,50 @@ typedef bool			CORBA_Boolean;
 #define CORBA_B_FALSE false
 #define CORBA_B_TRUE true
 
-#else	// "bool" not builtin to this compiler
+#else	/* "bool" not builtin to this compiler */
 typedef int			CORBA_Boolean;
 enum { CORBA_B_FALSE = 0, CORBA_B_TRUE = 1 };
-#endif	// "bool" not builtin
+#endif /* "bool" not builtin */
 
-typedef unsigned char		CORBA_Octet;
+typedef u_char		CORBA_Octet;
 
 typedef short			CORBA_Short;
-typedef unsigned short		CORBA_UShort;
+typedef u_short		CORBA_UShort;
 
 //
 // CORBA "Long" (and its unsigned cousin) are 32 bits, just like
 // on almost all C/C++ compilers.
 //
 #if	SIZEOF_LONG == 4
-typedef long			CORBA_Long;
-typedef unsigned long		CORBA_ULong;
+typedef long CORBA_Long;
+typedef u_long CORBA_ULong;
 #else
 // just assume "int" is 4 bytes long ...
-typedef int			CORBA_Long;
-typedef unsigned		CORBA_ULong;
-#endif	// SIZEOF_LONG != 4
+typedef int CORBA_Long;
+typedef u_int CORBA_ULong;
+#endif	/* SIZEOF_LONG != 4 */
 
-//
 // 94-9-32 Appendix A, also the OMG C++ mapping, stipulate that
 // 64 bit integers are "LongLong".
 //
 // NOTE:  those are IDL extensions, not yet standard.
-//
+
 #if	SIZEOF_LONG_LONG == 8
-typedef long long		CORBA_LongLong;
-typedef unsigned long long	CORBA_ULongLong;
+typedef long long CORBA_LongLong;
+typedef unsigned long long CORBA_ULongLong;
 #elif	SIZEOF_LONG == 8
-typedef long			CORBA_LongLong;
-typedef unsigned long		CORBA_ULongLong;
+typedef long CORBA_LongLong;
+typedef u_long CORBA_ULongLong;
 #elif	defined (_MSC_VER) && _MSC_VER >= 900
-typedef __int64			CORBA_LongLong;
-typedef unsigned __int64	CORBA_ULongLong;
+typedef __int64 CORBA_LongLong;
+typedef unsigned __int64 CORBA_ULongLong;
 #else
-//
-// If "long long" isn't native, programs can't use these
-// data types in normal arithmetic expressions.  If any
-// particular application can cope with the loss of range
-// it can define conversion operators itself.
-//
+
+// If "long long" isn't native, programs can't use these data types in
+// normal arithmetic expressions.  If any particular application can
+// cope with the loss of range it can define conversion operators
+// itself.
+
 #   define	NONNATIVE_LONGLONG
 #if	defined (WORDS_BIGENDIAN)
 struct CORBA_LongLong { CORBA_Long h, l; };
@@ -69,39 +67,42 @@ struct CORBA_ULongLong { CORBA_Long h, l; };
 #else
 struct CORBA_LongLong { CORBA_Long l, h; };
 struct CORBA_ULongLong { CORBA_ULong l, h; };
-#endif	// !WORDS_BIGENDIAN
-#endif	// no native 64 bit integer type
+#endif /* !WORDS_BIGENDIAN */
+#endif /* no native 64 bit integer type */
 
-typedef float		CORBA_Float;
-typedef double		CORBA_Double;
+typedef float CORBA_Float;
+typedef double CORBA_Double;
 
-//
-// 94-9-32 Appendix A defines a 128 bit floating point "long
-// double" data type, with greatly extended precision and
-// four more bits of exponent (compared to "double").
+// 94-9-32 Appendix A defines a 128 bit floating point "long double"
+// data type, with greatly extended precision and four more bits of
+// exponent (compared to "double").
 //
 // NOTE:  that is an IDL extension, not yet standard.
-//
+
 #if	SIZEOF_LONG_DOUBLE == 16
-typedef long double	CORBA_LongDouble;
+typedef long double CORBA_LongDouble;
 #else
 #define	NONNATIVE_LONGDOUBLE
-struct CORBA_LongDouble {
-    char	ld [16];
+struct CORBA_LongDouble 
+{
+  char ld[16];
 };
-#endif	// SIZEOF_LONG_DOUBLE != 16
+#endif /* SIZEOF_LONG_DOUBLE != 16 */
 
-typedef char 		CORBA_Char;
-typedef CORBA_Char	*CORBA_String;
+typedef char CORBA_Char;
+typedef CORBA_Char *CORBA_String;
 
-CORBA_String		CORBA_string_alloc (CORBA_ULong len);
-CORBA_String		CORBA_string_copy (const CORBA_Char *const);
-CORBA_String		CORBA_string_dup (const CORBA_Char *const);
-void			CORBA_string_free (CORBA_Char *const);
+CORBA_String CORBA_string_alloc (CORBA_ULong len);
+CORBA_String CORBA_string_copy (const CORBA_Char *const);
+CORBA_String CORBA_string_dup (const CORBA_Char *const);
+void CORBA_string_free (CORBA_Char *const);
 
 class CORBA_String_var
+  // = TITLE
+  // @@ Please add a comment here.
 {
 public:
+  // @@ Please add comments here.
   CORBA_String_var (void);
   CORBA_String_var (char *p);
   CORBA_String_var (const char *p);
@@ -115,24 +116,25 @@ public:
   operator const char* () const {return ptr_; };
   char &operator[] (CORBA_ULong index);
   char operator[] (CORBA_ULong index) const;
+
 private:
   char *ptr_;
 };
-//
+
 // 94-9-32 Appendix A defines 16-bit UNICODE characters as
 // "WChar", and null-terminated strings of them as "WString".
 //
 // NOTE:  those are IDL extensions, not yet standard.
-//
-#if	SIZEOF_WCHAR_T < 2
-typedef short		CORBA_WChar;
-#else
-typedef wchar_t		CORBA_WChar;
-#endif
-typedef CORBA_WChar	*CORBA_WString;
 
-CORBA_WString		CORBA_wstring_alloc (CORBA_ULong len);
-CORBA_WString		CORBA_wstring_copy (const CORBA_WChar *const);
-void			CORBA_wstring_free (CORBA_WChar *const);
+#if	SIZEOF_WCHAR_T < 2
+typedef short CORBA_WChar;
+#else
+typedef wchar_t CORBA_WChar;
+#endif /* SIZEOF _WCHAR_T < 2 */
+typedef CORBA_WChar *CORBA_WString;
+
+CORBA_WString CORBA_wstring_alloc (CORBA_ULong len);
+CORBA_WString CORBA_wstring_copy (const CORBA_WChar *const);
+void CORBA_wstring_free (CORBA_WChar *const);
 
 #endif /* TAO_CORBA_COM_H */
