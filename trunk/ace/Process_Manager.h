8b9825a7b23b2a52fd9d3e0aa899aa82c40bfde0
harrison <harrison@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-16 04:34:05 +0000

    Yippeee  spawn_n lives.

--- trunk/ace/Process_Manager.h
+++ trunk/ace/Process_Manager.h
@@ -63,13 +63,13 @@ public:
   int close (void);		
   // Release all resources.
 
-  pid_t start (ACE_Process_Options &options);
+  pid_t spawn (ACE_Process_Options &options);
   // Create a new process using ACE_Process::start (<options>).
 
   // Returns: on success a unique group id that can be used to control
   // other processs added to the same group.  On failure, returns -1.
 
-  int start_n (size_t n, ACE_Process_Options &options);
+  int spawn_n (size_t n, ACE_Process_Options &options);
   // Create N new processs.
   
   // Returns: on success a unique group id that can be used to control
