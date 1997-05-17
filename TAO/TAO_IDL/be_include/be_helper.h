a715a7aabd05c23ec7dfb62d213509e002e2e056
gokhale <gokhale@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-17 22:20:47 +0000

    Added header files and removed be_classes.h

---
+++ TAO/TAO_IDL/be_include/be_helper.h
@@ -0,0 +1,9 @@
+#if !defined (TAO_BE_HELPER_H)
+#define TAO_BE_HELPER_H
+
+const char* be_get_client_hdr_fname();
+const char* be_get_client_impl_fname();
+const char* be_get_server_hdr_fname();
+const char* be_get_server_impl_fname();
+
+#endif
