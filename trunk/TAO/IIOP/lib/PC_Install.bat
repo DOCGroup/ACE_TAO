e1a6c52beb7e8ef1083c5e45a436b6499ebe8358
bmendel <bmendel@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-20 19:38:05 +0000

    Changes required to compile examples.  Modified by brian Mendel

--- trunk/TAO/IIOP/lib/PC_Install.bat
+++ trunk/TAO/IIOP/lib/PC_Install.bat
@@ -1,31 +1,42 @@
 echo on
 mkdir ..\proto\lib
-copy TAO_Baseline.lib ..\proto\lib
+copy .\Build\TAO.lib ..\proto\lib
 rem copy %ACE_WRAPPERS%\ace.lib %TAO_ROOT%\proto\lib
-copy \OFP\Workspace\libfiles\ace.lib ..\proto\lib
+rem copy \OFP\Workspace\libfiles\ace.lib ..\proto\lib
+copy \bmendel\ACE_wrappers\ace\ace.lib ..\proto\lib
 
 mkdir ..\proto\include
-copy align.h    ..\proto\include
-copy thread.h   ..\proto\include
-copy cdr.h      ..\proto\include
-copy connmgr.h  ..\proto\include
-copy giop.h     ..\proto\include
-copy iiopobj.h  ..\proto\include
-copy iioporb.h  ..\proto\include
-copy roa.h      ..\proto\include
+copy align.h     ..\proto\include
+copy cdr.h       ..\proto\include
+copy cdr.i       ..\proto\include
+copy factories.h ..\proto\include
+copy factories.i ..\proto\include
+copy giop.h      ..\proto\include
+copy iiopobj.h   ..\proto\include
+copy iioporb.h   ..\proto\include
+copy roa.h       ..\proto\include
 copy roa.i       ..\proto\include
+copy svrrqst.i   ..\proto\include
 copy params.h    ..\proto\include
+copy params.i    ..\proto\include
 copy connect.h   ..\proto\include
-copy xdr.h      ..\proto\include
+copy connect.i   ..\proto\include
+copy xdr.h       ..\proto\include
 
 mkdir ..\proto\include\corba
 copy  any.h      ..\proto\include\corba
-copy  boa.h      ..\proto\include
+copy  any.i      ..\proto\include\corba
+copy  boa.h      ..\proto\include\corba
+copy  boa.i      ..\proto\include\corba
 copy  corbacom.h ..\proto\include\corba
 copy  except.h   ..\proto\include\corba
 copy  nvlist.h   ..\proto\include\corba
 copy  object.h   ..\proto\include\corba
+copy  object.i   ..\proto\include\corba
 copy  orbobj.h   ..\proto\include\corba
+copy  orbobj.i   ..\proto\include\corba
+copy  objtable.h ..\proto\include\corba
+copy  optable.h  ..\proto\include\corba
 copy  principa.h ..\proto\include\corba
 copy  request.h  ..\proto\include\corba
 copy  svrrqst.h  ..\proto\include\corba
