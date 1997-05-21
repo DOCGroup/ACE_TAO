85d51dd837225c6048b0cbb0944baa27dcdab4a4
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-05-21 17:03:15 +0000

    *** empty log message ***

--- netsvcs/lib/Log_Message_Receiver.cpp
+++ netsvcs/lib/Log_Message_Receiver.cpp
@@ -11,8 +11,8 @@ Static_Log_Message_Receiver<ACE_SYNCH_2>::log_record (char const *hostname,
 						      ACE_Log_Record &record)
 {
 #if defined (ACE_HAS_THREADS)
-    static ACE_SYNCH_MUTEX lock_;
-    ACE_Guard<ACE_SYNCH_MUTEX> guard (lock_);
+    static ACE_SYNCH_MUTEX_T lock_;
+    ACE_Guard<ACE_SYNCH_MUTEX_T> guard (lock_);
 #endif /* ACE_HAS_THREADS */
    
   record.print (hostname, 0, stderr);
@@ -21,6 +21,8 @@ Static_Log_Message_Receiver<ACE_SYNCH_2>::log_record (char const *hostname,
 #if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
 template<ACE_SYNCH_1> 
 ACE_SYNCH_MUTEX_T Log_Message_Receiver_Impl<ACE_SYNCH_2>::copy_lock_;
+#else
+static ACE_SYNCH_MUTEX global_copy_lock_;
 #endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
 
 template<ACE_SYNCH_1> inline 
@@ -41,12 +43,15 @@ Log_Message_Receiver_Impl<ACE_SYNCH_2> *
 Log_Message_Receiver_Impl<ACE_SYNCH_2>::attach (Log_Message_Receiver_Impl<ACE_SYNCH_2> *body)
 {
   assert(body != 0);
-  
-#if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
-  ACE_Guard<ACE_SYNCH_MUTEX> guard (copy_lock_);
-#else
-  ACE_Guard<ACE_SYNCH_MUTEX> guard (body->print_lock_);
-#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+
+#if defined (ACE_HAS_THREADS)  
+  #if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  Guard guard (copy_lock_);
+  #else
+  // Use the "body"s print lock as copy lock.
+  ACE_Guard<ACE_SYNCH_MUTEX> guard (global_copy_lock_);  
+  #endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+#endif
   ++body->count_;
   return body;
 }
@@ -56,21 +61,16 @@ Log_Message_Receiver_Impl<ACE_SYNCH_2>::detach(Log_Message_Receiver_Impl<ACE_SYN
 {
   assert (body != 0);
   
-#if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
-  ACE_Guard<ACE_SYNCH_MUTEX> guard (copy_lock_);
-  
+#if defined (ACE_HAS_THREADS)  
+  #if !defined (ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES)
+  Guard guard (copy_lock_);
+  #else
+  // Use the "body"s print lock as copy lock.
+  ACE_Guard<ACE_SYNCH_MUTEX> guard (global_copy_lock_);  
+  #endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
+#endif
   if (body->count_-- == 0)
     delete body;
-#else
-  bool result = false;
-  {
-    ACE_Guard<ACE_SYNCH_MUTEX> guard (body->print_lock_);
-    result = body->count_-- == 0;
-  }
-
-  if (result != false)
-    delete body;
-#endif /* ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES */
 }
 
 // Type based log message receiver
@@ -78,7 +78,7 @@ template<ACE_SYNCH_1> inline void
 Log_Message_Receiver_Impl<ACE_SYNCH_2>::log_record (char const *hostname,
 						    ACE_Log_Record &record)
 {
-  ACE_MT (ACE_Guard<ACE_SYNCH_MUTEX> guard (print_lock_));
+  ACE_MT (Guard guard (print_lock_));
   record.print (hostname, 0, stderr);
 }
 
