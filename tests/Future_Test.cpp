a009f1e658195f69d629d3ffd3762e5b6186ce8c
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-27 20:03:07 +0000

    added ACE_Future{_Rep}<int> template specializatoins

--- tests/Future_Test.cpp
+++ tests/Future_Test.cpp
@@ -297,10 +297,12 @@ static int n_loops = 100;
 
 #if defined (ACE_TEMPLATES_REQUIRE_SPECIALIZATION)
 template class ACE_Future<const char *>;
+template class ACE_Future<int>;
 template class ACE_Future<u_long>;
-template class auto_ptr<ACE_Method_Object>;
 template class ACE_Future_Rep<char const *>;
-template class ACE_Future_Rep<unsigned long>;
+template class ACE_Future_Rep<int>;
+template class ACE_Future_Rep<u_long>;
+template class auto_ptr<ACE_Method_Object>;
 #endif /* ACE_TEMPLATES_REQUIRE_SPECIALIZATION */
 
 #endif /* ACE_HAS_THREADS */
