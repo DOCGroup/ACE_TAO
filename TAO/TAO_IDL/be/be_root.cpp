d6d9bdd2fd80c8eeb76cf18b249f13fde799ac8a
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-28 13:15:55 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.2'.

--- TAO/TAO_IDL/be/be_root.cpp
+++
@@ -1,4 +0,0 @@
-#include	"idl.h"
-#include	"idl_extern.h"
-#include	"be.h"
-
--- TAO/TAO_IDL/be_include/be_root.h
+++
@@ -1,8 +0,0 @@
-#if !defined (TAO_BE_ROOT_H)
-#define TAO_BE_ROOT_H
-
-class be_root : public virtual AST_Root
-{
-};
-
-#endif
