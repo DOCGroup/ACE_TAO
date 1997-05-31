1aac74313e0bbb831c0f2fe2d888a29a11dfef59
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-05-31 04:15:27 +0000

    Added a signal handler which traps SIGINT and calls exit (0).

--- apps/JAWS/server/main.cpp
+++ apps/JAWS/server/main.cpp
@@ -5,10 +5,20 @@
 
 ACE_STATIC_SVC_REQUIRE(HTTP_Server)
 
+extern "C"
+void
+handler (int)
+{
+  exit (0);
+} 
+
 int
 main (int argc, char *argv[])
 {
   ACE_Service_Config daemon;
+  ACE_Sig_Action sa ((ACE_SignalHandler) handler, SIGINT);
+  ACE_OS::signal (SIGCLD, SIG_IGN);
+  ACE_UNUSED_ARG (sa);
 
   if (daemon.open (argc, argv) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "open"), 1);
