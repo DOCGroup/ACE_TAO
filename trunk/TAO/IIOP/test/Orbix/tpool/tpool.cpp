bf5fedeb21ba4eca8e303ff93b7d80ff0a5ccbf8
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-06-17 17:31:30 +0000

    Thread pool server

---
+++ trunk/TAO/IIOP/test/Orbix/tpool/tpool.cpp
@@ -0,0 +1,42 @@
+#include "tpool.h"
+
+Thread_Pool::Thread_Pool (int n_threads)
+  : ACE_Task<ACE_MT_SYNCH> (ACE_Service_Config::thr_mgr ())
+{
+  if (this->activate (THR_DETACHED | THR_NEW_LWP, n_threads) == -1)
+    ACE_ERROR ((LM_ERROR, "%p\n", "activate failed"));
+}    
+
+int
+Thread_Pool::svc (void)
+{
+  cout << "Pool thread activated" << endl;
+  while (1) 
+    {
+      // Dequeue message
+      ACE_Message_Block *mb;
+      this->msg_queue ()->dequeue_head (mb);
+      CORBA::Request *vp = (CORBA::Request *) mb->base ();
+      mb->release ();
+      // Hand it off to Orbix
+      CORBA::Orbix.continueThreadDispatch (*vp);
+    }
+  return -1;
+}
+
+
+int
+Thread_Pool::inRequestPreMarshal(CORBA::Request &r,  CORBA::Environment&)
+{
+//   thread_t	tid;
+//   thr_create(NULL, 0, worker, (void *)&r, THR_DETACHED, &tid);
+
+  // Enqueue the request
+  ACE_Message_Block *mb = new ACE_Message_Block ( (char *) (&r));
+  this->msg_queue ()->enqueue (mb);
+  // Done
+  return -1;
+}
+
+
+                             
