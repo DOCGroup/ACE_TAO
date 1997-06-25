2c0865a84214594cb179a3cb779fffda09314630
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-06-25 18:00:30 +0000

    *** empty log message ***

--- ace/Synch_T.h
+++ ace/Synch_T.h
@@ -639,17 +639,20 @@ class ACE_MT_SYNCH
 {
 public:
   typedef ACE_Thread_Mutex MUTEX;
+  typedef ACE_Process_Mutex PROCESS_MUTEX;
   typedef ACE_Recursive_Thread_Mutex RECURSIVE_MUTEX;
   typedef ACE_RW_Thread_Mutex RW_MUTEX;
   typedef ACE_Condition_Thread_Mutex CONDITION;
 };
 #define ACE_SYNCH_MUTEX ACE_MT_SYNCH::MUTEX
+#define ACE_SYNCH_PROCESS_MUTEX  ACE_MT_SYNCH::PROCESS_MUTEX;
 #define ACE_SYNCH_RECURSIVE_MUTEX ACE_MT_SYNCH::RECURSIVE_MUTEX
 #define ACE_SYNCH_RW_MUTEX ACE_MT_SYNCH::RW_MUTEX
 #define ACE_SYNCH_CONDITION ACE_MT_SYNCH::CONDITION
 #else /* Necessary to support broken cfront-based C++ compilers... */
 #define ACE_MT_SYNCH ACE_Thread_Mutex, ACE_Condition_Thread_Mutex
 #define ACE_SYNCH_MUTEX ACE_Thread_Mutex
+#define ACE_SYNCH_PROCESS_MUTEX  ACE_Process_Mutex;
 #define ACE_SYNCH_RECURSIVE_MUTEX ACE_Recursive_Thread_Mutex
 #define ACE_SYNCH_RW_MUTEX ACE_RW_Thread_Mutex
 #define ACE_SYNCH_CONDITION ACE_Thread_Condition
@@ -659,6 +662,7 @@ public:
 #else
 #define ACE_SYNCH ACE_NULL_SYNCH
 #define ACE_SYNCH_MUTEX ACE_Null_Mutex
+#define ACE_SYNCH_PROCESS_MUTEX ACE_Null_Mutex
 #define ACE_SYNCH_RECURSIVE_MUTEX ACE_Null_Mutex
 #define ACE_SYNCH_RW_MUTEX ACE_Null_Mutex
 #define ACE_SYNCH_CONDITION ACE_Null_Condition
