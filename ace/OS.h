ccdc4a0730db205cf7798c05a6638f393081fff9
Steve Huston <shuston@riverace.com>
1997-06-25 14:57:02 +0000

    Moved ACE_SHLIB_HANDLE and ACE_DEFAULT_SHLIB_MODE for Win32 to the place where the other platform types define similar types.

--- ace/OS.h
+++ ace/OS.h
@@ -1890,9 +1890,6 @@ struct iovec
 typedef DWORD ACE_id_t;
 typedef int ACE_pri_t;
 
-typedef HINSTANCE ACE_SHLIB_HANDLE;
-const int ACE_DEFAULT_SHLIB_MODE = 0;
-
 #else /* !defined (ACE_WIN32) */
 
 typedef const char *LPCTSTR;
@@ -2204,6 +2201,9 @@ const int ACE_DEFAULT_SHLIB_MODE = RTLD_LAZY;
 # endif /* HP aC++ vs. HP C++ */
 typedef shl_t ACE_SHLIB_HANDLE;
 const int ACE_DEFAULT_SHLIB_MODE = BIND_DEFERRED;
+#elif defined (ACE_WIN32)
+typedef HINSTANCE ACE_SHLIB_HANDLE;
+const int ACE_DEFAULT_SHLIB_MODE = 0;
 #endif /* ACE_HAS_SVR4_DYNAMIC_LINKING */
 
 #if defined (ACE_HAS_SOCKIO_H)
