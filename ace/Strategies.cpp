9889d49a7edfac0f9ef060f83ea283f727b3326d
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-06-25 23:45:22 +0000

    *** empty log message ***

--- ace/Strategies.cpp
+++ ace/Strategies.cpp
@@ -20,6 +20,30 @@ ACE_Notification_Strategy::~ACE_Notification_Strategy (void)
 {
 }
 
+ACE_Event_Handler *
+ACE_Notification_Strategy::event_handler (void)
+{
+  return eh_;
+}
+
+void 
+ACE_Notification_Strategy::event_handler (ACE_Event_Handler *eh)
+{
+  this->eh_ = eh;
+}
+
+ACE_Reactor_Mask 
+ACE_Notification_Strategy::mask (void)
+{
+  return mask_;
+}
+
+void 
+ACE_Notification_Strategy::mask (ACE_Reactor_Mask m)
+{
+  this->mask_ = m;
+}
+
 ACE_Reactor_Notification_Strategy::ACE_Reactor_Notification_Strategy (ACE_Reactor *reactor,
 								      ACE_Event_Handler *eh,
 								      ACE_Reactor_Mask mask)
@@ -41,25 +65,50 @@ ACE_Reactor_Notification_Strategy::notify (ACE_Event_Handler *eh,
   return this->reactor_->notify (eh, mask);
 }
 
-ACE_ReactorEx_Notification_Strategy::ACE_ReactorEx_Notification_Strategy (ACE_ReactorEx *reactorex,
+ACE_Reactor *
+ACE_Reactor_Notification_Strategy::reactor (void)
+{
+  return this->reactor_;
+}
+
+void 
+ACE_Reactor_Notification_Strategy::reactor (ACE_Reactor *r)
+{
+  this->reactor_ = r;
+}
+  
+ACE_ReactorEx_Notification_Strategy::ACE_ReactorEx_Notification_Strategy (ACE_ReactorEx *reactorEx,
 									  ACE_Event_Handler *eh,
 									  ACE_Reactor_Mask mask)
   : ACE_Notification_Strategy (eh, mask),
-    reactorex_ (reactorex)
+    reactorEx_ (reactorEx)
 {
 }
 
 int 
 ACE_ReactorEx_Notification_Strategy::notify (void)
 {
-  return this->reactorex_->notify (this->eh_, this->mask_);
+  return this->reactorEx_->notify (this->eh_, this->mask_);
 }
 
 int 
 ACE_ReactorEx_Notification_Strategy::notify (ACE_Event_Handler *eh,
 					     ACE_Reactor_Mask mask)
 {
-  return this->reactorex_->notify (eh, mask);
+  return this->reactorEx_->notify (eh, mask);
 }
 
+ACE_ReactorEx *
+ACE_ReactorEx_Notification_Strategy::reactorEx (void)
+{
+  return this->reactorEx_;
+}
+
+void 
+ACE_ReactorEx_Notification_Strategy::reactorEx (ACE_ReactorEx *r)
+{
+  this->reactorEx_ = r;
+}
+  
 #endif /* ACE_STRATEGIES_C */
+
