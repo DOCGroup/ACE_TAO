b0f42b7f68e680720653efb7305b370c17958eb2
gokhale <asgokhale@users.noreply.github.com>
1997-07-02 04:06:11 +0000

    Orbix Cubit thread per request

---
+++ TAO/IIOP/tests/Cubit/Orbix/tpr/tpr.cpp
@@ -0,0 +1,26 @@
+#include "tpr.h"
+
+void *
+worker(void *vp)
+{
+  ACE_Thread_Control tc (ACE_Service_Config::thr_mgr ());
+  CORBA::Orbix.continueThreadDispatch (*(CORBA::Request *)vp);
+  return NULL;
+}
+
+
+int
+tpr_filter::inRequestPreMarshal(CORBA::Request &r,  CORBA::Environment&)
+{
+//   thread_t	tid;
+//   thr_create(NULL, 0, worker, (void *)&r, THR_DETACHED, &tid);
+
+  ACE_Thread_Manager *thr_mgr = ACE_Service_Config::thr_mgr ();
+  thr_mgr->spawn (ACE_THR_FUNC (worker),
+                  (void *) &r, 
+                  THR_DETACHED);
+
+  return -1;
+}
+
+tpr_filter tpr_dispatcher;
