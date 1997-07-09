5db2130b5d4fcb795b0b8aa76e6be673f94817e8
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-07-09 01:56:27 +0000

    It is now as silent as a the earth after aliens come and kill us all.

--- TAO/tao/PC_Install.bat
+++ TAO/tao/PC_Install.bat
@@ -1,3 +1,4 @@
+@echo off
 rem mkdir ..\proto\lib
 rem copy .\Build\TAO.lib ..\proto\lib
 
@@ -6,47 +7,47 @@ rem copy \OFP\Workspace\libfiles\ace.lib ..\proto\lib
 
 rem copy \bmendel\ACE_wrappers\ace\ace.lib ..\proto\lib
 
-mkdir ..\proto\include
-copy align.h     ..\proto\include
-copy cdr.h       ..\proto\include
-copy cdr.i       ..\proto\include
-copy factories.h ..\proto\include
-copy factories.i ..\proto\include
-copy giop.h      ..\proto\include
-copy iiopobj.h   ..\proto\include
-copy iioporb.h   ..\proto\include
-copy roa.h       ..\proto\include
-copy roa.i       ..\proto\include
-copy svrrqst.i   ..\proto\include
-copy params.h    ..\proto\include
-copy params.i    ..\proto\include
-copy connect.h   ..\proto\include
-copy connect.i   ..\proto\include
-copy xdr.h       ..\proto\include
-copy marshal.h   ..\proto\include
-copy typecode.i  ..\proto\include
-copy marshal.i   ..\proto\include
+if not exist ..\proto\include mkdir ..\proto\include
+copy align.h     ..\proto\include >NUL
+copy cdr.h       ..\proto\include >NUL
+copy cdr.i       ..\proto\include >NUL
+copy factories.h ..\proto\include >NUL
+copy factories.i ..\proto\include >NUL
+copy giop.h      ..\proto\include >NUL
+copy iiopobj.h   ..\proto\include >NUL
+copy iioporb.h   ..\proto\include >NUL
+copy roa.h       ..\proto\include >NUL
+copy roa.i       ..\proto\include >NUL
+copy svrrqst.i   ..\proto\include >NUL
+copy params.h    ..\proto\include >NUL
+copy params.i    ..\proto\include >NUL
+copy connect.h   ..\proto\include >NUL
+copy connect.i   ..\proto\include >NUL
+copy xdr.h       ..\proto\include >NUL
+copy marshal.h   ..\proto\include >NUL
+copy typecode.i  ..\proto\include >NUL
+copy marshal.i   ..\proto\include >NUL
 
-mkdir ..\proto\include\corba
-copy  any.h      ..\proto\include\corba
-copy  any.i      ..\proto\include\corba
-copy  boa.h      ..\proto\include\corba
-copy  boa.i      ..\proto\include\corba
-copy  corbacom.h ..\proto\include\corba
-copy  except.h   ..\proto\include\corba
-copy  nvlist.h   ..\proto\include\corba
-copy  object.h   ..\proto\include\corba
-copy  object.i   ..\proto\include\corba
-copy  orbobj.h   ..\proto\include\corba
-copy  orbobj.i   ..\proto\include\corba
-copy  objtable.h ..\proto\include\corba
-copy  optable.h  ..\proto\include\corba
-copy  principa.h ..\proto\include\corba
-copy  request.h  ..\proto\include\corba
-copy  svrrqst.h  ..\proto\include\corba
-copy  typecode.h ..\proto\include\corba
-copy  orb.h      ..\proto\include\corba
-copy  sequence.h ..\proto\include\corba
-copy  stub.h     ..\proto\include\corba
-copy  orbconf.h  ..\proto\include\corba
-copy  debug.h    ..\proto\include\corba
+if not exist ..\proto\include\corba mkdir ..\proto\include\corba
+copy  any.h      ..\proto\include\corba >NUL
+copy  any.i      ..\proto\include\corba >NUL
+copy  boa.h      ..\proto\include\corba >NUL
+copy  boa.i      ..\proto\include\corba >NUL
+copy  corbacom.h ..\proto\include\corba >NUL
+copy  except.h   ..\proto\include\corba >NUL
+copy  nvlist.h   ..\proto\include\corba >NUL
+copy  object.h   ..\proto\include\corba >NUL
+copy  object.i   ..\proto\include\corba >NUL
+copy  orbobj.h   ..\proto\include\corba >NUL
+copy  orbobj.i   ..\proto\include\corba >NUL
+copy  objtable.h ..\proto\include\corba >NUL
+copy  optable.h  ..\proto\include\corba >NUL
+copy  principa.h ..\proto\include\corba >NUL
+copy  request.h  ..\proto\include\corba >NUL
+copy  svrrqst.h  ..\proto\include\corba >NUL
+copy  typecode.h ..\proto\include\corba >NUL
+copy  orb.h      ..\proto\include\corba >NUL
+copy  sequence.h ..\proto\include\corba >NUL
+copy  stub.h     ..\proto\include\corba >NUL
+copy  orbconf.h  ..\proto\include\corba >NUL
+copy  debug.h    ..\proto\include\corba >NUL
