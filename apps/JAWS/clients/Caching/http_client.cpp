e67dbedfe787e0bcb409990d4d586c2969963a75
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-07-09 18:09:54 +0000

    *** empty log message ***

---
+++ apps/JAWS/clients/Caching/http_client.cpp
@@ -0,0 +1,66 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    apps/JAWS/clients/Caching
+// 
+// = FILENAME
+//    http_client.cpp
+//
+// = AUTHOR
+//    James Hu
+// 
+// ============================================================================
+
+// = DESCRIPTION
+//    This is a very simple client.  It accepts URLs from a prompt, and
+//    will try to fetch them.  Also accepts shell escapes.
+
+#include "http_handler.h"
+
+int
+main (int argc, char *argv[])
+{
+  argc = argc;
+  argv = argv;
+
+  // Present a command line.
+  // * Accept a URL.
+  //     Pass it to the HTTP_Connector.
+  //     Connect.
+  //     Report status.
+  // * Accept shell escape character.
+
+  char buf[BUFSIZ];
+
+  ACE_OS::fprintf (stderr, "* ");
+  while (ACE_OS::fgets (buf, sizeof (buf), stdin) != NULL)
+    {
+      char * s = buf;
+      while (*s == ' ' || *s == '\t')
+        s++;
+
+      if (*s == '!')
+        {
+          // Shell command
+          do s++; while (*s == ' ' || *s == '\t');
+          if (ACE_OS::system (s) == -1)
+            ACE_OS::fprintf (stderr, " ! Error executing: %s\n", s);
+        }
+      else if (ACE_OS::strncmp (s, "http://", 7) == 0)
+        {
+          // URL
+          HTTP_Connector connector;
+          connector.connect (s);
+        }
+      else
+        ACE_OS::fprintf (stderr, " ? I don't understand: %s\n", s);
+
+      ACE_OS::fprintf (stderr, "* ");
+    }
+
+  ACE_DEBUG ((LM_DEBUG, "\nBye!\n"));
+
+  return 0;
+}
