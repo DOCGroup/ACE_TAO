b307b85f165fe6a15428c32cb013bf8ed61d45b0
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-07-12 22:44:13 +0000

    Updated based on Mike MacFaden <mrm@cisco.com>'s suggestion

--- java/tests/SOCK_SAP/SOCKConnectorTest.java
+++ java/tests/SOCK_SAP/SOCKConnectorTest.java
@@ -19,7 +19,7 @@ import JACE.SOCK_SAP.*;
 
 public class SOCKConnectorTest
 {
-  void print_usage_and_die ()
+  static void print_usage_and_die ()
     {
       System.out.println ("Usage: SOCKConnectorTest <hostname> [<port>]");
       System.exit (0);
@@ -27,26 +27,27 @@ public class SOCKConnectorTest
 
   void processRequests (SOCKStream stream) throws IOException
     {
-      DataInputStream in = new DataInputStream (System.in);
+      // 1.0 JDK      DataInputStream in = new DataInputStream (System.in);
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
       String msg;
       int ack_len;
 
       while (true)
-	{
-	  StringBuffer ack = new StringBuffer ();
-	  ACE.DEBUG ("Enter input: ");
-	  ACE.FLUSH ();
-	  msg = in.readLine ();
-	  if (msg == null)
-	    break;
-	  stream.send (msg);
-	  ACE.DEBUG ("Waiting for ack...");
-	  ack_len = stream.recv (ack);
-	  if (ack_len == 0)
-	    break;
-	  else
-	    ACE.DEBUG (ack.toString ());
-	}
+ {
+   StringBuffer ack = new StringBuffer ();
+   ACE.DEBUG ("Enter input: ");
+   ACE.FLUSH ();
+   msg = in.readLine ();
+   if (msg == null)
+     break;
+   stream.send (msg);
+   ACE.DEBUG ("Waiting for ack...");
+   ack_len = stream.recv (ack);
+   if (ack_len == 0)
+     break;
+   else
+     ACE.DEBUG (ack.toString ());
+ }
     }
 
   public void init (String host, int port)
@@ -54,33 +55,38 @@ public class SOCKConnectorTest
       SOCKStream stream = new SOCKStream ();
       SOCKConnector connector = new SOCKConnector ();
       try
-	{
-	  connector.connect (stream,
-			     host,
-			     port);
-	  processRequests (stream);
-	}
+ {
+   connector.connect (stream,
+        host,
+        port);
+   processRequests (stream);
+ }
       catch (IOException e)
-	{
-	  ACE.ERROR (e);
-	}
+ {
+   ACE.ERROR (e);
+ }
     }
 
   public static void main (String [] args)
     {
       int port = ACE.DEFAULT_SERVER_PORT;
       SOCKConnectorTest client = new SOCKConnectorTest ();
+
+      // check arg count 
+      if (args.length < 2 || args.length > 2) 
+         print_usage_and_die(); 
+
       if (args.length == 2)
-	{
-	  try
-	    {
-	      port = Integer.parseInt (args[1]);
-	    }
-	  catch (NumberFormatException e)
-	    {
-	      client.print_usage_and_die ();
-	    }
-	}
-      client.init (args[0], port);
+ {
+   try
+     {
+       port = Integer.parseInt (args[1]);
+     }
+   catch (NumberFormatException e)
+     {
+       client.print_usage_and_die ();
+     }
+          client.init (args[0], port);
+ }
     }
 }
