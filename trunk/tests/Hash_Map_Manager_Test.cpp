4bcfd3d5dbeaf87f88ecb475620d7f36799b2258
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-07-15 19:34:30 +0000

    Changes to support systems without template specialization.

--- trunk/tests/Hash_Map_Manager_Test.cpp
+++ trunk/tests/Hash_Map_Manager_Test.cpp
@@ -21,13 +21,17 @@
 #include <iostream.h>
 
 #include "ace/Hash_Map_Manager.h"
+#include "ace/SString.h"
 #include "ace/Synch.h"
 #include "test_config.h"
+
+#if defined (ACE_HAS_TEMPLATE_SPECIALIZATION)
 
 #define HASH_STRING_ENTRY ACE_Hash_Map_Entry<char *, char *>
 #define HASH_STRING_MAP ACE_Hash_Map_Manager<char *, char *, ACE_Null_Mutex>
 
-#if defined (ACE_HAS_TEMPLATE_SPECIALIZATION)
+#define HASH_MAP_STRING_TYPE char *
+#define ENTRY entry
 
 HASH_STRING_ENTRY::ACE_Hash_Map_Entry (char *const &ext_id,
                                        char *const &int_id,
@@ -61,10 +65,21 @@ HASH_STRING_MAP::equal (char *const &id1, char *const &id2)
   return ACE_OS::strcmp (id1, id2) == 0;
 }
 
-#endif /* ACE_HAS_TEMPLATE_SPECIALIZATION */
+#else
 
-// If code below breaks, it's probably because template specialization
-// is not supported.
+// Do this if we don't have template specialization
+
+#define HASH_STRING_ENTRY ACE_Hash_Map_Entry<ACE_CString, ACE_CString>
+#define HASH_STRING_MAP \
+        ACE_Hash_Map_Manager<ACE_CString, ACE_CString, ACE_Null_Mutex>
+
+#define HASH_MAP_STRING_TYPE ACE_CString
+#define ENTRY (entry.rep ())
+
+// Note that in this version, you will not get the Creating and Freeing
+// diagnostic messages.
+
+#endif /* ACE_HAS_TEMPLATE_SPECIALIZATION */
 
 static const int MAX_HASH = 256;
 
@@ -76,28 +91,28 @@ main(void)
   // Scoping below so that result of destruction can be seen in the log.
   {
     HASH_STRING_MAP hash (MAX_HASH);
-
+     
     hash.bind ("hello", "guten Tag");
     hash.bind ("goodbye", "auf wiedersehen");
     hash.bind ("funny", "lustig");
 
-    char * entry;
+    HASH_MAP_STRING_TYPE entry;
 
     if (hash.find ("hello", entry) == 0)
-      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "hello", entry));
+      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "hello", ENTRY));
     if (hash.find ("goodbye", entry) == 0)
-      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "goodbye", entry));
+      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "goodbye", ENTRY));
     if (hash.find ("funny", entry) == 0)
-      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "funny", entry));
+      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "funny", ENTRY));
 
     hash.unbind ("goodbye", entry);
 
     if (hash.find ("hello", entry) == 0)
-      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "hello", entry));
+      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "hello", ENTRY));
     if (hash.find ("goodbye", entry) == 0)
-      ACE_DEBUG ((LM_DEBUG, "OOPS!  `%s' found `%s'\n", "goodbye", entry));
+      ACE_DEBUG ((LM_DEBUG, "OOPS!  `%s' found `%s'\n", "goodbye", ENTRY));
     if (hash.find ("funny", entry) == 0)
-      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "funny", entry));
+      ACE_DEBUG ((LM_DEBUG, "`%s' found `%s'\n", "funny", ENTRY));
   }
 
   ACE_END_TEST;
@@ -106,7 +121,7 @@ main(void)
 }
 
 #if defined (ACE_TEMPLATES_REQUIRE_SPECIALIZATION)
-template class ACE_Hash_Map_Entry<char *, char *>;
-template class ACE_Hash_Map_Manager<char *, char *, ACE_Null_Mutex>;
-template class ACE_Hash_Map_Iterator<char *, char *, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Entry<HASH_MAP_STRING_TYPE, HASH_MAP_STRING_TYPE>;
+template class ACE_Hash_Map_Manager<HASH_MAP_STRING_TYPE, HASH_MAP_STRING_TYPE, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Iterator<HASH_MAP_STRING_TYPE, HASH_MAP_STRING_TYPE, ACE_Null_Mutex>;
 #endif /* ACE_TEMPLATES_REQUIRE_SPECIALIZATION */
