91918df235addfad9de1c755cb16c37d734b1b2e
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-07-24 16:51:03 +0000

    Forgot to credit all the people

--- trunk/ace/Timer_Heap_T.cpp
+++ trunk/ace/Timer_Heap_T.cpp
@@ -448,7 +448,6 @@ ACE_Timer_Heap_T<TYPE, FUNCTOR, LOCK>::reschedule (ACE_Timer_Node_T<TYPE> *expir
 {
   ACE_TRACE ("ACE_Timer_Heap::reschedule");
 
-  // From James Crawford <jamesc@in.ot.com.au>
   // If we are rescheduling then we have freed our timer id so we need
   // to reacquire it.
   // NOTE: we rely on the fact that we will get the same timer id we just
