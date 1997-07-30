03edec9aea3d74e1ff1b8f9350cfe1efafdf0a9c
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-07-30 20:43:50 +0000

    *** empty log message ***

--- ace/OS.h
+++ ace/OS.h
@@ -953,6 +953,12 @@ typedef struct iovec ACE_WRITEV_TYPE;
 typedef const struct iovec ACE_WRITEV_TYPE;
 #endif /* ACE_HAS_BROKEN_WRITEV */
  
+#if defined (ACE_HAS_BROKEN_READV)
+typedef const struct iovec ACE_READV_TYPE;
+#else
+typedef struct iovec ACE_READV_TYPE;
+#endif /* ACE_HAS_BROKEN_WRITEV */
+
 #if defined (ACE_HAS_BROKEN_SETRLIMIT)
 typedef struct rlimit ACE_SETRLIMIT_TYPE;
 #else
