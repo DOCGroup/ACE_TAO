054bf02960a4c8bf3f24df21b8ecc4a5e00afaf6
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-07-31 21:52:47 +0000

    added ACE_HAS_TEMPLATE_INSTANTATION_PRAGMA

--- trunk/ace/config-vxworks-ghs-1.8.h
+++ trunk/ace/config-vxworks-ghs-1.8.h
@@ -9,26 +9,13 @@
 
 // Processor type, if necessary.  GreenHills defines "ppc".
 #if defined (ppc)
-#define ACE_HAS_POWERPC
+# define ACE_HAS_POWERPC
 #endif /* ppc */
 
 #if ! defined (__ACE_INLINE__)
-#define __ACE_INLINE__
+# define __ACE_INLINE__
 #endif /* ! __ACE_INLINE__ */
 
-#define ACE_LACKS_ACCESS
-#define ACE_LACKS_GETSERVBYNAME
-#define ACE_LACKS_COND_T
-#define ACE_LACKS_EXEC
-#define ACE_LACKS_KEY_T
-#define ACE_LACKS_FILELOCKS
-#define ACE_LACKS_MEMORY_H
-#define ACE_LACKS_PWD_FUNCTIONS
-#define ACE_LACKS_RWLOCK_T
-#define ACE_LACKS_SIGNED_CHAR
-#define ACE_LACKS_STRCASECMP
-#define ACE_LACKS_SYSV_MSG_H
-#define ACE_LACKS_SYS_NERR
 #define ACE_HAS_BROKEN_SENDMSG
 #define ACE_HAS_BROKEN_WRITEV
 #define ACE_HAS_CHARPTR_SOCKOPT
@@ -40,39 +27,53 @@
 #define ACE_HAS_POSIX_NONBLOCK
 #define ACE_HAS_POSIX_SEM
 #define ACE_HAS_POSIX_TIME
-#define ACE_HAS_SIG_ATOMIC_T
 #define ACE_HAS_SIGINFO_T
 #define ACE_HAS_SIGWAIT
+#define ACE_HAS_SIG_ATOMIC_T
 #define ACE_HAS_STRERROR
+#define ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA
 #define ACE_HAS_THREADS
+#define ACE_LACKS_ACCESS
+#define ACE_LACKS_COND_T
+#define ACE_LACKS_EXEC
+#define ACE_LACKS_FILELOCKS
+#define ACE_LACKS_GETSERVBYNAME
+#define ACE_LACKS_KEY_T
 #define ACE_LACKS_MADVISE
 #define ACE_LACKS_MALLOC_H
+#define ACE_LACKS_MEMORY_H
 #define ACE_LACKS_MKTEMP
 #define ACE_LACKS_MMAP
 #define ACE_LACKS_MPROTECT
 #define ACE_LACKS_MSYNC
 #define ACE_LACKS_PARAM_H
+#define ACE_LACKS_PWD_FUNCTIONS
 #define ACE_LACKS_RLIMIT
+#define ACE_LACKS_RWLOCK_T
 #define ACE_LACKS_SBRK
 #define ACE_LACKS_SEMBUF_T
 #define ACE_LACKS_SIGINFO_H
+#define ACE_LACKS_SIGNED_CHAR
 #define ACE_LACKS_SI_ADDR
 #define ACE_LACKS_SOCKETPAIR
+#define ACE_LACKS_STRCASECMP
 #define ACE_LACKS_STRRECVFD
 #define ACE_LACKS_SYSCALL
+#define ACE_LACKS_SYSV_MSG_H
 #define ACE_LACKS_SYSV_SHMEM
+#define ACE_LACKS_SYS_NERR
 #define ACE_LACKS_UCONTEXT_H
 #define ACE_LACKS_UTSNAME_T
-#if !defined (ACE_MT_SAFE)
-	#define ACE_MT_SAFE 1
-#endif
 #define ACE_NEEDS_SYSTIME_H
+#if !defined (ACE_MT_SAFE)
+# define ACE_MT_SAFE 1
+#endif
 
 // Defines the page size of the system.
 #define ACE_PAGE_SIZE 4096
 
 #if !defined (ACE_NTRACE)
-#define ACE_NTRACE 1
+# define ACE_NTRACE 1
 #endif /* ACE_NTRACE */
 
 #endif /* ACE_CONFIG_H */
