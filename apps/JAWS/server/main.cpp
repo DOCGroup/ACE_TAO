03b71edad089c6fa7f67c673606dcd6300e0f18b
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-08-06 21:45:05 +0000

    Added another signal handler so I can kill JAWS when purifying.

--- apps/JAWS/server/main.cpp
+++ apps/JAWS/server/main.cpp
@@ -2,6 +2,8 @@
 
 #include "ace/Service_Config.h"
 #include "ace/Reactor.h"
+#include "ace/Filecache_New2.h"
+
 #include "HTTP_Server.h"
 
 ACE_STATIC_SVC_REQUIRE(HTTP_Server)
@@ -12,8 +14,9 @@ extern "C"
   // call exit() so that static destructors get called 
 static void
 handler (int) 
-{ 
-  ACE_OS::exit (0); 
+{
+  delete (ACE_Filecache *) ACE_Filecache::instance ();
+  ACE_OS::exit (0);
 }
 
 }
@@ -25,9 +28,12 @@ int
 main (int argc, char *argv[])
 {
   ACE_Service_Config daemon;
-  ACE_Sig_Action sa ((ACE_SignalHandler) handler, SIGINT);
+  ACE_Reactor::instance ();
+  //  ACE_Sig_Action sa ((ACE_SignalHandler) handler, SIGINT);
   ACE_OS::signal (SIGCHLD, SIG_IGN);
-  ACE_UNUSED_ARG (sa);
+  ACE_OS::signal (SIGINT, (ACE_SignalHandler) handler);
+  ACE_OS::signal (SIGUSR1, (ACE_SignalHandler) handler);
+  //  ACE_UNUSED_ARG (sa);
 
   if (daemon.open (argc, argv) != 0)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "open"), 1);
