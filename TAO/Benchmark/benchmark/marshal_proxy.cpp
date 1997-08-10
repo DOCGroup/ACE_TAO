77c27dd455853fa54768f27ab2f9cb59d24c7dce
gokhale <asgokhale@users.noreply.github.com>
1997-08-10 14:24:59 +0000

    A new suite of benchmark code is added. This is the library for common features.

---
+++ TAO/Benchmark/benchmark/marshal_proxy.cpp
@@ -0,0 +1,19 @@
+// ============================================================================
+//
+// = LIBRARY
+//    CORBA Benchmark
+// 
+// = FILENAME
+//    marshal_proxy.cpp
+//
+// = AUTHOR
+//    Aniruddha Gokhale
+// 
+// ============================================================================
+
+#include "benchmark/marshal_proxy.h"
+
+#if !defined (__ACE_INLINE__)
+#include "benchmark/marshal_proxy.i"
+#endif /* __ACE_INLINE__ */
+
---
+++ TAO/Benchmark/benchmark/marshal_results.h
@@ -0,0 +1,70 @@
+// ============================================================================
+//
+// = LIBRARY
+//    CORBA Benchmark
+// 
+// = FILENAME
+//    marshal_results.h
+//
+// = AUTHOR
+//    Aniruddha Gokhale
+// 
+// ============================================================================
+
+#if !defined (_CORBA_MARSHAL_RESULTS_H_)
+#define _CORBA_MARSHAL_RESULTS_H_
+
+#include "ace/Profile_Timer.h"
+
+class CORBA_Marshal_Results
+{
+public:
+  CORBA_Marshal_Results (void);
+  // Construction.
+
+  ~CORBA_Marshal_Results (void);
+  // destructor.
+
+  void clear (void);
+  // Clear all results.
+
+  ACE_Profile_Timer::ACE_Elapsed_Time & elapsed_time (void);
+  // Get the elapsed time.
+
+  ACE_Profile_Timer::Rusage elapsed_rusage (void);
+  // compute the rusage
+
+  int start_timer (void);
+  // starts a profile timer.
+
+  int stop_timer (void);
+  // stops the profile timer.
+
+  void filename (const char *fname);
+  // set the name of the file to store results
+
+  int log_results (void);
+  // log results
+
+  int print_results (void);
+  // prints the results
+
+private:
+
+  // = Used for performance statistics
+  ACE_Profile_Timer timer_;
+  ACE_Profile_Timer::ACE_Elapsed_Time et_;
+  ACE_Profile_Timer::Rusage rusage_;
+
+  double elapsed_time_;
+  // Real elapsed time in seconds.
+
+  const char *filename_;
+  // filename
+};
+
+#if defined (__ACE_INLINE__)
+#include "benchmark/marshal_results.i"
+#endif /* __ACE_INLINE__ */
+
+#endif
