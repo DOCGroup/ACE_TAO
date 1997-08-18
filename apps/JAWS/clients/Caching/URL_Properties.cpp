10a23071412620e7dcfa74553857a1a1af561fd2
nw1 <nw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-08-18 12:24:43 +0000

    *** empty log message ***

--- apps/JAWS/clients/Caching/URL_Properties.cpp
+++ apps/JAWS/clients/Caching/URL_Properties.cpp
@@ -1,15 +1,17 @@
 // URL_Locator.cpp
 // $Id$
 
-#if !defined (ACE_URL_LOCATOR_C)
-#define ACE_URL_LOCATOR_C
+#if !defined (ACE_URL_PROPERTIES_C)
+#define ACE_URL_PROPERTIES_C
 
 #define ACE_BUILD_DLL
-#include "URL_Locator.h"
+#include "URL_Properties.h"
 
 #if !defined (__ACE_INLINE__)
-#include "URL_Locator.i"
+#include "URL_Properties.i"
 #endif /* __ACE_INLINE__ */
+
+#include "ace/OS.h"
 
 template <class T>
 size_t ace_array_bsize (T &x)
@@ -28,16 +30,24 @@ ACE_URL_Property::encode (void *buf) const
   ACE_OS::memcpy ((void *) ((char *) buf + len),
 		  (void *) this->value_->fast_rep(),
 		  (this->value_->length () + 1) * sizeof (ACE_USHORT16));
+  ACE_USHORT16 *m_buf = (ACE_USHORT16 *) buf;
+  for (size_t i = 0; i < this->name_->length () + this->value_->length () + 2 ;
+       i++)
+    m_buf[i] = htons (m_buf[i]);
 }
 
 ACE_URL_Property *
 ACE_URL_Property::decode (void *buf)
 {
   ACE_USHORT16 *n = (ACE_USHORT16 *) buf;
-  ACE_USHORT16 *val = n;
   ACE_URL_Property *retv;
+  int i;
 
-  for (int i = 0; n[i] != 0; i++) ;
+  for (i = 0; n[i] != 0; i++)
+    n[i] = ntohs (n[i]);
+  ACE_USHORT16 *val = &n[i+1];
+  for (i = 0; val[i] != 0; i++)
+    val[i] = ntohs (val[i]);
   ACE_NEW_RETURN (retv, ACE_URL_Property (n, val), 0);
   return retv;
 }
@@ -50,4 +60,35 @@ ACE_URL_Offer::bsize (void) const
   return sum;
 }
 
-#endif /* ACE_URL_LOCATOR_C */
+void
+ACE_URL_Offer::encode (void *buf) const
+{
+  size_t len = ACE_OS::strlen (this->url_) + 1;
+  ACE_OS::memcpy (buf, this->url_, len);
+
+  ACE_UINT32 *s_buf = (ACE_UINT32 *) ((char *) buf + len);
+  *s_buf = htonl (this->prop_.size ());
+  len += sizeof (ACE_UINT32);
+
+  for (int i = 0; i < this->prop_.size (); i++)
+    {
+      this->prop_[i].encode ((void *) ((char *) buf + len));
+      len += this->prop_[i].bsize ();
+    }
+}
+
+ACE_URL_Offer *
+ACE_URL_Offer::decode (void *buf)
+{
+  ACE_URL_Offer *retv;
+
+  ACE_NEW_RETURN (retv, ACE_URL_Offer);
+
+  //
+  // Unfinished.
+  //
+
+  return 0;
+}
+
+#endif /* ACE_URL_PROPERTIES_C */
