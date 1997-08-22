ac0277c7eff21904db287b6efa6ae2c007c0a684
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-08-22 01:59:24 +0000

    *** empty log message ***

--- trunk/ace/Auto_Ptr.cpp
+++ trunk/ace/Auto_Ptr.cpp
@@ -11,9 +11,6 @@
 #include "ace/Auto_Ptr.i"
 #endif /* __ACE_INLINE__ */
 
-#if !defined (ACE_HAS_STANDARD_CPP_LIBRARY) || \
-	    (ACE_HAS_STANDARD_CPP_LIBRARY == 0)
-
 ACE_ALLOC_HOOK_DEFINE(ACE_Auto_Basic_Ptr)
 
 template<class X> void
@@ -22,34 +19,12 @@ ACE_Auto_Basic_Ptr<X>::dump (void) const
   ACE_TRACE ("ACE_Auto_Basic_Ptr<X>::dump");
 }
 
-template<class X> void
-ACE_Auto_Basic_Ptr<X>::remove (X *& x)
-{
-  ACE_TRACE ("ACE_Auto_Basic_Ptr<X>::remove");
-
-  X *tp = x;
-  x = 0;
-  delete tp;
-}
-
-#endif /* ACE_HAS_STANDARD_CPP_LIBRARY */
-
 ACE_ALLOC_HOOK_DEFINE(ACE_Auto_Basic_Array_Ptr)
 
 template<class X> void
 ACE_Auto_Basic_Array_Ptr<X>::dump (void) const
 {
   ACE_TRACE ("ACE_Auto_Basic_Array_Ptr<X>::dump");
-}
-
-template<class X> void
-ACE_Auto_Basic_Array_Ptr<X>::remove (X *& x)
-{
-  ACE_TRACE ("ACE_Auto_Basic_Array_Ptr<X>::remove");
-
-  X *tp = x;
-  x = 0;
-  delete [] tp;
 }
 
 #endif /* ACE_AUTO_PTR_C */
