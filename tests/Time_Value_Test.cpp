80806031af84e55279a121061640538899ac34b4
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-08-26 16:06:31 +0000

    added tests of ACE_U_LongLong

--- tests/Time_Value_Test.cpp
+++ tests/Time_Value_Test.cpp
@@ -4,26 +4,82 @@
 //
 // = LIBRARY
 //    tests
-// 
+//
 // = FILENAME
 //    Time_Value_Test.cpp
 //
 // = DESCRIPTION
-//      This is a simple test of ACE_Time_Value. No command line
-//      arguments are needed to run the test. 
+//      This is a simple test of ACE_Time_Value.  No command line arguments
+//      are needed to run the test.  It also tests the ACE_U_LongLong class.
+//      Because ACE_U_LongLong is used for ACE_hrtime_t on some platforms,
+//      this seems like a reasonable place to test it.
 //
 // = AUTHOR
-//    Prashant Jain
-// 
+//    Prashant Jain and David Levine
+//
 // ============================================================================
 
-#include "ace/ACE.h"
+#include "ace/config.h"
 
+// Force test of ACE_U_LongLong class.
+#undef ACE_HAS_HI_RES_TIMER
+#undef ACE_HAS_LONGLONG_T
+
+#include "ace/ACE.h"
 #include "test_config.h"
+
+u_long
+test_ace_u_longlong ()
+{
+  u_long errors = 0;
+
+  ACE_U_LongLong ull1 (0x21,1);
+  if (! (ull1.hi () == 1  &&  ull1.lo () == 0x21)) ++errors;
+  ACE_ASSERT (ull1.hi () == 1  &&  ull1.lo () == 0x21);
+
+  ACE_U_LongLong ull2 (0x20,2);
+  if (! (ull2.hi () == 2  &&  ull2.lo () == 0x20)) ++errors;
+  ACE_ASSERT (ull2.hi () == 2  &&  ull2.lo () == 0x20);
+
+  ull2 -= ull1;
+  if (! (ull2.hi () == 0  &&  ull2.lo () == 0xfffffffful)) ++errors;
+  ACE_ASSERT (ull2.hi () == 0  &&  ull2.lo () == 0xfffffffful);
+
+  ull2 += ull1;
+  if (! (ull2.hi () == 2  &&  ull2.lo () == 0x20)) ++errors;
+  ACE_ASSERT (ull2.hi () == 2  &&  ull2.lo () == 0x20);
+
+  ACE_U_LongLong ull3 = ull1 + ull1;
+  if (! (ull3.hi () == 2  &&  ull3.lo () == 0x42)) ++errors;
+  ACE_ASSERT (ull3.hi () == 2  &&  ull3.lo () == 0x42);
+
+  ACE_U_LongLong ull4 = ACE_U_LongLong (0x1111, 0) -
+                        ACE_U_LongLong (0x1112, 0);
+  if (! (ull4.hi () == 0xfffffffful  &&  ull4.lo () == 0xfffffffful)) ++errors;
+  ACE_ASSERT (ull4.hi () == 0xfffffffful  &&  ull4.lo () == 0xfffffffful);
+
+  ACE_U_LongLong ull5 = ACE_U_LongLong (0x1111, 1) -
+                        ACE_U_LongLong (0x1112, 0);
+  if (! (ull5.hi () == 0  &&  ull5.lo () == 0xfffffffful)) ++errors;
+  ACE_ASSERT (ull5.hi () == 0  &&  ull5.lo () == 0xfffffffful);
+
+  ACE_U_LongLong ull6 = ull2 + ACE_U_LongLong (0, 1);
+  if (! (ull6.hi () == 3  &&  ull6.lo () == 0x20)) ++errors;
+  ACE_ASSERT (ull6.hi () == 3  &&  ull6.lo () == 0x20);
+
+  ull6 += ACE_U_LongLong (0xffffffff, 0xfff0);
+  if (! (ull6.hi () == 0xfff4  &&  ull6.lo () == 0x1f)) ++errors;
+  ACE_ASSERT (ull6.hi () == 0xfff4  &&  ull6.lo () == 0x1f);
+
+  return errors;
+}
+
 
 int
 main (int, char *[])
 {
+  int ret = 0;
+
   ACE_START_TEST ("Time_Value_Test");
 
   ACE_Time_Value tv1;
@@ -46,6 +102,8 @@ main (int, char *[])
   ACE_ASSERT (tv6 == tv1);
   ACE_ASSERT (tv5 == tv7);
 
+  if (test_ace_u_longlong () != 0) ++ret;
+
   ACE_END_TEST;
-  return 0;
+  return ret;
 }
