9f32046d29e35a0f5b34c9754408194d467aaedf
nw1 <nw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-09-01 07:43:36 +0000

    Moved directive ACE_HAS_BROKEN_EXPLICIT_TEMPLATE_DESTRUCTOR closer to other template-related directives.

--- trunk/ace/config-sunos5.4-g++.h
+++ trunk/ace/config-sunos5.4-g++.h
@@ -11,12 +11,12 @@
 #define __ACE_INLINE__
 #endif /* ! __ACE_INLINE__ */
 
-#define ACE_HAS_BROKEN_EXPLICIT_TEMPLATE_DESTRUCTOR
 // Optimize ACE_Handle_Set for select().
 #define ACE_HAS_HANDLE_SET_OPTIMIZED_FOR_SELECT
 
 #define ACE_HAS_STRING_CLASS
 #define ACE_HAS_TEMPLATE_SPECIALIZATION
+#define ACE_HAS_BROKEN_EXPLICIT_TEMPLATE_DESTRUCTOR
 
 // Platform supports pread() and pwrite()
 #define ACE_HAS_P_READ_WRITE
