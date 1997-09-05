76e652f401b04207d957b5b068ecf73f2faed95b
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-09-05 21:17:36 +0000

    added command line options, and explanation on how to interpret the results

--- trunk/performance-tests/Misc/preempt.cpp
+++ trunk/performance-tests/Misc/preempt.cpp
@@ -9,12 +9,22 @@
 //    preempt.cpp
 //
 // = DESCRIPTION
-//      This is a simple test to illustrate OS thread preemption.
-//      A high priority thread periodically (every half second) reads the
-//      clock.  It uses select () to block for that duration.  Meanwhile,
-//      a low priority thread continually chews up the CPU.  Without
-//      preemption, the high priority thread won't have a chance to read
-//      the clock in a timely manner.
+//      This is a simple test to illustrate OS thread preemption.  One
+//      ore more high priority threads periodically (every half
+//      second, by default) reads the clock.  They use select () to
+//      block for that duration.  Meanwhile, a low priority thread
+//      continually chews up the CPU.  Without preemption, the high
+//      priority thread won't have a chance to read the clock in a
+//      timely manner.
+//
+//      At the end of the test, the actual clock read intervals by the
+//      high priority task(s) are printed out.  With proper
+//      preemption, the intervals should correspond to the requested
+//      clock read interval.
+//
+//      There is a -y option for the low priority thread to periodically
+//      yield.  It shouldn't be necessary to use that option, if preemption
+//      is supported.  It's a handy option for testing.
 //
 // = AUTHOR
 //    David L. Levine
@@ -23,9 +33,24 @@
 
 #include "ace/Task.h"
 #include "ace/Sched_Params.h"
+#include "ace/Get_Opt.h"
 
 #if defined (ACE_HAS_THREADS)
 
+static const char usage [] = "[-? |\n"
+                             "       [-c <iterations, default 10>]\n"
+                             "       [-n <high priority threads, default 1>\n"
+                             "       [-p <read period, default 500000 usec>\n"
+                             "       [-y to yield the low priority thread\n"
+                             "[<iterations>]]";
+
+// Configuration options.
+u_int iterations = 10;
+u_int high_priority_tasks = 1;
+u_long read_period = 500000; /* usec */
+u_int low_yield = 0;
+
+// To permit calculation of relative times.
 ACE_hrtime_t starttime;
 
 
@@ -39,6 +64,7 @@ class High_Priority_Task : public ACE_Task<ACE_MT_SYNCH>
 {
 public:
   High_Priority_Task (void);
+  ~High_Priority_Task (void);
 
   int open (void *);
   int svc (void);
@@ -48,10 +74,7 @@ public:
 private:
   int priority_;
   int done_;
-
-  enum { ITERATIONS = 10 };
-
-  u_long time_ [ITERATIONS];
+  u_long *time_;
 };
 
 High_Priority_Task::High_Priority_Task (void)
@@ -63,15 +86,19 @@ High_Priority_Task::High_Priority_Task (void)
                  ACE_SCOPE_THREAD)),
     done_ (0)
 {
+  ACE_NEW (time_, u_long[iterations]);
+}
+
+High_Priority_Task::~High_Priority_Task (void)
+{
+  delete [] time_;
+  time_ = 0;
 }
 
 int
 High_Priority_Task::open (void *)
 {
-  long flags = THR_BOUND;
-
-  // To get FIFO scheduling with PTHREADS.
-  flags |= THR_SCHED_FIFO;
+  long flags = THR_BOUND | THR_SCHED_FIFO;
 
   // Become an active object.
   if (this->activate (flags, 1, 0, this->priority_) == -1)
@@ -96,9 +123,9 @@ High_Priority_Task::svc (void)
 	      prio, this->priority_));
   ACE_ASSERT (this->priority_ == prio);
 
-  ACE_Time_Value pause (0, 500000);
+  ACE_Time_Value pause (0, read_period);
 
-  for (u_int i = 0; i < ITERATIONS; ++i)
+  for (u_int i = 0; i < iterations; ++i)
     {
       time_ [i] = (u_long) ((ACE_OS::gethrtime () - starttime)/1000000ul);
       ACE_OS::select (0, 0, 0, 0, &pause);
@@ -112,10 +139,10 @@ High_Priority_Task::svc (void)
 void
 High_Priority_Task::print_times () const
 {
-  for (u_int i = 0; i < ITERATIONS; ++i)
+  for (u_int i = 0; i < iterations; ++i)
     {
-      ACE_OS::fprintf (stderr, "High: iteration %u, time %lu msec\n",
-                       i, time_ [i]);
+      ACE_OS::printf ("  iteration %u, time %lu msec\n",
+                      i, time_ [i]);
     }
 }
 
@@ -151,10 +178,7 @@ Low_Priority_Task::Low_Priority_Task (
 int
 Low_Priority_Task::open (void *)
 {
-  long flags = THR_BOUND;
-
-  // To get FIFO scheduling with PTHREADS.
-  flags |= THR_SCHED_FIFO;
+  long flags = THR_BOUND | THR_SCHED_FIFO;
 
   // Become an active object.
   if (this->activate (flags, 1, 0, this->priority_) == -1)
@@ -184,7 +208,7 @@ Low_Priority_Task::svc (void)
 
   // Chew up CPU for the entire interval.
   for (;
-       //       ! high_priority_task_.done () &&
+         ! high_priority_task_.done () &&
          iterations < interval * 1000000 / 40.2;
        ++iterations)
     {
@@ -192,9 +216,11 @@ Low_Priority_Task::svc (void)
       ACE::is_prime (n,
                      2ul /* min_factor */,
                      n/2 /* max_factor */);
+
+      if (low_yield) ACE_OS::thr_yield ();
     }
 
-  ACE_OS::fprintf (stderr, "%lu iterations\n", iterations);
+  ACE_OS::printf ("%lu iterations\n", iterations);
 
   return 0;
 }
@@ -204,14 +230,93 @@ Low_Priority_Task::svc (void)
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
+// function get_options
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+static
+unsigned int
+get_options (int argc, char *argv[])
+{
+  ACE_Get_Opt get_opt (argc, argv, "c:n:p:y?");
+  int opt;
+  while ((opt = get_opt ()) != EOF) {
+    switch (opt) {
+    case 'c':
+      if (ACE_OS::atoi (get_opt.optarg) >= 2)
+        {
+          iterations = ACE_OS::atoi (get_opt.optarg);
+        }
+      else
+        {
+          ACE_DEBUG ((LM_ERROR, "%n: iterations must be >= 2\n"));
+          return 1;
+        }
+      break;
+    case 'n':
+      if (ACE_OS::atoi (get_opt.optarg) >= 1)
+        {
+          high_priority_tasks = ACE_OS::atoi (get_opt.optarg);
+        }
+      else
+        {
+          ACE_DEBUG ((LM_ERROR, "%n: number of high priority threads "
+                                "must be >= 1\n"));
+          return 1;
+        }
+      break;
+    case 'p':
+      if (ACE_OS::atoi (get_opt.optarg) > 0)
+        {
+          read_period = ACE_OS::atoi (get_opt.optarg);
+        }
+      else
+        {
+          ACE_DEBUG ((LM_ERROR, "%n: read period > 0\n"));
+          return 1;
+        }
+      break;
+    case 'y':
+      low_yield = 1;
+      break;
+    case '?':
+      ACE_DEBUG ((LM_ERROR, "usage: %n %s\n%a", usage, 0));
+      break;
+    default:
+      ACE_DEBUG ((LM_ERROR, "%n: unknown arg, %c\n", opt));
+      ACE_DEBUG ((LM_ERROR, "usage: %n %s\n", usage));
+      return 1;
+    }
+  }
+
+  switch (argc - get_opt.optind) {
+  case 0:
+    // use default number of iterations
+    break;
+  default:
+    ACE_DEBUG ((LM_ERROR, "%n: too many arguments\n"));
+    ACE_DEBUG ((LM_ERROR, "usage: %n %s\n", usage));
+    return 1;
+  }
+
+  return 0;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // function main
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
 int
-main (int, char *[])
+main (int argc, char *argv[])
 {
 #if defined (ACE_HAS_THREADS)
+
+  u_int i;
+
+  if (get_options (argc, argv)) ACE_OS::exit (-1);
 
   // Enable FIFO scheduling, e.g., RT scheduling class on Solaris.
   if (ACE_OS::sched_params (
@@ -232,15 +337,21 @@ main (int, char *[])
         }
     }
 
-  High_Priority_Task high_priority_task;
-  Low_Priority_Task low_priority_task (high_priority_task);
+  High_Priority_Task *high_priority_task;
+  ACE_NEW_RETURN (high_priority_task, High_Priority_Task [high_priority_tasks],
+                  1);
+                  
+  Low_Priority_Task low_priority_task (high_priority_task[0]);
 
   // Save the start time, so that deltas can be displayed later.
   starttime = ACE_OS::gethrtime ();
 
   // Spawn the threads . . .
+  for (i = 0; i < high_priority_tasks; ++i)
+  {
+    high_priority_task[i].open (0);
+  }
   low_priority_task.open (0);
-  high_priority_task.open (0);
 
   ACE_DEBUG ((LM_DEBUG, "(%t) main (), wait for threads to exit . . .\n"));
 
@@ -248,7 +359,13 @@ main (int, char *[])
   ACE_Thread_Manager::instance ()->wait ();
 
   // Display the time deltas.  They should be about a half second apart.
-  high_priority_task.print_times ();
+  for (i = 0; i < high_priority_tasks; ++i)
+  {
+    ACE_OS::printf ("High priority task %u:\n", i + 1);
+    high_priority_task[i].print_times ();
+  }
+
+  delete [] high_priority_task;
 
 #else
   ACE_ERROR ((LM_ERROR, "threads not supported on this platform\n"));
