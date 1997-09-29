fe9d3c146bf4cffc29607cf56d78cd917331e655
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-09-29 16:26:53 +0000

    *** empty log message ***

--- apps/JAWS/server/HTTP_Server_T.h
+++ apps/JAWS/server/HTTP_Server_T.h
@@ -29,7 +29,8 @@ public:
   int accept (ACE_SOCK_Stream &new_stream,
 	      ACE_Addr *remote_addr = 0,
 	      ACE_Time_Value *timeout = 0,
-	      int restart = 1) const;
+	      int restart = 1,
+              int reset_new_handle = 0) const;
   
 private:
   LOCK lock_;
