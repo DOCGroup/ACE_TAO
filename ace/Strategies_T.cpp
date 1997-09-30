c6489d434dfacc32dbf52d9f696419de36c447f0
nw1 <nw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-09-30 03:39:22 +0000

    Changed to use reset_new_handle () for querying whether we need to reset newly accepted/connected handles or not.

--- ace/Strategies_T.cpp
+++ ace/Strategies_T.cpp
@@ -433,12 +433,11 @@ ACE_Accept_Strategy<SVC_HANDLER, ACE_PEER_ACCEPTOR_2>::accept_svc_handler
   int reset_new_handle = 0;
 #if defined (ACE_WIN32)
   // Try to find out if the implementation of the reactor that we are
-  // using is the WFMO_Reactor. If so we need to reset the event
-  // association for the newly created handle. This is because the
-  // newly created handle will inherit the properties of the listen
-  // handle, including its event associations.
-  if (dynamic_cast <ACE_WFMO_Reactor *> (this->reactor_->implementation ()))
-    reset_new_handle = 1;
+  // using requires us to reset the event association for the newly
+  // created handle. This is because the newly created handle will
+  // inherit the properties of the listen handle, including its event
+  // associations.
+  reset_new_handle = this->reactor_->reset_new_handle ();
 #endif /* ACE_WIN32 */
   
   if (this->acceptor_.accept (svc_handler->peer (), // stream
