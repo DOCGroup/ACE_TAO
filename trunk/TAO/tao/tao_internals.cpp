4e7209470a7f5dcc0d709b3a816c0fa219c9b573
cleeland <cleeland@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-10-02 19:03:22 +0000

    ORB-per-thread now works as long as object keys are unique throughout the process

---
+++ trunk/TAO/tao/tao_internals.cpp
@@ -0,0 +1,43 @@
+#if !defined(__ACE_INLINE__)
+#  include "tao_internals.h"
+#endif /* __ACE_INLINE__ */
+
+#include "ace/Service_Config.h"
+#include "ace/Service_Repository.h"
+#include "tao/default_server.h"
+#include "tao/default_client.h"
+
+ACE_Thread_Mutex TAO_Internal::orbinit_lock_;
+ACE_Thread_Mutex TAO_Internal::service_lock_;
+int TAO_Internal::service_open_count_ = 0;
+
+int
+TAO_Internal::fake_service_entries_i (void)
+{
+#define FAKE_SVC_ENTRY(svcname, svctype, argc, argv) \
+  do \
+    { \
+      ACE_Service_Object *obj = _make_##svctype (); \
+      obj->init (argc, argv); \
+      ACE_Service_Repository::instance()->insert \
+        (new ACE_Service_Type (svcname,\
+                                 new ACE_Service_Object_Type\
+                                 (obj, svcname, \
+                                  (ACE_Service_Type::DELETE_OBJ \
+                                   | ACE_Service_Type::DELETE_THIS)), \
+                                  0, 1));\
+    }\
+  while (0) //;
+
+  char* client_args[] = { 0 };
+  FAKE_SVC_ENTRY ("Client_Strategy_Factory",
+                  TAO_Default_Client_Strategy_Factory,
+                  0, client_args);
+  
+  char* server_args[] = { "-T", "-L", "dynamic", "-o", "128", 0 };
+  FAKE_SVC_ENTRY ("Server_Strategy_Factory",
+                  TAO_Default_Server_Strategy_Factory,
+                  sizeof(server_args)/sizeof(server_args[0]), server_args);
+
+  return 0;
+}
---
+++ trunk/TAO/tao/tao_internals.i
@@ -0,0 +1,36 @@
+// -*- c++ -*-
+//
+// $Id$
+
+#include "ace/Service_Config.h"
+
+ACE_INLINE int
+TAO_Internal::open_services (int& argc, char** argv)
+{
+  ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, guard, service_lock_, -1));
+
+  if (TAO_Internal::service_open_count_++ == 0)
+    {
+#if defined(TAO_PLATFORM_SVC_CONF_FILE_NOTSUP)
+      return fake_service_entries_i ();
+#else      
+      return ACE_Service_Config::open (argc, argv);
+#endif
+    }
+}
+
+ACE_INLINE int
+TAO_Internal::close_services (void)
+{
+  ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, guard, service_lock_, -1));
+
+  if (service_open_count_ == 0)
+    return -1;
+  
+  if (--service_open_count_ == 0)
+    return ACE_Service_Config::close ();
+
+  return 0;
+}
+
+
