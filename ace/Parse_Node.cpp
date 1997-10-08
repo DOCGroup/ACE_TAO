e29329282bfd2d0bcd6b8cfd1440616c5247071f
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-10-08 14:54:44 +0000

    ChangeLogTag:Wed Oct  8 09:52:20 1997  Carlos O'Ryan  <coryan@macarena.cs.wustl.edu>

--- ace/Parse_Node.cpp
+++ ace/Parse_Node.cpp
@@ -586,8 +586,7 @@ ACE_Static_Function_Node::symbol (void)
 	  ace_yyerrno++;
 
 	  ACE_ERROR_RETURN ((LM_ERROR,
-			     "no static service registered for function %s\n
-",
+			     "no static service registered for function %s\n",
 			     this->function_name_),
 			     0);
 	}
