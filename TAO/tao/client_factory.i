bd05e81b1ba53375975ba302115dad2569bea3d8
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-10-12 23:25:37 +0000

    *** empty log message ***

--- TAO/tao/client_factory.i
+++ TAO/tao/client_factory.i
@@ -1,25 +1,10 @@
-ACE_INLINE
-TAO_Client_Connection_Handler::TAO_Client_Connection_Handler (ACE_Thread_Manager *)
-  : in_use_ (0)
-{}
-
-ACE_INLINE CORBA::Boolean
-TAO_Client_Connection_Handler::in_use (void)
-{
-  return in_use_;
-}
-
-ACE_INLINE void
-TAO_Client_Connection_Handler::in_use (CORBA::Boolean flag)
-{
-  in_use_ = flag;
-}
-
+#if 0
 ACE_INLINE TAO_CONNECTOR *
 TAO_Client_Strategy_Factory::connector (void)
 {
   return 0;
 }
+#endif /* 0 */
 
 ACE_INLINE
 TAO_Client_Strategy_Factory::TAO_Client_Strategy_Factory (void)
@@ -28,22 +13,4 @@ TAO_Client_Strategy_Factory::TAO_Client_Strategy_Factory (void)
 ACE_INLINE
 TAO_Client_Strategy_Factory::~TAO_Client_Strategy_Factory (void)
 {}
-
-// @@ Chris, shouldn't we protect this stuff with some type of #ifdef
-// for platforms that lack template specialization?  
-// Template specializations that allow the cached connection manager
-// to work better.
-
-ACE_INLINE size_t
-ACE_Hash_Addr<ACE_INET_Addr, TAO_Client_Connection_Handler>::hash_i (const ACE_INET_Addr &addr) const
-{
-  return addr.get_ip_address () + addr.get_port_number ();
-}
-
-ACE_INLINE int
-ACE_Hash_Addr<ACE_INET_Addr, TAO_Client_Connection_Handler>::compare_i (const ACE_INET_Addr &a1, 
-                                                                        const ACE_INET_Addr &a2) const
-{
-  return a1 != a2;
-}
 
--- TAO/tao/connect.h
+++ TAO/tao/connect.h
@@ -25,6 +25,51 @@
 // Forward Decls
 class TAO_OA_Parameters;
 
+class TAO_Client_Connection_Handler : public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH>
+  // = TITLE
+  //      <Svc_Handler> used on the client side and returned by the
+  //      <TAO_CONNECTOR>.  
+  // @@ Should this be in here or in the default_client.*?
+{
+public:
+  // = Intialization method.
+  TAO_Client_Connection_Handler (ACE_Thread_Manager * = 0);
+  // Do-nothing constructor
+
+  // = <Connector> hook.
+  virtual int open (void *);
+  // Initialization hook
+
+  void in_use (CORBA::Boolean);
+  // Set the in-use flag.
+
+  CORBA::Boolean in_use (void);
+  // Return state of the in-use flag.
+
+private:
+  CORBA::Boolean in_use_;
+  // True value indicates that something is using this handler.
+};
+
+// = Useful typedef.
+
+typedef ACE_Strategy_Connector<TAO_Client_Connection_Handler, ACE_SOCK_CONNECTOR> 
+	TAO_CONNECTOR;
+
+typedef ACE_Cached_Connect_Strategy<TAO_Client_Connection_Handler, 
+	                            ACE_SOCK_CONNECTOR,
+				    ACE_SYNCH_RW_MUTEX>
+        TAO_CACHED_CONNECT_STRATEGY;
+
+typedef ACE_NOOP_Creation_Strategy<TAO_Client_Connection_Handler> 
+	TAO_NULL_CREATION_STRATEGY;
+
+#if defined (TAO_HAS_CLIENT_CONCURRENCY)
+  // @@ Chris, shouldn't this always be "potentially" the case, even
+  // if a client didn't want to use it? 
+  virtual CONCURRENCY_STRATEGY *concurrency_strategy (void);
+#endif /* TAO_HAS_CLIENT_CONCURRENCY */
+
 // @@ Is this really an *OA* connection handler anymore?  Seems like
 // connections are really associated with ORBs in the POA-based
 // architecture.
