dba4ba17fa3b77d4ae34c97481a313bc943a7f28
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-10-14 04:35:17 +0000

    *** empty log message ***

--- TAO/tao/connect.h
+++ TAO/tao/connect.h
@@ -53,12 +53,19 @@ private:
 
 // = Useful typedef.
 
+// If we are using TSS, there is no reason to use locking in the connector
+#if defined (TAO_HAS_TSS_ORBCORE)
+#  define ACE_CACHED_CONNECT_STRATEGY_LOCK ACE_SYNCH_RW_MUTEX
+#else
+#  define ACE_CACHED_CONNECT_STRATEGY_LOCK ACE_SYNCH_NULL_MUTEX
+#endif
+
 typedef ACE_Strategy_Connector<TAO_Client_Connection_Handler, ACE_SOCK_CONNECTOR> 
 	TAO_CONNECTOR;
 
 typedef ACE_Cached_Connect_Strategy<TAO_Client_Connection_Handler, 
 	                            ACE_SOCK_CONNECTOR,
-				    ACE_SYNCH_RW_MUTEX>
+				    ACE_CACHED_CONNECT_STRATEGY_LOCK>
         TAO_CACHED_CONNECT_STRATEGY;
 
 typedef ACE_NOOP_Creation_Strategy<TAO_Client_Connection_Handler> 
