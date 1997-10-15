34b47070891a5892991a10c7dd7ba454aaffb338
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-10-15 19:52:48 +0000

    wrap numbers in parens

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -1,6 +1,6 @@
 // $Id$
 // This should be an automatically generated file.
 
-#define ACE_MAJOR_VERSION 4
-#define ACE_MINOR_VERSION 3
-#define ACE_BETA_VERSION 19
+#define ACE_MAJOR_VERSION (4)
+#define ACE_MINOR_VERSION (3)
+#define ACE_BETA_VERSION (19)
