0d7646f8db8abd05aaf83277a6afdb3ea95f7f2e
schmidt <douglascraigschmidt@users.noreply.github.com>
1997-10-20 07:04:34 +0000

    ACE version 4.3.22, released Mon Oct 20 02:04:19 1997.

--- ace/Version.h
+++ ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (3u)
-#define ACE_BETA_VERSION (21u)
-#define ACE_VERSION "4.3.21\0"
+#define ACE_BETA_VERSION (22u)
+#define ACE_VERSION "4.3.22\0"
