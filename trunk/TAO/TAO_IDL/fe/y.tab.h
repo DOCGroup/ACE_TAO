89d1d71a762f0aa8ad59f27058a92dcbd4351f0f
gokhale <gokhale@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-10-25 22:49:31 +0000

    used %array declaration for yytext. CVS: CVS: CVS: CVS:

--- trunk/TAO/TAO_IDL/fe/y.tab.h
+++ trunk/TAO/TAO_IDL/fe/y.tab.h
@@ -1,9 +1,4 @@
-
-typedef union
-#ifdef __cplusplus
-	YYSTYPE
-#endif
- {
+typedef union {
   AST_Decl		*dcval;		/* Decl value		*/
   UTL_StrList		*slval;		/* String list		*/
   UTL_NameList		*nlval;		/* Name list		*/
@@ -29,48 +24,50 @@ typedef union
   Identifier		*idval;		/* Identifier		*/
   UTL_IdList		*idlist;	/* Identifier list	*/
 } YYSTYPE;
+#define	IDENTIFIER	258
+#define	CONST	259
+#define	MODULE	260
+#define	INTERFACE	261
+#define	TYPEDEF	262
+#define	LONG	263
+#define	SHORT	264
+#define	UNSIGNED	265
+#define	DOUBLE	266
+#define	FLOAT	267
+#define	CHAR	268
+#define	WCHAR	269
+#define	OCTET	270
+#define	BOOLEAN	271
+#define	ANY	272
+#define	STRUCT	273
+#define	UNION	274
+#define	SWITCH	275
+#define	ENUM	276
+#define	SEQUENCE	277
+#define	STRING	278
+#define	WSTRING	279
+#define	EXCEPTION	280
+#define	CASE	281
+#define	DEFAULT	282
+#define	READONLY	283
+#define	ATTRIBUTE	284
+#define	ONEWAY	285
+#define	IDEMPOTENT	286
+#define	VOID	287
+#define	IN	288
+#define	OUT	289
+#define	INOUT	290
+#define	RAISES	291
+#define	CONTEXT	292
+#define	INTEGER_LITERAL	293
+#define	STRING_LITERAL	294
+#define	CHARACTER_LITERAL	295
+#define	FLOATING_PT_LITERAL	296
+#define	TRUETOK	297
+#define	FALSETOK	298
+#define	SCOPE_DELIMITOR	299
+#define	LEFT_SHIFT	300
+#define	RIGHT_SHIFT	301
+
+
 extern YYSTYPE yylval;
-# define IDENTIFIER 257
-# define CONST 258
-# define MODULE 259
-# define INTERFACE 260
-# define TYPEDEF 261
-# define LONG 262
-# define SHORT 263
-# define UNSIGNED 264
-# define DOUBLE 265
-# define FLOAT 266
-# define CHAR 267
-# define WCHAR 268
-# define OCTET 269
-# define BOOLEAN 270
-# define ANY 271
-# define STRUCT 272
-# define UNION 273
-# define SWITCH 274
-# define ENUM 275
-# define SEQUENCE 276
-# define STRING 277
-# define WSTRING 278
-# define EXCEPTION 279
-# define CASE 280
-# define DEFAULT 281
-# define READONLY 282
-# define ATTRIBUTE 283
-# define ONEWAY 284
-# define IDEMPOTENT 285
-# define VOID 286
-# define IN 287
-# define OUT 288
-# define INOUT 289
-# define RAISES 290
-# define CONTEXT 291
-# define INTEGER_LITERAL 292
-# define STRING_LITERAL 293
-# define CHARACTER_LITERAL 294
-# define FLOATING_PT_LITERAL 295
-# define TRUETOK 296
-# define FALSETOK 297
-# define SCOPE_DELIMITOR 298
-# define LEFT_SHIFT 299
-# define RIGHT_SHIFT 300
