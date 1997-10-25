aee7b278df7a992e1504d56a5c3f655e05cabeb6
sergio <sergio@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-10-25 22:46:42 +0000

    *** empty log message ***

--- examples/Timer_Queue/main_thread.cpp
+++ examples/Timer_Queue/main_thread.cpp
@@ -4,9 +4,9 @@
 //
 // = LIBRARY
 //    examples
-// 
+//
 // = FILENAME
-//    main_thread.cpp             
+//    main_thread.cpp
 //
 // = DESCRIPTION
 //      Implements an threaded timer queue.
@@ -15,15 +15,15 @@
 //
 // = AUTHORS
 //    Douglas Schmidt      <schmidt@cs.wustl.edu> &&
-//    Sergio Flores-Gaitan <sergio@cs.wustl.edu>  
-// 
+//    Sergio Flores-Gaitan <sergio@cs.wustl.edu>
+//
 // ============================================================================
 
 #include "ace/Auto_Ptr.h"
 #include "Driver.h"
 #include "Thread_Timer_Queue_Test.h"
 
-typedef Timer_Queue_Test_Driver<Thread_Timer_Queue, 
+typedef Timer_Queue_Test_Driver<Thread_Timer_Queue,
                                 Input_Task,
 				Input_Task::ACTION>
 	THREAD_TIMER_QUEUE_TEST_DRIVER;
@@ -35,14 +35,17 @@ main (int, char *[])
   // automatically.
   auto_ptr <THREAD_TIMER_QUEUE_TEST_DRIVER> driver;
   THREAD_TIMER_QUEUE_TEST_DRIVER *tqtd;
-  
+
   ACE_NEW_RETURN (tqtd, Thread_Timer_Queue_Test_Driver, -1);
   driver = tqtd;
-  
+
   return driver->run_test ();
 }
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 template class auto_ptr <THREAD_TIMER_QUEUE_TEST_DRIVER>;
 template class ACE_Auto_Basic_Ptr <THREAD_TIMER_QUEUE_TEST_DRIVER>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate auto_ptr <THREAD_TIMER_QUEUE_TEST_DRIVER>
+#pragma instantiate ACE_Auto_Basic_Ptr <THREAD_TIMER_QUEUE_TEST_DRIVER>
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
