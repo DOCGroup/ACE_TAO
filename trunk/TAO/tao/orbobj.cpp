7d054832f1a0b091f0c5e8356284eb227d350ffd
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-06 20:51:38 +0000

    ACE_Recursive_Thread_Mutex instead of ACE_Thread_Mutex

--- trunk/TAO/tao/orbobj.cpp
+++ trunk/TAO/tao/orbobj.cpp
@@ -180,7 +180,7 @@ CORBA::ORB_init (int &argc,
 {
   // Using ACE_Static_Object_Lock::instance() precludes ORB_init from being called
   // within a static object CTOR.
-  ACE_GUARD_RETURN (ACE_Thread_Mutex, guard,
+  ACE_GUARD_RETURN (ACE_Recursive_Thread_Mutex, guard,
                     *ACE_Static_Object_Lock::instance (), 0);
 
   env.clear ();
