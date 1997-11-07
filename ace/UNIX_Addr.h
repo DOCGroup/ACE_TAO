f1a7b878364b7c986e207dce3ffd808784d02971
Steve Huston <shuston@riverace.com>
1997-11-07 03:21:52 +0000

    Changed operators == and != to be non-virtual, and to take the 'sap' argument as their own type (not ACE_Addr).  Changed the implementations to use more of the type-specific data.

--- ace/UNIX_Addr.h
+++ ace/UNIX_Addr.h
@@ -59,10 +59,10 @@ public:
   virtual int string_to_addr (const char addr[]);
   // Transform the string into the current addressing format.
 
-  virtual int  operator == (const ACE_Addr &SAP) const;
+  int  operator == (const ACE_UNIX_Addr &SAP) const;
   // Compare two addresses for equality.
 
-  virtual int  operator != (const ACE_Addr &SAP) const;
+  int  operator != (const ACE_UNIX_Addr &SAP) const;
   // Compare two addresses for inequality.
 
   const char *get_path_name (void) const; 
