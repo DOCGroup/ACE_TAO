4a24cf15af6926a7102c36b357d02accdcb42bc2
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-12 04:45:27 +0000

    (inet_aton): declare local ip_addr as an ACE_UINT32 instead of a long.  Real programmers only use 64-bit machines.

--- ace/OS.cpp
+++ ace/OS.cpp
@@ -2878,8 +2878,8 @@ ACE_Thread_ID::operator!= (const ACE_Thread_ID &rhs)
 int
 ACE_OS::inet_aton (const char *host_name, struct in_addr *addr)
 {
-  long ip_addr = ACE_OS::inet_addr (host_name);
-  if (ip_addr == (long) htonl ((ACE_UINT32) ~0)
+  ACE_UINT32 ip_addr = ACE_OS::inet_addr (host_name);
+  if (ip_addr == (ACE_UINT32) htonl ((ACE_UINT32) ~0)
       // Broadcast addresses are weird...
       && ACE_OS::strcmp (host_name, "255.255.255.255") != 0)
     return 0;
