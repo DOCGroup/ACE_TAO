7676dbe27125f80238d7ee02da58488251788c5e
sergio <sergio@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-18 08:08:59 +0000

    *** empty log message ***

--- TAO/orbsvcs/tests/Logger/svr.cpp
+++ TAO/orbsvcs/tests/Logger/svr.cpp
@@ -65,43 +65,73 @@ main (int argc, char ** argv)
     }
   
   ACE_DEBUG ((LM_DEBUG, "listening as object '%s'\n", str)); 
-	     
-  CORBA::Object_ptr  obj_ptr = 
-    orb_ptr->resolve_initial_references ("NameService");
-  
-  if (CORBA::is_nil (obj_ptr) == CORBA::B_TRUE)
-    ACE_ERROR_RETURN ((LM_ERROR, "resolve_initial_references"), 1);
 
-  // resolve the naming service
-  CosNaming::NamingContext_ptr naming_service = 
-    CosNaming::NamingContext::_narrow (obj_ptr, env);
-
-  if (CORBA::is_nil (naming_service) == CORBA::B_TRUE)
-    ACE_ERROR_RETURN ((LM_ERROR, "_narrow"), 1);
-
-  // The name of the logger factory in the naming service.
+  int name_service_flag = 0;
+  CosNaming::NamingContext_ptr naming_service = 0;
   CosNaming::Name n(1);
-  n.length (1);
-  n[0].id = CORBA::string_dup ("logger_factory");
+
+  do {
+    CORBA::Object_ptr  obj_ptr = 
+      orb_ptr->resolve_initial_references ("NameService");
   
-  // bind the logger factory to a name using the naming service.
-  naming_service->bind (n, f, env);
-  
-  // check for errors!
-  if (env.exception () != 0)
+    // Use the naming service to advertise us, if we could successfully get a reference.
+    if (CORBA::is_nil (obj_ptr) == CORBA::B_TRUE)
+      {
+	ACE_ERROR ((LM_ERROR, "resolve_initial_references failed.\n"));
+	break;
+      }
+    
+    // resolve the naming service
+    naming_service = CosNaming::NamingContext::_narrow (obj_ptr, env);
+
+    if (CORBA::is_nil (naming_service) == CORBA::B_TRUE)
+      {
+	ACE_ERROR ((LM_ERROR, "_narrow failed\n"));
+	break;
+      }
+
+    // The name of the logger factory in the naming service.
+    n.length (1);
+    n[0].id = CORBA::string_dup ("logger_factory");
+    
+    // bind the logger factory to a name using the naming service.
+    naming_service->bind (n, f, env);
+    
+    // check for errors!
+    if (env.exception () != 0)
+      {
+	env.print_exception ("name_service->bind()");
+	break;
+      }
+    else
+      {
+	ACE_DEBUG ((LM_DEBUG, "Success using the naming service!! to bind the logger factory!\n"));
+	name_service_flag = 1;
+      }
+
+  } while (0);
+
+  if (name_service_flag == 0)
     {
-      env.print_exception ("name_service->bind()");
-      return 1;
-    }
-  else
-    {
-      ACE_DEBUG ((LM_DEBUG, "Success using the naming service!! to bind the logger factory!"));
+      ACE_ERROR ((LM_ERROR, "Unable to use the naming service.\n"));
     }
 
   // Handle requests for this object until we're killed, or one of the
   // methods asks us to exit.
   if (orb_ptr->run () == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "run"), -1);
+
+  // unbind the logger factory name 
+  naming_service->unbind (n, env);
+
+  // check for errors!
+  if (env.exception () != 0)
+    {
+      env.print_exception ("name_service->unbind()");
+    }
+
+  // release the object reference for the naming service.
+  CORBA::release (naming_service);
   
   cout << "Server logger_factory is terminating" << endl;
   return 0;
