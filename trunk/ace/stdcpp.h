771aa85325f80242efe439f05f6f2a66b326e58c
nw1 <nw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-19 18:20:18 +0000

    Reverted inclusion of OS.h back to stdcpp.h. Added ACE_CONFIG_H to prevent config.h from getting included multiple times.

--- trunk/ace/stdcpp.h
+++ trunk/ace/stdcpp.h
@@ -22,7 +22,10 @@
 #if !defined (ACE_STDCPP_H)
 #define ACE_STDCPP_H
 
-#include "ace/OS.h"
+#if !defined (ACE_CONFIG_H)
+#define ACE_CONFIG_H
+#include "ace/config.h"
+#endif /* ACE_CONFIG_H */
 
 #if !defined (ACE_HAS_MINIMUM_IOSTREAMH_INCLUSION)
 # if defined (ACE_HAS_STANDARD_CPP_LIBRARY) && (ACE_HAS_STANDARD_CPP_LIBRARY != 0)
