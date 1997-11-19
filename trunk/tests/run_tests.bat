002f11d4e5fa9c1e9262d4747d9583e24d6300fa
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-19 19:40:44 +0000

    Merged the purify testing into run_tests.  Just use "run_tests purify ..." to purify the already compiled tests.

--- trunk/tests/run_tests.bat
+++ trunk/tests/run_tests.bat
@@ -3,81 +3,109 @@ rem    To use this either give it no arguments to run all the tests or
 rem    pass it the test name (without the extention) to run only one
 rem    test
 
-if not "%1" == "" goto runtest
+setlocal
 
-call %0 Atomic_Op_Test
-call %0 Barrier_Test
-call %0 Buffer_Stream_Test
-call %0 Conn_Test
-call %0 Enum_Interfaces_Test
-call %0 Future_Test
-call %0 Handle_Set_Test
-call %0 Hash_Map_Manager_Test
-call %0 IOStream_Test
-call %0 Map_Manager_Test
-call %0 Mem_Map_Test
-call %0 Message_Block_Test
-call %0 Message_Queue_Notifications_Test
-call %0 Message_Queue_Test
-call %0 MM_Shared_Memory_Test
-call %0 MT_SOCK_Test
-call %0 Naming_Test
-call %0 Notify_Performance_Test
-call %0 Pipe_Test
-call %0 Priority_Buffer_Test
-call %0 Priority_Reactor_Test
-call %0 Priority_Task_Test
-call %0 Process_Mutex_Test
-call %0 Process_Strategy_Test
-call %0 Reactors_Test
-call %0 Reactor_Exceptions_Test
-call %0 Reactor_Notify_Test
-call %0 Reactor_Performance_Test
-call %0 Reactor_Timer_Test
-call %0 Reader_Writer_Test
-call %0 Recursive_Mutex_Test
-call %0 Semaphore_Test
-call %0 Service_Config_Test
-call %0 Sigset_Ops_Test
-call %0 Simple_Message_Block_Test
-call %0 SOCK_Test
-call %0 SOCK_Connector_Test
-call %0 SPIPE_Test
-call %0 SString_Test
-call %0 SV_Shared_Memory_Test
-call %0 Task_Test
-call %0 Thread_Manager_Test
-call %0 Thread_Mutex_Test
-call %0 Thread_Pool_Test
-call %0 Timer_Queue_Test
-call %0 Time_Service_Test
-call %0 Time_Value_Test
-call %0 Tokens_Test
-call %0 TSS_Test
-call %0 UPIPE_SAP_Test
+if "%1" == "purify" goto setuppurify
+set dopure=
+set arg=%1
+goto endsetup
+
+:setuppurify
+set dopure=purify
+set arg=%2
+
+:endsetup
+
+if not "%arg%" == "" goto runtest
+
+:runall
+
+call %0 %dopure% Atomic_Op_Test
+call %0 %dopure% Barrier_Test
+call %0 %dopure% Buffer_Stream_Test
+call %0 %dopure% Conn_Test
+call %0 %dopure% Enum_Interfaces_Test
+call %0 %dopure% Future_Test
+call %0 %dopure% Handle_Set_Test
+call %0 %dopure% Hash_Map_Manager_Test
+call %0 %dopure% IOStream_Test
+call %0 %dopure% Map_Manager_Test
+call %0 %dopure% Mem_Map_Test
+call %0 %dopure% Message_Block_Test
+call %0 %dopure% Message_Queue_Notifications_Test
+call %0 %dopure% Message_Queue_Test
+call %0 %dopure% MM_Shared_Memory_Test
+call %0 %dopure% MT_SOCK_Test
+call %0 %dopure% Naming_Test
+call %0 %dopure% Notify_Performance_Test
+call %0 %dopure% Pipe_Test
+call %0 %dopure% Priority_Buffer_Test
+call %0 %dopure% Priority_Reactor_Test
+call %0 %dopure% Priority_Task_Test
+call %0 %dopure% Process_Mutex_Test
+call %0 %dopure% Process_Strategy_Test
+call %0 %dopure% Reactors_Test
+call %0 %dopure% Reactor_Exceptions_Test
+call %0 %dopure% Reactor_Notify_Test
+call %0 %dopure% Reactor_Performance_Test
+call %0 %dopure% Reactor_Timer_Test
+call %0 %dopure% Reader_Writer_Test
+call %0 %dopure% Recursive_Mutex_Test
+call %0 %dopure% Semaphore_Test
+call %0 %dopure% Service_Config_Test
+call %0 %dopure% Sigset_Ops_Test
+call %0 %dopure% Simple_Message_Block_Test
+call %0 %dopure% SOCK_Test
+call %0 %dopure% SOCK_Connector_Test
+call %0 %dopure% SPIPE_Test
+call %0 %dopure% SString_Test
+call %0 %dopure% SV_Shared_Memory_Test
+call %0 %dopure% Task_Test
+call %0 %dopure% Thread_Manager_Test
+call %0 %dopure% Thread_Mutex_Test
+call %0 %dopure% Thread_Pool_Test
+call %0 %dopure% Timer_Queue_Test
+call %0 %dopure% Time_Service_Test
+call %0 %dopure% Time_Value_Test
+call %0 %dopure% Tokens_Test
+call %0 %dopure% TSS_Test
+call %0 %dopure% UPIPE_SAP_Test
 
 goto done
 
 :runtest
 
-echo Running %1
-%1.exe
+if not "%dopure%"=="purify" goto justrun
+
+if not exist purify_results mkdir purify_results
+
+echo Purifying %arg%
+purify /run /save-data=purify_results\%arg%.pfy /save-text-data=purify_results\%arg%.txt /AllocCallStackLength=20 /ErrorCallStackLength=20 /HandlesInUseAtExit /InUseAtExit /LeaksAtExit %arg%.exe
+
+goto done
+
+:justrun
+
+echo Running %arg%
+%arg%.exe
 if errorlevel 0 goto fine
 echo.
-echo %1 has FAILED!!!
+echo %arg% has FAILED!!!
 echo.
-type %temp%\log\%1.log | find /I "assertion failed"
-type %temp%\log\%1.log | find /I "not supported"
-type %temp%\log\%1.log | find /I "no such file or directory"
-type %temp%\log\%1.log | find /I "invalid argument"
-type %temp%\log\%1.log | find /I "timeout"
-type %temp%\log\%1.log | find /I "bad file number"
+type %temp%\log\%arg%.log | find /I "assertion failed"
+type %temp%\log\%arg%.log | find /I "not supported"
+type %temp%\log\%arg%.log | find /I "no such file or directory"
+type %temp%\log\%arg%.log | find /I "invalid argument"
+type %temp%\log\%arg%.log | find /I "timeout"
+type %temp%\log\%arg%.log | find /I "bad file number"
 echo.
 
 goto done
 :fine
 
 rem We should check the log files here to make sure the test ended correctly
-rem type %temp%\log\%1.log | find "Ending"
+rem type %temp%\log\%arg%.log | find "Ending"
 
 :done
+
+endlocal
