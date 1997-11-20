64cec3db5e35bf5e75842aaa2ea1c77b33dc5766
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-20 04:12:15 +0000

    ACE version 4.3.53, released Wed Nov 19 22:11:20 1997.

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (3u)
-#define ACE_BETA_VERSION (52u)
-#define ACE_VERSION "4.3.52\0"
+#define ACE_BETA_VERSION (53u)
+#define ACE_VERSION "4.3.53\0"
