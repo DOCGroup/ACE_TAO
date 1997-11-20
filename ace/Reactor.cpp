8752f0f3c20aeca4e07534e33ea6b8b3b3d01ba6
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-20 16:27:12 +0000

    *** empty log message ***

--- ace/Reactor.cpp
+++ ace/Reactor.cpp
@@ -25,15 +25,11 @@ ACE_Reactor::ACE_Reactor (ACE_Reactor_Impl *impl)
 
   if (this->implementation () == 0)
     {
-#if !defined (ACE_WIN32) || 
-    !defined (ACE_HAS_WINSOCK2) || 
-    (ACE_HAS_WINSOCK2 == 0) || 
-    defined (ACE_USE_SELECT_REACTOR_FOR_REACTOR_IMPL)
+#if !defined (ACE_WIN32) || !defined (ACE_HAS_WINSOCK2) || (ACE_HAS_WINSOCK2 == 0) || defined (ACE_USE_SELECT_REACTOR_FOR_REACTOR_IMPL)
       ACE_NEW (impl, ACE_Select_Reactor);
-#else /* We are on Win32 and we have winsock and
-         ACE_USE_SELECT_REACTOR_FOR_REACTOR_IMPL is not defined */
+#else /* We are on Win32 and we have winsock and ACE_USE_SELECT_REACTOR_FOR_REACTOR_IMPL is not defined */
       ACE_NEW (impl, ACE_WFMO_Reactor);
-#endif /* all the stuff above */
+#endif /* !defined (ACE_WIN32) || !defined (ACE_HAS_WINSOCK2) || (ACE_HAS_WINSOCK2 == 0) || defined (ACE_USE_SELECT_REACTOR_FOR_REACTOR_IMPL) */
       this->implementation (impl);
       this->delete_implementation_ = 1;
     }
