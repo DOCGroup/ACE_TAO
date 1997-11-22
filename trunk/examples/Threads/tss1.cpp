495a301567a60c4845196f03ae04ac3886d98578
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-11-22 23:54:55 +0000

    Moved class Errno out from thread_specific.cpp and tss1.cpp to a new header file, thread_specific.h.  AIX needs the class in a .h file.

--- trunk/examples/Threads/tss1.cpp
+++ trunk/examples/Threads/tss1.cpp
@@ -27,45 +27,11 @@
 
 #if defined (ACE_HAS_THREADS)
 
+#include "thread_specific.h"
+
 // (Sun C++ 4.2 with -O3 won't link if the following is not const.)
 static const int iterations = 100;
 
-class Errno
-{
-public:
-  int error (void) { return this->errno_; }
-  void error (int i) { this->errno_ = i; }
-
-  int line (void) { return this->lineno_; }
-  void line (int l) { this->lineno_ = l; }
-
-  // Errno::flags_ is a static variable, so we've got to protect it
-  // with a mutex since it isn't kept in thread-specific storage.
-  int flags (void) {
-    ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, ace_Mon, Errno::lock_, -1));
-
-    return Errno::flags_;
-  }
-  int flags (int f)
-  {
-    ACE_MT (ACE_GUARD_RETURN (ACE_Thread_Mutex, ace_mon, Errno::lock_, -1));
-
-    Errno::flags_ = f;
-    return 0;
-  }
-
-private:
-  // = errno_ and lineno_ will be thread-specific data so they don't
-  // need a lock.
-  int errno_;
-  int lineno_;
-
-  static int flags_;
-#if defined (ACE_HAS_THREADS)
-  // flags_ needs a lock.
-  static ACE_Thread_Mutex lock_;
-#endif /* ACE_HAS_THREADS */
-};
 
 // Static variables.
 ACE_MT (ACE_Thread_Mutex Errno::lock_);
