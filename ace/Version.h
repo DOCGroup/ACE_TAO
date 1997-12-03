97498f6d5b1928a0e891d3fce59bbf3ecc1d9114
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-12-03 07:13:38 +0000

    This commit was manufactured by cvs2svn to create branch 'STDSTREAMS'.

--- ace/Version.h
+++
@@ -1,7 +0,0 @@
-// $Id$
-// This is an automatically generated file.
-
-#define ACE_MAJOR_VERSION (4u)
-#define ACE_MINOR_VERSION (4u)
-#define ACE_BETA_VERSION (1u)
-#define ACE_VERSION "4.4.1\0"
--- ace/config-sco-5.0.0-nothread.h
+++
@@ -1,7 +0,0 @@
-#if !defined (ACE_CONFIG_H)
-#define ACE_CONFIG_H
-
-#include "ace/config-gcc-2.7.2.h"
-#include "ace/config-sco-5.0.0.h"
-
-#endif /* ACE_CONFIG_H */
