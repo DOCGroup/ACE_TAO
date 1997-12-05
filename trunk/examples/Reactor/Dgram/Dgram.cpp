9bcdc1ea3a01126ccb2617a2f51615e3bf5f5b51
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-12-05 21:24:57 +0000

    *** empty log message ***

--- trunk/examples/Reactor/Dgram/Dgram.cpp
+++ trunk/examples/Reactor/Dgram/Dgram.cpp
@@ -1,18 +1,14 @@
 // $Id$
 
 // Exercise the ACE_SOCK_Dgram wrapper along with the ACE_Reactor.
-//
-// Typical invocation sequence is:
-//
-// % Dgram 10000 localhost 10001 &
-// % Dgram 10001 localhost 10000
-//
-// This will start two interacting copies of the Dgram
-// application. 
+// This test simply ping-pongs datagrams back and forth between the
+// parent and child process.
 
 #include "ace/Reactor.h"
 #include "ace/SOCK_Dgram.h"
 #include "ace/INET_Addr.h"
+
+static u_short port1;
 
 class AAL_CP : public ACE_Event_Handler, public ACE_SOCK_Dgram
 {
@@ -25,10 +21,14 @@ public:
 
   virtual int handle_timeout (const ACE_Time_Value & tv, 
 			      const void *arg = 0);
+
+  int timeouts_;
+  // Number of timeouts thus far.
 };
 
 AAL_CP::AAL_CP (const ACE_INET_Addr &local_addr)
-  : ACE_SOCK_Dgram (local_addr)
+  : ACE_SOCK_Dgram (local_addr),
+    timeouts_ (0)
 {
 }
 
@@ -44,7 +44,7 @@ AAL_CP::handle_input (ACE_HANDLE)
   char buf[BUFSIZ];
   ACE_INET_Addr from_addr;
 
-  ACE_DEBUG ((LM_DEBUG, "Activity occurred on handle %d!\n",
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) activity occurred on handle %d!\n",
 	      ACE_SOCK_Dgram::get_handle ()));
 
   ssize_t n = ACE_SOCK_Dgram::recv (buf, sizeof buf, from_addr);
@@ -52,7 +52,7 @@ AAL_CP::handle_input (ACE_HANDLE)
   if (n == -1)
     ACE_ERROR ((LM_ERROR, "%p\n", "handle_input"));
   else
-    ACE_DEBUG ((LM_DEBUG, "got buf = %*s\n", n, buf));
+    ACE_DEBUG ((LM_DEBUG, "(%P|%t) buf of size %d = %*s\n", n, n, buf));
   
   return 0;
 }
@@ -60,65 +60,100 @@ AAL_CP::handle_input (ACE_HANDLE)
 int
 AAL_CP::handle_timeout (const ACE_Time_Value &, const void *)
 {
-  ACE_DEBUG ((LM_DEBUG, "timed out for aa1\n"));
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) timed out for aa1\n"));
+  this->timeouts_++;
   return 0;
+}
+
+static int
+run_test (u_short localport, 
+          const char *remotehost,
+          u_short remoteport)
+{
+  AAL_CP aal (localport);
+
+  // Read data from other side.
+  if (ACE_Reactor::instance ()->register_handler 
+      (&aal, ACE_Event_Handler::READ_MASK) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "ACE_Reactor::register_handler"),
+                      -1);
+
+  char buf[BUFSIZ];
+  ACE_OS::strcpy (buf, "Data to transmit");
+  size_t len = ACE_OS::strlen (buf);
+
+  ACE_INET_Addr remote_addr (remoteport,
+                             remotehost);
+  ACE_INET_Addr local_addr (localport);
+
+  if (localport == port1)	
+    {
+      ACE_DEBUG ((LM_DEBUG, "(%P|%t) sending data\n"));
+
+      for (size_t i = 0; i < 20; i++)
+        {
+          aal.send (buf, len, remote_addr);
+          ACE_DEBUG ((LM_DEBUG, "(%P|%t) .\n"));
+          ACE_OS::sleep (1);
+        }
+    }
+
+  for (int i = 0; i < 40; i++)
+    {
+      // Wait up to 4 seconds for data.
+      ACE_Time_Value tv (4, 0);
+
+      if (ACE_Reactor::instance ()->handle_events (tv) <= 0)
+        ACE_ERROR_RETURN ((LM_DEBUG,
+                           "(%P|%t) %p\n",
+                           "handle_events"),
+                          -1);
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "(%P|%t) return from handle events\n"));
+
+      aal.send (buf, len, remote_addr);
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "(%P|%t) .\n"));
+    }
+
+  if (ACE_Reactor::instance ()->remove_handler
+      (&aal, ACE_Event_Handler::READ_MASK) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "ACE_Reactor::remove_handler"),
+                      -1);
+
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) exiting\n"));
 }
 
 int
 main (int argc, char *argv[])
 {
-  // Estabish call backs, and socket names.
-  if (argc != 4)
-    ACE_ERROR_RETURN ((LM_ERROR, 
-		       "usage: %s localport remotehost remoteport\n", 
-		       argv[0]), -1);
+  // Estabish call backs and socket names.
 
-  ACE_Reactor reactor;
-  char buf[BUFSIZ];
-  u_short localport  = ACE_OS::atoi (argv[1]);
-  u_short remoteport = ACE_OS::atoi (argv[3]);
-  char *remotehost = argv[2];
+  port1 = argc > 1 ? ACE_OS::atoi (argv[1]) : ACE_DEFAULT_SERVER_PORT;
+  const char *remotehost = argc > 2 ? argv[2] : ACE_DEFAULT_SERVER_HOST;
+  const u_short port2 = argc > 3 ? ACE_OS::atoi (argv[3]) : port1 + 1;
 
-  ACE_INET_Addr remote_addr (remoteport, remotehost);
-  ACE_INET_Addr local_addr (localport);
+  ACE_DEBUG ((LM_DEBUG,
+              "(%P|%t) local port = %d, remote host = %s, remote port = %d\n",
+              port1, remotehost, port2));
 
-  AAL_CP aal (local_addr);
-
-  if (localport == 10000)	// HACK
-   {
-    ACE_OS::memcpy (buf, "Data to transmit", sizeof buf);
-    ACE_DEBUG ((LM_DEBUG, "sending data\n"));
-
-    for (size_t i = 0; i < 20; i++)
-      {
-	aal.send (buf, sizeof buf, remote_addr);
-	ACE_DEBUG ((LM_DEBUG, ".\n"));
-	ACE_OS::sleep (1);
-      }
-  }
-
-  // Read data from other side.
-  if (reactor.register_handler (&aal, ACE_Event_Handler::READ_MASK) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "ACE_Reactor::register_handler"), -1);
-
-  if (reactor.schedule_timer (&aal, 0, 
-			      ACE_Time_Value (1, 0), 
-			      ACE_Time_Value (0, 3500000)) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "ACE_Reactor::schedule_timer"), -1);
-
-  ACE_OS::memcpy (buf, "Data to transmit", sizeof buf);
-
-  for (;;)
+  switch (ACE_OS::fork (argv[0]))
     {
-      // Wait at most two seconds.
-      ACE_Time_Value tv (2, 0);
+    case -1: 
+      return -1;
 
-      reactor.handle_events (tv);
+    case 0:
+      run_test (port1, remotehost, port2);
+      break;
 
-      ACE_DEBUG ((LM_DEBUG, "return from handle events\n"));
-      aal.send (buf, sizeof buf, remote_addr);
-      ACE_DEBUG ((LM_DEBUG, ".\n"));
+    default:
+      run_test (port2, remotehost, port1);
+      break;
     }
 
-  ACE_NOTREACHED (return 0);
+  return 0;
 }
