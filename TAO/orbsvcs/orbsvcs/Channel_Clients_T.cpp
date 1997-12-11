559a1fc88bfb1b6a55b3c9a5119146d05826f4cd
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-12-11 02:45:11 +0000

    ChangeLogTag:Wed Dec 10 20:13:57 1997  Carlos O'Ryan  <coryan@macarena.cs.wustl.edu>

---
+++ TAO/orbsvcs/orbsvcs/Channel_Clients_T.cpp
@@ -0,0 +1,12 @@
+// $Id$
+
+#ifndef ACE_CHANNEL_CLIENTS_T_C
+#define ACE_CHANNEL_CLIENTS_T_C
+
+#include "orbsvcs/Channel_Clients_T.h"
+
+#if !defined (__ACE_INLINE__)
+#include "orbsvcs/Channel_Clients_T.i"
+#endif /* __ACE_INLINE__ */
+
+#endif /* ACE_CHANNEL_CLIENTS_T_C */
