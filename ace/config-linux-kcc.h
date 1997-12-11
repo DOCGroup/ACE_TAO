5fdc40c6cce7585d99dc96cbf2d950ce640474bb
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-12-11 22:39:18 +0000

    added linuxthreads support with the linux KAI C++ config

--- ace/config-linux-kcc.h
+++ ace/config-linux-kcc.h
@@ -11,9 +11,6 @@
 #define __ACE_INLINE__
 #endif /* ! __ACE_INLINE__ */
 
-// Do we really need this #define here?
-#define LINUX
-
 #if defined (i386)
   // If running an Intel, assume that it's a Pentium so that
   // ACE_OS::gethrtime () can use the RDTSC instruction.  If
@@ -89,6 +86,18 @@
 // Compiler/platform defines the sig_atomic_t typedef.
 #define ACE_HAS_SIG_ATOMIC_T
 
+// Platform supports SVR4 extended signals.
+#include /**/ <linux/version.h>
+#if LINUX_VERSION_CODE >= 131396
+// Linux 2.1.68 and above
+#define ACE_HAS_SIGINFO_T
+#define ACE_LACKS_SIGINFO_H
+#define ACE_LACKS_UCONTEXT_H
+//#define ACE_HAS_UCONTEXT_T
+//Platform has si_addr, but its not supported currently
+#define ACE_LACKS_SI_ADDR
+#endif
+
 // Compiler/platform supports sys_siglist array.
 // #define ACE_HAS_SYS_SIGLIST
 
@@ -105,6 +114,10 @@
 #define ACE_PAGE_SIZE 4096
 
 #define ACE_HAS_SUNOS4_GETTIMEOFDAY
+
+// ONLY define this if you have config'd multicast into a 2.x kernel.
+// If you do anything else, we've never tested it!
+#define ACE_HAS_IP_MULTICAST
 
 // Turns off the tracing feature.
 #if !defined (ACE_NTRACE)
@@ -131,12 +144,44 @@
 //#define ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES
 
 // Define this if you want to use the standard C++ library
-#if defined (__KCC)
 #define ACE_HAS_STANDARD_CPP_LIBRARY 1
 #define ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB 1
 #define ACE_LACKS_IOSTREAM_FX 1
 #define ACE_LACKS_LINEBUFFERED_STREAMBUF 1
 #define ACE_LACKS_UNBUFFERED_STREAMBUF 1
-#endif /* __KCC */
+#define ACE_HAS_EXCEPTIONS 1
+
+#define ACE_HAS_NONSTATIC_OBJECT_MANAGER 1
+
+//------------------------------------------------------------------------
+// Yes, we do have threads.
+#define ACE_HAS_THREADS
+// And they're even POSIX pthreads (MIT implementation)
+#define ACE_HAS_PTHREADS
+
+#define ACE_HAS_PTHREAD_SIGMASK			// JCEJ 12/19/96
+
+#if !defined (ACE_MT_SAFE)
+	#define ACE_MT_SAFE 1				// JCEJ 12/22/96	#1
+#endif
+#define ACE_HAS_THREAD_SPECIFIC_STORAGE		// jcej 12/22/96	#2
+#define PTHREAD_MIN_PRIORITY		0	// JCEJ 12/22/96	#3
+#if defined(ACE_HAS_PTHREAD_SIGMASK)
+#  define PTHREAD_MAX_PRIORITY		99	// CJC  02/11/97
+#else
+#  define PTHREAD_MAX_PRIORITY          32      // JCEJ 12/22/96	#3
+#endif
+#define	ACE_HAS_PTHREADS_1003_DOT_1C		// JCEJ 12/22/96	#4
+
+#define ACE_LACKS_THREAD_STACK_ADDR		// JCEJ 12/17/96
+#define ACE_LACKS_THREAD_STACK_SIZE		// JCEJ 12/17/96
+
+#define	ACE_LACKS_RWLOCK_T			// JCEJ 12/23/96	#1
+#define ACE_HAS_RECURSIVE_THR_EXIT_SEMANTICS	// JCEJ 1/7-8/96
+
+#define ACE_HAS_SIGWAIT
+#define ACE_LACKS_CONDATTR_PSHARED
+
+#include /**/ <pthread.h>
 
 #endif /* ACE_CONFIG_H */
