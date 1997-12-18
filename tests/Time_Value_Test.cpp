8cd89e19d91e186a4fbf15fadcaa637d31239f82
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1997-12-18 17:09:53 +0000

    fixed #ifdef's so that ACE_ULongLong isn't tests on SGIs, etc.

--- tests/Time_Value_Test.cpp
+++ tests/Time_Value_Test.cpp
@@ -22,9 +22,9 @@
 // Note, for this test the config.h file *must* come first!
 #include "ace/inc_user_config.h"
 
-// Force test of ACE_U_LongLong class on all platforms except
-// ACE_WIN32 and with ACE_SIZEOF_LONG == 8.  ACE_U_LongLong isn't
-// used on those platforms.
+// Force test of ACE_U_LongLong class on some Unix platforms.
+// ACE_U_LongLong isn't used on those platforms on WIN32 or
+// with ACE_SIZEOF_LONG == 8.
 #if defined (ACE_HAS_HI_RES_TIMER) || \
    (defined (ACE_HAS_LONGLONG_T) && (ACE_SIZEOF_LONG == 4))
 
@@ -50,7 +50,8 @@
 #include "test_config.h"
 #include "ace/ACE.h"
 
-#if !defined (ACE_WIN32)  &&  (ACE_SIZEOF_LONG == 4)
+#if defined (ACE_HAS_HI_RES_TIMER) || \
+   (defined (ACE_HAS_LONGLONG_T) && (ACE_SIZEOF_LONG == 4))
 
 static
 u_long
@@ -106,7 +107,8 @@ test_ace_u_longlong (void)
 
   return errors;
 }
-#endif /* ! ACE_WIN32 && ACE_SIZEOF_LONG == 4 */
+#endif /* ACE_HAS_HI_RES_TIMER || (ACE_HAS_LONGLONG_T && ACE_SIZEOF_LONG == 4) */
+
 
 int
 main (int, char *[])
@@ -135,10 +137,11 @@ main (int, char *[])
   ACE_ASSERT (tv6 == tv1);
   ACE_ASSERT (tv5 == tv7);
 
-#if !defined (ACE_WIN32) && (ACE_SIZEOF_LONG == 4)
+#if defined (ACE_HAS_HI_RES_TIMER) || \
+   (defined (ACE_HAS_LONGLONG_T) && (ACE_SIZEOF_LONG == 4))
   if (test_ace_u_longlong () != 0)
     ++ret;
-#endif /* ! ACE_WIN32 && ACE_SIZEOF_LONG == 4 */
+#endif /* ACE_HAS_HI_RES_TIMER || (ACE_HAS_LONGLONG_T && ACE_SIZEOF_LONG == 4) */
 
   ACE_END_TEST;
   return ret;
