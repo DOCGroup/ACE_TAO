8ff3062b1267c9cc434afeef13af74c0f57f6d3e
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-02 19:08:45 +0000

    added protection against multiple inclusion

--- ace/Env_Value_T.cpp
+++ ace/Env_Value_T.cpp
@@ -1,10 +1,10 @@
 // $Id$
 
+#if !defined (ACE_ENV_VALUE_T_C)
+#define ACE_ENV_VALUE_T_C
+
 #if ! defined (__ACE_INLINE__)
 #include "ace/Env_Value_T.i"
 #endif /* __ACE_INLINE__ */
 
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
+#endif /* ACE_ENV_VALUE_T_C */
