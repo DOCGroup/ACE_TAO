4be77e8cfaffe185946b33b51cbeac280e908ec7
nw1 <nw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-02 15:56:31 +0000

    Added explicit template instantiation for ACE_Map_Iterator_Base.

--- trunk/ace/Token_Collection.cpp
+++ trunk/ace/Token_Collection.cpp
@@ -286,10 +286,12 @@ ACE_Token_Collection::dump (void) const
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 template class ACE_Map_Manager<ACE_Token_Name, ACE_Token_Proxy *, ACE_Null_Mutex>;
+template class ACE_Map_Iterator_Base<ACE_Token_Name, ACE_Token_Proxy *, ACE_Null_Mutex>;
 template class ACE_Map_Iterator<ACE_Token_Name, ACE_Token_Proxy *, ACE_Null_Mutex>;
 template class ACE_Map_Entry<ACE_Token_Name, ACE_Token_Proxy *>;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate ACE_Map_Manager<ACE_Token_Name, ACE_Token_Proxy *, ACE_Null_Mutex>
+#pragma instantiate ACE_Map_Iterator_Base<ACE_Token_Name, ACE_Token_Proxy *, ACE_Null_Mutex>
 #pragma instantiate ACE_Map_Iterator<ACE_Token_Name, ACE_Token_Proxy *, ACE_Null_Mutex>
 #pragma instantiate ACE_Map_Entry<ACE_Token_Name, ACE_Token_Proxy *>
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
