cb1c5522ab12ca9f00ba9e4b4a9295b8dc3967db
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-01-09 22:39:48 +0000

    *** empty log message ***

--- examples/Service_Configurator/IPC-tests/server/server_test.cpp
+++ examples/Service_Configurator/IPC-tests/server/server_test.cpp
@@ -7,8 +7,11 @@
 int
 main (int argc, char *argv[])
 {
-  if (ACE_Service_Test::open (argc, argv) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "server_test.open"), -1);
+  if (ACE_Service_Config::open (argc, argv) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "ACE_Service_Config::open"),
+                      -1);
 
   // Create an adapter to end the event loop.
   ACE_Sig_Adapter sa ((ACE_Sig_Handler_Ex) ACE_Reactor::end_event_loop);
@@ -20,7 +23,9 @@ main (int argc, char *argv[])
   // Register ourselves to receive signals so we can shut down
   // gracefully.
   if (ACE_Reactor::instance ()->register_handler (sig_set, &sa) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n"), -1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n"),
+                      -1);
 
   // Run forever, performing the configured services until we are shut
   // down by a SIGINT/SIGQUIT signal.
