2d549a5573767be28b1000497b588de14f1cae33
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-10 03:49:19 +0000

    ChangeLogTag:Fri Jan 09 21:37:39 1998    <coryan@MILONGA>

--- TAO/orbsvcs/Naming_Service/CosNaming_i.cpp
+++ TAO/orbsvcs/Naming_Service/CosNaming_i.cpp
@@ -99,7 +99,7 @@ NS_NamingContext::bind (const CosNaming::Name& n,
   if (len == 0) 
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::InvalidName);
+      IT_env.exception (new CosNaming::NamingContext::InvalidName);
       return;
     }
 
@@ -125,7 +125,7 @@ NS_NamingContext::bind (const CosNaming::Name& n,
       if (context_.bind (name, entry) == -1)
 	{
 	  IT_env.clear ();
-	  IT_env.exception (new POA_CosNaming::NamingContext::AlreadyBound);
+	  IT_env.exception (new CosNaming::NamingContext::AlreadyBound);
 	  return;
 	}
         /**///	throw CosNaming::NamingContext::AlreadyBound ();
@@ -188,7 +188,7 @@ NS_NamingContext::bind_context (const CosNaming::Name &n,
   if (len == 0)
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::InvalidName);
+      IT_env.exception (new CosNaming::NamingContext::InvalidName);
       return;
     }
 
@@ -215,7 +215,7 @@ NS_NamingContext::bind_context (const CosNaming::Name &n,
       if (context_.bind (name, entry) == 1)
 	{
 	  IT_env.clear ();
-	  IT_env.exception (new POA_CosNaming::NamingContext::AlreadyBound);
+	  IT_env.exception (new CosNaming::NamingContext::AlreadyBound);
 	  return;
 	}
 
@@ -235,7 +235,7 @@ NS_NamingContext::rebind_context (const CosNaming::Name &n,
   if (len == 0)
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::InvalidName);
+      IT_env.exception (new CosNaming::NamingContext::InvalidName);
       return;
     }
 
@@ -277,7 +277,7 @@ NS_NamingContext::resolve (const CosNaming::Name& n,
   if (len == 0)
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::InvalidName);
+      IT_env.exception (new CosNaming::NamingContext::InvalidName);
       return 0;
     }
   
@@ -287,7 +287,7 @@ NS_NamingContext::resolve (const CosNaming::Name& n,
   if (context_.find (name, entry) == -1) 
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::NotFound (CosNaming::NamingContext::not_object, n));
+      IT_env.exception (new CosNaming::NamingContext::NotFound (CosNaming::NamingContext::not_object, n));
       return 0;
     }
   
@@ -340,7 +340,7 @@ NS_NamingContext::unbind (const CosNaming::Name& n,
   if (len == 0)
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::InvalidName);
+      IT_env.exception (new CosNaming::NamingContext::InvalidName);
       return;
     }
 
@@ -364,7 +364,7 @@ NS_NamingContext::unbind (const CosNaming::Name& n,
       if (context_.unbind (name) == -1)
 	{
 	  IT_env.clear ();
-	  IT_env.exception (new POA_CosNaming::NamingContext::NotFound (CosNaming::NamingContext::not_object, n));
+	  IT_env.exception (new CosNaming::NamingContext::NotFound (CosNaming::NamingContext::not_object, n));
 	  return;
 	}
     }
@@ -393,7 +393,7 @@ NS_NamingContext::bind_new_context (const CosNaming::Name& n,
   if (IT_env.exception () != 0)
     {
       CORBA::release (c);
-      return POA_CosNaming::NamingContext::_nil ();
+      return CosNaming::NamingContext::_nil ();
     }
   
   return NS_NamingContext::_duplicate (c);
@@ -405,7 +405,7 @@ NS_NamingContext::destroy (CORBA::Environment &IT_env)
   if (context_.current_size () != 0)
     {
       IT_env.clear ();
-      IT_env.exception (new POA_CosNaming::NamingContext::NotEmpty);
+      IT_env.exception (new CosNaming::NamingContext::NotEmpty);
       return;
     }
 
