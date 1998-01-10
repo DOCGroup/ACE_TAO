de5fd4c4beab07549a22f679cc7417db96ecd9ca
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-01-10 03:39:49 +0000

    *** empty log message ***

--- TAO/tests/Cubit/TAO/MT_Cubit/Util_Thread.h
+++ TAO/tests/Cubit/TAO/MT_Cubit/Util_Thread.h
@@ -9,37 +9,37 @@
 //    Util_Thread.h
 //
 // = AUTHOR
-//    Andy Gokhale, Sumedh Mungee and Sergio Flores-Gaitan
+//    Andy Gokhale, Sumedh Mungee, and Sergio Flores-Gaitan
 //
 // ============================================================================
 
 #include "Task_Client.h"
 
-#define CUBIT_ARBIT_NUMBER 2064885
+const int CUBIT_ARBIT_NUMBER = 2064885;
 
 class Util_Thread : public ACE_Task<ACE_MT_SYNCH>
   // = TITLE
-  //     The thread that computes utilization
+  //     The thread that computes utilization.
 {
 public:
-  Util_Thread (Task_State *, ACE_Thread_Manager *thr_mgr);
-  // constructor
+  Util_Thread (Task_State *,
+               ACE_Thread_Manager *thr_mgr);
+  // constructor.
 
   virtual int svc (void);
-  // the thread function
+  // The thread entry point function.
 
   double get_number_of_computations (void);
-  // gets the number of computations the thread was able
-  // to perform
+  // Gets the number of computations the thread was able to perform.
 
   int done_;
-  // Indicates whether its done.
+  // Indicates whether we're done.
 
 private:
+  int run_computations (void);
+  // Run the computations.
 
-  int run_computations ();
-  // run the computations
-
+  // Sumedh, please add comments here.
   double number_of_computations_;
   Task_State *ts_;
 };
