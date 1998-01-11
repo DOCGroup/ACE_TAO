77b5ee4d58a04dafe883a664b419c9def897f9f6
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-01-11 22:18:23 +0000

    ACE version 4.4.18, released Sun Jan 11 16:18:15 1998.

--- ace/Version.h
+++ ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (4u)
-#define ACE_BETA_VERSION (17u)
-#define ACE_VERSION "4.4.17\0"
+#define ACE_BETA_VERSION (18u)
+#define ACE_VERSION "4.4.18\0"
