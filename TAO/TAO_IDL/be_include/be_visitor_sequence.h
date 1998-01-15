784929943455b515b608d8fb242641908399301f
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-15 00:00:40 +0000

    ChangeLogTag:Wed Jan 14 17:54:31 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/TAO_IDL/be_include/be_visitor_sequence.h
+++ TAO/TAO_IDL/be_include/be_visitor_sequence.h
@@ -15,7 +15,8 @@ class be_visitor_sequence_ch : public be_visitor
   //    be_visitor_sequence_ch
   //
   // = DESCRIPTION
-  //   This is a concrete visitor to generate the client header for sequences
+  //   This is a concrete visitor to generate the client header for
+  //   sequences
   //
 public:
   be_visitor_sequence_ch (void);
@@ -38,42 +39,37 @@ class be_visitor_sequence_base_ch : public be_visitor
   //    be_visitor_sequence_base_ch
   //
   // = DESCRIPTION
-  //   This is a concrete visitor to generate the client header for sequence
-  //   base
+  //   This is a concrete visitor to generate the template parameters
+  //   for a TAO_*_Sequence instantiation.
+  //
+  // = BUGS
+  //   The class name is misleading, eventually this class could be
+  //   used to generate code in other files, not only the client
+  //   header. 
+  //   The visitor factory should provide a factory method that builds
+  //   this class on the fly.
   //
 public:
-  be_visitor_sequence_base_ch (void);
-  // constructor
+  be_visitor_sequence_base_ch (TAO_OutStream *stream,
+			       be_decl *sequence_scope,
+			       be_type *base_type);
+  // Constructor, set the stream to write to, the scope where the
+  // sequence is defined and the base type for the sequence.
 
   ~be_visitor_sequence_base_ch (void);
   // destructor
 
+  // = Visitor methods.
   virtual int visit_predefined_type (be_predefined_type *node);
-  // base type is predefined type
-
   virtual int visit_interface (be_interface *node);
-  // base type is object reference
-
   virtual int visit_interface_fwd (be_interface_fwd *node);
-  // base type is object reference
-
   virtual int visit_structure (be_structure *node);
-  // base type is a structure
-
   virtual int visit_enum (be_enum *node);
-  // base type is an enum
-
   virtual int visit_union (be_union *node);
-  // base type is a union
-
   virtual int visit_array (be_array *node);
-  // base type is an array
-
+  virtual int visit_string (be_string *node);
   virtual int visit_sequence (be_sequence *node);
-  // base type is a sequence
-
   virtual int visit_typedef (be_typedef *node);
-  // base type is a typedef to something
 
 protected:
   be_decl *seq_scope (void);
@@ -81,10 +77,14 @@ protected:
 
   int visit_node (be_type *);
   // helper that does the common job
-};
 
-typedef ACE_Singleton <be_visitor_sequence_base_ch, ACE_SYNCH_RECURSIVE_MUTEX>
-TAO_BE_VISITOR_SEQUENCE_BASE_CH;
+private:
+  be_type* current_type_;
+  // The scoped name for the current argument type name.
+  // We cannot use just node->name() because we could be using an
+  // aliased name (through typedefs) in which case the real name must
+  // be used.
+};
 
 class be_visitor_sequence_elemtype : public be_visitor
 {
@@ -93,47 +93,33 @@ class be_visitor_sequence_elemtype : public be_visitor
   //    be_visitor_sequence_elemtype
   //
   // = DESCRIPTION
-  //   This is a concrete visitor to generate the return type for the [] operators
+  //   This is a concrete visitor to generate the return type for the
+  //   [] operators
   //
   // = NOTES
   //    This class may eventually go away when the _var and _out classes are
   //    generated using templates
 public:
-  be_visitor_sequence_elemtype (void);
-  // constructor
+  be_visitor_sequence_elemtype (TAO_OutStream *stream,
+				be_decl *sequence_scope,
+				be_type *base_type);
+  // Constructor, set the stream to write to, the scope where the
+  // sequence is defined and the base type for the sequence.
 
   ~be_visitor_sequence_elemtype (void);
   // destructor
 
+  // = Visitors
   virtual int visit_predefined_type (be_predefined_type *node);
-  // base type is predefined type
-
   virtual int visit_interface (be_interface *node);
-  // base type is object reference
-
   virtual int visit_interface_fwd (be_interface_fwd *node);
-  // base type is object reference
-
   virtual int visit_structure (be_structure *node);
-  // base type is a structure
-
   virtual int visit_enum (be_enum *node);
-  // base type is an enum
-
   virtual int visit_union (be_union *node);
-  // base type is a union
-
   virtual int visit_array (be_array *node);
-  // base type is an array
-
   virtual int visit_sequence (be_sequence *node);
-  // base type is a sequence
-
   virtual int visit_typedef (be_typedef *node);
-  // base type is a typedef to something
-
   virtual int visit_string (be_string *node);
-  // base type is a string
 
 protected:
   be_decl *seq_scope (void);
@@ -141,10 +127,12 @@ protected:
 
   int visit_node (be_type *);
   // helper that does the common job
+
+  be_type* current_type_;
+  // The scoped name for the current argument type name.
+  // We cannot use just node->name() because we could be using an
+  // aliased name (through typedefs) in which case the real name must
+  // be used.
 };
-
-typedef ACE_Singleton <be_visitor_sequence_elemtype, ACE_SYNCH_RECURSIVE_MUTEX>
-TAO_BE_VISITOR_SEQUENCE_ELEMTYPE;
-
 
 #endif /* TAO_BE_VISITOR_SEQUENCE_H */
