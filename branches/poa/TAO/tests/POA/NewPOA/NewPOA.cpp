c7cec933e820f1a490b01345888d552b13d126ab
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-19 17:20:14 +0000

    *** empty log message ***

---
+++ branches/poa/TAO/tests/POA/NewPOA/NewPOA.cpp
@@ -0,0 +1,79 @@
+#include "ace/streams.h"
+#include "tao/corba.h"
+
+int 
+main (int argc, char **argv)
+{
+  CORBA::Environment env;
+
+  CORBA::ORB_ptr orb = CORBA::ORB_init (argc, argv, 0, env);
+  if (env.exception () != 0)
+    {
+      env.print_exception ("CORBA::ORB_init");
+      return -1;
+    }
+
+  CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
+
+  PortableServer::POA_var rootPOA = PortableServer::POA::_narrow (obj, env);
+  if (env.exception () != 0)
+    {
+      env.print_exception ("PortableServer::POA::_narrow");
+      return -1;
+    }
+  
+  // CORBA::PolicyList policies (2);
+  PortableServer::PolicyList policies (2);
+
+  policies[0] = rootPOA->create_thread_policy (PortableServer::ORB_CTRL_MODEL, env);
+  policies[1] = rootPOA->create_lifespan_policy (PortableServer::TRANSIENT, env);
+  policies.length (2);  
+  
+  PortableServer::POA_ptr childPOA = rootPOA->create_POA ("my_little_poa",
+                                                          PortableServer::POAManager::_nil(), 
+                                                          policies,
+                                                          env);  
+  if (env.exception () != 0)
+    {
+      env.print_exception ("PortableServer::POA::create_POA");
+      return -1;
+    }
+
+  for (int i = 0; i < policies.length () && env.exception == 0; i++)
+    {
+      // const CORBA::Policy_ptr policy = policies[i];
+      const PortableServer::Policy_ptr policy = policies[i];
+      policy->destroy (env);
+    }  
+  if (env.exception () != 0)
+    {
+      env.print_exception ("PortableServer::POA::create_POA");
+      return -1;
+    }
+
+  CORBA::String_var rootPOA_name = rootPOA->the_name (env);
+  if (env.exception () != 0)
+    {
+      env.print_exception ("PortableServer::POA::_narrow");
+      return -1;
+    }
+  
+  CORBA::String_var childPOA_name = childPOA->the_name (env);
+  if (env.exception () != 0)
+    {
+      env.print_exception ("PortableServer::POA::_narrow");
+      return -1;
+    }
+  
+  char *name = 0;
+
+  name = rootPOA_name;
+  cout << name << endl;
+
+  name = childPOA_name;
+  cout << name << endl;
+
+  CORBA::release (orb);
+
+  return 0;
+}
