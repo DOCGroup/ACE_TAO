30ac775a072b1065edfe4def03b58e1c46974bce
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-21 03:15:11 +0000

    ACE version 4.4.20, released Tue Jan 20 21:15:01 1998.

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (4u)
-#define ACE_BETA_VERSION (19u)
-#define ACE_VERSION "4.4.19\0"
+#define ACE_BETA_VERSION (20u)
+#define ACE_VERSION "4.4.20\0"
