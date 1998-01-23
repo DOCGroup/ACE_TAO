fe5fb19c3d912aab107f7af2c21aab5c07af0488
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-23 23:16:39 +0000

    This commit was manufactured by cvs2svn to create tag 'poa_end'.

---
+++ tags/poa_end/TAO/Benchmark/Marshal_Test/Orbix/server.cpp
@@ -0,0 +1,12 @@
+#include "benchmark/driver.h"
+#include "orbix_marshal_server.h"
+
+int main (int argc, char *argv [])
+{
+  // instantiate a DRIVER with the Orbix_Client_Proxy as the template
+  CORBA_Benchmark_Driver<Orbix_Marshal_Server_Proxy>  driver (new
+							     Orbix_Marshal_Server_Proxy ());
+
+  // let the driver do everything for us so that we remain very simple
+  return driver.run (argc, argv);
+}
