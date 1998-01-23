65c2aa0fe261fdbb08027e96fe4f7d2a225edd27
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-23 22:55:58 +0000

    This commit was manufactured by cvs2svn to create branch 'poa'.

---
+++ TAO/orbsvcs/orbsvcs/AVStreams_i.h
@@ -0,0 +1,25 @@
+/* -*- C++ -*- */
+
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    cos
+// 
+// = FILENAME
+//   AVStreams_i.h
+//
+// = AUTHOR
+//    Sumedh Mungee <sumedh@cs.wustl.edu>
+//    
+// 
+// ============================================================================
+
+#if !defined (AVSTREAMS_I_H)
+#define	AVSTREAMS_I_H
+
+#include "orbsvcs/AVStreamsS.h"
+  
+
+#endif /* AVSTREAMS_I_H */
