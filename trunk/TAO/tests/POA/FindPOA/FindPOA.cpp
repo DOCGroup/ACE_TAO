5222075ca250992e60f5e084b00b8dc4b7597d99
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-26 22:37:39 +0000

    Added Explanatory comments to the code

--- trunk/TAO/tests/POA/FindPOA/FindPOA.cpp
+++ trunk/TAO/tests/POA/FindPOA/FindPOA.cpp
@@ -1,12 +1,27 @@
-// $Id$ 
+// $Id$
+//
+// = LIBRARY
+//     TAO/tests/POA/FindPOA
+//
+// = FILENAME
+//     FindPOA.cpp
+//
+// = DESCRIPTION
+//     This Program tests the find_POA method of a POA.
+// = AUTHOR
+//     Irfan Pyarali
+//
+//==================================================================================
 
 #include "ace/streams.h"
 #include "tao/corba.h"
 
-int 
+int
 main (int argc, char **argv)
 {
   CORBA::Environment env;
+
+  // Initialize the ORB
 
   CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0, env);
   if (env.exception () != 0)
@@ -15,7 +30,11 @@ main (int argc, char **argv)
       return -1;
     }
 
+  // Get Object reference to RootPOA
+
   CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
+
+  // Narrow Object reference to RootPOA to a POA reference
 
   PortableServer::POA_var root_poa = PortableServer::POA::_narrow (obj.in(), env);
   if (env.exception () != 0)
@@ -23,7 +42,9 @@ main (int argc, char **argv)
       env.print_exception ("PortableServer::POA::_narrow");
       return -1;
     }
-  
+
+  // Get a TAO_Adapter_Activator reference
+
   TAO_Adapter_Activator activator_impl;
   PortableServer::AdapterActivator_var activator = activator_impl._this (env);
   if (env.exception () != 0)
@@ -31,34 +52,45 @@ main (int argc, char **argv)
       env.print_exception ("TAO_Adapter_Activator::_this");
       return -1;
     }
-  
+
+  // Register the TAO_Adapter_Activator reference to be the RootPOA's Adapter Activator
+
   root_poa->the_activator (activator.in (), env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::the_activator");
       return -1;
     }
-  
+
+  // Try to find a childPOA of RootPOA named firstPOA
+
   ACE_CString name = "firstPOA";
   PortableServer::POA_var first_poa = root_poa->find_POA (name.c_str (),
                                                           CORBA::B_TRUE,
-                                                          env);  
+                                                          env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::find_POA");
       return -1;
     }
 
+  // Use the TAO_POA name_separator (which is '/') to find a childPOA of
+  // firstPOA named secondPOA
+
   name += TAO_POA::name_separator ();
   name += "secondPOA";
   PortableServer::POA_var second_poa = root_poa->find_POA (name.c_str (),
                                                            CORBA::B_TRUE,
-                                                           env);  
+                                                           env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::find_POA");
       return -1;
     }
+
+  // Create a hierarchical string of POA names eg. thirdPOA/forthPOA/fifthPOA
+  // thirdPOA being the root of the hierarchy with forthPOA as its child
+  // and fifthPOA as its grandchild.
 
   name = "thirdPOA";
   name += TAO_POA::name_separator ();
@@ -66,14 +98,20 @@ main (int argc, char **argv)
   name += TAO_POA::name_separator ();
   name += "fifthPOA";
 
+  // Try to find the fifth_poa by passing the hierarchy of POA names
+  // resulting in the creation of third and forth POAs as well as the
+  // fifth POA.
+
   PortableServer::POA_var fifth_poa = root_poa->find_POA (name.c_str (),
                                                           CORBA::B_TRUE,
-                                                          env);  
+                                                          env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::find_POA");
       return -1;
     }
+
+  // Get the names of all the POAs
 
   CORBA::String_var root_poa_name = root_poa->the_name (env);
   if (env.exception () != 0)
@@ -81,36 +119,37 @@ main (int argc, char **argv)
       env.print_exception ("PortableServer::POA::_narrow");
       return -1;
     }
-  
+
   CORBA::String_var first_poa_name = first_poa->the_name (env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::_narrow");
       return -1;
     }
-  
+
   CORBA::String_var second_poa_name = second_poa->the_name (env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::_narrow");
       return -1;
     }
-  
+
   CORBA::String_var fifth_poa_name = fifth_poa->the_name (env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::_narrow");
       return -1;
     }
-  
+
   cout << root_poa_name.in () << endl;
   cout << first_poa_name.in () << endl;
   cout << second_poa_name.in () << endl;
   cout << fifth_poa_name.in () << endl;
 
   // This should destroy all its children
-  root_poa->destroy (CORBA::B_TRUE, 
-                     CORBA::B_TRUE, 
+
+  root_poa->destroy (CORBA::B_TRUE,
+                     CORBA::B_TRUE,
                      env);
   if (env.exception () != 0)
     {
