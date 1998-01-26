e0162fe8e5ccc6518713ad63105069ad6840b74c
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-26 20:44:25 +0000

    Added Comments

--- trunk/TAO/tests/POA/RootPOA/RootPOA.cpp
+++ trunk/TAO/tests/POA/RootPOA/RootPOA.cpp
@@ -1,3 +1,22 @@
+//==================================================================================
+//
+// = LIBRARY
+//     TAO/tests/POA/RootPOA
+//
+//
+// = FILENAME
+//     RootPOA.cpp
+// 
+// = DESCRIPTION
+//     This program gets the name of the  Root POA and prints it out on 
+//     the standard output. 
+// 
+// = AUTHOR
+//     Irfan Pyarali 
+//
+//==================================================================================
+
+
 #include "ace/streams.h"
 #include "tao/corba.h"
 
@@ -6,15 +25,20 @@ main (int argc, char **argv)
 {
   CORBA::Environment env;
 
+  // Initilize the ORB
   CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0, env);
   if (env.exception () != 0)
     {
       env.print_exception ("CORBA::ORB_init");
       return -1;
     }
-
+  
+  // Resolve the initial references for the name RootPOA
+  // thus getting an object of type CORBA::Object
   CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
 
+  //apply _narrow on the object of type CORBA::Object, 
+  // to make it a POA class Object
   PortableServer::POA_var root_poa = PortableServer::POA::_narrow (obj.in(), env);
   if (env.exception () != 0)
     {
@@ -22,6 +46,7 @@ main (int argc, char **argv)
       return -1;
     }
   
+  // Get the name of the RootPOA
   CORBA::String_var poa_name = root_poa->the_name (env);
   if (env.exception () != 0)
     {
@@ -29,9 +54,10 @@ main (int argc, char **argv)
       return -1;
     }
   
-  cout << poa_name.in () << endl;
+  cout << "The RootPOA is : " << poa_name.in () << endl;
 
-  root_poa->destroy (CORBA::B_TRUE, 
+  //destroy the POA object. Also destroys the child POAs if any.
+ root_poa->destroy (CORBA::B_TRUE, 
                      CORBA::B_TRUE, 
                      env);
   if (env.exception () != 0)
