f66a2e78b3f46111019625f7d31d81abd74e4c49
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-27 08:39:43 +0000

    *** empty log message ***

--- TAO/tests/POA/Generic_Servant/MyFooServant.h
+++ TAO/tests/POA/Generic_Servant/MyFooServant.h
@@ -1,3 +1,18 @@
+// $Id$
+//===============================================================================
+//
+//
+// = FILENAME
+//     MyFooServant.h
+//
+// = DESCRIPTION
+//     Defines MyFooServant class for the Foo interface
+//
+// = AUTHOR
+//     Irfan Pyarali
+//
+//==================================================================================
+
 #if !defined (MYFOOSERVANT_H)
 #define MYFOOSERVANT_H
 
@@ -6,16 +21,21 @@
 class MyFooServant : public POA_Foo
 {
 public:
-  MyFooServant (PortableServer::POA_ptr poa, 
+  // constructor - takes a POA and a value parameter
+  MyFooServant (PortableServer::POA_ptr poa,
                 CORBA::Long value);
-  
+
+  // Destructor
   virtual ~MyFooServant (void);
 
+  //Returns the Default POA of this Servant object
   virtual PortableServer::POA_ptr _default_POA (CORBA::Environment &env);
 
+  // Simple doit method
   virtual CORBA::Long doit (CORBA::Environment &env);
 
 protected:
+  // Default poa associated with this servant
   PortableServer::POA_var poa_;
   CORBA::Long value_;
 };
