a919975b1e6ad62c13388bff2489d5e956b695b5
gokhale <gokhale@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-29 16:35:11 +0000

    *** empty log message ***

--- TAO/tao/encode.cpp
+++ TAO/tao/encode.cpp
@@ -409,7 +409,7 @@ TAO_Marshal_Struct::encode (CORBA::TypeCode_ptr tc,
   CORBA::TypeCode::traverse_status retval = CORBA::TypeCode::TRAVERSE_CONTINUE;
   CORBA::Boolean continue_encoding = CORBA::B_TRUE;
   CORBA::TypeCode_ptr param;
-  CORBA::Long size, alignment;
+  CORBA::Long size, alignment, align_offset;
 
   void *start_addr = (void *)data;
 
@@ -431,10 +431,16 @@ TAO_Marshal_Struct::encode (CORBA::TypeCode_ptr tc,
 		  alignment = param->alignment (env);
 		  if (env.exception () == 0)
 		    {
-		      data = (const void *)
-                        ((ptr_arith_t) ptr_align_binary (data, alignment) +
-                        (ptr_arith_t) ptr_align_binary (start_addr, alignment) -
-                        (ptr_arith_t) start_addr);
+                      align_offset =
+                        (ptr_arith_t) ptr_align_binary (data, alignment)
+                        - (ptr_arith_t) data
+                        + (ptr_arith_t) ptr_align_binary (start_addr, alignment)
+                        - (ptr_arith_t) start_addr;
+                      // if both the start_addr and data are not aligned as per
+                      // the alignment, we do not add the offset
+                      data = (const void *) ((ptr_arith_t) data +
+                                             ((align_offset == alignment) ?
+                                              0 : align_offset));
 		      switch (param->kind_)
 			{
 			case CORBA::tk_null:
