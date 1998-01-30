93f97e857002b34ba5145b3c3f3b3bcd7c369ab6
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-30 02:27:22 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-This'.

--- ace/config-sco-5.0.0-fsu-pthread.h
+++
@@ -1,8 +0,0 @@
-#if !defined (ACE_CONFIG_H)
-#define ACE_CONFIG_H
-
-#include "ace/config-gcc-2.7.2.h"
-#include "ace/config-sco-5.0.0.h"
-#include "ace/config-fsu-pthread.h"
-
-#endif /* ACE_CONFIG_H */
