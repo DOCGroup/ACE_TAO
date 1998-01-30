c6d54698a595bacaf9a29399b62caca47242a26d
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-30 16:11:32 +0000

    replace config-gcc-2.7.2.h with config-g++-common.h, which will support all g++ versions, including egcs

---
+++ ace/config-g++-common.h
@@ -0,0 +1,29 @@
+/* -*- C++ -*- */
+// $Id$
+
+// This configuration file is designed to be included by another,
+// specific configuration file.  It provides config information common
+// to all g++ platforms, including egcs.
+
+// It is under construction, so it doesn't have much for non-egcs, yet.
+
+#if !defined (ACE_GNUG_COMMON_H)
+#define ACE_GNUG_COMMON_H
+
+#if __GNUC__ > 2 || ( __GNUC__ == 2 && __GNUC_MINOR__ >= 90)
+  // egcs or really modern gcc
+# define ACE_HAS_ANSI_CASTS
+# define ACE_HAS_CPLUSPLUS_HEADERS
+# define ACE_HAS_STDCPP_STL_INCLUDES
+# define ACE_HAS_TEMPLATE_TYPEDEFS
+# define ACE_HAS_TYPENAME_KEYWORD
+#else  /* ! EGCS */
+  // Plain old g++.
+# define ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES
+#endif /* ! EGCS */
+
+#define ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION
+#define ACE_HAS_TEMPLATE_SPECIALIZATION
+#define ACE_TEMPLATES_REQUIRE_SOURCE
+
+#endif /* ACE_GNUG_COMMON_H */
