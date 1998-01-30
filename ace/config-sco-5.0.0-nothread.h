571ea023c2f27ead5c04629fb798828cc3f385a3
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-01-30 16:12:21 +0000

    migrated to config-g++-common.h

--- ace/config-sco-5.0.0-nothread.h
+++ ace/config-sco-5.0.0-nothread.h
@@ -1,7 +1,9 @@
 #if !defined (ACE_CONFIG_H)
 #define ACE_CONFIG_H
 
-#include "ace/config-gcc-2.7.2.h"
+#include "ace/config-g++-common.h"
 #include "ace/config-sco-5.0.0.h"
+
+#define ACE_HAS_GNU_CSTRING_H
 
 #endif /* ACE_CONFIG_H */
