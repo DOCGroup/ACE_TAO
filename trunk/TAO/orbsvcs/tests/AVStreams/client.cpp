2d5ff36871078ce38be55d4a043bd15a0194e8aa
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-02 01:26:49 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/tests/AVStreams/client.cpp
+++ trunk/TAO/orbsvcs/tests/AVStreams/client.cpp
@@ -6,93 +6,114 @@
 //    TAO/orbsvcs/tests/AVStreams
 //
 // = FILENAME
-//    server.cpp
+//    client.cpp
 //
 // = DESCRIPTION 
-//    Test server for the AVStreams demo
+//    Test client for the AVStreams demo
 //   
 // = AUTHORS
 //   Sumedh Mungee <sumedh@cs.wustl.edu>
 //
 // ============================================================================
 
-#include "orbsvcs/AVStreamsC.h"
-#include "ace/Get_Opt.h"
+#include "client.h"
 
-char *basic_stream_ctrl_key = 0;
+Client::Client (void)
+{
+}
 
 int
-parse_args (int argc, char **argv)
+Client::init (int argc, 
+              char **argv,
+              CORBA::Environment &env)
 {
+
+  // init the ORB
+  manager_.init (argc,
+                 argv,
+                 "avstreams_poa",
+                 env);
+
+  TAO_CHECK_ENV_RETURN (env, 1);
+                                             
   ACE_Get_Opt get_opts (argc, argv, "k:");
   int c;
-
+  char *ior = 0;
+  
   while ((c = get_opts ()) != -1)
     switch (c)
       {
       case 'k':
-	basic_stream_ctrl_key = 
+	ior = 
           ACE_OS::strdup (get_opts.optarg);
 	break;
       case '?':
       default:
         ACE_ERROR_RETURN ((LM_ERROR,
                            "usage:  %s"
-                           " [-k basic_stream_ctrl_key]"
+                           " [-k mmdevice_key]"
                            "\n",
                            argv [0]),
                           -1);
       }
+  if (ior == 0)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage:  %s"
+                           " [-k mmdevice_key]"
+                           "\n",
+                           argv [0]),
+                          -1);
+  
+  CORBA::Object_var mmdevice_object = 
+    this->manager_.orb()->string_to_object (ior,
+                                            env);
+  TAO_CHECK_ENV_RETURN (env, 1);
 
-  // Indicates successful parsing of command line.
+  this->remote_mmdevice_ = 
+    AVStreams::MMDevice::_narrow (mmdevice_object.in (),
+                                  env);
+  TAO_CHECK_ENV_RETURN (env, 1);
+
+  if (CORBA::is_nil (this->remote_mmdevice_.in ()))
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "invalid key <%s>\n",
+                         ior),
+                        -1);
+    }
+
+  ACE_DEBUG ((LM_DEBUG, "Object received OK\n"));
+  
+  ACE_DEBUG ((LM_DEBUG, 
+              "\n(%P|%t) Bound to remote mmdevice"));
+
   return 0;
 }
 
+int
+Client::run (CORBA::Environment &env)
+{
+  return 0;
+}
 
 int
 main (int argc, char **argv)
 {
-  if (parse_args (argc, argv) == -1)
-    return -1;
-
   TAO_TRY
     {
-      CORBA::ORB_var orb = CORBA::ORB_init (argc,
-                                            argv,
-                                            0,
-                                            TAO_TRY_ENV);
+      Client client;
+      if (client.init (argc, 
+                       argv, 
+                       TAO_TRY_ENV) == -1)
+        return 1;
       TAO_CHECK_ENV;
-      
-      CORBA::Object_var basic_stream_ctrl_object =
-        orb->string_to_object (basic_stream_ctrl_key, 
-                               TAO_TRY_ENV);
-      TAO_CHECK_ENV;
-
-      Basic_StreamCtrl_var basic_stream_ctrl =
-        AVStreams::Basic_StreamCtrl::_narrow (basic_stream_ctrl_object,
-                                              TAO_TRY_ENV);
-      TAO_CHECK_ENV;
-
-      if (CORBA::is_nil (basic_stream_ctrl.in ()))
-	{
-	  ACE_ERROR_RETURN ((LM_ERROR,
-			     "invalid key <%s>\n",
-			     basic_stream_ctrl_key),
-			    -1);
-	}
-
-      ACE_DEBUG ((LM_DEBUG, "Object received OK\n"));
-      basic_stream_ctrl->push_event ("Hello world\n", 
-                                     TAO_TRY_ENV);
+      client.run (TAO_TRY_ENV);
       TAO_CHECK_ENV;
     }
-
   TAO_CATCHANY
     {
-      TAO_TRY_ENV.print_exception ("AVStreams: client: ");
+      TAO_TRY_ENV.print_exception ("Cubit::init");
       return -1;
     }
   TAO_ENDTRY;
-      
-
 }
