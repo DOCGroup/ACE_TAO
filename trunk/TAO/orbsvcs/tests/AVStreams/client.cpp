01c28da1fd427fb8574ace066368fc0cdc5e40fc
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-02 03:55:17 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/tests/AVStreams/client.cpp
+++ trunk/TAO/orbsvcs/tests/AVStreams/client.cpp
@@ -22,20 +22,63 @@ Client::Client (void)
 {
 }
 
+// initialize the ORB, get a grip on the remote mmdevice, and store it
+// in this->remote_mmdevice_
+// Also create a stream controlller and a local mmdevice
+
 int
 Client::init (int argc, 
               char **argv,
               CORBA::Environment &env)
 {
-
   // init the ORB
   manager_.init (argc,
                  argv,
                  "avstreams_poa",
                  env);
+  TAO_CHECK_ENV_RETURN (env, 1);
+
+  // create the local mmdevice
+  TAO_MMDevice *mmdevice_impl = new TAO_MMDevice;
+    this->local_mmdevice_ = mmdevice_impl->_this (env);
+  TAO_CHECK_ENV_RETURN (env, 1);
+
+  // create the local streamctrl
+  TAO_StreamCtrl *stream_ctrl = new TAO_StreamCtrl;
+  this->stream_ctrl_ = stream_ctrl->_this (env);
+  TAO_CHECK_ENV_RETURN (env, 1);
+  
+  // bind to a remote mmdevice, as supplied by argc argv
+  //  this->bind_to_remote_mmdevice (argc, argv, env);
+
+  // create a local mmdevice for now..
+  mmdevice_impl = new TAO_MMDevice;
+  this->remote_mmdevice_ = mmdevice_impl->_this (env);
 
   TAO_CHECK_ENV_RETURN (env, 1);
-                                             
+}
+
+// Use stream_ctrl_ to bind local_mmdevice_ and remote_mmdevice_
+int
+Client::run (CORBA::Environment &env)
+{
+  AVStreams::streamQoS the_qos;
+  AVStreams::flowSpec the_flows;
+  this->stream_ctrl_->bind_devs (local_mmdevice_,
+                                 remote_mmdevice_,
+                                 the_qos,
+                                 the_flows,
+                                 env);
+  TAO_CHECK_ENV_RETURN (env, 1);
+  return 0;
+}
+
+// bind to a remote mmdevice, as given by the command line arguments.
+int
+Client::bind_to_remote_mmdevice (int argc,
+                                 char **argv,
+                                 CORBA::Environment &env)
+{
   ACE_Get_Opt get_opts (argc, argv, "k:");
   int c;
   char *ior = 0;
@@ -65,7 +108,7 @@ Client::init (int argc,
                           -1);
   
   CORBA::Object_var mmdevice_object = 
-    this->manager_.orb()->string_to_object (ior,
+    this->manager_.orb ()->string_to_object (ior,
                                             env);
   TAO_CHECK_ENV_RETURN (env, 1);
 
@@ -87,12 +130,6 @@ Client::init (int argc,
   ACE_DEBUG ((LM_DEBUG, 
               "\n(%P|%t) Bound to remote mmdevice"));
 
-  return 0;
-}
-
-int
-Client::run (CORBA::Environment &env)
-{
   return 0;
 }
 
