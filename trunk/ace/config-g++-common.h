2abd7f6d578dcc0635f95c0953192144fa36f115
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-05 18:25:21 +0000

    moved ACE_HAS_EXCEPTIONS #define from config-linux-common.h to config-g++-common.h

--- trunk/ace/config-g++-common.h
+++ trunk/ace/config-g++-common.h
@@ -9,12 +9,21 @@
 #define ACE_GNUG_COMMON_H
 
 #if __GNUC__ > 2 || ( __GNUC__ == 2 && __GNUC_MINOR__ >= 90)
-  // egcs or really modern gcc
+  // egcs or really modern g++
 # define ACE_HAS_ANSI_CASTS
 # define ACE_HAS_CPLUSPLUS_HEADERS
 # define ACE_HAS_STDCPP_STL_INCLUDES
 # define ACE_HAS_TEMPLATE_TYPEDEFS
 # define ACE_HAS_TYPENAME_KEYWORD
+
+#elif __GNUC__ > 2 || ( __GNUC__ == 2 && __GNUC_MINOR__ >= 8)
+  // egcs or g++ >= 2.8.0
+// __EXCEPTIONS is defined with -fexceptions, the default.  It is not
+// defined with -fno-exceptions.
+# if defined (__EXCEPTIONS) && !defined (ACE_HAS_EXCEPTIONS)
+#   define ACE_HAS_EXCEPTIONS
+# endif /* __EXCEPTIONS && ! ACE_HAS_EXCEPTIONS */
+
 #else  /* ! EGCS */
   // Plain old g++.
 # define ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES
