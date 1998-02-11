06a21afcf2754ba2578d43cccd4a436fe1c4c0fc
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-11 17:58:36 +0000

    (print_times): divide by 1000u instead of 1000 to allow overload resolution of ACE_U_LongLong::operator /

--- trunk/TAO/tao/Timeprobe.cpp
+++ trunk/TAO/tao/Timeprobe.cpp
@@ -1,9 +1,6 @@
-//
 // $Id$
-//
 
 #include "ace/OS.h"
-
 #include "Timeprobe.h"
 
 #if !defined (__ACE_INLINE__)
@@ -50,25 +47,24 @@ ACE_Timeprobe::print_times () const
 
   ACE_OS::printf ("%-52.52s  %8.8s %10.10s\n", "NAME", "thread", "usec");
   ACE_OS::printf ("\"%-50s\" %8.8x %10.10s\n",
-		  timeprobes[0].id_, timeprobes[0].thread_, "START");
+                  timeprobes[0].id_, timeprobes[0].thread_, "START");
   for (u_int i = 1; i < current_slot_; ++i)
     {
       ACE_hrtime_t elapsed = timeprobes [i].time_ - timeprobes [i-1].time_;
       ACE_OS::printf ("\"%-50s\" %8.8x %10.3f\n",
-		      timeprobes [i].id_,
-		      timeprobes [i].thread_,
-		      (double) (elapsed / 1000) /* nanosec/microsec */);
+                      timeprobes [i].id_,
+                      timeprobes [i].thread_,
+                      (double) (elapsed / 1000u) /* nanosec/microsec */);
     }
 #if 0
   ACE_hrtime_t elapsed2 = (timeprobes [current_slot_ - 1].time_
-			   - timeprobes [0].time_);
+                           - timeprobes [0].time_);
 
   double total = elapsed2/1000.0; // nanoseconds/microsecond
   // print the total time
   ACE_OS::printf ("%52.52s =========\n"
-		  "%46.46s total %10.3f usec\n",
-		  "", "",
-		  total);
+                  "%46.46s total %10.3f usec\n",
+                  "", "",
+                  total);
 #endif /* 0 */
 }
-
