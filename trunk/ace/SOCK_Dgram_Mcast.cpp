7aad6609fe6820599a14479a0f7b78769b093976
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-15 00:35:15 +0000

    *** empty log message ***

--- trunk/ace/SOCK_Dgram_Mcast.cpp
+++ trunk/ace/SOCK_Dgram_Mcast.cpp
@@ -28,47 +28,53 @@ ACE_SOCK_Dgram_Mcast::subscribe (const ACE_INET_Addr &mcast_addr,
 				 int protocol)
 {
   ACE_TRACE ("ACE_SOCK_Dgram_Mcast::subscribe");
-  // make a local copy of address to use in sends
+
+  // Make a local copy of address to use in sends.
   this->mcast_addr_.set (mcast_addr);
 
-  if (ACE_SOCK::open (SOCK_DGRAM, protocol_family, 
+  // Only perform the open() initialization if we haven't been opened
+  // previously.
+  if (this->get_handle () == ACE_INVALID_HANDLE)
+    {
+      if (ACE_SOCK::open (SOCK_DGRAM, protocol_family, 
 		      protocol, reuse_addr) == -1)
-    return -1;
+        return -1;
+
+      int one = 1;
+      if (reuse_addr 
+          && this->ACE_SOCK::set_option (SOL_SOCKET,
+                                         SO_REUSEADDR,
+                                         &one,
+                                         sizeof one) == -1)
+        return -1;
+#if defined (SO_REUSEPORT)
+      if (this->ACE_SOCK::set_option (SOL_SOCKET,
+                                      SO_REUSEPORT,
+                                      &one,
+                                      sizeof one) == -1)
+        return -1;
+#endif /* SO_REUSEPORT */
+
+      // Create an address to bind the socket to.
+  
+      ACE_INET_Addr local;
+  
+      if (local.set (this->mcast_addr_.get_port_number ()) == -1)
+        return -1;
+      else if (ACE_SOCK_Dgram::shared_open (local, protocol_family) == -1)
+        return -1;
+    }
 
   // Create multicast request.
   if (this->make_multicast_address (this->mcast_addr_, net_if) == -1)
-    return -1;
-  
-  int one = 1;
-  if (reuse_addr 
-      && this->ACE_SOCK::set_option (SOL_SOCKET,
-				     SO_REUSEADDR,
-				     &one,
-				     sizeof one) == -1)
-    return -1;
-#if defined (SO_REUSEPORT)
-  if (this->ACE_SOCK::set_option (SOL_SOCKET,
-				  SO_REUSEPORT,
-				  &one,
-				  sizeof one) == -1)
-    return -1;
-#endif /* SO_REUSEPORT */
-
-  // Create an address to bind the socket to.
-  
-  ACE_INET_Addr local;
-  
-  if (local.set (this->mcast_addr_.get_port_number ()) == -1)
-    return -1;
-  else if (ACE_SOCK_Dgram::shared_open (local, protocol_family) == -1)
     return -1;
 
   // Tell network device driver to read datagrams with a
   // multicast_address address.
   else if (this->ACE_SOCK::set_option (IPPROTO_IP, 
-				       IP_ADD_MEMBERSHIP,
-				       &multicast_address_, 
-				       sizeof multicast_address_) == -1)
+                                       IP_ADD_MEMBERSHIP,
+                                       &multicast_address_, 
+                                       sizeof multicast_address_) == -1)
     return -1;
   return 0;
 }
