b22d67584fa598581015c9aed4f1690ec8111bf2
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-16 19:25:25 +0000

    *** empty log message ***

--- trunk/apps/JAWS/server/JAWS_Pipeline_Handler.cpp
+++ trunk/apps/JAWS/server/JAWS_Pipeline_Handler.cpp
@@ -1,25 +1,9 @@
 // $Id$
 
-#include "JAWS_Pipeline.h"
+#if !defined (JAWS_PIPELINE_HANDLER_CPP)
+#define JAWS_PIPELINE_HANDLER_CPP
 
-JAWS_Pipeline::JAWS_Pipeline (void)
-{
-}
-
-int
-JAWS_Pipeline::open (void * = 0)
-{
-  // Simply call into the virtual svc() method.
-  if (this->svc() == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "JAWS_Pipeline::svc"), -1);
-  return 0;
-}
-
-int
-JAWS_Pipeline::close (u_long = 0)
-{
-  return 0;
-}
+#include "JAWS_Pipeline_Handler.h"
 
 template <class TYPE>
 JAWS_Pipeline_Handler<TYPE>::JAWS_Pipeline_Handler (void)
@@ -33,3 +17,5 @@ JAWS_Pipeline_Handler<TYPE>::put (ACE_Message_Block *mb, ACE_Time_Value *tv)
   status = this->handle_input (data, tv);
   return (status != -1) ? this->put_next (mb, tv) : -1;
 }
+
+#endif /* !defined (JAWS_PIPELINE_HANDLER_CPP) */
