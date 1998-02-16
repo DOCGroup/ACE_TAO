7a091595e9c4f4e33e53a428988a75fc5fe3b823
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-16 19:25:25 +0000

    *** empty log message ***

--- apps/JAWS/server/JAWS_Pipeline.h
+++ apps/JAWS/server/JAWS_Pipeline.h
@@ -26,21 +26,6 @@ public:
   virtual int close (u_long = 0);
 };
 
-template <class TYPE>
-class JAWS_Pipeline_Handler : public JAWS_Pipeline_Task
-  // = TITLE
-  //   Methods that are common to pipeline components
-{
-public:
-  JAWS_Pipeline_Handler (void);
-  // ACE_Task hooks
-
-  virtual int put (ACE_Message_Block *mb, ACE_Time_Value *tv = 0);
-  // inherited from ACE_Task
-
-  virtual int handle_input (TYPE *data, ACE_Time_Value *tv) = 0;
-  // Callback hook for specialized data processing
-};
-
+#include "JAWS_Pipeline_Handler.h"
 
 #endif /* !defined (JAWS_PIPELINE_H) */
