8eafe7b968ad0fda7e0baba5ed131a5329085f82
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-16 08:05:13 +0000

    Added CosProperty_Hash.cpp file

---
+++ TAO/orbsvcs/orbsvcs/CosProperty_Hash.cpp
@@ -0,0 +1,100 @@
+// $:Id$
+//
+// FILE
+//   CosProperty_Hash.cpp
+//
+// Description
+//   Defines equal and hash functions for the Hash_Property_Map class.
+//   
+
+#include "CosProperty_Hash.h"
+
+EXT_ID::EXT_ID ()
+{
+}
+
+EXT_ID::EXT_ID (const CosPropertyService::PropertyName &name)
+  :  pname_ (name)
+{
+}
+
+int
+EXT_ID::operator == (const EXT_ID &ext_id) const   
+{
+  return (strcmp(this->pname_.in (), ext_id.pname_.in ()) );
+}
+
+long  unsigned int 
+EXT_ID::hash (void) const 
+{
+  return ACE::hash_pjw (pname_.in());
+}
+
+EXT_ID::~EXT_ID () 
+{
+
+}
+
+INT_ID::INT_ID ()
+{
+}
+
+INT_ID::INT_ID (const CORBA::Any &anyvalue)
+{
+  TAO_TRY {
+    pvalue_.replace (anyvalue.type (), anyvalue.value (), CORBA::B_FALSE, TAO_TRY_ENV);
+  }
+  TAO_CATCH (CORBA::SystemException, sysex) {
+    TAO_TRY_ENV.print_exception ("System Exception");
+    //return -1;
+  }
+  TAO_CATCH (CORBA::UserException, userex)
+    {
+      TAO_TRY_ENV.print_exception ("User Exception");
+      //return -1;
+    }
+  TAO_ENDTRY;
+}
+
+INT_ID::~INT_ID () 
+{
+
+}
+
+/*
+long unsigned int
+Hash_Property_Map::hash (const EXT_ID &ext_id) 
+{
+  return ACE::hash_pjw (ext_id.pname_.in ());
+}
+
+
+int
+Hash_Property_Map::equal (const EXT_ID &id1, const EXT_ID &id2)
+{
+  return (id1==id2);
+}
+*/
+
+/*
+static const int MAX_HASH=256;
+
+int 
+main (int argc, char *argv[]) 
+{
+  Hash_Property_Map hash (MAX_HASH);
+  
+  cout << "CosProperty_Hash" << endl;
+  
+  return 0;
+}  
+
+*/
+
+
+
+
+
+
+
+
