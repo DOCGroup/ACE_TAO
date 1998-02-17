507e00883ccdf05d272f60cd99f56e451700f522
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-17 20:27:43 +0000

    *** empty log message ***

--- trunk/apps/JAWS/server/JAWS_Pipeline_Handler.cpp
+++ trunk/apps/JAWS/server/JAWS_Pipeline_Handler.cpp
@@ -13,8 +13,11 @@ JAWS_Pipeline_Handler<TYPE>::JAWS_Pipeline_Handler (void)
 template <class TYPE> int
 JAWS_Pipeline_Handler<TYPE>::put (ACE_Message_Block *mb, ACE_Time_Value *tv)
 {
-  TYPE *data = ACE_dynamic_cast(TYPE *, mb->data_block ());
+  TYPE *data = ACE_dynamic_cast (TYPE *, 
+                                 mb->data_block ());
+
   status = this->handle_input (data, tv);
+
   return (status != -1) ? this->put_next (mb, tv) : -1;
 }
 
