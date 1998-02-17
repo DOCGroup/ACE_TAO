7e57523c94fce1f53f429fcf5357fd38987a6b4f
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-17 02:21:56 +0000

    *** empty log message ***

--- TAO/orbsvcs/tests/CosPropertyService/main.cpp
+++ TAO/orbsvcs/tests/CosPropertyService/main.cpp
@@ -24,60 +24,63 @@ main (int argc, char *argv [])
 {
   ACE_DEBUG ( (LM_DEBUG, "TAO_PropertySet Testing \n"));
   
-  TAO_TRY {
-    TAO_PropertySet propSet;
-    CORBA::Any testval;
-
-    // Number of properties in the PropertyaSet
-    ACE_DEBUG ( (LM_DEBUG, "Test : Number of props : %d \n", propSet.get_number_of_properties (TAO_TRY_ENV)) );
-
-    // prepare a Short and  "define" that in the PropertySet 
-    CORBA::Short s = 3;
-    testval.replace ( CORBA::_tc_short, &s, CORBA::B_FALSE, TAO_TRY_ENV);
-    ACE_DEBUG ( (LM_DEBUG, "Short s = %d \n", ( *(short *)testval.value ())) );
- 
-    propSet.define_property ("short_property", testval, TAO_TRY_ENV);
-    
-    // prepare a Long and  "define" that in the PropertySet 
-    CORBA::Long l = 931232;
-    testval.replace (CORBA::_tc_long, &l, CORBA::B_FALSE, TAO_TRY_ENV);
-    ACE_DEBUG ( (LM_DEBUG, "Long l = %d \n", (*(long *)testval.value ())) );
-    
-    propSet.define_property ("long_property", testval, TAO_TRY_ENV);
-     
-    // prepare a Long and  "define" that in the PropertySet 
-    float f = 3.14;
-    testval.replace (CORBA::_tc_float, &f, CORBA::B_FALSE, TAO_TRY_ENV);
-    f =   (*((float *)testval.value ()));
-    ACE_DEBUG ( (LM_DEBUG, "Float f = %f \n", f));
-    
-    propSet.define_property ("float_property", testval, TAO_TRY_ENV);
-
-    // Let us test the number of properties now
-    ACE_DEBUG ( (LM_DEBUG, "Number of props : %d \n", propSet.get_number_of_properties (TAO_TRY_ENV)) );
-    
-    // delete a few properties
-    propSet.delete_property ("short_property", TAO_TRY_ENV);
-    ACE_DEBUG ( (LM_DEBUG, "\nshort_property deleted !!") );
-
-    // Let us test the number of properties now
-    ACE_DEBUG ( (LM_DEBUG, "\n Number of props : %d \n", propSet.get_number_of_properties (TAO_TRY_ENV)) );
-    
-
-    // Let us try to "get" back those values
-    testval =  * propSet.get_property_value ("float_property", TAO_TRY_ENV);
-    testval =  * propSet.get_property_value ("short_property", TAO_TRY_ENV);
-  }
-  TAO_CATCH (CORBA::SystemException, sysex) {
-    TAO_TRY_ENV.print_exception ("System Exception");
-    return 1;
-  }
-  TAO_CATCH (CORBA::UserException, userex) {
-    TAO_TRY_ENV.print_exception ("User Exception");
-    return 1;
-  }
+  TAO_TRY 
+    {
+      TAO_PropertySet propSet;
+      CORBA::Any testval;
+      
+      // Number of properties in the PropertyaSet
+      ACE_DEBUG ( (LM_DEBUG, "Test : Number of props : %d \n", propSet.get_number_of_properties (TAO_TRY_ENV)) );
+      
+      // prepare a Short and  "define" that in the PropertySet 
+      CORBA::Short s = 3;
+      testval.replace ( CORBA::_tc_short, &s, CORBA::B_FALSE, TAO_TRY_ENV);
+      ACE_DEBUG ( (LM_DEBUG, "Short s = %d \n", ( *(short *)testval.value ())) );
+      
+      propSet.define_property ("short_property", testval, TAO_TRY_ENV);
+      
+      // prepare a Long and  "define" that in the PropertySet 
+      CORBA::Long l = 931232;
+      testval.replace (CORBA::_tc_long, &l, CORBA::B_FALSE, TAO_TRY_ENV);
+      ACE_DEBUG ( (LM_DEBUG, "Long l = %d \n", (*(long *)testval.value ())) );
+      
+      propSet.define_property ("long_property", testval, TAO_TRY_ENV);
+      
+      // prepare a Long and  "define" that in the PropertySet 
+      float f = 3.14;
+      testval.replace (CORBA::_tc_float, &f, CORBA::B_FALSE, TAO_TRY_ENV);
+      f =   (*((float *)testval.value ()));
+      ACE_DEBUG ( (LM_DEBUG, "Float f = %f \n", f));
+      
+      propSet.define_property ("float_property", testval, TAO_TRY_ENV);
+      
+      // Let us test the number of properties now
+      ACE_DEBUG ( (LM_DEBUG, "Number of props : %d \n", propSet.get_number_of_properties (TAO_TRY_ENV)) );
+      
+      // delete a few properties
+      propSet.delete_property ("short_property", TAO_TRY_ENV);
+      ACE_DEBUG ( (LM_DEBUG, "\nshort_property deleted !!") );
+      
+      // Let us test the number of properties now
+      ACE_DEBUG ( (LM_DEBUG, "\n Number of props : %d \n", propSet.get_number_of_properties (TAO_TRY_ENV)) );
+      
+      
+      // Let us try to "get" back those values
+      testval =  * propSet.get_property_value ("float_property", TAO_TRY_ENV);
+      testval =  * propSet.get_property_value ("short_property", TAO_TRY_ENV);
+    }
+  TAO_CATCH (CORBA::SystemException, sysex)
+    {
+      TAO_TRY_ENV.print_exception ("System Exception");
+      return 1;
+    }
+  TAO_CATCH (CORBA::UserException, userex)
+    {
+      TAO_TRY_ENV.print_exception ("User Exception");
+      return 1;
+    }
   TAO_ENDTRY;
-
+  
   return 0;
 }  
 
