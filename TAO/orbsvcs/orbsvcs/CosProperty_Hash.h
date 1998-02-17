dd748cf517553e0a5c51f767711fd91ec2d5baeb
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-17 02:36:02 +0000

    Indentation. commenting

--- TAO/orbsvcs/orbsvcs/CosProperty_Hash.h
+++ TAO/orbsvcs/orbsvcs/CosProperty_Hash.h
@@ -1,15 +1,16 @@
 //$Id$
+
+// ===================================================================================================
+// = FILE
+//     CosProperty_Hash.h
 //
+// = DESCRITION
+//     Defines INT_ID and EXT_ID classes for the Hash_Map_Manager to be used in the CosPropertyService.
 //
-// FILE
-//   CosProperty_Hash.h
-//
-// DESCRITION
-//   Defines INT_ID and EXT_ID classes for the Hash_Map_Manager to be used in the CosPropertyService.
-//
-// AUTHOR
-//   Alexander B. Arulanthu
-//
+// = AUTHOR
+//     Alexander B. Arulanthu
+// ===================================================================================================
+
 #if !defined (COSPROPERTY_HASH_H)
 #define	COSPROPERTY_HASH_H
 
@@ -21,38 +22,48 @@
 class EXT_ID 
 // Key for the Hash Table
 {
-public:  // Functions
+public: 
+  // Default constructor
   EXT_ID ();
   
+  // Costructor that takes PropertyName
   EXT_ID (const CosPropertyService::PropertyName &name);
 
+  // Copy constructor
   //EXT_ID (const EXT_ID &ext_id);
   
+  // The vital operator for hash binding and "find"ing
   virtual int operator == (const EXT_ID &ext_id) const;
 
+  // The vital function
   virtual long unsigned int hash (void) const;
 
+  // Destructor
   virtual ~EXT_ID ();
 
-public:  // Data
+  // This is still the public variable... 
   CosPropertyService::PropertyName_var pname_; 
 };
 
 class INT_ID
 // This is the Hash Entry
 {
-public:  // Functions
+public: 
+
+  // Default constructor 
   INT_ID ();
   
+  // Constructor with Any value
   INT_ID (const CORBA::Any &value);
 
+  // Copy constructor
   //INT_ID (const INT_ID &int_id);
-
+  
+  // Destructor
   virtual ~INT_ID ();
 
-public:  // Data
+  // Public data -- the property value
   CORBA::Any pvalue_;
-  
 };
 
 
