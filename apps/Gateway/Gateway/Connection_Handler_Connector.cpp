56f37216c4dfb464bf8534200af9367b6b94cc38
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-02-18 23:07:38 +0000

    *** empty log message ***

--- apps/Gateway/Gateway/Connection_Handler_Connector.cpp
+++ apps/Gateway/Gateway/Connection_Handler_Connector.cpp
@@ -10,22 +10,22 @@ Connection_Handler_Connector::Connection_Handler_Connector (void)
 
 int
 Connection_Handler_Connector::initiate_connection (Connection_Handler *connection_handler,
-					      ACE_Synch_Options &synch_options)
+                                                   ACE_Synch_Options &synch_options)
 {
   char addr_buf[MAXHOSTNAMELEN];
 
-  // Mark ourselves as idle so that the various iterators
-  // will ignore us until we are reconnected.
+  // Mark ourselves as idle so that the various iterators will ignore
+  // us until we are reconnected.
   connection_handler->state (Connection_Handler::IDLE);
 
-  // We check the remote addr second so that it remains in the addr_buf.
+  // We check the remote addr second so that it remains in the
+  // addr_buf.
   if (connection_handler->local_addr ().addr_to_string (addr_buf, sizeof addr_buf) == -1
       || connection_handler->remote_addr ().addr_to_string (addr_buf, sizeof addr_buf) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n",
 		      "can't obtain peer's address"), -1);
 
   // Try to connect to the Peer.
-
   if (this->connect (connection_handler,
                      connection_handler->remote_addr (),
 		     synch_options,
@@ -36,7 +36,6 @@ Connection_Handler_Connector::initiate_connection (Connection_Handler *connectio
 	  connection_handler->state (Connection_Handler::FAILED);
 	  ACE_DEBUG ((LM_DEBUG, "(%t) %p on address %s\n",
 		      "connect", addr_buf));
-
 	  return -1;
 	}
       else
