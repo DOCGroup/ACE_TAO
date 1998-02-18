85ecd50f20c9b3ec12c6bfed404b10b80c9b391d
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-18 23:11:48 +0000

    *** empty log message ***

--- trunk/TAO/tao/tao_util.h
+++ trunk/TAO/tao/tao_util.h
@@ -29,14 +29,12 @@ class TAO_ORB_Manager
 public:
   int init (int argc, 
             char **argv, 
-            char *poa_name,
             CORBA_Environment &env);
   // Initialize the ORB, using the supplied command line arguments.
   // the poa_name is a user-supplied string that is used to name the
   // POA created.
       
-  CORBA::String activate (const char *object_name,
-                          PortableServer::Servant servant,
+  CORBA::String activate (PortableServer::Servant servant,
                           CORBA_Environment &env);
   // Activate the servant with the supplied object_name
   // Call multiple times to activate multiple objects.
@@ -44,19 +42,16 @@ public:
   int run (CORBA_Environment &env);
   // Run the ORB event loop
       
-  CORBA::ORB_var orb (void);
+  CORBA::ORB_ptr orb (void);
   // Accessor which returns the ORB pointer
       
 protected:
-  CORBA::ORB_var orb_;
+  CORBA::ORB_ptr orb_;
   // The ORB
       
   PortableServer::POA_var root_poa_;
   // The root POA
       
-  PortableServer::POA_var my_poa_;
-  // We create our own POA
-  
   PortableServer::POAManager_var poa_manager_;
   // The POA manager
       
