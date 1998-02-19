dba11c88d58391d69a23d988c9ed8ec55ce0f77b
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-19 00:47:09 +0000

    *** empty log message ***

--- trunk/TAO/tao/tao_util.cpp
+++ trunk/TAO/tao/tao_util.cpp
@@ -11,13 +11,14 @@
 // = AUTHOR
 //    Sumedh Mungee <sumedh@cs.wustl.edu>
 //
-//
 // ============================================================================
+
 #include "tao_util.h"
 
-// Initialize the ORB, using the supplied command line arguments.
-// the poa_name is a user-supplied string that is used to name the
-// POA created.
+// Initialize the ORB, using the supplied command line arguments.  the
+// poa_name is a user-supplied string that is used to name the POA
+// created.
+
 int
 TAO_ORB_Manager::init (int argc,
                        char **argv,
@@ -29,21 +30,22 @@ TAO_ORB_Manager::init (int argc,
                                 env);
   TAO_CHECK_ENV_RETURN (env, 1);
 
-  // Get the POA from the ORB
+  // Get the POA from the ORB.
   CORBA::Object_var poa_object =
-    this->orb_->resolve_initial_references("RootPOA");
-  if (CORBA::is_nil(poa_object.in()))
-    ACE_ERROR_RETURN ((LM_ERROR,
+    this->orb_->resolve_initial_references ("RootPOA");
+
+  if (CORBA::is_nil (poa_object.in ()))
+    ACE_ERROR_RETURN ( (LM_ERROR,
                        " (%P|%t) Unable to initialize the POA.\n"),
                       1);
 
-  // Get the POA object
+  // Get the POA object.
   this->root_poa_ =
     PortableServer::POA::_narrow (poa_object.in (), env);
 
   TAO_CHECK_ENV_RETURN (env, 1);
 
-  // Get the POA_Manager
+  // Get the POA_Manager.
   this->poa_manager_ =
     this->root_poa_->the_POAManager (env);
 
@@ -70,28 +72,35 @@ TAO_ORB_Manager::activate (PortableServer::Servant servant,
   CORBA::String str =
     this->orb_->object_to_string (obj.in (),
                                   env);
-  
   TAO_CHECK_ENV_RETURN (env, 0);
 
   return str;
 }
 
+// @@ Sumedh, shouldn't run() take an ACE_Time_Value * so users can do
+// timed run()'s?!
 int
 TAO_ORB_Manager::run (CORBA::Environment &env)
 {
   this->poa_manager_->activate (env);
+
+  // @@ Sumedh, why do you sometimes return -1 and sometimes return 1
+  // @@ here?  Is this intended? 
   TAO_CHECK_ENV_RETURN (env, 1);
 
   if (this->orb_->run () == -1)
-    ACE_ERROR_RETURN ((LM_ERROR,
+    ACE_ERROR_RETURN ( (LM_ERROR,
                        "%p\n",
-                       "run"), -1);
+                       "run"),
+                       -1);
 
+  // @@ Sumedh, should the destroy() be done here, or in the
+  // destructor?  Doesn't this make it impossible to call run()
+  // multiple times (see comments about ACE_Time_Value above...)?
   this->root_poa_->destroy (CORBA::B_TRUE,
-                           CORBA::B_TRUE,
-                           env);
+                            CORBA::B_TRUE,
+                            env);
   TAO_CHECK_ENV_RETURN (env, 1);
-
   return 0;
 }
 
--- trunk/TAO/tao/tao_util.h
+++ trunk/TAO/tao/tao_util.h
@@ -12,7 +12,6 @@
 //
 // = AUTHOR
 //    Sumedh Mungee <sumedh@cs.wustl.edu>
-//    
 // 
 // ============================================================================
 
@@ -24,37 +23,46 @@
 class TAO_ORB_Manager
 {
   // = TITLE
-  //     Helper class for simple ORB initialization and 
-  //     registering objects
+  //     Helper class for simple ORB/POA initialization and 
+  //     registering servants with the POA.
 public:
   int init (int argc, 
             char **argv, 
             CORBA_Environment &env);
-  // Initialize the ORB, using the supplied command line arguments.
-  // the poa_name is a user-supplied string that is used to name the
-  // POA created.
+  // Initialize the ORB/POA, using the supplied command line
+  // arguments.  The <poa_name> is a user-supplied string that is used
+  // @@ Sumedh, where is <poa_name> actually used here?  Is this an obsolete comment?
+  // to name the POA created.
       
   CORBA::String activate (PortableServer::Servant servant,
                           CORBA_Environment &env);
-  // Activate the servant with the supplied object_name
-  // Call multiple times to activate multiple objects.
+  // Activate <servant> with the supplied <object_name>.
+  // @@ Sumedh, where is <object_name>?  Is this comment obsolete too?
+  // @@ Sumedh, can you please explain what is meant by "activate?"  Is
+  // @@ this what is meant by the activate() method on the POA?  If so,
+  // @@ please make sure you point this out!
+  // Users can call this method multiple times to activate multiple
+  // objects.
       
   int run (CORBA_Environment &env);
-  // Run the ORB event loop
+  // Run the ORB event loop.
       
   CORBA::ORB_ptr orb (void);
-  // Accessor which returns the ORB pointer
+  // Accessor which returns the ORB pointer.
       
 protected:
   CORBA::ORB_ptr orb_;
-  // The ORB
+  // The ORB.
       
   PortableServer::POA_var root_poa_;
-  // The root POA
+  // The root POA.
+  // @@ Sumedh, shouldn't the user be able to specify the POA to use
+  // @@ when they call the class constructor?!
       
   PortableServer::POAManager_var poa_manager_;
-  // The POA manager
-      
+  // The POA manager.
+  // @@ Sumedh, shouldn't users be able to specify which POAManager they
+  // @@ want to use when they call the class constructor?!
 };
   
 #endif /* TAO_UTIL_H */
