157ae0a4be17159663e93017eeee7954d6f35fce
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-25 21:48:59 +0000

    Added 2 new methods init_child_poa and activate_under_child_poa.

--- trunk/TAO/tao/tao_util.h
+++ trunk/TAO/tao/tao_util.h
@@ -34,16 +34,34 @@ public:
   int init (int argc, 
             char **argv, 
             CORBA_Environment &env);
-  // Initialize the ORB/POA, using the supplied command line
+  // Initialize the ORB/root POA, using the supplied command line
   // arguments or the default ORB components. 
   // Returns -1 on failure
       
+  int init_child_poa (int argc,
+		      char **argv,
+		      char *poa_name,
+		      CORBA_Environment &env);
+  // Creates a child poa under the root poa with PERSISTENT and
+  // USER_ID policies.Call this if you want a child_poa with the above
+  // policies, otherwise call init.
+
+		      
   CORBA::String activate (PortableServer::Servant servant,
                           CORBA_Environment &env);
   // Activate <servant>, using the POA <activate_object> call.  Users
   // can call this method multiple times to activate multiple objects.
   // Returns 0 on failure
       
+  CORBA::String activate_under_child_poa (const char* servant_name,
+					  PortableServer::Servant
+					  servant,
+					  CORBA_Environment& env);
+  // Precondition: init_child_poa has been called.
+  // Activate <servant> using the POA <activate_object_with_id>
+  // created from the string servant_name. Users should call this to
+  // activate objects under the child_poa.
+
   int run (CORBA_Environment &env,
            ACE_Time_Value *tv = 0);
   // Run the ORB event loop with the specified <tv> time value
@@ -61,9 +79,12 @@ protected:
       
   PortableServer::POA_var root_poa_;
   // The root POA.
-      
+
+  PortableServer::POA_var child_poa_;
+  // Child poa under the root POA.
+
   PortableServer::POAManager_var poa_manager_;
-  // The POA manager.
+  // The POA manager of root_poa_.
 };
   
 #endif /* TAO_UTIL_H */
