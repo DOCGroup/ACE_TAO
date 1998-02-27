f289bbdead98cd726b663379b76eca215d7745b7
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-27 04:57:51 +0000

    *** empty log message ***

--- TAO/orbsvcs/tests/AVStreams/server.cpp
+++ TAO/orbsvcs/tests/AVStreams/server.cpp
@@ -16,9 +16,39 @@
 //
 // ============================================================================
 
-#include "orbsvcs/AV/AVStreams_i.h"
-/* #include "tao/tao.h" */
-#include "tao/tao_util.h"
+#include "server.h"
+
+Video_Server_StreamEndPoint::Video_Server_StreamEndPoint ()
+{
+}
+
+void 
+Video_Server_StreamEndPoint::handle_stop (const AVStreams::flowSpec &the_spec,
+                                          CORBA::Environment &env)
+{
+}
+
+void 
+Video_Server_StreamEndPoint::handle_start (const AVStreams::flowSpec &the_spec,  
+                                           CORBA::Environment &env)
+{
+}
+
+void 
+Video_Server_StreamEndPoint::handle_destroy (const AVStreams::flowSpec &the_spec,  
+                                             CORBA::Environment &env)
+{
+}
+
+CORBA::Boolean 
+Video_Server_StreamEndPoint::handle_connection_requested (AVStreams::StreamEndPoint_ptr initiator, 
+                                                          CORBA::Boolean is_mcast, 
+                                                          AVStreams::streamQoS &qos, 
+                                                          AVStreams::flowSpec &the_spec,  
+                                                          CORBA::Environment &env)
+{
+  return 1;
+} 
 
 int
 main (int argc, char ** argv)
@@ -27,23 +57,29 @@ main (int argc, char ** argv)
     {
       TAO_ORB_Manager m;
 
+      // Initialize the ORB
       m.init (argc, 
               argv,
               TAO_TRY_ENV);
       TAO_CHECK_ENV;
 
-      TAO_MMDevice *mmdevice_impl = new TAO_MMDevice;
+      // Create server-side MMDevice
+      TAO_Server_MMDevice <Video_Server_StreamEndPoint> *mmdevice_impl 
+        = new TAO_Server_MMDevice <Video_Server_StreamEndPoint>;
       TAO_CHECK_ENV;
 
+      // Activate the MMDevice, i.e. register with POA
       CORBA::String_var s;
       s = m.activate (mmdevice_impl,
                       TAO_TRY_ENV);
       TAO_CHECK_ENV;
       
+      // Print the IOR
       ACE_DEBUG ((LM_DEBUG,
                   "\nThe IOR is: <%s>\n", 
                   s.in ()));
 
+      // Run the ORB Event loop
       m.run (TAO_TRY_ENV);
       TAO_CHECK_ENV;
     }
