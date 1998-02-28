0d8848e7e8178c3ea06b2c3fbf70fa06fc228ac1
cleeland <cleeland@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-02-28 00:05:25 +0000

    Whack out @@ comments.

--- trunk/TAO/tao/request.h
+++ trunk/TAO/tao/request.h
@@ -21,23 +21,38 @@
 #if !defined (TAO_REQUEST_H)
 #  define TAO_REQUEST_ H
 
-// @@ Please comment everything in this file.
-
 class TAO_Export CORBA_Request : public TAO_IUnknown
 {
 public:
-  // @@ These inlines should be moved into a *.i file.
   CORBA::Object_ptr target (void) const { return target_; }
+  // Return the target of this request.
+  
   const CORBA::Char *operation (void) const { return opname_; }
+  // Return the operation name for the request.
+  
   CORBA::NVList_ptr arguments (void) { return args_; }
+  // Return the arguments for the request.
+  
   CORBA::NamedValue_ptr result (void) { return result_; }
+  // Return the result for the request.
+  
   CORBA::ExceptionList_ptr exceptions (void) { return &exceptions_; }
+  // Return the exceptions resulting from this request.
+  
   CORBA::Environment *env (void) { return &env_; }
+  // Return the <Environment> for this request.
 
   void invoke (void);
+  // Perform method resolution and invoke an appropriate method. If
+  // the method returns successfully, its result is placed in the
+  // result argument specified on <create_request>. The behavior is
+  // undefined if this <Request> has already been used with a previous
+  // call to <invoke>, <send>, or <send_multiple_requests>.
+  
   void send_oneway (void);
+  // Send a oneway request.
 
-  // Stuff required for COM IUnknown support
+  // = Stuff required for COM IUnknown support
 
   ULONG AddRef (void);
   ULONG Release (void);
