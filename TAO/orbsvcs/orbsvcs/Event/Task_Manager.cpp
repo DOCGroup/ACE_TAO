9bcd4f58f9f154c21a7e6efe38cea9b39e13f7bc
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-04 03:10:00 +0000

    (initialize): maintain period in units of 100 ns when converting to a long

--- TAO/orbsvcs/orbsvcs/Event/Task_Manager.cpp
+++ TAO/orbsvcs/orbsvcs/Event/Task_Manager.cpp
@@ -19,20 +19,22 @@ void ACE_Task_Manager::initialize()
 {
   for (int x=0; x < ACE_Scheduler_MAX_PRIORITIES; x++)
     {
-      // @@ Apparently Period is measured in a different unit that the
-      // time, beats me.
-      ACE_hrtime_t nanosecs;
-      ORBSVCS_Time::TimeT_to_hrtime (nanosecs, ACE_Scheduler_Rates[x]);
+      // Convert ACE_Scheduler_Rate (it's really a period, not a rate!)
+      // to a form we can easily work with.
+      ACE_Time_Value period_tv;
+      ORBSVCS_Time::TimeT_to_Time_Value (period_tv, ACE_Scheduler_Rates[x]);
 
-      RtecScheduler::Period period = nanosecs;
+      RtecScheduler::Period period = period_tv.sec () * 10000000 +
+                                     period_tv.usec () * 10;
+
       reactorTasks[x] = new ReactorTask;
 
       if (reactorTasks[x] == 0 ||
-	  reactorTasks[x]->open_reactor (period) == -1)
-	{
-	  ACE_ERROR ((LM_ERROR, "%p.\n", "ACE_ORB::initialize_reactors"));
-	  return;
-	}
+          reactorTasks[x]->open_reactor (period) == -1)
+        {
+          ACE_ERROR ((LM_ERROR, "%p.\n", "ACE_ORB::initialize_reactors"));
+          return;
+        }
     }
 }
 
