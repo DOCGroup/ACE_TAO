217100b76abbb642d159eb4afe1e05e6b5b76253
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-08 20:29:11 +0000

    (initiate_acceptors): changed SUPPLIER_CONNECTOR to SUPPLIER_ACCEPTOR

--- apps/Gateway/Gateway/Event_Channel.cpp
+++ apps/Gateway/Gateway/Event_Channel.cpp
@@ -28,7 +28,7 @@ Event_Channel::compute_performance_statistics (void)
       != Options::REACTIVE)
     {
       if (ACE_Thread_Manager::instance ()->suspend_all () == -1)
-	ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n", "suspend_all"), -1);
+        ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n", "suspend_all"), -1);
       ACE_DEBUG ((LM_DEBUG, "(%t) suspending all threads..."));
     }
 
@@ -45,25 +45,25 @@ Event_Channel::compute_performance_statistics (void)
       Connection_Handler *connection_handler = me->int_id_;
 
       if (connection_handler->connection_role () == 'C')
-	total_bytes_out += connection_handler->total_bytes ();
+        total_bytes_out += connection_handler->total_bytes ();
       else // connection_handler->connection_role () == 'S'
-	total_bytes_in += connection_handler->total_bytes ();
+        total_bytes_in += connection_handler->total_bytes ();
     }
 
   ACE_DEBUG ((LM_DEBUG,
               "(%t) after %d seconds, \ntotal_bytes_in = %d\ntotal_bytes_out = %d\n",
-	      Options::instance ()->performance_window (),
-	      total_bytes_in,
-	      total_bytes_out));
+              Options::instance ()->performance_window (),
+              total_bytes_in,
+              total_bytes_out));
 
   ACE_DEBUG ((LM_DEBUG,
               "(%t) %f Mbits/sec received.\n",
-	      (float) (total_bytes_in * 8 /
+              (float) (total_bytes_in * 8 /
                        (float) (1024 * 1024 * Options::instance ()->performance_window ()))));
 
   ACE_DEBUG ((LM_DEBUG,
               "(%t) %f Mbits/sec sent.\n",
-	      (float) (total_bytes_out * 8 /
+              (float) (total_bytes_out * 8 /
                        (float) (1024 * 1024 * Options::instance ()->performance_window ()))));
 
   // Resume all the threads again.
@@ -72,7 +72,7 @@ Event_Channel::compute_performance_statistics (void)
       != Options::REACTIVE)
     {
       if (ACE_Thread_Manager::instance ()->resume_all () == -1)
-	ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n", "resume_all"), -1);
+        ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n", "resume_all"), -1);
       ACE_DEBUG ((LM_DEBUG, "(%t) resuming all threads..."));
     }
 
@@ -103,8 +103,8 @@ Event_Channel::put (ACE_Message_Block *event,
   switch (event_key->type_)
     {
     case ROUTING_EVENT:
-      this->routing_event (event_key, 
-                           data); 
+      this->routing_event (event_key,
+                           data);
       break;
     case SUBSCRIPTION_EVENT:
       this->subscription_event (data);
@@ -123,7 +123,7 @@ Event_Channel::subscription_event (ACE_Message_Block *data)
 
   ACE_DEBUG ((LM_DEBUG,
               "(%t) received a subscription with %d bytes from connection id %d\n",
-	      event->header_.len_,
+              event->header_.len_,
               event->header_.connection_id_));
   Subscription *subscription = (Subscription *) event->data_;
   // Convert the subscription into host byte order so that we can
@@ -140,9 +140,9 @@ Event_Channel::subscription_event (ACE_Message_Block *data)
        i++)
     ACE_DEBUG ((LM_DEBUG,
                 "(%t) consumers_[%d] = %d\n",
-                i, 
+                i,
                 subscription->consumers_[i]));
-    
+
 }
 
 void
@@ -158,58 +158,58 @@ Event_Channel::routing_event (Event_Key *forwarding_address,
                        dispatch_set) == -1)
     // Failure.
     ACE_ERROR ((LM_DEBUG,
-		"(%t) find failed on connection id = %d, type = %d\n",
-		forwarding_address->connection_id_,
-		forwarding_address->type_));
+                "(%t) find failed on connection id = %d, type = %d\n",
+                forwarding_address->connection_id_,
+                forwarding_address->type_));
   else
     {
       // Check to see if there are any consumers.
       if (dispatch_set->size () == 0)
-	ACE_DEBUG ((LM_WARNING,
+        ACE_DEBUG ((LM_WARNING,
                     "there are no active consumers for this event currently\n"));
 
       else // There are consumers, so forward the event.
-	{
+        {
           // Initialize the interator.
-	  Consumer_Dispatch_Set_Iterator dsi (*dispatch_set);
+          Consumer_Dispatch_Set_Iterator dsi (*dispatch_set);
 
-	  // At this point, we should assign a thread-safe locking
-	  // strategy to the <ACE_Message_Block> is we're running in a
-	  // multi-threaded configuration.
+          // At this point, we should assign a thread-safe locking
+          // strategy to the <ACE_Message_Block> is we're running in a
+          // multi-threaded configuration.
           data->locking_strategy (Options::instance ()->locking_strategy ());
 
-	  for (Connection_Handler **connection_handler = 0;
-	       dsi.next (connection_handler) != 0;
-	       dsi.advance ())
-	    {
-	      // Only process active connection_handlers.
-	      if ((*connection_handler)->state () == Connection_Handler::ESTABLISHED)
-		{
-		  // Duplicate the event portion via reference
-		  // counting.
-		  ACE_Message_Block *dup_msg = data->duplicate ();
+          for (Connection_Handler **connection_handler = 0;
+               dsi.next (connection_handler) != 0;
+               dsi.advance ())
+            {
+              // Only process active connection_handlers.
+              if ((*connection_handler)->state () == Connection_Handler::ESTABLISHED)
+                {
+                  // Duplicate the event portion via reference
+                  // counting.
+                  ACE_Message_Block *dup_msg = data->duplicate ();
 
-		  ACE_DEBUG ((LM_DEBUG, "(%t) forwarding to Consumer %d\n",
-			      (*connection_handler)->connection_id ()));
+                  ACE_DEBUG ((LM_DEBUG, "(%t) forwarding to Consumer %d\n",
+                              (*connection_handler)->connection_id ()));
 
-		  if ((*connection_handler)->put (dup_msg) == -1)
-		    {
-		      if (errno == EWOULDBLOCK) // The queue has filled up!
-			ACE_ERROR ((LM_ERROR, "(%t) %p\n",
+                  if ((*connection_handler)->put (dup_msg) == -1)
+                    {
+                      if (errno == EWOULDBLOCK) // The queue has filled up!
+                        ACE_ERROR ((LM_ERROR, "(%t) %p\n",
                                     "gateway is flow controlled, so we're dropping events"));
-		      else
-			ACE_ERROR ((LM_ERROR,
+                      else
+                        ACE_ERROR ((LM_ERROR,
                                     "(%t) %p transmission error to peer %d\n",
                                     "put",
                                     (*connection_handler)->connection_id ()));
 
-		      // We are responsible for releasing an
-		      // ACE_Message_Block if failures occur.
-		      dup_msg->release ();
-		    }
-		}
-	    }
-	}
+                      // We are responsible for releasing an
+                      // ACE_Message_Block if failures occur.
+                      dup_msg->release ();
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -224,7 +224,7 @@ Event_Channel::initiate_connection_connection (Connection_Handler *connection_ha
     synch_options = ACE_Synch_Options::synch;
 
   return this->connector_.initiate_connection (connection_handler,
-					       synch_options);
+                                               synch_options);
 }
 
 int
@@ -235,9 +235,9 @@ Event_Channel::complete_connection_connection (Connection_Handler *connection_ha
 
   if (socket_queue_size > 0)
     if (connection_handler->peer ().set_option (SOL_SOCKET,
-					   option,
-					   &socket_queue_size,
-					   sizeof (int)) == -1)
+                                           option,
+                                           &socket_queue_size,
+                                           sizeof (int)) == -1)
       ACE_ERROR ((LM_ERROR, "(%t) %p\n", "set_option"));
 
   connection_handler->thr_mgr (ACE_Thread_Manager::instance ());
@@ -256,8 +256,8 @@ Event_Channel::complete_connection_connection (Connection_Handler *connection_ha
 
   if (n != sizeof id)
     ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n",
-		      n == 0 ? "peer has closed down unexpectedly" : "send"),
-		      -1);
+                      n == 0 ? "peer has closed down unexpectedly" : "send"),
+                      -1);
   return 0;
 }
 
@@ -275,14 +275,14 @@ Event_Channel::reinitiate_connection_connection (Connection_Handler *connection_
   if (connection_handler->state () != Connection_Handler::DISCONNECTING)
     {
       ACE_DEBUG ((LM_DEBUG,
-		  "(%t) scheduling reinitiation of Connection_Handler %d\n",
-		  connection_handler->connection_id ()));
+                  "(%t) scheduling reinitiation of Connection_Handler %d\n",
+                  connection_handler->connection_id ()));
 
       // Reschedule ourselves to try and connect again.
       if (ACE_Reactor::instance ()->schedule_timer
-	  (connection_handler, 0, connection_handler->timeout ()) == -1)
-	ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n",
-			   "schedule_timer"), -1);
+          (connection_handler, 0, connection_handler->timeout ()) == -1)
+        ACE_ERROR_RETURN ((LM_ERROR, "(%t) %p\n",
+                           "schedule_timer"), -1);
     }
   return 0;
 }
@@ -329,10 +329,10 @@ Event_Channel::initiate_acceptors (void)
                        -1);
   else
     ACE_DEBUG ((LM_DEBUG,
-                "accepting Consumers at %d\n", 
+                "accepting Consumers at %d\n",
                 Options::instance ()->consumer_acceptor_port ()));
 
-  if (Options::instance ()->enabled (Options::SUPPLIER_CONNECTOR)
+  if (Options::instance ()->enabled (Options::SUPPLIER_ACCEPTOR)
       && this->supplier_acceptor_.open
       (Options::instance ()->supplier_acceptor_port (),
        ACE_Reactor::instance (),
@@ -343,7 +343,7 @@ Event_Channel::initiate_acceptors (void)
                       -1);
   else
     ACE_DEBUG ((LM_DEBUG,
-                "accepting Suppliers at %d\n", 
+                "accepting Suppliers at %d\n",
                 Options::instance ()->supplier_acceptor_port ()));
 
   return 0;
@@ -358,7 +358,7 @@ Event_Channel::close (u_long)
   if (Options::instance ()->threading_strategy () != Options::REACTIVE)
     {
       if (ACE_Thread_Manager::instance ()->suspend_all () == -1)
-	ACE_ERROR_RETURN ((LM_ERROR,
+        ACE_ERROR_RETURN ((LM_ERROR,
                            "(%t) %p\n",
                            "suspend_all"),
                           -1);
@@ -373,18 +373,18 @@ Event_Channel::close (u_long)
     // Iterate over all the handlers and shut them down.
 
     for (CONNECTION_MAP_ENTRY *me;
-	 cmi.next (me) != 0;
-	 cmi.advance ())
+         cmi.next (me) != 0;
+         cmi.advance ())
       {
-	Connection_Handler *connection_handler = me->int_id_;
+        Connection_Handler *connection_handler = me->int_id_;
 
-	ACE_DEBUG ((LM_DEBUG,
+        ACE_DEBUG ((LM_DEBUG,
                     "(%t) closing down connection %d\n",
-		    connection_handler->connection_id ()));
+                    connection_handler->connection_id ()));
 
-	// Mark Connection_Handler as DISCONNECTING so we don't try to
-	// reconnect...
-	connection_handler->state (Connection_Handler::DISCONNECTING);
+        // Mark Connection_Handler as DISCONNECTING so we don't try to
+        // reconnect...
+        connection_handler->state (Connection_Handler::DISCONNECTING);
       }
   }
 
@@ -402,13 +402,13 @@ Event_Channel::close (u_long)
     CONNECTION_MAP_ITERATOR cmi (this->connection_map_);
 
     for (CONNECTION_MAP_ENTRY *me;
-	 cmi.next (me) != 0;
-	 cmi.advance ())
+         cmi.next (me) != 0;
+         cmi.advance ())
       {
-	Connection_Handler *connection_handler = me->int_id_;
+        Connection_Handler *connection_handler = me->int_id_;
 
-	// Deallocate Connection_Handler resources.
-	connection_handler->destroy (); // Will trigger a delete.
+        // Deallocate Connection_Handler resources.
+        connection_handler->destroy (); // Will trigger a delete.
       }
   }
 
@@ -417,7 +417,7 @@ Event_Channel::close (u_long)
 
 int
 Event_Channel::find_proxy (ACE_INT32 connection_id,
-			       Connection_Handler *&connection_handler)
+                               Connection_Handler *&connection_handler)
 {
   return this->connection_map_.find (connection_id,
                                      connection_handler);
@@ -433,14 +433,14 @@ Event_Channel::bind_proxy (Connection_Handler *connection_handler)
     {
     case -1:
       ACE_ERROR_RETURN ((LM_ERROR,
-			 "(%t) bind failed for connection %d\n",
-			 connection_handler->connection_id ()),
+                         "(%t) bind failed for connection %d\n",
+                         connection_handler->connection_id ()),
                         -1);
       /* NOTREACHED */
     case 1: // Oops, found a duplicate!
       ACE_ERROR_RETURN ((LM_ERROR,
-			 "(%t) duplicate connection %d, already bound\n",
-			 connection_handler->connection_id ()),
+                         "(%t) duplicate connection %d, already bound\n",
+                         connection_handler->connection_id ()),
                         -1);
       /* NOTREACHED */
     case 0:
@@ -449,7 +449,7 @@ Event_Channel::bind_proxy (Connection_Handler *connection_handler)
       /* NOTREACHED */
     default:
       ACE_ERROR_RETURN ((LM_DEBUG,
-			 "(%t) invalid result %d\n",
+                         "(%t) invalid result %d\n",
                          result),
                         -1);
       /* NOTREACHED */
@@ -469,14 +469,14 @@ Event_Channel::subscribe (const Event_Key &event_addr,
     {
     case -1:
       ACE_ERROR_RETURN ((LM_ERROR,
-			 "(%t) bind failed for connection %d\n",
-			 event_addr.connection_id_),
+                         "(%t) bind failed for connection %d\n",
+                         event_addr.connection_id_),
                         -1);
       /* NOTREACHED */
     case 1: // Oops, found a duplicate!
       ACE_ERROR_RETURN ((LM_DEBUG,
-			 "(%t) duplicate consumer map entry %d, "
-			 "already bound\n",
+                         "(%t) duplicate consumer map entry %d, "
+                         "already bound\n",
                          event_addr.connection_id_),
                         -1);
       /* NOTREACHED */
@@ -485,7 +485,7 @@ Event_Channel::subscribe (const Event_Key &event_addr,
       return 0;
     default:
       ACE_ERROR_RETURN ((LM_DEBUG,
-			 "(%t) invalid result %d\n",
+                         "(%t) invalid result %d\n",
                          result),
                         -1);
       /* NOTREACHED */
