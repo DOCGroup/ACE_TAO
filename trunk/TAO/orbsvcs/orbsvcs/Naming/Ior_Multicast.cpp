46b79b7954e635aea358c740c3abd49a6c1620f2
sbw1 <sbw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-18 22:38:45 +0000

    Changed these files to use the new multicast protocol.

--- trunk/TAO/orbsvcs/orbsvcs/Naming/Ior_Multicast.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Naming/Ior_Multicast.cpp
@@ -16,11 +16,13 @@ IOR_Multicast::get_handle (void) const
 
 IOR_Multicast::IOR_Multicast (char * ior,
 			      u_short port, 
-			      const char *mcast_addr)
+			      const char *mcast_addr,
+			      TAO_Service_ID service_id)
   : mcast_addr_ (port, mcast_addr),
     ior_ (ior),
     response_addr_ ((u_short) 0),
-    response_ (response_addr_)
+    response_ (response_addr_),
+    service_id_ (service_id)
 {
   // Use ACE_SOCK_Dgram_Mcast factory to subscribe to multicast group.
   if (this->mcast_dgram_.subscribe (this->mcast_addr_) == -1)
@@ -44,9 +46,15 @@ IOR_Multicast::handle_timeout (const ACE_Time_Value &,
 int
 IOR_Multicast::handle_input (ACE_HANDLE)
 {
+  struct
+  {
+    u_short reply_port;
+    CORBA::Short service_id;
+  } mcast_info;	  
+
   ssize_t retcode = 
-    this->mcast_dgram_.recv (this->buf_, 
-			     BUFSIZ, 
+    this->mcast_dgram_.recv (&mcast_info,
+			     sizeof (mcast_info), 
 			     this->remote_addr_);
   
   if (retcode == -1)
@@ -57,35 +65,43 @@ IOR_Multicast::handle_input (ACE_HANDLE)
   // @@ validate data string received is from a valid client here
   // @@ Probably not needed
 
-  if (retcode != sizeof (CORBA::Short))
+  if (retcode != sizeof (mcast_info))
     ACE_ERROR_RETURN ((LM_ERROR, 
                        "Reply to multicast not sent. Received %d bytes, expected %d.", 
                        retcode, 
                        sizeof (CORBA::Short)),
                       -1);
 
-  // Convert port number received to network byte order.
-  CORBA::Short reply_port_number = ntohs (*(CORBA::Short *)this->buf_);
+  // Confirm that we were meant to respond to this request.
+  mcast_info.service_id = ntohs (mcast_info.service_id);
+  if (mcast_info.service_id == this->service_id_)
+    {
+      // Convert port number received to network byte order and set port
+      // number to reply;
+      mcast_info.reply_port = ntohs (mcast_info.reply_port);
+      this->remote_addr_.set_port_number (mcast_info.reply_port);
 
-  // set port number to reply.
-  this->remote_addr_.set_port_number (reply_port_number);
-  
-  // send the object reference for the naming service
-  retcode = response_.send (this->ior_, 
-			    ACE_OS::strlen (this->ior_) + 1, 
-			    this->remote_addr_, 
-			    0);
-
-  ACE_DEBUG ((LM_DEBUG, 
-	      "(%P|%t) ior_: <%s>\n"
-	      " sent through port %u.\n"
-	      "retcode=%d\n", 
-	      this->ior_, 
-	      this->remote_addr_.get_port_number (), 
-	      retcode));
-
-  if (retcode == -1)
-    return -1;  
+      // send the object reference for the naming service
+      retcode = response_.send (this->ior_, 
+				ACE_OS::strlen (this->ior_) + 1, 
+				this->remote_addr_, 
+				0);
+      
+      ACE_DEBUG ((LM_DEBUG, 
+		  "(%P|%t) ior_: <%s>\n"
+		  " sent through port %u.\n"
+		  "retcode=%d\n", 
+		  this->ior_, 
+		  this->remote_addr_.get_port_number (), 
+		  retcode));
+      
+      if (retcode == -1)
+	return -1;
+    }
+  else
+    {
+      ACE_DEBUG ((LM_DEBUG, "(%P|%t) Multicast was not for us."));
+    }
 
   return 0;
 }
