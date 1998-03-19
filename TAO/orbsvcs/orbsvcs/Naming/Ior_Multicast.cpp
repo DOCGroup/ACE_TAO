9589f45c500d28f69087767d39098e3e341fd8d1
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-19 03:24:53 +0000

    (IOR_Multicast): reordered initializers to match declaration order

--- TAO/orbsvcs/orbsvcs/Naming/Ior_Multicast.cpp
+++ TAO/orbsvcs/orbsvcs/Naming/Ior_Multicast.cpp
@@ -15,25 +15,25 @@ IOR_Multicast::get_handle (void) const
 }
 
 IOR_Multicast::IOR_Multicast (char * ior,
-			      u_short port, 
-			      const char *mcast_addr,
-			      TAO_Service_ID service_id)
-  : mcast_addr_ (port, mcast_addr),
+                              u_short port,
+                              const char *mcast_addr,
+                              TAO_Service_ID service_id)
+  : service_id_ (service_id),
+    mcast_addr_ (port, mcast_addr),
     ior_ (ior),
     response_addr_ ((u_short) 0),
-    response_ (response_addr_),
-    service_id_ (service_id)
+    response_ (response_addr_)
 {
   // Use ACE_SOCK_Dgram_Mcast factory to subscribe to multicast group.
   if (this->mcast_dgram_.subscribe (this->mcast_addr_) == -1)
     ACE_ERROR ((LM_ERROR, "%p\n", "subscribe"));
 }
 
-// destructor 
+// destructor
 
 IOR_Multicast::~IOR_Multicast (void)
 {
-  this->mcast_dgram_.unsubscribe ();    
+  this->mcast_dgram_.unsubscribe ();
 }
 
 int
@@ -50,25 +50,25 @@ IOR_Multicast::handle_input (ACE_HANDLE)
   {
     u_short reply_port;
     CORBA::Short service_id;
-  } mcast_info;	  
+  } mcast_info;
 
-  ssize_t retcode = 
+  ssize_t retcode =
     this->mcast_dgram_.recv (&mcast_info,
-			     sizeof (mcast_info), 
-			     this->remote_addr_);
-  
+                             sizeof (mcast_info),
+                             this->remote_addr_);
+
   if (retcode == -1)
     return -1;
 
   ACE_DEBUG ((LM_DEBUG, "(%P|%t) Received multicast.\n"));
-  
+
   // @@ validate data string received is from a valid client here
   // @@ Probably not needed
 
   if (retcode != sizeof (mcast_info))
-    ACE_ERROR_RETURN ((LM_ERROR, 
-                       "Reply to multicast not sent. Received %d bytes, expected %d.", 
-                       retcode, 
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "Reply to multicast not sent. Received %d bytes, expected %d.",
+                       retcode,
                        sizeof (CORBA::Short)),
                       -1);
 
@@ -82,21 +82,21 @@ IOR_Multicast::handle_input (ACE_HANDLE)
       this->remote_addr_.set_port_number (mcast_info.reply_port);
 
       // send the object reference for the naming service
-      retcode = response_.send (this->ior_, 
-				ACE_OS::strlen (this->ior_) + 1, 
-				this->remote_addr_, 
-				0);
-      
-      ACE_DEBUG ((LM_DEBUG, 
-		  "(%P|%t) ior_: <%s>\n"
-		  " sent through port %u.\n"
-		  "retcode=%d\n", 
-		  this->ior_, 
-		  this->remote_addr_.get_port_number (), 
-		  retcode));
-      
+      retcode = response_.send (this->ior_,
+                                ACE_OS::strlen (this->ior_) + 1,
+                                this->remote_addr_,
+                                0);
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "(%P|%t) ior_: <%s>\n"
+                  " sent through port %u.\n"
+                  "retcode=%d\n",
+                  this->ior_,
+                  this->remote_addr_.get_port_number (),
+                  retcode));
+
       if (retcode == -1)
-	return -1;
+        return -1;
     }
   else
     {
@@ -105,5 +105,3 @@ IOR_Multicast::handle_input (ACE_HANDLE)
 
   return 0;
 }
-
-
