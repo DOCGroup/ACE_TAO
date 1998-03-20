8b81392e4fc98631158820965bc0dcc18252a4dd
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-20 02:10:53 +0000

    *** empty log message ***

--- TAO/orbsvcs/tests/AVStreams/sfp/client.cpp
+++ TAO/orbsvcs/tests/AVStreams/sfp/client.cpp
@@ -3,29 +3,46 @@
 #include "ace/Message_Block.h"
 #include "orbsvcs/AV/sfp.h"
 
-const char *buf = "Hello World\n";
+const char *buf = "Hello Worldasdasda sda sdasd adasdaddd\n";
 
 main ()
 {
-  ACE_Message_Block *message;
-
-  ACE_NEW_RETURN (message,
-                  ACE_Message_Block (buf),
-                  -1);
+  
+  // ----------------------------------------------------------------------
+  // start message
 
   SFP_Encoder encoder;
 
   ACE_Message_Block *start_message =
     encoder.encode_start_message ();
 
-  //  for (int i = 0; i < start_message->length (); i++)
-  //    cerr << i << (start_message->rd_ptr ())[i] << endl;
-
   SFP_Decoder decoder;
 
   decoder.decode_start_message (start_message);
   
-  //  encoder.encode_simple_frame (message);
+  // ----------------------------------------------------------------------
+  // start reply
+
+  ACE_Message_Block *start_reply_message =
+    encoder.encode_start_reply_message ();
+
+  decoder.decode_start_reply_message (start_reply_message);
+  
+
+  // ----------------------------------------------------------------------
+  // simple frame
+
+  ACE_Message_Block *message;
+
+  ACE_NEW_RETURN (message,
+                  ACE_Message_Block (buf),
+                  -1);
+
+  message->length (::strlen (buf));
+
+  ACE_Message_Block *mb = encoder.encode_simple_frame (message);
+
+  decoder.decode_simple_frame (mb);
 
   return 0;
 }
