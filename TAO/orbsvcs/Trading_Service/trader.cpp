f9ddb16a24e87e2957204749fcfe1d0a6434c976
sbw1 <sbw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-21 05:22:09 +0000

    Changed trader.cpp to use Ior_Multicast to respond to resolve_initial_references("TradingService") calls.

--- TAO/orbsvcs/Trading_Service/trader.cpp
+++ TAO/orbsvcs/Trading_Service/trader.cpp
@@ -15,7 +15,9 @@
 // ======================================================================= 
 
 #include "tao/corba.h"
+#include "tao/orbobj.h"
 #include "orbsvcs/CosNamingC.h"
+#include "orbsvcs/Naming/Ior_Multicast.h"
 #include "orbsvcs/Trader/Trader.h"
 #include "orbsvcs/Trader/Service_Type_Repository.h"
 
@@ -48,18 +50,6 @@ int main(int argc, char * const *argv)
 	root_poa->the_POAManager (TAO_TRY_ENV);
       TAO_CHECK_ENV;
 
-      CORBA::Object_var naming_obj =
-	orb->resolve_initial_references ("NameService");
-      if (CORBA::is_nil (naming_obj.in ()))
-	ACE_ERROR_RETURN ((LM_ERROR,
-			   " (%P|%t) Unable to initialize the POA.\n"),
-			  1);
-
-      CosNaming::NamingContext_var naming_context = 
-        CosNaming::NamingContext::_narrow (naming_obj.in (), TAO_TRY_ENV);
-      TAO_CHECK_ENV;
-
-      
       // Create a Service Type Repository and a Trader Object.
       TYPE_REPOS type_repos;
       TRADER trader ((TRADER::Trader_Components)
@@ -70,15 +60,52 @@ int main(int argc, char * const *argv)
       // Set the service type repository
       sup_attr.type_repos (type_repos._this (TAO_TRY_ENV));
       TAO_CHECK_ENV;
-
-      CosTrading::Lookup_var lookup = trd_comp.lookup_if ();
       
-      CosNaming::Name trading_name (1);
-      trading_name.length (1);
-      trading_name[0].id = CORBA::string_dup (service_name);
-      naming_context->bind (trading_name, lookup.in (), TAO_TRY_ENV);
+#if defined (ACE_HAS_IP_MULTICAST)
+      // Get reactor instance from TAO.
+      ACE_Reactor *reactor =
+        TAO_ORB_Core_instance ()->reactor ();
+      
+      // First, see if the user has given us a multicast port number
+      // for the name service on the command-line;
+      u_short port =
+        TAO_ORB_Core_instance ()->orb_params ()->name_service_port ();
+
+      if (port == 0)
+	{
+	  const char *port_number =
+            ACE_OS::getenv ("NameServicePort");
+
+	  if (port_number != 0)
+	    port = ACE_OS::atoi (port_number);
+	  else
+	    port = TAO_DEFAULT_TRADING_SERVER_REQUEST_PORT;
+	}
+
+      // The Spec says: return a reference to the Lookup interface
+      // from the resolve_initial_references method.
+      CosTrading::Lookup_var lookup = trd_comp.lookup_if ();
+      CORBA::String_var trading_ior =
+	orb->object_to_string (lookup.in (), TAO_TRY_ENV);
       TAO_CHECK_ENV;
 
+      // Instantiate a server which will receive requests for an ior
+      IOR_Multicast ior_multicast ((char *) trading_ior.in (),
+				   port,
+				   ACE_DEFAULT_MULTICAST_ADDR,
+				   TAO_SERVICEID_TRADINGSERVICE);
+      
+      // Register event handler for the ior multicast.
+      if (reactor->register_handler (&ior_multicast,
+				     ACE_Event_Handler::READ_MASK) == -1)
+	ACE_DEBUG ((LM_DEBUG,
+                    "cannot register Event handler\n"));
+      else
+	ACE_DEBUG ((LM_DEBUG,
+                    "The multicast server setup is done.\n"));
+#endif /* ACE_HAS_IP_MULTICAST */
+
+      
       poa_manager->activate (TAO_TRY_ENV);
       TAO_CHECK_ENV;
 
