6d4b0faad001928b3908464119028882018bd8ec
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-03-22 02:23:39 +0000

    f

--- ace/Naming_Context.cpp
+++ ace/Naming_Context.cpp
@@ -10,8 +10,10 @@
 
 // Make life easier later on...
 
-typedef ACE_Local_Name_Space <ACE_MMAP_MEMORY_POOL, ACE_RW_Process_Mutex> LOCAL_NAME_SPACE;
-typedef ACE_Local_Name_Space <ACE_LITE_MMAP_MEMORY_POOL, ACE_RW_Process_Mutex> LITE_LOCAL_NAME_SPACE;
+typedef ACE_Local_Name_Space <ACE_MMAP_MEMORY_POOL, ACE_RW_Process_Mutex> 
+        LOCAL_NAME_SPACE;
+typedef ACE_Local_Name_Space <ACE_LITE_MMAP_MEMORY_POOL, ACE_RW_Process_Mutex> 
+        LITE_LOCAL_NAME_SPACE;
 
 // The following Factory is used by the ACE_Service_Config and
 // svc.conf file to dynamically initialize the state of the Name
