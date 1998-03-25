c339d418d2d1ab81bb4b680da0c5337b1d52ed2f
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-25 02:23:14 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/tests/AVStreams/mpeg/source/mpeg_shared/Video_Control_i.cpp
+++ trunk/TAO/orbsvcs/tests/AVStreams/mpeg/source/mpeg_shared/Video_Control_i.cpp
@@ -1,4 +1,95 @@
 // $Id$
 
-#include "Video_Control_i.h
+#include "Video_Control_i.h"
 
+Video_Control_i::Video_Control_i (void)
+{
+
+}
+
+CORBA::Boolean 
+init_video (const Video_Control::INITvideoPara &para,
+            CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+stat_stream (CORBA::Char_out ch,
+             CORBA::Long_out size,
+             CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+close (CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+stat_sent (CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+fast_forward (const Video_Control::FFpara &para,
+              CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+fast_backward (const Video_Control::FFpara &para,
+               CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+step (const Video_Control::STEPpara &para,
+      CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+play (const Video_Control::PLAYpara &para,
+      CORBA::Long_out vts,
+      CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+position (const Video_Control::POSITIONpara &para,
+          CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+speed (const Video_Control::SPEEDpara &para,
+       CORBA::Environment)
+{
+  return 0;
+}
+
+
+CORBA::Boolean 
+stop (CORBA::Long cmdsn,
+      CORBA::Environment)
+{
+  return 0;
+}
