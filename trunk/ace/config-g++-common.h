b0b6bd725e6333913293f59d1ea43e847b32b8f2
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-25 18:12:26 +0000

    don't #define ACE_HAS_TEMPLATE_SPECIALIZATION with egcs 2.91

--- trunk/ace/config-g++-common.h
+++ trunk/ace/config-g++-common.h
@@ -8,17 +8,7 @@
 #if !defined (ACE_GNUG_COMMON_H)
 #define ACE_GNUG_COMMON_H
 
-#if __GNUC__ > 2 || ( __GNUC__ == 2 && __GNUC_MINOR__ >= 90)
-  // egcs or really modern g++
-# define ACE_HAS_ANSI_CASTS
-# define ACE_HAS_CPLUSPLUS_HEADERS
-# define ACE_HAS_STDCPP_STL_INCLUDES
-# define ACE_HAS_TEMPLATE_TYPEDEFS
-# define ACE_HAS_TYPENAME_KEYWORD
-  // It has gets (), but warns about using it (on Linux) at link time.
-# define ACE_LACKS_GETS
-
-#elif __GNUC__ > 2 || ( __GNUC__ == 2 && __GNUC_MINOR__ >= 8)
+#if __GNUC__ > 2 || ( __GNUC__ == 2 && __GNUC_MINOR__ >= 8)
   // egcs or g++ >= 2.8.0
 
   // __EXCEPTIONS is defined with -fexceptions, the egcs default.  It
@@ -27,13 +17,29 @@
 #   define ACE_HAS_EXCEPTIONS
 # endif /* __EXCEPTIONS && ! ACE_HAS_EXCEPTIONS */
 
+# if __GNUC__ == 2 && __GNUC_MINOR__ >= 90
+    // egcs or really modern g++
+#   define ACE_HAS_ANSI_CASTS
+#   define ACE_HAS_CPLUSPLUS_HEADERS
+#   define ACE_HAS_STDCPP_STL_INCLUDES
+#   define ACE_HAS_TEMPLATE_TYPEDEFS
+#   define ACE_HAS_TYPENAME_KEYWORD
+    // It has gets (), but warns about using it (on Linux) at link time.
+#   define ACE_LACKS_GETS
+# endif /* __GNUC__ >= 2.90 */
+
+# if __GNUC__ == 2 && __GNUC_MINOR__ != 91
+#   // egcs 2.91 apparently has a bug with this . . .
+#   define ACE_HAS_TEMPLATE_SPECIALIZATION
+# endif /* __GNUC__ != 2.91 */
+
 #else  /* ! EGCS */
   // Plain old g++.
 # define ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES
+# define ACE_HAS_TEMPLATE_SPECIALIZATION
 #endif /* ! EGCS */
 
 #define ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION
-#define ACE_HAS_TEMPLATE_SPECIALIZATION
 #define ACE_TEMPLATES_REQUIRE_SOURCE
 
 #endif /* ACE_GNUG_COMMON_H */
