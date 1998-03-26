a55c1bf5cb0187ce8393f0eabcbbb42bfc109c74
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-03-26 16:37:02 +0000

    Thu Mar 26 10:07:02 1998  Michael Kircher  <mk1@cs.wustl.edu>

---
+++ trunk/TAO/examples/Simulator/DOVEMIB/AnyAnalyser.h
@@ -0,0 +1,53 @@
+// $Id$
+// ============================================================================
+//
+// = LIBRARY
+//
+// = FILENAME
+//    AnyAnalyser.h
+//
+// = AUTHOR
+//    Michael Kircher 
+//
+// = DESCRIPTION
+//   Accepts an CORBA::Any, traverses it, copies it into a tree structure
+//   and prints out the tree.
+//
+// ============================================================================
+
+
+#include "PrintVisitor.h"
+#include "tao/corba.h"
+
+#if !defined (ANYANALYSER_H)
+#define ANYANALYSER_H
+
+typedef enum {PARENT_IS_STRUCT, PARENT_IS_NO_STRUCT} KIND;
+
+typedef struct {
+          KIND kind;
+          CORBA::TypeCode_ptr parent_tc_ptr;
+          unsigned int member_number;
+          unsigned int recursion_level;
+} RecurseInfo;
+
+
+class AnyAnalyser {
+
+public:
+  AnyAnalyser (const char *filename);
+  
+  ~AnyAnalyser ();
+ 
+  // main starting point includes analysing and printing
+  void printAny (CORBA::Any any);
+  
+private:
+  Node *analyse (CORBA::TypeCode_ptr tc_ptr, 
+		             unsigned char *&value_ptr,
+                 RecurseInfo recurseInfo);
+
+  PrintVisitor *printVisitor_ptr_;
+};
+
+#endif
