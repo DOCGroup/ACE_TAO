1cc48eb4a4000afe7bf02f3071585710beade023
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-09 22:32:50 +0000

    *** empty log message ***

--- trunk/tests/Reactor_Exceptions_Test.cpp
+++ trunk/tests/Reactor_Exceptions_Test.cpp
@@ -9,8 +9,8 @@
 //    Reactor_Exceptions_Test.cpp
 //
 // = DESCRIPTION
-//      This is a test that makes sure the ACE_Reactor works correctly
-//      in the face of C++ exceptions and threads.
+//      This is a test that makes sure the <ACE_Reactor> works
+//      correctly in the face of C++ exceptions and threads.
 //
 // = AUTHOR
 //    Luca Priorelli <lucapri@mbox.vol.it> and Douglas C. Schmidt
@@ -28,27 +28,28 @@
 // Just need a simple exception class.
 class Except {};
 
-class MemoryEx : public ACE_Event_Handler, public ACE_SOCK_Dgram
+class Memory_Exception : public ACE_Event_Handler, public ACE_SOCK_Dgram
 {
 public:
-  MemoryEx (const ACE_INET_Addr &local_addr);
+  Memory_Exception (const ACE_INET_Addr &local_addr);
+
   virtual ACE_HANDLE get_handle (void) const;
   virtual int handle_input (ACE_HANDLE handle);
 };
 
-MemoryEx::MemoryEx (const ACE_INET_Addr &local_addr)
+Memory_Exception::Memory_Exception (const ACE_INET_Addr &local_addr)
   : ACE_SOCK_Dgram (local_addr)
 {
 }
 
 ACE_HANDLE
-MemoryEx::get_handle (void) const
+Memory_Exception::get_handle (void) const
 {
   return ACE_SOCK_Dgram::get_handle ();
 }
 
 int
-MemoryEx::handle_input (ACE_HANDLE)
+Memory_Exception::handle_input (ACE_HANDLE)
 {
   char buf[BUFSIZ];
   ACE_INET_Addr from_addr;
@@ -68,8 +69,8 @@ MemoryEx::handle_input (ACE_HANDLE)
   *(char *) 0 = 0;
 #else
   throw Except ();
-#endif
-  ACE_NOTREACHED(return 0);
+#endif /* ACE_WIN32 */
+  ACE_NOTREACHED (return 0);
 }
 
 class My_Reactor : public ACE_Reactor
@@ -79,17 +80,18 @@ public:
   {
     int ret = 0;
     try
-    {
-      ret = ACE_Reactor::handle_events (max_wait_time);
-    }
+      {
+        ret = ACE_Reactor::handle_events (max_wait_time);
+      }
     catch (...)
-    {
-      ACE_DEBUG ((LM_DEBUG, "(%t) caught exception\n"));
-      ret = -1;
-      // do your thing, etc.
-    }
+      {
+        ACE_DEBUG ((LM_DEBUG, " (%t) caught exception\n"));
+        ret = -1;
+        // do your thing, etc.
+      }
     return ret;
   }
+
   virtual int handle_events (ACE_Time_Value &max_wait_time)
   {
     return this->handle_events (&max_wait_time);
@@ -99,13 +101,13 @@ public:
 static int
 worker (void)
 {
-  ACE_Reactor::instance()->owner (ACE_OS::thr_self());
+  ACE_Reactor::instance ()->owner (ACE_OS::thr_self ());
 
   for (;;)
-    if (ACE_Reactor::instance()->handle_events () == -1)
-      ACE_ERROR_RETURN ((LM_ERROR, "(%t) error return\n"), -1);
+    if (ACE_Reactor::instance ()->handle_events () == -1)
+      ACE_ERROR_RETURN ((LM_ERROR, " (%t) error return\n"), -1);
 
-  ACE_NOTREACHED(return 0);
+  ACE_NOTREACHED (return 0);
 }
 #endif /* ACE_HAS_EXCEPTIONS */
 
@@ -118,15 +120,17 @@ main (int argc, char *argv[])
   My_Reactor reactor;
 
   u_short port = argc > 1 ? ACE_OS::atoi (argv[1]) : ACE_DEFAULT_SERVER_PORT;
+
   ACE_DEBUG ((LM_DEBUG, "Starting tracing\n"));
   ACE_LOG_MSG->start_tracing ();
+
   ACE_Reactor::instance (&reactor);
   ACE_Thread_Manager *thr_mgr = ACE_Thread_Manager::instance ();
 
   ACE_INET_Addr local_addr (port);
   ACE_INET_Addr remote_addr (port,
                              ACE_DEFAULT_SERVER_HOST);
-  MemoryEx ex (local_addr);
+  Memory_Exception ex (local_addr);
 
   if (ACE_Reactor::instance ()->register_handler
       (&ex, ACE_Event_Handler::READ_MASK) == -1)
@@ -148,7 +152,7 @@ main (int argc, char *argv[])
   ex.close ();
   dgram.close ();
 
-  ACE_DEBUG ((LM_DEBUG, "(%t) exiting main\n"));
+  ACE_DEBUG ((LM_DEBUG, " (%t) exiting main\n"));
 #else
   ACE_UNUSED_ARG (argc);
   ACE_UNUSED_ARG (argv);
