94680b1f84b4327b89e52cfb9a4abf04defa2d51
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-09 22:33:26 +0000

    *** empty log message ***

--- apps/JAWS/server/PROTOTYPE/HTTP_10.cpp
+++ apps/JAWS/server/PROTOTYPE/HTTP_10.cpp
@@ -2,67 +2,20 @@
 
 #include "JAWS/HTTP_10.h"
 
-JAWS_HTTP_IO_Handler::JAWS_HTTP_IO_Handler (JAWS_IO *io)
-  : io_(io)
+JAWS_HTTP_10_Read_Task::JAWS_HTTP_10_Read_Task (void)
 {
 }
 
-JAWS_HTTP_IO_Handler::~JAWS_HTTP_IO_Handler (void)
+JAWS_HTTP_10_Read_Task::~JAWS_HTTP_10_Read_Task (void)
 {
 }
 
-void
-JAWS_HTTP_IO_Handler::accept_complete (void)
+int
+JAWS_HTTP_10_Read_Task::handle_put (JAWS_Data_Block *data, ACE_Time_Value *)
 {
-}
+  JAWS_Dispatch_Policy *policy = data->policy ();
+  JAWS_IO_Handler *handler = data->handler ();
+  JAWS_Pipeline_Handler *task = data->task ();
 
-void
-JAWS_HTTP_IO_Handler::accept_error (void)
-{
+  JAWS_IO *io = policy->io ();
 }
-
-void
-JAWS_HTTP_IO_Handler::read_complete (ACE_Message_Block &data)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::read_error (void)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::transmit_file_complete (void)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::transmit_file_error (int result)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::receive_file_complete (void)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::receive_file_error (int result)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::write_error (void)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::confirmation_message_complete (void)
-{
-}
-
-void
-JAWS_HTTP_IO_Handler::error_message_complete (void)
-{
-}
-
