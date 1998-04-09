02fe48ab899f42b20ba922fae4eaa534a4c63dee
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-09 22:31:44 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/tests/AVStreams/mpeg/source/server/vs.cpp
+++ trunk/TAO/orbsvcs/tests/AVStreams/mpeg/source/server/vs.cpp
@@ -16,22 +16,39 @@ main (int argc, char **argv)
                              TAO_TRY_ENV) == -1)
         ACE_ERROR_RETURN ((LM_ERROR,
                            "(%P|%t) Video_Server: init failed\n"),
-                          1);
-
-      char sem_str[BUFSIZ];
+                          -1);
+      
+      char sem_str [BUFSIZ];
       pid_t my_pid;
-
+      
       if ((my_pid = ACE_OS::getpid ()) == -1)
-        ACE_ERROR_RETURN ((LM_ERROR,"(%P|%t)Unable to get pid\n"),2);
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "(%P|%t) Unable to get my pid!\n"),
+                          -1);
+      
 
-
-      sprintf (sem_str,"%s%d","Video_Server_Semaphore",my_pid);
-      ACE_DEBUG ((LM_DEBUG,"(%P|%t)semaphore is %s\n",sem_str));
+      sprintf (sem_str,
+               "%s:%d",
+               "Video_Server_Semaphore",
+               my_pid);
+      ACE_DEBUG ((LM_DEBUG,
+                  "(%P|%t) semaphore is %s\n",
+                  sem_str));
       // Release the lock on which the server is waiting
-      ACE_Process_Semaphore semaphore (0,sem_str);
+      ACE_Process_Semaphore semaphore (0, // 0 means that the semaphore is
+                                          // initially locked
+                                       sem_str);
                                        
-      semaphore.release ();
-      ACE_DEBUG ((LM_DEBUG, "(%P|%t) %s:%d\n", __FILE__, __LINE__));
+      if (semaphore.release () == -1)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "(%P|%t) Error releasing semaphore %s: %p\n",
+                           sem_str,
+                           "semaphore.release ()"),
+                          -1);
+      ACE_DEBUG ((LM_DEBUG, 
+                  "(%P|%t) %s:%d\n", 
+                  __FILE__, 
+                  __LINE__));
       // Increment ACE_SV_Semaphore by one. Dijkstra's V operation,
       // Tannenbaums UP operation.
 
@@ -44,9 +61,9 @@ main (int argc, char **argv)
   TAO_CATCHANY
     {
       TAO_TRY_ENV.print_exception ("Exception");
-      return 3;
+      return -1;
     }
   TAO_ENDTRY;
   
-  return 0;
+  return 0
 }
