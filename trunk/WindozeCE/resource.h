f03661897b091d99562b387e3ac357e3b176a8a2
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-10 08:23:32 +0000

    New

---
+++ trunk/WindozeCE/resource.h
@@ -0,0 +1,21 @@
+// $Id$
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by WinAdapter.rc
+//
+#define IDD_WinAdapter_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_TEXT_DISP                   1001
+#define IDC_CMDLINE                     1005
+#define IDC_START                       1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
