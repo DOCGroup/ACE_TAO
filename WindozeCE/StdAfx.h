627dc0dd503ddf102f5d096d1c451e4f2519c84c
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-10 08:23:32 +0000

    New

---
+++ WindozeCE/StdAfx.h
@@ -0,0 +1,25 @@
+// $Id$
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__5088F84A_CE8B_11D1_9621_0040054CF0F0__INCLUDED_)
+#define AFX_STDAFX_H__5088F84A_CE8B_11D1_9621_0040054CF0F0__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__5088F84A_CE8B_11D1_9621_0040054CF0F0__INCLUDED_)
