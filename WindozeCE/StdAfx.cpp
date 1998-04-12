328a43f41cd797e8a4e45ba87a7b497034a35311
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-12 00:53:57 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_4_39'.

--- WindozeCE/StdAfx.cpp
+++
@@ -1,7 +0,0 @@
-// $Id$
-// stdafx.cpp : source file that includes just the standard includes
-//	WinAdapter.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
