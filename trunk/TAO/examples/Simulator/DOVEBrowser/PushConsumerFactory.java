707bd25628594e031591153b4b591d163e18be28
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-16 15:32:56 +0000

    Pretty printing.

--- trunk/TAO/examples/Simulator/DOVEBrowser/PushConsumerFactory.java
+++ trunk/TAO/examples/Simulator/DOVEBrowser/PushConsumerFactory.java
@@ -27,38 +27,42 @@ public class PushConsumerFactory {
   private Weapons weapons_;
 
 
-  public PushConsumerFactory (DataHandler dataHandler, String nameServiceIOR, java.applet.Applet applet)
+  public PushConsumerFactory (DataHandler dataHandler, 
+			      String nameServiceIOR, 
+			      String[] args,
+			      java.applet.Applet applet)
     {
-      try {      
-	dataHandler_ = dataHandler;
-
-	// if the DOVE Browser is running as an Applet
-	if (applet != null) {
-	  orb_ = org.omg.CORBA.ORB.init (applet, null);
-	}
-	else { // not running as an Applet, but as an normal Application
-	   orb_ = org.omg.CORBA.ORB.init ();
-	}
-	boa_ = orb_.BOA_init ();
-	
-	// Get the Naming Service initial reference
-	
-	// Name Service Lookup cannot be used when running as an Applet
-	if (nameServiceIOR == null && applet != null) {
-	  System.out.println (" Name Service Lookup cannot be used when running as an Applet! Quit!");
-	  System.exit (1);
-	}
-
-	if (nameServiceIOR == null) {  // only used when running via "java" or "vbj"     
-		System.out.println ("Using the lookup protocol!");
-		NS_Resolve ns_resolve_ = new NS_Resolve ();
+      try 
+	{      
+	  dataHandler_ = dataHandler;
+	  
+	  // if the DOVE Browser is running as an Applet
+	  if (applet != null) {
+	    orb_ = org.omg.CORBA.ORB.init (applet, null);
+	  }
+	  else { // not running as an Applet, but as an normal Application
+	    orb_ = org.omg.CORBA.ORB.init ();
+	  }
+	  boa_ = orb_.BOA_init ();
+	  
+	  // Get the Naming Service initial reference
+	  
+	  // Name Service Lookup cannot be used when running as an Applet
+	  if (nameServiceIOR == null && applet != null) {
+	    System.out.println (" Name Service Lookup cannot be used when running as an Applet! Quit!");
+	    System.exit (1);
+	  }
+	  
+	  if (nameServiceIOR == null) {  // only used when running via "java" or "vbj"     
+	    System.out.println ("Using the lookup protocol!");
+	    NS_Resolve ns_resolve_ = new NS_Resolve ();
 		naming_service_object_ = ns_resolve_.resolve_name_service (orb_);
-	}
-	else {
-	  System.out.println ("Using the following IOR: " + nameServiceIOR);
-	  naming_service_object_ = orb_.string_to_object (nameServiceIOR);
-	}
-
+	  }
+	  else {
+	    System.out.println ("Using the following IOR: " + nameServiceIOR);
+	    naming_service_object_ = orb_.string_to_object (nameServiceIOR);
+	  }
+	  
       } 
       catch(org.omg.CORBA.SystemException e) {
 	System.err.println ("PushConsumerFactory constructor: ORB and Name Service initialization");
@@ -66,7 +70,7 @@ public class PushConsumerFactory {
       }	
       
     }
-
+  
   public class Object_is_null_exception extends Exception 
   {
     Object_is_null_exception (String s)
@@ -84,7 +88,7 @@ public class PushConsumerFactory {
 	  // ScheduleService
 	  CosNaming.NamingContext naming_context_ = 
 	    CosNaming.NamingContextHelper.narrow (naming_service_object_);
-
+	  
 	  if (naming_context_ == null)
 	    {
 	      System.err.println ("The Naming Context is null");
@@ -93,52 +97,52 @@ public class PushConsumerFactory {
 	  System.out.println ("Reference to the Naming Service is ok.");
 
 	  // Get a reference for the EventService
-
+	  
 	  CosNaming.NameComponent[] ec_name_components_ = new CosNaming.NameComponent[1];
 	  ec_name_components_[0] = new CosNaming.NameComponent ("EventService","");
 	  org.omg.CORBA.Object event_channel_object_ = naming_context_.resolve (ec_name_components_);
-
+	  
 	  if (event_channel_object_ == null)
 	    {
 	      throw new Object_is_null_exception("EventService Object is null");
 	    }
-	 
+	  
        	  RtecEventChannelAdmin.EventChannel event_channel_ = 
 	    RtecEventChannelAdmin.EventChannelHelper.narrow (event_channel_object_);
-
+	  
 	  System.out.println ("Reference to the Event Service is ok.");
-
+	  
 	  // Get a reference for the ScheduleService
-
+	  
 	  CosNaming.NameComponent[] s_name_components_ = new CosNaming.NameComponent[1];
 	  s_name_components_[0] = new CosNaming.NameComponent ("ScheduleService","");
        	  org.omg.CORBA.Object scheduler_object_ = naming_context_.resolve (s_name_components_); 
-
+	  
 	  if (scheduler_object_ == null)
 	    {
 	      throw new Object_is_null_exception("ScheduleService Object is null");
 	    }
-	 
+	  
 	  RtecScheduler.Scheduler scheduler_ = 
 	    RtecScheduler.SchedulerHelper.narrow (scheduler_object_);
-
+	  
 	  System.out.println ("Reference to the Naming Service is ok.");
-	 
- 
+	  
+	  
 	  // Start the consumer
 	  System.out.println ("Instantiating the Push Consumer.");
 	  PushConsumer pushConsumer_ = new PushConsumer (orb_, dataHandler_);
 	  System.out.println ("Initializing the Push Consumer.");
 	  pushConsumer_.open_consumer (event_channel_, scheduler_, "demo_consumer");
-	 
+	  
 	  // Tell the CORBA environment that we are ready
-
+	  
 	  boa_.obj_is_ready (pushConsumer_);
 	  
 	  System.out.println ("boa.obj_is_ready succeeded"); 
-
+	  
 	  boa_.impl_is_ready ();
-      }
+	}
       catch (CosNaming.NamingContextPackage.CannotProceed e)
 	{
 	  System.err.println ("CosNaming.NamingContextPackage.CannotProceed");
