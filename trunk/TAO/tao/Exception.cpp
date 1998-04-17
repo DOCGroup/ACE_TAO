e429a1ab1435a51bdcc4d889f6238a567c5510e0
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-17 03:59:54 +0000

    added ACE_Node and ACE_Unbounded_Queue_Iterator instantiations

--- trunk/TAO/tao/Exception.cpp
+++ trunk/TAO/tao/Exception.cpp
@@ -587,7 +587,11 @@ CORBA_ExceptionList::remove (CORBA::ULong index, CORBA::Environment &env)
 }
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class ACE_Node<CORBA::TypeCode_ptr>;
 template class ACE_Unbounded_Queue<CORBA::TypeCode_ptr>;
+template class ACE_Unbounded_Queue_Iterator<CORBA::TypeCode_ptr>;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate ACE_Node<CORBA::TypeCode_ptr>
 #pragma instantiate ACE_Unbounded_Queue<CORBA::TypeCode_ptr>
+#pragma instantiate ACE_Unbounded_Queue_Iterator<CORBA::TypeCode_ptr>
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
