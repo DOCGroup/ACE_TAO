159c2b7b7146b11a44ec969737c300301a004e95
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-20 15:28:56 +0000

    (do_priority_inversion_test): protected declaration of context_switch for ACE_HAS_PRUSAGE_T or ACE_HAS_GETRUSAGE.

--- trunk/TAO/performance-tests/Cubit/TAO/MT_Cubit/client.cpp
+++ trunk/TAO/performance-tests/Cubit/TAO/MT_Cubit/client.cpp
@@ -45,13 +45,15 @@ do_priority_inversion_test (Task_State &ts)
 
   // stores the total number of context switches incurred by the
   // program while making CORBA requests
+#if defined (ACE_HAS_PRUSAGE_T) || defined (ACE_HAS_GETRUSAGE)
   u_int context_switch = 0;
+#endif /* ACE_HAS_PRUSAGE_T || ACE_HAS_GETRUSAGE */
   double util_task_duration = 0.0;
   double total_latency = 0.0;
   double total_latency_low = 0.0;
   double total_latency_high = 0.0;
   double total_util_task_duration = 0.0;
-  
+
   // Create the clients.
   Client high_priority_client (&ts);
   Client low_priority_client (&ts);
@@ -81,7 +83,7 @@ do_priority_inversion_test (Task_State &ts)
   timer_.start ();
   // execute one computation.
   util_thread.computation ();
-  timer_.stop (); 
+  timer_.stop ();
   timer_.elapsed_time (delta_t);
   // Store the time in milli-seconds.
   util_task_duration = delta_t.sec () * ACE_ONE_SECOND_IN_MSECS + (double)delta_t.usec () / ACE_ONE_SECOND_IN_MSECS;
@@ -89,7 +91,7 @@ do_priority_inversion_test (Task_State &ts)
 
 #if (ACE_LACKS_FLOATING_POINT)
   printf ("Utilization Computation time is %u usecs\n", util_task_duration);
-#else /* ACE_LACKS_FLOATING_POINT */  
+#else /* ACE_LACKS_FLOATING_POINT */
   printf ("Utilization Computation time is %f msecs\n", util_task_duration);
 #endif /* !ACE_LACKS_FLOATING_POINT */
 
@@ -120,7 +122,7 @@ do_priority_inversion_test (Task_State &ts)
   ACE_DEBUG ((LM_DEBUG,
               "Creating 1 client with high priority of %d\n",
               priority));
- 
+
   // Drop the priority, so that the priority of clients will increase
   // with increasing client number.
   for (i = 0; i < ts.thread_count_; i++)
@@ -131,8 +133,8 @@ do_priority_inversion_test (Task_State &ts)
   ACE_DEBUG ((LM_DEBUG,
               "Creating %d clients with low priority ranging from %d to %d\n",
               ts.thread_count_ - 1,
-	      priority,
-	      priority + ts.thread_count_ - 2));
+              priority,
+              priority + ts.thread_count_ - 2));
 
   for (i = 0; i < ts.thread_count_ - 1; i++)
     {
@@ -179,16 +181,16 @@ do_priority_inversion_test (Task_State &ts)
   // Add up the voluntary context switches & involuntary context switches
   context_switch = usage.pr_vctx + usage.pr_ictx - context_switch;
   ACE_OS::printf("Voluntary context switches=%d, Involuntary context switches=%d\n",
-		 usage.pr_vctx, 
-		 usage.pr_ictx);
-#elif defined (ACE_HAS_GETRUSAGE) 
+                 usage.pr_vctx,
+                 usage.pr_ictx);
+#elif defined (ACE_HAS_GETRUSAGE)
   timer_for_context_switch.stop ();
   timer_for_context_switch.get_rusage (usage);
   // Add up the voluntary context switches & involuntary context switches
   context_switch = usage.ru_nvcsw + usage.ru_nivcsw - context_switch;
   ACE_OS::printf("Voluntary context switches=%d, Involuntary context switches=%d\n",
-		 usage.ru_nvcsw, 
-		 usage.ru_nivcsw);
+                 usage.ru_nvcsw,
+                 usage.ru_nivcsw);
 #endif /* ACE_HAS_GETRUSAGE */
 
 
@@ -197,8 +199,8 @@ do_priority_inversion_test (Task_State &ts)
 #if defined (VXWORKS)
   ACE_OS::printf ("Test done.\n"
                   "High priority client latency : %d usec\n"
-                  "Low priority client latency : %d usec\n",		  
-		  high_priority_client.get_high_priority_latency (),
+                  "Low priority client latency : %d usec\n",
+                  high_priority_client.get_high_priority_latency (),
                   low_priority_client.get_low_priority_latency ());
 #elif defined (CHORUS)
   ACE_OS::printf ("Test done.\n"
@@ -213,49 +215,49 @@ do_priority_inversion_test (Task_State &ts)
   FILE *latency_file_handle = 0;
   char latency_file[BUFSIZ];
   char buffer[BUFSIZ];
-  
-  ACE_OS::sprintf (latency_file, 
-		   "cb__%d.txt", 
-		   ts.thread_count_);
 
-  ACE_OS::fprintf(stderr, 
-		  "--->Output file for latency data is \"%s\"\n",
-		  latency_file);
-  
+  ACE_OS::sprintf (latency_file,
+                   "cb__%d.txt",
+                   ts.thread_count_);
+
+  ACE_OS::fprintf(stderr,
+                  "--->Output file for latency data is \"%s\"\n",
+                  latency_file);
+
   latency_file_handle = ACE_OS::fopen (latency_file, "w");
-  
+
   // This loop visits each client.  start_count_ is the number of clients.
   for (u_int j = 0; j < ts.start_count_; j ++)
     {
-      ACE_OS::sprintf(buffer, 
-		      "%s #%d", 
-		      j==0? "High Priority": "Low Priority", 
-		      j);
+      ACE_OS::sprintf(buffer,
+                      "%s #%d",
+                      j==0? "High Priority": "Low Priority",
+                      j);
       // this loop visits each request latency from a client
       for (u_int i = 0; i < ts.loop_count_/ts.grain_; i ++)
-	{
-	  ACE_OS::sprintf(buffer+strlen(buffer), 
-			  "\t%u\n", 
-			  ts.global_jitter_array_[j][i]);
-	  fputs (buffer, latency_file_handle);
-	  buffer[0]=0;
-	}
+        {
+          ACE_OS::sprintf(buffer+strlen(buffer),
+                          "\t%u\n",
+                          ts.global_jitter_array_[j][i]);
+          fputs (buffer, latency_file_handle);
+          buffer[0]=0;
+        }
     }
-  
-  ACE_OS::fclose (latency_file_handle);  
+
+  ACE_OS::fclose (latency_file_handle);
 #else /* !CHORUS */
   ACE_DEBUG ((LM_DEBUG, "Test done.\n"
               "High priority client latency : %f msec, jitter: %f msec\n"
               "Low priority client latency : %f msec, jitter: %f msec\n"
-	      "# of context switches: %d, context_switch_time: %f msec\n"
-	      "total context switch time: %f msec\n",
+              "# of context switches: %d, context_switch_time: %f msec\n"
+              "total context switch time: %f msec\n",
               high_priority_client.get_high_priority_latency (),
               high_priority_client.get_high_priority_jitter (),
               low_priority_client.get_low_priority_latency (),
               low_priority_client.get_low_priority_jitter (),
-	      context_switch,
-	      csw/1000,
-	      csw * context_switch/1000 ));
+              context_switch,
+              csw/1000,
+              csw * context_switch/1000 ));
 #endif /* !VXWORKS && !CHORUS */
 
   // signal the utilization thread to finish with its work..
@@ -267,25 +269,25 @@ do_priority_inversion_test (Task_State &ts)
   // This loop visits each client.  start_count_ is the number of clients.
   for (j = 1; j < ts.start_count_; j ++)
       for (i = 0; i < ts.loop_count_/ts.grain_; i ++)
-	  total_latency_low += ts.global_jitter_array_[j][i];
+          total_latency_low += ts.global_jitter_array_[j][i];
 
   for (i = 0; i < ts.loop_count_/ts.grain_; i ++)
     total_latency_high += ts.global_jitter_array_[0][i];
 
   total_util_task_duration = util_task_duration * util_thread.get_number_of_computations ();
 
-  total_latency = total_latency_low + 
-    total_latency_high + 
+  total_latency = total_latency_low +
+    total_latency_high +
     total_util_task_duration;
 
   // Calc and print the CPU percentage. I add 0.5 to round to the
   // nearest integer before casting it to int.
   printf ("\t%% Low Priority CPU utilization: %d %%\n"
-	  "\t%% High Priority CPU utilization: %d %%\n"
-	  "\t%% IDLE time: %d %%\n",
-	  (int) (total_latency_low * 100 / total_latency + 0.5),
-	  (int) (total_latency_high * 100 / total_latency + 0.5),
-	  (int) (total_util_task_duration * 100 / total_latency + 0.5) );
+          "\t%% High Priority CPU utilization: %d %%\n"
+          "\t%% IDLE time: %d %%\n",
+          (int) (total_latency_low * 100 / total_latency + 0.5),
+          (int) (total_latency_high * 100 / total_latency + 0.5),
+          (int) (total_util_task_duration * 100 / total_latency + 0.5) );
 
 #if defined (ACE_LACKS_FLOATING_POINT)
   ACE_DEBUG ((LM_DEBUG,
@@ -413,10 +415,10 @@ main (int argc, char *argv [])
   // Initialize the PCC timer Chip
   pccTimerInit();
 
-  if(pccTimer(PCC2_TIMER1_START,&pTime) !=K_OK) 
-    { 
-      printf("pccTimer has a pending benchmark\n"); 
-    } 
+  if(pccTimer(PCC2_TIMER1_START,&pTime) !=K_OK)
+    {
+      printf("pccTimer has a pending benchmark\n");
+    }
 #endif /* CHORUS */
 
   if (ts.thread_per_rate_ == 0)
@@ -425,10 +427,10 @@ main (int argc, char *argv [])
     do_thread_per_rate_test (ts);
 
 #if defined (CHORUS)
-  if(pccTimer(PCC2_TIMER1_STOP,&pTime) !=K_OK) 
-    { 
-      printf("pccTimer has a pending bench mark\n"); 
-    } 
+  if(pccTimer(PCC2_TIMER1_STOP,&pTime) !=K_OK)
+    {
+      printf("pccTimer has a pending bench mark\n");
+    }
 #endif /* CHORUS */
 
 #else /* !ACE_HAS_THREADS */
