019120f651ffedb250ad8a17571eb1935b155558
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-04-23 19:00:26 +0000

    *** empty log message ***

--- apps/Gateway/Gateway/gatewayd.cpp
+++ apps/Gateway/Gateway/gatewayd.cpp
@@ -24,38 +24,38 @@
 int
 main (int argc, char *argv[])
 {
-  if (ACE_Service_Config::open (argc, argv) == -1)
+  if (ACE_OS::access (ACE_DEFAULT_SVC_CONF, F_OK) != 0) 
     {
-      if (errno != ENOENT)
-	ACE_ERROR ((LM_ERROR,
-                    "%p\n%a",
-                    "open",
-                    1));
-      else // Use static linking.
-	{
-	  ACE_Service_Object_Ptr sp = ACE_SVC_INVOKE (Gateway);
+      // Use static linking.
+      ACE_Service_Object_Ptr sp = ACE_SVC_INVOKE (Gateway);
 
-	  if (sp->init (argc - 1, argv + 1) == -1)
-	    ACE_ERROR ((LM_ERROR,
-                        "%p\n%a",
-                        "init",
-                        1));
-	  
-	  // Run forever, performing the configured services until we
-	  // are shut down by a SIGINT/SIGQUIT signal.
+      if (sp->init (argc - 1, argv + 1) == -1)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "%p\n",
+                           "init"),
+                          1);
 
-	  ACE_Reactor::run_event_loop ();
+      // Run forever, performing the configured services until we
+      // are shut down by a SIGINT/SIGQUIT signal.
 
-	  // Destructor of <ACE_Service_Object_Ptr> automagically call
-	  // <fini>.
-	}
+      ACE_Reactor::run_event_loop ();
+
+      // Destructor of <ACE_Service_Object_Ptr> automagically call
+      // <fini>.
     }
-  else // Use dynamic linking.
+  else 
+    {
+      if (ACE_Service_Config::open (argc, argv) == -1) 
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "%p\n",
+                           "open"),
+                          1);
+      else // Use dynamic linking.
 
-    // Run forever, performing the configured services until we are
-    // shut down by a signal (e.g., SIGINT or SIGQUIT).
+        // Run forever, performing the configured services until we are
+        // shut down by a signal (e.g., SIGINT or SIGQUIT).
 
-    ACE_Reactor::run_event_loop ();
-
+        ACE_Reactor::run_event_loop ();
+    }
   return 0;
 }
--- apps/Gateway/Peer/peerd.cpp
+++ apps/Gateway/Peer/peerd.cpp
@@ -23,38 +23,38 @@
 int
 main (int argc, char *argv[])
 {
-  if (ACE_Service_Config::open (argc, argv) == -1)
+  if (ACE_OS::access (ACE_DEFAULT_SVC_CONF, F_OK) != 0) 
     {
-      if (errno != ENOENT)
-	ACE_ERROR_RETURN ((LM_ERROR,
+      // Use static linking.
+      ACE_Service_Object_Ptr sp = ACE_SVC_INVOKE (Peer_Factory);
+
+      if (sp->init (argc - 1, argv + 1) == -1)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "%p\n",
+                           "init"),
+                          1);
+
+      // Run forever, performing the configured services until we are
+      // shut down by a SIGINT/SIGQUIT signal.
+
+      ACE_Reactor::run_event_loop ();
+
+      // Destructor of <ACE_Service_Object_Ptr> automagically call
+      // <fini>.
+    }
+  else 
+    {
+      if (ACE_Service_Config::open (argc, argv) == -1) 
+        ACE_ERROR_RETURN ((LM_ERROR,
                            "%p\n",
                            "open"),
                           1);
-      else // Use static linking.
-	{
-	  ACE_Service_Object_Ptr sp = ACE_SVC_INVOKE (Peer_Factory);
-	  
-	  if (sp->init (argc - 1, argv + 1) == -1)
-	    ACE_ERROR_RETURN ((LM_ERROR,
-                               "%p\n",
-                               "init"),
-                              1);
-	  
-	  // Run forever, performing the configured services until we
-	  // are shut down by a SIGINT/SIGQUIT signal.
+      else // Use dynamic linking.
 
-	  ACE_Reactor::run_event_loop ();
+        // Run forever, performing the configured services until we
+        // are shut down by a signal (e.g., SIGINT or SIGQUIT).
 
-	  // Destructor of <ACE_Service_Object_Ptr> automagically call
-	  // <fini>.
-	}
+        ACE_Reactor::run_event_loop ();
     }
-  else // Use dynamic linking.
-
-    // Run forever, performing the configured services until we are
-    // shut down by a signal (e.g., SIGINT or SIGQUIT).
-    
-    ACE_Reactor::run_event_loop ();
-
   return 0;
 }
