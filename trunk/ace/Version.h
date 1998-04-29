4e951731cabd4403526a58d5da080c82daf4b6c4
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-29 23:55:30 +0000

    4.5

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -2,6 +2,6 @@
 // This is an automatically generated file.
 
 #define ACE_MAJOR_VERSION (4u)
-#define ACE_MINOR_VERSION (4u)
-#define ACE_BETA_VERSION (43u)
-#define ACE_VERSION "4.4.43\0"
+#define ACE_MINOR_VERSION (5u)
+#define ACE_BETA_VERSION (0u)
+#define ACE_VERSION "4.5\0"
