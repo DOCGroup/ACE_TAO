80585e8eb5f9bdda339ee64b67d4582c96951ca8
sumedh <sumedh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-04-30 19:44:56 +0000

    CVS: CVS:

--- trunk/TAO/orbsvcs/orbsvcs/AV/Endpoint_Strategy.h
+++ trunk/TAO/orbsvcs/orbsvcs/AV/Endpoint_Strategy.h
@@ -24,25 +24,33 @@
 
 class TAO_ORBSVCS_Export TAO_AV_Endpoint_Strategy
 // = DESCRIPTION
-//    Base class to define various strategies
+//    Base class to define various endpoint strategies
 //    used by the MMDevice to create the Endpoint and Vdev
 {
 public:
   TAO_AV_Endpoint_Strategy (void);
+  // Constructor
   
   virtual int create_A (AVStreams::StreamEndPoint_A_ptr &stream_endpoint,
                         AVStreams::VDev_ptr &vdev,
                         CORBA::Environment &env);
+  // Called by the MMDevice, when it needs to create an A type endpoint
 
   virtual int create_B (AVStreams::StreamEndPoint_B_ptr &stream_endpoint,
                         AVStreams::VDev_ptr &vdev,
                         CORBA::Environment &env);
+  // Called by the MMDevice, when it needs to create an B type endpoint
 
  protected:
-  //  TAO_AV_Endpoint_Factory *endpoint_factory_;
   AVStreams::StreamEndPoint_A_ptr stream_endpoint_a_;
+  // The "A" stream endpoint
+
   AVStreams::StreamEndPoint_B_ptr stream_endpoint_b_;
+  // The "B" stream endpoint
+
   AVStreams::VDev_ptr vdev_;
+  // The vdev
+
 };
 
 // ----------------------------------------------------------------------
@@ -50,21 +58,37 @@ public:
 class TAO_ORBSVCS_Export TAO_AV_Endpoint_Process_Strategy
   : public virtual TAO_AV_Endpoint_Strategy
 // = DESCRIPTION
-//    Process-based strategy
+//    Process-based strategy for creating endpoints
+//    Abstract base class.
 {
  public:
   TAO_AV_Endpoint_Process_Strategy (ACE_Process_Options *process_options);
+  // Constructor. The process_options contain the name and arguments
+  // for the process to be created
+
   virtual int activate (void);
+  // creates a new child process, and waits on a semaphore
+  // until the child process has finished creating the endpoints
 
  protected:
   virtual int bind_to_naming_service (CORBA::Environment &env);
+  // Bind to the naming service
+
   virtual int get_stream_endpoint (CORBA::Environment &env) = 0;
+  // Get the object reference for the newly created stream
+  // endpoint (which will be in the child process)
+  // Subclasses will define the functionality for this
+
   virtual int get_vdev (CORBA::Environment &env);
+  // Get the Vdev object reference for the newly created
+  // endpoint 
+
   CosNaming::NamingContext_var naming_context_;
+  // Naming context
 
  private:
   ACE_Process_Options *process_options_;
-
+  // Name and arguments for the process to be created
 };  
 
 
@@ -73,17 +97,20 @@ class TAO_ORBSVCS_Export TAO_AV_Endpoint_Process_Strategy
 class TAO_ORBSVCS_Export TAO_AV_Endpoint_Process_Strategy_A
   : public virtual TAO_AV_Endpoint_Process_Strategy
 // = DESCRIPTION
-//    Process-based strategy
+//    Process-based strategy to create "A" type endpoints
 {
  public:
   TAO_AV_Endpoint_Process_Strategy_A (ACE_Process_Options *process_options);
+  // Constructor
 
  protected:  
   virtual int create_A (AVStreams::StreamEndPoint_A_ptr &stream_endpoint,
                         AVStreams::VDev_ptr &vdev,
                         CORBA::Environment &env);
+  // Creates an "A" type stream endpoint, and a vdev
 
   virtual int get_stream_endpoint (CORBA::Environment &env);
+  // Gets the "A" type stream endpoint from the child process
   
 };  
 
@@ -92,15 +119,18 @@ class TAO_ORBSVCS_Export TAO_AV_Endpoint_Process_Strategy_A
 class TAO_ORBSVCS_Export TAO_AV_Endpoint_Process_Strategy_B
   : public virtual TAO_AV_Endpoint_Process_Strategy
 // = DESCRIPTION
-//    Process-based strategy
+//    Process-based strategy to create "B" type endpoints
 {
  public:
   TAO_AV_Endpoint_Process_Strategy_B (ACE_Process_Options *process_options);
+  // Constructor
 
  protected:
   virtual int create_B (AVStreams::StreamEndPoint_B_ptr &stream_endpoint,
                         AVStreams::VDev_ptr &vdev,
                         CORBA::Environment &env);
+  // Creates a "B" type stream endpoint, and a vdev
+  
   
   virtual int get_stream_endpoint (CORBA::Environment &env);
   
