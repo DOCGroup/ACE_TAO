6351b0a60408d7ead9e668a07233e1155155017a
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-01 16:23:13 +0000

    added #include of Env_Value_T.h

--- ace/Env_Value_T.cpp
+++ ace/Env_Value_T.cpp
@@ -3,6 +3,8 @@
 #if !defined (ACE_ENV_VALUE_T_C)
 #define ACE_ENV_VALUE_T_C
 
+#include "ace/Env_Value_T.h"
+
 #if ! defined (__ACE_INLINE__)
 #include "ace/Env_Value_T.i"
 #endif /* __ACE_INLINE__ */
