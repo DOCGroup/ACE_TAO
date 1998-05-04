d463f5d0776df0518eca1a61675b0fa2702e5191
tworm <tworm@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-04 14:12:40 +0000

    Updated

--- TAO/orbsvcs/orbsvcs/Concurrency/CC_LockSet.cpp
+++ TAO/orbsvcs/orbsvcs/Concurrency/CC_LockSet.cpp
@@ -46,36 +46,66 @@ CC_LockSet::~CC_LockSet(void)
 // Locks the lock in the desired mode. Blocks until success. In a later
 // version the lock set should be searched for incompatible locks
 void CC_LockSet::lock(CosConcurrencyControl::lock_mode mode,
-		      CORBA::Environment &env)
+		      CORBA::Environment &_env)
 {
-  if(lock==0) {
-    lock_ = new CC_Lock(mode);
+  if(lock_==0) {
+    ACE_NEW(lock_, CC_Lock(mode));
+    if(errno==ENOMEM) {
+      _env.clear();
+      _env.exception(new CORBA::NO_MEMORY (CORBA::COMPLETED_NO));
+      _env.print_exception("CC_Lock::lock()");
+    }
   }
-  lock_->lock();
+  TAO_TRY {
+    lock_->lock(_env);
+  }
+  TAO_CATCHANY {
+    TAO_RETHROW;
+  }
+  TAO_ENDTRY;
 }
 
 // Tries to lock. If it is not possible false is returned. Comments for lock
 // holds for later version
 CORBA::Boolean CC_LockSet::try_lock(CosConcurrencyControl::lock_mode mode,
-				    CORBA::Environment &env)
+				    CORBA::Environment &_env)
 {
-  return lock_->try_lock();
+  TAO_TRY {
+    return lock_->try_lock(_env);
+  }
+  TAO_CATCHANY {
+    TAO_RETHROW_RETURN(CORBA::B_FALSE);
+  }
+  TAO_ENDTRY;
 }
 
 // Drops the specified lock. In this simple case we have only one lock at
 // any time so we just drop that lock.
 void CC_LockSet::unlock(CosConcurrencyControl::lock_mode mode,
-			CORBA::Environment &env)
+			CORBA::Environment &_env)
 {
-  lock_->unlock();
+  TAO_TRY {
+    lock_->unlock(_env);
+  }
+  TAO_CATCHANY {
+    TAO_RETHROW;
+  }
+  TAO_ENDTRY;
 }
 
 // Changes the mode of a held lock. In this version we deal with only
 // one type of lock (in reality) and therefore the type is of no meaning
 void CC_LockSet::change_mode (CosConcurrencyControl::lock_mode held_mode,
 			      CosConcurrencyControl::lock_mode new_mode,
-			      CORBA::Environment &env)
+			      CORBA::Environment &_env)
 {
-  lock_->change_mode(new_mode);
+  TAO_TRY
+    {
+      lock_->change_mode(new_mode, _env);
+    }
+  TAO_CATCHANY {
+    TAO_RETHROW;
+  }
+  TAO_ENDTRY;
 }
 
