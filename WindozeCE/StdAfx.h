00337247f3a07cc5664f2c356bc7ba86e56defc1
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-05 07:28:49 +0000

    New CE adapter.

--- WindozeCE/StdAfx.h
+++ WindozeCE/StdAfx.h
@@ -4,8 +4,8 @@
 //      are changed infrequently
 //
 
-#if !defined(AFX_STDAFX_H__5088F84A_CE8B_11D1_9621_0040054CF0F0__INCLUDED_)
-#define AFX_STDAFX_H__5088F84A_CE8B_11D1_9621_0040054CF0F0__INCLUDED_
+#if !defined(AFX_STDAFX_H__37544C6D_E3BC_11D1_968E_0040054CF0F0__INCLUDED_)
+#define AFX_STDAFX_H__37544C6D_E3BC_11D1_968E_0040054CF0F0__INCLUDED_
 
 #if _MSC_VER >= 1000
 #pragma once
@@ -19,7 +19,8 @@
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
+
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Developer Studio will insert additional declarations immediately before the previous line.
 
-#endif // !defined(AFX_STDAFX_H__5088F84A_CE8B_11D1_9621_0040054CF0F0__INCLUDED_)
+#endif // !defined(AFX_STDAFX_H__37544C6D_E3BC_11D1_968E_0040054CF0F0__INCLUDED_)
