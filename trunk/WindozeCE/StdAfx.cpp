c49702435925873e92f16de3a0c351aa892458b8
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-05 07:28:49 +0000

    New CE adapter.

--- trunk/WindozeCE/StdAfx.cpp
+++ trunk/WindozeCE/StdAfx.cpp
@@ -1,6 +1,6 @@
 // $Id$
 // stdafx.cpp : source file that includes just the standard includes
-//	WinAdapter.pch will be the pre-compiled header
+//	WindozeCE.pch will be the pre-compiled header
 //	stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
--- trunk/WindozeCE/resource.h
+++ trunk/WindozeCE/resource.h
@@ -1,13 +1,13 @@
 // $Id$
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
-// Used by WinAdapter.rc
+// Used by WindozeCE.rc
 //
-#define IDD_WinAdapter_DIALOG           102
+#define IDD_WINDOZECE_DIALOG            102
 #define IDR_MAINFRAME                   128
-#define IDC_TEXT_DISP                   1001
-#define IDC_CMDLINE                     1005
-#define IDC_START                       1007
+#define IDC_PROGRAM_OUTPUT              1001
+#define IDC_RUN                         1003
+#define IDC_PROGRAM_ARGUMENTS           1004
 
 // Next default values for new objects
 // 
@@ -15,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
