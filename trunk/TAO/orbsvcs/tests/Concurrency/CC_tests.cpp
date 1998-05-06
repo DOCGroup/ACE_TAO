40623ddac88a5ed72e4992d3e150e8e3eed4090f
tworm <tworm@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-06 21:54:36 +0000

    Changed the structure and added more tests

--- trunk/TAO/orbsvcs/tests/Concurrency/CC_tests.cpp
+++ trunk/TAO/orbsvcs/tests/Concurrency/CC_tests.cpp
@@ -55,9 +55,32 @@ CC_Test:: create_lock_set(void)
   return lock_set;
 }
 
+char *CC_Test::get_lock_mode_name(CosConcurrencyControl::lock_mode mode)
+{
+  if(mode==CosConcurrencyControl::read)
+    return "read";
 
-Test_Single_Lock_With_Mode::Test_Single_Lock_With_Mode(CC_naming_service *naming_service,
-                                                       CosConcurrencyControl::lock_mode mode) : CC_Test (naming_service)
+  if(mode==CosConcurrencyControl::write)
+    return "write";
+
+  if(mode==CosConcurrencyControl::upgrade)
+    return "upgrade";
+
+  if(mode==CosConcurrencyControl::intention_read)
+    return "intension_read";
+
+  if(mode==CosConcurrencyControl::intention_write)
+    return "intension_write";
+
+  return "unknown lock mode";
+}
+
+// Here the test start
+
+Test_Single_Lock_With_Mode::
+Test_Single_Lock_With_Mode(CC_naming_service *naming_service,
+                           CosConcurrencyControl::lock_mode mode)
+  : CC_Test (naming_service), mode_ (mode)
 {
 }
 
@@ -67,88 +90,154 @@ Test_Single_Lock_With_Mode::~Test_Single_Lock_With_Mode()
 
 Test_Single_Lock_With_Mode::run(int times_to_run)
 {
-  CORBA::Environment _env;
-  CORBA::Boolean lock_held;
-  char s[10];
+  CORBA::Boolean lock_not_held;
   
-  gets(s);
-  
-  CosConcurrencyControl::LockSet_ptr cc_lock_set_ = create_lock_set();
-
-  naming_service_->bind_name("LockSet_1", cc_lock_set_, _env);
-  ACE_DEBUG((LM_DEBUG, "Name bound\n"));
-  cc_lock_set_->lock(CosConcurrencyControl::read, _env);
-  ACE_DEBUG((LM_DEBUG, "Read lock set\n"));
- 
-  gets(s);
-
-  CORBA::Object_var ccls_obj = 
-    naming_service_->get_obj_from_name("",
-                                       "LockSet_1", _env);
-  CosConcurrencyControl::LockSet_var ccls =
-    CosConcurrencyControl::LockSet::_narrow(ccls_obj.in(), _env);
-
-  lock_held = ccls->try_lock(CosConcurrencyControl::read, _env);
-  //  lock_held = this->cc_lock_set_->try_lock(CosConcurrencyControl::read, _env);
-  if(lock_held)
-    ACE_DEBUG((LM_DEBUG, "Read lock not held\n"));
-  else
-    ACE_DEBUG((LM_DEBUG, "Read lock held\n"));
-
-  gets(s);
-
-  cc_lock_set_->unlock(CosConcurrencyControl::read, _env);
-  ACE_DEBUG((LM_DEBUG, "Read lock released\n"));
-
-  gets(s);
-
-  cc_lock_set_->lock(CosConcurrencyControl::write, _env);
-  ACE_DEBUG((LM_DEBUG, "Write lock set\n"));
-  
-  gets(s);
-
-  CORBA::release (cc_lock_set_);
-
-  return CC_SUCCESS;
-}
-
-Test_Against_Other_LockSet::
-Test_Against_Other_LockSet(CC_naming_service *naming_service,
-                           char *lock_set) : CC_Test (naming_service)
-{
-}
-
-Test_Against_Other_LockSet::~Test_Against_Other_LockSet()
-{
-}
-
-int Test_Against_Other_LockSet::run(int times_to_run)
-{
-  CORBA::Boolean lock_held = CORBA::B_FALSE;
   CosConcurrencyControl::LockSet_ptr cc_lock_set_ = create_lock_set();
 
   TAO_TRY
     {
-      this->naming_service_->bind_name("LockSet_1", cc_lock_set_,
-                                       TAO_TRY_ENV);
-      ACE_DEBUG((LM_DEBUG, "Name bound\n"));
-      cc_lock_set_->lock(CosConcurrencyControl::read, TAO_TRY_ENV);
-      ACE_DEBUG((LM_DEBUG, "Read lock set\n"));
-      
-      CORBA::Object_var ccls_obj = 
-        this->naming_service_->get_obj_from_name("", "LockSet_1", 
-                                                 TAO_TRY_ENV);
-      CosConcurrencyControl::LockSet_var ccls =
-        CosConcurrencyControl::LockSet::_narrow(ccls_obj.in(), TAO_TRY_ENV);
-      
-      lock_held = ccls->try_lock(CosConcurrencyControl::read, TAO_TRY_ENV);
+      cc_lock_set_->lock(mode_, TAO_TRY_ENV);
+      ACE_DEBUG((LM_DEBUG, "%s lock set\n", get_lock_mode_name(mode_)));
+
+      lock_not_held = cc_lock_set_->try_lock(mode_, TAO_TRY_ENV);
+      if(lock_not_held)
+        {
+          ACE_DEBUG((LM_DEBUG, "%s lock not held\n",
+                    get_lock_mode_name(mode_)));
+          return CC_FAIL;
+        }
+      else
+        ACE_DEBUG((LM_DEBUG, "%s lock held\n", get_lock_mode_name(mode_)));
+
+      cc_lock_set_->unlock(mode_, TAO_TRY_ENV);
+      ACE_DEBUG((LM_DEBUG, "%s lock released\n", get_lock_mode_name(mode_)));
+
+      lock_not_held = cc_lock_set_->try_lock(mode_, TAO_TRY_ENV);
+      if(lock_not_held)
+        {
+          ACE_DEBUG((LM_DEBUG, "%s lock not held\n",
+                    get_lock_mode_name(mode_)));
+        }
+      else
+        {
+          ACE_DEBUG((LM_DEBUG, "%s lock held\n", get_lock_mode_name(mode_)));
+          return CC_FAIL;
+        }
+
     }
   TAO_CATCHANY
     {
-      TAO_TRY_ENV.print_exception ("CC_Client::run_extended_tests");
+      TAO_TRY_ENV.print_exception ("Test_Single_Lock_With_Mode::run");
       return CC_FAIL;
     }
   TAO_ENDTRY;
 
+  CORBA::release (cc_lock_set_);
+  
   return CC_SUCCESS;
 }
+
+Test_Setup_LockSet::Test_Setup_LockSet(CC_naming_service *naming_service,
+                                       char *name)
+  : CC_Test (naming_service), my_name_ (name)
+{
+}
+  
+Test_Setup_LockSet::~Test_Setup_LockSet()
+{
+}
+
+int Test_Setup_LockSet::run(int times_to_run)
+{
+  CORBA::Boolean lock_held = CORBA::B_FALSE;
+
+  TAO_TRY
+    {
+      CosConcurrencyControl::LockSet_ptr cc_lock_set_ = create_lock_set();
+      this->naming_service_->bind_name(my_name_, cc_lock_set_,
+                                       TAO_TRY_ENV);
+      ACE_DEBUG((LM_DEBUG, "Name bound\n"));
+      
+      cc_lock_set_->lock(CosConcurrencyControl::read, TAO_TRY_ENV);
+      ACE_DEBUG((LM_DEBUG, "Read lock set\n"));
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Test_Against_Other_LockSet::run");
+      return CC_FAIL;
+    }
+  TAO_ENDTRY;
+  
+  return CC_SUCCESS;
+}
+
+Test_Use_Already_Created_LockSet::
+Test_Use_Already_Created_LockSet(CC_naming_service *naming_service,
+                                 char *name)
+  : CC_Test (naming_service), my_name_ (name)
+{
+}
+  
+Test_Use_Already_Created_LockSet::~Test_Use_Already_Created_LockSet()
+{
+}
+  
+int Test_Use_Already_Created_LockSet::run(int times_to_run)
+{
+  CORBA::Boolean lock_held = CORBA::B_FALSE;
+  
+  TAO_TRY
+    {
+      CORBA::Object_var ccls_obj = 
+        this->naming_service_->get_obj_from_name("", my_name_, 
+                                                 TAO_TRY_ENV);
+      CosConcurrencyControl::LockSet_var ccls =
+        CosConcurrencyControl::LockSet::_narrow(ccls_obj.in(), TAO_TRY_ENV);
+      
+      ccls->lock(CosConcurrencyControl::read, TAO_TRY_ENV);
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Test_Against_Other_LockSet::run");
+      return CC_FAIL;
+    }
+  TAO_ENDTRY;
+  
+  return CC_SUCCESS;
+}
+
+Test_Unlock_Already_Created_LockSet::
+Test_Unlock_Already_Created_LockSet(CC_naming_service *naming_service,
+                                 char *name)
+  : CC_Test (naming_service), my_name_ (name)
+{
+}
+  
+Test_Unlock_Already_Created_LockSet::~Test_Unlock_Already_Created_LockSet()
+{
+}
+  
+int Test_Unlock_Already_Created_LockSet::run(int times_to_run)
+{
+  CORBA::Boolean lock_held = CORBA::B_FALSE;
+  
+  TAO_TRY
+    {
+      CORBA::Object_var ccls_obj = 
+        this->naming_service_->get_obj_from_name("", my_name_, 
+                                                 TAO_TRY_ENV);
+      CosConcurrencyControl::LockSet_var ccls =
+        CosConcurrencyControl::LockSet::_narrow(ccls_obj.in(), TAO_TRY_ENV);
+      
+      ccls->unlock(CosConcurrencyControl::read, TAO_TRY_ENV);
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Test_Against_Other_LockSet::run");
+      return CC_FAIL;
+    }
+  TAO_ENDTRY;
+  
+  return CC_SUCCESS;
+}
+
