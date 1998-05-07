26eb9d55658c7dd58adba51d5f6f531c3401cca8
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-05-07 06:21:54 +0000

    *** empty log message ***

--- TAO/orbsvcs/orbsvcs/Concurrency/CC_LockSetFactory.cpp
+++ TAO/orbsvcs/orbsvcs/Concurrency/CC_LockSetFactory.cpp
@@ -35,17 +35,17 @@ CC_LockSetFactory::create ( CORBA::Environment &_env)
 {
   CC_LockSet *ls = 0;
 
-  env.clear ();
-  env.exception (new CORBA::UNKNOWN (CORBA::COMPLETED_NO));
+  _env.clear ();
+  _env.exception (new CORBA::UNKNOWN (CORBA::COMPLETED_NO));
   ACE_GUARD_RETURN (ACE_Lock, ace_mon, *this->lock_, CosConcurrencyControl::LockSet::_nil ());
 
-  env.clear ();
+  _env.clear ();
   
-  env.exception (new CORBA::NO_MEMORY (CORBA::COMPLETED_NO));
+  _env.exception (new CORBA::NO_MEMORY (CORBA::COMPLETED_NO));
   ACE_NEW_RETURN (ls,
                   CC_LockSet,
                   CosConcurrencyControl::LockSet::_nil ());
-  env.clear ();
+  _env.clear ();
 
   return ls->_this (_env);
 }
@@ -56,17 +56,17 @@ CC_LockSetFactory::create_related (CosConcurrencyControl::LockSet_ptr which,
 {
   CC_LockSet *ls = 0;
 
-  env.clear ();
-  env.exception (new CORBA::UNKNOWN (CORBA::COMPLETED_NO));
+  _env.clear ();
+  _env.exception (new CORBA::UNKNOWN (CORBA::COMPLETED_NO));
   ACE_GUARD_RETURN (ACE_Lock, ace_mon, *this->lock_, CosConcurrencyControl::LockSet::_nil ());
 
-  env.clear ();
+  _env.clear ();
   
-  env.exception (new CORBA::NO_MEMORY (CORBA::COMPLETED_NO));
+  _env.exception (new CORBA::NO_MEMORY (CORBA::COMPLETED_NO));
   ACE_NEW_RETURN (ls,
                   CC_LockSet (which),
                   CosConcurrencyControl::LockSet::_nil ());
-  env.clear ();
+  _env.clear ();
 
   return ls->_this (_env);
 }
