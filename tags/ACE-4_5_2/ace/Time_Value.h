cd041eb202fa35509ef86b053c4b3ffe25aba57e
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-08 00:09:21 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_2'.

---
+++ tags/ACE-4_5_2/ace/Time_Value.h
@@ -0,0 +1,25 @@
+/* -*- C++ -*- */
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    ace
+// 
+// = FILENAME
+//    Time_Value.h
+//
+// = AUTHOR
+//    Doug Schmidt 
+// 
+// ============================================================================
+
+#if !defined (ACE_TIME_VALUE_H)
+#define ACE_TIME_VALUE_H
+
+// This file is no longer used and is only here due to backwards
+// compatibility.  All the functionality has been merged into OS.h.
+
+#include "ace/OS.h"
+
+#endif /* ACE_TIME_VALUE */
---
+++ tags/ACE-4_5_2/examples/Misc/test_set.cpp
@@ -0,0 +1,14 @@
+// $Id$
+
+#include "ace/Containers.h"
+
+int
+main (int, char *[])
+{
+  return 0;
+}
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
