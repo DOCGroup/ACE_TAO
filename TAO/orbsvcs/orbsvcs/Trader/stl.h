257625942ace37e5ec4ad8e67969fd87bbe2273f
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-12 01:22:57 +0000

    STL declarations

---
+++ TAO/orbsvcs/orbsvcs/Trader/stl.h
@@ -0,0 +1,38 @@
+/* -*- C++ -*- */
+
+// ========================================================================
+// $Id$
+// 
+// = LIBRARY
+//    orbsvcs
+// 
+// = FILENAME
+//   stl.h
+//
+// = AUTHOR
+//    Seth Widoff <sbw1@cs.wustl.edu>
+//
+// ========================================================================
+
+
+#include<map>
+#include<vector>
+#include<string>
+#include<set>
+#include<deque>
+#include<list>
+#include<queue>
+#include<stack>
+
+#if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
+            (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
+using std::map;
+using std::vector;
+using std::string;
+using std::set;
+using std::deque;
+using std::list;
+using std::queue;
+using std::stack;
+
+#endif // ACE_USES ...
