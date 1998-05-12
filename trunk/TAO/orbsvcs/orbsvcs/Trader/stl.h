9be950670ce7f853b48f8f3962cb857f5cda2f97
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-12 19:08:21 +0000

    minor

--- trunk/TAO/orbsvcs/orbsvcs/Trader/stl.h
+++ trunk/TAO/orbsvcs/orbsvcs/Trader/stl.h
@@ -23,8 +23,8 @@
 #include<queue>
 #include<stack>
 
-#if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
-(ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
+//#if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
+//(ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
 using std::map;
 using std::vector;
 using std::string;
@@ -37,7 +37,7 @@ using std::less;
 using std::pair;
 using std::multimap;
 
-#endif // ACE_USES
+//#endif // ACE_USES
 
 
 
