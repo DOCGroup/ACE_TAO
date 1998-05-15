476c7b9bcd8fac5985f842c75098202a1b4feeb0
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-05-15 07:42:20 +0000

    4.5.4

--- ace/Version.h
+++ ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (5u)
-#define ACE_BETA_VERSION (3u)
-#define ACE_VERSION "4.5.3\0"
+#define ACE_BETA_VERSION (4u)
+#define ACE_VERSION "4.5.4\0"
