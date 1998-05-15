793e16c56953aaffab4bfc0c73dd6cb17632d7c8
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-15 18:01:25 +0000

    added template instantiations

--- TAO/orbsvcs/orbsvcs/Property/CosPropertyService_i.cpp
+++ TAO/orbsvcs/orbsvcs/Property/CosPropertyService_i.cpp
@@ -1917,3 +1917,21 @@ void
 TAO_PropertiesIterator::destroy (CORBA::Environment &_env)
 {
 }
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class ACE_Hash_Map_Entry<CosProperty_Hash_Key, CosProperty_Hash_Value>;
+template class ACE_Hash_Map_Manager<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Iterator<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Iterator_Base<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Reverse_Iterator<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>;
+template class TAO_Unbounded_Sequence<TAO_PropertySet*>;
+template class TAO_Unbounded_Sequence<TAO_PropertySetDef*>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate template class ACE_Hash_Map_Entry<CosProperty_Hash_Key, CosProperty_Hash_Value>
+#pragma instantiate template class ACE_Hash_Map_Manager<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>
+#pragma instantiate template class ACE_Hash_Map_Iterator<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>
+#pragma instantiate template class ACE_Hash_Map_Iterator_Base<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>
+#pragma instantiate template class ACE_Hash_Map_Reverse_Iterator<CosProperty_Hash_Key, CosProperty_Hash_Value, ACE_Null_Mutex>
+#pragma instantiate TAO_Unbounded_Sequence<TAO_PropertySet*>;
+#pragma instantiate TAO_Unbounded_Sequence<TAO_PropertySetDef*>;
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
