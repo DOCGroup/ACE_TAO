009d76f7477b2ac2aa1244675a985665ff448dc3
sergio <sergio@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-25 23:07:53 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/orbsvcs/Event/Dispatching_Modules.h
+++ trunk/TAO/orbsvcs/orbsvcs/Event/Dispatching_Modules.h
@@ -23,7 +23,7 @@
 #ifndef ACE_DISPATCHING_MODULES_H
 #define ACE_DISPATCHING_MODULES_H
 
-#include "ace/Timeprobe.h"
+#include "tao/Timeprobe.h"
 #include "ReactorTask.h"
 #include "Event_Channel.h"
 
