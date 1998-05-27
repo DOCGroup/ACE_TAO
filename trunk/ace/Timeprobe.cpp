d45efec9ea2431da341d44edcfb04d442b207c8e
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-27 21:13:11 +0000

    *** empty log message ***

--- trunk/ace/Timeprobe.cpp
+++ trunk/ace/Timeprobe.cpp
@@ -5,7 +5,7 @@
 
 #include "ace/Timeprobe.h"
 
-#if defined (ACE_ENABLE_TIMEPROBES)
+#if defined (ACE_COMPILE_TIMEPROBES)
 
 #if !defined (__ACE_INLINE__)
 #include "ace/Timeprobe.i"
@@ -22,6 +22,10 @@ ACE_Timeprobe<ACE_LOCK>::ACE_Timeprobe (u_long size,
     max_size_ (size),
     current_size_ (0)
 {
+  // This will calibrate the High_Res_Timers
+  ACE_High_Res_Timer calibrator;
+  ACE_UNUSED_ARG (calibrator);
+
   void *space = this->allocator_->malloc ((sizeof (ACE_timeprobe_t)) * this->max_size_);
   this->timeprobes_ = new ((ACE_timeprobe_t *) space) ACE_timeprobe_t[this->max_size_];
 
@@ -212,6 +216,34 @@ ACE_Function_Timeprobe<Timeprobe>::~ACE_Function_Timeprobe (void)
   this->timeprobe_.timeprobe (this->event_ + 1);
 }
 
-#endif /* ACE_ENABLE_TIMEPROBES */
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class ACE_Timeprobe<ACE_TIMEPROBE_MUTEX>;
+template class ACE_Function_Timeprobe<ACE_Timeprobe<ACE_TIMEPROBE_MUTEX> >;
+template class ACE_Unbounded_Set_Iterator<ACE_Event_Descriptions>;
+template class ACE_Unbounded_Set<ACE_Event_Descriptions>;
+template class ACE_Node<ACE_Event_Descriptions>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate ACE_Timeprobe<ACE_TIMEPROBE_MUTEX>
+#pragma instantiate ACE_Function_Timeprobe<ACE_Timeprobe<ACE_TIMEPROBE_MUTEX> >
+#pragma instantiate ACE_Unbounded_Set_Iterator<ACE_Event_Descriptions>
+#pragma instantiate ACE_Unbounded_Set<ACE_Event_Descriptions>
+#pragma instantiate ACE_Node<ACE_Event_Descriptions>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
+#  if defined (ACE_TSS_TIMEPROBES)
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class ACE_TSS_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_NULL_MUTEX>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate ACE_TSS_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_NULL_MUTEX>;
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+#  else /* ACE_TSS_TIMEPROBES */
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class ACE_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_MUTEX>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate ACE_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_MUTEX>;
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+#  endif /* ACE_TSS_TIMEPROBES */
+
+#endif /* ACE_COMPILE_TIMEPROBES */
 
 #endif /* ACE_TIMEPROBE_C */
--- trunk/ace/Timeprobe.h
+++ trunk/ace/Timeprobe.h
@@ -213,44 +213,14 @@ typedef ACE_SYNCH_NULL_MUTEX ACE_TIMEPROBE_MUTEX;
 
 typedef ACE_Timeprobe<ACE_TIMEPROBE_MUTEX> ACE_TIMEPROBE_WITH_LOCKING;
 
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template class ACE_Timeprobe<ACE_TIMEPROBE_MUTEX>;
-template class ACE_Function_Timeprobe<ACE_Timeprobe<ACE_TIMEPROBE_MUTEX> >;
-template class ACE_Unbounded_Set_Iterator<ACE_Event_Descriptions>;
-template class ACE_Unbounded_Set<ACE_Event_Descriptions>;
-template class ACE_Node<ACE_Event_Descriptions>;
-#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#pragma instantiate ACE_Timeprobe<ACE_TIMEPROBE_MUTEX>
-#pragma instantiate ACE_Function_Timeprobe<ACE_Timeprobe<ACE_TIMEPROBE_MUTEX> >
-#pragma instantiate ACE_Unbounded_Set_Iterator<ACE_Event_Descriptions>
-#pragma instantiate ACE_Unbounded_Set<ACE_Event_Descriptions>
-#pragma instantiate ACE_Node<ACE_Event_Descriptions>
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
 // If ACE_TSS_TIMEPROBES is defined, store the ACE_Timeprobe singleton
 // in thread specific storage.  This allows multiple threads to use
 // their own instance of ACE_Timerprobe, without interfering with each
 // other.
 #  if defined (ACE_TSS_TIMEPROBES)
-
 typedef ACE_TSS_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_NULL_MUTEX> ACE_TIMEPROBE_SINGLETON;
-
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template class ACE_TSS_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_NULL_MUTEX>;
-#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#pragma instantiate ACE_TSS_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_NULL_MUTEX>;
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
 #  else /* ACE_TSS_TIMEPROBES */
-
 typedef ACE_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_MUTEX> ACE_TIMEPROBE_SINGLETON;
-
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template class ACE_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_MUTEX>;
-#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#pragma instantiate ACE_Singleton<ACE_TIMEPROBE_WITH_LOCKING, ACE_SYNCH_MUTEX>;
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
 #  endif /* ACE_TSS_TIMEPROBES */
 
 #endif /* ACE_COMPILE_TIMEPROBES */
