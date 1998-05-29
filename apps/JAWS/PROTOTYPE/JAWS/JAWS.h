96e84818d4493e9f1a506ffd3cb94470feffcbd0
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-05-29 22:52:46 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_9'.

--- apps/JAWS/PROTOTYPE/JAWS/JAWS.h
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#if (JAWS_NTRACE == 1)
-# define JAWS_TRACE(X)
-#else
-# define JAWS_TRACE(X) ACE_Trace ____ (ASYS_TEXT (X), \
-                                       __LINE__, \
-                                       ASYS_TEXT (__FILE__))
-#endif /* JAWS_NTRACE */
