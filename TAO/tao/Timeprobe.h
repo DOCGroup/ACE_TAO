edde21f0c7ea24bca69315de322ed9bb2c4205c0
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-02 20:45:55 +0000

    *** empty log message ***

--- TAO/tao/Timeprobe.h
+++ TAO/tao/Timeprobe.h
@@ -3,6 +3,8 @@
 #if !defined (TAO_TIMEPROBE_H)
 #define TAO_TIMEPROBE_H
 
+// Please see ace/Timeprobe.h for details on these #defines.
+
 //#define ACE_COMPILE_TIMEPROBES
 //#define ACE_ENABLE_TIMEPROBES
 //#define ACE_MT_TIMEPROBES
