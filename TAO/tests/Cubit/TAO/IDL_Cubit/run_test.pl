220ec6ab588b790c19c5f2ea565548b9a0e7236d
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-03 21:57:20 +0000

    *** empty log message ***

--- TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
+++ TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -12,7 +12,7 @@ $ior_file = "cubit_ior";
 $SV = Process::Create ("server".$Process::EXE_EXT, 
                        "-s -ORBport ".$server_port." -o ".$ior_file." -ORBobjrefstyle url");
 
-sleep (5);
+sleep (2);
 
 system ("client".$Process::EXE_EXT." -x -s -f ".$ior_file." -ORBport ".$client_port );
 
