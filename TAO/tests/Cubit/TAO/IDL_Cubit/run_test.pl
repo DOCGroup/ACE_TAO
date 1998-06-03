75f99a4238b9974026361478de0738afada095ea
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-03 21:32:11 +0000

    Perl script for testing IDL_Cubit

---
+++ TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -0,0 +1,19 @@
+#!/pkg/gnu/bin/perl -I../../../../../bin
+#
+# If your perl installation isn't in /pkg/gnu/bin/perl,
+# please make the change accordingly
+
+use Process;
+
+$client_port = 0;
+$server_port = 0;
+$ior_file = "cubit_ior";
+
+$SV = Process::Create ("server".$Process::EXE_EXT, 
+                       "-s -ORBport ".$server_port." -o ".$ior_file." -ORBobjrefstyle url");
+
+sleep (2);
+
+system ("client".$Process::EXE_EXT." -s -f ".$ior_file." -ORBport ".$client_port);
+
+$SV->Kill ();
