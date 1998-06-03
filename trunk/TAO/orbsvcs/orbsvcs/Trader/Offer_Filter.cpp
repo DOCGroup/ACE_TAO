a2a4dbe5202ac83fbfe732f74288474d79b4cf6b
sbw1 <sbw1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-03 21:34:28 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/orbsvcs/Trader/Offer_Filter.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Trader/Offer_Filter.cpp
@@ -20,21 +20,27 @@ TAO_Offer_Filter (SERVICE_TYPE_REPOS::TypeStruct* type_struct,
       SERVICE_TYPE_REPOS::PropertyMode mode = prop_seq[i].mode;
       if (mode != SERVICE_TYPE_REPOS::PROP_MANDATORY_READONLY ||
 	  mode != SERVICE_TYPE_REPOS::PROP_READONLY)
-	this->mod_props_.insert (string (prop_seq[i].name));
+	{
+	  TAO_String_Hash_Key prop_name ((const char*) prop_seq[i].name);
+	  this->mod_props_.insert (prop_name);
+	}
     }
 
   if (policies.exact_type_match (_env) == CORBA::B_TRUE)
-    this->limits_.insert
-      (string (TAO_Policies::POLICY_NAMES[TAO_Policies::EXACT_TYPE_MATCH]));
+    {
+      TAO_String_Hash_Key exact_match
+	(TAO_Policies::POLICY_NAMES[TAO_Policies::EXACT_TYPE_MATCH]);
+      this->limits_.insert (exact_match);
+    }
     
 }
 
 CORBA::Boolean
 TAO_Offer_Filter::ok_to_consider (CosTrading::Offer* offer)
 {
-  const char* use_mods =
+  TAO_String_Hash_Key use_mods =
     TAO_Policies::POLICY_NAMES[TAO_Policies::USE_MODIFIABLE_PROPERTIES];
-  const char* use_dyns =
+  TAO_String_Hash_Key use_dyns =
     TAO_Policies::POLICY_NAMES[TAO_Policies::USE_DYNAMIC_PROPERTIES];
   CORBA::Boolean return_value = CORBA::B_TRUE;
   TAO_Property_Evaluator prop_eval (*offer);
@@ -53,11 +59,10 @@ TAO_Offer_Filter::ok_to_consider (CosTrading::Offer* offer)
 	    {
 	      // Determine if this property name is found in the set
 	      // of modifiable properties for the type being considered.
-	      string prop_name (offer->properties[i].name);
-	      if (this->mod_props_.find (prop_name) !=
-		  this->mod_props_.end ())
+	      TAO_String_Hash_Key prop_name ((const char*) offer->properties[i].name);
+	      if (this->mod_props_.find (prop_name) == 0)
 		{
-		  this->limits_.insert (string (use_mods));
+		  this->limits_.insert (use_mods);
 		  return_value = 0;
 		}
 	    }
@@ -67,7 +72,7 @@ TAO_Offer_Filter::ok_to_consider (CosTrading::Offer* offer)
 	      // Determine if this property is dynamic.
 	      if (prop_eval.is_dynamic_property (i))
 		{
-		  this->limits_.insert (string (use_dyns));
+		  this->limits_.insert (use_dyns);
 		  return_value = 0;
 		}
 	    }
@@ -84,7 +89,7 @@ TAO_Offer_Filter::ok_to_consider (CosTrading::Offer* offer)
       this->search_card_--;
       if (this->search_card_ == 0)
 	{
-	  string search_card =
+	  TAO_String_Hash_Key search_card =
 	    TAO_Policies::POLICY_NAMES[TAO_Policies::SEARCH_CARD];
 	  this->limits_.insert (search_card);
 	}
@@ -107,14 +112,14 @@ TAO_Offer_Filter::matched_offer (void)
 
   if (this->match_card_ == 0)
     {
-      string match_card =
+      TAO_String_Hash_Key match_card =
 	TAO_Policies::POLICY_NAMES[TAO_Policies::MATCH_CARD];
       this->limits_.insert (match_card);
     }
 
   if (this->return_card_ == 0)
     {
-      string return_card =
+      TAO_String_Hash_Key return_card =
 	TAO_Policies::POLICY_NAMES[TAO_Policies::MATCH_CARD];
       this->limits_.insert (return_card);
     }
@@ -128,10 +133,14 @@ TAO_Offer_Filter::limits_applied (void)
   CosTrading::PolicyName* temp =
     CosTrading::PolicyNameSeq::allocbuf (size);
 
-  for (NAMES::iterator p_iter = this->limits_.begin();
-       p_iter != this->limits_.end ();
-       p_iter++)  
-    temp[i++] = CORBA::string_dup ((*p_iter).data ());
+  for (Names::iterator p_iter (this->limits_.begin());
+       ! p_iter.done ();
+       p_iter.advance ())
+    {
+      TAO_String_Hash_Key* policy_name_ptr = 0;
+      p_iter.next (policy_name_ptr);
+      temp[i++] = CORBA::string_dup ((const char*) *policy_name_ptr);
+    }
   
   return new CosTrading::PolicyNameSeq (i, i, temp, 1);
 }
