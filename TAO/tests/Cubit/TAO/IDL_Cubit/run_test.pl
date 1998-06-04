4730caf9712cb84c17eaee89625f3275083027c5
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-04 16:22:44 +0000

    Enabled the multicast stuff.

--- TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
+++ TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -5,25 +5,35 @@
 
 use Process;
 
+if ($^O eq "MSWin32")
+{
+  system ("ipconfig | find \"Address\">ipnum");
+
+  open (IPNUM, "ipnum");
+
+  read (IPNUM, $line, 80);
+
+  ($junk, $ip1, $ip2, $ip3, $ip4) = split (/: (\d+)\.(\d+)\.(\d+)\.(\d+)/, $line);
+
+  close IPNUM;
+
+  system ("del /q ipnum");
+
+  $uid = $ip4;
+}
+else
+{
+  $uid = getpwnam (getlogin ());
+}
+
+$nsport = 20000 + $uid;
 $client_port = 0;
 $server_port = 0;
 $iorfile = "cubit_ior";
 
 $SV = Process::Create ("server".$Process::EXE_EXT, 
-                       "-s -ORBport ".$server_port." -o ".$iorfile." -ORBobjrefstyle url");
+                       "-ORBport ".$server_port." -ORBnameserviceport ".$nsport." -ORBobjrefstyle url");
 
 sleep (2);
 
-system ("client".$Process::EXE_EXT." -x -s -f ".$iorfile." -ORBport ".$client_port );
-
-$SV->Kill ();
-
-
-if ($^O eq "MSWin32")
-{
-  system ("del ".$iorfile);
-}
-else
-{
-  system ("rm ".$iorfile);
-}
+system ("client".$Process::EXE_EXT." -x -ORBnameserviceport ".$nsport." -ORBport ".$client_port );
