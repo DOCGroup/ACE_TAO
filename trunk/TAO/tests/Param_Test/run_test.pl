16975d40a594c83c64b08723bfa119a0ff573b54
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-04 14:02:49 +0000

    Perl script for testing Param Test

---
+++ trunk/TAO/tests/Param_Test/run_test.pl
@@ -0,0 +1,28 @@
+#!/pkg/gnu/bin/perl -I../../../bin
+#
+# If your perl installation isn't in /pkg/gnu/bin/perl,
+# please make the change accordingly
+#
+
+use Process;
+
+#uid=`id | cut -c5-20 | cut -f1 -d"("`
+$port = 30001 + $uid;
+$iorfile = "theior";
+$invocation = "sii";
+
+$SV = Process::Create (".".$DIR_SEPARATOR."server".$Process::EXE_EXT, 
+                       "-ORBobjrefstyle url -ORBport ".$port." -o ".$iorfile." 2");
+sleep (2);     # Give the server a chance to start up
+
+@types= ("short", "ubstring", "fixed_struct", "strseq", "var_struct", "nested_struct", "struct_seq", "objref");
+
+
+foreach $type (@types)
+{
+	print ("BEGIN Testing type ".$type."\n");
+	system (".".$DIR_SEPARATOR."client -f ".$iorfile."  -i ".$invocation." -t ".$type." 2");
+	print ("END   Testing type ".$type."\n");
+}
+
+$SV->Kill ();
