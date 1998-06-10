4642b75c733a04955bb2e392003d44f1215cac69
tworm <tworm@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-10 19:30:37 +0000

    Added multiple possesion semsntics to the lock set

--- TAO/orbsvcs/orbsvcs/Concurrency/CC_Lock.h
+++ TAO/orbsvcs/orbsvcs/Concurrency/CC_Lock.h
@@ -25,11 +25,11 @@
 #include "orbsvcs/CosConcurrencyControlC.h"
 
 #define NUMBER_OF_LOCK_MODES 5
-// This contant defines the number of lock modes. There is really no
+// This constant defines the number of lock modes. There is really no
 // way to set this constant dynamically because the nuber of lock
 // modes are not stated as part of the IDL.
 
-class TAO_ORBSVCS_Export CC_Lock 
+class TAO_ORBSVCS_Export CC_Lock
 {
   // = TITLE
   //     CC_Lock
@@ -42,6 +42,9 @@ class TAO_ORBSVCS_Export CC_Lock
   //     which is essentially a write lock since it is not allowed to
   //     have more than one lock pr. servant in this implementation.
 public:
+  CC_Lock (void);
+  // Creates the lock with mode = intention_read (weakest)
+
   CC_Lock (CosConcurrencyControl::lock_mode mode);
   // Creates the lock with the desired mode
 
@@ -61,7 +64,10 @@ public:
   void change_mode (CosConcurrencyControl::lock_mode new_mode,
                    CORBA::Environment &env);
   // Changes the mode of this lock.
-  
+
+  void set_mode (CosConcurrencyControl::lock_mode mode);
+  // Sets the mode_ of the lock. Used in initialization
+
   CORBA::Boolean Compatible (const CC_Lock &other);
   // returns true if this lock is compatible with the other lock.
 
@@ -71,20 +77,59 @@ public:
   CosConcurrencyControl::lock_mode GetMode (void);
   // Returns the mode of the lock.
 
+  int GetLocksHeld(void);
+  // Returns the number of times this lock have been locked
+
+  void DecLocksHeld(void);
+  // Decrements the number of locks held in this mode. Used by change_mode.
+
+  void dump(void);
+  // Dumps the state of the object to stdout
+
 protected:
   CosConcurrencyControl::lock_mode mode_;
   // Holds the lock's mode.
 
 private:
-  ACE_Thread_Semaphore semaphore_;
-  // Holds the semaphore for this lock.
-  // @@ Torben, can you please explain this implementation in a bit more detail? 
-
   int lock_held_;
   // If greater than zero the lock is held (that number of times).
 
   static CORBA::Boolean compatible_[NUMBER_OF_LOCK_MODES][NUMBER_OF_LOCK_MODES];
   // Defines the compatibility of the locks.
+};
+
+class TAO_ORBSVCS_Export CC_LockModeIterator
+{
+  // = TITLE
+  //     CC_LockModeIterator
+  //
+  // = DESCRIPTION
+  //     This class implements an iterator over the lock modes in
+  //     order to make an ordered traversal over the locks from the
+  //     weakest (intention read) to the strongest (write).
+  //     Ordering: IR -> R -> U -> IW -> W
+public:
+  CC_LockModeIterator(void);
+  // Default constructor
+
+  ~CC_LockModeIterator(void);
+  // Destructor
+
+  void First(void);
+  // Reset the iterator to the first element
+
+  void Next(CORBA::Environment &_env);
+  // Advance the iterator to the next element
+  // Throws exception if out of range
+
+  CORBA::Boolean IsDone(void);
+  // Returns true if the iterator has reached the last element
+
+  CosConcurrencyControl::lock_mode GetLockMode(void);
+  // Get the current element
+
+private:
+  CosConcurrencyControl::lock_mode current_;
 };
 
 #endif /* !defined (_CC_LOCK_H) */
