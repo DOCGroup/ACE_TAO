61c540054ca95657ceec79465e7656590f537f18
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-16 22:46:08 +0000

    Allowed a switch for not bringing up a new console.

--- bin/Process_Win32.pm
+++ bin/Process_Win32.pm
@@ -4,13 +4,25 @@ use Win32::Process;
 
 $EXE_EXT = ".exe";
 
+$newwindow = "yes";
+
 sub Create
 {
   my $name = shift;
   my $args = shift;
   my $self = [];
+
+  if ($newwindow eq "no")
+  {
+    my $console = 0;
+  }
+  else
+  {
+    my $console = CREATE_NEW_CONSOLE;
+  }
+
   Win32::Process::Create ($self->[0], $name, $name." ".$args, 0, 
-                          CREATE_NEW_CONSOLE, ".");
+                          $console, ".");
   bless $self;
 }  
 
