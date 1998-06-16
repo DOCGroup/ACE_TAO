dd0175d716d9a8b02dfb8909ea92ab971b806e77
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-16 21:52:31 +0000

    Cleaned up code and made to work better with a "testall" type script.

--- trunk/TAO/tests/Quoter/run_test.pl
+++ trunk/TAO/tests/Quoter/run_test.pl
@@ -44,10 +44,10 @@ else
 # variables for parameters
 
 $nsport = 20000 + $uid;
-$clport = 20765 + $uid;
-$svport = 20510 + $uid;
-$ffport = 20255 + $uid;
-$gfport = 0;
+$clport = 0; 
+$svport = 0; 
+$ffport = 0; 
+$gfport = 0; 
 
 # other variables
 
@@ -59,151 +59,146 @@ $debug = "";
 $cm = "";
 $sm = "";
 $other = "";
+$c_conf = "client.conf";
+$s_conf = "server.conf";
 
 sub read_nsior
 {
-	open (FH, "<".$nsiorfile);
+  open (FH, "<".$nsiorfile);
 
-	read (FH, $ior, 255);
+  read (FH, $ior, 255);
 
-	close (FH);
+  close (FH);
 }
 
 sub name_server
 {
-	my $args = sprintf ("%s -ORBport %d -ORBobjrefstyle url -o %s",
-	                    $other,
-	                    $nsport,
-	                    $nsiorfile);
+  my $args = "$other -ORBport $nsport -ORBobjrefstyle url -o $nsiorfile";
+  my $prog = "..$DIR_SEPARATOR..$DIR_SEPARATOR"."orbsvcs$DIR_SEPARATOR".
+             "Naming_Service$DIR_SEPARATOR".
+             "Naming_Service$Process::EXE_EXT";
 
-	$NS = Process::Create ("..".$DIR_SEPARATOR."..".$DIR_SEPARATOR."orbsvcs".$DIR_SEPARATOR.
-	                       "Naming_Service".$DIR_SEPARATOR."Naming_Service".$Process::EXE_EXT, $args);
-
+  $NS = Process::Create ($prog, $args);
 }
 
 sub server
 {
-	my $args = sprintf ("%s %s %s -ORBport %d -ORBobjrefstyle url -ORBnameserviceior %s -ORBsvcconf server.conf",
-	                   $other,
-                           $debug,
-                           $sm,
-                           $svport,
-                           $ior);
+  my $args = "$other $debug $sm -ORBport $svport -ORBobjrefstyle url ".
+             "-ORBnameserviceior $ior -ORBsvcconf $s_conf";
 
-	$SV = Process::Create ("server".$Process::EXE_EXT, $args);
+  $SV = Process::Create ("server$Process::EXE_EXT", $args);
 }
 
 sub factory_finder
 {
-	my $args = sprintf ("%s -ORBport %d -ORBobjrefstyle url -ORBnameserviceior %s -ORBsvcconf svc.conf",
-	                    $other,
-	                    $ffport,
-	                    $ior);
+  my $args = "$other -ORBport $ffport -ORBobjrefstyle url -ORBnameserviceior ".
+             "$ior -ORBsvcconf svc.conf";
 	
-	$FF = Process::Create ("Factory_Finder".$Process::EXE_EXT, $args);
+  $FF = Process::Create ("Factory_Finder".$Process::EXE_EXT, $args);
 }
 
 sub generic_factory
 {
-	my $args = sprintf ("%s -ORBport %d -ORBobjrefstyle url -ORBnameserviceior %s -ORBsvcconf svc.conf",
-	                    $other,
-	                    $gfport,
-	                    $ior);
+  my $args = "$other -ORBport $gfport -ORBobjrefstyle url -ORBnameserviceior ".
+             "$ior -ORBsvcconf svc.conf";
 
-
-	$GF = Process::Create ("Generic_Factory".$Process::EXE_EXT, $args);
+  $GF = Process::Create ("Generic_Factory".$Process::EXE_EXT, $args);
 }
 
 sub client
 {
-	my $exe = sprintf ("client".$Process::EXE_EXT." %s %s %s -ORBobjrefstyle url -ORBport %d -ORBnameserviceior %s -ORBsvcconf client.conf",
-	                   $other,
-	                   $debug,
-	                   $cm,
-	                   $clport,
-	                   $ior);
+  my $exe = "client$Process::EXE_EXT $other $debug $cm -ORBobjrefstyle url ".
+            "-ORBport $clport -ORBnameserviceior $ior -ORBsvcconf $c_conf";
 
-	for ($j = 0; $j < $n; $j++)
-	{
-		system ($exe);
-	}
+  for ($j = 0; $j < $n; $j++)
+  {
+    system ($exe);
+  }
 }
 
 # Parse the arguments
 
 for ($i = 0; $i <= $#ARGV; $i++)
 {
-	SWITCH: {
-		if ($ARGV[$i] eq "-h" || $ARGV[$i] eq "-?")
-		{
-			print "run_test [-n num] [-leave] [-d] [-h] [-cm] [-sm] [-ns|sv|ff|cl|gf]\n";
-			print "\n";
-			print "-n num              -- runs the client num times\n";
-			print "-leave              -- leaves the servers running and their windows open\n";
-			print "-d                  -- runs each in debug mode\n";
-			print "-h                  -- prints this information\n";
-			print "-cm                 -- use more than one thread in the client\n";
-			print "-sm                 -- use more than one thread in the server\n";
-			print "-ns -sv -ff -cl -gf -- runs only one of the executables\n";
-			exit;
-		}
-		if ($ARGV[$i] eq "-n")
-		{
-			$n = $ARGV[$i + 1];
-			$i++;
-			last SWITCH;
-		}
-		if ($ARGV[$i] eq "-d")
-		{
-			$debug = $debug." -d";
-			last SWITCH;
-		}
-		if ($ARGV[$i] eq "-cm")
-		{
-			$cm = "-n ".$num_threads;
-			last SWITCH;
-		}
-		if ($ARGV[$i] eq "-sm")
-		{
-			$sm = "-n ".$num_threads;
-			last SWITCH;
-		}
-		if ($ARGV[$i] eq "-leave")
-		{
-			$leave = 0;
-			last SWITCH;
-		}
-		if ($ARGV[$i] eq "-ns")
-		{
-			name_server ();
-			exit;
-		}
-		if ($ARGV[$i] eq "-sv")
-		{
-			read_nsior ();
-			server ();
-			exit;
-		}
-		if ($ARGV[$i] eq "-ff")
-		{
-			read_nsior ();
-			factory_finder ();
-			exit;
-		}
-		if ($ARGV[$i] eq "-gf")
-		{
-			read_nsior ();
-			generic_factory ();
-			exit;
-		}
-		if ($ARGV[$i] eq "-cl")
-		{
-			read_nsior ();
-			client ();
-			exit;
-		}
-		$other = $other." ".$ARGV[$i];
-	}
+  SWITCH: 
+  {
+    if ($ARGV[$i] eq "-h" || $ARGV[$i] eq "-?")
+    {
+      print "run_test [-n num] [-leave] [-d] [-h] [-cm] [-sm] [-ns|sv|ff|cl|gf]\n";
+      print "\n";
+      print "-n num              -- runs the client num times\n";
+      print "-leave              -- leaves the servers running and their windows open\n";
+      print "-d                  -- runs each in debug mode\n";
+      print "-h                  -- prints this information\n";
+      print "-cm                 -- use more than one thread in the client\n";
+      print "-sm                 -- use more than one thread in the server\n";
+      print "-ns -sv -ff -cl -gf -- runs only one of the executables\n";
+      exit;
+    }
+    if ($ARGV[$i] eq "-n")
+    {
+      $n = $ARGV[$i + 1];
+      $i++;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-d")
+    {
+      $debug = $debug." -d";
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-cm")
+    {
+      $cm = "-n ".$num_threads;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-sm")
+    {
+      $sm = "-n ".$num_threads;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-leave")
+    {
+      $leave = 0;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-ns")
+    {
+      name_server ();
+      exit;
+    }
+    if ($ARGV[$i] eq "-sv")
+    {
+      read_nsior ();
+      server ();
+      exit;
+    }
+    if ($ARGV[$i] eq "-ff")
+    {
+      read_nsior ();
+      factory_finder ();
+      exit;
+    }
+    if ($ARGV[$i] eq "-gf")
+    {
+      read_nsior ();
+      generic_factory ();
+      exit;
+    }
+    if ($ARGV[$i] eq "-cl")
+    {
+      read_nsior ();
+      client ();
+      exit;
+    }
+    if ($ARGV[$i] eq "-customconf")  #secret flag from testall.pl
+    {
+      #use a different set of *.conf files
+      $c_conf = "c.conf";
+      $s_conf = "s.conf";
+      last SWITCH; 
+    }
+    $other = $other." ".$ARGV[$i];
+  }
 }
 
 name_server ();
@@ -225,10 +220,10 @@ client ();
 
 if ($leave)
 {
-	$GF->Kill ();
-	$FF->Kill ();
-	$SV->Kill ();
-	$NS->Kill ();
+  $GF->Kill ();
+  $FF->Kill ();
+  $SV->Kill ();
+  $NS->Kill ();
 }
 
 
@@ -236,9 +231,9 @@ exit ();
 
 if ($^O eq "MSWin32")
 {
-	system ("del ".$nsiorfile);
+  system ("del ".$nsiorfile);
 }
 else
 {
-	system ("rm ".$nsiorfile);
+  system ("rm ".$nsiorfile);
 }
