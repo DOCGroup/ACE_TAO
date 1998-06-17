efddf2bcdda4453ea6c86197d2edcf48d98d0f6f
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-06-17 18:43:53 +0000

    *** empty log message ***

--- TAO/orbsvcs/tests/Logger/svr.cpp
+++ TAO/orbsvcs/tests/Logger/svr.cpp
@@ -31,8 +31,8 @@ main (int argc, char ** argv)
 
       // Initialize the Object Adapter
       CORBA::Object_var poa_object =
-	orb->resolve_initial_references("RootPOA");
-      if (CORBA::is_nil(poa_object.in()))
+	orb->resolve_initial_references ("RootPOA");
+      if (CORBA::is_nil (poa_object.in ()))
 	ACE_ERROR_RETURN ((LM_ERROR,
 			   " (%P|%t) Unable to initialize the POA.\n"),
 			  1);
