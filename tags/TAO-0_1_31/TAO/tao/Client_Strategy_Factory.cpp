4dd95059663896aca2cfb62518fcae1434ff5b18
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-22 04:59:09 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-0_1_31'.

---
+++ tags/TAO-0_1_31/TAO/tao/Client_Strategy_Factory.cpp
@@ -0,0 +1,12 @@
+// $Id$
+
+#include "tao/corba.h"
+
+TAO_Client_Strategy_Factory::TAO_Client_Strategy_Factory (void)
+{
+}
+
+TAO_Client_Strategy_Factory::~TAO_Client_Strategy_Factory (void)
+{
+}
+
