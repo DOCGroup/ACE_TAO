e18c1fa80ab186ff98acb53d67d92d9e24b8c0fc
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-22 04:59:09 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-0_1_31'.

--- java/examples/Logger/simple-server/LoggerConstants.java
+++
@@ -1,7 +0,0 @@
-// package ACE.Logger;
-
-public class LoggerConstants
-{
-  final public static int DEFAULT_SERVER_PORT = 4000;
-  final public static String DEFAULT_SERVER_HOSTNAME = "localhost";
-}
