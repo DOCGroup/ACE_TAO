360db5044c24ec2b4e118e111d05e86c54521666
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-24 17:02:31 +0000

    *** empty log message ***

--- trunk/TAO/tests/POA/RootPOA/RootPOA.cpp
+++ trunk/TAO/tests/POA/RootPOA/RootPOA.cpp
@@ -30,11 +30,11 @@ main (int argc, char **argv)
 
   // Initilize the ORB
   CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0, env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("CORBA::ORB_init");
-      return -1;
-    }
+
+  TAO_CHECK_ENV_PRINT_RETURN (env,
+                              "CORBA::ORB_init",
+                              -1);
+
 
   // Resolve the initial references for the name RootPOA thus getting
   // an object of type CORBA::Object.
@@ -46,35 +46,30 @@ main (int argc, char **argv)
   PortableServer::POA_var root_poa =
     PortableServer::POA::_narrow (obj.in (), env);
 
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
+  TAO_CHECK_ENV_PRINT_RETURN (env,
+                              "PortableServer::POA::_narrow",
+                              -1);
 
   // Get the name of the root POA.
   CORBA::String_var poa_name =
     root_poa->the_name (env);
 
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
+  TAO_CHECK_ENV_PRINT_RETURN (env,
+                              "PortableServer::POA::_narrow",
+                              -1);
 
   ACE_DEBUG ((LM_DEBUG,
               "The RootPOA is : %s\n",
               poa_name.in ()));
-  
+
   // Destroy the POA object,also destroys the child POAs if any.
   root_poa->destroy (CORBA::B_TRUE,
                      CORBA::B_TRUE,
                      env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::destroy");
-      return -1;
-    }
+
+  TAO_CHECK_ENV_PRINT_RETURN (env,
+                              "PortableServer::POA::destroy",
+                              -1);
 
   return 0;
 }
