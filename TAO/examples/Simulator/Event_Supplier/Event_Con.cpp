f71d30a5688d32f02e8c66f34530f0c3346e91d2
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-26 21:43:53 +0000

    ChangeLog entry: Fri Jun 26 16:45:00 1998  Michael Kircher  <mk1@cs.wustl.edu>

--- TAO/examples/Simulator/Event_Supplier/Event_Con.cpp
+++ TAO/examples/Simulator/Event_Supplier/Event_Con.cpp
@@ -70,14 +70,17 @@ Demo_Consumer::open_consumer (RtecEventChannelAdmin::EventChannel_ptr ec,
       rt_info_ = server->create (my_name, TAO_TRY_ENV);
 
       server->set (rt_info_,
-                   ORBSVCS_Time::zero,
-                   ORBSVCS_Time::zero,
-                   ORBSVCS_Time::zero,
-                   0,
-                   RtecScheduler::VERY_LOW_IMPORTANCE,
-                   ORBSVCS_Time::zero,
-                   1,
-                   TAO_TRY_ENV);
+                       RtecScheduler::VERY_LOW_CRITICALITY,
+                       ORBSVCS_Time::zero,
+                       ORBSVCS_Time::zero,
+                       ORBSVCS_Time::zero,
+                       2500000,
+                       RtecScheduler::VERY_LOW_IMPORTANCE,
+                       ORBSVCS_Time::zero,
+                       1,
+                       RtecScheduler::OPERATION,
+                       TAO_TRY_ENV);
+
 
       // Create the event that we're registering for.
 
