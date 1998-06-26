32b2bf0f32d5eb0f6fd89f61bd47f1900e3ca68d
gokhale <gokhale@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-26 17:59:21 +0000

    *** empty log message ***

--- TAO/TAO_IDL/be/be_visitor_operation/rettype_post_docall_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_operation/rettype_post_docall_cs.cpp
@@ -48,9 +48,15 @@ int
 be_visitor_operation_rettype_post_docall_cs::visit_interface (be_interface *node)
 {
   TAO_OutStream *os = this->ctx_->stream (); // grab the out stream
+  be_type *bt; // return type
+
+  if (this->ctx_->alias ()) // a typedefed return type
+    bt = this->ctx_->alias ();
+  else
+    bt = node;
 
   os->indent ();
-  *os << "_tao_retval = " << node->name ()
+  *os << "_tao_retval = " << bt->name ()
       << "::_narrow (_tao_base_retval, _tao_environment);" << be_nl;
   *os << "CORBA::release (_tao_base_retval);\n";
   return 0;
@@ -61,9 +67,15 @@ be_visitor_operation_rettype_post_docall_cs::
 visit_interface_fwd (be_interface_fwd *node)
 {
   TAO_OutStream *os = this->ctx_->stream (); // grab the out stream
+  be_type *bt; // return type
+
+  if (this->ctx_->alias ()) // a typedefed return type
+    bt = this->ctx_->alias ();
+  else
+    bt = node;
 
   os->indent ();
-  *os << "_tao_retval = " << node->name ()
+  *os << "_tao_retval = " << bt->name ()
       << "::_narrow (_tao_base_retval, _tao_environment);" << be_nl;
   *os << "CORBA::release (_tao_base_retval);\n";
   return 0;
