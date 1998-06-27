68508a7fc7122cf4d42afae4550e1ca4ea97e0f7
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-27 12:08:03 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_17'.

---
+++ tags/ACE-4_5_17/ace/inc_user_config.h
@@ -0,0 +1,10 @@
+/* -*- C++ -*- */
+// $Id$
+
+// Use this file to include config.h instead of including
+// config.h directly.
+
+#if !defined (ACE_INC_USER_CONFIG_H)
+#define ACE_INC_USER_CONFIG_H
+#include "ace/config.h"
+#endif /* ACE_INC_USER_CONFIG_H */
