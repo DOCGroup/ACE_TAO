9841078ee585185b16baed6bc99b5c9e41ab4a7d
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-27 16:27:03 +0000

    use ACE_CAST_CONST instead of const in ACE_CU64_TO_CU32

--- ace/Basic_Types.h
+++ ace/Basic_Types.h
@@ -288,7 +288,7 @@ typedef ACE_UINT16 ACE_USHORT16;
 # define ACE_CU64_TO_CU32(n) ((n).lo ())
 #else  /* ! ACE_LACKS_LONGLONG_T */
 # define ACE_U64_TO_U32(n) (ACE_static_cast (ACE_UINT32, (n)))
-# define ACE_CU64_TO_CU32(n) (ACE_static_cast (const ACE_UINT32, (n)))
+# define ACE_CU64_TO_CU32(n) (ACE_static_cast (ACE_CAST_CONST ACE_UINT32, (n)))
 #endif /* ! ACE_LACKS_LONGLONG_T */
 
 // The number of bytes in a void *.
