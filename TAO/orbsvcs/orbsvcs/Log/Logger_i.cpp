d178f3929adc2ad71feabe749c865a680e659515
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-06-27 15:46:45 +0000

    *** empty log message ***

--- TAO/orbsvcs/orbsvcs/Log/Logger_i.cpp
+++ TAO/orbsvcs/orbsvcs/Log/Logger_i.cpp
@@ -34,13 +34,13 @@ Logger_i::~Logger_i (void)
 // the ACE_Log_Priority enumerated type
 
 ACE_Log_Priority
-priority_conversion (const Logger::Log_Priority prior1) 
+Logger_i::priority_conversion (Logger::Log_Priority priority)
 {
-  if (prior1 == Logger::LM_MAX)
+  if (priority == Logger::LM_MAX)
     return LM_MAX;
   else
     {
-      int pval = ACE_static_cast (int, prior1);
+      int pval = ACE_static_cast (int, priority);
 
       return ACE_static_cast (ACE_Log_Priority,
                               ACE_POW (pval));
@@ -51,35 +51,33 @@ void
 Logger_i::log (const Logger::Log_Record &log_rec,
 	       CORBA::Environment &_env)
 {
-  ACE_Log_Record rec (priority_conversion (log_rec.type),
+  ACE_Log_Record rec (this->priority_conversion (log_rec.type),
 		      log_rec.time,
                       log_rec.app_id);
   
-  // Create a ACE_Log_Record in order to leverage existing logging
-  // code.The log2() is used because we can't specify values for enum
-  // tags in IDL, but ACE_Log_Priority values are powers of 2
-  // priority_names_() is essentially used to cast to the enum type
+  // Create an <ACE_Log_Record> to leverage existing logging code. The
+  // <ACE::log2> method is used because we can't specify values for
+  // enum tags in IDL, but <ACE_Log_Priority> values are powers of 2.
+  // Thus, <priority_conversion> is used to transform to an enum type.
   
   ASYS_TCHAR msgbuf[ACE_MAXLOGMSGLEN + 1];
   ACE_OS::strncpy (msgbuf, 
                    log_rec.msg_data,
                    ACE_MAXLOGMSGLEN);
-  // I don't think this is a good way to do this. Suggestions?
+  // I don't think this is a good way to do this. Suggestions?  @@
+  // Matt, please make sure that the size of the array <msg_data> is
+  // no larger than ACE_MAXLOGMSGLEN...
 
+  // Set <ACE_Log_Record.msg_data> to the value stored in <msgbuf>.
   rec.msg_data (msgbuf);
-  // Set ACE_Log_Record.msg_data to the value stored in msgbuf
   
-#if 0
-  // @@ This is currently unused.  Remove them?
-  hostent rhost;
-  char storage[MAXHOSTNAMELEN + 1];
-#endif /* 0 */
+  CORBA::Long addr = log_rec.host_addr;
 
-  ACE_INET_Addr addy (ACE_static_cast (int, 0),
-                      ACE_static_cast (ACE_UINT32,
-                                       log_rec.host_addr));
-  // The constructor for ACE_INET_Addr requires a port number, which
+  // The constructor for <ACE_INET_Addr> requires a port number, which
   // is not relevant in this context, so we give it 0.
+  ACE_INET_Addr addy (ACE_static_cast (u_short, 0),
+                      ACE_static_cast (ACE_UINT32,
+                                       addr));
   
   ASYS_TCHAR namebuf[MAXHOSTNAMELEN + 1];
   addy.get_host_name (namebuf,
