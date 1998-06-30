f518fb033d0d79b4666cfad6d899bd81c6ca2fea
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-06-30 15:37:44 +0000

    ChangeLog entry: Tue Jun 30 10:40:00 1998  Michael Kircher  <mk1@cs.wustl.edu>

--- TAO/tao/Client_Strategy_Factory.cpp
+++ TAO/tao/Client_Strategy_Factory.cpp
@@ -10,3 +10,8 @@ TAO_Client_Strategy_Factory::~TAO_Client_Strategy_Factory (void)
 {
 }
 
+ACE_Lock *
+TAO_Client_Strategy_Factory::create_iiop_profile_lock (void)
+{
+  return 0;
+}
--- TAO/tao/default_client.cpp
+++ TAO/tao/default_client.cpp
@@ -8,6 +8,7 @@
 #endif /* ! __ACE_INLINE__ */
 
 TAO_Default_Client_Strategy_Factory::TAO_Default_Client_Strategy_Factory (void)
+: iiop_profile_lock_type_ (TAO_THREAD_LOCK)
 {
 }
 
@@ -22,10 +23,45 @@ TAO_Default_Client_Strategy_Factory::init (int argc, char *argv[])
 }
 
 int
-TAO_Default_Client_Strategy_Factory::parse_args (int /* argc */, char ** /* argv */)
+TAO_Default_Client_Strategy_Factory::parse_args (int argc, char ** argv)
 {
-  // no args to parse at this time
+  ACE_TRACE ("TAO_Default_Client_Strategy_Factory::parse_args");
+
+  int curarg;
+
+  for (curarg = 0; curarg < argc && argv[curarg]; curarg++)
+    if (ACE_OS::strcmp (argv[curarg], "-ORBiiopprofilelock") == 0)
+      {
+        curarg++;
+        if (curarg < argc)
+          {
+            char *name = argv[curarg];
+
+            if (ACE_OS::strcasecmp (name, "thread") == 0)
+              this->iiop_profile_lock_type_ = TAO_THREAD_LOCK;
+            else if (ACE_OS::strcasecmp (name, "null") == 0)
+              this->iiop_profile_lock_type_ = TAO_NULL_LOCK;
+          }
+      }
+  
   return 0;
+}
+
+ACE_Lock *
+TAO_Default_Client_Strategy_Factory::create_iiop_profile_lock (void)
+{
+  ACE_Lock* the_lock = 0;
+
+  if (this->iiop_profile_lock_type_ == TAO_NULL_LOCK)
+    ACE_NEW_RETURN (the_lock,
+		    ACE_Lock_Adapter<ACE_SYNCH_NULL_MUTEX> (),
+		    0);
+  else
+    ACE_NEW_RETURN (the_lock,
+		    ACE_Lock_Adapter<ACE_SYNCH_MUTEX> (),
+		    0);
+
+  return the_lock;
 }
 
 ACE_FACTORY_DEFINE (TAO, TAO_Default_Client_Strategy_Factory)
