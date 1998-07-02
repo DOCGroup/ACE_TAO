dff1a3afc73877a85d40b0017f78720056ef2b35
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-02 14:15:15 +0000

    (test_functionality): use timer_id outside of an assert, so that we don't get compile warnings with ACE_NDEBUG about it being unused.

--- examples/Reactor/Misc/test_timer_queue.cpp
+++ examples/Reactor/Misc/test_timer_queue.cpp
@@ -8,7 +8,7 @@ class Example_Handler : public ACE_Event_Handler
 {
 public:
   Example_Handler (void)
-    : count_ (0) 
+    : count_ (0)
   {}
 
   virtual int handle_timeout (const ACE_Time_Value &, const void *arg)
@@ -17,9 +17,9 @@ public:
     // size as a long on all current ACE platforms.
     int times = (int) (long) arg;
 
-    ACE_DEBUG ((LM_DEBUG, 
-		"yow, the time has come and gone %d times %d, Horatio!\n", 
-		this->count_++,
+    ACE_DEBUG ((LM_DEBUG,
+                "yow, the time has come and gone %d times %d, Horatio!\n",
+                this->count_++,
                 times));
     return 0;
   }
@@ -35,24 +35,29 @@ test_functionality (ACE_Timer_Queue *tq)
 
   ACE_ASSERT (tq->is_empty ());
   ACE_ASSERT (ACE_Time_Value::zero == ACE_Time_Value (0));
-  int timer_id; 
-  
+  int timer_id;
+
   timer_id = tq->schedule (&eh, (const void *) 1, ACE_OS::gettimeofday ());
+
+  // Use timer_id outside of an assert, so that we don't get compile
+  // warnings with ACE_NDEBUG about it being unused.
+  if (timer_id == -1)
+    ACE_ERROR ((LM_ERROR, "%p\n", "schedule () failed"));
   ACE_ASSERT (timer_id != -1);
 
   ACE_ASSERT (tq->schedule (&eh, (const void *) 42,
-			   ACE_OS::gettimeofday ()) != -1);
+                           ACE_OS::gettimeofday ()) != -1);
   ACE_ASSERT (tq->schedule (&eh, (const void *) 42,
-			   ACE_OS::gettimeofday ()) != -1);
+                           ACE_OS::gettimeofday ()) != -1);
   ACE_ASSERT (tq->cancel (timer_id) == 1);
   ACE_ASSERT (!tq->is_empty ());
 
   ACE_ASSERT (tq->expire () == 2);
 
   ACE_ASSERT (tq->schedule (&eh, (const void *) 4, ACE_OS::gettimeofday
-			   ()) != -1);
+                           ()) != -1);
   ACE_ASSERT (tq->schedule (&eh, (const void *) 5, ACE_OS::gettimeofday
-			   ()) != -1);
+                           ()) != -1);
   ACE_ASSERT (tq->cancel (&eh) == 2);
   ACE_ASSERT (tq->is_empty ());
   ACE_ASSERT (tq->expire () == 0);
@@ -62,7 +67,7 @@ struct Timer_Queues
 {
   ACE_Timer_Queue *queue_;
   // Pointer to the subclass of <ACE_Timer_Queue> that we're testing.
-  
+
   const char *name_;
   // Name of the Queue that we're testing.
 };
