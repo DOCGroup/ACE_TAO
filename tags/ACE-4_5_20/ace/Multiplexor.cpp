cd041b8051d333d2b83830f9122cae9758d00dbd
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-03 18:49:10 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_20'.

---
+++ tags/ACE-4_5_20/ace/Multiplexor.cpp
@@ -0,0 +1,13 @@
+// Multiplexor.cpp
+// $Id$
+
+#if defined (ACE_HAS_THREADS)
+
+#define ACE_BUILD_DLL
+#include "ace/Multiplexor.h"
+
+#if !defined (__ACE_INLINE__)
+#include "ace/Multiplexor.i"
+#endif /* __ACE_INLINE__ */
+
+#endif /* ACE_HAS_THREADS */
