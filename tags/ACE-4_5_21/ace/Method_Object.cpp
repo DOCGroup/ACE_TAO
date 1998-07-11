b9d6beea28a943eb6fff8fbe0d0a6d796b48196f
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-11 03:17:49 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_21'.

---
+++ tags/ACE-4_5_21/ace/Method_Object.cpp
@@ -0,0 +1,14 @@
+// Method_Object.cpp
+// $Id$
+
+#define ACE_BUILD_DLL
+#include "ace/Method_Object.h"
+
+ACE_Method_Object::ACE_Method_Object (void) 
+{
+}
+
+ACE_Method_Object::~ACE_Method_Object (void)
+{
+}
+
