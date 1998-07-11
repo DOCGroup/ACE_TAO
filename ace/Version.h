94e49ea4a4038b5f3b3d9e6221e0016a71777396
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-11 03:26:49 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-0_1_38'.

--- ace/Version.h
+++
@@ -1,7 +0,0 @@
-// $Id$
-// This is an automatically generated file.
-
-#define ACE_MAJOR_VERSION (4u)
-#define ACE_MINOR_VERSION (5u)
-#define ACE_BETA_VERSION (21u)
-#define ACE_VERSION "4.5.21\0"
