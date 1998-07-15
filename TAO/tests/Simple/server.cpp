b1dc10202b22440f709811fbf62e369f08c1966b
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-15 21:24:38 +0000

    New Simple Test

---
+++ TAO/tests/Simple/server.cpp
@@ -0,0 +1,35 @@
+// $Id$
+
+#include "server_impl.h"
+
+int
+main (int argc, char *argv[])
+{
+  Server_Impl server;
+
+  ACE_DEBUG ((LM_DEBUG, "\n\tSimple_Server\n\n"));
+  TAO_TRY
+    {
+      if (server.init (argc, argv, TAO_TRY_ENV) == -1)
+        return 1;
+      else
+        {
+          server.run (TAO_TRY_ENV);
+          TAO_CHECK_ENV;
+        }
+    }
+  TAO_CATCH (CORBA::SystemException, sysex)
+    {
+      ACE_UNUSED_ARG (sysex);
+      TAO_TRY_ENV.print_exception ("System Exception");
+      return -1;
+    }
+  TAO_CATCH (CORBA::UserException, userex)
+    {
+      ACE_UNUSED_ARG (userex);
+      TAO_TRY_ENV.print_exception ("User Exception");
+      return -1;
+    }
+  TAO_ENDTRY;
+  return 0;
+}
---
+++ TAO/tests/Simple/simple_object_impl.cpp
@@ -0,0 +1,38 @@
+// $Id$
+
+#include "simple_object_impl.h"
+#include "tao/corba.h"
+
+// Constructor
+
+Simple_Impl::Simple_Impl (const char *)
+{
+}
+
+
+// Destructor
+
+Simple_Impl::~Simple_Impl (void)
+{
+}
+
+// Cube a long
+
+CORBA::Long
+Simple_Impl::simple_method (CORBA::Long l, CORBA::Environment &env)
+{
+  ACE_UNUSED_ARG (env);
+  return l * l * l;
+}
+
+// Shutdown.
+
+void Simple_Impl::shutdown (CORBA::Environment &env)
+{
+  ACE_UNUSED_ARG (env);
+
+  ACE_DEBUG ((LM_DEBUG, "%s\n", "Simple_Impl is shutting down"));
+
+  TAO_ORB_Core_instance ()->orb ()->shutdown ();
+}
+
