67a647a6fc385f0972742cef3bcaa7376a8ec339
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-17 23:39:41 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/orbsvcs/Naming/Naming_Utils.h
+++ trunk/TAO/orbsvcs/orbsvcs/Naming/Naming_Utils.h
@@ -34,10 +34,12 @@ class TAO_ORBSVCS_Export TAO_Naming_Server
   //
   // = DESCRIPTION
   //    This class takes an orb and Poa reference and activates the
-  //    NamingService naming context object under that. It also
+  //    NamingService naming context object under that.  It also
   //    defines the operator -> so that NamingContext functions like
   //    bind, unbind .. can be called on a NameServer object.
 public:
+  // = Initialization and termination methods.
+
   TAO_Naming_Server (void);
   //Default constructor.
 
@@ -59,18 +61,18 @@ public:
   // if this name server instance is part of a naming tree that
   // resides under the default name server.
 
+  ~TAO_Naming_Server (void);
+  // Destructor.
+
   NS_NamingContext &GetNamingContext (void);
   // Returns the "NameService" NamingContext implementation object
   // reference.
 
   CORBA::String naming_service_ior (void);
-  // Returns the ior of the naming service.
+  // Returns the IOR of the naming service.
 
-  CosNaming::NamingContext_ptr operator -> (void) const;
-  // Returns a NamingContext_ptr.
-
-  ~TAO_Naming_Server (void);
-  // Destructor.
+  CosNaming::NamingContext_ptr operator-> (void) const;
+  // Returns a <NamingContext_ptr>.
 
 private:
   NS_NamingContext naming_context_impl_;
