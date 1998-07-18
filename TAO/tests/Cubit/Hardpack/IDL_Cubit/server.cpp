d66173281debd33e2ae3ba17dfdeda63a7ec7764
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-18 22:31:08 +0000

    *** empty log message ***

---
+++ TAO/tests/Cubit/Hardpack/IDL_Cubit/server.cpp
@@ -0,0 +1,19 @@
+// $Id$
+
+#include "server_i.h"
+
+int
+main (int argc, char *argv[])
+{
+  Cubit_Server cubit_server;
+
+  ACE_DEBUG ((LM_DEBUG,
+              "\n\tIDL_Cubit:SERVER\n\n"));
+  if (cubit_server.init (argc, argv) == -1)
+        return 1;
+  else
+    {
+      cubit_server.run ();
+    }
+  return 0;
+}
