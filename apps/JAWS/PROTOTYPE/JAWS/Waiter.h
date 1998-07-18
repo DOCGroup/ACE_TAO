d14d0ba531246da4611e850242361e64c1e653b5
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-18 04:15:43 +0000

    Changes that allows the ACE_Handler to communicate with the  appropriate thread.

--- apps/JAWS/PROTOTYPE/JAWS/Waiter.h
+++ apps/JAWS/PROTOTYPE/JAWS/Waiter.h
@@ -7,7 +7,8 @@
 #include "ace/Singleton.h"
 
 #include "JAWS/Assoc_Array.h"
-#include "JAWS/Data_Block.h"
+
+class JAWS_IO_Handler;
 
 typedef JAWS_Assoc_Array<ACE_Thread_ID, JAWS_IO_Handler *> JAWS_Waiter_Base;
 typedef JAWS_Assoc_Array_Iterator<ACE_Thread_ID, JAWS_IO_Handler *>
@@ -17,12 +18,18 @@ class JAWS_Waiter : public JAWS_Waiter_Base
 {
 public:
   JAWS_Waiter (void);
-  virtual ~JAWS_Waiter (void);
+  ~JAWS_Waiter (void);
 
   JAWS_Waiter_Base_Iterator &iter (void);
-  // returns an iterator to the headers container
+  // Returns an iterator to the headers container.
 
-  JAWS_IO_Handler * wait_for_completion (void);
+  int index (void);
+  // Returns the index into the table associated with calling thread.
+
+  JAWS_IO_Handler * wait_for_completion (int i = -1);
+  // The entry point for this class, handles outstanding asynchronous
+  // events.  Can optionally accept a parameter that points to which
+  // table entry to return.
 
 private:
   JAWS_Waiter_Base_Iterator iter_;
