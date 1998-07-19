298472a07420e8bdc23647f7b3da79f5bada4dfc
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-19 03:17:44 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-0_1_39'.

---
+++ tags/TAO-0_1_39/TAO/orbsvcs/orbsvcs/Log/Logger_i.cpp
@@ -0,0 +1,109 @@
+// $Id$
+
+#include "ace/ACE.h"
+#include "orbsvcs/LoggerC.h"
+#include "orbsvcs/Log/Logger_i.h"
+#include "ace/Log_Record.h"
+
+Logger_ptr
+Logger_Factory_i::make_logger (const char *name,
+                               CORBA::Environment &_env)
+{
+  Logger_i *l;
+
+  ACE_NEW_RETURN (l,
+		  Logger_i (name),
+		  0);
+
+  return l->_this (_env);
+}
+
+Logger_Factory_i::Logger_Factory_i (void)
+{
+  // Do nothing.
+}
+
+Logger_i::Logger_i (const char *name)
+  : name_ (ACE_OS::strdup (name))
+{
+  // Do nothing
+}
+
+Logger_i::~Logger_i (void)
+{
+  ACE_OS::free (this->name_);
+}
+
+
+ACE_Log_Priority
+Logger_i::priority_conversion (Logger::Log_Priority priority)
+{
+  if (priority == Logger::LM_MAX)
+    return LM_MAX;
+  else
+    {
+      int pval = ACE_static_cast (int, priority);
+
+      return ACE_static_cast (ACE_Log_Priority,
+                              1 << pval);
+      // (1 << pval) == 2^pval. <ACE_Log_Priority> are powers of 2.
+    }
+}
+
+u_long
+Logger_i::verbosity_conversion (Logger::Verbosity_Level verbosity_level)
+{
+  u_long pval = ACE_static_cast (int, verbosity_level);
+  return 1 << pval;
+}
+
+void
+Logger_i::log (const Logger::Log_Record &log_rec,
+	       CORBA::Environment &_env)
+{
+  ACE_Time_Value temp (log_rec.time);
+
+  // Create an <ACE_Log_Record> to leverage existing logging
+  // code. Since Logger::Log_Priority enum tags don't cleanly map to
+  // ACE_Log_Priority tags, <priority_conversion> is used to coerce
+  // the mapping.
+  ACE_Log_Record rec (this->priority_conversion (log_rec.type),
+		      ACE_Time_Value (log_rec.time),
+                      log_rec.app_id);
+  
+  // Create a temporary buffer for manipulating the logging message,
+  // adding additional space for formatting characters..
+  ASYS_TCHAR msgbuf [ACE_MAXLOGMSGLEN + 4];
+
+  // Format the message for proper display.
+  ACE_OS::strcpy (msgbuf, "\n::");
+
+  // Copy the message data into the temporary buffer
+  ACE_OS::strncat (msgbuf, 
+		   log_rec.msg_data,
+                   ACE_MAXLOGMSGLEN);
+
+  // Set <ACE_Log_Record.msg_data> to the value stored in <msgbuf>.
+  rec.msg_data (msgbuf);
+
+  CORBA::Long addr = log_rec.host_addr;
+
+  // The constructor for <ACE_INET_Addr> requires a port number, which
+  // is not relevant in this context, so we give it 0.
+  ACE_INET_Addr addy (ACE_static_cast (u_short, 0),
+                      ACE_static_cast (ACE_UINT32,
+                                       addr));
+
+  // Create a buffer and fill it with the host name of the logger
+  ASYS_TCHAR namebuf[MAXHOSTNAMELEN + 1];
+  addy.get_host_name (namebuf,
+                      MAXHOSTNAMELEN);
+  
+  rec.print (namebuf,
+	     this->verbosity_level_,
+	     stderr);
+  // Print out the logging message to stderr with the given level of
+  // verbosity
+}
+
+// @@ Matt, please add the method for verbosity().
---
+++ tags/TAO-0_1_39/TAO/tao/NVList.h
@@ -0,0 +1,161 @@
+// This may look like C, but it's really -*- C++ -*-
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    TAO
+//
+// = FILENAME
+//    NVList.h
+//
+// = AUTHOR
+//     Copyright 1994-1995 by Sun Microsystems Inc.
+//     and
+//     Aniruddha Gokhale (additions, missing operations)
+//
+// ============================================================================
+
+#if !defined (TAO_NVLIST_H)
+#  define TAO_NVLIST_H
+
+class TAO_Export CORBA_NamedValue
+{
+  // = TITLE
+  // NamedValue ... these occur only in "NVList" (named value list) data
+  // structures.  The binary form of the data structure is frozen and
+  // visible to programs using it (e.g. from C).  The C++ class supports
+  // some programming discipline, e.g. to avoid memory leaks.
+  //
+  // They just represent parameters to calls.  The name is optional, and
+  // the value is packaged as an Any.  The flags indicate parameter
+  // mode, and some ownership rules for "top level" memory.
+public:
+  const char *name (void) const;
+  // optional name
+
+  CORBA::Any_ptr value (void) const;
+  // return the value
+
+  CORBA::Flags flags (void) const;
+  // return the parameter mode flag
+
+  ~CORBA_NamedValue (void);
+  // destructor - manages the name and value
+
+  // = Methods required for COM IUnknown support.
+
+  CORBA::ULong _incr_refcnt (void);
+  CORBA::ULong _decr_refcnt (void);
+
+private:
+  u_int refcount_;
+  // refcount used in release
+
+  CORBA::Any any_;
+  // holds the value
+
+  CORBA::Flags flags_;
+  // parameter mode flags
+
+  char *name_;
+  // optional IDL name of the parameter
+
+  CORBA_NamedValue (void);
+  // private constructor. Cannot be directly instantiated other than by its
+  // friends.
+
+  friend class CORBA_ORB;
+  friend class CORBA_NVList;
+  friend class CORBA_Request;
+};
+
+class TAO_Export CORBA_NVList
+{
+  // = TITLE
+  //   NVList ... this is used in the (client side) DII (Dynamic
+  //   Invocation Interface) to hold parameters, except for the return
+  //   parameter. It's used in the same role in the (server side) DSI
+  //   (Dynamic Skeleton Interface).
+  //
+  // = DESCRIPTION
+  //   Each user (client, server) provides the typecode and memory for
+  //   each parameter using an NVList, then talks to the ORB using a
+  //   Request or ServerRequest pseudo-object.  The ORB copies data
+  //   to/from the IPC messages (e.g. IIOP::Request, IIOP::Response)
+  //   as appropriate.
+public:
+  ~CORBA_NVList (void);
+  // destructor
+
+  CORBA::ULong count (void) const;
+  // return the current number of elements in the list
+
+  CORBA::NamedValue_ptr add (CORBA::Flags,
+                             CORBA::Environment &);
+  // add an element and just initialize the flags
+
+  CORBA::NamedValue_ptr add_item (const char *,
+                                  CORBA::Flags,
+                                  CORBA::Environment &);
+  // add an element and initialize its name and flags
+
+  CORBA::NamedValue_ptr add_value (const char *,
+                                   const CORBA::Any &,
+                                   CORBA::Flags,
+                                   CORBA::Environment &);
+  // initializes a value, name, and flags
+
+  CORBA::NamedValue_ptr add_item_consume (char *,
+                                          CORBA::Flags,
+                                          CORBA::Environment &);
+  // just like add_item. In addition, memory management of char * name is taken
+  // over by the NVList
+
+  CORBA::NamedValue_ptr add_value_consume (char *,
+                                           CORBA::Any_ptr,
+                                           CORBA::Flags,
+                                           CORBA::Environment &);
+  // just like add_value. In addition, the NVList controls the memory
+  // management of the char *name and Any *value parameter
+
+  CORBA::NamedValue_ptr item (CORBA::ULong n, CORBA::Environment &env);
+  // retrieve the item at the nth location. Raises Bounds
+
+
+  //  CORBA::Status
+  void remove (CORBA::ULong n, CORBA::Environment &env);
+  // remove element at index n. Raises Bounds
+
+  // = Methods required for COM IUnknown support
+
+  CORBA::ULong _incr_refcnt (void);
+  CORBA::ULong _decr_refcnt (void);
+
+private:
+  CORBA_NVList (void);
+  // constructor - cannot be instantiated directly other than through the
+  // ORB::create_list method
+
+  CORBA::NamedValue_ptr add_element (CORBA::Flags, CORBA::Environment &);
+  // helper to increase the list size. This is used by all the add_ methods of
+  // the NVList class
+
+  ACE_Unbounded_Queue<CORBA::NamedValue_ptr> values_;
+  // internal list of parameters stored as NamedValues
+
+  CORBA::ULong max_;
+  // maximum length of list
+
+  CORBA::ULong refcount_;
+  // maintains how many references exist to this object
+
+  friend class CORBA_ORB;
+  friend class CORBA_Request;
+};
+
+#if defined (__ACE_INLINE__)
+# include "tao/NVList.i"
+#endif /* __ACE_INLINE__ */
+
+#endif /* TAO_NVLIST_H */
