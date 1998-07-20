d7dac785f201e70ad24a597b57e78a5b3ebca25c
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-20 03:26:58 +0000

    (Iterator): reordered initializers to match declaration order.

--- trunk/apps/gperf/src/Iterator.cpp
+++ trunk/apps/gperf/src/Iterator.cpp
@@ -22,18 +22,18 @@ USA.  */
 
 #include "Iterator.h"
 
-// Constructor for Iterator. 
+// Constructor for Iterator.
 
-Iterator::Iterator (char *s, 
-		    int lo, 
-		    int hi, 
-		    int word_end, 
-		    int bad_val, 
-		    int key_end)
-  : end (key_end),
-    error_value (bad_val),
+Iterator::Iterator (char *s,
+                    int lo,
+                    int hi,
+                    int word_end,
+                    int bad_val,
+                    int key_end)
+  : str (s),
+    end (key_end),
     end_word (word_end),
-    str (s),
+    error_value (bad_val),
     hi_bound (hi),
     lo_bound (lo)
 {
@@ -42,46 +42,46 @@ Iterator::Iterator (char *s,
 // Provide an Iterator, returning the ``next'' value from the list of
 // valid values given in the constructor.
 
-int 
-Iterator::operator() (void) 
-{ 
+int
+Iterator::operator() (void)
+{
   // Variables to record the Iterator's status when handling ranges,
   // e.g., 3-12.
 
-  static int size;              
-  static int curr_value;           
+  static int size;
+  static int curr_value;
   static int upper_bound;
 
-  if (size) 
-    { 
-      if (++curr_value >= upper_bound) 
-        size = 0;    
-      return curr_value; 
-    }
-  else 
+  if (size)
     {
-      while (*str) 
+      if (++curr_value >= upper_bound)
+        size = 0;
+      return curr_value;
+    }
+  else
+    {
+      while (*str)
         switch (*str)
           {
           default: return error_value;
           case ',': str++; break;
           case '$': str++; return end_word;
-	  case '0': case '1': case '2': case '3': case '4':
-	  case '5': case '6': case '7': case '8': case '9':
-            for (curr_value = 0; isdigit (*str); str++) 
+          case '0': case '1': case '2': case '3': case '4':
+          case '5': case '6': case '7': case '8': case '9':
+            for (curr_value = 0; isdigit (*str); str++)
               curr_value = curr_value * 10 + *str - '0';
 
-            if (*str == '-') 
+            if (*str == '-')
               {
 
-                for (size = 1, upper_bound = 0; 
-                     isdigit (*++str); 
+                for (size = 1, upper_bound = 0;
+                     isdigit (*++str);
                      upper_bound = upper_bound * 10 + *str - '0');
 
-                if (upper_bound <= curr_value || upper_bound > hi_bound) 
+                if (upper_bound <= curr_value || upper_bound > hi_bound)
                   return error_value;
               }
-            return curr_value >= lo_bound && curr_value <= hi_bound 
+            return curr_value >= lo_bound && curr_value <= hi_bound
               ? curr_value : error_value;
           }
 
