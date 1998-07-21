d2b0e368269c0670661d3754f2848551eefff602
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-21 22:55:25 +0000

    Uses common Uniqueid now.

--- TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
+++ TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -5,36 +5,17 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 
 unshift @INC, '../../../../../bin';
 require Process;
+require Uniqueid;
 
-if ($^O eq "MSWin32")
-{
-  system ("ipconfig | find \"Address\">ipnum");
-
-  open (IPNUM, "ipnum");
-
-  read (IPNUM, $line, 80);
-
-  ($junk, $ip1, $ip2, $ip3, $ip4) = split (/: (\d+)\.(\d+)\.(\d+)\.(\d+)/, $line);
-
-  close IPNUM;
-
-  system ("del /q ipnum");
-
-  $uid = $ip4;
-}
-else
-{
-  $uid = getpwnam (getlogin ());
-}
-
-$nsport = 20000 + $uid;
+$nsport = 20000 + uniqueid ();
 $client_port = 0;
 $server_port = 0;
 $iorfile = "theior";
+$sleeptime = 3;
 
 $SV = Process::Create ("server".$Process::EXE_EXT, 
                        "-ORBport ".$server_port." -ORBnameserviceport ".$nsport." -ORBobjrefstyle url");
 
-sleep (2);
+sleep ($sleeptime);
 
 system ("client".$Process::EXE_EXT." -x -ORBnameserviceport ".$nsport." -ORBport ".$client_port );
