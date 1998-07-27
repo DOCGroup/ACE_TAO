14c94de0a70e6cd80c56b5b78a55f1ab95965d3a
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-27 22:38:12 +0000

    Enabled all the LifeCycle functionality and all looks good on NT.

--- trunk/TAO/tests/Quoter/run_test.pl
+++ trunk/TAO/tests/Quoter/run_test.pl
@@ -8,7 +8,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 
 unshift @INC, '../../../bin';
 require Process;
-require Uniqueid;
+#require Uniqueid;
 
 $nsiorfile = "theior";
 
@@ -18,12 +18,14 @@ $num_threads = 4;
 
 # amount of delay between running the servers
 
-$sleeptime = 1;
+$sleeptime = 2;
 
 # variables for parameters
 
-$nsport = 20000 + uniqueid ();
+#$nsport = 20000 + uniqueid ();
+$nsport = 0;
 $clport = 0;
+$lcport = 0;
 $svport = 0;
 $ffport = 0;
 $gfport = 0;
@@ -60,6 +62,17 @@ sub name_server
   $NS = Process::Create ($prog, $args);
 }
 
+sub lifecycle_server
+{
+  my $args = "$other -ORBport $lcport -ORBobjrefstyle url -ORBnameserviceior ".
+             "$ior -ORBsvcconf svc.conf";
+  my $prog = "..$DIR_SEPARATOR..$DIR_SEPARATOR"."orbsvcs$DIR_SEPARATOR".
+             "LifeCycle_Service$DIR_SEPARATOR".
+             "LifeCycle_Service$Process::EXE_EXT";
+  
+  $LC = Process::Create ($prog, $args);
+}
+
 sub server
 {
   my $args = "$other $debug $sm -ORBport $svport -ORBobjrefstyle url ".
@@ -78,7 +91,7 @@ sub factory_finder
 
 sub generic_factory
 {
-  my $args = "$other -ORBport $gfport -ORBobjrefstyle url -ORBnameserviceior ".
+  my $args = "$other -l -ORBport $gfport -ORBobjrefstyle url -ORBnameserviceior ".
              "$ior -ORBsvcconf svc.conf";
 
   $GF = Process::Create ("Generic_Factory".$Process::EXE_EXT, $args);
@@ -86,7 +99,7 @@ sub generic_factory
 
 sub client
 {
-  my $exe = "client$Process::EXE_EXT $other $debug $cm -ORBobjrefstyle url ".
+  my $exe = "client$Process::EXE_EXT $other -l $debug $cm -ORBobjrefstyle url ".
             "-ORBport $clport -ORBnameserviceior $ior -ORBsvcconf $c_conf";
 
   for ($j = 0; $j < $n; $j++)
@@ -103,13 +116,15 @@ for ($i = 0; $i <= $#ARGV; $i++)
   {
     if ($ARGV[$i] eq "-h" || $ARGV[$i] eq "-?")
     {
-      print "run_test [-n num] [-leave] [-onewin] [-d] [-h] [-cm] [-sm] [-ns|sv|ff|cl|gf]\n";
+      print "run_test [-n num] [-leave] [-onewin] [-d] [-h] [-nt] [-cm] [-sm] [-ns|sv|ff|cl|gf]\n";
       print "\n";
       print "-n num              -- runs the client num times\n";
       print "-leave              -- leaves the servers running and their windows open\n";
       print "-onewin             -- keeps all tests in one window on NT\n";
       print "-d                  -- runs each in debug mode\n";
       print "-h                  -- prints this information\n";
+      print "-nt num             -- number of threads in the client (twice for server)\n";
+      print "                       make sure this is before any -cm or -sm\n";
       print "-cm                 -- use more than one thread in the client\n";
       print "-sm                 -- use more than one thread in the server\n";
       print "-ns -sv -ff -gf -cl -- runs only one of the executables\n";
@@ -118,6 +133,12 @@ for ($i = 0; $i <= $#ARGV; $i++)
     if ($ARGV[$i] eq "-n")
     {
       $n = $ARGV[$i + 1];
+      $i++;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-nt")
+    {
+      $num_threads = $ARGV[$i + 1];
       $i++;
       last SWITCH;
     }
@@ -133,7 +154,7 @@ for ($i = 0; $i <= $#ARGV; $i++)
     }
     if ($ARGV[$i] eq "-sm")
     {
-      $sm = "-n ".$num_threads;
+      $sm = "-n ".($num_threads * 3);
       last SWITCH;
     }
     if ($ARGV[$i] eq "-leave")
@@ -194,6 +215,9 @@ name_server ();
 sleep $sleeptime;
 read_nsior ();
 
+lifecycle_server ();
+sleep $sleeptime;
+
 server ();
 sleep $sleeptime;
 
@@ -210,6 +234,7 @@ if ($leave)
   $GF->Kill ();
   $FF->Kill ();
   $SV->Kill ();
+  $LC->Kill ();
   $NS->Kill ();
 }
 
