61fd6c1f57f48b6087cb14f18d1106fa5de0b459
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-29 03:04:52 +0000

    moved ACE_TSS_Singleton<ACE_Dynamic, ACE_Null_Mutex> from Thread_Manager.cpp to Dynamic.cpp

--- trunk/ace/Dynamic.cpp
+++ trunk/ace/Dynamic.cpp
@@ -3,6 +3,8 @@
 
 #define ACE_BUILD_DLL
 #include "ace/Dynamic.h"
+#include "ace/Singleton.h"
+#include "ace/Synch_T.h"
 
 #if !defined (__ACE_INLINE__)
 #include "ace/Dynamic.i"
@@ -11,8 +13,13 @@
 ACE_RCSID(ace, Dynamic, "$Id$")
 
 ACE_Dynamic::ACE_Dynamic (void)
-  : is_dynamic_ (0) 
+  : is_dynamic_ (0)
 {
   ACE_TRACE ("ACE_Dynamic::ACE_Dynamic");
 }
 
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+  template class ACE_TSS_Singleton<ACE_Dynamic, ACE_Null_Mutex>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+  #pragma instantiate ACE_TSS_Singleton<ACE_Dynamic, ACE_Null_Mutex>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
