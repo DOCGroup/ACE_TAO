badb89bf3931d39acdc488a097c4af98742c315c
jxh <jxh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-29 22:05:29 +0000

    Added a reaper class which waits for threads to die.  Changes to other files to enable Asynchronous Accept calls.

---
+++ apps/JAWS/PROTOTYPE/JAWS/Reaper.cpp
@@ -0,0 +1,36 @@
+// $Id$
+
+#include "JAWS/Reaper.h"
+#include "JAWS/Concurrency.h"
+
+JAWS_Reaper::JAWS_Reaper (JAWS_Concurrency_Base *concurrency)
+  : concurrency_ (concurrency)
+{
+}
+
+JAWS_Reaper::~JAWS_Reaper (void)
+{
+}
+
+int
+JAWS_Reaper::open (void *)
+{
+  if (this->waiting_ == 0)
+    {
+      ACE_Guard<ACE_SYNCH_MUTEX> g (this->lock_);
+      if (this->waiting_ == 0)
+        {
+          if (this->activate () == -1)
+            ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "JAWS_Reaper::activate"),
+                              -1);
+          this->waiting_ = 1;
+        }
+    }
+  return 0;
+}
+
+int
+JAWS_Reaper::svc (void)
+{
+  return this->concurrency_->wait ();
+}
