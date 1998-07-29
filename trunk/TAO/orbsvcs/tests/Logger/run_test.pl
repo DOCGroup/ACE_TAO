a585f72e715b70ee078a9c3f406f18a67d152462
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-29 04:30:53 +0000

    Cleaned this script up by removing the code that wasn't needed.

--- trunk/TAO/orbsvcs/tests/Logger/run_test.pl
+++ trunk/TAO/orbsvcs/tests/Logger/run_test.pl
@@ -3,8 +3,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
     & eval 'exec perl -S $0 $argv:q'
     if 0;
 
-# This is a Perl script that runs the client and all the other servers that
-# are needed
+# This is a Perl script that runs the Logger client and server
 
 unshift @INC, '../../../../bin';
 require Process;
@@ -12,59 +11,20 @@ require Process;
 # amount of delay between running the servers
 $sleeptime = 3;
 
-# Get the userid (or ip on NT)
-if ($^O eq "MSWin32")
-{
-  system ("ipconfig | find \"Address\">ipnum");
-
-  open (IPNUM, "ipnum");
-
-  read (IPNUM, $line, 80);
-
-  ($junk, $ip1, $ip2, $ip3, $ip4) = split (/: (\d+)\.(\d+)\.(\d+)\.(\d+)/, $line);
-
-  close IPNUM;
-
-  system ("del /q ipnum");
-
-  $uid = $ip4;
-}
-else
-{
-  $uid = getpwnam (getlogin ());
-}
-
-# variables for parameters
-
-$svport = 0; 
-
-# other variables
-
-$n = 1;
-$leave = 1;
-$ior = 0;
-$done = "";
-$debug = "";
-$cm = "";
-$sm = "";
-$other = "";
-$c_conf = "client.conf";
-$s_conf = "server.conf";
-
 sub server
 {
-    my $args = "";
-    my $prog = "server".$Process::EXE_EXT;
+  my $args = "";
+  my $prog = "server".$Process::EXE_EXT;
     
-    $SV = Process::Create ($prog, $args);
+  $SV = Process::Create ($prog, $args);
 }
 
 sub client
 {
-    my $args = "";
-    my $prog = "client".$Process::EXE_EXT;
+  my $args = "";
+  my $prog = "client".$Process::EXE_EXT;
     
-    $CL = Process::Create ($prog, $args);
+  system ("$prog $args");
 }
 
 server ();
@@ -73,9 +33,6 @@ sleep $sleeptime;
 
 client ();
 
-sleep $sleeptime;
-if ($leave)
-{
-  $SV->Kill ();
-  $CL->Kill ();
-}
+sleep 10;
+
+$SV->Kill ();
