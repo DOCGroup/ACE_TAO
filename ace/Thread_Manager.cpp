2b52313107e151167832b663c02038094a671443
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-29 03:04:52 +0000

    moved ACE_TSS_Singleton<ACE_Dynamic, ACE_Null_Mutex> from Thread_Manager.cpp to Dynamic.cpp

--- ace/Thread_Manager.cpp
+++ ace/Thread_Manager.cpp
@@ -2115,8 +2115,6 @@ ACE_Thread_Control::exit (void *exit_status, int do_thr_exit)
   template class ACE_Node<ACE_Thread_Descriptor*>;
   template class ACE_Double_Linked_List<ACE_Thread_Descriptor>;
   template class ACE_Double_Linked_List_Iterator<ACE_Thread_Descriptor>;
-  // This doesn't necessarily belong here, but it's a convenient place for it.
-  template class ACE_TSS_Singleton<ACE_Dynamic, ACE_Null_Mutex>;
   template class ACE_Free_List<ACE_Thread_Descriptor>;
   template class ACE_Locked_Free_List<ACE_Thread_Descriptor, ACE_DEFAULT_THREAD_MANAGER_LOCK>;
 # if (defined (ACE_HAS_THREADS) && (defined (ACE_HAS_THREAD_SPECIFIC_STORAGE) || defined (ACE_HAS_TSS_EMULATION)))
@@ -2133,8 +2131,6 @@ ACE_Thread_Control::exit (void *exit_status, int do_thr_exit)
   #pragma instantiate ACE_Node<ACE_Thread_Descriptor*>
   #pragma instantiate ACE_Double_Linked_List<ACE_Thread_Descriptor>
   #pragma instantiate ACE_Double_Linked_List_Iterator<ACE_Thread_Descriptor>
-  // This doesn't necessarily belong here, but it's a convenient place for it.
-  #pragma instantiate ACE_TSS_Singleton<ACE_Dynamic, ACE_Null_Mutex>
   #pragma instantiate ACE_Free_List<ACE_Thread_Descriptor>
   #pragma instantiate ACE_Locked_Free_List<ACE_Thread_Descriptor, ACE_DEFAULT_THREAD_MANAGER_LOCK>
 # if (defined (ACE_HAS_THREADS) && (defined (ACE_HAS_THREAD_SPECIFIC_STORAGE) || defined (ACE_HAS_TSS_EMULATION)))
