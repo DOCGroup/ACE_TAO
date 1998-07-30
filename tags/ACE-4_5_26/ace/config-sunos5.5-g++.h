e3f089cfba0a89c12b508664c916f37cf6ba49ec
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-07-30 21:55:14 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_26'.

---
+++ tags/ACE-4_5_26/ace/config-sunos5.5-g++.h
@@ -0,0 +1,7 @@
+/* -*- C++ -*- */
+// $Id$
+//
+// This file has been replaced by config-sunos5.5.h.  It is maintained
+// for backward compatibility.
+
+#include "ace/config-sunos5.5.h"
---
+++ tags/ACE-4_5_26/examples/Reactor/Multicast/Log_Wrapper.cpp
@@ -0,0 +1,72 @@
+// $Id$
+
+// client.C
+
+#include "Log_Wrapper.h"
+
+Log_Wrapper::Log_Wrapper (void)
+{
+  sequence_number_ = 0;
+  this->log_msg_.app_id = ACE_OS::getpid ();
+}
+
+Log_Wrapper::~Log_Wrapper (void) 
+{
+}
+
+// Set the log_msg_ host address.
+
+int
+Log_Wrapper::open (const int port, const char *mcast_addr) 
+{
+  struct hostent *host_info;
+  struct utsname host_data;
+
+  if (ACE_OS::uname (&host_data) < 0) 
+    return -1;
+      
+  if ((host_info = ACE_OS::gethostbyname (host_data.nodename)) == NULL) 
+    return -1;
+  else 
+    ACE_OS::memcpy ((char *) &this->log_msg_.host, 
+                    (char *) host_info->h_addr, 
+                    host_info->h_length);
+
+  // This starts out initialized to all zeros!
+  server_ = ACE_INET_Addr(port, mcast_addr);
+
+  if (logger_.subscribe (server_) == -1)
+      perror("can't subscribe to multicast group"), exit(1);
+
+  // success.
+  return 0;
+}
+
+// Send the message to a logger object.
+// This wrapper fills in all the log_record info for you.
+// uses iovector stuff to make contiguous header and message.
+
+int
+Log_Wrapper::log_message (ACE_Log_Priority type, char *message)
+{ 
+  sequence_number_++;
+
+  this->log_msg_.type = type;
+  this->log_msg_.time = time (0);
+  this->log_msg_.msg_length = strlen(message)+1;
+  this->log_msg_.sequence_number = htonl(sequence_number_);
+
+  iovec   *iovp = new iovec[2];
+  iovp[0].iov_base = (char *) &log_msg_;
+  iovp[0].iov_len = sizeof log_msg_;
+  iovp[1].iov_base = message;
+  iovp[1].iov_len = log_msg_.msg_length;
+
+  logger_.send (iovp, 2);
+
+  delete iovp;
+
+  // success.
+  return 0;
+}
+
