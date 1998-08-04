45fb92cb721c18a74be31cf9d42b837d009ca852
mjb2 <mjb2@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-04 17:56:51 +0000

    See Tue Aug  4 12:55:06 1998  Matthew J Braun  <mjb2@cec.wustl.edu>

--- trunk/TAO/orbsvcs/tests/Logger/run_test.pl
+++ trunk/TAO/orbsvcs/tests/Logger/run_test.pl
@@ -3,6 +3,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
     & eval 'exec perl -S $0 $argv:q'
     if 0;
 
+$tao_root = $ENV{TAO_ROOT};
 # This is a Perl script that runs the Logger client and server
 
 unshift @INC, '../../../../bin';
@@ -11,28 +12,35 @@ require Process;
 # amount of delay between running the servers
 $sleeptime = 3;
 
-sub server
+# Starts the Logging Service
+sub service
 {
   my $args = "";
-  my $prog = "server".$Process::EXE_EXT;
-    
+  my $prog = "$tao_root/orbsvcs/Logging_Service/Logging_Service"
+      .$Process::EXE_EXT;    
   $SV = Process::Create ($prog, $args);
 }
 
-sub client
+# Starts the test client
+sub test
 {
   my $args = "";
-  my $prog = "client".$Process::EXE_EXT;
+  my $prog = "Logging_Test".$Process::EXE_EXT;
     
   system ("$prog $args");
 }
 
-server ();
+# Start the service
+service ();
 
+# Give the service time to settle
 sleep $sleeptime;
 
-client ();
+# Start the client (which exits automatically)
+test ();
 
-sleep 10;
+# Give the client time to log and exit
+sleep 3;
 
+# Kill the service
 $SV->Kill ();
