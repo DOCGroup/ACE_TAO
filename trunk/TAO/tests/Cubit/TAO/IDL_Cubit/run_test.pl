32ad118caef29af6dcc3e78c01f22420e8347aae
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-05 21:19:18 +0000

    New and improved script that supports more useful options.

--- trunk/TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
+++ trunk/TAO/tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -6,21 +6,83 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 
 unshift @INC, '../../../../../bin';
 require Process;
+require Uniqueid;
 
+$nsport = 20000 + uniqueid ();
 $server_port = 0;
 $iorfile = "theior";
 $sleeptime = 3;
+$exepref = "";
+$svnsflags = " -s -o $iorfile";
+$clnsflags = " -s -f $iorfile";
+$debug = "";
+$numflag = "";
 
-$SV = Process::Create ("server".$Process::EXE_EXT, 
+# Parse the arguments
+
+for ($i = 0; $i <= $#ARGV; $i++)
+{
+  SWITCH:
+  {
+    if ($ARGV[$i] eq "-h" || $ARGV[$i] eq "-?")
+    {
+      print "run_test [-h] [-n num] [-mcast] [-sleeptime t] [-debug] [-release]\n";
+      print "\n";
+      print "-h                  -- prints this information\n";
+      print "-n num              -- client uses <num> iterations\n";
+      print "-mcast              -- uses the multicast version of the nameservice\n";
+      print "-sleeptime t        -- run_test should sleep for <t> seconds between running\n";
+      print "                       the server and client\n";
+      print "-debug              -- sets the debug flag for both client and server\n";
+      print "-release            -- runs the Release version of the test (for NT use)\n";
+      print "                       (default is 3 seconds)\n";
+      exit;
+    }
+    if ($ARGV[$i] eq "-mcast")
+    {
+      $clnsflags = " -ORBnameserviceport $nsport";
+      $svnsflags = " -ORBnameserviceport $nsport";
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-debug")
+    {
+      $debug = " -d";
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-release")
+    {
+      $exepref = "Release".$DIR_SEPARATOR;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-n")
+    {
+      $numflag = " -n ".$ARGV[$i + 1];
+      $i++;
+      last SWITCH;
+    }
+    if ($ARGV[$i] eq "-sleeptime")
+    {
+      $sleeptime = $ARGV[$i + 1];
+      $i++;
+      last SWITCH;
+    }
+    print "run_test: Unknown Option: ".$ARGV[$i]."\n";
+  }
+}
+
+$SV = Process::Create ($exepref."server".$Process::EXE_EXT, 
+                       $debug.
                        " -ORBport ".$server_port.
-		       " -ORBobjrefstyle url".
-		       " -s -o $iorfile");
+                       " -ORBobjrefstyle url".
+                       $nsflags);
 
 sleep $sleeptime;
 
-$status = system ("client".$Process::EXE_EXT.
-		  " -s -f $iorfile -x");
-
+$status = system ($exepref."client".$Process::EXE_EXT.
+                  $debug.
+                  $numflag.
+                  $nsflags.
+                  " -x");
 
 # @@ TODO change to Wait() once the -x option works.
 $SV->Kill (); $SV->Wait ();
