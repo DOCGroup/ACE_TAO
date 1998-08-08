3438d4238a0acdf981218e93ad32dc81174cc027
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-08 04:20:07 +0000

    ChangeLogTag:Fri Aug  7 23:18:53 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/ace/config-sunos5.5.h
+++ trunk/ace/config-sunos5.5.h
@@ -253,9 +253,20 @@
 
 #define ACE_HAS_GPERF
 
+#if defined (__SUNPRO_CC)
 #define ACE_CC_NAME "SunPro C++"
 #define ACE_CC_MAJOR_VERSION (__SUNPRO_CC >> 8)
 #define ACE_CC_MINOR_VERSION (__SUNPRO_CC & 0x00ff)
 #define ACE_CC_BETA_VERSION  (0)
+#elif defined (__GNUG__)
+#define ACE_CC_MAJOR_VERSION __GNUC__
+#define ACE_CC_MINOR_VERSION __GNUC_MINOR__
+#define ACE_CC_BETA_VERSION  __GNUC_(0)
+#if __GNUC_MINOR__ >= 90
+#define ACE_CC_NAME "egcs"
+#else
+#define ACE_CC_NAME "g++"
+#endif /* __GNUC_MINOR__ */
+#endif /* __GNUG__ */
 
 #endif /* ACE_CONFIG_H */
