3067a6f45a9a3d6326a68d62e0c580328d8648c6
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-12 22:20:14 +0000

    Added updated versions of project files (with correct include directories). Also changed the <main>'s to take in argc and argv.  Thanks to Jeff Hellzen <JHellzen@hwdcsaws.cahwnet.gov> for submitting these changes.

--- examples/Reactor/ReactorEx/test_apc.cpp
+++ examples/Reactor/ReactorEx/test_apc.cpp
@@ -83,7 +83,7 @@ queue_apc (void)
 }
 
 int 
-main (void)
+main (int argc, char** argv)
 {
   event_handler.iterations_ = 5;
   ACE_ASSERT (ACE_Reactor::instance ()->register_handler (&event_handler, 
