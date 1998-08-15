b61dd3e0b72004ea9a649529b6284b4d02a88102
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-15 01:26:57 +0000

    ChangeLogTag:Fri Aug 14 20:13:28 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/orbsvcs/tests/EC_Custom_Marshal/ECM_Data.cpp
+++ TAO/orbsvcs/tests/EC_Custom_Marshal/ECM_Data.cpp
@@ -4,49 +4,51 @@
 
 ACE_RCSID(EC_Custom_Marshal, ECM_Data, "$Id$")
 
-TAO_OutputCDR&
+CORBA::Boolean
 operator<< (TAO_OutputCDR& cdr, ECM_Data& x)
 {
   // We are a little careless about error checking in this routine,
   // because one the CDR gets the error bit on it is never disabled.
   CORBA::ULong count = x.inventory.current_size ();
-  cdr << x.description.in ()
-      << count;
-  for (ECM_Data::Inventory::ITERATOR i = x.inventory.begin ();
-       i != x.inventory.end ();
-       ++i)
+  if (cdr << x.description.in ()
+      && cdr << count )
     {
-      const ECM_Data::Inventory::ENTRY& v = *i;
-      cdr << v.ext_id_
-	  << v.int_id_;
+      for (ECM_Data::Inventory::ITERATOR i = x.inventory.begin ();
+	   i != x.inventory.end () && cdr.good_bit ();
+	   ++i)
+	{
+	  const ECM_Data::Inventory::ENTRY& v = *i;
+	  cdr << v.ext_id_;
+	  cdr << v.int_id_;
+	}
     }
-  return cdr;
+  return cdr.good_bit ();
 }
 
-TAO_InputCDR&
+CORBA::Boolean
 operator>> (TAO_InputCDR& cdr, ECM_Data& x)
 {
   // We are a little careless about error checking in this routine,
   // because one the CDR gets the error bit on it is never disabled.
   CORBA::ULong count;
-  cdr >> x.description.out ()
-      >> count;
-
-  // ACE_DEBUG ((LM_DEBUG, "Decoding <%d> elements\n", count));
-
-  for (CORBA::ULong i = 0; i < count && cdr.good_bit (); ++i)
+  if (cdr >> x.description.out ()
+      && cdr >> count)
     {
-      CORBA::ULong ext_id;
-      CORBA::Double int_id;
-      cdr >> ext_id
-	  >> int_id;
-      if (cdr.good_bit ())
-	x.inventory.bind (ext_id, int_id);
+      // ACE_DEBUG ((LM_DEBUG, "Decoding <%d> elements\n", count));
 
-      // ACE_DEBUG ((LM_DEBUG, "Boung <%d,%f>\n", ext_id, int_id));
+      for (CORBA::ULong i = 0; i < count && cdr.good_bit (); ++i)
+	{
+	  CORBA::ULong ext_id;
+	  CORBA::Double int_id;
+	  cdr >> ext_id;
+	  cdr >> int_id;
+	  if (cdr.good_bit ())
+	    x.inventory.bind (ext_id, int_id);
 
+	  // ACE_DEBUG ((LM_DEBUG, "Boung <%d,%f>\n", ext_id, int_id));
+	}
     }
-  return cdr;
+  return cdr.good_bit ();
 }
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
