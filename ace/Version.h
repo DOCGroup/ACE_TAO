f95e4809ffadf0a71b19c8e99eea1c83d497553f
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-18 21:45:24 +0000

    4.5.29

--- ace/Version.h
+++ ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (5u)
-#define ACE_BETA_VERSION (28u)
-#define ACE_VERSION "4.5.28\0"
+#define ACE_BETA_VERSION (29u)
+#define ACE_VERSION "4.5.29\0"
