f941952ece662f2bc1b95f3bd466f9681e27aad9
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-19 15:46:59 +0000

    (find_another_host): check to see if the other host is up, by attempting a blocking connection with it

--- tests/SOCK_Connector_Test.cpp
+++ tests/SOCK_Connector_Test.cpp
@@ -39,42 +39,74 @@ USELIB("..\ace\aced.lib");
 static void
 find_another_host (ASYS_TCHAR other_host[])
 {
-  ACE_OS::strcpy (other_host, ACE_DEFAULT_SERVER_HOST); // If all else fails
+  static ASYS_TCHAR cached_other_host[MAXHOSTNAMELEN] = {'\0'};
+
+  if (cached_other_host[0] == '\0')
+    {
+
+      ACE_OS::strcpy (other_host,
+                      ACE_DEFAULT_SERVER_HOST); // If all else fails
 
 #if !defined (ACE_LACKS_GETHOSTENT)
-  // These gethost-type things don't work everywhere.
-  struct hostent *h;
-  struct utsname un;
+      // These gethost-type things don't work everywhere.
+      struct hostent *h;
+      struct utsname un;
 
-  ACE_OS::uname (&un);
+      ACE_OS::uname (&un);
 
-  h = ACE_OS::gethostbyname (un.nodename);
+      h = ACE_OS::gethostbyname (un.nodename);
 
-  // Use me if can't find another
-  ACE_OS::strcpy (other_host, ASYS_WIDE_STRING (h->h_name));
+      // Use me if can't find another
+      ACE_OS::strcpy (other_host, ASYS_WIDE_STRING (h->h_name));
 
-  // @@ We really need to add wrappers for these hostent methods.
-  sethostent (1);
+      // @@ We really need to add wrappers for these hostent methods.
+      sethostent (1);
 
-  while ((h = gethostent ()) != NULL)
-    {
-      if (ACE_OS::strcmp (h->h_name, ACE_DEFAULT_SERVER_HOST) == 0)
-        continue;
-      // AIX just _has_ to be different
-      if (ACE_OS::strcmp (h->h_name, "loopback") == 0)
-        continue;
-
-      // If not me
-      if (ACE_OS::strcmp (h->h_name, other_host) != 0 &&
-          ACE_OS::strcmp (h->h_name, un.nodename) != 0   )
+      while ((h = gethostent ()) != NULL)
         {
-          ACE_OS::strcpy (other_host, h->h_name);
-          break;
-        }
-    }
+          if (ACE_OS::strcmp (h->h_name, ACE_DEFAULT_SERVER_HOST) == 0)
+            continue;
+          // AIX just _has_ to be different
+          if (ACE_OS::strcmp (h->h_name, "loopback") == 0)
+            continue;
 
-  endhostent ();
+          // If not me
+          if (ACE_OS::strcmp (h->h_name, other_host) != 0 &&
+              ACE_OS::strcmp (h->h_name, un.nodename) != 0 )
+            {
+              // Make sure the other host exists and is up.  Do that
+              // by attempting a blocking connection.
+              ACE_SOCK_Connector con;
+              ACE_SOCK_Stream sock;
+
+              // The ACE_INET_Addr construction causes gethostbyname_r
+              // to be called, so we need to copy the hostname.
+              ASYS_TCHAR test_host[MAXHOSTNAMELEN];
+              ACE_OS::strcpy (test_host, h->h_name);
+
+              ACE_INET_Addr another_host ((u_short) 7, test_host);
+              const int status = con.connect (sock, another_host, 0);
+
+              if (status == 0  ||  errno != ETIMEDOUT)
+                {
+                  ACE_OS::strcpy (other_host, h->h_name);
+                  sock.close ();
+                  break;
+                }
+              else
+                sock.close ();
+             }
+        }
+
+      endhostent ();
 #endif /* ! ACE_LACKS_GETHOSTENT */
+
+      ACE_OS::strcpy (cached_other_host, other_host);
+    }
+  else
+    {
+      ACE_OS::strcpy (other_host, cached_other_host);
+    }
 }
 
 static int
