be2fd7f5fca42bf2328ebb89d5f17f9110dd45a5
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-19 04:34:08 +0000

    *** empty log message ***

---
+++ trunk/TAO/tests/POA/Explicit_Activation/Alternate_Resource_Factory/Resource_Factory.cpp
@@ -0,0 +1,24 @@
+// $Id$
+
+#include "Resource_Factory.h"
+
+My_Resource_Factory::My_Reactors::My_Reactors (void)
+  : wfmo_reactor_ (),
+    reactor_ (&wfmo_reactor_)
+{
+}
+
+ACE_Reactor *
+My_Resource_Factory::get_reactor (void)
+{
+  switch (this->resource_source_)
+    {
+    case TAO_GLOBAL:
+      return &GLOBAL_REACTORS::instance ()->reactor_;
+    case TAO_TSS:
+      return &TSS_REACTORS::instance ()->reactor_;
+    }
+  return 0;
+}
+
+ACE_FACTORY_DEFINE (Alternate_Resource_Factory, My_Resource_Factory)
