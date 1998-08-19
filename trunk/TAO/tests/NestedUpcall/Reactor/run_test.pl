dbf816a1f0ec58dff38f323226156534e6281688
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-19 17:48:00 +0000

    ChangeLogTag:Wed Aug 19 10:48:37 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/TAO/tests/NestedUpcall/Reactor/run_test.pl
+++ trunk/TAO/tests/NestedUpcall/Reactor/run_test.pl
@@ -1,4 +1,6 @@
-#$Id$
+#
+# $Id$
+#
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
     & eval 'exec perl -S $0 $argv:q'
     if 0;
@@ -7,22 +9,16 @@ unshift @INC, '../../../../bin';
 require Process;
 require Uniqueid;
 
-$nsport = 20000 + uniqueid ();
-$client_port = 0;
-$server_port = 0;
 $iorfile = "theior";
-$sleeptime = 3;
+$sleeptime = 10;
 
 $SV = Process::Create ("server".$Process::EXE_EXT, 
-                       " -ORBport ".$server_port.
-		       " -ORBnameserviceport ".$nsport.
 		       " -ORBobjrefstyle url");
 
 sleep ($sleeptime);
 
 $status  = system ("client".$Process::EXE_EXT.
-		   " -x -ORBnameserviceport ".$nsport.
-		   " -ORBport ".$client_port );
+		   " -x ");
 
 $SV->Wait ();
 
