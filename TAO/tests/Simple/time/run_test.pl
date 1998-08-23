a61034ba0a4a7c4fbe8786254422dc497287f4b9
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-23 01:47:55 +0000

    We don't need ORBport 0 anymore

--- TAO/tests/Simple/time/run_test.pl
+++ TAO/tests/Simple/time/run_test.pl
@@ -7,17 +7,14 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 unshift @INC, '../../../../bin';
 require Process;
 
-$server_port = 0;
 $iorfile = "time.ior";
 $sleeptime = 5;
 
-$SV = Process::Create ("server".$Process::EXE_EXT, 
-                       " -ORBport $server_port -o $iorfile ");
+$SV = Process::Create ("server$Process::EXE_EXT", "-o $iorfile ");
 
 sleep $sleeptime;
 
-$status  = system ("client".$Process::EXE_EXT.
-		   " -f $iorfile -x");
+$status  = system ("client$Process::EXE_EXT -f $iorfile -x");
 
 $SV->Kill (); $SV->Wait ();
 
