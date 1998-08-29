a96860c13772a38aac95a644b890febf70d02056
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-08-29 04:31:57 +0000

    *** empty log message ***

--- examples/IPC_SAP/SOCK_SAP/CPP-inclient.cpp
+++ examples/IPC_SAP/SOCK_SAP/CPP-inclient.cpp
@@ -8,6 +8,7 @@
 #include "ace/Thread_Manager.h"
 #include "ace/Singleton.h"
 #include "ace/Get_Opt.h"
+#include "ace/High_Res_Timer.h"
 #include "ace/Synch.h"
 
 ACE_RCSID(SOCK_SAP, CPP_inclient, "$Id$")
@@ -22,11 +23,15 @@ public:
   Options (void);
   // Constructor.
 
+  ~Options (void);
+  // Destructor.
+
   int parse_args (int argc, char *argv[]);
   // Parse the command-line arguments.
 
-  ACE_Time_Value *timeout (void) const;
-  // Timeout value.
+  const ACE_Time_Value &sleep_time (void) const;
+  // Return the amount of time to sleep in order to implement the
+  // proper transmission rates.
 
   u_short port (void) const;
   // Port of the server.
@@ -54,8 +59,14 @@ public:
   // the client test function.
 
 private:
-  int initialize_message (void);
-  // Initialize the message we're sending to the user.
+  int init (void);
+  // Initialize the message we're sending to the user and set up the
+  // barrier.
+
+  void *shared_client_test (u_short port,
+                            ACE_SOCK_Stream &cli_stream);
+  // Performs the shared behavior of the oneway and twoway client
+  // tests.
 
   static void *twoway_client_test (void *);
   // Performs the twoway test.
@@ -69,8 +80,8 @@ private:
   u_short port_;
   // Port of the server.
 
-  ACE_Time_Value timeout_;
-  // Timeout value.
+  ACE_Time_Value sleep_time_;
+  // Sleep_Time value.
 
   size_t threads_;
   // Number of threads.
@@ -81,7 +92,7 @@ private:
   size_t message_len_;
   // Size of the message we send to the server.
 
-  void *message_buf_;
+  char *message_buf_;
   // Pointer to the message we send to the server.
 
   ACE_HANDLE io_source_;
@@ -92,47 +103,62 @@ private:
 
   char oneway_;
   // Are we running oneway or twoway?
+
+  ACE_MT (ACE_Barrier *barrier_);
+  // Barrier used to synchronize the start of all the threads.
 };
 
 Options::Options (void)
   : host_ (ACE_DEFAULT_SERVER_HOST),
     port_ (ACE_DEFAULT_SERVER_PORT),
-    timeout_ (ACE_DEFAULT_TIMEOUT),
+    sleep_time_ (0, 100000), // Sleep for 100 msecs between calls.
     threads_ (10),
     quit_string_ ("q"),
     message_len_ (0),
     message_buf_ (0),
     io_source_ (ACE_INVALID_HANDLE), // Defaults to using the generator.
     iterations_ (10000),
-    oneway_ (1) // Make oneway calls the default.
+    oneway_ (1), // Make oneway calls the default.
+    barrier_ (0)
 {
 }
 
+Options::~Options (void)
+{
+  delete this->barrier_;
+  delete [] this->message_buf_;
+}
+  
 // Options Singleton.
 typedef ACE_Singleton<Options, ACE_SYNCH_RECURSIVE_MUTEX> OPTIONS;
 
 int
-Options::initialize_message (void)
+Options::init (void)
 {
   // Check for default case.
   if (this->message_len_ == 0)
-    {
-      ACE_ALLOCATOR_RETURN (this->message_buf_,
-                            ACE_OS::strdup ("TAO"),
-                            -1);
-      this->message_len_ = ACE_OS::strlen ("TAO");
-    }
-  else
-    {
-      ACE_ALLOCATOR_RETURN (this->message_buf_,
-                            ACE_OS::malloc (this->message_len_),
-                            -1);
+    this->message_len_ = ACE_OS::strlen ("TAO");
 
-      ACE_OS::memset (this->message_buf_,
-                      'a',
-                      this->message_len_);
-    }
+  this->message_len_ += sizeof (ACE_UINT32);
 
+  ACE_NEW_RETURN (this->message_buf_,
+                  char[this->message_len_],
+                  -1);
+
+  // Copy the length into the beginning of the message.
+  ACE_UINT32 length = ntohl (this->message_len_);
+  ACE_OS::memcpy ((void *) this->message_buf_,
+                  (void *) &length,
+                  sizeof length);
+
+  ACE_OS::memset ((void *) (this->message_buf_ + sizeof (ACE_UINT32)),
+                  'a',
+                  this->message_len_ - sizeof (ACE_UINT32));
+
+  // Allocate the barrier with the correct count.
+  ACE_NEW_RETURN (this->barrier_,
+                  ACE_Barrier (this->threads_),
+                  -1);
   return 0;
 }
 
@@ -201,15 +227,15 @@ Options::parse_args (int argc, char *argv[])
         this->threads_ = (size_t) ACE_OS::atoi (getopt.optarg);
         break;
       case 'T':
-        this->timeout_ = ACE_OS::atoi (getopt.optarg);
+        this->sleep_time_.set (0, ACE_OS::atoi (getopt.optarg));
         break;
       default:
         ACE_ERROR_RETURN ((LM_ERROR,
-                           "(%P|%t) usage: %n [-2] [-h <host>] [-i iterations] [-m message-size] [-p <port>] [-q <quit string>] [-s] [-t <threads>] [-T <timeout>]"),
+                           "(%P|%t) usage: %n [-2] [-h <host>] [-i iterations] [-m message-size] [-p <port>] [-q <quit string>] [-s] [-t <threads>] [-T <sleep_time>]"),
                           -1);
       }
 
-  return this->initialize_message ();
+  return this->init ();
 }
 
 u_short
@@ -236,29 +262,20 @@ Options::threads (void) const
   return this->threads_;
 }
 
-ACE_Time_Value *
-Options::timeout (void) const
+const ACE_Time_Value &
+Options::sleep_time (void) const
 {
-  return (ACE_Time_Value *) &this->timeout_;
+  return this->sleep_time_;
 }
 
-// Static function entry point to the oneway client service.
-
 void *
-Options::oneway_client_test (void *)
+Options::shared_client_test (u_short port,
+                             ACE_SOCK_Stream &cli_stream)
 {
-  Options *options = OPTIONS::instance ();
-
-  ACE_SOCK_Stream cli_stream;
   // Add one to the port for the oneway test!
-  ACE_INET_Addr remote_addr (options->port () + 1,
-                             options->host ());
+  ACE_INET_Addr remote_addr (port, this->host_);
 
   ACE_SOCK_Connector con;
-
-  // Initiate blocking connection with server.
-  ACE_DEBUG ((LM_DEBUG,
-              "(%P|%t) starting oneway connect\n"));
 
   if (con.connect (cli_stream, remote_addr) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
@@ -271,39 +288,53 @@ Options::oneway_client_test (void *)
                 remote_addr.get_host_name (),
                 remote_addr.get_port_number ()));
 
-  ACE_INT32 len = htonl (options->message_len ());
-
-  if (cli_stream.send_n ((void *) &len,
-                         sizeof (ACE_INT32)) != sizeof (ACE_INT32))
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "(%P|%t) %p\n",
-                       "send_n failed"),
-                      0);
+  ACE_INT32 len = htonl (this->message_len ());
 
   // Allocate the transmit buffer.
   void *buf;
-  ACE_ALLOCATOR_RETURN (buf,
-                        ACE_OS::malloc (len),
-                        0);
+  ACE_NEW_RETURN (buf,
+                  char[len],
+                  0);
 
+  ACE_DEBUG ((LM_DEBUG,
+              "(%P|%t) waiting...\n"));
+              
+  // Wait for all other threads to get initialized.
+  this->barrier_->wait ();
+  return buf;
+}
+// Static function entry point to the oneway client service.
+
+void *
+Options::oneway_client_test (void *)
+{
+  Options *options = OPTIONS::instance ();
+  ACE_SOCK_Stream cli_stream;
+  void *buf = options->shared_client_test (options->port () + 1,
+                                           cli_stream);
   // This variable is allocated off the stack to obviate the need for
   // locking.
   size_t iteration = 0;
 
   // Keep track of return value.
   int result = 0;
+  ACE_INT32 len = options->message_len ();
+
+  ACE_DEBUG ((LM_DEBUG,
+              "(%P|%t) starting oneway transmission\n"));
 
   // Perform oneway transmission of data to server (correctly handles
   // "incomplete writes").
 
   for (ssize_t r_bytes;
        (r_bytes = options->read (buf, sizeof buf, iteration)) > 0;
-       )
+       // Transmit at the proper rate.
+       ACE_OS::sleep (options->sleep_time ()))
     if (ACE_OS::memcmp (buf,
                         options->quit_string (),
                         ACE_OS::strlen (options->quit_string ())) == 0)
       break;
-    else if (cli_stream.send (buf, r_bytes) == -1)
+    else if (cli_stream.send_n (buf, r_bytes) == -1)
       {
         ACE_ERROR ((LM_ERROR,
                     "(%P|%t) %p\n",
@@ -315,7 +346,7 @@ Options::oneway_client_test (void *)
   // Close the connection.
   cli_stream.close ();
 
-  ACE_OS::free (buf);
+  delete [] buf;
   return (void *) result;
 }
 
@@ -326,41 +357,12 @@ Options::twoway_client_test (void *)
 {
   Options *options = OPTIONS::instance ();
 
+  // Timer business.
+  ACE_High_Res_Timer timer;
+
   ACE_SOCK_Stream cli_stream;
-  ACE_INET_Addr remote_addr (options->port (),
-                             options->host ());
-
-  ACE_SOCK_Connector con;
-
-  // Initiate blocking connection with server.
-  ACE_DEBUG ((LM_DEBUG,
-              "(%P|%t) starting twoway connect\n"));
-
-  if (con.connect (cli_stream, remote_addr) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "(%P|%t) %p\n",
-                       "connection failed"),
-                      0);
-  else
-    ACE_DEBUG ((LM_DEBUG,
-                "(%P|%t) connected to %s at port %d\n",
-                remote_addr.get_host_name (),
-                remote_addr.get_port_number ()));
-
-  ACE_INT32 len = htonl (options->message_len ());
-
-  if (cli_stream.send_n ((void *) &len,
-                         sizeof (ACE_INT32)) != sizeof (ACE_INT32))
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "(%P|%t) %p\n",
-                       "send_n failed"),
-                      0);
-
-  // Allocate the transmit buffer.
-  void *buf;
-  ACE_ALLOCATOR_RETURN (buf,
-                        ACE_OS::malloc (len),
-                        0);
+  void *request = options->shared_client_test (options->port (),
+                                               cli_stream);
 
   // This variable is allocated off the stack to obviate the need for
   // locking.
@@ -368,38 +370,65 @@ Options::twoway_client_test (void *)
 
   // Keep track of return value.
   int result = 0;
+  ACE_INT32 len = options->message_len ();
+
+  ACE_DEBUG ((LM_DEBUG,
+              "(%P|%t) starting twoway transmission\n"));
 
   // Perform twoway transmission of data to server (correctly handles
   // "incomplete writes").
 
   for (ssize_t r_bytes;
-       (r_bytes = options->read (buf, len, iteration)) > 0;
-       )
-    if (ACE_OS::memcmp (buf,
+       (r_bytes = options->read (request, len, iteration)) > 0;
+       // Transmit at the proper rate.
+       ACE_OS::sleep (options->sleep_time ()))
+    if (ACE_OS::memcmp (request,
                         options->quit_string (),
                         ACE_OS::strlen (options->quit_string ())) == 0)
       break;
-    else if (cli_stream.send (buf, r_bytes) == -1)
+
+    // Transmit <request> to the server.
+    else
       {
-        ACE_ERROR ((LM_ERROR,
-                    "(%P|%t) %p\n",
-                    "send_n"));
-        result = -1;
-        break;
+        timer.start_incr ();
+
+        if (cli_stream.send (request, r_bytes) == -1)
+          {
+            ACE_ERROR ((LM_ERROR,
+                        "(%P|%t) %p\n",
+                        "send_n"));
+            result = -1;
+            break;
+          }
+        // Receive the reply from the server.  It just echos back the
+        // reply.
+        else if (cli_stream.recv (request, r_bytes) <= 0)
+          {
+            ACE_ERROR ((LM_ERROR,
+                        "(%P|%t) %p\n",
+                        "recv"));
+            result = -1;
+            break;
+          }
+        timer.stop_incr ();
       }
-    else if (cli_stream.recv (buf, r_bytes) <= 0)
-      {
-        ACE_ERROR ((LM_ERROR,
-                    "(%P|%t) %p\n",
-                    "recv"));
-        result = -1;
-        break;
-      }
+
+  ACE_Time_Value tv;
+  timer.elapsed_time_incr (tv);
+  double real_time = tv.sec () * ACE_ONE_SECOND_IN_USECS + tv.usec ();
+
+  double messages_per_sec = iteration * 1000000.0 / real_time;
+
+  ACE_DEBUG ((LM_DEBUG,
+	      ASYS_TEXT ("(%t) messages = %d\n(%t) usec-per-message = %f\n(%t) messages-per-second = %0.00f\n"),
+	      iteration,
+	      real_time / double (iteration),
+              messages_per_sec < 0 ? 0 : messages_per_sec));
 
   // Close the connection.
   cli_stream.close ();
 
-  ACE_OS::free (buf);
+  delete [] request;
   return (void *) result;
 }
 
@@ -412,6 +441,27 @@ Options::thr_func (void)
     return ACE_THR_FUNC (&Options::oneway_client_test);
 }
 
+static int
+run_client (void)
+{
+  // Raise the socket handle limit to the maximum.
+  ACE::set_handle_limit ();
+
+#if defined (ACE_HAS_THREADS)
+  if (ACE_Thread_Manager::instance ()->spawn_n (OPTIONS::instance ()->threads (),
+                                                OPTIONS::instance ()->thr_func ()) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "(%P|%t) %p\n",
+                       "spawn_n"),
+                      1);
+  else
+    ACE_Thread_Manager::instance ()->wait ();
+#else
+  client (0);
+#endif /* ACE_HAS_THREADS */
+  return 0;
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -421,15 +471,8 @@ main (int argc, char *argv[])
   if (OPTIONS::instance ()->parse_args (argc, argv) == -1)
     return -1;
 
-#if defined (ACE_HAS_THREADS)
-  if (ACE_Thread_Manager::instance ()->spawn_n (OPTIONS::instance ()->threads (),
-                                                OPTIONS::instance ()->thr_func ()) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) %p\n", "spawn_n"), 1);
-  else
-    ACE_Thread_Manager::instance ()->wait ();
-#else
-  client (0);
-#endif /* ACE_HAS_THREADS */
+  // Run the client
+  run_client ();
 
   return 0;
 }
