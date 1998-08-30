baee54d8c5d68465e395511b1dbb3427883209e5
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-08-30 23:47:15 +0000

    *** empty log message ***

--- trunk/docs/tutorials/002/server.cpp
+++ trunk/docs/tutorials/002/server.cpp
@@ -1,4 +1,6 @@
+
 // $Id$
+
 
 #include "ace/Acceptor.h"
 #include "ace/SOCK_Acceptor.h"
@@ -17,71 +19,71 @@ class Logging_Handler : public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH>
 public:
 
   Logging_Handler (void)
-  {
-  }
+	{
+	}
 
   virtual void destroy (void)
-  {
-    g_reactor->cancel_timer (this);
-    this->peer ().close ();
-    delete this;
-  }
+	{
+	  g_reactor->cancel_timer (this);
+	  this->peer ().close ();
+	  delete this;
+	}
 
   virtual int open (void *)
-  {
-    ACE_INET_Addr addr;
+	{
+	  ACE_INET_Addr addr;
 	  
-    if (this->peer ().get_remote_addr (addr) == -1)
-      return -1;
-    else
-      {
-        ACE_OS::strncpy (this->peer_name_, addr.get_host_name (), MAXHOSTNAMELEN + 1);
+	  if (this->peer ().get_remote_addr (addr) == -1)
+	    return -1;
+	  else
+	    {
+	      ACE_OS::strncpy (this->peer_name_, addr.get_host_name (), MAXHOSTNAMELEN + 1);
 	
-        if (g_reactor->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
-          ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) can't register with reactor\n"), -1);
+	      if (g_reactor->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
+		ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) can't register with reactor\n"), -1);
 
-        else if (g_reactor->schedule_timer (this, (const void *) this, ACE_Time_Value (2), ACE_Time_Value (2)) == -1)
-          ACE_ERROR_RETURN ((LM_ERROR, "can'(%P|%t) t register with reactor\n"), -1);
+	      else if (g_reactor->schedule_timer (this, (const void *) this, ACE_Time_Value (2), ACE_Time_Value (2)) == -1)
+		ACE_ERROR_RETURN ((LM_ERROR, "can'(%P|%t) t register with reactor\n"), -1);
 
-        else
-          ACE_DEBUG ((LM_DEBUG, "(%P|%t) connected with %s\n", this->peer_name_));
+	      else
+	      	ACE_DEBUG ((LM_DEBUG, "(%P|%t) connected with %s\n", this->peer_name_));
 
-        return 0;
-      }
-  }
+	      return 0;
+	    }
+	}
 
   virtual int close (u_long)
-  {
-    this->destroy ();
-    return 0;
-  }
+	{
+	  this->destroy ();
+	  return 0;
+	}
 
 protected:
 
   virtual int handle_input (ACE_HANDLE)
-  {
-    char buf[128];
-    memset(buf,0,sizeof(buf));
+        {
+          char buf[128];
+          memset(buf,0,sizeof(buf));
         
-    switch( this->peer().recv(buf,sizeof buf) )
-      {
-      case -1:
-        ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) %p bad read\n", "client logger"), -1);
-      case 0:
-        ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) closing log daemon (fd = %d)\n", this->get_handle ()), -1);
-      default:
-        ACE_DEBUG ((LM_DEBUG, "(%P|%t) from client: %s",buf));
-      }
+          switch( this->peer().recv(buf,sizeof buf) )
+          {
+          case -1:
+            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) %p bad read\n", "client logger"), -1);
+          case 0:
+            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) closing log daemon (fd = %d)\n", this->get_handle ()), -1);
+          default:
+            ACE_DEBUG ((LM_DEBUG, "(%P|%t) from client: %s",buf));
+          }
           
-    return 0;
-  }
+          return 0;
+        }
 
   virtual int handle_timeout (const ACE_Time_Value &tv, const void *arg)
-  {
-    ACE_ASSERT (arg == this);
-    ACE_DEBUG ((LM_DEBUG, "(%P|%t) handling timeout from this = %u\n", this));
-    return 0;
-  }
+	{
+	  ACE_ASSERT (arg == this);
+	  ACE_DEBUG ((LM_DEBUG, "(%P|%t) handling timeout from this = %u\n", this));
+	  return 0;
+	}
 
 private:
 
