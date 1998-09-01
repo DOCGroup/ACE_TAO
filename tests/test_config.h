b1bbb27b897fea2801a60521e8ed237a3b0356de
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-01 03:04:00 +0000

    *** empty log message ***

--- tests/test_config.h
+++ tests/test_config.h
@@ -130,11 +130,16 @@ const size_t ACE_MAX_CLIENTS = 30;
 const size_t ACE_NS_MAX_ENTRIES = 1000;
 const size_t ACE_DEFAULT_USECS = 1000;
 const size_t ACE_MAX_TIMERS = 4;
-const size_t ACE_MAX_THREADS = 4;
 const size_t ACE_MAX_DELAY = 10;
 const size_t ACE_MAX_INTERVAL = 0;
 const size_t ACE_MAX_ITERATIONS = 10;
-const size_t ACE_MAX_PROCESSES = 10;
+#if defined (__Lynx__)
+  const size_t ACE_MAX_PROCESSES = 4;
+  const size_t ACE_MAX_THREADS = 2;
+#else  /* ! __Lynx__ */
+  const size_t ACE_MAX_PROCESSES = 10;
+  const size_t ACE_MAX_THREADS = 4;
+#endif /* ! __Lynx__ */
 
 char ACE_ALPHABET[] = "abcdefghijklmnopqrstuvwxyz";
 
