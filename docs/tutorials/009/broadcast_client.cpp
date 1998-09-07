54d87cc753be12b0a4284273834df7caf2993a40
jcej <jcej@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-07 17:40:06 +0000

    *** empty log message ***

--- docs/tutorials/009/broadcast_client.cpp
+++ docs/tutorials/009/broadcast_client.cpp
@@ -6,39 +6,34 @@
 
 static const u_short PORT = ACE_DEFAULT_SERVER_PORT;
 
-int main(int argc,char *argv[] )
+int main (int argc, char *argv[])
 {
-	ACE_INET_Addr  local((u_short)0);
-	ACE_INET_Addr  remote(PORT,INADDR_BROADCAST);
-	ACE_SOCK_Dgram_Bcast dgram;
+  ACE_INET_Addr local ((u_short) 0);
+  ACE_INET_Addr remote (PORT, INADDR_BROADCAST);
+  ACE_SOCK_Dgram_Bcast dgram;
 
-	if( dgram.open(local) == -1 )
-	{
-		cerr << "Cannot create client datagram" << endl;
-		return(0);
-	}
+  if (dgram.open (local) == -1)
+  {
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "open"), -1);
+  }
 
-	char buf[512];
+  char buf[512];
 
-	sprintf(buf, argc > 1 ? argv[1] : "Hello World!");
+  sprintf (buf, argc > 1 ? argv[1] : "Hello World!");
 
-	if( dgram.send(buf,strlen(buf)+1,remote) == -1 )
-	{
-		cerr << "Failed to send datagram" << endl;
-		return(0);
-	}
+  if (dgram.send (buf, strlen (buf) + 1, remote) == -1)
+  {
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "send"), -1);
+  }
 
-	if( argc > 1 )
-	{
-		ACE_Time_Value timeout(2,0);
-		if( dgram.recv(buf,sizeof(buf),remote,0,&timeout) == -1 )
-		{
-			cerr << "Failed to receive server's response" << endl;
-			return(0);
-		}
+  ACE_Time_Value timeout (2, 0);
+  if (dgram.recv (buf, sizeof (buf), remote, 0, &timeout) == -1)
+  {
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "recv"), -1);
+  }
 
-		cerr << "Server responded with:  " << buf << endl;
-	}
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) The server at (%s) said (%s)\n",
+	      remote.get_host_name (), buf));
 
-	return(0);
+  return (0);
 }
