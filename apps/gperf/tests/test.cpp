f59f43a423373ab82501daef00a4b7ece3bedce9
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-09-08 18:19:58 +0000

    *** empty log message ***

--- apps/gperf/tests/test.cpp
+++ apps/gperf/tests/test.cpp
@@ -1,11 +1,11 @@
 // $Id: test.cpp 1.1	10/18/96
 
 // Tests the generated perfect has function.
-// The -v option prints diagnostics as to whether a word is in 
-// the set or not.  Without -v the program is useful for timing.
+
+// The -v option prints diagnostics as to whether a word is in the set
+// or not.  Without -v the program is useful for timing.
   
-#include <stdio.h>
-#include <string.h>
+#include "ace/OS.h"
 
 ACE_RCSID(tests, test, "$Id$")
 
@@ -16,17 +16,18 @@ char *in_word_set (const char *str, int len);
 int 
 main (int argc, char *argv[])
 {
-  int verbose = argc > 1 && strcmp (argv[1], "-v") == 0 ? 1 : 0;
+  int verbose = argc > 1 && ACE_OS::strcmp (argv[1], "-v") == 0 ? 1 : 0;
   char buf[MAX_LEN];
 
-  while (fgets (buf, sizeof buf, stdin) != 0) 
+  while (ACE_OS::fgets (buf, sizeof buf, stdin) != 0) 
     {
-      int len = strlen (buf) - 1;
+      int len = ACE_OS::strlen (buf) - 1;
       buf[len] = '\0';
       if (in_word_set (buf, len) && verbose)
         printf ("in word set %s\n", buf);
       else if (verbose) 
         printf ("NOT in word set %s\n", buf);
     }
+
   return 0;
 }
