598854a99d2ddba9e90ed11ff5723cb6c70b72db
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-09 02:56:55 +0000

    Updated stuff.

--- TAO/tests/Simple/grid/run_test.pl
+++ TAO/tests/Simple/grid/run_test.pl
@@ -4,7 +4,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
     & eval 'exec perl -S $0 $argv:q'
     if 0;
 
-unshift @INC, '../../../../bin';
+use lib "../../../../bin";
 require ACEutils;
 require Process;
 
@@ -12,7 +12,7 @@ $iorfile = "grid.ior";
 
 $SV = Process::Create ("server$Process::EXE_EXT", "-o $iorfile ");
 
-waitforfile ($iorfile);
+ACE::waitforfile ($iorfile);
 
 $status  = system ("client$Process::EXE_EXT -f $iorfile -s -w 12 -h 12 -x 2 -y 2 -v 2345");
 
