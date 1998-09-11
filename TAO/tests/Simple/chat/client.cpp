74bc324609254cb7ab193f19105f4e40c2e7fc25
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-11 20:34:27 +0000

    *** empty log message ***

--- TAO/tests/Simple/chat/client.cpp
+++ TAO/tests/Simple/chat/client.cpp
@@ -24,32 +24,26 @@ main(int argc, char* argv[])
 {
   printf("\n============= Simple Chat ===========\n");
 
-  char *nick;
-  char def_nick[] = "chat client";
+  char *nick = "chat client";
 
-  if (argc >=2 )
-    {
-      nick = argv[1];
-    }
+  if (argc >= 2)
+    nick = argv[1];
   else
-    {
-      printf("\n usage: Chat <nickname>\n");
-      nick = def_nick;
-    }
+    printf("\n usage: Chat <nickname>\n");
 
   TAO_TRY
     {
       Chat_i chat_i ("chat.ior", nick);
 
-      if (chat_i.init (argc, argv) == -1 || chat_i.run() == -1)
+      if (chat_i.init (argc, argv) == -1 || chat_i.run () == -1)
 	{
-	  return 1;
+	  return -1;
 	}
     }
   TAO_CATCHANY
     {
       TAO_TRY_ENV.print_exception ("chat::main\t\n");
-      return 1;
+      return -1;
     }
   TAO_ENDTRY;
 
