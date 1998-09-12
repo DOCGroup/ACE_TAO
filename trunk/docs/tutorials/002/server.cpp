f92d51b906ca8870696d5398ee59f7e15f58cc6b
jcej <jcej@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-12 02:48:31 +0000

    *** empty log message ***

--- trunk/docs/tutorials/002/server.cpp
+++ trunk/docs/tutorials/002/server.cpp
@@ -2,129 +2,76 @@
 // $Id$
 
 
+/*
+  As before, we need a few ACE objects as well as our Logging_Handler declaration.
+ */
 #include "ace/Acceptor.h"
 #include "ace/SOCK_Acceptor.h"
 #include "ace/Reactor.h"
+#include "handler.h"
 
+/*
+  We'll still use the global reactor pointer.  There's a snappy way around this
+  that shows up in later server tutorials.
+ */
 ACE_Reactor * g_reactor;
 
+/*
+  This was hinted at in Tutorial 1.  Remember the hand-coded acceptor that we
+  created there?  This template does all of that and more and better.  If you
+  find yourself creating code that doesn't feel like a part of your application,
+  there's a good chance that ACE has a template or framework component to do
+  it for you.
+ */
+typedef ACE_Acceptor < Logging_Handler, ACE_SOCK_ACCEPTOR > Logging_Acceptor;
+
+/*
+  One of the new things will be a signal handler so that we can exit the application
+  somewhat cleanly.  The 'finished' flag is used instead of the previous infninite
+  loop and the 'handler' will set that flag in respose to SIGINT (CTRL-C).
+ */
 static sig_atomic_t finished = 0;
-extern "C" void handler (int) { finished = 1; }
-
-// ----------------------------------------
-
-class Logging_Handler : public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH>
+extern "C" void handler (int)
 {
-
-public:
-
-  Logging_Handler (void)
-	{
-	}
-
-  virtual void destroy (void)
-	{
-	  g_reactor->cancel_timer (this);
-	  this->peer ().close ();
-	  delete this;
-	}
-
-  virtual int open (void *)
-	{
-	  ACE_INET_Addr addr;
-	  
-	  if (this->peer ().get_remote_addr (addr) == -1)
-	    return -1;
-	  else
-	    {
-	      ACE_OS::strncpy (this->peer_name_, addr.get_host_name (), MAXHOSTNAMELEN + 1);
-	
-	      if (g_reactor->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
-		ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) can't register with reactor\n"), -1);
-
-	      else if (g_reactor->schedule_timer (this, (const void *) this, ACE_Time_Value (2), ACE_Time_Value (2)) == -1)
-		ACE_ERROR_RETURN ((LM_ERROR, "can'(%P|%t) t register with reactor\n"), -1);
-
-	      else
-	      	ACE_DEBUG ((LM_DEBUG, "(%P|%t) connected with %s\n", this->peer_name_));
-
-	      return 0;
-	    }
-	}
-
-  virtual int close (u_long)
-	{
-	  this->destroy ();
-	  return 0;
-	}
-
-protected:
-
-  virtual int handle_input (ACE_HANDLE)
-        {
-          char buf[128];
-          memset(buf,0,sizeof(buf));
-        
-          switch( this->peer().recv(buf,sizeof buf) )
-          {
-          case -1:
-            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) %p bad read\n", "client logger"), -1);
-          case 0:
-            ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) closing log daemon (fd = %d)\n", this->get_handle ()), -1);
-          default:
-            ACE_DEBUG ((LM_DEBUG, "(%P|%t) from client: %s",buf));
-          }
-          
-          return 0;
-        }
-
-  virtual int handle_timeout (const ACE_Time_Value &tv, const void *arg)
-	{
-	  ACE_ASSERT (arg == this);
-	  ACE_DEBUG ((LM_DEBUG, "(%P|%t) handling timeout from this = %u\n", this));
-	  return 0;
-	}
-
-private:
-
-  char peer_name_[MAXHOSTNAMELEN + 1];
-
-};
-
-
-typedef ACE_Acceptor <Logging_Handler, ACE_SOCK_ACCEPTOR> Logging_Acceptor;
-
+  finished = 1;
+}
 
 static const u_short PORT = ACE_DEFAULT_SERVER_PORT;
 
-int main (int argc, char *argv[])
+int main (int, char **)
 {
+  // Create the reactor we'll register our event handler derivatives with.
   g_reactor = new ACE_Reactor;
 
-  // Acceptor factory.
+  // Create the acceptor that will listen for client connetions
   Logging_Acceptor peer_acceptor;
 
-  /*  Provide a reactor on the open.  This prevents ACE_Reactor::instance() from
-      creating an unnecessary instance.
+  /*
+    Notice how similar this is to the open() call in Tutorial 1.  I read
+    ahead when I created that one so that it would come out this way...
    */
   if (peer_acceptor.open (ACE_INET_Addr (PORT), g_reactor) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "open"), -1);
 
-  /*  This is redundant because opening the acceptor with a reactor pointer will
-      cause the acceptor to be registered with that reactor.
+  /*
+    Here's the easiest way to respond to signals in your application.  Simply
+    construct an ACE_Sig_Action object with a "C" function and the signal you
+    want to capture.  As you might expect, there is also a way to register
+    signal handlers with a reactor but we take the easy-out here.
    */
-  else if (g_reactor->register_handler (&peer_acceptor, ACE_Event_Handler::READ_MASK) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "registering service with ACE_Reactor\n"), -1);
-
   ACE_Sig_Action sa ((ACE_SignalHandler) handler, SIGINT);
-
-  // Run forever, performing logging service.
 
   ACE_DEBUG ((LM_DEBUG, "(%P|%t) starting up server logging daemon\n"));
 
-  // Perform logging service until QUIT_HANDLER receives SIGINT.
-  while ( !finished )
+  // Perform logging service until the signal handler receives SIGINT.
+  while (!finished)
     g_reactor->handle_events ();
+
+  // Close the acceptor so that no more clients will be taken in.
+  peer_acceptor.close();
+
+  // Free up the memory allocated for the reactor.
+  delete g_reactor;
 
   ACE_DEBUG ((LM_DEBUG, "(%P|%t) shutting down server logging daemon\n"));
 
