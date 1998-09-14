02e8ac847e9fdad71745cc9c7ac3d16ee700d9b0
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-14 03:31:56 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_38'.

---
+++ tags/ACE-4_5_38/ace/Array.h
@@ -0,0 +1,38 @@
+/* -*- C++ -*- */
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    ace
+//
+// = FILENAME
+//    Array.h
+//
+// = DESCRIPTION
+//    NOTE: this file has been deprecated and will soon go away.  You
+//    should directly include "Containers.h" instead.
+//
+// = AUTHOR
+//    Doug Schmidt
+//
+// ============================================================================
+
+#if !defined (ACE_ARRAY_H)
+#define ACE_ARRAY_H
+
+#include "ace/Containers.h"
+
+#if defined (__ACE_INLINE__)
+#include "ace/Containers.i"
+#endif /* __ACE_INLINE__ */
+
+#if defined (ACE_TEMPLATES_REQUIRE_SOURCE)
+#include "ace/Containers.cpp"
+#endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
+
+#if defined (ACE_TEMPLATES_REQUIRE_PRAGMA)
+#pragma implementation ("Containers.cpp")
+#endif /* ACE_TEMPLATES_REQUIRE_PRAGMA */
+
+#endif /* ACE_ARRAY_H */
