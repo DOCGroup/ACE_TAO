cc2f58726a28a4356d1f0f240f5b23724914df05
mjb2 <mjb2@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-15 22:11:37 +0000

    See Tue Sep 15 17:07:19 1998  Matthew J Braun  <mjb2@cec.wustl.edu>

--- TAO/tests/POA/Default_Servant/server.cpp
+++ TAO/tests/POA/Default_Servant/server.cpp
@@ -21,6 +21,38 @@
 
 ACE_RCSID(Default_Servant, server, "$Id$")
 
+static char *ior_output_file = 0;
+
+static int
+parse_args (int argc, char **argv)
+{
+  ACE_Get_Opt get_opts (argc, argv, "o:d");
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 'o':
+        ior_output_file = ACE_OS::strdup (get_opts.optarg);
+        break;
+      case 'd':
+	TAO_debug_level++;
+	break;
+      case '?':
+      default:
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage:  %s "
+                           "[-oior_output_file]"
+			   "[-d]"
+                           "\n",
+                           argv [0]),
+                          -1);
+      }
+
+  // Indicates successful parsing of command line.
+  return 0;
+}
+
 int 
 main (int argc, char **argv)
 {
@@ -34,6 +66,10 @@ main (int argc, char **argv)
       return -1;
     }
 
+  int result = parse_args (argc, argv);
+  if (result != 0)
+    return result;
+ 
   // Get the Root POA object reference
   CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
 
@@ -155,9 +191,22 @@ main (int argc, char **argv)
       return -1;
     }
 
-  ACE_DEBUG ((LM_DEBUG,"%s\n",
-              file_system_ior.in ()));
-
+  if (TAO_debug_level > 0)
+    ACE_DEBUG ((LM_DEBUG,"%s\n",
+		file_system_ior.in ()));
+  
+  // If the ior_output_file exists, output the ior to it
+  if (ior_output_file != 0)
+    {
+      FILE *output_file= ACE_OS::fopen (ior_output_file, "w");
+      if (output_file == 0)
+	ACE_ERROR_RETURN ((LM_DEBUG, "Cannot open output file for writing IOR: %s", 
+			   ior_output_file),
+			  -1);  
+      ACE_OS::fprintf (output_file, "%s", file_system_ior.in ());
+      ACE_OS::fclose (output_file);
+    }
+  
   // set the state of the poa_manager to active i.e ready to process requests
   poa_manager->activate (env);
   if (env.exception () != 0)
