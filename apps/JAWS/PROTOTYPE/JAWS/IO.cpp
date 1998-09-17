4d4eb81ea526843ed3cf238fb0a8b8864494837f
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-17 23:53:48 +0000

    ChangeLogTag:Thu Sep 17 18:53:05 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- apps/JAWS/PROTOTYPE/JAWS/IO.cpp
+++ apps/JAWS/PROTOTYPE/JAWS/IO.cpp
@@ -161,24 +161,24 @@ JAWS_Synch_IO::transmit_file (JAWS_IO_Handler *ioh,
 #else
       // Attempting to use writev
       // Is this faster?
-      ACE_IO_Vector iov[3];
+      iovec iov[3];
       int iovcnt = 0;
       if (header_size > 0)
         {
-          iov[iovcnt].buffer ((char*)header);
-          iov[iovcnt].length (header_size);
+          iov[iovcnt].iov_base = (char*)header;
+          iov[iovcnt].iov_len =  header_size;
           iovcnt++;
         }
       if (handle.size () > 0)
         {
-          iov[iovcnt].buffer (handle.address ());
-          iov[iovcnt].length (handle.size ());
+          iov[iovcnt].iov_base = handle.address ();
+          iov[iovcnt].iov_len = handle.size ();
           iovcnt++;
         }
       if (trailer_size > 0)
         {
-          iov[iovcnt].buffer ((char*)trailer);
-          iov[iovcnt].length (trailer_size);
+          iov[iovcnt].iov_base = (char*)trailer;
+          iov[iovcnt].iob_len = trailer_size;
           iovcnt++;
         }
       if (ACE_OS::writev (ioh->handle (), iov, iovcnt) < 0)
