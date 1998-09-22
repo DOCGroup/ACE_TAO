8b4282ea05440501f99af7cf3929d650c95e5b87
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-09-22 03:55:32 +0000

    *** empty log message ***

--- examples/Bounded_Packet_Relay/BPR_Drivers.cpp
+++ examples/Bounded_Packet_Relay/BPR_Drivers.cpp
@@ -23,9 +23,6 @@
 //
 // ============================================================================
 
-#if !defined (_BPR_DRIVER_CPP_)
-#define _BPR_DRIVER_CPP_
-
 #include "BPR_Drivers.h"
 
 ACE_RCSID(Bounded_Packet_Relay, BPR_Drivers, "$Id$")
@@ -169,4 +166,258 @@ Input_Device_Wrapper_Base::send_input_message (ACE_Message_Block *amb)
                       -1);
 }
 
-#endif /* _BPR_DRIVER_CPP_ */
+// Constructor.
+
+Bounded_Packet_Relay::Bounded_Packet_Relay (ACE_Thread_Manager *input_task_mgr,
+                                            Input_Device_Wrapper_Base *input_wrapper,
+                                            Output_Device_Wrapper_Base *output_wrapper)
+  : input_task_mgr_ (input_task_mgr),
+    input_wrapper_ (input_wrapper),
+    output_wrapper_ (output_wrapper),
+    transmission_number_ (0),
+    packets_sent_ (0),
+    status_ (Bounded_Packet_Relay::UN_INITIALIZED),
+    transmission_start_ (ACE_Time_Value::zero),
+    transmission_end_ (ACE_Time_Value::zero)
+{
+  if (input_task_mgr_ == 0)
+    input_task_mgr_ = ACE_Thread_Manager::instance ();
+}
+
+// Destructor.
+
+Bounded_Packet_Relay::~Bounded_Packet_Relay (void)
+{
+}
+
+// Requests output be sent to output device.
+
+int
+Bounded_Packet_Relay::send_input (void)
+{
+  // Don't block, return immediately if queue is empty.
+  ACE_Message_Block *item;
+
+  // Using a separate (non-const) time value 
+  // is necessary on some platforms
+  ACE_Time_Value immediate (ACE_Time_Value::zero);
+
+  if (queue_.dequeue_head (item,
+                           &immediate) < 0)
+    return 1;
+
+  // If a message block was dequeued, send it to the output device.
+
+  if (output_wrapper_->write_output_message ((void *) item) < 0)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%t %p\n", 
+                       "failed to write to output device object"), 
+                      -1);
+  // If all went OK, increase count of packets sent.
+  ++packets_sent_;
+  return 0;
+}
+
+// Requests a transmission be started.
+
+int
+Bounded_Packet_Relay::start_transmission (u_long packet_count,
+                                                         u_long arrival_period,
+                                                         u_long logging_level)
+{
+  // Serialize access to start and end transmission calls, statistics
+  // reporting calls.
+  ACE_GUARD_RETURN (ACE_SYNCH_MUTEX, ace_mon, this->transmission_lock_, -1);
+
+  // If a transmission is already in progress, just return.
+  if (status_ == STARTED)
+    return 1;
+
+  // Update statistics for a new transmission.
+  ++transmission_number_;
+  packets_sent_ = 0;
+  status_ = STARTED;
+  transmission_start_ = ACE_OS::gettimeofday ();  
+
+  // Initialize the output device.
+  if (output_wrapper_->modify_device_settings ((void *) &logging_level) < 0)
+    {
+      status_ = ERROR_DETECTED;
+      transmission_end_ = ACE_OS::gettimeofday ();  
+      ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                         "failed to initialize output device object"), 
+                        -1);
+    }
+  // Initialize the input device.
+  else if (input_wrapper_->set_input_period (arrival_period) < 0)
+    {
+      status_ = ERROR_DETECTED;
+      transmission_end_ = ACE_OS::gettimeofday ();  
+      ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                         "failed to initialize input device object"), 
+                        -1);
+    }
+  else if (input_wrapper_->set_send_count (packet_count) < 0)
+    {
+      status_ = ERROR_DETECTED;
+      transmission_end_ = ACE_OS::gettimeofday ();  
+      ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                         "failed to initialize input device object"), 
+                        -1);
+    }
+  // Activate the input device.
+  else if (input_wrapper_->activate () < 0)
+    {
+      status_ = ERROR_DETECTED;
+      transmission_end_ = ACE_OS::gettimeofday ();  
+      ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                         "failed to activate input device object"), 
+                        -1);
+    }
+
+  // If all went well, print a startup message and return success.
+  ACE_DEBUG ((LM_DEBUG,
+              "\n\nTransmission %u started\n\n",
+              transmission_number_));
+  return 0;
+}
+
+// Requests a transmission be ended.
+
+int
+Bounded_Packet_Relay::end_transmission (Transmission_Status status)
+{
+  // Serialize access to start and end transmission calls,
+  // statistics reporting calls.
+  ACE_GUARD_RETURN (ACE_SYNCH_MUTEX, ace_mon, this->transmission_lock_, -1);
+
+  // If a transmission is not already in progress, just return.
+  if (status_ != STARTED)
+    return 1;
+
+  // Ask the the input thread to stop.
+  if (input_wrapper_->request_stop () < 0)
+    {
+      status_ = ERROR_DETECTED;
+      transmission_end_ = ACE_OS::gettimeofday ();  
+      ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                         "failed asking input device thread to stop"), 
+                        -1);
+    }
+ 
+  // Deactivate the queue, allowing all waiting threads to continue.
+  queue_.deactivate ();
+
+  // Wait for input thread to stop.
+  if (input_task_mgr_->wait_task (input_wrapper_) < 0)
+    {
+      status_ = ERROR_DETECTED;
+      transmission_end_ = ACE_OS::gettimeofday ();  
+      ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                         "failed waiting for input device thread to stop"), 
+                        -1);
+    }
+
+  // Reactivate the queue, and then clear it.
+  queue_.activate ();
+  while (! queue_.is_empty ())
+    {
+      ACE_Message_Block *msg;
+      queue_.dequeue_head (msg);
+      delete msg;
+    }
+
+  // If all went well, set passed status, stamp end time, print a
+  // termination message, and return success.
+  status_ = status;
+  transmission_end_ = ACE_OS::gettimeofday ();  
+  ACE_DEBUG ((LM_DEBUG,
+              "\n\nTransmission %u ended with status: %s\n\n",
+              transmission_number_, status_msg ()));
+  return 0;
+}
+
+// Requests a report of statistics from the last transmission.
+
+int
+Bounded_Packet_Relay::report_statistics (void)
+{
+  // Serialize access to start and end transmission calls,
+  // statistics reporting calls.
+  ACE_GUARD_RETURN (ACE_SYNCH_MUTEX, ace_mon, this->transmission_lock_, -1);
+
+  // If a transmission is already in progress, just return.
+  if (status_ == STARTED)
+    return 1;
+
+  // Calculate duration of trasmission.
+  ACE_Time_Value duration (transmission_end_);
+  duration -= transmission_start_;
+
+  // Report transmission statistics.
+  ACE_DEBUG ((LM_DEBUG,
+              "\n\nStatisics for transmission %u:\n\n"
+              "Transmission status: %s\n"
+              "Start time:          %d (sec) %d (usec)\n"
+              "End time:            %d (sec) %d (usec)\n"
+              "Duration:            %d (sec) %d (usec)\n"
+              "Packets relayed:     %u\n\n",
+              transmission_number_, status_msg (), 
+              transmission_start_.sec (),
+              transmission_start_.usec (),
+              transmission_end_.sec (),
+              transmission_end_.usec (),
+              duration.sec (),
+              duration.usec (),
+              packets_sent_));
+  return 0;
+}
+
+// Public entry point to which to push input.
+
+int
+Bounded_Packet_Relay::receive_input (void * arg)
+{
+  ACE_Message_Block *message = ACE_static_cast (ACE_Message_Block *,
+                                                arg);
+  if (queue_.enqueue_tail (message) < 0)
+    ACE_ERROR_RETURN ((LM_ERROR, "%t %p\n", 
+                       "Bounded_Packet_Relay::receive_input failed"), 
+                      -1);
+  return 0;
+}
+
+// Returns string corresponding to current status.
+
+const char *
+Bounded_Packet_Relay::status_msg (void)
+{
+  const char *status_msg;
+  switch (status_)
+    {
+      case UN_INITIALIZED:
+        status_msg = "uninitialized";
+        break;
+      case STARTED: 
+        status_msg = "in progress";
+        break;
+      case COMPLETED: 
+        status_msg = "completed with all packets sent";
+        break;
+      case TIMED_OUT: 
+        status_msg = "terminated by transmission duration timer";
+        break;
+      case CANCELLED:
+        status_msg = "cancelled by external control";
+        break;
+      case ERROR_DETECTED:
+        status_msg = "error was detected";
+        break;
+      default:
+        status_msg = "unknown transmission status";
+        break;
+    }
+
+  return status_msg;
+}
+
