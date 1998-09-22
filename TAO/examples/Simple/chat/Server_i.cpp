2b90808996856bc5b9572f2b02d71b8b4f81986c
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-22 04:25:34 +0000

    *** empty log message ***

--- TAO/examples/Simple/chat/Server_i.cpp
+++ TAO/examples/Simple/chat/Server_i.cpp
@@ -18,66 +18,93 @@
 // ===========================================================
 
 #include "Server_i.h"
+#include "ace/Get_Opt.h"
 
-Server_i::Server_i(char *ior_file_name):ior_file_name_(ior_file_name)
+Server_i::Server_i ()
+  : ior_file_name_ ("chat.ior")
 {
+  // No Op.
 }
 
-Server_i::~Server_i()
+Server_i::~Server_i (void)
 {
+  // NO Op.
 }
 
-int Server_i::init (int argc,
-	    char *argv[],
-	    CORBA::Environment &env)
+int
+Server_i::parse_args (int argc, char *argv[])
 {
- if (this->orb_manager_.init_child_poa (argc,
-					 argv,
-					 "child_poa",
-  					 env) == -1)
-  {
+  ACE_Get_Opt get_opts (argc, argv, "o:");
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 'o':  // get the file name to write to
+       this->ior_file_name_ = get_opts.optarg;
+      break;
+
+      case '?':  // display help for use of the server.
+      default:
+	ACE_ERROR_RETURN ((LM_ERROR,
+			   "usage:  %s"
+			   " [-o] <ior_output_file>"
+			   "\n",
+			   argv [0]),
+			  -1);
+      }
+
+  return 0;
+}
+
+int
+Server_i::init (int argc,
+		char *argv[],
+		CORBA::Environment &env)
+{
+  // Parse the command line options.
+  if (this-> parse_args(argc, argv) == -1)
+    return -1;
+
+  if (this->orb_manager_.init (argc,
+			       argv,
+			       env) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
-                       "%p\n",
-                       "init_child_poa"),
-                      -1);
-  }
-
-  TAO_CHECK_ENV_RETURN (env, -1);
-
+		       "%p\n",
+		       "orb manager init failed\n"),
+		      -1);
+  TAO_CHECK_ENV_RETURN (env,
+			-1);
   CORBA::ORB_var orb = this->orb_manager_.orb ();
 
-  // Activate the servant in its own child POA.
-  CORBA::String_var str  =
-    this->orb_manager_.activate_under_child_poa ("chat_server",
-                                                 &(this->broadcaster_i_),
-                                                 env);
-  /*
-    ACE_DEBUG ((LM_DEBUG,
-              "The IOR is: <%s>\n",
-              str.in ()));
-  */
-
-  this->writeIOR(str.in());
-
+  // Activate the servant.
+  CORBA::String_var str =
+    this->orb_manager_.activate (&this->broadcaster_i_,
+				 env);
+  // Write the IOR to a file.
+  this->write_IOR (str.in ());
   return 0;
 }
 
-int Server_i::run (CORBA::Environment &env)
+int
+Server_i::run (CORBA::Environment &env)
 {
- // Run the main event loop for the ORB.
+  ACE_DEBUG ((LM_DEBUG,
+	      "Running chat server...\n"));
+
+  // Run the main event loop for the ORB.
   if (this->orb_manager_.run (env) == -1)
-    {
-      ACE_ERROR_RETURN ((LM_ERROR,
-                         "Server_i::run"),
-                         -1);
-    }
-
+    ACE_ERROR_RETURN ((LM_ERROR,
+		       "Server_i::run"),
+		      -1);
   return 0;
 }
 
-int Server_i::writeIOR(const char* ior)
+int
+Server_i::write_IOR(const char* ior)
 {
-  FILE* ior_output_file_ = ACE_OS::fopen (this->ior_file_name_, "w");
+  FILE* ior_output_file_ =
+    ACE_OS::fopen (this->ior_file_name_, "w");
 
   if (ior_output_file_)
     {
@@ -86,5 +113,6 @@ int Server_i::writeIOR(const char* ior)
                        ior);
       ACE_OS::fclose (ior_output_file_);
     }
+
   return 0;
 }
