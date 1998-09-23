24a00797e240822ed3cd5a859387df516be670ee
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-23 15:09:46 +0000

    (findIfVisiIor,interpretIor): removed unused locals "nullOctet".

--- trunk/TAO/utils/IOR-parser/ior-handler.cpp
+++ trunk/TAO/utils/IOR-parser/ior-handler.cpp
@@ -6,7 +6,7 @@ IorHandler::IorHandler (void)
 {
 }
 
-int 
+int
 IorHandler::hexChar2int (char thisChar)
 {
   switch (thisChar)
@@ -37,7 +37,7 @@ IorHandler::getOctet8Field (char *readPtr, int *hexCharsRead)
   char octet8Holder[8];
   u_long value;
   int i;
- 
+
   for (i = 0; i < 8; i++)
     octet8Holder[i] = * (readPtr + i);
 
@@ -63,7 +63,7 @@ IorHandler::getOctet4Field (char *readPtr, int *hexCharsRead)
   // On a big endian platform, the MSB is first and the LSB is next
   value = 16*16*16* hexChar2int (octet4Holder[0]) +
     16*16* hexChar2int (octet4Holder[1]) +
-    16* hexChar2int (octet4Holder[2]) + 
+    16* hexChar2int (octet4Holder[2]) +
     hexChar2int (octet4Holder[3]);
 #endif
 
@@ -72,7 +72,7 @@ IorHandler::getOctet4Field (char *readPtr, int *hexCharsRead)
   // On a little endian platform, the LSB is first and the MSB is next
   value = 16*16*16* hexChar2int (octet4Holder[2]) +
     16*16* hexChar2int (octet4Holder[3]) +
-    16* hexChar2int (octet4Holder[0]) + 
+    16* hexChar2int (octet4Holder[0]) +
     hexChar2int (octet4Holder[1]);
 #endif
 
@@ -114,7 +114,7 @@ IorHandler::skipSpaceIfAny(char *readPtr, int *hexCharsRead)
   nullOctet[1] = *(readPtr + offset + 1);
 
   if ((nullOctet[0] == '2') && (nullOctet[1] == '0'))
-    offset += 2;  
+    offset += 2;
 
   *hexCharsRead = offset;
 }
@@ -142,19 +142,19 @@ IorHandler::skipNullOctets (char *readPtr, int *hexCharsRead, int expectingStr)
       // ie., 2020 in ASCII equivalent
 
       if ((nullOctet[0] == '2') && (nullOctet[1] == '0') &&
-	  (nullOctet[2] == '2') && (nullOctet[3] == '0'))
-	{
-	  offset += 4;
-	  continue;
-	}
+          (nullOctet[2] == '2') && (nullOctet[3] == '0'))
+        {
+          offset += 4;
+          continue;
+        }
 
       if ((nullOctet[0] == '0') && (nullOctet[1] == '0'))
-	offset += 2;  
+        offset += 2;
       else
-	if ((expectingStr) && (hexChar2int(nullOctet[0]) >= 8))
-	  offset += 2;
+        if ((expectingStr) && (hexChar2int(nullOctet[0]) >= 8))
+          offset += 2;
         else
-	  break;
+          break;
     }
 
   *hexCharsRead = offset;
@@ -163,13 +163,12 @@ IorHandler::skipNullOctets (char *readPtr, int *hexCharsRead, int expectingStr)
 int
 IorHandler::findIfVisiIor(char *readPtr, int *hexCharsRead)
 {
-  char nullOctet[2];
   int offset;
   int hasVisiPreamble = 0;
 
   *hexCharsRead = 0;
   offset = 0;
-  
+
   if ((getCharacter(readPtr, &offset) == 'P') &&
       (getCharacter(readPtr, &offset) == 'M') &&
       (getCharacter(readPtr, &offset) == 'C'))
@@ -205,7 +204,7 @@ IorHandler::getString (char *readPtr, int givenLen)
   int intEquiv;
   int i = 0;
   int j = 0;
- 
+
   // i indexes hexChars while j indexes octet pairs
 
   while (i <= (givenLen - 2))
@@ -217,7 +216,7 @@ IorHandler::getString (char *readPtr, int givenLen)
       sprintf (parsedOctetPair, "%c", intEquiv);
       parsedStr[j] = parsedOctetPair[0];
       j ++;
-      
+
       i += 2;
     }
 
@@ -228,12 +227,11 @@ void
 IorHandler::interpretIor (char *thisIor, struct IOR *thisIorInfo)
 {
   int numCharsToSkip;
-  char nullOctet[2];
   int validTypeId = 0;
   int isVisiIor = 0;
   int ulongValue;
 
-  // Skip the prefix "IOR:" 
+  // Skip the prefix "IOR:"
   int numHexCharsRead = 4;
 
   // Type ID must be at least 4 bytes in length since type_id is prefixed
@@ -244,34 +242,34 @@ IorHandler::interpretIor (char *thisIor, struct IOR *thisIorInfo)
     {
       skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 1);
       numHexCharsRead += numCharsToSkip;
-      
+
       // Read the length of the type_id field
       ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-				  &numCharsToSkip);
+                                  &numCharsToSkip);
 
       if (ulongValue == 0)
-	{
-	  ACE_DEBUG ((LM_DEBUG, 
-		      "type_id len seems to be 0 \n"));
-	  ACE_OS::exit (1);
-	}
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "type_id len seems to be 0 \n"));
+          ACE_OS::exit (1);
+        }
 
       numHexCharsRead += numCharsToSkip;
 
       if (ulongValue > 4)
-	{
-	  validTypeIdLen = 1;
-	  thisIorInfo->typeIdLen = ulongValue;
-	}
+        {
+          validTypeIdLen = 1;
+          thisIorInfo->typeIdLen = ulongValue;
+        }
 
       skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 1);
       numHexCharsRead += numCharsToSkip;
 
       // Read the Type ID
-      ACE_OS::strncpy (thisIorInfo->typeId, 
-		       getString((char *)(thisIor + numHexCharsRead),
-				 2 * thisIorInfo->typeIdLen),
-		       thisIorInfo->typeIdLen);
+      ACE_OS::strncpy (thisIorInfo->typeId,
+                       getString((char *)(thisIor + numHexCharsRead),
+                                 2 * thisIorInfo->typeIdLen),
+                       thisIorInfo->typeIdLen);
 
       // Confirm that this is a valid TypeId by looking for the IDL
       // interface. Additionally, the IDL interface could be extracted
@@ -279,18 +277,18 @@ IorHandler::interpretIor (char *thisIor, struct IOR *thisIorInfo)
       getIdlInterface(thisIorInfo->typeId, &validTypeId);
 
       if (!validTypeId)
-	{
-	  validTypeIdLen = 0;
-	  numHexCharsRead -= numCharsToSkip;
-	}
+        {
+          validTypeIdLen = 0;
+          numHexCharsRead -= numCharsToSkip;
+        }
     }
 
   numHexCharsRead += 2 * thisIorInfo->typeIdLen;
 
-  ACE_DEBUG ((LM_DEBUG, 
-	      "\nTypeId\t\t: %s (%d bytes)\n", 
-	      thisIorInfo->typeId,
-	      thisIorInfo->typeIdLen));
+  ACE_DEBUG ((LM_DEBUG,
+              "\nTypeId\t\t: %s (%d bytes)\n",
+              thisIorInfo->typeId,
+              thisIorInfo->typeIdLen));
 
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 0);
   numHexCharsRead += numCharsToSkip;
@@ -298,51 +296,51 @@ IorHandler::interpretIor (char *thisIor, struct IOR *thisIorInfo)
   // Read the 4 octets, which should equal 1 (numTaggedProfiles = 1)
   // for a single-component profile.
   ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if (ulongValue != 1)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "numTaggedProfiles = %d\n", 
-		  ulongValue));
+      ACE_DEBUG ((LM_DEBUG,
+                  "numTaggedProfiles = %d\n",
+                  ulongValue));
      ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip;  
+  numHexCharsRead += numCharsToSkip;
 
   // Read the 4 octets, which should equal 0 (TAG_INTERNET_IOP = 0)
   ulongValue = getOctet8Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if (ulongValue != 0)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "TAG_INTERNET_IOP != 0\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "TAG_INTERNET_IOP != 0\n"));
       ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip;  
+  numHexCharsRead += numCharsToSkip;
 
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 0);
   numHexCharsRead += numCharsToSkip;
- 
+
   // Read the 4 octets, which represent the length of the ProfileBody
   ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if (ulongValue == 0)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "ProfileBody len equals 0\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "ProfileBody len equals 0\n"));
       ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip;  
+  numHexCharsRead += numCharsToSkip;
   thisIorInfo->profileBodyLen = ulongValue;
 
-  ACE_DEBUG ((LM_DEBUG, 
-	      "\nTAG_INTERNET_IOP Profile (%d bytes):\n", 
-	      ulongValue));
+  ACE_DEBUG ((LM_DEBUG,
+              "\nTAG_INTERNET_IOP Profile (%d bytes):\n",
+              ulongValue));
 
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 1);
   numHexCharsRead += numCharsToSkip;
@@ -350,35 +348,35 @@ IorHandler::interpretIor (char *thisIor, struct IOR *thisIorInfo)
   // Read the 4 octets, which represent the IIOP version number = 1
   // Read the major number of the IIOP Version (should be 1)
   ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if (ulongValue != 1)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "IIOP major version != 1\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "IIOP major version != 1\n"));
       ACE_OS::exit (1);
     }
-  
-  numHexCharsRead += numCharsToSkip;    
-  ACE_DEBUG ((LM_DEBUG, 
-	      "IIOP Version: %d.", 
-	      ulongValue));
+
+  numHexCharsRead += numCharsToSkip;
+  ACE_DEBUG ((LM_DEBUG,
+              "IIOP Version: %d.",
+              ulongValue));
 
   // Read the minor number of the IIOP Version (can be 0 or 1)
   ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if ((ulongValue != 0) && (ulongValue != 1))
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "IIOP minor version != 0 or 1\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "IIOP minor version != 0 or 1\n"));
      ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip; 
-  ACE_DEBUG ((LM_DEBUG, 
-	      "%d\n", 
-	      ulongValue));
+  numHexCharsRead += numCharsToSkip;
+  ACE_DEBUG ((LM_DEBUG,
+              "%d\n",
+              ulongValue));
 
   skipSpaceIfAny((char *)(thisIor + numHexCharsRead), &numCharsToSkip);
   numHexCharsRead += numCharsToSkip;
@@ -387,156 +385,156 @@ IorHandler::interpretIor (char *thisIor, struct IOR *thisIorInfo)
 
   // Read the 2 octets, which represent the length of the hostname
   ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if (ulongValue == 0)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "hostLen equals 0\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "hostLen equals 0\n"));
       ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip;  
-  thisIorInfo->hostLen = ulongValue;  
+  numHexCharsRead += numCharsToSkip;
+  thisIorInfo->hostLen = ulongValue;
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 1);
   numHexCharsRead += numCharsToSkip;
 
   // Read the hostname and store it
-  ACE_OS::strncpy (thisIorInfo->HostName, 
-		   getString((char *)(thisIor + numHexCharsRead),
-			     2 * thisIorInfo->hostLen), thisIorInfo->hostLen);
-  numHexCharsRead += 2 * thisIorInfo->hostLen;  
+  ACE_OS::strncpy (thisIorInfo->HostName,
+                   getString((char *)(thisIor + numHexCharsRead),
+                             2 * thisIorInfo->hostLen), thisIorInfo->hostLen);
+  numHexCharsRead += 2 * thisIorInfo->hostLen;
 
-  ACE_DEBUG ((LM_DEBUG, 
-	      "    HostName   : %s (%d bytes)\n", 
-	      thisIorInfo->HostName,
-	      thisIorInfo->hostLen));
+  ACE_DEBUG ((LM_DEBUG,
+              "    HostName   : %s (%d bytes)\n",
+              thisIorInfo->HostName,
+              thisIorInfo->hostLen));
 
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 0);
   numHexCharsRead += numCharsToSkip;
-  
+
   // Read the port number and store it
   ulongValue = getOctet4Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
-  
+                              &numCharsToSkip);
+
   if (ulongValue == 0)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "port number equals 0\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "port number equals 0\n"));
       ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip;  
-  thisIorInfo->portNum = ulongValue;  
+  numHexCharsRead += numCharsToSkip;
+  thisIorInfo->portNum = ulongValue;
 
-  ACE_DEBUG ((LM_DEBUG, 
-	      "    Port Number: %d\n", 
-	      thisIorInfo->portNum));
+  ACE_DEBUG ((LM_DEBUG,
+              "    Port Number: %d\n",
+              thisIorInfo->portNum));
 
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 0);
   numHexCharsRead += numCharsToSkip;
 
-  // Read the object key length 
+  // Read the object key length
   ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-			      &numCharsToSkip);
+                              &numCharsToSkip);
 
   if (ulongValue == 0)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-		  "objectKeyLen equals 0\n"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "objectKeyLen equals 0\n"));
       ACE_OS::exit (1);
     }
 
-  numHexCharsRead += numCharsToSkip;  
-  thisIorInfo->objectKeyLen = ulongValue;  
+  numHexCharsRead += numCharsToSkip;
+  thisIorInfo->objectKeyLen = ulongValue;
 
   skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 1);
-  numHexCharsRead += numCharsToSkip;  
+  numHexCharsRead += numCharsToSkip;
 
   // VisiBroker tends to insert the preamble "PMC" (=> Post Modern
   // Computing), before the "real" object key actually starts. Identify
-  // if this is the case. 
-  isVisiIor = findIfVisiIor((char *)(thisIor + numHexCharsRead), 
-			    &numCharsToSkip);
+  // if this is the case.
+  isVisiIor = findIfVisiIor((char *)(thisIor + numHexCharsRead),
+                            &numCharsToSkip);
   if (isVisiIor)
     {
       numHexCharsRead += numCharsToSkip;
       skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 0);
       numHexCharsRead += numCharsToSkip;
 
-      ACE_DEBUG ((LM_DEBUG, 
-		  "    ObjectKey  : PMC"));
+      ACE_DEBUG ((LM_DEBUG,
+                  "    ObjectKey  : PMC"));
 
       skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 0);
       numHexCharsRead += numCharsToSkip;
 
       // Read the real object key length and then the real object key
       ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-				  &numCharsToSkip);
+                                  &numCharsToSkip);
 
       if (ulongValue == 0)
-	{
-	  ACE_DEBUG ((LM_DEBUG, 
-		      "VisiBroker IOR - real objectKeyLen equals 0\n"));
-	  ACE_OS::exit (1);
-	}
-      
-      numHexCharsRead += numCharsToSkip;  
-      
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "VisiBroker IOR - real objectKeyLen equals 0\n"));
+          ACE_OS::exit (1);
+        }
+
+      numHexCharsRead += numCharsToSkip;
+
       ACE_OS::strncpy (thisIorInfo->objectKey,
-		       getString((char *)(thisIor + numHexCharsRead), 
-				 2 * ulongValue), ulongValue);
+                       getString((char *)(thisIor + numHexCharsRead),
+                                 2 * ulongValue), ulongValue);
 
       numHexCharsRead += 2 * ulongValue;
 
-      ACE_DEBUG ((LM_DEBUG, 
-		  "%s", 
-		  thisIorInfo->objectKey, 
-		  ulongValue));
+      ACE_DEBUG ((LM_DEBUG,
+                  "%s",
+                  thisIorInfo->objectKey,
+                  ulongValue));
 
       skipNullOctets((char *)(thisIor + numHexCharsRead), &numCharsToSkip, 1);
-      numHexCharsRead += numCharsToSkip;  
-      
+      numHexCharsRead += numCharsToSkip;
+
       // Read the object name length and then the object name
       ulongValue = getOctet2Field((char *)(thisIor + numHexCharsRead),
-				  &numCharsToSkip);
+                                  &numCharsToSkip);
 
       if (ulongValue == 0)
-	{
-	  ACE_DEBUG ((LM_DEBUG, 
-		      "\n[No object name] (%d bytes)\n\n", 
-		      thisIorInfo->objectKeyLen));
-	  ACE_OS::exit (1);
-	}
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "\n[No object name] (%d bytes)\n\n",
+                      thisIorInfo->objectKeyLen));
+          ACE_OS::exit (1);
+        }
 
-      numHexCharsRead += numCharsToSkip;  
+      numHexCharsRead += numCharsToSkip;
 
       ACE_OS::strncpy (thisIorInfo->objectKey,
-		       getString((char *)(thisIor + numHexCharsRead), 
-				 2 * ulongValue), ulongValue);
+                       getString((char *)(thisIor + numHexCharsRead),
+                                 2 * ulongValue), ulongValue);
 
       numHexCharsRead += 2 * ulongValue;
 
-      ACE_DEBUG ((LM_DEBUG, 
-		  "[object name: %s] (%d bytes)\n\n", 
-		  thisIorInfo->objectKey, 
-		  thisIorInfo->objectKeyLen));
-    }  
+      ACE_DEBUG ((LM_DEBUG,
+                  "[object name: %s] (%d bytes)\n\n",
+                  thisIorInfo->objectKey,
+                  thisIorInfo->objectKeyLen));
+    }
 
   else
     {
       // Non-VisiBroker IOR - Read the object_key and store it
       ACE_OS::strncpy (thisIorInfo->objectKey,
-		       getString((char *)(thisIor + numHexCharsRead), 
-				 2 * thisIorInfo->objectKeyLen), 
-		       thisIorInfo->objectKeyLen);
+                       getString((char *)(thisIor + numHexCharsRead),
+                                 2 * thisIorInfo->objectKeyLen),
+                       thisIorInfo->objectKeyLen);
 
       numHexCharsRead += 2 * thisIorInfo->objectKeyLen;
 
-      ACE_DEBUG ((LM_DEBUG, 
-		  "    ObjectKey  : %s (%d bytes)\n\n", 
-		  thisIorInfo->objectKey,
-		  thisIorInfo->objectKeyLen));
+      ACE_DEBUG ((LM_DEBUG,
+                  "    ObjectKey  : %s (%d bytes)\n\n",
+                  thisIorInfo->objectKey,
+                  thisIorInfo->objectKeyLen));
     }
 
 }
@@ -550,7 +548,7 @@ IorHandler::getIdlInterface (char *typeId, int *validTypeId)
 
   // A sample type_id for an IDL interface name "EchoTests" is
   // IDL:EchoTests:1.0 => the trick is to isolate the parts between
-  // the two colons. 
+  // the two colons.
   if ((*typeId != 'I') && (*(typeId+1) != 'D') && (*(typeId+2) != 'L'))
     {
       *validTypeId = 0;
@@ -566,7 +564,7 @@ IorHandler::getIdlInterface (char *typeId, int *validTypeId)
       ACE_OS::exit (1);
     }
 
-  
+
   char *readEnd = strrchr (typeId, ':');
 
   if (readEnd == NULL)
@@ -584,7 +582,7 @@ IorHandler::getIdlInterface (char *typeId, int *validTypeId)
                    readStart+1,
                    lenInterface);
   idlInterface[lenInterface] = '\0';
-  
+
   *validTypeId = 1;
   return (char *) idlInterface;
 }
