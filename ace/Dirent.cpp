2afb775dad6eea4612d28f955941485a657dcf29
Steve Huston <shuston@riverace.com>
1998-09-23 14:17:16 +0000

    Wrapped shut_up_aCC so only aCC/CC on HP-UX see it.

--- ace/Dirent.cpp
+++ ace/Dirent.cpp
@@ -4,5 +4,7 @@
 #if defined (ACE_LACKS_INLINE_FUNCTIONS)
 #include "ace/Dirent.i"
 #else
+#if defined (HPUX) && !defined (__GNUG__)
 static int shut_up_aCC = 0;
+#endif /* HPUX && !g++ */
 #endif /* ACE_LACKS_INLINE_FUNCTIONS */
