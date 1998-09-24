e599fd9a5676fa2fe7af1abb6000e44951502d7e
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-24 20:43:24 +0000

    ChangeLogTag:Thu Sep 24 15:39:39 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/TAO/tao/poa_macros.h
+++ trunk/TAO/tao/poa_macros.h
@@ -3,51 +3,19 @@
 #if !defined (POA_MACROS_H)
 #define POA_MACROS_H
 
+#include "tao/try_macros.h"
+
 // Convenient macro for testing for deadlock, as well as for detecting
 // when mutexes fail.
 #define TAO_POA_WRITE_GUARD(MUTEX,OBJ,LOCK,ENV) \
-  do { \
-    ACE_Write_Guard<MUTEX> OBJ (LOCK); \
-    if (OBJ.locked () == 0) \
-      { \
-        CORBA::Exception *exception = new CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO); \
-        ENV.exception (exception); \
-        return; \
-      } \
-  } \
-  while (0);
+  TAO_WRITE_GUARD_THROW(MUTEX,OBJ,LOCK,ENV,CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO))
+
 #define TAO_POA_READ_GUARD(MUTEX,OBJ,LOCK,ENV) \
-  do { \
-    ACE_Read_Guard<MUTEX> OBJ (LOCK); \
-    if (OBJ.locked () == 0) \
-      { \
-        CORBA::Exception *exception = new CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO); \
-        ENV.exception (exception); \
-        return; \
-      } \
-  } \
-  while (0);
+  TAO_READ_GUARD_THROW(MUTEX,OBJ,LOCK,ENV,CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO))
+
 #define TAO_POA_WRITE_GUARD_RETURN(MUTEX,OBJ,LOCK,RETURN,ENV) \
-  do { \
-    ACE_Write_Guard<MUTEX> OBJ (LOCK); \
-    if (OBJ.locked () == 0) \
-      { \
-        CORBA::Exception *exception = new CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO); \
-        ENV.exception (exception); \
-        return RETURN; \
-      } \
-  } \
-  while (0);
+  TAO_WRITE_GUARD_THROW_RETURN(MUTEX,OBJ,LOCK,RETURN,ENV,CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO))
 #define TAO_POA_READ_GUARD_RETURN(MUTEX,OBJ,LOCK,RETURN,ENV) \
-  do { \
-    ACE_Read_Guard<MUTEX> OBJ (LOCK); \
-    if (OBJ.locked () == 0) \
-      { \
-        CORBA::Exception *exception = new CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO); \
-        ENV.exception (exception); \
-        return RETURN; \
-      } \
-  } \
-  while (0);
+  TAO_READ_GUARD_THROW_RETURN(MUTEX,OBJ,LOCK,RETURN,ENV,CORBA::OBJ_ADAPTER (CORBA::COMPLETED_NO))
 
 #endif /* POA_MACROS_H */
