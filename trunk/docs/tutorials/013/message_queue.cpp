5311306d9b3379c6b77e1f59a925813b93cd84dc
jcej <jcej@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-27 15:07:51 +0000

    *** empty log message ***

--- trunk/docs/tutorials/013/message_queue.cpp
+++ trunk/docs/tutorials/013/message_queue.cpp
@@ -8,79 +8,81 @@
 
 int run_test (int iterations, int threads, int subtasks)
 {
-        // Create a task with some subtasks.  Each Task is a thread
-        // pool of 'threads' size.  If a task has a subtask, it will
-        // forward the unit of work to the subtask when finished.  See 
-        // task.{h|cpp} for more details.
-    Task * task = new Task(subtasks);
+  // Create a task with some subtasks.  Each Task is a thread
+  // pool of 'threads' size.  If a task has a subtask, it will
+  // forward the unit of work to the subtask when finished.  See 
+  // task.{h|cpp} for more details.
+  Task *task = new Task (subtasks);
 
-    if (task->open (threads) == -1)
-    {
-        ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "open"), -1);
-    }
+  if (task->open (threads) == -1)
+  {
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "open"), -1);
+  }
 
-        // Give the threads a chance to get ready.
-    ACE_OS::sleep (ACE_Time_Value (1));
+  // Give the threads a chance to get ready.
+ACE_OS::sleep (ACE_Time_Value (1));
 
-    for (int i = 0; i < iterations; ++i)
-    {
-            // Create a custom message block that can contain our Work object
-        Message_Block *message = new Message_Block( new Work(i) );
+  for (int i = 0; i < iterations; ++i)
+  {
+    // Create a custom message block that can contain our Work object
+    Message_Block *message = new Message_Block (new Work (i));
 
-            // Put the "unit of work" into the message queue
-        if (task->putq (message) == -1)
-        {
-        	ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "putq"), -1);
-        }
-    }
-
-        // The default constructor of our custom message block will
-        // insert a message telling our task to shutdown.
-    Message_Block *message = new Message_Block( );
-
-        // Put the shutdown request into the thread pool
+    // Put the "unit of work" into the message queue
     if (task->putq (message) == -1)
     {
-      	ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "putq"), -1);
-	}
-
-        // Wait for the task to shut down.  Any subtasks will also be
-        // waited for.
-    task->wait ();
-
-        // Delete our Task to prevent a memory leak
-    delete task;
-
-        // Ask our memory leak detector if things are OK
-    if( MLD_COUNTER != 0 )
-    {
-      	ACE_DEBUG ((LM_DEBUG, "(%P|%t) Memory Leak!\n"));
+      ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "putq"), -1);
     }
-        
-    return (0);
+  }
+
+  // The default constructor of our custom message block will
+  // insert a message telling our task to shutdown.
+  Message_Block *message = new Message_Block ();
+
+  // Put the shutdown request into the thread pool
+  if (task->putq (message) == -1)
+  {
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "putq"), -1);
+  }
+
+  // Wait for the task to shut down.  Any subtasks will also be
+  // waited for.
+  task->wait ();
+
+  // Delete our Task to prevent a memory leak
+  delete task;
+
+  // Ask our memory leak detector if things are OK
+  if (MLD_COUNTER != 0)
+  {
+    ACE_DEBUG ((LM_DEBUG, "(%P|%t) Memory Leak!\n"));
+  }
+
+  return (0);
 }
 
 int main (int argc, char *argv[])
 {
-        // Number of Work objects to put into the Task pool
-    int iterations = argc > 1 ? atoi (argv[1]) : 4;
-        // Number of threads for each Task
-    int threads = argc > 2 ? atoi (argv[2]) : 2;
-        // Number of tasks to chain after the primary task
-    int subtasks = argc > 3 ? atoi(argv[3]) : 1;
-    
-    (void) run_test (iterations, threads, subtasks);
+  // Number of Work objects to put into the Task pool
+  int iterations = argc > 1 ? atoi (argv[1]) : 4;
+  // Number of threads for each Task
+  int threads = argc > 2 ? atoi (argv[2]) : 2;
+  // Number of tasks to chain after the primary task
+  int subtasks = argc > 3 ? atoi (argv[3]) : 1;
 
-    ACE_DEBUG ((LM_DEBUG, "(%P|%t) Application exiting\n"));
+  (void) run_test (iterations, threads, subtasks);
 
-    exit (0);
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Application exiting\n"));
+
+  exit (0);
 }
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template class ACE_Guard<ACE_Mutex>;
-template class ACE_Lock_Adapter<ACE_Mutex>;
-template class ACE_Atomic_Op<ACE_Mutex, int>;
+template class ACE_Guard < ACE_Mutex >;
+template class ACE_Lock_Adapter < ACE_Mutex >;
+template class ACE_Atomic_Op < ACE_Mutex, int >;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate ACE_Guard<ACE_Mutex>;
 #pragma instantiate ACE_Lock_Adapter<ACE_Mutex>;
 #pragma instantiate ACE_Atomic_Op<ACE_Mutex, int>;
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+#endif /*
+          ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION 
+        */
--- trunk/docs/tutorials/013/task.h
+++ trunk/docs/tutorials/013/task.h
@@ -8,40 +8,40 @@
 #include "mld.h"
 
 /*
-  This is much like the Task we've used in the past for implementing a 
-  thread pool.  This time, however, I've made the Task an element in a 
-  singly-linked list.  As the svc() method finishes the process() on a 
-  unit of work, it will enqueue that unit of work to the next_ Task if 
-  there is one.  If the Task does not have a next_ Task, it will
-  invoke the unit of work object's fini() method after invoking process().
+   This is much like the Task we've used in the past for implementing a  thread 
+   pool.  This time, however, I've made the Task an element in a  singly-linked 
+   list.  As the svc() method finishes the process() on a  unit of work, it
+   will enqueue that unit of work to the next_ Task if  there is one.  If the
+   Task does not have a next_ Task, it will invoke the unit of work object's
+   fini() method after invoking process(). 
  */
 class Task : public ACE_Task < ACE_MT_SYNCH >
 {
 public:
 
-    typedef ACE_Task < ACE_MT_SYNCH > inherited;
+  typedef ACE_Task < ACE_MT_SYNCH > inherited;
 
-        // Construct ourselves and an optional number of subtasks
-        // chained beyond us.
-    Task ( int sub_tasks = 0 );
-    ~Task (void);
+  // Construct ourselves and an optional number of subtasks
+  // chained beyond us.
+    Task (int sub_tasks = 0);
+   ~Task (void);
 
-        // Open the Task with the proper thread-pool size
-    int open (int threads = 1 );
+  // Open the Task with the proper thread-pool size
+  int open (int threads = 1);
 
-        // Take Unit_Of_Work objects from the thread pool and invoke
-        // their process() and/or fini() as appropriate.
-    int svc (void);
+  // Take Unit_Of_Work objects from the thread pool and invoke
+  // their process() and/or fini() as appropriate.
+  int svc (void);
 
-        // Shut down the thread pool and it's associated subtasks
-    int close (u_long flags = 0);
+  // Shut down the thread pool and it's associated subtasks
+  int close (u_long flags = 0);
 
-        // Wait for the pool and subtasks to close
-    int wait(void);
-    
+  // Wait for the pool and subtasks to close
+  int wait (void);
+
 protected:
     ACE_Barrier * barrier_;
-    Task * next_;
+  Task *next_;
     MLD;
 };
 
