c017f915c9f917304a2014942a41badd67913319
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-09-30 21:10:00 +0000

    ChangeLogTag:Wed Sep 30 15:47:26 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/tao/ORB_Core.cpp
+++ TAO/tao/ORB_Core.cpp
@@ -1505,8 +1505,8 @@ TAO_ORB_Core_instance (void)
     template void ACE_Convert (const char *, u_int &);
 # endif /* __GNUG__ */
 
-template class ACE_Malloc<ACE_MEMORY_POOL,ACE_SYNCH_MUTEX>;
-template class ACE_Allocator_Adapter<ACE_Malloc<ACE_MEMORY_POOL,ACE_SYNCH)MUTEX> >;
+template class ACE_Malloc<ACE_LOCAL_MEMORY_POOL,ACE_SYNCH_MUTEX>;
+template class ACE_Allocator_Adapter<ACE_Malloc<ACE_LOCAL_MEMORY_POOL,ACE_SYNCH_MUTEX> >;
 template class ACE_Env_Value<int>;
 template class ACE_Env_Value<u_int>;
 template class ACE_Strategy_Acceptor<TAO_Server_Connection_Handler, TAO_SOCK_ACCEPTOR>;
@@ -1557,8 +1557,8 @@ template class ACE_Select_Reactor_T< ACE_Select_Reactor_Token_T<ACE_Noop_Token> 
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
-#pragma instantiate ACE_Malloc<ACE_MEMORY_POOL,ACE_SYNCH_MUTEX>
-#pragma instantiate ACE_Allocator_Adapter<ACE_Malloc<ACE_MEMORY_POOL,ACE_SYNCH)MUTEX> >
+#pragma instantiate ACE_Malloc<ACE_LOCAL_MEMORY_POOL,ACE_SYNCH_MUTEX>
+#pragma instantiate ACE_Allocator_Adapter<ACE_Malloc<ACE_LOCAL_MEMORY_POOL,ACE_SYNCH_MUTEX> >
 #pragma instantiate ACE_Env_Value<int>
 #pragma instantiate ACE_Env_Value<u_int>
 #pragma instantiate ACE_Strategy_Acceptor<TAO_Server_Connection_Handler, TAO_SOCK_ACCEPTOR>
