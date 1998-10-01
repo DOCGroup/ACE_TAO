d0c15c59b6ca672fad3034ef130def2b970d633a
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-01 19:59:01 +0000

    Added return 0; for generated code in operator>>=

--- trunk/TAO/TAO_IDL/be/be_visitor_exception/any_op_cs.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_exception/any_op_cs.cpp
@@ -101,7 +101,8 @@ be_visitor_exception_any_op_cs::visit_exception (be_exception *node)
       << "_tao_elem = (" << node->name () << " *)_tao_any.value ();"
       << be_nl
       << "return 1;" << be_uidt_nl
-      << "}" << be_uidt_nl
+      << "}" << be_nl
+      << "return 0;" << be_uidt_nl
       << "}\n\n";
 
 
