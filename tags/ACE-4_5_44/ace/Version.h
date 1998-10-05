0100f9f61593291a424e730010c5788f6a190294
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-05 21:03:27 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_44'.

---
+++ tags/ACE-4_5_44/ace/Version.h
@@ -0,0 +1,7 @@
+// $Id$
+// This is an automatically generated file.
+
+#define ACE_MAJOR_VERSION (4u)
+#define ACE_MINOR_VERSION (5u)
+#define ACE_BETA_VERSION (44u)
+#define ACE_VERSION "4.5.44\0"
