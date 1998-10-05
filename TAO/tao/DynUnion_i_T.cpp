6104c484ac1c591948ed5b16b07b69abad5fdd75
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-05 18:52:03 +0000

    added ACE_LACKS_LONGLONG_T support

--- TAO/tao/DynUnion_i_T.cpp
+++ TAO/tao/DynUnion_i_T.cpp
@@ -18,7 +18,7 @@
 
 #include "tao/DynUnion_i_T.h"
 
-// Method of template class that serves as a functor for all that 
+// Method of template class that serves as a functor for all that
 // compares Anys constructed from all legal discriminator
 // types - some are specifically defined in DynUnion_i.cpp.
 
@@ -29,7 +29,17 @@ DU_Extractor<Type>::check_match (const CORBA_Any& inside_any,
 {
   inside_any >>= this->member_index_;
   outside_any >>= this->arg_index_;
+
+#if defined (ACE_LACKS_LONGLONG_T)
+  // Oh, this is ugly.  But, it's the only way I can think of
+  // to satisfy instantiations of CORBA::LongLong as well as
+  // all other (native) types.
+  return ! ACE_OS::memcmp (&this->member_index_,
+                           &this->arg_index_,
+                           sizeof (Type));
+#else  /* ! ACE_LACKS_LONGLONG_T */
   return this->member_index_ == this->arg_index_;
+#endif /* ! ACE_LACKS_LONGLONG_T */
 }
 
 #endif /* TAO_DYNUNION_I_T_C */
