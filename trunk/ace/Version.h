8bae8b0bc311e72040d06a04137d213c94859c08
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-05 21:03:27 +0000

    4.5.44

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (5u)
-#define ACE_BETA_VERSION (43u)
-#define ACE_VERSION "4.5.43\0"
+#define ACE_BETA_VERSION (44u)
+#define ACE_VERSION "4.5.44\0"
