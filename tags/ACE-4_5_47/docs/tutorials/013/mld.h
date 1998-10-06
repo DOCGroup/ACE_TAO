4fc7ab9e3d03f8446e89ddda9f6bb14cb5c754f7
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-06 07:55:25 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_47'.

---
+++ tags/ACE-4_5_47/docs/tutorials/013/mld.h
@@ -0,0 +1,44 @@
+
+// $Id$
+
+#ifndef MLD_H
+#define MLD_H
+
+#include "ace/Synch.h"
+#include "ace/Singleton.h"
+
+/*
+   This is a cheap memory leak detector.  Each class I want to watch over
+   contains an mld object.  The mld object's ctor increments a global counter
+   while the dtor decrements it.  If the counter is non-zero when the program
+   is ready to exit then there may be a leak. 
+ */
+
+class mld
+{
+public:
+  mld (void);
+   ~mld (void);
+
+  static int value (void);
+
+protected:
+  static ACE_Atomic_Op < ACE_Mutex, int >counter_;
+};
+
+// ================================================
+
+/*
+   Just drop 'MLD' anywhere in your class definition to get cheap memory leak
+   detection for your class. 
+ */
+#define MLD            mld mld_
+
+/*
+   Use 'MLD_COUNTER' in main() to see if things are OK. 
+ */
+#define MLD_COUNTER    mld::value()
+
+// ================================================
+
+#endif
