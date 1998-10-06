9d77aadb9f8dd4f5a3d84b87c6c35d245ced2ffa
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-06 05:13:07 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-0_2_18'.

---
+++ tags/TAO-0_2_18/TAO/TAO_IDL/be/be_visitor_sequence/buffer_type.cpp
@@ -0,0 +1,154 @@
+//
+// $Id$
+//
+
+// ============================================================================
+//
+// = LIBRARY
+//    TAO IDL
+//
+// = FILENAME
+//    buffer_type.cpp
+//
+// = DESCRIPTION
+//    Visitor generating code for the internal buffer type of the Sequence node
+//
+// = AUTHOR
+//    Aniruddha Gokhale
+//
+// ============================================================================
+
+#include	"idl.h"
+#include	"idl_extern.h"
+#include	"be.h"
+
+#include "be_visitor_sequence.h"
+
+ACE_RCSID(be_visitor_sequence, buffer_type, "$Id$")
+
+
+// ****************************************************************
+// We have to generate the buffer type in the constructor
+// ****************************************************************
+
+be_visitor_sequence_buffer_type::
+be_visitor_sequence_buffer_type (be_visitor_context *ctx)
+  : be_visitor_decl (ctx)
+{
+}
+
+be_visitor_sequence_buffer_type::~be_visitor_sequence_buffer_type (void)
+{
+}
+
+// helper
+int
+be_visitor_sequence_buffer_type::visit_node (be_type *node)
+{
+  TAO_OutStream *os = this->ctx_->stream ();
+  be_type *bt;
+
+  if (this->ctx_->alias ())
+    bt = this->ctx_->alias ();
+  else
+    bt = node;
+
+  if (this->ctx_->state () == TAO_CodeGen::TAO_SEQUENCE_BUFFER_TYPE_CH)
+    *os << bt->nested_type_name (this->ctx_->scope ());
+  else
+    *os << bt->name ();
+  return 0;
+}
+
+int
+be_visitor_sequence_buffer_type::visit_predefined_type (be_predefined_type *node)
+{
+  TAO_OutStream *os = this->ctx_->stream ();
+  if (node->pt () == AST_PredefinedType::PT_pseudo)
+    *os << node->name () << "_ptr";
+  else
+    *os << node->name ();
+  return 0;
+}
+
+int
+be_visitor_sequence_buffer_type::visit_sequence (be_sequence *node)
+{
+  return this->visit_node (node);
+}
+
+int
+be_visitor_sequence_buffer_type::visit_interface (be_interface *node)
+{
+  TAO_OutStream *os = this->ctx_->stream ();
+  if (this->ctx_->state () == TAO_CodeGen::TAO_SEQUENCE_BUFFER_TYPE_CH)
+    *os << node->nested_type_name (this->ctx_->scope (), "_ptr");
+  else
+    *os << node->name () << "_ptr";
+  return 0;
+}
+
+int
+be_visitor_sequence_buffer_type::visit_interface_fwd (be_interface_fwd *node)
+{
+  TAO_OutStream *os = this->ctx_->stream ();
+  if (this->ctx_->state () == TAO_CodeGen::TAO_SEQUENCE_BUFFER_TYPE_CH)
+    *os << node->nested_type_name (this->ctx_->scope (), "_ptr");
+  else
+    *os << node->name () << "_ptr";
+  return 0;
+}
+
+int
+be_visitor_sequence_buffer_type::visit_string (be_string *)
+{
+  TAO_OutStream *os = this->ctx_->stream ();
+  *os << "char *";
+  return 0;
+}
+
+int
+be_visitor_sequence_buffer_type::visit_structure (be_structure *node)
+{
+  return this->visit_node (node);
+}
+
+int
+be_visitor_sequence_buffer_type::visit_union (be_union *node)
+{
+  return this->visit_node (node);
+}
+
+int
+be_visitor_sequence_buffer_type::visit_enum (be_enum *node)
+{
+  return this->visit_node (node);
+}
+
+int
+be_visitor_sequence_buffer_type::visit_exception (be_exception *node)
+{
+  return this->visit_node (node);
+}
+
+int
+be_visitor_sequence_buffer_type::visit_array (be_array *node)
+{
+  return this->visit_node (node);
+}
+
+int
+be_visitor_sequence_buffer_type::visit_typedef (be_typedef *node)
+{
+  this->ctx_->alias (node); // set the alias node
+  if (node->primitive_base_type ()->accept (this) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "be_visitor_sequence_buffer_type::"
+                         "visit_typedef - "
+                         "accept on primitive type failed\n"),
+                        -1);
+    }
+  this->ctx_->alias (0);
+  return 0;
+}
---
+++ tags/TAO-0_2_18/TAO/examples/Event_Comm/Supplier_Input_Handler.cpp
@@ -0,0 +1,138 @@
+// $Id$
+
+#include "Event_Comm_i.h"
+#include "Notifier_Handler.h"
+#include "Supplier_Input_Handler.h"
+
+ACE_RCSID(Supplier, Supplier_Input_Handler, "$Id$")
+
+int
+Supplier_Input_Handler::handle_close (ACE_HANDLE, ACE_Reactor_Mask)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              "closing down Supplier::Supplier_Input_Handler\n"));
+
+  Event_Comm::Notifier *notifier = this->notifier_->notifier ();
+  ACE_ASSERT (notifier != 0);
+
+  ACE_OS::fclose (this->fp_);
+
+  TAO_TRY
+    {
+      // Disconnect all the consumers gracefully.
+      notifier->disconnect ("quit", TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Error:Supplier_Input_Handler::handle_close\n ");
+    }
+  TAO_ENDTRY;
+
+  // Don't execute a callback here otherwise we'll recurse
+  // indefinitely!
+  if (ACE_Reactor::instance ()->remove_handler
+      (this,
+       ACE_Event_Handler::READ_MASK | ACE_Event_Handler::DONT_CALL) == -1)
+    ACE_ERROR ((LM_ERROR,
+                "%p\n",
+                "remove_handler"));
+
+  // *Must* be allocated dyanmically!
+  ::operator delete ((void *) this);
+  return 0;
+}
+
+Supplier_Input_Handler::Supplier_Input_Handler (Notifier_Handler *notifier,
+			      ACE_HANDLE handle) // Use stdin by default.
+  : notifier_ (notifier),
+    handle_ (handle)
+{
+  // Register ourselves with the ACE_Reactor so that input events
+  // cause our handle_input() method to be dispatched automatically.
+
+  if (ACE_Reactor::instance ()->register_handler
+      (this, ACE_Event_Handler::READ_MASK) == -1)
+    ACE_ERROR ((LM_ERROR,
+                "%p\n",
+                "register_handler"));
+
+  this->fp_ = ACE_OS::fdopen (handle, "r");
+
+  if (this->fp_ == 0)
+    ACE_ERROR ((LM_ERROR,
+                "%p\n",
+                "fdopen"));
+}
+
+Supplier_Input_Handler::~Supplier_Input_Handler (void)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              "closing down Supplier_Input_Handler::~Supplier_Input_Handler\n"));
+  this->handle_close ();
+}
+
+ACE_HANDLE
+Supplier_Input_Handler::get_handle (void) const
+{
+  return this->handle_;
+}
+
+// Frame input events and notify <Consumers>.
+
+int
+Supplier_Input_Handler::handle_input (ACE_HANDLE h)
+{
+  char buf[BUFSIZ];
+
+  // Read up to BUFSIZ worth of data from ACE_HANDLE h.
+
+  if (ACE_OS::fgets (buf, sizeof buf - 1, this->fp_) == 0)
+    {
+      ACE_OS::strcpy (buf, "quit");
+      ACE_DEBUG ((LM_DEBUG,
+                  "shutting down Supplier_Input_Handler\n"));
+    }
+  else
+    {
+      size_t n = ACE_OS::strlen (buf);
+
+      // Null terminate the buffer, replacing the '\n' with '\0'.
+      if (buf[n - 1] == '\n' || buf[n - 1] == EOF)
+	buf[n - 1] = '\0';
+      else
+	buf[n] = '\0';
+      ACE_DEBUG ((LM_DEBUG, "notifying for event %s\n", buf));
+    }
+
+  Event_Comm::Notifier *notifier = this->notifier_->notifier ();
+  ACE_ASSERT (notifier != 0);
+
+  if (ACE_OS::strcmp (buf, "quit") == 0)
+    // Tell the main event loop to shutdown.
+    ACE_Reactor::end_event_loop();
+  else
+    {
+      // Use the notifier to notify Consumers.
+      TAO_TRY
+        {
+          Event_Comm::Event event;
+
+          // Pass the buf over in the tag field.
+          event.tag_ = ACE_OS::strdup (buf);
+
+          // This is where the "any" value goes or the object
+          // reference...  event.value_ = ...
+
+          // Forward <Event> to all <Consumers>.
+          notifier->push (event, TAO_TRY_ENV);
+	  TAO_CHECK_ENV;
+        }
+      TAO_CATCHANY
+        {
+          TAO_TRY_ENV.print_exception ("Unexpected Error\n");
+        }
+      TAO_ENDTRY;
+    }
+  return 0;
+}
