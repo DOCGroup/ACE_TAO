c2625ce29baaab4811067b77675a5f3443b44ba1
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-06 02:16:35 +0000

    *** empty log message ***

--- TAO/examples/Callback_Quoter/Supplier_i.cpp
+++ TAO/examples/Callback_Quoter/Supplier_i.cpp
@@ -40,6 +40,10 @@ Supplier_i::register_callback (const char *stock_name,
 {
   // Store the client information.
   Consumer_Data consumer_data;
+
+  // necessary to make another copy of the consumer_handler using <_duplicate>
+  // so that we dont lose the consumer object reference after the method
+  // invocation is done.
   consumer_data.consumer_ = Callback_Quoter::Consumer::_duplicate (consumer_handler);
   consumer_data.desired_value_= threshold_value;
 
@@ -69,7 +73,27 @@ void
 Supplier_i::unregister_callback (Callback_Quoter::Consumer_ptr consumer_handler,
                                  CORBA::Environment &TAO_TRY_ENV)
 {
-  // To be done.
+
+
+  for (CONSUMER_MAP::ITERATOR iter = this->consumer_map_.begin ();
+       iter!= this->consumer_map_.end ();
+       iter ++)
+    {
+
+      // the *iter is nothing but the stockname + unbounded set
+      // of consumers+threshold values, i.e a ACE_Hash_Map_Entry.
+
+       CONSUMERS *consumers;
+       Consumer_Data consumer_to_remove;
+       consumer_to_remove.consumer_ = consumer_handle;
+
+       (*iter).consumers->remove (consumer_to_remove);
+       // the remove method will do a find internally using
+       // operator == which will check only the consumer pointers.
+       // If match found it will be removed from the set.
+
+    }
+
 }
 
 // gets the market status and sends the information to
@@ -82,9 +106,9 @@ Supplier_i::market_status (const char *stock_name,
 {
   ACE_DEBUG ((LM_DEBUG,
 	      "Supplier:: The stockname is %s with price %d\n",
-	      stock_name, 
+	      stock_name,
               stock_value));
-  
+
   // Go through the list of <Consumer_Data> to find which registered
   // client wants to be notified.
 
@@ -99,28 +123,28 @@ Supplier_i::market_status (const char *stock_name,
           if (stock_value >= (*iter).desired_value_)
             {
               Callback_Quoter::Info interested_consumer_data;
-              
+
               interested_consumer_data.stock_name = CORBA::string_dup (stock_name);
               interested_consumer_data.value = stock_value;
-              
+
               (*iter).consumer_->push (interested_consumer_data);
             }
         }
     }
 }
 
-void 
+void
 Supplier_i::shutdown (CORBA::Environment &)
 {
   ACE_DEBUG ((LM_DEBUG,
               "\n%s\n",
               "The Callback Quoter server is shutting down"));
-  
+
   // Instruct the ORB to shutdown.
   this->orb_->shutdown ();
 }
 
-int 
+int
 Supplier_i::Consumer_Data::operator== (const Consumer_Data &rhs)
 {
   // The <_is_equivalent> function checks if the _var and _ptr objects
