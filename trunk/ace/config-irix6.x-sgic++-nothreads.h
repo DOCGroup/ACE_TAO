fc719c785ab4ba1dcc9d74f0585edbdc3becd7d9
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-07 23:27:08 +0000

    ChangeLogTag:Wed Oct  7 18:18:14 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/ace/config-irix6.x-sgic++-nothreads.h
+++ trunk/ace/config-irix6.x-sgic++-nothreads.h
@@ -22,22 +22,8 @@
 #define ACE_CONFIG_IRIX6X_NTHR_H
 
 // Include this file to set the _MIPS_SIM*ABI* macros.
-#include <sgidefs.h>
-
-// The Irix 6.x float.h doesn't allow us to distinguish between a
-// double and a long double.  So, we have to hard-code this.  Thanks
-// to Bob Laferriere <laferrie@gsao.med.ge.com> for figuring it out.
-#if defined (_MIPS_SIM)             /* 6.X System */
-# if defined (_MIPS_SIM_NABI32) && (_MIPS_SIM == _MIPS_SIM_NABI32)
-#   define ACE_SIZEOF_LONG_DOUBLE 16
-# elif defined (_MIPS_SIM_ABI32) && (_MIPS_SIM == _MIPS_SIM_ABI32)
-#   define ACE_SIZEOF_LONG_DOUBLE 8
-# elif !defined (ACE_SIZEOF_LONG_DOUBLE)
-#   define ACE_SIZEOF_LONG_DOUBLE 8
-# endif
-#else
-# define ACE_SIZEOF_LONG_DOUBLE 8   /* 5.3 System */
-#endif
+#include /**/ <sgidefs.h>
+#include "ace/config-irix6.x-common.h"
 
 #define ACE_HAS_SGIDLADD
 #define ACE_HAS_P_READ_WRITE
@@ -56,8 +42,6 @@
 #define ACE_DEFAULT_BASE_ADDR ((char *) (1024U * 1024 * 1024))
 
 #define ACE_LACKS_SIGNED_CHAR
-// Platform supports getpagesize() call.
-#define ACE_HAS_GETPAGESIZE
 
 // Platform supports reentrant functions (i.e., all the POSIX *_r
 // functions).
@@ -77,108 +61,12 @@
 // getservbyname_r).
 #define ACE_LACKS_NETDB_REENTRANT_FUNCTIONS
 
-// Platform supports the tid_t type (e.g., AIX and Irix 6.2)
-#define ACE_HAS_TID_T
-
 // uses ctime_r & asctime_r with only two parameters vs. three
 #define ACE_HAS_2_PARAM_ASCTIME_R_AND_CTIME_R
-
-// Platform has no implementation of pthread_condattr_setpshared(),
-// even though it supports pthreads! (like Irix 6.2)
-#define ACE_LACKS_CONDATTR_PSHARED
-#define ACE_LACKS_MUTEXATTR_PSHARED
-
-#define ACE_LACKS_RWLOCK_T
-
-// Platform/compiler has the sigwait(2) prototype
-#define ACE_HAS_SIGWAIT
-
-// Platform supports System V IPC (most versions of UNIX, but not Win32)
-#define ACE_HAS_SYSV_IPC
-
-// Platform requires void * for mmap().
-#define ACE_HAS_VOIDPTR_MMAP
-
-// Platform supports recvmsg and sendmsg.
-#define ACE_HAS_MSG
-
-// Compiler/platform contains the <sys/syscall.h> file.
-#define ACE_HAS_SYSCALL_H
-
-// Compiler/platform supports alloca()
-#define ACE_HAS_ALLOCA
-
-// Compiler/platform has <alloca.h>
-#define ACE_HAS_ALLOCA_H
-
-// Irix needs to define bzero() in this odd file <bstring.h>
-#define ACE_HAS_BSTRING
-
-// Compiler/platform has the getrusage() system call.
-#define ACE_HAS_GETRUSAGE
-
-// Platform supports POSIX O_NONBLOCK semantics.
-#define ACE_HAS_POSIX_NONBLOCK
-
-// Platform supports POSIX timers via timestruc_t.
-#define ACE_HAS_POSIX_TIME
-// #define ACE_HAS_SVR4_TIME
-#define ACE_LACKS_SYSTIME_H
-
-// Compiler/platform has correctly prototyped header files.
-#define ACE_HAS_CPLUSPLUS_HEADERS
-
-// Platform contains <poll.h>.
-#define ACE_HAS_POLL
-
-// No multi-threading so use poll() call
-// - for easier debugging, if nothing else
-// #define ACE_USE_POLL
-
-// Platform supports the /proc file system.
-#define ACE_HAS_PROC_FS
-
-// Compiler/platform defines the sig_atomic_t typedef.
-#define ACE_HAS_SIG_ATOMIC_T
-
-// Platform supports SVR4 extended signals.
-#define ACE_HAS_SIGINFO_T
-#define ACE_HAS_UCONTEXT_T
-
-// Compiler supports the ssize_t typedef.
-#define ACE_HAS_SSIZE_T
-
-// Platform supports STREAMS.
-#define ACE_HAS_STREAMS
-
-// Platform supports STREAM pipes (note that this is disabled by
-// default, see the manual page on pipe(2) to find out how to enable
-// it).
-// #define ACE_HAS_STREAM_PIPES
-
-// Compiler/platform supports strerror ().
-#define ACE_HAS_STRERROR
-
-// Compiler/platform supports struct strbuf.
-#define ACE_HAS_STRBUF_T
-
-// Compiler/platform supports SVR4 dynamic linking semantics.
-#define ACE_HAS_SVR4_DYNAMIC_LINKING
 
 // Prototypes for both signal() and struct sigaction are consistent.
 #define ACE_HAS_CONSISTENT_SIGNAL_PROTOTYPES
 
-// Platform provides <sys/filio.h> header.
-#define ACE_HAS_SYS_FILIO_H
-
-// Platform has the xti header in <sys/xti.h>.
-#define ACE_HAS_SYS_XTI_H
-
-// Compiler/platform defines a union semun for SysV shared memory.
-#define ACE_HAS_SEMUN
-
-// Platform supports IP multicast
-#define ACE_HAS_IP_MULTICAST
 
 // The following three should be enabled/disabled together.
 #if _COMPILER_VERSION >= 720
@@ -200,18 +88,7 @@
 // it).
 // #define ACE_HAS_STREAM_PIPES
 
-// Platform supports POSIX timers via timestruc_t.
-// This will *NOT* work in 6.4, but ACE doesn't use it anymore.
-// #define ACE_HAS_POSIX_TIME
-// #define ACE_HAS_SVR4_TIME
-
-// Turns off the tracing feature.
-#if !defined (ACE_NTRACE)
-#define ACE_NTRACE 1
-#endif /* ACE_NTRACE */
-
 #define ACE_HAS_GPERF
-#define ACE_HAS_DIRENT
 
 #if defined (_COMPILER_VERSION)
 #define ACE_CC_NAME "SGI/MIPSPro"
