dd86264be0e885ce030ad42dea95f5466ea76fff
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-09 21:31:47 +0000

    ChangeLogTag:Fri Oct  9 16:27:46 1998  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/orbsvcs/orbsvcs/Event/Memory_Pools.i
+++ TAO/orbsvcs/orbsvcs/Event/Memory_Pools.i
@@ -24,31 +24,3 @@ ACE_ES_Memory_Pools::delete_Dispatch_Request (void *mem)
 {
   Dispatch_Request_::instance ()->free (mem);
 }
-
-#define USE_MEM_POOLS 1
-
-ACE_INLINE void *
-ACE_ES_Memory_Pools::new_Event (size_t len)
-{
-#if USE_MEM_POOLS
-  const u_int ev_size = sizeof (RtecEventComm::Event);
-  const u_int size = (ev_size % ACE_MALLOC_ALIGN) ?
-      ((ev_size / ACE_MALLOC_ALIGN) + 1) * ACE_MALLOC_ALIGN : ev_size;
-
-  char *const addr = (char *) Event_::instance ()->malloc (len * size);
-#else
-  char *const addr = new char[len * sizeof (ACE_ES_Event)];
-#endif
-
-  return addr;
-}
-
-ACE_INLINE void
-ACE_ES_Memory_Pools::delete_Event (void *mem)
-{
-#if USE_MEM_POOLS
-  Event_::instance ()->free (mem);
-#else
-  delete [] mem;
-#endif
-}
--- TAO/orbsvcs/tests/EC_Throughput/ECT_Supplier.cpp
+++ TAO/orbsvcs/tests/EC_Throughput/ECT_Supplier.cpp
@@ -171,71 +171,12 @@ ECTS_Driver::run (int argc, char* argv[])
 
       ACE_DEBUG ((LM_DEBUG, "suppliers finished\n"));
 
+      this->dump_results ();
+
       this->disconnect_suppliers (TAO_TRY_ENV);
       TAO_CHECK_ENV;
 
       // @@ Deactivate the suppliers (as CORBA Objects?)
-    }
-  TAO_CATCH (CORBA::SystemException, sys_ex)
-    {
-      TAO_TRY_ENV.print_exception ("SYS_EX");
-    }
-  TAO_CATCHANY
-    {
-      TAO_TRY_ENV.print_exception ("NON SYS EX");
-    }
-  TAO_ENDTRY;
-  return 0;
-}
-
-int
-ECTS_Driver::supplier_task (Test_Supplier *supplier,
-                            void* cookie)
-{
-  TAO_TRY
-    {
-      ACE_Time_Value tv (0, this->burst_pause_);
-
-      const ACE_Message_Block mb (this->event_size_);
-
-      RtecEventComm::EventSet event (1);
-      event.length (1);
-      event[0].header.source = supplier->supplier_id ();
-      event[0].header.ttl = 1;
-
-      ACE_hrtime_t t = ACE_OS::gethrtime ();
-      ORBSVCS_Time::hrtime_to_TimeT (event[0].header.creation_time, t);
-      event[0].header.ec_recv_time = ORBSVCS_Time::zero;
-      event[0].header.ec_send_time = ORBSVCS_Time::zero;
-
-      event[0].data.x = 0;
-      event[0].data.y = 0;
-
-      // We use replace to minimize the copies, this should result
-      // in just one memory allocation;
-      event[0].data.payload.replace (this->event_size_,
-                                     &mb);
-
-      for (int i = 0; i < this->burst_count_; ++i)
-        {
-          for (int j = 0; j < this->burst_size_; ++j)
-            {
-              if (j % 2 == 0)
-                event[0].header.type = this->event_a_;
-              else
-                event[0].header.type = this->event_b_;
-
-              // ACE_DEBUG ((LM_DEBUG, "(%t) supplier push event\n"));
-              supplier->consumer_proxy ()->push(event, TAO_TRY_ENV);
-            }
-          ACE_OS::sleep (tv);
-        }
-
-      // Send one event shutdown from each supplier
-      event[0].header.type = ACE_ES_EVENT_SHUTDOWN;
-      supplier->consumer_proxy ()->push(event, TAO_TRY_ENV);
-      TAO_CHECK_ENV;
-      
     }
   TAO_CATCH (CORBA::SystemException, sys_ex)
     {
@@ -261,9 +202,12 @@ ECTS_Driver::connect_suppliers (RtecEventChannelAdmin::EventChannel_ptr channel,
       ACE_NEW (this->suppliers_[i], Test_Supplier (this));
 
       this->suppliers_[i]->connect (buf,
+                                    this->burst_count_,
+                                    this->burst_size_,
+                                    this->event_size_,
+                                    this->burst_pause_,
                                     this->event_a_,
                                     this->event_b_,
-                                    this->burst_pause_,
                                     channel,
                                     _env);
       if (_env.exception () != 0) return;
@@ -286,6 +230,18 @@ ECTS_Driver::disconnect_suppliers (CORBA::Environment &_env)
     {
       this->suppliers_[i]->disconnect (_env);
       if (_env.exception () != 0) return;
+    }
+}
+
+void
+ECTS_Driver::dump_results (void)
+{
+  for (int i = 0; i < this->n_suppliers_; ++i)
+    {
+      char buf[BUFSIZ];
+      ACE_OS::sprintf (buf, "supplier_%02.2d", i);
+
+      this->suppliers_[i]->dump_results (buf);
     }
 }
 
@@ -398,86 +354,91 @@ ECTS_Driver::parse_args (int argc, char *argv [])
 
 Test_Supplier::Test_Supplier (ECTS_Driver *driver)
   :  driver_ (driver),
-     supplier_ (this)
+     supplier_ (this),
+     burst_count_ (0),
+     burst_size_ (0),
+     event_size_ (0),
+     burst_pause_ (0)
 {
 }
 
 void
 Test_Supplier::connect (const char* name,
+                        int burst_count,
+                        int burst_size,
+                        int event_size,
+                        int burst_pause,
                         int event_a,
                         int event_b,
-                        int event_period,
                         RtecEventChannelAdmin::EventChannel_ptr ec,
                         CORBA::Environment &_env)
 {
-  TAO_TRY
-    {
-      RtecScheduler::Scheduler_ptr server =
-        ACE_Scheduler_Factory::server ();
+  this->burst_count_ = burst_count;
+  this->burst_size_ = burst_size;
+  this->event_size_ = event_size;
+  this->burst_pause_ = burst_pause;
+  this->event_a_ = event_a;
+  this->event_b_ = event_b;
+    
+  RtecScheduler::Scheduler_ptr server =
+    ACE_Scheduler_Factory::server ();
 
-      RtecScheduler::handle_t rt_info =
-        server->create (name, TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+  RtecScheduler::handle_t rt_info =
+    server->create (name, _env);
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 
-      ACE_Time_Value tv (0, event_period);
-      RtecScheduler::Period rate = tv.usec () * 10;
+  ACE_Time_Value tv (0, burst_pause);
+  RtecScheduler::Period rate = tv.usec () * 10;
 
-      // The execution times are set to reasonable values, but
-      // actually they are changed on the real execution, i.e. we
-      // lie to the scheduler to obtain right priorities; but we
-      // don't care if the set is schedulable.
-      tv.set (0, 2000);
-      TimeBase::TimeT time;
-      ORBSVCS_Time::Time_Value_to_TimeT (time, tv);
-      server->set (rt_info,
-                   RtecScheduler::VERY_HIGH_CRITICALITY,
-                   time, time, time,
-                   rate,
-                   RtecScheduler::VERY_LOW_IMPORTANCE,
-                   time,
-                   1,
-                   RtecScheduler::OPERATION,
-                   TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+  // The execution times are set to reasonable values, but
+  // actually they are changed on the real execution, i.e. we
+  // lie to the scheduler to obtain right priorities; but we
+  // don't care if the set is schedulable.
+  tv.set (0, 2000);
+  TimeBase::TimeT time;
+  ORBSVCS_Time::Time_Value_to_TimeT (time, tv);
+  server->set (rt_info,
+               RtecScheduler::VERY_HIGH_CRITICALITY,
+               time, time, time,
+               rate,
+               RtecScheduler::VERY_LOW_IMPORTANCE,
+               time,
+               1,
+               RtecScheduler::OPERATION,
+               _env);
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 
-      this->supplier_id_ = ACE::crc32 (name);
-      ACE_DEBUG ((LM_DEBUG, "ID for <%s> is %04.4x\n", name,
-                  this->supplier_id_));
+  this->supplier_id_ = ACE::crc32 (name);
+  ACE_DEBUG ((LM_DEBUG, "ID for <%s> is %04.4x\n", name,
+              this->supplier_id_));
 
-      ACE_SupplierQOS_Factory qos;
-      qos.insert (this->supplier_id_,
-                  event_a,
-                  rt_info, 1);
-      qos.insert (this->supplier_id_,
-                  event_b,
-                  rt_info, 1);
-      qos.insert (this->supplier_id_,
-                  ACE_ES_EVENT_SHUTDOWN,
-                  rt_info, 1);
+  ACE_SupplierQOS_Factory qos;
+  qos.insert (this->supplier_id_,
+              event_a,
+              rt_info, 1);
+  qos.insert (this->supplier_id_,
+              event_b,
+              rt_info, 1);
+  qos.insert (this->supplier_id_,
+              ACE_ES_EVENT_SHUTDOWN,
+              rt_info, 1);
 
-      RtecEventChannelAdmin::SupplierAdmin_var supplier_admin =
-        ec->for_suppliers (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+  RtecEventChannelAdmin::SupplierAdmin_var supplier_admin =
+    ec->for_suppliers (_env);
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 
-      this->consumer_proxy_ =
-        supplier_admin->obtain_push_consumer (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+  this->consumer_proxy_ =
+    supplier_admin->obtain_push_consumer (_env);
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 
-      RtecEventComm::PushSupplier_var objref =
-        this->supplier_._this (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+  RtecEventComm::PushSupplier_var objref =
+    this->supplier_._this (_env);
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 
-      this->consumer_proxy_->connect_push_supplier (objref.in (),
-                                                    qos.get_SupplierQOS (),
-                                                    TAO_TRY_ENV);
-      TAO_CHECK_ENV;
-
-    }
-  TAO_CATCHANY
-    {
-      TAO_RETHROW;
-    }
-  TAO_ENDTRY;
+  this->consumer_proxy_->connect_push_supplier (objref.in (),
+                                                qos.get_SupplierQOS (),
+                                                _env);
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 }
 
 void
@@ -487,15 +448,72 @@ Test_Supplier::disconnect (CORBA::Environment &_env)
     return;
 
   this->consumer_proxy_->disconnect_push_consumer (_env);
-  if (_env.exception () != 0) return;
+  TAO_CHECK_ENV_RETURN_VOID (_env);
 
-  this->consumer_proxy_ = 0;
+  this->consumer_proxy_ =
+    RtecEventChannelAdmin::ProxyPushConsumer::_nil ();
 }
 
 int
 Test_Supplier::svc ()
 {
-  return this->driver_->supplier_task (this, this->cookie_);
+  TAO_TRY
+    {
+      ACE_Time_Value tv (0, this->burst_pause_);
+
+      const ACE_Message_Block mb (this->event_size_);
+
+      RtecEventComm::EventSet event (1);
+      event.length (1);
+      event[0].header.source = this->supplier_id ();
+      event[0].header.ttl = 1;
+
+      ACE_hrtime_t t = ACE_OS::gethrtime ();
+      ORBSVCS_Time::hrtime_to_TimeT (event[0].header.creation_time, t);
+      event[0].header.ec_recv_time = ORBSVCS_Time::zero;
+      event[0].header.ec_send_time = ORBSVCS_Time::zero;
+
+      event[0].data.x = 0;
+      event[0].data.y = 0;
+
+      // We use replace to minimize the copies, this should result
+      // in just one memory allocation;
+      event[0].data.payload.replace (this->event_size_,
+                                     &mb);
+
+      this->timer_.start ();
+      for (int i = 0; i < this->burst_count_; ++i)
+        {
+          for (int j = 0; j < this->burst_size_; ++j)
+            {
+              if (j % 2 == 0)
+                event[0].header.type = this->event_a_;
+              else
+                event[0].header.type = this->event_b_;
+
+              // ACE_DEBUG ((LM_DEBUG, "(%t) supplier push event\n"));
+              this->consumer_proxy ()->push(event, TAO_TRY_ENV);
+            }
+          ACE_OS::sleep (tv);
+        }
+
+      // Send one event shutdown from each supplier
+      event[0].header.type = ACE_ES_EVENT_SHUTDOWN;
+      this->consumer_proxy ()->push(event, TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      this->timer_.stop ();
+      
+    }
+  TAO_CATCH (CORBA::SystemException, sys_ex)
+    {
+      TAO_TRY_ENV.print_exception ("SYS_EX");
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("NON SYS EX");
+    }
+  TAO_ENDTRY;
+  return 0;
 }
 
 void
@@ -512,6 +530,27 @@ RtecEventChannelAdmin::ProxyPushConsumer_ptr
 Test_Supplier::consumer_proxy (void)
 {
   return this->consumer_proxy_.in ();
+}
+
+void
+Test_Supplier::dump_results (const char* name)
+{
+  ACE_Time_Value tv;
+  this->timer_.elapsed_time (tv);
+
+  int event_count = this->burst_count_ * this->burst_size_ + 1;
+  double f = 1.0 / (tv.sec () + tv.usec () / 1000000.0);
+  double eps = event_count * f;
+  
+  ACE_DEBUG ((LM_DEBUG,
+              "ECT_Consumer (%s):\n"
+              "    Total time: %d.%08.8d (secs.usecs)\n"
+              "    Total events: %d\n"
+              "    Events per second: %.3f\n",
+              name,
+              tv.sec (), tv.usec (),
+              event_count,
+              eps));
 }
 
 // ****************************************************************
