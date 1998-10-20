2ae14b2b85ad813a269e8a5529aa76cf984fa8a5
jcej <jcej@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-20 02:05:16 +0000

    *** empty log message ***

--- docs/tutorials/015/Crypt.h
+++ docs/tutorials/015/Crypt.h
@@ -6,25 +6,30 @@
 
 #include "Protocol_Task.h"
 
-class ACE_SOCK_Stream;
-
+/* An interface (adaptor) between your favorite encryption method and
+   an ACE_Stream.
+*/
 class Crypt : public Protocol_Task
 {
 public:
 
     typedef Protocol_Task inherited;
-    
+
+        // Again we have the option of multiple threads and again I
+        // regret tempting folks to use it.
     Crypt( int _thr_count = 0 );
+    
     ~Crypt(void);
 
 protected:
 
+        // Moving downstream will encrypt the data
      int send(ACE_Message_Block *message,
               ACE_Time_Value *timeout);
 
+        // And moving upstream will decrypt it.
      int recv(ACE_Message_Block *message,
               ACE_Time_Value *timeout);
-private:
 };
 
 #endif // CRYPT_H
