e2aa47539f724f3c98b2cb9e7022f8f31b1fa459
gokhale <gokhale@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-21 19:02:15 +0000

    *** empty log message ***

--- trunk/TAO/TAO_IDL/be/be_visitor_array/any_op_cs.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_array/any_op_cs.cpp
@@ -109,7 +109,8 @@ be_visitor_array_any_op_cs::visit_array (be_array *node)
       << node->name () << "_free (_tao_elem._retn ());" << be_nl
       << "return 0;" << be_uidt_nl
       << "}" << be_nl
-      << "TAO_ENDTRY;" << be_uidt_nl
+      << "TAO_ENDTRY;" << be_nl
+      << "return 0;" << be_uidt_nl
       << "}\n\n";
 
   node->cli_stub_any_op_gen (1);
