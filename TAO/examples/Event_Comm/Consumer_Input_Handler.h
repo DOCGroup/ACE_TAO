905ae47d26d4f283b7d04baf17821f18048b2ad0
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-23 20:59:38 +0000

    *** empty log message ***

--- TAO/examples/Event_Comm/Consumer_Input_Handler.h
+++ TAO/examples/Event_Comm/Consumer_Input_Handler.h
@@ -32,17 +32,21 @@ class Consumer_Input_Handler : public ACE_Service_Object
   //   Handles input events generated from a keyboard.
   //
   // = DESCRIPTION
-  //    This class holds a <Consumer_Handler> object. It accepts input from the user
-  //    and passes events to the <Notifier>. It also initiates shutdown.
-  //
+  //    This class holds a <Consumer_Handler> object. It accepts input
+  //    from the user and passes events to the <Notifier>. It also
+  //    initiates shutdown.
 public:
   // Initialization and termination methods.
   Consumer_Input_Handler(void);
+  // Constructor.
 
   ~Consumer_Input_Handler (void);
+  // Destructor.
 
   int initialize (Consumer_Handler *,
-                          ACE_HANDLE h = 0);
+		  ACE_HANDLE h = 0);
+  // registers the input event handler.
+  // returns 0 on success, -1 on error.
 
   virtual int handle_input (ACE_HANDLE);
   // Dispatch the callback when events occur.
@@ -59,6 +63,7 @@ public:
 
 private:
   virtual ACE_HANDLE get_handle (void) const;
+  // Obtain the input HANDLE.
 
   ACE_HANDLE handle_;
   // ACE_HANDLE where the input comes from.
