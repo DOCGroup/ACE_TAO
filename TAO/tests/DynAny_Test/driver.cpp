325d31d380c6906bb5c4910d03866985d041d7be
jp4 <jp4@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-23 21:04:16 +0000

    *** empty log message ***

--- TAO/tests/DynAny_Test/driver.cpp
+++ TAO/tests/DynAny_Test/driver.cpp
@@ -1,3 +1,4 @@
+// -*- c++ -*-
 // $Id$
 
 // ============================================================================
@@ -8,50 +9,209 @@
 // = FILENAME
 //    driver.cpp
 //
+// = DESCRIPTION
+//    Implementation file for the driver program.
+//
 // = AUTHOR
 //    Jeff Parsons <jp4@cs.wustl.edu>
 //
 // ============================================================================
 
-#include "DynAny_i.h"
-#include "DynStruct_i.h"
-#include "DynSequence_i.h"
-#include "DynArray_i.h"
-#include "DynEnum_i.h"
-#include "DynUnion_i.h"
-#include "da_testsC.cpp"
+#include "ace/Get_Opt.h"
+#include "test_dynany.h"
+#include "test_dynarray.h"
+#include "test_dynenum.h"
+#include "test_dynsequence.h"
+#include "test_dynstruct.h"
+#include "test_dynunion.h"
+#include "test_wrapper.h"
+#include "driver.h"
 
 int main (int argc, char* argv[])
 {
-  CORBA::Environment env;
-  CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0, env);
+  Driver driver;
+  
+  // initialize the driver
+  if (driver.init (argc, argv) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "(%N:%l) driver.cpp - "
+                       "Driver initialization failed\n"),
+                      -1);
 
-  DynAnyTests::test_union* tu;
+  // run the tests
+  if (driver.run () == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "(%N:%l) driver.cpp - "
+                       "tests failed\n"),
+                      -1);
+  return 0;
+}
 
-  tu->_d (DynAnyTests::SECOND);
+// constructor
+Driver::Driver (void)
+  : orb_ (0),
+    test_type_ (NO_TEST)
+{
+}
 
-//  DynAnyTests::enclosed_struct es_in;
-//  tu.ts ().l = -222;
-//  const char* val = "boing";
-//  tu->str (val);
+// destructor
+Driver::~Driver (void)
+{
+}
 
-  tu->tc (CORBA::_tc_double);
+// initialize the driver
+int
+Driver::init (int argc, char* argv[])
+{
+  TAO_TRY
+    {
+      // Retrieve the underlying ORB
+      this->orb_ = CORBA::ORB_init (argc,
+                                    argv,
+                                    "local",
+                                    TAO_TRY_ENV);
 
-  CORBA_Any a;
-  a <<= tu;
+      TAO_CHECK_ENV;
 
-//  DynUnion_i du (DynAnyTests::_tc_test_union);
-  DynUnion_i du (a);
+      // Parse command line and verify parameters.
+      if (this->parse_args (argc, argv) == -1)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                          "(%N:%l) driver.cpp - "
+                          "parse_args failed\n"),
+                          -1);
 
-//  du.from_any (a, env);
-
-  CORBA_Any* a_out = du.to_any (env);
-
-  DynAnyTests::test_union* tu_out;
-//  DynAnyTests::test_enum te_out;
-  *a_out >>= tu_out;
-
-  du.destroy (env);
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Driver::init");
+      return -1;
+    }
+  TAO_ENDTRY;
 
   return 0;
 }
+
+int
+Driver::parse_args (int argc, char* argv[])
+{
+  if (argc == 1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "usage:  %s"
+                       " -t [dynany|dynarray|dynenum|dynsequence|dynstruct|dynunion]"
+                       "\n",
+                       argv [0]),
+                      -1);
+
+  ACE_Get_Opt get_opts (argc, argv, "t:");
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 't':
+        if (!ACE_OS::strcmp (get_opts.optarg, "dynany"))
+          this->test_type_ = TEST_DYNANY;
+        else if (!ACE_OS::strcmp (get_opts.optarg, "dynarray"))
+          this->test_type_ = TEST_DYNARRAY;
+        else if (!ACE_OS::strcmp (get_opts.optarg, "dynenum"))
+          this->test_type_ = TEST_DYNENUM;
+        else if (!ACE_OS::strcmp (get_opts.optarg, "dynsequence"))
+          this->test_type_ = TEST_DYNSEQUENCE;
+        else if (!ACE_OS::strcmp (get_opts.optarg, "dynstruct"))
+          this->test_type_ = TEST_DYNSTRUCT;
+        else if (!ACE_OS::strcmp (get_opts.optarg, "dynunion"))
+          this->test_type_ = TEST_DYNUNION;
+        break;
+
+      case '?':
+      default:
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage:  %s"
+                           " -t [dynany|dynarray|dynenum|dynsequence|dynstruct|dynunion]"
+                           "\n",
+                           argv [0]),
+                          -1);
+      }
+
+  // Indicates successful parsing of command line.        
+  return 0;
+}
+
+int
+Driver::run (void)
+{
+  int retstatus;
+
+  switch (this->test_type_)
+    {
+      case TEST_DYNANY:
+        {
+          Test_Wrapper<Test_DynAny>* wrapper = 
+            new Test_Wrapper<Test_DynAny> (new Test_DynAny (this->orb_));
+          retstatus = wrapper->run_test ();
+          delete wrapper;
+        }
+        break;
+      case TEST_DYNARRAY:
+        {
+          Test_Wrapper<Test_DynArray>* wrapper = 
+            new Test_Wrapper<Test_DynArray> (new Test_DynArray (this->orb_));
+          retstatus = wrapper->run_test ();
+          delete wrapper;
+        }
+        break;
+      case TEST_DYNENUM:
+        {
+          Test_Wrapper<Test_DynEnum>* wrapper = 
+            new Test_Wrapper<Test_DynEnum> (new Test_DynEnum (this->orb_));
+          retstatus = wrapper->run_test ();
+          delete wrapper;
+        }
+        break;
+      case TEST_DYNSEQUENCE:
+        {
+          Test_Wrapper<Test_DynSequence>* wrapper = 
+            new Test_Wrapper<Test_DynSequence> (new Test_DynSequence (this->orb_));
+          retstatus = wrapper->run_test ();
+          delete wrapper;
+        }
+        break;
+      case TEST_DYNSTRUCT:
+        {
+          Test_Wrapper<Test_DynStruct>* wrapper = 
+            new Test_Wrapper<Test_DynStruct> (new Test_DynStruct (this->orb_));
+          retstatus = wrapper->run_test ();
+          delete wrapper;
+        }
+        break;
+      case TEST_DYNUNION:
+        {
+          Test_Wrapper<Test_DynUnion>* wrapper = 
+            new Test_Wrapper<Test_DynUnion> (new Test_DynUnion (this->orb_));
+          retstatus = wrapper->run_test ();
+          delete wrapper;
+        }
+        break;
+      default:
+        break;
+    }
+
+  return retstatus;
+}
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class Test_Wrapper<Test_DynAny>;
+template class Test_Wrapper<Test_DynArray>;
+template class Test_Wrapper<Test_DynEnum>;
+template class Test_Wrapper<Test_DynSequence>;
+template class Test_Wrapper<Test_DynStruct>;
+template class Test_Wrapper<Test_DynUnion>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate Test_Wrapper<Test_DynAny>
+#pragma instantiate Test_Wrapper<Test_DynArray>
+#pragma instantiate Test_Wrapper<Test_DynEnum>
+#pragma instantiate Test_Wrapper<Test_DynSequence>
+#pragma instantiate Test_Wrapper<Test_DynStruct>
+#pragma instantiate Test_Wrapper<Test_DynUnion>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
---
+++ TAO/tests/DynAny_Test/test_dynunion.cpp
@@ -0,0 +1,145 @@
+// -*- c++ -*-
+// $Id$
+// ============================================================================
+//
+// = LIBRARY
+//    TAO/tests/DynAny_Test
+//
+// = FILENAME
+//    test_dynunion.cpp
+//
+// = DESCRIPTION
+//    Implementation of the simple DynUnion test
+//
+// = AUTHOR
+//    Jeff Parsons <jp4@cs.wustl.edu>
+//
+// ============================================================================
+
+#include "tao/corba.h"
+#include "tao/DynUnion_i.h"
+#include "test_dynunion.h"
+#include "da_testsC.h"
+#include "data.h"
+
+Test_DynUnion::Test_DynUnion (CORBA::ORB_var orb)
+  : test_name_ (CORBA::string_dup ("test_dynunion")),
+    orb_ (orb),
+    error_count_ (0)
+{
+}
+
+Test_DynUnion::~Test_DynUnion (void)
+{
+  CORBA::string_free (this->test_name_);
+  this->test_name_ = 0;
+}
+
+const char*
+Test_DynUnion::test_name (void) const
+{ 
+  return this->test_name_;
+}
+
+int
+Test_DynUnion::run_test (void)
+{
+  Data data (this->orb_);
+
+  DynAnyTests::test_union tu;
+
+  TAO_TRY
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                 "\t*=*=*=*= %s =*=*=*=*\n",
+                 data.labels[3]));
+
+      ACE_DEBUG ((LM_DEBUG,
+                 "testing: constructor(Any)/insert/get\n"));
+
+      tu._d(DynAnyTests::FIRST);
+      CORBA_Any in_any1;
+      in_any1 <<= tu;
+      TAO_DynUnion_i fa1 (in_any1);
+      fa1.insert_short (data.m_short1,
+                         TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      CORBA::Short s_out1 = fa1.get_short (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      if (s_out1 == data.m_short1)
+        ACE_DEBUG ((LM_DEBUG,
+                   "++ OK ++\n"));
+      else 
+        ++this->error_count_;
+
+      ACE_DEBUG ((LM_DEBUG,
+                 "testing: constructor(TypeCode)/from_any/to_any\n"));
+
+      CORBA_Any* out_any1 = fa1.to_any (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      TAO_DynUnion_i ftc1 (DynAnyTests::_tc_test_union);
+      ftc1.from_any (*out_any1,
+                     TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      CORBA::Short s_out2 = ftc1.get_short (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      if (s_out2 == data.m_short1)
+        ACE_DEBUG ((LM_DEBUG,
+                   "++ OK ++\n"));
+      else 
+        ++this->error_count_;
+
+      ACE_DEBUG ((LM_DEBUG,
+                 "testing: set_as_default\n"));
+
+      if (!ftc1.set_as_default (TAO_TRY_ENV))
+        ACE_DEBUG ((LM_DEBUG,
+                   "++ OK ++\n"));
+      else 
+        ++this->error_count_;
+      TAO_CHECK_ENV;
+
+      ACE_DEBUG ((LM_DEBUG,
+                 "testing:discriminator/discriminator_kind\n"));
+
+      CORBA_DynAny_ptr dp1 = ftc1.discriminator (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      if (dp1->type (TAO_TRY_ENV)->kind () 
+            == ftc1.discriminator_kind (TAO_TRY_ENV))
+        ACE_DEBUG ((LM_DEBUG,
+                   "++ OK ++\n"));
+      else 
+        ++this->error_count_;        
+      TAO_CHECK_ENV;
+
+      ACE_DEBUG ((LM_DEBUG,
+                 "testing:member/member_kind/member_name\n"));
+
+      CORBA_DynAny_ptr dp2 = ftc1.member (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      CORBA::String m_nm = ftc1.member_name (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      CORBA::TCKind tk = ftc1.member_kind (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+      if (!ACE_OS::strcmp (m_nm, "s")         && 
+          tk == CORBA::tk_short               &&
+          dp2->get_short (TAO_TRY_ENV) == data.m_short1)
+        ACE_DEBUG ((LM_DEBUG,
+                   "++ OK ++\n"));
+      else 
+        ++this->error_count_;        
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("test_dynunion::run_test");
+      return -1;
+    }
+  TAO_ENDTRY;
+
+  ACE_DEBUG ((LM_DEBUG,
+              "\n%d errors\n",
+              this->error_count_));
+
+  return 0;
+}
+
