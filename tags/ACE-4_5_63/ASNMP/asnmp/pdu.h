844c5905be974c0745aa2ae44b834f6e5688ab26
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-23 05:29:35 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_5_63'.

---
+++ tags/ACE-4_5_63/ASNMP/asnmp/pdu.h
@@ -0,0 +1,213 @@
+/* -*-C++-*- */
+// $Id$
+#ifndef PDU_CLS_
+#define PDU_CLS_
+// ============================================================================
+//
+// = LIBRARY
+//    asnmp
+//
+// = FILENAME
+//     pdu.h
+//
+// = DESCRIPTION
+// Pdu class definition. Encapsulation of an SMI Protocol
+// Data Unit (PDU) aka Packet in C++.
+//
+// = AUTHOR
+//    Peter E Mellquist  original code
+//    Michael MacFaden  mrm@cisco.com  ACE port, add iterator class for pdus
+// ============================================================================
+/*===================================================================
+  Copyright (c) 1996
+  Hewlett-Packard Company
+
+  ATTENTION: USE OF THIS SOFTWARE IS SUBJECT TO THE FOLLOWING TERMS.
+  Permission to use, copy, modify, distribute and/or sell this software
+  and/or its documentation is hereby granted without fee. User agrees
+  to display the above copyright notice and this license notice in all
+  copies of the software and any documentation of the software. User
+  agrees to assume all liability for the use of the software; Hewlett-Packard
+  makes no representations about the suitability of this software for any
+  purpose. It is provided "AS-IS without warranty of any kind,either express
+  or implied. User hereby grants a royalty-free license to any and all
+  derivatives based upon this software code base.
+=====================================================================*/
+
+// TODO: this needs to be moved into the CLASS and modifyable at runtime
+// TODO: define an iterator object to traverse the vbs in a pdu
+
+#define MAX_VBS 25
+#include "asnmp/vb.h"         // include Vb class definition
+
+class ACE_Export Pdu
+  // = TITLE
+  //      Protocol Data Unit (PDU) concrete class. An abstraction of the
+  //      data packet used to by two SNMP sessions to communicate.
+{
+public:
+  Pdu( void);
+  // constructor no args
+
+  Pdu( Vb* pvbs, const int pvb_count);
+  // constructor with vbs and count
+
+  Pdu( const Pdu &pdu);
+  // constructor with another Pdu instance
+
+  ~Pdu();
+  // destructor
+
+  Pdu& operator=( const Pdu &pdu);
+  // assignment to another Pdu object overloaded
+
+  Pdu& operator+=( Vb &vb);
+  // append a vb to the pdu
+
+  // TODO: add Pdu& operator-=(const Vb &vb);
+
+  int get_vblist( Vb* pvbs, const int pvb_count);
+  // extract all Vbs from Pdu
+
+  int set_vblist( Vb* pvbs, const int pvb_count);
+  // deposit all Vbs to Pdu
+
+  int get_vb( Vb &vb, const int index = 0) const;
+  // get a particular vb
+  // where 0 is the first vb
+
+  int set_vb( Vb &vb, const int index);
+  // set a particular vb
+  // where 0 is the first vb
+
+  int get_vb_count() const;
+  // return number of vbs
+
+  int get_error_status() const;
+  // return the error status
+
+  char *agent_error_reason();
+  // return the complete error info from this pdu
+
+  friend void set_error_status( Pdu *pdu, const int status);
+  // set the error status
+
+  int get_error_index() const;
+  // return the error index
+
+  friend void set_error_index( Pdu *pdu, const int index);
+  // set the error index
+
+  friend void clear_error_status( Pdu *pdu);
+  // clear error status
+
+  friend void clear_error_index( Pdu *pdu);
+  // clear error index
+
+  unsigned long get_request_id() const;
+  // return the request id
+
+  friend void set_request_id( Pdu *pdu, const unsigned long rid);
+  // set the request id
+
+  unsigned short get_type() const;
+  // get the pdu type
+
+  void set_type( unsigned short type);
+  // set the pdu type
+
+  int valid() const;
+  // returns validity of Pdu instance
+
+  int trim(const int count=1);
+  // trim off count vbs from the end of the vb list
+
+  int delete_vb( const int position);
+  // delete a Vb anywhere within the Pdu
+
+  void delete_all_vbs();
+  // delete_all vbs in pdu
+
+  void set_notify_timestamp( const TimeTicks & timestamp);
+  // set notify timestamp
+
+  void get_notify_timestamp( TimeTicks & timestamp) const;
+  // get notify timestamp
+
+  void set_notify_id( const Oid id);
+  // set the notify id
+
+  void get_notify_id( Oid &id) const;
+  // get the notify id
+
+  void set_notify_enterprise( const Oid &enterprise);
+  // set the notify enterprise
+
+  void get_notify_enterprise( Oid & enterprise) const;
+  // get the notify enterprise
+
+  char *to_string();
+  // return fomatted version of this object
+
+
+  protected:
+    Vb *vbs_[MAX_VBS];
+    // pointer to array of Vbs
+
+    int vb_count_;
+    // count of Vbs
+
+    int error_status_;
+    // SMI error status
+
+    int error_index_;
+    // SMI error index
+
+    int validity_;
+    // valid boolean status of object construction
+    unsigned long request_id_;
+    // SMI request id
+
+    unsigned short pdu_type_;
+    // derived at run time based on request type
+
+    TimeTicks notify_timestamp_;
+    // a timestamp associated with an infor
+    // for notify Pdu objects only
+    // traps & notifies
+
+    Oid notify_id_;
+    // an id
+
+    Oid notify_enterprise_;
+
+   private:
+   char *output_;
+   // buffer for to_string()
+};
+
+
+class VbIter
+  // = TITLE
+  //      Utility class to iterate once through a PDU varbind list
+{
+   public:
+   VbIter(Pdu& pdu);
+   // default constructor
+
+   int next(Vb& vb);
+   // returns 1 if ok, else 0 if none left
+
+  private:
+   VbIter(const VbIter&);
+   // disallow copy constructor use
+
+   int idx_;
+   // current object in list
+
+   Pdu *pdu_;
+   // ptr to pdu being interated over
+};
+
+#endif //PDU_CLS_
+
---
+++ tags/ACE-4_5_63/ace/Service_Types.h
@@ -0,0 +1,187 @@
+/* -*- C++ -*- */
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    ace
+//
+// = FILENAME
+//    Service_Types.h
+//
+// = AUTHOR
+//    Doug Schmidt
+//
+// ============================================================================
+
+#ifndef ACE_SERVICE_TYPE_H
+#define ACE_SERVICE_TYPE_H
+
+#include "ace/Service_Object.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/Synch.h"
+
+class ACE_Export ACE_Service_Type_Impl
+{
+  // = TITLE
+  //     The abstract base class of the hierarchy that defines the
+  //     contents of the <ACE_Service_Repository>.  The subclasses of
+  //     this class allow the configuration of <ACE_Service_Objects>,
+  //     <ACE_Modules>, and <ACE_Streams>.
+  //
+  // = DESCRIPTION
+  //     This class provides the root of the implementation hierarchy
+  //     of the "Bridge" pattern.  It maintains a pointer to the
+  //     appropriate type of service implementation, i.e.,
+  //     <ACE_Service_Object>, <ACE_Module>, or <ACE_Stream>.
+public:
+  // = Initialization and termination methods.
+  ACE_Service_Type_Impl (void *object,
+                         const ASYS_TCHAR *s_name,
+                         u_int flags = 0,
+                         ACE_Service_Object_Exterminator gobbler = 0);
+  virtual ~ACE_Service_Type_Impl (void);
+
+  // = Pure virtual interface (must be defined by the subclass).
+  virtual int suspend (void) const = 0;
+  virtual int resume (void) const = 0;
+  virtual int init (int argc, ASYS_TCHAR *argv[]) const = 0;
+  virtual int fini (void) const;
+  virtual int info (ASYS_TCHAR **str, size_t len) const = 0;
+
+  void *object (void) const;
+  // The pointer to the service.
+
+  const ASYS_TCHAR *name (void) const;
+  // Get the name of the service.
+
+  void name (const ASYS_TCHAR *);
+  // Set the name of the service.
+
+  void dump (void) const;
+  // Dump the state of an object.
+
+  ACE_ALLOC_HOOK_DECLARE;
+  // Declare the dynamic allocation hooks.
+
+protected:
+  const ASYS_TCHAR *name_;
+  // Name of the service.
+
+  void *obj_;
+  // Pointer to object that implements the service.  This actually
+  // points to an <ACE_Service_Object>, <ACE_Module>, or <ACE_Stream>.
+
+  ACE_Service_Object_Exterminator gobbler_;
+  // Destroy function to deallocate obj_.
+
+  u_int flags_;
+  // Flags that control serivce behavior (particularly deletion).
+};
+
+class ACE_Export ACE_Service_Object_Type : public ACE_Service_Type_Impl
+{
+  // = TITLE
+  //     Define the methods for handling the configuration of
+  //     <ACE_Service_Objects>.
+public:
+  // = Initialization method.
+  ACE_Service_Object_Type (void *so,
+                           const ASYS_TCHAR *name,
+                           u_int flags = 0,
+                           ACE_Service_Object_Exterminator gobbler = 0);
+
+  ~ACE_Service_Object_Type (void);
+
+  // = Implement the hooks for <ACE_Service_Objects>.
+  virtual int suspend (void) const;
+  virtual int resume (void) const;
+  virtual int init (int argc, ASYS_TCHAR *argv[]) const;
+  virtual int fini (void) const;
+  virtual int info (ASYS_TCHAR **str, size_t len) const;
+};
+
+class ACE_Export ACE_Module_Type : public ACE_Service_Type_Impl
+{
+  // = TITLE
+  //     Define the methods for handling the configuration of
+  //     <ACE_Modules>.
+public:
+  // = Initialization method.
+  ACE_Module_Type (void *m, // Really an <ACE_Module> *.
+                   const ASYS_TCHAR *identifier,
+                   u_int flags = 0);
+
+  ~ACE_Module_Type (void);
+
+  // = Implement the hooks for <ACE_Modules>.
+  virtual int suspend (void) const;
+  virtual int resume (void) const;
+  virtual int init (int argc, ASYS_TCHAR *argv[]) const;
+  virtual int fini (void) const;
+  virtual int info (ASYS_TCHAR **str, size_t len) const;
+
+  // Get/set the link pointer.
+  ACE_Module_Type *link (void) const;
+  void link (ACE_Module_Type *);
+
+  void dump (void) const;
+  // Dump the state of an object.
+
+  ACE_ALLOC_HOOK_DECLARE;
+  // Declare the dynamic allocation hooks.
+
+private:
+  ACE_Module_Type *link_;
+  // Pointer to the next <ACE_Module_Type> in an <ACE_Stream_Type>.
+};
+
+class ACE_Export ACE_Stream_Type : public ACE_Service_Type_Impl
+{
+  // = TITLE
+  //     Define the methods for handling the configuration of
+  //     <ACE_Streams>.
+public:
+  // = Initialization method.
+  ACE_Stream_Type (void *s, // Really an <ACE_Stream> *.
+                   const ASYS_TCHAR *identifier,
+                   u_int flags = 0);
+
+  ~ACE_Stream_Type (void);
+
+  // = Implement the hooks for <ACE_Streams>.
+  virtual int suspend (void) const;
+  virtual int resume (void) const;
+  virtual int init (int argc, ASYS_TCHAR *argv[]) const;
+  virtual int fini (void) const;
+  virtual int info (ASYS_TCHAR **str, size_t len) const;
+
+  int push (ACE_Module_Type *new_module);
+  // Add a new <ACE_Module> to the top of the <ACE_Stream>.
+
+  int remove (ACE_Module_Type *module);
+  // Search for <module> and remove it from the <ACE_Stream>.
+
+  ACE_Module_Type *find (const ASYS_TCHAR *mod_name) const;
+  // Locate the <ACE_Module> with <mod_name>.
+
+  void dump (void) const;
+  // Dump the state of an object.
+
+  ACE_ALLOC_HOOK_DECLARE;
+  // Declare the dynamic allocation hooks.
+
+private:
+  ACE_Module_Type *head_;
+  // Pointer to the head of the <ACE_Module> list.
+};
+
+#if defined (__ACE_INLINE__)
+#include "ace/Service_Types.i"
+#endif /* __ACE_INLINE__ */
+
+#endif /* _SERVICE_TYPE_H */
