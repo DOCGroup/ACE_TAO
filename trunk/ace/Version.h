b5e93a5fee94f2df673159631fa3a6980953d1e1
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-10-31 23:17:47 +0000

    4.6.1

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (6u)
-#define ACE_BETA_VERSION (0u)
-#define ACE_VERSION "4.6\0"
+#define ACE_BETA_VERSION (1u)
+#define ACE_VERSION "4.6.1\0"
