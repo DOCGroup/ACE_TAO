c26ab4911dd102c89ea87a92ac5e7f9c5c2797d4
jcej <jcej@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-10 20:33:32 +0000

    *** empty log message ***

--- trunk/docs/tutorials/017/Barrier_i.cpp
+++ trunk/docs/tutorials/017/Barrier_i.cpp
@@ -3,6 +3,10 @@
 
 #include "Barrier_i.h"
 
+/* Initialize the threads_ count to zero and the barrier_ pointer to a
+   safe value.  At the same time, we remember the thread that created
+   us so that we can allow it to change the thread count.
+*/
 Barrier::Barrier(void)
    : threads_(0)
     ,barrier_(0)
@@ -10,17 +14,35 @@ Barrier::Barrier(void)
     owner_ = ACE_OS::thr_self();
 }
 
+/* Ensure that barrier_ get's deleted so that we don't have a memory leak.
+ */
 Barrier::~Barrier(void)
 {
-    this->done();
+    delete barrier_;
 }
 
+// Report on the number of threads.
 u_int Barrier::threads(void)
 {
     return threads_.value();
 }
 
-int Barrier::threads( u_int _threads )
+/* Allow the owning thread to (re)set the number of threads.
+   make_barrier() is called because it will wait() if we were already
+   configured.  Typical usage would be for the worker threads to
+   wait() while the primary (eg -- owner) thread adjusts the thread
+   count.
+
+   For instance:
+       In the worker threads:
+           if( myBarrier.threads() != current_thread_count )
+               myBarrier.wait();
+
+       In the primary thread:
+           if( myBarrier.threads() != current_thread_count )
+               myBarrier.threads( current_thread_count, 1 );
+ */
+int Barrier::threads( u_int _threads, int _wait )
 {
     if( ACE_OS::thr_self() != owner_ )
     {
@@ -29,9 +51,13 @@ int Barrier::threads( u_int _threads )
 
     threads_ = _threads;
     
-    return make_barrier();
+    return make_barrier(_wait);
 }
 
+/* Wait for all threads to synch if the thread count is valid.  Note
+   that barrier_ will be 0 if the threads() mutator has not been
+   invoked.
+*/
 int Barrier::wait(void)
 {
     if( ! barrier_ )
@@ -42,6 +68,12 @@ int Barrier::wait(void)
     return barrier_->wait();
 }
 
+/* Wait for all threads to synch.  As each thread passes wait(), it
+   will decrement our thread counter.  (That is why we had to make
+   threads_ an atomic op.)  When the last thread decrements the
+   counter it will also delete the ACE_Barrier & free up a little
+   memory.
+*/
 int Barrier::done(void)
 {
     if( this->wait() == -1 )
@@ -60,19 +92,33 @@ int Barrier::done(void)
     return 0;
 }
 
-int Barrier::make_barrier(void)
+/* This will build the actual barrier.  I broke this code out of the
+   threads() mutator in case it might be useful elsewhere.
+   If a barrier already exists, we will wait for all threads before
+   creating a new one.  This trait is what allows the threads mutator
+   to be used as shown above.
+ */
+int Barrier::make_barrier( int _wait )
 {
+        // Wait for and delete any existing barrier.
     if( barrier_ )
     {
-        barrier_->wait();
+        if( _wait )
+        {
+            barrier_->wait();
+        }
         delete barrier_;
     }
 
+        // Ensure we have a valid thread count.
     if( ! threads_.value() )
     {
         return -1;
     }
-        
+
+        // Create the actual barrier.  Note that we initialize it with 
+        // threads_.value() to set its internal thread count.  If the
+        // 'new' fails we will return -1 to the caller.
     ACE_NEW_RETURN(barrier_,ACE_Barrier(threads_.value()),-1);
 
     return 0;
