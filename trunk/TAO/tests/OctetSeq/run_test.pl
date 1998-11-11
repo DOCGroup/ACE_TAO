4d7e06b0d712a616111a7864b6b49b79eaac27e8
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-11 20:28:51 +0000

    General updates, including switching to the ACE class and using .\ or ./ before executable names.

--- trunk/TAO/tests/OctetSeq/run_test.pl
+++ trunk/TAO/tests/OctetSeq/run_test.pl
@@ -7,6 +7,6 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 unshift @INC, '../../../bin';
 require ACEutils;
 
-$status  = system ("OctetSeq$Process::EXE_EXT -n 16 -l 32 -h 512 -s 4");
+$status  = system ($EXEPREFIX."OctetSeq$Process::EXE_EXT -n 16 -l 32 -h 512 -s 4");
 
 exit $status;
