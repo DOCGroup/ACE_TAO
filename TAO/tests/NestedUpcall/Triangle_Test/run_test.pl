ec364532e11919373a17dc661821df3b350fa271
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-11 20:24:43 +0000

    Forgot to totally get rid of the ORB port stuff.

--- TAO/tests/NestedUpcall/Triangle_Test/run_test.pl
+++ TAO/tests/NestedUpcall/Triangle_Test/run_test.pl
@@ -16,18 +16,15 @@ unlink $ior1file;
 unlink $ior2file;
 
 $SV1 = Process::Create ($EXEPREFIX."server_A".$Process::EXE_EXT, 
-                        " -ORBport $server1_port -o $ior1file ".
-			" -ORBobjrefstyle url");
+                        "-o $ior1file -ORBobjrefstyle url");
 
 $SV2 = Process::Create ($EXEPREFIX."server_B".$Process::EXE_EXT, 
-                        " -ORBport $server2_port -o $ior2file ".
-			" -ORBobjrefstyle url");
+                        "-o $ior2file -ORBobjrefstyle url");
 
 ACE::waitforfile ($ior1file);
 ACE::waitforfile ($ior2file);
 
 $status = system ($EXEPREFIX."initiator".$Process::EXE_EXT.
-		  " -ORBport $client_port ".
 		  " -f $ior1file -g $ior2file");
 
 $SV1->Kill (); $SV1->Wait ();
