2964f368b343a5346ba4d7ad0dbf0f093e3f3a82
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-11 20:23:32 +0000

    General updates, including switching to the ACE class and using .\ or ./ before executable names.

--- trunk/TAO/tests/NestedUpcall/Triangle_Test/run_test.pl
+++ trunk/TAO/tests/NestedUpcall/Triangle_Test/run_test.pl
@@ -8,8 +8,6 @@ unshift @INC, '../../../../bin';
 require Process;
 require ACEutils;
 
-$server1_port = 0;
-$server2_port = 0;
 $ior1file = "servera.ior";
 $ior2file = "serverb.ior";
 
@@ -17,18 +15,18 @@ $ior2file = "serverb.ior";
 unlink $ior1file;
 unlink $ior2file;
 
-$SV1 = Process::Create ("server_A".$Process::EXE_EXT, 
+$SV1 = Process::Create ($EXEPREFIX."server_A".$Process::EXE_EXT, 
                         " -ORBport $server1_port -o $ior1file ".
 			" -ORBobjrefstyle url");
 
-$SV2 = Process::Create ("server_B".$Process::EXE_EXT, 
+$SV2 = Process::Create ($EXEPREFIX."server_B".$Process::EXE_EXT, 
                         " -ORBport $server2_port -o $ior2file ".
 			" -ORBobjrefstyle url");
 
 ACE::waitforfile ($ior1file);
 ACE::waitforfile ($ior2file);
 
-$status = system ("initiator".$Process::EXE_EXT.
+$status = system ($EXEPREFIX."initiator".$Process::EXE_EXT.
 		  " -ORBport $client_port ".
 		  " -f $ior1file -g $ior2file");
 
