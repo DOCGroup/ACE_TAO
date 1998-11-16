0fee264a2ba1fb50227167dac31a520456788007
cdgill <cdgill@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-16 23:54:19 +0000

    Added test, less than functor, better comments, etc. to RB_Tree class

--- ace/RB_Tree.h
+++ ace/RB_Tree.h
@@ -18,28 +18,31 @@
 #define ACE_RB_TREE_H
 
 #include "ace/ACE.h"
+#include "ace/Functor.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-class RB_Tree_Node_Base
+class ACE_RB_Tree_Node_Base
 {
 public:
   enum RB_Tree_Node_Color {RED, BLACK};
 };
 
 template <class KEY, class T>
-class RB_Tree_Node : public RB_Tree_Node_Base
+class ACE_RB_Tree_Node : public ACE_RB_Tree_Node_Base
 {
   // = TITLE
   //   Implements a node in a Red-Black Tree ADT.
 public:
+
   // Initialization and termination methods.
-  RB_Tree_Node (const KEY &k, const T &t);
+
+  ACE_RB_Tree_Node (const KEY &k, const T &t);
   // Constructor.
 
-  ~RB_Tree_Node (void);
+  ~ACE_RB_Tree_Node (void);
   // Destructor.
 
   KEY &key (void);
@@ -54,22 +57,22 @@ public:
   RB_Tree_Node_Color color (void);
   // Get color of the node.
 
-  RB_Tree_Node<KEY, T> *parent (void);
+  ACE_RB_Tree_Node<KEY, T> *parent (void);
   // Accessor for node's parent pointer.
 
-  void parent (RB_Tree_Node<KEY, T> * p);
+  void parent (ACE_RB_Tree_Node<KEY, T> * p);
   // Mutator for node's parent pointer.
 
-  RB_Tree_Node<KEY, T> *left (void);
+  ACE_RB_Tree_Node<KEY, T> *left (void);
   // Accessor for node's left child pointer.
 
-  void left (RB_Tree_Node<KEY, T> *l);
+  void left (ACE_RB_Tree_Node<KEY, T> *l);
   // Mutator for node's left child pointer.
 
-  RB_Tree_Node<KEY, T> *right (void);
+  ACE_RB_Tree_Node<KEY, T> *right (void);
   // Accessor for node's right child pointer.
 
-  void right (RB_Tree_Node<KEY, T> * r);
+  void right (ACE_RB_Tree_Node<KEY, T> * r);
   // Mutator for node's right child pointer
 
 private:
@@ -83,40 +86,45 @@ private:
   RB_Tree_Node_Color color_;
   // Color of the node.
 
-  RB_Tree_Node<KEY, T> *parent_;
+  ACE_RB_Tree_Node<KEY, T> *parent_;
   // Pointer to node's parent.
 
-  RB_Tree_Node<KEY, T> *left_;
+  ACE_RB_Tree_Node<KEY, T> *left_;
   // Pointer to node's left child.
 
-  RB_Tree_Node<KEY, T> *right_;
-  // Pointer to node's righ child.
+  ACE_RB_Tree_Node<KEY, T> *right_;
+  // Pointer to node's right child.
 };
 
 template <class KEY, class T>
-class RB_Tree
+class ACE_RB_Tree
 {
   // = TITLE
   //   Implements a Red-Black Tree ADT, according to T. H. Corman,
   //   C. E. Leiserson, and R. L. Rivest, "Introduction to Algorithms"
-  //   1990, MIT, chapter 14
+  //   1990, MIT, chapter 14.
+  //
+  // = Description
+  //   Optional flag passed to constructor indicates whether or not the
+  //   passed functor should be deleted by the ACE_RB_Tree's destructor.
 public:
   // = Initialization and termination methods.
-  RB_Tree (void);
-  // Constructor
+  ACE_RB_Tree (ACE_Const_Binary_Functor_Base<KEY, KEY> *less_than_functor = 0,
+               int free_functor = 0);
+  // Constructor.
 
-  RB_Tree (const RB_Tree<KEY, T> &rbt);
-  // copy constructor.
+  ACE_RB_Tree (const ACE_RB_Tree<KEY, T> &rbt);
+  // Copy constructor.
 
-  virtual ~RB_Tree (void);
-  // Destructor
+  virtual ~ACE_RB_Tree (void);
+  // Destructor.
 
-  void operator= (const RB_Tree<KEY, T> &rbt);
+  void operator= (const ACE_RB_Tree<KEY, T> &rbt);
   // Assignment operator.
 
   virtual int lessthan (const KEY &k1, const KEY &k2);
-  // Lessthan comparison function for keys.  returns 1 if k1 < k2, 0
-  // otherwise.
+  // Less than comparison function for keys.  Default implementation returns 1
+  // if k1 < k2, and 0 otherwise.
 
   T* find (const KEY &k);
   // Returns a pointer to the item corresponding to the
@@ -125,7 +133,8 @@ public:
   T* insert (const KEY &k, const T &t);
   // Inserts a *copy* of the key and the item into the tree: both the
   // key type KEY and the item type T must have well defined semantics
-  // for copy construction and < comparison.  This method returns a
+  // for copy construction.  The default implementation also requires that
+  // the key type support welll defined < semantics.  This method returns a
   // pointer to the inserted item copy, or 0 if an error occurred.
   // NOTE: if an identical key already exists in the tree, no new item
   // is created, and the returned pointer addresses the existing item
@@ -138,63 +147,74 @@ public:
   // occurred.
 
   void clear (void);
-  // destroys all nodes and sets the root pointer null.
+  // Destroys all nodes and sets the root pointer null.
 
   // These could all be made private methods by making the corresponding
   // class template instantiations friends, but there are some problems
-  // with this on certain compilers: leave them all public for now
+  // with this on certain compilers: leave them all public for now.
 
 // private:
 
-  void RB_rotate_right (RB_Tree_Node<KEY, T> * x);
+  void RB_rotate_right (ACE_RB_Tree_Node<KEY, T> * x);
   // Method for right rotation of the tree about a given node.
 
-  void RB_rotate_left (RB_Tree_Node<KEY, T> * x);
+  void RB_rotate_left (ACE_RB_Tree_Node<KEY, T> * x);
   // Method for left rotation of the tree about a given node.
 
-  void RB_delete_fixup (RB_Tree_Node<KEY, T> * x);
+  void RB_delete_fixup (ACE_RB_Tree_Node<KEY, T> * x);
   // Method for restoring Red-Black properties after deletion.
 
-  RB_Tree_Node<KEY, T> * RB_tree_successor (RB_Tree_Node<KEY, T> *x) const;
+  ACE_RB_Tree_Node<KEY, T> * 
+    RB_tree_successor (ACE_RB_Tree_Node<KEY, T> *x) const;
   // Method to find the successor node of the given node in the tree.
 
-  RB_Tree_Node<KEY, T> * RB_tree_predecessor (RB_Tree_Node<KEY, T> *x) const;
+  ACE_RB_Tree_Node<KEY, T> * 
+    RB_tree_predecessor (ACE_RB_Tree_Node<KEY, T> *x) const;
   // Method to find the predecessor node of the given node in the
   // tree.
 
-  RB_Tree_Node<KEY, T> * RB_tree_minimum (RB_Tree_Node<KEY, T> *x) const;
+  ACE_RB_Tree_Node<KEY, T> * 
+    RB_tree_minimum (ACE_RB_Tree_Node<KEY, T> *x) const;
   // Method to find the minimum node of the subtree rooted at the
   // given node.
 
-  RB_Tree_Node<KEY, T> * RB_tree_maximum (RB_Tree_Node<KEY, T> *x) const;
+  ACE_RB_Tree_Node<KEY, T> * 
+    RB_tree_maximum (ACE_RB_Tree_Node<KEY, T> *x) const;
   // Method to find the maximum node of the subtree rooted at the
   // given node.
 
-  RB_Tree_Node<KEY, T> * find_node (const KEY &k);
+  ACE_RB_Tree_Node<KEY, T> * find_node (const KEY &k);
   // Returns a pointer to a matching node if there is one, a pointer
   // to the node under which to insert the item if the tree is not
   // empty and there is no such match, or 0 if the tree is empty.
 
-  void RB_rebalance (RB_Tree_Node<KEY, T> * x);
+  void RB_rebalance (ACE_RB_Tree_Node<KEY, T> * x);
   // Rebalance the tree after insertion of a node.
 
-  // private members
+  // Private members.
 
-  RB_Tree_Node<KEY, T> *root_;
+  ACE_RB_Tree_Node<KEY, T> *root_;
   // The root of the tree.
+
+  ACE_Const_Binary_Functor_Base<KEY, KEY> *less_than_functor_;
+  // "Less than" functor for comparing nodes in the tree.
+
+  int free_functor_;
+  // Flag indicating whether or not to delete functor in destructor
+  // and assignment operator.
 };
 
 template <class KEY, class T>
-class RB_Tree_Iterator
+class ACE_RB_Tree_Iterator
 {
   // = TITLE
   //   Implements an iterator for a Red-Black Tree ADT.
 public:
   // = Initialization and termination methods.
-  RB_Tree_Iterator (const RB_Tree<KEY, T> &tree);
+  ACE_RB_Tree_Iterator (const ACE_RB_Tree<KEY, T> &tree);
   // Constructor.
 
-  ~RB_Tree_Iterator (void);
+  ~ACE_RB_Tree_Iterator (void);
   // Destructor.
 
   KEY *key (void);
@@ -216,22 +236,24 @@ public:
   // Move to the previous item in the tree.
 
   int is_done (void);
-  // Returns 0 if the iterator is positioned over a valid RB_Tree
+  // Returns 0 if the iterator is positioned over a valid ACE_RB_Tree
   // node, returns 1 if not.
 
 private:
   // = Declare private and do not define.
 
-  // Explicitly prevent assignment and copy construction of iterators
-  ACE_UNIMPLEMENTED_FUNC (RB_Tree_Iterator (const RB_Tree_Iterator<KEY, T> &))
-  ACE_UNIMPLEMENTED_FUNC (void operator = (const RB_Tree_Iterator<KEY, T> &))
+  // Explicitly prevent assignment and copy construction of iterators.
+  ACE_UNIMPLEMENTED_FUNC (
+    ACE_RB_Tree_Iterator (const ACE_RB_Tree_Iterator<KEY, T> &))
+  ACE_UNIMPLEMENTED_FUNC (
+    void operator = (const ACE_RB_Tree_Iterator<KEY, T> &))
 
   // Private members.
 
-  const RB_Tree<KEY, T> &tree_;
-  // Reference to the RB_Tree over which we're iterating.
+  const ACE_RB_Tree<KEY, T> &tree_;
+  // Reference to the ACE_RB_Tree over which we're iterating.
 
-  RB_Tree_Node <KEY, T> *node_;
+  ACE_RB_Tree_Node <KEY, T> *node_;
   // Pointer to the node currently under the iterator.
 
 };
--- ace/RB_Tree.i
+++ ace/RB_Tree.i
@@ -1,152 +1,185 @@
 /* -*- C++ -*- */
 // $Id$
 
-/////////////////////////////////////////
-// template class RB_Tree_Node<KEY, T> //
-/////////////////////////////////////////
+/////////////////////////////////////////////
+// template class ACE_RB_Tree_Node<KEY, T> //
+/////////////////////////////////////////////
+
+// Key accessor.
 
 template <class KEY, class T> ACE_INLINE KEY &
-RB_Tree_Node<KEY, T>::key ()
+ACE_RB_Tree_Node<KEY, T>::key ()
 {
   return k_;
 }
-  // key accessor
+
+
+// Item accessor.
 
 template <class KEY, class T> ACE_INLINE T &
-RB_Tree_Node<KEY, T>::item ()
+ACE_RB_Tree_Node<KEY, T>::item ()
 {
   return t_;
 }
-  // item accessor
+
+
+// Set color of the node.
 
 template <class KEY, class T> ACE_INLINE void
-RB_Tree_Node<KEY, T>::color (RB_Tree_Node_Base::RB_Tree_Node_Color c)
+ACE_RB_Tree_Node<KEY, T>::color (ACE_RB_Tree_Node_Base::RB_Tree_Node_Color c)
 {
   color_ = c;
 }
-  // set color of the node
+
+
+// Get color of the node.
 
 template <class KEY, class T>
-ACE_INLINE RB_Tree_Node_Base::RB_Tree_Node_Color
-RB_Tree_Node<KEY, T>::color ()
+ACE_INLINE ACE_RB_Tree_Node_Base::RB_Tree_Node_Color
+ACE_RB_Tree_Node<KEY, T>::color ()
 {
   return color_;
 }
-  // get color of the node
 
 
-template <class KEY, class T> ACE_INLINE RB_Tree_Node<KEY, T> *
-RB_Tree_Node<KEY, T>::parent ()
+// Accessor for node's parent pointer.
+
+template <class KEY, class T> ACE_INLINE ACE_RB_Tree_Node<KEY, T> *
+ACE_RB_Tree_Node<KEY, T>::parent ()
 {
   return parent_;
 }
-  // accessor for node's parent pointer
+
+
+// Mutator for node's parent pointer.
 
 template <class KEY, class T> ACE_INLINE void
-RB_Tree_Node<KEY, T>::parent (RB_Tree_Node<KEY, T> * p)
+ACE_RB_Tree_Node<KEY, T>::parent (ACE_RB_Tree_Node<KEY, T> * p)
 {
   parent_ = p;
 }
-  // mutator for node's parent pointer
+  
 
-template <class KEY, class T> ACE_INLINE RB_Tree_Node<KEY, T> *
-RB_Tree_Node<KEY, T>::left ()
+
+// Accessor for node's left child pointer.
+
+template <class KEY, class T> ACE_INLINE ACE_RB_Tree_Node<KEY, T> *
+ACE_RB_Tree_Node<KEY, T>::left ()
 {
   return left_;
 }
-  // accessor for node's left child pointer
+
+
+// Mutator for node's left child pointer.
 
 template <class KEY, class T> ACE_INLINE void
-RB_Tree_Node<KEY, T>::left (RB_Tree_Node<KEY, T> * l)
+ACE_RB_Tree_Node<KEY, T>::left (ACE_RB_Tree_Node<KEY, T> * l)
 {
   left_ = l;
 }
-  // mutator for node's left child pointer
 
-template <class KEY, class T> ACE_INLINE RB_Tree_Node<KEY, T> *
-RB_Tree_Node<KEY, T>::right ()
+
+// Accessor for node's right child pointer.
+
+template <class KEY, class T> ACE_INLINE ACE_RB_Tree_Node<KEY, T> *
+ACE_RB_Tree_Node<KEY, T>::right ()
 {
   return right_;
 }
-  // accessor for node's right child pointer
+
+
+// Mutator for node's right child pointer.
 
 template <class KEY, class T> ACE_INLINE void
-RB_Tree_Node<KEY, T>::right (RB_Tree_Node<KEY, T> * r)
+ACE_RB_Tree_Node<KEY, T>::right (ACE_RB_Tree_Node<KEY, T> * r)
 {
   right_ = r;
 }
-  // mutator for node's right child pointer
 
 
-////////////////////////////////////
-// template class RB_Tree<KEY, T> //
-////////////////////////////////////
+
+////////////////////////////////////////
+// template class ACE_RB_Tree<KEY, T> //
+////////////////////////////////////////
+
+
+// Destroys all nodes and sets the root pointer null.
 
 template <class KEY, class T> ACE_INLINE void
-RB_Tree<KEY, T>::clear ()
+ACE_RB_Tree<KEY, T>::clear ()
 {
   delete root_;
   root_ = 0;
 }
-  // destroys all nodes and sets the root pointer null.
 
 
-/////////////////////////////////////////////
-// template class RB_Tree_Iterator<KEY, T> //
-/////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+// template class ACE_RB_Tree_Iterator<KEY, T> //
+/////////////////////////////////////////////////
 
 
+// Accessor for key of node under iterator (if any).
 
 template <class KEY, class T> ACE_INLINE KEY *
-RB_Tree_Iterator<KEY, T>::key ()
+ACE_RB_Tree_Iterator<KEY, T>::key ()
 {
   return node_ ? (&(node_->key ())) : 0;
 }
-  // accessor for key of node under iterator (if any)
+
+
+// Accessor for item of node under iterator (if any).
 
 template <class KEY, class T> ACE_INLINE T *
-RB_Tree_Iterator<KEY, T>::item ()
+ACE_RB_Tree_Iterator<KEY, T>::item ()
 {
   return node_ ? (&(node_->item ())) : 0;
 }
-  // accessor for item of node under iterator (if any)
+
+
+// Move to the first item in the tree.
 
 template <class KEY, class T> ACE_INLINE int
-RB_Tree_Iterator<KEY, T>::first ()
+ACE_RB_Tree_Iterator<KEY, T>::first ()
 {
   node_ = tree_.RB_tree_minimum (tree_.root_);
   return node_ ? 1 : 0;
 }
-  // move to the first item in the tree
+
+
+// Move to the last item in the tree.
 
 template <class KEY, class T> ACE_INLINE int
-RB_Tree_Iterator<KEY, T>::last ()
+ACE_RB_Tree_Iterator<KEY, T>::last ()
 {
   node_ = tree_.RB_tree_maximum (tree_.root_);
   return node_ ? 1 : 0;
 }
-  // move to the last item in the tree
+
+
+// Moves to the next item in the tree,
+// returns 1 if there is a next item, 0 otherwise.
 
 template <class KEY, class T> ACE_INLINE int
-RB_Tree_Iterator<KEY, T>::next ()
+ACE_RB_Tree_Iterator<KEY, T>::next ()
 {
   node_ = tree_.RB_tree_successor (node_);
   return node_ ? 1 : 0;
 }
-  // move to the next item in the tree
-  // returns 1 if there is a next item, 0 otherwise
+
+
+// Moves to the previous item in the tree,
+// returns 1 if there is a previous item, 0 otherwise.
 
 template <class KEY, class T> ACE_INLINE int
-RB_Tree_Iterator<KEY, T>::previous ()
+ACE_RB_Tree_Iterator<KEY, T>::previous ()
 {
   node_ = tree_.RB_tree_predecessor (node_);
   return node_ ? 1 : 0;
 }
-  // move to the previous item in the tree
-  // returns 1 if there is a previous item, 0 otherwise
 
 template <class KEY, class T> ACE_INLINE int
-RB_Tree_Iterator<KEY, T>::is_done ()
+ACE_RB_Tree_Iterator<KEY, T>::is_done ()
 {
   return node_ ? 0 : 1;
 }
