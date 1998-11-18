25aaccefacb99cae8b45f79b1ee03ea3b0e5b4d8
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-18 07:21:10 +0000

    .

--- trunk/ace/DLL.h
+++ trunk/ace/DLL.h
@@ -23,20 +23,22 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-
 class ACE_Export ACE_DLL
 {
-  //= TITLE
-  //    Provides an abstract interface for handling various DLL operations.
-  //= DESCRIPTION
+  // = TITLE
+  //    Provides an abstract interface for handling various DLL
+  //    operations.
+  //
+  // = DESCRIPTION
   //   This class is an wrapper over the various methods for utilizing
-  //   an dynamically linked library. The operations like open, close,
-  //   symbol have been implemented which will help in opening anmd
-  //   extracting symbol information from the library.
- 
+  //   a dynamically linked library (DLL). The operations like open,
+  //   close, symbol have been implemented to help opening and
+  //   extracting symbol information from a DLL.
 public:
+  // = Initialization and termination methods.
+
   ACE_DLL (void);
-  // Initialisation.
+  // Initialization.
  
   ~ACE_DLL (void);
   // Called when object is destroyed.
@@ -44,9 +46,9 @@ public:
   int open (ACE_DL_TYPE dll_name, 
             int mode = ACE_DEFAULT_SHLIB_MODE);
   // This is the library open operation which uses ACE_SHLIB_HANDLE
-  // object internally to open the library.
-  // The default mode is RTLD_LAZY which means that the identifier
-  // symbols are loaded but not the symbols for the functions.
+  // object internally to open the library.  The default mode is
+  // RTLD_LAZY which means that the identifier symbols are loaded but
+  // not the symbols for the functions.
   // They are loaded dynamically on need.
   // The other modes include:
   //  RTLD_NOW     All necessary relocations are performed when the
@@ -61,7 +63,7 @@ public:
   int close (void);
   // The library is closed.
 
-  char *error ();
+  char *error (void);
   // The error on any of the dl-procedures.
 
 private:
