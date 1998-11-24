d782a5798b4485ad4dd94febcdaf382da9986bc9
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-11-24 21:55:14 +0000

    .

--- tests/DLL_Wrapper_Test.cpp
+++ tests/DLL_Wrapper_Test.cpp
@@ -26,8 +26,9 @@ class Hello
   //    The Hello class in the dynamically linkable library.
   //
   // = DESCRIPTION
-  //    This class is used in this example to show how a library can be loaded
-  //    on demand and its methods called on getting the symbols from the library.
+  //    This class is used in this example to show how a library can
+  //    be loaded on demand and its methods called on getting the
+  //    symbols from the library.
 public:
 
   Hello (void)
@@ -36,16 +37,13 @@ public:
   ~Hello (void)
     { }
 
-  // @@ Kirthika, please follow the ACE programming 
-  // guidelines when naming methods, i.e., say_hello (void) or just
-  // hello (void).
   void say_hello (void)
     {
       ACE_DEBUG ((LM_DEBUG,
+                  // @@ Kirthika, please replace \t with \n below.
                   "Hello\t"));
     }
 
-  // @@ Same here.
   void say_next (void)
     {
       ACE_DEBUG ((LM_DEBUG,
@@ -56,16 +54,13 @@ public:
 // This function returns the Hello object pointer.
 
 extern "C"
-// @@ *done*Kirthika, please following ACE programming guidelines to name
-// this function...
 Hello *get_hello (void)
 {
   Hello *hello;
-  //*done* @@ Kirthika, please always use ACE_NEW_RETURN for dynamic
-  // allocation.
 
+  // @@ Kirthika, please check the return types carefully here...
   ACE_NEW_RETURN (hello, Hello (), -1);
-  // hello = new Hello;
+
   return hello;
 }
 
@@ -76,33 +71,38 @@ main (void)
 { 
   ACE_START_TEST ("DLL_Test");
 
-  // @@*done* Kirthika, please follow the conventions for creating tests,
-  // i.e., use the ACE_START_TEST() and ACE_END_TEST() macros.
+  // @@ Kirthika, it turns out that with Windows NT you can't pass a 0
+  // in and have it default to the symbols in the executable.
+  // Therefore, you'll have to use something like ".obj/DLL_Test.o" on
+  // UNIX and whatever the equivalent is on WinNT (I'm not sure what
+  // it'll be, so please make sure that you check with Nanbor to find
+  // out).
+  ACE_DLL ace_dll_obj ("./DLL_Test.o");
+  // @@ Kirthika, I recommend that you don't use the constructor to
+  // open the DLL_Test file, but rather use the "open()" method since
+  // that way you can test the return value and print an error if it
+  // doesn't work.
 
-  // @@ Kirthika, I suspect that the use of ACE_DL_TYPE (0) may not be
-  // portable across NT and UNIX.  Therefore, can you please use the
-  // name of the *.o file, i.e., ".shobj/DLL_Test.so" or whatevery it
-  // is?  Note that you'll need to figure out how to use various ACE
-  // macros to get this portable...  First get an DLL object. Since
-  // the dllname is empty, the symbols are loaded from a.out.
-  ACE_DLL ace_dll_obj ("./DLL_Test.o");// ***check where it is
-
-  // @@ Kirthika, I'm not sure, but you may need to prefix a "_"
-  // before the "getHello" name, i.e., "_getHello" to make this work
-  // on some platforms.  Please use the ACE macros for portable here.
   TC f = (TC) ace_dll_obj.symbol ("get_hello");
 
+  // @@ Kirthika, please make sure to test whether <f> is a NULL
+  // pointer or a valid pointer before trying to call it.
+
   auto_ptr <Hello> my_hello = f ();
-  // Hello *my_hello = f ();
 
   // Make the method calls, now that the object pointer is available.
   my_hello->say_hello ();
   my_hello->say_next ();
 
-  //*done* Please use an auto_ptr<> to make sure that my_hello is properly
-  // deleted when the scope of main() is exited.
-  // delete my_hello;
-
   ACE_END_TEST;
   return 0;
 }
+
+// @@ Kirthika, please add the proper explicit template instantiation
+// format for GCC and other broken C++ compilers.
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+...
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+...
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
