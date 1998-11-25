ab8514e6522aba038141e007effc5dc315aa81af
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-11-25 16:49:23 +0000

    .

--- ace/Task.cpp
+++ ace/Task.cpp
@@ -89,7 +89,9 @@ ACE_Task_Base::activate (long flags,
   else
     {
       this->thr_count_ += n_threads;
-      if (grp_id != -1)
+      if (this->thr_count_ > 0 && grp_id != -1)
+        // If we're joining an existing group of threads then make
+        // sure to use its group id.
         grp_id = this->grp_id_;
     }
 
