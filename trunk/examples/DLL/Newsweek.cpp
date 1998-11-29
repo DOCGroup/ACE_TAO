8a9678f6bb16ca363a8ac8a143197a7661f3cb8e
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-29 02:43:06 +0000

    *** empty log message ***

--- trunk/examples/DLL/Newsweek.cpp
+++ trunk/examples/DLL/Newsweek.cpp
@@ -2,6 +2,17 @@
 
 #include "Newsweek.h"
 
+#ifdef __cplusplus
+extern "C" {  // only need to export C interface if
+              // used by C++ source code
+#endif
+
+	__declspec( dllexport ) Magazine *create_magazine (void);
+
+#ifdef __cplusplus
+}
+#endif
+
 // Implementation of the abstract class method which describes
 // the magazine.
 void Newsweek::title (void)
@@ -12,9 +23,7 @@ void Newsweek::title (void)
 
 // Returns the Newsweek class pointer.
 extern "C"
-
-Magazine *
-create_magazine (void)
+Magazine *create_magazine (void)
 {
   Magazine *mag;
   ACE_NEW_RETURN (mag,
