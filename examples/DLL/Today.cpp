1f8cd234099d9d650f13409056169c0a70c3742a
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-11-30 02:06:57 +0000

    *** empty log message ***

--- examples/DLL/Today.cpp
+++ examples/DLL/Today.cpp
@@ -1,33 +1,8 @@
 // $Id$
 
+#define ACE_BUILD_SVC_DLL
+
 #include "Today.h"
-
-// This declaration is necessary to get this working on NT.
-#if defined (ACE_HAS_WIN32)
-
-#ifdef __cplusplus
-// @@ Kirthika, this program will ALWAYS be compiled with a C++
-// compiler!  Therefore, you can omit this __cplusplus #ifdef stuff...
-extern "C" {  // only need to export C interface if
-              // used by C++ source code
-#endif
-  // @@ Kirthika, please check to see which of the ACE macros, e.g.,
-  // ACE_SVC_EXPORT, or ACE_EXPORT should be used here, rather than
-  // having to explicitly #ifdef your code you should use the ACE
-  // macros for this.
-	__declspec( dllexport ) Magazine *create_magazine (void);
-
-#ifdef __cplusplus
-}
-#endif
-
-// @@ Kirthika, please make sure that you always do something like this
-// #endif /* ACE_HAS_WIN32 */ when you close a #ifdef block.  However,
-// in this case, I think you can remove the #if defined
-// (ACE_HAS_WIN32) altogether since that'll be handled by the
-// ACE_*_EXPORT macro!
-
-#endif
 
 // Implementation of the abstract class method which describes the
 // magazine.
@@ -40,8 +15,10 @@ Today::title (void)
 }
 
 // Returns the pointer to the Today class.
+// The ACE_BUILD_SVC_DLL and ACE_Svc_Export directives are necessary as take care
+// of exporting the function for Win32 platforms.
+extern "C" ACE_Svc_Export Magazine *create_magazine (void);
 
-extern "C" 
 Magazine *create_magazine (void)
 {
  Magazine *mag;
