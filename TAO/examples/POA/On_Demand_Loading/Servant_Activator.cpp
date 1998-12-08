c8958f8a6c9196272e94a954abd29dfef7cb8161
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-12-08 15:30:19 +0000

    .

--- TAO/examples/POA/On_Demand_Loading/Servant_Activator.cpp
+++ TAO/examples/POA/On_Demand_Loading/Servant_Activator.cpp
@@ -1,4 +1,5 @@
 // $Id$
+
 // ============================================================================
 //
 // = LIBRARY
@@ -25,7 +26,8 @@ ACE_RCSID(On_Demand_Loading, Servant_Activator, "$Id$")
 
 Dir_Service_Activator::Dir_Service_Activator (CORBA::ORB_ptr orb)
   :  orb_ (CORBA::ORB::_duplicate (orb))
-{}
+{
+}
 
 // This method associates an seravnt with the ObjectID.
 
@@ -40,19 +42,24 @@ Dir_Service_Activator::incarnate (const PortableServer::ObjectId &oid,
 
   // Activate and return the servant else exception.
   
-  PortableServer::Servant servant = activate_servant (s.in (), poa);
-  if (servant !=0)
+  PortableServer::Servant servant = this->activate_servant (s.in (),
+                                                            poa);
+
+  if (servant != 0)
       return servant;
   else
     {
+      // @@ Kirthika, can you please make this work for both
+      // native and non-native C++ exceptions?  Please see
+      // Nanbor if you have any questions.
       CORBA::Exception *exception = new CORBA::OBJECT_NOT_EXIST (CORBA::COMPLETED_NO);
       env.exception (exception);
       return 0;
     }
 }
 
-// This is the method is invoked when the object is deactivated or the entire POA is 
-// is deactivated or destroyed.
+// This is the method is invoked when the object is deactivated or the
+// entire POA is is deactivated or destroyed.
 
 void
 Dir_Service_Activator::etherealize (const PortableServer::ObjectId &oid,
@@ -72,12 +79,11 @@ Dir_Service_Activator::etherealize (const PortableServer::ObjectId &oid,
 
   if (remaining_activations == 0)
     deactivate_servant (servant);
- 
 }
 
-
-// This method loads the  dynamically linked library which is the servant and returns
-// the servant object which is then used for other operations in the library.
+// This method loads the dynamically linked library which is the
+// servant and returns the servant object which is then used for other
+// operations in the library.
 
 PortableServer::Servant
 Dir_Service_Activator::activate_servant (const char *str, 
@@ -86,11 +92,13 @@ Dir_Service_Activator::activate_servant (const char *str,
   // The string format is function@dll which needs to be parsed.
   parse_string (str);
 
-  // Now that the library name is available we open teh library.
+  // Now that the library name is available we open the library.  @@
+  // Kirthika, make SURE to check return values from function calls
+  // like this and return failure results correctly...
   dll_.open (dllname_);
 
-  // The next step is to obtain the symbol for the function whihc will create
-  // the servant object and return it to us.
+  // The next step is to obtain the symbol for the function that will
+  // create the servant object and return it to us.
   Servant_Creator_Prototype servant_creator;
   servant_creator = (Servant_Creator_Prototype) dll_.symbol (create_symbol_.in ());
   
@@ -102,60 +110,58 @@ Dir_Service_Activator::activate_servant (const char *str,
                       0);
    
   // Now create and return the servant.
-  return servant_creator (this->orb_.in (), poa);
-  
- }
+  return this->servant_creator (this->orb_.in (), poa);
+}
 
-// This method removes the servant and the dll object associated with it with performed the
-// opening and symbol obtaining operations.
+// This method removes the servant and the dll object associated with
+// it with performed the opening and symbol obtaining operations.
  
 void 
 Dir_Service_Activator::deactivate_servant (PortableServer::Servant servant)
 {
   // the servant is destroyed.
   delete servant;
-
-  // The library loaded is now closed and destroyed.
-  //delete dll_;
-  
 }
 
-// The objectID is in a format of library:factory_method which has to be parsed and separated
-// into tokens to be used.
+// The objectID is in a format of library:factory_method which has to
+// be parsed and separated into tokens to be used.
 
 void
 Dir_Service_Activator::parse_string (const char *s)
 {
-  // The format of the object library:factory_method
-  // This string is parsed to obatin the library name and the
-  // function name which will create trhe servant and return it to us.
+  // The format of the object library:factory_method.  This string is
+  // parsed to obatin the library name and the function name which
+  // will create trhe servant and return it to us.
 
   char str[BUFSIZ],func[BUFSIZ], libname [BUFSIZ];
   char at[2];
 
   at[0]= ':';
   at[1]= '\0';
+  // @@ Kirthika, please use the ACE_OS::str*() functions
+  // consistently.
   strcpy (func, "");
-  // As strtok () puts a NULL in the position after it gives back a token,
-  // we make two copies of the input string.
+  // As strtok () puts a NULL in the position after it gives back a
+  // token, we make two copies of the input string.
   strcpy (str, s);
 
-  // The strtok() method returns the string until '@' i.e. the function name.
-   ACE_OS::strcpy (libname, ACE_OS::strtok(str, at));
+  // @@ Kirthika, this comment is out of date.
+  // The strtok() method returns the string until '@' i.e. the
+  // function name.
+  ACE_OS::strcpy (libname, ACE_OS::strtok (str, at));
    
-   // Get to ':' and make the libname point to the next location in the string.
-   ACE_OS::strcpy (func, ACE_OS::strchr (s,':') + 1);
+  // Get to ':' and make the libname point to the next location in the
+  // string.
+  ACE_OS::strcpy (func, ACE_OS::strchr (s,':') + 1);
   
-   // Assign the respective strings obtained.
+  // Assign the respective strings obtained.
 
-   this->dllname_ = CORBA::string_dup (libname);
-   this->create_symbol_ = CORBA::string_dup (func);
+  this->dllname_ = CORBA::string_dup (libname);
+  this->create_symbol_ = CORBA::string_dup (func);
 
-   ACE_DEBUG ((LM_DEBUG,
-               "the servant library:%s\n the factory_method:%s\n ", 
-               this->dllname_.in (),
-               this->create_symbol_.in ()));
-  
-  
+  ACE_DEBUG ((LM_DEBUG,
+              "the servant library:%s\n the factory_method:%s\n ", 
+              this->dllname_.in (),
+              this->create_symbol_.in ()));
 }
 
