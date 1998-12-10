71924641e4b1f04f602e3438d44815534735585d
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-10 15:08:19 +0000

    added ACE_LACKS_FSYNC

--- ace/config-chorus.h
+++ ace/config-chorus.h
@@ -10,10 +10,6 @@
 #ifndef ACE_CONFIG_H
 #define ACE_CONFIG_H
 
-#if !defined (__ACE_INLINE__)
-# define __ACE_INLINE__
-#endif /* ! __ACE_INLINE__ */
-
 #define CHORUS 3.1b
 #if defined (linux)
   // This shouldn't be necessary.
@@ -24,7 +20,7 @@
 
 #if defined (__GNUG__)
 # include "ace/config-g++-common.h"
-#undef ACE_HAS_ANSI_CASTS
+# undef ACE_HAS_ANSI_CASTS
 #elif defined (ghs)
 # define ACE_HAS_CONSISTENT_SIGNAL_PROTOTYPES
 # define ACE_HAS_TANDEM_SIGNALS
@@ -38,79 +34,63 @@
 
 // OS-specific configuration
 
-# define ACE_LACKS_WRITEV
-# define ACE_LACKS_READV
-# define ACE_HAS_BROKEN_READV
-
-// Optimize ACE_Handle_Set for select().
-#define ACE_HAS_HANDLE_SET_OPTIMIZED_FOR_SELECT
-
-#define ACE_LACKS_UNIX_SIGNALS
-#define ACE_HAS_IP_MULTICAST
 #define ACE_CHORUS_DEFAULT_MIN_STACK_SIZE 0x2000
-
-// Chorus cannot grow shared memory, so this is the default size for a local name space
+// Chorus cannot grow shared memory, so this is the default size for a
+// local name space
 #define ACE_CHORUS_LOCAL_NAME_SPACE_T_SIZE 128000
-
 // Used in OS.i to map an actor id into a KnCap.
 #define ACE_CHORUS_MAX_ACTORS 64
 
-#define ACE_LACKS_MPROTECT
-#define ACE_LACKS_ACCESS
-#define ACE_LACKS_SBRK
-#define ACE_LACKS_GETHOSTENT
-#define ACE_LACKS_GETSERVBYNAME
-#define ACE_LACKS_GETPGID
-#define ACE_LACKS_LONGLONG_T
-#define ACE_LACKS_SYSV_SHMEM
-#define ACE_LACKS_SYSV_MSG_H
-#define ACE_LACKS_KEY_T
-#define ACE_LACKS_FORK
-#define ACE_LACKS_SIGSET
-// #define ACE_LACKS_FILELOCKS
-#define ACE_LACKS_RLIMIT
-
-// got struct timespec
-#define ACE_HAS_POSIX_TIME
+#define ACE_HAS_BROKEN_READV
 #define ACE_HAS_CLOCK_GETTIME
-#define ACE_LACKS_STRRECVFD
-
 #define ACE_HAS_CPLUSPLUS_HEADERS
+#define ACE_HAS_DIRENT
+#define ACE_HAS_HANDLE_SET_OPTIMIZED_FOR_SELECT
+#define ACE_HAS_IP_MULTICAST
 #define ACE_HAS_LONG_MAP_FAILED
-
-// Platforms lacks UNIX domain sockets.
-//#define ACE_LACKS_UNIX_DOMAIN_SOCKETS
-#define ACE_LACKS_UTSNAME_T
-//#define ACE_LACKS_SETSCHED
-
-#define ACE_LACKS_MSYNC
+#define ACE_HAS_MSG
+#define ACE_HAS_POSIX_SEM
+#define ACE_HAS_POSIX_TIME
+#define ACE_HAS_SIGWAIT
+#define ACE_HAS_SSIZE_T
+#define ACE_HAS_STRDUP_EMULATION
+#define ACE_HAS_STRERROR
+#define ACE_HAS_TSS_EMULATION
+#define ACE_LACKS_ACCESS
+#define ACE_LACKS_CONDATTR_PSHARED
+#define ACE_LACKS_FORK
+#define ACE_LACKS_FSYNC
+#define ACE_LACKS_GETHOSTENT
+#define ACE_LACKS_GETPGID
+#define ACE_LACKS_GETSERVBYNAME
+#define ACE_LACKS_KEY_T
+#define ACE_LACKS_LONGLONG_T
 #define ACE_LACKS_MADVISE
 #define ACE_LACKS_MALLOC_H
 #define ACE_LACKS_MEMORY_H
+#define ACE_LACKS_MKFIFO
+#define ACE_LACKS_MPROTECT
+#define ACE_LACKS_MSYNC
+#define ACE_LACKS_NAMED_POSIX_SEM
 #define ACE_LACKS_PARAM_H
+#define ACE_LACKS_READDIR_R
+#define ACE_LACKS_READV
+#define ACE_LACKS_RLIMIT
+#define ACE_LACKS_RWLOCK_T
+#define ACE_LACKS_SBRK
+#define ACE_LACKS_SEMBUF_T
+#define ACE_LACKS_SIGSET
+#define ACE_LACKS_STRRECVFD
+#define ACE_LACKS_SYSV_MSG_H
+#define ACE_LACKS_SYSV_SHMEM
 #define ACE_LACKS_TRUNCATE
-
-// Compiler supports the ssize_t typedef.
-#define ACE_HAS_SSIZE_T
-
-// Compiler/platform supports strerror ().
-#define ACE_HAS_STRERROR
-
-// Defines the page size of the system.
+#define ACE_LACKS_UNIX_SIGNALS
+#define ACE_LACKS_UTSNAME_T
+#define ACE_LACKS_WRITEV
 #define ACE_PAGE_SIZE 4096
-
-// Turns off the tracing feature.
-#if !defined (ACE_NTRACE)
-# define ACE_NTRACE 1
-#endif /* ACE_NTRACE */
-
-// Chorus defines struct msghdr in posix/sys/socket.h
-#define ACE_HAS_MSG
 
 // Yes, we do have threads.
 #define ACE_HAS_THREADS
-#define ACE_HAS_POSIX_SEM
-#define ACE_LACKS_NAMED_POSIX_SEM
 #define ACE_HAS_THREAD_SPECIFIC_STORAGE
 #if !defined (ACE_MT_SAFE)
 # define ACE_MT_SAFE 1
@@ -122,21 +102,20 @@
 #define ACE_LACKS_PTHREAD_CANCEL
 #define ACE_LACKS_PTHREAD_CLEANUP
 #define ACE_LACKS_PTHREAD_THR_SIGSETMASK
-#define ACE_LACKS_RWLOCK_T
-#define ACE_HAS_SIGWAIT
-#define ACE_LACKS_CONDATTR_PSHARED
-#define ACE_LACKS_SEMBUF_T
-#define ACE_HAS_STRDUP_EMULATION
-#define ACE_HAS_DIRENT
-#define ACE_LACKS_READDIR_R
+
+#if !defined (__ACE_INLINE__)
+# define __ACE_INLINE__
+#endif /* ! __ACE_INLINE__ */
+
+#if !defined (ACE_NTRACE)
+# define ACE_NTRACE 1
+#endif /* ACE_NTRACE */
 
 // By default, don't include RCS Id strings in object code.
 #if !defined (ACE_USE_RCSID)
 # define ACE_USE_RCSID 0
 #endif /* #if !defined (ACE_USE_RCSID) */
 
-#define ACE_LACKS_MKFIFO
-#define ACE_HAS_TSS_EMULATION
 // Needed to wait for "processes" to exit.
 #include <am/await.h>
 
