9750bed1a5f91d13a9e044597638c02296f9363f
Steve Huston <shuston@riverace.com>
1998-12-11 20:09:50 +0000

    Fixed the MT timer set/cancel part of this test - it was bogus.

--- tests/MT_Reactor_Timer_Test.h
+++ tests/MT_Reactor_Timer_Test.h
@@ -14,7 +14,7 @@
 //      instantiation in the MT_Reactor_Timer_Test.cpp file.
 //
 // = AUTHOR
-//    Steve Huston
+//    Steve Huston <shuston@riverace.com>
 //
 // ============================================================================
 
@@ -29,6 +29,7 @@
 
 #include "ace/Task.h"
 #include "ace/Pipe.h"
+#include "ace/Synch.h"
 
 class Time_Handler : public ACE_Task<ACE_SYNCH>
 {
@@ -39,6 +40,8 @@ public:
   Time_Handler (void);
 
   void setup (void);
+
+  int verify_results(void);
 
   virtual int svc (void);
   // Run by a daemon thread to handle deferred processing.
@@ -53,7 +56,19 @@ private:
   };
 
   long timer_id_[TIMER_SLOTS];
-  ACE_Reactor my_reactor_;
+  // The timer_id_ array holds timer IDs.  They also have some other values
+  // that are specific to this test:
+  //    -1  the timer has not been set
+  //    -2  the timer was set, but has been cancelled
+  //    -3  the timer was set, and it already fired
+  enum { TIMER_NOTSET = -1, TIMER_CANCELLED = -2, TIMER_FIRED = -3 };
+
+  long prev_timer_;
+
+#if defined ACE_HAS_THREADS
+  ACE_Thread_Mutex  lock_;
+#endif /* ACE_HAS_THREADS */
+
 };
 
 class Dispatch_Count_Handler : public ACE_Event_Handler
