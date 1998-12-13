168d8a4700d0b0018af1550da64b4627cccffdba
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-13 22:49:59 +0000

    *** empty log message ***

--- trunk/TAO/orbsvcs/orbsvcs/CosEvent/EventChannel_i.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/CosEvent/EventChannel_i.cpp
@@ -15,19 +15,7 @@ EventChannel_i::EventChannel_i(const RtecEventChannelAdmin::ConsumerQOS &consume
 
 EventChannel_i::~EventChannel_i ()
 {
-  CORBA::Environment TAO_TRY_ENV;
-
-  // Deactivate the CosEventChannel
-  PortableServer::POA_var poa = this->_default_POA (TAO_TRY_ENV);
-
-  TAO_CHECK_ENV_RETURN_VOID (TAO_TRY_ENV);
-
-  PortableServer::ObjectId_var id = poa->servant_to_id (this, TAO_TRY_ENV);
-
-  TAO_CHECK_ENV_RETURN_VOID (TAO_TRY_ENV);
-
-  poa->deactivate_object (id.in (), TAO_TRY_ENV);
-  TAO_CHECK_ENV_RETURN_VOID (TAO_TRY_ENV);
+  //No-Op.
 }
 
 int
@@ -57,5 +45,17 @@ EventChannel_i::for_suppliers (CORBA::Environment &TAO_TRY_ENV)
 void
 EventChannel_i::destroy (CORBA::Environment &TAO_TRY_ENV)
 {
+  // Deactivate the CosEventChannel
+  PortableServer::POA_var poa = this->_default_POA (TAO_TRY_ENV);
+
+  TAO_CHECK_ENV_RETURN_VOID (TAO_TRY_ENV);
+
+  PortableServer::ObjectId_var id = poa->servant_to_id (this, TAO_TRY_ENV);
+
+  TAO_CHECK_ENV_RETURN_VOID (TAO_TRY_ENV);
+
+  poa->deactivate_object (id.in (), TAO_TRY_ENV);
+  TAO_CHECK_ENV_RETURN_VOID (TAO_TRY_ENV);
+
   delete this;
 }
