3a865b283ed91fe2a8a82703fb75326f175ba886
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-25 22:27:49 +0000

    *** empty log message ***

--- bin/Hash_Map_Manager.pl
+++ bin/Hash_Map_Manager.pl
@@ -8,16 +8,16 @@
 
 #
 # This script helps in fixing the template instantiations changes
-# required by the new design of ACE_Hash_Map_Manager.  
+# required by the new design of ACE_Hash_Map_Manager.
 #
-# Note that this script is not a 100% fool proof: 
+# Note that this script is not a 100% fool proof:
 #
 # (a) It uses commas to delimit the types in ACE_Hash_Map_Manager<X,
 # Y, Z>.  If X, Y, or Z are complicated types containing commas (e.g.,
 # T<a, b>), this script will not work.
 #
 # In the case where this script will not work, here are the cookbook,
-# do-it-yourself instructions: 
+# do-it-yourself instructions:
 #
 # ACE_Hash_Map_Entry<X, Y>
 #
@@ -72,7 +72,7 @@ $end_of_template_instantiation = ";";
 $pragma_instantiation = "#pragma instantiate";
 $end_of_pragma_instantiation = "";
 
-while ($line = <>) 
+while ($line = <>)
 {
     find_match ("ACE_Hash_Map_Entry");
     if ($match)
@@ -80,12 +80,12 @@ while ($line = <>)
         $template_parameters =~ /(.*),\s*(.*)/;
         $X = $1;
         $Y = $2;
-        
-        print "$instantiation ACE_Hash_Map_Entry<$template_parameters>$end_of_instantiation\n";           
+
+        print "$instantiation ACE_Hash_Map_Entry<$template_parameters>$end_of_instantiation\n";
         print "$instantiation ACE_Hash<$X>$end_of_instantiation\n";
         print "$instantiation ACE_Equal_To<$X>$end_of_instantiation\n";
         next;
-    }         
+    }
 
     add_ex ("ACE_Hash_Map_Manager", 1);
     add_ex ("ACE_Hash_Map_Iterator", 1);
@@ -98,17 +98,17 @@ while ($line = <>)
 sub find_match
 {
     $key = $_[0].'\s*<\s*(.*)>\s*';
-    if ($line =~ /$template_instantiation $key$end_of_template_instantiation$/) 
+    if ($line =~ /$template_instantiation $key$end_of_template_instantiation$/)
     {
         $match = 1;
-        $template_parameters = $1;        
+        $template_parameters = $1;
         $instantiation = $template_instantiation;
         $end_of_instantiation = $end_of_template_instantiation;
     }
-    elsif ($line =~ /$pragma_instantiation $key$end_of_pragma_instantiation$/) 
+    elsif ($line =~ /$pragma_instantiation $key$end_of_pragma_instantiation$/)
     {
         $match = 1;
-        $template_parameters = $1;        
+        $template_parameters = $1;
         $instantiation = $pragma_instantiation;
         $end_of_instantiation = $end_of_pragma_instantiation;
     }
@@ -128,13 +128,13 @@ sub add_ex
         $X = $1;
         $Y = $2;
         $Z = $3;
-        
+
         if ($add_original_line)
         {
-            print "$instantiation $_[0]<$template_parameters>$end_of_instantiation\n";           
+            print "$instantiation $_[0]<$template_parameters>$end_of_instantiation\n";
         }
 
-        print "$instantiation $_[0]_Ex<$X, $Y, ACE_Hash<$X>, ACE_Equal_To<$X>, $Z>$end_of_instantiation\n";           
+        print "$instantiation $_[0]_Ex<$X, $Y, ACE_Hash<$X>, ACE_Equal_To<$X>, $Z>$end_of_instantiation\n";
         next;
-    }         
+    }
 }
