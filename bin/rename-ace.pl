7f41ea7a678aa71ee7a35483de7dfa6c09b482b9
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-29 22:24:11 +0000

    use perl from user's path, so we don't have to hard-code its location

--- bin/rename-ace.pl
+++ bin/rename-ace.pl
@@ -1,4 +1,13 @@
-#!/pkg/gnu/bin/perl -pi
+# $Id$
+
+# The following three lines let this script run without specifying the
+# full path to perl, as long as it is in the user's PATH.
+# Taken from perlrun man page.
+
+eval '(exit $?0)' && eval 'exec perl -pi -S $0 ${1+"$@"}'
+    & eval 'exec perl -pi -S $0 $argv:q'
+    if 0;
+
 s/\bAcceptor\b/ACE_Acceptor/g;
 s/\bAddr\b/ACE_Addr/g;
 s/\bArgument_Vector\b/ACE_ARGV/g;
