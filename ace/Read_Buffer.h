edd5ae6a27c6ac14429bfbd978e1f757f755e85a
schmidt <douglascraigschmidt@users.noreply.github.com>
1998-12-29 05:14:28 +0000

    .

--- ace/Read_Buffer.h
+++ ace/Read_Buffer.h
@@ -94,8 +94,8 @@ private:
   // Pointer to the allocator.
 
   // = Disallow copying and assignment...
-  void operator= (const ACE_Read_Buffer &);
-  ACE_Read_Buffer (const ACE_Read_Buffer &);
+  ACE_UNIMPLEMENTED_FUNC (void operator= (const ACE_Read_Buffer &))
+  ACE_UNIMPLEMENTED_FUNC (ACE_Read_Buffer (const ACE_Read_Buffer &))
 };
 
 #if defined (__ACE_INLINE__)
