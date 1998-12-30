be2b51a141167b05a60a3d09b1e64efeb3da1ce8
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-30 08:38:15 +0000

    *** empty log message ***

---
+++ trunk/TAO/performance-tests/POA/Object_Creation_And_Registration/registration.cpp
@@ -0,0 +1,144 @@
+// $Id$
+
+// ================================================================
+//
+// = LIBRARY
+//    TAO/performance-tests/POA/Object_Creation_And_Registration
+//
+// = FILENAME
+//    registration.cpp
+//
+// = DESCRIPTION
+//
+//    This test is used to measure the time it takes to register and
+//    activate an object in the POA.  
+//
+// = AUTHOR
+//    Irfan Pyarali
+//
+// ================================================================
+
+#include "testS.h"
+#include "ace/Profile_Timer.h"
+
+#if defined (USING_PURIFY)
+#include "pure.h"
+#endif /* USING_PURIFY */
+
+class test_i : public POA_test
+  // = TITLE
+  //     Oversimplified servant class
+{
+};
+
+// Prototype for stat printing function
+void
+print_stats (ACE_Profile_Timer::ACE_Elapsed_Time &elapsed_time,
+             int iterations);
+
+int
+main (int argc, char **argv)
+{
+  // Default iterations
+  int iterations = 1000;
+
+  // Reset iterations if specified by the user.
+  if (argc > 1)
+    iterations = atoi (argv[1]);
+
+  // Initialize the ORB first.
+  CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0);
+
+  // Obtain the RootPOA.
+  CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
+
+  // Get the POA_var object from Object_var.
+  PortableServer::POA_var root_poa =
+    PortableServer::POA::_narrow (obj.in ());
+
+  // Create an array of servants
+  test_i *servants = new test_i[iterations];
+  
+  // Create an array of objects
+  test_var *objects = new test_var[iterations];
+
+  // Profile timer
+  ACE_Profile_Timer timer;
+  ACE_Profile_Timer::ACE_Elapsed_Time elapsed_time;
+      
+  // We start the profile timer here...
+  timer.start ();
+  
+#if defined (USING_PURIFY)
+  // Reset Quantify data recording; whatever happened in the past is
+  // not relevant to this test.
+  QuantifyClearData ();
+  QuantifyStartRecordingData ();
+#endif /* USING_PURIFY */
+
+  int i = 0;
+  for (i = 0; i < iterations; i++)
+    {
+      objects[i] = servants[i]._this ();
+    }
+
+#if defined (USING_PURIFY)
+  // Stop recording data here; whatever happens after this in the test
+  // is not relevant to this test.
+  QuantifyStopRecordingData ();
+#endif /* USING_PURIFY */
+
+  // stop the timer.
+  timer.stop ();
+  timer.elapsed_time (elapsed_time);
+      
+  // compute average time.
+  print_stats (elapsed_time, i);
+
+  // Cleanup
+  delete[] objects;
+  delete[] servants;
+  
+  // Destroy RootPOA. 
+  root_poa->destroy (1,
+                     1);
+
+  return 0;
+}
+
+//
+// Code for printing stats
+//
+void
+print_stats (ACE_Profile_Timer::ACE_Elapsed_Time &elapsed_time,
+             int iterations)
+{
+  if (iterations > 0)
+    {
+      elapsed_time.real_time *= ACE_ONE_SECOND_IN_MSECS;
+      elapsed_time.user_time *= ACE_ONE_SECOND_IN_MSECS;
+      elapsed_time.system_time *= ACE_ONE_SECOND_IN_MSECS;
+
+      elapsed_time.real_time /= iterations;
+      elapsed_time.user_time /= iterations;
+      elapsed_time.system_time /= iterations;
+
+      double tmp = 1000 / elapsed_time.real_time;
+
+      ACE_DEBUG ((LM_DEBUG,
+		  "\titerations\t = %d, \n"
+		  "\treal_time\t = %0.06f ms, \n"
+		  "\tuser_time\t = %0.06f ms, \n"
+		  "\tsystem_time\t = %0.06f ms, \n"
+		  "\t%0.00f calls/second\n",
+                  iterations,
+		  elapsed_time.real_time   < 0.0 ? 0.0 : elapsed_time.real_time,
+		  elapsed_time.user_time   < 0.0 ? 0.0 : elapsed_time.user_time,
+		  elapsed_time.system_time < 0.0 ? 0.0 : elapsed_time.system_time,
+		  tmp < 0.0 ? 0.0 : tmp));
+    }
+  else
+    ACE_ERROR ((LM_ERROR,
+		"\tNo time stats printed.  Zero iterations or error ocurred.\n"));
+}
+
