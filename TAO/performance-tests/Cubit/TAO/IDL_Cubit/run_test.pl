0912ee8a031fbb437f4f6141893274d745f6138a
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-30 04:52:23 +0000

    removed # at beginning of first line of script

--- TAO/performance-tests/Cubit/TAO/IDL_Cubit/run_test.pl
+++ TAO/performance-tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -1,8 +1,12 @@
-#$Id$
-# -*- perl -*-
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
     & eval 'exec perl -S $0 $argv:q'
     if 0;
+
+# $Id$
+
+# The first three lines above let this script run without specifying the
+# full path to perl, as long as it is in the user's PATH.
+# Taken from perlrun man page.
 
 # Do not use environment variables here since not all platforms use ACE_ROOT
 use lib "../../../../../bin";
