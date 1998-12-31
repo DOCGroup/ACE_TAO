517352c7eb1edc8c91754ef6166efa3c29327b18
vishal <vishal@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-31 18:25:21 +0000

    *** empty log message ***

--- TAO/orbsvcs/orbsvcs/Time/TAO_UTO.cpp
+++ TAO/orbsvcs/orbsvcs/Time/TAO_UTO.cpp
@@ -80,34 +80,42 @@ TAO_UTO::compare_time (CosTime::ComparisonType comparison_type,
 		       CosTime::UTO_ptr uto,
 		       CORBA::Environment &env)
 {
-  if (comparison_type == CosTime::MidC)
+  TAO_TRY
     {
-      if (this->time (env) == uto->time (env))
-	return CosTime::TCEqualTo;
-      else if (this->time (env) > uto->time (env))
-	return CosTime::TCGreaterThan;
+      if (comparison_type == CosTime::MidC)
+	{
+	  if (this->time (TAO_TRY_ENV) == uto->time (TAO_TRY_ENV))
+	    return CosTime::TCEqualTo;
+	  else if (this->time (TAO_TRY_ENV) > uto->time (TAO_TRY_ENV))
+	    return CosTime::TCGreaterThan;
+	  else
+	    return CosTime::TCLessThan;
+	}
+      else if (this->time (TAO_TRY_ENV) == uto->time (TAO_TRY_ENV))
+	{
+	  if (this->inaccuracy (TAO_TRY_ENV) == 0 && uto->inaccuracy (TAO_TRY_ENV) == 0)
+	    return CosTime::TCEqualTo;
+	}
       else
-	return CosTime::TCLessThan;
-    }
-  else if (this->time (env) == uto->time (env))
-    {
-      if (this->inaccuracy (env) == 0 && uto->inaccuracy (env) == 0)
-        return CosTime::TCEqualTo;
-    }
-  else
-    {
-      if (this->time (env) > uto->time (env))
-        {
-          if (this->time (env) - this->inaccuracy (env)
-              > uto->time (env) - uto->inaccuracy (env))
-            return CosTime::TCGreaterThan;
-        }
-      else if (this->time (env) + this->inaccuracy (env)
-               < uto->time (env) - uto->inaccuracy (env))
-        return CosTime::TCLessThan;
-    }
+	{
+	  if (this->time (TAO_TRY_ENV) > uto->time (TAO_TRY_ENV))
+	    {
+	      if (this->time (TAO_TRY_ENV) - this->inaccuracy (TAO_TRY_ENV)
+		  > uto->time (TAO_TRY_ENV) - uto->inaccuracy (TAO_TRY_ENV))
+		return CosTime::TCGreaterThan;
+	    }
+	  else if (this->time (TAO_TRY_ENV) + this->inaccuracy (TAO_TRY_ENV)
+		   < uto->time (TAO_TRY_ENV) - uto->inaccuracy (TAO_TRY_ENV))
+	    return CosTime::TCLessThan;
+	}
 
-  return CosTime::TCIndeterminate;
+      return CosTime::TCIndeterminate;
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Exception:");
+    }
+  TAO_ENDTRY;
 }
 
 // Returns a TIO representing the time interval between the time in
@@ -118,44 +126,63 @@ TAO_UTO::compare_time (CosTime::ComparisonType comparison_type,
 
 CosTime::TIO_ptr
 TAO_UTO::time_to_interval (CosTime::UTO_ptr uto,
-// @@ Vishal, please don't use _env, use TAO_TRY_ENV instead.  Please
-// apply this fix to ALL of your code.
 			   CORBA::Environment &_env)
 {
   TAO_TIO *tio = 0;
 
-  if (this->time (_env) > uto->time (_env))
-    ACE_NEW_THROW_RETURN (tio,
-			  TAO_TIO (uto->time (_env),
-				   this->time (_env)),
-			  CORBA::NO_MEMORY (CORBA::COMPLETED_NO),
-			  CosTime::TIO::_nil ());
-  else
-    ACE_NEW_THROW_RETURN (tio,
-			  TAO_TIO (this->time (_env),
-				   uto->time (_env)),
-			  CORBA::NO_MEMORY (CORBA::COMPLETED_NO),
-			  CosTime::TIO::_nil ());
-  return tio->_this ();
+  TAO_TRY
+    {
+      if (this->time (TAO_TRY_ENV) > uto->time (TAO_TRY_ENV))
+	ACE_NEW_THROW_RETURN (tio,
+			      TAO_TIO (uto->time (TAO_TRY_ENV),
+				       this->time (TAO_TRY_ENV)),
+			      CORBA::NO_MEMORY (CORBA::COMPLETED_NO),
+			      CosTime::TIO::_nil ());
+      else
+	ACE_NEW_THROW_RETURN (tio,
+			      TAO_TIO (this->time (TAO_TRY_ENV),
+				       uto->time (TAO_TRY_ENV)),
+			      CORBA::NO_MEMORY (CORBA::COMPLETED_NO),
+			      CosTime::TIO::_nil ());
+      TAO_CHECK_ENV;
+      return tio->_this ();
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Exception:");
+    }
+  TAO_ENDTRY;
 }
 
-// Returns a TIO object representing the error interval around the
-// time value in the UTO.
+  // Returns a TIO object representing the error interval around the
+  // time value in the UTO.
 
 CosTime::TIO_ptr
 TAO_UTO::interval (CORBA::Environment &_env)
 {
   TAO_TIO *tio = 0;
 
-  TimeBase::TimeT lower =
-    this->time (_env) - this->inaccuracy (_env);
-  TimeBase::TimeT upper =
-    this->time (_env) + this->inaccuracy (_env);
+  TAO_TRY
+    {
+      TimeBase::TimeT lower =
+	this->time (TAO_TRY_ENV) - this->inaccuracy (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
 
-  ACE_NEW_THROW_RETURN (tio,
-			TAO_TIO (lower,
-				 upper),
-			CORBA::NO_MEMORY (CORBA::COMPLETED_NO),
-			CosTime::TIO::_nil ());
-  return tio->_this ();
+      TimeBase::TimeT upper =
+	this->time (TAO_TRY_ENV) + this->inaccuracy (TAO_TRY_ENV);
+      TAO_CHECK_ENV;
+
+      ACE_NEW_THROW_RETURN (tio,
+			    TAO_TIO (lower,
+				     upper),
+			    CORBA::NO_MEMORY (CORBA::COMPLETED_NO),
+			    CosTime::TIO::_nil ());
+      TAO_CHECK_ENV;
+      return tio->_this ();
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception ("Exception:");
+    }
+  TAO_ENDTRY;
 };
