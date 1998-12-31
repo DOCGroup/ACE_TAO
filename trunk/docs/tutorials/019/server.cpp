a365bff4c3d184cfc0c393b60bea6f5e177d94e5
jcej <jcej@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1998-12-31 18:47:57 +0000

    *** empty log message ***

--- trunk/docs/tutorials/019/server.cpp
+++ trunk/docs/tutorials/019/server.cpp
@@ -8,7 +8,7 @@
  */
 #include "shmem.h"
 
-int main (int, char *[])
+int main (int, char *argv[])
 {
         /*
           You can use the ACE_Malloc template to create memory pools
@@ -27,7 +27,7 @@ int main (int, char *[])
           after starting this app to see that the segment is created.
           (I can't for the life of me correlate the SHM_KEY value back 
           to the key/id reported by ipcs though.)
-         */
+        */
     ACE_Shared_Memory_SV shm_server (SHM_KEY, SHMSZ,
                                      ACE_Shared_Memory_SV::ACE_CREATE);
 
@@ -37,20 +37,34 @@ int main (int, char *[])
           can pass a value to malloc() but it will be silently
           igored.)  The void* (cast to char*) that is returned will
           point to the beginning of the shared segment.
-         */
+        */
     char *shm = (char *) shm_server.malloc ();
+
+        /*
+          Since we're asking to create the segment, we will fail if it 
+          already exists.  We could fall back and simply attach to it
+          like the client but I'd rather not assume it was a previous
+          instance of this app that left the segment around.
+         */
+    if( ! shm )
+    {
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "%p\n\t(%P|%t) Cannot create shared memory segment.\n"
+                           "\tUse 'ipcs' to see if it already exists\n", 
+                           argv[0]),100);
+    }
 
         /*
           This second pointer will be used to walk through the block
           of memory...
-         */
+        */
     char *s   = shm;
 
         /*
           Out of curiosity, I added this output message.  The tests
           I've done so far show me the same address for client and
           server.  What does your OS tell you?
-         */
+        */
     ACE_DEBUG ((LM_INFO, "(%P|%t) Shared Memory is at 0x%x\n",
                 shm ));
 
@@ -58,7 +72,7 @@ int main (int, char *[])
           At this point, our application can use the pointer just like 
           any other given to us by new or malloc.  For our purposes,
           we'll copy in the alpabet as a null-terminated string.
-         */
+        */
     for (char c = 'a'; c <= 'z'; c++)
         *s++ = c;
 
@@ -69,13 +83,13 @@ int main (int, char *[])
           (or anyone else) to change the first byte in the shared area 
           to a '*' character.  This is where you would rather use
           semaphores or some similar "resource light" approach.
-         */
+        */
     while (*shm != '*')
         ACE_OS::sleep (1);
 
         /*
           Let's see what the client did to the segment...
-         */
+        */
     for (char *s = shm; *s != '\0'; s++)
     {
         putchar (*s);
@@ -93,7 +107,7 @@ int main (int, char *[])
           The free() method may be tempting but it doesn't actually do 
           anything.  If your app is *really* done with the shared
           memory then use either close() or remove().
-         */
+        */
     if (shm_server.remove () < 0)
         ACE_ERROR ((LM_ERROR, "%p\n", "remove"));
 
