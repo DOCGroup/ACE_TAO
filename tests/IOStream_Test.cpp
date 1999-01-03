55147c5ba6dc403aaf7428fb7720175d3161d80b
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-01-03 21:08:23 +0000

    .

--- tests/IOStream_Test.cpp
+++ tests/IOStream_Test.cpp
@@ -24,12 +24,12 @@
 #include "ace/SOCK_Acceptor.h"
 #include "ace/IOStream.h"
 
-ACE_RCSID(tests, IOStream_Test, "$Id$")
+ACE_RCSID (tests, IOStream_Test, "$Id$")
 
-#if defined(__BORLANDC__) && __BORLANDC__ >= 0x0530
-USELIB("..\ace\aced.lib");
+#if defined (__BORLANDC__) && __BORLANDC__ >= 0x0530
+USELIB ("..\ace\aced.lib");
 //---------------------------------------------------------------------------
-#endif /* defined(__BORLANDC__) && __BORLANDC__ >= 0x0530 */
+#endif /* defined (__BORLANDC__) && __BORLANDC__ >= 0x0530 */
 
 #if !defined (ACE_LACKS_ACE_IOSTREAM)
 
@@ -49,7 +49,6 @@ typedef ACE_IOStream<ACE_SOCK_Stream> ACE_SOCK_IOStream;
    If you're stuck with an array of characters then you
    will probably need somthing like I have below.
 
-
    On the other hand, one of the biggest advantages to an
    iostream is that it eats up whitespace :-)
 
@@ -57,7 +56,6 @@ typedef ACE_IOStream<ACE_SOCK_Stream> ACE_SOCK_IOStream;
    with any number of whitespace between the data then
    you can easily get (>>) the data from the iostream
    without having to worry about delimeters and such.
-
 
    The main thing to keep in mind when using an iostream
    between peers is that you MUST keep the data "fields"
@@ -76,7 +74,7 @@ class qchar
 public:
   qchar (void) { c_ = '\0'; }
 
-  qchar (char c) : c_(c) { };
+  qchar (char c) : c_ (c) { };
 
   operator char () const { return c_; }
 
@@ -104,7 +102,7 @@ operator>> (ACE_SOCK_IOStream & stream, qchar *buf)
 
   *buf = '\0';  // Initialize the string
 
-  stream.get(c);
+  stream.get (c);
 
   if (!stream) // eat space up to the first char
     return stream;
@@ -113,21 +111,21 @@ operator>> (ACE_SOCK_IOStream & stream, qchar *buf)
   if (c != '"')
     for (*buf++ = c;
 #ifdef CHORUS
-         stream.get(c) && !isspace(c);
+         stream.get (c) && !isspace (c);
 #else
-         (void *) stream.get(c) && !isspace(c);
-#endif
+ (void *) stream.get (c) && !isspace (c);
+#endif /* CHORUS */
          *buf++ = c)
       continue;
   else
 #ifdef CHORUS
-    for (; stream.get(c) && c != '"'; *buf++ = c)
+    for (; stream.get (c) && c != '"'; *buf++ = c)
 #else
-    for (; (void *) stream.get(c) && c != '"'; *buf++ = c)
-#endif
+    for (; (void *) stream.get (c) && c != '"'; *buf++ = c)
+#endif /* CHORUS */
       if (c == '\\')
         {
-          stream.get(c);
+          stream.get (c);
           if (c != '"')
             *buf++ = '\\';
         }
@@ -174,8 +172,8 @@ client (void *arg = 0)
 
   // We don't _need_ to dynamically allocate the ACE_SOCK_IOStream.
   // But if we don't, it doesn't get destroyed on some platforms,
-  // e.g., g++ 2.7.2.1 and Sun C++ 4.2 on Solaris 2.5.1.  (It does
-  // work on Linux, so the code seems fine.)  If we manage the storage
+  // e.g., g++ 2.7.2.1 and Sun C++ 4.2 on Solaris 2.5.1. (It does work
+  // on Linux, so the code seems fine.)  If we manage the storage
   // ourselves, we _will_ destroy it at the end of this function.
   ACE_SOCK_IOStream server;
 
@@ -186,15 +184,16 @@ client (void *arg = 0)
 
   if (connector.connect (server, addr) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
-                       "(%t) %p\n", "Failed to connect to server thread"),
+                       " (%t) %p\n",
+                       "Failed to connect to server thread"),
                       0);
 
   // Send a string to the server which it can interpret as a qchar[]
   const char *str = "\"This is a test     string.\"";
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Client Sending:  (%s)\n", str));
+  ACE_DEBUG ((LM_DEBUG, " (%P|%t) Client Sending: (%s)\n", str));
   server << str << endl;
 
-  // Allow the server to get the string and echo it to the user.  (The
+  // Allow the server to get the string and echo it to the user. (The
   // iostream doesn't need this, but humans do :)
   ACE_OS::sleep (2);
 
@@ -203,7 +202,9 @@ client (void *arg = 0)
   // the spaces sent by the client.
 
   str = "\"THIS IS A     TEST STRING.\"";
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Client Sending:  (%s)\n", str));
+  ACE_DEBUG ((LM_DEBUG,
+              " (%P|%t) Client Sending: (%s)\n",
+              str));
   server << str << endl;
 
   // Again, give the server time to display the happenings to the
@@ -214,7 +215,8 @@ client (void *arg = 0)
   // iostream will pull them out by using the whitespace provided by
   // the server.
 
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Client Receiving\n"));
+  ACE_DEBUG ((LM_DEBUG,
+              " (%P|%t) Client Receiving\n"));
 
   ACE_Time_Value timeout (2);
   ACE_Time_Value *timeoutp = &timeout;
@@ -226,17 +228,19 @@ client (void *arg = 0)
   long l;
   double d;
 
-  while (!(server >> i))
+  while (! (server >> i))
     {
       int eof = server.eof ();
       if (eof)
         {
-          ACE_DEBUG ((LM_DEBUG, "(%P|%t) Unrecoverable stream error/eof\n"));
+          ACE_DEBUG ((LM_DEBUG,
+                      " (%P|%t) Unrecoverable stream error/eof\n"));
           break;
         }
       else
         {
-          ACE_DEBUG ((LM_DEBUG, "(%P|%t) Recoverable stream error/timed out)\n"));
+          ACE_DEBUG ((LM_DEBUG,
+                      " (%P|%t) Recoverable stream error/timed out)\n"));
           server.clear (0);
         }
     }
@@ -247,12 +251,16 @@ client (void *arg = 0)
   server >> d;
 
   ACE_DEBUG ((LM_DEBUG,
-              "(%P|%t) Client Received: int %d float %f long %d float %f double %f\n",
-              i, f1, (int) l, f2, d));
+              " (%P|%t) Client Received: int %d float %f long %d float %f double %f\n",
+              i,
+              f1,
+              (int) l,
+              f2,
+              d));
 
   // Check for proper received values.
   ACE_ASSERT (i == 1  && (f1 >= 0.123420 && f1 <= 0.123422)
-              && l == 666555444  &&  (f2 >= 23.44 && f2 <= 23.46)
+              && l == 666555444  && (f2 >= 23.44 && f2 <= 23.46)
               && (d >= -47.1e+9 && d <= -45.9e+9));
   // Reset the precision to limit ourselves to two significant digits.
   server.precision (2);
@@ -279,29 +287,34 @@ server (void *arg = 0)
 {
   // We don't _need_ to dynamically allocate the ACE_SOCK_IOStream.
   // But if we don't, it doesn't get destroyed on some platforms,
-  // e.g., g++ 2.7.2.1 and Sun C++ 4.2 on Solaris 2.5.1.  (It does
-  // work on Linux, so the code seems fine.)  If we manage the storage
+  // e.g., g++ 2.7.2.1 and Sun C++ 4.2 on Solaris 2.5.1. (It does work
+  // on Linux, so the code seems fine.)  If we manage the storage
   // ourselves, we _will_ destroy it at the end of this function.
   ACE_SOCK_IOStream client_handler;
 
   ACE_INET_Addr server_addr;
-  ACE_SOCK_Acceptor *acceptor = (ACE_SOCK_Acceptor *) arg;
+  ACE_SOCK_Acceptor *acceptor =
+    ACE_reinterpret_cast (ACE_SOCK_Acceptor *, arg);
 
   if (acceptor->get_local_addr (server_addr) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "get_local_addr"), 0);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "get_local_addr"),
+                      0);
 
 #if defined (ACE_HAS_THREADS)
   if (ACE_Thread_Manager::instance ()->spawn (ACE_THR_FUNC (client),
-                                             (void *) &server_addr,
-                                             THR_NEW_LWP | THR_DETACHED) == -1)
+                                              (void *) &server_addr,
+                                              THR_NEW_LWP | THR_DETACHED) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
-                       "(%t) %p\n", "spawing client thread"),
+                       " (%t) %p\n",
+                       "spawing client thread"),
                       0);
 #endif /* ACE_HAS_THREADS */
 
   if (acceptor->accept (client_handler) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
-                       "(%P|%t) Failed to accept new client_handler"),
+                       " (%P|%t) Failed to accept new client_handler"),
                       0);
 
   // Read a qbuf[] from the client.  Notice that all of the client's
@@ -311,7 +324,7 @@ server (void *arg = 0)
   client_handler >> qbuf;
 
   ACE_DEBUG ((LM_DEBUG,
-              "(%P|%t) Server Received: (\"%s\")\n",
+              " (%P|%t) Server Received: (\"%s\")\n",
               qbuf));
 
   // Give the client time to announce the next test to the user.
@@ -321,48 +334,57 @@ server (void *arg = 0)
   // Compared to the method above, this is quite messy.  Notice also
   // that whitespace is lost.
 
-#if defined(ACE_HAS_STRING_CLASS) && defined (ACE_HAS_STANDARD_CPP_LIBRARY)
+#if defined (ACE_HAS_STRING_CLASS) && defined (ACE_HAS_STANDARD_CPP_LIBRARY)
   ACE_IOStream_String buf;
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Server Received: ("));
+  ACE_DEBUG ((LM_DEBUG,
+              " (%P|%t) Server Received: ("));
 
   while (client_handler &&
-         (buf.length() == 0 || buf[buf.length() - 1] != '"'))
+ (buf.length () == 0 || buf[buf.length () - 1] != '"'))
     {
-      if (!(client_handler >> buf))
+      if (! (client_handler >> buf))
         break;
 
-      if (buf.length() > 0)
-        ACE_DEBUG ((LM_DEBUG, "%s ", buf.c_str()));
+      if (buf.length () > 0)
+        ACE_DEBUG ((LM_DEBUG,
+                    "%s ",
+                    buf.c_str ()));
     }
 
-  ACE_DEBUG ((LM_DEBUG, ")\n"));
-
+  ACE_DEBUG ((LM_DEBUG,
+              ")\n"));
 #else
   char buf[BUFSIZ];
   ACE_OS::memset (buf, 0, sizeof buf);
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Server Received: ("));
+  ACE_DEBUG ((LM_DEBUG,
+              " (%P|%t) Server Received: ("));
 
   while (ACE_OS::strlen (buf) == 0
          || buf[ACE_OS::strlen (buf) - 1] != '"')
     {
-      if (!(client_handler >> buf))
+      if (! (client_handler >> buf))
         break;
-      ACE_DEBUG ((LM_DEBUG, "%s ", buf));
+      ACE_DEBUG ((LM_DEBUG,
+                  "%s ",
+                  buf));
     }
 
-  ACE_DEBUG ((LM_DEBUG, ")\n"));
+  ACE_DEBUG ((LM_DEBUG,
+              ")\n"));
 #endif /* ACE_HAS_STRING_CLASS */
 
   // Send some non-textual data to the client.  We use a single
   // character to separate the fields but we could have used any valid
   // whitespace.  The data will be sent if the iostream's buffer gets
-  // filled or when we flush it with an explicit client.sync() command
-  // or the implicit <<endl.
+  // filled or when we flush it with an explicit client.sync ()
+  // command or the implicit <<endl.
 
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Server sleeping\n"));
+  ACE_DEBUG ((LM_DEBUG,
+              " (%P|%t) Server sleeping\n"));
   ACE_OS::sleep (5);
 
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Server Sending:  1 .12342134 666555444 23.45 -46.5e9 \n"));
+  ACE_DEBUG ((LM_DEBUG,
+              " (%P|%t) Server Sending:  1 .12342134 666555444 23.45 -46.5e9 \n"));
   client_handler << 1 << " ";
   client_handler << .12342134 << " ";
   client_handler << 666555444 << " ";
@@ -372,7 +394,7 @@ server (void *arg = 0)
 
   // The client will have changed the sign of each data field and sent
   // 'em all back to us.  At the same time, the client used the
-  // precision() function to change the significant digits for
+  // precision () function to change the significant digits for
   // non-integer values.
   int i;
   float f1, f2;
@@ -381,12 +403,16 @@ server (void *arg = 0)
   client_handler >> i >> f1 >> l >> f2 >> d;
 
   ACE_DEBUG ((LM_DEBUG,
-              "(%P|%t) Server Received: int %d float %g long %d float %g double %g\n",
-              i, f1, (int) l, f2, d));
+              " (%P|%t) Server Received: int %d float %g long %d float %g double %g\n",
+              i,
+              f1,
+              (int) l,
+              f2,
+              d));
 
   // check for proper received values
   ACE_ASSERT (i == -1  && (f1 >= -0.13 && f1 <= -0.11)
-              && l == -666555444  &&  (f2 >= -24.0 && f2 <= -22.0)
+              && l == -666555444  && (f2 >= -24.0 && f2 <= -22.0)
               && (d >= 45e+9 && d <= 47e+9));
 
   client_handler.close ();
@@ -400,8 +426,11 @@ spawn (void)
   // Acceptor;
   ACE_SOCK_Acceptor acceptor;
 
-  if (acceptor.open ((const ACE_INET_Addr &) ACE_Addr::sap_any) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "(%P|%t) %p\n", "open"), -1);
+  if (acceptor.open (ACE_sap_any_cast (const ACE_INET_Addr &)) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       " (%P|%t) %p\n",
+                       "open"),
+                      -1);
 #if defined (ACE_HAS_THREADS)
   else if (ACE_Thread_Manager::instance ()->spawn (ACE_THR_FUNC (server),
                                                   &acceptor,
@@ -418,7 +447,9 @@ spawn (void)
   switch (ACE_OS::fork ("child"))
     {
     case -1:
-      ACE_ERROR ((LM_ERROR, "%p\n%a", "fork failed"));
+      ACE_ERROR ((LM_ERROR,
+                  "%p\n%a",
+                  "fork failed"));
       ACE_OS::_exit (-1);
     case 0: // In child
       {
@@ -426,7 +457,9 @@ spawn (void)
         ACE_INET_Addr server_addr;
 
         if (acceptor.get_local_addr (server_addr) == -1)
-          ACE_ERROR ((LM_ERROR, "%p\n", "get_local_addr"));
+          ACE_ERROR ((LM_ERROR,
+                      "%p\n",
+                      "get_local_addr"));
         else
           client ((void *) &server_addr);
         ACE_END_LOG;
@@ -460,7 +493,8 @@ main (int, char *[])
   ACE_INIT_LOG ("IOStream_Test-children");
   spawn ();
 #else
-  ACE_ERROR ((LM_INFO, "ACE_IOSTREAM not supported on this platform\n"));
+  ACE_ERROR ((LM_INFO,
+              "ACE_IOSTREAM not supported on this platform\n"));
 #endif /* !ACE_LACKS_ACE_IOSTREAM */
   ACE_END_TEST;
   return 0;
