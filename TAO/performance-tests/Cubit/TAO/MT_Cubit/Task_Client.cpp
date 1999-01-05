c893b8fcbd088e83fd9065cef6e7ba90d19e8ae1
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-01-05 21:51:46 +0000

    Removed explicit call to open_services on Win32 Static build

--- TAO/performance-tests/Cubit/TAO/MT_Cubit/Task_Client.cpp
+++ TAO/performance-tests/Cubit/TAO/MT_Cubit/Task_Client.cpp
@@ -580,21 +580,6 @@ Client::init_orb (void)
   int argc = tmp_args2.argc ();
   char **argv = tmp_args2.argv ();
 
-#if defined (ACE_WIN32) && (ACE_HAS_DLL == 0)
-  static char* rfactory[] =  { "-ORBresources", "tss",
-                               "-ORBreactorlock", "null" };
-
-  static char* cli_args[] = { 0 };
-  static char* svr_args[] = { "-ORBconcurrency", "thread-per-connection",
-                              "-ORBdemuxstrategy", "dynamic",
-                              "-ORBtablesize", "128" };
-
-  TAO_Internal::open_services
-    (sizeof rfactory / sizeof rfactory[0], rfactory,
-     sizeof cli_args / sizeof cli_args[0], cli_args,
-     sizeof svr_args / sizeof svr_args[0], svr_args);
-#endif /* ACE_WIN32 && ACE_HAS_DLL == 0 */
-
   this->orb_ = CORBA::ORB_init (argc,
                          argv,
                          "internet",
