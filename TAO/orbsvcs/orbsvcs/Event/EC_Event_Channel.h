06e6748e280cb9e4b30c70a1bce26289000f617f
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-01-06 14:18:47 +0000

    More iterations on the interfaces

--- TAO/orbsvcs/orbsvcs/Event/EC_Event_Channel.h
+++ TAO/orbsvcs/orbsvcs/Event/EC_Event_Channel.h
@@ -41,14 +41,68 @@ public:
   virtual ~TAO_EC_Event_Channel (void);
   // destructor
 
+  virtual void activate (CORBA::Environment& env);
+  // Create all the objects, connect them, start the internal threads
+  // (if any), etc.
+  // After this call the EC can be used.
+
+  virtual void shutdown (CORBA::Environment &env);
+  // Shutdown any internal threads, cleanup all the internal
+  // structures, flush all the messages, etc.
+
+  TAO_EC_Dispatching* dispatching (void) const;
+  // Access the dispatching module....
+
+  TAO_EC_Filter_Builder* filter_builder (void) const;
+  // Access the filter builder....
+
+  TAO_EC_ConsumerAdmin* consumer_admin (void) const;
+  // Access the consumer admin implementation, useful for controlling
+  // the activation...
+
+  TAO_EC_SupplierAdmin* supplier_admin (void) const;
+  // Access the supplier admin implementation, useful for controlling
+  // the activation...
+
+  TAO_EC_Timer_Module* timer_module (void) const;
+  // Access the timer module...
+
   // = The RtecEventChannelAdmin::EventChannel methods...
   virtual RtecEventChannelAdmin::ConsumerAdmin_ptr
       for_consumers (CORBA::Environment& env);
+  // The default implementation is:
+  //    this->consumer_admin ()->_this (env);
+
   virtual RtecEventChannelAdmin::SupplierAdmin_ptr
       for_suppliers (CORBA::Environment& env);
+  // The default implementation is:
+  //    this->supplier_admin ()->_this (env);
+
+  virtual void destroy (CORBA::Environment &env);
+  RtecEventChannelAdmin::Observer_Handle
+      append_observer (RtecEventChannelAdmin::Observer_ptr,
+		       CORBA::Environment &env);
+  void remove_objserver (RtecEventChannelAdmin::Observer_Handle,
+			 CORBA::Environment &env);
+  // @@ Do we need to strategize this also????? How???
 
 private:
-  TAO_EC_Factory* factory_;
+  TAO_EC_Factory *factory_;
+  // This is the abstract factory that creates all the objects that
+  // compose an event channel, the event channel simply acts as a
+  // Mediator among them.
+
+  TAO_EC_Dispatching *dispatching_;
+  // The dispatching "module"...
+
+  TAO_EC_ConsumerAdmin *consumer_admin_;
+  // The ConsumerAdmin implementation....
+
+  TAO_EC_SupplierAdmin *supplier_admin_;
+  // The SupplierAdmin implementation....
+
+  TAO_EC_TimerModule *timer_module_;
+  // The TimerModule...
 };
 
 #if defined (__ACE_INLINE__)
