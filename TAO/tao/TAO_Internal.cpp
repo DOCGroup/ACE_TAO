e41359dad83f395022dd4b87df3919eab7373fd4
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-01-06 05:01:32 +0000

    *** empty log message ***

--- TAO/tao/TAO_Internal.cpp
+++ TAO/tao/TAO_Internal.cpp
@@ -23,23 +23,23 @@ TAO_Internal::open_services (int& argc, char** argv)
   ACE_MT (ACE_GUARD_RETURN (ACE_SYNCH_RECURSIVE_MUTEX, guard, *ACE_Static_Object_Lock::instance (), -1));
 
   if (TAO_Internal::service_open_count_++ == 0)
-#if defined (TAO_USES_STATIC_SERVICE) || defined (TAO_PLATFORM_SVC_CONF_FILE_NOTSUP)
     {
+#if defined (TAO_USES_STATIC_SERVICE) || defined (TAO_PLATFORM_SVC_CONF_FILE_NOTSUP)
       int retv = ACE_Service_Config::open (argc, argv,
                                            ACE_DEFAULT_LOGGER_KEY, 0);
-#if defined (TAO_PLATFORM_SVC_CONF_FILE_NOTSUP)
+# if defined (TAO_PLATFORM_SVC_CONF_FILE_NOTSUP)
       ACE_Service_Config::process_directive
         (TAO_DEFAULT_RESOURCE_FACTORY_ARGS);
       ACE_Service_Config::process_directive
         (TAO_DEFAULT_CLIENT_STRATEGY_FACTORY_ARGS);
       ACE_Service_Config::process_directive
         (TAO_DEFAULT_SERVER_STRATEGY_FACTORY_ARGS);
-#endif /* TAO_PLATFORM_SVC_CONF_FILE_NOTSUP */
+# endif /* TAO_PLATFORM_SVC_CONF_FILE_NOTSUP */
       return retv;
-    }
 #else
-    return ACE_Service_Config::open (argc, argv);
+      return ACE_Service_Config::open (argc, argv);
 #endif /* TAO_USES_STATIC_SERVICE */
+    }
   else
     return 0;
 }
