b786a12556e412aba3337092a5bb18d023479078
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-01-07 10:50:13 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-4_6_12'.

---
+++ tags/ACE-4_6_12/ace/SOCK_Connector.cpp
@@ -0,0 +1,157 @@
+// SOCK_Connector.cpp
+// $Id$
+
+#define ACE_BUILD_DLL
+#include "ace/SOCK_Connector.h"
+#include "ace/Handle_Set.h"
+#include "ace/INET_Addr.h"
+
+#if defined (ACE_LACKS_INLINE_FUNCTIONS)
+#include "ace/SOCK_Connector.i"
+#endif /* ACE_LACKS_INLINE_FUNCTIONS */
+
+ACE_RCSID(ace, SOCK_Connector, "$Id$")
+
+ACE_ALLOC_HOOK_DEFINE(ACE_SOCK_Connector)
+
+void
+ACE_SOCK_Connector::dump (void) const
+{
+  ACE_TRACE ("ACE_SOCK_Connector::dump");
+}
+
+// Actively connect and produce a new ACE_SOCK_Stream if things go well...
+
+int
+ACE_SOCK_Connector::connect (ACE_SOCK_Stream &new_stream, 
+			     const ACE_Addr &remote_sap, 
+			     ACE_Time_Value *timeout,
+			     const ACE_Addr &local_sap,
+			     int reuse_addr,
+			     int /* flags */,
+			     int /* perms */,
+			     int protocol_family, 
+			     int protocol)
+{
+  ACE_TRACE ("ACE_SOCK_Connector::connect");
+  int result = 0;
+
+  // Only open a new socket if we don't already have a valid handle.
+  if (new_stream.get_handle () == ACE_INVALID_HANDLE
+      && new_stream.open (SOCK_STREAM,
+			  protocol_family, 
+			  protocol,
+			  reuse_addr) == -1)
+    return -1;
+
+  sockaddr *raddr = (sockaddr *) remote_sap.get_addr ();
+  size_t rsize = remote_sap.get_size ();
+
+  if (local_sap != ACE_Addr::sap_any)
+    {
+      sockaddr *laddr = (sockaddr *) local_sap.get_addr ();
+      size_t size = local_sap.get_size ();
+      result = ACE_OS::bind (new_stream.get_handle (),
+                             laddr,
+                             size);
+
+      if (result == -1)
+	{
+	  new_stream.close ();
+	  return -1;
+	}
+    }
+
+  // Enable non-blocking, if required.
+  if (timeout != 0)
+    {
+      if (new_stream.enable (ACE_NONBLOCK) == -1)
+	result = -1;
+      
+      if (ACE_OS::connect (new_stream.get_handle (), raddr, rsize) == -1)
+	{
+	  result = -1;
+
+	  // Check whether the connection is in progress.
+	  if (errno == EINPROGRESS || errno == EWOULDBLOCK)
+	    {
+	      // This expression checks if we were polling.
+	      if (timeout->sec () == 0 && timeout->usec () == 0)
+		errno = EWOULDBLOCK;
+	      // Wait synchronously
+	      else if (this->complete (new_stream, 0, timeout) != -1)
+		return 0;
+	    }
+	}
+    }
+  // Do a blocking connect.
+  else if (ACE_OS::connect (new_stream.get_handle (), raddr, rsize) == -1)
+    result = -1;
+
+  // EISCONN is treated specially since this routine may be used to
+  // check if we are already connected.
+  if (result != -1 || errno == EISCONN)
+    // Start out with non-blocking disabled on the <new_stream>.
+    new_stream.disable (ACE_NONBLOCK);
+  else if (!(errno == EWOULDBLOCK || errno == ETIMEDOUT))
+    {
+      // If things have gone wrong, close down and return an error.
+      int saved_errno = errno;
+      new_stream.close ();
+      errno = saved_errno;
+    }
+    
+  return result;
+}
+
+// Try to complete a non-blocking connection.
+
+int
+ACE_SOCK_Connector::complete (ACE_SOCK_Stream &new_stream,
+			      ACE_Addr *remote_sap,
+			      ACE_Time_Value *tv)
+{
+  ACE_TRACE ("ACE_SOCK_Connector::complete");
+#if defined (ACE_HAS_BROKEN_NON_BLOCKING_CONNECTS)
+  // Win32 has a timing problem - if you check to see if the
+  // connection has completed too fast, it will fail - so wait 1
+  // millisecond to let it catch up.
+  ACE_Time_Value time (0, ACE_NON_BLOCKING_BUG_DELAY);
+  ACE_OS::sleep (time);
+#endif /* ACE_HAS_BROKEN_NON_BLOCKING_CONNECTS */
+  ACE_HANDLE h = ACE::handle_timed_complete (new_stream.get_handle (), tv);
+
+  if (h == ACE_INVALID_HANDLE)
+    {
+      // Preserve the value of errno across the close() call.
+      int error = errno;
+      new_stream.close ();
+      errno = error;
+      return -1;
+    }
+  else 	  // We've successfully connected!
+    {
+      if (remote_sap != 0)
+	{
+	  int len;
+
+	  len = remote_sap->get_size ();
+	  sockaddr *addr = (sockaddr *) remote_sap->get_addr ();
+
+	  if (ACE_OS::getpeername (h, addr, &len) == -1)
+	    {
+	      // Preserve the value of errno across the close() call.
+	      int error = errno;
+	      new_stream.close ();
+	      errno = error;
+	      return -1;
+	    }
+	}
+
+      // Start out with non-blocking disabled on the <new_stream>.
+      new_stream.disable (ACE_NONBLOCK);
+      return 0;
+    }
+}
+
+
---
+++ tags/ACE-4_6_12/ace/config-irix5.3-sgic++.h
@@ -0,0 +1,121 @@
+/* -*- C++ -*- */
+// $Id$
+
+// The following configuration file is designed to work
+// for the SGI Indigo2EX running Irix 5.3 platform using
+// the SGI C++ Compiler.
+
+#ifndef ACE_CONFIG_H
+#define ACE_CONFIG_H
+
+#define IRIX5
+
+#include <sys/bsd_types.h>
+#define _BSD_TYPES
+
+#define ACE_SIZEOF_LONG_DOUBLE 8
+
+#define ACE_LACKS_SYSTIME_H
+// Optimize ACE_Handle_Set for select().
+#define ACE_HAS_HANDLE_SET_OPTIMIZED_FOR_SELECT
+
+// Platform supports getpagesize() call.
+#define ACE_HAS_GETPAGESIZE
+
+#define ACE_LACKS_SYSTIME_H
+#define ACE_HAS_SIGWAIT
+
+// Platform supports System V IPC (most versions of UNIX, but not Win32)
+#define ACE_HAS_SYSV_IPC
+
+// Platform requires void * for mmap().
+#define ACE_HAS_VOIDPTR_MMAP
+
+// Platform supports recvmsg and sendmsg.
+#define ACE_HAS_MSG
+
+// Compiler/platform contains the <sys/syscall.h> file.
+#define ACE_HAS_SYSCALL_H
+
+// Compiler/platform supports alloca()
+#define ACE_HAS_ALLOCA
+
+// Compiler/platform has <alloca.h>
+#define ACE_HAS_ALLOCA_H
+
+// IRIX5 needs to define bzero() in this odd file <bstring.h>
+#define ACE_HAS_BSTRING
+
+// Compiler/platform has the getrusage() system call.
+#define ACE_HAS_GETRUSAGE
+
+// Platform supports POSIX O_NONBLOCK semantics.
+#define ACE_HAS_POSIX_NONBLOCK
+
+// Platform supports POSIX timers via timestruc_t.
+#define ACE_HAS_POSIX_TIME
+
+// Compiler/platform has correctly prototyped header files.
+#define ACE_HAS_CPLUSPLUS_HEADERS
+
+// Platform contains <poll.h>.
+#define ACE_HAS_POLL
+
+// No multi-threading so use poll() call
+// - for easier debugging, if nothing else
+// #define ACE_USE_POLL
+
+// Platform supports the /proc file system.
+// #define ACE_HAS_PROC_FS
+
+// Compiler/platform defines the sig_atomic_t typedef.
+#define ACE_HAS_SIG_ATOMIC_T
+
+// Platform supports SVR4 extended signals.
+#define ACE_HAS_SIGINFO_T
+// #define ACE_HAS_UCONTEXT_T
+#define ACE_LACKS_UCONTEXT_H
+
+// Compiler supports the ssize_t typedef.
+#define ACE_HAS_SSIZE_T
+
+// Platform supports STREAMS.
+#define ACE_HAS_STREAMS
+
+// Platform supports STREAM pipes (note that this is disabled by
+// default, see the manual page on pipe(2) to find out how to enable
+// it).
+// #define ACE_HAS_STREAM_PIPES
+
+// Compiler/platform supports strerror ().
+#define ACE_HAS_STRERROR
+
+// Compiler/platform supports struct strbuf.
+#define ACE_HAS_STRBUF_T
+
+// Compiler/platform supports SVR4 dynamic linking semantics.
+#define ACE_HAS_SVR4_DYNAMIC_LINKING
+
+// Compiler/platform supports SVR4 signal typedef.
+#define ACE_HAS_IRIX_53_SIGNALS
+
+// Compiler/platform supports sys_siglist array.
+// #define ACE_HAS_SYS_SIGLIST
+
+// Platform provides <sys/filio.h> header.
+#define ACE_HAS_SYS_FILIO_H
+
+// Compiler/platform defines a union semun for SysV shared memory.
+#define ACE_HAS_SEMUN
+
+// Platform supports IP multicast
+#define ACE_HAS_IP_MULTICAST
+
+#define ACE_NEEDS_DEV_IO_CONVERSION
+
+// Turns off the tracing feature.
+#if !defined (ACE_NTRACE)
+# define ACE_NTRACE 1
+#endif /* ACE_NTRACE */
+
+#endif /* ACE_CONFIG_H */
