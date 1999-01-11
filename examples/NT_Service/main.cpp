010a9a19897cd0e9840ce75665ba1b229305bc49
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-01-11 18:57:50 +0000

    .

--- examples/NT_Service/main.cpp
+++ examples/NT_Service/main.cpp
@@ -9,29 +9,31 @@
 //    main.cpp
 //
 // = DESCRIPTION
-//    This is the main program - it just hands control off to the process
-//    instance to figure out what to do.
+//    This is the main program - it just hands control off to the
+//    process instance to figure out what to do.  This program only
+//    runs on Win32.
 //
 // = AUTHOR
-//    Gonzalo Diethelm and Steve Huston
+//    Gonzalo Diethelm <gonzo@cs.wustl.edu> 
+//    and Steve Huston <shuston@riverace.com>
 //
 // ============================================================================
 
 #include "ace/Get_Opt.h"
 #include "ntsvc.h"
 
-
 class Process
 {
 public:
-  Process();
-  ~Process();
+  Process (void);
+  ~Process (void);
 
   int run(int argc, char* argv[]);
 
 private:
-  void parse_args(int argc, char* argv[]);
-  void print_usage_and_die();
+  void parse_args (int argc,
+                   char* argv[]);
+  void print_usage_and_die (void);
 
 private:
   char progname[128];
@@ -48,51 +50,54 @@ private:
 
 typedef ACE_Singleton<Process, ACE_Mutex> PROCESS;
 
-
-
-
-Process::Process()
-: opt_install(0),
-  opt_remove(0),
-  opt_start(0),
-  opt_kill(0),
-  opt_type(0),
-  opt_debug(0),
-  opt_startup(0)
+Process::Process (void)
+  : opt_install (0),
+    opt_remove (0),
+    opt_start (0),
+    opt_kill (0),
+    opt_type (0),
+    opt_debug (0),
+    opt_startup (0)
 {
-  ACE_OS::strcpy(progname, "service");
-  ACE::init();
+  ACE_OS::strcpy (progname,
+                  "service");
+  ACE::init ();
 }
 
-Process::~Process()
+Process::~Process (void)
 {
-  ACE::fini();
+  ACE::fini ();
 }
 
-void Process::print_usage_and_die()
+void 
+Process::print_usage_and_die (void)
 {
-  ACE_DEBUG((LM_INFO, "Usage: %s"
-             " -in -r -s -k -tn -d\n"
-             "  -i: Install this program as an NT service, with specified startup\n"
-             "  -r: Remove this program from the Service Manager\n"
-             "  -s: Start the service\n"
-             "  -k: Kill the service\n"
-             "  -t: Set startup for an existing service\n"
-             "  -d: Debug; run as a regular application\n",
-             progname));
-  ACE_OS::exit(0);
+  ACE_DEBUG ((LM_INFO,
+              "Usage: %s"
+              " -in -r -s -k -tn -d\n%a"
+              "  -i: Install this program as an NT service, with specified startup\n"
+              "  -r: Remove this program from the Service Manager\n"
+              "  -s: Start the service\n"
+              "  -k: Kill the service\n"
+              "  -t: Set startup for an existing service\n"
+              "  -d: Debug; run as a regular application\n",
+              progname,
+              0));
+  /* NOTREACHED */
 }
 
-void Process::parse_args(int argc, char* argv[])
+void 
+Process::parse_args (int argc, char* argv[])
 {
-  ACE_Get_Opt get_opt(argc, argv, "i:rskt:d");
+  ACE_Get_Opt get_opt (argc, argv, "i:rskt:d");
   int c; 
-  while ((c = get_opt()) != -1)
+
+  while ((c = get_opt ()) != -1)
     switch (c)
     {
     case 'i':
       opt_install = 1;
-      opt_startup = ACE_OS::atoi(get_opt.optarg);
+      opt_startup = ACE_OS::atoi (get_opt.optarg);
       break;
     case 'r':
       opt_remove = 1;
@@ -105,94 +110,85 @@ void Process::parse_args(int argc, char* argv[])
       break;
     case 't':
       opt_type = 1;
-      opt_startup = ACE_OS::atoi(get_opt.optarg);
+      opt_startup = ACE_OS::atoi (get_opt.optarg);
       break;
     case 'd':
       opt_debug = 1;
       break;
     default:
-      print_usage_and_die();
+      print_usage_and_die ();
       break;
   }
 }
 
 // Define a function to handle Ctrl+C to cleanly shut this down.
-static BOOL __stdcall ConsoleHandler(DWORD ctrlType)
+
+static BOOL __stdcall
+ConsoleHandler (DWORD ctrlType)
 {
-  SERVICE::instance()->handle_control(SERVICE_CONTROL_STOP);
+  SERVICE::instance ()->handle_control (SERVICE_CONTROL_STOP);
   return TRUE;
 }
 
-ACE_NT_SERVICE_DEFINE(Beeper, Service, "Annoying Beeper Service")
+ACE_NT_SERVICE_DEFINE (Beeper,
+                       Service,
+                       "Annoying Beeper Service");
 
-int Process::run(int argc, char* argv[])
+int 
+Process::run (int argc, char* argv[])
 {
-  SERVICE::instance()->name("Beeper", "Annoying Beeper Service");
+  SERVICE::instance ()->name ("Beeper",
+                              "Annoying Beeper Service");
 
-  parse_args(argc, argv);
+  parse_args (argc, argv);
 
-  if (opt_install && ! opt_remove)
-	{
-    if (opt_startup > 0)
+  if (opt_install && !opt_remove)
     {
-      return SERVICE::instance()->insert(opt_startup);
+      if (opt_startup > 0)
+        return SERVICE::instance ()->insert (opt_startup);
+      else
+        return SERVICE::instance ()->insert ();
     }
-    else
-    {
-      return SERVICE::instance()->insert();
-    }
-	}
 
-  if (opt_remove && ! opt_install)
-	{
-	  return SERVICE::instance()->remove();
-	}
+  if (opt_remove && !opt_install)
+    return SERVICE::instance ()->remove ();
 
   if (opt_start && opt_kill)
-  {
-    print_usage_and_die();
-  }
+    print_usage_and_die ();
 
   if (opt_start)
-  {
-    return SERVICE::instance()->start_svc();
-  }
+    return SERVICE::instance ()->start_svc ();
 
   if (opt_kill)
-  {
-    return SERVICE::instance()->stop_svc();
-  }
+    return SERVICE::instance ()->stop_svc ();
 
   if (opt_type)
-  {
-    return SERVICE::instance()->startup(opt_startup);
-  }
+    return SERVICE::instance ()->startup (opt_startup);
 
-
-  // If we get here, we either run the app in debug mode
-  // (-d) or are being called from the service manager
-  // to start the service.
-
+  // If we get here, we either run the app in debug mode (-d) or are
+  // being called from the service manager to start the service.
 
   if (opt_debug)
-  {
-    SetConsoleCtrlHandler(&ConsoleHandler, 1);
-    SERVICE::instance()->svc();
-  }
-  else
-  {
-    ACE_NT_SERVICE_RUN(Beeper, SERVICE::instance(), ret)
-    if (ret == 0)
     {
-      ACE_ERROR((LM_ERROR, "%p\n", "Couldn't start service"));
+      SetConsoleCtrlHandler (&ConsoleHandler, 1);
+      SERVICE::instance ()->svc ();
     }
-  }
+  else
+    {
+      ACE_NT_SERVICE_RUN (Beeper,
+                          SERVICE::instance (),
+                          ret);
+      if (ret == 0)
+        ACE_ERROR ((LM_ERROR,
+                    "%p\n",
+                    "Couldn't start service"));
+    }
   
   return 0;
 }
 
-
-int main(int argc, char* argv[])
+int 
+main (int argc, char* argv[])
 {
-  return PROCESS::instance()->run(argc, argv);
+  return PROCESS::instance ()->run (argc, argv);
 }
--- examples/NT_Service/ntsvc.cpp
+++ examples/NT_Service/ntsvc.cpp
@@ -13,88 +13,91 @@
 //      seconds until the service is stopped.
 //
 // = AUTHOR
-//    Gonzalo Diethelm and Steve Huston
+//    Gonzalo Diethelm <gonzo@cs.wustl.edu> 
+//    and Steve Huston <shuston@riverace.com>
 //
 // ============================================================================
 
 #include "ace/OS.h"
 #include "ace/Reactor.h"
-
 #include "ntsvc.h"
 
-Service::Service()
+Service::Service (void)
 {
-
   // Remember the Reactor instance.
-  reactor(ACE_Reactor::instance());
+  reactor (ACE_Reactor::instance ());
 
   // Schedule a timer every two seconds.
-  ACE_Time_Value tv(2, 0);
-  ACE_Reactor::instance()->schedule_timer(this, 0, tv, tv);
+  ACE_Time_Value tv (2, 0);
+  ACE_Reactor::instance ()->schedule_timer (this, 0, tv, tv);
 }
 
-
 // This method is called when the service gets a control request.  It
-// handles requests for stop and shutdown by calling terminate().  All
-// others get handled by calling up to inherited::handle_control.
-void Service::handle_control(DWORD control_code)
-{
+// handles requests for stop and shutdown by calling terminate ().
+// All others get handled by calling up to inherited::handle_control.
 
-  if (control_code == SERVICE_CONTROL_SHUTDOWN ||
-      control_code == SERVICE_CONTROL_STOP)
+void 
+Service::handle_control (DWORD control_code)
+{
+  if (control_code == SERVICE_CONTROL_SHUTDOWN 
+      || control_code == SERVICE_CONTROL_STOP)
     {
-      report_status(SERVICE_STOP_PENDING);
-      ACE_DEBUG((LM_INFO, "Service control stop requested\n"));
+      report_status (SERVICE_STOP_PENDING);
+
+      ACE_DEBUG ((LM_INFO,
+                  "Service control stop requested\n"));
       stop_ = 1;
-      reactor()->notify(this, ACE_Event_Handler::EXCEPT_MASK);
+      reactor ()->notify (this,
+                          ACE_Event_Handler::EXCEPT_MASK);
     }
   else
-    {
-      inherited::handle_control(control_code);
-    }
+    inherited::handle_control (control_code);
 }
 
 // This is just here to be the target of the notify from above... it
 // doesn't do anything except aid on popping the reactor off its wait
 // and causing a drop out of handle_events.
-int Service::handle_exception(ACE_HANDLE)
+
+int 
+Service::handle_exception (ACE_HANDLE)
 {
   return 0;
 }
 
+// Beep every two seconds.  This is what this NT service does...
 
-// Beep every two seconds.
-// This is what this NT service does...
-int Service::handle_timeout(const ACE_Time_Value& tv,
-			                      const void*)
+int 
+Service::handle_timeout (const ACE_Time_Value &tv,
+                         const void *)
 {
-  MessageBeep(MB_OK);
+  MessageBeep (MB_OK);
   return 0;
 }
 
 // This is the main processing function for the Service.  It sets up
 // the initial configuration and runs the event loop until a shutdown
 // request is received.
-int Service::svc()
+
+int 
+Service::svc (void)
 {
-  ACE_DEBUG((LM_DEBUG, "Service::svc\n"));
+  ACE_DEBUG ((LM_DEBUG,
+              "Service::svc\n"));
 
   // As an NT service, we come in here in a different thread than the
   // one which created the reactor.  So in order to do anything, we
   // need to own the reactor. If we are not a service, report_status
   // will return -1.
-  if (report_status(SERVICE_RUNNING) == 0)
-    {
-      reactor()->owner(ACE_Thread::self());
-    }
+  if (report_status (SERVICE_RUNNING) == 0)
+    reactor ()->owner (ACE_Thread::self ());
 
   stop_ = 0;
-  while (! stop_)
-    {
-      reactor()->handle_events();
-    }
+
+  while (!stop_)
+    reactor ()->handle_events ();
 
   // Cleanly terminate connections, terminate threads.
-  ACE_DEBUG((LM_DEBUG, "Shutting down\n"));
+  ACE_DEBUG ((LM_DEBUG,
+              "Shutting down\n"));
   return 0;
 }
