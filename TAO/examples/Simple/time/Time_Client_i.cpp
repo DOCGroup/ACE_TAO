43ef97220dd8dc5208b12a2699b674cc11964383
bala <balanatarajan@users.noreply.github.com>
1999-01-16 00:22:05 +0000

    Interface file that acts as place for invoking the RMI's

---
+++ TAO/examples/Simple/time/Time_Client_i.cpp
@@ -0,0 +1,58 @@
+//$Id$
+#include "Time_Client_i.h"
+
+// This is the interface program that 
+// accesses the remote object 
+
+// Constructor.
+Time_Client_i::Time_Client_i( void)
+{
+  //no-op
+}
+
+//Destructor.
+Time_Client_i::~Time_Client_i( void)
+{
+  //no-op
+}
+
+
+int
+Time_Client_i::run (int argc,
+                    char** argv)
+{
+  // Instantiation of template with the
+  // interface object and its _var object
+  Client<Time, Time_var> client;
+
+  // Initialize the client
+  if (client.init (argc, argv) == -1)
+    return -1;
+
+  TAO_TRY 
+    {
+      //Make the RMI
+      CORBA::Long timedate = client->time (TAO_TRY_ENV);
+      
+      // Print out value
+      char *ascii_timedate =
+        ACE_OS::ctime (ACE_reinterpret_cast (time_t *,
+                                             &timedate));
+          
+      ACE_DEBUG ((LM_DEBUG,
+                  "string time is %s\n",
+                  ascii_timedate));
+    
+      if ( client.shutdown () == 1)
+        client->shutdown (TAO_TRY_ENV);
+    }
+  TAO_CATCHANY
+    {
+      TAO_TRY_ENV.print_exception("\tException");
+    }
+  TAO_ENDTRY;
+
+  return 0;
+}
+
+
