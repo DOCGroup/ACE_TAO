e2ef436caff1bc113f98eeb3c67b563905bb0162
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-01 19:57:33 +0000

    ChangeLogTag:Mon Feb  1 13:49:11 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- ace/config-linux-common.h
+++ ace/config-linux-common.h
@@ -172,6 +172,10 @@
 
 # define ACE_UINT64_FORMAT_SPECIFIER "%Lu"
 
+#if !defined (ACE_TIMER_SKEW)
+# define ACE_TIMER_SKEW 10 * 1000
+#endif /* ACE_TIMER_SKEW */
+
 // Turns off the tracing feature.
 #if !defined (ACE_NTRACE)
 # define ACE_NTRACE 1
