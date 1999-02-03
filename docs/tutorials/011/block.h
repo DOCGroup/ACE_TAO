cf2c6ec9f55dc12585f3d377b748818446f38e1b
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-02-03 01:27:30 +0000

    .

--- docs/tutorials/011/block.h
+++ docs/tutorials/011/block.h
@@ -1,4 +1,3 @@
-
 // $Id$
 
 #ifndef BLOCK_H
@@ -15,19 +14,25 @@ class Block : public ACE_Message_Block
 public:
   Block (void)
   {
-    ACE_DEBUG ((LM_DEBUG, "(%P|%t) Block ctor 0x%x\n", (void *) this));
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) Block ctor 0x%x\n",
+                (void *) this));
   }
 
   Block (size_t size)
-  : ACE_Message_Block (size)
+    : ACE_Message_Block (size)
   {
-    ACE_DEBUG ((LM_DEBUG, "(%P|%t) Block ctor 0x%x\n", (void *) this));
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) Block ctor 0x%x\n",
+                (void *) this));
   }
 
-  virtual ~ Block (void)
+  virtual ~Block (void)
   {
-    ACE_DEBUG ((LM_DEBUG, "(%P|%t) Block dtor 0x%x\n", (void *) this));
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) Block dtor 0x%x\n",
+                (void *) this));
   }
 };
 
-#endif
+#endif /* BLOCK_H */
