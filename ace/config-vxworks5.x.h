3103b1308a3c99dd931fd16a9ba93d86b1f8825d
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-05 16:10:53 +0000

    changed ENSURE_32_BITS to LM_ENSURE_32_BITS, and its value from 0xffffffff to 0x7fffffff

--- ace/config-vxworks5.x.h
+++ ace/config-vxworks5.x.h
@@ -61,7 +61,6 @@
 #   define ACE_HAS_STANDARD_CPP_LIBRARY 1
 # endif /* __STANDARD_CXX */
 
-# define ACE_HAS_BROKEN_ENUMS /* Necessary with 1.8.9, harmless with 1.8.8. */
 # define ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA
 # define ACE_HAS_WCHAR_TYPEDEFS_CHAR
 # define ACE_LACKS_LONGLONG_T
