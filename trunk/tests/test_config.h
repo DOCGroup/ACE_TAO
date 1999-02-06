a34df7073c7cad8123900689a9a9d6361f629def
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-06 13:48:16 +0000

    (~ACE_Test_Output): wrapped use of cerr with #ifndef ACE_LACKS_IOSTREAM_TOTALLY.

--- trunk/tests/test_config.h
+++ trunk/tests/test_config.h
@@ -229,14 +229,16 @@ ACE_Test_Output::ACE_Test_Output (void)
 
 ACE_Test_Output::~ACE_Test_Output (void)
 {
-#if !defined (ACE_PSOS)
+#if !defined (ACE_LACKS_IOSTREAM_TOTALLY) && !defined (ACE_PSOS)
   ACE_LOG_MSG->msg_ostream (&cerr);
-#endif /* !defined (ACE_PSOS) */
+#endif /* ! ACE_LACKS_IOSTREAM_TOTALLY && ! ACE_PSOS */
+
   ACE_LOG_MSG->clr_flags (ACE_Log_Msg::OSTREAM);
   ACE_LOG_MSG->set_flags (ACE_Log_Msg::STDERR);
+
 #if !defined (ACE_LACKS_IOSTREAM_TOTALLY)
   delete this->output_file_;
-#endif /* ACE_LACKS_IOSTREAM_TOTALLY */
+#endif /* ! ACE_LACKS_IOSTREAM_TOTALLY */
 }
 
 int
