09d77509dbb5f0e7a24e8964a2ead5fee5ef3f46
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-15 19:18:27 +0000

    4.6.22

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (6u)
-#define ACE_BETA_VERSION (21u)
-#define ACE_VERSION "4.6.21\0"
+#define ACE_BETA_VERSION (22u)
+#define ACE_VERSION "4.6.22\0"
