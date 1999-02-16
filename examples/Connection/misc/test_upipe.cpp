cbef364f1d1f1a053dd05fa758efedde41a9b294
Steve Huston <shuston@riverace.com>
1999-02-16 23:57:48 +0000

    Moved class defs needed for auto template instantiate to new file, test_upipe.h

--- examples/Connection/misc/test_upipe.cpp
+++ examples/Connection/misc/test_upipe.cpp
@@ -15,35 +15,7 @@ ACE_RCSID(misc, test_upipe, "$Id$")
 
 #if defined (ACE_HAS_THREADS)
 
-typedef ACE_Svc_Handler <ACE_UPIPE_STREAM, ACE_NULL_SYNCH> SVC_HANDLER;
-
-class Server_Service : public SVC_HANDLER
-  // = TITLE
-  //     Defines the interface for a service that recvs data from its
-  //     client and writes the data to its stdout.
-{
-public:
-  Server_Service (ACE_Thread_Manager * = 0) {}
-
-  virtual int open (void *)
-    {
-      ACE_TRACE ("Server_Service::open");
-      return 0;
-    }
-
-  virtual int svc (void)
-    {
-      ACE_TRACE ("Server_Service::svc");
-
-      char buf[BUFSIZ];
-      ssize_t n;
-
-      while ((n = this->peer ().recv (buf, sizeof buf)) > 0)
-	::write (1, buf, n);
-
-      return 0;
-    }
-};
+#include "test_upipe.h"
 
 class Server : public ACE_Strategy_Acceptor <Server_Service, ACE_UPIPE_ACCEPTOR>
   // = TITLE
@@ -86,38 +58,6 @@ private:
 
   ACE_Thread_Strategy<Server_Service> thr_strategy_;
   // Our concurrency strategy.
-};
-
-class Client_Service : public SVC_HANDLER
-  // = TITLE
-  //     Defines the interface for a service that recvs data from its
-  //     stdin and forward the data to its server.
-{
-public:
-  Client_Service (ACE_Thread_Manager *thr_mgr = 0)
-    : SVC_HANDLER (thr_mgr)
-    {
-      ACE_TRACE ("Client_Service::Client_Service");
-    }
-
-  virtual int open (void *)
-    {
-      ACE_TRACE ("Client_Service::open");
-      return this->activate (THR_DETACHED | THR_NEW_LWP);
-    }
-
-  virtual int svc (void)
-    {
-      ACE_TRACE ("Client_Service::svc");
-      char buf[BUFSIZ];
-      ssize_t n;
-
-      while ((n = ACE_OS::read (ACE_STDIN, buf, sizeof buf)) > 0)
-	this->peer ().send (buf, n);
-
-      this->peer ().close ();
-      return 0;
-    }
 };
 
 class Client : public ACE_Connector <Client_Service, ACE_UPIPE_CONNECTOR>
