44d83c414e1037313b65f681643d807fa185755c
Steve Huston <shuston@riverace.com>
1999-02-17 00:20:00 +0000

    Moved Connection_Handler class def to new file to build ok on AIX xlC.

--- examples/Connection/misc/Connection_Handler.cpp
+++ examples/Connection/misc/Connection_Handler.cpp
@@ -21,40 +21,9 @@
 #include "ace/Service_Config.h"
 #include "ace/Thread.h"
 
+#include "Connection_Handler.h"
+
 ACE_RCSID(misc, Connection_Handler, "$Id$")
-
-class Connection_Handler : public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH>
-{
-public:
-  virtual int open (void *);
-  // Initialize the <Connection_Handler> and make it an Active Object.
-
-  virtual int close (u_long);
-  // Terminate the <Connection_Handler>.
-
-  virtual int svc (void);
-  // Run the <Connection_Handler>'s main event loop.
-
-protected:
-  virtual int handle_close (ACE_HANDLE,
-			    ACE_Reactor_Mask);
-  // Signal the Active Object to stop when called.
-
-  virtual int handle_input (ACE_HANDLE);
-  // Handle input from the client.
-
-  virtual int handle_timeout (const ACE_Time_Value &tv,
-			      const void *arg);
-  // Handle timeouts.
-
-  virtual int handle_signal (int signum,
-			     siginfo_t *,
-			     ucontext_t *);
-  // Handle timeouts.
-
-  sig_atomic_t finished_;
-  // Keeps track of whether we're done.
-};
 
 int
 Connection_Handler::open (void *)
