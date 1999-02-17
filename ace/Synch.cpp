6b47dc52296a07cde3e0d02be736b77901521936
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-17 21:21:06 +0000

    Silly CE fix.

--- ace/Synch.cpp
+++ ace/Synch.cpp
@@ -15,11 +15,11 @@
 #include "ace/Synch.h"
 #include "ace/Object_Manager.h"
 
+ACE_RCSID(ace, Synch, "$Id$")
+
 #if !defined (__ACE_INLINE__)
 #include "ace/Synch.i"
 #endif /* __ACE_INLINE__ */
-
-ACE_RCSID(ace, Synch, "$Id$")
 
 ACE_ALLOC_HOOK_DEFINE(ACE_Null_Mutex)
 ACE_ALLOC_HOOK_DEFINE(ACE_File_Lock)
