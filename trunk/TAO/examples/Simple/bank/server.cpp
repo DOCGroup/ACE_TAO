9176f8e7f1c3282bbb18c202885098032d5f94cd
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-21 02:58:57 +0000

    *** empty log message ***

--- trunk/TAO/examples/Simple/bank/server.cpp
+++ trunk/TAO/examples/Simple/bank/server.cpp
@@ -1,45 +1,50 @@
 // $Id$
+#include "../Simple_util.h"
+#include "AccountManager_i.h"
 
-#include "Server_i.h"
-
-ACE_RCSID(Time, server, "$Id$")
-
-// This is the main driver program for the Bank server.
+// This is the main driver program for the time and date server.
 
 int
 main (int argc, char *argv[])
 {
-  Server_i server;
+  Server<AccountManager_i> server;
 
   ACE_DEBUG ((LM_DEBUG,
-              "[SERVER] Process/Thread Id : (%P/%t) Bank server\n"));
+              "\n\tBank server\n\n"));
 
-  TAO_TRY
+  ACE_TRY_NEW_ENV
     {
-      if (server.init (argc, argv, TAO_TRY_ENV) == -1)
-        {
-	  TAO_CHECK_ENV;
-	  return 1;
-	}
+      if (server.init ("Bank",
+                       argc,
+                       argv,
+                       ACE_TRY_ENV) == -1)
+        return 1;
       else
         {
-          server.run (TAO_TRY_ENV);
-          TAO_CHECK_ENV;
+          server.run (ACE_TRY_ENV);
+          ACE_TRY_CHECK;
         }
     }
-  TAO_CATCH (CORBA::SystemException, sysex)
+  ACE_CATCH (CORBA::SystemException, sysex)
     {
       ACE_UNUSED_ARG (sysex);
-      TAO_TRY_ENV.print_exception ("System Exception");
+      ACE_TRY_ENV.print_exception ("System Exception");
       return -1;
     }
-  TAO_CATCH (CORBA::UserException, userex)
+  ACE_CATCH (CORBA::UserException, userex)
     {
       ACE_UNUSED_ARG (userex);
-      TAO_TRY_ENV.print_exception ("User Exception");
+      ACE_TRY_ENV.print_exception ("User Exception");
       return -1;
     }
-  TAO_ENDTRY;
+  ACE_ENDTRY;
 
   return 0;
 }
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class Server<Account_Manager_i>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate Server<Account_Manager_i>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
