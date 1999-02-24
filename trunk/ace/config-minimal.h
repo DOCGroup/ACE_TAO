683b9393449e996ad4207b64f22aa65bb04f20aa
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-24 19:53:54 +0000

    changed #include protection to ACE_CONFIG_MINIMAL_H, so that this config can be #included by others

--- trunk/ace/config-minimal.h
+++ trunk/ace/config-minimal.h
@@ -4,9 +4,9 @@
 // This configuration file is designed to build only the minimal
 // ACE_OS adaptation layer.
 
-#ifndef ACE_CONFIG_H
-#define ACE_CONFIG_H
+#ifndef ACE_CONFIG_MINIMAL_H
+#define ACE_CONFIG_MINIMAL_H
 
 #define ACE_HAS_MINIMAL_ACE_OS
 
-#endif /* ACE_CONFIG_H */
+#endif /* ACE_CONFIG_MINIMAL_H */
