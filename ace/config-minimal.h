7bc380b15619608b14df34fbb089521d72c70ef2
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-24 03:53:06 +0000

    added ACE_HAS_MINIMAL_ACE_OS support

---
+++ ace/config-minimal.h
@@ -0,0 +1,12 @@
+/* -*- C++ -*- */
+// $Id$
+
+// The following configuration file is designed to be used to
+// build only the minimal ACE_OS adaptation layer.
+
+#ifndef ACE_CONFIG_H
+#define ACE_CONFIG_H
+
+#define ACE_HAS_MINIMAL_ACE_OS
+
+#endif /* ACE_CONFIG_H */
