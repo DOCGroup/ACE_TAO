f238840372252bd1061961cdf35687d4d0b13692
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-26 00:30:35 +0000

    *** empty log message ***

--- examples/Reactor/WFMO_Reactor/test_prerun_state_changes.cpp
+++ examples/Reactor/WFMO_Reactor/test_prerun_state_changes.cpp
@@ -4,7 +4,7 @@
 //
 // = LIBRARY
 //    examples
-// 
+//
 // = FILENAME
 //    test_prerun_state_changes.cpp
 //
@@ -12,11 +12,11 @@
 //
 //    Tests the Reactor's ability to handle state changes before
 //    getting a chance to run.
-//    
+//
 // = AUTHOR
-//    
+//
 //    Irfan Pyarali
-// 
+//
 // ============================================================================
 
 #include "ace/Reactor.h"
@@ -31,8 +31,8 @@ class Event_Handler : public ACE_Event_Handler
 public:
   virtual int handle_close (ACE_HANDLE handle, ACE_Reactor_Mask mask)
     {
-      ACE_DEBUG ((LM_DEBUG, 
-                  "event handler %d closed.\n", 
+      ACE_DEBUG ((LM_DEBUG,
+                  "event handler %d closed.\n",
                   (int) handle));
       delete this;
       return 0;
@@ -45,17 +45,17 @@ main (int argc, char *argv[])
   ACE_HANDLE handle = (ACE_HANDLE) ::socket (PF_INET, SOCK_STREAM, 0);
 
   Event_Handler *event_handler = new Event_Handler;
-  
-  ACE_ASSERT (ACE_Reactor::instance ()->register_handler (handle, 
-                                                          event_handler, 
+
+  ACE_ASSERT (ACE_Reactor::instance ()->register_handler (handle,
+                                                          event_handler,
                                                           ACE_Event_Handler::READ_MASK) == 0);
-  
-  ACE_ASSERT (ACE_Reactor::instance ()->register_handler (handle, 
-                                                          event_handler, 
+
+  ACE_ASSERT (ACE_Reactor::instance ()->register_handler (handle,
+                                                          event_handler,
                                                           ACE_Event_Handler::WRITE_MASK | ACE_Event_Handler::QOS_MASK) == 0);
-  
-  ACE_ASSERT (ACE_Reactor::instance ()->remove_handler (handle, 
-                                                        ACE_Event_Handler::READ_MASK) == 0);
+
+  ACE_ASSERT (ACE_Reactor::instance ()->remove_handler (handle,
+                                                        ACE_Event_Handler::READ_MASK | ACE_Event_Handler::DONT_CALL) == 0);
 
   return 0;
 }
