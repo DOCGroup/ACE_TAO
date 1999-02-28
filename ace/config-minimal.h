8f6a7887920b557e95b5768fc38b93e5fb9a6ace
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-02-28 20:32:21 +0000

    disable ACE_ASSERT, ACE_DEBUG, and ACE_ERROR

--- ace/config-minimal.h
+++ ace/config-minimal.h
@@ -9,4 +9,19 @@
 
 #define ACE_HAS_MINIMAL_ACE_OS
 
+#if defined (ACE_ASSERT)
+# undef ACE_ASSERT
+#endif /* ACE_ASSERT */
+#define ACE_ASSERT(x)
+
+#if defined (ACE_DEBUG)
+# undef ACE_DEBUG
+#endif /* ACE_DEBUG */
+#define ACE_DEBUG(x)
+
+#if defined (ACE_ERROR)
+# undef ACE_ERROR
+#endif /* ACE_ERROR */
+#define ACE_ERROR(x)
+
 #endif /* ACE_CONFIG_MINIMAL_H */
