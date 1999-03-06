bf12b014fb71038aad8350a32b5ca49afab00f4c
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-06 02:33:27 +0000

    *** empty log message ***

--- TAO/orbsvcs/tests/CosEC_Basic/CosEC_Basic.h
+++ TAO/orbsvcs/tests/CosEC_Basic/CosEC_Basic.h
@@ -82,7 +82,7 @@ private:
   RtecScheduler::Scheduler_var scheduler_;
   // Reference to the Scheduler after activating it in the ORB.
 
-  ACE_EventChannel ec_impl_;
+  ACE_EventChannel *ec_impl_;
   // The servant object of the Rtec.
 
   RtecEventChannelAdmin::EventChannel_var rtec_;
