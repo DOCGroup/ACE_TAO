04500f0f70496686c9c316c169f1648e06427cfc
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-12 22:24:16 +0000

    Added the new Nestea Bookshelf test.

---
+++ TAO/orbsvcs/tests/ImplRepo/nestea_server_i.h
@@ -0,0 +1,80 @@
+// -*- C++ -*-
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    TAO/orbsvcs/tests/ImplRepo
+//
+// = FILENAME
+//    nestea_server_i.h
+//
+// = DESCRIPTION
+//    Server that sets up the ORB and handles the registration and execution
+//    of the Paper Nestea Server.
+//
+// = AUTHORS
+//    Darrell Brunsch <brunsch@cs.wustl.edu>
+//
+// ============================================================================
+
+#if !defined (NESTEA_SERVER_I_H)
+#define NESTEA_SERVER_I_H
+
+#include "tao/TAO.h"
+#include "nestea_i.h"
+#include "IR_Helper.h"
+
+class Nestea_Server_i
+{
+  // = TITLE
+  //   Paper Nestea Server Implementation Class
+  //
+  // = DESCRIPTION
+  //   Sets up everything necessary to get the Paper Nestea Server 
+  //   running.
+  //   
+public:
+  // = Initialization and termination methods.
+  Nestea_Server_i (void);
+  // Default constructor
+
+  ~Nestea_Server_i (void);
+  // Destructor
+
+  int init (int argc, char **argv, CORBA::Environment& env);
+  // Initialize the Server state - parsing arguments and waiting
+
+  int run (CORBA::Environment& env);
+  // Run the orb 
+
+private:
+  int parse_args (void);
+  // Parses the commandline arguments.
+
+  int argc_;
+  // Number of command line arguments.
+
+  char **argv_;
+  // The command line arguments.
+
+  TAO_ORB_Manager orb_manager_;
+  // The ORB manager.
+
+  Nestea_i *server_impl_;
+  // The Paper Nestea Server Implementation.  
+
+  FILE *ior_output_file_;
+  // File where the IOR of the server object is stored.
+
+  IR_Helper *ir_helper_;
+  // Helper class for using the IR.
+
+  int register_with_ir_;
+  // Flag for whether or not to register startup with the IR.
+
+  int use_ir_;
+  // Flag for whether or not to use the IR.
+};
+
+#endif /* NESTEA_SERVER_I_H */
