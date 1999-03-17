576293a3f7d404a93bce187e40aa9ae9a64e7ca3
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-17 14:31:11 +0000

    4.6.29

--- trunk/ace/Version.h
+++ trunk/ace/Version.h
@@ -3,5 +3,5 @@
 
 #define ACE_MAJOR_VERSION (4u)
 #define ACE_MINOR_VERSION (6u)
-#define ACE_BETA_VERSION (28u)
-#define ACE_VERSION "4.6.28\0"
+#define ACE_BETA_VERSION (29u)
+#define ACE_VERSION "4.6.29\0"
