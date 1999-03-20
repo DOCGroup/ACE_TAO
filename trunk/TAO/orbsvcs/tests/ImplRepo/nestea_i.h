bfcc07d24229cbf3c1227668c0ec4d52cb8f6836
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-20 21:22:51 +0000

    Implemented persistance for the number of cans.  It saves it to a file.

--- trunk/TAO/orbsvcs/tests/ImplRepo/nestea_i.h
+++ trunk/TAO/orbsvcs/tests/ImplRepo/nestea_i.h
@@ -39,7 +39,7 @@ class Nestea_i: public POA_Nestea_Bookshelf
   //    number of nestea cans in a bookshelf.  You can drink Nestea to add
   //    it to the bookshelf or crush the cans to remove them.
 public:
-  Nestea_i (int shutdown = 0);
+  Nestea_i (const char *filename = "nestea.dat", int shutdown = 0);
   // Constructor that takes in an optional shutdown parameter which, if nonzero,
   // will shutdown the server after each call.
 
@@ -47,17 +47,14 @@ public:
   // Destructor
 
   virtual void drink (CORBA::Long cans,
-                      CORBA::Environment &ACE_TRY_ENV = 
-                        CORBA::Environment::default_environment ());
+                      CORBA::Environment &ACE_TRY_ENV = CORBA::Environment::default_environment ());
   // Add <cans> number of cans to the bookshelf.
 
   virtual void crush (CORBA::Long cans,
-                      CORBA::Environment &ACE_TRY_ENV = 
-                        CORBA::Environment::default_environment ());
+                      CORBA::Environment &ACE_TRY_ENV = CORBA::Environment::default_environment ());
   // Removes <cans> number of cans from the bookshelf.
 
-  virtual CORBA::Long bookshelf_size (CORBA::Environment &ACE_TRY_ENV = 
-                                        CORBA::Environment::default_environment ());
+  virtual CORBA::Long bookshelf_size (CORBA::Environment &ACE_TRY_ENV = CORBA::Environment::default_environment ());
   // Returns the number of cans in the bookshelf.
 
   virtual char *get_praise (CORBA::Environment &ACE_TRY_ENV = 
@@ -65,10 +62,19 @@ public:
   // Returns comments about your collection.
 
 private:
+  int save_data (void);
+  // Saves bookshelf data to a file.
+
+  int load_data (void);
+  // Loads bookshelf data from a file.
+
+  char *data_filename_;
+  // The name of the file to store the data in.
+
   int shutdown_;
   // Shutdown flag.
 
-  CORBA::Long cans_;
+  ACE_UINT32 cans_;
   // Number of cans in the bookshelf.
 };
 
