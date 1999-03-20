9a695edf2f3e24e20c0e240d9440f37440f77154
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-20 20:30:57 +0000

    ChangeLogTag:Sat Mar 20 14:28:59 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

---
+++ TAO/tests/MT_Client/test_i.cpp
@@ -0,0 +1,23 @@
+// $Id$
+
+#include "test_i.h"
+
+#if !defined(__ACE_INLINE__)
+#include "test_i.i"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID(MT_Client, test_i, "$Id$")
+
+void
+Simple_Server_i::test_method (CORBA::Environment&)
+{
+  // nop
+}
+
+void
+Simple_Server_i::shutdown (CORBA::Environment&)
+{
+  this->orb_->shutdown (1);
+}
+
+
