b9aeea835fd3f195d5898ed4f1dac8fa3fed8ef0
vishal <vishal@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-28 23:57:20 +0000

    *** empty log message ***

--- TAO/orbsvcs/orbsvcs/Time/Timer_Helper.cpp
+++ TAO/orbsvcs/orbsvcs/Time/Timer_Helper.cpp
@@ -29,7 +29,7 @@ Timer_Helper::handle_timeout (const ACE_Time_Value &,
 {
   int no_of_servers = 0;
   CORBA::ULongLong sum = 0;
-
+  
   // The following are used to keep a track of the inaccuracy
   // in synchronization.
 
@@ -40,8 +40,9 @@ Timer_Helper::handle_timeout (const ACE_Time_Value &,
 #endif
   
   CORBA::ULongLong highest_time  = 0;
-  
-  TAO_TRY
+
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
     {
       IORS::TYPE* value;
       for (IORS::ITERATOR server_iterator (this->clerk_->server_);
@@ -51,43 +52,45 @@ Timer_Helper::handle_timeout (const ACE_Time_Value &,
 	  
 	  // This is a remote call.
 	  CosTime::UTO_var UTO_server =
-	    (*value)->universal_time (TAO_TRY_ENV);
+	    (*value)->universal_time (ACE_TRY_ENV);
 	  
-	  TAO_CHECK_ENV;
+	  ACE_TRY_CHECK;
 	  
           #if defined (ACE_LACKS_LONGLONG_T)
 	  
 	  ACE_DEBUG ((LM_DEBUG,
 		      "\nTime = %Q\nInaccuracy = %Q\nTimeDiff = %d\nstruct.time = %Q\n"
 		      "struct.inacclo = %d\nstruct.inacchi = %d\nstruct.Tdf = %d\n",
-		      ACE_U64_TO_U32 (UTO_server->time (TAO_TRY_ENV)),
-		      ACE_U64_TO_U32 (UTO_server->inaccuracy (TAO_TRY_ENV)),
-		      UTO_server->tdf (TAO_TRY_ENV),
+		      ACE_U64_TO_U32 (UTO_server->time (ACE_TRY_ENV)),
+		      ACE_U64_TO_U32 (UTO_server->inaccuracy (ACE_TRY_ENV)),
+		      UTO_server->tdf (ACE_TRY_ENV),
 		      ACE_U64_TO_U32 ((UTO_server->utc_time ()).time),
 		      (UTO_server->utc_time ()).inacclo,
 		      (UTO_server->utc_time ()).inacchi,
 		      (UTO_server->utc_time ()).tdf));
-
+	  
           #else
 
 	  ACE_DEBUG ((LM_DEBUG,
 		      "\nTime = %Q\nInaccuracy = %Q\nTimeDiff = %d\nstruct.time = %Q\n"
 		      "struct.inacclo = %d\nstruct.inacchi = %d\nstruct.Tdf = %d\n",
-		      UTO_server->time (TAO_TRY_ENV),
-		      UTO_server->inaccuracy (TAO_TRY_ENV),
-		      UTO_server->tdf (TAO_TRY_ENV),
+		      UTO_server->time (ACE_TRY_ENV),
+		      UTO_server->inaccuracy (ACE_TRY_ENV),
+		      UTO_server->tdf (ACE_TRY_ENV),
 		      (UTO_server->utc_time ()).time,
 		      (UTO_server->utc_time ()).inacclo,
 		      (UTO_server->utc_time ()).inacchi,
 		      (UTO_server->utc_time ()).tdf));
           #endif
-
-	  CORBA::ULongLong curr_server_time = UTO_server->time (TAO_TRY_ENV);
+	  
+	  CORBA::ULongLong curr_server_time = UTO_server->time (ACE_TRY_ENV);
+	  
+	  ACE_TRY_CHECK;
 
 	  sum += curr_server_time;
-
+	  
 	  ++no_of_servers;
-
+	  
 	  // Set the highest time to the largest time seen so far.
 	  if (curr_server_time > highest_time)
 	    highest_time = curr_server_time;
@@ -95,13 +98,13 @@ Timer_Helper::handle_timeout (const ACE_Time_Value &,
 	  // Set the lowest time to the smallest time seen so far.
 	  if (curr_server_time < lowest_time)
 	    lowest_time = curr_server_time;
- 
+	  
 	}
 
       ACE_DEBUG ((LM_DEBUG,
 		  "\nUpdated time from %d servers in the network",
 		  no_of_servers));
-
+      
       // Return the average of the times retrieved from the various
       // servers.
       clerk_->time_ = sum / no_of_servers ;
@@ -125,13 +128,14 @@ Timer_Helper::handle_timeout (const ACE_Time_Value &,
         + ACE_OS::gettimeofday ().usec () * 10;
 
     }
-  TAO_CATCHANY
+  ACE_CATCHANY
     {
-      TAO_TRY_ENV.print_exception ("Exception in the handle_timeout ()\n");
+      ACE_PRINT_EXCEPTION (ACE_TRY_ENV, "Exception in the handle_timeout ()\n");
       return -1;
     }
-  TAO_ENDTRY;
-
+  ACE_ENDTRY;
+  ACE_CHECK_RETURN (-1);
+  
   return 0;
 }
 
