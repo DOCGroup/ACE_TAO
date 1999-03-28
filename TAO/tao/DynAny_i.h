8359832a6dfe48b63dfc3d2ffb01eb9175e0f43d
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-28 20:25:24 +0000

    *** empty log message ***

--- TAO/tao/DynAny_i.h
+++ TAO/tao/DynAny_i.h
@@ -9,7 +9,7 @@
 //    DynAny_i.h
 //
 // = AUTHOR
-//    Jeff Parsons <jp4@cs.wustl.edu>
+//    Jeff Parsons <parsons@cs.wustl.edu>
 //
 // =========================================================================
 
@@ -44,26 +44,26 @@ public:
   // Functions common to all Dynamic Any types
 
   void assign (CORBA_DynAny_ptr dyn_any,
-               CORBA::Environment &TAO_IN_ENV);
+               CORBA::Environment &ACE_TRY_ENV);
   // Sets the value of the receiver to the value of the argument
 
-  CORBA_DynAny_ptr copy (CORBA::Environment &TAO_IN_ENV);
+  CORBA_DynAny_ptr copy (CORBA::Environment &ACE_TRY_ENV);
   // Creates a copy
 
-  void destroy (CORBA::Environment &TAO_IN_ENV);
+  void destroy (CORBA::Environment &ACE_TRY_ENV);
   // Makes sure the contents are disposed of correctly
 
   void from_any (const CORBA::Any& any,
-                 CORBA::Environment &TAO_IN_ENV);
+                 CORBA::Environment &ACE_TRY_ENV);
   // Assigns value from an Any argument
 
-  CORBA::Any_ptr to_any (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Any_ptr to_any (CORBA::Environment &ACE_TRY_ENV);
   // Outputs contents as an Any
 
   CORBA::TypeCode_ptr type (CORBA::Environment &);
   // Returns the (constant) type
 
-  CORBA_DynAny_ptr current_component (CORBA::Environment &TAO_IN_ENV);
+  CORBA_DynAny_ptr current_component (CORBA::Environment &ACE_TRY_ENV);
   // In this class, there is only one
 
   CORBA::Boolean next (CORBA::Environment &);
@@ -79,102 +79,102 @@ public:
   // Insert and get functions
 
   void insert_boolean (CORBA::Boolean value,
-                       CORBA::Environment &TAO_IN_ENV);
+                       CORBA::Environment &ACE_TRY_ENV);
   void insert_octet (CORBA::Octet value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_char (CORBA::Char value,
-                    CORBA::Environment &TAO_IN_ENV);
+                    CORBA::Environment &ACE_TRY_ENV);
   void insert_short (CORBA::Short value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_ushort (CORBA::UShort value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_long (CORBA::Long value,
-                    CORBA::Environment &TAO_IN_ENV);
+                    CORBA::Environment &ACE_TRY_ENV);
   void insert_ulong (CORBA::ULong value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_float (CORBA::Float value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_double (CORBA::Double value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_string (const char * value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_reference (CORBA::Object_ptr value,
-                         CORBA::Environment &TAO_IN_ENV);
+                         CORBA::Environment &ACE_TRY_ENV);
   void insert_typecode (CORBA::TypeCode_ptr value,
-                        CORBA::Environment &TAO_IN_ENV);
+                        CORBA::Environment &ACE_TRY_ENV);
   void insert_longlong (CORBA::LongLong value,
-                        CORBA::Environment &TAO_IN_ENV);
+                        CORBA::Environment &ACE_TRY_ENV);
   void insert_ulonglong (CORBA::ULongLong value,
-                         CORBA::Environment &TAO_IN_ENV);
+                         CORBA::Environment &ACE_TRY_ENV);
   void insert_wchar (CORBA::WChar value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_any (const CORBA::Any& value,
-                   CORBA::Environment &TAO_IN_ENV);
-  CORBA::Boolean get_boolean (CORBA::Environment &TAO_IN_ENV);
+                   CORBA::Environment &ACE_TRY_ENV);
+  CORBA::Boolean get_boolean (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Octet get_octet (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Octet get_octet (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Char get_char (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Char get_char (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Short get_short (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Short get_short (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::UShort get_ushort (CORBA::Environment &TAO_IN_ENV);
+  CORBA::UShort get_ushort (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Long get_long (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Long get_long (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::ULong get_ulong (CORBA::Environment &TAO_IN_ENV);
+  CORBA::ULong get_ulong (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Float get_float (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Float get_float (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Double get_double (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Double get_double (CORBA::Environment &ACE_TRY_ENV);
 
-  char * get_string (CORBA::Environment &TAO_IN_ENV);
+  char * get_string (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Object_ptr get_reference (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Object_ptr get_reference (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::TypeCode_ptr get_typecode (CORBA::Environment &TAO_IN_ENV);
+  CORBA::TypeCode_ptr get_typecode (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::LongLong get_longlong (CORBA::Environment &TAO_IN_ENV);
+  CORBA::LongLong get_longlong (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::ULongLong get_ulonglong (CORBA::Environment &TAO_IN_ENV);
+  CORBA::ULongLong get_ulonglong (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::WChar get_wchar (CORBA::Environment &TAO_IN_ENV);
+  CORBA::WChar get_wchar (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Any_ptr get_any (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Any_ptr get_any (CORBA::Environment &ACE_TRY_ENV);
 
 // The ORB DynAny-factory functions will call these so we don't need to
 // keep an orb pointer in each DynAny object.
 
   static CORBA_DynAny_ptr create_dyn_any (const CORBA_Any& any,
-                                           CORBA::Environment &TAO_IN_ENV);
+                                          CORBA::Environment &ACE_TRY_ENV);
 
   static CORBA_DynAny_ptr create_basic_dyn_any (CORBA_TypeCode_ptr tc,
-                                                CORBA::Environment &TAO_IN_ENV);
+                                                CORBA::Environment &ACE_TRY_ENV);
 
   static CORBA_DynStruct_ptr create_dyn_struct (CORBA_TypeCode_ptr tc,
-                                                CORBA::Environment &TAO_IN_ENV);
+                                                CORBA::Environment &ACE_TRY_ENV);
 
   static CORBA_DynSequence_ptr create_dyn_sequence (CORBA_TypeCode_ptr tc,
-                                                    CORBA::Environment &TAO_IN_ENV);
+                                                    CORBA::Environment &ACE_TRY_ENV);
 
   static CORBA_DynArray_ptr create_dyn_array (CORBA_TypeCode_ptr tc,
-                                              CORBA::Environment &TAO_IN_ENV);
+                                              CORBA::Environment &ACE_TRY_ENV);
 
   static CORBA_DynUnion_ptr create_dyn_union (CORBA_TypeCode_ptr tc,
-                                              CORBA::Environment &TAO_IN_ENV);
+                                              CORBA::Environment &ACE_TRY_ENV);
 
   static CORBA_DynEnum_ptr create_dyn_enum (CORBA_TypeCode_ptr tc,
-                                            CORBA::Environment &TAO_IN_ENV);
+                                            CORBA::Environment &ACE_TRY_ENV);
 
   // And an extra generic one (not required in the spec)
   // that calls one of the above passing a typecode.
   // Comes in handy in implementing some versions of current_component().
   static CORBA_DynAny_ptr create_dyn_any (CORBA_TypeCode_ptr tc,
-                                          CORBA::Environment &TAO_IN_ENV);
+                                          CORBA::Environment &ACE_TRY_ENV);
 
 
   static CORBA::TCKind unalias (CORBA_TypeCode_ptr tc,
-                                CORBA::Environment &TAO_IN_ENV);
+                                CORBA::Environment &ACE_TRY_ENV);
   // Utility, in case we have tk_alias.
 
 private:
--- TAO/tao/DynStruct_i.h
+++ TAO/tao/DynStruct_i.h
@@ -10,7 +10,7 @@
 //    DynStruct_i.h
 //
 // = AUTHOR
-//    Jeff Parsons <jp4@cs.wustl.edu>
+//    Jeff Parsons <parsons@cs.wustl.edu>
 //
 // ========================================================================
 
@@ -47,118 +47,118 @@ public:
 
   // Functions specific to DynStruct
 
-  FieldName current_member_name (CORBA::Environment &TAO_IN_ENV);
+  FieldName current_member_name (CORBA::Environment &ACE_TRY_ENV);
   // FieldName is IDL typedef for string
 
-  CORBA::TCKind current_member_kind (CORBA::Environment &TAO_IN_ENV);
+  CORBA::TCKind current_member_kind (CORBA::Environment &ACE_TRY_ENV);
 
   // Assign and dump elements using IDL defined type NameValuePairSeq
 
-  NameValuePairSeq* get_members (CORBA::Environment &TAO_IN_ENV);
+  NameValuePairSeq* get_members (CORBA::Environment &ACE_TRY_ENV);
 
   void set_members (const NameValuePairSeq& value,
-                    CORBA::Environment &TAO_IN_ENV);
+                    CORBA::Environment &ACE_TRY_ENV);
 
   // Functions common to all Dynamic Any types
 
   void assign (CORBA_DynAny_ptr dyn_any,
-               CORBA::Environment &TAO_IN_ENV);
+               CORBA::Environment &ACE_TRY_ENV);
   // Sets the value of the receiver to the value of the argument
 
-  CORBA_DynAny_ptr copy (CORBA::Environment &TAO_IN_ENV);
+  CORBA_DynAny_ptr copy (CORBA::Environment &ACE_TRY_ENV);
   // Creates a copy
 
-  void destroy (CORBA::Environment &TAO_IN_ENV);
+  void destroy (CORBA::Environment &ACE_TRY_ENV);
   // Makes sure the contents are disposed of correctly
 
   void from_any (const CORBA::Any& any,
-                 CORBA::Environment &TAO_IN_ENV);
+                 CORBA::Environment &ACE_TRY_ENV);
   // Assigns value from an Any argument
 
-  CORBA::Any_ptr to_any (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Any_ptr to_any (CORBA::Environment &ACE_TRY_ENV);
   // Outputs contents as an Any
 
-  CORBA::TypeCode_ptr type (CORBA::Environment &TAO_IN_ENV);
+  CORBA::TypeCode_ptr type (CORBA::Environment &ACE_TRY_ENV);
   // Returns the (constant) type
 
-  CORBA_DynAny_ptr current_component (CORBA::Environment &TAO_IN_ENV);
+  CORBA_DynAny_ptr current_component (CORBA::Environment &ACE_TRY_ENV);
   // Used in iterating through the contents
 
-  CORBA::Boolean next (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Boolean next (CORBA::Environment &ACE_TRY_ENV);
   // Returns next component
 
   CORBA::Boolean seek (CORBA::Long index,
-                       CORBA::Environment &TAO_IN_ENV);
+                       CORBA::Environment &ACE_TRY_ENV);
   // Jump to component at <index>
 
-  void rewind (CORBA::Environment &TAO_IN_ENV);
+  void rewind (CORBA::Environment &ACE_TRY_ENV);
   // Makes first component the current one
 
   // Insert and get functions
 
   void insert_boolean (CORBA::Boolean value,
-                       CORBA::Environment &TAO_IN_ENV);
+                       CORBA::Environment &ACE_TRY_ENV);
   void insert_octet (CORBA::Octet value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_char (CORBA::Char value,
-                    CORBA::Environment &TAO_IN_ENV);
+                    CORBA::Environment &ACE_TRY_ENV);
   void insert_short (CORBA::Short value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_ushort (CORBA::UShort value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_long (CORBA::Long value,
-                    CORBA::Environment &TAO_IN_ENV);
+                    CORBA::Environment &ACE_TRY_ENV);
   void insert_ulong (CORBA::ULong value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_float (CORBA::Float value,
-                     CORBA::Environment &TAO_IN_ENV);
+                     CORBA::Environment &ACE_TRY_ENV);
   void insert_double (CORBA::Double value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_string (const char * value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_reference (CORBA::Object_ptr value,
-                         CORBA::Environment &TAO_IN_ENV);
+                         CORBA::Environment &ACE_TRY_ENV);
   void insert_typecode (CORBA::TypeCode_ptr value,
-                        CORBA::Environment &TAO_IN_ENV);
+                        CORBA::Environment &ACE_TRY_ENV);
   void insert_longlong (CORBA::LongLong value,
-                        CORBA::Environment &TAO_IN_ENV);
+                        CORBA::Environment &ACE_TRY_ENV);
   void insert_ulonglong (CORBA::ULongLong value,
-                         CORBA::Environment &TAO_IN_ENV);
+                         CORBA::Environment &ACE_TRY_ENV);
   void insert_wchar (CORBA::WChar value,
-                      CORBA::Environment &TAO_IN_ENV);
+                      CORBA::Environment &ACE_TRY_ENV);
   void insert_any (const CORBA::Any& value,
-                   CORBA::Environment &TAO_IN_ENV);
-  CORBA::Boolean get_boolean (CORBA::Environment &TAO_IN_ENV);
+                   CORBA::Environment &ACE_TRY_ENV);
+  CORBA::Boolean get_boolean (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Octet get_octet (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Octet get_octet (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Char get_char (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Char get_char (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Short get_short (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Short get_short (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::UShort get_ushort (CORBA::Environment &TAO_IN_ENV);
+  CORBA::UShort get_ushort (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Long get_long (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Long get_long (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::ULong get_ulong (CORBA::Environment &TAO_IN_ENV);
+  CORBA::ULong get_ulong (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Float get_float (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Float get_float (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Double get_double (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Double get_double (CORBA::Environment &ACE_TRY_ENV);
 
-  char * get_string (CORBA::Environment &TAO_IN_ENV);
+  char * get_string (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Object_ptr get_reference (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Object_ptr get_reference (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::TypeCode_ptr get_typecode (CORBA::Environment &TAO_IN_ENV);
+  CORBA::TypeCode_ptr get_typecode (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::LongLong get_longlong (CORBA::Environment &TAO_IN_ENV);
+  CORBA::LongLong get_longlong (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::ULongLong get_ulonglong (CORBA::Environment &TAO_IN_ENV);
+  CORBA::ULongLong get_ulonglong (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::WChar get_wchar (CORBA::Environment &TAO_IN_ENV);
+  CORBA::WChar get_wchar (CORBA::Environment &ACE_TRY_ENV);
 
-  CORBA::Any_ptr get_any (CORBA::Environment &TAO_IN_ENV);
+  CORBA::Any_ptr get_any (CORBA::Environment &ACE_TRY_ENV);
 
 private:
   CORBA::TypeCode_var type_;
