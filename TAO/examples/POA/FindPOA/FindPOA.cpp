55122ed57709c6e2b7393cfc2710a5b8af99a3e2
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-03-31 04:35:50 +0000

    *** empty log message ***

--- TAO/examples/POA/FindPOA/FindPOA.cpp
+++ TAO/examples/POA/FindPOA/FindPOA.cpp
@@ -82,40 +82,11 @@ main (int argc, char **argv)
       return -1;
     }
 
-  // Use the TAO_POA name_separator (which is '/') to find a childPOA
-  // of firstPOA named secondPOA.
-
-  name += TAO_POA::name_separator ();
-  name += "secondPOA";
+  name = "secondPOA";
   PortableServer::POA_var second_poa =
-    root_poa->find_POA (name.c_str (),
-                        1,
-                        env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::find_POA");
-      return -1;
-    }
-
-  // Create a hierarchical string of POA names
-  // eg. thirdPOA/forthPOA/fifthPOA thirdPOA being the root of the
-  // hierarchy with forthPOA as its child and fifthPOA as its
-  // grandchild.
-
-  name = "thirdPOA";
-  name += TAO_POA::name_separator ();
-  name += "forthPOA";
-  name += TAO_POA::name_separator ();
-  name += "fifthPOA";
-
-  // Try to find the fifth_poa by passing the hierarchy of POA names
-  // resulting in the creation of third and forth POAs as well as the
-  // fifth POA.
-
-  PortableServer::POA_var fifth_poa =
-    root_poa->find_POA (name.c_str (),
-                        1,
-                        env);
+    first_poa->find_POA (name.c_str (),
+                         1,
+                         env);
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::find_POA");
@@ -148,26 +119,18 @@ main (int argc, char **argv)
       return -1;
     }
 
-  CORBA::String_var fifth_poa_name =
-    fifth_poa->the_name (env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
-
-
-  ACE_DEBUG((LM_DEBUG,"%s\n%s\n%s\n%s\n",
-	     root_poa_name.in (),
-	     first_poa_name.in (),
-	     second_poa_name.in (),
-	     fifth_poa_name.in ()));
+  ACE_DEBUG ((LM_DEBUG,
+              "%s\n%s\n%s\n%s\n",
+              root_poa_name.in (),
+              first_poa_name.in (),
+              second_poa_name.in ()));
 
   // This should destroy all its children
 
   root_poa->destroy (1,
                      1,
                      env);
+
   if (env.exception () != 0)
     {
       env.print_exception ("PortableServer::POA::destroy");
