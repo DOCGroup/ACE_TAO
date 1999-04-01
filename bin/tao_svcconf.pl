d2bec53b773d95b090c5373d47fd01989248af01
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-01 09:32:33 +0000

    *** empty log message ***

--- bin/tao_svcconf.pl
+++ bin/tao_svcconf.pl
@@ -10,6 +10,6 @@ eval '(exit $?0)' && eval 'exec perl -pi -S $0 ${1+"$@"}'
 # You may want to run the "find" command with this script, which maybe
 # something like this:
 #
-# find . -name svc.conf -print | xargs $ACE_ROOT/bin/svcconf.pl
+# find . -name svc.conf -print | xargs $ACE_ROOT/bin/tao_svcconf.pl
 
 s/^ *dynamic *([A-Za-z_]+) [^\"]+(\"[^\"]+\").*/static $1 $2/;
