86eb91b5679b7b10bd9d490f12146309020c2de4
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-02 01:59:46 +0000

    Reverted the changes I made concerning explicitly disabling default copy constructors and assugnment operators.

--- trunk/ace/Filecache.h
+++ trunk/ace/Filecache.h
@@ -316,12 +316,6 @@ public:
   };
 
 private:
-  ACE_Filecache_Object (const ACE_Filecache_Object &);
-  ACE_Filecache_Object & operator= (const ACE_Filecache_Object & rhs);
-  // Explicitly disallow use of implicitly generated copy
-  // constructor and assignment operator to prevent inadvertent
-  // memory leaks.
-
   char *tempname_;
   char filename_[MAXPATHLEN + 1];
   // The temporary file name and the real file name.  The real file is
