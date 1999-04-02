25deae4a2ce9defdd4af76a7db982443a2e0cd2a
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-02 02:52:58 +0000

    *** empty log message ***

--- trunk/TAO/examples/POA/FindPOA/FindPOA.cpp
+++ trunk/TAO/examples/POA/FindPOA/FindPOA.cpp
@@ -23,119 +23,94 @@ ACE_RCSID(FindPOA, FindPOA, "$Id$")
 int
 main (int argc, char **argv)
 {
-  CORBA::Environment env;
+  //  CORBA::Environment env;
+  ACE_DECLARE_NEW_CORBA_ENV;
 
   // Initialize the ORB
-
-  CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0, env);
-  if (env.exception () != 0)
+  char str[256]; // Exception message
+  ACE_TRY 
     {
-      env.print_exception ("CORBA::ORB_init");
-      return -1;
-    }
+      ACE_OS::strcpy (str, "CORBA::ORB_init");
+      
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, 0, ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      
+      // Get Object reference to RootPOA.
+      CORBA::Object_var obj =
+        orb->resolve_initial_references ("RootPOA");
 
-  // Get Object reference to RootPOA.
-  CORBA::Object_var obj =
-    orb->resolve_initial_references ("RootPOA");
+      ACE_OS::strcpy (str, "PortableServer::POA::_narrow");
+      // Narrow Object reference to RootPOA to a POA reference.
+      PortableServer::POA_var root_poa =
+        PortableServer::POA::_narrow (obj.in(), ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  // Narrow Object reference to RootPOA to a POA reference.
-  PortableServer::POA_var root_poa =
-    PortableServer::POA::_narrow (obj.in(), env);
+      // Get a TAO_Adapter_Activator reference
+      TAO_Adapter_Activator activator_impl;
+      
+      ACE_OS::strcpy (str, "TAO_Adapter_Activator::_this");
+      PortableServer::AdapterActivator_var activator =
+        activator_impl._this (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      
+      // Register the TAO_Adapter_Activator reference to be the RootPOA's
+      // Adapter Activator.
+      ACE_OS::strcpy (str,"PortableServer::POA::the_activator"); 
+      root_poa->the_activator (activator.in (), ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
+      // Try to find a childPOA of RootPOA named firstPOA
+      ACE_CString name = "firstPOA";
+      ACE_OS::strcpy (str,"PortableServer::POA::find_POA");
+      PortableServer::POA_var first_poa =
+        root_poa->find_POA (name.c_str (),
+                            1,
+                            ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+  
+      name = "secondPOA";
+      PortableServer::POA_var second_poa =
+        first_poa->find_POA (name.c_str (),
+                             1,
+                             ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  // Get a TAO_Adapter_Activator reference
-  TAO_Adapter_Activator activator_impl;
+      // Get the names of all the POAs
+      ACE_OS::strcpy (str, "PortableServer::POA::_narrow");
 
-  PortableServer::AdapterActivator_var activator =
-    activator_impl._this (env);
+      CORBA::String_var root_poa_name =
+        root_poa->the_name (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  if (env.exception () != 0)
-    {
-      env.print_exception ("TAO_Adapter_Activator::_this");
-      return -1;
-    }
+      CORBA::String_var first_poa_name =
+        first_poa->the_name (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  // Register the TAO_Adapter_Activator reference to be the RootPOA's
-  // Adapter Activator.
+      CORBA::String_var second_poa_name =
+        second_poa->the_name (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  root_poa->the_activator (activator.in (), env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::the_activator");
-      return -1;
-    }
+      ACE_DEBUG ((LM_DEBUG,
+                  "%s\n%s\n%s\n%s\n",
+                  root_poa_name.in (),
+                  first_poa_name.in (),
+                  second_poa_name.in ()));
 
-  // Try to find a childPOA of RootPOA named firstPOA
-  ACE_CString name = "firstPOA";
-  PortableServer::POA_var first_poa =
-    root_poa->find_POA (name.c_str (),
-                        1,
-                        env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::find_POA");
-      return -1;
-    }
-
-  name = "secondPOA";
-  PortableServer::POA_var second_poa =
-    first_poa->find_POA (name.c_str (),
+      // This should destroy all its children
+      ACE_OS::strcpy (str, "PortableServer::POA::destroy");
+      root_poa->destroy (1,
                          1,
-                         env);
-  if (env.exception () != 0)
+                         ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+
+    }
+  ACE_CATCHANY
     {
-      env.print_exception ("PortableServer::POA::find_POA");
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, str);
       return -1;
     }
-
-  // Get the names of all the POAs
-
-  CORBA::String_var root_poa_name =
-    root_poa->the_name (env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
-
-  CORBA::String_var first_poa_name =
-    first_poa->the_name (env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
-
-  CORBA::String_var second_poa_name =
-    second_poa->the_name (env);
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::_narrow");
-      return -1;
-    }
-
-  ACE_DEBUG ((LM_DEBUG,
-              "%s\n%s\n%s\n%s\n",
-              root_poa_name.in (),
-              first_poa_name.in (),
-              second_poa_name.in ()));
-
-  // This should destroy all its children
-
-  root_poa->destroy (1,
-                     1,
-                     env);
-
-  if (env.exception () != 0)
-    {
-      env.print_exception ("PortableServer::POA::destroy");
-      return -1;
-    }
+  ACE_ENDTRY;
+  ACE_CHECK_RETURN (-1);
 
   return 0;
 }
