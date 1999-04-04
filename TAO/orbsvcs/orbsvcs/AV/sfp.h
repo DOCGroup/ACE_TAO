4f7510ce94ae4084612c9fbfd174087bffe246a7
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-04 07:06:45 +0000

    Added support for fragmentation.

--- TAO/orbsvcs/orbsvcs/AV/sfp.h
+++ TAO/orbsvcs/orbsvcs/AV/sfp.h
@@ -20,6 +20,21 @@
 #include "ace/SOCK_Dgram.h"
 #include "orbsvcs/sfpC.h"
 
+#define MAGIC_NUMBER_LEN 5
+#define TAO_WRITEV_MAX 128
+
+#define DUMP_BUF(BUF,SIZE) \
+{\
+  char *buf = BUF;\
+  ACE_DEBUG ((LM_DEBUG,"========================================\n"));\
+  for (int i=0;i<SIZE;i++)\
+    ACE_DEBUG ((LM_DEBUG,"%d ",buf[i]));\
+  ACE_DEBUG ((LM_DEBUG,"\n========================================\n"));\
+}
+
+//#define SFP_MAX_PACKET_SIZE ACE_MAX_DGRAM_SIZE
+#define SFP_MAX_PACKET_SIZE 100
+
 // default arguments to pass to use for the ORB
 const char *TAO_SFP_ORB_ARGUMENTS = "-ORBobjrefstyle URL";
 
@@ -36,6 +51,8 @@ const unsigned char TAO_SFP_MINOR_VERSION = 0;
 
 // lengths of various SFP headers
 const unsigned char TAO_SFP_FRAME_HEADER_LEN = 12;
+
+// various message class for SFP.
 
 class TAO_ORBSVCS_Export SFP_Callback
   // =TITLE
@@ -58,7 +75,32 @@ public:
   virtual int receive_frame (ACE_Message_Block *frame) =0;
 };
 
-class TAO_ORBSVCS_Export SFP :public virtual ACE_Event_Handler
+class TAO_SFP_Fragment_Node
+{
+public:
+  TAO_SFP_Fragment_Node (void)
+    :data_ (0)
+    {}
+  flowProtocol::fragment fragment_info_;
+  ACE_Message_Block *data_;
+  friend int operator< (const TAO_SFP_Fragment_Node& left,
+                        const TAO_SFP_Fragment_Node& right);
+};
+
+class TAO_SFP_Fragment_Table_Entry
+{
+public:
+  TAO_SFP_Fragment_Table_Entry (void)
+    :last_received_ (0),
+     num_fragments_ (0)
+    {}
+  int last_received_;
+  int num_fragments_;
+  ACE_Ordered_MultiSet<TAO_SFP_Fragment_Node> fragment_set_;
+  typedef ACE_Ordered_MultiSet_Iterator<TAO_SFP_Fragment_Node> FRAGMENT_SET_ITERATOR;
+};
+
+class TAO_ORBSVCS_Export TAO_SFP :public virtual ACE_Event_Handler
   // = TITLE
   //     SFP implementation on UDP.
   //
@@ -78,7 +120,7 @@ public:
     START_RECEIVED
   };
 
-  SFP (CORBA::ORB_ptr orb,
+  TAO_SFP (CORBA::ORB_ptr orb,
        ACE_Reactor* reactor,
        ACE_Time_Value timeout1,
        ACE_Time_Value timeout2,
@@ -92,11 +134,8 @@ public:
   virtual int start_stream (const char *local_addr,int credit_);
   // Passive start.
 
-  virtual int send_simple_frame (ACE_Message_Block *frame);
-  // sends a single frame over UDP.
-
   virtual int send_frame (ACE_Message_Block *frame);
-  // This will send a larger frame fragmenting if necessary.
+  // sends a single frame over UDP.
 
   virtual ACE_Message_Block* read_simple_frame (void);
   // receives a single frame from the network.
@@ -113,16 +152,8 @@ public:
   virtual ACE_HANDLE get_handle (void) const;
 private:
 
-  int create_cdr_buffer (char *message,
-                         size_t length);
-  // Helper - copies length bytes from the given message into the CDR
-  // buffer. Returns 0 on success, -1 on failure
-
-  ////  ACE_Message_Block *create_message_block (void);
-  void set_cdr_length (void);
-  // Helper method - copies the buffer in encoder_
-  // into a new ACE_Message_Block and returns it.
-  // Returns 0 on failure
+  ACE_Message_Block* read_fragment (void);
+  // reads a fragment from the wire.
 
   void create_local_dgram (void);
   // Create the local dgram endpoint.
@@ -136,14 +167,16 @@ private:
   int send_startReply (void);
   // sends the StartReply message to the receiver.
 
-  int send_cdr_buffer (void);
-  // sends the encoders cdr buffer using iovecs.
-
-  char *magic_number_to_string (char *magic_number);
-  // appends a 0 to the end of the magic number.
+  int send_cdr_buffer (TAO_OutputCDR &cdr,
+                       ACE_Message_Block *mb);
+  // sends the cdr buffer using iovecs.
 
   int register_dgram_handler (void);
   // registers the dgram socket with the reactor.
+
+  ACE_Message_Block *check_all_fragments (TAO_SFP_Fragment_Table_Entry *fragment_entry);
+  // checks if all the fragments for this entry has been received and returns the 
+  // head of the chain of message blocks for that frame.
 
   CORBA::ORB_ptr orb_;
   // ORB reference.
@@ -151,10 +184,10 @@ private:
   ACE_Reactor* reactor_;
   // Used for registering the dgram handler.
 
-  TAO_OutputCDR *encoder_;
-  // Use the TAO CDR encoder to encode everything
+  TAO_OutputCDR output_cdr_;
+  // Use the TAO CDR decoder to decode everything
 
-  TAO_InputCDR *decoder_;
+  //  TAO_InputCDR input_cdr_;
   // Use the TAO CDR encoder to encode everything
   
   ACE_SOCK_Dgram dgram_;
@@ -166,9 +199,6 @@ private:
   int startReply_tries_;
   // Number of tries to send a StartReply message.
 
-  CORBA::ULong credit_;
-  // Credit on the number of frames.
-
   ACE_Time_Value timeout1_;
   // Timeout used for Start on Sender side and also for Credit on
   // receiver side.
@@ -179,7 +209,6 @@ private:
 
   State state_;
   // State variable.
-  // @@We can use the state pattern here.
 
   const char *receiver_addr_;
   // The address of the receiver to which we're connected to.
@@ -192,6 +221,36 @@ private:
   
   int sequence_num_;
   // sequence number of the packet.
+
+  flowProtocol::frameHeader frame_header_;
+  size_t frame_header_len_;
+  // frame header to be sent with all frames.
+  // length of the frame header.
+
+  flowProtocol::fragment fragment_;
+  size_t fragment_len_;
+  // fragment header for each fragment.
+
+  flowProtocol::Start start_;
+  size_t start_len_;
+  // Start message and its length.
+
+  flowProtocol::StartReply start_reply_;
+  size_t start_reply_len_;
+  // StartReply message and its length.
+  
+  flowProtocol::credit credit_;
+  size_t credit_len_;
+  CORBA::ULong credit_num_;
+  // Credit message and its  length.
+
+  char magic_number_[MAGIC_NUMBER_LEN];
+  // used for peeking the magic_number.
+  const size_t magic_number_len_;
+  u_int more_fragments_;
+  // boolean flags indicating that there are more fragments.
+  ACE_Hash_Map_Manager<CORBA::ULong,TAO_SFP_Fragment_Table_Entry*,ACE_Null_Mutex> fragment_table_;
+  // chain of fragments of the current frame.
 };
 
 #endif /* !defined (TAO_SFP_H) */
