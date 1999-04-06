891f79c240765d2ad2fc99462fecd3b0c6e08120
joeh <joeh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-06 19:28:09 +0000

    .

--- examples/IPC_SAP/TLI_SAP/CPP-server.cpp
+++ examples/IPC_SAP/TLI_SAP/CPP-server.cpp
@@ -1,7 +1,7 @@
 // $Id$
 
 #include "ace/TLI_Acceptor.h"                             
-#include "ace/INET_Addr.h"                                       
+#include "ace/ATM_Addr.h"
 
 ACE_RCSID(TLI_SAP, CPP_server, "$Id$")
 
@@ -10,16 +10,26 @@ ACE_RCSID(TLI_SAP, CPP_server, "$Id$")
 
 int 
 main (int argc, char *argv[])
-{                                                                
+{
+#if defined (ACE_HAS_FORE_ATM_XTI)
+  ACE_Time_Value timeout (argc > 1 ? ACE_OS::atoi (argv[2]) : ACE_DEFAULT_TIMEOUT);
+#else
   u_short port = argc > 1 ? ACE_OS::atoi (argv[1]) : ACE_DEFAULT_SERVER_PORT;
   ACE_Time_Value timeout (argc > 2 ? ACE_OS::atoi (argv[2]) : ACE_DEFAULT_TIMEOUT);
+#endif /* ACE_HAS_FORE_ATM_XTI */
 
   // Create a server address. 
-  ACE_INET_Addr addr (port); 
+#if defined (ACE_HAS_FORE_ATM_XTI)
+  ACE_ATM_Addr addr;
+#else
+  ACE_INET_Addr addr (port);
+#endif /* ACE_HAS_FORE_ATM_XTI */
 
   // Create a server, reuse the addr. 
   ACE_TLI_Acceptor peer_acceptor;
 
+  // Not sure why but reuse_addr set to true/1 causes problems for
+  // FORE/XTI/ATM - this is now handled in ACE_TLI_Acceptor::open()
   if (peer_acceptor.open (addr, 1) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
                        "%p\n",
@@ -29,8 +39,8 @@ main (int argc, char *argv[])
   ACE_TLI_Stream new_stream;                                   
 
   ACE_DEBUG ((LM_DEBUG,
-              "starting server at port %d\n", 
-	     addr.get_port_number ()));
+              "starting server at address %s\n", 
+              addr.addr_to_string ()));
 
   // Performs the iterative server activities 
 
@@ -41,7 +51,8 @@ main (int argc, char *argv[])
       // Create a new ACE_TLI_Stream endpoint (note automatic restart
       // if errno == EINTR).
       if (peer_acceptor.accept (new_stream, 
-				&addr, &timeout) == -1)
+				&addr,
+                                &timeout) == -1)
 	{
 	  ACE_ERROR ((LM_ERROR,
                       "%p\n",
@@ -50,15 +61,16 @@ main (int argc, char *argv[])
 	}          
                                                                  
       ACE_DEBUG ((LM_DEBUG,
-                  "client %s connected from %d\n",
-		 addr.get_host_name (),
-                  addr.get_port_number ()));
+                  "client %s connected\n",
+                  addr.addr_to_string ()));
 
       // Read data from client (terminate on error).
 
       for (int r_bytes; 
 	   (r_bytes = new_stream.recv (buf, sizeof buf)) > 0; )
-        if (ACE_OS::write (ACE_STDOUT, buf, r_bytes) != r_bytes)
+        if (ACE_OS::write (ACE_STDOUT,
+                           buf,
+                           r_bytes) != r_bytes)
 	  ACE_ERROR ((LM_ERROR,
                       "%p\n",
                       "ACE::send_n"));
