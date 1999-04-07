ea39f4d68b165547a854a75c9aeab2316cffaf26
joeh <joeh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-07 17:42:34 +0000

    .

--- examples/IPC_SAP/TLI_SAP/CPP-client.cpp
+++ examples/IPC_SAP/TLI_SAP/CPP-client.cpp
@@ -1,7 +1,6 @@
 // $Id$
 
 #include "ace/TLI_Connector.h"
-#include "ace/ATM_Addr.h"                              
 #include "ace/INET_Addr.h"
 
 ACE_RCSID(TLI_SAP, CPP_client, "$Id$")
@@ -12,44 +11,17 @@ ACE_RCSID(TLI_SAP, CPP_client, "$Id$")
 
 int main (int argc, char *argv[])                       
 {
-#if defined (ACE_HAS_FORE_ATM_XTI)
-  if (argc < 2)
-    ACE_ERROR_RETURN ((LM_ERROR, 
-                       "Usage: %s hostname [QoS in KB/sec] [timeout]\n",
-                       argv[0]),
-                      1);
-  const char *host = argv[1];
-  int qos = argc > 2 ? ACE_OS::atoi (argv[2]) : 0;
-  int timeout = argc > 3 ? ACE_OS::atoi (argv[3]) : ACE_DEFAULT_TIMEOUT;
-#else
   const char *host = argc > 1 ? argv[1] : ACE_DEFAULT_SERVER_HOST;
   u_short r_port = argc > 2 ? ACE_OS::atoi (argv[2]) : ACE_DEFAULT_SERVER_PORT;
   int timeout = argc > 3 ? ACE_OS::atoi (argv[3]) : ACE_DEFAULT_TIMEOUT;
   u_short l_port = argc > 4 ? ACE_OS::atoi (argv[4]) : ACE_DEFAULT_LOCAL_PORT;
-#endif /* ACE_HAS_FORE_ATM_XTI */
 
   char buf[BUFSIZ];
 
   ACE_TLI_Stream cli_stream;
 
-#if defined (ACE_HAS_FORE_ATM_XTI)
-  ACE_ATM_Addr remote_addr (host);
-  char hostname[MAXNAMELEN];
-  ACE_OS::hostname(hostname, MAXNAMELEN);
-  ACE_ATM_Addr local_addr (hostname);
-
-  // In order to construct connections options the file handle is
-  // needed.  Therefore, we need to open the TLI_Stream before we
-  // construct the options.
-  if (cli_stream.open (ACE_TLI_TCP_DEVICE, O_RDWR, 0) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "%p\n",
-                       "open failed"),
-                      1);
-#else
   ACE_INET_Addr remote_addr (r_port, host);
   ACE_INET_Addr local_addr (l_port);
-#endif /* ACE_HAS_FORE_ATM_XTI */
 
   ACE_DEBUG ((LM_DEBUG,
               "starting non-blocking connect\n"));
@@ -57,38 +29,11 @@ int main (int argc, char *argv[])
   // Initiate timed, non-blocking connection with server.
   ACE_TLI_Connector con;
                                                         
-#if defined (ACE_HAS_FORE_ATM_XTI)
-  // Construct QoS options - currently FORE only supports bandwidth
-  long optlen = 0;
-  char *options = ACE_ATM_Addr::construct_options (cli_stream.get_handle (),
-                                                   qos,
-                                                   ACE_ATM_Addr::OPT_FLAGS_CPID,
-                                                   &optlen);
-  struct netbuf optbuf;
-  optbuf.len = optlen;
-  optbuf.buf = options;
-
-  // Not sure why but reuse_addr set to true/1 causes problems for
-  // FORE/XTI/ATM - this is now handled in
-  // ACE_TLI_Connector::connect()
-  if (con.connect (cli_stream, remote_addr, 
-                   (ACE_Time_Value *) &ACE_Time_Value::zero, 
-                   local_addr,
-                   1,
-                   O_RDWR,
-                   0,
-                   ACE_TLI_TCP_DEVICE,
-                   0,
-                   1,
-                   0,
-                   &optbuf) == -1)
-#else
   if (con.connect (cli_stream,
                    remote_addr, 
                    (ACE_Time_Value *) &ACE_Time_Value::zero, 
                    local_addr,
                    1) == -1)
-#endif /* ACE_HAS_FORE_ATM_XTI */
     {
       if (errno != EWOULDBLOCK)
 	ACE_ERROR_RETURN ((LM_ERROR,
@@ -113,7 +58,7 @@ int main (int argc, char *argv[])
       else
 	ACE_DEBUG ((LM_DEBUG,
                     "connected to %s\n", 
-		    remote_addr.addr_to_string ()));
+		    remote_addr.get_host_name ()));
     }
 
   // Send data to server (correctly handles "incomplete writes").
