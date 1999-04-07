fe30f505902c00f813d5c1672e40cdf272a9894f
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-07 21:15:12 +0000

    HTML'ized output.

--- bin/vc_filter.pl
+++ bin/vc_filter.pl
@@ -1,14 +1,41 @@
 # $Id$
 
+$header = "<html> <head>
+<title>Error/Warning Summary of Daily Build</title>
+</head>
+
+<body bgcolor=\"white\">
+<h1>Error/Warning Summary of Daily Build</h1><TT>\n";
+
+$trailer = "</TT> </body> </html>";
+
 $fname = $ARGV[0];
 
 open (fp, "$fname");
 
+print $header ;
+
 while ($line = <fp>)
 {
-    print $line if ($line =~/^--------------------Configuration:/ ||
-                    $line =~/^[A-Z_a-z0-9.]+ - [0-9]+ error\(s\), +[0-9]+ warning\(s\)/ ||
-                    $line =~/^[A-Z_a-z0-9.\\:]+\([0-9]+\) : / ||
-                    $line =~/^[A-Z_a-z0-9.\\:]+\.obj : / ||
-                    $line =~/^fatal error/);
+    print "<p> $line<BR>" if ($line =~/^--------------------Configuration:/);
+
+    if ($line =~/^[A-Z_a-z0-9.]+ - [0-9]+ error\(s\), +[0-9]+ warning\(s\)/)
+    {
+        if ($line =~/0 error\(s\), 0 warning\(s\)/)
+        {
+            print "$line <BR>";
+        }
+        else
+        {
+            print "<FONT COLOR=\"RED\"><B> $line </B></FONT><BR>";
+        }
+    }
+
+    print "<FONT COLOR=\"RED\"><B> $line </B></FONT><BR>"
+        if ($line =~/^[A-Z_a-z0-9.\\:]+\([0-9]+\) : / ||
+            $line =~/^[A-Z_a-z0-9.\\:]+\.obj : / ||
+            $line =~/^fatal error/);
+
 }
+
+print $trailer;
