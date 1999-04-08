4b615b24fcd1bc68102a19d4c4887665fabe6b31
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-04-08 00:13:11 +0000

    .

--- ace/Containers.h
+++ ace/Containers.h
@@ -34,11 +34,10 @@ class ACE_Export ACE_DLList_Node
   // = TITLE
   //     Base implementation of element in a DL list.  Needed for
   //     ACE_Double_Linked_List.
-
+public:
   friend class ACE_Double_Linked_List<ACE_DLList_Node>;
   friend class ACE_Double_Linked_List_Iterator<ACE_DLList_Node>;
 
-public:
   ACE_DLList_Node (void *&i,
                    ACE_DLList_Node *n = 0,
                    ACE_DLList_Node *p = 0);
