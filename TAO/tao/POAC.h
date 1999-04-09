e6bc04d48262941825fba5e4441ff78814c415c8
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-09 00:28:34 +0000

    *** empty log message ***

--- TAO/tao/POAC.h
+++ TAO/tao/POAC.h
@@ -24,6 +24,10 @@
 #include "tao/Environment.h"
 #include "tao/CDR.h"
 
+#if defined (TAO_EXPORT_MACRO)
+#undef TAO_EXPORT_MACRO
+#endif
+#define TAO_EXPORT_MACRO TAO_Export
 #if defined(_MSC_VER)
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
