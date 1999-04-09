111f0a925f351a11566a7ed9f2047df2eaf6e10d
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-09 00:28:34 +0000

    *** empty log message ***

--- trunk/TAO/tao/POAS.h
+++ trunk/TAO/tao/POAS.h
@@ -22,6 +22,10 @@
 #include "tao/Servant_Base.h"
 #include "tao/POA_CORBA.h"
 
+#if defined (TAO_EXPORT_MACRO)
+#undef TAO_EXPORT_MACRO
+#endif
+#define TAO_EXPORT_MACRO TAO_Export
 #if defined(_MSC_VER)
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
