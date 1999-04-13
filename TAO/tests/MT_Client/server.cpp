76d79e118f0223583624e5f53971a5ac7dd7453e
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-13 17:57:53 +0000

    ChangeLogTag:Tue Apr 13 12:35:18 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/tests/MT_Client/server.cpp
+++ TAO/tests/MT_Client/server.cpp
@@ -70,16 +70,16 @@ main (int argc, char *argv[])
       ACE_TRY_CHECK;
 
       ACE_DEBUG ((LM_DEBUG, "Activated as <%s>\n", ior.in ()));
-      
+
       // If the ior_output_file exists, output the ior to it
       if (ior_output_file != 0)
 	{
 	  FILE *output_file= ACE_OS::fopen (ior_output_file, "w");
 	  if (output_file == 0)
 	    ACE_ERROR_RETURN ((LM_DEBUG,
-			       "Cannot open output file for writing IOR: %s", 
+			       "Cannot open output file for writing IOR: %s",
 			       ior_output_file),
-			      1); 
+			      1);
 	  ACE_OS::fprintf (output_file, "%s", ior.in ());
 	  ACE_OS::fclose (output_file);
 	}
