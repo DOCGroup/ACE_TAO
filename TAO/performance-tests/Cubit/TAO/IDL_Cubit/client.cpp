e7f950591d2c2285c446aff36dd5a34eaca6fa53
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-13 19:06:33 +0000

    *** empty log message ***

--- TAO/performance-tests/Cubit/TAO/IDL_Cubit/client.cpp
+++ TAO/performance-tests/Cubit/TAO/IDL_Cubit/client.cpp
@@ -2,6 +2,7 @@
 
 #include "Cubit_Client.h"
 #include "tao/Timeprobe.h"
+#include "tests/test_config.h"
 
 ACE_RCSID(IDL_Cubit, client, "$Id$")
 
@@ -10,6 +11,8 @@ ACE_RCSID(IDL_Cubit, client, "$Id$")
 int
 main (int argc, char **argv)
 {
+//  ACE_START_TEST (ASYS_TEXT ("Cubit_Client"));
+
   Cubit_Client cubit_client;
 
   ACE_DEBUG ((LM_DEBUG,
@@ -21,6 +24,8 @@ main (int argc, char **argv)
   int retval = cubit_client.run ();
 
   ACE_TIMEPROBE_PRINT;
+
+//  ACE_END_TEST;
 
   return retval;
 }
