75c9f4e577355201d20900ba0e4602e5e2d4d73c
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-14 20:49:09 +0000

    Replaced TAO_TRY macros with ACE_TRY macros.

--- trunk/TAO/orbsvcs/orbsvcs/AV/AVStreams_i_T.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/AV/AVStreams_i_T.cpp
@@ -22,10 +22,10 @@ TAO_StreamEndPoint<IF>::TAO_StreamEndPoint (void)
 template <class IF>
 void
 TAO_StreamEndPoint<IF>::stop (const AVStreams::flowSpec &the_spec,
-                          CORBA::Environment &env)
+                          CORBA::Environment &ACE_TRY_ENV)
 {
   // Make the upcall into the app
-  this->handle_stop (the_spec, env);
+  this->handle_stop (the_spec, ACE_TRY_ENV);
 }
 
 // Start the physical flow of data on the stream
@@ -33,20 +33,20 @@ TAO_StreamEndPoint<IF>::stop (const AVStreams::flowSpec &the_spec,
 template <class IF>
 void
 TAO_StreamEndPoint<IF>::start (const AVStreams::flowSpec &flow_spec,
-                           CORBA::Environment &env)
+                           CORBA::Environment &ACE_TRY_ENV)
 {
   // Make the upcall into the app
-  this->handle_start (flow_spec, env);
+  this->handle_start (flow_spec, ACE_TRY_ENV);
 }
 
 // Close the connection
 template <class IF>
 void
 TAO_StreamEndPoint<IF>::destroy (const AVStreams::flowSpec &the_spec,
-                             CORBA::Environment &env)
+                             CORBA::Environment &ACE_TRY_ENV)
 {
   // Make the upcall into the app
-  this->handle_destroy (the_spec, env);
+  this->handle_destroy (the_spec, ACE_TRY_ENV);
 }
 // Called by our peer endpoint, requesting us to establish
 // a connection
@@ -80,11 +80,11 @@ template <class IF>
 CORBA::Boolean
 TAO_StreamEndPoint<IF>::modify_QoS (AVStreams::streamQoS &new_qos,
                                 const AVStreams::flowSpec &the_flows,
-                                CORBA::Environment &env)
+                                CORBA::Environment &ACE_TRY_ENV)
 {
   ACE_UNUSED_ARG (new_qos);
   ACE_UNUSED_ARG (the_flows);
-  ACE_UNUSED_ARG (env);
+  ACE_UNUSED_ARG (ACE_TRY_ENV);
   return 0;
 }
 
@@ -92,25 +92,35 @@ TAO_StreamEndPoint<IF>::modify_QoS (AVStreams::streamQoS &new_qos,
 template <class IF>
 CORBA::Boolean
 TAO_StreamEndPoint<IF>::set_protocol_restriction (const AVStreams::protocolSpec &the_pspec,
-                                              CORBA::Environment &env)
+                                              CORBA::Environment &ACE_TRY_ENV)
 {
-  CORBA::Any protocol_restriction_any;
+  ACE_TRY
+    {
+      CORBA::Any protocol_restriction_any;
 
-  protocol_restriction_any <<= the_pspec;
-  this->define_property ("ProtocolRestriction",
-                         protocol_restriction_any,
-                         env);
-  TAO_CHECK_ENV_RETURN (env,0);
+      protocol_restriction_any <<= the_pspec;
+      this->define_property ("ProtocolRestriction",
+                             protocol_restriction_any,
+                             ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,"TAO_StreamEndPoint::set_protocol_restriction");
+      return 0;
+    }
+  ACE_ENDTRY;
+  ACE_CHECK_RETURN (0);
   return 1;
 }
 
 template <class IF>
 void
 TAO_StreamEndPoint<IF>::disconnect (const AVStreams::flowSpec &the_spec,
-                                CORBA::Environment &env)
+                                CORBA::Environment &ACE_TRY_ENV)
 {
   ACE_UNUSED_ARG (the_spec);
-  ACE_UNUSED_ARG (env);
+  ACE_UNUSED_ARG (ACE_TRY_ENV);
 }
 
 // @@ Need to throw not-supported exception here
@@ -119,12 +129,12 @@ void
 TAO_StreamEndPoint<IF>::set_FPStatus (const AVStreams::flowSpec &the_spec,
                                   const char *fp_name,
                                   const CORBA::Any &fp_settings,
-                                  CORBA::Environment &env)
+                                  CORBA::Environment &ACE_TRY_ENV)
 {
   ACE_UNUSED_ARG (the_spec);
   ACE_UNUSED_ARG (fp_name);
   ACE_UNUSED_ARG (fp_settings);
-  ACE_UNUSED_ARG (env);
+  ACE_UNUSED_ARG (ACE_TRY_ENV);
 }
 
 template <class IF>
@@ -142,86 +152,113 @@ TAO_StreamEndPoint<IF>::get_fep (const char *flow_name,
 template <class IF>
 char *
 TAO_StreamEndPoint<IF>::add_fep (CORBA::Object_ptr the_fep,
-                             CORBA::Environment &env)
+                                 CORBA::Environment &ACE_TRY_ENV)
 {
   char *flow_name = 0;
-  ACE_NEW_RETURN (flow_name,char [BUFSIZ],0);
-  CORBA::Any_ptr flow_name_any;
-  AVStreams::FlowEndPoint_ptr fep =
-    AVStreams::FlowEndPoint::_narrow (the_fep,env);
-  TAO_CHECK_ENV_RETURN (env,0);
-  TAO_TRY
-    {
-      flow_name_any = fep->get_property_value ("FlowName",TAO_TRY_ENV);
-      TAO_CHECK_ENV;
-
-      *flow_name_any >>= flow_name;
-    }
-  TAO_CATCHANY
+  ACE_TRY_EX (flow_name);
+  {
+    ACE_NEW_RETURN (flow_name,char [BUFSIZ],0);
+    CORBA::Any_ptr flow_name_any;
+    AVStreams::FlowEndPoint_ptr fep =
+      AVStreams::FlowEndPoint::_narrow (the_fep,ACE_TRY_ENV);
+    ACE_TRY_CHECK_EX (flow_name);
+    flow_name_any = fep->get_property_value ("FlowName",ACE_TRY_ENV);
+    ACE_TRY_CHECK_EX (flow_name);
+    *flow_name_any >>= flow_name;
+  }
+  ACE_CATCHANY
     {
       // exception implies the flow name is not defined and is system generated.
       ACE_OS::sprintf (flow_name,"flow%d",flow_num_++);
     }
+  ACE_ENDTRY;
   // Add it to the sequence of flowNames supported.
   // put the flowname and the flowendpoint in a hashtable.
   TAO_String_Hash_Key fep_name_key (flow_name);
   if (this->fep_map_.bind (fep_name_key,the_fep) != 0)
-    TAO_THROW_ENV_RETURN (AVStreams::streamOpFailed (),env,0);
-
-  // increment the flow count.
-  this->flow_count_++;
-  this->flows_.length (this->flow_count_);
-  this->flows_ [this->flow_count_-1] = flow_name;
-  // define/modify the "Flows" property.
-  CORBA::Any flows_any;
-  flows_any <<= this->flows_;
-  this->define_property ("Flows",
-                         flows_any,
-                         env);
-  TAO_CHECK_ENV_PRINT_RETURN (env,"TAO_StreamEndPoint::add_fep",0);
-  TAO_ENDTRY;
+    {
+      ACE_THROW_RETURN (AVStreams::streamOpFailed (),0);
+      return 0;
+    }
+  ACE_TRY_EX (flows)
+    {
+      // increment the flow count.
+      this->flow_count_++;
+      this->flows_.length (this->flow_count_);
+      this->flows_ [this->flow_count_-1] = flow_name;
+      // define/modify the "Flows" property.
+      CORBA::Any flows_any;
+      flows_any <<= this->flows_;
+      this->define_property ("Flows",
+                             flows_any,
+                             ACE_TRY_ENV);
+      ACE_TRY_CHECK_EX (flows);
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,"TAO_StreamEndPoint::add_fep");
+      return 0;
+    }
+  ACE_ENDTRY;
+  ACE_CHECK_RETURN (0);
   return flow_name;
 }
 
 template <class IF>
 void
 TAO_StreamEndPoint<IF>::remove_fep (const char *flow_name,
-                                CORBA::Environment &env)
+                                CORBA::Environment &ACE_TRY_ENV)
 {
-  TAO_String_Hash_Key fep_name_key (flow_name);
-  CORBA::Object_ptr fep_entry = 0;
-  // Remove the fep from the hash table.
-  if (this->fep_map_.unbind (fep_name_key,fep_entry)!= 0)
-    TAO_THROW_ENV (AVStreams::streamOpFailed (),env);
-  // redefine the "Flows" property
-  AVStreams::flowSpec new_flows (this->flows_.length ());
-  for (u_int i=0,j=0 ; i <this->flows_.length (); i++)
-    if (ACE_OS::strcmp (flow_name,this->flows_[i]) != 0)
-      new_flows[j++] = this->flows_[i];
+  ACE_TRY
+    {
+      TAO_String_Hash_Key fep_name_key (flow_name);
+      CORBA::Object_ptr fep_entry = 0;
+      // Remove the fep from the hash table.
+      if (this->fep_map_.unbind (fep_name_key,fep_entry)!= 0)
+        TAO_THROW_ENV (AVStreams::streamOpFailed (),ACE_TRY_ENV);
+      // redefine the "Flows" property
+      AVStreams::flowSpec new_flows (this->flows_.length ());
+      for (u_int i=0,j=0 ; i <this->flows_.length (); i++)
+        if (ACE_OS::strcmp (flow_name,this->flows_[i]) != 0)
+          new_flows[j++] = this->flows_[i];
 
-  CORBA::Any flows;
-  flows <<= new_flows;
-  this->flows_ = new_flows;
-  this->define_property ("Flows",
-                         flows,
-                         env);
-  TAO_CHECK_ENV_PRINT_RETURN_VOID (env,"TAO_StreamEndPoint::remove_fep");
-  return;
+      CORBA::Any flows;
+      flows <<= new_flows;
+      this->flows_ = new_flows;
+      this->define_property ("Flows",
+                             flows,
+                             ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,"TAO_StreamEndPoint::remove_fep");
+    }
+  ACE_ENDTRY;
+  ACE_CHECK;
 }
 
 // @@ Need to throw not-supported exception here
 template <class IF>
 void
 TAO_StreamEndPoint<IF>::set_negotiator (AVStreams::Negotiator_ptr new_negotiator,
-                                    CORBA::Environment &env)
+                                    CORBA::Environment &ACE_TRY_ENV)
 {
-  CORBA::Any negotiator;
-  negotiator <<= new_negotiator;
-  this->define_property ("Negotiator",
-                         negotiator,
-                         env);
-  TAO_CHECK_ENV_PRINT_RETURN_VOID (env,"TAO_StreamEndPoint::set_negotiator");
-  return;
+  ACE_TRY
+    {
+      CORBA::Any negotiator;
+      negotiator <<= new_negotiator;
+      this->define_property ("Negotiator",
+                             negotiator,
+                             ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,"TAO_StreamEndPoint::set_negotiator");
+    }
+  ACE_ENDTRY;
+  ACE_CHECK;
 }
 
 // Sets the public key used for this streamendpoint.
@@ -229,17 +266,26 @@ template <class IF>
 void
 TAO_StreamEndPoint<IF>::set_key (const char *flow_name,
                              const AVStreams::key & the_key,
-                             CORBA::Environment &env)
+                             CORBA::Environment &ACE_TRY_ENV)
 {
-  CORBA::Any PublicKey;
-  PublicKey <<= the_key;
-  char PublicKey_property [BUFSIZ];
-  ACE_OS::sprintf (PublicKey_property,"%s_PublicKey",flow_name);
-  this->define_property (PublicKey_property,
-                         PublicKey,
-                         env);
-  TAO_CHECK_ENV_PRINT_RETURN_VOID (env,"TAO_StreamEndPoint::set_key");
-  return;
+  ACE_TRY
+    {
+
+      CORBA::Any PublicKey;
+      PublicKey <<= the_key;
+      char PublicKey_property [BUFSIZ];
+      ACE_OS::sprintf (PublicKey_property,"%s_PublicKey",flow_name);
+      this->define_property (PublicKey_property,
+                             PublicKey,
+                             ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,"TAO_StreamEndPoint::set_key");
+    }
+  ACE_ENDTRY;
+  ACE_CHECK;
 }
 
 // Set the source id.
