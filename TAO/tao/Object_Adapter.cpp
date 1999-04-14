6479da40f220afa5ee3c168e03f7160cd07c71a9
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-14 05:33:13 +0000

    Wed Apr 14 00:24:42 1999  Irfan Pyarali  <irfan@cs.wustl.edu>

--- TAO/tao/Object_Adapter.cpp
+++ TAO/tao/Object_Adapter.cpp
@@ -719,6 +719,8 @@ typedef ACE_Equal_To<key> compare_keys;
 typedef TAO_ObjectId_Hash hash_key;
 typedef ACE_Noop_Key_Generator<key> noop_key_generator;
 
+template class ACE_Noop_Key_Generator<key>;
+
 // Common
 template class ACE_Reference_Pair<const key, value>;
 
@@ -787,6 +789,8 @@ typedef ACE_Equal_To<key> compare_keys;
 typedef TAO_ObjectId_Hash hash_key;
 typedef ACE_Noop_Key_Generator<key> noop_key_generator;
 
+#pragma instantiate ACE_Noop_Key_Generator<key>
+
 // Common
 #pragma instantiate ACE_Reference_Pair<const key, value>
 
@@ -814,6 +818,7 @@ typedef ACE_Noop_Key_Generator<key> noop_key_generator;
 
 // Hash Map Manager related.
 #pragma instantiate ACE_Hash_Map_Manager_Ex_Adapter<key, value, hash_key, compare_keys, TAO_Incremental_Key_Generator>
+#pragma instantiate ACE_Hash_Map_Manager_Ex_Adapter<key, value, hash_key, compare_keys, noop_key_generator>;
 #pragma instantiate ACE_Hash_Map_Manager_Ex_Iterator_Adapter<value_type, key, value, hash_key, compare_keys>
 #pragma instantiate ACE_Hash_Map_Manager_Ex_Reverse_Iterator_Adapter<value_type, key, value, hash_key, compare_keys>
 #pragma instantiate ACE_Hash_Map_Manager_Ex<key, value, hash_key, compare_keys, ACE_Null_Mutex>
@@ -829,6 +834,7 @@ typedef ACE_Noop_Key_Generator<key> noop_key_generator;
 #pragma instantiate ACE_Map_Manager_Iterator_Adapter<value_type, key, value>
 #pragma instantiate ACE_Map_Manager_Reverse_Iterator_Adapter<value_type, key, value>
 #pragma instantiate ACE_Map_Manager_Adapter<key, value, TAO_Incremental_Key_Generator>
+#pragma instantiate ACE_Map_Manager_Adapter<key, value, noop_key_generator>;
 #pragma instantiate ACE_Map_Manager<key, value, ACE_Null_Mutex>
 #pragma instantiate ACE_Map_Iterator_Base<key, value, ACE_Null_Mutex>
 #pragma instantiate ACE_Map_Iterator<key, value, ACE_Null_Mutex>
