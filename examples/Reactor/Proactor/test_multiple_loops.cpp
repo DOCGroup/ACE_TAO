74a1675f740e1119beb4d1f329645aaa35623e36
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-17 00:07:20 +0000

    Updated comments.

--- examples/Reactor/Proactor/test_multiple_loops.cpp
+++ examples/Reactor/Proactor/test_multiple_loops.cpp
@@ -1,4 +1,4 @@
-// $Id: test_timeout.cpp
+// $Id$
 
 // ============================================================================
 //
@@ -11,7 +11,8 @@
 // = DESCRIPTION
 //
 //    This example application shows how to write programs that
-//    combine the Proactor and Reactor event loops
+//    combine the Proactor and Reactor event loops. This is possible
+//    only on WIN32 platform.
 //
 // = AUTHOR
 //    Irfan Pyarali
@@ -24,19 +25,23 @@
 
 ACE_RCSID(Proactor, test_multiple_loops, "$Id$")
 
+#if (defined (ACE_WIN32) && !defined (ACE_HAS_WINCE))
+
 class Timeout_Handler : public ACE_Handler, public ACE_Event_Handler
+{
   // = TITLE
   //     Generic timeout handler.
-{
+  
 public:
   Timeout_Handler (void) 
     { 
     }
-
+  
+  // This is called by the Proactor. This is declared in ACE_Handler.
   virtual void handle_time_out (const ACE_Time_Value &tv,
-			       const void *arg)
-    // Print out when timeouts occur.
+                                const void *arg)
     {
+      // Print out when timeouts occur.
       ACE_DEBUG ((LM_DEBUG, "(%t) %d timeout occurred for %s @ %d.\n", 
 		  ++count_,
 		  (char *) arg,
@@ -49,6 +54,8 @@ public:
 	// Sleep for a while
 	ACE_OS::sleep (1);
     }
+
+  // This method is declared in ACE_Event_Handler.
   virtual int handle_timeout (const ACE_Time_Value &tv,
 			      const void *arg)
     {
@@ -63,23 +70,25 @@ private:
 class Worker : public ACE_Task <ACE_NULL_SYNCH>
 {
 public:
+  
+  // Thread fuction.
   int svc (void)
-  {
-    ACE_DEBUG ((LM_DEBUG, "(%t) Worker started\n"));
-
-    // Handle events for 13 seconds.
-    ACE_Time_Value run_time (13);
-
-    // Try to become the owner
-    ACE_Reactor::instance ()->owner (ACE_Thread::self ());
-
-    if (ACE_Reactor::run_event_loop (run_time) == -1)
-      ACE_ERROR_RETURN ((LM_ERROR, "%p.\n", "Worker::svc"), -1);
-    else
-      ACE_DEBUG ((LM_DEBUG, "(%t) work complete\n"));
-
-    return 0;
-  }
+    {
+      ACE_DEBUG ((LM_DEBUG, "(%t) Worker started\n"));
+      
+      // Handle events for 13 seconds.
+      ACE_Time_Value run_time (13);
+      
+      // Try to become the owner
+      ACE_Reactor::instance ()->owner (ACE_Thread::self ());
+      
+      if (ACE_Reactor::run_event_loop (run_time) == -1)
+        ACE_ERROR_RETURN ((LM_ERROR, "%p.\n", "Worker::svc"), -1);
+      else
+        ACE_DEBUG ((LM_DEBUG, "(%t) work complete\n"));
+      
+      return 0;
+    }
 };
 
 int
@@ -87,7 +96,7 @@ main (int, char *[])
 {
   Timeout_Handler handler;
   ACE_Proactor proactor (0, 0, 1);
-
+  
   ACE_Reactor::instance ()->register_handler (&proactor);
   
   // Register a 2 second timer.
@@ -119,3 +128,5 @@ main (int, char *[])
 
   return 0;
 }
+
+#endif /* ACE_WIN32 && !ACE_HAS_WINCE */
