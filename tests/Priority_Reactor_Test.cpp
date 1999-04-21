8680e3b7c15696ab54427f96fa3853769ae9c847
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-21 22:00:51 +0000

    default to 5 children instead of 10 on LynxOS

--- tests/Priority_Reactor_Test.cpp
+++ tests/Priority_Reactor_Test.cpp
@@ -45,7 +45,12 @@ USELIB("..\ace\aced.lib");
 
 // The number of children to run, it can be changed using the -c
 // option.
-static int opt_nchildren = 10;
+static int opt_nchildren =
+#if defined (__Lynx__)
+  5;
+#else  /* ! __Lynx__ */
+ 10;
+#endif /* ! __Lynx__ */
 
 // The number of loops per children, it can be changed using the -l
 // option.
