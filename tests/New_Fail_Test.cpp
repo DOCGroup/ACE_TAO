7fb910dc5c90bc30cab06e3ebb4ea4d04e1a19df
Steve Huston <shuston@riverace.com>
1999-04-22 13:47:39 +0000

    Corrected to compile without exceptions enabled.

--- tests/New_Fail_Test.cpp
+++ tests/New_Fail_Test.cpp
@@ -20,6 +20,7 @@
 #include "test_config.h"
 #include "ace/Log_Msg.h"
 #include "ace/OS.h"
+#include "ace/CORBA_macros.h"
 
 ACE_RCSID(tests, New_Fail_Test, "$Id$")
 
@@ -69,54 +70,58 @@ main (int, ASYS_TCHAR *[])
   char *blocks[MAX_ALLOCS_IN_TEST];
   int i;
 
+#if defined (ACE_HAS_EXCEPTIONS)
   try
     {
+#endif /* ACE_HAS_EXCEPTIONS */
       // First part: test ACE_NEW
       for (i = 0; i < MAX_ALLOCS_IN_TEST; i++)
-        {
-          try_ace_new (&blocks[i]);
-          if (blocks[i] == 0)
-            break;
-        }
+	{
+	  try_ace_new (&blocks[i]);
+	  if (blocks[i] == 0)
+	    break;
+	}
       if (i == MAX_ALLOCS_IN_TEST)
-        {
-          ACE_ERROR((LM_WARNING,
-                     "Test didn't exhaust all available memory\n"));
-          --i;    // Back up to valid pointer for deleting
-        }
+	{
+	  ACE_ERROR((LM_WARNING,
+		     "Test didn't exhaust all available memory\n"));
+	  --i;    // Back up to valid pointer for deleting
+	}
       else
-        {
-          ACE_ASSERT (blocks[i] == 0);
-          ACE_ASSERT (errno == ENOMEM);
-          ACE_DEBUG((LM_DEBUG, "ACE_NEW failed properly at block %d\n", i));
-        }
+	{
+	  ACE_ASSERT (blocks[i] == 0);
+	  ACE_ASSERT (errno == ENOMEM);
+	  ACE_DEBUG((LM_DEBUG, "ACE_NEW failed properly at block %d\n", i));
+	}
 
       // Free the memory to try ACE_NEW_RETURN
       while (i >= 0)
-        delete [] blocks[i--];
+	delete [] blocks[i--];
 
       // Second part: test ACE_NEW_RETURN
       for (i = 0; i < MAX_ALLOCS_IN_TEST; i++)
-        {
-          blocks[i] = try_ace_new_return ();
-          if (blocks[i] == 0)
-            break;
-        }
+	{
+	  blocks[i] = try_ace_new_return ();
+	  if (blocks[i] == 0)
+	    break;
+	}
       if (i == MAX_ALLOCS_IN_TEST)
-        {
-          ACE_ERROR((LM_WARNING,
-                     "Test didn't exhaust all available memory\n"));
-          --i;    // Back up to valid pointer
-        }
+	{
+	  ACE_ERROR((LM_WARNING,
+		     "Test didn't exhaust all available memory\n"));
+	  --i;    // Back up to valid pointer
+	}
       else
-        {
-          ACE_ASSERT (blocks[i] == 0);
-          ACE_ASSERT (errno == ENOMEM);
-          ACE_DEBUG((LM_DEBUG, "ACE_NEW_RETURN failed properly at block %d\n",
-                     i));
-        }
+	{
+	  ACE_ASSERT (blocks[i] == 0);
+	  ACE_ASSERT (errno == ENOMEM);
+	  ACE_DEBUG((LM_DEBUG, "ACE_NEW_RETURN failed properly at block %d\n",
+		     i));
+	}
       while (i >= 0)
-        delete [] blocks[i--];
+	delete [] blocks[i--];
+
+#if defined (ACE_HAS_EXCEPTIONS)
     }
 
   catch (...)
@@ -125,7 +130,7 @@ main (int, ASYS_TCHAR *[])
       "Caught exception during test; ACE_bad_alloc not defined correctly.\n"));
       status = 1;        // Mark test failure
     }
-
+#endif /* ACE_HAS_EXCEPTIONS */
 #endif /* __SUNPRO_CC && !ACE_HAS_EXCEPTIONS */
 
   ACE_END_TEST;
