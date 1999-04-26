9863888258bb8243011209f20a81a68832c05894
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-26 10:36:31 +0000

    Un-inlined default_svc_conf_entries().

--- trunk/TAO/tao/TAO_Internal.cpp
+++ trunk/TAO/tao/TAO_Internal.cpp
@@ -21,6 +21,16 @@ char *TAO_Internal::server_strategy_args_ = 0;
 char *TAO_Internal::client_strategy_args_ = 0;
 #endif /* TAO_PLATFORM_SVC_CONF_FILE_NOTSUP */
 
+void
+TAO_Internal::default_svc_conf_entries (const char *resource_factory_args,
+                                        const char *server_strategy_args,
+                                        const char *client_strategy_args)
+{
+  TAO_Internal::resource_factory_args_ = resource_factory_args;
+  TAO_Internal::server_strategy_args_ = server_strategy_args;
+  TAO_Internal::client_strategy_args_ = client_strategy_args;
+}
+
 int
 TAO_Internal::open_services (int& argc, char** argv, int ignore_default_svc_conf)
 {
