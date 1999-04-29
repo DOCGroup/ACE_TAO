cfe196bf205d2b1024630058d0b3f1b0e8052acb
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-29 00:06:10 +0000

    *** empty log message ***

--- trunk/TAO/tao/Context.cpp
+++ trunk/TAO/tao/Context.cpp
@@ -1,5 +1,8 @@
 // $Id$
 
+#include "tao/Typecode.h"
+#include "tao/Environment.h"
+#include "tao/NVList.h"
 #include "tao/Context.h"
 
 #if !defined (__ACE_INLINE__)
@@ -35,3 +38,147 @@ CORBA_Context::_decr_refcnt (void)
   return 0;
 }
 
+const char *
+CORBA_Context::context_name (CORBA::Environment &ACE_TRY_ENV) const
+{
+  ACE_THROW_RETURN (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                         CORBA::COMPLETED_NO),
+                    0);
+}
+
+CORBA_Context_ptr 
+CORBA_Context::parent (CORBA::Environment &ACE_TRY_ENV) const
+{
+  ACE_THROW_RETURN (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                         CORBA::COMPLETED_NO),
+                    0);
+}
+
+void 
+CORBA_Context::create_child (const char *child_ctx_name, 
+                             CORBA_Context_out child_ctx,
+                             CORBA::Environment &ACE_TRY_ENV)
+{
+  ACE_THROW (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                  CORBA::COMPLETED_NO));
+}
+
+void 
+CORBA_Context::set_one_value (const char *propname, 
+                              const CORBA_Any &propvalue,
+                              CORBA::Environment &ACE_TRY_ENV)
+{
+  ACE_THROW (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                  CORBA::COMPLETED_NO));
+}
+
+void
+CORBA_Context::set_values (CORBA::NVList_ptr values,
+                           CORBA::Environment &ACE_TRY_ENV)
+{
+  ACE_THROW (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                  CORBA::COMPLETED_NO));
+}
+
+void 
+CORBA_Context::delete_values (const char *propname,
+                              CORBA::Environment &ACE_TRY_ENV)
+{
+  ACE_THROW (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                  CORBA::COMPLETED_NO));
+}
+
+void 
+CORBA_Context::get_values (const char *start_scope,
+                           CORBA::Flags op_flags,
+                           const char *pattern,
+                           CORBA::NVList_ptr &values,
+                           CORBA::Environment &ACE_TRY_ENV)
+{
+  ACE_THROW (CORBA::NO_IMPLEMENT (TAO_DEFAULT_MINOR_CODE, 
+                                  CORBA::COMPLETED_NO));
+}
+
+CORBA_ContextList::CORBA_ContextList (CORBA::ULong len,
+                                      char* *ctx_list)
+  : ref_count_ (1)
+{
+  for (CORBA::ULong i=0; i < len; i++)
+    this->add (ctx_list [i]);
+}
+
+CORBA_ContextList::~CORBA_ContextList (void)
+{
+  for (CORBA::ULong i = 0; i < this->count (); ++i)
+    {
+      char* *ctx;
+      if (this->ctx_list_.get (ctx, i) == -1)
+        return;
+      CORBA::string_free (*ctx);
+    }
+}
+
+void
+CORBA_ContextList::add (char *ctx)
+{
+  this->ctx_list_.enqueue_tail (CORBA::string_dup (ctx));
+}
+
+void
+CORBA_ContextList::add_consume (char *ctx)
+{
+  this->ctx_list_.enqueue_tail (ctx);
+}
+
+char *
+CORBA_ContextList::item (CORBA::ULong index,
+                         CORBA::Environment &ACE_TRY_ENV)
+{
+  char* *ctx;
+  ACE_TRY_ENV.clear ();
+  if (this->ctx_list_.get (ctx, index) == -1)
+    ACE_THROW_RETURN (CORBA::TypeCode::Bounds (), 0);
+  else
+    {
+      return CORBA::string_dup (*ctx);
+    }
+}
+
+void
+CORBA_ContextList::remove (CORBA::ULong, 
+                           CORBA::Environment &ACE_TRY_ENV)
+{
+  // unimplemented
+  ACE_TRY_ENV.clear ();
+}
+
+CORBA_ContextList_ptr
+CORBA_ContextList::_duplicate (void)
+{
+  ++this->ref_count_;
+  return this;
+}
+
+void
+CORBA_ContextList::_destroy (void)
+{
+  CORBA::ULong current = --this->ref_count_;
+  if (current == 0)
+    delete this;
+}
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+
+template class ACE_Node<char *>;
+template class ACE_Unbounded_Queue<char *>;
+template class ACE_Unbounded_Queue_Iterator<char *>;
+template class ACE_Atomic_Op<ACE_SYNCH_MUTEX, CORBA::ULong>;
+
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+
+#pragma instantiate ACE_Node<char *>
+#pragma instantiate ACE_Unbounded_Queue<char *>
+#pragma instantiate ACE_Unbounded_Queue_Iterator<char *>
+#pragma instantiate ACE_Atomic_Op<ACE_SYNCH_MUTEX, CORBA::ULong>
+
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
