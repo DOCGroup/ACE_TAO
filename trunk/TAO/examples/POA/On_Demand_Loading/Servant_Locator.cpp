2f5a3a75a5795d21f38c3eb8ad0fc8214ac3f8bc
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-04-30 05:57:01 +0000

    Updated CORBA::OBJECT_NOT_EXIST exception.

--- trunk/TAO/examples/POA/On_Demand_Loading/Servant_Locator.cpp
+++ trunk/TAO/examples/POA/On_Demand_Loading/Servant_Locator.cpp
@@ -35,7 +35,7 @@ ServantLocator_i::preinvoke (const PortableServer::ObjectId &oid,
                              PortableServer::POA_ptr poa,
                              const char * /* operation */,
                              PortableServer::ServantLocator::Cookie &cookie,
-                             CORBA::Environment &env)
+                             CORBA::Environment &ACE_TRY_ENV)
 {
   // Convert ObjectID to String.
 
@@ -58,9 +58,8 @@ ServantLocator_i::preinvoke (const PortableServer::ObjectId &oid,
       return servant;
     }
   else
-    TAO_THROW_ENV_RETURN (CORBA::OBJECT_NOT_EXIST (),
-                          env,
-                          0);
+    ACE_THROW_RETURN (CORBA::OBJECT_NOT_EXIST (),
+                      0);
 }
 
 // Since the servant gets invoked per operation, the servant has to be
@@ -73,7 +72,7 @@ ServantLocator_i::postinvoke (const PortableServer::ObjectId &oid,
                               const char * /* operation */,
                               PortableServer::ServantLocator::Cookie cookie,
                               PortableServer::Servant servant,
-                              CORBA::Environment &/* env */)
+                              CORBA::Environment &/* ACE_TRY_ENV */)
 {
   // Check the passed servant with the cookie.
 
