7fb22b09c45ea45c8062bfce1cc0cb930131e50f
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-04-30 00:57:51 +0000

    .

---
+++ TAO/examples/Simple/time-date/Time_Date_i.cpp
@@ -0,0 +1,33 @@
+// -*- C++ -*-
+// $Id$
+#include "Time_Date_i.h"
+#include "tao/corba.h"
+
+// Default constructor.
+
+Time_Date_i::Time_Date_i (void)
+{
+  //no-op
+}
+
+// Default destructor.
+
+Time_Date_i::~Time_Date_i (void)
+{
+  // no-op.
+}
+
+// Obtain the time and date in binary format.
+
+void
+Time_Date::bin_date (CORBA::Long_out time_date)
+{
+}
+
+// Obtain the time and date in string format.
+
+void 
+Time_Date::str_date (CORBA::String_out time_date)
+{
+
+}
