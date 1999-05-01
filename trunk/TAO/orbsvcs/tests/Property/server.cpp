3c1ed13881b82f588ca4451d167659b8d3ba264a
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-01 02:59:57 +0000

    Replaced TAO exception macros with ACE exception macros.

--- trunk/TAO/orbsvcs/tests/Property/server.cpp
+++ trunk/TAO/orbsvcs/tests/Property/server.cpp
@@ -25,7 +25,9 @@ ACE_RCSID(CosPropertyService, server, "$Id$")
 int
 main (int argc, char ** argv)
 {
-  TAO_TRY
+  ACE_DECLARE_NEW_CORBA_ENV;
+  
+  ACE_TRY
     {
       TAO_ORB_Manager m;
 
@@ -33,53 +35,51 @@ main (int argc, char ** argv)
       m.init_child_poa (argc,
                         argv,
                         "child_poa",
-                        TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+                        ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       // Using naming server.
       TAO_Naming_Client my_name_client;
       if (my_name_client.init (m.orb ()) < 0)
         return 1;
-
-      ACE_DEBUG ((LM_DEBUG, "Name init done\n"));
-
+      
       // Create PropertySetDef object and register.
       TAO_PropertySetDef *propsetdef_impl;
       ACE_NEW_RETURN (propsetdef_impl,
                       TAO_PropertySetDef,
                       -1);
       CosPropertyService::PropertySetDef_var propsetdef =
-        propsetdef_impl->_this (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+        propsetdef_impl->_this (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       CosNaming::Name propsetdef_name (1);
       propsetdef_name.length (1);
       propsetdef_name[0].id = CORBA::string_dup ("PropertySetDef");
       my_name_client->bind (propsetdef_name,
                             propsetdef.in (),
-                            TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+                            ACE_TRY_ENV);
+      ACE_TRY_CHECK;
       CORBA::Any any_val;
 
       // Make this IOR as one of the properties in there.
-      any_val <<= propsetdef_impl->_this (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+      any_val <<= propsetdef_impl->_this (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-      CORBA::Object_var ior = propsetdef_impl->_this (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+      CORBA::Object_var ior = propsetdef_impl->_this (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
       CORBA::Object_ptr ior_ptr = ior.in ();
       any_val <<= ior_ptr;
       // any_val.replace (CORBA::_tc_Object,
       //                  &ior_ptr,
       //                  1,
-      //                  TAO_TRY_ENV);
-      // TAO_CHECK_ENV;
+      //                  ACE_TRY_ENV);
+      // ACE_TRY_CHECK;
       
       propsetdef_impl->define_property_with_mode ("PropertySetDef_IOR",
                                                   any_val,
                                                   CosPropertyService::fixed_readonly,
-                                                  TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+                                                  ACE_TRY_ENV);
+      ACE_TRY_CHECK;
       
       // Create PropertySet factory and then register.
       TAO_PropertySetFactory *propset_factory_impl;
@@ -87,32 +87,34 @@ main (int argc, char ** argv)
                       TAO_PropertySetFactory,
                       -1);
       CosPropertyService::PropertySetFactory_var propset_factory =
-        propset_factory_impl->_this (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+        propset_factory_impl->_this (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       CosNaming::Name propset_factory_name (1);
       propset_factory_name.length (1);
       propset_factory_name[0].id = CORBA::string_dup ("PropertySetFactory");
       my_name_client->bind (propset_factory_name,
                             propset_factory.in (),
-                            TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+                            ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       // Run the ORB Event loop.
-      m.run (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+      m.run (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
     }
-  TAO_CATCH (CORBA::SystemException, sysex)
+  ACE_CATCH (CORBA::SystemException, sysex)
     {
-      TAO_TRY_ENV.print_exception ("System Exception");
+      ACE_PRINT_EXCEPTION (sysex,
+                           "System Exception");
       return 1;
     }
-  TAO_CATCH (CORBA::UserException, userex)
+  ACE_CATCH (CORBA::UserException, userex)
     {
-      TAO_TRY_ENV.print_exception ("User Exception");
+      ACE_PRINT_EXCEPTION (userex,
+                           "User Exception");
       return 1;
     }
-  TAO_ENDTRY;
+  ACE_ENDTRY;
   ACE_DEBUG ((LM_DEBUG,
               "\nServer is terminating"));
   return 0;
