6389eb92e8adac18fc48e9a129e073ce914ec0a0
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-01 13:23:07 +0000

    check to see that the executables exist before trying to run them.

--- TAO/performance-tests/Cubit/TAO/IDL_Cubit/run_test.pl
+++ TAO/performance-tests/Cubit/TAO/IDL_Cubit/run_test.pl
@@ -86,6 +86,9 @@ for ($i = 0; $i <= $#ARGV; $i++)
   }
 }
 
+(-f 'server'  &&  -f 'client')  ||
+  die "$0: server and/or client need to be built!\n";
+
 $SV = Process::Create ($exepref."server".$Process::EXE_EXT,
                        $svflags.
                        $svnsflags);
