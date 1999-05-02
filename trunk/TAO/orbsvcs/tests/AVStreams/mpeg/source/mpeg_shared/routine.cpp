4bbf2fc41ad0cc82ed60cae8eef796eb6d7a3bd5
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-02 05:09:30 +0000

    Fixed void usleep to int usleep to avoid ambiguity errors on Sun5.7 CC.

--- trunk/TAO/orbsvcs/tests/AVStreams/mpeg/source/mpeg_shared/routine.cpp
+++ trunk/TAO/orbsvcs/tests/AVStreams/mpeg/source/mpeg_shared/routine.cpp
@@ -132,11 +132,11 @@ long get_duration(long val1, long val2)
 
 #ifdef LINUX
 #else
-void usleep(unsigned int usec)
+int usleep(unsigned int usec)
 {
   struct timeval val;
 
-  if (usec <= 0) return;
+  if (usec <= 0) return -1;
   val.tv_sec = usec / 1000000;
   val.tv_usec = usec % 1000000;
   if (select(0, NULL, NULL, NULL, &val) == -1 && errno != 4)
@@ -144,6 +144,7 @@ void usleep(unsigned int usec)
    ACE_OS::perror ("sleep with select");
     ACE_OS::exit (1);
   }
+  return 0;
 }
 #endif
 
