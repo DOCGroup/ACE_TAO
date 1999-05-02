99cd8a80ef788e4f4f02f232bad8abdd7490501d
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-02 19:49:33 +0000

    removed ACE_Reverse_Lock<ACE_Lock> and ACE_Guard<ACE_Reverse_Lock<ACE_Lock> > explicit template instantiations because they're in tao/Object_Adapter.cpp.

--- trunk/TAO/orbsvcs/orbsvcs/Event/EC_ProxySupplier.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Event/EC_ProxySupplier.cpp
@@ -318,15 +318,3 @@ TAO_EC_ProxyPushSupplier::_remove_ref (CORBA::Environment &)
 {
   this->_decr_refcnt ();
 }
-
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-
-template class ACE_Reverse_Lock<ACE_Lock>;
-template class ACE_Guard<ACE_Reverse_Lock<ACE_Lock> >;
-
-#elif defined(ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-
-#pragma instantiate ACE_Reverse_Lock<ACE_Lock>
-#pragma instantiate ACE_Guard<ACE_Reverse_Lock<ACE_Lock> >
-
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
