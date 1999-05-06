cb070e1f1723c079faed7edeb4f09a8bb2f95a46
naga <naga@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-06 21:59:07 +0000

    Fixed the /* comment warnings on VC++. Thanks to Chris Gill for pointing this out.

--- trunk/TAO/orbsvcs/orbsvcs/AV/AVStreams_i.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/AV/AVStreams_i.cpp
@@ -85,9 +85,9 @@ TAO_Basic_StreamCtrl::destroy (const AVStreams::flowSpec &the_spec,
 // Changes the QoS associated with the stream
 // Empty the_spec means apply operation to all flows
 CORBA::Boolean
-TAO_Basic_StreamCtrl::modify_QoS (AVStreams::streamQoS & /*new_qos*/,
-                                  const AVStreams::flowSpec & /*the_spec*/,
-                                  CORBA::Environment & /*ACE_TRY_ENV*/)
+TAO_Basic_StreamCtrl::modify_QoS (AVStreams::streamQoS &/* new_qos */,
+                                  const AVStreams::flowSpec &/* the_spec */,
+                                  CORBA::Environment &/* ACE_TRY_ENV */)
 {
   return 0;
 }
@@ -95,18 +95,18 @@ TAO_Basic_StreamCtrl::modify_QoS (AVStreams::streamQoS & /*new_qos*/,
 // Used by StreamEndPoint and VDev to inform StreamCtrl of events.
 // E.g., loss of flow, reestablishment of flow, etc..
 void
-TAO_Basic_StreamCtrl::push_event (const struct CosPropertyService::Property &/*the_event*/,
-                                  CORBA::Environment & /*ACE_TRY_ENV*/)
+TAO_Basic_StreamCtrl::push_event (const struct CosPropertyService::Property &/* the_event */,
+                                  CORBA::Environment &/* ACE_TRY_ENV */)
 {
   ACE_DEBUG ((LM_DEBUG,"\n(%P|%t) Recieved event \""));
 }
 
 // @@ Need to throw not-supported exception here
 void
-TAO_Basic_StreamCtrl::set_FPStatus (const AVStreams::flowSpec & /*the_spec*/,
-                                    const char * /*fp_name*/,
-                                    const CORBA::Any & /*fp_settings*/,
-                                    CORBA::Environment & /*ACE_TRY_ENV*/)
+TAO_Basic_StreamCtrl::set_FPStatus (const AVStreams::flowSpec &/* the_spec */,
+                                    const char  */*  fp_name  */,
+                                    const CORBA::Any &/* fp_settings */,
+                                    CORBA::Environment &/* ACE_TRY_ENV */)
 {
 }
 
@@ -1836,8 +1836,8 @@ TAO_FDev::bind_mcast (AVStreams::FDev_ptr first_peer,
 }
 
 void
-TAO_FDev::destroy (AVStreams::FlowEndPoint_ptr /*the_ep*/,
-                   const char * /*fdev_name*/,
+TAO_FDev::destroy (AVStreams::FlowEndPoint_ptr /* the_ep */,
+                   const char * /* fdev_name */,
                    CORBA::Environment &ACE_TRY_ENV)
 {
   // Remove self from POA.  Because of reference counting, the POA
