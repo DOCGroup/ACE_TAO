40c8fffe662a038e55d5b09087919ea1fc7848c2
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-07 16:47:25 +0000

    Added "how to" delete the memory after recvv (iovec *).

--- trunk/ace/SOCK_IO.h
+++ trunk/ace/SOCK_IO.h
@@ -73,7 +73,8 @@ public:
   // buffer to read.  This method determines how much data is in the
   // socket, allocates a buffer of this size, reads in the data, and
   // returns the number of bytes read.  The caller is responsible for
-  // deleting the member in the <iov_base> field of <io_vec>.
+  // deleting the member in the <iov_base> field of <io_vec> using
+  // delete [] io_vec->iov_base.
 
   ssize_t recv (iovec *io_vec,
                 const ACE_Time_Value *timeout = 0);
