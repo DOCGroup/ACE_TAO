8249ee9635f420b8608060fdf4202a949e65865f
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-13 23:17:18 +0000

    Moved definition to a more usual place.

--- TAO/tao/corbafwd.cpp
+++ TAO/tao/corbafwd.cpp
@@ -2,10 +2,10 @@
 
 #include "tao/corbafwd.h"
 
-const CORBA::ServiceType CORBA::Security = 1;
-
 #if !defined (__ACE_INLINE__)
 # include "tao/corbafwd.i"
 #endif /* ! __ACE_INLINE__ */
 
 ACE_RCSID(tao, corbafwd, "$Id$")
+
+const CORBA::ServiceType CORBA::Security = 1;
