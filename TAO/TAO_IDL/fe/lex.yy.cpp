035efb980f34736981ce9cdedaf15a33385707ef
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-15 16:42:07 +0000

    Fixed TAO_YY_BREAK, again.

--- TAO/TAO_IDL/fe/lex.yy.cpp
+++ TAO/TAO_IDL/fe/lex.yy.cpp
@@ -850,7 +850,7 @@ TAO_YY_MALLOC_DECL
 
 /* Code executed at the end of each rule. */
 #ifndef TAO_YY_BREAK
-#define TAO_YY_BREAK break;
+#define TAO_YY_BREAK ACE_NOTREACHED (break;)
 #endif
 
 #define TAO_YY_RULE_SETUP \
