626d72862a5158e710f4f57275fe44d6b85a36a2
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-18 16:45:44 +0000

    Implementations for the three different Wait strategies.

---
+++ TAO/tao/IIOP_Wait_Strategy.h
@@ -0,0 +1,110 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//     TAO
+//
+// = FILENAME
+//     IIOP_Wait_Strategy.h
+//
+// = DESCRIPTION
+//     Classes to strategize waiting for reply.
+//
+// = AUTHOR
+//     Alexander Babu Arulanthu <alex@cs.wustl.edu>
+//
+// ============================================================================
+
+
+#include "tao/GIOP.h"
+
+class TAO_IIOP_Request_Multiplexing_Strategy;
+class TAO_Transport;
+
+class TAO_Export TAO_IIOP_Wait_Strategy
+{
+  // = TITLE
+  //
+  //    Strategy for waiting for the reply.
+  // 
+  // = DESCRIPTION
+  // 
+  
+public:
+  TAO_IIOP_Wait_Strategy (TAO_Transport *transport);
+  // Constructor.
+  
+  virtual ~TAO_IIOP_Wait_Strategy (void);
+  // Destructor.
+  
+  virtual int wait (void) = 0;
+  // Base class virtual method.
+
+protected:
+  TAO_Transport *transport_;
+  // Transport object.
+};
+
+class TAO_Export TAO_Wait_On_Reactor : public TAO_IIOP_Wait_Strategy
+{
+  // = TITLE
+  // 
+  //    Wait on the Reactor. Happens in s Single Threaded client
+  //    environment. 
+  //
+  // = DESCRIPTION
+  //
+
+public:
+  TAO_Wait_On_Reactor (TAO_Transport *transport);
+  // Constructor.
+  
+  virtual ~TAO_Wait_On_Reactor (void);
+  // Destructor.
+  
+  virtual int wait (void);
+  // Do the event loop of the Reactor.
+};
+
+class TAO_Export TAO_Wait_On_Leader_Follower : public TAO_IIOP_Wait_Strategy
+{
+  // = TITLE
+  //
+  //    Wait according to the Leader-Follower model. Leader does the
+  //    event loop of the Reactor and the Followers wait on the
+  //    condition variable.
+  //    
+  // = DESCRIPTION
+  // 
+
+public:
+  TAO_Wait_On_Leader_Follower (TAO_Transport *transport);
+  // Constructor.
+  
+  virtual ~TAO_Wait_On_Leader_Follower (void);
+  // Destructor.
+  
+  virtual int wait (void);
+  // Wait according to the L-F model.
+};
+
+class TAO_Export TAO_Wait_On_Read :  public TAO_IIOP_Wait_Strategy
+{
+  // = TITLE
+  // 
+  //    Wait on receiving the reply.
+  //
+  // = DESCRIPTION
+  //
+  
+public:
+  TAO_Wait_On_Read (TAO_Transport *transport);
+  // Constructor.
+
+  virtual ~TAO_Wait_On_Read (void);
+  // Destructor.
+
+  virtual int wait (void);
+  // Wait on the read operation.
+};
