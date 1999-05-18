8e975912b5d636fbb2a14c81bb2363e18873af76
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-18 16:47:09 +0000

    Implementation for the Reply Dispatcher strategies. (Synchronous and Asynchronous).

---
+++ branches/ami_phase1/TAO/tao/IIOP_Reply_Dispatcher.cpp
@@ -0,0 +1,74 @@
+// $Id$
+
+#include "tao/IIOP_Reply_Dispatcher.h"
+
+// Constructor.
+TAO_IIOP_Reply_Dispatcher::TAO_IIOP_Reply_Dispatcher (void)
+  : request_id_ (0),
+    cdr_ (0)
+{
+}
+
+// Destructor.
+TAO_IIOP_Reply_Dispatcher::~TAO_IIOP_Reply_Dispatcher (void)
+{
+}
+
+void
+TAO_IIOP_Reply_Dispatcher::request_id (CORBA::ULong request_id)
+{
+  this->request_id_ = request_id;
+}
+
+CORBA::ULong
+TAO_IIOP_Reply_Dispatcher::request_id (void) const
+{
+  return this->request_id_;
+}
+
+void
+TAO_IIOP_Reply_Dispatcher::reply_status (CORBA::ULong reply_status)
+{
+  this->reply_status_ = reply_status;
+}
+
+// Get the reply status.
+CORBA::ULong
+TAO_IIOP_Reply_Dispatcher::reply_status (void) const
+{
+  return this->reply_status_;
+}
+
+// Set the CDR which the has the reply message.
+void
+TAO_IIOP_Reply_Dispatcher::cdr (TAO_InputCDR *cdr)
+{
+  this->cdr_ = cdr;
+}
+
+// Get the CDR stream.
+TAO_InputCDR *
+TAO_IIOP_Reply_Dispatcher::cdr (void) const
+{
+  return this->cdr_;
+}
+
+// *********************************************************************
+
+// Constructor.
+TAO_Synch_Reply_Dispatcher::TAO_Synch_Reply_Dispatcher (void)
+{
+}
+
+// Destructor.
+TAO_Synch_Reply_Dispatcher::~TAO_Synch_Reply_Dispatcher (void)
+{
+}  
+
+// Dispatch the reply using the Stub object. 
+int
+TAO_Synch_Reply_Dispatcher::dispatch_reply (void)
+{
+  // NO OP.
+  return 0;
+}
