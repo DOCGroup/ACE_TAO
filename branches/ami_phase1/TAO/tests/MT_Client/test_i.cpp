3fa0780760480091d6daa20349c470c988a399ce
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-20 03:58:15 +0000

    Simpliefied version of the MT client.

--- branches/ami_phase1/TAO/tests/MT_Client/test_i.cpp
+++ branches/ami_phase1/TAO/tests/MT_Client/test_i.cpp
@@ -11,6 +11,16 @@ ACE_RCSID(MT_Client, test_i, "$Id$")
 void
 Simple_Server_i::test_method (CORBA::Environment&)
 {
+  ACE_DEBUG ((LM_DEBUG,
+              "%N:%l:(%P:%t):Simple_Server_i::test_method called\n"));
+}
+
+CORBA::Long
+Simple_Server_i::get_number (CORBA::Environment&)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              "%N:%l:(%P:%t):Simple_Server_i::get_number called\n"));
+  return (CORBA::ULong) 931232;
 }
 
 void
