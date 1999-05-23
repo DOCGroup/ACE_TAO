f2b55459d9ea65ad6b3867b62694b3f1d0e9fc0d
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-23 15:40:07 +0000

    ChangeLogTag:Sun May 23 10:06:36 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/orbsvcs/tests/Event/Performance/run_test.pl
+++ TAO/orbsvcs/tests/Event/Performance/run_test.pl
@@ -12,32 +12,35 @@ unshift @INC, '../../../../../bin';
 require Process;
 require Uniqueid;
 
-print "Throughput/Latency single threaded configuration\n";
+print STDERR "\n\nThroughput/Latency single threaded configuration\n";
 system ("Throughput -burstsize 100000 -burstcount 1");
 
-print "\n\nThroughput/Latency MT-safe configuration\n";
+print STDERR "\n\nThroughput/Latency MT-safe configuration\n";
 system ("Throughput -ORBsvcconf ec.st.conf -burstsize 100000"
 	." -burstcount 1");
 
-print "\n\nThroughput/Latency MT-safe configuration, 4 consumers\n";
+print STDERR "\n\nThroughput/Latency MT-safe configuration, 4 consumers\n";
 system ("Throughput -ORBsvcconf ec.st.conf -burstsize 100000"
 	." -burstcount 1 -consumers 4");
 
-print "Throughput/Latency MT-safe configuration, 4 consumers 4 suppliers\n";
+print STDERR "\n\nThroughput/Latency MT-safe configuration,",
+  " 4 consumers 4 suppliers\n";
 system ("Throughput -ORBsvcconf ec.st.conf -burstsize 100000"
 	." -burstcount 1 -consumers 4 -suppliers 4");
 
-print "\n\nThroughput/Latency MT-safe configuration,",
-  "4 consumers 4 suppliers\n";
+print STDERR "\n\nThroughput/Latency MT-safe configuration,",
+  " 4 consumers 4 suppliers\n";
 system ("Throughput -ORBsvcconf ec.st.conf -burstsize 100000"
 	." -burstcount 1 -consumers 4 -suppliers 4"
 	." -consumers_tshift 0 -suppliers_tshift 0");
 
-print "Connection and disconnection time, 100 consumers 100 suppliers\n";
+print STDERR "\n\nConnection and disconnection time,",
+  " 100 consumers 100 suppliers\n";
 system ("Connect -ORBsvcconf ec.st.conf -consumers 100 -suppliers 100"
 	." -connection_order interleaved");
 
-print "Connection and disconnection time, 500 consumers 500 suppliers\n";
+print STDERR "\n\nConnection and disconnection time,",
+  " 500 consumers 500 suppliers\n";
 system ("Connect -ORBsvcconf ec.st.conf -consumers 500 -suppliers 500"
 	." -connection_order interleaved");
 
