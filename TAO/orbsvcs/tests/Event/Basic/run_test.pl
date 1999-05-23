315c5dc2ac745d6c173897afa2c4d7d19960aba5
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-23 15:08:57 +0000

    ChangeLogTag:Sun May 23 10:06:36 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

---
+++ TAO/orbsvcs/tests/Event/Basic/run_test.pl
@@ -0,0 +1,32 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# $Id$
+# -*- perl -*-
+
+# This is a Perl script that runs the client and all the other servers that
+# are needed
+
+unshift @INC, '../../../../../bin';
+require Process;
+require Uniqueid;
+
+print "Reconnect suppliers and consumers, using disconnect/connect calls\n";
+system ("Reconnect -suppliers 100 -consumers 100 -d 100");
+
+print "\n\nReconnect suppliers and consumers, using connect calls\n";
+system ("Reconnect -suppliers 100 -consumers 100 -d 100 -s -c");
+
+print "\n\nShutdown EC without clients still attached\n";
+system ("Shutdown -verbose -suppliers 5 -consumers 5");
+
+print "\n\nComplex event channel test,",
+  "multiple ECs connected through gateways\n";
+system ("Observer -ORBsvcconf observer.conf"
+	." -consumer_tshift 0 -consumers 5"
+	." -supplier_tshift 0 -suppliers 2"
+	." -burstsize 10 -burstcount 10 -burstpause 0"
+	." -busyhwm 1024 -maxwritedelay 1024");
+
+exit 0;
