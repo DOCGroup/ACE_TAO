88723e7a99aac807ebd523f82717d1b0744a03d8
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-05-29 04:13:08 +0000

    ChangeLogTag:Fri May 28 23:11:56 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/performance-tests/Cubit/TAO/IDL_Cubit/server.cpp
+++ TAO/performance-tests/Cubit/TAO/IDL_Cubit/server.cpp
@@ -2,6 +2,7 @@
 
 #include "Cubit_Server.h"
 #include "tao/Timeprobe.h"
+#include "ace/Sched_Params.h"
 
 ACE_RCSID(IDL_Cubit, server, "$Id$")
 
@@ -10,7 +11,27 @@ ACE_RCSID(IDL_Cubit, server, "$Id$")
 int
 main (int argc, char *argv[])
 {
-//  ACE_START_TEST (ASYS_TEXT ("Cubit_Server"));
+  int priority =
+    (ACE_Sched_Params::priority_min (ACE_SCHED_FIFO)
+     + ACE_Sched_Params::priority_max (ACE_SCHED_FIFO)) / 2;
+  priority = ACE_Sched_Params::next_priority (ACE_SCHED_FIFO,
+                                                  priority);
+  // Enable FIFO scheduling, e.g., RT scheduling class on Solaris.
+
+  if (ACE_OS::sched_params (ACE_Sched_Params (ACE_SCHED_FIFO,
+                                              priority,
+                                              ACE_SCOPE_PROCESS)) != 0)
+    {
+      if (ACE_OS::last_error () == EPERM)
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "server (%P|%t): user is not superuser, "
+                      "test runs in time-shared class\n"));
+        }
+      else
+        ACE_ERROR ((LM_ERROR,
+                    "server (%P|%t): sched_params failed\n"));
+    }
 
   Cubit_Server cubit_server;
 
