756d17a6769562b90b93ad1c6ab0794e7eb9ee46
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-08 21:34:57 +0000

    .

--- trunk/TAO/orbsvcs/LifeCycle_Service/Criteria_Evaluator.cpp
+++ trunk/TAO/orbsvcs/LifeCycle_Service/Criteria_Evaluator.cpp
@@ -26,26 +26,24 @@ Criteria_Evaluator::~Criteria_Evaluator ()
 {
 }
 
-
-
 LifeCycleService::Criteria_Evaluator::SeqNamedValuePair *
 Criteria_Evaluator::getInitialization (CORBA::Environment &env)
 {
   LifeCycleService::Criteria_Evaluator::SeqNamedValuePair_ptr sequence_ptr = 0;
 
-  CORBA::Any * any_ptr = this->getCriteriaMember ("initialization");
+  CORBA::Any *any_ptr =
+    this->getCriteriaMember ("initialization");
 
   if (any_ptr == 0)
-  {
-    env.exception (new LifeCycleService::Criteria_Evaluator::NotAvailable ("No initialization member found.\n"));
-    return 0;
-  }
+    {
+      env.exception (new LifeCycleService::Criteria_Evaluator::NotAvailable ("No initialization member found.\n"));
+      return 0;
+    }
 
   *any_ptr >>= sequence_ptr;  
 
   return sequence_ptr;
 }
-
 
 char *
 Criteria_Evaluator::getFilter (CORBA::Environment &env)
@@ -55,35 +53,36 @@ Criteria_Evaluator::getFilter (CORBA::Environment &env)
   CORBA::Any_ptr any_ptr = this->getCriteriaMember ("filter");
 
   if (any_ptr == 0)
-  {
-    env.exception (new LifeCycleService::Criteria_Evaluator::NotAvailable ("No filter member found.\n"));
-    return 0;
-  }
+    {
+      env.exception (new LifeCycleService::Criteria_Evaluator::NotAvailable ("No filter member found.\n"));
+      return 0;
+    }
   *any_ptr >>= string;
   return string;
 }
 
-
 CORBA::Any *
-Criteria_Evaluator::getCriteriaMember (const CORBA::String member_name)
+Criteria_Evaluator::getCriteriaMember (const char *member_name)
 {
   if (!ACE_OS::strcmp(member_name, "initialization")
-    && !ACE_OS::strcmp(member_name, "filter")
-    && !ACE_OS::strcmp(member_name, "logical location")
-    && !ACE_OS::strcmp(member_name, "preferences"))
+      && !ACE_OS::strcmp(member_name, "filter")
+      && !ACE_OS::strcmp(member_name, "logical location")
+      && !ACE_OS::strcmp(member_name, "preferences"))
     return 0;
 
-  for (unsigned int i = 0; i < criteria_.length(); i++)
-  {
-    if (ACE_OS::strcmp (member_name, criteria_[i].name) == 0)
-    {
-      CORBA::Any_ptr value_ptr;
-      ACE_NEW_RETURN (value_ptr,
-                      CORBA::Any(criteria_[i].value),
-                      0);
-      return value_ptr;
-    }
-  }
+  for (u_int i = 0;
+       i < criteria_.length();
+       i++)
+    if (ACE_OS::strcmp (member_name,
+                        criteria_[i].name) == 0)
+      {
+        CORBA::Any_ptr value_ptr;
+        ACE_NEW_RETURN (value_ptr,
+                        CORBA::Any(criteria_[i].value),
+                        0);
+        return value_ptr;
+      }
+
   return 0;
 }
 
