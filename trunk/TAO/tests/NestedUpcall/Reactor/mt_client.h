acc89820d460b43c9d541856965b31ab305d01e8
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-08 05:10:03 +0000

    ChangeLogTag:Tue Jun  8 00:09:10 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/TAO/tests/NestedUpcall/Reactor/mt_client.h
+++ trunk/TAO/tests/NestedUpcall/Reactor/mt_client.h
@@ -68,11 +68,11 @@ private:
   int nthreads_;
   // # of threads
 
-  Reactor_var reactor_;
-  // reactor pointer for NestedUpCalls.
-
   CORBA::ORB_var orb_;
   // Remember our orb.
+
+  Reactor_var reactor_;
+  // reactor pointer for NestedUpCalls.
 };
 
 #endif /* TAO_NUC_MT_CLIENT_H */
