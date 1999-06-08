90177bfd2992f2b30a49693bbbae3046f640da59
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-08 05:10:03 +0000

    ChangeLogTag:Tue Jun  8 00:09:10 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/tests/NestedUpcall/Reactor/run_test.pl
+++ TAO/tests/NestedUpcall/Reactor/run_test.pl
@@ -10,26 +10,39 @@ require Process;
 require ACEutils;
 
 $file="test.ior";
+$mtfile="mttest.ior";
+
+print "\n\n==== Single-threaded test\n";
 
 $SV = Process::Create ($EXEPREFIX."server".$Process::EXE_EXT,
                        "-o $file");
+sleep 2;
 
 ACE::waitforfile ($file);
 
 $status  = system ($EXEPREFIX."client".$Process::EXE_EXT.
-                   "-x -k file://$file");
+                   " -x -k file://$file");
 
 $SV->Wait ();
+
+unlink ($file);
+
+print "\n\n==== Multi-threaded test\n";
+
+sleep 5;
 
 $SV = Process::Create ($EXEPREFIX."server".$Process::EXE_EXT,
-                       "-o $file");
+                       " -o $mtfile");
+sleep 2;
 
-ACE::waitforfile ($file);
+ACE::waitforfile ($mtfile);
 
 $status  = system ($EXEPREFIX."mt_client".$Process::EXE_EXT.
-                   "-x -k file://$file -ORBsvcconf svc.mt.conf");
+                   " -x -k file://$mtfile -ORBsvcconf svc.mt.conf");
 
 $SV->Wait ();
+
+unlink ($mtfile);
 
 # @@ Capture any errors from the server too.
 exit $status;
