eb12bfda8fe324847a3b24303668f8adf0f33ac9
alex <alex@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-10 18:08:44 +0000

    Thu Jun 10 13:05:17 1999  Alexander Babu Arulanthu  <alex@cs.wustl.edu>

--- TAO/TAO_IDL/be_include/be_visitor_argument/arglist_ami.h
+++ TAO/TAO_IDL/be_include/be_visitor_argument/arglist_ami.h
@@ -50,47 +50,47 @@ public:
   
   // = visit all the nodes that can be the types for the argument. 
   
-  //   virtual int visit_array (be_array *node);
-  //   // visit array type
-  // 
-  //   virtual int visit_enum (be_enum *node);
-  //   // visit the enum node
-  // 
-  //   virtual int visit_interface (be_interface *node);
-  //   // visit interface
-  // 
-  //   virtual int visit_interface_fwd (be_interface_fwd *node);
-  //   // visit interface forward
-  // 
+  virtual int visit_array (be_array *node);
+  // visit array type
+  
+  virtual int visit_enum (be_enum *node);
+  // visit the enum node
+  
+  virtual int visit_interface (be_interface *node);
+  // visit interface
+  
+  virtual int visit_interface_fwd (be_interface_fwd *node);
+  // visit interface forward
+  
 
   virtual int visit_native (be_native *node);
   // visit native node
 
-  //   virtual int visit_predefined_type (be_predefined_type *node);
-  //   // visit predefined type    
-  //                               
-  //   virtual int visit_sequence (be_sequence *node);
-  //   // visit sequence type      
-  //                               
-  //   virtual int visit_string (be_string *node);
-  //   // visit string type        
-  //                               
-  //   virtual int visit_structure (be_structure *node);
-  //   // visit structure type     
-  //                               
-  //   virtual int visit_union (be_union *node);
-  //   // visit union type         
-  //                               
-  //   virtual int visit_typedef (be_typedef *node);
-  //   // visit the typedef type   
-  //                               
-  // #ifdef IDL_HAS_VALUETYPE      
-  //   virtual int visit_valuetype (be_valuetype *node);
-  //   // visit valuetype          
-  //                               
-  //   virtual int visit_valuetype_fwd (be_valuetype_fwd *node);
-  //   // visit valuetype_fwd      
-  // #endif /* IDL_HAS_VALUETYPE */
+  virtual int visit_predefined_type (be_predefined_type *node);
+  // visit predefined type    
+                              
+  virtual int visit_sequence (be_sequence *node);
+  // visit sequence type      
+                              
+  virtual int visit_string (be_string *node);
+  // visit string type        
+                              
+  virtual int visit_structure (be_structure *node);
+  // visit structure type     
+                              
+  virtual int visit_union (be_union *node);
+  // visit union type         
+                              
+  virtual int visit_typedef (be_typedef *node);
+  // visit the typedef type   
+                              
+#ifdef IDL_HAS_VALUETYPE      
+  virtual int visit_valuetype (be_valuetype *node);
+  // visit valuetype          
+  
+  virtual int visit_valuetype_fwd (be_valuetype_fwd *node);
+  // visit valuetype_fwd      
+#endif /* IDL_HAS_VALUETYPE */
 };
 
 #endif /* _BE_VISITOR_ARGUMENT_ARGLIST_H_ */
