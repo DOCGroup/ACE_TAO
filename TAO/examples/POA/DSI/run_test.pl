6521645e9b3a2bb0de004661168b627db766f7d3
jwh1 <jwh1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-11 23:48:27 +0000

    Updated all wait()s too timed wait()s
    jwh1

--- TAO/examples/POA/DSI/run_test.pl
+++ TAO/examples/POA/DSI/run_test.pl
@@ -14,12 +14,31 @@ unlink $iorfile;
 
 $SV = Process::Create ($EXEPREFIX."server$Process::EXE_EXT", " -f $iorfile");
 
-ACE::waitforfile ($iorfile);
+if (ACE::waitforfile_timed ($iorfile, 5) == -1) {
+  print STDERR "ERROR: cannot find file <$iorfile>\n";
+  $SV->Kill (); $SV->TimedWait (1);
+  exit 1;
+}
 
-$status  = system ($EXEPREFIX."client$Process::EXE_EXT -f $iorfile -x");
+$CL = Process::Create ($EXEPREFIX."client$Process::EXE_EXT ",
+		       " -f $iorfile -x");
 
-$SV->Wait ();
+$client = $CL->TimedWait (60);
+if ($client == -1) {
+  print STDERR "ERROR: client timedout\n";
+  $CL->Kill (); $CL->TimedWait (1);
+}
 
+$server = $SV->TimedWait (5);
+if ($server == -1) {
+  print STDERR "ERROR: server timedout\n";
+  $SV->Kill (); $SV->TimedWait (1);
+}
+  
 unlink $iorfile;
 
-exit $status;
+if ($server != 0 || $client != 0) {
+  exit 1;
+}
+
+exit 0;
