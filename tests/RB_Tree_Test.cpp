9f7d6c9a1d00891a368baae1a86aa04527fd2c5d
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-06-11 16:41:53 +0000

    .

--- tests/RB_Tree_Test.cpp
+++ tests/RB_Tree_Test.cpp
@@ -39,22 +39,24 @@ USELIB("..\ace\aced.lib");
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK>
 class ACE_RB_Tree_Test
 {
-  //
   // = TITLE
-  //   Implements a templatized test class for the RB_Tree ADT and its iterators.
+  //   Implements a templatized test class for the RB_Tree ADT and its
+  //   iterators.
   //
   // = DESCRIPTION
-  //   To run the test class on a particular type instantiation of the RB_Tree,
-  //   simply instantiate the test class template with the same type parameters,
-  //   and invoke the run_test method.
-  //
-public:
 
+  //   To run the test class on a particular type instantiation of the
+  //   RB_Tree, simply instantiate the test class template with the
+  //   same type parameters, and invoke the run_test method.
+public:
   // = Traits
 
-  typedef ACE_RB_Tree<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> TREE;
-  typedef ACE_RB_Tree_Iterator<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> ITERATOR;
-  typedef ACE_RB_Tree_Reverse_Iterator<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> REVERSE_ITERATOR;
+  typedef ACE_RB_Tree<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> 
+          TREE;
+  typedef ACE_RB_Tree_Iterator<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> 
+          ITERATOR;
+  typedef ACE_RB_Tree_Reverse_Iterator<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> 
+          REVERSE_ITERATOR;
 
   // = Initialization and termination methods.
 
@@ -64,25 +66,27 @@ public:
                     int order_index []);
   // Constructor.
 
-  ~ACE_RB_Tree_Test ();
+  ~ACE_RB_Tree_Test (void);
   // Destructor.
 
-  void run_test ();
+  void run_test (void);
   // Run the individual interface and iteration tests in order.
 
 private:
 
-  void test_tree_insertion ();
+  void test_tree_insertion (void);
   // Tests stable and deprecated insertion interfaces.
 
-  void test_post_insertion_iteration ();
-  // Tests forward and reverse iteration after insertion in both trees.
+  void test_post_insertion_iteration (void);
+  // Tests forward and reverse iteration after insertion in both
+  // trees.
 
-  void test_tree_deletion ();
+  void test_tree_deletion (void);
   // Tests stable and deprecated deletion interfaces.
 
-  void test_post_deletion_iteration ();
-  // Tests forward and reverse iteration after deletions in both trees.
+  void test_post_deletion_iteration (void);
+  // Tests forward and reverse iteration after deletions in both
+  // trees.
 
   TREE stable_tree_;
   // Tree for testing stable interface.
@@ -116,7 +120,8 @@ private:
 
 };
 
-// Type definitions for the four distinct parameterizations of the test.
+// Type definitions for the four distinct parameterizations of the
+// test.
 
 typedef ACE_RB_Tree_Test<int, int, ACE_Less_Than<int>, ACE_Null_Mutex> INT_INT_RB_TREE_TEST;
 typedef ACE_RB_Tree_Test<int, const char *, ACE_Less_Than<int>, ACE_Null_Mutex> INT_STR_RB_TREE_TEST;
@@ -124,12 +129,10 @@ typedef ACE_RB_Tree_Test<const char *, int, ACE_Less_Than<const char *>, ACE_Nul
 typedef ACE_RB_Tree_Test<const char *, const char *, ACE_Less_Than<const char *>, ACE_Null_Mutex> STR_STR_RB_TREE_TEST;
 
 // Number of entries placed in each tree.
-
 static int RB_TREE_TEST_ENTRIES = 8;
 
-// These arrays of numbers as ints and character strings
-// are used to instantiate key and item arrays in the tests.
-
+// These arrays of numbers as ints and character strings are used to
+// instantiate key and item arrays in the tests.
 static const char *number_strings [] =
 {
   "10", "20", "30", "40", "50", "60", "70", "80"
@@ -140,13 +143,12 @@ static int number_integers [] =
   10, 20, 30, 40, 50, 60, 70, 80
 };
 
-// These arrays of ints are used to shuffle the order of insertion
-// and deletaion of keys and items for the various tests.
+// These arrays of ints are used to shuffle the order of insertion and
+// deletaion of keys and items for the various tests.
 static int int_int_index [] = {0, 1, 2, 3, 4, 5, 6, 7}; // LR inorder
 static int int_str_index [] = {7, 6, 5, 4, 3, 2, 1, 0}; // RL inorder
 static int str_int_index [] = {4, 6, 2, 7, 5, 3, 1, 0}; // RL BFS
 static int str_str_index [] = {4, 2, 1, 0, 3, 6, 5, 7}; // LR preorder
-
 
 int
 main (int, ASYS_TCHAR *[])
@@ -162,22 +164,18 @@ main (int, ASYS_TCHAR *[])
                                      number_integers,
                                      number_integers,
                                      int_int_index);
-
   INT_STR_RB_TREE_TEST int_str_test (RB_TREE_TEST_ENTRIES,
                                      number_integers,
                                      number_strings,
                                      int_str_index);
-
   STR_INT_RB_TREE_TEST str_int_test (RB_TREE_TEST_ENTRIES,
                                      number_strings,
                                      number_integers,
                                      str_int_index);
-
   STR_STR_RB_TREE_TEST str_str_test (RB_TREE_TEST_ENTRIES,
                                      number_strings,
                                      number_strings,
                                      str_str_index);
-
   int_int_test.run_test ();
   int_str_test.run_test ();
   str_int_test.run_test ();
@@ -186,10 +184,6 @@ main (int, ASYS_TCHAR *[])
   ACE_END_TEST;
   return 0;
 }
-
-////////////////////////////////////////////////////////////////////
-// class ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK> //
-////////////////////////////////////////////////////////////////////
 
 // Constructor.
 
@@ -210,19 +204,17 @@ ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::ACE_RB_Tree_Test
 {
 }
 
-
 // Destructor.
 
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK>
-ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::~ACE_RB_Tree_Test ()
+ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::~ACE_RB_Tree_Test (void)
 {
 }
-
 
 // Run the interface and iteration tests.
 
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK> void
-ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::run_test ()
+ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::run_test (void)
 {
   // Run the individual portions of the test, in order.
 
@@ -236,12 +228,12 @@ ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::run_test ()
 // Tests stable and deprecated insertion interfaces.
 
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK> void
-ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_tree_insertion ()
+ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_tree_insertion (void)
 {
   // Fill in each tree with the key and item from the appropriate
   // arrays, using the shuffle index to create the appropriate
-  // insertion orders.  Then, make sure the inserted item can be
-  // found using the insertion key.
+  // insertion orders.  Then, make sure the inserted item can be found
+  // using the insertion key.
 
   for (int i = 0; i < entry_count_; ++i)
     {
@@ -268,9 +260,9 @@ ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_tree_insertion ()
 // Tests forward and reverse iteration after insertion in both trees.
 
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK> void
-ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_post_insertion_iteration ()
+ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_post_insertion_iteration (void)
 {
-  // Reset iterators
+  // Reset iterators.
 
   stable_fwd_iter_ = stable_tree_.begin ();
   stable_rev_iter_ = stable_tree_.rbegin ();
@@ -310,16 +302,17 @@ ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_post_insertion_it
   ACE_ASSERT (deprecated_rev_iter_.done () == 1);
 }
 
-
 // Tests stable and deprecated deletion interfaces.
 
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK> void
-ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_tree_deletion ()
+ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_tree_deletion (void)
 {
   // Remove the even numbered entries from each of the trees.
+
   for (int i = 0; i < entry_count_; i += 2)
     {
-      // Test the new stable ACE_Hash_Map_Manager_Ex compliant interface.
+      // Test the new stable ACE_Hash_Map_Manager_Ex compliant
+      // interface.
       ACE_ASSERT (stable_tree_.unbind (key_array_ [i]) == 0);
 
       // Test the deprecated interface.
@@ -327,11 +320,10 @@ ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_tree_deletion ()
     }
 }
 
-
 // Tests forward and reverse iteration after deletions in both trees.
 
 template <class EXT_ID, class INT_ID, class COMPARE_KEYS, class ACE_LOCK> void
-ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_post_deletion_iteration ()
+ACE_RB_Tree_Test<EXT_ID, INT_ID, COMPARE_KEYS, ACE_LOCK>::test_post_deletion_iteration (void)
 {
   // Reset iterators
 
