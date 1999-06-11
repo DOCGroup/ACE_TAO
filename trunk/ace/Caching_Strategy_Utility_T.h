534b6fc031c968de7837847096ff9774ff6f0069
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-11 04:09:48 +0000

    added unused args marco to shut up g++ as well as method clear cache with another arg.

--- trunk/ace/Caching_Strategy_Utility_T.h
+++ trunk/ace/Caching_Strategy_Utility_T.h
@@ -131,6 +131,39 @@ public:
 
 ///////////////////////////////////////////////////////////////////////////
 
+template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES>
+class ACE_Null_Caching_Strategy_Utility : public ACE_Caching_Strategy_Utility <KEY, VALUE, CONTAINER, ATTRIBUTES>
+{
+  // = TITLE
+  //    Defines a dummy helper class for the Caching Strategies.
+  // 
+  // = DESCRIPTION
+  //    This class defines the methods commonly used by the 
+  //    different caching strategies. For instance: clear_cache ()
+  //    method which decides and purges the entry from the container.
+  //    Note: This class is be used with the Null_Caching_Strategy.
+
+public:
+
+  virtual int clear_cache (CONTAINER &container, 
+                           ACE_Cleanup_Strategy<KEY, VALUE, CONTAINER> *cleanup_s,
+                           const unsigned int purge_percent,
+                           unsigned int &entries);
+  // Purge entries from the <container>. The Cleanup_Strategy will do
+  // the actual job of cleanup once the entries to be cleaned up are
+  // decided. NOte: Here it is a no-op.
+  
+  virtual void minimum (CONTAINER &container, 
+                        KEY *&key,
+                        VALUE *&value);
+  // Find the entry with minimum caching attributes.
+  // This is handler specific since this utility is to be used very
+  // specifically for handler who have caching_attributes for server
+  // side acched connection management.Note: Here it is a no-op.
+
+};
+
+///////////////////////////////////////////////////////////////////////////
 
 #if defined (__ACE_INLINE__)
 #include "ace/Caching_Strategy_Utility_T.i"
