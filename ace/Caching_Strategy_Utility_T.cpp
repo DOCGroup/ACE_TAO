29b2abf2ce7284b596329f3cba474e0fdbd74c18
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-11 01:00:50 +0000

    *** empty log message ***

--- ace/Caching_Strategy_Utility_T.cpp
+++ ace/Caching_Strategy_Utility_T.cpp
@@ -17,15 +17,15 @@
 
 ACE_RCSID(ace, Caching_Strategy_Utility_T, "$Id$")
 
-template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> 
+template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES>
 ACE_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::~ACE_Caching_Strategy_Utility (void)
 {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> int 
-ACE_Svc_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::clear_cache (CONTAINER &container, 
+template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> int
+ACE_Svc_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::clear_cache (CONTAINER &container,
                                                                                   ACE_Cleanup_Strategy<KEY, VALUE, CONTAINER> *cleanup_s,
                                                                                   const unsigned int purge_percent,
                                                                                   unsigned int &entries)
@@ -38,45 +38,45 @@ ACE_Svc_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::clear_cache
   // Oops! then thers no way out but exiting. So return an error.
   if (entries == 1)
     return -1;
-  
+
   // Calculate the no of entries to remove from the cache depending
   // upon the <purge_percent>.
   double val = (double) purge_percent / 100;
   int no_of_entries = (int) ceil (val * entries);
- 
+
   KEY *key_to_remove = 0;
   VALUE *value_to_remove = 0;
-  
+
   for (int i = 0; i < no_of_entries ; ++i)
     {
-      this->minimum (container, 
+      this->minimum (container,
                      key_to_remove,
                      value_to_remove);
-       
+
       if (cleanup_s->cleanup (container, key_to_remove, value_to_remove) == -1)
         return -1;
-       
+
       --entries;
-              
+
     }
-  
+
   return 0;
 }
 
-template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> void 
-ACE_Svc_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::minimum (CONTAINER &container, 
+template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> void
+ACE_Svc_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::minimum (CONTAINER &container,
                                                                               KEY *&key,
                                                                               VALUE *&value)
 {
   typedef ACE_TYPENAME CONTAINER::ITERATOR ITERATOR;
   typedef ACE_TYPENAME CONTAINER::ENTRY ITEM;
- 
+
   ITERATOR iter (container);
   ATTRIBUTES  min = 0;
-  ITEM *item = 0;  
+  ITEM *item = 0;
 
   // The iterator moves thru the container searching for the entry with the
-  // lowest ATTRIBUTES.  
+  // lowest ATTRIBUTES.
   for (min = (*iter).int_id_.second (), key = &(*iter).ext_id_, value = &(*iter).int_id_;
        iter.next (item) != 0;
        ++iter)
@@ -93,8 +93,9 @@ ACE_Svc_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::minimum (CO
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> int 
-ACE_Handler_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::clear_cache (CONTAINER &container, 
+
+template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> int
+ACE_Handler_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::clear_cache (CONTAINER &container,
                                                                                       ACE_Cleanup_Strategy<KEY, VALUE, CONTAINER> *cleanup_s,
                                                                                       const unsigned int purge_percent,
                                                                                       unsigned int &entries)
@@ -107,46 +108,45 @@ ACE_Handler_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::clear_c
   // Oops! then thers no way out but exiting. So return an error.
   if (entries == 1)
     return -1;
-  
+
   // Calculate the no of entries to remove from the cache depending
   // upon the <purge_percent>.
   double val = (double) purge_percent / 100;
   int no_of_entries = (int) ceil (val * entries);
- 
+
   KEY *key_to_remove = 0;
   VALUE *value_to_remove = 0;
-  
+
   for (int i = 0; i < no_of_entries ; ++i)
     {
-      this->minimum (container, 
+      this->minimum (container,
                      key_to_remove,
                      value_to_remove);
-       
+
       if (cleanup_s->cleanup (container, key_to_remove, value_to_remove) == -1)
         return -1;
-       
+
       --entries;
-              
     }
-  
+
   return 0;
 }
 
-template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> void 
-ACE_Handler_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::minimum (CONTAINER &container, 
+template <class KEY, class VALUE, class CONTAINER, class ATTRIBUTES> void
+ACE_Handler_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::minimum (CONTAINER &container,
                                                                                   KEY *&key,
                                                                                   VALUE *&value)
 {
   typedef ACE_TYPENAME CONTAINER::ITERATOR ITERATOR;
   typedef ACE_TYPENAME CONTAINER::ENTRY ITEM;
- 
+
   ITERATOR iter (container);
   ATTRIBUTES  min = 0;
-  ITEM *item = 0;  
+  ITEM *item = 0;
 
   for (min = (*iter).int_id_->caching_attributes (), key = &(*iter).ext_id_, value = &(*iter).int_id_;
        iter.next (item) != 0;
-       ++iter)   
+       ++iter)
     {
       // Ah! an item with lower ATTTRIBUTES...
       if ((min.attributes () > (*iter).int_id_->caching_attributes ().attributes ()) && ((*iter).int_id_->active () != 1))
@@ -157,7 +157,8 @@ ACE_Handler_Caching_Strategy_Utility<KEY, VALUE, CONTAINER, ATTRIBUTES>::minimum
 
           value = &(*iter).int_id_;
         }
-    }   
+    }
 
 }
+
 #endif /* CACHING_STRATEGY_UTILITY_T_C */
