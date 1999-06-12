4dff6d9618c57058cbe76ec5a1089079230b05e4
bala <balanatarajan@users.noreply.github.com>
1999-06-12 00:24:02 +0000

    *** empty log message ***

--- TAO/examples/POA/TIE/run_test.pl
+++ TAO/examples/POA/TIE/run_test.pl
@@ -26,6 +26,12 @@ unlink $iorfile_6;
 
 $SV = Process::Create ($EXEPREFIX."server$Process::EXE_EXT");
 
+if (ACE::waitforfile_timed ($iorfile_1, 5) == -1) {
+  print STDERR "ERROR: cannot find file <$iorfile>\n";
+  $SV->Kill (); $SV->TimedWait (1);
+  exit 1;
+}
+
 ACE::waitforfile ($iorfile_1);
 $status  = system ("client$Process::EXE_EXT  -a file://$iorfile_1");
 ACE::waitforfile ($iorfile_2);
@@ -34,10 +40,10 @@ ACE::waitforfile ($iorfile_3);
 $status  = system ("client$Process::EXE_EXT  -c file://$iorfile_3 ");
 ACE::waitforfile ($iorfile_4);
 $status  = system ("client$Process::EXE_EXT  -d file://$iorfile_4 ");
-if (ACE::waitforfile_timed ($iorfile_5,2) == 0) {
+if (ACE::waitforfile_timed ($iorfile_5,1) == 0) {
 $status  = system ("client$Process::EXE_EXT  -e file://$iorfile_5 ");
 }
-if (ACE::waitforfile_timed ($iorfile_6,2) == 0) {
+if (ACE::waitforfile_timed ($iorfile_6,1) == 0) {
 $status  = system ("client$Process::EXE_EXT  -f file://$iorfile_6 ");
 }
 
