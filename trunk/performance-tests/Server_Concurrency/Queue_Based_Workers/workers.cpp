67b189b14d830d03f9a96fa136cd6a1c32430216
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-14 01:18:02 +0000

    *** empty log message ***

--- trunk/performance-tests/Server_Concurrency/Queue_Based_Workers/workers.cpp
+++ trunk/performance-tests/Server_Concurrency/Queue_Based_Workers/workers.cpp
@@ -2,6 +2,9 @@
 
 #include "ace/Task.h"
 #include "ace/Get_Opt.h"
+#include "ace/High_Res_Timer.h"
+#include "ace/Sched_Params.h"
+#include "../Latency_Stats.h"
 
 static size_t number_of_messages = 100;
 static size_t message_size = 100;
@@ -9,6 +12,24 @@ static size_t number_of_workers = 10;
 static size_t burst_size = 10;
 static size_t timeout_between_bursts = 1;
 static size_t debug = 0;
+
+static ACE_Data_Block *data_block = 0;
+
+class Message_Block : public ACE_Message_Block
+{
+public:
+  Message_Block (ACE_Data_Block *data_block,
+                 ACE_hrtime_t start_of_burst);
+
+  ACE_hrtime_t start_of_burst_;
+};
+
+Message_Block::Message_Block (ACE_Data_Block *data_block,
+                              ACE_hrtime_t start_of_burst)
+  : ACE_Message_Block (data_block),
+    start_of_burst_ (start_of_burst)
+{
+}
 
 typedef ACE_Task<ACE_MT_SYNCH> TASK;
 
@@ -20,6 +41,7 @@ public:
 
   size_t messages_dequeued_;
 
+  Latency_Stats stats_;
 };
 
 class IO_Task : public TASK
@@ -40,8 +62,8 @@ Worker_Task::svc (void)
 {
   for (;;)
     {
-      ACE_Message_Block *message_block = 0;
-      int result = this->getq (message_block);
+      ACE_Message_Block *mb = 0;
+      int result = this->getq (mb);
       if (result == -1)
         {
           ACE_ERROR_RETURN ((LM_ERROR,
@@ -49,6 +71,12 @@ Worker_Task::svc (void)
                              "getq error"),
                             -1);
         }
+
+      Message_Block *message_block =
+        ACE_dynamic_cast (Message_Block *, mb);
+
+      // Record arrival time.
+      this->stats_.sample (ACE_OS::gethrtime () - message_block->start_of_burst_);
 
       ACE_Message_Block::ACE_Message_Type message_type =
         message_block->msg_type ();
@@ -78,6 +106,18 @@ Worker_Task::svc (void)
                           "(%t) dequeued its %d message\n",
                           this->messages_dequeued_));
             }
+
+          //
+          // Process message here.
+          //
+
+          for (int j = 0; j < message_size; ++j)
+            {
+              // Eat a little CPU
+              /* takes about 40.2 usecs on a 167 MHz Ultra2 */
+              u_long n = 1279UL;
+              ACE::is_prime (n, 2, n / 2);
+            }
         }
     }
 
@@ -99,13 +139,16 @@ IO_Task::svc (void)
   // Data messages.
   while (number_of_messages > 0)
     {
+      ACE_hrtime_t start_of_burst = ACE_OS::gethrtime ();
+
       for (i = 1;
            i <= burst_size && number_of_messages > 0;
            ++i, --number_of_messages, ++messages_queued)
         {
-          ACE_Message_Block *message_block = 0;
+          Message_Block *message_block = 0;
           ACE_NEW_RETURN (message_block,
-                          ACE_Message_Block (message_size),
+                          Message_Block (data_block,
+                                         start_of_burst),
                           -1);
 
           int result = this->putq (message_block);
@@ -201,10 +244,24 @@ parse_args (int argc, ASYS_TCHAR *argv[])
 int
 main (int argc, ASYS_TCHAR *argv[])
 {
+  move_to_rt_class ();
+  ACE_High_Res_Timer::calibrate ();
+
   int result = parse_args (argc, argv);
   if (result != 0)
     {
       return result;
+    }
+
+  size_t i = 0;
+
+  ACE_NEW_RETURN (data_block,
+                  ACE_Data_Block,
+                  -1);
+
+  for (i = 0; i < number_of_messages; ++i)
+    {
+      data_block->duplicate ();
     }
 
   ACE_Message_Queue<ACE_MT_SYNCH> message_queue;
@@ -215,18 +272,38 @@ main (int argc, ASYS_TCHAR *argv[])
                   Worker_Task *[number_of_workers],
                   -1);
 
+  int priority =
+    (ACE_Sched_Params::priority_min (ACE_SCHED_FIFO) +
+     ACE_Sched_Params::priority_max (ACE_SCHED_FIFO)) / 2;
+  priority = ACE_Sched_Params::next_priority (ACE_SCHED_FIFO, priority);
+
+  long flags = THR_BOUND | THR_SCHED_FIFO;
+
   // Create and activate them.
-  for (size_t i = 0; i < number_of_workers; ++i)
+  for (i = 0; i < number_of_workers; ++i)
     {
       ACE_NEW_RETURN (workers[i],
                       Worker_Task (&message_queue),
                       -1);
 
       // Activate the workers.
-      result = workers[i]->activate (THR_BOUND);
+      result = workers[i]->activate (flags,
+                                     1,
+                                     1,
+                                     priority);
       if (result != 0)
         {
-          return result;
+          flags = THR_BOUND;
+          priority = ACE_Sched_Params::priority_min (ACE_SCHED_OTHER,
+                                                     ACE_SCOPE_THREAD);
+          result = workers[i]->activate (flags,
+                                         1,
+                                         1,
+                                         priority);
+          if (result != 0)
+            {
+              return result;
+            }
         }
     }
 
@@ -243,7 +320,21 @@ main (int argc, ASYS_TCHAR *argv[])
   // Wait for all threads to terminate.
   result = ACE_Thread_Manager::instance ()->wait ();
 
+  Latency_Stats latency;
+  for (i = 0; i < number_of_workers; ++i)
+    {
+      latency.accumulate (workers[i]->stats_);
+    }
+
+  ACE_DEBUG ((LM_DEBUG, "\nTotals:\n"));
+  latency.dump_results (argv[0], "latency");
+
+  for (i = 0; i < number_of_workers; ++i)
+    {
+      delete workers[i];
+    }
   delete[] workers;
+  delete data_block;
 
   return result;
 }
