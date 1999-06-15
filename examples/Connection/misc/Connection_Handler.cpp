111fe6a3512f6cb0dfbc0f5ea38dd27fb7992640
Steve Huston <shuston@riverace.com>
1999-06-15 21:50:47 +0000

    Correct signed/unsigned mismatch

--- examples/Connection/misc/Connection_Handler.cpp
+++ examples/Connection/misc/Connection_Handler.cpp
@@ -141,7 +141,7 @@ Connection_Handler::handle_input (ACE_HANDLE)
                          this->get_handle ()),
                         -1);
     default:
-      if (((int) buf[0]) == EOF)
+      if (((unsigned int) buf[0]) == EOF)
 	ACE_ERROR_RETURN ((LM_ERROR,
 			   "(%P|%t) closing log daemon (fd = %d)\n",
                            this->get_handle ()),
