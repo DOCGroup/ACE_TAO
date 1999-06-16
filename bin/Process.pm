5d637c04fb79192e27e10924c1796c9c092c75d9
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-16 21:12:00 +0000

    ChangeLogTag:Wed Jun 16 16:10:06 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- bin/Process.pm
+++ bin/Process.pm
@@ -1,11 +1,16 @@
 # $Id$
+
 $inc = "Process_Unix.pm";
 $DIR_SEPARATOR = "/";
+$EXE_EXT = "";
+$newwindow = "";
 
 if ($^O eq "MSWin32")
 {
-	$inc = "Process_Win32.pm";
-	$DIR_SEPARATOR = "\\";
+  $inc = "Process_Win32.pm";
+  $DIR_SEPARATOR = "\\";
+  $EXE_EXT = ".exe";
+  $newwindow = "no";
 }
 
 require $inc;
