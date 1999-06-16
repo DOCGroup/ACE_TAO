2e973be1fc6dde01d51c0f0756100fc568f2f61d
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-16 19:31:18 +0000

    Fixed #pragma implementation line.

--- trunk/ace/Caching_Strategy_Utility_T.h
+++ trunk/ace/Caching_Strategy_Utility_T.h
@@ -173,7 +173,7 @@ public:
 #endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
 
 #if defined (ACE_TEMPLATES_REQUIRE_PRAGMA)
-#pragma implementation "ace/Caching_Strategy_Utility_T.cpp"
+#pragma implementation ("ace/Caching_Strategy_Utility_T.cpp")
 #endif /* ACE_TEMPLATES_REQUIRE_PRAGMA */
 
 #endif /* CACHING_STRATEGY_UTILITY_H */
