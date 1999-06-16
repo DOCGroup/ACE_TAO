31a00ca8b110554cc54f24f716e61a58e4edb2f6
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-16 23:01:12 +0000

    .

--- trunk/TAO/examples/Simple/time-date/Time_Date_i.cpp
+++ trunk/TAO/examples/Simple/time-date/Time_Date_i.cpp
@@ -23,6 +23,8 @@ Time_Date_i::~Time_Date_i (void)
 void
 Time_Date_i::bin_date (CORBA::Long_out time_date,
                        CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+
 {
   time_date = ACE_OS::time (0);
 }
@@ -32,6 +34,7 @@ Time_Date_i::bin_date (CORBA::Long_out time_date,
 void 
 Time_Date_i::str_date (CORBA::String_out time_date,
                        CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
 {
   const time_t time = ACE_OS::time (0);
 
