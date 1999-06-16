77a888b30c188ddfcbfda7a9d0c76cc03e8430ea
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-16 00:21:05 +0000

    *** empty log message ***

--- TAO/tests/Multiple_Inheritance/Multiple_Inheritance_i.h
+++ TAO/tests/Multiple_Inheritance/Multiple_Inheritance_i.h
@@ -8,18 +8,22 @@ class Multiple_Inheritance_i : public POA_D
 {
 public:
   virtual CORBA::String method1 (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
     {
       return CORBA::string_dup ("method1");
     }
   virtual CORBA::String method2 (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
     {
       return CORBA::string_dup ("method2");
     }
   virtual CORBA::String method3 (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
     {
       return CORBA::string_dup ("method3");
     }
   virtual CORBA::String method4 (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
     {
       return CORBA::string_dup ("method4");
     }
