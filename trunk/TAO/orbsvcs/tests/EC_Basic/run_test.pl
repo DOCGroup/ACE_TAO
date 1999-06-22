29ce37253eed96ff68ca57158cf2633b13391aee
jwh1 <jwh1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-22 19:47:06 +0000

    Changed to reflect new guidelines.

--- trunk/TAO/orbsvcs/tests/EC_Basic/run_test.pl
+++ trunk/TAO/orbsvcs/tests/EC_Basic/run_test.pl
@@ -5,7 +5,18 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-$status = system ($EXEPREFIX."EC_Basic".$EXE_EXT, "");
+unshift @INC, '../../../../bin';
+require ACEutils;
+require Process;
+$status = 0;
+
+$TEST = Process::Create ($EXEPREFIX."EC_Basic".$EXE_EXT, "");
+
+if ($TEST->TimedWait (60) == -1) {
+  print STDERR "ERROR: test timedout\n";
+  $status = 1;
+  $TEST->Kill (); $TEST->TimedWait (1);
+}
 
 # @@ Capture any errors from the server too.
 exit $status;
--- trunk/TAO/orbsvcs/tests/EC_Custom_Marshal/run_test.pl
+++ trunk/TAO/orbsvcs/tests/EC_Custom_Marshal/run_test.pl
@@ -6,36 +6,49 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 unshift @INC, '../../../../bin';
+require ACEutils;
 require Process;
 
 $NS_ior = "NameService.ior";
 $sleeptime = 5;
+$status = 0;
 
 $NS = Process::Create ("..".$DIR_SEPARATOR.
                        "..".$DIR_SEPARATOR.
                        "Naming_Service".$DIR_SEPARATOR.
-                       "Naming_Service".$EXE_EXT.
+                       "Naming_Service".$Process::EXE_EXT,
                        " -o $NS_ior ");
 sleep $sleeptime;
 
 $ES = Process::Create ("..".$DIR_SEPARATOR.
                        "..".$DIR_SEPARATOR.
                        "Event_Service".$DIR_SEPARATOR.
-                       "Event_Service".$EXE_EXT);
+                       "Event_Service".$Process::EXE_EXT, "");
 
-$C = Process::Create ($EXEPREFIX."ECM_Consumer".$EXE_EXT);
+$C = Process::Create ($EXEPREFIX."ECM_Consumer".$Process::EXE_EXT, "");
 
 sleep $sleeptime;
 
-$S = Process::Create ($EXEPREFIX."ECM_Supplier".$EXE_EXT);
+$S = Process::Create ($EXEPREFIX."ECM_Supplier".$Process::EXE_EXT, "");
 
-$C->Wait ();
-$S->Wait ();
+if ($C->TimedWait (60) == -1) {
+  $status = 1;
+  print STDERR "ERROR: consumer timedout\n";
+  $C->Kill (); $C->TimedWait (1);
+}
 
-$NS->Kill (); $NS->Wait ();
-$ES->Kill (); $ES->Wait ();
+if ($S->TimedWait (60) == -1) {
+  $status =1;
+  print STDERR "ERROR: supplier timedout\n";
+  $S->Kill (); $S->TimedWait (1);
+}
+
+$NS->Kill (); $NS->TimedWait (1);
+$ES->Kill (); $ES->TimedWait (1);
 
 unlink $NS_ior;
 
 # @@ Capture the errors from the processes.
-exit 0;
+exit $status;
+
+
