8284450f6243259782d2da1c817add622ef8ca97
jwh1 <jwh1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-22 18:52:22 +0000

    Changed to reflect new guidelines.

--- TAO/examples/Simple/bank/run_test.pl
+++ TAO/examples/Simple/bank/run_test.pl
@@ -8,16 +8,27 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 use lib "../../../../bin";
 require ACEutils;
 require Process;
+$status = 0;
 
 $iorfile = "grid.ior";
 
 $SV = Process::Create ($EXEPREFIX."server$EXE_EXT", "-o $iorfile ");
 
-ACE::waitforfile ($iorfile);
+if (ACE::waitforfile_timed ($iorfile, 5) == -1) {
+  print STDERR "ERROR: timedout waiting for file <$iorfile>\n";
+  $SV->Kill (); $SV->TimedWait (1);
+  exit 1;
+}
 
-$status  = system ($EXEPREFIX."client$EXE_EXT -f $iorfile");
-
-$SV->Kill (); $SV->Wait ();
+$client = Process::Create($EXEPREFIX."client$EXE_EXT","-f $iorfile");
+if ($client->TimedWait (60) == -1) {
+  print STDERR "ERROR: the client timedout\n";
+  $status = 1;
+  $client->Kill (); $client->TimedWait (1);
+}
+  
+  
+$SV->Kill (); $SV->TimedWait (1);
 
 unlink $iorfile;
 
--- TAO/examples/Simple/grid/run_test.pl
+++ TAO/examples/Simple/grid/run_test.pl
@@ -9,15 +9,26 @@ use lib "../../../../bin";
 require ACEutils;
 require Process;
 
+$status = 0;
 $iorfile = "grid.ior";
 
 $SV = Process::Create ($EXEPREFIX."server$EXE_EXT", "-o $iorfile ");
 
-ACE::waitforfile ($iorfile);
+if (ACE::waitforfile_timed ($iorfile, 5) == -1) {
+  print STDERR "ERROR: timedout waiting for file <$iorfile>\n";
+  $SV->Kill (); $SV->TimedWait (1);
+  exit 1;
+}
 
-$status  = system ($EXEPREFIX."client$EXE_EXT -f $iorfile -x -w 12 -h 12 -p 2 -q 2 -v 2345");
+$client = Process::Create($EXEPREFIX."client$EXE_EXT", "-f $iorfile -x -w 12 -h 12 -p 2 -q 2 -v 2345");
 
-$SV->Kill (); $SV->Wait ();
+if ($client->TimedWait (60) == -1) {
+  print STDERR "ERROR: client timedout\n";
+  $status = 1;
+  $client->Kill (); $client->TimedWait (1);
+}
+
+$SV->Kill (); $SV->TimedWait (1);
 
 unlink $iorfile;
 
