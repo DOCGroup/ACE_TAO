432432ad3c2fb2c7def8d52658517db739d81dd6
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-22 04:34:29 +0000

    ChangeLogTag:Mon Jun 21 23:34:06 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/TAO/tao/IORManipulation.h
+++ trunk/TAO/tao/IORManipulation.h
@@ -41,26 +41,52 @@ public:
 
   virtual CORBA::Object_ptr merge_iors (
     const TAO_IOP::TAO_IOR_Manipulation::IORList & iors,
-    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ());
+    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ())
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        TAO_IOP::TAO_IOR_Manipulation::EmptyProfileList,
+        TAO_IOP::TAO_IOR_Manipulation::Duplicate,
+        TAO_IOP::TAO_IOR_Manipulation::Invalid_IOR
+      ));
 
   virtual CORBA::Object_ptr add_profiles (
       CORBA::Object_ptr ior1,
       CORBA::Object_ptr ior2,
-      CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ());
+      CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ())
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        TAO_IOP::TAO_IOR_Manipulation::EmptyProfileList,
+        TAO_IOP::TAO_IOR_Manipulation::Duplicate,
+        TAO_IOP::TAO_IOR_Manipulation::Invalid_IOR
+      ));
 
   virtual CORBA::Object_ptr remove_profiles (
     CORBA::Object_ptr ior1,
     CORBA::Object_ptr ior2,
-    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ());
+    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ())
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        TAO_IOP::TAO_IOR_Manipulation::Invalid_IOR,
+        TAO_IOP::TAO_IOR_Manipulation::EmptyProfileList,
+        TAO_IOP::TAO_IOR_Manipulation::NotFound
+      ));
 
   virtual CORBA::ULong is_in_ior (
     CORBA::Object_ptr ior1,
     CORBA::Object_ptr ior2,
-    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ());
+    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ())
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        TAO_IOP::TAO_IOR_Manipulation::NotFound
+      ));
 
   virtual CORBA::ULong get_profile_count (
     CORBA::Object_ptr ior,
-    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ());
+    CORBA::Environment &ACE_TRY_ENV = CORBA::default_environment ())
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        TAO_IOP::TAO_IOR_Manipulation::EmptyProfileList
+      ));
 };
 
 #endif /* TAO_IOR_MANIPULATION_H */
