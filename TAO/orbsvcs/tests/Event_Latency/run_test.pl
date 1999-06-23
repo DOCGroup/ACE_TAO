8559c1ae3badacdc7c5fd82e1a1cd5885f14c09e
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-23 01:25:56 +0000

    ChangeLogTag:Tue Jun 22 20:23:07 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/orbsvcs/tests/Event_Latency/run_test.pl
+++ TAO/orbsvcs/tests/Event_Latency/run_test.pl
@@ -13,17 +13,29 @@ $NS_ior = "NameService.ior";
 $sleeptime = 3;
 $status = 0;
 
-$NS = Process::Create ("../../Naming_Service/Naming_Service".$EXE_EXT,
+$NS = Process::Create ("..".$DIR_SEPARATOR
+		       ."..".$DIR_SEPARATOR
+		       ."Naming_Service".$DIR_SEPARATOR
+		       ."Naming_Service".$EXE_EXT,
 		       " -o $NS_ior ");
 
-sleep $sleeptime;
+if (ACE::waitforfile_timed ($NS_ior, 5) == -1) {
+  print STDERR "ERROR: waiting for naming service IOR file\n";
+  $NS->Kill (); $NS->TimedWait (1);
+  exit 1;
+}
 
-$ES = Process::Create ("../../Event_Service/Event_Service".$EXE_EXT, "");
+$ES = Process::Create ("..".$DIR_SEPARATOR
+		       ."..".$DIR_SEPARATOR
+		       ."Event_Service".$DIR_SEPARATOR
+		       ."Event_Service".$EXE_EXT,
+		       "-ORBNameServiceIOR file://$NS_ior");
 
 sleep $sleeptime;
 
 $TEST = Process::Create ($EXEPREFIX."Event_Latency".$EXE_EXT,
-                  " -j -m 100");
+			 "-ORBNameServiceIOR file://$NS_ior"
+			 ." -j -m 100");
 
 if ($TEST->TimedWait (60) == -1) {
   print STDERR "ERROR: test timedout\n";
@@ -34,7 +46,7 @@ if ($TEST->TimedWait (60) == -1) {
 $NS->Kill (); $NS->TimedWait (1);
 $ES->Kill (); $ES->TimedWait (1);
 
-unlink ($NS_ior);
+unlink $NS_ior;
 
 # @@ Capture any errors from the server too.
 exit $status;
