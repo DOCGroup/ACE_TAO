4e2cdd5cb09e0265ac230c45792b4a0877a453b1
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-23 16:33:21 +0000

    Cosmetic updates.

--- trunk/TAO/tao/UIOP_Acceptor.cpp
+++ trunk/TAO/tao/UIOP_Acceptor.cpp
@@ -121,8 +121,8 @@ TAO_UIOP_Acceptor::open (TAO_ORB_Core *orb_core,
                          ACE_CString &address)
 {
   if (major >= 0 && minor >= 0)
-    this->version_.set_version (ACE_static_cast (CORBA::Octet,major),
-                                ACE_static_cast (CORBA::Octet,minor));
+    this->version_.set_version (ACE_static_cast (CORBA::Octet, major),
+                                ACE_static_cast (CORBA::Octet, minor));
   ACE_UNIX_Addr addr (address.c_str ());
 
   return this->open_i (orb_core, addr);
