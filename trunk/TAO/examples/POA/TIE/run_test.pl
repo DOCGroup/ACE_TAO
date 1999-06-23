01275462eca020ca69fd9f52e794ed9f2308d19f
jwh1 <jwh1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-23 21:38:19 +0000

    Terminates servers nicely and times out correctly.

--- trunk/TAO/examples/POA/TIE/run_test.pl
+++ trunk/TAO/examples/POA/TIE/run_test.pl
@@ -8,6 +8,8 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 unshift @INC, '../../../../bin';
 require ACEutils;
 
+$status = 0;
+
 $iorfile = "ior";
 
 $iorfile_1 = $iorfile."_1";
@@ -34,11 +36,24 @@ if (ACE::waitforfile_timed ($iorfile_1, 5) == -1) {
   exit 1;
 }
 
-ACE::waitforfile ($iorfile_1);
-$status  = Process::Create ($EXEPREFIX."client$EXE_EXT  -a file://$iorfile_1 -b file://$iorfile_2 -c file://$iorfile_3 -d file://$iorfile_4");
+$client  = Process::Create ($EXEPREFIX."client$EXE_EXT",
+                            "-a file://$iorfile_1 -b file://$iorfile_2 -c file://$iorfile_3 -d file://$iorfile_4");
+
+if ($client->TimedWait (60) == -1) {
+  print STDERR "ERROR: client timedout\n";
+  $status = 1;
+  $client->Kill (); $client->TimedWait (1);
+}
 
 if (ACE::waitforfile_timed ($iorfile_5,1) == 0) {
-$status  = Process::Create ($EXEPREFIX."client$EXE_EXT  -e file://$iorfile_5 -f file://$iorfile_6");
+  $client  = Process::Create ($EXEPREFIX."client$EXE_EXT",
+                              " -e file://$iorfile_5 -f file://$iorfile_6");
+
+  if ($client->TimedWait (60) == -1) {
+    print STDERR "ERROR: client timedout\n";
+    $status = 1;
+    $client->Kill (); $client->TimedWait (1);
+  }
 }
 
 unlink $iorfile_1;
@@ -48,6 +63,10 @@ unlink $iorfile_4;
 unlink $iorfile_5;
 unlink $iorfile_6;
 
-$SV->Kill ();$SV->Wait ();
+$SV->Terminate (); if ($SV->TimedWait (5) == -1) {
+  print STDERR "ERROR: couldn't terminate server nicely\n";
+  $status = 1;
+  $SV->Kill (); $SV->TimedWait (1);
+}
 
-exit $client;
+exit $status;
