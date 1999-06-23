54e8868643b75efff404a9cd1fd25c073e540999
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-23 05:42:56 +0000

    .

--- trunk/TAO/examples/Simple/time-date/Time_Date_Client_i.cpp
+++ trunk/TAO/examples/Simple/time-date/Time_Date_Client_i.cpp
@@ -19,8 +19,8 @@ Time_Date_Client_i::~Time_Date_Client_i (void)
 }
 
 int
-Time_Date_Client_i::parse_args (int argc,
-                                char *argv[])
+Time_Date_Client_i::parse_args (int,
+                                char *[])
 {
   return 0;
 }
