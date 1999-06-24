f29de1885cdcb95d38de5b011371c95249034a4d
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-24 08:44:41 +0000

    Works with new IR.

--- trunk/TAO/orbsvcs/tests/Time/run_test.pl
+++ trunk/TAO/orbsvcs/tests/Time/run_test.pl
@@ -20,19 +20,23 @@ unlink $server_ior;
 unlink $clerk_ior;
 unlink $implrepo_ior;
 
-$time_dir = "..$DIR_SEPARATOR..".$DIR_SEPARATOR."Time_Service".$DIR_SEPARATOR;
+$implrepo_program = "..$DIR_SEPARATOR..$DIR_SEPARATOR"."ImplRepo_Service".$DIR_SEPARATOR."ImplRepo_Service".$EXE_EXT;
+$tao_ir_program = "..$DIR_SEPARATOR..$DIR_SEPARATOR"."ImplRepo_Service".$DIR_SEPARATOR."tao_ir".$EXE_EXT;
+$server_program = "..$DIR_SEPARATOR..$DIR_SEPARATOR"."Time_Service".$DIR_SEPARATOR."Time_Service_Server".$EXE_EXT;
+$clerk_program = "..$DIR_SEPARATOR..$DIR_SEPARATOR"."Time_Service".$DIR_SEPARATOR."Time_Service_Clerk".$EXE_EXT;
+$client_program = $EXEPREFIX."client".$EXE_EXT;
 
 sub time_service_test_using_naming_service
 {
-    $SV1 = Process::Create ($time_dir."Time_Service_Server".$EXE_EXT,"");
+    $SV1 = Process::Create ($server_program,"");
 
     sleep 5;
 
-    $SV2 = Process::Create ($time_dir."Time_Service_Clerk".$EXE_EXT,"-t 2");
+    $SV2 = Process::Create ($clerk_program,"-t 2");
 
     sleep 10;
 
-    $CL = Process::Create ($EXEPREFIX."client".$EXE_EXT, "");
+    $CL = Process::Create ($client_program, "");
     
     if ($CL->TimedWait (60) == -1) {
       print STDERR "ERROR: client timedout\n";
@@ -53,7 +57,7 @@ sub time_service_test_using_naming_service
 
 sub time_service_test_using_files
 {
-    $SV1 = Process::Create ($time_dir."Time_Service_Server".$EXE_EXT,
+    $SV1 = Process::Create ($server_program,
                             "-o $server_ior");
 
     if (ACE::waitforfile_timed ($server_ior, 5) == -1) {
@@ -65,8 +69,8 @@ sub time_service_test_using_files
       
       sleep 5;
       
-      $SV2 = Process::Create ($time_dir."Time_Service_Clerk".$EXE_EXT,
-                              "-f $server_ior -o clerk_ior -t 2");
+      $SV2 = Process::Create ($clerk_program,
+                              "-f $server_ior -o $clerk_ior -t 2");
       
       if (ACE::waitforfile_timed ($clerk_ior, 5) == -1) {
         print STDERR "ERROR: timedout waiting for file <$clerk_ior>\n";
@@ -77,8 +81,8 @@ sub time_service_test_using_files
         
         sleep 10;
         
-        $CL = Process::Create ($EXEPREFIX."client".$EXE_EXT,
-                          " -f clerk_ior");
+        $CL = Process::Create ($client_program,
+                          " -f $clerk_ior");
         if ($CL->TimedWait (60) == -1) {
           print STDERR "ERROR: client timedout\n";
           $status = 1;
@@ -102,9 +106,8 @@ sub time_service_test_using_files
 
 sub time_service_test_using_ir
 {
-  $ir_dir = "..".$DIR_SEPARATOR."..".$DIR_SEPARATOR."ImplRepo_Service".$DIR_SEPARATOR;
-  $IR = Process::Create ($ir_dir."ImplRepo_Service".$EXE_EXT,
-                         "-ORBsvcconf implrepo.conf -d 1");
+  $IR = Process::Create ($implrepo_program,
+                         "-o $implrepo_ior -d 1");
 
   if (ACE::waitforfile_timed ($implrepo_ior, 5) == -1) {
     print STDERR "ERROR: timedout waiting for file <$implrepo_ior>\n";
@@ -112,8 +115,15 @@ sub time_service_test_using_ir
     exit 1;
   } 
 
-  $SV1 = Process::Create ($time_dir."Time_Service_Server".$EXE_EXT,
-                         "-o $server_ior -i -r");
+  $TIR = Process::Create ($tao_ir_program, 
+                          "-ORBImplRepoIOR file://$implrepo_ior add ".
+                          "time_server -c \"$server_program ".
+                          "-ORBImplRepoIOR file://$implrepo_ior -i\"");
+
+  sleep 5;
+
+  $SV1 = Process::Create ($server_program,
+                         "-ORBImplRepoIOR file://$implrepo_ior -o $server_ior -i");
 
   if (ACE::waitforfile_timed ($server_ior, 5) == -1) {
     print STDERR "ERROR: timedout waiting for file <$implerepo_ior>\n";
@@ -124,12 +134,12 @@ sub time_service_test_using_ir
 
   sleep 10;
 
-  $SV2 = Process::Create ($time_dir."Time_Service_Clerk".$EXE_EXT,
-                          "-f $server_ior -o clerk_ior");
+  $SV2 = Process::Create ($clerk_program,
+                          "-f $server_ior -o $clerk_ior");
 
   sleep 10;
 
-  $CL = Process::Create ($EXEPREFIX."client.$EXE_EXT", "-f $clerk_ior");
+  $CL = Process::Create ($client_program, "-f $clerk_ior");
   
   if ($CL->TimedWait (60) == -1) {
     print STDERR "ERROR: client timedout\n";
@@ -142,6 +152,13 @@ sub time_service_test_using_ir
     print STDERR "ERROR: couldn't shutdown repository nicely\n";
     $status = 1;
     $IR->Kill (); $IR->TimedWait (1);
+  }
+
+  $TIR->Terminate ();
+  if ($TIR->TimedWait (5) == -1) {
+    print STDERR "ERROR: couldn't shutdown tao_ir nicely\n";
+    $status = 1;
+    $TIR->Kill (); $TIR->TimedWait (1);
   }
 
   $SV1->Terminate ();
@@ -171,6 +188,7 @@ for ($i = 0; $i <= $#ARGV; $i++)
       print "\n";
       print "use_naming               -- Runs the test using Naming Service\n";
       print "use_files                -- Runs the test using IOR Files\n";
+      print "use_ir                   -- Runs the test with the IR\n";
       exit;
     }
 
