95be5e57c3eae17f0c0dcc79ded0d624b6649141
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-25 18:08:12 +0000

    Fixed ACE_TRY stuff and cosmetic changes.

--- trunk/TAO/performance-tests/Pluggable/server.cpp
+++ trunk/TAO/performance-tests/Pluggable/server.cpp
@@ -14,34 +14,45 @@ main (int argc, char *argv[])
 
   ACE_DEBUG ((LM_DEBUG,
               "\n\tPluggable_Test: server\n\n"));
-  ACE_TRY_NEW_ENV
+
+  ACE_DECLARE_NEW_CORBA_ENV;
+
+  ACE_TRY
     {
-      int status = pp_test_server.init (argc, argv, ACE_TRY_ENV);
+      int status = pp_test_server.init (argc, 
+                                        argv, 
+                                        ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       if (status == -1)
         {
-          ACE_TRY_ENV.print_exception ("Initialization Exception");
-          return -1;
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "Initialization Error\n"),
+                            -1);
         }
       else
         {
           pp_test_server.run (ACE_TRY_ENV);
           ACE_TRY_CHECK;
         }
+
+        return 0;
     }
   ACE_CATCH (CORBA::SystemException, sysex)
     {
       ACE_UNUSED_ARG (sysex);
-      ACE_TRY_ENV.print_exception ("System Exception");
+      ACE_PRINT_EXCEPTION (sysex,
+                           "System Exception");
       return -1;
     }
   ACE_CATCH (CORBA::UserException, userex)
     {
       ACE_UNUSED_ARG (userex);
-      ACE_TRY_ENV.print_exception ("User Exception");
+      ACE_PRINT_EXCEPTION (userex,
+                           "User Exception");
       return -1;
     }
   ACE_ENDTRY;
   ACE_TIMEPROBE_PRINT;
-  return 0;
+  ACE_CHECK_RETURN (0);
 }
