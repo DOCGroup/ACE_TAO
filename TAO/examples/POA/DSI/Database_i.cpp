3e525281501eccbd67e15febf9e7ea38c86f4849
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-06-25 22:09:51 +0000

    .

--- TAO/examples/POA/DSI/Database_i.cpp
+++ TAO/examples/POA/DSI/Database_i.cpp
@@ -21,13 +21,15 @@ DatabaseImpl::Entry::Entry (CORBA::ORB_ptr orb,
     poa_ (PortableServer::POA::_duplicate (poa))
 {
   // Get the POA Current object reference
-  CORBA::Object_var obj = this->orb_->resolve_initial_references ("POACurrent",
-                                                                  ACE_TRY_ENV);
+  CORBA::Object_var obj =
+    this->orb_->resolve_initial_references ("POACurrent",
+                                            ACE_TRY_ENV);
   ACE_CHECK;
 
   // Narrow the object reference to a POA Current reference
-  this->poa_current_ = PortableServer::Current::_narrow (obj.in (),
-                                                         ACE_TRY_ENV);
+  this->poa_current_ =
+    PortableServer::Current::_narrow (obj.in (),
+                                      ACE_TRY_ENV);
   ACE_CHECK;
 }
 
@@ -40,7 +42,7 @@ DatabaseImpl::Entry::invoke (CORBA::ServerRequest_ptr request,
                              CORBA::Environment &ACE_TRY_ENV)
 {
   // The servant determines the key associated with thex database
-  // entry represented by self
+  // entry represented by self.
   PortableServer::ObjectId_var oid =
     this->poa_current_->get_object_id (ACE_TRY_ENV);
   ACE_CHECK;
@@ -50,17 +52,17 @@ DatabaseImpl::Entry::invoke (CORBA::ServerRequest_ptr request,
     PortableServer::ObjectId_to_string (oid.in ());
 
   // Get the operation name for this request
-  const char *operation = request->operation ();
+  const char *operation =
+    request->operation ();
 
-  if (ACE_OS::strcmp (operation, "_is_a") == 0)
+  if (ACE_OS::strcmp (operation,
+                      "_is_a") == 0)
     {
       this->is_a (request, ACE_TRY_ENV);
       ACE_CHECK;
     }
   else
-    {
-      ACE_THROW (CORBA::NO_IMPLEMENT ());
-    }
+    ACE_THROW (CORBA::NO_IMPLEMENT ());
 }
 
 void
@@ -82,20 +84,22 @@ DatabaseImpl::Entry::is_a (CORBA::ServerRequest_ptr request,
                       ACE_TRY_ENV);
   ACE_CHECK;
 
-  CORBA_NamedValue_ptr nv = list->item (0, ACE_TRY_ENV);
+  CORBA_NamedValue_ptr nv = list->item (0,
+                                        ACE_TRY_ENV);
   ACE_CHECK;
 
   CORBA::Any_ptr ap = nv->value ();
   char *value;
   *ap >>= value;
 
-  const char *object_id = CORBA::_tc_Object->id (ACE_TRY_ENV);
+  const char *object_id =
+    CORBA::_tc_Object->id (ACE_TRY_ENV);
   ACE_CHECK;
 
   CORBA::Boolean result;
-  if (!ACE_OS::strcmp (value, "IDL:Database/Employee:1.0") ||
-      !ACE_OS::strcmp (value, "IDL:Database/Entry:1.0") ||
-      !ACE_OS::strcmp (value, object_id))
+  if (ACE_OS::strcmp (value, "IDL:Database/Employee:1.0") == 0
+      || ACE_OS::strcmp (value, "IDL:Database/Entry:1.0") == 0
+      || ACE_OS::strcmp (value, object_id) == 0)
     result = 1;
   else
     result = 0;
@@ -152,46 +156,58 @@ DatabaseImpl::Agent::create_entry (const char *key,
                    Database::Duplicate_Key))
 {
   // Create a new entry in the database:
-  if (ACE_OS::strcmp (entry_type, "Employee") != 0 ||
-      initial_attributes.length () != 2)
-    {
-      ACE_THROW_RETURN (Database::Unknown_Type (),
-                        Database::Entry::_nil ());
-    }
+  if (ACE_OS::strcmp (entry_type, "Employee") != 0 
+      || initial_attributes.length () != 2)
+    ACE_THROW_RETURN (Database::Unknown_Type (),
+                      Database::Entry::_nil ());
 
   char *name = 0;
   CORBA::Long id = 0;
 
-  const Database::NamedValue &first = initial_attributes[0];
-  const Database::NamedValue &second = initial_attributes[1];
-  if (ACE_OS::strcmp (first.name.in (), "name") != 0 ||
-      ACE_OS::strcmp (second.name.in (), "id") != 0)
-    {
-      ACE_THROW_RETURN (Database::Unknown_Type (),
-                        Database::Entry::_nil ());
-    }
+  const Database::NamedValue &first =
+    initial_attributes[0];
+  const Database::NamedValue &second =
+    initial_attributes[1];
+
+  if (ACE_OS::strcmp (first.name.in (), "name") != 0 
+      || ACE_OS::strcmp (second.name.in (), "id") != 0)
+    ACE_THROW_RETURN (Database::Unknown_Type (),
+                      Database::Entry::_nil ());
 
   first.value >>= name;
   second.value >>= id;
 
-  Employee *new_employee = new Employee (name, id);
-  DATABASE::instance ()->bind (key, new_employee);
+  Employee *new_employee;
+  // This attempts to create a new Employee and throws an exception
+  // and returns a null value if it fails
+  ACE_NEW_THROW_EX (new_employee,
+                    Employee (name, id),
+                    CORBA::NO_MEMORY ());
+  ACE_CHECK_RETURN (Database::Entry::_nil ());
+
+  // @@ Should check the return value here and throw an exception if
+  // it fails.
+  DATABASE::instance ()->bind (key,
+                               new_employee);
 
   ACE_DEBUG ((LM_DEBUG,
               "New employee created with name = %s and id = %d\n",
               name,
               id));
 
-  // creates a reference to the CORBA object used to encapsulate
+  // Creates a reference to the CORBA object used to encapsulate
   // access to the new entry in the database.  There is an interface
   // for each entry type:
-  PortableServer::ObjectId_var obj_id = PortableServer::string_to_ObjectId (key);
-  CORBA::String_var repository_id = DatabaseImpl::entry_type_to_repository_id ("Entry");
+  PortableServer::ObjectId_var obj_id =
+    PortableServer::string_to_ObjectId (key);
+  CORBA::String_var repository_id =
+    DatabaseImpl::entry_type_to_repository_id ("Entry");
 
-  CORBA::Object_var obj = this->poa_->create_reference_with_id (obj_id.in (),
-                                                                repository_id.in (),
-                                                                ACE_TRY_ENV);
-  ACE_CHECK_RETURN (Database::Entry::_nil ()) ;
+  CORBA::Object_var obj =
+    this->poa_->create_reference_with_id (obj_id.in (),
+                                          repository_id.in (),
+                                          ACE_TRY_ENV);
+  ACE_CHECK_RETURN (Database::Entry::_nil ());
 
   Database::Entry_var entry = Database::Entry::_narrow (obj.in (),
                                                         ACE_TRY_ENV);
@@ -208,32 +224,35 @@ DatabaseImpl::Agent::find_entry (const char *key,
                    Database::Unknown_Type,
                    Database::Not_Found))
 {
-  if (ACE_OS::strcmp (entry_type, "Employee") != 0)
-    {
-      ACE_THROW_RETURN (Database::Unknown_Type (),
-                        Database::Entry::_nil ());
-    }
+  if (ACE_OS::strcmp (entry_type,
+                      "Employee") != 0)
+    ACE_THROW_RETURN (Database::Unknown_Type (),
+                      Database::Entry::_nil ());
 
   void *temp;
   Database::Entry_var entry;
   if (DATABASE::instance ()->find (key, temp) == 0)
     {
-      Employee *employee = (Employee *) temp;
-
+      Employee *employee =
+        ACE_reinterpret_cast (Employee *,
+                              temp);
       ACE_DEBUG ((LM_DEBUG,
                   "Employee with key = %s found: name = %s and id = %d\n",
                   key,
                   employee->name (),
                   employee->id ()));
 
-      // creates a reference to the CORBA object used to encapsulate
-      // access to the new entry in the database.  There is an interface
-      // for each entry type:
-      PortableServer::ObjectId_var obj_id = PortableServer::string_to_ObjectId (key);
-      CORBA::String_var repository_id = DatabaseImpl::entry_type_to_repository_id ("Entry");
-      CORBA::Object_var obj = this->poa_->create_reference_with_id (obj_id.in (),
-                                                                    repository_id.in (),
-                                                                    ACE_TRY_ENV);
+      // Creates a reference to the CORBA object used to encapsulate
+      // access to the new entry in the database.  There is an
+      // interface for each entry type:
+      PortableServer::ObjectId_var obj_id =
+        PortableServer::string_to_ObjectId (key);
+      CORBA::String_var repository_id =
+        DatabaseImpl::entry_type_to_repository_id ("Entry");
+      CORBA::Object_var obj =
+        this->poa_->create_reference_with_id (obj_id.in (),
+                                              repository_id.in (),
+                                              ACE_TRY_ENV);
       ACE_CHECK_RETURN (Database::Entry::_nil ());
 
       entry = Database::Entry::_narrow (obj.in (),
@@ -262,15 +281,14 @@ DatabaseImpl::Agent::destroy_entry (const char *key,
                    Database::Unknown_Key))
 {
   if (ACE_OS::strcmp (entry_type, "Employee") != 0)
-    {
-      ACE_THROW (Database::Unknown_Type ());
-    }
+    ACE_THROW (Database::Unknown_Type ());
 
   void *temp;
   if (DATABASE::instance ()->unbind (key, temp) == 0)
     {
-      Employee *employee = (Employee *) temp;
-
+      Employee *employee =
+        ACE_reinterpret_cast (Employee *,
+                              temp);
       ACE_DEBUG ((LM_DEBUG,
                   "Employee with key = %s will be removed from the database: "
                   "name = %s and id = %d \n",
@@ -351,8 +369,12 @@ DatabaseImpl::Employee::name (const char* name)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   DATABASE::instance ()->free (this->name_);
-  this->name_ = (char *) DATABASE::instance ()->malloc (ACE_OS::strlen (name) + 1);
-  ACE_OS::strcpy (this->name_, name);
+
+  this->name_ = 
+    (char *) DATABASE::instance ()->malloc (ACE_OS::strlen (name) + 1);
+
+  ACE_OS::strcpy (this->name_,
+                  name);
 }
 
 CORBA::Long
