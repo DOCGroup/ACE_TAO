f1cf38697c82183c51aa67e13bb7f89892a5030a
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-25 19:34:30 +0000

    Moved class defs from test_dump.cpp to test_dump.h.

--- trunk/examples/Misc/test_dump.cpp
+++ trunk/examples/Misc/test_dump.cpp
@@ -6,46 +6,9 @@
 
 #include "ace/Dump.h"
 
+#include "test_dump.h"
+
 ACE_RCSID(Misc, test_dump, "$Id$")
-
-class SOCK
-{
-public:
-  SOCK (void) { ACE_REGISTER_OBJECT (SOCK); }
-  ~SOCK (void) { ACE_REMOVE_OBJECT; }
-
-  void dump (void) const {
-    ACE_OS::fprintf (stderr, "hello from SOCK = %lu\n", (u_long) this);
-  }
-
-  // ...
-};
-
-class SOCK_Acceptor : public SOCK
-{
-public:
-  SOCK_Acceptor (void) { ACE_REGISTER_OBJECT (SOCK_Acceptor); }
-  ~SOCK_Acceptor (void) { ACE_REMOVE_OBJECT; }
-
-  void dump (void) const {
-    ACE_OS::fprintf (stderr, "hello from SOCK_Acceptor = %lu\n", (u_long) this);
-  }
-
-  // ...
-};
-
-class SOCK_Stream : public SOCK
-{
-public:
-  SOCK_Stream (void) { ACE_REGISTER_OBJECT (SOCK_Stream); }
-  ~SOCK_Stream (void) { ACE_REMOVE_OBJECT; }
-
-  void dump (void) const {
-    ACE_OS::fprintf (stderr, "hello from SOCK_Stream = %lu\n", (u_long) this);
-  }
-
-  // ...
-};
 
 int
 main (int, char *[])
