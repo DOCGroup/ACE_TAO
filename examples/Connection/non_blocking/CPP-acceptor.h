e9c5545bf7142c76669ccd26350586435a5a048b
Steve Huston <shuston@riverace.com>
1999-06-25 19:00:47 +0000

    Added #pragma implementation.

--- examples/Connection/non_blocking/CPP-acceptor.h
+++ examples/Connection/non_blocking/CPP-acceptor.h
@@ -84,4 +84,9 @@ private:
 #if defined (ACE_TEMPLATES_REQUIRE_SOURCE)
 #include "CPP-acceptor.cpp"
 #endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
+
+#if defined (ACE_TEMPLATES_REQUIRE_PRAGMA)
+#pragma implementation ("CPP-acceptor.cpp")
+#endif /* ACE_TEMPLATES_REQUIRE_PRAGMA */
+
 #endif /* CPP_ACCEPTOR_H */
