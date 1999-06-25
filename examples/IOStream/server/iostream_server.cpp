fa568c075926495cf91451af45da29b73f59a166
Steve Huston <shuston@riverace.com>
1999-06-25 19:15:12 +0000

    Move Handler class from iostream_server.cpp to new iostream_server.h.

--- examples/IOStream/server/iostream_server.cpp
+++ examples/IOStream/server/iostream_server.cpp
@@ -7,46 +7,12 @@
 #include "ace/Acceptor.h"
 #include "ace/SOCK_Acceptor.h"
 #include "ace/Service_Config.h"
-#include "ace/IOStream.h"
+
+#include "iostream_server.h"
 
 ACE_RCSID(server, iostream_server, "$Id$")
 
 #if !defined (ACE_LACKS_ACE_IOSTREAM)
-
-// Declare a new type which will case an ACE_SOCK_Stream to behave
-// like an iostream.  The new ACE_SOCK_IOStream type can be used
-// anywhere an ACE_SOCK_Stream is used.
-
-typedef ACE_IOStream<ACE_SOCK_Stream> ACE_SOCK_IOStream;
-
-// Need to handle brain-dead C++ compilers.
-#if defined (ACE_HAS_TYPENAME_KEYWORD)
-#define ACE_SOCK_IOSTREAM ACE_SOCK_IOStream
-#else
-#define ACE_SOCK_IOSTREAM ACE_SOCK_IOStream, ACE_INET_Addr
-#endif /* ACE_HAS_TYPENAME_KEYWORD */
-
-class Handler : public ACE_Svc_Handler<ACE_SOCK_IOSTREAM, ACE_NULL_SYNCH>
-  // = TITLE
-  //     Extend the <ACE_Svc_Handler> template to do our bidding.
-  //
-  // = DESCRIPTION
-  //     Create an <ACE_Svc_Handler> object based on our
-  //     iostream/SOCK_Stream hybrid.  All of this is fairly standard
-  //     until we get to the <handle_input> where we begin using the
-  //     iostream characteristics of the peer.
-{
-public:
-  // = Initialization and termination methods.
-  Handler (void);
-  ~Handler (void);
-
-  // = <Svc_Handler> hooks.
-  virtual int open (void *);
-
-  // = <Event_Handler> hooks.
-  virtual int handle_input (ACE_HANDLE);
-};
 
 int
 Handler::open (void *)
