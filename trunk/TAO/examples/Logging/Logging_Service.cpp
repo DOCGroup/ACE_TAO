ba7c574c676609475284f45bf37cf405d9812a4b
mjb2 <mjb2@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-28 17:20:37 +0000

    See Mon Jun 28 12:19:21 1999  Matthew J Braun  <mjb2@cs.wustl.edu>

--- trunk/TAO/examples/Logging/Logging_Service.cpp
+++ trunk/TAO/examples/Logging/Logging_Service.cpp
@@ -13,29 +13,30 @@ main (int argc, char *argv[])
   
   Logger_Server log_server; 
 
-   TAO_TRY
+  ACE_DECLARE_NEW_CORBA_ENV;
+   ACE_TRY
     {
-      if (log_server.init (argc, argv, TAO_TRY_ENV) == -1)
+      if (log_server.init (argc, argv, ACE_TRY_ENV) == -1)
         return 1;
       else
         {
-          log_server.run (TAO_TRY_ENV);
-          TAO_CHECK_ENV;
+          log_server.run (ACE_TRY_ENV);
+          ACE_TRY_CHECK;
         }
     }
-  TAO_CATCH (CORBA::SystemException, sysex)
+  ACE_CATCH (CORBA::SystemException, sysex)
     {
       ACE_UNUSED_ARG (sysex);
-      TAO_TRY_ENV.print_exception ("System Exception");
+      ACE_TRY_ENV.print_exception ("System Exception");
       return -1;
     }
-  TAO_CATCH (CORBA::UserException, userex)
+  ACE_CATCH (CORBA::UserException, userex)
     {
       ACE_UNUSED_ARG (userex);
-      TAO_TRY_ENV.print_exception ("User Exception");
+      ACE_TRY_ENV.print_exception ("User Exception");
       return -1;
     }
-  TAO_ENDTRY;
+  ACE_ENDTRY;
 
   return 0;
 }
