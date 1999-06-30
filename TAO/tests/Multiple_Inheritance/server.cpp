e34b28e42a692533159a99063add405ac08ef3aa
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-30 16:28:07 +0000

    ChangeLogTag:Wed Jun 30 11:27:39 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/tests/Multiple_Inheritance/server.cpp
+++ TAO/tests/Multiple_Inheritance/server.cpp
@@ -41,13 +41,14 @@ main (int argc, char **argv)
   TAO_ORB_Manager orb_manager;
 
   ACE_DEBUG ((LM_DEBUG, "\n\tMultiple Inheritance Server\n\n"));
-  TAO_TRY
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
     {
       orb_manager.init_child_poa (argc,
                                   argv,
                                   "child_poa",
-                                  TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+                                  ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       if (parse_args (argc, argv) != 0)
 	return -1;
@@ -55,8 +56,8 @@ main (int argc, char **argv)
       CORBA::String_var ior =
 	orb_manager.activate_under_child_poa ("my_object",
 					      &servant,
-					      TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+					      ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       ACE_DEBUG ((LM_DEBUG, "%s\n",
                   ior.in ()));
@@ -74,16 +75,15 @@ main (int argc, char **argv)
 	  ACE_OS::fclose (output_file);
 	}
 
-      orb_manager.run (TAO_TRY_ENV);
-      TAO_CHECK_ENV;
+      orb_manager.run (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
     }
-  TAO_CATCH (CORBA::SystemException, sysex)
+  ACE_CATCH (CORBA::SystemException, sysex)
     {
-      ACE_UNUSED_ARG (sysex);
-      TAO_TRY_ENV.print_exception ("System Exception");
+      ACE_PRINT_EXCEPTION (sysex, "System Exception");
       return -1;
     }
-  TAO_ENDTRY;
+  ACE_ENDTRY;
 
   return 0;
 }
