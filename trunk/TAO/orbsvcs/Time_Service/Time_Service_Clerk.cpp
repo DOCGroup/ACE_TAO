1b925b6896a2977da6548e5a7afab09643a2dd33
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-06-30 03:00:17 +0000

    ChangeLogTag:Tue Jun 29 21:59:29 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- trunk/TAO/orbsvcs/Time_Service/Time_Service_Clerk.cpp
+++ trunk/TAO/orbsvcs/Time_Service/Time_Service_Clerk.cpp
@@ -14,29 +14,30 @@ main (int argc, char *argv[])
   ACE_DEBUG ((LM_DEBUG,
               "[SERVER] Process/Thread Id : (%P/%t) Time Service clerk\n"));
 
-  TAO_TRY
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
     {
-      if (clerk.init (argc, argv, TAO_TRY_ENV) == -1)
+      int r = clerk.init (argc, argv, ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      if (r == -1)
         return 1;
       else
         {
-          clerk.run (TAO_TRY_ENV);
-          TAO_CHECK_ENV;
+          clerk.run (ACE_TRY_ENV);
+          ACE_TRY_CHECK;
         }
     }
-  TAO_CATCH (CORBA::SystemException, sysex)
+  ACE_CATCH (CORBA::SystemException, sysex)
     {
-      ACE_UNUSED_ARG (sysex);
-      TAO_TRY_ENV.print_exception ("System Exception");
+      ACE_PRINT_EXCEPTION (sysex, "System Exception");
       return -1;
     }
-  TAO_CATCH (CORBA::UserException, userex)
+  ACE_CATCH (CORBA::UserException, userex)
     {
-      ACE_UNUSED_ARG (userex);
-      TAO_TRY_ENV.print_exception ("User Exception");
+      ACE_PRINT_EXCEPTION (userex, "User Exception");
       return -1;
     }
-  TAO_ENDTRY;
+  ACE_ENDTRY;
 
   return 0;
 }
