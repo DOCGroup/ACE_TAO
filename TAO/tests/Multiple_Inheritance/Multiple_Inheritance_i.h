bd788b31daba1a9d5bbb107d623cf8884c1c6961
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-01 18:40:44 +0000

    ChangeLogTag:Thu Jul  1 12:37:01 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/tests/Multiple_Inheritance/Multiple_Inheritance_i.h
+++ TAO/tests/Multiple_Inheritance/Multiple_Inheritance_i.h
@@ -7,6 +7,8 @@ ACE_RCSID(Multiple_Inheritance, Multiple_Inheritance_i, "$Id$")
 class Multiple_Inheritance_i : public POA_D
 {
 public:
+  Multiple_Inheritance_i (void);
+
   virtual CORBA::String method1 (CORBA::Environment &)
     ACE_THROW_SPEC ((CORBA::SystemException))
     {
