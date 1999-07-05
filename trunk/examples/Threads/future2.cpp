a5ed932b7f37d6b8fc38c21029361a072488460f
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-05 18:04:44 +0000

    ChangeLogTag:Mon Jul 05 12:51:14 1999  Irfan Pyarali  <irfan@cs.wustl.edu>

--- trunk/examples/Threads/future2.cpp
+++ trunk/examples/Threads/future2.cpp
@@ -516,20 +516,31 @@ main (int, char *[])
 template class ACE_Atomic_Op<ACE_Thread_Mutex, int>;
 template class ACE_Future<char *>;
 template class ACE_Future<u_long>;
-template class ACE_Future_Rep<char *>;
+template class ACE_Future_Rep<char const *>;
 template class ACE_Future_Rep<u_long>;
 template class auto_ptr<ACE_Method_Request>;
 template class ACE_Auto_Basic_Ptr<ACE_Method_Request>;
+template class ACE_Node<ACE_Future_Observer<char *> *>;
+template class ACE_Node<ACE_Future_Observer<u_long> *>;
+template class ACE_Unbounded_Set<ACE_Future_Observer<char *> *>;
+template class ACE_Unbounded_Set<ACE_Future_Observer<u_long> *>;
+template class ACE_Unbounded_Set_Iterator<ACE_Future_Observer<char *> *>;
+template class ACE_Unbounded_Set_Iterator<ACE_Future_Observer<u_long> *>;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate ACE_Atomic_Op<ACE_Thread_Mutex, int>
 #pragma instantiate ACE_Future<char *>
 #pragma instantiate ACE_Future<u_long>
-#pragma instantiate ACE_Future_Rep<char *>
+#pragma instantiate ACE_Future_Rep<char const *>
 #pragma instantiate ACE_Future_Rep<u_long>
 #pragma instantiate auto_ptr<ACE_Method_Request>
 #pragma instantiate ACE_Auto_Basic_Ptr<ACE_Method_Request>
+#pragma instantiate ACE_Node<ACE_Future_Observer<char *> *>
+#pragma instantiate ACE_Node<ACE_Future_Observer<u_long> *>
+#pragma instantiate ACE_Unbounded_Set<ACE_Future_Observer<char *> *>
+#pragma instantiate ACE_Unbounded_Set<ACE_Future_Observer<u_long> *>
+#pragma instantiate ACE_Unbounded_Set_Iterator<ACE_Future_Observer<char *> *>
+#pragma instantiate ACE_Unbounded_Set_Iterator<ACE_Future_Observer<u_long> *>
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
 
 #else
 int
