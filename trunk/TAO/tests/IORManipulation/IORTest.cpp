df544991c86c7d22c931bb1b1e762258e3c26a23
fredk <fredk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-06 21:36:22 +0000

    *** empty log message ***

--- trunk/TAO/tests/IORManipulation/IORTest.cpp
+++ trunk/TAO/tests/IORManipulation/IORTest.cpp
@@ -36,8 +36,8 @@ main (int argc, char **argv)
 
   int Status = 0;
   CORBA::ORB_var orb_;
-  printf ("----------------------------------\n");
-  printf ("Running the IORManipulation Tests.\n");
+  ACE_DEBUG ((LM_DEBUG, "----------------------------------\n"));
+  ACE_DEBUG ((LM_DEBUG, "Running the IORManipulation Tests.\n"));
 
   ACE_DECLARE_NEW_CORBA_ENV;
 
@@ -75,15 +75,15 @@ main (int argc, char **argv)
       // Get the string reps for these IORs and show them to the user
       ACE_CString iorm_ior = orb_->object_to_string (IORM, ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      printf("\tIORManipultion IOR string = %s\n", iorm_ior.c_str());
+      ACE_DEBUG ((LM_DEBUG, "\tIORManipultion IOR string = %s\n", iorm_ior.c_str()));
 
       ACE_CString name1_ior = orb_->object_to_string (name1, ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      printf("\tFirst made up IOR = %s\n", name1_ior.c_str());
+      ACE_DEBUG ((LM_DEBUG, "\tFirst made up IOR = %s\n", name1_ior.c_str()));
 
       ACE_CString name2_ior = orb_->object_to_string (name2, ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      printf("\tSecond made up IOR = %s\n", name2_ior.c_str());
+      ACE_DEBUG ((LM_DEBUG, "\tSecond made up IOR = %s\n", name2_ior.c_str()));
 
       // ***********************************************************************
       // Create IOR list for use with merge_iors.
@@ -108,9 +108,9 @@ main (int argc, char **argv)
       ACE_TRY_CHECK;
 
       if (count != (count1 + count2))
-        printf("**ERROR (merge_profiles): Merged profile count incorrect!\n");
+        ACE_DEBUG ((LM_DEBUG, "**ERROR (merge_profiles): Merged profile count incorrect!\n"));
 
-      printf("\tMerged IOR(%d) = %s\n", count, merged_ior.c_str());
+      ACE_DEBUG ((LM_DEBUG, "\tMerged IOR(%d) = %s\n", count, merged_ior.c_str()));
 
       // is_in_ior throws an exception if the intersection of the two
       // IORs is NULL.
@@ -141,9 +141,9 @@ main (int argc, char **argv)
       ACE_TRY_CHECK;
 
       if (count1 != count)
-        printf ("**ERROR (merge_profiles): removing last IOR after a merge_iors ()\n");
+        ACE_DEBUG ((LM_DEBUG, "**ERROR (merge_profiles): removing last IOR after a merge_iors ()\n"));
 
-      printf("\tJust 1 IOR(%d) = %s\n", count, just1_ior.c_str ());
+      ACE_DEBUG ((LM_DEBUG, "\tJust 1 IOR(%d) = %s\n", count, just1_ior.c_str ()));
 
       in_count = iorm->is_in_ior (just1, name1, ACE_TRY_ENV);
       ACE_TRY_CHECK;
@@ -161,12 +161,12 @@ main (int argc, char **argv)
       ACE_TRY_CHECK;
 
       if (count != (count1 + count2))
-        printf("**ERROR: add_profile failed profile count test!\n");
+        ACE_DEBUG ((LM_DEBUG, "**ERROR: add_profile failed profile count test!\n"));
 
       ACE_CString merged2_ior = orb_->object_to_string (merged2, ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      printf("\tAdding 2 back in IOR(%d) = %s\n", count, merged2_ior.c_str ());
+      ACE_DEBUG ((LM_DEBUG, "\tAdding 2 back in IOR(%d) = %s\n", count, merged2_ior.c_str ()));
 
       // is_in_ior throws an exception if the intersection of the two
       // IORs is NULL.
@@ -181,7 +181,7 @@ main (int argc, char **argv)
       ACE_TRY_CHECK;
 
       if (count2 != in_count)
-        printf ("**ERROR (add_profiles): is_in_ior returned profile count bad!\n");
+        ACE_DEBUG ((LM_DEBUG, "**ERROR (add_profiles): is_in_ior returned profile count bad!\n"));
       // ***********************************************************************
 
       CORBA_Object_ptr just2 = iorm->remove_profiles (merged2, name1, ACE_TRY_ENV);
@@ -194,9 +194,9 @@ main (int argc, char **argv)
       ACE_TRY_CHECK;
 
       if (count2 != count)
-        printf ("ERROR removing last IOR after a merge_iors ()\n");
+        ACE_DEBUG ((LM_DEBUG, "ERROR removing last IOR after a merge_iors ()\n"));
 
-      printf("\tJust 2 IOR(%d) = %s\n", count, just2_ior.c_str ());
+      ACE_DEBUG ((LM_DEBUG, "\tJust 2 IOR(%d) = %s\n", count, just2_ior.c_str ()));
 
       in_count = iorm->is_in_ior (just2, name2, ACE_TRY_ENV);
       ACE_TRY_CHECK;
@@ -218,7 +218,7 @@ main (int argc, char **argv)
     ACE_CATCH (TAO_IOP::TAO_IOR_Manipulation::EmptyProfileList, userex)
       {
         ACE_UNUSED_ARG (userex);
-        printf ("Unexpected EmptyProfileList Exception!\n");
+        ACE_DEBUG ((LM_DEBUG, "Unexpected EmptyProfileList Exception!\n"));
         return -1;
       }
     ACE_CATCH (TAO_IOP::TAO_IOR_Manipulation::NotFound, userex)
@@ -227,35 +227,35 @@ main (int argc, char **argv)
         if (Status == 1)
           {
             Status = 2;
-            printf ("Caught NotFound Exception!\n");
+            ACE_DEBUG ((LM_DEBUG, "Caught NotFound Exception!\n"));
           }
         else
           {
-            printf ("Unexpected NotFound Exception!\n");
+            ACE_DEBUG ((LM_DEBUG, "Unexpected NotFound Exception!\n"));
             return -1;
           }
       }
     ACE_CATCH (TAO_IOP::TAO_IOR_Manipulation::Duplicate, userex)
       {
         ACE_UNUSED_ARG (userex);
-        printf ("Unexpected Duplicate Exception!\n");
+        ACE_DEBUG ((LM_DEBUG, "Unexpected Duplicate Exception!\n"));
         return -1;
       }
     ACE_CATCH (TAO_IOP::TAO_IOR_Manipulation::Invalid_IOR, userex)
       {
         ACE_UNUSED_ARG (userex);
-        printf ("Unexpected Invalid_IOR Exception!\n");
+        ACE_DEBUG ((LM_DEBUG, "Unexpected Invalid_IOR Exception!\n"));
         return -1;
       }
     ACE_CATCH (CORBA::SystemException, sysex)
       {
         ACE_UNUSED_ARG (sysex);
-        printf ("Unexpected system Exception!!\n");
+        ACE_DEBUG ((LM_DEBUG, "Unexpected system Exception!!\n"));
         return -1;
       }
     ACE_CATCHANY
       {
-        printf("Unexpected ACE_CATCHANY Exception!\n");
+        ACE_DEBUG ((LM_DEBUG, "Unexpected ACE_CATCHANY Exception!\n"));
         return -1;
       }
   ACE_ENDTRY;
@@ -263,14 +263,14 @@ main (int argc, char **argv)
   if (Status == 1)
     {
       // then exception didn't work!
-      printf ("**ERROR: Unable to generate the NotFound exception!\n");
+      ACE_DEBUG ((LM_DEBUG, "**ERROR: Unable to generate the NotFound exception!\n"));
       Status = 0;
     }
   if (Status == 0)
-    printf ("An ERROR occured during the tests!\n");
+    ACE_DEBUG ((LM_DEBUG, "An ERROR occured during the tests!\n"));
   else
-    printf ("Tests Successfully Completed!\n");
-  printf ("--------------------------------------\n");
+    ACE_DEBUG ((LM_DEBUG, "Tests Successfully Completed!\n"));
+  ACE_DEBUG ((LM_DEBUG, "--------------------------------------\n"));
 
   return 0;
 }
