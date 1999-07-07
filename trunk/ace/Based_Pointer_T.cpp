d0a86f21758f6b37f5a67bcf7f10755505ba4a40
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-07 05:42:03 +0000

    ChangeLogTag:Wed Jul  7 00:37:22 1999  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- trunk/ace/Based_Pointer_T.cpp
+++ trunk/ace/Based_Pointer_T.cpp
@@ -1,21 +1,21 @@
 // $Id$
 
-#if !defined (ACE_BASED_POINTER_REPOSITORY_T_CPP)
-#define ACE_BASED_POINTER_REPOSITORY_T_CPP
+#if !defined (ACE_BASED_POINTER_T_CPP)
+#define ACE_BASED_POINTER_T_CPP
 
 #include "ace/Based_Pointer_Repository.h"
-#include "ace/Based_Pointer_Repository_T.h"
+#include "ace/Based_Pointer_T.h"
 
 template <class CONCRETE>
 ACE_Based_Pointer<CONCRETE>::ACE_BasedPointer (void)
   : target_ (0),
     base_offset_ (0)
 {
-  // Find the base address associated with our <this> pointer.
+  // Find the base address associated with our <this> pointer.  Note
+  // that it's ok for <find> to return 0, which simply indicates that
+  // the address is not in memory-mapped virtual address space.
   void *base_addr =
     ACE_BASED_POINTER_REPOSITORY::instance ()->find (this);
-
-  ACE_ASSERT (base_addr != 0);
   this->base_offset_ = (char *) this - (char *) base_addr;
 }
 
@@ -49,5 +49,5 @@ ACE_Based_Pointer<CONCRETE>::operator [] (int index)
   return *((CONCRETE *)(base_addr + (long) this->target_) + index);
 }
 
-#endif/* ACE_BASED_POINTER_REPOSITORY_T_CPP */
+#endif/* ACE_BASED_POINTER_T_CPP */
 
