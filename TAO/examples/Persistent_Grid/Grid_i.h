b9a3d940606e2229abf5076b80aeca3ca9a52d0c
bala <balanatarajan@users.noreply.github.com>
1999-07-09 21:35:07 +0000

    Changed according to Dr.Schmidt's suggestions

--- TAO/examples/Persistent_Grid/Grid_i.h
+++ TAO/examples/Persistent_Grid/Grid_i.h
@@ -21,6 +21,9 @@
 
 #include "GridS.h"
 
+typedef ACE_Malloc<ACE_MMAP_MEMORY_POOL, ACE_Null_Mutex> pool_t;
+// Memory pool for the persistent stuff
+
 //class Grid_Factory_i;
 class Grid_Factory_i : public POA_Grid_Factory
 {
@@ -37,18 +40,19 @@ public:
   ~Grid_Factory_i (void);
   // Destructor.
 
-  typedef ACE_Malloc<ACE_MMAP_MEMORY_POOL, ACE_Null_Mutex> pool_t;
-  // Memory pool for the persistent stuff
 
   virtual Grid_ptr make_grid (CORBA::Short,
                               CORBA::Short,
-                              CORBA::Environment &_env);
+                              CORBA::Environment &_env)
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // This function creates and returns a <Grid>.
 
-  virtual void shutdown (CORBA::Environment &env);
+  virtual void shutdown (CORBA::Environment &env)
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // Shutdown the server.
 
-  virtual void cleanup (CORBA::Environment & );
+  virtual void cleanup (CORBA::Environment & )
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // Do a clean up of the memory map 
 
   void orb (CORBA::ORB_ptr o);
@@ -84,42 +88,53 @@ public:
 
   Grid_i (CORBA::Short,
           CORBA::Short,
-          Grid_Factory_i::pool_t *,
-          CORBA_Environment &);
+          pool_t *);
+
   // Constructor.
 
   ~Grid_i (void);
   // Destructor
 
-  virtual CORBA::Short width (CORBA_Environment &);
+  virtual CORBA::Short width (CORBA_Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // Returns the width of the grid
 
-  virtual CORBA::Short height (CORBA_Environment &);
+  virtual CORBA::Short height (CORBA_Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // Returns the height of the grid
 
   virtual void width (CORBA::Short,
-                      CORBA_Environment &);
+                      CORBA_Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // Sets the width of the grid.
 
   virtual void height (CORBA::Short,
-                       CORBA_Environment &);
+                       CORBA_Environment &)
+     ACE_THROW_SPEC ((CORBA::SystemException));
   // Sets the height of the grid.
 
   virtual void set (CORBA::Short,
                     CORBA::Short,
                     CORBA::Long,
-                    CORBA::Environment &);
+                    CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException,
+                     Grid::RANGE_ERROR));
   // Sets the grid value.
 
   virtual CORBA::Long get (CORBA::Short,
                            CORBA::Short,
-                           CORBA::Environment &);
+                           CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException,
+                     Grid::RANGE_ERROR));
   // Gets the grid value.
 
-  virtual void destroy (CORBA::Environment &);
+  virtual void destroy (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException));
   // Destroy the grid.
 
- 
+  void set_pool (pool_t *);
+  // Set a pointer to the pool
+
 private:
   CORBA::Short width_;
   // Width of the grid.
@@ -129,6 +144,9 @@ private:
 
   CORBA::Long **array_;
   // Pointer to the matrix.  This is organized as an "array of arrays."
+  
+  pool_t *pool_t_;
+  //Pointer to the memory pool..
 };
 
 
