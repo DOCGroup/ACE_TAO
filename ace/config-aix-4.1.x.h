c41c2d51bfcfdad681fadc881fa64b389179edfe
Steve Huston <shuston@riverace.com>
1999-07-13 12:18:58 +0000

    Added major/minor version setting if not defined, to ease backwards compatibility and for those who don't use the platform_macros.GNU file to build their apps.

--- ace/config-aix-4.1.x.h
+++ ace/config-aix-4.1.x.h
@@ -1,4 +1,11 @@
 /* -*- C++ -*- */
 // $Id$
 
+#if !defined (ACE_AIX_MAJOR_VERS)
+# define ACE_AIX_MAJOR_VERS 4
+#endif
+#if !defined (ACE_AIX_MINOR_VERS)
+# define ACE_AIX_MINOR_VERS 1
+#endif
+
 #include "ace/config-aix-4.x.h"
