4c178e92e8a926486ab4161f2b65c2194f102f47
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-15 22:30:21 +0000

    New test to make sure that IDL compiler bugs, once fixed, never recur.

---
+++ TAO/tests/IDL_Test/main.cpp
@@ -0,0 +1,26 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    TAO/tests/IDL_Test
+//
+// = FILENAME
+//    main.cpp
+//
+// = DESCRIPTION
+//    This test isn't for functionality, but only for IDL compiler
+//    and C++ compiler errors and warnings
+//
+// = AUTHORS
+//    Jeff Parsons <parsons@cs.wustl.edu>
+//
+// ============================================================================
+
+
+
+int 
+main (int argc, char *argv[])
+{
+  return 0;
+}
