025a6437be7d55ac98e65db28098840fea957322
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-16 22:21:30 +0000

    Removed some unneeded fucntions. Removed some unneeded functions.

--- TAO/TAO_IDL/be_include/be_visitor_array/cdr_op_cs.h
+++ TAO/TAO_IDL/be_include/be_visitor_array/cdr_op_cs.h
@@ -51,40 +51,6 @@ public:
   virtual int visit_array (be_array *node);
   // visit array
 
-  // = Visitor methods on array types
-
-  virtual int visit_enum (be_enum *node);
-  // visit an enum
-
-  virtual int visit_interface (be_interface *node);
-  // visit an interface
-
-  virtual int visit_interface_fwd (be_interface_fwd *node);
-  // visit an interface forward node
-
-  virtual int visit_predefined_type (be_predefined_type *node);
-  // visit a predefined type node
-
-  virtual int visit_sequence (be_sequence *node);
-  // visit sequence
-
-  virtual int visit_string (be_string *node);
-  // visit string
-
-  virtual int visit_structure (be_structure *node);
-  // visit structure
-
-  virtual int visit_typedef (be_typedef *node);
-  // visit typedef
-
-  virtual int visit_union (be_union *node);
-  // visit union
-
-protected:
-
-  int visit_node (be_type *);
-  // helper that does the common job
-
 };
 
 #endif /* _BE_VISITOR_ARRAY_CDR_OP_CS_H_ */
