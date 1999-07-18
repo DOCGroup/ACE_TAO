583c2f21b3896abea1f2c28cb31054cde6a9a7ee
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-18 02:58:53 +0000

    Added a command line option "-l" that tests and demonstrates usage of the CORBA::ORB::list_initial_services() method.

--- trunk/TAO/tests/InterOp-Naming/INS_test_client.cpp
+++ trunk/TAO/tests/InterOp-Naming/INS_test_client.cpp
@@ -7,6 +7,8 @@
 int
 main (int argc, char *argv[])
 {
+  int i = 0;
+
   ACE_DECLARE_NEW_CORBA_ENV;
   ACE_TRY
     {
@@ -21,68 +23,109 @@ main (int argc, char *argv[])
         {
           ACE_DEBUG ((LM_DEBUG,
                       "No Name specified to be resolved\n"));
-          return 0;
+          return -1;
         }
 
-      CORBA::Object_var objref =
-        orb->resolve_initial_references (argv[1], ACE_TRY_ENV);
-      ACE_TRY_CHECK;
+      CORBA::Object_var objref;
 
-      if (CORBA::is_nil (objref.in ()))
-        ACE_ERROR_RETURN ((LM_ERROR,
-                           " (%P|%t) Unable to resolve the given Name.\n"),
-                          -1);
+      for (i = 1; i < argc; ++i)
+        { 
+          if (ACE_OS::strcmp (argv[i], "-l") == 0)
+            {
+              // List initial services
+              CORBA::ORB::ObjectIdList_var list =
+                orb->list_initial_services (ACE_TRY_ENV);
+              ACE_TRY_CHECK;
 
-      if (ACE_OS::strcmp (argv[1], "NameService") == 0)
-        {
-          CosNaming::NamingContext_var naming_context =
-            CosNaming::NamingContext::_narrow (objref.in (),
-                                               ACE_TRY_ENV);
-          ACE_TRY_CHECK;
+              size_t length = list->length ();
+
+              if (length > 0)
+                {
+                  ACE_DEBUG ((LM_INFO,
+                              "Listing initial references...\n"));
+
+                  for (size_t n = 0; n < length; ++n)
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                  "  Reference %u: %s\n",
+                                  n,
+                                  list[n].in ()));
+                    }
+                }
+              else
+                {
+                  // We should at least get TAO's initial references,
+                  // such as "RootPOA", "NameService", etc.
+                  ACE_ERROR_RETURN ((LM_ERROR,
+                                     "\n"
+                                     "No initial references. "
+                                     "This condition shouldn't happen.\n"),
+                                    -1);
+                }
+            }
+          else 
+            {
+              objref = orb->resolve_initial_references (argv[i], ACE_TRY_ENV);
+              ACE_TRY_CHECK;
+
+              if (CORBA::is_nil (objref.in ()))
+                ACE_ERROR_RETURN ((LM_ERROR,
+                                   " (%P|%t) Unable to resolve the "
+                                   "given name.\n"),
+                                  -1);
+
+              if (ACE_OS::strcmp (argv[i], "NameService") == 0)
+                {
+                  CosNaming::NamingContext_var naming_context =
+                    CosNaming::NamingContext::_narrow (objref.in (),
+                                                       ACE_TRY_ENV);
+                  ACE_TRY_CHECK;
               
-          ACE_DEBUG ((LM_DEBUG,
-                      "Resolved IOR for %s : %s\n",
-                      argv[1],
-                      orb->object_to_string (naming_context.in ())));
+                  ACE_DEBUG ((LM_DEBUG,
+                              "Resolved IOR for %s : %s\n",
+                              argv[i],
+                              orb->object_to_string (naming_context.in ())));
 
-          // Sanity check to see if the reference to Naming
-          // Context is alright.
-          CosNaming::Name my_name;
-          my_name.length (1);
-          my_name[0].id = CORBA::string_dup ("ObjName");
+                  // Sanity check to see if the reference to Naming
+                  // Context is alright.
+                  CosNaming::Name my_name;
+                  my_name.length (1);
+                  my_name[0].id = CORBA::string_dup ("ObjName");
 
-          naming_context->bind (my_name,
-                                objref.in (),
-                                ACE_TRY_ENV);
-          ACE_TRY_CHECK;
+                  naming_context->bind (my_name,
+                                        objref.in (),
+                                        ACE_TRY_ENV);
+                  ACE_TRY_CHECK;
 
-          ACE_DEBUG ((LM_DEBUG,
-                      "\nBind successful on the resolved Naming Context\n"));
+                  ACE_DEBUG ((LM_DEBUG,
+                              "\nBind successful on the "
+                              "resolved Naming Context\n")); 
+                }
+              else
+                {
+                  INS_var server = INS::_narrow (objref.in (),
+                                                 ACE_TRY_ENV);
+                  ACE_TRY_CHECK;
 
-        }
-      else
-        {
-          INS_var server = INS::_narrow (objref.in (),
-                                         ACE_TRY_ENV);
-          ACE_TRY_CHECK;
+                  ACE_DEBUG ((LM_DEBUG,
+                              "Resolved IOR for %s : %s\n",
+                              argv[i],
+                              orb->object_to_string (server.in ())));
 
-          ACE_DEBUG ((LM_DEBUG,
-                      "Resolved IOR for %s : %s\n",
-                      argv[1],
-                      orb->object_to_string (server.in ())));
-
-          ACE_DEBUG ((LM_DEBUG,
-                      "\nResult of Remote Call : %s\n",
-                      server->test_ins (ACE_TRY_ENV)));
+                  ACE_DEBUG ((LM_DEBUG,
+                              "\nResult of Remote Call : %s\n",
+                              server->test_ins (ACE_TRY_ENV)));
+                }
+            }
         }
     }
   ACE_CATCH (CORBA::ORB::InvalidName, name)
     {
-      ACE_DEBUG ((LM_DEBUG, "Cannot resolve <%s>\n", argv[1]));
+      ACE_DEBUG ((LM_DEBUG, "Cannot resolve <%s>\n", argv[i]));
     }
   ACE_CATCHANY
     {
-       ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "Exception:");
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "Exception:");
     }
 
   ACE_ENDTRY;
