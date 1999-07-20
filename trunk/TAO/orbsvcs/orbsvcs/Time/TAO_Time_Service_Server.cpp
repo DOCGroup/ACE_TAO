5dc5925bb50ec50977fa26efcb474ceae430218e
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-20 12:36:22 +0000

    ChangeLogTag: Tue Jul 20 07:34:11 1999  David L. Levine  <levine@cs.wustl.edu>

--- trunk/TAO/orbsvcs/orbsvcs/Time/TAO_Time_Service_Server.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Time/TAO_Time_Service_Server.cpp
@@ -24,23 +24,23 @@ TAO_Time_Service_Server::universal_time (CORBA::Environment &ACE_TRY_ENV)
     ACE_THROW_SPEC ((CORBA::SystemException,
                      CosTime::TimeUnavailable))
 {
-  TAO_UTO *uto = 0;  
-  
-  // This is the difference in seconds between 
-  // 15th October 1582 and 1st Jan 1970. It needs to be added to the system 
-  // time to make the base time as 15th October 1582. The decimal value of 
-  // the offset is 1.221929 * 10^10. 
+  TAO_UTO *uto = 0;
+
+  // This is the difference in seconds between
+  // 15th October 1582 and 1st Jan 1970. It needs to be added to the system
+  // time to make the base time as 15th October 1582. The decimal value of
+  // the offset is 1.221929 * 10^10.
 
 #if defined (ACE_LACKS_LONGLONG_T)
-  CORBA::ULongLong MAGIC_OFFSET (D8539190, 2);
+  CORBA::ULongLong TAO_Time_Base_Offset (0xD8539190, 2);
   // (Lower 32 bits of the offset in hex, Upper 32 bits of the offset in hex)
 #else
-  CORBA::ULongLong  MAGIC_OFFSET = ACE_UINT64_LITERAL(0x2D8539190);
+  CORBA::ULongLong TAO_Time_Base_Offset = ACE_UINT64_LITERAL(0x2D8539190);
 #endif
 
   // Return the local time of the system as a UTO.
   ACE_NEW_THROW_EX (uto,
-                    TAO_UTO ((MAGIC_OFFSET +
+                    TAO_UTO ((TAO_Time_Base_Offset +
                               ACE_static_cast(CORBA::ULongLong,
                                               ACE_OS::gettimeofday ().sec ())) *
                              ACE_static_cast(ACE_UINT32,
