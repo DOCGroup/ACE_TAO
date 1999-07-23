7e3c7b382af92b5b636ccedd4fbd97809dc5bdb0
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-07-23 20:04:52 +0000

    ChangeLogTag:Fri Jul 23 14:51:48 1999  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- examples/Service_Configurator/IPC-tests/server/Handle_L_CODgram.cpp
+++ examples/Service_Configurator/IPC-tests/server/Handle_L_CODgram.cpp
@@ -2,31 +2,9 @@
 
 #include "Handle_L_CODgram.h"
 
+#if !defined (ACE_LACKS_UNIX_DOMAIN_SOCKETS)
+
 ACE_RCSID(server, Handle_L_CODgram, "$Id$")
-
-#if defined (SunOS4)
-extern "C" 
-{
-  int init (void);
-  int fini (void);
-  void __sti__Handle_L_CODgram_C_init_();
-  void __std__Handle_L_CODgram_C_init_();
-}
-
-int 
-init (void)
-{
-  __sti__Handle_L_CODgram_C_init_();
-  return 0;
-}
-
-int 
-fini (void)
-{
-  __std__Handle_L_CODgram_C_init_();
-  return 0;
-}
-#endif /* SunOS4 */
 
 const char *Handle_L_CODgram::DEFAULT_RENDEZVOUS = "/tmp/foo_codgram";
 
@@ -36,3 +14,5 @@ const char *Handle_L_CODgram::DEFAULT_RENDEZVOUS = "/tmp/foo_codgram";
 
 Handle_L_CODgram local_codgram;
 ACE_Service_Object_Type lc (&local_codgram, "Local_CODgram");
+
+#endif /* ACE_LACKS_UNIX_DOMAIN_SOCKETS */
