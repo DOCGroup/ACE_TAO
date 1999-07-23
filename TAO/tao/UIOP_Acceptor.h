5052545c94074148b767e8c86d9ea7667ac697a2
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-23 23:09:33 +0000

    ChangeLogTag:Fri Jul 23 17:54:45 1999  Irfan Pyarali  <irfan@cs.wustl.edu>

--- TAO/tao/UIOP_Acceptor.h
+++ TAO/tao/UIOP_Acceptor.h
@@ -82,6 +82,7 @@ public:
   typedef ACE_Strategy_Acceptor<TAO_UIOP_Server_Connection_Handler, ACE_LSOCK_ACCEPTOR> TAO_UIOP_BASE_ACCEPTOR;
   typedef TAO_Creation_Strategy<TAO_UIOP_Server_Connection_Handler> TAO_UIOP_CREATION_STRATEGY;
   typedef TAO_Concurrency_Strategy<TAO_UIOP_Server_Connection_Handler> TAO_UIOP_CONCURRENCY_STRATEGY;
+  typedef TAO_Accept_Strategy<TAO_UIOP_Server_Connection_Handler, ACE_LSOCK_ACCEPTOR> TAO_UIOP_ACCEPT_STRATEGY;
 
 private:
   int open_i (TAO_ORB_Core *orb_core, const char *rendezvous);
@@ -97,6 +98,7 @@ private:
 
   TAO_UIOP_CREATION_STRATEGY *creation_strategy_;
   TAO_UIOP_CONCURRENCY_STRATEGY *concurrency_strategy_;
+  TAO_UIOP_ACCEPT_STRATEGY *accept_strategy_;
   // Acceptor strategies.
 
   TAO_GIOP_Version version_;
