c36bab49196f2b2c69f121a1144cd87fbc6ba016
Ossama Othman <ossama-othman@users.noreply.github.com>
1999-07-26 23:24:13 +0000

    {iiop,uiop}loc should not be accepted as a proper prefix for server endpoints.

--- TAO/tao/IIOP_Factory.cpp
+++ TAO/tao/IIOP_Factory.cpp
@@ -22,13 +22,8 @@ TAO_IIOP_Protocol_Factory::~TAO_IIOP_Protocol_Factory (void)
 int
 TAO_IIOP_Protocol_Factory::match_prefix (const ACE_CString &prefix)
 {
-  const char *protocol[] = { "iiop", "iioploc" };
-  // This is valid for any protocol beginning with `iiop' or `iioploc'.
-
-  // Check for the proper prefix in the IOR.  If the proper prefix isn't
-  // in the IOR then it is not an IOR we can use.
-  return (ACE_OS::strcasecmp (prefix.c_str (), protocol[0]) == 0 ||
-          ACE_OS::strcasecmp (prefix.c_str (), protocol[1]) == 0);
+  // Check for the proper prefix for this protocol.
+  return (ACE_OS::strcasecmp (prefix.c_str (), ::prefix_) == 0);
 }
 
 const char *
