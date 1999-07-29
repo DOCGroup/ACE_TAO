5bcde818a1fe6c6174f2fba80e83df937a8d0675
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-29 23:33:41 +0000

    ChangeLogTag:Thu Jul 29 18:29:46 1999  Nanbor Wang  <nanbor@cs.wustl.edu>

--- bin/run_all_win32.pl
+++ bin/run_all_win32.pl
@@ -15,6 +15,8 @@ use File::Basename;
 use FileHandle;
 require POSIX;
 
+use lib "$ENV{ACE_ROOT}/bin";
+require run_all_list;
 
 # This is the module we will checkout, someday someone could define a
 # smaller module.
@@ -27,65 +29,13 @@ $exe_log_name='run_test.log';   # Name of the tempfile used.
 $sh_cmd="c:/bin/sh ";
 $sh_redirection=" > $exe_log_name 2>&1";
 
-#if using '4nt'
+#if using '4nt',  4nt is not quite working yet.
 $four_nt_cmd="d:/Utils/4NT301/4NT.EXE ";
 $four_nt_redirection=" >& $exe_log_name";
 
 # Pick the one your like.
 $shell_invoke = $sh_cmd;
 $redirect_output  = $sh_redirection;
-
-# This are the pairs "sub-directory , script" we run; the separator
-# *must* be a space followed by a comma and then another space.
-
-@RUN_LIST = (
-      'tests , run_tests.bat',
-      'TAO/tests/Param_Test , run_test.pl',
-      'TAO/tests/Param_Test , run_test.pl -i dii',
-      'TAO/performance-tests/Cubit/TAO/IDL_Cubit , run_test.pl',
-      'TAO/tests/OctetSeq , run_test.pl',
-      'TAO/tests/Multiple_Inheritance , run_test.pl',
-      'TAO/tests/MT_Client , run_test.pl',
-      'TAO/tests/MT_Server , run_test.pl',
-      'TAO/tests/Faults , run_test.pl',
-      'TAO/tests/NestedUpcall/Reactor , run_test.pl',
-      'TAO/tests/NestedUpcall/MT_Client_Test , run_test.pl',
-      'TAO/tests/POA/Identity , run_test.pl',
-      'TAO/tests/POA/Destruction , run_test.pl',
-      'TAO/tests/IORManipulation , run_test.pl',
-      'TAO/examples/POA/Adapter_Activator , run_test.pl',
-      'TAO/examples/POA/DSI , run_test.pl',
-      'TAO/examples/POA/Default_Servant , run_test.pl',
-      'TAO/examples/POA/Explicit_Activation , run_test.pl',
-      'TAO/examples/POA/FindPOA , run_test.pl',
-      'TAO/examples/POA/Forwarding , run_test.pl',
-      'TAO/examples/POA/NewPOA , run_test.pl',
-      'TAO/examples/POA/On_Demand_Activation , run_test.pl',
-      'TAO/examples/POA/On_Demand_Loading , run_test.pl',
-      'TAO/examples/POA/Reference_Counted_Servant , run_test.pl',
-      'TAO/examples/Simple/bank , run_test.pl',
-      'TAO/examples/Simple/grid , run_test.pl',
-      'TAO/examples/Simple/time-date , run_test.pl',
-      'TAO/examples/Simple/time , run_test.pl',
-      'TAO/orbsvcs/tests/Simple_Naming , run_test.pl',
-      'TAO/orbsvcs/tests/Trading , run_test.pl',
-      'TAO/orbsvcs/tests/Event/Basic , run_test.pl',
-      'TAO/orbsvcs/tests/Event/Performance , run_test.pl',
-      'TAO/orbsvcs/tests/EC_Basic , run_test.pl',
-      'TAO/orbsvcs/tests/EC_Custom_Marshal , run_test.pl',
-      'TAO/orbsvcs/tests/EC_Throughput , run_test.pl',
-#      'TAO/orbsvcs/tests/EC_Mcast , run_test.pl',
-#      'TAO/orbsvcs/tests/EC_Multiple , run_test.pl',
-      'TAO/orbsvcs/tests/Event_Latency , run_test.pl',
-      'TAO/examples/Simple/echo , run_test.pl < Echo.idl',
-#      'TAO/examples/Simple/chat , run_test.pl',
-      'TAO/orbsvcs/tests/Property , run_test.pl',
-      'TAO/performance-tests/POA/Object_Creation_And_Registration , run_test.pl',
-      'TAO/performance-tests/Cubit/TAO/MT_Cubit , run_test.pl -n 100'
-#      'TAO/orbsvcs/tests/ImplRepo , run_test.pl airplane',
-#      'TAO/orbsvcs/tests/ImplRepo , run_test.pl airplane_ir',
-    );
-
 
 # We obtain our revision to report errors.
 #$REVISION='$Revision$';
