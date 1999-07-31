0a9f966239b532c07d960cbbd8e4d190142b06f9
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-31 22:43:31 +0000

    ChangeLogTag:Sat Jul 31 17:42:17 1999  Carlos O'Ryan  <coryan@cs.wustl.edu>

--- TAO/tao/Timeprobe.h
+++ TAO/tao/Timeprobe.h
@@ -3,11 +3,25 @@
 #ifndef TAO_TIMEPROBE_H
 #define TAO_TIMEPROBE_H
 
+#include "ace/inc_user_config.h"
+
 // Please see ace/Timeprobe.h for details on these #defines.
 
-//#define ACE_ENABLE_TIMEPROBES
-//#define ACE_MT_TIMEPROBES
-//#define ACE_TSS_TIMEPROBES
+// #define TAO_ENABLE_TIMEPROBES
+// #define TAO_MT_TIMEPROBES
+// #define TAO_TSS_TIMEPROBES
+
+#if defined (TAO_ENABLE_TIMEPROBES)
+#define ACE_ENABLE_TIMEPROBES
+#endif /* TAO_ENABLE_TIMEPROBES */
+
+#if defined (TAO_MT_TIMEPROBES)
+#define ACE_MT_TIMEPROBES
+#endif /* TAO_MT_TIMEPROBES */
+
+#if defined (TAO_TSS_TIMEPROBES)
+#define ACE_TSS_TIMEPROBES
+#endif /* TAO_TSS_TIMEPROBES */
 
 #include "ace/Timeprobe.h"
 
@@ -56,7 +70,7 @@
 #   define TAO_PP_TIMEPROBE(id)
 #   undef TAO_FUNCTION_PP_TIMEPROBE
 #   define TAO_FUNCTION_PP_TIMEPROBE(X)
-# endif /* TAO_SELECT_MINIMAL_TIMEPROBES */   
+# endif /* TAO_SELECT_MINIMAL_TIMEPROBES */
 
 #else /* ACE_ENABLE_TIMEPROBES */
 
