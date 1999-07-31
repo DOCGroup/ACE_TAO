c7674895ffc22007c48882f8fbbbc8c7c787c95b
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-07-31 20:57:22 +0000

    5.0

--- ace/Version.h
+++ ace/Version.h
@@ -1,7 +1,7 @@
 // $Id$
 // This is an automatically generated file.
 
-#define ACE_MAJOR_VERSION 4
-#define ACE_MINOR_VERSION 6
-#define ACE_BETA_VERSION 47
-#define ACE_VERSION "4.6.47"
+#define ACE_MAJOR_VERSION 5
+#define ACE_MINOR_VERSION 0
+#define ACE_BETA_VERSION 0
+#define ACE_VERSION "5.0"
