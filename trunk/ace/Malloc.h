98380b1b939ec95e8e68eac52f09bc2ac98be530
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-08-04 18:22:16 +0000

    ChangeLogTag:Wed Aug  4 13:20:54 1999  Douglas C. Schmidt  <schmidt@ace.cs.wustl.edu>

--- trunk/ace/Malloc.h
+++ trunk/ace/Malloc.h
@@ -79,20 +79,13 @@ struct ACE_Export ACE_Malloc_Stats
 #endif /* ACE_MALLOC_PADDING */
 
 #if defined (ACE_HAS_POSITION_INDEPENDENT_MALLOC)
-
-class ACE_Malloc_Header;
-class ACE_Name_Node;
-typedef ACE_Based_Pointer<ACE_Malloc_Header> ACE_MALLOC_HEADER_PTR;
-typedef ACE_Based_Pointer<ACE_Name_Node> ACE_NAME_NODE_PTR;
-typedef ACE_Based_Pointer_Basic<char> ACE_CHAR_PTR;
-
+#define ACE_MALLOC_HEADER_PTR ACE_Based_Pointer<ACE_Malloc_Header>
+#define ACE_NAME_NODE_PTR ACE_Based_Pointer<ACE_Name_Node>
+#define ACE_CHAR_PTR ACE_Based_Pointer_Basic<char>
 #else
-
-class ACE_Malloc_Header;
-class ACE_Name_Node;
-typedef ACE_Malloc_Header* ACE_MALLOC_HEADER_PTR;
-typedef ACE_Name_Node * ACE_NAME_NODE_PTR;
-typedef char * ACE_CHAR_PTR;
+#define ACE_MALLOC_HEADER_PTR ACE_Malloc_Header *
+#define ACE_NAME_NODE_PTR ACE_Name_Node *
+#define ACE_CHAR_PTR char *
 #endif /* ACE_HAS_POSITION_INDEPENDENT_MALLOC */
 
 class ACE_Export ACE_Malloc_Header
