df25025cc3a92cc775741d8153a309a3bd8f3728
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-08-05 19:44:28 +0000

    ChangeLogTag: Thu Aug 05 14:31:48 1999  David L. Levine  <levine@cs.wustl.edu>

--- ace/iosfwd.h
+++ ace/iosfwd.h
@@ -33,42 +33,44 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#if defined (ACE_HAS_STANDARD_CPP_LIBRARY) && (ACE_HAS_STANDARD_CPP_LIBRARY != 0)
+#if defined (ACE_HAS_STANDARD_CPP_LIBRARY)  && \
+    (ACE_HAS_STANDARD_CPP_LIBRARY != 0)
 
-#if !defined (ACE_USES_OLD_IOSTREAMS)
-    #include /**/ <iosfwd>
-#endif /* ! ACE_USES_OLD_IOSTREAMS */
+# if !defined (ACE_USES_OLD_IOSTREAMS)  || \
+    defined (ACE_HAS_MINIMUM_IOSTREAMH_INCLUSION)
+#   include /**/ <iosfwd>
+# endif /* ! ACE_USES_OLD_IOSTREAMS  ||  ACE_HAS_MINIMUM_IOSTREAMH_INCLUSION */
 
-#if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
-            (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
+# if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
+             (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
 
-#if !defined (ACE_USES_OLD_IOSTREAMS)
-// Make these available in the global name space
-using std::ios;
-using std::streambuf;
-using std::istream;
-using std::ostream;
-using std::iostream;
-using std::filebuf;
-using std::ifstream;
-using std::ofstream;
-using std::fstream;
-#endif /* ! ACE_USES_OLD_IOSTREAMS */
+#   if !defined (ACE_USES_OLD_IOSTREAMS)
+      // Make these available in the global name space
+      using std::ios;
+      using std::streambuf;
+      using std::istream;
+      using std::ostream;
+      using std::iostream;
+      using std::filebuf;
+      using std::ifstream;
+      using std::ofstream;
+      using std::fstream;
+#   endif /* ! ACE_USES_OLD_IOSTREAMS */
 
-#endif /* ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB */
+# endif /* ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB */
 
-# else /* ACE_HAS_STANDARD_CPP_LIBRARY */
+#else /* ! ACE_HAS_STANDARD_CPP_LIBRARY */
 
-class ios;
-class streambuf;
-class istream;
-class ostream;
-class iostream;
-class filebuf;
-class ifstream;
-class ofstream;
-class fstream;
+  class ios;
+  class streambuf;
+  class istream;
+  class ostream;
+  class iostream;
+  class filebuf;
+  class ifstream;
+  class ofstream;
+  class fstream;
 
-# endif /* ACE_HAS_STANDARD_CPP_LIBRARY */
+# endif /* ! ACE_HAS_STANDARD_CPP_LIBRARY */
 
 #endif /* ACE_IOSFWD_H */
