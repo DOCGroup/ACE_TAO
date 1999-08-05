7f470b03adaf50dbcb84771b08a2826858e27cbd
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-08-05 20:31:35 +0000

    ChangeLogTag: Thu Aug 05 15:30:32 1999  David L. Levine  <levine@cs.wustl.edu>

--- ace/streams.h
+++ ace/streams.h
@@ -30,77 +30,82 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#if defined (ACE_HAS_STANDARD_CPP_LIBRARY)  && \
-    (ACE_HAS_STANDARD_CPP_LIBRARY != 0)
+#if defined (ACE_LACKS_IOSTREAM_TOTALLY)
+  // Hack to allow support of systems that don't have iostreams.
+# define ostream FILE *
 
-# if defined (_MSC_VER)
-#   pragma warning(disable: 4018 4114 4146 4245)
-#   pragma warning(disable: 4663 4664 4665 4511 4512)
-# endif /* _MSC_VER */
+#else  /* ! ACE_LACKS_IOSTREAM_TOTALLY */
 
-# if defined (ACE_USES_OLD_IOSTREAMS)
-#   include /**/ <iostream.h>
-#   include /**/ <fstream.h>
-#   include /**/ <istream.h>
-#   include /**/ <ostream.h>
-#   include /**/ <iomanip.h>
-# else
-#   include /**/ <iostream>
-#   include /**/ <fstream>
-#   include /**/ <istream>
-#   include /**/ <ostream>
-#   include /**/ <streambuf>
-#   include /**/ <iomanip>
-#   include /**/ <ios>
-# endif /* ACE_USES_OLD_IOSTREAMS */
+#  if defined (ACE_HAS_STANDARD_CPP_LIBRARY)  && \
+      (ACE_HAS_STANDARD_CPP_LIBRARY != 0)
 
-# if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
-     (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
+#   if defined (_MSC_VER)
+#     pragma warning(disable: 4018 4114 4146 4245)
+#     pragma warning(disable: 4663 4664 4665 4511 4512)
+#   endif /* _MSC_VER */
 
-#   if !defined (ACE_USES_OLD_IOSTREAMS)
-      // Make these available in the global name space
-      using std::ios;
-      using std::streambuf;
-      using std::istream;
-      using std::ostream;
-      using std::iostream;
-      using std::filebuf;
-      using std::ifstream;
-      using std::ofstream;
-      using std::fstream;
+#   if defined (ACE_USES_OLD_IOSTREAMS)
+#     include /**/ <iostream.h>
+#     include /**/ <fstream.h>
+#     include /**/ <istream.h>
+#     include /**/ <ostream.h>
+#     include /**/ <iomanip.h>
+#   else
+#     include /**/ <iostream>
+#     include /**/ <fstream>
+#     include /**/ <istream>
+#     include /**/ <ostream>
+#     include /**/ <streambuf>
+#     include /**/ <iomanip>
+#     include /**/ <ios>
+#   endif /* ACE_USES_OLD_IOSTREAMS */
 
-      using std::cin;
-      using std::cout;
-      using std::cerr;
-      using std::clog;
+#   if defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB) && \
+       (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB != 0)
 
-      using std::endl;
-      using std::ends;
-      using std::flush;
+#     if !defined (ACE_USES_OLD_IOSTREAMS)
+        // Make these available in the global name space
+        using std::ios;
+        using std::streambuf;
+        using std::istream;
+        using std::ostream;
+        using std::iostream;
+        using std::filebuf;
+        using std::ifstream;
+        using std::ofstream;
+        using std::fstream;
 
-      using std::ws;
+        using std::cin;
+        using std::cout;
+        using std::cerr;
+        using std::clog;
 
-      using std::resetiosflags;
-      using std::setfill;
-      using std::setiosflags;
-      using std::setprecision;
-      using std::setw;
+        using std::endl;
+        using std::ends;
+        using std::flush;
 
-      using std::dec;
-      using std::hex;
-      using std::oct;
-#   endif /* ! ACE_USES_OLD_IOSTREAMS */
+        using std::ws;
 
-# endif /* ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB */
+        using std::resetiosflags;
+        using std::setfill;
+        using std::setiosflags;
+        using std::setprecision;
+        using std::setw;
 
-# if defined (_MSC_VER)
-#   pragma warning(4: 4018 4114 4146 4245)
-#   pragma warning(4: 4663 4664 4665 4512 4511)
-# endif /* _MSC_VER */
+        using std::dec;
+        using std::hex;
+        using std::oct;
+#     endif /* ! ACE_USES_OLD_IOSTREAMS */
 
-#else /* ! ACE_HAS_STANDARD_CPP_LIBRARY */
+#   endif /* ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB */
 
-# if !defined (ACE_LACKS_IOSTREAM_TOTALLY)
+#   if defined (_MSC_VER)
+#     pragma warning(4: 4018 4114 4146 4245)
+#     pragma warning(4: 4663 4664 4665 4512 4511)
+#   endif /* _MSC_VER */
+
+# else /* ! ACE_HAS_STANDARD_CPP_LIBRARY */
+
 #   include /**/ <fstream.h>
 #   include /**/ <iostream.h>
 
@@ -114,8 +119,9 @@
 #     endif /* _MSC_VER */
 #     include /**/ <strstrea.h> // VSB
 #   endif /* ACE_WIN32 */
-# endif /* ACE_LACKS_IOSTREAM_TOTALLY */
 
-#endif /* ! ACE_HAS_STANDARD_CPP_LIBRARY */
+# endif /* ! ACE_HAS_STANDARD_CPP_LIBRARY */
+
+#endif /* ! ACE_LACKS_IOSTREAM_TOTALLY */
 
 #endif /* ACE_STREAMS_H */
