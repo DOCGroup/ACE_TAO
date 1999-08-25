1ca578d5f7155389cd8c05983f944603515812a6
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-08-25 21:13:08 +0000

    *** empty log message ***

--- trunk/TAO/TAO_IDL/be/be_visitor_interface/interface_is.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_interface/interface_is.cpp
@@ -41,6 +41,9 @@ be_visitor_interface_is::~be_visitor_interface_is (void)
 int
 be_visitor_interface_is::visit_interface (be_interface *node)
 {
+  
+  this->ctx_->interface (node);
+  
   TAO_OutStream *os; // output stream
 
   if (node->impl_skel_gen () || node->imported ())
@@ -48,6 +51,7 @@ be_visitor_interface_is::visit_interface (be_interface *node)
 
   os = this->ctx_->stream ();
 
+  
   // generate the skeleton class name
 
   os->indent (); // start with whatever indentation level we are at
@@ -59,12 +63,14 @@ be_visitor_interface_is::visit_interface (be_interface *node)
   *os << idl_global->impl_class_prefix () << node->flat_name () << idl_global->impl_class_suffix () <<"::" << idl_global->impl_class_prefix () << node->flat_name () << idl_global->impl_class_suffix () <<
         " (void)" << be_idt_nl;
   
-
+  /*
   if(node->n_inherits () >0)
     {
       //generate the default constructors of all the base classes      
       node->gen_def_ctors(os);
     }
+  */
+
   *os << "{" << be_nl
       << "}" << be_nl << be_uidt_nl;
 
@@ -116,12 +122,12 @@ be_visitor_interface_is::visit_interface (be_interface *node)
                          "codegen for scope failed\n"),
                         -1);
     }
-  
+  /*
    if (node->n_inherits () > 0)
     {
       // this interface inherits from other interfaces
       be_interface *intf; // inherited interface
-
+      
       for (int i = 0; i < node->n_inherits (); i++)
 	{
 	  intf = be_interface::narrow_from_decl (node->inherits ()[i]);
@@ -135,11 +141,44 @@ be_visitor_interface_is::visit_interface (be_interface *node)
 				-1);
 	    }
 	}
-      
+      */
+  if (node->traverse_inheritance_graph (be_visitor_interface_is::method_helper, os) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "be_visitor_interface_tie_sh_ss::"
+                         "visit_interface - "
+                         "traversal of inhertance graph failed\n"),
+                        -1);
     }
- 
-
-   return 0;
+  
+  
+  return 0;
 }
 
 
+//Helper method to generate the members in the scope of the base classes
+int
+be_visitor_interface_is::method_helper (be_interface *derived,
+					    be_interface *node,
+					    TAO_OutStream *os)
+{
+
+  if (strcmp (derived->flat_name (), node->flat_name ()) != 0)
+    {
+      be_visitor_context ctx;
+      ctx.state (TAO_CodeGen::TAO_INTERFACE_IS);
+      ctx.interface (derived);
+      ctx.stream (os);
+      
+      be_visitor* visitor = tao_cg->make_visitor (&ctx);
+      if (visitor == 0 || visitor->visit_scope (node) == -1)
+        {
+          delete visitor;
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "be_visitor_interface_is::"
+                             "method_helper\n"), -1);
+        }
+      delete visitor;
+    }
+  return 0;
+}
