4c5c96950d4b61e3a814f4abe93e974544973539
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-08-25 15:17:59 +0000

    This commit was manufactured by cvs2svn to create branch 'pre-PI'.

---
+++ TAO/tests/NestedUpcall/Reactor/reactor_i.cpp
@@ -0,0 +1,107 @@
+// $Id$
+
+#include "tao/corba.h"
+#include "reactor_i.h"
+
+ACE_RCSID(Reactor, reactor_i, "$Id$")
+
+// CTOR
+Reactor_i::Reactor_i (void)
+  :  quiet_ (0)
+{
+}
+
+// DTOR
+Reactor_i::~Reactor_i (void)
+{
+}
+
+void
+Reactor_i::be_quiet (int quiet)
+{
+  this->quiet_ = quiet;
+}
+
+// register...with nothing
+CORBA::Long
+Reactor_i::register_handler (EventHandler_ptr eh,
+                             CORBA::Environment &ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  if (!this->quiet_)
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) BEGIN Reactor_i::register_handler ()\n"));
+
+  CORBA::Long r = 0;
+
+  ACE_TRY
+    {
+      r = eh->peer (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "getting peer");
+    }
+  ACE_ENDTRY;
+
+  if (!this->quiet_)
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) got this value from peer: %d\n",
+                r));
+
+  if (!this->quiet_)
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) END Reactor_i::register_handler ()\n"));
+
+  return 0;
+}
+
+void
+Reactor_i::set_value (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  if (!this->quiet_)
+    ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) doing Reactor_i::set_value()\n"));
+}
+
+CORBA::UShort
+Reactor_i::decrement (EventHandler_ptr eh,
+                      CORBA::UShort num,
+                      CORBA::Environment &env)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  if (!this->quiet_)
+    ACE_DEBUG ((LM_DEBUG, "%{%I(%P|%t) Reactor::decrement (%d)%$", num));
+
+  CORBA::UShort ret;
+  if (--num <= 0)
+    ret = 0;
+  else
+    {
+      if (!this->quiet_)
+        ACE_DEBUG ((LM_DEBUG,
+                    "(%P|%t) Reactor::decrement() "
+                    "invoking EventHandler::decrement(%d)%$", num));
+
+      Reactor_var me = _this (env);
+      ret = eh->decrement (me.in (), num, env);
+    }
+
+  if (!this->quiet_)
+    ACE_DEBUG ((LM_DEBUG,
+                "%}(%P|%t) Reactor::decrement() "
+                "returning %d\n", ret));
+
+  return ret;
+}
+
+void
+Reactor_i::stop (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  // @@ TODO Keep an ORB pointer around...
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) stopping.\n"));
+  TAO_ORB_Core_instance ()->orb ()->shutdown ();
+}
