11b54dcbc75f39b9aa9e20ee758257135df66b29
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-08-30 17:31:12 +0000

    ChangeLogTag:Mon Aug 30 12:25:48 1999  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- trunk/TAO/examples/Advanced/ch_3/server.h
+++ trunk/TAO/examples/Advanced/ch_3/server.h
@@ -1,4 +1,5 @@
 // $Id$
+
 // ============================================================================
 //
 // = LIBRARY
@@ -11,21 +12,21 @@
 //   Source code used in TAO has been modified and adapted from the code
 //   provided in the book, "Advanced CORBA Programming with C++" by Michi
 //   Henning and Steve Vinoski. Copyright 1999. Addison-Wesley, Reading,
-//   MA.
+//   MA.  Used with permission of Addison-Wesley.
 //
 //   Modified for TAO by Mike Moran <mm4@cs.wustl.edu>
 //
 // ============================================================================
-
 
 #ifndef server_HH_
 #define server_HH_
 
 #include "timeS.h"
 
-class Time_impl : public virtual POA_Time {
+class Time_impl : public virtual POA_Time 
+{ 
 public:
-    virtual TimeOfDay get_gmt() throw(CORBA::SystemException);
+  virtual TimeOfDay get_gmt (void) throw (CORBA::SystemException);
 };
 
-#endif
+#endif /* server_HH_ */
