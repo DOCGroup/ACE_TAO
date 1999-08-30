c636f264d41b9ce90c43fb3f4a8f977cb50d877c
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-08-30 17:31:12 +0000

    ChangeLogTag:Mon Aug 30 12:25:48 1999  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- TAO/examples/Advanced/ch_12/icp.h
+++ TAO/examples/Advanced/ch_12/icp.h
@@ -1,4 +1,5 @@
 // $Id$
+
 // ============================================================================
 //
 // = LIBRARY
@@ -8,35 +9,33 @@
 //    icp.h
 //
 // = AUTHORS
-//   Source code used in TAO has been modified and adapted from the code
-//   provided in the book, "Advanced CORBA Programming with C++" by Michi
-//   Henning and Steve Vinoski. Copyright 1999. Addison-Wesley, Reading,
-//   MA.
+//   Source code used in TAO has been modified and adapted from the
+//   code provided in the book, "Advanced CORBA Programming with C++"
+//   by Michi Henning and Steve Vinoski. Copyright
+//   1999. Addison-Wesley, Reading, MA.  Used with permission of
+//   Addison-Wesley.
 //
 //   Modified for TAO by Mike Moran <mm4@cs.wustl.edu>
 //
 // ============================================================================
 
-
-
-
 #ifndef _ICP_H
 #define _ICP_H
 
-extern "C" {
-    int ICP_online(unsigned long id);   // Add device
-    int ICP_offline(unsigned long id);  // Remove device
-    int ICP_get(                        // Get attribute
-            unsigned long   id,
-            const char *    attr,
-            void *          value,
-            size_t          len
-        );
-    int ICP_set(                        // Set attribute
-            unsigned long   id, 
-            const char *    attr,
-            const void *    value
-        );
+extern "C" 
+{
+  int ICP_online (unsigned long id);   // Add device
+  int ICP_offline (unsigned long id);  // Remove device
+
+  // Get attribute
+  int ICP_get (unsigned long id,
+               const char * attr,
+               void *value,
+               size_t len);
+  // Set attribute
+  int ICP_set (unsigned long id, 
+               const char *attr,
+               const void *value);
 }
 
 #endif /* _ICP_H */
