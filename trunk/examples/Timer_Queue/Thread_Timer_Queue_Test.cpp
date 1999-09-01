e5d6d90bc4dcd3bfc167c25c7f0b76acc3b4ac37
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-09-01 22:16:09 +0000

    ChangeLogTag:Wed Sep  1 15:51:41 1999  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- trunk/examples/Timer_Queue/Thread_Timer_Queue_Test.cpp
+++ trunk/examples/Timer_Queue/Thread_Timer_Queue_Test.cpp
@@ -145,7 +145,7 @@ Input_Task::svc (void)
 int
 Input_Task::add_timer (void *argument)
 {
-  u_long useconds = *ACE_reinterpret_Cast (int *, argument);
+  u_long useconds = *ACE_reinterpret_cast (int *, argument);
   ACE_Time_Value interval (useconds / usecs_,
                            useconds % usecs_);
   ACE_Time_Value expire_at = ACE_OS::gettimeofday () + interval;
@@ -178,7 +178,8 @@ Input_Task::add_timer (void *argument)
 int
 Input_Task::cancel_timer (void *argument)
 {
-  return this->queue_->cancel (*(int *) argument);
+  return this->queue_->cancel (*ACE_reinterpret_cast (int *, 
+                                                      argument));
 }
 
 // Lists the timers in the queue.  Ignores the argument. This method
