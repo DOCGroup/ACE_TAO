ed47b4afd04f0197f9234ea02349976db4d021e6
motorola <motorola@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-09-16 22:46:38 +0000

    *** empty log message ***

--- trunk/bin/update-ace+tao.sh
+++ trunk/bin/update-ace+tao.sh
@@ -3,11 +3,12 @@
 # $Id$
 
 # Define helper function to extract version number into tag format
-d='\([0-9]*\)'; version() sed -ne "s,.*$1 version $d\.$d\.$d.*,$1-\1_\2_\3,p" \
-                               -e "s,.*$1 version $d\.$d.*,$1-\1_\2,p"
+d='\([0-9]*\)'
+version() { sed -n -e "s,.*$1 version $d\.$d\.$d.*,$1-\1_\2_\3,p" \
+                   -e "s,.*$1 version $d\.$d.*,$1-\1_\2,p"; }
 
-# If ccvs found, use it instead of plain cvs since it knows about SOCKS5.
-if [ ! -z `which ccvs` ]; then cvs () ccvs $*; fi
+# Use ccvs instead of cvs since it supports SOCKS5, if that environment found.
+if [ ! -z "$SOCKS5_USER" ]; then cvs () { ccvs $*; } fi
 
 old_ace_version=`version <VERSION ACE`
 old_tao_version=`version <TAO/VERSION TAO`
