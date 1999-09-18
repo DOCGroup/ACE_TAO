e7e1948ea95ca2c9af9132a0403ac13331ec9cc8
Ossama Othman <ossama-othman@users.noreply.github.com>
1999-09-18 21:25:43 +0000

    ChangeLogTag:Sat Sep 18 16:25:17 1999  Ossama Othman  <othman@cs.wustl.edu>

--- TAO/tests/IORManipulation/IORTest.cpp
+++ TAO/tests/IORManipulation/IORTest.cpp
@@ -26,14 +26,12 @@
 #include "tao/ORB.h"
 #include "tao/IORManipulation.h"
 #include "tao/IORS.h"
-#include "ace/SString.h"
 
 ACE_RCSID(IORManipluation, IORTest, "$Id$")
 
 int
 main (int argc, char *argv[])
 {
-
   int Status = 0;
 
   ACE_DEBUG ((LM_DEBUG, "---------------------------------------------\n"));
@@ -51,14 +49,15 @@ main (int argc, char *argv[])
       // **********************************************************************
 
       // Get an object reference for the ORBs IORManipulation object!
-      CORBA::Object_ptr IORM =
+      CORBA::Object_var IORM =
         orb_->resolve_initial_references (TAO_OBJID_IORMANIPULATION,
                                           0,
                                           ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      TAO_IOP::TAO_IOR_Manipulation_ptr iorm =
-               TAO_IOP::TAO_IOR_Manipulation::_narrow (IORM, ACE_TRY_ENV);
+      TAO_IOP::TAO_IOR_Manipulation_var iorm =
+               TAO_IOP::TAO_IOR_Manipulation::_narrow (IORM.in (),
+                                                       ACE_TRY_ENV);
       ACE_TRY_CHECK;
       // **********************************************************************
 
@@ -76,35 +75,35 @@ main (int argc, char *argv[])
       // @@ CORBA::ORB::object_to_string() cannot be called on
       //    locality constrained objects such as IORManipulation.
       //        -Ossama
-      // Get the string reps for these IORs and show them to the user
-      // ACE_CString iorm_ior = orb_->object_to_string (IORM, ACE_TRY_ENV);
+      // CORBA::String_var iorm_ior =
+      //   orb_->object_to_string (IORM, ACE_TRY_ENV);
       // ACE_TRY_CHECK;
       // ACE_DEBUG ((LM_DEBUG,
       //             "\tIORManipulation IOR string = %s\n",
-      //             iorm_ior.c_str()));
+      //             iorm_ior.in ()));
 
-      ACE_CString name1_ior = orb_->object_to_string (name1.in (),
-                                                      ACE_TRY_ENV);
+      CORBA::String_var name1_ior =
+        orb_->object_to_string (name1.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      ACE_DEBUG ((LM_DEBUG, "\tFirst made up IOR = %s\n", name1_ior.c_str()));
+      ACE_DEBUG ((LM_DEBUG, "\tFirst made up IOR = %s\n", name1_ior.in ()));
 
-      ACE_CString name2_ior = orb_->object_to_string (name2.in (),
-                                                      ACE_TRY_ENV);
+      CORBA::String_var name2_ior =
+        orb_->object_to_string (name2.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      ACE_DEBUG ((LM_DEBUG, "\tSecond made up IOR = %s\n", name2_ior.c_str()));
+      ACE_DEBUG ((LM_DEBUG, "\tSecond made up IOR = %s\n", name2_ior.in ()));
 
       // **********************************************************************
       // Create IOR list for use with merge_iors.
       TAO_IOP::TAO_IOR_Manipulation::IORList iors (2);
-      iors.length(2);
+      iors.length (2);
       iors [0] = name1;
       iors [1] = name2;
 
       CORBA::Object_var merged = iorm->merge_iors (iors, ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      ACE_CString merged_ior = orb_->object_to_string (merged.in (),
-                                                       ACE_TRY_ENV);
+      CORBA::String_var merged_ior =
+        orb_->object_to_string (merged.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       CORBA::ULong count1 = iorm->get_profile_count (iors [0], ACE_TRY_ENV);
@@ -124,7 +123,7 @@ main (int argc, char *argv[])
 
       ACE_DEBUG ((LM_DEBUG, "\tMerged IOR(%d) = %s\n",
                   count,
-                  merged_ior.c_str()));
+                  merged_ior.in ()));
 
       // is_in_ior throws an exception if the intersection of the two
       // IORs is NULL.
@@ -157,8 +156,8 @@ main (int argc, char *argv[])
         iorm->remove_profiles (merged.in (), name2.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      ACE_CString just1_ior = orb_->object_to_string (just1.in (),
-                                                      ACE_TRY_ENV);
+      CORBA::String_var just1_ior =
+        orb_->object_to_string (just1.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       count = iorm->get_profile_count (just1.in (), ACE_TRY_ENV);
@@ -172,7 +171,7 @@ main (int argc, char *argv[])
       ACE_DEBUG ((LM_DEBUG,
                   "\tJust 1 IOR(%d) = %s\n",
                   count,
-                  just1_ior.c_str ()));
+                  just1_ior.in ()));
 
       in_count = iorm->is_in_ior (just1.in (), name1.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
@@ -197,14 +196,14 @@ main (int argc, char *argv[])
         ACE_DEBUG ((LM_ERROR,
                     "**ERROR: add_profile failed profile count test!\n"));
 
-      ACE_CString merged2_ior = orb_->object_to_string (merged2.in (),
-                                                        ACE_TRY_ENV);
+      CORBA::String_var merged2_ior =
+        orb_->object_to_string (merged2.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       ACE_DEBUG ((LM_DEBUG,
                   "\tAdding 2 back in IOR(%d) = %s\n",
                   count,
-                  merged2_ior.c_str ()));
+                  merged2_ior.in ()));
 
       // is_in_ior throws an exception if the intersection of the two
       // IORs is NULL.
@@ -230,8 +229,8 @@ main (int argc, char *argv[])
         iorm->remove_profiles (merged2.in (), name1.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      ACE_CString just2_ior = orb_->object_to_string (just2.in (),
-                                                      ACE_TRY_ENV);
+      CORBA::String_var just2_ior =
+        orb_->object_to_string (just2.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       count = iorm->get_profile_count (just2.in (), ACE_TRY_ENV);
@@ -243,7 +242,7 @@ main (int argc, char *argv[])
 
       ACE_DEBUG ((LM_DEBUG, "\tJust 2 IOR(%d) = %s\n",
                   count,
-                  just2_ior.c_str ()));
+                  just2_ior.in ()));
 
       in_count = iorm->is_in_ior (just2.in (), name2.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
