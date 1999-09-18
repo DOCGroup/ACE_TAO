3f98dcf39867633bf140c02930738cde01f1dc28
schmidt <douglascraigschmidt@users.noreply.github.com>
1999-09-18 23:48:44 +0000

    ChangeLogTag:Sat Sep 18 17:31:52 1999  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- examples/QOS/Sender_QOS_Event_Handler.cpp
+++ examples/QOS/Sender_QOS_Event_Handler.cpp
@@ -47,26 +47,23 @@ ACE_QOS_Event_Handler::get_handle (void) const
 int
 ACE_QOS_Event_Handler::handle_qos (ACE_HANDLE)
 {
-
   ACE_DEBUG ((LM_DEBUG,
               "\nReceived a QOS event. Inside handle_qos ()\n"));
  
   ACE_OVERLAPPED ace_overlapped;
 
-#if (defined (ACE_HAS_WINSOCK2) && (ACE_HAS_WINSOCK2 != 0))
-  const iovec iov = {5,"Hello"};
-#else
-  const iovec iov = {"Hello", 5};
-#endif
+  iovec iov;
+  iov.iov_base = "Hello";
+  iov.iov_len = 5;
 
-  // For some really weird reason if I do not define the following 
-  // sockaddr_in, the send () call fails.
+  // For some really weird reason if I do not define the following
+  // sockaddr_in, the <send> call fails.
   
   sockaddr_in s;
   ACE_UNUSED_ARG (s);
 
-  ACE_INET_Addr send_to_addr (MY_DEFPORT, DEFAULT_MULTICASTGROUP);
-
+  ACE_INET_Addr send_to_addr (MY_DEFPORT,
+                              DEFAULT_MULTICASTGROUP);
   size_t bytes_sent;
 
   if (this->dgram_mcast_.send (&iov,
@@ -79,14 +76,10 @@ ACE_QOS_Event_Handler::handle_qos (ACE_HANDLE)
     ACE_ERROR_RETURN ((LM_ERROR,
                        "Error in dgram_mcast.send ()\n"),
                       -1);
-
   else
     ACE_DEBUG ((LM_DEBUG,
                 "Using ACE_OS::sendto () : Bytes sent : %d",
                 bytes_sent));
-
   return 0;
-
-
 }
 
