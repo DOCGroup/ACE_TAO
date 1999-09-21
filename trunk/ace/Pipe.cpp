d227a8537624d53afcd2ce358abc3c2d3999913e
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-09-21 21:11:42 +0000

    Reformatted code to conform to ACE guidelines.

--- trunk/ace/Pipe.cpp
+++ trunk/ace/Pipe.cpp
@@ -16,9 +16,9 @@ ACE_Pipe::dump (void) const
 {
   ACE_TRACE ("ACE_Pipe::dump");
   ACE_DEBUG ((LM_DEBUG, ACE_BEGIN_DUMP, this));
-  ACE_DEBUG ((LM_DEBUG,  ASYS_TEXT ("handles_[0] = %d"), this->handles_[0]));
-  ACE_DEBUG ((LM_DEBUG,  ASYS_TEXT ("\nhandles_[1] = %d"), this->handles_[1]));
-  ACE_DEBUG ((LM_DEBUG,  ASYS_TEXT ("\n")));
+  ACE_DEBUG ((LM_DEBUG, ASYS_TEXT ("handles_[0] = %d"), this->handles_[0]));
+  ACE_DEBUG ((LM_DEBUG, ASYS_TEXT ("\nhandles_[1] = %d"), this->handles_[1]));
+  ACE_DEBUG ((LM_DEBUG, ASYS_TEXT ("\n")));
   ACE_DEBUG ((LM_DEBUG, ACE_END_DUMP));
 }
 
