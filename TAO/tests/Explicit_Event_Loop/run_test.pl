156e71188cbb5bf0a4f41dd5b717c8b978d307c6
mm4 <mm4@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-09-22 04:25:25 +0000

    Added ACE_TRY macros to example and brought under ACE style guidelines.

--- TAO/tests/Explicit_Event_Loop/run_test.pl
+++ TAO/tests/Explicit_Event_Loop/run_test.pl
@@ -5,8 +5,8 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-use lib "/tmp";
 use lib "../../../bin";
+
 require ACEutils;
 require Process;
 
@@ -16,8 +16,11 @@ unlink $iorfile;
 
 
 
-#Create the Server and wait for the ior file
-$server = Process::Create ("exec server$EXE_EXT >$iorfile");
+# Hacked call
+$server = Process::Create ("exec ".$EXEPREFIX."server".$EXE_EXT.">$iorfile",
+                           "");
+# Proper call
+#$server = Process::Create ($EXEPREFIX."server".$EXE_EXT, ">$iorfile");
 
 if (ACE::waitforfile_timed ($iorfile, 15) == -1) {
   print STDERR "ERROR: timedout waiting for file <$iorfile>\n";
@@ -25,16 +28,11 @@ if (ACE::waitforfile_timed ($iorfile, 15) == -1) {
   exit 1;
 }
 
-#Open and load the IOR to a variable
 open(ior_handle, "$iorfile");
 $ior_content = <ior_handle>;
 
-
-#Create the Client
 $client = Process::Create($EXEPREFIX."client$EXE_EXT", "$ior_content");
 
-
-#Wait for the Client to finish
 if ($client->TimedWait (60) == -1) {
   print STDERR "ERROR: client timedout\n";
   $status = 1;
@@ -42,7 +40,8 @@ if ($client->TimedWait (60) == -1) {
 }
 
 
-#Gracefully kill the Server
+
+
 $server->Terminate (); 
 if ($server->TimedWait (5) == -1) {
   print STDERR "ERROR: cannot terminate the server\n";
--- TAO/tests/Explicit_Event_Loop/server.h
+++ TAO/tests/Explicit_Event_Loop/server.h
@@ -1,13 +1,33 @@
 // $Id$
 
+// ============================================================================
+//
+// = LIBRARY
+//    TAO/tests/Explicit_Event_Loop
+//
+// = FILENAME
+//    server.h
+//
+// = AUTHORS
+//   Source code used in TAO has been modified and adapted from the code
+//   provided in the book, "Advanced CORBA Programming with C++" by Michi
+//   Henning and Steve Vinoski. Copyright 1999. Addison-Wesley, Reading,
+//   MA.  Used with permission of Addison-Wesley.
+//
+//   Modified for TAO by Mike Moran <mm4@cs.wustl.edu>
+//
+// ============================================================================
+
 #ifndef server_HH_
 #define server_HH_
 
 #include "timeS.h"
 
-class Time_impl : public virtual POA_Time {
+class Time_impl : public virtual POA_Time 
+{ 
 public:
-    virtual TimeOfDay get_gmt() throw(CORBA::SystemException);
+  virtual TimeOfDay get_gmt (CORBA::Environment &) 
+    ACE_THROW_SPEC ((CORBA::SystemException));
 };
 
-#endif
+#endif /* server_HH_ */
