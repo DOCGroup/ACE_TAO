3cb4ec3c2ff86adafed948c9c9cbd69bc868bb2a
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-09-23 16:29:21 +0000

    ChangeLogTag: Thu Sep 23 11:26:20 1999  David L. Levine  <levine@cs.wustl.edu>

--- trunk/TAO/performance-tests/Cubit/TAO/MT_Cubit/Timer.cpp
+++ trunk/TAO/performance-tests/Cubit/TAO/MT_Cubit/Timer.cpp
@@ -5,33 +5,33 @@
 
 MT_Cubit_Timer::MT_Cubit_Timer (u_int granularity)
   :granularity_ (granularity)
-#if defined (CHORUS)
+#if defined (CHORUS_MVME)
   , pstartTime_ (0)
   , pstopTime_ (0)
-#endif /* CHORUS */
+#endif /* CHORUS_MVME */
 {
 }
 
 void
 MT_Cubit_Timer::start (void)
 {
-#if defined (CHORUS)
+#if defined (CHORUS_MVME)
   this->pstartTime_ = pccTime1Get();
-#else /* CHORUS */
+#else /* CHORUS_MVME */
   this->timer_.start ();
-#endif /* !CHORUS */
+#endif /* !CHORUS_MVME */
 }
 
 void
 MT_Cubit_Timer::stop (void)
 {
-#if defined (CHORUS)
+#if defined (CHORUS_MVME)
   this->pstopTime_ = pccTime1Get ();
-#else /* CHORUS */
-  // If CHORUS is not defined just use plain timer_.stop ().
+#else /* CHORUS_MVME */
+  // If CHORUS_MVME is not defined just use plain timer_.stop ().
   this->timer_.stop ();
   this->timer_.elapsed_time (this->delta_);
-#endif /* !CHORUS */
+#endif /* !CHORUS_MVME */
 }
 
 ACE_timer_t
@@ -39,13 +39,13 @@ MT_Cubit_Timer::get_elapsed (void)
 {
   ACE_timer_t real_time;
 #if defined (ACE_LACKS_FLOATING_POINT)
-# if defined (CHORUS)
+# if defined (CHORUS_MVME)
   real_time = (this->pstopTime_ - this->pstartTime_) / this->granularity_;
-# else /* CHORUS */
+# else /* CHORUS_MVME */
   // Store the time in usecs.
   real_time = (this->delta_.sec () * ACE_ONE_SECOND_IN_USECS  +
                this->delta_.usec ()) / this->granularity_;
-# endif /* !CHORUS */
+# endif /* !CHORUS_MVME */
 #else  /* !ACE_LACKS_FLOATING_POINT */
   // Store the time in usecs.
   real_time = (ACE_timer_t) this->delta_.sec () * ACE_ONE_SECOND_IN_USECS  +
