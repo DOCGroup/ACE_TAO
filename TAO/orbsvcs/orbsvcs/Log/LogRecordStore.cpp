73da3b54c7023bea3e7335d75259e1b37c9e576c
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-10-10 01:35:14 +0000

    *** empty log message ***

--- TAO/orbsvcs/orbsvcs/Log/LogRecordStore.cpp
+++ TAO/orbsvcs/orbsvcs/Log/LogRecordStore.cpp
@@ -139,3 +139,27 @@ LogRecordStore::get_storage (void)
 {
   return rec_hash_;
 }
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+
+template class ACE_Hash_Map_Entry<DsLogAdmin::LogId,DsLogAdmin::LogRecord>;
+template class ACE_Hash_Map_Manager<DsLogAdmin::LogId,DsLogAdmin::LogRecord,ACE_Null_Mutex>;
+template class ACE_Hash_Map_Manager_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Iterator<DsLogAdmin::LogId,DsLogAdmin::LogRecord,ACE_Null_Mutex>;
+template class ACE_Hash_Map_Iterator_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Iterator_Base_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>;
+template class ACE_Hash_Map_Reverse_Iterator<DsLogAdmin::LogId,DsLogAdmin::LogRecord,ACE_Null_Mutex>;
+template class ACE_Hash_Map_Reverse_Iterator_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>;
+
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+
+#pragma instantiate ACE_Hash_Map_Entry<DsLogAdmin::LogId,DsLogAdmin::LogRecord>
+#pragma instantiate ACE_Hash_Map_Manager<DsLogAdmin::LogId,DsLogAdmin::LogRecord,ACE_Null_Mutex>
+#pragma instantiate ACE_Hash_Map_Manager_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>
+#pragma instantiate ACE_Hash_Map_Iterator<DsLogAdmin::LogId,DsLogAdmin::LogRecord,ACE_Null_Mutex>
+#pragma instantiate ACE_Hash_Map_Iterator_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>
+#pragma instantiate ACE_Hash_Map_Iterator_Base_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>
+#pragma instantiate ACE_Hash_Map_Reverse_Iterator<DsLogAdmin::LogId,DsLogAdmin::LogRecord,ACE_Null_Mutex>
+#pragma instantiate ACE_Hash_Map_Reverse_Iterator_Ex<DsLogAdmin::LogId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::LogId>, ACE_Equal_To<DsLogAdmin::LogId>, ACE_Null_Mutex>
+
+#endif /* ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA */
