e3660e48c5bc5bdaa8d8db75c0d76375b8f16344
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-10-11 17:06:29 +0000

    New deferred synchronous client added to tests.

--- TAO/performance-tests/Latency/run_test.pl
+++ TAO/performance-tests/Latency/run_test.pl
@@ -88,4 +88,41 @@ if ($server != 0 || $client != 0) {
   exit 1;
 }
 
+print STDERR "================ Deferred synchronous test\n";
+
+unlink $iorfile;
+
+$SV = Process::Create ($EXEPREFIX."st_server$EXE_EXT ",
+                       " -ORBSvcConf st_server.conf"
+                       . " -o $iorfile");
+
+if (ACE::waitforfile_timed ($iorfile, 5) == -1) {
+  print STDERR "ERROR: cannot find file <$iorfile>\n";
+  $SV->Kill (); $SV->TimedWait (1);
+  exit 1;
+}
+
+$CL = Process::Create ($EXEPREFIX."deferred_synch_client$EXE_EXT ",
+                       " -ORBSvcConf client-st-muxed.conf "
+                       . " -k file://$iorfile "
+                       . " -i 1000");
+
+$client = $CL->TimedWait (60);
+if ($client == -1) {
+  print STDERR "ERROR: client timedout\n";
+  $CL->Kill (); $CL->TimedWait (1);
+}
+
+$server = $SV->TimedWait (5);
+if ($server == -1) {
+  print STDERR "ERROR: server timedout\n";
+  $SV->Kill (); $SV->TimedWait (1);
+}
+
+unlink $iorfile;
+
+if ($server != 0 || $client != 0) {
+  exit 1;
+}
+
 exit 0;
