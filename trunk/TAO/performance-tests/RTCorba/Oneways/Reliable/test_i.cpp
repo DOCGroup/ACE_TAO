68fd2a704e9abe2376a3f15f8f1d61853ec9ef70
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-11-29 20:11:30 +0000

    Test suite for reliable oneways.

---
+++ trunk/TAO/performance-tests/RTCorba/Oneways/Reliable/test_i.cpp
@@ -0,0 +1,41 @@
+// $Id$
+
+ACE_RCSID(Reliable, test_i, "$Id$")
+
+#include "test_i.h"
+#include "tao/debug.h"
+#include "ace/ACE.h"
+
+#if !defined(__ACE_INLINE__)
+#include "test_i.i"
+#endif /* __ACE_INLINE__ */
+
+void
+Test_i::oneway_op (CORBA::ULong request_number,
+                   CORBA::Environment &) 
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  if (TAO_debug_level > 0)
+    ACE_DEBUG ((LM_DEBUG,
+                "server: Oneway iteration %d @ %T\n",
+                request_number));
+}
+
+void
+Test_i::twoway_op (CORBA::ULong request_number,
+                   CORBA::Environment &) 
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  if (TAO_debug_level > 0)
+    ACE_DEBUG ((LM_DEBUG,
+                "server: Twoway iteration %d @ %T\n",
+                request_number));
+}
+
+void
+Test_i::shutdown (CORBA::Environment& ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  this->orb_->shutdown (0, 
+                        ACE_TRY_ENV);
+}
