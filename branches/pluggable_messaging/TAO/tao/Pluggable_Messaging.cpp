fad93c4a4fc270769e68d5811ce1d52fae0cbb9d
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-12-02 20:52:53 +0000

    Just

---
+++ branches/pluggable_messaging/TAO/tao/Pluggable_Messaging.cpp
@@ -0,0 +1,23 @@
+#include "Pluggable_Messaging.h"
+//$Id$
+#if defined (__ACE_INLINE__)
+# include "tao/Pluggable_Messaging.i"
+#endif /* __ACE_INLINE__ */
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// Methods for the Pluugable Messaging stuff
+/////////////////////////////////////////////////////////////////////////
+
+TAO_Pluggable_Message_Factory::TAO_Pluggable_Message_Factory (void)
+{
+  //no-op
+}
+
+
+TAO_Pluggable_Message_Factory::~TAO_Pluggable_Message_Factory (void)
+{
+  //no-op
+}
+
