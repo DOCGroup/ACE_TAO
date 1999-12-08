c3e4de45aa876dc59fa8fa2984f8c8e2c26e6889
marina <marina@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-12-08 02:42:23 +0000

    ChangeLogTag:Tue Dec  7 20:38:32 1999  Marina Spivak  <marina@cs.wustl.edu>

--- trunk/TAO/performance-tests/RTCorba/Multiple_Endpoints/Endpoint_Per_Priority/client.cpp
+++ trunk/TAO/performance-tests/RTCorba/Multiple_Endpoints/Endpoint_Per_Priority/client.cpp
@@ -23,7 +23,11 @@ public:
   Client (void);
   // ctor
 
-  void set (Test_ptr server, int niterations, int id, CORBA::ORB_ptr);
+  void set (Test_ptr server,
+            int niterations,
+            int id, CORBA::ORB_ptr,
+            ACE_Barrier *before,
+            ACE_Barrier *after);
   // Set the test attributes.
 
   void accumulate_into (ACE_Throughput_Stats &throughput) const;
@@ -49,6 +53,9 @@ private:
 
   ACE_Throughput_Stats throughput_;
   // Keep throughput statistics on a per-thread basis
+
+  ACE_Barrier *before_connection_;
+  ACE_Barrier *after_connection_;
 };
 
 // ****************************************************************
@@ -157,9 +164,18 @@ main (int argc, char *argv[])
 
       RTCORBA::PriorityMapping *pm =
         orb->orb_core ()->priority_mapping ();
+
+      ACE_Barrier before_connection (nthreads);
+      ACE_Barrier after_connection (nthreads);
+
       for (int i = 0; i != nthreads; ++i)
         {
-          client[i].set (server.in (), niterations, i, orb.in ());
+          client[i].set (server.in (),
+                         niterations,
+                         i,
+                         orb.in (),
+                         &before_connection,
+                         &after_connection);
 
           CORBA::Short native_priority = 0;
           pm->to_native (priorities[i], native_priority);
@@ -212,12 +228,16 @@ void
 Client::set (Test_ptr server,
              int niterations,
              int id,
-             CORBA::ORB_ptr orb)
+             CORBA::ORB_ptr orb,
+             ACE_Barrier *before,
+             ACE_Barrier *after)
 {
   this->server_ = Test::_duplicate (server);
   this->niterations_ = niterations;
   this->id_ = id;
   orb_ = orb;
+  this->before_connection_ = before;
+  this->after_connection_ = after;
 }
 
 int
@@ -235,7 +255,7 @@ Client::svc (void)
               native_priority));
 
 
-  int i;
+  int i = 0;
   ACE_TRY_NEW_ENV
     {
       // Set the Client Priority Policy for invocations.
@@ -279,6 +299,8 @@ Client::svc (void)
       policy->destroy (ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
+
+      this->before_connection_->wait ();
       // Try to increase chances of each thread having its own
       // connection. (Not an issue if all threads have different priorities.)
       for (int j = 0; j < 100; ++j)
@@ -292,6 +314,8 @@ Client::svc (void)
                                 ACE_TRY_ENV);
           ACE_TRY_CHECK;
         }
+
+      this->after_connection_->wait ();
 
       ACE_hrtime_t throughput_base = ACE_OS::gethrtime ();
 
@@ -314,15 +338,9 @@ Client::svc (void)
 	      ACE_OS::sleep (tv);
 	    }
 
-          // Skip the first sample (prime).
-          if (i != 0)
-            {
-              // Record statistics.
-              this->throughput_.sample (now - throughput_base,
-                                        now - latency_base);
-            }
-          else
-            throughput_base = ACE_OS::gethrtime ();
+          // Record statistics.
+          this->throughput_.sample (now - throughput_base,
+                                    now - latency_base);
         }
     }
   ACE_CATCHANY
