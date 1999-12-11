24769b41502de72321a1595a3b6633931763ff1d
bala <balanatarajan@users.noreply.github.com>
1999-12-11 02:35:46 +0000

    First revision

---
+++ TAO/tao/GIOP_Accept_State.h
@@ -0,0 +1,38 @@
+// -*- C++ -*-
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//     TAO
+//
+// = FILENAME
+//     GIOP_Message.h
+//
+// = DESCRIPTION
+//     Interface for the GIOP messaging protocol
+//
+// = AUTHOR
+//     Copyright 1994-1995 by Sun Microsystems Inc.,
+//     Balachandran Natarajan <bala@cs.wustl.edu>
+//
+// ============================================================================
+#ifndef _TAO_GIOP_SERVER_STATE_
+#define _TAO_GIOP_SERVER_STATE_
+
+class TAO_Export TAO_GIOP_Accept_State
+{
+  // = TITLE
+  //   An abstract interface for different versions of the GIOP
+  //   implementation classes.
+  //
+  // = DESCRIPTION
+  //
+  //
+public:
+  
+private:
+};
+
+
+#endif /*_TAO_GIOP_SERVER_STATE_*/
