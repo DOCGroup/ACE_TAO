0b6a6c9c446054347d7fee9407db677648bce1f1
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-12-16 20:16:37 +0000

    *** empty log message ***

---
+++ branches/pluggable_messaging/TAO/tao/GIOP_Acceptors.h
@@ -0,0 +1,36 @@
+// -*- C++ -*-
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//     TAO
+//
+// = FILENAME
+//     GIOP_Acceptors.h
+//
+// = DESCRIPTION
+//     Concrete classes for the different versions of GIOP classes 
+//
+// = AUTHOR
+//   Balachandran Natarajan <bala@cs.wustl.edu>
+//
+// ============================================================================
+#ifndef _TAO_GIOP_ACCEPTORS_H_
+#define _TAO_GIOP_ACCEPTORS_H_
+#include "tao/GIOP_Accept_State.h"
+
+class TAO_Export TAO_GIOP_Acceptor_1_1: 
+public TAO_GIOP_Accept_State
+{
+  // = TITLE
+  //   Definitions of GIOP 1.1 specific stuff
+  //
+  // = DESCRIPTION
+  //   This class will hold the specific details of 1.1 
+  //  
+ public:
+ private:
+};
+
+#endif /*_TAO_GIOP_ACCEPTORS_H_*/
