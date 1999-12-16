3f95851a6273cfc64074381efc10f663258d6f30
Ossama Othman <ossama-othman@users.noreply.github.com>
1999-12-16 23:40:07 +0000

    ChangeLogTag:Thu Dec 16 17:25:42 1999  Ossama Othman  <othman@cs.wustl.edu>

--- TAO/orbsvcs/orbsvcs/Naming/Persistent_Naming_Context.cpp
+++ TAO/orbsvcs/orbsvcs/Naming/Persistent_Naming_Context.cpp
@@ -506,6 +506,7 @@ TAO_Persistent_Naming_Context::list (CORBA::ULong how_many,
       ACE_NEW_THROW_EX (bind_iter,
                         ITER_SERVANT (this, hash_iter, this->poa_.in (), this->lock_),
                         CORBA::NO_MEMORY ());
+      ACE_CHECK;
 
       // Release <hash_iter> from auto pointer, and start using the
       // reference counting to control our servant.
