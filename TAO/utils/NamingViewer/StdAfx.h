d2472501c7ed329ede246ff1e7470a965beeab23
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
1999-12-23 18:09:55 +0000

    ChangeLogTag:Thu Dec 23 12:06:42 1999  Nanbor Wang  <nanbor@cs.wustl.edu>

--- TAO/utils/NamingViewer/StdAfx.h
+++ TAO/utils/NamingViewer/StdAfx.h
@@ -11,16 +11,20 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+
+#if !defined (ACE_HAS_MFC) || (ACE_HAS_MFC == 0)
+#error You must build your ACE/TAO with MFC support (by defining"ACE_HAS_MFC=1")
+#endif
+
+#include "tao/corba.h"
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC support for Windows Common Controls
+#include <afxcmn.h>                     // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
-
-#include "tao/corba.h"
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
