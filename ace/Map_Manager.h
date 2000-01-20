41eef8780e45fd44da831495735867bb09c785ca
schmidt <douglascraigschmidt@users.noreply.github.com>
2000-01-20 01:38:19 +0000

    ChangeLogTag:Tue Jan 19 13:33:08 2000  Joe Hoffert  <joeh@cs.wustl.edu>

--- ace/Map_Manager.h
+++ ace/Map_Manager.h
@@ -10,7 +10,7 @@
 //    Map_Manager.h
 //
 // = AUTHOR
-//    Doug Schmidt
+//    Douglas C. Schmidt <schmidt@cs.wustl.edu>
 //
 // ============================================================================
 
