7d1bf8ad89990be06b5ed4de5d460014a934f4d6
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-01-23 02:27:06 +0000

    ChangeLogTag:Sat Jan 22 18:25:51 2000  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/orbsvcs/orbsvcs/Event/EC_Supplier_Filter.cpp
+++ TAO/orbsvcs/orbsvcs/Event/EC_Supplier_Filter.cpp
@@ -3,7 +3,7 @@
 #include "EC_Supplier_Filter.h"
 
 #if ! defined (__ACE_INLINE__)
-#include "EC_SupplierFilter.i"
+#include "EC_Supplier_Filter.i"
 #endif /* __ACE_INLINE__ */
 
 ACE_RCSID(Event, EC_Supplier_Filter, "$Id$")
