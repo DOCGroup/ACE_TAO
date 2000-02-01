8f887b0854ea83568ddf81cabaabd69acf3499cf
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-02-01 03:16:56 +0000

    ChangeLogTag:Mon Jan 31 19:02:34 2000  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/orbsvcs/tests/Event/Basic/run_test.pl
+++ TAO/orbsvcs/tests/Event/Basic/run_test.pl
@@ -82,6 +82,14 @@ if ($T->TimedWait (60) == -1) {
   $T->Kill (); $T->TimedWait (1);
 }
 
+print STDERR "\n\nBitmask tests\n";
+$T = Process::Create ($prefix . "Bitmask".$EXE_EXT);
+if ($T->TimedWait (60) == -1) {
+  print STDERR "ERROR: Test timedout\n";
+  $status = 1;
+  $T->Kill (); $T->TimedWait (1);
+}
+
 print STDERR "\n\nDisconnect callbacks test\n";
 $T = Process::Create ($prefix . "Disconnect".$EXE_EXT);
 if ($T->TimedWait (60) == -1) {
