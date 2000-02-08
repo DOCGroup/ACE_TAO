91d10d8822701a96ef5485af4217b88adb0eb9c2
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-02-08 07:34:58 +0000

    Tue Feb  8 01:26:29 2000  Pradeep Gore  <pradeep@cs.wustl.edu>

--- TAO/orbsvcs/orbsvcs/Notify/Notify_EventChannelFactory_i.h
+++ TAO/orbsvcs/orbsvcs/Notify/Notify_EventChannelFactory_i.h
@@ -19,11 +19,15 @@
 #ifndef NOTIFY_EVENTCHANNELFACTORY_I_H
 #define NOTIFY_EVENTCHANNELFACTORY_I_H
 
+#include "ace/Hash_Map_Manager.h"
 #include "orbsvcs/orbsvcs/CosNotifyChannelAdminS.h"
+#include "orbsvcs/orbsvcs/Notify/ID_Pool_T.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
+
+class TAO_Notify_EventChannel_i;
 
 class TAO_ORBSVCS_Export TAO_Notify_EventChannelFactory_i :
 public virtual POA_CosNotifyChannelAdmin::EventChannelFactory
@@ -44,7 +48,7 @@ public:
   void activate (CORBA::Environment &ACE_TRY_ENV);
   // Activate the servant in the default POA.
 
-  CosNotifyChannelAdmin::EventChannelFactory_ptr get_refx (CORBA::Environment &ACE_TRY_ENV);
+  CosNotifyChannelAdmin::EventChannelFactory_ptr get_ref (CORBA::Environment &ACE_TRY_ENV);
   // Transform to object ref.
 
   // = CosNotifyChannelAdmin::EventChannelFactory methods.
@@ -77,8 +81,21 @@ virtual CosNotifyChannelAdmin::EventChannel_ptr get_event_channel (
   ));
 
  protected:
- CosNotifyChannelAdmin::EventChannelFactory_var self_;
- // Reference to the CORBA Object associated with this servant.
+  CosNotifyChannelAdmin::EventChannelFactory_var self_;
+  // Reference to the CORBA Object associated with this servant.
+
+  ID_Pool<CosNotifyChannelAdmin::AdminID> ec_ids;
+  // Id generator for event channels
+
+  typedef
+  ACE_Hash_Map_Manager <CosNotifyChannelAdmin::ChannelID,
+                                               TAO_Notify_EventChannel_i*,
+                                               ACE_SYNCH_MUTEX>
+  EC_MAP;
+
+  EC_MAP ec_map_;
+  // Mapping of channel id's to EC's.
+
 };
 
 #endif /* NOTIFY_EVENTCHANNELFACTORY_I_H */
