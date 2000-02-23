078e494bb00d4cba69224a03c9c290d8e23c9a3b
Steve Huston <shuston@riverace.com>
2000-02-23 23:06:47 +0000

    ChangeLogTag:Wed Feb 23 17:02:34 2000  Steve Huston  <shuston@riverace.com>

--- tests/makeicc.pl
+++ tests/makeicc.pl
@@ -9,31 +9,22 @@ foreach $tmp (@ls)
 	$tmp2 = "$a1\icc";
 
 open(T,"> $a1\icc");
+	print T "// $Id$\n";
+	print T "\n";
+	print T "include \"vacpp_setup.icc\"\n";
 	print T "option\n";
-        print T "    define (\"ACE_HAS_WINSOCK2\",\"1\"),\n";
-        print T "    define (\"__ACE_INLINE__\",\"0\"),\n";
-        print T "    define (\"WIN32\",\"1\"),\n";
-        print T "    define (\"_WINDOWS\",\"1\"),\n";
-        print T "    define(\"ACE_HAS_DLL\",\"0\"),\n";
-
-        print T "    link(libSearchPath,'H:\\ACE_wrappers\\ace'),\n";
-	print T "    incl(searchPath,'H:\\ACE_wrappers'),\n";
-	print T "    link(linkWithMultiThreadLib,yes)\n";
-	print T "     {\n";
+        print T "    link(libSearchPath, platformLibSearchPath),\n";
+	print T "    incl(searchPath, \"..\"),\n";
+	print T "    link(linkWithMultiThreadLib,yes),\n";
+	print T "    link(debug)\n";
+	print T "    {\n";
 	chop($a1);
-	print T "	target \"$a1\.exe\"\n";
+	print T "	target type (exe) \"$a1\"\n";
 	print T "	{\n";
-
-
-	print T "	   source type (cpp) \"$a1\.cpp\"\n\n";
-	print T "	   source type (lib) \"aced.lib\"\n";
-	print T "   	   source type (lib) \"advapi32.lib\"\n";
-	print T "	   source type (lib) \"user32.lib\"\n";
-	print T "	   source type (lib) \"ws2_32.lib\"\n";
-	print T "	   source type (lib) \"wsock32.lib\"\n";
-	print T "	   source type (lib) \"mswsock.lib\"\n";
+	print T "	   source type (cpp) \"$a1\.cpp\"\n";
+	print T "	   source platformLinkLibs\n";
 	print T "	}\n";
-	print T "   }\n";
+	print T "    }\n";
 close T;
 
 open(ALLICC,">> $allicc");
