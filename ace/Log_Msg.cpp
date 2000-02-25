4ad0881c9afa7b16c9b3ada305eed19564b1abe4
Steve Huston <shuston@riverace.com>
2000-02-25 22:05:26 +0000

    ChangeLogTag:Fri Feb 25 16:50:37 2000  Steve Huston  <shuston@riverace.com>

--- ace/Log_Msg.cpp
+++ ace/Log_Msg.cpp
@@ -1107,7 +1107,8 @@ ACE_Log_Msg::log (const ASYS_TCHAR *format_str,
                   ACE_OS::sprintf (bp, ASYS_TEXT ("%u"),
                                    pthread_getunique_np(&t_id));
 #  else
-                  ACE_OS::sprintf (bp, ASYS_TEXT ("%lu"), t_id);
+                  ACE_OS::sprintf (bp, ASYS_TEXT ("%lu"),
+                                   ACE_static_cast (unsigned long, t_id));
 #  endif /* ACE_HAS_PTHREADS_DRAFT4 && HPUX_10 */
 
 #endif /* ACE_WIN32 */
