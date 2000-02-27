72dbdbb6aa1735a1369ef384434a4a6ba5d91edc
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-02-27 23:12:27 +0000

    ChangeLogTag:Sat Feb 26 21:45:45 2000  Carlos O'Ryan  <coryan@uci.edu>

---
+++ branches/ESF/TAO/orbsvcs/orbsvcs/ESF/ESF_Peer_Admin.cpp
@@ -0,0 +1,46 @@
+// $Id$
+
+#ifndef TAO_ESF_PEER_ADMIN_CPP
+#define TAO_ESF_PEER_ADMIN_CPP
+
+#include "ESF_Peer_Admin.h"
+
+#if ! defined (__ACE_INLINE__)
+#include "ESF_Peer_Admin.i"
+#endif /* __ACE_INLINE__ */
+
+#include "ESF_Peer_Workers.h"
+
+ACE_RCSID(ESF, ESF_Peer_Admin, "$Id$")
+
+template<class EC, class P, class PEER> void
+TAO_ESF_Peer_Admin<EC,P,PEER>::peer_connected (PEER *peer,
+                                               CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC (())
+{
+  TAO_ESF_Peer_Connected<P,PEER> worker (peer);
+
+  this->for_each (&worker, ACE_TRY_ENV);
+}
+
+template<class EC, class P, class PEER> void
+TAO_ESF_Peer_Admin<EC,P,PEER>::peer_reconnected (PEER *peer,
+                                                 CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC (())
+{
+  TAO_ESF_Peer_Reconnected<P,PEER> worker (peer);
+
+  this->for_each (&worker, ACE_TRY_ENV);
+}
+
+template<class EC, class P, class PEER> void
+TAO_ESF_Peer_Admin<EC,P,PEER>::peer_disconnected (PEER *peer,
+                                                  CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC (())
+{
+  TAO_ESF_Peer_Disconnected<P,PEER> worker (peer);
+
+  this->for_each (&worker, ACE_TRY_ENV);
+}
+
+#endif /* TAO_ESF_PEER_ADMIN_CPP */
