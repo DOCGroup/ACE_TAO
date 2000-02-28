e472390bb0f54bd677f9ef9f5536a840705f6efb
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-02-28 20:40:53 +0000

    *** empty log message ***

--- branches/pluggable_messaging/TAO/tao/GIOP_Message_Base.cpp
+++ branches/pluggable_messaging/TAO/tao/GIOP_Message_Base.cpp
@@ -348,42 +348,7 @@ TAO_GIOP_Message_Base::dump_msg (const char *label,
     }
 }
 
-int
-TAO_GIOP_Message_Base::parse_magic_bytes (TAO_GIOP_Message_State *state)
-{
-  // Grab the read pointer
-  char *buf = state->cdr.rd_ptr ();
-  
-  // The values are hard-coded to support non-ASCII platforms.
-  if (!(buf [0] == 0x47      // 'G'
-        && buf [1] == 0x49   // 'I'
-        && buf [2] == 0x4f   // 'O'
-        && buf [3] == 0x50)) // 'P'
-    {
-      // For the present...
-      if (TAO_debug_level > 0)
-        ACE_DEBUG ((LM_DEBUG,
-                    ASYS_TEXT ("TAO (%P|%t) bad header, magic word [%c%c%c%c]\n"), 
-                    buf[0],
-                    buf[1],
-                    buf[2],
-                    buf[3]));
-      return -1;
-    }
 
-  if (this->validate_version (state) == -1)
-    {
-      if (TAO_debug_level > 0)
-        {
-          ACE_DEBUG ((LM_DEBUG,
-                      ASYS_TEXT ("(%N|%l|%p|%t) Error in validating")
-                      ASYS_TEXT ("revision \n")));
-          return -1;
-        }
-    }
-  
-  return 0;
-}
 
 int
 TAO_GIOP_Message_Base::parse_header (TAO_GIOP_Message_State *state)
--- branches/pluggable_messaging/TAO/tao/GIOP_Message_Connectors.cpp
+++ branches/pluggable_messaging/TAO/tao/GIOP_Message_Connectors.cpp
@@ -261,3 +261,29 @@ TAO_GIOP_Message_Connector_11::
 
   return 0;
 }
+
+CORBA::Octet
+TAO_GIOP_Message_Connector_11:: major_version (void)
+{
+  // Any harm in hardcoding??
+  return (CORBA::Octet) 1;
+}
+
+CORBA::Octet
+TAO_GIOP_Message_Connector_11:: minor_version (void)
+{
+  // Any harm in hardcoding??
+  return (CORBA::Octet) 1;
+}
+
+
+///////////////////////////////////////////////////////////////////
+// Methods for TAO_GIOP_Message_Connector_10
+//////////////////////////////////////////////////////////////////
+
+CORBA::Octet
+TAO_GIOP_Message_Connector_10:: minor_version (void)
+{
+  // Any harm in hardcoding??
+  return (CORBA::Octet) 0;
+}
