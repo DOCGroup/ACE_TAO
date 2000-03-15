ba62f2521e9fdae80e67a349ec42d4782737abf1
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-03-15 23:31:03 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_1'.

---
+++ tags/ACE-5_1/examples/Shared_Memory/test_SV.cpp
@@ -0,0 +1,62 @@
+// $Id$
+
+#include "ace/Shared_Memory_SV.h"
+
+ACE_RCSID(Shared_Memory, test_SV, "$Id$")
+
+#define SHMSZ 27
+#define SHM_KEY 5678
+
+static void
+client (void)
+{
+  ACE_Shared_Memory_SV shm_client (SHM_KEY, SHMSZ);
+  char *shm = (char *) shm_client.malloc ();
+
+  for (char *s = shm; *s != '\0'; s++)
+    putchar (*s);
+
+  putchar ('\n');
+  *shm = '*';
+}
+
+static void
+server (void)
+{
+  ACE_Shared_Memory_SV shm_server (SHM_KEY, SHMSZ, 
+				   ACE_Shared_Memory_SV::ACE_CREATE);
+  char *shm = (char *) shm_server.malloc ();
+  char *s   = shm;
+
+  for (char c = 'a'; c <= 'z'; c++)
+    *s++ = c;
+
+  *s = '\0';
+
+  while (*shm != '*')
+    ACE_OS::sleep (1);
+
+  if (shm_server.remove () < 0)
+    ACE_ERROR ((LM_ERROR, "%p\n", "remove"));
+}
+
+int
+main (int, char *[])
+{
+  switch (ACE_OS::fork ())
+    {
+    case -1:
+      ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "fork"), 1);
+    case 0: 
+      // Make sure the server starts up first.
+      ACE_OS::sleep (1); 
+      client ();
+      break;
+    default:
+      server ();
+      break;
+    }
+
+  return 0;
+}
+
