ff2a9150408bc3de8dfb3ecec7e652db547fedc1
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-03-15 23:31:03 +0000

    5.1

--- ace/Version.h
+++ ace/Version.h
@@ -2,6 +2,6 @@
 // This is an automatically generated file.
 
 #define ACE_MAJOR_VERSION 5
-#define ACE_MINOR_VERSION 0
-#define ACE_BETA_VERSION 16
-#define ACE_VERSION "5.0.16"
+#define ACE_MINOR_VERSION 1
+#define ACE_BETA_VERSION 0
+#define ACE_VERSION "5.1"
