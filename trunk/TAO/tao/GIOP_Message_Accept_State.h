987853a53e2dee443a2de1c25bcdfae373b6c9b2
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-03-16 00:22:33 +0000

    ChangeLogTag: Wed Mar 15 17:34:47 2000  Balachandran Natarajan  <bala@cs.wustl.edu>

---
+++ trunk/TAO/tao/GIOP_Message_Accept_State.h
@@ -0,0 +1,122 @@
+// -*- C++ -*-
+//$Id$
+// ============================================================================
+//
+// = LIBRARY
+//     TAO
+//
+// = FILENAME
+//     GIOP_Message_Acceptors.h
+//
+// = DESCRIPTION
+//   Interface for the different accept states     
+//
+// = AUTHOR
+//     Balachandran Natarajan <bala@cs.wustl.edu>
+// ============================================================================
+#ifndef TAO_GIOP_MESSAGE_ACCEPT_STATE_H
+#define TAO_GIOP_MESSAGE_ACCEPT_STATE_H
+#include "tao/GIOP_Server_Request.h"
+#include "tao/GIOP_Assorted_Headers.h"
+
+
+class TAO_GIOP_Message_Accept_State
+{
+  // = TITLE
+  //   TAO_GIOP_Message_Accept_State
+  //
+  // = DESCRIPTION
+  //   An abstract base class for different versions of GIOP. This is
+  //   similar to the base class in the strategy pattern
+  //  
+public:
+
+  virtual int parse_request_header (TAO_GIOP_ServerRequest &) = 0;
+  // Parse the Request Header from the incoming stream. This will do a
+  // version specific parsing of the incoming Request header
+  
+  virtual CORBA::Boolean write_reply_header (TAO_OutputCDR &output,
+                                             CORBA::ULong request_id) = 0;
+  // Write the reply header in to <output>
+
+  virtual int parse_locate_header (TAO_GIOP_Locate_Request_Header &) = 0; 
+  // Parse the Loacte Request Header from the incoming stream. This will do a
+  // version specific parsing of the incoming Request header
+ 
+  virtual CORBA::Boolean 
+  write_locate_reply_mesg (TAO_OutputCDR &output,
+                           CORBA::ULong request_id,
+                           TAO_GIOP_Locate_Status_Msg &status) = 0;
+  // Writes the locate _reply message in to the <output>
+  
+  virtual CORBA::Octet major_version (void) = 0;
+  virtual CORBA::Octet minor_version (void) = 0;
+  // Our versions
+
+ private:
+};
+
+class TAO_GIOP_Message_Accept_State_11 :
+  public TAO_GIOP_Message_Accept_State
+{
+  // = TITLE
+  //   TAO_GIOP_Message_Accept_State_11
+  // = DESCRIPTION
+  //   
+  
+public:
+  virtual int parse_request_header (TAO_GIOP_ServerRequest &);
+  // Parse the Request Header from the incoming stream. This will do a
+  // version specific parsing of the incoming Request header
+
+
+  virtual CORBA::Boolean  write_reply_header (TAO_OutputCDR &output,
+                                              CORBA::ULong request_id);
+  // Write the version specific reply header in to <output>
+  
+  virtual int parse_locate_header (TAO_GIOP_Locate_Request_Header &);
+  // Parse the Loacte Request Header from the incoming stream. This will do a
+  // version specific parsing of the incoming Request header
+  
+  virtual CORBA::Boolean 
+  write_locate_reply_mesg (TAO_OutputCDR &output,
+                           CORBA::ULong request_id,
+                           TAO_GIOP_Locate_Status_Msg &status);
+  // Writes the locate reply message in to <output>
+  
+  virtual CORBA::Octet major_version (void);
+  virtual CORBA::Octet minor_version (void);
+  // Our versions
+
+private:
+
+};
+
+class TAO_GIOP_Message_Accept_State_10 :
+  public TAO_GIOP_Message_Accept_State_11
+{
+  // = TITLE
+  // = DESCRIPTION
+public:
+  virtual CORBA::Octet minor_version (void);
+};
+
+class TAO_GIOP_Message_Accept_Impl
+{
+  // = TITLE
+  // = DESCRIPTION
+ public:
+  CORBA::Boolean check_revision (CORBA::Octet incoming_major,
+                                 CORBA::Octet incoming_minor);
+  // Performs a check of the revision tags
+
+  TAO_GIOP_Message_Accept_State_10 version_10;
+  TAO_GIOP_Message_Accept_State_11 version_11;
+  
+};
+
+#if defined (__ACE_INLINE__)
+# include "tao/GIOP_Message_Accept_State.i"
+#endif /* __ACE_INLINE__ */
+
+#endif /*_TAO_GIOP_MESSAGE_ACCEPT_STATE_H_*/
---
+++ trunk/TAO/tao/Pluggable_Messaging_Utils.h
@@ -0,0 +1,105 @@
+// -*- C++ -*-
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//     TAO
+//
+// = FILENAME
+//     Pluggable_Messaging_Utils.h
+//
+// = DESCRIPTION
+//     Utility classes for the TAO pluggable messaging framework.
+//
+// = AUTHOR
+//     Balachandran Natarajan <bala@cs.wustl.edu>
+//
+// ============================================================================
+#ifndef TAO_PLUGGABLE_MESSAGING_UTILS_H
+#define TAO_PLUGGABLE_MESSAGING_UTILS_H
+#include "tao/IOPC.h"
+
+class TAO_Export TAO_Pluggable_Connector_Params
+{
+  // = TITLE
+  //   TAO_Pluggable_Connector_Params
+  //
+  // = DESCRIPTION
+  //   This represents a set of data that would be received by the
+  //   connector from the acceptor. 
+public:
+  TAO_Pluggable_Connector_Params (void);
+  // Ctor
+
+  IOP::ServiceContextList  svc_ctx_;
+  // The IOP service context list
+
+  CORBA::ULong request_id_;
+  // The request id for which the reply we (connector) has received 
+
+  CORBA::ULong reply_status_;
+  // The reply status
+};
+
+typedef enum HeaderType
+{
+  // = TITLE
+  // = DESCRIPTION
+  // 
+  TAO_PLUGGABLE_MESSAGE_REQUEST_HEADER = 0,
+  TAO_PLUGGABLE_MESSAGE_LOCATE_REQUEST_HEADER
+}TAO_Pluggable_Header_Type;
+
+
+typedef enum MessageType
+{
+  // = DESCRIPTION
+  //   Provide an external interface for the users of this pluggable
+  //   messaging framework to denote  existing message types. This has
+  //   an inspiration from GIOP. So if anybody wants to add more message
+  //   types you are welcome but please do not change the numbering
+  //   scheme as this would affect GIOP. 
+  
+  TAO_PLUGGABLE_MESSAGE_REQUEST = 0,                // sent by client.
+  TAO_PLUGGABLE_MESSAGE_REPLY = 1,                  // by server.
+  TAO_PLUGGABLE_MESSAGE_CANCELREQUEST = 2,          // by client.
+  TAO_PLUGGABLE_MESSAGE_LOCATEREQUEST = 3,          // by client.
+  TAO_PLUGGABLE_MESSAGE_LOCATEREPLY = 4,
+  TAO_PLUGGABLE_MESSAGE_CLOSECONNECTION = 5,        
+  TAO_PLUGGABLE_MESSAGE_MESSAGERROR = 6,         
+  TAO_PLUGGABLE_MESSAGE_FRAGMENT = 7             
+}TAO_Pluggable_Message_Type;
+
+typedef enum Exception_Types
+{
+  // = DESCRIPTION
+  //   Provide an external interface for the users of this pluggable
+  //   messaging framework to denote  existing Exception types. This has
+  //   an inspiration from GIOP. So if anybody wants to add more message
+  //   types you are welcome but please do not change the numbering
+  //   scheme as this would affect GIOP. 
+  TAO_PLUGGABLE_MESSAGE_NO_EXCEPTION = 0,
+  // Request completed successfully
+
+  TAO_PLUGGABLE_MESSAGE_USER_EXCEPTION,
+  // Request terminated with user exception
+
+  TAO_PLUGGABLE_MESSAGE_SYSTEM_EXCEPTION,
+  // Request terminated with system exception
+
+  TAO_PLUGGABLE_MESSAGE_LOCATION_FORWARD,
+  // Reply is a location forward type
+  
+  TAO_PLUGGABLE_MESSAGE_LOCATION_FORWARD_PERM,
+  // PLUGGABLE_MESSAGE 1.2, Reply is a location forward perm type..
+
+  TAO_PLUGABLE_MESSAGE_NEEDS_ADDRESSING_MODE
+  // GIOP1.2, 
+}TAO_Pluggable_Message_Exception_Type;
+
+#if defined (__ACE_INLINE__)
+#include "tao/Pluggable_Messaging_Utils.i"
+#endif /* __ACE_INLINE__ */
+
+#endif /*_TAO_PLUGGABLE_MESSAGING_UTILS_H_ */
