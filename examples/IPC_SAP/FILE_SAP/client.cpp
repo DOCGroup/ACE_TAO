9d25a9cab3af5b7f3a2974aa1ef1c07a72cb5ac7
schmidt <douglascraigschmidt@users.noreply.github.com>
2000-03-24 17:05:44 +0000

    ChangeLogTag:Fri Mar 24 10:30:44 2000  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- examples/IPC_SAP/FILE_SAP/client.cpp
+++ examples/IPC_SAP/FILE_SAP/client.cpp
@@ -11,7 +11,10 @@ int
 main (int argc, char *argv[])
 {
   if (argc < 3 || argc > 3)
-    ACE_ERROR_RETURN ((LM_ERROR, "usage: %s filename string\n", argv[0]), 1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "usage: %s filename string\n",
+                       argv[0]),
+                      1);
 
   char *readback = new char[::strlen (argv[1]) + 1];
 
@@ -19,18 +22,30 @@ main (int argc, char *argv[])
   ACE_FILE_IO cli_file;
   ACE_FILE_Connector con;
 
-  if (con.connect (cli_file, ACE_FILE_Addr (argv[1]), 
-		   0, ACE_Addr::sap_any, 0,
+  if (con.connect (cli_file,
+                   ACE_FILE_Addr (argv[1]), 
+		   0,
+                   ACE_Addr::sap_any, 0,
 		   O_RDWR|O_APPEND|O_CREAT, 0666) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n to %s", "connect", argv[1]), -1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n to %s",
+                       "connect",
+                       argv[1]),
+                      -1);
 
   ssize_t len = ACE_OS::strlen (argv[2]) + 1;
 
   if (cli_file.send (argv[2], len) != len)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "send"), 1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "send"),
+                      1);
 
   if (cli_file.get_info (&fileinfo) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "get_info"), 1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "get_info"),
+                      1);
   else
     ACE_OS::printf ("fileinfo : mode = %o\nno of links = %lu\nsize = %lu\n",
                     fileinfo.mode_ & 0777,
@@ -40,20 +55,33 @@ main (int argc, char *argv[])
   off_t fpos = cli_file.position ();
 
   if (fpos == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "position"), 1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "position"),
+                      1);
   else
-    ACE_OS::printf ("current filepointer is at %ld\n", fpos);
+    ACE_OS::printf ("current filepointer is at %ld\n",
+                    fpos);
     
-  if (cli_file.position (0, SEEK_SET) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "position"), 1);
-
+  if (cli_file.position (0,
+                         SEEK_SET) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "position"),
+                      1);
   if (cli_file.recv (readback, len) != len)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "recv"), 1);
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "recv"),
+                      1);
 
-  ACE_OS::printf ("read back :%s\n", readback);
+  ACE_OS::printf ("read back :%s\n",
+                  readback);
 
   if (cli_file.close () == -1)
-    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "close"), 1);
-
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "%p\n",
+                       "close"),
+                      1);
   return 0;
 }
