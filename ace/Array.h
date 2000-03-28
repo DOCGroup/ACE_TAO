258c8e906f3e218dfab21eb4e551ed54ac4277dc
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-03-28 18:10:53 +0000

    ChangeLogTag:Tue Mar 28 12:06:31 2000  Nanbor Wang  <nanbor@cs.wustl.edu>

--- ace/Array.h
+++ ace/Array.h
@@ -11,7 +11,7 @@
 //
 // = DESCRIPTION
 //    NOTE: this file has been deprecated and will soon go away.  You
-//    should directly include "Containers.h" instead.
+//    should directly include "Containers_T.h" instead.
 //
 // = AUTHOR
 //    Doug Schmidt
@@ -22,23 +22,11 @@
 #define ACE_ARRAY_H
 #include "ace/pre.h"
 
-#include "ace/Containers.h"
+#include "ace/Containers_T.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
-
-#if defined (__ACE_INLINE__)
-#include "ace/Containers.i"
-#endif /* __ACE_INLINE__ */
-
-#if defined (ACE_TEMPLATES_REQUIRE_SOURCE)
-#include "ace/Containers.cpp"
-#endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
-
-#if defined (ACE_TEMPLATES_REQUIRE_PRAGMA)
-#pragma implementation ("Containers.cpp")
-#endif /* ACE_TEMPLATES_REQUIRE_PRAGMA */
 
 #include "ace/post.h"
 #endif /* ACE_ARRAY_H */
