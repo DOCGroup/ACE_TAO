99d1356e9099a86be66da1a46629e5d380d5c835
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-03-30 18:31:24 +0000

    Thu Mar 30 12:16:25 2000  Pradeep Gore  <pradeep@flamenco.cs.wustl.edu>

--- TAO/orbsvcs/Notify_Service/Notify_Service.cpp
+++ TAO/orbsvcs/Notify_Service/Notify_Service.cpp
@@ -1,18 +1,25 @@
 // $Id$
 
-#include "Notify_Service.h"
-#include "orbsvcs/Notify/Notify_EventChannelFactory_i.h"
+#include "ace/Arg_Shifter.h"
 #include "ace/Get_Opt.h"
+#include "orbsvcs/Notify/Notify_EventChannelFactory_i.h"
+#include "Notify_Service.h"
 
 Notify_Service::Notify_Service (void)
-  : notify_factory_name_ ("NotifyEventChannelFactory")
+  : bootstrap_ (0),
+    use_name_svc_ (1),
+    ior_output_file_ (0),
+    notify_factory_name_ (NOTIFY_KEY),
+    notify_channel_name_ (NOTIFY_CHANNEL_NAME),
+    register_event_channel_ (0)
 {
   // No-Op.
 }
 
 Notify_Service::~Notify_Service (void)
 {
-  // No-Op.
+  if (ior_output_file_)
+    fclose(ior_output_file_);
 }
 
 int
@@ -54,9 +61,6 @@ int
 Notify_Service::startup (int argc, char *argv[],
                           CORBA::Environment &ACE_TRY_ENV)
 {
-  ACE_DEBUG ((LM_DEBUG,
-              "\nStarting up the Notification Service...\n"));
-
   // initalize the ORB.
   if (this->init_ORB (argc, argv,
                       ACE_TRY_ENV) != 0)
@@ -64,11 +68,11 @@ Notify_Service::startup (int argc, char *argv[],
 
   ACE_CHECK_RETURN (-1);
 
-  // Resolve the naming service.
-  if (this->resolve_naming_service (ACE_TRY_ENV) != 0)
+  if (this->parse_args(argc, argv) != 0)
     return -1;
 
-  ACE_CHECK_RETURN (-1);
+  ACE_DEBUG ((LM_DEBUG,
+              "\nStarting up the Notification Service...\n"));
 
   // Activate the factory
   this->notify_factory_ =
@@ -78,28 +82,85 @@ Notify_Service::startup (int argc, char *argv[],
 
   ACE_ASSERT (!CORBA::is_nil (this->notify_factory_.in ()));
 
+  // Write IOR to a file, if asked.
   CORBA::String_var str =
     this->orb_->object_to_string (this->notify_factory_.in (), ACE_TRY_ENV);
+  if (this->ior_output_file_)
+    {
+      ACE_OS::fprintf (this->ior_output_file_,
+                       "%s",
+                       str.in ());
+      ACE_OS::fclose (this->ior_output_file_);
+      this->ior_output_file_ = 0;
+  }
+  else
+  {
+    ACE_DEBUG ((LM_DEBUG,
+                "The Notification Event Channel Factory IOR is <%s>\n",
+                str.in ()));
+  }
 
-  ACE_DEBUG ((LM_DEBUG,
-              "The Notification Event Channel Factory IOR is <%s>\n",
-              str.in ()));
+  // Make it bootstrappable, if asked.
+  if (this->bootstrap_)
+    {
+    // add to Object key-IOR table
+      if (this->orb_->_tao_add_to_IOR_table (this->notify_factory_name_.c_str(),
+                                           this->notify_factory_.in ()) != 0)
+      ACE_ERROR_RETURN((LM_ERROR,
+                        "Unable to add IOR to table\n"), -1);
+  }
 
-  // Register the Factory
-  ACE_ASSERT(!CORBA::is_nil (this->naming_.in ()));
+  // Register with the Name service, if asked
+  if (this->use_name_svc_)
+  {
+      // Resolve the naming service.
+      this->resolve_naming_service (ACE_TRY_ENV);
+      ACE_CHECK_RETURN (-1);
 
-  CosNaming::Name name (1);
-  name.length (1);
-  name[0].id = CORBA::string_dup (this->notify_factory_name_);
+      // Register the Factory
+      ACE_ASSERT(!CORBA::is_nil (this->naming_.in ()));
 
-  this->naming_->rebind (name,
-                         this->notify_factory_.in (),
-                         ACE_TRY_ENV);
-  ACE_CHECK_RETURN (-1);
+      CosNaming::Name name (1);
+      name.length (1);
+      name[0].id =
+          CORBA::string_dup (this->notify_factory_name_.c_str ());
 
-  ACE_DEBUG ((LM_DEBUG,
-              "Registered with the naming service as: %s\n",
-              this->notify_factory_name_));
+      this->naming_->rebind (name,
+                             this->notify_factory_.in (),
+                             ACE_TRY_ENV);
+      ACE_CHECK_RETURN (-1);
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "Registered with the naming service as: %s\n",
+                  this->notify_factory_name_.c_str()));
+
+      if (this->register_event_channel_ == 1)
+        {
+          // create an event channel
+          CosNotifyChannelAdmin::ChannelID id;
+
+          CosNotification::QoSProperties initial_qos;
+          CosNotification::AdminProperties initial_admin;
+
+          CosNotifyChannelAdmin::EventChannel_var ec =
+            this->notify_factory_->create_channel (initial_qos,
+                                                   initial_admin,
+                                                   id,
+                                                   ACE_TRY_ENV);
+          name[0].id =
+            CORBA::string_dup (this->notify_channel_name_.c_str ());
+
+          this->naming_->rebind (name,
+                                 ec.in (),
+                                 ACE_TRY_ENV);
+          ACE_CHECK_RETURN (-1);
+
+          ACE_DEBUG ((LM_DEBUG,
+                      "Registered an Event Channel with the naming service as: %s\n",
+                      this->notify_channel_name_.c_str()));
+
+        }
+  }
 
   return 0;
 }
@@ -151,17 +212,99 @@ Notify_Service::shutdown (CORBA::Environment &ACE_TRY_ENV)
                                  ACE_TRY_ENV);
   ACE_CHECK;
 
-  // Unbind from the naming service.
-  CosNaming::Name name (1);
-  name.length (1);
-  name[0].id = CORBA::string_dup (this->notify_factory_name_);
+  if (this->use_name_svc_)
+  {
+    // Unbind from the naming service.
+    CosNaming::Name name (1);
+    name.length (1);
+    name[0].id = CORBA::string_dup (this->notify_factory_name_.c_str ());
 
-  this->naming_->unbind (name,
-                         ACE_TRY_ENV);
+    this->naming_->unbind (name,
+                           ACE_TRY_ENV);
+  }
 
   // shutdown the ORB.
   if (!CORBA::is_nil (this->orb_.in ()))
     this->orb_->shutdown ();
+}
+
+
+int
+Notify_Service::parse_args(int argc, char *argv[])
+{
+    ACE_Arg_Shifter arg_shifter (argc, argv);
+
+    char *current_arg = 0;
+    while (arg_shifter.is_anything_left ())
+    {
+      if ((current_arg = arg_shifter.get_the_parameter ("-Factory")))
+        {
+          this->notify_factory_name_.set(current_arg);
+          arg_shifter.consume_arg ();
+        }
+      else if (arg_shifter.cur_arg_strncasecmp ("-Boot") == 0)
+        {
+          this->bootstrap_ = 1;
+          // Make it bootstrappable
+
+          arg_shifter.consume_arg ();
+        }
+      else if (arg_shifter.cur_arg_strncasecmp ("-NameSvc") == 0)
+        {
+          this->use_name_svc_ = 1;
+          // Register ref with the name service
+
+          arg_shifter.consume_arg ();
+        }
+      else if (arg_shifter.cur_arg_strncasecmp ("-NoNameSvc") == 0)
+        {
+          this->use_name_svc_ = 0;
+          // Don't Register ref with the name service
+
+          arg_shifter.consume_arg ();
+        }
+      else if ((current_arg = arg_shifter.get_the_parameter ("-IORoutput")))
+        {
+          this->ior_output_file_ = ACE_OS::fopen (current_arg, "w");
+          if (this->ior_output_file_ == 0)
+            ACE_ERROR_RETURN ((LM_ERROR,
+                               "Unable to open %s for writing: %p\n",
+                               current_arg), -1);
+          arg_shifter.consume_arg ();
+        }
+      else if ((current_arg = arg_shifter.get_the_parameter ("-ChannelName")))
+        {
+          this->notify_channel_name_.set(current_arg);
+          arg_shifter.consume_arg ();
+        }
+      else if (arg_shifter.cur_arg_strncasecmp ("-Channel") == 0)
+        {
+          this->register_event_channel_ = 1;
+          // Register one EC with naming.
+
+          arg_shifter.consume_arg ();
+        }
+      else if (arg_shifter.cur_arg_strncasecmp ("-?") == 0)
+        {
+          ACE_DEBUG((LM_DEBUG,
+                     "usage: %s -Factory factory_name "
+                     "-Boot -[No]NameSvc "
+                     "-IORoutput file_name "
+                     "-Channel -ChannelName channel_name\n"
+                     "default: %s -Factory NotifyEventChannelFactory "
+                     "-NameSvc -Channel NotifyEventChannel\n",
+                     argv[0], argv[0]));
+
+          arg_shifter.consume_arg ();
+
+          return -1;
+        }
+      else
+        {
+            arg_shifter.ignore_arg ();
+        }
+    }
+    return 0;
 }
 
 int
