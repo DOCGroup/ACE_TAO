292248bc6095e3b03e6a4d35b899e4de0b29b555
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-04-18 01:10:25 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_1_1'.

--- TAO/tao/GIOP_Message_Headers.cpp
+++
@@ -1,9 +0,0 @@
-//$Id$
-#include "tao/GIOP_Message_Headers.h"
-
-#if !defined (__ACE_INLINE__)
-# include "tao/GIOP_Message_Headers.i"
-#endif /* __ACE_INLINE__ */
-
-// @@ Bala: the RCSID macro is missing, please check all your files
-// @@ and make sure they conform to the ACE guidelines.
--- TAO/tao/Pluggable_Messaging_Utils.cpp
+++
@@ -1,8 +0,0 @@
-//$Id$
-#include "tao/Pluggable_Messaging_Utils.h"
-
-#if !defined (__ACE_INLINE__)
-#include "tao/Pluggable_Messaging_Utils.i"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID(tao, Pluggable_Messaging_Utils, "$Id$")
