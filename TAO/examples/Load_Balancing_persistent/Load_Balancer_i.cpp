45ee26d4f8f3b86752943296b7c1a3c883e5781c
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-04-20 16:49:31 +0000

    *** empty log message ***

--- TAO/examples/Load_Balancing_persistent/Load_Balancer_i.cpp
+++ TAO/examples/Load_Balancing_persistent/Load_Balancer_i.cpp
@@ -1008,9 +1008,9 @@ RR_Object_Group::unbind (const char *id,
 
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template class ACE_Allocator_Adapter<ACE_Malloc<ACE_MMAP_Memory_Pool, ACE_Thread_Mutex> >;
-template class ACE_Malloc<ACE_MMAP_Memory_Pool, ACE_Thread_Mutex>;
-template class ACE_Malloc_T<ACE_MMAP_Memory_Pool, ACE_Thread_Mutex, ACE_Control_Block>;
+template class ACE_Allocator_Adapter<ACE_Malloc<ACE_MMAP_MEMORY_POOL, ACE_SYNCH_MUTEX> >;
+template class ACE_Malloc<ACE_MMAP_MEMORY_POOL, ACE_SYNCH_MUTEX>;
+template class ACE_Malloc_T<ACE_MMAP_MEMORY_POOL, ACE_Thread_Mutex, ACE_Control_Block>;
 template class ACE_Hash_Map_With_Allocator<char *, char *>;
 template class ACE_Hash_Map_Iterator_Base_Ex<char *,  Load_Balancer::Object_Group_var, ACE_Hash<char *>, ACE_Equal_To<char *>, ACE_Null_Mutex>;
 template class ACE_Hash_Map_Iterator<char *, char *, ACE_Null_Mutex>;
@@ -1038,9 +1038,9 @@ template class ACE_DLList_Iterator<char *>;
 
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#pragma instantiate  ACE_Allocator_Adapter<ACE_Malloc<ACE_MMAP_Memory_Pool, ACE_Thread_Mutex> >
-#pragma instantaite  ACE_Malloc<ACE_MMAP_Memory_Pool, ACE_Thread_Mutex>
-#pragma instantaite  ACE_Malloc_T<ACE_MMAP_Memory_Pool, ACE_Thread_Mutex, ACE_Control_Block>
+#pragma instantiate  ACE_Allocator_Adapter<ACE_Malloc<ACE_MMAP_MEMORY_POOL, ACE_Thread_Mutex> >
+#pragma instantaite  ACE_Malloc<ACE_MMAP_MEMORY_POOL, ACE_Thread_Mutex>
+#pragma instantaite  ACE_Malloc_T<ACE_MMAP_MEMORY_POOL, ACE_Thread_Mutex, ACE_Control_Block>
 #pragma instantiate  ACE_Hash_Map_With_Allocator<char *, char *>
 #pragma instantiate  ACE_Hash_Map_Iterator_Base_Ex<char *, char *, ACE_Hash<char *>, ACE_Equal_To<char *>, ACE_Null_Mutex>
 #pragma instantiate  ACE_Hash_Map_Iterator<char *, char *, ACE_Null_Mutex>
