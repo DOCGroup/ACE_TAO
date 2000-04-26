8f47bf96f4b4a3874b7184f2caef40dab5a7aae5
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-04-26 22:45:46 +0000

    ChangeLogTag:Wed Apr 26 15:41:23 2000  Carlos O'Ryan  <coryan@uci.edu>

--- trunk/TAO/tests/Param_Test/ub_wstr_seq.cpp
+++ trunk/TAO/tests/Param_Test/ub_wstr_seq.cpp
@@ -152,9 +152,9 @@ Test_WString_Sequence::check_validity (void)
       // lengths are same. Now compare the contents
       for (CORBA::ULong i=0; i < this->in_->length () && flag; i++)
         {
-          if (ACE_OS::strcmp (this->in_[i], this->inout_[i]) ||
-              ACE_OS::strcmp (this->in_[i], this->out_[i]) ||
-              ACE_OS::strcmp (this->in_[i], this->ret_[i]))
+          if (ACE_OS::wscmp (this->in_[i], this->inout_[i]) ||
+              ACE_OS::wscmp (this->in_[i], this->out_[i]) ||
+              ACE_OS::wscmp (this->in_[i], this->ret_[i]))
             // not equal
             flag = 0;
         }
