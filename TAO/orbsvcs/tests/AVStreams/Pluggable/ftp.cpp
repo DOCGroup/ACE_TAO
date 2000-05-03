0bfa2ab3a57f69e4612aa6d487733a5652871a7a
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-03 22:38:45 +0000

    Removed the reference to ::feof

--- TAO/orbsvcs/tests/AVStreams/Pluggable/ftp.cpp
+++ TAO/orbsvcs/tests/AVStreams/Pluggable/ftp.cpp
@@ -1,6 +1,19 @@
 // $Id$
 
 #include "ftp.h"
+#include "ace/Get_Opt.h"
+#include "ace/High_Res_Timer.h"
+#include "ace/Stats.h"
+
+
+ACE_hrtime_t recv_throughput_base = 0;
+ACE_Throughput_Stats recv_latency;
+
+ACE_hrtime_t send_throughput_base = 0;
+ACE_Throughput_Stats send_latency;
+
+int count =0;
+int message_size = 64;
 
 FTP_Client_Callback::FTP_Client_Callback (void)
   :count_ (0)
@@ -30,11 +43,30 @@ FTP_Client_Callback::handle_timeout (void *)
 {
   //@@coryan: Use a preallocated buffer for benchmarks.
   //Memory map the file.
-  ACE_Message_Block mb (BUFSIZ);
+  count++;
+  ACE_Message_Block mb;
+  mb.size (message_size);
+  mb.wr_ptr (message_size);
+  if (count == 10)
+    {
+      AVStreams::flowSpec stop_spec (1);
+      ACE_DECLARE_NEW_CORBA_ENV;
+      CLIENT::instance ()->streamctrl ()->stop (stop_spec,ACE_TRY_ENV);
+      ACE_CHECK_RETURN (-1);
+      CLIENT::instance ()->streamctrl ()->destroy (stop_spec,ACE_TRY_ENV);
+      TAO_AV_CORE::instance ()->stop_run ();
+      return 0;
+    }
+  ACE_hrtime_t stamp = ACE_OS::gethrtime ();
+  //  ACE_Message_Block mb (BUFSIZ);
+  ACE_OS::memcpy (mb.rd_ptr (), &stamp, sizeof(stamp));
+  //  ACE_OS::memcpy (this->frame_.rd_ptr (), &stamp, sizeof(stamp));
   ACE_DEBUG ((LM_DEBUG,"FTP_Client_Callback::get_frame"));
-  char *buf = mb.rd_ptr ();
-  cerr << "message block size" << mb.size () << endl;
-  int n = ACE_OS::fread(buf,1,mb.size (),CLIENT::instance ()->file ());
+  //char *buf = mb.rd_ptr ();
+  //cerr << "message block size" << mb.size () << endl;
+  //  int n = ACE_OS::fread(buf,1,mb.size (),CLIENT::instance ()->file ());
+  //int n = ACE_OS::fread(buf,1,mb.size (),stamp);
+  /*
   if (n < 0)
     {
       ACE_ERROR_RETURN ((LM_ERROR,"FTP_Client_Flow_Handler::fread end of file\n"),-1);
@@ -55,6 +87,8 @@ FTP_Client_Callback::handle_timeout (void *)
               CLIENT::instance ()->streamctrl ()->stop (stop_spec,ACE_TRY_ENV);
               ACE_CHECK_RETURN (-1);
               CLIENT::instance ()->streamctrl ()->destroy (stop_spec,ACE_TRY_ENV);
+              TAO_AV_CORE::instance ()->stop_run ();
+              
             }
           else
             return 0;
@@ -63,7 +97,8 @@ FTP_Client_Callback::handle_timeout (void *)
         ACE_ERROR_RETURN ((LM_ERROR,"FTP_Client_Flow_Handler::fread error\n"),-1);
     }
   cerr << "read bytes = " << n << endl;
-  mb.wr_ptr (n);
+  */
+  //mb.wr_ptr (n);
   int result = this->protocol_object_->send_frame (&mb);
   if (result < 0)
     ACE_ERROR_RETURN ((LM_ERROR,"send failed:%p","FTP_Client_Flow_Handler::send \n"),-1);
@@ -143,6 +178,7 @@ Client::parse_args (int argc,
           break;
         case 't':
           this->test_ = 1;
+          break;
         default:
           ACE_DEBUG ((LM_DEBUG,"Unknown option\n"));
           return -1;
@@ -246,6 +282,8 @@ Client::init (int argc,char **argv)
                                           ACE_TRY_ENV);
       ACE_TRY_CHECK;
       this->parse_args (argc, argv);
+
+      this->orb_manager_->activate_poa_manager (ACE_TRY_ENV);
       // activate the client video mmdevice under the child poa.
       ior = this->orb_manager_->activate (&this->client_mmdevice_,
                                           ACE_TRY_ENV);
@@ -264,6 +302,8 @@ Client::init (int argc,char **argv)
         {
           ACE_DEBUG ((LM_DEBUG,"file opened successfully\n"));
         }
+      
+      
       if (this->bind_to_server () == -1)
         ACE_ERROR_RETURN ((LM_ERROR, 
                            "(%P|%t) Error binding to the naming service\n"),
@@ -291,10 +331,7 @@ Client::run (void)
       else
         ACE_OS::strcpy (flow_protocol_str,"");
       AVStreams::streamQoS_var the_qos (new AVStreams::streamQoS);
-      AVStreams::flowSpec flow_spec (1);
-      // Bind the client and server mmdevices.
 
-      this->orb_manager_->activate_poa_manager (ACE_TRY_ENV);
       ACE_TRY_CHECK;
       ACE_INET_Addr addr (this->address_);
       ACE_NEW_RETURN (this->flowname_,
@@ -309,20 +346,23 @@ Client::run (void)
                                         flow_protocol_str,
                                         this->protocol_,
                                         &addr);
-      flow_spec [0] = entry.entry_to_string ();
+
+      AVStreams::flowSpec flow_spec (1);
+      flow_spec [0] = CORBA::string_dup (entry.entry_to_string ());
       flow_spec.length (1);
-      ACE_High_Res_Timer timer;
-      ACE_Time_Value elapsed;
-      timer.start ();
+      //ACE_High_Res_Timer timer;
+      //ACE_Time_Value elapsed;
+      // timer.start ();
       CORBA::Boolean result = 
         this->streamctrl_.bind_devs (this->client_mmdevice_._this (ACE_TRY_ENV),
                                      this->server_mmdevice_.in (),
                                      the_qos.inout (),
                                      flow_spec,
                                      ACE_TRY_ENV);
-      timer.stop ();
-      timer.elapsed_time (elapsed);
-      elapsed.dump ();
+      //timer.stop ();
+      // timer.elapsed_time (elapsed);
+      //elapsed.dump ();
+
       // If we're supposed to do only bind_devs time calculation return.
       if (this->test_)
         {
@@ -334,12 +374,13 @@ Client::run (void)
       ACE_TRY_CHECK;
       if (result == 0)
         ACE_ERROR_RETURN ((LM_ERROR,"streamctrl::bind_devs failed\n"),-1);
+
       AVStreams::flowSpec start_spec (1);
-      start_spec.length (1);
-      start_spec [0] = CORBA::string_dup (this->flowname_);
+      //start_spec.length (1);
+      //start_spec [0] = CORBA::string_dup (this->flowname_);
       this->streamctrl_.start (start_spec,ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      // Schedule a timer for the for the flow handler.
+
       TAO_AV_CORE::instance ()->run ();
     }
   ACE_CATCHANY
@@ -356,6 +397,7 @@ int
 main (int argc,
       char **argv)
 {
+  
   int result = 0;
   result = CLIENT::instance ()->init (argc,argv);
   if (result < 0)
@@ -363,6 +405,13 @@ main (int argc,
   result = CLIENT::instance ()->run ();
   if (result < 0)
     ACE_ERROR_RETURN ((LM_ERROR,"client::run failed\n"),1);
+  ACE_DEBUG ((LM_DEBUG, "Calibrating scale factory . . . "));
+  ACE_UINT32 gsf = ACE_High_Res_Timer::global_scale_factor ();
+  ACE_DEBUG ((LM_DEBUG, "done\n"));
+  
+  recv_latency.dump_results ("Receive", gsf);
+  
+  send_latency.dump_results ("Send", gsf);
 }
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
