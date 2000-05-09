02b596c6b234a727780385ac10f9b3c81d213ffe
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-09 03:56:35 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_1_2'.

---
+++ tags/ACE-5_1_2/apps/drwho/SMR_Server.cpp
@@ -0,0 +1,17 @@
+// $Id$
+
+#include "Options.h"
+#include "SMR_Server.h"
+
+SMR_Server::SMR_Server (short port_number)
+{
+  if (CM_Server::open (port_number) < 0)
+    ACE_ERROR ((LM_ERROR,
+                "%p\n%a",
+                Options::program_name,
+                1));
+}
+
+SMR_Server::~SMR_Server (void)
+{
+}
