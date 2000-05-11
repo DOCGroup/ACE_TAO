e5ac0f1e977eec3af5f547b527a1fb1bbedb4987
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-11 19:30:52 +0000

    ChangeLogTag:Thu May 11 12:27:35 2000  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/docs/tutorials/Quoter/Simple/Persistent/server.cpp
+++ TAO/docs/tutorials/Quoter/Simple/Persistent/server.cpp
@@ -2,9 +2,9 @@
 
 //===========================================================================
 //
-// = FILENAME 
+// = FILENAME
 //     server.cpp
-// 
+//
 // = DESCRIPTION
 //
 //     In this example, a new POA (childPOA) is created and its
@@ -18,51 +18,51 @@
 #include "Stock_Factory_i.h"
 #include <iostream.h>
 
-int 
+int
 main (int argc, char *argv[])
 {
   try {
-    
+
     // Initialze the ORB.
     CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
-    
+
     // Get a reference to the RootPOA.
     CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
-    
+
     // Get the POA_var object from Object_var.
-    PortableServer::POA_var root_poa = 
+    PortableServer::POA_var root_poa =
       PortableServer::POA::_narrow (obj.in ());
-    
+
     // Get the POAManager of the RootPOA.
-    PortableServer::POAManager_var poa_manager = 
+    PortableServer::POAManager_var poa_manager =
       root_poa->the_POAManager ();
-    
+
     poa_manager->activate ();
-    
+
     // Create a USER_ID IdAssignmentpolicy object.
-    PortableServer::IdAssignmentPolicy_var idassignment = 
+    PortableServer::IdAssignmentPolicy_var idassignment =
       root_poa->create_id_assignment_policy (PortableServer::USER_ID);
-    
+
     // Create a PERSISTENT LifespanPolicy object.
-    PortableServer::LifespanPolicy_var lifespan = 
+    PortableServer::LifespanPolicy_var lifespan =
       root_poa->create_lifespan_policy (PortableServer::PERSISTENT);
 
     // Policies for the childPOA to be created.
     CORBA::PolicyList policies;
     policies.length (2);
-    
-    policies[0] = 
+
+    policies[0] =
       PortableServer::IdAssignmentPolicy::_duplicate (idassignment);
-    
-    policies[1] = 
+
+    policies[1] =
       PortableServer::LifespanPolicy::_duplicate (lifespan);
-					     
+
     // Create the childPOA under the RootPOA.
-    PortableServer::POA_var child_poa = 
-      root_poa->create_POA ("childPOA", 
+    PortableServer::POA_var child_poa =
+      root_poa->create_POA ("childPOA",
                             poa_manager.in (),
                             policies);
-    
+
     // Destroy policy objects.
     idassignment->destroy ();
     lifespan->destroy ();
@@ -75,20 +75,20 @@ main (int argc, char *argv[])
       PortableServer::string_to_ObjectId ("Stock_Factory");
 
     // Activate the Stock_Factory object.
-    child_poa->activate_object_with_id (oid.in (), 
+    child_poa->activate_object_with_id (oid.in (),
 					&stock_factory_i);
 
     // Get the object reference.
     CORBA::Object_var stock_factory =
       child_poa->id_to_reference (oid.in ());
-    
+
     // Stringify all the object referencs.
     CORBA::String_var ior = orb->object_to_string (stock_factory.in ());
     // Print them out !
     std::cout << ior.in () << std::endl;
 
     orb-> run ();
-    
+
     // Destroy POA, waiting until the destruction terminates.
     root_poa->destroy (1, 1);
     orb->destroy ();
