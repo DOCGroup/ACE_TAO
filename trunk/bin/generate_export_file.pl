247e8f7a57fa1e6ce90177ad0c3426ae49306f8e
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-13 00:51:23 +0000

    ChangeLogTag:Fri May 12 17:48:17 2000  Darrell Brunsch <brunsch@uci.edu>

--- trunk/bin/generate_export_file.pl
+++ trunk/bin/generate_export_file.pl
@@ -12,43 +12,50 @@ die "Error: Missing command line argument" if ($#ARGV < 0);
 $name = $ARGV[0];
 $ucname = uc $name;
 
-print "// -*- C++ -*-\n";
-print "// \$"."Id\$\n";
-print "// Definition for Win32 Export directives.\n";
-print "// This file is generated automatically by\n";
-print "// generate_export_file.pl\n";
-print "// ------------------------------\n";
-print "#if !defined ($ucname"."_EXPORT_H)\n";
-print "#define $ucname"."_EXPORT_H\n";
-print "\n";
-print "#include \"ace/config-all.h\"\n";
-print "\n";
-print "#if !defined ($ucname"."_HAS_DLL)\n";
-print "#define $ucname"."_HAS_DLL 1\n";
-print "#endif /* ! $ucname"."_HAS_DLL */\n";
-print "\n";
-print "#if defined ($ucname"."_HAS_DLL)\n";
-print "#  if ($ucname"."_HAS_DLL == 1)\n";
-print "#    if defined ($ucname"."_BUILD_DLL)\n";
-print "#      define $name"."_Export ACE_Proper_Export_Flag\n";
-print "#      define $ucname"."_SINGLETON_DECLARATION(T) ACE_EXPORT_SINGLETON_DECLARATION (T)\n";
-print "#      define $ucname"."_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)\n";
-print "#    else\n";
-print "#      define $name"."_Export ACE_Proper_Import_Flag\n";
-print "#      define $ucname"."_SINGLETON_DECLARATION(T) ACE_IMPORT_SINGLETON_DECLARATION (T)\n";
-print "#      define $ucname"."_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)\n";
-print "#    endif /* $ucname"."_BUILD_DLL */\n";
-print "#  else\n";
-print "#    define $name"."_Export\n";
-print "#    define $ucname"."_SINGLETON_DECLARATION(T)\n";
-print "#    define $ucname"."_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)\n";
-print "#  endif   /* ! $ucname"."_HAS_DLL == 1 */\n";
-print "#else\n";
-print "#  define $name"."_Export\n";
-print "#  define $ucname"."_SINGLETON_DECLARATION(T)\n";
-print "#  define $ucname"."_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)\n";
-print "#endif     /* $ucname"."_HAS_DLL */\n";
-print "\n";
-print "#endif     /* $ucname"."_EXPORT_H */\n";
-print "\n";
-print "// End of auto generated file.\n";
+## -NC- stands for normal case, the name as it is
+## -UC- stands for the name all upper case
+map { s/-NC-/$name/g; s/-UC-/$ucname/g; } @data=<DATA>;
+
+print @data;
+
+__DATA__
+// -*- C++ -*-
+// $Id$
+// Definition for Win32 Export directives.
+// This file is generated automatically by
+// generate_export_file.pl
+// ------------------------------
+#if !defined (-UC-_EXPORT_H)
+#define -UC-_EXPORT_H
+
+#include "ace/config-all.h"
+
+#if !defined (-UC-_HAS_DLL)
+#define -UC-_HAS_DLL 1
+#endif /* ! -UC-_HAS_DLL */
+
+#if defined (-UC-_HAS_DLL)
+#  if (-UC-_HAS_DLL == 1)
+#    if defined (-UC-_BUILD_DLL)
+#      define -NC-_Export ACE_Proper_Export_Flag
+#      define -UC-_SINGLETON_DECLARATION(T) ACE_EXPORT_SINGLETON_DECLARATION (T)
+#      define -UC-_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#    else
+#      define -NC-_Export ACE_Proper_Import_Flag
+#      define -UC-_SINGLETON_DECLARATION(T) ACE_IMPORT_SINGLETON_DECLARATION (T)
+#      define -UC-_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#    endif /* -UC-_BUILD_DLL */
+#  else
+#    define -NC-_Export
+#    define -UC-_SINGLETON_DECLARATION(T)
+#    define -UC-_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#  endif   /* ! -UC-_HAS_DLL == 1 */
+#else
+#  define -NC-_Export
+#  define -UC-_SINGLETON_DECLARATION(T)
+#  define -UC-_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#endif     /* -UC-_HAS_DLL */
+
+#endif     /* -UC-_EXPORT_H */
+
+// End of auto generated file.
