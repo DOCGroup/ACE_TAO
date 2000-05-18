3f2e6ce4277c18dad4f11effd2d91f5a9bb11aab
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-18 03:10:50 +0000

    ChangeLogTag:Wed May 17 20:02:25 2000  Darrell Brunsch <brunsch@uci.edu>

--- ace/config-all.h
+++ ace/config-all.h
@@ -290,8 +290,204 @@
 # endif /* ! ACE_CAST_CONST */
 
 // ============================================================================
-// Cast macros
+// Compiler Silencing macros
+//
+// Some compilers complain about parameters that are not used.  This macro
+// should keep them quiet.
 // ============================================================================
+
+#if defined (ghs) || defined (__GNUC__) || defined (__hpux) || defined (__sgi) || defined (__DECCXX) || defined (__KCC) || defined (__rational__) || (__USLC__)
+// Some compilers complain about "statement with no effect" with (a).
+// This eliminates the warnings, and no code is generated for the null
+// conditional statement.  NOTE: that may only be true if -O is enabled,
+// such as with GreenHills (ghs) 1.8.8.
+# define ACE_UNUSED_ARG(a) {if (&a) /* null */ ;}
+#else /* ghs || __GNUC__ || ..... */
+# define ACE_UNUSED_ARG(a) (a)
+#endif /* ghs || __GNUC__ || ..... */
+
+#if defined (__sgi) || defined (ghs) || defined (__DECCXX) || defined(__BORLANDC__) || defined (__KCC)
+# define ACE_NOTREACHED(a)
+#else  /* __sgi || ghs || ..... */
+# define ACE_NOTREACHED(a) a
+#endif /* __sgi || ghs || ..... */
+
+// ============================================================================
+// errno stuff
+// ============================================================================
+
+#if defined (ACE_HAS_H_ERRNO)
+void herror (const char *str);
+#endif /* ACE_HAS_H_ERRNO */
+
+#if !defined (ACE_HAS_WINCE)
+# include /**/ <errno.h>
+#endif /* ACE_HAS_WINCE */
+
+#if !defined (ACE_WIN32) && !defined (ACE_PSOS) && defined (ACE_LACKS_T_ERRNO)
+extern int t_errno;
+#endif /* ACE_WIN32 && !ACE_PSOS && ACE_LACKS_T_ERRNO */
+
+// ============================================================================
+// ACE_NEW macros
+//
+// A useful abstraction for expressions involving operator new since
+// we can change memory allocation error handling policies (e.g.,
+// depending on whether ANSI/ISO exception handling semantics are
+// being used).
+// ============================================================================
+
+#if defined (ACE_NEW_THROWS_EXCEPTIONS)
+# if (__SUNPRO_CC)
+#   if (__SUNPRO_CC < 0x500) || ((__SUNPRO_CC == 0x500 && __SUNPRO_CC_COMPAT == 4))
+#        include /**/ <exception.h>
+         // Note: we catch ::xalloc rather than just xalloc because of
+         // a name clash with unsafe_ios::xalloc()
+#        define ACE_bad_alloc ::xalloc
+#        define ACE_throw_bad_alloc throw ACE_bad_alloc ("no more memory")
+#   else
+#        include /**/ <new>
+#        define ACE_bad_alloc std::bad_alloc
+#        define ACE_throw_bad_alloc throw ACE_bad_alloc ()
+#   endif /* __SUNPRO_CC < 0x500 */
+# else
+    // I know this works for HP aC++... if <stdexcept> is used, it
+    // introduces other stuff that breaks things, like <memory>, which
+    // screws up auto_ptr.
+#   include /**/ <new>
+#   if (defined (__HP_aCC) && !defined (RWSTD_NO_NAMESPACE)) \
+       || defined (ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB)
+#     define ACE_bad_alloc std::bad_alloc
+#   else
+#     define ACE_bad_alloc bad_alloc
+#   endif /* RWSTD_NO_NAMESPACE */
+#   define ACE_throw_bad_alloc throw ACE_bad_alloc ()
+# endif /* __SUNPRO_CC */
+
+# define ACE_NEW_RETURN(POINTER,CONSTRUCTOR,RET_VAL) \
+   do { try { POINTER = new CONSTRUCTOR; } \
+        catch (ACE_bad_alloc) { errno = ENOMEM; return RET_VAL; } \
+   } while (0)
+
+# define ACE_NEW(POINTER,CONSTRUCTOR) \
+   do { try { POINTER = new CONSTRUCTOR; } \
+        catch (ACE_bad_alloc) { errno = ENOMEM; return; } \
+   } while (0)
+
+#else /* ACE_NEW_THROWS_EXCEPTIONS */
+
+# define ACE_NEW_RETURN(POINTER,CONSTRUCTOR,RET_VAL) \
+   do { POINTER = new CONSTRUCTOR; \
+     if (POINTER == 0) { errno = ENOMEM; return RET_VAL; } \
+   } while (0)
+# define ACE_NEW(POINTER,CONSTRUCTOR) \
+   do { POINTER = new CONSTRUCTOR; \
+     if (POINTER == 0) { errno = ENOMEM; return; } \
+   } while (0)
+
+# define ACE_throw_bad_alloc \
+  void* gcc_will_complain_if_literal_0_is_returned = 0; \
+  return gcc_will_complain_if_literal_0_is_returned
+
+#endif /* ACE_NEW_THROWS_EXCEPTIONS */
+
+// ============================================================================
+// ACE_OSCALL_* macros
+//
+// The following two macros ensure that system calls are properly
+// restarted (if necessary) when interrupts occur.
+// ============================================================================
+
+#if defined (ACE_HAS_SIGNAL_SAFE_OS_CALLS)
+#   define ACE_OSCALL(OP,TYPE,FAILVALUE,RESULT) \
+  do \
+    RESULT = (TYPE) OP; \
+  while (RESULT == FAILVALUE && errno == EINTR && ACE_LOG_MSG->restart ())
+#   define ACE_OSCALL_RETURN(OP,TYPE,FAILVALUE) \
+  do { \
+    TYPE ace_result_; \
+    do \
+      ace_result_ = (TYPE) OP; \
+    while (ace_result_ == FAILVALUE && errno == EINTR && ACE_LOG_MSG->restart ()); \
+    return ace_result_; \
+  } while (0)
+# elif defined (ACE_WIN32)
+#   define ACE_OSCALL_RETURN(X,TYPE,FAILVALUE) \
+  do \
+    return (TYPE) X; \
+  while (0)
+#   define ACE_OSCALL(X,TYPE,FAILVALUE,RESULT) \
+  do \
+    RESULT = (TYPE) X; \
+  while (0)
+#   if defined (__BORLANDC__) && (__BORLANDC__ <= 0x550)
+#   define ACE_WIN32CALL_RETURN(X,TYPE,FAILVALUE) \
+  do { \
+    TYPE ace_result_; \
+    TYPE ace_local_result_ = (TYPE) X; \
+    ace_result_ = ace_local_result_; \
+    if (ace_result_ == FAILVALUE) \
+      ACE_OS::set_errno_to_last_error (); \
+    return ace_result_; \
+  } while (0)
+#   else
+#     define ACE_WIN32CALL_RETURN(X,TYPE,FAILVALUE) \
+  do { \
+    TYPE ace_result_; \
+    ace_result_ = (TYPE) X; \
+    if (ace_result_ == FAILVALUE) \
+      ACE_OS::set_errno_to_last_error (); \
+    return ace_result_; \
+  } while (0)
+#   endif /* defined (__BORLANDC__) && (__BORLANDC__ <= 0x550) */
+#   define ACE_WIN32CALL(X,TYPE,FAILVALUE,RESULT) \
+  do { \
+    RESULT = (TYPE) X; \
+    if (RESULT == FAILVALUE) \
+      ACE_OS::set_errno_to_last_error (); \
+  } while (0)
+#else /* ACE_HAS_SIGNAL_SAFE_OS_CALLS */
+#   define ACE_OSCALL_RETURN(OP,TYPE,FAILVALUE) do { TYPE ace_result_ = FAILVALUE; ace_result_ = ace_result_; return OP; } while (0)
+#   define ACE_OSCALL(OP,TYPE,FAILVALUE,RESULT) do { RESULT = (TYPE) OP; } while (0)
+#endif /* ACE_HAS_SIGNAL_SAFE_OS_CALLS */
+
+// ============================================================================
+// at_exit declarations
+// ============================================================================
+
+// Marker for cleanup, used by ACE_Exit_Info.
+extern int ace_exit_hook_marker;
+
+// For use by <ACE_OS::exit>.
+extern "C"
+{
+  typedef void (*ACE_EXIT_HOOK) (void);
+}
+
+// ============================================================================
+// Miscellaneous macros
+// ============================================================================
+
+#if !defined (ENOTSUP)
+# define ENOTSUP ENOSYS  /* Operation not supported. */
+#endif /* !ENOTSUP */
+
+// This is used to indicate that a platform doesn't support a
+// particular feature.
+#if defined ACE_HAS_VERBOSE_NOTSUP
+  // Print a console message with the file and line number of the
+  // unsupported function.
+# if defined (ACE_HAS_STANDARD_CPP_LIBRARY) && (ACE_HAS_STANDARD_CPP_LIBRARY != 0)
+#   include /**/ <cstdio>
+# else
+#   include /**/ <stdio.h>
+# endif
+# define ACE_NOTSUP_RETURN(FAILVALUE) do { errno = ENOTSUP; fprintf (stderr, ACE_TEXT ("ACE_NOTSUP: %s, line %d\n"), __FILE__, __LINE__); return FAILVALUE; } while (0)
+# define ACE_NOTSUP do { errno = ENOTSUP; fprintf (stderr, ACE_TEXT ("ACE_NOTSUP: %s, line %d\n"), __FILE__, __LINE__); return; } while (0)
+#else /* ! ACE_HAS_VERBOSE_NOTSUP */
+# define ACE_NOTSUP_RETURN(FAILVALUE) do { errno = ENOTSUP ; return FAILVALUE; } while (0)
+# define ACE_NOTSUP do { errno = ENOTSUP; return; } while (0)
+#endif /* ! ACE_HAS_VERBOSE_NOTSUP */
 
 #include "ace/post.h"
 #endif /* ACE_CONFIG_ALL_H */
