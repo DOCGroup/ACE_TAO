a5641f7b7fbdec80ae6496aa07787952c57cb362
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-22 16:23:35 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_1_3'.

---
+++ tags/ACE-5_1_3/apps/drwho/SMR_Client.cpp
@@ -0,0 +1,21 @@
+// $Id$
+
+#include "Options.h"
+#include "PMC_All.h"
+#include "PMC_Flo.h"
+#include "PMC_Usr.h"
+#include "PMC_Ruser.h"
+#include "SMR_Client.h"
+
+SMR_Client::SMR_Client (short port_number)
+{
+  if (CM_Client::open (port_number) < 0)
+    ACE_ERROR ((LM_ERROR,
+                "%p\n%a", 
+                Options::program_name,
+                1));
+}
+
+SMR_Client::~SMR_Client (void)
+{
+}
