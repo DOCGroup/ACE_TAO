1b28bcb7fd7d7f5c4702b37ec514ce17e89a80ba
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-23 21:23:35 +0000

    Dyanmic files needed for PI

--- TAO/tao/DynamicC.h
+++ TAO/tao/DynamicC.h
@@ -10,7 +10,12 @@
 #ifndef _TAO_IDL_DYNAMICC_H_
 #define _TAO_IDL_DYNAMICC_H_
 
+//Added
+#include "ace/pre.h"
 #include "tao/corbafwd.h"
+#include "tao/IOPC.h"
+#include "tao/DynAnyC.h"
+#include "tao/Typecode.h"
 
 #if defined (ACE_HAS_MINIMUM_IOSTREAMH_INCLUSION)
 #include "ace/streams.h"
@@ -20,7 +25,6 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-//#include "CORBAC.h"
 
 #if defined (TAO_EXPORT_MACRO)
 #undef TAO_EXPORT_MACRO
@@ -43,6 +47,16 @@
 
 TAO_NAMESPACE  Dynamic
 {
+  enum ParameterMode
+  {
+        PARAM_IN,
+        PARAM_OUT,
+        PARAM_INOUT,
+        ParameterMode_TAO_ENUM_32BIT_ENFORCER = 0x7FFFFFFF
+  };
+  typedef ParameterMode &ParameterMode_out;
+  TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_ParameterMode;
+
   struct Parameter;
   class Parameter_var;
   
@@ -56,7 +70,7 @@ TAO_NAMESPACE  Dynamic
     static void _tao_any_destructor (void*);
 
     CORBA::Any argument;
-    CORBA::ParameterMode mode;
+    ACE_NESTED_CLASS (Dynamic, ParameterMode) mode;
   };
 
   class  Parameter_var
@@ -258,10 +272,119 @@ TAO_NAMESPACE  Dynamic
 
   TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_ParameterList;
 
-  typedef CORBA::StringSeq ContextList;
-  typedef CORBA::StringSeq_var ContextList_var;
-  typedef CORBA::StringSeq_out ContextList_out;
-    TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_ContextList;
+
+#if !defined (_DYNAMIC_CONTEXTLIST_CH_)
+#define _DYNAMIC_CONTEXTLIST_CH_
+
+  class ContextList;
+  class ContextList_var;
+  
+  // *************************************************************
+  // ContextList
+  // *************************************************************
+  
+  class  ContextList : public 
+#if !defined (TAO_USE_SEQUENCE_TEMPLATES)
+    TAO_Unbounded_String_Sequence
+#else /* TAO_USE_SEQUENCE_TEMPLATES */
+    TAO_Unbounded_String_Sequence
+#endif /* !TAO_USE_SEQUENCE_TEMPLATES */ 
+  {
+  public:
+    ContextList (void); // default ctor
+    ContextList (CORBA::ULong max); // uses max size
+    ContextList (
+      CORBA::ULong max, 
+      CORBA::ULong length, 
+      char * *buffer, 
+      CORBA::Boolean release=0
+    );
+    ContextList (const ContextList &); // copy ctor
+    ~ContextList (void);
+    static void _tao_any_destructor (void*);
+
+#if !defined(__GNUC__) || !defined (ACE_HAS_GNUG_PRE_2_8)
+    typedef ContextList_var _var_type;
+#endif /* ! __GNUC__ || g++ >= 2.8 */
+
+  };
+  
+#endif /* end #if !defined */
+
+
+#if !defined (_DYNAMIC_CONTEXTLIST___VAR_CH_)
+#define _DYNAMIC_CONTEXTLIST___VAR_CH_
+
+  // *************************************************************
+  // class Dynamic::ContextList_var
+  // *************************************************************
+
+  class  ContextList_var
+  {
+  public:
+    ContextList_var (void); // default constructor
+    ContextList_var (ContextList *);
+    ContextList_var (const ContextList_var &); // copy constructor
+    ~ContextList_var (void); // destructor
+    
+    ContextList_var &operator= (ContextList *);
+    ContextList_var &operator= (const ContextList_var &);
+    ContextList *operator-> (void);
+    const ContextList *operator-> (void) const;
+    
+    operator const ContextList &() const;
+    operator ContextList &();
+    operator ContextList &() const;
+    operator ContextList *&(); // variable-size base types only
+    
+    TAO_SeqElem_String_Manager operator[] (CORBA::ULong index);
+    // in, inout, out, _retn 
+    const ContextList &in (void) const;
+    ContextList &inout (void);
+    ContextList *&out (void);
+    ContextList *_retn (void);
+    ContextList *ptr (void) const;
+
+  private:
+    ContextList *ptr_;
+  };
+
+
+#endif /* end #if !defined */
+
+
+#if !defined (_DYNAMIC_CONTEXTLIST___OUT_CH_)
+#define _DYNAMIC_CONTEXTLIST___OUT_CH_
+
+  class  ContextList_out
+  {
+  public:
+    ContextList_out (ContextList *&);
+    ContextList_out (ContextList_var &);
+    ContextList_out (const ContextList_out &);
+    ContextList_out &operator= (const ContextList_out &);
+    ContextList_out &operator= (ContextList *);
+    operator ContextList *&();
+    ContextList *&ptr (void);
+    ContextList *operator-> (void);
+    TAO_SeqElem_String_Manager operator[] (CORBA::ULong index);
+    
+  private:
+    ContextList *&ptr_;
+    // assignment from T_var not allowed
+    void operator= (const ContextList_var &);
+  };
+
+
+#endif /* end #if !defined */
+
+  TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_ContextList;
+
+  typedef CORBA::TypeCode TypeCode;
+  typedef CORBA::TypeCode_ptr TypeCode_ptr;
+  typedef CORBA::TypeCode_var TypeCode_var;
+  typedef CORBA::TypeCode_out TypeCode_out;
+    TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_TypeCode;
 
 
 #if !defined (TAO_USE_SEQUENCE_TEMPLATES)
@@ -283,7 +406,7 @@ TAO_NAMESPACE  Dynamic
       _TAO_Unbounded_Object_Sequence_Dynamic_ExceptionList(const _TAO_Unbounded_Object_Sequence_Dynamic_ExceptionList &rhs);
       virtual ~_TAO_Unbounded_Object_Sequence_Dynamic_ExceptionList (void);
       _TAO_Unbounded_Object_Sequence_Dynamic_ExceptionList &operator= (const _TAO_Unbounded_Object_Sequence_Dynamic_ExceptionList &rhs);
-      TAO_Pseudo_Object_Manager<CORBA::TypeCode,CORBA::TypeCode_var> operator[] (CORBA::ULong index) const;
+      TAO_Pseudo_Object_Manager<Dynamic::TypeCode,Dynamic::TypeCode_var> operator[] (CORBA::ULong index) const;
       static CORBA::TypeCode **allocbuf (CORBA::ULong nelems);
       static void freebuf (CORBA::TypeCode **buffer);
       // The Base_Sequence functions, please see tao/Sequence.h
@@ -364,7 +487,7 @@ TAO_NAMESPACE  Dynamic
     operator ExceptionList &() const;
     operator ExceptionList *&(); // variable-size base types only
     
-    TAO_Pseudo_Object_Manager<CORBA::TypeCode,CORBA::TypeCode_var> operator[] (CORBA::ULong index);
+    TAO_Pseudo_Object_Manager<TypeCode,TypeCode_var> operator[] (CORBA::ULong index);
     // in, inout, out, _retn 
     const ExceptionList &in (void) const;
     ExceptionList &inout (void);
@@ -394,7 +517,7 @@ TAO_NAMESPACE  Dynamic
     operator ExceptionList *&();
     ExceptionList *&ptr (void);
     ExceptionList *operator-> (void);
-    TAO_Pseudo_Object_Manager<CORBA::TypeCode,CORBA::TypeCode_var> operator[] (CORBA::ULong index);
+    TAO_Pseudo_Object_Manager<TypeCode,TypeCode_var> operator[] (CORBA::ULong index);
     
   private:
     ExceptionList *&ptr_;
@@ -407,15 +530,120 @@ TAO_NAMESPACE  Dynamic
 
   TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_ExceptionList;
 
-  typedef CORBA::StringSeq RequestContext;
-  typedef CORBA::StringSeq_var RequestContext_var;
-  typedef CORBA::StringSeq_out RequestContext_out;
-    TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_RequestContext;
+
+#if !defined (_DYNAMIC_REQUESTCONTEXT_CH_)
+#define _DYNAMIC_REQUESTCONTEXT_CH_
+
+  class RequestContext;
+  class RequestContext_var;
+  
+  // *************************************************************
+  // RequestContext
+  // *************************************************************
+  
+  class  RequestContext : public 
+#if !defined (TAO_USE_SEQUENCE_TEMPLATES)
+    TAO_Unbounded_String_Sequence
+#else /* TAO_USE_SEQUENCE_TEMPLATES */
+    TAO_Unbounded_String_Sequence
+#endif /* !TAO_USE_SEQUENCE_TEMPLATES */ 
+  {
+  public:
+    RequestContext (void); // default ctor
+    RequestContext (CORBA::ULong max); // uses max size
+    RequestContext (
+      CORBA::ULong max, 
+      CORBA::ULong length, 
+      char * *buffer, 
+      CORBA::Boolean release=0
+    );
+    RequestContext (const RequestContext &); // copy ctor
+    ~RequestContext (void);
+    static void _tao_any_destructor (void*);
+
+#if !defined(__GNUC__) || !defined (ACE_HAS_GNUG_PRE_2_8)
+    typedef RequestContext_var _var_type;
+#endif /* ! __GNUC__ || g++ >= 2.8 */
+
+  };
+  
+#endif /* end #if !defined */
+
+
+#if !defined (_DYNAMIC_REQUESTCONTEXT___VAR_CH_)
+#define _DYNAMIC_REQUESTCONTEXT___VAR_CH_
+
+  // *************************************************************
+  // class Dynamic::RequestContext_var
+  // *************************************************************
+
+  class  RequestContext_var
+  {
+  public:
+    RequestContext_var (void); // default constructor
+    RequestContext_var (RequestContext *);
+    RequestContext_var (const RequestContext_var &); // copy constructor
+    ~RequestContext_var (void); // destructor
+    
+    RequestContext_var &operator= (RequestContext *);
+    RequestContext_var &operator= (const RequestContext_var &);
+    RequestContext *operator-> (void);
+    const RequestContext *operator-> (void) const;
+    
+    operator const RequestContext &() const;
+    operator RequestContext &();
+    operator RequestContext &() const;
+    operator RequestContext *&(); // variable-size base types only
+    
+    TAO_SeqElem_String_Manager operator[] (CORBA::ULong index);
+    // in, inout, out, _retn 
+    const RequestContext &in (void) const;
+    RequestContext &inout (void);
+    RequestContext *&out (void);
+    RequestContext *_retn (void);
+    RequestContext *ptr (void) const;
+
+  private:
+    RequestContext *ptr_;
+  };
+
+
+#endif /* end #if !defined */
+
+
+#if !defined (_DYNAMIC_REQUESTCONTEXT___OUT_CH_)
+#define _DYNAMIC_REQUESTCONTEXT___OUT_CH_
+
+  class  RequestContext_out
+  {
+  public:
+    RequestContext_out (RequestContext *&);
+    RequestContext_out (RequestContext_var &);
+    RequestContext_out (const RequestContext_out &);
+    RequestContext_out &operator= (const RequestContext_out &);
+    RequestContext_out &operator= (RequestContext *);
+    operator RequestContext *&();
+    RequestContext *&ptr (void);
+    RequestContext *operator-> (void);
+    TAO_SeqElem_String_Manager operator[] (CORBA::ULong index);
+    
+  private:
+    RequestContext *&ptr_;
+    // assignment from T_var not allowed
+    void operator= (const RequestContext_var &);
+  };
+
+
+#endif /* end #if !defined */
+
+  TAO_NAMESPACE_STORAGE_CLASS CORBA::TypeCode_ptr _tc_RequestContext;
 
 
 }
 TAO_NAMESPACE_CLOSE // module Dynamic
 
+ void operator<<= (CORBA::Any &, Dynamic::ParameterMode);
+ CORBA::Boolean operator>>= (const CORBA::Any &, Dynamic::ParameterMode &);
  void operator<<= (CORBA::Any &, const Dynamic::Parameter &); // copying version
  void operator<<= (CORBA::Any &, Dynamic::Parameter*); // noncopying version
  CORBA::Boolean operator>>= (const CORBA::Any &, Dynamic::Parameter *&); // deprecated
@@ -424,13 +652,23 @@ TAO_NAMESPACE_CLOSE // module Dynamic
  void operator<<= (CORBA::Any &, Dynamic::ParameterList*); // noncopying version
  CORBA::Boolean operator>>= (const CORBA::Any &, Dynamic::ParameterList *&); // deprecated
  CORBA::Boolean operator>>= (const CORBA::Any &, const Dynamic::ParameterList *&);
+ void operator<<= (CORBA::Any &, const Dynamic::ContextList &); // copying version
+ void operator<<= (CORBA::Any &, Dynamic::ContextList*); // noncopying version
+ CORBA::Boolean operator>>= (const CORBA::Any &, Dynamic::ContextList *&); // deprecated
+ CORBA::Boolean operator>>= (const CORBA::Any &, const Dynamic::ContextList *&);
  void operator<<= (CORBA::Any &, const Dynamic::ExceptionList &); // copying version
  void operator<<= (CORBA::Any &, Dynamic::ExceptionList*); // noncopying version
  CORBA::Boolean operator>>= (const CORBA::Any &, Dynamic::ExceptionList *&); // deprecated
  CORBA::Boolean operator>>= (const CORBA::Any &, const Dynamic::ExceptionList *&);
+ void operator<<= (CORBA::Any &, const Dynamic::RequestContext &); // copying version
+ void operator<<= (CORBA::Any &, Dynamic::RequestContext*); // noncopying version
+ CORBA::Boolean operator>>= (const CORBA::Any &, Dynamic::RequestContext *&); // deprecated
+ CORBA::Boolean operator>>= (const CORBA::Any &, const Dynamic::RequestContext *&);
 
 #ifndef __ACE_INLINE__
 
+ CORBA::Boolean operator<< (TAO_OutputCDR &, const Dynamic::ParameterMode &); // 
+ CORBA::Boolean operator>> (TAO_InputCDR &, Dynamic::ParameterMode &);
  CORBA::Boolean operator<< (TAO_OutputCDR &, const Dynamic::Parameter &);
  CORBA::Boolean operator>> (TAO_InputCDR &, Dynamic::Parameter &);
 
@@ -449,6 +687,21 @@ TAO_NAMESPACE_CLOSE // module Dynamic
 #endif /* _TAO_CDR_OP_Dynamic_ParameterList_H_ */
 
 
+#if !defined _TAO_CDR_OP_Dynamic_ContextList_H_
+#define _TAO_CDR_OP_Dynamic_ContextList_H_
+
+ CORBA::Boolean operator<< (
+    TAO_OutputCDR &,
+    const Dynamic::ContextList &
+  );
+ CORBA::Boolean operator>> (
+    TAO_InputCDR &,
+    Dynamic::ContextList &
+  );
+
+#endif /* _TAO_CDR_OP_Dynamic_ContextList_H_ */
+
+
 #if !defined _TAO_CDR_OP_Dynamic_ExceptionList_H_
 #define _TAO_CDR_OP_Dynamic_ExceptionList_H_
 
@@ -464,6 +717,21 @@ TAO_NAMESPACE_CLOSE // module Dynamic
 #endif /* _TAO_CDR_OP_Dynamic_ExceptionList_H_ */
 
 
+#if !defined _TAO_CDR_OP_Dynamic_RequestContext_H_
+#define _TAO_CDR_OP_Dynamic_RequestContext_H_
+
+ CORBA::Boolean operator<< (
+    TAO_OutputCDR &,
+    const Dynamic::RequestContext &
+  );
+ CORBA::Boolean operator>> (
+    TAO_InputCDR &,
+    Dynamic::RequestContext &
+  );
+
+#endif /* _TAO_CDR_OP_Dynamic_RequestContext_H_ */
+
+
 #endif /* __ACE_INLINE__ */
 
 
@@ -474,5 +742,6 @@ TAO_NAMESPACE_CLOSE // module Dynamic
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 #pragma warning(pop)
 #endif /* _MSC_VER */
-
+//Added
+#include "ace/post.h"
 #endif /* ifndef */
