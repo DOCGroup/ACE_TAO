63b51ebce7d5a81ca856990ddac0fa4dd6739e1d
schmidt <douglascraigschmidt@users.noreply.github.com>
2000-05-27 23:13:27 +0000

    ChangeLogTag:Sat May 27 17:09:03 2000  David L. Levine  <levine@cs.wustl.edu>

--- tests/Signal_Test.cpp
+++ tests/Signal_Test.cpp
@@ -241,7 +241,7 @@ worker_parent (void *arg)
   // portably.  Also, pass the test number, as well.
   ACE_OS::sprintf (pid_str,
                    "-p %ld -t %d",
-                   parent_pid,
+                   ACE_static_cast (long, parent_pid),
                    test_number);
 
   // We're going to create a new process that runs this program again,
