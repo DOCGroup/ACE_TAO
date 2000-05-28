517ef079675b0b3e1063c74de3d196f067250337
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-28 04:28:16 +0000

    Sat May 27 23:27:50 2000  Pradeep Gore  <pradeep@flamenco.cs.wustl.edu>

--- TAO/orbsvcs/tests/Notify/Basic/Updates.h
+++ TAO/orbsvcs/tests/Notify/Basic/Updates.h
@@ -18,6 +18,56 @@
 #define NOTIFY_TESTS_UPDATES_H
 
 #include "Notify_Test_Client.h"
+#include "Notify_StructuredPushConsumer.h"
+#include "Notify_StructuredPushSupplier.h"
+
+class Notify_Test_Updates_Client;
+
+class Update_StructuredPushConsumer : public TAO_Notify_StructuredPushConsumer
+{
+public:
+  virtual ~Update_StructuredPushConsumer ();
+  // Destructor.
+
+  virtual void offer_change (
+        const CosNotification::EventTypeSeq & added,
+        const CosNotification::EventTypeSeq & removed,
+        CORBA::Environment &ACE_TRY_ENV
+      )
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        CosNotifyComm::InvalidEventType
+      ));
+  // Offer change is conveyed here.
+};
+
+/***************************************************************************/
+
+class Update_StructuredPushSupplier : public TAO_Notify_StructuredPushSupplier
+{
+public:
+  Update_StructuredPushSupplier (Notify_Test_Updates_Client* test_client);
+  // Constructor.
+
+  virtual ~Update_StructuredPushSupplier ();
+  // Destructor.
+
+  virtual void subscription_change (
+        const CosNotification::EventTypeSeq & added,
+        const CosNotification::EventTypeSeq & removed,
+        CORBA::Environment &ACE_TRY_ENV
+        )
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        CosNotifyComm::InvalidEventType
+      ));
+  // Subscription change is conveyed here.
+
+protected:
+  Notify_Test_Updates_Client* test_client_;
+};
+
+/***************************************************************************/
 
 class Notify_Test_Updates_Client : public Notify_Test_Client
 {
@@ -32,6 +82,22 @@ class Notify_Test_Updates_Client : public Notify_Test_Client
   virtual void run_test (CORBA::Environment &ACE_TRY_ENV);
   // Run the test.
 
+protected:
+  int count_;
+  // Number of updates to test.
+
+  CosNotifyChannelAdmin::ConsumerAdmin_var consumer_admin_;
+  // The consumer admin used by consumers.
+
+  CosNotifyChannelAdmin::SupplierAdmin_var supplier_admin_;
+  // The supplier admin used by suppliers.
+
+  TAO_Notify_StructuredPushConsumer* consumer_;
+  // Consumer
+
+  TAO_Notify_StructuredPushSupplier* supplier_;
+  // Supplier
 };
 
+/***************************************************************************/
 #endif /* NOTIFY_TESTS_UPDATES_H */
