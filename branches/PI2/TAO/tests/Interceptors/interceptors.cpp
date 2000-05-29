d4674c03306f353d4e6da0d5fe7f7d35ad11ada0
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-29 00:21:12 +0000

    added changes

--- branches/PI2/TAO/tests/Interceptors/interceptors.cpp
+++ branches/PI2/TAO/tests/Interceptors/interceptors.cpp
@@ -39,92 +39,70 @@ Echo_Client_Request_Interceptor::name (CORBA::Environment &)
 {
   return CORBA::string_dup (this->myname_);
 }
-
-void
-Echo_Client_Request_Interceptor::preinvoke (CORBA::ULong rid,
-                                            CORBA::Boolean,
-                                            CORBA::Object_ptr objref,
-                                            const char *op,
-                                            IOP::ServiceContextList &sc,
-                                            CORBA::NVList_ptr &,
-                                            PortableInterceptor::Cookies &,
-                                            CORBA::Environment &ACE_TRY_ENV)
-  ACE_THROW_SPEC ((CORBA::SystemException))
+ 
+void 
+Echo_Client_Request_Interceptor::send_request (PortableInterceptor::ClientRequestInfo_ptr ri,
+                                               CORBA::Environment &ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException,
+                     PortableInterceptor::ForwardRequest))
 {
-  CORBA::String_var ior = this->orb_->object_to_string (objref,
+  CORBA::String_var ior = this->orb_->object_to_string (this,
                                                         ACE_TRY_ENV);
   ACE_CHECK;
 
   ACE_DEBUG ((LM_DEBUG,
-              "Echo_Client_Request_Interceptor::preinvoke from \"%s\" request_id: %d on object: %s\n",
-              op,
-              rid,
-              ior.in ()));
+              "Echo_Client_Request_Interceptor::preinvoke from \"%s\" on object: %s\n",
+              ri->operation (),
+              this->orb_->object_to_string (this)));
+  
+  // Populate target member of the ClientRequestInfo.
 
-  CORBA::ULong length = sc.length ();
-  sc.length (length + 1);
-
-  sc[length].context_id = request_ctx_id;
+  // MAke the context to send the context to the target
+  IOP::ServiceContext_var sc;
+  sc->context_id = request_ctx_id;
 
   CORBA::ULong string_len = ACE_OS::strlen (request_msg) + 1;
   CORBA::Octet *buf = 0;
   ACE_NEW (buf,
            CORBA::Octet [string_len]);
-
   ACE_OS::strcpy (ACE_reinterpret_cast (char *, buf), request_msg);
-  sc[length].context_data.replace (string_len, string_len, buf, 1);
+  
+  sc->context_data.replace (string_len, string_len, buf, 1);
+
+  // Add this context to the service context list.
+  ri->add_request_service_context (sc, 0);
+
 }
 
-void
-Echo_Client_Request_Interceptor::postinvoke (CORBA::ULong rid,
-                                             CORBA::Boolean,
-                                             CORBA::Object_ptr objref,
-                                             const char * op,
-                                             IOP::ServiceContextList &sc,
-                                             CORBA::NVList_ptr &,
-                                             PortableInterceptor::Cookies &,
-                                             CORBA::Environment &ACE_TRY_ENV)
-  ACE_THROW_SPEC ((CORBA::SystemException))
+void 
+Echo_Client_Request_Interceptor::receive_reply (PortableInterceptor::ClientRequestInfo_ptr ri,
+                                                CORBA::Environment &ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  CORBA::String_var ior = this->orb_->object_to_string (objref,
-                                                        ACE_TRY_ENV);
-  ACE_CHECK;
-
+  
   ACE_DEBUG ((LM_DEBUG,
-              "Echo_Client_Request_Interceptor::postinvoke from \"%s\" request_id: %d on object: %s\n",
-              op,
-              rid,
-              ior.in ()));
+              "Echo_Client_Request_Interceptor::postinvoke from \"%s\" on object: %s\n",
+              ri->operation (ACE_TRY_ENV),
+              ri->target (ACE_TRY_ENV)));
 
-  for (CORBA::ULong size = 0; size < sc.length (); ++size)
-    if (sc[size].context_id == reply_ctx_id)
-      {
-        const char *buf = ACE_reinterpret_cast (const char *, sc[size].context_data.get_buffer ());
-        ACE_DEBUG ((LM_DEBUG,
-                    "  Received service context: %s\n",
-                    buf));
-      }
+  // ServiceID? is hacked and set to 1 for now
+  IOP::ServiceId id = reply_ctx_id;
+  IOP::ServiceContext * sc = ri->get_reply_service_context (id);
+  
+  const char *buf = ACE_reinterpret_cast (const char *, sc->context_data.get_buffer ());
+  ACE_DEBUG ((LM_DEBUG,
+              "  Received service context: %s\n",
+              buf));
 }
 
-void
-Echo_Client_Request_Interceptor::exception_occurred (CORBA::ULong rid,
-                                                     CORBA::Boolean,
-                                                     CORBA::Object_ptr objref,
-                                                     const char * op,
-                                                     PortableInterceptor::Cookies &,
-                                                     CORBA::Environment &ACE_TRY_ENV)
-  ACE_THROW_SPEC ((CORBA::SystemException))
+void 
+Echo_Client_Request_Interceptor::receive_exception (PortableInterceptor::ClientRequestInfo_ptr ri,
+                                                    CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException, 
+                     PortableInterceptor::ForwardRequest))
 {
-  CORBA::String_var ior = this->orb_->object_to_string (objref,
-                                                        ACE_TRY_ENV);
-  ACE_CHECK;
-
-  ACE_DEBUG ((LM_DEBUG,
-              "Echo_Client_Request_Interceptor::exception_occurred from \"%s\" request_id: %d on object: %s\n",
-              op,
-              rid,
-              ior.in ()));
 }
+
 
 Echo_Server_Request_Interceptor::Echo_Server_Request_Interceptor (CORBA::ORB_ptr orb)
   : myname_ ("Echo_Server_Interceptor"),
@@ -156,89 +134,60 @@ Echo_Server_Request_Interceptor::name (CORBA::Environment &)
 {
   return CORBA::string_dup (this->myname_);
 }
-
-void
-Echo_Server_Request_Interceptor::preinvoke (CORBA::ULong rid,
-                                            CORBA::Boolean,
-                                            CORBA::Object_ptr objref,
-                                            const char *op,
-                                            IOP::ServiceContextList &sc,
-                                            CORBA::NVList_ptr &,
-                                            PortableInterceptor::Cookies &,
-                                            CORBA::Environment &ACE_TRY_ENV)
-  ACE_THROW_SPEC ((CORBA::SystemException))
+  
+void 
+Echo_Server_Request_Interceptor::receive_request (PortableInterceptor::ServerRequestInfo_ptr ri,
+                                                  CORBA::Environment &)
+  ACE_THROW_SPEC ((CORBA::SystemException,
+                   PortableInterceptor::ForwardRequest))
 {
-  CORBA::String_var ior = this->orb_->object_to_string (objref,
-                                                        ACE_TRY_ENV);
-  ACE_CHECK;
 
   ACE_DEBUG ((LM_DEBUG,
-              "Echo_Server_Request_Interceptor::preinvoke from \"%s\" request_id: %d on object: %s\n",
-              op,
-              rid,
-              ior.in ()));
+              "Echo_Server_Request_Interceptor::preinvoke from \"%s\"",
+              ri->operation ()));
+  
 
-  for (CORBA::ULong size = 0; size < sc.length (); ++size)
-    if (sc[size].context_id == request_ctx_id)
-      {
-        const char *buf = ACE_reinterpret_cast (const char *, sc[size].context_data.get_buffer ());
-        ACE_DEBUG ((LM_DEBUG,
-                    "  Received service context: %s\n",
-                    buf));
-      }
+  // ServiceID? is hacked and set to 1 for now
+  IOP::ServiceId id = request_ctx_id;
+  IOP::ServiceContext *sc = ri->get_request_service_context (id);
+ 
+  const char *buf = ACE_reinterpret_cast (const char *, sc->context_data.get_buffer ());
+  ACE_DEBUG ((LM_DEBUG,
+              "  Received service context: %s\n",
+              buf));
 }
 
-void
-Echo_Server_Request_Interceptor::postinvoke (CORBA::ULong rid,
-                                             CORBA::Boolean,
-                                             CORBA::Object_ptr objref,
-                                             const char * op,
-                                             IOP::ServiceContextList &sc,
-                                             CORBA::NVList_ptr &,
-                                             PortableInterceptor::Cookies &,
-                                             CORBA::Environment &ACE_TRY_ENV)
-  ACE_THROW_SPEC ((CORBA::SystemException))
+void 
+Echo_Server_Request_Interceptor::send_reply (PortableInterceptor::ServerRequestInfo_ptr ri,
+                                             CORBA::Environment &)
+  ACE_THROW_SPEC ((CORBA::SystemException,
+                   PortableInterceptor::ForwardRequest))
 {
-  CORBA::String_var ior = this->orb_->object_to_string (objref,
-                                                        ACE_TRY_ENV);
-  ACE_CHECK;
-
   ACE_DEBUG ((LM_DEBUG,
-              "Echo_Server_Request_Interceptor::postinvoke from \"%s\" request_id: %d on object: %s\n",
-              op,
-              rid,
-              ior.in ()));
+              "Echo_Server_Request_Interceptor::postinvoke from \"%s\"",
+              ri->operation ()));
 
-  CORBA::ULong length = sc.length ();
-  sc.length (length + 1);
+  // ServiceID? is hacked and set to 1 for now
+  IOP::ServiceId id = request_ctx_id;
+  IOP::ServiceContext_var sc = ri->get_request_service_context (id); 
 
-  sc[length].context_id = reply_ctx_id;
+  sc->context_id = reply_ctx_id;
 
   CORBA::ULong string_len = ACE_OS::strlen (reply_msg) + 1;
   CORBA::Octet *buf = 0;
   ACE_NEW (buf,
            CORBA::Octet [string_len]);
 
-  ACE_OS::strcpy (ACE_reinterpret_cast (char *, buf), reply_msg);
-  sc[length].context_data.replace (string_len, string_len, buf, 1);
+  ACE_OS::strcpy (ACE_reinterpret_cast (char *, buf), request_msg);
+  
+  sc->context_data.replace (string_len, string_len, buf, 1);
+
 }
 
-void
-Echo_Server_Request_Interceptor::exception_occurred (CORBA::ULong rid,
-                                                     CORBA::Boolean,
-                                                     CORBA::Object_ptr objref,
-                                                     const char * op,
-                                                     PortableInterceptor::Cookies &,
-                                                     CORBA::Environment &ACE_TRY_ENV)
-  ACE_THROW_SPEC ((CORBA::SystemException))
+void 
+Echo_Server_Request_Interceptor::send_exception (PortableInterceptor::ServerRequestInfo_ptr ri,
+                                                 CORBA::Environment &)
+  ACE_THROW_SPEC ((CORBA::SystemException,
+                   PortableInterceptor::ForwardRequest))
 {
-  CORBA::String_var ior = this->orb_->object_to_string (objref,
-                                                        ACE_TRY_ENV);
-  ACE_CHECK;
-
-  ACE_DEBUG ((LM_DEBUG,
-              "Echo_Server_Request_Interceptor::exception_occurred from \"%s\" request_id: %d on object: %s\n",
-              op,
-              rid,
-              ior.in ()));
 }
