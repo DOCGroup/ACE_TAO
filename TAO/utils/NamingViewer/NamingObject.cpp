23704d3d4c7621dca071e55ae7cdb51e61c72e2b
schmidt <douglascraigschmidt@users.noreply.github.com>
2000-05-29 22:02:13 +0000

    ChangeLogTag:Mon May 29 16:49:25 2000  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- TAO/utils/NamingViewer/NamingObject.cpp
+++ TAO/utils/NamingViewer/NamingObject.cpp
@@ -1,6 +1,6 @@
 // $Id$
 // NamingObject.cpp: implementation of the CNamingObject class.
-//
+
 //////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
@@ -17,33 +17,31 @@ static char THIS_FILE[]=__FILE__;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-CNamingObject::CNamingObject(CosNaming::Name& Name, CORBA::Object_ptr pObject, bool Context)
-: m_Name(Name), m_Object(CORBA::Object::_duplicate(pObject)), m_Context(Context)
+CNamingObject::CNamingObject(CosNaming::Name &Name,
+                             CORBA::Object_ptr pObject,
+                             bool Context)
+  : m_Name (Name),
+    m_Object (CORBA::Object::_duplicate (pObject)),
+    m_Context (Context)
 {
 
 }
 
-
-CNamingObject::CNamingObject(CORBA::Object_ptr pObject)
-: m_Object(CORBA::Object::_duplicate(pObject))
+CNamingObject::~CNamingObject (void)
 {
 }
 
-CNamingObject::~CNamingObject()
+CosNaming::NamingContext_ptr CNamingObject::NamingContext (void)
 {
+  return CosNaming::NamingContext::_narrow (m_Object);
 }
 
-CosNaming::NamingContext_ptr CNamingObject::NamingContext()
-{
-  return CosNaming::NamingContext::_narrow(m_Object);
-}
-
-CORBA::Object_ptr CNamingObject::Object()
+CORBA::Object_ptr CNamingObject::Object (void)
 {
   return m_Object;
 }
 
-CosNaming::Name& CNamingObject::Name()
+CosNaming::Name& CNamingObject::Name (void)
 {
   return m_Name;
 }
