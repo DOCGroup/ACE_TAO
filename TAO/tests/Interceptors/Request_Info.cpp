945401c42f5d8ab0c4e124ab59648d59b3836c41
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-05-30 06:20:18 +0000

    change in design

--- TAO/tests/Interceptors/Request_Info.cpp
+++ TAO/tests/Interceptors/Request_Info.cpp
@@ -1,17 +1,19 @@
 // $Id$
 
-#include "./Request_Info.h"
-
+#include "Request_Info.h"
+#include "tao/corba.h"
 //****************************************************************               
 
-Request_Info::Request_Info (char * operation, 
-                          Dynamic::ParameterList * arguments,
-                          Dynamic::ExceptionList * exceptions,
-                          Dynamic::ContextList * contexts,
-                          Dynamic::RequestContext * operation_context,
-                          CORBA::Object_ptr forward_reference,
-                          CORBA::Environment &)
-  : operation_ (operation),
+/*Request_Info::Request_Info (CORBA::ULong request_id,
+                            char * operation, 
+                            Dynamic::ParameterList * arguments,
+                            Dynamic::ExceptionList * exceptions,
+                            Dynamic::ContextList * contexts,
+                            Dynamic::RequestContext * operation_context,
+                            CORBA::Object_ptr forward_reference,
+                            CORBA::Environment &)
+  : request_id_ (request_id),
+    operation_ (operation),
     parameter_list_ (arguments),
     exception_list_ (exceptions),
     context_list_ (contexts),
@@ -19,79 +21,88 @@ Request_Info::Request_Info (char * operation,
     forward_reference_ (CORBA::Object::_duplicate (forward_reference))
 {
 }
+*/
+
+
+CORBA::ULong 
+ClientRequest_Info::request_id (CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return 0;
+}
 
 char * 
-Request_Info::operation (CORBA::Environment &)
+ClientRequest_Info::operation (CORBA::Environment &)
    ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return this->operation_;
 }
 
 Dynamic::ParameterList * 
-Request_Info::arguments (CORBA::Environment &)
+ClientRequest_Info::arguments (CORBA::Environment &)
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
  return this->parameter_list_._retn ();
 }
 
 Dynamic::ExceptionList * 
-Request_Info::exceptions (CORBA::Environment &)  
+ClientRequest_Info::exceptions (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return this->exception_list_._retn ();
 }
 
 Dynamic::ContextList * 
-Request_Info::contexts (CORBA::Environment &)  
+ClientRequest_Info::contexts (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
  return this->context_list_._retn ();
 }
 
 Dynamic::RequestContext * 
-Request_Info::operation_context (CORBA::Environment &)  
+ClientRequest_Info::operation_context (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
  return this->request_context_._retn ();
 }
 
 CORBA::Any * 
-Request_Info::result (CORBA::Environment &)  
+ClientRequest_Info::result (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return 0;
 }
 
 CORBA::Boolean 
-Request_Info::response_expected (CORBA::Environment &)  
+ClientRequest_Info::response_expected (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return 0;
 }
 
 Messaging::SyncScope 
-Request_Info::sync_scope (CORBA::Environment &)  
+ClientRequest_Info::sync_scope (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
  return 0;
 }
 
 PortableInterceptor::ReplyStatus 
-Request_Info::reply_status (CORBA::Environment &)  
+ClientRequest_Info::reply_status (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return 0;
 }
 
 CORBA::Object_ptr 
-Request_Info::forward_reference (CORBA::Environment &)  
+ClientRequest_Info::forward_reference (CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return 0;
 }
 
 CORBA::Any * 
-Request_Info::get_slot (PortableInterceptor::SlotId id,
+ClientRequest_Info::get_slot (PortableInterceptor::SlotId id,
                        CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException,
                    PortableInterceptor::InvalidSlot))
@@ -100,11 +111,11 @@ Request_Info::get_slot (PortableInterceptor::SlotId id,
 }
 
 IOP::ServiceContext * 
-Request_Info::get_request_service_context (IOP::ServiceId id,
+ClientRequest_Info::get_request_service_context (IOP::ServiceId id,
                               CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  for (CORBA::ULong size = 0; size < this->service_context_list_->length (); ++size)
+  for (CORBA::ULong size = 0; size < this->service_context_list_.length (); ++size)
     if (this->service_context_list_[size].context_id == id)
       {
          return &(this->service_context_list_[size]);
@@ -113,11 +124,11 @@ Request_Info::get_request_service_context (IOP::ServiceId id,
 }
 
 IOP::ServiceContext * 
-Request_Info::get_reply_service_context (IOP::ServiceId id,
+ClientRequest_Info::get_reply_service_context (IOP::ServiceId id,
                             CORBA::Environment &)  
   ACE_THROW_SPEC ((CORBA::SystemException)) 
 {
-  for (CORBA::ULong size = 0; size < this->service_context_list_->length (); ++size)
+  for (CORBA::ULong size = 0; size < this->service_context_list_.length (); ++size)
     if (this->service_context_list_[size].context_id == id)
       {
         // Used <_retn> becos then it will get safely destroyed at the
@@ -125,23 +136,37 @@ Request_Info::get_reply_service_context (IOP::ServiceId id,
         return &this->service_context_list_[size];
         
       }
-//****************************************************************               
+}
 
-ClientRequest_Info:: ClientRequest_Info (CORBA_Object * target,
-                                       CORBA_Object * effective_target,
-                                       IOP::ServiceContextList &service_context_list,               
-                                       char * operation, 
-                                       Dynamic::ParameterList * arguments,
-                                       Dynamic::ExceptionList * exceptions,
-                                       Dynamic::ContextList * contexts,
-                                       Dynamic::RequestContext * operation_context,
-                                       CORBA::Object_ptr forward_reference,
-                                       CORBA::Environment &)
-  : target_ (CORBA::Object::_duplicate (target)), service_context_list_ (service_context_list)
-    Request_Info (operation, arguments, exceptions, contexts, operation_context, forward_reference)
+/*ClientRequest_Info:: ClientRequest_Info (CORBA::ULong request_id,
+                                         char * operation, 
+                                         Dynamic::ParameterList * arguments,
+                                         Dynamic::ExceptionList * exceptions,
+                                         Dynamic::ContextList * contexts,
+                                         Dynamic::RequestContext * operation_context,
+                                         CORBA::Object_ptr forward_reference,
+                                         CORBA_Object_ptr target,
+                                         CORBA_Object_ptr effective_target,
+                                         IOP::ServiceContextList_ptr &service_context_list,               
+                                         CORBA::Environment &)
+  :    Request_Info (request_id, operation, arguments, exceptions, contexts, operation_context, forward_reference),
+       target_ (CORBA::Object::_duplicate (target))
+ 
+{
+  this->service_context_list_ = service_context_list;
+}
+*/
+
+ClientRequest_Info::ClientRequest_Info (CORBA::Environment &,
+                                        char * operation,
+                                        IOP::ServiceContextList &service_context_list ,               
+                                        CORBA::Object * target)
+  : operation_ (operation), 
+    service_context_list_ (service_context_list),
+    target_ (target)
 {
 }
-  
+
 CORBA::Object_ptr 
 ClientRequest_Info::target (CORBA::Environment &)
   ACE_THROW_SPEC ((CORBA::SystemException))
@@ -196,30 +221,153 @@ void
 ClientRequest_Info::add_request_service_context (const IOP::ServiceContext & service_context,
                                                 CORBA::Boolean replace,
                                                 CORBA::Environment &)
-  ACE_THROW_SPEC ((CORBA::SystemException));   
+  ACE_THROW_SPEC ((CORBA::SystemException)) 
 {
   // Need to take care of the replace semantics, see spec
-  CORBA::ULong length = this->service_context_list_->length ();
-  this->service_context_list_->length (length + 1);
+  CORBA::ULong length = this->service_context_list_.length ();
+  this->service_context_list_.length (length + 1);
 
   // Hey the service context is passe dby ref, do i haveto worry?
   service_context_list_[length] = service_context;
 }
 //****************************************************************               
 
- 
-ServerRequest_Info::ServerRequest_Info(PortableInterceptor::OctetSeq *object_id,
-                                     PortableInterceptor::OctetSeq *adapter_id,
-                                     char * operation, 
-                                     Dynamic::ParameterList * arguments,
-                                     Dynamic::ExceptionList * exceptions,
-                                     Dynamic::ContextList * contexts,
-                                     Dynamic::RequestContext * operation_context,
-                                     CORBA::Object_ptr forward_reference,
-                                     CORBA::Environment &)
-  : object_id_ (object_id),
-    adapter_id_ (adapter_id),
-    Request_Info (operation, arguments, exceptions, contexts, operation_context, forward_reference)
+CORBA::ULong 
+ServerRequest_Info::request_id (CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return 0;
+}
+
+char * 
+ServerRequest_Info::operation (CORBA::Environment &)
+   ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return this->operation_;
+}
+
+Dynamic::ParameterList * 
+ServerRequest_Info::arguments (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+ return this->parameter_list_._retn ();
+}
+
+Dynamic::ExceptionList * 
+ServerRequest_Info::exceptions (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return this->exception_list_._retn ();
+}
+
+Dynamic::ContextList * 
+ServerRequest_Info::contexts (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+ return this->context_list_._retn ();
+}
+
+Dynamic::RequestContext * 
+ServerRequest_Info::operation_context (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+ return this->request_context_._retn ();
+}
+
+CORBA::Any * 
+ServerRequest_Info::result (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return 0;
+}
+
+CORBA::Boolean 
+ServerRequest_Info::response_expected (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return 0;
+}
+
+Messaging::SyncScope 
+ServerRequest_Info::sync_scope (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+ return 0;
+}
+
+PortableInterceptor::ReplyStatus 
+ServerRequest_Info::reply_status (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return 0;
+}
+
+CORBA::Object_ptr 
+ServerRequest_Info::forward_reference (CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return 0;
+}
+
+CORBA::Any * 
+ServerRequest_Info::get_slot (PortableInterceptor::SlotId id,
+                       CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException,
+                   PortableInterceptor::InvalidSlot))
+{
+  return 0;
+}
+
+IOP::ServiceContext * 
+ServerRequest_Info::get_request_service_context (IOP::ServiceId id,
+                              CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  for (CORBA::ULong size = 0; size < this->service_context_list_.length (); ++size)
+    if (this->service_context_list_[size].context_id == id)
+      {
+         return &(this->service_context_list_[size]);
+        
+      }
+}
+
+IOP::ServiceContext * 
+ServerRequest_Info::get_reply_service_context (IOP::ServiceId id,
+                            CORBA::Environment &)  
+  ACE_THROW_SPEC ((CORBA::SystemException)) 
+{
+  for (CORBA::ULong size = 0; size < this->service_context_list_.length (); ++size)
+    if (this->service_context_list_[size].context_id == id)
+      {
+        // Used <_retn> becos then it will get safely destroyed at the
+        // client side when the service context gets back.
+        return &this->service_context_list_[size];
+        
+      }
+}
+
+/* 
+ServerRequest_Info::ServerRequest_Info(CORBA::ULong request_id,
+                                       char * operation, 
+                                       Dynamic::ParameterList * arguments,
+                                       Dynamic::ExceptionList * exceptions,
+                                       Dynamic::ContextList * contexts,
+                                       Dynamic::RequestContext * operation_context,
+                                       CORBA::Object_ptr forward_reference,
+                                       PortableInterceptor::OctetSeq *object_id,
+                                       PortableInterceptor::OctetSeq *adapter_id,
+                                       CORBA::Environment &)
+  : Request_Info (request_id, operation, arguments, exceptions, contexts, operation_context, forward_reference),
+    object_id_ (object_id),
+    adapter_id_ (adapter_id)
+{
+}
+*/
+ServerRequest_Info::ServerRequest_Info (char * operation,
+                                        IOP::ServiceContextList &service_context_list ,               
+                                        CORBA::Environment &)
+  : operation_ (operation), 
+    service_context_list_ (service_context_list)
 {
 }
 
@@ -234,19 +382,21 @@ PortableInterceptor::OctetSeq *
 ServerRequest_Info::object_id (CORBA::Environment &)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
- 
+  return 0; 
 }
 
 PortableInterceptor::OctetSeq * 
 ServerRequest_Info::adapter_id (CORBA::Environment &)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
+ return 0;
 }
 
 char * 
 ServerRequest_Info::target_most_derived_interface (CORBA::Environment &)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
+ return 0;
 }
 
 CORBA::Policy_ptr 
@@ -281,4 +431,11 @@ ServerRequest_Info::add_reply_service_context (const IOP::ServiceContext & servi
                                               CORBA::Environment &)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
+  // Need to take care of the replace semantics, see spec
+  CORBA::ULong length = this->service_context_list_.length ();
+  this->service_context_list_.length (length + 1);
+
+  // Hey the service context is passe dby ref, do i haveto worry?
+  service_context_list_[length] = service_context;
+
 }
