6ae5da91fa90b3145df56912989e9994fb34de92
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-01 22:26:07 +0000

    Thu Jun  1 17:18:17 2000  Luther J Baker  <luther@cs.wustl.edu>

--- PACE/pace/config/config.h
+++ PACE/pace/config/config.h
@@ -18,9 +18,17 @@
 #ifndef PACE_CONFIG_CONFIG_H
 #define PACE_CONFIG_CONFIG_H
 
+# if defined __EXTENSIONS__
+# define PACE_EXTENSIONS
 
-#include "pace/config/config_sunos_5.7.h"
+# if defined _REENTRANT
+# define PACE_HAS_REENTRANT
 
+# if defined _POSIX_PTHREAD_SEMANTICS
+# define PACE_HAS_POSIX_PTHREAD_SEMANTICS
+
+/* OS/PACE_mqueue.h */
+# define PACE_HAS_RT
 
 #endif /* PACE_CONFIG_CONFIG_H */
 
