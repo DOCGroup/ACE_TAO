7c23126e039504d60d187d757bd673ecc7b8cdb9
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-06 18:58:07 +0000

    Smart proxy support is not needed for Current since it is a local object

--- trunk/TAO/tao/CurrentC.cpp
+++ trunk/TAO/tao/CurrentC.cpp
@@ -68,11 +68,9 @@ CORBA_Current_ptr CORBA_Current::_unchecked_narrow (
         }
       if (CORBA::is_nil (default_proxy))
         ACE_NEW_RETURN (default_proxy, CORBA_Current (stub), CORBA_Current::_nil ());
-      #if (TAO_HAS_SMART_PROXIES == 1)
-        return TAO_CORBA_Current_PROXY_FACTORY_ADAPTER::instance ()->create_proxy (default_proxy);
-      #else 
-        return default_proxy;
-      #endif /*TAO_HAS_SMART_PROXIES == 1*/
+      
+      return default_proxy;
+
     }
   else 
     return
