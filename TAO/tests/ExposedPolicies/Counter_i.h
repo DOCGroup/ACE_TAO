113e12a11998ec25232d54126056d15bb3da7f95
corsaro <corsaro@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-07 19:39:58 +0000

    Wed Jun 07 13:41:17 2000  Angelo Corsaro  <corsaro@cs.wustl.edu>

---
+++ TAO/tests/ExposedPolicies/Counter_i.h
@@ -0,0 +1,26 @@
+//$Id$
+
+#ifndef COUNTER_I_H_
+#define COUNTER_I_H_
+
+#include "CounterS.h"
+
+class CounterServant : public POA_Counter {
+
+public:
+  
+  // Ctor-Dtor Declaration
+  CounterServant ( void );
+  virtual ~CounterServant ( void  );
+
+  // Counter Interface Methods Overloading.  
+  virtual void increment ( void );
+  virtual void decrement ( void );
+  virtual void reset ( void );
+  virtual CORBA::Long get_count ( void );
+
+protected:
+  CORBA::Long count_;
+};
+
+#endif /*COUNTER_I_H_*/
