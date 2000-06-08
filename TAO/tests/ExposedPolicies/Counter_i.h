aed98ca00461b995980d71dd7a4ac1788ac704cf
marina <marina@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-08 01:06:31 +0000

    ChangeLogTag:Wed Jun  7 20:04:11 2000  Marina Spivak  <marina@cs.wustl.edu>

--- TAO/tests/ExposedPolicies/Counter_i.h
+++ TAO/tests/ExposedPolicies/Counter_i.h
@@ -1,23 +1,27 @@
 //$Id$
 
+// @@ Angelo, please put header with author, file name, etc. here.
+
 #ifndef COUNTER_I_H_
 #define COUNTER_I_H_
 
 #include "CounterS.h"
 
-class CounterServant : public POA_Counter {
-
+class Counter_Servant : public POA_Counter
+{
 public:
-  
-  // Ctor-Dtor Declaration
-  CounterServant ( void );
-  virtual ~CounterServant ( void  );
 
-  // Counter Interface Methods Overloading.  
-  virtual void increment ( void );
-  virtual void decrement ( void );
-  virtual void reset ( void );
-  virtual CORBA::Long get_count ( void );
+  // = Ctor-Dtor Declaration
+
+  Counter_Servant (void);
+  virtual ~Counter_Servant (void);
+
+  // = Counter Interface Methods Overloading.
+
+  virtual void increment (void);
+  virtual void decrement (void);
+  virtual void reset (void);
+  virtual CORBA::Long get_count (void);
 
 protected:
   CORBA::Long count_;
