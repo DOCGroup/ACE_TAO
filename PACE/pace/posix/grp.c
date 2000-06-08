2db8cb13b2292345ec3ac429e1e7874a94d6eee2
joeh <joeh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-08 17:09:25 +0000

    Removing files from sunos and linux subdirectories and moving them to posix subdirectory.

---
+++ PACE/pace/posix/grp.c
@@ -0,0 +1,22 @@
+/* $Id$
+
+ * =============================================================================
+ *
+ * = LIBRARY
+ *    pace
+ *
+ * = FILENAME
+ *    grp.c
+ *
+ * = AUTHOR
+ *    Luther Baker
+ *
+ * ============================================================================= */
+
+
+#include "pace/grp.h"
+
+
+# if !defined (PACE_HAS_INLINE)
+# include "pace/grp.inl"
+# endif /* !PACE_HAS_INLINE */
---
+++ PACE/pace/posix/times.c
@@ -0,0 +1,22 @@
+/* $Id$
+
+ * =============================================================================
+ *
+ * = LIBRARY
+ *    pace
+ *
+ * = FILENAME
+ *    times.c
+ *
+ * = AUTHOR
+ *    Luther Baker
+ *
+ * ============================================================================= */
+
+
+#include "pace/sys/times.h"
+
+
+# if !defined (PACE_HAS_INLINE)
+# include "pace/sys/times.inl"
+# endif /* !PACE_HAS_INLINE */
