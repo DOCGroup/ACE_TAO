27c37a22bcfe067845dc074ea6b6f6a439e0d4d7
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-15 18:29:40 +0000

    Mon Jun 12 17:23:59 2000  Luther J Baker  <luther@cs.wustl.edu>

--- PACE/pace/defines.h
+++ PACE/pace/defines.h
@@ -21,7 +21,6 @@
 #include "pace/config/config.h"
 #include "pace/config/platform.h"
 #include "pace/config/compiler.h"
-#include "pace/config/codes.h"
 #include "pace/config/constants.h"
 #include "pace/config/defaults.h"
 #include "pace/config/types.h"
