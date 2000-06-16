17cf57428d3df582ea4be6b5ba266c9c4a51bf21
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-16 00:57:43 +0000

    Removed unnecessary 'static' modifier.

--- TAO/performance-tests/RTCorba/Oneways/Reliable/client.cpp
+++ TAO/performance-tests/RTCorba/Oneways/Reliable/client.cpp
@@ -44,7 +44,7 @@ inline int QuantifyStopRecordingData ()
 static const char *ior = "file://test.ior";
 
 // Levels at which syncscope policy can be set.
-static const enum LEVEL {ORB, THREAD, OBJECT};
+const enum LEVEL {ORB, THREAD, OBJECT};
 
 // Default is OBJECT level.
 static LEVEL level = OBJECT;
