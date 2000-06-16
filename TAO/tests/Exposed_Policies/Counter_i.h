9db5a8d556253338195779529ab303aa1b9b112b
corsaro <corsaro@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-16 18:27:37 +0000

    ChangeLogTag: Fri Jun 16 13:16:49 2000  Angelo Corsaro  <corsaro@cs.wustl.edu>

---
+++ TAO/tests/Exposed_Policies/Counter_i.h
@@ -0,0 +1,55 @@
+//$Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    TAO/tests/Exposed_Policies
+//
+// = FILENAME
+//    Counter_i.h
+//
+// = DESCRIPTION
+//     This class implements the Counter IDL interface.
+//
+// = AUTHOR
+//     Angelo Corsaro <corsaro@cs.wustl.edu>
+//
+// ============================================================================
+
+
+// @@ Angelo, please put header with author, file name, etc. here.
+// @@ Marina Done.
+
+#ifndef COUNTER_I_H_
+#define COUNTER_I_H_
+
+#include "CounterS.h"
+
+class Counter_Servant : public POA_Counter
+{
+public:
+
+  // = Ctor-Dtor Declaration
+
+  Counter_Servant (CORBA::ORB_ptr o);
+  virtual ~Counter_Servant (void);
+
+  // = Counter Interface Methods Overloading.
+
+  virtual void increment (CORBA::Environment &ACE_TRY_ENV = 
+                            TAO_default_environment ());
+  
+  virtual CORBA::Long get_count (CORBA::Environment &ACE_TRY_ENV = 
+                                   TAO_default_environment ());
+  
+  virtual void shutdown (CORBA::Environment &ACE_TRY_ENV = 
+                           TAO_default_environment ()
+                         ) ACE_THROW_SPEC ((CORBA::SystemException));
+
+
+protected:
+  CORBA::Long count_;
+  CORBA::ORB_var orb_;
+};
+
+#endif /*COUNTER_I_H_*/
