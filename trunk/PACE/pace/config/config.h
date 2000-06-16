3736d271c3f159cf61116946958c2a99a806ff60
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-16 22:53:31 +0000

    Fri Jun 16 17:47:59 2000  Luther J Baker  <luther@cs.wustl.edu>

--- trunk/PACE/pace/config/config.h
+++ trunk/PACE/pace/config/config.h
@@ -6,44 +6,50 @@
  *    pace
  *
  * = FILENAME
- *    config.h
+ *    pace/config/config.h
  *
  * = AUTHOR
  *    Luther Baker
  *
  * ============================================================================ */
 
-
 #ifndef PACE_CONFIG_CONFIG_H
 #define PACE_CONFIG_CONFIG_H
 
-# define PACE_HAS_INLINE
-
-# if defined (_POSIX_C_SOURCE)
+#if defined (_POSIX_C_SOURCE)
+/* current */
+# define PACE_HAS_POSIX_C_SOURCE _POSIX_C_SOURCE
+/* deprecated */
 # define PACE_POSIX_C_SOURCE _POSIX_C_SOURCE
-# endif /* _POSIX_C_SOURCE */
+#endif /* _POSIX_C_SOURCE */
 
-# if defined (__EXTENSIONS__)
+#if defined (__EXTENSIONS__)
+/* deprecated */
 # define PACE_EXTENSIONS __EXTENSIONS__
+/* current */
+# define PACE_HAS_EXTENSIONS __EXTENSIONS__
 # endif
 
-# if defined (_REENTRANT)
-/* deprecated */
+#if defined (_REENTRANT)
 # define PACE_HAS_REENTRANT
-/* current */
-# define PACE_REENTRANT _REENTRANT
-# endif
+#endif
 
-# if defined (_POSIX_PTHREAD_SEMANTICS)
-/* deprecated */
+#if defined (_POSIX_PTHREAD_SEMANTICS)
 # define PACE_HAS_POSIX_PTHREAD_SEMANTICS _POSIX_PTHREAD_SEMANTICS
-/* current */
-# define PACE_POSIX_PTHREAD_SEMANTICS _POSIX_PTHREAD_SEMANTICS
-# endif
+#endif
 
-/* OS/PACE_mqueue.h */
+/* pace/pace/mqueue.h */
 # define PACE_HAS_RT
 
 #endif /* PACE_CONFIG_CONFIG_H */
+
+
+
+
+
+
+
+
+
 
 
