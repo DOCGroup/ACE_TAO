06890fb70e107c1ead2c5a0b234abf8eea70ae31
marina <marina@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-18 04:05:26 +0000

    ChangeLogTag:Sat Jun 17 23:02:34 2000  Marina Spivak  <marina@cs.wustl.edu>

--- trunk/TAO/tests/Exposed_Policies/Counter_i.cpp
+++ trunk/TAO/tests/Exposed_Policies/Counter_i.cpp
@@ -2,9 +2,6 @@
 
 #include "Counter_i.h"
 
-// @@ Angelo, please add rcsid string here.
-// @@ Marina DONE.
-
 ACE_RCSID(tao, Counter_Servant, "$Id$")
 
 // Dtor-Ctor Implementation
@@ -12,8 +9,6 @@ ACE_RCSID(tao, Counter_Servant, "$Id$")
 Counter_Servant::Counter_Servant (CORBA::ORB_ptr o)
   : count_ (0),
     orb_ (CORBA::ORB::_duplicate (o))
-  // @@ Angelo, remove extra spaces in paranthesis.
-  // @@ Marina Done.
 {
 }
 
@@ -21,10 +16,11 @@ Counter_Servant::~Counter_Servant (void)
 {
 }
 
-
 // Counter Interface Methods Implementation
-// @@ Angelo, please fix the class name.
-// @@ Marina DONE.
+
+// @@ Angelo, you are going to get warnings about unused environment
+// parameter in the methods below.
+
 void
 Counter_Servant::increment (CORBA::Environment &ACE_TRY_ENV)
 {
@@ -37,10 +33,14 @@ Counter_Servant::get_count (CORBA::Environment &ACE_TRY_ENV)
   return this->count_;
 }
 
+// @@ Angelo, it is important that thowspecs in .h file match
+// throwspecs in .cpp file, which is not the case for the method
+// below.  You will get warnings here.
 void
 Counter_Servant::shutdown (CORBA::Environment &ACE_TRY_ENV)
 {
   this->orb_->shutdown ();
 }
 
-
+// @@ Angelo, please *use* environment parameter to catch exceptions,
+// i.e., ORB::shutdown takes environment argument - use it!
--- trunk/TAO/tests/Exposed_Policies/server.cpp
+++ trunk/TAO/tests/Exposed_Policies/server.cpp
@@ -3,15 +3,16 @@
 #include "Counter_i.h"
 #include <tao/RT_ORB.h>
 const char *ior_output_file = "ec.ior";
+// @@ Angelo, lets change the name of ior file above.
 
 //////////////////////////////////////////////////////////////////
 //
-// NOTE: In this test there are different work-around,that were
-// needed to access some functionalities that weren't available
-// by following the standard way.
-// To point out this code fragment the following convention will
+// NOTE: In this test there are different work-arounds, that were
+// needed to access some functionality not available at the time of
+// writing the test.
+// To point out these workarounds, the following convention will
 // be used:
-// 
+//
 //   @@ Workaround <name>
 //
 //   @@ Real Code <name>
@@ -26,12 +27,17 @@ main (int argc, char *argv[])
       // ORB Initialization.
       CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, "", ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      
+
       CORBA::Object_var object;
-      
+
        // @@ Workaround <RTORB access>
-      RTCORBA::RTORB_var rt_orb = new  TAO_RT_ORB();
-      
+      RTCORBA::RTORB_var rt_orb = new TAO_RT_ORB();
+
+      // @@ Angelo, the line above is just a temporary code, so it's
+      // ok.  However, in general, 1) do not use naked operator 'new'
+      // - use ACE_NEW & friends; 2) don't allocate memory where not
+      // neccessary, i.e., 'TAO_RT_ORB rt_orb;' will do just fine.
+
       //   @@ Real Code <RTORB access>
 
       /*
@@ -42,39 +48,51 @@ main (int argc, char *argv[])
                                                             ACE_TRY_ENV);
        ACE_TRY_CHECK;
        */
-      
+
       CORBA::PolicyList policy_list;
       policy_list.length (1);
 
       RTCORBA::Priority priority = 256;
-      policy_list[0] = 
+      policy_list[0] =
         rt_orb->create_priority_model_policy (RTCORBA::SERVER_DECLARED,
                                               priority,
                                               ACE_TRY_ENV);
-      
+      // @@ Angelo, don't forget to check 'ACE_TRY_CHECK' every time
+      // you pass environment to a method.
+
       object = orb->resolve_initial_references ("RootPOA", ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      
-      PortableServer::POA_var poa
-        = PortableServer::POA::_narrow (object.in (), ACE_TRY_ENV);
+
+      // @@ Angelo, please get into habit of thorough error checking,
+      // e.g., it is good to check at this point that object is not nil.
+
+      PortableServer::POA_var poa =
+        PortableServer::POA::_narrow (object.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      
+
       // Create a child POA with a given set of
       // policies.
-      PortableServer::POAManager_var poa_mgr = 
+      // @@ Angelo, why is this POA manager set to nil?  There is much
+      // more confusion with poa managers at the end of main ...
+      PortableServer::POAManager_var poa_mgr =
         PortableServer::POAManager::_nil ();
-      
-      PortableServer::POA_var child_poa = 
-        poa->create_POA ("Child_POA", 
-                         poa_mgr , 
+
+      PortableServer::POA_var child_poa =
+        poa->create_POA ("Child_POA",
+                         poa_mgr ,
                          policy_list);
-      
+
+
+      // @@ Angelo, the lines until TODO comment are garbage.
+      // Why isn't the real code here yet?
+
       // Create a Corba Object reference.
       int i = 0;
-      object = 
-        child_poa->create_reference ("IDL:omg.org/CORBA/Object:1.0", ACE_TRY_ENV);
+      object =
+        child_poa->create_reference ("IDL:omg.org/CORBA/Object:1.0",
+                                     ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      
+
       if (CORBA::is_nil (object))
         {
           ACE_DEBUG ((LM_DEBUG, "Unable to create a Counter Object!"));
@@ -82,23 +100,13 @@ main (int argc, char *argv[])
         }
       Counter_var counter = Counter::_narrow (object.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      
+
       Counter_Servant servant (orb.in ());
 
       // TODO: Incarnate the CORBA Object with  servant.
-      
 
-      // @@ Angelo, please create child poa with RT policies, and
-      // register two objects under it...
-      // Also, please include all the test code in the file, and just
-      // comment out what's not working at the moment.
-
-      // @@ Marina OK.
-     
-                  
-      CORBA::String_var ior = 
+      CORBA::String_var ior =
         orb->object_to_string (counter.in (), ACE_TRY_ENV);
-      
       ACE_TRY_CHECK;
 
       ACE_DEBUG ((LM_DEBUG, "Activated as <%s>\n", ior.in ()));
@@ -111,12 +119,11 @@ main (int argc, char *argv[])
                           1);
       ACE_OS::fprintf (output_file, "%s", ior.in ());
       ACE_OS::fclose (output_file);
-      
-      PortableServer::POAManager_var poa_manager
-        = child_poa->the_POAManager(ACE_TRY_ENV);
-      
+
+      PortableServer::POAManager_var poa_manager =
+        child_poa->the_POAManager (ACE_TRY_ENV);
       ACE_TRY_CHECK;
-          
+
       the_poa_mngr->activate (ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
@@ -125,8 +132,6 @@ main (int argc, char *argv[])
 
       poa->destroy (1, 1, ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      // @@ Angelo, check if this method needs ACE_TRY_ENV argument.
-      // @@ Marina Done
     }
 
   ACE_CATCHANY
