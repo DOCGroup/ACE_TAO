dad068afce003b39c27651f9ad8873a7bef238db
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-19 09:17:26 +0000

    Mon Jun 19 03:57:37 2000  Luther J Baker  <luther@cs.wustl.edu>

---
+++ PACE/pace/posix/math.h
@@ -0,0 +1,32 @@
+/* $Id$
+
+ * ============================================================================
+ *
+ * = LIBRARY
+ *    pace
+ *
+ * = FILENAME
+ *    pace/posix/math.h
+ *
+ * = AUTHOR
+ *    Luther Baker
+ *
+ * ============================================================================ */
+
+#ifndef PACE_MATH_H
+#define PACE_MATH_H
+
+#if defined (PACE_HAS_CPLUSPLUS)
+extern "C" {
+#endif /* PACE_HAS_CPLUSPLUS */
+
+
+#if defined (PACE_HAS_CPLUSPLUS)
+}
+#endif /* PACE_HAS_CPLUSPLUS */
+
+#if defined (PACE_HAS_INLINE)
+# include "pace/posix/math.inl"
+#endif /* PACE_HAS_INLINE */
+
+#endif /* PACE_MATH_H */
