acae5a30222fd9988b147e3858b93ccee70bb1e9
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-19 09:17:26 +0000

    Mon Jun 19 03:57:37 2000  Luther J Baker  <luther@cs.wustl.edu>

---
+++ PACE/pace/ctype.h
@@ -0,0 +1,30 @@
+/* $Id$
+
+ * ============================================================================
+ *
+ * = LIBRARY
+ *    pace
+ *
+ * = FILENAME
+ *    pace/ctype.h
+ *
+ * = AUTHOR
+ *    Luther Baker
+ *
+ * ============================================================================ */
+
+#ifndef PACE_CTYPE_H_INDIRECT
+#define PACE_CTYPE_H_INDIRECT
+
+#if defined (PACE_HAS_POSIX)
+# include "pace/posix/ctype.h"
+#elif defined (PACE_VXWORKS)
+# include "pace/vxworks/ctype.h"
+#elif defined (PACE_WIN32)
+# include "pace/win32/ctype.h"
+#endif
+
+#endif /* PACE_CTYPE_H_INDIRECT */
+
+
+
