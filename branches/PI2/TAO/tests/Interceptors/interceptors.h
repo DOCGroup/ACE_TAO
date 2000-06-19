8ea5adf0442bcfebabd659c7eba58aabf2df8d96
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-19 16:06:49 +0000

    removed Request_Info inclusion since uits there in corba.h

--- branches/PI2/TAO/tests/Interceptors/interceptors.h
+++ branches/PI2/TAO/tests/Interceptors/interceptors.h
@@ -4,7 +4,6 @@
 #define TAO_INTERCEPTORS_H
 
 #include "tao/corba.h"
-#include "Request_Info.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
@@ -17,9 +16,14 @@
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
 
+// Forward Declarations
+
+class PortableInterceptor::ClientRequestInfo;
+class PortableInterceptor::ServerRequestInfo;
+
 class Echo_Client_Request_Interceptor
 :  public PortableInterceptor::ClientRequestInterceptor//,
-//     public CORBA::LocalObject
+   //  public CORBA::LocalObject
 {
   // = Client-side echo interceptor.  For checking interceptor visually only.
 public:
