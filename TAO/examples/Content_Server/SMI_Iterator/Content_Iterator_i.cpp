8f348ce582612c7743e24d2add45d1da9cd20de9
Ossama Othman <ossama-othman@users.noreply.github.com>
2000-06-19 22:42:42 +0000

    ChangeLogTag:Mon Jun 19 15:36:38 2000  Ossama Othman  <ossama@uci.edu>

--- TAO/examples/Content_Server/SMI_Iterator/Content_Iterator_i.cpp
+++ TAO/examples/Content_Server/SMI_Iterator/Content_Iterator_i.cpp
@@ -3,24 +3,26 @@
 
 // Ossama Othman <ossama@uci.edu>
 
+#include "ace/FILE_Connector.h"
 #include "Content_Iterator_i.h"
 
-ACE_RCSID(ECE255, Content_Iterator_i, "$Id$")
+ACE_RCSID(SMI_Iterator, Content_Iterator_i, "$Id$")
 
 
-Content_Iterator_i::Content_Iterator_i (ACE_HANDLE handle,
+Content_Iterator_i::Content_Iterator_i (const char * pathname,
                                         CORBA::ULong file_size)
-  : handle_ (handle),
-    file_size_ (file_size),
-    chunk_index_ (1)
+  : file_ (pathname),
+    file_io_ (),
+    file_size_ (file_size), 
+    chunk_index_ (1),
+    initialized_ (0)
 {
   // Nothing else
 }
 
 Content_Iterator_i::~Content_Iterator_i (void)
 {
-  if (this->handle_ != ACE_INVALID_HANDLE)
-    ACE_OS::close (this->handle_);
+  (void) this->file_io_.close ();
 }
 
 CORBA::Boolean
@@ -32,12 +34,11 @@ Content_Iterator_i::next_chunk (CORBA::ULong offset,
   // Initialize/allocate the Chunk_Type sequence
   chunk = new Web_Server::Chunk_Type;
 
-  if (offset >= this->file_size_
-      || this->handle_ == ACE_INVALID_HANDLE)
+  if (offset >= this->file_size_)
     return 0;  // Applications shouldn't throw system exceptions.
 
   off_t real_offset =
-    ACE_OS::lseek (this->handle_,
+    ACE_OS::lseek (this->file_io_.get_handle (),
                    offset,
                    SEEK_SET);
 
@@ -57,8 +58,8 @@ Content_Iterator_i::next_chunk (CORBA::ULong offset,
       // @@ Is this precaution necessary?
 
       // Reset the file offset back to its original value.
-      
-      (void) ACE_OS::lseek (this->handle_,
+
+      (void) ACE_OS::lseek (this->file_io_.get_handle (),
                             real_offset,
                             SEEK_SET);
 
@@ -72,9 +73,8 @@ Content_Iterator_i::next_chunk (CORBA::ULong offset,
   CORBA::Octet * buf =
     Web_Server::Chunk_Type::allocbuf (BUFSIZ);
 
-  ssize_t bytes_read = ACE_OS::read (this->handle_,
-                                     buf,
-                                     BUFSIZ);
+  ssize_t bytes_read = this->file_io_.recv (buf,
+                                            BUFSIZ);
 
   if (bytes_read == -1)
     {
@@ -108,11 +108,10 @@ void
 Content_Iterator_i::destroy (CORBA::Environment &ACE_TRY_ENV)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  if (this->handle_ != ACE_INVALID_HANDLE)
-    {
-      ACE_OS::close (this->handle_);
-      this->handle_ = ACE_INVALID_HANDLE;
-    }
+
+  (void) this->file_io_.close ();
+
+  this->initialized_ = 0;
 
   // Get the POA used when activating the Content_Iterator object.
   PortableServer::POA_var poa = this->_default_POA (ACE_TRY_ENV);
@@ -131,3 +130,28 @@ Content_Iterator_i::destroy (CORBA::Environment &ACE_TRY_ENV)
   this->_remove_ref ();
 }
 
+int
+Content_Iterator_i::init (void)
+{
+  // Open the requested file.
+  ACE_FILE_Connector connector;
+
+  if (connector.connect (this->file_io_,
+                         this->file_,
+                         0,
+                         ACE_Addr::sap_any,
+                         0,
+                         O_RDONLY) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         ACE_TEXT ("%s %p\n"),
+                         ACE_TEXT ("Could not open file"),
+                         this->file_.get_path_name ()),
+                        -1);
+    }
+
+
+  this->initialized_ = 1;
+
+  return 0;
+}
