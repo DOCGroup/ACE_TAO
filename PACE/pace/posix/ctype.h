f6c28a9c73ca281ebb07f81b0c884b820610f24b
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-19 10:05:34 +0000

    Mon Jun 19 05:03:53 2000  Luther J Baker  <luther@cs.wustl.edu>

--- PACE/pace/posix/ctype.h
+++ PACE/pace/posix/ctype.h
@@ -20,7 +20,32 @@
 extern "C" {
 #endif /* PACE_HAS_CPLUSPLUS */
 
+  PACE_INLINE int pace_isalnum (int c);
 
+  PACE_INLINE int pace_isalpha (int c);
+
+  PACE_INLINE int pace_iscntrl (int c);
+
+  PACE_INLINE int pace_isdigit (int c);
+
+  PACE_INLINE int pace_isgraph (int c);
+
+  PACE_INLINE int pace_islower (int c);
+
+  PACE_INLINE int pace_isprint (int c);
+
+  PACE_INLINE int pace_ispunct (int c);
+
+  PACE_INLINE int pace_isspace (int c);
+
+  PACE_INLINE int pace_isupper (int c);
+
+  PACE_INLINE int pace_isxdigit (int c);
+
+  PACE_INLINE int pace_tolower (int c);
+
+  PACE_INLINE int pace_toupper (int c);
+  
 #if defined (PACE_HAS_CPLUSPLUS)
 }
 #endif /* PACE_HAS_CPLUSPLUS */
--- PACE/pace/posix/math.h
+++ PACE/pace/posix/math.h
@@ -20,6 +20,49 @@
 extern "C" {
 #endif /* PACE_HAS_CPLUSPLUS */
 
+  PACE_INLINE double pace_acos (double x);
+
+  PACE_INLINE double pace_asin (double x);
+
+  PACE_INLINE double pace_atan (double x);
+
+  PACE_INLINE double pace_atan2 (double x, double y);
+
+  PACE_INLINE double pace_cos (double x);
+
+  PACE_INLINE double pace_sin (double x);
+
+  PACE_INLINE double pace_tan (double x);
+
+  PACE_INLINE double pace_cosh (double x);
+
+  PACE_INLINE double pace_sinh (double x);
+
+  PACE_INLINE double pace_tanh (double x);
+
+  PACE_INLINE double pace_exp (double x);
+
+  PACE_INLINE double pace_frexp (double x, int * exp);
+
+  PACE_INLINE double pace_ldexp (double x, int exp);
+
+  PACE_INLINE double pace_log (double x);
+
+  PACE_INLINE double pace_log10 (double x);
+
+  PACE_INLINE double pace_modf (double x, double * iptr);
+
+  PACE_INLINE double pace_pow (double x);
+
+  PACE_INLINE double pace_sqrt (double x);
+
+  PACE_INLINE double pace_ceil (double x);
+
+  PACE_INLINE double pace_fabs (double x);
+
+  PACE_INLINE double pace_floor (double x);
+
+  PACE_INLINE double pace_fmod (double x, double y);
 
 #if defined (PACE_HAS_CPLUSPLUS)
 }
