faca3b325f5c7a69261d76aebe223e3fcd728e88
Ossama Othman <ossama-othman@users.noreply.github.com>
2000-06-22 00:23:06 +0000

    ChangeLogTag:Wed Jun 21 17:15:44 2000  Ossama Othman  <ossama@uci.edu>

--- TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Export.h
+++ TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Export.h
@@ -2,14 +2,12 @@
 // $Id$
 // Definition for Win32 Export directives.
 // This file is generated automatically by
-// ${ACE_ROOT}/GenExportH.BAT
+// generate_export_file.pl
 // ------------------------------
-#ifndef TAO_SSLIOP_EXPORT_H
+#if !defined (TAO_SSLIOP_EXPORT_H)
 #define TAO_SSLIOP_EXPORT_H
 
-#include "ace/pre.h"
-
-#include "ace/OS.h"
+#include "ace/config-all.h"
 
 #if !defined (TAO_SSLIOP_HAS_DLL)
 #define TAO_SSLIOP_HAS_DLL 1
@@ -20,20 +18,22 @@
 #    if defined (TAO_SSLIOP_BUILD_DLL)
 #      define TAO_SSLIOP_Export ACE_Proper_Export_Flag
 #      define TAO_SSLIOP_SINGLETON_DECLARATION(T) ACE_EXPORT_SINGLETON_DECLARATION (T)
+#      define TAO_SSLIOP_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
 #    else
 #      define TAO_SSLIOP_Export ACE_Proper_Import_Flag
 #      define TAO_SSLIOP_SINGLETON_DECLARATION(T) ACE_IMPORT_SINGLETON_DECLARATION (T)
+#      define TAO_SSLIOP_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
 #    endif /* TAO_SSLIOP_BUILD_DLL */
 #  else
 #    define TAO_SSLIOP_Export
 #    define TAO_SSLIOP_SINGLETON_DECLARATION(T)
+#    define TAO_SSLIOP_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
 #  endif   /* ! TAO_SSLIOP_HAS_DLL == 1 */
 #else
 #  define TAO_SSLIOP_Export
 #  define TAO_SSLIOP_SINGLETON_DECLARATION(T)
+#  define TAO_SSLIOP_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
 #endif     /* TAO_SSLIOP_HAS_DLL */
-
-#include "ace/post.h"
 
 #endif     /* TAO_SSLIOP_EXPORT_H */
 
