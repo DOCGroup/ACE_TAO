c9b5dacfdada24755bbd10776290f96527bcf5cf
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-28 19:04:10 +0000

    This commit was manufactured by cvs2svn to create branch 'PI2'.

---
+++ branches/PI2/TAO/tao/Policy_Factory.cpp
@@ -0,0 +1,11 @@
+// $Id$
+ 
+#include "tao/Policy_Factory.h"
+
+
+#if !defined (__ACE_INLINE__)
+#include "tao/Policy_Factory.i"
+#endif // __ACE_INLINE__ 
+
+ACE_RCSID(TAO, Policy_Factory, "$Id$")
+
