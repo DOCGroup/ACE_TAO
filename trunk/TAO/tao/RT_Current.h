b65cc34a6dcf75fc963c470e0337c316cdfa52ef
marina <marina@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-28 04:29:10 +0000

    ChangeLogTag:Tue Jun 27 23:27:46 2000  Marina Spivak  <marina@cs.wustl.edu>

--- trunk/TAO/tao/RT_Current.h
+++ trunk/TAO/tao/RT_Current.h
@@ -28,6 +28,13 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+#if defined(_MSC_VER)
+#if (_MSC_VER >= 1200)
+#pragma warning(push)
+#endif /* _MSC_VER >= 1200 */
+#pragma warning(disable:4250)
+#endif /* _MSC_VER */
+
 class TAO_Export TAO_RT_Current :
   public RTCORBA::Current,
   public CORBA::LocalObject
@@ -63,6 +70,10 @@ private:
 #if defined (__ACE_INLINE__)
 # include "tao/RT_Current.i"
 #endif /* __ACE_INLINE__ */
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma warning(pop)
+#endif /* _MSC_VER */
 
 #endif /* TAO_HAS_RT_CORBA == 1 */
 
