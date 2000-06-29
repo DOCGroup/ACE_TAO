2cd7c776fb9e8b860d3484487b51126ed52b16a5
jwh1 <jwh1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-29 09:07:29 +0000

    Thu Jun 29 02:38:52 2000  John Heitmann  <jwh1@cs.wustl.edu>

--- PACE/pace/posix/stdio.c
+++ PACE/pace/posix/stdio.c
@@ -14,51 +14,6 @@
  * ============================================================================= */
 
 #include "pace/stdio.h"
-#include <stdarg.h>
-
-int
-pace_fprintf (FILE *fp, const char *format, ...)
-{
-  int result = 0;
-  va_list ap;
-  va_start (ap, format);
-  result = vfprintf (fp, format, ap);
-  va_end (ap);
-  return result;
-}
-
-int
-pace_printf (const char* format, ...)
-{
-  int result = 0;
-  va_list ap;
-  va_start (ap, format);
-  result = vprintf (format, ap);
-  va_end (ap);
-  return result;
-}
-
-int
-pace_sprintf (char* s, const char* format, ...)
-{
-  int result = 0;
-  va_list ap;
-  va_start (ap, format);
-  result = vsprintf (s, format, ap);
-  va_end (ap);
-  return result;
-}
-
-int
-pace_snprintf (char *s, size_t  n, const char *format, ...)
-{
-  int result = 0;
-  va_list ap;
-  va_start (ap, format);
-  result = vsnprintf (s, n, format, ap);
-  va_end (ap);
-  return result;
-}
 
 #if !defined (PACE_HAS_INLINE)
 # include "pace/posix/stdio.inl"
