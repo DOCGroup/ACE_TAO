b2c46739f9a84852f985e07c02bc585ab5ce1dfd
jwh1 <jwh1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-29 07:40:37 +0000

    Thu Jun 29 02:38:52 2000  John Heitmann  <jwh1@cs.wustl.edu>

--- PACE/pace/posix/unistd.c
+++ PACE/pace/posix/unistd.c
@@ -12,8 +12,41 @@
  *    Luther Baker
  *
  * ============================================================================= */
-
+#include <stdarg.h>
 #include "pace/unistd.h"
+
+int
+execl (const char* path, const char* arg, ...)
+{
+  int result = 0;
+  va_list ap;
+  va_start (ap, arg);
+  result = pace_execv (path, ap);
+  va_end (ap);
+  return result;
+}
+
+int
+execle (const char* path, const char* arg, ...)
+{
+  int result = 0;
+  va_list ap;
+  va_start (ap, arg);
+  result = pace_execve (path, ap, 0);
+  va_end (ap);
+  return result;
+}
+
+int
+execlp (const char* file, const char* arg,  ...)
+{
+  int result = 0;
+  va_list ap;
+  va_start (ap, arg);
+  result = pace_execvp (file, ap);
+  va_end (ap);
+  return result;
+}
 
 #if !defined (PACE_HAS_INLINE)
 # include "pace/posix/unistd.inl"
