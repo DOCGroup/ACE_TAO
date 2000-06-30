37ac638bc63d582b9e1a5e7f01c6ba88088aa302
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-30 03:01:52 +0000

    interceptor changes for exceptionlist

--- TAO/TAO_IDL/be_include/be_visitor_operation/interceptors_cs.h
+++ TAO/TAO_IDL/be_include/be_visitor_operation/interceptors_cs.h
@@ -8,39 +8,39 @@
 //    TAO IDL
 //
 // = FILENAME
-//    interecptors_cs.h
+//    interceptors_cs.h
 //
 // = DESCRIPTION
 //    Visitor for generating code for IDL operations. This is a generic visitor
-//    that calls visitors for the Argument class.
+//    that calls visitors for the operation class.
 //
 // = AUTHOR
 //    Kirthika Parameswaran  <kirthika@cs.wustl.edu>
 //
 // ============================================================================
 
-#ifndef _BE_VISITOR_OPERATION_INTERCEPTORS_ARGUMENT_CS_H_
-#define _BE_VISITOR_OPERATION_INTERCEPTORS_ARGUMENT_CS_H_
+#ifndef _BE_VISITOR_OPERATION_INTERCEPTORS_CS_H_
+#define _BE_VISITOR_OPERATION_INTERCEPTORS_CS_H_
 
 // ************************************************************
 // generic operation visitor for docall, upcall, pre/post
 // ************************************************************
 
-class be_visitor_operation_interceptors_argument_cs : public be_visitor_operation
+class be_visitor_operation_interceptors_cs : public be_visitor_operation
 {
   //
   // = TITLE
-  //   be_visitor_operation_interceptors_argument_cs
+  //   be_visitor_operation_interceptors_cs
   //
   // = DESCRIPTION
   //   This is a visitor to generate a variable declaration ofr an operation
   //   return type
   //
 public:
-  be_visitor_operation_interceptors_argument_cs (be_visitor_context *ctx);
+  be_visitor_operation_interceptors_cs (be_visitor_context *ctx);
   // constructor
 
-  ~be_visitor_operation_interceptors_argument_cs (void);
+  ~be_visitor_operation_interceptors_cs (void);
   // destructor
 
   virtual int visit_operation (be_operation *node);
@@ -48,4 +48,4 @@ public:
 
 };
 
-#endif /* _BE_VISITOR_OPERATION_INTERCEPTORS_ARGUMENT_H_ */
+#endif /* _BE_VISITOR_OPERATION_INTERCEPTORS_H_ */
