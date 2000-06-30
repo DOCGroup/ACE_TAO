83c9d10f511b793c0f016e472831c6dd57950a2b
joeh <joeh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-06-30 15:53:30 +0000

    Fri Jun 30 10:10:53 2000  Joe Hoffert  <joeh@cs.wustl.edu>

--- PACE/tests/Posix_SP_Test.c
+++ PACE/tests/Posix_SP_Test.c
@@ -7,11 +7,10 @@
  *
  * =  DESCRIPTION
  *     Testing the platform for POSIX_SINGLE_PROCESS unit of
- *     functionality. This consists of calls to execl, execle, execlp,
- *     execv, execve, execvp, fork, getenv, getpid, getppid, sleep,
- *     times, wait, waitpid, and _exit.
+ *     functionality. This consists of calls to sysconf, time, and
+ *     uname.
  *     This is not meant to be an exhaustive test at this point but more
- *     a sanity check that PACE works (at least somewhat) as advertised.
+ *     a sanity check that PACE works (at least somewhat).
  *
  * = AUTHOR
  *    Joe Hoffert <joeh@cs.wustl.edu>
@@ -33,18 +32,32 @@ main (int argc, char **argv)
   long retval;
   time_t local_time;
   struct utsname name;
+  int confname = _SC_AIO_LISTIO_MAX;
 
   /* Call pace_sysconf() */
-  retval = pace_sysconf(_SC_2_C_BIND);
+  errno = 0;
+  retval = pace_sysconf(confname);
 
-  if (retval != _POSIX2_C_BIND)
+  /* Return value of -1 indicates an invalid name or no support
+     for the functionality associated with the name. In either
+     case, POSIX platforms should not return -1 for the name
+     above.
+   */
+  if (retval == -1)
     {
       printf("pace_sysconf %s\n", failure);
       printf("retval == %ld\n", retval);
+      if (errno == 0)
+        {
+          printf("Unsupported name [errno did not change]");
+        }
+      printf("\n");
       return -1;
     }
 
   printf("pace_sysconf %s\n", success);
+  printf("pace_sysconf: name == %d\n", confname);
+  printf("pace_sysconf: value == %ld\n\n", retval);
 
   /* Call pace_time() */
   local_time = 0;
@@ -54,22 +67,24 @@ main (int argc, char **argv)
   if (retval == -1)
     {
       printf("pace_time %s\n", failure);
-      printf("errno == %d\n", errno);
+      printf("errno == %d\n\n", errno);
       return -1;
     }
 
   printf("pace_time %s\n", success);
+  printf("pace_time == %ld\n\n", local_time);
 
   /* Call pace_uname() */
   retval = pace_uname(&name);
 
-  if (retval <= -1)
+  if (retval < 0)
     {
-      printf("pace_uname %s\n", failure);
+      printf("pace_uname %s\n\n", failure);
       return -1;
     }
 
   printf("pace_uname %s\n", success);
+  printf("pace_uname: uname.sysname == %s\n\n", name.sysname);
 
   PACE_UNUSED_ARG (argc);
   PACE_UNUSED_ARG (argv);
