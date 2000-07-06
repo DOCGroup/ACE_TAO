6c11f7ee1807e220d118b6e29b4acfffbec968f7
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-06 17:42:57 +0000

    New IFR implementation files.

---
+++ TAO/orbsvcs/IFR_Service/IFR_Server.cpp
@@ -0,0 +1,42 @@
+/* -*- C++ -*- */
+// $Id$
+
+#include "IFR_Service.h"
+
+ACE_RCSID(IFR_Service, IFR_Server, "$Id$")
+
+int
+main (int argc, char *argv[])
+{
+  IFR_Service server;
+
+  ACE_DEBUG ((LM_DEBUG, "TAO Interface Repository\n"));
+
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
+    {
+      if (server.init (argc, argv, ACE_TRY_ENV) == -1)
+        return 1;
+      else
+        {
+          server.run (ACE_TRY_ENV);
+          ACE_TRY_CHECK;
+
+          if (server.fini (ACE_TRY_ENV) == -1)
+            return 1;
+          ACE_TRY_CHECK;
+        }
+    }
+  ACE_CATCH (CORBA::SystemException, sysex)
+    {
+      ACE_PRINT_EXCEPTION (sysex, "System Exception");
+      return -1;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "Unknown Exception");
+      return -1;
+    }
+  ACE_ENDTRY;
+  return 0;
+}
