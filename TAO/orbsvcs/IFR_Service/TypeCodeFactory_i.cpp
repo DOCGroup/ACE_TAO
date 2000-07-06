5a8d02396a85b827da2962b2381c0aef8976c12e
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-06 17:00:54 +0000

    Commented out argument names in the TODO functions, and added an initialization to prevent egcs warnings.

--- TAO/orbsvcs/IFR_Service/TypeCodeFactory_i.cpp
+++ TAO/orbsvcs/IFR_Service/TypeCodeFactory_i.cpp
@@ -383,9 +383,9 @@ TAO_TypeCodeFactory_i::create_wstring_tc (
 
 CORBA::TypeCode_ptr 
 TAO_TypeCodeFactory_i::create_fixed_tc (
-    CORBA::UShort digits,
-    CORBA::UShort scale,
-    CORBA::Environment &ACE_TRY_ENV
+    CORBA::UShort /* digits */,
+    CORBA::UShort /* scale */,
+    CORBA::Environment & /* ACE_TRY_ENV */
   )
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
@@ -455,12 +455,12 @@ TAO_TypeCodeFactory_i::create_array_tc (
 
 CORBA::TypeCode_ptr 
 TAO_TypeCodeFactory_i::create_value_tc (
-    const char *id,
-    const char *name,
+    const char * /* id */,
+    const char * /* name */,
     CORBA::ValueModifier type_modifier,
-    CORBA::TypeCode_ptr concrete_base,
-    const IR::ValueMemberSeq &members,
-    CORBA::Environment &ACE_TRY_ENV
+    CORBA::TypeCode_ptr /* concrete_base */,
+    const IR::ValueMemberSeq & /* members */,
+    CORBA::Environment & /* ACE_TRY_ENV */
   )
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
@@ -535,8 +535,8 @@ TAO_TypeCodeFactory_i::create_native_tc (
 
 CORBA::TypeCode_ptr 
 TAO_TypeCodeFactory_i::create_recursive_tc (
-    const char *id,
-    CORBA::Environment &ACE_TRY_ENV
+    const char * /* id */,
+    CORBA::Environment & /* ACE_TRY_ENV */
   )
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
@@ -546,9 +546,9 @@ TAO_TypeCodeFactory_i::create_recursive_tc (
 
 CORBA::TypeCode_ptr 
 TAO_TypeCodeFactory_i::create_abstract_interface_tc (
-    const char *id,
-    const char *name,
-    CORBA::Environment &ACE_TRY_ENV
+    const char * /* id */,
+    const char * /* name */,
+    CORBA::Environment & /* ACE_TRY_ENV */
   )
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
@@ -558,9 +558,9 @@ TAO_TypeCodeFactory_i::create_abstract_interface_tc (
 
 CORBA::TypeCode_ptr 
 TAO_TypeCodeFactory_i::create_component_tc (
-    const char *id,
-    const char *name,
-    CORBA::Environment &ACE_TRY_ENV
+    const char * /* id */,
+    const char * /* name */,
+    CORBA::Environment & /* ACE_TRY_ENV */
   )
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
@@ -570,9 +570,9 @@ TAO_TypeCodeFactory_i::create_component_tc (
 
 CORBA::TypeCode_ptr 
 TAO_TypeCodeFactory_i::create_home_tc (
-    const char *id,
-    const char *name,
-    CORBA::Environment &ACE_TRY_ENV
+    const char * /* id */,
+    const char * /* name */,
+    CORBA::Environment & /* ACE_TRY_ENV */
   )
     ACE_THROW_SPEC ((CORBA::SystemException))
 {
@@ -599,8 +599,11 @@ TAO_TypeCodeFactory_i::compute_default_label (CORBA::TCKind kind,
     CORBA::ULong ulong_val;
     CORBA::ULongLong ulonglong_val;
     CORBA::ULong enum_val;
-    // TODO - handle longlong types
+    // TODO - handle (u)longlong types
   } dv, u;
+
+  // To prevent 'may be uninitialized' warnings.
+  dv.ulonglong_val = 0;
 
   // Set these to the minimum value they can have.
   switch (kind)
