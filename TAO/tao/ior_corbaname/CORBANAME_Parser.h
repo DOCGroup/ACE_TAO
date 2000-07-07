efecd46810155fee77711468cb1cc3aa4dc0bdf3
doccvs <doccvs@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-07 01:53:28 +0000

    Thu Jul  6 18:48:06 2000  Priyanka Gontla  <pgontla@ece.uci.edu>

---
+++ TAO/tao/ior_corbaname/CORBANAME_Parser.h
@@ -0,0 +1,65 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//   TAO
+//
+// = FILENAME
+//   CORBANAME_Parser.h
+//
+// = AUTHOR
+//   Priyanka Gontla (pgontla@uci.edu)
+//
+// ============================================================================
+
+#ifndef TAO_CORBANAME_PARSER_H
+#define TAO_CORBANAME_PARSER_H
+#include "ace/pre.h"
+
+#include "tao/IOR_Parser.h"
+#include "ior_corbaname_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+class TAO_IOR_CORBANAME_Export TAO_CORBANAME_Parser : public TAO_IOR_Parser
+{
+  // = TITLE
+  //   Implements the <corbaname:> IOR format
+  //
+  // = DESCRIPTION
+  //   This class implements the <corbaname:> IOR format.
+  //   It is dynamically loaded by the ORB and used to get reference
+  //   to a naming service at the given address and port and then resolve an
+  //   object in that context.
+  //
+public:
+  TAO_CORBANAME_Parser (void);
+  // Constructor
+
+  virtual ~TAO_CORBANAME_Parser (void);
+  // The destructor
+
+  // = The IOR_Parser methods, please read the documentation in
+  //   IOR_Parser.h
+  virtual int match_prefix (const char *ior_string) const;
+  virtual CORBA::Object_ptr parse_string (const char *ior,
+                                          CORBA::ORB_ptr orb,
+                                          CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+ private:
+  virtual void parse_string_count_helper (const char * &corbaname,
+                                          CORBA::ULong &pos_seperator);
+};
+
+#if defined (__ACE_INLINE__)
+# include "CORBANAME_Parser.i"
+#endif /* __ACE_INLINE__ */
+
+ACE_FACTORY_DECLARE (TAO_IOR_CORBANAME, TAO_CORBANAME_Parser)
+
+#include "ace/post.h"
+#endif /* TAO_CORBANAME_PARSER_H */
