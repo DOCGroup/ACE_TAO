62253b760854c0ccfb1638eeda0b339f104d2feb
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-07 18:01:00 +0000

    added soem changes to put the typecode of teh exceptioin into the exception list

--- branches/PI2/TAO/TAO_IDL/be/be_visitor_operation/interceptors_exceptlist.cpp
+++ branches/PI2/TAO/TAO_IDL/be/be_visitor_operation/interceptors_exceptlist.cpp
@@ -56,8 +56,6 @@ be_visitor_operation_interceptors_exceptlist::visit_operation (be_operation *nod
     {
     case TAO_CodeGen::TAO_OPERATION_INTERCEPTORS_EXCEPTLIST:
       return this->gen_exceptlist (node);
-    case TAO_CodeGen::TAO_OPERATION_INTERCEPTORS_EXCEPTION_ID:
-      return this->gen_exception_id (node);
     default:
       {
         ACE_ERROR_RETURN ((LM_ERROR,
@@ -83,60 +81,60 @@ be_visitor_operation_interceptors_exceptlist::gen_exceptlist (be_operation *node
 
   // Start with the current indentation level.
   os->indent ();
+  // Generate the exception data array.
+  *os << "static TAO_Exception_Data " << "_tao_" << node->flat_name ()
+      << "_exceptiondata [] = " << be_nl;
+  *os << "{" << be_idt_nl;
+  // initialize an iterator to iterate thru the exception list
+  UTL_ExceptlistActiveIterator *ei;
+  ACE_NEW_RETURN (ei,
+                  UTL_ExceptlistActiveIterator (node->exceptions ()),
+                  -1);
+  // continue until each element is visited
 
-  *os <<" CORBA::ULong length = this->exception_list_.length ();"<<be_nl
-      << "for (CORBA::Ulong i = 0;; ++i)" << be_nl
+  while (!ei->is_done ())
+    {
+      be_exception *excp = be_exception::narrow_from_decl (ei->item ());
+      
+      if (excp == 0)
+        {
+          delete ei;
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "(%N:%l) be_visitor_operation_exceptlist_cs"
+                             "visit_operation - "
+                             "codegen for scope failed\n"), -1);
+          
+        }
+      *os << "{";
+      // the typecode name
+      *os << excp->tc_name ();
+      *os << ", ";
+      // allocator method
+      *os << excp->name () << "::_alloc}";
+      ei->next ();
+      if (!ei->is_done ())
+        {
+          *os << ",\n";
+          os->indent ();
+        }
+      // except the last one is processed?
+      
+    } // end of while loop
+  delete ei;
+  *os << be_uidt_nl << "};\n\n";
+   
+  long excp_count = (node->exceptions())->length ();
+  *os <<" CORBA::ULong length = 0;"<<be_nl
+      << "for (CORBA::ULong i = 0;i < "<< excp_count <<"; ++i)" << be_nl
       << " {" << be_idt_nl
       << " CORBA::TypeCode_ptr tcp = _tao_" << node->flat_name ()
       << "_exceptiondata [i].tc;" << be_nl
-      << "// We stop add to the exception list once we strike a void entry."<<be_nl
-      << " if (tcp->kind () == CORBA::_tc_null)"<< be_idt_nl
-      << "break;"<<be_uidt_nl
+      << "length = this->exception_list_.length ();" << be_nl
       << "this->exception_list_.length (length + 1);"<<be_nl
-      << "this->exception_list_[length] = *tcp;" << be_uidt_nl
+      << "TAO_Pseudo_Object_Manager<CORBA::TypeCode,CORBA::TypeCode_var> tcp_object (&tcp, 1);"<<be_nl
+      << "this->exception_list_[length] = tcp_object;" << be_uidt_nl
       << " }\n";
 
 return 0;
 }
 
-int
-be_visitor_operation_interceptors_exceptlist::gen_exception_id (be_operation *node)
-{
-  TAO_OutStream *os; // output stream
-  be_type *bt;       // type node
-
-  os = this->ctx_->stream ();
-  this->ctx_->node (node); // save the node for future use
-
-  // Start with the current indentation level.
-  os->indent ();
-
-  // For received_exception id
-  *os << "// we have the is_system_exception boolean" << be_nl
-      << "// the byte_order boolean and" << be_nl
-      << "// the marshaled_exception sequence of octet" << be_nl
-      << " TAO_InputCDR _tao_in ((const char*) "
-      << "this->marshaled_exception ().get_buffer ()," << be_nl
-      << "  this->marshaled_exception ().length ()," << be_nl
-      << "  this->byte_order ());" << be_nl << be_nl
-      << "CORBA::String_var type_id;" << be_nl << be_nl
-      << "if ((_tao_in >> type_id.inout ()) == 0)" << be_nl
-      << "  {" << be_nl
-      << "    // Could not demarshal the exception id, raise an local" << be_nl
-      << "    // CORBA::MARSHAL" << be_nl;
-
-  if (idl_global->use_raw_throw ())
-    *os << "    throw (CORBA::MARSHAL (TAO_DEFAULT_MINOR_CODE," << be_nl;
-  else
-    *os << "    ACE_THROW (CORBA::MARSHAL (TAO_DEFAULT_MINOR_CODE," << be_nl;
-
-  *os << "                               CORBA::COMPLETED_YES));" << be_nl
-      << "  }" << be_nl 
-      << "else "<< be_nl
-      << "  {"<<be_nl
-      << "return type_id._retn ()"<< be_nl
-      << "  }\n\n";
-
-  return 0;
-  
-}
