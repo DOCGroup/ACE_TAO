0e4bdc6de96427192ee8078c86fa74c637e46516
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-11 01:35:15 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_1_5'.

--- TAO/tao/UIOP_Transport.i
+++
@@ -1,8 +0,0 @@
-// -*- C++ -*-
-//$Id$
-
-ACE_INLINE void 
-TAO_UIOP_Client_Transport::use_lite (CORBA::Boolean flag)
-{
-  this->lite_flag_ = flag;
-}
--- TAO/tao/Version.h
+++
@@ -1,7 +0,0 @@
-// $Id$
-// This is an automatically generated file.
-
-#define TAO_MAJOR_VERSION 1
-#define TAO_MINOR_VERSION 1
-#define TAO_BETA_VERSION 4
-#define TAO_VERSION "1.1.4"
