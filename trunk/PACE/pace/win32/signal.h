0925d0f65ac731c03611c4ea25428b726a279dec
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-12 19:21:19 +0000

    Wed Jul 12 14:15:10 2000  Luther J Baker  <luther@cs.wustl.edu>

--- trunk/PACE/pace/win32/signal.h
+++ trunk/PACE/pace/win32/signal.h
@@ -25,9 +25,30 @@ extern "C" {
 #endif /* PACE_HAS_CPLUSPLUS */
 
   typedef sig_atomic_t pace_sig_atomic_t;
-  typedef sigset_t pace_sigset_t;
+
+#ifndef PACE_SIGSET_T
+#define PACE_SIGSET_T
+  typedef struct sigset_t
+  {
+    /* signal set type */
+    unsigned int sigbits_[4];
+  } pace_sigset_t;
+#endif /* PACE_SIGSET_T */
+
   typedef struct sigaction pace_s_sigaction;
-  typedef siginfo_t pace_siginfo_t;
+
+#ifndef PACE_SIGINFO_T
+#define PACE_SIGINFO_T
+  typedef struct siginfo_t
+  {
+    PACE_HANDLE si_handle_;
+    // Win32 HANDLE that has become signaled.
+
+    PACE_HANDLE *si_handles_;
+    // Array of Win32 HANDLEs all of which have become signaled.
+  } pace_siginfo_t;
+# endif /* PACE_SIGINFO_T */
+
   typedef struct sigevent pace_sigevent;
   typedef union sigval pace_sigval;
 
