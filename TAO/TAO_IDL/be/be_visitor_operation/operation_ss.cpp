357f3547ed8bc51c70bdfdccedc2e7341776bd2a
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-13 23:54:30 +0000

    changed INFO_ARGLIST_SS back from ARG_UPCALL_SS

--- TAO/TAO_IDL/be/be_visitor_operation/operation_ss.cpp
+++ TAO/TAO_IDL/be/be_visitor_operation/operation_ss.cpp
@@ -238,8 +238,8 @@ be_visitor_operation_ss::visit_operation (be_operation *node)
 
   // Generate the formal argument fields which are passed to the RequestInfo object
   ctx = *this->ctx_;
-  //  ctx.state (TAO_CodeGen::TAO_OPERATION_INTERCEPTORS_INFO_ARGLIST_SS);
-  ctx.state (TAO_CodeGen::TAO_OPERATION_ARG_UPCALL_SS);
+  ctx.state (TAO_CodeGen::TAO_OPERATION_INTERCEPTORS_INFO_ARGLIST_SS);
+  //  ctx.state (TAO_CodeGen::TAO_OPERATION_ARG_UPCALL_SS);
   visitor = tao_cg->make_visitor (&ctx);
 
   if ((!visitor) || (node->accept (visitor) == -1))
@@ -252,11 +252,6 @@ be_visitor_operation_ss::visit_operation (be_operation *node)
                         -1);
     }
   delete visitor;
-
-  // generate the CORBA::Environment parameter for the alternative mapping
-  if (!idl_global->exception_support ())
-      *os << ",\n";
-
   *os << ");\n";
 
   *os << "if (_tao_vfr.valid ())" << be_idt_nl
