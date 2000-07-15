ffb238ae00d27929b687b3163e42fed6960bf892
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-15 01:54:38 +0000

    Fri Jul 14 20:49:46 2000  Luther J Baker  <luther@cs.wustl.edu>

--- PACE/pace/config/utility.h
+++ PACE/pace/config/utility.h
@@ -28,4 +28,22 @@ return ( (errno = ENOTSUP), FAILVALUE)
 
 # define PACE_ERRNO_NO_SUPPORT() errno=ENOTSUP
 
+/* A couple useful inline functions for checking whether bits are
+   enabled or disabled.
+ */
+# define PACE_POW(X) (((X) == 0)?1:(X-=1,X|=X>>1,X|=X>>2,X|=X>>4,X|=X>>8,X|=X>>16,(++X)))
+# define PACE_EVEN(NUM) (((NUM) & 1) == 0)
+# define PACE_ODD(NUM) (((NUM) & 1) == 1)
+# define PACE_BIT_ENABLED(WORD, BIT) (((WORD) & (BIT)) != 0)
+# define PACE_BIT_DISABLED(WORD, BIT) (((WORD) & (BIT)) == 0)
+# define PACE_BIT_CMP_MASK(WORD, BIT, MASK) (((WORD) & (BIT)) == MASK)
+# define PACE_SET_BITS(WORD, BITS) (WORD |= (BITS))
+# define PACE_CLR_BITS(WORD, BITS) (WORD &= ~(BITS))
+
+/* Turn a number into a string. */
+# define PACE_ITOA(X) #X
+
+/* Create a string of a server address with a "host:port" format. */
+# define PACE_SERVER_ADDRESS(H,P) H":"P
+
 #endif /* PACE_CONFIG_UTILITY_H */
