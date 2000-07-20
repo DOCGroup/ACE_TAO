10e0dbb7ae9c958d49e1b68e64ccd0a9b94738c1
kirthika <kirthika@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-20 18:33:53 +0000

    ssize_t had to be chanegd to size_t

--- ace/Caching_Utility_T.cpp
+++ ace/Caching_Utility_T.cpp
@@ -157,7 +157,7 @@ ACE_Recyclable_Handler_Caching_Utility<KEY, VALUE, CONTAINER, ITERATOR, ATTRIBUT
   // upon the <purge_percent>.
   size_t entries_to_remove 
     = ACE_MAX (ACE_static_cast (size_t, 1),
-               ACE_static_cast(ssize_t, 
+               ACE_static_cast(size_t, 
                                ACE_static_cast(double, purge_percent) 
                                / 100 * current_map_size));
   
@@ -277,7 +277,7 @@ ACE_Refcounted_Recyclable_Handler_Caching_Utility<KEY, VALUE, CONTAINER, ITERATO
   // upon the <purge_percent>.
   size_t entries_to_remove 
     = ACE_MAX (ACE_static_cast (size_t, 1),
-               ACE_static_cast(ssize_t, 
+               ACE_static_cast(size_t, 
                                ACE_static_cast(double, purge_percent) 
                                / 100 * available_entries));
 
@@ -402,7 +402,7 @@ ACE_Handler_Caching_Utility<KEY, VALUE, CONTAINER, ITERATOR, ATTRIBUTES>::clear_
   // upon the <purge_percent>.
   size_t entries_to_remove 
     = ACE_MAX (ACE_static_cast (size_t, 1),
-               ACE_static_cast(ssize_t, 
+               ACE_static_cast(size_t, 
                                ACE_static_cast(double, purge_percent) 
                                / 100 * current_map_size));
   
