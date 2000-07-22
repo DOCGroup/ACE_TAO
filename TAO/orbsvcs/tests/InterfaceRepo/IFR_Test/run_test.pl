6567ffcd66e46fc9b1a084f99e4f67e3faeccae9
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-22 21:34:42 +0000

    ChangeLogTag: Sat Jul 22 16:30:39 2000  Jeff Parsons  <parsons@cs.wustl.edu>

--- TAO/orbsvcs/tests/InterfaceRepo/IFR_Test/run_test.pl
+++ TAO/orbsvcs/tests/InterfaceRepo/IFR_Test/run_test.pl
@@ -13,9 +13,11 @@ use Cwd;
 $cwd = getcwd();
 ACE::checkForTarget($cwd);
 
+$locking = "-m";
+
 $if_repo_server = $EXEPREFIX."..".$DIR_SEPARATOR."..".$DIR_SEPARATOR.
 		  "..".$DIR_SEPARATOR."IFR_Service".$DIR_SEPARATOR.
-		  "IFR_Service".$EXE_EXT, " -m";
+		  "IFR_Service".$EXE_EXT, " $locking";
 
 
 $init_ref = 
