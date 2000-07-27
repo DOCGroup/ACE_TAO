63349d65f253cfe55bc07f285943b4fe34eedd30
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-07-27 18:55:33 +0000

    Thu Jul 27 13:50:48 2000  Luther J Baker  <luther@cs.wustl.edu>

--- PACE/pace/config/constants.h
+++ PACE/pace/config/constants.h
@@ -175,4 +175,75 @@
 # endif
 
 
+/* ============================================================================
+ * Temporary MACROS. Correct them later.
+ * ============================================================================ */
+
+#include "pace/sys/types.h"
+
+/* Need for NT */
+# define PACE_Export
+# define PACE_BUILD_DLL
+
+/* Tracing */
+# define PACE_TRACE(X) fprintf (stderr, X)
+# define PACE_DEBUG(X)
+# define PACE_LM_DEBUG PACE_stderr
+# define PACE_BEGIN_DUMP
+# define PACE_END_DUMP
+
+/* Identification for PACE.h */
+# define PACE_RCSID(X,Y,Z)
+
+/* Hooks */
+/* Dummy values */
+# define PACE_ALLOC_HOOK_DECLARE int declare_hook
+# define PACE_ALLOC_HOOK_DEFINE(X)
+
+/* Signals / ioctl / fcntl - dummy values */
+# define PACE_SIGURG 1
+# define PACE_NONBLOCK 2
+# define PACE_CLOEXEC 3
+
+/* Flags */
+/* Will not be a macro, dummy values */
+# define PACE_set_flags(X,Y) 1
+# define PACE_clr_flags(X,Y) 1
+
+/* Log Error */
+# define PACE_ERROR(X)
+
+/* Host Entries */
+# if defined (PACE_HAS_STRUCT_NETDB_DATA)
+typedef char PACE_HOSTENT_DATA[sizeof(struct hostent_data)];
+typedef char PACE_SERVENT_DATA[sizeof(struct servent_data)];
+typedef char PACE_PROTOENT_DATA[sizeof(struct protoent_data)];
+# else
+# if !defined PACE_HOSTENT_DATA_SIZE
+# define PACE_HOSTENT_DATA_SIZE (4*1024)
+# endif /* PACE_HOSTENT_DATA_SIZE */
+# if !defined PACE_SERVENT_DATA_SIZE
+# define PACE_SERVENT_DATA_SIZE (4*1024)
+# endif /*PACE_SERVENT_DATA_SIZE */
+# if !defined PACE_PROTOENT_DATA_SIZE
+# define PACE_PROTOENT_DATA_SIZE (2*1024)
+# endif /*PACE_PROTOENT_DATA_SIZE */
+typedef char PACE_HOSTENT_DATA[PACE_HOSTENT_DATA_SIZE];
+typedef char PACE_SERVENT_DATA[PACE_SERVENT_DATA_SIZE];
+typedef char PACE_PROTOENT_DATA[PACE_PROTOENT_DATA_SIZE];
+# endif /* PACE_HAS_STRUCT_NETDB_DATA */
+
+/* PACE_HAS_SIN_LEN */
+
+/* PACE_FREE */
+/* adjust later to use macros to decide how to free */
+# define PACE_FREE(X,Y) \
+X Y
+
+# define AF_ANY -1
+
+/* PACE CATCHALL */
+/* FIONREAD is used in PACE_SOCK_IO::recvv */
+/* PACE_HAS_ALLOCA is used in PACE_SOCK_IO::recvv */
+
 #endif /* PACE_CONFIG_CONSTANTS_H */
