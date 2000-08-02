78602cef31b7468a6d4c5014bc00d828d02fd14f
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-02 16:18:52 +0000

    ChangeLogTag:Tue Aug  1 15:25:39 2000  Carlos O'Ryan  <coryan@uci.edu>

--- trunk/TAO/performance-tests/Latency/server.cpp
+++ trunk/TAO/performance-tests/Latency/server.cpp
@@ -159,16 +159,6 @@ main (int argc, char *argv[])
 
       ACE_DEBUG ((LM_DEBUG, "event loop finished\n"));
 
-      PortableServer::ObjectId_var oid =
-        root_poa->servant_to_id (&server_impl, ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-
-      root_poa->deactivate_object (oid.in (), ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-
-      root_poa->destroy (1, 1, ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-
       orb->destroy (ACE_TRY_ENV);
       ACE_TRY_CHECK;
     }
