29efff381c4ee5900513415486b3dc7f9b3bdd2d
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-10 10:35:22 +0000

    ChangeLogTag: Thu Aug 10 05:23:30 2000  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/tao/GIOPC.h
+++ TAO/tao/GIOPC.h
@@ -77,6 +77,7 @@ TAO_NAMESPACE  GIOP
 
   TAO_NAMESPACE_STORAGE_CLASS const CORBA::Short ReferenceAddr;
 
+
   struct IORAddressingInfo;
   class IORAddressingInfo_var;
 
@@ -142,7 +143,7 @@ TAO_NAMESPACE  GIOP
 
   class TargetAddress;
   class TargetAddress_var;
-  
+
   class TAO_Export TargetAddress
   {
   public:
@@ -237,6 +238,14 @@ private:
   void operator= (const TargetAddress_var &);
 };
 
+// We dont have the _var _out for this struct. If need arises we can
+// add them later. For the present the Version struct alone should be
+// sufficient
+struct TAO_Export Version
+{
+  CORBA::Octet major;
+  CORBA::Octet minor;
+};
 
 }
 TAO_NAMESPACE_CLOSE // module GIOP
@@ -264,6 +273,8 @@ TAO_Export CORBA::Boolean operator<< (TAO_OutputCDR &, const GIOP::IORAddressing
 TAO_Export CORBA::Boolean operator>> (TAO_InputCDR &, GIOP::IORAddressingInfo &);
 TAO_Export CORBA::Boolean operator<< (TAO_OutputCDR &, const GIOP::TargetAddress &); //
 TAO_Export CORBA::Boolean operator>> (TAO_InputCDR &, GIOP::TargetAddress &);
+TAO_Export CORBA::Boolean operator<< (TAO_OutputCDR &, const GIOP::Version &);
+TAO_Export CORBA::Boolean operator>> (TAO_InputCDR &, GIOP::Version &);
 
 #endif /* __ACE_INLINE__ */
 
