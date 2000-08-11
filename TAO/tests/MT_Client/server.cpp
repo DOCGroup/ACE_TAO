0c2eba4018805208f337afbd3f8d5a3e01ee203b
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-11 06:16:01 +0000

    ChangeLogTag:Thu Aug 10 23:11:01 2000  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/tests/MT_Client/server.cpp
+++ TAO/tests/MT_Client/server.cpp
@@ -94,12 +94,13 @@ main (int argc, char *argv[])
       root_poa->destroy (1, 1, ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      ACE_OS::sleep (5);
+      orb->destroy (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
     }
   ACE_CATCHANY
     {
       ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
-                           "Catched exception:");
+                           "Exception caught:");
       return 1;
     }
   ACE_ENDTRY;
