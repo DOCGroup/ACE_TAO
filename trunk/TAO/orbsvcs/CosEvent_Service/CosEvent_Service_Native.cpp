219b2766908c4f8e1f50cd1715686a1e0d2c1178
doccvs <doccvs@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-11 19:05:45 +0000

    Fri Aug 11 12:03:41 2000  Priyanka Gontla  <pgontla@ece.uci.edu>

--- trunk/TAO/orbsvcs/CosEvent_Service/CosEvent_Service_Native.cpp
+++ trunk/TAO/orbsvcs/CosEvent_Service/CosEvent_Service_Native.cpp
@@ -7,6 +7,16 @@
 
 ACE_RCSID(CosEvent_Service, CosEvent_Service_Native, "$Id$")
 
+  // @@ Just remove this class, we don't use it...
+  //    The "Right Way"[tm] to fix this is to modify
+  //    TAO_CEC_EventChannel to get a new option.. If the option is
+  //    set then we destroy the ORB on EventChannel::destroy(),
+  //    otherwise we don't.
+  //    The option will not be set by default, but the user can pass
+  //    it in the command line, and in this main program with set it
+  //    to 1 by default or something like that...
+  //    Don't worry about this change yet... Let's get all the changes
+  //    in and then we can fix the EC shutdown problem...
 class EventChannel : public TAO_CEC_EventChannel
 {
   // = TITLE
@@ -43,134 +53,27 @@ main (int argc, char* argv[])
   ACE_DECLARE_NEW_CORBA_ENV;
   ACE_TRY
     {
-      // ORB initialization boiler plate...
-      CORBA::ORB_var orb =
-        CORBA::ORB_init (argc, argv, "", ACE_TRY_ENV);
+      int result;
+
+      // Intialize the ORB
+      CORBA::ORB_var orb = 
+        CORBA::ORB_init (argc, argv, 0, ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      CORBA::Object_var object =
-        orb->resolve_initial_references ("RootPOA", ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-      PortableServer::POA_var poa =
-        PortableServer::POA::_narrow (object.in (), ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-      PortableServer::POAManager_var poa_manager =
-        poa->the_POAManager (ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-      poa_manager->activate (ACE_TRY_ENV);
-      ACE_TRY_CHECK;
+      // Call TAO_Event_Loader::init (argc, argv) from here.
+      TAO_Event_Loader event_service;
 
-      // ****************************************************************
+      // To intialise the service
+      result = event_service.init (argc, argv);
       
-      // Parse the options, check if we should bind with the naming
-      // service and under what name...
-      ACE_Get_Opt get_opt (argc, argv, "n:xr");
-      int opt;
-      const char *service_name = "CosEventService";
-      int bind_to_naming_service = 1;
-      int use_rebind = 0;
-
-      while ((opt = get_opt ()) != EOF)
-        {
-          switch (opt)
-            {
-            case 'n':
-              service_name = get_opt.optarg;
-              break;
-
-            case 'x':
-              bind_to_naming_service = 0;
-              break;
-
-            case 'r':
-              use_rebind = 1;
-              break;
-
-            case '?':
-            default:
-              ACE_DEBUG ((LM_DEBUG,
-                          "Usage: %s "
-                          "-n service_name "
-                          "-x [disable naming service bind] "
-                          "-r [rebind, no AlreadyBound failures] "
-                          "\n",
-                          argv[0]));
-              return -1;
-            }
-        }
-
-      // ****************************************************************
-
-      // Control the event loop
-      int terminate_flag = 0;
-
-      // Create and activate the event service
-      TAO_CEC_EventChannel_Attributes attributes (poa.in (),
-                                                  poa.in ());
-
-      EventChannel ec_impl (attributes, &terminate_flag);
-      ec_impl.activate (ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-
-      CosEventChannelAdmin::EventChannel_var event_channel =
-        ec_impl._this (ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-
-      // ****************************************************************
-
-      CosNaming::Name channel_name (1);
-      CosNaming::NamingContext_var naming_context;
-
-      if (bind_to_naming_service)
-        {
-          CORBA::Object_var obj =
-            orb->resolve_initial_references ("NameService",
-                                             ACE_TRY_ENV);
-          ACE_TRY_CHECK;
-
-          naming_context =
-            CosNaming::NamingContext::_narrow (obj.in (),
-                                               ACE_TRY_ENV);
-          ACE_TRY_CHECK;
-
-          channel_name.length (1);
-          channel_name[0].id = CORBA::string_dup (service_name);
-
-          if (use_rebind)
-            {
-              naming_context->rebind (channel_name,
-                                      event_channel.in (),
-                                      ACE_TRY_ENV);
-              ACE_TRY_CHECK;
-            }
-          else
-            {
-              naming_context->bind (channel_name,
-                                    event_channel.in (),
-                                    ACE_TRY_ENV);
-              ACE_TRY_CHECK;
-            }
-        }
-
-      while (!terminate_flag)
-        {
-          ACE_Time_Value tv (0, 20000);
-          orb->perform_work (tv, ACE_TRY_ENV);
-          ACE_TRY_CHECK;
-        }
-
-      if (bind_to_naming_service)
-        {
-          naming_context->unbind (channel_name,
-                                  ACE_TRY_ENV);
-          ACE_TRY_CHECK;
-        }
+      if (result == -1)
+        return 1;
       
-      poa->destroy (1, 1, ACE_TRY_ENV);
-      ACE_TRY_CHECK;
-
-      orb->destroy (ACE_TRY_ENV);
-      ACE_TRY_CHECK;
+      orb->run();
+      
+      // Destroy the ORB
+      orb->destroy();
+      
     }
   ACE_CATCHANY
     {
