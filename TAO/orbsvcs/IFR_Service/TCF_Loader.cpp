c6717ef369eec033826298d8316db5e6af800d58
doccvs <doccvs@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-14 22:46:17 +0000

    ChangeLogTag: Mon Aug 14 15:42:45 2000  Priyanka Gontla  <pgontla@ece.uci.edu>

--- TAO/orbsvcs/IFR_Service/TCF_Loader.cpp
+++ TAO/orbsvcs/IFR_Service/TCF_Loader.cpp
@@ -13,18 +13,20 @@ TCF_Loader::TCF_Loader (void)
 
 CORBA::Object_ptr
 TCF_Loader::create_object (CORBA::ORB_ptr orb,
+                           int argc,
+                           char * argv[],
                            CORBA::Environment &ACE_TRY_ENV)
   ACE_THROW_SPEC (())
 {
   ACE_TRY
     {
       CORBA::Object_var obj =
-        orb->resolve_initial_references ("RootPOA", 
+        orb->resolve_initial_references ("RootPOA",
                                          ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       PortableServer::POA_var poa =
-        PortableServer::POA::_narrow (obj.in (), 
+        PortableServer::POA::_narrow (obj.in (),
                                       ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
@@ -36,7 +38,7 @@ TCF_Loader::create_object (CORBA::ORB_ptr orb,
       ACE_TRY_CHECK;
 
       TAO_TypeCodeFactory_i *servant = 0;
-      ACE_NEW_RETURN (servant, 
+      ACE_NEW_RETURN (servant,
                       TAO_TypeCodeFactory_i (),
                       CORBA::Object::_nil ());
 
