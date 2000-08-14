cde9dd479a377f1cb6e76cbef2734d2017261600
corsaro <corsaro@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-14 16:15:35 +0000

    ChangeLogTag: Mon Aug 14 11:03:08 2000  Angelo Corsaro  <corsaro@cs.wustl.edu>

--- TAO/tests/Exposed_Policies/client.cpp
+++ TAO/tests/Exposed_Policies/client.cpp
@@ -4,6 +4,24 @@
 
 ACE_RCSID(tao, client, "$Id$")
 
+
+  CORBA::Boolean check_reference (CORBA::Object_ptr object,
+                                  const char *msg = 0)
+{
+  if (CORBA::is_nil (object))
+    {
+      if (msg == 0)
+        ACE_DEBUG ((LM_DEBUG, 
+                    ACE_TEXT ("The Object reference is nil.\n")));
+      else
+        ACE_DEBUG ((LM_DEBUG, 
+                    ACE_TEXT (msg)));
+      return 0;
+    }
+  return 1;
+}
+
+
 int
 main (int argc, char *argv[])
 {
@@ -12,79 +30,100 @@ main (int argc, char *argv[])
   ACE_TRY
     {
       // ORB Initialization
-      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
-      // @@ Angelo, remember error checking, i.e., ACE_TRY_ENV
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, "TAO", ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       CORBA::Object_var object;
 
       // Get the IOR from a file.
-      object = orb->string_to_object ("file://ec.ior", ACE_TRY_ENV);
+      object = orb->string_to_object ("file://s.ior", ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
+      if (!check_reference (object, "Invalid IOR file!\n"))
+        return 1;
+      
       Counter_var counter = Counter::_narrow (object.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
-
-      if (CORBA::is_nil (counter))
-        {
-          ACE_DEBUG ((LM_DEBUG,
-                      "Unable to convert the IOR to the proper object reference.\n"));
-          return 1;
-        }
-
-      CORBA::ULong i;
-
-      for (i = 0; i < 10; i++)
-        {
-          counter->increment (ACE_TRY_ENV);
-          ACE_TRY_CHECK;
-        }
-      // @@ Angelo, above isn't strictly needed for the test...
-
-      // @@ Angelo, make sure to always use ACE_TRY & friends to
-      // detect exception conditions, especially on idl method invocations.
-
-      // Marina DONE.
-      // @@ Angelo, not done at all!! The next two lines contain
-      // remote invocations, and each is missing ACE_TRY_ENV!
-
-       ACE_DEBUG ((LM_DEBUG,
-                   "The Counter Value is: %d\n", counter->get_count()));
-       // @@ Angelo, never ever use result of expression that might
-       // throw exception in another expression.
-
-       counter->shutdown ();
-
-       // @@ Angelo, the code below is misindented.
-      CORBA::Policy *policy_ptr;
-      // @@ Angelo, please use Policy_ptr, not Policy *.
+      
+      if (!check_reference (object, 
+                            "Unable to convert the IOR to the proper object reference.\n"))
+        return 1;
+      
+      CORBA::Policy_var policy_ptr;
       policy_ptr = counter->_get_policy (RTCORBA::PRIORITY_MODEL_POLICY_TYPE,
                                          ACE_TRY_ENV);
       ACE_TRY_CHECK;
+      
+      if (!check_reference (policy_ptr, "Unable to get Priority Policy.\n"))
+        return 1;
 
-      // @@ Angelo, use is_nil ()...
-      if (policy_ptr == 0)
-        {
-          ACE_DEBUG ((LM_DEBUG, "Unable to get Priority Policy.\n"));
-          return 1;
-        }
-
-      RTCORBA::PriorityModelPolicy *priority_policy =
+      RTCORBA::PriorityModelPolicy_var priority_policy =
         RTCORBA::PriorityModelPolicy::_narrow (policy_ptr);
-
-      ACE_DEBUG ((LM_DEBUG, "Priority Model: %d\nCORBA Priority: %d",
-                  priority_policy->priority_model (ACE_TRY_ENV),
-                  priority_policy->server_priority (ACE_TRY_ENV)));
-
-      // @@ Angelo, again, don't use methods that may throw exceptions
-      // inside other expressions.  By the time you get to
-      // ACE_TRY_CHECK, you program would have fallen to the evil
-      // spirits of the seg daemon...
-
+        
+      RTCORBA::PriorityModel priority_model = 
+        priority_policy->priority_model (ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      // @@ Angelo, you have a memory leaks - never deallocating
-      // policies.  Make sure you read section on parameter passing
-      // considerations in CORBA 2.3 as well as description of narrow method.
+      RTCORBA::Priority priority = 
+        priority_policy->server_priority (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+
+      if (priority_model == RTCORBA::SERVER_DECLARED)
+        ACE_DEBUG ((LM_DEBUG, 
+                    ACE_TEXT ("\n\nPriority Model: RTCORBA::SERVER_DECLARED\n")
+                    ));
+
+      ACE_DEBUG ((LM_DEBUG, 
+                  ACE_TEXT ("\nPriority Model: %d\nCORBA Priority: %d\n"),
+                  priority_model,
+                  priority
+                  ));
+      
+      policy_ptr = object->_get_policy (RTCORBA::PRIORITY_BANDED_CONNECTION_POLICY_TYPE,
+                                        ACE_TRY_ENV);
+      if (!check_reference (policy_ptr, "Unable to get Priority Banded Policy\n"))
+        return 1;
+      
+      RTCORBA::PriorityBandedConnectionPolicy_var priority_banded_policy = 
+        RTCORBA::PriorityBandedConnectionPolicy::_narrow (policy_ptr,
+                                                          ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+
+      if (!check_reference (priority_banded_policy, "Unable to get Priority Banded Policy\n"))
+        return 1;
+      
+      // Here we have a priority banded connection policy.
+
+      RTCORBA::PriorityBands_var pb = priority_banded_policy->priority_bands ();
+      unsigned int band_num = pb->length ();
+      for (unsigned int i = 0; i < band_num; ++i)
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      ACE_TEXT ("Priority Band <%d>: (%d, %d)\n"), 
+                      i,
+                      pb[i].low,
+                      pb[i].high
+                      ));
+        }
+
+      policy_ptr = object->_get_policy (RTCORBA::CLIENT_PROTOCOL_POLICY_TYPE,
+                                        ACE_TRY_ENV);
+      if (!check_reference (policy_ptr, "Unable to get Priority Banded Policy\n"))
+        return 1;
+
+      RTCORBA::ClientProtocolPolicy_var client_protocol_policy = 
+        RTCORBA::ClientProtocolPolicy::_narrow (policy_ptr, ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      
+      RTCORBA::ProtocolList_var protocol_list = 
+        client_protocol_policy->protocols (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+ 
+      for (i = 0; i < protocol_list->length (); i++)
+        ACE_DEBUG ((LM_DEBUG,
+                    ACE_TEXT ("The Client Protocol Type: %d\n"), 
+                    protocol_list[i].protocol_type));
+                            
     }
 
   ACE_CATCHANY
--- TAO/tests/Exposed_Policies/server.cpp
+++ TAO/tests/Exposed_Policies/server.cpp
@@ -1,21 +1,43 @@
 //$Id$
-
+//
+// This files tests the Client exposed policies:
+//     - PriorityModelPolicy
+//     - PriorityBandedConnectionPolicy
+//     - ClientProtocolPolicy
+//
+// This policies are embedded in the object reference, by writem
+// them into the IOR. 
+//
+//
+// TODO: Create another object reference and override its POA's policies
+//       using the CORBA::Object::_set_policy_ovverides method.
+//      
+//
 #include "Counter_i.h"
 #include <tao/RT_ORB.h>
-const char *ior_output_file = "ec.ior";
-// @@ Angelo, lets change the name of ior file above.
+#include <tao/RT_Policy_i.h>
+#include <ace/Arg_Shifter.h>
 
-//////////////////////////////////////////////////////////////////
-//
-// NOTE: In this test there are different work-arounds, that were
-// needed to access some functionality not available at the time of
-// writing the test.
-// To point out these workarounds, the following convention will
-// be used:
-//
-//   @@ Workaround <name>
-//
-//   @@ Real Code <name>
+const char *ior_output_file = "s.ior";
+
+ACE_RCSID(tao, server, "$Id$");
+
+
+CORBA::Boolean check_reference (CORBA::Object_ptr object,
+                                const char *msg = 0)
+{
+  if (CORBA::is_nil (object))
+    {
+      if (msg == 0)
+        ACE_DEBUG ((LM_DEBUG, 
+                    ACE_TEXT ("The Object reference is nil.\n")));
+      else
+        ACE_DEBUG ((LM_DEBUG, 
+                    ACE_TEXT (msg)));
+      return 0;
+    }
+  return 1;
+}
 
 int
 main (int argc, char *argv[])
@@ -25,81 +47,112 @@ main (int argc, char *argv[])
   ACE_TRY
     {
       // ORB Initialization.
-      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, "", ACE_TRY_ENV);
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv, "TAO", ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      CORBA::Object_var object;
-
-       // @@ Workaround <RTORB access>
-      RTCORBA::RTORB_var rt_orb = new TAO_RT_ORB();
-
-      // @@ Angelo, the line above is just a temporary code, so it's
-      // ok.  However, in general, 1) do not use naked operator 'new'
-      // - use ACE_NEW & friends; 2) don't allocate memory where not
-      // neccessary, i.e., 'TAO_RT_ORB rt_orb;' will do just fine.
-
-      //   @@ Real Code <RTORB access>
-
+      // Here we parse the command line paramether passed
+      // to the application.
       /*
-       object = orb->resolve_initial_references ("RTORB", ACE_TRY_ENV);
-       ACE_TRY_CHECK;
-
-       RTCORBA::RTORB_var rt_orb = RTCORBA::RTORB::_narrow (object.in (),
-                                                            ACE_TRY_ENV);
-       ACE_TRY_CHECK;
-       */
-
+      ACE_Arg_Shifter arg_shifter (argc, argv);
+      
+      char *arg;
+      while (arg_shifter.is_anything_left ())
+        {
+        }  
+      */
+      CORBA::Object_var object;
+      
+      // Get a reference to the RT-ORB.
+      object = orb->resolve_initial_references ("RTORB", ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      
+      RTCORBA::RTORB_var rt_orb = RTCORBA::RTORB::_narrow (object.in (),
+                                                           ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      
       CORBA::PolicyList policy_list;
-      policy_list.length (1);
-
-      RTCORBA::Priority priority = 256;
+      policy_list.length (3);
+      
+      // Create the priority policy using the RT-ORB.
+      RTCORBA::Priority priority = 10;
       policy_list[0] =
         rt_orb->create_priority_model_policy (RTCORBA::SERVER_DECLARED,
                                               priority,
                                               ACE_TRY_ENV);
-      // @@ Angelo, don't forget to check 'ACE_TRY_CHECK' every time
-      // you pass environment to a method.
+      ACE_TRY_CHECK;
+      
+      // Create priority Banded Connection Policy.
+      // In order to do this we have to create a sequence
+      // of priority bands.
+
+      RTCORBA::PriorityBands priority_bands;
+      priority_bands.length (2);
+      
+      RTCORBA::PriorityBand p_band1, p_band2;
+      
+      p_band1.low = 10;
+      p_band1.high = 30;
+      
+      p_band2.low = 7;
+      p_band2.high = 21;
+      
+      priority_bands[0] = p_band1;
+      priority_bands[1] = p_band2;
+      
+      policy_list[1] = 
+        rt_orb->create_priority_banded_connection_policy (priority_bands, 
+                                                          ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+      
+      // Client Protocol Policy.
+      RTCORBA::ProtocolList protocol_list;
+      protocol_list.length (1);
+      
+      protocol_list[0].protocol_type = IOP::TAG_INTERNET_IOP;
+      protocol_list[0].orb_protocol_properties = 
+        TAO_Protocol_Properties_Factory::create_orb_protocol_property (IOP::TAG_INTERNET_IOP);
+
+      protocol_list[0].transport_protocol_properties =
+        TAO_Protocol_Properties_Factory::create_transport_protocol_property (IOP::TAG_INTERNET_IOP);
+
+      policy_list[2] =
+        rt_orb->create_client_protocol_policy (protocol_list);
 
       object = orb->resolve_initial_references ("RootPOA", ACE_TRY_ENV);
       ACE_TRY_CHECK;
-
-      // @@ Angelo, please get into habit of thorough error checking,
-      // e.g., it is good to check at this point that object is not nil.
-
+      
       PortableServer::POA_var poa =
         PortableServer::POA::_narrow (object.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      // Create a child POA with a given set of
-      // policies.
-      // @@ Angelo, why is this POA manager set to nil?  There is much
-      // more confusion with poa managers at the end of main ...
       PortableServer::POAManager_var poa_mgr =
         PortableServer::POAManager::_nil ();
-
+      
       PortableServer::POA_var child_poa =
         poa->create_POA ("Child_POA",
                          poa_mgr ,
                          policy_list);
-
-
-      // @@ Angelo, the lines until TODO comment are garbage.
-      // Why isn't the real code here yet?
-
-      // Create a Corba Object reference.
-      int i = 0;
+      
+      // Create a Corba Object reference, using the policies
+      // set at the POA level.
       object =
-        child_poa->create_reference ("IDL:omg.org/CORBA/Object:1.0",
+        child_poa->create_reference ("IDL:Counter:1.0",
                                      ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      if (CORBA::is_nil (object))
-        {
-          ACE_DEBUG ((LM_DEBUG, "Unable to create a Counter Object!"));
-          return 1;
-        }
+      ACE_DEBUG ((LM_DEBUG, 
+                  ACE_TEXT ("Reference Created!\n")));
+
+      if (!check_reference (object, 
+                            "Unable to create a Counter Object!\n"))
+        return 1;
+
       Counter_var counter = Counter::_narrow (object.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
+      
+      if (!check_reference (counter.in(), 
+                            "Unable to create a Counter Object!\n"))
+        return 1;
 
       Counter_Servant servant (orb.in ());
 
@@ -109,12 +162,13 @@ main (int argc, char *argv[])
         orb->object_to_string (counter.in (), ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      ACE_DEBUG ((LM_DEBUG, "Activated as <%s>\n", ior.in ()));
+      ACE_DEBUG ((LM_DEBUG, 
+                  ACE_TEXT ("Activated as <%s>\n"), ior.in ()));
 
       FILE *output_file = ACE_OS::fopen (ior_output_file, "w");
       if (output_file == 0)
         ACE_ERROR_RETURN ((LM_ERROR,
-                           "Cannot open output file for writing IOR: %s",
+                           ACE_TEXT ("Cannot open output file for writing IOR: %s"),
                            ior_output_file),
                           1);
       ACE_OS::fprintf (output_file, "%s", ior.in ());
@@ -136,10 +190,12 @@ main (int argc, char *argv[])
 
   ACE_CATCHANY
     {
-      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "CORBA Exception Raised.");
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, 
+                           ACE_TEXT ("CORBA Exception Raised."));
       return 1;
     }
   ACE_ENDTRY;
 
   return 0;
 }
+
