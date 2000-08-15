87c01d3efc2e2ecd71958d196d100f83c9b8994b
luther <luther@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-15 02:48:39 +0000

    Mon Aug 14 21:41:24 2000  Luther J Baker  <luther@cs.wustl.edu>

--- PACE/pace/win32/sched.h
+++ PACE/pace/win32/sched.h
@@ -16,19 +16,16 @@
 #ifndef PACE_SCHED_H_WIN32
 #define PACE_SCHED_H_WIN32
 
+  /* These definitions are used in other files.
+   * Separate the declarations from the definitions to avoid circular
+   * inclusion. ie: some other funcs just need the typedefs and structs. */
+
+#include "pace/win32/schedtypes.h"
 #include "pace/time.h"
 
 #if defined (PACE_HAS_CPLUSPLUS)
 extern "C" {
 #endif /* PACE_HAS_CPLUSPLUS */
-
-#define PACE_SCHED_FIFO SCHED_FIFO
-#define PACE_SCHED_OTHER SCHED_RR
-
-#ifndef PACE_SCHED_PARAM
-#define PACE_SCHED_PARAM
-  typedef struct sched_param { int a_; } pace_sched_param;
-#endif
 
 #if defined (PACE_HAS_CPLUSPLUS)
 }
