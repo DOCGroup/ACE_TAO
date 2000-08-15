907de784e37ffa96444c470877a702992a67ae80
doccvs <doccvs@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-15 15:47:31 +0000

    ChangeLogTag: Tue Aug 15 08:46:01 2000  Priyanka Gontla  <pgontla@ece.uci.edu>

--- TAO/orbsvcs/Trading_Service/Trading_Service.h
+++ TAO/orbsvcs/Trading_Service/Trading_Service.h
@@ -26,6 +26,7 @@
 #include "orbsvcs/IOR_Multicast.h"
 #include "orbsvcs/Trader/Trader.h"
 #include "orbsvcs/Trader/Service_Type_Repository.h"
+#include "orbsvcs/Trader/Trading_Loader.h"
 
 class Trading_Service;
 
@@ -70,46 +71,12 @@ public:
   int shutdown (void);
 
 protected:
-  int init_multicast_server (void);
-  // Enable the Trading Service to answer multicast requests for its
-  // IOR.
-
-  int bootstrap_to_federation (CORBA::Environment &ACE_TRY_ENV);
-  // Bootstrap to another trader, and attach to its trader network.
-
-  int parse_args (int &argc,
-                  char *argv[]);
-  // parses the arguments.
-
-  TAO_ORB_Manager orb_manager_;
-  // The ORB manager.
-
-  TAO_Service_Type_Repository type_repos_;
-  // Service Type Repository used by the trading service.
-
+  
   auto_ptr<TAO_Trader_Factory::TAO_TRADER> trader_;
   // Pointer to the linked trader.
 
-  CORBA::String_var name_;
-  // Name of this trading service: "hostname:pid".
-
-  CORBA::String_var ior_;
-  // IOR of the trader kept around for handiness purposes.
-
-  CORBA::Boolean federate_;
-  // Flag indicating whether this trader should join the federation.
-
-  FILE *ior_output_file_;
-  // File to output the Naming Service IOR.
-  
-  CORBA::Boolean dumpior_;
-  // Flag indicating whether to dump the ior to standard output.
-  
-  CORBA::Boolean bootstrapper_;
-  // Flag inidicating whether we're the trader others are bootstrapping to.
-  
-  TAO_IOR_Multicast ior_multicast_;
-  // Event handler that responds to resolve_initial_references requests.
+  TAO_Trading_Loader trading_loader_;
+  // Instance of TAO_Trading_Laoder
 };
 
 #endif /* _TRADING_SERVICE_H */
