36f3cbae433c227aaee289ee3c490cf3ed5190ac
levine <levine@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-08-24 14:06:02 +0000

    ChangeLogTag: Thu Aug 24 09:01:23 2000  JLopez <jlopez@uida.es>

--- ace/config-tru64.h
+++ ace/config-tru64.h
@@ -11,9 +11,9 @@
 
 #if defined (DIGITAL_UNIX)
 #  include "ace/config-osf1-4.0.h"
-#  if DIGITAL_UNIX >= 0x40F
+#  if DIGITAL_UNIX >= 0x40E
 #    define ACE_LACKS_SYSTIME_H
-#  endif /* DIGITAL_UNIX >= 0x40F */
+#  endif /* DIGITAL_UNIX >= 0x40E */
 #  if DIGITAL_UNIX >= 0x500
 #    define _LIBC_POLLUTION_H_
 #  endif /* DIGITAL_UNIX >= 0x500 */
