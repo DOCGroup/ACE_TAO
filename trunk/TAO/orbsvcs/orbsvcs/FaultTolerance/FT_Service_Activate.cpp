209a9b8de3731d67b276270cd0b26a68176caa71
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-09-23 16:25:02 +0000

    ChangeLogTag: Sat Sep 23 11:18:18 2000  Balachandran Natarajan  <bala@cs.wustl.edu>

--- trunk/TAO/orbsvcs/orbsvcs/FaultTolerance/FT_Service_Activate.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/FaultTolerance/FT_Service_Activate.cpp
@@ -1,20 +1,18 @@
 #include "FT_Service_Activate.h"
 #include "FT_Service_Callbacks.h"
-
 #include "ace/Dynamic_Service.h"
 
 ACE_RCSID(FaultTolerance, FT_Service_Activate, "$Id$")
 
 TAO_FT_Service_Activate::TAO_FT_Service_Activate (void)
-  :orb_core_ (0),
-   ft_service_callback_ (0)
+  :orb_core_ (0)
 {
 
 }
 
 TAO_FT_Service_Activate::~TAO_FT_Service_Activate (void)
 {
-  delete this->ft_service_callback_;
+
 }
 
 TAO_Service_Callbacks *
@@ -27,24 +25,37 @@ TAO_FT_Service_Activate::activate_services (TAO_ORB_Core *orb_core)
 
   this->orb_core_ = orb_core;
 
-  cout << "Did we gete here "<<endl;
-  if (this->ft_service_callback_ == 0)
-    {
-      // Construct service callback object
-      ACE_NEW_RETURN (this->ft_service_callback_,
-                      TAO_FT_Service_Callbacks (),
-                      0);
-    }
+  cout << "We are getting init " <<endl;
+  TAO_FT_Service_Callbacks *ft_service_callback = 0;
 
-  return this->ft_service_callback_;
+  // Construct service callback object
+  ACE_NEW_RETURN (ft_service_callback,
+                  TAO_FT_Service_Callbacks (),
+                  0);
+
+  return ft_service_callback;
   // @@ All the other things that we may need, will be added from time
   // @@ to time..
 
 }
 
+int
+TAO_FT_Service_Activate::Initializer (void)
+{
+  ACE_Service_Config::static_svcs ()->
+    insert (&ace_svc_desc_TAO_FT_Service_Activate);
 
+  return 0;
+}
 
-ACE_FACTORY_DEFINE (TAO_FT, TAO_FT_Service_Activate);
+ACE_FACTORY_DEFINE (TAO_FT,TAO_FT_Service_Activate)
+
+ACE_STATIC_SVC_DEFINE (TAO_FT_Service_Activate,
+                       ACE_TEXT ("FT_Service_Activate"),
+                       ACE_SVC_OBJ_T,
+                       &ACE_SVC_NAME (TAO_FT_Service_Activate),
+                       ACE_Service_Type::DELETE_THIS | ACE_Service_Type::DELETE_OBJ,
+                       0)
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 template class ACE_Dynamic_Service<TAO_FT_Service_Activate>;
