7a4694d33171a9e1278793e2518686b44adb1c00
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-09-28 19:26:50 +0000

    ChangeLogTag: Thu Sep 28 13:22:30 2000  Jeff Parsons  <parsons@cs.wustl.edu>

--- TAO/TAO_IDL/be/be_visitor_sequence/sequence_ci.cpp
+++ TAO/TAO_IDL/be/be_visitor_sequence/sequence_ci.cpp
@@ -397,7 +397,18 @@ be_visitor_sequence_ci::gen_var_impl (be_sequence *node)
 
   if (nt == AST_Decl::NT_pre_defined)
     {
-      AST_PredefinedType *p = AST_PredefinedType::narrow_from_decl (bt);
+      AST_PredefinedType *p = 0;
+      be_typedef *btd = be_typedef::narrow_from_decl (bt);
+
+      if (btd == 0)
+        {
+          p = AST_PredefinedType::narrow_from_decl (bt);
+        }
+      else
+        {
+          p = AST_PredefinedType::narrow_from_decl (btd->base_type ());
+        }
+
       pdt = p->pt ();
     }
 
