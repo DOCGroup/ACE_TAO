ba240e17b588f46528228ae1958af358592590e5
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-10-03 21:48:59 +0000

    ChangeLogTag:Tue Oct  3 14:25:09 2000  Darrell Brunsch <brunsch@uci.edu>

--- ace/Synch.cpp
+++ ace/Synch.cpp
@@ -356,15 +356,29 @@ ACE_Manual_Event::dump (void) const
 
 ACE_Auto_Event::ACE_Auto_Event (int initial_state,
                                 int type,
-                                const ACE_TCHAR *name,
+                                const char *name,
                                 void *arg)
   : ACE_Event (0,
                initial_state,
                type,
-               name,
+               ACE_TEXT_CHAR_TO_TCHAR (name),
                arg)
 {
 }
+
+#if defined (ACE_HAS_WCHAR)
+ACE_Auto_Event::ACE_Auto_Event (int initial_state,
+                                int type,
+                                const wchar_t *name,
+                                void *arg)
+  : ACE_Event (0,
+               initial_state,
+               type,
+               ACE_TEXT_WCHAR_TO_TCHAR (name),
+               arg)
+{
+}
+#endif /* ACE_HAS_WCHAR */
 
 void
 ACE_Auto_Event::dump (void) const
