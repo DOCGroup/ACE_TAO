31b313617c6e867bbd886b903e1dbe1afc14ef34
doccvs <doccvs@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-10-05 00:56:00 +0000

    ChangeLogTag: Wed Oct  4 17:41:53 2000  Priyanka Gontla  <pgontla@ece.uci.edu>

--- TAO/tao/CORBANAME_Parser.h
+++ TAO/tao/CORBANAME_Parser.h
@@ -17,15 +17,19 @@
 #define TAO_CORBANAME_PARSER_H
 #include "ace/pre.h"
 
+#include "tao/Object_Loader.h"
+#include "tao/Object.h"
+#include "tao/corba.h"
+#include "tao/ORB.h"
+
 #include "tao/IOR_Parser.h"
 #include "ior_corbaname_export.h"
-#include "orbsvcs/orbsvcs/CosNamingC.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-class TAO_IOR_CORBANAME_Export TAO_CORBANAME_Parser : public TAO_IOR_Parser
+class TAO_Export TAO_CORBANAME_Parser : public TAO_IOR_Parser
 {
   // = TITLE
   //   Implements the <corbaname:> IOR format
@@ -53,7 +57,7 @@ public:
 
  private:
   virtual CORBA::Object_ptr
-    parse_string_dynamic_request_helper (CosNaming::NamingContextExt_var &naming_context,
+    parse_string_dynamic_request_helper (CORBA::Object_ptr naming_context,
                                          ACE_CString &key_string,
                                          CORBA::Environment &ACE_TRY_ENV)
     ACE_THROW_SPEC ((CORBA::SystemException));
@@ -63,7 +67,7 @@ public:
 # include "CORBANAME_Parser.i"
 #endif /* __ACE_INLINE__ */
 
-ACE_FACTORY_DECLARE (TAO_IOR_CORBANAME, TAO_CORBANAME_Parser)
+ACE_FACTORY_DECLARE (TAO, TAO_CORBANAME_Parser)
 
 #include "ace/post.h"
 #endif /* TAO_CORBANAME_PARSER_H */
