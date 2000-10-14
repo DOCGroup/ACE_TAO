2cb8c637596a7ef03ae7462462262ae1223d9eed
marina <marina@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-10-14 05:20:10 +0000

    ChangeLogTag:Sat Oct 14 00:09:48 2000  Marina Spivak <marina@cs.wustl.edu>

--- TAO/tests/RTCORBA/Client_Protocol/client.cpp
+++ TAO/tests/RTCORBA/Client_Protocol/client.cpp
@@ -247,6 +247,8 @@ main (int argc, char *argv[])
       ACE_TRY_CHECK;
 
       // Testing over.  Shut down server ORB.
+      ACE_DEBUG ((LM_DEBUG,
+                  "\n     Testing over\n"));
       server2->shutdown (ACE_TRY_ENV);
       ACE_TRY_CHECK;
     }
@@ -266,6 +268,8 @@ main (int argc, char *argv[])
 int
 main (int argc, char *argv[])
 {
+  ACE_UNUSED_ARG (argc);
+  ACE_UNUSED_ARG (argv);
   ACE_ERROR_RETURN ((LM_ERROR,
                      "\nRTCORBA must be enabled to run this test!\n"),
                     1);
