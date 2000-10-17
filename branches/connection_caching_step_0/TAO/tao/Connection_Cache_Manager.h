a4c61d504b9f779c4742ad1552ec04d1e042d3e9
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-10-17 12:32:47 +0000

    *** empty log message ***

--- branches/connection_caching_step_0/TAO/tao/Connection_Cache_Manager.h
+++ branches/connection_caching_step_0/TAO/tao/Connection_Cache_Manager.h
@@ -30,17 +30,35 @@ class TAO_Cache_IntId;
 class TAO_Export TAO_Connection_Cache_Manager
 {
   // = TITLE
-  //     TAO_Connection_Cache_Manager is an abstract class for
+  //     TAO_Connection_Cache_Manager is an abstract class for the Connection Cache
 
   // = DESCRIPTION
-  //
+  //     This class offers a minmal interface that would be needed in
+  //     TAO to add connections in to Cache and find connections from
+  //     cache. Further, this interface also allows to purge entries
+  //     based on a user defined strategy. The sublasses of this
+  //     class would specialise with the right type of MAP data
+  //     structure. The subclasses would also be strategised by the
+  //     right caching strategy. Purging of entries from the Cache
+  //     would depend on those strategies.
 public:
 
   TAO_Connection_Cache_Manager (void);
   // Constructor
 
+  TAO_Connection_Cache_Manager (
+      ACE_Caching_Strategy<ATTRBIBUTES, CACHING_UTILITY> *strat);
+  // Constructor
+
   virtual ~TAO_Connection_Cache_Manager (void);
   // Destructor
+
+  virtual int open (size_t length = ACE_DEFAULT_MAP_SIZE,
+                    ACE_Allocator *alloc = 0) = 0;
+  // Initialize a cache with size <length>.
+
+  virtual int close (void) = 0;
+  // Close down a cache and release dynamically allocated resources.
 
   virtual int bind (const TAO_Cache_ExtId &ext_id,
                     const TAO_Cache_IntId &int_id) = 0;
@@ -57,7 +75,40 @@ public:
   // If notify_find also returns 0 (success), then this function returns
   // 0 (success) and sets the cached value in <value>.
 
+  virtual int rebind (const TAO_Cache_ExtId &key,
+                      const TAO_Cache_IntId &value) = 0;
+  // Reassociate the <key> with <value>. If the <key> already exists
+  // in the cache then returns 1, on a new bind returns 0 and returns
+  // -1 in case of any failures.
 
+  virtual int trybind (const TAO_Cache_ExtId &key,
+                       TAO_Cache_IntId &value);
+  // Associate <key> with <value> if and only if <key> is not in the
+  // cache.  If <key> is already in the cache, then the <value>
+  // parameter is overwritten with the existing value in the
+  // cache. Returns 0 if a new <key>/<value> association is created.
+  // Returns 1 if an attempt is made to bind an existing entry.  This
+  // function fails for maps that do not allow user specified keys.
+
+  virtual int unbind (const TAO_Cache_ExtId &key) = 0;
+  // Remove <key> from the cache.
+
+  virtual int unbind (const TAO_Cache_ExtId &key,
+                      TAO_Cache_IntId &value) = 0;
+  // Remove <key> from the cache, and return the <value> associated with
+  // <key>.
+
+  virtual int purge (void) = 0;
+  // Remove entries from the cache depending upon the strategy.
+
+  virtual size_t current_size (void) const = 0;
+  // Return the current size of the cache.
+
+  virtual size_t total_size (void) const = 0;
+  // Return the total size of the cache.
+
+  typdef
+protected:
 
 
 };
