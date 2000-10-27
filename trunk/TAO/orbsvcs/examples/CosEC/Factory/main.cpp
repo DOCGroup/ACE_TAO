388b406334c3a91036bcc3972561054cc7063412
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-10-27 18:45:38 +0000

    ChangeLogTag:Fri Oct 27 11:42:34 2000  Carlos O'Ryan  <coryan@uci.edu>

--- trunk/TAO/orbsvcs/examples/CosEC/Factory/main.cpp
+++ trunk/TAO/orbsvcs/examples/CosEC/Factory/main.cpp
@@ -2,10 +2,12 @@
 // $Id$
 
 #include "FactoryDriver.h"
+#include "orbsvcs/CosEvent/CEC_Default_Factory.h"
 
 int
 main (int argc, char *argv [])
 {
+  TAO_CEC_Default_Factory::init_svcs ();
   FactoryDriver driver;
   driver.start (argc, argv);
   return 0;
