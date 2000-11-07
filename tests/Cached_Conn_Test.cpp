2e8b73d72afacb1020a8dc82f7e68b32a5cd9c6e
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-07 19:51:13 +0000

    ChangeLogTag:Tue Nov  7 11:47:42 2000 Darrell Brunsch <brunsch@uci.edu>

--- tests/Cached_Conn_Test.cpp
+++ tests/Cached_Conn_Test.cpp
@@ -125,9 +125,13 @@ typedef ACE_Cached_Connect_Strategy_Ex<Svc_Handler, ACE_SOCK_CONNECTOR, CACHING_
   // with -frepo, because it misses some of them.
 
 // = Handle Gobbler
+#if (ACE_SIZEOF_INT != 4) && !defined (ACE_WIN32)
+// These might be already instantiated in ace/stats.cpp 
+// (if ACE_HANDLE == int && ACE_INT32 == int)
 template class ACE_Node<ACE_HANDLE>;
 template class ACE_Unbounded_Set<ACE_HANDLE>;
 template class ACE_Unbounded_Set_Iterator<ACE_HANDLE>;
+#endif /* ACE_SIZEOF_INT != 4 && !ACE_WIN32 */
 
 template class ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_NULL_SYNCH>;
 template class ACE_Refcounted_Hash_Recyclable<ACE_INET_Addr>;
