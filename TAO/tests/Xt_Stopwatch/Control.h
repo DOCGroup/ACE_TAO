1ae81805e5572d17aa0e483d9e354ad99670c704
bala <balanatarajan@users.noreply.github.com>
2000-11-11 17:39:57 +0000

    ChangeLogTag: Sat Nov 11 11:33:06 2000  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/tests/Xt_Stopwatch/Control.h
+++ TAO/tests/Xt_Stopwatch/Control.h
@@ -16,6 +16,9 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 #include "Client.h"
+
+#if defined (ACE_HAS_XT)
+
 #include <Xm/Xm.h>
 #include <Xm/PushB.h>
 #include <Xm/Label.h>
@@ -23,38 +26,40 @@
 #include <Xm/Frame.h>
 
 
-class Control  
+class Control
 {
  public:
   Control (Widget &parent);
   // Stopwatch constructor
-  
+
   virtual ~Control (void);
   // Destructor
 
-  virtual void manage (void);   
+  virtual void manage (void);
   // Manage the widget
 
   Widget& startwidget (void);
   // Return the start widget
 
   Widget& stopwidget (void);
-  // Return the stop widget.. 
+  // Return the stop widget..
 
  private:
-  
+
   Widget frame_;
   // A frame widget...
 
   Widget rowcolumn_;
   // A row column widget..
-  
+
   Widget startwidget_;
   // Pushbutton  Widget that is used to start the  timer..
-  
+
   Widget stopwidget_;
   // Pushbutton widget that is used to stop the timer..
 
 };
+
+#endif /*ACE_HAS_XT */
 
 #endif /* CONTROL_H */
--- TAO/tests/Xt_Stopwatch/timer.h
+++ TAO/tests/Xt_Stopwatch/timer.h
@@ -17,53 +17,58 @@
 #define TIMER_H
 
 #include "Stopwatch_display.h"
+
+#if defined (ACE_HAS_XT)
+
 #include <Xm/Xm.h>
 
 
 class Timer_imp
 {
  public:
-    
-  Timer_imp (XtAppContext &, 
+
+  Timer_imp (XtAppContext &,
              CORBA::Long,
              Stopwatch_display *);
   // Constructor..
-    
-  void start (void);        
+
+  void start (void);
   // Resets, and starts the clock ticking
 
-  void stop (void);         
+  void stop (void);
   // Stops the clock
 
-  CORBA::Float  elapsed_time (void); 
+  CORBA::Float  elapsed_time (void);
   // Returns time since timer started
 
  private:
-  
+
   static void tick_callback (XtPointer,
                              XtIntervalId *);
   // Static member function used for TimeOut callback.
-  
-  void tick (void);  
+
+  void tick (void);
   // Called every interval_ milliseconds
-  
+
   virtual void report_time (CORBA::Float);
-  // Called at each clock tick...  
+  // Called at each clock tick...
 
   Stopwatch_display *stopwatch_;
   // A copy of the stopwatch object
 
-  CORBA::Long counter_;    
+  CORBA::Long counter_;
   // Current number of ticks
 
-  CORBA::Long interval_;   
+  CORBA::Long interval_;
   // Time in milliseconds between updates
 
-  XtIntervalId id_;  
+  XtIntervalId id_;
   // Identifier of current TimeOut
 
-  XtAppContext app_; 
+  XtAppContext app_;
   // Required by Xt functions
 };
+
+#endif /*ACE_HAS_XT*/
 
 #endif /* TIMER_H */
