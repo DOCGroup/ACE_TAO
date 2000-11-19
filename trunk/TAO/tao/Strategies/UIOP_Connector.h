e5d298329385c93c1215b243e6ca284e92e87195
marina <marina@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-19 02:39:43 +0000

    ChangeLogTag:Sat Nov 18 20:36:54 2000  Marina Spivak <marina@cs.wustl.edu>

--- trunk/TAO/tao/Strategies/UIOP_Connector.h
+++ trunk/TAO/tao/Strategies/UIOP_Connector.h
@@ -96,7 +96,7 @@ public:
   // Pluggable.h
   int open (TAO_ORB_Core *orb_core);
   int close (void);
-  int connect (TAO_Connection_Decriptor_Interface *desc,
+  int connect (TAO_Connection_Descriptor_Interface *desc,
                TAO_Transport *&transport,
                ACE_Time_Value *max_wait_time,
                CORBA::Environment &ACE_TRY_ENV);
