28cc011aeeafe705ff0803a55cbbe470841fed82
bala <balanatarajan@users.noreply.github.com>
2000-11-19 05:34:42 +0000

    ChangeLogTag: Sat Nov 18 23:30:00 2000  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Connector.h
+++ TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Connector.h
@@ -88,7 +88,7 @@ public:
   // Pluggable.h
   virtual int open (TAO_ORB_Core *orb_core);
   virtual int close (void);
-  virtual int connect (TAO_Base_Connection_Property *prop,
+  virtual int connect (TAO_Connection_Descriptor_Interface *desc,
                        TAO_Transport *&transport,
                        ACE_Time_Value *max_wait_time,
                        CORBA::Environment &ACE_TRY_ENV);
