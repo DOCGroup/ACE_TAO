946cb2e2d75676253ede1027a5b5eab2f757a886
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-19 03:54:36 +0000

    ChangeLogTag:Sat Nov 18 19:50:21 2000  Carlos O'Ryan  <coryan@uci.edu>

--- trunk/TAO/orbsvcs/tests/Event_Latency/Event_Latency.h
+++ trunk/TAO/orbsvcs/tests/Event_Latency/Event_Latency.h
@@ -1,21 +1,12 @@
 /* -*- C++ -*- */
 // $Id$
 
-// ============================================================================
-//
-// = LIBRARY
-//    ACE Event Service Benchmarks
-//
-// = FILENAME
-//    Latency.h
-//
-// = AUTHOR
-//    David Levine (levine@cs.wustl.edu) and
-//    Tim Harrison (harrison@cs.wustl.edu)
-//
-// = DESCRIPTION
-//
-// ============================================================================
+/**
+ * @file Event_Latency.h
+ *
+ * @author David Levine (levine@cs.wustl.edu)
+ * @author Tim Harrison (harrison@cs.wustl.edu)
+ */
 
 #ifndef EVENT_LATENCY_H
 #define EVENT_LATENCY_H
@@ -29,202 +20,242 @@
 #include "orbsvcs/RtecEventChannelAdminC.h"
 #include "orbsvcs/RtecEventCommS.h"
 
-class Latency_Consumer : public POA_RtecEventComm::PushConsumer
-// = TITLE
-//    Latency Consumer
-//
-// = DESCRIPTION
-//    Simple example of a consumer that registers for supplier
-//    notifications.
+/// Latency Consumer
+/**
+ * Simple example of a consumer that registers for supplier
+ * notifications. 
+ */
+class Latency_Consumer
+  : public POA_RtecEventComm::PushConsumer
+  , PortableServer::RefCountServantBase
 {
 public:
+  /// Default constructor
+  /**
+   * Construction.  The default of not measuring jitter provides
+   * a "null" push routine, for measuring aggregate timing.
+   */
   Latency_Consumer (const int measure_jitter = 0);
-  // Construction.  The default of not measuring jitter provides
-  // a "null" push routine, for measuring aggregate timing.
 
+  /**
+   * Uses the name server to obtain a reference to the <supplier_name>
+   * and registers with channel to receive notifications from the
+   * supplier.  Also registers to receive shutdown messages from the
+   * supplier.  Stores <my_name> for printing out messages.  Returns 0
+   * on success, -1 on failure.
+   */
   int open_consumer (RtecEventChannelAdmin::EventChannel_ptr ec,
                      const char *my_name);
-  // Uses the name server to obtain a reference to the <supplier_name>
-  // and registers with channel to receive notifications from the
-  // supplier.  Also registers to receive shutdown messages from the
-  // supplier.  Stores <my_name> for printing out messages.  Returns 0
-  // on success, -1 on failure.
 
-  virtual void disconnect_push_consumer (CORBA::Environment &)
-    ACE_THROW_SPEC ((CORBA::SystemException));
-  // The channel is disconnecting.
+  /// Disconnect from the Event Service and deactivate the object
+  void close (CORBA::Environment &);
 
   void entry_point (const char*);
   const char *entry_point () const;
 
+  /// Print timing statistics.
   void print_stats () /* const */;
-  // Print timing statistics.
 
+  virtual void disconnect_push_consumer (CORBA::Environment &)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+  /**
+   * If the <events>[0] is a notification, prints out the data from
+   * the supplier.  If its a shutdown message, the consumer
+   * disconnects from the channel.
+   */
   virtual void push (const RtecEventComm::EventSet &events,
                      CORBA::Environment &)
     ACE_THROW_SPEC ((CORBA::SystemException));
-  // If the <events>[0] is a notification, prints out the data from
-  // the supplier.  If its a shutdown message, the consumer
-  // disconnects from the channel.
 
 protected:
   void shutdown (void);
-  // Disconnect from the Event Service.
 
-  // = Event channel adminstration references.
+  //@{
+  /// Event channel adminstration references.
   RtecEventChannelAdmin::EventChannel_var channel_admin_;
   RtecEventChannelAdmin::ConsumerAdmin_var consumer_admin_;
   RtecEventChannelAdmin::ProxyPushSupplier_var suppliers_;
+  //@}
 
 private:
   RtecBase::handle_t rt_info_;
 
   int measure_jitter_;
 
+  //@{
+  /// Registers and counters for keeping track of latency statistics.
   ACE_Time_Value min_latency_;
   ACE_Time_Value max_latency_;
   ACE_Time_Value total_latency_;
   u_long total_pushes_;
-  // Registers and counters for keeping track of latency statistics.
+  //@}
 
+  //@{
+  /// Statitics on time to get to the EC.
   ACE_Time_Value min_to_ec_;
   ACE_Time_Value max_to_ec_;
   ACE_Time_Value sum_to_ec_;
-  // Statitics on time to get to the EC.
+  //@}
 
+  //@{
+  /// Statitics on time spent in the EC.
   ACE_Time_Value min_in_ec_;
   ACE_Time_Value max_in_ec_;
   ACE_Time_Value sum_in_ec_;
-  // Statitics on time spent in the EC.
+  //@}
 
+  //@{
+  /// Statitics on time spent since the EC put the event on the wire
+  /// and it gets here.
   ACE_Time_Value min_from_ec_;
   ACE_Time_Value max_from_ec_;
   ACE_Time_Value sum_from_ec_;
-  // Statitics on time spent since the EC put the event on the wire
-  // and it gets here.
+  //@}
 
+  /// The name for the Scheduling service
   ACE_CString entry_point_;
 };
 
 // ************************************************************
 
+/// Latency Supplier
+/**
+ * Generates event nofications and a shutdown message.
+ */
 class Latency_Supplier
-// = TITLE
-//    Latency Supplier
-//
-// = DESCRIPTION
-//    Generates event nofications and a shutdown message.
 {
 public:
 
-  //
-  // This class provides IS-A Consumer and Supplier of events. But
-  // inheritance from two skeleton classes is non-complaint (or at
-  // least won't work with TAO). We use smaller implementation classes
-  // that delegate on Latency_Supplier to do the job.
-  //
-  class Supplier : public POA_RtecEventComm::PushSupplier {
+  /**
+   * This class provides IS-A Consumer and Supplier of events. But
+   * inheritance from two skeleton classes is non-complaint (or at
+   * least won't work with TAO). We use smaller implementation classes
+   * that delegate on Latency_Supplier to do the job.
+   */
+  class Supplier
+    : public POA_RtecEventComm::PushSupplier
+    , public PortableServer::RefCountServantBase
+  {
   public:
     virtual void disconnect_push_supplier (CORBA::Environment &)
       ACE_THROW_SPEC ((CORBA::SystemException));
-    // The channel is disconnecting.
 
   private:
+    /// Constructor
     Supplier (Latency_Supplier* impl);
+
+    /// Only the Latency_Supplier can create instances of this class
     friend class Latency_Supplier;
 
   private:
     Latency_Supplier* impl_;
   };
 
-  class Consumer : public POA_RtecEventComm::PushConsumer {
+  class Consumer
+    : public POA_RtecEventComm::PushConsumer
+    , public PortableServer::RefCountServantBase
+  {
   public:
     virtual void push (const RtecEventComm::EventSet &events,
                        CORBA::Environment &)
       ACE_THROW_SPEC ((CORBA::SystemException));
-    // The channel pushed some events to us.
 
     virtual void disconnect_push_consumer (CORBA::Environment &)
       ACE_THROW_SPEC ((CORBA::SystemException));
-    // The channel is disconnecting.
 
   private:
+    /// Constructor
     Consumer (Latency_Supplier* impl);
+
+    /// Only the Latency_Supplier can create instances of this class
     friend class Latency_Supplier;
 
   private:
     Latency_Supplier* impl_;
   };
 
+  /**
+   * Construction.  Requires the total number of messages to be
+   * sent.  If the timestamp flag is enabled, then events are
+   * timestamped, e.g., for use in measuring jitter.
+   */
   Latency_Supplier (const u_int total_messages,
                     CORBA::Long supplier_id,
                     const int timestamp = 0);
-  // Construction.  Requires the total number of messages to be
-  // sent.  If the timestamp flag is enabled, then events are
-  // timestamped, e.g., for use in measuring jitter.
 
+  /**
+   * Registers with the name server under the given <name>.  Also
+   * connects to the Event Channel as a supplier of notifications and
+   * shutdown messages.  If <master> != 0, then the supplier will
+   * destroy the channel upon shutting down. Returns 0 on success, -1
+   * on failure.
+   */
   int open_supplier (RtecEventChannelAdmin::EventChannel_ptr event_channel,
                      const char *name, int master);
-  // Registers with the name server under the given <name>.  Also
-  // connects to the Event Channel as a supplier of notifications and
-  // shutdown messages.  If <master> != 0, then the supplier will
-  // destroy the channel upon shutting down. Returns 0 on success, -1
-  // on failure.
 
+  /// Disconnect from the Event Service, deactivate the supplier and
+  /// consumer personalities
+  void close (CORBA::Environment &ACE_TRY_ENV);
+
+  /// The channel is disconnecting.
   void disconnect_push_supplier (CORBA::Environment &);
-  // The channel is disconnecting.
 
+  /// The channel is disconnecting.
   void disconnect_push_consumer (CORBA::Environment &);
-  // The channel is disconnecting.
 
+  /// Takes a timestamp and then pushes event_ to all consumers, either
+  /// directly, or via a channel.
   void push (const RtecEventComm::EventSet &events,
              CORBA::Environment &);
-  // Takes a timestamp and then pushes event_ to all consumers, either
-  // directly, or via a channel.
 
+  /**
+   * Called when the supplier should start generating events.
+   * Registers with the Event Channel to receive timeouts every .25
+   * seconds.  Will generate some number of events and then send a
+   * shutdown message.  Returns 0 on success, -1 on failure.
+   */
   int start_generating_events (void);
-  // Called when the supplier should start generating events.
-  // Registers with the Event Channel to receive timeouts every .25
-  // seconds.  Will generate some number of events and then send a
-  // shutdown message.  Returns 0 on success, -1 on failure.
 
   void entry_point (const char *);
   const char *entry_point () const;
 
+  /// Print timing statistics.
   void print_stats () /* const */;
-  // Print timing statistics.
 
 protected:
-
   void shutdown (void);
-  // Disconnect from the Event Service.
 
-  // = Event Channel adminstration references.
+  //@{
+  /// Event Channel adminstration references.
   RtecEventChannelAdmin::EventChannel_var channel_admin_;
   RtecEventChannelAdmin::ConsumerAdmin_var consumer_admin_;
   RtecEventChannelAdmin::ProxyPushConsumer_var consumers_;
   RtecEventChannelAdmin::SupplierAdmin_var supplier_admin_;
   RtecEventChannelAdmin::ProxyPushSupplier_var suppliers_;
+  //@}
 
 private:
   RtecBase::handle_t rt_info_;
 
+  /// How many events to push before ending the test.
   u_int total_messages_;
-  // How many events to push before ending the test.
 
+  /// Our supplier ID.
   CORBA::Long supplier_id_;
-  // Our supplier ID.
 
+  /// Flag indicating whether or not to timestamp outgoing events.
   int timestamp_;
-  // Flag indicating whether or not to timestamp outgoing events.
 
+  /// How many events we've pushed.
   u_int total_sent_;
-  // How many events we've pushed.
 
+  //@{
+  /// Start/stop times, marking the time period when events are
+  /// sent to consumers.
   ACE_Time_Value test_start_time_;
   ACE_Time_Value test_stop_time_;
-  // Start/stop times, marking the time period when events are
-  // sent to consumers.
+  //@}
 
   int master_;
 
