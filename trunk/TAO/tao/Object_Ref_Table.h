d52d2afd325dd052b08267d0232fc4a20acc0881
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-22 05:38:28 +0000

    ChangeLogTag:Tue Nov 21 21:13:18 2000  Ossama Othman  <ossama@uci.edu>

--- trunk/TAO/tao/Object_Ref_Table.h
+++ trunk/TAO/tao/Object_Ref_Table.h
@@ -48,6 +48,10 @@
 class TAO_Export TAO_Object_Ref_Table
 {
 public:
+
+  typedef ACE_Hash_Map_Manager_Ex<const char *, CORBA::Object_ptr, ACE_Hash<const char *>, ACE_Equal_To<const char *>, ACE_Null_Mutex> Table;
+  typedef Table::iterator Iterator;
+
   /// Constructor
   TAO_Object_Ref_Table (void);
 
@@ -67,10 +71,6 @@ public:
     CORBA::Environment &ACE_TRY_ENV);
 
 private:
-
-  typedef ACE_Hash_Map_Manager_Ex<const char *, CORBA::Object_ptr, ACE_Hash<const char *>, ACE_Equal_To<const char *>, ACE_Null_Mutex> Table;
-  typedef Table::iterator Iterator;
-
 
   /// The canonical ACE_Map methods.
   //@{
