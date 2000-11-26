ade2e55b604ab8f9639c2a283eb9678fb0abbd05
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-26 03:33:02 +0000

    Sat Nov 25 21:28:25 2000  Pradeep Gore  <pradeep@cs.wustl.edu>

---
+++ TAO/orbsvcs/tests/Notify/Basic/Simple.h
@@ -0,0 +1,119 @@
+/* -*- C++ -*- */
+// $Id$
+// ==========================================================================
+//
+// = FILENAME
+//   Simple_Test.h
+//
+// = DESCRIPTION
+//   Simple test any supplier to any consumer.
+//
+// = AUTHOR
+//    Pradeep Gore <pradeep@cs.wustl.edu>
+//
+// ==========================================================================
+
+#ifndef NOTIFY_TESTS_SIMPLE_H
+#define NOTIFY_TESTS_SIMPLE_H
+
+#include "Notify_Test_Client.h"
+#include "Notify_PushConsumer.h"
+#include "Notify_PushSupplier.h"
+
+class Simple_Test;
+
+/***************************************************************************/
+
+class Event_AnyPushConsumer : public TAO_Notify_PushConsumer
+{
+public:
+  Event_AnyPushConsumer (Simple_Test *test_client);
+  // Contructor.
+
+  // = PushSupplier methods
+  virtual void push (
+        const CORBA::Any & data,
+        CORBA::Environment &ACE_TRY_ENV
+      )
+      ACE_THROW_SPEC ((
+        CORBA::SystemException,
+        CosEventComm::Disconnected
+       ));
+
+protected:
+  Simple_Test * test_client_;
+};
+
+/***************************************************************************/
+
+class Event_AnyPushSupplier : public TAO_Notify_PushSupplier
+{
+public:
+  Event_AnyPushSupplier (Simple_Test * test_client);
+  // Constructor.
+
+  virtual ~Event_AnyPushSupplier ();
+  // Destructor.
+
+protected:
+  Simple_Test* test_client_;
+};
+
+/***************************************************************************/
+
+class Simple_Test : public Notify_Test_Client
+{
+public:
+  // Initialization and termination code
+  Simple_Test (void);
+  virtual ~Simple_Test ();
+
+  int parse_args(int argc, char *argv[]) ;
+
+  void init (int argc, char *argv [], CORBA::Environment &ACE_TRY_ENV);
+  // initialization.
+
+  void on_event_received (void);
+  // Called when an event is received.
+
+  void run_test (CORBA::Environment &ACE_TRY_ENV);
+  // Run the test.
+
+  void end_test (CORBA::Environment &ACE_TRY_ENV);
+  // End the test.
+
+  int check_results (void);
+  // check if we got the expected results.
+
+protected:
+  void create_EC (CORBA::Environment &ACE_TRY_ENV);
+  // Create EC
+
+  ACE_Atomic_Op <ACE_SYNCH_MUTEX, int> result_count_;
+  // Number of events received so far.
+
+  int event_count_;
+  // Number of events to send
+
+  CosNotifyChannelAdmin::EventChannel_var ec_;
+  // The one channel that we create using the factory.
+
+  CosNotifyChannelAdmin::ConsumerAdmin_var consumer_admin_;
+  // The consumer admin used by consumers.
+
+  CosNotifyChannelAdmin::SupplierAdmin_var supplier_admin_;
+  // The supplier admin used by suppliers.
+
+  TAO_Notify_PushConsumer* consumer_;
+  // Consumer
+
+  TAO_Notify_PushSupplier* supplier_;
+  // Supplier
+
+private:
+  friend class Event_AnyPushSupplier;
+  friend class Event_AnyPushConsumer;
+};
+
+/***************************************************************************/
+#endif /* NOTIFY_TESTS_SIMPLE_H */
