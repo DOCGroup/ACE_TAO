c0e6c9f74a6e5af657e20fd4fc834bf086349c60
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-27 18:36:25 +0000

    Mon Nov 27 12:33:23 2000  Pradeep Gore  <pradeep@cs.wustl.edu>

--- trunk/TAO/orbsvcs/orbsvcs/Trader/Trading_Loader.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Trader/Trading_Loader.cpp
@@ -456,9 +456,13 @@ ACE_FACTORY_DEFINE (TAO_Trading, TAO_Trading_Loader)
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
 template class ACE_Dynamic_Service<TAO_Trading_Loader>;
+template class auto_ptr<TAO_Trader_Factory::TAO_TRADER>;
+template class ACE_Auto_Basic_Ptr<TAO_Trader_Factory::TAO_TRADER>;
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
 #pragma instantiate ACE_Dynamic_Service<TAO_Trading_Loader>
+#pragma instantiate auto_ptr<TAO_Trader_Factory::TAO_TRADER>
+#pragma instantiate ACE_Auto_Basic_Ptr<TAO_Trader_Factory::TAO_TRADER>
 
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
