23e0caa4a345064689e654a269cd25f40db28e3f
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-11-27 18:36:25 +0000

    Mon Nov 27 12:33:23 2000  Pradeep Gore  <pradeep@cs.wustl.edu>

--- TAO/orbsvcs/Trading_Service/Trading_Service.cpp
+++ TAO/orbsvcs/Trading_Service/Trading_Service.cpp
@@ -85,9 +85,7 @@ Trading_Service::shutdown (void)
 }
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template class auto_ptr<TAO_Trader_Factory::TAO_TRADER>;
-template class ACE_Auto_Basic_Ptr<TAO_Trader_Factory::TAO_TRADER>;
+
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#pragma instantiate auto_ptr<TAO_Trader_Factory::TAO_TRADER>
-#pragma instantiate ACE_Auto_Basic_Ptr<TAO_Trader_Factory::TAO_TRADER>
+
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
