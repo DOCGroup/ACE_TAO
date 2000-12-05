dab563efd72ce10d867bff54f301c950bc719f99
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-12-05 20:18:48 +0000

    ChangeLogTag: Tue Dec  5 14:16:38 2000  Jeff Parsons  <parsons@cs.wustl.edu>

--- TAO/TAO_IDL/be/be_visitor_operation/ami_handler_reply_stub_operation_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_operation/ami_handler_reply_stub_operation_cs.cpp
@@ -217,7 +217,7 @@ be_visitor_operation_ami_handler_reply_stub_operation_cs::visit_operation (be_op
 
   *os << " ());" << be_uidt_nl;
 
-  *os << "Messaging::ExceptionHolder::_marshaled_exception_seq::_var_type marshaled_exception_var =" << be_idt_nl
+  *os << "Messaging::ExceptionHolder::_tao_seq_Octet_var marshaled_exception_var =" << be_idt_nl
       << "new Messaging::ExceptionHolder::_marshaled_exception_seq (cdr->length (), // max" << be_nl
       << "cdr->length (), // length" << be_nl
       << "(unsigned char*) cdr->rd_ptr (),// data" << be_nl
