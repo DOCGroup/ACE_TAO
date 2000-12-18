0856554538a49b7da4dcff9b35f3ee54e508c24f
schmidt <douglascraigschmidt@users.noreply.github.com>
2000-12-18 00:54:33 +0000

    ChangeLogTag:Sun Dec 17 18:42:13 2000  Douglas C. Schmidt  <schmidt@ace.cs.wustl.edu>

--- ace/Refcounted_Auto_Ptr.h
+++ ace/Refcounted_Auto_Ptr.h
@@ -10,12 +10,12 @@
  */
 //=============================================================================
 
-
 #ifndef ACE_REFCOUNTED_AUTO_PTR_H
 #define ACE_REFCOUNTED_AUTO_PTR_H
 #include "ace/pre.h"
 
 #include "ace/OS.h"
+#include "ace/Auto_Ptr.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
