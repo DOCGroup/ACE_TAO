b5cefdc6091a368e0087b57254f82ca7a36aaf38
Steve Huston <shuston@riverace.com>
2000-12-19 16:34:14 +0000

    ChangeLogTag:Tue Dec 19 11:02:41 2000  Steve Huston  <shuston@riverace.com>

--- ace/OS.h
+++ ace/OS.h
@@ -5514,14 +5514,31 @@ public:
 #if defined (ACE_HAS_LLSEEK) || defined (ACE_HAS_LSEEK64)
   ACE_LOFF_T llseek (ACE_HANDLE handle, ACE_LOFF_T offset, int whence);
 #endif /* ACE_HAS_LLSEEK */
+
+  // It used to be that the <perms> argument default was 0 on all
+  // platforms. Further, the ACE_OS::open implementations ignored <perms>
+  // for Win32 and always supplied read|write|delete. To preserve
+  // backward compatibility and allow users to pass in values
+  // that are used as desired, the defaults are now what the default
+  // action used to be on Win32. The implementation now obeys what is passed.
+#if defined (ACE_WIN32)
+#  if defined (ACE_HAS_WINNT4) && (ACE_HAS_WINNT4 == 1)
+#    define ACE_DEFAULT_OPEN_PERMS (FILE_SHARE_READ | FILE_SHARE_WRITE | \
+                                    FILE_SHARE_DELETE)
+#  else
+#    define ACE_DEFAULT_OPEN_PERMS (FILE_SHARE_READ | FILE_SHARE_WRITE)
+#  endif /* ACE_HAS_WINCE */
+#else
+#  define ACE_DEFAULT_OPEN_PERMS 0
+#endif  /* ACE_WIN32 */
   static ACE_HANDLE open (const char *filename,
                           int mode,
-                          int perms = 0,
+                          int perms = ACE_DEFAULT_OPEN_PERMS,
                           LPSECURITY_ATTRIBUTES sa = 0);
 #if defined (ACE_HAS_WCHAR)
   static ACE_HANDLE open (const wchar_t *filename,
                           int mode,
-                          int perms = 0,
+                          int perms = ACE_DEFAULT_OPEN_PERMS,
                           LPSECURITY_ATTRIBUTES sa = 0);
 #endif /* ACE_HAS_WCHAR */
   static int putmsg (ACE_HANDLE handle,
