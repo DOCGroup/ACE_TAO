c3c70163eaa42dc1f957916161c41055245fc746
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-12-22 18:26:04 +0000

    ChangeLogTag: Fri Dec 22 12:22:06 2000  Balachandran Natarajan  <bala@cs.wustl.edu>

---
+++ trunk/TAO/tests/BiDirectional_NestedUpcall/test_i.cpp
@@ -0,0 +1,70 @@
+// $Id$
+
+#include "test_i.h"
+
+#if !defined(__ACE_INLINE__)
+#include "test_i.i"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID(BiDirectional_NestedUpcall, test_i, "$Id$")
+
+void
+Callback_i::shutdown (CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  ACE_DEBUG ((LM_DEBUG, "Performing clean shutdown\n"));
+  this->orb_->shutdown (0, ACE_TRY_ENV);
+}
+
+void
+Callback_i::callback_method (CORBA::Environment & /*ACE_TRY_ENV*/)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  ACE_DEBUG ((LM_DEBUG, "Callback method called \n"));
+}
+
+
+// ****************************************************************
+
+CORBA::Long
+Simple_Server_i::test_method (CORBA::Boolean do_callback,
+                              CORBA::Environment &ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  if (do_callback)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  ACE_TEXT ("About to make a remote call in the Upcall \n")));
+
+      for (int times = 0;
+           times < this->no_iterations_;
+           ++times)
+        {
+          this->callback_->callback_method (ACE_TRY_ENV);
+          ACE_CHECK_RETURN (0);
+
+          TAO_ORB_Core *orb_core = this->orb_->orb_core ();
+          if (orb_core->connection_cache ().total_size () > 1)
+            ACE_ASSERT (-1);
+        }
+    }
+
+  return 0;
+}
+
+void
+Simple_Server_i::callback_object (Callback_ptr callback,
+                                  CORBA::Environment& )
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  // Store the callback object
+  this->callback_ = Callback::_duplicate (callback);
+}
+
+
+void
+Simple_Server_i::shutdown (CORBA::Environment& ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  this->orb_->shutdown (0, ACE_TRY_ENV);
+}
