f7cef393cee7cdd28ad5d5c55fe88e60b13f21e7
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2000-12-27 02:32:47 +0000

    ChangeLogTag:Tue Dec 26 18:31:08 2000  Carlos O'Ryan  <coryan@uci.edu>

--- examples/Naming/test_open.cpp
+++ examples/Naming/test_open.cpp
@@ -1,35 +1,36 @@
 // $Id$
 
 #include "ace/Naming_Context.h"
+#include "ace/Log_Msg.h"
 
 ACE_RCSID(Naming, test_open, "$Id$")
 
-int 
-main (int argc, char **argv) 
+int
+main (int argc, char **argv)
 {
   const char *host = argc > 1 ? argv[1] : "-hlocalhost";
   const char *port = argc > 2 ? argv[2] : "-p20012";
 
   ACE_STATIC_SVC_REGISTER(ACE_Naming_Context);
 
-  ACE_Naming_Context ns;   
+  ACE_Naming_Context ns;
   ACE_Name_Options *name_options = ns.name_options ();
-  
-  const char *m_argv[] = 
+
+  const char *m_argv[] =
   {
-    "MyName", 
-    "-cNET_LOCAL", 
-    host, 
-    port,  
-    NULL 
+    "MyName",
+    "-cNET_LOCAL",
+    host,
+    port,
+    NULL
   };
   int m_argc =
-    sizeof (m_argv) / sizeof (char *) -1; 
+    sizeof (m_argv) / sizeof (char *) -1;
 
   name_options->parse_args (m_argc,
                             (char **) m_argv);
 
-  int result = ns.open (ACE_Naming_Context::NET_LOCAL); 
+  int result = ns.open (ACE_Naming_Context::NET_LOCAL);
   ACE_DEBUG ((LM_DEBUG,
               "ACE_Naming_Context::open returned %d\n",
               result));
@@ -40,13 +41,13 @@ main (int argc, char **argv)
       char key[128];
       char val[32];
       char type[2];
-      
+
       type[0] = '-';
       type[1] = '\0';
-      
+
       int i = 0;
 
-      for (int l = 1; l <= 1000 ; l++) 
+      for (int l = 1; l <= 1000 ; l++)
         {
           ACE_OS::sprintf (key,
                            "K_%05d_%05d",
@@ -57,18 +58,18 @@ main (int argc, char **argv)
                            l);
           i = ns.bind (key,
                        val,
-                       type);  
+                       type);
           ACE_DEBUG ((LM_DEBUG,
                       "%d: bind of %s: %d\n",
                       ACE_OS::getpid (),
                       key,
                       i));
 
-          if (i != 0) 
+          if (i != 0)
             return -1;
 
         }
-      
+
       result = ns.close ();
       ACE_DEBUG ((LM_DEBUG,
                   "ACE_Naming_Context::close returned %d\n",
