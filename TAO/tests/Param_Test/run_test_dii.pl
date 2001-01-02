7454b80000609666fd98260dc08ea906579ec8d8
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-01-02 22:20:51 +0000

    ChangeLogTag:Tue Jan  2 14:19:03 2001  Carlos O'Ryan  <coryan@uci.edu>

---
+++ TAO/tests/Param_Test/run_test_dii.pl
@@ -0,0 +1,12 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# $Id$
+# -*- perl -*-
+
+@ARGS=('run_test.pl', '-i', 'dii');
+push @ARGS, @ARGV;
+
+exec 'run_test.pl', @ARGS;
+
