bf6cacf40e9d53bbcc2d149d174172f80458f36a
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-01-03 21:00:05 +0000

    ChangeLogTag:Wed Jan  3 12:56:13 2001  Darrell Brunsch <brunsch@uci.edu>

--- ace/Timer_Wheel_T.h
+++ ace/Timer_Wheel_T.h
@@ -36,7 +36,8 @@ class ACE_Timer_Wheel_T;
  * in the order of timeout values.
  */
 template <class TYPE, class FUNCTOR, class ACE_LOCK>
-class ACE_Timer_Wheel_Iterator_T : public ACE_Timer_Queue_Iterator_T <TYPE, FUNCTOR, ACE_LOCK>
+class ACE_Timer_Wheel_Iterator_T 
+  : public ACE_Timer_Queue_Iterator_T <TYPE, FUNCTOR, ACE_LOCK>
 {
 public:
   /// Constructor
@@ -96,31 +97,15 @@ public:
   /// Type inherited from
   typedef ACE_Timer_Queue_T<TYPE, FUNCTOR, ACE_LOCK> INHERITED;
 
-  // = Initialization and termination methods
+  /// Default constructor
+  ACE_Timer_Wheel_T (FUNCTOR *upcall_functor = 0,
+                     ACE_Free_List<ACE_Timer_Node_T <TYPE> > *freelist = 0);
 
-  /**
-   * Constructor that takes in <wheelsize> - size of the timing wheel,
-   * <resolution> - resolution of time values the hashing function uses,
-   * and <upcall_functor> - a functor that will be used instead of creating
-   * a default functor.  Also, when the freelist is created, <prealloc> nodes
-   * will be allocated. This can also take in a upcall functor and freelist
-   * (if 0, then defaults will be created)
-   */
+  /// Constructor with opportunities to set the wheelsize and resolution
   ACE_Timer_Wheel_T (size_t wheelsize,
                      size_t resolution,
                      size_t prealloc = 0,
                      FUNCTOR *upcall_functor = 0,
-                     ACE_Free_List<ACE_Timer_Node_T <TYPE> > *freelist = 0);
-
-  /**
-   * Default constructor. <upcall_functor> is the instance of the
-   * FUNCTOR to be used by the queue. If <upcall_functor> is 0, Timer
-   * Queue will create a default FUNCTOR.  <freelist> the freelist of
-   * timer nodes.  If 0, then a default freelist will be created.  The
-   * defaults will be used for size and resolution and no preallocation
-   * (ACE_DEFAULT_TIMER_WHEEL_SIZE, ACE_DEFAULT_TIMER_WHEEL_RESOLUTION)
-   */
-  ACE_Timer_Wheel_T (FUNCTOR *upcall_functor = 0,
                      ACE_Free_List<ACE_Timer_Node_T <TYPE> > *freelist = 0);
 
   /// Destructor
@@ -132,64 +117,39 @@ public:
   /// Returns the time of the earlier node in the <ACE_Timer_Wheel>.
   virtual const ACE_Time_Value &earliest_time (void) const;
 
-  /**
-   * Schedule <type> that will expire after <delay> amount of time,
-   * which is specified in absolute time.  If it expires then <act> is
-   * passed in as the value to the <functor>.  If <interval> is != to
-   * <ACE_Time_Value::zero> then it is used to reschedule the <type>
-   * automatically, using relative time to the current <gettimeofday>.
-   * This method returns a <timer_id> that uniquely identifies the the
-   * timer.  This <timer_id> can be used to cancel the timer before it
-   * expires.  Returns -1 on failure.
-   */
+  /// Schedules a timer.
   virtual long schedule (const TYPE &type,
                          const void *act,
                          const ACE_Time_Value &delay,
-                         const ACE_Time_Value &interval = ACE_Time_Value::zero);
+                         const ACE_Time_Value &interval 
+                           = ACE_Time_Value::zero);
 
-  /**
-   * Resets the interval of the timer represented by <timer_id> to
-   * <interval>, which is specified in relative time to the current
-   * <gettimeofday>.  If <interval> is equal to
-   * <ACE_Time_Value::zero>, the timer will become a non-rescheduling
-   * timer.  Returns 0 if successful, -1 if not.
-   */
+  /// Changes the interval of a timer (and can make it periodic or non
+  /// periodic by setting it to ACE_Time_Value::zero or not).
   virtual int reset_interval (long timer_id,
                               const ACE_Time_Value &interval);
 
-  /**
-   * Cancel all timer associated with <type>.  If <dont_call> is 0
-   * then the <functor> will be invoked.  Returns number of timers
-   * cancelled.
-   */
+  /// Cancel all timer associated with <type>.  If <dont_call> is 0
+  /// then the <functor> will be invoked.  Returns number of timers
+  /// cancelled.
   virtual int cancel (const TYPE &type,
                       int dont_call_handle_close = 1);
 
-  /**
-   * Cancel the single timer that matches the <timer_id> value (which
-   * was returned from the <schedule> method).  If act is non-NULL
-   * then it will be set to point to the ``magic cookie'' argument
-   * passed in when the timer was registered.  This makes it possible
-   * to free up the memory and avoid memory leaks.  If <dont_call> is
-   * 0 then the <functor> will be invoked.  Returns 1 if cancellation
-   * succeeded and 0 if the <timer_id> wasn't found.
-   */
+  // Cancel a timer, storing the magic cookie in act (if nonzero).
+  // Calls the functor if dont_call_handle_close is 0 and returns 1
+  // on success
   virtual int cancel (long timer_id,
                       const void **act = 0,
                       int dont_call_handle_close = 1);
 
-  /**
-   * Run the <functor> for all timers whose values are <=
-   * <ACE_OS::gettimeofday>.  Also accounts for <timer_skew>.  Returns
-   * the number of timers canceled.
-   */
+  /// Run the <functor> for all timers whose values are <=
+  /// <ACE_OS::gettimeofday>.  Also accounts for <timer_skew>.  Returns
+  /// the number of timers canceled.
   virtual int expire (void);
 
-  /**
-   * Run the <functor> for all timers whose values are <= <cur_time>.
-   * This does not account for <timer_skew>.  Returns the number of
-   * timers canceled.
-   */
+  // Run the <functor> for all timers whose values are <= <cur_time>.
+  // This does not account for <timer_skew>.  Returns the number of
+  // timers canceled.
   int expire (const ACE_Time_Value &);
 
   /// Returns a pointer to this <ACE_Timer_Queue_T>'s iterator.
@@ -230,13 +190,17 @@ private:
   ACE_Timer_Node_T<TYPE> *freelist_;
 
   // = Don't allow these operations for now.
-  ACE_UNIMPLEMENTED_FUNC (ACE_Timer_Wheel_T (const ACE_Timer_Wheel_T<TYPE, FUNCTOR, ACE_LOCK> &))
-  ACE_UNIMPLEMENTED_FUNC (void operator= (const ACE_Timer_Wheel_T<TYPE, FUNCTOR, ACE_LOCK> &))
+  ACE_UNIMPLEMENTED_FUNC (
+    ACE_Timer_Wheel_T (const ACE_Timer_Wheel_T<TYPE, FUNCTOR, ACE_LOCK> &))
+  ACE_UNIMPLEMENTED_FUNC (
+    void operator= (const ACE_Timer_Wheel_T<TYPE, FUNCTOR, ACE_LOCK> &))
 };
 
-#if defined (ACE_TEMPLATES_REQUIRE_SOURCE) && !defined (ACE_HAS_BROKEN_HPUX_TEMPLATES)
+#if defined (ACE_TEMPLATES_REQUIRE_SOURCE) 
+#if !defined (ACE_HAS_BROKEN_HPUX_TEMPLATES)
 #include "ace/Timer_Wheel_T.cpp"
-#endif /* ACE_TEMPLATES_REQUIRE_SOURCE && !ACE_HAS_BROKEN_HPUX_TEMPLATES */
+#endif /* !ACE_HAS_BROKEN_HPUX_TEMPLATES */
+#endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
 
 #if defined (ACE_TEMPLATES_REQUIRE_PRAGMA)
 #pragma implementation ("Timer_Wheel_T.cpp")
