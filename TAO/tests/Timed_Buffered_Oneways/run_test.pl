fab172778c16ca742f996961742384a7aeb0b912
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-01-26 22:02:18 +0000

    ChangeLogTag:Fri Jan 26 10:42:07 2001  Darrell Brunsch <brunsch@uci.edu>

--- TAO/tests/Timed_Buffered_Oneways/run_test.pl
+++ TAO/tests/Timed_Buffered_Oneways/run_test.pl
@@ -5,39 +5,36 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-unshift @INC, '../../../bin';
-require ACEutils;
+use lib "../../../bin";
+use PerlACE::Run_Test;
 use Cwd;
 
-$cwd = getcwd();
-$iorfile = "$cwd$DIR_SEPARATOR" . "ior";
+$iorfile = PerlACE::LocalFile ("ior");
 
 unlink $iorfile;
 
-ACE::checkForTarget($cwd);
+$SV = new PerlACE::Process ("server", "-o $iorfile");
+$CL = new PerlACE::Process ("client", "-k file://$iorfile -x");
 
-$SV = Process::Create ($EXEPREFIX."server$EXE_EXT", "-o $iorfile");
+$SV->Spawn ();
 
-if (ACE::waitforfile_timed ($iorfile, 5) == -1) {
+if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
   print STDERR "ERROR: cannot find file <$iorfile>\n";
   $SV->Kill (); $SV->TimedWait (1);
   exit 1;
 }
 
-$CL = Process::Create ($EXEPREFIX."client$EXE_EXT ", "-k file://$iorfile -x");
+$client = $CL->SpawnWaitKill (200);
 
-$client = $CL->TimedWait (200);
 if ($client == -1) {
   $time = localtime;
   print STDERR "ERROR: client timedout at $time\n";
-  $CL->Kill (); $CL->TimedWait (1);
 }
 
-$server = $SV->TimedWait (100);
+$server = $SV->WaitKill (100);
 if ($server == -1) {
   $time = localtime;
   print STDERR "ERROR: server timedout at $time\n";
-  $SV->Kill (); $SV->TimedWait (1);
 }
 
 unlink $iorfile;
