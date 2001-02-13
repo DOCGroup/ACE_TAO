a80b276aef65b39950ff786beb54547a97396937
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-02-13 09:21:43 +0000

    ChangeLogTag:Tue Feb 13 01:14:26 2001  Ossama Othman  <ossama@uci.edu>

--- trunk/TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_ORBInitializer.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_ORBInitializer.cpp
@@ -4,7 +4,9 @@
 
 #include "SSLIOP_ORBInitializer.h"
 
-ACE_RCSID (TAO_SSLIOP, SSLIOP_ORBInitializer, "$Id$")
+ACE_RCSID (TAO_SSLIOP,
+           SSLIOP_ORBInitializer,
+           "$Id$")
 
 #include "SSLIOP_Current.h"
 #include "SSLIOP_Invocation_Interceptor.h"
@@ -19,14 +21,6 @@ TAO_SSLIOP_ORBInitializer::TAO_SSLIOP_ORBInitializer (int no_protection)
 
 void
 TAO_SSLIOP_ORBInitializer::pre_init (
-    PortableInterceptor::ORBInitInfo_ptr
-    TAO_ENV_ARG_DECL_NOT_USED)
-  ACE_THROW_SPEC ((CORBA::SystemException))
-{
-}
-
-void
-TAO_SSLIOP_ORBInitializer::post_init (
     PortableInterceptor::ORBInitInfo_ptr info
     TAO_ENV_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException))
@@ -43,7 +37,7 @@ TAO_SSLIOP_ORBInitializer::post_init (
     {
       if (TAO_debug_level > 0)
         ACE_ERROR ((LM_ERROR,
-                    "(%P|%t) SSLIOP_ORBInitializer::post_init:\n"
+                    "(%P|%t) SSLIOP_ORBInitializer::pre_init:\n"
                     "(%P|%t)    Unable to narrow "
                     "\"PortableInterceptor::ORBInitInfo_ptr\" to\n"
                     "(%P|%t)   \"TAO_ORBInitInfo *.\"\n"));
@@ -61,6 +55,10 @@ TAO_SSLIOP_ORBInitializer::post_init (
   ACE_CHECK;
 
   // Create the SSLIOP::Current object.
+  // Note that a new SSLIOP::Current object is created for each ORB.
+  // It wouldn't be very used to share security context information
+  // with another ORB that isn't configured with security, for
+  // example.
   SSLIOP::Current_ptr current = SSLIOP::Current::_nil ();
   ACE_NEW_THROW_EX (current,
                     TAO_SSLIOP_Current (tss_slot, orb_id.in ()),
@@ -77,6 +75,33 @@ TAO_SSLIOP_ORBInitializer::post_init (
   info->register_initial_reference ("SSLIOPCurrent",
                                     ssliop_current.in (),
                                     ACE_TRY_ENV);
+  ACE_CHECK;
+}
+
+void
+TAO_SSLIOP_ORBInitializer::post_init (
+    PortableInterceptor::ORBInitInfo_ptr info
+    TAO_ENV_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  TAO_ENV_ARG_DEFN;
+
+  // Note we do not store the SSLIOP::Current as a class member since
+  // we need to avoid potential problems where the same
+  // SSLIOP::Current object is shared between ORBs.  Each ORB should
+  // have its own unique SSLIOP::Current object.  By obtaining the
+  // SSLIOP::Current object from the resolve_initial_references()
+  // mechanism, we are guaranteed that the SSLIOP::Current object is
+  // specific to the ORB being initialized since a new SSLIOP::Current
+  // object is registered for each ORB in this ORBInitializer's
+  // pre_init() method.
+
+  CORBA::Object_var obj =
+    info->resolve_initial_references ("SSLIOPCurrent", ACE_TRY_ENV);
+  ACE_CHECK;
+
+  SSLIOP::Current_var ssliop_current =
+    SSLIOP::Current::_narrow (obj.in (), ACE_TRY_ENV);
   ACE_CHECK;
 
   // Create the SSLIOP secure invocation server request interceptor.
