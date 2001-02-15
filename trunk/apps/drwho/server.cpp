520ecaea0075933bcb7c52ecb2840ede1c79da6b
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-02-15 12:16:53 +0000

    ChangeLogTag:Thu Feb 15 03:36:46 2001  Krishnakumar B  <kitty@cs.wustl.edu>

--- trunk/apps/drwho/server.cpp
+++ trunk/apps/drwho/server.cpp
@@ -25,7 +25,7 @@
 #include "ace/Log_Msg.h"
 
 static char *
-tstamp (void)
+time_stamp (void)
 {
   time_t time_now;
   char *temp;
@@ -43,7 +43,7 @@ exit_server (int sig)
 {
   ACE_DEBUG ((LM_DEBUG,
               "%s exiting on signal %S\n",
-              tstamp (),
+              time_stamp (),
               sig));
   ACE_OS::exit (0);
 }
