d78bc01affbcdb68c6d8017597c56e26da760b3a
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-02-20 18:06:27 +0000

    ChangeLogTag: Tue Feb 20 12:02:30 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/tao/Strategies/UIOP_Connection_Handler.cpp
+++ TAO/tao/Strategies/UIOP_Connection_Handler.cpp
@@ -180,6 +180,9 @@ TAO_UIOP_Connection_Handler::handle_close (ACE_HANDLE handle,
       // passed to the reactor on ORB destruction.
       this->is_registered (0);
 
+      // Close the handle..
+      this->peer ().close ();
+
       // Decrement the reference count
       this->decr_ref_count ();
     }
