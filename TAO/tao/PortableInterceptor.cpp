fad4968a88cbfa1f4a7cdc222459f0e742942b78
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-03-02 19:39:10 +0000

    ChangeLogTag:Fri Mar  2 11:37:43 2001  Ossama Othman  <ossama@uci.edu>

--- TAO/tao/PortableInterceptor.cpp
+++ TAO/tao/PortableInterceptor.cpp
@@ -3,13 +3,11 @@
 #include "tao/corbafwd.h"
 #include "tao/PortableInterceptor.h"
 
-#if (TAO_HAS_INTERCEPTORS == 1)
+#if TAO_HAS_INTERCEPTORS == 1
 
 #if !defined (__ACE_INLINE__)
 #include "tao/PortableInterceptor.i"
 #endif /* defined INLINE */
-
-#endif /* TAO_HAS_INTERCEPTORS == 1 */
 
 TAO_ClientRequestInterceptor_Adapter::
 ~TAO_ClientRequestInterceptor_Adapter (void)
@@ -22,3 +20,5 @@ TAO_ServerRequestInterceptor_Adapter::
 ~TAO_ServerRequestInterceptor_Adapter (void)
 {
 }
+
+#endif /* TAO_HAS_INTERCEPTORS == 1 */
