73aa4e1258e6567f4fa879500b26939b1a33b0be
fhunleth <fhunleth@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-04 16:07:44 +0000

    ChangeLogTag: Sun Mar  4 10:03:59 2001  Frank Hunleth  <fhunleth@cs.wustl.edu>

--- trunk/TAO/examples/PluggableUDP/tests/server.cpp
+++ trunk/TAO/examples/PluggableUDP/tests/server.cpp
@@ -62,7 +62,9 @@ main (int argc, char *argv[])
       ACE_TRY_CHECK;
 
       CORBA::Object_var poa_object =
-        orb->resolve_initial_references("RootPOA");
+        orb->resolve_initial_references("RootPOA", ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+
       if (CORBA::is_nil (poa_object.in ()))
         ACE_ERROR_RETURN ((LM_ERROR,
                            " (%P|%t) Unable to initialize the POA.\n"),
@@ -127,7 +129,8 @@ main (int argc, char *argv[])
       ACE_TRY_CHECK;
 
 
-      UDP_var udp_var = UDP::_narrow (obj, ACE_TRY_ENV);
+      UDP_var udp_var = UDP::_narrow (obj.in (), ACE_TRY_ENV);
+
       //*/
       //UDP_var udp_var = udp_i._this (ACE_TRY_ENV);
       ACE_TRY_CHECK;
