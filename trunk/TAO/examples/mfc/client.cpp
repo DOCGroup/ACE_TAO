cd128eef4d284e9311a7faf1b4fd13a3908c2bfd
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-05 13:59:41 +0000

    ChangeLogTag: Mon Mar  5 07:59:00 2001  Michael Kircher  <Michael.Kircher@mchp.siemens.de>

--- trunk/TAO/examples/mfc/client.cpp
+++ trunk/TAO/examples/mfc/client.cpp
@@ -7,40 +7,60 @@
 int
 main (int argc, char *argv[])
 {
-  const char *orb_name = "";
+  ACE_TRY_NEW_ENV
+    {
+      const char *orb_name = "";
 
-  cout << "Initializing the ORB!" << endl;
-  CORBA::ORB_var the_orb = CORBA::ORB_init (argc,
-                                            argv,
-                                            orb_name);
-  CORBA::Object_var orb_obj =
-    the_orb->resolve_initial_references ("RootPOA");
+      cout << "Initializing the ORB!" << endl;
+      CORBA::ORB_var the_orb = CORBA::ORB_init (argc,
+                                                argv,
+                                                orb_name,
+                                                ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  PortableServer::POA_var the_root_poa =
-    PortableServer::POA::_narrow (orb_obj.in ());
-  PortableServer::POAManager_var the_poa_manager =
-    the_root_poa->the_POAManager ();
+      CORBA::Object_var orb_obj =
+        the_orb->resolve_initial_references ("RootPOA", ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  // Retrieving the servants IOR from a file
-  cout << "Reading the IOR!" << endl;
+      PortableServer::POA_var the_root_poa =
+        PortableServer::POA::_narrow (orb_obj.in (), ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  const char *filename =
-    "file://ior.txt";
+      PortableServer::POAManager_var the_poa_manager =
+        the_root_poa->the_POAManager (ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  orb_obj =
-    the_orb->string_to_object (filename);
+      // Retrieving the servants IOR from a file
+      cout << "Reading the IOR!" << endl;
 
-  cout << "Narrowing the IOR!" << endl;
+      const char *filename =
+        "file://ior.txt";
 
-  W32_Test_Interface_var mycall =
-    W32_Test_Interface::_narrow (orb_obj.in ());
+      orb_obj =
+        the_orb->string_to_object (filename, ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-  cout << "Sending the Request!" << endl;
-  char *response = mycall->getresponse (1);
-  cout << "The answer ..." << response << endl;
+      cout << "Narrowing the IOR!" << endl;
 
-  // Free up the string.
-  CORBA::string_free (response);
+      W32_Test_Interface_var mycall =
+        W32_Test_Interface::_narrow (orb_obj.in (), ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+
+      cout << "Sending the Request!" << endl;
+      char *response = mycall->getresponse (1);
+      cout << "The answer ..." << response << endl;
+
+      // Free up the string.
+      CORBA::string_free (response);
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Catched exception:");
+      return 1;
+    }
+  ACE_ENDTRY;
+  ACE_CHECK_RETURN (-1);
 
   return 0;
 }
