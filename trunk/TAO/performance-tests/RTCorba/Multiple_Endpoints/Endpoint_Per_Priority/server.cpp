99bf576b60eb43b3cc1c4b5e4fd249fd44b3746c
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-06 03:15:27 +0000

    ChangeLogTag:Mon Mar  5 19:14:03 2001  Carlos O'Ryan  <coryan@uci.edu>

--- trunk/TAO/performance-tests/RTCorba/Multiple_Endpoints/Endpoint_Per_Priority/server.cpp
+++ trunk/TAO/performance-tests/RTCorba/Multiple_Endpoints/Endpoint_Per_Priority/server.cpp
@@ -72,7 +72,9 @@ main (int argc, char *argv[])
       ACE_TRY_CHECK;
 
       CORBA::Object_var poa_object =
-        orb->resolve_initial_references("RootPOA");
+        orb->resolve_initial_references("RootPOA", ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+
       if (CORBA::is_nil (poa_object.in ()))
         ACE_ERROR_RETURN ((LM_ERROR,
                            " (%P|%t) Unable to initialize the POA.\n"),
