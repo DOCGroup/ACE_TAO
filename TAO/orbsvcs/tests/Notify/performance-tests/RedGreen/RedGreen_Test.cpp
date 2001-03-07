e454c9e29160491652d759fbf6aeca56dc1763c3
oci <oci@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-07 14:49:22 +0000

    ChangeLogTag: Wed Mar  7 08:39:56 2001  Chad Elliott  <elliott_c@ociweb.com>

--- TAO/orbsvcs/tests/Notify/performance-tests/RedGreen/RedGreen_Test.cpp
+++ TAO/orbsvcs/tests/Notify/performance-tests/RedGreen/RedGreen_Test.cpp
@@ -619,10 +619,8 @@ Worker::svc (void)
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
-template class  ACE_Atomic_Op<TAO_SYNCH_MUTEX, int>;
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
-#pragma instantiate ACE_Atomic_Op<TAO_SYNCH_MUTEX, int>
 
 #endif /*ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
