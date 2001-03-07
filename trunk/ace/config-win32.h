727d94077c250af711f43efe9e8a35c3844839fb
brunsch <brunsch@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-07 17:09:04 +0000

    ChangeLogTag:Wed Mar  7 08:57:42 2001  Darrell Brunsch <brunsch@uci.edu>

--- trunk/ace/config-win32.h
+++ trunk/ace/config-win32.h
@@ -29,7 +29,7 @@
 // Include the platform specific config file (Windows CE is special)
 
 #if defined (_WIN32_WCE)
-#	include "ace/config-win32-wince.h"
+#	include "ace/config-WinCE.h"
 #else /* _WIN32_WCE */
 #   include "ace/config-win32-common.h"
 #endif /* _WIN32_WCE */
