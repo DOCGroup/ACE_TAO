78bcb1383d0679d171c589b34745de3d5df0ff46
schmidt <douglascraigschmidt@users.noreply.github.com>
2001-03-07 20:48:44 +0000

    ChangeLogTag:Wed Mar  7 12:41:05 2001  Douglas C. Schmidt  <schmidt@tango.cs.wustl.edu>

--- ace/Reactor_Impl.h
+++ ace/Reactor_Impl.h
@@ -349,18 +349,18 @@ public:
    * value to the <event_handler>'s <handle_timeout> callback method.
    * If <interval> is != to <ACE_Time_Value::zero> then it is used to
    * reschedule the <event_handler> automatically, also using relative
-   * time.  This method returns a <timer_id> that uniquely identifies
-   * the <event_handler> in an internal list.  This <timer_id> can be
+   * time.  This method returns a timer id that uniquely identifies
+   * the <event_handler> in an internal list.  This timer id can be
    * used to cancel an <event_handler> before it expires.  The
    * cancellation ensures that <timer_ids> are unique up to values of
    * greater than 2 billion timers.  As long as timers don't stay
    * around longer than this there should be no problems with
    * accidentally deleting the wrong timer.  Returns -1 on failure
-   * (which is guaranteed never to be a valid <timer_id>.
+   * (which is guaranteed never to be a valid timer id.
    */
   virtual long schedule_timer (ACE_Event_Handler *event_handler,
                                const void *arg,
-                               const ACE_Time_Value &delta,
+                               const ACE_Time_Value &delay,
                                const ACE_Time_Value &interval = ACE_Time_Value::zero) = 0;
 
   /**
