39c5c53af4af9e17ace59b31f58bceb4effb1234
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-03-07 16:19:52 +0000

    ChangeLogTag:Wed Mar 07 08:01:16 2001  Ossama Othman  <ossama@uci.edu>

--- TAO/tao/Stub.cpp
+++ TAO/tao/Stub.cpp
@@ -111,7 +111,9 @@ TAO_Stub::~TAO_Stub (void)
           // it being destroy()ed, then no connectors exist so do not
           // reset the hint in case it points to non-existent
           // connection handler.
-          this->profile_in_use_->endpoint ()->reset_hint ();
+          TAO_Endpoint* ep = this->profile_in_use_->endpoint ();
+          if (ep)
+            ep->reset_hint ();
         }
 
       // decrease reference count on profile
