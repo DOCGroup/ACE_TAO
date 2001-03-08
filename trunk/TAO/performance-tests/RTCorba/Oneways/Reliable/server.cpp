c24ed1e9cfd5f6d3723fcf0855b6abd997f61b1f
fhunleth <fhunleth@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-08 17:03:31 +0000

    ChangeLogTag: Thu Mar  8 11:01:45 2001  Frank Hunleth  <fhunleth@cs.wustl.edu>

--- trunk/TAO/performance-tests/RTCorba/Oneways/Reliable/server.cpp
+++ trunk/TAO/performance-tests/RTCorba/Oneways/Reliable/server.cpp
@@ -107,7 +107,8 @@ main (int argc, char *argv[])
         }
 
       CORBA::Object_var poa_object =
-        orb->resolve_initial_references ("RootPOA");
+        orb->resolve_initial_references ("RootPOA",
+                                         ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       PortableServer::POA_var root_poa =
