9cfea4454f588b155ea9b6d6dafc90b6c34a871f
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-10 00:19:13 +0000

    ChangeLogTag: Fri Mar 09 16:16:56 2001  Irfan Pyarali  <irfan@cs.wustl.edu>

--- trunk/TAO/examples/POA/FindPOA/FindPOA.cpp
+++ trunk/TAO/examples/POA/FindPOA/FindPOA.cpp
@@ -22,6 +22,32 @@
 
 ACE_RCSID(FindPOA, FindPOA, "$Id$")
 
+void
+find_non_existant_POA (PortableServer::POA_ptr parent,
+                       const char *child_poa_name,
+                       int activate)
+{
+  int expected_exception_raised = 0;
+
+  // New environment.
+  ACE_TRY_NEW_ENV
+    {
+      // Try to find child poa.
+      PortableServer::POA_var child_poa =
+        parent->find_POA (child_poa_name,
+                          activate,
+                          ACE_TRY_ENV);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCH (PortableServer::POA::AdapterNonExistent, foo)
+    {
+      expected_exception_raised = 1;
+    }
+  ACE_ENDTRY;
+
+  ACE_ASSERT (expected_exception_raised);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -53,6 +79,12 @@ main (int argc, char **argv)
       poa_manager->activate (ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
+      // Try to find a non-existant POA.  Since the Adapter Activator
+      // has not been installed yet, this call should fail.
+      find_non_existant_POA (root_poa.in (),
+                             "firstPOA",
+                             1);
+
       // Get a TAO_Adapter_Activator reference
       PortableServer::AdapterActivator_var activator =
         new TAO_Adapter_Activator (poa_manager.in ());
@@ -77,6 +109,13 @@ main (int argc, char **argv)
                              1,
                              ACE_TRY_ENV);
       ACE_TRY_CHECK;
+
+      // Try to find a non-existant POA.  Even though the Adapter
+      // Activator has been installed, this call should fail because
+      // the activate (if not found) flag is 0.
+      find_non_existant_POA (root_poa.in (),
+                             "thirdPOA",
+                             0);
 
       // Get the names of all the POAs
       CORBA::String_var root_poa_name =
