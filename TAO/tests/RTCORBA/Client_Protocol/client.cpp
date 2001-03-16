07a1777d618c58b950a1092ab9b27d47218e24d1
Angelo Corsaro <angelo@icorsaro.net>
2001-03-16 21:56:03 +0000

    Fri Mar 16 15:45:21 2001  Angelo Corsaro  <angelo@cs.wustl.edu>

--- TAO/tests/RTCORBA/Client_Protocol/client.cpp
+++ TAO/tests/RTCORBA/Client_Protocol/client.cpp
@@ -72,10 +72,10 @@ exception_test (Test_ptr server,
     {
       server->test_method (ACE_TRY_ENV);
       ACE_TRY_CHECK;
-      ACE_DEBUG ((LM_DEBUG, msg));
-    }
+     }
   ACE_CATCH (CORBA::INV_POLICY, ex)
     {
+      ACE_DEBUG ((LM_DEBUG, msg));
       ACE_DEBUG ((LM_DEBUG,
                   "INV_POLICY exception is caught as expected.\n"));
     }
