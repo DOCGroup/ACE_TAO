6bce0f30b62d21625a1b547958f6c4523e7db6bf
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-27 04:23:10 +0000

    ChangeLogTag: Mon Mar 26 22:20:34 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- trunk/TAO/tao/IIOP_Connection_Handler.cpp
+++ trunk/TAO/tao/IIOP_Connection_Handler.cpp
@@ -143,7 +143,7 @@ TAO_IIOP_Connection_Handler::activate (long flags,
                  THR_BOUND));
 
   // Set the id in the transport now that we're active.
-  this->transport ()->id (ACE_reinterpret_cast(int,this->get_handle ()));
+  this->transport ()->id ((int) this->get_handle ());
 
   return TAO_IIOP_SVC_HANDLER::activate (flags,
                                          n_threads,
