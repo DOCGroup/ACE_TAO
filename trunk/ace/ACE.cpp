6baa324a1ec94b32a9e99c0c77e2ee33d2d81a8e
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-28 04:49:49 +0000

    ChangeLogTag:Tue Mar 27 19:54:11 2001  Carlos O'Ryan  <coryan@uci.edu>

--- trunk/ace/ACE.cpp
+++ trunk/ace/ACE.cpp
@@ -2215,8 +2215,12 @@ ACE::handle_ready (ACE_HANDLE handle,
   fds.events = read_ready ? POLLIN : POLLOUT;
   fds.revents = 0;
 
-  int result = ACE_OS::poll (&fds, 1, *timeout);
+  int result;
 
+  if (timeout == 0)
+    result = ACE_OS::poll (&fds, 1, 0);
+  else
+    result = ACE_OS::poll (&fds, 1, *timeout);
 #else
 
   ACE_Handle_Set handle_set;
