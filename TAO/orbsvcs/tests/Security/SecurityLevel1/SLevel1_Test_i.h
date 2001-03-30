f598f37b74130bd5aacec33ab23589024497d637
doccvs <doccvs@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-30 03:15:55 +0000

    ChangeLogTag: Thu Mar 29 19:06:36 2001  Priyanka Gontla  <pgontla@ece.uci.edu>

--- TAO/orbsvcs/tests/Security/SecurityLevel1/SLevel1_Test_i.h
+++ TAO/orbsvcs/tests/Security/SecurityLevel1/SLevel1_Test_i.h
@@ -1,38 +1,50 @@
-// $Id$
+// -*- C++ -*-
 
-// ============================================================================
-//
-// = LIBRARY
-//   orbsvcs/examples/Security/SecurityLevel1
-//
-// = FILENAME
-//   SLevel1_Test_i.h
-//
-// = AUTHOR
-//   Priyanka Gontla <gontla_p@ociweb.com>
-//
-// ============================================================================
+//=============================================================================
+/**
+ * @file SLevel1_Test_i.h
+ *
+ * $Id$
+ *
+ * Implementation header for the Security Level1test.
+ *
+ * @author Priyanka Gontla <pgontla@doc.ece.uci.edu>
+ */
+//=============================================================================
 
 #ifndef TAO_SLEVEL1_TEST_I_H
 #define TAO_SLEVEL1_TEST_I_H
 
 #include "SLevel1_TestS.h"
+#include "orbsvcs/SecurityLevel1C.h"
+#include "openssl/x509.h"
 
 class SLevel1_Server_i : public POA_SLevel1_Server
 {
  public:
   /// Constructor
-  SLevel1_Server_i ();
-  
+  SLevel1_Server_i (CORBA::ORB_ptr,
+                    SecurityLevel1::Current_ptr ss_current);
+
   /// Simple method invoked to test if the client reached server etc.
   CORBA::Boolean authorize_level1 (CORBA::Environment &)
     ACE_THROW_SPEC ((CORBA::SystemException));
 
-  CORBA::Boolean authorize_level2 (CORBA::Environment &)
+  void shutdown (CORBA::Environment &ACE_TRY_ENV =
+                 TAO_default_environment ())
     ACE_THROW_SPEC ((CORBA::SystemException));
-  
+
+  X509 *create_check_cert ();
+
   static int authorized_1;
-  static int authorized_2;
+
+ private:
+
+  /// Reference to the ORB.
+  CORBA::ORB_var orb_;
+
+  /// Reference to the "SecurityCurrent" object.
+  SecurityLevel1::Current_var ss_current_;
 
 };
 
