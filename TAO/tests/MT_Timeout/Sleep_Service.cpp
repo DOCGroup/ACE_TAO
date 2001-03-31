ceaff5bbd5a73647c20c499fe39116abc733fd5b
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-03-31 03:58:21 +0000

    ChangeLogTag:Fri Mar 30 19:57:14 2001  Carlos O'Ryan  <coryan@uci.edu>

---
+++ TAO/tests/MT_Timeout/Sleep_Service.cpp
@@ -0,0 +1,30 @@
+//
+// $Id$
+//
+#include "Sleep_Service.h"
+
+ACE_RCSID(MT_Timeout, Sleep_Service, "$Id$")
+
+Sleep_Service::Sleep_Service (CORBA::ORB_ptr orb)
+  : orb_ (CORBA::ORB::_duplicate (orb))
+{
+}
+
+void
+Sleep_Service::go_to_sleep (CORBA::ULong microseconds,
+                            CORBA::Environment &)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  CORBA::ULong  secs = microseconds / 1000000;
+  CORBA::ULong usecs = microseconds % 1000000;
+  ACE_Time_Value tv (secs, usecs);
+
+  ACE_OS::sleep (tv);
+}
+
+void
+Sleep_Service::shutdown (CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  this->orb_->shutdown (0, ACE_TRY_ENV);
+}
