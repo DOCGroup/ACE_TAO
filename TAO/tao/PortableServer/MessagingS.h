2b4f307f44e1cd03d1dcb9cc20c334fea727a419
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-02 01:39:00 +0000

    ChangeLogTag:Sun Apr  1 18:35:16 2001  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/tao/PortableServer/MessagingS.h
+++ TAO/tao/PortableServer/MessagingS.h
@@ -66,7 +66,7 @@ TAO_NAMESPACE  POA_Messaging
   class _TAO_ReplyHandler_Direct_Proxy_Impl;
   class _TAO_ReplyHandler_Strategized_Proxy_Broker;
 
-  class TAO_Export ReplyHandler :  public virtual PortableServer::ServantBase
+  class TAO_PortableServer_Export ReplyHandler :  public virtual PortableServer::ServantBase
   {
   protected:
     ReplyHandler (void);
@@ -139,7 +139,7 @@ TAO_NAMESPACE  POA_Messaging
   //               Strategized Proxy Broker Declaration
   //
 
-  class TAO_Export _TAO_ReplyHandler_Strategized_Proxy_Broker : public virtual ::Messaging::_TAO_ReplyHandler_Proxy_Broker
+  class TAO_PortableServer_Export _TAO_ReplyHandler_Strategized_Proxy_Broker : public virtual ::Messaging::_TAO_ReplyHandler_Proxy_Broker
   {
   public:
     _TAO_ReplyHandler_Strategized_Proxy_Broker (void);
@@ -187,7 +187,7 @@ TAO_NAMESPACE  POA_Messaging
 //                    ThruPOA  Impl. Declaration
 //
 
-class TAO_Export _TAO_ReplyHandler_ThruPOA_Proxy_Impl :
+class TAO_PortableServer_Export _TAO_ReplyHandler_ThruPOA_Proxy_Impl :
   public virtual ::Messaging::_TAO_ReplyHandler_Proxy_Impl,
   public virtual TAO_ThruPOA_Object_Proxy_Impl
 {
@@ -208,7 +208,7 @@ public:
 //                    Direct  Impl. Declaration
 //
 
-class TAO_Export _TAO_ReplyHandler_Direct_Proxy_Impl :
+class TAO_PortableServer_Export _TAO_ReplyHandler_Direct_Proxy_Impl :
   public virtual Messaging::_TAO_ReplyHandler_Proxy_Impl,
   public virtual TAO_Direct_Object_Proxy_Impl
 {
@@ -228,7 +228,7 @@ public:
 #if defined (ACE_HAS_USING_KEYWORD)
 // TIE class: Refer to CORBA v2.2, Section 20.34.4
 template <class T>
-class TAO_Export ReplyHandler_tie : public ReplyHandler
+class TAO_PortableServer_Export ReplyHandler_tie : public ReplyHandler
 {
 public:
   ReplyHandler_tie (T &t);
