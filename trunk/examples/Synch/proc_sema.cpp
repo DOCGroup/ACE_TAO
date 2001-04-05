14649e1c8823131f268f18e1fdc4d2f030227a6a
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-05 09:30:57 +0000

    ChangeLogTag:Thu Apr 05 03:26:13 2001  Nanbor Wang  <nanbor@cs.wustl.edu>

---
+++ trunk/examples/Synch/proc_sema.cpp
@@ -0,0 +1,95 @@
+/// $Id$
+
+#include "ace/Process_Semaphore.h"
+#include "ace/Synch_T.h"
+#include "ace/Get_Opt.h"
+
+int producer (ACE_SYNCH_PROCESS_SEMAPHORE &sema,
+              int iter)
+{
+  for (int i = iter; i > 0; --i)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  "Try acquiring the semaphore (%d): ",
+                  i));
+
+      int result = sema.release ();
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "%s",
+                  (result != 0 ? "fail\n" : "succeed\n")));
+    }
+  return 0;
+}
+
+int consumer (ACE_SYNCH_PROCESS_SEMAPHORE &sema,
+              int iter)
+{
+  for (int i = iter; i > 0; --i)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  "Try acquiring the semaphore (%d): ",
+                  i));
+
+      int result = sema.acquire ();
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "%s",
+                  (result != 0 ? "fail\n" : "succeed\n")));
+    }
+  return 0;
+}
+
+
+int main (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt getopt (argc, argv, "csn:xi:d:");
+
+  int is_consumer = 1;          // By default, make us a consumer.
+  int delete_sema = 0;
+  int iteration = 0;
+  int exit_delay = 0;
+  ACE_TCHAR *sema_name = ACE_TEXT ("Process_Semaphore_Test");
+
+  int opt;
+
+  while ((opt = getopt ()) != -1)
+    {
+      switch (opt)
+        {
+        case 'c':               // Make us a consumer.
+          is_consumer = 1;
+          break;
+        case 's':               // Make us a supplier.
+          is_consumer = 0;
+          break;
+        case 'x':               // Remove the semaphore after we're done.
+          delete_sema = 1;
+          break;
+        case 'n':               // Specify the name of the semaphore.
+          sema_name = getopt.optarg;
+          break;
+        case 'i':               // Number of acquire/release we'll perform.
+          iteration = ACE_OS::atoi (getopt.optarg);
+          break;
+        case 'd':
+          exit_delay = ACE_OS::atoi (getopt.optarg);
+          break;
+        default:
+          return -1;
+        }
+    };
+
+  ACE_SYNCH_PROCESS_SEMAPHORE sema (0, sema_name);
+
+  if (is_consumer != 0)
+    consumer (sema, iteration);
+  else
+    producer (sema, iteration);
+
+  ACE_OS::sleep (exit_delay);
+
+  if (delete_sema != 0)
+    sema.remove ();
+  return 0;
+}
