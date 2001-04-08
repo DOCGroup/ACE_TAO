e9c91aad5aaaa513c6cfeeb86123f0b118d1cd23
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-08 17:13:42 +0000

    ChangeLogTag:Sun Apr 08 12:10:30 2001  Nanbor Wang  <nanbor@cs.wustl.edu>

--- trunk/examples/IPC_SAP/SOCK_SAP/CPP-memclient.cpp
+++ trunk/examples/IPC_SAP/SOCK_SAP/CPP-memclient.cpp
@@ -21,9 +21,9 @@ run_client (void)
 {
   ACE_MEM_Connector connector;
   ACE_MEM_Stream stream;
-  ACE_INET_Addr server_addr (ACE_DEFAULT_SERVER_PORT, "COYOTE");
+  ACE_MEM_Addr server_addr (ACE_DEFAULT_SERVER_PORT);
 
-  if (connector.connect (stream, server_addr) == -1)
+  if (connector.connect (stream, server_addr.get_remote_addr ()) == -1)
     ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "connect"), -1);
 
   char buf [MAXPATHLEN];
