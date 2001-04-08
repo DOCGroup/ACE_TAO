efe67fdf24910247a90f65a30d32396b44a1bb12
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-08 03:58:43 +0000

    ChangeLogTag:Sat Apr  7 20:52:38 2001  Carlos O'Ryan  <coryan@uci.edu>

---
+++ trunk/TAO/tests/Crash_On_Write/Oneway_Receiver.cpp
@@ -0,0 +1,16 @@
+//
+// $Id$
+//
+#include "Oneway_Receiver.h"
+
+ACE_RCSID(Crash_On_Write, Oneway_Receiver, "$Id$")
+
+Oneway_Receiver::Oneway_Receiver (void)
+{
+}
+
+void
+Oneway_Receiver::receive_oneway (CORBA::Environment &)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+}
