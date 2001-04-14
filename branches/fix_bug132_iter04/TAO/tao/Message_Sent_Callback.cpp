e9876bf493a366c9f30cf374121991a2c99befc6
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-14 20:02:29 +0000

    ChangeLogTag:Sat Apr 14 12:59:39 2001  Carlos O'Ryan  <coryan@uci.edu>

---
+++ branches/fix_bug132_iter04/TAO/tao/Message_Sent_Callback.cpp
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+// $Id$
+
+#include "Message_Sent_Callback.h"
+
+#if !defined (__ACE_INLINE__)
+# include "Message_Sent_Callback.inl"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID(tao, Message_Sent_Callback, "$Id$")
+
+TAO_Message_Sent_Callback::~TAO_Message_Sent_Callback (void)
+{
+}
