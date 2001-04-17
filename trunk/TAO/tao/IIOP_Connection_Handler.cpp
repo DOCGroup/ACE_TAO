109f55e07006822bbcb5d5f93d649b837ce4cb18
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-17 21:10:58 +0000

    ChangeLogTag:Tue Apr 17 15:48:20 2001  Nanbor Wang  <nanbor@cs.wustl.edu>

--- trunk/TAO/tao/IIOP_Connection_Handler.cpp
+++ trunk/TAO/tao/IIOP_Connection_Handler.cpp
@@ -247,13 +247,11 @@ TAO_IIOP_Connection_Handler::handle_close (ACE_HANDLE handle,
           // a reference to it.  This will eventually call
           // TAO_Transport::release ().
           this->transport (0);
-
-          this->peer ().close ();
         }
 
       // Follow usual Reactor-style lifecycle semantics and commit
       // suicide.
-      delete this;
+      this->destroy ();
     }
 
   return 0;
