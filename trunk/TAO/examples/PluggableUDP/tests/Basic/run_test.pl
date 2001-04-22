1c278ad01a936794be5fe5fec9c957864bd07c88
mk1 <mk1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-04-22 19:08:20 +0000

    ChangeLogTag: Sun Apr 22 14:00:00 2001  Michael Kircher  <Michael.Kircher@mchp.siemens.de>

--- trunk/TAO/examples/PluggableUDP/tests/Basic/run_test.pl
+++ trunk/TAO/examples/PluggableUDP/tests/Basic/run_test.pl
@@ -16,19 +16,19 @@ $CL = new PerlACE::Process ("client", "-k file://$iorfile -t 10 -i 10");
 
 $SV->Spawn ();
 
-if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
+if (PerlACE::waitforfile_timed ($iorfile, 10) == -1) {
     print STDERR "ERROR: could not find file <$iorfile>\n";
     $SV->Kill (); 
     exit 1;
 }
 
-$client = $CL->SpawnWaitKill (60);
+$client = $CL->SpawnWaitKill (90);
 if ($client != 0) {
     print STDERR "ERROR: client returned $client\n";
     $status = 1;
 }
 
-$server = $SV->WaitKill (5);
+$server = $SV->WaitKill (10);
 
 if ($server != 0) {
     print STDERR "ERROR: server returned $server\n";
