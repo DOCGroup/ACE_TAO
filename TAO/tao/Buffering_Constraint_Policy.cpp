65a2a435a1ce3f1e22a51a404023add2ebc98cff
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-05-04 01:57:24 +0000

    ChangeLogTag:Tue May 01 19:45:11 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/tao/Buffering_Constraint_Policy.cpp
+++ TAO/tao/Buffering_Constraint_Policy.cpp
@@ -60,7 +60,7 @@ TAO_Buffering_Constraint_Policy::clone (void) const
 
 TAO::BufferingConstraint
 TAO_Buffering_Constraint_Policy::buffering_constraint (CORBA::Environment &)
-  ACE_THROW_SPEC (())
+  ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return this->buffering_constraint_;
 }
