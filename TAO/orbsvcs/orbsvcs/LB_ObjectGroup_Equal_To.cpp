548d246ab1dd315166c631e559014e91f2d86240
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-05-07 20:25:41 +0000

    Inadvertently committed new LB files.  Reverted to old (working) ones.

--- TAO/orbsvcs/orbsvcs/LB_ObjectGroup_Equal_To.cpp
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-
-#include "LB_Equal_To.h"
-
-#if !defined (__ACE_INLINE__)
-#include "LB_Equal_To.inl"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID (TAO_LoadBalancing, LB_Equal_To, "$Id$")
--- TAO/orbsvcs/orbsvcs/LoadBalancing/LB_Replication_Manager.h
+++
@@ -1,6 +0,0 @@
-// -*- C++ -*-
-//
-// $Id$
-
-CORBA::Object_ptr
-TAO_LB_Replication_Manager::
