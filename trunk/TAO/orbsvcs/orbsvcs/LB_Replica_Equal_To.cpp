83760e079d06f0b43dedaa48a0bb126ce986fb25
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-05-07 20:25:41 +0000

    Inadvertently committed new LB files.  Reverted to old (working) ones.

--- trunk/TAO/orbsvcs/orbsvcs/LB_Replica_Equal_To.cpp
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-
-#include "LB_Replica_Equal_To.h"
-
-#if !defined (__ACE_INLINE__)
-#include "LB_Replica_Equal_To.inl"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID (TAO_LoadBalancing, LB_Replica_Equal_To, "$Id$")
