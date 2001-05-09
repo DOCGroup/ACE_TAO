aac06dd60b795e27ed64059590a78215ce89d4e9
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-05-09 21:08:59 +0000

    ChangeLogTag: Wed May  9 16:06:34 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- ace/OS.h
+++ ace/OS.h
@@ -114,6 +114,18 @@ typedef long      id_t;
 #   define ACE_NESTED_CLASS(TYPE, NAME) TYPE::NAME
 # endif /* ! ACE_HAS_BROKEN_NAMESPACES */
 
+#if !defined (VMIN)
+#define ACE_VMIN 4
+#else
+#define ACE_VMIN VMIN
+#endif /* VMIN */
+
+#if !defined (VTIME)
+#define ACE_VTIME 5
+#else
+#define ACE_VTIME VTIME
+#endif /* VTIME */
+
 
 # if !defined (ACE_DEFAULT_CLOSE_ALL_HANDLES)
 #   define ACE_DEFAULT_CLOSE_ALL_HANDLES 1
