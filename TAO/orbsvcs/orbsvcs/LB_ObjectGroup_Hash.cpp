e45935693fb4451b0b85abd65349fcb53c6c50f6
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-05-17 06:39:35 +0000

    Next generation load balancer files.

---
+++ TAO/orbsvcs/orbsvcs/LB_ObjectGroup_Hash.cpp
@@ -0,0 +1,11 @@
+// -*- C++ -*-
+
+#include "LB_Hash.h"
+
+#if !defined (__ACE_INLINE__)
+#include "LB_Hash.inl"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID (TAO_LoadBalancing,
+           LB_Hash,
+           "$Id$")
---
+++ TAO/orbsvcs/orbsvcs/LoadBalancing/ObjectGroupManager.cpp
@@ -0,0 +1,13 @@
+// -*- C++ -*-
+
+#include "ObjectGroupManager.h"
+
+
+ACE_RCSID (LoadBalancing,
+           ObjectGroupManager,
+           "$Id$")
+
+#if !defined (__ACE_INLINE__)
+#include "LoadBalancer_i.i"
+#endif /* __ACE_INLINE__ */
+
