8e48ab588d31a44d6f26936a922e911537214e42
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-05-24 15:17:32 +0000

    This commit was manufactured by cvs2svn to create branch 'bug_575_stage_0'.

---
+++ branches/bug_575_stage_0/TAO/tao/Pluggable_Messaging_Utils.cpp
@@ -0,0 +1,20 @@
+//$Id$
+#include "tao/Pluggable_Messaging_Utils.h"
+#include "tao/ORB_Core.h"
+
+#if !defined (__ACE_INLINE__)
+#include "tao/Pluggable_Messaging_Utils.i"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID(tao, Pluggable_Messaging_Utils, "$Id$")
+
+TAO_Pluggable_Reply_Params::TAO_Pluggable_Reply_Params (
+    TAO_ORB_Core *orb_core
+  )
+  : input_cdr_ (orb_core->create_input_cdr_data_block (
+                              ACE_CDR::DEFAULT_BUFSIZE
+                            ),
+                TAO_ENCAP_BYTE_ORDER,
+                orb_core)
+{
+}
---
+++ branches/bug_575_stage_0/TAO/tao/RT_Current.i
@@ -0,0 +1,7 @@
+//
+// $Id$
+//
+
+#if (TAO_HAS_RT_CORBA == 1)
+
+#endif /* TAO_RT_CURRENT_H */
