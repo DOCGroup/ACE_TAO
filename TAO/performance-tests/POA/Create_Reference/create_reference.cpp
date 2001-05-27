b7e7b8d0bf0931d4e1b67b9a60a242c157372dd9
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-05-27 17:08:28 +0000

    ChangeLogTag: Sun May 27 12:04:18 2001  Irfan Pyarali  <irfan@cs.wustl.edu>

--- TAO/performance-tests/POA/Create_Reference/create_reference.cpp
+++ TAO/performance-tests/POA/Create_Reference/create_reference.cpp
@@ -13,6 +13,7 @@ ACE_RCSID(Activation, server, "$Id$")
 
 int niterations = 10000;
 int do_dump_history = 0;
+ACE_UINT32 gsf;
 
 int
 parse_args (int argc, char *argv[])
@@ -35,7 +36,7 @@ parse_args (int argc, char *argv[])
       default:
         ACE_ERROR_RETURN ((LM_ERROR,
                            "usage:  %s "
-			   "-i <niterations> "
+                           "-i <niterations> "
                            "-h "
                            "\n",
                            argv [0]),
@@ -45,14 +46,14 @@ parse_args (int argc, char *argv[])
   return 0;
 }
 
-int
-main (int argc, char *argv[])
+void
+set_rt_scheduling (void)
 {
   int priority =
     (ACE_Sched_Params::priority_min (ACE_SCHED_FIFO)
      + ACE_Sched_Params::priority_max (ACE_SCHED_FIFO)) / 2;
   priority = ACE_Sched_Params::next_priority (ACE_SCHED_FIFO,
-                                                  priority);
+                                              priority);
   // Enable FIFO scheduling, e.g., RT scheduling class on Solaris.
 
   if (ACE_OS::sched_params (ACE_Sched_Params (ACE_SCHED_FIFO,
@@ -69,15 +70,78 @@ main (int argc, char *argv[])
         ACE_ERROR ((LM_ERROR,
                     "server (%P|%t): sched_params failed\n"));
     }
+}
+
+void
+object_creation_test (PortableServer::POA_ptr poa,
+                      int create_reference_with_id,
+                      CORBA::Environment &ACE_TRY_ENV)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              "\nCreating %d object references with %s\n",
+              niterations,
+              create_reference_with_id ?
+              "create_reference_with_id" :
+              "create_reference"));
+
+  ACE_Sample_History creation (niterations);
+
+  for (int i = 0; i != niterations; ++i)
+    {
+      char buf[16];
+      ACE_OS::sprintf (buf, "Object_%8.8x", i);
+      PortableServer::ObjectId_var oid
+        (PortableServer::string_to_ObjectId (buf));
+
+      ACE_hrtime_t start =
+        ACE_OS::gethrtime ();
+
+      CORBA::Object_var object;
+      if (create_reference_with_id)
+        {
+          object =
+            poa->create_reference_with_id (oid.in (),
+                                           "IDL:Test/Simple:1.0",
+                                           ACE_TRY_ENV);
+          ACE_TRY_CHECK;
+        }
+      else
+        {
+          object =
+            poa->create_reference ("IDL:Test/Simple:1.0",
+                                   ACE_TRY_ENV);
+          ACE_TRY_CHECK;
+        }
+
+      ACE_hrtime_t now = ACE_OS::gethrtime ();
+      creation.sample (now - start);
+    }
+
+  if (do_dump_history)
+    {
+      creation.dump_samples ("HISTORY", gsf);
+    }
+
+  ACE_Basic_Stats creation_stats;
+  creation.collect_basic_stats (creation_stats);
+  creation_stats.dump_results ("Creation", gsf);
+}
+
+int
+main (int argc, char *argv[])
+{
+  set_rt_scheduling ();
 
   ACE_TRY_NEW_ENV
     {
       CORBA::ORB_var orb =
-        CORBA::ORB_init (argc, argv, "", ACE_TRY_ENV);
+        CORBA::ORB_init (argc, argv, "",
+                         ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       CORBA::Object_var poa_object =
-        orb->resolve_initial_references("RootPOA", ACE_TRY_ENV);
+        orb->resolve_initial_references ("RootPOA",
+                                         ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       if (CORBA::is_nil (poa_object.in ()))
@@ -86,7 +150,8 @@ main (int argc, char *argv[])
                           1);
 
       PortableServer::POA_var root_poa =
-        PortableServer::POA::_narrow (poa_object.in (), ACE_TRY_ENV);
+        PortableServer::POA::_narrow (poa_object.in (),
+                                      ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
       PortableServer::POAManager_var poa_manager =
@@ -106,47 +171,24 @@ main (int argc, char *argv[])
                                                ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      PortableServer::POA_var poa =
+      PortableServer::POA_var child_poa =
         root_poa->create_POA ("TestPOA",
                               poa_manager.in (),
                               policies,
                               ACE_TRY_ENV);
       ACE_TRY_CHECK;
 
-      ACE_Sample_History creation (niterations);
+      gsf = ACE_High_Res_Timer::global_scale_factor ();
 
-      ACE_DEBUG ((LM_DEBUG, "High resolution timer calibration...."));
-      ACE_UINT32 gsf = ACE_High_Res_Timer::global_scale_factor ();
-      ACE_DEBUG ((LM_DEBUG, "done\n"));
+      object_creation_test (root_poa,
+                            0, // POA::create_reference
+                            ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
-      ACE_DEBUG ((LM_DEBUG, "Creating %d object references\n",
-                  niterations));
-      for (int i = 0; i != niterations; ++i)
-        {
-          char buf[16];
-          ACE_OS::sprintf (buf, "Object_%8.8x", i);
-          PortableServer::ObjectId_var oid (PortableServer::string_to_ObjectId (buf));
-
-          ACE_hrtime_t start = ACE_OS::gethrtime ();
-          CORBA::Object_var object =
-            poa->create_reference_with_id (oid.in (),
-                                           "IDL:Test/Simple:1.0",
-                                           ACE_TRY_ENV);
-          ACE_TRY_CHECK;
-
-          ACE_hrtime_t now = ACE_OS::gethrtime ();
-          creation.sample (now - start);
-        }
-      ACE_DEBUG ((LM_DEBUG, "test completed\n"));
-
-      if (do_dump_history)
-        {
-          creation.dump_samples ("HISTORY", gsf);
-        }
-
-      ACE_Basic_Stats creation_stats;
-      creation.collect_basic_stats (creation_stats);
-      creation_stats.dump_results ("Creation", gsf);
+      object_creation_test (child_poa,
+                            1, // POA::create_reference_with_id
+                            ACE_TRY_ENV);
+      ACE_TRY_CHECK;
 
       root_poa->destroy (1, 1, ACE_TRY_ENV);
       ACE_TRY_CHECK;
