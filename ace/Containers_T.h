c282765a8995ececaa4937dc76770a904b3b5bbd
schmidt <douglascraigschmidt@users.noreply.github.com>
2001-06-03 23:18:20 +0000

    ChangeLogTag:Sun Jun  3 18:08:39 2001  Douglas C. Schmidt  <schmidt@ace.cs.wustl.edu>

--- ace/Containers_T.h
+++ ace/Containers_T.h
@@ -6,10 +6,9 @@
  *
  *  $Id$
  *
- *  @author Doug Schmidt
+ *  @author Douglas C. Schmidt <schmidt@cs.wustl.edu>
  */
 //=============================================================================
-
 
 #ifndef ACE_CONTAINERS_T_H
 #define ACE_CONTAINERS_T_H
