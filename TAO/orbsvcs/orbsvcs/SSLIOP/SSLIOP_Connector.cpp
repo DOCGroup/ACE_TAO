6cf01a7e2863004ef725e09bea831d374cbdd299
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-06-08 14:31:45 +0000

    ChangeLogTag:Fri Jun 08 07:30:08 2001  Ossama Othman  <ossama@uci.edu>

--- TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Connector.cpp
+++ TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Connector.cpp
@@ -89,7 +89,13 @@ TAO_SSLIOP_Connector::open (TAO_ORB_Core *orb_core)
                   TAO_SSLIOP_CONNECT_CONCURRENCY_STRATEGY (orb_core),
                   -1);
 
-  return this->base_connector_.open (this->orb_core ()->reactor (),
+  ACE_Reactor *r = this->orb_core ()->reactor ();
+
+  // Set the reactor in the underlying ACE_SSL_SOCK_Connector to force
+  // it to use the one the ORB was configured to use.
+  this->base_connector_.connector ().reactor (r);
+
+  return this->base_connector_.open (r,
                                      connect_creation_strategy,
                                      &this->connect_strategy_,
                                      concurrency_strategy);
