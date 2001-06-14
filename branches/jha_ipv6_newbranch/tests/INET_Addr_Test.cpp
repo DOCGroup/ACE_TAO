afdf1b8ca757690e5eaeb421f75a5e154b81f6d2
jha <jha@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-14 17:41:48 +0000

    Added INET_Addr_Test files.

---
+++ branches/jha_ipv6_newbranch/tests/INET_Addr_Test.cpp
@@ -0,0 +1,68 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    tests
+//
+// = FILENAME
+//    INET_Addr_Test.cpp
+//
+// = DESCRIPTION
+//     Performs several tests on the ACE_INET_Addr class.  It creates several
+//     IPv4 and IPv6 addresses and checks that the address formed by the
+//     class is valid.
+//
+// = AUTHOR
+//    John Aughey (jha@aughey.com)
+//
+// ============================================================================
+
+#include <stdio.h>
+#include "ace/INET_Addr.h"
+
+int main(int argc, char *argv[])
+{
+  ACE_UNUSED_ARG(argc);
+  ACE_UNUSED_ARG(argv);
+
+  char *ipv4_addresses[] = {
+    "127.0.0.1", "138.38.180.251", "64.219.54.121", "192.0.0.1", "10.0.0.1", 0
+  };
+
+  ACE_INET_Addr addr;
+
+  for(int i=0; ipv4_addresses[i] != 0; i++) {
+    struct in_addr addrv4;
+    ACE_UINT32 addr32;
+
+    ACE_OS::inet_pton(AF_INET,ipv4_addresses[i],(void*)&addrv4);
+
+    memcpy((void*)&addr32,(void*)&addrv4,sizeof(addr32));
+
+    addr.set(80,ipv4_addresses[i]);
+
+    /* Now check to make sure get_ip_address matches and get_host_addr
+       matches. */
+
+    if(addr.get_ip_address() != htonl(addr32)) {
+      printf("Error: Address %s failed get_ip_address() check\n",ipv4_addresses[i]);
+      printf("0x%x != 0x%x\n",addr.get_ip_address(), addr32);
+    }
+    if(0 != ACE_OS::strcmp(addr.get_host_addr(),ipv4_addresses[i])) {
+      printf("Error: Address %s failed get_host_addr() check\n",ipv4_addresses[i]);
+      printf("%s != %s\n",addr.get_host_addr(),ipv4_addresses[i]);
+    }
+    addr.set(0,0);
+    if(addr.get_ip_address() != 0) {
+      printf("Error: failed to set address to 0\n");
+    }
+    addr.set(80,addr32);
+    if(0 != ACE_OS::strcmp(addr.get_host_addr(),ipv4_addresses[i])) {
+      printf("Error: Address %s failed second get_host_addr() check\n",ipv4_addresses[i]);
+      printf("%s != %s\n",addr.get_host_addr(),ipv4_addresses[i]);
+    }
+  }
+
+  return 0;
+}
