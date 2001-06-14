d593fdc610427e5e3988c720c564f4940e341a6c
schmidt <douglascraigschmidt@users.noreply.github.com>
2001-06-14 11:42:40 +0000

    ChangeLogTag:Thu Jun 14 03:10:11 2001  Krishnakumar B <kitty@danzon.cs.wustl.edu>

--- ace/Log_Priority.h
+++ ace/Log_Priority.h
@@ -6,10 +6,9 @@
  *
  *  $Id$
  *
- *  @author Doug Schmidt
+ *  @author Douglas C. Schmidt <schmidt@cs.wustl.edu>
  */
 //=============================================================================
-
 
 #ifndef ACE_LOG_PRIORITY_H
 #define ACE_LOG_PRIORITY_H
