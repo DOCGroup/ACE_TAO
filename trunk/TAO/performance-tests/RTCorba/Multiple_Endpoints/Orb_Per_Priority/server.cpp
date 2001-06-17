f72a554fbd9d01ef81454f6e484dbb73af9d3084
corsaro <corsaro@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-17 23:29:31 +0000

    ChangeLogTag: Sun Jun 17 18:24:18 2001  Angelo Corsaro  <corsaro@cs.wustl.edu>

--- trunk/TAO/performance-tests/RTCorba/Multiple_Endpoints/Orb_Per_Priority/server.cpp
+++ trunk/TAO/performance-tests/RTCorba/Multiple_Endpoints/Orb_Per_Priority/server.cpp
@@ -2,6 +2,8 @@
 
 #include "test_i.h"
 #include "tao/RTCORBA/RTCORBA.h"
+#include "tao/RTCORBA/Priority_Mapping.h"
+#include "tao/RTCORBA/Priority_Mapping_Manager.h"
 #include "ace/Get_Opt.h"
 #include "ace/Task.h"
 #include "ace/Sched_Params.h"
