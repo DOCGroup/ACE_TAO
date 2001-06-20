79f4fcb98db9e8477e5ca2a55490ebe57a1232a3
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-20 12:32:42 +0000

    ChangeLogTag: Wed Jun 20 07:31:14 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/tests/Big_Reply/Big_Reply_i.cpp
+++ TAO/tests/Big_Reply/Big_Reply_i.cpp
@@ -14,14 +14,11 @@ Big_Reply_i::~Big_Reply_i (void)
 }
 
 Test::Octet_Seq *
-Big_Reply_i::get_big_reply (CORBA::Environment &ACE_TRY_ENV)
+Big_Reply_i::get_big_reply (CORBA::Environment & /*ACE_TRY_ENV*/)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  Test::Octet_Seq_var reply_mesg;
-
-  ACE_NEW_THROW_EX (reply_mesg,
-                    Test::Octet_Seq (this->length_),
-                    CORBA::NO_MEMORY ());
+  Test::Octet_Seq_var reply_mesg = 
+    new Test::Octet_Seq (this->length_);
 
   reply_mesg->length (this->length_);
 
@@ -34,5 +31,4 @@ Big_Reply_i::shutdown (CORBA::Environment & /*ACE_TRY_ENV*/)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   this->orb_->shutdown ();
-  ACE_CHECK;
 }
