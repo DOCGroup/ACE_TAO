471ddeaf0e50eae96b35340b2041c256ac966bda
fhunleth <fhunleth@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-22 14:26:15 +0000

    ChangeLogTag: Fri Jun 22 09:24:54 2001  Frank Hunleth  <fhunleth@cs.wustl.edu>

--- TAO/tao/Protocols_Hooks.cpp
+++ TAO/tao/Protocols_Hooks.cpp
@@ -1,6 +1,7 @@
 // $Id$
 
 #include "Protocols_Hooks.h"
+#include "ace/Dynamic_Service.h"
 
 ACE_RCSID(tao, Protocols_Hooks, "$Id$")
 
@@ -9,3 +10,12 @@ TAO_Protocols_Hooks::~TAO_Protocols_Hooks (void)
 {
 }
 
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+
+template class ACE_Dynamic_Service<TAO_Protocols_Hooks>;
+
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+
+#pragma instantiate ACE_Dynamic_Service<TAO_Protocols_Hooks>
+
+#endif
