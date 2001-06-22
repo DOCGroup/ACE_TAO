f3ff6d3daa2ffbbcec011c8e0bc3870317df6ec0
fhunleth <fhunleth@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-22 14:26:15 +0000

    ChangeLogTag: Fri Jun 22 09:24:54 2001  Frank Hunleth  <fhunleth@cs.wustl.edu>

--- trunk/TAO/tao/Stub_Factory.cpp
+++ trunk/TAO/tao/Stub_Factory.cpp
@@ -1,5 +1,6 @@
 // $Id$
 #include "Stub_Factory.h"
+#include "ace/Dynamic_Service.h"
 
 ACE_RCSID(tao, TAO_Stub_Factory, "$Id$")
 
@@ -8,3 +9,12 @@ TAO_Stub_Factory::~TAO_Stub_Factory (void)
   // No-Op.
 }
 
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+
+template class ACE_Dynamic_Service<TAO_Stub_Factory>;
+
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+
+#pragma instantiate ACE_Dynamic_Service<TAO_Stub_Factory>
+
+#endif
