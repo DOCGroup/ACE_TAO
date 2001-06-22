9757b3d22f1703957cff6d1cafac508286ab160f
kitty <kitty@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-22 18:11:52 +0000

    ChangeLogTag: Fri Jun 22 13:10:13 2001  Krishnakumar B <kitty@cs.wustl.edu>

--- trunk/examples/Log_Msg/test_ostream.cpp
+++ trunk/examples/Log_Msg/test_ostream.cpp
@@ -46,7 +46,7 @@ main (int, char *[])
 #if !defined (ACE_LACKS_IOSTREAM_TOTALLY)
   // Create a persistent store.
   const char *filename = "output";
-  ios::openmode flags = ios::out | ios::trunc;
+  int flags = ios::out | ios::trunc;
   ofstream myostream (filename, flags);
 
   // Check for errors.
