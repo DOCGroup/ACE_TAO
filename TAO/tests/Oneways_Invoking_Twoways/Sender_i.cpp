60902a8002d3081f458f66ea88fc3fd21882f9ee
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-24 13:54:49 +0000

    ChangeLogTag: Sun Jun 24 08:42:23 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

---
+++ TAO/tests/Oneways_Invoking_Twoways/Sender_i.cpp
@@ -0,0 +1,60 @@
+#include "Sender_i.h"
+
+ACE_RCSID(Oneways_Invoking_Twoways, Sender_i, "$Id$")
+
+Sender_i::Sender_i (CORBA::ORB_ptr orb,
+                    CORBA::ULong no)
+  : orb_ (CORBA::ORB::_duplicate (orb)),
+    number_ (no),
+    active_objects_ (0)
+{
+}
+
+Sender_i::~Sender_i (void)
+{
+}
+
+void
+Sender_i::active_objects (CORBA::Short no_threads,
+                          CORBA::Environment & /*ACE_TRY_ENV */ )
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  this->active_objects_ = no_threads;
+}
+
+
+void
+Sender_i::send_ready_message (Test::Receiver_ptr receiver,
+                              CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  CORBA::ULong call_count = 0;
+
+  ACE_DEBUG ((LM_DEBUG,
+              "Received a call ...\n"));
+
+  ACE_DEBUG ((LM_DEBUG,
+              "Calling the Receiver ..\n"));
+
+  for (CORBA::ULong i = 0;
+       i < this->number_;
+       ++i)
+    {
+      call_count = receiver->receive_call (ACE_TRY_ENV);
+      ACE_CHECK;
+    }
+
+  /*if (call_count >= 8000)
+  {
+      // Shutdown the remote object
+      // receiver->shutdown (ACE_TRY_ENV);
+      // ACE_CHECK;
+      }*/
+}
+
+void
+Sender_i::ping (CORBA::Environment &ACE_TRY_ENV)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return;
+}
