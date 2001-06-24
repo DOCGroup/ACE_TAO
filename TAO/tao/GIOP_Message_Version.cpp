ab3c8d0d43faff89a3d6fb131b5f1abc1aa00843
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-06-24 16:27:17 +0000

    This commit was manufactured by cvs2svn to create branch 'bug_575'.

--- TAO/tao/GIOP_Message_Version.cpp
+++
@@ -1,8 +0,0 @@
-//$Id$
-# include "GIOP_Message_Version.h"
-
-#if !defined (__ACE_INLINE__)
-# include "tao/GIOP_Message_Version.inl"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID(tao, GIOP_Message_Version, "$Id$")
