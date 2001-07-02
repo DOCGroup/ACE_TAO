a552f73601e4136d666200c6410d4b6c54667452
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-02 20:34:30 +0000

    ChangeLogTag:Mon Jul  2 15:26:32 2001  Nanbor Wang  <nanbor@cs.wustl.edu>

--- ace/config-sunos5.6.h
+++ ace/config-sunos5.6.h
@@ -112,9 +112,6 @@
 #define ACE_HAS_AIO_CALLS
 #endif /* ACE_HAS_AIO_CALLS */
 
-#define ACE_HAS_POSIX_SEM
-#define ACE_LACKS_NAMED_POSIX_SEM
-
 // Sunos 5.6's aio_* with RT signals is broken.
 #define ACE_POSIX_AIOCB_PROACTOR
 
