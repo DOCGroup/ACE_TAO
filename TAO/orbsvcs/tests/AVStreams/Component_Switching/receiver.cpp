4d13ab7da41272a58d76bb9f6d872a9cea1569ce
crodrigu <crodrigu@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-05 09:08:16 +0000

    Thu Jul  5 04:53:00 2001  Craig Rodrigues <crodrigu@bbn.com>

--- TAO/orbsvcs/tests/AVStreams/Component_Switching/receiver.cpp
+++ TAO/orbsvcs/tests/AVStreams/Component_Switching/receiver.cpp
@@ -356,7 +356,7 @@ main (int argc,
 
       else
         ACE_DEBUG ((LM_DEBUG,
-                    "File Opened Successfull\n"));
+                    "File Opened Successfully\n"));
 
       result =
         receiver.init (argc,
