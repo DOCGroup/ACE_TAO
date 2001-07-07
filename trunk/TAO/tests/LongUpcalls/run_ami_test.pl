f979d2a7600dc36a22a8c20a8e77ed07f08b89a4
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-07 03:06:42 +0000

    ChangeLogTag: Fri Jul  6 22:04:51 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- trunk/TAO/tests/LongUpcalls/run_ami_test.pl
+++ trunk/TAO/tests/LongUpcalls/run_ami_test.pl
@@ -1,6 +1,6 @@
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
-    & eval 'exec perl -S $0 $argv:q'
-    if 0;
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
 
 # $Id$
 # -*- perl -*-
@@ -8,30 +8,35 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 use lib '../../../bin';
 use PerlACE::Run_Test;
 
-$status = 0;
-$iorfile = PerlACE::LocalFile ("test.ior");
-
-$svcfile = PerlACE::LocalFile ("svc.conf");
-
-$SV = new PerlACE::Process ("./ami_server", "-ORBSvcConf $svcfile -o $iorfile");
-$CL = new PerlACE::Process ("./ami_client", "-ORBSvcConf $svcfile -k file://$iorfile");
-
-print STDERR "==== Server upcall waits for operations on other threads\n";
-
+$iorfile = PerlACE::LocalFile ("server.ior");
 unlink $iorfile;
 
+$SV  = new PerlACE::Process ("ami_server", "-o $iorfile");
+$CL = new PerlACE::Process ("ami_client", " -k file://$iorfile");
+
+print STDERR "Running the ami tests that used to block \n";
 $SV->Spawn ();
 
-if (PerlACE::waitforfile_timed ($iorfile, 30) == -1) {
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
     print STDERR "ERROR: cannot find file <$iorfile>\n";
-    $BSV->Kill ();
+    $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-$client = $CL->SpawnWaitKill (240);
+$CL->Spawn (60);
 
-if ($client != 0) {
-    print STDERR "ERROR: client returned $client\n";
+$client = $CL->WaitKill (60);
+
+if ($client1 != 0) {
+    print STDERR "ERROR: client 1 returned $client1\n";
+    $status = 1;
+}
+
+
+$server = $SV->TerminateWaitKill (5);
+
+if ($server != 0) {
+    print STDERR "ERROR: server returned $server\n";
     $status = 1;
 }
 
