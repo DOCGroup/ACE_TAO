87af01d802993144e958cca2a557dbc7552b5216
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-07-08 18:30:28 +0000

    Sun Jul 08 11:26:23 2001  Ossama Othman  <ossama@uci.edu>

--- ace/FILE_Connector.cpp
+++ ace/FILE_Connector.cpp
@@ -39,20 +39,46 @@ ACE_FILE_Connector::connect (ACE_FILE_IO &new_io,
   ACE_TRACE ("ACE_FILE_Connector::connect");
   ACE_ASSERT (new_io.get_handle () == ACE_INVALID_HANDLE);
 
+  ACE_HANDLE handle = ACE_INVALID_HANDLE;
+
   // Check to see if caller has requested that we create the filename.
   if (ACE_reinterpret_cast (const ACE_Addr &,
                             ACE_const_cast (ACE_FILE_Addr &,
                                             remote_sap)) == ACE_Addr::sap_any)
-    // Create a new temporary file.
-    new_io.addr_ =
-      ACE_FILE_Addr (ACE_sap_any_cast (ACE_FILE_Addr &)); // class copy.
+    {
+      // Create a new temporary file.
+#ifdef ACE_LACKS_MKSTEMP
+      new_io.addr_ =
+        ACE_FILE_Addr (ACE_sap_any_cast (ACE_FILE_Addr &)); // class copy.
+#else
+      // Use ACE_OS::mkstemp() if it is available since it avoids a
+      // race condition, and subsequently a security hole due to that
+      // race condition (specifically, a denial-of-service attack).
+      //
+      // However, using mkstemp() prevents us from doing a timed open
+      // since it opens the file for us.  Better to avoid the race
+      // condition.
+      char filename[] = "ace-file-XXXXXX";
+
+      handle = ACE_OS::mkstemp (filename); // mkstemp() replaces "XXXXXX"
+
+      if (handle == ACE_INVALID_HANDLE
+          || new_io.addr_.set (filename) != 0)
+        return -1;
+
+      new_io.set_handle (handle);
+
+      return 0;
+#endif  /* ACE_LACKS_MKSTEMP */
+    }
   else
     new_io.addr_ = remote_sap; // class copy.
 
-  ACE_HANDLE handle = ACE_Handle_Ops::handle_timed_open (timeout,
-                                                         new_io.addr_.get_path_name (),
-                                                         flags,
-                                                         perms);
+  handle = ACE_Handle_Ops::handle_timed_open (timeout,
+                                              new_io.addr_.get_path_name (),
+                                              flags,
+                                              perms);
+
   new_io.set_handle (handle);
   return handle == ACE_INVALID_HANDLE ? -1 : 0;
 }
