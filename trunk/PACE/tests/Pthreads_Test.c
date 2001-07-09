2a0429e4773d7b046bf0a95f7a8c643e52be8544
joeh <joeh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-09 20:57:00 +0000

    ChangeLogTag: Mon Jul  9 15:53:43 2001  Joe Hoffert  <joeh@cs.wustl.edu>

--- trunk/PACE/tests/Pthreads_Test.c
+++ trunk/PACE/tests/Pthreads_Test.c
@@ -3,15 +3,14 @@
 /* ===================================================================== */
 /*                                                                       */
 /* = FILENAME                                                            */
-/*    Stdio_Test.c                                                       */
+/*    Pthreads_Test.c                                                    */
 /*                                                                       */
 /* =  DESCRIPTION                                                        */
-/*     Testing the platform for POSIX standard I/O. This is not meant    */
+/*     Testing the platform for POSIX threads. This is not meant         */
 /*     to be an exhaustive test at this point but more a sanity check    */
 /*     that PACE works (at least somewhat) as advertised.                */
-/*     This program creates/opens a temporary file, writes to the file,  */
-/*     reads from the file to make sure it's what was written, and       */
-/*     then closes/deletes the file.                                     */
+/*     This program simply creates some threads, joins them, and then    */
+/*     exits.                                                            */
 /*                                                                       */
 /* = AUTHOR                                                              */
 /*    Joe Hoffert <joeh@cs.wustl.edu>                                    */
@@ -19,14 +18,7 @@
 /* ===================================================================== */
 
 #include "pace/stdio.h"
-#include "pace/unistd.h"
-#include "pace/string.h"
-
-const char * filename = "temp";
-const char * mode = "w+";
-const char * string1 = "line 1\n";
-const char * success = "SUCCEEDED";
-const char * failure = "***FAILED***";
+#include "pace/pthread.h"
 
 #if defined (PACE_VXWORKS) && PACE_VXWORKS != 0
 
@@ -109,91 +101,75 @@ spa (FUNCPTR entry, ...)
 }
 #endif /* VXWORKS */
 
+void
+func1 (int *pnum_times)
+{
+  int i;
+  int j;
+  int x;
 
-#if PACE_LYNXOS != 0x300
+  for (i = 0; i < 4; i++) 
+    {
+      pace_printf("func1\n");
+      for (j = 0; j < 100000; j++)
+        x += i;
+
+      (*pnum_times)++;
+    }
+}
+
+void
+func2 (int *pnum_times)
+{
+  int i;
+  int j;
+  int x;
+
+  for (i = 0; i < 4; i++) 
+    {
+      pace_printf("func2\n");
+      for (j = 0; j < 100; j++)
+        x += i;
+
+      (*pnum_times)++;
+    }
+}
+
+void
+join_point (int func1_times, int func2_times)
+{
+  int total;
+
+  total = func1_times + func2_times;
+  pace_printf("join_point: func1 times %d, func2 times %d, total %d\n",
+              func1_times, func2_times, total);
+}
+
 int
 main (int argc, char **argv)
 {
-  /* Test creating/opening a file. */
-  PACE_FILE *file;
-  int retval;
-  char* retval2;
-  char buffer[256];
+  /* Test creating a thread. */
+  pace_pthread_t thread1;
+  pace_pthread_t thread2;
+  int func1_ret = 0;
+  int func2_ret = 0;
 
-  file = pace_fopen (filename,
-                     mode);
-  if (file == 0)
-    {
-      printf("pace_fopen %s\n", failure);
-      return -1;
-    }
+  pace_pthread_create(&thread1,
+                      NULL,
+                      (void *) func1,
+                      (void *) &func1_ret);
 
-  /* Test writing to a file. */
-  retval = pace_fputs (string1,
-                       file);
-  if (retval == EOF)
-    {
-      printf("pace_fputs %s\n", failure);
-      return -1;
-    }
+  pace_pthread_create(&thread2,
+                      NULL,
+                      (void *) func2,
+                      (void *) &func2_ret);
 
-  /* Test flushing a file. */
-  retval = pace_fflush (file);
-  if (retval != 0)
-    {
-      printf("pace_fflush %s\n", failure);
-      return -1;
-    }
+  pace_pthread_join(thread1, NULL);
+  pace_pthread_join(thread2, NULL);
 
-  /* Test seeking in a file. */
-  retval = pace_fseek (file,
-                       0,
-                       0);
-  if (retval != 0)
-    {
-      printf("pace_fseek %s\n", failure);
-      return -1;
-    }
-
-  /* Test reading from a file. */
-  retval2 = pace_fgets (buffer,
-                        sizeof(buffer),
-                        file);
-  if (retval2 == 0)
-    {
-      printf("pace_fgets %s\n", failure);
-      return -1;
-    }
-
-  if (pace_strcmp(buffer, string1) != 0)
-    {
-      printf("strcmp of pace_fgets %s\n", failure);
-      return -1;
-    }
-
-  /* Test closing a file. */
-  retval = pace_fclose (file);
-  if (retval != 0)
-    {
-      printf("pace_fclose %s\n", failure);
-      return -1;
-    }
-
-
-  /* uncomment this line to pause the program to test the size
-   * of the exe Cntr-Alt-Del and then look at the task manager to
-   *  find the size
-  pace_sleep (20);
-   */
+  join_point(func1_ret, func2_ret);
 
   PACE_UNUSED_ARG (argc);
   PACE_UNUSED_ARG (argv);
   return 0;
 }
-#else
-int
-main (int argc, char **argv)
-{
-  printf("PACE does not support LynxOS 3.0.0.\n");
-}
-#endif /* PACE_LYNXOS == 0x300 */
