947e12e0b19162158047451c993e85ae588edc23
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-11 10:49:04 +0000

    ChangeLogTag:Tue Jul 10 19:26:15 2001  Ossama Othman  <ossama@uci.edu>

--- trunk/ace/INET_Addr.h
+++ trunk/ace/INET_Addr.h
@@ -6,10 +6,9 @@
  *
  *  $Id$
  *
- *  @author Doug Schmidt
+ *  @author Douglas C. Schmidt <schmidt@cs.wustl.edu>
  */
 //=============================================================================
-
 
 #ifndef ACE_INET_ADDR_H
 #define ACE_INET_ADDR_H
