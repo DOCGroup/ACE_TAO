7379ffa98645961edce44199ed72962dfcdd2aa0
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-13 19:21:42 +0000

    ChangeLogTag:Fri Jul 13 12:16:03 2001  Ossama Othman  <ossama@uci.edu>

---
+++ trunk/ace/SSL/SSL_Asynch_BIO.h
@@ -0,0 +1,50 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file   SSL_Asynch_BIO.h
+ *
+ *  $Id$
+ *
+ *  @author Alexander Libman <alibman@baltimore.com>
+ *  @author Ossama Othman <ossama@uci.edu>
+ *
+ */
+//=============================================================================
+
+#ifndef ACE_SSL_ASYNCH_BIO_H
+#define ACE_SSL_ASYNCH_BIO_H
+
+#include "ace/pre.h"
+
+#include "SSL_Export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#if (defined (ACE_WIN32) && !defined (ACE_HAS_WINCE)) || (defined (ACE_HAS_AIO_CALLS))
+
+#include <openssl/bio.h>
+
+/**
+ * @name OpenSSL BIO Helper Methods for use with ACE's Asynchronous
+ *       SSL I/O support.
+ */
+//@{
+extern "C"
+{
+  int  ACE_Asynch_BIO_write (BIO *pBIO, const char *buf, int len);
+  int  ACE_Asynch_BIO_read  (BIO *pBIO, char *buf, int len);
+  int  ACE_Asynch_BIO_puts  (BIO *pBIO, const char *str);
+  long ACE_Asynch_BIO_ctrl  (BIO *pBIO, int cmd, long arg1, void *arg2);
+  int  ACE_Asynch_BIO_new   (BIO *pBIO);
+  int  ACE_Asynch_BIO_free  (BIO *pBIO);
+}
+//@}
+
+#endif  /* ACE_WIN32 || ACE_HAS_AIO_CALLS */
+
+#include "ace/post.h"
+
+#endif  /* ACE_SSL_ASYNCH_BIO_H */
