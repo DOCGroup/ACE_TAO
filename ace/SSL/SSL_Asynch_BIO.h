b2b3a7db9f798167bc735d32442ac6e0281a64e9
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-07-14 06:13:45 +0000

    ChangeLogTag:Fri Jul 13 23:02:46 2001  Ossama Othman  <ossama@uci.edu>

--- ace/SSL/SSL_Asynch_BIO.h
+++ ace/SSL/SSL_Asynch_BIO.h
@@ -23,9 +23,9 @@
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#if OPENSSL_VERSION_NUMBER > 0x0090581fL && ((defined (ACE_WIN32) && !defined (ACE_HAS_WINCE)) || (defined (ACE_HAS_AIO_CALLS)))
-
 #include <openssl/bio.h>
+
+#if OPENSSL_VERSION_NUMBER > 0x0090581fL && ((defined (ACE_WIN32) && !defined (ACE_HAS_WINCE)) || (defined (ACE_HAS_AIO_CALLS)))
 
 /**
  * @name OpenSSL BIO Helper Methods for use with ACE's Asynchronous
