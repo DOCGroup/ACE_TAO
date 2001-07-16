de6a355f247096e4aefd7022a9d2da00a3ca4208
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-16 19:54:11 +0000

    ChangeLogTag:Sun Jul 15 11:06:33 2001  Douglas C. Schmidt  <schmidt@ace.cs.wustl.edu>

--- trunk/ace/SOCK_Stream.h
+++ trunk/ace/SOCK_Stream.h
@@ -6,10 +6,9 @@
  *
  *  $Id$
  *
- *  @author Doug Schmidt
+ *  @author Douglas C. Schmidt <schmidt@cs.wustl.edu>
  */
 //=============================================================================
-
 
 #ifndef ACE_SOCK_STREAM_H
 #define ACE_SOCK_STREAM_H
