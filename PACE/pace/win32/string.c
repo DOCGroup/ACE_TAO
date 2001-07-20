7adb177134b918b415ff1db925a7c6b72bdd13d2
joeh <joeh@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-20 20:46:36 +0000

    ChangeLogTag: Fri Jul 20 15:44:02 2001  Joe Hoffert  <joeh@cs.wustl.edu>

--- PACE/pace/win32/string.c
+++ PACE/pace/win32/string.c
@@ -18,3 +18,22 @@
 #if !defined (PACE_HAS_INLINE)
 # include "pace/win32/string.inl"
 #endif /* ! PACE_HAS_INLINE */
+
+#if (PACE_HAS_POSIX_CLSR_UOF)
+char *
+strtok_r (char * s, const char * sep, char ** lasts)
+{
+  if (s == 0)
+    s = *lasts;
+  else
+    *lasts = s;
+  if (*s == 0)                  // We have reached the end
+    return 0;
+  int l_org = ACE_OS_String::strlen (s);
+  int l_sub = ACE_OS_String::strlen (s = ::strtok (s, sep));
+  *lasts = s + l_sub;
+  if (l_sub != l_org)
+    *lasts += 1;
+  return s ;
+}
+#endif /* PACE_HAS_POSIX_CLSR_UOF */
