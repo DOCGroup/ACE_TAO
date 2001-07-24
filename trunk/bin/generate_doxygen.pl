5f195f0b6e0f95d362b4571d1c403a2ca10b7de5
othman <othman@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-24 18:15:18 +0000

    ChangeLogTag:Tue Jul 24 10:56:04 2001  Ossama Othman  <ossama@uci.edu>

--- trunk/bin/generate_doxygen.pl
+++ trunk/bin/generate_doxygen.pl
@@ -34,6 +34,8 @@ $dot_path = '/usr/local/bin';
              ,'tao_strategies'
              ,'tao_smartproxies'
              ,'tao_av'
+	     ,'tao_security'
+	     ,'tao_ssliop'
             );
 
 # Modify defaults using the command line arguments
