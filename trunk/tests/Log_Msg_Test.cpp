96c62f59daf89633789f11ef3723630245ed148c
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-27 02:15:26 +0000

    ChangeLogTag:Thu Jul 26 22:09:19 2001  Steve Huston  <shuston@riverace.com>

--- trunk/tests/Log_Msg_Test.cpp
+++ trunk/tests/Log_Msg_Test.cpp
@@ -410,12 +410,116 @@ test_ostream (void)
   return 0;
 }
 
+
+// For testing the format specifiers, a class is defined as a callback
+// mechanism. It will get the formatted messages and check them for
+// correctness. The test_format_specs() function will set the first
+// few characters to say which test is being run, and the Log_Spec_Verify
+// class will use that to decide how to verify the results.
+
+class Log_Spec_Verify : public ACE_Log_Msg_Callback
+{
+public:
+  Log_Spec_Verify () : fail_ (0) {};
+
+  void log (ACE_Log_Record &log_record);
+  // Logging callback
+
+  int  result ();
+
+private:
+  int fail_;
+  // Count how many tests failed.
+};
+
+void
+Log_Spec_Verify::log (ACE_Log_Record &log_record)
+{
+  ACE_TCHAR *b = log_record.msg_data ();
+  const ACE_TCHAR *expect;
+
+  if (ACE_OS::strncmp (b, ACE_TEXT ("l1:"), 3) == 0)
+    {
+      expect = ACE_TEXT ("42");
+      b += 4;  //3
+    }
+  else if (ACE_OS::strncmp (b, ACE_TEXT ("l2:"), 3) == 0)
+    {
+      expect = ACE_TEXT ("   42");
+      b += 3;
+    }
+  else if (ACE_OS::strncmp (b, ACE_TEXT ("l3N1:"), 4) == 0)
+    {
+      expect = ACE_TEXT ("0042,Log_Msg");
+      b += 4;
+    }
+  else
+    {
+      ACE_ERROR ((LM_ERROR,
+                  ACE_TEXT ("Log_Spec_Verify, unrecognized test: %s\n"),
+                  b));
+      this->fail_++;
+    }
+
+  if (b != log_record.msg_data () && ACE_OS::strcmp (b, expect) != 0)
+    {
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Test %s failed; expected %s\n"),
+                  log_record.msg_data (), expect));
+      this->fail_++;
+    }
+
+  return;
+}
+
+int
+Log_Spec_Verify::result ()
+{
+  if (this->fail_ == 0)
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("All logging specifier tests passed.\n")));
+  else
+    ACE_ERROR ((LM_ERROR, ACE_TEXT ("%d logging specifier tests failed!\n"),
+                this->fail_));
+  return this->fail_;
+}
+
+static int
+test_format_specs ()
+{
+#if 0
+  Log_Spec_Verify  verifier;
+  ACE_Log_Msg      logger;
+
+  if (logger.open (ACE_TEXT ("Log_Msg_Test"), ACE_Log_Msg::MSG_CALLBACK) != 0)
+    ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
+                       ACE_TEXT ("%T: test_format_specs open")),
+                      1);
+  logger.msg_callback (&verifier);
+
+  logger.linenum (42);
+  logger.file (ACE_TEXT ("Log_Msg_Test.cpp"));
+  logger.log (LM_DEBUG, ACE_TEXT ("l1:%l"));
+  logger.log (LM_DEBUG, ACE_TEXT ("l2:%5l"));
+  logger.log (LM_DEBUG, ACE_TEXT ("l3N1:%0*l,%.7N"), 4);
+  return verifier.result ();
+#else
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("l1:%l\n")));
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("l2:%5l\n")));
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("l3N1:%0*l,%.7N\n"), 4));
+  return 0;
+#endif
+
+}
+
+
 // Main function.
 
 int
 main (int, ACE_TCHAR *argv[])
 {
   ACE_START_TEST (ACE_TEXT ("Log_Msg_Test"));
+
+  int status = 0;
+
   ACE_DEBUG ((LM_DEBUG,
               ACE_TEXT ("**** running ostream test\n")));
 
@@ -435,11 +539,18 @@ main (int, ACE_TCHAR *argv[])
   // Test various features of the <ACE_Log_Msg>.
   test_log_msg_features (argv[0]);
 
-  // Restore this mask so the shutdown message will print correctly!
+  // Test the format specifiers
+
+  // Restore this mask so diags and the shutdown message will print correctly!
   ACE_LOG_MSG->priority_mask (ACE_LOG_MSG->priority_mask () | LM_DEBUG,
                               ACE_Log_Msg::PROCESS);
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT ("**** running format specifiers test\n")));
+  if (test_format_specs () != 0)
+    status = 1;
+
   ACE_END_TEST;
-  return 0;
+  return status;
 }
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
