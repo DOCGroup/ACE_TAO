1fee01916d8d7275abf2340f8633fdd1dfcf2245
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-07-31 00:01:29 +0000

    Thread Pools getting associated with POAs...

--- branches/Thread_Pool/TAO/tests/RTCORBA/Thread_Pool/run_test.pl
+++ branches/Thread_Pool/TAO/tests/RTCORBA/Thread_Pool/run_test.pl
@@ -9,46 +9,141 @@ use lib '../../../../bin';
 use PerlACE::Run_Test;
 
 $status = 0;
-$iorfile = PerlACE::LocalFile ("ior");
+$iorfile_1 = PerlACE::LocalFile ("ior_1");
+$iorfile_2 = PerlACE::LocalFile ("ior_2");
+$iorfile_3 = PerlACE::LocalFile ("ior_3");
 
-unlink $iorfile;
+unlink $iorfile_1;
+unlink $iorfile_2;
+unlink $iorfile_3;
 
 $SV = new PerlACE::Process ("server");
-$CL1 = new PerlACE::Process ("client");
-$CL2 = new PerlACE::Process ("client");
-$CL3 = new PerlACE::Process ("client", "-x ");
+
+$CL1_1 = new PerlACE::Process ("client", "-k file://$iorfile_1");
+$CL1_2 = new PerlACE::Process ("client", "-k file://$iorfile_1");
+$CL1_3 = new PerlACE::Process ("client", "-k file://$iorfile_1");
+
+$CL2_1 = new PerlACE::Process ("client", "-k file://$iorfile_2");
+$CL2_2 = new PerlACE::Process ("client", "-k file://$iorfile_2");
+$CL2_3 = new PerlACE::Process ("client", "-k file://$iorfile_2");
+
+$CL3_1 = new PerlACE::Process ("client", "-k file://$iorfile_3");
+$CL3_2 = new PerlACE::Process ("client", "-k file://$iorfile_3");
+$CL3_3 = new PerlACE::Process ("client", "-k file://$iorfile_3 -x");
 
 $SV->Spawn ();
 
-if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile>\n";
-    $SV->Kill ();
-    exit 1;
+if (PerlACE::waitforfile_timed ($iorfile_1, 5) == -1 ||
+    PerlACE::waitforfile_timed ($iorfile_2, 5) == -1 ||
+    PerlACE::waitforfile_timed ($iorfile_3, 5) == -1)
+{
+    print STDERR "ERROR: cannot find file ior files: $iorfile_1; $iorfile_2; or $iorfile_3\n";
+    goto kill_server;
 }
 
-$CL1->Spawn ();
-$CL2->Spawn ();
-$CL3->Spawn ();
+print STDERR "\n**************************************************\n";
+print STDERR "Invoking methods on servant in default thread pool\n";
+print STDERR "**************************************************\n\n";
 
-$client = $CL1->WaitKill (120);
+$CL1_1->Spawn ();
+$CL1_2->Spawn ();
+$CL1_3->Spawn ();
+
+$client = $CL1_1->WaitKill (120);
 
 if ($client != 0) {
     print STDERR "ERROR: client returned $client\n";
     $status = 1
 }
-$client = $CL2->WaitKill (30);
+
+$client = $CL1_2->WaitKill (30);
 
 if ($client != 0) {
     print STDERR "ERROR: client returned $client\n";
     $status = 1
 }
 
-$client = $CL3->WaitKill (30);
+$client = $CL1_3->WaitKill (30);
 
 if ($client != 0) {
     print STDERR "ERROR: client returned $client\n";
     $status = 1
 }
+
+if ($status != 0) 
+{
+    goto kill_server;
+}
+
+print STDERR "\n**************************************************\n";
+print STDERR "Invoking methods on servant in first RT thread pool\n";
+print STDERR "***************************************************\n\n";
+
+$CL2_1->Spawn ();
+$CL2_2->Spawn ();
+$CL2_3->Spawn ();
+
+$client = $CL2_1->WaitKill (120);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1
+}
+
+$client = $CL2_2->WaitKill (30);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1
+}
+
+$client = $CL2_3->WaitKill (30);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1
+}
+
+if ($status != 0) 
+{
+    goto kill_server;
+}
+
+print STDERR "\n**************************************************\n";
+print STDERR "Invoking methods on servant in second RT thread pool\n";
+print STDERR "****************************************************\n\n";
+
+$CL3_1->Spawn ();
+$CL3_2->Spawn ();
+$CL3_3->Spawn ();
+
+$client = $CL3_1->WaitKill (120);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1
+}
+
+$client = $CL3_2->WaitKill (30);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1
+}
+
+$client = $CL3_3->WaitKill (30);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1
+}
+
+if ($status != 0) 
+{
+    goto kill_server;
+}
+
+kill_server:
 
 $server = $SV->WaitKill (5);
 
@@ -57,6 +152,8 @@ if ($server != 0) {
     $status = 1
 }
 
-unlink $iorfile;
+unlink $iorfile_1;
+unlink $iorfile_2;
+unlink $iorfile_3;
 
 exit $status
