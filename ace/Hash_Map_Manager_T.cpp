b18da2dfa565ceb4bcb7440ad9800c2ec4c3f887
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-08-09 22:00:02 +0000

    ChangeLogTag: Thu Aug  9 16:56:05 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- ace/Hash_Map_Manager_T.cpp
+++ ace/Hash_Map_Manager_T.cpp
@@ -471,14 +471,13 @@ ACE_Hash_Map_Const_Iterator_Base_Ex<EXT_ID, INT_ID, HASH_KEY, COMPARE_KEYS, ACE_
       this->index_++;
       return this->forward_i ();
     }
-  else if (this->index_ >= ACE_static_cast (ssize_t, this->map_man_->total_size_))
+  else if (this->index_ >= (ssize_t) this->map_man_->total_size_)
     return 0;
 
   this->next_ = this->next_->next_;
   if (this->next_ == &this->map_man_->table_[this->index_])
     {
-      while (++this->index_ < ACE_static_cast (ssize_t,
-                                               this->map_man_->total_size_))
+      while (++this->index_ < (ssize_t) this->map_man_->total_size_)
         {
           this->next_ = this->map_man_->table_[this->index_].next_;
           if (this->next_ != &this->map_man_->table_[this->index_])
@@ -486,7 +485,7 @@ ACE_Hash_Map_Const_Iterator_Base_Ex<EXT_ID, INT_ID, HASH_KEY, COMPARE_KEYS, ACE_
         }
     }
 
-  return this->index_ < ACE_static_cast (ssize_t, this->map_man_->total_size_);
+  return this->index_ < (ssize_t) this->map_man_->total_size_;
 }
 
 template <class EXT_ID, class INT_ID, class HASH_KEY, class COMPARE_KEYS, class ACE_LOCK> int
@@ -496,7 +495,7 @@ ACE_Hash_Map_Const_Iterator_Base_Ex<EXT_ID, INT_ID, HASH_KEY, COMPARE_KEYS, ACE_
 
   if (this->map_man_->table_ == 0)
     return -1;
-  else if (this->index_ == ACE_static_cast (ssize_t, this->map_man_->total_size_))
+  else if (this->index_ == (ssize_t) this->map_man_->total_size_)
     {
       this->index_--;
       return this->reverse_i ();
