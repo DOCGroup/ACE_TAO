45fb220ce4e11ba1a6b61d5119ebaaf5c5dd509c
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-11-08 19:59:41 +0000

    ChangeLogTag: Thu Nov  8 13:57:58 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/tao/Asynch_Reply_Dispatcher.cpp
+++ TAO/tao/Asynch_Reply_Dispatcher.cpp
@@ -109,7 +109,17 @@ TAO_Asynch_Reply_Dispatcher::dispatch_reply (
   this->reply_status_ = params.reply_status_;
 
   // Transfer the <params.input_cdr_>'s content to this->reply_cdr_
-  (void) this->reply_cdr_.clone_from (params.input_cdr_);
+  ACE_Data_Block *db =
+    this->reply_cdr_.clone_from (params.input_cdr_);
+
+  // See whether we need to delete the data block by checking the
+  // flags. We cannot be happy that we initally allocated the
+  // datablocks of the stack. If this method is called twice, as is in
+  // some cases where the same invocation object is used to make two
+  // invocations like forwarding, the release becomes essential.
+  if (ACE_BIT_DISABLED (db->flags (),
+                        ACE_Message_Block::DONT_DELETE))
+    db->release ();
 
   // Steal the buffer, that way we don't do any unnecesary copies of
   // this data.
