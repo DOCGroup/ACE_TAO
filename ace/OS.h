d3c595ac66608f155f38b83d657e169df0a3e40c
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-12-04 00:32:33 +0000

    ChangeLogTag:Mon Dec  3 16:30:07 2001  Ossama Othman  <ossama@uci.edu>

--- ace/OS.h
+++ ace/OS.h
@@ -329,13 +329,13 @@ typedef long      id_t;
 #   elif defined (VXWORKS)
 #     define ACE_DEFAULT_FILE_PERMS (S_IRUSR | S_IWUSR| S_IRGRP| S_IROTH)
 #   else
-#     define ACE_DEFAULT_FILE_PERMS 0644
+#     define ACE_DEFAULT_FILE_PERMS 0664
 #   endif /* ACE_WIN32 */
 # endif /* ACE_DEFAULT_FILE_PERMS */
 
 // Default directory permissions.
 # if !defined (ACE_DEFAULT_DIR_PERMS)
-#   define ACE_DEFAULT_DIR_PERMS 0755
+#   define ACE_DEFAULT_DIR_PERMS 0775
 # endif /* ACE_DEFAULT_DIR_PERMS */
 
 // Default size of the ACE Reactor.
