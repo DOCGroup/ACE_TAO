cde330ab1b36777ecdc2d749e412e6075c11d85a
Ossama Othman <ossama-othman@users.noreply.github.com>
2001-12-13 06:59:45 +0000

    Added downcast related methods to allow access to derived class methods.

--- TAO/orbsvcs/orbsvcs/Security/PrincipalAuthenticator.cpp
+++ TAO/orbsvcs/orbsvcs/Security/PrincipalAuthenticator.cpp
@@ -120,6 +120,311 @@ TAO_PrincipalAuthenticator::register_principal_authenticator (
     }
 }
 
+int TAO_PrincipalAuthenticator::_tao_class_id = 0;
+
+TAO_PrincipalAuthenticator_ptr
+tao_TAO_PrincipalAuthenticator_duplicate (
+    TAO_PrincipalAuthenticator_ptr p
+  )
+{
+  return TAO_PrincipalAuthenticator::_duplicate (p);
+}
+
+void
+tao_TAO_PrincipalAuthenticator_release (
+    TAO_PrincipalAuthenticator_ptr p
+  )
+{
+  CORBA::release (p);
+}
+
+TAO_PrincipalAuthenticator_ptr
+tao_TAO_PrincipalAuthenticator_nil (
+    void
+  )
+{
+  return TAO_PrincipalAuthenticator::_nil ();
+}
+
+TAO_PrincipalAuthenticator_ptr
+tao_TAO_PrincipalAuthenticator_narrow (
+    CORBA::Object *p
+    TAO_ENV_ARG_DECL
+  )
+{
+  return TAO_PrincipalAuthenticator::_narrow (p TAO_ENV_ARG_PARAMETER);
+}
+
+CORBA::Object *
+tao_TAO_PrincipalAuthenticator_upcast (
+    void *src
+  )
+{
+  TAO_PrincipalAuthenticator **tmp =
+    ACE_static_cast (TAO_PrincipalAuthenticator **, src);
+  return *tmp;
+}
+
+// *************************************************************
+// Operations for class TAO_PrincipalAuthenticator_var
+// *************************************************************
+
+TAO_PrincipalAuthenticator_var::TAO_PrincipalAuthenticator_var (void) // default constructor
+  : ptr_ (TAO_PrincipalAuthenticator::_nil ())
+{}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::ptr (void) const
+{
+  return this->ptr_;
+}
+
+TAO_PrincipalAuthenticator_var::TAO_PrincipalAuthenticator_var (
+  const ::TAO_PrincipalAuthenticator_var &p)
+  : TAO_Base_var (),
+    ptr_ (TAO_PrincipalAuthenticator::_duplicate (p.ptr ()))
+{}
+
+TAO_PrincipalAuthenticator_var::~TAO_PrincipalAuthenticator_var (void)
+{
+  CORBA::release (this->ptr_);
+}
+
+TAO_PrincipalAuthenticator_var &
+TAO_PrincipalAuthenticator_var::operator= (TAO_PrincipalAuthenticator_ptr p)
+{
+  CORBA::release (this->ptr_);
+  this->ptr_ = p;
+  return *this;
+}
+
+TAO_PrincipalAuthenticator_var &
+TAO_PrincipalAuthenticator_var::operator= (const ::TAO_PrincipalAuthenticator_var &p)
+{
+  if (this != &p)
+  {
+    CORBA::release (this->ptr_);
+    this->ptr_ = ::TAO_PrincipalAuthenticator::_duplicate (p.ptr ());
+  }
+  return *this;
+}
+
+TAO_PrincipalAuthenticator_var::operator const ::TAO_PrincipalAuthenticator_ptr &() const // cast
+{
+  return this->ptr_;
+}
+
+TAO_PrincipalAuthenticator_var::operator ::TAO_PrincipalAuthenticator_ptr &() // cast 
+{
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::operator-> (void) const
+{
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::in (void) const
+{
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr &
+TAO_PrincipalAuthenticator_var::inout (void)
+{
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr &
+TAO_PrincipalAuthenticator_var::out (void)
+{
+  CORBA::release (this->ptr_);
+  this->ptr_ = ::TAO_PrincipalAuthenticator::_nil ();
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::_retn (void)
+{
+  // yield ownership of managed obj reference
+  ::TAO_PrincipalAuthenticator_ptr val = this->ptr_;
+  this->ptr_ = ::TAO_PrincipalAuthenticator::_nil ();
+  return val;
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::tao_duplicate (TAO_PrincipalAuthenticator_ptr p)
+{
+  return ::TAO_PrincipalAuthenticator::_duplicate (p);
+}
+
+void
+TAO_PrincipalAuthenticator_var::tao_release (TAO_PrincipalAuthenticator_ptr p)
+{
+  CORBA::release (p);
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::tao_nil (void)
+{
+  return ::TAO_PrincipalAuthenticator::_nil ();
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_var::tao_narrow (
+    CORBA::Object *p
+    TAO_ENV_ARG_DECL
+  )
+{
+  return ::TAO_PrincipalAuthenticator::_narrow (p TAO_ENV_ARG_PARAMETER);
+}
+
+CORBA::Object *
+TAO_PrincipalAuthenticator_var::tao_upcast (void *src)
+{
+  TAO_PrincipalAuthenticator **tmp =
+    ACE_static_cast (TAO_PrincipalAuthenticator **, src);
+  return *tmp;
+}
+
+// *************************************************************
+// Operations for class TAO_PrincipalAuthenticator_out
+// *************************************************************
+
+TAO_PrincipalAuthenticator_out::TAO_PrincipalAuthenticator_out (TAO_PrincipalAuthenticator_ptr &p)
+  : ptr_ (p)
+{
+  this->ptr_ = ::TAO_PrincipalAuthenticator::_nil ();
+}
+
+TAO_PrincipalAuthenticator_out::TAO_PrincipalAuthenticator_out (TAO_PrincipalAuthenticator_var &p) // constructor from _var
+  : ptr_ (p.out ())
+{
+  CORBA::release (this->ptr_);
+  this->ptr_ = ::TAO_PrincipalAuthenticator::_nil ();
+}
+
+TAO_PrincipalAuthenticator_out::TAO_PrincipalAuthenticator_out (const ::TAO_PrincipalAuthenticator_out &p) // copy constructor
+  : ptr_ (ACE_const_cast (TAO_PrincipalAuthenticator_out &, p).ptr_)
+{}
+
+::TAO_PrincipalAuthenticator_out &
+TAO_PrincipalAuthenticator_out::operator= (const ::TAO_PrincipalAuthenticator_out &p)
+{
+  this->ptr_ = ACE_const_cast (TAO_PrincipalAuthenticator_out&, p).ptr_;
+  return *this;
+}
+
+TAO_PrincipalAuthenticator_out &
+TAO_PrincipalAuthenticator_out::operator= (const ::TAO_PrincipalAuthenticator_var &p)
+{
+  this->ptr_ = ::TAO_PrincipalAuthenticator::_duplicate (p.ptr ());
+  return *this;
+}
+
+TAO_PrincipalAuthenticator_out &
+TAO_PrincipalAuthenticator_out::operator= (TAO_PrincipalAuthenticator_ptr p)
+{
+  this->ptr_ = p;
+  return *this;
+}
+
+TAO_PrincipalAuthenticator_out::operator ::TAO_PrincipalAuthenticator_ptr &() // cast
+{
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr &
+TAO_PrincipalAuthenticator_out::ptr (void) // ptr
+{
+  return this->ptr_;
+}
+
+::TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator_out::operator-> (void)
+{
+  return this->ptr_;
+}
+
+  
+TAO_PrincipalAuthenticator_ptr 
+TAO_PrincipalAuthenticator::_narrow (
+    CORBA::Object_ptr obj
+    TAO_ENV_ARG_DECL)
+{
+  return
+    TAO_PrincipalAuthenticator::_unchecked_narrow (obj
+                                                   TAO_ENV_ARG_PARAMETER);
+}
+  
+TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator::_unchecked_narrow (
+      CORBA::Object_ptr obj
+      TAO_ENV_ARG_DECL_NOT_USED
+    )
+{
+  if (CORBA::is_nil (obj))
+      return TAO_PrincipalAuthenticator::_nil ();
+    return
+        ACE_reinterpret_cast
+          (
+            TAO_PrincipalAuthenticator_ptr,
+              obj->_tao_QueryInterface
+                (
+                  ACE_reinterpret_cast
+                    (
+                      ptr_arith_t,
+                      &TAO_PrincipalAuthenticator::_tao_class_id
+                    )
+                )
+          );
+}
+
+TAO_PrincipalAuthenticator_ptr
+TAO_PrincipalAuthenticator::_duplicate (TAO_PrincipalAuthenticator_ptr obj)
+{
+  if (!CORBA::is_nil (obj))
+    obj->_add_ref ();
+  return obj;
+}
+
+void *
+TAO_PrincipalAuthenticator::_tao_QueryInterface (ptr_arith_t type)
+{
+  void *retv = 0;
+  if (type == ACE_reinterpret_cast
+    (ptr_arith_t,
+      &TAO_PrincipalAuthenticator::_tao_class_id))
+    retv = ACE_reinterpret_cast (void*, this);
+  else if (type == ACE_reinterpret_cast
+    (ptr_arith_t,
+      &::SecurityLevel2::PrincipalAuthenticator::_tao_class_id))
+    retv = ACE_reinterpret_cast
+      (
+        void *,
+        ACE_static_cast
+          (
+            SecurityLevel2::PrincipalAuthenticator_ptr,
+            this
+          )
+      );
+  else if (type == ACE_reinterpret_cast (ptr_arith_t, &CORBA::Object::_tao_class_id))
+    retv = ACE_reinterpret_cast (void *,
+      ACE_static_cast (CORBA::Object_ptr, this));
+    
+  if (retv)
+    this->_add_ref ();
+  return retv;
+}
+
+const char*
+TAO_PrincipalAuthenticator::_interface_repository_id (void) const
+{
+  return "IDL:TAO_PrincipalAuthenticator:1.0";
+}
+
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
--- TAO/orbsvcs/orbsvcs/Security/PrincipalAuthenticator.h
+++ TAO/orbsvcs/orbsvcs/Security/PrincipalAuthenticator.h
@@ -26,6 +26,9 @@
 
 #include "ace/Array_Base.h"
 
+class TAO_PrincipalAuthenticator;
+typedef TAO_PrincipalAuthenticator *TAO_PrincipalAuthenticator_ptr;
+class TAO_PrincipalAuthenticator_var;
 class TAO_PrincipalAuthenticator_Impl;
 
 /**
@@ -98,6 +101,42 @@ public:
   /// PrincipalAuthenticator object (i.e. this object).
   void register_principal_authenticator (TAO_PrincipalAuthenticator_Impl *);
 
+  /**
+   * @name Downcast Related Methods
+   *
+   * Methods used for downcasting and manipulating the reference count
+   * of this interface.
+   */
+  //@{
+#if !defined(__GNUC__) || !defined (ACE_HAS_GNUG_PRE_2_8)
+  typedef TAO_PrincipalAuthenticator_ptr _ptr_type;
+  typedef TAO_PrincipalAuthenticator_var _var_type;
+#endif /* ! __GNUC__ || g++ >= 2.8 */
+
+  static int _tao_class_id;
+  
+  // The static operations.
+  static TAO_PrincipalAuthenticator_ptr _duplicate (TAO_PrincipalAuthenticator_ptr obj);
+  
+  static TAO_PrincipalAuthenticator_ptr _narrow (
+      CORBA::Object_ptr obj
+      TAO_ENV_ARG_DECL_WITH_DEFAULTS
+    );
+  
+  static TAO_PrincipalAuthenticator_ptr _unchecked_narrow (
+      CORBA::Object_ptr obj
+      TAO_ENV_ARG_DECL_WITH_DEFAULTS
+    );
+  
+  static TAO_PrincipalAuthenticator_ptr _nil (void)
+    {
+      return (TAO_PrincipalAuthenticator_ptr)0;
+    }
+
+  virtual void *_tao_QueryInterface (ptr_arith_t type);  
+  virtual const char* _interface_repository_id (void) const;
+  //@}
+
 protected:
 
   /// Destructor
@@ -115,6 +154,70 @@ private:
   ACE_Array_Base<TAO_PrincipalAuthenticator_Impl *> authenticators_;
 
 };
+
+
+class TAO_Security_Export TAO_PrincipalAuthenticator_var : public TAO_Base_var
+{
+public:
+  TAO_PrincipalAuthenticator_var (void); // default constructor
+  TAO_PrincipalAuthenticator_var (TAO_PrincipalAuthenticator_ptr p) : ptr_ (p) {} 
+  TAO_PrincipalAuthenticator_var (const TAO_PrincipalAuthenticator_var &); // copy constructor
+  ~TAO_PrincipalAuthenticator_var (void); // destructor
+  
+  TAO_PrincipalAuthenticator_var &operator= (TAO_PrincipalAuthenticator_ptr);
+  TAO_PrincipalAuthenticator_var &operator= (const TAO_PrincipalAuthenticator_var &);
+  TAO_PrincipalAuthenticator_ptr operator-> (void) const;
+  
+  operator const TAO_PrincipalAuthenticator_ptr &() const;
+  operator TAO_PrincipalAuthenticator_ptr &();
+  // in, inout, out, _retn 
+  TAO_PrincipalAuthenticator_ptr in (void) const;
+  TAO_PrincipalAuthenticator_ptr &inout (void);
+  TAO_PrincipalAuthenticator_ptr &out (void);
+  TAO_PrincipalAuthenticator_ptr _retn (void);
+  TAO_PrincipalAuthenticator_ptr ptr (void) const;
+  
+  // Hooks used by template sequence and object manager classes
+  // for non-defined forward declared interfaces.
+  static TAO_PrincipalAuthenticator_ptr tao_duplicate (TAO_PrincipalAuthenticator_ptr);
+  static void tao_release (TAO_PrincipalAuthenticator_ptr);
+  static TAO_PrincipalAuthenticator_ptr tao_nil (void);
+  static TAO_PrincipalAuthenticator_ptr tao_narrow (
+      CORBA::Object *
+      TAO_ENV_ARG_DECL_NOT_USED
+    );
+  static CORBA::Object * tao_upcast (void *);
+
+private:
+
+  TAO_PrincipalAuthenticator_ptr ptr_;
+  // Unimplemented - prevents widening assignment.
+  TAO_PrincipalAuthenticator_var (const TAO_Base_var &rhs);
+  TAO_PrincipalAuthenticator_var &operator= (const TAO_Base_var &rhs);
+
+};
+
+
+class TAO_Security_Export TAO_PrincipalAuthenticator_out
+{
+public:
+
+  TAO_PrincipalAuthenticator_out (TAO_PrincipalAuthenticator_ptr &);
+  TAO_PrincipalAuthenticator_out (TAO_PrincipalAuthenticator_var &);
+  TAO_PrincipalAuthenticator_out (const TAO_PrincipalAuthenticator_out &);
+  TAO_PrincipalAuthenticator_out &operator= (const TAO_PrincipalAuthenticator_out &);
+  TAO_PrincipalAuthenticator_out &operator= (const TAO_PrincipalAuthenticator_var &);
+  TAO_PrincipalAuthenticator_out &operator= (TAO_PrincipalAuthenticator_ptr);
+  operator TAO_PrincipalAuthenticator_ptr &();
+  TAO_PrincipalAuthenticator_ptr &ptr (void);
+  TAO_PrincipalAuthenticator_ptr operator-> (void);
+
+private:
+
+  TAO_PrincipalAuthenticator_ptr &ptr_;
+
+};
+
 
 #include "ace/post.h"
 
