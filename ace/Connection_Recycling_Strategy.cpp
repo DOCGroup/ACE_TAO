509d1f2cb56701207f8d03deccf152324696fd4e
Steve Huston <shuston@riverace.com>
2001-12-15 02:40:08 +0000

    ChangeLogTag:Fri Dec 14 21:36:29 2001  Steve Huston  <shuston@riverace.com>

--- ace/Connection_Recycling_Strategy.cpp
+++ ace/Connection_Recycling_Strategy.cpp
@@ -1,7 +1,7 @@
 #include "ace/Connection_Recycling_Strategy.h"
 
 
-ACE_RCSID(ace, Strategies, "$Id$")
+ACE_RCSID(ace, Connection_Recycling_Strategy, "$Id$")
 
 
 ACE_Connection_Recycling_Strategy::ACE_Connection_Recycling_Strategy (void)
