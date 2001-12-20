0c306062d50b93cd98f6e0bcd1459536cff549f6
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-12-20 00:03:34 +0000

    ChangeLogTag: Wed Dec 19 15:36:14 2001 Pradeep Gore  <pradeep@cs.wustl.edu>

---
+++ TAO/examples/RTCORBA/Activity/Thread_Task.h
@@ -0,0 +1,45 @@
+/* -*- C++ -*- */
+//=============================================================================
+/**
+ *  @file Thread_Task.h
+ *
+ *  $Id$
+ *
+ *  A periodic task implementation that uses sleep.
+ *
+ *  @author Pradeep Gore <pradeep@cs.wustl.edu>
+ */
+//=============================================================================
+#ifndef THREAD_TASK_H
+#define THREAD_TASK_H
+
+#include "Periodic_Task.h"
+
+/**
+ * @class Thread_Task
+ *
+ * @brief A periodic task implementation that uses sleep.
+ *
+ * simplified Periodic Task -
+ * if the <name_> activity, which is fired every <period_> time intervals,
+ * exceeds its <exec_time_>, it is "late".
+ * if <exec_time_> is greater than the <period_>, the activity(s) for the
+ * overlapped period(s) are considered "missed".
+ *
+ */
+class Thread_Task : public Periodic_Task
+{
+ public:
+  /// Constructor
+  Thread_Task (void);
+
+  /// Activate thread(s).
+  virtual int activate (ACE_Barrier* barrier);
+
+ protected:
+
+  /// task svc
+  virtual int svc (void);
+};
+
+#endif /* THREAD_TASK_H */
