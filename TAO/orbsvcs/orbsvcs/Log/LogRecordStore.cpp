9c039c564a11e1cb2ea057786659ec2eac9069c2
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-12-25 14:52:27 +0000

    ChangeLogTag: Tue Dec 25 08:50:54 2001  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/orbsvcs/orbsvcs/Log/LogRecordStore.cpp
+++ TAO/orbsvcs/orbsvcs/Log/LogRecordStore.cpp
@@ -165,7 +165,6 @@ template class ACE_Hash_Map_Iterator_Base_Ex<DsLogAdmin::RecordId, DsLogAdmin::L
 template class ACE_Hash_Map_Reverse_Iterator<DsLogAdmin::RecordId,DsLogAdmin::LogRecord,ACE_Null_Mutex>;
 template class ACE_Hash_Map_Reverse_Iterator_Ex<DsLogAdmin::RecordId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::RecordId>,
   ACE_Equal_To<DsLogAdmin::RecordId>, ACE_Null_Mutex>;
-template class ACE_Equal_To<DsLogAdmin::RecordId>;
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
@@ -181,6 +180,5 @@ ACE_Equal_To<DsLogAdmin::RecordId>, ACE_Null_Mutex>
 #pragma instantiate ACE_Hash_Map_Reverse_Iterator<DsLogAdmin::RecordId,DsLogAdmin::LogRecord,ACE_Null_Mutex>
 #pragma instantiate ACE_Hash_Map_Reverse_Iterator_Ex<DsLogAdmin::RecordId, DsLogAdmin::LogRecord, ACE_Hash<DsLogAdmin::RecordId>,
 ACE_Equal_To<DsLogAdmin::RecordId>, ACE_Null_Mutex>
-#pragma instantiate ACE_Equal_To<DsLogAdmin::RecordId>
 
 #endif /* ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA */
