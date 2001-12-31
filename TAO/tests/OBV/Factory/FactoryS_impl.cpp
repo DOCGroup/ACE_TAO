e46fc5f81c686c6d4cb7ee755eb4b20ca97ffe7f
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-12-31 17:56:44 +0000

    ChangeLogTag: Mon Dec 31 11:01:29 2001  Jeff Parsons <parsons@cs.wustl.edu>

---
+++ TAO/tests/OBV/Factory/FactoryS_impl.cpp
@@ -0,0 +1,55 @@
+// $Id$
+
+#include "FactoryC_impl.h"
+#include "FactoryS_impl.h"
+
+
+ACE_RCSID(Factory, FactoryS_impl, "$Id$")
+
+Test_impl::Test_impl (CORBA::ORB_ptr orb)
+    : orb_ (CORBA::ORB::_duplicate (orb))
+{
+}
+
+OBV_FactoryTest::BaseValue * 
+Test_impl::get_base_value (CORBA::Environment&)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  OBV_FactoryTest::BaseValue* ret_val = 0;
+  ACE_NEW_RETURN (ret_val, 
+                  OBV_OBV_FactoryTest::BaseValue,
+                  0);
+
+  return ret_val;
+}
+
+OBV_FactoryTest::Value1 * 
+Test_impl::get_value1 (CORBA::Environment&)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  OBV_FactoryTest::Value1* ret_val = 0;
+  ACE_NEW_RETURN (ret_val, 
+                  OBV_OBV_FactoryTest::Value1,
+                  0);
+
+  return ret_val;
+}
+
+OBV_FactoryTest::Value2 * 
+Test_impl::get_value2 (CORBA::Environment&)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  OBV_FactoryTest::Value2* ret_val = 0;
+  ACE_NEW_RETURN (ret_val, 
+                  Value2_impl,
+                  0);
+
+  return ret_val;
+}
+
+void
+Test_impl::shutdown (CORBA::Environment& ACE_TRY_ENV)
+    ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  this->orb_->shutdown (0, ACE_TRY_ENV);
+}
---
+++ TAO/tests/OBV/Factory/FactoryS_impl.h
@@ -0,0 +1,51 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//   TAO/tests/OBV/Factory
+//
+// = FILENAME
+//   FactoryS_impl.h
+//
+// = AUTHOR
+//   Boris Kolpackov <bosk@ipmce.ru>
+//
+// ============================================================================
+
+#ifndef TAO_FACTORY_S_IMPL_H
+#define TAO_FACTORY_S_IMPL_H
+
+#include "FactoryS.h"
+
+class Test_impl : public POA_OBV_FactoryTest::Test
+{
+  // = TITLE
+  //   Implementation of Test interface
+  //
+  // = DESCRIPTION
+  //   This interface is provided to produce valuetypes and test
+  //   marshaling
+  //
+public:
+  Test_impl (CORBA::ORB_ptr orb);
+  // ctor
+
+  virtual OBV_FactoryTest::BaseValue * get_base_value (CORBA::Environment&)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+  virtual OBV_FactoryTest::Value1 * get_value1 (CORBA::Environment&)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+  virtual OBV_FactoryTest::Value2 * get_value2 (CORBA::Environment&)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+  virtual void shutdown (CORBA::Environment&)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+private:
+  CORBA::ORB_var orb_;
+  // The ORB
+};
+
+#endif /* TAO_FACTORY_S_IMPL_H */
