ec0e1864a42138f664a98f2e9117cd7b04de18fe
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2001-12-31 17:03:14 +0000

    ChangeLogTag: Mon Dec 31 11:01:29 2001  Jeff Parsons <parsons@cs.wustl.edu>

--- TAO/tao/Sequence_T.cpp
+++ TAO/tao/Sequence_T.cpp
@@ -361,6 +361,71 @@ TAO_Pseudo_Object_Manager<T,T_var>::operator=(const T_var &p)
 }
 
 // *************************************************************
+// class TAO_Valuetype_Manager
+// *************************************************************
+
+template <class T, class T_var> TAO_Valuetype_Manager<T,T_var>&
+TAO_Valuetype_Manager<T,T_var>::
+    operator= (const TAO_Valuetype_Manager<T,T_var> &rhs)
+{
+  if (this == &rhs)
+    return *this;
+
+  if (this->release_)
+    {
+      if (*this->ptr_ != 0)
+        (*this->ptr_)->_remove_ref ();
+      *this->ptr_ = *rhs.ptr_;
+      if (*this->ptr_ != 0)
+        (*this->ptr_)->_add_ref ();
+    }
+  else
+    *this->ptr_ = *rhs.ptr_;
+
+  return *this;
+}
+
+template <class T, class T_var>TAO_Valuetype_Manager<T,T_var> &
+TAO_Valuetype_Manager<T,T_var>::operator=(T* p)
+{
+  if (this->release_)
+    {
+      // The semantics of the elements of a sequence are the same as
+      // that of a var variable.  Therefore we will not duplicate the
+      // user provided pointer before assigning it to the internal
+      // variable.
+      if (*this->ptr_ != 0)
+        (*this->ptr_)->_remove_ref ();
+      *this->ptr_ = p;
+    }
+  else
+    *this->ptr_ = p;
+
+  return *this;
+}
+
+template <class T, class T_var>TAO_Valuetype_Manager<T,T_var> &
+TAO_Valuetype_Manager<T,T_var>::operator=(const T_var &p)
+{
+  if (this->release_)
+    {
+      // The semantics of the elements of a sequence are the same as
+      // that of a var variable.  Therefore we duplicate p's
+      // pointer before assigning it to the internal
+      // variable.
+      if (*this->ptr_ != 0)
+        (*this->ptr_)->_remove_ref ();
+      *this->ptr_ = p.in ();
+      if (p != 0)
+        p->_add_ref ();
+    }
+  else
+    *this->ptr_ = p.in ();
+
+  return *this;
+}
+
+// *************************************************************
 // Operations for class TAO_Unbounded_Object_Sequence
 // *************************************************************
 
@@ -1045,6 +1110,357 @@ TAO_Bounded_Pseudo_Sequence<T, T_var,MAX>::_shrink_buffer (CORBA::ULong nl,
     {
       CORBA::release (tmp[i]);
       tmp[i] = T::_nil ();
+    }
+}
+
+// *************************************************************
+// Operations for class TAO_Unbounded_Valuetype_Sequence
+// *************************************************************
+
+// constructor for unbounded seq
+template <class T, class T_var>
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::
+TAO_Unbounded_Valuetype_Sequence (CORBA::ULong maximum)
+  : TAO_Unbounded_Base_Sequence (maximum,
+                                 TAO_Unbounded_Valuetype_Sequence<T,T_var>::allocbuf (maximum))
+{
+}
+
+template <class T, class T_var>
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::
+TAO_Unbounded_Valuetype_Sequence (const TAO_Unbounded_Valuetype_Sequence<T,T_var> &rhs)
+  : TAO_Unbounded_Base_Sequence (rhs)
+{
+  if (rhs.buffer_ != 0)
+    {
+      T **tmp1 =
+        TAO_Unbounded_Valuetype_Sequence<T,T_var>::allocbuf (this->maximum_);
+      T ** const tmp2 =
+        ACE_reinterpret_cast (T ** ACE_CAST_CONST, rhs.buffer_);
+
+      for (CORBA::ULong i = 0; i < rhs.length_; ++i)
+        {
+          if (tmp2[i] != 0)
+            tmp2[i]->_add_ref ();
+          tmp1[i] = tmp2[i];
+        }
+
+      this->buffer_ = tmp1;
+    }
+  else
+    {
+      this->buffer_ = 0;
+    }
+}
+
+template<class T, class T_var>
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::~TAO_Unbounded_Valuetype_Sequence (void)
+{
+  this->_deallocate_buffer ();
+}
+
+// assignment operator
+template <class T, class T_var>TAO_Unbounded_Valuetype_Sequence<T,T_var>&
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::
+operator= (const TAO_Unbounded_Valuetype_Sequence<T,T_var> &rhs)
+{
+  if (this == &rhs)
+    return *this;
+
+  if (this->release_)
+    {
+      T **tmp = ACE_reinterpret_cast (T **,
+                                      this->buffer_);
+
+      for (CORBA::ULong i = 0; i < this->length_; ++i)
+        {
+          if (tmp[i] != 0)
+            tmp[i]->_remove_ref ();
+          tmp[i] = 0;
+        }
+      if (this->maximum_ < rhs.maximum_)
+        {
+          TAO_Unbounded_Valuetype_Sequence<T,T_var>::freebuf (tmp);
+          this->buffer_ =
+            TAO_Unbounded_Valuetype_Sequence<T,T_var>::allocbuf (rhs.maximum_);
+        }
+    }
+  else
+    {
+      if (rhs.maximum_ == 0)
+        {
+          this->buffer_ = 0;
+        }
+      else
+        {
+          this->buffer_ =
+            TAO_Unbounded_Valuetype_Sequence<T,T_var>::allocbuf (rhs.maximum_);
+        }
+    }
+
+  TAO_Unbounded_Base_Sequence::operator= (rhs);
+
+  T **tmp1 = ACE_reinterpret_cast (T **, this->buffer_);
+  T ** const tmp2 = ACE_reinterpret_cast (T ** ACE_CAST_CONST, rhs.buffer_);
+
+  for (CORBA::ULong i = 0; i < rhs.length_; ++i)
+    {
+      if (tmp2[i] != 0)
+        tmp2[i]->_add_ref ();
+      tmp1[i] = tmp2[i];
+    }
+
+  return *this;
+}
+
+template <class T, class T_var> T **
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::allocbuf (CORBA::ULong nelems)
+{
+  T **buf = 0;
+
+  ACE_NEW_RETURN (buf, T*[nelems], 0);
+
+  for (CORBA::ULong i = 0; i < nelems; i++)
+    buf[i] = 0;
+
+  return buf;
+}
+
+template <class T, class T_var> void
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::freebuf (T **buffer)
+{
+  if (buffer == 0)
+    return;
+
+  // {orbos/97-05-15:16.11} The freebuf function ensures that the
+  // destructor for each element is called before the buffer is
+  // destroyed, except for string elements, which are freed using
+  // string_free(), and object reference elements, which are freed
+  // using release(). The freebuf function will ignore null pointers
+  // passed to it.
+
+  // @@ How are we supposed to implement that! We don't know the
+  // length of the buffer here.
+  // Mark the length in the first four bytes? For the moment we let
+  // that be.
+
+  delete[] buffer;
+}
+
+template<class T, class T_var> void
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::_allocate_buffer (CORBA::ULong length)
+{
+  T **tmp = TAO_Unbounded_Valuetype_Sequence<T,T_var>::allocbuf (length);
+
+  if (this->buffer_ != 0)
+    {
+      T **old = ACE_reinterpret_cast (T**, this->buffer_);
+      for (CORBA::ULong i = 0; i < this->length_; ++i)
+        // Only call duplicate when we did not own the previous
+        // buffer, since after this method we own it we must also own
+        // the objects.  If we already own the objects there is no
+        // need to copy them, if we did we would also have to remove
+        // the old instances.
+        if (!this->release_)
+          {
+            if (old[i] != 0)
+              old[i]->_add_ref ();
+            tmp[i] = old[i];
+          }
+        else
+          tmp[i] = old[i];
+
+      if (this->release_)
+        delete[] old;
+    }
+  this->buffer_ = tmp;
+}
+
+template<class T, class T_var> void
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::_deallocate_buffer (void)
+{
+  if (this->buffer_ == 0 || this->release_ == 0)
+    return;
+  T **tmp = ACE_reinterpret_cast (T**, this->buffer_);
+  for (CORBA::ULong i = 0; i < this->length_; ++i)
+    {
+      if (tmp[i] != 0)
+        tmp[i]->_remove_ref ();
+      tmp[i] = 0;
+    }
+  TAO_Unbounded_Valuetype_Sequence<T,T_var>::freebuf (tmp);
+  this->buffer_ = 0;
+}
+
+template<class T, class T_var> void
+TAO_Unbounded_Valuetype_Sequence<T,T_var>::_shrink_buffer (CORBA::ULong nl,
+                                                           CORBA::ULong ol)
+{
+  T **tmp = ACE_static_cast (T**, this->buffer_);
+
+  for (CORBA::ULong i = nl; i < ol; ++i)
+    {
+      if (tmp[i] != 0)
+        tmp[i]->_remove_ref ();
+      tmp[i] = 0;
+    }
+}
+
+// *************************************************************
+// Operations for class TAO_Bounded_Valuetype_Sequence
+// *************************************************************
+
+template <class T, class T_var, size_t MAX>
+TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::
+TAO_Bounded_Valuetype_Sequence (void)
+  :  TAO_Bounded_Base_Sequence (MAX,
+                                TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::allocbuf (MAX))
+{
+}
+
+template <class T, class T_var, size_t MAX>
+TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::
+TAO_Bounded_Valuetype_Sequence (const TAO_Bounded_Valuetype_Sequence<T, T_var,MAX> &rhs)
+  : TAO_Bounded_Base_Sequence (rhs)
+{
+  if (rhs.buffer_ != 0)
+    {
+      T **tmp1 =
+        TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::allocbuf (MAX);
+
+      T ** const tmp2 =
+        ACE_reinterpret_cast (T** ACE_CAST_CONST, rhs.buffer_);
+
+      for (CORBA::ULong i = 0; i < rhs.length_; i++)
+        {
+          if (tmp2[i] != 0)
+            tmp2[i]->_add_ref ();
+          tmp1[i] = tmp2[i];
+        }
+
+      this->buffer_ = tmp1;
+    }
+  else
+    {
+      this->buffer_ = 0;
+    }
+}
+
+template <class T, class T_var, size_t MAX> TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>&
+TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::operator=
+(const TAO_Bounded_Valuetype_Sequence<T,T_var, MAX> &rhs)
+{
+  if (this == &rhs)
+    return *this;
+
+  if (this->release_)
+    {
+      T **tmp = ACE_reinterpret_cast (T **, this->buffer_);
+
+      for (CORBA::ULong i = 0; i < this->length_; ++i)
+        {
+          if (tmp[i] != 0)
+            tmp[i]->_remove_ref ();
+          tmp[i] = 0;
+        }
+      // No need to reallocate the buffer since it is always of size
+      // MAX
+    }
+  else
+    {
+      if (rhs.maximum_ == 0)
+        {
+          this->buffer_ = 0;
+        }
+      else
+        {
+          this->buffer_ =
+            TAO_Bounded_Valuetype_Sequence<T,T_var,MAX>::allocbuf (rhs.maximum_);
+        }
+    }
+
+  TAO_Bounded_Base_Sequence::operator= (rhs);
+
+  T **tmp1 = ACE_reinterpret_cast (T **, this->buffer_);
+  T ** const tmp2 = ACE_reinterpret_cast (T ** ACE_CAST_CONST, rhs.buffer_);
+
+  for (CORBA::ULong i=0; i < rhs.length_; ++i)
+    {
+      if (tmp2[i] != 0)
+        tmp2[i]->_add_ref ();
+      tmp1[i] = tmp2[i];
+    }
+
+  return *this;
+}
+
+template <class T, class T_var, size_t MAX> T **
+TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::allocbuf (CORBA::ULong)
+{
+  T **buf = 0;
+
+  ACE_NEW_RETURN (buf, T*[MAX], 0);
+
+  for (CORBA::ULong i = 0; i < MAX; i++)
+    {
+      buf[i] = 0;
+    }
+
+  return buf;
+}
+
+template <class T, class T_var, size_t MAX> void
+TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::freebuf (T **buffer)
+{
+  // How much do we deallocate? Easy! allocbuf() always creates MAX
+  // elements and initialize them to T::_nil().  So we can be
+  // complaint and call CORBA::release() on each one.
+  for (CORBA::ULong i = 0; i < MAX; ++i)
+    {
+      if (buffer[i] != 0)
+        {
+          if (buffer[i] != 0)
+            buffer[i]->_remove_ref ();
+          buffer[i] = 0;
+        }
+    }
+
+  delete[] buffer;
+}
+
+template<class T, class T_var, size_t MAX> void
+TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::_allocate_buffer (CORBA::ULong length)
+{
+  // For this class memory is never reallocated so the implementation
+  // is *really* simple.
+  this->buffer_ =
+    TAO_Bounded_Valuetype_Sequence<T, T_var,MAX>::allocbuf (length);
+}
+
+template<class T, class T_var, size_t MAX> void
+TAO_Bounded_Valuetype_Sequence<T,T_var,MAX>::_deallocate_buffer (void)
+{
+  if (this->release_ == 0)
+    {
+      return;
+    }
+
+  T **tmp = ACE_reinterpret_cast (T **, this->buffer_);
+  TAO_Bounded_Valuetype_Sequence<T,T_var,MAX>::freebuf (tmp);
+  this->buffer_ = 0;
+}
+
+template<class T, class T_var, size_t MAX> void
+TAO_Bounded_Valuetype_Sequence<T,T_var, MAX>::_shrink_buffer (CORBA::ULong nl,
+                                                              CORBA::ULong ol)
+{
+  T **tmp = ACE_reinterpret_cast (T **, this->buffer_);
+
+  for (CORBA::ULong i = nl; i < ol; ++i)
+    {
+      if (tmp[i] != 0)
+        tmp[i]->_remove_ref ();
+      tmp[i] = 0;
     }
 }
 
--- TAO/tao/Sequence_T.h
+++ TAO/tao/Sequence_T.h
@@ -126,6 +126,8 @@ public:
                 CORBA::Boolean release = 0);
 };
 
+// *************************************************************
+
 /**
  * @class TAO_Bounded_Sequence
  *
@@ -234,7 +236,9 @@ public:
 
 template<class T,class T_var> class TAO_Unbounded_Object_Sequence;
 template<class T,class T_var> class TAO_Unbounded_Pseudo_Sequence;
+template<class T,class T_var> class TAO_Unbounded_Valuetype_Sequence;
 template<class T, class T_var, size_t MAX> class TAO_Bounded_Object_Sequence;
+template<class T, class T_var, size_t MAX> class TAO_Bounded_Valuetype_Sequence;
 template<size_t MAX> class TAO_Bounded_String_Sequence;
 
 // *************************************************************
@@ -340,6 +344,8 @@ private:
   CORBA::Boolean release_;
 };
 
+// *************************************************************
+
   /**
    * @class TAO_Pseudo_Object_Manager
    *
@@ -421,6 +427,95 @@ private:
 
 // *************************************************************
 
+  /**
+   * @class TAO_Valuetype_Manager
+   *
+   * @brief Manager for valuetypes.
+   *
+   * According to the spec., valuetypes are handled similarly to object
+   * references, so a manager class is needed.
+   *
+   * @see TAO_Object_Manager
+   */
+template<class T, class T_var>
+class TAO_Valuetype_Manager
+{
+  friend class TAO_Unbounded_Valuetype_Sequence<T,T_var>;
+public:
+  // @@ Use partial template specialization here to give access only
+  // to the right kind of sequence.
+  // friend template<CORBA::ULong MAX>
+  //    class TAO_Bounded_Valuetype_Sequence<T,MAX>;
+
+  // = Initialization and termination methods.
+  /**
+   * Copy constructor, the semantics are non-trivial:
+   *   + The referenced element is duplicated or not according to the
+   *   release value on the <rhs>.
+   *   + In any case a new reference to the same object is created.
+   */
+  TAO_Valuetype_Manager (const TAO_Valuetype_Manager<T,T_var> &rhs);
+
+  /**
+   * Constructor from address of an element, it should be private and
+   * only TAO_*_Object_Sequence would use it, but we have some
+   * problems with friendship and templates.
+   */
+  TAO_Valuetype_Manager (T **, CORBA::Boolean release);
+
+  /// Destructor, only releases the object if <release_> is true.
+  ~TAO_Valuetype_Manager (void);
+
+  /**
+   * Assignment from another managed type, only release if
+   * <this->release_> is true.
+   * @@ TODO what happens if rhs.release_ is true an this->relase_ is
+   * false?
+   */
+  TAO_Valuetype_Manager<T,T_var> &operator= (const TAO_Valuetype_Manager<T,T_var> &rhs);
+
+  /// Assignment from T *.
+  TAO_Valuetype_Manager<T,T_var> &operator= (T *);
+
+  /// Assignment from T_var.
+  TAO_Valuetype_Manager<T,T_var> &operator= (const T_var &);
+
+  /// Return pointer.
+  T * operator-> (void) const;
+
+  /// Cast (read-only).
+  operator const T *() const;
+
+  /// Cast.
+  operator T *&();
+
+  /// Cast (read-only) so that assignment from a structured
+  /// type to a T_var will make a copy.
+  operator const T_var() const;
+
+  /// for in parameter.
+  T *in (void) const;
+
+  /// for inout parameter.
+  T *&inout (void);
+
+  /// for out parameter.
+  T *&out (void);
+
+  /// for return type
+  T *_retn (void);
+
+private:
+  /// data member, notice that it is a pointer, to implement the
+  /// reference behavior for assignment.
+  T **ptr_;
+
+  /// release flag based on parent's flag
+  CORBA::Boolean release_;
+};
+
+// *************************************************************
+
 /**
  * @class TAO_Unbounded_Object_Sequence
  *
@@ -428,7 +523,7 @@ private:
  *
  * Some IDL types require that sequences on them have a "manager"
  * class, in charge of handling the object lifetime, examples are
- * pseudo objects, object references and strings.
+ * pseudo objects, object references, valuetypes, and strings.
  */
 template<class T,class T_var>
 class TAO_Unbounded_Object_Sequence : public TAO_Unbounded_Base_Sequence
@@ -726,6 +821,117 @@ public:
   static void freebuf (T **buffer);
 
   // The Base_Sequence functions, please see "tao/sequence.h"
+  virtual void _allocate_buffer (CORBA::ULong length);
+  virtual void _deallocate_buffer (void);
+  virtual void _shrink_buffer (CORBA::ULong new_length,
+                               CORBA::ULong old_length);
+};
+
+// *************************************************************
+
+/**
+ * @class TAO_Unbounded_Valuetype_Sequence
+ *
+ * @brief Parametric sequence for valuetypes
+ *
+ * @see TAO_Unbounded_Object_Sequence
+ */
+template<class T,class T_var>
+class TAO_Unbounded_Valuetype_Sequence : public TAO_Unbounded_Base_Sequence
+{
+public:
+  // = Initialization and termination methods.
+
+  // Default ctor.
+  TAO_Unbounded_Valuetype_Sequence (void);
+
+  /// Constructor with a "hint" for the maximum capacity.
+  TAO_Unbounded_Valuetype_Sequence (CORBA::ULong max);
+
+  /// Constructor with a given buffer.
+  TAO_Unbounded_Valuetype_Sequence (CORBA::ULong maximum,
+                                    CORBA::ULong length,
+                                    T* *data,
+                                    CORBA::Boolean release=0);
+
+  /// Copy ctor, deep copies.
+  TAO_Unbounded_Valuetype_Sequence(const TAO_Unbounded_Valuetype_Sequence<T,T_var> &);
+
+  /// The destructor releases all object reference memebrs and frees
+  /// all string members.
+  ~TAO_Unbounded_Valuetype_Sequence (void);
+
+  /**
+   * The assignment operator first releases all object reference
+   * members and frees all string members, and then performs a
+   * deepcopy to create a new structure.
+   */
+  TAO_Unbounded_Valuetype_Sequence<T,T_var> &operator= (const TAO_Unbounded_Valuetype_Sequence <T,T_var> &);
+
+  /// read-write accessor
+  TAO_Valuetype_Manager<T,T_var> operator[] (CORBA::ULong slot) const;
+
+  /// The allocbuf function allocates a vector of T elements that can
+  /// be passed to the T *data constructor.
+  static T **allocbuf (CORBA::ULong);
+
+  /// Release all the elements.
+  static void freebuf (T **);
+
+  // The Base_Sequence functions, please see "tao/Sequence.h"
+  /// No default to workaround egcs problem with templates and
+  /// namespaces
+  virtual void _allocate_buffer (CORBA::ULong length);
+  virtual void _deallocate_buffer (void);
+  virtual void _shrink_buffer (CORBA::ULong new_length,
+                               CORBA::ULong old_length);
+};
+
+// *************************************************************
+
+/**
+ * @class TAO_Bounded_Valuetype_Sequence
+ *
+ * @brief Parametric sequence for types that require managers.
+ *
+ * Please see the documentation for the unbounded case.
+ */
+template<class T, class T_var, size_t MAX>
+class TAO_Bounded_Valuetype_Sequence : public TAO_Bounded_Base_Sequence
+{
+public:
+  // = Initialization and termination methods.
+
+  // Default ctor.
+  TAO_Bounded_Valuetype_Sequence (void);
+
+  /// Constructor from data.
+  TAO_Bounded_Valuetype_Sequence (CORBA::ULong length,
+                                  T* *value,
+                                  CORBA::Boolean release=0);
+
+  /// Copy constructor.
+  TAO_Bounded_Valuetype_Sequence (const TAO_Bounded_Valuetype_Sequence<T,T_var,MAX> &);
+
+  /// destructor
+  ~TAO_Bounded_Valuetype_Sequence (void);
+
+  /// Assignment from another Bounded sequence.
+  TAO_Bounded_Valuetype_Sequence &operator= (const TAO_Bounded_Valuetype_Sequence<T,T_var,MAX> &);
+
+  /// Read-write accessor.
+  TAO_Valuetype_Manager<T,T_var> operator[] (CORBA::ULong slot) const;
+
+  /// Allocate storage for a sequence..
+  static T **allocbuf (CORBA::ULong length);
+
+  /// Free a buffer allocated by allocbuf() and release each element on
+  /// it.
+  static void freebuf (T **buffer);
+
+  // The Base_Sequence functions, please see "tao/sequence.h"
+  /// No default to workaround egcs problem with templates and
+  /// namespaces
   virtual void _allocate_buffer (CORBA::ULong length);
   virtual void _deallocate_buffer (void);
   virtual void _shrink_buffer (CORBA::ULong new_length,
