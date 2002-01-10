0e2125082b6a4f9cd9b15f249f31a41151d9ce57
venkita <venkita@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-10 18:40:37 +0000

    ChangeLogTag: Thu Jan 10 12:13:28 2002  Venkita Subramonian  <venkita@cs.wustl.edu>

---
+++ trunk/TAO/tests/Two_Objects/First_i.h
@@ -0,0 +1,33 @@
+//
+// $Id$
+//
+
+
+#ifndef TAO_TWO_OBJECTS_FIRST_I_H
+#define TAO_TWO_OBJECTS_FIRST_I_H
+
+#include "Two_ObjectsS.h"
+#include "ace/Synch.h"
+
+/// Implement the Two_Objects_Test::First interface
+class First_i
+  : public virtual POA_Two_Objects_Test::First
+{
+public:
+
+  // Ctor..
+  First_i (CORBA::ORB_ptr orb, ACE_Auto_Event &two_way_done);
+
+  /// Interface methods..
+  void oneway_method (TAO_ENV_SINGLE_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+private:
+
+  /// Use an ORB reference to conver strings to objects and shutdown
+  /// the application.
+  CORBA::ORB_var orb_;
+  ACE_Auto_Event &two_way_done_;
+};
+
+#endif /* TAO_TWO_OBJECTS_FIRST_I_H*/
