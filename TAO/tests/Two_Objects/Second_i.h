c5d2bdd1cf4582ba25bcc2cc22103bd54a268809
venkita <venkita@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-10 18:40:37 +0000

    ChangeLogTag: Thu Jan 10 12:13:28 2002  Venkita Subramonian  <venkita@cs.wustl.edu>

---
+++ TAO/tests/Two_Objects/Second_i.h
@@ -0,0 +1,34 @@
+//
+// $Id$
+//
+
+#ifndef TAO_TWO_OBJECTS_SECOND_I_H
+#define TAO_TWO_OBEJCTS_SECOND_I_H
+
+#include "Two_ObjectsS.h"
+
+/// Implement the POA_Two_Objects_Test::Second interface
+class Second_i
+  : public virtual POA_Two_Objects_Test::Second
+{
+public:
+
+  Second_i ( CORBA::ORB_ptr orb,
+             CORBA::ULong len, ACE_Auto_Event &two_way_done);
+
+  Two_Objects_Test::Octet_Seq *
+    twoway_method (TAO_ENV_SINGLE_ARG_DECL_NOT_USED)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+  void shutdown (TAO_ENV_SINGLE_ARG_DECL_NOT_USED)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+private:
+  /// Use an ORB reference to conver strings to objects and shutdown
+  /// the application.
+  CORBA::ORB_var orb_;
+  CORBA::ULong length_;
+  ACE_Auto_Event &two_way_done_;
+};
+
+#endif /* TAO_TWO_OBEJCTS_SECOND_I_H*/
