e939bdcf25825f65f27151361bd857b3360392fc
mayur <mayur@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-23 18:13:14 +0000

    Wed Jan 23 09:55:33 2002  Mayur Deshpande  <mayur@ics.uci.edu> Mon Jan 21 18:03:11 2002  Mayur Deshpande  <mayur@ics.uci.edu>

--- TAO/TAO_IDL/be/be_visitor_interface/amh_rh_ss.cpp
+++ TAO/TAO_IDL/be/be_visitor_interface/amh_rh_ss.cpp
@@ -46,22 +46,31 @@ be_visitor_amh_rh_interface_ss::visit_interface (be_interface *node)
   //    need to fix that...
   // Generate the skeleton class name, use the AMH-node name as a
   // basis, this is AMH_<InterfaceName>ResponseHandler...
-  ACE_CString rh_base_class_name = node->local_name ();
-  // ...and prepend either the "TAO_" prefix...
-  ACE_CString rh_skel_class_name =  "TAO_";
+  ACE_CString rh_skel_prefix =  "TAO_";
+
+  char *buf;
+  node->compute_full_name (rh_skel_prefix.c_str(), "", buf);
+  ACE_CString rh_skel_full_scope_name ("POA_");
+  rh_skel_full_scope_name += buf;
+  delete[] buf;
+
+  ACE_CString rh_skel_class_name_prefix (rh_skel_prefix.c_str ());
   if (!node->is_nested ())
     {
       // ...or the "POA_TAO_" prefix if we are in the global
       // namespace....
-      rh_skel_class_name = "POA_TAO_";
+      rh_skel_class_name_prefix = "POA_TAO_";
     }
-  rh_skel_class_name += rh_base_class_name.c_str ();
+  //node->compute_full_name (rh_skel_class_name_prefix.c_str(), "", buf);
+  ACE_CString rh_skel_class_name (rh_skel_class_name_prefix);
+  rh_skel_class_name += node->local_name (); 
 
   // constructor
   *os << be_nl << "// TAO_IDL - Generated from "
       << __FILE__ << ":" << __LINE__ << be_nl;
 
-  *os << rh_skel_class_name.c_str() << "::" << be_nl
+  *os << rh_skel_full_scope_name.c_str() 
+      << "::" << be_nl
       << "  " << rh_skel_class_name.c_str()
       << " (TAO_ServerRequest &sr)" << be_nl
       << "  : TAO_AMH_Response_Handler (sr)" << be_nl
@@ -72,7 +81,8 @@ be_visitor_amh_rh_interface_ss::visit_interface (be_interface *node)
   *os << be_nl << "// TAO_IDL - Generated from "
       << __FILE__ << ":" << __LINE__ << be_nl;
 
-  *os << rh_skel_class_name.c_str() << "::" << be_nl
+  *os << rh_skel_full_scope_name.c_str() 
+      << "::" << be_nl
       << "  ~" << rh_skel_class_name.c_str()
       << " (void)" << be_nl;
   *os << "{" << be_nl;
