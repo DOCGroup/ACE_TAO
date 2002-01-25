65fbd97f6df77152363887ff93073db2e8e380e1
mayur <mayur@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-25 22:28:33 +0000

    Fri Jan 25 14:25:11 2002  Mayur Deshpande  <mayur@ics.uci.edu>

--- TAO/performance-tests/AMH_Single_Threaded_Latency/Roundtrip.h
+++ TAO/performance-tests/AMH_Single_Threaded_Latency/Roundtrip.h
@@ -31,7 +31,7 @@ class Roundtrip
         ACE_THROW_SPEC ((CORBA::SystemException));
     
     void shutdown (Test::AMH_RoundtripResponseHandler_ptr _tao_rh
-                   TAO_ENV_SINGLE_ARG_DECL)
+                   TAO_ENV_ARG_DECL)
         ACE_THROW_SPEC ((CORBA::SystemException));
     
  private:
