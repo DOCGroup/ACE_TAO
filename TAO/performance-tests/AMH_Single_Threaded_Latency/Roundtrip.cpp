33376325953e12883d54cd6acc7eb18ca7019bec
mayur <mayur@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-25 03:16:12 +0000

    Thu Jan 24 18:51:21 2002  Mayur Deshpande  <mayur@ics.uci.edu>

---
+++ TAO/performance-tests/AMH_Single_Threaded_Latency/Roundtrip.cpp
@@ -0,0 +1,33 @@
+//
+// $Id$
+//
+#include "Roundtrip.h"
+
+ACE_RCSID(Single_Threaded_Latency, Roundtrip, "$Id$")
+
+Roundtrip::Roundtrip (CORBA::ORB_ptr orb)
+  : orb_ (CORBA::ORB::_duplicate (orb))
+{
+}
+
+void
+Roundtrip::test_method (
+                Test::AMH_RoundtripResponseHandler_ptr _tao_rh,
+                Test::Timestamp send_time
+                TAO_ENV_ARG_DECL_NOT_USED)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  //ACE_DEBUG ((LM_DEBUG, "Test_Method called\n"));
+  _tao_rh->test_method (send_time);
+  //ACE_DEBUG ((LM_DEBUG, "RH completed\n"));
+}
+
+void
+Roundtrip::shutdown (
+       Test::AMH_RoundtripResponseHandler_ptr _tao_rh
+       TAO_ENV_SINGLE_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  _tao_rh->shutdown ();
+  this->orb_->shutdown (0 TAO_ENV_ARG_PARAMETER);
+}
