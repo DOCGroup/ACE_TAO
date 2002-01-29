395984ea5d5e1e2da9266fd19d889b302d8dc311
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-29 17:03:24 +0000

    ChangeLogTag:Tue Jan 29 09:01:41 2002  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/TAO_IDL/be_include/be_visitor_interface/amh_ch.h
+++ TAO/TAO_IDL/be_include/be_visitor_interface/amh_ch.h
@@ -12,7 +12,8 @@
 //
 // = DESCRIPTION
 //    Concrete visitor for the AMH-RH Interface node.
-//    This one provides code generation for interfaces in the client header.
+//    This one provides code generation for interfaces in the client
+//    header.
 //
 // = AUTHOR
 //    Mayur Deshpande
@@ -22,14 +23,15 @@
 #ifndef _BE_INTERFACE_AMH_INTERFACE_CH_H_
 #define _BE_INTERFACE_AMH_INTERFACE_CH_H_
 
-class be_visitor_amh_interface_ch : be_visitor_interface
+class be_visitor_amh_interface_ch : public be_visitor_interface
 {
   //
   // = TITLE
   //   be_visitor_amh_interface_ch
   //
   // = DESCRIPTION
-  //   This is a concrete visitor to generate the client header for AMH interface
+  //   This is a concrete visitor to generate the client header for
+  //   AMH interface
   //
 public:
   be_visitor_amh_interface_ch (be_visitor_context *ctx);
