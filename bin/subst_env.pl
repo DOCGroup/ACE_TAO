5e12d6686388ccadee1714b94ddee0cce6587bfd
okellogg <okellogg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-01-29 20:04:44 +0000

    ChangeLogTag:Tue Jan 29 20:47:24 2002  Oliver Kellogg  <oliver.kellogg@sysde.eads.net>

--- bin/subst_env.pl
+++ bin/subst_env.pl
@@ -2,8 +2,8 @@
 #
 # $Id$
 # 
-# This script eases the transition from the ACE_TRY_ENV declarations
-# and method parameters to the TAO_ENV_ARG macros.
+# This script eases the transition from the CORBA::Environment& ACE_TRY_ENV
+# declarations and method parameters to the ACE_ENV_ARG macros.
 # It was used for converting TAO between versions 1.2.1 and 1.2.2.
 # The converted output is written to stdout. 
 # Typical usage would be as follows:
@@ -22,30 +22,30 @@ while (($l = <>)) {
             $single = "";
         }
         if ($l =~ /$default/) {
-          $l =~ s/$envdecl${default} *\(\)/TAO_ENV_${single}ARG_DECL_WITH_DEFAULTS/;
+          $l =~ s/$envdecl${default} *\(\)/ACE_ENV_${single}ARG_DECL_WITH_DEFAULTS/;
         } else {
-          $l =~ s/$envdecl/TAO_ENV_${single}ARG_DECL/;
+          $l =~ s/$envdecl/ACE_ENV_${single}ARG_DECL/;
         }
       } elsif ($l =~ /,\s*$envdecl/) {
         if ($l =~ /$default/) {
-          $l =~ s/,\s*$envdecl${default} *\(\)/ TAO_ENV_ARG_DECL_WITH_DEFAULTS/;
+          $l =~ s/,\s*$envdecl${default} *\(\)/ ACE_ENV_ARG_DECL_WITH_DEFAULTS/;
         } else {
-          $l =~ s/,\s*$envdecl/ TAO_ENV_ARG_DECL/;
+          $l =~ s/,\s*$envdecl/ ACE_ENV_ARG_DECL/;
         }
       } else {
         if ($l =~ /$default/) {
-          $l =~ s/$envdecl${default} *\(\)/TAO_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS/;
+          $l =~ s/$envdecl${default} *\(\)/ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS/;
         } else {
-          $l =~ s/$envdecl/TAO_ENV_SINGLE_ARG_DECL/;
+          $l =~ s/$envdecl/ACE_ENV_SINGLE_ARG_DECL/;
         }
       }
     } elsif ($l =~ /^\s*ACE_TRY_ENV/) {
       $last =~ s/,\s*$/\n/;
-      $l =~ s/ACE_TRY_ENV/TAO_ENV_ARG_PARAMETER/;
+      $l =~ s/ACE_TRY_ENV/ACE_ENV_ARG_PARAMETER/;
     } elsif ($l =~ /, *ACE_TRY_ENV/) {
-      $l =~ s/, *ACE_TRY_ENV/ TAO_ENV_ARG_PARAMETER/;
+      $l =~ s/, *ACE_TRY_ENV/ ACE_ENV_ARG_PARAMETER/;
     } else {
-      $l =~ s/ACE_TRY_ENV/TAO_ENV_SINGLE_ARG_PARAMETER/;
+      $l =~ s/ACE_TRY_ENV/ACE_ENV_SINGLE_ARG_PARAMETER/;
     }
     print $last;
     print $l;
@@ -53,11 +53,11 @@ while (($l = <>)) {
   } elsif ($l =~ /CORBA(::|_)Environment *&/) {
     if ($l =~ /^\s*CORBA(::|_)Environment *&/) {
       $last =~ s/,\s*$/\n/;
-      $l =~ s/CORBA(::|_)Environment *&/TAO_ENV_ARG_DECL_NOT_USED/;
+      $l =~ s/CORBA(::|_)Environment *&/ACE_ENV_ARG_DECL_NOT_USED/;
     } elsif ($l =~ /,\s*CORBA(::|_)Environment *&/) {
-      $l =~ s/,\s*CORBA(::|_)Environment *&/ TAO_ENV_ARG_DECL_NOT_USED/;
+      $l =~ s/,\s*CORBA(::|_)Environment *&/ ACE_ENV_ARG_DECL_NOT_USED/;
     } else {
-      $l =~ s/CORBA(::|_)Environment *&/TAO_ENV_SINGLE_ARG_DECL_NOT_USED/;
+      $l =~ s/CORBA(::|_)Environment *&/ACE_ENV_SINGLE_ARG_DECL_NOT_USED/;
     }
     print $last;
     print $l;
