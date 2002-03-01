4f7df1f39dbab3af1c4310984cb8049d696624d0
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-03-01 15:59:55 +0000

    ChangeLogTag:Fri Mar  1 07:56:43 2002  Carlos O'Ryan  <coryan@uci.edu>

--- TAO/orbsvcs/performance-tests/EC_Colocated_Latency/run_supplier_filtering.sh
+++ TAO/orbsvcs/performance-tests/EC_Colocated_Latency/run_supplier_filtering.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
+#
+# $Id$
+#
 
-LOW_PRIORITY="1 20 40 60 80 100 120 140 160"
+LOW_PRIORITY="1 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180"
 
 for c in $LOW_PRIORITY; do
   date
   echo $c
-  ./driver -ORBSvcConf ec.supplier_filter_null.conf -z -i 50000 -n $c > ec_supplier_filter_null.$c.txt 2>&1
-  ./driver -ORBSvcConf ec.supplier_filter_per_supplier.conf -z -i 50000 -n $c > ec_supplier_filter_per_supplier.$c.txt 2>&1
+  ./driver -ORBSvcConf ec.supplier_filter_null.conf -z -i 100000 -n $c > ec_supplier_filter_null.$c.txt 2>&1
+  ./driver -ORBSvcConf ec.supplier_filter_per_supplier.conf -z -i 100000 -n $c > ec_supplier_filter_per_supplier.$c.txt 2>&1
 done
