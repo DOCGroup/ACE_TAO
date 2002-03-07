37eae4d24f7e1ab7b602acdb4ec151f072dff549
Johnny Willemsen <jwillemsen@remedy.nl>
2002-03-07 07:31:11 +0000

    ChangeLogTag: Thu Mar 07 08:27:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- tests/RMCast/RMCast_Fragment_Test.cpp
+++ tests/RMCast/RMCast_Fragment_Test.cpp
@@ -43,7 +43,7 @@ private:
 };
 
 int
-main (int, ACE_TCHAR *[])
+ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACE_START_TEST (ACE_TEXT ("RMCast_Fragment_Test"));
 
--- tests/RMCast/RMCast_Membership_Test.cpp
+++ tests/RMCast/RMCast_Membership_Test.cpp
@@ -155,7 +155,7 @@ private:
 // ****************************************************************
 
 int
-main (int, ACE_TCHAR *[])
+ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACE_START_TEST (ACE_TEXT ("RMCast_Membership_Test"));
 
