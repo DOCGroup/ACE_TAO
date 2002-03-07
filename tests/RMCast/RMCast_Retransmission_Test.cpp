748f24b08612dca703c93d70e91ee92fce3c8d41
Johnny Willemsen <jwillemsen@remedy.nl>
2002-03-07 07:31:11 +0000

    ChangeLogTag: Thu Mar 07 08:27:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- tests/RMCast/RMCast_Retransmission_Test.cpp
+++ tests/RMCast/RMCast_Retransmission_Test.cpp
@@ -179,7 +179,7 @@ private:
 // ****************************************************************
 
 int
-main (int, ACE_TCHAR *[])
+ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACE_START_TEST (ACE_TEXT ("RMCast_Retransmission_Test"));
 
--- tests/RMCast/RMCast_UDP_Best_Effort_Test.cpp
+++ tests/RMCast/RMCast_UDP_Best_Effort_Test.cpp
@@ -116,7 +116,7 @@ private:
 // ****************************************************************
 
 int
-main (int, ACE_TCHAR *[])
+ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACE_START_TEST (ACE_TEXT ("RMCast_UDP_Best_Effort_Test"));
 
