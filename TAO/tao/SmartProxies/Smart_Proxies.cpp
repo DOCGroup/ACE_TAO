e10082518567f82e08621af941d9942d6f449deb
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-03-15 13:51:29 +0000

    ChangeLogTag: Fri Mar 15 07:50:23 2002  Chad Elliott  <elliott_c@ociweb.com>

--- TAO/tao/SmartProxies/Smart_Proxies.cpp
+++ TAO/tao/SmartProxies/Smart_Proxies.cpp
@@ -8,4 +8,9 @@ ACE_RCSID(SmartProxies, Smart_Proxies, "$Id$")
 #include "Smart_Proxies.inl"
 #endif /* defined INLINE */
 
+// Add this line to please the AIX linker, otherwise it complains
+// "ld: 0711-244 ERROR: No csects or exported symbols have been saved."
 
+#if defined (ACE_NDEBUG) && defined (AIX)
+int the_bogus_value_to_please_linker_to_export_symbol;
+#endif
