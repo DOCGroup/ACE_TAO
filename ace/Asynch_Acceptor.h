5c7e5e5738f014954367be734043512cdf3c7e48
Steve Huston <shuston@riverace.com>
2002-03-29 16:09:53 +0000

    ChangeLogTag:Fri Mar 29 11:08:29 2002  Steve Huston  <shuston@riverace.com>

--- ace/Asynch_Acceptor.h
+++ ace/Asynch_Acceptor.h
@@ -50,22 +50,58 @@ public:
   virtual ~ACE_Asynch_Acceptor (void);
 
   /**
-   * This starts the listening process at the port specified by
-   * <address>.  ACE_Asynch_Acceptor initiates the AcceptEx calls with
-   * <bytes_to_read>.  The buffer for the initial data will be created
-   * by ACE_Asynch_Acceptor.  This buffer will be passed to the
-   * handler in the <ACE_Service_Handler::open> callback.  If this
-   * buffer is required past the <open> callback, the
-   * ACE_Service_Handler must copy the data.  If the <pass_addresses>
-   * flag is set, ACE_Asynch_Acceptor will call
-   * <ACE_Service_Handler::addresses> before calling
-   * <ACE_Service_Handler::open>.  The <backlog> parameter specifies
-   * the listen backlog and the outstanding AcceptEx calls.
-   * <number_of_initial_accepts> is the number of asynchronous accepts
-   * that are started at the end of <open>.  If
-   * <number_of_initial_accepts> is -1, then
-   * <number_of_initial_accepts> is set to <backlog> and hence
-   * <backlog> number of asynchronous accepts are started.
+   * @c open starts one or more asynchronous accept requests on a
+   * @arg address. Each accept operation may optionally read an
+   * initial buffer from the new connection when accepted.
+   *
+   * @param address The address to listen/accept connections on.
+   *                If the address does not specify a port, a random
+   *                port is selected and bound.
+   * @param bytes_to_read Optional, specifies the maximum number of bytes
+   *                to read with the accept. The buffer for the initial
+   *                data is allocated internally and passed to the
+   *                @c ACE_Service_Handler::open() hook method. It is
+   *                legitimate only during the @c open() method and must
+   *                be copied if required after @c open() returns.
+   *                This pre-read function works only on Windows.
+   * @param pass_addresses Optional, a non-zero value indicates that
+   *                the local and peer addresses should be passed to the
+   *                associated @c ACE_Service_Handler::addresses() method
+   *                after any call to @c validate_new_connection() and prior
+   *                to the @c open() hook method call.
+   * @param backlog Optional, defaulting to @c ACE_DEFAULT_BACKLOG (which
+   *                can be adjusted in your platform's @c config.h file).
+   *                Specifies the listening backlog for the listening socket.
+   * @param reuse_addr Optional, indicates whether the @c SO_REUSEADDR
+   *                option is set on the listening socket or not.
+   * @param proactor Optional, pointer to the @c ACE_Proactor to use for
+   *                demultiplexing asynchronous accepts. If 0, the
+   *                process's singleton @c ACE_Proactor is used.
+   * @param validate_new_connection Optional, if non-zero, this object's
+   *                @c validate_new_connection() method is called after
+   *                the accept completes, but before the service handler's
+   *                @c open() hook method is called. If @c
+   *                validate_new_connection() returns -1, the newly-accepted
+   *                socket is immediately closed, and the @c addresses()
+   *                method is not called.
+   * @param reissue_accept Optional, if non-zero (the default), a new
+   *                asynchronous accept operation is started after each
+   *                completion, whether the completion is for success or
+   *                failure, and whether or not a successfully-accepted
+   *                connection is subsequently refused.
+   * @param number_of_initial_accepts Optional, the number of asynchronous
+   *                accepts that are started immediately. If -1 (the
+   *                default), the value of @arg backlog is used.
+   *
+   * @note On Windows, the peer address is only available at the time
+   *       the connection is accepted.  Therefore, if you require the peer
+   *       address on Windows, do not rely on the
+   *       @c ACE_SOCK::get_remote_addr() method - it won't work. You must
+   *       supply a non-zero value for @arg pass_addresses and obtain the
+   *       peer address in the @c ACE_Service_Handler::addresses() method.
+   *
+   * @see ACE_INET_Addr
+   * @see ACE_Service_Handler
    */
   virtual int open (const ACE_INET_Addr &address,
                     size_t bytes_to_read = 0,
@@ -88,32 +124,43 @@ public:
    */
   virtual void set_handle (ACE_HANDLE handle);
 
-  /// This initiates a new asynchronous accept through the <AcceptEx>
-  /// call.
+  /// This initiates a new asynchronous accept operation.
+  /**
+   * You need only call this method if the @arg reissue_accept argument
+   * passed to @c open() was 0.
+   */
   virtual int accept (size_t bytes_to_read = 0, const void *act = 0);
 
   /**
-   * This cancels all pending accepts operations that were issued by
-   * the calling thread.
-   * Windows NT- The function does not cancel accept operations
-   *             issued by other threads
-   * POSIX     - all OK, it delegates cancelation to the
-   *             ACE_POSIX_Asynch_Accept
+   * Cancels all pending accepts operations issued by this object.
    *
+   * @note On Windows, only accept operations initiated by the calling thread
+   *       are canceled.
    */
   virtual int cancel (void);
 
   /**
    * Template method for address validation.
    *
-   * Default implemenation always validates the remote address.
+   * This hook method is called after a connection is accepted if
+   * so specified on the call to the @c open() method. If this
+   * method returns -1, the connection is immediately closed and
+   * the service handler is not opened.
+   *
+   * The default implemenation always return 0.
    */
   virtual int validate_new_connection (const ACE_INET_Addr &remote_address);
 
   /**
    * Template method for deciding whether to reissue accept.
    *
-   * Default implemenation always returns this->reissue_accept_.
+   * This hook method is called after each accept completes to decide if
+   * another accept should be initiated. If the method returns a non-zero
+   * value, another accept is initiated.
+   *
+   * The default implemenation always returns the value passed as the
+   * @c open() method's @arg reissue_accept argument. That value can also
+   * be changed using the @c reissue_accept() method.
    */
   virtual int should_reissue_accept (void);
 
