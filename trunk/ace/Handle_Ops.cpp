c642aef2dd6483ccba9900f864615cc30e9e9772
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-04-03 23:38:25 +0000

    ChangeLogTag:Wed Apr  3 11:04:37 2002  Steve Huston  <shuston@riverace.com>

--- trunk/ace/Handle_Ops.cpp
+++ trunk/ace/Handle_Ops.cpp
@@ -12,7 +12,8 @@ ACE_HANDLE
 ACE_Handle_Ops::handle_timed_open (ACE_Time_Value *timeout,
                                    const ACE_TCHAR *name,
                                    int flags,
-                                   int perms)
+                                   int perms,
+                                   LPSECURITY_ATTRIBUTES sa)
 {
   ACE_TRACE ("ACE_Handle_Ops::handle_timed_open");
 
@@ -27,7 +28,8 @@ ACE_Handle_Ops::handle_timed_open (ACE_Time_Value *timeout,
       // Open the named pipe or file using non-blocking mode...
       ACE_HANDLE handle = ACE_OS::open (name,
                                         flags,
-                                        perms);
+                                        perms,
+                                        sa);
       if (handle == ACE_INVALID_HANDLE
           && (errno == EWOULDBLOCK
               && (timeout->sec () > 0 || timeout->usec () > 0)))
@@ -37,5 +39,5 @@ ACE_Handle_Ops::handle_timed_open (ACE_Time_Value *timeout,
       return handle;
     }
   else
-    return ACE_OS::open (name, flags, perms);
+    return ACE_OS::open (name, flags, perms, sa);
 }
