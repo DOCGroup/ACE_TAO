819ff930f9368ace2ced4efc7bbc937ff0bb4478
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-03 12:44:07 +0000

    ChangeLogTag: Fri May  3 07:37:34 2002  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/TAO/orbsvcs/performance-tests/RTEvent/lib/RTClient_Setup.cpp
+++ trunk/TAO/orbsvcs/performance-tests/RTEvent/lib/RTClient_Setup.cpp
@@ -30,18 +30,22 @@ RTClient_Setup::RTClient_Setup (int use_rt_corba,
 
   if (use_rt_corba)
     {
-      this->rtcorba_setup_.reset(
-          new RTCORBA_Setup (orb,
-                             rt_class,
-                             nthreads
-                             ACE_ENV_ARG_PARAMETER));
+      ACE_AUTO_PTR_RESET (this->rtcorba_setup_,
+                          new RTCORBA_Setup (orb,
+                                             rt_class,
+                                             nthreads
+                                             ACE_ENV_ARG_PARAMETER),
+                          RTCORBA_Setup
+                         );
       ACE_CHECK;
 
 #if 0
-      this->priorityband_setup_.reset(
-          new PriorityBand_Setup (orb,
-                                  *this->rtcorba_setup_
-                                  ACE_ENV_ARG_PARAMETER));      
+      ACE_AUTO_PTR_RESET (this->priorityband_setup_,
+                          new PriorityBand_Setup (orb,
+                                                  *this->rtcorba_setup_
+                                                  ACE_ENV_ARG_PARAMETER),
+                          PriorityBand_Setup
+                         );
       ACE_CHECK;
         
 #endif /* 0 */
