ff4d366cfff4e34655bfd0e9cd8f621cd439dd00
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-07 20:02:59 +0000

    This commit was manufactured by cvs2svn to create branch 'connector_lane_0'.

--- TAO/utils/catior/test.bat
+++
@@ -1,6 +0,0 @@
-catior -f .\test\EventService.ior
-catior -f .\test\test1.ior
-catior -f .\test\test2.ior
-catior -f .\test\timeserver.ior
-catior -f .\test\timeserver.iiop
-catior -f .\test\param_test.iiop
