564bbd6acbce07facaf26a10802f69f224919771
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-15 23:02:31 +0000

    Wed May 15 15:59:42 2002  Jaiganesh Balasubramanian  <jai@doc.ece.uci.edu>

--- trunk/TAO/orbsvcs/orbsvcs/Event/rtcorba_event_export.h
+++ trunk/TAO/orbsvcs/orbsvcs/Event/rtcorba_event_export.h
@@ -9,9 +9,15 @@
 
 #include "ace/config-all.h"
 
-#if !defined (TAO_RTCORBAEVENT_HAS_DLL)
-#  define TAO_RTCORBAEVENT_HAS_DLL 1
-#endif /* ! TAO_RTCORBAEVENT_HAS_DLL */
+#if defined (TAO_AS_STATIC_LIBS)
+#  if !defined (TAO_RTCORBAEVENT_HAS_DLL)
+#    define TAO_RTCORBAEVENT_HAS_DLL 0
+#  endif /* ! TAO_RTCORBAEVENT_HAS_DLL */
+#else
+#  if !defined (TAO_RTCORBAEVENT_HAS_DLL)
+#    define TAO_RTCORBAEVENT_HAS_DLL 1
+#  endif /* ! TAO_RTCORBAEVENT_HAS_DLL */
+#endif
 
 #if defined (TAO_RTCORBAEVENT_HAS_DLL) && (TAO_RTCORBAEVENT_HAS_DLL == 1)
 #  if defined (TAO_RTCORBAEVENT_BUILD_DLL)
