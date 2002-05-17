b9000c21ffa3a2c4807bc042ec6bea2ad3c18b26
dhinton <dhinton@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-17 13:52:05 +0000

    ChangeLogTag:Fri May 17 13:24:59 UTC 2002  Don Hinton  <dhinton@ieee.org>

--- tests/Framework_Component_DLL.h
+++ tests/Framework_Component_DLL.h
@@ -20,10 +20,13 @@
 #ifndef ACE_TESTS_FRAMEWORK_COMPONENT_DLL_H
 #define ACE_TESTS_FRAMEWORK_COMPONENT_DLL_H
 
+#include "Framework_Component_DLL_Export.h"
 #include "ace/Log_Msg.h"
+#include "ace/Synch_T.h"
+#include "ace/Singleton.h"
 
 /// This  we will use to test the ACE_Framework_Repository
-class Simple_Service
+class Framework_Component_DLL_Export Simple_Service
 {
 public:
   Simple_Service (void)
@@ -34,6 +37,27 @@ public:
     {
       ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Simple_Service::~Simple_Service\n")));
     }
+
+  virtual const ACE_TCHAR *dll_name (void) 
+    {
+      return 0;
+    }
+
 };
+
+// Adapter that contains the required dll_name() method and 
+// has the "right" name for our DLL.
+template <class TYPE>
+class FWCT_DLL_Singleton_Adapter_T : public TYPE
+{
+public:
+  const ACE_TCHAR *dll_name (void) 
+  { 
+    return ACE_TEXT("Framework_Component_DLL");
+  }
+};
+
+typedef ACE_DLL_Singleton_T < FWCT_DLL_Singleton_Adapter_T <Simple_Service>, 
+                     ACE_SYNCH_MUTEX > SS_SINGLETON;
 
 #endif /* ACE_TESTS_FRAMEWORK_COMPONENT_TEST_H */
--- tests/Framework_Component_Test.cpp
+++ tests/Framework_Component_Test.cpp
@@ -13,63 +13,18 @@
 //
 // ============================================================================
 
-//#include "ace/Framework_Component.h"
+#include "Framework_Component_DLL.h"
 #include "ace/Service_Config.h"
-//#include "ace/OS.h"
 #include "ace/ARGV.h"
 #include "tests/test_config.h"
-//#include "tests/Framework_Component_Test.h"
 
 ACE_RCSID(tests, Framework_Component_Test, "$Id$")
-
-// This is the original test, that tests the basic functions of the
-// repository.
-void repository_test (void)
-{
-  //Server s;
-  //ACE_ARGV a("xxx xxx");
-  //s.init(a.argc (), a.argv ());
-
-  // Create an instance of Simple_Service that will be managed
-  // by the ACE_Framework_Repository and keep a pointer to
-  // instance.
-  //Simple_Service *first = SS_SINGLETON::instance ();
-
-  //FWCT_DLL_Singleton_Adapter_T <Simple_Service> *ss = SS_SINGLETON::instance ();
-
-  //ACE_DEBUG ((LM_DEBUG, 
-  //            ACE_LIB_TEXT ("And the name of the dll is: %s\n"),
-  //            ss->dll_name ()));
-
-  // Now, close down the repository.
-  ACE_Framework_Repository::close_singleton ();
-
-  ACE_Framework_Repository::instance ();
-
-  // Now get another pointer to Simple_Service and make sure
-  // that it's different.  We could also test other pointers
-  // like ACE_Reactor, ACE_Proactor, or the repository itself
-  // if we wanted.
-  //Simple_Service *second = SS_SINGLETON::instance ();
-
-  //if (first == second)
-  //  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Pointer are equal, but shouldn't be.\n")));
-
-  // Close down the repository again since we are writing debug messages.
-  // And want all of them to go to the log.
-  ACE_Framework_Repository::close_singleton ();
-
-}
 
 int
 ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACE_START_TEST (ACE_TEXT("Framework_Component_Test"));
 
-  // First run the regular repository test (this was actually all the
-  // test was originally).
-  //repository_test ();
-  
   // Now, let the ACE Service Configurator framework load our service from a
   // dll, which contains a singleton.
   ACE_ARGV args;
@@ -79,13 +34,30 @@ ACE_TMAIN (int, ACE_TCHAR *[])
   //args.add (ACE_TEXT ("-f"));
   //args.add (ACE_TEXT ("Framework_Component_Test.conf"));
   args.add (ACE_TEXT ("-S"));
-  args.add (ACE_TEXT ("\"dynamic Server Service_Object * Framework_Component_DLL:_make_Server() 'xxx' \""));
+  args.add (ACE_TEXT ("\"dynamic Server_1 Service_Object * "
+                      "Framework_Component_DLL:_make_Server_1() 'xxx' \""));
 
   // Load it, should load a dll.
   ACE_Service_Config::open (args.argc (), args.argv ());
 
-  // And unload it, should unload the dll.
-  ACE_Service_Config::process_directive (ACE_TEXT ("\"remove Server\""));  
+  // Now that the dll has been loaded, the singleton is available.
+  Simple_Service *ss = SS_SINGLETON::instance ();
+  ACE_DEBUG ((LM_DEBUG, 
+              ACE_TEXT ("Simple_Service lives in library: %s\n"),
+              ss->dll_name ()));
+
+  // Now load another service from the same library.
+  ACE_Service_Config::process_directive 
+    (ACE_TEXT ("dynamic Server_2 Service_Object * "
+               "Framework_Component_DLL:_make_Server_2() 'xxx' "));
+
+  // And unload the first one, should *not* unload the dll.
+  ACE_Service_Config::process_directive (ACE_TEXT ("remove Server_1"));  
+
+  // Make sure our singlton is still happy..
+  ACE_DEBUG ((LM_DEBUG, 
+              ACE_TEXT ("Simple_Service is still alive in library: %s\n"),
+              ss->dll_name ()));
 
   //ACE_Service_Config::close ();
 
