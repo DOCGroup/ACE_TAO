d0f1f75fcadd38301742ce455842b85a37e1c85e
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-17 00:07:13 +0000

    This commit was manufactured by cvs2svn to create branch 'jeremy'.

--- TAO/orbsvcs/orbsvcs/Event/EC_Reactive_SupplierControl.i
+++
@@ -1,8 +0,0 @@
-// $Id$
-
-ACE_INLINE
-TAO_EC_Ping_Supplier::TAO_EC_Ping_Supplier (TAO_EC_SupplierControl *control)
-  :  control_ (control)
-{
-}
-
--- ace/config-aix5.1.h
+++
@@ -1,8 +0,0 @@
-// $Id$
-//
-// Config file for AIX 5.1
-
-#include "ace/config-aix-4.x.h"
-
-// I think this is correct, but needs to be verified...   -Steve Huston
-#define ACE_HAS_SIGTIMEDWAIT
