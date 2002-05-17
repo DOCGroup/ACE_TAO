b0a7a3f94e4070907c0d30bd26ff06ee27fabf5a
dhinton <dhinton@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-17 13:52:05 +0000

    ChangeLogTag:Fri May 17 13:24:59 UTC 2002  Don Hinton  <dhinton@ieee.org>

--- branches/dll_singleton/tests/Framework_Component_DLL.cpp
+++ branches/dll_singleton/tests/Framework_Component_DLL.cpp
@@ -1,31 +1,13 @@
 // -*- C++ -*-
 
 #include "Framework_Component_DLL.h"
-#include "Framework_Component_DLL_Export.h"
 #include "ace/Service_Config.h"
-#include "ace/Singleton.h"
+//#include "ace/Singleton.h"
 #include "ace/OS.h"
 
 ACE_RCSID (tests,
            Framework_Component_DLL,
            "$Id$")
-
-
-// Adapter that contains the required dll_name() method and 
-// has the "right" name for our DLL.
-template <class TYPE>
-class FWCT_DLL_Singleton_Adapter_T : public TYPE
-{
-public:
-  const ACE_TCHAR *dll_name (void) 
-  { 
-    return ACE_TEXT("Framework_Component_Test");
-  }
-};
-
-typedef ACE_DLL_Singleton_T < FWCT_DLL_Singleton_Adapter_T <Simple_Service>, 
-                     ACE_SYNCH_MUTEX > SS_SINGLETON;
-
 
 /// This is just a simple server that can be loaded via the ACE
 /// Service Configuration framework and uses the singleton 
@@ -33,7 +15,8 @@ typedef ACE_DLL_Singleton_T < FWCT_DLL_Singleton_Adapter_T <Simple_Service>,
 /// if were in it's own dll, so that when it is unloaded we can
 /// watch ACE_DLL call ACE_Framework_Repository::remove_components()
 /// in order to cleanup the singletons associated with the dll.
-class Server : public ACE_Service_Object
+template <int>
+class Server_T : public ACE_Service_Object
 {
 public:
   int init (int, ACE_TCHAR *[]) 
@@ -56,10 +39,11 @@ public:
   }
 };
 
-//#define Framework_Component_Test_Export
+typedef Server_T <1> Server_1;
+typedef Server_T <2> Server_2;
 
-ACE_FACTORY_DECLARE (Framework_Component_DLL, Server)
-ACE_FACTORY_DEFINE (Framework_Component_DLL, Server)
+ACE_FACTORY_DEFINE (Framework_Component_DLL, Server_1)
+ACE_FACTORY_DEFINE (Framework_Component_DLL, Server_2)
 
 
 
