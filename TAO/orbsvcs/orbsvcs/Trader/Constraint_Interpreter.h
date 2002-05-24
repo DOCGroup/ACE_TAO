850615752fd5ccae5c45b29a1745dec6c3cc17c0
Johnny Willemsen <jwillemsen@remedy.nl>
2002-05-24 07:22:35 +0000

    ChangeLogTag: Fri May 24 09:21:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/Trader/Constraint_Interpreter.h
+++ TAO/orbsvcs/orbsvcs/Trader/Constraint_Interpreter.h
@@ -1,18 +1,15 @@
 /* -*- C++ -*- */
-// $Id$
 
-// ========================================================================
-//
-// = LIBRARY
-//    orbsvcs
-//
-// = FILENAME
-//    Constraint_Interpreter.h
-//
-// = AUTHOR
-//    Seth Widoff <sbw1@cs.wustl.edu>
-//
-// ========================================================================
+//=============================================================================
+/**
+ *  @file    Constraint_Interpreter.h
+ *
+ *  $Id$
+ *
+ *  @author Seth Widoff <sbw1@cs.wustl.edu>
+ */
+//=============================================================================
+
 
 #ifndef TAO_CONSTRAINT_INTERPRETER_H
 #define TAO_CONSTRAINT_INTERPRETER_H
@@ -28,23 +25,24 @@
 class TAO_Constraint_Evaluator;
 class TAO_Constraint_Validator;
 
+/**
+ * @class TAO_Constraint_Interpreter
+ *
+ * @brief TAO_Constraint_Interpreter will, given a constraint string whose
+ * syntax and semantics comply with the trader specification for the
+ * constraint language, determine if a CosTrading::Offer meets the
+ * constraints.
+ *
+ * TAO_Constraint_Interpreter will first build an expression tree
+ * representing the constraint expression using Lex and Yacc. Then,
+ * using a TAO_Constraint_Validator, it will validate the semantic
+ * correctness of the tree. When the evaluate method is invoked with
+ * an Offer, the TAO_Constraint_Interpreter will construct an
+ * EvaluationVisitor, which will evaluate the tree and decide
+ * whether the offer meets the constraints.
+ */
 class TAO_Trading_Export TAO_Constraint_Interpreter : public TAO_Interpreter
 {
-  //
-  // = TITLE
-  //   TAO_Constraint_Interpreter will, given a constraint string whose
-  //   syntax and semantics comply with the trader specification for the
-  //   constraint language, determine if a CosTrading::Offer meets the
-  //   constraints.
-  //
-  // = DESCRIPTION
-  //   TAO_Constraint_Interpreter will first build an expression tree
-  //   representing the constraint expression using Lex and Yacc. Then,
-  //   using a TAO_Constraint_Validator, it will validate the semantic
-  //   correctness of the tree. When the evaluate method is invoked with
-  //   an Offer, the TAO_Constraint_Interpreter will construct an
-  //   EvaluationVisitor, which will evaluate the tree and decide
-  //   whether the offer meets the constraints.
 public:
   // = Initialization and termination methods.
   TAO_Constraint_Interpreter (const CosTradingRepos::ServiceTypeRepository::TypeStruct& ts,
@@ -53,18 +51,20 @@ public:
     ACE_THROW_SPEC ((CosTrading::IllegalConstraint,
                      CORBA::NO_MEMORY));
 
+  /**
+   * This constructor builds an expression tree representing the
+   * constraint specified in <constraints>, and throws an Illegal
+   * Constraint exception if the constraint given has syntax errors or
+   * semantic errors, such as mismatched types.
+   */
   TAO_Constraint_Interpreter (TAO_Constraint_Validator& validator,
                               const char* constraints
                               ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CosTrading::IllegalConstraint,
                      CORBA::NO_MEMORY));
-  // This constructor builds an expression tree representing the
-  // constraint specified in <constraints>, and throws an Illegal
-  // Constraint exception if the constraint given has syntax errors or
-  // semantic errors, such as mismatched types.
 
+  /// Destructor
   ~TAO_Constraint_Interpreter (void);
-  // Destructor
 
   CORBA::Boolean evaluate (CosTrading::Offer* offer);
 
@@ -74,20 +74,22 @@ public:
   // tree was constructed. This method is thread safe (hopefully).
 };
 
+/**
+ * @class TAO_Preference_Interpreter
+ *
+ * @brief The TAO_Preference_Interpreter will, given a valid preference
+ * string and offers, will order the offers based on the offers'
+ * compliance with the preferences.
+ *
+ * Each time the order_offer method is invoked, the
+ * TAO_Preference_Interpreter stores the offer reference in the
+ * order dictated by its evaluation of the preference string. After
+ * the TAO_Preference_Interpreter client has finished ordering all
+ * the offers, it will extract the offers in order using the
+ * remove_offer method.
+ */
 class TAO_Trading_Export TAO_Preference_Interpreter : public TAO_Interpreter
 {
-  // = TITLE
-  //   The TAO_Preference_Interpreter will, given a valid preference
-  //   string and offers, will order the offers based on the offers'
-  //   compliance with the preferences.
-  //
-  // = DESCRIPTION
-  //   Each time the order_offer method is invoked, the
-  //   TAO_Preference_Interpreter stores the offer reference in the
-  //   order dictated by its evaluation of the preference string. After
-  //   the TAO_Preference_Interpreter client has finished ordering all
-  //   the offers, it will extract the offers in order using the
-  //   remove_offer method.
 public:
   // = Initialization and termination methods.
   TAO_Preference_Interpreter (const CosTradingRepos::ServiceTypeRepository::TypeStruct& ts,
@@ -96,63 +98,64 @@ public:
     ACE_THROW_SPEC ((CosTrading::Lookup::IllegalPreference,
                      CORBA::NO_MEMORY));
 
+  /**
+	* Parse the preference string, determining first if it's
+	* valid. Throw an IllegalPreference exception if the preference
+	* doesn't conform to the BNF grammar for preferences.
+	*/
   TAO_Preference_Interpreter (TAO_Constraint_Validator& validator,
                               const char* preference
                               ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CosTrading::Lookup::IllegalPreference,
                      CORBA::NO_MEMORY));
 
-  // Parse the preference string, determining first if it's
-  // valid. Throw an IllegalPreference exception if the preference
-  // doesn't conform to the BNF grammar for preferences.
-
+  /// Destructor
   ~TAO_Preference_Interpreter(void);
-  // Destructor
 
   void order_offer (CosTrading::Offer* offer,
                     CosTrading::OfferId offer_id = 0);
 
+  /// Evaluate the offer, and order it internally based on the results
+  /// of the evaluation.
   void order_offer (TAO_Constraint_Evaluator& evaluator,
                     CosTrading::Offer* offer,
                     CosTrading::OfferId offer_id = 0);
-  // Evaluate the offer, and order it internally based on the results
-  // of the evaluation.
 
   int remove_offer (CosTrading::Offer*& offer,
                     CosTrading::OfferId& offer_id);
 
+  /// Remove the next offer. The offer returned will be the next in the
+  /// ordering determined by the preference string.
   int remove_offer (CosTrading::Offer*& offer);
-  // Remove the next offer. The offer returned will be the next in the
-  // ordering determined by the preference string.
 
+  /// Return the number of offers remaining in the ordering.
   int num_offers (void);
-  // Return the number of offers remaining in the ordering.
 
   struct Preference_Info
   {
+    /// True if the preference evaluation didn't return an error for this offer.
     CORBA::Boolean evaluated_;
-    // True if the preference evaluation didn't return an error for this offer.
 
+    /// The value of the preference evaluation.
     TAO_Literal_Constraint value_;
-    // The value of the preference evaluation.
 
+    /// The offer id of this offer.
     CosTrading::OfferId offer_id_;
-    // The offer id of this offer.
 
+    /// A pointer to the offer.
     CosTrading::Offer* offer_;
-    // A pointer to the offer.
   };
 
   typedef ACE_Unbounded_Queue<Preference_Info> Ordered_Offers;
 
 private:
 
+  /// Disallow copying.
   TAO_Preference_Interpreter (const TAO_Preference_Interpreter&);
   TAO_Preference_Interpreter& operator= (const TAO_Preference_Interpreter&);
-  // Disallow copying.
 
+  /// The ordered list of offers.
   Ordered_Offers offers_;
-  // The ordered list of offers.
 };
 
 #include "ace/post.h"
