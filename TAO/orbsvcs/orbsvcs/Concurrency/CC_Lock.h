23a4d9a3c1ed8cf08aee3d2c3ad284e8576bf329
Johnny Willemsen <jwillemsen@remedy.nl>
2002-05-24 07:10:21 +0000

    ChangeLogTag: Fri May 24 09:09:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/Concurrency/CC_Lock.h
+++ TAO/orbsvcs/orbsvcs/Concurrency/CC_Lock.h
@@ -1,22 +1,19 @@
 /* -*- C++ -*- */
-// $Id$
 
-// ============================================================================
-//
-// = LIBRARY
-//    TAO/orbsvcs/Concurrency_Service
-//
-// = FILENAME
-//    CC_Lock.h
-//
-// = DESCRIPTION
-//      This class implements a lock used by the lock set from the
-//      concurrency control service
-//
-// = AUTHORS
-//    Torben Worm <tworm@cs.wustl.edu>
-//
-// ============================================================================
+//=============================================================================
+/**
+ *  @file    CC_Lock.h
+ *
+ *  $Id$
+ *
+ *    This class implements a lock used by the lock set from the
+ *    concurrency control service
+ *
+ *
+ *  @author Torben Worm <tworm@cs.wustl.edu>
+ */
+//=============================================================================
+
 
 #ifndef _CC_LOCK_H
 #define _CC_LOCK_H
@@ -31,109 +28,113 @@
 #include "orbsvcs/CosConcurrencyControlC.h"
 #include "concurrency_export.h"
 
+/// This constant defines the number of lock modes. There is really no
+/// way to set this constant dynamically because the nuber of lock
+/// modes are not stated as part of the IDL.
 #define NUMBER_OF_LOCK_MODES 5
-// This constant defines the number of lock modes. There is really no
-// way to set this constant dynamically because the nuber of lock
-// modes are not stated as part of the IDL.
 
+/**
+ * @class CC_Lock
+ *
+ * @brief CC_Lock
+ *
+ * This class implements the lock concept from the concurrency
+ * control service. The lock holds its mode - this might later
+ * be changed to subclasses depending on the differences of the
+ * locks.  At present the is only a lock-pr-thread/client-type
+ * which is essentially a write lock since it is not allowed to
+ * have more than one lock pr. servant in this implementation.
+ */
 class TAO_Concurrency_Export CC_Lock
 {
-  // = TITLE
-  //     CC_Lock
-  //
-  // = DESCRIPTION
-  //     This class implements the lock concept from the concurrency
-  //     control service. The lock holds its mode - this might later
-  //     be changed to subclasses depending on the differences of the
-  //     locks.  At present the is only a lock-pr-thread/client-type
-  //     which is essentially a write lock since it is not allowed to
-  //     have more than one lock pr. servant in this implementation.
 public:
+  /// Creates the lock with mode = intention_read (weakest)
   CC_Lock (void);
-  // Creates the lock with mode = intention_read (weakest)
 
+  /// Creates the lock with the desired mode
   CC_Lock (CosConcurrencyControl::lock_mode mode);
-  // Creates the lock with the desired mode
 
+  /// Deletes the lock
   ~CC_Lock (void);
-  // Deletes the lock
 
+  /// Acquires this lock. Blocks until lock is obtained
   void lock (ACE_ENV_SINGLE_ARG_DECL);
-  // Acquires this lock. Blocks until lock is obtained
 
+  /// Tries to acquire this lock. If it is not possible to acquire the
+  /// lock, false is returned
   CORBA::Boolean try_lock (ACE_ENV_SINGLE_ARG_DECL);
-  // Tries to acquire this lock. If it is not possible to acquire the
-  // lock, false is returned
 
+  /// Releases this lock.
   void unlock (ACE_ENV_SINGLE_ARG_DECL);
-  // Releases this lock.
 
+  /// Changes the mode of this lock.
   void change_mode (CosConcurrencyControl::lock_mode new_mode
                    ACE_ENV_ARG_DECL);
-  // Changes the mode of this lock.
 
+  /// Sets the mode_ of the lock. Used in initialization
   void set_mode (CosConcurrencyControl::lock_mode mode);
-  // Sets the mode_ of the lock. Used in initialization
 
+  /// Returns true if this lock is compatible with the other lock.
   CORBA::Boolean Compatible (const CC_Lock &other);
-  // returns true if this lock is compatible with the other lock.
 
+  /// Returns true is this lock is compatible with the referenced mode.
   CORBA::Boolean Compatible (CosConcurrencyControl::lock_mode mode);
-  // Returns true is this lock is compatible with the referenced mode.
 
+  /// Returns the mode of the lock.
   CosConcurrencyControl::lock_mode GetMode (void);
-  // Returns the mode of the lock.
 
+  /// Returns the number of times this lock have been locked
   int GetLocksHeld(void);
-  // Returns the number of times this lock have been locked
 
+  /// Decrements the number of locks held in this mode. Used by change_mode.
   void DecLocksHeld(void);
-  // Decrements the number of locks held in this mode. Used by change_mode.
 
+  /// Dumps the state of the object to stdout
   void dump(void);
-  // Dumps the state of the object to stdout
 
 protected:
+  /// Holds the lock's mode.
   CosConcurrencyControl::lock_mode mode_;
-  // Holds the lock's mode.
 
 private:
+  /// If greater than zero the lock is held (that number of times).
   int lock_held_;
-  // If greater than zero the lock is held (that number of times).
 
+  /// Defines the compatibility of the locks.
   static CORBA::Boolean compatible_[NUMBER_OF_LOCK_MODES][NUMBER_OF_LOCK_MODES];
-  // Defines the compatibility of the locks.
 };
 
+/**
+ * @class CC_LockModeIterator
+ *
+ * @brief CC_LockModeIterator
+ *
+ * This class implements an iterator over the lock modes in
+ * order to make an ordered traversal over the locks from the
+ * weakest (intention read) to the strongest (write).
+ * Ordering: IR -> R -> U -> IW -> W
+ */
 class TAO_Concurrency_Export CC_LockModeIterator
 {
-  // = TITLE
-  //     CC_LockModeIterator
-  //
-  // = DESCRIPTION
-  //     This class implements an iterator over the lock modes in
-  //     order to make an ordered traversal over the locks from the
-  //     weakest (intention read) to the strongest (write).
-  //     Ordering: IR -> R -> U -> IW -> W
 public:
+  /// Default constructor
   CC_LockModeIterator(void);
-  // Default constructor
 
+  /// Destructor
   ~CC_LockModeIterator(void);
-  // Destructor
 
+  /// Reset the iterator to the first element
   void First(void);
-  // Reset the iterator to the first element
 
+  /// Advance the iterator to the next element
+  /// Throws exception if out of range
   void Next(ACE_ENV_SINGLE_ARG_DECL);
-  // Advance the iterator to the next element
-  // Throws exception if out of range
 
+  /// Returns true if the iterator has reached the last element
   CORBA::Boolean IsDone(void);
-  // Returns true if the iterator has reached the last element
 
+  /// Get the current element
   CosConcurrencyControl::lock_mode GetLockMode(void);
-  // Get the current element
 
 private:
   CosConcurrencyControl::lock_mode current_;
--- TAO/orbsvcs/orbsvcs/Concurrency/Concurrency_Utils.h
+++ TAO/orbsvcs/orbsvcs/Concurrency/Concurrency_Utils.h
@@ -1,24 +1,21 @@
 /* -*- C++ -*- */
-// $Id$
 
-// ============================================================================
-//
-// = LIBRARY
-//    TAO/orbsvcs/Concurrency_Service
-//
-// = FILENAME
-//    Concurrency_Utils.h
-//
-// = DESCRIPTION
-//      This class implements a Concurrency Server wrapper class which
-//      holds a number of lock sets.  The server must run in the
-//      thread per request concurrency model in order to let the
-//      clients block on the semaphores.
-//
-// = AUTHORS
-//    Torben Worm <tworm@cs.wustl.edu>
-//
-// ============================================================================
+//=============================================================================
+/**
+ *  @file    Concurrency_Utils.h
+ *
+ *  $Id$
+ *
+ *    This class implements a Concurrency Server wrapper class which
+ *    holds a number of lock sets.  The server must run in the
+ *    thread per request concurrency model in order to let the
+ *    clients block on the semaphores.
+ *
+ *
+ *  @author Torben Worm <tworm@cs.wustl.edu>
+ */
+//=============================================================================
+
 
 #ifndef _CONCURRENCY_SERVER_H
 #define _CONCURRENCY_SERVER_H
@@ -29,41 +26,43 @@
 #include "CC_LockSetFactory.h"
 #include "concurrency_export.h"
 
+/**
+ * @class TAO_Concurrency_Server
+ *
+ * @brief Defines a wrapper class for the implementation of the
+ * concurrency server.
+ *
+ * This class takes an orb and Poa reference and activates the
+ * concurrency service lock set factory object under them.
+ */
 class TAO_Concurrency_Export TAO_Concurrency_Server
 {
-  // = TITLE
-  //    Defines a wrapper class for the implementation of the
-  //    concurrency server.
-  //
-  // = DESCRIPTION
-  //    This class takes an orb and Poa reference and activates the
-  //    concurrency service lock set factory object under them.
 public:
   // = Initialization and termination methods.
+  ///Default constructor.
   TAO_Concurrency_Server (void);
-  //Default constructor.
 
+  /// Takes the POA under which to register the Concurrency Service
+  /// implementation object.
   TAO_Concurrency_Server (CORBA::ORB_ptr orb,
                           PortableServer::POA_ptr poa);
-  // Takes the POA under which to register the Concurrency Service
-  // implementation object.
 
+  /// Destructor.
   ~TAO_Concurrency_Server (void);
-  // Destructor.
 
+  /// Initialize the concurrency server under the given ORB and POA.
   CORBA::Object_ptr init (CORBA::ORB_ptr orb,
                           PortableServer::POA_ptr poa);
-  // Initialize the concurrency server under the given ORB and POA.
 
   /// Cleanup resources.
   int fini (void);
 
+  /// Get the lock set factory.
   CC_LockSetFactory *GetLockSetFactory (void);
-  // Get the lock set factory.
 
 private:
+  /// This is the lock set factory activated under the POA.
   CC_LockSetFactory lock_set_factory_;
-  // This is the lock set factory activated under the POA.
 
   /// The POA which the lock set factory servant was registered.
   PortableServer::POA_var poa_;
