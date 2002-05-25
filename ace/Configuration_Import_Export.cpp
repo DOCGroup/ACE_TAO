987115abd84168e457156fff08c6eb2c1a7f4805
schmidt <douglascraigschmidt@users.noreply.github.com>
2002-05-25 21:55:48 +0000

    ChangeLogTag:Sat May 25 16:42:46 2002  Douglas C. Schmidt  <schmidt@tango.doc.wustl.edu>

--- ace/Configuration_Import_Export.cpp
+++ ace/Configuration_Import_Export.cpp
@@ -598,7 +598,7 @@ ACE_Ini_ImpExp::squish (ACE_TCHAR *src)
 
   // Now start at the beginning and move over all whitespace.
   for (cp = src;
-       (*cp != '\0') && ((*cp == ' ') || (*cp == '\t'));
+       (*cp != '\0') && ((*cp == ' ') || (*cp == '\t') || (*cp == '\n'));
        cp++)
     continue;
 
