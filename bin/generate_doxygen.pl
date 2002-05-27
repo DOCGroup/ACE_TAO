8563c7c616d6a93c527ce6e3f57a6c5d03398aba
Johnny Willemsen <jwillemsen@remedy.nl>
2002-05-27 05:39:29 +0000

    ChangeLogTag: Mon May 27 07:34:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- bin/generate_doxygen.pl
+++ bin/generate_doxygen.pl
@@ -39,8 +39,9 @@ $dot_path = '/usr/local/bin';
              ,'tao_strategies'
              ,'tao_smartproxies'
              ,'tao_av'
-	     ,'tao_security'
-	     ,'tao_ssliop'
+             ,'tao_security'
+             ,'tao_ssliop'
+             ,'tao_cosnaming'
             );
 
 # Modify defaults using the command line arguments
