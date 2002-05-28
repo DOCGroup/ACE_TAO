ed60f3d74a9d56dfffea643ca6fe5ca5a04312e3
Johnny Willemsen <jwillemsen@remedy.nl>
2002-05-28 05:45:42 +0000

    ChangeLogTag: Tue May 28 07:43:12 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/Event/BCU.h
+++ TAO/orbsvcs/orbsvcs/Event/BCU.h
@@ -1,22 +1,19 @@
 /* -*- C++ -*- */
-// $Id$
-//
-// ============================================================================
-//
-// = LIBRARY
-//    ace ORB
-//
-// = FILENAME
-//    Benchmark Computation Units
-//
-// = AUTHOR
-//    David Levine and Tim Harrison (harrison@cs.wustl.edu)
-//
-// = DESCRIPTION
-//
-//    Times how long it takes to generate each of the first N prime
-//    numbers.
-// ============================================================================
+//=============================================================================
+/**
+ *  @file    Benchmark Computation Units
+ *
+ *  $Id$
+ *
+ *  @author David Levine and Tim Harrison (harrison@cs.wustl.edu)
+ *
+ *
+ *  Times how long it takes to generate each of the first N prime
+ *  numbers.
+ *
+ */
+//=============================================================================
+
 
 #ifndef ACE_BCU_H
 #define ACE_BCU_H
@@ -30,14 +27,12 @@
 
 #include "old_event_export.h"
 
-
-TAO_RTOLDEvent_Export u_long ACE_BCU (u_long n);
 // Check if a specific, hardcoded number is prime (via ACE::is_prime) <n>
 // times.
+TAO_RTOLDEvent_Export u_long ACE_BCU (u_long n);
 
-
-TAO_RTOLDEvent_Export u_long ACE_BCU (u_long number, u_long n);
 // Check if <number> is prime (via ACE::is_prime ()) <n> times.
+TAO_RTOLDEvent_Export u_long ACE_BCU (u_long number, u_long n);
 
 #include "ace/post.h"
 #endif /* ACE_BCU_H */
