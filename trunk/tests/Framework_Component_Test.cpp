37c008ef66ea48c0344a73b6ffc2043783e9db50
dhinton <dhinton@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-05-30 10:47:05 +0000

    ChangeLogTag:Thu May 30 10:18:27 UTC 2002  Don Hinton  <dhinton@ieee.org>

--- trunk/tests/Framework_Component_Test.cpp
+++ trunk/tests/Framework_Component_Test.cpp
@@ -46,7 +46,8 @@ ACE_RCSID(tests, Framework_Component_Test, "$Id$")
     ACE_TEXT ("path='Framework_Component_DLL' params=''/> </dynamic>")
 
 # define REMOVE_SERVICE(X) \
-   ACE_TEXT ( "<?xml version='1.0'?> <remove id='Server_") ACE_TEXT(#X) ACE_TEXT("'> </remove>")
+   ACE_TEXT ( "<?xml version='1.0'?> <remove id='Server_") ACE_TEXT(#X) \
+   ACE_TEXT("'> </remove>")
 
 #endif /* ACE_USES_CLASSIC_SVC_CONF */
 
@@ -54,12 +55,12 @@ int
 run_test (u_long unload_mask = 0)
 {
   ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Running test with mask = %s|%s\n"),
-              ACE_BIT_ENABLED(unload_mask, ACE_DLL_Manager_Ex::PER_DLL) == 0
+              ACE_BIT_ENABLED(unload_mask, ACE_DLL_UNLOAD_POLICY_PER_DLL) == 0
               ? ACE_TEXT ("PER_PROCESS") : ACE_TEXT ("PER_DLL"),
-              ACE_BIT_ENABLED(unload_mask, ACE_DLL_Manager_Ex::LAZY) == 0
+              ACE_BIT_ENABLED(unload_mask, ACE_DLL_UNLOAD_POLICY_LAZY) == 0
               ? ACE_TEXT ("EAGER") : ACE_TEXT ("LAZY")));
 
-  ACE_DLL_Manager::instance ()->unload_strategy (unload_mask);
+  ACE_DLL_Manager::instance ()->unload_policy (unload_mask);
 
   // Now, let the ACE Service Configurator framework load our service from a
   // dll, which contains a singleton.
@@ -90,7 +91,7 @@ run_test (u_long unload_mask = 0)
   ACE_Service_Config::process_directive (REMOVE_SERVICE(2));
 
   // Force unloading so we'll be ready for the next test.
-  ACE_DLL_Manager::instance ()->unload_strategy (ACE_DLL_Manager_Ex::DEFAULT);
+  ACE_DLL_Manager::instance ()->unload_policy (ACE_DLL_UNLOAD_POLICY_DEFAULT);
 
   ACE_Service_Config::close ();
   return 0;
@@ -104,16 +105,16 @@ ACE_TMAIN (int, ACE_TCHAR *[])
   int retval = 0;
 
   // Use defaults, i.e., per process, eager unloading.
-  retval += run_test ();
+  retval += run_test (ACE_DLL_UNLOAD_POLICY_DEFAULT);
 
   // Per process, lazy unloading
-  retval += run_test (ACE_DLL_Manager_Ex::LAZY);
+  retval += run_test (ACE_DLL_UNLOAD_POLICY_LAZY);
 
   // Per dll, eager unloading
-  retval += run_test (ACE_DLL_Manager_Ex::PER_DLL);
+  retval += run_test (ACE_DLL_UNLOAD_POLICY_PER_DLL);
 
   // Per dll, lazy unloading.
-  retval += run_test (ACE_DLL_Manager_Ex::PER_DLL | ACE_DLL_Manager_Ex::LAZY);
+  retval += run_test (ACE_DLL_UNLOAD_POLICY_PER_DLL | ACE_DLL_UNLOAD_POLICY_LAZY);
 
   ACE_END_TEST;
   return retval == 0 ? 0 : -1;
