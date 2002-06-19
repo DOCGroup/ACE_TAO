7bb6e8d523f399fbfc69a09a0b2bb399dbc3cd92
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-06-19 19:22:56 +0000

    ChangeLogTag:Wed Jun 19 14:34:09 2002  Balachandran Natarajan  <bala@cs.wustl.edu>

--- TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Connection_Handler.cpp
+++ TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_Connection_Handler.cpp
@@ -282,7 +282,7 @@ TAO_SSLIOP_Connection_Handler::handle_close_i (void)
 int
 TAO_SSLIOP_Connection_Handler::resume_handler (void)
 {
-  return TAO_RESUMES_CONNECTION_HANDLER;
+  return ACE_Event_Handler::ACE_APPLICATION_RESUMES_HANDLER;
 }
 
 int
