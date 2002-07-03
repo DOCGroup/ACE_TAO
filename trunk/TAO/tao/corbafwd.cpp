b7e9d0a740835427fa32371129acb066b06e8204
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-07-03 15:50:53 +0000

    ChangeLogTag: Wed Jul  3 10:47:58 2002  Jeff Parsons <parsons@cs.wustl.edu>

--- trunk/TAO/tao/corbafwd.cpp
+++ trunk/TAO/tao/corbafwd.cpp
@@ -6,6 +6,10 @@
 # include "tao/corbafwd.i"
 #endif /* ! __ACE_INLINE__ */
 
+ACE_RCSID (tao, 
+           corbafwd, 
+           "$Id$")
+
 char *
 CORBA::string_dup (const char *str)
 {
@@ -20,11 +24,15 @@ CORBA::string_dup (const char *str)
   // This allocates an extra byte for the '\0';
   char * copy = CORBA::string_alloc (len);
 
+  // The memcpy() below assumes that the destination is a valid buffer.  
   if (copy == 0)
-    return 0;    // The below memcpy() assumes that the destination is
-                 // a valid buffer.
+    {
+      return 0;
+    }
 
-  ACE_OS::memcpy (copy, str, len + 1);
+  ACE_OS::memcpy (copy, 
+                  str, 
+                  len + 1);
   return copy;
 }
 
@@ -43,19 +51,58 @@ CORBA::wstring_dup (const WChar *const str)
 
   CORBA::WChar* retval = CORBA::wstring_alloc (ACE_OS::wslen (str));
 
+  // The wscpy() below assumes that the destination is a valid buffer.
   if (retval == 0)
-    return 0;    // The below wscpy() assumes that the destination is
-                 // a valid buffer.
+    {
+      return 0;
+    }
 
-  return ACE_OS::wscpy (retval, str);
+  return ACE_OS::wscpy (retval, 
+                        str);
 }
 
 // ****************************************************************
 
-ACE_RCSID(tao, corbafwd, "$Id$")
-
 TAO_NAMESPACE_TYPE (const CORBA::ServiceType)
 TAO_NAMESPACE_BEGIN (CORBA)
 TAO_NAMESPACE_DEFINE (const CORBA::ServiceType, Security, 1)
+TAO_NAMESPACE_END
+
+// ****************************************************************
+
+TAO_NAMESPACE_TYPE (CORBA::TypeCode_ptr)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (CORBA::TypeCode_ptr, _tc_Visibility, 0)
+TAO_NAMESPACE_END
+
+TAO_NAMESPACE_TYPE (const CORBA::Visibility)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (const CORBA::Visibility, PRIVATE_MEMBER, 0)
+TAO_NAMESPACE_END
+TAO_NAMESPACE_TYPE (const CORBA::Visibility)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (const CORBA::Visibility, PUBLIC_MEMBER, 1)
+TAO_NAMESPACE_END
+
+TAO_NAMESPACE_TYPE (CORBA::TypeCode_ptr)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (CORBA::TypeCode_ptr, _tc_ValueModifier, 0)
+TAO_NAMESPACE_END
+
+TAO_NAMESPACE_TYPE (const CORBA::ValueModifier)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (const CORBA::ValueModifier, VM_NONE, 0)
+TAO_NAMESPACE_END
+TAO_NAMESPACE_TYPE (const CORBA::ValueModifier)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (const CORBA::ValueModifier, VM_CUSTOM, 1)
+TAO_NAMESPACE_END
+TAO_NAMESPACE_TYPE (const CORBA::ValueModifier)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (const CORBA::ValueModifier, VM_ABSTRACT, 2)
+TAO_NAMESPACE_END
+TAO_NAMESPACE_TYPE (const CORBA::ValueModifier)
+TAO_NAMESPACE_BEGIN (CORBA)
+TAO_NAMESPACE_DEFINE (const CORBA::ValueModifier, VM_TRUNCATABLE, 3)
 TAO_NAMESPACE_END
 
