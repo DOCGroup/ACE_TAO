8ffd0832a60ed15f18b119281e64d69c9df9a945
mayur <mayur@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-07-03 19:03:59 +0000

    Wed Jul  3 12:01:35 2002  Mayur Deshpande  <mayur@ics.uci.edu>

---
+++ trunk/TAO/tests/AMH_Exceptions/client.cpp
@@ -0,0 +1,49 @@
+// $Id$
+
+#include "ace/pre.h"
+
+//#include "ace/Sched_Params.h"
+//#include "tao/Strategies/advanced_resource.h"
+
+#include "TestC.h"
+
+const char *ior = "file://test.ior";
+
+int
+main (int argc, char *argv[])
+{
+  ACE_TRY_NEW_ENV
+    {
+      CORBA::ORB_var orb =
+        CORBA::ORB_init (argc, argv, "" TAO_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      CORBA::Object_var object =
+        orb->string_to_object (ior TAO_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      Test::Roundtrip_var roundtrip =
+        Test::Roundtrip::_narrow (object.in () TAO_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      if (CORBA::is_nil (roundtrip.in ()))
+        {
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "Nil Test::Roundtrip reference <%s>\n",
+                             ior),
+                            1);
+        }
+
+      long long unsigned int time = 10;
+      roundtrip->test_method (time TAO_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "");
+      return 1;
+    }
+  ACE_ENDTRY;
+
+  return 0;
+}
