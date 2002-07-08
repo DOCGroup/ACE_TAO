4b792f489c93ee8b334f76f2c88c4ba6e6175cb2
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-07-08 20:32:03 +0000

    ChangeLogTag:Mon Jul  8 16:29:16 2002  Carlos O'Ryan  <coryan@atdesk.com>

---
+++ TAO/tao/Utils/RIR_Narrow.h
@@ -0,0 +1,56 @@
+/**
+ * @file RIR_Narrow.hpp
+ *
+ * $Id$
+ *
+ * @author Carlos O'Ryan <coryan@atdesk.com>
+ */
+#ifndef RIR_Narrow__h_
+#define RIR_Narrow__h_
+
+#include <tao/ORB.h>
+#include <tao/PortableInterceptorC.h>
+
+namespace TAO
+{
+namespace Utils
+{
+
+/**
+ * @class RIR_Narrow
+ *
+ * @brief Helper class to obtain an initial reference and narrow it
+ *        to the proper object reference.
+ */
+template<class T> class RIR_Narrow
+{
+public:
+  typedef typename T::_ptr_type _ptr_type;
+  typedef typename T::_var_type _var_type;
+
+  /// Use resolve_initial_references to find an object and then
+  /// narrow it.
+  static _ptr_type narrow (CORBA::ORB_ptr orb,
+                           char const * id,
+                           CORBA::Environment &ACE_TRY_ENV =
+                           TAO_default_environment ());
+
+  /// Use resolve_initial_references to find an object and then
+  /// narrow it.
+  static _ptr_type narrow (PortableInterceptor::ORBInitInfo_ptr orb,
+                           char const * id);
+
+private:
+  static _ptr_type narrow_object (CORBA::Object_ptr object,
+                                  CORBA::Environment &ACE_TRY_ENV =
+                                  TAO_default_environment ());
+};
+
+} // namespace Utils
+} // namespace TAO
+
+#if defined (ACE_TEMPLATES_REQUIRE_SOURCE)
+# include "RIR_Narrow.cpp"
+#endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
+
+#endif // RIR_Narrow__h_
