f08c343813520cf1f32f8df367a545fed2ed7beb
Steve Huston <shuston@riverace.com>
2002-07-12 20:06:45 +0000

    ChangeLogTag:Fri Jul 12 16:03:22 2002  Steve Huston  <shuston@riverace.com>

--- ace/Basic_Types.h
+++ ace/Basic_Types.h
@@ -617,7 +617,8 @@ typedef unsigned char ACE_Byte;
 # else /* ! BYTE_ORDER && ! __BYTE_ORDER */
   // We weren't explicitly told, so we have to figure it out . . .
 #   if defined (i386) || defined (__i386__) || defined (_M_IX86) || \
-     defined (vax) || defined (__alpha) || defined (__LITTLE_ENDIAN__) || defined (ARM)
+     defined (vax) || defined (__alpha) || defined (__LITTLE_ENDIAN__) ||\
+     defined (ARM) || defined (_M_IA64)
     // We know these are little endian.
 #     define ACE_LITTLE_ENDIAN 0x0123
 #     define ACE_BYTE_ORDER ACE_LITTLE_ENDIAN
