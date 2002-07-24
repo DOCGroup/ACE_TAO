efe665c913eb6ff17008400a491135be27f7ce72
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-07-24 18:53:13 +0000

    This commit was manufactured by cvs2svn to create branch 'allocators_lanes'.

---
+++ branches/allocators_lanes/TAO/tao/RTCORBA/RT_Endpoint_Selector_Factory.h
@@ -0,0 +1,70 @@
+// This may look like C, but it's really -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file   RT_Endpoint_Selector_Factory.h
+ *
+ *  $Id$
+ *
+ * Strategies for selecting profile/endpoint from an IOR for making an
+ * invocation.
+ *
+ *  @author Marina Spivak <marina@cs.wustl.edu>
+ */
+//=============================================================================
+
+
+#ifndef RT_ENDPOINT_SELECTOR_FACTORY_H
+#define RT_ENDPOINT_SELECTOR_FACTORY_H
+#include "ace/pre.h"
+
+#include "rtcorba_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/Endpoint_Selector_Factory.h"
+#include "ace/Service_Config.h"
+
+class TAO_RT_Invocation_Endpoint_Selector;
+
+// ****************************************************************
+/**
+ * @class RT_Endpoint_Selector_Factory
+ *
+ * @brief Factory for obtaining appropriate <Invocation_Selector>.
+ *
+ * Used by Invocation classes to intialize its endpoint selection
+ * strategy and state based on the effective policies.  Endpoint
+ * selection strategies are stateless objects - all the state they
+ * need is contained by Invocation.  Thus, rather than allocating an
+ * endpoint selection strategy object for each Invocation, the factory
+ * simply returns the appropriate one from the set preallocated in the
+ * ORB_Core.  One endpoint selection strategy object can be used by
+ * many invocations concurrently.
+ */
+class TAO_RTCORBA_Export RT_Endpoint_Selector_Factory
+  : public TAO_Endpoint_Selector_Factory
+{
+public:
+  /// Constructor.
+  RT_Endpoint_Selector_Factory (void);
+
+  /// Destructor.
+  virtual ~RT_Endpoint_Selector_Factory (void);
+
+  /// Get an Invocation's endpoint selection strategy and
+  /// initialize the endpoint selection state instance.
+  virtual TAO_Invocation_Endpoint_Selector *get_selector (TAO_GIOP_Invocation *invocation
+                                                          ACE_ENV_ARG_DECL);
+
+private:
+  TAO_RT_Invocation_Endpoint_Selector *rt_invocation_endpoint_selector_;
+};
+
+ACE_STATIC_SVC_DECLARE_EXPORT (TAO_RTCORBA, RT_Endpoint_Selector_Factory)
+ACE_FACTORY_DECLARE (TAO_RTCORBA, RT_Endpoint_Selector_Factory)
+
+#include "ace/post.h"
+#endif  /* RT_ENDPOINT_SELECTOR_FACTORY_H */
---
+++ branches/allocators_lanes/TAO/tao/RTCORBA/RT_PolicyFactory.cpp
@@ -0,0 +1,66 @@
+// -*- C++ -*-
+//
+// $Id$
+
+#include "RT_PolicyFactory.h"
+
+ACE_RCSID (tao, RT_PolicyFactory, "$Id$")
+
+#include "tao/PolicyC.h"
+#include "RT_Policy_i.h"
+
+CORBA::Policy_ptr
+TAO_RT_PolicyFactory::create_policy (
+    CORBA::PolicyType type,
+    const CORBA::Any &
+    ACE_ENV_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException,
+                   CORBA::PolicyError))
+{
+  CORBA::Policy_ptr policy = CORBA::Policy::_nil ();
+
+  if (type == RTCORBA::PRIORITY_MODEL_POLICY_TYPE)
+    {
+      ACE_NEW_THROW_EX (policy,
+                        TAO_PriorityModelPolicy,
+                        CORBA::NO_MEMORY (
+                          CORBA_SystemException::_tao_minor_code (
+                            TAO_DEFAULT_MINOR_CODE,
+                            ENOMEM),
+                          CORBA::COMPLETED_NO));
+      ACE_CHECK_RETURN (CORBA::Policy::_nil ());
+
+      return policy;
+    }
+
+  else if (type == RTCORBA::PRIORITY_BANDED_CONNECTION_POLICY_TYPE)
+    {
+      ACE_NEW_THROW_EX (policy,
+                        TAO_PriorityBandedConnectionPolicy,
+                        CORBA::NO_MEMORY (
+                          CORBA_SystemException::_tao_minor_code (
+                            TAO_DEFAULT_MINOR_CODE,
+                            ENOMEM),
+                          CORBA::COMPLETED_NO));
+      ACE_CHECK_RETURN (CORBA::Policy::_nil ());
+
+      return policy;
+    }
+
+  else if (type == RTCORBA::CLIENT_PROTOCOL_POLICY_TYPE)
+    {
+      ACE_NEW_THROW_EX (policy,
+                        TAO_ClientProtocolPolicy,
+                        CORBA::NO_MEMORY (
+                          CORBA_SystemException::_tao_minor_code (
+                            TAO_DEFAULT_MINOR_CODE,
+                            ENOMEM),
+                          CORBA::COMPLETED_NO));
+      ACE_CHECK_RETURN (CORBA::Policy::_nil ());
+
+      return policy;
+    }
+
+  ACE_THROW_RETURN (CORBA::PolicyError (CORBA::BAD_POLICY_TYPE),
+                    CORBA::Policy::_nil ());
+}
