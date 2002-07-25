86a4d39659069d3c5690b2ade83779f0ce1e0ff4
pgontla <pgontla@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-07-25 15:34:08 +0000

    ChangeLogTag: Thu Jul 25 08:05:47 2002  Priyanka Gontla  <pgontla@ece.uci.edu>

---
+++ TAO/orbsvcs/examples/PSS/Server_i.cpp
@@ -0,0 +1,22 @@
+// $Id$
+
+#include "Server_i.h"
+
+ACE_RCSID (PSS, Server_i, "$Id$")
+
+  Server_i::Server_i (void)
+{
+}
+
+Server_i::~Server_i (void)
+{
+}
+
+char *
+Server_i::get_status (ACE_ENV_SINGLE_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  char *status = "active";
+  
+  return CORBA::string_dup (status);
+}
