56b2ff3313391e7f9465500af3ab774e7019fbfb
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-04 21:48:23 +0000

    ChangeLogTag: Sun Aug  4 16:47:29 2002  Balachandran Natarajan  <bala@cs.wustl.edu>

--- tests/Framework_Component_DLL.cpp
+++ tests/Framework_Component_DLL.cpp
@@ -1,6 +1,7 @@
 // -*- C++ -*-
 #include "Framework_Component_DLL.h"
 #include "ace/Service_Config.h"
+#include "ace/Framework_Component_T.h"
 #include "ace/OS.h"
 
 ACE_RCSID (tests,
@@ -69,12 +70,14 @@ template class Server_T<2>;
 template class FWCT_DLL_Singleton_Adapter_T<Simple_Service>;
 template class ACE_DLL_Singleton_T< FWCT_DLL_Singleton_Adapter_T <Simple_Service>,
                                     ACE_SYNCH_MUTEX >;
+template class ACE_Framework_Component_T<ACE_DLL_Singleton_T<FWCT_DLL_Singleton_Adapter_T<Simple_Service>,
+                                         ACE_SYNCH_MUTEX> >;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate Server_T<1>
 #pragma instantiate Server_T<2>
 #pragma instantiate FWCT_DLL_Singleton_Adapter_T<Simple_Service>
 #pragma instantiate ACE_DLL_Singleton_T< FWCT_DLL_Singleton_Adapter_T <Simple_Service>,
                                          ACE_SYNCH_MUTEX >
+#pragma instanitiate ACE_Framework_Component_T<ACE_DLL_Singleton_T<FWCT_DLL_Singleton_Adapter_T<Simple_Service>,
+                                               ACE_SYNCH_MUTEX> >;
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
-
