135581693a53458f3ad938a5043427cad590eacd
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-07 18:00:18 +0000

    ChangeLogTag: Wed Aug  7 12:39:31 2002  Chad Elliott  <elliott_c@ociweb.com>

--- bin/MakeProjectCreator/modules/GNUWorkspaceCreator.pm
+++ bin/MakeProjectCreator/modules/GNUWorkspaceCreator.pm
@@ -51,23 +51,36 @@ sub write_comps {
 
   ## Print out the projet Makefile
   print $fh "include \$(ACE_ROOT)/include/makeinclude/macros.GNU$crlf" .
-            "TARGETS_NESTED := \$(TARGETS_NESTED:.nested=)$crlf" .
-            "MFILES = \\$crlf";
-  for(my $i = 0; $i <= $#list; $i++) {
-    print $fh "         $list[$i]";
-    if ($i != $#list) {
-      print $fh " \\";
+            "TARGETS_NESTED := \$(TARGETS_NESTED:.nested=)$crlf";
+
+  ## Only use the list if there is more than one project
+  if ($#list > 0) {
+    print $fh "MFILES = \\$crlf";
+    for(my $i = 0; $i <= $#list; $i++) {
+      print $fh "         $list[$i]";
+      if ($i != $#list) {
+        print $fh " \\";
+      }
+      print $fh $crlf;
     }
-    print $fh $crlf;
   }
 
   print $fh "$crlf" .
             "\$(TARGETS_NESTED):$crlf" .
-            "ifneq (Windows,\$(findstring Windows,\$(OS)))$crlf" .
-            "\t\@for file in \$(MFILES); do \\$crlf" .
-            "\t\$(MAKE) -f `basename \$\$file` -C `dirname \$\$file` \$(\@); \\$crlf" .
-            "\tdone$crlf" .
-            "else$crlf";
+            "ifneq (Windows,\$(findstring Windows,\$(OS)))$crlf";
+
+  ## If there is more than one project, use a for loop
+  if ($#list > 0) {
+    print $fh "\t\@for file in \$(MFILES); do \\$crlf" .
+              "\t\$(MAKE) -f `basename \$\$file` -C `dirname \$\$file` \$(\@); \\$crlf" .
+              "\tdone$crlf";
+  }
+  else {
+    ## Otherwise, just list the call to make without a for loop
+    print $fh "\t\@\$(MAKE) -f " . basename($list[0]) . " -C " . dirname($list[0]) . " \$(\@);$crlf";
+  }
+
+  print $fh "else$crlf";
   foreach my $project (@list) {
     print $fh "\t-\@cmd /c \"\$(MAKE) -f " . basename($project) . " -C " . dirname($project) . " \$(\@)\"$crlf";
   }
