46d12344f0f78ffb56aa063102475c985cb72f08
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-21 16:16:31 +0000

    ChangeLogTag: Wed Aug 21 11:15:11 2002  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/bin/MakeProjectCreator/modules/NMakeWorkspaceCreator.pm
+++ trunk/bin/MakeProjectCreator/modules/NMakeWorkspaceCreator.pm
@@ -45,16 +45,14 @@ sub pre_workspace {
 }
 
 
-sub write_comps {
+sub write_project_targets {
   my($self)     = shift;
   my($fh)       = shift;
-  my($projects) = $self->get_projects();
-  my($pjs)      = $self->get_project_info();
-  my(@list)     = $self->sort_dependencies($projects, $pjs);
+  my($target)   = shift;
+  my($list)     = shift;
   my($crlf)     = $self->crlf();
 
-  print $fh "ALL:$crlf";
-  foreach my $project (@list) {
+  foreach my $project (@$list) {
     my($dir)    = dirname($project);
     my($chdir)  = 0;
     my($back)   = 1;
@@ -71,12 +69,27 @@ sub write_comps {
       }
     }
 
-    ## These commands will work.  In practicality, only the
-    ## default configuration can be built at the top level.
     print $fh ($chdir ? "\tcd $dir$crlf" : "") .
-              "\t\$(MAKE) /f " . basename($project) . " CFG=\"\$(CFG)\"$crlf" .
+              "\t\$(MAKE) /f " . basename($project) . " $target$crlf" .
               ($chdir ? "\tcd " . ("../" x $back) . $crlf : "");
   }
+}
+
+
+sub write_comps {
+  my($self)     = shift;
+  my($fh)       = shift;
+  my($projects) = $self->get_projects();
+  my($pjs)      = $self->get_project_info();
+  my(@list)     = $self->sort_dependencies($projects, $pjs);
+  my($crlf)     = $self->crlf();
+
+  print $fh "ALL:$crlf";
+  $self->write_project_targets($fh, "CFG=\"\$(CFG)\"", \@list);
+
+  print $fh "$crlf" .
+            "CLEAN\tREALCLEAN:$crlf";
+  $self->write_project_targets($fh, "CLEAN", \@list);
 }
 
 
