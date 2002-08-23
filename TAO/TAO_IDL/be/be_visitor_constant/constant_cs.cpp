bac2d01866ab5f11c250985b7bd8b04c4dad51c2
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-23 22:25:05 +0000

    ChangeLogTag: Fri Aug 23 17:21:23 2002  Jeff Parsons <parsons@cs.wustl.edu>

--- TAO/TAO_IDL/be/be_visitor_constant/constant_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_constant/constant_cs.cpp
@@ -39,92 +39,41 @@ be_visitor_constant_cs::~be_visitor_constant_cs (void)
 
 // visit the Constant_cs node and its scope
 int
-be_visitor_constant_cs::visit_constant (be_constant * /* node */)
+be_visitor_constant_cs::visit_constant (be_constant *node)
 {
-/*
+
+  if (node->cli_stub_gen () 
+      || node->imported ())
+    {
+      return 0;
+    }
+
   TAO_OutStream *os = this->ctx_->stream ();
 
-  if (!node->cli_stub_gen ()
-      && !node->imported ()
-      && !be_global->gen_inline_constants ())
+  if (node->is_nested ()
+      && node->defined_in ()->scope_node_type () != AST_Decl::NT_module)
     {
-      if (node->is_nested ())
+      // For those constants not defined in the outermost scope,
+      // or in a module, they get assigned to their values in the source file.
+      os->indent ();
+
+      *os << "const ";
+      
+      if (node->et () == AST_Expression::EV_enum)
         {
-          if (node->defined_in ()->scope_node_type () == AST_Decl::NT_module)
-            {
-              *os << "TAO_NAMESPACE_TYPE (const ";
-
-              if (node->et () == AST_Expression::EV_enum)
-                {
-                  *os << node->enum_full_name ();
-                }
-              else
-                {
-                  *os << node->exprtype_to_string ();
-                }
-
-              *os << ")" << be_nl;
-              be_module *module =
-                be_module::narrow_from_scope (node->defined_in ());
-
-              if (!module || this->gen_nested_namespace_begin (module) == -1)
-                {
-                  ACE_ERROR_RETURN ((LM_ERROR,
-                                     "be_visitor_constant_cs::"
-                                     "visit_constant - "
-                                     "Error parsing nested name\n"),
-                                    -1);
-                }
-
-              *os << "TAO_NAMESPACE_DEFINE (const ";
-
-              if (node->et () == AST_Expression::EV_enum)
-                {
-                  *os << node->enum_full_name ();
-                }
-              else
-                {
-                  *os << node->exprtype_to_string ();
-                }
-
-              *os << ", " << node->local_name () << ", "
-                  << node->constant_value () << ")" << be_nl;
-
-              if (this->gen_nested_namespace_end (module) == -1)
-                {
-                  ACE_ERROR_RETURN ((LM_ERROR,
-                                     "be_visitor_constant_cs::"
-                                     "visit_constant - "
-                                     "Error parsing nested name\n"),
-                                    -1);
-                }
-            }
-          else
-            {
-              // For those constants not defined in the outer most scope,
-              // they get assigned to their values in the impl file.
-              os->indent ();
-
-              *os << "const ";
-
-              if (node->et () == AST_Expression::EV_enum)
-                {
-                  *os << node->enum_full_name ();
-                }
-              else
-                {
-                  *os << node->exprtype_to_string ();
-                }
-
-              *os << " "
-                  << node->name () << " = " << node->constant_value ()
-                  << ";\n\n";
-            }
+          *os << node->enum_full_name ();
         }
-
-      node->cli_stub_gen (I_TRUE);
+      else
+        {
+          *os << node->exprtype_to_string ();
+        }
+       
+      *os << " "
+          << node->name () << " = " << node->constant_value ()
+          << ";\n\n";
     }
-*/
+
+  node->cli_stub_gen (I_TRUE);
   return 0;
 }
 
