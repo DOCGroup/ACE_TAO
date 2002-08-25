ab6d425d4c6a0ca072d152444fe04a24d995212d
chris <chris@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-25 04:27:09 +0000

    ChangeLogTag:Sat Aug 24 22:47:22 2002  Christopher Kohlhoff  <chris@kohlhoff.com>

--- trunk/bin/MakeProjectCreator/modules/BorlandWorkspaceCreator.pm
+++ trunk/bin/MakeProjectCreator/modules/BorlandWorkspaceCreator.pm
@@ -49,32 +49,41 @@ sub write_comps {
   my(@list)     = $self->sort_dependencies($projects, $pjs);
   my($crlf)     = $self->crlf();
 
-  print $fh "!include <\$(ACE_ROOT)\include\makeinclude\make_flags.bor>" .
-            "$crlf$crlf" .
-            "all:$crlf";
-  foreach my $project (@list) {
-    my($dir)    = dirname($project);
-    my($chdir)  = 0;
-    my($back)   = 1;
+  print $fh "!include <\$(ACE_ROOT)\\include\\makeinclude\\make_flags.bor>$crlf";
 
-    ## If the directory isn't "." then we need
-    ## to figure out how to get back to our starting point
-    if ($dir ne ".") {
-      $chdir = 1;
-      my($length) = length($dir);
-      for(my $i = 0; $i < $length; $i++) {
-        if (substr($dir, $i, 1) eq "/") {
-          $back++;
+  foreach my $target ("all", "clean", "realclean", "install") {
+    print $fh "$crlf" .
+              "$target\:$crlf";
+    foreach my $project (@list) {
+      my($dir)    = dirname($project);
+      my($chdir)  = 0;
+      my($back)   = 1;
+
+      ## If the directory isn't "." then we need
+      ## to figure out how to get back to our starting point
+      if ($dir ne ".") {
+        $chdir = 1;
+        my($length) = length($dir);
+        for(my $i = 0; $i < $length; $i++) {
+          if (substr($dir, $i, 1) eq "/") {
+            $back++;
+          }
         }
-      }  
-    }  
-     
-    ## These commands will work.  In practicality, only the
-    ## default configuration can be built at the top level.
-    print $fh ($chdir ? "\t\@cd $dir$crlf" : "") .
-              "\t\$(MAKE) -\$(MAKEFLAGS) \$(MAKE_FLAGS) -f " . basename($project) . $crlf .
-              ($chdir ? "\t\@cd " . ("../" x $back) . $crlf : "");
+      }
+      
+      ## These commands will work.  In practicality, only the
+      ## default configuration can be built at the top level.
+      print $fh ($chdir ? "\t\@cd $dir$crlf" : "") .
+                "\t\$(MAKE) -\$(MAKEFLAGS) \$(MAKE_FLAGS) -f " . basename($project) . " $target$crlf" .
+                ($chdir ? "\t\@cd " . ("../" x $back) . $crlf : "");
+    }
   }
+
+  # Generate a convenient rule for regenerating the workspace.
+  my($cmd) = "perl " . $0 . " " . join(" ", @ARGV);
+  print $fh "$crlf" .
+            "regenerate:$crlf" .
+            "\t$cmd$crlf";
 }
 
 
