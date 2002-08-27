f7a739ec24fb80f36f6d5befec64e22ba89c9026
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-27 22:57:11 +0000

    ChangeLogTag:Tue Aug 27 15:56:10 2002  Ossama Othman  <ossama@uci.edu>

---
+++ trunk/ace/config-sunos5.8-sunc++-5.4.h
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// $Id$
+
+// The following configuration file is designed to work for SunOS 5.8
+// (Solaris 8) platforms using the SunC++ SPRO 5.4 compiler.
+
+#ifndef ACE_CONFIG_H
+
+#define ACE_ONLY_LATEST_AND_GREATEST
+#define ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB 1
+#define ACE_HAS_REACTOR_NOTIFICATION_QUEUE 1
+#include "ace/config-sunos5.7.h"
+
+#undef ACE_HAS_TEMPLATE_TYPEDEFS
