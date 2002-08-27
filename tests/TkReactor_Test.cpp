ad4bf4f98197e998461e58f9d2e8f8042f737ba4
Ossama Othman <ossama-othman@users.noreply.github.com>
2002-08-27 00:16:58 +0000

    ChangeLogTag:Mon Aug 26 17:14:20 2002  Ossama Othman  <ossama@uci.edu>

--- tests/TkReactor_Test.cpp
+++ tests/TkReactor_Test.cpp
@@ -1,6 +1,3 @@
-/* -*- C++ -*- */
-// $Id$
-
 // ============================================================================
 //
 // = LIBRARY
@@ -27,7 +24,9 @@
 
 #include "test_config.h"
 
-ACE_RCSID(tests, TkReactor_Test, "$Id$")
+ACE_RCSID (tests,
+           TkReactor_Test,
+           "$Id$")
 
 #if !defined (ACE_HAS_TK)
 
@@ -157,7 +156,7 @@ inc_count (ClientData client_data, Tcl_Interp *interp,int, char **)
            count1++,
            count2,
            count3);
-  char command [BUFSIZ];
+
   //  sprintf (command,"set %s %s",(char *)client_data,new_string);
   // eval (command);
   char *varValue = Tcl_SetVar (interp,(char *)client_data,new_string,TCL_LEAVE_ERR_MSG);
@@ -181,7 +180,6 @@ inc_tmo (ClientData client_data)
            count2++,
            count3);
 
-  char command [BUFSIZ];
   //  sprintf (command,"set %s %s",(char *)client_data,new_string);
   //  eval (command);
   char *varValue = Tcl_SetVar (tcl_interp,(char *)client_data,new_string,TCL_LEAVE_ERR_MSG);
@@ -205,7 +203,7 @@ public:
              count1,
              count2,
              count3++);
-    char command [BUFSIZ];
+
     //    sprintf (command,"set %s %s",(char *)arg,new_string);
     //    eval (command);
     char *varValue = Tcl_SetVar (tcl_interp,(char *)arg,new_string,TCL_LEAVE_ERR_MSG);
@@ -265,7 +263,7 @@ init (Tcl_Interp *interp)
 }
 
 int
-ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACE_START_TEST (ACE_TEXT ("TkReactor_Test"));
 
@@ -283,7 +281,8 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
       ACE_ERROR_RETURN ((LM_ERROR, "Tk_Reactor_Test: %s\n", tcl_interp->result),1);
     }
 
-  if (Tcl_Eval (tcl_interp, "source TkReactor_Test.tcl") != TCL_OK) {
+  char tcl_cmd[] = "source TkReactor_Test.tcl"
+  if (Tcl_Eval (tcl_interp, tcl_cmd) != TCL_OK) {
     cerr << "error: " << tcl_interp->result << "\n";
     exit (1);
   }
