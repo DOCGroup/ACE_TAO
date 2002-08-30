667318ce1328b27eebf6060725939a501a80a446
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-30 01:18:18 +0000

    ChangeLogTag: Thu Aug 29 21:08:59 2002  Irfan Pyarali  <irfan@oomworks.com>

--- TAO/tao/RTCORBA/RT_PolicyFactory.cpp
+++ TAO/tao/RTCORBA/RT_PolicyFactory.cpp
@@ -12,7 +12,7 @@ ACE_RCSID (tao, RT_PolicyFactory, "$Id$")
 CORBA::Policy_ptr
 TAO_RT_PolicyFactory::create_policy (
     CORBA::PolicyType type,
-    const CORBA::Any &
+    const CORBA::Any &value
     ACE_ENV_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    CORBA::PolicyError))
@@ -20,46 +20,28 @@ TAO_RT_PolicyFactory::create_policy (
   CORBA::Policy_ptr policy = CORBA::Policy::_nil ();
 
   if (type == RTCORBA::PRIORITY_MODEL_POLICY_TYPE)
-    {
-      ACE_NEW_THROW_EX (policy,
-                        TAO_PriorityModelPolicy,
-                        CORBA::NO_MEMORY (
-                          CORBA_SystemException::_tao_minor_code (
-                            TAO_DEFAULT_MINOR_CODE,
-                            ENOMEM),
-                          CORBA::COMPLETED_NO));
-      ACE_CHECK_RETURN (CORBA::Policy::_nil ());
+    return TAO_PriorityModelPolicy::create (value
+                                            ACE_ENV_ARG_PARAMETER);
 
-      return policy;
-    }
+  if (type == RTCORBA::THREADPOOL_POLICY_TYPE)
+    return TAO_ThreadpoolPolicy::create (value
+                                         ACE_ENV_ARG_PARAMETER);
 
-  else if (type == RTCORBA::PRIORITY_BANDED_CONNECTION_POLICY_TYPE)
-    {
-      ACE_NEW_THROW_EX (policy,
-                        TAO_PriorityBandedConnectionPolicy,
-                        CORBA::NO_MEMORY (
-                          CORBA_SystemException::_tao_minor_code (
-                            TAO_DEFAULT_MINOR_CODE,
-                            ENOMEM),
-                          CORBA::COMPLETED_NO));
-      ACE_CHECK_RETURN (CORBA::Policy::_nil ());
+  if (type == RTCORBA::SERVER_PROTOCOL_POLICY_TYPE)
+    return TAO_ServerProtocolPolicy::create (value
+                                             ACE_ENV_ARG_PARAMETER);
 
-      return policy;
-    }
+  if (type == RTCORBA::CLIENT_PROTOCOL_POLICY_TYPE)
+    return TAO_ClientProtocolPolicy::create (value
+                                             ACE_ENV_ARG_PARAMETER);
 
-  else if (type == RTCORBA::CLIENT_PROTOCOL_POLICY_TYPE)
-    {
-      ACE_NEW_THROW_EX (policy,
-                        TAO_ClientProtocolPolicy,
-                        CORBA::NO_MEMORY (
-                          CORBA_SystemException::_tao_minor_code (
-                            TAO_DEFAULT_MINOR_CODE,
-                            ENOMEM),
-                          CORBA::COMPLETED_NO));
-      ACE_CHECK_RETURN (CORBA::Policy::_nil ());
+  if (type == RTCORBA::PRIVATE_CONNECTION_POLICY_TYPE)
+    return TAO_PrivateConnectionPolicy::create (value
+                                                ACE_ENV_ARG_PARAMETER);
 
-      return policy;
-    }
+  if (type == RTCORBA::PRIORITY_BANDED_CONNECTION_POLICY_TYPE)
+    return TAO_PriorityBandedConnectionPolicy::create (value
+                                                       ACE_ENV_ARG_PARAMETER);
 
   ACE_THROW_RETURN (CORBA::PolicyError (CORBA::BAD_POLICY_TYPE),
                     CORBA::Policy::_nil ());
