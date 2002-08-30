564ef906bd19909bb082ee649444f99ca365965a
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-08-30 18:41:34 +0000

    ChangeLogTag: Fri Aug 30 13:36:24 2002  Jeff Parsons <parsons@cs.wustl.edu>

--- TAO/TAO_IDL/be/be_visitor_interface/any_op_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_interface/any_op_cs.cpp
@@ -1,4 +1,4 @@
-// $id: any_op_cs.cpp,v 1.12 1998/10/30 19:46:55 levine Exp $
+// $Id$
 
 // ============================================================================
 //
@@ -26,8 +26,9 @@ ACE_RCSID (be_visitor_interface,
 // stubs file
 // ***************************************************************************
 
-be_visitor_interface_any_op_cs::be_visitor_interface_any_op_cs
-(be_visitor_context *ctx)
+be_visitor_interface_any_op_cs::be_visitor_interface_any_op_cs (
+    be_visitor_context *ctx
+  )
   : be_visitor_interface (ctx)
 {
 }
@@ -50,85 +51,155 @@ be_visitor_interface_any_op_cs::visit_interface (be_interface *node)
   *os << be_nl << "// TAO_IDL - Generated from" << be_nl
       << "// " << __FILE__ << ":" << __LINE__ << be_nl << be_nl;
 
-  // Generate the Any <<= and >>= operator declarations
-  // Any <<= and >>= operators.
-  *os << "// Copying version." << be_nl
-      << "void" << be_nl
-      << "operator<<= (" << be_idt << be_idt_nl
-      << "CORBA::Any &_tao_any," << be_nl
-      << node->full_name () << "_ptr _tao_elem" << be_uidt_nl
-      << ")" << be_uidt_nl
-      << "{" << be_idt_nl
-      << node->full_name () << "_ptr _tao_objptr =" << be_idt_nl
-      << node->full_name () << "::_duplicate (_tao_elem);" << be_uidt_nl
-      << "_tao_any <<= &_tao_objptr;" << be_uidt_nl
-      << "}" << be_nl << be_nl;
+  if (node->is_local ())
+    {
+      *os << "// Copying version." << be_nl
+          << "void" << be_nl
+          << "operator<<= (" << be_idt << be_idt_nl
+          << "CORBA::Any &_tao_any," << be_nl
+          << node->full_name () << "_ptr _tao_elem" << be_uidt_nl
+          << ")" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "_tao_any._tao_replace (" << be_idt << be_idt_nl
+          << node->tc_name () << "," << be_nl
+          << "1," << be_nl
+          << node->name () << "::_duplicate (_tao_elem)," << be_nl
+          << node->name () << "::_tao_any_destructor" << be_uidt_nl
+          << ");" << be_uidt_nl << be_nl
+          << "_tao_any.contains_local (1);" << be_uidt_nl
+          << "}" << be_nl << be_nl;
 
-  *os << "// Non-copying version." << be_nl
-      << "void" << be_nl
-      << "operator<<= (" << be_idt << be_idt_nl
-      << "CORBA::Any &_tao_any," << be_nl
-      << node->full_name () << "_ptr *_tao_elem" << be_uidt_nl
-      << ")" << be_uidt_nl
-      << "{" << be_idt_nl
-      << "TAO_OutputCDR stream;" << be_nl << be_nl
-      << "if (stream << *_tao_elem)" << be_idt_nl
-      << "{" << be_idt_nl
-      << "_tao_any._tao_replace (" << be_idt << be_idt_nl
-      << node->tc_name () << ", " << be_nl
-      << "TAO_ENCAP_BYTE_ORDER," << be_nl
-      << "stream.begin ()," << be_nl
-      << "1," << be_nl
-      << "*_tao_elem," << be_nl
-      << node->name () << "::_tao_any_destructor" << be_uidt_nl
-      << ");" << be_uidt << be_uidt_nl
-      << "}" << be_uidt << be_uidt_nl
-      << "}" << be_nl << be_nl;
+      *os << "// Non-copying version." << be_nl
+          << "void" << be_nl
+          << "operator<<= (" << be_idt << be_idt_nl
+          << "CORBA::Any &_tao_any," << be_nl
+          << node->full_name () << "_ptr *_tao_elem" << be_uidt_nl
+          << ")" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "_tao_any._tao_replace (" << be_idt << be_idt_nl
+          << node->tc_name () << "," << be_nl
+          << "1," << be_nl
+          << "*_tao_elem," << be_nl
+          << node->name () << "::_tao_any_destructor" << be_uidt_nl
+          << ");" << be_uidt_nl << be_nl
+          << "_tao_any.contains_local (1);" << be_uidt_nl
+          << "}" << be_nl << be_nl;
 
-  *os << "CORBA::Boolean" << be_nl
-      << "operator>>= (" << be_idt << be_idt_nl
-      << "const CORBA::Any &_tao_any," << be_nl
-      << node->full_name () << "_ptr &_tao_elem" << be_uidt_nl
-      << ")" << be_uidt_nl
-      << "{" << be_idt_nl
-      << "ACE_TRY_NEW_ENV" << be_nl
-      << "{" << be_idt_nl
-      << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
-      << "CORBA::TypeCode_var type = _tao_any.type ();" << be_nl << be_nl
-      << "CORBA::Boolean result =" << be_idt_nl
-      << "type->equivalent (" << be_idt << be_idt_nl
-      << node->tc_name () << be_nl
-      << "ACE_ENV_ARG_PARAMETER" << be_uidt_nl
-      << ");" << be_uidt << be_uidt_nl
-      << "ACE_TRY_CHECK;" << be_nl << be_nl
-      << "if (!result)" << be_idt_nl
-      << "{" << be_idt_nl
-      << "return 0; // not equivalent" << be_uidt_nl 
-      << "}" << be_uidt_nl << be_nl
-      << "TAO_InputCDR stream (" << be_idt << be_idt_nl
-      << "_tao_any._tao_get_cdr ()," << be_nl
-      << "_tao_any._tao_byte_order ()" << be_uidt_nl
-      << ");" << be_uidt_nl << be_nl
-      << "if (stream >> _tao_elem)" << be_idt_nl
-      << "{" << be_idt_nl
-      << "((CORBA::Any *)&_tao_any)->_tao_replace (" << be_idt << be_idt_nl
-      << node->tc_name () << "," << be_nl
-      << "1," << be_nl
-      << "_tao_elem," << be_nl
-      << node->name () << "::_tao_any_destructor" << be_uidt_nl
-      << ");" << be_uidt_nl << be_nl
-      << "return 1;" << be_uidt_nl
-      << "}" << be_uidt << be_uidt_nl
-      << "}" << be_nl
-      << "ACE_CATCHANY" << be_nl
-      << "{" << be_idt_nl
-      << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
-      << "return 0;" << be_uidt_nl
-      << "}" << be_nl
-      << "ACE_ENDTRY;" << be_nl
-      << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
-      << "return 0;" << be_uidt_nl
-      << "}" << be_nl << be_nl;
+      *os << "CORBA::Boolean" << be_nl
+          << "operator>>= (" << be_idt << be_idt_nl
+          << "const CORBA::Any &_tao_any," << be_nl
+          << node->full_name () << "_ptr &_tao_elem" << be_uidt_nl
+          << ")" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "ACE_TRY_NEW_ENV" << be_nl
+          << "{" << be_idt_nl
+          << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
+          << "CORBA::TypeCode_var type = _tao_any.type ();" << be_nl << be_nl
+          << "CORBA::Boolean result =" << be_idt_nl
+          << "type->equivalent (" << be_idt << be_idt_nl
+          << node->tc_name () << be_nl
+          << "ACE_ENV_ARG_PARAMETER" << be_uidt_nl
+          << ");" << be_uidt << be_uidt_nl
+          << "ACE_TRY_CHECK;" << be_nl << be_nl
+          << "if (!result)" << be_idt_nl
+          << "{" << be_idt_nl
+          << "return 0; // not equivalent" << be_uidt_nl 
+          << "}" << be_uidt_nl << be_nl
+          << "_tao_elem =" << be_idt_nl
+          << "ACE_reinterpret_cast (" << be_idt << be_idt_nl
+          << node->name () << "_ptr," << be_nl
+          << "ACE_const_cast (void *, _tao_any.value ())" << be_uidt_nl
+          << ");" << be_uidt_nl << be_uidt_nl
+          << "return 1;" << be_uidt_nl
+          << "}" << be_nl
+          << "ACE_CATCHANY" << be_nl
+          << "{" << be_nl
+          << "}" << be_nl
+          << "ACE_ENDTRY;" << be_nl << be_nl
+          << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
+          << "return 0;" << be_uidt_nl
+          << "}" << be_nl << be_nl;
+    }
+  else
+    {
+      *os << "// Copying version." << be_nl
+          << "void" << be_nl
+          << "operator<<= (" << be_idt << be_idt_nl
+          << "CORBA::Any &_tao_any," << be_nl
+          << node->full_name () << "_ptr _tao_elem" << be_uidt_nl
+          << ")" << be_uidt_nl
+          << "{" << be_idt_nl
+          << node->full_name () << "_ptr _tao_objptr =" << be_idt_nl
+          << node->full_name () << "::_duplicate (_tao_elem);" << be_uidt_nl
+          << "_tao_any <<= &_tao_objptr;" << be_uidt_nl
+          << "}" << be_nl << be_nl;
+
+      *os << "// Non-copying version." << be_nl
+          << "void" << be_nl
+          << "operator<<= (" << be_idt << be_idt_nl
+          << "CORBA::Any &_tao_any," << be_nl
+          << node->full_name () << "_ptr *_tao_elem" << be_uidt_nl
+          << ")" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "TAO_OutputCDR stream;" << be_nl << be_nl
+          << "if (stream << *_tao_elem)" << be_idt_nl
+          << "{" << be_idt_nl
+          << "_tao_any._tao_replace (" << be_idt << be_idt_nl
+          << node->tc_name () << ", " << be_nl
+          << "TAO_ENCAP_BYTE_ORDER," << be_nl
+          << "stream.begin ()," << be_nl
+          << "1," << be_nl
+          << "*_tao_elem," << be_nl
+          << node->name () << "::_tao_any_destructor" << be_uidt_nl
+          << ");" << be_uidt << be_uidt_nl
+          << "}" << be_uidt << be_uidt_nl
+          << "}" << be_nl << be_nl;
+
+      *os << "CORBA::Boolean" << be_nl
+          << "operator>>= (" << be_idt << be_idt_nl
+          << "const CORBA::Any &_tao_any," << be_nl
+          << node->full_name () << "_ptr &_tao_elem" << be_uidt_nl
+          << ")" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "ACE_TRY_NEW_ENV" << be_nl
+          << "{" << be_idt_nl
+          << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
+          << "CORBA::TypeCode_var type = _tao_any.type ();" << be_nl << be_nl
+          << "CORBA::Boolean result =" << be_idt_nl
+          << "type->equivalent (" << be_idt << be_idt_nl
+          << node->tc_name () << be_nl
+          << "ACE_ENV_ARG_PARAMETER" << be_uidt_nl
+          << ");" << be_uidt << be_uidt_nl
+          << "ACE_TRY_CHECK;" << be_nl << be_nl
+          << "if (!result)" << be_idt_nl
+          << "{" << be_idt_nl
+          << "return 0; // not equivalent" << be_uidt_nl 
+          << "}" << be_uidt_nl << be_nl
+          << "TAO_InputCDR stream (" << be_idt << be_idt_nl
+          << "_tao_any._tao_get_cdr ()," << be_nl
+          << "_tao_any._tao_byte_order ()" << be_uidt_nl
+          << ");" << be_uidt_nl << be_nl
+          << "if (stream >> _tao_elem)" << be_idt_nl
+          << "{" << be_idt_nl
+          << "((CORBA::Any *)&_tao_any)->_tao_replace (" << be_idt << be_idt_nl
+          << node->tc_name () << "," << be_nl
+          << "1," << be_nl
+          << "_tao_elem," << be_nl
+          << node->name () << "::_tao_any_destructor" << be_uidt_nl
+          << ");" << be_uidt_nl << be_nl
+          << "return 1;" << be_uidt_nl
+          << "}" << be_uidt << be_uidt_nl
+          << "}" << be_nl
+          << "ACE_CATCHANY" << be_nl
+          << "{" << be_idt_nl
+          << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
+          << "return 0;" << be_uidt_nl
+          << "}" << be_nl
+          << "ACE_ENDTRY;" << be_nl << be_nl
+          << "_tao_elem = " << node->full_name () << "::_nil ();" << be_nl
+          << "return 0;" << be_uidt_nl
+          << "}" << be_nl << be_nl;
+    }
 
   *os << "#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)  || \\"
       << be_idt_nl
