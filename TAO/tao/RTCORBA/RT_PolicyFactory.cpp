23dd130f6d9add5fb816bcaf6bcda55b0d40a562
Ossama Othman <ossama-othman@users.noreply.github.com>
2002-09-03 00:14:56 +0000

    ChangeLogTag:Mon Sep  2 16:59:08 2002  Ossama Othman  <ossama@uci.edu>

--- TAO/tao/RTCORBA/RT_PolicyFactory.cpp
+++ TAO/tao/RTCORBA/RT_PolicyFactory.cpp
@@ -1,13 +1,14 @@
-// -*- C++ -*-
-//
-// $Id$
-
 #include "RT_PolicyFactory.h"
 
-ACE_RCSID (tao, RT_PolicyFactory, "$Id$")
+
+ACE_RCSID (RTCORBA,
+           RT_PolicyFactory,
+           "$Id$")
+
 
 #include "tao/PolicyC.h"
 #include "RT_Policy_i.h"
+
 
 CORBA::Policy_ptr
 TAO_RT_PolicyFactory::create_policy (
@@ -17,8 +18,6 @@ TAO_RT_PolicyFactory::create_policy (
   ACE_THROW_SPEC ((CORBA::SystemException,
                    CORBA::PolicyError))
 {
-  CORBA::Policy_ptr policy = CORBA::Policy::_nil ();
-
   if (type == RTCORBA::PRIORITY_MODEL_POLICY_TYPE)
     return TAO_PriorityModelPolicy::create (value
                                             ACE_ENV_ARG_PARAMETER);
