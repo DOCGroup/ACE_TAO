e58f60df44437d093c03c814ae3a3e25fe11f879
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-09-17 22:27:25 +0000

    ChangeLogTag:Tue Sep 17 17:26:40 2002  Ossama Othman  <ossama@uci.edu>

--- trunk/ace/CDR_Base.h
+++ trunk/ace/CDR_Base.h
@@ -158,13 +158,13 @@ public:
   // Definitions of the	IDL basic types, for use in the	CDR
   // classes. The cleanest way to avoid	complaints from	all compilers
   // is	to define them all.
-  #if defined (CHORUS) && defined (ghs)	&& !defined (__STANDARD_CXX)
+#  if defined (CHORUS) && defined (ghs)	&& !defined (__STANDARD_CXX)
     // This is non-compliant, but a nasty bout with
     // Green Hills C++68000 1.8.8 forces us into it.
     typedef unsigned long Boolean;
-  #else	 /* ! (CHORUS && ghs 1.8.8) */
+#  else	 /* ! (CHORUS && ghs 1.8.8) */
     typedef u_char Boolean;
-  #endif /* ! (CHORUS && ghs 1.8.8) */
+#  endif /* ! (CHORUS && ghs 1.8.8) */
 
   typedef u_char Octet;
   typedef char Char;
@@ -175,24 +175,24 @@ public:
   typedef ACE_UINT32 ULong;
   typedef ACE_UINT64 ULongLong;
 
-  # if	  (defined (_MSC_VER) && (_MSC_VER >= 900)) \
+#   if	  (defined (_MSC_VER) && (_MSC_VER >= 900)) \
 	  || (defined (__BORLANDC__) &&	(__BORLANDC__ >= 0x530))
       typedef __int64 LongLong;
-  # elif ACE_SIZEOF_LONG == 8 && !defined(_CRAYMPP)
+#   elif ACE_SIZEOF_LONG == 8 && !defined(_CRAYMPP)
       typedef long LongLong;
-  # elif ACE_SIZEOF_LONG_LONG == 8 && !defined (ACE_LACKS_LONGLONG_T)
-  #   if defined (sun) && !defined (ACE_LACKS_U_LONGLONG_T) && !defined	(__KCC)
+#   elif ACE_SIZEOF_LONG_LONG == 8 && !defined (ACE_LACKS_LONGLONG_T)
+#     if defined (sun) && !defined (ACE_LACKS_U_LONGLONG_T) && !defined	(__KCC)
 	      // sun #defines	u_longlong_t, maybe other platforms do also.
 	      // Use it, at least with g++, so that its -pedantic doesn't
 	      // complain about no ANSI C++ long long.
 	      typedef	longlong_t LongLong;
-  #   else
+#     else
 	      // LynxOS 2.5.0	and Linux don't	have u_longlong_t.
 	      typedef	long long LongLong;
-  #   endif /* sun */
-  # else  /* no	native 64 bit integer type */
-  #   define NONNATIVE_LONGLONG
-  #   if defined (ACE_BIG_ENDIAN)
+#     endif /* sun */
+#   else  /* no	native 64 bit integer type */
+#     define NONNATIVE_LONGLONG
+#     if defined (ACE_BIG_ENDIAN)
 	      struct ACE_Export LongLong 
         { 
           ACE_CDR::Long	h;
@@ -200,7 +200,7 @@ public:
           int operator== (const LongLong &rhs) const;
           int operator!= (const LongLong &rhs) const;
         };
-  #   else
+#     else
 	      struct ACE_Export LongLong 
         { 
           ACE_CDR::Long	l;
@@ -208,62 +208,62 @@ public:
           int operator== (const LongLong &rhs) const;
           int operator!= (const LongLong &rhs) const;
         };
-  #   endif /* ! ACE_BIG_ENDIAN	*/
-  # endif /* no	native 64 bit integer type */
+#     endif /* ! ACE_BIG_ENDIAN	*/
+#   endif /* no	native 64 bit integer type */
 
-  # if defined (NONNATIVE_LONGLONG)
-  #   define ACE_CDR_LONGLONG_INITIALIZER {0,0}
-  # else
-  #   define ACE_CDR_LONGLONG_INITIALIZER 0
-  # endif /* NONNATIVE_LONGLONG */
+#   if defined (NONNATIVE_LONGLONG)
+#     define ACE_CDR_LONGLONG_INITIALIZER {0,0}
+#   else
+#     define ACE_CDR_LONGLONG_INITIALIZER 0
+#   endif /* NONNATIVE_LONGLONG */
 
-  # if ACE_SIZEOF_FLOAT	== 4
+#   if ACE_SIZEOF_FLOAT	== 4
       typedef float Float;
-  # else  /* ACE_SIZEOF_FLOAT != 4 */
+#   else  /* ACE_SIZEOF_FLOAT != 4 */
       struct Float
       {
-  #	if ACE_SIZEOF_INT == 4
+#  	if ACE_SIZEOF_INT == 4
 	  // Use u_int to get word alignment.
 	  u_int	f;
-  #	else  /* ACE_SIZEOF_INT	!= 4 */
+#  	else  /* ACE_SIZEOF_INT	!= 4 */
 	  // Applications will probably	have trouble with this.
 	  char f[4];
-  #	  if defined(_UNICOS) && !defined(_CRAYMPP)
+#  	  if defined(_UNICOS) && !defined(_CRAYMPP)
 	    Float (void);
 	    Float (const float &init);
 	    Float & operator= (const float &rhs);
 	    int	operator!= (const Float	&rhs) const;
-  #	  endif	/* _UNICOS */
-  #	endif /* ACE_SIZEOF_INT	!= 4 */
+#  	  endif	/* _UNICOS */
+#  	endif /* ACE_SIZEOF_INT	!= 4 */
       };
-  # endif /* ACE_SIZEOF_FLOAT != 4 */
+#   endif /* ACE_SIZEOF_FLOAT != 4 */
 
-  # if ACE_SIZEOF_DOUBLE == 8
+#   if ACE_SIZEOF_DOUBLE == 8
       typedef double Double;
-  # else  /* ACE_SIZEOF_DOUBLE != 8 */
+#   else  /* ACE_SIZEOF_DOUBLE != 8 */
       struct Double
       {
-  #	if ACE_SIZEOF_LONG == 8
+#  	if ACE_SIZEOF_LONG == 8
 	  // Use u_long	to get word alignment.
 	  u_long f;
-  #	else  /* ACE_SIZEOF_INT	!= 8 */
+#  	else  /* ACE_SIZEOF_INT	!= 8 */
 	  // Applications will probably	have trouble with this.
 	  char f[8];
-  #	endif /* ACE_SIZEOF_INT	!= 8 */
+#  	endif /* ACE_SIZEOF_INT	!= 8 */
       };
-  # endif /* ACE_SIZEOF_DOUBLE != 8 */
+#   endif /* ACE_SIZEOF_DOUBLE != 8 */
 
     // 94-9-32 Appendix	A defines a 128	bit floating point "long
     // double" data type, with greatly extended	precision and four
     // more bits of exponent (compared to "double").  This is an IDL
     // extension, not yet standard.
 
-  #  if	  ACE_SIZEOF_LONG_DOUBLE == 16
+#    if	  ACE_SIZEOF_LONG_DOUBLE == 16
        typedef long double	LongDouble;
-       #define	ACE_CDR_LONG_DOUBLE_INITIALIZER	0
-  #  else
-  #    define NONNATIVE_LONGDOUBLE
-  #    define	ACE_CDR_LONG_DOUBLE_INITIALIZER	{{0}}
+#      define	ACE_CDR_LONG_DOUBLE_INITIALIZER	0
+#    else
+#      define NONNATIVE_LONGDOUBLE
+#      define	ACE_CDR_LONG_DOUBLE_INITIALIZER	{{0}}
        struct ACE_Export LongDouble
        {
          char ld[16];
@@ -271,7 +271,7 @@ public:
          int operator!= (const LongDouble &rhs) const;
          // @@ also need other comparison operators.
        };
-  #  endif /* ACE_SIZEOF_LONG_DOUBLE !=	16 */
+#    endif /* ACE_SIZEOF_LONG_DOUBLE !=	16 */
 
 #if !defined (ACE_CDR_GIOP_MAJOR_VERSION)
 #   define ACE_CDR_GIOP_MAJOR_VERSION 1
