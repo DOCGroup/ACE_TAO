2ffb3df4330b67ed622951dfc80917ef538c4653
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-09-25 16:12:04 +0000

    ChangeLogTag: Wed Sep 25 11:07:21 2002  Jeff Parsons <parsons@cs.wustl.edu>

--- TAO/tao/TypeCodeFactory_Adapter.cpp
+++ TAO/tao/TypeCodeFactory_Adapter.cpp
@@ -1,8 +1,11 @@
 // @(#) $Id$
 
 #include "tao/TypeCodeFactory_Adapter.h"
+#include "ace/Dynamic_Service.h"
 
-ACE_RCSID(tao, TypeCodeFactory_Adapter, "$Id$")
+ACE_RCSID (tao, 
+           TypeCodeFactory_Adapter, 
+           "$Id$")
 
 TAO_TypeCodeFactory_Adapter::~TAO_TypeCodeFactory_Adapter (void)
 {
