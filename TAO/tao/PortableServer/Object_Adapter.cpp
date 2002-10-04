3bd40faeb81d4479f15da3386d4a3f173385ccd6
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-10-04 23:23:25 +0000

    ChangeLogTag: Fri Oct 04 19:23:08 2002  Irfan Pyarali  <irfan@oomworks.com>

--- TAO/tao/PortableServer/Object_Adapter.cpp
+++ TAO/tao/PortableServer/Object_Adapter.cpp
@@ -10,6 +10,7 @@
 #include "ServerRequestInfo.h"
 #include "Default_Servant_Dispatcher.h"
 #include "ServerInterceptorAdapter.h"
+#include "PortableServer_ORBInitializer.h"
 
 // -- ACE Include --
 #include "ace/Auto_Ptr.h"
@@ -877,6 +878,44 @@ TAO_Object_Adapter_Factory::create (TAO_ORB_Core *orb_core)
                                  *orb_core);
 }
 
+int
+TAO_Object_Adapter_Factory::init (int argc,
+                                  ACE_TCHAR* argv[])
+{
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
+    {
+      /// Register the Messaging ORBInitializer.
+      PortableInterceptor::ORBInitializer_ptr temp_orb_initializer =
+        PortableInterceptor::ORBInitializer::_nil ();
+
+      ACE_NEW_THROW_EX (temp_orb_initializer,
+                        TAO_PortableServer_ORBInitializer,
+                        CORBA::NO_MEMORY (
+                          CORBA_SystemException::_tao_minor_code (
+                            TAO_DEFAULT_MINOR_CODE,
+                            ENOMEM),
+                          CORBA::COMPLETED_NO));
+      ACE_TRY_CHECK;
+
+      PortableInterceptor::ORBInitializer_var orb_initializer =
+        temp_orb_initializer;
+
+      PortableInterceptor::register_orb_initializer (orb_initializer.in ()
+                                                     ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "(%P | %t) Caught exception:");
+      return -1;
+    }
+  ACE_ENDTRY;
+
+  return 0;
+}
+
 ACE_FACTORY_DEFINE (TAO_PortableServer, TAO_Object_Adapter_Factory)
 ACE_STATIC_SVC_DEFINE (TAO_Object_Adapter_Factory,
                        ACE_TEXT ("TAO_POA"),
@@ -1284,7 +1323,7 @@ TAO_Object_Adapter::Servant_Upcall::pre_invoke_remote_request (TAO_ServerRequest
 {
   this->object_adapter_->servant_dispatcher_->pre_invoke_remote_request (this->poa (),
                                                                          this->priority (),
-                                                                         req,                                                                         
+                                                                         req,
                                                                          this->pre_invoke_state_
                                                                          ACE_ENV_ARG_PARAMETER);
   ACE_CHECK;
