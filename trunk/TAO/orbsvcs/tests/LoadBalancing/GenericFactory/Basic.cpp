22a90600b1ab51884ceec36f8ef51fdcb14e8b31
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-10-08 23:30:22 +0000

    Tue Oct  8 16:28:05 2002  Jaiganesh Balasubramanian  <jai@doc.ece.uci.edu>

--- trunk/TAO/orbsvcs/tests/LoadBalancing/GenericFactory/Basic.cpp
+++ trunk/TAO/orbsvcs/tests/LoadBalancing/GenericFactory/Basic.cpp
@@ -14,8 +14,7 @@ Basic::Basic (CORBA::Object_ptr object_group,
 {
   this->object_group_ = CORBA::Object::_duplicate (object_group);
   this->lm_ = CosLoadBalancing::LoadManager::_duplicate (lm);
-  this->location_.length (1);
-  this->location_[0].id = CORBA::string_dup (loc);
+  this->location_ = CORBA::string_dup (loc);
 }
 
 char *
@@ -26,31 +25,44 @@ Basic::get_string (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
 }
 
 void
-Basic::shutdown (ACE_ENV_SINGLE_ARG_DECL)
+Basic::remove_member (ACE_ENV_SINGLE_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   ACE_TRY
     {
+      PortableGroup::Location location (1);
+      location.length (1);
+      location[0].id = CORBA::string_dup (this->location_);
       this->lm_->remove_member (this->object_group_.in (),
-                                this->location_
+                                location
                                 ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      this->orb_->shutdown (0 ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
     }
   ACE_CATCH (PortableGroup::ObjectNotFound, ex)
     {
       ACE_PRINT_EXCEPTION (ex,
-                           "Caught exception");
+                           "Caught exception in remove_member");
       ACE_TRY_THROW (CORBA::INTERNAL ());
     }
   ACE_CATCH (PortableGroup::MemberNotFound, ex)
     {
       ACE_PRINT_EXCEPTION (ex,
-                           "Caught exception");
+                           "Caught exception in remove_member");
       ACE_TRY_THROW (CORBA::INTERNAL ());
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Exception caught while destroying member\n");
     }
   ACE_ENDTRY;
 
+}
+
+void
+Basic::shutdown (ACE_ENV_SINGLE_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  this->orb_->shutdown (0 ACE_ENV_ARG_PARAMETER);
 }
