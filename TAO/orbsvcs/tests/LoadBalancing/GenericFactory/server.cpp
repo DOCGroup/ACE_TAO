5ef45a446ae52b3fc90a05fefb18a8a470134c7f
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-10-13 20:49:38 +0000

    Sun Oct 13 13:47:21 2002  Jaiganesh Balasubramanian  <jai@doc.ece.uci.edu>

--- TAO/orbsvcs/tests/LoadBalancing/GenericFactory/server.cpp
+++ TAO/orbsvcs/tests/LoadBalancing/GenericFactory/server.cpp
@@ -13,48 +13,43 @@ main (int argc, char *argv[])
       const char *loc1 = "MyLocation 1";
       const char *loc2 = "MyLocation 2";
 
-      LB_Basic_Test basic_test (argc, argv);
+      LB_server lb_server (argc, argv);
 
-      if (basic_test.start_orb_and_poa () == -1)
+      if (lb_server.start_orb_and_poa () == -1)
         return 1;
 
-      if (basic_test.create_object_group () == -1)
+      if (lb_server.create_object_group () == -1)
         return 1;
 
-      Basic basic_servant1 (basic_test.object_group (),
-                            basic_test.load_manager (),
-                            basic_test.orb (),
+      Basic basic_servant1 (lb_server.object_group (),
+                            lb_server.load_manager (),
+                            lb_server.orb (),
                             loc1);
 
-      Basic basic_servant2 (basic_test.object_group (),
-                            basic_test.load_manager (),
-                            basic_test.orb (),
+      Basic basic_servant2 (lb_server.object_group (),
+                            lb_server.load_manager (),
+                            lb_server.orb (),
                             loc2);
 
-      if (basic_test.register_servant (&basic_servant1, loc1) == -1)
+      if (lb_server.register_servant (&basic_servant1, loc1) == -1)
         return 1;
 
-      if (basic_test.register_servant (&basic_servant2, loc2) == -1)
+      if (lb_server.register_servant (&basic_servant2, loc2) == -1)
         return 1;
 
-      basic_test.orb ()->run (ACE_ENV_SINGLE_ARG_PARAMETER);
+      lb_server.orb ()->run (ACE_ENV_SINGLE_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
       ACE_DEBUG ((LM_DEBUG, "(%P|%t) server - event loop finished\n"));
 
-      // @@ Jai, this destroy() call doesn't match the signature of
-      //    your LB_Basic_Test::destroy() method.  You're also
-      //    ignoring the return value of that method.  You need to
-      //    decide whether this method will report errors through
-      //    CORBA exceptions or through a return value.  If you decide
-      //    to use a return value, make sure you check it!
-      basic_test.destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK;
+      if (lb_server.destroy () == -1)
+        return 1;
+
     }
   ACE_CATCHANY
     {
       ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
-                           "server exception");
+                           "lb_server exception");
       return 1;
     }
   ACE_ENDTRY;
