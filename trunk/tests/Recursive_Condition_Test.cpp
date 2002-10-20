5ff4820fcf09587d32728fdb21c6aca8e28c2575
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-10-20 14:46:02 +0000

    Cosmetic fixes

--- trunk/tests/Recursive_Condition_Test.cpp
+++ trunk/tests/Recursive_Condition_Test.cpp
@@ -12,12 +12,12 @@
 //      This test program verifies the functionality of the
 //      ACE_Condition<ACE_Recursive_Thread_Mutex> template
 //      specialization when combined with the
-//      ACE_Thread_Timer_Queue_Adapter on Win32 and Posix pthreads. 
+//      ACE_Thread_Timer_Queue_Adapter on Win32 and Posix pthreads.
 //
 // = AUTHOR
 //    Stephen Howard <stephen.e.howard@lmco.com> and
 //    Douglas C. Schmidt <schmidt@cs.wustl.edu>
-// 
+//
 // ============================================================================
 
 #include "test_config.h"
@@ -29,27 +29,29 @@
 #include "ace/Timer_Queue_Adapters.h"
 
 ACE_RCSID(tests, Recursive_Condition_Test, "$Id$")
-  
+
 #if defined (ACE_HAS_THREADS)
-  
+
   typedef ACE_Thread_Timer_Queue_Adapter<ACE_Timer_Heap> Thread_Timer_Queue;
 
 class Test_Handler : public ACE_Event_Handler
 {
 public:
   virtual int handle_timeout (const ACE_Time_Value &,
-                              const void *arg)
+                              const void * /*arg*/)
   {
-    ACE_TRACE ("[%t] handle_timeout\n");
-    
-    Thread_Timer_Queue *timer_queue = (Thread_Timer_Queue *) arg;
-    
+    ACE_TRACE ("Test_Handler::handle_timeout");
+
+    /*Thread_Timer_Queue *timer_queue =
+      (Thread_Timer_Queue *) arg;*/
+
     ACE_Time_Value timeout = ACE_OS::gettimeofday () + ACE_Time_Value (1, 0);
 
     ACE_UNUSED_ARG (timeout);
 
-    ACE_TRACE ("[%t] scheduling timer\n");
-    
+    ACE_DEBUG ((LM_DEBUG,
+               ACE_TEXT ("(%P|%t) scheduling timer\n")));
+
     /*
       int timer_id =
       timer_queue->schedule (this, timer_queue, timeout);
@@ -80,21 +82,21 @@ test_1(void)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("spawn")), 1);
     }
-  
+
   ACE_OS::sleep (2);
   if (mutex_.acquire () == -1)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
-			 ACE_TEXT ("mutex acquire")),
-			1);
+                         ACE_TEXT ("mutex acquire")),
+                        1);
     }
   ACE_TRACE (ACE_TEXT ("(%t) signaling condition...\n"));
-  
+
   if (condition_.signal () == -1)
     {
       ACE_ERROR ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("signal")));
     }
-  
+
   mutex_.release ();
   ACE_Thread_Manager::instance ()->wait ();
   return 0;
@@ -107,16 +109,16 @@ test_2(void)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("spawn")), 1);
     }
-  
+
   ACE_OS::sleep (2);
   if (mutex_.acquire () == -1)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
-			 ACE_TEXT ("mutex acquire")),
-			1);
+                         ACE_TEXT ("mutex acquire")),
+                        1);
     }
   ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("(%t) signaling condition...\n")));
-  
+
   if (condition_.signal () == -1)
     {
       ACE_ERROR ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("signal")));
@@ -135,23 +137,23 @@ test_3()
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("spawn")), 1);
     }
-  
+
   ACE_OS::sleep (2);
   if (mutex_.acquire () == -1)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
-			 ACE_TEXT ("mutex acquire")),
+                         ACE_TEXT ("mutex acquire")),
                       1);
     }
   ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("(%t) signaling condition...\n")));
-  
+
   if (condition_.broadcast () == -1)
     {
       ACE_ERROR ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("signal")));
     }
   mutex_.release ();
   ACE_Thread_Manager::instance ()->wait ();
-  
+
   return 0;
 }
 
@@ -162,22 +164,22 @@ test_4()
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT ("spawn")), 1);
     }
-  
+
   ACE_OS::sleep (2);
   for(int i = 0; i < 3; ++i){
     if (mutex_.acquire () == -1)
       {
-	ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
-			   ACE_TEXT ("mutex acquire")),
-			  1);
+        ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
+                           ACE_TEXT ("mutex acquire")),
+                          1);
       }
   }
-  
+
   if (mutex_.get_nesting_level() != 3)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT("%d\n"),
-			 ACE_TEXT("get_nestling_level")),
-			1);
+                         ACE_TEXT("get_nestling_level")),
+                        1);
     }
 
   ACE_TRACE (ACE_TEXT ("(%t) signaling condition...\n"));
@@ -193,8 +195,8 @@ test_4()
   if (mutex_.get_nesting_level() != 0)
     {
       ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT("%d\n"),
-			 ACE_TEXT("get_nestling_level")),
-			1);
+                         ACE_TEXT("get_nestling_level")),
+                        1);
     }
   ACE_Thread_Manager::instance ()->wait ();
   return 0;
@@ -206,7 +208,7 @@ ACE_TMAIN (int, ACE_TCHAR *[])
   ACE_START_TEST (ACE_TEXT ("Recursive_Condition_Test"));
 
 #if defined (ACE_HAS_THREADS)
-#if 0 
+#if 0
   Thread_Timer_Queue timer_queue;
   Test_Handler handler;
   int status = timer_queue.activate ();
@@ -221,20 +223,20 @@ ACE_TMAIN (int, ACE_TCHAR *[])
   ACE_OS::sleep (10);
   timer_queue.deactivate ();
   timer_queue.wait ();
-#endif 
+#endif
 
   /* Test 1 - Simple test */
   if (test_1 () != 0)
     {
       ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT("test #1")), 1);
     }
-  
+
   /* Test #2 - Sleep 2 seconds before releasing mutex */
   if (test_2 () != 0)
     {
       ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT("test #2")), 1);
     }
-  
+
   /* Test #3 - One main thread - 4 subthreads */
   if (test_3 () != 0)
     {
@@ -246,7 +248,7 @@ ACE_TMAIN (int, ACE_TCHAR *[])
     {
       ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT ("%p\n"), ACE_TEXT("test #4")), 1);
     }
-  
+
 #else
   ACE_ERROR ((LM_ERROR,
               ACE_TEXT ("ACE doesn't support recursive condition variables on this platform\n")));
@@ -260,4 +262,3 @@ template class ACE_Thread_Timer_Queue_Adapter<ACE_Timer_Heap>;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate ACE_Thread_Timer_Queue_Adapter<ACE_Timer_Heap>
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
