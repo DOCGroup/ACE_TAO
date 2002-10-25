d9ff8bdf596f2ed1953bc1276ace4c6182e32eb2
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-10-25 14:37:23 +0000

    *** empty log message ***

--- branches/DynamicScheduling/TAO/tao/RTScheduling/Current.h
+++ branches/DynamicScheduling/TAO/tao/RTScheduling/Current.h
@@ -14,24 +14,18 @@ class TAO_RTScheduler_Current;
 class TAO_TSS_Resources;
 
 class TAO_RTScheduler_Export TAO_RTScheduler_Current: 
-public RTScheduler::Current,
+public RTScheduling::Current,
   public TAO_Local_RefCounted_Object
 {
  public:
   TAO_Scheduler_Current (void);
   
-  virtual RTScheduler::DistributableThread
-    spawn (RTScheduling::ThreadAction_ptr start,
-	   CORBA::ULong stack_size,
-	   RTCORBA::Priority base_priority
-	   ACE_ENV_SINGLE_ARG_DECL)
-    ACE_THROW_SPEC ((CORBA::SystemException));
   
   virtual void begin_scheduling_segment
     (const char * name,
      CORBA::Policy_ptr sched_param,
      CORBA::Policy_ptr implicit_sched_param
-     ACE_ENV_SINGLE_ARG_DECL)
+     ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CORBA::SystemException,
 		     RTScheduling::Current::UNSUPPORTED_SCHEDULING_DISCIPLINE));
     
@@ -39,18 +33,83 @@ public RTScheduler::Current,
     (const char * name,
      CORBA::Policy_ptr sched_param,
      CORBA::Policy_ptr implicit_sched_param
-     ACE_ENV_SINGLE_ARG_DECL)
+     ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CORBA::SystemException,
 		     RTScheduling::Current::UNSUPPORTED_SCHEDULING_DISCIPLINE));
 
   virtual void end_scheduling_segment
     (const char * name
-     ACE_ENV_SINGLE_ARG_DECL)
+     ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CORBA::SystemException));
   
-  virtual RTScheduler::DistributableThread_ptr 
+  virtual RTScheduling::DistributableThread_ptr 
     lookup(const RTScheduling::Current::IdType & id
-	   ACE_ENV_SINGLE_ARG_DECL)
+	   ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+  // returns a null reference if
+  // the distributable thread is
+  // not known to the local scheduler
+
+  virtual RTScheduling::DistributableThread_ptr
+    spawn (RTScheduling::ThreadAction_ptr start,
+	   CORBA::ULong stack_size,
+	   RTCORBA::Priority base_priority
+	   ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+  virtual ::RTScheduling::Current::IdType * 
+    id (ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+  
+  virtual ::CORBA::Policy_ptr 
+    scheduling_parameter (ACE_ENV_SINGLE_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+  
+  virtual ::CORBA::Policy_ptr 
+    implicit_scheduling_parameter (ACE_ENV_SINGLE_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+  
+  virtual ::RTScheduling::Current::NameList * 
+    current_scheduling_segment_names (ACE_ENV_SINGLE_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));  
+  
+};
+
+
+class TAO_RTScheduler_Export TAO_RTScheduler_Current_i
+{
+  virtual RTScheduling::DistributableThread_ptr
+    spawn (RTScheduling::ThreadAction_ptr start,
+	   CORBA::ULong stack_size,
+	   RTCORBA::Priority base_priority
+	   ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+  
+  virtual void begin_scheduling_segment
+    (const char * name,
+     CORBA::Policy_ptr sched_param,
+     CORBA::Policy_ptr implicit_sched_param
+     ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException,
+		     RTScheduling::Current::UNSUPPORTED_SCHEDULING_DISCIPLINE));
+  
+  virtual void update_scheduling_segment
+    (const char * name,
+     CORBA::Policy_ptr sched_param,
+     CORBA::Policy_ptr implicit_sched_param
+     ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException,
+		     RTScheduling::Current::UNSUPPORTED_SCHEDULING_DISCIPLINE));
+
+  virtual void end_scheduling_segment
+    (const char * name
+     ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+  
+  virtual RTScheduling::DistributableThread_ptr 
+    lookup(const RTScheduling::Current::IdType & id
+	   ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CORBA::SystemException));
 
   // returns a null reference if
