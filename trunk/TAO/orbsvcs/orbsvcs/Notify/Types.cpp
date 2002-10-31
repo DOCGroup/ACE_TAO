65d6f35b010293243bc8f7298a9c5762d0b313d7
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-10-31 01:10:18 +0000

    ChangelogTag: Wed Oct 30 19:06:59 2002  Yamuna Krishnamurthy  <yamuna@oomworks.com>

--- trunk/TAO/orbsvcs/orbsvcs/Notify/Types.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Notify/Types.cpp
@@ -2,9 +2,16 @@
 
 #include "Types.h"
 #include "orbsvcs/NotifyExtC.h"
+#include "orbsvcs/ESF/ESF_RefCount_Guard.h"
 
 #if ! defined (__ACE_INLINE__)
 #include "Types.inl"
 #endif /* __ACE_INLINE__ */
 
 ACE_RCSID(RT_Notify, TAO_NS_Types, "$Id$")
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class TAO_ESF_RefCount_Guard<CORBA::ULong>; 
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate TAO_ESF_RefCount_Guard<CORBA::ULong> 
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
