9bef5553c45665b63d21dc3c7c060458f32c8f7b
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-11-02 01:11:22 +0000

    ChangeLogTag: Fri Nov  1 19:06:45 2002  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- TAO/tao/Utils/PolicyList_Destroyer.cpp
+++ TAO/tao/Utils/PolicyList_Destroyer.cpp
@@ -1,33 +1,36 @@
-/**
- * @file PolicyList_Destroyer.cpp
- *
- * $Id$
- *
- * @author Carlos O'Ryan <coryan@atdesk.com>
- */
 #include "PolicyList_Destroyer.h"
 
 #if !defined (__ACE_INLINE__)
 # include "PolicyList_Destroyer.inl"
 #endif /* __ACE_INLINE__ */
 
-TAO::Utils::PolicyList_Destroyer::
-~PolicyList_Destroyer()
+ACE_RCSID (Utils,
+           PolicyList_Deactivator,
+           "$Id$")
+
+TAO::Utils::PolicyList_Destroyer::~PolicyList_Destroyer()
+  ACE_THROW_SPEC (())
 {
+  ACE_DECLARE_NEW_CORBA_ENV;
+
   for (CORBA::ULong i = 0; i != length(); ++i)
-  {
-    CORBA::Policy_ptr policy = (*this)[i];
-    if (CORBA::is_nil(policy))
     {
-      continue;
+      CORBA::Policy_ptr policy = (*this)[i];
+      if (CORBA::is_nil (policy))
+        {
+          continue;
+        }
+
+      ACE_TRY
+        {
+          policy->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+        }
+      ACE_CATCHALL
+        {
+        }
+      ACE_ENDTRY;
+
+      (*this)[i] = CORBA::Policy::_nil();
     }
-    try
-    {
-      policy->destroy();
-    }
-    catch (...)
-    {
-    }
-    (*this)[i] = CORBA::Policy::_nil();
-  }
 }
