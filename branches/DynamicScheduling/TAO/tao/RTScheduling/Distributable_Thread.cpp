b192124d6aa647477482e60b226ab191a9881813
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-11-09 00:48:42 +0000

    *** empty log message ***

--- branches/DynamicScheduling/TAO/tao/RTScheduling/Distributable_Thread.cpp
+++ branches/DynamicScheduling/TAO/tao/RTScheduling/Distributable_Thread.cpp
@@ -7,3 +7,21 @@ TAO_DistributableThread::cancel (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
 }
+
+
+RTScheduling::DistributableThread_ptr 
+TAO_DistributableThread_Factory::create_DT (void)
+{
+  TAO_DistributableThread* DT;
+
+  ACE_NEW_THROW_EX (DT,
+                    TAO_DistributableThread,
+                    CORBA::NO_MEMORY (
+                      CORBA::SystemException::_tao_minor_code (
+                        TAO_DEFAULT_MINOR_CODE,
+                        ENOMEM),
+                      CORBA::COMPLETED_NO));
+  ACE_CHECK;
+
+  return DT;
+}
--- branches/DynamicScheduling/TAO/tao/RTScheduling/Distributable_Thread.h
+++ branches/DynamicScheduling/TAO/tao/RTScheduling/Distributable_Thread.h
@@ -11,4 +11,10 @@ class TAO_RTScheduler_Export TAO_DistributableThread
   virtual void cancel (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
     ACE_THROW_SPEC ((CORBA::SystemException));
 };
+
+
+class TAO_DistributableThread_Factory
+{
+  static RTScheduling::DistributableThread_ptr create_DT (void);
+}
 #endif /*DISTRIBUTABLE_THREAD_H*/
