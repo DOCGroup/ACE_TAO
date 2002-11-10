3cccbea10f2e0a3a861322a738e086cfe3bb60c9
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-11-10 05:10:11 +0000

    *** empty log message ***

--- TAO/CIAO/examples/handcrafted/Hello/hello_executors.cpp
+++ TAO/CIAO/examples/handcrafted/Hello/hello_executors.cpp
@@ -1,1 +1,44 @@
 // $Id$
+
+#include "hello_executors.h"
+
+HelloWorld_Impl::HelloWorld ()
+{
+}
+
+HelloWorld_Impl::~HelloWorld ()
+{
+}
+
+char *
+HelloWorld_Impl::sayHello (const char *username
+                           ACE_ENV_ARG_DECL_NOT_USED)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  static const char *fmt = "Hello World message for \n";
+
+  CORBA::String_var str =
+    CORBA::string_alloc (ACE_OS::strlen (fmt) +
+                         ACE_OS::strlen (username));
+
+  ACE_OS::strcpy (str.inout (), fmt);
+  ACE_OS::strcat (str.inout (), username);
+
+  return str._retn ();
+}
+
+HelloHome_Impl::HelloHome_impl ()
+{
+}
+
+HelloHome_Impl::~HelloHome_impl ()
+{
+}
+
+Components::EnterpriseComponent_ptr
+HelloHome_Impl::create (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+  ACE_THROW_SPEC ((CORBA::SystemException,
+                   Components::CreateFailure))
+{
+  return new HelloWorld_Impl ();
+}
