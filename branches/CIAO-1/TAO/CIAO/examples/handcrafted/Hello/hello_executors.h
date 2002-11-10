61c7de9edc1eb0dd1bddda5e8a40b3f6abce3650
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-11-10 05:10:11 +0000

    *** empty log message ***

--- branches/CIAO-1/TAO/CIAO/examples/handcrafted/Hello/hello_executors.h
+++ branches/CIAO-1/TAO/CIAO/examples/handcrafted/Hello/hello_executors.h
@@ -1,1 +1,40 @@
 // $Id$
+
+// This file contains executor implementations.
+
+#include "helloEC.h"
+
+#if !defined (HELLO_EXECUTORS_H)
+#define (HELLO_EXECUTORS_H)
+
+class HelloWorld_Impl : public virtual CCM_HelloWorld
+{
+public:
+  /// Default ctor.
+  HelloWorld ();
+
+  /// Default dtor.
+  ~HelloWorld ();
+
+  virtual char *sayHello (const char *username
+                          ACE_ENV_ARG_DECL_WITH_DEFAULTS)
+    ACE_THROW_SPEC ((
+      CORBA::SystemException
+    ));
+};
+
+class HelloHome_Impl : public virtual CCM_HelloHome
+{
+public:
+  /// Default ctor.
+  HelloHome_impl ();
+
+  /// Default dtor.
+  ~HelloHome_impl ();
+
+  virtual ::Components::EnterpriseComponent_ptr
+  create (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+    ACE_THROW_SPEC ((CORBA::SystemException,
+                     Components::CreateFailure));
+};
+#endif /* HELLO_EXECUTORS_H */
