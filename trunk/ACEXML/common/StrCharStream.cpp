9e3039c6a6088caeb11a7cd28c7f7defaa4cca96
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-11-27 20:50:58 +0000

    ChangeLogTag:Wed Nov 27 15:47:41 2002  Steve Huston  <shuston@riverace.com>

--- trunk/ACEXML/common/StrCharStream.cpp
+++ trunk/ACEXML/common/StrCharStream.cpp
@@ -3,6 +3,7 @@
 #include "ACEXML/common/StrCharStream.h"
 #include "ACEXML/common/Encoding.h"
 #include "ace/ACE.h"
+#include "ace/Log_Msg.h"
 
 ACEXML_StrCharStream::ACEXML_StrCharStream (void)
   : start_ (0), ptr_ (0), end_ (0), encoding_ (0)
--- trunk/ace/config-aix5.1.h
+++ trunk/ace/config-aix5.1.h
@@ -4,5 +4,10 @@
 
 #include "ace/config-aix-4.x.h"
 
+// AIX 5.1 has AIO, but it doesn't have the same API as other POSIX
+// systems, and the semantics of operations are a bit different. Will take
+// some real work to get this going.
+//#define ACE_HAS_AIO_CALLS
+
 // I think this is correct, but needs to be verified...   -Steve Huston
 #define ACE_HAS_SIGTIMEDWAIT
