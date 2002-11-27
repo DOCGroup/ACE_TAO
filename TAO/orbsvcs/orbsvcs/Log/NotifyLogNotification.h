6ac7bc1470b9a4796c920fd390989d59cb933b3a
dhanvey <dhanvey@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-11-27 16:37:32 +0000

    *** empty log message ***

--- TAO/orbsvcs/orbsvcs/Log/NotifyLogNotification.h
+++ TAO/orbsvcs/orbsvcs/Log/NotifyLogNotification.h
@@ -1,4 +1,3 @@
-<<<<<<< NotifyLogNotification.h
 /* -*- C++ -*- */
 
 //=============================================================================
@@ -27,7 +26,6 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "orbsvcs/Notify/Notify_EventChannelFactory_i.h"
 #include "orbsvcs/CosEventCommS.h"
 #include "orbsvcs/CosNotifyChannelAdminS.h"
 #include "orbsvcs/CosNotifyCommC.h"
@@ -111,99 +109,3 @@ private:
 
 #include "ace/post.h"
 #endif /* TAO_TLS_NOTIFY_LOG_NOTIFICATION_H */
-=======
-/* -*- C++ -*- */
-
-//=============================================================================
-/**
- *  @file   NotifyLogNotification.h
- *
- *  $Id$
- *
- *  Methods to enable log-generated events.
- *
- *
- *  @author D A Hanvey <d.hanvey@qub.ac.uk>
- *   
- *  
- */
-//=============================================================================
-
-#ifndef TLS_NOTIFY_LOG_NOTIFICATION_H
-#define TLS_NOTIFY_LOG_NOTIFICATION_H
-#include "ace/pre.h"
-
-#include "orbsvcs/DsNotifyLogAdminS.h"
-#include "orbsvcs/Notify/Notify_EventChannelFactory_i.h"
-#include "orbsvcs/CosEventCommS.h"
-#include "orbsvcs/CosNotifyChannelAdminS.h"
-#include "orbsvcs/CosNotifyCommC.h"
-#include "orbsvcs/CosNamingC.h"
-
-#include "notifylog_export.h"
-#if !defined (ACE_LACKS_PRAGMA_ONCE)
-# pragma once
-#endif /* ACE_LACKS_PRAGMA_ONCE */
-
-
-class TAO_NotifyLog_Export NotifyLogNotification : public LogNotification, 
-  public POA_CosNotifyComm::PushSupplier, public PortableServer::RefCountServantBase
-{
-  // = TITLE
-  //   Used to forward log generated events to a logging server
-  //
-  // = DESCRIPTION
-  //   This implementation attempts to conform to the telecom
-  //   logging specification.
-
-public:
-  // = Initialization and Termination
-  NotifyLogNotification (CosNotifyChannelAdmin::EventChannel_ptr);
-  // Constructor.
-
-  ~NotifyLogNotification (void);
-  // Destructor.
-
-protected:
-  // = Helper methods
-  // = Data members
-  CosNotifyChannelAdmin::ProxyPushConsumer_var proxy_consumer_;
-  // The proxy that we are connected to.
-
-  CosNotifyChannelAdmin::ProxyID proxy_consumer_id_;
-  // This supplier's id.
-
-  // = NotifySubscribe
-  virtual void subscription_change (
-        const CosNotification::EventTypeSeq & added,
-        const CosNotification::EventTypeSeq & removed
-        ACE_ENV_ARG_DECL
-      )
-      ACE_THROW_SPEC ((
-        CORBA::SystemException,
-        CosNotifyComm::InvalidEventType
-      ));
-
-  // = StructuredPushSupplier method
-    virtual void disconnect_push_supplier (
-        ACE_ENV_SINGLE_ARG_DECL
-      )
-      ACE_THROW_SPEC ((
-        CORBA::SystemException
-      ));
-
-  virtual void send_notification (const CORBA::Any& any)
-      ACE_THROW_SPEC ((
-        CORBA::SystemException
-      ));
-  // Send the event on the event channel.
-
-
-private:
-  CORBA::ORB_var orb_;
-
-  CosNotifyChannelAdmin::EventChannel_var event_channel_;
-};
-#include "ace/post.h"
-#endif /* TLS_NOTIFY_LOG_NOTIFICATION_H */
->>>>>>> 1.4
