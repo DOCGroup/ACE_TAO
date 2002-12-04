b94a9af6847596cc05877efa26120f285e2603fc
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-12-04 12:59:06 +0000

    ChangeLogTag:Wed Dec 04 06:59:27 2002  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- trunk/TAO/tests/Bug_1020_Regression/run_test.pl
+++ trunk/TAO/tests/Bug_1020_Regression/run_test.pl
@@ -25,7 +25,7 @@ if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
 }
 
 local $start_time = time();
-local $max_running_time = 600; # 5 minutes
+local $max_running_time = 300; # 5 minutes
 local $elapsed = time() - $start_time;
 
 while($elapsed < $max_running_time) {
