0a0659d744fb7a7fc8afcf541ad19c7d99fb6945
bala <balanatarajan@users.noreply.github.com>
2002-12-04 02:32:43 +0000

    ChangeLogTag: Tue Dec  3 21:27:52 2002  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- TAO/tao/Connect_Strategy.cpp
+++ TAO/tao/Connect_Strategy.cpp
@@ -1,4 +1,5 @@
 #include "Connect_Strategy.h"
+#include "Connection_Handler.h"
 
 
 ACE_RCSID(tao,
@@ -16,7 +17,24 @@ TAO_Connect_Strategy::~TAO_Connect_Strategy (void)
 }
 
 int
-TAO_Connect_Strategy::post_failed_connect (TAO_Connection_Handler * /*ch*/)
+TAO_Connect_Strategy::post_failed_connect (TAO_Connection_Handler *ch,
+                                           int is_successful)
 {
-  return 0;
+  int ref = 0;
+  if (!is_successful)
+    {
+      // We need to do this here else we will leak memory. We cannot call
+      // close_connection () for the following reasons
+      // . the event_handlers get_handle () will return an
+      //   ACE_INVALID_HANDLE as things havent been shutdown
+      //   properly.
+      // . We cannot relax the conditions since the conditions are right
+      //   for other natural cases ie. LF
+      // Under these odd circumstances we need to take care of things
+      // ourseleves
+      ch->transport (0);
+      ref =  ch->decr_refcount ();
+    }
+
+  return ref;
 }
