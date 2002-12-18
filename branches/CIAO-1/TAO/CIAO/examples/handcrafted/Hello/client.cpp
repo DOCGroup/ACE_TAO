57f7967553ee2240d73311bf7dcd57ce591441ad
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-12-18 05:03:13 +0000

    *** empty log message ***

--- branches/CIAO-1/TAO/CIAO/examples/handcrafted/Hello/client.cpp
+++ branches/CIAO-1/TAO/CIAO/examples/handcrafted/Hello/client.cpp
@@ -3,39 +3,34 @@
 #include "helloC.h"
 #include "ace/Read_Buffer.h"
 
-char *ior = 0;
-
 int
-read_ior (const char *filename)
+invoke (CORBA::Object_ptr obj,
+        const char *msg
+        ACE_ENV_ARG_DECL)
 {
-  // Open the file for reading.
-  ACE_HANDLE f_handle = ACE_OS::open (filename, 0);
+  // Narrow
+  HelloHome_var hh = HelloHome::_narrow (obj ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK_RETURN (-1); ;
 
-  if (f_handle == ACE_INVALID_HANDLE)
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "Unable to open %s for writing: %p\n",
-                       filename,
-                       "invalid handle"),
-                      -1);
+  if  (CORBA::is_nil (hh.in ()))
+    ACE_ERROR_RETURN ((LM_ERROR, "Argument is not a HelloHome reference\n"), -1);
 
-  ACE_Read_Buffer ior_buffer (f_handle);
-  char *data = ior_buffer.read ();
+  HelloWorld_var hw = hh->create ();
 
-  if (data == 0)
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "Unable to read ior: %p\n"),
-                      -1);
+  if  (CORBA::is_nil (hw.in ()))
+    ACE_ERROR_RETURN ((LM_ERROR, "Fail to create a HelloWorld reference\n"), -1);
 
-  ior = ACE_OS::strdup (data);
-  ior_buffer.alloc ()->free (data);
+  CORBA::String_var name = CORBA::string_dup (msg);
+  CORBA::String_var hi = hw->sayhello (name
+                                       ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK_RETURN (-1);
 
-  ACE_OS::close (f_handle);
+  ACE_DEBUG ((LM_INFO, "%s\n", hi.in ()));
 
-  if (ior == 0)
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       "failed to read ior from file\n",
-                       ""),
-                      -1);
+  hh->remove_component (hw
+                        ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK_RETURN (-1);
+
   return 0;
 }
 
@@ -44,69 +39,50 @@ main (int argc, char *argv[])
 {
   ACE_TRY_NEW_ENV
     {
-      // Check arguments
-      if  (argc != 2)
-        {
-          cerr << "Usage: client IOR_file" << endl;
-          return -1;
-        }
-
       // Initialize orb
       CORBA::ORB_var orb = CORBA::ORB_init (argc, argv ACE_ENV_ARG_PARAMETER);
 
-//       if (read_ior (argv[1]) != 0)
-//         return -1;
+      // Resolve HomeFinder interface
 
-      // Destringify argv[1]
-      CORBA::Object_var obj = orb->string_to_object (argv[1] ACE_ENV_ARG_PARAMETER);
-      if  (CORBA::is_nil (obj.in ()))
-        {
-          cerr << "Nil simple reference" << endl;
-          return -1;
-        }
-
-      // Narrow
-      HelloHome_var hh = HelloHome::_narrow (obj.in () ACE_ENV_ARG_PARAMETER);
+      CORBA::Object_var obj = orb->resolve_initial_references ("HomeFinder"
+                                                               ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      if  (CORBA::is_nil (hh.in ()))
-        {
-          cerr << "Argument is not a HelloHome reference" << endl;
-          return -1;
-        }
-
-      HelloWorld_var hw = hh->create ();
-
-      if  (CORBA::is_nil (hw.in ()))
-        {
-          cerr << "Fail to create a HelloWorld reference" << endl;
-          return -1;
-        }
-
-      CORBA::String_var name = CORBA::string_dup ("Frodo");
-      CORBA::String_var hi = hw->sayhello (name
-                                           ACE_ENV_ARG_PARAMETER);
+      Components::HomeFinder_var home_finder =
+        Components::HomeFinder::_narrow (obj ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      cout << hi.in () << endl;
+      if  (CORBA::is_nil (home_finder.in ()))
+        ACE_ERROR_RETURN ((LM_ERROR, "Can't resolve initial HomeFinder reference\n"), -1);
 
-      hh->remove_component (hw
-                            ACE_ENV_ARG_PARAMETER);
+      // Now get the HelloHome
+
+      obj = home_finder->find_home_by_name ("HelloHome"
+                                            ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-#if 0
-      HelloWorld_var hw2 = hh->create ();
-
-      if  (CORBA::is_nil (hw2.in ()))
-        {
-          cerr << "Fail to create a HelloWorld reference" << endl;
-          return -1;
-        }
-
-      hi = hw->sayhello (name
-                         ACE_ENV_ARG_PARAMETER);
+      invoke (obj, "Frodo Baggins using 'find_home_by_name'"
+              ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
-#endif
+
+      obj = home_finder->find_home_by_home_type
+        ("IDL:omg.org/HelloHome:1.0"
+         ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      invoke (obj, "Samwise Gamgee using 'find_home_by_home_type'"
+              ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      obj = home_finder->find_home_by_component_type
+        ("IDL:omg.org/HelloWorld:1.0"
+         ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      invoke (obj, "Meriadoc Brandybuck using 'find_home_by_component_type'"
+              ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
     }
   ACE_CATCHANY
     {
