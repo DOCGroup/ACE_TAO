4bc8d4f9f7cd7733e632d6a4ee9a46632cb50e90
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-12-18 23:27:22 +0000

    ChangeLogTag:Wed Dec 18 17:12:04 2002  Nanbor Wang  <nanbor@cs.wustl.edu>

---
+++ ace/Hash_Map_Manager_RT.cpp
@@ -0,0 +1,18 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    ace
+//
+// = FILENAME
+//    Hash_Map_Manager_RT.cpp
+//
+// = AUTHOR
+//    Jeremy M Nolle <jmn3@cs.wustl.edu>
+//
+// ============================================================================
+
+#include "ace/Hash_Map_Manager_RT.h"
+
+ACE_RCSID(ace, Hash_Map_Manager_RT, "$Id$")
---
+++ ace/Hash_Map_Manager_RT.h
@@ -0,0 +1,29 @@
+/* -*- C++ -*- */
+
+//=============================================================================
+/**
+ *  @file    Hash_Map_Manager_RT.h
+ *
+ *  $Id$
+ *
+ *  @author Jeremy M Nolle <jmn3@cs.wustl.edu>
+ */
+//=============================================================================
+
+#ifndef ACE_HASH_MAP_MANAGER_RT_H
+#define ACE_HASH_MAP_MANAGER_RT_H
+
+#include "ace/pre.h"
+
+#include "ace/config-all.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+// Include the templates here.
+#include "ace/Hash_Map_Manager_RT_T.h"
+
+#include "ace/post.h"
+
+#endif /* ACE_HASH_MAP_MANAGER_RT_H */
