081dad836e12f54dd48a434196afa81c1446fa42
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2002-12-30 21:29:38 +0000

    *** empty log message ***

---
+++ TAO/examples/RTScheduling/fixed_priority_scheduler/Thread_Task.cpp
@@ -0,0 +1,85 @@
+//$Id$
+
+#include "Thread_Task.h"
+
+#include "ace/Atomic_Op.h"
+
+ACE_Atomic_Op<ACE_Thread_Mutex, long> thread_count = 0;
+
+Thread_Task::Thread_Task (RTScheduling::Current_ptr current)
+{
+  this->current_ = RTScheduling::Current::_narrow (current);
+}
+
+int
+Thread_Task::activate_task (CORBA::Policy_ptr sched_param,
+			    long flags)
+{
+  this->sched_param_ = CORBA::Policy::_duplicate (sched_param);
+
+  if (this->ACE_Task <ACE_SYNCH>::activate (flags,
+					    1) == -1)
+    {
+      if (ACE_OS::last_error () == EPERM)
+	ACE_ERROR_RETURN ((LM_ERROR,
+			   ACE_TEXT ("Insufficient privilege to run this test.\n")),
+			  -1);
+    }
+  
+  return 0;
+}
+
+
+int
+Thread_Task::svc (void)
+{
+
+  ACE_DEBUG ((LM_DEBUG,
+	      "After Thread_Task::svc \n"));
+      
+
+  thread_count++;
+
+  const char * name = 0;
+  CORBA::Policy_ptr implicit_sched_param = 0;
+  this->current_->begin_scheduling_segment (name,
+					    this->sched_param_.in (),
+					    implicit_sched_param
+					    ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+  
+  ACE_OS::sleep (10);
+
+  for (int i = 0; i < 100; i++)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+		  "%d\n", 
+		  thread_count.value_i ()));
+      
+      ACE_OS::sleep (2);
+      
+    }
+  /*
+  //Start - Nested Scheduling Segment
+  this->current_->begin_scheduling_segment (name,
+					    sched_param,
+					    implicit_sched_param
+					    ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+  
+  this->current_->end_scheduling_segment (name
+					  ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+  //End - Nested Scheduling Segment
+
+  */
+
+  this->current_->end_scheduling_segment (name
+					  ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+
+  return 0;
+}
+
+
+
--- TAO/examples/RTScheduling/fixed_priority_scheduler/test.cpp
+++ TAO/examples/RTScheduling/fixed_priority_scheduler/test.cpp
@@ -11,9 +11,9 @@ check_supported_priorities (void)
   // Check that we have sufficient priority range to run this
   // test, i.e., more than 1 priority level.
   int max_priority =
-    ACE_Sched_Params::priority_max (ACE_SCHED_OTHER);
+    ACE_Sched_Params::priority_max (ACE_SCHED_RR);
   int min_priority =
-    ACE_Sched_Params::priority_min (ACE_SCHED_OTHER);
+    ACE_Sched_Params::priority_min (ACE_SCHED_RR);
 
   if (max_priority == min_priority)
     {
@@ -38,7 +38,7 @@ main (int argc, char* argv [])
 			 ACE_ENV_ARG_PARAMETER);
       ACE_CHECK_RETURN (-1);
 
-      check_supported_priorities ();
+      //check_supported_priorities ();
 
       CORBA::Object_ptr manager_obj = orb->resolve_initial_references ("RTSchedulerManager"
 								       ACE_ENV_ARG_PARAMETER);
@@ -61,9 +61,44 @@ main (int argc, char* argv [])
 	RTScheduling::Current::_narrow (object.in () ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      RTCORBA::Priority desired_priority = 2;
+      
+      long flags = THR_NEW_LWP | THR_JOINABLE;
+      flags |= 
+	orb->orb_core ()->orb_params ()->scope_policy () |
+	orb->orb_core ()->orb_params ()->sched_policy ();
+      
+      Thread_Task task (current.in ());
+		
+      int min_priority = ACE_Sched_Params::priority_min (ACE_SCHED_RR);
+      int max_priority = ACE_Sched_Params::priority_max (ACE_SCHED_RR);				
+
+
+      ACE_DEBUG ((LM_DEBUG, "max_priority = %d, min_priority = %d\n",
+                  max_priority, min_priority));
+      
+      // Set the main thread to min priority...
+      int priority = max_priority;
+      
+      if (ACE_OS::sched_params (ACE_Sched_Params (ACE_SCHED_RR,
+						  priority,
+						  ACE_SCOPE_PROCESS)) != 0)
+	{
+	  if (ACE_OS::last_error () == EPERM)
+	    {
+	      ACE_DEBUG ((LM_DEBUG,
+			  "(%P|%t): user is not superuser, "
+			  "test runs in time-shared class\n"));
+	    }
+	  else
+	    ACE_ERROR_RETURN ((LM_ERROR,
+			       "(%P|%t): sched_params failed\n"),-1);
+	}
+      
+      
+      /*
+      RTCORBA::Priority desired_priority = 99;
       current->the_priority (desired_priority
-				    ACE_ENV_ARG_PARAMETER);
+			     ACE_ENV_ARG_PARAMETER);
       
       CORBA::Short priority = 
 	current->the_priority (ACE_ENV_SINGLE_ARG_PARAMETER);
@@ -76,30 +111,29 @@ main (int argc, char* argv [])
 	  return 0;
 	}
       
+      */
       
-      long flags = THR_NEW_LWP | THR_JOINABLE;
-      flags |= 
-	orb->orb_core ()->orb_params ()->scope_policy () |
-	orb->orb_core ()->orb_params ()->sched_policy ();
-
-	  Thread_Task task (current.in ());
-						
-
       FP_Scheduling::SegmentSchedulingParameterPolicy_ptr sched_param =
-	scheduler.create_segment_scheduling_parameter (1);
+	scheduler.create_segment_scheduling_parameter (20);
       
       task.activate_task (sched_param,
 			  flags);
 
-      ACE_OS::sleep (10);
+      ACE_DEBUG ((LM_DEBUG,
+		  "After activate task\n"));
+
+      //ACE_OS::sleep (10);
       
       sched_param =
-	scheduler.create_segment_scheduling_parameter (0);
+	scheduler.create_segment_scheduling_parameter (80);
       
       task.activate_task (sched_param,
 			  flags);
       
-      ACE_OS::sleep (10);
+      //ACE_OS::sleep (10);
+
+      ACE_DEBUG ((LM_DEBUG,
+		  "Threads Activated\n"));
 
       orb->run ();
     }
