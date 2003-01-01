155c3b52f2415148a4882c9383a1abed5d5970dc
bala <balanatarajan@users.noreply.github.com>
2003-01-01 19:21:11 +0000

    ChangeLogTag: Wed Jan  1 14:26:09 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- bin/topinfo_stats.sh
+++ bin/topinfo_stats.sh
@@ -4,17 +4,23 @@
 #
 
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 [ROOT] [DEST]"
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 [ROOT] [DEST] [USER]"
   exit 0
 fi
 
 ROOT=$1
 DEST=$2
+US=$3
+
 DATE=`date +%Y/%m/%d-%H:%M`
 cd $ROOT
 ACE_ROOT=$ROOT
 export ACE_ROOT
+LD_LIBRARY_PATH=$ACE_ROOT/ace
+export LD_LIBRARY_PATH
+PATH=/usr/bin:$PATH
+export PATH
 cd TAO/performance-tests/Memory/Single_Threaded
 # start the server
 ./server &
@@ -27,7 +33,7 @@ if test -f $file
     then
     # Just get the size as soon the server is started, ie. the vanilla
     # server.  
-    s_up=`top -p $s_id -n 1 | grep $USER| awk '{print $5}'`;
+    s_up=`top -p $s_id -n 1 -b | grep $US| awk '{print $5}'`;
 
     # Write it a file
     echo $DATE $s_up >> $DEST/source/st_start_size.txt
@@ -37,8 +43,7 @@ if test -f $file
     # Wait till all the invocations are done
     sleep 10;
     # Get the size once the client has made sufficient invocations. 
-    s_invocations=`top -p $s_id -n 1 | grep $USER| awk '{print $5}'`;
-    cd $ROOT
+    s_invocations=`top -p $s_id -n 1 -b | grep $US| awk '{print $5}'`;
     
     echo $DATE $s_invocations >> $DEST/source/st_after_invoke_size.txt
 
@@ -50,9 +55,10 @@ if test -f $file
     sleep 5;
 
     # Get the size once the client is killed or crashed
-    s_client_death=`top -p $s_id -n 1 | grep $USER| awk '{print $5}'`;
+    s_client_death=`top -p $s_id -n 1 -b | grep $US| awk '{print $5}'`;
     echo $DATE $s_client_death  >> $DEST/source/st_after_peer_death_size.txt
     kill -9 $s_id;
+    rm -f $file
 else
     echo $file doesnt exist
 fi
