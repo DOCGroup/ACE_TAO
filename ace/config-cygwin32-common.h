41e314135d32d5b4be42ee3a9301becee2d0246d
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-01-02 22:39:17 +0000

    ChangeLogTag: Thu Jan  2 17:44:29 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- ace/config-cygwin32-common.h
+++ ace/config-cygwin32-common.h
@@ -22,6 +22,15 @@
 # define ACE_IOV_MAX 64
 #endif /* ACE_IOV_MAX */
 
+// Define custom export macros for export/import of symbols from/of dll's
+#define ACE_HAS_CUSTOM_EXPORT_MACROS
+#define ACE_Proper_Export_Flag __declspec (dllexport)
+#define ACE_Proper_Import_Flag __declspec (dllimport)
+#define ACE_EXPORT_SINGLETON_DECLARATION(T) template class __declspec (dllexport) T
+#define ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) template class __declspec (dllexport) SINGLETON_TYPE<CLASS, LOCK>;
+#define ACE_IMPORT_SINGLETON_DECLARATION(T) extern template class T
+#define ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) extern template class SINGLETON_TYPE <CLASS, LOCK>;
+
 #define ACE_LACKS_UNIX_DOMAIN_SOCKETS
 #define ACE_LACKS_SYSV_MSG_H
 #define ACE_HAS_SIG_MACROS
