cb3f208dd24af4edf0a3962ec866000cc340442f
bala <balanatarajan@users.noreply.github.com>
2003-01-06 22:11:36 +0000

    ChangeLogTag: Mon Jan  6 17:12:40 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

---
+++ TAO/performance-tests/Memory/IORsize/Foo_i.h
@@ -0,0 +1,30 @@
+//
+// $Id$
+//
+#ifndef FOO_I_H
+#define FOO_I_H
+#include "ace/pre.h"
+
+#include "TestS.h"
+
+#if defined (_MSC_VER)
+# if (_MSC_VER >= 1200)
+#  pragma warning(push)
+# endif /* _MSC_VER >= 1200 */
+# pragma warning (disable:4250)
+#endif /* _MSC_VER */
+
+/// Implement the Test::Hello interface
+class Foo_i
+  : public virtual POA_Test::Foo
+  , public virtual PortableServer::RefCountServantBase
+{
+public:
+  Foo_i (CORBA::ORB_ptr p)
+    : orb_ (CORBA::ORB::_duplicate (p)) {}
+
+private:
+  CORBA::ORB_var orb_;
+};
+
+#endif /*FOO_I_H*/
