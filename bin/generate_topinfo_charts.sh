c9de7a81177a6f2056b954026eab71fc11b6fe86
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-01-06 23:24:56 +0000

    ChangeLogTag: Mon Jan  6 18:31:09 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- bin/generate_topinfo_charts.sh
+++ bin/generate_topinfo_charts.sh
@@ -3,13 +3,12 @@
 # $Id$
 #
 
-gnuplot <<_EOF_ >/tmp/null.bala 2>&1
+gnuplot <<_EOF_ >/dev/null 2>&1
   set xdata time
   set timefmt '%Y/%m/%d-%H:%M'
   set xlabel 'Date (MM/DD)'
   set ylabel 'Size (KBytes)'
   set terminal png small color
-  set yrange [3000:6000]    
   set output "$2"
   plot '$1' using 1:2 title '$3' w l
   exit
