91dc704cb622c982816c0b46bacc8a04cd5ff953
Johnny Willemsen <jwillemsen@remedy.nl>
2003-01-13 12:55:27 +0000

    ChangeLogTag: Mon Jan 13 12:50:12 UTC 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- ace/config-win32-common.h
+++ ace/config-win32-common.h
@@ -85,7 +85,7 @@
 //  #endif
 
 // Define the special export macros needed to export symbols outside a dll
-#if !defined (__MINGW32__) && !defined(__BORLANDC__)
+#ifndef __BORLANDC__
 #define ACE_HAS_CUSTOM_EXPORT_MACROS
 #define ACE_Proper_Export_Flag __declspec (dllexport)
 #define ACE_Proper_Import_Flag __declspec (dllimport)
@@ -93,7 +93,7 @@
 #define ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) template class __declspec (dllexport) SINGLETON_TYPE<CLASS, LOCK>;
 #define ACE_IMPORT_SINGLETON_DECLARATION(T) extern template class T
 #define ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) extern template class SINGLETON_TYPE <CLASS, LOCK>;
-#endif /* !__MINGW32__ */
+#endif /* !__BORLANDC__ */
 
 // Define ACE_HAS_WINSOCK2 to 0 in your config.h file if you do *not*
 // want to compile with WinSock 2.0.
