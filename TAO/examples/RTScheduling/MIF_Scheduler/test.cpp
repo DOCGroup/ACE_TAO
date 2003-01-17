b58ef5148b3a3ed6bb6302fdcda4e7e467a78beb
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-01-17 00:44:59 +0000

    *** empty log message ***

---
+++ TAO/examples/RTScheduling/MIF_Scheduler/test.cpp
@@ -0,0 +1,183 @@
+//$Id$
+
+#include "test.h"
+#include "tao/RTScheduling/RTScheduler_Manager.h"
+#include "tao/ORB_Core.h"
+#include "ace/Arg_Shifter.h"
+#include "MIF_DT_Creator.h"
+#include "../Thread_Task.h"
+#include "../Task_Stats.h"
+
+
+DT_Test::DT_Test (void)
+{
+}	
+
+void
+DT_Test::check_supported_priorities (void)
+{
+  // Check that we have sufficient priority range to run this
+  // test, i.e., more than 1 priority level.
+
+  this->thr_sched_policy_ = orb_->orb_core ()->orb_params ()->sched_policy ();
+  this->thr_scope_policy_ = orb_->orb_core ()->orb_params ()->scope_policy ();
+
+  if (thr_sched_policy_ == THR_SCHED_RR)
+    {
+      //if (TAO_debug_level > 0)
+      ACE_DEBUG ((LM_DEBUG, "Sched policy = THR_SCHED_RR\n"));
+
+      sched_policy_ = ACE_SCHED_RR;
+    }
+  else 
+  if (thr_sched_policy_ == THR_SCHED_FIFO)
+    {
+      // if (TAO_debug_level > 0)
+        ACE_DEBUG ((LM_DEBUG, "Sched policy = THR_SCHED_FIFO\n"));
+
+      sched_policy_ = ACE_SCHED_FIFO;
+    }
+  else
+    {
+      if (TAO_debug_level > 0)
+        ACE_DEBUG ((LM_DEBUG, "Sched policy = THR_SCHED_OTHER\n"));
+      
+      sched_policy_ = ACE_SCHED_OTHER;
+    }
+
+  max_priority_ = ACE_Sched_Params::priority_max (sched_policy_);
+  min_priority_ = ACE_Sched_Params::priority_min (sched_policy_);
+
+  if (max_priority_ == min_priority_)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  "Not enough priority levels on this platform"
+                  " to run the test, aborting \n"));
+      ACE_OS::exit (2);
+    }
+  else ACE_DEBUG ((LM_DEBUG, "max_priority = %d, min_priority = %d\n",
+		   max_priority_, min_priority_));
+}
+
+int
+DT_Test::init (int argc, char *argv []
+	       ACE_ENV_ARG_DECL)
+{
+  orb_ = CORBA::ORB_init (argc,
+			  argv,
+			  ""
+			  ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+  
+  this->check_supported_priorities ();
+  
+  CORBA::Object_ptr manager_obj = orb_->resolve_initial_references ("RTSchedulerManager"
+								   ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK_RETURN (-1);
+      
+  TAO_RTScheduler_Manager_var manager = TAO_RTScheduler_Manager::_narrow (manager_obj
+									  ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+      
+  
+  ACE_NEW_RETURN (scheduler_,
+	   Fixed_Priority_Scheduler (orb_.in ()), -1);
+  
+  manager->rtscheduler (scheduler_);
+  
+  CORBA::Object_var object =
+    orb_->resolve_initial_references ("RTScheduler_Current" 
+				      ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+  
+  current_  =
+    RTScheduling::Current::_narrow (object.in () ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+  
+
+  // Set the main thread to max priority...
+  if (ACE_OS::sched_params (ACE_Sched_Params (sched_policy_,
+					      max_priority_,
+					      ACE_SCOPE_PROCESS)) != 0)
+    {
+      if (ACE_OS::last_error () == EPERM)
+	{
+	  ACE_DEBUG ((LM_DEBUG,
+		      "(%P|%t): user is not superuser, "
+		      "test runs in time-shared class\n"));
+	}
+      else
+	ACE_ERROR ((LM_ERROR,
+			   "(%P|%t): sched_params failed\n"));
+    }
+
+  
+  return 0;
+}
+
+void
+DT_Test::run (int argc, char* argv [] 
+			  ACE_ENV_ARG_DECL)
+{
+  init (argc,argv
+	ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+
+  TASK_STATS::instance ()->init (this->dt_creator_->dt_count () * 100);
+  
+  dt_creator_->create_distributable_threads (orb_,
+					     current_
+					     ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+
+  orb_->run (ACE_ENV_SINGLE_ARG_PARAMETER);
+  ACE_CHECK;
+}
+
+
+void
+DT_Test::dt_creator (MIF_DT_Creator* dt_creator)
+{
+  this->dt_creator_ = dt_creator;
+}
+
+
+Fixed_Priority_Scheduler* 
+DT_Test::scheduler (void)
+{
+  return this->scheduler_;
+}
+
+
+int
+main (int argc, char* argv [])
+{
+  ACE_TRY_NEW_ENV
+    {
+      ACE_Service_Config::static_svcs ()->insert (&ace_svc_desc_MIF_DT_Creator);
+      
+      DT_TEST::instance ()->run (argc, argv
+				 ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Caught exception:");
+      return 1;
+    }
+  ACE_ENDTRY; 
+  
+  return 0;
+}
+
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+
+template class ACE_Singleton<DT_Test, ACE_Null_Mutex>;
+
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+
+#pragma instantiate ACE_Singleton<DT_Test, ACE_Null_Mutex>
+
+#endif /*ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
---
+++ TAO/examples/RTScheduling/MIF_Scheduler/test.h
@@ -0,0 +1,49 @@
+//$Id$
+#ifndef TEST_H
+#define TEST_H
+#include "ace/pre.h"
+
+
+#include "MIF_Scheduler.h"
+#include "ace/Sched_Params.h"
+#include "tao/ORB_Core.h"
+#include "ace/Synch.h"
+#include "MIF_DT_Creator.h"
+
+class Task_Stats;
+
+class DT_Test 
+{
+ public:
+  
+  DT_Test (void);
+  
+  void check_supported_priorities (void);
+  
+  int init (int argc, char *argv []
+	    ACE_ENV_ARG_DECL_WITH_DEFAULTS);
+
+  void run (int argc, char *argv [] 
+	  ACE_ENV_ARG_DECL_WITH_DEFAULTS);
+
+  void dt_creator (MIF_DT_Creator* dt_creator);
+
+  Fixed_Priority_Scheduler* scheduler (void);
+
+ private:
+  /// = Policies
+  long thr_sched_policy_;
+  long thr_scope_policy_;
+  ACE_Sched_Params::Policy sched_policy_;
+  CORBA::ORB_var orb_;
+  int max_priority_;
+  int min_priority_;
+  RTScheduling::Current_var current_; 
+  MIF_DT_Creator* dt_creator_;
+  MIF_Scheduler* scheduler_;
+};
+
+typedef ACE_Singleton <DT_Test, ACE_Thread_Mutex> DT_TEST;
+
+#include "ace/post.h"
+#endif /*TEST_H*/
