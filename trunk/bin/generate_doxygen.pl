ed7b56845844ff2097f78134fec1b4bb67079ab5
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-01-21 13:38:59 +0000

    ChangeLogTag: Tue Jan 21 08:43:24 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- trunk/bin/generate_doxygen.pl
+++ trunk/bin/generate_doxygen.pl
@@ -185,16 +185,6 @@ sub generate_doxy_files {
     rename $name_with_whitespace, $name_without_whitespace;
   }
   close FIND;
-  
-  open(FIND, "find man -type f -print |") or die "Can't run find\n";
-  while (<FIND>) {
-    chop;
-    my $name_with_whitespace = $_;
-    next unless ($name_with_whitespace =~ /\s/);
-    my $name_without_whitespace = $name_with_whitespace;
-    $name_without_whitespace =~ s/\s+//g;
-    rename $name_with_whitespace, $name_without_whitespace;
-  }
 }
 
 sub run_doxy {
