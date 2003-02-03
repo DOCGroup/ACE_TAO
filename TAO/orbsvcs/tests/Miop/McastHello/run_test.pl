298036ef0041063ef5185a6723f849d080b1ea79
fhunleth <fhunleth@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-02-03 02:07:55 +0000

    Sun Feb 02 21:01:12 2003  Frank Hunleth  <frank@hunleth.com>

--- TAO/orbsvcs/tests/Miop/McastHello/run_test.pl
+++ TAO/orbsvcs/tests/Miop/McastHello/run_test.pl
@@ -29,14 +29,14 @@ if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
     $SV1->Kill (); $SV1->TimedWait (1);
     $SV2->Kill (); $SV2->TimedWait (1);
     exit 1;
-} 
+}
 
 if (PerlACE::waitforfile_timed ($iorfile_server2, 5) == -1) {
     print STDERR "ERROR: cannot find file <$iorfile_server2>\n";
     $SV1->Kill (); $SV1->TimedWait (1);
     $SV2->Kill (); $SV2->TimedWait (1);
     exit 1;
-} 
+}
 
 $client = $CL->SpawnWaitKill (300);
 
