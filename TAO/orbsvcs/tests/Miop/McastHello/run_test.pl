d7ce14b2438a6d09726d1fb04f174598c5cbac85
fhunleth <fhunleth@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-02-03 02:07:05 +0000

    Sun Feb 02 21:01:12 2003  Frank Hunleth  <frank@hunleth.com>

--- TAO/orbsvcs/tests/Miop/McastHello/run_test.pl
+++ TAO/orbsvcs/tests/Miop/McastHello/run_test.pl
@@ -9,14 +9,16 @@ use lib '../../../../../bin';
 use PerlACE::Run_Test;
 
 $iorfile = PerlACE::LocalFile ("server.ior");
+$iorfile_server2 = PerlACE::LocalFile ("server.ior.unused");
 unlink $iorfile;
+unlink $iorfile_server2;
 $status = 0;
 
 $server_conf = PerlACE::LocalFile ("server$PerlACE::svcconf_ext");
 $client_conf = PerlACE::LocalFile ("client$PerlACE::svcconf_ext");
 
 $SV1 = new PerlACE::Process ("server", "-o $iorfile -ORBsvcconf $server_conf");
-$SV2 = new PerlACE::Process ("server", "-o $iorfile -ORBsvcconf $server_conf");
+$SV2 = new PerlACE::Process ("server", "-o $iorfile_server2 -ORBsvcconf $server_conf");
 $CL = new PerlACE::Process ("client", " -k file://$iorfile -ORBsvcconf $client_conf");
 
 $SV1->Spawn ();
@@ -24,6 +26,13 @@ $SV2->Spawn ();
 
 if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
     print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV1->Kill (); $SV1->TimedWait (1);
+    $SV2->Kill (); $SV2->TimedWait (1);
+    exit 1;
+} 
+
+if (PerlACE::waitforfile_timed ($iorfile_server2, 5) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile_server2>\n";
     $SV1->Kill (); $SV1->TimedWait (1);
     $SV2->Kill (); $SV2->TimedWait (1);
     exit 1;
@@ -51,5 +60,6 @@ if ($server != 0) {
 }
 
 unlink $iorfile;
+unlink $iorfile_server2;
 
 exit $status;
