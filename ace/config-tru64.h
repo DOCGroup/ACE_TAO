3bb4a6965a56e8360d5f4b41e7c08570ca594626
seibel_r <seibel_r@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-02-06 22:07:38 +0000

    Thu Feb  6 13:50:43 2003  Rich Seibel <seibel_r@ociweb.com>

--- ace/config-tru64.h
+++ ace/config-tru64.h
@@ -12,11 +12,14 @@
 #if defined (DIGITAL_UNIX)
 # include "ace/config-osf1-4.0.h"
 # define ACE_HAS_NONSTATIC_OBJECT_MANAGER
+# if DIGITAL_UNIX >= 0x500
 // It's possible that snprintf() is available on earlier Tru64 versions,
 // but this is the only way it's been tested.  Steve Huston, 22-Jan-2003
+// It is not on 4.0F.  Rich Seibel, 6 Feb 2003
 # ifndef ACE_HAS_SNPRINTF
 #  define ACE_HAS_SNPRINTF
 # endif /* ACE_HAS_SNPRINTF */
+# endif /* DIGITAL_UNIX >= 0x500 */
 # if DIGITAL_UNIX >= 0x40D
 #   define ACE_LACKS_SYSTIME_H
 # endif /* DIGITAL_UNIX >= 0x40D */
