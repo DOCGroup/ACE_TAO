636fe014ae93d5dcb7a3491c969eefea67d92d54
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-02-07 22:55:20 +0000

    *** empty log message ***

--- TAO/tests/RTScheduling/Current/Thread_Task.cpp
+++ TAO/tests/RTScheduling/Current/Thread_Task.cpp
@@ -3,9 +3,13 @@
 #include "Thread_Task.h"
 
 int
-Thread_Task::activate_task (CORBA::ORB_ptr orb)
+Thread_Task::activate_task (CORBA::ORB_ptr orb,
+			    int thr_count)
 {
   ACE_DECLARE_NEW_CORBA_ENV;
+  shutdown_lock_ = new ACE_Lock_Adapter <TAO_SYNCH_MUTEX>;
+  active_thread_count_ = thr_count;
+
   this->orb_ = CORBA::ORB::_duplicate (orb);
   
   CORBA::Object_ptr current_obj = this->orb_->resolve_initial_references ("RTScheduler_Current");
@@ -16,7 +20,7 @@ Thread_Task::activate_task (CORBA::ORB_ptr orb)
   
   long flags = THR_NEW_LWP | THR_JOINABLE;
   if (this->ACE_Task <ACE_SYNCH>::activate (flags,
-					    4) == -1)
+					    thr_count) == -1)
     {
       if (ACE_OS::last_error () == EPERM)
 	ACE_ERROR_RETURN ((LM_ERROR,
@@ -29,35 +33,102 @@ Thread_Task::activate_task (CORBA::ORB_ptr orb)
 int
 Thread_Task::svc (void)
 {
-  ACE_DECLARE_NEW_CORBA_ENV;
-  const char * name = 0;
-  CORBA::Policy_ptr sched_param = 0;
-  CORBA::Policy_ptr implicit_sched_param = 0;
+  ACE_TRY_NEW_ENV
+    {
+      const char * name = 0;
+      CORBA::Policy_ptr sched_param = 0;
+      CORBA::Policy_ptr implicit_sched_param = 0;
+
+      ACE_TRY
+	{
+	  this->current_->end_scheduling_segment (name
+						  ACE_ENV_ARG_PARAMETER);
+	  ACE_TRY_CHECK;
+	}
+      ACE_CATCHANY
+	{
+	  ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+			       "End Scheduling Segment is out of context");
+	}
+      ACE_ENDTRY;
+
+      ACE_TRY
+	{
+	  this->current_->update_scheduling_segment (name,
+						     sched_param,
+						     implicit_sched_param
+						     ACE_ENV_ARG_PARAMETER);
+	  ACE_TRY_CHECK;
+	}
+      ACE_CATCHANY
+	{
+	  ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+			       "Update Scheduling Segment is out of context:");
+	}
+      ACE_ENDTRY;
+      
+      this->current_->begin_scheduling_segment ("Fellowship of the Rings",
+						sched_param,
+						implicit_sched_param
+						ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      //Start - Nested Scheduling Segment
+      this->current_->begin_scheduling_segment ("Two Towers",
+						sched_param,
+						implicit_sched_param
+						ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      int count ;
+      ACE_OS::memcpy (&count,
+		      this->current_->id ()->get_buffer (),
+		      this->current_->id ()->length ());
   
-  this->current_->begin_scheduling_segment (name,
-					    sched_param,
-					    implicit_sched_param
-					    ACE_ENV_ARG_PARAMETER);
-  ACE_CHECK_RETURN (-1);
+      ACE_DEBUG ((LM_DEBUG,
+		  "The Current DT Guid is %d\n",
+		  count));
 
-  //Start - Nested Scheduling Segment
-  this->current_->begin_scheduling_segment (name,
-					    sched_param,
-					    implicit_sched_param
-					    ACE_ENV_ARG_PARAMETER);
-  ACE_CHECK_RETURN (-1);
+      RTScheduling::Current::NameList* segment_name_list = this->current_->current_scheduling_segment_names (ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      
+      ACE_DEBUG ((LM_DEBUG,
+		  "Segment Names:\n"));
+
+      for (int i = 0; i < segment_name_list->length (); i ++)
+	{
+	  ACE_DEBUG ((LM_DEBUG,
+		      "%s\n",
+		      CORBA::string_dup ((*segment_name_list) [i])));
+	}
+
+      this->current_->end_scheduling_segment (name
+					      ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      //End - Nested Scheduling Segment
   
-  this->current_->end_scheduling_segment (name
-					  ACE_ENV_ARG_PARAMETER);
-  ACE_CHECK_RETURN (-1);
-  //End - Nested Scheduling Segment
+      this->current_->end_scheduling_segment (name
+					      ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
 
-  this->current_->end_scheduling_segment (name
-					  ACE_ENV_ARG_PARAMETER);
-  ACE_CHECK_RETURN (-1);
-
-  return 0;
+      {
+	ACE_GUARD_RETURN (ACE_Lock, ace_mon, *shutdown_lock_,-1); 
+	--active_thread_count_;
+	if (active_thread_count_ == 0)
+	  orb_->shutdown ();
+	return 0;
+      }
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+			   "Caught exception:");
+      return -1;
+    }
+  ACE_ENDTRY;
 }
+
+
 
 
 
--- TAO/tests/RTScheduling/Current/Thread_Task.h
+++ TAO/tests/RTScheduling/Current/Thread_Task.h
@@ -9,7 +9,8 @@
 class Thread_Task : public ACE_Task <ACE_SYNCH>
 {
  public:
-  int activate_task (CORBA::ORB_ptr orb);
+  int activate_task (CORBA::ORB_ptr orb,
+		     int thr_count);
 
  protected:
   /// task svc
@@ -17,6 +18,8 @@ class Thread_Task : public ACE_Task <ACE_SYNCH>
  private:
   CORBA::ORB_var orb_;
  RTScheduling::Current_var current_;
+ int active_thread_count_;
+ ACE_Lock* shutdown_lock_;
 };
 
 #endif /* THREAD_TASK_H */
