557269db079608d87bfc7186286e649669aa97b6
Johnny Willemsen <jwillemsen@remedy.nl>
2003-02-12 09:01:07 +0000

    ChangeLogTag: Wed Feb 13 08:58:12 UTC 2002 Johnny Willemsen <jwillemsen@remedy.nl>

--- ace/UPIPE_Addr.h
+++ ace/UPIPE_Addr.h
@@ -24,22 +24,5 @@
 
 typedef ACE_SPIPE_Addr ACE_UPIPE_Addr;
 
-#if 0
-// We need this "class" to make the class2man documentation utility
-// happy.
-/**
- * @class ACE_UPIPE_Addr
- *
- * @brief Defines the ACE "user pipe" address family address format.
- *
- * This class has an identical interface to the <ACE_SPIPE_Addr>
- * class.  In fact, it's simply a typedef!
- */
-class ACE_Export ACE_UPIPE_Addr
-{
-public:
-  // = Same interface as <ACE_SPIPE_Addr>.
-};
-#endif /* 0 */
 #include "ace/post.h"
 #endif /* ACE_UPIPE_ADDR_H */
