b68a9ea9bb97aa2a70aea736a9976ea3af08c5b0
pradeep <pradeep@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-02-25 13:15:26 +0000

    *** empty log message ***

---
+++ branches/RT_Notify/TAO/orbsvcs/orbsvcs/Notify/Structured/RT_StructuredProxyPushSupplier.cpp
@@ -0,0 +1,44 @@
+// $Id$
+
+#include "RT_StructuredProxyPushSupplier.h"
+
+#if ! defined (__ACE_INLINE__)
+#include "RT_StructuredProxyPushSupplier.inl"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID(Notify, TAO_NS_RT_StructuredProxyPushSupplier, "$id$")
+
+TAO_NS_RT_StructuredProxyPushSupplier::TAO_NS_RT_StructuredProxyPushSupplier (void)
+{
+}
+
+TAO_NS_RT_StructuredProxyPushSupplier::~TAO_NS_RT_StructuredProxyPushSupplier ()
+{
+}
+
+CORBA::Object_ptr
+TAO_NS_RT_StructuredProxyPushSupplier::activate (PortableServer::Servant servant ACE_ENV_ARG_DECL)
+{
+  CORBA::Object_var object = TAO_NS_Object::activate (servant ACE_ENV_ARG_DECL);
+  ACE_CHECK_RETURN (CORBA::Object::_nil ());
+
+  // Obtain our ref.
+  CORBA::Object_var obj = this->ref (ACE_ENV_SINGLE_ARG_PARAMETER);
+  ACE_CHECK_RETURN (CORBA::Object::_nil ());
+
+  this->event_forwarder_ = Event_Forwarder::StructuredProxyPushSupplier::_narrow (obj.in () ACE_ENV_ARG_PARAMETER);
+
+  return object._retn ();
+}
+
+void
+TAO_NS_RT_StructuredProxyPushSupplier::push (const TAO_NS_Event_var &event)
+{
+  event->push (this->event_forwarder_.in () ACE_ENV_ARG_PARAMETER);
+}
+
+void
+TAO_NS_RT_StructuredProxyPushSupplier::push_no_filtering (const TAO_NS_Event_var &event)
+{
+  event->push_no_filtering (this->event_forwarder_.in () ACE_ENV_ARG_PARAMETER);
+}
