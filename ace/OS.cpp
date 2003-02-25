13f2cca2cb1d963f5d9fcd8dd0a13533c54413f5
schmidt <douglascraigschmidt@users.noreply.github.com>
2003-02-25 15:37:50 +0000

    ChangeLogTag:Tue Feb 25 09:24:06 2003  Douglas C. Schmidt  <schmidt@macarena.cs.wustl.edu>

--- ace/OS.cpp
+++ ace/OS.cpp
@@ -4804,6 +4804,55 @@ ACE_OS::localtime_r (const time_t *t, struct tm *res)
       *res = *res_ptr;
       return res;
     }
+#elif defined (ACE_HAS_WINCE) 
+  // This is really stupid, converting FILETIME to timeval back and
+  // forth.  It assumes FILETIME and DWORDLONG are the same structure
+  // internally.
+  
+  TIME_ZONE_INFORMATION pTz;
+ 
+  const unsigned short int __mon_yday[2][13] = 
+  {
+    /* Normal years.  */
+    { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 },
+    /* Leap years.  */
+    { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 }
+  };
+ 
+  ULARGE_INTEGER _100ns;
+  ::GetTimeZoneInformation (&pTz);
+ 
+  _100ns.QuadPart = (DWORDLONG) *t * 10000 * 1000 + ACE_Time_Value::FILETIME_to_timval_skew;
+  FILETIME file_time;
+  file_time.dwLowDateTime = _100ns.LowPart;
+  file_time.dwHighDateTime = _100ns.HighPart;
+   
+  FILETIME localtime;
+  SYSTEMTIME systime;
+  FileTimeToLocalFileTime (&file_time, &localtime);
+  FileTimeToSystemTime (&localtime, &systime);
+ 
+  res->tm_hour = systime.wHour;
+ 
+  if(pTz.DaylightBias!=0)
+    res->tm_isdst = 1;
+  else
+    res->tm_isdst = 1;
+ 
+   int iLeap;
+   iLeap = (res->tm_year % 4 == 0 && (res->tm_year% 100 != 0 || res->tm_year % 400 == 0));
+   // based on leap select which group to use
+   
+   res->tm_mday = systime.wDay;
+   res->tm_min = systime.wMinute;
+   res->tm_mon = systime.wMonth;
+   res->tm_sec = systime.wSecond;
+   res->tm_wday = systime.wDayOfWeek;
+   res->tm_yday = __mon_yday[iLeap][systime.wMonth] + systime.wDay;
+   res->tm_year = systime.wYear;// this the correct year but bias the value to start at the 1900
+   res->tm_year = res->tm_year - 1900;
+ 
+   return res;
 #else
   // @@ Same as ACE_OS::localtime (), you need to implement it
   //    yourself.
