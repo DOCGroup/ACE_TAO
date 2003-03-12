222e008f4531186433d414f9392225e08b3a884b
schmidt <douglascraigschmidt@users.noreply.github.com>
2003-03-12 15:45:52 +0000

    ChangeLogTag:Wed Mar 12 08:36:12 2003  Douglas C. Schmidt  <schmidt@tango.doc.wustl.edu>

--- ace/FILE.cpp
+++ ace/FILE.cpp
@@ -34,8 +34,13 @@ int
 ACE_FILE::close (void)
 {
   ACE_TRACE ("ACE_FILE::close");
-  int result = ACE_OS::close (this->get_handle ());
-  this->set_handle (ACE_INVALID_HANDLE);
+  int result = 0;
+
+  if (this->get_handle () != ACE_INVALID_HANDLE)
+    {
+      result = ACE_OS::close (this->get_handle ());
+      this->set_handle (ACE_INVALID_HANDLE);
+    }
   return result;
 }
 
