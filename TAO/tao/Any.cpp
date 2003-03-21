000736dcdda87888a054978fc0892cc3a6434304
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-03-21 19:24:06 +0000

    ChangeLogTag: Fri Mar 21 13:22:06 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/tao/Any.cpp
+++ TAO/tao/Any.cpp
@@ -12,6 +12,110 @@ ACE_RCSID (tao,
            Any,
            "$Id$")
 
+// Specializations of the create_empty method for long long and long double.
+
+template<>
+TAO::Any_Basic_Impl_T<CORBA::LongLong> *
+TAO::Any_Basic_Impl_T<CORBA::LongLong>::create_empty (
+    CORBA::TypeCode_ptr tc
+  )
+{
+  CORBA::LongLong zero = ACE_CDR_LONGLONG_INITIALIZER;
+  TAO::Any_Basic_Impl_T<CORBA::LongLong> * retval = 0;
+  ACE_NEW_RETURN (retval,
+                  TAO::Any_Basic_Impl_T<CORBA::LongLong> (tc,
+                                                          zero),
+                  0);
+  return retval;
+}
+
+template<>
+TAO::Any_Basic_Impl_T<CORBA::LongDouble> *
+TAO::Any_Basic_Impl_T<CORBA::LongDouble>::create_empty (
+    CORBA::TypeCode_ptr tc
+  )
+{
+  CORBA::LongDouble zero = ACE_CDR_LONG_DOUBLE_INITIALIZER;
+  TAO::Any_Basic_Impl_T<CORBA::LongDouble> * retval = 0;
+  ACE_NEW_RETURN (retval,
+                  TAO::Any_Basic_Impl_T<CORBA::LongDouble> (tc,
+                                                            zero),
+                  0);
+  return retval;
+}
+
+// =======================================================================
+
+// Specializations for CORBA::Exception
+
+template<>
+TAO::Any_Dual_Impl_T<CORBA::Exception>::Any_Dual_Impl_T (
+    _tao_destructor destructor,
+    CORBA::TypeCode_ptr tc,
+    const CORBA::Exception & val
+  )
+  : Any_Impl (destructor,
+              tc)
+{
+  this->value_ = val._tao_duplicate ();
+}
+
+template<>
+CORBA::Boolean
+TAO::Any_Dual_Impl_T<CORBA::Exception>::marshal_value (TAO_OutputCDR &cdr)
+{
+  ACE_TRY_NEW_ENV
+    {
+      this->value_->_tao_encode (cdr
+                                 ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      return 1;
+    }
+  ACE_CATCHANY
+    {
+    }
+  ACE_ENDTRY;
+  
+  return 0;
+}
+
+template<>
+CORBA::Boolean
+TAO::Any_Dual_Impl_T<CORBA::Exception>::demarshal_value (TAO_InputCDR &cdr)
+{
+  ACE_TRY_NEW_ENV
+    {
+      this->value_->_tao_decode (cdr
+                                 ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      return 1;
+    }
+  ACE_CATCHANY
+    {
+    }
+  ACE_ENDTRY;
+  
+  return 0;
+}
+
+// This should never get called since we don't have extraction operators
+// for CORBA::Exception, but it is here to sidestep the constructor call
+// in the unspecialized version that causes a problem with compilers that
+// require explicit instantiation
+template<>
+CORBA::Boolean
+TAO::Any_Dual_Impl_T<CORBA::Exception>::extract (const CORBA::Any &,
+                                                 _tao_destructor,
+                                                 CORBA::TypeCode_ptr,
+                                                 const CORBA::Exception *&)
+{
+  return 0;
+}
+
+// =======================================================================
+
 CORBA::Any::Any (void)
   : impl_ (0)
 {
@@ -1004,110 +1108,6 @@ operator>>= (const CORBA::Any &any, CORBA::TypeCode_ptr &tc)
       CORBA::_tc_TypeCode,
       tc
     );
-}
-
-// =======================================================================
-
-// Specializations of the create_empty method for long long and long double.
-
-template<>
-TAO::Any_Basic_Impl_T<CORBA::LongLong> *
-TAO::Any_Basic_Impl_T<CORBA::LongLong>::create_empty (
-    CORBA::TypeCode_ptr tc
-  )
-{
-  CORBA::LongLong zero = ACE_CDR_LONGLONG_INITIALIZER;
-  TAO::Any_Basic_Impl_T<CORBA::LongLong> * retval = 0;
-  ACE_NEW_RETURN (retval,
-                  TAO::Any_Basic_Impl_T<CORBA::LongLong> (tc,
-                                                          zero),
-                  0);
-  return retval;
-}
-
-template<>
-TAO::Any_Basic_Impl_T<CORBA::LongDouble> *
-TAO::Any_Basic_Impl_T<CORBA::LongDouble>::create_empty (
-    CORBA::TypeCode_ptr tc
-  )
-{
-  CORBA::LongDouble zero = ACE_CDR_LONG_DOUBLE_INITIALIZER;
-  TAO::Any_Basic_Impl_T<CORBA::LongDouble> * retval = 0;
-  ACE_NEW_RETURN (retval,
-                  TAO::Any_Basic_Impl_T<CORBA::LongDouble> (tc,
-                                                            zero),
-                  0);
-  return retval;
-}
-
-// =======================================================================
-
-// Specializations for CORBA::Exception
-
-template<>
-TAO::Any_Dual_Impl_T<CORBA::Exception>::Any_Dual_Impl_T (
-    _tao_destructor destructor,
-    CORBA::TypeCode_ptr tc,
-    const CORBA::Exception & val
-  )
-  : Any_Impl (destructor,
-              tc)
-{
-  this->value_ = val._tao_duplicate ();
-}
-
-template<>
-CORBA::Boolean
-TAO::Any_Dual_Impl_T<CORBA::Exception>::marshal_value (TAO_OutputCDR &cdr)
-{
-  ACE_TRY_NEW_ENV
-    {
-      this->value_->_tao_encode (cdr
-                                 ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      return 1;
-    }
-  ACE_CATCHANY
-    {
-    }
-  ACE_ENDTRY;
-  
-  return 0;
-}
-
-template<>
-CORBA::Boolean
-TAO::Any_Dual_Impl_T<CORBA::Exception>::demarshal_value (TAO_InputCDR &cdr)
-{
-  ACE_TRY_NEW_ENV
-    {
-      this->value_->_tao_decode (cdr
-                                 ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      return 1;
-    }
-  ACE_CATCHANY
-    {
-    }
-  ACE_ENDTRY;
-  
-  return 0;
-}
-
-// This should never get called since we don't have extraction operators
-// for CORBA::Exception, but it is here to sidestep the constructor call
-// in the unspecialized version that causes a problem with compilers that
-// require explicit instantiation
-template<>
-CORBA::Boolean
-TAO::Any_Dual_Impl_T<CORBA::Exception>::extract (const CORBA::Any &,
-                                                 _tao_destructor,
-                                                 CORBA::TypeCode_ptr,
-                                                 const CORBA::Exception *&)
-{
-  return 0;
 }
 
 // =======================================================================
