2240688a921c23cfcef7519d16a80a6e4e9be0e5
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-03-21 17:44:56 +0000

    ChangeLogTag: Fri Mar 21 11:41:10 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/tao/Policy_ForwardC.cpp
+++ TAO/tao/Policy_ForwardC.cpp
@@ -603,6 +603,13 @@ CORBA::Boolean operator>>= (
       );
 }
 
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)  || \
+    defined (ACE_HAS_GNU_REPO)
+  template class TAO::Any_Dual_Impl_T<CORBA::PolicyList>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+# pragma instantiate TAO::Any_Dual_Impl_T<CORBA::PolicyList>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
 // TAO_IDL - Generated from 
 // W:\ACE_wrappers\TAO\TAO_IDL\be\be_visitor_sequence/any_op_cs.cpp:54
 
