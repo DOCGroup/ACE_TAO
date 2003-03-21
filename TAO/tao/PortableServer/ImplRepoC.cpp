36f775fa3d6cce7b16d3cbbb3f6a15282d8a1f90
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-03-21 00:31:59 +0000

    ChangeLogTag: Thu Mar 20 18:29:10 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/tao/PortableServer/ImplRepoC.cpp
+++ TAO/tao/PortableServer/ImplRepoC.cpp
@@ -8336,6 +8336,13 @@ CORBA::Boolean operator>>= (
       );
 }
 
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)  || \
+    defined (ACE_HAS_GNU_REPO)
+  template class TAO::Any_Dual_Impl_T<ImplementationRepository::EnvironmentList>;
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+# pragma instantiate TAO::Any_Dual_Impl_T<ImplementationRepository::EnvironmentList>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
 // TAO_IDL - Generated from
 // W:\ACE_wrappers\TAO\TAO_IDL\be\be_visitor_enum/any_op_cs.cpp:52
 
