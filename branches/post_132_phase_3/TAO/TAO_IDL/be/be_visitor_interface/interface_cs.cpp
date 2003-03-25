a80ef7281cf289812bd6b13b7b4895a2bbec5017
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-03-25 22:04:43 +0000

    *** empty log message ***

--- branches/post_132_phase_3/TAO/TAO_IDL/be/be_visitor_interface/interface_cs.cpp
+++ branches/post_132_phase_3/TAO/TAO_IDL/be/be_visitor_interface/interface_cs.cpp
@@ -1,5 +1,6 @@
 //
 // $Id$
+
 //
 
 // ============================================================================
@@ -21,6 +22,7 @@
 ACE_RCSID (be_visitor_interface,
            interface_cs,
            "$Id$")
+
 
 // ************************************************************
 // Interface visitor for client stubs
@@ -775,8 +777,35 @@ be_visitor_interface_cs::gen_concrete_unchecked_narrow (be_interface *node,
       << "::_nil ();" << be_uidt_nl
       << "}" << be_uidt_nl << be_nl;
 
+
+
+
+
   if (! node->is_local ())
     {
+      // Declare the default proxy.
+      *os << bt->nested_type_name (this->ctx_->scope ())
+          << "_ptr default_proxy = "
+          << bt->nested_type_name (this->ctx_->scope ())
+          <<"::_nil ();" << be_nl << be_nl;
+
+      // Code for lzay evaluation..
+      *os << "// Code for lazily evaluated IOR's" << be_nl;
+
+      *os << "if (!obj->is_evaluated ())" << be_idt_nl
+          << "{" << be_idt_nl
+          << "ACE_NEW_RETURN (" << be_idt << be_idt_nl
+          << "default_proxy," << be_nl
+          << "::" <<  bt->name ()
+          << " (" << be_idt << be_idt_nl
+          << "obj->steal_ior ()," << be_nl
+          << "obj->orb_core ()" << be_uidt_nl << ")," << be_uidt_nl
+          << bt->nested_type_name (this->ctx_->scope ())
+          << "::_nil ()" << be_uidt_nl << ");" << be_nl
+          << "return default_proxy;" << be_nl
+          << be_uidt << be_uidt_nl
+          << "}" << be_uidt_nl << be_nl;
+
       // Remote _unchecked_narrow implementation.
       *os << "if (! obj->_is_local ())" << be_idt_nl
           << "{" << be_idt_nl
@@ -785,12 +814,6 @@ be_visitor_interface_cs::gen_concrete_unchecked_narrow (be_interface *node,
           << "{" << be_idt_nl
           << "stub->_incr_refcnt ();" << be_uidt_nl
           << "}" << be_uidt_nl << be_nl;
-
-      // Declare the default proxy.
-      *os << bt->nested_type_name (this->ctx_->scope ())
-          << "_ptr default_proxy = "
-          << bt->nested_type_name (this->ctx_->scope ())
-          <<"::_nil ();" << be_nl << be_nl;
 
       // If the policy didtates that the proxy be collocated, use the
       // function to create one.
@@ -851,7 +874,7 @@ be_visitor_interface_cs::gen_concrete_unchecked_narrow (be_interface *node,
       *os << be_idt;
     }
 
-  // Local _unchecked_narrow.
+  // Local  & lazily evaluated _unchecked_narrow.
   *os << "return" << be_idt_nl
       << "ACE_reinterpret_cast (" << be_idt << be_idt_nl
       << node->local_name () << "_ptr," << be_nl
