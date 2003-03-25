eed062861a2872bf06bd4b44c98050b89eee5a75
okellogg <okellogg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-03-25 16:19:17 +0000

    Tue Mar 25 17:09:22 CET 2003  Oliver Kellogg  <oliver.kellogg@sysde.eads.net>

--- trunk/tests/Unbounded_Set_Test.cpp
+++ trunk/tests/Unbounded_Set_Test.cpp
@@ -9,9 +9,8 @@
 //    Unbounded_Set_Test.cpp
 //
 // = DESCRIPTION
-//      This test shall evolve to illustrate the use of ACE_Unbounded_Set.
-//      For the moment it only tests some insertion and removal, and acts
-//      as a regression test for Bugzilla bug 1460.
+//      This test illustrates the use of ACE_Unbounded_Set.
+//      In addition, it acts as a regression test for Bugzilla bug 1460.
 //      No command line arguments are needed to run the test.
 //
 // = AUTHOR
@@ -20,29 +19,38 @@
 //
 // ============================================================================
 
-#include "test_config.h"
+// #include "test_config.h"
 #include <ace/Unbounded_Set.h>
 
 ACE_RCSID(tests, Unbounded_Set_Test, "$Id$")
 
 struct MyNode
 {
-  int k;
+  unsigned k;
   MyNode () : k (0) {}
   MyNode (int pk) : k (pk) {}
   MyNode (const MyNode& o) : k (o.k) {}
   bool operator== (const MyNode& o) { return (k == o.k); }
 };
 
+size_t count_const_set (const ACE_Unbounded_Set<MyNode>& cubs)
+{
+  size_t number_of_elements = 0;
+  for (ACE_Unbounded_Set<MyNode>::const_iterator ci (cubs); !ci.done(); ci++)
+    number_of_elements++;
+  return number_of_elements;
+}
+
 int main()
 {
   int r;
+  unsigned k;
   MyNode node (1);
   ACE_Unbounded_Set<MyNode> ubs;
 
   ACE_ASSERT (ubs.size () == 0);
 
-  // Insert a node. Immediately remove it.
+  // Insert a value. Immediately remove it.
   r = ubs.insert (node);
   ACE_ASSERT (r == 0);
   ACE_ASSERT (ubs.size () == 1);
@@ -50,19 +58,74 @@ int main()
   ACE_ASSERT (r == 0);
   ACE_ASSERT (ubs.size () == 0);
 
-  // Insert a node. Remove it within an iterator.
-  int n_iterations = 0;
-  r = ubs.insert (node);
-  ACE_Unbounded_Set<MyNode>::ITERATOR end = ubs.end ();
-  for (ACE_Unbounded_Set<MyNode>::ITERATOR i = ubs.begin (); i != end; i++)
+  // Insert several different values.
+  for (node.k = 1; node.k <= 5; node.k++)
     {
-      ACE_ASSERT (*i == node);
-      r = ubs.remove (*i);
-      n_iterations++;
+      r = ubs.insert (node);
+      ACE_ASSERT (r == 0);
+      ACE_ASSERT (ubs.size () == node.k);
     }
-  ACE_ASSERT (n_iterations == 1);
-  ACE_ASSERT (r == 0);
-  ACE_ASSERT (ubs.size () == 0);
+
+  // Test assigment of sets.
+  // To do that, we also test some of the iterator methods.
+  typedef ACE_Unbounded_Set<MyNode> MySet;
+  MySet ubs2 = ubs;              // Test a typedef of a set.
+  ACE_ASSERT (ubs2.size() == ubs.size());
+  {
+    MySet::ITERATOR it1 (ubs);
+    MySet::iterator it2 (ubs2);
+    for (k = 1; k <= 5; k++)
+      {
+        ACE_ASSERT (! it1.done ());
+        ACE_ASSERT (! it2.done ());
+        MyNode n1 = *it1;
+        MyNode n2 = *it2;
+        ACE_ASSERT (n1 == n2);
+        it1.advance ();
+        it2.advance ();
+      }
+    ACE_ASSERT (it1.done ());
+    ACE_ASSERT (it2.done ());
+  }
+
+  // Selective deletion of elements and element retrieval.
+  {
+    MySet::iterator end = ubs2.end();
+    int deleted = 0;
+    for (MySet::iterator i = ubs2.begin (); i != end; i++)
+      {
+        if ((*i).k % 2 == 1)
+          {
+            r = ubs2.remove (*i);
+            deleted++;
+          }
+      }
+    ACE_ASSERT (ubs2.size () + deleted == ubs.size());
+
+    MyNode node2 (2);
+    ACE_ASSERT (ubs2.find (node2) == 0);
+
+    MyNode node3 (3);
+    ACE_ASSERT (ubs2.find (node3) != 0);
+
+    ubs2.insert(node3);
+  }
+
+  size_t s = count_const_set (ubs);
+  ACE_ASSERT (s == ubs.size ());
+
+  // Test deletion under the cursor.
+  // This is the regression test for Bugzilla bug 1460.
+  {
+    MySet::iterator end = ubs2.end ();
+    for (MySet::iterator i = ubs2.begin (); i != end; i++)
+      {
+        r = ubs2.remove (*i);
+        ACE_ASSERT (r == 0);
+      }
+    ACE_ASSERT (ubs2.size () == 0);
+  }
+  ACE_ASSERT (ubs2.is_empty ());
 
   return 0;
 }
