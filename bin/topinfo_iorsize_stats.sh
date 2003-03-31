1066ebe3c308cca688a8e63d1fd90f2ce93b2d8c
bala <balanatarajan@users.noreply.github.com>
2003-03-31 03:56:37 +0000

    ChangeLogTag: Sun Mar 30 21:46:54 2003  Balachandran Natarajan  <bala@isis-server.isis.vanderbilt.edu>

--- bin/topinfo_iorsize_stats.sh
+++ bin/topinfo_iorsize_stats.sh
@@ -3,15 +3,15 @@
 # $Id$
 #
 
-
-if [ $# -lt 3 ]; then
-  echo "Usage: $0 [ROOT] [DEST] [USER]"
+if [ $# -lt 4 ]; then
+  echo "Usage: $0 [ROOT] [DEST] [USER] [OPTIMIZED]"
   exit 0
 fi
 
 ROOT=$1
 DEST=$2
 US=$3
+OPT=$4
 
 DATE=`date +%Y/%m/%d-%H:%M`
 cd $ROOT
@@ -22,9 +22,17 @@ export LD_LIBRARY_PATH
 PATH=/usr/bin:$PATH
 export PATH
 cd TAO/performance-tests/Memory/IORsize
-# start the server
-./server &
+
+# start the server. If OPT == 1 then start the optimized version, else
+# the non-optimized version
+
+if test $OPT == 1
+    then ./server -ORBSvcConf server.conf &
+    else ./server &
+fi
+
 s_id=$!;
+
 # Just sleep for 2 seconds.
 sleep 2;
 # Check whether the server has started 
@@ -38,9 +46,14 @@ if test -f $file
     sleep 30;
     # Get the size once the client has made sufficient invocations. 
     s_invocations=`top -p $s_id -n 1 -b | grep $US| awk '{print $5}'`;
-    
-    echo $DATE $s_invocations >> $DEST/source/server_ior_size.txt
-    
+
+    if test $OPT == 1
+        then 
+        echo $DATE $s_invocations >> $DEST/source/server_opt_ior_size.txt
+        else
+        echo $DATE $s_invocations >> $DEST/source/server_ior_size.txt
+    fi
+
     # Get teh size of the client after all the invocations
     c_invocations=`top -p $c_id -n 1 -b | grep $US| awk '{print $5}'`;
     echo $DATE $c_invocations >> $DEST/source/client_ior_size.txt
@@ -57,7 +70,7 @@ fi
 
 cd $DEST/source
 
-FILES="server client"
+FILES="server server_opt client"
 for i in $FILES ; do
 /usr/bin/tac ${i}_ior_size.txt > $DEST/data/${i}_ior_size.txt
 /usr/bin/tail -5 ${i}_ior_size.txt > $DEST/data/LAST_${i}_ior_size.txt
