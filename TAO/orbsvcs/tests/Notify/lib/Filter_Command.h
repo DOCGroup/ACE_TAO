b0799db23d5e3ddba1375d0df9e47e158e10099c
dsmith <dsmith@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-04-11 09:01:39 +0000

    Removed symbol clash on HPUX

--- TAO/orbsvcs/tests/Notify/lib/Filter_Command.h
+++ TAO/orbsvcs/tests/Notify/lib/Filter_Command.h
@@ -50,6 +50,10 @@ public:
 
 protected:
 
+#ifdef HPUX_11
+   // Remove clash with /usr/include/machine/cpu.h and /usr/include/pa/cpu.h
+#undef COMMAND
+#endif
   enum COMMAND
   {
     CREATE_FACTORY,
