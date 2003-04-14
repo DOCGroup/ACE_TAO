450a0edc492c2c35aa3a028ebbe2f497decb9d31
venkita <venkita@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-04-14 17:38:51 +0000

    *** empty log message ***

--- trunk/Kokyu/Kokyu.cpp
+++ trunk/Kokyu/Kokyu.cpp
@@ -42,4 +42,41 @@ create_dispatcher (const ConfigInfoSet& config_info_set)
   return disp;
 }
 
+ACE_INLINE void
+DSRT_Dispatcher::implementation (DSRT_Dispatcher_Impl* impl)
+{
+  dispatcher_impl_ = impl;
+}
+
+int
+DSRT_Dispatcher::schedule (guid_t guid, const QoSDescriptor& qos)
+{
+  return dispatcher_impl_->schedule (guid, qos);
+}
+
+ACE_INLINE int
+DSRT_Dispatcher::update_schedule (guid_t guid, const QoSDescriptor& qos)
+{
+  return dispatcher_impl_->update_schedule (guid, qos);
+}
+
+ACE_INLINE int
+DSRT_Dispatcher::cancel_schedule (guid_t guid, const QoSDescriptor& qos)
+{
+  return dispatcher_impl_->cancel_schedule (guid, qos);
+}
+
+DSRT_Dispatcher*
+Dispatcher_Factory::
+create_DSRT_dispatcher (const DSRT_ConfigInfo& config_info)
+{
+  DSRT_Dispatcher_Impl* tmp;
+  ACE_NEW_RETURN (tmp, DSRT_Dispatcher_Impl, (DSRT_Dispatcher*)0);
+  DSRT_Dispatcher* disp;
+  ACE_NEW_RETURN (disp, DSRT_Dispatcher, (DSRT_Dispatcher*)0);
+  disp->implementation (tmp);
+  tmp->init (config_info);
+  return disp;
+}
+
 }
