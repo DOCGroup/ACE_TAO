5eb7b94070cfce3928ee751d99cdde15dc4f8672
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-04-15 17:35:25 +0000

    ChangeLogTag: Tue Apr 15 19:28:12 2002  Johnny Willemsen <jwillemsen@remedy.nl>

--- trunk/ace/Addr.cpp
+++ trunk/ace/Addr.cpp
@@ -7,25 +7,9 @@
 
 ACE_RCSID(ace, Addr, "$Id$")
 
-#if defined (ACE_HAS_BROKEN_SAP_ANY)
-#if defined (sap_any)
-#undef sap_any
-// Note: this object requires static construction and destruction.
-/* static */
-const ACE_Addr ace_sap_any (AF_ANY, -1);
-
-const ACE_Addr &
-ACE_Addr::sap_any (void)
-{
-  return ace_sap_any;
-}
-#define sap_any sap_any()
-#endif /* sap_any */
-#else
 // Note: this object requires static construction and destruction.
 /* static */
 const ACE_Addr ACE_Addr::sap_any (AF_ANY, -1);
-#endif /* ACE_HAS_BROKEN_SAP_ANY */
 
 #if !defined (__ACE_INLINE__)
 #include "ace/Addr.i"
