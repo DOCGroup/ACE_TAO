80c61a736ea56708ae0315373c92940853909ca2
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-04-25 22:06:17 +0000

    ChangeLogTag:Fri Apr 25 17:04:34 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/TAO_IDL/be_include/be_visitor_sequence/sequence_cs.h
+++ TAO/TAO_IDL/be_include/be_visitor_sequence/sequence_cs.h
@@ -24,6 +24,7 @@
 #ifndef _BE_VISITOR_SEQUENCE_SEQUENCE_CS_H_
 #define _BE_VISITOR_SEQUENCE_SEQUENCE_CS_H_
 
+class TAO_OutStream;
 class be_visitor_sequence_cs : public be_visitor_decl
 {
   //
@@ -53,6 +54,12 @@ public:
                                   be_type *bt);
   // Generate explicit instantiation for our element type, if
   // it is a managed type and if Any operator generation is suppressed.
+
+private:
+
+  // Generate explicit instantiation for the base class type.
+  int gen_base_class_tmplinst (be_sequence *node,
+                               be_type *bt);
 };
 
 #endif /* _BE_VISITOR_SEQUENCE_SEQUENCE_CS_H_ */
