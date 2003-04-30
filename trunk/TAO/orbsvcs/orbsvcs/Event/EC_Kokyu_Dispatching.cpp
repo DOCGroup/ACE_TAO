e2247138205f0993f52eb1dbd9438125c7bff670
venkita <venkita@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-04-30 15:43:55 +0000

    ChangeLogTag: Wed Apr 30 10:40:31 2003  Venkita Subramonian  <venkita@cs.wustl.edu>

--- trunk/TAO/orbsvcs/orbsvcs/Event/EC_Kokyu_Dispatching.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Event/EC_Kokyu_Dispatching.cpp
@@ -12,87 +12,97 @@
 
 #include "Kokyu/Kokyu.h"
 
-#if	! defined (__ACE_INLINE__)
+#if     ! defined (__ACE_INLINE__)
 #include "EC_Kokyu_Dispatching.i"
 #endif /* __ACE_INLINE__ */
 
 ACE_RCSID(Event, EC_Kokyu_Dispatching, "$Id$")
 
 TAO_EC_Kokyu_Dispatching::TAO_EC_Kokyu_Dispatching (TAO_EC_Event_Channel *ec)
-  :	dispatcher_(0)
+  :dispatcher_ (0)
 {
-  CORBA::Object_var	tmp	= ec->scheduler	();
+  CORBA::Object_var tmp = ec->scheduler ();
   this->scheduler_ = RtecScheduler::Scheduler::_narrow (tmp.in ());
 }
 
 void
 TAO_EC_Kokyu_Dispatching::activate (void)
 {
-  // Query the scheduler to	get	Config_Infos
+  ACE_DECLARE_NEW_CORBA_ENV;
+  // Query the scheduler togetConfig_Infos
   RtecScheduler::Config_Info_Set_var configs;
-  this->scheduler_->get_config_infos(configs.out());
-  ACE_TRY_CHECK;
+  ACE_TRY
+    {
+      this->scheduler_->get_config_infos(configs.out());
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      // Ignore exceptions..
+    }
+  ACE_ENDTRY;
 
-  //might be no	Config_Infos in	the	set	(if	none passed	to scheduler_)
+  //might be no Config_Infos in the set (if none passed to scheduler_)
 
-  // Convert RtecScheduler::Config_Info_Set	to Kokyu::ConfigInfoSet
-  // OK	to assume exact	correspondence betwen Config_Info and ConfigInfo?
+  // Convert RtecScheduler::Config_Info_Set     to Kokyu::ConfigInfoSet
+  // OK to assume exact correspondence betwen Config_Info and ConfigInfo?
   Kokyu::ConfigInfoSet kconfigs(configs->length());
-  for(CORBA::ULong i=0;	i<configs->length(); ++i) {
-	kconfigs[i].preemption_priority_ = configs[i].preemption_priority;
-	kconfigs[i].thread_priority_ = configs[i].thread_priority;
-	switch (configs[i].dispatching_type) {
-		case RtecScheduler::STATIC_DISPATCHING:
-			kconfigs[i].dispatching_type_ =	Kokyu::FIFO_DISPATCHING;
-			break;
-		case RtecScheduler::DEADLINE_DISPATCHING:
-			kconfigs[i].dispatching_type_ =	Kokyu::DEADLINE_DISPATCHING;
-			break;
-		case RtecScheduler::LAXITY_DISPATCHING:
-			kconfigs[i].dispatching_type_ =	Kokyu::LAXITY_DISPATCHING;
-			break;
-	}
+  for(CORBA::ULong i=0; i<configs->length(); ++i) {
+        kconfigs[i].preemption_priority_ = configs[i].preemption_priority;
+        kconfigs[i].thread_priority_ = configs[i].thread_priority;
+        switch (configs[i].dispatching_type) {
+                case RtecScheduler::STATIC_DISPATCHING:
+                        kconfigs[i].dispatching_type_ = Kokyu::FIFO_DISPATCHING;
+                        break;
+                case RtecScheduler::DEADLINE_DISPATCHING:
+                        kconfigs[i].dispatching_type_ = Kokyu::DEADLINE_DISPATCHING;
+                        break;
+                case RtecScheduler::LAXITY_DISPATCHING:
+                        kconfigs[i].dispatching_type_ = Kokyu::LAXITY_DISPATCHING;
+                        break;
+        }
   }
 
-  // Create	Kokyu::Dispatcher using	factory
-  this->dispatcher_	= Kokyu::Dispatcher_Factory::create_dispatcher(kconfigs);
+  // Create Kokyu::Dispatcher using factory
+  this->dispatcher_ = Kokyu::Dispatcher_Factory::create_dispatcher(kconfigs);
 }
 
 void
 TAO_EC_Kokyu_Dispatching::shutdown (void)
 {
   this->dispatcher_->shutdown();
-  //delete dispatcher_?
+  delete dispatcher_;
 }
 
 void
 TAO_EC_Kokyu_Dispatching::push (TAO_EC_ProxyPushSupplier* proxy,
-								   RtecEventComm::PushConsumer_ptr consumer,
-								   const RtecEventComm::EventSet& event,
-								   TAO_EC_QOS_Info&	qos_info
-								   ACE_ENV_ARG_DECL)
+                                RtecEventComm::PushConsumer_ptr consumer,
+                                const RtecEventComm::EventSet& event,
+                                TAO_EC_QOS_Info&     qos_info
+                                ACE_ENV_ARG_DECL)
 {
   RtecEventComm::EventSet event_copy = event;
-  this->push_nocopy	(proxy,	consumer, event_copy, qos_info ACE_ENV_ARG_PARAMETER);
+  this->push_nocopy (proxy, consumer, event_copy, qos_info ACE_ENV_ARG_PARAMETER);
 }
 
 void
 TAO_EC_Kokyu_Dispatching::push_nocopy (TAO_EC_ProxyPushSupplier* proxy,
-										  RtecEventComm::PushConsumer_ptr consumer,
-										  RtecEventComm::EventSet& event,
-										  TAO_EC_QOS_Info& qos_info
-										  ACE_ENV_ARG_DECL)
+                                       RtecEventComm::PushConsumer_ptr consumer,
+                                       RtecEventComm::EventSet& event,
+                                       TAO_EC_QOS_Info& qos_info
+                                       ACE_ENV_ARG_DECL)
 {
-  if (this->dispatcher_	== 0)
-	this->activate();
+  if (this->dispatcher_ == 0)
+        this->activate();
 
-  // Create	Dispatch_Command
-  TAO_EC_Kokyu_Push_Command	*cmd = new TAO_EC_Kokyu_Push_Command(proxy,consumer,event);
+  // Create Dispatch_Command
+  TAO_EC_Kokyu_Push_Command *cmd =
+    new TAO_EC_Kokyu_Push_Command(proxy,consumer,event);
 
-  // Convert TAO_EC_QOS_Info to	QoSDescriptor
-  RtecScheduler::RT_Info *rt_info =	this->scheduler_->get(qos_info.rt_info);
+  // Convert TAO_EC_QOS_Info to QoSDescriptor
+  RtecScheduler::RT_Info *rt_info =     this->scheduler_->get(qos_info.rt_info);
   Kokyu::QoSDescriptor qosd;
-  qosd.preemption_priority_	= qos_info.preemption_priority;
+  qosd.preemption_priority_     = qos_info.preemption_priority;
   qosd.deadline_ = rt_info->period;
   qosd.execution_time_ = rt_info->worst_case_execution_time;
 
@@ -119,10 +129,22 @@ TAO_EC_Kokyu_Push_Command::~TAO_EC_Kokyu_Push_Command(void)
 }
 
 int
-TAO_EC_Kokyu_Push_Command::execute (ACE_ENV_SINGLE_ARG_DECL)
+TAO_EC_Kokyu_Push_Command::execute ()
 {
-  this->proxy_->push_to_consumer (this->consumer_.in (),
-								  this->event_
-								   ACE_ENV_ARG_PARAMETER);
+  ACE_DECLARE_NEW_CORBA_ENV;
+
+  ACE_TRY
+    {
+      this->proxy_->push_to_consumer (this->consumer_.in (),
+                                      this->event_
+                                      ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+    }
+  ACE_CATCHANY
+    {
+      return -1;
+    }
+  ACE_ENDTRY;
+
   return 0;
 }
