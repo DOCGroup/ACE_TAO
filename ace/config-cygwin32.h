091b5365e7899c35e3b1a806280584ecd98f096a
dhinton <dhinton@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-05-09 01:32:53 +0000

    ChangeLogTag:Fri May  9 01:20:49 UTC 2003  Don Hinton  <dhinton@dresystems.com>

--- ace/config-cygwin32.h
+++ ace/config-cygwin32.h
@@ -12,9 +12,10 @@
 
 #define ACE_HAS_AUTOMATIC_INIT_FINI
 
-#define ACE_LACKS_STDINT_H
 #define ACE_LACKS_INTTYPES_H
+#define ACE_LACKS_STDINT_H
 #define ACE_LACKS_STROPTS_H
+#define ACE_LACKS_SYS_SEM_H
 #define ACE_LACKS_UCONTEXT_H
 
 #include "ace/post.h"
