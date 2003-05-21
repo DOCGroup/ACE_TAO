c6b293cd00ddb8a6096c52a73b439fa1b2959de1
dhinton <dhinton@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-05-21 22:02:20 +0000

    ChangeLogTag:Wed May 21 21:46:19 UTC 2003  Don Hinton  <dhinton@dresystems.com>

--- trunk/tests/Main.cpp
+++ trunk/tests/Main.cpp
@@ -18,7 +18,9 @@
 // ============================================================================
 
 #include "ace/OS.h"
-
+#if defined (ACE_HAS_WINCE)
+#  include "ace/ACE.h"
+#endif /* ACE_HAS_WINCE */
 int run_main (int argc, ACE_TCHAR *argv[]);
 
 int
