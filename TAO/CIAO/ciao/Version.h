713a83cfe9c321bb773785dcf82ac3ef071f2cfa
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-05-30 19:12:12 +0000

    ChangeLogTag:Fri May 30 14:11:11 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/CIAO/ciao/Version.h
+++ TAO/CIAO/ciao/Version.h
@@ -2,6 +2,6 @@
 // This is an automatically generated file.
 
 #define CIAO_MAJOR_VERSION 0
-#define CIAO_MINOR_VERSION 0
+#define CIAO_MINOR_VERSION 3
 #define CIAO_BETA_VERSION 1
-#define CIAO_VERSION "0.0.1"
+#define CIAO_VERSION "0.3.1"
