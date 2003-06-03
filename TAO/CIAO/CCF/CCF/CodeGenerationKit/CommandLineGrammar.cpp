29d2010fe370cf96fa836bea3b58e723c2e7bf08
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-03 12:51:39 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_3_2'.

--- TAO/CIAO/CCF/CCF/CodeGenerationKit/CommandLineGrammar.cpp
+++
@@ -1,5 +0,0 @@
-// File   : CommandLineGrammar.cpp
-// Author : Boris Kolpackov <boris@dre.vanderbilt.edu>
-// $Id$
-
-#include "CCF/CodeGenerationKit/CommandLineGrammar.hpp"
--- TAO/CIAO/ciao/Version.h
+++
@@ -1,7 +0,0 @@
-// $Id$
-// This is an automatically generated file.
-
-#define CIAO_MAJOR_VERSION 0
-#define CIAO_MINOR_VERSION 3
-#define CIAO_BETA_VERSION 1
-#define CIAO_VERSION "0.3.1"
