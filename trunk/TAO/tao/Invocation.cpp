440ae8706d04392438f0d5f27b2b8e297d59b7b3
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-04 07:23:59 +0000

    ChangeLogTag: Wed Jun 04 07:21:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Invocation.cpp
+++ trunk/TAO/tao/Invocation.cpp
@@ -1191,10 +1191,22 @@ TAO_GIOP_Locate_Request_Invocation::invoke (ACE_ENV_SINGLE_ARG_DECL)
 
 template class auto_ptr<CORBA::SystemException>;
 template class auto_ptr<CORBA::Exception>;
+#  if defined (ACE_LACKS_AUTO_PTR) \
+      || !(defined (ACE_HAS_STANDARD_CPP_LIBRARY) \
+           && (ACE_HAS_STANDARD_CPP_LIBRARY != 0))
+template class ACE_Auto_Basic_Ptr<CORBA::SystemException>;
+template class ACE_Auto_Basic_Ptr<CORBA::Exception>;
+#  endif  /* ACE_LACKS_AUTO_PTR */
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
 #pragma instantiate auto_ptr<CORBA::SystemException>
 #pragma instantiate auto_ptr<CORBA::Exception>
+#  if defined (ACE_LACKS_AUTO_PTR) \
+      || !(defined (ACE_HAS_STANDARD_CPP_LIBRARY) \
+           && (ACE_HAS_STANDARD_CPP_LIBRARY != 0))
+#    pragma instantiate ACE_Auto_Basic_Ptr<CORBA::SystemException>
+#    pragma instantiate ACE_Auto_Basic_Ptr<CORBA::Exception>
+#  endif  /* ACE_LACKS_AUTO_PTR */
 
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
