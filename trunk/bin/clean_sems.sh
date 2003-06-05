8ccac48d81b3da2f7f8982a08458675f9d3f9251
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-05 12:06:55 +0000

    ChangeLogTag: Thu Jun  5 07:05:42 2003  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/bin/clean_sems.sh
+++ trunk/bin/clean_sems.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-if test "$OSTYPE" = "solaris"; then
-  semids=`ipcs | grep $USER | awk '{ print ($2) }'`
-  for p in $semids
-    do ipcrm -s $p
-  done
-else
-  # Linux/GNU
-  ipcs -a | grep $USER | awk '{ print ($2) }' | xargs ipcrm sem;
+if [ -z "$USER" ]; then
+  USER=`id | sed 's/).*//; s/.*(//'`
 fi
 
+SYSTEM=`uname -s`
+case "$SYSTEM" in
+  "Linux" )
+    ipcs -a | grep $USER | awk '{ print ($2) }' | xargs ipcrm sem;
+    ;;
+  * )
+    semids=`ipcs | grep "^s" | grep $USER | awk '{ print ($2) }'`
+    for p in $semids
+      do ipcrm -s $p
+    done
+    ;;
+esac
