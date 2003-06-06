971fb1f0be88c2f202e23d7e03aa86eea748a33d
Johnny Willemsen <jwillemsen@remedy.nl>
2003-06-06 06:40:09 +0000

    ChangeLogTag: Fri Jun 06 06:39:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- ACEXML/common/Element_Def_Builder.cpp
+++ ACEXML/common/Element_Def_Builder.cpp
@@ -10,8 +10,16 @@ ACEXML_Element_Def_Builder::~ACEXML_Element_Def_Builder ()
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 template class auto_ptr<ACEXML_Element_Def_Builder>;
+#  if defined (ACE_LACKS_AUTO_PTR) \
+      || !(defined (ACE_HAS_STANDARD_CPP_LIBRARY) \
+           && (ACE_HAS_STANDARD_CPP_LIBRARY != 0))
 template class ACE_Auto_Basic_Ptr<ACEXML_Element_Def_Builder>;
+#  endif  /* ACE_LACKS_AUTO_PTR */
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate auto_ptr<ACEXML_Element_Def_Builder>
-#pragma instantiate ACE_Auto_Basic_Ptr<ACEXML_Element_Def_Builder>
+#  if defined (ACE_LACKS_AUTO_PTR) \
+      || !(defined (ACE_HAS_STANDARD_CPP_LIBRARY) \
+           && (ACE_HAS_STANDARD_CPP_LIBRARY != 0))
+#    pragma instantiate ACE_Auto_Basic_Ptr<ACEXML_Element_Def_Builder>
+#  endif  /* ACE_LACKS_AUTO_PTR */
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
