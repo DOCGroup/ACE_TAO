5d370d1a3f0a6b4600e966d560fe39ed78d7b03c
Johnny Willemsen <jwillemsen@remedy.nl>
2003-06-06 06:40:09 +0000

    ChangeLogTag: Fri Jun 06 06:39:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- ACEXML/common/Attributes_Def_Builder.cpp
+++ ACEXML/common/Attributes_Def_Builder.cpp
@@ -15,11 +15,19 @@ ACEXML_Attributes_Def_Builder::~ACEXML_Attributes_Def_Builder ()
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 template class auto_ptr<ACEXML_Attribute_Def_Builder>;
 template class auto_ptr<ACEXML_Attributes_Def_Builder>;
+#  if defined (ACE_LACKS_AUTO_PTR) \
+      || !(defined (ACE_HAS_STANDARD_CPP_LIBRARY) \
+           && (ACE_HAS_STANDARD_CPP_LIBRARY != 0))
 template class ACE_Auto_Basic_Ptr<ACEXML_Attribute_Def_Builder>;
 template class ACE_Auto_Basic_Ptr<ACEXML_Attributes_Def_Builder>;
+#  endif  /* ACE_LACKS_AUTO_PTR */
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate auto_ptr<ACEXML_Attribute_Def_Builder>
 #pragma instantiate auto_ptr<ACEXML_Attributes_Def_Builder>
-#pragma instantiate ACE_Auto_Basic_Ptr<ACEXML_Attribute_Def_Builder>
-#pragma instantiate ACE_Auto_Basic_Ptr<ACEXML_Attributes_Def_Builder>
+#  if defined (ACE_LACKS_AUTO_PTR) \
+      || !(defined (ACE_HAS_STANDARD_CPP_LIBRARY) \
+           && (ACE_HAS_STANDARD_CPP_LIBRARY != 0))
+#    pragma instantiate ACE_Auto_Basic_Ptr<ACEXML_Attribute_Def_Builder>
+#    pragma instantiate ACE_Auto_Basic_Ptr<ACEXML_Attributes_Def_Builder>
+#  endif  /* ACE_LACKS_AUTO_PTR */
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
