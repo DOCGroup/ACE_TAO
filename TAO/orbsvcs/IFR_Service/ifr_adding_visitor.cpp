d34693ba56be450e827d415e1e068434bcf78c0e
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-10 00:26:33 +0000

    ChangeLogTag: Mon Jun  9 19:20:27 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/orbsvcs/IFR_Service/ifr_adding_visitor.cpp
+++ TAO/orbsvcs/IFR_Service/ifr_adding_visitor.cpp
@@ -1996,7 +1996,7 @@ ifr_adding_visitor::create_interface_def (AST_Interface *node
 
 int
 ifr_adding_visitor::create_value_def (AST_ValueType *
-                                      ACE_ENV_ARG_DECL)
+                                      ACE_ENV_ARG_DECL_NOT_USED)
 {
   return 0;
 }
