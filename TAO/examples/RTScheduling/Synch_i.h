fe26d40a25b309313356dc00ca6e77dcab461e29
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-16 16:38:30 +0000

    *** empty log message ***

---
+++ TAO/examples/RTScheduling/Synch_i.h
@@ -0,0 +1,25 @@
+//$Id$
+
+#ifndef SYNCH_I_H
+#define SYNCH_I_H
+
+#include "SynchS.h"
+
+class Synch_i : public POA_Synch
+{
+ public:
+
+  Synch_i (void);
+  virtual void go (CORBA::Long base_time)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+  int synched (void);
+
+  ACE_Time_Value* base_time (void);
+
+ private:
+  int synched_;
+  ACE_Time_Value base_time_;
+};
+
+#endif /*SYNCH_I_H*/
