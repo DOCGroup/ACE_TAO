dd41475b02f69984eda88bff94580a7347ccced0
Ossama Othman <ossama-othman@users.noreply.github.com>
2003-06-18 01:35:00 +0000

    ChangeLogTag:Tue Jun 17 18:31:31 2003  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- TAO/orbsvcs/orbsvcs/PortableGroup/PG_Null_Property_Validator.h
+++ TAO/orbsvcs/orbsvcs/PortableGroup/PG_Null_Property_Validator.h
@@ -49,7 +49,8 @@ public:
    * This particular implementation performs no validation
    * whatsoever.
    */
-  void validate_criteria (const PortableGroup::Properties & criteria)
+  void validate_criteria (const PortableGroup::Properties & criteria
+                          ACE_ENV_ARG_DECL)
     ACE_THROW_SPEC ((CORBA::SystemException,
                      PortableGroup::InvalidCriteria,
                      PortableGroup::CannotMeetCriteria));
