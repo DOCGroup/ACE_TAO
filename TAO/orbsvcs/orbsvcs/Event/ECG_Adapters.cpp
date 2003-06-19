64f41125a8cb0023641ce19d8fab90c64126c643
Johnny Willemsen <jwillemsen@remedy.nl>
2003-06-19 15:24:42 +0000

    ChangeLogTag: Thu Jun 19 15:23:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/Event/ECG_Adapters.cpp
+++ TAO/orbsvcs/orbsvcs/Event/ECG_Adapters.cpp
@@ -7,3 +7,10 @@ ACE_RCSID(Event, ECG_Adapters, "$Id$")
 TAO_ECG_Handler_Shutdown::~TAO_ECG_Handler_Shutdown (void)
 {
 }
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+template class ACE_Refcounted_Auto_Ptr<TAO_ECG_Handler_Shutdown, ACE_Null_Mutex>;
+#elif defined(ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+#pragma instantiate ACE_Refcounted_Auto_Ptr<TAO_ECG_Handler_Shutdown, ACE_Null_Mutex>
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
