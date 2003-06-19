0fbac24221a6bb6aabceb367a7aad09efe75f204
bala <balanatarajan@users.noreply.github.com>
2003-06-19 12:03:56 +0000

    ChangeLogTag:Thu Jun 19 07:01:57 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- ace/config-lynxos.h
+++ ace/config-lynxos.h
@@ -112,6 +112,8 @@
 #define ACE_LACKS_UCONTEXT_H
 #define ACE_LACKS_MKSTEMP
 #define ACE_LACKS_INET_ATON
+#define ACE_LACKS_DLFCN_H
+#define ACE_LACKS_SYS_SELECT_H
 #define ACE_MALLOC_ALIGN 8
 #define ACE_HAS_TYPENAME_KEYWORD
 // Don't use MAP_FIXED, at least for now.
