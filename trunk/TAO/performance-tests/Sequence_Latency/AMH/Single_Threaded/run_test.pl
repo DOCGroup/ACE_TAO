9fc674186f25932daa7475f663ca1444238eea15
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-19 00:36:04 +0000

    ChangeLogTag: Wed Jun 18 19:33:24 2003  Gan Deng <gan.deng@vanderbilt.edu>

---
+++ trunk/TAO/performance-tests/Sequence_Latency/AMH/Single_Threaded/run_test.pl
@@ -0,0 +1,185 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# $Id$
+# -*- perl -*-
+
+use lib "../../../../../bin";
+use PerlACE::Run_Test;
+
+$status = 0;
+$iorfile = PerlACE::LocalFile ("test.ior");
+
+print STDERR "================ DII Sequence Latency Test -- octet sequence\n";
+
+unlink $iorfile;
+
+$SV = new PerlACE::Process ("server",
+                            "-o $iorfile");
+
+$CL = new PerlACE::Process ("client",
+                            "-t octet "
+                            . "-k file://$iorfile "
+                            . " -i 10000");
+
+$SV->Spawn ();
+
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (120);
+$server = $SV->WaitKill (40);
+
+unlink $iorfile;
+
+
+
+print STDERR "================ AMH Sequence Latency Test -- long sequence\n";
+
+unlink $iorfile;
+
+$SV = new PerlACE::Process ("server",
+                            "-o $iorfile");
+
+$CL = new PerlACE::Process ("client",
+                            "-t long "
+                            . "-k file://$iorfile "
+                            . " -i 10000");
+
+$SV->Spawn ();
+
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (120);
+$server = $SV->WaitKill (40);
+
+unlink $iorfile;
+
+print STDERR "================ AMH Sequence Latency Test -- short sequence\n";
+
+unlink $iorfile;
+
+$SV = new PerlACE::Process ("server",
+                            "-o $iorfile");
+
+$CL = new PerlACE::Process ("client",
+                            "-t short "
+                            . "-k file://$iorfile "
+                            . " -i 10000");
+
+$SV->Spawn ();
+
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (120);
+$server = $SV->WaitKill (40);
+
+unlink $iorfile;
+
+
+print STDERR "================ AMH Sequence Latency Test -- char sequence\n";
+
+unlink $iorfile;
+
+$SV = new PerlACE::Process ("server",
+                            "-o $iorfile");
+
+$CL = new PerlACE::Process ("client",
+                            "-t char "
+                            . "-k file://$iorfile "
+                            . " -i 10000");
+
+$SV->Spawn ();
+
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (120);
+$server = $SV->WaitKill (40);
+
+unlink $iorfile;
+
+
+
+print STDERR "================ AMH Sequence Latency Test -- longlong sequence\n";
+
+unlink $iorfile;
+
+$SV = new PerlACE::Process ("server",
+                            "-o $iorfile");
+
+$CL = new PerlACE::Process ("client",
+                            "-t longlong "
+                            . "-k file://$iorfile "
+                            . " -i 10000");
+
+$SV->Spawn ();
+
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (120);
+$server = $SV->WaitKill (40);
+
+unlink $iorfile;
+
+
+
+
+print STDERR "================ AMH Sequence Latency Test -- double sequence\n";
+
+unlink $iorfile;
+
+$SV = new PerlACE::Process ("server",
+                            "-o $iorfile");
+
+$CL = new PerlACE::Process ("client",
+                            "-t double "
+                            . "-k file://$iorfile "
+                            . " -i 10000");
+
+$SV->Spawn ();
+
+if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$iorfile>\n";
+    $SV->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (120);
+$server = $SV->WaitKill (40);
+
+unlink $iorfile;
+
+
+
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1;
+}
+
+if ($server != 0) {
+    print STDERR "ERROR: server returned $server\n";
+    $status = 1;
+}
+
+exit $status;
