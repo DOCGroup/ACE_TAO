38afbdcf77cf155619f8181d09f112215ff56ecb
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-06-20 22:21:53 +0000

    ChangeLogTag: Fri Jun 20 17:17:57 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- branches/refactor-stage3/TAO/tao/Basic_Arguments.h
+++ branches/refactor-stage3/TAO/tao/Basic_Arguments.h
@@ -15,12 +15,11 @@
 #define TAO_BASIC_ARGUMENTS_H
 
 #include "ace/pre.h"
+#include "Basic_Argument_T.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
-
-#include "Basic_Argument_T.h"
 
 namespace TAO
 {
