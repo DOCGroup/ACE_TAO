47543f3c34880eacce36bd731ea7c218745229f5
Chris Cleeland <chris.cleeland@gmail.com>
2003-06-27 17:42:32 +0000

    Fri Jun 27 12:12:09 2003  Chris Cleeland  <cleeland_c@ociweb.com>

--- TAO/orbsvcs/orbsvcs/Event/ECG_UDP_Receiver.cpp
+++ TAO/orbsvcs/orbsvcs/Event/ECG_UDP_Receiver.cpp
@@ -246,7 +246,7 @@ TAO_ECG_UDP_Receiver::handle_input (ACE_SOCK_Dgram& dgram)
 // ****************************************************************
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-template void activate(TAO_Objref_Var_T<RtecEventComm::PushSupplier, RtecEventComm::tao_PushSupplier_life>&, PortableServer::POA*, TAO_ServantBase*, TAO_EC_Object_Deactivator&);
+template void activate<>(TAO_Objref_Var_T<RtecEventComm::PushSupplier, RtecEventComm::tao_PushSupplier_life>&, PortableServer::POA*, TAO_ServantBase*, TAO_EC_Object_Deactivator& ACE_ENV_ARG_DECL);
 #elif defined(ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#pragma instantiate void activate(TAO_Objref_Var_T<RtecEventComm::PushSupplier, RtecEventComm::tao_PushSupplier_life>&, PortableServer::POA*, TAO_ServantBase*, TAO_EC_Object_Deactivator&)
+#pragma instantiate void activate<>(TAO_Objref_Var_T<RtecEventComm::PushSupplier, RtecEventComm::tao_PushSupplier_life>&, PortableServer::POA*, TAO_ServantBase*, TAO_EC_Object_Deactivator& ACE_ENV_ARG_DECL)
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
