3ac577d0314b221b9e5a6cf5015d40a145552de9
Johnny Willemsen <jwillemsen@remedy.nl>
2003-07-03 18:51:27 +0000

    ChangeLogTag: Thu Jul 03 18:51:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- TAO/orbsvcs/tests/Event/lib/Supplier.h
+++ TAO/orbsvcs/tests/Event/lib/Supplier.h
@@ -1,18 +1,14 @@
 /* -*- C++ -*- */
-// $Id$
-//
-// ============================================================================
-//
-// = LIBRARY
-//   ORBSVCS Real-time Event Channel testsuite
-//
-// = FILENAME
-//   Supplier
-//
-// = AUTHOR
-//   Carlos O'Ryan (coryan@cs.wustl.edu)
-//
-// ============================================================================
+//=============================================================================
+/**
+ *  @file   Supplier
+ *
+ *  $Id$
+ *
+ *  @author Carlos O'Ryan (coryan@cs.wustl.edu)
+ */
+//=============================================================================
+
 
 #ifndef EC_SUPPLIER_H
 #define EC_SUPPLIER_H
@@ -27,129 +23,132 @@
 #include "orbsvcs/RtecEventChannelAdminC.h"
 #include "ace/Task.h"
 
+/**
+ * @class EC_Supplier
+ *
+ * @brief Simple supplier object to implement EC tests.
+ *
+ * This class is a supplier of events.
+ * The class is just a helper to simplify common tasks in EC
+ * tests, such as subscribing for a range of events, disconnecting
+ * from the EC, informing the driver of shutdown messages, etc.
+ * There are several ways to connect and disconnect this class,
+ * and it is up to the driver program to use the right one.
+ * The driver can request that to this class to send a new event,
+ * a new shutdown event or to become "active" and send a number of
+ * events at a certain rate.
+ */
 class EC_Test_Export EC_Supplier : public POA_RtecEventComm::PushSupplier
 {
-  //
-  // = TITLE
-  //   Simple supplier object to implement EC tests.
-  //
-  // = DESCRIPTION
-  //   This class is a supplier of events.
-  //   The class is just a helper to simplify common tasks in EC
-  //   tests, such as subscribing for a range of events, disconnecting
-  //   from the EC, informing the driver of shutdown messages, etc.
-  //
-  //   There are several ways to connect and disconnect this class,
-  //   and it is up to the driver program to use the right one.
-  //
-  //   The driver can request that to this class to send a new event,
-  //   a new shutdown event or to become "active" and send a number of
-  //   events at a certain rate.
-  //
 public:
+  /**
+   * Constructor, specifies the types of events to send.
+   * Notice that the user can connect to the EC using other
+   * publications, this is useful for filtering tests.
+   */
   EC_Supplier (EC_Driver *driver,
                void* supplier_cookie);
-  // Constructor, specifies the types of events to send.
-  // Notice that the user can connect to the EC using other
-  // publications, this is useful for filtering tests.
 
+  /// The types of the event is chosen by the driver, based on the
+  /// cookie and the <event_number>
   void send_event (int event_number
                    ACE_ENV_ARG_DECL);
-  // The types of the event is chosen by the driver, based on the
-  // cookie and the <event_number>
 
+  /// Send <event> to the EC.
   void send_event (const RtecEventComm::EventSet& event
                    ACE_ENV_ARG_DECL);
-  // Send <event> to the EC.
 
+  /// Set the event type and source in <event>
   void send_event (int event_number,
                    const RtecEventComm::Event& event);
-  // Set the event type and source in <event>
 
+  /// Send a shutdown event.
   void send_shutdown (ACE_ENV_SINGLE_ARG_DECL);
-  // Send a shutdown event.
 
+  /// Connect using a <supplier_admin> and publications (<qos>)
+  /// computed by the user
   virtual void connect (
         RtecEventChannelAdmin::SupplierAdmin_ptr supplier_admin,
         const RtecEventChannelAdmin::SupplierQOS& qos,
         int shutdown_event_type
         ACE_ENV_ARG_DECL);
-  // Connect using a <supplier_admin> and publications (<qos>)
-  // computed by the user
 
+  /// Connect using the current consumer_proxy (useful for reconnect test)
   virtual void connect (
         const RtecEventChannelAdmin::SupplierQOS& qos,
         int shutdown_event_type
         ACE_ENV_ARG_DECL);
-  // Connect using the current consumer_proxy (useful for reconnect test)
 
+  /// Disconnect from the EC, also deactivates the object
   void disconnect (ACE_ENV_SINGLE_ARG_DECL);
-  // Disconnect from the EC, also deactivates the object
 
+  /// Disconnect from the EC, also deactivates the object
   void shutdown (ACE_ENV_SINGLE_ARG_DECL);
-  // Disconnect from the EC, also deactivates the object
 
+  /// Dump the results...
   virtual void dump_results (const char* name,
                              ACE_UINT32 global_scale_factor);
-  // Dump the results...
 
+  /// Add our statistics to <stats>
   void accumulate (ACE_Throughput_Stats& stats) const;
-  // Add our statistics to <stats>
 
+  /// Return an event type to push....
   void event_type (int event_number,
                    RtecEventComm::Event& event);
-  // Return an event type to push....
 
   // = The PushSupplier methods
   virtual void disconnect_push_supplier (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
     ACE_THROW_SPEC ((CORBA::SystemException));
 
 private:
+  /// Class we forward to.
   EC_Driver *driver_;
-  // Class we forward to.
 
+  /// Magic cookie provided by the supplier to identify ourselves
   void* cookie_;
-  // Magic cookie provided by the supplier to identify ourselves
 
+  /// Protect the internal state
   TAO_SYNCH_MUTEX lock_;
-  // Protect the internal state
 
+  /// Count the number of push() calls
   int push_count_;
-  // Count the number of push() calls
 
+  /// We talk to the EC (as a supplier) using this proxy.
   RtecEventChannelAdmin::ProxyPushConsumer_var consumer_proxy_;
-  // We talk to the EC (as a supplier) using this proxy.
 
+  /// The time for the first event sent
   ACE_hrtime_t throughput_start_;
-  // The time for the first event sent
 
+  /// Measure the elapsed time spent while sending the events.
   ACE_Throughput_Stats throughput_;
-  // Measure the elapsed time spent while sending the events.
 
   int burst_count_;
   int burst_size_;
   int payload_size_;
   int burst_pause_;
 
+  /// The test data.
   int shutdown_event_type_;
-  // The test data.
 
+  /// The publications, used to select the events.
   RtecEventChannelAdmin::SupplierQOS qos_;
-  // The publications, used to select the events.
 
+  /// Is the supplier active in the POA?
   int is_active_;
-  // Is the supplier active in the POA?
 
+  /// Cache the object reference to speed up connect/disconnect calls
   RtecEventComm::PushSupplier_var myself_;
-  // Cache the object reference to speed up connect/disconnect calls
 };
 
 // ****************************************************************
 
+/**
+ * @class EC_Supplier_Task
+ */
 class EC_Supplier_Task : public ACE_Task_Base
 {
-  //
 public:
+  /// Constructor
   EC_Supplier_Task (EC_Supplier* supplier,
                     EC_Driver* driver,
                     void* cookie,
@@ -159,36 +158,35 @@ public:
                     int payload_size,
                     int shutdown_event_type,
                     ACE_Thread_Manager* thr_mgr = 0);
-  // Constructor
 
+  /// The svc call
   virtual int svc (void);
-  // The svc call
 
 private:
+  /// The supplier
   EC_Supplier* supplier_;
-  // The supplier
 
+  /// The driver program
   EC_Driver* driver_;
-  // The driver program
 
+  /// The magic cookie assigned to the supplier
   void* cookie_;
-  // The magic cookie assigned to the supplier
 
+  /// Number of events "bursts"
   int burst_count_;
-  // Number of events "bursts"
 
+  /// The number of events in a "burst", i.e. between two calls to
+  /// sleep.
   int burst_size_;
-  // The number of events in a "burst", i.e. between two calls to
-  // sleep.
 
+  /// The sleep time (in usecs) between each burst
   int burst_pause_;
-  // The sleep time (in usecs) between each burst
 
+  /// The size of the payload in each event.
   int payload_size_;
-  // The size of the payload in each event.
 
+  /// Define the shutdown event, invoked at the end of the loop.
   int shutdown_event_type_;
-  // Define the shutdown event, invoked at the end of the loop.
 };
 
 #endif /* EC_SUPPLIER_H */
