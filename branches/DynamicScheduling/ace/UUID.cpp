839041d3035a37f6c9bbf4ad55662388cfafa528
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-03 22:51:57 +0000

    *** empty log message ***

--- branches/DynamicScheduling/ace/UUID.cpp
+++ branches/DynamicScheduling/ace/UUID.cpp
@@ -2,7 +2,6 @@
 #include "ace/OS.h"
 #include "ace/Singleton.h"
 #include "ace/UUID.h"
-//#include "stdafx.h"
 #include <Windows.h>
 #include <rpc.h>
 #include <rpcdce.h>
@@ -24,22 +23,13 @@ UUID_node::nodeID (void)
 void 
 UUID_node::nodeID (NodeID& nodeID)
 {
-
-  		
-  nodeID_ [0] = nodeID [0];
-  nodeID_ [1] = nodeID [1];
-  nodeID_ [2] = nodeID [2];
-  nodeID_ [3] = nodeID [3];
-  nodeID_ [4] = nodeID [4];
-  nodeID_ [5] = nodeID [5];
+  for (int i = 0; i < 6; i++)
+    {
+      nodeID_ [i] = nodeID [i];
+    }
 }
 
 ACE_UUID ACE_UUID::NIL_UUID;
-
-/*
-ACE_UUID::UUID_t nil_uuid_t =
-    {0, 0, 0, 0, 0, 0, 0, {0, 0, 0, 0, 0, 0}};
-*/
 
 /// Construct a nil UUID. Such a UUID has every one of it's data
 /// elements set to zero.
@@ -51,6 +41,10 @@ ACE_UUID::ACE_UUID()
     clockSeqLow_ (0),
     as_string_ (0)
 {
+  ACE_NEW (node_,
+	   UUID_node);
+  
+  node_release_ = 1;
 }
 
 ACE_UUID::ACE_UUID(const ACE_UUID &right)
@@ -61,11 +55,12 @@ ACE_UUID::ACE_UUID(const ACE_UUID &right)
     clockSeqLow_ (0),
     as_string_ (0)
 {
+  node_release_ = 0;
   *this = right;
 }
 
 /// Construct a UUID from a string representation of an UUID.
-ACE_UUID::ACE_UUID (const ACE_SString& uuid_string)
+ACE_UUID::ACE_UUID (const ACE_CString& uuid_string)
   : timeLow_ (0),
     timeMid_ (0),
     timeHiAndVersion_ (0),
@@ -73,8 +68,15 @@ ACE_UUID::ACE_UUID (const ACE_SString& uuid_string)
     clockSeqLow_ (0),
     as_string_ (0)
 {
+  ACE_NEW (node_,
+	   UUID_node);
+  
+  node_release_ = 1;
+
   ACE_TRACE ("ACE_UUID::ACE_UUID");
  
+  int len1 = uuid_string.length ();
+  int len2 = NIL_UUID.to_string()->length();
   if (uuid_string.length() != NIL_UUID.to_string()->length()) 
     {
       ACE_DEBUG ((LM_DEBUG,
@@ -89,17 +91,17 @@ ACE_UUID::ACE_UUID (const ACE_SString& uuid_string)
       return;
     }
   
-  long timeLow;
-  int  timeMid;
-  int  timeHiAndVersion;
-  int  clockSeqHiAndReserved;
-  int  clockSeqLow;
-  int  node [UUID_node::NODE_ID_SIZE];
+  ACE_UINT64 timeLow;
+  ACE_UINT16  timeMid;
+  ACE_UINT16  timeHiAndVersion;
+  u_char  clockSeqHiAndReserved;
+  u_char  clockSeqLow;
+  u_char  node [UUID_node::NODE_ID_SIZE];
   
-  // This might seem quite strange seeing as this is ACE, but it 
-  // seems to be a bit difficult to write a facade for ::sscanf because
-  // some compilers dont support vsscanf, including MSVC. It appears
-  // that most platforms support sscanf though so we need to use it
+  // This might seem quite strange this being in ACE, but it seems to
+  // be a bit difficult to write a facade for ::sscanf because some
+  // compilers dont support vsscanf, including MSVC. It appears that
+  // most platforms support sscanf though so we need to use it
   // directly.
 #if defined (ACE_HAS_PACE)
   int nScanned = ::pace_sscanf(uuid_string.c_str(),
@@ -141,19 +143,20 @@ ACE_UUID::ACE_UUID (const ACE_SString& uuid_string)
 		  "IllegalArgument(invalid string representation)"));
     }
 
-  this->timeLow_ = ACE_static_cast (ACE_UINT32, timeLow);
-  this->timeMid_ = ACE_static_cast (ACE_UINT16, timeMid);
-  this->timeHiAndVersion_ = ACE_static_cast (ACE_UINT16, timeHiAndVersion);
-  this->clockSeqHiAndReserved_ = ACE_static_cast (u_char , clockSeqHiAndReserved);
-  this->clockSeqLow_ = ACE_static_cast (u_char , clockSeqLow);
+  this->timeLow_ =  timeLow;
+  this->timeMid_ =  timeMid;
+  this->timeHiAndVersion_ = timeHiAndVersion;
+  this->clockSeqHiAndReserved_ = clockSeqHiAndReserved;
+  this->clockSeqLow_ = clockSeqLow;
 
+  
   UUID_node::NodeID nodeID;
-  nodeID [0] = ACE_static_cast (u_char , node[0]);
-  nodeID [1] = ACE_static_cast (u_char , node[1]);
-  nodeID [2] = ACE_static_cast (u_char , node[2]);
-  nodeID [3] = ACE_static_cast (u_char , node[3]);
-  nodeID [4] = ACE_static_cast (u_char , node[4]);
-  nodeID [5] = ACE_static_cast (u_char , node[5]);
+  nodeID [0] = node[0];
+  nodeID [1] = node[1];
+  nodeID [2] = node[2];
+  nodeID [3] = node[3];
+  nodeID [4] = node[4];
+  nodeID [5] = node[5];
 
   this->node_->nodeID (nodeID);
 
@@ -166,9 +169,11 @@ ACE_UUID::ACE_UUID (const ACE_SString& uuid_string)
     }
   
   /// Support versions 1, 3, and 4 only
-  if (((this->timeHiAndVersion & 0xF000) != 0x1000) &&
-      ((this->timeHiAndVersion & 0xF000) != 0x3000) &&
-      ((this->timeHiAndVersion & 0xF000) != 0x4000)) 
+  ACE_UINT16 V1 = this->timeHiAndVersion_;
+  
+  if ((V1 & 0xF000) != 0x1000 &&
+      (V1 & 0xF000) != 0x3000 &&
+      (V1 & 0xF000) != 0x4000) 
     {
       ACE_DEBUG ((LM_DEBUG,
 		  "ACE_UUID::ACE_UUID - "
@@ -176,18 +181,20 @@ ACE_UUID::ACE_UUID (const ACE_SString& uuid_string)
     }
 }
 
-ACE_UUID::~ACE_UUID()
+ACE_UUID::~ACE_UUID (void)
 {
+  if (node_release_)
+    delete node_;
 }
 
-const ACE_SString*
+const ACE_CString*
 ACE_UUID::to_string (void)
 {
   /// Only compute the string representation once.
   if (as_string_ == 0) 
     {     
-      // Get a buffer exactly the correct size. Use the nil UUID as a gauge.
-      // Don't forget the trailing nul.   
+      // Get a buffer exactly the correct size. Use the nil UUID as a
+      // gauge.  Don't forget the trailing nul.
       int UUID_STRING_LENGTH = 36; ///+ uuid_.thr_id->length () + uuid_.pid->length ();
 
       char *buf;
@@ -212,29 +219,35 @@ ACE_UUID::to_string (void)
 		      (this->node_->nodeID ()) [4],
 		      (this->node_->nodeID ()) [5]);
 
-      this->as_string_ = new ACE_SString (buf, UUID_STRING_LENGTH + 1);
+      ACE_NEW_RETURN (this->as_string_,
+		      ACE_CString (buf, UUID_STRING_LENGTH),
+		      0);
     }
-
+  
   return as_string_;
 }
 
 ACE_UUID_Generator::ACE_UUID_Generator(void) 
-  : timeLast_ (0),
-    lock_ (0)
+  : timeLast_ (0)
 {   
+  ACE_NEW (lock_,
+	   ACE_SYNCH_MUTEX);
+  destroy_lock_ = 1;
 }
 
 ACE_UUID_Generator::~ACE_UUID_Generator()
 {
+  if (destroy_lock_)
+    delete lock_;
 }
 
 void
 ACE_UUID_Generator::init (void)
 {
-    ACE_OS::macaddr_node_t macaddress;  
+  ACE_OS::macaddr_node_t macaddress;  
   int result = 
     ACE_OS::getmacaddress (&macaddress); 
-
+  
   UUID_node::NodeID nodeID;  
   if (result != -1) 
     {
@@ -246,19 +259,15 @@ ACE_UUID_Generator::init (void)
 		  macaddress.node [3],
 		  macaddress.node [4],
 		  macaddress.node [5]));
-
-
+      
       ACE_OS::memcpy (&nodeID,
 		      macaddress.node, 
 		      sizeof (nodeID));
-
-      
-  
     }
   else 
     {
-      // Generate random node. This is pretty bad, but until
-      // ACE has a strong prng, this is what we can do.
+      // Generate random node. This is pretty bad, but until ACE has a
+      // strong prng, this is what we can do.
       nodeID [0] = static_cast<u_char>(ACE_OS::rand());
       nodeID [1] = static_cast<u_char>(ACE_OS::rand());
       nodeID [2] = static_cast<u_char>(ACE_OS::rand());
@@ -266,31 +275,13 @@ ACE_UUID_Generator::init (void)
       nodeID [4] = static_cast<u_char>(ACE_OS::rand());
       nodeID [5] = static_cast<u_char>(ACE_OS::rand());
     }
-
-  uuid_state_.node.nodeID (nodeID);
-  
-  
-  ACE_DEBUG ((LM_DEBUG,
-	      "%02X-%02X-%02X-%02X-%02X-%02X\n",
-	      (uuid_state_.node.nodeID ()) [0],
-	      (uuid_state_.node.nodeID ()) [1],
-	      (uuid_state_.node.nodeID ()) [2],
-	      (uuid_state_.node.nodeID ()) [3],
-	      (uuid_state_.node.nodeID ()) [4],
-	      (uuid_state_.node.nodeID ()) [5]));
-  
-  if (lock_ != 0)
-    {
-      ACE_GUARD (ACE_Lock, mon, *lock_);
-      this->get_timestamp (timeLast_);
-      uuid_state_.timestamp = timeLast_;
-    }
-  else
-    {
-      this->get_timestamp (timeLast_);
-      uuid_state_.timestamp = timeLast_;
-    }
-
+ 
+  { 
+    ACE_GUARD (ACE_SYNCH_MUTEX, mon, *lock_);
+    this->get_timestamp (timeLast_);
+    uuid_state_.timestamp = timeLast_;
+    uuid_state_.node.nodeID (nodeID);
+  }
 }
 
 
@@ -300,104 +291,85 @@ ACE_UUID_Generator::generateUUID (ACE_UUID& uuid)
   UUID_time timestamp;
   this->get_timestamp (timestamp);
 
+    
   // Construct a Version 1 UUID with the information in the arguements.
   uuid.timeLow (ACE_static_cast (ACE_UINT32, timestamp & 0xFFFFFFFF));
   uuid.timeMid (ACE_static_cast(ACE_UINT16, (timestamp >> 32) & 0xFFFF));
 
+  
   ACE_UINT16 tHAV = ACE_static_cast (ACE_UINT16, (timestamp >> 48) & 0xFFFF);
   tHAV |= (1 << 12);
   uuid.timeHiAndVersion (tHAV);
 
-  u_char cseqHAV = (uuid_state_.clockSequence & 0x3f00) >> 8;
-  cseqHAV |= 0x80;
-
-  if (lock_ != 0)
-    {
-      ACE_GUARD (ACE_Lock, mon, *lock_);
-      uuid.clockSeqLow (uuid_state_.clockSequence & 0xFF);
-      uuid.clockSeqHiAndReserved (cseqHAV);
-    }
-  else 
-    {
-      uuid.clockSeqLow (uuid_state_.clockSequence & 0xFF);
-      uuid.clockSeqHiAndReserved (cseqHAV);
-    }
-
-  uuid.node (&(uuid_state_.node));
-
-  /*
-  ACE_thread_t thr_id = ACE_OS::thr_self ();
-  ACE_hthread_t thr_handle;
-  ACE_OS::thr_self (thr_handle);
-  ACE_Thread_ID thread_id (thr_id, thr_handle);
-  char buf [BUFSIZ];
-  thread_id.to_string (buf);
-
-  uuid.thr_id = new ACE_CString (buf);
-  
-  ACE_OS::sprintf (buf, 
-		   "%d",
-		   ACE_static_cast (int, ACE_OS::getpid ()));
-  
-  uuid.pid = new ACE_CString (buf);
-  */
-
-  if (lock_ != 0)
-    {
-      ACE_GUARD (ACE_Lock, mon, *lock_);
-      uuid_state_.timestamp = timestamp;
-    }
-  else 
+  u_char cseqHAV;
+  {
+    ACE_GUARD (ACE_SYNCH_MUTEX, mon, *lock_);
+    uuid.clockSeqLow (uuid_state_.clockSequence & 0xFF);
+    cseqHAV = (uuid_state_.clockSequence & 0x3f00) >> 8;
     uuid_state_.timestamp = timestamp;
+  }
+
+  cseqHAV |= 0x80;
+  uuid.clockSeqHiAndReserved (cseqHAV);
+  uuid.node (&(uuid_state_.node));
+  
+  /*
+    ACE_thread_t thr_id = ACE_OS::thr_self ();
+    ACE_hthread_t thr_handle;
+    ACE_OS::thr_self (thr_handle);
+    ACE_Thread_ID thread_id (thr_id, thr_handle);
+    char buf [BUFSIZ];
+    thread_id.to_string (buf);
+
+    uuid.thr_id = new ACE_CString (buf);
+  
+    ACE_OS::sprintf (buf, 
+    "%d",
+    ACE_static_cast (int, ACE_OS::getpid ()));
+  
+    uuid.pid = new ACE_CString (buf);
+  */
 }
 
 ACE_UUID* 
 ACE_UUID_Generator::generateUUID (void)
 {
-    ACE_UUID* uuid;
-    ACE_NEW_RETURN (uuid,
-		    ACE_UUID,
-		    0);
+  ACE_UUID* uuid;
+  ACE_NEW_RETURN (uuid,
+		  ACE_UUID,
+		  0);
 
-    this->generateUUID (*uuid);
-    return uuid;
+  this->generateUUID (*uuid);
+  return uuid;
 }
 
-/// Obtain a new timestamp. If UUID's are being generated to quickly
+/// Obtain a new timestamp. If UUID's are being generated too quickly
 /// this method will 'spin' until enough time has elapsed.
 void 
 ACE_UUID_Generator::get_timestamp (UUID_time& timestamp)
 {   
   this->get_systemtime(timestamp);
   
-  /// Account for the clock being set back. Increment the clock sequence.
+  /// Account for the clock being set back. Increment the clock
+  /// sequence.
   if (timestamp <= timeLast_) 
     {
-      if (lock_ != 0)
-	{
-	  ACE_GUARD (ACE_Lock, mon, *lock_);
-	  uuid_state_.clockSequence = (uuid_state_.clockSequence + 1) & ACE_UUID_CLOCK_SEQ_MASK;
-	}
-      else
-	{
-	  uuid_state_.clockSequence = (uuid_state_.clockSequence + 1) & ACE_UUID_CLOCK_SEQ_MASK;
-	}
+      {
+	ACE_GUARD (ACE_SYNCH_MUTEX, mon, *lock_);
+	uuid_state_.clockSequence = (uuid_state_.clockSequence + 1) & ACE_UUID_CLOCK_SEQ_MASK;
+      }
     }
 
-  /// If the system time ticked since the last UUID was generated. Set the clock sequence back.
+  /// If the system time ticked since the last UUID was generated. Set
+  /// the clock sequence back.
   else if (timestamp > timeLast_)
     {
-      if (lock_ != 0)
-	{
-	  ACE_GUARD (ACE_Lock, mon, *lock_);
-	  uuid_state_.clockSequence = 0;
-	}
-      else
-	{
-	  uuid_state_.clockSequence = 0;
-	}
+      {
+	ACE_GUARD (ACE_SYNCH_MUTEX, mon, *lock_);
+	uuid_state_.clockSequence = 0;
+      }
     }
-
+  
   timeLast_ = timestamp;
 }
 
@@ -411,7 +383,7 @@ ACE_UUID_Generator::get_timestamp (UUID_time& timestamp)
  *   1 Jan 1900 - 1 Jan 1970: 70 years + 17 leap days.
  * This adds up, in days: (17+30+31+365*17+4)+(365*300+73)+(365*70+17) or
  * 122192928000000000U (0x1B21DD213814000) 100 ns ticks.
-*/
+ */
 void 
 ACE_UUID_Generator::get_systemtime(UUID_time& timestamp)
 {
@@ -421,6 +393,25 @@ ACE_UUID_Generator::get_systemtime(UUID_time& timestamp)
   ACE_Time_Value now = ACE_OS::gettimeofday();
   UUID_time time = now.sec() * 10000000 + now.usec() * 10;
   timestamp = time + timeOffset;
+}
+
+ACE_SYNCH_MUTEX*
+ACE_UUID_Generator::lock (void)
+{
+  return this->lock_;
+}
+
+ACE_SYNCH_MUTEX*
+ACE_UUID_Generator::lock (ACE_SYNCH_MUTEX* lock,
+			  int release_lock_)
+{
+  if (destroy_lock_)
+    delete lock_;
+
+  ACE_SYNCH_MUTEX* prev_lock = this->lock_;		
+  this->lock_ = lock;
+  this->destroy_lock_ = release_lock_;
+  return prev_lock;
 }
 
 #if !defined (__ACE_INLINE__)
