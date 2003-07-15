436485c2e8897bcdbcb0fd31a2b007f976d80485
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-15 15:44:59 +0000

    ChangeLogTag: Tue Jul 15 10:31:29 2003  Gan Deng  <gan.deng@vanderbilt.edu>

---
+++ TAO/tests/Bug_1551_Regression/Reply_Handler.h
@@ -0,0 +1,38 @@
+#ifndef REPLY_HANDLER_H
+#define REPLY_HANDLER_H
+/**
+ * @file
+ *
+ * $Id$
+ */
+#include "ace/pre.h"
+
+#include "TestS.h"
+
+class Reply_Handler
+  : public POA_Test::AMI_HelloHandler
+  , public PortableServer::RefCountServantBase
+{
+public:
+  Reply_Handler (
+      Test::Hello_ptr hello,
+      CORBA::ORB_ptr orb);
+
+  void short_sleep (ACE_ENV_SINGLE_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+  void short_sleep_excep (Test::AMI_HelloExceptionHolder * excep_holder
+      ACE_ENV_ARG_DECL)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+private:
+  void check_counter(ACE_ENV_SINGLE_ARG_DECL);
+
+private:
+  ACE_Atomic_Op<ACE_Thread_Mutex,long> counter_;
+  Test::Hello_var hello_;
+  CORBA::ORB_var orb_;
+};
+
+#include "ace/post.h"
+#endif /* REPLY_HANDLER_H */
