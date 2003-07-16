248670fc49eb9b71314c2571774ca43617b781a2
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-16 04:31:02 +0000

    ChangelogTag: Wed Jul 16 00:03:40 2003  Yamuna Krishnamurthy  <yamuna@oomworks.com>

---
+++ TAO/tests/RTScheduling/Thread_Cancel/test.cpp
@@ -0,0 +1,74 @@
+//$Id$
+#include "Thread_Task.h"
+#include "../Scheduler.h"
+#include "tao/RTScheduling/RTScheduler_Manager.h"
+#include "tao/ORB_Core.h"
+
+int
+main (int argc, char* argv [])
+{
+  ACE_TRY
+    {
+      CORBA::ORB_var orb =
+	CORBA::ORB_init (argc,
+			 argv,
+			 ""
+			 ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK_RETURN (-1);
+
+      CORBA::Object_ptr manager_obj = orb->resolve_initial_references ("RTSchedulerManager"
+								       ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK_RETURN (-1);
+      
+      TAO_RTScheduler_Manager_var manager = TAO_RTScheduler_Manager::_narrow (manager_obj
+									      ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK_RETURN (-1);
+      
+      TAO_Scheduler scheduler (orb.in ());
+      
+      manager->rtscheduler (&scheduler);
+      
+      Thread_Task task;
+      
+      task.activate_task (orb.in ());
+
+      ACE_DEBUG ((LM_DEBUG,
+		  "Waiting for Threads to Activate...\n"));
+      ACE_OS::sleep (10);
+      ACE_DEBUG ((LM_DEBUG,
+		  "Threads Activated\n"));
+
+
+      ACE_DEBUG ((LM_DEBUG,
+		  "Cancelling Threads.....\n"));
+
+      CORBA::Object_ptr current_obj = orb->resolve_initial_references ("RTScheduler_Current");
+      
+      RTScheduling::Current_var current = RTScheduling::Current::_narrow (current_obj
+									  ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+            
+      for (int i = 0; i < 4; i++)
+	{
+	  RTScheduling::DistributableThread_var DT = current->lookup ((task.guids ())[i]
+								      ACE_ENV_ARG_PARAMETER);
+	  ACE_TRY_CHECK;
+
+	  DT->cancel (ACE_ENV_ARG_PARAMETER);
+	}
+
+      orb->run ();
+
+      ACE_Thread_Manager::instance ()->wait ();
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Caught exception:");
+      return 1;
+    }
+  ACE_ENDTRY; 
+  
+  return 0;
+}
+
