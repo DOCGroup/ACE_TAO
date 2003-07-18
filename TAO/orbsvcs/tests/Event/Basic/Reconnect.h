5a80b73e583418df8f6e5fbda343a56b0a8ac5ff
Johnny Willemsen <jwillemsen@remedy.nl>
2003-07-18 15:15:34 +0000

    ChangeLogTag: Fri Jul 18 15:15:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- TAO/orbsvcs/tests/Event/Basic/Reconnect.h
+++ TAO/orbsvcs/tests/Event/Basic/Reconnect.h
@@ -1,18 +1,14 @@
 /* -*- C++ -*- */
-// $Id$
-//
-// ============================================================================
-//
-// = LIBRARY
-//   ORBSVCS Real-time Event Channel testsuite
-//
-// = FILENAME
-//   Reconnect
-//
-// = AUTHOR
-//   Carlos O'Ryan (coryan@cs.wustl.edu)
-//
-// ============================================================================
+//=============================================================================
+/**
+ *  @file   Reconnect.h
+ *
+ *  $Id$
+ *
+ *  @author Carlos O'Ryan (coryan@cs.wustl.edu)
+ */
+//=============================================================================
+
 
 #ifndef EC_RECONNECT_H
 #define EC_RECONNECT_H
@@ -23,54 +19,54 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+/**
+ * @class EC_Reconnect
+ *
+ * @brief Test the EC reconnection feature
+ *
+ * The EC can be configured to allow re-connection of suppliers
+ * and consumers, this test verifies that:
+ * + The EC does *not* allow reconnections if the feature is
+ * disabled (the default)
+ * + The EC does allow reconnections if the feature is enabled
+ * and:
+ * - There are no memory leaks
+ * - Compares the time required for a reconnection vs a complete
+ * connect/disconnect cycle, specially as the number of
+ * suppliers and consumers increases.
+ */
 class EC_Reconnect : public EC_Driver
 {
-  //
-  // = TITLE
-  //   Test the EC reconnection feature
-  //
-  // = DESCRIPTION
-  //   The EC can be configured to allow re-connection of suppliers
-  //   and consumers, this test verifies that:
-  //   + The EC does *not* allow reconnections if the feature is
-  //     disabled (the default)
-  //   + The EC does allow reconnections if the feature is enabled
-  //     and:
-  //     - There are no memory leaks
-  //     - Compares the time required for a reconnection vs a complete
-  //       connect/disconnect cycle, specially as the number of
-  //       suppliers and consumers increases.
-  //
 public:
+  /// Constructor
   EC_Reconnect (void);
-  // Constructor
 
   // = The EC_Driver methods
+  /// add some command line args to enable/disable reconnections
   virtual int parse_args (int& argc, char* argv[]);
   virtual void print_args (void) const;
   virtual void print_usage (void);
-  // add some command line args to enable/disable reconnections
 
+  /// set the reconnection flags
   virtual void modify_attributes (TAO_EC_Event_Channel_Attributes& attr);
-  // set the reconnection flags
 
+  /// Don't run the suppliers, just test connect and disconnect calls.
   void execute_test (ACE_ENV_SINGLE_ARG_DECL);
-  // Don't run the suppliers, just test connect and disconnect calls.
 
+  /// Don't dump the EC_Driver results, they are meaningless.
   void dump_results (void);
-  // Don't dump the EC_Driver results, they are meaningless.
 
+  /// Separate the suppliers and consumers.
   void execute_consumer_test (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS);
   void execute_supplier_test (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS);
-  // Separate the suppliers and consumers.
 
 private:
+  /// What aspect of reconnection are we going to test?
   int allow_consumer_reconnect_;
   int allow_supplier_reconnect_;
-  // What aspect of reconnection are we going to test?
 
+  /// The number of disconnections
   int disconnections_;
-  // The number of disconnections
 
   ACE_Throughput_Stats consumer_reconnect_;
   ACE_Throughput_Stats supplier_reconnect_;
--- TAO/orbsvcs/tests/Event/Basic/Shutdown.h
+++ TAO/orbsvcs/tests/Event/Basic/Shutdown.h
@@ -1,18 +1,14 @@
 /* -*- C++ -*- */
-// $Id$
-//
-// ============================================================================
-//
-// = LIBRARY
-//   ORBSVCS Real-time Event Channel testsuite
-//
-// = FILENAME
-//   Shutdown
-//
-// = AUTHOR
-//   Carlos O'Ryan (coryan@cs.wustl.edu)
-//
-// ============================================================================
+//=============================================================================
+/**
+ *  @file   Shutdown.h
+ *
+ *  $Id$
+ *
+ *  @author Carlos O'Ryan (coryan@cs.wustl.edu)
+ */
+//=============================================================================
+
 
 #ifndef EC_SHUTDOWN_H
 #define EC_SHUTDOWN_H
@@ -23,41 +19,41 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+/**
+ * @class EC_Shutdown
+ *
+ * @brief Test the EC shutdown features
+ *
+ * The EC must inform its suppliers and consumers on the event of
+ * its destruction.
+ * This test exercises that feature of the EC.
+ */
 class EC_Shutdown : public EC_Driver
 {
-  //
-  // = TITLE
-  //   Test the EC shutdown features
-  //
-  // = DESCRIPTION
-  //   The EC must inform its suppliers and consumers on the event of
-  //   its destruction.
-  //   This test exercises that feature of the EC.
-  //
 public:
+  /// Constructor
   EC_Shutdown (void);
-  // Constructor
 
   // = The EC_Driver methods
+  /// Don't run the suppliers just create the EC and then destroy it.
   void execute_test (ACE_ENV_SINGLE_ARG_DECL);
-  // Don't run the suppliers just create the EC and then destroy it.
 
+  /// Don't dump the EC_Driver results, they are meaningless.
   void dump_results (void);
-  // Don't dump the EC_Driver results, they are meaningless.
 
+  /// One of the consumers in the test has been disconnected from the EC
   virtual void consumer_disconnect (void* consumer_cookie
                                     ACE_ENV_ARG_DECL);
-  // One of the consumers in the test has been disconnected from the EC
 
+  /// One of the suppliers in the test has been disconnected from the EC
   virtual void supplier_disconnect (void* supplier_cookie
                                     ACE_ENV_ARG_DECL);
-  // One of the suppliers in the test has been disconnected from the EC
 
 
 private:
+  /// Number of consumer and supplier disconnect messages.
   int consumer_disconnects_;
   int supplier_disconnects_;
-  // Number of consumer and supplier disconnect messages.
 };
 
 #endif /* EC_SHUTDOWN_H */
