7a2188879f901f63179c101295f72bb78bc24544
kitty <kitty@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-18 15:34:31 +0000

    ChangeLogTag: Fri Jul 18 10:29:55 2003  Krishnakumar B  <kitty@nospam.invalid.domain>

--- trunk/ACEXML/tests/NamespaceSupport_Test.cpp
+++ trunk/ACEXML/tests/NamespaceSupport_Test.cpp
@@ -5,7 +5,12 @@
 int ACE_TMAIN (int, ACE_TCHAR *[])
 {
   ACEXML_NamespaceSupport xmlns;
-
+  if (xmlns.init() == -1)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  ACE_TEXT ("Error in initializing namespace support.")));
+      return 1;
+    }
   xmlns.pushContext();
   xmlns.declarePrefix(ACE_TEXT (""),
                       ACE_TEXT ("http://www.w3.org/1999/xhtml"));
