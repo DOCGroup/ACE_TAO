d8dd0e49b5de0168e5fee7c07d2e99bd98895d00
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-21 21:26:41 +0000

    ChangeLogTag: Mon Jul 21 16:24:07 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/tao/append.cpp
+++ trunk/TAO/tao/append.cpp
@@ -543,12 +543,34 @@ TAO_Marshal_Union::append (CORBA::TypeCode_ptr tc,
 
         case CORBA::tk_enum:
           {
-            const CORBA::ULong *d =
-              ACE_reinterpret_cast (const CORBA::ULong *,
-                                    any->value ());
-            if (*d == enum_v)
+            ACE_Message_Block *mb = any->_tao_get_cdr ();
+
+            if (mb != 0)
               {
-                current_member = i;
+                CORBA::ULong d = 0;
+                TAO_InputCDR cdr (mb->data_block (),
+                                  ACE_Message_Block::DONT_DELETE,
+                                  mb->rd_ptr () - mb->base (),
+                                  mb->wr_ptr () - mb->base (),
+                                  ACE_CDR_BYTE_ORDER,
+                                  TAO_DEF_GIOP_MAJOR,
+                                  TAO_DEF_GIOP_MINOR); 
+                cdr.read_ulong (d); 
+
+                if (d == enum_v)
+                  {
+                    current_member = i;
+                  }
+              }
+            else
+              {
+                const CORBA::ULong *d =
+                  ACE_reinterpret_cast (const CORBA::ULong *,
+                                        any->value ());
+                if (*d == enum_v)
+                  {
+                    current_member = i;
+                  }
               }
           }
           break;
