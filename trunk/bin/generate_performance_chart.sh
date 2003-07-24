bf95c45c7a2fc9274ca78f0cf70ea6b2b0702975
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-24 21:24:42 +0000

    ChangeLogTag:Thu Jul 24 16:23:53 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- trunk/bin/generate_performance_chart.sh
+++ trunk/bin/generate_performance_chart.sh
@@ -9,7 +9,7 @@ gnuplot <<_EOF_ >/dev/null 2>&1
     set xlabel 'Date (YYYYMMDD)'
     set ylabel 'Throughput (Requests/Second)'
     set terminal png small color
-    set yrange [2000:12000]
+    set yrange [4000:35000]
     set output "$2"
     plot '$1' using 1:2 title '$3' w l
     exit
