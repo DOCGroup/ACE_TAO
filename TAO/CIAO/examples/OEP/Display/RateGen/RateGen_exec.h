274c2a85de203f2c9dfaf7d8f7c932089fd8db62
dsevilla <dsevilla@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-07-31 16:25:41 +0000

    ChangeLogTag: 2003-07-31  Diego Sevilla  <dsevilla@ditec.um.es>

--- TAO/CIAO/examples/OEP/Display/RateGen/RateGen_exec.h
+++ TAO/CIAO/examples/OEP/Display/RateGen/RateGen_exec.h
@@ -161,8 +161,7 @@ namespace MyImpl
     virtual ::Components::EnterpriseComponent_ptr
     new_RateGen (CORBA::Long hertz
                  ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      ACE_THROW_SPEC ((CORBA::SystemException));
 
     // Implicit home operations.
 
