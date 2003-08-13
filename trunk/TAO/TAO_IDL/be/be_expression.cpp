03560827766de4377f52e22ba1e202307e8ac87b
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-08-13 21:38:13 +0000

    ChangeLogTag: Wed Aug 13 16:33:16 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/TAO_IDL/be/be_expression.cpp
+++ trunk/TAO/TAO_IDL/be/be_expression.cpp
@@ -58,8 +58,10 @@ be_expression::be_expression (long l,
 {
 }
 
-be_expression::be_expression (ACE_UINT64 l)
-  : AST_Expression (l)
+be_expression::be_expression (ACE_UINT64 l,
+                              AST_Expression::ExprType t)
+  : AST_Expression (l, 
+                    t)
 {
 }
 
