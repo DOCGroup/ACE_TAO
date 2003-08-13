f82244ce42799278d624f5985a9483292a519b7a
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-08-13 08:26:54 +0000

    ChangeLogTag: Wed Aug 13 08:24:12 UTC 2003  Johnny Willemsen <jwillemsen@remedy.nl>

--- trunk/TAO/orbsvcs/tests/EC_Throughput/run_test.pl
+++ trunk/TAO/orbsvcs/tests/EC_Throughput/run_test.pl
@@ -52,7 +52,7 @@ $NS = new PerlACE::Process ("../../Naming_Service/Naming_Service",
 $ES = new PerlACE::Process ("../../Event_Service/Event_Service",
                             "-ORBInitRef NameService=file://$ns_ior "
                             . " -ORBSvcConf $ec_conf "
-                            . " -t NEW");
+                            . " -t NEW -s local");
                             
 $C = new PerlACE::Process ("ECT_Consumer",
                            "-ORBInitRef NameService=file://$ns_ior "
