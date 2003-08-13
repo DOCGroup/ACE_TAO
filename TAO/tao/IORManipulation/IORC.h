72da7b5b5f1304bb05ff5ba9191778d5268d95f3
bala <balanatarajan@users.noreply.github.com>
2003-08-13 15:09:00 +0000

    ChangeLogTag:Wed Aug 13 10:08:01 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/tao/IORManipulation/IORC.h
+++ TAO/tao/IORManipulation/IORC.h
@@ -31,7 +31,7 @@
 #ifndef _TAO_IDL_ORIG_IORC_H_
 #define _TAO_IDL_ORIG_IORC_H_
 
-#include "ace/pre.h"
+#include /**/ "ace/pre.h"
 
 #include "tao/corba.h"
 
@@ -899,5 +899,5 @@ TAO_IORManip_Export CORBA::Boolean operator>> (TAO_InputCDR &, TAO_IOP::MultiPro
 #pragma option pop
 #endif /* __BORLANDC__ */
 
-#include "ace/post.h"
+#include /**/ "ace/post.h"
 #endif /* ifndef */
