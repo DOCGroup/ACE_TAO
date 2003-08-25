dfddb2fcc9652c48bbf13b0566acf6734980f56b
wilson_d <wilson_d@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-08-25 22:03:24 +0000

    ChangeLogTag: Mon Aug 25 16:28:22 2003  Dale Wilson  <wilson_d@ociweb.com>

---
+++ TAO/orbsvcs/Fault_Notifier/Fault_Notifier_Main.cpp
@@ -0,0 +1,105 @@
+/* -*- C++ -*- */
+//=============================================================================
+/**
+ *  @file    FaultNotifierMain.cpp
+ *
+ *  $Id$
+ *
+ *  This file is part of Fault Tolerant CORBA.
+ *  This file provides the main routine for a process that
+ *  implements the FaultNotifier interface and manages
+ *  a set of FaultNotifiers.
+ *
+ *  @author Dale Wilson <wilson_d@ociweb.com>
+ */
+//=============================================================================
+
+#include "ace/pre.h"
+#include "FT_Notifier_i.h"
+
+#include "ace/Argv_Type_Converter.h"
+#include "tao/PortableServer/ORB_Manager.h"
+
+int ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+{
+  int result = 0;
+  // Copy command line parameter.
+  // and hide it's unicodeness.
+  ACE_Argv_Type_Converter command_line(argc, argv);
+
+  char ** asciiArgv = command_line.get_ASCII_argv();
+
+  ACE_TRY_NEW_ENV
+  {
+    // Create an object that manages all the
+    // details of being a server.  It, too, gets to see the command line.
+    TAO_ORB_Manager orbManager;
+
+    result = orbManager.init (argc, asciiArgv
+        ACE_ENV_ARG_PARAMETER);
+    if(result == 0)
+    {
+      ACE_CHECK_RETURN (-1);
+
+      // create an instance of the notifier and give it the first
+      // chance at the arguments.
+      FT_FaultNotifier_i notifier;
+      result = notifier.parse_args (argc, asciiArgv);
+      if (result == 0)
+      {
+        //////////////////////////////////
+        // let the notifier register itself
+        result = notifier.self_register(orbManager);
+        if (result == 0)
+        {
+          ACE_ERROR ((LM_ERROR,
+            "%n\n%T: FaultNotifier Ready %s\n", notifier.identity()
+            ));
+
+          //////////////////////////////////
+          // Run the main event loop for the ORB.
+          result = orbManager.run (ACE_ENV_SINGLE_ARG_PARAMETER);
+          if (result == -1)
+          {
+            ACE_ERROR_RETURN (
+              (LM_ERROR, "%n\n%T: FT_Replica_i::run error"),
+              -1);
+          }
+          ACE_TRY_CHECK;
+          ACE_ERROR ((LM_ERROR,
+            "%n\n%T: Terminated normally. %s\n", notifier.identity()
+            ));
+        }
+        else
+        {
+          ACE_ERROR ((LM_ERROR,
+            "%n\n%T: FaultNotifier registration failed: %p\n"
+            ));
+          result = -1;
+        }
+      }
+      else
+      {
+        // error found parsing arguments
+        // we've already complained...
+      }
+    }
+    else
+    {
+      ACE_ERROR ((LM_ERROR,
+        "%n\n%T: ORB manager init failed\n"
+      ));
+      result = -1;
+    }
+  }
+  ACE_CATCHANY
+  {
+    ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+      "Fault Notifier::main\t\n");
+    result = -1;
+  }
+  ACE_ENDTRY;
+  return result;
+}
+
+#include "ace/post.h"
