9a8af57c918630d9052cebef307a7697ed145fac
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-09-04 14:44:00 +0000

    ChangeLogTag:Thu Sep  4 09:41:52 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- branches/refactor-stage_7/TAO/TAO_IDL/be/be_visitor_interface/any_op_cs.cpp
+++ branches/refactor-stage_7/TAO/TAO_IDL/be/be_visitor_interface/any_op_cs.cpp
@@ -93,6 +93,65 @@ be_visitor_interface_any_op_cs::visit_interface (be_interface *node)
       << ");" << be_uidt << be_uidt << be_uidt_nl
       << "}";
 
+  if (! node->is_abstract ())
+    {
+      *os << be_nl << be_nl
+          << "ACE_TEMPLATE_SPECIALIZATION" << be_nl
+          << "CORBA::Boolean" << be_nl
+          << "TAO::Any_Impl_T<" << node->name () << ">::to_object ("
+          << be_idt <<  be_idt_nl
+          << "CORBA::Object_ptr &_tao_elem" << be_uidt_nl
+          << ") const" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "_tao_elem = CORBA::Object::_duplicate (this->value_);" << be_nl
+          << "return 1;" << be_uidt_nl
+          << "}";
+    }
+
+  if (node->is_abstract () || node->has_mixed_parentage ())
+    {
+      *os << be_nl << be_nl
+          << "ACE_TEMPLATE_SPECIALIZATION" << be_nl
+          << "CORBA::Boolean" << be_nl
+          << "TAO::Any_Impl_T<" << node->name ()
+          << ">::to_abstract_base ("
+          << be_idt <<  be_idt_nl
+          << "CORBA::AbstractBase_ptr &_tao_elem" << be_uidt_nl
+          << ") const" << be_uidt_nl
+          << "{" << be_idt_nl
+          << "_tao_elem = CORBA::AbstractBase::_duplicate (this->value_);"
+          << be_nl
+          << "return 1;" << be_uidt_nl
+          << "}";
+    }
+
+  // Since we don't generate CDR stream operators for types that
+  // explicitly contain a local interface (at some level), we
+  // must override these Any template class methods to avoid
+  // calling the non-existent operators. The zero return value
+  // will eventually cause CORBA::MARSHAL to be raised if this
+  // type is inserted into an Any and then marshaled.
+  if (node->is_local ())
+    {
+      *os << be_nl << be_nl
+          << "ACE_TEMPLATE_SPECIALIZATION" << be_nl
+          << "CORBA::Boolean" << be_nl
+          << "TAO::Any_Impl_T<" << node->name ()
+          << ">::marshal_value (TAO_OutputCDR &)" << be_nl
+          << "{" << be_idt_nl
+          << "return 0;" << be_uidt_nl
+          << "}";
+
+      *os << be_nl << be_nl
+          << "ACE_TEMPLATE_SPECIALIZATION" << be_nl
+          << "CORBA::Boolean" << be_nl
+          << "TAO::Any_Impl_T<" << node->name ()
+          << ">::demarshal_value (TAO_InputCDR &)" << be_nl
+          << "{" << be_idt_nl
+          << "return 0;" << be_uidt_nl
+          << "}";
+    }
+
   // All we have to do is to visit the scope and generate code.
   if (!node->is_local ())
     {
