f5b77fc06e56aa781aba32604d735e4d28e41c3f
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-09-08 16:33:05 +0000

    ChangeLogTag: Mon Sep  8 11:21:14 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

---
+++ branches/refactor-stage_7/TAO/tao/DynamicInterface/DII_Arguments.cpp
@@ -0,0 +1,115 @@
+// $Id$
+
+#include "DII_Arguments.h"
+
+#include "tao/NVList.h"
+#include "tao/DynamicC.h"
+
+ACE_RCSID (DynamicInterface,
+           DII_Arguments,
+           "$Id$")
+
+#if !defined (__ACE_INLINE__)
+# include "DII_Arguments.inl"
+#endif /* ! __ACE_INLINE__ */
+
+namespace TAO
+{
+  CORBA::Boolean
+  NamedValue_Argument::demarshal (TAO_InputCDR &cdr)
+  {
+    ACE_TRY_NEW_ENV
+      {
+        this->x_->value ()->impl ()->_tao_decode (cdr
+                                                  ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
+      }
+    ACE_CATCHANY
+      {
+        return 0;
+      }
+    ACE_ENDTRY;
+
+    return 1;
+  }
+
+  void
+  NamedValue_Argument::interceptor_result (CORBA::Any *any)
+  {
+    (*any) <<= this->x_->value ();
+  }
+
+  // ========================================================================
+
+  CORBA::Boolean
+  NVList_Argument::marshal (TAO_OutputCDR &cdr)
+  {
+    ACE_TRY_NEW_ENV
+      {
+        this->x_->_tao_encode (cdr,
+                               CORBA::ARG_IN | CORBA::ARG_INOUT
+                               ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
+      }
+    ACE_CATCHANY
+      {
+        return 0;
+      }
+    ACE_ENDTRY;
+
+    return 1;
+  }
+
+  CORBA::Boolean
+  NVList_Argument::demarshal (TAO_InputCDR &cdr)
+  {
+    ACE_TRY_NEW_ENV
+      {
+        this->x_->_tao_decode (cdr,
+                               CORBA::ARG_INOUT | CORBA::ARG_OUT
+                               ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
+      }
+    ACE_CATCHANY
+      {
+        return 0;
+      }
+    ACE_ENDTRY;
+
+    return 1;
+  }
+
+  void
+  NVList_Argument::interceptor_paramlist (Dynamic::ParameterList *list)
+  {
+    const CORBA::ULong len = this->x_->count ();
+    list->length (len);
+
+    for (CORBA::ULong i = 0; i < len; ++i)
+      {
+        (*list)[len].argument <<= *this->x_->item (len)->value ();
+
+        switch (this->x_->item (len)->flags ())
+          {
+          case CORBA::ARG_IN:
+            {
+              (*list)[len].mode = CORBA::PARAM_IN;
+              break;
+            }
+          case CORBA::ARG_INOUT:
+            {
+              (*list)[len].mode = CORBA::PARAM_INOUT;
+              break;
+            }
+          case CORBA::ARG_OUT:
+            {
+              (*list)[len].mode = CORBA::PARAM_OUT;
+              break;
+            }
+          default:
+            break;
+          }
+      }
+  }
+}
+
