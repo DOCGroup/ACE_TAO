735f7b6d8ba95a4f761325e308d357efed955b0d
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-09-24 14:10:28 +0000

    ChangeLogTag:Wed Sep 24 09:04:42 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/orbsvcs/orbsvcs/FaultTolerance/FT_ClientRequest_Interceptor.h
+++ TAO/orbsvcs/orbsvcs/FaultTolerance/FT_ClientRequest_Interceptor.h
@@ -21,6 +21,7 @@
 #include "tao/PortableInterceptorC.h"
 #include "tao/LocalObject.h"
 #include "tao/TimeBaseC.h"
+#include "ace/Singleton.h"
 
 // This is to remove "inherits via dominance" warnings from MSVC.
 // MSVC is being a little too paranoid.
@@ -36,74 +37,108 @@ namespace ACE_Utils
 {
   class UUID;
 }
-/**
- * @class TAO_FT_ClientRequest_Interceptor
- *
- * @brief Interceptors for the client
- *
- *  @@TODO
- */
-class TAO_FT_Export TAO_FT_ClientRequest_Interceptor
-  : public virtual PortableInterceptor::ClientRequestInterceptor,
-    public virtual TAO_Local_RefCounted_Object
+
+namespace TAO
 {
-public:
+  /**
+   * @class FT_TSS
+   *
+   * @brief Request service context generator class for the client.
+   *
+   * @@TODO Need to see whether we can have seperate resource factory
+   * and put this there.  Let us get this going first
+   */
+  class FT_TSS
+  {
+  public:
+    CORBA::Long retention_id_;
 
-  TAO_FT_ClientRequest_Interceptor (void);
-
-  ~TAO_FT_ClientRequest_Interceptor (void);
-
-  /// Canonical name of the interceptor.
-  virtual char * name (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+    bool clean_flag_;
+  };
 
 
-  virtual void destroy (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+  /**
+   * @class FT_ClientRequest_Interceptor
+   *
+   * @brief Interceptors for the client
+   *
+   *  @@TODO
+   */
+  class TAO_FT_Export FT_ClientRequest_Interceptor
+    : public virtual PortableInterceptor::ClientRequestInterceptor,
+      public virtual TAO_Local_RefCounted_Object
+  {
+  public:
 
-  virtual void send_poll (PortableInterceptor::ClientRequestInfo_ptr
-                          ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+    FT_ClientRequest_Interceptor (void);
 
-  virtual void send_request (PortableInterceptor::ClientRequestInfo_ptr ri
-                             ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException,
-                     PortableInterceptor::ForwardRequest));
+    ~FT_ClientRequest_Interceptor (void);
 
-  virtual void receive_reply (PortableInterceptor::ClientRequestInfo_ptr ri
-                              ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+    /// Canonical name of the interceptor.
+    virtual char * name (ACE_ENV_SINGLE_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
 
-  virtual void receive_other (PortableInterceptor::ClientRequestInfo_ptr
-                              ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException,
-                     PortableInterceptor::ForwardRequest));
 
-  virtual void receive_exception (PortableInterceptor::ClientRequestInfo_ptr ri
-                                  ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException,
-                    PortableInterceptor::ForwardRequest));
+    virtual void destroy (ACE_ENV_SINGLE_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
 
-private:
+    virtual void send_poll (PortableInterceptor::ClientRequestInfo_ptr
+                            ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
 
-  void group_version_context (PortableInterceptor::ClientRequestInfo_ptr ri
-                              ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+    virtual void send_request (PortableInterceptor::ClientRequestInfo_ptr ri
+                               ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException,
+                       PortableInterceptor::ForwardRequest));
 
-  void request_service_context (PortableInterceptor::ClientRequestInfo_ptr ri
-                                ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+    virtual void receive_reply (PortableInterceptor::ClientRequestInfo_ptr ri
+                                ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
 
-  TimeBase::TimeT request_expiration_time (CORBA::Policy *policy
-                                           ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-    ACE_THROW_SPEC ((CORBA::SystemException));
+    virtual void receive_other (PortableInterceptor::ClientRequestInfo_ptr
+                                ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException,
+                       PortableInterceptor::ForwardRequest));
 
-private:
-  const char *name_;
+    virtual void receive_exception (PortableInterceptor::ClientRequestInfo_ptr ri
+                                    ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException,
+                       PortableInterceptor::ForwardRequest));
 
-  ACE_Utils::UUID *uuid_;
-};
+  private:
 
+    void group_version_context (PortableInterceptor::ClientRequestInfo_ptr ri
+                                ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+    void request_service_context (PortableInterceptor::ClientRequestInfo_ptr ri
+                                  ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+    TimeBase::TimeT request_expiration_time (CORBA::Policy *policy
+                                             ACE_ENV_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException));
+
+    FT_TSS *tss_resources (void);
+
+  private:
+
+    /// Name of the interceptor
+    const char *name_;
+
+    /// UUID for this client
+    ACE_Utils::UUID *uuid_;
+
+    /// TSS data
+    ACE_TSS_TYPE (FT_TSS) tss_;
+
+  /// @@ Need a resource factory for this too
+    ACE_Lock *lock_;
+
+    /// Retention id generated by the client
+    CORBA::Long retention_id_;
+  };
+}
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 #pragma warning(pop)
 #endif /* _MSC_VER */
