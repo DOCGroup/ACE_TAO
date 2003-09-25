c28f3c58ed6c8e5071b3320f8d9e7a809ea4fd77
bala <balanatarajan@users.noreply.github.com>
2003-09-25 16:47:42 +0000

    ChangeLogTag:Thu Sep 25 11:46:28 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/orbsvcs/orbsvcs/PortableGroup/UIPMC_Connector.h
+++ TAO/orbsvcs/orbsvcs/PortableGroup/UIPMC_Connector.h
@@ -62,9 +62,9 @@ protected:
   ///   Transport_Connector.h
   int set_validate_endpoint (TAO_Endpoint *endpoint);
 
-  int make_connection (TAO_GIOP_Invocation *,
-                       TAO_Transport_Descriptor_Interface *,
-                       ACE_Time_Value *timeout);
+  TAO_Transport *make_connection (TAO::Profile_Transport_Resolver *r,
+                                  TAO_Transport_Descriptor_Interface &desc,
+                                  ACE_Time_Value *timeout = 0);
 
   virtual TAO_Profile * make_profile (ACE_ENV_SINGLE_ARG_DECL);
 
