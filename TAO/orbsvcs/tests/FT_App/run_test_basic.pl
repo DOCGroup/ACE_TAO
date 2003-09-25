aebe66ac0a158745457f11d8158304dd761a7613
wilson_d <wilson_d@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-09-25 21:57:09 +0000

    ChangeLogTag: Thu Sep 25 16:08:35 2003  Dale Wilson  <wilson_d@ociweb.com>

--- TAO/orbsvcs/tests/FT_App/run_test_basic.pl
+++ TAO/orbsvcs/tests/FT_App/run_test_basic.pl
@@ -5,29 +5,34 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-use lib '../../../bin';
+use lib '../../../../bin';
 use PerlACE::Run_Test;
 
-my($debug) = 0;
+my($debug) = 1;
 
-my($iorfile1) = PerlACE::LocalFile ("test1.ior");
-my($iorfile2) = PerlACE::LocalFile ("test2.ior");
-unlink $iorfile1;
-unlink $iorfile2;
+
+my($factory1_ior) = PerlACE::LocalFile ("factory1.ior");
+my($factory2_ior) = PerlACE::LocalFile ("factory2.ior");
+my($replica1_ior) = PerlACE::LocalFile ("replica1.ior");
+my($replica2_ior) = PerlACE::LocalFile ("replica2.ior");
+
+unlink $factory1_ior;
+unlink $replica1_ior;
+unlink $replica2_ior;
 my($status) = 0;
 
-my($SV1) = new PerlACE::Process ("ft_replica", "-o $iorfile1");
-my($SV2) = new PerlACE::Process ("ft_replica", "-o $iorfile2");
-my($CL) = new PerlACE::Process ("ft_client", "-f $iorfile1,$iorfile2 -c testscript");
-#my($CL) = new PerlACE::Process ("ft_client", "-f $iorfile1 -c testscript");
+my($SV1) = new PerlACE::Process ("release/ft_replica", "-o $factory1_ior -t $replica1_ior -q");
+my($SV2) = new PerlACE::Process ("release/ft_replica", "-o $factory2_ior -t $replica2_ior -q");
+my($CL) = new PerlACE::Process ("release/ft_client", "-f $replica1_ior,$replica2_ior -c testscript");
+#my($CL) = new PerlACE::Process ("release/ft_client", "-f $replica1_ior -c testscript");
 
 if ($debug) {print "starting replica 1\n";}
 $SV1->Spawn ();
 
 if ($debug) {print "waiting for replica 1's IOR\n";}
 
-if (PerlACE::waitforfile_timed ($iorfile1, 5) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile1>\n";
+if (PerlACE::waitforfile_timed ($replica1_ior, 5) == -1) {
+    print STDERR "ERROR: cannot find replica 1 file <$replica1_ior>\n";
     $SV1->Kill (); $SV1->TimedWait (1);
     exit 1;
 }
@@ -36,8 +41,8 @@ if ($debug) {print "starting replica 2\n";}
 $SV2->Spawn ();
 
 if ($debug) {print "waiting for replica 2's IOR\n";}
-if (PerlACE::waitforfile_timed ($iorfile2, 5) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile2>\n";
+if (PerlACE::waitforfile_timed ($replica2_ior, 5) == -1) {
+    print STDERR "ERROR: cannot find replica 2 file <$replica2_ior>\n";
     $SV1->Kill (); $SV1->TimedWait (1);
     $SV2->Kill (); $SV2->TimedWait (1);
     exit 1;
@@ -64,8 +69,8 @@ if ($server != 0) {
 }
 
 if ($debug) {print "releasing scratch files.\n";}
-unlink $iorfile1;
-unlink $iorfile2;
+unlink $replica1_ior;
+unlink $replica2_ior;
 unlink "Persistent.dat";
 
 exit $status;
