763cf8591719309ea56c734d7c928c444ef4301a
boris <boris@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-02 20:04:07 +0000

    ChangeLogTag: Thu Oct  2 15:02:10 2003  Boris Kolpackov  <boris@dre.vanderbilt.edu>

--- TAO/CIAO/CCF/CCF/IDL2/SyntaxTree.hpp
+++ TAO/CIAO/CCF/CCF/IDL2/SyntaxTree.hpp
@@ -6,12 +6,12 @@
 #define CCF_IDL2_SYNTAX_TREE_HPP
 
 #include "CCF/IDL2/SyntaxTree/Elements.hpp"
-#include "CCF/IDL2/SyntaxTree/TypeId.hpp"
 #include "CCF/IDL2/SyntaxTree/BuiltIn.hpp"
 #include "CCF/IDL2/SyntaxTree/Interface.hpp"
 #include "CCF/IDL2/SyntaxTree/Module.hpp"
 #include "CCF/IDL2/SyntaxTree/Operation.hpp"
 #include "CCF/IDL2/SyntaxTree/Translation.hpp"
+#include "CCF/IDL2/SyntaxTree/TypeId.hpp"
 #include "CCF/IDL2/SyntaxTree/ValueType.hpp"
 
 #endif  // CCF_IDL2_SYNTAX_TREE_HPP
