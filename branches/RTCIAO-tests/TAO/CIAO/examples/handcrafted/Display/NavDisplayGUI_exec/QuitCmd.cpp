1a2dd4846bd1b43edc22d8f6d263de5546cc79a7
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-09 03:26:32 +0000

    This commit was manufactured by cvs2svn to create branch 'RTCIAO-tests'.

---
+++ branches/RTCIAO-tests/TAO/CIAO/examples/handcrafted/Display/NavDisplayGUI_exec/QuitCmd.cpp
@@ -0,0 +1,25 @@
+// $Id$
+
+#include "QuitCmd.h"
+#include "RootPanel.h"
+
+
+QuitCmd::QuitCmd()
+{
+}
+
+
+QuitCmd *
+QuitCmd::create(RootPanel *form)
+{
+  QuitCmd *cmd = new QuitCmd();
+  cmd->form_ = form;
+  return cmd;
+}
+
+
+int
+QuitCmd::execute(void *context)
+{
+  return form_->close();
+}
