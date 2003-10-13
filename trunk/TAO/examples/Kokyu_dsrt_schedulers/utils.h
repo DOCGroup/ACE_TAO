c8619b33a1147a7631881cb56c8536986ed94137
venkita <venkita@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-13 20:36:56 +0000

    ChangeLogTag: Mon Oct 13 15:29:09 2003  Venkita Subramonian  <venkita@cs.wustl.edu>

--- trunk/TAO/examples/Kokyu_dsrt_schedulers/utils.h
+++ trunk/TAO/examples/Kokyu_dsrt_schedulers/utils.h
@@ -2,7 +2,7 @@
 
 #include "ace/OS.h"
 #include "Kokyu_qosC.h"
-#include "RTScheduler.h"
+#include "tao/RTScheduling/RTScheduler.h"
 
 void guid_copy( Kokyu::GuidType& lhs, const RTScheduling::Current::IdType& rhs);
 void guid_copy( RTScheduling::Current::IdType& lhs, const Kokyu::GuidType& rhs);
