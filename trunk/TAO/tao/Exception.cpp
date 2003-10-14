8543c51e462f98ff0eca4927ad6321d4ac208a32
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-14 22:52:29 +0000

    ChangeLog:Tue Oct 14 17:51:14 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- trunk/TAO/tao/Exception.cpp
+++ trunk/TAO/tao/Exception.cpp
@@ -1035,6 +1035,17 @@ TAO_Exceptions::make_standard_typecode (CORBA::TypeCode_ptr &tcp,
   return;
 }
 
+#if defined (__Lynx__)
+
+// LynxOS uses macro THREAD_CANCELLED internally, so it must
+// be undefined to avoid compilation errors
+#if defined (THREAD_CANCELLED)
+#undef THREAD_CANCELLED
+#endif /* THREAD_CANCELLED */
+
+#endif /*__Lynx__ */
+
+
 // List of standard/system exceptions ... used to create static
 // storage for their typecodes, then later to initialize that storage
 // using the routine above. (It's just too painful to init these
