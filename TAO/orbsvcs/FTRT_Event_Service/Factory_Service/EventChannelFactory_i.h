07fb6fc767601c896b688bfa1c80f12afe5f2c7d
huangming <huangminghuang@users.noreply.github.com>
2003-10-14 06:03:52 +0000

    *** empty log message ***

--- TAO/orbsvcs/FTRT_Event_Service/Factory_Service/EventChannelFactory_i.h
+++ TAO/orbsvcs/FTRT_Event_Service/Factory_Service/EventChannelFactory_i.h
@@ -8,7 +8,7 @@
  *
  *  @author Huang-Ming Huang <hh1@cse.wustl.edu>
  */
-//=============================================================================#ifndef FT_EVENTSERVICE_H
+//=============================================================================
 
 #ifndef EVENTCHANNELFACTORY_I_H
 #define EVENTCHANNELFACTORY_I_H
@@ -19,47 +19,47 @@
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
-#endif /* ACE_LACKS_PRAGMA_ONCE */ 
+#endif /* ACE_LACKS_PRAGMA_ONCE */
 
 class EventChannelFactory_i : public POA_FT::GenericFactory {
 public:
-	EventChannelFactory_i(const char* conf_filename, CORBA::ORB_ptr);
+  EventChannelFactory_i(const char* conf_filename, CORBA::ORB_ptr);
 
-	virtual CORBA::Object_ptr create_object (
-		const char * type_id,
-		const FT::Criteria & the_criteria,
-		FT::GenericFactory::FactoryCreationId_out factory_creation_id
-		ACE_ENV_ARG_DECL_WITH_DEFAULTS
-		)
-		ACE_THROW_SPEC ((
-		CORBA::SystemException
-		, FT::NoFactory
-		, FT::ObjectNotCreated
-		, FT::InvalidCriteria
-		, FT::InvalidProperty
-		, FT::CannotMeetCriteria
-		)) ;
+  virtual CORBA::Object_ptr create_object (
+    const char * type_id,
+    const FT::Criteria & the_criteria,
+    FT::GenericFactory::FactoryCreationId_out factory_creation_id
+    ACE_ENV_ARG_DECL_WITH_DEFAULTS
+    )
+    ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , FT::NoFactory
+    , FT::ObjectNotCreated
+    , FT::InvalidCriteria
+    , FT::InvalidProperty
+    , FT::CannotMeetCriteria
+    )) ;
 
-	virtual void delete_object (
-		const FT::GenericFactory::FactoryCreationId & factory_creation_id
-		ACE_ENV_ARG_DECL_WITH_DEFAULTS
-		)
-		ACE_THROW_SPEC ((
-		CORBA::SystemException
-		, FT::ObjectNotFound
-		));
+  virtual void delete_object (
+    const FT::GenericFactory::FactoryCreationId & factory_creation_id
+    ACE_ENV_ARG_DECL_WITH_DEFAULTS
+    )
+    ACE_THROW_SPEC ((
+    CORBA::SystemException
+    , FT::ObjectNotFound
+    ));
 
 private:
   CORBA::Object_ptr create_process (
-		char * process,
-		const FT::Criteria & the_criteria,
-		FT::GenericFactory::FactoryCreationId_out factory_creation_id);
+    char * process,
+    const FT::Criteria & the_criteria,
+    FT::GenericFactory::FactoryCreationId_out factory_creation_id);
 
-	const char* conf_file;
-	int id;
-	CORBA::ORB_ptr orb;
-	typedef ACE_Hash_Map_Manager<int, CORBA::Object_var, ACE_Null_Mutex> Objects;
-	Objects objects;
+  const char* conf_file;
+  int id;
+  CORBA::ORB_ptr orb;
+  typedef ACE_Hash_Map_Manager<int, CORBA::Object_var, ACE_Null_Mutex> Objects;
+  Objects objects;
 };
 #endif
 
