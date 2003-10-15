662c4caae556d3e18e5ae5005408cd0160bc3a63
huangming <huangming@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-15 18:07:27 +0000

    Wed Oct 15 13:00:24  2003  Huang-Ming Huang  <hh1@cse.wustl.edu>

---
+++ TAO/orbsvcs/orbsvcs/FtRtEvent/EventChannel/GroupInfoPublisher.cpp
@@ -0,0 +1,130 @@
+// $Id$
+
+#include "GroupInfoPublisher.h"
+#include "../Utils/resolve_init.h"
+#include "IOGR_Maker.h"
+#include "Identification_Service.h"
+#include "FTEC_Become_Primary_Listener.h"
+
+ACE_RCSID (EventChannel,
+           GroupInfoPublisher,
+           "$Id$")
+
+
+
+GroupInfoPublisherBase::GroupInfoPublisherBase()
+: primary_(false)
+{
+}
+
+
+
+void
+GroupInfoPublisherBase::subscribe(TAO_FTEC_Become_Primary_Listener* listener)
+{
+  subscribers_.push_back(listener);
+}
+
+void GroupInfoPublisherBase::set_naming_context(CosNaming::NamingContext_var naming_context)
+{
+  naming_context_ = naming_context;
+}
+
+bool
+GroupInfoPublisherBase::is_primary() const
+{
+  return primary_;
+}
+
+CORBA::Object_var
+GroupInfoPublisherBase::group_reference() const
+{
+  return iogr_;
+}
+
+FtRtecEventChannelAdmin::EventChannel_var
+GroupInfoPublisherBase::successor() const
+{
+  return successor_;
+}
+
+
+
+const GroupInfoPublisherBase::BackupList&
+GroupInfoPublisherBase::backups() const
+{
+  return backups_;
+}
+
+void
+GroupInfoPublisherBase::update(const FTRT::ManagerInfoList & info_list,
+                           int my_position
+                           ACE_ENV_ARG_DECL_WITH_DEFAULTS)
+{
+  bool become_primary = (my_position == 0 && !primary_);
+  primary_ = (my_position == 0);
+
+  /// create the object group
+  size_t len = info_list.length();
+
+  TAO_IOP::TAO_IOR_Manipulation::IORList iors;
+  iors.length(len);
+
+  size_t i;
+  for (i = 0; i < len; ++i) {
+    iors[i] = CORBA::Object::_duplicate(info_list[i].ior.in());
+  }
+
+  CORBA::Object_var obj =
+    IOGR_Maker::instance()->make_iogr(iors ACE_ENV_ARG_PARAMETER);
+  ACE_CHECK;
+
+  iogr_ =
+    ::FtRtecEventChannelAdmin::EventChannel::_narrow(obj.in()
+                                                     ACE_ENV_SINGLE_ARG_PARAMETER);
+  ACE_CHECK;
+
+  if (primary_ && !CORBA::is_nil(naming_context_.in())) {
+    ACE_DEBUG((LM_DEBUG, "Registering to the Name Service\n"));
+    naming_context_->rebind(FTRTEC::Identification_Service::instance()->name(),
+                            iogr_.in() ACE_ENV_ARG_PARAMETER);
+    ACE_CHECK;
+  }
+
+  /// check if sucessor changed
+  size_t successors_length = info_list.length() - my_position -1;
+
+  if (successors_length != backups_.length())  {
+    // successor changed, update successor
+    iors.length(successors_length);
+    for (i = 0; i < successors_length; ++i) {
+      iors[i] = CORBA::Object::_duplicate(info_list[i+ my_position+1].ior.in());
+
+    }
+
+    obj =  IOGR_Maker::instance()->merge_iors(iors
+                                              ACE_ENV_ARG_PARAMETER);
+    ACE_CHECK;
+
+    successor_ = FtRtecEventChannelAdmin::EventChannel::_narrow(obj.in()
+                                                                ACE_ENV_ARG_PARAMETER);
+    FtRtecEventChannelAdmin::EventChannel_var t = successor_;
+    ACE_CHECK;
+
+    // update backups
+    backups_.length(successors_length);
+    for (i = 0; i < successors_length; ++i)  {
+      backups_[i] =
+        FtRtecEventChannelAdmin::EventChannel::_narrow(
+                      info_list[i+ my_position+1].ior.in()
+                      ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK;
+    }
+  }
+
+  if (become_primary) {
+    // notify the subscribers
+    for (i = 0; i < subscribers_.size(); ++i)
+      subscribers_[i]->become_primary();
+  }
+}
---
+++ TAO/orbsvcs/orbsvcs/FtRtEvent/EventChannel/Replication_Service.cpp
@@ -0,0 +1,150 @@
+// $Id$
+
+#include "Replication_Service.h"
+#include "AMI_Replication_Strategy.h"
+#include "Basic_Replication_Strategy.h"
+#include "FTEC_ORBInitializer.h"
+
+ACE_RCSID (EventChannel,
+           Replication_Service,
+           "$Id$")
+
+
+
+
+namespace FTRTEC
+{
+  namespace {
+    auto_ptr<Replication_Strategy> replication_strategy;
+    Replication_Service* service;
+  }
+
+  Replication_Service* Replication_Service::instance()
+  {
+    return service;
+  }
+
+  Replication_Service::Replication_Service()
+  {
+    service = this;
+  }
+
+  Replication_Service::~Replication_Service()
+  {
+  }
+
+  int Replication_Service::init (int argc, ACE_TCHAR* argv[])
+  {
+    ACE_TRACE ("Replication_Service::init");
+
+    static int initialized = 0;
+
+    // Only allow initialization once.
+    if (initialized)
+      return 0;
+
+    initialized = 1;
+
+    // Parse any service configurator parameters.
+    if (argc > 0 && ACE_OS::strcasecmp (argv[0], ACE_LIB_TEXT("AMI")) == 0)
+      replication_strategy.reset(new AMI_Replication_Strategy);
+    else
+      replication_strategy.reset(new Basic_Replication_Strategy);
+
+      ACE_TRY_NEW_ENV
+      {
+        PortableInterceptor::ORBInitializer_ptr temp_orb_initializer =
+          PortableInterceptor::ORBInitializer::_nil ();
+        PortableInterceptor::ORBInitializer_var orb_initializer;
+
+        /// Register the RTCORBA ORBInitializer.
+        ACE_NEW_THROW_EX (temp_orb_initializer,
+          FTEC_ORBInitializer,
+          CORBA::NO_MEMORY ());
+        ACE_TRY_CHECK;
+        orb_initializer = temp_orb_initializer;
+
+        PortableInterceptor::register_orb_initializer (orb_initializer.in ()
+          ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
+      }
+      ACE_CATCHANY
+      {
+        ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+          "Unexpected exception caught while "
+          "initializing the TransactionDepth");
+        return 1;
+      }
+      ACE_ENDTRY;
+    return 0;
+  }
+
+
+  void Replication_Service::become_primary()
+  {
+    Replication_Strategy* strategy =
+      replication_strategy->make_primary_strategy();
+    if (strategy == 0)
+      ACE_THROW(CORBA::NO_MEMORY());
+
+    if (replication_strategy.get() != strategy) {
+      replication_strategy.reset(strategy);
+    }
+  }
+
+  void Replication_Service::check_validity(ACE_ENV_SINGLE_ARG_DECL)
+  {
+    replication_strategy->check_validity(ACE_ENV_SINGLE_ARG_PARAMETER);
+  }
+
+
+  void Replication_Service::replicate_request(const FtRtecEventChannelAdmin::Operation& update,
+    RollbackOperation rollback
+    ACE_ENV_ARG_DECL)
+  {
+    TAO_OutputCDR cdr;
+    cdr << update;
+
+    ACE_Message_Block mb;
+    ACE_CDR::consolidate(&mb, cdr.begin());
+    FTRT::State state(mb.length(), &mb);
+    //FT::State state;
+
+    replication_strategy->replicate_request(
+      state,
+      rollback,
+      update.object_id
+      ACE_ENV_ARG_PARAMETER);
+  }
+
+  int  Replication_Service::acquire_read (void)
+  {
+    int r =  replication_strategy->acquire_read();
+    ACE_DEBUG((LM_DEBUG, "Read Lock acquired %d\n", r));
+    return r;
+  }
+
+  int  Replication_Service::acquire_write (void)
+  {
+    int r= replication_strategy->acquire_write();
+    ACE_DEBUG((LM_DEBUG, "Write Lock acqured %d\n", r));
+    return r;
+  }
+
+  int  Replication_Service::release (void)
+  {
+    int r= replication_strategy->release();
+    ACE_DEBUG((LM_DEBUG, "Lock Released %d\n", r));
+    return r;
+  }
+
+  ACE_FACTORY_DEFINE (TAO_FTRTEC, Replication_Service);
+
+  ACE_STATIC_SVC_DEFINE (Replication_Service,
+    ACE_TEXT ("FTRTEC_Replication"),
+    ACE_SVC_OBJ_T,
+    &ACE_SVC_NAME (Replication_Service),
+    ACE_Service_Type::DELETE_THIS
+    | ACE_Service_Type::DELETE_OBJ,
+    0);
+}
