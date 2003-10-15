416c517a6e16fd04c87891bee8542f87a80f3f6d
huangming <huangming@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-15 18:07:27 +0000

    Wed Oct 15 13:00:24  2003  Huang-Ming Huang  <hh1@cse.wustl.edu>

---
+++ trunk/TAO/orbsvcs/orbsvcs/FtRtEvent/Utils/activate_with_id.h
@@ -0,0 +1,33 @@
+// -*- C++ -*-
+//=============================================================================
+/**
+ *  @file   activate_with_id.h
+ *
+ *  $Id$
+ *
+ *  @author Huang-Ming Huang <hh1@cse.wustl.edu>
+ */
+//=============================================================================
+#ifndef ACTIVATE_WITH_ID_H
+#define ACTIVATE_WITH_ID_H
+
+template<class T>
+void
+activate_object_with_id (T * &result,
+                         PortableServer::POA_var poa,
+                         PortableServer::ServantBase *servant,
+                         const PortableServer::ObjectId &oid
+                           ACE_ENV_ARG_DECL_WITH_DEFAULTS)
+{
+    poa->activate_object_with_id(oid,
+                                 servant ACE_ENV_ARG_PARAMETER);
+
+    ACE_CHECK_RETURN(T::_nil());
+
+    CORBA::Object_var object = poa->
+      id_to_reference(oid ACE_ENV_ARG_PARAMETER);
+    ACE_CHECK_RETURN(T::_nil());
+
+    result = T::_narrow(object.in() ACE_ENV_ARG_PARAMETER);
+}
+#endif
