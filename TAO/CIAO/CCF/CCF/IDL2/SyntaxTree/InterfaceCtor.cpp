964afa834c861a03a3f48dc46f39d4321325f98b
boris <boris@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-16 17:54:52 +0000

    ChangeLogTag: Thu Oct 16 12:55:06 2003  Boris Kolpackov  <boris@dre.vanderbilt.edu>

---
+++ TAO/CIAO/CCF/CCF/IDL2/SyntaxTree/InterfaceCtor.cpp
@@ -0,0 +1,19 @@
+// file      : CCF/IDL2/SyntaxTree/InterfaceCtor.cpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+namespace CCF
+{
+  namespace IDL2
+  {
+    namespace SyntaxTree
+    {
+      struct InterfaceDef
+      {
+        InterfaceDef ();
+      };
+      
+      InterfaceDef::InterfaceDef () {}
+    }
+  }
+}
---
+++ TAO/CIAO/CCF/CCF/IDL2/SyntaxTree/ValueTypeCtor.cpp
@@ -0,0 +1,19 @@
+// file      : CCF/IDL2/SyntaxTree/ValueTypeCtor.cpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+namespace CCF
+{
+  namespace IDL2
+  {
+    namespace SyntaxTree
+    {
+      struct ValueTypeDef
+      {
+        ValueTypeDef ();
+      };
+      
+      ValueTypeDef::ValueTypeDef () {}
+    }
+  }
+}
