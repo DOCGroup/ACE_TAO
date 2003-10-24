b43491ee3ae55d1bd8e964f2c28f39a1a07bb232
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-24 16:31:41 +0000

    This commit was manufactured by cvs2svn to create branch 'CIAO_STATIC_CONFIG'.

---
+++ branches/CIAO_STATIC_CONFIG/TAO/CIAO/CCF/CCF/IDL2/SemanticAction/Factory.hpp
@@ -0,0 +1,69 @@
+// file      : CCF/IDL2/SemanticAction/Factory.hpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+#ifndef CCF_IDL2_SEMANTIC_ACTION_FACTORY_HPP
+#define CCF_IDL2_SEMANTIC_ACTION_FACTORY_HPP
+
+namespace CCF
+{
+  namespace IDL2
+  {
+    namespace SemanticAction
+    {
+      class Attribute;
+      class Include;
+      class Interface;
+      class Member;
+      class Module;
+      class Operation;
+      class Struct;
+      class Typedef;
+      class TypeId;
+      class TypePrefix;
+
+      //
+      //
+      //
+      class Factory
+      {
+      public:
+        virtual
+        ~Factory () throw () {}
+
+        virtual Attribute&
+        attribute () = 0;
+
+        virtual Include&
+        include () = 0;
+
+        virtual Interface&
+        interface () = 0;
+
+        virtual Member&
+        member () = 0;
+
+        virtual Module&
+        module () = 0;
+
+        virtual Operation&
+        operation () = 0;
+
+        virtual Struct&
+        struct_ () = 0;
+
+        virtual Typedef&
+        typedef_ () = 0;
+
+        virtual TypeId&
+        type_id () = 0;
+
+        virtual TypePrefix&
+        type_prefix () = 0;
+      };
+
+    }
+  }
+}
+
+#endif  // CCF_IDL2_SEMANTIC_ACTION_FACTORY_HPP
---
+++ branches/CIAO_STATIC_CONFIG/TAO/CIAO/ciao/Client_init.h
@@ -0,0 +1,35 @@
+// $Id$
+
+/**
+ * @file Client_init.h
+ *
+ * Initializing CIAO client side ORB, if they need to.
+ *
+ * @author Nanbor Wang <nanbor@cs.wustl.edu>
+ */
+
+#ifndef CIAO_CLIENT_INIT_H
+#define CIAO_CLIENT_INIT_H
+#include /**/ "ace/pre.h"
+
+#include "tao/corbafwd.h"
+#include "CIAO_Client_Export.h"
+
+#if !defined ACE_LACKS_PRAGMA_ONCE
+#pragma once
+#endif /* ! ACE_LACKS_PRAGMA_ONCE */
+
+namespace CIAO
+{
+  /**
+   * The initialize routine for any clients that need to access
+   * component.  This routine now simply registers various valuetype
+   * factories defined in CIAO_Component.pidl.  Currently, this method
+   * should be call right after ORB initialization but we should try
+   * to register these stuff automatically.
+   */
+  CIAO_CLIENT_Export int Client_init (CORBA::ORB_ptr o);
+}
+
+#include /**/ "ace/post.h"
+#endif /* CIAO_CLIENT_INIT_H */
