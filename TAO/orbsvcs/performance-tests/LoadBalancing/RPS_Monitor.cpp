367b35f639fbe5c3da06bceca4c83b5c7f85f25c
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-10-29 19:00:54 +0000

    Wed Oct 29 12:57:51 2003  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- TAO/orbsvcs/performance-tests/LoadBalancing/RPS_Monitor.cpp
+++ TAO/orbsvcs/performance-tests/LoadBalancing/RPS_Monitor.cpp
@@ -1,6 +1,7 @@
 #include "RPS_Monitor.h"
 #include "ServerRequestInterceptor.h"
 #include "ace/UUID.h"
+#include "tao/ORB_Constants.h"
 
 ACE_RCSID (LoadBalancing,
            RPS_Monitor,
@@ -79,7 +80,7 @@ RPS_Monitor::loads (ACE_ENV_SINGLE_ARG_DECL)
   load_list[0].id = CosLoadBalancing::RequestsPerSecond;
   load_list[0].value = request_count / elapsed_time.msec () * 1000;
 
-  // Strictly for debugging or 
+  // Strictly for debugging or
   ACE_DEBUG ((LM_DEBUG, "%f\n", load_list[0].value));
 
   return load_list._retn ();
