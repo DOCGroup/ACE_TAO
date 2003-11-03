b74c3ce9b719f1b325370d067ad44fa2404a23c3
boris <boris@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-03 23:23:22 +0000

    ChangeLogTag: Mon Nov  3 17:02:42 UTC 2003  Don Hinton  <dhinton@dresystems.com>

---
+++ protocols/ace/TMCast/GroupFwd.hpp
@@ -0,0 +1,15 @@
+// file      : TMCast/GroupFwd.hpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+#ifndef TMCAST_GROUP_FWD_HPP
+#define TMCAST_GROUP_FWD_HPP
+
+#include "Export.hpp"
+
+namespace TMCast
+{
+  class TMCast_Export Group;
+}
+
+#endif // TMCAST_GROUP_FWD_HPP
