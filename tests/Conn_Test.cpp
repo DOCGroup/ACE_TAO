0b1f9e72dcd6634966a33051ae3185140ee71c1d
Ossama Othman <ossama-othman@users.noreply.github.com>
2003-11-04 23:36:30 +0000

    ChangeLogTag:Tue Nov  4 15:35:30 2003  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- tests/Conn_Test.cpp
+++ tests/Conn_Test.cpp
@@ -32,6 +32,7 @@
 #include "ace/Auto_Ptr.h"
 #include "ace/Get_Opt.h"
 #include "ace/Process_Mutex.h"
+#include "ace/Signal.h"
 #include "Conn_Test.h"
 #include "ace/Barrier.h"
 #include "ace/OS_NS_signal.h"
