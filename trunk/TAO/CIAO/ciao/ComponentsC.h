cb46d32a3f1a215688d03bef8a348b68329766b7
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-05 03:22:56 +0000

    ChangeLogTag:Tue Nov  4 21:21:06 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/ciao/ComponentsC.h
+++ trunk/TAO/CIAO/ciao/ComponentsC.h
@@ -26,24 +26,20 @@
 //     http://www.cs.wustl.edu/~schmidt/TAO.html
 
 // TAO_IDL - Generated from
-// W:\ACE_wrappers\TAO\TAO_IDL\be\be_codegen.cpp:151
+// be/be_codegen.cpp:150
 
 #ifndef _TAO_IDL_COMPONENTSC_H_
 #define _TAO_IDL_COMPONENTSC_H_
 
-
-#include "tao/corba.h"
+#include "tao/ORB.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "tao/Valuetype/AbstractBase.h"
-#include "tao/Valuetype/ValueBase.h"
-#include "tao/Valuetype/Value_VarOut_T.h"
-#include "tao/Valuetype/Valuetype_Adapter_Impl.h"
-#include "tao/Valuetype/ValueFactory.h"
-#include "tao/Valuetype/Sequence_T.h"
+#include "tao/CDR.h"
+#include "tao/Environment.h"
+#include "tao/Typecode.h"
 
 #include "CCM_ContainerC.h"
 
@@ -71,14 +67,29 @@
 #endif /* __BORLANDC__ */
 
 // TAO_IDL - Generated from
-// W:\ACE_wrappers\TAO\TAO_IDL\be\be_visitor_root/cdr_op.cpp:48
+// be/be_visitor_traits.cpp:50
+
+// Traits specializations.
+namespace TAO
+{
+};
+
+// TAO_IDL - Generated from
+// be/be_visitor_root/cdr_op.cpp:48
 
 #ifndef __ACE_INLINE__
 
 // TAO_IDL - Generated from
-// W:\ACE_wrappers\TAO\TAO_IDL\be\be_visitor_root/cdr_op.cpp:64
+// be/be_visitor_root/cdr_op.cpp:64
 
 #endif /* __ACE_INLINE__ */
+
+// TAO_IDL - Generated from
+// be/be_codegen.cpp:911
+
+#if defined (__ACE_INLINE__)
+#include "ComponentsC.i"
+#endif /* defined INLINE */
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 #pragma warning(pop)
