7faa56734dd2ed3fcad7e8b2718e42e6b8881593
wilson_d <wilson_d@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-10 15:17:28 +0000

    ChangeLogTag: Mon Nov 10 09:13:45 2003  Dale Wilson  <wilson_d@ociweb.com>

--- TAO/orbsvcs/orbsvcs/PortableGroup/PG_Properties_Decoder.cpp
+++ TAO/orbsvcs/orbsvcs/PortableGroup/PG_Properties_Decoder.cpp
@@ -162,11 +162,11 @@ int TAO_PG::test_encode_decode ()
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
   template ACE_Hash_Map_Manager<
     ACE_CString,
-    PortableGroup::Value *,
+    const PortableGroup::Value *,
     ACE_SYNCH_NULL_MUTEX>;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 # pragma instantiate ACE_Hash_Map_Manager<
     ACE_CString,
-    PortableGroup::Value *,
+    const PortableGroup::Value *,
     ACE_SYNCH_NULL_MUTEX>
 #endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
