bcfd92394681b45f38dd9baa6141f49691f9613d
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-14 23:53:33 +0000

    ChangeLogTag:Fri Nov 14 16:50:41 2003  Nanbor Wang  <nanbor@cse.wustl.edu>

--- TAO/CIAO/tools/Assembly_Deployer/Assembly_Deployer.cpp
+++ TAO/CIAO/tools/Assembly_Deployer/Assembly_Deployer.cpp
@@ -5,14 +5,16 @@
 #include "Client_init.h"
 #include "ace/Get_Opt.h"
 #include "ace/streams.h"
+#include "ace/High_Res_Timer.h"
 
 const char *ior = "file://test.ior";
 const char *cad = 0;
+int benchmark = 0;
 
 int
 parse_args (int argc, char *argv[])
 {
-  ACE_Get_Opt get_opts (argc, argv, "a:k:");
+  ACE_Get_Opt get_opts (argc, argv, "a:k:t");
   int c;
 
   while ((c = get_opts ()) != -1)
@@ -20,6 +22,10 @@ parse_args (int argc, char *argv[])
       {
       case 'a':
         cad = get_opts.opt_arg ();
+        break;
+
+      case 't':
+        benchmark = 1;
         break;
 
       case 'k':
@@ -31,6 +37,7 @@ parse_args (int argc, char *argv[])
         ACE_ERROR_RETURN ((LM_ERROR,
                            "usage:  %s \n"
                            "-a <assembly descriptor (.cad)>\n"
+                           "-t: turn on deployment timer\n"
                            "-k <Assembly_Manager ior> \n"
                            "\n",
                            argv [0]),
@@ -49,6 +56,10 @@ parse_args (int argc, char *argv[])
 int
 main (int argc, char *argv[])
 {
+  ACE_High_Res_Timer overall, make_assembly, build_assembly;
+  overall.start ();             // Start measuring the overall time to
+                                // deploy an application.
+
   ACE_TRY_NEW_ENV
     {
       CORBA::ORB_var orb =
@@ -84,18 +95,42 @@ main (int argc, char *argv[])
 
       ACE_DEBUG ((LM_DEBUG, "Creating an Assembly with %s\n", cad));
 
+      make_assembly.start ();   // Start measuring the time to parse a
+                                // cad file and create an assembly object.
       Components::Cookie_var ck =
         factory->create_assembly (cad
                                   ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
+      make_assembly.stop ();    // Stop measuring
 
       Components::Deployment::Assembly_var assembly =
         factory->lookup (ck.in ()
                          ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
+      build_assembly.start ();  // Start measuring the time to
+                                // instantiate and connect the
+                                // application.
       assembly->build (ACE_ENV_SINGLE_ARG_PARAMETER);
       ACE_TRY_CHECK;
+      build_assembly.stop ();
+      overall.stop ();
+
+      if (benchmark != 0)
+        {
+          ACE_Time_Value overall_time, make_time, build_time;
+          overall.elapsed_time (overall_time);
+          make_assembly.elapsed_time (make_time);
+          build_assembly.elapsed_time (build_time);
+
+          ACE_DEBUG ((LM_DEBUG,
+                      "Total deployment time: %d.%06d sec\n"
+                      " create assembly time: %d.%06d sec\n"
+                      "  build assembly time: %d.%06d sec\n",
+                      overall_time.sec (), overall_time.usec (),
+                      make_time.sec (), make_time.usec (),
+                      build_time.sec (), build_time.usec ()));
+        }
 
       ACE_DEBUG ((LM_DEBUG, "Press <enter> to tear down the assembly\n"));
       char dummy [256];
