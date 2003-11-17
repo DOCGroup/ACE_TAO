5be188645a01602c03cf41a9664a68410d58c657
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-17 00:08:52 +0000

    ChangeLogTag: Sun Nov 16 18:04:05 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/tao/Exception.h
+++ trunk/TAO/tao/Exception.h
@@ -129,7 +129,7 @@ namespace CORBA
     virtual ~Exception (void);
 
     // = To throw the exception (when using the standard mapping).
-    virtual void _raise (void) const {}
+    virtual void _raise (void) const = 0;
 
     // = The static narrow operation.
     static Exception *_downcast (Exception *x);
@@ -229,7 +229,7 @@ namespace CORBA
         /// The const version of narrow operation
     static const UserException *_downcast (const CORBA::Exception *exception);
 
-    virtual void _raise (void) const {}
+    virtual void _raise (void) const = 0;
 
     // = TAO specific extension.
 
