62a47422db1d250dc8b8e209d5d5ae25315b6d39
elliott_c <ocielliottc@users.noreply.github.com>
2003-11-19 16:10:44 +0000

    ChangeLogTag: Wed Nov 19 10:09:12 2003  Chad Elliott  <elliott_c@ociweb.com>

--- bin/DependencyGenerator/Preprocessor.pm
+++ bin/DependencyGenerator/Preprocessor.pm
@@ -67,39 +67,45 @@ sub process {
   my($fh)       = new FileHandle();
   my(@incs)     = ();
 
-  ++$self->{'recurse'};
   if (open($fh, $file)) {
     my($ifcount) = 0;
     my(@zero)    = ();
 
+    ++$self->{'recurse'};
     $self->{'files'}->{$file} = [];
     while(<$fh>) {
       $_ =~ s/\/\/.*//;
 
-      if (/#\s*if\s+0/) {
-        push(@zero, $ifcount);
-        ++$ifcount;
-      }
-      elsif (/#\s*if/) {
-        ++$ifcount;
-      }
-      elsif (/#\s*endif/) {
-        --$ifcount;
-        if (defined $zero[0] && $ifcount == $zero[$#zero]) {
-          pop(@zero);
+      ## As an optimization, use a very simple regular expression no the
+      ## outside that all of the inner regular expressions have in
+      ## common.  That way we go down the path of if elsif only if it is
+      ## even possible due to the outside regular expression.
+      if (/#/) {
+        if (/#\s*if\s+0/) {
+          push(@zero, $ifcount);
+          ++$ifcount;
         }
-      }
-      elsif (!defined $zero[0] &&
-             /#\s*include\s+(\/\*\*\/\s*)?[<"]([^">]+)[">]/) {
-        my($inc) = $self->locateFile($2);
-        if (defined $inc) {
-          $inc =~ s/\\/\//g;
-          if (!$noinline ||
-              ($inc !~ /\.i(nl)?$/ || $self->{'recurse'} == 1)) {
-            push(@{$self->{'files'}->{$file}}, $inc);
-            if (!defined $self->{'files'}->{$inc}) {
-              ## Process this file, but do not return the include files
-              $self->process($inc, $noinline, 1);
+        elsif (/#\s*if/) {
+          ++$ifcount;
+        }
+        elsif (/#\s*endif/) {
+          --$ifcount;
+          if (defined $zero[0] && $ifcount == $zero[$#zero]) {
+            pop(@zero);
+          }
+        }
+        elsif (!defined $zero[0] &&
+               /#\s*include\s+(\/\*\*\/\s*)?[<"]([^">]+)[">]/) {
+          my($inc) = $self->locateFile($2);
+          if (defined $inc) {
+            $inc =~ s/\\/\//g;
+            if (!$noinline ||
+                ($inc !~ /\.i(nl)?$/ || $self->{'recurse'} == 1)) {
+              push(@{$self->{'files'}->{$file}}, $inc);
+              if (!defined $self->{'files'}->{$inc}) {
+                ## Process this file, but do not return the include files
+                $self->process($inc, $noinline, 1);
+              }
             }
           }
         }
@@ -112,8 +118,8 @@ sub process {
       $self->getFiles($file);
       @incs = keys %{$self->{'ifiles'}};
     }
+    --$self->{'recurse'};
   }
-  --$self->{'recurse'};
   return \@incs;
 }
 
