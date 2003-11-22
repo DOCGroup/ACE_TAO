23497a2576633d86613809b0eb97b9dd22478728
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-22 04:22:07 +0000

    ChangeLogTag:Fri Nov 21 22:19:39 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/tests/Bug_1636_Regression/client.cpp
+++ TAO/tests/Bug_1636_Regression/client.cpp
@@ -12,15 +12,19 @@ int main (int argc, char* argv[])
   {
     CORBA::ORB_var orb =
       CORBA::ORB_init (argc, argv,
-                       "" /* the ORB name, it can be anything! */ACE_ENV_ARG_PARAMETER);
+                       "" /* the ORB name, it can be anything! */
+                       ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
 
     ACE_DEBUG ((LM_DEBUG,"Get reference to the DynAny Factory\n"));
-    CORBA::Object_var obj =  orb->resolve_initial_references("DynAnyFactory" ACE_ENV_ARG_PARAMETER);
+    CORBA::Object_var obj =
+      orb->resolve_initial_references ("DynAnyFactory"
+                                       ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
 
     DynamicAny::DynAnyFactory_var daf =
-      DynamicAny::DynAnyFactory::_narrow(obj.in() ACE_ENV_ARG_PARAMETER);
+      DynamicAny::DynAnyFactory::_narrow (obj.in()
+                                          ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
 
     UnionIecs::S_i_num s_num;
@@ -32,13 +36,20 @@ int main (int argc, char* argv[])
     CORBA::Any MyAny1;
     MyAny1 <<= num1;
 
-    ACE_DEBUG ((LM_DEBUG, "calling from_any () and to_any () with non-empty union...\n"));
-    DynamicAny::DynAny_var dynany1 = daf-> create_dyn_any_from_type_code (UnionIecs::_tc_S_num ACE_ENV_ARG_PARAMETER);
+    ACE_DEBUG ((LM_DEBUG,
+                "calling from_any () and to_any () with non-empty union...\n"));
+    DynamicAny::DynAny_var dynany1 =
+      daf->create_dyn_any_from_type_code (UnionIecs::_tc_S_num
+                                          ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
-    dynany1->from_any(MyAny1 ACE_ENV_ARG_PARAMETER);
+
+    dynany1->from_any(MyAny1
+                      ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
-    CORBA::Any_var arg1 = dynany1->to_any(ACE_ENV_SINGLE_ARG_PARAMETER);
+    CORBA::Any_var arg1 =
+      dynany1->to_any (ACE_ENV_SINGLE_ARG_PARAMETER);
     ACE_TRY_CHECK;
+
     ACE_DEBUG ((LM_DEBUG, "...done\n"));
 
     UnionIecs::S_num num2;
@@ -46,13 +57,21 @@ int main (int argc, char* argv[])
     CORBA::Any MyAny2;
     MyAny2 <<= num2;
 
-    ACE_DEBUG ((LM_DEBUG, "calling from_any () and to_any () with empty union...\n" ));
-    DynamicAny::DynAny_var dynany2 = daf-> create_dyn_any_from_type_code (UnionIecs::_tc_S_num ACE_ENV_ARG_PARAMETER);
+    ACE_DEBUG ((LM_DEBUG,
+                "calling from_any () and to_any () with empty union...\n" ));
+    DynamicAny::DynAny_var dynany2 =
+      daf-> create_dyn_any_from_type_code (UnionIecs::_tc_S_num
+                                           ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
-    dynany2->from_any(MyAny2 ACE_ENV_ARG_PARAMETER);
+
+    dynany2->from_any (MyAny2
+                       ACE_ENV_ARG_PARAMETER);
     ACE_TRY_CHECK;
-    CORBA::Any_var arg2 = dynany2->to_any(ACE_ENV_SINGLE_ARG_PARAMETER);
+
+    CORBA::Any_var arg2 =
+      dynany2->to_any(ACE_ENV_SINGLE_ARG_PARAMETER);
     ACE_TRY_CHECK;
+
     ACE_DEBUG ((LM_DEBUG,"...done.\nTest passed.\n"));
 
     // Finally destroy the ORB
@@ -60,12 +79,12 @@ int main (int argc, char* argv[])
     ACE_TRY_CHECK;
   }
   ACE_CATCHANY
-  {
-    ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "Exception - test failed:\n");
-    return 1;
-  }
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Exception - test failed:\n");
+      return 1;
+    }
   ACE_ENDTRY;
-
   ACE_CHECK_RETURN(1);
 
   return 0;
