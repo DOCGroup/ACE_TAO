6a0d589b060fc8eb80490408e247fecbe2ea25d8
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-23 17:08:58 +0000

    ChangeLogTag:Sun Nov 23 11:00:02 2003  Douglas C. Schmidt  <schmidt@ace.cs.wustl.edu>

--- ace/Service_Object.cpp
+++ ace/Service_Object.cpp
@@ -9,6 +9,7 @@
 #include "ace/Service_Types.h"
 #include "ace/DLL.h"
 #include "ace/ACE.h"
+#include "ace/config-all.h"
 
 ACE_RCSID (ace,
 	   Service_Object,
@@ -49,7 +50,8 @@ ACE_Service_Type::ACE_Service_Type (const ACE_TCHAR *n,
     fini_already_called_ (0)
 {
   ACE_TRACE ("ACE_Service_Type::ACE_Service_Type");
-  ACE_DLL &dll = ACE_const_cast <ACE_DLL &> (this->dll_);
+  ACE_DLL &dll = ACE_const_cast (ACE_DLL &,
+		                 this->dll_);
   dll.set_handle (handle);
   this->name (n);
 }
