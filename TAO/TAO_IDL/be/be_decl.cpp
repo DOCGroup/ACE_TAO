103d06537ba6dbc6549d0ab7ff62b50723f403a0
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-23 23:56:30 +0000

    ChangeLogTag: Sun Nov 23 17:48:09 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/TAO_IDL/be/be_decl.cpp
+++ TAO/TAO_IDL/be/be_decl.cpp
@@ -201,7 +201,7 @@ be_decl::compute_full_name  (const char *prefix,
       result_str += suffix_str;
     }
 
-  name = result_str.rep ();
+  name = ACE_OS::strdup (result_str.fast_rep ());
 }
 
 void
