7abd2fcc88297ccd2a260a9404e910e92ac5c607
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-24 05:23:17 +0000

    ChangeogTag:Sun Nov 23 23:22:14 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- trunk/TAO/orbsvcs/tests/EC_Mcast/EC_Mcast.cpp
+++ trunk/TAO/orbsvcs/tests/EC_Mcast/EC_Mcast.cpp
@@ -570,7 +570,7 @@ ECM_Federation::ECM_Federation (char* name,
      consumer_names_ (consumer_names),
      addr_server_ (mcast_port)
 {
-  sender_ = TAO_ECG_UDP_Sender::create();
+  sender_ = TAO_ECG_UDP_Sender::create (true);
 
   ACE_NEW (this->supplier_ipaddr_, CORBA::ULong[this->supplier_types_]);
   ACE_NEW (this->consumer_ipaddr_, CORBA::ULong[this->consumer_types_]);
@@ -903,7 +903,7 @@ ECM_Local_Federation::ECM_Local_Federation (ECM_Federation *federation,
      subscription_change_period_ (10000),
      publication_change_period_ (10000)
 {
-  receiver_ = TAO_ECG_UDP_Receiver::create();
+  receiver_ = TAO_ECG_UDP_Receiver::create (true);
 
   ACE_NEW (mcast_eh_, TAO_ECG_Mcast_EH((&*receiver_)));
 
