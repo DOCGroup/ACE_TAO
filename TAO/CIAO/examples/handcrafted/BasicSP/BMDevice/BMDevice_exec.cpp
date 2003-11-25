53cc5702fd4e9503672795acb252dbcf3005304f
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-25 01:28:05 +0000

    ChangeLogTag:Mon Nov 24 19:24:42 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/CIAO/examples/handcrafted/BasicSP/BMDevice/BMDevice_exec.cpp
+++ TAO/CIAO/examples/handcrafted/BasicSP/BMDevice/BMDevice_exec.cpp
@@ -43,7 +43,7 @@ char *
 MyImpl::BMDevice_exec_i::data_read (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  return this->str_;
+  return CORBA::string_dup (this->str_);
 }
 
 char *
