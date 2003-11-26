04ad6bbfd4bd480c50eae6841b162b95e83dba71
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-11-26 03:21:39 +0000

    ChangeLogTag: Tue Nov 25 21:17:58 2003  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/TAO_IDL/be/be_sunsoft.cpp
+++ TAO/TAO_IDL/be/be_sunsoft.cpp
@@ -98,9 +98,10 @@ TAO_SunSoft_OutStream::print (AST_Expression *expr)
           break;
         case AST_Expression::EV_ulonglong:
 #if ! defined (ACE_LACKS_LONGLONG_T)
-          this->TAO_OutStream::print (ACE_UINT64_FORMAT_SPECIFIER "%c",
-                                      ev->u.ullval,
-                                      'U');
+          this->TAO_OutStream::print ("ACE_UINT64_LITERAL (");
+          this->TAO_OutStream::print (ACE_UINT64_FORMAT_SPECIFIER,
+                                      ev->u.ullval);
+          this->TAO_OutStream::print (")");
 #endif /* ! defined (ACE_LACKS_LONGLONG_T) */
           break;
         case AST_Expression::EV_float:
