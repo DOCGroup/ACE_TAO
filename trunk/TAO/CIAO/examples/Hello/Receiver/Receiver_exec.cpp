6e71f35db7bb764eb4a9a613bffc974b4880e1f0
lut <lut@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-12-03 00:46:12 +0000

    ChangeLogTag: Tue Dec  2 18:40:59 2003  Tao Lu  <lu@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/examples/Hello/Receiver/Receiver_exec.cpp
+++ trunk/TAO/CIAO/examples/Hello/Receiver/Receiver_exec.cpp
@@ -1,4 +1,4 @@
-//$Id$:
+//$Id$
 /*
  * @file Receiver_exec.cpp
  *
@@ -6,6 +6,7 @@
  */
 
 #include "Receiver_exec.h"
+#include "CIAO_common.h"
 
 MyImpl::Receiver_exec_i::Receiver_exec_i ()
 {
@@ -65,6 +66,20 @@ MyImpl::Receiver_exec_i::ccm_activate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
                    Components::CCMException))
 {
   ACE_DEBUG ((LM_DEBUG, "MyImpl::Receiver_exec_i::ccm_activate\n"));
+
+  // @@ This hack work around a missing feature in CIAO's assembly
+  // mechanism where a Softpkg descriptor can specify it's dependency
+  // to a valuetype factory and instruct the deployment framework to
+  // initialize and register the corresponding valuefactory in the
+  // component server.  Here, we are registering the valuefactory
+  // explicitly to work around this problem.
+  char *argv[1] = { "Receiver_exec"};
+  int argc = sizeof(argv)/sizeof(argv[0]);
+  CORBA::ORB_var orb = CORBA::ORB_init(argc, argv ACE_ENV_ARG_PARAMETER);
+
+  CIAO_REGISTER_VALUE_FACTORY (orb.in(),
+			       Hello::timeout_init,
+                               Hello::timeout);
 }
 
 void
