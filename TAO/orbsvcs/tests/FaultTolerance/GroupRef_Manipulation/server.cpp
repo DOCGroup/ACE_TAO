cda70b430476c2bfcee5f4cd299092880cc91b22
wilson_d <wilson_d@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-12-04 16:08:23 +0000

    ChangeLogTag: Thu Dec  4 10:07:33 2003  Dale Wilson  <wilson_d@ociweb.com>

--- TAO/orbsvcs/tests/FaultTolerance/GroupRef_Manipulation/server.cpp
+++ TAO/orbsvcs/tests/FaultTolerance/GroupRef_Manipulation/server.cpp
@@ -62,8 +62,8 @@ add_ft_prop (CORBA::ORB_ptr o,
 
   // Property values
   // Major and Minor revision numbers
-  ft_tag_component.version.major = (CORBA::Octet) 1;
-  ft_tag_component.version.minor = (CORBA::Octet) 0;
+  ft_tag_component.component_version.major = (CORBA::Octet) 1;
+  ft_tag_component.component_version.minor = (CORBA::Octet) 0;
 
   // Domain id
   const char *id = "version_testing";
