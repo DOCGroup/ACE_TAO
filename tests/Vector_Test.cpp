88d175e429a7c3e7e5ab80a37dbe6641d5f86d4b
Johnny Willemsen <jwillemsen@remedy.nl>
2003-12-05 07:28:50 +0000

    ChangeLogTag:  Fri Dec  5 07:25:46 UTC 2003  Johnny Willemsen  <jwillemsen@remedy.nl>

--- tests/Vector_Test.cpp
+++ tests/Vector_Test.cpp
@@ -135,11 +135,13 @@ int run_main (int, ACE_TCHAR *[])
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 template class ACE_Vector<DATA>;
+template class ACE_Vector_Iterator<DATA>;
 template class ACE_Array<DATA>;
 template class ACE_Array_Base<DATA>;
 template class ACE_Array_Iterator<DATA>;
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 #pragma instantiate ACE_Vector<DATA>
+#pragma instantiate ACE_Vector_Iterator<DATA>
 #pragma instantiate ACE_Array<DATA>
 #pragma instantiate ACE_Array_Base<DATA>
 #pragma instantiate ACE_Array_Iterator<DATA>
