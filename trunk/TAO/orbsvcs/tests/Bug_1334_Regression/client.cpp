cb897a54b1c01b1f2b811450937d0e247de29899
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-12-12 09:30:02 +0000

    ChangeLogTag:  Fri Dec 12 09:29:12 UTC 2003  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/orbsvcs/tests/Bug_1334_Regression/client.cpp
+++ trunk/TAO/orbsvcs/tests/Bug_1334_Regression/client.cpp
@@ -11,28 +11,34 @@
 //
 int main( int argc, char *argv[] )
 {
-   try
-   {
-      CORBA::ORB_var orb = CORBA::ORB_init( argc, argv ) ;
+  ACE_TRY_NEW_ENV
+    {
+      CORBA::ORB_var orb = CORBA::ORB_init( argc, argv, 0 ACE_ENV_ARG_PARAMETER ) ;
+      ACE_TRY_CHECK;
 
-      try
-      {
-         CORBA::Object_var naming =
-            orb->resolve_initial_references( "NameService" ) ;
-         if( CORBA::is_nil( naming.in() ) )
-         {
-            return 1 ;
-         }
-      }
-      catch( CORBA::Exception&  )
-      {
-         return 1 ;
-      }
-   }
-   catch( CORBA::Exception&  )
-   {
-      return 2 ;
-   }
+      ACE_TRY_EX (nested)
+        {
+           CORBA::Object_var naming =
+              orb->resolve_initial_references( "NameService" ACE_ENV_ARG_PARAMETER) ;
+           ACE_TRY_CHECK_EX (nested);
+           if( CORBA::is_nil( naming.in() ) )
+             {
+                return 1 ;
+             }
+        }
+      ACE_CATCH (CORBA::Exception, ex)
+        {
+          ACE_UNUSED_ARG (ex);
+          return 1 ;
+        }
+      ACE_ENDTRY;
+    }
+  ACE_CATCH ( CORBA::Exception, ex )
+    {
+       ACE_UNUSED_ARG (ex);
+       return 2 ;
+    }
+  ACE_ENDTRY;
 
-   return 0 ;
+  return 0 ;
 }
