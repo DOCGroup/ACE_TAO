26b202ad383751b37987343fbd8a70398c966720
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-12-14 16:03:49 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.97.2'.

--- TAO/orbsvcs/tests/Event/Mcast/Simple/Constants.h
+++
@@ -1,6 +0,0 @@
-// $Id$
-
-#include "orbsvcs/orbsvcs/Event_Service_Constants.h"
-
-#define EVENT_TYPE ACE_ES_EVENT_UNDEFINED+1
-#define SOURCE_ID ACE_ES_EVENT_SOURCE_ANY+1
--- TAO/tao/ORB_Table.inl
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-//
-// $Id$
-
-ACE_INLINE TAO_ORB_Core *
-TAO_ORB_Table::first_orb (void)
-{
-  return this->first_orb_;
-}
