f69ffd460f1c469eb53aeeba2113afe8cf61f452
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2003-12-17 17:23:26 +0000

    ChangeLogTag:Wed Dec 17 11:17:09 2003  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/tao/Messaging/MessagingS.cpp
+++ TAO/tao/Messaging/MessagingS.cpp
@@ -497,7 +497,7 @@ CORBA::Boolean POA_Messaging::ReplyHandler::_is_a (
         ) ||
       !ACE_OS::strcmp (
           (char *)value,
-          "IDL:org.omg/CORBA/Object:1.0"
+          "IDL:omg.org/CORBA/Object:1.0"
         )
      )
     {
