a3a18c0206efb7b9ae1c00853fe88fad835b4dbb
Johnny Willemsen <jwillemsen@remedy.nl>
2003-12-27 13:20:22 +0000

    ChangeLogTag:  Sat Dec 27 13:20:11 UTC 2003  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/examples/Borland/build_idl.bat
+++ TAO/examples/Borland/build_idl.bat
@@ -1,2 +1,2 @@
-..\..\..\bin\Dynamic\Release\Pascal\tao_idl -Wb,pch_include=pch.h Broadcaster.idl
-..\..\..\bin\Dynamic\Release\Pascal\tao_idl -Wb,pch_include=pch.h Receiver.idl
+..\..\..\bin\Dynamic\Debug\tao_idl -Wb,pch_include=pch.h Broadcaster.idl
+..\..\..\bin\Dynamic\Debug\tao_idl -Wb,pch_include=pch.h Receiver.idl
--- TAO/examples/Borland/pch.h
+++ TAO/examples/Borland/pch.h
@@ -2,7 +2,6 @@
 #ifndef INCLUDE_PCH_H
 #define INCLUDE_PCH_H
 
-#include <vcl.h>
 #include <tao/corba.h>
 #include <cctype>
 #include <dos.h>
