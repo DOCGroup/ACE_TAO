c5329bf429c79f76c6daccf686d3f68c1484b23f
Johnny Willemsen <jwillemsen@remedy.nl>
2003-12-30 13:29:31 +0000

    ChangeLogTag: Tue Dec 30 13:22:56 UTC 2003  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/tests/RTCosScheduling/Object1_i.h
+++ TAO/orbsvcs/tests/RTCosScheduling/Object1_i.h
@@ -14,19 +14,10 @@
 #define Object1_I_H
 
 #include "testSchedS.h"
-//#include "tao/RTCORBA/RTCORBA.h"
-//##include "ace/Task.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
-
-//#include "ace/CORBA_macros.h"
-//#include "tao/RTCORBA/Priority_Mapping_Manager.h"
-//#include "tao/corba.h"
-//#include "tao/ORB_Core.h"
-
-
 
 /**
  * @class Object1_i
@@ -34,10 +25,8 @@
  * @brief This class is used to test the RTCORBA 1.0 Scheduling Service
  *
  */
-
-
 class Object1_impl : public POA_testSched::Object1,
-                            PortableServer::RefCountServantBase
+                     public PortableServer::RefCountServantBase
 {
   public:
     Object1_impl()
