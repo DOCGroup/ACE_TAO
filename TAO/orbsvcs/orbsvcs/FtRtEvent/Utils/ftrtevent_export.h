68510e063cb27ef76000fc829911bb3e7f75ec02
Johnny Willemsen <jwillemsen@remedy.nl>
2004-01-08 17:51:22 +0000

    ChangeLogTag: Thu Jan  8 17:50:33 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/FtRtEvent/Utils/ftrtevent_export.h
+++ TAO/orbsvcs/orbsvcs/FtRtEvent/Utils/ftrtevent_export.h
@@ -10,9 +10,15 @@
 
 #include "ace/config-all.h"
 
-#if !defined(TAO_FTRTEVENT_HAS_DLL)
-#define TAO_FTRTEVENT_HAS_DLL 1
-#endif /* ! TAO_FTRTEVENT_HAS_DLL */
+#if defined (TAO_AS_STATIC_LIBS)
+#  if !defined (TAO_FTRTEVENT_HAS_DLL)
+#    define TAO_FTRTEVENT_HAS_DLL 0
+#  endif /* ! TAO_FTRTEVENT_HAS_DLL */
+#else
+#  if !defined (TAO_FTRTEVENT_HAS_DLL)
+#    define TAO_FTRTEVENT_HAS_DLL 1
+#  endif /* ! TAO_FTRTEVENT_HAS_DLL */
+#endif
 
 #if defined(TAO_FTRTEVENT_HAS_DLL) \
 && (TAO_FTRTEVENT_HAS_DLL == 1)
