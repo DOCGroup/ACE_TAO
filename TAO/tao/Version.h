fa2437caabc3b6f87cca49b985c954f53d42c68e
bala <balanatarajan@users.noreply.github.com>
2004-01-15 00:47:04 +0000

    1.4

--- TAO/tao/Version.h
+++ TAO/tao/Version.h
@@ -2,6 +2,6 @@
 // This is an automatically generated file.
 
 #define TAO_MAJOR_VERSION 1
-#define TAO_MINOR_VERSION 3
-#define TAO_BETA_VERSION 6
-#define TAO_VERSION "1.3.6"
+#define TAO_MINOR_VERSION 4
+#define TAO_BETA_VERSION 0
+#define TAO_VERSION "1.4"
