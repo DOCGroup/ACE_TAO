7b78a9414406f873f3cb713d678ddbd85063425e
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-01-16 14:15:04 +0000

    ChangeLogTag: Fri Jan 16 14:14:11 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/examples/Advanced/ch_8_and_10/server.h
+++ trunk/TAO/examples/Advanced/ch_8_and_10/server.h
@@ -45,7 +45,7 @@ public:
                                 throw(CORBA::SystemException);
     virtual void            location(const char * loc)
                                 throw(CORBA::SystemException);
-    
+
     // Constructor and destructor
     Thermometer_impl(CCS::AssetType anum, const char * location);
     virtual ~Thermometer_impl();
@@ -150,10 +150,10 @@ private:
         {
             switch (m_sc) {
             case CCS::Controller::LOCATION:
-                return strcmp(p.second->location(), m_str) == 0;
+                return ::strcmp(p.second->location(), m_str) == 0;
                 break;
             case CCS::Controller::MODEL:
-                return strcmp(p.second->model(), m_str) == 0;
+                return ::strcmp(p.second->model(), m_str) == 0;
                 break;
             default:
               assert(0);  // Precondition violation
