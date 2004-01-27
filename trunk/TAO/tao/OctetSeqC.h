6be232595d7334ba4e8695d73b4af44fbe3e9f06
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-01-27 23:45:28 +0000

    ChangeLogTag: Tue Jan 27 17:41:38 2004  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/tao/OctetSeqC.h
+++ trunk/TAO/tao/OctetSeqC.h
@@ -128,6 +128,11 @@ namespace CORBA
 
 #endif /* end #if !defined */
 
+  // TAO_IDL - Generated from
+  // be/be_visitor_typecode/typecode_decl.cpp:44
+
+  TAO_NAMESPACE_STORAGE_CLASS ::CORBA::TypeCode_ptr _tc_OctetSeq;
+
 // TAO_IDL - Generated from
 // be/be_visitor_module/module_ch.cpp:66
 
@@ -140,6 +145,14 @@ namespace CORBA
 namespace TAO
 {
 }
+
+// TAO_IDL - Generated from
+// be/be_visitor_sequence/any_op_ch.cpp:52
+
+TAO_Export void operator<<= (CORBA::Any &, const CORBA::OctetSeq &); // copying version
+TAO_Export void operator<<= (CORBA::Any &, CORBA::OctetSeq*); // noncopying version
+TAO_Export CORBA::Boolean operator>>= (const CORBA::Any &, CORBA::OctetSeq *&); // deprecated
+TAO_Export CORBA::Boolean operator>>= (const CORBA::Any &, const CORBA::OctetSeq *&);
 
 // TAO_IDL - Generated from
 // be/be_visitor_root/cdr_op.cpp:48
