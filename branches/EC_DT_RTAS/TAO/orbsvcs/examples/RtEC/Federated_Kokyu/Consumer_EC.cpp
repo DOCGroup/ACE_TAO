d42c959f3c839a98ae097f6520d4e02567b827fe
thrall <thrall@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-01-29 23:20:02 +0000

    Added functional Federated EC example written by Huang-Ming. Note that the MPC file has problems detecting the Kokyu library, so it is recommended you just use the Makefiles committed to the repository until that is fixed.

---
+++ branches/EC_DT_RTAS/TAO/orbsvcs/examples/RtEC/Federated_Kokyu/Consumer_EC.cpp
@@ -0,0 +1,206 @@
+// $Id$
+
+#include "ace/SString.h"
+#include "ace/Get_Opt.h"
+#include "ace/Sched_Params.h"
+#include "ace/Thread.h"
+
+#include "orbsvcs/Event/EC_Kokyu_Factory.h"
+#include "orbsvcs/Time_Utilities.h"
+#include "orbsvcs/Event_Service_Constants.h"
+
+#include "Kokyu_EC.h"
+#include "Consumer.h"
+
+namespace
+{
+  int config_run = 0;
+  ACE_CString sched_type ="rms";
+  FILE * ior_output_file;
+}
+
+inline RtecScheduler::Period_t time_val_to_period (const ACE_Time_Value &tv)
+{
+  //100s of nanoseconds
+  return (tv.sec () * 1000000 + tv.usec ())*10;
+}
+
+int parse_args (int argc, char *argv[]);
+
+int
+main (int argc, char* argv[])
+{
+  //TAO_EC_Default_Factory::init_svcs ();
+
+  TAO_EC_Kokyu_Factory::init_svcs ();
+
+
+  ACE_DECLARE_NEW_CORBA_ENV;
+  ACE_TRY
+    {
+      // ORB initialization boiler plate...
+      CORBA::ORB_var orb =
+        CORBA::ORB_init (argc, argv, "" ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      if (parse_args (argc, argv) == -1)
+        {
+          ACE_ERROR ((LM_ERROR,
+                      "Usage: Service [-o IOR_file_name]\n"));
+          return 1;
+        }
+
+      CORBA::Object_var object =
+        orb->resolve_initial_references ("RootPOA" ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      PortableServer::POA_var poa =
+        PortableServer::POA::_narrow (object.in () ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      PortableServer::POAManager_var poa_manager =
+        poa->the_POAManager (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      poa_manager->activate (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      // ****************************************************************
+
+      Kokyu_EC kokyu_ec;
+      if (kokyu_ec.init(sched_type.c_str(), poa.in()) == -1)
+        ACE_ERROR_RETURN((LM_ERROR, "Unable to initialize Kokyu_EC"), 1);
+
+      // *************************************************************
+      // Create a consumer, intialize its RT_Info structures, and
+      // connnect to the event channel....
+
+      Consumer consumer_impl1, consumer_impl2;
+      RtecEventChannelAdmin::ProxyPushSupplier_var  proxy_supplier1,
+                                                    proxy_supplier2;
+
+      //consumer's rate will get propagated from the supplier.
+      //so no need to specify a period here. Specifying
+      //criticality is crucial since it propagates from
+      //consumer to supplier.
+      ACE_Time_Value tv (0,0);
+      TimeBase::TimeT tmp;
+      ORBSVCS_Time::Time_Value_to_TimeT (tmp, tv);
+
+      RtEventChannelAdmin::SchedInfo info;
+      info.criticality = RtecScheduler::VERY_LOW_CRITICALITY;
+      info.period = time_val_to_period (tv);
+      info.importance = RtecScheduler::VERY_LOW_IMPORTANCE;
+      info.threads = 0;
+      info.info_type = RtecScheduler::OPERATION;
+
+      RtecEventComm::PushConsumer_var consumer1 =
+        consumer_impl1._this (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+
+      RtecScheduler::handle_t consumer1_rt_info =
+        kokyu_ec.register_consumer("consumer1",
+                                   info,
+                                   ACE_ES_EVENT_UNDEFINED,
+                                   consumer1.in(),
+                                   proxy_supplier1.out()
+                                   ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      info.criticality = RtecScheduler::VERY_HIGH_CRITICALITY;
+      info.importance = RtecScheduler::VERY_HIGH_IMPORTANCE;
+
+      RtecEventComm::PushConsumer_var consumer2 =
+        consumer_impl2._this (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      RtecScheduler::handle_t consumer2_rt_info =
+        kokyu_ec.register_consumer("consumer2",
+                                   info,
+                                   ACE_ES_EVENT_UNDEFINED + 1,
+                                   consumer2.in(),
+                                   proxy_supplier2.out()
+                                   ACE_ENV_ARG_PARAMETER);
+
+      // ****************************************************************
+      RtEventChannelAdmin::RtSchedEventChannel_var kokyu_ec_ior =
+        kokyu_ec._this(ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
+      CORBA::String_var ior = orb->object_to_string(kokyu_ec_ior.in()
+                                                    ACE_ENV_ARG_PARAMETER);
+
+      ACE_OS::fprintf(ior_output_file, ior.in());
+      ACE_OS::fclose(ior_output_file);
+
+
+      // ****************************************************************
+
+      // At this point the consumer and supplier are connected to the
+      // EC, they have provided their QoS info to the Scheduling
+      // Service and the EC has informed the Scheduler about the
+      // dependencies between them.
+      // We can now compute the schedule for this configuration...
+
+      // The schedule is returned in this variables....
+
+
+      // ****************************************************************
+
+      ACE_hthread_t thr_handle;
+      ACE_Thread::self (thr_handle);
+
+      int prio = ACE_Sched_Params::priority_max (ACE_SCHED_FIFO);
+      ACE_OS::thr_setprio (thr_handle, prio);
+
+
+      orb->run (ACE_ENV_SINGLE_ARG_PARAMETER);
+
+      // ****************************************************************
+
+      // We should do a lot of cleanup (disconnect from the EC,
+      // deactivate all the objects with the POA, etc.) but this is
+      // just a simple demo so we are going to be lazy.
+
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION, "Service");
+      return 1;
+    }
+  ACE_ENDTRY;
+  return 0;
+}
+
+// ****************************************************************
+
+int parse_args (int argc, char *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, "cs:o:");
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 'o':
+        ior_output_file = ACE_OS::fopen (get_opts.opt_arg (), "w");
+        if (ior_output_file == 0)
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "Unable to open %s for writing: %p\n",
+                             get_opts.opt_arg ()), -1);
+        break;
+      case 's':
+        sched_type = ACE_TEXT_ALWAYS_CHAR(get_opts.opt_arg ());
+        break;
+
+      case '?':
+      default:
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage:  %s -s <rms|muf>"
+                           "\n",
+                           argv [0]),
+                          -1);
+      }
+  // Indicates sucessful parsing of the command line
+  if (ior_output_file == 0)
+    ior_output_file = ACE_OS::fopen ("consumer_ec.ior", "w");
+  return 0;
+}
---
+++ branches/EC_DT_RTAS/TAO/orbsvcs/examples/RtEC/Federated_Kokyu/Kokyu_EC.cpp
@@ -0,0 +1,227 @@
+// $Id$
+
+#include "Kokyu_EC.h"
+#include "orbsvcs/Sched/Reconfig_Scheduler.h"
+#include "orbsvcs/Runtime_Scheduler.h"
+#include "orbsvcs/Event_Service_Constants.h"
+#include "orbsvcs/Event_Utilities.h"
+#include "orbsvcs/Scheduler_Factory.h"
+#include "orbsvcs/Event/EC_Event_Channel.h"
+#include "orbsvcs/Event/EC_Default_Factory.h"
+#include "orbsvcs/Event/EC_Kokyu_Factory.h"
+#include "ace/OS_NS_strings.h" //for ACE_OS::strcasecmp
+
+namespace {
+
+  typedef TAO_Reconfig_Scheduler<TAO_RMS_FAIR_Reconfig_Sched_Strategy, TAO_SYNCH_MUTEX> RECONFIG_RMS_SCHED_TYPE;
+  typedef TAO_Reconfig_Scheduler<TAO_MUF_FAIR_Reconfig_Sched_Strategy, TAO_SYNCH_MUTEX> RECONFIG_MUF_SCHED_TYPE;
+
+  POA_RtecScheduler::Scheduler*
+    create_scheduler(const char* schedule_discipline)
+  {
+    POA_RtecScheduler::Scheduler* sched_impl=0;
+    if (ACE_OS::strcasecmp(schedule_discipline, "rms") == 0)
+    {
+      ACE_DEBUG ((LM_DEBUG, "Creating RMS scheduler\n"));
+      ACE_NEW_RETURN (sched_impl,
+        RECONFIG_RMS_SCHED_TYPE,
+        0);
+    }
+    else if (ACE_OS::strcasecmp(schedule_discipline, "muf") == 0)
+    {
+      ACE_DEBUG ((LM_DEBUG, "Creating MUF scheduler\n"));
+      ACE_NEW_RETURN (sched_impl,
+        RECONFIG_MUF_SCHED_TYPE,
+        0);
+    }
+    return sched_impl;
+  }
+}
+
+Kokyu_EC::Kokyu_EC()
+{
+}
+
+int Kokyu_EC::init(const char* schedule_discipline, PortableServer::POA_ptr poa)
+{
+  ACE_TRY_NEW_ENV {
+
+    scheduler_impl_ = create_scheduler(schedule_discipline);
+    if (scheduler_impl_.in() == 0) return -1;
+
+    scheduler_ = scheduler_impl_->_this();
+
+    TAO_EC_Event_Channel_Attributes attrib(poa, poa);
+    attrib.scheduler = scheduler_.in();
+
+    TAO_EC_Event_Channel* ec;
+    ACE_NEW_RETURN(ec, TAO_EC_Event_Channel(attrib), -1);
+    ec_impl_ = ec;
+
+    consumer_admin_ = ec_impl_->for_consumers(ACE_ENV_SINGLE_ARG_PARAMETER);
+    ACE_TRY_CHECK;
+    supplier_admin_ = ec_impl_->for_suppliers(ACE_ENV_SINGLE_ARG_PARAMETER);
+    ACE_TRY_CHECK;
+  }
+  ACE_CATCHALL {
+    return -1;
+  }
+  ACE_ENDTRY;
+  return 0;
+}
+
+
+RtEventChannelAdmin::handle_t Kokyu_EC::register_consumer (
+        const char * entry_point,
+        const RtEventChannelAdmin::SchedInfo & info,
+        RtecEventComm::EventType type,
+        RtecEventComm::PushConsumer_ptr consumer,
+        RtecEventChannelAdmin::ProxyPushSupplier_out proxy_supplier
+        ACE_ENV_ARG_DECL
+      )
+      ACE_THROW_SPEC ((
+        CORBA::SystemException
+        , RtecScheduler::UNKNOWN_TASK
+        , RtecScheduler::INTERNAL
+        , RtecScheduler::SYNCHRONIZATION_FAILURE
+      ))
+{
+  RtecScheduler::handle_t consumer1_rt_info =
+    scheduler_impl_->create (entry_point ACE_ENV_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+
+  scheduler_impl_->set (consumer1_rt_info,
+    info.criticality,
+    info.wc_time,
+    info.typical_time,
+    info.cached_time,
+    info.period,
+    info.importance,
+    info.quantum,
+    info.threads,
+    info.info_type
+    ACE_ENV_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+
+  ACE_ConsumerQOS_Factory consumer_qos1;
+
+  if (type != ACE_ES_EVENT_INTERVAL_TIMEOUT)
+    consumer_qos1.insert_type (type,  consumer1_rt_info);
+  else
+    consumer_qos1.insert_time (ACE_ES_EVENT_INTERVAL_TIMEOUT,
+                               info.period, //in 100s of nanosec
+                               consumer1_rt_info);
+
+
+  proxy_supplier =
+    consumer_admin_->obtain_push_supplier (ACE_ENV_SINGLE_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+
+  proxy_supplier->connect_push_consumer (consumer,
+    consumer_qos1.get_ConsumerQOS ()
+    ACE_ENV_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+  return consumer1_rt_info;
+}
+
+RtEventChannelAdmin::handle_t Kokyu_EC::register_supplier (
+        const char * entry_point,
+        RtecEventComm::EventSourceID source,
+        RtecEventComm::EventType type,
+        RtecEventComm::PushSupplier_ptr supplier,
+        RtecEventChannelAdmin::ProxyPushConsumer_out proxy_consumer
+        ACE_ENV_ARG_DECL
+      )
+      ACE_THROW_SPEC ((
+        CORBA::SystemException
+        , RtecScheduler::UNKNOWN_TASK
+        , RtecScheduler::INTERNAL
+        , RtecScheduler::SYNCHRONIZATION_FAILURE
+      ))
+{
+  RtecScheduler::handle_t supplier1_rt_info =
+    scheduler_impl_->create (entry_point ACE_ENV_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+
+  ACE_SupplierQOS_Factory supplier_qos1;
+  supplier_qos1.insert (source,
+    type,
+    supplier1_rt_info,
+    1 /* number of calls, but what does that mean? */);
+
+  proxy_consumer =
+    supplier_admin_->obtain_push_consumer (ACE_ENV_SINGLE_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+
+  proxy_consumer->connect_push_supplier (supplier,
+    supplier_qos1.get_SupplierQOS ()
+    ACE_ENV_ARG_PARAMETER);
+  ACE_TRY_CHECK;
+  return supplier1_rt_info;
+}
+
+void Kokyu_EC::add_dependency (
+        RtEventChannelAdmin::handle_t handle,
+        RtEventChannelAdmin::handle_t dependency,
+        CORBA::Long number_of_calls,
+        RtecScheduler::Dependency_Type_t dependency_type
+        ACE_ENV_ARG_DECL
+      )
+      ACE_THROW_SPEC ((
+        CORBA::SystemException
+        , RtecScheduler::SYNCHRONIZATION_FAILURE
+        , RtecScheduler::UNKNOWN_TASK
+      ))
+{
+      scheduler_impl_->add_dependency (handle,
+                                 dependency,
+                                 number_of_calls,
+                                 dependency_type
+                                 ACE_ENV_ARG_PARAMETER);
+}
+
+void Kokyu_EC::start (ACE_ENV_SINGLE_ARG_DECL)
+      ACE_THROW_SPEC ((
+        CORBA::SystemException
+        , RtecScheduler::UNKNOWN_TASK
+        , RtecScheduler::INTERNAL
+        , RtecScheduler::SYNCHRONIZATION_FAILURE
+      ))
+{
+      RtecScheduler::RT_Info_Set_var infos;
+      RtecScheduler::Config_Info_Set_var configs;
+      RtecScheduler::Dependency_Set_var dependencies;
+      RtecScheduler::Scheduling_Anomaly_Set unsafe_anomalies;
+      RtecScheduler::Scheduling_Anomaly_Set_var anomalies;
+
+      int min_os_priority =
+        ACE_Sched_Params::priority_min (ACE_SCHED_FIFO,
+                                        ACE_SCOPE_THREAD);
+      int max_os_priority =
+        ACE_Sched_Params::priority_max (ACE_SCHED_FIFO,
+                                        ACE_SCOPE_THREAD);
+      scheduler_impl_->compute_scheduling (min_os_priority,
+                                     max_os_priority,
+                                     infos.out (),
+                                     dependencies.out (),
+                                     configs.out (),
+                                     anomalies.out ()
+                                     ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      ec_impl_->activate (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+}
+
+::RtecEventChannelAdmin::EventChannel_ptr
+Kokyu_EC::event_channel (ACE_ENV_SINGLE_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return ec_impl_->_this(ACE_ENV_SINGLE_ARG_PARAMETER);
+}
+
+::RtecScheduler::Scheduler_ptr
+Kokyu_EC::scheduler (ACE_ENV_SINGLE_ARG_DECL)
+      ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return RtecScheduler::Scheduler::_duplicate(scheduler_.in());
+}
