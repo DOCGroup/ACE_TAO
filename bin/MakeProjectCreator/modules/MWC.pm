b8aea87fa03afef575bdde7562d2341d5f3b771d
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-01-30 13:43:48 +0000

    ChangeLogTag: Fri Jan 30 07:41:20 2004  Chad Elliott  <elliott_c@ociweb.com>

---
+++ bin/MakeProjectCreator/modules/MWC.pm
@@ -0,0 +1,55 @@
+package MWC;
+
+# ******************************************************************
+# Description : Instantiate a Driver and run it
+# Author      : Chad Elliott
+# Create Date : 1/30/2004
+# ******************************************************************
+
+# ******************************************************************
+# Pragma Section
+# ******************************************************************
+
+use strict;
+use Driver;
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+sub new {
+  my($class) = shift;
+  my($self)  = bless {'creators' => [ 'GNUACEWorkspaceCreator',
+                                      'NMakeWorkspaceCreator',
+                                      'VC6WorkspaceCreator',
+                                      'VC7WorkspaceCreator',
+                                      'VC71WorkspaceCreator',
+                                      'BorlandWorkspaceCreator',
+                                      'GHSWorkspaceCreator',
+                                      'EM3WorkspaceCreator',
+                                      'VA4WorkspaceCreator',
+                                      'MakeWorkspaceCreator',
+                                      'AutomakeWorkspaceCreator',
+                                    ],
+                     }, $class;
+  return $self;
+}
+
+
+sub getCreatorList {
+  my($self) = shift;
+  return $self->{'creators'};
+}
+
+
+sub execute {
+  my($self)   = shift;
+  my($base)   = shift;
+  my($name)   = shift;
+  my($args)   = shift;
+  my($driver) = new Driver($base, $name, @{$self->{'creators'}});
+  return $driver->run(@$args);
+}
+
+
+1;
