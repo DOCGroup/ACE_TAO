f7f6efca1f6d5a4c5551012c33215b2de6e0b9e8
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-01-30 13:43:48 +0000

    ChangeLogTag: Fri Jan 30 07:41:20 2004  Chad Elliott  <elliott_c@ociweb.com>

---
+++ trunk/bin/MakeProjectCreator/modules/MPC.pm
@@ -0,0 +1,56 @@
+package MPC;
+
+# ******************************************************************
+# Description : Instantiate a Driver and run it
+# Author      : Chad Elliott
+# Create Date : 1/30/2004
+# ******************************************************************
+
+# ******************************************************************
+# Pragma Section
+# ******************************************************************
+
+use strict;
+use Driver;
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+sub new {
+  my($class) = shift;
+  my($self)  = bless {'creators' => [ 'GNUACEProjectCreator',
+                                      'NMakeProjectCreator',
+                                      'VC6ProjectCreator',
+                                      'VC7ProjectCreator',
+                                      'VC71ProjectCreator',
+                                      'BorlandProjectCreator',
+                                      'CbxProjectCreator',
+                                      'GHSProjectCreator',
+                                      'EM3ProjectCreator',
+                                      'VA4ProjectCreator',
+                                      'MakeProjectCreator',
+                                      'AutomakeProjectCreator',
+                                    ],
+                     }, $class;
+  return $self;
+}
+
+
+sub getCreatorList {
+  my($self) = shift;
+  return $self->{'creators'};
+}
+
+
+sub execute {
+  my($self)   = shift;
+  my($base)   = shift;
+  my($name)   = shift;
+  my($args)   = shift;
+  my($driver) = new Driver($base, $name, @{$self->{'creators'}});
+  return $driver->run(@$args);
+}
+
+
+1;
