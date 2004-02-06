0da18848f9c0a17703c42b2396810fb1978db4f9
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-02-06 12:37:09 +0000

    ChangeLogTag: Fri Feb  6 06:35:32 2004  Chad Elliott  <elliott_c@ociweb.com>

---
+++ trunk/bin/MakeProjectCreator/modules/CbxWorkspaceCreator.pm
@@ -0,0 +1,65 @@
+package CbxWorkspaceCreator;
+
+# ************************************************************
+# Description   : A Cbx Workspace Creator
+# Author        : Chad Elliott
+# Create Date   : 2/06/2004
+# ************************************************************
+
+# ************************************************************
+# Pragmas
+# ************************************************************
+
+use strict;
+
+use CbxProjectCreator;
+use WorkspaceCreator;
+
+use vars qw(@ISA);
+@ISA = qw(WorkspaceCreator);
+
+# ************************************************************
+# Subroutine Section
+# ************************************************************
+
+
+sub compare_output {
+  #my($self) = shift;
+  return 1;
+}
+
+
+sub workspace_file_name {
+  my($self) = shift;
+  return $self->get_modified_workspace_name($self->get_workspace_name(),
+                                            '.bpgr');
+}
+
+
+sub pre_workspace {
+  my($self) = shift;
+  my($fh)   = shift;
+  my($crlf) = $self->crlf();
+
+  print $fh "<?xml version=\"1.0\" encoding=\"UTF-8\"?>$crlf" .
+            "<!--Project Group-->$crlf";
+}
+
+
+sub write_comps {
+  my($self)     = shift;
+  my($fh)       = shift;
+  my($projects) = $self->get_projects();
+  my($pjs)      = $self->get_project_info();
+  my(@list)     = $self->sort_dependencies($projects, $pjs);
+  my($crlf)     = $self->crlf();
+
+  print $fh "<projectgroup>$crlf";
+  foreach my $project (@list) {
+    print $fh "  <project path=\"$project\"/>$crlf";
+  }
+  print $fh "</projectgroup>$crlf";
+}
+
+
+1;
