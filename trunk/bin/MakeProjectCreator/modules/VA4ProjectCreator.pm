46630604213d66b9d3c39de5d0039e76bfd5d483
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-02-17 16:33:59 +0000

    ChangeLogTag: Tue Feb 17 10:29:34 2004  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/bin/MakeProjectCreator/modules/VA4ProjectCreator.pm
+++ trunk/bin/MakeProjectCreator/modules/VA4ProjectCreator.pm
@@ -21,23 +21,6 @@ use vars qw(@ISA);
 # Subroutine Section
 # ************************************************************
 
-sub translate_value {
-  my($self) = shift;
-  my($key)  = shift;
-  my($val)  = shift;
-
-  if ($key eq 'after' && $val ne '') {
-    my($arr) = $self->create_array($val);
-    $val = '';
-    foreach my $entry (@$arr) {
-      $val .= '"' . $self->project_file_name($entry) . '" ';
-    }
-    $val =~ s/\s+$//;
-  }
-  return $val;
-}
-
-
 sub convert_slashes {
   #my($self) = shift;
   return 0;
