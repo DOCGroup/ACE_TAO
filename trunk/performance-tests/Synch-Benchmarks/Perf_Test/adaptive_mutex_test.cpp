086308be20e0bba92311fedd88b9037acdd5bb38
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-02-24 17:33:12 +0000

    ChangeLogTag: Thu Feb 19 17:33:11 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/performance-tests/Synch-Benchmarks/Perf_Test/adaptive_mutex_test.cpp
+++ trunk/performance-tests/Synch-Benchmarks/Perf_Test/adaptive_mutex_test.cpp
@@ -11,11 +11,11 @@ ACE_RCSID(Synch_Benchmarks, adaptive_mutex_test, "$Id$")
 class ACE_Svc_Export Adaptive_Mutex_Test : public Adaptive_Lock_Performance_Test_Base
 {
 public:
-  virtual int init (int, char *[]);
+  virtual int init (int, ACE_TCHAR *[]);
 };
 
 int
-Adaptive_Mutex_Test::init (int, char *[])
+Adaptive_Mutex_Test::init (int, ACE_TCHAR *[])
 {
   ACE_Lock *lock;
   ACE_NEW_RETURN (lock,
