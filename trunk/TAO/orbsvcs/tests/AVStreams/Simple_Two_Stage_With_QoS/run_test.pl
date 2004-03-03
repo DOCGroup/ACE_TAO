48bc360f436d7c477d72192ee587c938d288f34a
yamuna <yamuna@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-03-03 00:56:23 +0000

    ChangelogTag: Tue Mar  2 19:57:30 2004  Yamuna Krishnamurthy  <yamuna@oomworks.com>

--- trunk/TAO/orbsvcs/tests/AVStreams/Simple_Two_Stage_With_QoS/run_test.pl
+++ trunk/TAO/orbsvcs/tests/AVStreams/Simple_Two_Stage_With_QoS/run_test.pl
@@ -16,42 +16,94 @@ $status = 0;
 $nsior = PerlACE::LocalFile ("ns.ior");
 $outfile = PerlACE::LocalFile ("output");
 
+$debug = 0;
+
 unlink $nsior;
 
+for ($i = 0; $i <= $#ARGV; $i++) 
+{
+    if ($ARGV[$i] eq "-h" || $ARGV[$i] eq "-?") 
+    {
+        print STDERR "\nusage:  run_test\n";
+
+        print STDERR "\t-h shows options menu\n";
+
+        print STDERR "\t-p: Transport protocols defaults to (";
+        for $protocol (@protocols)
+        {
+            print STDERR "$protocol, ";
+        }
+        print STDERR ")\n";
+
+	print STDERR "\t-d: Debug Level defaults to 0";
+
+        print STDERR "\n";
+
+	exit;
+    }
+    elsif ($ARGV[$i] eq "-p") 
+    {
+	@protocols = split (',', $ARGV[$i + 1]);
+	$i++;
+    }
+    elsif ($ARGV[$i] eq "-d") 
+    {
+	$debug = $ARGV[$i + 1];
+	$i++;
+    }
+}
+
 $NS = new PerlACE::Process ("../../../Naming_Service/Naming_Service", "-o $nsior");
-$SV = new PerlACE::Process ("receiver", "-ORBInitRef NameService=file://$nsior");
-$CL = new PerlACE::Process ("sender", "-ORBInitRef NameService=file://$nsior");
 
 print STDERR "Starting Naming Service\n";
 
 $NS->Spawn ();
 
-if (PerlACE::waitforfile_timed ($nsior, 5) == -1) {
+if (PerlACE::waitforfile_timed ($nsior, 100) == -1) 
+{
     print STDERR "ERROR: cannot find naming service IOR file\n";
     $NS->Kill (); 
     exit 1;
 }
 
-print STDERR "Starting Receiver\n";
+$output_file = "TCP_output";
 
-$SV->Spawn ();
+for $protocol (@protocols)
+{
+    if ($protocol eq "RTP/UDP")
+    {
+	$output_file = "RTP_output";
+    }
+    else 
+    { 
+	$output_file = $protocol."_output";
+    }
 
-sleep $sleeptime;
-
-print STDERR "Starting Sender\n";
-
-$sender = $CL->SpawnWaitKill (60);
-
-if ($sender != 0) {
-    print STDERR "ERROR: sender returned $sender\n";
-    $status = 1;
-}
-
-$receiver = $SV->TerminateWaitKill (5);
-
-if ($receiver != 0) {
-    print STDERR "ERROR: receiver returned $receiver\n";
-    $status = 1;
+    $SV = new PerlACE::Process ("receiver", "-ORBInitRef NameService=file://$nsior -ORBDebugLevel ".$debug." -f ".$output_file);
+    $CL = new PerlACE::Process ("sender", "-ORBInitRef NameService=file://$nsior -ORBDebugLevel ".$debug." -p ".$protocol);
+    
+    print STDERR "Using ".$protocol."\n";
+    print STDERR "Starting Receiver\n";
+    
+    $SV->Spawn ();
+    
+    sleep $sleeptime;
+    
+    print STDERR "Starting Sender\n";
+    
+    $sender = $CL->SpawnWaitKill (200);
+    
+    if ($sender != 0) {
+	print STDERR "ERROR: sender returned $sender\n";
+	$status = 1;
+    }
+    
+    $receiver = $SV->TerminateWaitKill (200);
+    
+    if ($receiver != 0) {
+	print STDERR "ERROR: receiver returned $receiver\n";
+	$status = 1;
+    }
 }
 
 $nserver = $NS->TerminateWaitKill (5);
