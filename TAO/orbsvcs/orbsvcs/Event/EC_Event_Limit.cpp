6930b79408981a3d7bfcd2663acbbc76d0903a6e
thrall <thrall@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-03-09 23:43:18 +0000

    Added checks for definition of ACE_HAS_DSUI

--- TAO/orbsvcs/orbsvcs/Event/EC_Event_Limit.cpp
+++ TAO/orbsvcs/orbsvcs/Event/EC_Event_Limit.cpp
@@ -3,28 +3,39 @@
 #include "ace/CORBA_macros.h"
 #include "tao/ORB_Core.h"
 
+#ifdef ACE_HAS_DSUI
 #include <dsui.h>
 //#include "ec_dsui_config.h"
 //#include "ec_dsui_families.h"
+#endif //ACE_HAS_DSUI
 
+#ifdef ACE_HAS_DSUI
 EC_Event_Limit::EC_Event_Limit (TAO_ORB_Core* orb_ptr, ds_control* ds_ptr)
   : m_orb_ptr (orb_ptr),
     m_ds_ptr (ds_ptr)
 {}
+#else
+EC_Event_Limit::EC_Event_Limit (TAO_ORB_Core* orb_ptr)
+  : m_orb_ptr (orb_ptr)
+{}
+#endif //ACE_HAS_DSUI
 
 int EC_Event_Limit::handle_timeout (const ACE_Time_Value &, const void *)
 {
   ACE_TRY_NEW_ENV
     {
-      //DSUI_EVENT_LOG (TIMEOUT_FAM, TIMEOUT_EVENT, 0, 0, NULL);
       ACE_DEBUG((LM_DEBUG, "*************** STOP! *************\n"));
+#ifdef ACE_HAS_DSUI
+      //DSUI_EVENT_LOG (TIMEOUT_FAM, TIMEOUT_EVENT, 0, 0, NULL);
+      delete m_ds_ptr;
+      m_ds_ptr = 0;
+#endif //ACE_HAS_DSUI
       /*
-	ACE_Reactor* rx_ptr = m_orb_ptr->reactor();
-	rx_ptr->cancel_timer(this);
-	m_orb_ptr->shutdown (false
+        ACE_Reactor* rx_ptr = m_orb_ptr->reactor();
+        rx_ptr->cancel_timer(this);
+        m_orb_ptr->shutdown (false
                           ACE_ENV_ARG_PARAMETER);
       */
-      delete m_ds_ptr;
       exit(0);
       ACE_TRY_CHECK;
     }
--- TAO/orbsvcs/orbsvcs/Event/EC_Event_Limit.h
+++ TAO/orbsvcs/orbsvcs/Event/EC_Event_Limit.h
@@ -4,19 +4,29 @@
 
 #include "ace/Event_Handler.h"
 #include "tao/ORB_Core.h"
+
+#ifdef ACE_HAS_DSUI
 #include <dsui.h>
+#endif //ACE_HAS_DSUI
 
 class EC_Event_Limit : public ACE_Event_Handler
 {
 
  public:
+#ifdef ACE_HAS_DSUI
   EC_Event_Limit (TAO_ORB_Core* orb_ptr, ds_control* ds_ptr);
+#else
+  EC_Event_Limit (TAO_ORB_Core* orb_ptr);
+#endif //ACE_HAS_DSUI
+
   virtual int handle_timeout (const ACE_Time_Value &tv, const void *arg);
 
  private:
   TAO_ORB_Core* m_orb_ptr;
-  ds_control* m_ds_ptr;
-};
 
+#ifdef ACE_HAS_DSUI
+  ds_control* m_ds_ptr;
+#endif //ACE_HAS_DSUI
+};
 
 #endif /* EC_EVENT_LIMIT_H_ */
