e3d322670c764d331f0cba4226d82a0fb52d0d72
thrall <thrall@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-03-18 18:17:52 +0000

    Made more compliant with ACE coding style w/ regard to inlining

--- branches/EC_DT_RTAS/Kokyu/Counter.h
+++ branches/EC_DT_RTAS/Kokyu/Counter.h
@@ -10,30 +10,33 @@
 
 namespace Kokyu {
 
-  class Object_Counter
-    {
-    public:
+class Object_Counter
+  {
+  public:
 
-      struct object_id {
-        uint32_t id;
-        uint32_t tid;
-        uint32_t queue_id;
-      };
-
-      Object_Counter ();
-
-      inline object_id increment (void);
-
-    private:
-      unsigned int m_counter;
+    struct object_id {
+      uint32_t id;
+      uint32_t tid;
+      uint32_t queue_id;
     };
 
-#include "Counter.i"
+    Object_Counter ();
 
-  typedef ACE_TSS_Singleton<Object_Counter, ACE_Null_Mutex> ObjectCounterSingleton;
+    object_id increment (void);
+
+  private:
+    unsigned int m_counter;
+};
+
+typedef ACE_TSS_Singleton<Object_Counter, ACE_Null_Mutex> ObjectCounterSingleton;
 #define OBJECT_COUNTER ObjectCounterSingleton::instance()
 
 } /* namespace kokyu */
+
+#if defined (__ACE_INLINE__)
+#include "Counter.i"
+#endif /* __ACE_INLINE__ */
+
 #include "ace/post.h"
 #endif /* OBJECT_COUNTER_H */
 
