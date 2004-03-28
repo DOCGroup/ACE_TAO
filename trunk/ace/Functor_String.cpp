47c28afacd65902287e611bc74458212060a1791
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-03-28 18:16:09 +0000

    ChangeLogTag:Sun Mar 28 12:14:05 2004  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

---
+++ trunk/ace/Functor_String.cpp
@@ -0,0 +1,8 @@
+#include "ace/Functor_T.h"
+#include "ace/Functor_String.h"
+
+#if !defined (__ACE_INLINE__)
+#include "ace/Functor_String.inl"
+#endif /* __ACE_INLINE__ */
+
+ACE_RCSID(ace, Functor, "$Id$")
