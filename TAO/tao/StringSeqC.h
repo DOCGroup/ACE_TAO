e3023946a1462d5fac57f68e71d7f54d41929e3e
Johnny Willemsen <jwillemsen@remedy.nl>
2004-04-12 16:46:22 +0000

    ChangeLogTag: Mon Apr 12 16:45:43 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/StringSeqC.h
+++ TAO/tao/StringSeqC.h
@@ -26,7 +26,7 @@
 //     http://www.cs.wustl.edu/~schmidt/TAO.html
 
 // TAO_IDL - Generated from
-// be/be_codegen.cpp:186
+// be\be_codegen.cpp:171
 
 #ifndef _TAO_IDL_ORIG_STRINGSEQC_H_
 #define _TAO_IDL_ORIG_STRINGSEQC_H_
@@ -39,6 +39,8 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+#include "tao/TAO_Export.h"
+#include "tao/CDR.h"
 #include "tao/Seq_Var_T.h"
 #include "tao/Seq_Out_T.h"
 
@@ -66,13 +68,13 @@
 #endif /* __BORLANDC__ */
 
 // TAO_IDL - Generated from
-// be/be_visitor_module/module_ch.cpp:48
+// be\be_visitor_module/module_ch.cpp:48
 
 namespace CORBA
 {
 
   // TAO_IDL - Generated from
-  // be/be_visitor_sequence/sequence_ch.cpp:101
+  // be\be_visitor_sequence/sequence_ch.cpp:101
 
 #if !defined (_CORBA_STRINGSEQ_CH_)
 #define _CORBA_STRINGSEQ_CH_
@@ -118,12 +120,12 @@ namespace CORBA
 #endif /* end #if !defined */
 
   // TAO_IDL - Generated from
-  // be/be_visitor_typecode/typecode_decl.cpp:44
+  // be\be_visitor_typecode/typecode_decl.cpp:44
 
   TAO_NAMESPACE_STORAGE_CLASS ::CORBA::TypeCode_ptr _tc_StringSeq;
 
   // TAO_IDL - Generated from
-  // be/be_visitor_sequence/sequence_ch.cpp:101
+  // be\be_visitor_sequence/sequence_ch.cpp:101
 
 #if !defined (_CORBA_WSTRINGSEQ_CH_)
 #define _CORBA_WSTRINGSEQ_CH_
@@ -169,17 +171,17 @@ namespace CORBA
 #endif /* end #if !defined */
 
   // TAO_IDL - Generated from
-  // be/be_visitor_typecode/typecode_decl.cpp:44
+  // be\be_visitor_typecode/typecode_decl.cpp:44
 
   TAO_NAMESPACE_STORAGE_CLASS ::CORBA::TypeCode_ptr _tc_WStringSeq;
 
 // TAO_IDL - Generated from
-// be/be_visitor_module/module_ch.cpp:66
+// be\be_visitor_module/module_ch.cpp:66
 
 } // module CORBA
 
 // TAO_IDL - Generated from
-// be/be_visitor_traits.cpp:50
+// be\be_visitor_traits.cpp:59
 
 // Traits specializations.
 namespace TAO
@@ -187,7 +189,7 @@ namespace TAO
 }
 
 // TAO_IDL - Generated from
-// be/be_visitor_sequence/any_op_ch.cpp:52
+// be\be_visitor_sequence/any_op_ch.cpp:52
 
 TAO_Export void operator<<= (CORBA::Any &, const CORBA::StringSeq &); // copying version
 TAO_Export void operator<<= (CORBA::Any &, CORBA::StringSeq*); // noncopying version
@@ -195,7 +197,7 @@ TAO_Export CORBA::Boolean operator>>= (const CORBA::Any &, CORBA::StringSeq *&);
 TAO_Export CORBA::Boolean operator>>= (const CORBA::Any &, const CORBA::StringSeq *&);
 
 // TAO_IDL - Generated from
-// be/be_visitor_sequence/any_op_ch.cpp:52
+// be\be_visitor_sequence/any_op_ch.cpp:52
 
 TAO_Export void operator<<= (CORBA::Any &, const CORBA::WStringSeq &); // copying version
 TAO_Export void operator<<= (CORBA::Any &, CORBA::WStringSeq*); // noncopying version
@@ -203,12 +205,7 @@ TAO_Export CORBA::Boolean operator>>= (const CORBA::Any &, CORBA::WStringSeq *&)
 TAO_Export CORBA::Boolean operator>>= (const CORBA::Any &, const CORBA::WStringSeq *&);
 
 // TAO_IDL - Generated from
-// be/be_visitor_root/cdr_op.cpp:48
-
-#ifndef __ACE_INLINE__
-
-// TAO_IDL - Generated from
-// be/be_visitor_sequence/cdr_op_ch.cpp:70
+// be\be_visitor_sequence/cdr_op_ch.cpp:71
 
 #if !defined _TAO_CDR_OP_CORBA_StringSeq_H_
 #define _TAO_CDR_OP_CORBA_StringSeq_H_
@@ -225,7 +222,7 @@ TAO_Export CORBA::Boolean operator>> (
 #endif /* _TAO_CDR_OP_CORBA_StringSeq_H_ */
 
 // TAO_IDL - Generated from
-// be/be_visitor_sequence/cdr_op_ch.cpp:70
+// be\be_visitor_sequence/cdr_op_ch.cpp:71
 
 #if !defined _TAO_CDR_OP_CORBA_WStringSeq_H_
 #define _TAO_CDR_OP_CORBA_WStringSeq_H_
@@ -242,12 +239,7 @@ TAO_Export CORBA::Boolean operator>> (
 #endif /* _TAO_CDR_OP_CORBA_WStringSeq_H_ */
 
 // TAO_IDL - Generated from
-// be/be_visitor_root/cdr_op.cpp:64
-
-#endif /* __ACE_INLINE__ */
-
-// TAO_IDL - Generated from
-// be/be_codegen.cpp:1050
+// be\be_codegen.cpp:978
 
 #if defined (__ACE_INLINE__)
 #include "StringSeqC.i"
