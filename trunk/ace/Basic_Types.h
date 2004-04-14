f3617eb88f6cdaf13b0d2bcfd0ff4d9501d91fe4
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-04-14 20:20:20 +0000

    ChangeLogTag:Wed Apr 14 16:14:59 2004  Steve Huston  <shuston@riverace.com>

--- trunk/ace/Basic_Types.h
+++ trunk/ace/Basic_Types.h
@@ -292,7 +292,7 @@ typedef ptrdiff_t ptr_arith_t;
   // We weren't explicitly told, so we have to figure it out . . .
 #   if defined (i386) || defined (__i386__) || defined (_M_IX86) || \
      defined (vax) || defined (__alpha) || defined (__LITTLE_ENDIAN__) ||\
-     defined (ARM) || defined (_M_IA64)
+     defined (ARM) || defined (_M_IA64) || defined (_M_AMD64)
     // We know these are little endian.
 #     define ACE_LITTLE_ENDIAN 0x0123
 #     define ACE_BYTE_ORDER ACE_LITTLE_ENDIAN
