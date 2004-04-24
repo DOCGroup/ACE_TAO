6a77218f9e95812ad8ad64f4f415babc81b43580
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-04-24 21:00:13 +0000

    *** empty log message ***

---
+++ trunk/TAO/CIAO/DnC/Config_Handlers/Domain_Handler.h
@@ -0,0 +1,102 @@
+//==================================================================
+/**
+ *  @file  Domain_Handler.h
+ *
+ *  $Id$
+ *
+ *  @author Jaiganesh Balasubramanian <jai@dre.vanderbilt.edu>
+ */
+//=====================================================================
+#ifndef DOMAIN_HANDLER_H
+#define DOMAIN_HANDLER_H
+#include /**/ "ace/pre.h"
+
+#include "DeploymentC.h"
+#include "Config_Handler_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/SString.h"
+#include <xercesc/util/XercesDefs.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include "XercesString.h"
+
+using Config_Handler::XStr;
+using xercesc::XMLUni;
+using xercesc::XMLString;
+using xercesc::XMLException;
+using xercesc::DOMException;
+using xercesc::DOMBuilder;
+using xercesc::DOMImplementationRegistry;
+using xercesc::DOMImplementationLS;
+using xercesc::DOMImplementation;
+using xercesc::DOMText;
+using xercesc::DOMNamedNodeMap;
+using xercesc::DOMLocator;
+using xercesc::DOMError;
+using xercesc::DOMDocument;
+using xercesc::DOMDocumentTraversal;
+using xercesc::DOMNodeIterator;
+using xercesc::DOMNode;
+using xercesc::DOMNodeFilter;
+
+namespace CIAO 
+{
+  namespace Config_Handler
+  {
+    /**
+     * @class Domain_Handler
+     *
+     * @brief Handler class for <Domain> type 
+     *
+     * This class is within the Target Data Model subpackage of the
+     * Deployment & Configuration package.
+     *
+     * This class defines handler methods to parse the aforementioned type
+     * in the description files. The corresponding CORBA IDL type for this
+     * element is returned.
+     */
+
+    class Config_Handler_Export Domain_Handler
+    {
+    public:
+
+      /// constructor
+      Domain_Handler (DOMDocument* doc, unsigned long filter_);
+
+      /// constructor
+      Domain_Handler (DOMNodeIterator* iter, bool release = false);
+
+      /// destructor
+      ~Domain_Handler(void);
+
+      /// Process the Domain type
+      void process_domain (::Deployment::Domain &domain);
+
+      /// Process the label attribute
+      void process_label (const XMLCh* label, ::Deployment::Domain &domain);
+
+      /// Process the UUID attribute
+      void process_uuid (const XMLCh* uuid, ::Deployment::Domain &domain);
+
+    private:
+
+      DOMDocument* doc_;
+
+      DOMNode* root_;
+
+      unsigned long filter_;
+
+      DOMNodeIterator* iter_;
+
+      bool release_;
+
+    };
+  };
+};
+
+#include /**/ "ace/post.h"
+
+#endif /* DOMAIN_HANDLER_H */
