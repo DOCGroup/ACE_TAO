bf9d8dd42e97739dd58182f828fc58a818eb9f2a
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-04-26 10:40:41 +0000

    *** empty log message ***

---
+++ TAO/CIAO/DnC/Config_Handlers/IR_Handler.h
@@ -0,0 +1,125 @@
+// $Id$
+
+#ifndef IR_HANDLER_H
+#define IR_HANDLER_H
+#include /**/ "ace/pre.h"
+
+#include "DeploymentC.h"
+#include "Config_Handler_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/SString.h"
+#include <xercesc/util/XercesDefs.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include "XercesString.h"
+
+using Config_Handler::XStr;
+using xercesc::XMLUni;
+using xercesc::XMLString;
+using xercesc::XMLException;
+using xercesc::DOMException;
+using xercesc::DOMBuilder;
+using xercesc::DOMImplementationRegistry;
+using xercesc::DOMImplementationLS;
+using xercesc::DOMImplementation;
+using xercesc::DOMText;
+using xercesc::DOMNamedNodeMap;
+using xercesc::DOMLocator;
+using xercesc::DOMError;
+using xercesc::DOMDocument;
+using xercesc::DOMDocumentTraversal;
+using xercesc::DOMNodeIterator;
+using xercesc::DOMNode;
+using xercesc::DOMNodeFilter;
+
+namespace CIAO 
+{
+  namespace Config_Handler
+  {
+
+    class Req_Handler
+    {
+    public:
+
+      /// constructor
+      Req_Handler (DOMDocument* doc, unsigned long filter_);
+
+      /// constructor
+      Req_Handler (DOMNodeIterator* iter, bool release = false);
+
+      /// destructor
+      ~Req_Handler();
+
+      /// Process the package configuration
+      void process_Requirement (::Deployment::Requirement &req);
+
+      /// Process the label attribute
+      void process_name (const XMLCh* name, ::Deployment::Requirement &req);
+
+      /// Process the label attribute
+      void process_resourceType (const XMLCh* name, ::Deployment::Requirement &req);
+
+    private:
+
+      DOMDocument* doc_;
+
+      DOMNode* root_;
+
+      unsigned long filter_;
+
+      DOMNodeIterator* iter_;
+
+      bool release_;
+
+    };
+
+    // TODO: This class suppose to derive drom Req_Handler?
+    class IR_Handler
+    {
+    public:
+
+      /// constructor
+      IR_Handler (DOMDocument* doc, unsigned long filter_);
+
+      /// constructor
+      IR_Handler (DOMNodeIterator* iter, bool release = false);
+
+      /// destructor
+      ~IR_Handler();
+
+      /// Process the package configuration
+      void process_ImplementationRequirement (::Deployment::ImplementationRequirement &ir);
+
+      /// Process the resourceUsage attribute
+      void process_resourceUsage (const XMLCh* name, ::Deployment::ImplementationRequirement &ir);
+
+      /// Process the resourcePort attribute
+      void process_resourcePort (const XMLCh* name, ::Deployment::ImplementationRequirement &ir);
+
+      /// Process the componentPort attribute
+      void process_componentPort (const XMLCh* name, ::Deployment::ImplementationRequirement &ir);
+
+    private:
+
+      DOMDocument* doc_;
+
+      DOMNode* root_;
+
+      unsigned long filter_;
+
+      DOMNodeIterator* iter_;
+
+      bool release_;
+
+    };
+
+  }
+
+}
+
+#include /**/ "ace/post.h"
+
+#endif /* IR_HANDLER_H */
---
+++ TAO/CIAO/DnC/Config_Handlers/NIA_Handler.cpp
@@ -0,0 +1,93 @@
+// $Id$
+
+#ifndef NIA_HANDLER_C
+#define NIA_HANDLER_C
+
+#include "tao/Exception.h"
+#include "ace/Auto_Ptr.h"
+#include "ace/Log_Msg.h"
+
+#include "IAD_Handler.h"
+#include "NIA_Handler.h"
+
+#include <iostream>
+
+using std::cerr;
+using std::endl;
+
+namespace CIAO
+{
+  namespace Config_Handler
+  {
+    NIA_Handler::NIA_Handler (DOMDocument* doc, unsigned long filter)
+      : doc_ (doc),
+        root_ (doc->getDocumentElement()),
+        filter_ (filter),
+        iter_ (doc_->createNodeIterator (this->root_,
+                                              this->filter_,
+                                              0,
+                                              true)),
+        release_ (true)
+    {}
+
+    NIA_Handler::NIA_Handler (DOMNodeIterator* iter, bool release)
+      : doc_ (0), root_ (0), filter_ (0), iter_ (iter), release_ (release)
+    {}
+
+
+    NIA_Handler::~NIA_Handler()
+    {
+      if (this->release_)
+        this->iter_->release();
+    }
+
+    /// handle the package configuration and populate it
+    void NIA_Handler::process_NamedImplementationArtifact
+      (::Deployment::NamedImplementationArtifact &nia)
+    {
+      // This is bogus and should be replaced later.
+      ACE_DECLARE_NEW_CORBA_ENV;
+
+      for (DOMNode* node = this->iter_->nextNode();
+           node != 0;
+           node = this->iter_->nextNode())
+        {
+          XStr node_name (node->getNodeName());
+          if (node_name == XStr (ACE_TEXT ("name")))
+            {
+              // Fetch the text node which contains the "label"
+	      node = this->iter_->nextNode();
+	      DOMText* text = ACE_reinterpret_cast (DOMText*, node);
+              this->process_name (text->getNodeValue(), nia);
+	    }
+	  else if (node_name == XStr (ACE_TEXT ("referencedArtifact")))
+            {
+              // fetch the component package reference handler
+              IAD_Handler iad_handler (iter_, false);
+
+              // delegate the populating process
+              iad_handler.process_ImplementationArtifactDescription (nia.referencedArtifact);
+            }
+          else
+            {
+              // ??? How did we get here ???
+              ACE_THROW (CORBA::INTERNAL());
+            }
+        }
+      return;
+    }
+
+    /// handle name attribute
+    void NIA_Handler::process_name
+      (const XMLCh* name, ::Deployment::NamedImplementationArtifact &nia)
+    {
+      if (name)
+        {
+          nia.name = XMLString::transcode (name);
+        }
+    }
+
+  }
+}
+
+#endif /* NIA_HANDLER_C */
