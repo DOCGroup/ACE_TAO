cf9d045c1de3a6029be26599f0a37a5fbfba2ffc
yfzhang <yfzhang@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-04-28 22:47:18 +0000

    *** empty log message ***

--- branches/EC_DT_RTAS/TAO/examples/Kokyu_dsrt_schedulers/chain_example/threeway_client.cpp
+++ branches/EC_DT_RTAS/TAO/examples/Kokyu_dsrt_schedulers/chain_example/threeway_client.cpp
@@ -507,7 +507,7 @@ Worker::svc (void)
 {
   /* MEASURE: Worker start time */
   Object_ID oid = ACE_OBJECT_COUNTER->increment();
-  oid.task_id = m_id;
+  oid.task_id = ID_BEGIN++;
   DSTRM_EVENT (WORKER_GROUP_FAM, WORKER_STARTED, 0, sizeof(Object_ID), (char*)&oid);
 
   ACE_DECLARE_NEW_CORBA_ENV;
@@ -552,7 +552,7 @@ Worker::svc (void)
                                          ACE_Time_Value (period_,0) -
 					 ACE_Time_Value (left_work,0));
       sched_param.period = period_*10000000;
-      sched_param.task_id = ID_BEGIN++;
+      sched_param.task_id = oid.task_id;
       sched_param.id = oid.id;
       sched_param.pid = oid.pid;
       sched_param.tid = oid.tid;
@@ -587,6 +587,7 @@ Worker::svc (void)
           sched_param.deadline = sched_param.deadline+period_*10000000-left_work*10000000;
           sched_param_policy = scheduler_->create_scheduling_parameter (sched_param);
           oid = ACE_OBJECT_COUNTER->increment();
+	  oid.id = sched_param.task_id;
           sched_param.id = oid.id;
           sched_param.tid = oid.tid;
           sched_param.pid = oid.pid;
@@ -611,7 +612,7 @@ Worker::svc (void)
 
       timeval tv;
 
-//      ACE_DEBUG((LM_DEBUG,"OBJECT id is %d\n",oid.id));
+      ACE_DEBUG((LM_DEBUG,"OBJECT id is %d and thread id is %d\n",oid.id,oid.tid));
       DSTRM_EVENT (TEST_ONE_FAM, START_SERVICE, 0, sizeof(Object_ID), (char*)&oid);
 
       tv.tv_sec = server_load_-1;
@@ -707,7 +708,7 @@ Worker_c::svc (void)
 {
   /* MEASURE: Worker start time */
   Object_ID oid = ACE_OBJECT_COUNTER->increment();
-  oid.task_id = m_id;
+  oid.task_id = ID_BEGIN++;
   DSTRM_EVENT (WORKER_GROUP_FAM, WORKER_STARTED, 0, sizeof(Object_ID), (char*)&oid);
 
   ACE_DECLARE_NEW_CORBA_ENV;
@@ -751,7 +752,7 @@ Worker_c::svc (void)
                                          ACE_OS::gettimeofday () +
                                          ACE_Time_Value (period_,0) );
       sched_param.period = period_*10000000;
-      sched_param.task_id = ID_BEGIN++;
+      sched_param.task_id = oid.task_id;
       sched_param.id = oid.id;
       sched_param.tid = oid.tid;
       sched_param.pid = oid.pid;
