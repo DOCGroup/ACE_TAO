eba62c15deca5bf882897a3c700b5c48e5de6ed7
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-04-29 22:09:51 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/Cap_Handler.h
+++ TAO/CIAO/DnC/Config_Handlers/Cap_Handler.h
@@ -4,9 +4,9 @@
  *
  *  $Id$
  *
- *  @author Emre Turkay  <turkaye@dre.vanderbilt.edu>
+ *  @author Emre Turkay <turkaye@dre.vanderbilt.edu>
  */
-//=====================================================================
+//==================================================================
 
 #ifndef CAP_HANDLER_H
 #define CAP_HANDLER_H
@@ -15,112 +15,43 @@
 #include "DeploymentC.h"
 #include "Config_Handler_export.h"
 
-#if !defined (ACE_LACKS_PRAGMA_ONCE)
-#pragma once
-#endif /* ACE_LACKS_PRAGMA_ONCE */
-
-#include "ace/SString.h"
 #include <xercesc/util/XercesDefs.hpp>
 #include <xercesc/dom/DOM.hpp>
 #include "XercesString.h"
 
 using Config_Handler::XStr;
-using xercesc::XMLUni;
 using xercesc::XMLString;
-using xercesc::XMLException;
-using xercesc::DOMException;
-using xercesc::DOMBuilder;
-using xercesc::DOMImplementationRegistry;
-using xercesc::DOMImplementationLS;
-using xercesc::DOMImplementation;
 using xercesc::DOMText;
-using xercesc::DOMNamedNodeMap;
-using xercesc::DOMLocator;
-using xercesc::DOMError;
-using xercesc::DOMDocument;
-using xercesc::DOMDocumentTraversal;
 using xercesc::DOMNodeIterator;
 using xercesc::DOMNode;
-using xercesc::DOMNodeFilter;
 
-namespace CIAO 
+namespace CIAO
 {
   namespace Config_Handler
   {
-
-    class RS_Handler
+    /**
+     * @class CAP_Handler
+     *
+     * @brief Handler class for <Capability> type
+     *
+     * This class defines handler methods to parse Capability
+     * types in the descriptor files. The corresponding
+     * CORBA IDL type for the schema element is returned.
+     *
+     */
+    class Config_Handler_Export CAP_Handler
     {
     public:
-
-      /// constructor
-      RS_Handler (DOMDocument* doc, unsigned long filter_);
-
-      /// constructor
-      RS_Handler (DOMNodeIterator* iter, bool release = false);
-
-      /// destructor
-      ~RS_Handler();
-
-      /// Process the package configuration
-      void process_RequirementSatisfier (::Deployment::RequirementSatisfier &rs);
-
-      /// Process the label attribute
-      void process_name (const XMLCh* name, ::Deployment::RequirementSatisfier &rs);
-
-      /// Process the label attribute
-      void process_resourceType (const XMLCh* name, ::Deployment::RequirementSatisfier &rs);
-
-    private:
-
-      DOMDocument* doc_;
-
-      DOMNode* root_;
-
-      unsigned long filter_;
-
-      DOMNodeIterator* iter_;
-
-      bool release_;
+      static void
+        process_Capability (DOMNodeIterator * iter,
+			    Deployment::Capability &ret_struct);
+      // process elements of type Capability definitions in
+      // the descriptor files
 
     };
-
-    // TODO: This class suppose to derive drom RS_Handler?
-    //       If not then process_name and process_resourceType should be
-    //       defined here
-    class Cap_Handler
-    {
-    public:
-
-      /// constructor
-      Cap_Handler (DOMDocument* doc, unsigned long filter_);
-
-      /// constructor
-      Cap_Handler (DOMNodeIterator* iter, bool release = false);
-
-      /// destructor
-      ~Cap_Handler();
-
-      /// Process the package configuration
-      void process_Capability (::Deployment::Capability &cap);
-
-    private:
-
-      DOMDocument* doc_;
-
-      DOMNode* root_;
-
-      unsigned long filter_;
-
-      DOMNodeIterator* iter_;
-
-      bool release_;
-
-    };
-
   }
-
 }
 
 #include /**/ "ace/post.h"
 
-#endif /* CAP_HANDLER_H */
+#endif /* ID_HANDLER_H */
