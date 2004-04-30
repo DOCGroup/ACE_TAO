cfe8ab4e912dbbb5294f6bc54742e889cd5096dc
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-04-30 03:51:59 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/IR_Handler.cpp
+++ TAO/CIAO/DnC/Config_Handlers/IR_Handler.cpp
@@ -91,9 +91,6 @@ namespace CIAO
     void IR_Handler::process_ImplementationRequirement
       (::Deployment::ImplementationRequirement &ir)
     {
-      // This is bogus and should be replaced later.
-      ACE_DECLARE_NEW_CORBA_ENV;
-
       for (DOMNode* node = this->iter_->nextNode();
            node != 0;
            node = this->iter_->nextNode())
