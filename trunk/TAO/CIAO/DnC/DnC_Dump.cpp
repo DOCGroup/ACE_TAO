2422c76e473b23c6f006be0b7dcfbfefb75450d8
edwardgt <edwardgt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-03 22:44:09 +0000

    *** empty log message ***

--- trunk/TAO/CIAO/DnC/DnC_Dump.cpp
+++ trunk/TAO/CIAO/DnC/DnC_Dump.cpp
@@ -69,6 +69,74 @@ namespace Deployment
       }
   }
 
+  // ComponentPackageDescription
+
+  void DnC_Dump::dump (const ::Deployment::ComponentPackageDescription &comppkgdesc)
+  {
+    ACE_DEBUG ((LM_DEBUG, "ComponentPackageDescription: \n"));
+    
+    ACE_DEBUG ((LM_DEBUG, "  label: %s\n", comppkgdesc.label.in ()));
+    ACE_DEBUG ((LM_DEBUG, "  UUID: %s\n", comppkgdesc.UUID.in ()));
+    
+    ACE_DEBUG ((LM_DEBUG, "  configProperty: \n"));
+    for (CORBA::ULong i = 0;
+          i < comppkgdesc.configProperty.length ();
+          ++i)
+      {
+        DnC_Dump::dump (comppkgdesc.configProperty[i]); // Property
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  infoProperty: \n"));
+    for (CORBA::ULong i = 0;
+          i < comppkgdesc.infoProperty.length ();
+          ++i)
+      {
+        DnC_Dump::dump (comppkgdesc.infoProperty[i]); // Property
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  realizes: \n"));
+    DnC_Dump::dump (comppkgdesc.realizes); // ComponentInterfaceDescription
+
+    ACE_DEBUG ((LM_DEBUG, "  implementation: \n"));
+    for (CORBA::ULong i = 0;
+          i < comppkgdesc.implementation.length ();
+          ++i)
+      {
+        DnC_Dump::dump (comppkgdesc.implementation[i]); // PackagedComponentImplementation
+      }
+  }
+
+  // ComponentPortDescription
+
+  void DnC_Dump::dump (const ::Deployment::ComponentPortDescription &compportdesc)
+  {
+    ACE_DEBUG ((LM_DEBUG, "ComponentPortDescription: \n"));
+    
+    ACE_DEBUG ((LM_DEBUG, "  name: %s\n", compportdesc.name.in ()));
+    ACE_DEBUG ((LM_DEBUG, "  specificType: %s\n", compportdesc.specificType.in ()));
+
+    ACE_DEBUG ((LM_DEBUG, "  supportedType: \n"));
+    for (CORBA::ULong i = 0;
+          i < compportdesc.supportedType.length ();
+          ++i)
+      {
+        ACE_DEBUG ((LM_DEBUG, "%s\n", compportdesc.supportedType[i].in ()));
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  provider: %i\n", compportdesc.provider));
+    ACE_DEBUG ((LM_DEBUG, "  exclusiveProvider: %i\n", compportdesc.exclusiveProvider));
+    ACE_DEBUG ((LM_DEBUG, "  exclusiveUser: %i\n", compportdesc.exclusiveUser));
+    ACE_DEBUG ((LM_DEBUG, "  optional: %i\n", compportdesc.optional));
+  }
+
+  // ComponentPropertyDescription
+
+  void DnC_Dump::dump (const ::Deployment::ComponentPropertyDescription &comppropdesc)
+  {
+    ACE_DEBUG ((LM_DEBUG, "ComponentPropertyDescription: \n"));  
+    ACE_DEBUG ((LM_DEBUG, "  name: %s\n", comppropdesc.name.in ()));
+    ACE_DEBUG ((LM_DEBUG, "  type: \n"));
+  }
 
   // MonolithicImplementationDescription
 
@@ -99,6 +167,66 @@ namespace Deployment
       {
         DnC_Dump::dump (mid.primaryArtifact[i]); // NamedImplementationArtifact
       }
+  }
+
+  // PackageConfiguration
+
+  void DnC_Dump::dump (const ::Deployment::PackageConfiguration &pc)
+  {
+    ACE_DEBUG ((LM_DEBUG, "PackageConfiguration: \n"));  
+    ACE_DEBUG ((LM_DEBUG, "  label: %s\n", pc.label.in ()));
+    ACE_DEBUG ((LM_DEBUG, "  UUID: %s\n", pc.UUID.in ()));
+
+    ACE_DEBUG ((LM_DEBUG, "  specializedConfig: \n"));
+    for (CORBA::ULong i = 0;
+          i < pc.specializedConfigRef.length ();
+          ++i)
+      {
+        ACE_DEBUG ((LM_DEBUG, "%i\n", pc.specializedConfigRef[i]));
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  configProperty: \n"));
+    for (CORBA::ULong i = 0;
+          i < pc.configProperty.length ();
+          ++i)
+      {
+        DnC_Dump::dump (pc.configProperty[i]); // Property
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  selectRequirement: \n"));
+    for (CORBA::ULong i = 0;
+          i < pc.selectRequirement.length ();
+          ++i)
+      {
+        DnC_Dump::dump (pc.selectRequirement[i]); // Requirement
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  reference: \n"));
+    for (CORBA::ULong i = 0;
+          i < pc.reference.length ();
+          ++i)
+      {
+        DnC_Dump::dump (pc.reference[i]); // ComponentPackageReference
+      }
+
+    ACE_DEBUG ((LM_DEBUG, "  basePackage: \n"));
+    for (CORBA::ULong i = 0;
+          i < pc.basePackage.length ();
+          ++i)
+      {
+        DnC_Dump::dump (pc.basePackage[i]); // ComponentPackageDescription
+      }
+  }
+
+  // PackagedComponentImplementation
+
+  void DnC_Dump::dump (const ::Deployment::PackagedComponentImplementation &pci)
+  {
+    ACE_DEBUG ((LM_DEBUG, "PackagedComponentImplementation: \n"));  
+    ACE_DEBUG ((LM_DEBUG, "  name: %s\n", pci.name.in ()));
+
+    ACE_DEBUG ((LM_DEBUG, "  referencedImplementation: \n"));
+    DnC_Dump::dump (pci.referencedImplementation); // ComponentImplementationDescription
   }
 
   // SubcomponentPortEndpoint
