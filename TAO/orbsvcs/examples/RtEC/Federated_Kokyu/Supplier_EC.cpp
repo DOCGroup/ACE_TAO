cca37e30a928a908673c272a151640fb3e608916
thrall <thrall@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-05 22:27:44 +0000

    Turned ACE_DEBUG for start time to ACE_OS::printf and merged time gets with ACE_DEBUGs in Supplier.cpp

--- TAO/orbsvcs/examples/RtEC/Federated_Kokyu/Supplier_EC.cpp
+++ TAO/orbsvcs/examples/RtEC/Federated_Kokyu/Supplier_EC.cpp
@@ -213,7 +213,8 @@ main (int argc, char* argv[])
 
   //@BT
   //DSTRM_EVENT(MAIN_GROUP_FAM, START,1,0,NULL);
-  ACE_DEBUG((LM_DEBUG,"Supplier_EC thread %t START at %u\n",ACE_OS::gettimeofday().msec()));
+  ACE_Time_Value now(ACE_OS::gettimeofday());
+  ACE_OS::printf("Supplier_EC START at %isec %iusec\n",now.sec(),now.usec());
   DSTRM_EVENT(MAIN_GROUP_FAM, START,0,0,NULL);
 
   ACE_DECLARE_NEW_CORBA_ENV;
