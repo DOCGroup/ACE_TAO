141db1a337c51f5b0780bbaba43cc88077326383
arvindk <arvindk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-06 21:25:11 +0000

    Thu May  6 16:20:00 2004  Arvind S. Krishna  <arvindk@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DnC/Config_Handlers/SP_Handler.cpp
+++ trunk/TAO/CIAO/DnC/Config_Handlers/SP_Handler.cpp
@@ -17,48 +17,35 @@ CIAO::Config_Handler::SP_Handler::
 process_SatisfierProperty (DOMNodeIterator * iter,
                            Deployment::SatisfierProperty &property)
 {
-  //Check if the Schema IDs for both the elements match
-  DOMNode * node = iter->nextNode ();
-  XStr name (node->getNodeName ());
-  if (name != XStr (ACE_TEXT ("name")))
+  for (DOMNode * node = iter->nextNode (); ; node = iter->nextNode ())
     {
-      ACE_DEBUG ((LM_DEBUG,
-                  "Config_Handlers::SP_Handler::process_RequirementSatisfier \
-                   element mismatch expected <name>"));
-      ACE_THROW (CORBA::INTERNAL ());
+      // Get name of the current element
+      XStr name (node->getNodeName ());
+
+      if (name == XStr (ACE_TEXT ("name")))
+        // Populate name field in the Structure
+        property.name = Utils::parse_string (iter);
+      else
+        if (name == XStr (ACE_TEXT ("Deployment:SatisfierPropertyKind")))
+            // Populate the Satisfier property kind value
+          SPK_Handler::process_SatisfierPropertyKind (iter, property.kind);
+        else
+          if (name == XStr (ACE_TEXT ("Deployment:Any")))
+            // Populate value for the Property
+            Any_Handler::process_Any (iter, property.value);
+          else
+            {
+              ACE_DEBUG ((LM_DEBUG, "Config_Handlers::SP_Handler::process_ \
+                         RequirementSatisfier element mismatch expected <name> \
+                         or <PropertyKind> <Deployment:Any>"));
+
+              // Get the previous iterator
+              iter->previousNode ();
+
+              // Exit from the for loop
+              break;
+            }
     }
-
-  // Populate name field in the Structure
-  property.name = Utils::parse_string (iter);
-
-  // Check if any <SatisfierProperty> elements are present
-  node = iter->nextNode ();
-  name = node->getNodeName ();
-
-  if (name != XStr (ACE_TEXT ("Deployment:SatisfierPropertyKind")))
-    {
-      ACE_DEBUG ((LM_DEBUG,
-                  "Config_Handlers::SP_Handler::process_SP \
-                   did not find any <SatisfierProperty> tags"));
-      // Shift back
-      iter->previousNode ();
-    }
-  else
-    SPK_Handler::process_SatisfierPropertyKind (iter, property.kind);
-
-  // Process Any type
-  node = iter->nextNode ();
-  name = node->getNodeName ();
-  if (name != XStr (ACE_TEXT ("Deployment:Any")))
-    {
-      ACE_DEBUG ((LM_DEBUG,
-                  "Config_Handlers::SP_Handler::process_SP \
-                   did not fine any <Andy> tags"));
-      // Shift back
-      iter->previousNode ();
-    }
-  else
-    Any_Handler::process_Any (iter, property.value);
 }
 
 #endif /* RS_HANDLER_C */
