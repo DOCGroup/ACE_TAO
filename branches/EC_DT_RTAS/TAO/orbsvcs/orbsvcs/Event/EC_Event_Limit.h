213363dddf77f7aceb06caaf2eb3bffeee413e6d
thrall <thrall@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-08 23:55:49 +0000

    Changed EC_Event_Limit to produce PROGRAM_STOP instead of generic TIMEOUT_EVENT

--- branches/EC_DT_RTAS/TAO/orbsvcs/orbsvcs/Event/EC_Event_Limit.h
+++ branches/EC_DT_RTAS/TAO/orbsvcs/orbsvcs/Event/EC_Event_Limit.h
@@ -3,7 +3,7 @@
 #define EC_EVENT_LIMIT_H_
 
 #include "ace/Event_Handler.h"
-#include "tao/ORB_Core.h"
+#include "tao/ORB.h"
 
 #ifdef ACE_HAS_DSUI
 #include <dsui.h>
@@ -14,15 +14,15 @@ class EC_Event_Limit : public ACE_Event_Handler
 
  public:
 #ifdef ACE_HAS_DSUI
-  EC_Event_Limit (TAO_ORB_Core* orb_ptr, ds_control* ds_ptr);
+  EC_Event_Limit (CORBA::ORB_var orb, ds_control* ds_ptr);
 #else
-  EC_Event_Limit (TAO_ORB_Core* orb_ptr);
+  EC_Event_Limit (CORBA::ORB_var orb);
 #endif //ACE_HAS_DSUI
 
   virtual int handle_timeout (const ACE_Time_Value &tv, const void *arg);
 
  private:
-  TAO_ORB_Core* m_orb_ptr;
+  CORBA::ORB_var m_orb_;
 
 #ifdef ACE_HAS_DSUI
   ds_control* m_ds_ptr;
--- branches/EC_DT_RTAS/TAO/orbsvcs/orbsvcs/Event/ec_dsui_vars.cpp
+++ branches/EC_DT_RTAS/TAO/orbsvcs/orbsvcs/Event/ec_dsui_vars.cpp
@@ -1,4 +1,5 @@
-// $Id$
+$Id$
+
 #include <datastream_common.h>
 #include <dsui_types.h>
 #include "ec_dsui_families.h"
@@ -7,16 +8,16 @@ counter_t EC2_GROUP_COUNTERS[NUM_COUNTERS_EC2_GROUP] = {
 histogram_t EC2_GROUP_HISTOGRAMS[NUM_HISTOGRAMS_EC2_GROUP] = {
 };
 family_t EC2_GROUP = {
-	"EC2_GROUP",
-	NUM_EVENTS_EC2_GROUP,
-	NUM_COUNTERS_EC2_GROUP,
-	NUM_HISTOGRAMS_EC2_GROUP,
-	EC2_GROUP_COUNTERS,
-	EC2_GROUP_HISTOGRAMS,
-	0, 
-	0, 
-	0, 
-	0
+        "EC2_GROUP",
+        NUM_EVENTS_EC2_GROUP,
+        NUM_COUNTERS_EC2_GROUP,
+        NUM_HISTOGRAMS_EC2_GROUP,
+        EC2_GROUP_COUNTERS,
+        EC2_GROUP_HISTOGRAMS,
+        0,
+        0,
+        0,
+        0
 };
 
 counter_t TIMEOUT_COUNTERS[NUM_COUNTERS_TIMEOUT] = {
@@ -24,32 +25,32 @@ counter_t TIMEOUT_COUNTERS[NUM_COUNTERS_TIMEOUT] = {
 histogram_t TIMEOUT_HISTOGRAMS[NUM_HISTOGRAMS_TIMEOUT] = {
 };
 family_t TIMEOUT = {
-	"TIMEOUT",
-	NUM_EVENTS_TIMEOUT,
-	NUM_COUNTERS_TIMEOUT,
-	NUM_HISTOGRAMS_TIMEOUT,
-	TIMEOUT_COUNTERS,
-	TIMEOUT_HISTOGRAMS,
-	0, 
-	0, 
-	0, 
-	0
+        "TIMEOUT",
+        NUM_EVENTS_TIMEOUT,
+        NUM_COUNTERS_TIMEOUT,
+        NUM_HISTOGRAMS_TIMEOUT,
+        TIMEOUT_COUNTERS,
+        TIMEOUT_HISTOGRAMS,
+        0,
+        0,
+        0,
+        0
 };
 
 int ec_table_offset= 2;
-family_t *ec_family_table[ec_NUM_FAMILIES] = {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	&EC2_GROUP,
-	&TIMEOUT,
+family_t *ec_family_table[NUM_FAMILIES] = {
+
+
+
+
+
+
+
+
+
+
+
+        &EC2_GROUP,
+        &TIMEOUT,
 };
 
