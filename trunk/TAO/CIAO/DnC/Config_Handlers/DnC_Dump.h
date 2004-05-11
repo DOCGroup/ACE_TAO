bbe36216cc2f3e0d4510b680d02f0153ab209839
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-11 03:42:16 +0000

    Mon May 10 22:40:37 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

---
+++ trunk/TAO/CIAO/DnC/Config_Handlers/DnC_Dump.h
@@ -0,0 +1,80 @@
+//==================================================================
+/**
+ *  @file  DnC_Dump.h
+ *
+ *  $Id$
+ *
+ *  @author Gan Deng <gan.deng@vanderbilt.edu>
+ */
+//=====================================================================
+
+#ifndef DNC_DUMP_H
+#define DNC_DUMP_H
+#include /**/ "ace/pre.h"
+
+#include "DeploymentC.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+
+namespace Deployment
+{
+  /**
+   * @class DnC_Dump
+   *
+   * @brief This class defines a set of overloaded "dump" methods to print
+   * the contents of various IDL data types, which passed as input.
+   */  
+  class DnC_Dump 
+  {
+  public:
+    /// A whole slew of overloaded dump routines for different IDL data types.
+
+    static void dump (const ::Deployment::AssemblyConnectionDescription &acd);
+
+    static void dump (const ::Deployment::AssemblyPropertyMapping &apm);
+
+    static void dump (const ::Deployment::ComponentPackageDescription &comppkgdesc);
+
+    static void dump (const ::Deployment::ComponentPortDescription &compportdesc);
+
+    static void dump (const ::Deployment::ComponentPropertyDescription &comppropdesc);
+
+    static void dump (const ::Deployment::MonolithicImplementationDescription &mid);
+
+    static void dump (const ::Deployment::PackageConfiguration &pc);
+
+    static void dump (const ::Deployment::PackagedComponentImplementation &pci);
+
+    static void dump (const ::Deployment::SubcomponentPortEndpoint &spe);
+
+    static void dump (const ::Deployment::Requirement &requirement);
+
+    static void dump (const ::Deployment::ComponentExternalPortEndpoint &cepe);
+
+    static void dump (const ::Deployment::ComponentPackageReference &cpr);
+
+    static void dump (const ::Deployment::ComponentImplementationDescription &cid);
+
+    static void dump (const ::Deployment::SubcomponentInstantiationDescription &sid);
+
+    static void dump (const ::Deployment::Property &property);
+
+    static void dump (const ::Deployment::NamedImplementationArtifact &named_implementation);
+
+    static void dump (const ::Deployment::ComponentInterfaceDescription &cid);
+
+    static void dump (const ::Deployment::SubcomponentPropertyReference &scpr);
+
+    static void dump (const ::Deployment::ExternalReferenceEndpoint &ere);
+
+    ///   ... Zillions of such things ....
+
+   };
+}
+
+#include /**/ "ace/post.h"
+
+#endif /* DNC_DUMP_H */
