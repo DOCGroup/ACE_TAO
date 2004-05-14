ced98bd8e5c98b356a3bc28b6e662d235552fbd0
arvindk <arvindk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-14 17:22:50 +0000

    Fri May 14 12:17:55 2004  Arvind S. Krishna  <arvindk@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DnC/Config_Handlers/Requirement_Handler.cpp
+++ trunk/TAO/CIAO/DnC/Config_Handlers/Requirement_Handler.cpp
@@ -15,62 +15,97 @@ CIAO::Config_Handler::Requirement_Handler::process_Requirement (DOMNodeIterator 
   for (DOMNode * node = iter->nextNode (); 
        node != 0; 
        node = iter->nextNode ())
-  {
-    XStr name (node->getNodeName ());
-    if (name == XStr (ACE_TEXT ("resourceType")))
     {
-      valid_resourceType = 1;
-      // Populate the structure
-      ret_struct.resourceType = 
-        CIAO::Config_Handler::Utils::parse_string (iter);
-    }
-    else if (name == XStr (ACE_TEXT ("property")))
-    {
-      Deployment::Properties_var properties = 0;
-      ACE_NEW_THROW_EX (properties,
-                        Deployment::Properties,
-                        CORBA::NO_MEMORY ());
-      properties->length (0);
-      for (node = iter->nextNode ();
-           name == XStr (ACE_TEXT ("property"));
-           iter->nextNode ())
-      {
-        ::CORBA::ULong index = properties->length ();
-        // Increment length of sequence
-        properties->length (properties->length () + 1);
+      XStr name (node->getNodeName ());
+      if (name == XStr (ACE_TEXT ("resourceType")))
+        {
+          valid_resourceType = 1;
+          // Populate the structure
+          ret_struct.resourceType = 
+            CIAO::Config_Handler::Utils::parse_string (iter);
+        }
+      else if (name == XStr (ACE_TEXT ("property")))
+        {
+          Deployment::Properties_var properties = 0;
+          ACE_NEW_THROW_EX (properties,
+                            Deployment::Properties,
+                            CORBA::NO_MEMORY ());
+          properties->length (0);
 
-        // Deep copy the value
-        CIAO::Config_Handler::Property_Handler::
-          process_Property (iter, properties [index]);
-      }
-      valid_property = 1;
+          for (node = iter->nextNode ();
+               name == XStr (ACE_TEXT ("property"));
+               iter->nextNode ())
+            {
+              // Increment length of sequence
+              ::CORBA::ULong index = properties->length ();
+              properties->length (properties->length () + 1);
+              DOMNodeIterator * property_iter = 0;
 
-      // On exit go one step back to faciliate parsing next tag
-      iter->previousNode ();
+              // Check for <href> tags
+              if (node->hasAttributes ())
+                {
+                  xercesc::DOMNamedNodeMap * named_node_map =
+                    node->getAttributes ();
+                  int length = named_node_map->getLength ();
 
-      // Copy sequence on to return struct
-      ret_struct.property = properties;
-    }
-    else if (name == XStr (ACE_TEXT ("name")))
-    {
-      valid_name = 1;
-      ret_struct.name = CIAO::Config_Handler::Utils::parse_string (iter);
-    }
-    else 
-    {
-      if (! valid_resourceType || 
-          ! valid_property     ||
-          ! valid_name)
-      {
-        ACE_DEBUG ((LM_DEBUG,
-                    "Config_Handlers::Requirement_Handler::process_Requirement \
-                     element mismatch expected <resourceType>, <property> or <name>"));
-        ACE_THROW (CORBA::INTERNAL ());
-      }
+                  if (length > 1)
+                    {
+                      for (int j = 0; j < length; j++)
+                        {
+                          DOMNode * attr_node = named_node_map->item (j);
+                          XStr attr_node_name = attr_node->getNodeName ();
+                          ACE_TString attr_node_value =
+                            XMLString::transcode (attr_node->getNodeValue ());
+                          if (attr_node_name = XStr (ACE_TEXT ("href")))
+                            {
+                              XMLURL url (attr_node_value.c_str ());
+                              DOMNodeIterator * value_iter =
+                                Utils::parse_href_tag (url, 
+                                                       node->getOwnerDocument ());
+                              // Get to the root-node
+                              value_iter->nextNode ();
 
-      // On exit go one step back as we have read one too many
-      iter->previousNode ();
-      break;
+                              // Process the property href tag
+                              property_iter = value_iter;
+                            }
+                        }
+                    }
+                  else
+                    property_iter = iter;
+
+                  // Deep copy the value
+                  CIAO::Config_Handler::Property_Handler::
+                    process_Property (property_iter, properties [index]);
+                }  
+            }
+          valid_property = 1;
+
+          // On exit go one step back to faciliate parsing next tag
+          iter->previousNode ();
+
+          // Copy sequence on to return struct
+          ret_struct.property = properties;
+        }
+      else if (name == XStr (ACE_TEXT ("name")))
+        {
+          valid_name = 1;
+          ret_struct.name = CIAO::Config_Handler::Utils::parse_string (iter);
+        }
+      else 
+        {
+          if (! valid_resourceType || 
+              ! valid_property     ||
+              ! valid_name)
+            {
+              ACE_DEBUG ((LM_DEBUG,
+                          "Config_Handlers::Requirement_Handler::process_\
+                          Requirement element mismatch expected <resourceType>\
+                          , <property> or <name>"));
+              ACE_THROW (CORBA::INTERNAL ());
+            }
+          // On exit go one step back as we have read one too many
+          iter->previousNode ();
+          break;
+        }
     }
-  }
 }
--- trunk/TAO/CIAO/DnC/Config_Handlers/SP_Handler.cpp
+++ trunk/TAO/CIAO/DnC/Config_Handlers/SP_Handler.cpp
@@ -20,27 +20,57 @@ process_SatisfierProperty (DOMNodeIterator * iter,
       XStr name (node->getNodeName ());
 
       if (name == XStr (ACE_TEXT ("name")))
-        // Populate name field in the Structure
         property.name = Utils::parse_string (iter);
-      else
-        if (name == XStr (ACE_TEXT ("Deployment:SatisfierPropertyKind")))
-            // Populate the Satisfier property kind value
-          SPK_Handler::process_SatisfierPropertyKind (iter, property.kind);
-        else
-          if (name == XStr (ACE_TEXT ("Deployment:Any")))
+      else if (name == XStr (ACE_TEXT ("kind")))
+        SPK_Handler::process_SatisfierPropertyKind (iter, property.kind);
+      else if (name == XStr (ACE_TEXT ("value")))
+        {
+          // Check for <href> tags
+          if (node->hasAttributes ())
+            {
+              xercesc::DOMNamedNodeMap * named_node_map =
+                node->getAttributes ();
+              int length = named_node_map->getLength ();
+              if (length > 1)
+                {
+                  for (int j = 0; j < length; j++)
+                    {
+                      DOMNode * attr_node = named_node_map->item (j);
+                      XStr attr_node_name = attr_node->getNodeName ();
+                      ACE_TString attr_node_value =
+                        XMLString::transcode (attr_node->getNodeValue ());
+                      if (attr_node_name = XStr (ACE_TEXT ("href")))
+                        {
+                          XMLURL url (attr_node_value.c_str ());
+                          DOMNodeIterator * value_iter =
+                            Utils::parse_href_tag (url, 
+                                                   node->getOwnerDocument ());
+
+                          // Get to the root-node
+                          value_iter->nextNode ();
+
+                          // Process the property href tag
+                          Any_Handler::process_Any (value_iter, 
+                                                    property.value);
+                        }
+                    }
+                }
+            }
+          else
             // Populate value for the Property
             Any_Handler::process_Any (iter, property.value);
-          else
-            {
-              ACE_DEBUG ((LM_DEBUG, "Config_Handlers::SP_Handler::process_ \
-                         RequirementSatisfier element mismatch expected <name> \
-                         or <PropertyKind> <Deployment:Any>"));
+        }
+      else
+        {
+          ACE_DEBUG ((LM_DEBUG, "Config_Handlers::SP_Handler::process_ \
+                      RequirementSatisfier element mismatch expected <name> \
+                      or <PropertyKind> <Deployment:Any>"));
 
-              // Get the previous iterator
-              iter->previousNode ();
+          // Get the previous iterator
+          iter->previousNode ();
 
-              // Exit from the for loop
-              break;
-            }
+          // Exit from the for loop
+          break;
+        }
     }
 }
