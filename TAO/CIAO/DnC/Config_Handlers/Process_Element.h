1faf50386e661fdd957374b2f408345a3259b7e5
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-18 22:26:59 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/Process_Element.h
+++ TAO/CIAO/DnC/Config_Handlers/Process_Element.h
@@ -12,22 +12,58 @@
 #ifndef PROCESS_ELEMENT_H
 #define PROCESS_ELEMENT_H
 
+#include "DeploymentC.h"
+#include "Config_Handler_export.h"
+#include "ace/SString.h"
 #include "ace/Hash_Map_Manager.h"
 #include "ace/Null_Mutex.h"
-#include <xercesc/dom/DOM.hpp>
 
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/Auto_Ptr.h"
+#include "ace/Log_Msg.h"
+#include "ace/OS_main.h"
+#include "tao/Exception.h"
+#include "XercesString.h"
+#include <xercesc/util/XMLUniDefs.hpp>
+#include <xercesc/util/XercesDefs.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/util/XMLURL.hpp>
+#include <xercesc/util/XMLUri.hpp>
+
+using Config_Handler::XStr;
+using xercesc::XMLUni;
+using xercesc::XMLUri;
+using xercesc::XMLURL;
+using xercesc::XMLString;
+using xercesc::XMLException;
+using xercesc::DOMException;
+using xercesc::DOMBuilder;
+using xercesc::DOMImplementationRegistry;
+using xercesc::DOMImplementationLS;
+using xercesc::DOMImplementation;
+using xercesc::DOMInputSource;
+using xercesc::DOMText;
+using xercesc::DOMNamedNodeMap;
+using xercesc::DOMLocator;
+using xercesc::DOMError;
 using xercesc::DOMDocument;
-using xercesc::DOMNode;
+using xercesc::DOMDocumentTraversal;
 using xercesc::DOMNodeIterator;
+using xercesc::DOMNode;
+using xercesc::DOMNodeFilter;
+using xercesc::DOMNamedNodeMap;
 
 // ID map type definition
-typedef ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex> REF_MAP;
+typedef ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex> REFMAP;
 
 
-template <typename OBJ>
+template <typename OBJ, typename DATA>
 class Process_Function {
 public:
-  virtual void call(DOMNodeIterator* iter, DATA& data) = 0;
+  virtual void call(DOMNodeIterator*, DATA&) { }
 
   void operator() (DOMNodeIterator* iter, DATA& data)
   {
@@ -39,22 +75,22 @@ public:
  *  Wrapper class for the process member functions.
  */
 
-template <typename OBJ>
-class Process_Member_Function: public Process_Function<OBJ> {
+template <typename OBJ, typename DATA>
+class Process_Member_Function: public Process_Function<OBJ, DATA> {
 public:
   typedef void (OBJ::*func_type) (DOMDocument*, DOMNodeIterator*, DATA&);
 
-  Process_Function(OBJ& obj, func_type f, DOMDocument* doc)
+  Process_Member_Function(OBJ& obj, func_type f, DOMDocument* doc)
     : obj_(&obj), f_(f), doc_(doc)
   {
   }
 
-  Process_Function(OBJ* obj, func_type f, DOMDocument* doc)
+  Process_Member_Function(OBJ* obj, func_type f, DOMDocument* doc)
     : obj_(obj), f_(f), doc_(doc)
   {
   }
 
-  Process_Function& operator=(func_type ft)
+  Process_Member_Function& operator=(func_type ft)
   {
     f_ = f;
   }
@@ -79,21 +115,22 @@ private:
  *  Wrapper class for the static process member functions.
  */
 
-class Process_Static_Function: public Process_Function<OBJ> {
+template <typename OBJ, typename DATA>
+class Process_Static_Function: public Process_Function<OBJ, DATA> {
 public:
   typedef void (*func_type) (DOMNodeIterator*, DATA&);
 
-  Process_Function(func_type f)
+  Process_Static_Function(func_type f)
     : f_(f)
   {
   }
 
-  Process_Function(func_type f)
+  Process_Static_Function(func_type f)
     : f_(f)
   {
   }
 
-  Process_Function& operator=(func_type ft)
+  Process_Static_Function& operator=(func_type ft)
   {
     f_ = f;
   }
@@ -118,22 +155,22 @@ DOMDocument* create_document (const char *url);
 
 // This function only works for calling static process_ methods
 template <typename DATA, typename VALUE, typename OBJECT>
-inline void process_element (DOMNode* node,
-                             DOMDocument* doc,
-                             DOMNodeIterator* iter,
-                             DATA& data,
-                             VALUE val,
-                             Process_Function <OBJECT>* func,
-                             REFMAP& id_map);
+void process_element (DOMNode* node,
+                      DOMDocument* doc,
+                      DOMNodeIterator* iter,
+                      DATA& data,
+                      VALUE val,
+                      Process_Function <OBJECT, DATA> func,
+                      REFMAP& id_map);
 
 // This function only works for calling static process_ methods
 template <typename SEQUENCE, typename DATA, typename OBJECT>
-inline void process_sequential_element (DOMNode* node,
-                                        DOMDocument* doc,
-                                        DOMNodeIterator* iter,
-                                        SEQUENCE& seq,
-                                        Process_Function <OBJECT>* func,
-                                        REFMAP& id_map);
+void process_sequential_element (DOMNode* node,
+                                 DOMDocument* doc,
+                                 DOMNodeIterator* iter,
+                                 SEQUENCE& seq,
+                                 Process_Function <OBJECT, DATA> func,
+                                 REFMAP& id_map);
 
 #include "Process_Element.i"
 
