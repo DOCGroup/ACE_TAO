6860a900c09b1833b6a250aea9f229b6b8bdda16
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-18 19:59:04 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/Process_Element.h
+++ TAO/CIAO/DnC/Config_Handlers/Process_Element.h
@@ -1,3 +1,4 @@
+// -*-C++-*-
 //==================================================================
 /**
  *  @file Process_Element.h
@@ -20,75 +21,6 @@ DOMDocument* create_document (const char *url);
  *
  */
 
-template <typename VALUE, typename DATA, typename OBJECT>
-inline void process_element_attributes(DOMNamedNodeMap* named_node_map,
-                                       DOMDocument* doc,
-                                       DOMNodeIterator* iter,
-                                       VALUE value,
-                                       DATA& data,
-                                       OBJECT& obj,
-                                       void (OBJECT::*func)(DOMDocument*,
-                                                            DOMNodeIterator*,
-                                                            DATA&),
-                                       ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex>& id_map)
-{
-  // the number of attributes
-  int length = named_node_map->getLength();
-  // iterate the attributes
-  for (int j = 0; j < length; ++j)
-    {
-      DOMNode* attribute_node = named_node_map->item (j);
-      XStr strattrnodename (attribute_node->getNodeName ());
-      ACE_TString aceattrnodevalue =
-        XMLString::transcode (attribute_node->getNodeValue ());
-
-      // if xmi::id is given process the element and bind the value
-      if (strattrnodename == XStr (ACE_TEXT ("xmi:id")))
-        {
-          (obj.*func)(doc, iter, data);
-          id_map.bind (aceattrnodevalue, value);
-        }
-      // if href is given find out the referenced position
-      // and process the element
-      else if (strattrnodename == XStr (ACE_TEXT ("href")))
-        {
-          XMLURL xml_url (aceattrnodevalue.c_str ());
-          XMLURL result (aceattrnodevalue.c_str ());
-          std::string url_string = aceattrnodevalue.c_str ();
-          ACE_TString doc_path =
-            XMLString::transcode ( doc->getDocumentURI ());
-          result.makeRelativeTo
-            (XMLString::transcode (doc_path.c_str ()));
-          ACE_TString final_url =
-            XMLString::transcode (result.getURLText ());
-
-          DOMDocument* href_doc;
-
-          if (xml_url.isRelative ())
-            {
-              href_doc = create_document(final_url.c_str ());
-            }
-          else
-            {
-              href_doc = create_document (url_string.c_str ());
-            }
-
-          DOMDocumentTraversal* traverse (href_doc);
-          DOMNode* root = (href_doc->getDocumentElement ());
-          unsigned long filter = DOMNodeFilter::SHOW_ELEMENT |
-            DOMNodeFilter::SHOW_TEXT;
-          DOMNodeIterator* href_iter = traverse->createNodeIterator
-            (root,
-             filter,
-             0,
-             true);
-          href_iter->nextNode ();
-
-          (obj.*func) (href_doc, href_iter, data);
-        }
-    }
-}
-
 // This function only works for calling static process_ methods
 template <typename DATA, typename VALUE, typename OBJECT>
 inline void process_element (DOMNode* node,
@@ -100,24 +32,7 @@ inline void process_element (DOMNode* node,
                              void (OBJECT::*func)(DOMDocument*,
                                                   DOMNodeIterator*,
                                                   DATA&),
-                             ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex>& id_map)
-{
-  // fetch attributes
-  DOMNamedNodeMap* named_node_map = node->getAttributes ();
-  // the number of attributes the element have
-  int length = named_node_map->getLength();
-  // if there is no other attribute but 'version'
-  if (length == 1)
-    {
-      // call directly the static process_ method
-      (obj.*func)(doc, iter, data);
-    }
-  else if (length > 1)
-    {
-      // Check the xmi::id & href attributes
-      process_element_attributes(named_node_map, doc, iter, val, data, obj, func, id_map);
-    }
-}
+                             ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex>& id_map);
 
 // This function only works for calling static process_ methods
 template <typename SEQUENCE, typename DATA, typename OBJECT>
@@ -129,17 +44,6 @@ inline void process_sequential_element (DOMNode* node,
                                         void (OBJECT::*func)(DOMDocument*,
                                                              DOMNodeIterator*,
                                                              DATA&),
-                                        ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex>& id_map)
-{
-  if (node->hasAttributes ())
-    {
-      // the size of the sequence
-      CORBA::ULong i (seq.length ());
-      // add 1 to the size of the sequence
-      seq.length (i + 1);
-      // call process only one element
-      process_element(node, doc, iter, seq[i], i, obj, func, id_map);
-    }
-}
+                                        ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex>& id_map);
 
 #endif // PROCESS_ELEMENT_H
