45a00cbaccdeec407f29092ac5665a14c1a98e94
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-19 18:34:39 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/MID_Handler.h
+++ TAO/CIAO/DnC/Config_Handlers/MID_Handler.h
@@ -5,18 +5,30 @@
 #include /**/ "ace/pre.h"
 
 #include "DeploymentC.h"
+#include "Config_Handler_export.h"
+#include "ace/SString.h"
+#include "ace/Hash_Map_Manager.h"
+#include "ace/Null_Mutex.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "ace/SString.h"
+#include "ace/Auto_Ptr.h"
+#include "ace/Log_Msg.h"
+#include "ace/OS_main.h"
+#include "tao/Exception.h"
+#include "XercesString.h"
+#include <xercesc/util/XMLUniDefs.hpp>
 #include <xercesc/util/XercesDefs.hpp>
 #include <xercesc/dom/DOM.hpp>
-#include "XercesString.h"
+#include <xercesc/util/XMLURL.hpp>
+#include <xercesc/util/XMLUri.hpp>
 
 using Config_Handler::XStr;
 using xercesc::XMLUni;
+using xercesc::XMLUri;
+using xercesc::XMLURL;
 using xercesc::XMLString;
 using xercesc::XMLException;
 using xercesc::DOMException;
@@ -24,6 +36,7 @@ using xercesc::DOMBuilder;
 using xercesc::DOMImplementationRegistry;
 using xercesc::DOMImplementationLS;
 using xercesc::DOMImplementation;
+using xercesc::DOMInputSource;
 using xercesc::DOMText;
 using xercesc::DOMNamedNodeMap;
 using xercesc::DOMLocator;
@@ -33,6 +46,7 @@ using xercesc::DOMDocumentTraversal;
 using xercesc::DOMNodeIterator;
 using xercesc::DOMNode;
 using xercesc::DOMNodeFilter;
+using xercesc::DOMNamedNodeMap;
 
 namespace CIAO
 {
@@ -50,9 +64,46 @@ namespace CIAO
 
       void process_MonolithicImplementationDescription (::Deployment::MonolithicImplementationDescription &mid);
 
+      /// process exec parameter element
+      void process_exec_parameter_element (DOMNode* node,
+        DOMDocument* doc, DOMNodeIterator* iter,
+        Deployment::MonolithicImplementationDescription& mid);
+
+      /// process attributes for property element
+      void process_attributes_for_property (DOMNamedNodeMap* named_node_map,
+        DOMDocument* doc,
+        DOMNodeIterator* iter,
+        int value,
+        Deployment::Property& ccd_property);
+
+     /// parse a document
+      DOMDocument* create_document (const char *url);
+
+      /// process attributes for deploy requirement
+      void process_attributes_for_deploy_requirement (DOMNamedNodeMap* nm,
+        DOMDocument* doc,
+        DOMNodeIterator* iter,
+        int value,
+        Deployment::Requirement& req);
+
+      /// process attributes for NIA
+      void process_attributes_for_nia (DOMNamedNodeMap* nm,
+        DOMDocument* doc,
+        DOMNodeIterator* iter,
+        int value,
+        Deployment::NamedImplementationArtifact& nia);
+
+      /// process IDREFS
+      void process_refs (DOMNamedNodeMap* named_node_map);
+
     private:
 
+      typedef ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex> REF_MAP;
+      typedef ACE_Hash_Map_Iterator<ACE_TString, int, ACE_Null_Mutex> REF_ITER;
+      typedef ACE_Hash_Map_Manager<int, ACE_TString, ACE_Null_Mutex> IDREF_MAP;
       DOMDocumentTraversal* traverse_;
+
+      DOMDocument* doc_;
 
       DOMNode* root_;
 
@@ -61,6 +112,9 @@ namespace CIAO
       DOMNodeIterator* iter_;
 
       bool release_;
+      int index_;
+      REF_MAP id_map_;
+      IDREF_MAP idref_map_;
 
     };
   }
