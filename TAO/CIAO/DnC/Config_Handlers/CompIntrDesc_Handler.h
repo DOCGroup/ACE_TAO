8aaefd5cb3957f44be4b7003d95c88152f6a0452
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-19 03:40:02 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/CompIntrDesc_Handler.h
+++ TAO/CIAO/DnC/Config_Handlers/CompIntrDesc_Handler.h
@@ -14,18 +14,29 @@
 
 #include "DeploymentC.h"
 #include "Config_Handler_export.h"
+#include "ace/SString.h"
+#include "ace/Hash_Map_Manager.h"
+#include "ace/Null_Mutex.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "ace/SString.h"
+#include "ace/Auto_Ptr.h"
+#include "ace/Log_Msg.h"
+#include "ace/OS_main.h"
+#include "tao/Exception.h"
+#include "XercesString.h"
+#include <xercesc/util/XMLUniDefs.hpp>
 #include <xercesc/util/XercesDefs.hpp>
 #include <xercesc/dom/DOM.hpp>
-#include "XercesString.h"
+#include <xercesc/util/XMLURL.hpp>
+#include <xercesc/util/XMLUri.hpp>
 
 using Config_Handler::XStr;
 using xercesc::XMLUni;
+using xercesc::XMLUri;
+using xercesc::XMLURL;
 using xercesc::XMLString;
 using xercesc::XMLException;
 using xercesc::DOMException;
@@ -33,6 +44,7 @@ using xercesc::DOMBuilder;
 using xercesc::DOMImplementationRegistry;
 using xercesc::DOMImplementationLS;
 using xercesc::DOMImplementation;
+using xercesc::DOMInputSource;
 using xercesc::DOMText;
 using xercesc::DOMNamedNodeMap;
 using xercesc::DOMLocator;
@@ -42,6 +54,7 @@ using xercesc::DOMDocumentTraversal;
 using xercesc::DOMNodeIterator;
 using xercesc::DOMNode;
 using xercesc::DOMNodeFilter;
+using xercesc::DOMNamedNodeMap;
 
 namespace CIAO
 {
@@ -66,18 +79,46 @@ namespace CIAO
 
     protected:
       /// Process the label attribute
-      void process_label (const XMLCh* label, ::Deployment::ComponentInterfaceDescription &compintrdesc);
+      void process_label (const XMLCh* label,
+        Deployment::ComponentInterfaceDescription &compintrdesc);
 
       /// Process the UUID attribute
-      void process_UUID (const XMLCh* UUID, ::Deployment::ComponentInterfaceDescription &compintrdesc);
+      void process_UUID (const XMLCh* UUID,
+        Deployment::ComponentInterfaceDescription &compintrdesc);
 
       /// Process the specificType attribute
-      void process_specificType (const XMLCh* specificType, ::Deployment::ComponentInterfaceDescription &compintrdesc);
+      void process_specificType (const XMLCh* specificType,
+        Deployment::ComponentInterfaceDescription &compintrdesc);
 
       /// Process the supportedType attribute
-      void process_supportedType (const XMLCh* supportedType, ::Deployment::ComponentInterfaceDescription &compintrdesc);
+      void process_supportedType (const XMLCh* supportedType,
+        Deployment::ComponentInterfaceDescription &compintrdesc);
+
+      /// process config property element
+      void process_config_property_element (DOMNode* node,
+        DOMDocument* doc, DOMNodeIterator* iter,
+        Deployment::ComponentInterfaceDescription& cid);
+
+      /// process info property element
+      void process_info_property_element (DOMNode* node,
+        DOMDocument* doc, DOMNodeIterator* iter,
+        Deployment::ComponentInterfaceDescription& cid);
+
+      /// process attributes for property
+      void process_attributes_for_property (DOMNamedNodeMap* named_node_map,
+        DOMDocument* doc,
+        DOMNodeIterator* iter,
+        int value,
+        Deployment::Property& ccd_property);
+
+      /// create a XML document
+      DOMDocument* create_document (const char *url);
 
     private:
+
+      typedef ACE_Hash_Map_Manager<ACE_TString, int, ACE_Null_Mutex> REF_MAP;
+      typedef ACE_Hash_Map_Iterator<ACE_TString, int, ACE_Null_Mutex> REF_ITER;
+      typedef ACE_Hash_Map_Manager<int, ACE_TString, ACE_Null_Mutex> IDREF_MAP;
 
       DOMDocument* doc_;
 
@@ -88,6 +129,10 @@ namespace CIAO
       DOMNodeIterator* iter_;
 
       bool release_;
+      int index_;
+
+      REF_MAP id_map_;
+      IDREF_MAP idref_map_;
 
     };
 
