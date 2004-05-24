8f8997175963b277d88ab0599b13bd5a0d46209d
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-24 19:30:57 +0000

    Mon May 24 14:13:51 2004  Gan Deng  <gan.deng@vanderbilt.edu>

--- TAO/CIAO/DnC/DomainApplication/DomainApplication_Impl.cpp
+++ TAO/CIAO/DnC/DomainApplication/DomainApplication_Impl.cpp
@@ -1,21 +1,17 @@
 // $Id$
 
-#include "DomainApplicationManager_Impl.h"
-
-#if !defined (__ACE_INLINE__)
-# include "DomainApplicationManager_Impl.inl"
-#endif /* __ACE_INLINE__ */
+#include "DomainApplication_Impl.h"
 
 //===============================================================
-//============   DomainApplicationManager_Impl  =================
+//============   DomainApplication_Impl  =================
 //===============================================================
 
-CIAO::DomainApplicationManager_Impl::DomainApplicationManager_Impl ()
+CIAO::DomainApplication_Impl::DomainApplication_Impl ()
 {
   // @@ 
 }
 
-CIAO::DomainApplicationManager_Impl::~DomainApplicationManager_Impl ()
+CIAO::DomainApplication_Impl::~DomainApplication_Impl ()
 {
   // @@ remove all NodeApplication's?
 }
@@ -23,23 +19,18 @@ CIAO::DomainApplicationManager_Impl::~DomainApplicationManager_Impl ()
 
 
 
-virtual ::Deployment::Application_ptr 
-CIAO::DomainApplicationManager_Impl::
-startLaunch (const Deployment::Properties & configProperty,
-             Deployment::Connections_out providedReference,
-             CORBA::Boolean start
-             ACE_ENV_ARG_DECL_WITH_DEFAULTS)
+virtual void 
+CIAO::DomainApplication_Impl::
+finishLaunch (const Deployment::Connections & providedReference,
+              CORBA::Boolean start
+              ACE_ENV_ARG_DECL_WITH_DEFAULTS)
   ACE_THROW_SPEC ((CORBA::SystemException,
-                   Deployment::ResourceNotAvailable,
                    Deployment::StartError,
-                   Deployment::InvalidProperty))
+                   Deployment::InvalidConnection))
 {
   /**
-   *  1. First Map properties to TAO/CIAO specific property/configurations
-   *  2. Necessary property checking (needed?)
-   *  3. Call create_nade_application to spawn new process.
-   *  4. Initialize the NodeApplication.
-   *  5. get the provided connection endpoints back and return them.
+   *  1. Call finishLaunch operation on each individual NodeApplication.
+   *  2. ...
    */
   ACE_TRY
     {
@@ -56,23 +47,11 @@ startLaunch (const Deployment::Properties & configProperty,
 }
 
 
-virtual void
-CIAO::DomainApplicationManager_Impl::
-destroyApplication (Deployment::Application_ptr app
-		                ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-  ACE_THROW_SPEC ((CORBA::SystemException, 
-                   Deployment::StopError))
+virtual void 
+CIAO::DomainApplication_Impl::
+start (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+  ACE_THROW_SPEC ((CORBA::SystemException, Deployment::StartError))
 {
-  ACE_GUARD (TAO_SYNCH_MUTEX, ace_mon, this->lock_);
 
-  if (this->cs_set_.object_in_set (app) == 0)
-    ACE_THROW (Deployment::StopError ());
-
-  app->remove (ACE_ENV_SINGLE_ARG_PARAMETER);
-  ACE_CHECK;
-
-  // Should we remove the server still even if the previous call failed.
-
-  if (this->cs_set_.remove (app) == -1)
-    ACE_THROW (Deployment::StopError ());
 }
+
