1714330a9815454faa45dce18472da018080b02f
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-25 16:05:38 +0000

    *** empty log message ***

--- trunk/TAO/CIAO/DnC/DomainApplicationManager/DomainApplicationManager_Impl.cpp
+++ trunk/TAO/CIAO/DnC/DomainApplicationManager/DomainApplicationManager_Impl.cpp
@@ -2,19 +2,17 @@
 
 #include "DomainApplicationManager_Impl.h"
 
-#if !defined (__ACE_INLINE__)
-# include "DomainApplicationManager_Impl.inl"
-#endif /* __ACE_INLINE__ */
-
 //===============================================================
 //============   DomainApplicationManager_Impl  =================
 //===============================================================
 
 CIAO::DomainApplicationManager_Impl::
 DomainApplicationManager_Impl (CORBA::ORB_ptr orb,
-  	                           PortableServer::POA_ptr poa)
-                               : orb_ (orb),
-                                 poa_ (poa)
+  	                           PortableServer::POA_ptr poa,
+                               Deployment::TargetManager_ptr manager)
+  : orb_ (CORBA::ORB::_duplicate (o)),
+    poa_ (PortableServer::POA::_duplicate (poa)),
+    target_manager_ (manager)
 {
   // @@ 
 }
@@ -27,15 +25,35 @@ CIAO::DomainApplicationManager_Impl::~DomainApplicationManager_Impl ()
 
 int
 CIAO::DomainApplicationManager_Impl::
-init (const Deployment::DeploymentPlan & plan
+init (const ::Deployment::DeploymentPlan & plan
       ACE_ENV_ARG_DECL_WITH_DEFAULTS)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  this->plan_ = plan;
+  //this->plan_ = plan;
 }
 
 
-virtual ::Deployment::Application_ptr 
+void 
+CIAO::DomainApplicationManager_Impl::
+split_plan (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  // Get the # of node-level plans inside the global plan.
+  int num = (this->plan_.instance)->length ();
+  ACE_NEW (this->child_plan_, ::Deployment::DeploymentPlan[num]);
+
+  // (1) Iterate over the <instance> field of the global DeploymentPlan 
+  //     variabl.
+  // (2) 
+
+
+}
+
+
+
+
+
+::Deployment::Application_ptr 
 CIAO::DomainApplicationManager_Impl::
 startLaunch (const Deployment::Properties & configProperty,
              Deployment::Connections_out providedReference,
@@ -64,11 +82,11 @@ startLaunch (const Deployment::Properties & configProperty,
 
   ACE_CHECK_RETURN (0);
 
-  return;
+  return 0;
 }
 
 
-virtual void
+void
 CIAO::DomainApplicationManager_Impl::
 destroyApplication (Deployment::Application_ptr app
 		                ACE_ENV_ARG_DECL_WITH_DEFAULTS)
