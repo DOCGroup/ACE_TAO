4e81e0ffd3e3745a76b6285b0ee03a3371157431
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-27 14:00:13 +0000

    *** empty log message ***

---
+++ TAO/CIAO/DnC/tests/Config_Handlers/CompIntrDesc_Handler_Test.cpp
@@ -0,0 +1,11 @@
+// $Id$
+
+#include "Handler_Test.h"
+#include "Config_Handlers/CompIntrDesc_Handler.h"
+
+int
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+{
+  return run_test <CIAO::Config_Handler::CompIntrDesc_Handler, Deployment::ComponentInterfaceDescription>
+    (argc, argv, &CIAO::Config_Handler::CompIntrDesc_Handler::process_ComponentInterfaceDescription);
+}
---
+++ TAO/CIAO/DnC/tests/Config_Handlers/Handler_Test.h
@@ -0,0 +1,42 @@
+// $Id$
+
+#ifndef HANDLER_TEST_H
+#define HANDLER_TEST_H
+
+#include "ace/Get_Opt.h"
+#include "ace/Auto_Ptr.h"
+#include "ace/Log_Msg.h"
+#include "ace/OS_main.h"
+#include "tao/Exception.h"
+#include "Config_Handlers/XercesString.h"
+#include <xercesc/util/XMLUniDefs.hpp>
+#include "Config_Handlers/Config_Handler_export.h"
+#include "Config_Handlers/DnC_Dump.h"
+#include "Config_Handlers/Config_Error_Handler.h"
+
+using Config_Handler::XStr;
+using xercesc::XMLUni;
+using xercesc::XMLString;
+using xercesc::XMLException;
+using xercesc::DOMException;
+using xercesc::DOMBuilder;
+using xercesc::DOMImplementationRegistry;
+using xercesc::DOMImplementationLS;
+using xercesc::DOMImplementation;
+using xercesc::DOMAttr;
+using xercesc::DOMNamedNodeMap;
+using xercesc::DOMLocator;
+using xercesc::DOMError;
+using xercesc::DOMNodeList;
+using xercesc::DOMDocument;
+using xercesc::DOMDocumentTraversal;
+using xercesc::DOMNodeIterator;
+using xercesc::DOMNode;
+using xercesc::DOMNodeFilter;
+
+template <typename HANDLER, typename DATA>
+int run_test (int argc, ACE_TCHAR *argv[], void (HANDLER::*func) (DATA&));
+
+#include "Handler_Test.i"
+
+#endif // HANDLER_TEST_H
