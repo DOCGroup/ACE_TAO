d0a1500ee63787ab6d3699e663151c7ed169326a
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-27 14:00:13 +0000

    *** empty log message ***

---
+++ trunk/TAO/CIAO/DnC/tests/Config_Handlers/CompPkgDesc_Handler_Test.cpp
@@ -0,0 +1,11 @@
+// $Id$
+
+#include "Handler_Test.h"
+#include "Config_Handlers/CompPkgDesc_Handler.h"
+
+int
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+{
+  return run_test <CIAO::Config_Handler::CompPkgDesc_Handler, Deployment::ComponentPackageDescription>
+    (argc, argv, &CIAO::Config_Handler::CompPkgDesc_Handler::process_ComponentPackageDescription);
+}
---
+++ trunk/TAO/CIAO/DnC/tests/Config_Handlers/PC_Handler_Test.cpp
@@ -0,0 +1,11 @@
+// $Id$
+
+#include "Handler_Test.h"
+#include "Config_Handlers/PC_Handler.h"
+
+int
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+{
+  return run_test <CIAO::Config_Handler::PC_Handler, Deployment::PackageConfiguration>
+    (argc, argv, &CIAO::Config_Handler::PC_Handler::process_PackageConfiguration);
+}
