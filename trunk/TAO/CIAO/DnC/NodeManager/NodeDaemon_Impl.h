35c50d3a75106381c5d941bd35cf5144a1faedac
arvindk <arvindk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-05-29 04:25:35 +0000

    Fri May 28 23:17:44 2004  Arvind S. Krishna  <arvindk@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DnC/NodeManager/NodeDaemon_Impl.h
+++ trunk/TAO/CIAO/DnC/NodeManager/NodeDaemon_Impl.h
@@ -88,9 +88,9 @@ namespace CIAO
                        Deployment::StopError));
 
   protected:
-    typedef ACE_Hash_Map_Manager_Ex<const char *, 
-                                    CORBA::Object_ptr, 
-                                    ACE_Hash<const char *>, ACE_Equal_To<const char *>, 
+    typedef ACE_Hash_Map_Manager_Ex<const char *,
+                                    CORBA::Object_ptr,
+                                    ACE_Hash<const char *>, ACE_Equal_To<const char *>,
                                     TAO_SYNCH_MUTEX> Table;
     typedef Table::iterator Iterator;
 
