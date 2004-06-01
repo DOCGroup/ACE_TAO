5bc007932e2f821e3b871c5e5d107057f7a4fd06
arvindk <arvindk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-01 14:41:06 +0000

    Tue Jun  1 09:39:41 2004  Arvind S. Krishna  <arvindk@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DnC/tests/NodeApplicationTest/NodeApp_test_client.cpp
+++ trunk/TAO/CIAO/DnC/tests/NodeApplicationTest/NodeApp_test_client.cpp
@@ -113,12 +113,20 @@ main (int argc, char *argv[])
         roundtrip_var->cube_long (input ACE_ENV_ARG_PARAMETER);
 
      if (input == output)
-       ACE_DEBUG ((LM_DEBUG, "Test succeeded\n"));
+       ACE_DEBUG ((LM_DEBUG, "Retrun values matched!!\n"));
      else
-       ACE_DEBUG ((LM_DEBUG, "Test failure\n"));
+     {
+       ACE_DEBUG ((LM_DEBUG, "Return values did not match: failure\n"));
+       exit (1);
+     }
+
+     ACE_DEBUG ((LM_DEBUG, "Try removing home\n"));
+     comserv->remove_home (info.component_instance_name); 
+     ACE_DEBUG ((LM_DEBUG, "Home removed successfully\n"));
 
       orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
       ACE_TRY_CHECK;
+     ACE_DEBUG ((LM_DEBUG, "Test success!!\n"));
     }
   ACE_CATCHANY
     {
