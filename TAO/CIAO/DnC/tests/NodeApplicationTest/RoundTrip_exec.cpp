2a5926743b807a0e22ea8669f5397daab9df525f
lut <lut@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-04 20:35:47 +0000

    ChangeLogTag: Fri Jun  4 15:32:50 2004  Tao Lu  <lu@dre.vanderbilt.edu>

--- TAO/CIAO/DnC/tests/NodeApplicationTest/RoundTrip_exec.cpp
+++ TAO/CIAO/DnC/tests/NodeApplicationTest/RoundTrip_exec.cpp
@@ -8,27 +8,31 @@
 
 //=================================================================
 
-MyImpl::RoundTrip_exec_i::RoundTrip_exec_i ()
+RoundTrip_Impl::RoundTrip_exec_i::RoundTrip_exec_i ()
 {
 
 }
 
-MyImpl::RoundTrip_exec_i::~RoundTrip_exec_i ()
+RoundTrip_Impl::RoundTrip_exec_i::~RoundTrip_exec_i ()
 {
 }
 
-/*
-::NodeAppTest::CCM_LatencyTest *
-MyImpl::RoundTrip_exec_i::get_latency (ACE_ENV_SINGLE_ARG_DECL)
+NodeAppTest::CCM_LatencyTest_ptr
+RoundTrip_Impl::RoundTrip_exec_i::get_facet_1(ACE_ENV_SINGLE_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  return ::NodeAppTest::CCM_LatencyTest::_duplicate (this);
+  return new RoundTrip_Impl::LatencyTest_Impl ();
 }
-*/
 
-/* Main method that invokes the octet_sequence */
+NodeAppTest::CCM_LatencyTest_ptr
+RoundTrip_Impl::RoundTrip_exec_i::get_facet_2(ACE_ENV_SINGLE_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return new RoundTrip_Impl::LatencyTest_Impl ();
+}
+
 CORBA::Long
-MyImpl::RoundTrip_exec_i::cube_long (CORBA::Long data)
+RoundTrip_Impl::RoundTrip_exec_i::cube_long (CORBA::Long data)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   return data*data*data;
@@ -36,80 +40,87 @@ MyImpl::RoundTrip_exec_i::cube_long (CORBA::Long data)
 
 // Operations from Components::SessionComponent
 void
-MyImpl::RoundTrip_exec_i::set_session_context (Components::SessionContext_ptr 
+RoundTrip_Impl::RoundTrip_exec_i::set_session_context (Components::SessionContext_ptr
                                                 ACE_ENV_ARG_DECL_WITH_DEFAULTS)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
   if (CIAO::debug_level () > 0)
-    ACE_DEBUG ((LM_DEBUG, "MyImpl::RoundTrip_exec_i::set_session_context\n"));
+    ACE_DEBUG ((LM_DEBUG, "RoundTrip_Impl::RoundTrip_exec_i::set_session_context\n"));
   ACE_CHECK;
 
 }
 
 void
-MyImpl::RoundTrip_exec_i::ccm_activate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+RoundTrip_Impl::RoundTrip_exec_i::ccm_activate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
   if (CIAO::debug_level () > 0)
-    ACE_DEBUG ((LM_DEBUG, "MyImpl::RoundTrip_exec_i::ccm_activate\n"));
+    ACE_DEBUG ((LM_DEBUG, "RoundTrip_Impl::RoundTrip_exec_i::ccm_activate\n"));
 
 }
 
 void
-MyImpl::RoundTrip_exec_i::ccm_passivate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+RoundTrip_Impl::RoundTrip_exec_i::ccm_passivate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
   if (CIAO::debug_level () > 0)
-    ACE_DEBUG ((LM_DEBUG, "MyImpl::RoundTrip_exec_i::ccm_passivate\n"));
+    ACE_DEBUG ((LM_DEBUG, "RoundTrip_Impl::RoundTrip_exec_i::ccm_passivate\n"));
 }
 
 void
-MyImpl::RoundTrip_exec_i::ccm_remove (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+RoundTrip_Impl::RoundTrip_exec_i::ccm_remove (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
   if (CIAO::debug_level () > 0)
-    ACE_DEBUG ((LM_DEBUG, "MyImpl::RoundTrip_exec_i::ccm_remove\n"));
+    ACE_DEBUG ((LM_DEBUG, "RoundTrip_Impl::RoundTrip_exec_i::ccm_remove\n"));
 }
 
 void
-MyImpl::RoundTrip_exec_i::ciao_preactivate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+RoundTrip_Impl::RoundTrip_exec_i::ciao_preactivate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
   if (CIAO::debug_level () > 0)
-    ACE_DEBUG ((LM_DEBUG, "MyImpl::RoundTrip_exec_i::ciao_preactivate\n"));
+    ACE_DEBUG ((LM_DEBUG, "RoundTrip_Impl::RoundTrip_exec_i::ciao_preactivate\n"));
 
 }
 
 void
-MyImpl::RoundTrip_exec_i::ciao_postactivate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+RoundTrip_Impl::RoundTrip_exec_i::ciao_postactivate (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
   if (CIAO::debug_level () > 0)
-    ACE_DEBUG ((LM_DEBUG, "MyImpl::RoundTrip_exec_i::ciao_postactivate\n"));
+    ACE_DEBUG ((LM_DEBUG, "RoundTrip_Impl::RoundTrip_exec_i::ciao_postactivate\n"));
 }
 
-MyImpl::RoundTripHome_exec_i::RoundTripHome_exec_i ()
+RoundTrip_Impl::RoundTripHome_exec_i::RoundTripHome_exec_i ()
 {
 }
 
-MyImpl::RoundTripHome_exec_i::~RoundTripHome_exec_i ()
+RoundTrip_Impl::RoundTripHome_exec_i::~RoundTripHome_exec_i ()
 {
 }
 
 ::Components::EnterpriseComponent_ptr
-MyImpl::RoundTripHome_exec_i::create (ACE_ENV_SINGLE_ARG_DECL)
+RoundTrip_Impl::RoundTripHome_exec_i::create (ACE_ENV_SINGLE_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException,
                    Components::CCMException))
 {
-  return new MyImpl::RoundTrip_exec_i ();
+  return new RoundTrip_Impl::RoundTrip_exec_i ();
 }
 
+// Implementation for the probided inferface.
+CORBA::Long
+RoundTrip_Impl::LatencyTest_Impl::cube_long (CORBA::Long data)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  return data*data*data;
+}
 
 extern "C" NODEAPPTEST_ROUNDTRIP_EXEC_Export
 ::Components::HomeExecutorBase_ptr
@@ -118,5 +129,5 @@ createRoundTripHome_Impl (void)
   if (CIAO::debug_level () > 0)
     ACE_DEBUG ((LM_DEBUG, "Creating Round_trip impl\n"));
 
-  return new MyImpl::RoundTripHome_exec_i ();
+  return new RoundTrip_Impl::RoundTripHome_exec_i ();
 }
