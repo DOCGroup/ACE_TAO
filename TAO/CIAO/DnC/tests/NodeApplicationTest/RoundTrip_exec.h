97ceec7505b6750e36136ec1fe42f80e85bbaf97
lut <lut@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-04 20:35:47 +0000

    ChangeLogTag: Fri Jun  4 15:32:50 2004  Tao Lu  <lu@dre.vanderbilt.edu>

--- TAO/CIAO/DnC/tests/NodeApplicationTest/RoundTrip_exec.h
+++ TAO/CIAO/DnC/tests/NodeApplicationTest/RoundTrip_exec.h
@@ -13,10 +13,11 @@
 #ifndef NODEAPP_ROUNDTRIP_EXEC_H
 #define NODEAPP_ROUNDTRIP_EXEC_H
 
-#include "RoundTripEIC.h"
+#include "NodeAppTest_RoundTripEC.h"
+#include "RoundTrip_exec_export.h"
 #include "tao/LocalObject.h"
 
-namespace MyImpl
+namespace RoundTrip_Impl
 {
 
   /**
@@ -26,7 +27,8 @@ namespace MyImpl
    */
 
   class NODEAPPTEST_ROUNDTRIP_EXEC_Export RoundTrip_exec_i :
-    public virtual NodeAppTest::RoundTrip_Exec,
+    public virtual NodeAppTest::LatencyTest,
+    public virtual RoundTrip_Impl::RoundTrip_Exec,
     public virtual TAO_Local_RefCounted_Object
   {
 
@@ -47,35 +49,59 @@ namespace MyImpl
                 ACE_THROW_SPEC ((CORBA::SystemException));
     */
 
+      NodeAppTest::CCM_LatencyTest_ptr
+      get_facet_1(ACE_ENV_SINGLE_ARG_DECL)
+	ACE_THROW_SPEC ((CORBA::SystemException));
+
+
+      NodeAppTest::CCM_LatencyTest_ptr
+      get_facet_2(ACE_ENV_SINGLE_ARG_DECL)
+	ACE_THROW_SPEC ((CORBA::SystemException));
+
     // Operations from Components::SessionComponent
-    virtual void set_session_context (Components::SessionContext_ptr ctx
-                                      ACE_ENV_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      virtual void set_session_context (Components::SessionContext_ptr ctx
+					ACE_ENV_ARG_DECL_WITH_DEFAULTS)
+	ACE_THROW_SPEC ((CORBA::SystemException,
+			 Components::CCMException));
 
-    virtual void ccm_activate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      virtual void ccm_activate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+	ACE_THROW_SPEC ((CORBA::SystemException,
+			 Components::CCMException));
 
-    virtual void ccm_passivate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      virtual void ccm_passivate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+	ACE_THROW_SPEC ((CORBA::SystemException,
+			 Components::CCMException));
 
-    virtual void ccm_remove (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      virtual void ccm_remove (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+	ACE_THROW_SPEC ((CORBA::SystemException,
+			 Components::CCMException));
 
     /// Helper function to be called back by timeout_Handler
-    void pulse (void);
+      void pulse (void);
 
     // CIAO defined methods
-    virtual void ciao_preactivate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      virtual void ciao_preactivate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+	ACE_THROW_SPEC ((CORBA::SystemException,
+			 Components::CCMException));
 
-    virtual void ciao_postactivate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
-      ACE_THROW_SPEC ((CORBA::SystemException,
-                       Components::CCMException));
+      virtual void ciao_postactivate (ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS)
+	ACE_THROW_SPEC ((CORBA::SystemException,
+			 Components::CCMException));
+  };
+
+  //
+  //
+  //
+  class LatencyTest_Impl : public virtual NodeAppTest::CCM_LatencyTest,
+                           public virtual TAO_Local_RefCounted_Object
+  {
+  public:
+    LatencyTest_Impl ()
+    {
+    }
+
+    virtual CORBA::Long cube_long (CORBA::Long data)
+        ACE_THROW_SPEC ((CORBA::SystemException));
   };
 
   /**
@@ -84,7 +110,7 @@ namespace MyImpl
    * RoundTrip home executor implementation class.
    */
   class NODEAPPTEST_ROUNDTRIP_EXEC_Export RoundTripHome_exec_i :
-    public virtual NodeAppTest::CCM_NodeAppTest_RoundTripHome,
+    public virtual RoundTrip_Impl::RoundTripHome_Exec,
     public virtual TAO_Local_RefCounted_Object
   {
   public:
