24ded8c6278ec68af3c999a89d86506292e02fc3
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-05 20:05:15 +0000

    Sat Jun  5 15:04:15 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
+++ TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
@@ -7,23 +7,27 @@ usage (const ACE_TCHAR* program)
 {
   ACE_ERROR ((LM_ERROR,
               ACE_TEXT ("Usage: %s -u <URI>\n")
-              ACE_TEXT (" <URI>: URI identifying the resource\n"),
+              ACE_TEXT (" <URI>: URI identifying the package\n"),
               program));
 }
 
 int
 ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
-  ACE_TCHAR* url = 0;
-  ACE_Get_Opt get_opt (argc, argv, ACE_TEXT ("u:"));
+  ACE_TCHAR* package_url = 0;
+  ACE_TCHAR* plan_url = 0;
+  ACE_Get_Opt get_opt (argc, argv, ACE_TEXT ("p:d"));
   int c;
 
   while ((c = get_opt ()) != EOF)
     {
       switch (c)
         {
-          case 'u':
-            url = get_opt.opt_arg();
+          case 'p':
+            package_url = get_opt.opt_arg();
+            break;
+          case 'd':
+            plan_url = get_opt.opt_arg();
             break;
           default:
             usage(argv[0]);
@@ -31,7 +35,7 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
         }
     }
 
-  if (url == 0) {
+  if (package_url == 0) {
     usage(argv[0]);
     return -1;
   }
@@ -43,7 +47,16 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
   try
     {
       CIAO::RepositoryManager_Impl rep_impl;
-      rep_impl.installPackage ("PC", url);
+      rep_impl.installPackage ("PC", package_url);
+
+      DOMDocument* doc = CIAO::Config_Handler::Utils::
+                         create_document (plan_url);
+      CIAO::Config_Handler::Plan_Handler plan_handler (doc,
+                                                DOMNodeFilter::SHOW_ELEMENT |
+                                                DOMNodeFilter::SHOW_TEXT);
+      Deployment::DeploymentPlan plan;
+      plan_handler.process_plan (plan);
+      Deployment::DnC_Dump::dump (plan);
     }
   catch (CORBA::Exception& ex)
     {
