6d0cfa4d61299ead2c848935772b7615bd6114b9
smcqueen <smcqueen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-08 12:02:27 +0000

    ChangeLogTag: Tue Jun 08 12:07:55 2004  Simon McQueen  <sm@prismtechnologies.com>

--- trunk/TAO/orbsvcs/ImplRepo_Service/Activator_Options.cpp
+++ trunk/TAO/orbsvcs/ImplRepo_Service/Activator_Options.cpp
@@ -265,6 +265,14 @@ Options::init (int argc, char *argv[])
   // Now initialize the orb and pass it the leftover arguments
   ACE_TRY_NEW_ENV
     {
+      // First make sure the Activator doesn't pick up an environment setting 
+      // that would make it try to register its POAs with the ImplRepo.
+      char* use_IMR_env_var_value = ACE_OS::getenv ("TAO_USE_IMR");
+      if (use_IMR_env_var_value != 0)
+        {
+          ACE_OS::putenv ("TAO_USE_IMR=0");
+        }
+
       this->orb_ = CORBA::ORB_init (orb_argc,
                                     orb_args.argv (),
                                     0
