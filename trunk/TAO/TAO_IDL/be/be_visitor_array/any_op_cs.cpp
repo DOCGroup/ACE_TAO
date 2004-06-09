72edc8794bb69ef97a667829e5bcdb3aea0bad9d
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-09 00:04:54 +0000

    ChangeLogTag:Tue Jun 08 17:00:14 2004  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- trunk/TAO/TAO_IDL/be/be_visitor_array/any_op_cs.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_array/any_op_cs.cpp
@@ -118,10 +118,7 @@ be_visitor_array_any_op_cs::visit_array (be_array *node)
       << "_tao_any," << be_nl
       << node->name () << "_forany::_tao_any_destructor," << be_nl
       << node->tc_name () << "," << be_nl
-      << "const_cast<" << be_idt << be_idt_nl
-      << "const " << node->name () << "_slice *&> (" << be_nl
       << "_tao_elem.out ()" << be_uidt_nl
-      << ")" << be_uidt << be_uidt_nl
       << ");" << be_uidt << be_uidt << be_uidt << be_uidt_nl
       << "}";
 
