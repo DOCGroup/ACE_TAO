194cd613287d043fef64ac1742abf8d9a9f7a1c0
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-10 22:55:58 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/Config_Handlers/CPK_Handler.cpp
+++ TAO/CIAO/DnC/Config_Handlers/CPK_Handler.cpp
@@ -11,52 +11,61 @@ CIAO::Config_Handler::CPK_Handler::
 process_CCMComponentPortKind (DOMNodeIterator * iter,
                               Deployment::CCMComponentPortKind &kind)
 {
-  XStr facet ("Facet");
-  XStr simplex_receptacle ("SimplexReceptacle");
-  XStr multiplex_receptacle ("MultiplexReceptacle");
-  XStr event_emitter ("EventEmitter");
-  XStr event_publisher ("EventPublisher");
-  XStr event_consumer ("EventConsumer");
-
-
+  const char* facet = XMLString::transcode (XStr ("Facet"));
+  const char* simplex_receptacle = XMLString::transcode (XStr ("SimplexReceptacle"));
+  const char* multiplex_receptacle = XMLString::transcode (XStr ("MultiplexReceptacle"));
+  const char* multiplex_receptacle = XMLString::transcode (XStr ("EventEmitter"));
+  const char* event_publisher = XMLString::transcode (XStr ("EventPublisher"));
+  const char* event_consumer = XMLString::transcode (XStr ("EventConsumer"));
 
   XStr kind_str = XStr (Utils::parse_string (iter));
 
-  if ((ACE_OS::strcmp (XMLString::transcode (kind_str),
-                       XMLString::transcode (facet))) == 0)
+  const char* kind_str_tr = XMLString::transcode (kind_str);
+
+  if ((ACE_OS::strcmp (kind_str_tr, facet)) == 0)
   {
     kind = Deployment::Facet;
   }
-  else if ((ACE_OS::strcmp (XMLString::transcode (kind_str),
-                       XMLString::transcode (simplex_receptacle))) == 0)
+  else if ((ACE_OS::strcmp (kind_str_tr, simplex_receptacle)) == 0)
   {
     kind = Deployment::SimplexReceptacle;
   }
-  else if ((ACE_OS::strcmp (XMLString::transcode (kind_str),
-                       XMLString::transcode (multiplex_receptacle))) == 0)
+  else if ((ACE_OS::strcmp (kind_str_tr, multiplex_receptacle)) == 0)
   {
     kind = Deployment::MultiplexReceptacle;
   }
-  else if ((ACE_OS::strcmp (XMLString::transcode (kind_str),
-                       XMLString::transcode (event_emitter))) == 0)
+  else if ((ACE_OS::strcmp (kind_str_tr, event_emitter)) == 0)
   {
     kind = Deployment::EventEmitter;
   }
-  else if ((ACE_OS::strcmp (XMLString::transcode (kind_str),
-                       XMLString::transcode (event_publisher))) == 0)
+  else if ((ACE_OS::strcmp (kind_str_tr, event_publisher)) == 0)
   {
     kind = Deployment::EventPublisher;
   }
-  else if ((ACE_OS::strcmp (XMLString::transcode (kind_str),
-                       XMLString::transcode (event_consumer))) == 0)
+  else if ((ACE_OS::strcmp (kind_str_tr, event_consumer)) == 0)
   {
     kind = Deployment::EventConsumer;
   }
   else
   {
-     // Something wrong here.. Throw exception
-  ACE_DEBUG ((LM_DEBUG,
-              "Config_Handler::CPK_Handler::process_CCMComponentPortKing illegal <CCMComponentPortKind> value found \n"));
-  ACE_THROW (CORBA::INTERNAL ());
+    // Something wrong here.. Throw exception
+    XMLString::release (kind_str_tr);
+    XMLString::release (facet);
+    XMLString::release (simplex_receptacle);
+    XMLString::release (multiplex_receptacle);
+    XMLString::release (multiplex_receptacle);
+    XMLString::release (event_publisher);
+    XMLString::release (event_consumer);
+
+    ACE_DEBUG ((LM_DEBUG,
+                "Config_Handler::CPK_Handler::process_CCMComponentPortKing illegal <CCMComponentPortKind> value found \n"));
+    ACE_THROW (CORBA::INTERNAL ());
   }
+  XMLString::release (kind_str_tr);
+  XMLString::release (facet);
+  XMLString::release (simplex_receptacle);
+  XMLString::release (multiplex_receptacle);
+  XMLString::release (multiplex_receptacle);
+  XMLString::release (event_publisher);
+  XMLString::release (event_consumer);
 }
