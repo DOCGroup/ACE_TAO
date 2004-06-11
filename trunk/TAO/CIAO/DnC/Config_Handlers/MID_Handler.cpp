3a890241919431ff196e145d53a799e0a930ec86
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-11 21:14:42 +0000

    Fri Jun 11 16:12:54 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DnC/Config_Handlers/MID_Handler.cpp
+++ trunk/TAO/CIAO/DnC/Config_Handlers/MID_Handler.cpp
@@ -3,7 +3,6 @@
 #include "tao/Exception.h"
 #include "ace/Auto_Ptr.h"
 #include "ace/Log_Msg.h"
-
 #include "Property_Handler.h"
 #include "Requirement_Handler.h"
 #include "NIA_Handler.h"
@@ -11,15 +10,10 @@
 #include "IR_Handler.h"
 #include "Process_Element.h"
 
-#include <iostream>
-
-using std::cerr;
-using std::endl;
-
 BEGIN_DEPLOYMENT_NAMESPACE
 
 void MID_Handler::process_MonolithicImplementationDescription
-(::Deployment::MonolithicImplementationDescription &mid)
+              (::Deployment::MonolithicImplementationDescription &mid)
 {
   for (DOMNode* node = this->iter_->nextNode();
        node != 0;
@@ -27,24 +21,28 @@ void MID_Handler::process_MonolithicImplementationDescription
     {
       XStr node_name (node->getNodeName());
       if (node_name == XStr 
-        (ACE_TEXT ("Deployment:MonolithicImplementationDescription")))
+               (ACE_TEXT ("Deployment:MonolithicImplementationDescription")))
         {
         }
       else if
-        (process_sequence_common<Deployment::Property>(this->doc_, this->iter_, node,
-                                                       node_name, "execParameter", mid.execParameter,
-                                                       &Property_Handler::process_Property,
-                                                       this->id_map_));
+        (process_sequence_common<Deployment::Property>
+           (this->doc_, this->iter_, node,
+            node_name, "execParameter", mid.execParameter,
+            &Property_Handler::process_Property,
+            this->id_map_));
       else if
-        (process_sequence_remote<Deployment::NamedImplementationArtifact, NIA_Handler>
-         (this->doc_, this->iter_, node,
-          node_name, "primaryArtifact", mid.primaryArtifact,
-          &NIA_Handler::process_NamedImplementationArtifact, this->id_map_));
+        (process_sequence_remote<Deployment::NamedImplementationArtifact, 
+                                 NIA_Handler>
+           (this->doc_, this->iter_, node,
+            node_name, "primaryArtifact", mid.primaryArtifact,
+            &NIA_Handler::process_NamedImplementationArtifact, 
+            this->id_map_));
       else if
-        (process_sequence_remote<Deployment::ImplementationRequirement, IR_Handler>
-         (this->doc_, this->iter_, node,
-          node_name, "deployRequirement", mid.deployRequirement,
-          &IR_Handler::process_ImplementationRequirement, this->id_map_));
+        (process_sequence_remote<Deployment::ImplementationRequirement, 
+                                 IR_Handler>
+           (this->doc_, this->iter_, node,
+            node_name, "deployRequirement", mid.deployRequirement,
+            &IR_Handler::process_ImplementationRequirement, this->id_map_));
       else
         {
           this->iter_->previousNode ();
--- trunk/TAO/CIAO/DnC/Config_Handlers/NIA_Handler.cpp
+++ trunk/TAO/CIAO/DnC/Config_Handlers/NIA_Handler.cpp
@@ -3,17 +3,10 @@
 #include "tao/Exception.h"
 #include "ace/Auto_Ptr.h"
 #include "ace/Log_Msg.h"
-
 #include "IAD_Handler.h"
 #include "NIA_Handler.h"
-
 #include "Process_Basic_Type.h"
 #include "Process_Element.h"
-
-#include <iostream>
-
-using std::cerr;
-using std::endl;
 
 BEGIN_DEPLOYMENT_NAMESPACE
 
@@ -27,16 +20,18 @@ void NIA_Handler::process_NamedImplementationArtifact
     {
       XStr node_name (node->getNodeName());
       if (node_name == XStr 
-        (ACE_TEXT ("Deployment:NamedImplementationArtifact")))
+               (ACE_TEXT ("Deployment:NamedImplementationArtifact")))
         {
         }
       else if
-        (process_string(this->iter_, node_name, "name", nia.name));
+        (process_string (this->iter_, node_name, "name", nia.name));
       else if
-        (process_element_remote<Deployment::ImplementationArtifactDescription, IAD_Handler>
-         (this->doc_, this->iter_, node,
-          node_name, "referencedArtifact", nia.referencedArtifact,
-          &IAD_Handler::process_ImplementationArtifactDescription, this->id_map_));
+        (process_element_remote<Deployment::ImplementationArtifactDescription,
+                                IAD_Handler>
+           (this->doc_, this->iter_, node,
+            node_name, "referencedArtifact", nia.referencedArtifact,
+            &IAD_Handler::process_ImplementationArtifactDescription, 
+            this->id_map_));
       else
        {
          this->iter_->previousNode ();
