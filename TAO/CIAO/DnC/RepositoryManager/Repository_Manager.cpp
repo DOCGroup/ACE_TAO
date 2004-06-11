f66e06979b625f52cdcb1944545964a85e472d52
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-11 17:56:54 +0000

    Fri Jun 11 12:55:49 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
+++ TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
@@ -68,12 +68,12 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
                                                 DOMNodeFilter::SHOW_TEXT);
       Deployment::DeploymentPlan plan;
       plan_handler.process_plan (plan);
-      Deployment::DnC_Dump::dump (plan);
+      //Deployment::DnC_Dump::dump (plan);
       Deployment::PackageConfiguration* pc;
       CIAO::RepositoryManager_Impl rep_impl;
       rep_impl.installPackage ("PC", package_url);
       pc = rep_impl.findPackageByName ("PC");
-      Deployment::DnC_Dump::dump (*pc);
+      //Deployment::DnC_Dump::dump (*pc);
 
       /*
 
