69448aaabdf3d45e3c565225481f559a3d1d6ef7
coryan <coryan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-14 20:48:26 +0000

    Mon Jun 14 16:46:35 2004  Carlos O'Ryan  <coryan@atdesk.com>

--- bin/DependencyGenerator/Preprocessor.pm
+++ bin/DependencyGenerator/Preprocessor.pm
@@ -53,65 +53,65 @@ sub process {
       ## outside that all of the inner regular expressions have in
       ## common.  That way we go down the path of if elsif only if it is
       ## even possible due to the outside regular expression.
-      if (/#/) {
-        ## Remove c++ and same line c comments inside this if statement.
-        ## This saves about 5% off of processing the ace directory
-        ## and we only need to strip comments if we are actually
-        ## going to look at the string.
-        $_ =~ s/\/\/.*//o;
-        $_ =~ s/\/\*.*\*\///o;
+      next if (not /^\s*#/);
 
-        if (/#\s*endif/) {
-          --$ifcount;
-          if (defined $zero[0] && $ifcount == $zero[$#zero]) {
-            pop(@zero);
-          }
+      ## Remove c++ and same line c comments inside this if statement.
+      ## This saves about 5% off of processing the ace directory
+      ## and we only need to strip comments if we are actually
+      ## going to look at the string.
+      $_ =~ s/\/\/.*//o;
+      $_ =~ s/\/\*.*\*\///o;
+
+      if (/#\s*endif/) {
+        --$ifcount;
+        if (defined $zero[0] && $ifcount == $zero[$#zero]) {
+          pop(@zero);
         }
-        elsif (/#\s*if\s+0/) {
-          push(@zero, $ifcount);
-          ++$ifcount;
+      }
+      elsif (/#\s*if\s+0/) {
+        push(@zero, $ifcount);
+        ++$ifcount;
+      }
+      elsif (/#\s*if/) {
+        ++$ifcount;
+      }
+      elsif (!defined $zero[0] &&
+             /#\s*include\s+[<"]([^">]+)[">]/o) {
+        ## Locate the include file
+        my($inc) = undef;
+        if (exists $self->{'ifound'}->{$1}) {
+          $inc = $self->{'ifound'}->{$1};
         }
-        elsif (/#\s*if/) {
-          ++$ifcount;
-        }
-        elsif (!defined $zero[0] &&
-               /#\s*include\s+[<"]([^">]+)[">]/o) {
-          ## Locate the include file
-          my($inc) = undef;
-          if (exists $self->{'ifound'}->{$1}) {
-            $inc = $self->{'ifound'}->{$1};
-          }
-          else {
-            foreach my $dirp (@{$self->{'ipaths'}}) {
-              if (-r "$dirp/$1") {
-                $inc = "$dirp/$1";
-                last;
-              }
+        else {
+          foreach my $dirp (@{$self->{'ipaths'}}) {
+            if (-r "$dirp/$1") {
+              $inc = "$dirp/$1";
+              last;
             }
-
-            if (!defined $inc) {
-              ## If the file we're currently looking at contains a
-              ## directory name then, we need to look for include
-              ## files in that directory.
-              if (-r "$dir/$1") {
-                $inc = "$dir/$1";
-              }
-            }
-            $self->{'ifound'}->{$1} = $inc;
           }
 
-          ## If we've found the include file, then process it too.
-          if (defined $inc) {
-            $inc =~ s/\\/\//go;
-            if (!$noinline ||
-                ($recurse == 1 || $inc !~ /\.i(nl)?$/o)) {
-              push(@{$$files{$file}}, $inc);
-              if (!defined $$files{$inc}) {
-                ## Process this file, but do not return the include files
-                if (!defined $self->{'exclude'}->{basename($inc)}) {
-                  $self->process($inc, $noinline, 1);
-                }
-              }
+          if (!defined $inc) {
+            ## If the file we're currently looking at contains a
+            ## directory name then, we need to look for include
+            ## files in that directory.
+            if (-r "$dir/$1") {
+              $inc = "$dir/$1";
+            }
+          }
+          $self->{'ifound'}->{$1} = $inc;
+        }
+
+        ## If we've found the include file, then process it too.
+        next if (not defined $inc);
+
+        $inc =~ s/\\/\//go;
+        if (!$noinline ||
+            ($recurse == 1 || $inc !~ /\.i(nl)?$/o)) {
+          push(@{$$files{$file}}, $inc);
+          if (!defined $$files{$inc}) {
+            ## Process this file, but do not return the include files
+            if (!defined $self->{'exclude'}->{basename($inc)}) {
+              $self->process($inc, $noinline, 1);
             }
           }
         }
