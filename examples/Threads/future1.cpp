ccfb2b58e3734a84992c6f60a339ad774cad4ff6
Johnny Willemsen <jwillemsen@remedy.nl>
2004-06-14 18:20:30 +0000

    ChangeLogTag: Mon Jun 14  18:20:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- examples/Threads/future1.cpp
+++ examples/Threads/future1.cpp
@@ -18,6 +18,7 @@
 // ============================================================================
 
 #include "ace/OS_NS_string.h"
+#include "ace/OS_NS_unistd.h"
 #include "ace/OS_main.h"
 #include "ace/ACE.h"
 #include "ace/Task.h"
