29ecfba63c90012bc6571851f1086abd17cabc18
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-14 20:11:10 +0000

    Mon Jun 14 15:01:08 2004  Gan Deng  <dengg@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
+++ trunk/TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
@@ -2,6 +2,7 @@
 
 #include "RepositoryManager_Impl.h"
 #include "DeploymentC.h"
+#include "ExecutionManager/ExecutionManagerC.h"
 #include "ace/OS_NS_stdio.h"
 #include "ace/streams.h"
 
@@ -104,8 +105,8 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
                                                 ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      Deployment::ExecutionManager_var exec_mgr =
-        Deployment::ExecutionManager::_narrow (obj.in () ACE_ENV_ARG_PARAMETER);
+      CIAO::ExecutionManagerDaemon_var exec_mgr =
+        CIAO::ExecutionManagerDaemon::_narrow (obj.in () ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
       if (CORBA::is_nil (exec_mgr.in ()))
@@ -161,7 +162,11 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 
       ACE_DEBUG ((LM_DEBUG, "Executor: destroy the manager....."));
       exec_mgr->destroyManager (dapp_mgr.in ());
+      exec_mgr->shutdown ();
       ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+
+      orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+      ACE_TRY_CHECK;
     }
   catch (CORBA::Exception& ex)
     {
