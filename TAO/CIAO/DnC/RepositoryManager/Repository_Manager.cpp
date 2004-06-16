d924b286bfa40c8865ee093dc1d7484c1ebb80d5
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-16 17:04:11 +0000

    Wed Jun 16 12:02:12 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
+++ TAO/CIAO/DnC/RepositoryManager/Repository_Manager.cpp
@@ -2,20 +2,38 @@
 
 #include "RepositoryManager_Impl.h"
 #include "DeploymentC.h"
-//#include "ExecutionManager/ExecutionManagerC.h"
+#include "ace/Hash_Map_Manager.h"
+#include "ExecutionManager/ExecutionManagerC.h"
+#include "NodeManager/NodeDaemonC.h"
 #include "ace/OS_NS_stdio.h"
 #include "ace/streams.h"
+#include <map>
 
 const char * exec_ior = "file://exec_mgr.ior";
+const char * node_daemon_ior = 0;
 
 class Null_Dom_Document { };
 // exception thrown when we have a null dom document.
+
+typedef std::map<std::string, int> REF_MAP;
+typedef std::map<int, int> ART_REF_MAP;
+
+void traverse_instance (Deployment::SubcomponentInstantiationDescription 
+                        &instance,
+			Deployment::DeploymentPlan &plan, int l,
+                        REF_MAP &ref_map, REF_MAP &primary_ref_map);
+
+void traverse_interface (Deployment::SubcomponentInstantiationDescription
+                         &instance,
+                         Deployment::PlanSubcomponentPortEndpoint
+                         &pspe);
 
 static void
 usage (const ACE_TCHAR* program)
 {
   ACE_ERROR ((LM_ERROR,
-              ACE_TEXT ("Usage: %s -p <URI> -d <URI> -k <EX_IOR>\n")
+              ACE_TEXT ("Usage: %s -p <URI> -d <URI> -k <EX_IOR> \
+                         -t <NODE_MANAGER_IOR> \n")
               ACE_TEXT (" <URI>: URI identifying the package\n"),
               program));
 }
@@ -29,7 +47,7 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
   // deployment plan URL
   ACE_TCHAR* plan_url = 0;
 
-  ACE_Get_Opt get_opt (argc, argv, ACE_TEXT ("p:d:k:"));
+  ACE_Get_Opt get_opt (argc, argv, ACE_TEXT ("p:d:k:t"));
   int c;
 
   while ((c = get_opt ()) != EOF)
@@ -44,6 +62,9 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
             break;
           case 'k':
             exec_ior = get_opt.opt_arg ();
+            break;
+          case 't':
+            node_daemon_ior = get_opt.opt_arg ();
             break;
           default:
             usage(argv[0]);
@@ -83,12 +104,83 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
                                                   DOMNodeFilter::SHOW_TEXT);
       Deployment::DeploymentPlan plan;
       plan_handler.process_plan (plan);
-
-      //Deployment::DnC_Dump::dump (plan);
       Deployment::PackageConfiguration* pc;
       CIAO::RepositoryManager_Impl rep_impl;
       rep_impl.installPackage ("PC", package_url);
       pc = rep_impl.findPackageByName ("PC");
+
+      REF_MAP ref_map;
+      REF_MAP primary_ref_map;
+
+      for (CORBA::ULong x = 0; x < pc->basePackage.length (); ++x)
+        {
+          for (CORBA::ULong y = 0; 
+               y < pc->basePackage[x].implementation.length (); ++y)
+            {
+              Deployment::ComponentImplementationDescription cid =
+                pc->basePackage[x].implementation[y].referencedImplementation;
+              for (CORBA::ULong z = 0; z < cid.assemblyImpl.length (); ++z)
+                {
+                  for (CORBA::ULong k = 0; 
+                       k < cid.assemblyImpl[z].instance.length (); ++k)
+                    {
+                      const char* in_name =
+                        cid.assemblyImpl[z].instance[k].name;
+                      for (CORBA::ULong l = 0; l < plan.instance.length (); ++l)
+                        {
+                          const char* plan_name = plan.instance[l].name;
+                          if (strcmp (plan_name, in_name) == 0)
+                            {
+			      traverse_instance (cid.assemblyImpl[z].
+                                                 instance[k],
+						 plan, l, ref_map,
+                                                 primary_ref_map);
+                            }
+                        }
+                    }
+                  for (CORBA::ULong m = 0; 
+                       m < cid.assemblyImpl[z].connection.length (); ++m)
+                    {
+	              CORBA::ULong con_length (plan.connection.length ());
+	              plan.connection.length (con_length + 1);
+                      for (CORBA::ULong n = 0;
+                           n < cid.assemblyImpl[z].connection[m].
+                           internalEndpoint.length (); ++n)
+                        {
+                          CORBA::ULong iep_len (plan.connection[con_length].
+                                                internalEndpoint.length ());
+                          plan.connection[con_length].internalEndpoint
+                                       .length (iep_len + 1);
+                          plan.connection[con_length].internalEndpoint
+                              [iep_len].portName = cid.assemblyImpl[z].
+                                 connection[m].internalEndpoint[n].portName;
+                          CORBA::ULong ins_ref = cid.assemblyImpl[z].
+                            connection[m].internalEndpoint[n].instanceRef;
+                          const char* ins_name =
+                            cid.assemblyImpl[z].instance[ins_ref].name;
+                          for (CORBA::ULong w = 0; w < plan.instance.length (); 
+                               ++w)
+                            {
+                              const char* pl_name = plan.instance[w].name;
+                              if (strcmp (pl_name, ins_name) == 0)
+                                {
+                                  plan.connection[con_length].internalEndpoint
+                                    [iep_len].instanceRef = w;
+                                  break;
+                                }
+                            }
+                          traverse_interface (cid.assemblyImpl[z]
+                                              .instance[ins_ref],
+                                              plan.connection[con_length].
+                                              internalEndpoint[iep_len]);
+                        }
+                    }
+                }
+            }
+        }
+
+      Deployment::DnC_Dump::dump (plan);
+      
 
       //Deployment::DnC_Dump::dump (*pc);
 
@@ -119,6 +211,28 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
           return 1;
 
         }
+
+      CIAO::NodeDaemon_var node_mgr;
+      if (node_daemon_ior != 0)
+      {
+        CORBA::Object_var node_mgr_obj = orb->string_to_object
+                                                 (node_daemon_ior
+                                                  ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
+
+        node_mgr =
+          CIAO::NodeDaemon::_narrow (node_mgr_obj.in ()
+                                     ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
+
+        if (CORBA::is_nil (node_mgr.in ()))
+          {
+            ACE_DEBUG ((LM_DEBUG,
+                        "Executor: nil Node Manager reference, \
+                        narrow failed\n"));
+            return 1;
+          }
+      }
 
       ACE_DEBUG ((LM_DEBUG, "Executor: Obtained Execution Manager ref \n"));
       Deployment::DomainApplicationManager_var dapp_mgr =
@@ -164,11 +278,14 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 
       ACE_DEBUG ((LM_DEBUG, "Executor: destroy the manager....."));
       exec_mgr->destroyManager (dapp_mgr.in ());
+      exec_mgr->shutdown ();
       ACE_DEBUG ((LM_DEBUG, "[success]\n"));
 
-      //ACE_DEBUG ((LM_DEBUG, "Executor: shutdown execution manager....."));
-      //exec_mgr->shutdown ();
-      //ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+      if (node_daemon_ior != 0)
+        {
+          ACE_DEBUG ((LM_DEBUG, "shutting down node manager \n"));
+          node_mgr->shutdown (); // shut down the node manager.
+        }
 
       orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
       ACE_TRY_CHECK;
@@ -187,4 +304,192 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
     }
 
   return 0;
+}
+
+void traverse_interface (Deployment::SubcomponentInstantiationDescription
+                         &instance,
+                         Deployment::PlanSubcomponentPortEndpoint
+                         &pspe)
+{
+  for (CORBA::ULong m = 0; m < instance.package.length (); ++m)
+    {
+      Deployment::ComponentPackageDescription
+	package = instance.package[m];
+
+      Deployment::ComponentInterfaceDescription
+        cid = package.realizes;
+
+      for (CORBA::ULong n = 0; n < cid.port.length (); ++n)
+        {
+          const char* main_port_name = cid.port[n].name;
+          const char* port_name = pspe.portName;
+          if (strcmp (main_port_name, port_name) == 0)
+            {
+              pspe.kind = cid.port[n].kind;
+            }
+        }
+    }
+}
+
+void traverse_instance (Deployment::SubcomponentInstantiationDescription 
+                        &instance,
+			Deployment::DeploymentPlan &plan, int l,
+                        REF_MAP &ref_map, REF_MAP &primary_ref_map)
+{
+  ART_REF_MAP art_ref_map;
+
+  for (CORBA::ULong m = 0; m < instance.package.length (); ++m)
+    {
+      Deployment::ComponentPackageDescription
+	package = instance.package[m];
+
+      for (CORBA::ULong n = 0; n < package.implementation.length (); ++n)
+	{
+	  Deployment::PackagedComponentImplementation
+	    impl = instance.package[m].implementation[n];
+	  CORBA::ULong impl_length (plan.implementation.length ());
+	  plan.implementation.length (impl_length + 1);
+	  plan.implementation[impl_length].name = plan.instance[l].name;
+	  plan.instance[l].implementationRef = impl_length;
+
+	  for (CORBA::ULong p = 0; 
+               p < impl.referencedImplementation.monolithicImpl.length (); ++p)
+	    {
+	      Deployment::MonolithicImplementationDescription
+		mid = impl.referencedImplementation.monolithicImpl[p];
+
+	      for (CORBA::ULong q = 0; q < mid.primaryArtifact.length (); ++q)
+		{
+		  Deployment::ImplementationArtifactDescription
+		    pack_iad = mid.primaryArtifact[q].referencedArtifact;
+
+		  CORBA::ULong art_length (plan.artifact.length ());
+		  plan.artifact.length (art_length + 1);
+		  plan.artifact[art_length].name = 
+                       mid.primaryArtifact[q].name;
+		  plan.artifact[art_length].node = plan.instance[l].node;
+                  ref_map[std::string((const char*)plan.artifact
+                        [art_length].name)] = art_length;
+                  primary_ref_map[std::string((const char*)plan.artifact
+                        [art_length].name)] = art_length;
+		  CORBA::ULong
+		    art_ref_len (plan.implementation[impl_length].
+                                 artifactRef.length ());
+		  plan.implementation[impl_length].artifactRef.length 
+                        (art_ref_len + 1);
+		  plan.implementation[impl_length].artifactRef[art_ref_len]
+		    = art_length;
+		  for (CORBA::ULong e = 0; e < pack_iad.location.length (); ++e)
+		    {
+		      CORBA::ULong
+			art_loc_len 
+                          (plan.artifact[art_length].location.length ());
+		      plan.artifact[art_length].location.length 
+                        (art_loc_len + 1);
+		      plan.artifact[art_length].location[art_loc_len]
+			= pack_iad.location[e];
+		    }
+		  for (CORBA::ULong f = 0; 
+                       f < pack_iad.execParameter.length (); ++f)
+		    {
+		      CORBA::ULong
+			art_pro_len 
+                         (plan.artifact[art_length].execParameter.length ());
+		      plan.artifact[art_length].execParameter.length
+			(art_pro_len + 1);
+		      plan.artifact[art_length].execParameter[art_pro_len]
+			= pack_iad.execParameter[f];
+		    }
+		  for (CORBA::ULong g = 0; 
+                       g < pack_iad.dependsOn.length (); ++g)
+		    {
+                      REF_MAP::iterator map_iter
+                         = ref_map.find (std::string((const
+                                         char*)pack_iad.dependsOn[g].name));
+                      REF_MAP::iterator primary_map_iter
+                         = primary_ref_map.find (std::string((const
+                                         char*)pack_iad.dependsOn[g].name));
+                      if (map_iter != ref_map.end ())
+                        {
+                          if (primary_map_iter == primary_ref_map.end ())
+                            {
+                              ART_REF_MAP::iterator art_ref_map_iter
+                                = art_ref_map.find (map_iter->second);
+                              if (art_ref_map_iter == art_ref_map.end ())
+                                {
+		                  CORBA::ULong
+		                    new_art_ref_len 
+                                      (plan.implementation[impl_length].
+                                             artifactRef.length ());
+		                  plan.implementation[impl_length].
+                                     artifactRef.length 
+                                       (new_art_ref_len + 1);
+		                  plan.implementation[impl_length].
+                                     artifactRef[new_art_ref_len] =
+                                       map_iter->second;
+                                  art_ref_map[map_iter->second] = 
+                                     map_iter->second;
+                                }
+                            }
+                        }
+                      else
+                        {
+                          Deployment::ImplementationArtifactDescription
+                             depends_iad = pack_iad.dependsOn[g].
+                                  referencedArtifact;
+		          CORBA::ULong new_art_length (plan.artifact.length ());
+		          plan.artifact.length (new_art_length + 1);
+                          plan.artifact[new_art_length].name =
+                            pack_iad.dependsOn[g].name;
+		          plan.artifact[new_art_length].node = 
+                             plan.instance[l].node;
+		          for (CORBA::ULong h = 0; 
+                               h < depends_iad.location.length (); ++h)
+		            {
+		              CORBA::ULong
+			        new_art_loc_len 
+                                  (plan.artifact[new_art_length].
+                                      location.length ());
+		              plan.artifact[new_art_length].location.length 
+                                (new_art_loc_len + 1);
+		              plan.artifact[new_art_length].
+                                 location[new_art_loc_len]
+			           = depends_iad.location[h];
+		            }
+                          ref_map[std::string((const char*)plan.
+                                  artifact[new_art_length].
+                                  name)] = new_art_length;
+		          CORBA::ULong
+		            new_art_ref_len (plan.implementation[impl_length].
+                                         artifactRef.length ());
+		          plan.implementation[impl_length].artifactRef.length 
+                                (new_art_ref_len + 1);
+		          plan.implementation[impl_length].
+                                artifactRef[new_art_ref_len] = new_art_length;
+                          art_ref_map[new_art_length] = new_art_length;
+                        }
+                    }
+		}
+	    }
+	  for (CORBA::ULong x = 0; 
+               x < impl.referencedImplementation.configProperty.length (); ++x)
+	    {
+	      CORBA::ULong
+                impl_pro_len 
+                  (plan.implementation[impl_length].execParameter.length ());
+              plan.implementation[impl_length].execParameter.length
+			(impl_pro_len + 1);
+              plan.implementation[impl_length].execParameter[impl_pro_len]
+			= impl.referencedImplementation.configProperty[x];
+
+	      CORBA::ULong
+                ins_pro_len 
+                  (plan.instance[l].configProperty.length ());
+              plan.instance[l].configProperty.length
+			(ins_pro_len + 1);
+              plan.instance[l].configProperty[ins_pro_len]
+			= impl.referencedImplementation.configProperty[x];
+            }
+	}
+    }
 }
