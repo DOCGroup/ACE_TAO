0ea7f99067d55f50510429fb52991c07c47b6980
irfan <irfan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-16 15:26:11 +0000

    ChangeLogTag: Wed Jun 16 11:25:49 2004  Irfan Pyarali  <irfan@oomworks.com>

--- TAO/tests/RTCORBA/Diffserv/server.cpp
+++ TAO/tests/RTCORBA/Diffserv/server.cpp
@@ -31,7 +31,7 @@ Test_i::Test_i (CORBA::ORB_ptr orb)
 }
 
 void
-Test_i::test_method (ACE_ENV_SINGLE_ARG_DECL)
+Test_i::test_method (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   ACE_DEBUG ((LM_DEBUG,
