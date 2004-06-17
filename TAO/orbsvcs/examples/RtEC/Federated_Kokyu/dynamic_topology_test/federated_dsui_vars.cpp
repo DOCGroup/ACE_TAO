a3385ebdac8dc1c1f86e201faf9e8555f8d1e177
thrall <thrall@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-17 02:41:08 +0000

    Remade federated_dsui_vars.cpp

--- TAO/orbsvcs/examples/RtEC/Federated_Kokyu/dynamic_topology_test/federated_dsui_vars.cpp
+++ TAO/orbsvcs/examples/RtEC/Federated_Kokyu/dynamic_topology_test/federated_dsui_vars.cpp
@@ -1,4 +1,5 @@
 // $Id$
+
 #include <datastream_common.h>
 #include <dsui_types.h>
 #include "federated_dsui_families.h"
@@ -7,16 +8,16 @@ counter_t TEST_ONE_COUNTERS[NUM_COUNTERS_TEST_ONE] = {
 histogram_t TEST_ONE_HISTOGRAMS[NUM_HISTOGRAMS_TEST_ONE] = {
 };
 family_t TEST_ONE = {
-	"TEST_ONE",
-	NUM_EVENTS_TEST_ONE,
-	NUM_COUNTERS_TEST_ONE,
-	NUM_HISTOGRAMS_TEST_ONE,
-	TEST_ONE_COUNTERS,
-	TEST_ONE_HISTOGRAMS,
-	0, 
-	0, 
-	0, 
-	0
+        "TEST_ONE",
+        NUM_EVENTS_TEST_ONE,
+        NUM_COUNTERS_TEST_ONE,
+        NUM_HISTOGRAMS_TEST_ONE,
+        TEST_ONE_COUNTERS,
+        TEST_ONE_HISTOGRAMS,
+        0,
+        0,
+        0,
+        0
 };
 
 counter_t WORKER_GROUP_COUNTERS[NUM_COUNTERS_WORKER_GROUP] = {
@@ -24,16 +25,16 @@ counter_t WORKER_GROUP_COUNTERS[NUM_COUNTERS_WORKER_GROUP] = {
 histogram_t WORKER_GROUP_HISTOGRAMS[NUM_HISTOGRAMS_WORKER_GROUP] = {
 };
 family_t WORKER_GROUP = {
-	"WORKER_GROUP",
-	NUM_EVENTS_WORKER_GROUP,
-	NUM_COUNTERS_WORKER_GROUP,
-	NUM_HISTOGRAMS_WORKER_GROUP,
-	WORKER_GROUP_COUNTERS,
-	WORKER_GROUP_HISTOGRAMS,
-	0, 
-	0, 
-	0, 
-	0
+        "WORKER_GROUP",
+        NUM_EVENTS_WORKER_GROUP,
+        NUM_COUNTERS_WORKER_GROUP,
+        NUM_HISTOGRAMS_WORKER_GROUP,
+        WORKER_GROUP_COUNTERS,
+        WORKER_GROUP_HISTOGRAMS,
+        0,
+        0,
+        0,
+        0
 };
 
 counter_t MAIN_GROUP_COUNTERS[NUM_COUNTERS_MAIN_GROUP] = {
@@ -41,23 +42,23 @@ counter_t MAIN_GROUP_COUNTERS[NUM_COUNTERS_MAIN_GROUP] = {
 histogram_t MAIN_GROUP_HISTOGRAMS[NUM_HISTOGRAMS_MAIN_GROUP] = {
 };
 family_t MAIN_GROUP = {
-	"MAIN_GROUP",
-	NUM_EVENTS_MAIN_GROUP,
-	NUM_COUNTERS_MAIN_GROUP,
-	NUM_HISTOGRAMS_MAIN_GROUP,
-	MAIN_GROUP_COUNTERS,
-	MAIN_GROUP_HISTOGRAMS,
-	0, 
-	0, 
-	0, 
-	0
+        "MAIN_GROUP",
+        NUM_EVENTS_MAIN_GROUP,
+        NUM_COUNTERS_MAIN_GROUP,
+        NUM_HISTOGRAMS_MAIN_GROUP,
+        MAIN_GROUP_COUNTERS,
+        MAIN_GROUP_HISTOGRAMS,
+        0,
+        0,
+        0,
+        0
 };
 
 int federated_table_offset= 1;
-family_t *federated_family_table[federated_NUM_FAMILIES] = {
-	
-	&TEST_ONE,
-	&MAIN_GROUP,
-	&WORKER_GROUP,
+family_t *federated_family_table[NUM_FAMILIES] = {
+
+        &TEST_ONE,
+        &MAIN_GROUP,
+        &WORKER_GROUP,
 };
 
