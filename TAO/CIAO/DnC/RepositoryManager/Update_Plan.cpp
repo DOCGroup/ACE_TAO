29b1974d5f6514a877fe2eb8742ad77ec8f171c9
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-18 16:57:23 +0000

    Fri Jun 18 11:56:09 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- TAO/CIAO/DnC/RepositoryManager/Update_Plan.cpp
+++ TAO/CIAO/DnC/RepositoryManager/Update_Plan.cpp
@@ -22,41 +22,46 @@ void traverse_package (Deployment::PackageConfiguration* &pc,
             {
               Deployment::ComponentAssemblyDescription assembly =
                 cid.assemblyImpl[z];
-              for (CORBA::ULong k = 0; 
-                   k < assembly.instance.length (); ++k)
-                {
-                  Deployment::SubcomponentInstantiationDescription ins =
-                    assembly.instance[k];
-                  const char* in_name = ins.name;
-                  for (CORBA::ULong l = 0; l < plan.instance.length (); ++l)
-                    {
-                      const char* plan_name = plan.instance[l].name;
-                      if (strcmp (plan_name, in_name) == 0)
-                        {
-                          traverse_instance (ins, plan, l,
-                                             ref_map, primary_ref_map);
-                        }
-                    }
-                }
-              for (CORBA::ULong m = 0; 
-                   m < assembly.connection.length (); ++m)
-                {
-                  Deployment::AssemblyConnectionDescription
-                     assembly_connection = assembly.connection[m];
-                  traverse_connection (assembly,
-                                       assembly_connection,
-                                       plan);
-                }
+              traverse_assembly (assembly, plan, ref_map, primary_ref_map);
             }
         }
     }
 }
 
-void traverse_connection (Deployment::ComponentAssemblyDescription
-                          &assembly,
-                          Deployment::AssemblyConnectionDescription
-                          &assembly_connection,
-                          Deployment::DeploymentPlan &plan)
+void traverse_assembly (Deployment::ComponentAssemblyDescription &assembly,
+                        Deployment::DeploymentPlan &plan,
+                        REF_MAP &ref_map, REF_MAP &primary_ref_map)
+{
+  for (CORBA::ULong k = 0; k < assembly.instance.length (); ++k)
+    {
+      Deployment::SubcomponentInstantiationDescription ins =
+        assembly.instance[k];
+      const char* in_name = ins.name;
+      for (CORBA::ULong l = 0; l < plan.instance.length (); ++l)
+        {
+          const char* plan_name = plan.instance[l].name;
+          if (strcmp (plan_name, in_name) == 0)
+            {
+              traverse_assembly_instance (ins, plan, l,
+                                          ref_map, primary_ref_map);
+            }
+        }
+    }
+  for (CORBA::ULong m = 0; m < assembly.connection.length (); ++m)
+    {
+      Deployment::AssemblyConnectionDescription
+         assembly_connection = assembly.connection[m];
+      traverse_assembly_connection (assembly,
+                                    assembly_connection,
+                                    plan);
+    }
+}
+
+void traverse_assembly_connection (Deployment::ComponentAssemblyDescription
+                                   &assembly,
+                                   Deployment::AssemblyConnectionDescription
+                                   &assembly_connection,
+                                   Deployment::DeploymentPlan &plan)
 {
   CORBA::ULong con_length (plan.connection.length ());
   plan.connection.length (con_length + 1);
@@ -116,10 +121,11 @@ void traverse_interface (Deployment::SubcomponentInstantiationDescription
     }
 }
 
-void traverse_instance (Deployment::SubcomponentInstantiationDescription
-                        &instance,
-			Deployment::DeploymentPlan &plan, int l,
-                        REF_MAP &ref_map, REF_MAP &primary_ref_map)
+void traverse_assembly_instance (Deployment::
+                                 SubcomponentInstantiationDescription
+                                 &instance,
+			         Deployment::DeploymentPlan &plan, int l,
+                                 REF_MAP &ref_map, REF_MAP &primary_ref_map)
 {
   ART_REF_MAP art_ref_map;
 
@@ -131,7 +137,7 @@ void traverse_instance (Deployment::SubcomponentInstantiationDescription
       for (CORBA::ULong n = 0; n < package.implementation.length (); ++n)
 	{
 	  Deployment::PackagedComponentImplementation
-	    impl = instance.package[m].implementation[n];
+	    impl = package.implementation[n];
 	  CORBA::ULong impl_length (plan.implementation.length ());
 	  plan.implementation.length (impl_length + 1);
 	  plan.implementation[impl_length].name = plan.instance[l].name;
@@ -143,134 +149,155 @@ void traverse_instance (Deployment::SubcomponentInstantiationDescription
 	      Deployment::MonolithicImplementationDescription
 		mid = impl.referencedImplementation.monolithicImpl[p];
 
-	      for (CORBA::ULong q = 0; q < mid.primaryArtifact.length (); ++q)
-		{
-		  Deployment::ImplementationArtifactDescription
-		    pack_iad = mid.primaryArtifact[q].referencedArtifact;
-
-		  CORBA::ULong art_length (plan.artifact.length ());
-		  plan.artifact.length (art_length + 1);
-		  plan.artifact[art_length].name =
-                       mid.primaryArtifact[q].name;
-		  plan.artifact[art_length].node = plan.instance[l].node;
-                  ACE_TString artifact_name =
-                     (const char*)plan.artifact[art_length].name;
-                  ref_map.bind (artifact_name, art_length);
-                  primary_ref_map.bind (artifact_name,
-                                        art_length);
-		  CORBA::ULong
-		    art_ref_len (plan.implementation[impl_length].
-                                 artifactRef.length ());
-		  plan.implementation[impl_length].artifactRef.length
-                        (art_ref_len + 1);
-		  plan.implementation[impl_length].artifactRef[art_ref_len]
-		    = art_length;
-		  for (CORBA::ULong e = 0; e < pack_iad.location.length (); ++e)
-		    {
-		      CORBA::ULong
-			art_loc_len
-                          (plan.artifact[art_length].location.length ());
-		      plan.artifact[art_length].location.length
-                        (art_loc_len + 1);
-		      plan.artifact[art_length].location[art_loc_len]
-			= pack_iad.location[e];
-		    }
-		  for (CORBA::ULong f = 0;
-                       f < pack_iad.execParameter.length (); ++f)
-		    {
-		      CORBA::ULong
-			art_pro_len
-                         (plan.artifact[art_length].execParameter.length ());
-		      plan.artifact[art_length].execParameter.length
-			(art_pro_len + 1);
-		      plan.artifact[art_length].execParameter[art_pro_len]
-			= pack_iad.execParameter[f];
-		    }
-		  for (CORBA::ULong g = 0;
-                       g < pack_iad.dependsOn.length (); ++g)
-		    {
-                      ACE_TString dep_name =
-                         (const char*)pack_iad.dependsOn[g].name;
-                      int arti_len;
-
-                      if (ref_map.find (dep_name, arti_len) == 0)
-                        {
-                          if (primary_ref_map.find (dep_name, arti_len) != 0)
-                            {
-                              if (art_ref_map.find (arti_len, arti_len) != 0)
-                                {
-		                  CORBA::ULong
-		                    new_art_ref_len
-                                      (plan.implementation[impl_length].
-                                             artifactRef.length ());
-		                  plan.implementation[impl_length].
-                                     artifactRef.length
-                                       (new_art_ref_len + 1);
-		                  plan.implementation[impl_length].
-                                     artifactRef[new_art_ref_len] =
-                                       arti_len;
-                                  art_ref_map.bind (arti_len, arti_len);
-                                }
-                            }
-                        }
-                      else
-                        {
-                          Deployment::ImplementationArtifactDescription
-                             depends_iad = pack_iad.dependsOn[g].
-                                  referencedArtifact;
-		          CORBA::ULong new_art_length (plan.artifact.length ());
-		          plan.artifact.length (new_art_length + 1);
-                          plan.artifact[new_art_length].name =
-                            pack_iad.dependsOn[g].name;
-		          plan.artifact[new_art_length].node =
-                             plan.instance[l].node;
-		          for (CORBA::ULong h = 0;
-                               h < depends_iad.location.length (); ++h)
-		            {
-		              CORBA::ULong
-			        new_art_loc_len
-                                  (plan.artifact[new_art_length].
-                                      location.length ());
-		              plan.artifact[new_art_length].location.length
-                                (new_art_loc_len + 1);
-		              plan.artifact[new_art_length].
-                                 location[new_art_loc_len]
-			           = depends_iad.location[h];
-		            }
-                          ref_map.bind (
-                             (const char*)plan.artifact[new_art_length].name,
-                              new_art_length);
-		          CORBA::ULong
-		            new_art_ref_len (plan.implementation[impl_length].
-                                         artifactRef.length ());
-		          plan.implementation[impl_length].artifactRef.length
-                                (new_art_ref_len + 1);
-		          plan.implementation[impl_length].
-                                artifactRef[new_art_ref_len] = new_art_length;
-                          art_ref_map.bind (new_art_length, new_art_length);
-                        }
-                    }
-		}
+              update_artifacts (mid, plan, plan.instance[l],
+                                ref_map, primary_ref_map, art_ref_map,
+                                plan.implementation[impl_length]);
 	    }
-	  for (CORBA::ULong x = 0;
-               x < impl.referencedImplementation.configProperty.length (); ++x)
-	    {
-	      CORBA::ULong
-                impl_pro_len
-                  (plan.implementation[impl_length].execParameter.length ());
-              plan.implementation[impl_length].execParameter.length
-			(impl_pro_len + 1);
-              plan.implementation[impl_length].execParameter[impl_pro_len]
-			= impl.referencedImplementation.configProperty[x];
-
-	      CORBA::ULong
-                ins_pro_len
-                  (plan.instance[l].configProperty.length ());
-              plan.instance[l].configProperty.length
-			(ins_pro_len + 1);
-              plan.instance[l].configProperty[ins_pro_len]
-			= impl.referencedImplementation.configProperty[x];
-            }
+          update_impl_config_property (impl, plan.implementation[impl_length],
+                                       plan.instance[l]);
 	}
+    }
+}
+
+void update_artifacts (Deployment::MonolithicImplementationDescription &mid,
+                       Deployment::DeploymentPlan &plan,
+                       Deployment::InstanceDeploymentDescription &instance,
+                       REF_MAP &ref_map, REF_MAP &primary_ref_map,
+                       ART_REF_MAP &art_ref_map,
+                       Deployment::MonolithicDeploymentDescription &mdd)
+{
+  for (CORBA::ULong q = 0; q < mid.primaryArtifact.length (); ++q)
+    {
+      Deployment::ImplementationArtifactDescription
+        pack_iad = mid.primaryArtifact[q].referencedArtifact;
+
+      CORBA::ULong art_length (plan.artifact.length ());
+      plan.artifact.length (art_length + 1);
+      plan.artifact[art_length].name = mid.primaryArtifact[q].name;
+      plan.artifact[art_length].node = instance.node;
+      ACE_TString artifact_name = (const char*)plan.artifact[art_length].name;
+      ref_map.bind (artifact_name, art_length);
+      primary_ref_map.bind (artifact_name, art_length);
+      CORBA::ULong art_ref_len (mdd.artifactRef.length ());
+      mdd.artifactRef.length (art_ref_len + 1);
+      mdd.artifactRef[art_ref_len] = art_length;
+      update_artifact_location (pack_iad,
+                                plan.artifact[art_length]);
+      update_artifact_property (pack_iad,
+                                plan.artifact[art_length]);
+      update_common_artifact_and_art_ref (pack_iad,
+                                          primary_ref_map, ref_map,
+                                          art_ref_map, mdd,
+                                          plan, instance);
+    }
+}
+
+void update_common_artifact_and_art_ref (Deployment::
+                                         ImplementationArtifactDescription 
+                                         &pack_iad,
+                                         REF_MAP &primary_ref_map,
+                                         REF_MAP &ref_map,
+                                         ART_REF_MAP &art_ref_map,
+                                         Deployment::
+                                         MonolithicDeploymentDescription &mid,
+                                         Deployment::DeploymentPlan &plan,
+                                         Deployment::
+                                         InstanceDeploymentDescription
+                                         &instance)
+{
+  for (CORBA::ULong g = 0;
+       g < pack_iad.dependsOn.length (); ++g)
+    {
+      ACE_TString dep_name =
+         (const char*)pack_iad.dependsOn[g].name;
+      int arti_len;
+
+      if (ref_map.find (dep_name, arti_len) == 0)
+        {
+          if (primary_ref_map.find (dep_name, arti_len) != 0)
+            {
+              if (art_ref_map.find (arti_len, arti_len) != 0)
+                {
+                  update_impl_art_ref (mid, arti_len);
+                  art_ref_map.bind (arti_len, arti_len);
+                }
+            }
+        }
+      else
+        {
+          Deployment::ImplementationArtifactDescription
+             depends_iad = pack_iad.dependsOn[g].
+                  referencedArtifact;
+          CORBA::ULong new_art_length (plan.artifact.length ());
+          plan.artifact.length (new_art_length + 1);
+          plan.artifact[new_art_length].name =
+            pack_iad.dependsOn[g].name;
+          plan.artifact[new_art_length].node = instance.node;
+          update_artifact_location (depends_iad,
+                                    plan.artifact
+                                    [new_art_length]);
+          ref_map.bind (
+             (const char*)plan.artifact[new_art_length].name,
+              new_art_length);
+          update_impl_art_ref (mid, new_art_length);
+          art_ref_map.bind (new_art_length, new_art_length);
+        }
+    }
+}
+
+void update_impl_config_property (Deployment::PackagedComponentImplementation
+                                  &impl,
+                                  Deployment::MonolithicDeploymentDescription
+                                  &mid,
+                                  Deployment::
+                                  InstanceDeploymentDescription
+                                  &instance)
+{
+  for (CORBA::ULong x = 0;
+       x < impl.referencedImplementation.configProperty.length (); ++x)
+    {
+      CORBA::ULong impl_pro_len (mid.execParameter.length ());
+      mid.execParameter.length (impl_pro_len + 1);
+      mid.execParameter[impl_pro_len]
+                = impl.referencedImplementation.configProperty[x];
+      CORBA::ULong ins_pro_len (instance.configProperty. length ());
+      instance.configProperty.length (ins_pro_len + 1);
+      instance.configProperty[ins_pro_len]
+                = impl.referencedImplementation.configProperty[x];
+    }
+}
+
+void update_impl_art_ref (Deployment::MonolithicDeploymentDescription
+                          &mid, int arti_len)
+{
+  CORBA::ULong new_art_ref_len (mid.artifactRef.length ());
+  mid.artifactRef.length (new_art_ref_len + 1);
+  mid.artifactRef[new_art_ref_len] = arti_len;
+}
+
+void update_artifact_location (Deployment::ImplementationArtifactDescription
+                               &pack_iad,
+                               Deployment::ArtifactDeploymentDescription
+                               &plan_artifact)
+{
+  for (CORBA::ULong e = 0; e < pack_iad.location.length (); ++e)
+    {
+      CORBA::ULong art_loc_len (plan_artifact.location.length ());
+      plan_artifact.location.length (art_loc_len + 1);
+      plan_artifact.location[art_loc_len] = pack_iad.location[e];
+    }
+}
+
+void update_artifact_property (Deployment::ImplementationArtifactDescription
+                               &pack_iad,
+                               Deployment::ArtifactDeploymentDescription
+                               &plan_artifact)
+{
+  for (CORBA::ULong f = 0;
+       f < pack_iad.execParameter.length (); ++f)
+    {
+      CORBA::ULong art_pro_len (plan_artifact.execParameter.length ());
+      plan_artifact.execParameter.length (art_pro_len + 1);
+      plan_artifact.execParameter[art_pro_len] = pack_iad.execParameter[f];
     }
 }
