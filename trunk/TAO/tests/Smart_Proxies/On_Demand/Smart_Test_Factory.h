ad1483a208ae4a4676443f2c5aac6718893fbfd7
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-20 18:02:19 +0000

    ChangeLogTag: Sun Jun 20 18:01:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tests/Smart_Proxies/On_Demand/Smart_Test_Factory.h
+++ trunk/TAO/tests/Smart_Proxies/On_Demand/Smart_Test_Factory.h
@@ -6,18 +6,18 @@
 
 class ACE_Svc_Export Smart_Test_Factory
   : public virtual TAO_Test_Default_Proxy_Factory,
-    public ACE_Service_Object
+    public virtual ACE_Service_Object
 
 {
 public:
 
   Smart_Test_Factory (void);
 
+  // This method will create the smart_proxy.
   virtual Test_ptr create_proxy (Test_ptr proxy
                                  ACE_ENV_ARG_DECL_WITH_DEFAULTS);
-  // This method will create the smart_proxy.
 };
 
-// The following Factory is used by the <ACE_Service_Config> to
+// The following Factory is used by the ACE_Service_Config to
 // dynamically initialize the state of the Smart_Proxy_Factory
 ACE_SVC_FACTORY_DECLARE (Smart_Test_Factory)
