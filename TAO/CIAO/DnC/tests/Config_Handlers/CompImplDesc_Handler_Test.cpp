dd2d8cc593d91bbddc029f57dc7d5cc0b45f9dfd
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-06-28 15:43:13 +0000

    *** empty log message ***

--- TAO/CIAO/DnC/tests/Config_Handlers/CompImplDesc_Handler_Test.cpp
+++ TAO/CIAO/DnC/tests/Config_Handlers/CompImplDesc_Handler_Test.cpp
@@ -1,7 +1,10 @@
 // $Id$
 
-#include "Config_Handlers/CompImplDesc_Handler.h"
 #include "Handler_Test.h"
+
+#ifndef ACE_HAS_BROKEN_NESTED_TEMPLATES
+
+#include "Config_Handlers/CompImplDesc_Handler.h"
 
 int
 ACE_TMAIN (int argc, ACE_TCHAR *argv[])
@@ -9,3 +12,5 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
   return run_test <CIAO::Config_Handler::CompImplDesc_Handler, Deployment::ComponentImplementationDescription>
     (argc, argv, &CIAO::Config_Handler::CompImplDesc_Handler::process_ComponentImplementationDescription);
 }
+
+#endif // ACE_HAS_BROKEN_NESTED_TEMPLATES
--- TAO/CIAO/DnC/tests/Config_Handlers/IAD_Handler_Test.cpp
+++ TAO/CIAO/DnC/tests/Config_Handlers/IAD_Handler_Test.cpp
@@ -1,7 +1,10 @@
 // $Id$
 
-#include "Config_Handlers/IAD_Handler.h"
 #include "Handler_Test.h"
+
+#ifndef ACE_HAS_BROKEN_NESTED_TEMPLATES
+
+#include "Config_Handlers/IAD_Handler.h"
 
 int
 ACE_TMAIN (int argc, ACE_TCHAR *argv[])
@@ -9,3 +12,5 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
   return run_test <CIAO::Config_Handler::IAD_Handler, Deployment::ImplementationArtifactDescription>
     (argc, argv, &CIAO::Config_Handler::IAD_Handler::process_ImplementationArtifactDescription);
 }
+
+#endif // ACE_HAS_BROKEN_NESTED_TEMPLATES
