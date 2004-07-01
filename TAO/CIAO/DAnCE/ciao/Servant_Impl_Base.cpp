f659de772218aa475410f1d02a86ef2167befc3c
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-07-01 20:45:24 +0000

    Thu Jul  1 15:40:06 2004  Jaiganesh B  <jai@dre.vanderbilt.edu>

---
+++ TAO/CIAO/DAnCE/ciao/Servant_Impl_Base.cpp
@@ -0,0 +1,55 @@
+// $Id$
+
+#include "Servant_Impl_Base.h"
+
+namespace CIAO
+{
+  Servant_Impl_Base::Servant_Impl_Base (Session_Container * c)
+    : container_ (c)
+  {
+  }
+
+  Servant_Impl_Base::~Servant_Impl_Base (void)
+  {
+  }
+
+  void
+  Servant_Impl_Base::add_facet (const char *port_name,
+                                CORBA::Object_ptr port_ref)
+  {
+    ::Components::FacetDescription *fd = 0;
+    ACE_NEW (fd,
+             OBV_Components::FacetDescription);
+    ::Components::FacetDescription_var safe = fd;
+
+    fd->name (port_name);
+    fd->type_id (port_ref->_interface_repository_id ());
+    fd->facet_ref (port_ref);
+
+    if (this->facet_table_.bind (port_name, fd) == 0)
+      {
+        safe._retn ();
+      }
+  }
+
+  void
+  Servant_Impl_Base::add_consumer (
+      const char *port_name,
+      ::Components::EventConsumerBase_ptr port_ref
+    )
+  {
+    ::Components::ConsumerDescription *cd = 0;
+    ACE_NEW (cd,
+             OBV_Components::ConsumerDescription);
+    ::Components::ConsumerDescription_var safe = cd;
+
+    cd->name (port_name);
+    cd->type_id (port_ref->_interface_repository_id ());
+    cd->consumer (port_ref);
+
+    if (this->consumer_table_.bind (port_name, cd) == 0)
+      {
+        safe._retn ();
+      }
+  }
+}
---
+++ TAO/CIAO/DAnCE/ciao/Servant_Impl_Base.h
@@ -0,0 +1,80 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    Servant_Impl_Base.h
+ *
+ *  $Id$
+ *
+ *  This file contains the non-template declaration of a base class for
+ *  the template mixin for the generated servant class.
+ *
+ *  @author Jeff Parsons <j.parsons@vanderbilt.edu>
+ */
+//=============================================================================
+
+
+#ifndef CIAO_SERVANT_IMPL_BASE_H
+#define CIAO_SERVANT_IMPL_BASE_H
+
+#include /**/ "ace/pre.h"
+
+#include "ace/Hash_Map_Manager_T.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "CCM_EventC.h"
+#include "CIAO_Server_Export.h"
+
+namespace CIAO
+{
+  class Session_Container;
+
+  /**
+   * @class Servant_Impl_Base
+   *
+   * @brief Non-template base class for Servant_Impl.
+   *
+   * Holds the non-template parts of its child class
+   * Servant_Impl.
+   */
+  class CIAO_SERVER_Export Servant_Impl_Base
+  {
+  public:
+    Servant_Impl_Base (Session_Container * c);
+
+    virtual ~Servant_Impl_Base (void);
+
+  protected:
+    void add_facet (const char *port_name,
+                    CORBA::Object_ptr port_ref);
+
+    void add_consumer (const char *port_name,
+                       ::Components::EventConsumerBase_ptr port_ref);
+
+  protected:
+    typedef ACE_Hash_Map_Manager_Ex<const char *,
+                                    ::Components::FacetDescription_var,
+                                    ACE_Hash<const char *>,
+                                    ACE_Equal_To<const char *>,
+                                    ACE_Null_Mutex>
+       FacetTable;
+
+    typedef ACE_Hash_Map_Manager_Ex<const char *,
+                                    ::Components::ConsumerDescription_var,
+                                    ACE_Hash<const char *>,
+                                    ACE_Equal_To<const char *>,
+                                    ACE_Null_Mutex>
+       ConsumerTable;
+
+    FacetTable facet_table_;
+    ConsumerTable consumer_table_;
+    Session_Container * container_;
+  };
+}
+
+#include /**/ "ace/post.h"
+
+#endif /* CIAO_SERVANT_IMPL_T_H */
