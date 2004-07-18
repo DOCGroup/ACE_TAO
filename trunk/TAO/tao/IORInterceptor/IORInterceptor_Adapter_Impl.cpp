c81d81310bfddfe7fcd72c7b94d08b960c62ce2e
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-07-18 16:26:20 +0000

    Sun Jul 18 08:54:29 2004  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- trunk/TAO/tao/IORInterceptor/IORInterceptor_Adapter_Impl.cpp
+++ trunk/TAO/tao/IORInterceptor/IORInterceptor_Adapter_Impl.cpp
@@ -1,10 +1,12 @@
 // $Id$
 
 #include "IORInterceptor_Adapter_Impl.h"
+#include "tao/debug.h"
 
 ACE_RCSID (IORInterceptor,
            IORInterceptor_Adapter_Impl,
            "$Id$")
+
 
 TAO_IORInterceptor_Adapter_Impl::~TAO_IORInterceptor_Adapter_Impl (void)
 {
@@ -32,23 +34,39 @@ TAO_IORInterceptor_Adapter_Impl::destroy_interceptors (ACE_ENV_SINGLE_ARG_DECL)
   const size_t len = i.size ();
   size_t ilen = len;
 
-  for (size_t k = 0; k < len; ++k)
+  ACE_TRY
     {
-      // Destroy the interceptors in reverse order in case the array
-      // list is only partially destroyed and another invocation
-      // occurs afterwards.
-      --ilen;
+      for (size_t k = 0; k < len; ++k)
+        {
+          // Destroy the interceptors in reverse order in case the
+          // array list is only partially destroyed and another
+          // invocation occurs afterwards.
+          --ilen;
 
-      i[k].in()->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_CHECK;
+          i[k]->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
 
-      // Since Interceptor::destroy() can throw an exception, decrease
-      // the size of the interceptor array incrementally since some
-      // interceptors may not have been destroyed yet.  Note that this
-      // size reduction is fast since no memory is actually
-      // deallocated.
-      i.size (ilen);
+          // Since Interceptor::destroy() can throw an exception,
+          // decrease the size of the interceptor array incrementally
+          // since some interceptors may not have been destroyed yet.
+          // Note that this size reduction is fast since no memory is
+          // actually deallocated.
+          i.size (ilen);
+        }
     }
+  ACE_CATCHALL
+    {
+      // Exceptions should not be propagated beyond this call.
+      if (TAO_debug_level > 3)
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      ACE_TEXT ("TAO (%P|%t) - Exception in ")
+                      ACE_TEXT ("IORInterceptor_Adapter_Impl")
+                      ACE_TEXT ("::destroy_interceptors () \n")));
+        }
+    }
+  ACE_ENDTRY;
+  ACE_CHECK;
 
   delete this;
 }
