f3ae469ab648de13a1fde9bd1c43c1e18357324e
huangming <huangming@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-08-09 14:35:22 +0000

    ChangeLogTag:Mon Aug  9 09:25:12 2004  Huang-Ming Huang  <hh1@cse.wustl.edu>

--- trunk/tests/Dynamic_Priority_Test.cpp
+++ trunk/tests/Dynamic_Priority_Test.cpp
@@ -411,6 +411,7 @@ performance_producer (void *args)
 
       // Set a character in the current message block at its
       // read pointer position, and adjust the write pointer.
+      mb->reset();
       *mb->wr_ptr () = 'a';
       mb->wr_ptr (1);
 
