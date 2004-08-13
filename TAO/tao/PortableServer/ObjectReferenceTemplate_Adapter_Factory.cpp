543b68f7892e882cc4b5b7f380d2331968c10b3c
Johnny Willemsen <jwillemsen@remedy.nl>
2004-08-13 18:00:08 +0000

    ChangeLogTag: Fri Aug 13 18:00:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ TAO/tao/PortableServer/ObjectReferenceTemplate_Adapter_Factory.cpp
@@ -0,0 +1,24 @@
+// $Id$
+
+#include "tao/ObjectReferenceTemplate_Adapter_Factory.h"
+#include "ace/Dynamic_Service.h"
+
+ACE_RCSID (PortableServer,
+           ObjectReferenceTemplate_Adapter_Factory,
+           "$Id$")
+
+TAO_ObjectReferenceTemplate_Adapter_Factory::~TAO_ObjectReferenceTemplate_Adapter_Factory (void)
+{
+}
+
+#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+
+template class ACE_Dynamic_Service<TAO_ObjectReferenceTemplate_Adapter_Factory>;
+
+#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
+
+#pragma instantiate ACE_Dynamic_Service<TAO_ObjectReferenceTemplate_Adapter_Factory>
+
+#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+
+
