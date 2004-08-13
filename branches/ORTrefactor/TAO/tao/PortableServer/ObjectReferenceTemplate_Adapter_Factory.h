f2340b8d9fb12d56a1761dcab3a47b26e3dd9ace
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-08-13 18:00:08 +0000

    ChangeLogTag: Fri Aug 13 18:00:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ branches/ORTrefactor/TAO/tao/PortableServer/ObjectReferenceTemplate_Adapter_Factory.h
@@ -0,0 +1,52 @@
+// This may look like C, but it's really -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    ObjectReferenceTemplate_Adapter_Factory.h
+ *
+ *  $Id$
+ *
+ *  @author  Johnny Willemsen  <jwillemsen@remedy.nl>
+ */
+//=============================================================================
+
+
+#ifndef TAO_OBJECTREFERENCETEMPLATE_ADAPTER_FACTORY_H
+#define TAO_OBJECTREFERENCETEMPLATE_ADAPTER_FACTORY_H
+
+#include /**/ "ace/pre.h"
+#include "ace/Service_Object.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/Exception.h"
+
+class TAO_ObjectReferenceTemplate_Adapter;
+
+/**
+ * @class TAO_ObjectReferenceTemplate_Adapter_Factory
+ *
+ * @brief TAO_ObjectReferenceTemplate_Adapter_Factory.
+ *
+ * Class that creates one instance of TAO_ObjectReferenceTemplate_Adapter per
+ * POA on the POA's first usage of its objectreferencetemplate_adapter_.
+ * This is a base class for the actual implementation in the
+ * TAO_ObjectReferenceTemplate library.
+ */
+class TAO_Export TAO_ObjectReferenceTemplate_Adapter_Factory
+  : public ACE_Service_Object
+{
+public:
+  virtual ~TAO_ObjectReferenceTemplate_Adapter_Factory (void);
+
+  virtual TAO_ObjectReferenceTemplate_Adapter * create (
+      ACE_ENV_SINGLE_ARG_DECL
+    )
+    ACE_THROW_SPEC ((CORBA::SystemException)) = 0;
+};
+
+#include /**/ "ace/post.h"
+
+#endif /* TAO_OBJECTREFERENCETEMPLATE_ADAPTER_FACTORY_H */
