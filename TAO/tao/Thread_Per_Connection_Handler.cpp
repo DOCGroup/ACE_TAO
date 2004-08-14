2a2b0abf3e8aa1340381a9270ecabc563e767e8a
Ossama Othman <ossama-othman@users.noreply.github.com>
2004-08-14 07:01:16 +0000

    ChangeLogTag:Fri Aug 13 23:58:50 2004  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- TAO/tao/Thread_Per_Connection_Handler.cpp
+++ TAO/tao/Thread_Per_Connection_Handler.cpp
@@ -61,9 +61,8 @@ TAO_Thread_Per_Connection_Handler::activate (long flags,
 int
 TAO_Thread_Per_Connection_Handler::svc (void)
 {
-  ACE_Flag_Manip::clr_flags (
-      this->ch_->transport ()->event_handler_i ()->get_handle (),
-      ACE_NONBLOCK);
+  ACE::clr_flags (this->ch_->transport ()->event_handler_i ()->get_handle (),
+                  ACE_NONBLOCK);
 
   // Call the implementation here
   return this->ch_->svc_i ();
