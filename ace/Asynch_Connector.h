93c3081d0460665a9045572ae3b79bbe049f313c
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-08-18 08:51:59 +0000

    ChangeLogTag: Wed Aug 18 08:50:00 UTC 2004  Simon Massey  <simon.massey@prismtechnologies.com>

--- ace/Asynch_Connector.h
+++ ace/Asynch_Connector.h
@@ -44,6 +44,7 @@ template <class HANDLER>
 class ACE_Asynch_Connector : public ACE_Handler
 {
 public:
+  static const ACE_INET_Addr local_default;
 
   /// A do nothing constructor.
   ACE_Asynch_Connector (void);
@@ -60,8 +61,7 @@ public:
 
   /// This initiates a new asynchronous connect
   virtual int connect (const ACE_INET_Addr &remote_sap,
-                       const ACE_INET_Addr &local_sap =
-                                   ACE_INET_Addr ((u_short)0),
+                       const ACE_INET_Addr &local_sap = local_default,
                        int reuse_addr = 1,
                        const void *act = 0);
 
