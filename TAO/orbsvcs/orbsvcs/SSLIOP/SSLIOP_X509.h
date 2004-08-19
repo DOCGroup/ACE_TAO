7ec01074d0e114ef11046b9b4db0645521ef10c0
Ossama Othman <ossama-othman@users.noreply.github.com>
2004-08-19 06:20:11 +0000

    ChangeLogTag:Wed Aug 18 23:09:29 2004  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_X509.h
+++ TAO/orbsvcs/orbsvcs/SSLIOP/SSLIOP_X509.h
@@ -47,12 +47,12 @@ namespace TAO
        */
       static ::X509 * _duplicate (::X509 * st)
       {
-	if (st != 0)
-	  CRYPTO_add (&(st->references),
-		      1,
-		      LOCK_ID);
+        if (st != 0)
+          CRYPTO_add (&(st->references),
+                      1,
+                      LOCK_ID);
 
-	return st;
+        return st;
       }
 
       /// Perform deep copy of the given OpenSSL structure.
