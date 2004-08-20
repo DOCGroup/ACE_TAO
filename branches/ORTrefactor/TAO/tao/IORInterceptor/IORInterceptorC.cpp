167e4657b4dee38bfff8881ac38be2455fe06ce7
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-08-20 07:59:56 +0000

    ChangeLogTag:  Fri Aug 20 07:59:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/ORTrefactor/TAO/tao/IORInterceptor/IORInterceptorC.cpp
+++ branches/ORTrefactor/TAO/tao/IORInterceptor/IORInterceptorC.cpp
@@ -31,7 +31,6 @@
 
 #include "IORInterceptorC.h"
 #include "tao/CDR.h"
-#include "tao/Object_Argument_T.h"
 #include "ace/OS_NS_string.h"
 
 #if defined (__BORLANDC__)
@@ -48,162 +47,8 @@
 // Arg traits specializations.
 namespace TAO
 {
-
-#if !defined (_PORTABLEINTERCEPTOR_OBJECTREFERENCEFACTORY__ARG_TRAITS_CS_)
-#define _PORTABLEINTERCEPTOR_OBJECTREFERENCEFACTORY__ARG_TRAITS_CS_
-  
-  ACE_TEMPLATE_SPECIALIZATION
-  class TAO_IORInterceptor_Export Arg_Traits<PortableInterceptor::ObjectReferenceFactory>
-    : public
-        Object_Arg_Traits_T<
-            PortableInterceptor::ObjectReferenceFactory *,
-            PortableInterceptor::ObjectReferenceFactory_var,
-            PortableInterceptor::ObjectReferenceFactory_out,
-            TAO::Value_Traits<PortableInterceptor::ObjectReferenceFactory>
-          >
-  {
-  };
-
-#endif /* end #if !defined */
 }
 
-
-// TAO_IDL - Generated from
-// C:\ACE\develop\ACE_wrappers\TAO\TAO_IDL\be\be_visitor_interface/interface_cs.cpp:60
-
-// Traits specializations for PortableInterceptor::IORInfo.
-
-ACE_TEMPLATE_CLASS_MEMBER_SPECIALIZATION
-PortableInterceptor::IORInfo_ptr
-TAO::Objref_Traits<PortableInterceptor::IORInfo>::tao_duplicate (
-    PortableInterceptor::IORInfo_ptr p
-  )
-{
-  return PortableInterceptor::IORInfo::_duplicate (p);
-}
-
-ACE_TEMPLATE_CLASS_MEMBER_SPECIALIZATION
-void
-TAO::Objref_Traits<PortableInterceptor::IORInfo>::tao_release (
-    PortableInterceptor::IORInfo_ptr p
-  )
-{
-  CORBA::release (p);
-}
-
-ACE_TEMPLATE_CLASS_MEMBER_SPECIALIZATION
-PortableInterceptor::IORInfo_ptr
-TAO::Objref_Traits<PortableInterceptor::IORInfo>::tao_nil (void)
-{
-  return PortableInterceptor::IORInfo::_nil ();
-}
-
-ACE_TEMPLATE_CLASS_MEMBER_SPECIALIZATION
-CORBA::Boolean
-TAO::Objref_Traits<PortableInterceptor::IORInfo>::tao_marshal (
-    PortableInterceptor::IORInfo_ptr p,
-    TAO_OutputCDR & cdr
-  )
-{
-  return CORBA::Object::marshal (p, cdr);
-}
-
-// Function pointer for collocation factory initialization.
-TAO::Collocation_Proxy_Broker * 
-(*PortableInterceptor__TAO_IORInfo_Proxy_Broker_Factory_function_pointer) (
-    CORBA::Object_ptr obj
-  ) = 0;
-
-PortableInterceptor::IORInfo::IORInfo (void)
-{}
-
-PortableInterceptor::IORInfo::~IORInfo (void)
-{}
-
-PortableInterceptor::IORInfo_ptr
-PortableInterceptor::IORInfo::_narrow (
-    CORBA::Object_ptr _tao_objref
-    ACE_ENV_ARG_DECL_NOT_USED
-  )
-{
-  if (CORBA::is_nil (_tao_objref))
-    {
-      return IORInfo::_nil ();
-    }
-  
-  IORInfo_ptr proxy =
-    dynamic_cast<IORInfo_ptr> (_tao_objref);
-  
-  return IORInfo::_duplicate (proxy);
-}
-
-PortableInterceptor::IORInfo_ptr
-PortableInterceptor::IORInfo::_unchecked_narrow (
-    CORBA::Object_ptr _tao_objref
-    ACE_ENV_ARG_DECL_NOT_USED
-  )
-{
-  if (CORBA::is_nil (_tao_objref))
-    {
-      return IORInfo::_nil ();
-    }
-  
-  IORInfo_ptr proxy =
-    dynamic_cast<IORInfo_ptr> (_tao_objref);
-  
-  return IORInfo::_duplicate (proxy);
-}
-
-PortableInterceptor::IORInfo_ptr
-PortableInterceptor::IORInfo::_duplicate (IORInfo_ptr obj)
-{
-  if (! CORBA::is_nil (obj))
-    {
-      obj->_add_ref ();
-    }
-  
-  return obj;
-}
-
-CORBA::Boolean
-PortableInterceptor::IORInfo::_is_a (
-    const char *value
-    ACE_ENV_ARG_DECL_NOT_USED
-  )
-{
-  if (
-      !ACE_OS::strcmp (
-          (char *)value,
-          "IDL:omg.org/PortableInterceptor/IORInfo:1.0"
-        ) ||
-      !ACE_OS::strcmp (
-          (char *)value,
-          "IDL:omg.org/CORBA/LocalObject:1.0"
-        ) ||
-      !ACE_OS::strcmp (
-          (char *)value,
-          "IDL:omg.org/CORBA/Object:1.0"
-        )
-     )
-    {
-      return 1; // success using local knowledge
-    }
-  else
-    {
-      return 0;
-    }
-}
-
-const char* PortableInterceptor::IORInfo::_interface_repository_id (void) const
-{
-  return "IDL:omg.org/PortableInterceptor/IORInfo:1.0";
-}
-
-CORBA::Boolean
-PortableInterceptor::IORInfo::marshal (TAO_OutputCDR &)
-{
-  return 0;
-}
 
 // TAO_IDL - Generated from
 // C:\ACE\develop\ACE_wrappers\TAO\TAO_IDL\be\be_visitor_interface/interface_cs.cpp:60
@@ -366,39 +211,6 @@ PortableInterceptor::IORInterceptor::marshal (TAO_OutputCDR &)
         PortableInterceptor::IORInterceptor
       >;
 
-#if !defined (_PORTABLEINTERCEPTOR_OBJECTREFERENCEFACTORY__ARG_TRAITS_TMPLINST_CS_)
-#define _PORTABLEINTERCEPTOR_OBJECTREFERENCEFACTORY__ARG_TRAITS_TMPLINST_CS_
-
-  template class
-     TAO::Arg_Traits<
-        PortableInterceptor::ObjectReferenceFactory
-      >;
-
-  template class
-    TAO::Object_Arg_Traits_T<
-        PortableInterceptor::ObjectReferenceFactory *,
-        PortableInterceptor::ObjectReferenceFactory_var,
-        PortableInterceptor::ObjectReferenceFactory_out,
-        TAO::Objref_Traits<PortableInterceptor::ObjectReferenceFactory> 
-      >;
-
-#endif /* end #if !defined */
-
-  template class
-    TAO::Objref_Traits<
-        PortableInterceptor::IORInfo
-      >;
-
-  template class
-    TAO_Objref_Var_T<
-        PortableInterceptor::IORInfo
-      >;
-  
-  template class
-    TAO_Objref_Out_T<
-        PortableInterceptor::IORInfo
-      >;
-
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
 # pragma instantiate \
@@ -414,39 +226,6 @@ PortableInterceptor::IORInterceptor::marshal (TAO_OutputCDR &)
 # pragma instantiate \
     TAO_Objref_Out_T< \
         PortableInterceptor::IORInterceptor
-      >
-
-#if !defined (_PORTABLEINTERCEPTOR_OBJECTREFERENCEFACTORY__ARG_TRAITS_TMPLINST_CS_)
-#define _PORTABLEINTERCEPTOR_OBJECTREFERENCEFACTORY__ARG_TRAITS_TMPLINST_CS_
-
-# pragma instantiate \
-     TAO::Arg_Traits< \
-        PortableInterceptor::ObjectReferenceFactory \
-      >
-
-# pragma instantiate \
-    TAO::Object_Arg_Traits_T< \
-        PortableInterceptor::ObjectReferenceFactory *, \
-        PortableInterceptor::ObjectReferenceFactory_var, \
-        PortableInterceptor::ObjectReferenceFactory_out, \
-        TAO::Objref_Traits<PortableInterceptor::ObjectReferenceFactory>  \
-      >
-
-#endif /* end #if !defined */
-
-# pragma instantiate \
-    TAO::Objref_Traits< \
-        PortableInterceptor::IORInfo \
-      >
-
-# pragma instantiate \
-    TAO_Objref_Var_T< \
-        PortableInterceptor::IORInfo
-      >
-  
-# pragma instantiate \
-    TAO_Objref_Out_T< \
-        PortableInterceptor::IORInfo
       >
 
 #endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */ 
