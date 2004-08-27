a8235d0791dae8e4827790a736e0269ad9d2583f
Johnny Willemsen <jwillemsen@remedy.nl>
2004-08-27 09:35:32 +0000

    ChangeLogTag:  Fri Aug 27 09:35:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- examples/Service_Configurator/IPC-tests/server/Handle_Timeout.h
+++ examples/Service_Configurator/IPC-tests/server/Handle_Timeout.h
@@ -21,8 +21,8 @@ class ACE_Svc_Export Handle_Timeout : public ACE_Service_Object
 {
 public:
   Handle_Timeout (void);
-  virtual int init (int argc, char *argv[]);
-  virtual int info (char **, size_t) const;
+  virtual int init (int argc, ACE_TCHAR *argv[]);
+  virtual int info (ACE_TCHAR **, size_t) const;
   virtual int fini (void);
 
 private:
