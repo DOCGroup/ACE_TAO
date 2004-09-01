7ffed51c2ad9c0485ea316763f33a17cf1556225
jtc <jtc@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-01 17:06:20 +0000

    ChangeLogTag: Wed Sep  1 10:01:57 2004  J.T. Conklin  <jtc@acorntoolworks.com>

--- ace/SOCK_Dgram_Bcast.cpp
+++ ace/SOCK_Dgram_Bcast.cpp
@@ -162,7 +162,7 @@ ACE_SOCK_Dgram_Bcast::mk_broadcast (const ACE_TCHAR *host_name)
     }
 
 
-#if !defined(CHORUS_4) && !defined(AIX) && !defined (__QNX__) && !defined (__FreeBSD__)
+#if !defined(CHORUS_4) && !defined(AIX) && !defined (__QNX__) && !defined (__FreeBSD__) && !defined(__NetBSD__)
   for (int n = ifc.ifc_len / sizeof (struct ifreq) ; n > 0;
        n--, ifr++)
 #else
@@ -180,7 +180,7 @@ ACE_SOCK_Dgram_Bcast::mk_broadcast (const ACE_TCHAR *host_name)
             ifr = (struct ifreq *)
               ((caddr_t) &ifr->ifr_addr + ifr->ifr_addr.sa_len)) :
           (nbytes -= sizeof (struct ifreq), ifr++)))
-#endif /* !defined(CHORUS_4) && !defined(AIX) && !defined (__QNX__) && !defined (__FreeBSD__) */
+#endif /* !defined(CHORUS_4) && !defined(AIX) && !defined (__QNX__) && !defined (__FreeBSD__) && !defined(__NetBSD__) */
     {
 #if defined (__QNX__)
       // Silently skip link interfaces
