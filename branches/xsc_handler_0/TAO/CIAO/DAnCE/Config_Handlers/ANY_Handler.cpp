d4e56d692e315ddc031670d857afef7d457a45f6
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-02 17:58:17 +0000

    *** empty log message ***

--- branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/ANY_Handler.cpp
+++ branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/ANY_Handler.cpp
@@ -1,77 +1,96 @@
 // $Id$
 
 #include "ANY_Handler.h"
+#include "tao/Any.h"
 
-CIAO::Config_Handlers::ANY_Handler::ANY_Handler()
-{}
-CIAO::Config_Handlers::ANY_Handler::~ANY_Handler()
-{}
+namespace CIAO
+{
+  namespace Config_Handlers
+  {
+
+    ANY_Handler::ANY_Handler (void)
+    {
+    }
+
+    ANY_Handler::~ANY_Handler (void)
+    {
+    }
 
 
+    void
+    ANY_Handler::get_any (CORBA::Any& toconfig,
+                          Any& desc)
+    {
 
-using CIAO::Config_Handlers;
+      // Get the value that should be assigned to the Any.
+      DataValue value =
+        desc.value ();
 
-CORBA::Any& 
-CIAO::Config_Handlers::ANY_Handler::get_Any(
-                        CORBA::Any& toconfig, Any& desc)
-{ 
- 
- //Get the value that should be assigned to the Any.
- DataValue value = desc.value();
- 
- //Here, we check each type to see if 
- //it is present. If a type is listed as 
- //present, we can assume that it is the 
- //intended value for the Any. This relieves
- //us from the burden of having to check the
- //type field on <desc>.
- if(value.short_p()){
-    toconfig <<= ACE_static_cast (CORBA::Short, 
-                                CORBA::Short(value.short_()));
- }
-  if(value.ushort_p()){
-    toconfig <<= ACE_static_cast (CORBA::UShort,
-                                CORBA::UShort(value.ushort()));
- }
- if(value.long_p()){
-    toconfig <<= ACE_static_cast (CORBA::Long,
-                                CORBA::Long(value.long_()));
- }
- if(value.ulong_p()){
-    toconfig <<= ACE_static_cast (CORBA::ULong,
-                                CORBA::ULong(value.ulong()));
- }
- if(value.boolean_p()){
-    toconfig <<= ACE_static_cast (CORBA::Boolean,
-                                CORBA::Boolean(value.boolean()));
- }
- if(value.double_p()){
-    toconfig <<= ACE_static_cast (CORBA::Double, 
-                                CORBA::Double(value.double_()));
- }
- if(value.float_p()){
-    toconfig <<= ACE_static_cast (CORBA::Float, 
-                                CORBA::Float(value.float_()));
- }
- if(value.string_p()){
-    toconfig <<= CORBA::string_dup (value.string().c_str());
- }
- if(value.octet_p()){
-    toconfig <<= CORBA::Any::from_octet ( 
-                                CORBA::Octet(value.octet()));
- }
- if(value.longlong_p()){
-    toconfig <<= ACE_static_cast (CORBA::LongLong, 
-                                CORBA::LongLong(value.longlong()));
- }
- if(value.ulonglong_p()){
-    toconfig <<= ACE_static_cast (CORBA::ULongLong, 
-                                CORBA::ULongLong(value.ulonglong()));
- }
+      // @@ Jules, you may want to align your coding standards to ACE
+      // coding standards. Else, Dr. Schmidt is going to come down on
+      // you heavily in CS291 :-)
+      if (value.short_p ())
+        {
+          // @@ Jules, please try using regular C++ casts
+          toconfig <<=
+            static_cast <CORBA::Short> (value.short ());
+        }
+      else if (value.ushort_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::UShort> (value.ushort ());
+        }
+      else if (value.long_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::UShort> (value.long ());
+        }
+      else if (value.ulong_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::UShort> (value.ulong ());
+        }
+      else if (value.boolean_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::Boolean> (value.boolean ());
+        }
+      else if (value.double_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::Double> (value.double ());
+        }
+      else if (value.float_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::Float> (value.float_ ());
+        }
+      else if (value.string_p ())
+        {
+          toconfig <<=
+            value.string ().c_str ();
+        }
+      else if (value.octet_p ())
+        {
+          toconfig <<=
+            CORBA::Any::from_octet (static_cast<CORBA::Octet> (value.octet ()));
+        }
+      else if (value.longlong_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::LongLong> (value.longlong ());
+        }
+      else if (value.ulonglong_p ())
+        {
+          toconfig <<=
+            static_cast <CORBA::ULongLong> (value.ulonglong ());
+        }
  //if(value.longdouble_p()){
- //   toconfig <<= ACE_static_cast (CORBA::LongDouble, 
+ //   toconfig <<= ACE_static_cast (CORBA::LongDouble,
  //                               CORBA::LongDouble(value.longdouble()));
  // }
- 
- return toconfig;
+
+      return toconfig;
+    }
+  }
 }
--- branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/ANY_Handler.h
+++ branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/ANY_Handler.h
@@ -1,4 +1,4 @@
-//================================================
+//==============================================================
 /**
  *  @file  ANY_Handler.h
  *
@@ -6,51 +6,55 @@
  *
  *  @author Jules White <jules@dre.vanderbilt.edu>
  */
-//================================================
+//================================================================
 
-#ifndef ANY_HANDLER_H
-#define ANY_HANDLER_H
-
+#ifndef CIAO_CONFIG_HANDLERS_ANY_HANDLER_H
+#define CIAO_CONFIG_HANDLERS_ANY_HANDLER_H
 #include /**/ "ace/pre.h"
-                                                                                
-#include "ciao/DeploymentC.h"
+
 #include "Basic_Deployment_Data.hpp"
-             
-                                                                                
+
+
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+namespace CORBA
+{
+  class Any;
+}
 
-namespace CIAO{
-    
-  namespace Config_Handlers{
+namespace CIAO
+{
 
-
+  namespace Config_Handlers
+  {
    /*
     * @class ANY_Handler
-    * 
+    *
     * @brief Handler class for <ComponentInterfaceDescription> types.
-    * 
-    * This class defines handler methods to map values from 
+    *
+    * This class defines handler methods to map values from
     * XSC Any objects, parsed from the descriptor files, to the
     * corresponding CORBA IDL Any type.
-    * 
+    *
     */
-    
-    class ANY_Handler{
-     
-      public:
-       
-      ANY_Handler();
-      virtual ~ANY_Handler();
-  
-      static CORBA::Any& get_Any(CORBA::Any& toconfig, Any& desc); 
-         
+
+    class ANY_Handler
+    {
+    public:
+
+      ANY_Handler (void);
+
+      virtual ~ANY_Handler (void);
+
+      static void get_any(CORBA::Any& toconfig,
+                          Any& desc);
+
     };
 
   }
-   
 }
+
 #include /**/ "ace/post.h"
-#endif /* ANY_HANDLER_H */
+#endif /* CIAO_CONFIG_HANDLERS_ANY_HANDLER_H*/
