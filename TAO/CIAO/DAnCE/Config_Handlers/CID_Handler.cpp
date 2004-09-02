b1d2d17ed16cba14d80e42d4bc9ff47f79850b26
bala <balanatarajan@users.noreply.github.com>
2004-09-02 17:58:17 +0000

    *** empty log message ***

--- TAO/CIAO/DAnCE/Config_Handlers/CID_Handler.cpp
+++ TAO/CIAO/DAnCE/Config_Handlers/CID_Handler.cpp
@@ -6,49 +6,55 @@
 
 typedef ::std::vector< ::CIAO::Config_Handlers::ComponentPortDescription >::iterator port_iterator;
 
-CIAO::Config_Handlers::CID_Handler::CID_Handler()
-{}
-CIAO::Config_Handlers::CID_Handler::~CID_Handler()
-{}
-
-Deployment::ComponentInterfaceDescription&
-CIAO::Config_Handlers::CID_Handler::get_ComponentInterfaceDescription
-(Deployment::ComponentInterfaceDescription& toconfig,
- ComponentInterfaceDescription& desc)
+namespace CIAO
 {
-  
-    //We first set up some of the basic
-    //properties such as UUID. We check
-    //to make sure each one is actually
-    //present before assigning it to 
-    //the corresponding field in <toconfig>.
-    if( desc.UUID_p() )
+  namespace Config_Handlers
+  {
+    CID_Handler::CID_Handler (void)
     {
-        toconfig.UUID = 
-            CORBA::string_dup (desc.UUID().c_str());
     }
-    if( desc.label_p() ){
-        toconfig.label = 
-            CORBA::string_dup (desc.label().c_str());
+
+    CID_Handler::~CID_Handler (void)
+    {
     }
-    if( desc.specificType_p() ){
-        toconfig.specificType = 
-            CORBA::string_dup (desc.specificType().c_str());
-    }
-    
-    
-    if(desc.supportedType_p()){
-        //Copy the values of the sequence types idlFile and 
-        //supportedType to the 
+
+    bool
+    CID_Handler::get_component_interface_descr (
+        Deployment::ComponentInterfaceDescription& toconfig,
+        ComponentInterfaceDescription& desc)
+    {
+
+    // We first set up some of the basic properties such as UUID. We
+    // check to make sure each one is actually present before
+    // assigning it to the corresponding field in <toconfig>.
+    if(desc.UUID_p ())
+      {
+        toconfig.UUID =
+          CORBA::string_dup (desc.UUID ().c_str ());
+      }
+    else if(desc.label_p ())
+      {
+        toconfig.label =
+          CORBA::string_dup (desc.label ().c_str ());
+      }
+    else if(desc.specificType_p ())
+      {
+        toconfig.specificType =
+          CORBA::string_dup (desc.specificType ().c_str ());
+      }
+    else if (desc.supportedType_p ())
+      {
+        //Copy the values of the sequence types idlFile and
+        //supportedType to the
         //<Deployment::ComponentInterfaceDescription>.
         if(!desc.supportedType().empty())
         {
          toconfig.supportedType.length (1);
-         toconfig.supportedType[0] = 
+         toconfig.supportedType[0] =
                   CORBA::string_dup (desc.supportedType().c_str());
         }
     }
-    
+
     //Make sure the <idlFile> property
     //is actually present before attempting
     //any operations on it.
@@ -56,17 +62,17 @@ CIAO::Config_Handlers::CID_Handler::get_ComponentInterfaceDescription
         if(!desc.idlFile().empty())
         {
          toconfig.idlFile.length (1);
-         toconfig.idlFile[0] = 
+         toconfig.idlFile[0] =
                   CORBA::string_dup (desc.idlFile().c_str());
         }
     }
-    
-    
+
+
     Prop_Handler propertyhandler;
-    
+
     //The IDL for the <configProperty> specifies
     //a sequence of <Property> structs but the schema
-    //specifies <configProperty> as a single 
+    //specifies <configProperty> as a single
     //<Property>. We construct that single property
     //element and assign it to the first position in the
     //<configProperty> sequence. We only do this if it
@@ -76,19 +82,19 @@ CIAO::Config_Handlers::CID_Handler::get_ComponentInterfaceDescription
      //First construct the <Deployment::Property>
      //to configure.
      Deployment::Property prop;
-     
+
      //Now, propogate the values from the <desc> into <prop>.
      propertyhandler.get_Property (prop,desc.configProperty());
-     
+
      //Finally, add it to the sequence.
      toconfig.configProperty.length (1);
      toconfig.configProperty[0] = prop;
     }
-    
+
     //Create the handler for the
     //<ComponentPortDescriptions>.
-	CPD_Handler cpd_handler; 
-	
+        CPD_Handler cpd_handler;
+
     //Iterate through each of the XSC Component
     //Port Descriptions and use the CPD_Handler
     //to propogate their values into the <port>
@@ -101,23 +107,23 @@ CIAO::Config_Handlers::CID_Handler::get_ComponentInterfaceDescription
          toconfig.port.length (toconfig.port.length () + 1);
          cpd_handler.get_ComponentPortDescription(
                      toconfig.port[toconfig.port.length () - 1],
-                     *port); 
+                     *port);
     }
-    
+
     /*ComponentPropertyDescription stuff
-     * 
-     * 
-     * 
-     * 
-     * 
+     *
+     *
+     *
+     *
+     *
      */
-     
-     
-     
-     
-        
-    
-    
-  
-	return toconfig; 	
+
+
+
+
+
+
+
+
+        return toconfig;
 }
--- TAO/CIAO/DAnCE/Config_Handlers/CID_Handler.h
+++ TAO/CIAO/DAnCE/Config_Handlers/CID_Handler.h
@@ -8,60 +8,58 @@
  */
 //================================================
 
-#ifndef CID_HANDLER_H
-#define CID_HANDLER_H
+#ifndef CIAO_CONFIG_HANDLERS_CID_HANDLER_H
+#define CIAO_CONFIG_HANDLERS_CID_HANDLER_H
 
 #include /**/ "ace/pre.h"
-                                                                                
-#include "ciao/DeploymentC.h"
-#include "ccd.hpp"
-#include "Basic_Deployment_Data.hpp"
-             
-                                                                                
+
+#include "cid.hpp"
+
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+namespace Deployment
+{
+  namespace ComponentInterfaceDescription;
+}
 
-namespace CIAO{
-	
-  namespace Config_Handlers{
-
-
+namespace CIAO
+{
+  namespace Config_Handlers
+  {
    /*
     * @class CID_Handler
-    * 
+    *
     * @brief Handler class for <ComponentInterfaceDescription> types.
-    * 
-    * This class defines handler methods to map values from 
+    *
+    * This class defines handler methods to map values from
     * XSC objects, parsed from the descriptor files, to the
     * corresponding CORBA IDL type for the schema element.
-    * 
+    *
     */
-    
-    class CID_Handler{
-     
+    class CID_Handler
+    {
       public:
-		
-      CID_Handler();
-	  virtual ~CID_Handler();
-	
-      ///This method maps the values from the
-      ///XSC object <ComponentPortDescription> to
-      ///the CORBA IDL type <Deployment::ComponentPortDescription>.   
-      ///It handles the creation of the Deployment::ComponentPort's,
-      ///Deployment::ComponentPortDescriptions's, and 
-      ///Deployment::Properties and delegates mapping the values
-      ///from their corresponding XSC objects to their handlers.     
-      Deployment::ComponentInterfaceDescription&
-      get_ComponentInterfaceDescription(
-		  Deployment::ComponentInterfaceDescription& toconfig,
+
+      CID_Handler (void);
+      virtual ~CID_Handler(void);
+
+      /// This method maps the values from the XSC object
+      /// <ComponentPortDescription> to the CORBA IDL type
+      /// <Deployment::ComponentPortDescription>. It handles the
+      /// creation of the Deployment::ComponentPort's,
+      /// Deployment::ComponentPortDescriptions's, and
+      /// Deployment::Properties and delegates mapping the values from
+      /// their corresponding XSC objects to their handlers.
+      bool  get_component_interface_descr (
+          Deployment::ComponentInterfaceDescription& toconfig,
           ComponentInterfaceDescription& desc);
-			
     };
 
   }
-	
+
 }
+
 #include /**/ "ace/post.h"
 #endif /* CID_HANDLER_H */
