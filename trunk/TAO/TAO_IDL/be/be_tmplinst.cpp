a6792f3215f77e4395d00d8070ec4e5b868c3313
olli <olli@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-03 16:09:23 +0000

    ChangeLogTag: Fri Sep  3 19:01:59 2004  Olli Savia  <ops@iki.fi>

--- trunk/TAO/TAO_IDL/be/be_tmplinst.cpp
+++ trunk/TAO/TAO_IDL/be/be_tmplinst.cpp
@@ -52,7 +52,7 @@ template class ACE_Unbounded_Queue_Iterator <be_interface_fwd *>;
 #pragma instantiate ACE_Unbounded_Queue <be_interface_fwd *>
 #pragma instantiate ACE_Unbounded_Queue_Iterator <be_interface_fwd *>
 
-#elif defined (__GNUC__) && (defined (_AIX) || defined (__hpux))
+#elif defined (__GNUC__) && (defined (_AIX) || defined (__hpux) || defined (__Lynx__))
 template class ACE_Singleton<TAO_CodeGen, ACE_SYNCH_RECURSIVE_MUTEX>;
 template class ACE_Singleton<TAO_OutStream_Factory,ACE_SYNCH_RECURSIVE_MUTEX>;
 template class ACE_Singleton<TAO_Visitor_Factory,ACE_SYNCH_RECURSIVE_MUTEX>;
