afb7e039a7b6f472961e41604871ccde39374d9d
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-06 16:40:06 +0000

    *** empty log message ***

--- TAO/CIAO/DAnCE/tests/Config_Handlers/CompPkgDesc_Handler_Test.cpp
+++ TAO/CIAO/DAnCE/tests/Config_Handlers/CompPkgDesc_Handler_Test.cpp
@@ -7,7 +7,7 @@
 #include "Config_Handlers/CompPkgDesc_Handler.h"
 
 int
-ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+main (int argc, ACE_TCHAR *argv[])
 {
   return run_test <CIAO::Config_Handler::CompPkgDesc_Handler, Deployment::ComponentPackageDescription>
     (argc, argv, &CIAO::Config_Handler::CompPkgDesc_Handler::process_ComponentPackageDescription);
