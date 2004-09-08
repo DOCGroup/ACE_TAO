c3f2c9e019e8c8a96ee236bd53509249ec96c185
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-08 08:18:33 +0000

    ChangeLogTag: Wed Sep  8 08:15:00 UTC 2004  Simon Massey  <simon.massey@prismtechnologies.com>

--- tests/IOStream_Test.cpp
+++ tests/IOStream_Test.cpp
@@ -29,7 +29,7 @@ ACE_RCSID (tests, IOStream_Test, "$Id$")
 
 #if !defined (ACE_LACKS_ACE_IOSTREAM)
 #  include "ace/OS_NS_unistd.h"
-#  include "ace/OS_NS_ctype.h"  // Needed for isspace() function
+#  include "ace/os_include/os_ctype.h"  // Needed for isspace() function
 
 typedef ACE_IOStream<ACE_SOCK_Stream> ACE_SOCK_IOStream;
 
