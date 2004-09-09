3ae81cea0e17814df0cee295b84f7bd8cc85c3ea
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-09 19:02:06 +0000

    *** empty log message ***

--- branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/BaseTypes_Handler.cpp
+++ branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/BaseTypes_Handler.cpp
@@ -7,16 +7,17 @@ CIAO::Config_Handlers::BaseTypes_Handler::BaseTypes_Handler()
 CIAO::Config_Handlers::BaseTypes_Handler::~BaseTypes_Handler()
 {}
 
+// @@ Jules, is this documentationin the C++ required at all?
+
 ///This method takes a <XMLSchema::string>
 ///and returns a duplicate char* that is
-///created with CORBA::string_dup. 
-char* 
+///created with CORBA::string_dup.
+char*
 CIAO::Config_Handlers::BaseTypes_Handler::get_string (XMLSchema::string<char>& desc)
 {
     //Convert duplicate the XMLSchema::string into
     //a char*. The caller receives ownership of the pointer.
-    char* str = CORBA::string_dup(desc.c_str());
-    
+    char* str = CORBA::string_dup (desc.c_str());
+
     return str;
 }
-
--- branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/CPD_Handler.h
+++ branches/xsc_handler_0/TAO/CIAO/DAnCE/Config_Handlers/CPD_Handler.h
@@ -12,41 +12,42 @@
 #define CPD_HANDLER_H
 
 #include /**/ "ace/pre.h"
-                                                                                
+
 #include "ciao/DeploymentC.h"
 #include "Basic_Deployment_Data.hpp"
-             
-                                                                                
+
+
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
 
 namespace CIAO{
-  
+
   namespace Config_Handlers{
 
   /*
    * @class CPD_Handler
-   * 
+   *
    * @brief Handler class for <CCMComponentPortDescription> types.
-   * 
-   * This class defines handler methods to map values from 
+   *
+   * This class defines handler methods to map values from
    * XSC objects, parsed from the descriptor files, to the
    * corresponding CORBA IDL type for the schema element.
-   * 
+   *
    */
 
-   class CPD_Handler{
-  
+   class CPD_Handler
+   {
+
     public:
-    
+
       CPD_Handler (void);
       virtual ~CPD_Handler (void);
-        
+
       ///This method maps the values from the
       ///XSC object <ComponentInterfaceDescription> to
-      ///the CORBA IDL type <Deployment::ComponentInterfaceDescription>.  
+      ///the CORBA IDL type <Deployment::ComponentInterfaceDescription>.
       void
       get_ComponentPortDescription (
         Deployment::ComponentPortDescription& toconfig,
