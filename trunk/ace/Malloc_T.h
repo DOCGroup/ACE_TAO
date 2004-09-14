e47d0f30974c60e1db2f5969808cfdd608224a3d
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-14 15:33:15 +0000

    ChangeLogTag:Tue Sep 14 11:07:25 2004  Steve Huston  <shuston@riverace.com>

--- trunk/ace/Malloc_T.h
+++ trunk/ace/Malloc_T.h
@@ -115,6 +115,9 @@ public:
   /// Return a chunk of memory back to free list cache.
   void free (void *);
 
+  /// Return the number of chunks available in the cache.
+  size_t pool_depth (void);
+
 private:
   /// Remember how we allocate the memory in the first place so
   /// we can clear things up later.
@@ -176,6 +179,9 @@ public:
 
   /// Return a chunk of memory back to free list cache.
   void free (void *);
+
+  /// Return the number of chunks available in the cache.
+  size_t pool_depth (void);
 
 private:
   /// Remember how we allocate the memory in the first place so
