59e0aaef033d97e8820da07ef5dde1a7d7ef7ac4
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-09-15 01:46:19 +0000

    ChangeLogTag: Tue Sep 14 20:43:31 2004  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/CIAO/DAnCE/ciao/Context_Impl_Base.h
+++ TAO/CIAO/DAnCE/ciao/Context_Impl_Base.h
@@ -19,13 +19,14 @@
 
 #include /**/ "ace/pre.h"
 
+#include "tao/Objref_VarOut_T.h"
+
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
 #include "CIAO_Server_Export.h"
 #include "ciao/CCM_ContainerC.h"
-#include "tao/Objref_VarOut_T.h"
 
 namespace Components
 {
