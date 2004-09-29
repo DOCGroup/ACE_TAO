a15b945f45c276329dd68e25820dddf6eae056c8
Johnny Willemsen <jwillemsen@remedy.nl>
2004-09-29 07:51:04 +0000

    ChangeLogTag: Wed Sep 29 07:49:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/CIAO/ciao/Servant_Activator.h
+++ TAO/CIAO/ciao/Servant_Activator.h
@@ -25,9 +25,7 @@
 #include "tao/LocalObject.h"
 
 #if defined(_MSC_VER)
-#if (_MSC_VER >= 1200)
 #pragma warning(push)
-#endif /* _MSC_VER >= 1200 */
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
 
@@ -99,6 +97,10 @@ namespace CIAO
     ACE_SYNCH_MUTEX  mutex_;
   };
 }
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif /* _MSC_VER */
 
 #include /**/ "ace/post.h"
 #endif /*CIAO_SERVANT_ACTIVATOR_H*/
