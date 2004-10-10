183c9fa20f05af1221ef6b035bc6de92574fbae2
bala <balanatarajan@users.noreply.github.com>
2004-10-10 02:48:49 +0000

    ChangeLogTag:Sun Oct 10 02:47:21 2004  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- TAO/CIAO/DAnCE/tests/Config_Handlers/Domain_Handler_Test.cpp
+++ TAO/CIAO/DAnCE/tests/Config_Handlers/Domain_Handler_Test.cpp
@@ -2,15 +2,12 @@
 
 #include "Handler_Test.h"
 
-#ifndef ACE_HAS_BROKEN_NESTED_TEMPLATES
-
 #include "Config_Handlers/Domain_Handler.h"
 
 int
 ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
-  return run_test <CIAO::Config_Handler::Domain_Handler, Deployment::Domain>
+  return Config_Handler_Test::run_test <CIAO::Config_Handler::Domain_Handler, Deployment::Domain>
     (argc, argv, &CIAO::Config_Handler::Domain_Handler::process_domain);
 }
 
-#endif // ACE_HAS_BROKEN_NESTED_TEMPLATES
