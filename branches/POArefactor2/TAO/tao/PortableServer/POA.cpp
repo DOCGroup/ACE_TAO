ef39b3960790a8578bab331515c33db256336096
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-10-11 15:15:13 +0000

    ChangeLogTag:  Mon Oct 11 15:15:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/POArefactor2/TAO/tao/PortableServer/POA.cpp
+++ branches/POArefactor2/TAO/tao/PortableServer/POA.cpp
@@ -4072,8 +4072,7 @@ TAO_POA::ORT_adapter_i (void)
       ACE_TRY_CHECK;
 
       this->ort_adapter_ =
-          ort_ap_factory->create (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK;
+          ort_ap_factory->create ();
 
       if (!ort_adapter_)
         return 0;
