db46f08be3025e6117fd050e33a8d2a9436d326b
Phil Mesnier <mesnier_p@ociweb.com>
2004-10-26 15:53:47 +0000

    ChangeLog tag: Tue Oct 26 10:47:51 2004  Phil Mesnier  <mesnier_p@ociweb.com>

--- TAO/orbsvcs/orbsvcs/HTIOP/HTIOP_Acceptor_Impl.h
+++ TAO/orbsvcs/orbsvcs/HTIOP/HTIOP_Acceptor_Impl.h
@@ -46,7 +46,6 @@ namespace TAO
 
       */
       Creation_Strategy (TAO_ORB_Core *orb_core,
-                         void *arg = 0,
                          CORBA::Boolean flag = 0);
 
       /// Create a SVC_HANDLER  and set the ORB_Core pointer on it.
@@ -55,9 +54,6 @@ namespace TAO
     protected:
       /// Pointer to the ORB Core.
       TAO_ORB_Core *orb_core_;
-
-      /// Some info/state to be passed to the service handler we create.
-      void *arg_;
 
       /// Should we use the Lite version for any protocol?
       CORBA::Boolean lite_flag_;
