2295fa196b3d7b2b8f18112d174ce0e4ad9f35eb
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-03 22:39:30 +0000

    ChangeLogTag: Wed Nov  3 16:32:54 2004  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/CIAO/CIDLC/ExecutorMappingGenerator.cpp
+++ trunk/TAO/CIAO/CIDLC/ExecutorMappingGenerator.cpp
@@ -993,19 +993,19 @@ namespace
     virtual void
     pre (InParameter&)
     {
-      os << "in ";
+      os << endl << "in ";
     }
 
     virtual void
     pre (OutParameter&)
     {
-      os << "out ";
+      os << endl << "out ";
     }
 
     virtual void
     pre (InOutParameter&)
     {
-      os << "inout ";
+      os << endl << "inout ";
     }
 
     virtual void
