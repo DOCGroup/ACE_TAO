1fa7eddec274a58fdf52ecfdc361cdca757cdb6d
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-09 20:22:47 +0000

    This commit was manufactured by cvs2svn to create branch 'dynamic_swapping'.

--- TAO/orbsvcs/orbsvcs/Notify/Any/AnyEvent.inl
+++
@@ -1,3 +0,0 @@
-// $Id$
-
-#include "AnyEvent.h"
--- ace/Shared_Memory.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "ace/Shared_Memory.h"
-
-ACE_RCSID(ace, Shared_Memory, "$Id$")
-
-ACE_Shared_Memory::~ACE_Shared_Memory (void)
-{
-}
