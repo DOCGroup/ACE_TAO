a67df157813a1f666a70b5b1c786f319f9f9e1e6
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-15 11:06:40 +0000

    ChangeLogTag: Mon Nov 15 10:45:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ branches/POArefactor2/TAO/tao/makediffs.bat
@@ -0,0 +1,17 @@
+diff -wub orig/PortableInterceptorC.h PortableInterceptorC.h > diffs/PortableInterceptor.diff
+diff -wub orig/PortableInterceptorC.cpp PortableInterceptorC.cpp >> diffs/PortableInterceptor.diff
+unix2dos diffs\PortableInterceptor.diff
+
+diff -wub orig/PolicyC.h PolicyC.h > diffs/Policy.diff
+diff -wub orig/PolicyC.cpp PolicyC.cpp >> diffs/Policy.diff
+unix2dos diffs\Policy.diff
+
+diff -wub orig/Typecode_typesC.h Typecode_typesC.h > diffs/Typecode_types.diff
+unix2dos diffs\Typecode_types.diff
+
+diff -wub orig/UShortSeqC.h UShortSeqC.h > diffs/UShortSeq.diff
+unix2dos diffs\UShortSeq.diff
+
+diff -wub orig/DomainC.cpp DomainC.cpp > diffs/Domain.diff
+unix2dos diffs\Domain.diff
+
---
+++ branches/POArefactor2/TAO/tao/regenerate.bat
@@ -0,0 +1,34 @@
+tao_idl.exe -o orig -Gp -Gd -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao PortableInterceptor.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao WStringSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao WCharSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao ULongSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao UShortSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao ShortSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao BooleanSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao DoubleSeq.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Dynamic.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Dynamic.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Messaging_PolicyValue.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao WrongTransaction.pidl
+tao_idl.exe -o orig -Ge 1 -St -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Messaging_SyncScope.pidl
+tao_idl.exe -o orig -Ge 1 -Sa -St -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Typecode_types.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Bounds.pidl
+tao_idl.exe -o orig -Ge 1 -Gp -Gd -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Policy.pidl
+tao_idl.exe -o orig -Ge 1 -Gp -Gd -St -GT -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Pollable.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao StringSeq.pidl
+tao_idl.exe -o orig -Gp -Gd -Ge 1 -GT -GA -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao TAO.pidl
+tao_idl.exe -o orig -Gp -Gd -Ge 1 -GT -GA -Sc -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao PI_Forward.pidl
+tao_idl.exe -o orig -Ge 1 -GT -GA -Sa -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao ObjectIdList.pidl
+tao_idl.exe -o orig -Gp -Gd -Ge 1 -GT -GA -Wb,export_macro=TAO_Export -Wb,export_include=tao/TAO_Export.h -Wb,pre_include="ace/pre.h" -Wb,post_include="ace/post.h" -I c:\ace\poarefactor\ace_wrappers\tao Domain.pidl
+
+copy orig\*A.cpp .
+copy orig\*C.cpp .
+copy orig\*C.h .
+copy orig\*C.inl .
+copy orig\PollableS.* .
+copy orig\PollableS_T.* .
+
+patch < diffs/PortableInterceptor.diff
+patch < diffs/Policy.diff
+patch < diffs/Typecode_types.diff
+patch < diffs/UShortSeq.diff
