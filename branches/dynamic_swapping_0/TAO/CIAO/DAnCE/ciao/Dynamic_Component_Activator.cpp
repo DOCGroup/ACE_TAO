ffa23c26866c0f6af2d28ca1d7e669f3ad2b6474
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-15 21:19:49 +0000

    *** empty log message ***

--- branches/dynamic_swapping_0/TAO/CIAO/DAnCE/ciao/Dynamic_Component_Activator.cpp
+++ branches/dynamic_swapping_0/TAO/CIAO/DAnCE/ciao/Dynamic_Component_Activator.cpp
@@ -43,9 +43,9 @@ namespace CIAO
   }
 
   // @@ Jai, what is this update map supposed to be doing.
-  void update_map (PortableServer::ObjectId &oid,
-                   Dynamic_Component_Servant_Base* servant
-                   ACE_ENV_ARG_DECL)
+  void update_servant_map (PortableServer::ObjectId &oid,
+                           Dynamic_Component_Servant_Base* servant
+                           ACE_ENV_ARG_DECL)
   {
   }
 
