8dca9a677502e7d13548484aebbe674a6d5ee6b3
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-16 16:54:37 +0000

    *** empty log message ***

--- branches/dynamic_swapping_0/TAO/CIAO/DAnCE/ciao/Swapping_Servant_Home_Impl_T.cpp
+++ branches/dynamic_swapping_0/TAO/CIAO/DAnCE/ciao/Swapping_Servant_Home_Impl_T.cpp
@@ -27,7 +27,6 @@ namespace CIAO
       SWapping_Container * c
     )
     : Swapping_Servant_Home_Impl_Base (c),
-      activator_ (),
       executor_ (EXEC::_duplicate (exe))
   {
   }
@@ -209,10 +208,7 @@ namespace CIAO
     Dynamic_Component_Servant_Base *svt =
       new Dynamic_Component_Servant<COMP_SVNT> ();
 
-    // @@ Jai, why is the <this> activator updated? Shouldn't it be in
-    // some other place, like the container so that all the servants
-    // could share this?
-    this->activator_.update_map (oid.in (), svt);
+    this->container_.update_map (oid.in (), svt);
 
     return ho._retn ();
   }
