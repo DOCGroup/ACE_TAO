9e60f9298ea8fcb001b0d9fe9583d345d23e31a6
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-16 22:45:34 +0000

    *** empty log message ***

--- TAO/CIAO/DAnCE/ciao/Dynamic_Component_Servant_T.cpp
+++ TAO/CIAO/DAnCE/ciao/Dynamic_Component_Servant_T.cpp
@@ -10,22 +10,34 @@ namespace CIAO
   template <typename COMP_SERVANT,
             typename COMP_EXEC,
             typename COMP_EXEC_VAR,
-            typename EXEC>
-  Dynamic_Component_Servant<COMP_SERVANT, COMP_EXEC, COMP_EXEC_VAR, EXEC>
+            typename EXEC,
+            typename COMP>
+  Dynamic_Component_Servant<COMP_SERVANT, COMP_EXEC, COMP_EXEC_VAR, EXEC, COMP>
     ::Dynamic_Component_Servant (EXEC *exe, Components::CCMHome_ptr home)
       :Dynamic_Component_Servant_Base (),
-       executor_ (EXEC::_duplicate (exe))
+       executor_ (EXEC::_duplicate (exe),
+       home_ (Components::CCMHome::_duplicate (home.in ())))
   {
   }
 
   template <typename COMP_SERVANT,
             typename COMP_EXEC,
             typename COMP_EXEC_VAR,
-            typename EXEC>
+            typename EXEC,
+            typename COMP>
   PortableServer::Servant Dynamic_Component_Servant
-    <COMP_SERVANT, COMP_EXEC, COMP_EXEC_VAR, EXEC>::create (void)
+    <COMP_SERVANT, COMP_EXEC, COMP_EXEC_VAR, EXEC, COMP>::create (void)
   {
-    return new COMP_SERVANT;
+    ::Components::EnterpriseComponent_var ciao_ec = 
+      this->executor_->create (ACE_ENV_SINGLE_ARG_PARAMETER);
+
+    COMP_EXEC_VAR ciao_comp = COMP_EXEC::_narrow (ciao_ec.in ()
+                                                  ACE_ENV_ARG_PARAMETER);
+
+    COMP_SERVANT *svt = 0;
+    ACE_NEW_RETURN (svt,
+                    COMP_SERVANT (ciao_comp.in (), this->home_.in ()),
+                                  COMP::_nil ());
   }
 }
 
