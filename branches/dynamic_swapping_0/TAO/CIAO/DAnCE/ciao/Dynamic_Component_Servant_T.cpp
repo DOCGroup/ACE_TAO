167f9147190a4ed4a60d67ca34ffb584449ed632
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-16 21:42:09 +0000

    *** empty log message ***

--- branches/dynamic_swapping_0/TAO/CIAO/DAnCE/ciao/Dynamic_Component_Servant_T.cpp
+++ branches/dynamic_swapping_0/TAO/CIAO/DAnCE/ciao/Dynamic_Component_Servant_T.cpp
@@ -9,8 +9,21 @@ namespace CIAO
 {
   template <typename COMP_SERVANT,
             typename COMP_EXEC,
-            typename COMP_EXEC_VAR>
-  PortableServer::Servant Dynamic_Component_Servant<COMP_SERVANT>::create (void)
+            typename COMP_EXEC_VAR,
+            typename EXEC>
+  Dynamic_Component_Servant<COMP_SERVANT, COMP_EXEC, COMP_EXEC_VAR, EXEC>
+    ::Dynamic_Component_Servant (EXEC *exe)
+      :Dynamic_Component_Servant_Base (),
+       executor_ (EXEC::_duplicate (exe))
+  {
+  }
+
+  template <typename COMP_SERVANT,
+            typename COMP_EXEC,
+            typename COMP_EXEC_VAR,
+            typename EXEC>
+  PortableServer::Servant Dynamic_Component_Servant
+    <COMP_SERVANT, COMP_EXEC, COMP_EXEC_VAR, EXEC>::create (void)
   {
     return new COMP_SERVANT;
   }
