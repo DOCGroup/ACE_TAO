370a2cef1cf2b960dfe6844b5a2a74300043d700
wilson_d <wilson_d@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-21 17:45:07 +0000

    ChangeLogTag: Sun Nov 21 11:43:00 2004  Dale Wilson  <wilson_d@ociweb.com>

--- trunk/TAO/orbsvcs/orbsvcs/Notify/Method_Request_Event.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Notify/Method_Request_Event.cpp
@@ -35,11 +35,6 @@ TAO_Notify_Method_Request_Event::~TAO_Notify_Method_Request_Event()
 {
 }
 
-const TAO_Notify_Event * TAO_Notify_Method_Request_Event::event() const
-{
-  return event_;
-}
-
 void
 TAO_Notify_Method_Request_Event::complete ()
 {
