5fb6ca364e312f9ce382f2ba9487a666a62282ae
Johnny Willemsen <jwillemsen@remedy.nl>
2004-11-23 07:24:37 +0000

    ChangeLogTag: Tue Nov 23 07:24:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/Notify/Method_Request_Event.cpp
+++ TAO/orbsvcs/orbsvcs/Notify/Method_Request_Event.cpp
@@ -96,7 +96,6 @@ TAO_Notify_Method_Request_Event_Queueable::execute (ACE_ENV_SINGLE_ARG_DECL_NOT_
   return -1;
 }
 
-ACE_INLINE
 const TAO_Notify_Event *
 TAO_Notify_Method_Request_Event::event() const
 {
