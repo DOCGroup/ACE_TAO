270bc4a2cceee19db6f3888509f0f66b8f661ea7
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-23 10:28:56 +0000

    ChangeLogTag: Tue Nov 23 10:09:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/POArefactor2/TAO/tao/PortableServer/PortableServer.cpp
+++ branches/POArefactor2/TAO/tao/PortableServer/PortableServer.cpp
@@ -11,8 +11,24 @@
 //=============================================================================
 
 
-#include "PortableServer.h"
-#include "Object_Adapter_Factory.h"
+#include "tao/PortableServer/PortableServer.h"
+#include "tao/PortableServer/Object_Adapter_Factory.h"
+
+#include "tao/PortableServer/ThreadPolicyFactoryImpl.h"
+#include "tao/PortableServer/LifespanPolicyFactoryImpl.h"
+#include "tao/PortableServer/IdAssignmentPolicyFactoryImpl.h"
+#include "tao/PortableServer/IdUniquenessPolicyFactoryImpl.h"
+#include "tao/PortableServer/ImplicitActivationPolicyFactoryImpl.h"
+#include "tao/PortableServer/RequestProcessingPolicyFactoryImpl.h"
+#include "tao/PortableServer/ServantRetentionPolicyFactoryImpl.h"
+
+#include "tao/PortableServer/ThreadStrategyFactoryImpl.h"
+#include "tao/PortableServer/LifespanStrategyFactoryImpl.h"
+#include "tao/PortableServer/IdAssignmentStrategyFactoryImpl.h"
+#include "tao/PortableServer/IdUniquenessStrategyFactoryImpl.h"
+#include "tao/PortableServer/ImplicitActivationStrategyFactoryImpl.h"
+#include "tao/PortableServer/RequestProcessingStrategyFactoryImpl.h"
+#include "tao/PortableServer/ServantRetentionStrategyFactoryImpl.h"
 
 ACE_RCSID (PortableServer,
            PortableServer,
@@ -21,6 +37,64 @@ ACE_RCSID (PortableServer,
 int
 TAO_POA_Initializer::init (void)
 {
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_ThreadPolicyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_LifespanPolicyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_IdAssignmentPolicyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_IdUniquenessPolicyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_ImplicitActivationPolicyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_RequestProcessingPolicyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_ServantRetentionPolicyFactoryImpl
+    );
+
+  // Strategies
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_ThreadStrategyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_LifespanStrategyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_IdAssignmentStrategyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_IdUniquenessStrategyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_ImplicitActivationStrategyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_RequestProcessingStrategyFactoryImpl
+    );
+
+  ACE_Service_Config::process_directive (
+      TAO::Portable_Server::ace_svc_desc_ServantRetentionStrategyFactoryImpl
+    );
+
   return
     ACE_Service_Config::process_directive (
         ace_svc_desc_TAO_Object_Adapter_Factory
