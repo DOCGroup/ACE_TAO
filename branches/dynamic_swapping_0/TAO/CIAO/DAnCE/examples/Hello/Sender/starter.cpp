752e90a865114c9795add75c664bb3f4f3f1dccb
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-11-25 00:10:34 +0000

    *** empty log message ***

--- branches/dynamic_swapping_0/TAO/CIAO/DAnCE/examples/Hello/Sender/starter.cpp
+++ branches/dynamic_swapping_0/TAO/CIAO/DAnCE/examples/Hello/Sender/starter.cpp
@@ -79,10 +79,15 @@ main (int argc, char *argv[])
                             -1);
         }
 
+      char *return_message;
       sender->local_message (message);
+      return_message = sender->local_message ();
+      ACE_DEBUG ((LM_DEBUG, "the message is %s\n", return_message));
 
-      sender->start (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK;
+      sender->remove ();
+
+      //sender->start (ACE_ENV_SINGLE_ARG_PARAMETER);
+      //ACE_TRY_CHECK;
 
       orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
       ACE_TRY_CHECK;
