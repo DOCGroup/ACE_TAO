59f872279921ab416962cd92498f8b2caa4f49cf
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-12-01 17:22:38 +0000

    ChangeLogTag:Wed Dec  1 17:19:39 2004  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

--- trunk/TAO/TAO_IDL/be/be_visitor_interface/strategized_proxy_broker_ss.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_interface/strategized_proxy_broker_ss.cpp
@@ -111,7 +111,7 @@ be_visitor_interface_strategized_proxy_broker_ss::visit_interface (
       << "ACE_ENV_ARG_PARAMETER" << be_uidt_nl
       << ");" << be_uidt_nl
       << "ACE_CHECK;" << be_nl << be_nl
-      << "servant_upcall.pre_invoke_collocated_request (" 
+      << "servant_upcall.pre_invoke_collocated_request ("
       << be_idt << be_idt_nl
       << "ACE_ENV_SINGLE_ARG_PARAMETER" << be_uidt_nl
       << ");" << be_uidt_nl
@@ -128,7 +128,7 @@ be_visitor_interface_strategized_proxy_broker_ss::visit_interface (
       << "                             op_len);" << be_nl << be_nl
       << "if (status == -1)" << be_idt_nl
       << "{" << be_idt_nl
-      << "ACE_THROW (CORBA::BAD_OPERATION ());" << be_uidt_nl
+      << "ACE_THROW (CORBA::BAD_OPERATION (2, CORBA::COMPLETED_NO));" << be_uidt_nl
       << "}" << be_uidt_nl << be_nl
       << "ACE_TRY" << be_idt_nl
       << "{" << be_idt_nl
