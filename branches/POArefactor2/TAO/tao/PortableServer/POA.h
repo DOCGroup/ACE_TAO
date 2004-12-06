9c41953733ae50baed7b1ec615dd2221fdb57522
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-12-06 12:06:04 +0000

    ChangeLogTag: Mon Dec 06 11:34:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/POArefactor2/TAO/tao/PortableServer/POA.h
+++ branches/POArefactor2/TAO/tao/PortableServer/POA.h
@@ -17,11 +17,12 @@
 
 #include /**/ "ace/pre.h"
 
+#include "tao/PortableServer/Root_POA.h"
+
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "tao/PortableServer/Root_POA.h"
 /**
  * @class TAO_POA
  *
