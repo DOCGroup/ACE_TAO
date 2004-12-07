d722252521ca69fcf072e4ffac9219c0c7d2dbe6
Johnny Willemsen <jwillemsen@remedy.nl>
2004-12-07 18:39:22 +0000

    ChangeLogTag: Tue Dec  7 18:39:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/BoundsC.cpp
+++ TAO/tao/BoundsC.cpp
@@ -26,13 +26,13 @@
 //     http://www.cs.wustl.edu/~schmidt/TAO.html
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:323
+// be\be_codegen.cpp:302
 
 
 #include "BoundsC.h"
-#include "tao/SystemException.h"
 #include "tao/CDR.h"
 #include "tao/Typecode.h"
+#include "tao/SystemException.h"
 #include "ace/OS_NS_string.h"
 
 #if defined (__BORLANDC__)
@@ -40,11 +40,11 @@
 #endif /* __BORLANDC__ */
 
 #if !defined (__ACE_INLINE__)
-#include "BoundsC.i"
+#include "BoundsC.inl"
 #endif /* !defined INLINE */
 
 // TAO_IDL - Generated from
-// be\be_visitor_arg_traits.cpp:64
+// be\be_visitor_arg_traits.cpp:65
 
 // Arg traits specializations.
 namespace TAO
@@ -52,7 +52,7 @@ namespace TAO
 }
 
 
-// TAO_IDL - Generated from
+// TAO_IDL - Generated from 
 // be\be_visitor_exception/exception_cs.cpp:63
 
 CORBA::Bounds::Bounds (void)
@@ -85,21 +85,20 @@ CORBA::Bounds::operator= (const ::CORBA::Bounds &_tao_excp)
 void CORBA::Bounds::_tao_any_destructor (void *_tao_void_pointer)
 {
   Bounds *_tao_tmp_pointer =
-    ACE_static_cast (Bounds*, _tao_void_pointer);
+    static_cast<Bounds *> (_tao_void_pointer);
   delete _tao_tmp_pointer;
 }
 
 CORBA::Bounds *
 CORBA::Bounds::_downcast (CORBA::Exception *_tao_excp)
 {
-  if (!ACE_OS::strcmp ("IDL:omg.org/CORBA/Bounds:1.0", _tao_excp->_rep_id ()))
-    {
-      return ACE_dynamic_cast (Bounds *, _tao_excp);
-    }
-  else
-    {
-      return 0;
-    }
+  return dynamic_cast<Bounds *> (_tao_excp);
+}
+
+const CORBA::Bounds *
+CORBA::Bounds::_downcast (CORBA::Exception const *_tao_excp)
+{
+  return dynamic_cast<const Bounds *> (_tao_excp);
 }
 
 CORBA::Exception *CORBA::Bounds::_alloc (void)
@@ -135,7 +134,7 @@ void CORBA::Bounds::_tao_encode (
     {
       return;
     }
-
+  
   ACE_THROW (CORBA::MARSHAL ());
 }
 
@@ -148,27 +147,33 @@ void CORBA::Bounds::_tao_decode (
     {
       return;
     }
-
+  
   ACE_THROW (CORBA::MARSHAL ());
 }
 
+// TAO extension - the virtual _type method.
+CORBA::TypeCode_ptr CORBA::Bounds::_type (void) const
+{
+  return ::CORBA::_tc_Bounds;
+}
+
 // TAO_IDL - Generated from
-// be\be_visitor_typecode/typecode_defn.cpp:290
+// be\be_visitor_typecode/typecode_defn.cpp:295
 
 static const CORBA::Long _oc_CORBA_Bounds[] =
 {
     TAO_ENCAP_BYTE_ORDER, // byte order
   29,
-  ACE_NTOHL (0x49444c3a),
-  ACE_NTOHL (0x6f6d672e),
-  ACE_NTOHL (0x6f72672f),
-  ACE_NTOHL (0x434f5242),
-  ACE_NTOHL (0x412f426f),
-  ACE_NTOHL (0x756e6473),
-  ACE_NTOHL (0x3a312e30),
+  ACE_NTOHL (0x49444c3a), 
+  ACE_NTOHL (0x6f6d672e), 
+  ACE_NTOHL (0x6f72672f), 
+  ACE_NTOHL (0x434f5242), 
+  ACE_NTOHL (0x412f426f), 
+  ACE_NTOHL (0x756e6473), 
+  ACE_NTOHL (0x3a312e30), 
   ACE_NTOHL (0x0),  // repository ID = IDL:omg.org/CORBA/Bounds:1.0
     7,
-  ACE_NTOHL (0x426f756e),
+  ACE_NTOHL (0x426f756e), 
   ACE_NTOHL (0x64730000),  // name = Bounds
   0, // member count
   };
@@ -178,19 +183,13 @@ static CORBA::TypeCode _tc_TAO_tc_CORBA_Bounds (
     sizeof (_oc_CORBA_Bounds),
     (char *) &_oc_CORBA_Bounds,
     0,
-    sizeof (CORBA::Bounds)
+    0
   );
 
 namespace CORBA
 {
   ::CORBA::TypeCode_ptr _tc_Bounds =
     &_tc_TAO_tc_CORBA_Bounds;
-}
-
-// TAO extension - the virtual _type method.
-CORBA::TypeCode_ptr CORBA::Bounds::_type (void) const
-{
-  return ::CORBA::_tc_Bounds;
 }
 
 // TAO_IDL - Generated from
@@ -221,10 +220,10 @@ CORBA::Boolean operator>> (
 }
 
 // TAO_IDL - Generated from
-// be\be_visitor_root/root.cpp:1628
+// be\be_visitor_root/root.cpp:1629
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
 #elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
 
-#endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
+#endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */ 
