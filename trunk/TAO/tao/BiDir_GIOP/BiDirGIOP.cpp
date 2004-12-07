415be2a582d1445efc99730701e9b5bdd48f7bfd
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-12-07 14:32:44 +0000

    ChangeLogTag: Tue Dec  7 08:31:17 2004  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/TAO/tao/BiDir_GIOP/BiDirGIOP.cpp
+++ trunk/TAO/tao/BiDir_GIOP/BiDirGIOP.cpp
@@ -80,7 +80,7 @@ TAO_BiDirGIOP_Loader::load_policy_validators (TAO_Policy_Validator &val
 
   // We may be adding another TAO_BiDirPolicy_Validator instance for the
   // same ORB (different POA). In cases where huge numbers of bi-directional POA instances
-  // are created, having a validator instance per POA may introduce additional delays in 
+  // are created, having a validator instance per POA may introduce additional delays in
   // policy validation and hence, the overal policy creation time. Since this is out of the
   // critical invocation processing path, I plan to keep the design simple and not try to
   // avoid an ineficiency of such small proportions.
