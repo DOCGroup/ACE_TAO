71d6adc8311b5a87d109c80dfbe4bd0f8226e8d7
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-12-10 08:23:27 +0000

    ChangeLogTag: Thu Dec  9 18:39:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/POArefactor2/TAO/tao/PortableServer/Active_Object_Map.cpp
+++ branches/POArefactor2/TAO/tao/PortableServer/Active_Object_Map.cpp
@@ -12,17 +12,6 @@
 
 ACE_RCSID(tao, Active_Object_Map, "$Id$")
 
-
-TAO_Active_Object_Map_Entry::TAO_Active_Object_Map_Entry (void)
-  : user_id_ (),
-    system_id_ (),
-    servant_ (0),
-    reference_count_ (1),
-    deactivated_ (0),
-    priority_ (-1)
-{
-}
-
 /* static */
 size_t TAO_Active_Object_Map::system_id_size_ = 0;
 
