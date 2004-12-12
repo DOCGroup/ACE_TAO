27b1652db07303efc627d24d8d06f4a9876acc01
Johnny Willemsen <jwillemsen@remedy.nl>
2004-12-12 13:29:13 +0000

    ChangeLogTag: Sun Dec 12 13:28:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/ServicesS.h
+++ TAO/tao/ServicesS.h
@@ -26,28 +26,23 @@
 //     http://www.cs.wustl.edu/~schmidt/TAO.html
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:411
+// be\be_codegen.cpp:390
 
 #ifndef _TAO_IDL_ORIG_SERVICESS_H_
 #define _TAO_IDL_ORIG_SERVICESS_H_
 
 #include /**/ "ace/pre.h"
 
-#include "OctetSeqS.h"
+#include "tao/OctetSeqS.h"
 #include "ServicesC.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "tao/Collocation_Proxy_Broker.h"
-#include "tao/PortableServer/PortableServer.h"
-#include "tao/PortableServer/Servant_Base.h"
 
 #if defined(_MSC_VER)
-#if (_MSC_VER >= 1200)
 #pragma warning(push)
-#endif /* _MSC_VER >= 1200 */
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
 
@@ -68,15 +63,15 @@ namespace POA_CORBA
 } // module CORBA
 
 // TAO_IDL - Generated from 
-// be\be_codegen.cpp:1014
+// be\be_codegen.cpp:997
 
 #include "ServicesS_T.h"
 
 #if defined (__ACE_INLINE__)
-#include "ServicesS.i"
+#include "ServicesS.inl"
 #endif /* defined INLINE */
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#if defined(_MSC_VER)
 #pragma warning(pop)
 #endif /* _MSC_VER */
 
@@ -86,3 +81,4 @@ namespace POA_CORBA
 
 #include /**/ "ace/post.h"
 #endif /* ifndef */
+
