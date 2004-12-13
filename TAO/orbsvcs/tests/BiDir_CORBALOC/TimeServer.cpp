46c58cc3591792a593f683c5f667c02a12da9a26
Johnny Willemsen <jwillemsen@remedy.nl>
2004-12-13 08:41:38 +0000

    ChangeLogTag: Mon Dec 13 08:41:12 UTC 2004  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/tests/BiDir_CORBALOC/TimeServer.cpp
+++ TAO/orbsvcs/tests/BiDir_CORBALOC/TimeServer.cpp
@@ -154,7 +154,7 @@ main(int argc, char * argv[])
                 ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      rootpoa->destroy (0 , 0 ACE_ENV_SINGLE_ARG_PARAMETER);
+      rootpoa->destroy (0 , 0 ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
       orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
