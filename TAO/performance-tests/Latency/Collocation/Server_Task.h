59d53f6dfd2c56da246e85be128db0daf1ca8f88
arvindk <arvindk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-12-14 23:47:32 +0000

    ChangelogTag: Tue Dec 14 17:38:16 2004  Arvind S. Krishna  <arvindk@dre.vanderbilt.edu>

--- TAO/performance-tests/Latency/Collocation/Server_Task.h
+++ TAO/performance-tests/Latency/Collocation/Server_Task.h
@@ -7,34 +7,38 @@
 #include /**/ "ace/pre.h"
 
 #include "ace/Task.h"
+#include "ace/Manual_Event.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
 #include "tao/ORB.h"
+#include "TestC.h"
 
 /// Implement a Task to run the server in a single thread
 class Server_Task : public ACE_Task_Base
 {
 public:
   /// Constructor
-  Server_Task (const char* ior_file,
-               CORBA::ORB_ptr sorb,
-               ACE_Null_Condition &cond,
+  Server_Task (CORBA::ORB_ptr sorb,
+               ACE_Manual_Event &cond,
                ACE_Thread_Manager *thr_mgr);
 
   /// Thread entry point
-  int svc (void);
+  int svc ();
+
+  //Test::Roundtrip_prt get_reference ();
+  Test::Roundtrip * get_reference ();
 
 private:
 
-  const char* ior_file;
+  ACE_Manual_Event &cond_;
   /// Manual event to wake up the main thread to create a client
   /// thread.
-  ACE_Null_Condition &cond_;
 
   CORBA::ORB_var sorb_;
+  Test::Roundtrip_var rt_var_;
 };
 
 #include /**/ "ace/post.h"
