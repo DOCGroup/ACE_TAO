be3ceeb877cf233f270cbd543dbeffe4f866c03c
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2004-12-21 15:52:41 +0000

    ChangeLogTag: Tue Dec 21 09:49:50 2004  Chad Elliott  <elliott_c@ociweb.com>

--- trunk/TAO/orbsvcs/orbsvcs/Notify/EventTypeSeq.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Notify/EventTypeSeq.cpp
@@ -33,9 +33,7 @@ TAO_Notify_EventTypeSeq::operator = (const TAO_Notify_EventTypeSeq & rhs)
 }
 
 TAO_Notify_EventTypeSeq::TAO_Notify_EventTypeSeq (const TAO_Notify_EventTypeSeq & rhs)
-  : TAO_Notify_Object ()
-  , Topology_Savable ()
-  , ACE_Unbounded_Set <TAO_Notify_EventType> (rhs)
+  : ACE_Unbounded_Set <TAO_Notify_EventType> (rhs)
   , TAO_Notify::Topology_Object ()
 {
 }
