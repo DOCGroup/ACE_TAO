4d44bb5a8603d529fd3e4eca668cf035d99db813
jtc <jtc@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-01 06:11:09 +0000

    ChangeLogTag: Fri Dec 31 22:10:51 2004  J.T. Conklin  <jtc@acorntoolworks.com>

--- trunk/TAO/orbsvcs/orbsvcs/Time_Utilities.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Time_Utilities.cpp
@@ -3,7 +3,7 @@
 #include "orbsvcs/Time_Utilities.h"
 
 #if !defined (__ACE_INLINE__)
-# include "orbsvcs/orbsvcs/Time_Utilities.i"
+# include "orbsvcs/Time_Utilities.i"
 #endif /* __ACE_INLINE__ */
 
 ACE_RCSID(orbsvcs, Time_Utilities, "$Id$")
