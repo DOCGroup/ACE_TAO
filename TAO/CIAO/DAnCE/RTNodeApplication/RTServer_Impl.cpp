07ef210abd24526f0eba3111e2f60df2287c68d4
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-03 17:06:37 +0000

    ChangeLogTag:Mon Jan  3 10:01:09 2005  Nanbor Wang  <nanbor@cs.wustl.edu>

---
+++ TAO/CIAO/DAnCE/RTNodeApplication/RTServer_Impl.cpp
@@ -0,0 +1,24 @@
+// $Id$
+
+#include "RTServer_Impl.h"
+#include "ciao/CIAO_common.h"
+
+#if !defined (__ACE_INLINE__)
+# include "RTServer_Impl.inl"
+#endif /* __ACE_INLINE__ */
+
+CIAO::RTServer::RTNodeApplication_Impl::~RTNodeApplication_Impl ()
+{
+}
+
+CORBA::Long
+CIAO::RTServer::RTNodeApplication_Impl::init (ACE_ENV_SINGLE_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  CIAO::NodeApplication_Impl::init (ACE_ENV_SINGLE_ARG_PARAMETER);
+
+  // We will probably need two ORBs in this process.  One for the
+  // deployment framework, and one for the actual components.
+  return 0;
+}
+
