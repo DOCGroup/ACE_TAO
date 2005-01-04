3c46f5c56f490eec99760a00f23a519bcd110439
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-04 09:53:51 +0000

    ChangeLogTag: Tue Jan  4 09:42:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/LF_Strategy.cpp
+++ trunk/TAO/tao/LF_Strategy.cpp
@@ -3,12 +3,8 @@
 
 #include "tao/LF_Strategy.h"
 
-#if !defined (__ACE_INLINE__)
-# include "tao/LF_Strategy.inl"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID (tao, 
-           LF_Strategy, 
+ACE_RCSID (tao,
+           LF_Strategy,
            "$Id$")
 
 TAO_LF_Strategy::~TAO_LF_Strategy (void)
