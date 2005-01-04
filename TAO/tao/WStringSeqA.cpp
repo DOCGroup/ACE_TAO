7d88f36a9de88f6cb9c3d9d1264540bdbbacbd81
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-04 10:47:52 +0000

    ChangeLogTag: Tue Jan  4 10:42:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/WStringSeqA.cpp
+++ TAO/tao/WStringSeqA.cpp
@@ -27,10 +27,12 @@
 
 #include "WStringSeqC.h"
 #include "tao/Typecode.h"
+#include "tao/CDR.h"
+#include "tao/Any.h"
 #include "tao/Any_Dual_Impl_T.h"
 
 // TAO_IDL - Generated from
-// w:\ace_wrappers\tao\tao_idl\be\be_visitor_typecode/typecode_defn.cpp:290
+// be\be_visitor_typecode/typecode_defn.cpp:295
 
 static const CORBA::Long _oc_CORBA_WStringSeq[] =
 {
@@ -63,7 +65,7 @@ static CORBA::TypeCode _tc_TAO_tc_CORBA_WStringSeq (
     sizeof (_oc_CORBA_WStringSeq),
     (char *) &_oc_CORBA_WStringSeq,
     0,
-    sizeof (CORBA::WStringSeq)
+    0
   );
 
 namespace CORBA
@@ -73,7 +75,7 @@ namespace CORBA
 }
 
 // TAO_IDL - Generated from 
-// w:\ace_wrappers\tao\tao_idl\be\be_visitor_sequence/any_op_cs.cpp:54
+// be\be_visitor_sequence/any_op_cs.cpp:54
 
 // Copying insertion.
 void operator<<= (
@@ -130,8 +132,6 @@ CORBA::Boolean operator>>= (
       );
 }
 
-
-
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
   template class
@@ -146,5 +146,4 @@ CORBA::Boolean operator>>= (
         CORBA::WStringSeq \
       >
 
-#endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
+#endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */ 
