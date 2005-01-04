e79f85a570f0eb9e81564fadbdbf8b6ccb136d21
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-04 19:53:08 +0000

    ChangeLogTag: Tue Jan  4 19:42:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/BiDir_GIOP/BiDirPolicyC.h
+++ TAO/tao/BiDir_GIOP/BiDirPolicyC.h
@@ -26,7 +26,7 @@
 //     http://www.cs.wustl.edu/~schmidt/TAO.html
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:171
+// be\be_codegen.cpp:153
 
 #ifndef _TAO_IDL_ORIG_BIDIRPOLICYC_H_
 #define _TAO_IDL_ORIG_BIDIRPOLICYC_H_
@@ -37,13 +37,15 @@
 #error "You should not include BiDirPolicyC.h directly, use BiDirGIOP.h"
 #endif /* !TAO_BIDIRGIOP_SAFE_INCLUDE */
 
-#include "tao/ORB.h"
+#include "ace/config-all.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
 #include "bidirgiop_export.h"
+#include "tao/ORB.h"
+#include "tao/SystemException.h"
 #include "tao/Environment.h"
 #include "tao/Object.h"
 #include "tao/Objref_VarOut_T.h"
@@ -63,9 +65,7 @@
 #endif /* TAO_EXPORT_NESTED_CLASSES */
 
 #if defined(_MSC_VER)
-#if (_MSC_VER >= 1200)
 #pragma warning(push)
-#endif /* _MSC_VER >= 1200 */
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
 
@@ -73,13 +73,13 @@
 #pragma option push -w-rvl -w-rch -w-ccc -w-inl
 #endif /* __BORLANDC__ */
 
-// TAO_IDL - Generated from
-// be\be_visitor_root/root_ch.cpp:63
+// TAO_IDL - Generated from 
+// be\be_visitor_root/root_ch.cpp:62
 
 namespace TAO
 {
   class Collocation_Proxy_Broker;
-
+  
   template<typename T> class Narrow_Utils;
   template<typename T> class AbstractBase_Narrow_Utils;
 }
@@ -89,43 +89,43 @@ namespace TAO
 
 namespace BiDirPolicy
 {
-
+  
   // TAO_IDL - Generated from
   // be\be_visitor_typedef/typedef_ch.cpp:342
-
+  
   typedef CORBA::UShort BidirectionalPolicyValue;
   typedef CORBA::UShort_out BidirectionalPolicyValue_out;
-
+  
   // TAO_IDL - Generated from
   // be\be_visitor_constant/constant_ch.cpp:52
-
+  
   const BiDirPolicy::BidirectionalPolicyValue NORMAL = 0U;
-
+  
   // TAO_IDL - Generated from
   // be\be_visitor_constant/constant_ch.cpp:52
-
+  
   const BiDirPolicy::BidirectionalPolicyValue BOTH = 1U;
-
+  
   // TAO_IDL - Generated from
   // be\be_visitor_constant/constant_ch.cpp:52
-
+  
   const CORBA::PolicyType BIDIRECTIONAL_POLICY_TYPE = 37U;
-
+  
   // TAO_IDL - Generated from
-  // be\be_interface.cpp:611
+  // be\be_interface.cpp:598
 
 #if !defined (_BIDIRPOLICY_BIDIRECTIONALPOLICY__VAR_OUT_CH_)
 #define _BIDIRPOLICY_BIDIRECTIONALPOLICY__VAR_OUT_CH_
-
+  
   class BidirectionalPolicy;
   typedef BidirectionalPolicy *BidirectionalPolicy_ptr;
-
+  
   typedef
     TAO_Objref_Var_T<
         BidirectionalPolicy
       >
     BidirectionalPolicy_var;
-
+  
   typedef
     TAO_Objref_Out_T<
         BidirectionalPolicy
@@ -133,91 +133,91 @@ namespace BiDirPolicy
     BidirectionalPolicy_out;
 
 #endif /* end #if !defined */
-
+  
   // TAO_IDL - Generated from
   // be\be_visitor_interface/interface_ch.cpp:54
 
 #if !defined (_BIDIRPOLICY_BIDIRECTIONALPOLICY_CH_)
 #define _BIDIRPOLICY_BIDIRECTIONALPOLICY_CH_
-
+  
   class TAO_BiDirGIOP_Export BidirectionalPolicy
-    : public virtual CORBA::Policy
+    : public virtual ::CORBA::Policy
   {
   public:
     typedef BidirectionalPolicy_ptr _ptr_type;
     typedef BidirectionalPolicy_var _var_type;
-
+    
     // The static operations.
     static BidirectionalPolicy_ptr _duplicate (BidirectionalPolicy_ptr obj);
-
+    
     static BidirectionalPolicy_ptr _narrow (
         CORBA::Object_ptr obj
         ACE_ENV_ARG_DECL_WITH_DEFAULTS
       );
-
+    
     static BidirectionalPolicy_ptr _unchecked_narrow (
         CORBA::Object_ptr obj
         ACE_ENV_ARG_DECL_WITH_DEFAULTS
       );
-
+    
     static BidirectionalPolicy_ptr _nil (void)
     {
       return (BidirectionalPolicy_ptr)0;
     }
-
-
-
+    
+    
+    
     // TAO_IDL - Generated from
     // be\be_visitor_operation/operation_ch.cpp:46
-
+    
     virtual BiDirPolicy::BidirectionalPolicyValue value (
         ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS
       )
       ACE_THROW_SPEC ((
         CORBA::SystemException
       )) = 0;
-
+    
     // TAO_IDL - Generated from
     // be\be_visitor_operation/operation_ch.cpp:46
-
+    
     virtual ::CORBA::Policy_ptr copy (
         ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS
       )
       ACE_THROW_SPEC ((
         CORBA::SystemException
       )) = 0;
-
+    
     // TAO_IDL - Generated from
     // be\be_visitor_operation/operation_ch.cpp:46
-
+    
     virtual void destroy (
         ACE_ENV_SINGLE_ARG_DECL_WITH_DEFAULTS
       )
       ACE_THROW_SPEC ((
         CORBA::SystemException
       )) = 0;
-
+    
     // TAO_IDL - Generated from
     // be\be_visitor_interface/interface_ch.cpp:208
-
+    
     virtual CORBA::Boolean _is_a (
         const char *type_id
         ACE_ENV_ARG_DECL_WITH_DEFAULTS
       );
-
+    
     virtual const char* _interface_repository_id (void) const;
     virtual CORBA::Boolean marshal (TAO_OutputCDR &cdr);
-
+  
   protected:
     // Abstract or local interface only.
     BidirectionalPolicy (void);
-
+    
     virtual ~BidirectionalPolicy (void);
-
+  
   private:
     // Private and unimplemented for concrete interfaces.
     BidirectionalPolicy (const BidirectionalPolicy &);
-
+    
     void operator= (const BidirectionalPolicy &);
   };
 
@@ -237,19 +237,19 @@ namespace TAO
 
 #if !defined (_BIDIRPOLICY_BIDIRECTIONALPOLICY__TRAITS_CH_)
 #define _BIDIRPOLICY_BIDIRECTIONALPOLICY__TRAITS_CH_
-
+  
   ACE_TEMPLATE_SPECIALIZATION
-  struct TAO_BiDirGIOP_Export Objref_Traits<BiDirPolicy::BidirectionalPolicy>
+  struct TAO_BiDirGIOP_Export Objref_Traits< ::BiDirPolicy::BidirectionalPolicy>
   {
-    static BiDirPolicy::BidirectionalPolicy_ptr duplicate (
-        BiDirPolicy::BidirectionalPolicy_ptr
+    static ::BiDirPolicy::BidirectionalPolicy_ptr duplicate (
+        ::BiDirPolicy::BidirectionalPolicy_ptr
       );
     static void release (
-        BiDirPolicy::BidirectionalPolicy_ptr
+        ::BiDirPolicy::BidirectionalPolicy_ptr
       );
-    static BiDirPolicy::BidirectionalPolicy_ptr nil (void);
+    static ::BiDirPolicy::BidirectionalPolicy_ptr nil (void);
     static CORBA::Boolean marshal (
-        BiDirPolicy::BidirectionalPolicy_ptr p,
+        ::BiDirPolicy::BidirectionalPolicy_ptr p,
         TAO_OutputCDR & cdr
       );
   };
@@ -258,13 +258,13 @@ namespace TAO
 }
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:978
+// be\be_codegen.cpp:961
 
 #if defined (__ACE_INLINE__)
-#include "BiDirPolicyC.i"
+#include "BiDirPolicyC.inl"
 #endif /* defined INLINE */
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#if defined(_MSC_VER)
 #pragma warning(pop)
 #endif /* _MSC_VER */
 
@@ -275,3 +275,5 @@ namespace TAO
 #include /**/ "ace/post.h"
 
 #endif /* ifndef */
+
+
