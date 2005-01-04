c5bb3cdf3b4050b0e290ac887917f1686db645bf
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-04 11:00:37 +0000

    ChangeLogTag: Tue Jan  4 11:00:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/UShortSeqA.cpp
+++ trunk/TAO/tao/UShortSeqA.cpp
@@ -27,26 +27,28 @@
 
 #include "UShortSeqC.h"
 #include "tao/Typecode.h"
+#include "tao/CDR.h"
+#include "tao/Any.h"
 #include "tao/Any_Dual_Impl_T.h"
 
 // TAO_IDL - Generated from
-// be\be_visitor_typecode/typecode_defn.cpp:290
+// be\be_visitor_typecode/typecode_defn.cpp:295
 
 static const CORBA::Long _oc_CORBA_UShortSeq[] =
 {
     TAO_ENCAP_BYTE_ORDER, // byte order
   32,
-  ACE_NTOHL (0x49444c3a),
-  ACE_NTOHL (0x6f6d672e),
-  ACE_NTOHL (0x6f72672f),
-  ACE_NTOHL (0x434f5242),
-  ACE_NTOHL (0x412f5553),
-  ACE_NTOHL (0x686f7274),
-  ACE_NTOHL (0x5365713a),
+  ACE_NTOHL (0x49444c3a), 
+  ACE_NTOHL (0x6f6d672e), 
+  ACE_NTOHL (0x6f72672f), 
+  ACE_NTOHL (0x434f5242), 
+  ACE_NTOHL (0x412f5553), 
+  ACE_NTOHL (0x686f7274), 
+  ACE_NTOHL (0x5365713a), 
   ACE_NTOHL (0x312e3000),  // repository ID = IDL:omg.org/CORBA/UShortSeq:1.0
     10,
-  ACE_NTOHL (0x5553686f),
-  ACE_NTOHL (0x72745365),
+  ACE_NTOHL (0x5553686f), 
+  ACE_NTOHL (0x72745365), 
   ACE_NTOHL (0x71000000),  // name = UShortSeq
     CORBA::tk_sequence, // typecode kind
   12, // encapsulation length
@@ -62,7 +64,7 @@ static CORBA::TypeCode _tc_TAO_tc_CORBA_UShortSeq (
     sizeof (_oc_CORBA_UShortSeq),
     (char *) &_oc_CORBA_UShortSeq,
     0,
-    sizeof (CORBA::UShortSeq)
+    0
   );
 
 namespace CORBA
@@ -71,7 +73,7 @@ namespace CORBA
     &_tc_TAO_tc_CORBA_UShortSeq;
 }
 
-// TAO_IDL - Generated from
+// TAO_IDL - Generated from 
 // be\be_visitor_sequence/any_op_cs.cpp:54
 
 // Copying insertion.
@@ -108,8 +110,8 @@ CORBA::Boolean operator>>= (
     CORBA::UShortSeq *&_tao_elem
   )
 {
-  return _tao_any >>= ACE_const_cast (
-      const CORBA::UShortSeq *&,
+  return _tao_any >>= const_cast<
+      const CORBA::UShortSeq *&> (
       _tao_elem
     );
 }
@@ -129,8 +131,6 @@ CORBA::Boolean operator>>= (
       );
 }
 
-
-
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
   template class
@@ -145,5 +145,4 @@ CORBA::Boolean operator>>= (
         CORBA::UShortSeq \
       >
 
-#endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
-
+#endif /* !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */ 
