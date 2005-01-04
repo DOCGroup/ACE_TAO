f299f0ae918aeb17c34d70c849b8507f1bb51c1b
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-04 12:39:11 +0000

    ChangeLogTag: Tue Jan  4 12:25:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/TAOC.h
+++ TAO/tao/TAOC.h
@@ -74,7 +74,7 @@
 #endif /* __BORLANDC__ */
 
 // TAO_IDL - Generated from 
-// be\be_visitor_root/root_ch.cpp:63
+// be\be_visitor_root/root_ch.cpp:62
 
 namespace TAO
 {
@@ -161,7 +161,7 @@ namespace TAO
   const CORBA::PolicyType BUFFERING_CONSTRAINT_POLICY_TYPE = 1413545985U;
   
   // TAO_IDL - Generated from
-  // be\be_interface.cpp:603
+  // be\be_interface.cpp:598
 
 #if !defined (_TAO_BUFFERINGCONSTRAINTPOLICY__VAR_OUT_CH_)
 #define _TAO_BUFFERINGCONSTRAINTPOLICY__VAR_OUT_CH_
@@ -343,7 +343,7 @@ TAO_Export CORBA::Boolean operator<< (TAO_OutputCDR &, const TAO::BufferingConst
 TAO_Export CORBA::Boolean operator>> (TAO_InputCDR &, TAO::BufferingConstraint &);
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:963
+// be\be_codegen.cpp:961
 
 #if defined (__ACE_INLINE__)
 #include "TAOC.inl"
--- TAO/tao/TAOS_T.inl
+++ TAO/tao/TAOS_T.inl
@@ -25,17 +25,4 @@
 // Information about TAO is available at:
 //     http://www.cs.wustl.edu/~schmidt/TAO.html
 
-// TAO_IDL - Generated from 
-// be\be_codegen.cpp:683
 
-#ifndef _TAO_IDL_ORIG_TAOS_T_CPP_
-#define _TAO_IDL_ORIG_TAOS_T_CPP_
-
-#include "TAOS_T.h"
-
-#if !defined (__ACE_INLINE__)
-#include "TAOS_T.inl"
-#endif /* !defined INLINE */
-
-
-#endif /* ifndef */
