3ef86f51709cee702ba88768aaa528d5ccd85f22
Johnny Willemsen <jwillemsen@remedy.nl>
2005-01-06 08:53:10 +0000

    ChangeLogTag: Thu Jan  6 08:52:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- examples/IPC_SAP/SPIPE_SAP/producer_msg.cpp
+++ examples/IPC_SAP/SPIPE_SAP/producer_msg.cpp
@@ -5,6 +5,8 @@
 #include "ace/SPIPE_Connector.h"
 #include "ace/Log_Msg.h"
 #include "ace/OS_NS_stdio.h"
+#include "ace/OS_NS_unistd.h"
+#include "ace/Time_Value.h"
 
 ACE_RCSID(SPIPE_SAP, producer_msg, "$Id$")
 
