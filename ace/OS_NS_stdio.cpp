217821063acd934504139a97e6cbce4de46ee877
Ossama Othman <ossama-othman@users.noreply.github.com>
2005-01-13 06:52:44 +0000

    ChangeLogTag:Wed Jan 12 22:52:12 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- ace/OS_NS_stdio.cpp
+++ ace/OS_NS_stdio.cpp
@@ -1,9 +1,12 @@
-// -*- C++ -*-
 // $Id$
 
 #include "ace/OS_NS_stdio.h"
 
-ACE_RCSID(ace, OS_NS_stdio, "$Id$")
+
+ACE_RCSID (ace,
+           OS_NS_stdio,
+           "$Id$")
+
 
 #if !defined (ACE_HAS_INLINED_OSCALLS)
 # include "ace/OS_NS_stdio.inl"
@@ -248,45 +251,6 @@ ACE_OS::fprintf (FILE *fp, const wchar_t *format, ...)
 }
 #endif /* ACE_HAS_WCHAR */
 
-char *
-ACE_OS::gets (char *str, int n)
-{
-  ACE_OS_TRACE ("ACE_OS::gets");
-  int c;
-  char *s = str;
-
-  if (str == 0 || n < 0) n = 0;
-  if (n == 0) str = 0;
-  else n--;
-
-#   if defined (DIGITAL_UNIX)
-  // On Tru64 getchar is a macro which calls fgetc, in that case we must explicit
-  // use the global namespace, because fgetc is also in the ACE_OS namespace
-  while ((c = ::getchar ()) != '\n')
-#   else
-  while ((c = getchar ()) != '\n')
-#   endif
-    {
-
-      if (c == EOF && errno == EINTR)
-        {
-#   if defined (ACE_HAS_SIGNAL_SAFE_OS_CALLS)
-          continue;
-#   else
-          break;
-#   endif /* ACE_HAS_SIGNAL_SAFE_OS_CALLS */
-        }
-
-      if (c == EOF)
-        break;
-
-      if (n > 0)
-        n--, *s++ = c;
-    }
-  if (s) *s = '\0';
-
-  return (c == EOF) ? 0 : str;
-}
 
 // The following *printf functions aren't inline because
 // they use varargs.
