c37f4edc485c5a00d56e4aa4fcc39d46bd3b8f21
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-14 17:40:27 +0000

    *** empty log message ***

--- TAO/CIAO/DAnCE/ciao/Container_Base.h
+++ TAO/CIAO/DAnCE/ciao/Container_Base.h
@@ -203,13 +203,14 @@ namespace CIAO
                                        ACE_ENV_ARG_DECL_WITH_DEFAULTS)
       ACE_THROW_SPEC ((CORBA::SystemException));
 
+/*
     virtual void add_servant_map (PortableServer::ObjectId &oid,
                                   Dynamic_Component_Servant_Base* servant
                                   ACE_ENV_ARG_DECL) = 0;
 
     virtual void delete_servant_map (PortableServer::ObjectId &oid
                                      ACE_ENV_ARG_DECL) = 0;
-
+*/
     // Install a component servant.
     CORBA::Object_ptr install_component (PortableServer::Servant p,
                                          PortableServer::ObjectId_out oid
@@ -221,9 +222,11 @@ namespace CIAO
                                   ACE_ENV_ARG_DECL_WITH_DEFAULTS)
       ACE_THROW_SPEC ((CORBA::SystemException));
 
+/*
     virtual CORBA::Object_ptr get_home_objref (PortableServer::Servant p
                                   ACE_ENV_ARG_DECL_WITH_DEFAULTS)
       ACE_THROW_SPEC ((CORBA::SystemException)) = 0;
+*/
 
     // Uninstall a servant for component or home.
     void uninstall (CORBA::Object_ptr objref,
