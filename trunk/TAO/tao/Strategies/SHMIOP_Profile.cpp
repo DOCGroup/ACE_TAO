e80b3b1cc6da419ad3f316b59feb2c323b95dec1
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-19 12:26:19 +0000

    ChangeLogTag: Tue Jan 18 19:59:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Strategies/SHMIOP_Profile.cpp
+++ trunk/TAO/tao/Strategies/SHMIOP_Profile.cpp
@@ -171,7 +171,7 @@ TAO_SHMIOP_Profile::parse_string_i (const char *string
   if (ACE_OS::strspn (tmp.in (), "1234567890") == length)
     {
       this->endpoint_.port_ =
-        ACE_static_cast (CORBA::UShort, ACE_OS::atoi (tmp.in ()));
+        static_cast <CORBA::UShort> (ACE_OS::atoi (tmp.in ()));
     }
   else
     {
@@ -267,7 +267,7 @@ CORBA::Boolean
 TAO_SHMIOP_Profile::do_is_equivalent (const TAO_Profile *other_profile)
 {
   const TAO_SHMIOP_Profile *op =
-    ACE_dynamic_cast (const TAO_SHMIOP_Profile *, other_profile);
+    dynamic_cast <const TAO_SHMIOP_Profile *> (other_profile);
 
   if (op == 0)
     return 0;
@@ -347,7 +347,7 @@ TAO_SHMIOP_Profile::to_string (ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
                    1 /* object key separator */ +
                    ACE_OS::strlen (key.in ()));
 
-  char * buf = CORBA::string_alloc (ACE_static_cast (CORBA::ULong, buflen));
+  char * buf = CORBA::string_alloc (static_cast <CORBA::ULong> (buflen));
 
   static const char digits [] = "0123456789";
 
@@ -433,8 +433,7 @@ TAO_SHMIOP_Profile::encode_endpoints (void)
 
   IOP::TaggedComponent tagged_component;
   tagged_component.tag = TAO_TAG_ENDPOINTS;
-  tagged_component.component_data.length (ACE_static_cast (CORBA::ULong,
-                                                           length));
+  tagged_component.component_data.length (static_cast <CORBA::ULong>(length));
   CORBA::Octet *buf =
     tagged_component.component_data.get_buffer ();
 
@@ -466,14 +465,14 @@ TAO_SHMIOP_Profile::decode_endpoints (void)
       const CORBA::Octet *buf =
         tagged_component.component_data.get_buffer ();
 
-      TAO_InputCDR in_cdr (ACE_reinterpret_cast (const char*, buf),
+      TAO_InputCDR in_cdr (reinterpret_cast <const char*> (buf),
                            tagged_component.component_data.length ());
 
       // Extract the Byte Order.
       CORBA::Boolean byte_order;
       if ((in_cdr >> ACE_InputCDR::to_boolean (byte_order)) == 0)
         return -1;
-      in_cdr.reset_byte_order (ACE_static_cast(int, byte_order));
+      in_cdr.reset_byte_order (static_cast<int> (byte_order));
 
       // Extract endpoints sequence.
       TAO::IIOPEndpointSequence endpoints;
