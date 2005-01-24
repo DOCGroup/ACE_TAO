1cde922e44442f16558ad289f42ffab41c374f4e
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-24 11:28:54 +0000

    ChangeLogTag: Mon Jan 24 11:28:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/POArefactor2/TAO/tao/PortableServer/IdAssignmentPolicyValueSystem.h
+++ branches/POArefactor2/TAO/tao/PortableServer/IdAssignmentPolicyValueSystem.h
@@ -14,8 +14,7 @@
 #define TAO_PORTABLESERVER_IDASSIGNMENTPOLICYVALUESYSTEM_H
 #include /**/ "ace/pre.h"
 
-#include "tao/POrtableServer/portableserver_export.h"
-
+#include "tao/PortableServer/portableserver_export.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
