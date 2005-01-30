4be020c437576cb7761a5f8da3a3d0d3366e7551
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-01-30 21:19:21 +0000

    *** empty log message ***

--- TAO/CIAO/DAnCE/ciao/Dynamic_Component_Servant_T.cpp
+++ TAO/CIAO/DAnCE/ciao/Dynamic_Component_Servant_T.cpp
@@ -95,8 +95,6 @@ namespace CIAO
                                    this->home_servant_,
                                    this->container_);
 
-    ACE_DEBUG ((LM_DEBUG, "created the servant and activated\n"));
-
     if (component_removed_ == 1)
       {
         // svt->ciao_preactivate ();
