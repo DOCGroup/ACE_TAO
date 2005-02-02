081a3196d22590bff4793d231b3e9c917d198bf4
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-02-02 15:41:01 +0000

    This commit was manufactured by cvs2svn to create branch 'dynamic_swapping_1'.

--- TAO/orbsvcs/orbsvcs/Event/EC_Supplier_Filter_Builder.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "EC_Supplier_Filter_Builder.h"
-
-ACE_RCSID(Event, EC_Supplier_Filter_Builder, "$Id$")
-
-TAO_EC_Supplier_Filter_Builder::~TAO_EC_Supplier_Filter_Builder (void)
-{
-}
