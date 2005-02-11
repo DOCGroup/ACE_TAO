eeb3a49fbe79862fabd3bc586ec9a27673ad6b80
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-02-11 14:00:16 +0000

    ChangeLogTag: Fri Feb 11 09:41:39 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ branches/POArefactor3/TAO/tao/IORInterceptor/makediffs.bat
@@ -0,0 +1,3 @@
+diff -wub orig/IORInterceptorC.h IORInterceptorC.h > diff/IORInterceptor.diff
+diff -wub orig/IORInterceptorC.cpp IORInterceptorC.cpp >> diff/IORInterceptor.diff
+unix2dos diff\IORInterceptor.diff
