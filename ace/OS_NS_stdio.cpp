c7cc36234effd4e19024ab8a82445f3088ee9e48
schmidt <douglascraigschmidt@users.noreply.github.com>
2005-02-27 14:57:41 +0000

    ChangeLogTag:Sun Feb 27 08:51:23 2005  Douglas C. Schmidt  <schmidt@cs.wustl.edu>

--- ace/OS_NS_stdio.cpp
+++ ace/OS_NS_stdio.cpp
@@ -327,6 +327,11 @@ ACE_OS::snprintf (wchar_t *buf, size_t maxlen, const wchar_t *format, ...)
   // as a substitute, which does have the same signature as the UNIX98 one.
   ACE_OSCALL (ACE_SPRINTF_ADAPTER (::_vsnwprintf (buf, maxlen, format, ap)),
               int, -1, result);
+  // Win32 doesn't regard a full buffer with no 0-terminate as an
+  // overrun.
+  if (result == maxlen)
+    result = -1;
+ 
   // Win32 doesn't 0-terminate the string if it overruns maxlen.
   if (result == -1)
     buf[maxlen-1] = '\0';
