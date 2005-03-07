7a4f8334b2537ebc1d318bea61b7a549232fc049
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-03-07 13:41:58 +0000

    ChangeLogTag: Mon Mar 07 13:40:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tests/Portable_Interceptors/Collocated/Service_Context_Manipulation/run_test.pl
+++ trunk/TAO/tests/Portable_Interceptors/Collocated/Service_Context_Manipulation/run_test.pl
@@ -8,15 +8,6 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 use lib '../../../../../bin';
 use PerlACE::Run_Test;
 
-print STDERR <<EOF;
-INFO: Do not expect this test to work until portable interceptors
-INFO: get invoked during thru_POA colocation optimizations.
-INFO:
-INFO: See http://deuce.doc.wustl.edu/bugzilla/show_bug.cgi?id=586
-INFO: for more information.
-
-EOF
-
 $iorfile = PerlACE::LocalFile ("test.ior");
 unlink $iorfile;
 $status = 0;
@@ -77,7 +68,6 @@ print STDERR "======== Running in no collocation mode and two ORBS \n";
 $SV->Arguments ("-n -ORBCollocation per-orb");
 $sv = $SV->SpawnWaitKill (60);
 
-$SV->check_return_value (0);
 if ($sv != 0) {
     print STDERR "ERROR in Collocated_Test\n";
     $status = 1;
