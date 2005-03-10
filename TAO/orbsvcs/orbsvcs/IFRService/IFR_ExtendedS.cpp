5e6995822b11ffc77731bce89a31e29f4d210a31
Johnny Willemsen <jwillemsen@remedy.nl>
2005-03-10 20:01:35 +0000

    ChangeLogTag: Thu Mar 10 19:58:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/IFRService/IFR_ExtendedS.cpp
+++ TAO/orbsvcs/orbsvcs/IFRService/IFR_ExtendedS.cpp
@@ -27,7 +27,7 @@
 
 
 // TAO_IDL - Generated from 
-// be\be_codegen.cpp:609
+// be\be_codegen.cpp:617
 
 #ifndef _TAO_IDL_ORIG_IFR_EXTENDEDS_CPP_
 #define _TAO_IDL_ORIG_IFR_EXTENDEDS_CPP_
@@ -35,7 +35,6 @@
 
 #include "IFR_ExtendedS.h"
 #include "tao/PortableServer/Operation_Table_Perfect_Hash.h"
-#include "tao/PortableServer/ForwardRequestC.h"
 #include "tao/PortableServer/Upcall_Command.h"
 #include "tao/PortableServer/Upcall_Wrapper.h"
 #include "tao/PortableServer/Basic_SArguments.h"
@@ -7314,7 +7313,7 @@ POA_CORBA::ValueBoxDef::_this (ACE_ENV_SINGLE_ARG_DECL)
 }
 
 // TAO_IDL - Generated from
-// be\be_visitor_root/root.cpp:1575
+// be\be_visitor_root/root.cpp:1582
 
 #if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
 
