50f9d85987c27f9fc08bd399f7f1bf5b2c1e0860
bala <balanatarajan@users.noreply.github.com>
2005-03-14 08:34:33 +0000

    CHangeLogTag:Mon Mar 14 13:55:41 (IST) 2005  Balachandran Natarajan  <bala@dre.vanderbilt.edu>

---
+++ TAO/tests/Hang_Shutdown/test_i.h
@@ -0,0 +1,55 @@
+//
+// $Id$
+//
+#ifndef TAO_HANG_TEST_I_H
+#define TAO_HANG_TEST_I_H
+#include /**/ "ace/pre.h"
+
+#include "testS.h"
+
+#if defined (_MSC_VER)
+# if (_MSC_VER >= 1200)
+#  pragma warning(push)
+# endif /* _MSC_VER >= 1200 */
+# pragma warning (disable:4250)
+#endif /* _MSC_VER */
+
+#include "ace/OS_NS_unistd.h"
+
+namespace TAO
+{
+  namespace Test
+  {
+    class test_i
+      : public virtual POA_TAO::Test::Hang
+      , public virtual PortableServer::RefCountServantBase
+    {
+    public:
+      // = The skeleton methods
+      virtual void send_stuff (const char* str,
+                               bool flag)
+        ACE_THROW ((CORBA::SystemException))
+      {
+        ACE_DEBUG ((LM_DEBUG,
+                    "(%P|%t) - [%C]\n", str));
+
+        if (flag)
+          {
+            ACE_OS::sleep (10);
+
+            ACE_DEBUG ((LM_DEBUG,
+                        "TAO (%P|%t) - Returning from send_stuff\n", str));
+          }
+
+        return;
+      }
+    private:
+    };
+  }
+}
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma warning(pop)
+#endif /* _MSC_VER */
+
+#include /**/ "ace/post.h"
+#endif /* TAO_HANG_TEST_I_H */
