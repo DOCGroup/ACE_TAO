d71e3b8f9ecf1ac6fa017120e6e30eaf146b2891
Johnny Willemsen <jwillemsen@remedy.nl>
2005-03-18 10:52:15 +0000

    ChangeLogTag: Fri Mar 18 10:52:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/tests/EC_MT_Mcast/MCast.cpp
+++ TAO/orbsvcs/tests/EC_MT_Mcast/MCast.cpp
@@ -421,8 +421,3 @@ int parse_args (int argc, char *argv[])
   return 0;
 }
 
-// ****************************************************************
-
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-#elif defined(ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
