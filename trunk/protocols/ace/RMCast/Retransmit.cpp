1e72a4dba4067a671e24e330fd2b3d32aa9950e3
boris <boris@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-03-18 07:33:31 +0000

    ChangeLogTag: Fri Mar 18 09:49:32 2005  Boris Kolpackov  <boris@kolpackov.net>

--- trunk/protocols/ace/RMCast/Retransmit.cpp
+++ trunk/protocols/ace/RMCast/Retransmit.cpp
@@ -2,7 +2,8 @@
 // author    : Boris Kolpackov <boris@kolpackov.net>
 // cvs-id    : $Id$
 
-#include <ace/OS.h>
+#include <ace/Time_Value.h>     // ACE_Time_Value
+#include <ace/OS_NS_sys_time.h> // gettimeofday
 
 #include "Retransmit.h"
 
