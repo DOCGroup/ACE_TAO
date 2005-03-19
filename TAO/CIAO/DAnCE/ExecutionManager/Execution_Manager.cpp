3fa246e120ea529e6fac804e4563d46df5422737
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-03-19 21:08:04 +0000

    Sat Mar 19 15:06:37 2005  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- TAO/CIAO/DAnCE/ExecutionManager/Execution_Manager.cpp
+++ TAO/CIAO/DAnCE/ExecutionManager/Execution_Manager.cpp
@@ -1,5 +1,4 @@
 //--*C++*--
-
 //==============================================================
 /**
  * @file Execution_Manager.cpp
@@ -29,6 +28,7 @@
 
 // Include Name Service header
 #include "orbsvcs/CosNamingC.h"
+#include "tao/Utils/Implicit_Deactivator.h"
 
 #include "ace/SString.h"
 #include "ace/Read_Buffer.h"
@@ -36,216 +36,231 @@
 #include "ace/OS_NS_unistd.h"
 #include "ace/OS_NS_stdio.h"
 
-const char *ior_file_name_ = "executionManager.ior";
-char * default_svcconf_ = 0;
-char * svcconf_config_ = 0;
-const char * name = "ExecutionManager";
-const char * init_file_name = "deployment.dat";
-
-int write_to_ior_ = 0;
-int register_with_ns_ = 0;
-
-int
-parse_args (int argc, char *argv[])
+namespace CIAO
 {
-  ACE_Get_Opt get_opts (argc, argv, "o:c:m:i:n");
-  int c;
-  while ((c = get_opts ()) != -1)
-    switch (c)
-      {
-      case 'o':  // get the file name to write to
-       ior_file_name_ = get_opts.opt_arg ();
-       write_to_ior_ = 1;
-      break;
+  namespace Execution_Manager
+  {
+    const char *ior_file_name_ = "executionManager.ior";
+    const char *init_file_name = "deployment.dat";
 
-      case 'c':  // get the default svc.conf filename
-        default_svcconf_ = get_opts.opt_arg ();
-      break;
+    bool register_with_ns_ = 0;
 
-      case 'i':  // get the svc.conf map configuration filename
-        init_file_name = get_opts.opt_arg ();
-      break;
-
-      case 'n':
-        register_with_ns_ = 1;
-      break;
-
-      case '?':  // display help for use of the server.
-      default:
-        ACE_ERROR_RETURN ((LM_ERROR,
-                           "usage:  %s\n"
-                           "-o <ior_output_file>\n"
-                           "-c <svc.conf file>\n"
-                           "-i <installation data filename>\n"
-                           "-n <use naming service>\n"
-                           "\n",
-                           argv [0]),
-                          -1);
-      }
-
-  return 0;
-}
-
-int
-write_IOR(const char* ior)
-{
-  FILE* ior_output_file_ =
-    ACE_OS::fopen (ior_file_name_, "w");
-
-  if (ior_output_file_)
+    bool
+    parse_args (int argc, char *argv[])
     {
-      ACE_OS::fprintf (ior_output_file_,
-                       "%s",
-                       ior);
-      ACE_OS::fclose (ior_output_file_);
+      ACE_Get_Opt get_opts (argc, argv, "o:m:i:n");
+      int c;
+      while ((c = get_opts ()) != -1)
+        switch (c)
+          {
+          case 'o':
+            ior_file_name_ = get_opts.opt_arg ();
+            break;
+          case 'i':
+            init_file_name = get_opts.opt_arg ();
+            break;
+          case 'n':
+            register_with_ns_ = 1;
+            break;
+          case '?':  // display help for use of the server.
+          default:
+            ACE_ERROR_RETURN ((LM_ERROR,
+                               "usage:  %s\n"
+                               "-o <ior_output_file>\n"
+                               "-i <installation data filename>\n"
+                               "-n <use naming service>\n"
+                               "\n",
+                               argv [0]),
+                              false);
+          }
+
+      return true;
     }
 
-  return 0;
-}
+    bool
+    write_ior_file (CORBA::ORB_ptr orb,
+                    CIAO::ExecutionManagerDaemon_ptr obj
+                    ACE_ENV_ARG_DECL)
+    {
+      CORBA::String_var ior =
+        orb->object_to_string (obj
+                               ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK_RETURN (false);
 
-bool
-register_with_ns (const char * name_context,
-                  CORBA::ORB_ptr orb,
-                  CIAO::ExecutionManagerDaemon_ptr obj
-                  ACE_ENV_ARG_DECL)
-{
-  // Naming Service related operations
-  CORBA::Object_var naming_context_object =
-    orb->resolve_initial_references ("NameService"
-                                     ACE_ENV_ARG_PARAMETER);
-  ACE_CHECK_RETURN (false);
+      FILE* ior_output_file_ =
+        ACE_OS::fopen (ior_file_name_, "w");
 
-  CosNaming::NamingContext_var naming_context =
-    CosNaming::NamingContext::_narrow (naming_context_object.in ());
+      if (ior_output_file_)
+        {
+          ACE_OS::fprintf (ior_output_file_,
+                           "%s",
+                           ior.in ());
+          ACE_OS::fclose (ior_output_file_);
+        }
+      else
+        return false;
 
-  // Initialize the Naming Sequence
-  CosNaming::Name name (1);
-  name.length (1);
-  // Register the name with the NS
-  name[0].id = name_context;
+      return true;
+    }
 
-  // Register the servant with the Naming Service
-  naming_context->bind (name,
-                        obj
+    bool
+    register_with_ns (CORBA::ORB_ptr orb,
+                      CIAO::ExecutionManagerDaemon_ptr obj
+                      ACE_ENV_ARG_DECL)
+    {
+      // Naming Service related operations
+      CORBA::Object_var naming_context_object =
+        orb->resolve_initial_references ("NameService"
+                                         ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK_RETURN (false);
+
+      CosNaming::NamingContext_var naming_context =
+        CosNaming::NamingContext::_narrow (naming_context_object.in ());
+
+      // Initialize the Naming Sequence
+      CosNaming::Name name (2);
+      name.length (2);
+
+      // String dup required for MSVC6
+      name[0].id = CORBA::string_dup ("CIAO");
+      name[1].id = CORBA::string_dup ("ExecutionManager");
+
+      // Register the servant with the Naming Service
+      naming_context->bind (name,
+                            obj
+                            ACE_ENV_ARG_PARAMETER);
+      ACE_CHECK_RETURN (false);
+
+      return true;
+    }
+
+    int
+    run_main (int argc, char *argv[])
+    {
+      ACE_DECLARE_NEW_CORBA_ENV;
+
+      ACE_TRY
+        {
+          CORBA::ORB_var orb =
+            CORBA::ORB_init (argc,
+                             argv,
+                             ""
+                             ACE_ENV_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+          if (!parse_args (argc, argv))
+            return -1;
+
+          // Get reference to Root POA.
+          CORBA::Object_var obj
+            = orb->resolve_initial_references ("RootPOA"
+                                               ACE_ENV_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+          PortableServer::POA_var poa =
+            PortableServer::POA::_narrow (obj.in ()
+                                          ACE_ENV_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+
+          if (poa.in () == 0)
+            ACE_ERROR_RETURN ((LM_ERROR,
+                              "(%P|%t) CIAO_ExecutionManager: "
+                               "Nil POA panic error, returning \n"),
+                               -1);
+
+          // Create and install the CIAO Daemon servant
+          Execution_Manager_Impl *daemon_servant = 0;
+          ACE_NEW_RETURN (daemon_servant,
+                          Execution_Manager_Impl(orb.in (),
+                                                 poa.in (),
+                                                 init_file_name),
+                          -1);
+
+          // Implicit activation
+          PortableServer::ServantBase_var safe_daemon (daemon_servant);
+
+          CIAO::ExecutionManagerDaemon_var daemon =
+            daemon_servant->_this ();
+
+          TAO::Utils::Implicit_Deactivator de (daemon_servant
+                                               ACE_ENV_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+          bool retval = false;
+
+          if (register_with_ns_)
+            {
+              retval =
+                register_with_ns (orb.in (),
+                                  daemon.in ()
+                                  ACE_ENV_ARG_PARAMETER);
+              ACE_TRY_CHECK;
+            }
+          else
+            {
+              retval =
+                write_ior_file (orb.in (),
+                                daemon.in ()
+                                ACE_ENV_ARG_PARAMETER);
+            }
+
+          if (!retval)
+            return -1;
+
+          // Activate POA manager
+          PortableServer::POAManager_var mgr =
+            poa->the_POAManager (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+          if (mgr.in () == 0)
+            ACE_ERROR_RETURN ((LM_ERROR,
+                               "(%P|%t) CIAO_ExecutionManager: "
+                               "Nil POA Manager error, returning \n"),
+                               -1);
+
+          mgr->activate (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+          // End Deployment part
+          ACE_DEBUG ((LM_DEBUG,
+                      "CIAO_ExecutionManager is running...\n"));
+
+          // Run the main event loop for the ORB.
+          orb->run (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+
+          // Forget the pointer. The POA will take care of it during
+          // destroy.
+          (void) de.release ();
+
+          poa->destroy (1,
+                        1
                         ACE_ENV_ARG_PARAMETER);
-  ACE_CHECK_RETURN (false);
+          ACE_TRY_CHECK;
 
-  return true;
+          orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+        }
+      ACE_CATCHANY
+        {
+          ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                               "CIAO_ExecutionManager::main\t\n");
+          return -1;
+        }
+      ACE_ENDTRY;
+
+      ACE_DEBUG ((LM_DEBUG,
+                  "CIAO_ExecutionManager has closed\n"));
+      return 0;
+    }
+
+  }
 }
 
 int
 main (int argc, char *argv[])
 {
-  ACE_TRY_NEW_ENV
-    {
-      // @@ Error checking is very bad! Need to be fixed!
-      // -- bala
-      // Initialize orb
-      CORBA::ORB_var orb = CORBA::ORB_init (argc,
-                                            argv,
-                                            ""
-                                            ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      if (parse_args (argc, argv) != 0)
-        return -1;
-
-      // Get reference to Root POA.
-      CORBA::Object_var obj
-        = orb->resolve_initial_references ("RootPOA"
-                                           ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      PortableServer::POA_var poa
-        = PortableServer::POA::_narrow (obj.in ()
-                                        ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      CORBA::Object_var table_object =
-        orb->resolve_initial_references ("IORTable"
-                                         ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      IORTable::Table_var adapter =
-        IORTable::Table::_narrow (table_object.in ()
-                                  ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      if (CORBA::is_nil (adapter.in ()))
-          ACE_ERROR_RETURN ((LM_ERROR, "Nil IORTable\n"), -1);
-
-      // Create and install the CIAO Daemon servant
-      CIAO::ExecutionManager_Impl *daemon_servant = 0;
-      ACE_NEW_RETURN (daemon_servant,
-                      CIAO::ExecutionManager_Impl(orb.in (),
-                                                  poa.in (),
-                                                  init_file_name),
-                      -1);
-
-      // Implicit activation
-      PortableServer::ServantBase_var safe_daemon (daemon_servant);
-      CIAO::ExecutionManagerDaemon_var daemon =
-        daemon_servant->_this ();
-
-      // Now register daemon with IOR table and write its IOR.
-      CORBA::String_var str =
-        orb->object_to_string (daemon.in ()
-                               ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      adapter->bind ("ExecutionManager",
-                     str.in ()
-                     ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      if (write_to_ior_)
-        write_IOR (str.in ());
-      else if (register_with_ns_)
-        {
-          // Register this name with the Naming Service
-          (void) register_with_ns (name,
-                                   orb.in (),
-                                   daemon.in ()
-                                   ACE_ENV_ARG_PARAMETER);
-          ACE_TRY_CHECK;
-        }
-
-#if 0
-      ACE_DEBUG ((LM_INFO,
-                  "CIAO_ExecutionMananger IOR: %s\n", str.in ()));
-#endif /*if 0*/
-
-      // Activate POA manager
-      PortableServer::POAManager_var mgr
-        = poa->the_POAManager (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      mgr->activate (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      // End Deployment part
-      ACE_DEBUG ((LM_DEBUG,
-                  "CIAO_ExecutionManager is running...\n"));
-
-      // Run the main event loop for the ORB.
-      orb->run (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK
-
-      poa->destroy (1, 1 ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-    }
-  ACE_CATCHANY
-    {
-      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
-                           "CIAO_ExecutionManager::main\t\n");
-      return 1;
-    }
-  ACE_ENDTRY;
-
-  ACE_DEBUG ((LM_DEBUG,
-              "CIAO_ExecutionManager has closed\n"));
-  return 0;
+  return CIAO::Execution_Manager::run_main (argc,
+                                           argv);
 }
