643be454b9e9482d63ab0fce881d0f56e8b8610b
Ossama Othman <ossama-othman@users.noreply.github.com>
2005-03-28 07:25:30 +0000

    *** empty log message ***

--- TAO/TAO_IDL/be/be_visitor_typecode/union_typecode.cpp
+++ TAO/TAO_IDL/be/be_visitor_typecode/union_typecode.cpp
@@ -29,37 +29,19 @@ TAO::be_visitor_union_typecode::visit_union (be_union * node)
      << "// " << __FILE__ << ":" << __LINE__ << be_nl << be_nl;
 
 
-  std::string const fields_name (std::string ("_tao_cases_")
-                                 + node->flat_name ());
-
-
   be_type * const discriminant_type =
     be_type::narrow_from_decl (node->disc_type ());
 
   ACE_ASSERT (discriminant_type != 0);
 
-  // Generate array containing union non-default case
-  // characteristics.
-  os << "static "
-     << "TAO::TypeCode::Non_Default_Case<"
-     << discriminant_type->full_name ()
-     << ", char const *> const "
-     << fields_name.c_str ()
-     << "[] =" << be_idt_nl
-     << "{" << be_idt_nl;
-
-  be_union_branch * default_case = 0;
-  if (this->visit_cases (node, default_case) != 0)
+  if (this->visit_cases (node) != 0)
     return -1;
-
-  os << be_uidt_nl
-     << "};" << be_uidt_nl << be_nl;
 
   // Generate the TypeCode instantiation.
   os
     << "static TAO::TypeCode::Union<char const *," << be_nl
     << "                            TAO::TypeCode::Case<char const *>"
-    << " const *," << be_nl
+    << " const * const *," << be_nl
     << "                            TAO::Null_RefCount_Policy>"
     << be_idt_nl
     << "_tao_tc_" << node->flat_name () << " (" << be_idt_nl
@@ -67,47 +49,38 @@ TAO::be_visitor_union_typecode::visit_union (be_union * node)
     << "\"" << node->original_local_name () << "\"," << be_nl
     << "&" << discriminant_type->tc_name () << "," << be_nl
     << "_tao_cases_" << node->flat_name () << "," << be_nl
-    << node->nfields () << ","
-    << node->default_index () << "," << be_nl;
-
-  if (default_case)
-    {
-      be_type * const type =
-        be_type::narrow_from_decl (default_case->field_type ());
-
-      os << "\"" << default_case->original_local_name () << "\", "
-         << "&"  << type->tc_name ();
-    }
-  else
-    {
-      os << "0, 0"; // No default_case.
-    }
-
-  os << ");" << be_uidt_nl
-     << be_uidt_nl;
+    << node->nfields () << ", "
+    << node->default_index () << ");" << be_uidt_nl
+    << be_uidt_nl;
 
   return
     this->gen_typecode_ptr (be_type::narrow_from_decl (node));
 }
 
 int
-TAO::be_visitor_union_typecode::visit_cases (be_union * node,
-                                             be_union_branch *& default_case)
+TAO::be_visitor_union_typecode::visit_cases (be_union * node)
 {
-  AST_Field ** member_ptr = 0;
-
-  size_t const count = node->nfields ();
-
-  TAO_OutStream & os = *this->ctx_->stream ();
-
-  default_case = 0;
+  // Generate array containing union case/branch characterstics.
 
   be_type * const discriminant_type =
     be_type::narrow_from_decl (node->disc_type ());
 
+  std::string const fields_name (std::string ("_tao_cases_")
+                                 + node->flat_name ());
+
+  TAO_OutStream & os = *this->ctx_->stream ();
+
+  // Start with static instances of each array element-to-be.
+  size_t const count = node->nfields ();
+
   for (size_t i = 0; i < count; ++i)
     {
+      AST_Field ** member_ptr = 0;
+
       node->field (member_ptr, i);
+
+      be_type * const type =
+        be_type::narrow_from_decl ((*member_ptr)->field_type ());
 
       be_union_branch * const branch =
         be_union_branch::narrow_from_decl (*member_ptr);
@@ -116,12 +89,12 @@ TAO::be_visitor_union_typecode::visit_cases (be_union * node,
 
       if (branch->label ()->label_kind () == AST_UnionLabel::UL_label)
         {
-          // Only deal with non-default cases.  The default case has
-          // special handling in the union TypeCode implementation.
+          // Non-default case.
 
-          os << "TAO::TypeCode::Non_Default_Case<"
+          os << "static TAO::TypeCode::Non_Default_Case<"
              << discriminant_type->full_name () << ", "
-             << "char const *> (";
+             << "char const *> const "
+             << fields_name.c_str () << "_" << i <<" (";
 
           // Generate the label value.  Only the first label value is
           // used in the case where multiple labels are used for the
@@ -129,24 +102,42 @@ TAO::be_visitor_union_typecode::visit_cases (be_union * node,
           branch->gen_label_value (&os, 0);
 
           os << ", ";
-
-          be_type * const type =
-            be_type::narrow_from_decl ((*member_ptr)->field_type ());
-
-          os << "\"" << branch->original_local_name () << "\", "
-             << "&"  << type->tc_name ()
-             << ")";
-
-          if (i < count - (node->default_index () != -1 ? 2 : 1))
-            os << ",";
-
-          os << be_nl;
         }
       else
         {
-          default_case = branch;
+          // Default case.
+
+          ACE_ASSERT (branch->label ()->label_kind ()
+                      == AST_UnionLabel::UL_default);
+
+          os << "static TAO::TypeCode::Default_Case<char const *> const "
+             << fields_name.c_str () << "_" << i << "(";        
         }
+
+      os << "\"" << branch->original_local_name () << "\", "
+         << "&"  << type->tc_name ()
+         << ");" << be_nl;
     }
+
+  // Now generate the TAO::TypeCode::Case array.
+  os << be_nl
+     << "static TAO::TypeCode::Case<char const *> const * const "
+     << fields_name.c_str ()
+     << "[] =" << be_idt_nl
+     << "{" << be_idt_nl;
+
+  for (size_t n = 0; n < count; ++n)
+    {
+      os << "&" << fields_name.c_str () << "_" << n;
+
+      if (n < count - 1)
+        os << ",";
+
+      os << be_nl;
+    }
+
+  os << be_uidt_nl
+     << "};" << be_uidt_nl << be_nl;
 
   return 0;
 }
