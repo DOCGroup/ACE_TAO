4b0e7af254e87a995cfd7e3c339ee8be7c80d1bc
Johnny Willemsen <jwillemsen@remedy.nl>
2005-03-29 18:43:57 +0000

    ChangeLogTag: Tue Mar 29 19:43:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/Refcounted_Auto_Ptr.h
+++ ace/Refcounted_Auto_Ptr.h
@@ -43,7 +43,7 @@ public:
 
   /// Constructor that initializes an @c ACE_Refcounted_Auto_Ptr to
   /// point to the result immediately.
-  explicit ACE_Refcounted_Auto_Ptr (X *p = 0);
+  ACE_Refcounted_Auto_Ptr (X *p = 0);
 
   /// Copy constructor binds the created object and @c r to the same
   /// @c ACE_Refcounted_Auto_Ptr_Rep. An @c ACE_Refcounted_Auto_Ptr_Rep
