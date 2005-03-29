2a908bdcd94d4dfd37f7fa72d014dfd9708a0f3e
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-03-29 12:30:21 +0000

    ChangeLogTag: Mon Mar 28 12:48:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/TypeCode_Base_Attributes.cpp
+++ trunk/TAO/tao/TypeCode_Base_Attributes.cpp
@@ -3,6 +3,8 @@
 #ifndef TAO_TYPECODE_BASE_ATTRIBUTES_CPP
 #define TAO_TYPECODE_BASE_ATTRIBUTES_CPP
 
+#include "tao/TypeCode_Base_Attributes.h"
+
 #ifndef __ACE_INLINE__
 # include "tao/TypeCode_Base_Attributes.inl"
 #endif /* !__ACE_INLINE__ */
