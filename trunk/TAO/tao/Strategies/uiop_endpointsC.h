1116ed1e5d704e228bcb7fdc2d4c2da5579de0a5
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-03-30 19:42:33 +0000

    ChangeLogTag: Wed Mar 30 19:41:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Strategies/uiop_endpointsC.h
+++ trunk/TAO/tao/Strategies/uiop_endpointsC.h
@@ -53,13 +53,6 @@
 #endif
 #define TAO_EXPORT_MACRO TAO_Strategies_Export
 
-#if defined (TAO_EXPORT_NESTED_CLASSES)
-#  if defined (TAO_EXPORT_NESTED_MACRO)
-#    undef TAO_EXPORT_NESTED_MACRO
-#  endif /* defined (TAO_EXPORT_NESTED_MACRO) */
-#  define TAO_EXPORT_NESTED_MACRO TAO_Strategies_Export
-#endif /* TAO_EXPORT_NESTED_CLASSES */
-
 #if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable:4250)
@@ -205,7 +198,7 @@ TAO_Strategies_Export CORBA::Boolean operator>> (
 #endif /* _TAO_CDR_OP_TAO_UIOPEndpointSequence_H_ */
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:969
+// be\be_codegen.cpp:958
 
 #if defined (__ACE_INLINE__)
 #include "uiop_endpointsC.inl"
