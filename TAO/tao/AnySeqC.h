b19ea73145d95904519c9224c22c6dc0469760b7
Johnny Willemsen <jwillemsen@remedy.nl>
2005-03-31 11:42:32 +0000

    ChangeLogTag: Thu Mar 31 11:56:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/AnySeqC.h
+++ TAO/tao/AnySeqC.h
@@ -54,13 +54,6 @@
 #endif
 #define TAO_EXPORT_MACRO TAO_Export
 
-#if defined (TAO_EXPORT_NESTED_CLASSES)
-#  if defined (TAO_EXPORT_NESTED_MACRO)
-#    undef TAO_EXPORT_NESTED_MACRO
-#  endif /* defined (TAO_EXPORT_NESTED_MACRO) */
-#  define TAO_EXPORT_NESTED_MACRO TAO_Export
-#endif /* TAO_EXPORT_NESTED_CLASSES */
-
 #if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable:4250)
@@ -127,7 +120,7 @@ namespace CORBA
   // TAO_IDL - Generated from
   // be\be_visitor_typecode/typecode_decl.cpp:44
   
-  TAO_NAMESPACE_STORAGE_CLASS ::CORBA::TypeCode_ptr const _tc_AnySeq;
+  extern TAO_Export ::CORBA::TypeCode_ptr const _tc_AnySeq;
 
 // TAO_IDL - Generated from
 // be\be_visitor_module/module_ch.cpp:66
@@ -168,7 +161,7 @@ TAO_Export CORBA::Boolean operator>> (
 #endif /* _TAO_CDR_OP_CORBA_AnySeq_H_ */
 
 // TAO_IDL - Generated from
-// be\be_codegen.cpp:969
+// be\be_codegen.cpp:947
 
 #if defined (__ACE_INLINE__)
 #include "AnySeqC.inl"
