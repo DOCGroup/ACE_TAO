1faef09df5c69f911820ed6b549e1315f324d6d5
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-04-10 09:15:33 +0000

    ChangeLogTag:Sun Apr 10 02:10:06 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- trunk/ace/Copy_Disabled.cpp
+++ trunk/ace/Copy_Disabled.cpp
@@ -9,11 +9,11 @@
 
 #include "ace/Copy_Disabled.h"
 
-#if !defined (ACE_LACKS_PRAGMA_ONCE)
-# pragma once
-#endif /* ACE_LACKS_PRAGMA_ONCE */
 
-ACE_RCSID(ace, Copy_Disabled, "$Id$")
+ACE_RCSID (ace,
+           Copy_Disabled,
+           "$Id$")
+
 
 ACE_Copy_Disabled::ACE_Copy_Disabled (void)
 {
