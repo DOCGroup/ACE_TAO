ef9b3ff210a51b3f466ed3f02406634a6dd587f5
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-04-14 21:38:01 +0000

    ChangeLogTag:Thu Apr 14 14:33:22 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- trunk/TAO/tao/TypeCode.h
+++ trunk/TAO/tao/TypeCode.h
@@ -506,8 +506,9 @@ namespace TAO
    *       on the unaliased @c TypeCode returned from
    *       @c unaliased_typecode().
    */
-  TAO_Export CORBA::TCKind unaliased_kind (CORBA::TypeCode_ptr tc
-                                           ACE_ENV_ARG_DECL);
+  ACE_NAMESPACE_INLINE_FUNCTION
+  CORBA::TCKind unaliased_kind (CORBA::TypeCode_ptr tc
+                                ACE_ENV_ARG_DECL);
 
 }
 
