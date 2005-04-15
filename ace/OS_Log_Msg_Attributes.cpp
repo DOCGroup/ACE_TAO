8957a3c0b8873642d62d5f750df9755edce5274a
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-04-15 17:55:46 +0000

    This commit was manufactured by cvs2svn to create branch 'xsc_reverse_handlers'.

--- ace/OS_Log_Msg_Attributes.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "ace/OS_Log_Msg_Attributes.h"
-
-#if !defined (ACE_HAS_INLINED_OSCALLS)
-# include "ace/OS_Log_Msg_Attributes.inl"
-#endif /* ACE_HAS_INLINED_OS_CALLS */
-
-ACE_RCSID(ace, OS_Log_Msg_Attributes, "$Id$")
