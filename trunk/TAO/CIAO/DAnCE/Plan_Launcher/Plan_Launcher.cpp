c070acab05ccc631ec40ae086fd6111a903466e6
turkaye <turkaye@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-04-16 22:12:23 +0000

    *** empty log message ***

--- trunk/TAO/CIAO/DAnCE/Plan_Launcher/Plan_Launcher.cpp
+++ trunk/TAO/CIAO/DAnCE/Plan_Launcher/Plan_Launcher.cpp
@@ -16,23 +16,37 @@ namespace CIAO
     const char* package_url = 0;
     bool use_naming = false;
     const char* ior_file = "file://em.ior";
+    const char* dap_ior_filename = 0;
+    const char* dap_ior = 0;
+    enum { 
+      pl_mode_start,
+      pl_mode_stop,
+      pl_mode_interactive
+    } mode = pl_mode_interactive;
 
-    void
+    static void
     usage (const ACE_TCHAR* program)
     {
       ACE_ERROR ((LM_ERROR,
-                  "(%P|%t) Executor: Usage: %s -p <URI> -k <EX_IOR> "
-                  " (or -n <Use NameService>) \n",
+                  ACE_TEXT ("[(%P|%t) Executor] Usage: %s\n")
+                  ACE_TEXT ("-p <PACKAGE_URI>\n")
+                  ACE_TEXT ("-k <EXECUTION_MANAGER_IOR>")
+                  ACE_TEXT (" : Default file://em.ior\n")
+                  ACE_TEXT ("-n : Use naming service to fetch")
+                  ACE_TEXT (" Execution Manager IOR Alternative to -k\n")
+                  ACE_TEXT ("-o <DOMAIN_APPLICATION_MANAGER_IOR_OUTPUT_FILE>\n")
+                  ACE_TEXT ("-i <DOMAIN_APPLICATION_MANAGER_IOR_FOR_INPUT>\n")
+                  ACE_TEXT ("-h : Show this usage information\n"),
                   program));
     }
 
-    bool
+    static bool
     parse_args (int argc,
                 ACE_TCHAR *argv[])
     {
       ACE_Get_Opt get_opt (argc,
                            argv,
-                           ACE_TEXT ("p:nk:"));
+                           ACE_TEXT ("p:nk:o:i:h"));
       int c;
 
       while ((c = get_opt ()) != EOF)
@@ -48,16 +62,31 @@ namespace CIAO
             case 'k':
               ior_file = get_opt.opt_arg ();
               break;
+            case 'o':
+              dap_ior_filename = get_opt.opt_arg ();
+              mode = pl_mode_start;
+              break;
+            case 'i':
+              dap_ior = get_opt.opt_arg ();
+              mode = pl_mode_stop;
+              break;
+            case 'h':
             default:
               usage(argv[0]);
               return false;
             }
         }
 
+      if ((mode != pl_mode_stop) && (package_url == 0))
+        {
+          usage (argv[0]);
+          return false;
+        }
+      
       return true;
     }
 
-    CORBA::Object_ptr
+    static CORBA::Object_ptr
     fetch_reference_naming (CORBA::ORB_ptr orb
                             ACE_ENV_ARG_DECL)
     {
@@ -82,7 +111,7 @@ namespace CIAO
                            ACE_ENV_ARG_PARAMETER);
     }
 
-    CORBA::Object_ptr
+    static CORBA::Object_ptr
     fetch_reference_file (CORBA::ORB_ptr orb
                           ACE_ENV_ARG_DECL)
     {
@@ -90,7 +119,50 @@ namespace CIAO
                                     ACE_ENV_ARG_PARAMETER);
     }
 
-    int
+    static ::Deployment::DomainApplicationManager_ptr
+    init_dap_ior (::CIAO::ExecutionManagerDaemon_ptr exec_mgr 
+                  ACE_ENV_ARG_DECL)
+    {
+      CIAO::Config_Handlers::XML_File_Intf intf (package_url);
+      
+      ::Deployment::DeploymentPlan_var plan =
+          intf.get_plan ();
+
+      return exec_mgr->preparePlan (plan, 1);
+    }
+
+    static ::Deployment::DomainApplicationManager_ptr
+    read_dap_ior (CORBA::ORB_ptr orb
+                  ACE_ENV_ARG_DECL)
+    {
+      CORBA::Object_var obj = orb->string_to_object (dap_ior
+                                                     ACE_ENV_ARG_PARAMETER);
+      return
+        Deployment::DomainApplicationManager::_narrow (obj.in ()
+                                                       ACE_ENV_ARG_PARAMETER);
+    }
+
+    static int
+    write_dap_ior (CORBA::ORB_ptr orb,
+                   ::Deployment::DomainApplicationManager_ptr dap
+                   ACE_ENV_ARG_DECL)
+    {
+      CORBA::String_var ior = orb->object_to_string (dap);
+
+      FILE* ior_output_file = ACE_OS::fopen (dap_ior_filename, "w");
+
+      if (ior_output_file)
+        {
+          ACE_OS::fprintf (ior_output_file, "%s", ior.in ());
+          ACE_OS::fclose (ior_output_file);
+
+          return 0;
+        }
+
+      return -1;
+    }
+
+    static int
     run_main_implementation (int argc, char *argv[])
     {
       ACE_DECLARE_NEW_CORBA_ENV;
@@ -104,14 +176,12 @@ namespace CIAO
                              ACE_ENV_ARG_PARAMETER);
           ACE_TRY_CHECK;
 
-          if (!parse_args (argc, argv) || package_url == 0)
-            {
-              usage (argv[0]);
-              return -1;
-            }
+          if (parse_args (argc, argv) == false)
+            return -1;
 
           CORBA::Object_var obj;
 
+          // Initialize Execution Manager reference
           if (use_naming)
             {
               obj =
@@ -146,20 +216,26 @@ namespace CIAO
                         "(%P|%t) CIAO_PlanLauncher: Obtained Execution"
                         " Manager ref \n"));
           
-          CIAO::Config_Handlers::XML_File_Intf intf (package_url);
-          
-          ::Deployment::DeploymentPlan_var plan =
-              intf.get_plan ();
-          
-          ::Deployment::DomainApplicationManager_var dapp_mgr =
-              exec_mgr->preparePlan (plan,
-                                     1);
+          // Initialize Domain Application Manager reference
+          ::Deployment::DomainApplicationManager_var dapp_mgr;
+          if (mode != pl_mode_stop)
+            {
+              dapp_mgr = init_dap_ior (exec_mgr.in ()
+                                       ACE_ENV_ARG_PARAMETER);
+              ACE_TRY_CHECK;
+            }
+          else
+            {
+              dapp_mgr = read_dap_ior (orb.in ()
+                                       ACE_ENV_ARG_PARAMETER);
+              ACE_TRY_CHECK;
+            }
           
           if (CORBA::is_nil (dapp_mgr.in ()))
             {
               ACE_DEBUG ((LM_DEBUG,
-                          "(%P|%t) CIAO_PlanLauncher:preparePlan call failed:nil "
-                          "DomainApplicationManager reference\n"));
+                          "(%P|%t) CIAO_PlanLauncher:preparePlan call failed: "
+                          "nil DomainApplicationManager reference\n"));
               return -1;
             }
 
@@ -170,43 +246,62 @@ namespace CIAO
           // == BALA check some bogus things here..
           // Create a dummy set of properties and start the
           // Launching of applications
-          ::Deployment::Properties_var properties;
-          ACE_NEW_RETURN (properties,
-                          Deployment::Properties,
-                          1);
-          if (CIAO::debug_level ())
+          if (mode != pl_mode_stop)
+            {
+              ::Deployment::Properties_var properties;
+              ACE_NEW_RETURN (properties,
+                              Deployment::Properties,
+                              1);
+              if (CIAO::debug_level ())
+                ACE_DEBUG ((LM_DEBUG,
+                            "CIAO_PlanLauncher: start Launch application..."));
+
+              // Start the Application immediately
+              int start = 1;
+
+              dapp_mgr->startLaunch (properties.in (), start);
+              ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+
+              ACE_DEBUG ((LM_DEBUG,
+                          "CIAO_PlanLauncher: finish Launch application..."));
+              // Call finish Launch to complete the connections
+              dapp_mgr->finishLaunch (start);
+              ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+
+              ACE_DEBUG ((LM_DEBUG,
+                          ACE_TEXT ("CIAO_PlanLauncher: ")
+                          ACE_TEXT ("Application Deployed successfully\n")));
+
+              if (mode == pl_mode_start)
+                {
+                  write_dap_ior (orb.in (), dapp_mgr.in ()
+                                 ACE_ENV_ARG_PARAMETER);
+                }
+              
+              if (mode == pl_mode_interactive)
+                {
+                  ACE_DEBUG ((LM_DEBUG,
+                             "Press <Enter> to tear down application\n"));
+                  char dummy [256];
+                  std::cin.getline (dummy, 256);
+                }
+            }
+
+          if (mode != pl_mode_start)
+          {
+            // Tear down the assembly
             ACE_DEBUG ((LM_DEBUG,
-                        "CIAO_PlanLauncher: start Launch application....."));
+                        "CIAO_PlanLauncher: destroy the application....."));
+            dapp_mgr->destroyApplication ();
 
-          // Start the Application immediately
-          int start = 1;
+            ACE_DEBUG ((LM_DEBUG, "[success]\n"));
 
-          dapp_mgr->startLaunch (properties.in (), start);
-          ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+            ACE_DEBUG ((LM_DEBUG,
+                        "CIAO_PlanLauncher: destroy the manager....."));
 
-          ACE_DEBUG ((LM_DEBUG, "CIAO_PlanLauncher: finish Launch application....."));
-          // Call finish Launch to complete the connections
-          dapp_mgr->finishLaunch (start);
-          ACE_DEBUG ((LM_DEBUG, "[success]\n"));
-
-          ACE_DEBUG ((LM_DEBUG, "CIAO_PlanLauncher: Application Deployed successfully \n"));
-          ACE_DEBUG ((LM_DEBUG, "Press <Enter> to tear down application \n"));
-
-          char dummy [256];
-          std::cin.getline (dummy, 256);
-          // Tear down the assembly
-
-          ACE_DEBUG ((LM_DEBUG,
-                      "CIAO_PlanLauncher: destroy the application....."));
-          dapp_mgr->destroyApplication ();
-
-          ACE_DEBUG ((LM_DEBUG, "[success]\n"));
-
-          ACE_DEBUG ((LM_DEBUG,
-                      "CIAO_PlanLauncher: destroy the manager....."));
-
-          exec_mgr->destroyManager (dapp_mgr.in ());
-          ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+            exec_mgr->destroyManager (dapp_mgr.in ());
+            ACE_DEBUG ((LM_DEBUG, "[success]\n"));
+          }
 
           orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
           ACE_TRY_CHECK;
@@ -220,6 +315,8 @@ namespace CIAO
         }
       ACE_CATCHALL
         {
+          ACE_ERROR ((LM_ERROR,
+                      "(%P|%t) Plan_Launcher: Unknown exception.\n"));
           return -1;
         }
       ACE_ENDTRY;
