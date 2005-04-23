3d8af8d060ec2274c972a9f3a016d37533ea5c06
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-04-23 18:39:35 +0000

    ChangeLogTag: Sat Apr 23 18:06:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Exception.cpp
+++ trunk/TAO/tao/Exception.cpp
@@ -165,70 +165,72 @@ TAO_DONT_CATCH::TAO_DONT_CATCH (void)
 #endif /* TAO_DONT_CATCH_DOT_DOT_DOT */
 
 // Specializations for CORBA::Exception Any operators.
-
-template<>
-void
-TAO::Any_Dual_Impl_T<CORBA::Exception>::value (
-    const CORBA::Exception & val
-  )
+namespace TAO
 {
-  this->value_ = val._tao_duplicate ();
-}
+  template<>
+  void
+  Any_Dual_Impl_T<CORBA::Exception>::value (
+      const CORBA::Exception & val
+    )
+  {
+    this->value_ = val._tao_duplicate ();
+  }
 
-template<>
-CORBA::Boolean
-TAO::Any_Dual_Impl_T<CORBA::Exception>::marshal_value (TAO_OutputCDR &cdr)
-{
-  ACE_TRY_NEW_ENV
-    {
-      this->value_->_tao_encode (cdr
-                                 ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
+  template<>
+  CORBA::Boolean
+  Any_Dual_Impl_T<CORBA::Exception>::marshal_value (TAO_OutputCDR &cdr)
+  {
+    ACE_TRY_NEW_ENV
+      {
+        this->value_->_tao_encode (cdr
+                                   ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
 
-      return 1;
-    }
-  ACE_CATCHANY
-    {
-    }
-  ACE_ENDTRY;
+        return 1;
+      }
+    ACE_CATCHANY
+      {
+      }
+    ACE_ENDTRY;
 
-  return 0;
-}
+    return 0;
+  }
 
-template<>
-CORBA::Boolean
-TAO::Any_Dual_Impl_T<CORBA::Exception>::demarshal_value (TAO_InputCDR &cdr)
-{
-  ACE_TRY_NEW_ENV
-    {
-      this->value_->_tao_decode (cdr
-                                 ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
+  template<>
+  CORBA::Boolean
+  Any_Dual_Impl_T<CORBA::Exception>::demarshal_value (TAO_InputCDR &cdr)
+  {
+    ACE_TRY_NEW_ENV
+      {
+        this->value_->_tao_decode (cdr
+                                   ACE_ENV_ARG_PARAMETER);
+        ACE_TRY_CHECK;
 
-      return 1;
-    }
-  ACE_CATCHANY
-    {
-    }
-  ACE_ENDTRY;
+        return 1;
+      }
+    ACE_CATCHANY
+      {
+      }
+    ACE_ENDTRY;
 
-  return 0;
-}
+    return 0;
+  }
 
-// This should never get called since we don't have extraction operators
-// for CORBA::Exception, but it is here to sidestep the constructor call
-// in the unspecialized version that causes a problem with compilers that
-// require explicit instantiation
-template<>
-CORBA::Boolean
-TAO::Any_Dual_Impl_T<CORBA::Exception>::extract (
-    const CORBA::Any &,
-    _tao_destructor,
-    CORBA::TypeCode_ptr,
-    const CORBA::Exception *&
-  )
-{
-  return 0;
+  // This should never get called since we don't have extraction operators
+  // for CORBA::Exception, but it is here to sidestep the constructor call
+  // in the unspecialized version that causes a problem with compilers that
+  // require explicit instantiation
+  template<>
+  CORBA::Boolean
+  Any_Dual_Impl_T<CORBA::Exception>::extract (
+      const CORBA::Any &,
+      _tao_destructor,
+      CORBA::TypeCode_ptr,
+      const CORBA::Exception *&
+    )
+  {
+    return 0;
+  }
 }
 
 // =======================================================================
