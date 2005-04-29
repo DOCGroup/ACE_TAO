3a0ba8b0351ba6b50eacbafc751db3168a946d7c
Johnny Willemsen <jwillemsen@remedy.nl>
2005-04-29 10:21:38 +0000

    ChangeLogTag: Fri Apr 29 09:50:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/PortableServer/ServantLocatorA.cpp
+++ TAO/tao/PortableServer/ServantLocatorA.cpp
@@ -46,7 +46,7 @@ static TAO::TypeCode::Objref<char const *,
   _tao_tc_PortableServer_ServantLocator (
     "IDL:omg.org/PortableServer/ServantLocator:2.3",
     "ServantLocator");
-
+  
 namespace PortableServer
 {
   ::CORBA::TypeCode_ptr const _tc_ServantLocator =
@@ -58,28 +58,34 @@ namespace PortableServer
 // TAO_IDL - Generated from
 // be\be_visitor_interface/any_op_cs.cpp:50
 
-template<>
-CORBA::Boolean
-TAO::Any_Impl_T<PortableServer::ServantLocator>::to_object (
-    CORBA::Object_ptr &_tao_elem
-  ) const
+namespace TAO
 {
-  _tao_elem = CORBA::Object::_duplicate (this->value_);
-  return true;
+  template<>
+  CORBA::Boolean
+  Any_Impl_T<PortableServer::ServantLocator>::to_object (
+      CORBA::Object_ptr &_tao_elem
+    ) const
+  {
+    _tao_elem = CORBA::Object::_duplicate (this->value_);
+    return true;
+  }
 }
 
-template<>
-CORBA::Boolean
-TAO::Any_Impl_T<PortableServer::ServantLocator>::marshal_value (TAO_OutputCDR &)
+namespace TAO
 {
-  return false;
-}
-
-template<>
-CORBA::Boolean
-TAO::Any_Impl_T<PortableServer::ServantLocator>::demarshal_value (TAO_InputCDR &)
-{
-  return false;
+  template<>
+  CORBA::Boolean
+  Any_Impl_T<PortableServer::ServantLocator>::marshal_value (TAO_OutputCDR &)
+  {
+    return false;
+  }
+  
+  template<>
+  CORBA::Boolean
+  Any_Impl_T<PortableServer::ServantLocator>::demarshal_value (TAO_InputCDR &)
+  {
+    return false;
+  }
 }
 
 // Copying insertion.
