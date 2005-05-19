f57bc94aa1e215cc399ef3caa934d7ef7b2a1b64
jeliazkov_i <jeliazkov_i@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-05-19 14:01:37 +0000

    Thu May 19 08:55:36 2005  Iliyan Jeliazkov  <jeliazkov_i@ociweb.com>

--- TAO/orbsvcs/tests/Security/BiDirectional/test_i.cpp
+++ TAO/orbsvcs/tests/Security/BiDirectional/test_i.cpp
@@ -8,7 +8,7 @@
 #include "tao/Thread_Lane_Resources.h"
 
 #if !defined(__ACE_INLINE__)
-#include "test_i.i"
+#include "test_i.inl"
 #endif /* __ACE_INLINE__ */
 
 ACE_RCSID(BiDirectional, test_i, "$Id$")
