d9b9672ccffe4497acc63c52d31c939b8a09a48e
Johnny Willemsen <jwillemsen@remedy.nl>
2005-05-26 07:34:37 +0000

    ChangeLogTag: Thu May 26 07:30:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACEXML/common/XML_Util.h
+++ ACEXML/common/XML_Util.h
@@ -17,11 +17,12 @@
 
 #include /**/ "ace/pre.h"
 
+#include "ACEXML/common/ACEXML_Export.h"
+
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "ACEXML/common/ACEXML_Export.h"
 #include "ACEXML/common/XML_Types.h"
 
 ACEXML_Export ACEXML_String ACEXML_escape_string(const ACEXML_String& str);
