a42b8c9b795de62986f425a44c2471ca9bafcd54
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-05-27 18:44:13 +0000

    Fri May 27 13:41:20 2005  Jaiganesh B  <jai@dre.vanderbilt.edu>

--- trunk/TAO/CIAO/DAnCE/ciao/Servant_Impl_Base.cpp
+++ trunk/TAO/CIAO/DAnCE/ciao/Servant_Impl_Base.cpp
@@ -65,6 +65,7 @@ namespace CIAO
     ACE_THROW_SPEC ((CORBA::SystemException,
                      Components::RemoveFailure))
   {
+    // Removing Facets
     Components::FacetDescriptions_var facets =
       this->get_all_facets (ACE_ENV_SINGLE_ARG_PARAMETER);
     ACE_CHECK;
@@ -73,19 +74,14 @@ namespace CIAO
     CORBA::ULong i = 0;
     for (i = 0; i < facet_len; ++i)
     {
-      PortableServer::ObjectId_var id =
+      PortableServer::ObjectId_var facet_id =
         this->container_->the_facet_cons_POA ()->reference_to_id
             (facets[i]->facet_ref () ACE_ENV_ARG_PARAMETER);
       
-      CIAO::Servant_Activator *sa =
-        this->container_->ports_servant_activator ();
-
-      sa->update_port_activator (id ACE_ENV_ARG_PARAMETER);
-
       ACE_TRY
         {
           this->container_->the_facet_cons_POA ()->deactivate_object
-            (id ACE_ENV_ARG_PARAMETER);
+            (facet_id ACE_ENV_ARG_PARAMETER);
           ACE_TRY_CHECK;
         }
       ACE_CATCHANY
@@ -94,10 +90,47 @@ namespace CIAO
                                "Facet not active\n");
         }
       ACE_ENDTRY;
+
+      CIAO::Servant_Activator *sa =
+        this->container_->ports_servant_activator ();
+      sa->update_port_activator (facet_id ACE_ENV_ARG_PARAMETER);
     }
 
-   Components::SessionComponent_var temp = this->get_executor ();
-   temp->ccm_remove (ACE_ENV_SINGLE_ARG_PARAMETER);
+  // Removed Facets
+
+  // Removing Consumers
+    Components::ConsumerDescriptions_var consumers =
+      this->get_all_consumers (ACE_ENV_SINGLE_ARG_PARAMETER);
+    ACE_CHECK;
+
+    const CORBA::ULong consumer_len = consumers->length ();
+    CORBA::ULong j = 0;
+    for (j = 0; j < consumer_len; ++j)
+    {
+      PortableServer::ObjectId_var cons_id =
+        this->container_->the_facet_cons_POA ()->reference_to_id
+            (consumers[j]->consumer () ACE_ENV_ARG_PARAMETER);
+
+      ACE_TRY
+        {
+          this->container_->the_facet_cons_POA ()->deactivate_object
+            (cons_id ACE_ENV_ARG_PARAMETER);
+          ACE_TRY_CHECK;
+        }
+      ACE_CATCHANY
+        {
+          ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                               "Consumer not active\n");
+        }
+      ACE_ENDTRY;
+
+      CIAO::Servant_Activator *sa =
+        this->container_->ports_servant_activator ();
+      sa->update_port_activator (cons_id ACE_ENV_ARG_PARAMETER);
+    }
+
+    Components::SessionComponent_var temp = this->get_executor ();
+    temp->ccm_remove (ACE_ENV_SINGLE_ARG_PARAMETER);
 
     CORBA::Object_var objref =
       this->container_->get_objref (this);
@@ -262,7 +295,8 @@ namespace CIAO
 
     ::Components::ConsumerDescriptions *tmp = 0;
     ACE_NEW_THROW_EX (tmp,
-                      ::Components::ConsumerDescriptions (this->consumer_table_.current_size ()),
+                      ::Components::ConsumerDescriptions (
+                      this->consumer_table_.current_size ()),
                       CORBA::NO_MEMORY ());
 
     ::Components::ConsumerDescriptions_var retval = tmp;
