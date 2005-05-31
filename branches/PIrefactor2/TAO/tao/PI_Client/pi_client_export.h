524aae57032f1b54c1de35379fe8425b66539772
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-05-31 10:10:53 +0000

    ChangeLogTag: Tue May 31 09:05:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ branches/PIrefactor2/TAO/tao/PI_Client/pi_client_export.h
@@ -0,0 +1,40 @@
+
+// -*- C++ -*-
+// $Id$
+// Definition for Win32 Export directives.
+// This file is generated automatically by generate_export_file.pl
+// ------------------------------
+#ifndef TAO_PI_CLIENT_GIOP_EXPORT_H
+#define TAO_PI_CLIENT_GIOP_EXPORT_H
+
+#include "ace/config-all.h"
+
+#if defined (TAO_AS_STATIC_LIBS)
+#  if !defined (TAO_PI_CLIENT_HAS_DLL)
+#    define TAO_PI_CLIENT_HAS_DLL 0
+#  endif /* ! TAO_PI_CLIENT_HAS_DLL */
+#else
+#  if !defined (TAO_PI_CLIENT_HAS_DLL)
+#    define TAO_PI_CLIENT_HAS_DLL 1
+#  endif /* ! TAO_PI_CLIENT_HAS_DLL */
+#endif
+
+#if defined (TAO_PI_CLIENT_HAS_DLL) && (TAO_PI_CLIENT_HAS_DLL == 1)
+#  if defined (TAO_PI_CLIENT_BUILD_DLL)
+#    define TAO_PI_CLIENT_Export ACE_Proper_Export_Flag
+#    define TAO_PI_CLIENT_SINGLETON_DECLARATION(T) ACE_EXPORT_SINGLETON_DECLARATION (T)
+#    define TAO_PI_CLIENT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#  else /* TAO_PI_CLIENT_BUILD_DLL */
+#    define TAO_PI_CLIENT_Export ACE_Proper_Import_Flag
+#    define TAO_PI_CLIENT_SINGLETON_DECLARATION(T) ACE_IMPORT_SINGLETON_DECLARATION (T)
+#    define TAO_PI_CLIENT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#  endif /* TAO_PI_CLIENT_BUILD_DLL */
+#else /* TAO_PI_CLIENT_HAS_DLL == 1 */
+#  define TAO_PI_CLIENT_Export
+#  define TAO_PI_CLIENT_SINGLETON_DECLARATION(T)
+#  define TAO_PI_CLIENT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK)
+#endif /* TAO_PI_CLIENT_HAS_DLL == 1 */
+
+#endif /* TAO_PI_CLIENT_GIOP_EXPORT_H */
+
+// End of auto generated file.
