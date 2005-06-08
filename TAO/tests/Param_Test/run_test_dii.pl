14c6bbb9b3030abba9f0e4b38378f6a3b0d1c9cd
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-06-08 13:46:20 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_4_6'.

--- TAO/tests/Param_Test/run_test_dii.pl
+++
@@ -1,9 +0,0 @@
-eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
-    & eval 'exec perl -S $0 $argv:q'
-    if 0;
-
-# $Id$
-# -*- perl -*-
-
-system 'perl run_test.pl -i dii ' . join ' ', @ARGV;
-
