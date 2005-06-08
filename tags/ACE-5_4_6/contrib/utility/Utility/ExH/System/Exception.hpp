7269e50de67a602bbebb1b97d66ff7d3edf3b111
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-06-08 13:46:20 +0000

    This commit was manufactured by cvs2svn to create tag 'ACE-5_4_6'.

---
+++ tags/ACE-5_4_6/contrib/utility/Utility/ExH/System/Exception.hpp
@@ -0,0 +1,29 @@
+// file      : Utility/ExH/System/Exception.hpp
+// author    : Boris Kolpackov <boris@kolpackov.net>
+// copyright : Copyright (c) 2002-2003 Boris Kolpackov
+// license   : http://kolpackov.net/license.html
+
+#ifndef UTILITY_EX_H_SYSTEM_EXCEPTION_HPP
+#define UTILITY_EX_H_SYSTEM_EXCEPTION_HPP
+
+#include <exception>
+
+namespace Utility
+{
+  namespace ExH
+  {
+    namespace System
+    {
+      // This is the only way to make predefined exceptions like
+      // std::bad_alloc, etc to appear in the right place of the hierarchy.
+      //
+
+      typedef std::exception Exception;
+    }
+  }
+}
+
+#endif  // UTILITY_EX_H_SYSTEM_EXCEPTION_HPP
+
+
+//$Id$
