152c0f5c127eac99b5c9097aa4db2b34e14e6dd0
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-06-18 18:53:27 +0000

    This commit was manufactured by cvs2svn to create branch 'MyACE'.

--- TAO/tao/IORInterceptor_Adapter_Factory.cpp
+++
@@ -1,8 +0,0 @@
-// $Id$
-#include "tao/IORInterceptor_Adapter_Factory.h"
-
-ACE_RCSID (tao,
-           IORInterceptor_Adapter_Factory,
-           "$Id$")
-
-
--- ace/Log_Msg_Callback.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "ace/Log_Msg_Callback.h"
-
-ACE_RCSID(ace, Log_Msg_Callback, "$Id$")
-
-ACE_Log_Msg_Callback::~ACE_Log_Msg_Callback (void)
-{
-}
