122b9d99ab99a0c2ca605e90cac8e3f228efe26f
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-06-24 13:50:42 +0000

    ChangeLogTag: Fri Jun 24 12:30:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/ClientRequestInfo.cpp
+++ trunk/TAO/tao/ClientRequestInfo.cpp
@@ -647,4 +647,31 @@ TAO_ClientRequestInfo::reply_status (TAO::Invocation_Status invoke_status)
     }
 }
 
+void
+TAO_ClientRequestInfo::exception (CORBA::Exception *exception)
+{
+  if (CORBA::SystemException::_downcast (exception) != 0)
+    this->reply_status_ = PortableInterceptor::SYSTEM_EXCEPTION;
+  else if (CORBA::UserException::_downcast (exception) != 0)
+    this->reply_status_ = PortableInterceptor::USER_EXCEPTION;
+
+  // @@ Is it possible for both of the above downcasts to fail?
+
+  this->caught_exception_ = exception;
+}
+
+void
+TAO_ClientRequestInfo::forward_reference (
+  PortableInterceptor::ForwardRequest &)
+{
+  // Note that we're converting the ForwardRequest exception in to a
+  // LOCATION_FORWARD reply, so we do not set the exception status.
+  //
+  // The forward object reference is not handled here.  Rather, it is
+  // handled by the TAO::Invocation_Base object so that its profiles
+  // can be added to the list of forward profiles.
+
+  this->reply_status_ = PortableInterceptor::LOCATION_FORWARD;
+}
+
 #endif /* TAO_HAS_INTERCEPTORS == 1 */
