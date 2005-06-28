7083517d242c2ffdd10b0bc4769e1deb7ef7efa1
gmaxey <gmaxey@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-06-28 23:14:37 +0000

    ChangeLogTag: Tue Jun 28 16:11:50 2005  Gary Maxey <gary.maxey@hp.com>

--- TAO/TAO_IDL/be/be_visitor_valuetype/any_op_ch.cpp
+++ TAO/TAO_IDL/be/be_visitor_valuetype/any_op_ch.cpp
@@ -53,41 +53,17 @@ be_visitor_valuetype_any_op_ch::visit_valuetype (be_valuetype *node)
   *os << be_nl << be_nl << "// TAO_IDL - Generated from" << be_nl
       << "// " << __FILE__ << ":" << __LINE__ << be_nl << be_nl;
 
-
-  be_module *module = 0;
- 
-  if (node->is_nested () &&
-      node->defined_in ()->scope_node_type () == AST_Decl::NT_module)
-    {
-      module = be_module::narrow_from_scope (node->defined_in ());
-
-      if (!module)
-        {
-          ACE_ERROR_RETURN ((LM_ERROR,
-                             "be_visitor_valuetype_any_op_ch::"
-                             "visit_valuetype - "
-                             "Error parsing nested name\n"),
-                            -1);
-        }
-      be_util::gen_nested_namespace_begin (os, module);
-    }
-
   *os << be_global->stub_export_macro () << " void"
-      << " operator<<= (CORBA::Any &, " << node->local_name ()
+      << " operator<<= (CORBA::Any &, " << node->name ()
       << " *); // copying" << be_nl;
 
   *os << be_global->stub_export_macro () << " void"
-      << " operator<<= (CORBA::Any &, " << node->local_name ()
+      << " operator<<= (CORBA::Any &, " << node->name ()
       << " **); // non-copying" << be_nl;
 
   *os << be_global->stub_export_macro () << " CORBA::Boolean"
       << " operator>>= (const CORBA::Any &, "
-      << node->local_name () << " *&);";
-
-  if (module != 0)
-    {
-      be_util::gen_nested_namespace_end (os, module);
-    }
+      << node->name () << " *&);";
 
   node->cli_hdr_any_op_gen (1);
   return 0;
