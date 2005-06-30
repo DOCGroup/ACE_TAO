07bcb2109934b92996c870612842533e1272fcd0
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-06-30 23:37:43 +0000

    ChangeLogTag: Thu Jun 30 23:34:40 UTC 2005  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/CIAO/DAnCE/tests/Minimum/Sender/Sender_exec.h
+++ trunk/TAO/CIAO/DAnCE/tests/Minimum/Sender/Sender_exec.h
@@ -32,7 +32,7 @@
 #include "Sender_exec_export.h"
 #include "tao/LocalObject.h"
 
-namespace Sender_Impl
+namespace CIDL_Sender_Impl
 {
   class SENDER_EXEC_Export Sender_exec_i
   : public virtual Sender_Exec,
