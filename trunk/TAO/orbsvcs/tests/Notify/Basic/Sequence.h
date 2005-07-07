f5b0ef28c5214a2f624f1d58579011597ee04418
michel_j <michel_j@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-07-07 15:02:52 +0000

    Thu Jul  7 10:01:26 2005  Justin Michel  <michel_j@ociweb.com>

--- trunk/TAO/orbsvcs/tests/Notify/Basic/Sequence.h
+++ trunk/TAO/orbsvcs/tests/Notify/Basic/Sequence.h
@@ -22,9 +22,7 @@
 #include "orbsvcs/TimeBaseC.h"
 
 #if defined(_MSC_VER)
-#if (_MSC_VER >= 1200)
 #pragma warning(push)
-#endif /* _MSC_VER >= 1200 */
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
 
@@ -137,7 +135,7 @@ private:
 
 /***************************************************************************/
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#if defined(_MSC_VER)
 #pragma warning(pop)
 #endif /* _MSC_VER */
 
--- trunk/TAO/orbsvcs/tests/Notify/Basic/Simple.h
+++ trunk/TAO/orbsvcs/tests/Notify/Basic/Simple.h
@@ -21,9 +21,7 @@
 #include "Notify_PushSupplier.h"
 
 #if defined(_MSC_VER)
-#if (_MSC_VER >= 1200)
 #pragma warning(push)
-#endif /* _MSC_VER >= 1200 */
 #pragma warning(disable:4250)
 #endif /* _MSC_VER */
 
@@ -125,7 +123,7 @@ private:
 
 /***************************************************************************/
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#if defined(_MSC_VER)
 #pragma warning(pop)
 #endif /* _MSC_VER */
 
