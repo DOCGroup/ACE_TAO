01fbeabb834ce4b64af3bd962738bb8346405a50
michel_j <michel_j@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-07-13 16:20:12 +0000

    Wed Jul 13 11:18:44 2005  Justin Michel  <michel_j@ociweb.com>

--- TAO/orbsvcs/tests/Notify/Ordering/Notify_Sequence_Push_Consumer.cpp
+++ TAO/orbsvcs/tests/Notify/Ordering/Notify_Sequence_Push_Consumer.cpp
@@ -1,11 +1,13 @@
 // $Id$
 
-#include "ace/OS_NS_unistd.h"
 #include "Notify_Sequence_Push_Consumer.h"
 #include "Notify_Test_Client.h"
-#include "orbsvcs/TimeBaseC.h"
 #include "common.h"
+#include "orbsvcs/TimeBaseC.h"
 #include "tao/debug.h"
+#include "ace/OS_NS_unistd.h"
+
+const int BATCH_SIZE = 5;
 
 Notify_Sequence_Push_Consumer::Notify_Sequence_Push_Consumer (
   const char* name,
@@ -18,7 +20,7 @@ Notify_Sequence_Push_Consumer::Notify_Sequence_Push_Consumer (
   use_ordering_ (use_ordering),
   expected_ (expected),
   count_ (0),
-  first_ (0),
+  previous_first_ (0),
   client_ (client)
 {
   this->client_.consumer_start (this);
@@ -50,7 +52,7 @@ Notify_Sequence_Push_Consumer::_connect (
   CosNotification::QoSProperties properties (3);
   properties.length (3);
   properties[0].name = CORBA::string_dup (CosNotification::MaximumBatchSize);
-  properties[0].value <<= (CORBA::Long)5;
+  properties[0].value <<= (CORBA::Long) BATCH_SIZE;
   properties[1].name = CORBA::string_dup (CosNotification::PacingInterval);
   properties[1].value <<= (TimeBase::TimeT) (1000 * 10000); // 1 secs
   if (use_ordering_)
@@ -86,6 +88,8 @@ Notify_Sequence_Push_Consumer::push_structured_events (
     ACE_OS::sleep(2);
   }
 
+  ACE_ASSERT(events.length() == BATCH_SIZE);
+
   count_ += events.length();
 
   if (this->count_ > this->expected_)
@@ -100,38 +104,55 @@ Notify_Sequence_Push_Consumer::push_structured_events (
 
   ACE_ASSERT(events[0].header.variable_header.length() == 3);
   ACE_ASSERT(ACE_OS::strcmp(events[0].header.variable_header[0].name.in(), "id") == 0);
-  CORBA::Long id1 = 0;
-  CORBA::Long id2 = 0;
-  events[0].header.variable_header[0].value >>= id1;
-  events[events.length() - 1].header.variable_header[0].value >>= id2;
+  CORBA::Long first_id = 0;
+  events[0].header.variable_header[0].value >>= first_id;
+  CORBA::Long last_id = 0;
+  events[events.length() - 1].header.variable_header[0].value >>= last_id;
 
-  ACE_DEBUG((LM_DEBUG, "\n Received id %d-%d\n", id1, id2));
+  ACE_DEBUG((LM_DEBUG, "\n Received id %d-%d\n", first_id, last_id));
 
-  for (CORBA::ULong idx = 0; idx < events.length(); ++idx)
+  int events_length = static_cast<int>(events.length());
+
+  CORBA::Long previous_id = first_id;
+
+  if (count_ > events_length) // Ignore the very first batch
+  {
+    // First check that the sequences are ordered correctly
+    for (CORBA::ULong idx = 1; idx < events.length(); ++idx)
     {
       CORBA::Long id = 0;
 
       events[idx].header.variable_header[0].value >>= id;
-
-      // The first batch may be out of order on faster platforms
-      if (count_ > static_cast<int>(events.length()))
-        {
-          int eid = idx + count_ - static_cast<int>(events.length());
-          if (order_policy_ == CosNotification::PriorityOrder
-              || order_policy_ == CosNotification::DeadlineOrder)
-            {
-              eid = expected_ - 1;
-              eid -= (count_ - static_cast<int>(events.length()));
-              eid -= idx;
-              if (first_ < static_cast<int>(events.length()))
-                eid += static_cast<int>(events.length());
-            }
-          if (id != eid)
-            ACE_ERROR((LM_ERROR, "\nError: Expected id:%d Received id:%d\n", eid, id));
-        }
-      else
-        {
-          first_ = id1;
-        }
+      CORBA::Long expected_id = previous_id + 1;
+      if (order_policy_ == CosNotification::PriorityOrder
+        || order_policy_ == CosNotification::DeadlineOrder)
+      {
+        expected_id = previous_id - 1;
+      }
+      if (id != expected_id)
+      {
+        ACE_ERROR((LM_ERROR, "Error: Expected:%d Received:%d\n", expected_id, id));
+        return;
+      }
+      previous_id = id;
     }
+
+    // Next check that the first id in the sequence is ordered
+    // relative to the previously retrieved sequence.
+    if (previous_first_ != 0)
+    {
+      CORBA::Long expected_id = previous_first_ + BATCH_SIZE;
+      if (order_policy_ == CosNotification::PriorityOrder
+        || order_policy_ == CosNotification::DeadlineOrder)
+      {
+        expected_id = previous_first_ - BATCH_SIZE;
+      }
+      if (first_id != expected_id)
+      {
+        ACE_ERROR((LM_ERROR, "Error: Expected:%d Received:%d\n", expected_id, first_id));
+        return;
+      }
+    }
+    previous_first_ = first_id;
+  }
 }
