4d2f8667adf54ff19d67025ceea282e8f17fa4c8
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-07-15 03:38:23 +0000

    Thu Jul 14 22:15:02 2005  Phil Mesnier  <mesnier_p@ociweb.com>

--- trunk/TAO/tao/orbconf.h
+++ trunk/TAO/tao/orbconf.h
@@ -81,7 +81,11 @@
 // to the first request. The use of codeset negotiation also causes
 // libTAO_Codeset to be linked in.
 #if !defined (TAO_NEGOTIATE_CODESETS)
-# define TAO_NEGOTIATE_CODESETS 1
+# if defined (TAO_AS_STATIC_LIBS)
+#  define TAO_NEGOTIATE_CODESETS 0
+# else
+#  define TAO_NEGOTIATE_CODESETS 1
+# endif /* TAO_AS_STATIC_LIBS */
 #endif /*TAO_NEGOTIATE_CODESETS*/
 
 // The default size of TAO's ORB table, i.e. the one used as the
