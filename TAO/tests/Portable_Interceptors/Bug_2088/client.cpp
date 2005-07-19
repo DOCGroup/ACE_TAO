6460e0fda289554f8a886d4117fd468257cb4e17
Johnny Willemsen <jwillemsen@remedy.nl>
2005-07-19 11:56:15 +0000

    ChangeLogTag: Tue Jul 19 11:49:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/Portable_Interceptors/Bug_2088/client.cpp
+++ TAO/tests/Portable_Interceptors/Bug_2088/client.cpp
@@ -9,25 +9,26 @@ ACE_RCSID(Interceptors, client, "$Id$")
 int
 main (int argc, char *argv[])
 {
+  Client_ORBInitializer* initializer1 = 0;
+  Client_ORBInitializer* initializer2 = 0;
+  bool caught_exception = false;
+
+  ACE_NEW_RETURN (initializer1,
+                  Client_ORBInitializer,
+                  -1);  // No exceptions yet!
+
+  ACE_NEW_RETURN (initializer2,
+                  Client_ORBInitializer,
+                  -1);  // No exceptions yet!
+
+  PortableInterceptor::ORBInitializer_var initializer_var1 =
+    initializer1;
+
+  PortableInterceptor::ORBInitializer_var initializer_var2 =
+    initializer2;
+
   ACE_TRY_NEW_ENV
     {
-      Client_ORBInitializer* initializer1 = 0;
-      Client_ORBInitializer* initializer2 = 0;
-
-      ACE_NEW_RETURN (initializer1,
-                      Client_ORBInitializer,
-                      -1);  // No exceptions yet!
-
-      ACE_NEW_RETURN (initializer2,
-                      Client_ORBInitializer,
-                      -1);  // No exceptions yet!
-
-      PortableInterceptor::ORBInitializer_var initializer_var1 =
-        initializer1;
-
-      PortableInterceptor::ORBInitializer_var initializer_var2 =
-        initializer2;
-
       PortableInterceptor::register_orb_initializer (initializer_var1.in ()
                                                      ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
@@ -40,11 +41,11 @@ main (int argc, char *argv[])
         CORBA::ORB_init (argc, argv, "" ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      // For both initializers pre/post must be called.
-      ACE_ASSERT (initializer1->pre_init_called == true);
-      ACE_ASSERT (initializer2->pre_init_called == true);
-      ACE_ASSERT (initializer1->post_init_called == true);
-      ACE_ASSERT (initializer2->post_init_called == true);
+    }
+  ACE_CATCH (CORBA::NO_MEMORY, ex)
+    {
+      // Initializer1 throws this exception and we should get it here
+      caught_exception = true;
     }
   ACE_CATCHANY
     {
@@ -53,6 +54,14 @@ main (int argc, char *argv[])
       return 1;
     }
   ACE_ENDTRY;
+
+  // Only the pre init for initalizer 1 must be called, other initializers
+  // shouldn't be caught
+  ACE_ASSERT (initializer1->pre_init_called == true);
+  ACE_ASSERT (initializer2->pre_init_called == false);
+  ACE_ASSERT (initializer1->post_init_called == false);
+  ACE_ASSERT (initializer2->post_init_called == false);
+  ACE_ASSERT (caught_exception == true);
 
   return 0;
 }
