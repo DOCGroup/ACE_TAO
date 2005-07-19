a38509c73ceb3ee8c8d4732252315ad821ab9c13
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-07-19 11:31:44 +0000

    ChangeLogTag: Tue Jul 19 10:28:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Object_Loader.cpp
+++ trunk/TAO/tao/Object_Loader.cpp
@@ -11,12 +11,3 @@ TAO_Object_Loader::~TAO_Object_Loader (void)
 {
 }
 
-#if defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
-
-template class ACE_Dynamic_Service<TAO_Object_Loader>;
-
-#elif defined (ACE_HAS_TEMPLATE_INSTANTIATION_PRAGMA)
-
-#pragma instantiate ACE_Dynamic_Service<TAO_Object_Loader>
-
-#endif /* ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
