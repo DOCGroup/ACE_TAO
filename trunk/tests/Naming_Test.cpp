0d582d9a21bae6b4b3835d0ed9f0d9f9e3e5a27f
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-07-25 19:24:40 +0000

    ChangeLogTag: Mon Jul 25 18:40:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/tests/Naming_Test.cpp
+++ trunk/tests/Naming_Test.cpp
@@ -182,7 +182,7 @@ run_main (int argc, ACE_TCHAR *argv[])
 {
   ACE_START_TEST (ACE_TEXT ("Naming_Test"));
   ACE_TCHAR temp_file [BUFSIZ];
-  ACE_Naming_Context *ns_context;
+  ACE_Naming_Context *ns_context = 0;
   ACE_NEW_RETURN (ns_context, ACE_Naming_Context, -1);
 
   ACE_Name_Options *name_options = ns_context->name_options ();
