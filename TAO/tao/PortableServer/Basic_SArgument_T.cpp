a487f92ff92c66802c0c6808047e3b6bfb3cdc87
Johnny Willemsen <jwillemsen@remedy.nl>
2005-07-28 11:30:31 +0000

    ChangeLogTag: Thu Jul 28 11:18:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/PortableServer/Basic_SArgument_T.cpp
+++ TAO/tao/PortableServer/Basic_SArgument_T.cpp
@@ -4,7 +4,10 @@
 #define TAO_BASIC_SARGUMENT_T_CPP
 
 #include "tao/PortableServer/Basic_SArgument_T.h"
-#include "tao/Dynamic_ParameterC.h"
+
+#if TAO_HAS_INTERCEPTORS == 1
+# include "tao/Dynamic_ParameterC.h"
+#endif /* TAO_HAS_INTERCEPTORS */
 
 #if !defined (__ACE_INLINE__)
 #include "tao/PortableServer/Basic_SArgument_T.inl"
