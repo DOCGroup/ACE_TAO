81e9fee804b46a88caaaa76d9006a8a4f3d9f69f
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-07-29 14:34:56 +0000

    ChangeLogTag: Fri Jul 29 11:02:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/AnyRefactor/TAO/tao/AnyTypeCode/AnyTypeCode_methods.h
+++ branches/AnyRefactor/TAO/tao/AnyTypeCode/AnyTypeCode_methods.h
@@ -16,7 +16,7 @@
 #define TAO_ANYTYPECODE_METHODS_H
 
 #include /**/ "ace/pre.h"
-#include "tao/AnyTypeCode/TAO_AnyTypeCode_export.h"
+#include "tao/AnyTypeCode/TAO_AnyTypeCode_Export.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
