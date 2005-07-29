9211aa7acc4dd16bcf15da2e1bf9553ddf79f23b
Johnny Willemsen <jwillemsen@remedy.nl>
2005-07-29 11:01:01 +0000

    ChangeLogTag: Fri Jul 29 09:28:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/Exception.cpp
+++ TAO/tao/Exception.cpp
@@ -3,11 +3,7 @@
 #include "Exception.h"
 #include "SystemException.h"
 #include "Environment.h"
-//#include "Any_SystemException.h"
-//#include "Any_Dual_Impl_T.h"
-//#include "TypeCode.h"
 #include "ORB_Constants.h"
-//#include "TypeCode_Constants.h"
 #include "CORBA_String.h"
 #include "CDR.h"
 #include "debug.h"
@@ -163,101 +159,4 @@ namespace CORBA
 TAO_DONT_CATCH::TAO_DONT_CATCH (void)
 {}
 #endif /* TAO_DONT_CATCH_DOT_DOT_DOT */
-
-
-// Specializations for CORBA::Exception Any operators.
-namespace TAO
-{
-/*  template<>
-  void
-  Any_Dual_Impl_T<CORBA::Exception>::value (
-      const CORBA::Exception & val
-    )
-  {
-    this->value_ = val._tao_duplicate ();
-  }
-
-  template<>
-  CORBA::Boolean
-  Any_Dual_Impl_T<CORBA::Exception>::marshal_value (TAO_OutputCDR &cdr)
-  {
-    ACE_TRY_NEW_ENV
-      {
-        this->value_->_tao_encode (cdr
-                                   ACE_ENV_ARG_PARAMETER);
-        ACE_TRY_CHECK;
-
-        return 1;
-      }
-    ACE_CATCHANY
-      {
-      }
-    ACE_ENDTRY;
-
-    return 0;
-  }
-
-  template<>
-  CORBA::Boolean
-  Any_Dual_Impl_T<CORBA::Exception>::demarshal_value (TAO_InputCDR &cdr)
-  {
-    ACE_TRY_NEW_ENV
-      {
-        this->value_->_tao_decode (cdr
-                                   ACE_ENV_ARG_PARAMETER);
-        ACE_TRY_CHECK;
-
-        return 1;
-      }
-    ACE_CATCHANY
-      {
-      }
-    ACE_ENDTRY;
-
-    return 0;
-  }
-  */
-  // This should never get called since we don't have extraction operators
-  // for CORBA::Exception, but it is here to sidestep the constructor call
-  // in the unspecialized version that causes a problem with compilers that
-  // require explicit instantiation
-  /*template<>
-  CORBA::Boolean
-  Any_Dual_Impl_T<CORBA::Exception>::extract (
-      const CORBA::Any &,
-      _tao_destructor,
-      CORBA::TypeCode_ptr,
-      const CORBA::Exception *&
-    )
-  {
-    return 0;
-  } */
-}
-
-// =======================================================================
-
-/*
-// Insertion of CORBA::Exception - copying.
-void
-operator<<= (CORBA::Any &any, const CORBA::Exception &exception)
-{
-  TAO::Any_Dual_Impl_T<CORBA::Exception>::insert_copy (
-      any,
-      CORBA::Exception::_tao_any_destructor,
-      exception._tao_type (),
-      exception
-    );
-}
-
-// Insertion of CORBA::Exception - non-copying.
-void
-operator<<= (CORBA::Any &any, CORBA::Exception *exception)
-{
-  TAO::Any_Dual_Impl_T<CORBA::Exception>::insert (
-      any,
-      CORBA::Exception::_tao_any_destructor,
-      exception->_tao_type (),
-      exception
-    );
-} */
 
