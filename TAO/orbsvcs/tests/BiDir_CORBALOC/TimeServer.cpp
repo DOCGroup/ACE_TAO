aa86b21ff5701ca2c63f92b7b4f2a0faa86627bd
Johnny Willemsen <jwillemsen@remedy.nl>
2005-08-11 14:34:40 +0000

    ChangeLogTag: Thu Aug 11 10:48:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/tests/BiDir_CORBALOC/TimeServer.cpp
+++ TAO/orbsvcs/tests/BiDir_CORBALOC/TimeServer.cpp
@@ -1,10 +1,9 @@
 //$Id$
 #include "TimeModuleS.h"
 #include "tao/BiDir_GIOP/BiDirGIOP.h"
+#include "tao/AnyTypeCode/Any.h"
 #include "tao/Utils/PolicyList_Destroyer.h"
 #include "orbsvcs/CosNamingC.h"
-
-
 
 class Time_impl :
   public POA_TimeModule::Time
