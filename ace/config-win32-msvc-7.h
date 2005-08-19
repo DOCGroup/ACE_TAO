fd21c170de20cd9bb8b025ce4b71e79ee4f61ab8
Johnny Willemsen <jwillemsen@remedy.nl>
2005-08-19 08:39:36 +0000

    ChangeLogTag Fri Aug 19 08:35:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/config-win32-msvc-7.h
+++ ace/config-win32-msvc-7.h
@@ -103,11 +103,15 @@
 // Maybe in the future.
 // 'this' : used in base member initializer list
 #pragma warning(disable:4355)
+
 // 'class1' : inherits 'class2::member' via dominance
 #pragma warning(disable:4250)
 
+// C++ Exception Specification ignored
+#pragma warning(disable:4290)
+
 // Disable warning of using Microsoft Extension.
-# pragma warning(disable:4231)
+#pragma warning(disable:4231)
 
 // 'function' : unreferenced local function has been removed
 # pragma warning(disable:4505)
