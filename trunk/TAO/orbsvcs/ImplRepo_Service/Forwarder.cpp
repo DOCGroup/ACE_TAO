265774f02d890c6d2e8573c91d4b0e6f79850e98
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-08-22 11:17:40 +0000

    ChangeLogTag: Mon Aug 22 09:09:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/orbsvcs/ImplRepo_Service/Forwarder.cpp
+++ trunk/TAO/orbsvcs/ImplRepo_Service/Forwarder.cpp
@@ -18,7 +18,7 @@
 #include "tao/Object_KeyC.h"
 #include "tao/ORB_Constants.h"
 
-#include "tao/PortableServer/POA_Current_Impl.h"
+#include "tao/PortableServer/PortableServer.h"
 
 /**
 * This constructor takes in orb and ImR_Locator_i pointers to store for later
