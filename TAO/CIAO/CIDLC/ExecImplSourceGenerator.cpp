0c1c5dc563c41c57e04c94591b44d77985e7f3e3
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-08-24 20:43:53 +0000

    ChangeLogTag: Wed Aug 24 20:41:59 UTC 2005  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/CIAO/CIDLC/ExecImplSourceGenerator.cpp
+++ TAO/CIAO/CIDLC/ExecImplSourceGenerator.cpp
@@ -713,12 +713,6 @@ namespace
          << STRS[EXCP_CE] << "))" << endl
          << "{"
          << "this->context_ =" << endl
-/*
-         << "CIAO_GLUE"
-         << regex::perl_s (t.scoped_name ().scope_name ().str (),
-                           "/::/_/")
-         << "::"
-*/
          << t.name () << "_Context::_narrow (" << endl
          << "ctx" << endl
          << STRS[ENV_ARG] <<");"
