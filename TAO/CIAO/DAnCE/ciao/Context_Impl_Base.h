32235b46aa58a7e5b224fa1f730ad7e98a2dc8ce
Ossama Othman <ossama-othman@users.noreply.github.com>
2005-08-24 03:13:09 +0000

    ChangeLogTag:Tue Aug 23 20:09:59 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- TAO/CIAO/DAnCE/ciao/Context_Impl_Base.h
+++ TAO/CIAO/DAnCE/ciao/Context_Impl_Base.h
@@ -19,14 +19,14 @@
 
 #include /**/ "ace/pre.h"
 
-#include "tao/Objref_VarOut_T.h"
+#include "CIAO_Server_Export.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "CIAO_Server_Export.h"
 #include "ciao/CCM_ContainerC.h"
+#include "tao/Objref_VarOut_T.h"
 
 namespace Components
 {
