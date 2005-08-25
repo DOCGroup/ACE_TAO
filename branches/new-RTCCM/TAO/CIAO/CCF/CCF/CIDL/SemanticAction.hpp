e0d1d4c84f4dce6e34dfc239ccd444174323efa7
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-08-25 21:29:53 +0000

    This commit was manufactured by cvs2svn to create branch 'new-RTCCM'.

---
+++ branches/new-RTCCM/TAO/CIAO/CCF/CCF/CIDL/SemanticAction.hpp
@@ -0,0 +1,17 @@
+// file      : CCF/CIDL/SemanticAction.hpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+#ifndef CCF_CIDL_SEMANTIC_ACTION_HPP
+#define CCF_CIDL_SEMANTIC_ACTION_HPP
+
+#include "CCF/IDL3/SemanticAction.hpp"
+
+#include "CCF/CIDL/SemanticAction/Elements.hpp"
+
+#include "CCF/CIDL/SemanticAction/Composition.hpp"
+#include "CCF/CIDL/SemanticAction/HomeExecutor.hpp"
+
+#include "CCF/CIDL/SemanticAction/Factory.hpp"
+
+#endif  // CCF_CIDL_SEMANTIC_ACTION_HPP
