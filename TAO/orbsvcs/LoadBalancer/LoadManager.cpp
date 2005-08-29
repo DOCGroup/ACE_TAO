fa91685137a9b1ce000927e9ff5585d8eb85d621
Johnny Willemsen <jwillemsen@remedy.nl>
2005-08-29 13:36:55 +0000

    ChangeLogTag: Mon Aug 29 13:34:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>Mon Aug 29 12:41:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>Mon Aug 29 13:35:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/LoadBalancer/LoadManager.cpp
+++ TAO/orbsvcs/LoadBalancer/LoadManager.cpp
@@ -225,12 +225,15 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
       // Get a reference to the IOR table.
       CORBA::Object_var tobj = orb->resolve_initial_references ("IORTable"
                                                                ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+
       IORTable::Table_var table = IORTable::Table::_narrow (tobj.in ()
                                                             ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
 
       // bind your stringified IOR in the IOR table
-      table->bind ("LoadManager", str.in ()
-                   ACE_ENV_ARG_PARAMETER);
+      table->bind ("LoadManager", str.in () ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
 
       FILE * lm_ior = ACE_OS::fopen (lm_ior_file, "w");
       ACE_OS::fprintf (lm_ior, "%s", str.in ());
