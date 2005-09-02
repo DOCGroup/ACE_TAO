e4b282b80bb7f26941ea4e93a0ebd028dcf2fc7e
john_c <john_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-09-02 17:54:38 +0000

    Fri Sep  2 12:51:23 2005  Ciju John  <john_c@ociweb.com>

--- TAO/tests/Big_Request_Muxing/Payload_Receiver.cpp
+++ TAO/tests/Big_Request_Muxing/Payload_Receiver.cpp
@@ -12,7 +12,8 @@ Payload_Receiver::Payload_Receiver (int expected)
 }
 
 void
-Payload_Receiver::more_data (const Test::Payload& payload ACE_ENV_ARG_DECL)
+Payload_Receiver::more_data (const Test::Payload& payload
+			     ACE_ENV_ARG_DECL_NOT_USED)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
   if (payload.length() > 0)
