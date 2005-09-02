2564d21498913b36384bda6c4ebeb46b248a2272
john_c <john_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-09-02 17:54:38 +0000

    Fri Sep  2 12:51:23 2005  Ciju John  <john_c@ociweb.com>

--- TAO/tests/Big_Request_Muxing/Payload_Receiver.h
+++ TAO/tests/Big_Request_Muxing/Payload_Receiver.h
@@ -8,13 +8,6 @@
 
 #include "TestS.h"
 
-#if defined (_MSC_VER)
-# if (_MSC_VER >= 1200)
-#  pragma warning(push)
-# endif /* _MSC_VER >= 1200 */
-# pragma warning (disable:4250)
-#endif /* _MSC_VER */
-
 /// Implement the Test::Payload_Receiver interface
 /**
  * Simply print count how many bytes were received.
@@ -36,10 +29,6 @@ private:
   int message_count_;
   int expected_;
 };
-
-#if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma warning(pop)
-#endif /* _MSC_VER */
 
 #include /**/ "ace/post.h"
 #endif /* BIG_REQUEST_MUXING_PAYLOAD_RECEIVER_H */
