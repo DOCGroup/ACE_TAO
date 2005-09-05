e10d27c269a519e5345da42747f3dd3dcaab16f2
smcqueen <smcqueen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-09-05 11:19:04 +0000

    ChangeLogTag: Mon Sep  5 12:11:28 2005  Simon McQueen  <sm@prismtech.com>

---
+++ TAO/tests/Bug_2222_Regression/test.cpp
@@ -0,0 +1,32 @@
+// $Id$
+
+#include "TestCaseC.h"
+
+/**
+* This test does not run - successful compile and link == successful test.
+*/
+int
+main (int argc, char *argv[])
+{
+  ACE_TRY_NEW_ENV
+    {
+      Array_Typedef_slice*  body3 = Array_Typedef_alloc();
+      Array_Typedef_slice* body31 = Array_Typedef_dup (body3);
+      Array_Typedef_free (body3);
+      Array_Typedef_free (body31);
+
+      Typedef_Of_Imported_Array_Typedef_slice*  body4 = Typedef_Of_Imported_Array_Typedef_alloc ();
+      Typedef_Of_Imported_Array_Typedef_slice*  body41 = Typedef_Of_Imported_Array_Typedef_dup (body4);
+      Typedef_Of_Imported_Array_Typedef_free (body4);
+      Typedef_Of_Imported_Array_Typedef_free (body41);
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Exception caught:");
+      return 1;
+    }
+  ACE_ENDTRY;
+
+  return 0;
+}
