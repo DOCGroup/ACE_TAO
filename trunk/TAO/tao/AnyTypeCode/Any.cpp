34d1da8dc22aa9a73cfd36a3c6cc9950186a5f1f
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-09-13 10:28:42 +0000

    ChangeLogTag: Tue Sep 13 10:30:00 UTC 2005  Simon Massey  <sma@prismtech.com>

--- trunk/TAO/tao/AnyTypeCode/Any.cpp
+++ trunk/TAO/tao/AnyTypeCode/Any.cpp
@@ -449,12 +449,15 @@ operator<<= (CORBA::Any &any, CORBA::LongDouble ld)
 void
 operator<<= (CORBA::Any &any, const CORBA::Any &a)
 {
-  TAO::Any_Dual_Impl_T<CORBA::Any>::insert_copy (
-      any,
-      CORBA::Any::_tao_any_destructor,
-      CORBA::_tc_any,
-      a
-    );
+  if (0 == &a) // Trying to de-reference NULL Any
+    any <<= static_cast<CORBA::Any *>( 0 ); // Use non-copying insertion of a NULL
+  else
+    TAO::Any_Dual_Impl_T<CORBA::Any>::insert_copy (
+        any,
+        CORBA::Any::_tao_any_destructor,
+        CORBA::_tc_any,
+        a
+      );
 }
 
 // Insertion of Any - non-copying.
