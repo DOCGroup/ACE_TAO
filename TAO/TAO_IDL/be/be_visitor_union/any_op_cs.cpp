c2421f2dcdf4c7b57a370e315b60fc0d9145951b
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-09-13 10:28:42 +0000

    ChangeLogTag: Tue Sep 13 10:30:00 UTC 2005  Simon Massey  <sma@prismtech.com>

--- TAO/TAO_IDL/be/be_visitor_union/any_op_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_union/any_op_cs.cpp
@@ -90,13 +90,18 @@ be_visitor_union_any_op_cs::visit_union (be_union *node)
       << "const " << node->name () << " &_tao_elem" << be_uidt_nl
       << ")" << be_uidt_nl
       << "{" << be_idt_nl
+
+      << "if (0 == &_tao_elem) // Trying to de-reference NULL object" << be_idt_nl
+      << "_tao_any <<= static_cast<" << node->name () << " *>( 0 ); // Use non-copying insertion of a NULL" << be_uidt_nl
+      << "else" << be_idt_nl
+
       << "TAO::Any_Dual_Impl_T<" << node->name () << ">::insert_copy ("
       << be_idt << be_idt_nl
       << "_tao_any," << be_nl
       << node->name () << "::_tao_any_destructor," << be_nl
       << node->tc_name () << "," << be_nl
       << "_tao_elem" << be_uidt_nl
-      << ");" << be_uidt << be_uidt_nl
+      << ");" << be_uidt << be_uidt << be_uidt_nl
       << "}" << be_nl << be_nl;
 
   // Non-copying insertion.
