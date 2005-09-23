25bed7eed8083ca9faa8bae0c50c43c9a1f875c7
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-09-23 19:24:57 +0000

    ChangeLogTag:Fri Sep 23 12:17:29 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- trunk/TAO/tests/Bug_2222_Regression/test.cpp
+++ trunk/TAO/tests/Bug_2222_Regression/test.cpp
@@ -8,25 +8,15 @@
 int
 main (int, char *[])
 {
-  ACE_TRY_NEW_ENV
-    {
-      Array_Typedef_slice*  body3 = Array_Typedef_alloc();
-      Array_Typedef_slice* body31 = Array_Typedef_dup (body3);
-      Array_Typedef_free (body3);
-      Array_Typedef_free (body31);
+  Array_Typedef_slice*  body3 = Array_Typedef_alloc();
+  Array_Typedef_slice* body31 = Array_Typedef_dup (body3);
+  Array_Typedef_free (body3);
+  Array_Typedef_free (body31);
 
-      Typedef_Of_Imported_Array_Typedef_slice*  body4 = Typedef_Of_Imported_Array_Typedef_alloc ();
-      Typedef_Of_Imported_Array_Typedef_slice*  body41 = Typedef_Of_Imported_Array_Typedef_dup (body4);
-      Typedef_Of_Imported_Array_Typedef_free (body4);
-      Typedef_Of_Imported_Array_Typedef_free (body41);
-    }
-  ACE_CATCHANY
-    {
-      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
-                           "Exception caught:");
-      return 1;
-    }
-  ACE_ENDTRY;
+  Typedef_Of_Imported_Array_Typedef_slice*  body4 = Typedef_Of_Imported_Array_Typedef_alloc ();
+  Typedef_Of_Imported_Array_Typedef_slice*  body41 = Typedef_Of_Imported_Array_Typedef_dup (body4);
+  Typedef_Of_Imported_Array_Typedef_free (body4);
+  Typedef_Of_Imported_Array_Typedef_free (body41);
 
   return 0;
 }
