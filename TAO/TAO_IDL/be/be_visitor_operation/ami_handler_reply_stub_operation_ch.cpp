b7c1117daa96dda441b1742420d3417916f7ebca
Johnny Willemsen <jwillemsen@remedy.nl>
2005-10-19 10:50:00 +0000

    ChangeLogTag: Wed Oct 19 09:34:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/TAO_IDL/be/be_visitor_operation/ami_handler_reply_stub_operation_ch.cpp
+++ TAO/TAO_IDL/be/be_visitor_operation/ami_handler_reply_stub_operation_ch.cpp
@@ -20,8 +20,8 @@
 //
 // ============================================================================
 
-ACE_RCSID (be_visitor_operation, 
-           ami_handler_reply_stub_operation_ch, 
+ACE_RCSID (be_visitor_operation,
+           ami_handler_reply_stub_operation_ch,
            "$Id$")
 
 // ************************************************************
@@ -52,7 +52,7 @@ be_visitor_operation_ami_handler_reply_stub_operation_ch::visit_operation (
   if (!node->has_native ())
     {
       // Next line.
-      *os << be_nl << be_nl 
+      *os << be_nl << be_nl
           << "static void ";
 
       // Check if we are an attribute node in disguise
@@ -73,7 +73,7 @@ be_visitor_operation_ami_handler_reply_stub_operation_ch::visit_operation (
           << "_reply_stub (" << be_idt << be_idt_nl;
 
       *os << "TAO_InputCDR &_tao_reply_cdr," << be_nl
-          << "Messaging::ReplyHandler_ptr _tao_reply_handler," << be_nl
+          << "::Messaging::ReplyHandler_ptr _tao_reply_handler," << be_nl
           << "::CORBA::ULong reply_status";
 
       *os << be_nl
