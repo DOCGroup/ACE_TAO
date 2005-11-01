8a822080d68fddfbebc031cd64afee86aad3f0df
Simon McQueen <simon.mcqueen@gmail.com>
2005-11-01 09:46:37 +0000

    ChangeLogTag: Tue Nov  1 09:35:36 2005  Simon McQueen  <sm@prismtech.com>

---
+++ TAO/tests/Bug_2289_Regression/MyInterfaceImpl.h
@@ -0,0 +1,19 @@
+//
+// $Id$
+//
+
+#include "TestS.h"
+
+class MyInterfaceImpl
+  : public virtual POA_MyInterface, public virtual PortableServer::RefCountServantBase
+{
+  public:
+  MyInterfaceImpl (CORBA::ORB_ptr orb);
+
+  // = The skeleton methods
+  virtual void myMethod (const MyStruct & structParam ACE_ENV_ARG_DECL_WITH_DEFAULTS)
+    ACE_THROW_SPEC ((CORBA::SystemException));
+
+private:
+  CORBA::ORB_var orb_;
+};
