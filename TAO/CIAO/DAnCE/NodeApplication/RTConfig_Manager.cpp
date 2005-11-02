4f212d19fb1d3673aa36adcf21c255b078d6f4fd
nanbor <nanbor@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-11-02 22:03:25 +0000

    ChangeLogTag:Wed Nov 02 14:55:14 2005  Nanbor Wang  <nanbor@exothermic.txcorp.com>

--- TAO/CIAO/DAnCE/NodeApplication/RTConfig_Manager.cpp
+++ TAO/CIAO/DAnCE/NodeApplication/RTConfig_Manager.cpp
@@ -15,17 +15,109 @@ CIAO::RTResource_Config_Manager::init (RTCORBA::RTORB_ptr rtorb)
 }
 
 void
+CIAO::RTResource_Config_Manager::print_resources
+(const CIAO::DAnCE::ServerResource &server_resource
+ ACE_ENV_ARG_DECL)
+  ACE_THROW_SPEC ((CORBA::SystemException))
+{
+  const CIAO::DAnCE::ORBResource &orb_resource
+    = server_resource.orb_config.orb_resources[0];
+
+  CORBA::ULong i;
+
+  // Creating and binding name with RT Threadpool
+  for (i = 0; i < orb_resource.threadpool_list.length (); ++i)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  "-RTResource_Config_Manager::print_resource threadpool: %s\n"
+                  "\tStack size: %d\n"
+                  "\tStatic: %d\n"
+                  "\tDynamic %d\n"
+                  "\tDefault Prio: %d\n"
+                  "\tBuffering %s\n"
+                  "\tMax Buffered: %d\n"
+                  "\tBuffer Size %d\n",
+                  orb_resource.threadpool_list[i].Id.in (),
+                  orb_resource.threadpool_list[i].stacksize,
+                  orb_resource.threadpool_list[i].static_threads,
+                  orb_resource.threadpool_list[i].dynamic_threads,
+                  orb_resource.threadpool_list[i].default_priority,
+                  (orb_resource.threadpool_list[i].allow_request_buffering ? "Y" : "N"),
+                  orb_resource.threadpool_list[i].max_buffered_requests,
+                  orb_resource.threadpool_list[i].max_request_buffer_size));
+    }
+
+  for (i = 0; i < orb_resource.threadpool_with_lanes_list.length (); ++i)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  "-RTResource_Config_Manager::print_resource threadpool_with %d, lanes: %s\n",
+                  orb_resource.threadpool_with_lanes_list[i].threadpool_lanes.length (),
+                  orb_resource.threadpool_with_lanes_list[i].Id.in ()));
+      /*
+      for (CORBA::ULong l = 0;
+           l < orb_resource.threadpool_with_lanes_list[i].threadpool_lanes.length ();
+           ++l)
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "\tLane: %s\n"
+                      "\t\tPriority: %d\n"
+                      "\t\tStatic: %d\n"
+                      "\t\tDynamic: %d\n",
+                      orb_resource.threadpool_with_lanes_list[i].threadpool_lanes[l].lane_priority,
+                      orb_resource.threadpool_with_lanes_list[i].threadpool_lanes[l].static_threads,
+                      orb_resource.threadpool_with_lanes_list[i].threadpool_lanes[l].dynamic_threads));
+                      }*/
+      ACE_DEBUG ((LM_DEBUG,
+                  "\tStack size: %d\n"
+                  "\tBorrowing: %s\n"
+                  "\tBuffering %s\n"
+                  "\tMax Buffered: %d\n"
+                  "\tBuffer Size %d\n",
+                  orb_resource.threadpool_with_lanes_list[i].stacksize,
+                  (orb_resource.threadpool_with_lanes_list[i].allow_borrowing ? "Y" : "N"),
+                  (orb_resource.threadpool_with_lanes_list[i].allow_request_buffering ? "Y" : "N"),
+                  orb_resource.threadpool_with_lanes_list[i].max_buffered_requests,
+                  orb_resource.threadpool_with_lanes_list[i].max_request_buffer_size));
+    }
+
+  for (i = 0; i < orb_resource.connection_bands_list.length (); ++i)
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                  "-RTResource_Config_Manager::print_resource connection %d bands: %s\n",
+                  orb_resource.connection_bands_list[i].bands.length (),
+                  orb_resource.connection_bands_list[i].Id.in ()));
+
+      for (CORBA::ULong l = 0;
+           l < orb_resource.connection_bands_list[i].bands.length ();
+           ++l)
+        {
+      ACE_DEBUG ((LM_DEBUG,
+                  "\tBand %d Prio: %d -- %d\n",
+                  l,
+                  orb_resource.connection_bands_list[i].bands[l].low,
+                  orb_resource.connection_bands_list[i].bands[l].high));
+        }
+    }
+}
+
+void
 CIAO::RTResource_Config_Manager::init_resources
 (const CIAO::DAnCE::ServerResource &server_resource
  ACE_ENV_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
+  ACE_DEBUG ((LM_DEBUG,
+              "RTResource_Config_Manager::init_resource.\n"));
+
   if (CORBA::is_nil (this->rtorb_.in()))
     {
       ACE_DEBUG ((LM_DEBUG,
                   "RTResource_Config_Manager has not been properly initialized.\n"));
       ACE_THROW (CORBA::INTERNAL ());
     }
+
+  this->print_resources (server_resource);
+  //  return;
 
   const CIAO::DAnCE::ORBResource &orb_resource
     = server_resource.orb_config.orb_resources[0];
@@ -35,6 +127,9 @@ CIAO::RTResource_Config_Manager::init_resources
   // Creating and binding name with RT Threadpool
   for (i = 0; i < orb_resource.threadpool_list.length (); ++i)
     {
+      ACE_DEBUG ((LM_DEBUG,
+                  "RTResource_Config_Manager::init_resource threadpool: %s\n",
+                  orb_resource.threadpool_list[i].Id.in ()));
       RTCORBA::ThreadpoolId thr_id =
         this->rtorb_->create_threadpool
         (orb_resource.threadpool_list[i].stacksize,
@@ -54,10 +149,21 @@ CIAO::RTResource_Config_Manager::init_resources
           ACE_DEBUG ((LM_DEBUG, "Error binding thread pool Id: %s to map when initializing RTNodeApplication resources.\n", orb_resource.threadpool_list[i].Id.in ()));
           ACE_THROW (CORBA::INTERNAL ());
         }
+      else
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "RTResource_Config_Manager::init_resource added thread pool: %s\n",
+                      orb_resource.threadpool_list[i].Id.in ()));
+        }
     }
 
   for (i = 0; i < orb_resource.threadpool_with_lanes_list.length (); ++i)
     {
+      ACE_DEBUG ((LM_DEBUG,
+                  "RTResource_Config_Manager::init_resource threadpool_with %d, lanes: %s\n",
+                  orb_resource.threadpool_with_lanes_list[i].threadpool_lanes.length (),
+                  orb_resource.threadpool_with_lanes_list[i].Id.in ()));
+
       // Duplicate RTCORBA lanes
       RTCORBA::ThreadpoolLanes lanes
         (orb_resource.threadpool_with_lanes_list[i].threadpool_lanes.length ());
@@ -97,6 +203,13 @@ CIAO::RTResource_Config_Manager::init_resources
                       orb_resource.threadpool_with_lanes_list[i].Id.in ()));
           ACE_THROW (CORBA::INTERNAL ());
         }
+
+      else
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "RTResource_Config_Manager::init_resource added thread pool with lanes: %s\n",
+                      orb_resource.threadpool_with_lanes_list[i].Id.in ()));
+        }
     }
 
   for (i = 0; i < orb_resource.connection_bands_list.length (); ++i)
@@ -121,6 +234,12 @@ CIAO::RTResource_Config_Manager::init_resources
                       "when initializing RTNodeApplication resources.\n",
                       orb_resource.connection_bands_list[i].Id.in ()));
           ACE_THROW (CORBA::INTERNAL ());
+        }
+      else
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "RTResource_Config_Manager::init_resource added connection bands: %s\n",
+                      orb_resource.connection_bands_list[i].Id.in ()));
         }
     }
 
@@ -154,6 +273,12 @@ CIAO::RTResource_Config_Manager::init_resources
                       "Error binding Policy_Set with name: %s\n",
                       sets[i].Id.in ()));
           ACE_THROW (CORBA::INTERNAL ());
+        }
+      else
+        {
+          ACE_DEBUG ((LM_DEBUG,
+                      "RTResource_Config_Manager::init_resource added policy set: %s\n",
+                      sets[i].Id.in ()));
         }
     }
 }
@@ -240,6 +365,9 @@ CIAO::RTResource_Config_Manager::find_policies_by_name (const char *name
     }
 
 
+  ACE_DEBUG ((LM_DEBUG, "RTNodeApp_Configurator::find_policies_by_name: %s\n",
+              name));
+              
   POLICY_MAP::ENTRY *entry = 0;
 
   if (this->policy_map_.find (name, entry) != 0)
@@ -263,8 +391,10 @@ CIAO::RTResource_Config_Manager::create_single_policy
  ACE_ENV_ARG_DECL)
   ACE_THROW_SPEC ((CORBA::SystemException))
 {
-  if (CIAO::debug_level () > 9)
-    ACE_DEBUG ((LM_DEBUG, "RTResource_Config_Manager::create_single_policy\n"));
+  //  if (CIAO::debug_level () > 9)
+    ACE_DEBUG ((LM_DEBUG,
+                "RTResource_Config_Manager::create_single_policy: %d\n",
+                policy_def._d ()));
 
   CORBA::Policy_var retv;
 
@@ -272,13 +402,18 @@ CIAO::RTResource_Config_Manager::create_single_policy
     {
     case RTCORBA::PRIORITY_MODEL_POLICY_TYPE:
       {
-      const CIAO::DAnCE::PriorityModelPolicyDef &tmp
-        = policy_def.PriorityModelDef ();
+        const CIAO::DAnCE::PriorityModelPolicyDef &tmp
+          = policy_def.PriorityModelDef ();
 
-      retv = this->rtorb_->create_priority_model_policy ((RTCORBA::PriorityModel) tmp.priority_model,
-                                                         tmp.server_priority
-                                                         ACE_ENV_ARG_PARAMETER);
-      ACE_CHECK_RETURN (0);
+        retv = this->rtorb_->create_priority_model_policy ((RTCORBA::PriorityModel) tmp.priority_model,
+                                                           tmp.server_priority
+                                                           ACE_ENV_ARG_PARAMETER);
+        ACE_CHECK_RETURN (0);
+
+        //        if (! CORBA::is_nil (retv.in ()))
+        ACE_DEBUG ((LM_DEBUG,
+                    "Create PriorityModel policy: %d - %d\n",
+                    tmp.priority_model, tmp.server_priority));
       }
       break;
 
@@ -292,6 +427,11 @@ CIAO::RTResource_Config_Manager::create_single_policy
         retv = this->rtorb_->create_threadpool_policy (tpid
                                                        ACE_ENV_ARG_PARAMETER);
         ACE_CHECK_RETURN (0);
+
+        //        if (! CORBA::is_nil (retv.in ()))
+        ACE_DEBUG ((LM_DEBUG,
+                    "Create Threadpool policy: %s, TPid: %d\n",
+                    policy_def.ThreadpoolDef().Id.in (), tpid));
       }
       break;
 
@@ -306,13 +446,18 @@ CIAO::RTResource_Config_Manager::create_single_policy
           this->rtorb_->create_priority_banded_connection_policy (bands.in ()
                                                                   ACE_ENV_ARG_PARAMETER);
         ACE_CHECK_RETURN (0);
+
+        //        if (! CORBA::is_nil (retv.in ()))
+        ACE_DEBUG ((LM_DEBUG,
+                    "Created Banded Connection policy: %s\n",
+                    policy_def.PriorityBandedConnectionDef().Id.in ()));
       }
       break;
 
     default:
       ACE_DEBUG ((LM_DEBUG,
                   "Invalid policy type - RTPolicy_Set_Manager::create_single_policy\n"));
-      ACE_THROW_RETURN (CORBA::INTERNAL (), 0);
+      //      ACE_THROW_RETURN (CORBA::INTERNAL (), 0);
     }
 
   return retv._retn ();
