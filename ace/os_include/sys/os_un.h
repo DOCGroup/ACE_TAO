e8b74baa00a3f24e30b4bbb9c32c3d66a41458f4
Johnny Willemsen <jwillemsen@remedy.nl>
2005-11-04 10:37:44 +0000

    ChangeLogTag: Fri Nov  4 10:33:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/os_include/sys/os_un.h
+++ ace/os_include/sys/os_un.h
@@ -36,7 +36,7 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-#if defined (VXWORKS)
+#if defined (ACE_VXWORKS) && (ACE_VXWORKS < 0x600)
 struct sockaddr_un {
   short sun_family;    // AF_UNIX.
   char  sun_path[108]; // path name.
