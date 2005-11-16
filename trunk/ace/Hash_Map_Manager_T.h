664ccb65617d99bff8c609ae773b72599cd527b2
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-11-16 23:58:42 +0000

    ChangeLogTag:Wed Nov 16 15:48:29 2005  Steve Huston  <shuston@riverace.com>

--- trunk/ace/Hash_Map_Manager_T.h
+++ trunk/ace/Hash_Map_Manager_T.h
@@ -473,8 +473,8 @@ private:
   // = Disallow these operations.
   // Hack alert... Sun C++ 5.4 can't hack explicit templates with this
   // in place. Please remove this when taking out explicit templates.
-#if defined (__SUNPRO_CC) && (__SUNPRO_CC <= 0x540) && \
-   !defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION)
+#if !(defined (__SUNPRO_CC) && (__SUNPRO_CC <= 0x540) && \
+      defined (ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION))
   ACE_UNIMPLEMENTED_FUNC (void operator= (const ACE_Hash_Map_Manager_Ex<EXT_ID, INT_ID,  HASH_KEY, COMPARE_KEYS, ACE_LOCK> &))
 #endif /* __SUNPRO_CC <= 0x540 && !ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION */
 
