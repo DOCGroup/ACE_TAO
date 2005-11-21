daa011c97f98c90cf6039f514d58723528a7272d
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-11-21 11:06:18 +0000

    ChangeLogTag: Sun Nov 20 10:40:12 UTC 2005  Johnny Willemsen  <jwillemsen@remedy.nl>

--- branches/sequenceupdate/TAO/tao/bounded_sequence_cdr.hpp
+++ branches/sequenceupdate/TAO/tao/bounded_sequence_cdr.hpp
@@ -501,7 +501,7 @@ namespace TAO {
 
   template <typename stream, typename object_t, typename object_t_var, CORBA::ULong MAX>
   bool insert_sequence(stream & strm, const TAO::bounded_object_reference_sequence<object_t, object_t_var, MAX> & source) {
-    typedef TAO::bounded_object_reference_sequence<object_t, object_t_var, MAX>::object_type object_t;
+    typedef typename TAO::bounded_object_reference_sequence<object_t, object_t_var, MAX>::object_type object_t;
     const ::CORBA::ULong length = source.length ();
     if (!(strm << length)) {
       return false;
