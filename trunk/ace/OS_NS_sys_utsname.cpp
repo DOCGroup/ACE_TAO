1a0355b46428f96eddf1dec9ffaa686549a3b8ae
bala <bala@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2005-11-22 04:02:05 +0000

    ChangeLogTag:Tue Nov 22 09:27:30 (IST) 2005  Bala Natarajan <bala_natarajan@symantec.com>

--- trunk/ace/OS_NS_sys_utsname.cpp
+++ trunk/ace/OS_NS_sys_utsname.cpp
@@ -19,6 +19,32 @@ ACE_RCSID(ace, OS_NS_sys_utsname, "$Id$")
 
 ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 
+#if !defined (ACE_WIN32) && !defined (VXWORKS) && !defined (CHORUS) && !defined (ACE_PSOS)
+// Don't inline on those platforms because this function contains
+// string literals, and some compilers, e.g., g++, don't handle those
+// efficiently in unused inline functions.
+int
+ACE_OS::uname (ACE_utsname *name)
+{
+#if defined (INTEGRITY)
+  if(!name) {
+    errno = EFAULT;
+    return -1;
+  }
+  strcpy(name->sysname,"INTEGRITY");
+  int status = gethostname(name->nodename,_SYS_NMLN);
+  strcpy(name->release,"4.0");
+  strcpy(name->version,"4.0.9");
+  strcpy(name->machine,"a standard name");
+  return status;
+#else
+  ACE_OS_TRACE ("ACE_OS::uname");
+  ACE_OSCALL_RETURN (::uname (name), int, -1);
+#endif
+}
+
+#endif /* ! ACE_WIN32 && ! VXWORKS && ! CHORUS */
+
 #if defined (ACE_WIN32) || defined (VXWORKS) || defined (CHORUS) || defined (ACE_PSOS)
 // Don't inline on those platforms because this function contains
 // string literals, and some compilers, e.g., g++, don't handle those
