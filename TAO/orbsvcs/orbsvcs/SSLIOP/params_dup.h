8abe9707f65512aa02cd9dea3df144410758644b
Ossama Othman <ossama-othman@users.noreply.github.com>
2005-12-02 09:45:19 +0000

    ChangeLogTag:Fri Dec  2 01:35:31 2005  Ossama Othman  <ossama@dre.vanderbilt.edu>

--- TAO/orbsvcs/orbsvcs/SSLIOP/params_dup.h
+++ TAO/orbsvcs/orbsvcs/SSLIOP/params_dup.h
@@ -3,11 +3,9 @@
 #ifndef TAO_SSLIOP_PARAMS_DUP_H
 #define TAO_SSLIOP_PARAMS_DUP_H
 
-#include /**/ "ace/pre.h"
+#include "ace/config-macros.h"
+#include "tao/Versioned_Namespace.h"
 
-#include "tao/orbconf.h"
-
-#include "ace/Global_Macros.h"
 
 // As of 0.9.7e, OpenSSL's DSAparams_dup() and DHparams_dup() macros
 // contain casts that are invalid in C++.  These C wrapper functions
