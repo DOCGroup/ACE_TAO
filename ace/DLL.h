77d6fb49e4042262f114d65711a8596a36273b77
Steve Huston <shuston@riverace.com>
2005-12-29 17:03:33 +0000

    ChangeLogTag:Thu Dec 29 11:55:39 2005  Steve Huston  <shuston@riverace.com>

--- ace/DLL.h
+++ ace/DLL.h
@@ -34,7 +34,7 @@ class ACE_DLL_Handle;
  * @brief Provides an abstract interface for handling various DLL
  * operations.
  *
- * This class is an wrapper over the various methods for utilizing
+ * This class is a wrapper over the various methods for utilizing
  * a dynamically linked library (DLL), which is called a shared
  * library on some platforms.  Operations open(), close(), and
  * symbol() have been implemented to help opening/closing and
@@ -45,18 +45,36 @@ class ACE_Export ACE_DLL
 public:
   // = Initialization and termination methods.
 
-  /// Default constructor.  By default, the close() operation on the
-  /// object will be invoked before it is destroyed.
+  /**
+   * Default constructor.  By default, the close() operation on the
+   * object will be invoked before it is destroyed.
+   * @param close_handle_on_destruction  Indicates whether or not the
+   *        close() method will be called to close an open DLL when this
+   *        object is destroyed. By default, close() will be called.
+   *        Set this parameter to 0 for situations where the DLL's lifetime
+   *        is controlled in a scope other than that of this ACE_DLL object.
+   *        For example, termination by ACE_DLL_Manager via ACE::fini().
+   */
   explicit ACE_DLL (int close_handle_on_destruction = 1);
 
   /**
-   * This constructor opens and dynamically links @a dll_name.  The
-   * default mode is <RTLD_LAZY>, which loads identifier symbols but
-   * not the symbols for functions, which are loaded dynamically
-   * on-demand.  Other supported modes include: <RTLD_NOW>, which
-   * performs all necessary relocations when @a dll_name is first
-   * loaded and <RTLD_GLOBAL>, which makes symbols available for
-   * relocation processing of any other DLLs.
+   * This constructor performs the actions of open() during construction.
+   * @param dll_name  The name or path of the DLL to load.
+   * @param open_mode  Flags to alter the actions taken when loading the DLL.
+   *        The possible values are:
+   *        @li @c RTLD_LAZY (this the default): loads identifier symbols but
+   *            not the symbols for functions, which are loaded dynamically
+   *            on-demand.
+   *        @li @c RTLD_NOW: performs all necessary relocations when
+   *            @a dll_name is first loaded
+   *        @li RTLD_GLOBAL: makes symbols available for relocation
+   *            processing of any other DLLs.
+   * @param close_handle_on_destruction  Indicates whether or not the
+   *        close() method will be called to close an open DLL when this
+   *        object is destroyed. By default, close() will be called.
+   *        Set this parameter to 0 for situations where the DLL's lifetime
+   *        is controlled in a scope other than that of this ACE_DLL object.
+   *        For example, termination by ACE_DLL_Manager via ACE::fini().
    */
   explicit ACE_DLL (const ACE_TCHAR *dll_name,
                     int open_mode = ACE_DEFAULT_SHLIB_MODE,
@@ -66,16 +84,23 @@ public:
   ACE_DLL (const ACE_DLL &);
 
   /**
-   * This method opens and dynamically links @a dll_name.  The default
-   * mode is <RTLD_LAZY>, which loads identifier symbols but not the
-   * symbols for functions, which are loaded dynamically on-demand.
-   * Other supported modes include: <RTLD_NOW>, which performs all
-   * necessary relocations when @a dll_name>is first loaded and
-   * <RTLD_GLOBAL>, which makes symbols available for relocation
-   * processing of any other DLLs.
-   * @param dll_name The name of the dll.
-   * @param open_mode How the dll is opened.
-   * @param close_handle_on_destruction Close the handle when object is destroyed.
+   * This method opens and dynamically links a specified DLL.
+   * @param dll_name  The name or path of the DLL to load.
+   * @param open_mode  Flags to alter the actions taken when loading the DLL.
+   *        The possible values are:
+   *        @li @c RTLD_LAZY (this the default): loads identifier symbols but
+   *            not the symbols for functions, which are loaded dynamically
+   *            on-demand.
+   *        @li @c RTLD_NOW: performs all necessary relocations when
+   *            @a dll_name is first loaded
+   *        @li RTLD_GLOBAL: makes symbols available for relocation
+   *            processing of any other DLLs.
+   * @param close_handle_on_destruction  Indicates whether or not the
+   *        close() method will be called to close an open DLL when this
+   *        object is destroyed. By default, close() will be called.
+   *        Set this parameter to 0 for situations where the DLL's lifetime
+   *        is controlled in a scope other than that of this ACE_DLL object.
+   *        For example, termination by ACE_DLL_Manager via ACE::fini().
    * @retval -1 On failure
    * @retval 0 On success.
    */
@@ -88,16 +113,21 @@ public:
 
   /**
    * Called when the DLL object is destroyed -- invokes close() if the
-   * <close_handle_on_destruction> flag is set in the constructor or open()
-   * method.
+   * @a close_handle_on_destruction flag was set to non-zero in the
+   * constructor or open() method.
    */
   ~ACE_DLL (void);
 
-  /// If @a symbol_name is in the symbol table of the DLL a pointer to
-  /// the @a symbol_name is returned.  Otherwise, returns 0.  Setting
-  /// ignore_errors = 1 allows you to probe a dll without generating
-  /// error messages in the log.  Handy for determining the capabilities
-  /// of a library.
+  /**
+   * Look up a named symbol in the DLL. DLL must be successfully opened
+   * before calling symbol().
+   * @param symbol_name The symbol name to look up.
+   * @param ignore_errors If set to 1, allows you to probe a dll without
+   *        generating error messages in the log.  Handy for determining
+   *        the capabilities of a library.
+   * @return Returns the value of @a symbol_name if it is a valid symbol
+   *        in the DLL. Otherwise, returns 0.
+   */
   void *symbol (const ACE_TCHAR *symbol_name, int ignore_errors = 0);
 
   /// Returns a pointer to a string explaining that an error occured.  You
