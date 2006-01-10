b8f6fc87c7da6c8ffc49a27adf8272eb929e95c6
harris_s <harris_s@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-01-10 19:42:10 +0000

    Tue Jan 10 19:38:49 UTC 2006  Scott Harris  <harris_s@ociweb.com>

--- TAO/orbsvcs/tests/InterfaceRepo/IFR_Inheritance_Test/main.cpp
+++ TAO/orbsvcs/tests/InterfaceRepo/IFR_Inheritance_Test/main.cpp
@@ -11,52 +11,110 @@ ACE_RCSID (IFR_Inheritance_Test,
 
 void printContents( const CORBA::ContainedSeq& cont )
 {
-  for( unsigned int i=0; i<cont.length(); i++ )
-  {
-    if( cont[i]->describe()->kind == CORBA::dk_Interface )
+  ACE_TRY_NEW_ENV
     {
-      CORBA::InterfaceDef_var intDef = CORBA::InterfaceDef::_narrow( cont[i].in() );
-      CORBA::InterfaceDef::FullInterfaceDescription* desc = intDef->describe_interface();
+      for( unsigned int i=0; i<cont.length(); i++ )
+        {
+          if( cont[i]->describe()->kind == CORBA::dk_Interface )
+            {
+              CORBA::InterfaceDef_var intDef =
+                CORBA::InterfaceDef::_narrow (cont[i].in()
+                ACE_ENV_ARG_PARAMETER );
+              ACE_TRY_CHECK;
 
-      //printf( "-- %s:\n", (const char*)(desc->name) );
+              CORBA::InterfaceDef::FullInterfaceDescription* desc =
+                intDef->describe_interface (ACE_ENV_SINGLE_ARG_PARAMETER);
+              ACE_TRY_CHECK;
 
-      for( unsigned int j=0; j < desc->operations.length(); j++ )
-        printf( "operation %s::%s\n",
-                (const char*)(desc->name),
-                (const char*)((desc->operations[j]).name) );
+              //printf( "-- %s:\n", (const char*)(desc->name) );
 
-      for( unsigned int j=0; j < desc->attributes.length(); j++ )
-        printf( "attribute %s::%s\n",
-                (const char*)(desc->name),
-                (const char*)((desc->attributes[j]).name) );
+              for( unsigned int j1=0; j1 < desc->operations.length (); j1++ )
+                printf( "operation %s::%s\n",
+                        (const char*)(desc->name),
+                        (const char*)((desc->operations[j1]).name) );
+
+              for( unsigned int j2=0; j2 < desc->attributes.length (); j2++ )
+                printf( "attribute %s::%s\n",
+                        (const char*)(desc->name),
+                        (const char*)((desc->attributes[j2]).name) );
+            }
+          else if( cont[i]->describe ()->kind == CORBA::dk_Module )
+            {
+              CORBA::ModuleDef_var moduleDef =
+                CORBA::ModuleDef::_narrow (cont[i].in ()
+                                           ACE_ENV_ARG_PARAMETER );
+              ACE_TRY_CHECK;
+
+              CORBA::ContainedSeq_var moduleContents =
+                moduleDef->contents (CORBA::dk_all,1
+                                     ACE_ENV_ARG_PARAMETER );
+              ACE_TRY_CHECK;
+              printContents (moduleContents.in ());
+            }
+        }
     }
-    else if( cont[i]->describe()->kind == CORBA::dk_Module )
+  ACE_CATCHANY
     {
-      CORBA::ModuleDef_var moduleDef = CORBA::ModuleDef::_narrow(cont[i].in());
-      CORBA::ContainedSeq_var moduleContents = moduleDef->contents(CORBA::dk_all,1);
-      printContents( moduleContents.in() );
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "printContents");
+      ACE_OS::exit(-1); // the test has failed!
     }
-  }
+  ACE_ENDTRY;
 }
 
 
 int  main(int argc, char** argv)
 {
-  CORBA::ORB_var      orb = CORBA::ORB_init(argc, argv, "IFR_Test ORB");;
-  CORBA::Object_var    obj = orb->resolve_initial_references( "InterfaceRepository" );
-  CORBA::Repository_var  ifr = CORBA::Repository::_narrow( obj.in() );
+  ACE_TRY_NEW_ENV
+    {
+      CORBA::ORB_var      orb = CORBA::ORB_init (argc,
+                                    argv,
+                                    0
+                                    ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      CORBA::Object_var object =
+        orb->resolve_initial_references ("InterfaceRepository"
+                                         ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
 
-  if( CORBA::is_nil(ifr.in()) )
-  {
-    printf( "Unable to locate the Interface Repository.\n" );
-    return -1;
-  }
+      if (CORBA::is_nil (object.in ()))
+        {
+          ACE_ERROR_RETURN ((
+              LM_ERROR,
+              "Null objref from resolve_initial_references\n"
+            ),
+            -1
+          );
+        }
 
-  CORBA::ContainedSeq_var  cont = ifr->contents( CORBA::dk_all, 0 );
+      CORBA::Repository_var  ifr =
+        CORBA::Repository::_narrow (object.in ()
+                                    ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
 
-  printContents( cont.in() );
+      if (CORBA::is_nil (ifr.in ()))
+        {
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "CORBA::Repository::_narrow failed\n"),
+                            -1);
+        }
 
-  orb->shutdown();
+     CORBA::ContainedSeq_var  cont = ifr->contents (CORBA::dk_all, 0
+                                                    ACE_ENV_ARG_PARAMETER);
+     ACE_TRY_CHECK;
+
+     printContents (cont.in ());
+
+     orb->shutdown ();
+
+    }
+  ACE_CATCHANY
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "main");
+      return -1;
+    }
+  ACE_ENDTRY;
 
   return 0;
 }
