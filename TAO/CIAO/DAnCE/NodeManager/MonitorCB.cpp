876a88f1a69c064cdbe2b4f378a82ae78271da94
nilabjar <nilabjar@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-01-16 16:28:52 +0000

    Mon Jan 16 17:22:25 UTC 2006  Nilabja R  <nilabjar@dre.vanderbilt.edu>

---
+++ TAO/CIAO/DAnCE/NodeManager/MonitorCB.cpp
@@ -0,0 +1,55 @@
+// $Id$
+//----------------------------------------------------------------------------------
+/**
+ * @file MonitorCB.cpp
+ *
+ * @brief The Monitor Update class
+ *
+ * The class which updates the resource data
+ *
+ * @author Nilabja Roy <nilabjar@dre.vanderbilt.edu>
+ */
+//----------------------------------------------------------------------------------
+
+#include <iostream>
+#include "MonitorCB.h"
+#include "CIAO_common.h"
+
+
+MonitorCB::MonitorCB (CORBA::ORB_ptr orb,
+                      Deployment::TargetManager_ptr target,
+                      int interval
+                      ):orb_ (orb),
+                        target_mgr_ (target),
+                        interval_ (interval)
+{
+}
+
+int MonitorCB::update_data (::Deployment::Domain& data)
+{
+  CORBA::StringSeq elements;
+  elements.length (0);
+
+  // data to be updated ...
+  ::Deployment::DomainUpdateKind update_kind
+      = ::Deployment::UpdateAvailable;
+
+  // The copy of the domain variable
+  ::Deployment::Domain_var domain
+      = new ::Deployment::Domain (data);
+
+  try
+  {
+    if (CIAO::debug_level () > 9)
+      {
+        ACE_DEBUG ((LM_DEBUG , "---Making a call to update\n"));
+      }
+    target_mgr_->updateDomain (elements , domain , update_kind);
+  }
+  catch (CORBA::Exception& ex)
+    {
+      std::cout << ex << std::endl;
+    }
+
+  return 0;
+}
