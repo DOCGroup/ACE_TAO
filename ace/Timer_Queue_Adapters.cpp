dabf64812308833d9a43b45f1143e20b5454734b
schmidt <douglascraigschmidt@users.noreply.github.com>
2006-01-16 13:20:46 +0000

    ChangeLogTag:Mon Jan 16 05:54:44 2006  Douglas C. Schmidt  <schmidt@cs.wustl.edu>

--- ace/Timer_Queue_Adapters.cpp
+++ ace/Timer_Queue_Adapters.cpp
@@ -258,11 +258,19 @@ ACE_Thread_Timer_Queue_Adapter<TQ>::svc (void)
         {
           // Compute the remaining time, being careful not to sleep
           // for "negative" amounts of time.
-          ACE_Time_Value tv = this->timer_queue_->earliest_time ();
-
-          // ACE_DEBUG ((LM_DEBUG,  ACE_LIB_TEXT ("waiting until %u.%3.3u secs\n"),
-          // tv.sec(), tv.msec()));
-          this->condition_.wait (&tv);
+          const ACE_Time_Value tv_curr = this->timer_queue_->gettimeofday ();
+          const ACE_Time_Value tv_earl = this->timer_queue_->earliest_time ();
+ 
+          if (tv_earl > tv_curr)
+            {
+              // The earliest time on the Timer_Queue is in future, so
+              // use ACE_OS::gettimeofday() to convert the tv to the
+              // absolute time.
+              const ACE_Time_Value tv = ACE_OS::gettimeofday () + (tv_earl - tv_curr);  
+              // ACE_DEBUG ((LM_DEBUG,  ACE_LIB_TEXT ("waiting until %u.%3.3u secs\n"),
+              // tv.sec(), tv.msec()));
+              this->condition_.wait (&tv);
+            }
         }
 
       // Expire timers anyway, at worst this is a no-op.
