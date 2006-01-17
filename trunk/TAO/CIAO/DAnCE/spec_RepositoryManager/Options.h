0018fe72069901b8d1ee4afb70972c12c741e7a9
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-01-17 19:37:52 +0000

    ChangeLogTag: Tue Jan 17 18:50:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/CIAO/DAnCE/spec_RepositoryManager/Options.h
+++ trunk/TAO/CIAO/DAnCE/spec_RepositoryManager/Options.h
@@ -1,23 +1,20 @@
 // $Id$
 
 /* -*- C++ -*- */
-// Options.h, Stoyan
 
-//============================================================================
-//
-// = LIBRARY
-//    JAWS
-//
-// = FILENAME
-//    Options.h
-//
-// = DESCRIPTION
-//     Options is an Singleton for blobby
-//
-// = AUTHOR
-//    Stoyan Paunov
-//
-//============================================================================
+//=============================================================================
+/**
+ *  @file    Options.h
+ *
+ *  $Id$
+ *
+ *   Options is an Singleton for blobby
+ *
+ *
+ *  @author Stoyan Paunov
+ */
+//=============================================================================
+
 
 #ifndef RM_OPTIONS_H
 #define RM_OPTIONS_H
@@ -36,31 +33,31 @@ public:
   /// parses commandline arguments
   void parse_args (int argc, ACE_TCHAR *argv[]);
 
+  /// Name of package
   ACE_CString name_;
-  //Name of package
 
+  /// Name of package
   ACE_CString uuid_;
-  //Name of package
 
+  /// specifies the local path for install
   ACE_CString path_;
-  //specifies the local path for install
 
-  //delete the name_
+  /// delete the name_
   bool delete_;
 
-  //installs the name_
+  /// installs the name_
   bool install_;
 
-  //finds the name_
+  /// finds the name_
   bool find_;
 
-  //get all Names
+  /// get all Names
   bool all_names_;
 
-  //get all types
+  /// get all types
   bool all_types_;
 
-  //find all names by type
+  /// find all names by type
   bool names_by_type_;
 
   /// shutdown the RepositoryManagerDemon
@@ -73,7 +70,6 @@ protected:
 
   /// the singleton
   static Options *instance_;
-
 
   void usage (void);
 };
