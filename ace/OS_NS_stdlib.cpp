17d5ac0f8e993871f194bc8c0fba419143b182df
Johnny Willemsen <jwillemsen@remedy.nl>
2006-01-24 13:59:30 +0000

    ChangeLogTag: Tue Jan 24 13:43:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/OS_NS_stdlib.cpp
+++ ace/OS_NS_stdlib.cpp
@@ -25,7 +25,7 @@ ACE_RCSID (ace,
 # include "ace/OS_NS_fcntl.h"
 # include "ace/OS_NS_ctype.h"
 # include "ace/OS_NS_sys_time.h"
-# if !defined (ACE_HAS_WINCE) && !defined (ACE_VXWORKS) && !defined (max)
+# if !defined (ACE_HAS_WINCE) && !(defined (ACE_VXWORKS) && (ACE_VXWORKS == 0x551)) && !defined (max)
 #  include /**/ <limits>
 # endif
 #endif  /* ACE_LACKS_MKSTEMP */
@@ -626,7 +626,7 @@ ACE_OS::mkstemp_emulation (ACE_TCHAR * s)
   // greatly slowing down this mkstemp() implementation.  It is more
   // practical to limit the search space to UTF-8 / ASCII characters
   // (i.e. 127 characters).
-#  if defined (ACE_HAS_WINCE) || defined (ACE_VXWORKS) || defined (max)
+#  if defined (ACE_HAS_WINCE) || (defined (ACE_VXWORKS) && (ACE_VXWORKS == 0x551)) || defined (max)
   static float const MAX_VAL = static_cast<float> (127);
 #else
   static float const MAX_VAL =
