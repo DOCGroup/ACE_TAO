84716638895ec0a8013bcda446e038a061c5256b
olli <olli@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-01-25 14:39:56 +0000

    ChangeLogTag: Wed Jan 25 14:38:10 UTC 2006  Olli Savia  <ops@iki.fi>

--- trunk/ace/SSL/SSL_Asynch_BIO.h
+++ trunk/ace/SSL/SSL_Asynch_BIO.h
@@ -23,6 +23,9 @@
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+// This must be included before any <openssl> include on LynxOS
+#include "ace/os_include/os_stdio.h"
+
 #include <openssl/bio.h>
 
 #if OPENSSL_VERSION_NUMBER > 0x0090581fL && ((defined (ACE_WIN32) && !defined (ACE_HAS_WINCE)) || (defined (ACE_HAS_AIO_CALLS)))
