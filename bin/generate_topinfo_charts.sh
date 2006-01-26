3c59f5bf7ece6a27a7a5bf861feae8770dbe88ee
Johnny Willemsen <jwillemsen@remedy.nl>
2006-01-26 13:21:00 +0000

    ChangeLogTag: Thu Jan 26 13:16:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- bin/generate_topinfo_charts.sh
+++ bin/generate_topinfo_charts.sh
@@ -8,7 +8,7 @@ gnuplot <<_EOF_ >/dev/null 2>&1
   set timefmt '%Y/%m/%d-%H:%M'
   set xlabel 'Date (MM/DD)'
   set ylabel 'Size (KBytes) $4'
-  set terminal png small color
+  set terminal png small size 800,600 color
   set output "$2"
   plot '$1' using 1:2 title '$3' w l
   exit
