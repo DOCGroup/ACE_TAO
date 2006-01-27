7a559384baf7e75ad38a926c3adeb5651ce40885
William R. Otte <wotte@dre.vanderbilt.edu>
2006-01-27 19:08:48 +0000

    Fri Jan 27 19:07:08 UTC 2006  William Otte  <wotte@dre.vanderbilt.edu>

--- TAO/CIAO/DAnCE/NodeManager/MonitorController.h
+++ TAO/CIAO/DAnCE/NodeManager/MonitorController.h
@@ -18,15 +18,10 @@
 #include "ace/Task.h"
 #include "ace/Auto_Ptr.h"
 
-class MonitorBase;
+#include "ace/Synch_Traits.h"
+#include "ace/Synch.h"
 
-#ifdef ACE_HAS_THREADS
-#include "ace/Thread_Mutex.h"
-typedef ACE_Thread_Mutex Monitor_Mutex;
-#else
-#include "ace/Mutex.h"
-typedef ACE_Mutex Monitor_Mutex;
-#endif /* ACE_HAS_THREADS */
+class MonitorBase;
 
 /**
  *  @class MonitorController
@@ -98,7 +93,7 @@ class MonitorController : public ACE_Task_Base
     bool terminate_flag_;
 
     //Thread Mutex for synchronizing call
-  Monitor_Mutex lock_;
+  ACE_SYNCH_MUTEX lock_;
 
     // the ORB pointer ..
     CORBA::ORB_ptr orb_;
