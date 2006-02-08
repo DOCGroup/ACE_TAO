de3e2691ec302b3cefde618ff10819f41f49bd73
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-08 21:51:22 +0000

    This commit was manufactured by cvs2svn to create tag 'TAO-1_4_9'.

--- contrib/utility/Test/Synch/Inline/unit.cpp
+++
@@ -1,8 +0,0 @@
-// file      : Test/Synch/Inline/unit.cpp
-// author    : Boris Kolpackov <boris@kolpackov.net>
-// copyright : Copyright (c) 2002-2003 Boris Kolpackov
-// license   : http://kolpackov.net/license.html
-
-#include "Utility/Synch/Policy/Null.hpp"
-
-//$Id$
