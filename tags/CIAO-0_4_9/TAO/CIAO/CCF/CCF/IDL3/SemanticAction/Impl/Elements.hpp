244400893ca97ad45ca5220bf5ba59e474b381ed
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-08 21:53:48 +0000

    This commit was manufactured by cvs2svn to create tag 'CIAO-0_4_9'.

---
+++ tags/CIAO-0_4_9/TAO/CIAO/CCF/CCF/IDL3/SemanticAction/Impl/Elements.hpp
@@ -0,0 +1,30 @@
+// file      : CCF/IDL3/SemanticAction/Impl/Elements.hpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+#ifndef CCF_IDL3_SEMANTIC_ACTION_IMPL_ELEMENTS_HPP
+#define CCF_IDL3_SEMANTIC_ACTION_IMPL_ELEMENTS_HPP
+
+#include "CCF/IDL3/SemanticAction/Elements.hpp"
+#include "CCF/IDL2/SemanticAction/Impl/Elements.hpp"
+
+namespace CCF
+{
+  namespace IDL3
+  {
+    namespace SemanticAction
+    {
+      namespace Impl
+      {
+        using IDL2::SemanticAction::Impl::Context;
+        using IDL2::SemanticAction::Impl::Base;
+        using IDL2::SemanticAction::Impl::ScopeBase;
+
+        using IDL2::SemanticAction::Impl::lookup;
+
+      }
+    }
+  }
+}
+
+#endif  // CCF_IDL3_SEMANTIC_ACTION_IMPL_ELEMENTS_HPP
---
+++ tags/CIAO-0_4_9/TAO/CIAO/tools/Config_Handlers/XMLSchema/TypeInfo.ipp
@@ -0,0 +1,7 @@
+// file      : XMLSchema/TypeInfo.ipp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+namespace XMLSchema
+{ 
+}
