e6f778cae22bf61dfa457d007e6099684ba3e0e7
zhangw <zhangw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-09 20:08:49 +0000

    Thu Feb  9 20:06:18 UTC 2006  Wallace Zhang  <zhangw@ociweb.com>

--- trunk/TAO/tao/IORManipulation/IORManipulation.cpp
+++ trunk/TAO/tao/IORManipulation/IORManipulation.cpp
@@ -184,17 +184,15 @@ TAO_IOR_Manipulation_impl::remove_profiles (
     ACE_THROW_RETURN (TAO_IOP::Invalid_IOR (),
                       CORBA::Object::_nil ());
 
-  ACE_AUTO_PTR_RESET (tmp_pfiles,
-                      ior2->_stubobj ()->make_profiles (),
-                      TAO_MProfile);
-
-  if (Diff_Profiles.remove_profiles (tmp_pfiles.get ()) < 0)
-    ACE_THROW_RETURN (TAO_IOP::NotFound (),
-                      CORBA::Object::_nil ());
-
-  // MS C++ knows nothing about reset!
-  // tmp_pfiles.reset (0); // get rid of last MProfile
-
+  // We are done with add_profiles.
+  // At this point, we don't do remove_profiles()
+  // immediately like before,
+  // because it could result in an
+  // Object Reference with 0 profile. And it would not pass
+  // the CORBA::is_nil() evaluation.
+  // Instead, we create the Object Reference right here, which is
+  // earlier than before.(Actually, I just moved some code
+  // from below up to here).
   TAO_ORB_Core *orb_core = TAO_ORB_Core_instance ();
 
   TAO_Stub *stub = orb_core->create_stub (id.in (), // give the id string
@@ -224,6 +222,20 @@ TAO_IOR_Manipulation_impl::remove_profiles (
       ACE_THROW_RETURN (TAO_IOP::Invalid_IOR (),
                         CORBA::Object::_nil ());
     }
+
+  // Now we can remove the profiles which we want to elimitate from
+  // the Object.
+  ACE_AUTO_PTR_RESET (tmp_pfiles,
+                      ior2->_stubobj ()->make_profiles (),
+                      TAO_MProfile);
+
+  TAO_MProfile& mp = stub -> base_profiles();
+  if (mp.remove_profiles (tmp_pfiles.get ()) < 0)
+    ACE_THROW_RETURN (TAO_IOP::NotFound (),
+                      CORBA::Object::_nil ());
+
+  // MS C++ knows nothing about reset!
+  // tmp_pfiles.reset (0); // get rid of last MProfile
 
   return new_obj._retn ();
 }
