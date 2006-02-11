2f53541c129f662e6b09b364942102fbe79fa021
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-11 21:27:46 +0000

    Sat Feb 11 21:26:08 UTC 2006  Gan Deng  <gan.deng@vanderbilt.edu>

--- trunk/TAO/CIAO/tests/Bug_2130_Regression/descriptors/run_test.pl
+++ trunk/TAO/CIAO/tests/Bug_2130_Regression/descriptors/run_test.pl
@@ -61,7 +61,7 @@ sub run_node_daemons {
       $node_app = "$DAnCE/NodeApplication/NodeApplication";
 
       $d_cmd = "$DAnCE/NodeManager/NodeManager";
-      $d_param = "-ORBEndpoint $iiop -s $node_app -o $iorfile";
+      $d_param = "-ORBEndpoint $iiop -s $node_app -o $iorfile -a \"-r\"";
 
       $Daemons[$i] = new PerlACE::Process ($d_cmd, $d_param);
 
