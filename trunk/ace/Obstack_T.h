e5dc52dae41490b2519562da9befe491391b7329
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-13 10:08:06 +0000

    ChangeLogTag: Mon Feb 13 10:01:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ace/Obstack_T.h
+++ trunk/ace/Obstack_T.h
@@ -27,7 +27,7 @@ ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 class ACE_Allocator;
 
 /**
- * @class ACE_Obstack
+ * @class ACE_Obstack_T
  *
  * @brief Define a simple "mark and release" memory allocation utility.
  *
