c1fbc40bcf8a321b76161bf8fb4452fdd78f5031
jtc <jtc@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-23 19:55:35 +0000

    ChangeLogTag: Thu Feb 23 19:53:51 UTC 2006  J.T. Conklin  <jtc@acorntoolworks.com>

--- tests/DLL_Test_Impl.cpp
+++ tests/DLL_Test_Impl.cpp
@@ -73,7 +73,7 @@ Hello_Impl::operator new (size_t bytes, const ACE_nothrow_t &nt)
 
 #if !defined (ACE_LACKS_PLACEMENT_OPERATOR_DELETE)
 void
-Hello_Impl::operator delete (void *ptr, const ACE_nothrow_t&)
+Hello_Impl::operator delete (void *ptr, const ACE_nothrow_t&) throw ()
 {
   ACE_DEBUG ((LM_INFO, "Hello_Impl::delete\n"));
   ::delete [] static_cast<char *> (ptr);
