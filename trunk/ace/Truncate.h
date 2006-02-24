55b8686527dd969743aa96c7e90f3093779e02f6
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-02-24 17:34:00 +0000

    ChangeLogTag:Fri Feb 24 17:29:47 UTC 2006  Steve Huston  <shuston@riverace.com>

--- trunk/ace/Truncate.h
+++ trunk/ace/Truncate.h
@@ -20,6 +20,19 @@
 
 #include "ace/Global_Macros.h"
 
+#if !defined(ACE_LACKS_NUMERIC_LIMITS)
+// some platforms pollute the namespace by defining max() and min() macros
+#  ifdef max
+#    undef max
+#  endif
+#  ifdef min
+#    undef min
+#  endif
+#  include <limits>
+#else
+#  include "ace/os_include/os_limits.h"
+#endif /* ACE_LACKS_NUMERIC_LIMITS */
+
 ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 
 namespace ACE_Utils
@@ -28,25 +41,44 @@ namespace ACE_Utils
  * @class Truncate
  *
  * @brief Helper function to truncate an integral value to an int.
+ *
  *        Very useful since ACE methods return int very often and the value's
  *        source is often a different-size integral type, such as size_t.
  *        This function hides the truncation logic and resolves compiler
  *        diagnostics.
+ *
+ * @internal Internal use only.
  */
 template<typename X>
-ACE_INLINE_TEMPLATE_FUNCTION int Truncate (const X& val);
+inline int Truncate (const X& val)
+{
+#if !defined (ACE_LACKS_NUMERIC_LIMITS)
+  if (val > static_cast<X> (std::numeric_limits<int>::max ()))
+    return std::numeric_limits<int>::max ();
+#else
+  if (val > static_cast<X> (INT_MAX))
+    return INT_MAX;
+#endif /* ACE_LACKS_NUMERIC_LIMITS */
+  return static_cast<int> (val);
+}
 
 // Specialize one for size_t to alleviate the explicit instantiation pain.
 template<>
-ACE_INLINE_TEMPLATE_FUNCTION int Truncate<size_t> (const size_t& val);
+inline int Truncate<size_t> (const size_t& val)
+{
+#if !defined (ACE_LACKS_NUMERIC_LIMITS)
+  if (val > static_cast<size_t> (std::numeric_limits<int>::max ()))
+    return std::numeric_limits<int>::max ();
+#else
+  if (val > static_cast<size_t> (INT_MAX))
+    return INT_MAX;
+#endif /* ACE_LACKS_NUMERIC_LIMITS */
+  return static_cast<int> (val);
+}
 
 } // namespace ACE_Utils
 
 ACE_END_VERSIONED_NAMESPACE_DECL
-
-#if defined(__ACE_INLINE__)
-# include "ace/Truncate.inl"
-#endif /* __ACE_INLINE__ */
 
 #include /**/ "ace/post.h"
 #endif /* ACE_TRUNCATE_H*/
