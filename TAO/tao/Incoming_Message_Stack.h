123530551232fd5f1288acaf3c7a154cc1784f9b
Johnny Willemsen <jwillemsen@remedy.nl>
2006-02-25 14:55:59 +0000

    ChangelogTag: Sat Feb 25 12:45:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/Incoming_Message_Stack.h
+++ TAO/tao/Incoming_Message_Stack.h
@@ -15,11 +15,13 @@
 
 #include /**/ "ace/pre.h"
 
-#include "tao/Incoming_Message_Queue.h"
+#include "tao/TAO_Export.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/Incoming_Message_Queue.h"
 
 ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 class ACE_Allocator;
@@ -37,36 +39,36 @@ namespace TAO
  * Internal class, providing stack functionality for TAO_Queued_Data
  * objects.  Stack operations don't require memory allocation.
 */
-class Incoming_Message_Stack
+class TAO_Export Incoming_Message_Stack
 {
 public:
   /// default constructor, intiliazes empty stack.
   Incoming_Message_Stack();
-  
+
   /// destructor, releases all elements on stack
   ~Incoming_Message_Stack() ;
-  
-  /// pushing a new element onto stack, 
+
+  /// pushing a new element onto stack,
   /// @a data must be a valid pointer, not NULL
   void push(TAO_Queued_Data *data);
-  
+
   /// removing top element of stack,
   /// @return 0 for Ok and @a data is defined, -1 for error
   int pop (TAO_Queued_Data* &data);
-  
+
   /// peeking top element of stack
   /// @return 0 for Ok, -1 for error
   int top (TAO_Queued_Data* &data);
-  
+
 private:
   /// top element of stack
   TAO_Queued_Data *top_;
-  
+
 private:
   /// Default Copy-Constructor -  not for public usage.
   Incoming_Message_Stack (const Incoming_Message_Stack&);
-  
-  /// Assignment operator - not for public usage 
+
+  /// Assignment operator - not for public usage
   Incoming_Message_Stack& operator= (Incoming_Message_Stack& other);
 };
 }
