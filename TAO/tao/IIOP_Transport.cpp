5d7d13772adb9ce0caedc42034dbb16e5dc32325
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-06 15:57:41 +0000

    ChangeLogTag: Mon Mar 06 15:56:12 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/IIOP_Transport.cpp
+++ TAO/tao/IIOP_Transport.cpp
@@ -375,7 +375,7 @@ TAO_IIOP_Transport::get_listen_point (
 #if defined (ACE_HAS_IPV6)
   // If this is an IPv6 decimal linklocal address containing a scopeid than
   // remove the scopeid from the information being sent.
-  const char *cp_scope;
+  const char *cp_scope = 0;
   if (local_addr.get_type () == PF_INET6 &&
         (cp_scope = ACE_OS::strchr (local_interface.in (), '%')) != 0)
     {
