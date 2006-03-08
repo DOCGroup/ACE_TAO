3e6ad7c4d8dba8c5067eb7d730838d43d57175f9
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-08 13:04:29 +0000

    ChangeLogTag: Wed Mar  8 12:31:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/IFR_Client/IFR_Client_Adapter_Impl.cpp
+++ TAO/tao/IFR_Client/IFR_Client_Adapter_Impl.cpp
@@ -1,7 +1,7 @@
 // $Id$
 
 #include "IFR_Client_Adapter_Impl.h"
-#include "IFR_ExtendedC.h"
+#include "IFR_BasicC.h"
 
 #include "tao/AnyTypeCode/NVList.h"
 #include "tao/AnyTypeCode/Any_Unknown_IDL_Type.h"
