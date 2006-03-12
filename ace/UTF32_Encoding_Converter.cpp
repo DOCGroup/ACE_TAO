f8a3b7c212caa78599f1ff6027e8855c4db53423
Johnny Willemsen <jwillemsen@remedy.nl>
2006-03-12 19:20:50 +0000

    ChangeLogTag: Sun Mar 12 19:11:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/UTF32_Encoding_Converter.cpp
+++ ace/UTF32_Encoding_Converter.cpp
@@ -230,7 +230,7 @@ ACE_UTF32_Encoding_Converter::encoded (const ACE_Byte* source,
   static const size_t converted = begin * 4;
 
   ACE_Byte target[converted];
-  ACE_UTF32_Encoding_Converter* converter;
+  ACE_UTF32_Encoding_Converter* converter = 0;
   ACE_NEW_RETURN (converter,
                   ACE_UTF32_Encoding_Converter (false),
                   0);
