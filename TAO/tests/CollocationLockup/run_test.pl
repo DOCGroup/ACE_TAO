de94db8db3c0865d539e2519152de1b4c9f1b5c2
elliott_c <ocielliottc@users.noreply.github.com>
2006-03-13 15:06:58 +0000

    ChangeLogTag: Mon Mar 13 15:06:08 UTC 2006  Chad Elliott  <elliott_c@ociweb.com>

--- TAO/tests/CollocationLockup/run_test.pl
+++ TAO/tests/CollocationLockup/run_test.pl
@@ -5,11 +5,9 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-# This is a Perl script that runs some tests.
-
 use lib '../../../bin';
 use PerlACE::Run_Test;
-use Cwd;
+
 use strict;
 
 # Amount of delay (in seconds) between starting a server and a client
@@ -18,7 +16,8 @@ my $sleeptime = 10;
 
 # Variables for command-line arguments to client and server
 # executables.
-my $iorfile = PerlACE::LocalFile ("SimpleNamingService.ior");
+my $iorbase = "SimpleNamingService.ior";
+my $iorfile = PerlACE::LocalFile ($iorbase);
 
 my $status = 0;
 
@@ -34,9 +33,17 @@ if (PerlACE::waitforfile_timed ($iorfile, $sleeptime) == -1) {
   exit 1;
 }
 
-my $CL = new PerlACE::Process ("CollocationLockup",
-                               "-ORBInitRef " .
-                               "SimpleNamingService=file://$iorfile");
+my $CL;
+if (PerlACE::is_vxworks_test()) {
+  $CL = new PerlACE::ProcessVX ("CollocationLockup",
+                                "-ORBInitRef " .
+                                "SimpleNamingService=file://$iorbase");
+}
+else {
+  $CL = new PerlACE::Process ("CollocationLockup",
+                              "-ORBInitRef " .
+                              "SimpleNamingService=file://$iorfile");
+}
 
 # In testing on various platforms, the builds with the bug failed before 
 # 20 seconds and when the bug was fixed it returned before 20 seconds.
