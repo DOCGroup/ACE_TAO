442cbf8ad7fcfd5143d303210c17cb25a21c917f
nilabjar <nilabjar@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-03-13 21:24:43 +0000

    ChangeLogTag: Mon Mar 13 22:21:25 UTC 2006  Nilabja R  <nilabjar@dre.vanderbilt.edu>

--- TAO/CIAO/DAnCE/NodeManager/MonitorController.h
+++ TAO/CIAO/DAnCE/NodeManager/MonitorController.h
@@ -34,6 +34,91 @@
 namespace CIAO
 {
 
+<<<<<<< MonitorController.h
+  class MonitorBase;
+
+  /**
+   *  @class MonitorController
+   *
+   *  @brief The Controller class
+   *
+   *  Acts as a facade to the Monitor
+   *  module
+   *
+   */
+  class MonitorController : public ACE_Task_Base
+    {
+    public:
+      /**
+       * @function svc.
+       * @brief The main function containing the
+       * entire activity.
+       *
+       * This function runs in the context of the
+       * thread
+       * @return int The status of the function
+       */
+      int svc (void);
+      /**
+       * @function terminate.
+       * @brief The terminate function
+       *
+       * This function is called to terminate the
+       * thread
+       */
+      void terminate ();
+      
+      
+      /// The Constructor.
+      MonitorController (CORBA::ORB_ptr orb,
+                         ::Deployment::Domain& domain,
+                         ::Deployment::TargetManager_ptr target
+                         );
+      /// The init function. This function initializes the monitor
+      
+      /**
+       * @function init.
+       * @brief The init function
+       *
+       * This function is called to during initialization
+       */
+      int init();
+      
+      ~MonitorController ();
+    protected:
+      
+      /**
+       * @function terminating.
+       * @brief returns the terminating flag
+       * @return bool The terminting state of the thread
+       */
+      bool terminating ();
+      
+      /// The monitor object
+      auto_ptr <MonitorBase>  monitor_;
+      
+      /// The TargetManagerImpl object
+      CIAO::TargetManagerImpl_var target_impl_cmp_;
+      
+      /// The TargetManager Facet ....
+      Deployment::TargetManager_var target_facet_i_;
+      
+      /// The terminate flag_
+      bool terminate_flag_;
+      
+      //Thread Mutex for synchronizing call
+      ACE_SYNCH_MUTEX lock_;
+      
+      // the ORB pointer ..
+      CORBA::ORB_ptr orb_;
+      
+      /// The initial domain
+      ::Deployment::Domain initial_domain_;
+    };
+
+}; // CIAO
+  
+=======
   class MonitorBase;
 
   /**
@@ -108,4 +193,5 @@ namespace CIAO
 
 } // CIAO
   
+>>>>>>> 1.8
 #endif
