4df11023733f2a4d256908c1271b676e617a0a96
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-03-29 12:00:51 +0000

    *** empty log message ***

--- branches/sendfile/ace/OS_NS_sys_sendfile.cpp
+++ branches/sendfile/ace/OS_NS_sys_sendfile.cpp
@@ -17,10 +17,15 @@ ACE_OS::sendfile_emulation (ACE_HANDLE out_fd,
                             ACE_LOFF_T * offset,
                             size_t count)
 {
-  void const * const buf =
+  // @@ We may want set up a signal lease (or oplock) if supported by
+  //    the platform so that we don't get a bus error if the mmap()ed
+  //    file is truncated.
+  void * const buf =
     ACE_OS::mmap (0, count, PROT_READ, MAP_PRIVATE, in_fd, *offset);
 
   ssize_t const r = ACE_OS::write (out_fd, buf, count);
+
+  (void) ACE_OS::munmap (buf, count);
 
   if (r > 0)
     *offset += static_cast<ACE_LOFF_T> (r);
