568aa729b79a3a15080f57d425aaae6c02937273
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-03-30 12:44:05 +0000

    ChangeLogTag: Thu Mar 30 12:32:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/examples/APG/Containers/Stacks.cpp
+++ trunk/examples/APG/Containers/Stacks.cpp
@@ -128,7 +128,7 @@ int StackExample::runUnboundedStack (void)
   ACE_Unbounded_Stack_Iterator<DataElement*> iter (ustack);
   for (iter.first (); !iter.done (); iter.advance ())
     {
-      DataElement** elem;
+      DataElement** elem = 0;
       iter.next (elem);
       ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("%d:"),
                   (*elem)->getData ()));
