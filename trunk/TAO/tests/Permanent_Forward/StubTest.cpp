a812e9f49caf2bd0de8314b946926aa57efa44a7
smcqueen <smcqueen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-03-30 10:05:46 +0000

    ChangeLogTag: Thu Mar 30 09:58:11 UTC 2006  Simon McQueen  <sm@prismtech.com>

--- trunk/TAO/tests/Permanent_Forward/StubTest.cpp
+++ trunk/TAO/tests/Permanent_Forward/StubTest.cpp
@@ -21,6 +21,13 @@
 #include "tao/Profile.h"
 #include "tao/IIOP_Profile.h"
 
+#ifndef FRANKS_ASSERT
+#define FRANKS_ASSERT(X) \
+  ((X)                                                                  \
+   ? static_cast<void>(0)                                               \
+   : ACE_VERSIONED_NAMESPACE_NAME::__ace_assert(__FILE__, __LINE__, ACE_TEXT_CHAR_TO_TCHAR (#X)))
+#endif
+
 ACE_RCSID (Permanent_Forward,
            StubTest,
            "$Id$")
@@ -33,9 +40,9 @@ is_endpoint (TAO_Profile *profile, const char *host, unsigned short port)
     TAO_IIOP_Endpoint *iiop_endpoint = dynamic_cast<TAO_IIOP_Endpoint*> (endpoint);
     const char     * endpoint_host = iiop_endpoint->host();
     unsigned short   endpoint_port = iiop_endpoint->port();
-    
-    bool retval = 
-      strcmp (endpoint_host, host)==0 
+
+    bool retval =
+      strcmp (endpoint_host, host)==0
       && endpoint_port == port;
 
     return retval;
@@ -55,9 +62,9 @@ equal_endpoint (TAO_Profile *profile, TAO_Profile *other)
     TAO_IIOP_Endpoint *other_iiop_endpoint = dynamic_cast<TAO_IIOP_Endpoint*> (other_endpoint);
     const char     * other_endpoint_host = other_iiop_endpoint->host();
     unsigned short   other_endpoint_port = other_iiop_endpoint->port();
-    
-    bool retval = 
-      strcmp (endpoint_host, other_endpoint_host)==0 
+
+    bool retval =
+      strcmp (endpoint_host, other_endpoint_host)==0
       && endpoint_port == other_endpoint_port;
 
     return retval;
@@ -74,7 +81,7 @@ marshaled_equal_to_other (CORBA::ORB_ptr orb, CORBA::Object_ptr obj, TAO_MProfil
 
   TAO_Stub *stub_copy = obj_copy->_stubobj ();
   TAO_MProfile *copy_mprofile  = &(stub_copy->base_profiles ());
- 
+
   if ( copy_mprofile->size() != other_mprofile->size())
     return false;
 
@@ -83,7 +90,7 @@ marshaled_equal_to_other (CORBA::ORB_ptr orb, CORBA::Object_ptr obj, TAO_MProfil
       TAO_Profile *copy_profile  = copy_mprofile->get_profile (i);
       TAO_Profile *other_profile = other_mprofile->get_profile (i);
 
-      if ( ! equal_endpoint (copy_profile, other_profile) ) 
+      if ( ! equal_endpoint (copy_profile, other_profile) )
         return false;
     }
 
@@ -95,46 +102,46 @@ test_forward_permanent (CORBA::ORB_ptr orb)
 {
   CORBA::Object_var obj1 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:1111,iiop:192.168.1.3:1111/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
-  
+
   // the permanent profile
   CORBA::Object_var obj4 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:4444,iiop:192.168.1.3:4444/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
- 
+
   TAO_Stub *stub1 = obj1->_stubobj ();
   TAO_Stub *stub4 = obj4->_stubobj ();
 
   TAO_Profile *profile = NULL;
-  
+
   profile = stub1->profile_in_use ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 1111));
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 1111));
 
   // ----- forward to obj2 permanently
   stub1->add_forward_profiles (stub4->base_profiles(), true /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 4444));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 4444));
 
   // ----- stringified object reference must be equal to obj4->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub4->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub4->base_profiles())) );
 
   // ----- consume second profile of obj2
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.3", 4444));
-  
-  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.3", 4444));
+
+
   // ----- reached end, next_profile() must yield NULL
-  
+
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (profile == NULL);
+  FRANKS_ASSERT (profile == NULL);
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL); 
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 }
 
 static void
@@ -142,13 +149,13 @@ test_forward_permanent_mix (CORBA::ORB_ptr orb)
 {
   CORBA::Object_var obj1 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:1111,iiop:192.168.1.3:1111/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
-  
+
   CORBA::Object_var obj2 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:2222,iiop:192.168.1.3:2222/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
-   
+
   CORBA::Object_var obj3 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:3333,iiop:192.168.1.3:3333/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
-    
+
   CORBA::Object_var obj4 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:4444,iiop:192.168.1.3:4444/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
 
@@ -162,95 +169,95 @@ test_forward_permanent_mix (CORBA::ORB_ptr orb)
   TAO_Stub *stub5 = obj5->_stubobj ();
 
   TAO_Profile *profile = NULL;
-  
+
   profile = stub1->profile_in_use ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 1111));
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 1111));
 
-  // ----- forward to obj2 
+  // ----- forward to obj2
   stub1->add_forward_profiles (stub2->base_profiles(), false /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 2222));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 2222));
 
-  // ----- forward to obj3 
+  // ----- forward to obj3
   stub1->add_forward_profiles (stub3->base_profiles(), false /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 3333));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 3333));
 
   // ----- stringified object reference must be equal to obj1->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub1->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub1->base_profiles())) );
 
-  // ----- forward to obj4 permanently 
+  // ----- forward to obj4 permanently
   stub1->add_forward_profiles (stub4->base_profiles(), true /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 4444));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 4444));
 
   // ----- stringified object reference must be equal to obj4->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub4->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub4->base_profiles())) );
 
   // ----- consume second profile from obj4
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.3", 4444));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.3", 4444));
 
-  // ----- forward to obj2 
+  // ----- forward to obj2
   stub1->add_forward_profiles (stub2->base_profiles(), false /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 2222));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 2222));
 
-  // ----- forward to obj3 
+  // ----- forward to obj3
   stub1->add_forward_profiles (stub3->base_profiles(), false /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 3333));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 3333));
 
   // ----- stringified object reference must be equal to obj4->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub4->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub4->base_profiles())) );
 
-  // ----- forward to obj5 permanently 
+  // ----- forward to obj5 permanently
   stub1->add_forward_profiles (stub5->base_profiles(), true /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 5555));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 5555));
 
   // ----- stringified object reference must be equal to obj5->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub5->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub5->base_profiles())) );
 
   // ----- consume second profile from obj5
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.3", 5555));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.3", 5555));
 
   // ----- stringified object reference must be equal to obj1->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub5->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub5->base_profiles())) );
 
   // reached end of profiles, next_profile must return NULL
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (profile == NULL);
+  FRANKS_ASSERT (profile == NULL);
 }
 
 static void
@@ -258,10 +265,10 @@ test_forward (CORBA::ORB_ptr orb)
 {
   CORBA::Object_var obj1 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:1111,iiop:192.168.1.3:1111/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
-  
+
   CORBA::Object_var obj2 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:2222,iiop:192.168.1.3:2222/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
- 
+
   CORBA::Object_var obj3 = orb->string_to_object ("corbaloc:iiop:192.168.1.2:3333,iiop:192.168.1.3:3333/NameService" ACE_ENV_ARG_PARAMETER);
   ACE_TRY_CHECK;
 
@@ -270,52 +277,52 @@ test_forward (CORBA::ORB_ptr orb)
   TAO_Stub *stub3 = obj3->_stubobj ();
 
   TAO_Profile *profile = NULL;
-  
+
   profile = stub1->profile_in_use ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 1111));
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 1111));
 
-  // ----- forward to obj2 
+  // ----- forward to obj2
   stub1->add_forward_profiles (stub2->base_profiles(), false /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 2222));  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 2222));
 
-  // ----- forward to obj3 
+  // ----- forward to obj3
 
   stub1->add_forward_profiles (stub3->base_profiles(), false /* permanent */ );
 
-  ACE_ASSERT (stub1->forward_profiles () != NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () != NULL);
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.2", 3333)); 
-  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.2", 3333));
+
   // ----- stringified object reference must be equal to obj1->base_prpfiles().
-  ACE_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub1->base_profiles())) );
+  FRANKS_ASSERT (marshaled_equal_to_other (orb, obj1, &(stub1->base_profiles())) );
 
   // ----- consume second profile of obj3
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.3", 3333)); 
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.3", 3333));
 
   // ----- consume second profile of obj2
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.3", 2222));
-  
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.3", 2222));
+
   // ----- consume second profile of obj1
 
   profile = stub1->next_profile ();
 
-  ACE_ASSERT (is_endpoint (profile, "192.168.1.3", 1111)); 
+  FRANKS_ASSERT (is_endpoint (profile, "192.168.1.3", 1111));
 
-  ACE_ASSERT (stub1->forward_profiles () == NULL);
+  FRANKS_ASSERT (stub1->forward_profiles () == NULL);
 
   // ----- reached end, next_profile() must yield NULL
 
