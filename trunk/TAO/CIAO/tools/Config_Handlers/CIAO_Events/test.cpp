8cab67f336685c081d4d23a5f7a4987bac5d9b12
mxiong <mxiong@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-04-04 17:29:48 +0000

    ChangeLogTag:Tue Apr  4 17:27:52 UTC 2006  xiong,ming  <ming.xiong@vanderbilt.edu>

--- trunk/TAO/CIAO/tools/Config_Handlers/CIAO_Events/test.cpp
+++ trunk/TAO/CIAO/tools/Config_Handlers/CIAO_Events/test.cpp
@@ -46,41 +46,10 @@ int main (int argc, char *argv[])
   if (parse_args (argc, argv) != 0)
     return 1;
 
-  // Initialize an ORB so Any will work
-  CORBA::ORB_ptr orb = CORBA::ORB_init (argc, argv, "");
+  CIAOEvents_Handler event_handler (input_file);
+  // Convert XSC to idl datatype
 
-  //Create an XML_Helper for all the file work
-  XML_Helper the_helper;
-
-//  if (xercesc::DOMDocument *doc = the_helper.create_dom (input_file))
-  {
-    //Read in the XSC type structure from the DOMDocument
-    //ServerResourcesDef srd = ServerResources (doc);
-
-    CIAOEvents_Handler event_handler (input_file);
-    // Convert XSC to idl datatype
-    // SRD_Handler srd_handler (&srd);
-
-    std::cout << "Instance document import succeeded.  Dumping contents to file\n";
-
-    /*CIAO::DAnCE::ServerResource *sr (srd_handler.srd_idl ());
-
-    SRD_Handler reverse_handler (sr);
-
-    xercesc::DOMDocument *the_xsc (the_helper.create_dom ("CIAO:ServerResources",
-                                                          "http://www.dre.vanderbilt.edu/ServerResources"));
-
-    std::cout << "NS:"
-              << std::string (xercesc::XMLString::transcode (the_xsc->getDocumentElement ()->getNamespaceURI ())) << std::endl
-              << std::string (xercesc::XMLString::transcode (the_xsc->getDocumentElement ()->getPrefix ())) << std::endl
-              << std::string (xercesc::XMLString::transcode (the_xsc->getDocumentElement ()->getLocalName ())) << std::endl
-              << std::endl;
-
-    ServerResources (*reverse_handler.srd_xsc (), the_xsc);
-
-    // write out the result
-    the_helper.write_DOM (the_xsc, "output.srd"); */
-  }
+  std::cout << "Instance document import succeeded.  Dumping contents to file\n";
 
   std::cout << "Test completed!";
 
