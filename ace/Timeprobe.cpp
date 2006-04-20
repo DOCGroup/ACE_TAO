f6183f00b296e44229b01f6634813f155d34e01b
Johnny Willemsen <jwillemsen@remedy.nl>
2006-04-20 07:23:16 +0000

    ChangeLogTag: Thu Apr 20 07:17:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/Timeprobe.cpp
+++ ace/Timeprobe.cpp
@@ -6,15 +6,9 @@ ACE_RCSID(ace, Timeprobe, "$Id$")
 
 #if defined (ACE_COMPILE_TIMEPROBES)
 
-#include "ace/Timeprobe.h"
-#include "ace/Mutex.h"
-#include "ace/Null_Mutex.h"
-
 #if !defined (__ACE_INLINE__)
 #include "ace/Timeprobe.inl"
 #endif /* __ACE_INLINE__ */
-
-ACE_END_VERSIONED_NAMESPACE_DECL
 
 #endif /* ACE_COMPILE_TIMEPROBES */
 
