f8421f5f9913bcd691912d3ca673c4509d2579f6
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-04-20 19:26:45 +0000

    ChangeLogTag: Thu Apr 20 18:16:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ace/os_include/sys/os_types.h
+++ trunk/ace/os_include/sys/os_types.h
@@ -78,6 +78,8 @@ typedef double ACE_timer_t;
 
 #if defined (ACE_SIZEOF_LONG) && ACE_SIZEOF_LONG == 8
    typedef off_t ACE_LOFF_T;
+#elif defined (__FreeBSD__) || defined (__NetBSD__) || defined (__OpenBSD__) || defined (__APPLE__)
+   typedef off_t ACE_LOFF_T;
 #elif defined (__sgi) || defined (AIX) || defined (HPUX) || defined (__QNX__)
    typedef off64_t ACE_LOFF_T;
 #elif defined (__sun)
