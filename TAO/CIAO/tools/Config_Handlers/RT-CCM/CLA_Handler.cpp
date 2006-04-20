fc005338de86e9aa869e06e1a490b49f7e108c47
Johnny Willemsen <jwillemsen@remedy.nl>
2006-04-20 09:13:59 +0000

    ChangeLogTag: Thu Apr 20 08:32:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/CIAO/tools/Config_Handlers/RT-CCM/CLA_Handler.cpp
+++ TAO/CIAO/tools/Config_Handlers/RT-CCM/CLA_Handler.cpp
@@ -12,7 +12,7 @@ namespace CIAO
                                                  ::CIAO::DAnCE::CommandlineArgs &dest)
 	{
 	    ServerCmdlineOptions sco = src.cmdline();
-	   
+
 	    for(ServerCmdlineOptions::arg_iterator loc = sco.begin_arg();
 		loc != sco.end_arg();
 		loc++)
@@ -20,25 +20,25 @@ namespace CIAO
 		CORBA::ULong len = dest.length();
 
 		dest.length(len+1);
-                
+
                 dest[len] = CORBA::string_dup (loc->c_str ());
 	    }
 
-	    return true;	
+	    return true;
 	}
 
         ServerCmdlineOptions
 	CLA_Handler::commandline_argument (const CIAO::DAnCE::CommandlineArgs &src)
 	{
           ServerCmdlineOptions co;
-          
+
           for  (CORBA::ULong i = 0;
                 i < src.length ();
                 ++i)
             {
-              co.add_arg (src[i].in ());
+              co.add_arg (src[i]);
             }
-          
+
           return co;
         }
     }
