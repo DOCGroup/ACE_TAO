17ca8057973a85ae1c0ccbffa0f57b27bb6d5e08
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-04-25 09:43:56 +0000

    ChangeLogTag: Tue Apr 25 09:35:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/orbsvcs/orbsvcs/Trader/Interpreter_Utils.h
+++ trunk/TAO/orbsvcs/orbsvcs/Trader/Interpreter_Utils.h
@@ -34,27 +34,27 @@ class TAO_Trading_Serv_Export TAO_String_Hash_Key : public CORBA::String_var
 {
 public:
   // = Initialization and termination methods.
+  /// Default constructor.
   TAO_String_Hash_Key (void);
-  // Default constructor.
 
+  /// Constructor from a const string.
   TAO_String_Hash_Key (char * name);
-  // Constructor from a const string.
 
+  /// Constructor from a const string.
   TAO_String_Hash_Key (const char * name);
-  // Constructor from a const string.
 
+  /// Copy constructor.
   TAO_String_Hash_Key (const CORBA::String_var &hash_key);
-  // Copy constructor.
 
+  /// The operator for hash binding and "find"ing.
   bool operator == (const TAO_String_Hash_Key &hash_key) const;
-  // The operator for hash binding and "find"ing.
 
+  /// The operator for hash binding and "find"ing.
   friend bool operator < (const TAO_String_Hash_Key &left,
                           const TAO_String_Hash_Key &right);
-  // The operator for hash binding and "find"ing.
 
+  /// The function that computes a hash value.
   u_long hash (void) const;
-  // The function that computes a hash value.
 };
 
 #if defined ACE_HAS_EXPLICIT_TEMPLATE_INSTANTIATION_EXPORT
