e74ad671ad09cb0fceb2489289eb8125c7f42b63
jeliazkov_i <jeliazkov_i@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-04-30 06:47:57 +0000

    ChangeLogTag: Sun Apr 30 04:54:25 UTC 2006  Iliyan Jeliazkov  <iliyan@ociweb.com>

--- trunk/TAO/tests/ORB_Local_Config/Bug_1459/Test.cpp
+++ trunk/TAO/tests/ORB_Local_Config/Bug_1459/Test.cpp
@@ -2,12 +2,13 @@
 // @author Jeff Mirwaisi <jeff_mirwaisi@yahoo.com>
 // @author Iliyan Jeliazkov <iliyan2ociweb.com>
 
-//the following is a simplification of the above problems and the different
-//scenarios minus the creation of the servant the service manager or threads
-//even without the servant to demonstrate that the server is capable of
-//responding (which in some cases it isnt) problems can allready be seen
-//in the multiple orb scenarios AB b isnt prompted for a new certificate
-//password, MA ssliop isnt loaded at all etc
+// the following is a simplification of the above problems (see
+// bugzilla 1459) and the different scenarios minus the creation of
+// the servant the service manager or threads even without the servant
+// to demonstrate that the server is capable of responding (which in
+// some cases it isnt) problems can allready be seen in the multiple
+// orb scenarios AB b isnt prompted for a new certificate password, MA
+// ssliop isnt loaded at all etc
 
 
 /// It's a test - we need ACE_ASSERT
@@ -17,7 +18,9 @@
 
 #include "tao/corba.h"
 #include "ace/ARGV.h"
+#include "ace/Dynamic_Service.h"
 
+/// The combination of orb instances and their configurations to test
 #define MORB_MA
 
 ACE_RCSID (tests, server, "$Id$")
@@ -25,65 +28,51 @@ ACE_RCSID (tests, server, "$Id$")
 
 #include "Service_Configuration_Per_ORB.h"
 
-// @brief The "new" interfaces must be compatible with the "old" ones
+const char argA[] = "-ORBId ORB-A -ORBSvcConf a.conf";
+
+// dynamic SSLIOP_Factory Service_Object * TAO_SSLIOP:_make_TAO_SSLIOP_Protocol_Factory() "-SSLAuthenticate SERVER_AND_CLIENT -SSLPrivateKey PEM:server_key.pem -SSLCertificate PEM:server_cert.pem";
+// static Resource_Factory "-ORBProtocolFactory SSLIOP_Factory"
+
+const char argB[] = "-ORBSvcConf b.conf";
+
+// dynamic SSLIOP_Factory Service_Object * TAO_SSLIOP:_make_TAO_SSLIOP_Protocol_Factory() "-SSLAuthenticate SERVER_AND_CLIENT -SSLPrivateKey PEM:client_key.pem -SSLCertificate PEM:client_cert.pem"
+// static Resource_Factory "-ORBProtocolFactory SSLIOP_Factory"
+
+const char argM[] = "-ORBId ORB-M -ORBSvcConf m.conf";
+
+// dynamic UIPMC_Factory Service_Object * TAO_PortableGroup:_make_TAO_UIPMC_Protocol_Factory() ""
+// static Resource_Factory "-ORBProtocolFactory IIOP_Factory -ORBProtocolFactory UIPMC_Factory"
+// #static PortableGroup_Loader ""
+// dynamic PortableGroup_Loader Service_Object * TAO_PortableGroup:_make_TAO_PortableGroup_Loader() ""
 
 int
-testBug14559 (int , ACE_TCHAR *[])
+testBug1459 (int , ACE_TCHAR *[])
 {
-  ACE_TRACE ("testBug14559");
-  const char argA[] = "-ORBId ORB-A -ORBSvcConf a.conf";
-
-  //dynamic SSLIOP_Factory Service_Object * TAO_SSLIOP:_make_TAO_SSLIOP_Protocol_Factory() "-SSLAuthenticate SERVER_AND_CLIENT -SSLPrivateKey PEM:server_key.pem -SSLCertificate PEM:server_cert.pem";
-  //static Resource_Factory "-ORBProtocolFactory SSLIOP_Factory"
-
-  //dynamic SSLIOP_Factory Service_Object * TAO_SSLIOP:_make_TAO_SSLIOP_Protocol_Factory() "-SSLAuthenticate SERVER_AND_CLIENT -SSLPrivateKey PEM:client_key.pem -SSLCertificate PEM:client_cert.pem"
-  //static Resource_Factory "-ORBProtocolFactory SSLIOP_Factory"
-
-  const char argM[] = "-ORBId ORB-M -ORBSvcConf m.conf";
-
-//dynamic UIPMC_Factory Service_Object * TAO_PortableGroup:_make_TAO_UIPMC_Protocol_Factory() ""
-//static Resource_Factory "-ORBProtocolFactory IIOP_Factory -ORBProtocolFactory UIPMC_Factory"
-//#static PortableGroup_Loader ""
-//dynamic PortableGroup_Loader Service_Object * TAO_PortableGroup:_make_TAO_PortableGroup_Loader() ""
+  ACE_TRACE ("testBug1459");
 
   try
   {
 
-#ifdef SORB_AM
-    const char argAM[] = "-ORBSvcConf am.conf";
-    // combination of A followed by M (above)
-
-    ACE_ARGV arg0(argAM);
-    int n = arg0.argc();
-    CORBA::ORB_var ORBA = CORBA::ORB_init(n,arg0.argv());
-#endif /* SORB_AM */
-
-#ifdef SORB_MA
-    const char argMA[] = "-ORBSvcConf ma.conf";
-    // combination of M followed by A (above)
-
-    ACE_ARGV arg0(argMA);
-    int n = arg0.argc();
-    CORBA::ORB_var ORBA = CORBA::ORB_init(n,arg0.argv());
-#endif /* SORB_MA */
-
 #ifdef MORB_AB
-    const char argB[] = "-ORBSvcConf b.conf";
-
     ACE_ARGV arg0(argA);
     int n = arg0.argc();
     CORBA::ORB_var ORBA = CORBA::ORB_init(n,arg0.argv());
+
     ACE_ARGV arg1(argB);
     n = arg1.argc();
     CORBA::ORB_var ORBB = CORBA::ORB_init(n,arg1.argv());
 #else
     ACE_UNUSED_ARG (argA);
+    ACE_UNUSED_ARG (argB);
 #endif /* MORB_AB */
+
+
 
 #ifdef MORB_AM
     ACE_ARGV arg0(argA);
     int n = arg0.argc();
     CORBA::ORB_var ORBA = CORBA::ORB_init(n,arg0.argv());
+
     ACE_ARGV arg1(argM);
     n = arg1.argc();
     CORBA::ORB_var ORBB = CORBA::ORB_init(n,arg1.argv());
@@ -92,15 +81,36 @@ testBug14559 (int , ACE_TCHAR *[])
     ACE_UNUSED_ARG (argM);
 #endif /* MORB_AM */
 
+
+
 #ifdef MORB_MA
     int n = 0;
     ACE_ARGV arg0(argM);
     n = arg0.argc();
     CORBA::ORB_var ORBA = CORBA::ORB_init(n, arg0.argv());
+    ACE_ASSERT (ORBA.in () != 0);
 
     ACE_ARGV arg1(argA);
     n = arg1.argc();
     CORBA::ORB_var ORBB = CORBA::ORB_init(n, arg1.argv());
+    ACE_ASSERT (ORBB.in () != 0);
+
+    ACE_ASSERT (ORBA.in () != ORBB.in ());
+
+
+    // Look ma!! No ... services?!
+
+    ACE_Service_Object *so = 0;
+    so = ACE_Dynamic_Service<ACE_Service_Object>::instance ("SSLIOP_Factory");
+    ACE_ASSERT (so == 0);
+
+    so = ACE_Dynamic_Service<ACE_Service_Object>::instance ("UIPMC_Factory");
+    ACE_ASSERT (so == 0);
+
+    // Since each svc conf file causes the ORB to load the services in
+    // its own service space no services are reachable through the
+    // global service repo
+
 #else
     ACE_UNUSED_ARG (argM);
     ACE_UNUSED_ARG (argA);
@@ -124,5 +134,5 @@ testBug14559 (int , ACE_TCHAR *[])
 int
 run_main(int argc, ACE_TCHAR *argv[])
 {
-  return testBug14559(argc, argv);
+  return testBug1459(argc, argv);
 }
