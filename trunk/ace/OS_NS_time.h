263cc1a1c266df13975948a28d35a466ef625999
jwillemsen <jwillemsen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-05-16 13:20:20 +0000

    ChangeLogTag: Tue May 16 13:09:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ace/OS_NS_time.h
+++ trunk/ace/OS_NS_time.h
@@ -47,66 +47,6 @@ static const ACE_UINT32 ACE_U_ONE_SECOND_IN_MSECS = 1000U;
 static const ACE_UINT32 ACE_U_ONE_SECOND_IN_USECS = 1000000U;
 static const ACE_UINT32 ACE_U_ONE_SECOND_IN_NSECS = 1000000000U;
 
-#if defined (ACE_PSOS_HAS_TIME)
-
-// Use pSOS time, wrapped . . .
-class ACE_Export ACE_PSOS_Time_t
-{
-public:
-    /// default ctor: date, time, and ticks all zeroed.
-  ACE_PSOS_Time_t (void);
-
-    /// ctor from a timespec_t
-  ACE_PSOS_Time_t (const timespec_t& t);
-
-    /// type cast operator (to a timespec_t)
-  operator timespec_t ();
-
-    /// static member function to get current system time
-  static u_long get_system_time (ACE_PSOS_Time_t& t);
-
-    /// static member function to set current system time
-  static u_long set_system_time (const ACE_PSOS_Time_t& t);
-
-#   if defined (ACE_PSOSIM)
-    /// static member function to initialize system time, using UNIX calls
-  static u_long init_simulator_time (void);
-#   endif /* ACE_PSOSIM */
-
-    /// max number of ticks supported in a single system call
-  static const u_long max_ticks;
-private:
-  // = Constants for prying info out of the pSOS time encoding.
-  static const u_long year_mask;
-  static const u_long month_mask;
-  static const u_long day_mask;
-  static const u_long hour_mask;
-  static const u_long minute_mask;
-  static const u_long second_mask;
-  static const int year_shift;
-  static const int month_shift;
-  static const int hour_shift;
-  static const int minute_shift;
-  static const int year_origin;
-  static const int month_origin;
-
-  // error codes
-  static const u_long err_notime;   // system time not set
-  static const u_long err_illdate;  // date out of range
-  static const u_long err_illtime;  // time out of range
-  static const u_long err_illticks; // ticks out of range
-
-  /// date : year in bits 31-16, month in bits 15-8, day in bits 7-0
-   u_long date_;
-
-  /// time : hour in bits 31-16, minutes in bits 15-8, seconds in bits 7-0
-  u_long time_;
-
-  /// ticks: number of system clock ticks (KC_TICKS2SEC-1 max)
-  u_long ticks_;
-} ;
-#endif /* ACE_PSOS_HAS_TIME */
-
 #if defined (ACE_HAS_WINCE) && (defined (_MSC_VER) && (_MSC_VER < 1400))
 // WinCE prior to Visual Studio 2005 integration doesn't have most of
 // the standard C library time functions. It also doesn't define struct tm.
@@ -181,12 +121,7 @@ inline long ace_timezone()
  */
 inline double ace_difftime(time_t t1, time_t t0)
 {
-# if defined (ACE_PSOS) && ! defined (ACE_PSOS_HAS_TIME)
-  // simulate difftime ; just subtracting ; ACE_PSOS case
-  return ((double)t1) - ((double)t0);
-# else
   return difftime (t1, t0);
-# endif /* ACE_PSOS !ACE_PSOS_HAS_TIME */
 }
 #endif /* !ACE_LACKS_DIFFTIME */
 
@@ -202,11 +137,9 @@ inline DWORD ACE_HIGH_DWORD (ACE_QWORD q) { return (DWORD) (q >> 32); }
 // Can't find ANY place that ACE_QWORD is used, but hrtime_t is.
 typedef ACE_UINT64 ACE_hrtime_t;
 #   endif // ACE_LACKS_LONGLONG_T
-# elif defined (ACE_PSOS)
-typedef ACE_UINT64 ACE_hrtime_t;
 # elif defined (_TNS_R_TARGET)
 typedef long long ACE_hrtime_t;
-# else /* !ACE_WIN32 && !ACE_PSOS */
+# else /* !ACE_WIN32 */
 #   if defined (ACE_HAS_HI_RES_TIMER) &&  !defined (ACE_LACKS_LONGLONG_T)
   /* hrtime_t is defined on systems (Suns) with ACE_HAS_HI_RES_TIMER */
   typedef hrtime_t ACE_hrtime_t;
@@ -225,17 +158,6 @@ typedef long long ACE_hrtime_t;
 
 namespace ACE_OS
 {
-
-# if defined (CHORUS) && !defined (CHORUS_4)
-  // We must format this code as follows to avoid confusing OSE.
-  enum ACE_HRTimer_Op
-    {
-      ACE_HRTIMER_START = K_BSTART,
-      ACE_HRTIMER_INCR = K_BPOINT,
-      ACE_HRTIMER_STOP = K_BSTOP,
-      ACE_HRTIMER_GETTIME = 0xFFFF
-    };
-# else  /* ! CHORUS */
   enum ACE_HRTimer_Op
     {
       ACE_HRTIMER_START = 0x0,  // Only use these if you can stand
@@ -243,7 +165,6 @@ namespace ACE_OS
       ACE_HRTIMER_STOP = 0x2,   // the timed interval!!!!
       ACE_HRTIMER_GETTIME = 0xFFFF
     };
-# endif /* ! CHORUS */
 
   //@{ @name A set of wrappers for operations on time.
 
