28037b326a548b83c8350e82ae9cd2731b43fea4
nobody <nobody@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-05-18 11:33:07 +0000

    This commit was manufactured by cvs2svn to create branch 'taoarray'.

---
+++ branches/taoarray/TAO/CIAO/CCF/CCF/CIDL/SemanticAction/Impl/Elements.hpp
@@ -0,0 +1,27 @@
+// file      : CCF/CIDL/SemanticAction/Impl/Elements.hpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+#ifndef CCF_CIDL_SEMANTIC_ACTION_IMPL_ELEMENTS_HPP
+#define CCF_CIDL_SEMANTIC_ACTION_IMPL_ELEMENTS_HPP
+
+#include "CCF/CIDL/SemanticAction/Elements.hpp"
+#include "CCF/IDL3/SemanticAction/Impl/Elements.hpp"
+
+namespace CCF
+{
+  namespace CIDL
+  {
+    namespace SemanticAction
+    {
+      namespace Impl
+      {
+        using IDL3::SemanticAction::Impl::Context;
+        using IDL3::SemanticAction::Impl::Base;
+        using IDL3::SemanticAction::Impl::ScopeBase;
+      }
+    }
+  }
+}
+
+#endif  // CCF_CIDL_SEMANTIC_ACTION_IMPL_ELEMENTS_HPP
---
+++ branches/taoarray/TAO/tao/Services_Activate.cpp
@@ -0,0 +1,13 @@
+#include "tao/Services_Activate.h"
+
+ACE_RCSID (tao,
+           Services_Activate,
+           "$Id$")
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+TAO_Services_Activate::~TAO_Services_Activate (void)
+{
+}
+
+TAO_END_VERSIONED_NAMESPACE_DECL
