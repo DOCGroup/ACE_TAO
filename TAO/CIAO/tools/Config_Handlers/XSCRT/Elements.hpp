0e1bdae86775b6027f29a263124f9ea3b4614982
boris <boris@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-05-30 18:38:21 +0000

    ChangeLogTag: Tue May 30 10:08:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/CIAO/tools/Config_Handlers/XSCRT/Elements.hpp
+++ TAO/CIAO/tools/Config_Handlers/XSCRT/Elements.hpp
@@ -268,7 +268,8 @@ namespace XSCRT
   };
 
 #if ((defined (__GNUC__) && (__GNUC__ == 3 && (__GNUC_MINOR__ < 3))) || \
-    (defined (__BORLANDC__) && (__BORLANDC__ == 0x564)))
+    (defined (__BORLANDC__) && (__BORLANDC__ == 0x564)) || \
+    (defined (__SUNPRO_CC) && (__SUNPRO_CC <= 0x570)))
 
   // Stuff for broken gcc < 3.3. Don't like what you see - use better
   // compiler!
