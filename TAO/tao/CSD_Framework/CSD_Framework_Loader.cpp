02bf43f6b56ab9cbbbee6a5dae729e4f1dd4f856
Phil Mesnier <mesnier_p@ociweb.com>
2006-06-04 14:44:07 +0000

    ChangeLog tag: Sun Jun  4 14:39:56 UTC 2006  Phil Mesnier  <mesnier_p@ociweb.com>

--- TAO/tao/CSD_Framework/CSD_Framework_Loader.cpp
+++ TAO/tao/CSD_Framework/CSD_Framework_Loader.cpp
@@ -4,6 +4,7 @@
 #include "tao/CSD_Framework/CSD_Object_Adapter_Factory.h"
 #include "tao/CSD_Framework/CSD_Strategy_Repository.h"
 #include "ace/Dynamic_Service.h"
+#include "ace/Service_Gestalt.h"
 
 ACE_RCSID (CSD_Framework,
            CSD_Framework_Loader,
@@ -16,13 +17,20 @@ TAO_BEGIN_VERSIONED_NAMESPACE_DECL
 int
 TAO_CSD_Framework_Loader::init (void)
 {
+
+  // The ORB_Core's POA factory information is held in a static entity
+  // Therefore we need to force this initialization to occur in the
+  // global configuration context. Whenever the ORB Core's static resource
+  // problem is resolved, this should be removed.
+  ACE_Service_Gestalt * theone = ACE_Service_Config::global ();
+  ACE_Service_Config_Guard config_guard (theone);
+
   ACE_Service_Config::process_directive (ace_svc_desc_TAO_CSD_Object_Adapter_Factory);
 
-  TAO_ORB_Core::set_poa_factory ("TAO_CSD_Object_Adapter_Factory",
-                                 "dynamic TAO_CSD_Object_Adapter_Factory Service_Object * _make_TAO_CSD_Object_Adapter_Factory()");
-
-
   ACE_Service_Config::process_directive (ace_svc_desc_TAO_CSD_Strategy_Repository);
+
+  TAO_ORB_Core::set_poa_factory ("TAO_CSD_Object_Adapter_Factory",
+                                 "dynamic TAO_CSD_Object_Adapter_Factory Service_Object * TAO_CSD_Framework:_make_TAO_CSD_Object_Adapter_Factory()");
 
   return 0;
 }
