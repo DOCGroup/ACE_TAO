7d7e120bf2d512b3462cf9f67b7698702f34009d
Johnny Willemsen <jwillemsen@remedy.nl>
2006-06-12 11:06:34 +0000

    ChangeLogTag: Mon Jun 12 10:24:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ace/OS_NS_sys_stat.h
+++ ace/OS_NS_sys_stat.h
@@ -44,6 +44,8 @@ typedef struct __stat64 ACE_stat;
 #elif defined (ACE_WIN32) && !defined (ACE_HAS_WINCE) && !defined(__IBMCPP__) && ((defined _MSC_VER) && _MSC_VER >= 1300)
 // For vc71 which has time_t as 32bit
 typedef struct _stati64 ACE_stat;
+#elif defined (__MINGW32__)
+typedef struct _stat ACE_stat;
 #else
 typedef struct stat ACE_stat;
 #endif /* ACE_WIN32 */
