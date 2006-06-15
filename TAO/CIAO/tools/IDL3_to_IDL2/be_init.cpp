21bb9248b214af174407bb8334c2ab6fa6dc1a98
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-06-15 15:13:31 +0000

    ChangeLogTag: Thu Jun 15 15:10:58 UTC 2006  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/CIAO/tools/IDL3_to_IDL2/be_init.cpp
+++ TAO/CIAO/tools/IDL3_to_IDL2/be_init.cpp
@@ -16,12 +16,17 @@ BE_version (void)
 TAO_IDL3_TO_IDL2_BE_Export int
 BE_init (int & /* argc */, char * /* argv */ [])
 {
+  // Initialize BE global data object.
+  ACE_NEW_RETURN (be_global,
+                  BE_GlobalData,
+                  -1);
+
   idl_global->pass_orb_idl (true);
   return 0;
 }
 
 TAO_IDL3_TO_IDL2_BE_Export void
-BE_post_init (const char * /* files */ [], long /* nfiles */)
+BE_post_init (char * /* files */ [], long /* nfiles */)
 {
 }
 
