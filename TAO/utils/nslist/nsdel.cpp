18093a1922c2ee84629d43992609a6db6eabe21f
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-06-27 10:32:35 +0000

    ChangeLogTag: Tue Jun 27 10:25:00 UTC 2006  Simon Massey  <sma@prismtech.com>

--- TAO/utils/nslist/nsdel.cpp
+++ TAO/utils/nslist/nsdel.cpp
@@ -14,6 +14,7 @@
 // = AUTHOR
 //     Carlos O'Ryan <coryan@uci.edu>
 //     enhanced Jan 15, 2001 Paul Caffrey <denginere@hotmail.com>
+//     redone   Jun 21, 2006 Simon Massey <sma@prismtech.com>
 //
 // ================================================================
 
@@ -23,107 +24,161 @@
 #include "ace/OS_NS_string.h"
 #include "ace/Argv_Type_Converter.h"
 
-CORBA::ORB_var orb;
-int showIOR = 0;
-int showNSonly = 0;
-
-const char* USAGE = "Usage: %s [ --name <name> ] [--destroy]\n";
 int
 ACE_TMAIN (int argcw, ACE_TCHAR *argvw[])
 {
   ACE_DECLARE_NEW_CORBA_ENV;
+  CosNaming::Name the_name (0);
+  CORBA::ORB_var orb;
 
-  int i;
-  int destroy_after_unbind = 0;
   ACE_TRY
     {
+      // Contact the orb
       ACE_Argv_Type_Converter argcon (argcw, argvw);
-      CORBA::ORB_var orb =
-        CORBA::ORB_init (argcon.get_argc (), argcon.get_ASCII_argv (),
-                         "" ACE_ENV_ARG_PARAMETER);
+      orb = CORBA::ORB_init (argcon.get_argc (), argcon.get_ASCII_argv (),
+                             "" ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
-      int argc = argcon.get_argc ();
-      ACE_TCHAR** argv = argcon.get_TCHAR_argv ();
+      // Scan through the command line options
+      bool
+        failed = false,
+        quiet = false,
+        destroy = false;
+      int
+        argc = argcon.get_argc ();
+      ACE_TCHAR
+        **argv = argcon.get_TCHAR_argv (),
+      const char
+        *const pname = ACE_TEXT_ALWAYS_CHAR (argv[0]),
+        *nameService = 0;
+      char
+        kindsep = '.',
+        ctxsep[] = "/",
+        *name = 0;
 
-      ACE_TCHAR *pname = argv[0];
-      const ACE_TCHAR *name = 0;
-
-      while (argc > 0)
+      if (0 < argc)
         {
-          if (ACE_OS::strcmp (*argv, ACE_TEXT ("--name")) == 0)
+          while (0 < --argc)
             {
-              if (argc == 1)
+              ++argv;
+              if (0 == ACE_OS::strcmp (*argv, ACE_TEXT ("--ns")))
+                {
+                  if (!--argc)
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                 "Error: --ns requires an argument\n"));
+                      failed= true;
+                    }
+                  else
+                    {
+                      ++argv;
+                      if (nameService)
+                        {
+                          ACE_DEBUG ((LM_DEBUG,
+                                     "Error: more than one --ns.\n"));
+                          failed= true;
+                        }
+                      else
+                        nameService = ACE_TEXT_ALWAYS_CHAR (*argv);
+                    }
+                }
+              else if (0 == ACE_OS::strcmp (*argv, ACE_TEXT ("--quiet")))
+                {
+                  quiet = true;
+                }
+              else if (0 == ACE_OS::strcmp (*argv, ACE_TEXT ("--name")))
+                {
+                  if (name)
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                 "Error: more than one --name\n"));
+                      failed = true;
+                    }
+                  else if (!--argc)
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                  "Error: --name requires an argument\n"));
+                      failed = true;
+                    }
+                  else
+                    name = ACE_TEXT_ALWAYS_CHAR (*++argv);
+                }
+              else if (0 == ACE_OS::strcmp (*argv, ACE_TEXT ("--ctxsep")))
+                {
+                  if (!--argc)
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                  "Error: --ctxsep requires a character\n"));
+                      failed = true;
+                    }
+                  else if (1 != ACE_OS::strlen(*++argv))
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                 "Error: --ctxsep takes a single character (not %s)\n", *argv));
+                      failed = true;
+                    }
+                  else
+                    ctxsep[0] = ACE_TEXT_ALWAYS_CHAR (*argv)[0];
+                }
+              else if (0 == ACE_OS::strcmp (*argv, ACE_TEXT ("--kindsep")))
+                {
+                  if (!--argc)
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                  "Error: --kindsep requires a character\n"));
+                      failed = true;
+                    }
+                  else if (1 != ACE_OS::strlen(*++argv))
+                    {
+                      ACE_DEBUG ((LM_DEBUG,
+                                 "Error: --kindsep takes a single character (not %s)\n", *argv));
+                      failed = true;
+                    }
+                  else
+                    kindsep = ACE_TEXT_ALWAYS_CHAR (*argv)[0];
+                }
+              else if (0 == ACE_OS::strcmp (*argv, ACE_TEXT ("--destroy")))
+                {
+                  destroy = true;
+                }
+              else
                 {
                   ACE_DEBUG ((LM_DEBUG,
-                              "Error: --name requires an argument\n"));
-                  return 1;
+                             "Unknown option %s\n", *argv));
+                  failed = true;
                 }
-              argc--;
-              argv++;
-              name = *argv;
             }
-          else if (ACE_OS::strcmp (*argv, ACE_TEXT ("--destroy")) == 0)
-            {
-              destroy_after_unbind = 1;
-            }
-          else if (ACE_OS::strncmp(*argv, ACE_TEXT ("--"), 2) == 0)
-            {
-              ACE_DEBUG ((LM_DEBUG, USAGE, pname));
-              return 1;
-            }
-          argc--;
-          argv++;
         }
 
-      if (name == 0)
+      if (!name || failed)
         {
-          ACE_DEBUG ((LM_DEBUG, USAGE, pname));
+          ACE_DEBUG ((LM_DEBUG,
+                      "\nUsage:\n  %s --name <name>\n"
+                      "optional:\n"
+                      "  --ns <ior>\n"
+                      "  --ctxsep  <character>\n"
+                      "  --kindsep <character>\n"
+                      "  --destroy\n"
+                      "  --quiet\n\n"
+                      "where <name> uses the --ctxsep character (defaults to /)\n"
+                      "to separate sub-contexts and --kindsep (defaults to .)\n"
+                      "to separate ID & Kind. Will destroy a naming context\n"
+                      "before unbinding if --destroy is given, otherwise it\n"
+                      "will orphan them. Connects to default NameService\n"
+                      "unless --ns is given. Displays all ID/Kinds found\n"
+                      "on path unless --quiet is given.\n",
+                      pname));
+          orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
           return 1;
         }
-        // make a copy
 
-        char buf[BUFSIZ];
-        ACE_OS::strcpy (buf, ACE_TEXT_ALWAYS_CHAR (name));
-        char *bp = &buf[0];
-        char *cp = 0;
-        int ntoks = 0;
-        char *toks[20];
-
-        while ((cp = ACE_OS::strtok (bp, "/")) != 0)
-          {
-            toks[ntoks] = cp;
-            ntoks++;
-            if (cp == NULL)
-              {
-                printf ("break, null token\n");
-                break;
-              }
-            bp = 0; // way strtok works
-          }
-
-        for (i=0; i<ntoks; i++)
-          ACE_OS::printf ("%s\n", toks[i]);
-
-        // now assign name = toks[ntoks]
-        char lastname[BUFSIZ];
-        ACE_OS::strcpy (lastname, toks[ntoks - 1]);
-
-        // search for '.' in name; if exists then the part after '.' is the kind
-        char *kind = ACE_OS::strchr (lastname, '.');
-
-        if (kind != 0)
-          {
-            *kind = 0;
-            kind++;
-            ACE_OS::printf ("name='%s'\n", lastname);
-            ACE_OS::printf ( "kind='%s'\n" , kind );
-          }
-        else
-          ACE_OS::printf ("name=%s\n", lastname );
-
-      CORBA::Object_var nc_obj =
-        orb->resolve_initial_references ("NameService" ACE_ENV_ARG_PARAMETER);
+      // Contact the name service
+      CORBA::Object_var nc_obj;
+      if (nameService)
+        nc_obj = orb->string_to_object (nameService ACE_ENV_ARG_PARAMETER);
+      else
+        nc_obj = orb->resolve_initial_references ("NameService" ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
       CosNaming::NamingContext_var root_nc =
@@ -134,47 +189,129 @@ ACE_TMAIN (int argcw, ACE_TCHAR *argvw[])
         {
           ACE_DEBUG ((LM_DEBUG,
                       "Error: nil naming context\n"));
+          orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+          ACE_TRY_CHECK;
           return 1;
         }
 
-      CosNaming::Name the_name (ntoks);
-      the_name.length (ntoks);
-      for (i=0; i<(ntoks - 1); i++) // up to last directory entry
-        the_name[i].id = CORBA::string_dup (toks[i]);
-
-      the_name[ntoks - 1].id = CORBA::string_dup (lastname);
-      if (kind != 0)
-        the_name[ntoks - 1].kind = CORBA::string_dup (kind);
-
-      // This needs to be bounded by a try/catch
-      CORBA::Object_var the_context = root_nc->resolve (the_name ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
-
-      root_nc->unbind (the_name ACE_ENV_ARG_PARAMETER );
-      ACE_TRY_CHECK;
-
-      if (destroy_after_unbind)
+      // Assemble the name from the user string given
+      char *cp;
+      while (0 != (cp = ACE_OS::strtok (name, ctxsep)))
         {
-          CosNaming::NamingContext_var nc =
-            CosNaming::NamingContext::_narrow (the_context.in () ACE_ENV_ARG_PARAMETER);
-          ACE_TRY_CHECK;
-
-          if (! CORBA::is_nil(nc.in()))
+          const int index= the_name.length();
+          the_name.length (index+1);
+          char *kind = (char *)ACE_OS::strchr (cp, kindsep);
+          if (kind)
             {
-              nc->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
-              ACE_TRY_CHECK;
+              *kind = '\0';
+              the_name[index].kind= CORBA::string_dup (++kind);
             }
+          the_name[index].id = CORBA::string_dup (cp);
+          name = 0; // way strtok works
         }
 
+      // Attempt to locate the object and destroy/unbind it
+      CORBA::Object_var
+        obj = root_nc->resolve( the_name ACE_ENV_ARG_PARAMETER );
       ACE_TRY_CHECK;
+      CosNaming::NamingContext_var this_nc =
+        CosNaming::NamingContext::_narrow (obj.in () ACE_ENV_ARG_PARAMETER);
+      ACE_TRY_CHECK;
+      if (!CORBA::is_nil (this_nc.in ()))
+        {
+          if (destroy)
+            {
+              if (!quiet)
+                ACE_DEBUG ((LM_DEBUG,"Destroying\n"));
+              this_nc->destroy( ACE_ENV_SINGLE_ARG_PARAMETER );
+              ACE_TRY_CHECK;
+            }
+          else if (!quiet)
+            {
+              CORBA::String_var str =
+                orb->object_to_string (obj.in () ACE_ENV_ARG_PARAMETER);
+              ACE_TRY_CHECK;
+              ACE_DEBUG ((LM_DEBUG,
+                         "\n*** Possiably Orphaned Naming Context ***\n%s\n\n", str.in()));
+            }
+        }
+      else if (destroy && !quiet)
+        ACE_DEBUG ((LM_DEBUG,"Can't Destroy object, it is not a naming context!\n"));
+      root_nc->unbind (the_name ACE_ENV_ARG_PARAMETER );
+      ACE_TRY_CHECK;
+      if (!quiet)
+        {
+          unsigned int index;
+          for (index= 0u; index < the_name.length()-1u; ++index)
+            {
+              if (the_name[index].kind && the_name[index].kind[0])
+                ACE_DEBUG ((LM_DEBUG, "Found ID: %s  (Kind: %s)\n",
+                           the_name[index].id.in(),
+                           the_name[index].kind.in()));
+              else
+                ACE_DEBUG ((LM_DEBUG, "Found ID: %s\n",
+                           the_name[index].id.in()));
+            }
+          ACE_DEBUG ((LM_DEBUG, "UnBound ID: %s",
+                     the_name[index].id.in()));
+          if (the_name[index].kind && the_name[index].kind[0])
+            ACE_DEBUG ((LM_DEBUG, "  (Kind: %s)\n",
+                       the_name[index].kind.in()));
+          ACE_DEBUG ((LM_DEBUG, "\n"));
+        }
+    }
+  ACE_CATCH (CosNaming::NamingContext::NotFound, nf)
+    {
+      unsigned int index;
+      const unsigned int limit= the_name.length()-nf.rest_of_name.length();
+      ACE_DEBUG ((LM_DEBUG, "\nError:\n"));
+      for (index= 0u; index < limit; ++index)
+        {
+          if (the_name[index].kind && the_name[index].kind[0])
+             ACE_DEBUG ((LM_DEBUG, "ID: %s  (Kind: %s)\n",
+               the_name[index].id.in(),
+               the_name[index].kind.in()));
+          else
+             ACE_DEBUG ((LM_DEBUG, "ID: %s\n",
+               the_name[index].id.in()));
+        }
+      const char *why= "Unknown reason";
+      switch (nf.why)
+        {
+        case CosNaming::NamingContext::missing_node:
+          why= "\nThe following node is missing";
+          break;
+        case CosNaming::NamingContext::not_context:
+          why= "\nThe following is a final object binding, not a naming context";
+          break;
+        case CosNaming::NamingContext::not_object:
+          why= "\nThe following is a naming context, not a final object binding";
+          break;
+        }
+      ACE_PRINT_EXCEPTION (nf, why );
+      for (index= 0u; index < nf.rest_of_name.length(); ++index)
+        {
+          if (nf.rest_of_name[index].kind && nf.rest_of_name[index].kind[0])
+             ACE_DEBUG ((LM_DEBUG, "ID: %s  (Kind: %s)\n",
+               nf.rest_of_name[index].id.in(),
+               nf.rest_of_name[index].kind.in()));
+          else
+             ACE_DEBUG ((LM_DEBUG, "ID: %s\n",
+               nf.rest_of_name[index].id.in()));
+        }
+      orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
+      return 1;
     }
   ACE_CATCHANY
     {
+      ACE_DEBUG ((LM_DEBUG, "\nError:\n"));
       ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
                            "Exception in nsdel");
+      orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
       return 1;
     }
   ACE_ENDTRY;
 
+  orb->destroy (ACE_ENV_SINGLE_ARG_PARAMETER);
   return 0;
 }
