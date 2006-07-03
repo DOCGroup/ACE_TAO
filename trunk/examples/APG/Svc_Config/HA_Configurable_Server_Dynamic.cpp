327e5dc07c626295c3d5a5f31fef8cfb5a7346e5
schmidt <schmidt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-07-03 14:22:05 +0000

    ChangeLogTag:Mon

--- trunk/examples/APG/Svc_Config/HA_Configurable_Server_Dynamic.cpp
+++ trunk/examples/APG/Svc_Config/HA_Configurable_Server_Dynamic.cpp
@@ -8,7 +8,10 @@
 int ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
   ACE_Service_Config::open (argc, argv);
-  ACE_Reactor::instance ()->run_reactor_event_loop ();
+
+  // We use this version of the event loop so that reconfigurations
+  // are triggered properly.
+  ACE_Reactor::instance ()->run_reactor_event_loop (ACE_Reactor::check_reconfiguration);
   return 0;
 }
 // Listing 1
