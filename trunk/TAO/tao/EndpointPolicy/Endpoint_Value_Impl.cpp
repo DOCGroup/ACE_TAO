7c055159d3e829545078a0b7c3ba4d09b20f29fc
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-07-13 17:29:27 +0000

    ChangeLogTag:Thu Jul 13 10:26:42 UTC 2006  Ossama Othman  <ossama_othman at symantec dot com>

--- trunk/TAO/tao/EndpointPolicy/Endpoint_Value_Impl.cpp
+++ trunk/TAO/tao/EndpointPolicy/Endpoint_Value_Impl.cpp
@@ -11,6 +11,10 @@
 
 #include "tao/EndpointPolicy/Endpoint_Value_Impl.h"
 
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
 TAO_Endpoint_Value_Impl::~TAO_Endpoint_Value_Impl()
 {
 }
+
+TAO_END_VERSIONED_NAMESPACE_DECL
