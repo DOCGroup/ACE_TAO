0c2002202848a11b0cc9cbce2dde27ba281ad47a
mitza <mitza@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-07-14 15:34:25 +0000

    Fri Jul 14 15:11:11 UTC 2006  Adam Mitz  <mitza@ociweb.com>

--- TAO/orbsvcs/orbsvcs/CosEvent/CEC_EventChannel.cpp
+++ TAO/orbsvcs/orbsvcs/CosEvent/CEC_EventChannel.cpp
@@ -232,7 +232,7 @@ TAO_CEC_EventChannel::destroy (ACE_ENV_SINGLE_ARG_DECL)
   this->shutdown (ACE_ENV_SINGLE_ARG_PARAMETER);
 }
 
-CORBA::Policy_ptr 
+CORBA::Policy_ptr
 TAO_CEC_EventChannel::create_roundtrip_timeout_policy
   (ACE_Time_Value timeout)
 {
