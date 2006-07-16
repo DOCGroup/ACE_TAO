ffafaebfa0035ce1dd749c951736c8b71b49df21
john_c <john_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-07-16 23:21:31 +0000

    Sun Jul 16 23:14:53 UTC 2006  Ciju John  <johnc@ociweb.com>
            * ace/String_Base_Const.cpp:
    
              Took out an extra sem-colon. It seems to make the
              FC4_Versioned_Namespace build grumpy.

--- ace/String_Base_Const.cpp
+++ ace/String_Base_Const.cpp
@@ -6,6 +6,6 @@
 ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 
 ACE_String_Base_Const::size_type const ACE_String_Base_Const::npos =
-  ACE_Numeric_Limits<ACE_String_Base_Const::size_type>::max ();;
+  ACE_Numeric_Limits<ACE_String_Base_Const::size_type>::max ();
 
 ACE_END_VERSIONED_NAMESPACE_DECL
