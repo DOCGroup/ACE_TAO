ec0e503b2f3b8e0d3edf39f4f9d4330ee16d6391
john_c <john_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-07-19 22:33:01 +0000

    Wed Jul 19 22:28:50 UTC 2006  Ciju John  <johnc@ociweb.com>
            * orbsvcs/orbsvcs/Event/ECG_Mcast_EH.cpp:
    
    	  The open() initializes the underlying socket that is later
              enabled to be non-blocking. Without open() the enable call will
              fail as it should. This fixes the scoreboard failure introduced
              in:
    
              Mon Jul 17 14:26:08 UTC 2006  Ciju John  <john_c@ociweb.com>

--- trunk/TAO/orbsvcs/orbsvcs/Event/ECG_Mcast_EH.cpp
+++ trunk/TAO/orbsvcs/orbsvcs/Event/ECG_Mcast_EH.cpp
@@ -230,13 +230,24 @@ TAO_ECG_Mcast_EH::add_new_subscriptions (Address_Set& multicast_addresses)
       this->subscriptions_[subscriptions_size] = new_subscription;
 
       ACE_SOCK_Dgram_Mcast *socket = new_subscription.dgram;
+
+      if (socket->open (new_subscription.mcast_addr, this->net_if_, 1) == -1) {
+        ACE_ERROR ((LM_ERROR,
+                    "Error: %d - Unable to open multicast socket\n",
+                    errno ));
+      }
+
       if ( socket->enable (ACE_NONBLOCK) != 0 ) {
         ACE_ERROR ((LM_ERROR,
                     "Error: %d - Unable to enable nonblocking on mcast_eh\n",
                     errno ));
       }
 
-      socket->join (new_subscription.mcast_addr, 1, this->net_if_);
+      if (socket->join (new_subscription.mcast_addr, 1, this->net_if_) == -1) {
+        ACE_ERROR ((LM_ERROR,
+                    "Error: %d - Unable to join multicast group\n",
+                    errno ));
+      }
 
       if (this->recvbuf_size_ != 0
           && (((ACE_SOCK_Dgram *)socket)->set_option(SOL_SOCKET,
