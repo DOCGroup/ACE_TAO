880ca41f037c85a08f302d226aac38671ef03594
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-21 14:12:57 +0000

    ChangeLogTag: Fri Jul 21 13:28:31 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>Fri Jul 21 14:09:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/RTScheduling/Thread_Cancel/Thread_Task.cpp
+++ TAO/tests/RTScheduling/Thread_Cancel/Thread_Task.cpp
@@ -113,7 +113,7 @@ Thread_Task::svc (void)
           {
             ACE_DEBUG ((LM_DEBUG,
                         "Scheduling Segment Name - %s\n",
-                        static_cast<char const*>((*name_list) [i])));
+                        (*name_list) [i].in ()));
           }
       }
 
