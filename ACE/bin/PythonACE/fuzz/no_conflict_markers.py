ec712e908cf3f87699173339c6b55e617f18f80a
William R. Otte <wotte@dre.vanderbilt.edu>
2006-07-25 19:42:30 +0000

    Tue Jul 25 19:40:54 UTC 2006  William R. Otte  <wotte@dre.vanderbilt.edu>

--- ACE/bin/PythonACE/fuzz/no_conflict_markers.py
+++ ACE/bin/PythonACE/fuzz/no_conflict_markers.py
@@ -6,7 +6,7 @@ type_list = ["*"]
 from sys import stderr
 import re
 
-regex = re.compile ("^<<<<|^>>>>|^====", re.MULTILINE)
+regex = re.compile ("^<<<<<<< \.|>>>>>>> \.r|^=======$", re.MULTILINE)
 begin_exclude = re.compile ("FUZZ\: disable conflict_marker_check")
 end_exclude = re.compile ("FUZZ\: enable conflict_marker_check")
 
