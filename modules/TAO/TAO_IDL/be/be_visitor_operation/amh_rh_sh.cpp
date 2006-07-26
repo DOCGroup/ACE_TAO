f81e60b7422223d0b7064c5a5b3a2d242e9eb70e
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-26 13:22:29 +0000

    branching/tagging

---
+++ modules/TAO/TAO_IDL/be/be_visitor_operation/amh_rh_sh.cpp
@@ -0,0 +1,106 @@
+//
+// $Id$
+//
+//
+// ============================================================================
+//
+// = LIBRARY
+//    TAO IDL
+//
+// = FILENAME
+//    amh_rh_sh.cpp
+//
+// = DESCRIPTION
+//    Visitor generating AMH-RH skeleton code for Operation node in the
+//    skeleton header.
+//
+// = AUTHOR
+//    Mayur Deshpande <mayur@ics.uci.edu>
+//
+// ============================================================================
+
+ACE_RCSID (be_visitor_operation, 
+           operation_amh_rh_sh, 
+           "$Id$")
+
+// ******************************************************
+// Visitor for generating AMH-RH skeleton for "operation"
+// in skeleton header.
+// ******************************************************
+
+be_visitor_amh_rh_operation_sh::be_visitor_amh_rh_operation_sh (
+    be_visitor_context *ctx
+  )
+  : be_visitor_operation (ctx)
+{
+}
+
+be_visitor_amh_rh_operation_sh::~be_visitor_amh_rh_operation_sh (void)
+{
+}
+
+int
+be_visitor_amh_rh_operation_sh::visit_operation (be_operation *node)
+{
+  // Nothing to be done for oneway operations.
+  if (node->flags () == AST_Operation::OP_oneway)
+    {
+      return 0;
+    }
+
+  // Output stream.
+  TAO_OutStream *os = this->ctx_->stream ();
+  this->ctx_->node (node);
+
+  be_interface *intf;
+  intf = this->ctx_->attribute ()
+    ? be_interface::narrow_from_scope (this->ctx_->attribute()->defined_in ())
+    : be_interface::narrow_from_scope (node->defined_in ());
+
+  if (!intf)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "(%N:%l) be_visitor_amh_rh_operation_sh::"
+                         "visit_operation - "
+                         "bad interface scope\n"),
+                        -1);
+    }
+
+  *os << be_nl << be_nl << "// TAO_IDL - Generated from" << be_nl
+      << "// " << __FILE__ << ":" << __LINE__ << be_nl << be_nl;
+
+  *os << "virtual void ";
+
+  // Step 2: Generate the method name
+  // Check if we are an attribute node in disguise.
+  if (this->ctx_->attribute ())
+    {
+      // Now check if we are a "get" or "set" operation.
+      if (node->nmembers () == 1)
+        {
+          *os << "set_";
+        }
+      else
+        {
+          *os << "get_";
+        }
+    }
+
+  *os << node->local_name();
+
+  be_visitor_context ctx (*this->ctx_);
+  be_visitor_operation_arglist visitor (&ctx);
+
+  if (node->accept (&visitor) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "(%N:%l) be_visitor_operation_amh_rh_sh::"
+                         "visit_operation - "
+                         "codegen for argument list failed\n"),
+                        -1);
+    }
+
+  *os << ";";
+
+  return 0;
+}
---
+++ modules/TAO/tao/LF_Multi_Event.cpp
@@ -0,0 +1,123 @@
+#include "tao/LF_Multi_Event.h"
+#include "tao/Connection_Handler.h"
+#include "ace/OS_Memory.h"
+
+ACE_RCSID(tao,
+          LF_Multi_Event,
+          "$Id$")
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+
+TAO_LF_Multi_Event::TAO_LF_Multi_Event (void)
+  : TAO_LF_Event (),
+    events_ (0),
+    winner_ (0)
+{
+}
+
+TAO_LF_Multi_Event::~TAO_LF_Multi_Event (void)
+{
+  while (this->events_ != 0)
+    {
+      Event_Node *n = this->events_->next_;
+      delete this->events_;
+      this->events_ = n;
+    }
+}
+
+int
+TAO_LF_Multi_Event::bind (TAO_LF_Follower *follower)
+{
+  if (this->TAO_LF_Event::bind(follower) == -1)
+    {
+      return -1;
+    }
+
+  for (Event_Node *n = this->events_; n != 0; n = n->next_)
+    if (n->ptr_->bind(follower) == -1)
+      {
+        return -1;
+      }
+  return 0;
+}
+
+int
+TAO_LF_Multi_Event::unbind (void)
+{
+  if (this->TAO_LF_Event::unbind() == -1)
+    {
+      return -1;
+    }
+
+  for (Event_Node *n = this->events_; n != 0; n = n->next_)
+    if (n->ptr_->unbind() == -1)
+      {
+        return -1;
+      }
+  return 0;
+}
+
+
+void
+TAO_LF_Multi_Event::add_event (TAO_Connection_Handler *ev)
+{
+  Event_Node *node = 0;
+  ACE_NEW (node, Event_Node);
+  node->next_ = this->events_;
+  node->ptr_ = ev;
+
+  this->events_ = node;
+}
+
+TAO_Connection_Handler*
+TAO_LF_Multi_Event::winner (void)
+{
+  return this->winner_;
+}
+
+TAO_Transport *
+TAO_LF_Multi_Event::base_transport (void)
+{
+  return (this->events_ == 0) ? 0 : this->events_->ptr_->transport();
+}
+
+int
+TAO_LF_Multi_Event::successful (void) const
+{
+  for (Event_Node *n = this->events_; n != 0; n = n->next_)
+    if (n->ptr_->successful() == 1)
+      {
+        this->winner_ = n->ptr_;
+        return 1;
+      }
+  return 0;
+}
+
+int
+TAO_LF_Multi_Event::error_detected (void) const
+{
+  int result = 1;
+  for (Event_Node *n = this->events_; n != 0; n = n->next_)
+    if (n->ptr_->error_detected () == 0)
+      result = 0;
+  return result;
+}
+
+void
+TAO_LF_Multi_Event::state_changed_i (int )
+{
+  // no-op
+}
+
+int
+TAO_LF_Multi_Event::is_state_final (void)
+{
+  int result = 1;
+  for (Event_Node *n = this->events_; n != 0; n = n->next_)
+    if (!n->ptr_->is_state_final () == 0)
+      result = 0;
+  return result;
+}
+
+TAO_END_VERSIONED_NAMESPACE_DECL
