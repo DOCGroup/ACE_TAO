ccc2dc1a72d1b697d99d1acab190405e44fefbc6
William R. Otte <wotte@dre.vanderbilt.edu>
2006-07-26 14:47:41 +0000

    branching/tagging

---
+++ modules/TAO/orbsvcs/orbsvcs/PortableGroup/PG_MemberInfo.h
@@ -0,0 +1,62 @@
+// -*- C++ -*-
+
+//=======================================================================
+/**
+ *  @file    PG_MemberInfo.h
+ *
+ *  $Id$
+ *
+ *  @author  Ossama Othman <ossama@uci.edu>
+ */
+//=======================================================================
+
+
+#ifndef TAO_PG_MEMBER_INFO_H
+#define TAO_PG_MEMBER_INFO_H
+
+#include /**/ "ace/pre.h"
+
+#include "ace/config-all.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "orbsvcs/PortableGroupC.h"
+#include "ace/Unbounded_Set.h"
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+/**
+ * @class TAO_PG_MemberInfo
+ *
+ * @brief Structure that contains all member-specific information.
+ *
+ * Structure that contains all member-specific information.
+ */
+struct TAO_PG_MemberInfo
+{
+
+  /// Reference to the member.
+  CORBA::Object_var member;
+
+  /// The location at which the member resides.
+  PortableGroup::Location location;
+
+  /// Equality operator.
+  /**
+   * For the purposes of the member info set, only the location is
+   * important.  In particular, if a member already resides at the
+   * given location, this equality operator will return true.
+   */
+  bool operator== (const TAO_PG_MemberInfo & rhs);
+
+};
+
+typedef ACE_Unbounded_Set<TAO_PG_MemberInfo> TAO_PG_MemberInfo_Set;
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+#include /**/ "ace/post.h"
+
+#endif  /* TAO_PG_MEMBER_INFO_H */
---
+++ modules/TAO/tao/PolicyFactory_Registry_Factory.cpp
@@ -0,0 +1,15 @@
+//$Id$
+
+#include "tao/PolicyFactory_Registry_Factory.h"
+
+ACE_RCSID (tao,
+           PolicyFactory_Registry_Factory,
+           "$Id$")
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+TAO_PolicyFactory_Registry_Factory::~TAO_PolicyFactory_Registry_Factory (void)
+{
+}
+
+TAO_END_VERSIONED_NAMESPACE_DECL
