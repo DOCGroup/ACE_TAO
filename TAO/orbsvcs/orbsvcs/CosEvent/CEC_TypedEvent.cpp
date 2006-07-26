6c4bd3d5ed7a00038eb189c0a36ba932b834bc47
Johnny Willemsen <jwillemsen@remedy.nl>
2006-07-26 13:22:01 +0000

    branching/tagging

--- TAO/orbsvcs/orbsvcs/CosEvent/CEC_TypedEvent.cpp
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-//
-// $Id$
-
-#include "orbsvcs/CosEvent/CEC_TypedEvent.h"
-
-#if ! defined (__ACE_INLINE__)
-#include "orbsvcs/CosEvent/CEC_TypedEvent.i"
-#endif /* __ACE_INLINE__ */
