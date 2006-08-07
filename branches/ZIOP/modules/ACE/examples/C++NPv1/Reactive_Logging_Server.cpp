09d928dcca1fe1923fadfb0e7f19b8ff48183caf
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-07 08:24:35 +0000

    branching/tagging

---
+++ branches/ZIOP/modules/ACE/examples/C++NPv1/Reactive_Logging_Server.cpp
@@ -0,0 +1,17 @@
+/*
+** $Id$
+**
+** Copyright 2001 Addison Wesley. All Rights Reserved.
+*/
+
+#include "ace/Log_Msg.h"
+#include "Reactive_Logging_Server.h"
+
+int main (int argc, char *argv[])
+{
+  Reactive_Logging_Server server;
+
+  if (server.run (argc, argv) == -1)
+    ACE_ERROR_RETURN ((LM_ERROR, "%p\n", "server.run()"), 1);
+  return 0;
+}
