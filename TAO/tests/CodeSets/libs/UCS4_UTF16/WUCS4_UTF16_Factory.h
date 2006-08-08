81187827a80cd2c74eb48fd04d7e201ed79e0fb5
Johnny Willemsen <jwillemsen@remedy.nl>
2006-08-08 14:14:12 +0000

    Tue Aug  8 14:13:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/CodeSets/libs/UCS4_UTF16/WUCS4_UTF16_Factory.h
+++ TAO/tests/CodeSets/libs/UCS4_UTF16/WUCS4_UTF16_Factory.h
@@ -16,7 +16,7 @@
 
 #include /**/ "ace/pre.h"
 #include "ace/Service_Config.h"
-#include "tao/Codeset_Translator_Factory.h"
+#include "tao/Codeset/Codeset_Translator_Factory.h"
 
 #include "WUCS4_UTF16_export.h"
 #include "WUCS4_UTF16.h"
