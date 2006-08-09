2dcc467358a7f87e744b3bcaed5f7b5b04d8bca3
Johnny Willemsen <jwillemsen@remedy.nl>
2006-08-09 13:17:11 +0000

    Wed Aug  9 13:16:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/Bug_2503_Regression/run_test.pl
+++ TAO/tests/Bug_2503_Regression/run_test.pl
@@ -19,8 +19,8 @@ if (PerlACE::is_vxworks_test()) {
 else {
     $SV = new PerlACE::Process ("server", "-o $iorfile");
 }
-$CL = new PerlACE::Process ("client", " -k file://$iorfile -ORBDebugLevel 10");
-    
+$CL = new PerlACE::Process ("client", " -k file://$iorfile");
+
 $server = $SV->Spawn ();
 
 if ($server != 0) {
@@ -33,7 +33,7 @@ if (PerlACE::waitforfile_timed ($iorfile,
     print STDERR "ERROR: cannot find file <$iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
-} 
+}
 
 $client = $CL->SpawnWaitKill (300);
 
