76b216a35a2140a6fbdfd39e5742b2845a121383
mitza <mitza@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-10 18:02:05 +0000

    fix for the stream corruption problem

--- branches/OCI_Oneway_Buffering/TAO/tests/Timed_Buffered_Oneways/run_test.pl
+++ branches/OCI_Oneway_Buffering/TAO/tests/Timed_Buffered_Oneways/run_test.pl
@@ -17,13 +17,9 @@ if (PerlACE::is_vxworks_test()) {
     $SV = new PerlACE::ProcessVX ("server", "-o server.ior");
 }
 else {
-    $SV = new PerlACE::Process ("server", "-o $iorfile"
-." -ORBDebugLevel 6 -ORBLogFile server.log"
-);
+    $SV = new PerlACE::Process ("server", "-o $iorfile");
 }
-$CL = new PerlACE::Process ("client", "-k file://$iorfile -x"
-." -ORBDebugLevel 6 -ORBLogFile client.log"
-);
+$CL = new PerlACE::Process ("client", "-k file://$iorfile -x");
 
 $SV->Spawn ();
 
