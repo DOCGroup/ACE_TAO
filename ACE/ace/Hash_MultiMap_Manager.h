95de8bac3eb3e55389d1979ec7fcfb201f402bdb
sjiang <sjiang@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-11 17:36:55 +0000

    ChangeLog Tag: Fri Aug 11 16:46:56 UTC 2006  Shanshan Jiang <shanshan.jiang@vanderbilt.edu>

---
+++ ACE/ace/Hash_MultiMap_Manager.h
@@ -0,0 +1,31 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    Hash_MultiMap_Manager.h
+ *
+ *  $Id$
+ *
+ *  Backward compatibility header.
+ *
+ *  @author Shanshan Jiang <shanshan.jiang@vanderbilt.edu>
+ */
+//=============================================================================
+
+#ifndef ACE_HASH_MULTIMAP_MANAGER_H
+#define ACE_HASH_MULTIMAP_MANAGER_H
+
+#include /**/ "ace/pre.h"
+
+#include "ace/config-all.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+// Include the templates here.
+#include "ace/Hash_MultiMap_Manager_T.h"
+
+#include /**/ "ace/post.h"
+
+#endif /* ACE_HASH_MULTIMAP_MANAGER_H */
