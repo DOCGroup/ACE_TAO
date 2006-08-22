7f72573113a36418e3bc1bce54ea85d74d8a1972
johnc <johnc@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-22 19:54:48 +0000

    Tue Aug 22 19:39:07 UTC 2006  Ciju John  <john@ociweb.com>

--- TAO/tao/Wait_On_Read.cpp
+++ TAO/tao/Wait_On_Read.cpp
@@ -103,8 +103,10 @@ TAO_Wait_On_Read::wait (ACE_Time_Value * max_wait_time,
                         ACE_TEXT ("registration with reactor returned an error \n"),
                          this->transport_->id ()));
         }
-
-       this->is_registered_ = true;
+       else {
+   // Only set this flag when registration succeeds
+   this->is_registered_ = true;
+       }
 
        return 0;
      }
