874f60b7c501ce184e60a188b41a860c5785166b
dai_y <dai_y@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-23 17:49:53 +0000

    Wed Aug 23 17:43:29 UTC 2006  Yan Dai  <dai_y@ociweb.com>

--- trunk/TAO/tao/PortableServer/DII_Arguments_Converter.cpp
+++ trunk/TAO/tao/PortableServer/DII_Arguments_Converter.cpp
@@ -1,7 +1,6 @@
 // @(#) $Id$
 
 #include "tao/PortableServer/DII_Arguments_Converter.h"
-#include "ace/Service_Config.h"
 
 ACE_RCSID(PortableServer,
           DII_Arguments_Converter,
@@ -10,39 +9,10 @@ ACE_RCSID(PortableServer,
 
 TAO_BEGIN_VERSIONED_NAMESPACE_DECL
 
-void TAO_DII_Arguments_Converter::convert (
-TAO_ServerRequest & ,
-TAO::Argument * const [],
-size_t
-ACE_ENV_ARG_DECL )
+TAO_DII_Arguments_Converter::~TAO_DII_Arguments_Converter (void)
 {
-#if defined (ACE_HAS_EXCEPTIONS)
-      ACE_THROW (CORBA::INTERNAL ());
-#else
-      ACE_OS::abort ();
-#endif /* ACE_HAS_EXCEPTIONS */
 }
-
-
-int
-TAO_DII_Arguments_Converter::Initializer (void)
-{
-  return ACE_Service_Config::process_directive (ace_svc_desc_TAO_DII_Arguments_Converter);
-}
-
 
 TAO_END_VERSIONED_NAMESPACE_DECL
-
-
-ACE_STATIC_SVC_DEFINE (
-  TAO_DII_Arguments_Converter,
-  ACE_TEXT ("DII_Arguments_Converter"),
-  ACE_SVC_OBJ_T,
-  &ACE_SVC_NAME (TAO_DII_Arguments_Converter),
-  ACE_Service_Type::DELETE_THIS | ACE_Service_Type::DELETE_OBJ,
-  0
-  )
-
-ACE_FACTORY_DEFINE (TAO_PortableServer, TAO_DII_Arguments_Converter)
 
 
