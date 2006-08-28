854c38f0a497c6fef263094499f40db7d4fb8718
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-28 09:38:34 +0000

    Mon Aug 28 09:38:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tests/OBV/Simple/run_test.pl
+++ trunk/TAO/tests/OBV/Simple/run_test.pl
@@ -14,14 +14,19 @@ $iorfile = PerlACE::LocalFile ("obv.ior");
 
 unlink $iorfile;
 
-$SV = new PerlACE::Process ("server", "-o $iorfile");
+if (PerlACE::is_vxworks_test()) {
+    $SV = new PerlACE::ProcessVX ("server", "-o server.ior");
+}
+else {
+    $SV = new PerlACE::Process ("server", "-o $iorfile");
+}
 $CL = new PerlACE::Process ("client", "-f $iorfile");
 
 $SV->Spawn ();
 
 if (PerlACE::waitforfile_timed ($iorfile, 15) == -1) {
     print STDERR "ERROR: timed out waiting for file <$iorfile>\n";
-    $SV->Kill (); 
+    $SV->Kill ();
     exit 1;
 }
 
@@ -32,7 +37,7 @@ if ($client != 0) {
     $status = 1;
 }
 
-$server = $SV->TerminateWaitKill (5); 
+$server = $SV->TerminateWaitKill (5);
 
 if ($server != 0) {
     print STDERR "ERROR: server returned $server\n";
