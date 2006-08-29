9d24e0f3ce9c8fe39284bcfd38357f8f690e2c41
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-08-29 21:22:39 +0000

    ChangeLogTag:Tue Aug 29 21:17:26 UTC 2006  Steve Huston  <shuston@riverace.com>

--- trunk/ACE/tests/Service_Config_DLL.h
+++ trunk/ACE/tests/Service_Config_DLL.h
@@ -21,6 +21,7 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+#include "ace/Service_Object.h"
 #include "ace/Task.h"
 
 /**
@@ -60,8 +61,30 @@ private:
 
 };
 
+/**
+ * @class Refuses_Init
+ *
+ * @brief The Refuses_Init service simply tests for proper operation
+ *        when a service's init() hook returns -1.
+ */
+class Service_Config_DLL_Export Refuses_Init : public ACE_Service_Object
+{
+public:
+  // Constructor; required for factory function.
+  Refuses_Init () {}
+
+  /// Initializes object when dynamic linking occurs.
+  virtual int init (int argc, ACE_TCHAR *argv[]);
+
+  /// Terminates object when dynamic unlinking occurs.
+  virtual int fini (void);
+
+  /// Returns information on a service object.
+  virtual int info (ACE_TCHAR **info_string, size_t length = 0) const;
+};
 
 ACE_FACTORY_DECLARE (Service_Config_DLL, Service_Config_DLL)
+ACE_FACTORY_DECLARE (Service_Config_DLL, Refuses_Init)
 
 #include /**/ "ace/post.h"
 
