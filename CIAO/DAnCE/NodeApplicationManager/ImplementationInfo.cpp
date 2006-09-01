054ef60fc87348dc8376439f91d21fef5ce2b96b
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-09-01 16:29:31 +0000

    made corrections on  reading the right info property

--- CIAO/DAnCE/NodeApplicationManager/ImplementationInfo.cpp
+++ CIAO/DAnCE/NodeApplicationManager/ImplementationInfo.cpp
@@ -30,11 +30,6 @@ CIAO::NodeImplementationInfoHandler::node_impl_info (void) const
 void
 CIAO::NodeImplementationInfoHandler::populate_server_resource_def (void)
 {
-  const CORBA::ULong instance_len = plan_.instance.length ();
-  const char * target_resource_id = 0;
-
-  CORBA::ULong i;
-
   // Iterate over the instance list to find whether any server resource
   // has been specified
   // TODO: We shoud do some sanity check here, since all the component
@@ -48,33 +43,41 @@ CIAO::NodeImplementationInfoHandler::populate_server_resource_def (void)
   // ServerResource file will be deployed in the same NA.  However,
   // those using different ServerResource files should be deployed to
   // different NAs.  Something to be fixed later.
+
+  const CORBA::ULong instance_len = plan_.instance.length ();
+  const char * target_resource_id = 0;
+  const char * server_resource_id = 0;
+  CIAO::DAnCE::ServerResource *server_resource_def = 0;
+  CORBA::ULong dep_res_len;
+  CORBA::ULong i;
+  CORBA::ULong j;
+  CORBA::ULong info_pro_len = this->plan_.infoProperty.length ();
+
+  for (i = 0; i < info_pro_len; ++i)
+    {
+      if (ACE_OS::strcmp (this->plan_.infoProperty[i].name.in (),
+                          "CIAOServerResources") == 0)
+        {
+          this->plan_.infoProperty[i].value >>= server_resource_def;
+          server_resource_id = (*server_resource_def).Id;
+        }
+    }
+
   for (i = 0; i < instance_len; ++i)
     {
-      if (this->plan_.instance[i].deployedResource.length () != 0)
+      dep_res_len = this->plan_.instance[i].deployedResource.length ();
+      for (j = 0; j < dep_res_len; ++j)
         {
-          target_resource_id =
-            this->plan_.instance[i].deployedResource[0].resourceName.in ();
-
-          // Some component instance has server resource usage defined, so we
-          // set the <nodeapp_config> field of the NodeImplementationInfo struct.
-          for (CORBA::ULong j = 0; j < this->plan_.infoProperty.length (); ++j)
+          target_resource_id = this->plan_.instance[i].deployedResource[j].
+                                 resourceName.in ();
+          if (ACE_OS::strcmp (server_resource_id, target_resource_id) == 0)
             {
-              CIAO::DAnCE::ServerResource *server_resource_def = 0;
-              this->plan_.infoProperty[j].value >>= server_resource_def;
-
-              if (ACE_OS::strcmp ((*server_resource_def).Id,
-                                  target_resource_id) == 0)
-                {
-                  // Found the target server resource def, and store it.
-                  this->node_info_->nodeapp_config.length (1);
-
-                  this->node_info_->nodeapp_config[0].name =
-                    CORBA::string_dup ("CIAOServerResources");
-
-                  this->node_info_->nodeapp_config[0].value <<=
-                    *server_resource_def;
-                  break;
-                }
+              this->node_info_->nodeapp_config.length (1);
+              this->node_info_->nodeapp_config[0].name =
+                CORBA::string_dup ("CIAOServerResources");
+              this->node_info_->nodeapp_config[0].value <<=
+                *server_resource_def;
+              break;
             }
         }
     }
