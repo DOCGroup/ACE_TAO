3ac0efe61bb79dc5b00043749d0b6f0f4d9ed740
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-09-29 16:00:30 +0000

    Creating a branch for adding the Transport Current feature

---
+++ branches/doc-transport-current/bin/cltime.pl
@@ -0,0 +1,12 @@
+use strict;
+
+my($name)  = shift;
+my($email)  = shift;
+my($entry) = scalar(gmtime());
+
+my($tz) = 'UTC';
+$entry =~ s/(:\d\d\s+)(.*)(\d\d\d\d)$/$1$tz $3/;
+
+$entry .= "  $name <$email>";
+
+print $entry;
