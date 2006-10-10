5187c234a231c2bf133cf751f0d263981c9353eb
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-10-10 22:45:08 +0000

    Created for LMCO ATC ANA code testing.

--- TAO/examples/Kokyu_dsrt_schedulers/utils.h
+++
@@ -1,9 +0,0 @@
-//$Id$
-
-#include "ace/OS.h"
-#include "tao/RTCORBA/RTCORBA.h"
-#include "Kokyu_qosC.h"
-#include "tao/RTScheduling/RTScheduler.h"
-
-void guid_copy( Kokyu::GuidType& lhs, const RTScheduling::Current::IdType& rhs);
-void guid_copy( RTScheduling::Current::IdType& lhs, const Kokyu::GuidType& rhs);
