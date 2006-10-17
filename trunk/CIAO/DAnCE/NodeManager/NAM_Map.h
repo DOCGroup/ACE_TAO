b8736f71a91243c99bb2055671392917bbfb198b
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-10-17 13:11:05 +0000

    Tue Oct 17 13:10:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/CIAO/DAnCE/NodeManager/NAM_Map.h
+++ trunk/CIAO/DAnCE/NodeManager/NAM_Map.h
@@ -39,7 +39,7 @@ namespace CIAO
     NAM_Map (void);
 
     /// Determine if there is a NAM associated with a UUID
-    bool is_available (const ACE_CString &str);
+    bool is_available (const ACE_CString &str) const;
 
     /// Insert a NAM OID into the collection
     bool insert_nam (const ACE_CString &str,
