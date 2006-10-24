28b7e2df7485b5b63428e0b3774e3a1ef1cea83d
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-10-24 14:10:41 +0000

    ChangeLogTag: Tue Oct 24 14:09:49 UTC 2006  Iliyan Jeliazkov  <iliyan@ociweb.com> - importing changes to the HEAD

--- ace/config-lynxos.h
+++ ace/config-lynxos.h
@@ -1,7 +1,7 @@
 // $Id$
 
 // The following configuration file is designed to work for LynxOS,
-// version 3.0.0 and later, using the GNU g++ compiler.
+// version 4.0.0 and later, using the GNU g++ compiler.
 
 #ifndef ACE_CONFIG_H
 #define ACE_CONFIG_H
@@ -15,11 +15,8 @@
 # include "ace/config-g++-common.h"
 #endif /* __GNUG__ */
 
-#if ACE_LYNXOS_MAJOR > 3 || (ACE_LYNXOS_MAJOR == 3 && ACE_LYNXOS_MINOR > 0)
-  // LynxOS 3.1.0 or greater needs ipc_1c.h to be included before net/if.h
-  // to avoid macro conflict.
-# define ACE_NEEDS_IPC_1C_H
-#endif /* ACE_LYNXOS_MAJOR */
+#define ACE_NEEDS_IPC_1C_H
+
 
 #if defined (__x86__)
 # define ACE_HAS_PENTIUM
@@ -96,13 +93,13 @@
 #define ACE_LACKS_STRCASECMP_PROTOTYPE
 #define ACE_LACKS_STRNCASECMP_PROTOTYPE
 #define ACE_LACKS_STRPTIME
+#define ACE_LACKS_SUSECONDS_T
 #define ACE_LACKS_SWAB_PROTOTYPE
 #define ACE_LACKS_SYS_SELECT_H
 #define ACE_LACKS_TIMESPEC_T
 #define ACE_LACKS_UCONTEXT_H
-#define ACE_LACKS_WCHAR_H
-#define ACE_LACKS_SUSECONDS_T
 #define ACE_LACKS_USECONDS_T
+#define ACE_LACKS_WCHAR_H
 #define ACE_MALLOC_ALIGN 8
 #define ACE_PAGE_SIZE 4096
 #define ACE_SCANDIR_CMP_USES_CONST_VOIDPTR
@@ -116,17 +113,7 @@
 #if ACE_MT_SAFE == 1
   // Platform supports threads.
 # define ACE_HAS_PTHREADS
-# if ACE_LYNXOS_MAJOR > 3 || (ACE_LYNXOS_MAJOR == 3 && ACE_LYNXOS_MINOR > 0)
-    /* LynxOS 3.1.0 or greater */
-#   define ACE_HAS_PTHREADS_STD
-# else  /* LynxOS < 3.1.0 */
-#   define ACE_HAS_PTHREADS_DRAFT4
-#   define ACE_HAS_STDARG_THR_DEST
-#   define ACE_LACKS_MUTEXATTR_PSHARED
-    // Without TSS emulation, you'll only have 3 native TSS keys on
-    // LynxOS 3.0.0/ppc.
-#   define ACE_HAS_TSS_EMULATION
-# endif /* LynxOS < 3.1.0 */
+# define ACE_HAS_PTHREADS_STD
 # define ACE_HAS_THREADS
 # define ACE_HAS_THREAD_SPECIFIC_STORAGE
 # define ACE_LACKS_NULL_PTHREAD_STATUS
@@ -146,14 +133,6 @@
 
 #define ACE_HAS_BROKEN_PREALLOCATED_OBJECTS_AFTER_FORK 1
 
-#if __GNUC__ == 2  &&  __GNUC_MINOR__ == 9
-  // config-g++-common.h defines these incorrectly for LynxOS 3.x
-  // with G++ version 2.9-gnupro-98r2
-# define ACE_HAS_TEMPLATE_SPECIALIZATION
-# define ACE_HAS_EXPLICIT_STATIC_TEMPLATE_MEMBER_INSTANTIATION
-# undef ACE_LACKS_STATIC_DATA_MEMBER_TEMPLATES
-#endif /* __GNUC__ == 2  &&  __GNUC_MINOR__ == 9 */
-
 #if __GNUC__ < 3
 # define ACE_LACKS_NUMERIC_LIMITS
 #endif /* __GNUC__ < 3 */
@@ -166,42 +145,12 @@
 // System include files are not in sys/, this gets rid of warning.
 #define __NO_INCLUDE_WARN__
 
-#if ACE_LYNXOS_MAJOR > 3 /* LynxOS 4.x */
-# define ACE_HAS_POSIX_SEM
-  // "changes signedness" error (OS.i and many other files)
-# define ACE_HAS_SOCKLEN_T
-  // LSOCK.cpp uses a macro from param.h, not included
-# define ALIGNBYTES (sizeof(int) - 1)
-# define ALIGN(p) (((unsigned)p + ALIGNBYTES) & ~ALIGNBYTES)
-#else /* LynxOS 3.x */
-  // Note on why ACE_HAS_POSIX_SEM is not #defined:
-  // ACE_HAS_POSIX_SEM would cause native LynxOS mutexes and condition
-  // variables to be used.  But, they don't appear to be intended to be
-  // used between processes.  Without ACE_HAS_POSIX_SEM, ACE uses
-  // semaphores for all synchronization.  Those can be used between
-  // processes
-  //# define ACE_HAS_POSIX_SEM
-
-  // Don't use MAP_FIXED, at least for now.
-# define ACE_MAP_FIXED 0
-  // LynxOS, through 3.0.0, does not support MAP_PRIVATE,
-  // so map it to MAP_SHARED.
-# define ACE_MAP_PRIVATE ACE_MAP_SHARED
-
-  // This gets around Lynx broken macro calls resulting in "::0"
-# define _POSIX_THREADS_CALLS
-
-# define ACE_HAS_LYNXOS_BROKEN_MMAP
-# define ACE_HAS_POLL
-# define ACE_LACKS_AUTO_PTR
-# define ACE_LACKS_DLFCN_H
-# define ACE_LACKS_SETPGID
-# define ACE_LACKS_SETREGID
-# define ACE_LACKS_SETREUID
-# define ACE_LACKS_SETUID
-# define ACE_LACKS_SI_ADDR
-# define ACE_LACKS_TCP_NODELAY
-#endif /* ACE_LYNXOS_MAJOR > 3 */
+#define ACE_HAS_POSIX_SEM
+// "changes signedness" error (OS.i and many other files)
+#define ACE_HAS_SOCKLEN_T
+// LSOCK.cpp uses a macro from param.h, not included
+#define ALIGNBYTES (sizeof(int) - 1)
+#define ALIGN(p) (((unsigned)p + ALIGNBYTES) & ~ALIGNBYTES)
 
 #include /**/ "ace/post.h"
 #endif /* ACE_CONFIG_H */
