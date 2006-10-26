088ab75379a3b21577f0ea94b0f2354323de3f32
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-10-26 16:43:25 +0000

    Merged from CIAO-events-3 branch.

---
+++ branches/EC_integration_04/modules/CIAO/ciaosvcs/Events/CIAO_RTEC/SimpleAddressServer.h
@@ -0,0 +1,26 @@
+// SimpleAddressServer.h
+
+#ifndef SIMPLEADDRESSSERVER_H
+#define SIMPLEADDRESSSERVER_H
+
+#include <orbsvcs/RtecUDPAdminS.h>
+
+class ACE_INET_Addr;
+
+class SimpleAddressServer : public POA_RtecUDPAdmin::AddrServer {
+public:
+  SimpleAddressServer (const ACE_INET_Addr& address);
+  virtual void get_addr (const RtecEventComm::EventHeader& header,
+                         RtecUDPAdmin::UDP_Addr& address)
+    throw (CORBA::SystemException);
+/*
+  virtual void get_ip_address (const RtecEventComm::EventHeader& header,
+                               RtecUDPAdmin::UDP_IP_Address_out address
+                               ACE_ENV_ARG_DECL)
+    throw (CORBA::SystemException);
+*/
+private:
+  RtecUDPAdmin::UDP_Addr address_;
+};
+
+#endif
