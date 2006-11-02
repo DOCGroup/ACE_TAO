2f2a8bacd825c71138e58434ac80c9d72dd285ee
nshankar <nshankar@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-11-02 17:55:54 +0000

    Thu Nov  2 17:48:07 UTC 2006  Nishanth Shankaran  <nshanka@nospam.com>

--- branches/ATC_RACE/Output_Adapters/XML_Output_Adapter/XML_Output_Adapter_exec.cpp
+++ branches/ATC_RACE/Output_Adapters/XML_Output_Adapter/XML_Output_Adapter_exec.cpp
@@ -1,19 +1,22 @@
-#include "Output_Manager_exec.h"
+#include "XML_Output_Adapter_exec.h"
 #include "ciao/CIAO_common.h"
-#include <map>
+#include "Config_Handlers/DP_Handler.h"
+#include "Config_Handlers/Utils/XML_Helper.h"
+#include "Config_Handlers/Deployment.hpp"
+#include <string>
 
 namespace CIAO
 {
   namespace RACE
   {
-    namespace CIDL_Output_Manager_Impl
+    namespace CIDL_XML_Output_Adapter_Impl
     {
       //==================================================================
       // Facet Executor Implementation Class:   PlanEgress_exec_i
       //==================================================================
 
-      PlanEgress_exec_i::PlanEgress_exec_i (Output_Manager_exec_i &comp) :
-        comp_ (comp)
+      PlanEgress_exec_i::PlanEgress_exec_i (XML_Output_Adapter_exec_i &comp)
+        : comp_ (comp)
       {
       }
 
@@ -24,41 +27,48 @@ namespace CIAO
       // Operations from ::CIAO::RACE::PlanEgress
 
       ::CORBA::Boolean
-      PlanEgress_exec_i::output_plan (
-        const ::CIAO::RACE::Plan_Actions & plan_seq
-        ACE_ENV_ARG_DECL_NOT_USED)
-      ACE_THROW_SPEC ((CORBA::SystemException))
+      PlanEgress_exec_i::output_plan (const ::CIAO::RACE::Plan_Actions &plan_seq)
+        ACE_THROW_SPEC ((CORBA::SystemException))
       {
-	ACE_DEBUG ((LM_ERROR, "RACE:Output_Manager::output_plan\n"));
-        CIAO::RACE::Output_Manager::adaptersConnections_var conn
-            (comp_.context_->get_connections_adapters ());
-
-        for (CORBA::ULong i = 0; i < conn->length (); ++i)
+        CIAO_TRACE ("XML_Output_Adapter_exec::PlanEgres_exec::output_plan");
+        for (size_t i = 0;i < plan_seq.length(); ++i)
           {
-            ACE_DEBUG ((LM_DEBUG, "In for loop\n"));
-            try
+            if (!this->print_plan (plan_seq[i].plan))
               {
-                conn[i].objref->output_plan (plan_seq);
+                return false;
               }
-	    catch (...)
-	      {
-                ACE_ERROR ((LM_ERROR, "Output_Manager: Caught unknown "
-                            "exception while pushing plan\n"));
-	      }
           }
+        return true;
+      }
 
+      bool
+      PlanEgress_exec_i::print_plan (const ::Deployment::DeploymentPlan &plan)
+      {
+        using namespace CIAO::Config_Handlers;
+
+        DP_Handler reverse_handler (plan);
+        xercesc::DOMDocument *the_xsc (XML_HELPER->create_dom (0));
+
+        deploymentPlan (*reverse_handler.xsc (), the_xsc);
+
+        std::string dom_out (plan.UUID.in ());
+        dom_out += ".cdp";
+        XML_HELPER->write_DOM (the_xsc, dom_out.c_str());
+
+        delete the_xsc;
+	ACE_DEBUG ((LM_DEBUG, "6\n"));
         return true;
       }
 
       //==================================================================
-      // Component Executor Implementation Class:   Output_Manager_exec_i
+      // Component Executor Implementation Class:   XML_Output_Adapter_exec_i
       //==================================================================
 
-      Output_Manager_exec_i::Output_Manager_exec_i (void)
+      XML_Output_Adapter_exec_i::XML_Output_Adapter_exec_i (void)
       {
       }
 
-      Output_Manager_exec_i::~Output_Manager_exec_i (void)
+      XML_Output_Adapter_exec_i::~XML_Output_Adapter_exec_i (void)
       {
       }
 
@@ -69,7 +79,7 @@ namespace CIAO
       // Port operations.
 
       ::CIAO::RACE::CCM_PlanEgress_ptr
-      Output_Manager_exec_i::get_output (
+      XML_Output_Adapter_exec_i::get_plan_egress (
         ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
       ACE_THROW_SPEC ((CORBA::SystemException))
       {
@@ -79,7 +89,7 @@ namespace CIAO
       // Operations from Components::SessionComponent
 
       void
-      Output_Manager_exec_i::set_session_context (
+      XML_Output_Adapter_exec_i::set_session_context (
         ::Components::SessionContext_ptr ctx
         ACE_ENV_ARG_DECL)
       ACE_THROW_SPEC ((
@@ -87,7 +97,7 @@ namespace CIAO
                         ::Components::CCMException))
       {
         this->context_ =
-        Output_Manager_Context::_narrow (
+        XML_Output_Adapter_Context::_narrow (
           ctx
           ACE_ENV_ARG_PARAMETER);
         ACE_CHECK;
@@ -99,16 +109,7 @@ namespace CIAO
       }
 
       void
-      Output_Manager_exec_i::ciao_preactivate (
-        ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
-      ACE_THROW_SPEC ((
-                        ::CORBA::SystemException,
-                        ::Components::CCMException))
-      {
-      }
-
-      void
-      Output_Manager_exec_i::ciao_postactivate (
+      XML_Output_Adapter_exec_i::ciao_preactivate (
         ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
       ACE_THROW_SPEC ((
                         ::CORBA::SystemException,
@@ -118,7 +119,7 @@ namespace CIAO
       }
 
       void
-      Output_Manager_exec_i::ccm_activate (
+      XML_Output_Adapter_exec_i::ciao_postactivate (
         ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
       ACE_THROW_SPEC ((
                         ::CORBA::SystemException,
@@ -128,7 +129,7 @@ namespace CIAO
       }
 
       void
-      Output_Manager_exec_i::ccm_passivate (
+      XML_Output_Adapter_exec_i::ccm_activate (
         ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
       ACE_THROW_SPEC ((
                         ::CORBA::SystemException,
@@ -138,7 +139,17 @@ namespace CIAO
       }
 
       void
-      Output_Manager_exec_i::ccm_remove (
+      XML_Output_Adapter_exec_i::ccm_passivate (
+        ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
+      ACE_THROW_SPEC ((
+                        ::CORBA::SystemException,
+                        ::Components::CCMException))
+      {
+        // Your code here.
+      }
+
+      void
+      XML_Output_Adapter_exec_i::ccm_remove (
         ACE_ENV_SINGLE_ARG_DECL_NOT_USED)
       ACE_THROW_SPEC ((
                         ::CORBA::SystemException,
@@ -148,14 +159,14 @@ namespace CIAO
       }
 
       //==================================================================
-      // Home Executor Implementation Class:   Output_Manager_Home_exec_i
+      // Home Executor Implementation Class:   XML_Output_Adapter_Home_exec_i
       //==================================================================
 
-      Output_Manager_Home_exec_i::Output_Manager_Home_exec_i (void)
+      XML_Output_Adapter_Home_exec_i::XML_Output_Adapter_Home_exec_i (void)
       {
       }
 
-      Output_Manager_Home_exec_i::~Output_Manager_Home_exec_i (void)
+      XML_Output_Adapter_Home_exec_i::~XML_Output_Adapter_Home_exec_i (void)
       {
       }
 
@@ -170,7 +181,7 @@ namespace CIAO
       // Implicit operations.
 
       ::Components::EnterpriseComponent_ptr
-      Output_Manager_Home_exec_i::create (
+      XML_Output_Adapter_Home_exec_i::create (
         ACE_ENV_SINGLE_ARG_DECL)
       ACE_THROW_SPEC ((
                         ::CORBA::SystemException,
@@ -181,22 +192,22 @@ namespace CIAO
 
         ACE_NEW_THROW_EX (
           retval,
-          Output_Manager_exec_i,
+          XML_Output_Adapter_exec_i,
           CORBA::NO_MEMORY ());
         ACE_CHECK_RETURN (::Components::EnterpriseComponent::_nil ());
 
         return retval;
       }
 
-      extern "C" OUTPUT_MANAGER_EXEC_Export ::Components::HomeExecutorBase_ptr
-      create_CIAO_RACE_Output_Manager_Home_Impl (void)
+      extern "C" XML_OUTPUT_ADAPTER_EXEC_Export ::Components::HomeExecutorBase_ptr
+      create_CIAO_RACE_XML_Output_Adapter_Home_Impl (void)
       {
         ::Components::HomeExecutorBase_ptr retval =
         ::Components::HomeExecutorBase::_nil ();
 
         ACE_NEW_RETURN (
           retval,
-          Output_Manager_Home_exec_i,
+          XML_Output_Adapter_Home_exec_i,
           ::Components::HomeExecutorBase::_nil ());
 
         return retval;
