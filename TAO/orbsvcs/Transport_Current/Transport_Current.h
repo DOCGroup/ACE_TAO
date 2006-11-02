42047e7082dfdb3e91a01c422b423831060a9b13
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-11-02 17:41:07 +0000

    Fixing a patch gone doubly wrong

--- TAO/orbsvcs/Transport_Current/Transport_Current.h
+++ TAO/orbsvcs/Transport_Current/Transport_Current.h
@@ -30,3 +30,19 @@ static int tao_transport_current_initializer_called =
 
 #endif /* TAO_AS_STATIC_LIBS */
 
+// -*- C++ -*-
+// $Id$
+
+
+#include "orbsvcs/Transport_Current/TCC.h"
+#include "orbsvcs/Transport_Current/Current_Loader.h"
+
+#if defined (TAO_AS_STATIC_LIBS)
+
+// Only do this for static builds. It causes a circular dependency for
+// dynamic builds.
+static int tao_transport_current_initializer_called =
+  TAO::Transport::current_static_initializer ();
+
+#endif /* TAO_AS_STATIC_LIBS */
+
