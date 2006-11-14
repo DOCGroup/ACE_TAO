5c6a15c93bd69bc3b63fa1bf9c4983abcf210f66
Johnny Willemsen <jwillemsen@remedy.nl>
2006-11-14 13:31:44 +0000

    Tue Nov 14 12:57:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/Wait_Strategy.cpp
+++ TAO/tao/Wait_Strategy.cpp
@@ -2,6 +2,8 @@
 
 #include "tao/Wait_Strategy.h"
 
+//@@ WAIT_STRATEGY_SPL_COPY_HOOK_START
+
 ACE_RCSID (tao,
            Wait_Strategy,
            "$Id$")
@@ -9,7 +11,6 @@ ACE_RCSID (tao,
 #if !defined (__ACE_INLINE__)
 # include "tao/Wait_Strategy.inl"
 #endif /* __ACE_INLINE__ */
-
 
 TAO_BEGIN_VERSIONED_NAMESPACE_DECL
 
