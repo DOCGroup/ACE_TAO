837534242441ea98caa63723ac89f60f8b24ee1d
seibelr <seibelr@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-11-24 22:12:20 +0000

    Added the ACE and TAO for this branch

---
+++ branches/GH5_0port/ACE/apps/JAWS3/jaws3/Options.cpp
@@ -0,0 +1,25 @@
+// $Id$
+
+#include "ace/OS_NS_stdlib.h"
+
+#ifndef JAWS_BUILD_DLL
+#define JAWS_BUILD_DLL
+#endif /*JAWS_BUILD_DLL*/
+
+#include "jaws3/Options.h"
+
+JAWS_Options::JAWS_Options (void)
+{
+  this->cf_ = new JAWS_Config_File ("jaws.conf");
+}
+
+const char *
+JAWS_Options::getenv (const char *key)
+{
+  const char *value = 0;
+  if (this->cf_ == 0 || this->cf_->find (key, value) < 0)
+    value = ACE_OS::getenv (key);
+
+  return value;
+}
+
---
+++ branches/GH5_0port/TAO/tao/Utils/Encodable.cpp
@@ -0,0 +1,11 @@
+// $Id$
+
+#include "tao/Utils/Encodable.h"
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+TAO_Encodable::~TAO_Encodable (void)
+{
+}
+
+TAO_END_VERSIONED_NAMESPACE_DECL
