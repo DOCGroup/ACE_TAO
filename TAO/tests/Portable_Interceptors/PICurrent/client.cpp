fa685b98fe80dc73d067ab14ca88a6d9fed6c612
Johnny Willemsen <jwillemsen@remedy.nl>
2006-11-24 13:46:19 +0000

    Fri Nov 24 13:45:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/Portable_Interceptors/PICurrent/client.cpp
+++ TAO/tests/Portable_Interceptors/PICurrent/client.cpp
@@ -115,6 +115,22 @@ main (int argc, char *argv[])
                             1);
         }
 
+      // BUG 2656 testing - _get_policy_overrides() should return an empty
+      // sequence rather than nill.
+      CORBA::PolicyTypeSeq types;
+      CORBA::PolicyList_var policies = server->_get_policy_overrides(types);
+
+      if (policies.ptr () == 0)
+        {
+            ACE_ERROR ((LM_ERROR,
+                        "(%P|%t) _get_policy_overrides returned nill pointer\n"));
+            ACE_TRY_THROW (CORBA::INTERNAL ());
+        }
+      else
+        {
+          CORBA::ULong ListSize = policies->length();
+        }
+
       server->invoke_me (ACE_ENV_SINGLE_ARG_PARAMETER);
       ACE_TRY_CHECK;
 
