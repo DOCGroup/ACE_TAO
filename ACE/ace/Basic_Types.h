58e7eb8d34083dec56d99dd0221c8ed73cba2107
Johnny Willemsen <jwillemsen@remedy.nl>
2006-12-04 12:50:02 +0000

    Mon Dec  4 12:49:12 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/ace/Basic_Types.h
+++ ACE/ace/Basic_Types.h
@@ -392,8 +392,8 @@ ACE_END_VERSIONED_NAMESPACE_DECL
 # else /* ! BYTE_ORDER && ! __BYTE_ORDER */
   // We weren't explicitly told, so we have to figure it out . . .
 #   if defined (i386) || defined (__i386__) || defined (_M_IX86) || \
-     defined (vax) || defined (__alpha) || defined (__LITTLE_ENDIAN__) ||\
-     defined (ARM) || defined (_M_IA64) || \
+     defined (vax) || defined (__alpha) || defined (__LITTLE_ENDIAN__) || \
+     defined (ARM) || defined (_M_IA64) || defined (__ia64__) || \
      defined (_M_AMD64) || defined (__amd64)
     // We know these are little endian.
 #     define ACE_LITTLE_ENDIAN 0x0123
