63174076ed95fecdff268832378d4bdee50e8e4e
Johnny Willemsen <jwillemsen@remedy.nl>
2006-12-06 19:22:23 +0000

    Wed Dec  6 19:21:57 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/PortableServer/Active_Object_Map_Entry.cpp
+++ TAO/tao/PortableServer/Active_Object_Map_Entry.cpp
@@ -13,7 +13,7 @@ TAO_Active_Object_Map_Entry::TAO_Active_Object_Map_Entry (void)
     system_id_ (),
     servant_ (0),
     reference_count_ (1),
-    deactivated_ (0),
+    deactivated_ (false),
     priority_ (-1)
 {
 }
