6c4bf2536ba341c7f7efddc910a9a3b83cd61e02
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-12-12 20:56:46 +0000

    Tue Dec 12 20:55:02 UTC 2006    Gan Deng <gan.deng@vanderbilt.edu>

--- CIAO/ciaosvcs/Events/CIAO_Events_Base/CIAO_EventServiceBase.h
+++ CIAO/ciaosvcs/Events/CIAO_Events_Base/CIAO_EventServiceBase.h
@@ -114,7 +114,8 @@ namespace CIAO
      */
     virtual void ciao_push_event (
         ::Components::EventBase * evt,
-        const char * source_id
+        const char * source_id,
+        ::CORBA::TypeCode_ptr tc
         ACE_ENV_ARG_DECL_WITH_DEFAULTS)
       ACE_THROW_SPEC ((
         ::CORBA::SystemException,
