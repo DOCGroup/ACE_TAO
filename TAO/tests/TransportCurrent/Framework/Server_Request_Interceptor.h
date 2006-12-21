edcc28173281bb19cb82ee655175a64d32696c72
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2006-12-21 21:30:59 +0000

    ChangeLogTag: Thu Dec 21 21:29:47 UTC 2006  Iliyan Jeliazkov  <iliyan@ociweb.com>

--- TAO/tests/TransportCurrent/Framework/Server_Request_Interceptor.h
+++ TAO/tests/TransportCurrent/Framework/Server_Request_Interceptor.h
@@ -30,11 +30,6 @@
 #include "tao/LocalObject.h"
 #include "tao/PI_Server/PI_Server.h"
 
-#if defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable:4250)
-#endif /* _MSC_VER */
-
 #include "ORBInitializer_T.h"
 
 namespace Test
