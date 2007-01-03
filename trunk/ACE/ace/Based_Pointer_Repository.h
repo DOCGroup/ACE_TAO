c215d995dc81f65e27c3df59f3f8062a27b0aefb
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-03 07:52:38 +0000

    Wed Jan  3 07:52:05 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ACE/ace/Based_Pointer_Repository.h
+++ trunk/ACE/ace/Based_Pointer_Repository.h
@@ -76,9 +76,9 @@ private:
 // ----------------------------------
 
 /// Declare a process wide singleton
-ACE_EXPORT_SINGLETON_DECLARE (ACE_Singleton,
-                              ACE_Based_Pointer_Repository,
-                              ACE_SYNCH_RW_MUTEX)
+ACE_SINGLETON_DECLARE (ACE_Singleton,
+                       ACE_Based_Pointer_Repository,
+                       ACE_SYNCH_RW_MUTEX)
 
 /// Provide a Singleton access point to the based pointer repository.
 typedef ACE_Singleton<ACE_Based_Pointer_Repository, ACE_SYNCH_RW_MUTEX>
