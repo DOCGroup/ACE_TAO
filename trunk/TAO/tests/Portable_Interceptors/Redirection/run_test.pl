e03e6f3bfb08a2b539ad6bb87ac5f1f290d7a438
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-03 15:16:20 +0000

    Wed Jan  3 15:15:05 UTC 2006  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ trunk/TAO/tests/Portable_Interceptors/Redirection/run_test.pl
@@ -0,0 +1,65 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# -*- perl -*-
+#
+# $Id$
+
+
+use lib "$ENV{ACE_ROOT}/bin";
+use PerlACE::Run_Test;
+
+$file1 = PerlACE::LocalFile ("test1.ior");
+$file2 = PerlACE::LocalFile ("test2.ior");
+
+unlink $file1;
+unlink $file2;
+
+$SV1 = new PerlACE::Process ("server", "-o $file1 -c 1 -n 1");
+$SV2 = new PerlACE::Process ("server", "-o $file2 -n 2");
+$CL = new PerlACE::Process ("client", "-p file://$file1 -s file://$file2");
+
+$status = 0;
+
+print STDERR "\n\n==== Running PortableInterceptor::Redirection test\n";
+
+$SV1->Spawn ();
+$SV2->Spawn ();
+
+if (PerlACE::waitforfile_timed ($file1, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$file1>\n";
+    $SV1->Kill ();
+    exit 1;
+}
+
+if (PerlACE::waitforfile_timed ($file2, 15) == -1) {
+    print STDERR "ERROR: cannot find file <$file2>\n";
+    $SV2->Kill ();
+    exit 1;
+}
+
+$client = $CL->SpawnWaitKill (60);
+
+if ($client != 0) {
+    print STDERR "ERROR: client returned $client\n";
+    $status = 1;
+}
+
+$server1 = $SV1->WaitKill (5);
+
+if ($server1 != 0) {
+    $status = 1;
+}
+
+$server2 = $SV2->WaitKill (5);
+
+if ($server2 != 0) {
+    print STDERR "ERROR: server returned $server\n";
+    $status = 1;
+}
+
+unlink $file1;
+unlink $file2;
+
+exit $status;
