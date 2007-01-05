2082d6bfc337e55a1e10fa62c0f721520167b4ad
Johnny Willemsen <jwillemsen@remedy.nl>
2007-01-05 10:05:29 +0000

    Fri Jan  5 10:04:18 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/ace/config-g++-common.h
+++ ACE/ace/config-g++-common.h
@@ -78,20 +78,24 @@
 #  define ACE_Proper_Export_Flag __attribute__ ((visibility("default")))
 #  define ACE_Proper_Import_Flag
 
-# if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ > 1)) || ((__GNUC__ == 4) && (__GNUC_MINOR__ == 1) && (__GNUC_PATCHLEVEL__ >= 1))
+# if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 2))
 // Sadly, G++ 4.x silently ignores visibility attributes on
 // template instantiations, which breaks singletons.
 // As a workaround, we use the GCC visibility pragmas.
 // And to make them fit in a macro, we use C99's _Pragma()
 // http://gcc.gnu.org/bugzilla/show_bug.cgi?id=17470
-// This has been fixed in GCC 4.1.1
-#  define GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS 1
+// This has been fixed in GCC 4.1.1 with FC6 but not with SuSE 10.2
+// that gets shipped with GCC 4.1.2 so we assume that with GCC 4.2
+// this will be fixed on the head. With FC6 just set this define yourself
+#  ifndef ACE_GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS
+#    define ACE_GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS 1
+#  endif
 # endif
 
-#  if defined (GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS) && GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS == 1
+#  if defined (ACE_GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS) && GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS == 1
 #   define ACE_EXPORT_SINGLETON_DECLARATION(T) template class ACE_Proper_Export_Flag T
 #   define ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) template class ACE_Proper_Export_Flag SINGLETON_TYPE <CLASS, LOCK>;
-#  else  /* ! GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS */
+#  else  /* ACE_GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS */
 #   define ACE_EXPORT_SINGLETON_DECLARATION(T)     \
         _Pragma ("GCC visibility push(default)")  \
         template class T                          \
@@ -100,7 +104,7 @@
         _Pragma ("GCC visibility push(default)")                    \
         template class SINGLETON_TYPE<CLASS, LOCK>;                 \
         _Pragma ("GCC visibility pop")
-#  endif /* ! GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS */
+#  endif /* ACE_GCC_HAS_TEMPLATE_INSTANTIATION_VISIBILITY_ATTRS */
 
 // Note that the "__extension__" is needed to prevent g++ from issuing
 // an error when using its "-pedantic" command line flag.
