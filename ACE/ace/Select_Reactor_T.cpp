408f6742b3eda974e8e0b1d2ed4ff5d533c377ed
Johnny Willemsen <jwillemsen@remedy.nl>
2007-01-08 12:00:53 +0000

    Mon Jan  8 12:00:30 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/ace/Select_Reactor_T.cpp
+++ ACE/ace/Select_Reactor_T.cpp
@@ -210,7 +210,7 @@ ACE_Select_Reactor_T<ACE_SELECT_REACTOR_TOKEN>::notify (ACE_Event_Handler *eh,
   // caller to dictate which Event_Handler method the receiver
   // invokes.  Note that this call can timeout.
 
-  ssize_t n = this->notify_handler_->notify (eh, mask, timeout);
+  ssize_t const n = this->notify_handler_->notify (eh, mask, timeout);
   return n == -1 ? -1 : 0;
 }
 
