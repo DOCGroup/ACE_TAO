b92c7cc4d663d8a3cdbef665b5e68969c9d0d441
elliott_c <ocielliottc@users.noreply.github.com>
2007-01-10 19:24:58 +0000

    ChangeLogTag: Wed Jan 10 19:24:11 UTC 2007  Chad Elliott  <elliott_c@ociweb.com>

--- TAO/tests/Bug_1551_Regression/Client_Task.cpp
+++ TAO/tests/Bug_1551_Regression/Client_Task.cpp
@@ -29,6 +29,28 @@ Client_Task::svc (void)
   ACE_DECLARE_NEW_CORBA_ENV;
   ACE_TRY
     {
+      CORBA::Object_var object =
+        this->orb_->resolve_initial_references ("PolicyCurrent");
+
+      CORBA::PolicyCurrent_var policy_current =
+        CORBA::PolicyCurrent::_narrow (object.in ());
+
+      TimeBase::TimeT timeout_period = 20 * 1000;
+
+      CORBA::Any timeout_as_any;
+      timeout_as_any <<= timeout_period;
+
+      CORBA::PolicyList policy_list (1);
+      policy_list.length (1);
+      policy_list[0] =
+        this->orb_->create_policy (Messaging::RELATIVE_RT_TIMEOUT_POLICY_TYPE,
+                                   timeout_as_any);
+
+      policy_current->set_policy_overrides (policy_list,
+                                            CORBA::ADD_OVERRIDE);
+
+      policy_list[0]->destroy();
+
       // Start 25 separate concurrent request streams
       for (CORBA::Short i = 0; i != 25; ++i)
       {
@@ -40,6 +62,14 @@ Client_Task::svc (void)
       ACE_Time_Value tv(10, 0);
       orb_->run(tv ACE_ENV_ARG_PARAMETER);
       ACE_TRY_CHECK;
+    }
+  ACE_CATCH (CORBA::TIMEOUT, ex)
+    {
+      // A CORBA::TIMEOUT exception is expected since
+      // the server aborts itself
+      ACE_UNUSED_ARG (ex);
+      ACE_DEBUG ((LM_DEBUG,
+                  "Client received an expected CORBA::TIMEOUT exception.\n"));
     }
   ACE_CATCHANY
     {
