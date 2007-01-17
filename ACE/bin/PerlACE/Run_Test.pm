26ffb398774c08032fa8bb976bc62ae9853010a5
elliott_c <ocielliottc@users.noreply.github.com>
2007-01-17 12:22:32 +0000

    ChangeLogTag: Wed Jan 17 12:20:57 UTC 2007  Chad Elliott  <elliott_c@ociweb.com>

--- ACE/bin/PerlACE/Run_Test.pm
+++ ACE/bin/PerlACE/Run_Test.pm
@@ -190,6 +190,31 @@ sub add_lib_path {
 
 }
 
+sub check_privilege_group {
+  if ($^O eq 'hpux') {
+    my($access) = 'RTSCHED';
+    my($status) = 0;
+    my($getprivgrp) = '/bin/getprivgrp';
+
+    if (-x $getprivgrp) {
+      if (open(GPG, "$getprivgrp |")) {
+        while(<GPG>) {
+          if (index($_, $access) >= 0) {
+            $status = 1;
+          }
+        }
+        close(GPG);
+      }
+    }
+
+    if (!$status) {
+      print STDERR "WARNING: You must have $access privileges to run this test.\n",
+                   "         Run \"man 1m setprivgrp\" for more information.\n";
+      exit(0);
+    }
+  }
+}
+
 $sleeptime = 5;
 
 1;
