c0f1c545fa47256f9e7c2781b147ae5ed67ec928
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-21 21:56:28 +0000

    adding DS library

--- branches/Telcordia_Diffserv/TAO/tao/DiffServPolicy/DiffServPolicy_ORBInitializer.cpp
+++ branches/Telcordia_Diffserv/TAO/tao/DiffServPolicy/DiffServPolicy_ORBInitializer.cpp
@@ -36,50 +36,64 @@ TAO_DiffServPolicy_ORBInitializer::register_policy_factories (
   PortableInterceptor::ORBInitInfo_ptr info
   ACE_ENV_ARG_DECL)
 {
-  TAO_ORBInitInfo * local_info = dynamic_cast <TAO_ORBInitInfo *> (info);
-  TAO_ORB_Core * the_orb_core = local_info->orb_core ();
-
-  // Register the EndpointPolicy policy factories.
-  PortableInterceptor::PolicyFactory_ptr policy_factory_ptr;
-  ACE_NEW_THROW_EX (policy_factory_ptr,
-                    TAO_EndpointPolicy_Factory(the_orb_core),
-                    CORBA::NO_MEMORY (
-                      CORBA::SystemException::_tao_minor_code (
-                        TAO::VMCID,
-                        ENOMEM),
-                      CORBA::COMPLETED_NO));
-  ACE_CHECK;
-
-
-  PortableInterceptor::PolicyFactory_var policy_factory =
-    policy_factory_ptr;
-
-  ACE_TRY
+  if (CORBA::is_nil (this->policy_factory_.in ()))
     {
-      info->register_policy_factory (EndpointPolicy::ENDPOINT_POLICY_TYPE,
-                                     policy_factory.in ()
-                                     ACE_ENV_ARG_PARAMETER);
-      ACE_TRY_CHECK;
+      PortableInterceptor::PolicyFactory_ptr policy_factory;
+      ACE_NEW_THROW_EX (policy_factory,
+                        TAO_DiffServ_PolicyFactory,
+                          CORBA::NO_MEMORY (
+                            CORBA::SystemException::_tao_minor_code (
+                              TAO::VMCID,
+                              ENOMEM),
+                            CORBA::COMPLETED_NO));
+      ACE_CHECK;
+
+      this->policy_factory_ = policy_factory;
     }
-  ACE_CATCH (CORBA::BAD_INV_ORDER, ex)
+
+  // Bind the same policy factory to all DiffServ related policy
+  // types since a single policy factory is used to create each of
+  // the different types of DiffServ policies.
+  static CORBA::PolicyType const type[] = {
+    TAO::CLIENT_NETWORK_PRIORITY_TYPE,
+    TAO::NETWORK_PRIORITY_TYPE
+  };
+
+  const CORBA::PolicyType *end =
+    type + sizeof (type) / sizeof (type[0]);
+  
+  for (CORBA::PolicyType const * i = type;
+       i != end;
+       ++i)
     {
-      if (ex.minor () == (CORBA::OMGVMCID | 16))
+      ACE_TRY
         {
-          // The factory is already there, it happens because the
-          // magic initializer in Messaging.cpp registers with the
-          // ORB multiple times.  This is an indication that we
-          // should do no more work in this ORBInitializer.
-          return;
+          info->register_policy_factory (*i,
+                                         this->policy_factory_.in ()
+                                         ACE_ENV_ARG_PARAMETER);
+          ACE_TRY_CHECK;
         }
-      ACE_RE_THROW;
+      ACE_CATCH (CORBA::BAD_INV_ORDER, ex)
+        {
+          if (ex.minor () == (CORBA::OMGVMCID | 16))
+            {
+              // The factory is already there, it happens because the
+              // magic initializer in PortableServer.cpp registers
+              // with the ORB multiple times.  This is an indication
+              // that we should do no more work in this
+              // ORBInitializer.
+              return;
+            }
+          ACE_RE_THROW;
+        }
+      ACE_CATCHANY
+        {
+          // Rethrow any other exceptions...
+          ACE_RE_THROW;
+        }
+      ACE_ENDTRY;
+      ACE_CHECK;
     }
-  ACE_CATCHANY
-    {
-      // Rethrow any other exceptions...
-      ACE_RE_THROW;
-    }
-  ACE_ENDTRY;
-  ACE_CHECK;
 }
 
 TAO_END_VERSIONED_NAMESPACE_DECL
