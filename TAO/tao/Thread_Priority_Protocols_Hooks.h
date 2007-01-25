712494562d94a2a80661c8a0cd3c83b121a22cab
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-25 23:00:33 +0000

    refactoring protocols hooks

---
+++ TAO/tao/Thread_Priority_Protocols_Hooks.h
@@ -0,0 +1,73 @@
+// -*- C++ -*-
+
+// ===================================================================
+/**
+ *  @file   Thread_Priority_Protocols_Hooks.h
+ *
+ *  $Id$
+ *
+ *  @author Jaiganesh Balasubramanian <jai@dre.vanderbilt.edu>
+ *          Johnny Willemsen <jwillemsen@remedy.nl>
+ */
+// ===================================================================
+
+#ifndef TAO_THREAD_PRIORITY_PROTOCOLS_HOOKS_H
+#define TAO_THREAD_PRIORITY_PROTOCOLS_HOOKS_H
+
+#include /**/ "ace/pre.h"
+#include "ace/CORBA_macros.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/Service_Object.h"
+#include /**/ "tao/TAO_Export.h"
+#include "tao/Basic_Types.h"
+#include "tao/IOP_IORC.h"
+#include "ace/SString.h"
+
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace CORBA
+{
+  class Policy;
+  class Environment;
+}
+
+class TAO_ORB_Core;
+class TAO_Service_Context;
+class TAO_Connection_Handler;
+class TAO_Stub;
+
+class TAO_Export TAO_Thread_Priority_Protocols_Hooks 
+      : public ACE_Service_Object
+{
+public:
+  /// destructor
+  virtual ~TAO_Thread_Priority_Protocols_Hooks (void);
+  
+  virtual int get_thread_CORBA_priority (CORBA::Short &
+                                         ACE_ENV_ARG_DECL) = 0;
+
+  virtual int get_thread_native_priority (CORBA::Short &
+                                          ACE_ENV_ARG_DECL) = 0;
+
+  virtual int get_thread_CORBA_and_native_priority (
+    CORBA::Short &,
+    CORBA::Short &
+    ACE_ENV_ARG_DECL) = 0;
+
+  virtual int set_thread_CORBA_priority (CORBA::Short
+                                         ACE_ENV_ARG_DECL) = 0;
+
+  virtual int set_thread_native_priority (CORBA::Short
+                                          ACE_ENV_ARG_DECL) = 0;
+};
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+#include /**/ "ace/post.h"
+
+#endif /* TAO_THREAD_PRIORITY_PROTOCOLS_HOOKS_H */
