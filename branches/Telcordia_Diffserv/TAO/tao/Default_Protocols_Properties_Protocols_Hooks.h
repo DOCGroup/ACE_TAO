b922313866b9e506d51fcd9be400477c5ea5bdfc
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-25 23:20:10 +0000

    refactoring protocols hooks

---
+++ branches/Telcordia_Diffserv/TAO/tao/Default_Protocols_Properties_Protocols_Hooks.h
@@ -0,0 +1,76 @@
+// -*- C++ -*-
+
+// ===================================================================
+/**
+ *  @file   Default_Protocols_Properties_Protocols_Hooks.h
+ *
+ *  $Id$
+ *
+ *  @author Jaiganesh Balasubramanian <jai@dre.vanderbilt.edu>
+ *          Johnny Willemsen <jwillemsen@remedy.nl>
+ */
+// ===================================================================
+
+#ifndef TAO_DEFAULT_PROTOCOLS_PROPERTIES_PROTOCOLS_HOOKS_H
+#define TAO_DEFAULT_PROTOCOLS_PROPERTIES_PROTOCOLS_HOOKS_H
+
+#include /**/ "ace/pre.h"
+#include "ace/Service_Config.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/Protocols_Properties_Protocols_Hooks.h"
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+class TAO_POA;
+
+class TAO_Export TAO_Default_Protocols_Properties_Protocols_Hooks 
+      : public TAO_Protocols_Properties_Protocols_Hooks
+{
+public:
+
+  /// destructor
+  virtual ~TAO_Default_Protocols_Properties_Protocols_Hooks (void);
+
+  void server_protocol_properties_at_orb_level (TAO_IIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void client_protocol_properties_at_orb_level (TAO_IIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void server_protocol_properties_at_orb_level (TAO_UIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void client_protocol_properties_at_orb_level (TAO_UIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void server_protocol_properties_at_orb_level (TAO_SHMIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void client_protocol_properties_at_orb_level (TAO_SHMIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void server_protocol_properties_at_orb_level (TAO_DIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void client_protocol_properties_at_orb_level (TAO_DIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void server_protocol_properties_at_orb_level (TAO_SCIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+
+  void client_protocol_properties_at_orb_level (TAO_SCIOP_Protocol_Properties &protocol_properties
+                                                ACE_ENV_ARG_DECL);
+};
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+ACE_STATIC_SVC_DECLARE_EXPORT (TAO, 
+                               TAO_Default_Protocols_Properties_Protocols_Hooks)
+ACE_FACTORY_DECLARE (TAO, TAO_Default_Protocols_Properties_Protocols_Hooks)
+
+#include /**/ "ace/post.h"
+#endif /* TAO_DEFAULT_PROTOCOLS_PROPERTIES_PROTOCOLS_HOOKS_H */
