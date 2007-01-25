9bcd8be4727e8792808385266d1ae12809789514
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-25 23:25:22 +0000

    refactoring protocols hooks

---
+++ TAO/tao/Default_Thread_Priority_Protocols_Hooks.h
@@ -0,0 +1,61 @@
+// -*- C++ -*-
+
+// ===================================================================
+/**
+ *  @file   Default_Thread_Priority_Protocols_Hooks.h
+ *
+ *  $Id$
+ *
+ *  @author Jaiganesh Balasubramanian <jai@dre.vanderbilt.edu>
+ *          Johnny Willemsen <jwillemsen@remedy.nl>
+ */
+// ===================================================================
+
+#ifndef TAO_DEFAULT_THREAD_PRIORITY_PROTOCOLS_HOOKS_H
+#define TAO_DEFAULT_THREAD_PRIORITY_PROTOCOLS_HOOKS_H
+
+#include /**/ "ace/pre.h"
+#include "ace/Service_Config.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/Thread_Priority_Protocols_Hooks.h"
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+class TAO_POA;
+
+class TAO_Export TAO_Default_Thread_Priority_Protocols_Hooks 
+      : public TAO_Thread_Priority_Protocols_Hooks
+{
+public:
+
+  /// destructor
+  virtual ~TAO_Default_Thread_Priority_Protocols_Hooks (void);
+
+  int get_thread_CORBA_priority (CORBA::Short &
+                                 ACE_ENV_ARG_DECL);
+
+  int get_thread_native_priority (CORBA::Short &
+                                  ACE_ENV_ARG_DECL);
+
+  int get_thread_CORBA_and_native_priority (CORBA::Short &,
+                                            CORBA::Short &
+                                            ACE_ENV_ARG_DECL);
+
+  int set_thread_CORBA_priority (CORBA::Short
+                                 ACE_ENV_ARG_DECL);
+
+  int set_thread_native_priority (CORBA::Short
+                                  ACE_ENV_ARG_DECL);
+};
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+ACE_STATIC_SVC_DECLARE_EXPORT (TAO, TAO_Default_Thread_Priority_Protocols_Hooks)
+ACE_FACTORY_DECLARE (TAO, TAO_Default_Thread_Priority_Protocols_Hooks)
+
+#include /**/ "ace/post.h"
+#endif /* TAO_DEFAULT_THREAD_PRIORITY_PROTOCOLS_HOOKS_H */
