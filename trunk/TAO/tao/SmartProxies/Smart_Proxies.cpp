2d80f63f03484c8f5b70a377c428a4bcbd2c1140
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-26 06:20:59 +0000

    Fri Jan 26 06:20:12 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/SmartProxies/Smart_Proxies.cpp
+++ trunk/TAO/tao/SmartProxies/Smart_Proxies.cpp
@@ -2,13 +2,18 @@
 
 #include "tao/SmartProxies/Smart_Proxies.h"
 
-ACE_RCSID(SmartProxies, Smart_Proxies, "$Id$")
+ACE_RCSID(SmartProxies,
+          Smart_Proxies,
+          "$Id$")
 
 #if !defined (__ACE_INLINE__)
 #include "tao/SmartProxies/Smart_Proxies.inl"
 #endif /* defined INLINE */
 
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
 TAO_Smart_Proxy_Base::~TAO_Smart_Proxy_Base (void)
 {
 }
 
+TAO_END_VERSIONED_NAMESPACE_DECL
