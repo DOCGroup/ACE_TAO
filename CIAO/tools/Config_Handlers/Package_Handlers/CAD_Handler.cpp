869c5eb7fe85445d92e26d4210730f386b2eed5f
William R. Otte <wotte@dre.vanderbilt.edu>
2007-01-30 20:50:13 +0000

    ChangeLogTag:Tue Jan 30 20:49:50 UTC 2007  William R. Otte  <wotte@dre.vanderbilt.edu>

--- CIAO/tools/Config_Handlers/Package_Handlers/CAD_Handler.cpp
+++ CIAO/tools/Config_Handlers/Package_Handlers/CAD_Handler.cpp
@@ -17,184 +17,179 @@ namespace CIAO
   {
     namespace Packaging
     {
-      namespace
+      struct Packaging_Handlers_Export SPE_Handler
       {
-        struct SPE_Handler
-        {
-          static void handle_spe (const SubcomponentPortEndpoint &desc,
-                               ::Deployment::SubcomponentPortEndpoint &toconfig)
-          {
-            CIAO_TRACE("SPE_Handler::get_spe");
+	static void handle_spe (const SubcomponentPortEndpoint &desc,
+				::Deployment::SubcomponentPortEndpoint &toconfig)
+	{
+	  CIAO_TRACE("SPE_Handler::get_spe");
 
-            toconfig.portName = desc.portName ().c_str ();
+	  toconfig.portName = desc.portName ().c_str ();
 
-            ACE_CString str (desc.instance ().id ().c_str ());
-            CORBA::ULong pos (0);
+	  ACE_CString str (desc.instance ().id ().c_str ());
+	  CORBA::ULong pos (0);
 
-            SID_Handler::IDREF.find_ref (str, pos);
-            toconfig.instanceRef = pos;
-          }
+	  SID_Handler::IDREF.find_ref (str, pos);
+	  toconfig.instanceRef = pos;
+	}
 
-          static SubcomponentPortEndpoint
-          get_spe (const ::Deployment::SubcomponentPortEndpoint &src)
-          {
-            CIAO_TRACE("SPE_Handler::get_spe - reverse");
-            ACE_CString str;
-            SID_Handler::IDREF.find_ref (src.instanceRef, str);
+	static SubcomponentPortEndpoint
+	get_spe (const ::Deployment::SubcomponentPortEndpoint &src)
+	{
+	  CIAO_TRACE("SPE_Handler::get_spe - reverse");
+	  ACE_CString str;
+	  SID_Handler::IDREF.find_ref (src.instanceRef, str);
 
-            return SubcomponentPortEndpoint (src.portName.in (),
-                                             str.c_str ());
-          }
-        };
+	  return SubcomponentPortEndpoint (src.portName.in (),
+					   str.c_str ());
+	}
+      };
 
-        typedef Sequence_Handler < SubcomponentPortEndpoint,
-                                   ::Deployment::SubcomponentPortEndpoints,
-                                   ::Deployment::SubcomponentPortEndpoint,
-                                   SPE_Handler::handle_spe > SPE_Functor;
+      typedef Sequence_Handler < SubcomponentPortEndpoint,
+				 ::Deployment::SubcomponentPortEndpoints,
+				 ::Deployment::SubcomponentPortEndpoint,
+				 SPE_Handler::handle_spe > SPE_Functor;
 
-        struct ACD_Handler
-        {
-          static void handle_acd (const AssemblyConnectionDescription &desc,
-                               ::Deployment::AssemblyConnectionDescription &toconfig)
-          {
-            CIAO_TRACE("ACD_Handler::get_acd");
+      struct Packaging_Handlers_Export ACD_Handler
+      {
+	static void handle_acd (const AssemblyConnectionDescription &desc,
+				::Deployment::AssemblyConnectionDescription &toconfig)
+	{
+	  CIAO_TRACE("ACD_Handler::get_acd");
 
-            toconfig.name = desc.name ().c_str ();
+	  toconfig.name = desc.name ().c_str ();
 
-            toconfig.deployRequirement.length (desc.count_deployRequirement ());
-            std::for_each (desc.begin_deployRequirement (),
-                           desc.end_deployRequirement (),
-                           Requirement_Functor (toconfig.deployRequirement));
+	  toconfig.deployRequirement.length (desc.count_deployRequirement ());
+	  std::for_each (desc.begin_deployRequirement (),
+			 desc.end_deployRequirement (),
+			 Requirement_Functor (toconfig.deployRequirement));
 
-            toconfig.internalEndpoint.length (desc.count_internalEndpoint ());
-	    SEQ_HAND_GCC_BUG_WORKAROUND (SPE_Handler::handle_spe,
-					 desc.begin_internalEndpoint (),
-					 toconfig.internalEndpoint);
-            std::for_each (desc.begin_internalEndpoint (),
-                           desc.end_internalEndpoint (),
-                           SPE_Functor (toconfig.internalEndpoint));
+	  toconfig.internalEndpoint.length (desc.count_internalEndpoint ());
+	  SEQ_HAND_GCC_BUG_WORKAROUND (SPE_Handler::handle_spe,
+				       desc.begin_internalEndpoint (),
+				       toconfig.internalEndpoint);
+	  std::for_each (desc.begin_internalEndpoint (),
+			 desc.end_internalEndpoint (),
+			 SPE_Functor (toconfig.internalEndpoint));
 
-            toconfig.externalEndpoint.length (desc.count_externalEndpoint ());
-            std::for_each (desc.begin_externalEndpoint (),
-                           desc.end_externalEndpoint (),
-                           CEPE_Functor (toconfig.externalEndpoint));
+	  toconfig.externalEndpoint.length (desc.count_externalEndpoint ());
+	  std::for_each (desc.begin_externalEndpoint (),
+			 desc.end_externalEndpoint (),
+			 CEPE_Functor (toconfig.externalEndpoint));
 
-            toconfig.externalReference.length (desc.count_externalReference ());
-            std::for_each (desc.begin_externalReference (),
-                           desc.end_externalReference (),
-                           ERE_Functor (toconfig.externalReference));
+	  toconfig.externalReference.length (desc.count_externalReference ());
+	  std::for_each (desc.begin_externalReference (),
+			 desc.end_externalReference (),
+			 ERE_Functor (toconfig.externalReference));
 
-          }
+	}
 
-          static AssemblyConnectionDescription
-          get_acd (const ::Deployment::AssemblyConnectionDescription &src)
-          {
-            CIAO_TRACE("ACD_Handler::get_acd - reverse");
+	static AssemblyConnectionDescription
+	get_acd (const ::Deployment::AssemblyConnectionDescription &src)
+	{
+	  CIAO_TRACE("ACD_Handler::get_acd - reverse");
 
-            AssemblyConnectionDescription retval (src.name.in ());
+	  AssemblyConnectionDescription retval (src.name.in ());
 
-            for (CORBA::ULong i = 0; i < src.deployRequirement.length (); ++i)
-              retval.add_deployRequirement
-                (Req_Handler::get_requirement (src.deployRequirement[i]));
+	  for (CORBA::ULong i = 0; i < src.deployRequirement.length (); ++i)
+	    retval.add_deployRequirement
+	      (Req_Handler::get_requirement (src.deployRequirement[i]));
 
-            for (CORBA::ULong i = 0; i < src.internalEndpoint.length (); ++i)
-              retval.add_internalEndpoint
-                (SPE_Handler::get_spe (src.internalEndpoint[i]));
+	  for (CORBA::ULong i = 0; i < src.internalEndpoint.length (); ++i)
+	    retval.add_internalEndpoint
+	      (SPE_Handler::get_spe (src.internalEndpoint[i]));
 
-            for (CORBA::ULong i = 0; i < src.externalEndpoint.length (); ++i)
-              retval.add_externalEndpoint
-                (CEPE_Handler::external_port_endpoint (src.externalEndpoint[i]));
+	  for (CORBA::ULong i = 0; i < src.externalEndpoint.length (); ++i)
+	    retval.add_externalEndpoint
+	      (CEPE_Handler::external_port_endpoint (src.externalEndpoint[i]));
 
-            for (CORBA::ULong i = 0; i < src.externalReference.length (); ++i)
-              retval.add_externalReference
-                (ERE_Handler::external_ref_endpoint (src.externalReference[i]));
+	  for (CORBA::ULong i = 0; i < src.externalReference.length (); ++i)
+	    retval.add_externalReference
+	      (ERE_Handler::external_ref_endpoint (src.externalReference[i]));
 
-            return retval;
-          }
-        };
+	  return retval;
+	}
+      };
 
-        typedef Sequence_Handler < AssemblyConnectionDescription,
-                                   ::Deployment::AssemblyConnectionDescriptions,
-                                   ::Deployment::AssemblyConnectionDescription,
-                                   ACD_Handler::handle_acd > ACD_Functor;
+      typedef Sequence_Handler < AssemblyConnectionDescription,
+				 ::Deployment::AssemblyConnectionDescriptions,
+				 ::Deployment::AssemblyConnectionDescription,
+				 ACD_Handler::handle_acd > ACD_Functor;
 
 
-        struct SPR_Handler
-        {
-          static void handle_spr (const SubcomponentPropertyReference &desc,
-                               ::Deployment::SubcomponentPropertyReference &toconfig)
-          {
-            CIAO_TRACE("SPR_Handler::get_spr");
+      struct Packaging_Handlers_Export SPR_Handler
+      {
+	static void handle_spr (const SubcomponentPropertyReference &desc,
+				::Deployment::SubcomponentPropertyReference &toconfig)
+	{
+	  CIAO_TRACE("SPR_Handler::get_spr");
 
-            toconfig.propertyName = desc.propertyName ().c_str ();
+	  toconfig.propertyName = desc.propertyName ().c_str ();
 
-            ACE_CString str (desc.instance ().id ().c_str ());
-            CORBA::ULong pos (0);
+	  ACE_CString str (desc.instance ().id ().c_str ());
+	  CORBA::ULong pos (0);
 
-            SID_Handler::IDREF.find_ref (str, pos);
-            toconfig.instanceRef = pos;
+	  SID_Handler::IDREF.find_ref (str, pos);
+	  toconfig.instanceRef = pos;
 
-          }
+	}
 
-          static SubcomponentPropertyReference
-          get_spr (const ::Deployment::SubcomponentPropertyReference &src)
-          {
-            CIAO_TRACE("SPR_HAndler::get_spr - reverse");
-            ACE_CString str;
-            SID_Handler::IDREF.find_ref (src.instanceRef, str);
+	static SubcomponentPropertyReference
+	get_spr (const ::Deployment::SubcomponentPropertyReference &src)
+	{
+	  CIAO_TRACE("SPR_HAndler::get_spr - reverse");
+	  ACE_CString str;
+	  SID_Handler::IDREF.find_ref (src.instanceRef, str);
 
-            return SubcomponentPropertyReference (src.propertyName.in (),
-                                                  str.c_str ());
-          }
+	  return SubcomponentPropertyReference (src.propertyName.in (),
+						str.c_str ());
+	}
 
-        };
+      };
 
-        typedef Sequence_Handler < SubcomponentPropertyReference,
-                                   ::Deployment::SubcomponentPropertyReferences,
-                                   ::Deployment::SubcomponentPropertyReference,
-                                   SPR_Handler::handle_spr > SPR_Functor;
+      typedef Sequence_Handler < SubcomponentPropertyReference,
+				 ::Deployment::SubcomponentPropertyReferences,
+				 ::Deployment::SubcomponentPropertyReference,
+				 SPR_Handler::handle_spr > SPR_Functor;
 
-        struct APM_Handler
-        {
-          static void handle_apm (const AssemblyPropertyMapping &desc,
-                               ::Deployment::AssemblyPropertyMapping &toconfig)
-          {
-            CIAO_TRACE("APM_Handler::get_apm");
+      struct Packaging_Handlers_Export APM_Handler
+      {
+	static void handle_apm (const AssemblyPropertyMapping &desc,
+				::Deployment::AssemblyPropertyMapping &toconfig)
+	{
+	  CIAO_TRACE("APM_Handler::get_apm");
 
-            toconfig.name = desc.name ().c_str ();
-            toconfig.externalName = desc.externalName ().c_str ();
+	  toconfig.name = desc.name ().c_str ();
+	  toconfig.externalName = desc.externalName ().c_str ();
 
-            toconfig.delegatesTo.length (desc.count_delegatesTo ());
-	    SEQ_HAND_GCC_BUG_WORKAROUND (SPR_Handler::handle_spr,
-					 desc.begin_delegatesTo (),
-					 toconfig.delegatesTo);
-            std::for_each (desc.begin_delegatesTo (),
-                           desc.end_delegatesTo (),
-                           SPR_Functor (toconfig.delegatesTo));
-          }
+	  toconfig.delegatesTo.length (desc.count_delegatesTo ());
+	  SEQ_HAND_GCC_BUG_WORKAROUND (SPR_Handler::handle_spr,
+				       desc.begin_delegatesTo (),
+				       toconfig.delegatesTo);
+	  std::for_each (desc.begin_delegatesTo (),
+			 desc.end_delegatesTo (),
+			 SPR_Functor (toconfig.delegatesTo));
+	}
 
-          static AssemblyPropertyMapping
-          get_apm (const ::Deployment::AssemblyPropertyMapping &src)
-          {
-            CIAO_TRACE("APM_Handler::get_apm - reverse");
+	static AssemblyPropertyMapping
+	get_apm (const ::Deployment::AssemblyPropertyMapping &src)
+	{
+	  CIAO_TRACE("APM_Handler::get_apm - reverse");
 
-            AssemblyPropertyMapping retval (src.name.in (),
-                                            src.externalName.in ());
+	  AssemblyPropertyMapping retval (src.name.in (),
+					  src.externalName.in ());
 
-            for (CORBA::ULong i = 0; i < src.delegatesTo.length (); ++i)
-              retval.add_delegatesTo (SPR_Handler::get_spr (src.delegatesTo[i]));
+	  for (CORBA::ULong i = 0; i < src.delegatesTo.length (); ++i)
+	    retval.add_delegatesTo (SPR_Handler::get_spr (src.delegatesTo[i]));
 
-            return retval;
-          }
-        };
+	  return retval;
+	}
+      };
 
-        typedef Sequence_Handler < AssemblyPropertyMapping,
-                                   ::Deployment::AssemblyPropertyMappings,
-                                   ::Deployment::AssemblyPropertyMapping,
-                                   APM_Handler::handle_apm > APM_Functor;
-      }
-
-
+      typedef Sequence_Handler < AssemblyPropertyMapping,
+				 ::Deployment::AssemblyPropertyMappings,
+				 ::Deployment::AssemblyPropertyMapping,
+				 APM_Handler::handle_apm > APM_Functor;
 
       void
       CAD_Handler::component_assem_descr (const ComponentAssemblyDescription &desc,
