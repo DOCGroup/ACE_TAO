3f6ab18e7eec0202024362e05c7efd3f2285ca7d
wotte <wotte@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-30 15:54:37 +0000

    branching/tagging

---
+++ branches/test_branch_one/modules/CIAO/CCF/CCF/CIDL/SemanticAction/Elements.hpp
@@ -0,0 +1,23 @@
+// file      : CCF/CIDL/SemanticAction/Elements.hpp
+// author    : Boris Kolpackov <boris@dre.vanderbilt.edu>
+// cvs-id    : $Id$
+
+#ifndef CCF_CIDL_SEMANTIC_ACTION_ELEMENTS_HPP
+#define CCF_CIDL_SEMANTIC_ACTION_ELEMENTS_HPP
+
+#include "CCF/IDL3/SemanticAction/Elements.hpp"
+
+#include "CCF/CIDL/Token.hpp"
+
+namespace CCF
+{
+  namespace CIDL
+  {
+    namespace SemanticAction
+    {
+      using IDL3::SemanticAction::Scope;
+    }
+  }
+}
+
+#endif  // CCF_CIDL_SEMANTIC_ACTION_ELEMENTS_HPP
