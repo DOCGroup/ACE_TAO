2242c964f538a521694fef7a9fcc569625f55845
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-01-31 22:09:20 +0000

    adding DS library

--- modules/TAO/tao/DiffServPolicy/DiffServ_Protocols_Hooks.h
+++ modules/TAO/tao/DiffServPolicy/DiffServ_Protocols_Hooks.h
@@ -42,7 +42,7 @@ public:
   /// Initialize the protocols hooks instance.
   void init_hooks (TAO_ORB_Core *orb_core);
 
-  const CORBA::Long get_dscp_codepoint (void);
+  const CORBA::Long get_dscp_codepoint (TAO_Stub *stub, CORBA::Object *object);
 
   void set_dscp_codepoint (CORBA::Long dscp_codepoint);
 
