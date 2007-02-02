1d1192b2afd7f88c81db101296d7ee9ce29f8a95
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-02-02 17:53:36 +0000

    adding DS library

---
+++ modules/TAO/tao/DiffServPolicy/DiffServ_Network_Priority_Hook.h
@@ -0,0 +1,49 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    Diffserv_Network_Priority_Hook.h
+ *
+ *  $Id$
+ *
+ *  @author  Jaiganesh Balasubramanian <jai@dre.vanderbilt.edu>
+ *           Johnny Willemsen <jwillemsen@remedy.nl>
+ */
+//=============================================================================
+
+#ifndef TAO_DIFFSERV_NETWORK_PRIORITY_HOOK_H
+#define TAO_DIFFSERV_NETWORK_PRIORITY_HOOK_H
+
+#include /**/ "ace/pre.h"
+#include "ace/Service_Config.h"
+
+#include "tao/DiffServPolicy/DiffServPolicy_Export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/PortableServer/Network_Priority_Hook.h"
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+class TAO_DiffServPolicy_Export TAO_DiffServ_Network_Priority_Hook
+  : public TAO_Network_Priority_Hook
+{
+public:
+  virtual ~TAO_DiffServ_Network_Priority_Hook(void);
+
+  void update_network_priority (TAO_Root_POA &poa, 
+                                TAO_POA_Policy_Set &poa_policy_set);
+};
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+ACE_STATIC_SVC_DECLARE_EXPORT (TAO_DiffServPolicy,
+                               TAO_DiffServ_Network_Priority_Hook)
+ACE_FACTORY_DECLARE (TAO_DiffServPolicy, TAO_DiffServ_Network_Priority_Hook)
+
+
+#include /**/ "ace/post.h"
+
+#endif /* TAO_DIFFSERV_NETWORK_PRIORITY_HOOK_H */
