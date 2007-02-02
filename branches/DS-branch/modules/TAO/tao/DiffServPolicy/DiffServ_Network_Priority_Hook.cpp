a46aa699edfeabd3eff6a256259fb62efdd33d2f
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-02-02 17:53:36 +0000

    adding DS library

---
+++ branches/DS-branch/modules/TAO/tao/DiffServPolicy/DiffServ_Network_Priority_Hook.cpp
@@ -0,0 +1,63 @@
+// @(#) $Id$
+
+#include "tao/DiffServPolicy/DiffServ_Network_Priority_Hook.h"
+#include "tao/PortableServer/Root_POA.h"
+#include "tao/PortableServer/POA_Policy_Set.h"
+#include "tao/DiffServPolicy/Server_Network_Priority_Policy.h"
+#include "tao/DiffServPolicy/DiffServPolicyC.h"
+
+ACE_RCSID(DiffServPolicy,
+          DiffServ_Network_Priority_Hook,
+          "$Id$")
+
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+
+TAO_DiffServ_Network_Priority_Hook::~TAO_DiffServ_Network_Priority_Hook(void)
+{
+}
+
+void
+TAO_DiffServ_Network_Priority_Hook::update_network_priority (
+  TAO_Root_POA &poa, TAO_POA_Policy_Set &policy_set)
+{
+  for (CORBA::ULong i = 0; i < policy_set.num_policies (); i++)
+    {
+      CORBA::Policy_var policy = policy_set.get_policy_by_index (i);
+      ::TAO::NetworkPriorityPolicy_var npp
+        = ::TAO::NetworkPriorityPolicy::_narrow (policy);
+
+      if (!CORBA::is_nil (npp.in ()))
+        {
+          TAO::NetworkPriorityModel network_priority_model =
+            npp->network_priority_model ();
+
+          poa.cached_policies ().network_priority_model (
+            TAO::Portable_Server::Cached_Policies::NetworkPriorityModel (
+              network_priority_model));
+
+          TAO::DiffservCodepoint request_diffserv_codepoint  =
+            npp->request_diffserv_codepoint ();
+
+          TAO::DiffservCodepoint reply_diffserv_codepoint  =
+            npp->reply_diffserv_codepoint ();
+
+          poa.cached_policies ().request_diffserv_codepoint (
+            request_diffserv_codepoint);
+          poa.cached_policies ().reply_diffserv_codepoint (
+            reply_diffserv_codepoint);
+        }
+    }
+}
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+ACE_FACTORY_DEFINE (TAO_DiffServPolicy, TAO_DiffServ_Network_Priority_Hook)
+ACE_STATIC_SVC_DEFINE (TAO_DiffServ_Network_Priority_Hook,
+                       ACE_TEXT ("TAO_Network_Priority_Hook"),
+                       ACE_SVC_OBJ_T,
+                       &ACE_SVC_NAME (TAO_DiffServ_Network_Priority_Hook),
+                       ACE_Service_Type::DELETE_THIS
+                       | ACE_Service_Type::DELETE_OBJ,
+                       0)
