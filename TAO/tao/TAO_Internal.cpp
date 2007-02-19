4deeac21460f9e9b462155a243d517e805084c55
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-02-19 14:36:02 +0000

    adding DS library

--- TAO/tao/TAO_Internal.cpp
+++ TAO/tao/TAO_Internal.cpp
@@ -17,6 +17,7 @@
 #include "tao/Default_Stub_Factory.h"
 #include "tao/Default_Endpoint_Selector_Factory.h"
 #include "tao/Default_Protocols_Hooks.h"
+#include "tao/Default_Network_Priority_Protocols_Hooks.h"
 #include "tao/Default_Thread_Lane_Resources_Manager.h"
 #include "tao/Default_Collocation_Resolver.h"
 #include "tao/Codeset_Manager_Factory_Base.h"
@@ -539,6 +540,8 @@ namespace
     pcfg->process_directive (
       ace_svc_desc_TAO_Default_Endpoint_Selector_Factory);
     pcfg->process_directive (ace_svc_desc_TAO_Default_Protocols_Hooks);
+    pcfg->process_directive (
+      ace_svc_desc_TAO_Default_Network_Priority_Protocols_Hooks);
     pcfg->process_directive (
       ace_svc_desc_TAO_Default_Thread_Lane_Resources_Manager_Factory);
     pcfg->process_directive (ace_svc_desc_TAO_Default_Collocation_Resolver);
