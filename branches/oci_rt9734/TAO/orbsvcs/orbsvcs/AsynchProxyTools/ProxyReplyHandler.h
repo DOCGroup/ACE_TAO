c8cbe23a65721d43c401dfeed40fe3a2b2a28ad6
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-02-28 22:15:47 +0000

    Wed Feb 28 22:14:41 UTC 2007  Phil Mesnier  <mesnier_p@ociweb.com>

--- branches/oci_rt9734/TAO/orbsvcs/orbsvcs/AsynchProxyTools/ProxyReplyHandler.h
+++ branches/oci_rt9734/TAO/orbsvcs/orbsvcs/AsynchProxyTools/ProxyReplyHandler.h
@@ -1,23 +1,36 @@
+// -*- C++ -*-
+
 //
 // $Id$
 //
+
+#ifndef TAO_PROXY_REPLY_HANDLER_H
+#define TAO_PROXY_REPLY_HANDLER_H
+
+#include "orbsvcs/AsynchProxyTools/AsynchProxyTools_Export.h"
+
+
 #include "tao/DynamicInterface/DII_Reply_Handler.h"
 #include "tao/DynamicInterface/Server_Request.h"
 #include "tao/DynamicInterface/AMH_DSI_Response_Handler.h"
 #include "tao/DynamicInterface/Dynamic_Implementation.h"
 #include "tao/DynamicInterface/Request.h"
-class ProxyReplyHandler : public virtual TAO_DII_Reply_Handler
-                        , public virtual Messaging::ReplyHandler
+
+class TAO_AsynchProxyTools_Export TAO_ProxyReplyHandler :
+  public virtual TAO_DII_Reply_Handler,
+  public virtual Messaging::ReplyHandler
 {
   public:
-    ProxyReplyHandler ();
-    virtual ~ProxyReplyHandler ();
+    TAO_ProxyReplyHandler ();
+    virtual ~TAO_ProxyReplyHandler ();
 
   // Callback method for deferred synchronous requests.
   virtual void handle_response (TAO_InputCDR &incoming);
   virtual void handle_excep (TAO_InputCDR &incoming,
                              CORBA::ULong reply_status);
 
-  virtual handle_response_i () = 0;
-  virtual handle_excep_i () = 0;
+  virtual void handle_response_i () = 0;
+  virtual void handle_excep_i () = 0;
 };
+
+#endif // TAO_PROXY_REPLY_HANDLER_H
--- branches/oci_rt9734/TAO/orbsvcs/orbsvcs/AsynchProxyTools/ProxyServant.h
+++ branches/oci_rt9734/TAO/orbsvcs/orbsvcs/AsynchProxyTools/ProxyServant.h
@@ -1,11 +1,27 @@
+// -*- C++ -*-
+
 //
 //$Id$
 //
-class ProxyServant : public virtual TAO_DynamicImplementation
+
+#ifndef TAO_PROXY_SERVANT
+#define TAO_PROXY_SERVANT
+
+#include "tao/DynamicInterface/Dynamic_Implementation.h"
+#include "orbsvcs/AsynchProxyTools/AsynchProxyTools_Export.h"
+
+class TAO_AMH_DSI_Response_Handler;
+typedef TAO_AMH_DSI_Response_Handler * TAO_AMH_DSI_Response_Handler_ptr;
+
+class TAO_AsynchProxyTools_Export TAO_ProxyServant :
+  public virtual TAO_DynamicImplementation
 {
 public:
-  ProxyServant();
+  TAO_ProxyServant();
   void invoke (CORBA::ServerRequest_ptr request,
                TAO_AMH_DSI_Response_Handler_ptr rh);
   virtual void invoke_i () = 0;
 };
+
+
+#endif // TAO_PROXY_SERVANT
