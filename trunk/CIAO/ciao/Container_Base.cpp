f9d089a1d075532e273f8dac37c5640a50121536
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-02-28 22:20:28 +0000

    ChangeLogTag: Wed Feb 28 22:19:37 UTC 2007  Jaiganesh B  <jai.dre.vanderbilt.edu>

--- trunk/CIAO/ciao/Container_Base.cpp
+++ trunk/CIAO/ciao/Container_Base.cpp
@@ -33,6 +33,19 @@ namespace CIAO
       }
   }
 
+  CORBA::PolicyList
+  Container::get_receptacle_policy (const char* name)
+  {
+    CORBA::PolicyList policy_list;
+    if (this->rec_pol_map_.find (name, policy_list) != 0)
+      {
+        ACE_DEBUG ((LM_DEBUG, "Unable to find policies "
+                    "for the receptacle %s\n", name));
+        policy_list.length (0);
+      }
+    return policy_list;
+  }
+
   PortableServer::POA_ptr
   Container::the_POA (void) const
   {
