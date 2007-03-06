714c3bebde17b9af60ef287bd0579d3fca45d88f
Johnny Willemsen <jwillemsen@remedy.nl>
2007-03-06 09:04:02 +0000

    Tue Mar  6 09:03:32 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/POA/Nested_Non_Servant_Upcalls/run_test.pl
+++ TAO/tests/POA/Nested_Non_Servant_Upcalls/run_test.pl
@@ -15,7 +15,7 @@ else {
     $T = new PerlACE::Process ("Nested_Non_Servant_Upcalls");
 }
 
-$test = $T->SpawnWaitKill (60);
+$test = $T->SpawnWaitKill ($PerlACE::wait_interval_for_process_creation);
 
 if ($test != 0) {
     print STDERR "ERROR: test returned $test\n";
