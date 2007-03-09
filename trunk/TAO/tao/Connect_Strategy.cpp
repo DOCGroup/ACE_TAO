e66f9c25bea65e80a55ae769d41fdd81dff8abd1
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-03-09 13:01:34 +0000

    Fri Mar 9 12:55:12 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Connect_Strategy.cpp
+++ trunk/TAO/tao/Connect_Strategy.cpp
@@ -26,12 +26,11 @@ TAO_Connect_Strategy::wait (TAO_Connection_Handler *ch,
   if (ch == 0)
     return -1;
 
-  return this->wait_i (ch, ch->transport (),max_wait_time);
+  return this->wait_i (ch, ch->transport (), max_wait_time);
 }
 
 int
-TAO_Connect_Strategy::wait (TAO_Transport *t,
-                            ACE_Time_Value *max_wait_time)
+TAO_Connect_Strategy::wait (TAO_Transport *t, ACE_Time_Value *max_wait_time)
 {
   // Basically the connection was EINPROGRESS, but before we could
   // wait for it some other thread detected a failure and cleaned up
@@ -39,21 +38,21 @@ TAO_Connect_Strategy::wait (TAO_Transport *t,
   if (t == 0)
     return -1;
 
-  return this->wait_i (t->connection_handler(),t,max_wait_time);
+  return this->wait_i (t->connection_handler (), t, max_wait_time);
 }
 
 int
 TAO_Connect_Strategy::wait (TAO_LF_Multi_Event *mev,
                             ACE_Time_Value *max_wait_time)
 {
-  return this->wait_i (mev, mev->base_transport(), max_wait_time);
+  return this->wait_i (mev, mev->base_transport (), max_wait_time);
 }
 
 int
 TAO_Connect_Strategy::poll (TAO_LF_Multi_Event *mev)
 {
-  ACE_Time_Value zero(ACE_Time_Value::zero);
-  return this->wait_i (mev, mev->base_transport(), &zero);
+  ACE_Time_Value zero (ACE_Time_Value::zero);
+  return this->wait_i (mev, mev->base_transport (), &zero);
 }
 
 TAO_END_VERSIONED_NAMESPACE_DECL
