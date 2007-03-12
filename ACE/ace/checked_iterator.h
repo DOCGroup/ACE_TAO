029163aebb28bac03a46348c302d8c9c60eaf6c0
Ossama Othman <ossama-othman@users.noreply.github.com>
2007-03-12 22:47:03 +0000

    ChangeLogTag:Mon Mar 12 23:44:25 UTC 2007  Ossama Othman  <ossama_othman at symantec dot com>

--- ACE/ace/checked_iterator.h
+++ ACE/ace/checked_iterator.h
@@ -8,6 +8,21 @@
  *
  * @brief Checked iterator factory function.
  *
+ * Some compilers (e.g. MSVC++ >= 8) issue security related
+ * diagnostics if algorithms such as std::copy() are used in an unsafe
+ * way.  Normally this isn't an issue if STL container iterators are
+ * used in conjuction with the standard algorithms.  However, in cases
+ * where application-specific iterators are use with standard
+ * algorithms that could potentially overrun a buffer, extra care must
+ * be taken to prevent such an overrun.  If supported, checked
+ * iterators can be used to address the potential destination buffer
+ * overrun.
+ *
+ * This header provides function templates that generate the
+ * appropriate checked iterator.  In cases where checked iterators are
+ * not supported, the pointer passed to the function is returned
+ * instead.
+ *
  * $Id$
  *
  * @author Ossama Othman
@@ -21,15 +36,17 @@
 # if defined (_MSC_VER) && (_MSC_VER >= 1400)
 template <typename PTR>
 stdext::checked_array_iterator<PTR>
-ACE_make_checked_array_iterator (PTR buf, ptrdiff_t len)
+ACE_make_checked_array_iterator (PTR buf, size_t len)
 {
   return stdext::checked_array_iterator<PTR> (buf, len);
 }
 # else
 template <typename PTR>
 PTR
-ACE_make_checked_array_iterator (PTR buf, ptrdiff_t /* len */)
+ACE_make_checked_array_iterator (PTR buf, size_t /* len */)
 {
+  // Checked iterators are unsupported.  Just return the pointer to
+  // the buffer itself.
   return buf;
 }
 # endif  /* _MSC_VER >= 1400 */
