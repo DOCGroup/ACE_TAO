e8ebc9f2fb9cf6d23e7921a8e6c9ecb68f097833
William R. Otte <wotte@dre.vanderbilt.edu>
2007-03-22 16:37:03 +0000

    ChangeLogTag:Thu Mar 22 16:29:16 UTC 2007  William R. Otte  <wotte@dre.vanderbilt.edu>

---
+++ ACE/bin/MakeProjectCreator/modules/GNUAutobuildProjectCreator.pm
@@ -0,0 +1,23 @@
+package GNUAutobuildProjectCreator;
+
+# ************************************************************
+# Description   : A Project creator for the GNUAutobuild project type
+# Author        : Chad Elliott
+# Create Date   : 3/13/2002
+# ************************************************************
+
+# ************************************************************
+# Pragmas
+# ************************************************************
+
+use strict;
+use File::Basename;
+
+use GNUACEProjectCreator;
+use MakeProjectBase;
+use ProjectCreator;
+
+use vars qw(@ISA);
+@ISA = qw(GNUACEProjectCreator MakeProjectBase ProjectCreator);
+
+1;
