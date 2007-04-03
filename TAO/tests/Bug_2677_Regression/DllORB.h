b5b35973dcd888f7853b9093e936a554399a00ad
elliott_c <ocielliottc@users.noreply.github.com>
2007-04-03 18:30:33 +0000

    ChangeLogTag: Tue Apr  3 18:29:29 UTC 2007  Chad Elliott  <elliott_c@ociweb.com>

--- TAO/tests/Bug_2677_Regression/DllORB.h
+++ TAO/tests/Bug_2677_Regression/DllORB.h
@@ -2,16 +2,12 @@
 #ifndef DLLORB_H
 #define DLLORB_H
 
-#include "ace/Task.h"
+#include "ace/Service_Object.h"
 #include "tao/ORB.h"
 #include "tao/PortableServer/PortableServer.h"
 #include "bug_2677_regression_export.h"
 
-ACE_BEGIN_VERSIONED_NAMESPACE_DECL
-class ACE_Thread_Barrier;
-ACE_END_VERSIONED_NAMESPACE_DECL
-
-class bug_2677_regression_Export DllORB: public ACE_Task_Base
+class bug_2677_regression_Export DllORB: public ACE_Service_Object
 {
 public:
   DllORB (void);
@@ -23,12 +19,8 @@ public:
 
   virtual int fini (void);
 
-protected:
-  virtual int svc (void);
-
 private:
   unsigned int                   failPrePostInit_;
-  ACE_Thread_Barrier*            mp_barrier_;
   CORBA::ORB_var                 mv_orb_;
   PortableServer::POA_var        mv_rootPOA_;
   PortableServer::POAManager_var mv_poaManager_;
