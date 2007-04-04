64aa4d9be60ec8b03fce363798e49ad06d118932
zhangw <zhangw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-04-04 14:50:13 +0000

    Wed Apr  4 14:42:01 UTC 2007  Wallace Zhang  <zhang_w@ociweb.com>

--- TAO/tao/IFR_Client_Adapter.cpp
+++ TAO/tao/IFR_Client_Adapter.cpp
@@ -6,3 +6,11 @@ ACE_RCSID (tao,
            IFR_Client_Adapter,
            "$Id$")
 
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+TAO_IFR_Client_Adapter::~TAO_IFR_Client_Adapter ()
+{
+}
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
