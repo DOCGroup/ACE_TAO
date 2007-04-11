049832471d14d01b6897c540c27a55b3a2240f58
elliott_c <elliott_c@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-04-11 12:35:40 +0000

    ChangeLogTag: Wed Apr 11 12:33:37 UTC 2007  Chad Elliott  <elliott_c@ociweb.com>

--- ACE/ace/SOCK_Dgram.cpp
+++ ACE/ace/SOCK_Dgram.cpp
@@ -350,7 +350,7 @@ ACE_SOCK_Dgram::send (const iovec iov[],
 
   // Determine the total length of all the buffers in <iov>.
   for (i = 0; i < n; i++)
-#if ! (defined(__BORLANDC__) || defined(linux) || defined(__RTEMS_MAJOR__))
+#if ! (defined(__BORLANDC__) || defined(linux) || defined(ACE_HAS_RTEMS))
     // The iov_len is unsigned on Linux, RTEMS and with Borland. If we go
     // ahead and try the if, it will emit a warning.
     if (iov[i].iov_len < 0)
@@ -399,7 +399,7 @@ ACE_SOCK_Dgram::recv (iovec iov[],
   int i;
 
   for (i = 0; i < n; i++)
-#if ! (defined(__BORLANDC__) || defined(linux) || defined(__RTEMS_MAJOR__))
+#if ! (defined(__BORLANDC__) || defined(linux) || defined(ACE_HAS_RTEMS))
     // The iov_len is unsigned on Linux, RTEMS and with Borland. If we go
     // ahead and try the if, it will emit a warning.
     if (iov[i].iov_len < 0)
