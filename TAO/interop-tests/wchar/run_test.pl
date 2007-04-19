e994589779a9df72f88c6e407b9ae4990fd01620
Johnny Willemsen <jwillemsen@remedy.nl>
2007-04-19 12:35:24 +0000

    Thu Apr 19 12:34:12 UTC 2007 Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/interop-tests/wchar/run_test.pl
+++ TAO/interop-tests/wchar/run_test.pl
@@ -17,7 +17,7 @@ $CL = new PerlACE::Process ("client", "-k file://$iorfile -x");
 
 $SV->Spawn ();
 
-if (PerlACE::waitforfile_timed ($iorfile, 5) == -1) {
+if (PerlACE::waitforfile_timed ($iorfile, $PerlACE::wait_interval_for_process_creation) == -1) {
     print STDERR "ERROR: cannot find file <$iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
