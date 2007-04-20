13e10bfa0fbf134b64ee0ab5b192199b86137a08
Johnny Willemsen <jwillemsen@remedy.nl>
2007-04-20 11:42:55 +0000

    Fri Apr 20 11:38:12 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/Bug_1330_Regression/run_test.pl
+++ TAO/tests/Bug_1330_Regression/run_test.pl
@@ -46,9 +46,9 @@ if (PerlACE::waitforfile_timed ($server_ior_file, $PerlACE::wait_interval_for_pr
 # Try the corbaloc URL with incorrect '\' escaping of hex characters
 # We expect this to 'fail'
 $CLIENT->Arguments("-k corbaloc:iiop:$TARGETHOSTNAME:$port/Name\\2dwith\\2dhyphens");
-if ($CLIENT->SpawnWaitKill (30) == 0)
+if ($CLIENT->SpawnWaitKill (30) != 0)
 {
-   print STDERR "ERROR: Bug 1330 Regression failed. Incorrect escape characters accepted\n";
+   print STDERR "ERROR: Bug 1330 Regression failed. Correct escape characters accepted\n";
    $SERVER->Kill();
    exit 1;
 }
