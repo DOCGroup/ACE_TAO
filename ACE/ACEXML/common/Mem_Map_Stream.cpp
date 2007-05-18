735232bc256e11c73666cc1293783920fd24c89a
Abdullah Sowayan <sowayan@users.noreply.github.com>
2007-05-18 02:54:37 +0000

    Fri May 18 02:50:42 UTC 2007  Abdullah Sowayan  <abdullah.sowayan@lmco.com>

--- ACE/ACEXML/common/Mem_Map_Stream.cpp
+++ ACE/ACEXML/common/Mem_Map_Stream.cpp
@@ -116,7 +116,6 @@ ACEXML_Mem_Map_Stream::seek (ACE_OFF_T offset, int whence)
         this->end_of_mapping_plus1_ + offset;
       // @@ Not sure how to implement this (yet).
       ACE_NOTSUP_RETURN (-1);
-      break;
     }
 
   // Make sure that the backing store will cover this.
