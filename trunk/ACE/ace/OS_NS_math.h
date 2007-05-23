4a92568bc21f6e518b401672226979e3d519e761
ossama <ossama@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-05-23 04:35:10 +0000

    ChangeLogTag:Tue May 23 05:12:04 2007  Ossama Othman  <ossama othman at symantec dot com>

--- trunk/ACE/ace/OS_NS_math.h
+++ trunk/ACE/ace/OS_NS_math.h
@@ -34,6 +34,7 @@
 #endif
 #define ACE_EXPORT_MACRO ACE_Export
 
+
 /*
  * We inline and undef some functions that may be implemented
  * as macros on some platforms. This way macro definitions will
@@ -41,7 +42,6 @@
  * using the pre-processor.
  *
  */
-
 inline double ace_log2_helper (double x)
 {
 #if defined (log2)
@@ -51,10 +51,37 @@ inline double ace_log2_helper (double x)
 #  if !defined (ACE_LACKS_LOG2)
   return ACE_STD_NAMESPACE::log2 (x);
 #  else
-  return log (x) / log (2.0);
+  /*
+    ==================================================================
+
+                log (x)
+                   k
+      log (x) = -------
+         b      log (b)
+                   k
+
+    meaning the binary logarithm of x using the natural logarithm, for
+    example, is:
+
+
+                log (x)
+                   e
+      log (x) = -------
+         2      log (2)
+                   e
+
+    ==================================================================
+   */
+
+  // Precomputed value of 1/log(2.0).  Saves an expensive division and
+  // computing log(2.0) in each call.
+  double const _1_ln2 = 1.442695040888963407359924681002;
+
+  return log (x) * _1_ln2;
 #  endif /* !ACE_LACKS_LOG2 */
 #endif /* defined (log2) */
 }
+
 
 ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 
@@ -68,7 +95,7 @@ namespace ACE_OS
   ACE_NAMESPACE_INLINE_FUNCTION
   double ceil (double x);
 
-  /// This method computes base-2 logarithm of x
+  /// This method computes the base-2 logarithm of x.
   ACE_NAMESPACE_INLINE_FUNCTION
   double log2 (double x);
 
