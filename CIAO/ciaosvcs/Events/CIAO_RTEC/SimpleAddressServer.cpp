3af8822942e6b7b4125c68a48672f53607dd4f68
Phil Mesnier <mesnier_p@ociweb.com>
2007-05-29 21:18:14 +0000

    Tue May 29 21:02:08 UTC 2007  Phil Mesnier  <mesnier_p@ociweb.com>

--- CIAO/ciaosvcs/Events/CIAO_RTEC/SimpleAddressServer.cpp
+++ CIAO/ciaosvcs/Events/CIAO_RTEC/SimpleAddressServer.cpp
@@ -4,34 +4,46 @@
 #include <ace/INET_Addr.h>
 #include <ace/OS_NS_string.h>
 
-SimpleAddressServer::SimpleAddressServer (const ACE_INET_Addr& address) {
-  this->address_.ipaddr = address.get_ip_address ();
-  this->address_.port   = address.get_port_number ();
+SimpleAddressServer::SimpleAddressServer (const ACE_INET_Addr& address)
+{
+#if defined (ACE_HAS_IPV6)
+  if (address.get_type() == PF_INET6)
+    {
+      RtecUDPAdmin::UDP_Addr_v6 v6;
+      sockaddr_in6 *in6 =
+        reinterpret_cast<sockaddr_in6 *>(address.get_addr());
+      ACE_OS::memcpy (v6.ipaddr,&in6->sin6_addr,16);
+      v6.port = address.get_port_number();
+      this->address_.v6_addr (v6);
+      return;
+    }
+#endif /* ACE_HAS_IPV6 */
+  RtecUDPAdmin::UDP_Addr v4;
+  v4.ipaddr = address.get_ip_address ();
+  v4.port   = address.get_port_number ();
+  this->address_.v4_addr (v4);
 }
 
 SimpleAddressServer::SimpleAddressServer (const RtecUDPAdmin::UDP_Addr& addr)
-  : address_ (addr)
 {
+  this->address_.v4_addr (addr);
 }
 
 void
 SimpleAddressServer::get_addr (const RtecEventComm::EventHeader&,
                                RtecUDPAdmin::UDP_Addr& address)
-    throw (CORBA::SystemException) {
+    throw (CORBA::SystemException)
+{
+  if (this->address_._d() == RtecUDPAdmin::INET6)
+    throw CORBA::DATA_CONVERSION(0, CORBA::COMPLETED_YES);
+  address = this->address_.v4_addr();
+}
+
+
+void
+SimpleAddressServer::get_address (const RtecEventComm::EventHeader&,
+                                  RtecUDPAdmin::UDP_Address_out address)
+  throw (CORBA::SystemException)
+{
   address = this->address_;
 }
-/*
-void
-SimpleAddressServer::get_ip_address (const RtecEventComm::EventHeader&,
-                                     RtecUDPAdmin::UDP_IP_Address_out address)
-    throw (CORBA::SystemException) {
-  address = new RtecUDPAdmin::UDP_IP_Address;
-
-  ACE_INET_Addr x (this->address_.port,
-       static_cast<ACE_UINT32>(this->address_.ipaddr));
-  address->length (x.get_addr_size ());
-  ACE_OS::memcpy (address->get_buffer (),
-                  x.get_addr (),
-                  x.get_addr_size ());
-}
-*/
