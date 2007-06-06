93bf19c3ea08799dccf3318f200565b364b898e1
dengg <dengg@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-06-06 20:01:41 +0000

    Wed Jun  6 20:00:19 UTC 2007  Gan Deng  <gan.deng@vanderbilt.edu>

--- trunk/CIAO/examples/Null_Component/plan.h
+++ trunk/CIAO/examples/Null_Component/plan.h
@@ -6,6 +6,21 @@ extern "C" ::PortableServer::Servant create_Null_Component_Home_Servant
 		::CIAO::Session_Container *c,
 		const char*);
 
+// Below two extern "C++" is to fixing the "Anachronism" warning of Sun C++ 5.7
+
+extern "C++" ::Components::HomeExecutorBase_ptr create_Null_Component_Home_Impl_wrapper (void)
+{
+  return create_Null_Component_Home_Impl ();
+}
+
+extern "C++" ::PortableServer::Servant create_Null_Component_Home_Servant_wrapper
+		(::Components::HomeExecutorBase_ptr p,
+		::CIAO::Session_Container *c,
+		const char* no_op)
+{
+  return create_Null_Component_Home_Servant (p, c, no_op);
+}
+
 struct HomeAttributes
 {
   char const * component_instance_name_;
@@ -23,9 +38,9 @@ static const HomeAttributes homes_table[] =
   {
     "Null_Component_ComponentImplementations_Null_Assembly_Container_Null_Assembly_Null_Component",
     "create_Null_Component_Home_Impl",
-    create_Null_Component_Home_Impl,
+    create_Null_Component_Home_Impl_wrapper,
     "create_Null_Component_Home_Servant",
-    create_Null_Component_Home_Servant
+    create_Null_Component_Home_Servant_wrapper
   },
 };
 
