a659f7c618adc5e2b8681c8c5b09efb41242f28b
Abdullah Sowayan <sowayan@users.noreply.github.com>
2007-07-01 17:05:41 +0000

    Sun Jul  1 17:01:21 UTC 2007  Abdullah Sowayan  <abdullah.sowayan@lmco.com>

--- ACE/apps/drwho/PMS_All.cpp
+++ ACE/apps/drwho/PMS_All.cpp
@@ -8,6 +8,7 @@
 #include "ace/OS_NS_string.h"
 #include "ace/OS_NS_pwd.h"
 #include "ace/OS_NS_unistd.h"
+#include "ace/OS_NS_stdio.h"
 #include "ace/OS_Memory.h"
 
 // This function packs the located friends userids, plus the machines
