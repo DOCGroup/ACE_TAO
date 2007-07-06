7ebee21d470fb4ae62028a2903dfe1b714021a92
Steve Huston <shuston@riverace.com>
2007-07-06 15:14:47 +0000

    ChangeLogTag:Fri Jul  6 14:52:00 UTC 2007  Steve Huston  <shuston@riverace.com>

--- ACE/ace/os_include/os_semaphore.h
+++ ACE/ace/os_include/os_semaphore.h
@@ -59,13 +59,13 @@ extern "C"
      int new_sema_;
 #  endif /* ACE_LACKS_NAMED_POSIX_SEM */
 
-#  if !defined (ACE_HAS_POSIX_SEM_TIMEOUT) && defined (ACE_HAS_POSIX_SEM_TIMEOUT_EMULATION)
+#  if !defined (ACE_HAS_POSIX_SEM_TIMEOUT) && !defined (ACE_DISABLE_POSIX_SEM_TIMEOUT_EMULATION)
      /// Serialize access to internal state.
      ACE_mutex_t lock_;
 
      /// Block until there are no waiters.
      ACE_cond_t count_nonzero_;
-#  endif /* !ACE_HAS_POSIX_SEM_TIMEOUT && ACE_HAS_POSIX_SEM_TIMEOUT_EMULATION */
+#  endif /* !ACE_HAS_POSIX_SEM_TIMEOUT && !ACE_DISABLE_POSIX_SEM_TIMEOUT_EMULATION */
    } ACE_sema_t;
 #endif /* ACE_HAS_POSIX_SEM */
 
