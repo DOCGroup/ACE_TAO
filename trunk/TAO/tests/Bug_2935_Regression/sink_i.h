7704f32270a6baca56752928dc64175c9f6ae09b
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-08-02 03:16:58 +0000

    Thu Aug  2 03:15:32 UTC 2007  Phil Mesnier  <mesnier_p@ociweb.com>

--- trunk/TAO/tests/Bug_2935_Regression/sink_i.h
+++ trunk/TAO/tests/Bug_2935_Regression/sink_i.h
@@ -37,16 +37,11 @@ public:
   bool parse_args (int & argc, char * argv[]);
 
   ///Implement ThreeTier methods
-  virtual void tick ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
-  virtual void tock ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
-  virtual ::CORBA::ULong tickCount ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
-  virtual ::CORBA::ULong tockCount ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
-  virtual void shutdown ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
+  virtual void tick ();
+  virtual void tock ();
+  virtual ::CORBA::ULong tickCount ();
+  virtual ::CORBA::ULong tockCount ();
+  virtual void shutdown ();
 
 private:
   CORBA::ORB_var orb_;
