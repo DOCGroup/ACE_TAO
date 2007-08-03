d7de1bf9f54af621a47cc1c51f2e748a8d1cc6ea
sowayaa <sowayaa@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-08-03 21:36:19 +0000

    Fri Aug  3 21:34:40 UTC 2007  Abdullah Sowayan  <abdullah.sowayan@lmco.com>

--- trunk/ACE/contrib/minizip/crypt.h
+++ trunk/ACE/contrib/minizip/crypt.h
@@ -37,8 +37,8 @@ Encryption is not supported.
 static int decrypt_byte(unsigned long* pkeys, const unsigned long* pcrc_32_tab)
 {
   unsigned temp;  /* POTENTIAL BUG:  temp*(temp^1) may overflow in an
-		   * unpredictable manner on 16-bit systems; not a problem
-		   * with any known compiler so far, though */
+                   * unpredictable manner on 16-bit systems; not a problem
+                   * with any known compiler so far, though */
 
   MINIZIP_UNUSED_ARG(pcrc_32_tab);
 
