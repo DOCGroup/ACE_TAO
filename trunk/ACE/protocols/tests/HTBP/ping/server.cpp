0f80a96e45bf4ee22fc47ec67ac94ecfcc81ffff
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-08-15 12:17:01 +0000

    Wed Aug 15 12:15:16 UTC 2007  Phil Mesnier  <mesnier_p@ociweb.com>

--- trunk/ACE/protocols/tests/HTBP/ping/server.cpp
+++ trunk/ACE/protocols/tests/HTBP/ping/server.cpp
@@ -17,9 +17,9 @@
 unsigned port = 8088;
 
 int
-parse_args (int argc, char *argv[])
+parse_args (int argc, ACE_TCHAR *argv[])
 {
-  ACE_Get_Opt get_opts (argc, argv, "p:");
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("p:"));
   int c;
 
   while ((c = get_opts ()) != -1)
