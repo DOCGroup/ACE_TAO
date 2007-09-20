4737d360f196880a4806a993a7ff056bf550b7ab
Johnny Willemsen <jwillemsen@remedy.nl>
2007-09-20 07:13:20 +0000

    Thu Sep 20 07:12:15 UTC 2007  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tao/Objref_VarOut_T.h
+++ TAO/tao/Objref_VarOut_T.h
@@ -120,6 +120,10 @@ private:
 
 TAO_END_VERSIONED_NAMESPACE_DECL
 
+#if defined (__ACE_INLINE__)
+#include "tao/Objref_VarOut_T.inl"
+#endif /* __ACE_INLINE__ */
+
 #if defined (ACE_TEMPLATES_REQUIRE_SOURCE)
 #include "tao/Objref_VarOut_T.cpp"
 #endif /* ACE_TEMPLATES_REQUIRE_SOURCE */
