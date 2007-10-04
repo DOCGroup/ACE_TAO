58654e67bf8b45dd2462198cac62f426d4deabed
johnc <johnc@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-10-04 20:11:46 +0000

    ChangeLogTag: Thu Oct  4 20:05:47 UTC 2007  Ciju John  <johnc at ociweb dot com>

---
+++ TAO/orbsvcs/tests/Security/ssliop_CSD/Messenger_i.h
@@ -0,0 +1,32 @@
+/* -*- C++ -*- $Id$ */
+
+#ifndef MESSENGERI_H_
+#define MESSENGERI_H_
+
+#include "MessengerS.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+//Class Messenger_i
+class  Messenger_i : public virtual POA_Messenger
+{
+public:
+  //Constructor 
+  Messenger_i (void);
+  
+  //Destructor 
+  virtual ~Messenger_i (void);
+  
+virtual CORBA::Boolean send_message (
+    const char * user_name,
+    const char * subject,
+    char *& message
+  )
+  throw(CORBA::SystemException);
+
+};
+
+
+#endif /* MESSENGERI_H_  */
