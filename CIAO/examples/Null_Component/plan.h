8633d7283d860ca19bbcf513e7d18b3ad1bf9611
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-10-19 15:27:36 +0000

    ChangeLogTag: Fri Oct 19 15:22:36 UTC 2007  Jeff Parsons <j.parsons@vanderbilt.edu>

--- CIAO/examples/Null_Component/plan.h
+++ CIAO/examples/Null_Component/plan.h
@@ -1,11 +1,15 @@
 // $Id$
 
-extern "C" ::Components::HomeExecutorBase_ptr create_Null_ComponentHome_Impl (void);
-extern "C" ::PortableServer::Servant create_Null_ComponentHome_Servant 
-    (::Components::HomeExecutorBase_ptr p,
-     ::CIAO::Session_Container *c,
-     const char*
-		);
+#include "Session_Container.h"
+
+extern "C"
+::Components::HomeExecutorBase_ptr create_Null_ComponentHome_Impl (void);
+
+extern "C"
+::PortableServer::Servant create_Null_ComponentHome_Servant (
+  ::Components::HomeExecutorBase_ptr p,
+  ::CIAO::Session_Container *c,
+  const char*);
 
 struct HomeAttributes
 {
@@ -19,8 +23,15 @@ struct HomeAttributes
 };
 
 /// Homes
-HomeAttributes homes_table[]= 
+HomeAttributes homes_table[] = 
 {
- {"Null_Component.ComponentImplementations.Null_Assembly_Container.Null_Assembly.Null_Component", "create_Null_ComponentHome_Impl", create_Null_ComponentHome_Impl, "create_Null_ComponentHome_Servant", create_Null_ComponentHome_Servant}
+  {
+    "Null_Component.ComponentImplementations.Null_Assembly_Container."
+    "Null_Assembly.Null_Component",
+    "create_Null_ComponentHome_Impl",
+    create_Null_ComponentHome_Impl,
+    "create_Null_ComponentHome_Servant",
+    create_Null_ComponentHome_Servant
+  }
 };
 
