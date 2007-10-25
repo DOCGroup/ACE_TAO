a73211fbb2f0c1ad93657649744f0ab0d452bc8c
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-10-25 14:29:01 +0000

    Thu Oct 25 14:27:42 UTC 2007  Phil Mesnier  <mesnier_p@ociweb.com>

--- trunk/TAO/tests/DII_AMI_Forward/client.cpp
+++ trunk/TAO/tests/DII_AMI_Forward/client.cpp
@@ -1,28 +1,11 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: f; c-basic-offset: 2 -*- */
 
 /*
- *    Lorica source file.
- *    Copyright (C) 2007 OMC Denmark ApS.
- *
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * $Id$
  */
 
 #include "tao/DynamicInterface/Request.h"  /* This must come first for
                                               G++ 3.4 or better */
-
-
 #include "tao/debug.h"
 #include "ace/Get_Opt.h"
 #include "ace/Task.h"
