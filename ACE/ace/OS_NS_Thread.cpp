7de78a75382de9f8d3c035cf26cb285780bae13b
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-11-05 11:43:37 +0000

    ChangeLogTag: Mon Nov  5 11:40:00 UTC 2007  Simon Massey  <simon.massey@prismtech.com>

--- ACE/ace/OS_NS_Thread.cpp
+++ ACE/ace/OS_NS_Thread.cpp
@@ -4963,15 +4963,6 @@ int
 ACE_OS::thr_keyfree_native (ACE_OS_thread_key_t key)
 {
   ACE_OS_TRACE ("ACE_OS::thr_keyfree_native");
-# if defined (ACE_HAS_BROKEN_THREAD_KEYFREE)
-    // For some systems, e.g. LynxOS, we need to ensure that
-    // any registered thread destructor action for this slot
-    // is now disabled. Otherwise in the event of a dynamic library
-    // unload of libACE, by a program not linked with libACE,
-    // ACE_TSS_cleanup will be invoked again at the thread exit
-    // after libACE has been actually been unmapped from memory.
-    (void) ACE_OS::thr_setspecific (key, 0);
-# endif /* ACE_HAS_BROKEN_THREAD_KEYFREE */
 # if defined (ACE_HAS_THREADS)
 #   if defined (ACE_HAS_PTHREADS_DRAFT4) || defined (ACE_HAS_PTHREADS_DRAFT6)
     ACE_UNUSED_ARG (key);
