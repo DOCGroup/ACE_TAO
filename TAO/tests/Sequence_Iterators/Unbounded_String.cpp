2b724f1a4e8ff47aa1fb6da5ef033d99e4dc82b5
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-11-12 15:52:28 +0000

    reordered tests and introduced FAIL_RETURN_IF macro

--- TAO/tests/Sequence_Iterators/Unbounded_String.cpp
+++ TAO/tests/Sequence_Iterators/Unbounded_String.cpp
@@ -12,6 +12,14 @@
 
 #include <iostream>
 #include <iterator>
+#include <sstream>
+
+#define FAIL_RETURN_IF(CONDITION) \
+          if (CONDITION) \
+	    { \
+              ACE_DEBUG ((LM_ERROR, ACE_TEXT ("\tFailed at %N:%l\n"))); \
+	      return 1; \
+	    }
 
 int main(int,char*[])
 {
@@ -26,16 +34,15 @@ int main(int,char*[])
 
   ::CORBA::StringSeq::iterator a_it = a.begin ();
 
-  if (a_it != a.end ())
-    {
-      ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-      return 1;
-    }
+  FAIL_RETURN_IF (a_it != a.end ());
+
+  // setup of an example sequence
 
   const char * elem0_cstr = "elem0";
   const char * elem1_cstr = "elem1";
   const char * elem2_cstr = "elem2";
   const char * elem3_cstr = "elem3";
+
   ::CORBA::StringSeq::value_type elem0 = CORBA::string_dup (elem0_cstr);
   ::CORBA::StringSeq::value_type elem1 = CORBA::string_dup (elem1_cstr);
   ::CORBA::StringSeq::value_type elem2 = CORBA::string_dup (elem2_cstr);
@@ -47,65 +54,19 @@ int main(int,char*[])
   a[2] = elem2;
   a[3] = elem3;
 
-
-  ::CORBA::StringSeq test;
-  test.length (a.length ());
-
-  // Memory is leaked here from
-  // TAO::details::string_traits_base<char>::default_initializer()
-  std::copy (a.begin (),
-             a.end (),
-             test.begin ());
-
-  ::CORBA::StringSeq::iterator copytest_iter = test.begin ();
-  for (::CORBA::StringSeq::iterator copya_iter = a.begin ();
-       copya_iter != a.end ();
-       ++copya_iter, ++copytest_iter)
-    {
-      if (ACE_OS::strcmp (*copya_iter, *copytest_iter) != 0)
-        {
-          ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-          return 1;
-        }
-    }
-
-  /// Testing - using ostream_iterator
-  std::cout << "Output should be: " << std::endl
-            << elem0_cstr << std::endl
-            << elem1_cstr << std::endl
-            << elem2_cstr << std::endl
-            << elem3_cstr << std::endl
-            << "End of expected output" << std::endl;
-
-  std::copy (a.begin (),
-             a.end (),
-             std::ostream_iterator<CORBA::StringSeq::value_type> (std::cout,
-                                                                  "\n"));
   // test dereferencing
 
   ::CORBA::StringSeq::iterator a_it2 = a.begin ();
   
-  if (ACE_OS::strcmp (*a_it2, elem0) != 0)
-    {
-      ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-      return 1;
-    }
+  FAIL_RETURN_IF (ACE_OS::strcmp (*a_it2, elem0) != 0);
 
   // test increment operation
 
   a_it2++;
 
-  if (a_it2 == a.begin())
-    {
-      ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-      return 1;
-    }
+  FAIL_RETURN_IF (a_it2 == a.begin());
 
-  if (ACE_OS::strcmp (*a_it2, elem1) != 0)
-    {
-      ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-      return 1;
-    }
+  FAIL_RETURN_IF (ACE_OS::strcmp (*a_it2, elem1) != 0);
 
   // test pre-increment operator
 
@@ -115,11 +76,7 @@ int main(int,char*[])
   a_it3++;
   ++a_it31;
 
-  if (a_it3 != a_it31)
-    {
-      ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-      return 1;
-    }
+  FAIL_RETURN_IF (a_it3 != a_it31);
 
   // test for loop behaviour
   ::CORBA::StringSeq b = a;
@@ -129,12 +86,39 @@ int main(int,char*[])
        a_it4 != a.end ();
        a_it4++, b_it++)
     {
-      if (ACE_OS::strcmp (*a_it4, *b_it) != 0)
-	{
-	  ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Failed at %N:%l\n")));
-	  return 1;
-	}
+      FAIL_RETURN_IF (ACE_OS::strcmp (*a_it4, *b_it) != 0);
     }
+
+  ::CORBA::StringSeq test;
+  test.length (a.length ());
+
+  // Memory is leaked here from
+  // TAO::details::string_traits_base<char>::default_initializer()
+
+  std::copy (a.begin (),
+             a.end (),
+             test.begin ());
+
+  FAIL_RETURN_IF (test.length () != a.length ());
+
+  ::CORBA::StringSeq::iterator copytest_iter = test.begin ();
+  for (::CORBA::StringSeq::iterator copya_iter = a.begin ();
+       copya_iter != a.end ();
+       ++copya_iter, ++copytest_iter)
+    {
+      FAIL_RETURN_IF (ACE_OS::strcmp (*copya_iter, *copytest_iter) != 0);
+    }
+
+  /// Testing - using ostream_iterator
+
+  std::ostringstream ostream;
+  std::copy (a.begin (),
+             a.end (),
+             std::ostream_iterator<CORBA::StringSeq::value_type> (ostream,
+								  "\n"));
+
+  FAIL_RETURN_IF (
+     ostream.str ().compare ("elem0\nelem1\nelem2\nelem3\n") != 0);
 
   return 0;
 }
