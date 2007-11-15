1cc709c345c906474958c038525b614e2221b7cf
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-11-15 22:02:14 +0000

    added more complete iterator tests and removed past_end_ and before_begin_ iterator members, fixed a typo in Unbounded_Basic_String_Sequence_T

--- branches/TAO_Sequence_Iterators/TAO/tests/Sequence_Iterators/StringSeq.cpp
+++ branches/TAO_Sequence_Iterators/TAO/tests/Sequence_Iterators/StringSeq.cpp
@@ -15,20 +15,17 @@
 #include <sstream>
 
 #define FAIL_RETURN_IF(CONDITION) \
+          ACE_DEBUG ((LM_INFO, ACE_TEXT ("in %N:%l\n"))); \
           if (CONDITION) \
 	    { \
               ACE_DEBUG ((LM_ERROR, ACE_TEXT ("\tFailed at %N:%l\n"))); \
 	      return 1; \
 	    }
 
-int main(int,char*[])
+template <typename ITERATOR_T>
+int test_sequence ()
 {
-  // Questions:
-  // Should assignment of iterators be allowed? g++ seems to allow this.
-  //  I'm not sure if it makes sense.
-  // What should happen to iterators when the containers are modified?
-
-  ::CORBA::StringSeq a;
+ ::CORBA::StringSeq a;
 
   // test equality operator
   FAIL_RETURN_IF (!(a.begin () == a.begin ()));
@@ -54,7 +51,7 @@ int main(int,char*[])
   a[3] = CORBA::string_dup (elem3_cstr);
 
   // test iterator copy constructor
-  ::CORBA::StringSeq::iterator a_it (a.begin ());
+  ITERATOR_T a_it (a.begin ());
   FAIL_RETURN_IF (a_it != a.begin ());
 
   // test assignment operator
@@ -79,12 +76,11 @@ int main(int,char*[])
   FAIL_RETURN_IF (a_it < a.begin ());
 
   // test difference type
-  ::CORBA::StringSeq::iterator::difference_type a_diff =
-      a_it - a.begin ();
+  int a_diff = a_it - a.begin ();
   FAIL_RETURN_IF (a_diff != 1);
 
   // test copy constructor 
-  ::CORBA::StringSeq::iterator a_it1 (a_it);
+  ITERATOR_T a_it1 (a_it);
   FAIL_RETURN_IF (a_it1 != a_it);
 
   // test preincrement operator
@@ -92,7 +88,7 @@ int main(int,char*[])
   FAIL_RETURN_IF ((a_it1 - a_it) != 1);
 
   // test = and += operator
-  ::CORBA::StringSeq::iterator a_it2 = a_it += 3;
+  ITERATOR_T a_it2 = a_it += 3;
   FAIL_RETURN_IF (a_it2 != a_it);
   FAIL_RETURN_IF ((a_it - a_it1) != 2);
 
@@ -125,28 +121,26 @@ int main(int,char*[])
   // test operator[] read
   a_it = a.begin ();
   FAIL_RETURN_IF (ACE_OS::strcmp (a_it[0],a[0]) != 0);
-  FAIL_RETURN_IF (ACE_OS::strcmp (a_it[1],a[1]) != 0);
+  a_it += 2;
+  FAIL_RETURN_IF (ACE_OS::strcmp (a_it[0],a[2]) != 0);
 
   // test operator[] write
   // NOTE: This now changes the sequence a.
-  a_it[2] = CORBA::string_dup (elem0_cstr);
+  a_it[0] = CORBA::string_dup (elem0_cstr);
   FAIL_RETURN_IF (ACE_OS::strcmp (a[2],elem0_cstr) != 0);
 
   // reset content of sequence a
-  a[0] = CORBA::string_dup (elem0_cstr);
-  a[1] = CORBA::string_dup (elem1_cstr);
   a[2] = CORBA::string_dup (elem2_cstr);
-  a[3] = CORBA::string_dup (elem3_cstr);
 
   // test for loop behaviour
   ::CORBA::StringSeq b = a;
-  ::CORBA::StringSeq::iterator b_it = b.begin ();
+  ITERATOR_T b_it = b.begin ();
 
-  for (::CORBA::StringSeq::iterator a_it4 = a.begin ();
-       a_it4 != a.end ();
-       a_it4++, b_it++)
+  for (a_it = a.begin ();
+       a_it != a.end ();
+       a_it++, b_it++)
     {
-      FAIL_RETURN_IF (ACE_OS::strcmp (*a_it4, *b_it) != 0);
+      FAIL_RETURN_IF (ACE_OS::strcmp (*a_it, *b_it) != 0);
     }
 
   ::CORBA::StringSeq test;
@@ -161,8 +155,8 @@ int main(int,char*[])
 
   FAIL_RETURN_IF (test.length () != a.length ());
 
-  ::CORBA::StringSeq::iterator copytest_iter = test.begin ();
-  for (::CORBA::StringSeq::iterator copya_iter = a.begin ();
+  ITERATOR_T copytest_iter = test.begin ();
+  for (ITERATOR_T copya_iter = a.begin ();
        copya_iter != a.end ();
        ++copya_iter, ++copytest_iter)
     {
@@ -177,8 +171,185 @@ int main(int,char*[])
              std::ostream_iterator<CORBA::StringSeq::value_type> (ostream,
 								  "\n"));
 
-  //  FAIL_RETURN_IF (
-  //   ostream.str ().compare ("elem0\nelem1\nelem2\nelem3\n") != 0);
+  FAIL_RETURN_IF (
+    ostream.str ().compare ("elem0\nelem1\nelem2\nelem3\n") != 0);
 
   return 0;
+}
+
+//-----------------------------------------------------------------------------
+
+template <typename REVERSE_ITERATOR_T>
+int test_sequence_reverse ()
+{
+ ::CORBA::StringSeq a;
+
+  // test equality operator
+  FAIL_RETURN_IF (!(a.begin () == a.begin ()));
+
+  // test non-equality operator
+  FAIL_RETURN_IF (a.end () != a.end ());
+
+  // test for correct behaviour for empty sequence
+
+  FAIL_RETURN_IF (a.begin() != a.end ());
+  
+  // setup of an example sequence
+
+  const char * elem0_cstr = "elem0";
+  const char * elem1_cstr = "elem1";
+  const char * elem2_cstr = "elem2";
+  const char * elem3_cstr = "elem3";
+
+  a.length (4);
+  a[0] = CORBA::string_dup (elem0_cstr);
+  a[1] = CORBA::string_dup (elem1_cstr);
+  a[2] = CORBA::string_dup (elem2_cstr);
+  a[3] = CORBA::string_dup (elem3_cstr);
+
+  // test iterator copy constructor
+  REVERSE_ITERATOR_T a_it (a.rbegin ());
+  FAIL_RETURN_IF (a_it != a.rbegin ());
+
+  // test assignment operator
+  a_it = a.rbegin ();
+  FAIL_RETURN_IF (a_it != a.rbegin ());
+
+  // test non const dereferencing
+  char* value0 = *a_it;
+  FAIL_RETURN_IF (ACE_OS::strcmp (value0, elem3_cstr) != 0);
+
+  // test const dereferencing
+  const char* const value1 = *a_it;
+  FAIL_RETURN_IF (ACE_OS::strcmp (value1, elem3_cstr) != 0);
+
+  // test increment operation
+  a_it++;
+  FAIL_RETURN_IF (a_it == a.rbegin());
+  FAIL_RETURN_IF (ACE_OS::strcmp (*a_it, elem2_cstr) != 0);
+
+  // test < operator
+  FAIL_RETURN_IF (!(a.rbegin () < a_it));
+  FAIL_RETURN_IF (a_it < a.rbegin ());
+
+  // test difference type
+  int a_diff = a_it - a.rbegin ();
+  FAIL_RETURN_IF (a_diff != 1);
+
+  // test copy constructor 
+  REVERSE_ITERATOR_T a_it1 (a_it);
+  FAIL_RETURN_IF (a_it1 != a_it);
+
+  // test preincrement operator
+  ++a_it1;
+  FAIL_RETURN_IF ((a_it1 - a_it) != 1);
+
+  // test = and += operator
+  REVERSE_ITERATOR_T a_it2 = a_it += 3;
+  FAIL_RETURN_IF (a_it2 != a_it);
+  FAIL_RETURN_IF ((a_it - a_it1) != 2);
+
+  // test + operator
+  a_it2 = a_it1 + 3;
+  FAIL_RETURN_IF ((a_it2 - a_it1) != 3);
+
+  // test post-decrement operation
+  a_it = a.rend ();
+  a_it--;
+  FAIL_RETURN_IF (a_it == a.rend ());
+  FAIL_RETURN_IF ((a.rend () - a_it) != 1);
+  FAIL_RETURN_IF (ACE_OS::strcmp (*a_it, elem0_cstr) != 0);
+
+  // test pre-decrement operator
+  a_it = a.rend ();
+  --a_it;
+  FAIL_RETURN_IF (a_it == a.rend ());
+  FAIL_RETURN_IF ((a.rend () - a_it) != 1);
+  FAIL_RETURN_IF (ACE_OS::strcmp (*a_it, elem0_cstr) != 0);
+
+  // test -= operator
+  a_it -= 3;
+  FAIL_RETURN_IF ((a_it1 - a_it) != 2);
+
+  // test - operator
+  a_it2 = a_it1 - 2;
+  FAIL_RETURN_IF ((a_it1 - a_it2) != 2);
+
+  // test operator[] read
+  a_it = a.rbegin ();
+  FAIL_RETURN_IF (ACE_OS::strcmp (a_it[0],a[3]) != 0);
+  a_it += 2;
+  FAIL_RETURN_IF (ACE_OS::strcmp (a_it[0],a[1]) != 0);
+
+  // test operator[] write
+  // NOTE: This now changes the sequence a.
+  a_it[0] = CORBA::string_dup (elem0_cstr);
+  FAIL_RETURN_IF (ACE_OS::strcmp (a[1],elem0_cstr) != 0);
+
+  // reset content of sequence a
+  a[1] = CORBA::string_dup (elem1_cstr);
+
+  // test for loop behaviour
+  ::CORBA::StringSeq b = a;
+  REVERSE_ITERATOR_T b_it = b.rbegin ();
+
+  for (a_it = a.rbegin ();
+       a_it != a.rend ();
+       a_it++, b_it++)
+    {
+      FAIL_RETURN_IF (ACE_OS::strcmp (*a_it, *b_it) != 0);
+    }
+
+  ::CORBA::StringSeq test;
+  test.length (a.length ());
+
+  // Memory is leaked here from
+  // TAO::details::string_traits_base<char>::default_initializer()
+
+  std::copy (a.begin (),
+             a.end (),
+             test.begin ());
+
+  FAIL_RETURN_IF (test.length () != a.length ());
+
+  REVERSE_ITERATOR_T copytest_iter = test.rbegin ();
+  for (REVERSE_ITERATOR_T copya_iter = a.rbegin ();
+       copya_iter != a.rend ();
+       ++copya_iter, ++copytest_iter)
+    {
+      FAIL_RETURN_IF (ACE_OS::strcmp (*copya_iter, *copytest_iter) != 0);
+    }
+
+  /// Testing - using ostream_iterator
+
+  std::ostringstream ostream;
+  std::copy (a.rbegin (),
+             a.rend (),
+             std::ostream_iterator<CORBA::StringSeq::value_type> (ostream,
+								  "\n"));
+
+  FAIL_RETURN_IF (
+    ostream.str ().compare ("elem3\nelem2\nelem1\nelem0\n") != 0);
+
+  return 0;
+}
+
+
+int main(int,char*[])
+{
+  int status = 0;
+
+  // test Generic_Sequence_Iterator
+  status += test_sequence< ::CORBA::StringSeq::iterator> ();
+
+  // test Const_Generic_Sequence_Iterator
+  status += test_sequence< ::CORBA::StringSeq::const_iterator> ();
+
+  // test Generic_Sequence_Reverse_Iterator
+  status += test_sequence_reverse< ::CORBA::StringSeq::reverse_iterator> ();
+
+  // test Const_Generic_Sequence_Reverse_Iterator
+  status += test_sequence_reverse< ::CORBA::StringSeq::reverse_iterator> ();
+
+  return status;
 }
