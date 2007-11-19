d2bf9ec35436282b2ad3c0ffb4690ceb255e7877
nshankar <nshankar@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-11-19 21:52:56 +0000

    Mon Nov 19 21:51:41 UTC 2007  Nishanth Shankaran  <nshankar@nospam.com>

---
+++ TM_Proxy/SystemSnapshot.cpp
@@ -0,0 +1,21 @@
+#include "SystemSnapshot.h"
+
+namespace CIAO
+{
+  namespace RACE
+  {
+    namespace TM_Proxy
+    {
+      namespace CIDL_TM_Proxy_Component_Impl
+      {
+        SystemSnapshot_exec_i::SystemSnapshot_exec_i (void)
+        {
+        }
+
+        SystemSnapshot_exec_i::~SystemSnapshot_exec_i (void)
+        {
+        }
+      }
+    }
+  }
+}
