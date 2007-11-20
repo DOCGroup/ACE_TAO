21e9fa82ca04bb672824b14a18b2cf478cf65842
nshankar <nshankar@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2007-11-20 22:59:20 +0000

    Tue Nov 20 22:57:32 UTC 2007  Nishanth Shankaran  <nshankar@nospam.com>

--- TM_Proxy/TM_Proxy_exec.cpp
+++ TM_Proxy/TM_Proxy_exec.cpp
@@ -1,6 +1,9 @@
-// $Id:$
+// $Id$
 #include "TM_Proxy_exec.h"
 #include "ciao/CIAO_common.h"
+#include "tao/ORB_Core.h"
+#include "orbsvcs/CosNamingC.h"
+#include <string>
 
 namespace CIAO
 {
@@ -11,6 +14,7 @@ namespace CIAO
       namespace CIDL_TM_Proxy_Component_Impl
       {
         TM_Proxy_Component_exec_i::TM_Proxy_Component_exec_i (void)
+          : logger_ ("TM_Proxy.log")
         {
         }
 
@@ -19,33 +23,35 @@ namespace CIAO
         }
         // Attribute operations.
         char *
-        TM_Proxy_Component_exec_i::TM_Name ()
+          TM_Proxy_Component_exec_i::TM_Name ()
         {
-          return 0;
+          return CORBA::string_dup (this->TM_Name_.in ());
         }
 
         void
-        TM_Proxy_Component_exec_i::TM_Name (const char * /* name */ )
-        {}
+          TM_Proxy_Component_exec_i::TM_Name (const char * name )
+        {
+          this->TM_Name_ = CORBA::string_dup (name);
+        }
 
         // Port operations.
 
         ::CIAO::RACE::TM_Proxy::CCM_SystemSnapshot_ptr
-        TM_Proxy_Component_exec_i::get_snapshot ()
+          TM_Proxy_Component_exec_i::get_snapshot ()
         {
           // Your code here.
           return ::CIAO::RACE::TM_Proxy::CCM_SystemSnapshot::_nil ();
         }
 
         ::CIAO::RACE::TM_Proxy::CCM_Utility_ptr
-        TM_Proxy_Component_exec_i::get_utils ()
+          TM_Proxy_Component_exec_i::get_utils ()
         {
           // Your code here.
           return ::CIAO::RACE::TM_Proxy::CCM_Utility::_nil ();
         }
 
         ::CIAO::RACE::TM_Proxy::CCM_ResourceManager_ptr
-        TM_Proxy_Component_exec_i::get_manager ()
+          TM_Proxy_Component_exec_i::get_manager ()
         {
           // Your code here.
           return ::CIAO::RACE::TM_Proxy::CCM_ResourceManager::_nil ();
@@ -54,44 +60,84 @@ namespace CIAO
         // Operations from Components::SessionComponent
 
         void
-        TM_Proxy_Component_exec_i::set_session_context (
+          TM_Proxy_Component_exec_i::set_session_context (
           ::Components::SessionContext_ptr ctx)
         {
           this->context_ =
-            ::CIAO::RACE::TM_Proxy::CCM_TM_Proxy_Component_Context::_narrow (ctx);
-
-          if (CORBA::is_nil (this->context_.in ()))
+            TM_Proxy_Component_Context::_narrow (ctx);
+          if (CORBA::is_nil (this->context_))
           {
             throw ::CORBA::INTERNAL ();
           }
         }
 
         void
-        TM_Proxy_Component_exec_i::ciao_preactivate ()
+          TM_Proxy_Component_exec_i::ciao_preactivate ()
         {
           // Your code here.
         }
 
         void
-        TM_Proxy_Component_exec_i::ciao_postactivate ()
+          TM_Proxy_Component_exec_i::ciao_postactivate ()
+        {
+          std::string msg = "TM_Proxy_Component_exec_i::"
+                            "ciao_postactivate (): ";
+          try
+          {
+            ::CORBA::ORB_ptr orb =
+              this->context_->_ciao_the_Container()->the_ORB();
+
+            /// Trying to get the object reference from the Naming Service.
+            CORBA::Object_var naming_obj =
+              orb->resolve_initial_references ("NameService");
+            if (CORBA::is_nil (naming_obj.in ()))
+            {
+              msg += "Unable to resolve the Name Service.";
+              this->logger_.log (msg);
+              return;
+            }
+
+            CosNaming::NamingContext_var namingContext =
+              CosNaming::NamingContext::_narrow (naming_obj.in ());
+            CosNaming::Name name (1);
+            name.length (1);
+            name[0].id = CORBA::string_dup (this->TM_Name_.in ());
+            CORBA::Object_var TM = namingContext->resolve (name);
+
+            this->TM_Daemon_ =
+              ::Deployment::TargetManager::_narrow (TM.in ());
+
+            if (CORBA::is_nil (this->TM_Daemon_.in ()))
+            {
+              msg += "Object reference of TM_Daemon is nil!";
+              this->logger_.log (msg);
+            }
+          }
+          catch (CORBA::Exception &ex)
+          {
+            msg += "Exception caught\n";
+            msg += ex._info ().c_str();
+            msg += "\nUnable to resolve reference to ";
+            msg += this->TM_Name_.in ();
+            this->logger_.log (msg);
+          }
+          return;
+        }
+
+        void
+          TM_Proxy_Component_exec_i::ccm_activate ()
         {
           // Your code here.
         }
 
         void
-        TM_Proxy_Component_exec_i::ccm_activate ()
+          TM_Proxy_Component_exec_i::ccm_passivate ()
         {
           // Your code here.
         }
 
         void
-        TM_Proxy_Component_exec_i::ccm_passivate ()
-        {
-          // Your code here.
-        }
-
-        void
-        TM_Proxy_Component_exec_i::ccm_remove ()
+          TM_Proxy_Component_exec_i::ccm_remove ()
         {
           // Your code here.
         }
@@ -109,7 +155,7 @@ namespace CIAO
         }
 
         ::Components::EnterpriseComponent_ptr
-        TM_Proxy_Component_Home_exec_i::create ()
+          TM_Proxy_Component_Home_exec_i::create ()
         {
           ::Components::EnterpriseComponent_ptr retval =
             ::Components::EnterpriseComponent::_nil ();
@@ -123,7 +169,7 @@ namespace CIAO
         }
 
         extern "C" TM_PROXY_EXEC_Export ::Components::HomeExecutorBase_ptr
-        create_CIAO_RACE_TM_Proxy_TM_Proxy_Component_Home_Impl (void)
+          create_CIAO_RACE_TM_Proxy_TM_Proxy_Component_Home_Impl (void)
         {
           ::Components::HomeExecutorBase_ptr retval =
             ::Components::HomeExecutorBase::_nil ();
