9218d3f878c90015c79d27c1764908ce4031c07a
Abdullah Sowayan <sowayan@users.noreply.github.com>
2007-11-21 16:37:49 +0000

    Wed Nov 21 16:36:28 UTC 2007  Abdullah Sowayan  <abdullah.sowayan@lmco.com>

--- CIAO/ciao/Container_Base.cpp
+++ CIAO/ciao/Container_Base.cpp
@@ -3,6 +3,10 @@
 #include "Container_Base.h"
 #include "CIAO_common.h"
 
+#if !defined (__ACE_INLINE__)
+# include "Container_Base.inl"
+#endif /* __ACE_INLINE__ */
+
 namespace CIAO
 {
   ////////////////////////////////////////////////////////////////
@@ -23,17 +27,6 @@ namespace CIAO
   {
   }
 
-  void
-  Container::set_receptacle_policy_map (::CIAO::REC_POL_MAP &rec_pol_map)
-  {
-    for (::CIAO::REC_POL_MAP_ITERATOR it = rec_pol_map.begin ();
-         it != rec_pol_map.end ();
-         ++it)
-      {
-        this->rec_pol_map_.bind ((*it).ext_id_, (*it).int_id_);
-      }
-  }
-
   CORBA::PolicyList
   Container::get_receptacle_policy (const char* name)
   {
@@ -49,23 +42,5 @@ namespace CIAO
       }
       
     return policy_list;
-  }
-
-  PortableServer::POA_ptr
-  Container::the_POA (void) const
-  {
-    return this->component_poa_.in ();
-  }
-
-  PortableServer::POA_ptr
-  Container::the_facet_cons_POA (void) const
-  {
-    return this->facet_cons_poa_.in ();
-  }
-
-  CORBA::ORB_ptr
-  Container::the_ORB (void) const
-  {
-    return this->orb_.in ();
   }
 }
