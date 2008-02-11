7be9f8be660d355a7d938b193110d2835d2a3e55
johnc <johnc@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-02-11 17:10:56 +0000

    ChangeLogTag: Mon Feb 11 17:00:23 UTC 2008  Ciju John  <johnc at ociweb dot com>

--- TAO/tests/Oneway_Send_Timeouts/Test_i.h
+++ TAO/tests/Oneway_Send_Timeouts/Test_i.h
@@ -9,20 +9,15 @@ class Test_i : public virtual POA_Test
   Test_i (CORBA::ORB_ptr orb);
   virtual ~Test_i ();
 
-  virtual void sleep (CORBA::Long sec, CORBA::Long msec)
-    ACE_THROW_SPEC ((::CORBA::SystemException));
+  virtual void sleep (CORBA::Long sec, CORBA::Long msec);
 
-  virtual void unsleep ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
+  virtual void unsleep ();
 
-  virtual void dummy_one_way (const char* msg)
-    ACE_THROW_SPEC ((::CORBA::SystemException));
+  virtual void dummy_one_way (const char* msg);
 
-  virtual void dummy_two_way ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
+  virtual void dummy_two_way ();
 
-  virtual void shutdown ()
-    ACE_THROW_SPEC ((::CORBA::SystemException));
+  virtual void shutdown ();
 
  private:
   volatile bool sleep_;
