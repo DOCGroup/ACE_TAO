67af79e3d146f57731603d42c96b78e9e6bca305
Johnny Willemsen <jwillemsen@remedy.nl>
2008-02-13 12:41:57 +0000

    Wed Feb 13 12:41:12 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ ACE/examples/Semaphores/starter.cpp
@@ -0,0 +1,20 @@
+// $Id$
+
+#include "ace/Process_Semaphore.h"
+#include "ace/OS_NS_unistd.h"
+#include "ace/Log_Msg.h"
+
+int main(int, char**)
+{
+  // Create locked
+  ACE_Process_Semaphore s (0, ACE_TEXT("AceTest"));
+
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Starter has created and acquired semaphore\n")));
+
+  // Do some init stuff. Simulated by a sleep...
+  ACE_OS::sleep(10);
+
+  s.release();
+
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Starter has released semaphore\n")));
+}
