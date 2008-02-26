2e973539bbf9bdda9d924f693219f89cf35c17b0
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-02-26 13:41:14 +0000

    ChangeLogTag: Tue Feb 26 13:36:47 UTC 2008  Iliyan Jeliazkov  <iliyan@ociweb.com>

--- branches/iliyan-gestalt/ACE/tests/Bug_2980_Regression_Dll.cpp
+++ branches/iliyan-gestalt/ACE/tests/Bug_2980_Regression_Dll.cpp
@@ -1,27 +1,38 @@
 #include "ace/Init_ACE.h"
 #include "ace/OS.h"
 #include "ace/Log_Msg.h"
+#include "test_config.h"
 
 
 extern "C"
 {
 
-ACE_Proper_Export_Flag void capi_init(void)
+ACE_Proper_Export_Flag int capi_init(void)
 {
-  ACE::init();
-  ACE_OS::printf("ACE::init()\n");
+  int ret = ACE::init();
+  ACE_START_TEST (ACE_TEXT ("Bug_2980_Regression_Test"));
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("ACE::init ()\n")));
+  ACE_END_TEST;
+  return ret;
 }
 
 
-ACE_Proper_Export_Flag void capi_fini(void)
+ACE_Proper_Export_Flag int capi_fini(void)
 {
-  ACE_OS::printf("ACE::fini()\n");
-  ACE::fini();
+  ACE_START_TEST (ACE_TEXT ("Bug_2980_Regression_Test"));
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("ACE::fini ()\n")));
+  ACE_END_TEST;
+
+  return ACE::fini();
 }
 
 
-ACE_Proper_Export_Flag void capi_dosomething(void)
+ACE_Proper_Export_Flag int capi_dosomething(void)
 {
+  ACE_START_TEST (ACE_TEXT ("Bug_2980_Regression_Test"));
+
   ACE_DEBUG((
     LM_DEBUG,
     ACE_TEXT ("(%P|%t) capi_dosomething - entered\n")
@@ -31,6 +42,9 @@ ACE_Proper_Export_Flag void capi_dosomething(void)
     LM_DEBUG,
     ACE_TEXT ("(%P|%t) capi_dosomething - leaving\n")
   ));
+
+  ACE_END_TEST;
+  return 0;
 }
 
 
