5a8c4ba34c664583c44d17095e1acf801957545b
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-03-12 15:33:28 +0000

    Merged revisions 80901-80902,80908-80910,80913,80915,80917-80918,80920 via svnmerge from https://svn.dre.vanderbilt.edu/DOC/Middleware/trunk/ACE
    ........
      r80901 | shuston | 2008-03-11 07:23:02 -0500 (Tue, 11 Mar 2008) | 1 line
      
      ChangeLogTag:Tue Mar 11 12:20:02 UTC 2008  Steve Huston  <shuston@riverace.com>
    ........
      r80902 | shuston | 2008-03-11 07:27:42 -0500 (Tue, 11 Mar 2008) | 1 line
      
      ChangeLogTag:Tue Mar 11 12:24:43 UTC 2008  Steve Huston  <shuston@riverace.com>
    ........
      r80908 | johnnyw | 2008-03-12 01:56:45 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 06:55:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........
      r80909 | johnnyw | 2008-03-12 02:01:17 -0500 (Wed, 12 Mar 2008) | 1 line
    ........
      r80910 | johnnyw | 2008-03-12 02:08:10 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 07:07:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........
      r80913 | johnnyw | 2008-03-12 06:54:05 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 11:53:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........
      r80915 | johnnyw | 2008-03-12 07:03:26 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 11:59:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........
      r80917 | johnnyw | 2008-03-12 08:01:17 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 12:59:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........
      r80918 | johnnyw | 2008-03-12 08:02:12 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 12:59:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........
      r80920 | johnnyw | 2008-03-12 10:09:15 -0500 (Wed, 12 Mar 2008) | 1 line
      
      Wed Mar 12 15:08:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>
    ........

--- branches/iliyan-gestalt/ACE/ace/FoxReactor/FoxReactor.h
+++ branches/iliyan-gestalt/ACE/ace/FoxReactor/FoxReactor.h
@@ -25,9 +25,8 @@
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#if defined (ACE_HAS_FOX)
-
 #include "ace/Select_Reactor.h"
+#include "ace/FoxReactor/ACE_FoxReactor_export.h"
 
 #include <fox/fx.h>
 
@@ -107,8 +106,6 @@ private:
   ACE_FoxReactor (const ACE_FoxReactor &);
   ACE_FoxReactor &operator = (const ACE_FoxReactor &);
 };
-
-#endif /* ACE_HAS_FL */
 
 #include /**/ "ace/post.h"
 #endif /* ACE_FOXREACTOR_H */
