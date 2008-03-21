6a5f5ba22862e61769598158e460e4cd270d2f0c
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-03-21 15:42:18 +0000

    Fri Mar 21 15:41:12 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/utils/nslist/nslist.cpp
+++ trunk/TAO/utils/nslist/nslist.cpp
@@ -213,7 +213,7 @@ namespace
           {
             ACE_DEBUG ((LM_DEBUG, ": Naming context"));
 
-//            obj = set_rtt(obj.in ());
+            obj = set_rtt(obj.in ());
 
             CosNaming::NamingContext_var xc;
             try
@@ -351,7 +351,7 @@ namespace
       ACE_DEBUG ((LM_DEBUG, "RTT not supported in TAO build.\n"));
 #endif
     }
-    return obj;
+    return CORBA::Object::_duplicate(obj);
   }
 } // end of local unnamed namespace
 
@@ -665,7 +665,7 @@ ACE_TMAIN (int argcw, ACE_TCHAR *argvw[])
       else
         obj = orb->resolve_initial_references ("NameService");
 
-//      obj = set_rtt(obj.in ());
+      obj = set_rtt(obj.in ());
 
       CosNaming::NamingContext_var root_nc =
         CosNaming::NamingContext::_narrow (obj.in ());
