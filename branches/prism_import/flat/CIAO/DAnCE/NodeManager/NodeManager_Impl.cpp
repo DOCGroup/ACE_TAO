259a7d5de26b41d22e5ff8a0937b9c6783d7d809
wotte <wotte@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-03-27 18:30:41 +0000

    ChangeLogTag:Thu Mar 27 18:30:16 UTC 2008  William R. Otte  <wotte@dre.vanderbilt.edu>

--- branches/prism_import/flat/CIAO/DAnCE/NodeManager/NodeManager_Impl.cpp
+++ branches/prism_import/flat/CIAO/DAnCE/NodeManager/NodeManager_Impl.cpp
@@ -9,76 +9,80 @@
 #include <ace/OS_Memory.h>
 
 namespace DAnCE
+{
+  NodeManager_Impl::NodeManager_Impl(CORBA::ORB_ptr orb, 
+                                     PortableServer::POA_ptr poa, 
+                                     const char* name, 
+                                     RedirectionService& redirection)
+    : orb_ (CORBA::ORB::_duplicate (orb)), 
+      poa_ (PortableServer::POA::_duplicate (poa)), 
+      name_ (name), 
+      redirection_ (redirection)
   {
-  NodeManager_Impl::NodeManager_Impl (CORBA::ORB_ptr orb
-                                      , PortableServer::POA_ptr poa
-                                      , const char* name
-                                      , RedirectionService& redirection)
-      : orb_ (CORBA::ORB::_duplicate (orb))
-      , poa_ (PortableServer::POA::_duplicate (poa))
-      , name_ (name)
-      , redirection_ (redirection)
-  {
+    DANCE_TRACE (DLINFO "NodeManager_Impl::NodeManager_Impl");
     redirection.add_node (name);
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::NodeManager_impl has been created\n"));
+    DANCE_DEBUG ((LM_INFO, DLINFO "NodeManager_impl::NodeManager_impl has been created\n"));
   }
 
   NodeManager_Impl::~NodeManager_Impl()
   {
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::~NodeManager_impl - started\n"));
+    DANCE_TRACE (DLINFO "NodeManager_Impl::~NodeManager_Impl()");
     for (TManagers::iterator iter = this->managers_.begin();
          iter != this->managers_.end();
          ++iter)
       {
         PortableServer::ObjectId_var id = this->poa_->servant_to_id ( (*iter).int_id_);
         this->poa_->deactivate_object (id.in());
-        DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::~NodeManager_impl - deleting NodeApplicationManager\n"));
+        DANCE_DEBUG ((LM_TRACE, DLINFO
+                      "NodeManager_impl::~NodeManager_impl - deleting NodeApplicationManager\n"));
         delete (*iter).int_id_;
-        DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::~NodeManager_impl - NodeApplicationManager deleted\n"));
+        DANCE_DEBUG ((LM_DEBUG, DLINFO
+                      "NodeManager_impl::~NodeManager_impl - NodeApplicationManager deleted\n"));
       }
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::~NodeManager_impl - finished\n"));
   }
 
   void
-  NodeManager_Impl::joinDomain (
-    const ::Deployment::Domain & /*theDomain*/,
-    ::Deployment::TargetManager_ptr /*manager*/,
-    ::Deployment::Logger_ptr /*log*/,
-    ::CORBA::Long /*updateInterval*/
-  )
+  NodeManager_Impl::joinDomain (const ::Deployment::Domain & /*theDomain*/,
+                                ::Deployment::TargetManager_ptr /*manager*/,
+                                ::Deployment::Logger_ptr /*log*/,
+                                ::CORBA::Long /*updateInterval*/)
   {
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::joinDomain - started\n"));
-    // Method will be implemented on next developemnt stage
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::joinDomain - finished\n"));
+    DANCE_TRACE (DLINFO "NodeManager_Impl::joinDomain");
+    ACE_DEBUG ((LM_ERROR, DLINFO "NodeManager_Impl::joinDomain - "
+                "joinDomain not implemented\n"))
+      throw CORBA::NO_IMPLEMENT;
   }
 
   void
-  NodeManager_Impl::leaveDomain (
-  )
+  NodeManager_Impl::leaveDomain ()
   {
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::leaveDomain - started\n"));
-    // Method will be implemented on next development stage
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::leaveDomain - finished\n"));
+    DANCE_TRACE (DLINFO "NodeManager_Impl::leaveDomain");
+    ACE_DEBUG ((LM_ERROR, DLINFO "NodeManager_Impl::leaveDomain - "
+                "leave not implemented\n"))
+      throw CORBA::NO_IMPLEMENT;
   }
 
   ::Deployment::NodeApplicationManager_ptr
-  NodeManager_Impl::preparePlan (
-    const ::Deployment::DeploymentPlan & plan,
-    ::Deployment::ResourceCommitmentManager_ptr /*resourceCommitment*/
-  )
+  NodeManager_Impl::preparePlan (const ::Deployment::DeploymentPlan & plan,
+                                 ::Deployment::ResourceCommitmentManager_ptr /*resourceCommitment*/)
   {
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::preparePlan - started for node %s and plan %s\n", this->name_.c_str(), plan.UUID.in()));
+    DANCE_TRACE (DLINFO "NodeManager_Impl::preparePlan");
+    
+    DANCE_DEBUG ((LM_DEBUG, DLINFO "NodeManager_impl::preparePlan - "
+                  "started for node %s and plan %s\n", this->name_.c_str(), plan.UUID.in()));
     // resourceCommitment will be used on next development stage
     if (0 == this->managers_.find (plan.UUID.in()))
       {
         // What should we do here if we already have application for this plan?
         // Probably it is mistake because we should previously call destroyApplication
         // before performe relaunching of application
-        DANCE_ERROR ( (LM_ERROR, "[%M] NodeManager_impl::preparePlan - ApplicationManager for the specified UUID already exists\n"));
+        DANCE_ERROR ((LM_ERROR, "[%M] NodeManager_impl::preparePlan - "
+                      "ApplicationManager for  UUID %s already exists\n",
+                      plan.UUID.in ()));
         throw ::Deployment::PlanError();
       }
 
-    DANCE_DEBUG ( (LM_TRACE, "[%M] NodeManager_impl::preparePlan - creating NodeApplicationManager...\n"));
+    DANCE_DEBUG ((LM_TRACE, DLINFO "NodeManager_impl::preparePlan - creating NodeApplicationManager...\n"));
     NodeApplicationManager_Impl* manager;
     ACE_NEW_THROW_EX (manager,
                       NodeApplicationManager_Impl (this->orb_.in(),
@@ -87,24 +91,22 @@ namespace DAnCE
                                                    this->redirection_,
                                                    this->name_),
                       CORBA::NO_MEMORY());
-    DANCE_DEBUG ( (LM_TRACE, "[%M] NodeManager_impl::preparePlan - activating NodeApplicationManager...\n"));
+    DANCE_DEBUG ((LM_TRACE, DLINFO "NodeManager_impl::preparePlan - activating NodeApplicationManager...\n"));
     PortableServer::ObjectId_var id = this->poa_->activate_object (manager);
     CORBA::Object_var nam = this->poa_->id_to_reference (id.in());
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::preparePlan - NodeApplicationManager created\n"));
 
     //there is an idea to check if plan.UUID really exists
     this->managers_.bind (plan.UUID.in(), manager);
     // narrow should return a nil reference if it fails.
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::preparePlan - finished\n"));
+    DANCE_DEBUG ((LM_INFO, DLINFO "NodeManager_impl::preparePlan - NodeApplicationManager for plan %s completed\n",
+                  plan.UUID.in ()));
     return Deployment::NodeApplicationManager::_narrow (nam.in ());
   }
 
   void
-  NodeManager_Impl::destroyManager (
-    ::Deployment::NodeApplicationManager_ptr appManager
-  )
+  NodeManager_Impl::destroyManager (::Deployment::NodeApplicationManager_ptr appManager)
   {
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::destroyManager - started\n"));
+    DANCE_TRACE (DLINFO "NodeManager_Impl::destroyManager");
 
     for (TManagers::iterator iter = this->managers_.begin();
          iter != this->managers_.end();
@@ -114,26 +116,27 @@ namespace DAnCE
           {
             PortableServer::ObjectId_var id = this->poa_->reference_to_id (appManager);
             this->poa_->deactivate_object (id.in());
-            DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::destroyManager - deleting NodeApplicationManager\n"));
+            DANCE_DEBUG ((LM_TRACE, DLINFO "NodeManager_impl::destroyManager - deleting NodeApplicationManager\n"));
             delete (*iter).int_id_;
-            DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::destroyManager - NodeApplicationManager deleted\n"));
+            DANCE_DEBUG ((LM_TRACE, DLINFO "NodeManager_impl::destroyManager - NodeApplicationManager deleted\n"));
             this->managers_.unbind ( (*iter).ext_id_);
-            DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::destroyManager - finished\n"));
+            DANCE_DEBUG ((LM_TRACE, DLINFO "NodeManager_impl::destroyManager - finished\n"));
             return;
           }
       }
 
-    DANCE_ERROR ( (LM_ERROR, "[%M] NodeManager_impl::destroyManager - correponding NodeApplicationManager cannot be found\n"));
+    DANCE_ERROR((LM_ERROR, DLINFO "NodeManager_impl::destroyManager - "
+                 "correponding NodeApplicationManager cannot be found\n"));
     throw ::Deployment::InvalidReference();
   }
 
   ::Deployment::Resources *
-  NodeManager_Impl::getDynamicResources (
-  )
+  NodeManager_Impl::getDynamicResources ()
   {
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::getDynamicResources - started\n"));
-    // Method will be implemented on next development stage
-    DANCE_DEBUG ( (LM_DEBUG, "[%M] NodeManager_impl::getDynamicResources - finished\n"));
+    DANCE_TRACE (DLINFO, "NodeManager_Impl::getDynamicResources ()");
+    ACE_DEBUG ((LM_ERROR, DLINFO "NodeManager_Impl::getDynamicResources - "
+                "getDynamicResources not implemented\n"))
+      throw CORBA::NO_IMPLEMENT ();
     return 0;
   }
 } //DAnCE
