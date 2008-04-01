2dcaf127a04310251ef3fb8d249808bb4abe8627
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-04-01 19:17:37 +0000

    ChangeLogTag: Tue Apr  1 19:14:04 UTC 2008  Jeff Parsons <j.parsons@vanderbilt.edu>

--- branches/MonitorControl/TAO/performance-tests/Throughput/client.cpp
+++ branches/MonitorControl/TAO/performance-tests/Throughput/client.cpp
@@ -3,27 +3,33 @@
 #include "TestC.h"
 #include "ace/High_Res_Timer.h"
 #include "ace/Get_Opt.h"
+#include "ace/Task.h"
 #include "tao/Strategies/advanced_resource.h"
+#include "tao/Monitor/Monitor.h"
 
-ACE_RCSID(Throughput, client, "$Id$")
+ACE_RCSID (Throughput,
+           client,
+           "$Id$")
 
-const char *ior = "file://test.ior";
+const char *test_ior = "file://test.ior";
+const char *monitor_ior = "monitor.ior";
 int message_size  = 2048;
 int message_count = 10 * 1024;
 int test_runs   = 6;
-int do_shutdown = 0;
+bool do_shutdown = false;
+bool expose_monitors = false;
 
 int
 parse_args (int argc, char *argv[])
 {
-  ACE_Get_Opt get_opts (argc, argv, "k:b:i:n:x");
+  ACE_Get_Opt get_opts (argc, argv, "k:b:i:n:mx");
   int c;
 
   while ((c = get_opts ()) != -1)
     switch (c)
       {
       case 'k':
-        ior = get_opts.opt_arg ();
+        test_ior = get_opts.opt_arg ();
         break;
 
       case 'b':
@@ -37,9 +43,12 @@ parse_args (int argc, char *argv[])
       case 'n':
         test_runs = ACE_OS::atoi (get_opts.opt_arg ());
         break;
+        
+      case 'm':
+        expose_monitors = true;
 
       case 'x':
-        do_shutdown = 1;
+        do_shutdown = true;
         break;
 
       case '?':
@@ -50,6 +59,8 @@ parse_args (int argc, char *argv[])
                            "-b <message_size> "
                            "-i <message_count> "
                            "-n <test_repetitions> "
+                           "-m "
+                           "-x "
                            "\n",
                            argv [0]),
                           -1);
@@ -58,28 +69,83 @@ parse_args (int argc, char *argv[])
   return 0;
 }
 
+/// Runs the ORB in a separate thread so we can listen
+/// for calls on our MC interface without blocking.
+class ORB_Runner : public ACE_Task_Base
+{
+private:
+  CORBA::ORB_ptr orb_;
+  
+public:
+  ORB_Runner (CORBA::ORB_ptr orb)
+    : orb_ (orb)
+  {}
+  
+  int svc (void)
+  {
+    this->orb_->run ();
+    return 0;
+  }
+};
+
 int
-ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
   try
     {
-      CORBA::ORB_var orb =
-        CORBA::ORB_init (argc, argv);
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
 
       if (parse_args (argc, argv) != 0)
-        return 1;
+        {
+          return 1;
+        }
+
+      if (expose_monitors)
+        {
+          /// Here we take on aspects of a server role, exposing the
+          /// Monitor interface of our ORB so the monitoring client
+          /// can access it remotely. No need to activate the POA
+          /// manager etc here - it is done in the Object_Loader code
+          /// in the Monitor lib.
+          CORBA::Object_var obj =
+            orb->resolve_initial_references ("Monitor");
+          Monitor::MC_var monitor = Monitor::MC::_narrow (obj.in ());
+          
+          CORBA::String_var mon_ior_str =
+            orb->object_to_string (monitor.in ());
+          
+	        FILE *output_file = ACE_OS::fopen (monitor_ior, "w");
+
+	        if (output_file == 0)
+            {
+	            ACE_ERROR_RETURN ((LM_ERROR,
+			                            "Can't open output file for writing IOR: %s",
+			                            monitor_ior),
+			                          1);
+            }
+
+	        ACE_OS::fprintf (output_file, 
+                            "%s", 
+                            mon_ior_str.in ());
+	        ACE_OS::fclose (output_file);
+    	    
+	        /// Call orb->run() in a separate thread so the MC interface can
+	        /// get remote calls.
+	        ORB_Runner orb_runner (orb.in ());
+	        orb_runner.activate ();
+	      }
 
       CORBA::Object_var tmp =
-        orb->string_to_object(ior);
+        orb->string_to_object (test_ior);
 
       Test::Receiver_Factory_var receiver_factory =
-        Test::Receiver_Factory::_narrow(tmp.in ());
+        Test::Receiver_Factory::_narrow (tmp.in ());
 
       if (CORBA::is_nil (receiver_factory.in ()))
         {
           ACE_ERROR_RETURN ((LM_DEBUG,
                              "Nil receiver factory reference <%s>\n",
-                             ior),
+                             test_ior),
                             1);
         }
 
@@ -100,6 +166,7 @@ ACE_TMAIN(int argc, ACE_TCHAR *argv[])
             receiver_factory->create_receiver ();
 
           ACE_hrtime_t start = ACE_OS::gethrtime ();
+          
           for (int i = 0; i != message_count; ++i)
             {
               message.message_id = i;
---
+++ branches/MonitorControl/TAO/performance-tests/Throughput/monitor_client.cpp
@@ -0,0 +1,59 @@
+#include "ace/streams.h"
+#include "ace/OS_NS_unistd.h"
+
+#include "tao/Monitor/Monitor.h"
+
+const char *monitor_input_file = "file://monitor.ior";
+
+int 
+main (int argc, char *argv[])
+{
+  try
+    {
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, 
+                                            argv, 
+                                            "");
+      
+      /// Get the MC object reference that the client has exposed.
+      CORBA::Object_var obj =
+        orb->string_to_object (monitor_input_file);
+        
+      if (CORBA::is_nil (obj.in ()))
+        {
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "Monitor client - string_to_object failed\n"),
+                             -1);
+        }
+        
+      Monitor::MC_var monitor = Monitor::MC::_narrow (obj.in ());
+      
+      if (CORBA::is_nil (monitor.in ()))
+        {
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             "Monitor client - narrow failed\n"),
+                             -1);
+        }
+        
+      /// Access the monitor's value a few times and watch it grow.
+      for (int i = 0; i < 8; ++i)
+        {
+          Monitor::MC::Data data =
+            monitor->get_statistic ("Output CDR Buffer");
+            
+          cout << "client marshal buffer size = " << data.value
+               << " bytes" << endl;
+            
+          ACE_OS::sleep (1);
+        }
+      
+      orb->destroy ();
+    }
+  catch (CORBA::Exception &ex)
+    {
+      ACE_PRINT_EXCEPTION (ACE_ANY_EXCEPTION,
+                           "Monitor_client: exception caught - ");
+      return 1;
+    }
+
+  return 0;
+}
