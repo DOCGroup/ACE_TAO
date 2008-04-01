95f224b45219571d9d743bd5703b8532c2fcc78b
Johnny Willemsen <jwillemsen@remedy.nl>
2008-04-01 08:47:01 +0000

    Tue Apr  1 08:46:12 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/examples/Logging/run_test.pl
+++ TAO/examples/Logging/run_test.pl
@@ -13,7 +13,7 @@ use PerlACE::Run_Test;
 $status = 0;
 
 # amount of delay between running the servers
-$sleeptime = 7;
+$sleeptime = 15;
 
 $SV = new PerlACE::Process ("Logging_Service");
 $CL = new PerlACE::Process ("Logging_Test");
