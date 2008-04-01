1cae543544f2ba99c1c32bbf0932d9cfbf44f2fd
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-04-01 14:17:09 +0000

    ChangeLogTag: Tue Apr  1 14:11:00 UTC 2008  Vladimir Zykov  <vladimir.zykov@prismtech.com>

---
+++ TAO/tests/Collocated_Forwarding/Server_Request_Interceptor.h
@@ -0,0 +1,83 @@
+// -*- C++ -*-
+
+#ifndef SERVER_REQUEST_INTERCEPTOR_H
+#define SERVER_REQUEST_INTERCEPTOR_H
+
+#include "ace/config-all.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "testC.h"
+#include "tao/LocalObject.h"
+
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:4250)
+#endif /* _MSC_VER */
+
+/**
+ * @class Server_Request_Interceptor
+ *
+ * @brief Simple concrete server request interceptor.
+ */
+class Server_Request_Interceptor
+  : public virtual Collocated_ForwardRequestTest::ServerRequestInterceptor,
+    public virtual TAO_Local_RefCounted_Object
+{
+public:
+
+  /// Constructor.
+  Server_Request_Interceptor (CORBA::ULong request_pass_count);
+
+  /// Set the references to which requests will be forwarded.
+  virtual void forward (CORBA::Object_ptr to);
+
+  /**
+   * @name Methods Required by the Server Request Interceptor
+   * Interface
+   *
+   * These are methods that must be implemented since they are pure
+   * virtual in the abstract base class.  They are the canonical
+   * methods required for all server request interceptors.
+   */
+  //@{
+  /// Return the name of this ServerRequestinterceptor.
+  virtual char * name (void);
+
+  virtual void destroy (void);
+
+  virtual void receive_request_service_contexts (
+      PortableInterceptor::ServerRequestInfo_ptr ri);
+
+  virtual void receive_request (
+      PortableInterceptor::ServerRequestInfo_ptr ri);
+
+  virtual void send_reply (
+      PortableInterceptor::ServerRequestInfo_ptr ri);
+
+  virtual void send_exception (
+      PortableInterceptor::ServerRequestInfo_ptr ri);
+
+  virtual void send_other (
+      PortableInterceptor::ServerRequestInfo_ptr ri);
+  //@}
+
+private:
+
+  /// How much requests to pass thru before forwarding.
+  CORBA::ULong request_pass_count_;
+
+  /// The number of requests intercepted by this interceptor.
+  CORBA::ULong request_count_;
+
+  /// References to the object used in this test.
+  CORBA::Object_ptr to_;
+};
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif /* _MSC_VER */
+
+#endif  /* SERVER_REQUEST_INTERCEPTOR_H */
