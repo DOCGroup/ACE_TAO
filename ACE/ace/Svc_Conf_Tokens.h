882e3727cd202e3f2bb6aca4d8174a3a27fd7f0b
iliyan <iliyan@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-04-05 11:54:32 +0000

    ChangeLogTag: Sat Apr  5 11:50:40 UTC 2008  Iliyan Jeliazkov  <iliyan@ociweb.com>

--- ACE/ace/Svc_Conf_Tokens.h
+++ ACE/ace/Svc_Conf_Tokens.h
@@ -1,11 +1,13 @@
-// $Id$
+// $Id:$
 #ifndef BISON_SVC_CONF_TAB_H
 # define BISON_SVC_CONF_TAB_H
 # define ACE_YYSTYPE_IS_DECLARED 1
-/* A Bison parser, made by GNU Bison 2.0.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Skeleton parser for Yacc-like parsing with Bison,
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
