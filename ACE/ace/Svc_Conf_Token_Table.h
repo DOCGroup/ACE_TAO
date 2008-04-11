67c888f946aa9497399be34c3fea6adcc271a312
Johnny Willemsen <jwillemsen@remedy.nl>
2008-04-11 06:50:58 +0000

    added missing Id

--- ACE/ace/Svc_Conf_Token_Table.h
+++ ACE/ace/Svc_Conf_Token_Table.h
@@ -2,6 +2,8 @@
 
 /* Skeleton interface for Bison's Yacc-like parsers in C
 
+   $Id$
+
    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
    Free Software Foundation, Inc.
 
