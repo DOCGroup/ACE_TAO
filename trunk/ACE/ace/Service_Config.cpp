de5a4bf182d085a6ebca1dfe2ad21095e5a252dc
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-04-14 09:00:32 +0000

    Mon Apr 14 08:59:57 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ACE/ace/Service_Config.cpp
+++ trunk/ACE/ace/Service_Config.cpp
@@ -30,61 +30,6 @@ ACE_RCSID (ace,
 
 ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 
-template <>
-ACE_Threading_Helper<ACE_Thread_Mutex>::ACE_Threading_Helper ()
-  :  key_ (ACE_OS::NULL_key)
-{
-# if defined (ACE_HAS_TSS_EMULATION)
-  ACE_Object_Manager::init_tss ();
-# endif
-
-  if (ACE_Thread::keycreate (&key_, 0, 0) == -1)
-    {
-      ACE_ERROR ((LM_ERROR,
-                  ACE_TEXT ("(%P|%t) Failed to create thread key: %p\n"),
-                  ""));
-    }
-}
-
-template <>
-ACE_Threading_Helper<ACE_Null_Mutex>::ACE_Threading_Helper ()
-  :  key_ (ACE_OS::NULL_key)
-{
-}
-
-template <> void
-ACE_Threading_Helper<ACE_Thread_Mutex>::set (void* p)
-{
-  if (ACE_Thread::setspecific (key_, p) == -1)
-    ACE_ERROR ((LM_ERROR,
-                       ACE_TEXT ("(%P|%t) Service Config failed to set thread key value: %p\n"),
-                       ""));
-}
-
-template <> void
-ACE_Threading_Helper<ACE_Null_Mutex>::set (void*)
-{
-}
-
-template <> void*
-ACE_Threading_Helper<ACE_Thread_Mutex>::get (void)
-{
-  void* temp = 0;
-  if (ACE_Thread::getspecific (key_, &temp) == -1)
-    ACE_ERROR_RETURN ((LM_ERROR,
-                       ACE_TEXT ("(%P|%t) Service Config failed to get thread key value: %p\n"),
-                       ""),
-                      0);
-  return temp;
-}
-
-template <> void*
-ACE_Threading_Helper<ACE_Null_Mutex>::get (void)
-{
-  return ACE_Service_Config::singleton()->instance_.get ();
-}
-
-
 /**
   * @c ACE_Service_Config is supposed to be a Singleton. This is the
   * only Configuration Gestalt available for access from static
@@ -560,7 +505,7 @@ ACE_Service_Config::reconfigure (void)
 int
 ACE_Service_Config::close (void)
 {
-    ACE_Service_Config::singleton ()->instance_->close ();
+  ACE_Service_Config::singleton ()->instance_->close ();
 
   // Delete the service repository.  All the objects inside the
   // service repository should already have been finalized.
@@ -614,7 +559,5 @@ ACE_Service_Config::reconfig_occurred (int config_occurred)
   ACE_TRACE ("ACE_Service_Config::reconfig_occurred");
   ACE_Service_Config::reconfig_occurred_ = config_occurred;
 }
-
-// ************************************************************
 
 ACE_END_VERSIONED_NAMESPACE_DECL
--- trunk/ACE/ace/Service_Config.inl
+++ trunk/ACE/ace/Service_Config.inl
@@ -8,19 +8,6 @@ ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 
 // This is the primary entry point into the ACE_Service_Config (the
 // constructor just handles simple initializations).
-
-template <> ACE_INLINE
-ACE_Threading_Helper<ACE_Thread_Mutex>::~ACE_Threading_Helper ()
-{
-  ACE_OS::thr_key_detach (this->key_, 0);
-  ACE_OS::thr_keyfree (this->key_);
-}
-
-template <> ACE_INLINE
-ACE_Threading_Helper<ACE_Null_Mutex>::~ACE_Threading_Helper ()
-{
-}
-
 ACE_INLINE int
 ACE_Service_Config::open (const ACE_TCHAR program_name[],
                           const ACE_TCHAR *logger_key,
@@ -132,8 +119,7 @@ ACE_Service_Config::initialize (const ACE_Service_Type *sr,
                                 const ACE_TCHAR *parameters)
 {
   ACE_TRACE ("ACE_Service_Config::initialize");
-  return ACE_Service_Config::current ()->initialize (sr,
-                                                     parameters);
+  return ACE_Service_Config::current ()->initialize (sr, parameters);
 }
 
 /// Process a file containing a list of service configuration
