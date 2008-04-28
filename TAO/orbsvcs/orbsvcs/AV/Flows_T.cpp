c41a1e1f4f37530e7b186a5844199c112184cd24
Johnny Willemsen <jwillemsen@remedy.nl>
2008-04-28 17:58:17 +0000

    Mon Apr 28 17:57:32 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/AV/Flows_T.cpp
+++ TAO/orbsvcs/orbsvcs/AV/Flows_T.cpp
@@ -92,7 +92,7 @@ TAO_FDev<T_Producer, T_Consumer>::make_producer (AVStreams::FlowConnection_ptr /
   try
     {
       // Activate the producer implementation under the Root POA.
-      T_Producer *producer_i;
+      T_Producer *producer_i = 0;
       ACE_NEW_RETURN (producer_i, T_Producer, 0);
       this->producer_list_.insert_tail (producer_i);
       producer = producer_i->_this ();
