6573fe3e8ed32ee286ef6dc2ceb49d401c1ea5d7
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-05-01 18:14:05 +0000

    ChangeLogTag: hu May  1 18:09:05 UTC 2008  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/tao/Monitor/Monitor_Impl.cpp
+++ TAO/tao/Monitor/Monitor_Impl.cpp
@@ -7,7 +7,7 @@
 
 #include "ace/Monitor_Point_Registry.h"
 #include "ace/Monitor_Control_Action.h"
-#include "MonitorControl/MonitorControl.h"
+#include "ace/MonitorControl/MonitorControl.h"
 
 ACE_RCSID (Monitor,
            Monitor_Impl,
