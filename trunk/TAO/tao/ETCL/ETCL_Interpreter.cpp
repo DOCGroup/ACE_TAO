8f7d69ade731b0c273ca068ec696944c34cb2042
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-05-02 19:39:07 +0000

    ChangeLogTag: Fri May  2 19:33:55 UTC 2008  Jeff Parsons <j.parsons@vanderbilt.edu>

---
+++ trunk/TAO/tao/ETCL/ETCL_Interpreter.cpp
@@ -0,0 +1,10 @@
+// -*- C++ -*-
+// $Id$
+
+// Since there seems to be no known way of setting up a makefile to
+// recompile source files from another build into new object files
+// in a different location, and have realclean, etc. depend on the
+// new object files, we must do this.
+//  -- Jeff Parsons  5/2/2008
+
+#include "ace/ETCL/ETCL_Interpreter.cpp"
