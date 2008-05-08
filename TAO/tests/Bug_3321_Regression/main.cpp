d67c42e82b03f1f22200f633f2aa5524fc780089
Johnny Willemsen <jwillemsen@remedy.nl>
2008-05-08 07:40:20 +0000

    Thu May  8 07:38:10 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

---
+++ TAO/tests/Bug_3321_Regression/main.cpp
@@ -0,0 +1,70 @@
+// $Id$
+
+#include "tao/ORB.h"
+#include "ace/Log_Msg.h"
+#include "ace/Thread.h"
+#include "ace/os_include/os_pthread.h"
+
+#include "tao/ORB.h"
+#include "ace/Log_Msg.h"
+#include "tao/Messaging/TAO_ExtC.h"
+#include "tao/Messaging/Messaging_RT_PolicyC.h"
+#include "tao/AnyTypeCode/Any.h"
+#include "tao/Policy_Manager.h"
+
+int result = 0;
+
+ACE_THR_FUNC_RETURN main_orb(void*)
+{
+  try
+  {
+    int argc = 0;
+    CORBA::ORB_var orb = CORBA::ORB_init (argc, 0);
+
+    ACE_Time_Value const connectionTimout(100);
+    ACE_Time_Value const requestReplyTimeout(100);
+
+    TimeBase::TimeT CONNECTION_TIMEOUT = 1000000;
+    TimeBase::TimeT ROUND_TRIP_TIMEOUT = 1000000;
+
+    if (!CORBA::is_nil(orb.in()))
+      {
+        ACE_DEBUG((LM_DEBUG, "ORB not nil\n"));
+
+        CORBA::Any connection_timeout_as_any;
+        connection_timeout_as_any <<= CONNECTION_TIMEOUT;
+
+        CORBA::Any rt_timeout_as_any;
+        rt_timeout_as_any <<= ROUND_TRIP_TIMEOUT;
+
+        orb->destroy ();
+        ACE_DEBUG((LM_DEBUG, "Destroyed ORB\n"));
+      }
+  }
+  catch(const ::CORBA::Exception& ex)
+  {
+    result = 1;
+    ex._tao_print_exception ("Exception caught:");
+  }
+  catch(...)
+  {
+    result = 1;
+    ACE_ERROR ((LM_ERROR, " (%P|%t) Unknown error\n"));
+  }
+
+  return 0;
+}
+
+
+int ACE_TMAIN(int, ACE_TCHAR*[])
+{
+  ACE_hthread_t thread1;
+# if defined (ACE_HAS_PTHREADS)
+  pthread_create(&thread1, 0, main_orb, 0);
+#else
+  ACE_Thread::spawn (main_orb, 0, THR_NEW_LWP|THR_JOINABLE, 0, &thread1);
+#endif
+  ACE_Thread::join( thread1);
+
+  return result;
+}
