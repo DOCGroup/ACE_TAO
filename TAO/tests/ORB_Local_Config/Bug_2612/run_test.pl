937ff70419c9f07317e8cef9bbe6f92b8bd7c431
Johnny Willemsen <jwillemsen@remedy.nl>
2008-05-13 07:43:59 +0000

    Tue May 13 07:37:10 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/ORB_Local_Config/Bug_2612/run_test.pl
+++ TAO/tests/ORB_Local_Config/Bug_2612/run_test.pl
@@ -8,28 +8,20 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::Run_Test;
 
-sub add_path {
-  my($name)  = shift;
-  my($value) = shift;
-  if (defined $ENV{$name}) {
-    $ENV{$name} .= ':' . $value
-  }
-  else {
-    $ENV{$name} = $value;
-  }
-}
-
-# Set the library path for the client to be able to load
-# the Time_Date library.
-add_path('LD_LIBRARY_PATH', '../lib');
-add_path('LIBPATH', '../lib');
-add_path('SHLIB_PATH', '../lib');
+# Set the library path for the client to be able to load the library
+PerlACE::add_lib_path ('../lib');
 
 sub test($)
 {
     (my $executable, my $arguments) = @_;
-    my $t1 = new PerlACE::Process ($executable, ($arguments ? $arguments : ""));
-    my $status = $t1->SpawnWaitKill (10);
+    my $t1;
+    if (PerlACE::is_vxworks_test()) {
+      $t1 = new PerlACE::ProcessVX ($executable, ($arguments ? $arguments : ""));
+    }
+    else {
+      $t1 = new PerlACE::Process ($executable, ($arguments ? $arguments : ""));
+    }
+    my $status = $t1->SpawnWaitKill ($PerlACE::wait_interval_for_process_creation);
     if ($status != 0) {
         print STDERR "ERROR: test failed, status=$status\n";
     }
