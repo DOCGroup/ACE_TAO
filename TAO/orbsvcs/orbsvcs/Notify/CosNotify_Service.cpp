e234aaaf7cd69d431bf1d32354e68c102e89538f
Johnny Willemsen <jwillemsen@remedy.nl>
2008-05-13 14:50:51 +0000

    Tue May 13 14:50:10 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/orbsvcs/orbsvcs/Notify/CosNotify_Service.cpp
+++ TAO/orbsvcs/orbsvcs/Notify/CosNotify_Service.cpp
@@ -215,6 +215,17 @@ TAO_CosNotify_Service::set_threads (CosNotification::QoSProperties &qos, int thr
 int
 TAO_CosNotify_Service::fini (void)
 {
+  if (TAO_Notify_PROPERTIES::instance()->separate_dispatching_orb())
+    {
+      if (!CORBA::is_nil (TAO_Notify_PROPERTIES::instance()->dispatching_orb()))
+        {
+          CORBA::ORB_var dispatcher =
+            TAO_Notify_PROPERTIES::instance()->dispatching_orb();
+          dispatcher->shutdown ();
+          dispatcher->destroy ();
+        }
+    }
+
   TAO_Notify_Properties::instance()->close ();
   return 0;
 }
