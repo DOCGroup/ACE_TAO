5332e950d1ad2feadfc544f3d6848d9e86af0d8b
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-05-15 10:18:07 +0000

    Thu May 15 10:17:15 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ACE/tests/Unbounded_Set_Test.cpp
+++ trunk/ACE/tests/Unbounded_Set_Test.cpp
@@ -46,50 +46,103 @@ int
 run_main (int, ACE_TCHAR *[])
 {
   int r;
+  int retval = 0;
   unsigned k;
   MyNode node (1);
 
   ACE_START_TEST (ACE_TEXT ("Unbounded_Set_Test"));
 
   ACE_Unbounded_Set<MyNode> ubs;
-  ACE_ASSERT (ubs.size () == 0);
+  if (ubs.size () != 0)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: ubs.size () != 0\n"));
+      retval = -1;
+    }
 
   // Insert a value. Immediately remove it.
   r = ubs.insert (node);
-  ACE_ASSERT (r == 0);
-  ACE_ASSERT (ubs.size () == 1);
+  if (r != 0)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: r != 0\n"));
+      retval = -1;
+    }
+  if (ubs.size () != 1)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: ubs.size () != 1\n"));
+      retval = -1;
+    }
   r = ubs.remove (node);
-  ACE_ASSERT (r == 0);
-  ACE_ASSERT (ubs.size () == 0);
+  if (r != 0)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: r != 0\n"));
+      retval = -1;
+    }
+  if (ubs.size () != 0)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: ubs.size () != 0\n"));
+      retval = -1;
+    }
 
   // Insert several different values.
   for (node.k = 1; node.k <= 5; node.k++)
     {
       r = ubs.insert (node);
-      ACE_ASSERT (r == 0);
-      ACE_ASSERT (ubs.size () == node.k);
+      if (r != 0)
+        {
+          ACE_ERROR ((LM_ERROR, "Error: r != 0\n"));
+          retval = -1;
+        }
+      if (ubs.size () != node.k)
+        {
+          ACE_ERROR ((LM_ERROR, "Error: ubs.size () != node.k\n"));
+          retval = -1;
+        }
     }
 
   // Test assigment of sets.
   // To do that, we also test some of the iterator methods.
   typedef ACE_Unbounded_Set<MyNode> MySet;
   MySet ubs2 = ubs;              // Test a typedef of a set.
-  ACE_ASSERT (ubs2.size() == ubs.size());
+  if (ubs2.size() != ubs.size())
+    {
+      ACE_ERROR ((LM_ERROR, "Error: ubs2.size() != ubs.size()\n"));
+      retval = -1;
+    }
   {
     MySet::ITERATOR it1 (ubs);
     MySet::iterator it2 (ubs2);
     for (k = 1; k <= 5; k++)
       {
-        ACE_ASSERT (! it1.done ());
-        ACE_ASSERT (! it2.done ());
+        if (it1.done ())
+          {
+            ACE_ERROR ((LM_ERROR, "Error: it1.done ()\n"));
+            retval = -1;
+          }
+        if (it2.done ())
+          {
+            ACE_ERROR ((LM_ERROR, "Error: it2.done ()\n"));
+            retval = -1;
+          }
         MyNode n1 = *it1;
         MyNode n2 = *it2;
-        ACE_ASSERT (n1 == n2);
+        if (!(n1 == n2))
+          {
+            ACE_ERROR ((LM_ERROR, "Error: !(n1 == n2)\n"));
+            retval = -1;
+          }
         it1.advance ();
         it2.advance ();
       }
-    ACE_ASSERT (it1.done ());
-    ACE_ASSERT (it2.done ());
+    if (!it1.done ())
+      {
+        ACE_ERROR ((LM_ERROR, "Error: !it1.done ()\n"));
+        retval = -1;
+      }
+    if (!it2.done ())
+      {
+        ACE_ERROR ((LM_ERROR, "Error: !it2.done ()\n"));
+        retval = -1;
+      }
     // Verify that a set may be emptied while an iterator on the set is
     // in-scope but inactive:
     ubs.reset ();
@@ -115,21 +168,50 @@ run_main (int, ACE_TCHAR *[])
             deleted++;
           }
       }
-    ACE_ASSERT (ubs2.size () + deleted == ubs.size());
+    if (ubs2.size () + deleted != ubs.size())
+      {
+        ACE_ERROR ((LM_ERROR, "Error: ubs2.size () + deleted != ubs.size()\n"));
+        retval = -1;
+      }
 
     MyNode node2 (2);
-    ACE_ASSERT (ubs2.find (node2) == 0);
+    if (ubs2.find (node2) != 0)
+      {
+        ACE_ERROR ((LM_ERROR, "Error: ubs2.find (node2) != 0\n"));
+        retval = -1;
+      }
 
     MyNode node3 (3);
-    ACE_ASSERT (ubs2.find (node3) != 0);
+    if (ubs2.find (node3) == 0)
+      {
+        ACE_ERROR ((LM_ERROR, "Error: ubs2.find (node3) == 0\n"));
+        retval = -1;
+      }
 
     ubs2.insert (node3);
   }
 
   size_t s = count_const_set (ubs);
-  ACE_ASSERT (s == ubs.size ());
+  if (s != ubs.size ())
+    {
+      ACE_ERROR ((LM_ERROR, "Error: s != ubs.size ()\n"));
+      retval = -1;
+    }
+
+  ACE_Unbounded_Set<MyNode> ubs_insert;
+  MyNode node4 (4);
+  if (ubs_insert.insert (node4) != 0)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: insert node4 failed\n"));
+      retval = -1;
+    }
+  if (ubs_insert.insert (node4) != 1)
+    {
+      ACE_ERROR ((LM_ERROR, "Error: insert node4 didn't return 1\n"));
+      retval = -1;
+    }
 
   ACE_END_TEST;
-  return 0;
+  return retval;
 }
 
