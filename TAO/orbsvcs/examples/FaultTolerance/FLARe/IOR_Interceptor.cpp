0cbddee90ca2d09a0c1b730bf3a8395637fd370c
jai <jai@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-05-26 16:24:27 +0000

    added FLARe directory

---
+++ TAO/orbsvcs/examples/FaultTolerance/FLARe/IOR_Interceptor.cpp
@@ -0,0 +1,63 @@
+#include "IOR_Interceptor.h"
+#include "ObjectReferenceFactory.h"
+#include "tao/ORB_Constants.h"
+
+ACE_RCSID (Hello,
+           IOR_Interceptor,
+           "$Id$")
+
+IOR_Interceptor::IOR_Interceptor (void)
+{
+}
+
+char *
+IOR_Interceptor::name (void)
+{
+  return CORBA::string_dup ("IOR_Interceptor");
+}
+
+void
+IOR_Interceptor::destroy (void)
+{
+}
+
+void
+IOR_Interceptor::establish_components (
+    PortableInterceptor::IORInfo_ptr /* info */)
+{
+}
+
+void
+IOR_Interceptor::components_established (
+    PortableInterceptor::IORInfo_ptr info)
+{
+  PortableInterceptor::ObjectReferenceFactory_var old_orf =
+    info->current_factory ();
+
+  PortableInterceptor::ObjectReferenceFactory * tmp;
+  ACE_NEW_THROW_EX (tmp,
+                    ObjectReferenceFactory (old_orf.in ()),
+                    CORBA::NO_MEMORY (
+                      CORBA::SystemException::_tao_minor_code (
+                        TAO::VMCID,
+                        ENOMEM),
+                      CORBA::COMPLETED_NO));
+
+  PortableInterceptor::ObjectReferenceFactory_var orf = tmp;
+
+  info->current_factory (orf.in ());
+}
+
+void
+IOR_Interceptor::adapter_manager_state_changed (
+    const char *,
+    PortableInterceptor::AdapterState)
+{
+}
+
+void
+IOR_Interceptor:: adapter_state_changed (
+    const PortableInterceptor::ObjectReferenceTemplateSeq &,
+    PortableInterceptor::AdapterState)
+{
+}
