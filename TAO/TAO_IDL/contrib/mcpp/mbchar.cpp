610dcc97547c80b5736fae56f43b51967203d77c
William R. Otte <wotte@dre.vanderbilt.edu>
2008-06-02 14:59:15 +0000

    ChangeLogTag: Mon Jun  2 14:58:26 UTC 2008  William R. Otte  <wotte@dre.vanderbilt.edu>

--- TAO/TAO_IDL/contrib/mcpp/mbchar.cpp
+++ TAO/TAO_IDL/contrib/mcpp/mbchar.cpp
@@ -331,8 +331,8 @@ static size_t   mb_read_utf8( int c1, char ** in_pp, char ** out_pp);
 #define NAMLEN  20
 
 const char *    set_encoding(const char *  name,       /* Name of encoding specified   */
-			     const char *  env,        /* Name of environment variable */
-			     int     pragma
+                             const char *  env,        /* Name of environment variable */
+                             int     pragma
         /* 2: #pragma setlocale, 1: #pragma __setlocale, 0: not #pragma */
 )
 /*
