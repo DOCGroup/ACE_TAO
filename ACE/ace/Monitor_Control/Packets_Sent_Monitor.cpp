af6ca5cc4bbe094ad729f0a2333761f099904ff9
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-06-10 21:35:51 +0000

    ChangeLogTag: Tue Jun 10 21:33:25 UTC 2008  Jeff Parsons <j.parsons@vanderbilt.edu>

--- ACE/ace/Monitor_Control/Packets_Sent_Monitor.cpp
+++ ACE/ace/Monitor_Control/Packets_Sent_Monitor.cpp
@@ -17,11 +17,13 @@ namespace ACE
       : Monitor_Base (name)
 #if defined (ACE_HAS_WIN32_PDH)
       , Windows_Multi_Instance_Monitor (
-          ACE_TEXT("\\Network Interface(*)\\Packets Sent/sec"))
+          ACE_TEXT ("\\Network Interface(*)\\Packets Sent/sec"))
 #elif defined (linux)
         , Linux_Network_Interface_Monitor (
             " %*[^:]: %*u %*u %*u %*u %*u %*u %*u %*u %*u %lu")
             /// Scan format for /proc/net/dev
+#elif defined (ACE_HAS_KSTAT)
+        , Solaris_Network_Interface_Monitor (ACE_TEXT ("opackets"))
 #endif
     {}
 
@@ -35,6 +37,11 @@ namespace ACE
       this->receive (this->value_.doubleValue);
 #elif defined (linux)
       this->lin_update ();
+
+      /// Stores value and timestamp with thread-safety.
+      this->receive (static_cast<double> (this->value_));
+#elif defined (ACE_HAS_KSTAT)
+      this->sun_update ();
 
       /// Stores value and timestamp with thread-safety.
       this->receive (static_cast<double> (this->value_));
