cd32cc027e905bcff7d7c0a274d2684930b9470f
Adam Mitz <mitza-oci@users.noreply.github.com>
2008-06-12 14:43:09 +0000

    ChangeLogTag: Thu Jun 12 14:38:19 UTC 2008  Adam Mitz  <mitza@ociweb.com>

--- ACE/ace/Stack_Trace.h
+++ ACE/ace/Stack_Trace.h
@@ -52,8 +52,8 @@
  *        - Any platform using glibc as its runtime library, or where ACE_HAS_EXECINFO_H is defined 
  *          (this covers Linux and Mac) and gcc version >= 3.3.
  *        - VxWorks, both kernel and RTP
- *        - Solaris (not Sparc 64bits)
- *        - Windows 32 and 64 bit (not WinCE/mobile, not MinGW)  
+ *        - Solaris
+ *        - Windows 32 and 64 bit (Visual C++, excluding WinCE/mobile)
  *
  *  @note Since stack trace buffer size has limitation(@c ACE_STACK_TRACE_SYMBUFSIZ), you will not 
  *        get a complete stack trace if @c ACE_STACK_TRACE_SYMBUFSIZ value is less than actual stack 
