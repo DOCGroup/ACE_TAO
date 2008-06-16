dc40816626b074a14b9d8cd5919f1ace20d9d305
sowayaa <sowayaa@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-06-16 18:00:46 +0000

    Mon Jun 16 18:00:26 UTC 2008  Abdullah Sowayan  <abdullah.sowayan@lmco.com>

--- trunk/ACE/ASNMP/examples/next/next.cpp
+++ trunk/ACE/ASNMP/examples/next/next.cpp
@@ -189,8 +189,8 @@ int nextapp::run()
       else {
         VbIter iter(pdu_);
         while (iter.next(vb)) {
- 	  cout << "\tOid = " << vb.to_string_oid() << "\n";
- 	  cout << "\tValue = " << vb.to_string_value() << "\n";
+          cout << "\tOid = " << vb.to_string_oid() << "\n";
+          cout << "\tValue = " << vb.to_string_value() << "\n";
        }
      }
    }
