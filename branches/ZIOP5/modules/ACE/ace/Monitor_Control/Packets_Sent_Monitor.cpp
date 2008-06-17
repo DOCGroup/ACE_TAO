82b2b8dfa7f42ed238ec93e90fca900a009ee59e
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-06-17 12:38:14 +0000

    branching/tagging

---
+++ branches/ZIOP5/modules/ACE/ace/Monitor_Control/Packets_Sent_Monitor.cpp
@@ -0,0 +1,52 @@
+// $Id$
+
+#include "ace/Monitor_Control/Packets_Sent_Monitor.h"
+
+#if defined (ACE_HAS_MONITOR_FRAMEWORK) && (ACE_HAS_MONITOR_FRAMEWORK == 1)
+
+ACE_BEGIN_VERSIONED_NAMESPACE_DECL
+
+namespace ACE
+{
+  namespace Monitor_Control
+  {
+    const char* Packets_Sent_Monitor::default_name_ =
+      "OS/Network/PacketsSent";
+
+    Packets_Sent_Monitor::Packets_Sent_Monitor (const char* name)
+      : Monitor_Base (name)
+#if defined (ACE_HAS_WIN32_PDH)
+      , Windows_Multi_Instance_Monitor (
+          ACE_TEXT ("\\Network Interface(*)\\Packets Sent/sec"))
+#elif defined (linux) || defined (AIX)
+        , Linux_Network_Interface_Monitor (
+            " %*[^:]: %*u %*u %*u %*u %*u %*u %*u %*u %*u %lu")
+            /// Scan format for /proc/net/dev
+#elif defined (ACE_HAS_KSTAT)
+        , Solaris_Network_Interface_Monitor ("opackets")
+#elif defined (__FreeBSD__) || defined (__NetBSD__) || defined (__OpenBSD__)
+        , BSD_Network_Interface_Monitor ("opackets") 
+#endif
+    {}
+
+    void
+    Packets_Sent_Monitor::update (void)
+    {
+      this->update_i ();
+      
+      /// On some platforms, value_ is an ACE_UINT64.
+      this->receive (static_cast<double> (this->value_));
+    }
+
+    const char*
+    Packets_Sent_Monitor::default_name (void)
+    {
+      return Packets_Sent_Monitor::default_name_;
+    }
+  }
+}
+
+ACE_END_VERSIONED_NAMESPACE_DECL
+
+#endif /* ACE_HAS_MONITOR_FRAMEWORK==1 */
+
---
+++ branches/ZIOP5/modules/ACE/examples/Web_Crawler/URL.cpp
@@ -0,0 +1,39 @@
+// $Id$
+
+#include "URL.h"
+
+ACE_RCSID(Web_Crawler, URL, "$Id$")
+
+Mem_Map_Stream &
+URL::stream (void)
+{
+  return this->stream_;
+}
+
+URL::~URL (void)
+{
+}
+
+const URL_Status &
+URL::reply_status (void)
+{
+  return this->reply_status_;
+}
+
+void 
+URL::reply_status (const URL_Status &rs)
+{
+  this->reply_status_ = rs;
+}
+
+const ACE_CString &
+URL::content_type (void)
+{
+  return this->content_type_;
+}
+
+void 
+URL::content_type (const ACE_CString &ct)
+{
+  this->content_type_ = ct;
+}
