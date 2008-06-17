8465d8b1eb8b51713e5654e55ef18454bdd5f279
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-06-17 14:54:07 +0000

    ChangeLogTag: Tue Jun 17 14:54:00 UTC 2008  Simon Massey  <sma at prismtech dot com>

--- TAO/tests/POA/Nested_Non_Servant_Upcalls/Nested_Non_Servant_Upcalls.cpp
+++ TAO/tests/POA/Nested_Non_Servant_Upcalls/Nested_Non_Servant_Upcalls.cpp
@@ -196,14 +196,12 @@ Servant_Activator::etherealize (const PortableServer::ObjectId &,
 }
 
 int
-ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
   try
     {
       CORBA::ORB_var orb =
-        CORBA::ORB_init (argc,
-                         argv,
-                         0);
+        CORBA::ORB_init (argc, argv);
 
       CORBA::Object_var obj =
         orb->resolve_initial_references ("RootPOA");
