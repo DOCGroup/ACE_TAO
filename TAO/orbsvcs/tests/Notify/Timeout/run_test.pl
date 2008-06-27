d4d2dc5728577707d58df673f9bd921aa0c5f873
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-06-27 08:00:33 +0000

    ChangeLogTag: Fri Jun 27 08:00:00 UTC 2008  Simon Massey  <sma at prismtech dot com>

--- TAO/orbsvcs/tests/Notify/Timeout/run_test.pl
+++ TAO/orbsvcs/tests/Notify/Timeout/run_test.pl
@@ -8,6 +8,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::Run_Test;
 use strict;
+PerlACE::add_lib_path ('../lib');
 
 PerlACE::check_privilege_group();
 
