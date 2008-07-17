c6ed163d0a6c75a889762e589b13fcd8f4405963
jmoore <jmoore@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-07-17 05:48:07 +0000

    deleting all files previously added to the branch after realizing they are woefully out of ACE compliance

--- branches/priority_inheritance_protocol/ACE/ace/PIP_DA_Strategy_Adapter.cpp
+++
@@ -1,4 +0,0 @@
-// $Id$
-
-#include "PIP_DA_Strategy_Adapter.h"
-
--- branches/priority_inheritance_protocol/ACE/ace/k_Efficient_P_Strategy.cpp
+++
@@ -1,5 +0,0 @@
-#include "ace/k_Efficient_P_Strategy.h"
-
-#if !defined (__ACE_INLINE__)
-//#include "ace/k_Efficient_P_Strategy.inl"
-#endif /* __ACE_INLINE__ */
