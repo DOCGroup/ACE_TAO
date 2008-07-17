5cfeca379c243824d2e6fd12a817cc79145916c1
jmoore <jmoore@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-07-17 05:48:07 +0000

    deleting all files previously added to the branch after realizing they are woefully out of ACE compliance

--- ACE/ace/DA_Strategy_Base.cpp
+++
@@ -1,5 +0,0 @@
-#include "ace/DA_Strategy_Base.h"
-
-#if !defined (__ACE_INLINE__)
-//#include "ace/DA_Strategy_Base.inl"
-#endif /* __ACE_INLINE__ */
--- ACE/ace/Live_P_Strategy.cpp
+++
@@ -1,5 +0,0 @@
-#include "ace/Live_P_Strategy.h"
-
-#if !defined (__ACE_INLINE__)
-#include "ace/Live_P_Strategy.inl"
-#endif /* __ACE_INLINE__ */
