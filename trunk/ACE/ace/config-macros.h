cd40b23b066278c020f583389d8c6987ca944dc8
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-07-31 10:06:11 +0000

    Thu Jul 31 10:05:12 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ACE/ace/config-macros.h
+++ trunk/ACE/ace/config-macros.h
@@ -377,19 +377,21 @@
 // should keep them quiet.
 // ============================================================================
 
-#if defined (ghs) || defined (__GNUC__) || defined (__hpux) || defined (__sgi) || defined (__DECCXX) || defined (__rational__) || defined (__USLC__) || defined (ACE_RM544) || defined (__DCC__) || defined (__PGI) || defined (__TANDEM)
+#if !defined (ACE_UNUSED_ARG)
+# if defined (ghs) || defined (__GNUC__) || defined (__hpux) || defined (__sgi) || defined (__DECCXX) || defined (__rational__) || defined (__USLC__) || defined (ACE_RM544) || defined (__DCC__) || defined (__PGI) || defined (__TANDEM)
 // Some compilers complain about "statement with no effect" with (a).
 // This eliminates the warnings, and no code is generated for the null
 // conditional statement.  @note that may only be true if -O is enabled,
 // such as with GreenHills (ghs) 1.8.8.
-# define ACE_UNUSED_ARG(a) do {/* null */} while (&a == 0)
-#elif defined (__DMC__)
-  #define ACE_UNUSED_ID(identifier)
-  template <class T>
-  inline void ACE_UNUSED_ARG(const T& ACE_UNUSED_ID(t)) { }
-#else /* ghs || __GNUC__ || ..... */
-# define ACE_UNUSED_ARG(a) (a)
-#endif /* ghs || __GNUC__ || ..... */
+#  define ACE_UNUSED_ARG(a) do {/* null */} while (&a == 0)
+# elif defined (__DMC__)
+   #define ACE_UNUSED_ID(identifier)
+   template <class T>
+   inline void ACE_UNUSED_ARG(const T& ACE_UNUSED_ID(t)) { }
+# else /* ghs || __GNUC__ || ..... */
+#  define ACE_UNUSED_ARG(a) (a)
+# endif /* ghs || __GNUC__ || ..... */
+#endif /* !ACE_UNUSED_ARG */
 
 #if defined (_MSC_VER) || defined(__sgi) || defined (ghs) || defined (__DECCXX) || defined(__BORLANDC__) || defined (ACE_RM544) || defined (__USLC__) || defined (__DCC__) || defined (__PGI) || defined (__TANDEM) || (defined (__HP_aCC) && (__HP_aCC >= 60500))
 # define ACE_NOTREACHED(a)
