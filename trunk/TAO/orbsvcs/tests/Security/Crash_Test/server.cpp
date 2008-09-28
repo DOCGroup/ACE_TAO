697689f2251f56fd95bcd0b260c8d0a4a59faae1
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-09-28 11:40:48 +0000

    Sun Sep 28 11:39:45 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/orbsvcs/tests/Security/Crash_Test/server.cpp
+++ trunk/TAO/orbsvcs/tests/Security/Crash_Test/server.cpp
@@ -10,7 +10,7 @@ ACE_RCSID (Send_File,
            server,
            "$Id$")
 
-const char *ior_output_file = 0;
+const ACE_TCHAR *ior_output_file = 0;
 const char *cert_file = "cacert.pem";
 
 int
