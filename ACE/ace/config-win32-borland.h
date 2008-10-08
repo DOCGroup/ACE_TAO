36c018c8c8a36f21cba1f9413039cedd2ac14204
Johnny Willemsen <jwillemsen@remedy.nl>
2008-10-08 09:45:39 +0000

    Wed Oct  8 09:44:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/ace/config-win32-borland.h
+++ ACE/ace/config-win32-borland.h
@@ -11,7 +11,31 @@
 #error Use config-win32.h in config.h instead of this header
 #endif /* ACE_CONFIG_WIN32_H */
 
-#include "config-borland-common.h"
+#define ACE_HAS_CUSTOM_EXPORT_MACROS
+#define ACE_Proper_Export_Flag __declspec (dllexport)
+#define ACE_Proper_Import_Flag __declspec (dllimport)
+#define ACE_EXPORT_SINGLETON_DECLARATION(T) template class __declspec (dllexport) T
+#define ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) template class __declspec (dllexport) SINGLETON_TYPE<CLASS, LOCK>;
+#define ACE_IMPORT_SINGLETON_DECLARATION(T) template class __declspec (dllimport) T
+#define ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) template class __declspec (dllimport) SINGLETON_TYPE <CLASS, LOCK>;
+
+// In later versions of C++Builder we will prefer inline functions by
+// default. The debug configuration of ACE is built with functions
+// out-of-line, so when linking your application against a debug ACE
+// build, you can choose to use the out-of-line functions by adding
+// ACE_NO_INLINE=1 to your project settings.
+# if !defined (__ACE_INLINE__)
+#  define __ACE_INLINE__ 1
+# endif /* __ACE_INLINE__ */
+
+# define ACE_CC_NAME ACE_TEXT ("Borland C++ Builder")
+# define ACE_CC_MAJOR_VERSION (__BORLANDC__ / 0x100)
+# define ACE_CC_MINOR_VERSION (__BORLANDC__ % 0x100)
+# define ACE_CC_BETA_VERSION (0)
+
+# ifndef ACE_USING_MCPP_PREPROCESSOR
+#  define ACE_CC_PREPROCESSOR_ARGS "-q -P- -o%s"
+# endif
 
 // Automatically define WIN32 macro if the compiler tells us it is our
 // target platform.
@@ -30,16 +54,10 @@
 
 # include "ace/config-win32-common.h"
 
-// Borland on win32 has swab
-# undef ACE_LACKS_SWAB
-
 # define ACE_WSTRING_HAS_USHORT_SUPPORT 1
 # define ACE_HAS_DIRENT
 
-#ifdef ACE_USES_STD_NAMESPACE_FOR_STDC_LIB
-#undef ACE_USES_STD_NAMESPACE_FOR_STDC_LIB
 #define ACE_USES_STD_NAMESPACE_FOR_STDC_LIB 1
-#endif
 
 #define ACE_NEEDS_DL_UNDERSCORE
 
@@ -84,6 +102,29 @@
 #define ACE_LACKS_STRRECVFD
 #define ACE_USES_EXPLICIT_STD_NAMESPACE
 
+# define ACE_EXPORT_NESTED_CLASSES 1
+# define ACE_HAS_CPLUSPLUS_HEADERS 1
+# define ACE_HAS_EXCEPTIONS
+# define ACE_HAS_GNU_CSTRING_H 1
+# define ACE_HAS_NONCONST_SELECT_TIMEVAL
+# define ACE_HAS_SIG_ATOMIC_T
+# define ACE_HAS_STANDARD_CPP_LIBRARY 1
+# define ACE_HAS_STDCPP_STL_INCLUDES 1
+# define ACE_HAS_STRERROR
+# define ACE_HAS_STRING_CLASS 1
+# define ACE_HAS_TEMPLATE_TYPEDEFS 1
+# define ACE_HAS_USER_MODE_MASKS 1
+# define ACE_LACKS_ACE_IOSTREAM 1
+# define ACE_LACKS_LINEBUFFERED_STREAMBUF 1
+# define ACE_LACKS_PRAGMA_ONCE 1
+# define ACE_HAS_NEW_NOTHROW
+# define ACE_TEMPLATES_REQUIRE_SOURCE 1
+# define ACE_SIZEOF_LONG_DOUBLE 10
+# define ACE_UINT64_FORMAT_SPECIFIER ACE_TEXT ("%Lu")
+# define ACE_INT64_FORMAT_SPECIFIER ACE_TEXT ("%Ld")
+# define ACE_USES_STD_NAMESPACE_FOR_STDCPP_LIB 1
+# define ACE_ENDTHREADEX(STATUS) ::_endthreadex ((DWORD) STATUS)
+
 #if defined(ACE_MT_SAFE) && (ACE_MT_SAFE != 0)
 // must have _MT defined to include multithreading
 // features from win32 headers
@@ -100,6 +141,10 @@
 # define ACE_HAS_NONCONST_SWAB
 # define ACE_HAS_NONCONST_FDOPEN
 #endif
+
+# if (__BORLANDC__ < 0x590)
+#   define ACE_LACKS_PLACEMENT_OPERATOR_DELETE 1
+# endif
 
 #if (__BORLANDC__ < 0x610)
 # define ACE_HAS_NONCONST_TEMPNAM
