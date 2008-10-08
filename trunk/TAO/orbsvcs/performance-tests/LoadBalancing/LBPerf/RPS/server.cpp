edb8b00e8da76117657349752771a456963e7866
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-10-08 11:32:47 +0000

    Wed Oct  8 11:31:45 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/orbsvcs/performance-tests/LoadBalancing/LBPerf/RPS/server.cpp
+++ trunk/TAO/orbsvcs/performance-tests/LoadBalancing/LBPerf/RPS/server.cpp
@@ -37,22 +37,22 @@ parse_args (int argc, ACE_TCHAR *argv[])
         break;
 
       case 's':
-        strategy = get_opts.opt_arg ();
+        strategy = ACE_TEXT_ALWAYS_CHAR(get_opts.opt_arg ());
         break;
 
       case 'r':
         reject_threshold =
-          static_cast<CORBA::Float> (::atof (get_opts.opt_arg ()));
+          static_cast<CORBA::Float> (::atof (ACE_TEXT_ALWAYS_CHAR(get_opts.opt_arg ())));
         break;
 
       case 'c':
         critical_threshold =
-          static_cast<CORBA::Float> (::atof (get_opts.opt_arg ()));
+          static_cast<CORBA::Float> (::atof (ACE_TEXT_ALWAYS_CHAR(get_opts.opt_arg ())));
         break;
 
       case 'd':
         dampening =
-          static_cast<CORBA::Float> (::atof (get_opts.opt_arg ()));
+          static_cast<CORBA::Float> (::atof (ACE_TEXT_ALWAYS_CHAR(get_opts.opt_arg ())));
         break;
 
       case '?':
