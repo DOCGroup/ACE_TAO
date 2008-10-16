9c0edb523ae920488408a849cc933d685a443c89
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-10-16 21:57:01 +0000

    restructured DDSStateUpdate_T class

---
+++ branches/FLARE_F6/TAO/orbsvcs/examples/FaultTolerance/FLARe/no-RT/DDSStateUpdate_T.cpp
@@ -0,0 +1,280 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    DDSStateUpdate_T.cpp
+ *
+ *  $Id$
+ *
+ * @author   Friedhelm Wolf (fwolf@dre.vanderbilt.edu)
+ */
+//=============================================================================
+
+#ifndef _DDS_STATE_UPDATE_T_CPP_
+#define _DDS_STATE_UPDATE_T_CPP_
+
+#include "ccpp_dds_dcps.h"
+
+#include "DDSStateReaderListener_T.h"
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::DDSStateUpdate_T (
+    const std::string & topic_name,
+    const std::string & id,
+    DDS::DomainParticipant_ptr participant,
+    DDS::Publisher_ptr publisher,
+    DDS::Subscriber_ptr subscriber,
+    ReplicatedApplication_ptr application)
+  : topic_name_ (topic_name),
+    id_ (id),
+    participant_ (DDS::DomainParticipant::_duplicate (participant)),
+    pub_ (DDS::Publisher::_duplicate (publisher)),
+    sub_ (DDS::Subscriber::_duplicate (subscriber)),
+    topic_ (DDS::Topic::_nil ()),
+    datawriter_ (TOPIC_DATA_WRITER::_nil ()),
+    datareader_ (TOPIC_DATA_READER::_nil ()),
+    listener_ (new DDSStateReaderListener_T <TOPIC_TYPE,
+	                                     TOPIC_DATA_READER,
+	                                     STATE_TYPE> (id, application))    
+{
+  state_.id = id.c_str ();
+  this->init ();
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::~DDSStateUpdate_T ()
+{
+  this->fini ();
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+bool
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::init ()
+{
+  return this->create_topic ()
+    && this->create_datawriter ()
+    && this->create_datareader ();
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+bool
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::fini ()
+{
+  DDS::ReturnCode_t status =
+    this->pub_->delete_datawriter (this->datawriter_.in ());
+
+  if (status != DDS::RETCODE_OK)
+    {
+      return false;
+    }
+
+  status = this->participant_->delete_topic (this->topic_.in ());
+
+  if (status != DDS::RETCODE_OK)
+    {
+      return false;
+    }
+    
+  return true;
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+void
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::set_state (
+  const CORBA::Any & state_value)
+{
+  // extract value from any
+  STATE_TYPE value;
+
+  // update value on state topic sample
+  if (state_value >>= value)
+    state_.value = value;
+
+  // publish value
+  DDS::ReturnCode_t ret =
+    this->datawriter_->write (this->state_, this->instance_handle_);
+          
+  if (ret != DDS::RETCODE_OK)
+    {
+      ACE_DEBUG ((
+          LM_ERROR,
+          ACE_TEXT ("StateUpdate::publish_update: write returned %d.\n"),
+          ret));
+    }
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+bool
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::create_topic ()
+{
+  DDS::TypeSupport_var svnt = new TOPIC_TYPE_SUPPORT;
+  CORBA::String_var data_type_name = svnt->get_type_name ();
+  
+  DDS::ReturnCode_t status = svnt->register_type (this->participant_.in (),
+                                                  data_type_name.in ());
+                                                   
+  if (status != DDS::RETCODE_OK)
+    {
+      return false;
+    }
+
+  DDS::TopicQos default_topic_qos;
+  status = this->participant_->get_default_topic_qos (default_topic_qos);
+  
+  if (status != DDS::RETCODE_OK)
+    {
+      return false;
+    }
+
+  this->topic_ =
+    this->participant_->create_topic (topic_name_.c_str (),
+                                      data_type_name.in (),
+                                      default_topic_qos,
+                                      DDS::TopicListener::_nil (),
+                                      DDS::ANY_STATUS);
+                               
+  if (CORBA::is_nil (this->topic_.in ()))
+    {
+      return false;
+    }
+
+  return true;
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+bool
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::create_datawriter ()
+{
+  DDS::DataWriterQos dw_qos;
+  DDS::ReturnCode_t status =
+    pub_->get_default_datawriter_qos (dw_qos);
+
+  if (status != DDS::RETCODE_OK)
+    {
+      return false;
+    }
+
+  DDS::DataWriter_var datawriter_base = 
+    pub_->create_datawriter (this->topic_.in (),
+			     dw_qos,
+			     DDS::DataWriterListener::_nil (),
+			     DDS::ANY_STATUS);
+                       
+  if (CORBA::is_nil (datawriter_base.in ()))
+    {
+      return false;
+    }
+  
+  datawriter_ = TOPIC_DATA_WRITER::_narrow (datawriter_base.in ());
+  
+  if (CORBA::is_nil (datawriter_.in ()))
+    {
+      return false;
+    }
+    
+  // Since there is no key, we need only one instance handle for all writes.  
+  instance_handle_ =
+    datawriter_->register_instance (state_);
+
+  return true;
+}
+
+template <typename STATE_TYPE,
+	  typename TOPIC_TYPE, 
+	  typename TOPIC_TYPE_SUPPORT,
+	  typename TOPIC_DATA_WRITER,
+          typename TOPIC_DATA_READER>
+bool
+DDSStateUpdate_T<STATE_TYPE,
+                 TOPIC_TYPE,
+		 TOPIC_TYPE_SUPPORT,
+		 TOPIC_DATA_WRITER,
+		 TOPIC_DATA_READER>::create_datareader ()
+{
+  DDS::DataReaderQos dr_qos;
+  DDS::ReturnCode_t status =
+    sub_->get_default_datareader_qos (dr_qos);
+
+  if (status != DDS::RETCODE_OK)
+    {
+      return false;
+    }
+
+  DDS::DataReader_var datareader_base = 
+    sub_->create_datareader (this->topic_.in (),
+			     dr_qos,
+			     listener_.in (),
+			     DDS::ANY_STATUS);
+                       
+  if (CORBA::is_nil (datareader_base.in ()))
+    {
+      return false;
+    }
+  
+  datareader_ = TOPIC_DATA_READER::_narrow (datareader_base.in ());
+  
+  if (CORBA::is_nil (datareader_.in ()))
+    {
+      return false;
+    }
+    
+  return true;
+}
+
+#endif /* _DDS_STATE_UPDATE_T_CPP_ */
