2f515e8ca4fc4fd78487ea343375cba7a58fe738
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-10-22 13:12:14 +0000

    ChangeLogTag: Wed Oct 22 13:11:00 UTC 2008  Simon Massey  <sma at prismtech dot com>

--- CIAO/DevGuideExamples/Messenger/StaticDAnCE/plan.h
+++ CIAO/DevGuideExamples/Messenger/StaticDAnCE/plan.h
@@ -1,27 +1,27 @@
 extern "C" ::Components::HomeExecutorBase_ptr create_MessengerHome_Impl (void);
 extern "C" ::PortableServer::Servant create_MessengerHome_Servant 
-		(::Components::HomeExecutorBase_ptr p,
-		::CIAO::Session_Container *c,
-		const char*
-		);
+    (::Components::HomeExecutorBase_ptr p,
+    ::CIAO::Session_Container *c,
+    const char*
+    );
 extern "C" ::Components::HomeExecutorBase_ptr create_ReceiverHome_Impl (void);
 extern "C" ::PortableServer::Servant create_ReceiverHome_Servant 
-		(::Components::HomeExecutorBase_ptr p,
-		::CIAO::Session_Container *c,
-		const char*
-		);
+    (::Components::HomeExecutorBase_ptr p,
+    ::CIAO::Session_Container *c,
+    const char*
+    );
 extern "C" ::Components::HomeExecutorBase_ptr create_ReceiverHome_Impl (void);
 extern "C" ::PortableServer::Servant create_ReceiverHome_Servant 
-		(::Components::HomeExecutorBase_ptr p,
-		::CIAO::Session_Container *c,
-		const char*
-		);
+    (::Components::HomeExecutorBase_ptr p,
+    ::CIAO::Session_Container *c,
+    const char*
+    );
 extern "C" ::Components::HomeExecutorBase_ptr create_AdministratorHome_Impl (void);
 extern "C" ::PortableServer::Servant create_AdministratorHome_Servant 
-		(::Components::HomeExecutorBase_ptr p,
-		::CIAO::Session_Container *c,
-		const char*
-		);
+    (::Components::HomeExecutorBase_ptr p,
+    ::CIAO::Session_Container *c,
+    const char*
+    );
 struct HomeAttributes
 {
   char const * component_instance_name_;
@@ -35,8 +35,8 @@ struct HomeAttributes
 /// Homes
 HomeAttributes homes_table[]= 
 {
-	{"Messenger_Instance", "create_MessengerHome_Impl", create_MessengerHome_Impl, "create_MessengerHome_Servant", create_MessengerHome_Servant},
-	{"First_Receiver_Instance", "create_ReceiverHome_Impl", create_ReceiverHome_Impl, "create_ReceiverHome_Servant", create_ReceiverHome_Servant},
-	{"Second_Receiver_Instance", "create_ReceiverHome_Impl", create_ReceiverHome_Impl, "create_ReceiverHome_Servant", create_ReceiverHome_Servant},
-	{"Administrator_Instance", "create_AdministratorHome_Impl", create_AdministratorHome_Impl, "create_AdministratorHome_Servant", create_AdministratorHome_Servant}};
-
+  {"Messenger_Instance", "create_MessengerHome_Impl", create_MessengerHome_Impl, "create_MessengerHome_Servant", create_MessengerHome_Servant},
+  {"First_Receiver_Instance", "create_ReceiverHome_Impl", create_ReceiverHome_Impl, "create_ReceiverHome_Servant", create_ReceiverHome_Servant},
+  {"Second_Receiver_Instance", "create_ReceiverHome_Impl", create_ReceiverHome_Impl, "create_ReceiverHome_Servant", create_ReceiverHome_Servant},
+  {"Administrator_Instance", "create_AdministratorHome_Impl", create_AdministratorHome_Impl, "create_AdministratorHome_Servant", create_AdministratorHome_Servant}
+};
