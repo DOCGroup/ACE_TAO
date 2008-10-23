3c2b2b7bb07602c957d5faf5fe9fba1913196bfa
smcqueen <smcqueen@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-10-23 10:28:26 +0000

    ChangeLogTag: Thu Oct 23 10:28:43 UTC 2008  Simon McQueen  <sm@prismtech.com>

--- TAO/tests/Bug_3471_Regression/server.cpp
+++ TAO/tests/Bug_3471_Regression/server.cpp
@@ -8,12 +8,12 @@ ACE_RCSID (Hello,
            server,
            "$Id$")
 
-const char *ior_output_file = "test.ior";
+const ACE_TCHAR *ior_output_file = ACE_TEXT ("test.ior");
 
 int
-parse_args (int argc, char *argv[])
+parse_args (int argc, ACE_TCHAR *argv[])
 {
-  ACE_Get_Opt get_opts (argc, argv, "o:");
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("o:"));
   int c;
 
   while ((c = get_opts ()) != -1)
