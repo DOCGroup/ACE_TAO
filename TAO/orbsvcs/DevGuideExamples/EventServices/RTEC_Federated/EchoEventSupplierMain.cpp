79505603f01cdad0951af271e231c2f615ea10be
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-10-24 10:40:13 +0000

    ChangeLogTag: Fri Oct 24 10:40:00 UTC 2008  Simon Massey  <sma at prismtech dot com>

--- TAO/orbsvcs/DevGuideExamples/EventServices/RTEC_Federated/EchoEventSupplierMain.cpp
+++ TAO/orbsvcs/DevGuideExamples/EventServices/RTEC_Federated/EchoEventSupplierMain.cpp
@@ -40,9 +40,9 @@ int ACE_TMAIN (int argc, ACE_TCHAR *argv[])
     // Initialize the ORB.
     CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
 
-    const char* ecname = "EventService";
-    const char* remote_ecname = 0;
-    const char* iorfile = 0;
+    const ACE_TCHAR* ecname = ACE_TEXT ("EventService");
+    const ACE_TCHAR* remote_ecname = 0;
+    const ACE_TCHAR* iorfile = 0;
     for (int i = 0; argv[i] != 0; i++) {
       if (ACE_OS::strcmp(argv[i], ACE_TEXT("-ecname")) == 0) {
         if (argv[i+1] != 0) {
