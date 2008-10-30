a4e74c1dc9c785a40324c90da81aa852c6743269
Johnny Willemsen <jwillemsen@remedy.nl>
2008-10-30 12:16:38 +0000

    Thu Oct 30 12:16:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/tests/IOStream_Test.cpp
+++ ACE/tests/IOStream_Test.cpp
@@ -180,8 +180,8 @@ client (void *arg = 0)
 
   // Send a string to the server which it can interpret as a qchar[]
   const char *str = "\"This is a test     string.\"";
-  ACE_DEBUG ((LM_DEBUG, ACE_TEXT (" (%P|%t) Client Sending: (%s)\n"),
-              ACE_TEXT_CHAR_TO_TCHAR (str)));
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT (" (%P|%t) Client Sending: (%C)\n"),
+              str));
   server << str << endl;
 
   // Allow the server to get the string and echo it to the user. (The
@@ -315,8 +315,8 @@ server (void *arg = 0)
   client_handler >> qbuf;
 
   ACE_DEBUG ((LM_DEBUG,
-              ACE_TEXT (" (%P|%t) Server Received: (\"%s\")\n"),
-              ACE_TEXT_CHAR_TO_TCHAR ((char *) qbuf)));
+              ACE_TEXT (" (%P|%t) Server Received: (\"%C\")\n"),
+              (char *) qbuf));
 
   // Give the client time to announce the next test to the user.
   ACE_OS::sleep (2);
@@ -356,8 +356,8 @@ server (void *arg = 0)
       if (! (client_handler >> buf))
         break;
       ACE_DEBUG ((LM_DEBUG,
-                  ACE_TEXT ("%s "),
-                  ACE_TEXT_CHAR_TO_TCHAR (buf)));
+                  ACE_TEXT ("%C "),
+                  buf));
     }
 
   ACE_DEBUG ((LM_DEBUG,
