d1c0844dcb42f52d567033b3b3402de9962ba82e
pollockt <pollockt@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-03 14:14:53 +0000

    Mon Nov 03 14:13:12 UTC 2008  Tim Pollock  <pollockt@ociweb.com>

--- trunk/TAO/orbsvcs/tests/Notify/Bug_2415_Regression/Supplier.cpp
+++ trunk/TAO/orbsvcs/tests/Notify/Bug_2415_Regression/Supplier.cpp
@@ -26,7 +26,7 @@
 const int PER_BATCH = 1;
 static TAO_Notify_Tests_StructuredPushSupplier* supplier = 0;
 static const ACE_TCHAR *ior_output_file = ACE_TEXT ("supplier.ior");
-ACE_TCHAR* domain = ACE_TEXT ("domain1");
+static const ACE_TCHAR *domain = ACE_TEXT ("domain1");
 
 // ******************************************************************
 // Subroutine Section
