d8029c91a9f04fc108cf18e83ce4ebfe39ea364e
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-03 15:45:55 +0000

    Mon Nov  3 15:45:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tao/Compression/lzo/LzoCompressor.cpp
+++ trunk/TAO/tao/Compression/lzo/LzoCompressor.cpp
@@ -25,14 +25,14 @@ LzoCompressor::compress (
   )
 {
   void* wrkmem = (lzo_bytep) lzo_malloc(LZO1X_1_MEM_COMPRESS);
-  unsigned int max_length = static_cast <unsigned int> (source.length () * 1.1) + 12;
+  lzo_uint max_length = static_cast <lzo_uint> (source.length () * 1.1) + 12;
   target.length (static_cast <CORBA::ULong> (max_length));
 
   int const retval = ::lzo1x_1_compress (
-            reinterpret_cast <const char*>(source.get_buffer ()),
+            reinterpret_cast <const unsigned char*>(source.get_buffer ()),
             source.length (),
-            reinterpret_cast <char*>(target.get_buffer ()),
-            max_length,
+            reinterpret_cast <unsigned char*>(target.get_buffer ()),
+            &max_length,
             wrkmem);
 
   lzo_free(wrkmem);
@@ -55,15 +55,14 @@ LzoCompressor::decompress (
   const ::Compression::Buffer & source,
   ::Compression::Buffer & target)
 {
-
-  unsigned int max_length = static_cast <unsigned int> (target.length ());
+  lzo_uint max_length = static_cast <lzo_uint> (target.length ());
 
   int const retval = ::lzo1x_decompress (
-                                 reinterpret_cast <char*>(const_cast<CORBA::Octet*>(source.get_buffer ())),
+                                 reinterpret_cast <const unsigned char*>(source.get_buffer ()),
                                  source.length (),
-                                 reinterpret_cast <char*>(target.get_buffer ()),
+                                 reinterpret_cast <unsigned char*>(target.get_buffer ()),
                                  &max_length,
-                                 0)
+                                 0);
 
   if (retval != LZO_E_OK)
     {
