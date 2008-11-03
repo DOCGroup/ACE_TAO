8c52e86f822ab2345bb597dada2d4e193dc8bb09
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-03 09:09:03 +0000

    Mon Nov  3 09:08:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/tests/CallbackTest/run_test.pl
+++ trunk/TAO/tests/CallbackTest/run_test.pl
@@ -9,23 +9,18 @@ use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::Run_Test;
 use PerlACE::TestTarget;
 
-my $target = PerlACE::TestTarget::create_target (1);
-my $host = PerlACE::TestTarget::create_target (2);
+my $server = PerlACE::TestTarget::create_target(1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target(2) || die "Create target 2 failed\n";
 
 $iorbase = "server.ior";
-$server_iorfile = $target->LocalFile ("$iorbase");
-$client_iorfile = $host->LocalFile ("$iorbase");
-$target->DeleteFile ($server_iorfile);
-$host->DeleteFile ($client_iorfile);
+$server_iorfile = $server->LocalFile ("$iorbase");
+$client_iorfile = $client->LocalFile ("$iorbase");
+$server->DeleteFile ($iorbase);
+$client->DeleteFile ($iorbase);
 $status = 0;
 
-if (PerlACE::is_vxworks_test()) {
-    $SV = new PerlACE::ProcessVX ("server", "-o $iorbase");
-}
-else {
-    $SV = $target->CreateProcess ("server", "-o $server_iorfile");
-}
-$CL = $host->CreateProcess ("client", "-k file://$client_iorfile");
+$SV = $server->CreateProcess ("server", "-o $server_iorfile");
+$CL = $client->CreateProcess ("client", "-k file://$client_iorfile");
 
 $server_status = $SV->Spawn ();
 
@@ -34,42 +29,43 @@ if ($server_status != 0) {
     exit 1;
 }
 
-if ($target->WaitForFileTimed ($server_iorfile,
-                               $target->ProcessStartWaitInterval()) == -1) {
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
     print STDERR "ERROR: cannot find file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-if ($target->GetFile ($server_iorfile, $iorbase) == -1) {
+if ($server->GetFile ($iorbase) == -1) {
     print STDERR "ERROR: cannot retrieve file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
-} 
-if ($host->PutFile ($iorbase, $client_iorfile) == -1) {
+}
+if ($client->PutFile ($iorbase) == -1) {
     print STDERR "ERROR: cannot set file <$client_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
-} 
+}
 
-$client = $CL->SpawnWaitKill (60);
+$client_status = $CL->SpawnWaitKill (60);
 
 # The client crashes, therefore it normally exists with status != 0,
 # but a status of -1 would be a serious error.
-if ($client == -1) {
-    print STDERR "ERROR: client returned $client\n";
+if ($client_status == -1) {
+    print STDERR "ERROR: client returned $client_status\n";
     $status = 1;
 }
 
-$server = $SV->WaitKill (60);
+$server_status = $SV->WaitKill (60);
 
-if ($server != 0) {
-    print STDERR "ERROR: server returned $server\n";
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     $status = 1;
 }
 
-$target->GetStderrLog();
-$target->DeleteFile ($server_iorfile);
-$host->DeleteFile($client_iorfile);
+$server->GetStderrLog();
+
+$server->DeleteFile ($iorbase);
+$client->DeleteFile($iorbase);
 
 exit $status;
