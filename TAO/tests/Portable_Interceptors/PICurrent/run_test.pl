c6da7aa2d68251281c75d0dc9f0037ce43911de0
Johnny Willemsen <jwillemsen@remedy.nl>
2008-11-04 13:38:10 +0000

    Tue Nov  4 13:37:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/Portable_Interceptors/PICurrent/run_test.pl
+++ TAO/tests/Portable_Interceptors/PICurrent/run_test.pl
@@ -1,71 +1,76 @@
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
-    & eval 'exec perl -S $0 $argv:q'
-    if 0;
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
 
-# -*- perl -*-
 # $Id$
+# -*- perl -*-
 
-use lib  "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use lib "$ENV{ACE_ROOT}/bin";
+use PerlACE::TestTarget;
 
 $status = 0;
-$file = PerlACE::LocalFile ("test.ior");
-$cvsfile= PerlACE::LocalFile ("svc.conf");
-unlink $cvsfile;
+$debug_level = '0';
 
-sub DoTest
-{
-  my $opt = shift;
-  $status = 0;
-
-  if (PerlACE::is_vxworks_test()) {
-      $SV = new PerlACE::ProcessVX ("server", "-o test.ior $opt");
-  }
-  else {
-      $SV = new PerlACE::Process ("server", "-o $file $opt");    
-  }
-  $CL = new PerlACE::Process ("client", "-k file://$file");
-
-  print STDERR "\n\n==== Running PICurrent test\n";
-
-  unlink $file;
-  $SV->Spawn ();
-
-  if (PerlACE::waitforfile_timed ($file, $PerlACE::wait_interval_for_process_creation) == -1) {
-      print STDERR "ERROR: cannot find file <$file>\n";
-      $SV->Kill (); 
-      return 1;
-  }
-
-  $client = $CL->SpawnWaitKill (60);
-
-  if ($client != 0) {
-      print STDERR "ERROR: client returned $client\n";
-      $status = 1;
-  }
-
-  $serverexit = $SV->WaitKill (20);
-
-  if ($serverexit != 0) {
-      print STDERR "ERROR: server returned $serverexit\n";
-      $status = $serverexit;
-  }
-  
-  return $status;
+foreach $i (@ARGV) {
+    if ($i eq '-debug') {
+        $debug_level = '10';
+    }
 }
 
-print STDERR "\n\n==== single-threaded\n";
-$status= DoTest();
-if (0 != $status) {
-   exit $status
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
+
+my $iorbase = "server.ior";
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
+
+$SV = $server->CreateProcess ("server", "-ORBdebuglevel $debug_level -o $server_iorfile");
+$CL = $client->CreateProcess ("client", "-k file://$client_iorfile");
+$server_status = $SV->Spawn ();
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    exit 1;
 }
 
-print STDERR "\n\n==== thread-per-connection\n";
-$status= DoTest('-t 1');
-if (0 != $status) {
-   exit $status
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
 }
 
-unlink $file;
+if ($server->GetFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot retrieve file <$server_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
+}
+if ($client->PutFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot set file <$client_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
+}
+
+$client_status = $CL->SpawnWaitKill (60);
+
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
+    $status = 1;
+}
+
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    $status = 1;
+}
+
+$server->GetStderrLog();
+$client->GetStderrLog();
+
+$server->DeleteFile($server_iorfile);
+$client->DeleteFile($client_iorfile);
 
 exit $status;
