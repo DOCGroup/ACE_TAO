061399b7af54d3355986279bf5a56c183d63d6cd
Johnny Willemsen <jwillemsen@remedy.nl>
2008-11-05 08:50:05 +0000

    Wed Nov  5 08:49:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/ace/config-aix-5.x.h
+++ ACE/ace/config-aix-5.x.h
@@ -332,5 +332,6 @@
 #define ACE_SCANDIR_SEL_LACKS_CONST
 #define ACE_HAS_SIGSUSPEND
 #define ACE_HAS_TIMEZONE  /* Call tzset() to set timezone */
+#define ACE_LACKS_ISCTYPE
 
 #endif /* ACE_CONFIG_AIX_5_X_H */
