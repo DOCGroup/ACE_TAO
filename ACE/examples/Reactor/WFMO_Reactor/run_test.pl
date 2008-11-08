c248d02e5b0a08ee960bb30ed056ab568a47c192
Johnny Willemsen <jwillemsen@remedy.nl>
2008-11-08 07:04:32 +0000

    Sat Nov  8 07:03:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- ACE/examples/Reactor/WFMO_Reactor/run_test.pl
+++ ACE/examples/Reactor/WFMO_Reactor/run_test.pl
@@ -5,20 +5,19 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-use lib '../../../bin';
-use PerlACE::Run_Test;
+use lib "$ENV{ACE_ROOT}/bin";
+use PerlACE::TestTarget;
 
 #
 # These tests only run on Win32
 #
-if ($^O ne "MSWin32") 
+if ($^O ne "MSWin32")
 {
     exit;
 }
 
-$test_timeout = 60;
 
-@tests = 
+@tests =
     (
      "Abandoned",
      "APC",
@@ -38,15 +37,18 @@ $test_timeout = 60;
      "Window_Messages",
      );
 
+my $target = PerlACE::TestTarget::create_target (1);
+$test_timeout = 60 + $target->ProcessStartWaitInterval();
+
 for $test (@tests)
 {
     print STDOUT "\n________________________________________\n";
     print STDOUT "\nStarting test \"$test\"";
     print STDOUT "\n________________________________________\n\n";
-    
-    $test_process = new PerlACE::Process ($test);
 
-    if (! -x $test_process->Executable ()) {
+    my $test_process = $target->CreateProcess($test);
+
+    if (! -e $test_process->Executable ()) {
         print STDERR "Error: " . $test_process->Executable () .
                      " does not exist or is not runnable\n";
     }
@@ -54,8 +56,8 @@ for $test (@tests)
     {
        $test_process->Spawn ();
        $test_result = $test_process->WaitKill ($test_timeout);
-        
-       if ($test_result != 0) 
+
+       if ($test_result != 0)
        {
            print STDERR "\n________________________________________\n";
            print STDERR "\nERROR: \"$test\" returned $test_result";
