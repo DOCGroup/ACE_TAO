7410bb505da98ff34ed9798c4c4f836030a91472
fields_t <fields_t@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-10 23:23:40 +0000

    ChangeLogTag: Mon Nov 10 23:21:57 UTC 2008  Trevor Fields  <fields_t@ociweb.com>

---
+++ TAO/orbsvcs/tests/InterfaceRepo/Bug_3495_Regression/TestImpl.h
@@ -0,0 +1,32 @@
+// -*- C++ -*-
+//
+// $Id$
+
+
+#ifndef TESTIMPL_H_
+#define TESTIMPL_H_
+
+#include "TestS.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+
+class  Test_interfaceOne_i
+  : public virtual POA_Test::interfaceOne
+{
+public:
+  // Constructor 
+  Test_interfaceOne_i (void);
+  
+  // Destructor 
+  virtual ~Test_interfaceOne_i (void);
+  
+  virtual void getstruct (::Test::structOne_out tester)
+    ACE_THROW_SPEC ((::CORBA::SystemException));
+};
+
+
+#endif /* TESTIMPL_H_  */
+
