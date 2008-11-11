22870137ef7c518fac81a1a0e777236c06a827e0
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-11 13:26:49 +0000

    Tue Nov 11 13:26:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ACE/ace/OS_NS_Thread.cpp
+++ trunk/ACE/ace/OS_NS_Thread.cpp
@@ -4318,10 +4318,15 @@ ACE_OS::thr_create (ACE_THR_FUNC func,
           // Set the priority of the new thread and then let it
           // continue, but only if the user didn't start it suspended
           // in the first place!
-          ACE_OS::thr_setprio (*thr_handle, priority);
+          if (ACE_OS::thr_setprio (*thr_handle, priority) != 0)
+            {
+              return -1;
+            }
 
           if (start_suspended == 0)
-            ACE_OS::thr_continue (*thr_handle);
+            {
+              ACE_OS::thr_continue (*thr_handle);
+            }
         }
     }
 #   if 0
