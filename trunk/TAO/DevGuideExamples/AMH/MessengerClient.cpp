7858e85c3699a2b85b50a7b2b8e54052d3333051
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-12 10:49:28 +0000

    Wed Nov 12 10:49:30 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/TAO/DevGuideExamples/AMH/MessengerClient.cpp
+++ trunk/TAO/DevGuideExamples/AMH/MessengerClient.cpp
@@ -2,16 +2,46 @@
 
 #include "amh_pch.h"
 
+#include "ace/Get_Opt.h"
 #include "MessengerC.h"
 #include <iostream>
+
+ACE_TString ior = ACE_TEXT ("file://test.ior");
+
+int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("k:"));
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+  {
+      case 'k':
+        ior = get_opts.optarg;
+        break;
+
+      case '?':
+      default:
+        std::cerr << "usage: " << argv[0] << " -k <ior>" << std::endl;
+        return -1;
+        break;
+  }
+  return 0;
+}
+
 int ACE_TMAIN (int argc, ACE_TCHAR* argv[])
 {
   try {
     // Initialize the ORB.
     CORBA::ORB_var orb = CORBA::ORB_init( argc, argv );
 
+    if (parse_args(argc, argv) != 0) {
+      return 1;
+    }
+
     // Read and destringify the Messenger object's IOR.
-    CORBA::Object_var obj = orb->string_to_object( "file://Messenger.ior" );
+    CORBA::Object_var obj = orb->string_to_object(ior.c_str());
     if( CORBA::is_nil( obj.in() ) ) {
       std::cerr << "Could not get Messenger IOR." << std::endl;
       return 1;
