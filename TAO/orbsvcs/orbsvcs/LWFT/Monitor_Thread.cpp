7d9d10742920dff089dc36391d83194ac1149b1a
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-17 18:21:52 +0000

    ChangeLogTag:Mon Nov 17 18:20:56 UTC 2008  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/orbsvcs/orbsvcs/LWFT/Monitor_Thread.cpp
+++ TAO/orbsvcs/orbsvcs/LWFT/Monitor_Thread.cpp
@@ -12,15 +12,19 @@
 
 #include "ace/TP_Reactor.h"
 
-Monitor_Thread::Monitor_Thread ()
+Monitor_Thread::Monitor_Thread (void)
   : reactor_ (new ACE_TP_Reactor())
 {
 }
 
-int Monitor_Thread::svc ()
+int Monitor_Thread::svc (void)
 {
   if (reactor_.run_reactor_event_loop() == -1)
-    ACE_ERROR_RETURN ((LM_ERROR,"run_reactor_event_loop failed\n"), -1);
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         "Monitor_Thread: run_reactor_event_loop failed\n"),
+                        -1);
+    }
 
   return 0;
 }
@@ -30,7 +34,7 @@ int Monitor_Thread::register_handler (ACE_Event_Handler *event_handler, ACE_Reac
   return reactor_.register_handler (event_handler, mask);
 }
 */
-ACE_Reactor * Monitor_Thread::get_reactor ()
+ACE_Reactor * Monitor_Thread::get_reactor (void)
 {
   return &reactor_;
 }
