2326009b953f46af8171f56deefca0bbca8baa9c
Johnny Willemsen <jwillemsen@remedy.nl>
2008-11-18 09:52:55 +0000

    Tue Nov 18 09:52:33 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- TAO/tests/Bug_933_Regression/MessengerClient.cpp
+++ TAO/tests/Bug_933_Regression/MessengerClient.cpp
@@ -2,6 +2,35 @@
 
 #include "MessengerC.h"
 #include "tao/CodecFactory/CodecFactory.h"
+#include "ace/Get_Opt.h"
+
+const ACE_TCHAR *ior = ACE_TEXT ("file://test.ior");
+
+int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("k:"));
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 'k':
+        ior = get_opts.opt_arg ();
+        break;
+
+      case '?':
+      default:
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage:  %s "
+                           "-k <ior> "
+                           "\n",
+                           argv [0]),
+                          -1);
+      }
+  // Indicates sucessful parsing of the command line
+  return 0;
+}
 
 int
 ACE_TMAIN(int argc, ACE_TCHAR *argv[])
@@ -10,8 +39,11 @@ ACE_TMAIN(int argc, ACE_TCHAR *argv[])
     // Initialize orb
     CORBA::ORB_var orb = CORBA::ORB_init( argc, argv );
 
+      if (parse_args (argc, argv) != 0)
+        return 1;
+
     // Destringify ior
-    CORBA::Object_var obj = orb->string_to_object( "file://Messenger.ior" );
+    CORBA::Object_var obj = orb->string_to_object(ior);
 
     // Narrow
     Messenger_var messenger = Messenger::_narrow( obj.in() );
