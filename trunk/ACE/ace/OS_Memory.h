1d2f234bc2ca68a7c99c54b73d413e9c67d10871
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-11-21 11:01:45 +0000

    ChangeLogTag: Fri Nov 21 11:00:00 UTC 2008  Simon Massey  <sma at prismtech dot com>

--- trunk/ACE/ace/OS_Memory.h
+++ trunk/ACE/ace/OS_Memory.h
@@ -231,10 +231,14 @@ ACE_END_VERSIONED_NAMESPACE_DECL
      if (POINTER == 0) { errno = ENOMEM; } \
    } while (0)
 
+# if !defined (ACE_bad_alloc)
+    class ACE_bad_alloc_class {};
+#   define ACE_bad_alloc  ACE_bad_alloc_class
+# endif
 # if defined (ACE_HAS_MFC) && (ACE_HAS_MFC == 1)
-#   define ACE_throw_bad_alloc AfxThrowMemoryException ()
+#   define ACE_throw_bad_alloc  AfxThrowMemoryException ()
 # else
-#   define ACE_throw_bad_alloc throw ACE_bad_alloc ()
+#   define ACE_throw_bad_alloc  throw ACE_bad_alloc ()
 # endif
 
 #endif /* ACE_NEW_THROWS_EXCEPTIONS */
