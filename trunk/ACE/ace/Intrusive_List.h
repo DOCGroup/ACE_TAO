df72ef123818e1d9763d83707d5ab07a3f15e63a
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-12-02 20:01:24 +0000

    Tue Dec  2 20:01:00 UTC 2008  Johnny Willemsen  <jwillemsen@remedy.nl>

--- trunk/ACE/ace/Intrusive_List.h
+++ trunk/ACE/ace/Intrusive_List.h
@@ -68,12 +68,12 @@ public:
 
   // = Check boundary conditions.
 
-  /// Returns 1 if the container is empty, otherwise returns 0.
-  int is_empty (void) const;
+  /// Returns true if the container is empty, otherwise returns false.
+  bool is_empty (void) const;
 
-  /// Returns 1 if the container is empty, otherwise returns 0.
+  /// Returns true if the container is empty, otherwise returns false.
   /// @deprecated Use is_empty() instead.
-  int empty (void) const;
+  bool empty (void) const;
 
   /// Insert an element at the beginning of the list
   void push_front (T *node);
