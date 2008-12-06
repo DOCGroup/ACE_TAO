a4112a65b4bda569a3c8677452cfb047962a74d2
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2008-12-06 04:38:43 +0000

    ChangeLogTag: Sat Dec  6 04:36:06 UTC 2008  Friedhelm Wolf  <fwolf@dre.vanderbilt.edu>

---
+++ branches/CIAO_FLARE/CIAO/DAnCE/tests/CIAO/FTClientComponent/FTClient_Timer_Handler.cpp
@@ -0,0 +1,64 @@
+// $Id$
+
+#include <ace/High_Res_Timer.h>
+#include <tao/RTCORBA/RTCORBA.h>
+#include <ciao/CIAO_common.h>
+#include "FTClient_Timer_Handler.h"
+#include "TestC.h"
+#include "FTClient_exec.h"
+
+namespace CIDL_FTClient_Impl
+{
+
+  FTClient_Timer_Handler::FTClient_Timer_Handler (
+    CIDL_FTClient_Impl::FTClient_exec_i * client_executor)
+    : client_executor_ (client_executor)
+  {
+  }
+
+  void
+  FTClient_Timer_Handler::set_orb (CORBA::ORB_ptr orb)
+  {
+    orb_ = CORBA::ORB::_duplicate (orb);
+  }
+
+  int
+  FTClient_Timer_Handler::handle_timeout (const ACE_Time_Value &,
+                                          const void *)
+  {
+    test_var server = client_executor_->server ();
+
+    CORBA::Object_var obj = orb_->resolve_initial_references ("RTCurrent");
+    RTCORBA::Current_var rt_current = RTCORBA::Current::_narrow (obj);
+
+    rt_current->the_priority (client_executor_->priority ());
+
+    try
+      {
+        ACE_hrtime_t start = ACE_OS::gethrtime ();
+
+        // we have to do some profiling first to see how we can achieve
+        // the correct execution time.
+        server->method (0, 0, 
+                        static_cast<CORBA::ULong> (client_executor_->execution_time ()), 
+                        201827, 0);
+        
+        ACE_hrtime_t end = ACE_OS::gethrtime ();
+        
+        CIAO_DEBUG ((LM_DEBUG,
+                     ACE_TEXT ("FTClient_Timer_Handler::handle_timeout () -"
+                               "response time for %f was %d.\n"),
+                     client_executor_->execution_time (),
+                     end - start));
+      }
+    catch (CORBA::SystemException & ex)
+      {
+        CIAO_DEBUG ((LM_WARNING, 
+                     ACE_TEXT ("FTClient_Timer_Handler::handle_timeout () -"
+                               "caught: %s"), ex._info ().c_str ()));
+      }
+
+    return 0;
+  }
+    
+} // end namespace CIDL_FTClient_Impl
