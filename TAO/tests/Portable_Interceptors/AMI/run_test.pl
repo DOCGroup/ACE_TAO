d92fce3d6b1436ff2ea080f89a54fcf5e3201bb1
msmit <msmit@remedy.nl>
2009-01-08 19:12:51 +0000

    Thu Jan  8 19:10:56 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * tests/Portable_Interceptors/AMI/run_test.pl:
            * tests/Portable_Interceptors/PolicyFactory/run_test.pl:
            * tests/Portable_Interceptors/Request_Interceptor_Flow/run_test.pl:
              Made "Fuzz" compliant.

--- TAO/tests/Portable_Interceptors/AMI/run_test.pl
+++ TAO/tests/Portable_Interceptors/AMI/run_test.pl
@@ -6,43 +6,45 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 
-$iorfile = PerlACE::LocalFile ("server.ior");
-unlink $iorfile;
-$status = 0;
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
-if (PerlACE::is_vxworks_test()) {
-    $SV = new PerlACE::ProcessVX ("server", "-o server.ior");
-}
-else {
-    $SV = new PerlACE::Process ("server", "-o $iorfile");    
-}
-$CL = new PerlACE::Process ("client", " -k file://$iorfile");
+my $iorbase = "server.ior";
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
-$SV->Spawn ();
+$SV = $server->CreateProcess ("server", "-o $server_iorfile");
+$CL = $client->CreateProcess ("client", "-k file://$client_iorfile");
+$server_status = $SV->Spawn ();
 
-if (PerlACE::waitforfile_timed ($iorfile,
-                        $PerlACE::wait_interval_for_process_creation) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile>\n";
+my $status = 0;
+
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
-} 
+}
 
-$client = $CL->SpawnWaitKill (300);
+$client_status = $CL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
-if ($client != 0) {
+if ($client_status != 0) {
     print STDERR "ERROR: client returned $client\n";
     $status = 1;
 }
 
-$server = $SV->WaitKill (10);
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
 
-if ($server != 0) {
+if ($server_status != 0) {
     print STDERR "ERROR: server returned $server\n";
     $status = 1;
 }
 
-unlink $iorfile;
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
 exit $status;
