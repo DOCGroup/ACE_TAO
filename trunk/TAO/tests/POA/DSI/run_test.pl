1c96f029881c912fe5f157bc2271fab74fc8f053
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-01-15 11:10:04 +0000

    hu Jan 15 12:06:46 UTC 2009  Marcel Smit <msmit@remedy.nl>
            * tests/POA/DSI/run_test.pl:
            * tests/POA/Default_Servant2/run_test.pl:
              Made "fuzz" compliant.
            
            * tests/POA/EndpointPolicy/run_test.pl:
              Reduced number of targets.

--- trunk/TAO/tests/POA/DSI/run_test.pl
+++ trunk/TAO/tests/POA/DSI/run_test.pl
@@ -6,43 +6,43 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 
 $status = 0;
+
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
+
 $iorbase = "server.ior";
-$iorfile = PerlACE::LocalFile ("$iorbase");
+$iorfile = $server->LocalFile ($iorbase);
 
-unlink $iorfile;
+$server->DeleteFile($iorbase);
 
-if (PerlACE::is_vxworks_test()) {
-  $SV = new PerlACE::ProcessVX ("server", "-f $iorbase");
-}
-else {
-  $SV = new PerlACE::Process ("server", "-f $iorfile");
-}
-$CL = new PerlACE::Process ("client", "-f $iorfile -x");
+$SV = $server->CreateProcess ("server", "-f $iorfile");
+$CL = $client->CreateProcess ("client", "-f $iorfile -x");
 
 $SV->Spawn ();
 
-if (PerlACE::waitforfile_timed ($iorfile, $PerlACE::wait_interval_for_process_creation) == -1) {
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
     print STDERR "ERROR: cannot find file <$iorfile>\n";
-    $SV->Kill ();
+    $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-$client = $CL->SpawnWaitKill (60);
-$server = $SV->WaitKill (15);
+$client_status = $CL->SpawnWaitKill ($client->ProcessStartWaitInterval());
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
 
-unlink $iorfile;
-
-if ($client != 0) {
-    print STDERR "ERROR: client returned $client\n";
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
     $status = 1;
 }
 
-if ($server != 0) {
-    print STDERR "ERROR: server returned $server\n";
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     $status = 1;
 }
+
+$server->DeleteFile($iorbase);
 
 exit $status;
--- trunk/TAO/tests/POA/EndpointPolicy/run_test.pl
+++ trunk/TAO/tests/POA/EndpointPolicy/run_test.pl
@@ -10,35 +10,27 @@ use PerlACE::TestTarget;
 
 $status = 0;
 
-my $server_shared = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
-my $server_multi  = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
-my $client_good = PerlACE::TestTarget::create_target (3) || die "Create target 3 failed\n";
-my $client_bad  = PerlACE::TestTarget::create_target (4) || die "Create target 4 failed\n";
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
 my $goodiorbase = "good.ior";
 my $badiorbase = "bad.ior";
 
-my $server_shared_good_iorfile = $server_shared->LocalFile ($goodiorbase);
-my $server_multi_good_iorfile = $server_shared->LocalFile ($goodiorbase);
-my $server_shared_bad_iorfile = $server_multi->LocalFile ($badiorbase);
-my $server_multi_bad_iorfile = $server_multi->LocalFile ($badiorbase);
-my $client_good_iorfile = $client_good->LocalFile ($goodiorbase);
-my $client_bad_iorfile = $client_bad->LocalFile ($badiorbase);
+my $server_good_iorfile = $server->LocalFile ($goodiorbase);
+my $server_bad_iorfile = $server->LocalFile ($badiorbase);
+my $client_good_iorfile = $client->LocalFile ($goodiorbase);
+my $client_bad_iorfile = $client->LocalFile ($badiorbase);
 
-$server_shared->DeleteFile($goodiorbase);
-$server_shared->DeleteFile($badiorbase);
-$server_multi->DeleteFile($goodiorbase);
-$server_multi->DeleteFile($badiorbase);
-$client_good->DeleteFile($goodiorbase);
-$client_bad->DeleteFile($badiorbase);
+$server->DeleteFile($goodiorbase);
+$server->DeleteFile($badiorbase);
+$client->DeleteFile($goodiorbase);
+$client->DeleteFile($badiorbase);
 
 $port = 12345;
 
-$sharedSV = $server_shared->CreateProcess ("server", "-ORBDottedDecimalAddresses 0 -ORBUseSharedProfile 1 -g $server_shared_good_iorfile -b $server_shared_bad_iorfile -p $port");
-$multiSV = $server_multi->CreateProcess ("server", "-ORBDottedDecimalAddresses 0 -ORBUseSharedProfile 0 -g $server_multi_good_iorfile -b $server_multi_bad_iorfile -p $port");
-
-$goodCL = $client_good->CreateProcess ("client", "-k file://$client_good_iorfile");
-$badCL = $client_bad->CreateProcess ("client", "-b -k file://$client_bad_iorfile");
+$sharedSV = $server->CreateProcess ("server", "-ORBDottedDecimalAddresses 0 -ORBUseSharedProfile 1 -g $server_good_iorfile -b $server_bad_iorfile -p $port");
+$goodCL = $client->CreateProcess ("client", "-k file://$client_good_iorfile");
+$badCL  = $client->CreateProcess ("client", "-b -k file://$client_bad_iorfile");
 
 print "Starting server using shared profiles\n";
 
@@ -49,78 +41,81 @@ if ($server_status_shared != 0) {
     exit 1;
 }
 
-if ($server_shared->WaitForFileTimed ($goodiorbase,
-                               $server_shared->ProcessStartWaitInterval()) == -1) {
-    print STDERR "ERROR: cannot find file <$server_shared_good_iorfile>\n";
+if ($server->WaitForFileTimed ($goodiorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_good_iorfile>\n";
     $sharedSV->Kill (); $sharedSV->TimedWait (1);
     exit 1;
 }
 
-$client_status_bad = $badCL->SpawnWaitKill ($client_bad->ProcessStartWaitInterval());
+$client_status_bad = $badCL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
 if ($client_status_bad != 0) {
     print STDERR "ERROR: bad client returned $client_status_bad\n";
     $status = 1;
 }
 
-$client_good_status = $goodCL->SpawnWaitKill ($client_good->ProcessStartWaitInterval());
+$client_good_status = $goodCL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
 if ($client_good_status != 0) {
     print STDERR "ERROR: good client returned $client_good_status\n";
     $status = 1;
 }
 
-$server_status_shared = $sharedSV->WaitKill ($server_shared->ProcessStopWaitInterval());
+$server_status_shared = $sharedSV->WaitKill ($server->ProcessStopWaitInterval());
 
 if ($server_status_shared != 0) {
     print STDERR "ERROR: server [single profile per IOR] returned $server_status_shared\n";
     $status = 1;
 }
 
-$server_shared->DeleteFile($goodiorbase);
-$server_shared->DeleteFile($badiorbase);
-$server_multi->DeleteFile($goodiorbase);
-$server_multi->DeleteFile($badiorbase);
-$client_good->DeleteFile($goodiorbase);
-$client_bad->DeleteFile($badiorbase);
+$server->DeleteFile($goodiorbase);
+$server->DeleteFile($badiorbase);
+$client->DeleteFile($goodiorbase);
+$client->DeleteFile($badiorbase);
 
 print "Starting server using multiple profiles\n";
 
+$multiSV = $server->CreateProcess ("server", "-ORBDottedDecimalAddresses 0 -ORBUseSharedProfile 0 -g $server_good_iorfile -b $server_bad_iorfile -p $port");
+
 $server_status_multi = $multiSV->Spawn ();
 
-if ($server_multi->WaitForFileTimed ($goodiorbase,
-                               $server_multi->ProcessStartWaitInterval()) == -1) {
-    print STDERR "ERROR: cannot find file <$server_multi_good_iorfile>\n";
+if ($server_status_multi != 0) {
+    print STDERR "ERROR: server returned $server_status_multi\n";
+    exit 1;
+}
+
+if ($server->WaitForFileTimed ($goodiorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_good_iorfile>\n";
     $multiSV->Kill (); $multiSV->TimedWait (1);
     exit 1;
 }
 
-$client_bad_status = $badCL->SpawnWaitKill ($client_bad->ProcessStartWaitInterval());
+$client_bad_status = $badCL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
 if ($client_bad_status != 0) {
     print STDERR "ERROR: bad client returned $client_bad_status\n";
     $status = 1;
 }
 
-$client_good_status = $goodCL->SpawnWaitKill ($client_good->ProcessStartWaitInterval());
+$client_good_status = $goodCL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
 if ($client_good_status != 0) {
     print STDERR "ERROR: good client returned $client_good_status\n";
     $status = 1;
 }
 
-$server_multi_status = $multiSV->WaitKill ($server_multi->ProcessStopWaitInterval());
+$server_multi_status = $multiSV->WaitKill ($server->ProcessStopWaitInterval());
 
 if ($server_multi_status != 0) {
     print STDERR "ERROR: server [multiple profiles per IOR] returned $server_multi_status\n";
     $status = 1;
 }
 
-$server_shared->DeleteFile($goodiorbase);
-$server_shared->DeleteFile($badiorbase);
-$server_multi->DeleteFile($goodiorbase);
-$server_multi->DeleteFile($badiorbase);
-$client_good->DeleteFile($goodiorbase);
-$client_bad->DeleteFile($badiorbase);
+$server->DeleteFile($goodiorbase);
+$server->DeleteFile($badiorbase);
+$client->DeleteFile($goodiorbase);
+$client->DeleteFile($badiorbase);
 
 exit $status;
