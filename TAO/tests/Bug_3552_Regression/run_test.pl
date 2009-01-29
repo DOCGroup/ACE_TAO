d7290585f09ab8fd1c4ccc80293a1fe5577e41d6
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-01-29 15:01:02 +0000

    ChangeLogTag: Thu Jan 29 15:00:00 UTC 2009  Simon Massey  <sma@prismtech.com>

--- TAO/tests/Bug_3552_Regression/run_test.pl
+++ TAO/tests/Bug_3552_Regression/run_test.pl
@@ -12,8 +12,8 @@ my $target1 = PerlACE::TestTarget::create_target (1) || die "Create target 1 fai
 my $check = $target1->CreateProcess ("check");
 my $result = $check->SpawnWaitKill ($target1->ProcessStartWaitInterval());
 if ($result != 0) {
-  print STDERR "ERROR: check returned $result\n";
-  exit 1;
+    print STDERR "ERROR: check returned $result\n";
+    exit 1;
 }
 
 exit 0;
