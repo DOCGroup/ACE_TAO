0219757bf2477964cf34c5e216705f250e6360d5
stallions <stallions@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-02-05 21:24:40 +0000

    Thu Feb  5 21:14:15 UTC 2009  Steven Stallion  <stallions@ociweb.com>

--- ACE/tests/Log_Thread_Inheritance_Test.cpp
+++ ACE/tests/Log_Thread_Inheritance_Test.cpp
@@ -5,6 +5,10 @@
 #include "ace/Task.h"
 #include <fstream>
 
+#ifdef ACE_HAS_PTHREADS
+# include <pthread.h>
+#endif /* ACE_HAS_PTHREADS */
+
 ACE_RCSID (tests, Log_Thread_Inheritance_Test, "$Id$")
 
 #if !defined (ACE_HAS_THREADS) || defined (ACE_LACKS_IOSTREAM_TOTALLY)
@@ -13,6 +17,47 @@ int run_main (int, ACE_TCHAR *[])
   return 0;
 }
 #else
+
+#ifdef ACE_HAS_PTHREADS
+struct Inheritor : ACE_Task_Base
+{
+  int svc ()
+  {
+    ACE_DEBUG ((LM_DEBUG, "(%P|%t) - this test might crash ACE if it does not "
+                          "have the fix for the second bug in #3480.\n"));
+    return 0; 
+  }
+};
+
+void* spawn_ace_task (void*)
+{
+  Inheritor inheritor;
+  
+  inheritor.activate ();
+  inheritor.wait ();
+
+  return 0;
+}
+
+bool test_inherited_attributes ()
+{
+  // This test verifies ACE_OS_Log_Msg_Attributes correctly initializes
+  // when an ACE thread is created from a non-ACE thread (i.e. pthreads)
+  // and is then used for logging.
+  // 
+  // This test will cause occasional SEGVs on failure.
+  // stallions 2009/02/05
+  pthread_t parent; 
+
+  if (pthread_create (&parent, 0, spawn_ace_task, 0) != 0)
+    {
+      return false;
+    }
+  pthread_join (parent, 0);
+
+  return true;
+}
+#endif /* ACE_HAS_PTHREADS */
 
 struct MyThread : ACE_Task_Base
 {
@@ -72,6 +117,9 @@ int run_main (int, ACE_TCHAR *[])
   mt.wait ();
   MyThread::childthread_.wait ();
   ACE_LOG_MSG->msg_ostream (initial_stream, 0);
+#ifdef ACE_HAS_PTHREADS 
+  if (!test_inherited_attributes ()) return -1;
+#endif /* ACE_HAS_PTHREADS */
   ACE_DEBUG ((LM_DEBUG, "(%P|%t) - Test passed.\n"));
   ACE_END_TEST;
   return 0;
