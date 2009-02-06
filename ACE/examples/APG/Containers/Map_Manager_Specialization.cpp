31ebbfa640fe3edd54febf5bf31739ed86931072
Johnny Willemsen <jwillemsen@remedy.nl>
2009-02-06 10:33:12 +0000

    Fri Feb  6 10:33:17 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * examples/APG/Containers/Map_Manager_Specialization.cpp:
              Fixed compile error in versioned namespace build

--- ACE/examples/APG/Containers/Map_Manager_Specialization.cpp
+++ ACE/examples/APG/Containers/Map_Manager_Specialization.cpp
@@ -36,6 +36,7 @@ ACE_Map_Manager<KeyType, DataElement, ACE_Null_Mutex>::equal
 }
 // Listing 1
 #else
+ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 template<>
 int
 ACE_Map_Manager<KeyType, DataElement, ACE_Null_Mutex>::equal
@@ -43,6 +44,7 @@ ACE_Map_Manager<KeyType, DataElement, ACE_Null_Mutex>::equal
 {
   return (r1 == r2);
 }
+ACE_END_VERSIONED_NAMESPACE_DECL
 #endif /* 0 */
 
 class Map_Example
