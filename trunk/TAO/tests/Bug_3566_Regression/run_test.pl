278627b127b9628ef22c9ac57d76dbcdc00ce9fe
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-02-10 18:33:44 +0000

    Tue Feb 10 18:33:28 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Bug_3565_Regression/*
              New test for bugzilla 3565
    
            * tests/Bug_3566_Regression/*
              New test for bugzilla 3566

---
+++ trunk/TAO/tests/Bug_3566_Regression/run_test.pl
@@ -0,0 +1,19 @@
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
+
+# $Id$
+# -*- perl -*-
+
+use lib "$ENV{ACE_ROOT}/bin";
+use PerlACE::TestTarget;
+
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+
+$SV = $server->CreateProcess ("server");
+
+$SV->Spawn ();
+
+$SV->WaitKill ($server->ProcessStopWaitInterval());
+
+exit 0;
