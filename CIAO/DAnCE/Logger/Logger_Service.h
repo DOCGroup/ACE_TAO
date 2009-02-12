a82bbcd570e5ff5a1754f5e1da369176431962be
William R. Otte <wotte@dre.vanderbilt.edu>
2009-02-12 16:20:58 +0000

    ChangeLogTag: Thu Feb 12 16:18:49 UTC 2009  William R. Otte  <wotte@dre.vanderbilt.edu>

--- CIAO/DAnCE/Logger/Logger_Service.h
+++ CIAO/DAnCE/Logger/Logger_Service.h
@@ -2,14 +2,14 @@
 #ifndef LOGGER_SERVICE_H_
 #define LOGGER_SERVICE_H_
 
-#include "DAnCE_Logger_Export.h"
-#include "Starter/DAnCELoggerFactory.h"
-#include "File_Logger_Backend.h"
+#include "DAnCE/Logger/DAnCE_Logger_Export.h"
+#include "DAnCE/Logger/DAnCELoggerFactory.h"
+#include "DAnCE/Logger/File_Logger_Backend.h"
 
 namespace DAnCE
   {
 
-  class DAnCE_Logger_Export Logger_Service : public DAnCELoggerFactory
+    class DAnCE_Logger_Export Logger_Service : public ::DAnCE::DAnCELoggerFactory
     {
     public:
       Logger_Service (void);
