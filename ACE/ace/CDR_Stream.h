99d6d74ef6225ded925dbc9d74f75f9912d2d8c8
Johnny Willemsen <jwillemsen@remedy.nl>
2009-02-16 18:58:16 +0000

    Mon Feb 16 18:58:28 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/CDR_Stream.h:
            * ace/FIFO_Recv_Msg.h:
            * ace/FIFO_Send_Msg.h:
            * ace/SPIPE_Stream.h:
              Doxygen changes

--- ACE/ace/CDR_Stream.h
+++ ACE/ace/CDR_Stream.h
@@ -120,8 +120,8 @@ public:
                  ACE_CDR::Octet minor_version = ACE_CDR_GIOP_MINOR_VERSION);
 
   /// Build a CDR stream with an initial buffer, it will *not* remove
-  /// <data>, since it did not allocated it.  It's important to be careful
-  /// with the alignment of <data>.
+  /// @a data, since it did not allocated it.  It's important to be careful
+  /// with the alignment of @a data.
   /**
    * Create an output stream from an arbitrary buffer, care must be
    * exercised with alignment, because this contructor will align if
@@ -663,7 +663,7 @@ public:
                 ACE_Lock* lock = 0);
 
   /// Create an input stream from an ACE_Data_Block. The <flag>
-  /// indicates whether the <data> can be deleted by the CDR stream
+  /// indicates whether the @a data can be deleted by the CDR stream
   /// or not
   ACE_InputCDR (ACE_Data_Block *data,
                 ACE_Message_Block::Message_Flags flag = 0,
