e6e3998853dff07d610e03a884f77a2204d1ee1e
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-02-19 12:25:16 +0000

    Thu Feb 19 12:25:18 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tools/Config_Handlers/DataType_Handler.cpp:
              Fixed GCC warning

--- trunk/CIAO/tools/Config_Handlers/DataType_Handler.cpp
+++ trunk/CIAO/tools/Config_Handlers/DataType_Handler.cpp
@@ -99,7 +99,7 @@ namespace CIAO
           break;
 
         default:
-          ACE_ERROR ((LM_ERROR, "Invalid typecode in any\n"));
+          ACE_ERROR ((LM_ERROR, "DataType_Handler::data_type, Invalid typecode in any\n"));
           throw 1;
         }
 
@@ -110,71 +110,69 @@ namespace CIAO
     }
 
     DataType
-    DataType_Handler::data_type (
-                                 const CORBA::TypeCode_ptr&  src)
+    DataType_Handler::data_type (const CORBA::TypeCode_ptr&  src)
     {
       CIAO_TRACE("DataType_Handler::data_type - reverse");
       switch (src->kind ())
         {
         case ::CORBA::tk_null:
-          return DataType ( (TCKind::tk_null));
+          return DataType (TCKind::tk_null);
           break;
         case CORBA::tk_short:
-          return DataType ( (TCKind::tk_short));
+          return DataType (TCKind::tk_short);
           break;
         case CORBA::tk_long:
-          return DataType ( (TCKind::tk_long));
+          return DataType (TCKind::tk_long);
           break;
         case CORBA::tk_ushort:
-          return DataType ( (TCKind::tk_ushort));
+          return DataType (TCKind::tk_ushort);
           break;
         case CORBA::tk_ulong:
-          return DataType ( (TCKind::tk_ulong));
+          return DataType (TCKind::tk_ulong);
           break;
         case CORBA::tk_float:
-          return DataType ( (TCKind::tk_float));
+          return DataType (TCKind::tk_float);
           break;
         case CORBA::tk_double:
-          return DataType ( (TCKind::tk_double));
+          return DataType (TCKind::tk_double);
           break;
         case CORBA::tk_boolean:
-          return DataType ( (TCKind::tk_boolean));
+          return DataType (TCKind::tk_boolean);
           break;
         case CORBA::tk_char:
-          return DataType ( (TCKind::tk_char));
+          return DataType (TCKind::tk_char);
           break;
         case CORBA::tk_octet:
-          return DataType ( (TCKind::tk_octet));
+          return DataType (TCKind::tk_octet);
           break;
         case CORBA::tk_string:
-          return DataType ( (TCKind::tk_string));
+          return DataType (TCKind::tk_string);
           break;
         case CORBA::tk_longlong:
-          return DataType ( (TCKind::tk_longlong));
+          return DataType (TCKind::tk_longlong);
           break;
         case CORBA::tk_ulonglong:
-          return DataType ( (TCKind::tk_ulonglong));
+          return DataType (TCKind::tk_ulonglong);
           break;
         case CORBA::tk_longdouble:
-          return DataType ( (TCKind::tk_longdouble));
+          return DataType (TCKind::tk_longdouble);
           break;
         case CORBA::tk_wchar:
-          return DataType ( (TCKind::tk_wchar));
+          return DataType (TCKind::tk_wchar);
           break;
         case CORBA::tk_wstring:
-          return DataType ( (TCKind::tk_wstring));
+          return DataType (TCKind::tk_wstring);
           break;
         case CORBA::tk_any:
-          return DataType ( (TCKind::tk_any));
+          return DataType (TCKind::tk_any);
           break;
         case CORBA::tk_TypeCode:
-          return DataType ( (TCKind::tk_TypeCode));
+          return DataType (TCKind::tk_TypeCode);
         default:
-          ACE_ERROR ((LM_ERROR, "Invalid typecode\n"));
+          ACE_ERROR ((LM_ERROR, "DataType_Handler::data_type, Invalid typecode\n"));
           throw 1;
         }
-
+      throw 1;
     }
-
   }
 }
