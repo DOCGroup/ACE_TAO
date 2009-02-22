60b62d4792cfed22a0e5ed91f7e7b792437d4c17
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-02-22 00:14:14 +0000

    Added FT_Session_Container

--- branches/CIAO_FLARE/CIAO/ciao/FTComponentServer/CIAO_Container_Impl.h
+++ branches/CIAO_FLARE/CIAO/ciao/FTComponentServer/CIAO_Container_Impl.h
@@ -12,7 +12,7 @@
 #pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
-#include "ciao/Containers/Session/Session_Container.h"
+#include "ciao/Containers/FTSession/FT_Session_Container.h"
 
 #include "CIAO_FTComponentServer_svnt_export.h"
 
