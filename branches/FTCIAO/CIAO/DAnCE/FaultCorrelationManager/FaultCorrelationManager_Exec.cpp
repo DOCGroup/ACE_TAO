bdfdc5f6d02d4439db37878a065641fb693211c3
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-02-26 18:20:26 +0000

    Thu Feb 26 18:17:03 UTC 2009  Friedhelm Wolf  <fwolf@dre.vanderbilt.edu>

---
+++ branches/FTCIAO/CIAO/DAnCE/FaultCorrelationManager/FaultCorrelationManager_Exec.cpp
@@ -0,0 +1,5 @@
+// $Id$
+#include "FaultCorrelationManager_Module.h"
+
+#define DANCE_MODULE_MAIN_CLASS_NAME FaultCorrelationManager_Module
+#include "Deployment/Module_Main.h"
---
+++ branches/FTCIAO/CIAO/DAnCE/FaultCorrelationManager/FaultCorrelationManager_Module.h
@@ -0,0 +1,106 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ * @file FaultCorrelationManager_Module.h
+ *
+ * $Id$
+ *
+ * @author Friedhelm Wolf <fwolf@dre.vanderbilt.edu>
+ */
+//=============================================================================
+
+#ifndef FAULTCORRELATIONMANAGER_MODULE_H
+#define FAULTCORRELATIONMANAGER_MODULE_H
+
+#include /**/ "ace/pre.h"
+
+#include "FaultCorrelationManager_Module_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/Service_Config.h"
+#include "ace/Vector_T.h"
+#include "ace/Map_Manager.h"
+#include "ace/Null_Mutex.h"
+#include "tao/ORB.h"
+#include "tao/PortableServer/POAC.h"
+#include "orbsvcs/CosNamingC.h"
+#include "tao/Object_Loader.h"
+#include "DAnCE/Deployment/DAnCE_Loader.h"
+#include "DAnCE/Deployment/Deployment_common.h"
+
+namespace DAnCE {
+  class FaultCorrelationManager_Impl;
+}
+
+/**
+ * @class FaultCorrelationManager_Module
+ *
+ * @brief The shared object that is instantiated when the fault
+ *        correlation manager module/library is dynamically loaded.
+ *
+ * This class runs the fault correlation manager instance
+ */
+class FaultCorrelationManager_Module_Export FaultCorrelationManager_Module
+  : public DAnCE::DAnCE_Object_Loader
+{
+ public:
+      struct SOptions
+      {
+        bool process_ns_;
+        const char* process_ns_file_;
+        bool create_plan_ns_;
+        const char* create_plan_ns_ior_;
+        const char * domain_nc_;
+        const char * instance_nc_;
+        
+        SOptions()
+            : process_ns_ (false),
+              process_ns_file_ (0),
+              create_plan_ns_ (false),
+              create_plan_ns_ior_ (0),
+              domain_nc_ (0),
+              instance_nc_ (0)
+        {
+        }
+      };
+
+  /// Constructor.
+  FaultCorrelationManager_Module (void);
+
+  /// Destructor.
+  ~FaultCorrelationManager_Module (void);
+
+  /// Overload the base class method to create a new instance
+  /// of a FaultCorrelationManager_Module object.
+  virtual CORBA::Object_ptr create_object (CORBA::ORB_ptr orb,
+                                           int argc,
+                                           ACE_TCHAR *argv []);
+
+  virtual const char * usage (void);
+
+  virtual bool parse_args (int argc, ACE_TCHAR *argv []);
+
+ private:
+  void register_value_factories (void);
+  void create_poas (void);
+  void create_fcm_properties (DAnCE::PROPERTY_MAP &props);
+
+  std::auto_ptr <DAnCE::FaultCorrelationManager_Impl> fcm_;
+  CORBA::ORB_var orb_;
+  CosNaming::NamingContext_var domain_nc_;
+
+  SOptions options_;
+
+  PortableServer::POA_var root_poa_;
+  PortableServer::POA_var fcm_poa_;
+};
+
+ACE_FACTORY_DECLARE (FaultCorrelationManager_Module, FaultCorrelationManager_Module)
+
+#include /**/ "ace/post.h"
+
+#endif  /* FAULTCORRELATIONMANAGER_MODULE_H */
