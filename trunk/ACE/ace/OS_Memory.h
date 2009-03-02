bfaa7f191755d35993f699cc2eed28f40c27a1f6
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-03-02 11:44:35 +0000

    Mon Mar  2 11:44:15 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/Auto_IncDec_T.h:
            * ace/OS_Memory.h:
            * ace/OS_NS_Thread.h:
              Doxygen fixes

--- trunk/ACE/ace/OS_Memory.h
+++ trunk/ACE/ace/OS_Memory.h
@@ -266,7 +266,7 @@ ACE_BEGIN_VERSIONED_NAMESPACE_DECL
  *
  * ~(alignment - 1) = 1...110...0 = T2
  *
- * Notice that there is a multiple of <alignment> in the range
+ * Notice that there is a multiple of @a alignment in the range
  * [<value>,<value> + T1], also notice that if
  *
  * X = ( <value> + T1 ) & T2
@@ -276,7 +276,7 @@ ACE_BEGIN_VERSIONED_NAMESPACE_DECL
  * <value> <= X <= <value> + T1
  *
  * because the & operator only changes the last bits, and since X is a
- * multiple of <alignment> (its last bits are zero) we have found the
+ * multiple of @a alignment (its last bits are zero) we have found the
  * multiple we wanted.
  */
 /// Return the next integer aligned to a required boundary
