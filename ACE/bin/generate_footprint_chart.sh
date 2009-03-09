48790902083b2f889efc763cf817d5433144b535
Johnny Willemsen <jwillemsen@remedy.nl>
2009-03-09 07:08:59 +0000

    Mon Mar  9 07:08:08 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * bin/generate_footprint_chart.sh:
              Changed location of footer
    
            * bin/MakeProjectCreator/config/global.features:
              lzo1 is disabled by default

--- ACE/bin/generate_footprint_chart.sh
+++ ACE/bin/generate_footprint_chart.sh
@@ -7,7 +7,7 @@ gnuplot <<_EOF_ >/dev/null 2>&1
   set xdata time
   set xtics rotate
   set timefmt '%Y/%m/%d-%H:%M'
-  set xlabel 'Date (MM/DD)'
+  set xlabel 'Date (MM/DD)'  0,-3
   set ylabel 'Size (KBytes)'
   set terminal png small size 1024,768 color
   set output "$2"
