7f910dfa2817375895c00f53d271cba8d21e2aaf
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-03-23 19:12:58 +0000

    ChangeLogTag: Mon Mar 23 19:05:25 UTC 2009  Jeff Parsons <j.parsons@vanderbilt.edu>

---
+++ trunk/TAO/tests/IDL_Inherited_Operation/InheritedOp_i.cpp
@@ -0,0 +1,18 @@
+#include "InheritedOp_i.h"
+
+Derived_i::Derived_i (CORBA::ORB_ptr orb)
+  : orb_ (orb)
+{
+}
+
+void
+Derived_i::BaseOp (void)
+{
+  ACE_DEBUG ((LM_DEBUG, "BaseOp upcall successful\n"));
+}
+
+void
+Derived_i::shutdown (void)
+{
+  orb_->shutdown ();
+}
