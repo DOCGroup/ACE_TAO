73560378d334b29c02d4c830da8555edf91e6364
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-03-26 13:46:20 +0000

    Thu Mar 26 13:47:23 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Big_AMI/run_test.pl:
            * tests/Bug_2936_Regression/run_test.pl:
            * bin/tao_orb_tests.lst:
              Converted to the new test framework

--- trunk/TAO/tests/Big_AMI/run_test.pl
+++ trunk/TAO/tests/Big_AMI/run_test.pl
@@ -6,7 +6,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 
 $debug_level = '0';
 $iterations = '10';
@@ -18,35 +18,48 @@ foreach $i (@ARGV) {
     }
 }
 
-$iorfile = PerlACE::LocalFile ("server.ior");
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
-unlink $iorfile;
+my $iorbase = "server.ior";
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
-if (PerlACE::is_vxworks_test()) {
-    $SV = new PerlACE::ProcessVX ("server", "-ORBdebuglevel $debug_level -d -o server.ior");
+$SV = $server->CreateProcess ("server", "-ORBdebuglevel $debug_level -d -o $server_iorfile");
+$CL = $client->CreateProcess ("client", "-k file://$client_iorfile -i $iterations -x -b $payload");
+
+$server_status = $SV->Spawn ();
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    exit 1;
 }
-else {
-    $SV = new PerlACE::Process ("server", "-ORBdebuglevel $debug_level -d -o $iorfile");
-}
 
-$SV->Spawn ();
-
-if (PerlACE::waitforfile_timed ($iorfile, $PerlACE::wait_interval_for_process_creation) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile>\n";
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-$CL = new PerlACE::Process ("client", " -k file://$iorfile -i $iterations -x -b $payload");
+$client_status = $CL->SpawnWaitKill (60);
 
-$client = $CL->SpawnWaitKill (60);
-$server = $SV->WaitKill (10);
-
-unlink $iorfile;
-
-if ($server != 0 || $client != 0) {
-    exit 1;
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
+    $status = 1;
 }
 
-exit 0;
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    $status = 1;
+}
+
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
+
+exit $status;
 
