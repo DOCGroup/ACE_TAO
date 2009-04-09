e8dc9fb3e97b8d2c353815b69f1a07f51d80ba2f
Johnny Willemsen <jwillemsen@remedy.nl>
2009-04-09 12:20:52 +0000

    Thu Apr  9 13:20:22 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * DAnCE/tests/ObjectLocatorTest/srv_main.cpp
            * DAnCE/tests/RedirectionServiceTest/srv_main.cpp
            * examples/Hello/Sender/starter.cpp
            * performance-tests/Protocols/Controller/Controller.cpp
              Unicode changes

--- CIAO/DAnCE/tests/ObjectLocatorTest/srv_main.cpp
+++ CIAO/DAnCE/tests/ObjectLocatorTest/srv_main.cpp
@@ -23,7 +23,7 @@ int ACE_TMAIN (int argc, ACE_TCHAR * argv[])
       ACE_TCHAR buf1[32];
       ACE_OS::sprintf (buf1, ACE_TEXT("-ORBListenEndpoints"));
       argvm[argcm-2] = buf1;
-      char buf2[32];
+      ACE_TCHAR buf2[32];
       ACE_OS::sprintf (buf2, ACE_TEXT("iiop://:12345"));
       argvm[argcm-1] = buf2;
       argvm[argcm] = 0;
