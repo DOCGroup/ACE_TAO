985f6056fcc6faa9917fe9db82b6464541d7ad98
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-04-17 21:48:43 +0000

    ChangeLogTag: Fri Apr 17 21:42:53 UTC 2009  Friedhelm Wolf  <fwolf@dre.vanderbilt.edu>

---
+++ TAO/orbsvcs/examples/FaultTolerance/FLARe/RTCORBA/ClientServer/Worker_i.cpp
@@ -0,0 +1,144 @@
+#include "Worker_i.h"
+
+Failure_Task::Failure_Task (CORBA::ORB_ptr orb,
+                            long limit,
+                            long & count)
+  : condition_ (lock_),
+    orb_ (CORBA::ORB::_duplicate (orb)),
+    limit_ (limit),
+    count_ (count),
+    stop_ (false)
+{
+}
+
+int
+Failure_Task::svc (void)
+{
+  ACE_Guard <ACE_Thread_Mutex> guard (lock_);
+
+  while (((limit_ == 0) || (count_ <= limit_)) && !stop_)
+    {
+      condition_.wait ();
+    }
+
+  orb_->shutdown (true);
+
+  return 0;
+}
+
+void 
+Failure_Task::signal (void)
+{
+  condition_.signal ();
+}
+
+void
+Failure_Task::stop (void)
+{
+  stop_ = true;
+  condition_.signal ();
+}
+
+Worker_i::Worker_i (CORBA::ORB_ptr orb,
+                    PortableServer::POA_ptr poa,
+                    const std::string & object_id,
+		    StateSynchronizationAgent_ptr agent,
+		    long invocations)
+  : orb_ (CORBA::ORB::_duplicate (orb)),
+    poa_ (PortableServer::POA::_duplicate (poa)),
+    object_id_ (object_id),
+    agent_ (StateSynchronizationAgent::_duplicate (agent)),
+    state_ (0),
+    suicidal_count_ (invocations),
+    task_ (orb_.in (), suicidal_count_, state_)
+{
+  timer_.calibrate ();
+  task_.activate ();
+}
+
+CORBA::ULong
+Worker_i::run_task (CORBA::Double execution_time)
+{
+  ACE_DEBUG ((LM_EMERGENCY, "x(%d) ", state_));
+
+  timer_.start ();
+
+  /* do prime number calculation here */
+
+  ++state_;
+
+  agent_->state_changed (object_id_.c_str ());
+
+  timer_.stop ();
+
+  timer_.elapsed_time (last_execution_time_);
+
+  ACE_DEBUG ((LM_TRACE, "x=%d ", last_execution_time_.msec ()));
+
+  task_.signal ();
+
+  return last_execution_time_.msec ();
+}
+
+void Worker_i::stop ()
+{
+  task_.stop ();
+}
+
+void
+Worker_i::set_state (const CORBA::Any & state_value)
+{
+  // extract value to an intermediate long variable since it's not possible
+  // to extract to a long & directly
+  CORBA::Long value;
+
+  if (state_value >>= value)
+    state_ = value;
+  else
+    ACE_DEBUG ((LM_WARNING,
+                "(%P|%t) Worker_i::set_state () "
+                "could not extract state value from Any."));
+
+  ACE_DEBUG ((LM_TRACE, "Worker_i::set_state (%d) called.\n", value));
+}
+
+CORBA::Any *
+Worker_i::get_state ()
+{
+  // create new any object
+  CORBA::Any_var state (new CORBA::Any);
+  
+  // create intermediate object with the value
+  CORBA::Long value = state_;
+
+  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Worker_i::get_state returns %d.\n", value));
+
+  // insert value into the any object
+  *state <<= value;
+
+  return state._retn ();
+}
+
+StateSynchronizationAgent_ptr
+Worker_i::agent (void)
+{
+  return StateSynchronizationAgent::_duplicate (agent_.in ());
+}
+  
+void
+Worker_i::agent (StateSynchronizationAgent_ptr agent)
+{
+  agent_ = agent;
+}
+
+char *
+Worker_i::object_id (void)
+{
+  return CORBA::string_dup (object_id_.c_str ());
+}
+  
+void
+Worker_i::object_id (const char * object_id)
+{
+  object_id_ = object_id;
+}
---
+++ TAO/orbsvcs/examples/FaultTolerance/FLARe/RTCORBA/ClientServer/client.cpp
@@ -0,0 +1,161 @@
+// $Id$
+
+#include <sstream>
+#include <fstream>
+#include "WorkerC.h"
+#include "ace/Get_Opt.h"
+#include "orbsvcs/orbsvcs/LWFT/LWFT_Client_Init.h"
+#include "Client_Timer_Handler.h"
+#include "ace/Sig_Handler.h"
+#include "ace/Reactor.h"
+#include "tao/ORB_Core.h"
+#include "Trigger_i.h"
+
+#include "tao/RTCORBA/RTCORBA.h"
+
+const ACE_TCHAR *ior1 = ACE_TEXT("file://test.ior");
+unsigned long iterations = 0;
+unsigned long log_start = 0;
+ACE_Time_Value period;
+double execution_time = 100;
+std::string server_id = "server";
+std::string prefix = "";
+bool logging = false;
+bool kill_me = false;
+
+int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("f:n:p:r:e:s:i:lk"));
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 'f':
+	prefix = get_opts.opt_arg ();
+        break;
+      case 'n':
+	log_start = atoi (get_opts.opt_arg ());
+        break;
+      case 'p':
+        ior1 = get_opts.opt_arg ();
+        break;
+      case 'r':
+	period.msec (atoi (get_opts.opt_arg ()));
+	break;
+      case 'e':
+	execution_time = atof (get_opts.opt_arg ());
+	break;
+      case 's':
+	server_id = get_opts.opt_arg ();
+	break;
+      case 'i':
+        iterations = atoi (get_opts.opt_arg ());
+	break;
+      case 'l':
+	logging = true;
+	break;
+      case 'k':
+	kill_me = true;
+	break;
+      case '?':
+      default:
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage:  %s "
+                           "-p <ior> "
+                           "-o <ior> "
+                           "\n",
+                           argv [0]),
+                          -1);
+      }
+
+  return 0;
+}
+
+int
+check_for_nil (CORBA::Object_ptr obj, const char *msg)
+{
+  if (CORBA::is_nil (obj))
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       "ERROR: Object reference <%C> is nil\n",
+                       msg),
+                      -1);
+  else
+    return 0;
+}
+
+int
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+{
+  try
+    {
+      LWFT_Client_Init ci;
+      CORBA::ORB_var orb = ci.init (argc, argv);
+
+      // Initialize the ORB, resolve references and parse arguments.
+
+      // Parse arguments.
+      if (parse_args (argc, argv) != 0)
+        {
+          return -1;
+        }
+
+      // Test object 1.
+      CORBA::Object_var object =
+        orb->string_to_object (ior1);
+
+      DeCoRAM::Worker_var server1 = DeCoRAM::Worker::_narrow (object.in ());
+      if (check_for_nil (server1.in (), "server1") == -1)
+        return -1;
+
+      // Check that test objects are configured with SERVER_DECLARED
+      // PriorityModelPolicy, and get their server priorities.
+
+      Client_Timer_Handler timeout_handler (iterations,
+                                            log_start,
+                                            prefix + server_id + "-client.txt",
+                                            period,
+                                            logging);
+
+      timeout_handler.set_orb (orb.in ());
+      timeout_handler.set_worker (server1.in ());
+
+      // add a the handler for the SIGINT signal here
+      ACE_Sig_Handler sig_handler;
+      sig_handler.register_handler (SIGINT, &timeout_handler);
+
+      Trigger_i * trigger = new Trigger_i (orb.in (),
+                                           timeout_handler,
+                                           period.msec ());
+
+      PortableServer::ServantBase_var ownership_transfer (trigger);
+
+      CORBA::Object_var obj = orb->resolve_initial_references ("RootPOA");
+
+      PortableServer::POA_var poa = PortableServer::POA::_narrow (obj.in ());
+
+      PortableServer::POAManager_var poa_mgr = poa->the_POAManager ();
+
+      poa_mgr->activate ();
+
+      Trigger_var trig = trigger->_this ();
+
+      std::string fname = server_id + "Client.ior";
+      std::ofstream file (fname.c_str ());
+      file << orb->object_to_string (trig.in ());
+      file.close ();
+
+      orb->run ();
+
+      //      orb->orb_core ()->reactor ()->cancel_timer (&timeout_handler);
+    }
+  catch (const CORBA::Exception& ex)
+    {
+      ex._tao_print_exception (
+        "Unexpected exception in Server_Declared test client:");
+      return -1;
+    }
+
+  return 0;
+}
