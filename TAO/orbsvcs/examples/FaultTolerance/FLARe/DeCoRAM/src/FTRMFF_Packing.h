5d4b30f759946b28a643edc706ae62b3e97c80b3
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-04-24 21:05:49 +0000

    implemented first version of packing algorithm

---
+++ TAO/orbsvcs/examples/FaultTolerance/FLARe/DeCoRAM/src/FTRMFF_Packing.h
@@ -0,0 +1,60 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    FTRMFF_Packing.h
+ *
+ *  $Id$
+ *
+ *  @author  Friedhelm Wolf (fwolf@dre.vanderbilt.edu)
+ */
+//=============================================================================
+
+#ifndef FTRMFF_PACKING_ALGORITHM_H_
+#define FTRMFF_PACKING_ALGORITHM_H_
+
+#include "Schedule.h"
+
+class FTRMFF_Packing : public FTRMFF_Algorithm
+{
+public:
+  virtual ~FTRMFF_Packing ();
+
+  virtual FTRMFF_Output operator () (const FTRMFF_Input & input);
+};
+
+typedef std::vector<TASK_LIST> TASK_LISTS;
+
+class FTRMFF_Packing_Algorithm : 
+  public std::unary_function <TASK_LIST,
+                              SCHEDULING_MAP>
+{
+public:
+  FTRMFF_Packing_Algorithm (const PROCESSOR_LIST & processors,
+                                  unsigned int consistency_level);
+
+  virtual ~FTRMFF_Packing_Algorithm ();
+
+  virtual SCHEDULING_MAP operator () (const TASK_LIST & tasks);
+
+  SCHEDULE_PROGRESS_LIST get_unschedulable ();
+
+  const SCHEDULE & schedule () const;
+
+private:
+  void copy_tasks (unsigned long primary_end,
+                   unsigned long number);
+
+  void merge_processors (unsigned long primary_processors);
+
+  bool merge_check (const TASK_LISTS & lists);
+
+  TASK_LIST merge_lists (const TASK_LISTS & lists);
+
+private:
+  SCHEDULE schedule_;
+  SCHEDULE_PROGRESS_LIST unschedulable_;
+  unsigned int consistency_level_;
+};
+
+#endif /* FTRMFF_PACKING_ALGORITHM_H_ */
