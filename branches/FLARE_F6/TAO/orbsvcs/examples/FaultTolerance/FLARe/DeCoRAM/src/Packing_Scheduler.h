d5a7319d2d76b104dac4e803668928e36d9ce15f
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-04-30 03:38:06 +0000

    added Packing_Scheduler

---
+++ branches/FLARE_F6/TAO/orbsvcs/examples/FaultTolerance/FLARe/DeCoRAM/src/Packing_Scheduler.h
@@ -0,0 +1,61 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    Packing_Scheduler.h
+ *
+ *  $Id$
+ *
+ *  @author  Friedhelm Wolf (fwolf@dre.vanderbilt.edu)
+ */
+//=============================================================================
+
+#ifndef PACKING_SCHEDULER_H_
+#define PACKING_SCHEDULER_H_
+
+#include "Scheduler.h"
+
+typedef std::vector<TASK_LIST> TASK_LISTS;
+typedef std::map <Processor, TASK_LISTS> TASK_GROUPS;
+
+std::ostream & operator<< (std::ostream & ostr, 
+                           const TASK_LISTS & tl);
+
+std::ostream & operator<< (std::ostream & ostr, 
+                           const TASK_GROUPS & tg);
+
+struct Packing_Scheduler : public Scheduler
+{
+public:
+  /// default ctor
+  Packing_Scheduler (const PROCESSOR_LIST & processors,
+                     unsigned int max_failures);
+
+  virtual double schedule_task (const Task & task,
+                                const Processor & processor);
+
+protected:
+  virtual void update_schedule (const Task & task,
+                                const Processor & processor);
+
+private:
+  void update_task_groups (const Task & task,
+                           const Processor & processor);
+
+  void add_backup (const Task & task,
+                   TASK_LISTS & tg);
+
+  bool rank_check (const TASK_LISTS & list);
+
+  unsigned int paired_rank_check (const TASK_LIST & list1,
+                                  const TASK_LIST & list2);
+
+  bool merge_check (const TASK_LISTS & lists);
+
+  TASK_LIST merge_lists (const TASK_LISTS & lists);
+
+private:
+  TASK_GROUPS task_groups_;
+};
+
+#endif /* PACKING_SCHEDULER_H_ */
