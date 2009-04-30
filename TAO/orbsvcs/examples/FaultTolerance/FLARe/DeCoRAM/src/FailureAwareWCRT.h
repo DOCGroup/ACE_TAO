d4246be678873221e7ffe08d59084e6759b0650c
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-04-30 22:18:46 +0000

    added new file

---
+++ TAO/orbsvcs/examples/FaultTolerance/FLARe/DeCoRAM/src/FailureAwareWCRT.h
@@ -0,0 +1,36 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    FailureAwareWCRT.h
+ *
+ *  $Id$
+ *
+ *  @author  Friedhelm Wolf (fwolf@dre.vanderbilt.edu)
+ */
+//=============================================================================
+
+#ifndef FAILURE_AWARE_WCRT_H_
+#define FAILURE_AWARE_WCRT_H_
+
+#include "CTT_Enhanced.h"
+#include "Scheduler.h"
+
+class FailureAwareWCRT : public std::binary_function <double,
+  PROCESSOR_SET,
+  double>
+{
+public:
+  FailureAwareWCRT (const TASK_LIST & tasks,
+                    const REPLICA_GROUPS & rep_groups);
+
+  double operator () (double previous,
+                      const PROCESSOR_SET & failures);
+
+private:
+  CTT_Enhanced ctt_;
+  TASK_LIST tasks_;
+  ReplicaFinder replica_finder_;
+};
+
+#endif /* FAILURE_AWARE_WCRT_H_ */
