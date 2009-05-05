cf1756793043e645a99f489631cb1e321ee91a6c
wolff1 <wolff1@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-05-05 13:45:18 +0000

    added new analysis capabilities

---
+++ TAO/orbsvcs/examples/FaultTolerance/FLARe/DeCoRAM/experiments/packing/taskcount.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+EXPERIMENT_NAME=taskcount
+DECORAM_BIN=../../bin
+DATA_DIR=$1
+RESULT_DIR=$DATA_DIR/$EXPERIMENT_NAME
+
+if [ ! -d ./$DATA_DIR ]; then echo Directory $DATA_DIR does not exist && exit; fi
+if [ ! -d ./$RESULT_DIR ]; then mkdir -p ./$RESULT_DIR; fi
+
+for ALGORITHM in pftrmff aftrmff frftrmff bsftrmbf bsftrmwf bsftrmbfnf bsftrmwfnf; do \
+  for TASK_NO in 10 20 40 80 160; do \
+    for BACKUP_NO in 1 2 3 4; do \
+      if [ -s ./$DATA_DIR/$ALGORITHM-$BACKUP_NO-backups-$TASK_NO-tasks.txt ]; then \
+        echo "calculate number of tasks of $ALGORITHM result for $BACKUP_NO backups and $TASK_NO tasks ..." && \
+        $DECORAM_BIN/scheck -t -f ./$DATA_DIR/$ALGORITHM-$BACKUP_NO-backups-$TASK_NO-tasks.txt > ./$RESULT_DIR/$EXPERIMENT_NAME-$ALGORITHM-$BACKUP_NO-backups-$TASK_NO-tasks.txt; \
+      fi;\
+    done; \
+  done; \
+done
