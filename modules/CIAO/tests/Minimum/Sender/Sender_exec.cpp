72497b2abffb115023c24cab16eada241a397811
William R. Otte <wotte@dre.vanderbilt.edu>
2009-06-17 18:04:51 +0000

    Wed Jun 17 18:04:28 UTC 2009  William R. Otte  <wotte@dre.vanderbilt.edu>

--- modules/CIAO/tests/Minimum/Sender/Sender_exec.cpp
+++ modules/CIAO/tests/Minimum/Sender/Sender_exec.cpp
@@ -21,7 +21,7 @@
 #include "Sender_exec.h"
 #include "ciao/CIAO_common.h"
 
-namespace CIDL_Sender_Impl
+namespace CIAO_Minimum_Sender_Impl
 {
   //==================================================================
   // Component Executor Implementation Class:   Sender_exec_i
