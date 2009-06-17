e85a1d197364ce91115bfa73a66b78c84d1bc607
Johnny Willemsen <jwillemsen@remedy.nl>
2009-06-17 10:10:22 +0000

    Wed Jun 17 10:07:19 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender.mpc
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.mpc
            * DAnCE/tests/CIAO/Components/ProcessColocation.mpc
            * DAnCE/tests/CIAO/Components/SimpleAttribute.mpc
            * DAnCE/tests/CIAO/Components/SimpleConsumer.mpc
            * DAnCE/tests/CIAO/Components/SimpleEmitter.mpc
            * DAnCE/tests/CIAO/Components/SimpleMultipleUser.mpc
            * DAnCE/tests/CIAO/Components/SimpleNull.mpc
            * DAnCE/tests/CIAO/Components/SimpleProvider.mpc
            * DAnCE/tests/CIAO/Components/SimplePublisher.mpc
            * DAnCE/tests/CIAO/Components/SimpleUser.mpc
            * docs/tutorials/Quoter/Simple/Broker/StockBroker.mpc
            * docs/tutorials/Quoter/Simple/Distributor/StockDistributor.mpc
            * examples/BasicSP/BasicSP.mpc
            * examples/BasicSP/BMClosedED/BMClosedED.mpc
            * examples/BasicSP/BMDevice/BMDevice.mpc
            * examples/BasicSP/BMDisplay/BMDisplay.mpc
            * examples/BasicSP/EC/EC.mpc
            * examples/DevGuideExamples/Messenger/Administrator.mpc
            * examples/Display/GPS/GPS.mpc
            * examples/Display/NavDisplay/NavDisplay.mpc
            * examples/Display/NavDisplayGUI_exec/NavDisplayGUI.mpc
            * examples/Display/RateGen/RateGen.mpc
            * examples/Hello/Sender/Sender.mpc
            * examples/Null_Component/Null_Component.mpc
            * examples/Swapping/Receiver/Receiver.mpc
            * examples/Swapping/Sender/Sender.mpc
            * MPC/config/ciao_servant.mpb
            * MPC/config/ciaocidldefaults.mpb
            * performance-tests/Benchmark/RoundTrip/RoundTrip.mpc
            * performance-tests/Benchmark/RoundTripClient/RoundTripClient.mpc
            * performance-tests/Protocols/Receiver/Receiver.mpc
            * performance-tests/Protocols/Sender/Sender.mpc
            * tests/Bug_2130_Regression/SEC_CheckPoint/TSEC_CheckPoint.mpc
            * tests/CIAO_ComponentServer/SimpleComponent/SimpleComponent.mpc
            * tests/CIDL/CodeGen/Basic.idl
            * tests/CIDL/CodeGen/CodeGen.mpc
            * tests/IDL_Test/Cornucopia/Cornucopia.mpc
            * tests/IDL_Test/Reused_Facet_Interface/CodeGen.mpc
            * tests/Minimum/Receiver/Receiver.mpc
            * tests/Minimum/Sender/Sender.mpc
            * tools/Config_Handlers/XML_File_Intf.cpp
              Zap ciaocidldefaults from the MPC files. Everything has to be rebuild to
              sort out the compile/link issues, but at least now cidlc is not used anymore

--- modules/CIAO/tools/Config_Handlers/XML_File_Intf.cpp
+++ modules/CIAO/tools/Config_Handlers/XML_File_Intf.cpp
@@ -28,41 +28,41 @@ namespace CIAO
         {
           if (!XML_Helper::XML_HELPER.is_initialized ())
             return false;
-          
+
           CIAO_DEBUG ((LM_TRACE, CLINFO "XML_File_Intf::read_process_file - "
                        "Constructing DOM\n"));
           XERCES_CPP_NAMESPACE::DOMDocument *dom = 0;
-          dom = 
+          dom =
             XML_Helper::XML_HELPER.create_dom (file);
-          
+
           if (dom == 0)
             {
               CIAO_DEBUG ((LM_TRACE, CLINFO "XML_File_Intf::read_process_file - "
-                           "Failed to open file $s\n", file));
+                           "Failed to open file %C\n", file));
               return false;
             }
-          
+
           XERCES_CPP_NAMESPACE::DOMElement *foo = dom->getDocumentElement ();
           CIAO_DEBUG ((LM_TRACE, CLINFO "XML_File_Intf::read_process_file - "
                        "DOMElement pointer: %u\n", foo));
-          
+
           CIAO_DEBUG ((LM_TRACE, CLINFO "XML_File_Intf::read_process_file - "
                        "Parsing XML file with XSC\n"));
           deploymentPlan dp =
             CIAO::Config_Handlers::reader::DeploymentPlan (dom);
-          
+
           CIAO_DEBUG ((LM_TRACE, CLINFO "XML_File_Intf::read_process_file - "
                        "Processing using config handlers\n"));
           DP_Handler dp_handler (dp);
-          
+
           this->idl_dp_.reset (dp_handler.plan ());
-          
+
           if (this->idl_dp_.get ())
             return true;
         }
       catch (Config_Error &ex)
         {
-          ACE_ERROR ((LM_ERROR, "error at %s: %s\n",
+          ACE_ERROR ((LM_ERROR, "error at %C: %C\n",
                       ex.name_.c_str (), ex.error_.c_str ()));
         }
       catch (...)
@@ -74,12 +74,12 @@ namespace CIAO
     }
 
     ::Deployment::DeploymentPlan const *
-    XML_File_Intf::get_plan (void) 
+    XML_File_Intf::get_plan (void)
     {
       CIAO_TRACE("get_plan");
       if (this->idl_dp_.get () == 0 && !this->read_process_file (this->file_.c_str ()))
         return 0;
-      
+
       return this->idl_dp_.get ();
     }
 
@@ -91,13 +91,13 @@ namespace CIAO
         return 0;
       return this->idl_dp_.release ();
     }
-    
+
     void
     XML_File_Intf::add_search_path (const ACE_TCHAR *environment,
                                     const ACE_TCHAR *relpath)
     {
       XML_Helper::_path_resolver.add_path (environment, relpath);
     }
-    
+
   }
 }
