312ba6273eaa9efbe7142f87f9a708b9dcce0c8f
William R. Otte <wotte@dre.vanderbilt.edu>
2009-06-30 17:50:16 +0000

    Tue Jun 30 17:49:49 UTC 2009  William R. Otte  <wotte@dre.vanderbilt.edu>

---
+++ modules/CIAO/connectors/dds4ccm/impl/ndds/Writer.cpp
@@ -0,0 +1,17 @@
+#include "Writer.h"
+
+// Implementation skeleton constructor
+CCM_DDS_string_Writer_i::CCM_DDS_string_Writer_i (void)
+{
+}
+
+// Implementation skeleton destructor
+CCM_DDS_string_Writer_i::~CCM_DDS_string_Writer_i (void)
+{
+}
+
+void CCM_DDS_string_Writer_i::write (
+  const char * an_instance)
+{
+  // Add your implementation here
+}
