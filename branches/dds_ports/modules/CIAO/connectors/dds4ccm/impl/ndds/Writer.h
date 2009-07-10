02a305c52af460dd03c03aa9f4d9b4c35a385b73
wotte <wotte@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-07-10 17:17:57 +0000

    ==> ChangeLog-dds4ccm <== Fri Jul 10 17:13:35 UTC 2009  William R. Otte  <wotte@dre.vanderbilt.edu>
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector.mpc:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Base.idl:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Base.mpc:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.idl:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.mpc:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_ConnectorE.idl:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.h:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.cpp:

--- branches/dds_ports/modules/CIAO/connectors/dds4ccm/impl/ndds/Writer.h
+++ branches/dds_ports/modules/CIAO/connectors/dds4ccm/impl/ndds/Writer.h
@@ -1,14 +1,37 @@
-class  CCM_DDS_string_Writer_i
-  : public virtual POA_CCM_DDS::string_Writer
+/**
+ * @file Writer.h
+ * @author William R. Otte <wotte@dre.vanderbilt.edu>
+ *
+ * $Id$
+ *
+ * Writer template for RTI NDDS dds4ccm support.
+ */
+
+#include "dds4ccm/idl/dds_rtf2_dcpsC.h"
+
+namespace CIAO
 {
-public:
-  // Constructor 
-  CCM_DDS_string_Writer_i (void);
-  
-  // Destructor 
-  virtual ~CCM_DDS_string_Writer_i (void);
-  
-  virtual
-  void write (
-    const char * an_instance);
-};
+  namespace DDS4CCM
+  {
+    namespace RTI
+    {
+      template <typename NDDS_TYPE, typename BASE>
+      class Writer_T
+      {
+      public:
+        // Constructor 
+        Writer_T (::DDS::DataWriter_ptr dw);
+        
+        // Destructor 
+        virtual ~Writer_T (void);
+        
+        virtual void write (typename NDDS_TYPE::value_type an_instance);
+        
+      private:
+        typename NDDS_TYPE::data_writer *writer_;
+      };
+    }
+  }
+}
+
+#include "dds4ccm/impl/ndds/Writer.tpp"
