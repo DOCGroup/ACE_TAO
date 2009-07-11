f6582640103e216e1ead161d09ab483719879518
William R. Otte <wotte@dre.vanderbilt.edu>
2009-07-11 21:01:53 +0000

    Moving aside connectors in preparation for merge

--- TAO/orbsvcs/orbsvcs/Event/EC_QOS_Info.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "orbsvcs/Event/EC_QOS_Info.h"
-
-#if ! defined (__ACE_INLINE__)
-#include "orbsvcs/Event/EC_QOS_Info.inl"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID(Event, EC_QOS_Info, "$Id$")
