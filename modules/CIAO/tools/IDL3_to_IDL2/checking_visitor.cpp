a0c672bddc7d2fd36e9073537412fdac6da4fdf3
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-07-17 19:09:17 +0000

    ChangeLogTag: Fri Jul 17 19:07:47 UTC 2009  Jeff Parsons  <j.parsons@vanderbilt.edu>

--- modules/CIAO/tools/IDL3_to_IDL2/checking_visitor.cpp
+++ modules/CIAO/tools/IDL3_to_IDL2/checking_visitor.cpp
@@ -169,6 +169,36 @@ checking_visitor::visit_porttype (AST_PortType *)
 }
 
 int
+checking_visitor::visit_provides (AST_Provides *)
+{
+  return 0;
+}
+
+int
+checking_visitor::visit_uses (AST_Uses *)
+{
+  return 0;
+}
+
+int
+checking_visitor::visit_publishes (AST_Publishes *)
+{
+  return 0;
+}
+
+int
+checking_visitor::visit_emits (AST_Emits *)
+{
+  return 0;
+}
+
+int
+checking_visitor::visit_consumes (AST_Consumes *)
+{
+  return 0;
+}
+
+int
 checking_visitor::visit_eventtype (AST_EventType *node)
 {
   this->is_idl3_ = true;
