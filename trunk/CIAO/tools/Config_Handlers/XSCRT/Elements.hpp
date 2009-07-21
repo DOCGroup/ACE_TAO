3b0cafc79ce923854b63e202a39e52ec82fd20af
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-07-21 13:47:27 +0000

    Tue Jul 21 13:48:47 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tools/Config_Handlers/XSCRT/Elements.hpp:
              Backported some CB2009 fixes

--- trunk/CIAO/tools/Config_Handlers/XSCRT/Elements.hpp
+++ trunk/CIAO/tools/Config_Handlers/XSCRT/Elements.hpp
@@ -240,7 +240,7 @@ namespace XSCRT
   public:
     // Trait for marshaling a FundamentalType X
     typedef X CDR_Type__;
-    typedef ACE_Refcounted_Auto_Ptr < FundamentalType, ACE_Null_Mutex > _ptr;
+//    typedef ACE_Refcounted_Auto_Ptr < FundamentalType, ACE_Null_Mutex > _ptr;
 
     FundamentalType ()
     {
