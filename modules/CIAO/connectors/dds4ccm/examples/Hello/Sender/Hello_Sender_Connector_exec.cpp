bc9942bcd5b16e12aa8ea9feda08d9611f97a2d3
Johnny Willemsen <jwillemsen@remedy.nl>
2009-07-24 12:31:45 +0000

    Fri Jul 24 12:32:35 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver.mpc
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Base.mpc
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector.mpc
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender.mpc
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Base.mpc
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.mpc
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.cpp
              Various changes to get the code to compile on windows

--- modules/CIAO/connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.cpp
+++ modules/CIAO/connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.cpp
@@ -38,77 +38,76 @@ namespace CIAO_Hello_DDS_Hello_sender_Connector_Impl
   //============================================================
   // Facet Executor Implementation Class: string_Writer_exec_i
   //============================================================
-  
+
   string_Writer_exec_i::string_Writer_exec_i (String_Writer sw)
     : writer_ (sw)
   {
   }
-  
+
   string_Writer_exec_i::~string_Writer_exec_i (void)
   {
   }
-  
+
   void
   string_Writer_exec_i::write (const char *an_instance)
   {
     writer_.write (an_instance);
   }
-  
+
   //============================================================
   // Component Executor Implementation Class: Hello_sender_Connector_exec_i
   //============================================================
-  
+
   Hello_sender_Connector_exec_i::Hello_sender_Connector_exec_i (void)
     : dds_configured_ (false)
   {
   }
-  
+
   Hello_sender_Connector_exec_i::~Hello_sender_Connector_exec_i (void)
   {
   }
-  
+
   // Supported operations and attributes.
-  
+
   // Component attributes.
-  
+
   // Port operations.
-  
+
   ::CCM_DDS::CCM_string_Writer_ptr
   Hello_sender_Connector_exec_i::get_sender_data (void)
   {
     this->configure_dds ();
     return ::CCM_DDS::CCM_string_Writer::_duplicate (this->sw_.in ());
   }
-  
+
   ::DDS::CCM_DataWriter_ptr
   Hello_sender_Connector_exec_i::get_sender_dds_entity (void)
   {
     this->configure_dds ();
-    
     return ::DDS::CCM_DataWriter::_duplicate (this->dw_.in ());
   }
-  
+
   // Operations from Components::SessionComponent.
-  
+
   void
   Hello_sender_Connector_exec_i::set_session_context (
     ::Components::SessionContext_ptr ctx)
   {
     this->context_ =
       ::Hello_DDS::CCM_Hello_sender_Connector_Context::_narrow (ctx);
-    
+
     if ( ::CORBA::is_nil (this->context_.in ()))
       {
         throw ::CORBA::INTERNAL ();
       }
   }
-  
-  void 
+
+  void
   Hello_sender_Connector_exec_i::configure_dds (void)
   {
     if (this->dds_configured_)
       return;
-    
+
     try
       {
         //NDDSConfigLogger::get_instance()->set_verbosity_by_category(NDDS_CONFIG_LOG_CATEGORY_API,
@@ -117,19 +116,19 @@ namespace CIAO_Hello_DDS_Hello_sender_Connector_Impl
         dpf_ = new CIAO::DDS4CCM::RTI::RTI_DomainParticipantFactory_i ();
         ::DDS::DomainParticipantQos qos;
         dp_ = dpf_->create_participant (0, qos, 0, 0);
-        
+
         ::DDS::TopicQos tqos;
         t_ = dp_->create_topic ("Hello, World",
                                 CIAO::DDS4CCM::RTI::String_Traits::type_support::get_type_name (),
                                 tqos,
                                 0,
                                 0);
-        
+
         ::DDS::PublisherQos pqos;
         pub_ = dp_->create_publisher (pqos,
                                       0,
                                       0);
-        
+
         ::DDS::DataWriterQos dwqos;
         DDS::DataWriter_var dwv_tmp = pub_->create_datawriter (t_.in (),
                                                                dwqos,
@@ -137,12 +136,12 @@ namespace CIAO_Hello_DDS_Hello_sender_Connector_Impl
                                                                0);
 
         dw_ = DDS::CCM_DataWriter::_narrow (dwv_tmp);
-        
-          
+
+
         String_Writer sw (dw_.in ());
-        
+
         sw_ = new string_Writer_exec_i (sw);
-        
+
       }
     catch (CORBA::Exception &ex)
       {
@@ -155,46 +154,46 @@ namespace CIAO_Hello_DDS_Hello_sender_Connector_Impl
         CIAO_ERROR ((LM_ERROR, CLINFO "Hello_sender_Connector_exec_i::configure_dds - "
                      "Error: caught unknown C++ exception while configuring DDS\n"));
       }
-    
+
     this->dds_configured_ = true;
   }
-  
-      
+
+
   void
   Hello_sender_Connector_exec_i::configuration_complete (void)
   {
     /* Your code here. */
   }
-  
+
   void
   Hello_sender_Connector_exec_i::ccm_activate (void)
   {
     /* Your code here. */
   }
-  
+
   void
   Hello_sender_Connector_exec_i::ccm_passivate (void)
   {
     /* Your code here. */
   }
-  
+
   void
   Hello_sender_Connector_exec_i::ccm_remove (void)
   {
     /* Your code here. */
   }
-  
+
   extern "C" HELLO_SENDER_CONNECTOR_EXEC_Export ::Components::EnterpriseComponent_ptr
   create_Hello_DDS_Hello_sender_Connector_Impl (void)
   {
     ::Components::EnterpriseComponent_ptr retval =
       ::Components::EnterpriseComponent::_nil ();
-    
+
     ACE_NEW_RETURN (
       retval,
       Hello_sender_Connector_exec_i,
       ::Components::EnterpriseComponent::_nil ());
-    
+
     return retval;
   }
 }
