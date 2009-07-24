0910011ef5756f291b9cc7d5917d5596a1670944
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-07-24 19:19:26 +0000

    Fri Jul 24 19:20:35 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver.mpc
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector.mpc
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.mpc
              More base project fixes
    
            * connectors/dds4ccm/impl/ndds/Utils.cpp:
              Simplified the conversion of return codes to string

--- branches/dds_ports/modules/CIAO/connectors/dds4ccm/impl/ndds/Utils.cpp
+++ branches/dds_ports/modules/CIAO/connectors/dds4ccm/impl/ndds/Utils.cpp
@@ -11,49 +11,25 @@ namespace CIAO
     {
       const char * translate_retcode (DDS_ReturnCode_t ret)
       {
+#define RTI_DDS_RETCODE(X) case X: return #X
         switch (ret)
           {
-          case DDS_RETCODE_OK:
-            return "DDS_RETCODE_OK";
-            
-          case DDS_RETCODE_ERROR:
-            return "DDS_RETCODE_ERROR";
-            
-          case DDS_RETCODE_UNSUPPORTED:
-            return "DDS_RETCODE_UNSUPPORTED";
-            
-          case DDS_RETCODE_BAD_PARAMETER:
-            return "DDS_RETCODE_BAD_PARAMETER";
-            
-          case DDS_RETCODE_PRECONDITION_NOT_MET:
-            return "DDS_RETCODE_PRECONDITION_NOT_MET";
-            
-          case DDS_RETCODE_OUT_OF_RESOURCES:
-            return "DDS_RETCODE_OUT_OF_RESOURCES";
-            
-          case DDS_RETCODE_NOT_ENABLED:
-            return "DDS_RETCODE_NOT_ENABLED";
-            
-          case DDS_RETCODE_IMMUTABLE_POLICY:
-            return "DDS_RETCODE_IMMUTABLE_POLICY";
-            
-          case DDS_RETCODE_INCONSISTENT_POLICY:
-            return "DDS_RETCODE_INCONSISTENT_POLICY";
-            
-          case DDS_RETCODE_ALREADY_DELETED:
-            return "DDS_RETCODE_ALREADY_DELETED";
-            
-          case DDS_RETCODE_TIMEOUT:
-            return "DDS_RETCODE_TIMEOUT";
-            
-          case DDS_RETCODE_NO_DATA:
-            return "DDS_RETCODE_NO_DATA";
-            
-          case DDS_RETCODE_ILLEGAL_OPERATION:
-            return "DDS_RETCODE_ILLEGAL_OPERATION";
+            RTI_DDS_RETCODE (DDS_RETCODE_OK);
+            RTI_DDS_RETCODE (DDS_RETCODE_ERROR);
+            RTI_DDS_RETCODE (DDS_RETCODE_UNSUPPORTED);
+            RTI_DDS_RETCODE (DDS_RETCODE_BAD_PARAMETER);
+            RTI_DDS_RETCODE (DDS_RETCODE_PRECONDITION_NOT_MET);
+            RTI_DDS_RETCODE (DDS_RETCODE_OUT_OF_RESOURCES);
+            RTI_DDS_RETCODE (DDS_RETCODE_NOT_ENABLED);
+            RTI_DDS_RETCODE (DDS_RETCODE_IMMUTABLE_POLICY);
+            RTI_DDS_RETCODE (DDS_RETCODE_INCONSISTENT_POLICY);
+            RTI_DDS_RETCODE (DDS_RETCODE_ALREADY_DELETED);
+            RTI_DDS_RETCODE (DDS_RETCODE_TIMEOUT);
+            RTI_DDS_RETCODE (DDS_RETCODE_NO_DATA);
+            RTI_DDS_RETCODE (DDS_RETCODE_ILLEGAL_OPERATION);
           }
-        
-        return "UNKNOWN ERROR";
+        return "***Unknown enum value, update RTI::translate_retcode()";
+#undef RTI_DDS_RETCODE
       }
     }
   }
