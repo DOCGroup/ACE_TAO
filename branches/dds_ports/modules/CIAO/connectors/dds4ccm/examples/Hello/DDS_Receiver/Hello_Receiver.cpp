2f0e71876c4212d3293f4001494ae43e6d1eab5e
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-07-27 07:22:58 +0000

    Mon Jul 27 06:58:35 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/examples/Hello/DDS_Receiver
            * connectors/dds4ccm/examples/Hello/DDS_Receiver/DDS_Receiver.mpc
            * connectors/dds4ccm/examples/Hello/DDS_Receiver/Hello_Receiver.cpp
            * connectors/dds4ccm/examples/Hello/DDS_Sender
            * connectors/dds4ccm/examples/Hello/DDS_Sender/DDS_Sender.mpc
            * connectors/dds4ccm/examples/Hello/DDS_Sender/Hello_Sender.cpp
              Receiver and sender to send plain strings as addition to the CCM based
              sender/receiver. These programs use RTI DDS

---
+++ branches/dds_ports/modules/CIAO/connectors/dds4ccm/examples/Hello/DDS_Receiver/Hello_Receiver.cpp
@@ -0,0 +1,142 @@
+// $Id$
+
+#include "ace/OS_main.h"
+#include "ace/Get_Opt.h"
+#include "ace/OS_NS_unistd.h"
+#include "ace/OS_NS_string.h"
+#include <ndds/ndds_cpp.h>
+
+/* By default DDS::String type manage strings up to 1k */
+#define MAX_STRING_SIZE         1024
+
+bool shutdown_flag = false;
+
+/* The listener of events and data from the middleware */
+class HelloListener: public DDSDataReaderListener {
+    public:
+        void on_data_available(DDSDataReader *reader);
+};
+
+
+int ACE_TMAIN(int, ACE_TCHAR*[]) {
+    HelloListener listener;
+    int           main_result = 1; /* error by default */
+
+    /* Create the domain participant on domain ID 0 */
+    DDSDomainParticipant *participant = DDSDomainParticipantFactory::get_instance()->
+                       create_participant(
+                        0,                              /* Domain ID */
+                        DDS_PARTICIPANT_QOS_DEFAULT,    /* QoS */
+                        0,                           /* Listener */
+                        DDS_STATUS_MASK_NONE);
+    if (!participant) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create domain participant.\n")));
+        goto clean_exit;
+    }
+
+    /* Create the topic "Hello, World" for the String type */
+    DDSTopic *topic = participant->create_topic(
+                        "Hello, World",                        /* Topic name*/
+                        DDSStringTypeSupport::get_type_name(), /* Type name */
+                        DDS_TOPIC_QOS_DEFAULT,                 /* Topic QoS */
+                        0,                                  /* Listener  */
+                        DDS_STATUS_MASK_NONE);
+    if (!topic) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create topic.\n")));
+        goto clean_exit;
+    }
+
+    /* Create the data writer using the default publisher */
+    DDSDataReader *data_reader = participant->create_datareader(
+                        topic,
+                        DDS_DATAREADER_QOS_DEFAULT,    /* QoS */
+                        &listener,                      /* Listener */
+                        DDS_DATA_AVAILABLE_STATUS);
+    if (!data_reader) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create data reader.\n")));
+        goto clean_exit;
+    }
+
+    /* --- Sleep During Asynchronous Reception ---------------------------- */
+
+    /* This thread sleeps forever. When a sample is received, RTI Data
+     * Distribution Service will call the on_data_available_callback function.
+     */
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Ready to read data.\n")));
+    for (;;) {
+        ACE_OS::sleep (1);
+        if(shutdown_flag){
+            break;
+        }
+    }
+
+    /* --- Clean Up ------------------------------------------------------- */
+
+    main_result = 0;
+clean_exit:
+    DDS_ReturnCode_t retcode;
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Exiting.")));
+    if (participant != 0) {
+        retcode = participant->delete_contained_entities();
+        if (retcode != DDS_RETCODE_OK) {
+            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
+            main_result = 1;
+        }
+        retcode = DDSDomainParticipantFactory::get_instance()->
+                        delete_participant(participant);
+        if (retcode != DDS_RETCODE_OK) {
+            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
+            main_result = 1;
+        }
+    }
+
+    return main_result;
+}
+
+
+/* This method gets called back by DDS when one or more data samples have been
+ * received.
+ */
+void HelloListener::on_data_available(DDSDataReader *reader) {
+    /* Perform a safe type-cast from a generic data reader into a
+     * specific data reader for the type "DDS::String"
+     */
+    DDSStringDataReader * string_reader = DDSStringDataReader::narrow(reader);
+    if (!string_reader) {
+        /* In this specific case, this will never fail */
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("DDSStringDataReader::narrow failed.\n")));
+        return;
+    }
+
+    /* Loop until there are messages available in the queue */
+    char                  sample[MAX_STRING_SIZE];
+    char *ptr_sample = &sample[0];
+    for(;;) {
+        DDS_SampleInfo        info;
+        DDS_ReturnCode_t retcode = string_reader->take_next_sample(
+                            ptr_sample,
+                            info);
+        if (retcode == DDS_RETCODE_NO_DATA) {
+            /* No more samples */
+            break;
+        } else if (retcode != DDS_RETCODE_OK) {
+            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to take data from data reader, error %d.\n"), retcode));
+            return;
+        }
+        if (info.valid_data) {
+            // Valid (this isn't just a lifecycle sample): print it
+            ACE_DEBUG ((LM_DEBUG, ACE_TEXT("%C\n"), sample));
+            if(ACE_OS::strlen(sample) == 0){
+                shutdown_flag = true;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ branches/dds_ports/modules/CIAO/connectors/dds4ccm/examples/Hello/DDS_Sender/Hello_Sender.cpp
@@ -0,0 +1,131 @@
+// $Id$
+
+#include "ace/OS_main.h"
+#include "ace/Get_Opt.h"
+#include "ace/OS_NS_unistd.h"
+#include <ndds/ndds_cpp.h>
+
+int number_of_iterations = 100;
+const char* send_string = "This is a DDS sender";
+
+int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("i:s:"));
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 's':
+        send_string = ACE_TEXT_ALWAYS_CHAR (get_opts.opt_arg ());
+        break;
+      case 'i':
+        number_of_iterations = ACE_OS::atoi (get_opts.opt_arg ());
+        break;
+
+      case '?':
+      default:
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           "usage: %s "
+                           "-s <send string>"
+                           "-i <number of iterations>"
+                           "\n",
+                           argv [0]),
+                          -1);
+      }
+  // Indicates sucessful parsing of the command line
+  return 0;
+}
+
+int ACE_TMAIN(int argc, ACE_TCHAR* argv[]) {
+    DDS_ReturnCode_t retcode;
+    int              main_result = 1; /* error by default */
+
+    if (parse_args (argc, argv) != 0)
+      return 1;
+
+    /* Create the domain participant */
+    DDSDomainParticipant *participant = DDSDomainParticipantFactory::get_instance()->
+                        create_participant(
+                                0,                              /* Domain ID */
+                                DDS_PARTICIPANT_QOS_DEFAULT,    /* QoS */
+                                0,                           /* Listener */
+                                DDS_STATUS_MASK_NONE);
+    if (!participant) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create domain participant.\n")));
+        goto clean_exit;
+    }
+
+    /* Create the topic "Hello, World" for the String type */
+    DDSTopic *topic = participant->create_topic(
+                        "Hello, World",                        /* Topic name*/
+                        DDSStringTypeSupport::get_type_name(), /* Type name */
+                        DDS_TOPIC_QOS_DEFAULT,                 /* Topic QoS */
+                        0,                                  /* Listener  */
+                        DDS_STATUS_MASK_NONE);
+    if (!topic) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create topic.\n")));
+        goto clean_exit;
+    }
+
+    /* Create the data writer using the default publisher */
+    DDSDataWriter *data_writer = participant->create_datawriter(
+                                topic,
+                                DDS_DATAWRITER_QOS_DEFAULT,     /* QoS */
+                                0,                           /* Listener */
+                                DDS_STATUS_MASK_NONE);
+    if (!data_writer) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create data writer.\n")));
+        goto clean_exit;
+    }
+
+    /* Perform a safe type-cast from a generic data writer into a
+     * specific data writer for the type "DDS::String"
+     */
+    DDSStringDataWriter *string_writer = DDSStringDataWriter::narrow(data_writer);
+    if (!string_writer) {
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("DDS_StringDataWriter_narrow failed.\n")));
+        /* In this specific case, this will never fail */
+        goto clean_exit;
+    }
+
+    /* --- Write Data ----------------------------------------------------- */
+
+    for (int i = 0; i < number_of_iterations; i++)
+      {
+        retcode = string_writer->write(
+                            send_string,
+                            DDS_HANDLE_NIL);
+        if (retcode != DDS_RETCODE_OK)
+          {
+            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Write failed: %d.\n"), retcode));
+          }
+        else
+          {
+            ACE_DEBUG ((LM_DEBUG, ACE_TEXT("Sending: %C\n"), send_string));
+          }
+        ACE_OS::sleep (1);
+    }
+
+    /* --- Clean Up ------------------------------------------------------- */
+
+    main_result = 0;
+clean_exit:
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Exiting.")));
+    if (!participant) {
+        retcode = participant->delete_contained_entities();
+        if (retcode != DDS_RETCODE_OK) {
+            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
+            main_result = 1;
+        }
+        retcode = DDSDomainParticipantFactory::get_instance()->
+                        delete_participant(participant);
+        if (retcode != DDS_RETCODE_OK) {
+            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
+            main_result = 1;
+        }
+    }
+    return main_result;
+}
+
