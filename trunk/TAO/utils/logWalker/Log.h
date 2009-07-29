4f866844fde817a3cde35ee61d760c64fa33cbf0
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-07-29 03:31:25 +0000

    Wed Jul 29 03:19:17 UTC 2009  Phil Mesnier  <mesnier_p@ociweb.com>
            * utils/logWalker/HostProcess.h:
            * utils/logWalker/HostProcess.cpp:
            * utils/logWalker/Invocation.h:
            * utils/logWalker/Invocation.cpp:
            * utils/logWalker/Log.h:
            * utils/logWalker/Log.cpp:
            * utils/logWalker/PeerProcess.h:
            * utils/logWalker/PeerProcess.cpp:
            * utils/logWalker/README:
            * utils/logWalker/Session.h:
            * utils/logWalker/Session.cpp:
            * utils/logWalker/Thread.h:
            * utils/logWalker/Thread.cpp:
            * utils/logWalker/hello.mft:
            * utils/logWalker/logWalker.cpp:
    
              Incremental improvement to log parsing utility. Invocations are
              now sorted by thread, indenting nested upcalls. The output can 
              be split out to multiple files under a named directory. This is 
              for useful for dealing with large collections of logfiles.

--- trunk/TAO/utils/logWalker/Log.h
+++ trunk/TAO/utils/logWalker/Log.h
@@ -16,8 +16,6 @@ class Session;
 class HostProcess;
 
 typedef ACE_DLList<HostProcess> HostProcesses;
-typedef ACE_DLList<PeerProcess> PeerProcesses;
-
 
 class Log
 {
@@ -27,21 +25,42 @@ public:
   ~Log();
 
   bool init (const ACE_TCHAR *filename, const char *alias = "");
-  void parse_line (char* line, size_t offset);
 
   void summarize(void);
 
   void dump (ostream &strm);
 
 private:
+  typedef void (* parser_func)(Log *this_, char *line, size_t offset);
+
+  struct parse_key
+  {
+    const char *text;
+    parser_func op;
+  };
+
   bool get_pid_tid (long &pid, long &tid, char *line);
   HostProcess *get_host (long pid);
+  void handle_msg_dump (char *line, size_t offset);
+  void parse_line (char* line, size_t offset);
+
+  static void parse_handler_open (Log *this_, char *line, size_t offset);
+  static void parse_dump_msg (Log *this_, char *line, size_t offset);
+  static void parse_HEXDUMP (Log *this_, char *line, size_t offset);
+  static void parse_open_listener (Log *this_, char *line, size_t offset);
+  static void parse_muxed_tms (Log *this_, char *line, size_t offset);
+  static void parse_process_parsed_msgs (Log *this_, char *line, size_t offset);
+  static void parse_wait_for_event (Log *this_, char *line, size_t offset);
+  static void parse_cleanup_queue (Log *this_, char *line, size_t offset);
+  static void parse_close_connection (Log *this_, char *line, size_t offset);
+  static void parse_begin_connection (Log *this_, char *line, size_t offset);
+  static void parse_local_addr (Log *this_, char *line, size_t offset);
+  static void parse_open_as_server (Log *this_, char *line, size_t offset);
 
   ACE_CString origin_;
   ACE_CString alias_;
   Session &session_;
   HostProcesses procs_;
-//  PeerProcesses peers_;
   Invocation::GIOP_Buffer* dump_target_;
   ThreadList giop_waiters_;
 
--- trunk/TAO/utils/logWalker/PeerProcess.h
+++ trunk/TAO/utils/logWalker/PeerProcess.h
@@ -8,7 +8,8 @@
 
 #include "ace/SString.h"
 #include "ace/Synch.h"
-#include "ace/Hash_Map_Manager.h"
+#include "ace/RB_Tree.h"
+#include "ace/Functor.h"
 #include "ace/Containers.h"
 #include "ace/streams.h"
 
@@ -16,58 +17,71 @@ class PeerObject;
 class HostProcess;
 class Invocation;
 class Session;
+class Thread;
 
-typedef ACE_Hash_Map_Manager<u_long, PeerObject*, ACE_Null_Mutex> PeerObjectTable;
+class Transport
+{
+public:
+  Transport (const char *addr, bool is_client, size_t offset);
+  long handle_;
+  ACE_CString client_endpoint_;
+  bool local_is_client_;
+  size_t open_offset_;
+  size_t close_offset_;
+};
+
+
+typedef ACE_RB_Tree<u_long, PeerObject*, ACE_Less_Than<u_long>, ACE_Null_Mutex> PeerObjectTable;
 typedef ACE_DLList<Invocation> InvocationList;
+typedef ACE_DLList<Transport> TransportList;
 
 class PeerProcess
 {
 public:
   static char *nextIdent(void);
-  PeerProcess (const char *text, 
-               size_t offset, 
-               bool as_client, 
-               const char *target_addr, 
-               long handle_);
-  ~PeerProcess (void);
-  void handle (long h);
-  long handle (void) const;
-  bool is_client (void) const;
+  PeerProcess (size_t offset, bool is_server);
+
+  virtual ~PeerProcess (void);
+
   const char * id (void) const;
 
   void set_owner (HostProcess *host);
   HostProcess *owner (void);
 
   void match_hosts (Session *session);
-  void set_addr (const char *addr, bool local);
+  void set_server_addr (const char *addr);
+  const ACE_CString &server_addr (void) const;
+  const ACE_CString &last_client_addr (void) const;
+
+  bool is_server (void) const;
+  void add_transport (Transport *t);
+  Transport *last_transport (void);
+  Transport *find_transport (long handle);
+  
   bool match_local (const char *addr) const;
 
-  void close (size_t offset);
-  bool is_closed (void) const;
-
-  Invocation *new_invocation (size_t req_id);
-  Invocation *find_invocation (size_t req_id);
+  Invocation *new_invocation (size_t req_id, Thread *thr);
+  Invocation *find_invocation (size_t req_id, long handle);
   Invocation *find_invocation_size (size_t len);
 
   PeerObject *object_for(const char *oid, size_t len);
 
-  void dump_summary (ostream &strm);
-  void dump_object_detail (ostream &strm);
-  void dump_invocation_detail (ostream &strm);
+  virtual void dump_summary (ostream &strm);
+  virtual void dump_object_detail (ostream &strm);
+  virtual void dump_invocation_detail (ostream &strm);
 
 private:
   char *ident_;
   HostProcess *owner_;
   HostProcess *remote_;
-  ACE_CString local_addr_;
-  ACE_CString remote_addr_;
-  long handle_;
-  bool client_;
-  ACE_CString origin_;
+  ACE_CString server_addr_;
+  TransportList transports_;
+  Transport *last_transport_;
+  bool server_;
   size_t origin_offset_;
-  size_t closed_offset_;
   PeerObjectTable objects_;
   InvocationList invocations_;
 };
+
 
 #endif // LOG_WALKER_PEER_PROCESS_H
