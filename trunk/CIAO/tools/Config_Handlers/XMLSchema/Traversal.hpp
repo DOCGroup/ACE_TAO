107f2d027cbb4c4dbe439e211dd7367b1a75631d
wotte <wotte@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-08-12 18:01:21 +0000

    Wed Aug 12 18:00:56 UTC 2009  William R. Otte  <Wotte@dre.vanderbilt.edu>

--- trunk/CIAO/tools/Config_Handlers/XMLSchema/Traversal.hpp
+++ trunk/CIAO/tools/Config_Handlers/XMLSchema/Traversal.hpp
@@ -16,7 +16,7 @@ namespace XMLSchema
     //
     //
     struct IDREF :
-      XSCRT::Traversal::Traverser<XMLSchema::IDREF_Base, XSCRT::Type>
+    XSCRT::Traversal::Traverser<XMLSchema::IDREF_Base, XSCRT::Type>
     {
       virtual void
       traverse (XMLSchema::IDREF_Base& r)
@@ -61,6 +61,11 @@ namespace XMLSchema
 
     template <typename C>
     struct ID : Traverser<XMLSchema::ID<C> >
+    {
+    };
+
+    template <typename C>
+    struct anyURI : Traverser <XMLSchema::anyURI<C> >
     {
     };
   }
