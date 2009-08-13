af7ad22f6a58b2e50a151db991bd0306670dde1a
Johnny Willemsen <jwillemsen@remedy.nl>
2009-08-13 07:15:05 +0000

    Thu Aug 13 07:14:43 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/config-win32-cegcc.h:
              Set the correct export defines
    
            * ace/DLL_Manager.cpp:
              Improved debug statement so that we can easily see that we have
              an empty string
    
            * ace/OS_NS_stdio.cpp:
              Added support for ACE_LACKS_FOPEN and simplified some methods
    
            * ace/OS_NS_stdio.inl:
              Const change

--- ACE/ace/config-win32-cegcc.h
+++ ACE/ace/config-win32-cegcc.h
@@ -103,7 +103,12 @@
 #define ACE_UINT64_FORMAT_SPECIFIER_ASCII "%I64u"
 #define ACE_ENDTHREADEX(STATUS) ExitThread ((DWORD) STATUS)
 
-#undef ACE_HAS_CUSTOM_EXPORT_MACROS
+#define ACE_Proper_Export_Flag __declspec (dllexport)
+#define ACE_Proper_Import_Flag __declspec (dllimport)
+#define ACE_EXPORT_SINGLETON_DECLARATION(T) template class __declspec (dllexport) T
+#define ACE_EXPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) template class __declspec (dllexport) SINGLETON_TYPE<CLASS, LOCK>;
+#define ACE_IMPORT_SINGLETON_DECLARATION(T) extern template class T
+#define ACE_IMPORT_SINGLETON_DECLARE(SINGLETON_TYPE, CLASS, LOCK) extern template class SINGLETON_TYPE <CLASS, LOCK>;
 
 #define ACE_DLL_PREFIX ACE_TEXT ("lib")
 
