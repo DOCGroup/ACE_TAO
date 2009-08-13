d2c1ccadb685877479d7a2091e9aca5a5b333652
William R. Otte <wotte@dre.vanderbilt.edu>
2009-08-13 17:22:20 +0000

    branching/tagging

---
+++ modules/CIAO/connectors/dds4ccm/impl/ndds/RawListener.cpp
@@ -0,0 +1,20 @@
+// $Id$
+
+#include "RawListener.h"
+
+// Implementation skeleton constructor
+CCM_DDS_string_RawListener_i::CCM_DDS_string_RawListener_i (void)
+{
+}
+
+// Implementation skeleton destructor
+CCM_DDS_string_RawListener_i::~CCM_DDS_string_RawListener_i (void)
+{
+}
+
+void CCM_DDS_string_RawListener_i::on_data (
+  const char * an_instance,
+  const ::CCM_DDS::ReadInfo & info)
+{
+  // Add your implementation here
+}
