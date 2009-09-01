3e7c82fa9d9684b4399006a8fe7fc2c83bf9f5d7
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-09-01 21:41:58 +0000

    ChangeLogTag:Tue Sep  1 21:38:48 UTC 2009  Steve Huston  <shuston@riverace.com>

--- trunk/TAO/tests/AMH_Oneway/run_test.pl
+++ trunk/TAO/tests/AMH_Oneway/run_test.pl
@@ -36,6 +36,16 @@ if ($server->WaitForFileTimed ($iorbase,
     $sv->Kill (); $sv->TimedWait (1);
     exit 1;
 }
+if ($server->GetFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot get file <$server_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
+}
+if ($client->PutFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot set file <$client_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
+}
 
 # Run the client.
 $client_status = $CL->Spawn ();
