9e5c85188f914382bbec995961721f20c2f539fd
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-09-26 07:53:03 +0000

    Sat Sep 26 07:52:39 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/DLL_ORB/run_test.pl
            * tests/IDL_Inherited_Operation/client.cpp
            * tests/IDL_Inherited_Operation/run_test.pl
            * tests/IDL_Inherited_Operation/server.cpp
            * tests/Oneways_Invoking_Twoways/run_test.pl
            * tests/POA/EndpointPolicy/run_test.pl
            * tests/POA/EndpointPolicy/server.cpp
            * tests/Portable_Interceptors/AdvSlotExt/run_test.pl
            * tests/Portable_Interceptors/AdvSlotExt/server.cpp
            * tests/RTCORBA/Destroy_Thread_Pool/run_test.pl
            * tests/RTCORBA/Policies/run_test.pl
            * tests/RTCORBA/Priority_Inversion_With_Bands/run_test.pl
            * tests/RTCORBA/RTMutex/run_test.pl
            * tests/Smart_Proxies/dtor/run_test.pl
            * tests/Timeout/run_test.pl
              Various improvements for cross platform testing

--- trunk/TAO/tests/Timeout/run_test.pl
+++ trunk/TAO/tests/Timeout/run_test.pl
@@ -10,46 +10,47 @@ use PerlACE::Run_Test;
 
 $status = 0;
 
-$iorfile = PerlACE::LocalFile ("server.ior");
-unlink $iorfile;
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
-if (PerlACE::is_vxworks_test()) {
-    $SV = new PerlACE::ProcessVX ("server", "-o server.ior");
-}
-else {
-    $SV = new PerlACE::Process ("server", "-o $iorfile");
-}
-$CL = new PerlACE::Process ("client", "-ORBDebugLevel 0 -k file://$iorfile -l 20 -h 40");
+my $iorbase = "server.ior";
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
+$SV = $server->CreateProcess ("server", "-o $server_iorfile");
+$CL = $client->CreateProcess ("client", "-ORBDebugLevel 0 -k file://$client_iorfile -l 20 -h 40");
 
-$server_spawn = $SV->Spawn ();
+$server_status = $SV->Spawn ();
 
-if ($server_spawn != 0) {
-    print STDERR "ERROR: server returned $server_spawn\n";
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     exit 1;
 }
 
-if (PerlACE::waitforfile_timed ($iorfile,
-                                $PerlACE::wait_interval_for_process_creation) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile>\n";
-    $SV->Kill ();
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-$client = $CL->SpawnWaitKill (60);
+$client_status = $CL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
-if ($client != 0) {
-    print STDERR "ERROR: client returned $client\n";
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
     $status = 1;
 }
 
-$server = $SV->WaitKill (5);
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
 
-if ($server != 0) {
-    print STDERR "ERROR: server returned $server\n";
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     $status = 1;
 }
 
-unlink $iorfile;
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
 exit $status;
