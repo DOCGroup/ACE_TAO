8147da15f4ee0e51d51827ef91111a42f7904cfb
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-09-29 14:40:52 +0000

    ChangeLogTag: Tue Sep 29 14:37:17 UTC 2009  Vladimir Zykov  <vz@prismtech.com>

--- CIAO/tests/IDL3_to_XMI/XMI_For_Module_Reopen/run_test.pl
+++ CIAO/tests/IDL3_to_XMI/XMI_For_Module_Reopen/run_test.pl
@@ -39,28 +39,33 @@ if ($target->GetFile ($xmi) == -1) {
 $A_occurs = 0;
 $A_indent = "";
 $A_line = "";
+$A_xmiid = "one";
 $B_occurs = 0;
 $B_indent = "";
 $B_line = "";
+$B_ns = "two";
 @data = ();
 
-# search for right tag in generated xmi
+# search for right tag in generated xmi 
 open (DAT, $xmi) || die ("ERROR: Could not open file <$xmi>!");
 while ($line = <DAT>) {
     push @data, $line;
-    if ($line =~ /(\s*)<UML:Package name=\"A\"/) {
+    if ($line =~ /(\s*)<UML:Package name=\"A\".*xmi\.id=\"([^\"]*)\"/) {
         ++$A_occurs;
         $A_indent = $1;
+        $A_xmiid = $2 unless ($2 eq "");
         $A_line = $line;
-    } elsif ($line =~ /(\s*)<UML:Package name=\"B\"/) {
+    } elsif ($line =~ /(\s*)<UML:Package name=\"B\".*namespace=\"([^\"]*)\"/) {
         ++$B_occurs;
         $B_indent = $1;
+        $B_ns = $2 unless ($2 eq "");
         $B_line = $line;
     }
 }
 close (DAT);
 
-if ($A_occurs == 1 && $B_occurs == 1 && length ($A_indent) < length ($B_indent)) {
+if ($A_occurs == 1 && $B_occurs == 1 && $A_xmiid eq $B_ns &&
+        length ($A_indent) < length ($B_indent)) {
     print "TEST OK. Correct tags are there.\n$A_line$B_line";
 } else {
     print STDERR "ERROR: generated XMI does is not correct!\n\nXMI is:\n\n";
