a7a635ff1ad807e03c2ee1dd9436bad347ddd951
Johnny Willemsen <jwillemsen@remedy.nl>
2009-10-01 08:21:32 +0000

    Thu Oct  1 08:21:32 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Bug_2494_Regression/run_test.pl
            * tests/Bug_2593_Regression/run_test.pl
            * tests/Bug_2734_Regression/run_test.pl
            * tests/Bug_2735_Regression/run_test.pl
            * tests/Bug_2804_Regression/run_test.pl
            * tests/Bug_2844_Regression/run_test.pl
            * tests/Bug_3276_Regression/run_test.pl
              Converted to the new test framework

--- TAO/tests/Bug_2593_Regression/run_test.pl
+++ TAO/tests/Bug_2593_Regression/run_test.pl
@@ -6,7 +6,7 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 
 $status = 0;
 $debug_level = '0';
@@ -14,55 +14,56 @@ $debug_level = '0';
 foreach $i (@ARGV) {
     if ($i eq '-debug') {
         $debug_level = '10';
-    } 
+    }
 }
 
-$iorfile = PerlACE::LocalFile ("server.ior");
-unlink $iorfile;
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
+my $iorbase = "server.ior";
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
 $endpoints = "-ORBEndpoint shmiop://12345 -ORBEndpoint iiop://:"
     . PerlACE::random_port();
 $debug_conf = "-ORBDebugLevel $debug_level";
 $svc_conf = "-ORBSvcConf server.conf";
 
-$server_opts = "-o $iorfile $svc_conf $endpoints $debug_conf";
+$server_opts = "-o $server_iorfile $svc_conf $endpoints $debug_conf";
 
-if (PerlACE::is_vxworks_test()) {
-    $SV = new PerlACE::ProcessVX ("server", $server_opts);
-}
-else {
-    $SV = new PerlACE::Process ("server", $server_opts);
-}
-$CL = new PerlACE::Process ("client", " -k file://$iorfile");
-    
-$server = $SV->Spawn ();
+$SV = $server->CreateProcess ("server", $server_opts);
+$CL = $client->CreateProcess ("client", "-k file://$client_iorfile");
 
-if ($server != 0) {
-    print STDERR "ERROR: server returned $server\n";
+$server_status = $SV->Spawn ();
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     exit 1;
 }
 
-if (PerlACE::waitforfile_timed ($iorfile,
-                        $PerlACE::wait_interval_for_process_creation) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile>\n";
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
-} 
+}
 
-$client = $CL->SpawnWaitKill (300);
+$client_status = $CL->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
-if ($client != 0) {
-    print STDERR "ERROR: client returned $client\n";
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
     $status = 1;
 }
 
-$server = $SV->WaitKill (10);
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
 
-if ($server != 0) {
-    print STDERR "ERROR: server returned $server\n";
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     $status = 1;
 }
 
-unlink $iorfile;
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
 exit $status;
--- TAO/tests/Bug_2735_Regression/run_test.pl
+++ TAO/tests/Bug_2735_Regression/run_test.pl
@@ -6,26 +6,31 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 use File::Spec;
 
-## Avoid code duplication by determining the process type and
-## storing it as a string for use later.
-my $class = (PerlACE::is_vxworks_test() ? 'PerlACE::ProcessVX' :
-                                          'PerlACE::Process');
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
 
 ## First test that the -ORBSvcConfDirective works with good options.
-my $SV = $class->new('server', '-s');
-$SV->Spawn ();
-my $server = $SV->WaitKill($PerlACE::wait_interval_for_process_creation);
-if ($server != 0) {
-  print STDERR "ERROR: server returned $server\n";
-  exit(1);
+my $SV = $server->CreateProcess ("server", "-s");
+
+$server_status = $SV->Spawn ();
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    exit 1;
+}
+
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    $status = 1;
 }
 
 ## Next, test that the -ORBSvcConfDirective gives an error with bad
 ## options.
-$SV = $class->new('server', '-f');
+my $SV = $server->CreateProcess ("server", "-f");
 
 ## Run the server, but redirect stdout and stderr to /dev/null
 ## so that the errors messages don't show up on the scoreboard
@@ -33,14 +38,21 @@ open(SAVEOUT, ">&STDOUT");
 open(SAVEERR, ">&STDERR");
 open(STDOUT, '>' . File::Spec->devnull());
 open(STDERR, ">&STDOUT");
-$SV->Spawn ();
-$server = $SV->WaitKill(5);
+$server_status = $SV->Spawn ();
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    exit 1;
+}
+
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
+
 open(STDOUT, ">&SAVEOUT");
 open(STDERR, ">&SAVEERR");
 
 ## It should have thrown an exception, so if the server exits
 ## with a zero status, then something is wrong.
-if ($server == 0) {
+if ($server_status == 0) {
   print STDERR "ERROR: server returned $server\n";
   exit(1);
 }
