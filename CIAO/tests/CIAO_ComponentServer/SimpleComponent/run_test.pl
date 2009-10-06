0439845cc26f64db7387b44912157c81f3a9da8a
msmit <msmit@remedy.nl>
2009-10-06 07:43:30 +0000

    Tue Oct  6 07:48:47 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * bin/ciao_tests.lst:
              Enabled some tests for ce/labview/fuzz
    
            * connectors/dds4ccm/examples/Quoter/Broker/Broker.idl3:
            * connectors/dds4ccm/examples/Quoter/Connector/Quoter_Connector.idl3:
            * connectors/dds4ccm/examples/Quoter/Distributor/Distributor.idl3:
            * connectors/dds4ccm/idl/dds4ccm_Base.idl3:
            * connectors/dds4ccm/idl/dds4ccm_ConnectorStatusListener.idl3:
            * connectors/dds4ccm/idl/dds4ccm_ListenerControl.idl3:
            * connectors/dds4ccm/idl/dds4ccm_PortStatusListener.idl3:
              Resolved some fuzz errors.
    
            * tests/CIAO_ComponentServer/SimpleComponent/run_test.pl:
              Converted to the new test framework

--- CIAO/tests/CIAO_ComponentServer/SimpleComponent/run_test.pl
+++ CIAO/tests/CIAO_ComponentServer/SimpleComponent/run_test.pl
@@ -6,39 +6,28 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
 use PerlACE::TestTarget;
 
 $status = 0;
-$debug_level = '0';
 $ciao_root = "$ENV{CIAO_ROOT}";
 
 foreach $i (@ARGV) {
     if ($i eq '-debug') {
-        $debug_level = '10';
-    } 
+        $ENV{"DANCE_TRACE_ENABLE"} = 1;
+        $ENV{"CIAO_TRACE_ENABLE"} = 1;
+    }
 }
 
 my $target = PerlACE::TestTarget::create_target ($PerlACE::TestConfig);
-my $host = new PerlACE::TestTarget;
 
-if (PerlACE::is_vxworks_test()) {
-    $SV1 = new PerlACE::ProcessVX ("client", "-ORBDebuglevel $debug_level -s $ciao_root/bin/ciao_componentserver -d 120");
-}
-else {
-    $SV1 = $target->CreateProcess ("client", "-ORBDebuglevel $debug_level -s $ciao_root/bin/ciao_componentserver -d 120");
-}
-    
-$server1 = $SV1->SpawnWaitKill (300);
+$TG = $target->CreateProcess ("client", "-s $ciao_root/bin/ciao_componentserver -d 120");
+$server_status = $TG->SpawnWaitKill ($target->ProcessStartWaitInterval ());
 
-if ($server1 != 0) {
-    print STDERR "ERROR: server1 returned $server\n";
+if ($server_status != 0) {
+    print STDERR "ERROR: process returned $server_status\n";
     exit 1;
 }
 
 $target->GetStderrLog();
-
-$target->DeleteFile($iorfile);
-$host->DeleteFile ($iorbase);
 
 exit $status;
