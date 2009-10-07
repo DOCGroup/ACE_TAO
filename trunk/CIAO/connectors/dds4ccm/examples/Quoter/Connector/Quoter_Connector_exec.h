bbe8a9b3e94f0e7ebdbf0b835e20cca97db40e02
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-10-07 13:32:57 +0000

    Wed Oct  7 13:33:44 UTC 2009  Johnny Willemsen <jwillemsen@remedy.nl>
            * connectors/dds4ccm/examples/Quoter/Connector/Quoter_Connector_exec.cpp
            * connectors/dds4ccm/examples/Quoter/Connector/Quoter_Connector_exec.h
              Refactored the connector into a template

--- trunk/CIAO/connectors/dds4ccm/examples/Quoter/Connector/Quoter_Connector_exec.h
+++ trunk/CIAO/connectors/dds4ccm/examples/Quoter/Connector/Quoter_Connector_exec.h
@@ -23,13 +23,14 @@
 
 namespace CIAO_Quoter_Quoter_Connector_Impl
 {
-  class QUOTER_CONNECTOR_EXEC_Export Quoter_Connector_exec_i
-    : public virtual Quoter_Connector_Exec,
+  template <typename NDDS_TYPE, typename CONNECTOR_TYPE>
+  class Connector_T 
+    : public virtual CONNECTOR_TYPE::base_type,
       public virtual ::CORBA::LocalObject
   {
   public:
-    Quoter_Connector_exec_i (void);
-    virtual ~Quoter_Connector_exec_i (void);
+    Connector_T (void);
+    virtual ~Connector_T (void);
 
     virtual char *topic_name (void);
 
@@ -47,11 +48,11 @@ namespace CIAO_Quoter_Quoter_Connector_Impl
 
     virtual void qos_profile (const char * qos_profile);
 
-    virtual ::CCM_DDS::CCM_Stock_Info_Writer_ptr get_info_in_data (void);
+    virtual typename CONNECTOR_TYPE::writer_type::_ptr_type get_info_in_data (void);
 
     virtual ::DDS::CCM_DataWriter_ptr get_info_in_dds_entity (void);
 
-    virtual ::CCM_DDS::CCM_Stock_Info_Reader_ptr get_info_out_data (void);
+    virtual typename CONNECTOR_TYPE::reader_type::_ptr_type get_info_out_data (void);
 
     virtual ::CCM_DDS::CCM_ListenerControl_ptr get_info_out_control (void);
 
@@ -66,14 +67,7 @@ namespace CIAO_Quoter_Quoter_Connector_Impl
     virtual void ccm_remove (void);
 
   private:
-    ::Quoter::CCM_Quoter_Connector_Context_var context_;
-
-    // @from use of Stock_Info as a parameter
-    typedef CIAO::DDS4CCM::RTI::Type_Traits < ::Quoter::Stock_Info,
-      ::Quoter::Stock_Info_Seq,
-      ::Quoter::Stock_InfoTypeSupport,
-      ::Quoter::Stock_InfoDataWriter,
-      ::Quoter::Stock_InfoDataReader > Stock_Info_Traits;
+    typename CONNECTOR_TYPE::context_type::_var_type context_;
 
     // Default stuff
     void configure_default_domain_ (void);
@@ -98,18 +92,54 @@ namespace CIAO_Quoter_Quoter_Connector_Impl
     bool __info_in_configured_;
     ::DDS::Publisher_var __info_in_publisher_;
     ::DDS::CCM_DataWriter_var __info_in_datawriter_;
-    ::CCM_DDS::CCM_Stock_Info_Writer_var __info_in_writer_;
+    typename CONNECTOR_TYPE::writer_type::_var_type __info_in_writer_;
 
     // @from DDS_RawListen
     void configure_port_info_out_ (void);
     bool __info_out_configured_;
-    CCM_DDS::Stock_Info_RawListener_var __info_out_rawlistener_;
+    typename CONNECTOR_TYPE::rawlistener_type::_var_type __info_out_rawlistener_;
     ACE_Atomic_Op <TAO_SYNCH_MUTEX, bool> __info_out_rawlistener_enabled_;
     CCM_DDS::PortStatusListener_var __info_out_portstatus_;
     ::DDS::Subscriber_var __info_out_subscriber_;
     ::DDS::DataReaderListener_var __info_out_listener_;
     ::DDS::DataReader_var __info_out_datareader_;
     ::DDS::DataReaderListener_var __info_out_datareaderlistener;
+  };
+
+  // @from use of Stock_Info as a parameter
+  typedef CIAO::DDS4CCM::RTI::Type_Traits < ::Quoter::Stock_Info,
+    ::Quoter::Stock_Info_Seq,
+    ::Quoter::Stock_InfoTypeSupport,
+    ::Quoter::Stock_InfoDataWriter,
+    ::Quoter::Stock_InfoDataReader > Stock_Info_DDS_Traits;
+
+  template <typename BASE_TYPE,
+            typename WRITER_TYPE,
+            typename READER_TYPE,
+            typename CONTEXT_TYPE,
+            typename RAWLISTENER_TYPE>
+  struct Connector_Traits
+  {
+    typedef BASE_TYPE base_type;
+    typedef WRITER_TYPE writer_type;
+    typedef READER_TYPE reader_type;
+    typedef CONTEXT_TYPE context_type;
+    typedef RAWLISTENER_TYPE rawlistener_type;
+  };
+
+  typedef Connector_Traits <
+    ::CIAO_Quoter_Quoter_Connector_Impl::Quoter_Connector_Exec,
+    ::CCM_DDS::CCM_Stock_Info_Writer, 
+    ::CCM_DDS::CCM_Stock_Info_Reader, 
+    ::Quoter::CCM_Quoter_Connector_Context, 
+    ::CCM_DDS::Stock_Info_RawListener> Stock_Info_Connector_Traits;
+
+  class QUOTER_CONNECTOR_EXEC_Export Quoter_Connector_exec_i :
+    public Connector_T <Stock_Info_DDS_Traits, Stock_Info_Connector_Traits>
+  {
+  public:
+    Quoter_Connector_exec_i (void);
+    virtual ~Quoter_Connector_exec_i (void);
   };
 
   extern "C" QUOTER_CONNECTOR_EXEC_Export ::Components::EnterpriseComponent_ptr
