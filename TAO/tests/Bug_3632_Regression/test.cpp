0b9b63cf5d11386ac41e05660f4f7cd90057f300
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-10-12 06:58:45 +0000

    Mon Oct 12 06:57:13 UTC 2009  Vladimir Zykov  <vz@prismtech.com>
            * tests/Bug_3632_Regression/test.cpp:
              Fixed this test after the changes for bug#3746.

--- TAO/tests/Bug_3632_Regression/test.cpp
+++ TAO/tests/Bug_3632_Regression/test.cpp
@@ -5,9 +5,20 @@
 int
 ACE_TMAIN (int, ACE_TCHAR *[])
 {
-  Test::bounded_string_seq seq;
-  seq.length (11);
-  ACE_TEST_ASSERT (seq.maximum () == 10);
+  try
+    {
+      Test::bounded_string_seq seq;
+      seq.length (11);
+      ACE_TEST_ASSERT (seq.maximum () == 10);
+    }
+  catch (::CORBA::BAD_PARAM const &)
+    {
+      // ignore this exception since it's expected.
+    }
+  catch (::CORBA::Exception const &)
+    {
+      return 1;
+    }
 
   return 0;
 }
