3722da5acfc996a128dfe2c074d46abb83bbe5de
Johnny Willemsen <jwillemsen@remedy.nl>
2009-10-13 09:45:16 +0000

    Tue Oct 13 09:44:48 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/INET_Addr_Test.cpp:
              Extended this test to for is_multicast. This fails on little endian
              machines at this moment. Thanks to <pr2345 at gmail dot com> for
              extending this test. This is related to bugzilla 3729

--- ACE/tests/INET_Addr_Test.cpp
+++ ACE/tests/INET_Addr_Test.cpp
@@ -60,6 +60,12 @@ struct Address {
   bool loopback;
 };
 
+struct Multicast_Address
+{
+  const char *addr_;
+  bool is_multicast_;
+};
+
 int run_main (int argc, ACE_TCHAR *argv[])
 {
   ACE_UNUSED_ARG (argc);
@@ -257,6 +263,33 @@ int run_main (int argc, ACE_TCHAR *argv[])
                   ACE_TEXT ("ACE_INET_Addr::string_to_addr() ")
                   ACE_TEXT ("failed to detect port number overflow\n")));
       status = 1;
+    }
+
+  // Test is_multicast()
+  Multicast_Address mcast_addresses[] =
+    {
+      { "223.0.0.5:23006", false },
+      { "224.0.0.0:23006", true },
+      { "224.0.0.1:23006", true },
+      { "239.255.255.255:23006", true },
+      { "240.0.0.0:23006", false },
+      { "2.0.0.224", false },
+      { 0, false }
+    };
+
+  for (int i = 0; mcast_addresses[i].addr_; ++i)
+    {
+      ACE_INET_Addr addr (mcast_addresses[i].addr_);
+
+      if (addr.is_multicast () != mcast_addresses[i].is_multicast_)
+        {
+          ACE_ERROR ((LM_ERROR,
+                      ACE_TEXT ("ACE_INET_Addr::is_multicast() for ")
+                      ACE_TEXT ("\"%C\" incorrectly returned %d\n"),
+                      mcast_addresses[i].addr_,
+                      (int)addr.is_multicast ()));
+          status = 1;
+        }
     }
 
   ACE_END_TEST;
