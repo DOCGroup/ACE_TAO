5c2a4f6275fae14cf09f19acb35218e9d3cb024e
Abdullah Sowayan <sowayan@users.noreply.github.com>
2009-10-14 23:52:50 +0000

    Wed Oct 14 23:15:59 UTC 2009  Abdul Sowayan  <sowayan@gmail.com>

---
+++ ACE/ace/config-macosx-snowleopard.h
@@ -0,0 +1,13 @@
+// $Id$
+#ifndef ACE_CONFIG_MACOSX_SNOWLEOPARD_H
+#define ACE_CONFIG_MACOSX_SNOWLEOPARD_H
+
+#include "ace/config-macosx-leopard.h"
+
+// This define suppresses the following error message:
+// error: #error ucontext routines are deprecated, 
+// and require _XOPEN_SOURCE to be defined
+#define _XOPEN_SOURCE
+
+#endif ACE_CONFIG_MACOSX_SNOWLEOPARD_H
+
