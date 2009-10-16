d37612581c07e20d95dd4dcbb1cddbb521b83604
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-10-16 19:55:20 +0000

    ChangeLogTag: Fri Oct 16 19:54:38 UTC 2009  Jeff Parsons <j.parsons@vanderbilt.edu>

---
+++ modules/TAO/TAO_IDL/be/be_visitor_component/facet_svh.cpp
@@ -0,0 +1,86 @@
+//
+// $Id$
+//
+
+// ============================================================================
+//
+// = LIBRARY
+//    TAO IDL
+//
+// = FILENAME
+//    facet_svh.cpp
+//
+// = DESCRIPTION
+//    Visitor generating code for a facet servant class in the
+//    servant header.
+//
+// = AUTHOR
+//    Jeff Parsons
+//
+// ============================================================================
+
+be_visitor_facet_svh::be_visitor_facet_svh (be_visitor_context *ctx)
+  : be_visitor_scope (ctx),
+    os_ (*ctx->stream ()),
+    export_macro_ (be_global->svnt_export_macro ())
+{
+  /// All existing CIAO examples set the servant export values in the CIDL
+  /// compiler to equal the IDL compiler's skel export values. Below is a
+  /// partial effort to decouple them, should be completely decoupled
+  /// sometime. See comment in codegen.cpp, line 1173.
+  if (export_macro_ == "")
+    {
+      export_macro_ = be_global->skel_export_macro ();
+    }
+}
+
+be_visitor_facet_svh::~be_visitor_facet_svh (void)
+{
+}
+
+int
+be_visitor_facet_svh::visit_component (be_component *node)
+{
+  if (this->visit_scope (node) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         ACE_TEXT ("be_visitor_facet_svh::")
+                         ACE_TEXT ("visit_component() - ")
+                         ACE_TEXT ("visit_scope() failed\n")),
+                        -1);
+    }
+    
+  return 0;
+}
+
+int
+be_visitor_facet_svh::visit_provides (be_provides *node)
+{
+  if (node->gen_facet_svnt_decl (os_) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         ACE_TEXT ("be_visitor_facet_svh")
+                         ACE_TEXT ("::visit_provides - ")
+                         ACE_TEXT ("gen_facet_svnt_decl() ")
+                         ACE_TEXT ("failed\n")),
+                        -1);
+    }
+                
+  return 0;
+}
+
+int
+be_visitor_facet_svh::visit_extended_port (be_extended_port *node)
+{
+  be_visitor_extended_port_facet_svh visitor (this->ctx_);
+
+  return visitor.visit_extended_port (node);
+}
+
+int
+be_visitor_facet_svh::visit_mirror_port (be_mirror_port *)
+{
+  // TODO
+  return 0;
+}
+
---
+++ modules/TAO/TAO_IDL/be_include/be_visitor_component/facet_svh.h
@@ -0,0 +1,53 @@
+//
+// $Id$
+//
+/* -*- c++ -*- */
+// ============================================================================
+//
+// = LIBRARY
+//    TAO IDL
+//
+// = FILENAME
+//    facet_svh.h
+//
+// = DESCRIPTION
+//    Concrete visitor for the Component node.
+//    This provides for code generation for the facet servant
+//    class declaration
+//
+// = AUTHOR
+//    Jeff Parsons
+//
+// ============================================================================
+
+#ifndef _BE_COMPONENT_FACET_SVH_H_
+#define _BE_COMPONENT_FACET_SVH_H_
+
+class be_visitor_facet_svh : public be_visitor_scope
+{
+  //
+  // = TITLE
+  //   be_visitor_facet_svh
+  //
+  // = DESCRIPTION
+  //   This is a concrete visitor to generate the facet servant decl
+  //   for a component.
+  //
+  //
+public:
+  be_visitor_facet_svh (be_visitor_context *ctx);
+  
+  ~be_visitor_facet_svh (void);
+  
+  virtual int visit_component (be_component *node);
+  virtual int visit_provides (be_provides *node);
+  virtual int visit_extended_port (be_extended_port *node);
+  virtual int visit_mirror_port (be_mirror_port *node);
+                  
+private:
+  TAO_OutStream &os_;
+  ACE_CString export_macro_;
+};
+
+#endif /* _BE_COMPONENT_FACET_SVH_H_ */
+
