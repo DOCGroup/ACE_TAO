9ea7ea1c90897f762a1a868b9dc63d745097acbc
Johnny Willemsen <jwillemsen@remedy.nl>
2009-10-19 12:10:10 +0000

    Mon Oct 19 12:10:04 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Bug_3729_Regression_Test.cpp:
            * tests/INET_Addr_Test.cpp:
            * tests/run_test.lst:
            * tests/tests.mpc:
              Moved regression test for 3729 to its own file

--- ACE/tests/INET_Addr_Test.cpp
+++ ACE/tests/INET_Addr_Test.cpp
@@ -60,17 +60,8 @@ struct Address {
   bool loopback;
 };
 
-struct Multicast_Address
+int run_main (int, ACE_TCHAR *[])
 {
-  const char *addr_;
-  bool is_multicast_;
-};
-
-int run_main (int argc, ACE_TCHAR *argv[])
-{
-  ACE_UNUSED_ARG (argc);
-  ACE_UNUSED_ARG (argv);
-
   ACE_START_TEST (ACE_TEXT ("INET_Addr_Test"));
 
   int status = 0;     // Innocent until proven guilty
@@ -263,33 +254,6 @@ int run_main (int argc, ACE_TCHAR *argv[])
                   ACE_TEXT ("ACE_INET_Addr::string_to_addr() ")
                   ACE_TEXT ("failed to detect port number overflow\n")));
       status = 1;
-    }
-
-  // Test is_multicast()
-  Multicast_Address mcast_addresses[] =
-    {
-      { "223.0.0.5:23006", false },
-      { "224.0.0.0:23006", true },
-      { "224.0.0.1:23006", true },
-      { "239.255.255.255:23006", true },
-      { "240.0.0.0:23006", false },
-      { "2.0.0.224", false },
-      { 0, false }
-    };
-
-  for (int i = 0; mcast_addresses[i].addr_; ++i)
-    {
-      ACE_INET_Addr addr (mcast_addresses[i].addr_);
-
-      if (addr.is_multicast () != mcast_addresses[i].is_multicast_)
-        {
-          ACE_ERROR ((LM_ERROR,
-                      ACE_TEXT ("ACE_INET_Addr::is_multicast() for ")
-                      ACE_TEXT ("\"%C\" incorrectly returned %d\n"),
-                      mcast_addresses[i].addr_,
-                      (int)addr.is_multicast ()));
-          status = 1;
-        }
     }
 
   ACE_END_TEST;
