e545c4c2d84752adbdfbeccb45cef9797245ba33
Johnny Willemsen <jwillemsen@remedy.nl>
2009-10-28 07:10:25 +0000

    Wed Oct 28 06:09:33 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * bin/tao_other_tests.lst:
              Enabled a few tests for ce/fuzz
    
            * orbsvcs/tests/Notify/Bug_2926_Regression/run_test.pl:
            * orbsvcs/tests/Notify/Bug_3688b_Regression/run_test.pl:
            * orbsvcs/tests/Notify/Bug_3252_Regression/run_test.pl:
            * orbsvcs/tests/Notify/Bug_3646b_Regression/run_test.pl
              Converted to the new test framework

--- TAO/orbsvcs/tests/Notify/Bug_2926_Regression/run_test.pl
+++ TAO/orbsvcs/tests/Notify/Bug_2926_Regression/run_test.pl
@@ -9,28 +9,20 @@ use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::Run_Test;
 
 $status = 0;
-$file = PerlACE::LocalFile ("test.ior");
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $iorbase = "test.ior";
+my $server_iorfile = $server->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
 
-unlink $file;
+$SV = $server->CreateProcess ("server", "");
 
-if (PerlACE::is_vxworks_test()) {
-    $SV = new PerlACE::ProcessVX ("server", "");
-}
-else {
-    $SV = new PerlACE::Process ("server", "");
-}
-
-print STDERR "\n\n==== Running bug 2926 regression test\n";
-
-$SV->Spawn ();
-
-$collocated = $SV->WaitKill (15);
+$collocated = $SV->SpawnWaitKill ($server->ProcessStartWaitInterval());
 
 if ($collocated != 0) {
-    print STDERR "ERROR: Bug_2926_Regression returned $collocated\n";
+    print STDERR "ERROR: server returned $collocated\n";
     $status = 1;
 }
 
-unlink $file;
+$server->DeleteFile($iorbase);
 
 exit $status;
