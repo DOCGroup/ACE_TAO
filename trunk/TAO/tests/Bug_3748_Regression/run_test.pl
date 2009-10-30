16c77cc78cb03217585ea0deb6ff418a12c1fb14
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-10-30 13:11:41 +0000

    Fri Oct 30 13:08:30 UTC 2009  Vladimir Zykov  <vladimir.zykov@prismtech.com>
            * tests/Bug_3748_Regression/client.cpp:
            * tests/Bug_3748_Regression/Bug_3748_Regression.mpc:
            * tests/Bug_3748_Regression/Server_ORBInitializer.h:
            * tests/Bug_3748_Regression/Hello.cpp:
            * tests/Bug_3748_Regression/Test.idl:
            * tests/Bug_3748_Regression/server.cpp:
            * tests/Bug_3748_Regression/Test_Protocols_Hooks.cpp:
            * tests/Bug_3748_Regression/Makefile.am:
            * tests/Bug_3748_Regression/Hello.h:
            * tests/Bug_3748_Regression/Test_Protocols_Hooks.h:
            * tests/Bug_3748_Regression/README:
            * tests/Bug_3748_Regression/run_test.pl:
            * tests/Bug_3748_Regression/Server_ORBInitializer.cpp:
            * bin/tao_orb_tests.lst:
              Complitely rewrote the test for bug#3748 and changed configs
              in which this test can run.

--- trunk/TAO/tests/Bug_3748_Regression/run_test.pl
+++ trunk/TAO/tests/Bug_3748_Regression/run_test.pl
@@ -21,18 +21,13 @@ my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 fail
 my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
 my $iorbase = "server.ior";
-my $shutdown_iorbase = "shutdown.ior";
 my $server_iorfile = $server->LocalFile ($iorbase);
 my $client_iorfile = $client->LocalFile ($iorbase);
-my $shutdown_server_iorfile = $server->LocalFile ($shutdown_iorbase);
-my $shutdown_client_iorfile = $client->LocalFile ($shutdown_iorbase);
 $server->DeleteFile($iorbase);
 $client->DeleteFile($iorbase);
-$server->DeleteFile($shutdown_iorbase);
-$client->DeleteFile($shutdown_iorbase);
 
-$SV = $server->CreateProcess ("server", "-ORBdebuglevel $debug_level -o $server_iorfile -s $shutdown_server_iorfile");
-$CL = $client->CreateProcess ("client", "-k file://$client_iorfile -s file://$shutdown_client_iorfile");
+$SV = $server->CreateProcess ("server", "-ORBdebuglevel $debug_level -o $server_iorfile");
+$CL = $client->CreateProcess ("client", "-k file://$client_iorfile");
 $server_status = $SV->Spawn ();
 
 if ($server_status != 0) {
@@ -40,7 +35,7 @@ if ($server_status != 0) {
     exit 1;
 }
 
-if ($server->WaitForFileTimed ($shutdown_iorbase,
+if ($server->WaitForFileTimed ($iorbase,
                                $server->ProcessStartWaitInterval()) == -1) {
     print STDERR "ERROR: cannot find file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
@@ -54,16 +49,6 @@ if ($server->GetFile ($iorbase) == -1) {
 }
 if ($client->PutFile ($iorbase) == -1) {
     print STDERR "ERROR: cannot set file <$client_iorfile>\n";
-    $SV->Kill (); $SV->TimedWait (1);
-    exit 1;
-}
-if ($server->GetFile ($shutdown_iorbase) == -1) {
-    print STDERR "ERROR: cannot retrieve file <$shutdown_server_iorfile>\n";
-    $SV->Kill (); $SV->TimedWait (1);
-    exit 1;
-}
-if ($client->PutFile ($shutdown_iorbase) == -1) {
-    print STDERR "ERROR: cannot set file <$shutdown_client_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
@@ -84,7 +69,5 @@ if ($server_status != 0) {
 
 $server->DeleteFile($iorbase);
 $client->DeleteFile($iorbase);
-$server->DeleteFile($shutdown_iorbase);
-$client->DeleteFile($shutdown_iorbase);
 
 exit $status;
--- trunk/TAO/tests/Bug_3748_Regression/server.cpp
+++ trunk/TAO/tests/Bug_3748_Regression/server.cpp
@@ -1,22 +1,21 @@
 // $Id$
 
 #include "Hello.h"
+#include "Server_ORBInitializer.h"
 #include "ace/Get_Opt.h"
 #include "ace/OS_NS_stdio.h"
-#include "ace/Task.h"
-#include "ace/ARGV.h"
+#include "tao/ORBInitializer_Registry.h"
 
 ACE_RCSID (Hello,
            server,
            "$Id$")
 
 const ACE_TCHAR *ior_output_file = ACE_TEXT ("test.ior");
-const ACE_TCHAR *shutdown_ior_output_file = ACE_TEXT ("shutdown_test.ior");
 
 int
 parse_args (int argc, ACE_TCHAR *argv[])
 {
-  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("o:s:h"));
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("o:h"));
   int c;
 
   while ((c = get_opts ()) != -1)
@@ -26,15 +25,10 @@ parse_args (int argc, ACE_TCHAR *argv[])
         ior_output_file = get_opts.opt_arg ();
         break;
 
-      case 's':
-        shutdown_ior_output_file = get_opts.opt_arg ();
-        break;
-
       case 'h':
         ACE_ERROR_RETURN ((LM_ERROR,
                            "usage:  %s "
-                           "-o <iorfile> "
-                           "-s <shutdown_iorfile>"
+                           "-o <iorfile>"
                            "\n",
                            argv [0]),
                           -1);
@@ -43,56 +37,30 @@ parse_args (int argc, ACE_TCHAR *argv[])
   return 0;
 }
 
-class Server_Task : public ACE_Task_Base
-{
-public:
-  /// Constructor
-  Server_Task (CORBA::ORB_ptr orb,
-               ACE_Thread_Manager *thr_mgr)
-    : ACE_Task_Base (thr_mgr)
-    , orb_ (CORBA::ORB::_duplicate (orb))
-  {
-  }
-
-  /// Thread entry point
-  int svc (void)
-  {
-    try
-      {
-        this->orb_->run ();
-      }
-    catch (const CORBA::Exception&)
-      {
-        return -1;
-      }
-    return 0;
-  }
-
-private:
-  /// Reference to the ORB
-  CORBA::ORB_var orb_;
-};
-
 int
-prepare_one_server (int argc, ACE_TCHAR *argv[],
-                    ACE_TCHAR const *output_filename,
-                    CORBA::ORB_out orb,
-                    CORBA::ORB_ptr shutdown_orb,
-                    PortableServer::POA_out root_poa,
-                    Hello *&hello_impl)
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
   try
     {
-      // Use output_filename as an ORB's name, just to
-      // obtain a unique ORB.
-      orb =
-        CORBA::ORB_init (argc, argv,
-                         ACE_TEXT_ALWAYS_CHAR (output_filename));
+      PortableInterceptor::ORBInitializer_ptr temp_initializer = 0;
+
+      ACE_NEW_RETURN (temp_initializer,
+                      Server_ORBInitializer (),
+                      -1);  // No exceptions yet!
+      PortableInterceptor::ORBInitializer_var initializer =
+        temp_initializer;
+
+      PortableInterceptor::register_orb_initializer (initializer.in ());
+
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
+
+      if (parse_args (argc, argv) != 0)
+        return 1;
 
       CORBA::Object_var poa_object =
         orb->resolve_initial_references("RootPOA");
 
-      root_poa =
+      PortableServer::POA_var root_poa =
         PortableServer::POA::_narrow (poa_object.in ());
 
       if (CORBA::is_nil (root_poa))
@@ -102,9 +70,11 @@ prepare_one_server (int argc, ACE_TCHAR *argv[],
 
       PortableServer::POAManager_var poa_manager = root_poa->the_POAManager ();
 
+      Hello *hello_impl = 0;
       ACE_NEW_RETURN (hello_impl,
-                      Hello (orb, shutdown_orb),
+                      Hello (orb),
                       1);
+      PortableServer::ServantBase_var owner_transfer(hello_impl);
 
       PortableServer::ObjectId_var id =
         root_poa->activate_object (hello_impl);
@@ -116,84 +86,23 @@ prepare_one_server (int argc, ACE_TCHAR *argv[],
       CORBA::String_var ior = orb->object_to_string (hello.in ());
 
       // Output the IOR to the <output_filename>
-      FILE *output_file= ACE_OS::fopen (output_filename, "w");
+      FILE *output_file= ACE_OS::fopen (ior_output_file, "w");
       if (output_file == 0)
         ACE_ERROR_RETURN ((LM_ERROR,
                            "Cannot open output file for writing IOR: %s\n",
-                           output_filename),
+                           ior_output_file),
                            1);
       ACE_OS::fprintf (output_file, "%s", ior.in ());
       ACE_OS::fclose (output_file);
 
       poa_manager->activate ();
-    }
-  catch (const CORBA::Exception& ex)
-    {
-      ex._tao_print_exception ("Exception caught:");
-      return 1;
-    }
-
-  return 0;
-}
-
-int
-ACE_TMAIN(int argc, ACE_TCHAR *argv[])
-{
-  try
-    {
-      ACE_ARGV_T<ACE_TCHAR> saved;
-      for (int i = 0; i < argc; ++i)
-        {
-          saved.add (argv[i]);
-        }
-      saved.add (ACE_TEXT ("-ORBIPHopLimit"));
-      saved.add (ACE_TEXT ("10000"));
-
-      if (parse_args (argc, argv) != 0)
-        return 1;
-
-      CORBA::ORB_var orb;
-      CORBA::ORB_out orb_param (orb);
-      PortableServer::POA_var root_poa;
-      PortableServer::POA_out root_poa_param (root_poa);
-
-      Hello *hello_impl = 0;
-      if (prepare_one_server (saved.argc (), saved.argv (), ior_output_file,
-          orb_param, CORBA::ORB::_nil (), root_poa_param, hello_impl))
-        return 1;
-      PortableServer::ServantBase_var owner_transfer(hello_impl);
-
-      CORBA::ORB_var shutdown_orb;
-      CORBA::ORB_out shutdown_orb_param (shutdown_orb);
-      PortableServer::POA_var shutdown_root_poa;
-      PortableServer::POA_out shutdown_root_poa_param (shutdown_root_poa);
-
-      Hello *shutdown_hello_impl = 0;
-      if (prepare_one_server (argc, argv, shutdown_ior_output_file,
-                              shutdown_orb_param, orb.in (),
-                              shutdown_root_poa_param,
-                              shutdown_hello_impl))
-        return 1;
-      PortableServer::ServantBase_var shutdown_owner_transfer(shutdown_hello_impl);
-
-      Server_Task server_task (shutdown_orb.in (),
-                               ACE_Thread_Manager::instance ());
-      if (server_task.activate (THR_NEW_LWP | THR_JOINABLE, 1) == -1)
-        {
-          ACE_ERROR_RETURN ((LM_ERROR,
-                             "Error activating server task\n"),
-                            1);
-        }
 
       orb->run ();
+
       ACE_DEBUG ((LM_DEBUG, "(%P|%t) server - event loop finished\n"));
+
       root_poa->destroy (1, 1);
       orb->destroy ();
-
-      ACE_Thread_Manager::instance ()->wait ();
-      ACE_DEBUG ((LM_DEBUG, "(%P|%t) shutdown server - event loop finished\n"));
-      shutdown_root_poa->destroy (1, 1);
-      shutdown_orb->destroy ();
     }
   catch (const CORBA::Exception& ex)
     {
