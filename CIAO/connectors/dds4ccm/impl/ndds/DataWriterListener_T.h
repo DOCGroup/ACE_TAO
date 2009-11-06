b0c25c6906669e77eb09ae0350d232e08be17277
Johnny Willemsen <jwillemsen@remedy.nl>
2009-11-06 13:03:20 +0000

    Fri Nov  6 12:03:35 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/ndds/Connector_T.cpp:
            * connectors/dds4ccm/impl/ndds/DataWriterListener_T.cpp:
            * connectors/dds4ccm/impl/ndds/DataWriterListener_T.h:
              Moved this listener out of the RTI specific namespace, it
              is a vendor indepedent template

--- CIAO/connectors/dds4ccm/impl/ndds/DataWriterListener_T.h
+++ CIAO/connectors/dds4ccm/impl/ndds/DataWriterListener_T.h
@@ -13,50 +13,47 @@ namespace CIAO
 {
   namespace DDS4CCM
   {
-    namespace RTI
+    template <typename DDS_TYPE, typename CCM_TYPE>
+    class DataWriterListener_T :
+      public virtual ::DDS::DataWriterListener
     {
-      template <typename DDS_TYPE, typename CCM_TYPE>
-      class DataWriterListener_T :
-        public virtual ::DDS::DataWriterListener
-      {
-      public:
-        // Constructor
-        DataWriterListener_T (
-                      typename CCM_TYPE::context_type::_ptr_type context,
-                      ACE_Atomic_Op <TAO_SYNCH_MUTEX, bool> &enabled);
+    public:
+      // Constructor
+      DataWriterListener_T (
+                    typename CCM_TYPE::context_type::_ptr_type context,
+                    ACE_Atomic_Op <TAO_SYNCH_MUTEX, bool> &enabled);
 
-        // Destructor
-        virtual ~DataWriterListener_T (void);
+      // Destructor
+      virtual ~DataWriterListener_T (void);
 
-        virtual void on_offered_deadline_missed (
-          ::DDS::DataWriter *the_writer,
-          const ::DDS::OfferedDeadlineMissedStatus & status);
+      virtual void on_offered_deadline_missed (
+        ::DDS::DataWriter *the_writer,
+        const ::DDS::OfferedDeadlineMissedStatus & status);
 
-        virtual void on_offered_incompatible_qos (
-          ::DDS::DataWriter *the_writer,
-          const ::DDS::OfferedIncompatibleQosStatus & status);
+      virtual void on_offered_incompatible_qos (
+        ::DDS::DataWriter *the_writer,
+        const ::DDS::OfferedIncompatibleQosStatus & status);
 
-        virtual void on_liveliness_lost (
-          ::DDS::DataWriter *the_writer,
-          const ::DDS::LivelinessLostStatus & status);
+      virtual void on_liveliness_lost (
+        ::DDS::DataWriter *the_writer,
+        const ::DDS::LivelinessLostStatus & status);
 
-        virtual void on_publication_matched (
-          ::DDS::DataWriter *the_writer,
-          const ::DDS::PublicationMatchedStatus & status);
+      virtual void on_publication_matched (
+        ::DDS::DataWriter *the_writer,
+        const ::DDS::PublicationMatchedStatus & status);
 
-        bool enabled () const;
-        void enabled (bool enable);
+      bool enabled () const;
+      void enabled (bool enable);
 
-      private:
-        DataWriterListener_T<DDS_TYPE, CCM_TYPE> (const DataWriterListener_T<DDS_TYPE, CCM_TYPE> &);
-        DataWriterListener_T<DDS_TYPE, CCM_TYPE> & operator = (const DataWriterListener_T<DDS_TYPE, CCM_TYPE> &);
+    private:
+      DataWriterListener_T<DDS_TYPE, CCM_TYPE> (const DataWriterListener_T<DDS_TYPE, CCM_TYPE> &);
+      DataWriterListener_T<DDS_TYPE, CCM_TYPE> & operator = (const DataWriterListener_T<DDS_TYPE, CCM_TYPE> &);
 
-        typename CCM_TYPE::context_type::_var_type context_;
+      typename CCM_TYPE::context_type::_var_type context_;
 
-        CCM_DDS::ConnectorStatusListener_var  info_out_connector_status_;
-        ACE_Atomic_Op <TAO_SYNCH_MUTEX, bool> &enable_;
-      };
-    }
+      CCM_DDS::ConnectorStatusListener_var  info_out_connector_status_;
+      ACE_Atomic_Op <TAO_SYNCH_MUTEX, bool> &enable_;
+    };
   }
 }
 
