5d3fbbc988ae2826fb9e52112bc0cd556dadd07d
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-06 13:24:35 +0000

    Fri Nov  6 13:22:43 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/ndds/Connector_T.cpp:
            * connectors/dds4ccm/impl/ndds/Getter_T.h:
            * connectors/dds4ccm/impl/ndds/Reader_T.h:
            * connectors/dds4ccm/impl/ndds/Updater_T.h:
            * connectors/dds4ccm/impl/ndds/Writer_T.h:
              Every class is now derived from its appropriate type
              (instead of CCM_TYPE).

--- trunk/CIAO/connectors/dds4ccm/impl/ndds/Writer_T.h
+++ trunk/CIAO/connectors/dds4ccm/impl/ndds/Writer_T.h
@@ -17,7 +17,7 @@ namespace CIAO
     {
       template <typename NDDS_TYPE, typename CCM_TYPE>
       class Writer_T :
-        public virtual CCM_TYPE,
+        public virtual CCM_TYPE::writer_type,
         public virtual ::CORBA::LocalObject
       {
       public:
