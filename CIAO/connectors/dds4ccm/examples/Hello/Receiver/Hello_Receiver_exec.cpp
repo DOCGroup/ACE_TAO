5d9b849c5f75c902527ea6957d3f703b4f683fef
msmit <msmit@remedy.nl>
2009-11-09 18:49:02 +0000

    Mon Nov  9 18:47:23 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/examples/Hello/Base:
            * connectors/dds4ccm/examples/Hello/Connector:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver.idl:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver.mpc:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_exec.h:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender.idl:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender.mpc:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_exec.h:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_exec.cpp:
            * connectors/dds4ccm/examples/Hello/descriptors/DeploymentPlan.cdp:
            * connectors/dds4ccm/examples/Hello/descriptors/run_test.pl:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver.idl3p:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Base.idl:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Base.mpc:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector.idl:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector.mpc:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector_exec.h:
            * connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_Connector_exec.cpp:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender.idl3p:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Base.idl:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Base.mpc:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.idl:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector.mpc:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.h:
            * connectors/dds4ccm/examples/Hello/Sender/Hello_Sender_Connector_exec.cpp:
              Completely rewritten this example. This example now uses the connector
              template implementation.

--- CIAO/connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp
+++ CIAO/connectors/dds4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp
@@ -2,207 +2,137 @@
 //
 // $Id$
 
-// TAO_IDL - Generated from
-// be/be_codegen.cpp:1278
-
 #include "Hello_Receiver_exec.h"
-#include "ace/Date_Time.h"
-#include "ace/High_Res_Timer.h"
-
 #include "ciao/Logger/Log_Macros.h"
-#include "ace/Atomic_Op.h"
+#include "tao/ORB_Core.h"
 
-namespace CIAO_Hello_DDS_Receiver_Impl
+namespace CIAO_Hello_Receiver_Impl
 {
   //============================================================
-  // Facet Executor Implementation Class: string_RawListener_exec_i
+  // Facet Executor Implementation Class: DDSHello_RawListener_exec_i
   //============================================================
-
-  string_RawListener_exec_i::string_RawListener_exec_i (Atomic_ULong &received,
-                                                        const ACE_CString &name)
-    : received_ (received),
-      name_ (name)
+  
+  DDSHello_RawListener_exec_i::DDSHello_RawListener_exec_i (void)
   {
   }
-
-  string_RawListener_exec_i::~string_RawListener_exec_i (void)
+  
+  DDSHello_RawListener_exec_i::~DDSHello_RawListener_exec_i (void)
   {
   }
-
-  // Operations from ::CCM_DDS::string_RawListener
-
+  
+  // Operations from ::CCM_DDS::DDSHello_RawListener
+  
   void
-  string_RawListener_exec_i::on_data (
-    const char * an_instance,
+  DDSHello_RawListener_exec_i::on_data (
+    const DDSHello & an_instance ,
     const ::CCM_DDS::ReadInfo & /* info */)
   {
-    
-    ++received_;
-    ACE_CString rec (an_instance);
-    ACE_Date_Time now;
-    int sec_rec = ACE_OS::atoi (rec.substr (0, 2).c_str() );
-    if (sec_rec > 0)
-      {
-        int usec_rec = ACE_OS::atoi (rec.substr (3, 6).c_str ());
-        if (sec_rec != now.second ())
-          usec_rec += 10000000;
-        printf ("<%s> received <%s>. difference <%ld>\n", 
-              this->name_.c_str (), 
-              an_instance,
-              now.microsec () - usec_rec);
-      }
-    else 
-      {
-        printf ("<%s> received <%s>.\n", 
-              this->name_.c_str (), 
-              an_instance);
-      }
+    CIAO_DEBUG ((LM_ERROR, ACE_TEXT ("DDSHello_RawListener: ")
+            ACE_TEXT ("received hello for <%s> - iterator <%d>\n"),
+            an_instance.hello.in (),
+            an_instance.iterator));
   }
   //============================================================
   // Facet Executor Implementation Class: PortStatusListener_exec_i
   //============================================================
-
-  PortStatusListener_exec_i::PortStatusListener_exec_i (Atomic_ULong &lost)
-    : lost_ (lost)
+  
+  PortStatusListener_exec_i::PortStatusListener_exec_i (void)
   {
   }
-
+  
   PortStatusListener_exec_i::~PortStatusListener_exec_i (void)
   {
   }
-
+  
   // Operations from ::CCM_DDS::PortStatusListener
-
+  
   void
   PortStatusListener_exec_i::on_requested_deadline_missed (
     ::DDS::DataReader_ptr /* the_reader */,
     const ::DDS::RequestedDeadlineMissedStatus & /* status */)
   {
-    printf ("port status listener::on_requested_deadline_missed\n");
+    /* Your code here. */
   }
-
+  
   void
   PortStatusListener_exec_i::on_sample_lost (
     ::DDS::DataReader_ptr /* the_reader */,
     const ::DDS::SampleLostStatus & /* status */)
   {
-    ++lost_;
-    printf ("port status listener::on_sample_lost\n");
+   
   }
-
+  
   //============================================================
-  // Component Executor Implementation Class: Receiver_exec_i
+  // Component Executor Implementation Class: Receiver_exec_iDDSHello_RawListener_exec_i ();
   //============================================================
-
+  
   Receiver_exec_i::Receiver_exec_i (void)
-    : context_ (0),
-      expected_ (10),
-      received_ (0),
-      lost_ (0)
   {
   }
-
+  
   Receiver_exec_i::~Receiver_exec_i (void)
   {
   }
-
-  // Supported operations and attributes.
-
-  // Component attributes.
-
-  ::CORBA::ULong
-  Receiver_exec_i::expected_samples (void)
-  {
-    return this->expected_;
-  }
-
-  void
-  Receiver_exec_i::expected_samples (::CORBA::ULong expected_samples)
-  {
-    this->expected_ = expected_samples;
-  }
-
-  char *
-  Receiver_exec_i::name (void)
-  {
-    return CORBA::string_dup (this->name_.c_str ());
-  }
-
-  void
-  Receiver_exec_i::name (const char *name)
-  {
-    this->name_ = name;
-  }
-
+  
   // Port operations.
-
-  ::CCM_DDS::CCM_string_RawListener_ptr
-  Receiver_exec_i::get_read_message_listener (void)
+  ::CCM_DDS::CCM_DDSHello_RawListener_ptr
+  Receiver_exec_i::get_info_out_listener (void)
   {
-    return new string_RawListener_exec_i (this->received_, this->name_);
+    CIAO_DEBUG ((LM_DEBUG, ACE_TEXT ("new DDSHello RAW listener\n")));
+    return new DDSHello_RawListener_exec_i ();
   }
-
+  
   ::CCM_DDS::CCM_PortStatusListener_ptr
-  Receiver_exec_i::get_read_message_status (void)
+  Receiver_exec_i::get_info_out_status (void)
   {
-    return new PortStatusListener_exec_i (this->lost_);
+    CIAO_DEBUG ((LM_DEBUG, ACE_TEXT ("new PortStatuslistener\n")));
+    return new PortStatusListener_exec_i ();
   }
-
+  
   // Operations from Components::SessionComponent.
-
   void
   Receiver_exec_i::set_session_context (
     ::Components::SessionContext_ptr ctx)
   {
     this->context_ =
-      ::Hello_DDS::CCM_Receiver_Context::_narrow (ctx);
-
+      ::Hello::CCM_Receiver_Context::_narrow (ctx);
     if ( ::CORBA::is_nil (this->context_.in ()))
       {
         throw ::CORBA::INTERNAL ();
       }
   }
-
+  
   void
   Receiver_exec_i::configuration_complete (void)
   {
-    /* Your code here. */
   }
-
+  
   void
   Receiver_exec_i::ccm_activate (void)
   {
-  /* Code to use the reader
-    ::CCM_DDS::string_Reader_var reader = this->context_->get_connection_read_message_data ();
-    ::CCM_DDS::ReadInfo_var readinfo;
-    ::CORBA::String_var string;
-    reader->read_one (string.out (), readinfo.out ());
-  */
+    ::CCM_DDS::ListenerControl_var lc = 
+    this->context_->get_connection_info_out_control ();
 
+    if (CORBA::is_nil (lc.in ()))
+      {
+        CIAO_ERROR ((LM_INFO, ACE_TEXT ("Error:  Listener control receptacle is null!\n")));
+        throw CORBA::INTERNAL ();
+      }
+    lc->enabled (true);
   }
-
+  
   void
   Receiver_exec_i::ccm_passivate (void)
   {
-    /* Your code here. */
   }
-
+  
   void
   Receiver_exec_i::ccm_remove (void)
   {
-    CIAO_DEBUG ((LM_INFO, "Receiver_exec_i received %u messages and lost %u messages\n",
-                 this->received_.value (),
-                 this->lost_.value ()));
-
-    if (this->received_ != this->expected_)
-      {
-        CIAO_ERROR ((LM_EMERGENCY, "Receiver_exec_i: Warning: Expected to receive %u messages, actually got %u\n",
-                     this->expected_, this->received_.value ()));
-      }
   }
-
+  
   extern "C" HELLO_RECEIVER_EXEC_Export ::Components::EnterpriseComponent_ptr
-  create_Hello_DDS_Receiver_Impl (void)
+  create_Hello_Receiver_Impl (void)
   {
     ::Components::EnterpriseComponent_ptr retval =
       ::Components::EnterpriseComponent::_nil ();
@@ -211,57 +141,6 @@ namespace CIAO_Hello_DDS_Receiver_Impl
       retval,
       Receiver_exec_i);
 
-    return retval;
-  }
-}
-
-namespace CIAO_Hello_DDS_Receiver_Impl
-{
-  //============================================================
-  // Home Executor Implementation Class: ReceiverHome_exec_i
-  //============================================================
-
-  ReceiverHome_exec_i::ReceiverHome_exec_i (void)
-  {
-  }
-
-  ReceiverHome_exec_i::~ReceiverHome_exec_i (void)
-  {
-  }
-
-  // All operations and attributes.
-
-  // Factory operations.
-
-  // Finder operations.
-
-  // Implicit operations.
-
-  ::Components::EnterpriseComponent_ptr
-  ReceiverHome_exec_i::create (void)
-  {
-    ::Components::EnterpriseComponent_ptr retval =
-      ::Components::EnterpriseComponent::_nil ();
-    
-    ACE_NEW_THROW_EX (
-      retval,
-      Receiver_exec_i,
-      ::CORBA::NO_MEMORY ());
-    
-    return retval;
-  }
-  
-  extern "C" HELLO_RECEIVER_EXEC_Export ::Components::HomeExecutorBase_ptr
-  create_Hello_DDS_ReceiverHome_Impl (void)
-  {
-    ::Components::HomeExecutorBase_ptr retval =
-      ::Components::HomeExecutorBase::_nil ();
-    
-    ACE_NEW_RETURN (
-      retval,
-      ReceiverHome_exec_i,
-      ::Components::HomeExecutorBase::_nil ());
-    
     return retval;
   }
 }
