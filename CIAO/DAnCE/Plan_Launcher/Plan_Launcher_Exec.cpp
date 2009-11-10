28a198d4c44e35d77460e55ea58137faa240c40a
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-10 21:22:07 +0000

    branching/tagging

--- CIAO/DAnCE/Plan_Launcher/Plan_Launcher_Exec.cpp
+++
@@ -1,8 +0,0 @@
-// $Id$
-#include "Plan_Launcher_Module.h"
-
-#define DANCE_MODULE_MAIN_SKIP_ORB_RUN
-#define DANCE_MODULE_MAIN_CLASS_NAME DAnCE_Plan_Launcher_Module
-#include "Deployment/Module_Main.h"
-
-
