85fbddbf232cc602a99b1003b256b62c5696dcd1
dai_y <dai_y@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-10 20:16:14 +0000

    Tue Nov 10 20:14:12 UTC 2009  Yan Dai  <dai_y@ociweb.com>

--- trunk/TAO/orbsvcs/orbsvcs/Notify/MonitorControlExt/MonitorSequenceProxyPushSupplier.h
+++ trunk/TAO/orbsvcs/orbsvcs/Notify/MonitorControlExt/MonitorSequenceProxyPushSupplier.h
@@ -13,6 +13,8 @@
 #include "orbsvcs/Notify/Sequence/SequenceProxyPushSupplier.h"
 #include "orbsvcs/Notify/MonitorControlExt/MonitorProxySupplier_T.h"
 
+#if defined (TAO_HAS_MONITOR_FRAMEWORK) && (TAO_HAS_MONITOR_FRAMEWORK == 1)
+
 TAO_BEGIN_VERSIONED_NAMESPACE_DECL
 
 class TAO_Notify_MC_Ext_Export TAO_MonitorSequenceProxyPushSupplier
@@ -22,6 +24,8 @@ class TAO_Notify_MC_Ext_Export TAO_MonitorSequenceProxyPushSupplier
   // we're just mixing in the MonitorProxySupplier
 };
 TAO_END_VERSIONED_NAMESPACE_DECL
+
+#endif // TAO_HAS_MONITOR_FRAMEWORK == 1
 
 #include /**/ "ace/post.h"
 #endif /* MONITORSEQUENCEPROXYPUSHSUPPLIER_H */
