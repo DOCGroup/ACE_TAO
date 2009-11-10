66b39481d3e5048dccf0d26fb490a3a60d554162
dbudko <dbudko@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-10 12:44:44 +0000

    Tue Nov 10 12:36:57 UTC 2009  Denis Budko  <denis.budko@remedy.nl>
            * tests/IORManipulation/filter/run_test.pl
            * tests/Connection_Failure/run_test.pl
            * orbsvcs/DevGuideExamples/NamingService/Naming_Server/run_test.pl
            * orbsvcs/tests/unit/Notify/MC/Statistic/run_test.pl
            * orbsvcs/tests/unit/Notify/MC/Statistic_Registry/run_test.pl
            * orbsvcs/tests/unit/Notify/MC/MonitorControlExt/run_test.pl
            * orbsvcs/tests/unit/Notify/MC/NotificationServiceMonitor/run_test.pl
            * orbsvcs/tests/unit/Notify/MC/Control/run_test.pl
            * orbsvcs/tests/unit/Trading/Interpreter/run_test.pl
            * orbsvcs/tests/Bug_2074_Regression/run_test.pl
            * tests/GIOP_Fragments/PMB_With_Fragments/run_test.pl
            * tests/Optimized_Connection/run_test.pl
            * performance-tests/Latency/Collocation/run_test.pl
            * performance-tests/POA/Object_Creation_And_Registration/run_test.pl
            * orbsvcs/tests/Bug_2137_Regression/run_test.pl
            * orbsvcs/tests/Bug_2377_Regression/run_test.pl
            * orbsvcs/tests/Bug_2377_Regression/run_test_ipv6.pl
    
              Simple porting to new test framework.

--- TAO/tests/IORManipulation/filter/run_test.pl
+++ TAO/tests/IORManipulation/filter/run_test.pl
@@ -5,27 +5,35 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-use strict;
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 use Sys::Hostname;
 
 my $status = 0;
-my $host = hostname();
-my $host = (PerlACE::is_vxworks_test() ? $ENV{'ACE_RUN_VX_TGTHOST'} :
-                                         hostname());
-my $class = (PerlACE::is_vxworks_test() ? 'PerlACE::ProcessVX' :
-                                          'PerlACE::Process');
-my $SV = $class->new('server',
-                     '-ORBUseSharedProfiles 0 ' .
-                     '-ORBEndpoint iiop://localhost ' .
-                     "-ORBEndpoint iiop://${host}");
+$debug_level = '0';
 
-my $server = $SV->SpawnWaitKill($PerlACE::wait_interval_for_process_creation);
-
-if ($server != 0) {
-  print STDERR "ERROR: server returned $server \n";
-  $status = 1;
+foreach $i (@ARGV) {
+    if ($i eq '-debug') {
+        $debug_level = '10';
+    }
 }
 
-exit($status);
+my $host = hostname();
+
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+
+
+$SV = $server->CreateProcess ("server",
+                              "-ORBdebuglevel $debug_level " .
+                              "-ORBUseSharedProfiles 0 " .
+                              "-ORBEndpoint iiop://localhost " .
+                              "-ORBEndpoint iiop://${host}");
+    
+$server_status = $SV->SpawnWaitKill ($server->ProcessStartWaitInterval());
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    $status = 1;
+}
+
+exit $status;
