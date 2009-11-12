0649cb5f79d840f4e0e12fab1479a68c24eaa1ad
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-12 14:47:44 +0000

    blowing away

--- branches/ZIOP8/modules/ACE/ace/Active_Map_Manager.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "ace/Active_Map_Manager.h"
-
-ACE_RCSID(ace, Active_Map_Manager, "$Id$")
-
-#if !defined (__ACE_INLINE__)
-#include "ace/Active_Map_Manager.inl"
-#endif /* __ACE_INLINE__ */
