9005a22991b20d74b29eac0896607c272bd5feb4
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-13 20:28:22 +0000

    Fri Nov 13 20:28:00 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/ndds/DomainParticipantFactoryQos.h
            * connectors/dds4ccm/impl/ndds/DurabilityServiceQosPolicy.h
            * connectors/dds4ccm/impl/ndds/HistoryQosPolicy.h
              New QoS conversions

---
+++ trunk/CIAO/connectors/dds4ccm/impl/ndds/DurabilityServiceQosPolicy.h
@@ -0,0 +1,56 @@
+/**
+ * @author Johnny Willemsen <jwillemsen@remedy.nl>
+ *
+ * $Id$
+ *
+ * Wrapper facade for NDDS.
+ */
+
+#ifndef CIAO_RTI_DURABILITYSERVICEQOSPOLICY_H
+#define CIAO_RTI_DURABILITYSERVICEQOSPOLICY_H
+
+inline void
+operator<<= (::DDS::DurabilityServiceQosPolicy &ddsqos, const ::DDS_DurabilityServiceQosPolicy & qos)
+{
+  ddsqos.service_cleanup_delay <<= qos.service_cleanup_delay;
+  ddsqos.history_kind = qos.history_kind;
+  ddsqos.history_depth = qos.history_depth;
+  ddsqos.max_samples = qos.max_samples;
+  ddsqos.max_instances = qos.max_instances;
+  ddsqos.max_samples_per_instance = qos.max_samples_per_instance;
+}
+
+inline void
+operator<<= (::DDS_DurabilityServiceQosPolicy &ddsqos, const ::DDS::DurabilityServiceQosPolicy & qos)
+{
+  ddsqos.service_cleanup_delay <<= qos.service_cleanup_delay;
+  ddsqos.history_kind = qos.history_kind;
+  ddsqos.history_depth = qos.history_depth;
+  ddsqos.max_samples = qos.max_samples;
+  ddsqos.max_instances = qos.max_instances;
+  ddsqos.max_samples_per_instance = qos.max_samples_per_instance;
+}
+
+inline void
+operator>>= (const ::DDS_DurabilityServiceQosPolicy &qos, ::DDS::DurabilityServiceQosPolicy & ddsqos)
+{
+  ddsqos.service_cleanup_delay <<= qos.service_cleanup_delay;
+  ddsqos.history_kind = qos.history_kind;
+  ddsqos.history_depth = qos.history_depth;
+  ddsqos.max_samples = qos.max_samples;
+  ddsqos.max_instances = qos.max_instances;
+  ddsqos.max_samples_per_instance = qos.max_samples_per_instance;
+}
+
+inline void
+operator>>= (const ::DDS::DurabilityServiceQosPolicy &qos, ::DDS_DurabilityServiceQosPolicy & ddsqos)
+{
+  ddsqos.service_cleanup_delay <<= qos.service_cleanup_delay;
+  ddsqos.history_kind = qos.history_kind;
+  ddsqos.history_depth = qos.history_depth;
+  ddsqos.max_samples = qos.max_samples;
+  ddsqos.max_instances = qos.max_instances;
+  ddsqos.max_samples_per_instance = qos.max_samples_per_instance;
+}
+
+#endif /* CIAO_RTI_DURABILITYSERVICEQOSPOLICY_H */
