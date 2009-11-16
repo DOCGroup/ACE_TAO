48451b612da53ebffb5165f5053fe91daff5a09a
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-16 10:17:53 +0000

    branching/tagging

---
+++ branches/dds4ccm_2/modules/ACE/ace/Log_Msg_Callback.cpp
@@ -0,0 +1,13 @@
+// $Id$
+
+#include "ace/Log_Msg_Callback.h"
+
+ACE_RCSID(ace, Log_Msg_Callback, "$Id$")
+
+
+ACE_BEGIN_VERSIONED_NAMESPACE_DECL
+
+ACE_Log_Msg_Callback::~ACE_Log_Msg_Callback (void)
+{
+}
+ACE_END_VERSIONED_NAMESPACE_DECL
