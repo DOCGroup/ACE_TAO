a28fd9cc9cc515e0ba0d6a49a1e068f326cd15e4
msmit <msmit@remedy.nl>
2009-11-18 14:00:33 +0000

    Wed Nov 18 13:58:28 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * DAnCE/NodeApplication/NodeApplication_Impl.cpp:
              Resolved minor typo.
    
            * bin/ciao_tests.lst:
            * connectors/dds4ccm/tests/Get_Connection_Test:
            * connectors/dds4ccm/tests/Get_Connection_Test/Base:
            * connectors/dds4ccm/tests/Get_Connection_Test/Base/Get_Connection_Test_Base.idl:
            * connectors/dds4ccm/tests/Get_Connection_Test/Base/Get_Connection_Test_Base.mpc:
            * connectors/dds4ccm/tests/Get_Connection_Test/Connector:
            * connectors/dds4ccm/tests/Get_Connection_Test/Connector/Get_Connection_Test_Connector.idl:
            * connectors/dds4ccm/tests/Get_Connection_Test/Connector/Get_Connection_Test_Connector.mpc:
            * connectors/dds4ccm/tests/Get_Connection_Test/Connector/Get_Connection_Test_Connector_exec.h:
            * connectors/dds4ccm/tests/Get_Connection_Test/Connector/Get_Connection_Test_Connector_exec.cpp:
            * connectors/dds4ccm/tests/Get_Connection_Test/Receiver:
            * connectors/dds4ccm/tests/Get_Connection_Test/Receiver/Get_Connection_Test_Receiver.idl:
            * connectors/dds4ccm/tests/Get_Connection_Test/Receiver/Get_Connection_Test_Receiver.mpc:
            * connectors/dds4ccm/tests/Get_Connection_Test/Receiver/Get_Connection_Test_Receiver_exec.h:
            * connectors/dds4ccm/tests/Get_Connection_Test/Receiver/Get_Connection_Test_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/Get_Connection_Test/Sender:
            * connectors/dds4ccm/tests/Get_Connection_Test/Sender/Get_Connection_Test_Sender.idl:
            * connectors/dds4ccm/tests/Get_Connection_Test/Sender/Get_Connection_Test_Sender.mpc:
            * connectors/dds4ccm/tests/Get_Connection_Test/Sender/Get_Connection_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/Get_Connection_Test/Sender/Get_Connection_Test_Sender_exec.cpp:
            * connectors/dds4ccm/tests/Get_Connection_Test/descriptors:
            * connectors/dds4ccm/tests/Get_Connection_Test/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/Get_Connection_Test/descriptors/USER_QOS_PROFILES.xml:
            * connectors/dds4ccm/tests/Get_Connection_Test/descriptors/run_test.pl:
              Added test to test connections within DDS4CCM.
    
            * connectors/dds4ccm/tests/KeyedSamples/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/UnkeyedSamples/descriptors/Plan.cdp:
              Changed naming of topics.

---
+++ CIAO/connectors/dds4ccm/tests/Get_Connection_Test/Sender/Get_Connection_Test_Sender_exec.h
@@ -0,0 +1,56 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_SENDER_EXEC_H_
+#define CIAO_SENDER_EXEC_H_
+
+
+#include "Get_Connection_Test_SenderEC.h"
+
+#include /**/ "Sender_exec_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+
+#include <map>
+
+namespace CIAO_Get_Connection_Test_Sender_Impl
+{
+  class Sender_exec_i
+    : public virtual Sender_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Sender_exec_i (void);
+    virtual ~Sender_exec_i (void);
+
+    virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+  private:
+    ::Get_Connection_Test::CCM_Sender_Context_var context_;
+    CCM_DDS::Get_ConnectionTest::Updater_var updater_;
+    CCM_DDS::Get_ConnectionTest::Writer_var writer_;
+    DDS::DataWriter_var updater_dds_datawriter_;
+    DDS::DataWriter_var writer_dds_datawriter_;
+    
+    bool updater_ok_;
+    bool writer_ok_;
+    bool updater_dds_datawriter_ok_;
+    bool writer_dds_datawriter_ok_;
+  };
+
+  extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_Get_Connection_Test_Sender_Impl (void);
+}
+
+#endif /* ifndef */
+
