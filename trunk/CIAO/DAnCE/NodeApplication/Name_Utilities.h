bc367158f0e50d334c095288cf5190786b7fe388
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-20 11:49:16 +0000

    Fri Nov 20 11:42:24 UTC 2009  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ciao/CIAO.mpc
            * ciao/CIAO_Client_Export.h
            * ciao/CIAO_Config.h
            * ciao/CIAO_FailureReasons.idl
            * ciao/CIAO_Server_Export.h
            * ciao/Client_init.cpp
            * ciao/Client_init.h
            * ciao/ComponentServer/CIAO_ComponentServer.mpc
            * ciao/ComponentServer/CIAO_ComponentServer_Task.cpp
            * ciao/ComponentServer/CIAO_Container_Impl.cpp
            * ciao/ComponentServer/CIAO_FailureReasons.idl
            * ciao/ComponentServer/CIAO_ServerActivator_Impl.cpp
            * ciao/ComponentServer/CIAO_ServerActivator_Impl.h
            * ciao/ComponentServer/Client_init.cpp
            * ciao/ComponentServer/Client_init.h
            * ciao/ComponentServer/Server_init.cpp
            * ciao/ComponentServer/Server_init.h
            * ciao/Server_init.cpp
            * ciao/Server_init.h
            * DAnCE/MPC/config/dance_nodeapplication.mpb
            * DAnCE/NodeApplication/Name_Utilities.cpp
            * DAnCE/NodeApplication/Name_Utilities.h
            * DAnCE/RepositoryManager/RepositoryManager.cpp
            * MPC/config/ciao_client.mpb
            * MPC/config/ciao_componentserver_stub.mpb
            * MPC/config/ciao_componentserver_svnt.mpb
            * MPC/config/ciao_server.mpb
              Moved a lot of files inside the componentserver directory, saves
              us a few libraries and makes things much easier to maintain.
              Zap several helper functions that are not needed in the component
              server but which did pullin the naming service

--- trunk/CIAO/DAnCE/NodeApplication/Name_Utilities.h
+++ trunk/CIAO/DAnCE/NodeApplication/Name_Utilities.h
@@ -1,9 +1,9 @@
-/** 
+/**
  * @file Name_Utilities.h
  * @author William R. Otte <wotte@dre.vanderbilt.edu>
  *  // $Id$
- * A set of utility methods to manages references. 
- * 
+ * A set of utility methods to manages references.
+ *
  */
 
 #ifndef NAME_UTILITIES_H
@@ -13,30 +13,28 @@
 
 namespace DAnCE
 {
-  class Name_Utilities
+  namespace Name_Utilities
   {
-  public:
-    /// Write IOR to named file.  Will overwrite file if present. 
-    static bool write_ior (const ACE_TCHAR *file, const char *ior);
-    
+    /// Write IOR to named file.  Will overwrite file if present.
+    bool write_ior (const ACE_TCHAR *file, const char *ior);
+
     /// Binds object to provided name, which may be formatted with / to indicate
-    /// naming contexts, e.g. A/B/C. 
-    static bool bind_object (const char *name,
-                             CORBA::Object_ptr obj, 
-                             CosNaming::NamingContext_ptr);
-    
-    /// Will unbind the provided name.  
-    static bool unbind_object (const char *name,
-                               CosNaming::NamingContext_ptr);
-    
-  private:
-    static void bind_context (CosNaming::Name &nm,
-                              CosNaming::NamingContext_ptr);
-    
-    static void build_name (const char *nm,
-                            CosNaming::Name &);
-    
-  };
+    /// naming contexts, e.g. A/B/C.
+    bool bind_object (const char *name,
+                      CORBA::Object_ptr obj,
+                      CosNaming::NamingContext_ptr);
+
+    /// Will unbind the provided name.
+    bool unbind_object (const char *name,
+                        CosNaming::NamingContext_ptr);
+
+    void bind_context (CosNaming::Name &nm,
+                       CosNaming::NamingContext_ptr);
+
+    void build_name (const char *nm,
+                     CosNaming::Name &);
+
+  }
 }
 
 #endif /* NAME_UTILITIES_H */
