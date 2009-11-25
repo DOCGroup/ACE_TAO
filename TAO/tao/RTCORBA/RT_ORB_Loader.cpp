08353dec6e7b216bc306680ba8b55e7e2aafdb02
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-11-25 15:39:24 +0000

    Wed Nov 25 15:37:11 UTC 2009  Vladimir Zykov  <vladimir.zykov@prismtech.com>
            * tao/RTCORBA/RT_ORB_Loader.cpp:
              Additional fix for bug#3755. Previous fix broke Bug_3755_Regression.

--- TAO/tao/RTCORBA/RT_ORB_Loader.cpp
+++ TAO/tao/RTCORBA/RT_ORB_Loader.cpp
@@ -32,13 +32,14 @@ TAO_RT_ORB_Loader::init (int argc, ACE_TCHAR* argv[])
   ACE_TRACE ("TAO_RT_ORB_Loader::init");
 
   ACE_Service_Gestalt *gestalt = ACE_Service_Config::current ();
+  ACE_Service_Gestalt *global = ACE_Service_Config::global ();
 
   ACE_Service_Object * const rt_loader =
     ACE_Dynamic_Service<ACE_Service_Object>::instance (
       gestalt,
       "RT_ORB_Loader");
 
-  if (rt_loader != 0 && rt_loader != this)
+  if (rt_loader != 0 && rt_loader != this && gestalt == global)
     {
       this->initialized_ = true;
       return rt_loader->init (argc, argv);
