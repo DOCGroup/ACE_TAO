1a279119c6efec26ddf864e27d07c162330b3f85
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2009-11-30 11:38:15 +0000

    Mon Nov 30 11:32:37 UTC 2009  Marijke Hengstmengel <mhengstmengel@remedy.nl>
            * connectors/dds4ccm/tests/CSLDeadline:
            * connectors/dds4ccm/tests/CSLDeadline/Base:
            * connectors/dds4ccm/tests/CSLDeadline/Base/CSL_DeadlineTest_Base.idl:
            * connectors/dds4ccm/tests/CSLDeadline/Base/CSL_DeadlineTest_Base.mpc:
            * connectors/dds4ccm/tests/CSLDeadline/Connector:
            * connectors/dds4ccm/tests/CSLDeadline/Connector/CSL_DeadlineTest_Connector.idl:
            * connectors/dds4ccm/tests/CSLDeadline/Connector/CSL_DeadlineTest_Connector.mpc:
            * connectors/dds4ccm/tests/CSLDeadline/Connector/CSL_DeadlineTest_Connector_exec.cpp:
            * connectors/dds4ccm/tests/CSLDeadline/Connector/CSL_DeadlineTest_Connector_exec.h:
            * connectors/dds4ccm/tests/CSLDeadline/descriptors:
            * connectors/dds4ccm/tests/CSLDeadline/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/CSLDeadline/descriptors/run_test.pl:
            * connectors/dds4ccm/tests/CSLDeadline/descriptors/USER_QOS_PROFILES.xml:
            * connectors/dds4ccm/tests/CSLDeadline/Receiver:
            * connectors/dds4ccm/tests/CSLDeadline/Receiver/CSL_DeadlineTest_Receiver.idl:
            * connectors/dds4ccm/tests/CSLDeadline/Receiver/CSL_DeadlineTest_Receiver.mpc:
            * connectors/dds4ccm/tests/CSLDeadline/Receiver/CSL_DeadlineTest_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/CSLDeadline/Receiver/CSL_DeadlineTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLDeadline/Sender:
            * connectors/dds4ccm/tests/CSLDeadline/Sender/CSL_DeadlineTest_Sender.idl:
            * connectors/dds4ccm/tests/CSLDeadline/Sender/CSL_DeadlineTest_Sender.mpc:
            * connectors/dds4ccm/tests/CSLDeadline/Sender/CSL_DeadlineTest_Sender_exec.cpp:
            * connectors/dds4ccm/tests/CSLDeadline/Sender/CSL_DeadlineTest_Sender_exec.h:
              Test ConnectorStatusListener on_offered_deadline_missed.
            
            * connectors/dds4ccm/tests/CSLQoS:
            * connectors/dds4ccm/tests/CSLQoS/Base:
            * connectors/dds4ccm/tests/CSLQoS/Base/CSL_QoSTest_Base.idl:
            * connectors/dds4ccm/tests/CSLQoS/Base/CSL_QoSTest_Base.mpc:
            * connectors/dds4ccm/tests/CSLQoS/Connector:
            * connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector.idl:
            * connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector.mpc:
            * connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector_exec.cpp:
            * connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector_exec.h:
            * connectors/dds4ccm/tests/CSLQoS/descriptors:
            * connectors/dds4ccm/tests/CSLQoS/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/CSLQoS/descriptors/run_test.pl:
            * connectors/dds4ccm/tests/CSLQoS/descriptors/USER_QOS_PROFILES.xml:
            * connectors/dds4ccm/tests/CSLQoS/Receiver:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver.idl:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver.mpc:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLQoS/Sender:
            * connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender.idl:
            * connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender.mpc:
            * connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.cpp:
            * connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.h:
              Test ConnectorStatusListener on_requested- and on_offered_incompatible_QoS.

---
+++ CIAO/connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector_exec.cpp
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+// $Id$
+
+#include "CSL_QoSTest_Connector_exec.h"
+
+namespace CIAO_CSL_QoSTest_CSL_QoSTest_Connector_Impl
+{
+  CSL_QoSTest_Connector_exec_i::CSL_QoSTest_Connector_exec_i (const char * topic_name)
+    : DDS_Event_Connector_T<TestTopic_DDS_Traits, TestTopic_Connector_Traits> ()
+  {
+  }
+
+  CSL_QoSTest_Connector_exec_i::~CSL_QoSTest_Connector_exec_i (void)
+  {
+  }
+
+  extern "C" CSL_QOSTEST_CONNECTOR_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_CSL_QoSTest_CSL_QoSTest_Connector_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_NORETURN (
+      retval,
+      CSL_QoSTest_Connector_exec_i ("Square")); //should be set by dep. plan.
+
+    return retval;
+  }
+}
+
---
+++ CIAO/connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.cpp
@@ -0,0 +1,147 @@
+// -*- C++ -*-
+// $Id$
+
+#include "CSL_QoSTest_Sender_exec.h"
+#include "ace/Guard_T.h"
+#include "ciao/Logger/Log_Macros.h"
+#include "tao/ORB_Core.h"
+#include "ace/Reactor.h"
+
+namespace CIAO_CSL_QoSTest_Sender_Impl
+{
+
+  //============================================================
+  // Facet Executor Implementation Class: ConnectorStatusListener_exec_i
+  //============================================================
+  
+  ConnectorStatusListener_exec_i::ConnectorStatusListener_exec_i (Atomic_Boolean &incompatible)
+   : incompatible_ (incompatible)
+  {
+  }
+  
+  ConnectorStatusListener_exec_i::~ConnectorStatusListener_exec_i (void)
+  {
+    
+  }
+  
+  // Operations from ::CCM_DDS::ConnectorStatusListener
+  void ConnectorStatusListener_exec_i::on_inconsistent_topic(
+    ::DDS::Topic_ptr /*the_topic*/, 
+     const DDS::InconsistentTopicStatus & /*status*/)
+    {
+       //printf("Sender : ConnectorStatusListener_exec_i::on_inconsistent_topic\n");
+    }
+  void ConnectorStatusListener_exec_i::on_requested_incompatible_qos(
+    ::DDS::DataReader_ptr /*the_reader*/,
+     const DDS::RequestedIncompatibleQosStatus & /*status*/)  {
+       //printf("Sender:ConnectorStatusListener_exec_i::on_requested_incompatible_qos, status = %dl\n", status);
+    }
+  void ConnectorStatusListener_exec_i::on_sample_rejected(
+     ::DDS::DataReader_ptr /*the_reader*/, 
+     const DDS::SampleRejectedStatus & /*status*/)  {
+      //printf("ConnectorStatusListener_exec_i::on_sample_rejected\n");
+    }
+  void ConnectorStatusListener_exec_i::on_offered_deadline_missed(
+     ::DDS::DataWriter_ptr /*the_writer*/,
+     const DDS::OfferedDeadlineMissedStatus & /*status*/)  {
+     //printf("ConnectorStatusListener_exec_i::on_offered_deadline_missed\n");
+    }
+  void ConnectorStatusListener_exec_i::on_offered_incompatible_qos(
+     ::DDS::DataWriter_ptr the_writer, 
+     const DDS::OfferedIncompatibleQosStatus & /*status*/)  {
+//     printf("Sender:ConnectorStatusListener_exec_i::on_offered_incompatible_qos status = %dl\n", status);
+       this->incompatible_ = true;
+    }
+  void ConnectorStatusListener_exec_i::on_unexpected_status(
+    ::DDS::Entity_ptr /*the_entity*/,
+    ::DDS::StatusKind  /*status_kind*/)  {
+    //  CORBA::ULong kind = status_kind;
+    //  printf("Sender :ConnectorStatusListener_exec_i::on_unexpected_status #### status_kind = %d \n", kind);
+    }
+  //============================================================
+  // Component Executor Implementation Class: Sender_exec_i
+  //============================================================
+
+  Sender_exec_i::Sender_exec_i (void)
+    : incompatible_ (false)
+  {
+  }
+
+  Sender_exec_i::~Sender_exec_i (void)
+  {
+  }
+
+  ::CCM_DDS::CCM_ConnectorStatusListener_ptr
+  Sender_exec_i::get_test_topic_connector_status (void)
+  {
+   // printf ("*************** out connector status sender ************************\n");
+    return new ConnectorStatusListener_exec_i (this->incompatible_);
+  }
+ 
+  // Supported operations and attributes.
+ 
+  void
+  Sender_exec_i::set_session_context (::Components::SessionContext_ptr ctx)
+  {
+    this->context_ =
+      ::CSL_QoSTest::CCM_Sender_Context::_narrow (ctx);
+
+    if ( ::CORBA::is_nil (this->context_.in ()))
+      {
+        throw ::CORBA::INTERNAL ();
+      }
+   }
+
+  void
+  Sender_exec_i::configuration_complete (void)
+  {
+    //printf("-------------configuration_complete ----------------\n"); 
+  }
+ 
+  void
+  Sender_exec_i::ccm_activate (void)
+  {
+  }
+
+  void
+  Sender_exec_i::ccm_passivate (void)
+  {
+  }
+
+  void
+  Sender_exec_i::ccm_remove (void)
+  {
+    //printf("*************in remove Sender********** \n");
+  
+    CORBA::Boolean _expected = true;
+    if(!this->incompatible_.value ())
+      {   
+     
+         CIAO_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: did not receive the expected ")
+                               ACE_TEXT (" error 'on_offered_incompatible_Qos' in Sender")
+                    )); 
+      }
+   
+    else
+      {
+         CIAO_DEBUG ((LM_DEBUG, ACE_TEXT ("Received the expected ")
+                               ACE_TEXT ("'on_offered_incompatible_qos' in Sender\n")
+                    ));
+      }
+
+  }
+
+  extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_CSL_QoSTest_Sender_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_NORETURN (
+      retval,
+      Sender_exec_i);
+
+    return retval;
+  }
+}
+
