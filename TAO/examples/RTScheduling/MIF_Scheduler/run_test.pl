9e260213f3fff6863390bd487c5c50edf4ff3ff9
dbudko <dbudko@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-02 10:08:36 +0000

    Wed Dec  2 10:07:02 UTC 2009  Denis Budko  <denis.budko@remedy.nl>
            * orbsvcs/tests/Bug_1630_Regression/run_test.pl:
            * orbsvcs/tests/BiDir_CORBALOC/run_test.pl:
            * orbsvcs/tests/HTIOP/Hello/run_test.pl:
            * orbsvcs/tests/HTIOP/AMI/run_test.pl:
            * orbsvcs/tests/HTIOP/BiDirectional/run_test.pl:
            * orbsvcs/tests/Notify/Lanes/README:
            * orbsvcs/tests/Notify/Lanes/run_test.pl:
            * orbsvcs/tests/FaultTolerance/IOGR/run_test.pl:
            * orbsvcs/tests/FaultTolerance/GroupRef_Manipulation/run_test.pl:
            * orbsvcs/tests/Bug_2248_Regression/run_test.pl:
            * orbsvcs/tests/Time/run_test.pl:
            * DevGuideExamples/Multithreading/ThreadPerConnection/run_test.pl:
            * tests/IOR_Endpoint_Hostnames/list_interfaces.cpp:
            * tests/IOR_Endpoint_Hostnames/generate_ior.cpp:
            * tests/IOR_Endpoint_Hostnames/IOR_Endpoint_Hostnames.mpc:
            * tests/IOR_Endpoint_Hostnames/Makefile.am:
            * tests/IOR_Endpoint_Hostnames/run_test.pl:
            * tests/Bug_1482_Regression/run_test.pl:
            * tests/Nested_Upcall_Crash/run_test.pl:
            * examples/RTScheduling/Fixed_Priority_Scheduler/run_test.pl:
            * examples/RTScheduling/MIF_Scheduler/run_test.pl:
            * examples/AMH/Sink_Server/run_test.pl:
            * examples/Load_Balancing/run_test.pl:
    
              Tests are converted to use new test framework
              and added to fuzz build.

--- TAO/examples/RTScheduling/MIF_Scheduler/run_test.pl
+++ TAO/examples/RTScheduling/MIF_Scheduler/run_test.pl
@@ -1,88 +1,152 @@
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
-    & eval 'exec perl -S $0 $argv:q'
-    if 0;
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
 
 # $Id$
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
-
-PerlACE::add_lib_path ('..');
+use PerlACE::TestTarget;
 
 $status = 0;
+$debug_level = '0';
 
-$nsior = PerlACE::LocalFile ("ns.ior");
+foreach $i (@ARGV) {
+    if ($i eq '-debug') {
+        $debug_level = '10';
+    }
+}
 
-unlink $nsior;
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
+my $nstarget = PerlACE::TestTarget::create_target (3) || die "Create target 3 failed\n";
+my $starter = PerlACE::TestTarget::create_target (4) || die "Create target 4 failed\n";
 
-$NS = new PerlACE::Process ("../../../orbsvcs/Naming_Service/Naming_Service", "-o $nsior");
-$TS1 = new PerlACE::Process ("test", "-ORBInitRef NameService=file://$nsior -ORBSvcConf svc.conf.server");
-$TS2 = new PerlACE::Process ("test", "-ORBInitRef NameService=file://$nsior -ORBSvcConf svc.conf.client");
-$ST = new PerlACE::Process ("Starter", "-ORBInitRef NameService=file://$nsior");
+$server->AddLibPath ('..');
+$client->AddLibPath ('..');
+$starter->AddLibPath ('..');
+
+my $iorbase = "ns.ior";
+my $s_svcbase = "svc$PerlACE::svcconf_ext.server";
+my $c_svcbase = "svc$PerlACE::svcconf_ext.client";
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $server_svcfile = $server->LocalFile ($s_svcbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+my $client_svcfile = $client->LocalFile ($c_svcbase);
+my $nstarget_iorfile = $nstarget->LocalFile ($iorbase);
+my $starter_iorfile = $starter->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
+$nstarget->DeleteFile($iorbase);
+$starter->DeleteFile($iorbase);
+
+$NS = $nstarget->CreateProcess ("../../../orbsvcs/Naming_Service/Naming_Service",
+                                "-o $nstarget_iorfile");
+$SV = $server->CreateProcess ("test",
+                              "-ORBInitRef NameService=file://$server_iorfile ".
+                              "-ORBSvcConf $server_svcfile");
+$CL = $client->CreateProcess ("test",
+                              "-ORBInitRef NameService=file://$client_iorfile ".
+                              "-ORBSvcConf $client_svcfile");
+$ST = $client->CreateProcess ("Starter",
+                              "-ORBInitRef NameService=file://$starter_iorfile");
 
 print STDERR "Starting Naming Service\n";
 
-$NS->Spawn ();
+$nstarget_status = $NS->Spawn ();
 
-if (PerlACE::waitforfile_timed ($nsior, $PerlACE::wait_interval_for_process_creation) == -1) {
-    print STDERR "ERROR: cannot find naming service IOR file\n";
-    $NS->Kill (); 
+if ($nstarget_status != 0) {
+    print STDERR "ERROR: ns returned $ns_status\n";
+    exit 1;
+}
+
+if ($nstarget->WaitForFileTimed ($iorbase,
+                                 $nstarget->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$nstarget_iorfile>\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    exit 1;
+}
+
+if ($nstarget->GetFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot retrieve file <$nstarget_iorfile>\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    exit 1;
+}
+if ($server->PutFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot set file <$server_iorfile>\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    exit 1;
+}
+if ($client->PutFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot set file <$client_iorfile>\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    exit 1;
+}
+if ($starter->PutFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot set file <$starter_iorfile>\n";
+    $NS->Kill (); $NS->TimedWait (1);
     exit 1;
 }
 
 print STDERR "Starting Test 1\n";
 
-$TS1->Spawn ();
+$server_status = $SV->Spawn ();
 
-sleep 5;
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    exit 1;
+}
+
+sleep (10);
 
 print STDERR "Starting Test 2\n";
 
-$TS2->Spawn ();
+$client_status = $CL->Spawn ();
 
-sleep 5;
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
+}
 
-$starter = $ST->SpawnWaitKill (200);
+sleep (5);
 
-if ($starter!= 0) {
-    print STDERR "ERROR:  starter returned $starter\n";
+$starter_status = $ST->SpawnWaitKill ($starter->ProcessStartWaitInterval() + 185);
+
+if ($starter_status != 0) {
+    print STDERR "ERROR: starter returned $starter_status\n";
+    $NS->Kill (); $NS->TimedWait (1);
+    $SV->Kill (); $SV->TimedWait (1);
+    $CL->Kill (); $CL->TimedWait (1);
+    exit 1;
+}
+
+$server_status = $SV->TerminateWaitKill ($server->ProcessStopWaitInterval());
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
     $status = 1;
 }
 
-sleep 200;
+$client_status = $CL->TerminateWaitKill ($client->ProcessStopWaitInterval());
 
-$test1 = $TS1->TerminateWaitKill (5);
-
-if ($test1 != 0) {
-    print STDERR "ERROR: receiver returned $test1\n";
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
     $status = 1;
 }
 
-$test2 = $TS2->TerminateWaitKill (5);
+$nstarget_status = $NS->TerminateWaitKill ($nstarget->ProcessStopWaitInterval());
 
-if ($test2!= 0) {
-    print STDERR "ERROR: test 1 returned $test2\n";
+if ($nstarget_status != 0) {
+    print STDERR "ERROR: ns returned $nstarget_status\n";
     $status = 1;
 }
 
-
-$nserver = $NS->TerminateWaitKill (5);
-
-if ($nserver != 0) {
-    print STDERR "ERROR: Naming Service returned $nserver\n";
-    $status = 1;
-}
-
-unlink $nsior;
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
+$nstarget->DeleteFile($iorbase);
+$starter->DeleteFile($iorbase);
 
 exit $status;
-
-
-
-
-
-
-
-
-
--- TAO/orbsvcs/tests/HTIOP/AMI/run_test.pl
+++ TAO/orbsvcs/tests/HTIOP/AMI/run_test.pl
@@ -1,60 +1,89 @@
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
-    & eval 'exec perl -S $0 $argv:q'
-    if 0;
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
 
 # $Id$
 # -*- perl -*-
 
 use lib "$ENV{ACE_ROOT}/bin";
-use PerlACE::Run_Test;
-use Sys::Hostname;
+use PerlACE::TestTarget;
 
-$client_conf = PerlACE::LocalFile ("muxed$PerlACE::svcconf_ext");
-
+$status = 0;
 $debug_level = '0';
-$iterations = '1';
-$host = hostname();
+
+my $clnt_conf = "muxed$PerlACE::svcconf_ext";
 
 foreach $i (@ARGV) {
     if ($i eq '-mux') {
-        $client_conf = PerlACE::LocalFile ("muxed$PerlACE::svcconf_ext");
+#        $clnt_conf = "muxed$PerlACE::svcconf_ext";
     }
     elsif ($i eq '-debug') {
         $debug_level = '10';
     }
     elsif ($i eq '-exclusive') {
-        $client_conf = PerlACE::LocalFile ("exclusive$PerlACE::svcconf_ext");
+        $clnt_conf = "exclusive$PerlACE::svcconf_ext";
     }
 }
 
-$iorfile = PerlACE::LocalFile ("server.ior");
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
-unlink $iorfile;
+my $iterations = '1';
+my $host = $server->HostName();
 
-$SV = new PerlACE::Process ("server",
-                            "-ORBEndpoint htiop://$host: -ORBdebuglevel $debug_level -d -o $iorfile");
+my $iorbase = "server.ior";
+my $client_clnt_conf = $client->LocalFile($clnt_conf);
+my $server_iorfile = $server->LocalFile ($iorbase);
+my $client_iorfile = $client->LocalFile ($iorbase);
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
 
-$SV->Spawn ();
+$SV = $server->CreateProcess ("server", "-ORBEndpoint htiop://$host: -ORBdebuglevel $debug_level ".
+                                        "-d -o $server_iorfile");
+$CL = $client->CreateProcess ("simple_client", "-ORBsvcconf $client_clnt_conf -ORBdebuglevel $debug_level ".
+                                               "-k file://$client_iorfile -i $iterations -x -d");
 
-if (PerlACE::waitforfile_timed ($iorfile, $PerlACE::wait_interval_for_process_creation) == -1) {
-    print STDERR "ERROR: cannot find file <$iorfile>\n";
+$server_status = $SV->Spawn ();
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    exit 1;
+}
+
+if ($server->WaitForFileTimed ($iorbase,
+                               $server->ProcessStartWaitInterval()) == -1) {
+    print STDERR "ERROR: cannot find file <$server_iorfile>\n";
     $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-$CL = new PerlACE::Process ("simple_client",
-                            "-ORBsvcconf $client_conf "
-                            . "-ORBdebuglevel $debug_level"
-                            . " -k file://$iorfile "
-                            . " -i $iterations -x -d");
-
-$client = $CL->SpawnWaitKill (60);
-$server = $SV->WaitKill (5);
-
-unlink $iorfile;
-
-if ($server != 0 || $client != 0) {
+if ($server->GetFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot retrieve file <$server_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
     exit 1;
 }
 
-exit 0;
+if ($client->PutFile ($iorbase) == -1) {
+    print STDERR "ERROR: cannot set file <$client_iorfile>\n";
+    $SV->Kill (); $SV->TimedWait (1);
+    exit 1;
+}
+
+$client_status = $CL->SpawnWaitKill ($client->ProcessStartWaitInterval() + 45);
+
+if ($client_status != 0) {
+    print STDERR "ERROR: client returned $client_status\n";
+    $status = 1;
+}
+
+$server_status = $SV->WaitKill ($server->ProcessStopWaitInterval());
+
+if ($server_status != 0) {
+    print STDERR "ERROR: server returned $server_status\n";
+    $status = 1;
+}
+
+$server->DeleteFile($iorbase);
+$client->DeleteFile($iorbase);
+
+exit $status;
