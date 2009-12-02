7404b8584968da1a83ea856d9abb86b00471a4ef
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2009-12-02 13:53:36 +0000

    Wed Dec  2 13:49:57 UTC 2009  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/dds4ccm/impl/ndds/Utils.cpp:
            * connectors/dds4ccm/impl/ndds/Utils.h:
            * connectors/dds4ccm/tests/CSLSampleRejected:
            * connectors/dds4ccm/tests/CSLSampleRejected/Base:
            * connectors/dds4ccm/tests/CSLSampleRejected/Base/CSL_SRTest_Base.idl:
            * connectors/dds4ccm/tests/CSLSampleRejected/Base/CSL_SRTest_Base.mpc:
            * connectors/dds4ccm/tests/CSLSampleRejected/Connector:
            * connectors/dds4ccm/tests/CSLSampleRejected/Connector/CSL_SRTest_Connector.idl:
            * connectors/dds4ccm/tests/CSLSampleRejected/Connector/CSL_SRTest_Connector.mpc:
            * connectors/dds4ccm/tests/CSLSampleRejected/Connector/CSL_SRTest_Connector_exec.cpp:
            * connectors/dds4ccm/tests/CSLSampleRejected/Connector/CSL_SRTest_Connector_exec.h:
            * connectors/dds4ccm/tests/CSLSampleRejected/descriptors:
            * connectors/dds4ccm/tests/CSLSampleRejected/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/CSLSampleRejected/descriptors/run_test.pl:
            * connectors/dds4ccm/tests/CSLSampleRejected/descriptors/USER_QOS_PROFILES.xml:
            * connectors/dds4ccm/tests/CSLSampleRejected/Receiver:
            * connectors/dds4ccm/tests/CSLSampleRejected/Receiver/CSL_SRTest_Receiver.idl:
            * connectors/dds4ccm/tests/CSLSampleRejected/Receiver/CSL_SRTest_Receiver.mpc:
            * connectors/dds4ccm/tests/CSLSampleRejected/Receiver/CSL_SRTest_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/CSLSampleRejected/Receiver/CSL_SRTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLSampleRejected/Sender:
            * connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender.idl:
            * connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender.mpc:
            * connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender_exec.cpp:
            * connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender_exec.h:
    	Created test ConnectorStatusListener:on_sample_rejected

--- CIAO/connectors/dds4ccm/impl/ndds/Utils.h
+++ CIAO/connectors/dds4ccm/impl/ndds/Utils.h
@@ -10,18 +10,63 @@
 #ifndef NDDS_UTILS_H
 #define NDDS_UTILS_H
 
-#include "ndds/ndds_cpp.h"
 #include "dds4ccm_ndds_impl_export.h"
+#include "dds4ccm/idl/dds_rtf2_dcpsC.h"
 
 namespace CIAO
 {
   namespace DDS4CCM
   {
-    namespace RTI
+    inline const char * translate_retcode (::DDS::ReturnCode_t ret)
     {
-      DDS4CCM_NDDS_IMPL_Export const char * translate_retcode (DDS_ReturnCode_t ret);
+#define RTI_DDS_RETCODE(X) case X: return #X
+        switch (ret)
+          {
+            RTI_DDS_RETCODE (DDS::RETCODE_OK);
+            RTI_DDS_RETCODE (DDS::RETCODE_ERROR);
+            RTI_DDS_RETCODE (DDS::RETCODE_UNSUPPORTED);
+            RTI_DDS_RETCODE (DDS::RETCODE_BAD_PARAMETER);
+            RTI_DDS_RETCODE (DDS::RETCODE_PRECONDITION_NOT_MET);
+            RTI_DDS_RETCODE (DDS::RETCODE_OUT_OF_RESOURCES);
+            RTI_DDS_RETCODE (DDS::RETCODE_NOT_ENABLED);
+            RTI_DDS_RETCODE (DDS::RETCODE_IMMUTABLE_POLICY);
+            RTI_DDS_RETCODE (DDS::RETCODE_INCONSISTENT_POLICY);
+            RTI_DDS_RETCODE (DDS::RETCODE_ALREADY_DELETED);
+            RTI_DDS_RETCODE (DDS::RETCODE_TIMEOUT);
+            RTI_DDS_RETCODE (DDS::RETCODE_NO_DATA);
+            RTI_DDS_RETCODE (DDS::RETCODE_ILLEGAL_OPERATION);
+          }
+        return "***Unknown enum value, update RTI::translate_retcode()";
+#undef RTI_DDS_RETCODE
+    }
 
-      DDS4CCM_NDDS_IMPL_Export const char * translate_statuskind (DDS_StatusKind ret);
+    inline const char * translate_statuskind (DDS::StatusKind ret)
+      {
+#define RTI_DDS_RETCODE(X) case X: return #X
+        switch (ret)
+          {
+            RTI_DDS_RETCODE (DDS::INCONSISTENT_TOPIC_STATUS);
+            RTI_DDS_RETCODE (DDS::OFFERED_DEADLINE_MISSED_STATUS);
+            RTI_DDS_RETCODE (DDS::REQUESTED_DEADLINE_MISSED_STATUS);
+            RTI_DDS_RETCODE (DDS::OFFERED_INCOMPATIBLE_QOS_STATUS);
+            RTI_DDS_RETCODE (DDS::REQUESTED_INCOMPATIBLE_QOS_STATUS);
+            RTI_DDS_RETCODE (DDS::SAMPLE_LOST_STATUS);
+            RTI_DDS_RETCODE (DDS::SAMPLE_REJECTED_STATUS);
+            RTI_DDS_RETCODE (DDS::DATA_ON_READERS_STATUS);
+            RTI_DDS_RETCODE (DDS::DATA_AVAILABLE_STATUS);
+            RTI_DDS_RETCODE (DDS::LIVELINESS_LOST_STATUS);
+            RTI_DDS_RETCODE (DDS::LIVELINESS_CHANGED_STATUS);
+            RTI_DDS_RETCODE (DDS::PUBLICATION_MATCHED_STATUS);
+            RTI_DDS_RETCODE (DDS::SUBSCRIPTION_MATCHED_STATUS);
+            //RTI_DDS_RETCODE (DDS::RELIABLE_WRITER_CACHE_CHANGED_STATUS);
+            //RTI_DDS_RETCODE (DDS::RELIABLE_READER_ACTIVITY_CHANGED_STATUS);
+            //RTI_DDS_RETCODE (DDS::DATA_WRITER_CACHE_STATUS);
+            //RTI_DDS_RETCODE (DDS::DATA_WRITER_PROTOCOL_STATUS);
+            //RTI_DDS_RETCODE (DDS::DATA_READER_CACHE_STATUS);
+            //RTI_DDS_RETCODE (DDS::DATA_READER_PROTOCOL_STATUS);
+          }
+        return "***Unknown enum value, update RTI::translate_statuskind()";
+#undef RTI_DDS_RETCODE
     }
   }
 }
---
+++ CIAO/connectors/dds4ccm/tests/CSLSampleRejected/Connector/CSL_SRTest_Connector_exec.cpp
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+// $Id$
+
+#include "CSL_SRTest_Connector_exec.h"
+
+namespace CIAO_CSL_SRTest_CSL_SRTest_Connector_Impl
+{
+  CSL_SRTest_Connector_exec_i::CSL_SRTest_Connector_exec_i (const char * /*topic_name*/)
+    : DDS_Event_Connector_T<TestTopic_DDS_Traits, TestTopic_Connector_Traits> ()
+  {
+  }
+
+  CSL_SRTest_Connector_exec_i::~CSL_SRTest_Connector_exec_i (void)
+  {
+  }
+
+  extern "C" CSL_SRTEST_CONNECTOR_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_CSL_SRTest_CSL_SRTest_Connector_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_NORETURN (
+      retval,
+      CSL_SRTest_Connector_exec_i ("Square")); //should be set by dep. plan.
+
+    return retval;
+  }
+}
+
