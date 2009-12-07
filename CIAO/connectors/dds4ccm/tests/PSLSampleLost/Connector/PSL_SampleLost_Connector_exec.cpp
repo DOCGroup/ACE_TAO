001a98555345d2951ca5baa49727e5dfc6ba7bfc
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2009-12-07 15:40:08 +0000

    Mon Dec  7 15:37:57 UTC 2009  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * bin/ciao_tests.lst
            * connectors/dds4ccm/tests/PSLSampleLost:
            * connectors/dds4ccm/tests/PSLSampleLost/Base/PSL_SampleLost_Base.idl:
            * connectors/dds4ccm/tests/PSLSampleLost/Base/PSL_SampleLost_Base.mpc:
            * connectors/dds4ccm/tests/PSLSampleLost/Connector:
            * connectors/dds4ccm/tests/PSLSampleLost/Connector/PSL_SampleLost_Connector.idl:
            * connectors/dds4ccm/tests/PSLSampleLost/Connector/PSL_SampleLost_Connector_exec.cpp:
            * connectors/dds4ccm/tests/PSLSampleLost/Connector/PSL_SampleLost_Connector_exec.h:
            * connectors/dds4ccm/tests/PSLSampleLost/descriptors:
            * connectors/dds4ccm/tests/PSLSampleLost/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/PSLSampleLost/descriptors/run_test.pl:
            * connectors/dds4ccm/tests/PSLSampleLost/descriptors/USER_QOS_PROFILES.xml:
            * connectors/dds4ccm/tests/PSLSampleLost/Receiver:
            * connectors/dds4ccm/tests/PSLSampleLost/Receiver/PSL_SampleLost_Receiver.idl:
            * connectors/dds4ccm/tests/PSLSampleLost/Receiver/PSL_SampleLost_Receiver.mpc:
            * connectors/dds4ccm/tests/PSLSampleLost/Receiver/PSL_SampleLost_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/PSLSampleLost/Receiver/PSL_SampleLost_Receiver_exec.h:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender/PSL_SampleLost_Sender.idl:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender/PSL_SampleLost_Sender.mpc:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender/PSL_SampleLost_Sender_exec.cpp:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender/PSL_SampleLost_Sender_exec.h:
              Add test PortStatusListener:on_sample_lost

---
+++ CIAO/connectors/dds4ccm/tests/PSLSampleLost/Connector/PSL_SampleLost_Connector_exec.cpp
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+// $Id$
+
+#include "PSL_SampleLost_Connector_exec.h"
+
+namespace CIAO_PSL_SampleLost_PSL_SampleLost_Connector_Impl
+{
+  PSL_SampleLost_Connector_exec_i::PSL_SampleLost_Connector_exec_i (const char * /*topic_name*/)
+    : DDS_Event_Connector_T<TestTopic_DDS_Traits, TestTopic_Connector_Traits> ()
+  {
+  }
+
+  PSL_SampleLost_Connector_exec_i::~PSL_SampleLost_Connector_exec_i (void)
+  {
+  }
+
+  extern "C" PSL_SAMPLELOST_CONNECTOR_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_PSL_SampleLost_PSL_SampleLost_Connector_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_NORETURN (
+      retval,
+      PSL_SampleLost_Connector_exec_i ("Square")); //should be set by dep. plan.
+
+    return retval;
+  }
+}
+
