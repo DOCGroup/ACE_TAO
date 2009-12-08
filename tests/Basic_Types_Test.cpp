23b81e6c850ada0436e0a88b2f254575d85c2857
Steve Huston <shuston@riverace.com>
2009-12-08 19:24:35 +0000

    Fix tabs (fuzz error)

--- tests/Basic_Types_Test.cpp
+++ tests/Basic_Types_Test.cpp
@@ -295,14 +295,14 @@ run_main (int, ACE_TCHAR *[])
   ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("little endian\n")));
   if (ACE_HTONS (test_val) == test_val)
     ACE_ERROR ((LM_ERROR,
-		ACE_TEXT ("Endian test: %d == %d but should be different\n"),
-		ACE_HTONS (test_val), test_val));
+                ACE_TEXT ("Endian test: %d == %d but should be different\n"),
+                ACE_HTONS (test_val), test_val));
 #elif defined (ACE_BIG_ENDIAN)
   ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("big endian\n")));
   if (ACE_HTONS (test_val) != test_val)
     ACE_ERROR ((LM_ERROR,
-		ACE_TEXT ("Endian test: %d != %d but should be equal\n"),
-		ACE_HTONS (test_val), test_val));
+                ACE_TEXT ("Endian test: %d != %d but should be equal\n"),
+                ACE_HTONS (test_val), test_val));
 #else
   ACE_ERROR ((LM_ERROR,
               ACE_TEXT ("assertion failed: no ACE_*_ENDIAN definition!\n")));
