7b6fb87173badc8200135f0b2c70db7bbfc273fc
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-10 12:42:52 +0000

    Thu Dec 10 12:42:00 UTC 2009  Martin Corino  <mcorino@remedy.nl>
            * tests/BiDirectional_DelayedUpcall/client.cpp:
            * tests/BiDirectional_DelayedUpcall/test_i.cpp:
            * tests/BiDirectional_DelayedUpcall/test_i.inl:
            * tests/BiDirectional_DelayedUpcall/test_i.h:
              Fixed problem with illegal send of request after ORB shutdown.
              See Bugzilla 2068.

--- trunk/TAO/tests/BiDirectional_DelayedUpcall/test_i.h
+++ trunk/TAO/tests/BiDirectional_DelayedUpcall/test_i.h
@@ -29,7 +29,7 @@ class Callback_i : public virtual POA_Callback
   //   connection that was established by the client
   //
 public:
-  Callback_i (CORBA::ORB_ptr orb);
+  Callback_i (CORBA::ORB_ptr orb, Simple_Server_ptr srv);
   // ctor
 
   void shutdown (void);
@@ -41,6 +41,8 @@ public:
 private:
   CORBA::ORB_var orb_;
   // The orb
+  Simple_Server_var server_;
+  // The server
 };
 
 class Simple_Server_i : public virtual POA_Simple_Server, public virtual ACE_Event_Handler
