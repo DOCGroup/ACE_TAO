48c03b83fc9e987963185cad4bfcc6478c7cd9fa
msmit <msmit@remedy.nl>
2009-12-11 11:05:41 +0000

    Fri Dec 11 11:07:13 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * performance-tests/Benchmark/RoundTripClient/run_test.pl:
              Resolved fuzz errors/warnings
    
            * performance-tests/Protocols/descriptors/run_test.pl:
              Resolved fuzz warnings

--- CIAO/performance-tests/Benchmark/RoundTripClient/run_test.pl
+++ CIAO/performance-tests/Benchmark/RoundTripClient/run_test.pl
@@ -10,18 +10,13 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 #
 
 use lib "$ENV{'ACE_ROOT'}/bin";
-use PerlACE::Run_Test;
+use PerlACE::TestTarget;
 
 $status = 0;
 $daemon_ior = PerlACE::LocalFile ("daemon.ior");
 $am_ior = PerlACE::LocalFile ("am.ior");
 
-$ACE_ROOT=$ENV{'ACE_ROOT'};
 $CIAO_ROOT=$ENV{'CIAO_ROOT'};
-
-if ($CIAO_ROOT eq "") {
-    $CIAO_ROOT="$ACE_ROOT/TAO/CIAO";
-}
 
 unlink $daemon_ior;
 unlink $am_ior;
@@ -38,66 +33,58 @@ $assembly_manager_args = "-o $am_ior -c test.dat";
 $ad_args = " -k file://$am_ior -a RoundTripClient.cad";
 
 # CIAO daemon process definition
-$DS = new PerlACE::Process ("$CIAO_ROOT/tools/Daemon/CIAO_Daemon",
-                            "$daemon_args1");
+$DS = $tg->CreateProcess ("$CIAO_ROOT/tools/Daemon/CIAO_Daemon",
+                            "$daemon_args");
 
 ## Starting up the CIAO daemon
 $DS->Spawn ();
-if (PerlACE::waitforfile_timed ($daemon_ior, $PerlACE::wait_interval_for_process_creation) == -1) {
+if ($tg->WaitForFileTimed($daemon_base,
+                          $tg->ProcessStartWaitInterval ()) == -1) {
     print STDERR "ERROR: Could not find daemon ior file <$daemon_ior>\n";
     $DS->Kill ();
     exit 1;
 }
 
 # CIAO daemon process definition
-$DS2 = new PerlACE::Process ("$CIAO_ROOT/tools/Daemon/CIAO_Daemon",
+$DS2 = $tg->CreateProcess ("$CIAO_ROOT/tools/Daemon/CIAO_Daemon",
                             "$daemon_args2");
 
 ## Starting up the CIAO daemon
 $DS2->Spawn ();
-if (PerlACE::waitforfile_timed ($daemon_ior, $PerlACE::wait_interval_for_process_creation) == -1) {
+if ($tg->WaitForFileTimed($daemon_base,
+                          $tg->ProcessStartWaitInterval ()) == -1) {
     print STDERR "ERROR: Could not find daemon ior file <$daemon_ior>\n";
     $DS->Kill ();
     exit 1;
 }
 
-$AM = new PerlACE::Process("$CIAO_ROOT/tools/Assembly_Deployer/Assembly_Manager",
+$AM = $tg->CreateProcess  ("$CIAO_ROOT/tools/Assembly_Deployer/Assembly_Manager",
                            $assembly_manager_args);
 $AM->Spawn ();
-if (PerlACE::waitforfile_timed ($am_ior, $PerlACE::wait_interval_for_process_creation) == -1) {
+if ($tg->WaitForFileTimed($am_base,
+                          $tg->ProcessStartWaitInterval ()) == -1) {
     print STDERR "ERROR: Could not find assembly ior file <$am_ior>\n";
     $AM->Kill ();
     exit 1;
 }
 
-$AD = new PerlACE::Process("$CIAO_ROOT/tools/Assembly_Deployer/Assembly_Deployer",
+$AD = $tg->CreateProcess  ("$CIAO_ROOT/tools/Assembly_Deployer/Assembly_Deployer",
                            $ad_args);
 $AD->Spawn ();
 
 sleep (5);
 
 #Start the client to send the trigger message
-$CL = new PerlACE::Process ("../RoundTripClient/client", "");
-$CL->SpawnWaitKill(60);
-
-#$ctrl = $DS->WaitKill (5);
-#$AM->WaitKill(5);
-#$AD->WaitKill(5);
+$CL = $tg->CreateProcess ("../RoundTripClient/client", "");
+$CL->SpawnWaitKill($tg->ProcessStopWaitInterval ());
 
 $AM->Kill ();
 $AD->Kill ();
 $DS->Kill ();
-$DS2->Kill();
-
-#if ($ctrl != 0) {
-#    print STDERR "ERROR: CIAODaemon didn't shutdown gracefully $ctrl\n";
-#    $DS->Kill ();
-#    exit 1;
-#}
-
 $CL->Kill ();
+$DS2->Kill ();
 
-unlink $daemon_ior;
-unlink $am_ior;
+$tg->DeleteFile ($daemon_base);
+$tg->DeleteFile ($am_base);
 
 exit $status;
