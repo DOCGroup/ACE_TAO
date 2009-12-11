54b0cb752d586a349f81e4720ef9218834bd1055
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-11 14:02:01 +0000

    Fri Dec 11 14:03:46 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * examples/Display/descriptors/run_NodeDaemons.pl:
            * examples/Display/descriptors/run_test_without_ns.pl:
              Resolved fuzz errors/warnings/

--- trunk/CIAO/examples/Display/descriptors/run_NodeDaemons.pl
+++ trunk/CIAO/examples/Display/descriptors/run_NodeDaemons.pl
@@ -10,21 +10,10 @@ use PerlACE::TestTarget;
 
 $tg = PerlACE::TestTarget::create_target (1) || die "Create target for ns failed\n";
 
-$status = 0;
 $iorbase1 = "NodeApp1.ior";
 $iorfile1 = $tg->LocalFile ($iorbase1);
 $iorbase2 = "NodeApp2.ior";
 $iorfile2 = $tg->LocalFile ($iorbase2);
-
-#for ($iter = 0; $iter <= $#ARGV; $iter++) {
-#    if ($ARGV[$iter] eq "-h" || $ARGV[$iter] eq "-?") {
-#      print "Run_Test Perl script for NodeApplicationTest \n\n";
-#      print "run_test \n";
-#      print "\n";
-#      print "-h                  -- prints this information\n";
-#      exit 0;
-#  }
-#}
 
 $iorfile1 = $tg->DeleteFile ($iorbase1);
 $iorfile2 = $tg->DeleteFile ($iorbase2);
