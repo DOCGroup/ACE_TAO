93f052f52e217ed5c0a1c3d741965ade569dd854
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-14 13:14:47 +0000

    Mon Dec 14 13:16:16 UTC 2009  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/ndds/DataReaderStateListener_T.cpp:
              The deleted samples may be invalid. Therefore don't check
              on valid_data flag. Implemented this for many_by_many.

--- trunk/CIAO/connectors/dds4ccm/impl/ndds/DataReaderStateListener_T.cpp
+++ trunk/CIAO/connectors/dds4ccm/impl/ndds/DataReaderStateListener_T.cpp
@@ -122,52 +122,49 @@ CIAO::DDS4CCM::RTI::DataReaderStateListener_T<DDS_TYPE, CCM_TYPE>::on_data_avail
 
           for (::DDS_Long i = 0 ; i < sample_info.length(); i++)
             {
-              if (sample_info[i].valid_data)
+              if ((sample_info[i].valid_data &&
+                   sample_info[i].view_state == ::DDS_NEW_VIEW_STATE) ||
+                  sample_info[i].instance_state == ::DDS_NOT_ALIVE_DISPOSED_INSTANCE_STATE)
                 {
-                  // first gather updates until we find a new or
-                  // deleted sample.
-                  if (sample_info[i].view_state != ::DDS_NEW_VIEW_STATE &&
-                      sample_info[i].instance_state != ::DDS_NOT_ALIVE_DISPOSED_INSTANCE_STATE)
-                    {
-                      ++nr_of_updates;
-                      updates.push_back (i);
-                    }
-                  else
-                    {
-                      // new or delete found -> first send out the updated
-                      // samples in one go
-                      typename CCM_TYPE::seq_type::_var_type  inst_seq = new typename CCM_TYPE::seq_type;
-                      ::CCM_DDS::ReadInfoSeq_var infoseq = new ::CCM_DDS::ReadInfoSeq;
+                  // new or delete found -> first send out the updated
+                  // samples in one go
+                  typename CCM_TYPE::seq_type::_var_type  inst_seq = new typename CCM_TYPE::seq_type;
+                  ::CCM_DDS::ReadInfoSeq_var infoseq = new ::CCM_DDS::ReadInfoSeq;
 
-                      infoseq->length (nr_of_updates);
-                      inst_seq->length (nr_of_updates);
-                      CORBA::ULong ix = 0;
-                      for(Updates::iterator iter = updates.begin();
-                          iter != updates.end();
-                          ++iter)
-                        {
-                          infoseq[ix] <<= sample_info[*iter];
-                          inst_seq[ix] = data[*iter];
-                          ++ix;
-                        }
-                      listener_->on_many_updates (inst_seq, infoseq);
-                      // now invoke on_creation or on_deletion
-                      if (sample_info[i].view_state == ::DDS_NEW_VIEW_STATE)
-                        {
-                          ::CCM_DDS::ReadInfo readinfo;
-                          readinfo <<= sample_info[i];
-                          listener_->on_creation (data[i], readinfo);
-                        }
-                      else if (sample_info[i].instance_state != ::DDS_NOT_ALIVE_DISPOSED_INSTANCE_STATE)
-                        {
-                          ::CCM_DDS::ReadInfo readinfo;
-                          readinfo <<= sample_info[i];
-                          listener_->on_deletion (data[i], readinfo);
-                        }
-                      //clean up
-                      updates.clear ();
-                      nr_of_updates = 0;
+                  infoseq->length (nr_of_updates);
+                  inst_seq->length (nr_of_updates);
+                  CORBA::ULong ix = 0;
+                  for(Updates::iterator iter = updates.begin();
+                      iter != updates.end();
+                      ++iter)
+                    {
+                      infoseq[ix] <<= sample_info[*iter];
+                      inst_seq[ix] = data[*iter];
+                      ++ix;
                     }
+                  listener_->on_many_updates (inst_seq, infoseq);
+                  // now invoke on_creation or on_deletion
+                  if (sample_info[i].valid_data &&
+                      sample_info[i].view_state == ::DDS_NEW_VIEW_STATE)
+                    {
+                      ::CCM_DDS::ReadInfo readinfo;
+                      readinfo <<= sample_info[i];
+                      listener_->on_creation (data[i], readinfo);
+                    }
+                  else if (sample_info[i].instance_state == ::DDS_NOT_ALIVE_DISPOSED_INSTANCE_STATE)
+                    {
+                      ::CCM_DDS::ReadInfo readinfo;
+                      readinfo <<= sample_info[i];
+                      listener_->on_deletion (data[i], readinfo);
+                    }
+                  //clean up
+                  updates.clear ();
+                  nr_of_updates = 0;
+                }
+              else if (sample_info[i].valid_data)
+                {
+                  ++nr_of_updates;
+                  updates.push_back (i);
                 }
             }
           //send the latest updates.
