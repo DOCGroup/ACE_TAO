04ce5147d658e9a4d99cc37187c95ea073b2b238
mhengstmengel <mhengstmengel@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-15 14:33:21 +0000

    Tue Dec 15 14:32:46 UTC 2009  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector_exec.cpp:
            * connectors/dds4ccm/tests/CSLQoS/Connector/CSL_QoSTest_Connector_exec.h:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.cpp:
            * connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.h:
              Cleanup test.

--- trunk/CIAO/connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.cpp
+++ trunk/CIAO/connectors/dds4ccm/tests/CSLQoS/Sender/CSL_QoSTest_Sender_exec.cpp
@@ -1,6 +1,8 @@
 // -*- C++ -*-
 // $Id$
 
+//Test for ConnectorStatusListener: on_offered_incompatible_qos
+
 #include "CSL_QoSTest_Sender_exec.h"
 #include "ace/Guard_T.h"
 #include "ciao/Logger/Log_Macros.h"
@@ -21,7 +23,6 @@ namespace CIAO_CSL_QoSTest_Sender_Impl
 
   ConnectorStatusListener_exec_i::~ConnectorStatusListener_exec_i (void)
   {
-
   }
 
   // Operations from ::CCM_DDS::ConnectorStatusListener
@@ -29,34 +30,35 @@ namespace CIAO_CSL_QoSTest_Sender_Impl
     ::DDS::Topic_ptr /*the_topic*/,
      const DDS::InconsistentTopicStatus & /*status*/)
     {
-       //printf("Sender : ConnectorStatusListener_exec_i::on_inconsistent_topic\n");
     }
+
   void ConnectorStatusListener_exec_i::on_requested_incompatible_qos(
     ::DDS::DataReader_ptr /*the_reader*/,
      const DDS::RequestedIncompatibleQosStatus & /*status*/)  {
-       //printf("Sender:ConnectorStatusListener_exec_i::on_requested_incompatible_qos, status = %dl\n", status);
     }
+
   void ConnectorStatusListener_exec_i::on_sample_rejected(
      ::DDS::DataReader_ptr /*the_reader*/,
      const DDS::SampleRejectedStatus & /*status*/)  {
-      //printf("ConnectorStatusListener_exec_i::on_sample_rejected\n");
     }
+
   void ConnectorStatusListener_exec_i::on_offered_deadline_missed(
      ::DDS::DataWriter_ptr /*the_writer*/,
      const DDS::OfferedDeadlineMissedStatus & /*status*/)  {
-     //printf("ConnectorStatusListener_exec_i::on_offered_deadline_missed\n");
     }
+
   void ConnectorStatusListener_exec_i::on_offered_incompatible_qos(
-     ::DDS::DataWriter_ptr /*the_writer*/,
+     ::DDS::DataWriter_ptr the_writer,
      const DDS::OfferedIncompatibleQosStatus & /*status*/)  {
-//     printf("Sender:ConnectorStatusListener_exec_i::on_offered_incompatible_qos status = %dl\n", status);
-       this->incompatible_ = true;
+       if(!CORBA::is_nil(the_writer))
+       {
+         this->incompatible_ = true;
+       }
     }
+
   void ConnectorStatusListener_exec_i::on_unexpected_status(
     ::DDS::Entity_ptr /*the_entity*/,
     ::DDS::StatusKind  /*status_kind*/)  {
-    //  CORBA::ULong kind = status_kind;
-    //  printf("Sender :ConnectorStatusListener_exec_i::on_unexpected_status #### status_kind = %d \n", kind);
     }
   //============================================================
   // Component Executor Implementation Class: Sender_exec_i
@@ -74,7 +76,6 @@ namespace CIAO_CSL_QoSTest_Sender_Impl
   ::CCM_DDS::CCM_ConnectorStatusListener_ptr
   Sender_exec_i::get_test_topic_connector_status (void)
   {
-   // printf ("*************** out connector status sender ************************\n");
     return new ConnectorStatusListener_exec_i (this->incompatible_);
   }
 
@@ -95,7 +96,6 @@ namespace CIAO_CSL_QoSTest_Sender_Impl
   void
   Sender_exec_i::configuration_complete (void)
   {
-    //printf("-------------configuration_complete ----------------\n");
   }
 
   void
@@ -111,18 +111,16 @@ namespace CIAO_CSL_QoSTest_Sender_Impl
   void
   Sender_exec_i::ccm_remove (void)
   {
-    //printf("*************in remove Sender********** \n");
-
     if(!this->incompatible_.value ())
       {
-        CIAO_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: did not receive the expected ")
+        ACE_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: did not receive the expected ")
                                ACE_TEXT ("error 'on_offered_incompatible_Qos' in Sender\n")
                     ));
       }
 
     else
       {
-        CIAO_DEBUG ((LM_DEBUG, ACE_TEXT ("Received the expected ")
+        ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Received the expected ")
                                ACE_TEXT ("'on_offered_incompatible_qos' in Sender\n")
                     ));
       }
@@ -142,4 +140,3 @@ namespace CIAO_CSL_QoSTest_Sender_Impl
     return retval;
   }
 }
-
