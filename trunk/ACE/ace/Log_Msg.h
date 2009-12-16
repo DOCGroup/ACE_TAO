4c5d7fcfe57cc2779e950b05a6fe6f325e3b16fa
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-16 11:24:17 +0000

    Wed Dec 16 11:24:00 UTC 2009  Martin Corino  <mcorino@remedy.nl>
            * ACE_wrappers/ace/Log_Msg.h:
              Updated doxygen docu regarding additions to %D and %T
              options.

--- trunk/ACE/ace/Log_Msg.h
+++ trunk/ACE/ace/Log_Msg.h
@@ -528,8 +528,13 @@ public:
    *  - 'S': print out the appropriate signal message corresponding
    *         to var-argument, e.g., as done by strsignal()
    *  - 's': prints a ACE_TCHAR* character string (also see C and W)
-   *  - 'T': print timestamp in hour:minute:sec:usec format.
+   *  - 'T': print timestamp in hour:minute:sec:usec format (plain option, 
+   *         i.e. without any flags, prints system supplied timestamp; 
+   *         with '#' flag added expects ACE_Time_Value* in argument list)
    *  - 'D': print timestamp as Weekday Month day year hour:minute:sec.usec
+   *         (plain option, i.e. without any flags, prints system supplied 
+   *         timestamp; with '#' flag added expects ACE_Time_Value* in 
+   *         argument list)
    *  - 't': print thread id (1 if single-threaded)
    *  - 'u': print as unsigned int
    *  - 'w': prints a wide character
