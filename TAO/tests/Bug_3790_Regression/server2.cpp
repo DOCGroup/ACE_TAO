6ed74662b908b879014bee84c266d4b02d01acd2
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-18 09:25:03 +0000

    Fri Dec 18 09:23:37 UTC 2009  Vladimir Zykov  <vladimir.zykov@prismtech.com>
            * tests/Bug_3790_Regression/server2.cpp:
              Simplified the test.

--- TAO/tests/Bug_3790_Regression/server2.cpp
+++ TAO/tests/Bug_3790_Regression/server2.cpp
@@ -44,15 +44,11 @@ parse_args (int argc, ACE_TCHAR *argv[])
 
 //*************************************************************************
 class Task
-  : public virtual ACE_Task_Base,
-    public virtual IORTable::Locator,
+  : public virtual IORTable::Locator,
     public virtual CORBA::LocalObject
 {
 public:
-  Task (ACE_Thread_Manager &thread_manager,
-        CORBA::ORB_ptr orb);
-
-  int svc (void);
+  Task (CORBA::ORB_ptr orb);
 
   char * locate (const char *);
 
@@ -73,20 +69,24 @@ Task::locate (const char *)
   return fwdString._retn();
 }
 
-Task::Task (ACE_Thread_Manager &thread_manager,
-            CORBA::ORB_ptr orb)
-  : ACE_Task_Base (&thread_manager),
-    orb_ (CORBA::ORB::_duplicate (orb))
+Task::Task (CORBA::ORB_ptr orb)
+  : orb_ (CORBA::ORB::_duplicate (orb))
 {
 }
 
 int
-Task::svc (void)
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
   try
     {
+      CORBA::ORB_var orb =
+        CORBA::ORB_init (argc, argv, "MY_TEST_ORB");
+
+      if (parse_args (argc, argv) != 0)
+        return 1;
+
       CORBA::Object_var object =
-        this->orb_->resolve_initial_references("RootPOA");
+        orb->resolve_initial_references("RootPOA");
 
       PortableServer::POA_var root_poa =
         PortableServer::POA::_narrow (object.in ());
@@ -105,14 +105,21 @@ Task::svc (void)
       // Run ORB Event loop.
       poa_manager->activate ();
 
+      // Create task.
+      Task *task = 0;
+      ACE_NEW_RETURN (task,
+                      Task (orb.in ()),
+                      1);
+      IORTable::Locator_var owner_transfer (task);
+
       // Resolve the IORTable and set the locator
       CORBA::Object_var iorTableObj =
-        orb_->resolve_initial_references("IORTable");
+        orb->resolve_initial_references("IORTable");
 
       IORTable::Table_var local_ior_table =
         IORTable::Table::_narrow (iorTableObj.in ());
 
-      local_ior_table->set_locator (this);
+      local_ior_table->set_locator (task);
 
       try
         {
@@ -120,7 +127,7 @@ Task::svc (void)
                       "OK, now try to resolve a co-located "
                       "IORTable entry...\n"));
 
-          CORBA::Object_var clientObj = orb_->string_to_object (testIOR);
+          CORBA::Object_var clientObj = orb->string_to_object (testIOR);
 
           simple::SimpleTest_T_var simpleTestClient =
             simple::SimpleTest_T::_narrow (clientObj.in ());
@@ -137,83 +144,21 @@ Task::svc (void)
 
           simpleTestClient->shutdown ();
         }
-      catch(CORBA::OBJ_ADAPTER &objAdapter)
+      catch(const CORBA::OBJ_ADAPTER &objAdapter)
         {
           objAdapter._tao_print_exception ("CAUGHT OBJ_ADAPTER!!!");
         }
-      catch(CORBA::OBJECT_NOT_EXIST &objNE)
+      catch(const CORBA::OBJECT_NOT_EXIST &objNE)
         {
           objNE._tao_print_exception ("CAUGHT OBJECT NOT EXIST!!!");
         }
-      catch(CORBA::Exception &ex)
-        {
-          ex._tao_print_exception ("CAUGHT EXCEPTION: ");
-        }
-      catch(...)
-        {
-          ACE_ERROR ((LM_ERROR,
-                      "CAUGHT UNEXPECTED EXCEPTION!\n"));
-        }
 
       ACE_DEBUG ((LM_DEBUG,
-                  "Leaving task!\n"));
+                  "Leaving!\n"));
     }
-  catch (...)
+  catch (const CORBA::Exception &ex)
     {
-      ACE_ERROR ((LM_ERROR,
-                  "Caught unexpected exception!\n"));
-      return -1;
-    }
-
-  return 0;
-}
-
-int
-ACE_TMAIN(int argc, ACE_TCHAR *argv[])
-{
-  try
-    {
-      CORBA::ORB_var orb =
-        CORBA::ORB_init (argc, argv, "MY_TEST_ORB");
-
-      if (parse_args (argc, argv) != 0)
-        return 1;
-
-      // Thread Manager for managing task.
-      ACE_Thread_Manager thread_manager;
-
-      // Create task.
-      Task *task = 0;
-      ACE_NEW_RETURN (task,
-                      Task (thread_manager, orb.in ()),
-                      1);
-      IORTable::Locator_var owner_transfer (task);
-
-      // Task activation flags.
-      long flags =
-        THR_NEW_LWP |
-        THR_JOINABLE |
-        orb->orb_core ()->orb_params ()->thread_creation_flags ();
-
-      // Activate task.
-      int result =
-        task->activate (flags);
-
-      if (result == -1)
-        {
-          ACE_DEBUG ((LM_DEBUG,
-                      "Cannot create thread with scheduling policy.  "
-                      "Check svc.conf options.\n"));
-
-          return -1;
-        }
-
-      thread_manager.wait ();
-    }
-  catch (...)
-    {
-      ACE_ERROR ((LM_ERROR,
-                  "Caught unexpected exception in main!\n"));
+      ex._tao_print_exception ("CAUGHT EXCEPTION: ");
       return -1;
     }
 
