f8e739753a6aa45b82c63a91fc0b1f066dae1daa
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2009-12-23 09:13:09 +0000

    Wed Dec 23 09:13:42 UTC 2009  Johnny Willemsen  <jwillemsen {at} remedy.nl>
            * tests/Bug_3801_Regression:
              New test to reproduce problem with AMI and attributes
    
            * bin/tao_orb_tests.lst:
              Added 3801
    
            * bin/tao_other_tests.lst:
              Removed WinCE
    
            * tao/Messaging/ExceptionHolder_i.cpp:
              bool changes
    
    `       * tao/Object.cpp:
              bool changes, check all return values
    
            * tao/Valuetype/ValueBase.cpp:
              bool changes, fixed a lot of debug messages that had a totally
              wrong layout

--- trunk/TAO/tao/Object.cpp
+++ trunk/TAO/tao/Object.cpp
@@ -827,7 +827,9 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
     {
       if (orb_core->resource_factory ()->resource_usage_strategy () ==
           TAO_Resource_Factory::TAO_LAZY)
-        lazy_strategy = true;
+        {
+          lazy_strategy = true;
+        }
     }
 
   if (!lazy_strategy)
@@ -835,11 +837,11 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
       // If the user has set up a eager strategy..
       CORBA::String_var type_hint;
 
-      if ((cdr >> type_hint.inout ()) == 0)
+      if (!(cdr >> type_hint.inout ()))
         return false;
 
       CORBA::ULong profile_count;
-      if ((cdr >> profile_count) == 0)
+      if (!(cdr >> profile_count))
         return false;
 
       if (profile_count == 0)
@@ -878,7 +880,9 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
             {
               TAO_Profile *pfile = connector_registry->create_profile (cdr);
               if (pfile != 0)
-                mp.give_profile (pfile);
+                {
+                  mp.give_profile (pfile);
+                }
             }
 
           // Make sure we got some profiles!
@@ -887,9 +891,9 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
               // @@ This occurs when profile creation fails when decoding the
               //    profile from the IOR.
               ACE_ERROR_RETURN ((LM_ERROR,
-                                 ACE_TEXT ("TAO (%P|%t) ERROR: Could not create all ")
+                                 ACE_TEXT ("TAO (%P|%t) - ERROR: Could not create all ")
                                  ACE_TEXT ("profiles while extracting object [%d, %d]\n")
-                                 ACE_TEXT ("TAO (%P|%t) ERROR: reference from the ")
+                                 ACE_TEXT ("TAO (%P|%t) - ERROR: reference from the ")
                                  ACE_TEXT ("CDR stream.\n"),
                                  mp.profile_count (), profile_count),
                                 false);
@@ -901,7 +905,7 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
         {
           if (TAO_debug_level > 0)
             ex._tao_print_exception (
-              ACE_TEXT ("TAO - ERROR creating stub ")
+              ACE_TEXT ("TAO (%P|%t) - ERROR creating stub ")
               ACE_TEXT ("object when demarshaling object ")
               ACE_TEXT ("reference.\n"));
 
@@ -912,7 +916,9 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
 
       x = orb_core->create_object (safe_objdata.get ());
       if (!x)
-        return false;
+        {
+          return false;
+        }
 
       // Transfer ownership to the CORBA::Object
       (void) safe_objdata.release ();
@@ -924,12 +930,16 @@ operator>> (TAO_InputCDR& cdr, CORBA::Object*& x)
 
       ACE_NEW_RETURN (ior,
                       IOP::IOR (),
-                      0);
+                      false);
 
-      cdr >> *ior;
+      if (!(cdr >> *ior))
+        {
+          return false;
+        }
+        
       ACE_NEW_RETURN (x,
                       CORBA::Object (ior, orb_core),
-                      0);
+                      false);
     }
 
   return (CORBA::Boolean) cdr.good_bit ();
