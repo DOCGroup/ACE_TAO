f94e9fe4ac7c6a4e125aff40dd00b213cb21d81f
Johnny Willemsen <jwillemsen@remedy.nl>
2010-01-05 14:53:15 +0000

    branching/tagging

---
+++ modules/CIAO/examples/DevGuideExamples/Messenger/Administrator_Client_IDL2.cpp
@@ -0,0 +1,10 @@
+// $Id$
+
+/**
+ *  @file Administrator_Client_IDL2.cpp
+ *
+ *  @author Don Busch <busch_d@ociweb.com>
+ */
+
+#include "idl2/Messenger_IDL2C.h"
+#include "Administrator_Client.cpp"
