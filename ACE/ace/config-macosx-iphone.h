cc3387bb7335bb9a967b9ebe4598f695a541d7a5
Abdullah Sowayan <sowayan@users.noreply.github.com>
2010-01-09 00:46:51 +0000

    Sat Jan  9 00:42:55 UTC 2010  Abdul Sowayan  <sowayan@gmail.com>

--- ACE/ace/config-macosx-iphone.h
+++ ACE/ace/config-macosx-iphone.h
@@ -2,7 +2,10 @@
 #ifndef ACE_CONFIG_MACOSX_IPHONE_H
 #define ACE_CONFIG_MACOSX_IPHONE_H
 
+#define ACE_SIZEOF_LONG_DOUBLE 8
+
 #include "ace/config-macosx-snowleopard.h"
+
 
 #endif ACE_CONFIG_MACOSX_IPHONE_H
 
