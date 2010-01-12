c0d8d5916781edcb5e62e02b1c5cfa4c4295213b
sowayaa <sowayaa@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-01-12 04:15:38 +0000

    Tue Jan 12 04:12:41 UTC 2010  Abdul Sowayan  <sowayan@gmail.com>

---
+++ trunk/ACE/ace/config-macosx-iphone-simulator.h
@@ -0,0 +1,8 @@
+// $Id$
+#ifndef ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
+#define ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
+
+#include "ace/config-macosx-snowleopard.h"
+
+#endif ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
+
