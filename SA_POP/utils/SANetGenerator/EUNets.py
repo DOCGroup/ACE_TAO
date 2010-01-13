10a3df789a00e1f4f1a8dfe4e7f54db8bb1f6782
dmack <dmack@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-01-13 16:22:23 +0000

    Wed Jan 13 16:16:38 UTC 2010  Daniel L.C. Mack  <daniel.l.mack@vanderbilt.edu>

---
+++ SA_POP/utils/SANetGenerator/EUNets.py
@@ -0,0 +1,21 @@
+#Daniel L.C. Mack
+#EUNets.py
+#Runs the Net_Complete Script with a set of params for a certain number of runs.
+#Nnets takes the number of networks to make, the rest of the params are from
+#The Net Complete Script
+
+import os
+import sys
+
+Nnets = int(sys.argv[1])
+
+Ntasks = sys.argv[2]
+Nconds = sys.argv[3]
+inDegree = sys.argv[4]
+outDegree = sys.argv[5]
+rGen = sys.argv[6]
+outname = sys.argv[7]
+
+for each in range(0,Nnets):
+	nname = outname + str(each)
+	os.system("python Net_Complete.py " + Ntasks + " " + Nconds + " " + inDegree + " " + outDegree + " " + rGen + " " + nname)
