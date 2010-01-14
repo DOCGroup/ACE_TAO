851f590234564c426b45671fbd9f5b2d939d9578
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-01-14 12:28:04 +0000

    Thu Jan 14 12:27:19 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * TAO_IDL/be/be_visitor_array/array_ch.cpp
            * TAO_IDL/be/be_visitor_reifying.cpp
            * TAO_IDL/be/be_visitor_tmpl_module_inst.cpp
            * TAO_IDL/be_include/be_visitor_reifying.h
              Fuzz fixes

--- trunk/TAO/TAO_IDL/be/be_visitor_reifying.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_reifying.cpp
@@ -56,7 +56,7 @@ be_visitor_reifying::visit_interface (be_interface *node)
                                     node->is_local (),
                                     node->is_abstract ()),
                       -1);
-                      
+
       this->ctx_->template_module_inst_scope ()->add_to_scope (
         this->reified_node_ );
     }
@@ -64,7 +64,7 @@ be_visitor_reifying::visit_interface (be_interface *node)
     {
       this->reified_node_ = node;
     }
-    
+
   return 0;
 }
 
@@ -72,7 +72,7 @@ int
 be_visitor_reifying::visit_exception (be_exception *node)
 {
   this->reified_node_ = node;
-  
+
   return 0;
 }
 
@@ -80,7 +80,7 @@ int
 be_visitor_reifying::visit_typedef (be_typedef *node)
 {
   this->reified_node_ = node;
-  
+
   return 0;
 }
 
@@ -89,7 +89,7 @@ be_visitor_reifying::visit_array (be_array *node)
 {
   be_type *bt =
     be_type::narrow_from_decl (node->base_type ());
-    
+
   if (bt->accept (this) != 0)
     {
       ACE_ERROR_RETURN ((LM_ERROR,
@@ -98,18 +98,18 @@ be_visitor_reifying::visit_array (be_array *node)
                          ACE_TEXT ("visit of base type failed\n")),
                         -1);
     }
-    
+
   bt = be_type::narrow_from_decl (this->reified_node_);
-  
+
   AST_Expression **dims = node->dims ();
   AST_Expression *v = 0;
   UTL_ExprList *v_list = 0;
-  
+
   for (ACE_CDR::ULong i = 0; i < node->n_dims (); ++i)
     {
       be_param_holder *ph =
         be_param_holder::narrow_from_decl (dims[i]->param_holder ());
-      
+
       if (ph != 0)
         {
           if (this->visit_param_holder (ph) != 0)
@@ -121,10 +121,10 @@ be_visitor_reifying::visit_array (be_array *node)
                                  ACE_TEXT ("failed\n")),
                                 -1);
             }
-      
+
           AST_Constant *c =
             AST_Constant::narrow_from_decl (this->reified_node_);
-            
+
           ACE_NEW_RETURN (v,
                           AST_Expression (c->constant_value (),
                                           AST_Expression::EV_ulong),
@@ -137,12 +137,12 @@ be_visitor_reifying::visit_array (be_array *node)
                                           AST_Expression::EV_ulong),
                           -1);
         }
-        
+
       UTL_ExprList *el = 0;
       ACE_NEW_RETURN (el,
                       UTL_ExprList (v, 0),
                       -1);
-                      
+
       if (v_list == 0)
         {
           v_list = el;
@@ -152,12 +152,12 @@ be_visitor_reifying::visit_array (be_array *node)
           v_list->nconc (el);
         }
     }
-    
+
   UTL_ScopedName sn (node->name ()->last_component ()->copy (),
                      0);
-                     
+
   be_array *arr = 0;
-    
+
   ACE_NEW_RETURN (arr,
                   be_array (&sn,
                             node->n_dims (),
@@ -165,15 +165,15 @@ be_visitor_reifying::visit_array (be_array *node)
                             false,
                             false),
                   -1);
-                  
+
   sn.destroy ();
   v_list->destroy ();
   delete v_list;
   v_list = 0;
-  
+
   arr->set_base_type (bt);
   this->reified_node_ = arr;
-  
+
   return 0;
 }
 
@@ -182,7 +182,7 @@ be_visitor_reifying::visit_sequence (be_sequence *node)
 {
   be_type *bt =
     be_type::narrow_from_decl (node->base_type ());
-    
+
   if (bt->accept (this) != 0)
     {
       ACE_ERROR_RETURN ((LM_ERROR,
@@ -191,13 +191,13 @@ be_visitor_reifying::visit_sequence (be_sequence *node)
                          ACE_TEXT ("visit of base type failed\n")),
                         -1);
     }
-    
+
   bt = be_type::narrow_from_decl (this->reified_node_);
-  
+
   AST_Expression *v = node->max_size ();
   be_param_holder *ph =
     be_param_holder::narrow_from_decl (v->param_holder ());
-  
+
   if (ph != 0)
     {
       if (this->visit_param_holder (ph) != 0)
@@ -209,19 +209,19 @@ be_visitor_reifying::visit_sequence (be_sequence *node)
                              ACE_TEXT ("failed\n")),
                             -1);
         }
-      
+
       AST_Constant *c =
         AST_Constant::narrow_from_decl (this->reified_node_);
-        
+
       v = c->constant_value ();
     }
-  
+
   AST_Expression *bound = 0;
   ACE_NEW_RETURN (bound,
                   AST_Expression (v,
                                   AST_Expression::EV_ulong),
                   -1);
-      
+
   Identifier id ("sequence");
   UTL_ScopedName sn (&id, 0);
 
@@ -265,16 +265,16 @@ be_visitor_reifying::visit_param_holder (be_param_holder *node)
     {
       FE_Utils::T_Param_Info *item = 0;
       iter.next (item);
-      
+
       if (item == node->info ())
         {
           AST_Decl **ret_ptr = 0;
-          
+
           if (t_args->get (ret_ptr, i) == 0)
             {
               be_decl *candidate =
                 be_decl::narrow_from_decl (*ret_ptr);
-                
+
               return candidate->accept (this);
             }
           else
@@ -291,7 +291,7 @@ be_visitor_reifying::visit_param_holder (be_param_holder *node)
             }
         }
     }
-    
+
   ACE_ERROR_RETURN ((LM_ERROR,
                      ACE_TEXT ("be_visitor_reifying::")
                      ACE_TEXT ("visit_param_holder() - no match for ")
@@ -305,16 +305,17 @@ bool
 be_visitor_reifying::declared_in_template_module (AST_Decl *d)
 {
   AST_Decl *tmp = d;
-  
+
   while (tmp != 0)
     {
       if (AST_Template_Module::narrow_from_decl (tmp) != 0)
         {
           return true;
         }
-        
+
       tmp = ScopeAsDecl (tmp->defined_in ());
     }
-    
+
   return false;
 }
+
