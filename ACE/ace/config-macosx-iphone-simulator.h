febec9e1b570d1e5c77fdf6e60c514503a0503a7
Abdullah Sowayan <sowayan@users.noreply.github.com>
2010-01-21 23:19:50 +0000

    Thu Jan 21 23:16:27 UTC 2010  Abdul Sowayan  <sowayan@gmail.com>

--- ACE/ace/config-macosx-iphone-simulator.h
+++ ACE/ace/config-macosx-iphone-simulator.h
@@ -2,6 +2,7 @@
 #ifndef ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
 #define ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
 
+#define ACE_HAS_IPHONE
 #include "ace/config-macosx-snowleopard.h"
 
 #endif ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
