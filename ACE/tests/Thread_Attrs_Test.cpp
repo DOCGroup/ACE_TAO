045140fe00c9eab5a2d28f11622a12c846c5437c
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-01-23 13:58:06 +0000

    Sat Jan 23 13:53:30 UTC 2010  Martin Corino  <mcorino@remedy.nl>
            * tests/Thread_Attrs_Test.cpp:
            
              Changed test to check only if resulting stack size is
              *at least* as large as requested.
              As the Posix docs say that the size set for the threads stack 
              will be the *minimum* size allocated (the actual size may f.i. be 
              bigger because of a) pagesize rounding, b) guardsize addition)
              this is the best we can do without konowing the exact algorithm
              used by the pthread implementation.
              Also see this https://bugzilla.redhat.com/show_bug.cgi?id=435337
              for more information.

--- ACE/tests/Thread_Attrs_Test.cpp
+++ ACE/tests/Thread_Attrs_Test.cpp
@@ -83,7 +83,11 @@ Stack_Size_Check::svc (void)
   my_size = this->stack_size_;
 #endif /* __USE_GNU */
 
-  if (my_size != this->stack_size_)
+  // The Posix docs say that the size set for the threads stack will be the
+  // *minimum* size allocated (the actual size may be bigger because of
+  // a) pagesize rounding, b) guardsize addition) so we can really only
+  // check if we have gotten *at least* what we asked for.
+  if (my_size < this->stack_size_)
     {
       ACE_ERROR ((LM_ERROR,
                   ACE_TEXT ("%t: My stack size attr %B; expected %B\n"),
