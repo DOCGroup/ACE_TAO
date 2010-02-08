96252f25e68a3a8d1261b8f5057e8932c8642883
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-02-08 11:38:30 +0000

    Mon Feb  8 11:35:22 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/ndds/DataWriterListener_T.h:
            * connectors/dds4ccm/impl/ndds/DataWriterListener_T.cpp:
            * connectors/dds4ccm/impl/ndds/DomainParticipantListener_T.h:
            * connectors/dds4ccm/impl/ndds/DomainParticipantListener_T.cpp:
            * connectors/dds4ccm/impl/ndds/PortStatusListener_T.h:
            * connectors/dds4ccm/impl/ndds/PortStatusListener_T.cpp:
            * connectors/dds4ccm/impl/ndds/SubscriberListener_T.h:
            * connectors/dds4ccm/impl/ndds/SubscriberListener_T.cpp:
              Moved all empty inline methods from header to source file.

--- trunk/CIAO/connectors/dds4ccm/impl/ndds/DataWriterListener_T.cpp
+++ trunk/CIAO/connectors/dds4ccm/impl/ndds/DataWriterListener_T.cpp
@@ -17,6 +17,46 @@ CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::~DataWriterListener_T (
 }
 
 template <typename DDS_TYPE, typename CCM_TYPE>
+void
+CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::on_offered_deadline_missed (
+  ::DDS::DataWriter_ptr ,
+  const ::DDS::OfferedDeadlineMissedStatus & )
+{
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE>
+void
+CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::on_offered_incompatible_qos (
+  ::DDS::DataWriter_ptr ,
+  const ::DDS::OfferedIncompatibleQosStatus & )
+{
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE>
+void
+CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::on_liveliness_lost (
+  ::DDS::DataWriter_ptr ,
+  const ::DDS::LivelinessLostStatus & )
+{
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE>
+void
+CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::on_publication_matched (
+  ::DDS::DataWriter_ptr ,
+  const ::DDS::PublicationMatchedStatus & )
+{
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE>
+void
+CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::on_reliable_reader_activity_changed (
+  ::DDS::DataWriter_ptr ,
+  const ::DDS::ReliableReaderActivityChangedStatus & )
+{
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE>
 ::DDS::StatusMask
 CIAO::DDS4CCM::DataWriterListener_T<DDS_TYPE, CCM_TYPE>::get_mask (void)
 {
