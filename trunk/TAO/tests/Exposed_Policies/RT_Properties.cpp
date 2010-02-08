c0d284e7cf6c3a5ad4f224ada20a1bbd83e3de81
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-02-08 10:37:34 +0000

    Mon Feb  8 10:35:30 UTC 2010  Martin Corino  <mcorino@remedy.nl>
            * orbsvcs/orbsvcs/Trader/Constraint_l.cpp:
            * orbsvcs/tests/Concurrency/lex.CC_command.cpp:
            * performance-tests/POA/Demux/demux_test_server.cpp:
            * tests/Exposed_Policies/RT_Properties.cpp:
            
              Fixed new GCC 4.4.x warning about ignoring return values of
              function marked by the new warn_unused_result attribute.

--- trunk/TAO/tests/Exposed_Policies/RT_Properties.cpp
+++ trunk/TAO/tests/Exposed_Policies/RT_Properties.cpp
@@ -36,26 +36,27 @@ RT_Properties::read_from (const ACE_TCHAR *file_name)
   char string_field[256];
   int int_field;
   unsigned int i = 0;
+  int n_matched;
 
   while (fscanf (fp, "%s", string_field) != EOF )
     {
       if (ACE_OS::strcmp (string_field, "Priority") == 0)
         {
-          fscanf (fp, "%d", &int_field);
+          n_matched = fscanf (fp, "%d", &int_field);
           rt_properties->priority (int_field);
         }
       else if (ACE_OS::strcmp (string_field, "Priority_Bands") == 0)
         {
-          fscanf (fp, "%d", &int_field);
+          n_matched = fscanf (fp, "%d", &int_field);
           rt_properties->priority_bands_.length (int_field);
 
         }
       else if (ACE_OS::strcmp (string_field, "Priority_Range") == 0)
         {
-          fscanf (fp, "%d", &int_field);
+          n_matched = fscanf (fp, "%d", &int_field);
           rt_properties->priority_bands_[i].low = int_field;
 
-          fscanf (fp, "%d", &int_field);
+          n_matched = fscanf (fp, "%d", &int_field);
           rt_properties->priority_bands_[i].high = int_field;
 
           ++i;
