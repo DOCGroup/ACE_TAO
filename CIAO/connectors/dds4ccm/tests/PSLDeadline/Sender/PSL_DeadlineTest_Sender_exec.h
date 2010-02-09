cf3d720db530607a8654a810461bd45ca4aa1749
msmit <msmit@remedy.nl>
2010-02-09 12:51:58 +0000

    Tue Feb  9 12:49:17 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/PSLDeadline/Sender/PSL_DeadlineTest_Sender_exec.h:
            * connectors/dds4ccm/tests/PSLDeadline/Sender/PSL_DeadlineTest_Sender_exec.cpp:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender/PSL_SampleLost_Sender_exec.h:
            * connectors/dds4ccm/tests/PSLSampleLost/Sender/PSL_SampleLost_Sender_exec.cpp:
              Code alignment in order to be consistent with other tests.

--- CIAO/connectors/dds4ccm/tests/PSLDeadline/Sender/PSL_DeadlineTest_Sender_exec.h
+++ CIAO/connectors/dds4ccm/tests/PSLDeadline/Sender/PSL_DeadlineTest_Sender_exec.h
@@ -22,49 +22,55 @@ namespace CIAO_PSL_DeadlineTest_Sender_Impl
   typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, CORBA::Boolean > Atomic_Boolean;
 
   class Sender_exec_i;
-  
+
+  //============================================================
+  // pulse_Generator
+  //============================================================
   class pulse_Generator :
     public ACE_Event_Handler
   {
   public:
     pulse_Generator (Sender_exec_i &callback);
-    /// Handle the timeout.
     virtual int handle_timeout (const ACE_Time_Value &tv,
                                 const void *arg);
   private:
-    /// Maintains a handle that actually process the event
     Sender_exec_i &pulse_callback_;
   };
 
-class SENDER_EXEC_Export ConnectorStatusListener_exec_i
+  //============================================================
+  // ConnectorStatusListener_exec_i
+  //============================================================
+  class SENDER_EXEC_Export ConnectorStatusListener_exec_i
     : public virtual ::CCM_DDS::CCM_ConnectorStatusListener,
       public virtual ::CORBA::LocalObject
   {
   public:
     ConnectorStatusListener_exec_i (void);
-   virtual ~ConnectorStatusListener_exec_i (void);
-    
+    virtual ~ConnectorStatusListener_exec_i (void);
+
     virtual
-    void on_inconsistent_topic( ::DDS::Topic_ptr the_topic, 
+    void on_inconsistent_topic (::DDS::Topic_ptr the_topic,
                                 const DDS::InconsistentTopicStatus & status);
     virtual
-    void on_requested_incompatible_qos( ::DDS::DataReader_ptr the_reader,
+    void on_requested_incompatible_qos (::DDS::DataReader_ptr the_reader,
                                         const DDS::RequestedIncompatibleQosStatus & status);
     virtual
-    void on_sample_rejected( ::DDS::DataReader_ptr the_reader, 
+    void on_sample_rejected (::DDS::DataReader_ptr the_reader,
                              const DDS::SampleRejectedStatus & status);
     virtual
-    void on_offered_deadline_missed( ::DDS::DataWriter_ptr the_writer,
+    void on_offered_deadline_missed (::DDS::DataWriter_ptr the_writer,
                                      const DDS::OfferedDeadlineMissedStatus & status);
     virtual
-    void on_offered_incompatible_qos( ::DDS::DataWriter_ptr the_writer, 
+    void on_offered_incompatible_qos (::DDS::DataWriter_ptr the_writer,
                                       const DDS::OfferedIncompatibleQosStatus & status);
     virtual
-    void on_unexpected_status( ::DDS::Entity_ptr the_entity,
-       ::DDS::StatusKind  status_kind);
-  
+    void on_unexpected_status (::DDS::Entity_ptr the_entity,
+                               ::DDS::StatusKind  status_kind);
   };
 
+  //============================================================
+  // Sender_exec_i
+  //============================================================
   class Sender_exec_i
     : public virtual Sender_Exec,
       public virtual ::CORBA::LocalObject
@@ -72,7 +78,7 @@ class SENDER_EXEC_Export ConnectorStatusListener_exec_i
   public:
     Sender_exec_i (void);
     virtual ~Sender_exec_i (void);
-    virtual void add_instance_of_topic (const char *, int x );
+
     void tick (void);
 
     virtual void set_session_context (::Components::SessionContext_ptr ctx);
@@ -80,19 +86,24 @@ class SENDER_EXEC_Export ConnectorStatusListener_exec_i
     virtual void ccm_activate (void);
     virtual void ccm_passivate (void);
     virtual void ccm_remove (void);
+
     // Port operations.
     virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr
-      get_test_topic_connector_status(void);
+    get_test_topic_connector_status(void);
 
   private:
     void start (void);
     void stop (void);
+
     pulse_Generator * ticker_;
     ::PSL_DeadlineTest::CCM_Sender_Context_var context_;
     CCM_DDS::TestTopic::Writer_var writer_;
+
     TAO_SYNCH_MUTEX mutex_;
     typedef std::map<ACE_CString, TestTopic_var> PSL_DLTest_Table;
     PSL_DLTest_Table _ktests_;
+
+    void add_instance_of_topic (const char *, int x );
  };
 
   extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
