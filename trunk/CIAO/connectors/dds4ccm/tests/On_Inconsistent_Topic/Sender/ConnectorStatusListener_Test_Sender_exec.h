b2e8178a40835a02181fc3729d4b654cdf4e6f28
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-02-09 12:39:18 +0000

    Tue Feb  9 12:36:38 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/On_Inconsistent_Topic/Receiver/ConnectorStatusListener_Test_Receiver_exec.cpp:
              Optimized logging.
    
            * connectors/dds4ccm/tests/On_Inconsistent_Topic/Sender/ConnectorStatusListener_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/On_Inconsistent_Topic/Sender/ConnectorStatusListener_Test_Sender_exec.cpp:
              Added test for thread switch.

--- trunk/CIAO/connectors/dds4ccm/tests/On_Inconsistent_Topic/Sender/ConnectorStatusListener_Test_Sender_exec.h
+++ trunk/CIAO/connectors/dds4ccm/tests/On_Inconsistent_Topic/Sender/ConnectorStatusListener_Test_Sender_exec.h
@@ -20,40 +20,48 @@
 namespace CIAO_ConnectorStatusListener_Test_Sender_Impl
 {
   typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, CORBA::Boolean > Atomic_Boolean;
+  typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, ACE_thread_t> Atomic_ThreadId;
 
   class Sender_exec_i;
 
-class SENDER_EXEC_Export ConnectorStatusListener_sec_exec_i
+  //============================================================
+  // ConnectorStatusListener_sec_exec_i
+  //============================================================
+  class SENDER_EXEC_Export ConnectorStatusListener_sec_exec_i
     : public virtual ::CCM_DDS::CCM_ConnectorStatusListener,
       public virtual ::CORBA::LocalObject
   {
   public:
-    ConnectorStatusListener_sec_exec_i (Atomic_Boolean &);
-   virtual ~ConnectorStatusListener_sec_exec_i (void);
+    ConnectorStatusListener_sec_exec_i (Atomic_Boolean &,
+                                        Atomic_ThreadId &);
+    virtual ~ConnectorStatusListener_sec_exec_i (void);
 
     virtual
-    void on_inconsistent_topic( ::DDS::Topic_ptr the_topic,
+    void on_inconsistent_topic (::DDS::Topic_ptr the_topic,
                                 const DDS::InconsistentTopicStatus & status);
     virtual
-    void on_requested_incompatible_qos( ::DDS::DataReader_ptr the_reader,
+    void on_requested_incompatible_qos (::DDS::DataReader_ptr the_reader,
                                         const DDS::RequestedIncompatibleQosStatus & status);
     virtual
-    void on_sample_rejected( ::DDS::DataReader_ptr the_reader,
+    void on_sample_rejected (::DDS::DataReader_ptr the_reader,
                              const DDS::SampleRejectedStatus & status);
     virtual
-      void on_offered_deadline_missed( ::DDS::DataWriter_ptr the_writer,
-                                     const DDS::OfferedDeadlineMissedStatus & status);
+      void on_offered_deadline_missed (::DDS::DataWriter_ptr the_writer,
+                                       const DDS::OfferedDeadlineMissedStatus & status);
     virtual
-    void on_offered_incompatible_qos( ::DDS::DataWriter_ptr the_writer,
+    void on_offered_incompatible_qos (::DDS::DataWriter_ptr the_writer,
                                       const DDS::OfferedIncompatibleQosStatus & status);
     virtual
-      void on_unexpected_status( ::DDS::Entity_ptr the_entity,
-       ::DDS::StatusKind  status_kind);
-     private:
+      void on_unexpected_status (::DDS::Entity_ptr the_entity,
+                                 ::DDS::StatusKind  status_kind);
+  private:
     Atomic_Boolean &inconsistent_;
-
+    Atomic_ThreadId &thread_id_;
   };
 
+  //============================================================
+  // Sender_exec_i
+  //============================================================
   class Sender_exec_i
     : public virtual Sender_Exec,
       public virtual ::CORBA::LocalObject
@@ -64,28 +72,27 @@ class SENDER_EXEC_Export ConnectorStatusListener_sec_exec_i
 
     virtual void set_session_context (::Components::SessionContext_ptr ctx);
 
-    virtual void add_instance_of_sec_topic (const char *, int x );
-
-
     virtual void configuration_complete (void);
-
     virtual void ccm_activate (void);
     virtual void ccm_passivate (void);
     virtual void ccm_remove (void);
 
      // Port operations.
     virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr
-      get_test_sec_topic_connector_status(void);
+    get_test_sec_topic_connector_status(void);
 
   private:
     ::ConnectorStatusListener_Test::CCM_Sender_Context_var context_;
 
     Atomic_Boolean inconsistent_;
+    Atomic_ThreadId thread_id_listener_;
 
     TAO_SYNCH_MUTEX mutex_;
     typedef std::map<ACE_CString, TestSecondTopic_var> ConnectorStatusListener_TestSec_Table;
     ConnectorStatusListener_TestSec_Table sec_ktests_;
- };
+
+    void add_instance_of_sec_topic (const char *, int x );
+  };
 
   extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
   create_ConnectorStatusListener_Test_Sender_Impl (void);
