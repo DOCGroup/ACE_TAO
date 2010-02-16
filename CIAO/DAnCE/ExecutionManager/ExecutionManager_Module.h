208270fe9ea645d2d8e56d152fe347b2444bd258
Johnny Willemsen <jwillemsen@remedy.nl>
2010-02-16 20:04:53 +0000

    Tue Feb 16 20:04:57 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            Simplified execution manager
    
            * DAnCE/ExecutionManager/DAnCE_ExecutionManager_Module_Export.h
            * DAnCE/ExecutionManager/Execution_Manager_Exec.cpp
            * DAnCE/ExecutionManager/ExecutionManager_Export.h
              Removed
    
            * DAnCE/ExecutionManager/Execution_Manager.cpp
            * DAnCE/ExecutionManager/ExecutionManager.mpc
            * DAnCE/ExecutionManager/ExecutionManager_Impl.h
            * DAnCE/ExecutionManager/ExecutionManager_Module.cpp
            * DAnCE/ExecutionManager/ExecutionManager_Module.h
              Simplified
    
            * DAnCE/Plan_Launcher/Plan_Launcher.cpp:
              Layout changes

--- CIAO/DAnCE/ExecutionManager/ExecutionManager_Module.h
+++ CIAO/DAnCE/ExecutionManager/ExecutionManager_Module.h
@@ -17,15 +17,12 @@
 
 #include /**/ "ace/pre.h"
 
-#include "DAnCE_ExecutionManager_Module_Export.h"
-
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
 #include "ace/Service_Config.h"
 #include "ace/Vector_T.h"
-#include "tao/Object_Loader.h"
 
 namespace DAnCE
 {
@@ -35,15 +32,25 @@ namespace DAnCE
 /**
  * @class Execution_Manager_Module
  *
- * @brief The shared object that is instantiated when the execution manager
- *        module/library is dynamically loaded.
- *
- * This class runs the execution manager instance
+ * This class implements the execution manager instance
  */
-class DAnCE_ExecutionManager_Module_Export DAnCE_ExecutionManager_Module
-      : public TAO_Object_Loader
+class DAnCE_ExecutionManager_Module
   {
   public:
+    /// Constructor.
+    DAnCE_ExecutionManager_Module (void);
+
+    /// Destructor.
+    ~DAnCE_ExecutionManager_Module (void);
+
+    /// Setup our infrastructure
+    CORBA::Object_ptr init (CORBA::ORB_ptr orb,
+                            int argc,
+                            ACE_TCHAR *argv []);
+
+  private:
+    bool parse_args (int argc, ACE_TCHAR *argv []);
+
     struct SOptions
       {
         const ACE_TCHAR* exec_mgr_file_;
@@ -74,27 +81,11 @@ class DAnCE_ExecutionManager_Module_Export DAnCE_ExecutionManager_Module
         }
     };
 
-    /// Constructor.
-    DAnCE_ExecutionManager_Module (void);
-
-    /// Destructor.
-    ~DAnCE_ExecutionManager_Module (void);
-
-    /// Overload the base class method to create a new instance
-    /// of a DAnCE_NodeManager_Module object.
-    virtual CORBA::Object_ptr create_object (CORBA::ORB_ptr orb,
-                                             int argc,
-                                             ACE_TCHAR *argv []);
-
-    virtual bool parse_args (int argc, ACE_TCHAR *argv []);
-
     SOptions options_;
-  private:
+
     /// Storage for ExecutionManager servant.
     DAnCE::ExecutionManager_Impl * em_impl_;
   };
-
-ACE_FACTORY_DECLARE (DAnCE_ExecutionManager_Module, DAnCE_ExecutionManager_Module)
 
 #include /**/ "ace/post.h"
 
