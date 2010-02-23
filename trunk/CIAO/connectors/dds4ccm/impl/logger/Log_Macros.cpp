780dcfeed3354301fa70088506c1c941c3dc5691
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-02-23 12:48:55 +0000

    Tue Feb 23 12:45:56 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/logger/Log_Macros.h:
            * connectors/dds4ccm/impl/logger/Log_Macros.cpp:
              Making use of the CIAO_Debug_Level flag of the
              CIAO logging framework.

--- trunk/CIAO/connectors/dds4ccm/impl/logger/Log_Macros.cpp
+++ trunk/CIAO/connectors/dds4ccm/impl/logger/Log_Macros.cpp
@@ -2,4 +2,7 @@
 
 #include "Log_Macros.h"
 
-CIAO_Logger_Export unsigned int CIAO_debug_level = 0;
+// For now, DDS4CCM is making use of the CIAO logging framework.
+// This file is there in preparation of the DDS4CCM logging framework.
+// For now, this should be empty because the CIAO_debug_level flag is
+// set in the Log_Macros.cpp of the CIAO logging framework.
