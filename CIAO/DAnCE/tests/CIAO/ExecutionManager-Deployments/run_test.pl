fcfa5ea14c1560983b84db436d6486b9284e10fc
William R. Otte <wotte@dre.vanderbilt.edu>
2010-02-26 15:55:42 +0000

    Fri Feb 26 15:54:33 UTC 2010  William R. Otte  <wotte@dre.vanderbilt.edu>
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/MultiplexConnectionExplicitHome_NS.cdp:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/MultiplexConnectionExplicitHome_NS_PL_DP.cdp:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/MultiplexConnectionExplicitHome_NS_PL_SP.cdp:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/SimplexConnectionExplicitHome_NS.cdp:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/SimplexConnectionExplicitHome_NS_PL_DP.cdp:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/SimplexConnectionExplicitHome_NS_PL_SP.cdp:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/run_test.pl:
    
              Expanded NS/PlanLocality testing merged in, converted this test
              to use dance_plan_launcher.
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/em_launcher.mpc:
            * DAnCE/tests/CIAO/ExecutionManager-Deployments/simple_em_launcher.cpp:
    
              Removed these files as they are no longer needed for above test.

--- CIAO/DAnCE/tests/CIAO/ExecutionManager-Deployments/run_test.pl
+++ CIAO/DAnCE/tests/CIAO/ExecutionManager-Deployments/run_test.pl
@@ -197,11 +197,14 @@ foreach $file (@files) {
 
     # Invoke executor - start the application -.
     print "Invoking executor - launch the application -\n";
-    $E = $tg_executor->CreateProcess ("simple_em_launcher",
-				      "file://$ior_emfile $file");
-    
+    $E = $tg_executor->CreateProcess ("$DANCE_ROOT/bin/dance_plan_launcher",
+                                      "-x $file -k file://$ior_emfile");
     $E->SpawnWaitKill (120);
-    
+
+    print "Teardown the application\n";
+    $E = $tg_executor->CreateProcess ("$DANCE_ROOT/bin/dance_plan_launcher",
+                                      "-k file://$ior_emfile -x $file -q");
+    $E->SpawnWaitKill (120);
     print "Executor finished.\n";
 
     delete_ior_files ();
