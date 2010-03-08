153563935292e94704c20c4ba8812d9f3694daca
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-03-08 19:18:29 +0000

    ChangeLogTag: Mon Mar  8 19:17:55 UTC 2010  Jeff Parsons <j.parsons@vanderbilt.edu>

--- trunk/TAO/TAO_IDL/be/be_decl.cpp
+++ trunk/TAO/TAO_IDL/be/be_decl.cpp
@@ -96,7 +96,8 @@ be_decl::be_decl (AST_Decl::NodeType type,
     svnt_hdr_facet_gen_ (false),
     svnt_src_facet_gen_ (false),
     exec_hdr_facet_gen_ (false),
-    exec_src_facet_gen_ (false)
+    exec_src_facet_gen_ (false),
+    ami4ccm_ex_idl_gen_ (false)
 {
 }
 
@@ -589,6 +590,12 @@ be_decl::exec_src_facet_gen (void)
   return this->exec_src_facet_gen_;
 }
 
+bool
+be_decl::ami4ccm_ex_idl_gen (void)
+{
+  return this->ami4ccm_ex_idl_gen_;
+}
+
 // Set the flag indicating that code generation is done.
 void
 be_decl::cli_hdr_gen (bool val)
@@ -871,6 +878,12 @@ void
 be_decl::exec_src_facet_gen (bool val)
 {
   this->exec_src_facet_gen_ = val;
+}
+
+void
+be_decl::ami4ccm_ex_idl_gen (bool val)
+{
+  this->ami4ccm_ex_idl_gen_ = val;
 }
 
 //==========================================
--- trunk/TAO/TAO_IDL/be/be_visitor_interface/ami4ccm_sendc_ex_idl.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_interface/ami4ccm_sendc_ex_idl.cpp
@@ -1,7 +1,7 @@
 
 //=============================================================================
 /**
- *  @file    ami4ccm_rh_ex_idl.cpp
+ *  @file    ami4ccm_sendc_ex_idl.cpp
  *
  *  $Id$
  *
@@ -12,31 +12,31 @@
  */
 //=============================================================================
 
-be_visitor_ami4ccm_rh_ex_idl::be_visitor_ami4ccm_rh_ex_idl (
+be_visitor_ami4ccm_sendc_ex_idl::be_visitor_ami4ccm_sendc_ex_idl (
       be_visitor_context *ctx)
   : be_visitor_scope (ctx),
     os_ (*ctx->stream ())
 {
 }
 
-be_visitor_ami4ccm_rh_ex_idl::~be_visitor_ami4ccm_rh_ex_idl (void)
+be_visitor_ami4ccm_sendc_ex_idl::~be_visitor_ami4ccm_sendc_ex_idl (void)
 {
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::visit_interface (be_interface *node)
+be_visitor_ami4ccm_sendc_ex_idl::visit_interface (be_interface *node)
 {
-  os_ << be_nl
+  this->iface_ = node;
+
+  os_ << be_nl << be_nl
       << "local interface AMI_"
-      << node->original_local_name ()
-      << "Callback" << be_idt_nl
-      << ": ::CCM_AMI::ReplyHandler" << be_uidt_nl
+      << node->original_local_name () << be_nl
       << "{" << be_idt;
       
   if (this->visit_scope (node) == -1)
     {
       ACE_ERROR_RETURN ((LM_ERROR,
-                         ACE_TEXT ("be_visitor_ami4ccm_rh_ex_idl")
+                         ACE_TEXT ("be_visitor_ami4ccm_sendc_ex_idl")
                          ACE_TEXT ("::visit_interface - ")
                          ACE_TEXT ("visit_scope() failed\n")),
                         -1);         
@@ -47,10 +47,8 @@ be_visitor_ami4ccm_rh_ex_idl::visit_interface (be_interface *node)
       
   os_ << be_nl << be_nl
       << "local interface CCM_AMI_"
-      << node->original_local_name ()
-      << "Callback" << be_idt_nl
-      << ": AMI_" << node->original_local_name ()
-      << "Callback" << be_uidt_nl
+      << node->original_local_name () << be_idt_nl
+      << ": AMI_" << node->original_local_name () << be_uidt_nl
       << "{" << be_nl
       << "};";
      
@@ -58,7 +56,7 @@ be_visitor_ami4ccm_rh_ex_idl::visit_interface (be_interface *node)
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::visit_operation (be_operation *node)
+be_visitor_ami4ccm_sendc_ex_idl::visit_operation (be_operation *node)
 {
   if (node->flags () == AST_Operation::OP_oneway)
     {
@@ -67,15 +65,11 @@ be_visitor_ami4ccm_rh_ex_idl::visit_operation (be_operation *node)
     }
 
   os_ << be_nl
-      << "void " << node->original_local_name ()
-      << " (" << be_idt;
+      << "void sendc_" << node->original_local_name ()
+      << " (" << be_idt_nl
+      << "in AMI_" << this->iface_->original_local_name ()
+      << "Callback ami_handler";
       
-  if (!node->void_return_type ())
-    {
-      os_ << be_nl
-          << "in long ami_return_val";
-    }
-    
   if (this->visit_scope (node) == -1)
     {
       ACE_ERROR_RETURN ((LM_ERROR,
@@ -86,31 +80,26 @@ be_visitor_ami4ccm_rh_ex_idl::visit_operation (be_operation *node)
       
   os_ << ");" << be_uidt;
   
-  os_ << be_nl
-      << "void " << node->original_local_name ()
-      << "_excep (" << be_idt_nl
-      << "in ::CCM_AMI::ExceptionHolder excep_holder);" << be_uidt;
-  
   return 0;
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::visit_attribute (be_attribute *node)
+be_visitor_ami4ccm_sendc_ex_idl::visit_attribute (be_attribute *node)
 {
-  this->gen_attr_rh_ops (false, node);
+  this->gen_attr_sendc_ops (false, node);
   
   if (!node->readonly ())
     {
-      this->gen_attr_rh_ops (true, node);
+      this->gen_attr_sendc_ops (true, node);
     }
     
   return 0;
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::visit_argument (be_argument *node)
+be_visitor_ami4ccm_sendc_ex_idl::visit_argument (be_argument *node)
 {
-  if (node->direction () == AST_Argument::dir_IN)
+  if (node->direction () != AST_Argument::dir_IN)
     {
       return 0;
     }
@@ -129,7 +118,7 @@ be_visitor_ami4ccm_rh_ex_idl::visit_argument (be_argument *node)
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::visit_string (be_string *node)
+be_visitor_ami4ccm_sendc_ex_idl::visit_string (be_string *node)
 {
   os_ << (node->width () > 1 ? "w" : "") << "string";
 
@@ -144,7 +133,7 @@ be_visitor_ami4ccm_rh_ex_idl::visit_string (be_string *node)
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::visit_sequence (be_sequence *node)
+be_visitor_ami4ccm_sendc_ex_idl::visit_sequence (be_sequence *node)
 {
   // Keep output statements separate because of side effects.
   os_ << "sequence<";
@@ -161,7 +150,7 @@ be_visitor_ami4ccm_rh_ex_idl::visit_sequence (be_sequence *node)
 }
 
 int
-be_visitor_ami4ccm_rh_ex_idl::pre_process (be_decl *node)
+be_visitor_ami4ccm_sendc_ex_idl::pre_process (be_decl *node)
 {
   be_operation *op =
     be_operation::narrow_from_scope (this->ctx_->scope ());
@@ -181,7 +170,7 @@ be_visitor_ami4ccm_rh_ex_idl::pre_process (be_decl *node)
       return 0;
     }
     
-  if (arg->direction () == AST_Argument::dir_IN)
+  if (arg->direction () != AST_Argument::dir_IN)
     {
       return 0;
     }
@@ -192,28 +181,27 @@ be_visitor_ami4ccm_rh_ex_idl::pre_process (be_decl *node)
 }
 
 void
-be_visitor_ami4ccm_rh_ex_idl::gen_attr_rh_ops (bool is_set_op,
-                                               be_attribute *node)
+be_visitor_ami4ccm_sendc_ex_idl::gen_attr_sendc_ops (
+  bool is_set_op,
+  be_attribute *node)
 {
   os_ << be_nl
-      << "void " << (is_set_op ? "set_" : "get_")
-      << node->original_local_name () << " (";
+      << "void sendc_" << (is_set_op ? "set_" : "get_")
+      << node->original_local_name () << " (" << be_idt_nl
+      << "in AMI_" << this->iface_->original_local_name ()
+      << "Callback ami_handler";
       
-  if (!is_set_op)
+  if (is_set_op)
     {
       be_type *t =
         be_type::narrow_from_decl (node->field_type ());
         
-      os_ << be_idt_nl << "in ";
+      os_ << "," << be_nl << "in ";
       
       os_ << IdentifierHelper::type_name (t, this);
       
-      os_ << " " << node->original_local_name () << be_uidt;
+      os_ << " " << node->original_local_name ();
     }
     
-  os_ << ");"
-      << be_nl
-      << "void " << (is_set_op ? "set_" : "get_")
-      << node->original_local_name () << "_excep (" << be_idt_nl
-      << "in CCM_AMI::ExceptionHolder excep_holder);" << be_uidt;
+  os_ << ");" << be_uidt;
 }
