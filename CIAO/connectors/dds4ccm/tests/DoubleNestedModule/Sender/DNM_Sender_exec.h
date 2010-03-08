f7d9d441478ac97939e466f67557963e472e2624
msmit <msmit@remedy.nl>
2010-03-08 14:02:03 +0000

    Mon Mar  8 13:58:26 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/DoubleNestedModule:
            * connectors/dds4ccm/tests/DoubleNestedModule/Base:
            * connectors/dds4ccm/tests/DoubleNestedModule/Base/DNM_Base.idl:
            * connectors/dds4ccm/tests/DoubleNestedModule/Base/DNM_Base.mpc:
            * connectors/dds4ccm/tests/DoubleNestedModule/Connector:
            * connectors/dds4ccm/tests/DoubleNestedModule/Connector/DNM_Connector.idl:
            * connectors/dds4ccm/tests/DoubleNestedModule/Connector/DNM_Connector.mpc:
            * connectors/dds4ccm/tests/DoubleNestedModule/Sender:
            * connectors/dds4ccm/tests/DoubleNestedModule/Sender/DNM_Sender.idl:
            * connectors/dds4ccm/tests/DoubleNestedModule/Sender/DNM_Sender.mpc:
            * connectors/dds4ccm/tests/DoubleNestedModule/Sender/DNM_Sender_exec.h:
            * connectors/dds4ccm/tests/DoubleNestedModule/Sender/DNM_Sender_exec.cpp:
              Compilation test for double nested modules. The sender_exec project has a
              dummy_label since it doesn't seem to compile.

---
+++ CIAO/connectors/dds4ccm/tests/DoubleNestedModule/Sender/DNM_Sender_exec.h
@@ -0,0 +1,49 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_SENDER_EXEC_H_
+#define CIAO_SENDER_EXEC_H_
+
+
+#include "DNM_SenderEC.h"
+
+#include /**/ "Sender_exec_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+
+namespace CIAO_DNM_Test_Sender_Impl
+{
+  //============================================================
+  // Sender_exec_i
+  //============================================================
+  class Sender_exec_i
+    : public virtual Sender_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Sender_exec_i (void);
+    virtual ~Sender_exec_i (void);
+
+    virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+  private:
+    ::DNM_Test::CCM_Sender_Context_var context_;
+    ::DNM_Test::DNM_TestConn::Writer_var writer_;
+  };
+
+  extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_CoherentWrite_Test_Sender_Impl (void);
+}
+
+#endif /* ifndef */
+
