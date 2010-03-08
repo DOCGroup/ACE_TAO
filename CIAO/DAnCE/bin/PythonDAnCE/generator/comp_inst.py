817e6f52216fdb00c5dcc1ee9a8613401699ced0
William R. Otte <wotte@dre.vanderbilt.edu>
2010-03-08 23:53:40 +0000

    Mon Mar  8 23:52:06 UTC 2010  William R. Otte  <wotte@dre.vanderbilt.edu>
            * DAnCE/NodeApplication/NodeApplication_Impl.cpp:
            * ciao/Containers/Session/Session_Container.cpp:
    
              Improved error reporting.
    
            * DAnCE/bin/PythonDAnCE/generator/comp_inst.py:
    
              bugfix, requested node names weren't inserted properly.
    
            * DAnCE/tools/Split_Plan/Split_Plan_impl.cpp:
    
              Increased log level needed for trace-level split plan messages.

--- CIAO/DAnCE/bin/PythonDAnCE/generator/comp_inst.py
+++ CIAO/DAnCE/bin/PythonDAnCE/generator/comp_inst.py
@@ -14,7 +14,7 @@ def template (component_name, config_values, impl, node) :
 
   <instance xmi:id="${component_name}ComponentInstance">
     <name>${component_name}Component</name>
-    <node>Node</node>
+    <node>${node}</node>
     <!-- hostname -->
     <source/>
     <implementation xmi:idref="${impl}" />
