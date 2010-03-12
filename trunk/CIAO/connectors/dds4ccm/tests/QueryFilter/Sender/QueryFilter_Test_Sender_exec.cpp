99dfde4ee83d1da29502bf56427c5ee138067224
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-03-12 12:57:09 +0000

    Fri Mar 12 12:53:58 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/QueryFilter/Receiver/QueryFilter_Test_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/QueryFilter/Sender/QueryFilter_Test_Sender_exec.cpp:
              Rewritten interaction between sender and receiver.

--- trunk/CIAO/connectors/dds4ccm/tests/QueryFilter/Sender/QueryFilter_Test_Sender_exec.cpp
+++ trunk/CIAO/connectors/dds4ccm/tests/QueryFilter/Sender/QueryFilter_Test_Sender_exec.cpp
@@ -76,23 +76,26 @@ namespace CIAO_QueryFilter_Test_Sender_Impl
       {
         ACE_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: Unable to start the reader\n")));
       }
-    for (CORBA::UShort iter_key = 1; iter_key < this->keys_ + 1; ++iter_key)
+    if (this->run_ > 1)
       {
-        char key[7];
-        QueryFilterTest *new_key = new QueryFilterTest;
-        ACE_OS::sprintf (key, "KEY_%d", iter_key);
-        new_key->symbol = CORBA::string_dup(key);
-        for (CORBA::UShort iter = ((this->run_ - 1) * this->iterations_) + 1;
-             iter < this->run_ * this->iterations_ + 1;
-             ++iter)
+        for (CORBA::UShort iter_key = 1; iter_key < this->keys_ + 1; ++iter_key)
           {
-            new_key->iteration = iter;
-            this->writer_->write_one (*new_key, ::DDS::HANDLE_NIL);
-            ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Written key <%C> with <%d>\n"),
-                        key, iter));
+            char key[7];
+            QueryFilterTest *new_key = new QueryFilterTest;
+            ACE_OS::sprintf (key, "KEY_%d", iter_key);
+            new_key->symbol = CORBA::string_dup(key);
+            for (CORBA::UShort iter = ((this->run_ - 2) * this->iterations_) + 1;
+                iter < this->run_ * this->iterations_ + 1;
+                ++iter)
+              {
+                new_key->iteration = iter;
+                this->writer_->write_one (*new_key, ::DDS::HANDLE_NIL);
+                ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Written key <%C> with <%d>\n"),
+                            key, iter));
+              }
           }
+        ACE_OS::sleep (1);
       }
-    ACE_OS::sleep (2);
     this->starter_->start_read (this->run_);
   }
 
