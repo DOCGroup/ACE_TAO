91dcbf8ec572d7c49c06a4578197314d9f071a08
Johnny Willemsen <jwillemsen@remedy.nl>
2010-03-12 11:20:13 +0000

    Fri Mar 12 11:19:54 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * TAO_IDL/be/be_visitor_connector/executor_ami_exh.cpp:
              Fixed argument not used warning
    
            * util/utl_decllist.cpp:
              Zap old documentation

--- TAO/TAO_IDL/be/be_visitor_connector/executor_ami_exh.cpp
+++ TAO/TAO_IDL/be/be_visitor_connector/executor_ami_exh.cpp
@@ -33,12 +33,12 @@ int
 be_visitor_executor_ami_exh::visit_connector (be_connector *node)
 {
   node_ = node;
-  
+
   const char *suffix = "_exec_i";
   AST_Decl *scope = ScopeAsDecl (node->defined_in ());
   bool global = (scope->node_type () == AST_Decl::NT_root);
   const char *smart_scope = (global ? "" : "::");
-  
+
   os_ << be_nl << be_nl
       << "class " << this->export_macro_.c_str ()
       << node->local_name () << suffix << be_idt_nl
@@ -52,9 +52,9 @@ be_visitor_executor_ami_exh::visit_connector (be_connector *node)
       << node->local_name () << suffix << " (void);" << be_nl
       << "virtual ~" << node->local_name () << suffix
       << " (void);";
-      
+
   // TODO - visit scope and generate get_provides_*
-      
+
   os_ << be_nl << be_nl
       << "virtual void" << be_nl
       << "set_session_context (" << be_idt_nl
@@ -65,14 +65,14 @@ be_visitor_executor_ami_exh::visit_connector (be_connector *node)
       << "virtual void ccm_activate (void);" << be_nl
       << "virtual void ccm_passivate (void);" << be_nl
       << "virtual void ccm_remove (void);";
-      
+
   os_ << be_uidt_nl << be_nl
       << "private:" << be_idt_nl
       << smart_scope << scope->full_name () << "::CCM_"
       << node->local_name () << "_Context_var context_;" << be_nl;
-      
+
       // TODO - rest of members.
-      
+
   os_ << be_uidt_nl
       << "};";
 
@@ -80,7 +80,7 @@ be_visitor_executor_ami_exh::visit_connector (be_connector *node)
 }
 
 int
-be_visitor_executor_ami_exh::visit_provides (be_provides *node)
+be_visitor_executor_ami_exh::visit_provides (be_provides *)
 {
   return 0;
 }
