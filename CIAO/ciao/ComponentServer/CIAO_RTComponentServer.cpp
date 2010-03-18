0e5d87326891fed54c20fe559f06d22fe2fe5f7b
Johnny Willemsen <jwillemsen@remedy.nl>
2010-03-18 11:50:23 +0000

    Thu Mar 18 11:50:27 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ciao/ComponentServer/CIAO_ComponentServer.cpp:
            * ciao/ComponentServer/CIAO_RTComponentServer.cpp:
              Add more failure reporting to the user

--- CIAO/ciao/ComponentServer/CIAO_RTComponentServer.cpp
+++ CIAO/ciao/ComponentServer/CIAO_RTComponentServer.cpp
@@ -26,10 +26,15 @@ int ACE_TMAIN (int argc, ACE_TCHAR **argv)
       // Wait for the threads to exit.
       //ACE_Thread_Manager::instance ()->wait ();
     }
-  catch (CIAO::Deployment::RTComponentServer_Task::Error &e)
+  catch (const CIAO::Deployment::RTComponentServer_Task::Error &e)
     {
       CIAO_DEBUG (6, (LM_ALERT, CLINFO "CIAO_RTComponentServer main: Caught ComponentServer exception: %C\n",
                   e.err_.c_str ()));
+      retval = -1;
+    }
+  catch (const CORBA::Exception& ex)
+    {
+      ex._tao_print_exception ("CIAO_RTComponentServer main: Caught corba exception:");
       retval = -1;
     }
   catch (...)
