4ef7082e5ae5b40a2e9932bed39ee4e76b61a828
msmit <msmit@remedy.nl>
2010-03-22 08:55:31 +0000

    Mon Mar 22 08:52:11 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * bin/ciao_tests.lst:
            * connectors/dds4ccm/tests/QueryFilterListenEvent/*:
              Added Queryfilter test for the Listener

---
+++ CIAO/connectors/dds4ccm/tests/QueryFilterListenEvent/Receiver/QFLE_Test_Receiver_exec.h
@@ -0,0 +1,106 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_RECEIVER_EXEC_H_
+#define CIAO_RECEIVER_EXEC_H_
+
+#include "QFLE_Test_ReceiverEC.h"
+
+#include /**/ "Receiver_exec_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+#include "ace/OS_NS_Thread.h"
+#include "ace/Reactor.h"
+#include "ace/Task.h"
+
+#include <map>
+
+namespace CIAO_QFLE_Test_Receiver_Impl
+{
+  class Receiver_exec_i;
+
+  typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, ACE_thread_t> Atomic_ThreadId;
+
+  //============================================================
+  // QueryFilterListenEventTest_Listener_exec_i
+  //============================================================
+  class RECEIVER_EXEC_Export QueryFilterListenEventTest_Listener_exec_i
+    : public virtual ::QFLE_Test::QueryFilterListenEventTestConn::CCM_Listener,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    QueryFilterListenEventTest_Listener_exec_i (
+                            Atomic_ThreadId &thread_id);
+    virtual ~QueryFilterListenEventTest_Listener_exec_i (void);
+
+    virtual void
+    on_one_data (
+      const QueryFilterListenEventTest & an_instance,
+      const ::CCM_DDS::ReadInfo & info);
+    virtual void
+    on_many_data (
+      const QueryFilterListenEventTestSeq & an_instance,
+      const ::CCM_DDS::ReadInfoSeq & info);
+  private:
+    Atomic_ThreadId &thread_id_;
+  };
+
+  //============================================================
+  // Receiver_exec_i
+  //============================================================
+  class RECEIVER_EXEC_Export Receiver_exec_i
+    : public virtual Receiver_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Receiver_exec_i (void);
+    virtual ~Receiver_exec_i (void);
+
+    void start ();
+
+    virtual ::CORBA::UShort iterations (void);
+
+    virtual void iterations (::CORBA::UShort iterations);
+
+    virtual ::CORBA::UShort keys (void);
+
+    virtual void keys (::CORBA::UShort keys);
+
+    virtual ::QFLE_Test::QueryFilterListenEventTestConn::CCM_Listener_ptr
+    get_info_listen_data_listener (void);
+
+    virtual ::CCM_DDS::CCM_PortStatusListener_ptr
+    get_info_listen_status (void);
+
+    virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr
+    get_info_listen_connector_status (void);
+
+    virtual void
+    set_session_context (
+      ::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+  private:
+    ::QFLE_Test::CCM_Receiver_Context_var context_;
+    ::QFLE_Test::QueryFilterListenEventTestConn::Reader_var reader_;
+
+    Atomic_ThreadId thread_id_listener_;
+
+    CORBA::UShort iterations_;
+    CORBA::UShort keys_;
+  };
+
+  extern "C" RECEIVER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_QFLE_Test_Receiver_Impl (void);
+}
+
+#endif /* ifndef */
