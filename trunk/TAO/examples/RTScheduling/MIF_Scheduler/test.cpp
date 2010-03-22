7eea7024eecd431551591c2a4f14a833f265e0e1
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-03-22 09:51:40 +0000

    Mon Mar 22 09:49:04 UTC 2010  Vladimir Zykov  <vladimir.zykov@prismtech.com>
            * examples/RTScheduling/Fixed_Priority_Scheduler/test.cpp:
            * examples/RTScheduling/DT_Creator.cpp:
            * examples/RTScheduling/MIF_Scheduler/test.cpp:
              Fixed these examples on Windows builds.

--- trunk/TAO/examples/RTScheduling/MIF_Scheduler/test.cpp
+++ trunk/TAO/examples/RTScheduling/MIF_Scheduler/test.cpp
@@ -167,23 +167,26 @@ DT_Test::svc (void)
 int
 ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
+  int status = 0;
+
+  ACE_Service_Config::static_svcs ()->insert (&ace_svc_desc_MIF_DT_Creator);
+
   try
     {
-      ACE_Service_Config::static_svcs ()->insert (&ace_svc_desc_MIF_DT_Creator);
-
       ACE_DEBUG ((LM_DEBUG,
-         "%t\n"));
+         "%t Main Thread\n"));
       DT_TEST::instance ()->run (argc, argv);
-
     }
   catch (const CORBA::Exception& ex)
     {
       ex._tao_print_exception ("Caught exception:");
-      return 1;
+      status = 1;
     }
 
+  ACE_Service_Config::static_svcs ()->remove ("MIF_DT_Creator");
+
   ACE_DEBUG ((LM_DEBUG, "END\n"));
-  return 0;
+  return status;
 }
 
 #if defined (ACE_HAS_EXPLICIT_STATIC_TEMPLATE_MEMBER_INSTANTIATION)
