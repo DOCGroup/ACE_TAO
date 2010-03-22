0f0f25f0ed34755acd92a88dffad4b3a36e95222
Johnny Willemsen <jwillemsen@remedy.nl>
2010-03-22 10:40:04 +0000

    Mon Mar 20 10:39:37 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * TAO_IDL/be/be_visitor_connector/executor_ami_exh.cpp:
              Generate missing space when connector export macro is used

--- TAO/TAO_IDL/be/be_visitor_connector/executor_ami_exh.cpp
+++ TAO/TAO_IDL/be/be_visitor_connector/executor_ami_exh.cpp
@@ -37,7 +37,7 @@ be_visitor_executor_ami_exh::visit_connector (be_connector *node)
   AST_Decl *scope = ScopeAsDecl (node->defined_in ());
 
   os_ << be_nl << be_nl
-      << "class " << this->export_macro_.c_str ()
+      << "class " << this->export_macro_.c_str () << " "
       << scope->local_name () << suffix << be_idt_nl
       << ": public virtual " << "::"
       << scope->full_name () << "::CCM_"
@@ -85,24 +85,24 @@ be_visitor_executor_ami_exh::visit_connector (be_connector *node)
   ACE_CString connector_name (scope->local_name ()->get_string ());
   ACE_CString half_stripped_name (
     connector_name.substr (0, connector_name.find ("_Connector")));
-    
+
   /// And remember that the implied IDL interfaces are created
   /// in the same scope as the instantiated module.
   AST_Decl *i_scope = ScopeAsDecl (scope->defined_in ());
   bool global = (i_scope->node_type () == AST_Decl::NT_root);
   const char *smart_scope = (global ? "" : "::");
-  
+
   os_ << smart_scope << i_scope->full_name () << "::"
       << half_stripped_name.c_str ()
       << "Callback_var callback_;" << be_nl;
-      
-  /// Now we strip off the "AMI_" prefix.    
+
+  /// Now we strip off the "AMI_" prefix.
   ACE_CString stripped_name (
     half_stripped_name.substr (ACE_OS::strlen ("AMI_")));
-    
+
   os_ << smart_scope << i_scope->full_name () << "::"
       << stripped_name.c_str () << "_var receptacle_;" << be_nl;
-      
+
   os_ << half_stripped_name.c_str () <<  "_exec_i *facet_exec_;";
 
   os_ << be_uidt_nl
@@ -118,20 +118,20 @@ be_visitor_executor_ami_exh::visit_provides (be_provides *node)
   AST_Decl *scope = ScopeAsDecl (t->defined_in ());
   bool global = (scope->node_type () == AST_Decl::NT_root);
   const char *smart_scope = (global ? "" : "::");
-  
+
   os_ << be_nl << be_nl
       << "virtual " << smart_scope << scope->full_name ()
       << "::CCM_" << t->local_name () << "_ptr" << be_nl
       << "get_";
-      
+
   /// The port is the only thing in the connector's scope, and
-  /// we need to insert its name into the operation name.    
+  /// we need to insert its name into the operation name.
   UTL_ScopeActiveIterator i (this->node_, UTL_Scope::IK_decls);
   AST_Decl *d = i.item ();
-  
+
   os_ << d->local_name () << "_"
       << node->local_name () << " (void);";
-  
+
   return 0;
 }
 
