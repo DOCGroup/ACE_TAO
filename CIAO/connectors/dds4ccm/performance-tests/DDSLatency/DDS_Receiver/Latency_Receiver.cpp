479b6b15637ef029bc50e23dfaed96915f8d4027
msmit <msmit@remedy.nl>
2010-03-25 10:36:28 +0000

    Thu Mar 25 10:33:19 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/performance-tests/DDSLatency/DDS_Receiver/Latency_Receiver.cpp:
            * connectors/dds4ccm/performance-tests/DDSLatency/DDS_Sender/Latency_Sender.cpp:
              Layout changes.
              Addes -q as a commandline parameter. One can set the qos_profile name with
              this commandline parameter.
    
            * connectors/dds4ccm/performance-tests/DDSLatency/descriptors/USER_QOS_PROFILES.xml:
              Copied from the LatencyTT test.
    
            * connectors/dds4ccm/performance-tests/DDSLatency/descriptors/run_test_dds.pl:
              Changed this script in order to run more test with different
              qos names.

--- CIAO/connectors/dds4ccm/performance-tests/DDSLatency/DDS_Receiver/Latency_Receiver.cpp
+++ CIAO/connectors/dds4ccm/performance-tests/DDSLatency/DDS_Receiver/Latency_Receiver.cpp
@@ -4,51 +4,79 @@
 #include "tao/ORB_Core.h"
 #include "ace/High_Res_Timer.h"
 #include "ace/Env_Value_T.h"
+#include "ace/Tokenizer_T.h"
 #include "Latency_Base.h"
 #include "Latency_BaseSupport.h"
 #include "Latency_BasePlugin.h"
 
 #include <ndds/ndds_namespace_cpp.h>
 
-bool shutdown_flag = false;
+bool shutdown_flag_ = false;
 
 CORBA::LongLong count_ = 0; // total count of all received messages
 
-LatencyTest *instance =0;
-::DDS::Topic *send_topic = 0;
-::DDS::Topic *receive_topic = 0;
-::DDS::DataWriter *data_writer = 0;
-LatencyTestDataWriter *test_data_writer = 0;
+LatencyTest * instance_ =0;
+LatencyTestDataWriter * test_data_writer_ = 0;
 
-const char *lib_name = "HelloTest_Library";
-const char *prof_name = "LatencyQoS";
+const char * lib_name_ = 0;
+const char * prof_name_ = 0;
 
-CORBA::UShort domain_id = 0;
+CORBA::UShort domain_id_ = 0;
 
-  int
-  parse_args (int argc, ACE_TCHAR *argv[])
-  {
-    ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("d:O"));
-    int c;
+void
+split_qos (const char * qos)
+{
+  char* buf = const_cast <char *> (qos);
+  ACE_Tokenizer_T<char> tok (buf);
+  tok.delimiter_replace ('#', 0);
+  for (char *p = tok.next (); p; p = tok.next ())
+    {
+      if (!lib_name_)
+        {
+          lib_name_ = p;
+        }
+      else if (!prof_name_)
+        {
+          prof_name_ = p;
+        }
+    }
+  ACE_DEBUG ((LM_DEBUG, "Receiver : Found QoS profile %C %C\n",
+    lib_name_,
+    prof_name_));
+}
 
-    while ((c = get_opts ()) != -1)
+int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("q:d:O"));
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    {
       switch (c)
         {
-       case 'd':
-          domain_id  = ACE_OS::atoi (get_opts.opt_arg ());
+          case 'd':
+            domain_id_  = ACE_OS::atoi (get_opts.opt_arg ());
+            break;
+          case 'q':
+            {
+              const char * qos = get_opts.opt_arg ();
+              split_qos (qos);
+            }
           break;
-       case '?':
-        default:
-          ACE_ERROR_RETURN ((LM_ERROR,
-                             "usage: %s "
-                             "-d <domain_id >"
-                             "\n",
-                             argv [0]),
-                            -1);
+          case '?':
+          default:
+            ACE_ERROR_RETURN ((LM_ERROR,
+                                "usage:\n\n"
+                                "  -d <domain_id>\n"
+                                "  -q <QoS profile>\n"
+                                "\n"),
+                              -1);
         }
-    // Indicates sucessful parsing of the command line
-    return 0;
-  }
+    }
+  // Indicates sucessful parsing of the command line
+  return 0;
+}
 
 /* The listener of events and data from the middleware */
 class HelloListener: public ::DDS::DataReaderListener {
@@ -57,183 +85,192 @@ public:
 };
 
 
-  void
-  write_back (LatencyTest & an_instance)
-  {
-     ++count_;
-    // Change ping , so this receiever doesn't sent this message back again
-    an_instance.ping = 0L;
-    try
-      {
-        test_data_writer->write(an_instance, ::DDS::HANDLE_NIL);
-      }
-     catch (const CORBA::Exception& ex)
-          {
-            ex._tao_print_exception ("Exception caught:");
-             ACE_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: Internal Error ")
-                         ACE_TEXT ("while writing ping back.\n")));
-          }
+void
+write_back (LatencyTest & an_instance)
+{
+  ++count_;
+  // Change ping , so this receiever doesn't sent this message back again
+  an_instance.ping = 0L;
 
+  try
+    {
+      test_data_writer_->write(an_instance, ::DDS::HANDLE_NIL);
+    }
+  catch (const CORBA::Exception& ex)
+    {
+      ex._tao_print_exception ("Exception caught:");
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: Internal Error ")
+                        ACE_TEXT ("while writing ping back.\n")));
+    }
+}
+
+int ACE_TMAIN(int argc, ACE_TCHAR** argv)
+{
+  ::DDS::ReturnCode_t retcode;
+  HelloListener listener;
+  ::DDS::DataReader *data_reader = 0;
+  const char * type_name = 0;
+  ::DDS::Topic * send_topic = 0;
+  ::DDS::Topic * receive_topic = 0;
+  ::DDS::DataWriter * data_writer = 0;
+
+  ACE_Env_Value<int> id (ACE_TEXT("DDS4CCM_DEFAULT_DOMAIN_ID"), domain_id_);
+  domain_id_ = id;
+
+  int main_result = 1; /* error by default */
+  if (parse_args (argc, argv) != 0)
+    return 1;
+
+  /* Create the domain participant on domain ID 0 */
+  ::DDS::DomainParticipant *participant =
+              ::DDS::DomainParticipantFactory::get_instance()->
+                create_participant_with_profile(
+                                  domain_id_,
+                                  lib_name_,
+                                  prof_name_,
+                                  0,
+                                  DDS_STATUS_MASK_NONE);
+  if (!participant)
+    {
+      ACE_ERROR ((LM_ERROR,
+                  ACE_TEXT ("Receiver : Unable to create domain participant.\n")));
+      goto clean_exit;
+    }
+  /* Register type before creating topic */
+  type_name = LatencyTestTypeSupport::get_type_name ();
+  retcode = LatencyTestTypeSupport::register_type (participant, type_name);
+  if (retcode != DDS_RETCODE_OK)
+    {
+      ACE_ERROR ((LM_ERROR,
+                  ACE_TEXT ("Unable to register topic type.\n")));
+      goto clean_exit;
+    }
+
+  send_topic = participant->create_topic_with_profile (
+                      "send",
+                      type_name,
+                      lib_name_,
+                      prof_name_,
+                      0,
+                      DDS_STATUS_MASK_NONE);
+  if (!send_topic)
+    {
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create topic.\n")));
+      goto clean_exit;
+    }
+
+  receive_topic = participant->create_topic_with_profile (
+                      "receive",
+                      type_name,
+                      lib_name_,
+                      prof_name_,
+                      0,
+                      DDS_STATUS_MASK_NONE);
+  if (!receive_topic)
+    {
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create topic.\n")));
+      goto clean_exit;
+    }
+
+  /* Create the data reader using the default publisher */
+  data_reader = participant->create_datareader_with_profile(
+                                              send_topic,
+                                              lib_name_,
+                                              prof_name_,
+                                              &listener,
+                                              DDS_DATA_AVAILABLE_STATUS);
+
+  if (!data_reader )
+    {
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create data reader.\n")));
+      goto clean_exit;
+    }
+
+  /* Create the data writer using the default publisher */
+  data_writer = participant->create_datawriter_with_profile(
+                              receive_topic,
+                              lib_name_,
+                              prof_name_,
+                              0,
+                              DDS_STATUS_MASK_NONE);
+  if (!data_writer)
+    {
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create data writer.\n")));
+      goto clean_exit;
+    }
+
+  test_data_writer_ = LatencyTestDataWriter::narrow (data_writer);
+  if (!test_data_writer_)
+    {
+      ACE_ERROR ((LM_ERROR,
+                  ACE_TEXT ("DDS_StringDataWriter_narrow failed.\n")));
+      goto clean_exit;
+    }
+
+  /* --- Sleep During Asynchronous Reception ---------------------------- */
+
+  /* This thread sleeps forever. When a sample is received, RTI Data
+  * Distribution Service will call the on_data_available_callback function.
+  */
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Ready to read data.\n")));
+  for (;;)
+  {
+    ACE_OS::sleep (1);
+    if (shutdown_flag_)
+      {
+        break;
+      }
   }
 
-  int ACE_TMAIN(int argc, ACE_TCHAR** argv)
-  {
-    ::DDS::ReturnCode_t retcode;
-    HelloListener listener;
-    ::DDS::DataReader *data_reader = 0;
-    const char * type_name = 0;
+  /* --- Clean Up ------------------------------------------------------- */
 
-    ACE_Env_Value<int> id (ACE_TEXT("DDS4CCM_DEFAULT_DOMAIN_ID"), domain_id);
-    domain_id = id;
-
-    int main_result = 1; /* error by default */
-    if (parse_args (argc, argv) != 0)
-      return 1;
-
-    /* Create the domain participant on domain ID 0 */
-    ::DDS::DomainParticipant *participant =
-               ::DDS::DomainParticipantFactory::get_instance()->
-        create_participant_with_profile(
-                           domain_id,          /* Domain ID */
-                           lib_name,
-                           prof_name,           /* QoS */
-                           0,                   /* Listener */
-                           DDS_STATUS_MASK_NONE);
-    if (!participant)
-      {
-        ACE_ERROR ((LM_ERROR,
-                    ACE_TEXT ("Unable to create domain participant.\n")));
-        goto clean_exit;
-      }
-    /* Register type before creating topic */
-    type_name = LatencyTestTypeSupport::get_type_name();
-    retcode = LatencyTestTypeSupport::register_type(participant, type_name);
-      if (retcode != DDS_RETCODE_OK)
-        {
-          ACE_ERROR ((LM_ERROR,
-                      ACE_TEXT ("Unable to register topic type.\n")));
-          goto clean_exit;
-        }
-    send_topic = participant->create_topic(
-                        "send",            /* Topic name*/
-                        type_name,               /* Type name */
-                        DDS_TOPIC_QOS_DEFAULT,   /* Topic QoS */
-                        0,                       /* Listener  */
-                        DDS_STATUS_MASK_NONE);
-    if (!send_topic)
-    {
-      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create topic.\n")));
-      goto clean_exit;
-    }
-    receive_topic = participant->create_topic(
-                        "receive",            /* Topic name*/
-                        type_name,               /* Type name */
-                        DDS_TOPIC_QOS_DEFAULT,   /* Topic QoS */
-                        0,                       /* Listener  */
-                        DDS_STATUS_MASK_NONE);
-    if (!receive_topic)
-    {
-      ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create topic.\n")));
-      goto clean_exit;
-    }
-
-    /* Create the data reader using the default publisher */
-    data_reader = participant->create_datareader_with_profile(
-                                                 send_topic,
-                                                 lib_name,
-                                                 prof_name,    /* QoS */
-                                                 &listener,    /* Listener */
-                                                 DDS_DATA_AVAILABLE_STATUS);
-
-    if (!data_reader )
-      {
-         ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create data reader.\n")));
-         goto clean_exit;
-      }
-    /* Create the data writer using the default publisher */
-    data_writer = participant->create_datawriter_with_profile(
-                                receive_topic,
-                                lib_name,
-                                prof_name,             /* QoS */
-                                0,                     /* Listener */
-                                DDS_STATUS_MASK_NONE);
-    if (!data_writer)
-      {
-        ACE_ERROR ((LM_ERROR, ACE_TEXT ("Unable to create data writer.\n")));
-        goto clean_exit;
-      }
-    test_data_writer = LatencyTestDataWriter::narrow(data_writer);
-    if (!test_data_writer )
-      {
-        ACE_ERROR ((LM_ERROR,
-                    ACE_TEXT ("DDS_StringDataWriter_narrow failed.\n")));
-        goto clean_exit;
-     }
-
-    /* --- Sleep During Asynchronous Reception ---------------------------- */
-
-    /* This thread sleeps forever. When a sample is received, RTI Data
-     * Distribution Service will call the on_data_available_callback function.
-    */
-    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Ready to read data.\n")));
-    for (;;)
-    {
-      ACE_OS::sleep (1);
-      if(shutdown_flag)
-        {
-          break;
-        }
-    }
-
-    /* --- Clean Up ------------------------------------------------------- */
-
-    main_result = 0;
+  main_result = 0;
 clean_exit:
 
-    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Exiting.")));
-    if (count_ > 0)
-      {
-        ACE_DEBUG((LM_DEBUG, "SUMMARY RECEIVER:\n "
-                             "Number of samples received: %u \n",
-                             count_));
-      }
-    else
-      {
-        ACE_DEBUG((LM_DEBUG, "SUMMARY RECEIVER:\n "
-                           "No samples received\n "));
-      }
-    if (participant)
-      {
-        retcode = participant->delete_contained_entities();
-        if (retcode != DDS_RETCODE_OK)
-          {
-            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
-            main_result = 1;
-          }
-        retcode = ::DDS::DomainParticipantFactory::get_instance()->
-        delete_participant(participant);
-        if (retcode != DDS_RETCODE_OK)
-          {
-            ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
-            main_result = 1;
-          }
-      }
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Exiting.")));
+  if (count_ > 0)
+    {
+      ACE_DEBUG((LM_DEBUG, "SUMMARY RECEIVER:\n "
+                          "Number of samples received: %u \n",
+                          count_));
+    }
+  else
+    {
+      ACE_DEBUG((LM_DEBUG, "SUMMARY RECEIVER:\n "
+                        "No samples received\n "));
+    }
+  if (participant)
+    {
+      retcode = participant->delete_contained_entities ();
+      if (retcode != DDS_RETCODE_OK)
+        {
+          ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
+          main_result = 1;
+        }
+      retcode = ::DDS::DomainParticipantFactory::get_instance ()->
+                  delete_participant(participant);
+      if (retcode != DDS_RETCODE_OK)
+        {
+          ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
+          main_result = 1;
+        }
+    }
 
-    return main_result;
-  }
+  return main_result;
+}
 
-  /* This method gets called back by DDS when one or more data samples
-   *  have been received.
-   */
-  void HelloListener::on_data_available(::DDS::DataReader *reader)
-  {
-    /* Perform a safe type-cast from a generic data reader into a
-     * specific data reader for the type "LatencyTestDataReader"
-     */
-    LatencyTestDataReader * test_reader =
-                             LatencyTestDataReader::narrow(reader);
-    LatencyTest *instance = new LatencyTest;
-    if (!test_reader)
+/* This method gets called back by DDS when one or more data samples
+  *  have been received.
+  */
+void HelloListener::on_data_available(::DDS::DataReader *reader)
+{
+  /* Perform a safe type-cast from a generic data reader into a
+    * specific data reader for the type "LatencyTestDataReader"
+    */
+  LatencyTestDataReader * test_reader =
+                            LatencyTestDataReader::narrow (reader);
+  LatencyTest *instance = new LatencyTest;
+  if (!test_reader)
     {
       /* In this specific case, this will never fail */
       ACE_ERROR ((LM_ERROR,
@@ -241,29 +278,28 @@ clean_exit:
       return;
     }
 
-    /* Loop until there are messages available in the queue */
-     for(;;)
-       {
-         ::DDS::SampleInfo        info;
-         ::DDS::ReturnCode_t retcode = test_reader->take_next_sample(*instance,
-                                                                     info);
-         if (retcode == DDS_RETCODE_NO_DATA)
-           {
-             /*  No more samples */
-             break;
-           }
-         else if (retcode != DDS_RETCODE_OK)
-           {
-             ACE_ERROR ((LM_ERROR,
-                         ACE_TEXT ("Unable to take data from data reader,"
-                                   " error %d.\n"),
-                                   retcode));
-             return;
-           }
-         if (info.valid_data)
-           {
-               write_back(*instance);
-           }
+  /* Loop until there are messages available in the queue */
+  for(;;)
+    {
+      ::DDS::SampleInfo        info;
+      ::DDS::ReturnCode_t retcode = test_reader->take_next_sample(*instance,
+                                                                  info);
+      if (retcode == DDS_RETCODE_NO_DATA)
+        {
+          /*  No more samples */
+          break;
         }
-  }
-
+      else if (retcode != DDS_RETCODE_OK)
+        {
+          ACE_ERROR ((LM_ERROR,
+                      ACE_TEXT ("Unable to take data from data reader,"
+                                " error %d.\n"),
+                                retcode));
+          return;
+        }
+      if (info.valid_data)
+        {
+            write_back(*instance);
+        }
+    }
+}
