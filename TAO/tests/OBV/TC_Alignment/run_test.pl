ec0bd83284ff15cbf8af9d35d4291a8ea27c5c69
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2010-04-07 08:53:26 +0000

    Wed Apr  7 09:51:12 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * tests/Hang_Shutdown/run_test.pl
            * tests/Hang_Shutdown/server.cpp
            * tests/OBV/TC_Alignment/run_test.pl
            Hang_Shutdown:Pass ior filenmae through commandline.
            TC_Alignment: remove duplicate header.

--- TAO/tests/OBV/TC_Alignment/run_test.pl
+++ TAO/tests/OBV/TC_Alignment/run_test.pl
@@ -1,12 +1,4 @@
 eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
-     & eval 'exec perl -S $0 $argv:q'
-     if 0;
-
-# $Id$
-# -*- perl -*-
-
-use lib "$ENV{ACE_ROOT}/bin";
-eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
     & eval 'exec perl -S $0 $argv:q'
     if 0;
 
