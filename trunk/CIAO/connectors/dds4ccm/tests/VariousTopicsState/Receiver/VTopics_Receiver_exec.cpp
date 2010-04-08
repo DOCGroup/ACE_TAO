270bb5ba6a67270f8f2c0c2540970b49ef5c9896
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-04-08 11:31:44 +0000

    Thu Apr  8 11:28:53 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/VariousTopicsState/Receiver/VTopics_Receiver.idl:
            * connectors/dds4ccm/tests/VariousTopicsState/Receiver/VTopics_Receiver_exec.h:
            * connectors/dds4ccm/tests/VariousTopicsState/Receiver/VTopics_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/VariousTopicsState/Sender/VTopics_Sender.idl:
            * connectors/dds4ccm/tests/VariousTopicsState/Sender/VTopics_Sender_exec.h:
            * connectors/dds4ccm/tests/VariousTopicsState/Sender/VTopics_Sender_exec.cpp:
            * connectors/dds4ccm/tests/VariousTopicsState/descriptors/Plan.cdp:
              Solved test issues. In order to receive a subscription matched in the
              Receiver and a publication matched in the Sender, there must be connection
              between a datareader and a datawriter configured in the deployment plan.
              Sender should receive a publication matched since it only uses a
              publisher (and no subscriber).
              Layout changed.

--- trunk/CIAO/connectors/dds4ccm/tests/VariousTopicsState/Receiver/VTopics_Receiver_exec.cpp
+++ trunk/CIAO/connectors/dds4ccm/tests/VariousTopicsState/Receiver/VTopics_Receiver_exec.cpp
@@ -5,9 +5,6 @@
 //Test for different topic types , with DDS_STATE connector, look for 'SUBSCRIPTION_MATCHED_STATUS
 
 #include "VTopics_Receiver_exec.h"
-#include "ace/Log_Msg.h"
-#include "tao/ORB_Core.h"
-#include "ace/OS_NS_time.h"
 #include "dds4ccm/impl/dds/Utils.h"
 
 
@@ -16,8 +13,9 @@ namespace CIAO_VTopics_Receiver_Impl
 //============================================================
   // Facet Executor Implementation Class: ConnectorStatusListener_exec_i
   //============================================================
-  ConnectorStatusListener_exec_i::ConnectorStatusListener_exec_i (Atomic_Boolean &unexpected_matched)
-   : unexpected_matched_ (unexpected_matched)
+  ConnectorStatusListener_exec_i::ConnectorStatusListener_exec_i (
+    Atomic_Boolean &subscription_matched)
+    : subscription_matched_ (subscription_matched)
   {
   }
 
@@ -26,47 +24,52 @@ namespace CIAO_VTopics_Receiver_Impl
   }
 
   // Operations from ::CCM_DDS::ConnectorStatusListener
-  void ConnectorStatusListener_exec_i::on_inconsistent_topic(
+  void ConnectorStatusListener_exec_i::on_inconsistent_topic (
      ::DDS::Topic_ptr /*the_topic*/,
      const DDS::InconsistentTopicStatus & /*status*/)
-    {
-    }
+  {
+  }
 
-  void ConnectorStatusListener_exec_i::on_requested_incompatible_qos(
+  void ConnectorStatusListener_exec_i::on_requested_incompatible_qos (
     ::DDS::DataReader_ptr /*the_reader*/,
-     const DDS::RequestedIncompatibleQosStatus & /*status*/)  {
-    }
+     const DDS::RequestedIncompatibleQosStatus & /*status*/)
+  {
+  }
 
-  void ConnectorStatusListener_exec_i::on_sample_rejected(
-     ::DDS::DataReader_ptr /*the_reader*/,
-     const DDS::SampleRejectedStatus & /*status*/)  {
-    }
+  void ConnectorStatusListener_exec_i::on_sample_rejected (
+    ::DDS::DataReader_ptr /*the_reader*/,
+    const DDS::SampleRejectedStatus & /*status*/)
+  {
+  }
 
-  void ConnectorStatusListener_exec_i::on_offered_deadline_missed(
-     ::DDS::DataWriter_ptr /*the_writer*/,
-     const DDS::OfferedDeadlineMissedStatus & /*status*/)  {
-    }
+  void ConnectorStatusListener_exec_i::on_offered_deadline_missed (
+    ::DDS::DataWriter_ptr /*the_writer*/,
+    const DDS::OfferedDeadlineMissedStatus & /*status*/)
+  {
+  }
 
-  void ConnectorStatusListener_exec_i::on_offered_incompatible_qos(
-     ::DDS::DataWriter_ptr /*the_writer*/,
-     const DDS::OfferedIncompatibleQosStatus & /*status*/)  {
-    }
+  void ConnectorStatusListener_exec_i::on_offered_incompatible_qos (
+    ::DDS::DataWriter_ptr /*the_writer*/,
+    const DDS::OfferedIncompatibleQosStatus & /*status*/)
+  {
+  }
 
-  void ConnectorStatusListener_exec_i::on_unexpected_status(
+  void ConnectorStatusListener_exec_i::on_unexpected_status (
     ::DDS::Entity_ptr the_entity,
-    ::DDS::StatusKind  status_kind)  {
-      CORBA::ULong kind = status_kind;
-      if((! ::CORBA::is_nil(the_entity)) && (kind==DDS::SUBSCRIPTION_MATCHED_STATUS))
+    ::DDS::StatusKind  status_kind)
+  {
+    if (! ::CORBA::is_nil(the_entity) &&
+        status_kind == DDS::SUBSCRIPTION_MATCHED_STATUS)
       {
-        this->unexpected_matched_ = true;
+        this->subscription_matched_ = true;
       }
-    }
+  }
 
   //============================================================
-  // Component Executor Implementation Class: Receiver_exec_iTestTopic_RawListener_exec_i ();
+  // Receiver_exec_i
   //============================================================
   Receiver_exec_i::Receiver_exec_i (void)
-    : unexpected_matched_ (false)
+    : subscription_matched_ (false)
   {
   }
 
@@ -74,12 +77,23 @@ namespace CIAO_VTopics_Receiver_Impl
   {
   }
 
-  // Supported operations and attributes.
   // Port operations.
   ::CCM_DDS::CCM_ConnectorStatusListener_ptr
-  Receiver_exec_i::get_info_out_connector_status (void)
+  Receiver_exec_i::get_connector_status (void)
   {
-    return new ConnectorStatusListener_exec_i (this->unexpected_matched_);
+    return new ConnectorStatusListener_exec_i (this->subscription_matched_);
+  }
+
+  VTopics::TestTopicConn::CCM_Listener_ptr
+  Receiver_exec_i::get_info_out_data_listener (void)
+  {
+    return VTopics::TestTopicConn::CCM_Listener::_nil ();
+  }
+
+  CCM_DDS::CCM_PortStatusListener_ptr
+  Receiver_exec_i::get_info_out_status (void)
+  {
+    return CCM_DDS::CCM_PortStatusListener::_nil ();
   }
 
   // Operations from Components::SessionComponent.
@@ -113,16 +127,18 @@ namespace CIAO_VTopics_Receiver_Impl
   void
   Receiver_exec_i::ccm_remove (void)
   {
-     if(!this->unexpected_matched_.value ())
+     if(!this->subscription_matched_.value ())
       {
         ACE_ERROR ((LM_ERROR, ACE_TEXT ("ERROR: did not receive the expected ")
-                               ACE_TEXT ("states 'SUBSCRIPTION_MATCHED_STATUS  in Receiver\n")
+                              ACE_TEXT ("status 'SUBSCRIPTION_MATCHED_STATUS' ")
+                              ACE_TEXT ("in Receiver\n")
                     ));
       }
     else
       {
          ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Received the expected ")
-                               ACE_TEXT ("'SUBSCRIPTION_MATCHED_STATUS  in Receiver\n")
+                               ACE_TEXT ("'SUBSCRIPTION_MATCHED_STATUS' ")
+                               ACE_TEXT ("in Receiver\n")
                     ));
       }
   }
--- trunk/CIAO/connectors/dds4ccm/tests/VariousTopicsState/Sender/VTopics_Sender_exec.h
+++ trunk/CIAO/connectors/dds4ccm/tests/VariousTopicsState/Sender/VTopics_Sender_exec.h
@@ -15,14 +15,11 @@
 
 #include "tao/LocalObject.h"
 
-#include <map>
-
 namespace CIAO_VTopics_Sender_Impl
 {
   typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, CORBA::Boolean > Atomic_Boolean;
 
   class Sender_exec_i;
-
 
   class SENDER_EXEC_Export ConnectorStatusListener_exec_i
     : public virtual ::CCM_DDS::CCM_ConnectorStatusListener,
@@ -31,27 +28,27 @@ namespace CIAO_VTopics_Sender_Impl
     public:
     ConnectorStatusListener_exec_i (Atomic_Boolean &);
     virtual ~ConnectorStatusListener_exec_i (void);
-      
+
     virtual
-    void on_inconsistent_topic( ::DDS::Topic_ptr the_topic, 
+    void on_inconsistent_topic (::DDS::Topic_ptr the_topic,
                   const DDS::InconsistentTopicStatus & status);
     virtual
-    void on_requested_incompatible_qos( ::DDS::DataReader_ptr the_reader,
+    void on_requested_incompatible_qos (::DDS::DataReader_ptr the_reader,
                   const DDS::RequestedIncompatibleQosStatus & status);
     virtual
-    void on_sample_rejected( ::DDS::DataReader_ptr the_reader, 
+    void on_sample_rejected (::DDS::DataReader_ptr the_reader,
                   const DDS::SampleRejectedStatus & status);
     virtual
-    void on_offered_deadline_missed( ::DDS::DataWriter_ptr the_writer,
+    void on_offered_deadline_missed (::DDS::DataWriter_ptr the_writer,
                   const DDS::OfferedDeadlineMissedStatus & status);
     virtual
-    void on_offered_incompatible_qos( ::DDS::DataWriter_ptr the_writer, 
+    void on_offered_incompatible_qos (::DDS::DataWriter_ptr the_writer,
                   const DDS::OfferedIncompatibleQosStatus & status);
     virtual
-    void on_unexpected_status( ::DDS::Entity_ptr the_entity,
-      ::DDS::StatusKind  status_kind);
-     private:
-    Atomic_Boolean &unexpected_matched_;
+    void on_unexpected_status (::DDS::Entity_ptr the_entity,
+                  ::DDS::StatusKind  status_kind);
+  private:
+    Atomic_Boolean &publication_matched_;
   };
 
   class Sender_exec_i
@@ -61,7 +58,9 @@ namespace CIAO_VTopics_Sender_Impl
   public:
     Sender_exec_i (void);
     virtual ~Sender_exec_i (void);
+
     virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
     virtual void configuration_complete (void);
     virtual void ccm_activate (void);
     virtual void ccm_passivate (void);
@@ -69,13 +68,12 @@ namespace CIAO_VTopics_Sender_Impl
 
     // Port operations.
     virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr
-      get_test_topic_connector_status(void);
+    get_connector_status (void);
 
   private:
     ::VTopics::CCM_Sender_Context_var context_;
- 
-    Atomic_Boolean unexpected_matched_;
-     TAO_SYNCH_MUTEX mutex_;
+
+    Atomic_Boolean publication_matched_;
 };
 
   extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
