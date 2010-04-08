08914bb3666a66e0f9366fda6c560d07c6a0eb61
msmit <msmit@remedy.nl>
2010-04-08 10:19:43 +0000

    Thu Apr  8 10:15:07 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/VariousTopicsEvent/Receiver/VTopic_Receiver.idl:
            * connectors/dds4ccm/tests/VariousTopicsEvent/Receiver/VTopic_Receiver_exec.h:
            * connectors/dds4ccm/tests/VariousTopicsEvent/Receiver/VTopic_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/VariousTopicsEvent/Sender/VTopic_Sender.idl:
            * connectors/dds4ccm/tests/VariousTopicsEvent/Sender/VTopic_Sender_exec.h:
            * connectors/dds4ccm/tests/VariousTopicsEvent/Sender/VTopic_Sender_exec.cpp:
            * connectors/dds4ccm/tests/VariousTopicsEvent/descriptors/Plan.cdp:
              Solved test issues. In order to receive a subscription matched in the
              Receiver and a publication matched in the Sender, there must be connection
              between a datareader and a datawriter configured in the deployment plan.
              Sender should receive a publication matched since it only uses a
              publisher (and no subscriber).
              Layout changed.

--- CIAO/connectors/dds4ccm/tests/VariousTopicsEvent/Sender/VTopic_Sender_exec.h
+++ CIAO/connectors/dds4ccm/tests/VariousTopicsEvent/Sender/VTopic_Sender_exec.h
@@ -4,7 +4,6 @@
 #ifndef CIAO_SENDER_EXEC_H_
 #define CIAO_SENDER_EXEC_H_
 
-
 #include "VTopic_SenderEC.h"
 
 #include /**/ "Sender_exec_export.h"
@@ -15,14 +14,11 @@
 
 #include "tao/LocalObject.h"
 
-#include <map>
-
 namespace CIAO_VTopic_Sender_Impl
 {
   typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, CORBA::Boolean > Atomic_Boolean;
 
   class Sender_exec_i;
-
 
   class SENDER_EXEC_Export ConnectorStatusListener_exec_i
     : public virtual ::CCM_DDS::CCM_ConnectorStatusListener,
@@ -30,28 +26,28 @@ namespace CIAO_VTopic_Sender_Impl
   {
   public:
     ConnectorStatusListener_exec_i (Atomic_Boolean &);
-   virtual ~ConnectorStatusListener_exec_i (void);
-    
+    virtual ~ConnectorStatusListener_exec_i (void);
+
     virtual
-    void on_inconsistent_topic( ::DDS::Topic_ptr the_topic, 
+    void on_inconsistent_topic (::DDS::Topic_ptr the_topic,
                                 const DDS::InconsistentTopicStatus & status);
     virtual
-    void on_requested_incompatible_qos( ::DDS::DataReader_ptr the_reader,
+    void on_requested_incompatible_qos (::DDS::DataReader_ptr the_reader,
                                         const DDS::RequestedIncompatibleQosStatus & status);
     virtual
-    void on_sample_rejected( ::DDS::DataReader_ptr the_reader, 
+    void on_sample_rejected (::DDS::DataReader_ptr the_reader,
                              const DDS::SampleRejectedStatus & status);
     virtual
-    void on_offered_deadline_missed( ::DDS::DataWriter_ptr the_writer,
+    void on_offered_deadline_missed (::DDS::DataWriter_ptr the_writer,
                                      const DDS::OfferedDeadlineMissedStatus & status);
     virtual
-    void on_offered_incompatible_qos( ::DDS::DataWriter_ptr the_writer, 
+    void on_offered_incompatible_qos (::DDS::DataWriter_ptr the_writer,
                                       const DDS::OfferedIncompatibleQosStatus & status);
     virtual
-    void on_unexpected_status( ::DDS::Entity_ptr the_entity,
-       ::DDS::StatusKind  status_kind);
-     private:
-    Atomic_Boolean &unexpected_matched_;
+    void on_unexpected_status (::DDS::Entity_ptr the_entity,
+                               ::DDS::StatusKind  status_kind);
+  private:
+    Atomic_Boolean &publication_matched_;
   };
 
   class Sender_exec_i
@@ -61,7 +57,9 @@ namespace CIAO_VTopic_Sender_Impl
   public:
     Sender_exec_i (void);
     virtual ~Sender_exec_i (void);
+
     virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
     virtual void configuration_complete (void);
     virtual void ccm_activate (void);
     virtual void ccm_passivate (void);
@@ -69,13 +67,12 @@ namespace CIAO_VTopic_Sender_Impl
 
     // Port operations.
     virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr
-      get_test_topic_connector_status(void);
+    get_connector_status(void);
 
   private:
     ::VTopic::CCM_Sender_Context_var context_;
- 
-    Atomic_Boolean unexpected_matched_;
-     TAO_SYNCH_MUTEX mutex_;
+
+    Atomic_Boolean publication_matched_;
 };
 
   extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
