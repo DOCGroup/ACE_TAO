f61855cb3c33d43d816696dcd99822faedde675c
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-04-09 11:41:21 +0000

    Fri Apr  9 11:42:31 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/IDL3/Connectors:
              New test to validate that connectors don't support consumes/emits/
              publishes
    
            * bin/ciao_tests.lst:
              Added new IDL3/Connectors test. This executes tao_idl and validates
              that it fails

---
+++ trunk/CIAO/tests/IDL3/Connectors/run_test.pl
@@ -0,0 +1,68 @@
+
+# $Id$
+
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+     & eval 'exec perl -S $0 $argv:q'
+     if 0;
+
+# -*- perl -*-
+
+use lib "$ENV{ACE_ROOT}/bin";
+use PerlACE::TestTarget;
+use File::Spec;
+
+$status =0;
+
+open (OLDOUT, ">&STDOUT");
+open (STDOUT, ">" . File::Spec->devnull());
+open (OLDERR, ">&STDERR");
+open (STDERR, ">&STDOUT");
+
+# The location of the tao_idl utility - depends on O/S
+if ($^O eq "MSWin32"){
+   $tao_idl = "../../../../../bin/tao_idl";
+}
+else{
+   $tao_idl = "../../../../..TAO/TAO_IDL/tao_idl";
+}
+
+my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+
+$input_file1 = $server->LocalFile ("incorrect_publishes.idl");
+$input_file2 = $server->LocalFile ("incorrect_emits.idl");
+$input_file3 = $server->LocalFile ("incorrect_consumes.idl");
+
+# Compile the IDL
+$SV = $server->CreateProcess ("$tao_idl", "$input_file1");
+
+$server_status1 = $SV->SpawnWaitKill ($server->ProcessStartWaitInterval());
+
+# Compile the IDL
+$SV = $server->CreateProcess ("$tao_idl", "$input_file2");
+
+$server_status2 = $SV->SpawnWaitKill ($server->ProcessStartWaitInterval());
+
+# Compile the IDL
+$SV = $server->CreateProcess ("$tao_idl", "$input_file3");
+
+$server_status3 = $SV->SpawnWaitKill ($server->ProcessStartWaitInterval());
+
+open (STDOUT, ">&OLDOUT");
+open (STDERR, ">&OLDERR");
+
+if ($server_status1 != 1) {
+    print STDERR "ERROR: tao returned $server_status1 for $input_file1, should have failed\n";
+    $status = 1;
+}
+
+if ($server_status2 != 1) {
+    print STDERR "ERROR: tao returned $server_status2 for $input_file2, should have failed\n";
+    $status = 1;
+}
+
+if ($server_status3 != 1) {
+    print STDERR "ERROR: tao returned $server_status3 for $input_file3, should have failed\n";
+    $status = 1;
+}
+
+exit $status;
