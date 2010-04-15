5bb9295a96224b29f2a2f86d4a697e948a6afae9
msmit <msmit@remedy.nl>
2010-04-15 07:25:08 +0000

    Thu Apr 15 07:21:02 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/dds/DataReaderListener.h:
            * connectors/dds4ccm/impl/dds/DataReaderListener.cpp:
            * connectors/dds4ccm/impl/dds/DataWriterListener.h:
            * connectors/dds4ccm/impl/dds/DataWriterListener.cpp:
              Stored pointer to our proxy as class variable. The
              proxy is reused.
    
            * connectors/dds4ccm/impl/dds/PublisherListener_T.cpp:
              Optimized logging.

--- CIAO/connectors/dds4ccm/impl/dds/DataWriterListener.h
+++ CIAO/connectors/dds4ccm/impl/dds/DataWriterListener.h
@@ -64,6 +64,10 @@ namespace CIAO
       ::DDS::DataWriterListener_ptr get_datawriterlistener (void);
     private:
       ::DDS::DataWriterListener_var impl_;
+      ::DDS::DataWriter_var dds_writer_;
+
+      ::DDS::DataWriter_ptr
+      get_datawriter_proxy (::DDSDataWriter * the_writer);
     };
   }
 }
