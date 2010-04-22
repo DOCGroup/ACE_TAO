80ac7ee528d2ef4f2cdee572b3fca1b555b0e39b
Steve Huston <shuston@riverace.com>
2010-04-22 23:33:36 +0000

    ChangeLogTag:Thu Apr 22 19:33:15 EDT 2010  Steve Huston  <shuston@riverace.com>

---
+++ contrib/utility/Utility/ExH/ExH.hpp
@@ -0,0 +1,22 @@
+// file      : Utility/ExH/ExH.hpp
+// author    : Boris Kolpackov <boris@kolpackov.net>
+// copyright : Copyright (c) 2002-2003 Boris Kolpackov
+// license   : http://kolpackov.net/license.html
+
+#ifndef UTILITY_EX_H_EX_H_HPP
+#define UTILITY_EX_H_EX_H_HPP
+
+#include "Utility/ExH/System/Exception.hpp"
+#include "Utility/ExH/System/DescriptiveException.hpp"
+
+#include "Utility/ExH/Logic/Exception.hpp"
+#include "Utility/ExH/Logic/DescriptiveException.hpp"
+
+#include "Utility/ExH/Compound.hpp"
+
+#include "Utility/ExH/Converter.hpp"
+#include "Utility/ExH/StringStreamConverter.hpp"
+
+#endif  // UTILITY_EX_H_EX_H_HPP
+
+//$Id$
