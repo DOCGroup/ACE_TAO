4442521c307269cae20c91ac948cd22bd87f5df5
mhengstmengel <mhengstmengel@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-04-28 13:16:47 +0000

    Wed Apr 28 14:15:13 UTC 2010  Marijke Hengstmengel <mhengstmengel@remedy.nl>
    	* connectors/ami4ccm/tests/InterInArgs
    	* connectors/ami4ccm/tests/InterInArgs/Base
    	* connectors/ami4ccm/tests/InterInArgs/Base/InterInArgsT.idl
    	* connectors/ami4ccm/tests/InterInArgs/Base/InterInArgsT.mpc
    	* connectors/ami4ccm/tests/InterInArgs/descriptors
    	* connectors/ami4ccm/tests/InterInArgs/descriptors/Plan.cdp
    	* connectors/ami4ccm/tests/InterInArgs/descriptors/run_test.pl
    	* connectors/ami4ccm/tests/InterInArgs/Receiver
    	* connectors/ami4ccm/tests/InterInArgs/Receiver/InterInArgsT_Receiver.idl
    	* connectors/ami4ccm/tests/InterInArgs/Receiver/InterInArgsT_Receiver.mpc
    	* connectors/ami4ccm/tests/InterInArgs/Receiver/InterInArgsT_Receiver_exec.cpp
    	* connectors/ami4ccm/tests/InterInArgs/Receiver/InterInArgsT_Receiver_exec.h
    	* connectors/ami4ccm/tests/InterInArgs/Sender
    	* connectors/ami4ccm/tests/InterInArgs/Sender/InterInArgsT_Sender.idl
    	* connectors/ami4ccm/tests/InterInArgs/Sender/InterInArgsT_Sender.mpc
    	* connectors/ami4ccm/tests/InterInArgs/Sender/InterInArgsT_Sender_exec.cpp
    	* connectors/ami4ccm/tests/InterInArgs/Sender/InterInArgsT_Sender_exec.h
    	* connectors/ami4ccm/tests/InterOutArgs
    	* connectors/ami4ccm/tests/InterOutArgs/Base
    	* connectors/ami4ccm/tests/InterOutArgs/Base/InterOutArgsT.idl
    	* connectors/ami4ccm/tests/InterOutArgs/Base/InterOutArgsT.mpc
    	* connectors/ami4ccm/tests/InterOutArgs/descriptors
    	* connectors/ami4ccm/tests/InterOutArgs/descriptors/Plan.cdp
    	* connectors/ami4ccm/tests/InterOutArgs/descriptors/run_test.pl
    	* connectors/ami4ccm/tests/InterOutArgs/Receiver
    	* connectors/ami4ccm/tests/InterOutArgs/Receiver/InterOutArgsT_Receiver.idl
    	* connectors/ami4ccm/tests/InterOutArgs/Receiver/InterOutArgsT_Receiver.mpc
    	* connectors/ami4ccm/tests/InterOutArgs/Receiver/InterOutArgsT_Receiver_exec.cpp
    	* connectors/ami4ccm/tests/InterOutArgs/Receiver/InterOutArgsT_Receiver_exec.h
    	* connectors/ami4ccm/tests/InterOutArgs/Sender
    	* connectors/ami4ccm/tests/InterOutArgs/Sender/InterOutArgsT_Sender.idl
    	* connectors/ami4ccm/tests/InterOutArgs/Sender/InterOutArgsT_Sender.mpc
    	* connectors/ami4ccm/tests/InterOutArgs/Sender/InterOutArgsT_Sender_exec.cpp
    	* connectors/ami4ccm/tests/InterOutArgs/Sender/InterOutArgsT_Sender_exec.h
    	AMI4CCM tests for interfaces with different in- and out arguments.

---
+++ CIAO/connectors/ami4ccm/tests/InterOutArgs/Receiver/InterOutArgsT_Receiver_exec.h
@@ -0,0 +1,60 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_INTEROUTARGS_T_RECEIVER_EXEC_H_
+#define CIAO_INTEROUTARGS_T_RECEIVER_EXEC_H_
+
+#include "InterOutArgsT_ReceiverEC.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+#include "InterOutArgsT_Receiver_exec_export.h"
+
+namespace CIAO_InterOutArgsT_Receiver_Impl
+{
+  class MyFoo_exec_i
+    : public virtual ::InterOutArgsT::CCM_MyFoo,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    MyFoo_exec_i (void);
+    virtual ~MyFoo_exec_i (void);
+    virtual void var_outs (const char * in_str,  ::CORBA::String_out answer,
+      ::CORBA::Double_out d_cmd, ::CORBA::Long_out l_cmd);
+    virtual void var_div_outs (InterOutArgsT::TestTopic_out test_topic,
+                               InterOutArgsT::TopicString_out topic_str,
+                               InterOutArgsT::TopicArray_out topic_arr);
+    virtual void var_div2_outs (InterOutArgsT::X_Union_out x_uni,
+                                InterOutArgsT::test_seq_out seq);
+  };
+
+  class  Receiver_exec_i
+    : public virtual Receiver_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Receiver_exec_i (void);
+    virtual ~Receiver_exec_i (void);
+
+    virtual ::InterOutArgsT::CCM_MyFoo_ptr get_do_my_foo (void);
+
+    virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+  private:
+    ::InterOutArgsT::CCM_Receiver_Context_var context_;
+  };
+
+  extern "C" INTEROUTARGS_T_RECEIVER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_InterOutArgsT_AMI_Receiver_Impl (void);
+}
+
+#endif /* ifndef */
