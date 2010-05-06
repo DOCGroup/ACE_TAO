d9a7852f4baa7fbc656008af54b981a36a609a87
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-05-06 15:33:47 +0000

    ChangeLogTag: Thu May  6 15:30:43 UTC 2010  Jeff Parsons <j.parsons@vanderbilt.edu>

--- TAO/TAO_IDL/be/be_visitor_connector/connector_dds_ex_base.cpp
+++ TAO/TAO_IDL/be/be_visitor_connector/connector_dds_ex_base.cpp
@@ -17,7 +17,8 @@ be_visitor_connector_dds_ex_base::be_visitor_connector_dds_ex_base (
       be_visitor_context *ctx)
   : be_visitor_component_scope (ctx),
     t_inst_ (0),
-    t_args_ (0)
+    t_args_ (0),
+    base_tname_ (0)
 {
 }
 
@@ -25,13 +26,61 @@ be_visitor_connector_dds_ex_base::~be_visitor_connector_dds_ex_base (void)
 {
 }
 
+bool
+be_visitor_connector_dds_ex_base::begin (be_connector *node)
+{
+  node_ = node;
+
+  AST_Connector *base = node->base_connector ();
+
+  this->process_template_args (base);
+
+  /// If the previous call hasn't initialized our arg
+  /// list, it could be because we were processing
+  /// DDS_State or DDS_Event, whose base class,
+  /// DDS_TopicBase, is not in a template module.
+  /// To check this, we call again with the node itself,
+  /// and if we get an arg list this time, proceed.
+  if (this->t_args_ == 0)
+    {
+      this->process_template_args (node);
+      
+      if (this->t_args_ == 0)
+        {
+          return false;
+        }
+    }
+
+  /// CIDL-generated namespace used 'CIDL_' + composition name.
+  /// Now we use 'CIAO_' + component's flat name.
+  os_ << be_nl << be_nl
+      << "namespace CIAO_" << node->flat_name ()
+      << "_Impl" << be_nl
+      << "{" << be_idt;
+
+  ACE_CString lname = node->local_name ();  
+  const char *base_tname = 0;
+  
+  if (lname == "DDS_Event" || lname == "DDS_State")
+    {
+      this->base_tname_ = node->local_name ();  
+    }
+  else
+    {
+      this->base_tname_ =
+        node->base_connector ()->local_name ()->get_string ();
+    }
+    
+  return true;
+}
+
 void
 be_visitor_connector_dds_ex_base::process_template_args (
-  AST_Connector *base)
+  AST_Connector *node)
 {
   AST_Decl *d =
-    node_->defined_in ()->lookup_by_name (
-      ScopeAsDecl (base->defined_in ())->name (),
+    this->node_->defined_in ()->lookup_by_name (
+      ScopeAsDecl (node->defined_in ())->name (),
       true);
 
   // The template module instantiation and the corresponding
