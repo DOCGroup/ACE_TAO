3c1fef619af04f871506a54e22bc68f286e9b4b2
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2010-05-11 12:41:32 +0000

    Tue May  11 13:39:20 UTC 2010  Marijke Hengstmengel <mhengstmengel@remedy.nl>
            * connectors/ami4ccm/tests/OneProcess
            * connectors/ami4ccm/tests/OneProcess/Base
            * connectors/ami4ccm/tests/OneProcess/Base/OneProcess.idl
            * connectors/ami4ccm/tests/OneProcess/Base/OneProcess.mpc
            * connectors/ami4ccm/tests/OneProcess/descriptors
            * connectors/ami4ccm/tests/OneProcess/descriptors/Plan.cdp
            * connectors/ami4ccm/tests/OneProcess/descriptors/PlanOne.cdp
            * connectors/ami4ccm/tests/OneProcess/descriptors/run_test.pl
            * connectors/ami4ccm/tests/OneProcess/descriptors/run_test_one.pl
            * connectors/ami4ccm/tests/OneProcess/Receiver
            * connectors/ami4ccm/tests/OneProcess/Receiver/OneProcess_Receiver.idl
            * connectors/ami4ccm/tests/OneProcess/Receiver/OneProcess_Receiver.mpc
            * connectors/ami4ccm/tests/OneProcess/Receiver/OneProcess_Receiver_exec.cpp
            * connectors/ami4ccm/tests/OneProcess/Receiver/OneProcess_Receiver_exec.h
            * connectors/ami4ccm/tests/OneProcess/Sender
            * connectors/ami4ccm/tests/OneProcess/Sender/OneProcess_Sender.idl
            * connectors/ami4ccm/tests/OneProcess/Sender/OneProcess_Sender.mpc
            * connectors/ami4ccm/tests/OneProcess/Sender/OneProcess_Sender_exec.cpp
            * connectors/ami4ccm/tests/OneProcess/Sender/OneProcess_Sender_exec.h
            Test with Client, AMI-Connector and Receievr in one node.

---
+++ CIAO/connectors/ami4ccm/tests/OneProcess/Receiver/OneProcess_Receiver_exec.cpp
@@ -0,0 +1,92 @@
+// -*- C++ -*-
+// $Id$
+
+#include "OneProcess_Receiver_exec.h"
+#include "ace/OS_NS_unistd.h"
+
+namespace CIAO_OneProcess_Receiver_Impl
+{
+  MyFoo_exec_i::MyFoo_exec_i (void)
+  {
+  }
+
+  MyFoo_exec_i::~MyFoo_exec_i (void)
+  {
+  }
+
+  ::CORBA::Long
+  MyFoo_exec_i::foo (const char * in_str,::CORBA::Long cmd,
+                     ::CORBA::String_out answer)
+  {
+    if (cmd == 1)
+      {
+        ACE_OS::sleep(2);
+        answer = CORBA::string_dup ("ASYNCH answer");
+      }
+    if (cmd == 2)
+      {
+        answer = CORBA::string_dup ("SYNCH answer");
+      }
+    return cmd;
+  }
+
+  Receiver_exec_i::Receiver_exec_i (void)
+  {
+  }
+
+  Receiver_exec_i::~Receiver_exec_i (void)
+  {
+  }
+
+  ::OneProcess::CCM_MyFoo_ptr
+  Receiver_exec_i::get_do_my_foo (void)
+  {
+    return new MyFoo_exec_i ();
+  }
+
+  void
+  Receiver_exec_i::set_session_context (
+    ::Components::SessionContext_ptr ctx)
+  {
+    this->context_ = ::OneProcess::CCM_Receiver_Context::_narrow (ctx);
+
+    if ( ::CORBA::is_nil (this->context_.in ()))
+      {
+        throw ::CORBA::INTERNAL ();
+      }
+  }
+
+  void
+  Receiver_exec_i::configuration_complete (void)
+  {
+  }
+
+  void
+  Receiver_exec_i::ccm_activate (void)
+  {
+  }
+
+  void
+  Receiver_exec_i::ccm_passivate (void)
+  {
+  }
+
+  void
+  Receiver_exec_i::ccm_remove (void)
+  {
+  }
+
+  extern "C"  ::Components::EnterpriseComponent_ptr
+  create_OneProcess_AMI_Receiver_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_RETURN (
+      retval,
+      Receiver_exec_i,
+      ::Components::EnterpriseComponent::_nil ());
+
+    return retval;
+  }
+}
