5a3dde9fd0054f4bb7e969ab3747aaa85f2d025c
Adam Mitz <mitza-oci@users.noreply.github.com>
2010-05-18 21:50:52 +0000

    ChangeLogTag: Tue May 18 21:48:11 UTC 2010  Adam Mitz  <mitza@ociweb.com>

--- CIAO/DAnCE/tests/NameServiceTest/srv_main.cpp
+++ CIAO/DAnCE/tests/NameServiceTest/srv_main.cpp
@@ -1,7 +1,7 @@
 // $Id$
 #include "ace/OS_NS_stdio.h"
-#include <orbsvcs/orbsvcs/CosNamingC.h>
-#include <orbsvcs/orbsvcs/Naming/Naming_Loader.h>
+#include <orbsvcs/CosNamingC.h>
+#include <orbsvcs/Naming/Naming_Loader.h>
 #include "DAnCE/Logger/Log_Macros.h"
 
 #include "DummyC.h"
