5f0adbc62f15bec748292e30964a530960fd36aa
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-05-19 09:53:05 +0000

    ChangeLogTag: Wed May 19 09:52:30 UTC 2010  Simon Massey  <sma at prismtech dot com>

--- TAO/tests/IDL_Test/run_test.pl
+++ TAO/tests/IDL_Test/run_test.pl
@@ -7,16 +7,44 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 
 use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::TestTarget;
+use File::Spec;
 
+my $status =0;
 my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
-
 $SV = $server->CreateProcess ("main");
-
 $test = $SV->SpawnWaitKill ($server->ProcessStartWaitInterval());
-
 if ($test != 0) {
     print STDERR "ERROR: test returned $test\n";
-    exit 1;
+    $status= 1;
 }
 
-exit 0;
+#Redirect the screen output to the null device.
+open (OLDOUT, ">&STDOUT");
+open (STDOUT, ">" . File::Spec->devnull());
+open (OLDERR, ">&STDERR");
+open (STDERR, ">&STDOUT");
+
+# The location of the tao_idl utility - depends on O/S
+if ($^O eq "MSWin32"){
+   $tao_idl = "../../bin/tao_idl";
+}
+else{
+   $tao_idl = "../TAO_IDL/tao_idl";
+}
+
+my $server2 = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
+my $input_file1 = $server2->LocalFile ("invalid_scoping.idl");
+
+# Compile the IDL
+$SV = $server2->CreateProcess ("$tao_idl", "$input_file1");
+$test = $SV->SpawnWaitKill ($server2->ProcessStartWaitInterval());
+
+#Redirect the null device output back to the screen
+open (STDOUT, ">&OLDOUT");
+open (STDERR, ">&OLDERR");
+if ($test== 0) {
+    print STDERR "ERROR: tao_idl returned $test for $input_file1, should have failed!\n";
+    $status = 1;
+}
+
+exit $status;
