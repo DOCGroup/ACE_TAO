268bea4bc13104e5ec5e1a050ff5f28903bbff76
msmit <msmit@remedy.nl>
2010-05-31 14:01:04 +0000

    Mon May 31 13:58:17 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * bin/ciao_tests.lst:
            * connectors/dds4ccm/tests/UserdataCopyCount:
            * connectors/dds4ccm/tests/UserdataCopyCount/Base:
            * connectors/dds4ccm/tests/UserdataCopyCount/Base/UCC_Test_Base.idl:
            * connectors/dds4ccm/tests/UserdataCopyCount/Base/UCC_Test_Base.mpc:
            * connectors/dds4ccm/tests/UserdataCopyCount/Component:
            * connectors/dds4ccm/tests/UserdataCopyCount/Component/UCC_Test_Component.idl:
            * connectors/dds4ccm/tests/UserdataCopyCount/Component/UCC_Test_Component.mpc:
            * connectors/dds4ccm/tests/UserdataCopyCount/Component/UCC_Test_Component_exec.h:
            * connectors/dds4ccm/tests/UserdataCopyCount/Component/UCC_Test_Component_exec.cpp:
            * connectors/dds4ccm/tests/UserdataCopyCount/Connector:
            * connectors/dds4ccm/tests/UserdataCopyCount/Connector/UCC_Test_Connector.idl:
            * connectors/dds4ccm/tests/UserdataCopyCount/Connector/UCC_Test_Connector.mpc:
            * connectors/dds4ccm/tests/UserdataCopyCount/descriptors:
            * connectors/dds4ccm/tests/UserdataCopyCount/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/UserdataCopyCount/descriptors/run_test.pl:
              Added test which counts how many times a userdata struct is copied.

---
+++ CIAO/connectors/dds4ccm/tests/UserdataCopyCount/Component/UCC_Test_Component_exec.h
@@ -0,0 +1,103 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_COMPONENT_EXEC_H_
+#define CIAO_COMPONENT_EXEC_H_
+
+#include "UCC_Test_ComponentEC.h"
+
+#include /**/ "Component_exec_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+
+#include <map>
+
+namespace CIAO_UCC_Test_UCCTestComponent_Impl
+{
+  class Component_exec_i;
+
+  //============================================================
+  // WriteVariableHandler
+  //============================================================
+  class WriteVariableHandler :
+    public ACE_Event_Handler
+  {
+  public:
+    WriteVariableHandler (Component_exec_i &callback);
+    int handle_timeout (const ACE_Time_Value &, const void *);
+  private:
+    /// Maintains a handle that actually process the event
+    Component_exec_i &callback_;
+  };
+
+  //============================================================
+  // WriteFixedHandler
+  //============================================================
+  class WriteFixedHandler :
+    public ACE_Event_Handler
+  {
+  public:
+    WriteFixedHandler (Component_exec_i &callback);
+    int handle_timeout (const ACE_Time_Value &, const void *);
+  private:
+    /// Maintains a handle that actually process the event
+    Component_exec_i &callback_;
+  };
+
+  //============================================================
+  // Component_exec_i
+  //============================================================
+  class COMPONENT_EXEC_Export Component_exec_i
+    : public virtual UCCTestComponent_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Component_exec_i (void);
+    virtual ~Component_exec_i (void);
+
+    // Operations from Components::SessionComponent.
+    virtual void
+    set_session_context (
+      ::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+    void write_variable (void);
+
+    void write_fixed (void);
+
+  private:
+    ::UCC_Test::CCM_UCCTestComponent_Context_var context_;
+
+
+    ::UCCVariableSizedStruct_Test::UCCVariableSizedStructTestConnector::Writer_var var_writer_;
+    void create_variable_sized_samples (void);
+    WriteVariableHandler *var_rh_;
+
+    TAO_SYNCH_MUTEX var_mutex_;
+    typedef std::map< ACE_CString, UCCVariableSizedStructTest_var > UCCVarSized_Table;
+    UCCVarSized_Table var_samples_;
+
+    ::UCCFixedSizeStruct_Test::UCCFixedSizedStructTestConnector::Writer_var fixed_writer_;
+    void create_fixed_sized_samples (void);
+    WriteFixedHandler *fixed_rh_;
+
+    TAO_SYNCH_MUTEX fixed_mutex_;
+    typedef std::map< ::CORBA::UShort, UCCFixedSizedStructTest_var > UCCFixedSized_Table;
+    UCCFixedSized_Table fixed_samples_;
+  };
+
+  extern "C" COMPONENT_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_UCCTest_Component_Impl (void);
+}
+
+#endif /* ifndef */
+
