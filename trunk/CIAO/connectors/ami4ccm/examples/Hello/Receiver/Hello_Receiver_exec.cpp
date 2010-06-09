e631a14949c27bd2bbad46a9572e81ec9ab1862f
mhengstmengel <mhengstmengel@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-06-09 13:49:46 +0000

    Wed Jun  9 13:49:56 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/ami4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp
            * connectors/ami4ccm/examples/Hello/Sender/Hello_Sender_exec.cpp
            Cleanup code. Remove printf's.

--- trunk/CIAO/connectors/ami4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp
+++ trunk/CIAO/connectors/ami4ccm/examples/Hello/Receiver/Hello_Receiver_exec.cpp
@@ -19,13 +19,11 @@ namespace CIAO_Hello_Receiver_Impl
   {
     if (ACE_OS::strlen (in_str) == 0)
       {
-        printf ("Receiver (FOO) :\tEMPTY string received -> throw internal excep with id <42> and errorstring <Hello World>\n");
         Hello::InternalError ex (42, "Hello world");
         throw ex;
       }
     else
       {
-        printf ("Receiver (FOO) :\tReceived string <%s>\n", in_str);
         ACE_OS::sleep (ACE_OS::rand () % 2);
         answer = CORBA::string_dup ("This is my answer : Hi");
         return ACE_OS::rand () % 100;
@@ -35,7 +33,6 @@ namespace CIAO_Hello_Receiver_Impl
   void
   MyFoo_exec_i::hello (::CORBA::Long_out answer)
   {
-    printf ("Receiver (HELLO) :\tReceived request\n");
     ACE_OS::sleep (ACE_OS::rand () % 2);
     answer = ACE_OS::rand () % 100;
   }
@@ -43,11 +40,9 @@ namespace CIAO_Hello_Receiver_Impl
   ::CORBA::Short
   MyFoo_exec_i::rw_attrib ()
   {
-    printf ("Receiver (RW_ATTRIB) :\tReceived request\n");
     if (this->get_rw_)
       {
         this->get_rw_ = false;
-        printf ("Receiver (FOO) :\tSecond get rw_attrib received -> throw internal excep with id <42> and errorstring <Hello World>\n");
         Hello::InternalError ex (42, "Hello world");
         throw ex;
       }
@@ -64,13 +59,11 @@ namespace CIAO_Hello_Receiver_Impl
   {
     if (new_value == 0)
       {
-        printf ("Receiver (RW_ATTRIB) :\tnew_value == 0 -> throw internal excep with id <42> and errorstring <Hello World>\n");
         Hello::InternalError ex (42, "Hello world");
         throw ex;
       }
     else
       {
-        printf ("Receiver (RW_ATTRIB) :\tSet <%d>\n", new_value);
         ACE_OS::sleep (ACE_OS::rand () % 2);
       }
   }
@@ -78,11 +71,9 @@ namespace CIAO_Hello_Receiver_Impl
   CORBA::Short
   MyFoo_exec_i::ro_attrib ()
   {
-    printf ("Receiver (RO_ATTRIB) :\tReceived request\n");
-    if (this->get_ro_)
+   if (this->get_ro_)
       {
         this->get_ro_ = false;
-        printf ("Receiver (FOO) :\tSecond get ro_attrib received -> throw internal excep with id <42> and errorstring <Hello World>\n");
         Hello::InternalError ex (42, "Hello world");
         throw ex;
       }
