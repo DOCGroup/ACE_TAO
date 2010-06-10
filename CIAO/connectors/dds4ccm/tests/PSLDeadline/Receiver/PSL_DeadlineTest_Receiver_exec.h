efaf0afe439f3cc4f27a9dd743ec8a7ec3c7db39
msmit <msmit@remedy.nl>
2010-06-10 12:16:04 +0000

    Thu Jun 10 12:12:35 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/CSLDeadline/Receiver/CSL_DeadlineTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLQoS/Receiver/CSL_QoSTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLSampleRejected/Receiver/CSL_SRTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/CSLUnexpStat/Receiver/CSL_USTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/ListenOneByOne/Receiver/LOBO_Test_Receiver_exec.h:
            * connectors/dds4ccm/tests/PSLDeadline/Receiver/PSL_DeadlineTest_Receiver_exec.h:
            * connectors/dds4ccm/tests/PSLSampleLost/Receiver/PSL_SampleLost_Receiver_exec.h:
            * connectors/dds4ccm/tests/QueryCondition/EventListener/Receiver/QCLE_Test_Receiver_exec.h:
            * connectors/dds4ccm/tests/QueryCondition/StateListener/Receiver/QCLS_Test_Receiver_exec.h:
            * connectors/dds4ccm/tests/SLDisabled/Receiver/SL_Disabled_Receiver_exec.h:
            * connectors/dds4ccm/tests/SLManyByMany/Receiver/SL_ManyByMany_Receiver_exec.h:
            * connectors/dds4ccm/tests/SLOneByOne/Receiver/SL_OneByOne_Receiver_exec.h:
            * connectors/dds4ccm/tests/State_Connection_Test/Receiver/State_Connection_Test_Receiver_exec.h:
            * connectors/dds4ccm/tests/UnkeyedSamples/Receiver/Unkeyed_Test_Receiver_exec.h:
            * connectors/dds4ccm/tests/UnkeyedWriter/Receiver/Writer_Receiver_exec.h:
            * connectors/dds4ccm/tests/Updater/Receiver/Updater_Receiver_exec.h:
            * connectors/dds4ccm/tests/VariousTopicsEvent/Receiver/VTopic_Receiver_exec.h:
              Removed unnecessary header file ace/Task.h

--- CIAO/connectors/dds4ccm/tests/PSLDeadline/Receiver/PSL_DeadlineTest_Receiver_exec.h
+++ CIAO/connectors/dds4ccm/tests/PSLDeadline/Receiver/PSL_DeadlineTest_Receiver_exec.h
@@ -15,7 +15,7 @@
 #include "tao/LocalObject.h"
 #include "ace/OS_NS_Thread.h"
 #include "ace/Reactor.h"
-#include "ace/Task.h"
+
 
 namespace CIAO_PSL_DeadlineTest_Receiver_Impl
 {
--- CIAO/connectors/dds4ccm/tests/Updater/Receiver/Updater_Receiver_exec.h
+++ CIAO/connectors/dds4ccm/tests/Updater/Receiver/Updater_Receiver_exec.h
@@ -15,7 +15,7 @@
 #include "tao/LocalObject.h"
 #include "ace/OS_NS_Thread.h"
 #include "ace/Reactor.h"
-#include "ace/Task.h"
+
 
 namespace CIAO_Updater_Receiver_Impl
 {
