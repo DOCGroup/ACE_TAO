089cc6a5ccf5fe22b4ac981119bd352cce268bbd
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2010-06-15 08:16:07 +0000

    Tue Jun 15 08:13:41 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/ami4ccm/tests/SimplexMulti
            * connectors/ami4ccm/tests/SimplexMulti/Base
            * connectors/ami4ccm/tests/SimplexMulti/Base/UsesSM.idl
            * connectors/ami4ccm/tests/SimplexMulti/Base/UsesSM.mpc
            * connectors/ami4ccm/tests/SimplexMulti/descriptors
            * connectors/ami4ccm/tests/SimplexMulti/descriptors/Plan.cdp
            * connectors/ami4ccm/tests/SimplexMulti/descriptors/run_test.pl
            * connectors/ami4ccm/tests/SimplexMulti/Receiver
            * connectors/ami4ccm/tests/SimplexMulti/Receiver/UsesSM_Receiver.idl
            * connectors/ami4ccm/tests/SimplexMulti/Receiver/UsesSM_Receiver.mpc
            * connectors/ami4ccm/tests/SimplexMulti/Receiver/UsesSM_Receiver_exec.cpp
            * connectors/ami4ccm/tests/SimplexMulti/Receiver/UsesSM_Receiver_exec.h
            * connectors/ami4ccm/tests/SimplexMulti/Sender
            * connectors/ami4ccm/tests/SimplexMulti/Sender/UsesSM_Sender.idl
            * connectors/ami4ccm/tests/SimplexMulti/Sender/UsesSM_Sender.mpc
            * connectors/ami4ccm/tests/SimplexMulti/Sender/UsesSM_Sender_exec.cpp
            * connectors/ami4ccm/tests/SimplexMulti/Sender/UsesSM_Sender_exec.h
            * bin/ciao_tests.lst
            AMI4CCM test uses and uses multiple side by side.

---
+++ CIAO/connectors/ami4ccm/tests/SimplexMulti/Receiver/UsesSM_Receiver_exec.h
@@ -0,0 +1,73 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_USESSM_RECEIVER_EXEC_H_
+#define CIAO_USESSM_RECEIVER_EXEC_H_
+
+#include "UsesSM_ReceiverEC.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+#include "UsesSM_Receiver_exec_export.h"
+
+namespace CIAO_UsesSM_Receiver_Impl
+{
+  typedef ACE_Atomic_Op <TAO_SYNCH_MUTEX, CORBA::UShort > Atomic_UShort;
+
+  class One_exec_i
+    : public virtual ::UsesSM::CCM_One,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    One_exec_i ( Atomic_UShort &nr_of_received);
+    virtual ~One_exec_i (void);
+
+    virtual ::CORBA::Long foo (const char * in_str, ::CORBA::Long cmd,
+                               ::CORBA::String_out answer);
+    private:
+      Atomic_UShort &nr_of_received_;
+  };
+  class Two_exec_i
+    : public virtual ::UsesSM::CCM_Two,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Two_exec_i (Atomic_UShort &nr_of_received);
+    virtual ~Two_exec_i (void);
+
+    virtual void bar (::CORBA::Long cmd,
+                      ::CORBA::String_out answer);
+    private:
+      Atomic_UShort &nr_of_received_;
+  };
+  class  Receiver_exec_i
+    : public virtual Receiver_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Receiver_exec_i (void);
+    virtual ~Receiver_exec_i (void);
+
+    virtual ::UsesSM::CCM_One_ptr get_do_my_one (void);
+    virtual ::UsesSM::CCM_Two_ptr get_do_my_two (void);
+
+    virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+  private:
+    ::UsesSM::CCM_Receiver_Context_var context_;
+    Atomic_UShort nr_of_received_;
+  };
+
+  extern "C" USESSM_RECEIVER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_UsesSM_AMI_Receiver_Impl (void);
+}
+#endif /* ifndef */
