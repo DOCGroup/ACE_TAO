f4b179c51e937911168f5cec80bb4a3604ca1f7b
msmit <msmit@remedy.nl>
2010-06-16 09:21:13 +0000

    Wed Jun 16 09:17:26 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ShapesControl_comp.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ShapesControl_exec.h:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ShapesControl_exec.cpp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ShapesControl_obj.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ports:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ports/ShapesControl_obj.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/src:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/src/ShapesControl_comp.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/ShapesReceiver_comp.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/src/ShapesReceiver_comp.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesSender_comp.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/src/ShapesSender_comp.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/ShapesReceiver.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesControl.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesSender.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ports/Shapes_Control_obj.idl:
              Added a non-DDS controller which controls the sender. Functionality needs to be
              added.

---
+++ CIAO/connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesControl_comp/ShapesControl_exec.h
@@ -0,0 +1,106 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_CONTROL_EXEC_H_
+#define CIAO_CONTROL_EXEC_H_
+
+#include "ShapesControl_compEC.h"
+
+#include /**/ "ShapesControl_exec_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+#include "ace/Reactor.h"
+
+namespace CIAO_Shapes_Control_comp_Impl
+{
+  class Control_exec_i;
+
+  //============================================================
+  // pulse_Generator
+  //============================================================
+  class pulse_Generator : public ACE_Event_Handler
+  {
+  public:
+    pulse_Generator (Control_exec_i &callback);
+
+    virtual ~pulse_Generator ();
+
+    virtual int handle_timeout (const ACE_Time_Value &tv,
+                                const void *arg);
+  private:
+    Control_exec_i &pulse_callback_;
+  };
+
+  //============================================================
+  // Control_exec_i
+  //============================================================
+  class Control_exec_i
+    : public virtual Control_comp_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Control_exec_i (void);
+    virtual ~Control_exec_i (void);
+
+    ::CORBA::ULong rate (void);
+
+    void rate (::CORBA::ULong rate);
+
+    ::CORBA::UShort max_x (void);
+
+    void max_x (::CORBA::UShort max_x);
+
+    ::CORBA::UShort max_y (void);
+
+    void max_y (::CORBA::UShort max_y);
+
+    ::CORBA::UShort max_size (void);
+
+    void max_size (::CORBA::UShort max_size);
+
+    void resize_shape (::CORBA::Boolean resize);
+
+    ::CORBA::Boolean resize_shape (void);
+
+    virtual ::Shapes::CCM_Control_obj_ptr
+    get_control (void);
+
+    virtual void set_session_context (::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+    void tick ();
+
+  private:
+    ::Shapes::CCM_Control_comp_Context_var context_;
+
+    pulse_Generator * ticker_;
+
+    CORBA::ULong rate_;
+    CORBA::UShort max_x_;
+    CORBA::UShort max_y_;
+    CORBA::UShort max_size_;
+    CORBA::Boolean resize_;
+
+    bool x_increasing_;
+    bool y_increasing_;
+    bool size_increasing_;
+
+    void start (void);
+    void stop (void);
+  };
+
+  extern "C" SHAPES_CONTROL_COMP_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_Shapes_Control_comp_Impl (void);
+}
+
+#endif /* ifndef */
+
