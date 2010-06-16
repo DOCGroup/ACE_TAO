78f14919c3887f4cc017a56af921909073b8cf1e
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-06-16 06:30:17 +0000

    Wed Jun 16 06:25:25 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tutorials/Shapes/Models:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/ShapesReceiver.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/ShapesReceiver_exec.h:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/ShapesReceiver_exec.cpp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/src:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/src/ShapesReceiver_comp.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesControl.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesSender.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesSender_exec.h:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ShapesSender_exec.cpp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ports:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/ports/Shapes_Control_obj.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/src:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesSender_comp/src/ShapesSender_comp.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/descriptors:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/descriptors/Plan.cdp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/descriptors/run_test.pl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ports:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ports/Shapes.mpb:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ports/Shapes_conn.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ports/Shapes_conn.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ports/Shapes_msg.idl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ports/Shapes_msg.mpc:
            * connectors/dds4ccm/tutorials/Shapes/Tutorial:
              First iteration.

---
+++ trunk/CIAO/connectors/dds4ccm/tutorials/Shapes/Shapes_asm/ShapesReceiver_comp/ShapesReceiver_exec.cpp
@@ -0,0 +1,128 @@
+// -*- C++ -*-
+//
+// $Id$
+
+#include "ShapesReceiver_exec.h"
+#include "ciao/Logger/Log_Macros.h"
+#include "tao/ORB_Core.h"
+#include "ace/OS_NS_time.h"
+
+namespace CIAO_Shapes_Receiver_comp_Impl
+{
+  //============================================================
+  // ShapeType_Listener_exec_i
+  //============================================================
+  ShapeType_Listener_exec_i::ShapeType_Listener_exec_i (void)
+  {
+  }
+
+  ShapeType_Listener_exec_i::~ShapeType_Listener_exec_i (void)
+  {
+  }
+
+  // Operations from ::CCM_DDS::ShapeType_Listener
+  void
+  ShapeType_Listener_exec_i::on_many_data (
+    const ShapeTypeSeq & /* an_instance */,
+    const ::CCM_DDS::ReadInfoSeq & /* info */)
+  {
+  }
+
+  void
+  ShapeType_Listener_exec_i::on_one_data (
+    const ShapeType & an_instance ,
+    const ::CCM_DDS::ReadInfo & /* info */)
+  {
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("ShapeType_Listener: ")
+            ACE_TEXT ("received shape_info for <%C> at %u:%u:%u\n"),
+            an_instance.color.in (),
+            an_instance.x,
+            an_instance.y,
+            an_instance.shapesize));
+  }
+
+  //============================================================
+  // Receiver_exec_i
+  //============================================================
+  Receiver_exec_i::Receiver_exec_i (void)
+  {
+  }
+
+  Receiver_exec_i::~Receiver_exec_i (void)
+  {
+  }
+
+  // Supported operations and attributes.
+
+  // Component attributes.
+
+  // Port operations.
+  ::Shapes::ShapesConnector::CCM_Listener_ptr
+  Receiver_exec_i::get_info_out_data_listener (void)
+  {
+    return new ShapeType_Listener_exec_i ();
+  }
+
+  ::CCM_DDS::CCM_PortStatusListener_ptr
+  Receiver_exec_i::get_info_out_status (void)
+  {
+    return ::CCM_DDS::CCM_PortStatusListener::_nil ();
+  }
+
+  // Operations from Components::SessionComponent.
+  void
+  Receiver_exec_i::set_session_context (
+    ::Components::SessionContext_ptr ctx)
+  {
+    this->context_ =
+      ::Shapes::CCM_Receiver_comp_Context::_narrow (ctx);
+    if ( ::CORBA::is_nil (this->context_.in ()))
+      {
+        throw ::CORBA::INTERNAL ();
+      }
+  }
+
+  void
+  Receiver_exec_i::configuration_complete (void)
+  {
+  }
+
+  void
+  Receiver_exec_i::ccm_activate (void)
+  {
+    ::CCM_DDS::DataListenerControl_var lc =
+    this->context_->get_connection_info_out_data_control ();
+
+    if (::CORBA::is_nil (lc.in ()))
+      {
+        ACE_ERROR ((LM_INFO, ACE_TEXT ("Error:  Listener control receptacle is null!\n")));
+        throw CORBA::INTERNAL ();
+      }
+
+    lc->mode (::CCM_DDS::ONE_BY_ONE);
+  }
+
+  void
+  Receiver_exec_i::ccm_passivate (void)
+  {
+  }
+
+  void
+  Receiver_exec_i::ccm_remove (void)
+  {
+  }
+
+  extern "C" SHAPES_RECEIVER_COMP_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_Shapes_Receiver_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_NORETURN (
+      retval,
+      Receiver_exec_i);
+
+    return retval;
+  }
+}
+
