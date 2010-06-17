ae7133835cf6c0e9258039d7b64406637d6f7cb1
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-06-17 08:18:44 +0000

    Thu Jun 17 08:15:26 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * bin/ciao_tests.lst:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment/descriptors:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment/scripts:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment/scripts/run_test.pl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment/descriptors/run_test.pl:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/descriptors:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/descriptors/Plan.cdp:
            * connectors/dds4ccm/tutorials/Shapes/Shapes_asm/descriptors/run_test.pl:
              Reorganized descriptors directory.

--- trunk/CIAO/connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment/scripts/run_test.pl
+++ trunk/CIAO/connectors/dds4ccm/tutorials/Shapes/Shapes_asm/Deployment/scripts/run_test.pl
@@ -42,23 +42,23 @@ $tg_exe_man = 0;
 $tg_executor = 0;
 
 $status = 0;
-$cdp_file = "Plan.cdp";
+$cdp_file = "../descriptors/Plan.cdp";
 
 sub create_targets {
     #   naming service
     $tg_naming = PerlACE::TestTarget::create_target (1) || die "Create target for ns failed\n";
-    $tg_naming->AddLibPath ('../lib');
+    $tg_naming->AddLibPath ('../../lib');
     #   daemon
     for ($i = 0; $i < $nr_daemon; ++$i) {
         $tg_daemons[$i] = PerlACE::TestTarget::create_target ($i+1) || die "Create target for deamon $i failed\n";
-        $tg_daemons[$i]->AddLibPath ('../lib');
+        $tg_daemons[$i]->AddLibPath ('../../lib');
     }
     #   execution manager
     $tg_exe_man = PerlACE::TestTarget::create_target (1) || die "Create target for EM failed\n";
-    $tg_exe_man->AddLibPath ('../lib');
+    $tg_exe_man->AddLibPath ('../../lib');
     #   executor (plan_launcher)
     $tg_executor = PerlACE::TestTarget::create_target (1) || die "Create target for executor failed\n";
-    $tg_executor->AddLibPath ('../lib');
+    $tg_executor->AddLibPath ('../../lib');
 }
 
 sub init_ior_files {
