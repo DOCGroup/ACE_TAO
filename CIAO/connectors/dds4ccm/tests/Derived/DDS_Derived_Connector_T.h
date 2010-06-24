ae3474510e334e15434fe9430249e9f5b1a58022
Johnny Willemsen <jwillemsen@remedy.nl>
2010-06-24 07:15:32 +0000

    Thu Jun 24 07:13:02 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * MPC/config/dds4ccm_ts_default.mpb:
              RTI DDS needs the TAO_IDL typecodes, so use typecode as base project
    
            * connectors/dds4ccm/tests/Derived/DDS_Derived_Connector_T.h:
              Extended templates
    
            * connectors/dds4ccm/tests/Derived/Derived.mpc:
              Cleanup
    
            * connectors/dds4ccm/tests/Derived/Derived_Connector.idl:
            * connectors/dds4ccm/tests/Derived/Derived_Connector.mpc:
              Added some more test IDL

--- CIAO/connectors/dds4ccm/tests/Derived/DDS_Derived_Connector_T.h
+++ CIAO/connectors/dds4ccm/tests/Derived/DDS_Derived_Connector_T.h
@@ -10,18 +10,20 @@ template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED>
 class MyEvent_Connector_T
   : public virtual DDS_Event_Connector_T<DDS_TYPE, CCM_TYPE, FIXED>
 {
+  virtual ::CCM_fooA_ptr get_my_foo (void)
+  {
+    return ::CCM_fooA::_nil ();
+  }
 };
 
 template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED>
 class MyState_Connector_T
   : public virtual DDS_State_Connector_T<DDS_TYPE, CCM_TYPE, FIXED>
 {
-};
-
-template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED>
-class MyOwn_Connector_T
-  : public virtual DDS_TopicBase_Connector_T<DDS_TYPE, CCM_TYPE, FIXED>
-{
+  virtual ::CCM_fooB_ptr get_my_foo (void)
+  {
+    return ::CCM_fooB::_nil ();
+  }
 };
 
 #endif
