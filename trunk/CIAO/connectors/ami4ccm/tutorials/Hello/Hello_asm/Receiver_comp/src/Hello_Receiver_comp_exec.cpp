a34d2a920b91eab91d42c4030a3731a91548e946
mhengstmengel <mhengstmengel@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-06-24 18:21:59 +0000

    Tue Jun 24 18:159:31 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Deployment/descriptors/Plan.cdp
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello.mpc
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_Common_defn.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_Common_defn.mpc
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_defn.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_defn.mpc
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_MyFoo_obj.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_MyFoo_obj.mpc
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/ports/Hello_obj.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/ports/Hello_Receiver_comp.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/src/Hello_Receiver_comp.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/src/Hello_Receiver_comp.mpc
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/src/Hello_Receiver_comp_exec.cpp
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/src/Hello_Receiver_comp_exec.h
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/ports/Hello_Sender_comp.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/src/Hello_Sender_comp.idl
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/src/Hello_Sender_comp.mpc
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/src/Hello_Sender_comp_exec.cpp
            * connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/src/Hello_Sender_comp_exec.h
            Changes in dir structure and naming.

--- trunk/CIAO/connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/src/Hello_Receiver_comp_exec.cpp
+++ trunk/CIAO/connectors/ami4ccm/tutorials/Hello/Hello_asm/Receiver_comp/src/Hello_Receiver_comp_exec.cpp
@@ -4,18 +4,18 @@
 #include "Hello_Receiver_comp_exec.h"
 #include "ace/OS_NS_unistd.h"
 
-namespace CIAO_Hello_Receiver_Impl
+namespace CIAO_Hello_Receiver_comp_Impl
 {
-  MyFoo_exec_i::MyFoo_exec_i (void)
+  MyFoo_obj_exec_i::MyFoo_obj_exec_i (void)
   {
   }
 
-  MyFoo_exec_i::~MyFoo_exec_i (void)
+  MyFoo_obj_exec_i::~MyFoo_obj_exec_i (void)
   {
   }
 
   ::CORBA::Long
-  MyFoo_exec_i::foo (const char * in_str, ::CORBA::String_out answer)
+  MyFoo_obj_exec_i::foo (const char * in_str, ::CORBA::String_out answer)
   {
     /* Your code here. */
 
@@ -34,7 +34,7 @@ namespace CIAO_Hello_Receiver_Impl
   }
 
   void
-  MyFoo_exec_i::hello (::CORBA::Long_out answer)
+  MyFoo_obj_exec_i::hello (::CORBA::Long_out answer)
   {
     /* Your code here. */
     ACE_OS::sleep (ACE_OS::rand () % 2);
@@ -42,44 +42,44 @@ namespace CIAO_Hello_Receiver_Impl
   }
 
   ::CORBA::Short
-  MyFoo_exec_i::rw_attrib ()
+  MyFoo_obj_exec_i::rw_attrib ()
   {
     /* Your code here. */
     return ACE_OS::rand () % 100;
   }
 
   void
-  MyFoo_exec_i::rw_attrib (::CORBA::Short /*new_value*/)
+  MyFoo_obj_exec_i::rw_attrib (::CORBA::Short /*new_value*/)
   {
     /* Your code here.*/
   }
 
   CORBA::Short
-  MyFoo_exec_i::ro_attrib ()
+  MyFoo_obj_exec_i::ro_attrib ()
   {
     /* Your code here. */
     return ACE_OS::rand () % 100;
   }
 
-  Receiver_exec_i::Receiver_exec_i (void)
+  Receiver_comp_exec_i::Receiver_comp_exec_i (void)
   {
   }
 
-  Receiver_exec_i::~Receiver_exec_i (void)
+  Receiver_comp_exec_i::~Receiver_comp_exec_i (void)
   {
   }
 
-  ::Hello::CCM_MyFoo_ptr
-  Receiver_exec_i::get_do_my_foo (void)
+  ::Hello::CCM_MyFoo_obj_ptr
+  Receiver_comp_exec_i::get_do_my_foo (void)
   {
-    return new MyFoo_exec_i ();
+    return new MyFoo_obj_exec_i ();
   }
 
   void
-  Receiver_exec_i::set_session_context (
+  Receiver_comp_exec_i::set_session_context (
     ::Components::SessionContext_ptr ctx)
   {
-    this->context_ = ::Hello::CCM_Receiver_Context::_narrow (ctx);
+    this->context_ = ::Hello::CCM_Receiver_comp_Context::_narrow (ctx);
 
     if ( ::CORBA::is_nil (this->context_.in ()))
       {
@@ -88,34 +88,34 @@ namespace CIAO_Hello_Receiver_Impl
   }
 
   void
-  Receiver_exec_i::configuration_complete (void)
+  Receiver_comp_exec_i::configuration_complete (void)
   {
   }
 
   void
-  Receiver_exec_i::ccm_activate (void)
+  Receiver_comp_exec_i::ccm_activate (void)
   {
   }
 
   void
-  Receiver_exec_i::ccm_passivate (void)
+  Receiver_comp_exec_i::ccm_passivate (void)
   {
   }
 
   void
-  Receiver_exec_i::ccm_remove (void)
+  Receiver_comp_exec_i::ccm_remove (void)
   {
   }
 
   extern "C"  ::Components::EnterpriseComponent_ptr
-  create_Hello_AMI_Receiver_Impl (void)
+  create_Hello_AMI_Receiver_comp_Impl (void)
   {
     ::Components::EnterpriseComponent_ptr retval =
       ::Components::EnterpriseComponent::_nil ();
 
     ACE_NEW_RETURN (
       retval,
-      Receiver_exec_i,
+      Receiver_comp_exec_i,
       ::Components::EnterpriseComponent::_nil ());
 
     return retval;
--- trunk/CIAO/connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/src/Hello_Sender_comp_exec.h
+++ trunk/CIAO/connectors/ami4ccm/tutorials/Hello/Hello_asm/Sender_comp/src/Hello_Sender_comp_exec.h
@@ -2,11 +2,11 @@
 //
 // $Id$
 
-#ifndef CIAO_HELLO_SENDER_EXEC_H_
-#define CIAO_HELLO_SENDER_EXEC_H_
+#ifndef CIAO_HELLO_SENDER_COMP_EXEC_H_
+#define CIAO_HELLO_SENDER_COMP_EXEC_H_
 
 #include "Hello_Sender_compEC.h"
-#include "Hello_Sender_exec_export.h"
+#include "Hello_Sender_comp_exec_export.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
@@ -15,39 +15,39 @@
 #include "tao/LocalObject.h"
 #include "ace/Task.h"
 
-namespace CIAO_Hello_Sender_Impl
+namespace CIAO_Hello_Sender_comp_Impl
 {
   /// Worker thread for asynchronous invocations
   class asynch_foo_generator : public virtual ACE_Task_Base
   {
   public:
-    asynch_foo_generator (::Hello::AMI4CCM_MyFoo_ptr my_foo_ami);
+    asynch_foo_generator (::Hello::AMI4CCM_MyFoo_obj_ptr my_foo_ami);
 
     virtual int svc (void);
 
   private:
-    ::Hello::AMI4CCM_MyFoo_var my_foo_ami_;
+    ::Hello::AMI4CCM_MyFoo_obj_var my_foo_ami_;
   };
 
   /// Worker thread for synchronous invocations
   class synch_foo_generator : public virtual ACE_Task_Base
   {
   public:
-    synch_foo_generator (::Hello::MyFoo_ptr my_foo_ami);
+    synch_foo_generator (::Hello::MyFoo_obj_ptr my_foo_ami);
 
     virtual int svc (void);
 
   private:
-    ::Hello::MyFoo_var my_foo_ami_;
+    ::Hello::MyFoo_obj_var my_foo_ami_;
   };
 
-  class MyFoo_callback_exec_i
-    : public virtual ::Hello::CCM_AMI4CCM_MyFooReplyHandler,
+  class MyFoo_obj_callback_exec_i
+    : public virtual ::Hello::CCM_AMI4CCM_MyFoo_objReplyHandler,
       public virtual ::CORBA::LocalObject
   {
   public:
-    MyFoo_callback_exec_i (void);
-    virtual ~MyFoo_callback_exec_i (void);
+    MyFoo_obj_callback_exec_i (void);
+    virtual ~MyFoo_obj_callback_exec_i (void);
 
     virtual void foo (::CORBA::Long ami_return_val, const char * answer);
 
@@ -76,13 +76,13 @@ namespace CIAO_Hello_Sender_Impl
       ::CCM_AMI::ExceptionHolder_ptr excep_holder);
   };
 
-  class Sender_exec_i
-    : public virtual Sender_Exec,
+  class Sender_comp_exec_i
+    : public virtual Sender_comp_Exec,
       public virtual ::CORBA::LocalObject
   {
   public:
-    Sender_exec_i (void);
-    virtual ~Sender_exec_i (void);
+    Sender_comp_exec_i (void);
+    virtual ~Sender_comp_exec_i (void);
 
     virtual void
     set_session_context (
@@ -95,11 +95,11 @@ namespace CIAO_Hello_Sender_Impl
     virtual void ccm_remove (void);
 
   private:
-    ::Hello::CCM_Sender_Context_var context_;
+    ::Hello::CCM_Sender_comp_Context_var context_;
   };
 
-  extern "C" HELLO_SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
-  create_Hello_AMI_Sender_Impl (void);
+  extern "C" HELLO_SENDER_COMP_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_Hello_AMI_Sender_comp_Impl (void);
 }
 
 #endif /* ifndef */
