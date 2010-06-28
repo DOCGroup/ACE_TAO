fe5a3d6a5ef2ccaada4a1473d03733d481aa5669
msmit <msmit@remedy.nl>
2010-06-28 08:40:52 +0000

    Mon Jun 28 08:37:18 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/DomainParticipantManager.cpp:
              Resolved minor valgrind issue.
    
            * connectors/dds4ccm/tests/QosProfile/descriptors/run_test.pl:
              Optimized script in order to reveal the real errors better.

--- CIAO/connectors/dds4ccm/impl/DomainParticipantManager.cpp
+++ CIAO/connectors/dds4ccm/impl/DomainParticipantManager.cpp
@@ -222,7 +222,6 @@ namespace CIAO
             {
               //save to remove from list
               this->dps_.erase (iter);
-              delete iter->second;
             }
           else
             {
