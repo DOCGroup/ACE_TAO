e097f98441868e45a34879cdbcd1667d2c472a61
msmit <msmit@remedy.nl>
2010-06-28 12:16:40 +0000

    Mon Jun 28 12:13:08 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * bin/ciao_tests.lst:
    
            * connectors/dds4ccm/impl/DDS_Listen_T.h:
            * connectors/dds4ccm/impl/DDS_Listen_T.cpp:
            * connectors/dds4ccm/impl/DDS_StateListen_T.h:
            * connectors/dds4ccm/impl/DDS_StateListen_T.cpp:
            * connectors/dds4ccm/impl/DataListenerControl_T.h:
            * connectors/dds4ccm/impl/DataListenerControl_T.cpp:
            * connectors/dds4ccm/impl/StateListenerControl_T.h:
            * connectors/dds4ccm/impl/StateListenerControl_T.cpp:
              Added _get_component method to the listener controllers.

--- CIAO/connectors/dds4ccm/impl/DDS_Listen_T.cpp
+++ CIAO/connectors/dds4ccm/impl/DDS_Listen_T.cpp
@@ -8,13 +8,48 @@
 template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
 DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::DDS_Listen_T (void) :
   data_control_ (new CCM_DDS_DataListenerControl_T
-    < ::CCM_DDS::CCM_DataListenerControl> ())
+    < ::CCM_DDS::CCM_DataListenerControl, CCM_TYPE> ())
 {
 }
 
 template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
 DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::~DDS_Listen_T (void)
 {
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
+bool
+DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::configuration_complete (
+  typename CCM_TYPE::base_type::_ptr_type component,
+  ::DDS::Topic_ptr topic,
+  ::DDS::Subscriber_ptr subscriber,
+  const char* library_name,
+  const char* profile_name)
+{
+  DDS4CCM_TRACE ("DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::configuration_complete");
+
+  if (DDS_Subscriber_Base_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::configuration_complete (
+            component,
+            topic,
+            subscriber,
+            library_name,
+            profile_name))
+    {
+      DataListenerControl *dds_dlc = dynamic_cast < DataListenerControl * >
+        (this->data_control_.in ());
+      if (dds_dlc)
+        {
+          dds_dlc->_set_component (component);
+          return true;
+        }
+      else
+        {
+          DDS4CCM_ERROR (1, (LM_ERROR, CLINFO "DDS_Listen_T::configuration_complete - "
+                            "Unable to cast DataListenerControl.\n"));
+          return false;
+        }
+    }
+  return false;
 }
 
 template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
@@ -25,6 +60,7 @@ DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::activate (
   ACE_Reactor* reactor)
 {
   DDS4CCM_TRACE ("DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::activate");
+
   try
     {
       if (::CORBA::is_nil (this->listener_.in ()))
@@ -46,6 +82,28 @@ DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::activate (
       DDS4CCM_ERROR (1, (LM_EMERGENCY, "DDS_Listen_T::activate: Caught unexpected exception.\n"));
       throw CORBA::INTERNAL ();
     }
+}
+
+template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
+void
+DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::remove (
+  ::DDS::Subscriber_ptr subscriber)
+{
+  DDS4CCM_TRACE ("DDS_Listen_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::remove");
+
+  DataListenerControl *dds_dlc = dynamic_cast < DataListenerControl * >
+    (this->data_control_.in ());
+  if (dds_dlc)
+    {
+      dds_dlc->_set_component (CCM_TYPE::base_type::_nil ());
+    }
+  else
+    {
+      DDS4CCM_ERROR (1, (LM_ERROR, CLINFO "DDS_Listen_T::remove - "
+                        "Unable to cast DataListenerControl.\n"));
+    }
+
+  DDS_Subscriber_Base_T<DDS_TYPE, CCM_TYPE, FIXED, VENDOR_TYPE>::remove (subscriber);
 }
 
 template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
--- CIAO/connectors/dds4ccm/impl/DDS_StateListen_T.h
+++ CIAO/connectors/dds4ccm/impl/DDS_StateListen_T.h
@@ -9,6 +9,7 @@
 #define DDS_STATELISTEN_T_H_
 
 #include "dds4ccm/impl/DDS_Subscriber_Base_T.h"
+#include "dds4ccm/impl/StateListenerControl_T.h"
 
 template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
 class DDS_StateListen_T
@@ -26,10 +27,19 @@ public:
   ::CCM_DDS::CCM_StateListenerControl_ptr get_data_control (void);
   //@}
 
+  bool configuration_complete (
+    typename CCM_TYPE::base_type::_ptr_type component,
+    ::DDS::Topic_ptr topic,
+    ::DDS::Subscriber_ptr subscriber,
+    const char* library_name,
+    const char* profile_name);
+
   void activate (
     typename CCM_TYPE::statelistener_type::_ptr_type listener,
     ::CCM_DDS::PortStatusListener_ptr status,
     ACE_Reactor* reactor);
+
+  void remove (::DDS::Subscriber_ptr subscriber);
 
 private:
   /**
@@ -38,7 +48,10 @@ private:
   //@{
   ::CCM_DDS::CCM_StateListenerControl_var data_control_;
   //@}
-  typedef ::CIAO::DDS4CCM::DataReaderStateListener_T<DDS_TYPE, CCM_TYPE, VENDOR_TYPE> DataReaderStateListener;
+  typedef ::CIAO::DDS4CCM::DataReaderStateListener_T<DDS_TYPE, CCM_TYPE, VENDOR_TYPE>
+    DataReaderStateListener;
+  typedef CCM_DDS_StateListenerControl_T< ::CCM_DDS::CCM_StateListenerControl, CCM_TYPE>
+    StateListenerControl;
 };
 
 #include "dds4ccm/impl/DDS_StateListen_T.cpp"
