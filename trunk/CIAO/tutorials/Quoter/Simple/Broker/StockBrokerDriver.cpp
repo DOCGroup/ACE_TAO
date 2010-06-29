c6c3fd062158e0971faeba11ec3bade09cbd3e54
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-06-29 08:44:37 +0000

    Tue Jun 29 08:40:39 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/QosProfile/descriptors/run_test.pl:
    
            * tutorials/Quoter/Simple/Broker/StockBrokerDriver.cpp:
              Code layout. Catch InvalidStock exception.
    
            * tutorials/Quoter/Simple/Distributor/StockDistributorDriver.cpp:
              Code layout.

--- trunk/CIAO/tutorials/Quoter/Simple/Broker/StockBrokerDriver.cpp
+++ trunk/CIAO/tutorials/Quoter/Simple/Broker/StockBrokerDriver.cpp
@@ -20,33 +20,33 @@ parse_args (int argc, ACE_TCHAR *argv[])
   int c;
 
   while ((c = get_opts ()) != -1)
-  {
-    switch (c)
     {
-      case 'k':
-        broker_ior = get_opts.opt_arg ();
-        break;
+      switch (c)
+        {
+          case 'k':
+            broker_ior = get_opts.opt_arg ();
+            break;
 
-      case 's':
-        subscribe_name = get_opts.opt_arg ();
-        break;
+          case 's':
+            subscribe_name = get_opts.opt_arg ();
+            break;
 
-      case 'u':
-        unsubscribe_name = get_opts.opt_arg ();
-        break;
+          case 'u':
+            unsubscribe_name = get_opts.opt_arg ();
+            break;
 
-      case '?':
-      default:
-        ACE_ERROR_RETURN ((LM_ERROR,
-                          "usage:  %s\n"
-                          "-k <Broker IOR> (default is file://StockBroker.ior)\n"
-                          "-s <stock name you wish to subscribe>\n"
-                          "-u <stock name you wish to unsubscribe>\n"
-                          "\n",
-                          argv [0]),
-                          -1);
+          case '?':
+          default:
+            ACE_ERROR_RETURN ((LM_ERROR,
+                              "usage:  %s\n"
+                              "-k <Broker IOR> (default is file://StockBroker.ior)\n"
+                              "-s <stock name you wish to subscribe>\n"
+                              "-u <stock name you wish to unsubscribe>\n"
+                              "\n",
+                              argv [0]),
+                              -1);
+        }
     }
-  }
 
   return 0;
 }
@@ -54,53 +54,57 @@ parse_args (int argc, ACE_TCHAR *argv[])
 int ACE_TMAIN (int argc, ACE_TCHAR* argv[])
 {
   try
-  {
-    // initialize the ORB
-    CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
-
-    if (parse_args (argc, argv) != 0)
-      return -1;
-
-    // create the factory object reference of StockBrokerHome
-    CORBA::Object_var broker_obj =
-      orb->string_to_object (broker_ior);
-
-    // downcast the object reference to the appropriate type
-    Stock::StockBroker_var broker =
-    Stock::StockBroker::_narrow (broker_obj.in ());
-
-    if (CORBA::is_nil (broker.in ()))
     {
-      ACE_ERROR_RETURN ((LM_ERROR,
-                        "Unable to acquire 'Broker' objref\n"),
-                        -1);
-    }
+      // initialize the ORB
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
 
-    if (subscribe_name != 0)
+      if (parse_args (argc, argv) != 0)
+        return -1;
+
+      // create the factory object reference of StockBrokerHome
+      CORBA::Object_var broker_obj =
+        orb->string_to_object (broker_ior);
+
+      // downcast the object reference to the appropriate type
+      Stock::StockBroker_var broker =
+      Stock::StockBroker::_narrow (broker_obj.in ());
+
+      if (CORBA::is_nil (broker.in ()))
+        {
+          ACE_ERROR_RETURN ((LM_ERROR,
+                            "Unable to acquire 'Broker' objref\n"),
+                            -1);
+        }
+
+      if (subscribe_name != 0)
+        {
+          broker->stock_subscribe (ACE_TEXT_ALWAYS_CHAR (subscribe_name));
+
+          ACE_DEBUG ((LM_DEBUG, "Subscribe successful!\n"));
+        }
+
+      if (unsubscribe_name != 0)
+        {
+          broker->stock_unsubscribe (ACE_TEXT_ALWAYS_CHAR (unsubscribe_name));
+
+          ACE_DEBUG ((LM_DEBUG, "Unsubscribe successful!\n"));
+        }
+
+      // Finally destroy the ORB
+      orb->destroy ();
+
+    }
+  catch (const Stock::Invalid_Stock& ex)
     {
-      broker->stock_subscribe (ACE_TEXT_ALWAYS_CHAR (subscribe_name));
-
-      ACE_DEBUG ((LM_DEBUG, "Subscribe successful!\n"));
+      ACE_DEBUG ((LM_DEBUG, "Invalid stock exception.\n"));
     }
-
-    if (unsubscribe_name != 0)
-    {
-      broker->stock_unsubscribe (ACE_TEXT_ALWAYS_CHAR (unsubscribe_name));
-
-      ACE_DEBUG ((LM_DEBUG, "Unsubscribe successful!\n"));
-    }
-
-    // Finally destroy the ORB
-    orb->destroy ();
-
-  }
   catch (const CORBA::Exception& ex)
-  {
-    ex._tao_print_exception ("Who is the culprit\n");
-    ACE_ERROR_RETURN ((LM_ERROR,
-                        "Uncaught CORBA exception\n"),
-                      1);
-  }
+    {
+      ex._tao_print_exception ("Who is the culprit\n");
+      ACE_ERROR_RETURN ((LM_ERROR,
+                          "Uncaught CORBA exception\n"),
+                        1);
+    }
 
   return 0;
 }
