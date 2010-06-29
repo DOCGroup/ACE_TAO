8e2d6e295363658818bd1a0f2b6fcd2170946c1d
mcorino <mcorino@users.noreply.github.com>
2010-06-29 08:54:42 +0000

    Tue Jun 29 08:50:10 UTC 2010  Martin Corino  <mcorino@remedy.nl>
            * NEWS:
              Updated.
    
            * DAnCE/NodeManager/NodeManager_Impl.cpp:
            * DAnCE/LocalityManager/Daemon/Locality_Manager_Impl.cpp:
              Corrected best effort interceptor installations.

--- CIAO/DAnCE/NodeManager/NodeManager_Impl.cpp
+++ CIAO/DAnCE/NodeManager/NodeManager_Impl.cpp
@@ -37,24 +37,24 @@ namespace DAnCE
         this->properties_.bind (i->key (), i->item ());
         i.advance ();
       }
-    
+
     ::Deployment::Properties prop;
     ::DAnCE::Utility::build_property_sequence (prop, properties);
     PLUGIN_MANAGER::instance ()->set_configuration (prop);
     PLUGIN_MANAGER::instance ()->register_installation_handler (ACE_TEXT_CHAR_TO_TCHAR ("DAnCE_Locality_Handler"),
                                                                 ACE_TEXT_CHAR_TO_TCHAR ("create_Locality_Handler"));
-    
+
     if (this->properties_.find (DAnCE::LOCALITY_BESTEFFORT) == 0)
       PLUGIN_MANAGER::instance ()->register_interceptor (ACE_TEXT_CHAR_TO_TCHAR ("DAnCE_Error_Interceptors"),
-                                                         ACE_TEXT_CHAR_TO_TCHAR ("create_DAnCE_Standard_Error"));
+                                                         ACE_TEXT_CHAR_TO_TCHAR ("create_DAnCE_Best_Effort"));
     else
       PLUGIN_MANAGER::instance ()->register_interceptor (ACE_TEXT_CHAR_TO_TCHAR ("DAnCE_Error_Interceptors"),
-                                                         ACE_TEXT_CHAR_TO_TCHAR ("create_DAnCE_Best_Effort"));
+                                                         ACE_TEXT_CHAR_TO_TCHAR ("create_DAnCE_Standard_Error"));
 
     DANCE_DEBUG (8, (LM_INFO, DLINFO
                      ACE_TEXT("NodeApplication_Impl::NodeApplication_Impl - ")
                      ACE_TEXT("Plugin loaded\n")));
-    
+
   }
 
   NodeManager_Impl::~NodeManager_Impl()
