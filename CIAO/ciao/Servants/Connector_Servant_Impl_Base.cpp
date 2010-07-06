8b25de5ffc063384c095db2e8b4810e5e90edad0
mcorino <mcorino@users.noreply.github.com>
2010-07-06 09:56:18 +0000

    Tue Jul 06 09:50:30 UTC 2010  Martin Corino  <mcorino@remedy.nl>
            * DAnCE/tests/CIAO/EM-Shutdown-Failures:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/1NodeFailureAtPassivate.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/1NodeFailureAtRemove.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/1NodeNoFailure.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/2NodeFailureAtPassivate.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/2NodeFailureAtRemove.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/2NodeNoFailure.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/5NodeFailureAtPassivate.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/5NodeFailureAtRemove.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/5NodeNoFailure.cdp:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/README.txt:
            * DAnCE/tests/CIAO/EM-Shutdown-Failures/run_test.pl:
              Added new testset.
    
            * ciao/Servants/Connector_Servant_Impl_Base.cpp:
            * ciao/Servants/Servant_Impl_Base.cpp:
              Fixed 'eating' of exceptions at component removal.
    
            * DAnCE/tests/CIAO/EM-Launch-Failures/README.txt:
              Updated.
    
            * DAnCE/tests/CIAO/EM-Launch-Failures/run_test.pl:
              Added cleanup of test results.
    
            * bin/ciao_tests.lst:
              Added new testsets to autobuild testsuite.

--- CIAO/ciao/Servants/Connector_Servant_Impl_Base.cpp
+++ CIAO/ciao/Servants/Connector_Servant_Impl_Base.cpp
@@ -79,7 +79,9 @@ namespace CIAO
     }
     catch (const CORBA::Exception& ex)
     {
-      ex._tao_print_exception ("Port not active\n");
+      // log error and propagate so error gets reported to deployment framework
+      CIAO_ERROR (1, (LM_ERROR, CLINFO "Connector_Servant_Impl_Base::remove - CORBA exception : %C\n", ex._info ().c_str ()));
+      ex._raise (); // propagate
     }
   }
 
