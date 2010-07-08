37775b7bbd5bd09d3fd612e1d44217a9c8bebacb
msmit <msmit@remedy.nl>
2010-07-08 19:10:44 +0000

    Thu Jul  8 19:07:32 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/dds4ccm_conf.h:
              Updated the comment in order to reflect the
              CIAO_DDS4CCM_CONTEXT_SWITCH better.

--- CIAO/connectors/dds4ccm/impl/dds4ccm_conf.h
+++ CIAO/connectors/dds4ccm/impl/dds4ccm_conf.h
@@ -1,3 +1,4 @@
+
 /**
  * @author Marcel Smit <msmit@remedy.nl>
  *
@@ -9,10 +10,19 @@
 #ifndef DDS4CCM_CONF_H_
 #define DDS4CCM_CONF_H_
 
-/// Defines whether DDS4CCM does a thread switch when
-/// a DDS callback has been received. If this is not
-/// defined, DDS4CCM uses the DDS thread to handle the
-/// callbacks.
+/**
+ * CIAO_DDS4CCM_CONTEXT_SWITCH defines whether DDS4CCM
+ * does use the ORB core reactor thread when a DDS callback
+ * has been received.
+ *
+ * If this is defined to 1 DDS4CCM will pass the DDS callback to the
+ * ORB Core Reactor thread which means a thread context switch
+ *
+ * If this isdefined to 0 no context switch takes place and the DDS
+ * callback is delivered to the user code on the DDS thread
+ *
+ * If the user doesn't explicitly choose a value we will default to 1
+ */
 #if !defined (CIAO_DDS4CCM_CONTEXT_SWITCH)
 #  define CIAO_DDS4CCM_CONTEXT_SWITCH 1
 #endif /* CIAO_DDS4CCM_CONTEXT_SWITCH */
