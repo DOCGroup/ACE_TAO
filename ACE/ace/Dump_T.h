354cc8bf375595f3a9974fa118e3139ae3c6f2e4
Johnny Willemsen <jwillemsen@remedy.nl>
2010-07-12 10:11:24 +0000

    Mon Jul 12 10:11:24 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/DLL.h:
            * ace/DLL_Manager.h:
            * ace/Dirent.h:
            * ace/Dump_T.h:
            * ace/Event_Handler.h:
            * ace/File_Lock.h:
            * ace/INET_Addr.h:
            * ace/Log_Msg_Callback.h:
            * ace/Log_Record.h:
            * ace/Logging_Strategy.h:
              Fixed set of doxygen warnings

--- ACE/ace/Dump_T.h
+++ ACE/ace/Dump_T.h
@@ -26,7 +26,8 @@ ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 /**
  * @class ACE_Dumpable_Adapter
  *
- * @brief This class inherits the interface of the abstract ACE_Dumpable
+ * @brief
+ * This class inherits the interface of the abstract ACE_Dumpable
  * class and is instantiated with the implementation of the
  * concrete component class <class Concrete>.
  *
