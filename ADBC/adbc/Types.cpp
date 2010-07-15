c55d527f08631fb2ba347f2b6de310eb6dc5d1cd
Steve Huston <shuston@riverace.com>
2010-07-15 21:22:42 +0000

    Branch ACE 5.8 for Riverace fix kits

--- ADBC/adbc/Types.cpp
+++
@@ -1,7 +0,0 @@
-// $Id$
-
-#include "Types.h"
-
-#if !defined (__ADBC_INLINE__)
-#include "Types.inl"
-#endif
---
+++ ace/config-macosx-snowleopard.h
@@ -0,0 +1,10 @@
+// $Id$
+#ifndef ACE_CONFIG_MACOSX_SNOWLEOPARD_H
+#define ACE_CONFIG_MACOSX_SNOWLEOPARD_H
+
+#include "ace/config-macosx-leopard.h"
+
+// This header has been deprecated in Snow Leopard.
+#define ACE_LACKS_UCONTEXT_H
+
+#endif ACE_CONFIG_MACOSX_SNOWLEOPARD_H
