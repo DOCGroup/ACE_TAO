170c37071e32b924bbb02600e413aad2ecc230fa
William R. Otte <wotte@dre.vanderbilt.edu>
2010-07-23 15:05:36 +0000

    Fri Jul 23 15:05:19 UTC 2010  William R. Otte  <wotte@dre.vanderbilt.edu>
            * DAnCE/LocalityManager/Configuration/Process_Name.cpp:
    
              Compile fixes.

--- CIAO/DAnCE/LocalityManager/Configuration/Process_Name.cpp
+++ CIAO/DAnCE/LocalityManager/Configuration/Process_Name.cpp
@@ -4,10 +4,15 @@
 #include "DAnCE/DAnCE_PropertiesC.h"
 #include "Deployment/Deployment_StartErrorC.h"
 
+#include "DAnCE/Logger/Log_Macros.h"
+
 #if defined (LINUX_VERSION_CODE) && defined (KERNEL_VERSION)
 # if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,9))
-#include <sys/prct.h>
+#include <sys/prctl.h>
 #include <sstream>
+
+#include "ace/Auto_Ptr.h"
+
 #endif
 #endif
 
@@ -33,27 +38,27 @@ namespace DAnCE
 # if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,9))
     CORBA::String_var pname;
     
-    prop.value >>= CORBA::Any::to_string (pname);
+    prop.value >>= CORBA::Any::to_string (pname, 0);
     unsigned long arg (0);
 
-    ACE_Auto_Basic_Array_ptr safe_array;
+    ACE_Auto_Basic_Array_Ptr<char> safe_array;
 
     if (!(ACE_OS::strlen (pname.in ()) <= 16))
       {
         char *tmp (0);
-        ACE_NEW (tmp,
-                 char[16],
-                 CORBA::NO_MEMORY ());
+        ACE_NEW_THROW_EX (tmp,
+			  char[16],
+			  CORBA::NO_MEMORY ());
         
         safe_array.reset (tmp);
         
         ACE_OS::strncpy (tmp, pname.in (), 15);
         tmp[15] = '\0';
         
-        arg = tmp;
+        arg = reinterpret_cast<unsigned long> (tmp);
       }
     else
-      arg = pname.in ();
+      arg = reinterpret_cast<unsigned long> (pname.in ());
 
     int retval = ::prctl (PR_SET_NAME,
                           arg);
@@ -61,14 +66,14 @@ namespace DAnCE
     if (retval != 0)
       {
         std::stringstream str;
-        ACE_Auto_Basic_Array_ptr safe_error (ACE_OS::strerror (ACE_OS::last_error ()));
+        ACE_Auto_Basic_Array_Ptr<char> safe_error (ACE_OS::strerror (ACE_OS::last_error ()));
         
         str << "Unable to set process name to <" << pname.in () << ">: " 
             << safe_error.get ();
         std::string message = str.str ();
 
         DANCE_ERROR (1, (LM_ERROR, DLINFO
-                         ACE_TEXT ("Process_Name::configure - %C\n")
+                         ACE_TEXT ("Process_Name::configure - %C\n"),
                          message.c_str ()));
 
         throw ::Deployment::StartError (prop.name.in (),
