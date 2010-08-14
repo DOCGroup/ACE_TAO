8326bb7a16195793df46243393002599e6369cc2
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-08-14 18:18:01 +0000

    Sat Aug 14 18:15:53 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/DomainParticipantFactory_T.cpp:
            * connectors/dds4ccm/impl/DomainParticipant_T.cpp:
            * connectors/dds4ccm/impl/Publisher_T.cpp:
            * connectors/dds4ccm/impl/Subscriber_T.cpp:
              Corrected error levels and use some DEBUG macros instead of ERROR

--- trunk/CIAO/connectors/dds4ccm/impl/DomainParticipantFactory_T.cpp
+++ trunk/CIAO/connectors/dds4ccm/impl/DomainParticipantFactory_T.cpp
@@ -230,7 +230,8 @@ namespace CIAO
                         "type, bailing.\n"));
           return DDS::RETCODE_ERROR;
         }
-      DDS4CCM_ERROR (DDS4CCM_LOG_LEVEL_CAST_SUCCESSFUL, (LM_TRACE, CLINFO
+        
+      DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_CAST_SUCCESSFUL, (LM_TRACE, CLINFO
                     "CCM_DDS_DomainParticipantFactory_T"
                     "<DDS_TYPE, CCM_TYPE, VENDOR_TYPE>::delete_participant - "
                     "Successfully casted provided object reference to servant type.\n"));
@@ -249,10 +250,13 @@ namespace CIAO
                             "RTI delete_participant returned non-ok error code %C\n",
                             translate_retcode (retval)));
             }
-          else DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_ACTION, (LM_INFO, CLINFO
+          else
+            {
+              DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_ACTION, (LM_INFO, CLINFO
                              "CCM_DDS_DomainParticipantFactory_T"
                              "<DDS_TYPE, CCM_TYPE, VENDOR_TYPE>::delete_participant - "
                              "Successfully deleted provided participant.\n"));
+            }
         }
       return retval;
 #else
