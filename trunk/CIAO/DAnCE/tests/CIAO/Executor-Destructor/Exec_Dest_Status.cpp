2ca50d0fcfd49524d8d0867a16939aac8fef04af
wotte <wotte@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-08-27 03:05:57 +0000

    Fri Aug 27 03:03:43 UTC 2010  William R. Otte  <wotte@dre.vanderbilt.edu>
            * DAnCE/tests/CIAO/Executor-Destructor:
            * DAnCE/tests/CIAO/Executor-Destructor/Component.cdp:
            * DAnCE/tests/CIAO/Executor-Destructor/Exec_Dest_Status.h:
            * DAnCE/tests/CIAO/Executor-Destructor/Exec_Dest_Status.cpp:
            * DAnCE/tests/CIAO/Executor-Destructor/Exec_Dest_export.h:
            * DAnCE/tests/CIAO/Executor-Destructor/Executor_Destructor.idl:
            * DAnCE/tests/CIAO/Executor-Destructor/Executor_Destructor.mpc:
            * DAnCE/tests/CIAO/Executor-Destructor/Executor_Destructor_Interceptor.h:
            * DAnCE/tests/CIAO/Executor-Destructor/Executor_Destructor_Interceptor.cpp:
            * DAnCE/tests/CIAO/Executor-Destructor/Executor_Destructor_exec.h:
            * DAnCE/tests/CIAO/Executor-Destructor/Executor_Destructor_exec.cpp:
            * DAnCE/tests/CIAO/Executor-Destructor/exec-dest.localityconfig:
            * DAnCE/tests/CIAO/Executor-Destructor/run_test.pl:
            * DAnCE/bin/dance_tests.lst:
    
              A test that verifies that the executor destructors are called for
              CIAO components.

---
+++ trunk/CIAO/DAnCE/tests/CIAO/Executor-Destructor/Exec_Dest_Status.cpp
@@ -0,0 +1,51 @@
+#include "Exec_Dest_Status.h"
+
+#include "Deployment/Deployment_DeploymentPlanC.h"
+
+namespace dance_test
+{
+  Exec_Dest_Status::Instance_Status::Instance_Status (void)
+    : destructor_ (false)
+  {
+  }
+
+  Exec_Dest_Status::Exec_Dest_Status (void)
+    : instances_ ()
+  {
+    
+  }
+  
+  Exec_Dest_Status::~Exec_Dest_Status (void)
+  {
+    
+  }
+
+  void 
+  Exec_Dest_Status::plan (const ::Deployment::DeploymentPlan *plan)
+  {
+    if (plan == 0)
+      {
+        ACE_ERROR ((LM_ERROR, "Error:  Nil plan passed to exec_dest_status\n"));
+        return;
+      }
+    
+    for (CORBA::ULong i = 0; i < plan->instance.length (); ++i)
+      {
+        this->instances_[plan->instance[i].name.in ()] = Instance_Status ();
+      }
+  }
+  
+  void 
+  Exec_Dest_Status::destructor_called (const std::string &id)
+  {
+    ACE_DEBUG ((LM_TRACE, "Got destructor invocation for id <%C>\n",
+                id.c_str ()));
+    this->instances_[id].destructor_ = true;    
+  }
+  
+  bool 
+  Exec_Dest_Status::check_destructor (const std::string &id)
+  {
+    return this->instances_[id].destructor_;
+  }
+}
