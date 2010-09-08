2614c72580a271568f5652edb399d32b9d7c6e07
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2010-09-08 15:07:44 +0000

    Wed Sep  8 14:41:52 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * bin/ciao_tests.lst:
            *tests/Thread:
            *tests/Thread/Sender:
            *tests/Thread/Sender/Sender_exec.cpp:
            *tests/Thread/Sender/starter.cpp:
            *tests/Thread/Sender/Sender.idl:
            *tests/Thread/Sender/Sender_exec.h:
            *tests/Thread/Sender/Sender.mpc:
            *tests/Thread/descriptors:
            *tests/Thread/descriptors/DeploymentPlan.cdp:
            *tests/Thread/descriptors/NodeManagerMap.dat:
            *tests/Thread/descriptors/run_test.pl:
            *tests/Thread/Thread_Base:
            *tests/Thread/Thread_Base/Thread_Base.mpc:
            *tests/Thread/Thread_Base/Thread_Base.idl:
            *tests/Thread/Receiver:
            *tests/Thread/Receiver/Receiver.mpc:
            *tests/Thread/Receiver/Receiver_exec.cpp:
            *tests/Thread/Receiver/Receiver.idl:
            *tests/Thread/Receiver/Receiver_exec.h:
            *tests/Thread/README.txt:
            Test use of same thread by ccm component.

---
+++ CIAO/tests/Thread/Sender/Sender_exec.h
@@ -0,0 +1,119 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_SENDER_EXEC_H_
+#define CIAO_SENDER_EXEC_H_
+
+
+#include "SenderEC.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include /**/ "Sender_exec_export.h"
+#include "tao/LocalObject.h"
+
+
+namespace CIAO_Thread_Sender_Impl
+{
+  class Sender_exec_i;
+
+  class SENDER_EXEC_Export ReadMessage_exec_i
+    : public virtual ::Thread::CCM_ReadMessage,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    ReadMessage_exec_i (Sender_exec_i& component);
+
+    virtual ~ReadMessage_exec_i (void);
+
+    // Operations and attributes from ::Thread::ReadMessage
+
+    virtual char *
+    get_message (void);
+
+  private:
+    Sender_exec_i& component_;
+  };
+
+  class SENDER_EXEC_Export Sender_exec_i
+    : public virtual Sender_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Sender_exec_i (const ACE_Thread_ID &thr);
+    virtual ~Sender_exec_i (void);
+
+    //@{
+    /** Supported operations and attributes. */
+
+
+    virtual void
+    start (void);
+    //@}
+
+    //@{
+    /** Component attributes and port operations. */
+
+
+    virtual ::Thread::CCM_ReadMessage_ptr
+    get_push_message (void);
+
+
+    //@{
+    /** Operations from Components::SessionComponent. */
+
+    virtual void
+    set_session_context (
+      ::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+    //@}
+
+
+  private:
+    ::Thread::CCM_Sender_Context_var context_;
+    ACE_Thread_ID thr_;
+    CORBA::String_var message_;
+    CORBA::Boolean log_time_;
+    friend class ReadMessage_exec_i;
+  };
+}
+
+namespace CIAO_Thread_Sender_Impl
+{
+  class SENDER_EXEC_Export SenderHome_exec_i
+    : public virtual SenderHome_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    SenderHome_exec_i (const ACE_Thread_ID &thr);
+
+    virtual ~SenderHome_exec_i (void);
+
+    // All operations and attributes.
+
+    // Factory operations.
+
+    // Finder operations.
+
+    // Implicit operations.
+
+    virtual ::Components::EnterpriseComponent_ptr
+    create (void);
+  private:
+    ACE_Thread_ID thr_;
+
+  };
+
+  extern "C" SENDER_EXEC_Export ::Components::HomeExecutorBase_ptr
+  create_Thread_SenderHome_Impl (void);
+}
+
+#endif /* ifndef */
+
