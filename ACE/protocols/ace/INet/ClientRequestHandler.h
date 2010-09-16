f2078ad3a19191b56d3fd2c4c396d2d30fd91126
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-09-16 13:38:56 +0000

    Thu Sep 16 13:36:51 UTC 2010  Martin Corino  <mcorino@remedy.nl>
            * protocols\ace\INet\ClientRequestHandler.h:
            * protocols\ace\INet\URLBase.h:
            * protocols\ace\INet\HTTP_ClientRequestHandler.h:
               Changes to fix export problems with nested classes with MSVC 9.

--- ACE/protocols/ace/INet/ClientRequestHandler.h
+++ ACE/protocols/ace/INet/ClientRequestHandler.h
@@ -74,7 +74,7 @@ namespace ACE
               * @brief
               *
               */
-#if (_MSC_VER < 1500)
+#if (_MSC_VER < 1600)
               class ACE_INET_Export INetConnectionKey
 #else
               class INetConnectionKey
