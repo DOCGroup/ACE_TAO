505cfcbb119edee4a00cf48e34ae502ba243039b
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-09-16 13:38:56 +0000

    Thu Sep 16 13:36:51 UTC 2010  Martin Corino  <mcorino@remedy.nl>
            * protocols\ace\INet\ClientRequestHandler.h:
            * protocols\ace\INet\URLBase.h:
            * protocols\ace\INet\HTTP_ClientRequestHandler.h:
               Changes to fix export problems with nested classes with MSVC 9.

--- trunk/ACE/protocols/ace/INet/HTTP_ClientRequestHandler.h
+++ trunk/ACE/protocols/ace/INet/HTTP_ClientRequestHandler.h
@@ -176,7 +176,7 @@ namespace ACE
                                                         std::istream* put_data = 0);
 */
 
-#if (_MSC_VER < 1500)
+#if (_MSC_VER < 1600)
               class ACE_INET_Export HttpConnectionKey
 #else
               class HttpConnectionKey
