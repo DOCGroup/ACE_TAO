3c8916409f99de915772dae72b41cbedf523c3d3
Johnny Willemsen <jwillemsen@remedy.nl>
2010-09-21 10:26:43 +0000

    Tue Sep 21 10:22:45 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ciao/Contexts/Extension/ExtensionContext_T.h:
            * ciao/Contexts/Extension/ExtensionContext_T.cpp:
            * ciao/Contexts/Extension/ExtensionContext.h:
            * ciao/Contexts/Extension/ExtensionContext.cpp:
              Renamed to _T and converted to a C++ template

---
+++ CIAO/ciao/Contexts/Extension/ExtensionContext_T.cpp
@@ -0,0 +1,77 @@
+// $Id$
+
+#ifndef CIAO_EXTENSION_CONTEXT_IMPL_T_C
+#define CIAO_EXTENSION_CONTEXT_IMPL_T_C
+
+namespace CIAO
+{
+  template <typename BASE_CTX,
+            typename COMP>
+  ExtensionContext_Impl<BASE_CTX, COMP>::ExtensionContext_Impl (
+      Components::CCMHome_ptr the_home,
+      Container_ptr c,
+      PortableServer::Servant sv,
+      const char* id)
+    : Context_Impl_Base (the_home, c, id),
+      servant_ (sv)
+  {
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ExtensionContext_Impl<BASE_CTX, COMP>::~ExtensionContext_Impl (void)
+  {
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ::Components::ContainerPortableInterceptor::ClientContainerInterceptorRegistration_ptr
+  ExtensionContext_Impl<BASE_CTX, COMP>::get_client_interceptor_registration (void)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ::Components::ContainerPortableInterceptor::ServerContainerInterceptorRegistration_ptr
+  ExtensionContext_Impl<BASE_CTX, COMP>::get_server_interceptor_registration (void)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ::Components::ContainerPortableInterceptor::StubContainerInterceptorRegistration_ptr
+  ExtensionContext_Impl<BASE_CTX, COMP>::get_stub_interceptor_registration (void)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ::Components::ContainerPortableInterceptor::ServantContainerInterceptorRegistration_ptr
+  ExtensionContext_Impl<BASE_CTX, COMP>::get_servant_interceptor_registration (void)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ::Components::Cookie *
+  ExtensionContext_Impl<BASE_CTX, COMP>::install_service_reference (
+        const char * /*service_id*/,
+        ::CORBA::Object_ptr /*objref*/)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  template <typename BASE_CTX,
+            typename COMP>
+  ::CORBA::Object_ptr
+  ExtensionContext_Impl<BASE_CTX, COMP>::uninstall_service_reference (::Components::Cookie * /*ck*/)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+}
+
+#endif /* CIAO_EXTENSION_CONTEXT_IMPL_T_C */
---
+++ CIAO/ciao/Contexts/Extension/ExtensionContext_T.h
@@ -0,0 +1,86 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    ExtensionContext.h
+ *
+ *  $Id$
+ *
+ *  @author Johnny Willemsen  <jwillemsen@remedy.nl>
+ */
+//=============================================================================
+
+
+#ifndef CIAO_EXTENSIONCONTEXT_H
+#define CIAO_EXTENSIONCONTEXT_H
+
+#include /**/ "ace/pre.h"
+
+#include "CIAO_Server_Export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ciao/CCM_ExtensionContextC.h"
+#include "ciao/Context_Impl_Base.h"
+#include "tao/Objref_VarOut_T.h"
+
+namespace Components
+{
+  class CCMHome;
+  typedef CCMHome *CCMHome_ptr;
+
+  typedef
+    TAO_Objref_Var_T<
+        CCMHome
+      >
+    CCMHome_var;
+}
+
+namespace CIAO
+{
+  template <typename BASE_CTX>
+            typename COMP>
+  class ExtensionContext_Impl
+    : public virtual BASE_CTX,
+      public virtual ::CIAO::Context_Impl_Base,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    ExtensionContext_Impl(Components::CCMHome_ptr home,
+                          Session_Container * c,
+                          PortableServer::Servant sv,
+                          const char* id);
+
+    virtual ~ExtensionContext_Impl (void);
+
+    virtual ::Components::ContainerPortableInterceptor::ClientContainerInterceptorRegistration_ptr
+    get_client_interceptor_registration (void);
+
+    virtual ::Components::ContainerPortableInterceptor::ServerContainerInterceptorRegistration_ptr
+    get_server_interceptor_registration (void);
+
+    virtual ::Components::ContainerPortableInterceptor::StubContainerInterceptorRegistration_ptr
+    get_stub_interceptor_registration (void);
+
+    virtual ::Components::ContainerPortableInterceptor::ServantContainerInterceptorRegistration_ptr
+    get_servant_interceptor_registration (void);
+
+    virtual ::Components::Cookie * install_service_reference (
+        const char * service_id,
+        ::CORBA::Object_ptr objref);
+
+    virtual ::CORBA::Object_ptr uninstall_service_reference (
+        ::Components::Cookie * ck);
+
+  private:
+    ExtensionContext_Impl (void);
+    PortableServer::Servant servant_;
+    typename COMP::_var_type component_;
+  };
+}
+
+#include /**/ "ace/post.h"
+
+#endif /* CIAO_EXTENSIONCONTEXT_H */
