aacd0408f68c197b9c2f7da2d9a2df6a13bba971
Johnny Willemsen <jwillemsen@remedy.nl>
2010-10-13 06:20:39 +0000

    Wed Oct 13 06:15:00 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/Framework_Component.h:
            * ace/Framework_Component.inl:
            * ace/Framework_Component.cpp:
            * ace/Framework_Component_T.h:
              Use ACE_Copy_Disabled, change c-style cast to const cast, use
              sync macros instead of ACE_MT. Thanks to Ranjit Hande
              <hande at avaya dot com> for reporting the c-style cast warnings
    
            * tests/Framework_Component_Test.h:
              Layout change

--- ACE/ace/Framework_Component_T.h
+++ ACE/ace/Framework_Component_T.h
@@ -52,9 +52,9 @@ public:
 
 ACE_END_VERSIONED_NAMESPACE_DECL
 
-// This macro should be called in the instance() method
-// of the Concrete class that will be managed.  Along
-// with the appropriate template instantiation.
+/// This macro should be called in the instance() method
+/// of the Concrete class that will be managed.  Along
+/// with the appropriate template instantiation.
 #define ACE_REGISTER_FRAMEWORK_COMPONENT(CLASS, INSTANCE) \
         ACE_Framework_Repository::instance ()->register_component \
           (new ACE_Framework_Component_T<CLASS> (INSTANCE));
