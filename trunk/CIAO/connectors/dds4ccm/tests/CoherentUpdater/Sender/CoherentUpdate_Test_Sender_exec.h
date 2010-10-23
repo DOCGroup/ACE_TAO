8d78a9f645ccfc1c2477224b3dc33f8afde11d44
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-10-23 07:36:07 +0000

    Sat Oct 23 07:27:48 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/tests/CoherentUpdater/Sender/CoherentUpdate_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/CoherentUpdater/Sender/CoherentUpdate_Test_Sender_exec.cpp:
            * connectors/dds4ccm/tests/ListenManyByMany/Sender/LMBM_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/ListenManyByMany/Sender/LMBM_Test_Sender_exec.cpp:
              Regenerated using -Gexr and reimplemented

--- trunk/CIAO/connectors/dds4ccm/tests/CoherentUpdater/Sender/CoherentUpdate_Test_Sender_exec.h
+++ trunk/CIAO/connectors/dds4ccm/tests/CoherentUpdater/Sender/CoherentUpdate_Test_Sender_exec.h
@@ -1,17 +1,18 @@
 // -*- C++ -*-
 // $Id$
 
-#ifndef CIAO_SENDER_EXEC_H_
-#define CIAO_SENDER_EXEC_H_
+#ifndef CIAO_COHERENTUPDATE_TEST_SENDER_EXEC_VI4L1L_H_
+#define CIAO_COHERENTUPDATE_TEST_SENDER_EXEC_VI4L1L_H_
+
+#include /**/ "ace/pre.h"
 
 #include "CoherentUpdate_Test_SenderEC.h"
-
-#include /**/ "Sender_exec_export.h"
 
 #if !defined (ACE_LACKS_PRAGMA_ONCE)
 # pragma once
 #endif /* ACE_LACKS_PRAGMA_ONCE */
 
+#include /**/ "Sender_exec_export.h"
 #include "tao/LocalObject.h"
 
 namespace CIAO_CoherentUpdate_Test_Sender_Impl
@@ -31,26 +32,37 @@ namespace CIAO_CoherentUpdate_Test_Sender_Impl
     Sender_exec_i &callback_;
   };
 
+
   //============================================================
-  // Restarter_exec_i
+  // Provider Executor Implementation Class: restart_updater_exec_i
   //============================================================
-  class Restarter_exec_i
+
+  class restart_updater_exec_i
     : public virtual ::CCM_CoherentUpdateRestarter,
       public virtual ::CORBA::LocalObject
   {
   public:
-    Restarter_exec_i (Sender_exec_i & callback);
-    virtual ~Restarter_exec_i (void);
+    restart_updater_exec_i (
+      ::CoherentUpdate_Test::CCM_Sender_Context_ptr ctx,
+      Sender_exec_i & callback);
+    virtual ~restart_updater_exec_i (void);
 
-    virtual void restart_update ();
+    //@{
+    /** Operations and attributes from ::CoherentUpdateRestarter. */
+
+    virtual
+    void restart_update (void);
+    //@}
 
   private:
+    ::CoherentUpdate_Test::CCM_Sender_Context_var ciao_context_;
     Sender_exec_i &callback_;
   };
 
   //============================================================
-  // Sender_exec_i
+  // Component Executor Implementation Class: Sender_exec_i
   //============================================================
+
   class Sender_exec_i
     : public virtual Sender_Exec,
       public virtual ::CORBA::LocalObject
@@ -59,39 +71,67 @@ namespace CIAO_CoherentUpdate_Test_Sender_Impl
     Sender_exec_i (void);
     virtual ~Sender_exec_i (void);
 
-    virtual ::CORBA::UShort iterations (void);
+    //@{
+    /** Supported operations and attributes. */
 
-    virtual void iterations (::CORBA::UShort iterations);
+    //@}
+
+    //@{
+    /** Component attributes and port operations. */
 
     virtual ::CCM_CoherentUpdateRestarter_ptr
     get_restart_updater (void);
 
+    virtual ::CORBA::UShort iterations (void);
+
+    virtual void iterations (::CORBA::UShort iterations);
+    //@}
+
+    //@{
+    /** Operations from Components::SessionComponent. */
     virtual void set_session_context (::Components::SessionContext_ptr ctx);
-
     virtual void configuration_complete (void);
-
     virtual void ccm_activate (void);
     virtual void ccm_passivate (void);
     virtual void ccm_remove (void);
+    //@}
 
+    //@{
+    /** User defined public operations. */
     void start (void);
     void restart (void);
+    //@}
 
   private:
-    ::CoherentUpdate_Test::CCM_Sender_Context_var context_;
-    ::CoherentUpdate_Test::Updater_var updater_;
-    CoherentUpdateStarter_var starter_;
+    ::CoherentUpdate_Test::CCM_Sender_Context_var ciao_context_;
 
-    CORBA::UShort iterations_;
+    //@{
+    /** Component attributes and port operations. */
+    ::CCM_CoherentUpdateRestarter_var ciao_restart_updater_;
+
+    ::CORBA::UShort iterations_;
+    //@}
+
+    //@{
+    /** User defined members. */
     CORBA::UShort run_;
     CORBA::ULong total_iter;
-
     WriteHandler *wh_;
+    //@}
+
+    //@{
+    /** User defined private operations. */
+
+    //@}
+
+    /// Get the ACE_Reactor
+    ACE_Reactor* reactor (void);
   };
 
   extern "C" SENDER_EXEC_Export ::Components::EnterpriseComponent_ptr
   create_CoherentUpdate_Test_Sender_Impl (void);
 }
 
-#endif /* ifndef */
+#include /**/ "ace/post.h"
 
+#endif /* ifndef */
