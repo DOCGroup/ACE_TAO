0dc51ea14445eb6c563f2beb8e1b9bd27d004206
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-10-30 07:02:24 +0000

    Sat Oct 30 07:07:44 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/Policies/deadline_qos_test.h:
            * connectors/dds4ccm/tests/Policies/dest_order_qos_test.h:
            * connectors/dds4ccm/tests/Policies/dp_factory_qos_test.h:
            * connectors/dds4ccm/tests/Policies/dp_qos_test.h:
            * connectors/dds4ccm/tests/Policies/dr_qos_test.h:
            * connectors/dds4ccm/tests/Policies/dur_qos_test.h:
            * connectors/dds4ccm/tests/Policies/dur_service_qos_test.h:
            * connectors/dds4ccm/tests/Policies/duration.h:
            * connectors/dds4ccm/tests/Policies/dw_qos_test.h:
            * connectors/dds4ccm/tests/Policies/entity_factory_qos_test.h:
            * connectors/dds4ccm/tests/Policies/group_data_qos_test.h:
            * connectors/dds4ccm/tests/Policies/hist_qos_test.h:
            * connectors/dds4ccm/tests/Policies/lat_budget_qos_test.h:
            * connectors/dds4ccm/tests/Policies/lifespan_qos_test.h:
            * connectors/dds4ccm/tests/Policies/liveliness_qos_test.h:
            * connectors/dds4ccm/tests/Policies/octet_seq.h:
            * connectors/dds4ccm/tests/Policies/ownership_qos_test.h:
            * connectors/dds4ccm/tests/Policies/ownership_strength_qos_test.h:
            * connectors/dds4ccm/tests/Policies/partition_qos_test.h:
            * connectors/dds4ccm/tests/Policies/presentation_qos_test.h:
            * connectors/dds4ccm/tests/Policies/pub_qos_test.h:
            * connectors/dds4ccm/tests/Policies/rd_lifecycle_qos_test.h:
            * connectors/dds4ccm/tests/Policies/reliability_qos_test.h:
            * connectors/dds4ccm/tests/Policies/resource_limits_qos_test.h:
            * connectors/dds4ccm/tests/Policies/sub_qos_test.h:
            * connectors/dds4ccm/tests/Policies/tbf_qos_test.h:
            * connectors/dds4ccm/tests/Policies/tp_data_qos_test.h:
            * connectors/dds4ccm/tests/Policies/tp_qos_test.h:
            * connectors/dds4ccm/tests/Policies/transport_prio_qos_test.h:
            * connectors/dds4ccm/tests/Policies/user_data_qos_test.h:
            * connectors/dds4ccm/tests/Policies/wr_lifecycle_qos_test.h:
              Fixed compile errors due to directory change.

--- trunk/CIAO/connectors/dds4ccm/tests/Policies/tbf_qos_test.h
+++ trunk/CIAO/connectors/dds4ccm/tests/Policies/tbf_qos_test.h
@@ -5,7 +5,7 @@
 
 #include "dds4ccm/idl/dds_rtf2_dcpsC.h"
 #include "ndds/ndds_cpp.h"
-#include "dds4ccm/impl/ndds/TimeBasedFilterQosPolicy.h"
+#include "dds4ccm/impl/ndds/convertors/TimeBasedFilterQosPolicy.h"
 
 class TimeBasedFilterPolicyTest
 {
