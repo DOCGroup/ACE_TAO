3b9f1ed37b0248ab93482bd24305948c97ad6803
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-11-02 19:49:11 +0000

    Tue Nov  2 19:55:15 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/CCM_DataReader.h:
            * connectors/dds4ccm/impl/CCM_DataReader.cpp:
            * connectors/dds4ccm/impl/CCM_DataWriter.h:
            * connectors/dds4ccm/impl/CCM_DataWriter.cpp:
              Reorganized code.

--- trunk/CIAO/connectors/dds4ccm/impl/CCM_DataWriter.cpp
+++ trunk/CIAO/connectors/dds4ccm/impl/CCM_DataWriter.cpp
@@ -13,6 +13,136 @@ CIAO::DDS4CCM::CCM_DataWriter::~CCM_DataWriter (void)
   DDS4CCM_TRACE ("CIAO::DDS4CCM::CCM_DataWriter::~CCM_DataWriter");
 }
 
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::enable (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::StatusCondition_ptr
+CIAO::DDS4CCM::CCM_DataWriter::get_statuscondition (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::StatusCondition::_nil ();
+}
+
+::DDS::StatusMask
+CIAO::DDS4CCM::CCM_DataWriter::get_status_changes (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return 0;
+}
+
+::DDS::InstanceHandle_t
+CIAO::DDS4CCM::CCM_DataWriter::get_instance_handle (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::HANDLE_NIL;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::set_qos (
+  const ::DDS::DataWriterQos & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_qos (::DDS::DataWriterQos & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::set_listener (::DDS::DataWriterListener_ptr ,
+  ::DDS::StatusMask )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::DataWriterListener_ptr
+CIAO::DDS4CCM::CCM_DataWriter::get_listener (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::DataWriterListener::_nil ();
+}
+
+::DDS::Topic_ptr
+CIAO::DDS4CCM::CCM_DataWriter::get_topic (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::Topic::_nil ();
+}
+
+::DDS::Publisher_ptr
+CIAO::DDS4CCM::CCM_DataWriter::get_publisher (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::Publisher::_nil ();
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::wait_for_acknowledgments (const ::DDS::Duration_t & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_liveliness_lost_status (::DDS::LivelinessLostStatus & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_offered_deadline_missed_status (::DDS::OfferedDeadlineMissedStatus & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_offered_incompatible_qos_status (::DDS::OfferedIncompatibleQosStatus & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_publication_matched_status (::DDS::PublicationMatchedStatus & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::assert_liveliness (void)
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_matched_subscriptions (::DDS::InstanceHandleSeq & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
+::DDS::ReturnCode_t
+CIAO::DDS4CCM::CCM_DataWriter::get_matched_subscription_data (
+  ::DDS::SubscriptionBuiltinTopicData & ,
+  const ::DDS::InstanceHandle_t & )
+{
+  throw ::CORBA::NO_IMPLEMENT ();
+  return ::DDS::RETCODE_OK;
+}
+
 void
 CIAO::DDS4CCM::CCM_DataWriter::set_impl (::DDS::DataWriter_ptr dr)
 {
--- trunk/CIAO/connectors/dds4ccm/impl/CCM_DataWriter.h
+++ trunk/CIAO/connectors/dds4ccm/impl/CCM_DataWriter.h
@@ -27,26 +27,61 @@ namespace CIAO
       /// Destructor
       virtual ~CCM_DataWriter (void);
 
-      virtual ::DDS::ReturnCode_t enable (void) {return 0;}
-      virtual ::DDS::StatusCondition_ptr get_statuscondition (void) {return 0;}
-      virtual ::DDS::StatusMask get_status_changes (void) {return 0;}
-      virtual ::DDS::InstanceHandle_t get_instance_handle (void) {return ::DDS::HANDLE_NIL;}
-      virtual ::DDS::ReturnCode_t set_qos (const ::DDS::DataWriterQos & ) {return 0;}
-      virtual ::DDS::ReturnCode_t get_qos (::DDS::DataWriterQos & ) {return 0;}
-      virtual ::DDS::ReturnCode_t set_listener (::DDS::DataWriterListener_ptr ,
-      ::DDS::StatusMask ) {return 0;}
-      virtual ::DDS::DataWriterListener_ptr get_listener (void) {return 0;}
-      virtual ::DDS::Topic_ptr get_topic (void) {return 0;}
-      virtual ::DDS::Publisher_ptr get_publisher (void) {return 0;}
-      virtual ::DDS::ReturnCode_t wait_for_acknowledgments (const ::DDS::Duration_t & ) {return 0;}
-      virtual ::DDS::ReturnCode_t get_liveliness_lost_status (::DDS::LivelinessLostStatus & ) {return 0;}
-      virtual ::DDS::ReturnCode_t get_offered_deadline_missed_status (::DDS::OfferedDeadlineMissedStatus & ) {return 0;}
-      virtual ::DDS::ReturnCode_t get_offered_incompatible_qos_status (::DDS::OfferedIncompatibleQosStatus & ) {return 0;}
-      virtual ::DDS::ReturnCode_t get_publication_matched_status (::DDS::PublicationMatchedStatus & ) {return 0;}
-      virtual ::DDS::ReturnCode_t assert_liveliness (void) {return 0;}
-      virtual ::DDS::ReturnCode_t get_matched_subscriptions (::DDS::InstanceHandleSeq & ) {return 0;}
-      virtual ::DDS::ReturnCode_t get_matched_subscription_data (::DDS::SubscriptionBuiltinTopicData & ,
-      const ::DDS::InstanceHandle_t & ) {return 0;}
+      virtual ::DDS::ReturnCode_t
+      enable (void);
+
+      virtual ::DDS::StatusCondition_ptr
+      get_statuscondition (void);
+
+      virtual ::DDS::StatusMask
+      get_status_changes (void);
+
+      virtual ::DDS::InstanceHandle_t
+      get_instance_handle (void);
+
+      virtual ::DDS::ReturnCode_t
+      set_qos (const ::DDS::DataWriterQos & );
+
+      virtual ::DDS::ReturnCode_t
+      get_qos (::DDS::DataWriterQos & );
+
+      virtual ::DDS::ReturnCode_t
+      set_listener (::DDS::DataWriterListener_ptr ,
+                    ::DDS::StatusMask );
+
+      virtual ::DDS::DataWriterListener_ptr
+      get_listener (void);
+
+      virtual ::DDS::Topic_ptr
+      get_topic (void);
+
+      virtual ::DDS::Publisher_ptr
+      get_publisher (void);
+
+      virtual ::DDS::ReturnCode_t
+      wait_for_acknowledgments (const ::DDS::Duration_t & );
+
+      virtual ::DDS::ReturnCode_t
+      get_liveliness_lost_status (::DDS::LivelinessLostStatus & );
+
+      virtual ::DDS::ReturnCode_t
+      get_offered_deadline_missed_status (::DDS::OfferedDeadlineMissedStatus & );
+
+      virtual ::DDS::ReturnCode_t
+      get_offered_incompatible_qos_status (::DDS::OfferedIncompatibleQosStatus & );
+
+      virtual ::DDS::ReturnCode_t
+      get_publication_matched_status (::DDS::PublicationMatchedStatus & );
+
+      virtual ::DDS::ReturnCode_t
+      assert_liveliness (void);
+
+      virtual ::DDS::ReturnCode_t
+      get_matched_subscriptions (::DDS::InstanceHandleSeq & );
+
+      virtual ::DDS::ReturnCode_t
+      get_matched_subscription_data (::DDS::SubscriptionBuiltinTopicData & ,
+                                     const ::DDS::InstanceHandle_t & );
 
       void set_impl (::DDS::DataWriter_ptr dr);
 
