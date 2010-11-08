844af7145bfafa9acad7b1b5fc778078d63f4699
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2010-11-08 14:43:48 +0000

    Mon Nov  8 14:40:06 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/dds4ccm/tests/MultiTopic:
            * connectors/dds4ccm/tests/MultiTopic/Sender:
            * connectors/dds4ccm/tests/MultiTopic/Sender/MultiTopic_Sender.idl:
            * connectors/dds4ccm/tests/MultiTopic/Sender/MultiTopic_Sender_exec.h:
            * connectors/dds4ccm/tests/MultiTopic/Sender/MultiTopic_Sender.mpc:
            * connectors/dds4ccm/tests/MultiTopic/Sender/MultiTopic_Sender_exec.cpp:
            * connectors/dds4ccm/tests/MultiTopic/descriptors:
            * connectors/dds4ccm/tests/MultiTopic/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/MultiTopic/descriptors/run_test.pl:
            * connectors/dds4ccm/tests/MultiTopic/Receiver:
            * connectors/dds4ccm/tests/MultiTopic/Receiver/MultiTopic_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/MultiTopic/Receiver/MultiTopic_Receiver.idl:
            * connectors/dds4ccm/tests/MultiTopic/Receiver/MultiTopic_Receiver_exec.h:
            * connectors/dds4ccm/tests/MultiTopic/Receiver/MultiTopic_Receiver.mpc:
            * connectors/dds4ccm/tests/MultiTopic/Connector:
            * connectors/dds4ccm/tests/MultiTopic/Connector/MultiTopic_Connector.mpc:
            * connectors/dds4ccm/tests/MultiTopic/Connector/ccm_dds_mt.idl:
            * connectors/dds4ccm/tests/MultiTopic/Connector/MultiTopic_Connector.idl:
            * connectors/dds4ccm/tests/MultiTopic/Connector/MultiTopic_Connector_T.h:
            * connectors/dds4ccm/tests/MultiTopic/Base:
            * connectors/dds4ccm/tests/MultiTopic/Base/MultiTopic_Base.mpc:
            * connectors/dds4ccm/tests/MultiTopic/Base/MultiTopic_Base.idl:
            First frame for Multi Topic test.

---
+++ CIAO/connectors/dds4ccm/tests/MultiTopic/Connector/MultiTopic_Connector_T.h
@@ -0,0 +1,292 @@
+// $Id$
+
+#ifndef DDS_MT_CONNECTOR_T
+#define DDS_MT_CONNECTOR_T
+
+#include "connectors/dds4ccm/impl/DDS_TopicBase_Connector_T.h"
+
+template <typename DDS_TYPE, typename CCM_TYPE, bool FIXED, DDS4CCM_Vendor VENDOR_TYPE>
+class DDS_MT_Event_Connector_T
+  : public virtual DDS_TopicBase_Connector_T<DDS_TYPE, CCM_TYPE, VENDOR_TYPE>
+{
+    //square
+    virtual
+    typename CCM_TYPE::writer_type::_ptr_type
+    get_supplier_sq_data (void)
+    {
+      return CCM_TYPE::writer_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datawriter_type::_ptr_type
+    get_supplier_sq_dds_entity (void)
+    {
+      return CCM_TYPE::datawriter_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::getter_type::_ptr_type
+    get_pull_consumer_sq_fresh_data (void)
+    {
+      return CCM_TYPE::getter_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::reader_type::_ptr_type
+    get_pull_consumer_sq_data (void)
+    {
+      return CCM_TYPE::reader_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datareader_type::_ptr_type
+    get_pull_consumer_sq_dds_entity (void)
+    {
+      return CCM_TYPE::datareader_type::_nil ();
+    }
+
+    virtual
+    CCM_DDS::QueryFilter*
+    pull_consumer_sq_filter (void)
+    {
+      return 0;
+    }
+    virtual
+    void
+    pull_consumer_sq_filter (const CCM_DDS::QueryFilter&)
+    {
+    }
+    virtual
+    ::CCM_DDS::CCM_ContentFilterSetting_ptr
+     //CCM_DDS::CCM_ContentFilterSetting*
+    get_pull_consumer_sq_filter_config(void)
+    {
+      return CCM_DDS::CCM_ContentFilterSetting::_nil ();
+    }
+    virtual
+    CCM_DDS::CCM_ContentFilterSetting*
+    get_push_consumer_sq_filter_config(void)
+    {
+      return CCM_DDS::CCM_ContentFilterSetting::_nil ();
+    }
+
+    virtual
+    CCM_DDS::QueryFilter *
+    push_consumer_sq_filter (void)
+    {
+      return 0;
+    }
+
+    virtual
+    void
+    push_consumer_sq_filter (const ::CCM_DDS::QueryFilter &)
+    {
+    }
+
+    virtual
+    typename CCM_TYPE::reader_type::_ptr_type
+    get_push_consumer_sq_data (void)
+    {
+      return CCM_TYPE::reader_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datalistenercontrol_type::_ptr_type
+    get_push_consumer_sq_data_control (void)
+    {
+      return CCM_TYPE::datalistenercontrol_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datareader_type::_ptr_type
+    get_push_consumer_sq_dds_entity (void)
+    {
+      return CCM_TYPE::datareader_type::_nil ();
+    }
+
+//triangel
+    virtual
+    typename CCM_TYPE::writer_type::_ptr_type
+    get_supplier_tr_data (void)
+    {
+      return CCM_TYPE::writer_type::_nil ();
+    }
+    virtual
+    typename CCM_TYPE::datawriter_type::_ptr_type
+    get_supplier_tr_dds_entity (void)
+    {
+      return CCM_TYPE::datawriter_type::_nil ();
+    }
+    virtual
+    typename CCM_TYPE::getter_type::_ptr_type
+    get_pull_consumer_tr_fresh_data (void)
+    {
+      return CCM_TYPE::getter_type::_nil ();
+    }
+    virtual
+    typename CCM_TYPE::reader_type::_ptr_type
+    get_pull_consumer_tr_data (void)
+    {
+      return CCM_TYPE::reader_type::_nil ();
+    }
+    virtual
+    typename CCM_TYPE::datareader_type::_ptr_type
+    get_pull_consumer_tr_dds_entity (void)
+    {
+      return CCM_TYPE::datareader_type::_nil ();
+    }
+
+    virtual
+    CCM_DDS::QueryFilter*
+    pull_consumer_tr_filter (void)
+    {
+      return 0;
+    }
+    virtual
+    void
+    pull_consumer_tr_filter (const CCM_DDS::QueryFilter&)
+    {
+    }
+    virtual
+    CCM_DDS::CCM_ContentFilterSetting*
+    get_pull_consumer_tr_filter_config(void)
+    {
+      return CCM_DDS::CCM_ContentFilterSetting::_nil ();
+    }
+    virtual
+    CCM_DDS::CCM_ContentFilterSetting*
+    get_push_consumer_tr_filter_config(void)
+    {
+      return CCM_DDS::CCM_ContentFilterSetting::_nil ();
+    }
+
+    virtual
+    CCM_DDS::QueryFilter *
+    push_consumer_tr_filter (void)
+    {
+      return 0;
+    }
+
+    virtual
+    void
+    push_consumer_tr_filter (const ::CCM_DDS::QueryFilter &)
+    {
+    }
+
+    virtual
+    typename CCM_TYPE::reader_type::_ptr_type
+    get_push_consumer_tr_data (void)
+    {
+      return CCM_TYPE::reader_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datalistenercontrol_type::_ptr_type
+    get_push_consumer_tr_data_control (void)
+    {
+      return CCM_TYPE::datalistenercontrol_type::_nil ();
+    }
+    virtual
+    typename CCM_TYPE::datareader_type::_ptr_type
+    get_push_consumer_tr_dds_entity (void)
+    {
+      return CCM_TYPE::datareader_type::_nil ();
+    }
+
+  //circle
+
+    virtual
+    typename CCM_TYPE::writer_type::_ptr_type
+    get_supplier_cq_data (void)
+    {
+      return CCM_TYPE::writer_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datawriter_type::_ptr_type
+    get_supplier_cq_dds_entity (void)
+    {
+      return CCM_TYPE::datawriter_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::getter_type::_ptr_type
+    get_pull_consumer_cq_fresh_data (void)
+    {
+      return CCM_TYPE::getter_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::reader_type::_ptr_type
+    get_pull_consumer_cq_data (void)
+    {
+      return CCM_TYPE::reader_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datareader_type::_ptr_type
+    get_pull_consumer_cq_dds_entity (void)
+    {
+      return CCM_TYPE::datareader_type::_nil ();
+    }
+
+    virtual
+    CCM_DDS::QueryFilter*
+    pull_consumer_cq_filter (void)
+    {
+      return 0;
+    }
+    virtual
+    void
+    pull_consumer_cq_filter (const CCM_DDS::QueryFilter&)
+    {
+    }
+    virtual
+    CCM_DDS::CCM_ContentFilterSetting*
+    get_pull_consumer_cq_filter_config(void)
+    {
+      return CCM_DDS::CCM_ContentFilterSetting::_nil ();
+    }
+    virtual
+    CCM_DDS::CCM_ContentFilterSetting*
+    get_push_consumer_cq_filter_config(void)
+    {
+      return CCM_DDS::CCM_ContentFilterSetting::_nil ();
+    }
+
+    virtual
+    CCM_DDS::QueryFilter *
+    push_consumer_cq_filter (void)
+    {
+      return 0;
+    }
+
+    virtual
+    void
+    push_consumer_cq_filter (const ::CCM_DDS::QueryFilter &)
+    {
+    }
+
+    virtual
+    typename CCM_TYPE::reader_type::_ptr_type
+    get_push_consumer_cq_data (void)
+    {
+      return CCM_TYPE::reader_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datalistenercontrol_type::_ptr_type
+    get_push_consumer_cq_data_control (void)
+    {
+      return CCM_TYPE::datalistenercontrol_type::_nil ();
+    }
+
+    virtual
+    typename CCM_TYPE::datareader_type::_ptr_type
+    get_push_consumer_cq_dds_entity (void)
+    {
+      return CCM_TYPE::datareader_type::_nil ();
+    }
+};
+
+#endif
