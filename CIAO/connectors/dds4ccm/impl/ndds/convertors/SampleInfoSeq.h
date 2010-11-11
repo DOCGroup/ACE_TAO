282c5555cdcf9f82c807e47555e406dc8b104771
msmit <msmit@remedy.nl>
2010-11-11 09:47:02 +0000

    Thu Nov 11 09:52:13 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/CCM_DataReader.cpp:
            * connectors/dds4ccm/impl/CCM_DataWriter.cpp:
              Fuzz.
    
            * connectors/dds4ccm/impl/ndds/DataReader_T.h:
            * connectors/dds4ccm/impl/ndds/DataReader_T.cpp:
              Implemented the IDL representation of the ::DDS::DataReader.
              First step in cleaning this DataReader.
    
            * connectors/dds4ccm/impl/ndds/convertors/SampleInfo.h:
            * connectors/dds4ccm/impl/ndds/convertors/SampleInfoSeq.h:
              Converting DDS::SampleInfoSeq to DDS_SampleInfoSeq and
              DDS::SampleInfo to DDS_SampleInfo.

---
+++ CIAO/connectors/dds4ccm/impl/ndds/convertors/SampleInfoSeq.h
@@ -0,0 +1,36 @@
+/**
+ * @author William R. Otte <wotte@dre.vanderbilt.edu>
+ * @author Johnny Willemsen <jwillemsen@remedy.nl>
+ *
+ * $Id$
+ *
+ * Wrapper facade for NDDS.
+ */
+
+#ifndef SAMPLEINFOSEQ_H
+#define SAMPLEINFOSEQ_H
+
+#include "dds4ccm/impl/dds4ccm_conf.h"
+#include "SampleInfo.h"
+
+inline void
+operator<<= (::DDS::SampleInfoSeq &ddsinfo_seq, const ::DDS_SampleInfoSeq & info_seq)
+{
+  ddsinfo_seq.length (info_seq.length ());
+  for (::DDS_Long i = 0; i < info_seq.length (); ++i)
+    {
+      ddsinfo_seq[i] <<= info_seq[i];
+    }
+}
+
+inline void
+operator<<= (::DDS_SampleInfoSeq &ddsinfo_seq, const ::DDS::SampleInfoSeq & info_seq)
+{
+  ddsinfo_seq.length (info_seq.length ());
+  for (::CORBA::ULong i = 0; i < info_seq.length (); ++i)
+    {
+      ddsinfo_seq[i] <<= info_seq[i];
+    }
+}
+
+#endif /* DDS4CCM_SAMPLELOSTSTATUS_H */
