3c336c6eece4c7fee6209d7dabe9f290dfb14f1f
Johnny Willemsen <jwillemsen@remedy.nl>
2010-11-11 19:30:11 +0000

    Thu Nov 11 19:25:56 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/Coherent_Changes_Guard.h:
              Fixed export macro

--- CIAO/connectors/dds4ccm/impl/Coherent_Changes_Guard.h
+++ CIAO/connectors/dds4ccm/impl/Coherent_Changes_Guard.h
@@ -11,14 +11,14 @@
 #define _CIAO_COHERENT_CHANGES_GUARD
 
 #include "ace/Copy_Disabled.h"
-#include "dds4ccm/impl/ndds/dds4ccm_ndds_export.h"
+#include "dds4ccm/impl/dds4ccm_dds_impl_export.h"
 #include "dds4ccm/idl/dds_rtf2_dcpsC.h"
 
 namespace CIAO
 {
   namespace DDS4CCM
   {
-    class DDS4CCM_NDDS_Export Coherent_Changes_Guard :
+    class DDS4CCM_DDS_IMPL_Export Coherent_Changes_Guard :
       private ACE_Copy_Disabled
     {
       public:
