ce951d60be460e6157d6a3c90e10dd7c3d663b79
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2010-11-18 13:18:04 +0000

    Thu Nov 18 13:17:42 UTC 2010  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * connectors/dds4ccm/tests/MultipleTemp:
            * connectors/dds4ccm/tests/MultipleTemp/Sender:
            * connectors/dds4ccm/tests/MultipleTemp/Sender/MultipleTemp_Sender.mpc:
            * connectors/dds4ccm/tests/MultipleTemp/Sender/MultipleTemp_Sender_exec.cpp:
            * connectors/dds4ccm/tests/MultipleTemp/Sender/MultipleTemp_Sender.idl:
            * connectors/dds4ccm/tests/MultipleTemp/Sender/MultipleTemp_Sender_exec.h:
            * connectors/dds4ccm/tests/MultipleTemp/descriptors:
            * connectors/dds4ccm/tests/MultipleTemp/descriptors/Plan.cdp:
            * connectors/dds4ccm/tests/MultipleTemp/descriptors/run_test.pl:
            * connectors/dds4ccm/tests/MultipleTemp/Receiver:
            * connectors/dds4ccm/tests/MultipleTemp/Receiver/MultipleTemp_Receiver_exec.h:
            * connectors/dds4ccm/tests/MultipleTemp/Receiver/MultipleTemp_Receiver.mpc:
            * connectors/dds4ccm/tests/MultipleTemp/Receiver/MultipleTemp_Receiver_exec.cpp:
            * connectors/dds4ccm/tests/MultipleTemp/Receiver/MultipleTemp_Receiver.idl:
            * connectors/dds4ccm/tests/MultipleTemp/Connector:
            * connectors/dds4ccm/tests/MultipleTemp/Connector/MultipleTemp_Connector.idl:
            * connectors/dds4ccm/tests/MultipleTemp/Connector/MultipleTemp_Connector.mpc:
            * connectors/dds4ccm/tests/MultipleTemp/Base:
            * connectors/dds4ccm/tests/MultipleTemp/Base/MultipleTemp_Base.idl:
            * connectors/dds4ccm/tests/MultipleTemp/Base/MultipleTemp_Base.mpc:
           Test for multiple templates.

---
+++ CIAO/connectors/dds4ccm/tests/MultipleTemp/Receiver/MultipleTemp_Receiver_exec.cpp
@@ -0,0 +1,253 @@
+// -*- C++ -*-
+// $Id$
+
+// Test for MULTIPLE_TEMPLATES
+
+#include "MultipleTemp_Receiver_exec.h"
+#include "ace/Log_Msg.h"
+
+#include "ace/OS_NS_time.h"
+#include "dds4ccm/impl/Utils.h"
+#include "dds4ccm/impl/TimeUtilities.h"
+
+#include "dds4ccm/impl/dds4ccm_conf.h"
+
+namespace CIAO_MultipleTemp_Receiver_Impl
+{
+  CORBA::UShort received_one= 0;
+  CORBA::UShort received_two= 0;
+
+  //============================================================
+  // ListenOne_Listener_exec_i one
+  //============================================================
+  ListenOne_Listener_exec_i::ListenOne_Listener_exec_i (
+      ::MultipleTemp::CCM_Receiver_Context_ptr ctx)
+  : ciao_context_ (
+      ::MultipleTemp::CCM_Receiver_Context::_duplicate (ctx))
+
+  {
+  }
+
+  ListenOne_Listener_exec_i::~ListenOne_Listener_exec_i (void)
+  {
+  }
+
+  void
+  ListenOne_Listener_exec_i::on_one_data (
+                                  const TestTopicOne & an_instance,
+                                  const ::CCM_DDS::ReadInfo & info)
+  {
+    ACE_DEBUG ((LM_DEBUG, "ListenOne_Listener_exec_i::on_one_data: "
+                            "key <%C> - iteration <%d>\n",
+                            an_instance.keyOne.in (),
+                            an_instance.x));
+    if (!info.instance_handle.isValid)
+      {
+        ACE_ERROR ((LM_ERROR, "ERROR: ListenOne_Listener_exec_i::on_one_data: "
+                            "instance handle seems to be invalid "
+                            "key <%C> - iteration <%d>\n",
+                            an_instance.keyOne.in (),
+                            an_instance.x));
+      }
+    if (info.source_timestamp.sec == 0 &&
+        info.source_timestamp.nanosec == 0)
+      {
+        ACE_ERROR ((LM_ERROR, "ERROR: ListenOne_Listener_exec_i::on_one_data: "
+                            "source timestamp seems to be invalid (nil) "
+                            "key <%C> - iteration <%d>\n",
+                            an_instance.keyOne.in (),
+                            an_instance.x));
+      }
+    ++received_one;
+  }
+
+  void
+  ListenOne_Listener_exec_i::on_many_data (
+                                  const TestTopicOneSeq & ,
+                                  const ::CCM_DDS::ReadInfoSeq & )
+  {
+  }
+  //============================================================
+  // ListenTwo_Listener_exec_i twoe
+  //============================================================
+  ListenTwo_Listener_exec_i::ListenTwo_Listener_exec_i (
+      ::MultipleTemp::CCM_Receiver_Context_ptr ctx)
+  : ciao_context_ (
+      ::MultipleTemp::CCM_Receiver_Context::_duplicate (ctx))
+  {
+  }
+
+  ListenTwo_Listener_exec_i::~ListenTwo_Listener_exec_i (void)
+  {
+  }
+
+  void
+  ListenTwo_Listener_exec_i::on_one_data (
+                                  const TestTopicTwo & an_instance,
+                                  const ::CCM_DDS::ReadInfo & info)
+    {
+      ACE_DEBUG ((LM_DEBUG, "ListenTwo_Listener_exec_i::on_one_data: "
+                              "key <%C> - iteration <%d>\n",
+                              an_instance.keyTwo.in (),
+                              an_instance.y));
+      if (!info.instance_handle.isValid)
+        {
+          ACE_ERROR ((LM_ERROR, "ERROR: ListenTwo_Listener_exec_i::on_one_data: "
+                              "instance handle seems to be invalid "
+                              "key <%C> - iteration <%d>\n",
+                              an_instance.keyTwo.in (),
+                              an_instance.y));
+        }
+      if (info.source_timestamp.sec == 0 &&
+          info.source_timestamp.nanosec == 0)
+        {
+          ACE_ERROR ((LM_ERROR, "ERROR: ListenTwo_Listener_exec_i::on_one_data: "
+                              "source timestamp seems to be invalid (nil) "
+                              "key <%C> - iteration <%d>\n",
+                              an_instance.keyTwo.in (),
+                              an_instance.y));
+        }
+    ++received_two;
+  }
+
+  void
+  ListenTwo_Listener_exec_i::on_many_data (
+                                  const TestTopicTwoSeq & ,
+                                  const ::CCM_DDS::ReadInfoSeq & )
+  {
+  }
+
+  //============================================================
+  // Receiver_exec_i
+  //============================================================
+  Receiver_exec_i::Receiver_exec_i (void)
+  {
+  }
+
+  Receiver_exec_i::~Receiver_exec_i (void)
+  {
+  }
+
+  // Port operations.
+  ::CCM_DDS::CCM_PortStatusListener_ptr
+  Receiver_exec_i::get_info_out_one_status (void)
+  {
+    return ::CCM_DDS::CCM_PortStatusListener::_nil ();
+  }
+
+  ::CCM_DDS::CCM_PortStatusListener_ptr
+   Receiver_exec_i::get_info_out_two_status (void)
+   {
+     return ::CCM_DDS::CCM_PortStatusListener::_nil ();
+   }
+
+  ::MultipleTemp::MultipleTempConnector::CCM_Listener_ptr
+  Receiver_exec_i::get_info_out_one_data_listener (void)
+  {
+    if ( ::CORBA::is_nil (this->ciao_info_out_one_data_listener_.in ()))
+       {
+         info_out_one_data_listener_exec_i *tmp = 0;
+         ACE_NEW_RETURN (
+           tmp,
+           info_out_one_data_listener_exec_i (
+             this->ciao_context_.in ()),
+             :::MultipleTemp::MultipleTempConnector::CCM_Listener::_nil ());
+           this->ciao_info_out_one_data_listener_ = tmp;
+       }
+
+     return
+         ::MultipleTemp::MultipleTempConnector::CCM_Listener::_duplicate (
+         this->ciao_info_out_one_data_listener_.in ());
+   }
+
+  ::MultipleTemp::MultipleTempConnector::CCM_Listener_ptr
+   Receiver_exec_i::get_info_out_two_data_listener (void)
+   {
+    if ( ::CORBA::is_nil (this->ciao_info_out_two_data_listener_.in ()))
+        {
+          info_out_two_data_listener_exec_i *tmp = 0;
+          ACE_NEW_RETURN (
+            tmp,
+            info_out_two_data_listener_exec_i (
+              this->ciao_context_.in ()),
+              :::MultipleTemp::MultipleTempConnector::CCM_Listener::_nil ());
+            this->ciao_info_out_two_data_listener_ = tmp;
+        }
+
+      return
+          ::MultipleTemp::MultipleTempConnector::CCM_Listener::_duplicate (
+          this->ciao_info_out_two_data_listener_.in ());
+   }
+
+  // Operations from Components::SessionComponent.
+  void
+  Receiver_exec_i::set_session_context (
+    ::Components::SessionContext_ptr ctx)
+  {
+    this->ciao_context_ =
+      ::MultipleTemp::CCM_Receiver_Context::_narrow (ctx);
+    if ( ::CORBA::is_nil (this->context_.in ()))
+      {
+        throw ::CORBA::INTERNAL ();
+      }
+  }
+
+  void
+  Receiver_exec_i::configuration_complete (void)
+  {
+  }
+
+  void
+  Receiver_exec_i::ccm_activate (void)
+  {
+    ::CCM_DDS::DataListenerControl_var dlc_one =
+        this->context_->get_connection_info_out_one_data_control ();
+    ::CCM_DDS::DataListenerControl_var dlc_one =
+        this->context_->get_connection_info_out_two_data_control ();
+    dlc_one->mode (::CCM_DDS::ONE_BY_ONE);
+    dlc_one->mode (::CCM_DDS::ONE_BY_ONE);
+  }
+
+  void
+  Receiver_exec_i::ccm_passivate (void)
+  {
+
+  }
+
+  void
+  Receiver_exec_i::ccm_remove (void)
+  {
+    if (received_one != 5)
+      {
+        ACE_ERROR ((LM_ERROR, "ERROR: Receiver: "
+                               "Received wrong number of topic one: "
+                               "received <%C> - expected <5>\n",
+                               received_one));
+       }
+    if (received_two != 5)
+      {
+        ACE_ERROR ((LM_ERROR, "ERROR: Receiver: "
+                               "Received wrong number of topic two: "
+                               "received <%C> - expected <5>\n",
+                               received_two));
+       }
+    if ((received_two == 5) && (received_one != 5))
+      {
+        ACE_DEBUG ((LM_DEBUG, "OK: Receiver: "
+                               "Received received expected topics.\n"));
+      }
+  }
+
+  extern "C" RECEIVER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_MultipleTemp_Receiver_Impl (void)
+  {
+    ::Components::EnterpriseComponent_ptr retval =
+      ::Components::EnterpriseComponent::_nil ();
+
+    ACE_NEW_NORETURN (
+      retval,
+      Receiver_exec_i);
+
+    return retval;
+  }
+}
---
+++ CIAO/connectors/dds4ccm/tests/MultipleTemp/Receiver/MultipleTemp_Receiver_exec.h
@@ -0,0 +1,114 @@
+// -*- C++ -*-
+// $Id$
+
+#ifndef CIAO_RECEIVER_EXEC_H_
+#define CIAO_RECEIVER_EXEC_H_
+
+#include "MultipleTemp_ReceiverEC.h"
+
+#include /**/ "Receiver_exec_export.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "tao/LocalObject.h"
+
+
+namespace CIAO_MultipleTemp_Receiver_Impl
+{
+  class Receiver_exec_i;
+
+  //============================================================
+  // ListenOneByOneTest_Listener_exec_i one
+  //============================================================
+  class RECEIVER_EXEC_Export ListenOne_Listener_exec_i
+    : public virtual ::MultipleTemp::MultipleTempConnector::CCM_Listener,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    ListenOne_Listener_exec_i (
+        :::MultipleTemp::CCM_Receiver_Context_ptr ctx);
+
+    virtual ~ListenOne_Listener_exec_i (void);
+
+    virtual void
+    on_one_data (
+      const TestTopicOne& an_instance,
+      const ::CCM_DDS::ReadInfo & info);
+    virtual void
+    on_many_data (
+      const TestTopicOneSeq & an_instance,
+      const ::CCM_DDS::ReadInfoSeq & info);
+  private:
+    ::MultipleTemp::CCM_Receiver_Context_var ciao_context_;
+  };
+  //============================================================
+  // ListenOneByOneTest_Listener_exec_i two
+  //============================================================
+  class RECEIVER_EXEC_Export ListenTwo_Listener_exec_i
+    : public virtual ::MultipleTemp::MultipleTempConnector::CCM_Listener,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    ListenTwo_Listener_exec_i (
+        ::MultipleTemp::CCM_Receiver_Context_ptr ctx);
+
+    virtual ~ListenTwo_Listener_exec_i (void);
+
+    virtual void
+    on_one_data (
+      const TestTopicTwo& an_instance,
+      const ::CCM_DDS::ReadInfo & info);
+    virtual void
+      on_many_data (
+        const TestTopicTwoSeq & an_instance,
+        const ::CCM_DDS::ReadInfoSeq & info);
+  private:
+    ::MultipleTemp::CCM_Receiver_Context_var ciao_context_;
+  };
+  //============================================================
+  // Receiver_exec_i
+  //============================================================
+  class RECEIVER_EXEC_Export Receiver_exec_i
+    : public virtual Receiver_Exec,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    Receiver_exec_i (void);
+    virtual ~Receiver_exec_i (void);
+
+    // Port operations.
+    virtual ::MultipleTemp::MultipleTempConnector::CCM_Listener_ptr
+    get_info_out_one_data_listener (void);
+    virtual ::MultipleTemp::MultipleTempConnector::CCM_Listener_ptr
+    get_info_out_two_data_listener (void);
+
+
+    virtual ::CCM_DDS::CCM_PortStatusListener_ptr
+    get_info_out_one_status (void);
+    virtual ::CCM_DDS::CCM_PortStatusListener_ptr
+    get_info_out_two_status (void);
+
+    // Operations from Components::SessionComponent.
+    virtual void
+    set_session_context (
+      ::Components::SessionContext_ptr ctx);
+
+    virtual void configuration_complete (void);
+
+    virtual void ccm_activate (void);
+    virtual void ccm_passivate (void);
+    virtual void ccm_remove (void);
+
+  private:
+    ::MultipleTemp::CCM_Receiver_Context_var ciao_context_;
+    ::MultipleTemp::MultipleTempConnector::CCM_Listener_var ciao_info_out_one_data_listener_;
+    ::MultipleTemp::MultipleTempConnector::CCM_Listener_var ciao_info_out_two_data_listener_;
+   };
+
+  extern "C" RECEIVER_EXEC_Export ::Components::EnterpriseComponent_ptr
+  create_MultipleTemp_Receiver_Impl (void);
+}
+
+#endif /* ifndef */
