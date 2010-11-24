69f1f002a68c830bb3fde63de8334143d4de5a04
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-11-24 14:03:03 +0000

    Wed Nov 24 13:58:17 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * TAO_IDL/be/be_visitor_connector/connector_dds_ex_base.cpp:
            * TAO_IDL/be/be_visitor_connector/connector_dds_exh.cpp:
            * TAO_IDL/be/be_visitor_connector/connector_dds_exs.cpp:
              Always generate a bool fixed for each templated module argument

--- trunk/TAO/TAO_IDL/be/be_visitor_connector/connector_dds_exs.cpp
+++ trunk/TAO/TAO_IDL/be/be_visitor_connector/connector_dds_exs.cpp
@@ -71,7 +71,7 @@ be_visitor_connector_dds_exs::visit_connector (be_connector *node)
             }
           else
             {
-              os_ << dt_name;
+              os_ << dt_name << ",";
             }
 
           AST_Structure *s = AST_Structure::narrow_from_decl (d);
@@ -84,16 +84,13 @@ be_visitor_connector_dds_exs::visit_connector (be_connector *node)
                   s = AST_Structure::narrow_from_decl (td->primitive_base_type ());
                 }
             }
-          if (s)
+          if (s && s->size_type () == AST_Type::FIXED)
             {
-              if (s->size_type () == AST_Type::FIXED)
-                {
-                  os_ << be_nl << "true";
-                }
-              else
-                {
-                  os_ << be_nl << "false";
-                }
+              os_ << be_nl << "true";
+            }
+          else
+            {
+              os_ << be_nl << "false";
             }
           if (slot < this->t_args_->size ())
             {
