20216a5b35613ba2c180c8ef0fea2ac8b6032ba8
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-11-25 12:22:13 +0000

    Thu Nov 25 12:17:31 UTC 2010  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/OS_NS_unistd.h:
            * ace/OS_NS_unistd.cpp:
              Removed not enabled ftruncate workaround
    
            * ace/Env_Value_T.h:
              Doxygen change
    
            * ace/OS_NS_sys_utsname.cpp:
            * ace/OS_NS_sys_wait.cpp:
            * ace/OS_NS_time.cpp:
            * ace/OS_NS_wchar.cpp:
              Layout changes

--- trunk/ACE/ace/OS_NS_time.cpp
+++ trunk/ACE/ace/OS_NS_time.cpp
@@ -2,8 +2,6 @@
 
 #include "ace/OS_NS_time.h"
 
-
-
 #if !defined (ACE_HAS_INLINED_OSCALLS)
 # include "ace/OS_NS_time.inl"
 #endif /* ACE_HAS_INLINED_OSCALLS */
--- trunk/ACE/ace/OS_NS_wchar.cpp
+++ trunk/ACE/ace/OS_NS_wchar.cpp
@@ -2,8 +2,6 @@
 
 #include "ace/OS_NS_wchar.h"
 
-
-
 #if !defined (ACE_HAS_INLINED_OSCALLS)
 # include "ace/OS_NS_wchar.inl"
 #endif /* ACE_HAS_INLINED_OSCALLS */
