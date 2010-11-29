a19a3960439a66da09da77c836d9e24021ecf6e9
msmit <msmit@remedy.nl>
2010-11-29 13:17:02 +0000

    Mon Nov 29 13:17:52 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/DDS_Base_Connector_T.h:
              Added doxygen comments.

--- CIAO/connectors/dds4ccm/impl/DDS_Base_Connector_T.h
+++ CIAO/connectors/dds4ccm/impl/DDS_Base_Connector_T.h
@@ -46,15 +46,37 @@ public:
 
   virtual void set_session_context (::Components::SessionContext_ptr ctx);
 
+  /**
+    *
+    * Interface methods for the Deployment and Configuration Framework
+    */
+  //@{
   virtual void configuration_complete (void);
   virtual void ccm_remove (void);
   virtual void ccm_activate (void);
   virtual void ccm_passivate (void);
+  //@}
 
 protected:
+
+  /**
+   * Initialization of a connector.
+   *
+   * When one wants to use his own ::DDS::DomainParticipant, ::DDS::Topic,
+   * ::DDS::Subscriber and/or ::DDS::Publisher, these helper methods allows
+   * to create these, independant of the DDS vendor.
+   *
+   * See
+   * $CIAO_ROOT/connectors/dds4ccm/tests/MultiTopic/Connector/MultiTopic_Connector_T.cpp
+   * on how to use these methods.
+   *
+   * These methods will create the DDS4CCM Entities (and therefor the DDS
+   * entities in DDS itself), like DomainParticipant, Topic, Subscriber and Publisher
+   *
+   */
+  //@{
   void init_domain (::DDS::DomainParticipant_ptr & participant);
 
-  // Initialization
   void register_type    (::DDS::DomainParticipant_ptr participant,
                          const char * typesupport_name);
   void init_topic       (::DDS::DomainParticipant_ptr participant,
@@ -65,8 +87,16 @@ protected:
                          ::DDS::Publisher_ptr & publisher);
   void init_subscriber  (::DDS::DomainParticipant_ptr participant,
                          ::DDS::Subscriber_ptr & subscriber);
+  //@}
 
-  //activation
+  /**
+   * Activation of a connector.
+   *
+   * Helper methods to create the DDS listeners and attach them to the DDS Entities.
+   * This'll will activate the DDS Entities.
+   *
+   */
+  //@{
   void activate_topic       (ACE_Reactor* reactor,
                              ::DDS::Topic_ptr topic,
                              ::DDS::TopicListener_ptr & listener);
@@ -76,16 +106,51 @@ protected:
   void activate_subscriber  (ACE_Reactor* reactor,
                              ::DDS::Subscriber_ptr subscriber,
                              ::DDS::SubscriberListener_ptr & subscriber_listener);
+  //@}
 
-  //passivation
+  /**
+   * Passivation of a connector.
+   *
+   * Helper methods to detach the DDS listeners from the DDS Entities.
+   * This'll passivate the DDS Entities.
+   *
+   * In order to allow a thread safe and memory leak free passivation, one should
+   * use a temporary _var variable and assign this by using _retn () in the calling
+   * methods.
+   *
+   * For example:
+   *
+   * ::DDS::TopicListener_var tmp = this->topic_listener_._retn ();
+   * if (!::CORBA::is_nil (tmp.in ())) this->passivate_topi (this->topic_.in (),
+   *                                                         tmp.in ());
+   *
+   * Where topic_listener_ and topic_ are _var class members of a derived, user
+   * defined class.
+   *
+   * Again, see
+   * $CIAO_ROOT/connectors/dds4ccm/tests/MultiTopic/Connector/MultiTopic_Connector_T.cpp
+   * on how to passivate a connector.
+   *
+   */
+  //@{
   void passivate_topic      (::DDS::Topic_ptr topic,
                              ::DDS::TopicListener_ptr topic_listener);
   void passivate_publisher  (::DDS::Publisher_ptr publisher,
                              ::DDS::PublisherListener_ptr publisher_listener);
   void passivate_subscriber (::DDS::Subscriber_ptr subscriber,
                              ::DDS::SubscriberListener_ptr subscriber_listener);
+  //@}
 
-  //removal
+  /**
+   * Removal of a connector.
+   *
+   * Helper methods to remove the DDS Entities from memory.
+   *
+   * Again, one should use a temporary _var variable and _retn () in order to
+   * remove the DDS Entities in a thread safe and memory leak free manner.
+   * See 'Passivation of a connector' in this file.
+   */
+  //@{
   void remove_topic       (::DDS::DomainParticipant_ptr participant,
                            ::DDS::Topic_ptr topic);
   void remove_publisher   (::DDS::DomainParticipant_ptr participant,
@@ -96,22 +161,48 @@ protected:
 
   void unregister_type    (::DDS::DomainParticipant_ptr participant,
                            const char * typesupport_name);
+  //@}
 
   /// Get the reactor associated with this component
-  ACE_Reactor* reactor (void);
+  ACE_Reactor * reactor (void);
 
+  /**
+   * Attribute of DDS4CCM Base
+   */
+  //@{
   ::DDS::DomainId_t domain_id_;
   ::CORBA::String_var qos_profile_;
+  //@}
+
+  /**
+   * @name configuration_complete_
+   * Indicates whether configuration_complete has been invoked. Once invoked,
+   * changing a connectors attribute is not allowed anymore. If the user still
+   * wants to set an attribute, a NonChangeable exception is thrown.
+    */
   bool configuration_complete_;
+
+  /**
+    * Result of splitting qos_profile into to different names.
+    */
+  //@{
   char* library_name_;
   char* profile_name_;
+  //@}
 
   ::CIAO::DDS4CCM::Logger_Service * dlf_;
 
   ::DDS::DomainParticipant_var domain_participant_;
 
+  /**
+    * Context of the connector. Used to make connection to the user component
+    * like the interface to the data listeners.
+    */
   typename CCM_TYPE::context_type::_var_type context_;
 
+  /**
+    * DomainParticipantFactory. Administration of Domain Participants
+    */
   DomainParticipantFactory participant_factory_;
 };
 
