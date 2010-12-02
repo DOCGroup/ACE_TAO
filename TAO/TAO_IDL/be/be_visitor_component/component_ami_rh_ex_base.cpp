4ac9602c06b6b4d95121bfd1be5792705ae4cee6
parsons <parsons@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-12-02 14:14:05 +0000

    ChangeLogTag: Thu Dec  2 14:12:04 UTC 2010  Jeff Parsons <j.parsons@vanderbilt.edu>

---
+++ TAO/TAO_IDL/be/be_visitor_component/component_ami_rh_ex_base.cpp
@@ -0,0 +1,178 @@
+
+//=============================================================================
+/**
+ *  @file    component_ami_rh_ex_base.cpp
+ *
+ *  $Id$
+ *
+ *  Base class visitor generating code for AMI4CCM reply handler impl\
+ *  visitors.
+ *
+ *
+ *  @author Jeff Parsons
+ */
+//=============================================================================
+
+be_visitor_component_ami_rh_ex_base::be_visitor_component_ami_rh_ex_base (
+      be_visitor_context *ctx)
+  : be_visitor_scope (ctx),
+    iface_ (0),
+    scope_name_ (0),
+    iface_name_ (0),
+    smart_scope_ (0),
+    prefix_ ("AMI4CCM_"),
+    suffix_ ("ReplyHandler")
+{
+}
+
+be_visitor_component_ami_rh_ex_base::~be_visitor_component_ami_rh_ex_base (
+  void)
+{
+}
+
+int
+be_visitor_component_ami_rh_ex_base::post_process (be_decl *bd)
+{
+  // Has to be (1) an argument, (2) not an IN arg, and
+  // (3) not the last INOUT or OUT arg.
+  if (AST_Decl::NodeType () == AST_Decl::NT_argument)
+    {
+      AST_Argument *arg =
+        AST_Argument::narrow_from_decl (bd);
+
+      if (arg->direction () != AST_Argument::dir_IN
+          && ! this->last_inout_or_out_node (bd))
+        {
+          os_ << "," << be_nl;
+        }
+    }
+
+  return 0;
+}
+
+void
+be_visitor_component_ami_rh_ex_base::init (void)
+{
+  UTL_Scope *s = this->iface_->defined_in ();
+  AST_Decl *scope = ScopeAsDecl (s);
+  this->scope_name_ = scope->full_name ();
+  bool global = (scope->node_type () == AST_Decl::NT_root);
+  this->smart_scope_ = (global ? "" : "::");
+  this->iface_name_ =
+    this->iface_->original_local_name ()->get_string ();
+  this->class_name_ = ACE_CString (this->prefix_)
+                      + this->iface_name_
+                      + this->suffix_;
+}
+
+void
+be_visitor_component_ami_rh_ex_base::gen_excep_op (
+  const char *prefix,
+  be_decl *node,
+  bool for_defn)
+{
+  os_ << be_nl_2
+      << (for_defn ? "" : "virtual ") << "void" << be_nl;
+
+  if (for_defn)
+    {
+      os_ << this->class_name_ << "_i::";
+    }
+
+  os_ << prefix << node->local_name ()->get_string ()
+      << "_excep (" << be_idt_nl
+      << "::CCM_AMI::ExceptionHolder_ptr "
+      << (for_defn ? "/* " : "") << "excep_holder"
+      << (for_defn ? " */" : "") << ")"
+      << (for_defn ? "" : ";") << be_uidt;
+
+  this->gen_op_body ();
+}
+
+void
+be_visitor_component_ami_rh_ex_base::gen_op_body (void)
+{
+}
+
+int
+be_visitor_component_ami_rh_ex_base::gen_attr_op (
+  be_attribute *node,
+  bool for_defn)
+{
+  os_ << be_nl_2
+      << (for_defn ? "" : "virtual ") << "void" << be_nl;
+
+  if (for_defn)
+    {
+      os_ << this->class_name_ << "_i::";
+    }
+
+  os_ << "get_" << node->local_name ()->get_string ()
+      << " (" << be_idt_nl;
+
+  be_argument arg (AST_Argument::dir_IN,
+                   node->field_type (),
+                   node->name ());
+
+  be_visitor_args_arglist arg_visitor (this->ctx_);
+
+  if (for_defn)
+    {
+      arg_visitor.unused (true);
+    }
+
+  if (arg_visitor.visit_argument (&arg) == -1)
+    {
+      ACE_ERROR_RETURN ((LM_ERROR,
+                         ACE_TEXT ("be_visitor_component_ami_rh_ex_base")
+                         ACE_TEXT ("::gen_attr_op - ")
+                         ACE_TEXT ("attr arg gen failed\n")),
+                        -1);
+    }
+
+  os_ << ")" << (for_defn ? "" : ";") << be_uidt;
+
+  arg.destroy ();
+
+  this->gen_op_body ();
+
+  this->gen_excep_op ("get_", node, for_defn);
+
+  if (! node->readonly ())
+    {
+      os_ << be_nl_2
+          << (for_defn ? "" : "virtual ") << "void" << be_nl;
+
+      if (for_defn)
+        {
+          os_ << this->class_name_ << "_i::";
+        }
+
+      os_ << "set_" << node->local_name ()->get_string ()
+          << " (void)" << (for_defn ? "" : ";");
+
+      this->gen_op_body ();
+
+      this->gen_excep_op ("set_", node, for_defn);
+    }
+
+  return 0;
+}
+
+// ======================================================
+
+Exec_ReplyHandler_Op_Attr_Generator::Exec_ReplyHandler_Op_Attr_Generator (
+    be_visitor_scope * visitor)
+  : visitor_ (visitor)
+{
+}
+
+int
+Exec_ReplyHandler_Op_Attr_Generator::emit (
+  be_interface * /* derived_interface */,
+  TAO_OutStream * /* os */,
+  be_interface * base_interface)
+{
+  return visitor_->visit_scope (base_interface);
+}
+
