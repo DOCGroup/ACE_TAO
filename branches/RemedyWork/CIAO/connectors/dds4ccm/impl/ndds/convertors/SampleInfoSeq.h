2afa0a94aa6d1c284fc04623289e5b97200c9958
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-12-07 15:15:25 +0000

    Tue Dec  7 15:16:39 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/impl/SampleInfo.h:
            * connectors/dds4ccm/impl/ndds/DataReader_T.cpp:
            * connectors/dds4ccm/impl/ndds/convertors/SampleInfoSeq.h:
            * connectors/dds4ccm/impl/ndds/convertors/SampleInfo.h:
              Moved the conversion from ::DDS::SampleInfo to CCM_DDS::ReadInfo to
              the impl directory.

--- branches/RemedyWork/CIAO/connectors/dds4ccm/impl/ndds/convertors/SampleInfoSeq.h
+++ branches/RemedyWork/CIAO/connectors/dds4ccm/impl/ndds/convertors/SampleInfoSeq.h
@@ -11,7 +11,26 @@
 #define SAMPLEINFOSEQ_H
 
 #include "dds4ccm/impl/dds4ccm_conf.h"
-#include "SampleInfo.h"
+
+#include "Time_t.h"
+#include "InstanceHandle_t.h"
+
+inline void
+operator<<= (::DDS::SampleInfo& sample_info, const ::DDS_SampleInfo& dds_sample_info)
+{
+  sample_info.sample_state = dds_sample_info.sample_state;
+  sample_info.view_state = dds_sample_info.view_state;
+  sample_info.instance_state = dds_sample_info.instance_state;
+  sample_info.source_timestamp <<= dds_sample_info.source_timestamp;
+  sample_info.instance_handle <<= dds_sample_info.instance_handle;
+  sample_info.publication_handle <<= dds_sample_info.publication_handle;
+  sample_info.disposed_generation_count = dds_sample_info.disposed_generation_count;
+  sample_info.no_writers_generation_count = dds_sample_info.no_writers_generation_count;
+  sample_info.sample_rank = dds_sample_info.sample_rank;
+  sample_info.generation_rank = dds_sample_info.generation_rank;
+  sample_info.absolute_generation_rank = dds_sample_info.absolute_generation_rank;
+  sample_info.valid_data = dds_sample_info.valid_data;
+}
 
 inline void
 operator<<= (::DDS::SampleInfoSeq &ddsinfo_seq, const ::DDS_SampleInfoSeq & info_seq)
