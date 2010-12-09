0352de2da6dfd958dd61f598141c9afddc0bd7c2
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-12-09 13:25:39 +0000

    Thu Dec  9 13:25:55 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * bin/ciao_tests.lst:
            * connectors/dds4ccm/examples/IDL2CPPWrapper/*:
              Added test/example which tests the IDL to c++ wrapper. This is a
              small shapes sender/receiver application.

---
+++ branches/RemedyWork/CIAO/connectors/dds4ccm/examples/IDL2CPPWrapper/Shapes/Common/I2C_Shapes_Common.h
@@ -0,0 +1,52 @@
+// $Id$
+
+#ifndef I2C_SHAPES_COMMON_H_
+#define I2C_SHAPES_COMMON_H_
+
+#include "I2C_Shapes_Common_export.h"
+#include "Connector/I2C_Shapes_Traits.h"
+#include "connectors/dds4ccm/impl/ndds/DomainParticipant.h"
+#include "connectors/dds4ccm/impl/ndds/DomainParticipantFactory.h"
+#include "connectors/dds4ccm/impl/ndds/TypeSupport.h"
+
+/**
+ * Pulse generator
+ */
+
+class I2C_SHAPES_Export I2C_Shapes_Common
+{
+public:
+  I2C_Shapes_Common ();
+  ~I2C_Shapes_Common ();
+
+  //@{
+  /** Public methods. */
+  ::DDS::Publisher_ptr create_publisher (void);
+  ::DDS::Subscriber_ptr create_subscriber (void);
+  //@}
+
+  //@{
+  /** Getters. */
+  ::DDS::Topic_ptr get_topic (void);
+  //@}
+
+private:
+  typedef ::CIAO::NDDS::DDS_TypeFactory_T <ShapeType_DDS_Traits,
+                                           ShapeTypeSeq> dds_type_factory;
+
+  //@{
+  /** Members. */
+  ::CIAO::NDDS::DDS_DomainParticipantFactory_i participant_factory_;
+  ::DDS::DomainParticipant_var participant_;
+  ::CIAO::NDDS::DDS_TypeFactory_i * factory_;
+  ::DDS::Topic_var topic_;
+  //@}
+
+  //@{
+  /** Private methods. */
+  int get_domain_id (void);
+  void initialize (void);
+  //@}
+};
+
+#endif /* I2C_SHAPES_COMMON_H_ */
