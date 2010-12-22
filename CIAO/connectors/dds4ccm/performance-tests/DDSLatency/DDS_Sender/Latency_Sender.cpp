24e719e6f3fc5eb84207809a53de821e3f68d981
msmit <msmit@remedy.nl>
2010-12-22 14:01:21 +0000

    Wed Dec 22 14:03:38 UTC 2010  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/performance-tests/DDSLatency/DDS_Sender/Latency_Sender.cpp:
            * connectors/dds4ccm/performance-tests/DDSLatency/DDS_Sender/NDDS_Latency_Sender.cpp:
            * connectors/dds4ccm/performance-tests/Latency/Sender/LatencyTT_Test_Sender_exec.cpp:
              Resolved valgrind warnings/errors.

--- CIAO/connectors/dds4ccm/performance-tests/DDSLatency/DDS_Sender/Latency_Sender.cpp
+++ CIAO/connectors/dds4ccm/performance-tests/DDSLatency/DDS_Sender/Latency_Sender.cpp
@@ -166,11 +166,7 @@ stop (void)
 void
 init_values (void)
 {
-  if (duration_times_)
-    {
-      delete duration_times_;
-      duration_times_ = 0;
-    }
+  delete [] duration_times_;
   duration_times_ = new ACE_UINT64[iterations_];
   datalen_range_ = new CORBA::Short[nr_of_runs_];
   int start = 16;
@@ -219,11 +215,7 @@ void
 reset_results (void)
 {
   count_ = 0;
-  if (duration_times_)
-    {
-      delete duration_times_;
-      duration_times_ = 0;
-    }
+  delete [] duration_times_;
   duration_times_ = new ACE_UINT64[iterations_];
   tv_total_ = 0L;
   tv_max_ = 0L;
@@ -604,11 +596,9 @@ clean_exit:
             ACE_ERROR ((LM_ERROR, ACE_TEXT ("Deletion failed.\n")));
             main_result = 1;
           }
-    }
-    if (datalen_range_)
-      {
-        delete datalen_range_;
       }
+    delete [] datalen_range_;
+    delete [] duration_times_;
     return main_result;
 }
 
