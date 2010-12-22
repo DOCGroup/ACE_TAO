e8b6747c174baeaf44f2f57625732fcf79f1cbec
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2010-12-22 00:20:19 +0000

    Branch ACE 6.0 for Riverace support maintenance

---
+++ branches/Riverace-6.0/examples/Web_Crawler/main.cpp
@@ -0,0 +1,51 @@
+// $Id$
+
+// ============================================================================
+//
+// = LIBRARY
+//    examples/Web_Crawler
+//
+// = FILENAME
+//    main.cpp
+//
+// = DESCRIPTION
+//     This program implements a Web crawler that can be configured to
+//     apply various strategies to URLs that it visits.
+//
+// = AUTHOR
+//    Doug Schmidt <schmidt@cs.wustl.edu>
+//
+// ============================================================================
+
+#include "ace/OS_main.h"
+#include "ace/Signal.h"
+#include "Web_Crawler.h"
+#include "Options.h"
+
+
+
+void sig_handler (int)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT ("aborting!\n")));
+  ACE_OS::abort ();
+}
+
+int
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+{
+#if !defined (ACE_HAS_WINCE)
+  ACE_Sig_Action sa ((ACE_SignalHandler) sig_handler, SIGFPE);
+#endif
+  Web_Crawler crawler;
+
+  if (crawler.open (argc, argv) == -1)
+    return 1;
+  else if (crawler.run () == -1)
+    return 1;
+  else
+    return 0;
+}
+
+
+
