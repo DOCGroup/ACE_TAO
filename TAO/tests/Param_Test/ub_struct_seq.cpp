96751d3f3e848bbee802a64a9e3f12e3afb1dc19
Olli Savia <ops@iki.fi>
2011-01-12 13:01:19 +0000

    ChangeLogTag: Wed Jan 12 13:00:13 UTC 2011  Olli Savia  <ops@iki.fi>

--- TAO/tests/Param_Test/ub_struct_seq.cpp
+++ TAO/tests/Param_Test/ub_struct_seq.cpp
@@ -180,9 +180,9 @@ Test_Struct_Sequence::compare (const Param_Test::StructSeq &s1,
           || vs1.c != vs2.c
           || vs1.s != vs2.s
           || vs1.o != vs2.o
-          || vs1.f != vs2.f
+          || !ACE::is_equal (vs1.f, vs2.f)
           || vs1.b != vs2.b
-          || vs1.d != vs2.d )
+          || !ACE::is_equal (vs1.d, vs2.d))
         return 0;
     }
 
