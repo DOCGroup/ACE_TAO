ba4fd1b41224ec135cb74ff1de82acd4c6dd216f
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-01-26 11:33:58 +0000

    Wed Jan 26 11:29:44 UTC 2011  Marcel Smit  <msmit@remedy.nl>
            * tests/DAnCE/Components/SimpleConsumer_exec.h:
            * tests/DAnCE/Components/SimpleConsumer_exec.cpp:
            * tests/DAnCE/Components/SimpleEmitter_exec.cpp:
              Due to renaming of some ports.

--- branches/RemedyWork/CIAO/tests/DAnCE/Components/SimpleConsumer_exec.h
+++ branches/RemedyWork/CIAO/tests/DAnCE/Components/SimpleConsumer_exec.h
@@ -31,7 +31,7 @@ namespace CIAO_Simple_SimpleConsumer_Impl
     // Port operations.
 #if  !defined (CCM_NOEVENT)
     virtual void
-    push_hello_consumer (
+    push_hello_consumes (
       ::Simple::Hello *ev);
 #endif
     // Operations from Components::SessionComponent
