8c1737c1c6dfb7767fb6aae4c31359528bcb22cf
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-02-11 11:37:11 +0000

    reset Remedy work branch

--- tags/RemedyWorkMark/ADBC/adbc/Record.cpp
+++
@@ -1,7 +0,0 @@
-// $Id$
-
-#include "Record.h"
-
-#if !defined (__ADBC_INLINE__)
-#include "Record.inl"
-#endif
--- tags/RemedyWorkMark/TAO/orbsvcs/orbsvcs/Scheduler_Utilities.cpp
+++
@@ -1,8 +0,0 @@
-// $Id$
-
-#include "orbsvcs/Scheduler_Utilities.h"
-
-#if ! defined (__ACE_INLINE__)
-#include "orbsvcs/Scheduler_Utilities.inl"
-#endif /* __ACE_INLINE__ */
-
