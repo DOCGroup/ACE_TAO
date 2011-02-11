aa91e89623ea9c3869a1c2fe81b9d47f5ba3095e
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-02-11 11:37:43 +0000

    reset Remedy work branch

--- branches/RemedyWork/ACE/etc/xlc_dummy.cpp
+++
@@ -1,8 +0,0 @@
-// $Id$
-//
-// This file exists only to help with template instantiation when building
-// shared libraries on AIX using C Set++.  See rules.lib.GNU for usage.
-
-/* FUZZ: disable check_for_improper_main_declaration */
-
-main() {}
--- branches/RemedyWork/TAO/orbsvcs/orbsvcs/PortableGroup/PG_Location_Hash.cpp
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-// $Id$
-
-#include "orbsvcs/PortableGroup/PG_Location_Hash.h"
-
-#if !defined (__ACE_INLINE__)
-#include "orbsvcs/PortableGroup/PG_Location_Hash.inl"
-#endif /* __ACE_INLINE__ */
-
