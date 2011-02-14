f3a8869d137930f12e369d3044480792034ee706
Johnny Willemsen <jwillemsen@remedy.nl>
2011-02-14 12:07:35 +0000

    Mon Feb 14 12:03:02 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tao/Refcounted_ObjectKey.inl:
            * tao/Refcounted_ObjectKey.cpp:
              Committed back these files, also didn't break ImR

--- TAO/tao/Refcounted_ObjectKey.cpp
+++ TAO/tao/Refcounted_ObjectKey.cpp
@@ -20,19 +20,4 @@ TAO::Refcounted_ObjectKey::~Refcounted_ObjectKey (void)
 {
 }
 
-CORBA::ULong
-TAO::Refcounted_ObjectKey::decr_refcount (void)
-{
-  if (--this->ref_count_ > 0)
-    {
-      return this->ref_count_;
-    }
-
-  ACE_ASSERT (this->ref_count_ == 0);
-
-  delete this;
-
-  return 0;
-}
-
 TAO_END_VERSIONED_NAMESPACE_DECL
