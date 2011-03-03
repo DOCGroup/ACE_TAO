43d418dc719f5db1c71e1c89eeb3ae5c1764b152
William R. Otte <wotte@dre.vanderbilt.edu>
2011-03-03 05:06:17 +0000

    Thu Mar  3 05:05:20 UTC 2011  William R. Otte  <r._w@local>
            * tools/Config_Handlers/Any_Handler.cpp:
    
              Catch CORBA exceptions to give better error message.

--- DAnCE/tools/Config_Handlers/Any_Handler.cpp
+++ DAnCE/tools/Config_Handlers/Any_Handler.cpp
@@ -38,6 +38,16 @@ namespace DAnCE
 
           dyn->destroy ();
         }
+      catch (CORBA::Exception &ex)
+	{
+	  DANCE_ERROR (1, (LM_ERROR, DLINFO
+			   ACE_TEXT ("Any_Handler::extract_into_any - ")
+			   ACE_TEXT ("Caught CORBA Exception while extracting into ")
+			   ACE_TEXT ("dynany: %C\n"),
+			   ex._info ().c_str ()));
+	  throw Config_Error (ACE_TEXT (""), 
+			      ACE_TEXT ("CORBA Exception while extracting into dynany\n"));
+	}
       catch (Config_Error &ex)
         {
           throw ex;
