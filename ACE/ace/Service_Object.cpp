f90b9313e71e2924735b4fddd5d01ee59472b1b8
vzykov <vzykov@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-03-09 10:42:51 +0000

    Wed Mar  9 10:38:39 UTC 2011  Vladimir Zykov  <vladimir.zykov@prismtech.com>
            * ace/Service_Object.cpp:
              Fixed incorrect arguments for the provided format.
    
            * ace/Process.h:
              Reverted my change from 'Mon Mar  7 16:14:33 UTC 2011'. GCC cannot
              check formats in wide strings.

--- ACE/ace/Service_Object.cpp
+++ ACE/ace/Service_Object.cpp
@@ -36,9 +36,9 @@ ACE_Service_Type::dump (void) const
   // the generated C++ code.
   ACE_OS::fprintf(stderr,
                   "// [ST] dump, this=%p, name=%s, type=%p, so=%p, active=%d\n",
-                  this,
+                  static_cast<void const *> (this),
                   this->name_,
-                  this->type_,
+                  static_cast<void const *> (this->type_),
                   (this->type_ != 0) ? this->type_->object () : 0,
                   this->active_);
 
