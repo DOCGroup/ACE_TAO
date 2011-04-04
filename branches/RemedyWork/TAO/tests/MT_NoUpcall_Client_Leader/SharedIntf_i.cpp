e790d50a85d935eac1efa3022d1c96a2b3376264
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-04-04 12:11:35 +0000

    Cleanup of the code

--- branches/RemedyWork/TAO/tests/MT_NoUpcall_Client_Leader/SharedIntf_i.cpp
+++ branches/RemedyWork/TAO/tests/MT_NoUpcall_Client_Leader/SharedIntf_i.cpp
@@ -12,9 +12,9 @@ Test_Idl_SharedIntf_i::~Test_Idl_SharedIntf_i(void) {}
 
 void Test_Idl_SharedIntf_i::ping ()
 {
-   ACE_DEBUG((LM_DEBUG,"    (%P|%t) Test_Idl_SharedIntf::ping - sleeping for 5 seconds\n"));
+   ACE_DEBUG((LM_DEBUG,"(%P|%t) Test_Idl_SharedIntf::ping - sleeping for 5 seconds\n"));
    ACE_OS::sleep(5);
-   ACE_DEBUG((LM_DEBUG,"    (%P|%t) Test_Idl_SharedIntf::ping - waking up\n"));
+   ACE_DEBUG((LM_DEBUG,"(%P|%t) Test_Idl_SharedIntf::ping - waking up\n"));
 }
 
 void Test_Idl_SharedIntf_i::farewell ()
