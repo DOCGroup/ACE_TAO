aa9d8eb697921b0e2595f3fa37affa49ce24da4f
Johnny Willemsen <jwillemsen@remedy.nl>
2011-04-07 07:28:02 +0000

    Thu Apr  7 07:26:03 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/tests/QueryCondition/DDS/Base/Base.mpc:
            * connectors/dds4ccm/tests/QueryCondition/DDS/DDS_Receiver/DDS_Receiver.mpc:
            * connectors/dds4ccm/tests/QueryCondition/DDS/DDS_Receiver/QC_Receiver.cpp:
            * connectors/dds4ccm/tests/QueryCondition/DDS/DDS_Sender/DDS_Sender.mpc:
            * connectors/dds4ccm/tests/QueryCondition/DDS/DDS_Sender/QC_Sender.cpp:
            * connectors/dds4ccm/tests/QueryCondition/DDS_OneByOne/Base/Base_ObO.mpc:
            * connectors/dds4ccm/tests/QueryCondition/DDS_OneByOne/DDS_Receiver/DDS_ObO_Receiver.mpc:
            * connectors/dds4ccm/tests/QueryCondition/DDS_OneByOne/DDS_Receiver/ObO_Receiver.cpp:
            * connectors/dds4ccm/tests/QueryCondition/DDS_OneByOne/DDS_Sender/DDS_ObO_Sender.mpc:
            * connectors/dds4ccm/tests/QueryCondition/DDS_OneByOne/DDS_Sender/ObO_Sender.cpp:
              Simplified these plain RTI DDS tests, don't use the CCK and TAO

--- CIAO/connectors/dds4ccm/tests/QueryCondition/DDS/DDS_Sender/QC_Sender.cpp
+++ CIAO/connectors/dds4ccm/tests/QueryCondition/DDS/DDS_Sender/QC_Sender.cpp
@@ -66,6 +66,13 @@ void write (DDSDataWriter * writer)
   last_iteration_ = number_of_iterations_ * run_;
 }
 
+#if !defined (ACE_TMAIN)
+# define ACE_TMAIN main
+#endif
+
+#if !defined (ACE_TCHAR)
+# define ACE_TCHAR char
+#endif
 
 int ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
