04a34be9c9513bae5eff824e5301e382b61bab80
William R. Otte <wotte@dre.vanderbilt.edu>
2011-04-07 21:06:30 +0000

    Thu Apr  7 21:04:51 UTC 2011  William R. Otte  <wotte@dre.vanderbilt.edu>
            * ciao/Base/CIAO_Base.mpc:
            * ciao/Base/CIAO_Exceptions.idl:
            * ciao/Containers/Container_Base_T.cpp:
            * ciao/Containers/Session/Session_Container.cpp:
            * ciao/Deployment/Handlers/Component_Handler.cpp:
            * ciao/Deployment/Handlers/Home_Handler.cpp:
    
              Vastly improved error reporting for component loading problems.
              Plain CCM exceptions are no longer reported to DAnCE, and
              additional meta-data is now included that will report the exact
              source of library loading problems.

--- CIAO/ciao/Deployment/Handlers/Component_Handler.cpp
+++ CIAO/ciao/Deployment/Handlers/Component_Handler.cpp
@@ -3,6 +3,7 @@
 #include "Component_Handler.h"
 #include "ciao/Logger/Log_Macros.h"
 #include "ciao/Base/CIAO_PropertiesC.h"
+#include "ciao/Base/CIAO_ExceptionsC.h"
 #include "ciao/Containers/Container_BaseC.h"
 #include "ccm/CCM_ObjectC.h"
 #include "CIAO_State.h"
@@ -200,6 +201,15 @@ namespace CIAO
 
           container->set_attributes (comp_ref.in (),
                                      attr_config);
+        }
+      catch (::CIAO::Installation_Failure &ex)
+        {
+          CIAO_ERROR (1, (LM_ERROR, CLINFO
+                          "Component_Handler::install_instance - "
+                          "Caught Installation_Failure exception: %C:%C\n",
+                          ex.name.in (), ex.reason.in ()));
+          throw ::Deployment::StartError (ex.name.in (),
+                                          ex.reason.in ());
         }
       catch (const ::CORBA::Exception &ex)
         {
