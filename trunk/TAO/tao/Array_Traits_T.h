f94af610196858b9a361f6cdedc7076834c1c819
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-04-08 13:25:36 +0000

    Fri Apr  8 13:21:17 UTC 2011  Phil Mesnier  <mesnier_p@ociweb.com>
            * tao/Array_Traits_T.h:
            * tao/Generic_Sequence_T.h:
            * tao/Object_Reference_Traits_T.h:
            * tao/String_Traits_T.h:
            * tao/Value_Traits_T.h:
            * tao/Valuetype/Valuetype_Traits_T.h:
    
              Optimizations for growing unbounded sequences by changing the
              length. Shallow copies are now used whenever possible during the
              duplication of the existing sequence elements.

--- trunk/TAO/tao/Array_Traits_T.h
+++ trunk/TAO/tao/Array_Traits_T.h
@@ -82,11 +82,14 @@ struct array_traits
   }
 
   // Allow MSVC++ >= 8 checked iterators to be used.
+
+  /// @return true if shallow copy.
   template <typename iter>
-  inline static void copy_swap_range(
+  inline static bool copy_swap_range(
       value_type * begin, value_type * end, iter dst)
   {
     copy_range(begin, end, dst);
+    return false;
   }
 };
 
