5e866c031134995b1337eafd14262a906d2ec577
William R. Otte <wotte@dre.vanderbilt.edu>
2011-04-09 01:43:34 +0000

    Sat Apr  9 01:42:54 UTC 2011  William R. Otte  <wotte@dre.vanderbilt.edu>
            * tests/DAnCE/Launch-Failures/Component/Launch_Failure.idl:
            * tests/DAnCE/Launch-Failures/Component/Launch_Failure_exec.h:
            * tests/DAnCE/Launch-Failures/Component/Launch_Failure_exec.cpp:
            * tests/DAnCE/Launch-Failures/descriptors/DestEndpointFailure.cdp:
            * tests/DAnCE/Launch-Failures/descriptors/SourceEndpointFailure.cdp:
            * tests/DAnCE/Launch-Failures/descriptors/besteffort.localityconfig:
            * tests/DAnCE/Launch-Failures/descriptors/run_tests_besteffort.pl:
    
              Expanded tested failure cases.

--- CIAO/tests/DAnCE/Launch-Failures/Component/Launch_Failure_exec.cpp
+++ CIAO/tests/DAnCE/Launch-Failures/Component/Launch_Failure_exec.cpp
@@ -86,7 +86,7 @@ namespace CIAO_Launch_Failure_Impl
    */
 
   Launch_Failure_exec_i::Launch_Failure_exec_i (void)
-    : failure_reason_ (static_cast< ::Failure_Reason> (0UL))
+    : failure_reason_ (NO_ERROR)
   {
   }
 
@@ -101,6 +101,11 @@ namespace CIAO_Launch_Failure_Impl
   ::CCM_Remote_Interface_ptr
   Launch_Failure_exec_i::get_remote_out (void)
   {
+    if (this->failure_reason_ == FACET_REFERENCE_EXCEPTION)
+      {
+        throw 1;
+      }
+
     if ( ::CORBA::is_nil (this->ciao_remote_out_.in ()))
       {
         remote_out_exec_i *tmp = 0;
@@ -121,6 +126,11 @@ namespace CIAO_Launch_Failure_Impl
   ::CCM_Local_Interface_ptr
   Launch_Failure_exec_i::get_local_out (void)
   {
+    if (this->failure_reason_ == FACET_REFERENCE_EXCEPTION)
+      {
+        throw 1;
+      }
+
     if ( ::CORBA::is_nil (this->ciao_local_out_.in ()))
       {
         local_out_exec_i *tmp = 0;
@@ -138,26 +148,6 @@ namespace CIAO_Launch_Failure_Impl
         this->ciao_local_out_.in ());
   }
 
-  ::CCM_Local_Interface_ptr
-  Launch_Failure_exec_i::get_local_in (void)
-  {
-    if ( ::CORBA::is_nil (this->ciao_local_in_.in ()))
-      {
-        local_in_exec_i *tmp = 0;
-        ACE_NEW_RETURN (
-          tmp,
-          local_in_exec_i (
-            this->ciao_context_.in ()),
-            ::CCM_Local_Interface::_nil ());
-
-          this->ciao_local_in_ = tmp;
-      }
-
-    return
-      ::CCM_Local_Interface::_duplicate (
-        this->ciao_local_in_.in ());
-  }
-
   ::Failure_Reason
   Launch_Failure_exec_i::failure_reason (void)
   {
@@ -168,6 +158,9 @@ namespace CIAO_Launch_Failure_Impl
   Launch_Failure_exec_i::failure_reason (
     const ::Failure_Reason failure_reason)
   {
+    if (failure_reason == ATTRIBUTE_EXCEPTION)
+      throw 1;
+
     this->failure_reason_ = failure_reason;
   }
 
