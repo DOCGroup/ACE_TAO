0e057476728c7bb5cb929358340c9d7514019924
wotte <wotte@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-04-11 03:01:39 +0000

    Mon Apr 11 03:00:40 UTC 2011  William R. Otte  <wotte@dre.vanderbilt.edu>
            * tests/LocalityManager/CircularDependency:
            * tests/LocalityManager/CircularDependency/CircularDependency.mpc:
            * tests/LocalityManager/CircularDependency/CircularDependency_export.h:
            * tests/LocalityManager/CircularDependency/Deployment.cdp:
            * tests/LocalityManager/CircularDependency/Handler_A.h:
            * tests/LocalityManager/CircularDependency/Handler_A.cpp:
            * tests/LocalityManager/CircularDependency/Handler_B.h:
            * tests/LocalityManager/CircularDependency/Handler_B.cpp:
            * tests/LocalityManager/CircularDependency/run_test.pl:
    
              Test excercising the behavior when a circular dependency exists between
              installation handlers.  This has revealed a bug, which has yet to be fixed.

---
+++ trunk/DAnCE/tests/LocalityManager/CircularDependency/Handler_B.cpp
@@ -0,0 +1,106 @@
+// -*- C++ -*-
+// $Id$
+
+// TAO_IDL - Generated from
+// be/be_codegen.cpp:1560
+
+#include "tao/ORB_Core.h"
+#include "Handler_B.h"
+
+#include "dance/DAnCE_PropertiesC.h"
+
+#ifdef GEN_OSTREAM_OPS
+#include <iostream>
+#include <sstream>
+#endif /* GEN_OSTREAM_OPS */
+
+namespace DAnCE
+{
+  const char *
+  Handler_B_Impl::instance_type_ =
+    "Handler_B";
+
+  // Implementation skeleton constructor
+  Handler_B_Impl::
+  Handler_B_Impl (void)
+  {
+  }
+
+  // Implementation skeleton destructor
+  Handler_B_Impl::~Handler_B_Impl (void)
+  {
+  }
+
+  ::CORBA::StringSeq *
+  Handler_B_Impl::dependencies (void)
+  {
+    ::CORBA::StringSeq *retval (0);
+    ACE_NEW_THROW_EX (retval,
+                      ::CORBA::StringSeq (1),
+                      CORBA::NO_MEMORY ());
+
+    retval->length (1);
+    (*retval)[0] = "Handler_A";
+
+    return retval;
+  }
+
+  void
+  Handler_B_Impl::close (void)
+  {
+  }
+
+  char * Handler_B_Impl::instance_type (void)
+  {
+    DANCE_TRACE ("Handler_B_Impl::instance_type");
+    return CORBA::string_dup (Handler_B_Impl::instance_type_);
+  }
+
+  void
+  Handler_B_Impl::install_instance (const ::Deployment::DeploymentPlan & plan,
+                                       ::CORBA::ULong instanceRef,
+                                       ::CORBA::Any_out instance_reference)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  void
+  Handler_B_Impl::remove_instance (const ::Deployment::DeploymentPlan &,
+                                             ::CORBA::ULong,
+                                             const ::CORBA::Any &)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  void
+  Handler_B_Impl::connect_instance (const ::Deployment::DeploymentPlan &,
+                                              ::CORBA::ULong,
+                                              const ::CORBA::Any &)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  void
+  Handler_B_Impl::disconnect_instance (const ::Deployment::DeploymentPlan &,
+                                          ::CORBA::ULong)
+  {
+    throw CORBA::NO_IMPLEMENT ();
+  }
+
+  void
+  Handler_B_Impl::configure (const ::Deployment::Properties &prop )
+  {
+    ::DAnCE::Utility::PROPERTY_MAP pmap (prop.length ());
+
+    ::DAnCE::Utility::build_property_map (pmap,
+                                          prop);
+  }
+}
+
+extern "C"
+{
+  ::DAnCE::InstanceDeploymentHandler_ptr create_Handler_B (void)
+  {
+    return new DAnCE::Handler_B_Impl ();
+  }
+}
