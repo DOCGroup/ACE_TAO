b21f48d8bf8838126ee7540877bf4e2c0e983a1a
Johnny Willemsen <jwillemsen@remedy.nl>
2011-04-20 07:54:31 +0000

    ChangeLogTag:Wed Apr 20 09:52:52 CEST 2011  Johnny Willemsen  <jwillemsen@remedy.nl>

--- CIAO/ciao/Version.h
+++ CIAO/ciao/Version.h
@@ -5,5 +5,5 @@
 
 #define CIAO_MAJOR_VERSION 1
 #define CIAO_MINOR_VERSION 0
-#define CIAO_BETA_VERSION 1
-#define CIAO_VERSION "1.0.1"
+#define CIAO_BETA_VERSION 2
+#define CIAO_VERSION "1.0.2"
--- DAnCE/dance/Version.h
+++ DAnCE/dance/Version.h
@@ -5,5 +5,5 @@
 
 #define DAnCE_MAJOR_VERSION 1
 #define DAnCE_MINOR_VERSION 0
-#define DAnCE_BETA_VERSION 1
-#define DAnCE_VERSION "1.0.1"
+#define DAnCE_BETA_VERSION 2
+#define DAnCE_VERSION "1.0.2"
