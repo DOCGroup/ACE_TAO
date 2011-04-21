70eb2e8474345eb6af0341821f635e3c172476bb
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-04-21 13:03:43 +0000

    Thu Apr 21 13:02:25 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * bin/tao_orb_tests.lst:
              Updated test list
    
            * tests/AMI/mt_noupcall.conf:
            * tests/AMI/run_mt_noupcall.pl:
            * tests/AMI/run_test.pl:
              New run_mt_noupcall, the existing script was using -ORBCollocation no
              which doesn't work with AMI and mt_noupcall
    
            * tests/AMI/simple_client.cpp:
              Layout change

--- trunk/TAO/tests/AMI/run_mt_noupcall.pl
+++ trunk/TAO/tests/AMI/run_mt_noupcall.pl
@@ -11,22 +11,16 @@ use PerlACE::TestTarget;
 my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
 my $client = PerlACE::TestTarget::create_target (2) || die "Create target 2 failed\n";
 
-$client_conf = $client->LocalFile ("muxed$PerlACE::svcconf_ext");
+$client_conf = $client->LocalFile ("mt_noupcall$PerlACE::svcconf_ext");
 
 $server_debug_level = '0';
 $client_debug_level = '0';
 $iterations = '1';
 
 foreach $i (@ARGV) {
-    if ($i eq '-mux') {
-        $client_conf = $client->LocalFile ("muxed$PerlACE::svcconf_ext");
-    }
-    elsif ($i eq '-debug') {
+    if ($i eq '-debug') {
         $server_debug_level = '1';
         $client_debug_level = '1';
-    }
-    elsif ($i eq '-exclusive') {
-        $client_conf = $client->LocalFile ("exclusive$PerlACE::svcconf_ext");
     }
 }
 
@@ -66,24 +60,9 @@ $CL = $client->CreateProcess ("simple_client",
                               "-ORBsvcconf $client_conf "
                               . "-ORBdebuglevel $client_debug_level"
                               . " -k file://$client_iorfile "
-                              . " -i $iterations -d");
+                              . " -i $iterations -d -x");
 
 $client_status = $CL->SpawnWaitKill ($client->ProcessStartWaitInterval());
-
-if ($client_status != 0) {
-    print STDERR "ERROR: client returned $client_status\n";
-    $status = 1;
-}
-
-
-$CL2 = $client->CreateProcess ("simple_client",
-                               "-ORBsvcconf $client_conf"
-                               . " -ORBCollocation no"
-                               . " -ORBdebuglevel $client_debug_level"
-                               . " -k file://$client_iorfile "
-                               . " -i $iterations -x -d");
-
-$client_status = $CL2->SpawnWaitKill ($client->ProcessStartWaitInterval());
 
 if ($client_status != 0) {
     print STDERR "ERROR: client returned $client_status\n";
