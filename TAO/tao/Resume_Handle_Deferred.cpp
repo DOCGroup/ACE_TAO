b5910718da4d347179402e50195325054cef5bff
Johnny Willemsen <jwillemsen@remedy.nl>
2011-04-22 12:15:22 +0000

    Fri Apr 22 12:14:18 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tao/Connection_Handler.cpp:
            * tao/Resume_Handle_Deferred.h:
            * tao/Resume_Handle_Deferred.cpp:
              Notify a specific event handler
    
            * tao/Resume_Handle_Deferred.inl:
              Removed this file, virtual methods may not be inline

--- TAO/tao/Resume_Handle_Deferred.cpp
+++ TAO/tao/Resume_Handle_Deferred.cpp
@@ -5,13 +5,9 @@
 #include "tao/ORB_Core.h"
 #include "ace/Reactor.h"
 
-#if !defined (__ACE_INLINE__)
-#  include "Resume_Handle_Deferred.inl"
-#endif /* __ACE_INLINE__ */
-
 TAO_BEGIN_VERSIONED_NAMESPACE_DECL
 
-TAO_Resume_Handle_Deferred::TAO_Resume_Handle_Deferred  (TAO_ORB_Core* orbc, ACE_HANDLE h)
+TAO_Resume_Handle_Deferred::TAO_Resume_Handle_Deferred  (TAO_ORB_Core* orbc, ACE_Event_Handler* h)
       : ACE_Event_Handler (orbc->reactor())
       , h_ (h)
       , orbc_ (orbc)
@@ -28,7 +24,7 @@ TAO_Resume_Handle_Deferred:: handle_timeout (const ACE_Time_Value &, const void 
   // Send a notification to the reactor to cause the awakening of a new
   // follower, if there is one already available.
   ACE_Reactor *reactor = this->orbc_->reactor ();
-  int retval = reactor->notify (0, ACE_Event_Handler::READ_MASK);
+  int const retval = reactor->notify (this->h_, ACE_Event_Handler::READ_MASK);
   if (TAO_debug_level > 2)
     {
       // @@todo: need to think about what is the action that
@@ -36,9 +32,20 @@ TAO_Resume_Handle_Deferred:: handle_timeout (const ACE_Time_Value &, const void 
       ACE_DEBUG ((LM_DEBUG,
                   ACE_TEXT ("TAO (%P|%t) - TAO_Resume_Handle_Deferred[%d]::handle_timeout, ")
                   ACE_TEXT ("a connection_handler has been resumed, ")
-                  ACE_TEXT ("notified the reactor, retval=%d.\n"), h_, retval));
+                  ACE_TEXT ("notified the reactor, retval=%d.\n"), h_->get_handle (), retval));
     }
   return -1; // -1 will cause the reactor to call handle_close() and dispose of the instance
+}
+
+int
+TAO_Resume_Handle_Deferred::handle_close (ACE_HANDLE, ACE_Reactor_Mask)
+{
+  if (TAO_debug_level > 5)
+    ACE_DEBUG ((LM_DEBUG,
+                ACE_TEXT ("TAO (%P|%t) - TAO_Resume_Handle_Deferred[%d]::handle_close, ")
+                ACE_TEXT ("performing a ritual sepuku\n"), this->h_->get_handle ()));
+  delete this;
+  return 0;
 }
 
 TAO_END_VERSIONED_NAMESPACE_DECL
