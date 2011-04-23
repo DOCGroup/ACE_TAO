7c6e3d8401de910567b85e4bb67daa1f5e5d45d2
msmit <msmit@remedy.nl>
2011-04-23 19:48:50 +0000

    Sat Apr 23 19:48:23 UTC 2011  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender_exec.cpp:
            * connectors/dds4ccm/tests/Getter/Sender/Getter_Test_Sender_exec.cpp:
            * connectors/dds4ccm/tests/UnkeyedWriter/Sender/Writer_Sender_exec.cpp:
            * connectors/tests/AMIDDS/Sender/AmiDds_Sender_exec.cpp:
              Deleting the ticker in a consistent manner. Created in the constructor,
              deleting it in the destructor.

--- CIAO/connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender_exec.cpp
+++ CIAO/connectors/dds4ccm/tests/CSLSampleRejected/Sender/CSL_SRTest_Sender_exec.cpp
@@ -135,7 +135,6 @@ namespace CIAO_CSL_SRTest_Sender_Impl
   {
     this->reactor ()->cancel_timer (this->ticker_);
     ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Sender_exec_i::stop : Timer canceled.\n")));
-    delete this->ticker_;
   }
 
   void
--- CIAO/connectors/dds4ccm/tests/UnkeyedWriter/Sender/Writer_Sender_exec.cpp
+++ CIAO/connectors/dds4ccm/tests/UnkeyedWriter/Sender/Writer_Sender_exec.cpp
@@ -283,7 +283,6 @@ namespace CIAO_Writer_Sender_Impl
   {
     this->reactor ()->cancel_timer (this->ticker_);
     ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Sender_exec_i::stop : Timer canceled.\n")));
-    delete this->ticker_;
   }
 
   // Component attributes and port operations.
