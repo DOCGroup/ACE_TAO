c05ea6b99ea70bd2597f2b7c8bf9b87350875dde
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2011-05-11 07:10:45 +0000

    Tue May 11 07:09:37 UTC 2011  Marijke Hengstmengel  <mhengstmengel@remedy.nl>
            * examples/Display/descriptors/run_test_more.pl:
            * examples/Display/descriptors/deploymentplanone.cdp:
            * examples/Display/descriptors/deploymentplanmore.cdp:
            * examples/Display/descriptors/run_test_more_contr.pl:
            * examples/Display/descriptors/flattened_deploymentplannat.cdp:
            * examples/Display/descriptors/run_all_in_one.pl:
            * examples/Display/descriptors/run_test.pl:
            * examples/Display/descriptors/run_test_one.pl:
            Solved Fuzz errors.

--- CIAO/examples/Display/descriptors/run_test_more.pl
+++ CIAO/examples/Display/descriptors/run_test_more.pl
@@ -119,7 +119,6 @@ sub run_node_daemons {
 #        $d_param = "-ORBEndpoint $iiop -s $node_app -n $nodename=$iorfile -t 30 --domain-nc corbaloc:rir:/NameService";
        $d_param = "-s $node_app -n $nodename=$iorfile -t 30 --domain-nc corbaloc:rir:/NameService";
 
-  
         print "Run dance_node_manager with $d_param\n";
 
         $DEAMONS[$i] = $tg_daemons[$i]->CreateProcess ($d_cmd, $d_param);
