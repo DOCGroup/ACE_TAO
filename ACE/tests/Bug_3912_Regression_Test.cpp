3b1eb5b1089fd50cf4192944bb62acb2a4834def
cbeaulac <cbeaulac@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-05-12 01:09:41 +0000

    Thu May 12 01:04:19 UTC 2011   Chad Beaulac  <chad.beaulac@objectivesolutions.com> * tests/Bug_3912_Regression_Test.cpp:   Integrated changes from Phil Mesnier.   Added static_cast and const_cast instead of C-style casts.

--- ACE/tests/Bug_3912_Regression_Test.cpp
+++ ACE/tests/Bug_3912_Regression_Test.cpp
@@ -15,14 +15,17 @@
 // ============================================================================
 
 #include "test_config.h"
-#include "Service_Config_Stream_DLL.h"
 
 #include "ace/Log_Msg.h"
 #include "ace/Service_Config.h"
 #include "ace/Service_Repository.h"
 #include "ace/Service_Object.h"
 #include "ace/Service_Types.h"
+#include "ace/Task.h"
+#include "ace/Module.h"
 
+typedef ACE_Task<ACE_SYNCH> MT_Task;
+typedef ACE_Module<ACE_SYNCH> MT_Module;
 
 /**
  * We use this Task to track if close was called.
@@ -31,21 +34,98 @@ class Close_Handler : public virtual MT_Task
 {
 public:
 
-  Close_Handler(int* close_called_arg) : close_called(close_called_arg)
+  Close_Handler(bool* close_called_arg)
+  : close_called_ (close_called_arg)
   {
   }
 
-  virtual int
-  close(u_long flags)
-  {
-    *close_called = 1;
-    return 0;
-  }
+  virtual int close(u_long );
 
 private:
-  int* close_called;
-
+  bool* close_called_;
 };
+
+
+int Close_Handler::close(u_long )
+{
+  ACE_DEBUG ((LM_DEBUG,"Close_Handler::close \n"));
+  *close_called_ = true;
+  return 0;
+}
+
+int
+run_test (int argc, ACE_TCHAR *argv[])
+{
+  int status = 0;
+  bool close_called = false;
+  Close_Handler* close_handler = 0;
+  ACE_NEW_RETURN(close_handler, Close_Handler (&close_called), -1);
+
+  status = ACE_Service_Config::open (argc,
+                                     argv,
+                                     ACE_DEFAULT_LOGGER_KEY,
+                                     true,
+                                     true /*ignore def svc.conf*/);
+  if (status != 0)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       ACE_TEXT ("run_test, %p\n")
+                       ACE_TEXT ("ACE_Service_Config::open")),
+                       status);
+
+  ACE_Service_Repository *asr = ACE_Service_Repository::instance ();
+  const ACE_Service_Type* st = 0;
+  status = asr->find (ACE_TEXT ("Close_Test_Module"), &st);
+  if (status != 0)
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       ACE_TEXT ("run_test, %p on Close_Test_Module\n")
+                       ACE_TEXT ("ACE_Service_Repository::find")),
+                       status);
+
+  //
+  // Put our Close_Handler Task into the Module
+  //
+  MT_Module* close_test_module =
+    static_cast <MT_Module *> (st->type()->object ());
+
+  close_test_module->reader (close_handler);
+
+  //
+  // Remove the Module from the Stream.
+  // This is what happens during ACE_Service_Repository::fini
+  // We want to make sure that close is called on Modules and their Tasks
+  // at this time to ensure proper cleanup during the shutdown sequence
+  // by getting the close methods called so user shutdown hooks can fire.
+  //
+
+  const ACE_Module_Type* module_type =
+    static_cast< const ACE_Module_Type*>(st->type ());
+
+  status = asr->find (ACE_TEXT ("Close_Test_Stream"), &st);
+  if (status != 0)
+  {
+    ACE_ERROR_RETURN ((LM_ERROR,
+                       ACE_TEXT ("run_test, %p on Close_Test_Stream\n")
+                       ACE_TEXT ("ACE_Service_Repository::find")),
+                       status);
+  }
+  const ACE_Stream_Type* close_test_stream =
+    static_cast<const ACE_Stream_Type*> (st->type ());
+
+  ACE_Stream_Type *nc_stream = const_cast<ACE_Stream_Type*>(close_test_stream);
+  ACE_Module_Type *nc_module = const_cast<ACE_Module_Type*>(module_type);
+  nc_stream->remove (nc_module);
+
+  if (!close_called)
+    {
+      ACE_ERROR ((LM_ERROR, ACE_TEXT ("close not called\n")));
+      status++;
+    }
+  else
+    {
+      ACE_DEBUG ((LM_DEBUG, ACE_TEXT (" SUCCESS: close called\n")));
+    }
+  return status;
+}
 
 int
 run_main(int, ACE_TCHAR *argv[])
@@ -56,72 +136,8 @@ run_main(int, ACE_TCHAR *argv[])
                           const_cast<ACE_TCHAR*> (ACE_TEXT ("-f")),
                           const_cast<ACE_TCHAR*>
                           (ACE_TEXT ("Bug_3912_Regression_Test.conf"))};
-  int status = 0;
 
-  int close_called = 0;
-  Close_Handler* close_handler = new Close_Handler (&close_called);
-
-  if ((status = ACE_Service_Config::open (3,
-                                          _argv,
-                                          ACE_DEFAULT_LOGGER_KEY,
-                                          true,
-                                          true /*ignore def svc.conf*/)) == -1)
-    ACE_ERROR ((LM_ERROR,
-                ACE_TEXT ("%p\n"),
-                ACE_TEXT ("open"),
-                1));
-
-  const ACE_Service_Type* st = 0;
-  if (ACE_Service_Repository::instance ()->find ("Close_Test_Module", &st) != 0)
-  {
-    ACE_ERROR ((LM_ERROR,
-                ACE_TEXT ("%p\n"),
-                ACE_TEXT ("Couldn't find Close_Test_Module in ASR"),
-                1));
-    status++;
-  }
-  else
-  {
-    //
-    // Put our Close_Handler Task into the Module
-    //
-    MT_Module* close_test_module = (MT_Module*) (st->type ()->object ());
-    close_test_module->reader (close_handler);
-  }
-  //
-  // Find the Stream that the Module is in.
-  //
-  const ACE_Service_Type* st2 = 0;
-  if (ACE_Service_Repository::instance ()->find ("Close_Test_Stream", &st2) != 0)
-  {
-    ACE_ERROR ((LM_ERROR,
-                ACE_TEXT ("%p\n"),
-                ACE_TEXT ("Couldn't find Close_Test_Stream in ASR"),
-                1));
-    status++;
-  }
-  else
-  {
-    //
-    // Remove the Module from the Stream.
-    // This is what happens during ACE_Service_Repository::fini
-    // We want to make sure that close is called on Modules and their Tasks
-    // at this time to ensure proper cleanup during the shutdown sequence
-    // by getting the close methods called so user shutdown hooks can fire.
-    //
-    ACE_Stream_Type* close_test_stream = (ACE_Stream_Type*) (st2->type ());
-
-    close_test_stream->remove ((ACE_Module_Type*)(st->type ()));
-
-   if (close_called != 1)
-    {
-      ACE_ERROR ((LM_ERROR,
-                  ACE_TEXT ("%p\n"),
-                  ACE_TEXT ("Module::close not called"),
-                  1));
-      status++;
-    }
-  }
+  int status = run_test (3,_argv);
 
   ACE_Service_Config::fini_svcs ();
 
