892e778e99be95addaa74e8670bcd36a70e29d72
msmit <msmit@remedy.nl>
2011-05-26 11:48:07 +0000

    Thu May 26 13:48:29 UTC 2011  Marcel Smit  <msmit@remedy.nl>
            * tests/Sequence_Unit_Tests/Bounded_Simple_Types.cpp:
            * tests/Sequence_Unit_Tests/Bounded_String.cpp:
            * tests/Sequence_Unit_Tests/Unbounded_Octet.cpp:
            * tests/Sequence_Unit_Tests/Unbounded_Simple_Types.cpp:
            * tests/Sequence_Unit_Tests/Unbounded_String.cpp:
              Catch uncaught exceptions in main.

--- TAO/tests/Sequence_Unit_Tests/Bounded_Simple_Types.cpp
+++ TAO/tests/Sequence_Unit_Tests/Bounded_Simple_Types.cpp
@@ -9,6 +9,7 @@
  * @author Carlos O'Ryan
  */
 #include "tao/Bounded_Value_Sequence_T.h"
+#include "ace/Log_Msg.h"
 
 struct Foo
 {
@@ -20,54 +21,62 @@ CORBA::ULong const TEST_FOO_MAX = 64;
 
 int ACE_TMAIN (int, ACE_TCHAR *[])
 {
-  typedef TAO::bounded_value_sequence<int,TEST_INT_MAX> int_sequence;
-
-  int_sequence a;
-  int_sequence b(a);
-
-  int_sequence c(0, int_sequence::allocbuf(), true);
-
-  a = b;
-
-  a.length(c.maximum());
-  if (a.release())
-  {
-    b.length(a.length());
-  }
-
-  a[0] = 0;
-  b[0] = a[0];
-
-  int_sequence const & d = a;
   try
     {
-      c[0] = d[0];
+      typedef TAO::bounded_value_sequence<int,TEST_INT_MAX> int_sequence;
+
+      int_sequence a;
+      int_sequence b(a);
+
+      int_sequence c(0, int_sequence::allocbuf(), true);
+
+      a = b;
+
+      a.length(c.maximum());
+      if (a.release())
+      {
+        b.length(a.length());
+      }
+
+      a[0] = 0;
+      b[0] = a[0];
+
+      int_sequence const & d = a;
+      try
+        {
+          c[0] = d[0];
 #if defined (TAO_CHECKED_SEQUENCE_INDEXING) && (TAO_CHECKED_SEQUENCE_INDEXING == 1)
-      return 1;
+          return 1;
 #endif
+        }
+      catch (const ::CORBA::BAD_PARAM &)
+        {
+          // c has length = 0, so there is an exception when we try
+          // to access element above length.
+        }
+
+      b.replace(0, int_sequence::allocbuf(), true);
+
+      int const * x = d.get_buffer();
+      if (x != 0)
+      {
+        int_sequence::freebuf(a.get_buffer(true));
+      }
+      x = b.get_buffer();
+
+      int_sequence e(c);
+
+      typedef TAO::bounded_value_sequence<Foo,TEST_FOO_MAX> Foo_sequence;
+
+      Foo_sequence u;
+      Foo_sequence v(u);
+      u = v;
+
     }
-  catch (const ::CORBA::BAD_PARAM &)
+  catch (const ::CORBA::Exception &)
     {
-      // c has length = 0, so there is an exception when we try
-      // to access element above length.
+      ACE_ERROR ((LM_ERROR, "Caught unexpected exception\n"));
+      return 1;
     }
-
-  b.replace(0, int_sequence::allocbuf(), true);
-
-  int const * x = d.get_buffer();
-  if (x != 0)
-  {
-    int_sequence::freebuf(a.get_buffer(true));
-  }
-  x = b.get_buffer();
-
-  int_sequence e(c);
-
-  typedef TAO::bounded_value_sequence<Foo,TEST_FOO_MAX> Foo_sequence;
-
-  Foo_sequence u;
-  Foo_sequence v(u);
-  u = v;
-
   return 0;
 }
