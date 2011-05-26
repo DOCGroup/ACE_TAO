8b440a52fc9d235f18ce572419d8bd31505ee9f0
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-05-26 12:11:42 +0000

    Thu May 26 14:11:57 UTC 2011  Marcel Smit  <msmit@remedy.nl>
            * ace/Monitor_Control/Linux_Network_Interface_Monitor.cpp:
              Resolved gcc 4.6 warning: variable was set but not used.

--- trunk/ACE/ace/Monitor_Control/Linux_Network_Interface_Monitor.cpp
+++ trunk/ACE/ace/Monitor_Control/Linux_Network_Interface_Monitor.cpp
@@ -39,8 +39,8 @@ namespace ACE
 
       /// Ignore the first 2 lines of the file, which are file
       /// and column headers.
-      void* dummy = ACE_OS::fgets (buf, sizeof (buf), fp);
-      dummy = ACE_OS::fgets (buf, sizeof (buf), fp);
+      ACE_OS::fgets (buf, sizeof (buf), fp);
+      ACE_OS::fgets (buf, sizeof (buf), fp);
 
       unsigned long iface_value = 0UL;
       ACE_UINT32 iface_index = 0UL;
