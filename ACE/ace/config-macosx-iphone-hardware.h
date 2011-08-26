4242cf806a408200ae9f41388a2558a387fa24e4
Abdullah Sowayan <sowayan@users.noreply.github.com>
2011-08-26 16:00:33 +0000

    Fri Aug 26 15:59:19 UTC 2011  Abdul Sowayan  <sowayan@gmail.com>

--- ACE/ace/config-macosx-iphone-hardware.h
+++ ACE/ace/config-macosx-iphone-hardware.h
@@ -5,11 +5,11 @@
 #define ACE_HAS_IPHONE
 #define ACE_SIZEOF_LONG_DOUBLE 8
 
-#include "ace/config-macosx-snowleopard.h"
+#include "ace/config-macosx-lion.h"
 
 #ifdef ACE_HAS_SYSV_IPC
 #undef ACE_HAS_SYSV_IPC
 #endif
 
-#endif ACE_CONFIG_MACOSX_IPHONE_HARDWARE_H
+#endif /* ACE_CONFIG_MACOSX_IPHONE_HARDWARE_H */
 
