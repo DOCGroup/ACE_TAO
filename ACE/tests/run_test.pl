08f6064c0a595dbf4fd1d300b2af02e2780ba7c6
Johnny Willemsen <jwillemsen@remedy.nl>
2011-09-11 18:56:14 +0000

    Use target localfile

--- ACE/tests/run_test.pl
+++ ACE/tests/run_test.pl
@@ -204,8 +204,9 @@ sub check_log ($)
     # found in the SSL subdirectory.
     local $the_program = basename($program);
     local $log = "log/".$the_program.$log_suffix;
+    local $target_log = $target->LocalFile ($log);
 
-    if ($target->GetFile ($log) == -1) {
+    if ($target->GetFile ($target_log) == -1) {
         print STDERR "ERROR: cannot retrieve file <$log>\n";
     }
 
