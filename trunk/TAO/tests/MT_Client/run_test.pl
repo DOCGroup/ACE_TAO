4abbfd146bef3889d96691d01bd73607c2c99e7f
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-09-21 13:41:56 +0000

    Wed Sep 21 13:40:30 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/MT_Client/run_test.pl:
              Removed conf files that are not in the repo at all

--- trunk/TAO/tests/MT_Client/run_test.pl
+++ trunk/TAO/tests/MT_Client/run_test.pl
@@ -15,16 +15,14 @@ $conf = $PerlACE::svcconf_ext;
 
 $client_process = "client";
 
-$client_conf = "client.global" . $conf;
+$client_conf = "client" . $conf;
 $server_conf = "server" . $conf;
 
 $threads = '4';
 $iterations = '1000';
 
 foreach $i (@ARGV) {
-    if ($i eq '-tss') {
-        $client_conf = "client.tss" . $PerlACE::svcconf_ext;
-    } elsif ($i eq '-debug') {
+    if ($i eq '-debug') {
         $debug_level = '10';
     } elsif ($i eq '-creation') {
         $client_process = 'orb_creation';
