1bc29fe21751607fcbe89f4eb639528da4369617
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-10-20 09:48:15 +0000

    clean up Remedy work branch

--- branches/RemedyWork/ACE/ace/OS_NS_ctype.cpp
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-// $Id$
-
-#include "ace/OS_NS_ctype.h"
-
-#if !defined (ACE_HAS_INLINED_OSCALLS)
-# include "ace/OS_NS_ctype.inl"
-#endif /* ACE_HAS_INLINED_OSCALLS */
-
--- branches/RemedyWork/CIAO/connectors/dds4ccm/impl/logger/Log_Macros.cpp
+++
@@ -1,5 +0,0 @@
-// $Id$
-
-#include "Log_Macros.h"
-
-DDS4CCM_Logger_Export unsigned int DDS4CCM_debug_level = 0;
