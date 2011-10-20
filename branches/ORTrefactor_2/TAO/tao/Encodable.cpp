3a7edfe2669f7f97a2bb02b46b4e2bd42f89b9f2
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-10-20 11:20:39 +0000

    Removed old work branch

--- branches/ORTrefactor_2/TAO/tao/Encodable.cpp
+++
@@ -1,7 +0,0 @@
-// $Id$
-
-#include "tao/Encodable.h"
-
-TAO_Encodable::~TAO_Encodable (void) 
-{ 
-}
--- branches/ORTrefactor_2/ace/XML_Svc_Conf.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "ace/XML_Svc_Conf.h"
-
-#if (ACE_USES_CLASSIC_SVC_CONF == 0)
-ACE_XML_Svc_Conf::~ACE_XML_Svc_Conf (void)
-{
-}
-#endif /* ACE_USES_CLASSIC_SVC_CONF == 0 */
