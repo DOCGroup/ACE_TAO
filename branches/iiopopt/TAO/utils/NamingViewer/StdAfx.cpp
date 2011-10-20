796b7850a70e1f9c099f411237bb40f502a1cafa
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-10-20 11:24:37 +0000

    Removed old work branch

--- branches/iiopopt/TAO/utils/NamingViewer/StdAfx.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-// stdafx.cpp : source file that includes just the standard includes
-//	NamingViewer.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
--- branches/iiopopt/ace/String_Base_Const.cpp
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-#include "ace/String_Base_Const.h"
-
-ACE_BEGIN_VERSIONED_NAMESPACE_DECL
-
-ssize_t const ACE_String_Base_Const::npos = -1;
-
-ACE_END_VERSIONED_NAMESPACE_DECL
