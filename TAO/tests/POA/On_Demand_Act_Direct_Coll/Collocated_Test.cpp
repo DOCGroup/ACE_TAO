63e3a6cdf969a2066b77a3c0d2f9256d40bb29ab
Marijke Hengstmengel <MHengstmengel@users.noreply.github.com>
2011-10-20 08:43:52 +0000

    Thu Oct 20 08:42:33 UTC 2011  Marijke Hengstmengel  <hengstmengel_m@remedy.nl>
            * On_Demand_Act_Direct_Coll:
            * On_Demand_Act_Direct_Coll/Collocated_Test.cpp:
            * On_Demand_Act_Direct_Coll/Servant_Activator.cpp:
            * On_Demand_Act_Direct_Coll/test_i.cpp:
            * On_Demand_Act_Direct_Coll/Server_Task.h:
            * On_Demand_Act_Direct_Coll/run_test.pl:
            * On_Demand_Act_Direct_Coll/test_i.h:
            * On_Demand_Act_Direct_Coll/Client_Task.cpp:
            * On_Demand_Act_Direct_Coll/Server_Task.cpp:
            * On_Demand_Act_Direct_Coll/Servant_Activator.h:
            * On_Demand_Act_Direct_Coll/Client_Task.h:
            * On_Demand_Act_Direct_Coll/On_Demand_Act_D_Coll.mpc:
            * On_Demand_Act_Direct_Coll/test.idl:
            Test with Servant Activator and thru_poa- and direct collocation.
            
            * bin/tao_orb_tests.lst
            Added On_Demand_Act_Direct_Coll test.

---
+++ TAO/tests/POA/On_Demand_Act_Direct_Coll/Collocated_Test.cpp
@@ -0,0 +1,103 @@
+//$Id$
+#include "Server_Task.h"
+#include "Client_Task.h"
+#include "ace/Get_Opt.h"
+#include "ace/Argv_Type_Converter.h"
+#include "ace/SString.h"
+#include "ace/Manual_Event.h"
+
+const ACE_TCHAR *output = ACE_TEXT("test.ior");
+const ACE_TCHAR *input = ACE_TEXT("file://test.ior");
+CORBA::Boolean exception = false;
+
+int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT("k:o:td:"));
+  int c;
+
+  while ((c = get_opts ()) != -1)
+    switch (c)
+      {
+      case 'o':
+        output = get_opts.opt_arg ();
+        break;
+      case 'k':
+        input = get_opts.opt_arg ();
+        break;
+      case 't':
+        // no exception expected
+        exception = false;
+        break;
+      case 'd':
+        // exception expected
+        exception = true;
+        break;
+      case '?':
+      default:
+        // This is a hack but that is okay!
+        return 0;
+      }
+  // Indicates successful parsing of the command line
+  return 0;
+}
+
+int
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+{
+
+  if (parse_args (argc,
+                  argv) == -1)
+    return -1;
+
+  try
+    {
+      ACE_Argv_Type_Converter satc (argc, argv);
+      CORBA::ORB_var sorb =
+        CORBA::ORB_init (satc.get_argc (),
+                         satc.get_TCHAR_argv ());
+
+      ACE_Manual_Event me;
+      Server_Task server_task (output,
+                               sorb.in (),
+                               me,
+                               ACE_Thread_Manager::instance ());
+
+      if (server_task.activate (THR_NEW_LWP | THR_JOINABLE,
+                                1,
+                                1) == -1)
+        {
+          ACE_ERROR ((LM_ERROR, "Error activating server task\n"));
+        }
+
+      // Wait for the server thread to do some processing
+      me.wait ();
+      ACE_Argv_Type_Converter catc (argc, argv);
+      CORBA::ORB_var corb =
+        CORBA::ORB_init (catc.get_argc (),
+                         catc.get_TCHAR_argv ());
+
+      Client_Task client_task (input,
+                               corb.in (),
+                               ACE_Thread_Manager::instance (),
+                               exception);
+
+      if (client_task.activate (THR_NEW_LWP | THR_JOINABLE,
+                                1,
+                                1) == -1)
+        {
+          ACE_ERROR ((LM_ERROR, "Error activating client task\n"));
+        }
+
+      // Wait for the client and server to finish
+      ACE_Thread_Manager::instance ()->wait ();
+
+      // Now that all threads have completed we can destroy the ORB
+      sorb->destroy ();
+    }
+  catch (const CORBA::Exception&)
+    {
+      // Ignore exceptions..
+    }
+  return 0;
+}
---
+++ TAO/tests/POA/On_Demand_Act_Direct_Coll/test_i.cpp
@@ -0,0 +1,58 @@
+// $Id$
+
+#include "test_i.h"
+#include "ace/OS_NS_unistd.h"
+#include "ace/OS_NS_string.h"
+
+// Constructor
+test_i::test_i (CORBA::ORB_ptr orb,
+                PortableServer::POA_ptr poa,
+                ACE_thread_t thrid)
+  : orb_ (CORBA::ORB::_duplicate (orb))
+   ,poa_ (PortableServer::POA::_duplicate (poa))
+   ,thr_id_ (thrid)
+{
+}
+
+void
+test_i::method (void)
+{
+}
+
+char *
+test_i::get_string (void)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              "(%P|%t) Upcall in process ..\n"));
+  // Use portable thread IDs
+  ACE_Thread_ID self_ID;
+  // Thread ID from Server
+  ACE_Thread_ID this_ID;
+  this_ID.id(this->thr_id_);
+
+  // Servant Thread ID same as Thread ID server, so a remote call,
+  // in case of a collocation the servant runs in calling thread (Client)
+  if (self_ID == this_ID)
+    {
+      ACE_ERROR ((LM_ERROR,
+                 "(%P|%t) ERROR: A remote call has been made "
+                       " exiting ..\n"));
+      ACE_OS::abort ();
+    }
+  else
+    {
+      ACE_DEBUG ((LM_DEBUG,
+                "(%P|%t) OK: A collocated invocation has been made.\n"));
+    }
+  return CORBA::string_dup ("Hello there!");
+}
+
+void
+test_i::shutdown (void)
+{
+  // Give the client thread time to return from the collocated
+  // call to this method before shutting down the ORB.  We sleep
+  // to avoid BAD_INV_ORDER exceptions on fast dual processor machines.
+  ACE_OS::sleep (1);
+  this->orb_->shutdown (0);
+}
