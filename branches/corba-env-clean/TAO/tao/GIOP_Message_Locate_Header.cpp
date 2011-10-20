af7731050539a1f17cd7e7c19aeb4190ff674c85
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-10-20 11:24:07 +0000

    Removed old work branch

--- branches/corba-env-clean/TAO/tao/GIOP_Message_Locate_Header.cpp
+++
@@ -1,8 +0,0 @@
-//$Id$
-#include "tao/GIOP_Message_Locate_Header.h"
-
-#if !defined (__ACE_INLINE__)
-# include "tao/GIOP_Message_Locate_Header.i"
-#endif /* __ACE_INLINE__ */
-
-ACE_RCSID(tao, GIOP_Message_Headers, "$Id$")
