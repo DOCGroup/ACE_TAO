6f675824295c16abf7812db7b915177cbddeb19b
mcorino <mcorino@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2011-11-02 08:47:40 +0000

    reset work branch

--- tags/RemedyWorkStart/TAO/tao/CSD_ThreadPool/CSD_TP_Servant_State_Map.cpp
+++
@@ -1,7 +0,0 @@
-// $Id$
-
-#include "tao/CSD_ThreadPool/CSD_TP_Servant_State_Map.h"
-
-#if !defined (__ACE_INLINE__)
-# include "tao/CSD_ThreadPool/CSD_TP_Servant_State_Map.inl"
-#endif /* ! __ACE_INLINE__ */
