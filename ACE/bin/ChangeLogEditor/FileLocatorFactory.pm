88dba0e960cc594bc5da5c033e298a83d961cac3
James H. Hill <hilljh82@gmail.com>
2011-11-08 19:03:33 +0000

    Tue Nov  8 18:58:36 UTC 2011  James H. Hill  <hillj at cs dot iupui dot edu>

--- ACE/bin/ChangeLogEditor/FileLocatorFactory.pm
+++ ACE/bin/ChangeLogEditor/FileLocatorFactory.pm
@@ -13,6 +13,7 @@ package FileLocatorFactory;
 
 use strict;
 
+use Cwd;
 use CVSFileLocator;
 use SVNFileLocator;
 
@@ -25,13 +26,36 @@ sub create {
   ## will exist when Subversion isn't the rcs being used.  However, that
   ## is not the case for CVS directories.
   switch: {
-    ((defined $ENV{SVN_ASP_DOT_NET_HACK} && -d '_svn') || -d '.svn')
+    ((defined $ENV{SVN_ASP_DOT_NET_HACK} && -d '_svn') || searchParentDirectory('.svn'))
       && do { return new SVNFileLocator(); };
     -d 'CVS'  && do { return new CVSFileLocator(); };
     print STDERR "WARNING: Unsupported revision control protocol\n";
   }
 
   return new FileLocator();
+}
+
+sub searchParentDirectory {
+  my($hidden)  = shift;
+  my($path)    = cwd();
+  my($index)   = -1;
+  
+  # Search all parent directories for the specified hidden
+  # directory. We stop when we either found the hidden directory
+  # of there are no more parent directories let to search.
+  do {
+    if (-d $path . '/' . $hidden) {
+      return 1;
+    }
+    
+    $index = rindex($path, '/');
+    
+    if ($index != -1) {
+      $path = substr ($path, 0, $index);
+    }
+  } while ($index != -1);
+  
+  return 0;
 }
 
 
