0776868648349ae21708961b21e6148a637e4237
Johnny Willemsen <jwillemsen@remedy.nl>
2011-11-17 07:40:16 +0000

    Thu Nov 17 07:35:20 UTC 2011  Johnny Willemsen  <jwillemsen@remedy.nl>
            * performance-tests/Latency/DSI/Roundtrip.h:
            * performance-tests/Sequence_Latency/DSI/Roundtrip.h:
            * tests/DSI_AMI_Gateway/test_dsi.h:
              Last runtime fixes

--- TAO/performance-tests/Latency/DSI/Roundtrip.h
+++ TAO/performance-tests/Latency/DSI/Roundtrip.h
@@ -30,7 +30,7 @@ public:
       PortableServer::POA_ptr poa);
 
 private:
-  /// Use an ORB reference to conver strings to objects and shutdown
+  /// Use an ORB reference to convert strings to objects and shutdown
   /// the application.
   CORBA::ORB_var orb_;
 };
