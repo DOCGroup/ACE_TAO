5dc8b539a8a2090a1a38c58843377e8ab186f1c8
msmit <msmit@remedy.nl>
2011-12-14 12:37:01 +0000

    merge remedy work branch

--- CIAO/connectors/dds4ccm/impl/ndds/DomainParticipantManager.cpp
+++ CIAO/connectors/dds4ccm/impl/ndds/DomainParticipantManager.cpp
@@ -139,9 +139,10 @@ namespace CIAO
 
     bool
     DomainParticipantManager::add_topic (DDSDomainParticipant * dp,
-                                         DDSTopic * tp)
+      DDSTopic * tp)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::add_topic");
+
       DomainParticipants_iterator iter =
         this->get_participanttopic_by_participant (dp);
       if (iter != this->dps_.end ())
@@ -150,91 +151,115 @@ namespace CIAO
         }
       DDS4CCM_ERROR (DDS4CCM_LOG_LEVEL_ERROR, (LM_ERROR, DDS4CCM_INFO
                     "DomainParticipantManager::add_topic - "
-                    "Unable to find provided DomainParticipant.\n"));
+                    "Unable to find provided DomainParticipant for.\n"));
       return false;
     }
 
     bool
     DomainParticipantManager::remove_topic (DDSDomainParticipant * dp,
-                                            DDSTopic * tp)
+      DDSTopic * tp)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::remove_topic");
+
       DomainParticipants_iterator iter =
         this->get_participanttopic_by_participant (dp);
       if (iter != this->dps_.end ())
         {
           return iter->second->remove_topic (tp);
         }
+
       return false;
     }
 
     DDSDomainParticipant *
-    DomainParticipantManager::get_participant (
+    DomainParticipantManager::get_participant (const DDS_DomainId_t domain_id,
       const char * qos_profile)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::get_participant");
 
-      ACE_GUARD_THROW_EX (TAO_SYNCH_MUTEX, _guard,
-                      this->dps_mutex_, CORBA::INTERNAL ());
-      DDSParticipantTopic * dpt = this->dps_[qos_profile];
-      if (dpt)
+      IdQosProfile idqos =
+        std::make_pair<std::string, DDS_DomainId_t>(qos_profile, domain_id);
+      DomainParticipants_iterator it_found = this->dps_.find (idqos);
+
+      if (it_found != this->dps_.end () && it_found->second)
         {
-          dpt->_inc_ref ();
+          it_found->second->_inc_ref ();
           DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_ACTION, (LM_DEBUG, DDS4CCM_INFO
                         "DomainParticipantManager::get_participant - "
-                        "DomainParticipant found. profile <%C> - ref_count <%d>\n",
+                        "DomainParticipant found. domain <%d> - "
+                        "profile <%C> - ref_count <%d>\n",
+                        domain_id,
                         qos_profile,
-                        dpt->_ref_count ()));
-          return dpt->get_participant ();
+                        it_found->second->_ref_count ()));
+          return it_found->second->get_participant ();
         }
       DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_ACTION, (LM_DEBUG, DDS4CCM_INFO
                     "DomainParticipantManager::get_participant - "
-                    "DomainParticipant with profile <%C> does not exist.\n",
-                    qos_profile));
+                    "DomainParticipant for domain <%d> with profile <%C> "
+                    "does not exist.\n",
+                    domain_id, qos_profile));
       return 0;
     }
 
     bool
-    DomainParticipantManager::add_participant (
-      const char * qos_profile,
+    DomainParticipantManager::add_participant (const char * qos_profile,
       DDSDomainParticipant * dp)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::add_participant");
 
       ACE_GUARD_THROW_EX (TAO_SYNCH_MUTEX, _guard,
                       this->dps_mutex_, CORBA::INTERNAL ());
-      DomainParticipants_iterator iter =
-        this->get_participanttopic_by_participant (dp);
-      if (iter == this->dps_.end ())
+
+      const DDS_DomainId_t domain_id = dp->get_domain_id ();
+      IdQosProfile idqos =
+        std::make_pair<std::string, DDS_DomainId_t>(qos_profile, domain_id);
+      DomainParticipants_iterator it_found = this->dps_.find (idqos);
+
+      if (it_found == this->dps_.end())
         {
           DDSParticipantTopic * dpt = 0;
           ACE_NEW_THROW_EX (dpt,
                             DDSParticipantTopic (dp),
                             ::CORBA::NO_MEMORY ());
-          this->dps_[qos_profile] = dpt;
+          std::pair <DomainParticipants_iterator, bool> to_insert =
+            this->dps_.insert (std::make_pair<IdQosProfile,
+                                              DDSParticipantTopic *>(idqos, dpt));
+          if (!to_insert.second)
+            {
+              DDS4CCM_ERROR (DDS4CCM_LOG_LEVEL_ERROR, (LM_ERROR, DDS4CCM_INFO
+                            "DomainParticipantManager::add_participant - "
+                            "Unable to insert a new DomainParticipant/Topic "
+                            "combination for <%d, %C>.\n",
+                            domain_id, qos_profile));
+              return false;
+            }
+
           DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_ACTION, (LM_DEBUG, DDS4CCM_INFO
                         "DomainParticipantManager::add_participant - "
-                        "Added participant with profile <%C>.\n",
-                        qos_profile));
+                        "Added a new DomainParticipant/Topic "
+                        " combination for <%d, %C>.\n",
+                        domain_id, qos_profile));
           return true;
+
         }
+
       DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_ACTION, (LM_DEBUG, DDS4CCM_INFO
                     "DomainParticipantManager::add_participant - "
-                    "Don't add participant with profile <%C> since it already "
+                    "Don't add participant for domain <%d> with profile <%C> since it already "
                     "exists.\n",
-                    qos_profile));
+                    domain_id, qos_profile));
       return false;
     }
 
     bool
-    DomainParticipantManager::remove_participant (
-      DDSDomainParticipant * dp)
+    DomainParticipantManager::remove_participant (DDSDomainParticipant * dp)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::remove_participant");
 
       ACE_GUARD_THROW_EX (TAO_SYNCH_MUTEX, _guard,
                       this->dps_mutex_, CORBA::INTERNAL ());
 
+      const DDS_DomainId_t domain_id = dp->get_domain_id ();
       DomainParticipants_iterator iter =
         this->get_participanttopic_by_participant (dp);
       if (iter != this->dps_.end ())
@@ -243,7 +268,8 @@ namespace CIAO
             {
               DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_UNIMP_ACTION, (LM_TRACE, DDS4CCM_INFO
                             "DomainParticipantManager::remove_participant - "
-                            "Delete participant since ref_count is one. \n"));
+                            "Delete participant for domain <%d> since ref_count is one. \n",
+                            domain_id));
               delete iter->second;
 
               // Save to remove from list
@@ -254,8 +280,9 @@ namespace CIAO
               iter->second->_dec_ref ();
               DDS4CCM_DEBUG (DDS4CCM_LOG_LEVEL_UNIMP_ACTION, (LM_TRACE, DDS4CCM_INFO
                             "DomainParticipantManager::remove_participant - "
-                            "Don't delete participant since "
+                            "Don't delete participant for domain <%d> since "
                             "it's still used - ref_count is still <%d>\n",
+                            domain_id,
                             iter->second->_ref_count ()));
               return false;
             }
@@ -265,9 +292,10 @@ namespace CIAO
 
     void
     DomainParticipantManager::_inc_ref (DDSDomainParticipant * dp,
-                                        DDSTopic * tp)
+      DDSTopic * tp)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::_inc_ref");
+
       DomainParticipants_iterator iter =
         this->get_participanttopic_by_participant (dp);
       if (iter != this->dps_.end ())
@@ -277,8 +305,7 @@ namespace CIAO
     }
 
     DomainParticipantManager::DomainParticipants_iterator
-    DomainParticipantManager::get_participanttopic_by_participant (
-      DDSDomainParticipant * dp)
+    DomainParticipantManager::get_participanttopic_by_participant (DDSDomainParticipant * dp)
     {
       DDS4CCM_TRACE ("DomainParticipantManager::get_participanttopic_by_participant");
 
