10d19ab8ca01435aa079e28330207e9f0d145198
Johnny Willemsen <jwillemsen@remedy.nl>
2012-01-04 08:51:26 +0000

    Wed Jan  4 08:50:18 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * bin/cleanbuilds.sh:
            * bin/mail_test_stats.sh:
              Accept date and email as arguments

--- ACE/bin/cleanbuilds.sh
+++ ACE/bin/cleanbuilds.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 # $Id$
 
-if test -z $ACE_ROOT; then ACE_ROOT=..; fi
-if test -z $TAO_ROOT; then TAO_ROOT=${ACE_ROOT}/TAO; fi
-if test -z $CIAO_ROOT; then CIAO_ROOT=${TAO_ROOT}/CIAO; fi
-if test -z $DANCE_ROOT; then DANCE_ROOT=${TAO_ROOT}/DAnCE; fi
-#
-wget http://download.theaceorb.nl/teststat/cleanbuild_testfails-YYYY_MM_DD.txt -O cleanbuild.txt
+if test -z $1; then CURRENTDATE=YYYY_MM_DD; else CURRENTDATE=$1; fi
+
+wget http://download.theaceorb.nl/teststat/cleanbuild_testfails-$CURRENTDATE.txt -O cleanbuild.txt
 #
 grep -h \!FIXED_BUGS_ONLY ${ACE_ROOT}/tests/*.lst ${ACE_ROOT}/bin/*.lst ${TAO_ROOT}/bin/*.lst ${CIAO_ROOT}/bin/*.lst ${DANCE_ROOT}/bin/*.lst | sed -e "s/^\([^\:]*\).*/\1/" | sed -e "s/\(\/run_test.pl\)\?\s*$//" > Ignore.txt
 #
