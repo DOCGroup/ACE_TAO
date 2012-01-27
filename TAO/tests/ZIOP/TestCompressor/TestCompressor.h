73525c86c931f0339484b631c192f2f67ec17512
Johnny Willemsen <jwillemsen@remedy.nl>
2012-01-27 18:49:05 +0000

    Fri Jan 27 18:47:34 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Compression/RLECompressorTest.cpp:
            * tests/ZIOP/TestCompressor/TestCompressor.h:
            * tests/ZIOP/TestCompressor/TestCompressor.cpp:
              Fixed compile errors

--- TAO/tests/ZIOP/TestCompressor/TestCompressor.h
+++ TAO/tests/ZIOP/TestCompressor/TestCompressor.h
@@ -33,8 +33,8 @@ namespace TAO
   class TAO_TESTCOMPRESSOR_Export TestCompressor : public BaseCompressor
   {
     public:
-      TestCompressor (::Compression::CompressionLevel compression_level,
-                      ::Compression::CompressorFactory_ptr compressor_factory);
+      TestCompressor (::Compression::CompressorFactory_ptr compressor_factory,
+                      ::Compression::CompressionLevel compression_level);
 
       virtual void compress (
           const ::Compression::Buffer &,
