7c787f15c984cad851e80421acb6372d1fe311e1
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-01-31 20:18:17 +0000

    Tue Jan 31 20:17:35 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * examples/IPC_SAP/SOCK_SAP/CPP-memclient.cpp:
              Fixed gcc 4.7 warning

--- trunk/ACE/examples/IPC_SAP/SOCK_SAP/CPP-memclient.cpp
+++ trunk/ACE/examples/IPC_SAP/SOCK_SAP/CPP-memclient.cpp
@@ -14,9 +14,6 @@
 #include "ace/Get_Opt.h"
 #include "ace/High_Res_Timer.h"
 
-
-
-
 static int
 run_client (void)
 {
@@ -29,7 +26,7 @@ run_client (void)
                        ACE_TEXT ("connect")), -1);
 
   char buf [MAXPATHLEN];
-  while (ACE_OS::fgets (buf, MAXPATHLEN, stdin) >0)
+  while (ACE_OS::fgets (buf, MAXPATHLEN, stdin) != 0)
     {
       stream.send (buf, ACE_OS::strlen (buf)+1);
       stream.recv (buf, MAXPATHLEN);
@@ -39,9 +36,8 @@ run_client (void)
   return 0;
 }
 
-int ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+int ACE_TMAIN (int, ACE_TCHAR *argv[])
 {
-  ACE_UNUSED_ARG(argc);
   // Initialize the logger.
   ACE_LOG_MSG->open (argv[0]);
 
@@ -50,4 +46,3 @@ int ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 
   return 0;
 }
-
