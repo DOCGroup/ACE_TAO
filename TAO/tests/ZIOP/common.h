9c9e192843317880052ada586d5e6b045d3b9685
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-03-16 10:43:25 +0000

    Fri Mar 16 10:45:00 UTC 2012  Simon Massey  <simon dot massey at prismtech dot com> * tests/ZIOP/common.h: * tests/ZIOP/client.cpp: * tests/ZIOP/server.cpp:
    Remove scoreboard warnings.

--- TAO/tests/ZIOP/common.h
+++ TAO/tests/ZIOP/common.h
@@ -5,6 +5,5 @@
 #define LEAST_COMPRESSION_LEVEL ((CLIENT_COMPRESSION_LEVEL < SERVER_COMPRESSION_LEVEL) ?\
                                   CLIENT_COMPRESSION_LEVEL : SERVER_COMPRESSION_LEVEL)
 
-static const ACE_TCHAR *ior = ACE_TEXT("file://test.ior");
-static const ACE_TCHAR *ior_output_file = ACE_TEXT("test.ior");
+#define DEFAULT_IOR_FILENAME ACE_TEXT("test.ior");
 static int test = 1;
