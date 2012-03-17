e401773ab453eac264eb22bd4f8df938aca65757
Johnny Willemsen <jwillemsen@remedy.nl>
2012-03-17 12:12:23 +0000

    Sat Mar 17 12:11:15 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * bin/cleanbuilds.sh:
            * bin/diff-builds.pl:
              Updated teststat links

--- ACE/bin/cleanbuilds.sh
+++ ACE/bin/cleanbuilds.sh
@@ -3,7 +3,7 @@
 
 if test -z $1; then CURRENTDATE=YYYY_MM_DD; else CURRENTDATE=$1; fi
 
-wget http://download.theaceorb.nl/teststat/cleanbuild_testfails-$CURRENTDATE.txt -O cleanbuild.txt
+wget http://teststat.theaceorb.nl/teststat/cleanbuild_testfails-$CURRENTDATE.txt -O cleanbuild.txt
 #
 grep -h \!FIXED_BUGS_ONLY ${ACE_ROOT}/tests/*.lst ${ACE_ROOT}/bin/*.lst ${TAO_ROOT}/bin/*.lst ${CIAO_ROOT}/bin/*.lst ${DANCE_ROOT}/bin/*.lst | sed -e "s/^\([^\:]*\).*/\1/" | sed -e "s/\(\/run_test.pl\)\?\s*$//" > Ignore.txt
 #
