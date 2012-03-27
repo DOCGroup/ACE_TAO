182a8f9f10f5afd42c3add7802b7a70aea897add
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-03-27 09:35:58 +0000

    Tue Mar 27 10:40:00 UTC 2012  Simon Massey  <simon dot massey at prismtech dot com>
    Wrong wide char string being compared should always be narrow char.

--- TAO/tests/ZIOP/client.cpp
+++ TAO/tests/ZIOP/client.cpp
@@ -308,7 +308,7 @@ run_string_test (Test::Hello_ptr hello)
                                                     "This is a test string");
   ACE_DEBUG ((LM_DEBUG, "(%P|%t) - string returned <%C>\n",
               the_string.in ()));
-  if (ACE_OS::strstr (the_string._retn (), ACE_TEXT ("Hello there")) == 0)
+  if (ACE_OS::strstr (the_string._retn (), "Hello there") == 0)
     {
       ACE_ERROR_RETURN ((LM_ERROR,
                          ACE_TEXT ("ERROR : run_string_test, unexpected string received\n")),
