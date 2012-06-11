1363f225f252fc3c45bc490051740f151dc3fa69
mitza <mitza@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-06-11 21:46:38 +0000

    ChangeLogTag: Mon Jun 11 21:45:19 UTC 2012  Adam Mitz  <mitza@ociweb.com>

--- ACE/ace/config-lite.h
+++ ACE/ace/config-lite.h
@@ -116,7 +116,7 @@ ACE_END_VERSIONED_NAMESPACE_DECL
 // Once all C++ compilers support the standard reverse_iterator
 // adapters, we can drop this generator macro or at least drop the
 // MSVC++ or Sun Studio preprocessor conditional blocks.
-#if defined (__SUNPRO_CC) && __SUNPRO_CC <= 0x5110 \
+#if defined (__SUNPRO_CC) && __SUNPRO_CC <= 0x5120 \
       && !defined (_STLPORT_VERSION)
   // If we're not using the stlport4 C++ library (which has standard
   // iterators), we need to ensure this is included in order to test
@@ -131,7 +131,7 @@ ACE_END_VERSIONED_NAMESPACE_DECL
   typedef std::reverse_iterator<iterator, value_type> reverse_iterator; \
   typedef std::reverse_iterator<const_iterator, \
                                 value_type const> const_reverse_iterator;
-#elif defined (__SUNPRO_CC) && __SUNPRO_CC <= 0x5110 \
+#elif defined (__SUNPRO_CC) && __SUNPRO_CC <= 0x5120 \
       && defined (_RWSTD_NO_CLASS_PARTIAL_SPEC)
 # define ACE_DECLARE_STL_REVERSE_ITERATORS \
   typedef std::reverse_iterator<iterator, \
