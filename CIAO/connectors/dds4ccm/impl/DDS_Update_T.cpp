467ea4437f61c1c611df7a2459e465e53d88b01b
Johnny Willemsen <jwillemsen@remedy.nl>
2012-06-13 17:43:40 +0000

    Wed Jun 13 17:42:32 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/DDS_Base_Connector_T.cpp:
            * connectors/dds4ccm/impl/DDS_Subscriber_Base_T.cpp:
            * connectors/dds4ccm/impl/DDS_Update_T.cpp:
            * connectors/dds4ccm/impl/DDS_Write_T.cpp:
            * connectors/dds4ccm/impl/DataReaderListener_T.cpp:
            * connectors/dds4ccm/impl/DataReaderStateListener_T.cpp:
              Don't use the word proxy in logging, that is only the case
    with
              RTI DDS
    
            * connectors/dds4ccm/tests/On_Inconsistent_Topic/Sender/OIT_Test_Sender_exec.cpp:
              Layout change

--- CIAO/connectors/dds4ccm/impl/DDS_Update_T.cpp
+++ CIAO/connectors/dds4ccm/impl/DDS_Update_T.cpp
@@ -104,7 +104,7 @@ DDS_Update_T<CCM_TYPE, TYPED_WRITER, VALUE_TYPE, SEQ_VALUE_TYPE>::configuration_
         {
           DDS4CCM_ERROR (DDS4CCM_LOG_LEVEL_DDS_NIL_RETURN, (LM_ERROR, DDS4CCM_INFO
                         "DDS_Update_T::configuration_complete - "
-                        "Error: Proxy returned a nil datawriter.\n"));
+                        "Error: DDS returned a nil datawriter.\n"));
           throw ::CORBA::INTERNAL ();
         }
       DDS::ReturnCode_t retcode = dwv_tmp->enable ();
