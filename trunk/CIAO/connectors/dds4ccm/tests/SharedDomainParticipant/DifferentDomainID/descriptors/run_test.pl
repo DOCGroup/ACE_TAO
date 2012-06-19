94f307fd2b15ae9d9516cd517b521d5038be18d7
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-06-19 18:07:54 +0000

    Extended timeout

--- trunk/CIAO/connectors/dds4ccm/tests/SharedDomainParticipant/DifferentDomainID/descriptors/run_test.pl
+++ trunk/CIAO/connectors/dds4ccm/tests/SharedDomainParticipant/DifferentDomainID/descriptors/run_test.pl
@@ -222,8 +222,8 @@ for ($i = 0; $i < $nr_daemon; ++$i) {
     }
 }
 
-print "Sleeping 1 seconds to allow task to complete\n";
-sleep (1);
+print "Sleeping 30 seconds to allow task to complete\n";
+sleep (30);
 
 # Invoke executor - stop the application -.
 print "Invoking executor - stop the application -\n";
