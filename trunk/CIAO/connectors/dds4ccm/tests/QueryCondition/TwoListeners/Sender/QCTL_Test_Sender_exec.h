a117cbdcfce58217132df80e2460fd204d6ca969
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-06-21 13:44:31 +0000

    Thu Jun 21 13:43:20 UTC 2012  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/QueryCondition/TwoListeners/Sender/QCTL_Test_Sender.idl:
            * connectors/dds4ccm/tests/QueryCondition/TwoListeners/Sender/QCTL_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/QueryCondition/TwoListeners/Sender/QCTL_Test_Sender_exec.cpp:
            * connectors/dds4ccm/tests/QueryCondition/TwoListeners/descriptors/Plan.cdp:
              Added connector status listener. The test starts upon receiving a publication matched
              event from DDS.

--- trunk/CIAO/connectors/dds4ccm/tests/QueryCondition/TwoListeners/Sender/QCTL_Test_Sender_exec.h
+++ trunk/CIAO/connectors/dds4ccm/tests/QueryCondition/TwoListeners/Sender/QCTL_Test_Sender_exec.h
@@ -45,6 +45,39 @@ namespace CIAO_QCTL_Test_Sender_Impl
   class Sender_exec_i;
 
   /**
+   * ConnectorStatusListener_exec_i
+   */
+  class SENDER_EXEC_Export ConnectorStatusListener_exec_i
+    : public virtual ::CCM_DDS::CCM_ConnectorStatusListener,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    ConnectorStatusListener_exec_i (Sender_exec_i &callback);
+    virtual ~ConnectorStatusListener_exec_i (void);
+
+    virtual
+    void on_inconsistent_topic (::DDS::Topic_ptr the_topic,
+                                const DDS::InconsistentTopicStatus & status);
+    virtual
+    void on_requested_incompatible_qos (::DDS::DataReader_ptr the_reader,
+                                        const DDS::RequestedIncompatibleQosStatus & status);
+    virtual
+    void on_sample_rejected (::DDS::DataReader_ptr the_reader,
+                             const DDS::SampleRejectedStatus & status);
+    virtual
+      void on_offered_deadline_missed (::DDS::DataWriter_ptr the_writer,
+                                       const DDS::OfferedDeadlineMissedStatus & status);
+    virtual
+    void on_offered_incompatible_qos (::DDS::DataWriter_ptr the_writer,
+                                      const DDS::OfferedIncompatibleQosStatus & status);
+    virtual
+      void on_unexpected_status (::DDS::Entity_ptr the_entity,
+                                 ::DDS::StatusKind status_kind);
+  private:
+    Sender_exec_i &callback_;
+  };
+
+  /**
    * Write ticker
    */
 
@@ -77,6 +110,8 @@ namespace CIAO_QCTL_Test_Sender_Impl
 
     //@{
     /** Component attributes and port operations. */
+    virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr get_connector_status (
+      void);
 
     virtual ::CORBA::UShort keys (void);
 
