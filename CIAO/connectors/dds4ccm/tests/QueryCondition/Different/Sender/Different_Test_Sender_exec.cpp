e5e17b875982b768629982b646190a5da72e818c
msmit <msmit@remedy.nl>
2012-06-27 19:08:15 +0000

    Wed Jun 27 19:07:49 UTC 2012  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/QueryCondition/Different/Sender/Different_Test_Sender.idl:
            * connectors/dds4ccm/tests/QueryCondition/Different/Sender/Different_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/QueryCondition/Different/Sender/Different_Test_Sender_exec.cpp:
            * connectors/dds4ccm/tests/QueryCondition/Different/descriptors/Plan.cdp:
              Start the writing of samples upon a publication matched event.

--- CIAO/connectors/dds4ccm/tests/QueryCondition/Different/Sender/Different_Test_Sender_exec.cpp
+++ CIAO/connectors/dds4ccm/tests/QueryCondition/Different/Sender/Different_Test_Sender_exec.cpp
@@ -35,6 +35,63 @@ namespace CIAO_Different_Test_Sender_Impl
 {
 
   /**
+   * ConnectorStatusListener_exec_i
+   */
+  ConnectorStatusListener_exec_i::ConnectorStatusListener_exec_i (
+    Sender_exec_i &callback)
+    : callback_ (callback)
+  {
+  }
+
+  ConnectorStatusListener_exec_i::~ConnectorStatusListener_exec_i (void)
+  {
+  }
+
+  // Operations from ::CCM_DDS::ConnectorStatusListener
+  void ConnectorStatusListener_exec_i::on_inconsistent_topic (
+    ::DDS::Topic_ptr /*the_topic*/,
+    const DDS::InconsistentTopicStatus & /*status*/)
+  {
+  }
+
+  void ConnectorStatusListener_exec_i::on_requested_incompatible_qos (
+    ::DDS::DataReader_ptr /*the_reader*/,
+    const DDS::RequestedIncompatibleQosStatus & /*status*/)
+  {
+  }
+
+  void ConnectorStatusListener_exec_i::on_sample_rejected (
+    ::DDS::DataReader_ptr /*the_reader*/,
+    const DDS::SampleRejectedStatus & /*status*/)
+  {
+  }
+
+  void ConnectorStatusListener_exec_i::on_offered_deadline_missed(
+    ::DDS::DataWriter_ptr /*the_writer*/,
+    const DDS::OfferedDeadlineMissedStatus & /*status*/)
+  {
+  }
+
+  void ConnectorStatusListener_exec_i::on_offered_incompatible_qos(
+    ::DDS::DataWriter_ptr /*the_writer*/,
+    const DDS::OfferedIncompatibleQosStatus & /*status*/)
+  {
+  }
+
+  void ConnectorStatusListener_exec_i::on_unexpected_status(
+    ::DDS::Entity_ptr /*the_entity*/,
+    ::DDS::StatusKind status_kind)
+  {
+    if (status_kind == ::DDS::PUBLICATION_MATCHED_STATUS)
+      {
+        ACE_DEBUG ((LM_DEBUG, "ConnectorStatusListener_exec_i::on_unexpected_status - "
+          "Publication matched received: starting the test\n"));
+
+        this->callback_.get_started ();
+      }
+  }
+
+  /**
    * WriteHandler
    */
 
@@ -91,6 +148,26 @@ namespace CIAO_Different_Test_Sender_Impl
   }
 
   void
+  Sender_exec_i::get_started (void)
+  {
+    try
+      {
+        this->reactor ()->notify (this->wh_);
+      }
+    catch (const ::CORBA::Exception& ex)
+      {
+        ex._tao_print_exception ("Exception caught:");
+        ACE_ERROR ((LM_ERROR,
+          ACE_TEXT ("ERROR: get_started : Exception caught\n")));
+      }
+    catch (...)
+      {
+        ACE_ERROR ((LM_ERROR,
+          ACE_TEXT ("ERROR: get_started : Unknown exception caught\n")));
+      }
+  }
+
+  void
   Sender_exec_i::start (void)
   {
     for (CORBA::UShort iter_key = 1; iter_key < this->keys_ + 1; ++iter_key)
@@ -115,6 +192,11 @@ namespace CIAO_Different_Test_Sender_Impl
   }
 
   // Component attributes and port operations.
+  ::CCM_DDS::CCM_ConnectorStatusListener_ptr
+  Sender_exec_i::get_connector_status (void)
+  {
+    return new ConnectorStatusListener_exec_i  (*this);
+  }
 
   ::CORBA::UShort
   Sender_exec_i::keys (void)
@@ -153,21 +235,6 @@ namespace CIAO_Different_Test_Sender_Impl
   void
   Sender_exec_i::ccm_activate (void)
   {
-    try
-      {
-        this->reactor ()->notify (this->wh_);
-      }
-    catch (const ::CORBA::Exception& ex)
-      {
-        ex._tao_print_exception ("Exception caught:");
-        ACE_ERROR ((LM_ERROR,
-          ACE_TEXT ("ERROR: GET_CONNECTION_START_READER : Exception caught\n")));
-      }
-    catch (...)
-      {
-        ACE_ERROR ((LM_ERROR,
-          ACE_TEXT ("ERROR: GET_CONNECTION_START_READER : Unknown exception caught\n")));
-      }
   }
 
   void
