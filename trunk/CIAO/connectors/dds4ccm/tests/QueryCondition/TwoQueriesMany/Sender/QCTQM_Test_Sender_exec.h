a7bfd52e1832febd25822e2142ebb2f5d945384c
msmit <msmit@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-06-28 08:08:47 +0000

    Thu Jun 28 08:08:12 UTC 2012  Marcel Smit  <msmit@remedy.nl>
            * connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/Sender/QCTQM_Test_Sender.idl:
            * connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/Sender/QCTQM_Test_Sender_exec.h:
            * connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/Sender/QCTQM_Test_Sender_exec.cpp:
            * connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/descriptors/Plan.cdp:
              Start the writing of samples upon a publication matched event.
    
            * connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/descriptors/run_test.pl:
              Optimized logging.

--- trunk/CIAO/connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/Sender/QCTQM_Test_Sender_exec.h
+++ trunk/CIAO/connectors/dds4ccm/tests/QueryCondition/TwoQueriesMany/Sender/QCTQM_Test_Sender_exec.h
@@ -43,6 +43,40 @@ namespace CIAO_QCTQM_Test_Sender_Impl
   class Sender_exec_i;
 
   /**
+   * ConnectorStatusListener_exec_i
+   */
+  class SENDER_EXEC_Export ConnectorStatusListener_exec_i
+    : public virtual ::CCM_DDS::CCM_ConnectorStatusListener,
+      public virtual ::CORBA::LocalObject
+  {
+  public:
+    ConnectorStatusListener_exec_i (Sender_exec_i &callback);
+    virtual ~ConnectorStatusListener_exec_i (void);
+
+    virtual
+    void on_inconsistent_topic (::DDS::Topic_ptr the_topic,
+                                const DDS::InconsistentTopicStatus & status);
+    virtual
+    void on_requested_incompatible_qos (::DDS::DataReader_ptr the_reader,
+                                        const DDS::RequestedIncompatibleQosStatus & status);
+    virtual
+    void on_sample_rejected (::DDS::DataReader_ptr the_reader,
+                             const DDS::SampleRejectedStatus & status);
+    virtual
+      void on_offered_deadline_missed (::DDS::DataWriter_ptr the_writer,
+                                       const DDS::OfferedDeadlineMissedStatus & status);
+    virtual
+    void on_offered_incompatible_qos (::DDS::DataWriter_ptr the_writer,
+                                      const DDS::OfferedIncompatibleQosStatus & status);
+    virtual
+      void on_unexpected_status (::DDS::Entity_ptr the_entity,
+                                 ::DDS::StatusKind status_kind);
+  private:
+    Sender_exec_i &callback_;
+    bool started_;
+  };
+
+  /**
    * WriteHandler
    */
 
@@ -101,6 +135,8 @@ namespace CIAO_QCTQM_Test_Sender_Impl
 
     //@{
     /** Component attributes and port operations. */
+    virtual ::CCM_DDS::CCM_ConnectorStatusListener_ptr
+    get_connector_status (void);
 
     virtual ::CCM_TwoQueriesRestarter_ptr
     get_restart_writer (void);
@@ -121,6 +157,7 @@ namespace CIAO_QCTQM_Test_Sender_Impl
 
     //@{
     /** User defined public operations. */
+    void get_started (void);
     void start (void);
     void restart (void);
 
