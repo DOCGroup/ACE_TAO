261233f31cc92befd4c13c5853a2799b33792c60
mitza <mitza@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-07-23 20:59:08 +0000

    ChangeLogTag: Mon Jul 23 20:58:16 UTC 2012  Adam Mitz  <mitza@ociweb.com>

--- trunk/TAO/tests/Bug_3630_Regression/run_test.pl
+++ trunk/TAO/tests/Bug_3630_Regression/run_test.pl
@@ -7,21 +7,22 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 
 use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::TestTarget;
+use strict;
 
 my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
 
 my $srvconf = "TAO Conf File.conf";
-my $server_svcconf = $server->LocalFile ($svcconf);
+my $server_svcconf = $server->LocalFile ($srvconf);
 
-if ($server->PutFile ($svcconf) == -1) {
+if ($server->PutFile ($srvconf) == -1) {
     print STDERR "ERROR: cannot set file <$server_svcconf>\n";
     exit 1;
 }
 
 
-$SV = $server->CreateProcess ("server");
+my $SV = $server->CreateProcess ("server");
 
-$server_status = $SV->Spawn ();
+my $server_status = $SV->Spawn ();
 
 if ($server_status != 0) {
     print STDERR "ERROR: server returned $server_status\n";
@@ -29,7 +30,7 @@ if ($server_status != 0) {
 }
 
 $server_status = $SV->WaitKill ($server->ProcessStartWaitInterval());
-
+my $status = 0;
 if ($server_status != 0) {
     print STDERR "ERROR: server returned $server_status\n";
     $status = 1;
