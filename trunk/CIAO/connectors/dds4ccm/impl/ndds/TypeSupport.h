53e1676048bb0f4a314bb66f542efbf62481c1a8
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-08-16 10:31:40 +0000

    Thu Aug 16 10:30:43 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/DDS_TopicBase_Connector_T.cpp:
            * connectors/dds4ccm/impl/ndds/TypeSupport.h:
            * connectors/dds4ccm/impl/ndds/TypeSupport.cpp:
              Fixed problem when shutting down the system. The type factories
              wheren't reference counted, the first connector that was
              shutting down destroyed the type factory. Added a refcount to
              the typesupport administration and greatly simplified the code
              and improved the logging.

--- trunk/CIAO/connectors/dds4ccm/impl/ndds/TypeSupport.h
+++ trunk/CIAO/connectors/dds4ccm/impl/ndds/TypeSupport.h
@@ -62,43 +62,64 @@ namespace CIAO
       }
     };
 
+    /**
+     * Helper class to administrate the type factory and how many times
+     * it has been registered
+     */
+    class DDS_TypeFactory_i_var
+    {
+      public:
+        DDS_TypeFactory_i_var (DDS_TypeFactory_i* tf);
+        ~DDS_TypeFactory_i_var (void);
+
+        DDS_TypeFactory_i* get_factory ();
+
+        /**
+         * Increments the reference count
+         */
+        long _inc_ref ();
+        /**
+         * Decrements the reference count
+         */
+        long _dec_ref ();
+
+      private:
+        long ref_count_;
+        DDS_TypeFactory_i* tf_;
+    };
+
     class DDS4CCM_NDDS_Export DDS_TypeSupport_i
     {
     public:
       /**
-        * @name close
         * Clears all internal maps, freeing the memory.
         */
       static void close (void);
 
       /**
-        * @name register_tye
         * Registering a type-factory combination per DomainParticipant.
-        * Returns false if insertions to one of the maps fails or if there's
+        * @retval false Insertions to one of the maps fails or if there's
         * already a factory for the given type registered.
-        * Returns true if insertions succeeds.
+        * @retval true Insertions succeeded
         */
-      static bool register_type (const char* type, DDS_TypeFactory_i* factory,
-                                 ::DDS::DomainParticipant_ptr dp);
+      static bool register_type (::DDS::DomainParticipant_ptr dp,
+                                 const char* type, DDS_TypeFactory_i* factory);
 
       /**
-        * @name unregister_tye
         * Unregistering a type-factory combination for a specific
-        * DomainParticipant. If the combination cannot be found,
-        * this method returns nil.
+        * DomainParticipant. At the moment the type factory isn't used anymore
+        * it is automatically destructed
         */
-      static DDS_TypeFactory_i * unregister_type (const char* type,
-                                                  ::DDS::DomainParticipant_ptr dp);
+      static void unregister_type (::DDS::DomainParticipant_ptr dp,
+                                   const char* type);
 
       /**
-        * @name create_datawriter
         * Create a type specific datawriter
         */
       static ::DDS::DataWriter_ptr create_datawriter (DDSDataWriter* dw,
                                                       ::DDS::DomainParticipant_ptr dp,
                                                       ::DDS::Publisher_ptr pub);
       /**
-        * @name create_datawriter
         * Create a type specific datareader
         */
       static ::DDS::DataReader_ptr create_datareader (DDSDataReader* dr,
@@ -108,38 +129,19 @@ namespace CIAO
       DDS_TypeSupport_i (void);
       ~DDS_TypeSupport_i (void);
 
-      typedef std::map < ACE_CString, DDS_TypeFactory_i* > typefactories;
+      /// Store for a specific DDS type a type factory var with its own refcount
+      typedef std::map < ACE_CString, DDS_TypeFactory_i_var* > typefactories;
 
+      /// For each domain participant we store a map with type factories for the
+      /// types that participant has
       typedef std::map< ::DDS::DomainParticipant_ptr, typefactories > participantfactories;
       static participantfactories participant_factories;
 
       /**
-        * @name get_factory_i
         * Searches for a TypeFactory, based on a type and DomainParticipant
         */
-      static DDS_TypeFactory_i* get_factory_i (const char* type,
-                                              ::DDS::DomainParticipant_ptr dp);
-
-      /**
-        * @name register_factory_i
-        * Insert a new type-factory combination per DomainParticipant and type.
-        * Returns true if no factory for the given type was registered.
-        * Returns false if insertions to the internal lists fail.
-        */
-      static bool register_factory_i (const char* type,
-                                      DDS_TypeFactory_i* f,
-                                      ::DDS::DomainParticipant_ptr dp);
-
-      /**
-        * @name unregister_participant_factory_i
-        * Erasing the registered type-factory combination per DomainParticipant.
-        * If the latest type-factory combination is removed, the DomainParticipant
-        * will also be removed from the internal lists.
-        * This method does not free any memory!
-        */
-      static void unregister_participant_factory_i (const char * type,
-                                                    ::DDS::DomainParticipant_ptr dp);
-
+      static DDS_TypeFactory_i* get_factory_i (::DDS::DomainParticipant_ptr dp,
+                                               const char* type);
     };
   }
 }
