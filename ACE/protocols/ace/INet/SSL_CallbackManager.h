e78d63f6e5be431373760e529ccdebb79cad0d44
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-08-21 12:26:44 +0000

    Tue Aug 21 12:25:45 UTC 2012  Simon Massey  <simon dot massey at prismtech dot com> ace/SSL/SSL_Context.h, SSL_Context.inl, protocols/ace/INet/SSL_CallbackManager.h, SSL_CallbackManager.cpp: requires extern C for SSL callback function use.

--- ACE/protocols/ace/INet/SSL_CallbackManager.h
+++ ACE/protocols/ace/INet/SSL_CallbackManager.h
@@ -24,6 +24,32 @@ namespace ACE
   {
     namespace INet
       {
+        // NOTE: Solaris studio compilers amongst others will issue warnings if the
+        // the correct type of function pointer (i.e. extern "C" ) is not stored/used
+        // of the form:
+        //   Warning (Anachronism): Formal argument 1 of type
+        //     extern "C" int(*)(int,x509_store_ctx_st*) in call to
+        //     ACE_SSL_Context::default_verify_callback(extern "C" int(*)(int,x509_store_ctx_st*))
+        //     is being passed int(*)(int,x509_store_ctx_st*).
+        // and
+        //   Warning (Anachronism): Formal argument cb of type
+        //     extern "C" int(*)(char*,int,int,void*) in call to
+        //     SSL_CTX_set_default_passwd_cb(ssl_ctx_st*, extern "C" int(*)(char*,int,int,void*))
+        //     is being passed int(*)(char*,int,int,void*).
+        // when C library routines are passed CallBack functions pointers that are
+        // actually C++ functions. (Static class member functions are NOTE extern "C" by default.)
+        //
+        // Unfortunatly you can not specify extern "C" linkage anywhere inside a class
+        // declaration or inside a function prototype for individual parameters. We are therefore
+        // forced to declare friend functions external to the class to do this job (and of course
+        // the function names can not be overloaded as they are C functions). They can not be
+        // static functions to make them private to the cpp file as they need to be seen by the
+        // class declaration and therefore included in the header file) thus:
+        extern "C" {
+          int extern_C_verify_certificate_callback (int ok, X509_STORE_CTX* cert_ctx);
+          int extern_C_passwd_callback (char* buf, int size, int rwflag, void* user_data);
+        }
+
         /**
         * @class ACE_INet_SSL_CallbackManager
         *
@@ -60,12 +86,11 @@ namespace ACE
               TCertificateCallback cert_callback_;
               TPasswordCallback passwd_callback_;
 
-              static int verify_certificate_callback (int ok, X509_STORE_CTX* cert_ctx);
-              static int passwd_callback (char* buf, int size, int rwflag, void* user_data);
+              friend int extern_C_verify_certificate_callback (int ok, X509_STORE_CTX* cert_ctx);
+              friend int extern_C_passwd_callback (char* buf, int size, int rwflag, void* user_data);
 
               static int ssl_ctx_mngr_index_;
           };
-
       }
   }
 
