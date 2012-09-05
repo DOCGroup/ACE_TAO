ac229229b3b98c37b38f7ad562a9867a8135882e
Steve Huston <shuston@riverace.com>
2012-09-05 22:16:38 +0000

    ChangeLogTag:Wed Sep  5 20:09:09 UTC 2012  Steve Huston  <shuston@riverace.com>

--- ACE/ace/SSL/SSL_SOCK_Stream.cpp
+++ ACE/ace/SSL/SSL_SOCK_Stream.cpp
@@ -409,22 +409,9 @@ ACE_SSL_SOCK_Stream::recv_n (void *buf,
                       timeout);
 
       if (n < 0)
-        {
-          if (errno == EWOULDBLOCK)
-            {
-              // If blocked, try again.
-              n = 0;
-              continue;
-            }
-          else
-            {
-              return -1;
-            }
-        }
+        return -1;
       else if (n == 0)
-        {
-          break;
-        }
+        break;
     }
 
   return ACE_Utils::truncate_cast<ssize_t> (bytes_transferred);
@@ -455,22 +442,9 @@ ACE_SSL_SOCK_Stream::recv_n (void *buf, int len, int flags) const
                       flags);
 
       if (n < 0)
-        {
-          if (errno == EWOULDBLOCK)
-            {
-              // If blocked, try again.
-              n = 0;
-              continue;
-            }
-          else
-            {
-              return -1;
-            }
-        }
+        return -1;
       else if (n == 0)
-        {
-          break;
-        }
+        break;
     }
 
   return ACE_Utils::truncate_cast<ssize_t> (bytes_transferred);
@@ -498,24 +472,10 @@ ACE_SSL_SOCK_Stream::send_n (const void *buf, int len, int flags) const
       n = this->send ((const char*) buf + bytes_transferred,
                       len - bytes_transferred,
                       flags);
-
       if (n < 0)
-        {
-          if (errno == EWOULDBLOCK)
-            {
-              // If blocked, try again.
-              n = 0;
-              continue;
-            }
-          else
-            {
-              return -1;
-            }
-        }
+        return -1;
       else if (n == 0)
-        {
-          break;
-        }
+        break;
     }
 
   return ACE_Utils::truncate_cast<ssize_t> (bytes_transferred);
