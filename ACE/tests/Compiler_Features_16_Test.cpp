5ffa6b74c00a887f9a01ab8c9252a17b014fd572
Johnny Willemsen <jwillemsen@remedy.nl>
2012-09-27 06:33:47 +0000

    Thu Sep 27 06:30:22 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/config-g++-common.h:
              Introduced a new ACE_HAS_CPP11 which is set with GCC 4.7 at
              the moment the C++11 features are enabled
    
            * tests/Compiler_Features_15_Test.cpp:
            * tests/Compiler_Features_16_Test.cpp:
            * tests/Compiler_Features_17_Test.cpp:
            * tests/Compiler_Features_18_Test.cpp:
            * tests/run_test.lst:
            * tests/tests.mpc:
              Added some new compiler tests to validate the compiler C++11
              support

---
+++ ACE/tests/Compiler_Features_16_Test.cpp
@@ -0,0 +1,63 @@
+// $Id$
+
+/**
+ * This program checks if the compiler / platform supports strongly
+ * typed enums
+ */
+
+#include "test_config.h"
+
+#if defined (ACE_HAS_CPP11)
+
+#include <limits>
+#include <stdint.h>
+#include <ostream>
+
+static constexpr uint32_t bound = std::numeric_limits<uint32_t>::max();
+
+namespace CORBA {
+  enum class TCKind : uint32_t
+  {
+    tk_null,
+    tk_void,
+    tk_event
+  };// TCKind
+};
+
+std::ostream& operator<< 
+(std::ostream& strm,CORBA::TCKind _enumerator)
+{
+  switch (_enumerator) {
+    case CORBA::TCKind::tk_null: return strm << "CORBA::TCKind::tk_null"; break;
+    case CORBA::TCKind::tk_void: return strm << "CORBA::TCKind::tk_void"; break;
+    default: return strm;
+  }
+}
+
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_16_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("Strongly typed enums work.\n")));
+  
+  ACE_END_TEST;
+  
+  return 0;
+}
+
+#else
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_16_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("No C++11 support enabled\n")));
+  
+  ACE_END_TEST;
+  return 0;
+}
+
+#endif
