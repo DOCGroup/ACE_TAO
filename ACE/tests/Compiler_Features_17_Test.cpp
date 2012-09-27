66b042e086bb7aaa10cd18dd73a5157d885c1f7e
Johnny Willemsen <jwillemsen@remedy.nl>
2012-09-27 06:33:47 +0000

    Thu Sep 27 06:30:22 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/config-g++-common.h:
              Introduced a new ACE_HAS_CPP11 which is set with GCC 4.7 at
              the moment the C++11 features are enabled
    
            * tests/Compiler_Features_15_Test.cpp:
            * tests/Compiler_Features_16_Test.cpp:
            * tests/Compiler_Features_17_Test.cpp:
            * tests/Compiler_Features_18_Test.cpp:
            * tests/run_test.lst:
            * tests/tests.mpc:
              Added some new compiler tests to validate the compiler C++11
              support

---
+++ ACE/tests/Compiler_Features_17_Test.cpp
@@ -0,0 +1,48 @@
+// $Id$
+
+/**
+ * This program checks if the compiler / platform supports constexpr
+ */
+
+#include "test_config.h"
+
+#if defined (ACE_HAS_CPP11)
+
+#include <limits>
+#include <stdint.h>
+
+class A
+{
+public:
+  A ();
+  static constexpr char const* adapter_name = "TAOX11_AnyInsert_Adapter";
+  static constexpr uint32_t bound = std::numeric_limits<uint32_t>::max();
+};
+
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_17_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("Constexpr works.\n")));
+  
+  ACE_END_TEST;
+  
+  return 0;
+}
+
+#else
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_17_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("No C++11 support enabled\n")));
+  
+  ACE_END_TEST;
+  return 0;
+}
+
+#endif
---
+++ ACE/tests/Compiler_Features_18_Test.cpp
@@ -0,0 +1,53 @@
+// $Id$
+
+/**
+ * This program checks if the compiler / platform supports strongly
+ * array
+ */
+
+#include "test_config.h"
+
+#if defined (ACE_HAS_CPP11)
+
+#include <array>
+namespace Test
+{
+  typedef std::array< float, 5 > arrayType;
+  typedef std::array< arrayType, 5 > array_array;
+};
+
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_18_Test"));
+
+  Test::array_array v_in = { { Test::arrayType {
+    { 1.0, 2.0, 3.0, 4.0, 5.0 } }, Test::arrayType { { 1.0, 2.0, 3.0, 4.0,
+    5.0 } }, Test::arrayType { { 1.0, 2.0, 3.0, 4.0, 5.0 } },
+    Test::arrayType { { 1.0, 2.0, 3.0, 4.0, 5.0 } }, Test::arrayType { {
+    1.0, 2.0, 3.0, 4.0, 5.0 } } } };  
+    
+  ACE_UNUSED_ARG (v_in);
+    
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("Std arrays work.\n")));
+  
+  ACE_END_TEST;
+  
+  return 0;
+}
+
+#else
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_18_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("No C++11 support enabled\n")));
+  
+  ACE_END_TEST;
+  return 0;
+}
+
+#endif
