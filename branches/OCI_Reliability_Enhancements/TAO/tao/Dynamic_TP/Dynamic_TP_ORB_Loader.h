0f07d4f9d91e30e0c55bf884b6b3e7a9c58a5972
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-10-22 14:42:23 +0000

    Mon Oct 22 14:40:47 UTC 2012  Phil Mesnier  <mesnier_p@ociweb.com>
            * tao/Dynamic_TP:
    
            * tao/Dynamic_TP/Dynamic_TP.mpc:
            * tao/Dynamic_TP/Dynamic_TP_ORB_Loader.h:
            * tao/Dynamic_TP/Dynamic_TP_ORB_Loader.cpp:
            * tao/Dynamic_TP/Dynamic_TP_POA_Loader.h:
            * tao/Dynamic_TP/Dynamic_TP_POA_Loader.cpp:
            * tao/Dynamic_TP/dynamic_tp_export.h:
    
              New container library for Dynamic Thread Pools applied to either ORB or POA.

---
+++ branches/OCI_Reliability_Enhancements/TAO/tao/Dynamic_TP/Dynamic_TP_ORB_Loader.h
@@ -0,0 +1,59 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    Dynamic_TP_ORB_Loader.h
+ *
+ *  $Id$
+ *
+ *   Header file for Loading DynamicTP.
+ *
+ *
+ *  @author  Phil Mesnier <mesnier_p@ociweb.com>
+ */
+//=============================================================================
+
+#ifndef TAO_DYNAMICTP_ORB_LOADER_H
+#define TAO_DYNAMICTP_ORB_LOADER_H
+#include /**/ "ace/pre.h"
+
+#include "tao/orbconf.h"
+
+#include "tao/DynamicTP/dynamictp_export.h"
+#include "ace/Service_Object.h"
+#include "ace/Service_Config.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+class TAO_ORB_Core;
+
+class TAO_DynamicTP_Export TAO_Dynamic_TP_ORB_Loader : public ACE_Service_Object
+{
+public:
+
+  /// Constructor.
+  TAO_Dynamic_TP_ORB_Loader (void);
+
+  /// Destructor.
+  virtual ~TAO_Dynamic_TP_ORB_Loader (void);
+
+  /// Initialize the DynamicTP loader hooks.
+  virtual int init (int argc, ACE_TCHAR* []);
+
+private:
+  /// Set to true after init is called.
+  bool initialized_;
+};
+
+
+ACE_STATIC_SVC_DECLARE_EXPORT (TAO_Dynamic_TP_ORB, TAO_Dynamic_TP_ORB_Loader)
+ACE_FACTORY_DECLARE (TAO_Dynamic_TP_ORB, TAO_Dynamic_TP_ORB_Loader)
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+#include /**/ "ace/post.h"
+#endif /* TAO_DYNAMIC_TP_LOADER_H */
---
+++ branches/OCI_Reliability_Enhancements/TAO/tao/Dynamic_TP/Dynamic_TP_POA_Loader.h
@@ -0,0 +1,59 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ *  @file    Dynamic_TP_POA_Loader.h
+ *
+ *  $Id$
+ *
+ *   Header file for Loading DynamicTP.
+ *
+ *
+ *  @author  Phil Mesnier <mesnier_p@ociweb.com>
+ */
+//=============================================================================
+
+#ifndef TAO_DYNAMIC_TP_POA_LOADER_H
+#define TAO_DYNAMIC_TP_POA_LOADER_H
+#include /**/ "ace/pre.h"
+
+#include "tao/orbconf.h"
+
+#include "tao/Dynamic_TP/dynamic_tp_export.h"
+#include "ace/Service_Object.h"
+#include "ace/Service_Config.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+# pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+TAO_BEGIN_VERSIONED_NAMESPACE_DECL
+
+class TAO_POA_Core;
+
+class TAO_Dynamic_TP_Export TAO_Dynamic_TP_POA_Loader : public ACE_Service_Object
+{
+public:
+
+  /// Constructor.
+  TAO_Dynamic_TP_POA_Loader (void);
+
+  /// Destructor.
+  virtual ~TAO_Dynamic_TP_POA_Loader (void);
+
+  /// Initialize the DynamicTP loader hooks.
+  virtual int init (int argc, ACE_TCHAR* []);
+
+private:
+  /// Set to true after init is called.
+  bool initialized_;
+};
+
+
+ACE_STATIC_SVC_DECLARE_EXPORT (TAO_Dynamic_TP_POA, TAO_Dynamic_TP_POA_Loader)
+ACE_FACTORY_DECLARE (TAO_Dynamic_TP_POA, TAO_Dynamic_TP_POA_Loader)
+
+TAO_END_VERSIONED_NAMESPACE_DECL
+
+#include /**/ "ace/post.h"
+#endif /* TAO_DYNAMIC_TP_POA_LOADER_H */
