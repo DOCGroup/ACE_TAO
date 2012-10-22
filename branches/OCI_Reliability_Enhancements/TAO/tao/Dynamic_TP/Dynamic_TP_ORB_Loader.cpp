618f4bebd8a8934623641dd34ea26a89c0c32f0f
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-10-22 21:15:42 +0000

    Mon Oct 22 21:12:11 UTC 2012  Phil Mesnier  <mesnier_p@ociweb.com>
            * tao/Dynamic_TP/Dynamic_TP_ORB_Loader.h:
            * tao/Dynamic_TP/Dynamic_TP_ORB_Loader.cpp:
            * tao/Dynamic_TP/Dynamic_TP_ORB_ORBInitializer.h:
    
              Updates to actually compile the ORB portion.

--- branches/OCI_Reliability_Enhancements/TAO/tao/Dynamic_TP/Dynamic_TP_ORB_Loader.cpp
+++ branches/OCI_Reliability_Enhancements/TAO/tao/Dynamic_TP/Dynamic_TP_ORB_Loader.cpp
@@ -4,12 +4,13 @@
 
 #if defined (TAO_HAS_CORBA_MESSAGING) && TAO_HAS_CORBA_MESSAGING != 0
 
-//#include "tao/DynamicTP/RT_ORBInitializer.h"
+#include "tao/Dynamic_TP/Dynamic_TP_ORB_ORBInitializer.h"
 
 #include "tao/debug.h"
 #include "tao/ORB_Constants.h"
 #include "tao/ORBInitializer_Registry.h"
 #include "tao/SystemException.h"
+#include "ace/Dynamic_Service.h"
 #include "ace/OS_NS_strings.h"
 #include "ace/Arg_Shifter.h"
 
@@ -43,138 +44,14 @@ TAO_Dynamic_TP_ORB_Loader::init (int argc, ACE_TCHAR* argv[])
       "Dynamic_TP_ORB_Loader",
       true);
 
-  if (rt_loader != 0 && rt_loader != this)
-    {
-      return rt_loader->init (argc, argv);
-    }
+  // TODO: Stuff goes in here
 
-  // Set defaults.
-  int priority_mapping_type =
-    TAO_RT_ORBInitializer::TAO_PRIORITY_MAPPING_DIRECT;
-  int network_priority_mapping_type =
-    TAO_RT_ORBInitializer::TAO_NETWORK_PRIORITY_MAPPING_LINEAR;
-  int ace_sched_policy = ACE_SCHED_OTHER;
-  long sched_policy = THR_SCHED_DEFAULT;
-  long scope_policy = THR_SCOPE_PROCESS;
-  int curarg = 0;
-  ACE_Time_Value dynamic_thread_time;
-  TAO_RT_ORBInitializer::TAO_DynamicTP_DT_LifeSpan lifespan = TAO_RT_ORBInitializer::TAO_DynamicTP_DT_INFINITIVE;
-
-  ACE_Arg_Shifter arg_shifter (argc, argv);
-
-  // Parse any service configurator parameters.
-  while (arg_shifter.is_anything_left ())
-    {
-      const ACE_TCHAR *current_arg = 0;
-      if (0 != (current_arg = arg_shifter.get_the_parameter
-                  (ACE_TEXT("-ORBPriorityMapping"))))
-        {
-          const ACE_TCHAR *name = current_arg;
-          if (ACE_OS::strcasecmp (name,
-                                  ACE_TEXT("continuous")) == 0)
-            priority_mapping_type =
-              TAO_RT_ORBInitializer::TAO_PRIORITY_MAPPING_CONTINUOUS;
-          else if (ACE_OS::strcasecmp (name,
-                                       ACE_TEXT("linear")) == 0)
-            priority_mapping_type =
-              TAO_RT_ORBInitializer::TAO_PRIORITY_MAPPING_LINEAR;
-          else if (ACE_OS::strcasecmp (name,
-                                       ACE_TEXT("direct")) == 0)
-            priority_mapping_type =
-              TAO_RT_ORBInitializer::TAO_PRIORITY_MAPPING_DIRECT;
-          else
-            ACE_DEBUG ((LM_DEBUG,
-                        ACE_TEXT("Dynamic_TP_ORB_Loader - unknown argument")
-                        ACE_TEXT(" <%s> for -ORBPriorityMapping\n"),
-                        name));
-          arg_shifter.consume_arg ();
-        }
-      else if (0 != (current_arg = arg_shifter.get_the_parameter
-                                 (ACE_TEXT("-ORBSchedPolicy"))))
-        {
-          const ACE_TCHAR *name = current_arg;
-          if (ACE_OS::strcasecmp (name,
-                                  ACE_TEXT("SCHED_OTHER")) == 0)
-            {
-              ace_sched_policy = ACE_SCHED_OTHER;
-              sched_policy = THR_SCHED_DEFAULT;
-            }
-          else if (ACE_OS::strcasecmp (name,
-                                       ACE_TEXT("SCHED_FIFO")) == 0)
-            {
-              ace_sched_policy = ACE_SCHED_FIFO;
-              sched_policy = THR_SCHED_FIFO;
-            }
-          else if (ACE_OS::strcasecmp (name,
-                                       ACE_TEXT("SCHED_RR")) == 0)
-            {
-              ace_sched_policy = ACE_SCHED_RR;
-              sched_policy = THR_SCHED_RR;
-            }
-          else
-            ACE_DEBUG ((LM_DEBUG,
-                        ACE_TEXT("Dynamic_TP_ORB_Loader - unknown argument")
-                        ACE_TEXT(" <%s> for -ORBSchedPolicy\n"),
-                        name));
-          arg_shifter.consume_arg ();
-        }
-      else if (0 != (current_arg = arg_shifter.get_the_parameter
-                                 (ACE_TEXT("-ORBScopePolicy"))))
-        {
-          const ACE_TCHAR *name = current_arg;
-          if (ACE_OS::strcasecmp (name,
-                                  ACE_TEXT("SYSTEM")) == 0)
-            scope_policy = THR_SCOPE_SYSTEM;
-          else if (ACE_OS::strcasecmp (name,
-                                       ACE_TEXT("PROCESS")) == 0)
-            scope_policy = THR_SCOPE_PROCESS;
-          else
-            ACE_DEBUG ((LM_DEBUG,
-                        ACE_TEXT("Dynamic_TP_ORB_Loader - unknown argument")
-                        ACE_TEXT(" <%s> for -ORBScopePolicy\n"),
-                        name));
-          arg_shifter.consume_arg ();
-        }
-      else if (0 != (current_arg = arg_shifter.get_the_parameter
-                                 (ACE_TEXT("-RTORBNetworkPriorityMapping"))))
-        {
-          const ACE_TCHAR *name = current_arg;
-          if (ACE_OS::strcasecmp (name,
-                                  ACE_TEXT("linear")) == 0)
-              network_priority_mapping_type =
-                TAO_RT_ORBInitializer::TAO_NETWORK_PRIORITY_MAPPING_LINEAR;
-          arg_shifter.consume_arg ();
-        }
-      else if (0 != (current_arg = arg_shifter.get_the_parameter
-                                   (ACE_TEXT("-RTORBDynamicThreadIdleTimeout"))))
-        {
-          const ACE_TCHAR *name = current_arg;
-          int timeout = ACE_OS::atoi (name);
-          dynamic_thread_time = ACE_Time_Value (0, timeout);
-          lifespan = TAO_RT_ORBInitializer::TAO_DynamicTP_DT_IDLE;
-          arg_shifter.consume_arg ();
-        }
-      else if (0 != (current_arg = arg_shifter.get_the_parameter
-                                   (ACE_TEXT("-RTORBDynamicThreadRunTime"))))
-        {
-          const ACE_TCHAR *name = current_arg;
-          int timeout = ACE_OS::atoi (name);
-          dynamic_thread_time = ACE_Time_Value (0, timeout);
-          lifespan = TAO_RT_ORBInitializer::TAO_DynamicTP_DT_FIXED;
-          arg_shifter.consume_arg ();
-        }
-    else
-      {
-        arg_shifter.ignore_arg ();
-        if (TAO_debug_level > 0)
-        {
-          ACE_ERROR ((LM_ERROR,
-                      ACE_TEXT("Dynamic_TP_ORB_Loader: Unknown option ")
-                      ACE_TEXT("<%s>.\n"),
-                      argv[curarg]));
-        }
-      }
-    }
+  size_t num_static = 1;
+  size_t num_dynamic = 0;
+  size_t stack = 0;
+  ACE_Time_Value dynamic_thread_time = ACE_Time_Value (0,0);
+  TAO_Dynamic_TP_ORB_ORBInitializer::TAO_Dynamic_TP_DT_LifeSpan lifespan =
+    TAO_Dynamic_TP_ORB_ORBInitializer::TAO_Dynamic_TP_DT_INFINITIVE;
 
   // Register the ORB initializer.
   try
@@ -184,13 +61,11 @@ TAO_Dynamic_TP_ORB_Loader::init (int argc, ACE_TCHAR* argv[])
 
       /// Register the DynamicTP ORBInitializer.
       ACE_NEW_THROW_EX (temp_orb_initializer,
-                        TAO_RT_ORBInitializer (priority_mapping_type,
-                                               network_priority_mapping_type,
-                                               ace_sched_policy,
-                                               sched_policy,
-                                               scope_policy,
-                                               lifespan,
-                                               dynamic_thread_time),
+                        TAO_Dynamic_TP_ORB_ORBInitializer (num_static,
+                                                           num_dynamic,
+                                                           stack,
+                                                           lifespan,
+                                                           dynamic_thread_time),
                         CORBA::NO_MEMORY (
                           CORBA::SystemException::_tao_minor_code (
                             TAO::VMCID,
@@ -216,7 +91,7 @@ TAO_Dynamic_TP_ORB_Loader::init (int argc, ACE_TCHAR* argv[])
 
 /////////////////////////////////////////////////////////////////////
 
-ACE_FACTORY_DEFINE (TAO_DynamicTP_ORB, TAO_Dynamic_TP_ORB_Loader)
+ACE_FACTORY_DEFINE (TAO_Dynamic_TP, TAO_Dynamic_TP_ORB_Loader)
 ACE_STATIC_SVC_DEFINE (TAO_Dynamic_TP_ORB_Loader,
                        ACE_TEXT ("Dynamic_TP_ORB_Loader"),
                        ACE_SVC_OBJ_T,
@@ -226,4 +101,5 @@ ACE_STATIC_SVC_DEFINE (TAO_Dynamic_TP_ORB_Loader,
                        0)
 TAO_END_VERSIONED_NAMESPACE_DECL
 
-#endif /* TAO_HAS_CORBA_MESSAGING && TAO_HAS_CORBA_MESSAGING != 0 */
+#endif /* (TAO_HAS_CORBA_MESSAGING) && TAO_HAS_CORBA_MESSAGING != 0 */
+
