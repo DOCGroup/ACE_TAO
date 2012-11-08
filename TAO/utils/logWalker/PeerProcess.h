3320704ab2db4a9f60c2f75e836767aca3ea3c78
Phil Mesnier <mesnier_p@ociweb.com>
2012-11-08 21:54:38 +0000

    Thu Nov  8 21:51:42 UTC 2012  Phil Mesnier  <mesnier_p@ociweb.com>
            * utils/logWalker/HostProcess.h:
            * utils/logWalker/HostProcess.cpp:
            * utils/logWalker/Invocation.h:
            * utils/logWalker/Invocation.cpp:
            * utils/logWalker/Log.h:
            * utils/logWalker/Log.cpp:
            * utils/logWalker/PeerObject.cpp:
            * utils/logWalker/PeerProcess.h:
            * utils/logWalker/PeerProcess.cpp:
            * utils/logWalker/Session.h:
            * utils/logWalker/Session.cpp:
            * utils/logWalker/Thread.h:
            * utils/logWalker/Thread.cpp:
            * utils/logWalker/logWalker.cpp:
    
              Bringing over changes from OCI TAO 2.0a aimed at improving the
              ability to handle logs from heavily threaded processes and for
              inspecting some request contents for common service operations.

--- TAO/utils/logWalker/PeerProcess.h
+++ TAO/utils/logWalker/PeerProcess.h
@@ -32,13 +32,14 @@ public:
 
 
 typedef ACE_RB_Tree<u_long, PeerObject*, ACE_Less_Than<u_long>, ACE_Null_Mutex> PeerObjectTable;
+typedef ACE_RB_Tree<long, PeerObject*, ACE_Less_Than<long>, ACE_Null_Mutex> ObjectByIndex;
 typedef ACE_DLList<Invocation> InvocationList;
 typedef ACE_DLList<Transport> TransportList;
 
 class PeerProcess
 {
 public:
-  static char *nextIdent(void);
+  static char *nextIdent(bool is_server);
   PeerProcess (size_t offset, bool is_server);
 
   virtual ~PeerProcess (void);
@@ -49,11 +50,12 @@ public:
   HostProcess *owner (void);
 
   void match_hosts (Session *session);
-  void set_server_addr (const char *addr);
+  void set_server_addr (const ACE_CString &addr);
   const ACE_CString &server_addr (void) const;
   const ACE_CString &last_client_addr (void) const;
 
   bool is_server (void) const;
+  bool ssl (bool is_ssl);
   void add_transport (Transport *t);
   Transport *last_transport (void);
   Transport *find_transport (long handle);
@@ -72,15 +74,18 @@ public:
 
 private:
   char *ident_;
+  char *origin_;
   HostProcess *owner_;
   HostProcess *remote_;
   ACE_CString server_addr_;
   TransportList transports_;
   Transport *last_transport_;
   bool server_;
+  bool ssl_;
   size_t origin_offset_;
   PeerObjectTable objects_;
   InvocationList invocations_;
+  ObjectByIndex object_by_index_;
 };
 
 
--- TAO/utils/logWalker/Thread.h
+++ TAO/utils/logWalker/Thread.h
@@ -14,44 +14,60 @@
 #include "PeerProcess.h"
 
 typedef ACE_Unbounded_Stack<PeerProcess *> UpcallStack;
+typedef ACE_Unbounded_Stack<Invocation *> InvocationStack;
 
 class Thread
 {
 public:
-  Thread (long tid, const char *alias);
+  Thread (long tid, const char *alias, size_t offset);
   long max_depth (void) const;
-  long encounters (void) const;
+  long client_encounters (void) const;
+  long server_encounters (void) const;
   long id (void) const;
   const ACE_CString &alias (void) const;
   void incoming_from (PeerProcess *);
   void add_invocation (Invocation *);
+  void push_invocation (Invocation *);
+  void pop_invocation (void);
+  Invocation *current_invocation (void) const;
 
   PeerProcess *incoming (void) const;
   void handle_request (void);
   void enter_wait (PeerProcess *);
   void exit_wait (PeerProcess *, size_t linenum);
-  Invocation::GIOP_Buffer *giop_target (void);
-  void set_giop_target (Invocation::GIOP_Buffer *buffer);
-  void dump_detail (ostream &strm);
+  GIOP_Buffer *giop_target (void);
+  void set_giop_target (GIOP_Buffer *buffer);
+  void dump_detail (ostream &strm) const;
   void dump_invocations (ostream &strm);
+  void get_summary (long &sent_reqs, long &recv_reqs, size_t &sent_size, size_t &recv_size);
   PeerProcess *pending_peer (void) const;
   void pending_peer (PeerProcess *pp);
-
+  void pending_local_addr (const ACE_CString &addr);
+  const ACE_CString& pending_local_addr (void) const;
   void active_handle (long handle);
   long active_handle (void) const;
+  void set_dup (Thread *other, bool set_other);
+  void clear_dup (void);
+  void swap_target (void);
+  bool has_dup (void);
 
 private:
   long id_;
   ACE_CString alias_;
   size_t max_depth_;
-  long encounters_;
+  long client_encounters_;
+  long server_encounters_;
   long nested_;
   UpcallStack pending_;
   PeerProcess *incoming_;
   PeerProcess *new_connection_;
-  Invocation::GIOP_Buffer *giop_target_;
+  ACE_CString pending_local_addr_;
+  GIOP_Buffer *giop_target_;
+  Thread *target_dup_;
   InvocationList invocations_;
+  InvocationStack current_invocation_;
   long active_handle_;
+  size_t first_line_;
 };
 
 #endif // LOG_WALKER_THREAD_H
