dabb6d91ab4089e7e77f4595194c8403aa921d95
labancap <labancap@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-11-14 03:24:17 +0000

    Fixed nsgroup tests

--- branches/OCI_Reliability_Enhancements/TAO/utils/nsgroup/run_test.pl
+++ branches/OCI_Reliability_Enhancements/TAO/utils/nsgroup/run_test.pl
@@ -80,28 +80,18 @@ sub run_client ($)
 
 sub run_clients ()
 {
-    #create -type rr GiasGateway
-    #bind -og_name GiasGateway GiasGateway
-    #add_member -og_name GiasGateway -member_name mcknerney2-lnx5-dev/GiasGateway1 IOR:010000002300000049444c3a4741562f4f626a6563745265666572656e6365466163746f72793a312e3000000100000000000000b2000000010102000e00000031302e33352e3232352e313530005046530000003a3e0232311c756e636c6173735f757374696c2e6363692d612e6c6f636174696f6e0d476961734761746577617931002249444c3a4741562f4f626a6563745265666572656e6365466163746f72793a312e300003000000000000000800000001000000415f544901000000180000000100000001000100000000000401010001000000090101000600000006000000010000002f00
-    #add_member -og_name GiasGateway -member_name mcknerney2-lnx5-dev/GiasGateway2 IOR:010000002300000049444c3a4741562f4f626a6563745265666572656e6365466163746f72793a312e3000000100000000000000b2000000010102000e00000031302e33352e3232352e313530005046530000003a3e0232311c756e636c6173735f757374696c2e6363692d612e6c6f636174696f6e0d476961734761746577617932002249444c3a4741562f4f626a6563745265666572656e6365466163746f72793a312e300003000000000000000800000001000000415f544901000000180000000100000001000100000000000401010001000000090101000600000006000000010000002f00
-    my $IOR = "IOR:010000002100000049444c3a6f6d672e6f72672f46542f4e616d696e674d616e616765723a312e300000000001000000000000006c000000010102000e00000031302e3230312e3230302e36340005e71b00000014010f00525354caaaa250c6ba0e000000000001000000010000004e02000000000000000800000001000000004f41540100000018000000010000000100010001000000010001050901010000000000";
-
+    run_client ("group_list");
     run_client ("group_create -group GiasGateway -policy round -type_id IDL:omg.org/FT/NamingManager:1.0");
-    run_client ("group_create -group group2 -policy round -type_id IDL:omg.org/FT/NamingManager:1.0");
     run_client ("group_list");
     run_client ("group_bind -group GiasGateway -n GiasGateway");
-    run_client ("group_modify -group group1 -policy rand");
-    run_client ("member_add -group GiasGateway -location 10.201.200.64 -ior $IOR");
-    run_client ("member_add -group GiasGateway -location dhcp34.ociweb.com -ior $IOR");
-    run_client ("member_list -group group1");
-    run_client ("member_show -group group1 -location dhcp34.ociweb.com");
-    run_client ("member_show -group group2 -location nowhere junk_command");
-    run_client ("member_remove -group group1 -location dhcp34.ociweb.com");
+    run_client ("group_modify -group GiasGateway -policy rand");
+    run_client ("member_list -group GiasGateway");
+    run_client ("member_add -group GiasGateway -location 10.201.200.64 -ior file://$naming_mgr_client_iorfile");
+    run_client ("member_list -group GiasGateway");
+    run_client ("member_show -group GiasGateway -location 10.201.200.64");
+    run_client ("member_remove -group GiasGateway -location 10.201.200.64");
+    run_client ("member_list -group GiasGateway");
     run_client ("group_remove -group GiasGateway");
-    run_client ("group_list");
-    run_client ("group_remove -group group2");
-    run_client ("group_list");
-    run_client ("group_remove -group groupN");
     run_client ("group_list");
     run_client ("--help");
 }
