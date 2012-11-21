a32f6d2a9e6e5b3045852fad51bf76ec87be91bc
stanleyk <stanleyk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-11-21 02:31:17 +0000

    Added files to implement the replication interface.

---
+++ branches/OCI_Reliability_Enhancements/TAO/orbsvcs/orbsvcs/Naming/FaultTolerant/FT_Naming_Replication_Manager.cpp
@@ -0,0 +1,59 @@
+#include "FT_Naming_Replication_Manager.h"
+#include "FT_NamingReplicationC.h"
+#include "FT_Naming_Server.h"
+#include "tao/corba.h"
+
+
+FT_Naming::ReplicationManager_var TAO_FT_Naming_Replication_Manager::peer_replica_;
+
+TAO_FT_Naming_Replication_Manager::TAO_FT_Naming_Replication_Manager(TAO_FT_Naming_Server *naming_svr)
+  : naming_svr_ (naming_svr)
+{
+}
+
+
+TAO_FT_Naming_Replication_Manager::~TAO_FT_Naming_Replication_Manager(void)
+{
+}
+
+void 
+TAO_FT_Naming_Replication_Manager::initialize (CORBA::ORB_ptr orb,
+                   PortableServer::POA_ptr root_poa)
+{
+   // Nothing to do now
+  ACE_UNUSED_ARG (orb);
+  ACE_UNUSED_ARG (root_poa);
+}
+
+
+void 
+TAO_FT_Naming_Replication_Manager::register_replica (
+    ::FT_Naming::ReplicationManager_ptr replica)
+{
+  // Store the provided reference
+  peer_replica_ = replica;
+}
+
+void 
+TAO_FT_Naming_Replication_Manager::notify_updated_object_group (
+    const FT_Naming::ObjectGroupUpdate & group_info)
+{
+  // Make sure that we have a valid naming server
+  ACE_ASSERT (naming_svr_);
+  int result = this->naming_svr_->update_object_group (group_info);
+
+}
+
+void 
+TAO_FT_Naming_Replication_Manager::notify_updated_context (
+    const FT_Naming::NamingContextUpdate & context_info)
+{
+  ACE_ASSERT (naming_svr_);
+  int result = this->naming_svr_->update_naming_context (context_info);
+}
+
+FT_Naming::ReplicationManager_ptr 
+TAO_FT_Naming_Replication_Manager::peer_replica (void)
+{
+  return peer_replica_._retn ();
+}
---
+++ branches/OCI_Reliability_Enhancements/TAO/orbsvcs/orbsvcs/Naming/FaultTolerant/FT_Naming_Replication_Manager.h
@@ -0,0 +1,71 @@
+// -*- C++ -*-
+
+//=============================================================================
+/**
+ * @file   FT_Naming_Replication_Manager.h
+ *
+ * $Id$
+ *
+ * @author Kevin Stanley <stanleyk@ociweb.com>
+ */
+//=============================================================================
+
+
+#ifndef TAO_FT_NAMING_REPLICATION_MANAGER_H
+#define TAO_FT_NAMING_REPLICATION_MANAGER_H
+
+#include /**/ "ace/pre.h"
+
+#include "orbsvcs/Naming/FaultTolerant/ftnaming_export.h"
+
+#include "FT_NamingReplicationS.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "FT_NamingReplicationC.h"
+
+class TAO_FT_Naming_Server;
+
+class TAO_FtNaming_Export TAO_FT_Naming_Replication_Manager 
+  : public virtual POA_FT_Naming::ReplicationManager
+{
+public:
+
+ /* 
+  * Create a Replication Manager and provide it with the naming server
+  * to be updated whenever notified by the peer replica
+  */
+  TAO_FT_Naming_Replication_Manager(TAO_FT_Naming_Server *naming_svr);
+  virtual ~TAO_FT_Naming_Replication_Manager(void);
+
+  /// Initialize the naming manager. This will provide the poa to 
+  /// the naming manager and underlying components for use in 
+  /// managing the object groups.
+  void initialize (CORBA::ORB_ptr orb,
+                   PortableServer::POA_ptr root_poa);
+
+  virtual void register_replica (
+    FT_Naming::ReplicationManager_ptr replica);
+
+  virtual void notify_updated_object_group (
+    const FT_Naming::ObjectGroupUpdate & group_info);
+
+  virtual void notify_updated_context (
+    const FT_Naming::NamingContextUpdate & group_info);
+
+  FT_Naming::ReplicationManager_ptr peer_replica (void);
+
+protected:
+
+  // The object which implements the naming service and the object manager
+  TAO_FT_Naming_Server *naming_svr_;
+
+  // Store the reference to the replica object reference
+  // For now only a single replica is supported.
+  static FT_Naming::ReplicationManager_var peer_replica_;
+
+};
+
+#endif /* TAO_FT_NAMING_REPLICATION_MANAGER_H */
