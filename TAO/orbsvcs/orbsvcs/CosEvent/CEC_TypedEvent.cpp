f1e91809c8e38bf5e142dbb2c9c4e3f3a33727ba
Steve Huston <shuston@riverace.com>
2012-11-29 15:53:12 +0000

    ChangeLogTag: Thu Nov 29 10:52:52 EST 2012  Steve Huston  <shuston@riverace.com>

--- TAO/orbsvcs/orbsvcs/CosEvent/CEC_TypedEvent.cpp
+++
@@ -1,9 +0,0 @@
-// -*- C++ -*-
-//
-// $Id$
-
-#include "orbsvcs/CosEvent/CEC_TypedEvent.h"
-
-#if ! defined (__ACE_INLINE__)
-#include "orbsvcs/CosEvent/CEC_TypedEvent.inl"
-#endif /* __ACE_INLINE__ */
