2f0ab307fdec1cad7ed97ecc2216c213bd34f96f
Steve Huston <shuston@riverace.com>
2012-11-29 15:18:15 +0000

    ChangeLogTag: Thu Nov 29 10:17:56 EST 2012  Steve Huston  <shuston@riverace.com>

--- ACE/ace/config-macosx-mountain-lion.h
+++
@@ -1,7 +0,0 @@
-// $Id$
-#ifndef ACE_CONFIG_MACOSX_MOUNTAIN_LION_H
-#define ACE_CONFIG_MACOSX_MOUNTAIN_LION_H
-
-#include "ace/config-macosx-lion.h"
-
-#endif // ACE_CONFIG_MACOSX_MOUNTAIN_LION_H
---
+++ ace/Time_Policy_T.cpp
@@ -0,0 +1,27 @@
+// $Id$
+
+#ifndef ACE_TIME_POLICY_T_CPP
+#define ACE_TIME_POLICY_T_CPP
+
+#include "ace/Time_Policy_T.h"
+
+#if !defined (__ACE_INLINE__)
+#include "ace/Time_Policy_T.inl"
+#endif /* __ACE_INLINE__ */
+
+ACE_BEGIN_VERSIONED_NAMESPACE_DECL
+
+template <typename TIME_POLICY> ACE_INLINE
+ACE_Time_Policy_T<TIME_POLICY>::~ACE_Time_Policy_T ()
+{
+}
+
+template <typename TIME_POLICY> ACE_INLINE ACE_Time_Value
+ACE_Time_Policy_T<TIME_POLICY>::gettimeofday () const
+{
+  return this->time_policy_ ();
+}
+
+ACE_END_VERSIONED_NAMESPACE_DECL
+
+#endif /* ACE_TIME_POLICY_T_CPP */
