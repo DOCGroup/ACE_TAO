5fe78f0d88353d090b12ccd776bd6c31e7ac13bd
sma <sma@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-12-03 09:29:39 +0000

    Mon Dec 03 09:253:00 UTC 2012  Simon Massey  <simon dot massey at prismtech dot com> orbsvcs/orbsvcs/PortableGroup/UIPMC_Mcast_Transport.cpp: Re-introduced memory leak in MIOP server listerner. Something more complex and unknown is interacting with the memory data block requiring it's extended life.

--- TAO/orbsvcs/orbsvcs/PortableGroup/UIPMC_Mcast_Transport.cpp
+++ TAO/orbsvcs/orbsvcs/PortableGroup/UIPMC_Mcast_Transport.cpp
@@ -423,7 +423,7 @@ TAO_UIPMC_Mcast_Transport::handle_input (
                          0,
                          this->orb_core_->input_cdr_buffer_allocator (),
                          this->orb_core_->locking_strategy (),
-                         0,
+                         ACE_Message_Block::DONT_DELETE,
                          this->orb_core_->input_cdr_dblock_allocator ());
 
       // Create a message block
