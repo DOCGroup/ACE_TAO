cf448350e45409843d9f3627905ef24fa36e46c1
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-12-14 18:39:09 +0000

    Dec 14 18:38:56 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/impl/ndds/convertors/DataReaderQos.h:
              Fixed compile error with RTI DDS < 5

--- trunk/CIAO/connectors/dds4ccm/impl/ndds/convertors/DataReaderQos.h
+++ trunk/CIAO/connectors/dds4ccm/impl/ndds/convertors/DataReaderQos.h
@@ -42,7 +42,9 @@ operator<<= (::DDS::DataReaderQos &ddsqos, const ::DDS_DataReaderQos & qos)
   ddsqos.time_based_filter <<= qos.time_based_filter;
   ddsqos.reader_data_lifecycle <<= qos.reader_data_lifecycle;
   ddsqos.reader_resource_limits <<= qos.reader_resource_limits;
+#if defined (RTI_DDS_VERSION_MAJOR) && (RTI_DDS_VERSION_MAJOR >= 5)
   ddsqos.type_consistency <<= qos.type_consistency;
+#endif
 }
 
 inline void
@@ -61,7 +63,9 @@ operator<<= (::DDS_DataReaderQos &ddsqos, const ::DDS::DataReaderQos & qos)
   ddsqos.time_based_filter <<= qos.time_based_filter;
   ddsqos.reader_data_lifecycle <<= qos.reader_data_lifecycle;
   ddsqos.reader_resource_limits <<= qos.reader_resource_limits;
+#if defined (RTI_DDS_VERSION_MAJOR) && (RTI_DDS_VERSION_MAJOR >= 5)
   ddsqos.type_consistency <<= qos.type_consistency;
+#endif
 }
 
 #endif /* DDS4CCM_DATAREADERQOS_H */
