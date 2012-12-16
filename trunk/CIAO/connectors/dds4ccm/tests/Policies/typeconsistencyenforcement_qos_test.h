f165f4b75c44d6748bcf5f6bf9acdce99e7ad6d4
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2012-12-16 09:50:01 +0000

    Sun Dec 16 09:49:50 UTC 2012  Johnny Willemsen  <jwillemsen@remedy.nl>
            * connectors/dds4ccm/tests/Policies/dr_qos_test.cpp:
            * connectors/dds4ccm/tests/Policies/typeconsistencyenforcement_qos_test.h:
            * connectors/dds4ccm/tests/Policies/typeconsistencyenforcement_qos_test.cpp:
              Fixed compile error with RTI DDS < 5

--- trunk/CIAO/connectors/dds4ccm/tests/Policies/typeconsistencyenforcement_qos_test.h
+++ trunk/CIAO/connectors/dds4ccm/tests/Policies/typeconsistencyenforcement_qos_test.h
@@ -7,6 +7,8 @@
 #include "ndds/ndds_cpp.h"
 #include "dds4ccm/impl/ndds/convertors/TypeConsistencyEnforcementQosPolicy.h"
 
+#if defined (RTI_DDS_VERSION_MAJOR) && (RTI_DDS_VERSION_MAJOR >= 5)
+
 class TypeConsistencyEnforcementPolicyPolicyTest
 {
 public:
@@ -15,5 +17,7 @@ public:
   static bool check (const ::DDS_TypeConsistencyEnforcementQosPolicy & dds_qos,
                      const ::DDS::TypeConsistencyEnforcementQosPolicy & qos);
 };
+
+#endif
 
 #endif /* TYPECONSISTENCYENFORCEMENT_QOS_TEST_H_ */
