32a78dbb74dcb9589c3bc4706158c154d9d45cc9
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-01-28 07:56:36 +0000

    Mon Jan 28 07:55:27 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/config-macosx-mountainlion.h:
            * ace/config-macosx-mountain-lion.h:
            * include/makeinclude/platform_macosx_mountainlion.GNU:
            * include/makeinclude/platform_macosx_mountain_lion.GNU:
              Renamed mountain-lion to mountainlion to simplify packaging

--- trunk/ACE/ace/config-macosx-mountainlion.h
+++ trunk/ACE/ace/config-macosx-mountainlion.h
@@ -1,6 +1,6 @@
 // $Id$
-#ifndef ACE_CONFIG_MACOSX_MOUNTAIN_LION_H
-#define ACE_CONFIG_MACOSX_MOUNTAIN_LION_H
+#ifndef ACE_CONFIG_MACOSX_MOUNTAINLION_H
+#define ACE_CONFIG_MACOSX_MOUNTAINLION_H
 
 #include "ace/config-macosx-lion.h"
 
