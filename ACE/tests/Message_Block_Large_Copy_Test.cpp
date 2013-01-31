94df53b603bb6bfa49e7a7f40a52fd1f432a555d
stanleyk <stanleyk@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-01-31 14:32:27 +0000

    Fixed error in allocation of character array. Previous checkin did not have change in it.

--- ACE/tests/Message_Block_Large_Copy_Test.cpp
+++ ACE/tests/Message_Block_Large_Copy_Test.cpp
@@ -20,7 +20,7 @@ size_t
 run_duplicate_test (size_t msg_block_count, size_t msg_block_size)
 {
     size_t rc = 0;
-    char block[msg_block_size];
+    char* block = new char[msg_block_size];
 
     for(size_t j = 0 ; j != msg_block_size; j++)
       block[j] = 'A';
@@ -51,6 +51,8 @@ run_duplicate_test (size_t msg_block_count, size_t msg_block_size)
       mb_test-> release();
       mb_top-> release();
     }
+
+    delete block;
 
     return rc;
 }
