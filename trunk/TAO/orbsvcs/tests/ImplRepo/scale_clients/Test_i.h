f66db326ed326dcf66579b3dd83b12a84dcf9f23
harrisb <harrisb@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-02-06 14:59:19 +0000

    Wed Feb  6 14:57:44 UTC 2013  Byron Harris  <harrisb@ociweb.com>

---
+++ trunk/TAO/orbsvcs/tests/ImplRepo/scale_clients/Test_i.h
@@ -0,0 +1,34 @@
+/* -*- C++ -*- $Id$ */
+
+#ifndef TEST_I_H_
+#define TEST_I_H_
+
+#include "TestS.h"
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+
+class  Test_i : public virtual POA_Test
+{
+public:
+
+  Test_i (CORBA::Short num_requests_expected);
+
+  virtual ~Test_i ();
+
+  virtual CORBA::Short get_num_requests (const CORBA::Short delay_secs);
+
+  /// Valid after shutdown () is called.
+  static bool expected_requests_made ();
+
+private:
+
+  CORBA::Short num_requests_expected_;
+  CORBA::Short num_requests_made_;
+
+  static bool expected_requests_made_;
+};
+
+#endif /* TEST_I_H_ */
