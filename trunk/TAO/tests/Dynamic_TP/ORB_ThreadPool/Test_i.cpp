639e6b954c55e7baa95a0e69677efbfbd5fc5333
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-02-15 19:29:50 +0000

    Fri Feb 15 19:25:53 UTC 2013  Phil Mesnier  <mesnier_p@ociweb.com>

--- trunk/TAO/tests/Dynamic_TP/ORB_ThreadPool/Test_i.cpp
+++ trunk/TAO/tests/Dynamic_TP/ORB_ThreadPool/Test_i.cpp
@@ -4,21 +4,74 @@
 #include "Test_i.h"
 #include "ace/OS_NS_unistd.h"
 
-Test_i::Test_i (CORBA::ORB_ptr orb)
+Sleeper_i::Sleeper_i (CORBA::ORB_ptr orb)
   : orb_ (CORBA::ORB::_duplicate (orb))
 {
 }
 
 void
-Test_i::delay(void)
+Sleeper_i::delay(CORBA::Short sec)
 {
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Test_i::delay called\n"));
-  ACE_OS::sleep (1);
-  ACE_DEBUG ((LM_DEBUG, "(%P|%t) Test_i::delay returning\n"));
+  ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("(%P|%t) Sleeper_i::delay called\n")));
+  ACE_OS::sleep (sec);
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT ("(%P|%t) Sleeper_i::delay returning\n")));
 }
 
 void
-Test_i::shutdown (void)
+Sleeper_i::shutdown (void)
 {
+  this->orb_->shutdown (0);
+}
+
+//-----------------------------------------------
+
+Middle_i::Middle_i (CORBA::ORB_ptr orb, Test::Sleeper_ptr s)
+  : orb_ (CORBA::ORB::_duplicate (orb)),
+    sleeper_ (Test::Sleeper::_duplicate (s))
+{
+}
+
+void
+Middle_i::call_delay(CORBA::Short sec)
+{
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT ("(%P|%t) Middle_i::delay called\n")));
+
+  ACE_hthread_t thr;
+  ACE_Thread::self (thr);
+  bool recursive = false;
+  {
+    ACE_GUARD (ACE_Thread_Mutex, guard, this->lock_);
+    int result = this->threads_.insert (thr);
+    ACE_ASSERT (result != -1);
+    if (result == 1)
+      {
+        ACE_DEBUG ((LM_DEBUG,
+                    ACE_TEXT ("(%P|%t) Middle_i::call_delay, ")
+                    ACE_TEXT ("recursive upcall detected for thr = %d\n"),
+                    thr));
+        recursive = true;
+      }
+  }
+
+  this->sleeper_->delay(sec);
+
+  if (!recursive)
+    {
+      ACE_GUARD (ACE_Thread_Mutex, guard, this->lock_);
+      int result = this->threads_.remove (thr);
+      ACE_ASSERT (result != -1);
+    }
+
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT("(%P|%t) Middle_i::delay returning recursive = %d\n"),
+              recursive));
+}
+
+void
+Middle_i::shutdown (void)
+{
+  sleeper_->shutdown ();
   this->orb_->shutdown (0);
 }
--- trunk/TAO/tests/Dynamic_TP/ORB_ThreadPool/Test_i.h
+++ trunk/TAO/tests/Dynamic_TP/ORB_ThreadPool/Test_i.h
@@ -7,21 +7,45 @@
 #include /**/ "ace/pre.h"
 
 #include "TestS.h"
+#include "ace/Synch.h"
+#include "ace/Unbounded_Set.h"
 
 /// Implement the Test::Sleeper interface
-class Test_i : public virtual POA_Test::Sleeper
+class Sleeper_i : public virtual POA_Test::Sleeper
 {
 public:
   /// Constructor
-  Test_i (CORBA::ORB_ptr orb);
+  Sleeper_i (CORBA::ORB_ptr orb);
 
   // = The skeleton methods
-  virtual void delay (void);
+  virtual void delay (CORBA::Short sec);
 
   virtual void shutdown (void);
 
 private:
   CORBA::ORB_var orb_;
+
+};
+
+
+/// Implement the Test::Sleeper interface
+class Middle_i : public virtual POA_Test::Middle
+{
+public:
+  /// Constructor
+  Middle_i (CORBA::ORB_ptr orb, Test::Sleeper_ptr sleeper);
+
+  // = The skeleton methods
+  virtual void call_delay (CORBA::Short sec);
+
+  virtual void shutdown (void);
+
+private:
+  CORBA::ORB_var orb_;
+  Test::Sleeper_var sleeper_;
+
+  ACE_Thread_Mutex lock_;
+  ACE_Unbounded_Set<ACE_hthread_t> threads_;
 
 };
 
