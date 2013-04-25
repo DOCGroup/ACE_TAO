97dfe47096c5b4d777e450b2875d88d4a828c52d
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-04-25 21:06:40 +0000

    Thu Apr 25 21:04:51 UTC 2013  Phil Mesnier  <mesnier_p@ociweb.com>

--- trunk/TAO/orbsvcs/tests/ImplRepo/servers_interact_on_startup/run_test.pl
+++ trunk/TAO/orbsvcs/tests/ImplRepo/servers_interact_on_startup/run_test.pl
@@ -18,14 +18,28 @@ my $client_count = 2;
 my $server_reply_delay = 5;
 my $usage = 0;
 
+my $debuglog = "";
+my @srvlogfile = ( "", "", "" );
+my @cltlogfile = ( "", "", "" );
+my $actlogfile = "";
+
 # Ping interval in milliseconds
 my $verification_interval_msecs = 1000;
 
 if ($#ARGV >= 0) {
     for (my $i = 0; $i <= $#ARGV; $i++) {
-	if ($ARGV[$i] eq '-debug') {
+	if ($ARGV[$i] eq "-debug") {
 	    $debug_level = '10';
 	    $imr_debug_level = '10';
+            $i++;
+        }
+        elsif ($ARGV[$i] eq "-debuglog") {
+	    $debug_level = '10';
+	    $imr_debug_level = '10';
+            $debuglog = "-ORBVerboseLogging 1 -ORBLogFile ";
+            @srvlogfile = ( "server1.log", "server2.log", "server3.log" );
+            @cltlogfile = ( "client1.log", "client2.log", "client3.log" );
+            $actlogfile = "imr_act.log";
 	    $i++;
 	}
 	elsif ($ARGV[$i] eq "-server_reply_delay") {
@@ -51,7 +65,7 @@ my $ti =  PerlACE::TestTarget::create_target (++$tgt_num) || die "Create target 
 my @cli;
 
 # Have list indices match client IDs (C1, C2) with first element of list not being used.
-for(my $i = 0; $i <= $client_count; $i++) {
+for(my $i = 0; $i < $client_count; $i++) {
     push (@cli, PerlACE::TestTarget::create_target (++$tgt_num)) || die "Create target $tgt_num failed\n";
 }
 
@@ -61,7 +75,7 @@ my $port = $imr->RandomPort();
 
 my $forward_on_exception_arg = "-ORBForwardOnceOnTransient 1";
 
-my $debug_arg = "-ORBDebugLevel $debug_level";
+my $debug_arg = "-ORBDebugLevel $debug_level" . $debuglog;
 my $imr_debug_arg = "-ORBDebugLevel $imr_debug_level";
 if ($imr_debug_level == 10) {
     $imr_debug_arg = $imr_debug_arg . " -ORBVerboseLogging 1 -ORBLogFile imr_loc.log ";
@@ -91,7 +105,7 @@ my @srv_statusfile;
 my @obj;
 my @srv_server_cmd;
 # Have list indices match server IDs (S1, S2, S3) with first element of list not being used.
-for(my $i = 0; $i <= $servers_count; $i++) {
+for(my $i = 0; $i < $servers_count; $i++) {
     push (@srv, PerlACE::TestTarget::create_target (++$tgt_num)) || die "Create target $tgt_num failed\n";
     push (@obj, $objprefix. "_" . $i);
     push (@srviorfile,  $obj[$i] . ".ior");
@@ -105,8 +119,8 @@ for(my $i = 0; $i <= $servers_count; $i++) {
     push (@srv_server_cmd, $imr->LocalFile ($server_cmd));
 }
 
-for(my $i = 0; $i <= $client_count; $i++) {
-    push (@CLI, $cli[$i]->CreateProcess ("client", "$debug_arg -k file://$srviorfile[1] -n $i $forward_on_exception_arg"));
+for(my $i = 0; $i < $client_count; $i++) {
+    push (@CLI, $cli[$i]->CreateProcess ("client", "$debug_arg $cltlogfile[$i] -k file://$srviorfile[0] -n $i $forward_on_exception_arg"));
 }
 
 sub cleanup_output {
@@ -114,7 +128,7 @@ sub cleanup_output {
     $act->DeleteFile ($imriorfile);
     $ti->DeleteFile ($imriorfile);
     $act->DeleteFile ($actiorfile);
-    for (my $i = 1; $i <= $servers_count; $i++) {
+    for (my $i = 0; $i < $servers_count; $i++) {
 	$srv[$i]->DeleteFile ($srviorfile[$i]);
 	$srv[$i]->DeleteFile ($srvstatusfile[$i]);
     }
@@ -142,7 +156,7 @@ sub register_server_with_activator {
     my $srv_to_invoke_id = shift;
 
     $srv_args =
-	"$debug_arg -orbuseimr 1 $refstyle ".
+	"$debug_arg $srvlogfile[$srv_id] -orbuseimr 1 $refstyle ".
 	"$forward_on_exception_arg ".
 	"-ORBInitRef ImplRepoService=file://$imr_imriorfile -n $srv_id";
 
@@ -180,7 +194,7 @@ sub run_test
 
     $IMR->Arguments ("-o $imr_imriorfile $refstyle -orbendpoint iiop://:$port ".
 		     "$forward_on_exception_arg ".
-		     "-d 1 $imr_debug_arg ".
+		     "-d 2 $imr_debug_arg ".
 		     "-v $verification_interval_msecs");
     print ">>> " . $IMR->CommandLine () . "\n";
     $IMR_status = $IMR->Spawn ();
@@ -209,7 +223,7 @@ sub run_test
         $IMR->Kill (); $IMR->TimedWait (1);
         return 1;
     }
-    for (my $i = 1; $i <= $servers_count; $i++) {
+    for (my $i = 0; $i < $servers_count; $i++) {
 	if ($srv[$i]->PutFile ($imriorfile) == -1) {
 	    print STDERR "ERROR: cannot set file <$srv_imriorfile>\n";
 	    $IMR->Kill (); $IMR->TimedWait (1);
@@ -221,7 +235,7 @@ sub run_test
 
     print_msg ("Start Activator");
 
-    $ACT->Arguments ("$debug_arg -d 1 -o $act_actiorfile -ORBInitRef ImplRepoService=file://$act_imriorfile");
+    $ACT->Arguments ("$debug_arg $actlogfile -d 2 -o $act_actiorfile -ORBInitRef ImplRepoService=file://$act_imriorfile");
     print ">>> " . $ACT->CommandLine () . "\n";
 
     $ACT_status = $ACT->Spawn ();
@@ -240,36 +254,36 @@ sub run_test
 
     print_msg ("Start S3");
 
-    $SRV[3]->Arguments ("$debug_arg -orbuseimr 1 $refstyle -ORBInitRef ImplRepoService=file://$imr_imriorfile ".
-			    "-d $server_reply_delay -n 3");
-    print ">>> " . $SRV[3]->CommandLine () . "\n";
-    $SRV[3]-> Spawn();
-    if ($srv[3]->WaitForFileTimed ($srvstatusfile[3], $srv[3]->ProcessStartWaitInterval()) == -1) {
-        print STDERR "ERROR: cannot find file $srvstatusfile[3]\n";
+    $SRV[2]->Arguments ("$debug_arg $srvlogfile[2] -orbuseimr 1 $refstyle -ORBInitRef ImplRepoService=file://$imr_imriorfile ".
+			    "-d $server_reply_delay -n 2");
+    print ">>> " . $SRV[2]->CommandLine () . "\n";
+    $SRV[2]-> Spawn();
+    if ($srv[2]->WaitForFileTimed ($srvstatusfile[2], $srv[2]->ProcessStartWaitInterval()) == -1) {
+        print STDERR "ERROR: cannot find file $srvstatusfile[2]\n";
         $IMR->Kill (); $IMR->TimedWait (1);
         return 1;
     }
     # Get its IOR so S2 can use to invoke S3
-    run_imr_util("ior $obj[3] -f $srviorfile[3]");
+    run_imr_util("ior $obj[2] -f $srviorfile[2]");
 
     ##### Register S2 with ImR using Activator #####
 
     print_msg ("Register S2 with ImR to start on demand");
 
-    register_server_with_activator(2, 3);
+    register_server_with_activator(1, 2);
 
     ##### Register S1 with ImR using Activator #####
 
     print_msg ("Register S1 with ImR to start on demand");
 
-    register_server_with_activator(1, 2);
+    register_server_with_activator(0, 1);
 
     ##### C1 invokes S1 #####
 
     print_msg ("C1 invokes S1");
 
-    print ">>> " . $CLI[1]->CommandLine () . "\n";
-    $CLI_status = $CLI[1]->Spawn ();
+    print ">>> " . $CLI[0]->CommandLine () . "\n";
+    $CLI_status = $CLI[0]->Spawn ();
     if ($CLI_status != 0) {
 	print STDERR "ERROR: client 1 returned $CLI_status\n";
 	return 1;
@@ -282,8 +296,8 @@ sub run_test
     # Let ping interval pass to ensure another ping will be done.
     sleep ($verification_interval_msecs / 1000 + 1);
 
-    print ">>> " . $CLI[2]->CommandLine () . "\n";
-    $CLI_status = $CLI[2]->Spawn ();
+    print ">>> " . $CLI[1]->CommandLine () . "\n";
+    $CLI_status = $CLI[1]->Spawn ();
     if ($CLI_status != 0) {
 	print STDERR "ERROR: client 2 returned $CLI_status\n";
 	return 1;
@@ -291,7 +305,7 @@ sub run_test
 
     ##### Wait for clients to terminate #####
     print_msg ("Wait for clients to terminate");
-    for (my $i = 1; $i <= $client_count; $i++) {
+    for (my $i = 0; $i < $client_count; $i++) {
 	if ($CLI[$i]->WaitKill ($cli[$i]->ProcessStopWaitInterval () + $server_reply_delay + 60) == -1) {
 	    print STDERR "ERROR: client $i not terminated correctly\n";
 	    $status = 1;
@@ -307,7 +321,7 @@ sub run_test
     }
 
     ##### Shutdown servers #####
-    for (my $i = 1; $i <= $servers_count; $i++ ) {
+    for (my $i = 0; $i < $servers_count; $i++ ) {
         # Shutting down any server object within the server will shutdown the whole server
         run_imr_util ("shutdown $obj[$i]");
 	if ($SRV[$i]->WaitKill ($srv[$i]->ProcessStopWaitInterval ()) == -1) {
