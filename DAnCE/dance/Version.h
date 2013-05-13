65e70ed0e2c34022791958063beff82b339d1c5a
Phil Mesnier <mesnier_p@ociweb.com>
2013-05-13 15:55:41 +0000

    merge from trunk

--- DAnCE/dance/Version.h
+++ DAnCE/dance/Version.h
@@ -5,5 +5,5 @@
 
 #define DAnCE_MAJOR_VERSION 1
 #define DAnCE_MINOR_VERSION 1
-#define DAnCE_BETA_VERSION 8
-#define DAnCE_VERSION "1.1.8"
+#define DAnCE_BETA_VERSION 9
+#define DAnCE_VERSION "1.1.9"
