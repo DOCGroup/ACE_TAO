0c60e947b066ea14ffca575977ae4ec6167a2756
Phil Mesnier <mesnier_p@ociweb.com>
2013-05-19 13:24:54 +0000

    Sun May 19 13:20:04 UTC 2013  Phil Mesnier  <mesnier_p@ociweb.com>

--- TAO/tao/CSD_Framework/CSD_FW_Server_Request_Wrapper.cpp
+++ TAO/tao/CSD_Framework/CSD_FW_Server_Request_Wrapper.cpp
@@ -264,7 +264,7 @@ TAO::CSD::FW_Server_Request_Wrapper::clone (TAO_ServerRequest*& request)
   // ACTION: Primitive data type assignment.
   clone_obj->argument_flag_ = request->argument_flag_;
 
-  //#if TAO_HAS_INTERCEPTORS == 1
+  #if TAO_HAS_INTERCEPTORS == 1
   // TYPE: size_t
   // ACTION: Primitive data type assignment.
   clone_obj->interceptor_count_ = request->interceptor_count_;
@@ -281,7 +281,7 @@ TAO::CSD::FW_Server_Request_Wrapper::clone (TAO_ServerRequest*& request)
   //         Just leave this alone for a clone.
   //
   //clone_obj->result_seq_ = request->result_seq_;
-  //#endif  /* TAO_HAS_INTERCEPTORS == 1 */
+  #endif  /* TAO_HAS_INTERCEPTORS == 1 */
 
   if (clone_obj->transport_ != 0)
     {
