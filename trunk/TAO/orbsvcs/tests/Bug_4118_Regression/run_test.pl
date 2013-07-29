2268b718f886982cf28e01e5e87cf7746b98dbcf
mesnier_p <mesnier_p@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-07-29 20:12:07 +0000

    Mon Jul 29 20:10:32 UTC 2013  Phil Mesnier  <mesnier_p@ociweb.com>
            * orbsvcs/tests/Bug_4118_Regression/README:
            * orbsvcs/tests/Bug_4118_Regression/run_test.pl:
    
              Added a readme file and cleaned up the test script.

--- trunk/TAO/orbsvcs/tests/Bug_4118_Regression/run_test.pl
+++ trunk/TAO/orbsvcs/tests/Bug_4118_Regression/run_test.pl
@@ -5,10 +5,6 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # $Id$
 # -*- perl -*-
 
-# This is a Perl script that runs some Naming Service tests.
-# It runs all the tests that will run with min CORBA.
-# It starts all the servers and clients as necessary.
-
 use lib "$ENV{ACE_ROOT}/bin";
 use PerlACE::TestTarget;
 use Cwd;
@@ -29,6 +25,7 @@ $persistence_dir = "NameService";
 #Files which used by test
 my $test_iorfile = $test->LocalFile ($iorfile);
 
+# clean up from a previous test run, or from this run
 sub cleanup
 {
     $test->DeleteFile($iorfile);
@@ -50,14 +47,10 @@ sub cleanup
 
 sub start_name_server
 {
-    my $args = " -o $test_iorfile -ORBCollocation no -n 5 -ORBListenEndpoints iiop://$hostname:$ns_orb_port -u $persistence_dir";
+    my $args = "-ORBCollocation no -ORBListenEndpoints iiop://$hostname:$ns_orb_port -o $test_iorfile -n 5 -u $persistence_dir";
     my $prog = "$startdir/../../Naming_Service/tao_cosnaming";
 
     $SV = $test->CreateProcess ("$prog", "$args");
-
-    $test->DeleteFile($iorfile);
-
-
     $SV->Spawn ();
 
     if ($test->WaitForFileTimed ($iorfile,
@@ -66,16 +59,12 @@ sub start_name_server
         $SV->Kill (); $SV->TimedWait (1);
         exit 1;
     }
-
-    sleep(5);
 }
 
 sub nsadd
 {
     my $args = "--ns file://$test_iorfile "."@_"." ";
     my $prog = "$startdir/../../../utils/nslist/tao_nsadd";
-
-    print STDERR "command line $args\n";
 
     $CL = $test->CreateProcess ("$prog", "$args");
 
@@ -88,17 +77,11 @@ sub nsadd
 
 }
 
-## The options below have been reordered due to a
-## initialization problem (within the Naming_Service)
-## that has only been seen on Windows XP.
-
 #1. use the tao_nsadd command add a cyclical reference to the root context
 #2. use the tao_nsadd command to add a binding that traverses the cycle
 #3. verify that step 2 does not hang
 #4. clean up old references
 
-
-# Options for all simple tests recognized by the ns_add program.
 @opts = ("--name recurse --ior file://$test_iorfile --ctx",
          "--name recurse/recurse/recurse/dummy --ior file://$test_iorfile");
 
@@ -121,12 +104,6 @@ foreach $o (@opts) {
 
     nsadd ($o);
 
-    ## For some reason, only on Windows XP, we need to
-    ## wait before starting another tao_cosnaming when
-    ## the mmap persistence option is used
-    if ($^O eq "MSWin32") {
-      sleep(1);
-    }
     $test_number++;
 }
 
