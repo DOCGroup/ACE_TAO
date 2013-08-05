4d5bbcb87c140b9466bca2781540081716f301e5
Johnny Willemsen <jwillemsen@remedy.nl>
2013-08-05 14:27:44 +0000

    Mon Aug  5 14:25:16 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/MessengerClient.cpp:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/cacert.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/client.conf:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/client_cert.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/client_key.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/server.conf:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/server_cert.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/server_key.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/MessengerClient.cpp:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/README:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/cacert.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/client.conf:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/client1.conf:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/client_cert.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/client_key.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/server.conf:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/server1.conf:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/server_cert.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/server_key.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/README:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/cacert.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/client.conf:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/client1.conf:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/client_cert.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/client_key.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/server.conf:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/server_cert.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/server_key.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/clientcert.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/clientkey.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/servercert.pem:
            * orbsvcs/DevGuideExamples/Security/ParticipatingApp/serverkey.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/clientcert.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/clientkey.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/servercert.pem:
            * orbsvcs/DevGuideExamples/Security/PolicyControllingApp/serverkey.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/clientcert.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/clientkey.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/servercert.pem:
            * orbsvcs/DevGuideExamples/Security/SecurityUnawareApp/serverkey.pem:
              Certificates where expired, use now the certificates we also
    use
              for the other orbsvcs security tests

--- TAO/orbsvcs/DevGuideExamples/Security/ParticipatingApp/MessengerClient.cpp
+++ TAO/orbsvcs/DevGuideExamples/Security/ParticipatingApp/MessengerClient.cpp
@@ -13,8 +13,8 @@ dynamic SSLIOP_Factory Service_Object *
   TAO_SSLIOP:_make_TAO_SSLIOP_Protocol_Factory()
   "-SSLNoProtection \
    -SSLAuthenticate SERVER_AND_CLIENT \
-   -SSLPrivateKey PEM:serverkey.pem \
-   -SSLCertificate PEM:servercert.pem"
+   -SSLPrivateKey PEM:server_key.pem \
+   -SSLCertificate PEM:server_cert.pem"
 
 static Resource_Factory "-ORBProtocolFactory SSLIOP_Factory"
 ---------------------------------------
@@ -27,8 +27,8 @@ dynamic SSLIOP_Factory Service_Object *
   TAO_SSLIOP:_make_TAO_SSLIOP_Protocol_Factory()
   "-SSLNoProtection  \
    -SSLAuthenticate SERVER \
-   -SSLPrivateKey PEM:clientkey.pem  \
-   -SSLCertificate PEM:clientcert.pem"
+   -SSLPrivateKey PEM:client_key.pem  \
+   -SSLCertificate PEM:client_cert.pem"
 
 static Resource_Factory "-ORBProtocolFactory SSLIOP_Factory"
 ---------------------------------------
