a3710b5f17175eb547d170ed11098d957a931ef2
Johnny Willemsen <jwillemsen@remedy.nl>
2013-08-07 06:52:37 +0000

    Wed Aug  7 06:52:43 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tao/AnyTypeCode/TypeCode_Case_Enum_T.cpp:
              Layout changes

--- TAO/tao/AnyTypeCode/TypeCode_Case_Enum_T.cpp
+++ TAO/tao/AnyTypeCode/TypeCode_Case_Enum_T.cpp
@@ -43,8 +43,7 @@ bool
 TAO::TypeCode::Case_Enum_T<StringType,
                            TypeCodeType>::marshal_label (TAO_OutputCDR & cdr) const
 {
-  return
-    (cdr << this->label_);
+  return (cdr << this->label_);
 }
 
 template <typename StringType,
@@ -52,8 +51,7 @@ template <typename StringType,
 bool
 TAO::TypeCode::Case_Enum_T<StringType,
                            TypeCodeType>::equal_label (::CORBA::ULong index,
-                                                       ::CORBA::TypeCode_ptr tc
-                                                       ) const
+                                                       ::CORBA::TypeCode_ptr tc) const
 {
   CORBA::Any_var any = tc->member_label (index);
   TAO_OutputCDR out_cdr;
