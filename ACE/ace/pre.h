da3167ab0232545d399a0a7ce5675f207273dfb3
Johnny Willemsen <jwillemsen@remedy.nl>
2013-08-09 17:58:02 +0000

    Fri Aug  9 17:57:43 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/post.h:
            * ace/pre.h:
              Disable warning 8127 with Embarcadero C++ BuilderXE4, it is
              given incorrectly and reported as QC 117740.

--- ACE/ace/pre.h
+++ ACE/ace/pre.h
@@ -19,6 +19,11 @@
 # pragma pack (push, 8)
 #elif defined (__BORLANDC__)
 # pragma option push -a8 -b -Ve- -Vx- -w-rvl -w-rch -w-ccc -w-obs -w-aus -w-pia -w-inl -w-sig
+# if (__BORLANDC__ == 0x660)
+// False warning: Function defined with different linkage, reported to
+// Embarcadero as QC 117740
+#  pragma option push -w-8127
+# endif
 # pragma nopushoptwarn
 # pragma nopackwarning
 #endif
