2e241defa535b9ccbf0f7cdd998634444ebcd96d
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-08-09 14:28:12 +0000

    Fri Aug  9 14:27:56 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Compiler_Features_22_DLL.h:
            * tests/Compiler_Features_22_DLL.cpp:
            * tests/Compiler_Features_22_DLL_Export.h:
            * tests/tests.mpc:
              New test for template method specialization combined with
              dllexport

---
+++ trunk/ACE/tests/Compiler_Features_22_DLL.h
@@ -0,0 +1,40 @@
+
+//=============================================================================
+/**
+ *  @file    Compiler_Features_22_DLL.h
+ *
+ *  $Id$
+ *
+ *  Test DLL export with a template method specialization
+ */
+//=============================================================================
+
+#ifndef ACE_TESTS_COMPILER_FEATURES_22_DLL_H
+#define ACE_TESTS_COMPILER_FEATURES_22_DLL_H
+
+#include "test_config.h"
+
+#include "Compiler_Features_22_DLL_Export.h"
+
+class A
+{
+public:
+
+A () {};
+};
+
+typedef A* A_ptr;
+
+template<typename T> inline bool is_nil (T x)
+{
+return x == 0;
+}
+
+template<>
+bool
+COMPILER_FEATURES_22_DLL_Export is_nil<> (A_ptr)
+{
+  return false;
+}
+
+#endif /* ACE_TESTS_COMPILER_FEATURES_22_DLL_H */
