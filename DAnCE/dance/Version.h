393904aef4fb6933f47278af41f4d8accc5b318f
Johnny Willemsen <jwillemsen@remedy.nl>
2013-08-16 06:57:50 +0000

    ChangeLogTag:Fri Aug 16 08:56:17 CEST 2013  Johnny Willemsen  <jwillemsen@remedy.nl>

--- DAnCE/dance/Version.h
+++ DAnCE/dance/Version.h
@@ -5,5 +5,5 @@
 
 #define DAnCE_MAJOR_VERSION 1
 #define DAnCE_MINOR_VERSION 2
-#define DAnCE_BETA_VERSION 0
-#define DAnCE_VERSION "1.2.0"
+#define DAnCE_BETA_VERSION 1
+#define DAnCE_VERSION "1.2.1"
