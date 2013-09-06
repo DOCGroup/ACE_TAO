6aa06f8879aab8f5b3d66a4e91285455a9b8fbc6
Johnny Willemsen <jwillemsen@remedy.nl>
2013-09-06 12:34:50 +0000

    Fri Sep  6 12:35:04 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Compiler_Features_23_Test.cpp:
            * tests/Compiler_Features_24_Test.cpp:
            * tests/run_test.lst:
            * tests/tests.mpc:
              Two new C++11 compiler features tests

---
+++ ACE/tests/Compiler_Features_23_Test.cpp
@@ -0,0 +1,59 @@
+// $Id$
+
+/**
+ * This program checks if the compiler doesn't have a certain bug
+ * that we encountered when testing C++11 features
+ */
+
+#include "test_config.h"
+
+#if defined (ACE_HAS_CPP11)
+
+#include <array>
+#include <string>
+
+std::array<std::string, 2> priority_names =
+{
+  {
+     std::string("NOT USED"),
+     "P_PANIC"
+  }
+};
+
+std::string t (uint32_t prop)
+{
+  uint32_t log = 0;
+
+  for (; prop > 1; ++log)
+    prop >>= 1;
+
+  return priority_names[log];
+}
+
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_23_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("Compiler Feature 23 Test does compile and run.\n")));
+
+  ACE_END_TEST;
+
+  return 0;
+}
+
+#else
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_23_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("No C++11 support enabled\n")));
+
+  ACE_END_TEST;
+  return 0;
+}
+
+#endif
