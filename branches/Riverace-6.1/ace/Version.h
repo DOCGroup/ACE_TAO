7bdfce4e90c2f91d901ad98e5454aad5a0d93fee
shuston <shuston@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2013-09-30 20:28:03 +0000

    ChangeLogTag:Mon Sep 30 16:27:55 EDT 2013  Steve Huston  <shuston@riverace.com>

--- branches/Riverace-6.1/ace/Version.h
+++ branches/Riverace-6.1/ace/Version.h
@@ -6,5 +6,5 @@
 #define ACE_MAJOR_VERSION 6
 #define ACE_MINOR_VERSION 1
 #define ACE_BETA_VERSION 0
-#define ACE_FIX_VERSION "d"
-#define ACE_VERSION "6.1d"
+#define ACE_FIX_VERSION "e"
+#define ACE_VERSION "6.1e"
