a063eef89f4547c0a2ed465323e9b2fccb64bb07
Phil Mesnier <mesnier_p@ociweb.com>
2013-11-08 15:56:06 +0000

    Fri Nov  8 15:33:29 UTC 2013  Phil Mesnier  <mesnier_p@ociweb.com>
            * interop-tests/CdrOutArg/orbix/README:
            * interop-tests/CdrOutArg/orbix/Server_ORBInitializer.h:
            * interop-tests/CdrOutArg/orbix/Server_ORBInitializer.cxx:
            * interop-tests/CdrOutArg/orbix/cxx_demo.mk:
            * interop-tests/CdrOutArg/orbix/demo.mk:
            * interop-tests/CdrOutArg/orbix/makefile:
            * interop-tests/CdrOutArg/orbix/server.cxx:
            * interop-tests/CdrOutArg/orbix/server_interceptor.h:
            * interop-tests/CdrOutArg/orbix/server_interceptor.cxx:
            * interop-tests/CdrOutArg/orbix/test_i.h:
            * interop-tests/CdrOutArg/orbix/test_i.cxx:
              Companion test to be built using Orbix 6.2. *UNSUPPORTED*
              The DOC group can only support the TAO portion of the test.

---
+++ TAO/interop-tests/CdrOutArg/orbix/demo.mk
@@ -0,0 +1,21 @@
+#$Id$
+#################################################################################
+#
+#        Copyright (c) 1993-2009 Progress Software Corporation and/or
+#                         its affiliates or subsidiaries.
+#                             All Rights Reserved.
+#
+#################################################################################
+
+
+RM=				rm -f
+RMDIR=				rm -rf
+
+IT_PRODUCT_VER=			6.3
+
+ART_IDL_INCLUDE_DIR=    	$(IT_PRODUCT_DIR)/asp/$(IT_PRODUCT_VER)/idl
+
+ART_BIN_DIR=            	$(IT_PRODUCT_DIR)/asp/$(IT_PRODUCT_VER)/bin
+
+IDL=                    	$(ART_BIN_DIR)/idl
+ITADMIN=			$(ART_BIN_DIR)/itadmin
