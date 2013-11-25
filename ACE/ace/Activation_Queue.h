e7f34fcd79dcb12b0de6c452aea90e336e52a02c
Johnny Willemsen <jwillemsen@remedy.nl>
2013-11-25 10:48:49 +0000

    Mon Nov 25 10:49:19 UTC 2013  Johnny Willemsen  <jwillemsen@remedy.nl>
            * ace/Activation_Queue.h:
            * ace/Compression/Compressor.h:
              Use ACE_UNIMPLEMENTED_FUNC
    
            * ace/Caching_Utility_T.h:
              Doxygen improvements

--- ACE/ace/Activation_Queue.h
+++ ACE/ace/Activation_Queue.h
@@ -49,7 +49,7 @@ class ACE_Method_Request;
  *
  * @sa ACE_Method_Request
  */
-class ACE_Export ACE_Activation_Queue : private ACE_Copy_Disabled
+class ACE_Export ACE_Activation_Queue
 {
 public:
   /// Constructor.
@@ -156,6 +156,8 @@ private:
   /// Allocation strategy of the message blocks.
   ACE_Allocator *data_block_allocator_;
 
+  ACE_UNIMPLEMENTED_FUNC (void operator= (const ACE_Activation_Queue &))
+  ACE_UNIMPLEMENTED_FUNC (ACE_Activation_Queue (const ACE_Activation_Queue &))
 };
 
 ACE_END_VERSIONED_NAMESPACE_DECL
