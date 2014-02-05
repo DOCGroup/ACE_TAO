8f304d01725904b8c963bb674ab458dae8ac024d
Phil Mesnier <mesnier_p@ociweb.com>
2014-02-05 21:05:42 +0000

    Wed Feb  5 20:37:24 UTC 2014  Phil Mesnier  <mesnier_p@ociweb.com>
            * TAO_IDL/be/be_visitor_valuetype/field_cdr_cs.cpp:
              Apply the same fix for forwarded interfaces as applied to forwarded
              valuetypes on Jan 23 2014.
    
            * orbsvcs/tests/ImplRepo/Bug_4152_Regression/run_test.pl:
              Fix a typo.

--- TAO/orbsvcs/tests/ImplRepo/Bug_4152_Regression/run_test.pl
+++ TAO/orbsvcs/tests/ImplRepo/Bug_4152_Regression/run_test.pl
@@ -225,7 +225,7 @@ sub validate_servers
 
     if ($count != 2) {
         print STDERR "ERROR: expected 2 server pids, got $count\n";
-        for ($i = 0; i < $count; $i++) {
+        for ($i = 0; $i < $count; $i++) {
             kill $pid[$i];
         }
     }
