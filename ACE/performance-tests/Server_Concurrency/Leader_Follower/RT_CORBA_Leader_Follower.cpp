ab55b0ae4d1c930b18891534027cf562a70cde35
Johnny Willemsen <jwillemsen@remedy.nl>
2014-02-24 09:40:09 +0000

    Zapped empty lines

--- ACE/performance-tests/Server_Concurrency/Leader_Follower/RT_CORBA_Leader_Follower.cpp
+++ ACE/performance-tests/Server_Concurrency/Leader_Follower/RT_CORBA_Leader_Follower.cpp
@@ -371,7 +371,6 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
               "(%P|%t) Throughput is [%f]\n",
               1000000000/ elapsed_time_per_invocation));
 
-
   for (i = 0; i < number_of_threads; ++i)
     {
       ACE_DEBUG ((LM_DEBUG,
@@ -384,9 +383,6 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 
   return result;
 }
-
-
-
 
 #else /*if defined (ACE_HAS_THREADS)*/
 
