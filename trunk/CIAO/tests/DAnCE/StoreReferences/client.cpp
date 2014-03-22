2056026e5dcc83229c847ccca21328705504578d
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2014-03-22 13:38:49 +0000

    Sat Mar 22 13:39:42 UTC 2014  Johnny Willemsen  <jwillemsen@remedy.nl>
            * bin/ciao_tests.lst:
            * tests/DAnCE/StoreReferences/DeploymentPlan.cdp:
            * tests/DAnCE/StoreReferences/StoreReferences.idl:
            * tests/DAnCE/StoreReferences/StoreReferences.mpc:
            * tests/DAnCE/StoreReferences/StoreReferences_exec.h:
            * tests/DAnCE/StoreReferences/StoreReferences_exec.cpp:
            * tests/DAnCE/StoreReferences/client.cpp:
            * tests/DAnCE/StoreReferences/run_test.pl:
              Integrate unit test for bugzilla 4106, thanks to 
              Michael Frommberger <michael dot frommberger at gmx dot net>
              for creating this

---
+++ trunk/CIAO/tests/DAnCE/StoreReferences/client.cpp
@@ -0,0 +1,56 @@
+// $Id$
+
+#include "orbsvcs/CosNamingC.h"
+
+int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+{
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Starting StoreReferences test\n")));
+
+    try
+    {
+        // Initialize orb
+        CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
+        CORBA::Object_var obj;
+        CosNaming::NamingContextExt_var ns;
+
+        try
+        {
+            ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Resolving name server\n")));
+
+            // retrieve ns
+            obj = orb->resolve_initial_references ("NameService");
+            ns = CosNaming::NamingContextExt::_narrow (obj.in ());
+            if (true == CORBA::is_nil (obj.in ()))
+                ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Unable to resolve name server\n")));
+        }
+        catch (const CORBA::Exception& ex)
+        {
+            ex._tao_print_exception (ACE_TEXT ("Unable to resolve name server"));
+            return -1;
+        }
+
+        try
+        {
+            ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("Resolving deployed component\n")));
+
+            // retrieve deployed component
+            obj = ns->resolve_str ("TestComp");
+            if (true == CORBA::is_nil (obj.in ()))
+                ACE_DEBUG ((LM_ERROR, ACE_TEXT ("Unable to resolve deployed component\n")));
+
+        }
+        catch (const CORBA::Exception& ex)
+        {
+            ex._tao_print_exception (ACE_TEXT ("Unable to resolve deployed component"));
+            return -1;
+        }
+    }
+    catch (const CORBA::Exception& ex)
+    {
+        ex._tao_print_exception (ACE_TEXT ("Error while running test"));
+        return -1;
+    }
+
+    ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("StoreReferences test OK\n")));
+    return 0;
+}
