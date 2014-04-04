66ac9e27733972cea981261e55b03d75e16439cf
johnnyw <johnnyw@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2014-04-04 12:31:33 +0000

    Fri Apr  4 12:32:57 UTC 2014  Johnny Willemsen  <jwillemsen@remedy.nl>
            * tests/Compiler_Features_34_Test.cpp:
            * tests/run_test.lst:
            * tests/tests.mpc:
              New C++ compiler feature test

---
+++ trunk/ACE/tests/Compiler_Features_34_Test.cpp
@@ -0,0 +1,48 @@
+// $Id$
+
+/**
+ * This program checks if the compiler doesn't have a certain bug
+ * that we encountered when testing C++11 features
+ */
+
+#include "test_config.h"
+
+#if defined (ACE_HAS_CPP11)
+
+class B
+{
+public:
+private:
+    B& operator= (B&& x) = delete;
+};
+
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_34_Test"));
+
+  B a;
+  ACE_UNUSED_ARG (a);
+  
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("C++11 support ok\n")));
+
+  ACE_END_TEST;
+
+  return 0;
+}
+
+#else
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_34_Test"));
+
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("No C++11 support enabled\n")));
+
+  ACE_END_TEST;
+  return 0;
+}
+
+#endif
