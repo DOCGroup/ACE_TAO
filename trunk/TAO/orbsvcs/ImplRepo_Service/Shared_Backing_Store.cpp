39efab4197f13559876c0166514cb931d86ca103
olli <olli@ae88bc3d-4319-0410-8dbf-d08b4c9d3795>
2014-09-26 10:18:17 +0000

    ChangeLogTag: Fri Sep 26 10:16:30 UTC 2014  Olli Savia  <ops@iki.fi>

--- trunk/TAO/orbsvcs/ImplRepo_Service/Shared_Backing_Store.cpp
+++ trunk/TAO/orbsvcs/ImplRepo_Service/Shared_Backing_Store.cpp
@@ -1069,7 +1069,7 @@ Shared_Backing_Store::load_server (Server_Info *info,
       si->server = ImplementationRepository::ServerObject::_narrow (obj.in ());
       this->opts_.pinger ()->add_server (info->key_name_.c_str (),
                                          this->opts_.ping_external (),
-                                         si->server);
+                                         si->server.in ());
     }
 
 }
