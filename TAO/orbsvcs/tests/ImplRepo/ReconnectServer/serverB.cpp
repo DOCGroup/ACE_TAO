5460c7f6d4a069f918a8f6ccfd5afba1957fee36
Phil Mesnier <mesnier_p@ociweb.com>
2014-10-10 15:48:44 +0000

    Fri Oct 10 14:27:50 UTC 2014  Phil Mesnier  <mesnier_p@ociweb.com>
            * orbsvcs/tests/ImplRepo/ReconnectServer/ReconnectServer.mpc:
            * orbsvcs/tests/ImplRepo/ReconnectServer/serverB.cpp:
              Revert the serverB changes from Fri Oct  3 19:54:01 UTC 2014
              as it seems to have broken the forwardonce test.

--- TAO/orbsvcs/tests/ImplRepo/ReconnectServer/serverB.cpp
+++ TAO/orbsvcs/tests/ImplRepo/ReconnectServer/serverB.cpp
@@ -1,5 +1,6 @@
 // $Id$
 #include "test_i.h"
+#include "ORB_Task.h"
 #include "tao/ImR_Client/ImR_Client.h"
 #include <ace/Task.h>
 #include <ace/Get_Opt.h>
@@ -52,6 +53,10 @@ ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 
     CORBA::Object_var object =
       orb->resolve_initial_references ("RootPOA");
+
+    ORB_Task worker (orb.in ());
+    worker.activate (THR_NEW_LWP | THR_JOINABLE,
+                      1);
 
     PortableServer::POA_var rootPOA =
       PortableServer::POA::_narrow (object.in ());
@@ -121,7 +126,7 @@ ACE_TMAIN(int argc, ACE_TCHAR *argv[])
     ACE_OS::fprintf (output_file, "%s", ior.in ());
     ACE_OS::fclose (output_file);
 
-    orb->run ();
+    worker.wait ();
 
     rootPOA->destroy (1, 1);
     orb->destroy ();
