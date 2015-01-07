5b5bc8e425c6adbd9f38a4c789c9ab343ddfc8cb
Johnny Willemsen <jwillemsen@remedy.nl>
2015-01-07 15:49:39 +0100

    Initial commit of script to zap all old svn Id lines     * ACE/bin/zap_svn_id.pl:       Added.

---
+++ ACE/bin/zap_svn_id.pl
@@ -0,0 +1,17 @@
+eval '(exit $?0)' && eval 'exec perl -pi -S $0 ${1+"$@"}'
+    & eval 'exec perl -0 -S $0 $argv:q'
+    if 0;
+
+# $Id$
+#
+# You may want to run the "find" command with this script, which maybe
+# something like this:
+#
+# find . -type f \( -name "*.C" -o -name "*.cc" -o -name "*.c" -o -name "*.cpp" \) -print | xargs $ACE_ROOT/bin/zap_svn_id.pl
+
+# The first three lines above let this script run without specifying the
+# full path to perl, as long as it is in the user's PATH.
+# Taken from perlrun man page.
+
+#undef $/; $s = <>; $s =~ s/ \*\n \* \$Id\$\n \*\n/ \*\n/m; print $s
+BEGIN{undef $/;} s/ \*\n \* \$Id\$\n \*\n/ \*\n/smg
