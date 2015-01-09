db578e352ed55e43c6b0f76f33f3e5c34b17eeaa
Steve Huston <shuston@riverace.com>
2015-01-09 11:29:00 -0500

    Properly detect lack of TLS versions

--- ACE/ace/SSL/SSL_Context.cpp
+++ ACE/ace/SSL/SSL_Context.cpp
@@ -273,7 +273,7 @@ ACE_SSL_Context::set_mode (int mode)
     case ACE_SSL_Context::TLSv1:
       method = ::TLSv1_method ();
       break;
-#ifdef TLS1_1_VERSION
+#if defined(TLS1_1_VERSION) && (TLS_MAX_VERSION >= TLS1_1_VERSION)
     case ACE_SSL_Context::TLSv1_1_client:
       method = ::TLSv1_1_client_method ();
       break;
@@ -284,7 +284,7 @@ ACE_SSL_Context::set_mode (int mode)
       method = ::TLSv1_1_method ();
       break;
 #endif
-#ifdef TLS1_2_VERSION
+#if defined(TLS1_2_VERSION) && (TLS_MAX_VERSION >= TLS1_2_VERSION)
     case ACE_SSL_Context::TLSv1_2_client:
       method = ::TLSv1_2_client_method ();
       break;
