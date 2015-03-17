57c6f367640694b185db7f836396319730653334
Johnny Willemsen <jwillemsen@remedy.nl>
2015-03-17 08:58:50 +0100

    Layout changes

--- TAO/orbsvcs/tests/ImplRepo/manual_start/server.cpp
+++ TAO/orbsvcs/tests/ImplRepo/manual_start/server.cpp
@@ -64,34 +64,34 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
       PortableServer::POAManager_var mgr = root_poa->the_POAManager ();
       PortableServer::POA_var test_poa = createPOA (root_poa.in(), poa_name );
 
-    Test_i *impl = new Test_i ();
-    PortableServer::Servant_var<Test_i> test_servant = impl;
+      Test_i *impl = new Test_i ();
+      PortableServer::Servant_var<Test_i> test_servant = impl;
 
-    PortableServer::ObjectId_var object_id =
-      PortableServer::string_to_ObjectId ("test_object");
+      PortableServer::ObjectId_var object_id =
+        PortableServer::string_to_ObjectId ("test_object");
 
-    test_poa->activate_object_with_id (object_id.in(), test_servant.in());
+      test_poa->activate_object_with_id (object_id.in(), test_servant.in());
 
-    obj = test_poa->id_to_reference (object_id.in ());
-    CORBA::String_var test_ior = orb->object_to_string (obj.in ());
-    FILE *f = ACE_OS::fopen (ior_file_name, ACE_TEXT ("w"));
-    ACE_OS::fprintf (f, "%s", test_ior.in ());
-    ACE_OS::fclose (f);
+      obj = test_poa->id_to_reference (object_id.in ());
+      CORBA::String_var test_ior = orb->object_to_string (obj.in ());
+      FILE *f = ACE_OS::fopen (ior_file_name, ACE_TEXT ("w"));
+      ACE_OS::fprintf (f, "%s", test_ior.in ());
+      ACE_OS::fclose (f);
 
-    mgr->activate();
+      mgr->activate();
 
-    ACE_DEBUG ((LM_DEBUG,
-                ACE_TEXT ("(%P|%t) Started Server <%s>\n"),
-                poa_name));
+      ACE_DEBUG ((LM_DEBUG,
+                  ACE_TEXT ("(%P|%t) Started Server <%s>\n"),
+                  poa_name));
 
-    ACE_Time_Value tv (1,0);
-    orb->run (tv);
-    root_poa->destroy(1,1);
-    orb->destroy();
-    ACE_DEBUG ((LM_DEBUG,
-                ACE_TEXT ("(%P|%t) Server <%s> orb destroyed\n"),
-                poa_name));
-  }
+      ACE_Time_Value tv (1,0);
+      orb->run (tv);
+      root_poa->destroy(1,1);
+      orb->destroy();
+      ACE_DEBUG ((LM_DEBUG,
+                  ACE_TEXT ("(%P|%t) Server <%s> orb destroyed\n"),
+                  poa_name));
+    }
   catch(const CORBA::Exception& ex) {
     ex._tao_print_exception (ACE_TEXT ("Server main()"));
     return 1;
