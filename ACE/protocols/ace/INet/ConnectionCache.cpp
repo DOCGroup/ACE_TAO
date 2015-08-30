8c7ee87b771f36340aedb591b9db5aad10789394
Martin Corino <mcorino@remedy.nl>
2015-08-30 11:44:19 +0200

    Plug a hole. Add missing dtor to clean up.
        * ACE/protocols/ace/INet/ConnectionCache.cpp:
        * ACE/protocols/ace/INet/ConnectionCache.h:

--- ACE/protocols/ace/INet/ConnectionCache.cpp
+++ ACE/protocols/ace/INet/ConnectionCache.cpp
@@ -40,6 +40,15 @@ namespace ACE
         *this = cachekey;
       }
 
+    ConnectionCacheKey::~ConnectionCacheKey ()
+      {
+        if (this->key_ != 0 && this->delete_key_)
+          {
+            delete this->key_;
+            this->delete_key_ = false;
+          }
+      }
+
     ConnectionCacheKey& ConnectionCacheKey::operator =(const ConnectionCacheKey& cachekey)
       {
         if (this != &cachekey)
