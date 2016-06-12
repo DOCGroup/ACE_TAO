ffa965fe198d0dd2a833ba27a3ada21f26b55f98
bjovan@gmail.com <bjovan@gmail.com>
2016-06-12 22:32:16 +0200

    Travis CI errors fix

--- TAO/tests/HTTP_handler_bugs/HTTP_handler_bugs.cpp
+++ TAO/tests/HTTP_handler_bugs/HTTP_handler_bugs.cpp
@@ -14,14 +14,14 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[]) {
   try {
     CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
 
-		CORBA::Object_var obj;
-		obj=orb->string_to_object("corbaloc:iiop:127.0.0.1:" HTTP_SERVER_PORT "/GenericService");
+    CORBA::Object_var obj;
+    obj=orb->string_to_object("corbaloc:iiop:127.0.0.1:" HTTP_SERVER_PORT "/GenericService");
 
     // First test is for bug where IOR string returned is malformed.
     // HTTP_handler returns IOR string in list of "ACE_Message_Block"s which are later concatenated as C strings.
     // Because "ACE_Message_Block"s are not zero terminated, a lot of garbage is picked up after end of each "ACE_Message_Block".
     // This is why IOR returned is malformed and fails processing.
-		
+
     try {
       CORBA::Object_var tmp = orb->string_to_object("http://127.0.0.1:" HTTP_SERVER_PORT "/ior_with_http_response_headers");
     }
@@ -37,10 +37,10 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[]) {
 
     try {
       CORBA::Object_var tmp = orb->string_to_object("http://127.0.0.1:" HTTP_SERVER_PORT "/ior_without_http_response_headers_zero_terminated");
-			if (tmp==NULL) {
-				ACE_OS::puts("Test 2 failed: Object is NULL.");
-				ret=2;
-			}
+      if (tmp==0) {
+        ACE_OS::puts("Test 2 failed: Object is NULL.");
+        ret=2;
+      }
     }
     catch (const CORBA::Exception& ex) {
       // Test failed
--- TAO/tests/HTTP_handler_bugs/http_server.pl
+++ TAO/tests/HTTP_handler_bugs/http_server.pl
@@ -8,36 +8,36 @@ else { $mswin=0; }
 
 
 {
-	package MyWebServer;
-	use HTTP::Server::Simple::CGI;
-	use base qw(HTTP::Server::Simple::CGI);
-	
-	# corbaloc:iiop:127.0.0.1:12345/GenericService
-	my $IOR="IOR:010000000100000000000000010000000000000026000000010100cd0a0000003132372e302e302e310039300e00000047656e6572696353657276696365";
+  package MyWebServer;
+  use HTTP::Server::Simple::CGI;
+  use base qw(HTTP::Server::Simple::CGI);
 
-	sub handle_request {
-		my $self = shift;
-		my $cgi  = shift;
-		my $path = $cgi->path_info();
-		if ($path eq "/ior_with_http_response_headers") {
-			# Normal response is sent, we want to check for bug where IOR string is not zero terminated
-			print "HTTP/1.0 200 OK\r\n";
-			print $cgi->header,
-						$IOR;
-		}
-		elsif ($path eq "/ior_without_http_response_headers_zero_terminated") {
-			# Only IOR string is sent as a response, without any HTTP headers.
-			# We zero terminate IOR to avoid first non-zero-termination bug
-			print $IOR.pack("C",0);
-		}		
-		else {
-			print "HTTP/1.0 404 Not found\r\n";
-			print $cgi->header,
-						$cgi->start_html('Not found'),
-						$cgi->h1('Not found'),
-						$cgi->end_html;
-		}
-	}
+  # corbaloc:iiop:127.0.0.1:12345/GenericService
+  my $IOR="IOR:010000000100000000000000010000000000000026000000010100cd0a0000003132372e302e302e310039300e00000047656e6572696353657276696365";
+
+  sub handle_request {
+    my $self = shift;
+    my $cgi  = shift;
+    my $path = $cgi->path_info();
+    if ($path eq "/ior_with_http_response_headers") {
+      # Normal response is sent, we want to check for bug where IOR string is not zero terminated
+      print "HTTP/1.0 200 OK\r\n";
+      print $cgi->header,
+            $IOR;
+    }
+    elsif ($path eq "/ior_without_http_response_headers_zero_terminated") {
+      # Only IOR string is sent as a response, without any HTTP headers.
+      # We zero terminate IOR to avoid first non-zero-termination bug
+      print $IOR.pack("C",0);
+    }
+    else {
+      print "HTTP/1.0 404 Not found\r\n";
+      print $cgi->header,
+            $cgi->start_html('Not found'),
+            $cgi->h1('Not found'),
+            $cgi->end_html;
+    }
+  }
 }
 
 # Prevent printing to STDOUT of banner from HTTP::Server::Simple
@@ -47,17 +47,17 @@ open STDOUT, '>>', '/dev/null';
 # $pid returned here is wrong on Windows
 my $pid = MyWebServer->new(12345)->background();
 
-sleep 300; 	# 	Sleep 300 seconds, that is enough for test to finish
+sleep 300;  #   Sleep 300 seconds, that is enough for test to finish
 
-if($mswin) {		# Use Windows stuff only on Windows
-	require Win32::Process;
-	Win32::Process->import();
-	my $exitcode;
-	my $mypid=Win32::Process::GetCurrentProcessID();
-	Win32::Process::KillProcess($mypid, $exitcode); 	# Commit suicide
+if($mswin) {    # Use Windows stuff only on Windows
+  require Win32::Process;
+  Win32::Process->import();
+  my $exitcode;
+  my $mypid=Win32::Process::GetCurrentProcessID();
+  Win32::Process::KillProcess($mypid, $exitcode);   # Commit suicide
 }
 else {
-	kill $$; 	# Commit suicide
+  kill $$;  # Commit suicide
 }
 
 
