6fa996c9cd813e4be1c2baffb26c002d3b3cc3b7
Steve Huston <shuston@riverace.com>
2017-03-30 13:18:10 -0400

    Fix ability to request IPv4 address from ACE_INET_Addr::set

--- ACE/tests/INET_Addr_Test_IPV6.cpp
+++ ACE/tests/INET_Addr_Test_IPV6.cpp
@@ -56,11 +56,11 @@ static bool check_both_families()
 {
   bool good = true;
   ACE_INET_Addr a;
-  if (-1 == a.set(ACE_TEXT (""), ACE_TEXT ("facebook.com"), ACE_TEXT ("tcp")))
+  if (-1 == a.set(ACE_TEXT (""), ACE_TEXT ("www.google.com"), ACE_TEXT ("tcp")))
     {
       ACE_ERROR ((LM_ERROR,
                   ACE_TEXT ("%p\n"),
-                  ACE_TEXT ("both test, facebook.com")));
+                  ACE_TEXT ("both test 4, www.google.com")));
       good = false;
     }
   else
@@ -77,6 +77,27 @@ static bool check_both_families()
         }
     }
 
+  ACE_INET_Addr b;
+  if (-1 == b.set(ACE_TEXT(""), ACE_TEXT("www.google.com"), ACE_TEXT("tcp6")))
+  {
+	  ACE_ERROR((LM_ERROR,
+		  ACE_TEXT("%p\n"),
+		  ACE_TEXT("both test 6, www.google.com")));
+	  good = false;
+  }
+  else
+  {
+	  ACE_TCHAR str[1000];
+	  b.addr_to_string(str, 1000, 1);
+	  ACE_DEBUG((LM_DEBUG, ACE_TEXT("got type %d, addr %s\n"), b.get_type(), str));
+	  // Should have selected IPv6.
+	  if (b.get_type() != AF_INET6)
+	  {
+		  ACE_ERROR((LM_ERROR,
+			  ACE_TEXT("Wrong address family, expecting IPv6\n")));
+		  good = false;
+	  }
+  }
   return good;
 }
 
