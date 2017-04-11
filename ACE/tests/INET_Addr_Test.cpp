13fb5d25bc7c0b3036ab2bb2cacea048914135ae
Huang-Ming Huang <huangming.huang@gmail.com>
2017-04-11 09:20:56 -0500

    Revert PR #394 #385 #382 #397
    This revert commits 020cfd8d8ffa71ec6169287dc2e98cc0d4f7e815 db516618eee937974d9e902864ad0165320d85d8 76dcc3fed2faf5c2148df8adb09ef3bc7b11b440 6dd56850da60fde290f39d313397d11cbae47df2 8eb07a10571eb342755f4036424b0724f9eb407c f5c4d13041422d8f713c150d480e09fadccecf70

--- ACE/tests/INET_Addr_Test.cpp
+++ ACE/tests/INET_Addr_Test.cpp
@@ -387,23 +387,6 @@ int run_main (int, ACE_TCHAR *[])
 #if defined (ACE_HAS_IPV6)
   if (ACE::ipv6_enabled ())
     {
-      {
-         ACE_INET_Addr addr;
-         int old_type = addr.get_type();
-         if (addr.set(12345, ACE_TEXT ("localhost")) == 0) {
-           if (addr.get_type() != old_type) {
-             ACE_ERROR ((LM_ERROR,
-                         ACE_TEXT ("IPv6 set failed: addr.set(12345, \"localhost\"), old addr.type() = %d, new addr_type()= %d\n"),
-                         old_type,
-                         addr.get_type ()));
-             status = 1;
-           }
-         }
-         else {
-           ACE_DEBUG ((LM_DEBUG, ACE_TEXT ("IPv6 set failed: addr.set(12345, \"localhost\") returns nonzero\n")));
-         }
-      }
-
       const char *ipv6_addresses[] = {
         "1080::8:800:200c:417a", // unicast address
         "ff01::101",             // multicast address
