2aeaf51d0efe793cf67281262260b906a5b0f21c
Johnny Willemsen <jwillemsen@remedy.nl>
2018-06-23 11:41:51 +0200

    Make use of std::atomic for the refcount when we have C++11 enabled
        * TAO/tao/PortableServer/Servant_Base.cpp:
        * TAO/tao/PortableServer/Servant_Base.h:

--- TAO/tao/PortableServer/Servant_Base.cpp
+++ TAO/tao/PortableServer/Servant_Base.cpp
@@ -672,7 +672,11 @@ TAO_ServantBase::_remove_ref (void)
 CORBA::ULong
 TAO_ServantBase::_refcount_value (void) const
 {
+#if defined (ACE_HAS_CPP11)
+  return this->ref_count_;
+#else
   return this->ref_count_.value ();
+#endif /* ACE_HAS_CPP11 */
 }
 
 void
