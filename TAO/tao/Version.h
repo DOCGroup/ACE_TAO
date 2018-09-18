2057858a56b3e489fdf3b8a8e2dabd8541e4b82d
Johnny Willemsen <jwillemsen@remedy.nl>
2018-09-18 09:25:52 +0200

    ACE+TAO-6_5_2

--- TAO/tao/Version.h
+++ TAO/tao/Version.h
@@ -4,9 +4,9 @@
 
 #define TAO_MAJOR_VERSION 2
 #define TAO_MINOR_VERSION 5
-#define TAO_MICRO_VERSION 1
-#define TAO_BETA_VERSION 1
-#define TAO_VERSION "2.5.1"
-#define TAO_VERSION_CODE 132353
+#define TAO_MICRO_VERSION 2
+#define TAO_BETA_VERSION 2
+#define TAO_VERSION "2.5.2"
+#define TAO_VERSION_CODE 132354
 #define TAO_MAKE_VERSION_CODE(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
