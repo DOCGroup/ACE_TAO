7d6b33e807e9d75a2d28248d0f69b143a18bddc3
Johnny Willemsen <jwillemsen@remedy.nl>
2018-10-30 09:51:53 +0100

    New unit test for bugzilla 4213

---
+++ TAO/tests/Bug_4213_Regression/Hello.h
@@ -0,0 +1,28 @@
+
+#ifndef HELLO_H
+#define HELLO_H
+#include /**/ "ace/pre.h"
+
+#include "TestS.h"
+
+/// Implement the Test::Hello interface
+class Hello
+  : public virtual POA_Test::Hello
+{
+public:
+  /// Constructor
+  Hello (CORBA::ORB_ptr orb);
+
+  // = The skeleton methods
+  virtual char * get_string (void);
+
+  virtual void shutdown (void);
+
+private:
+  /// Use an ORB reference to convert strings to objects and shutdown
+  /// the application.
+  CORBA::ORB_var orb_;
+};
+
+#include /**/ "ace/post.h"
+#endif /* HELLO_H */
