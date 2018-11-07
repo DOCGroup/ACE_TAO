1f110c0861392f44ff76335cd3a7ac64d9c4fdb3
Johnny Willemsen <jwillemsen@remedy.nl>
2018-11-07 12:47:42 +0100

    Only define ACE_HAS_SEMUN when we have API >= 12 and at least NDK 15, if not, we just not define it
        * ACE/ace/config-android.h:

--- ACE/ace/config-android.h
+++ ACE/ace/config-android.h
@@ -90,11 +90,8 @@
 
 // semun was added to sys/sem.h in r15
 #if __ANDROID_API__ >= 21
-#  define ACE_HAS_SEMUN
-#  if ACE_ANDROID_NDK_LESS_THAN(15, 0)
-#    error \
-This combination of Android NDK < r15 and API >= 21 is not supported. \
-There are conflicting versions of semun between ACE and linux/sem.h.
+#  if ACE_ANDROID_NDK_AT_LEAST(15, 0)
+#   define ACE_HAS_SEMUN
 #  endif
 #endif
 
