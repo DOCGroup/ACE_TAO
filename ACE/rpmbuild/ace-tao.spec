5e0a46b39218748b39d177c321eef82713955781
Johnny Willemsen <jwillemsen@remedy.nl>
2019-04-08 08:25:32 +0200

    ACE+TAO-6_5_5

--- ACE/rpmbuild/ace-tao.spec
+++ ACE/rpmbuild/ace-tao.spec
@@ -1,6 +1,6 @@
 # Set the version number here.
-%define ACEVER  6.5.4
-%define TAOVER  2.5.4
+%define ACEVER  6.5.5
+%define TAOVER  2.5.5
 
 # Conditional build
 # Default values are
--- TAO/tao/Version.h
+++ TAO/tao/Version.h
@@ -4,9 +4,9 @@
 
 #define TAO_MAJOR_VERSION 2
 #define TAO_MINOR_VERSION 5
-#define TAO_MICRO_VERSION 4
-#define TAO_BETA_VERSION 4
-#define TAO_VERSION "2.5.4"
-#define TAO_VERSION_CODE 132356
+#define TAO_MICRO_VERSION 5
+#define TAO_BETA_VERSION 5
+#define TAO_VERSION "2.5.5"
+#define TAO_VERSION_CODE 132357
 #define TAO_MAKE_VERSION_CODE(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
