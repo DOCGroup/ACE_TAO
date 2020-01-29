0f723d9f5e8a77fa0b80198cc0523295c730ca01
Martin Corino <mcorino@remedy.nl>
2020-01-29 10:44:17 +0100

    Added regression test for transient error handling with deferred DII requests.
        * TAO/tests/DII_Deferred_Transient/DII_Deferred_Transient.mpc:
        * TAO/tests/DII_Deferred_Transient/Hello.cpp:
        * TAO/tests/DII_Deferred_Transient/Hello.h:
        * TAO/tests/DII_Deferred_Transient/README:
        * TAO/tests/DII_Deferred_Transient/Test.idl:
        * TAO/tests/DII_Deferred_Transient/client.cpp:
        * TAO/tests/DII_Deferred_Transient/run_test.pl:
        * TAO/tests/DII_Deferred_Transient/server.cpp:
          Added.
    
        * TAO/bin/tao_orb_tests.lst:

---
+++ TAO/tests/DII_Deferred_Transient/Hello.cpp
@@ -0,0 +1,20 @@
+// -*- C++ -*-
+#include "Hello.h"
+#include "ace/OS_NS_unistd.h"
+
+Hello::Hello ()
+{
+}
+
+void
+Hello::crash ()
+{
+  ACE::terminate_process (ACE_OS::getpid ());
+}
+
+char *
+Hello::get_string ()
+{
+//  ACE_DEBUG ((LM_DEBUG, "(%P|%t) - Hello::get_string called\n"));
+  return CORBA::string_dup ("Hello there!");
+}
