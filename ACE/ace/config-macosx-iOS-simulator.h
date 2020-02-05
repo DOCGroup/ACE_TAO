b40e6033e033d10b1f59b7627467ed9f12ed5970
Chip Jones <jonesc@objectcomputing.com>
2020-02-05 16:13:41 -0600

    Use single config file for iOS builds. Update build flags for iOS hardware.

--- ACE/ace/config-macosx-iOS-simulator.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
-#define ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H
-
-#define ACE_HAS_IPHONE
-#define ACE_LACKS_SYSTEM
-
-#include "ace/config-macosx-mojave.h"
-
-#endif /* ACE_CONFIG_MACOSX_IPHONE_SIMULATOR_H */
