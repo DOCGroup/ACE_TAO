502c8866973364f8fc78e4b9d39d8cc8f1521700
Knut P <knut@altuma.no>
2020-02-28 13:03:21 +0100

    Update TAO/TAO_IDL/util/utl_global.cpp
    Co-Authored-By: Fred Hornsey <fred@hornsey.us>

--- TAO/TAO_IDL/util/utl_global.cpp
+++ TAO/TAO_IDL/util/utl_global.cpp
@@ -1939,7 +1939,7 @@ namespace
         : old_filename_ (idl_global->filename ()),
           old_lineno_ (idl_global->lineno ()),
           old_idl_src_file_(idl_global->idl_src_file ()),
-          disable_output_(the_disable_output),
+          disable_output_ (the_disable_output),
           flags_(ACE_LOG_MSG->flags ())
       {
         idl_global->in_eval_ = true;
