82f8587c74e84654cff32984c42810df9344f625
Nick Williams <nicholas.williams@zaxiom.com>
2020-07-29 16:24:10 -0500

    Fix #1166: Support the portspan endpoint option in DIOP
    - First, fixed the `ORB_init/Portspan` test, which was failing in master:
      - `Spawn()` returns instantly and does not wait for the process to fail, so the test was failing with the error "ERROR: Last server didn't fail! Err:0."
      - Updated the test to, after spawning, wait a second to make sure the process is still running (or, in the case of the last server, to check that the process has failed).
      - The test now passed.
    - Next, updated `ORB_init/Portspan` to test DIOP as well as IIOP. The test then failed.
    - Finally, updated `DIOP_Acceptor` to support `portspan` in the same manner as `IIOP_Acceptor` (namely, to loop through the port span until an open port is found). The test now passes again.

--- TAO/tests/ORB_init/Portspan/run_test.pl
+++ TAO/tests/ORB_init/Portspan/run_test.pl
@@ -11,40 +11,61 @@ $status = 0;
 
 my $server = PerlACE::TestTarget::create_target (1) || die "Create target 1 failed\n";
 
-$port = $server->RandomPort ();
+foreach my $protocol ("iiop", "diop") {
+    $port = $server->RandomPort ();
+    $endpoint = "$protocol://:$port/portspan=2";
+    print STDOUT "Creating three $protocol servers with endpoint $endpoint...\n";
 
-$SV1 = $server->CreateProcess ("server", "-ORBEndpoint iiop://:$port/portspan=2");
-$SV2 = $server->CreateProcess ("server", "-ORBEndpoint iiop://:$port/portspan=2");
-$SV3 = $server->CreateProcess ("server", "-ORBEndpoint iiop://:$port/portspan=2");
+    $SV1 = $server->CreateProcess ("server", "-ORBEndpoint $endpoint");
+    $SV2 = $server->CreateProcess ("server", "-ORBEndpoint $endpoint");
+    $SV3 = $server->CreateProcess ("server", "-ORBEndpoint $endpoint");
 
-$server_status = $SV1->Spawn ();
+    print STDOUT "Starting server 1\n";
+    $server_status = $SV1->Spawn ();
+    if ($server_status != 0) {
+        print STDERR "ERROR: server failed to start: $server_status\n";
+        exit 1;
+    }
+    $server_status = $SV1->Wait (1);
+    if ($server_status != -1) {
+        print STDERR "ERROR: server exited prematurely: $server_status\n";
+        exit 1;
+    }
+    print STDOUT "Started server 1\n";
 
-if ($server_status != 0) {
-    print STDERR "ERROR: server returned $server_status\n";
-    exit 1;
+    print STDOUT "Starting server 2\n";
+    $server_status = $SV2->Spawn ();
+    if ($server_status != 0) {
+        print STDERR "ERROR: server failed to start: $server_status\n";
+        $SV1->Kill ();
+        exit 1;
+    }
+    $server_status = $SV2->Wait (1);
+    if ($server_status != -1) {
+        print STDERR "ERROR: server exited prematurely: $server_status\n";
+        exit 1;
+    }
+    print STDOUT "Started server 2\n";
+
+    print STDOUT "Starting server 3\n";
+    $server_status = $SV3->Spawn ();
+    if ($server_status == 0) {
+        $server_status = $SV3->Wait (1);
+        if ($server_status == -1) {
+            print STDERR "ERROR: Last server didn't fail, still running!\n";
+            $SV1->Kill ();
+            $SV2->Kill ();
+            $SV3->Kill ();
+            exit 1;
+        }
+    }
+    print STDOUT "Success: server 3 failed to start, stopping...\n";
+
+    $SV1->Kill();
+    $SV2->Kill();
+    $SV3->Kill();
+
+    print STDOUT "Portspan $protocol test completed successfully.\n\n";
 }
-
-$server_status = $SV2->Spawn ();
-
-if ($server_status != 0) {
-    print STDERR "ERROR: server returned $server_status\n";
-    $SV1->Kill ();
-    exit 1;
-}
-
-$server_status = $SV3->Spawn ();
-if ($server_status == 0) {
-    print STDERR "ERROR: Last server didn't fail! Err:$server_status\n";
-    $SV1->Kill ();
-    $SV2->Kill ();
-    $SV3->Kill ();
-    exit 1;
-}
-
-$SV1->Kill();
-$SV2->Kill();
-$SV3->Kill();
-
-print STDOUT "Portspan test completed successfully.\n";
 
 exit $status;
