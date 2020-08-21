7bc0aecc3d80e8d105be68f329ede4a8c3a3fc6e
Johnny Willemsen <jwillemsen@remedy.nl>
2020-08-21 11:16:06 +0200

    With C++11 make use of std::unique_ptr
        * ACE/examples/APG/Naming/Name_Binding.h:
        * ACE/examples/APG/Reactor/HAStatus.cpp:

--- ACE/examples/APG/Reactor/HAStatus.cpp
+++ ACE/examples/APG/Reactor/HAStatus.cpp
@@ -93,9 +93,13 @@ ClientAcceptor::open (const ACE_INET_Addr &listen_addr)
 int
 ClientAcceptor::handle_input (ACE_HANDLE)
 {
-  ClientService *client;
+  ClientService *client = 0;
   ACE_NEW_RETURN (client, ClientService, -1);
+#if defined ACE_HAS_CPP11
+  std::unique_ptr<ClientService> p (client);
+#else
   auto_ptr<ClientService> p (client);
+#endif
 
   if (this->acceptor_.accept (client->peer ()) == -1)
     ACE_ERROR_RETURN ((LM_ERROR,
