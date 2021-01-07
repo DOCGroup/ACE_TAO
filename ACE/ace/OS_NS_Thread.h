8c07da5c4316895917a33dbaed2171ed7ad00448
Johnny Willemsen <jwillemsen@remedy.nl>
2021-01-07 09:32:19 +0100

    Move thr_gettid to inline
        * ACE/ace/OS_NS_Thread.cpp:
        * ACE/ace/OS_NS_Thread.h:
        * ACE/ace/OS_NS_Thread.inl:

--- ACE/ace/OS_NS_Thread.h
+++ ACE/ace/OS_NS_Thread.h
@@ -1767,7 +1767,7 @@ namespace ACE_OS {
    * For older Linux (pre 2.4.11) and other systems that don't have gettid(),
    * this uses ACE_NOTSUP_RETURN (-1).
    */
-  ACE_Export
+  ACE_NAMESPACE_INLINE_FUNCTION
   pid_t thr_gettid ();
 
   /**
