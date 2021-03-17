a2c34ec23eb876c261950835ed0b55ea1ec3fe30
Johnny Willemsen <jwillemsen@remedy.nl>
2021-03-17 10:28:38 +0100

    Removed ancient _MSVC_VER checks
        * ACE/ace/Select_Reactor.h:
        * ACE/tests/ACE_Init_Test.h:
        * ACE/tests/ACE_Init_TestDlg.h:
        * ACE/tests/ACE_Init_Test_StdAfx.h:

--- ACE/tests/ACE_Init_Test.h
+++ ACE/tests/ACE_Init_Test.h
@@ -14,10 +14,6 @@
 #ifndef AFX_ACE_INIT_TEST_H__64FDC9FE_F7F9_11D2_89B6_00A024CC68DB__INCLUDED_
 #define AFX_ACE_INIT_TEST_H__64FDC9FE_F7F9_11D2_89B6_00A024CC68DB__INCLUDED_
 
-#if _MSC_VER >= 1000
-#pragma once
-#endif // _MSC_VER >= 1000
-
 #ifndef __AFXWIN_H__
   #error include 'stdafx.h' before including this file for PCH
 #endif
