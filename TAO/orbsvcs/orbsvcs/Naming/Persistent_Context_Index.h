6e13193f8e9260d0a1dad3a7206d761439aa97b2
Johnny Willemsen <jwillemsen@remedy.nl>
2021-03-19 11:47:24 +0100

    Layout/void cleanup
        * TAO/orbsvcs/orbsvcs/Naming/Entries.h:
        * TAO/orbsvcs/orbsvcs/Naming/FaultTolerant/FT_PG_Group_Factory.cpp:
        * TAO/orbsvcs/orbsvcs/Naming/Naming_Loader.h:
        * TAO/orbsvcs/orbsvcs/Naming/Naming_Server.h:
        * TAO/orbsvcs/orbsvcs/Naming/Persistent_Context_Index.h:
        * TAO/orbsvcs/orbsvcs/Naming/Persistent_Naming_Context.h:
        * TAO/orbsvcs/orbsvcs/Naming/Persistent_Naming_Context_Factory.h:
        * TAO/orbsvcs/orbsvcs/Naming/Storable_Naming_Context_Activator.h:
        * TAO/orbsvcs/orbsvcs/Naming/Storable_Naming_Context_Factory.h:

--- TAO/orbsvcs/orbsvcs/Naming/Persistent_Context_Index.h
+++ TAO/orbsvcs/orbsvcs/Naming/Persistent_Context_Index.h
@@ -127,13 +127,12 @@ public:
   CosNaming::NamingContext_ptr root_context (void);
 
 private:
-
   /// Helper for the <init> method.  Iterates over <index_>, and
   /// creates a servant for each entry.
-  int recreate_all (void);
+  int recreate_all ();
 
   /// Helper for the <open> method.
-  int create_index (void);
+  int create_index ();
 
   /// Helper for <create_index> method: places hash table into an
   /// allocated space.
