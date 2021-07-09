6f4f2c50cdae99aeda006d9289557c4c3a20a98a
Johnny Willemsen <jwillemsen@remedy.nl>
2021-07-09 16:27:17 +0200

    Log the reply_status to indicate what we received
        * TAO/tao/DynamicInterface/Request.cpp:

--- TAO/tao/DynamicInterface/Request.cpp
+++ TAO/tao/DynamicInterface/Request.cpp
@@ -381,7 +381,7 @@ CORBA::Request::handle_response (TAO_InputCDR &incoming,
     default:
       // @@ (JP) Don't know what to do about any of these yet.
       TAOLIB_ERROR ((LM_ERROR,
-                  ACE_TEXT ("TAO (%P|%t) - Request::handle_response, unhandled reply status\n")));
+                  ACE_TEXT ("TAO (%P|%t) - Request::handle_response, unhandled reply status %d\n"), reply_status));
   }
 }
 
