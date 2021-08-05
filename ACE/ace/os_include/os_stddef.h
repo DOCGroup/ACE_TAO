2d5df2ff7e8012581017626ee8aa2c7586d8a3ea
Johnny Willemsen <jwillemsen@remedy.nl>
2021-08-05 13:28:47 +0200

    Removed check for ACE_LACKS_WCHAR_T, not set in any config file
        * ACE/ace/os_include/os_stddef.h:

--- ACE/ace/os_include/os_stddef.h
+++ ACE/ace/os_include/os_stddef.h
@@ -26,30 +26,5 @@
 
 #include /**/ <stddef.h>
 
-// Place all additions (especially function declarations) within extern "C" {}
-#ifdef __cplusplus
-extern "C"
-{
-#endif /* __cplusplus */
-
-/*
-  Integer type whose range of values can represent distinct wide-character
-  codes for all members of the largest character set specified among the
-  locales supported by the compilation environment: the null character has
-  the code value 0 and each member of the portable character set has a code
-  value equal to its value when used as the lone character in an integer
-  character constant.
-*/
-#if defined (ACE_LACKS_WCHAR_T)
-#  if !defined (ACE_WCHAR_T_TYPE)
-#    define ACE_WCHAR_T_TYPE long
-#  endif /* !ACE_WCHAR_T_TYPE */
-   typedef ACE_WCHAR_T_TYPE wchar_t;
-#endif /* ACE_LACKS_WCHAR_T */
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
 #include /**/ "ace/post.h"
 #endif /* ACE_OS_INCLUDE_OS_STDDEF_H */
