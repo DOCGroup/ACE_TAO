38bfd5a69ed2f584e436980b9fe309935010919f
Johnny Willemsen <jwillemsen@remedy.nl>
2021-08-16 08:28:09 +0200

    Updated Embarcadero version checks

--- ACE/ace/config-win32-borland.h
+++ ACE/ace/config-win32-borland.h
@@ -145,14 +145,10 @@
 # endif /* !__MT__ */
 #endif /* ACE_MT_SAFE && ACE_MT_SAFE != 0 */
 
-#if (__BORLANDC__ <= 0x750)
-# define ACE_LACKS_ISWCTYPE
-# define ACE_LACKS_ISCTYPE
-#endif
-
-#if (__BORLANDC__ >= 0x640) && (__BORLANDC__ <= 0x750)
-# define ACE_LACKS_STRTOK_R
-#endif
+#define ACE_LACKS_ISWCTYPE
+#define ACE_LACKS_ISCTYPE
+#define ACE_LACKS_STRTOK_R
+#define ACE_LACKS_ASCTIME_R
 
 #if (__BORLANDC__ <= 0x740)
 # define ACE_LACKS_LOCALTIME_R
