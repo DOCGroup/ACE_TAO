db2924baf25546bfb0572899e2d02082ecec4094
Tyler Mayoff <tyler@tylermayoff.com>
2022-06-19 20:56:16 -0400

    Updated test, and stubbed stream operators

--- TAO/tests/IDLv4/maps/main.cpp
+++ TAO/tests/IDLv4/maps/main.cpp
@@ -4,6 +4,7 @@
 #include "ace/OS_main.h"
 #include "ace/streams.h"
 #include <cstdlib>
+#include <cstring>
 
 namespace stream8 {
   std::ostream &
@@ -31,13 +32,42 @@ expect_equals (bool &any_failed, const char *name, IntType actual, IntType expec
     }
 }
 
+template <>
+void
+expect_equals (bool &any_failed, const char *name, const char* actual, const char* expected)
+{
+  if (std::strcmp(actual, expected) != 0)
+    {
+      using stream8::operator<<;
+      *ACE_DEFAULT_LOG_STREAM
+        << "ERROR: For " << name << " expected: " << expected
+        << ", but got " << actual << "\n";
+      any_failed = true;
+    }
+}
+
 int ACE_TMAIN(int, ACE_TCHAR *[])
 {
   DataStruct d;
-  d.mapData[0] = 1;
+  d.mapData[0] = 0;
+  d.mapData[1] = 1;
+  d.mapData[2] = 2;
+  d.mapData[3] = 3;
+  d.mapData[4] = 4;
+  d.mapData[5] = 5;
 
   bool any_failed = false;
-  expect_equals<CORBA::Long> (any_failed, "d.mapData.size()", d.mapData.size(), 1);
+  expect_equals<CORBA::Long> (any_failed, "d.mapData.size()", d.mapData.size(), 6);
+  expect_equals<CORBA::Long> (any_failed, "d.mapData[0]", d.mapData[0], 0);
+  d.mapData[0] = 10;
+  expect_equals<CORBA::Long> (any_failed, "d.mapData[0]", d.mapData[0], 10);
+
+  d.stringMapStructs["Test"] = TestStruct{0, "Test Struct"};
+  expect_equals<CORBA::Long> (any_failed, "d.stringMapStructs['Test']", d.stringMapStructs["Test"].id, 0);
+  expect_equals<const char*> (any_failed, "d.stringMapStructs['Test']", d.stringMapStructs["Test"].msg, "Test Struct");
+
+  if (any_failed)
+    return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
 }
