d1a837e449fa8908c3e836f4faf977b22f2de98a
Johnny Willemsen <jwillemsen@remedy.nl>
2022-07-01 12:25:29 +0200

    Updated comment
        * ACE/ace/config-vxworks22.03.h:

--- ACE/ace/config-vxworks22.03.h
+++ ACE/ace/config-vxworks22.03.h
@@ -1,7 +1,7 @@
 /* -*- C++ -*- */
 // The following configuration file is designed to work for VxWorks
 // 22.03 platforms using one of these compilers:
-// 1) The clang compiler that is shipped with VxWorks 22.03
+// 1) The llvm compiler that is shipped with VxWorks 22.03
 
 #ifndef ACE_CONFIG_VXWORKS_22_03_H
 #define ACE_CONFIG_VXWORKS_22_03_H
