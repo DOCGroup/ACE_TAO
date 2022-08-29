93d938a9e43e4c368d0cd5caabedf4651a0737f2
Johnny Willemsen <jwillemsen@remedy.nl>
2022-08-29 08:59:18 +0200

    No need to define ACE_FALLTHROUGH for C++14, it is alreadys set from the C++11 part

--- ACE/ace/config-g++-common.h
+++ ACE/ace/config-g++-common.h
@@ -26,11 +26,6 @@
 #endif
 #if __cplusplus >= 201402L
 # define ACE_HAS_CPP14
-#if __GNUC__ >= 7
-# define ACE_FALLTHROUGH [[gnu::fallthrough]]
-#else
-# define ACE_FALLTHROUGH
-#endif
 #endif
 #if __cplusplus >= 201703L
 # define ACE_HAS_CPP17
