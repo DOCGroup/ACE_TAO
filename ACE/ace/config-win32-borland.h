c9737bc0a77324eb91dd7e0a94d0ff7099eda09e
Johnny Willemsen <jwillemsen@remedy.nl>
2022-09-14 13:49:46 +0200

    Use bcc32x in 32bit mode, at that moment it should have the same arguments as bcc64
        * ACE/ace/config-win32-borland.h:

--- ACE/ace/config-win32-borland.h
+++ ACE/ace/config-win32-borland.h
@@ -47,12 +47,12 @@
 # define ACE_HAS_BCC32
 #endif
 
+#define ACE_CC_PREPROCESSOR_ARGS "--precompile -q -o%s"
+
 #if defined (ACE_HAS_BCC64)
-# define ACE_CC_PREPROCESSOR_ARGS "--precompile -q -o%s"
 # define ACE_CC_PREPROCESSOR "BCC64.EXE"
 #else
-# define ACE_CC_PREPROCESSOR_ARGS "-Xdriver -E -q -o%s"
-# define ACE_CC_PREPROCESSOR "BCC32C.EXE"
+# define ACE_CC_PREPROCESSOR "BCC32X.EXE"
 #endif
 
 # include "ace/config-win32-common.h"
