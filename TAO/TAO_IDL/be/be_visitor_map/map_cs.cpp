6ceaada7c54077c188b207ee69fe54906e3010b1
Tyler Mayoff <tyler@tylermayoff.com>
2022-10-13 18:50:35 -0400

    cleaned up some generated code

--- TAO/TAO_IDL/be/be_visitor_map/map_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_map/map_cs.cpp
@@ -22,45 +22,5 @@ be_visitor_map_cs::be_visitor_map_cs (be_visitor_context *ctx)
 
 int be_visitor_map_cs::visit_map (be_map *node)
 {
-  if (node->defined_in () == nullptr)
-    {
-      // The node is a nested map, and has had no scope defined.
-      node->set_defined_in (DeclAsScope (this->ctx_->scope ()->decl ()));
-    }
-
-  // First create a name for ourselves.
-  if (node->create_name (this->ctx_->tdef ()) == -1)
-    {
-      ACE_ERROR_RETURN ((LM_ERROR,
-                         ACE_TEXT ("be_visitor_map_ch::")
-                         ACE_TEXT ("visit_map - ")
-                         ACE_TEXT ("failed creating name\n")),
-                        -1);
-    }
-
-  TAO_OutStream *os = this->ctx_->stream ();
-
-  *os << be_nl_2;
-
-  TAO_INSERT_COMMENT (os);
-
-  os->gen_ifdef_macro (node->flat_name ());
-
-  *os << be_nl_2;
-
-  *os << "typedef std::map< ";
-
-  be_type* kt = node->key_type();
-  be_type* vt = node->value_type();
-
-  *os << kt->full_name ();
-  *os << ", ";
-  *os << vt->full_name ();
-
-  *os << "> " << node->local_name () << ";";
-
-  os->gen_endif ();
-  node->cli_hdr_gen (true);
-
   return 0;
 }
