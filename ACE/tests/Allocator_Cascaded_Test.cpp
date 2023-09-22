cf686b38b7705bcc6c70e5e5556322469ec3506b
smitAchang <changyunlei@126.com>
2023-09-22 17:49:28 +0800

    add the pool_sum api

--- ACE/tests/Allocator_Cascaded_Test.cpp
+++ ACE/tests/Allocator_Cascaded_Test.cpp
@@ -31,29 +31,39 @@ run_free_lock_cascaded_allocator_test ()
 {
   ACE_DEBUG ((LM_INFO, "%C begin to run ...\n", __func__));
 
+  const size_t initial_n_chunks  = 1;
+  const size_t chunk_size        = sizeof(void*);
+
   void *ptr, *ptr1, *ptr2;
-  size_t initial_n_chunks  = 1;
-  size_t chunk_size        = sizeof(void*);
+  size_t nbytes = chunk_size;
+  size_t chunk_sum, old_chunk_sum;
 
   ACE_Cascaded_Dynamic_Cached_Allocator<ACE_Null_Mutex> alloc (initial_n_chunks, sizeof (void*));
+  chunk_sum = alloc.pool_sum ();
+  ACE_TEST_EXCEPTION_RETURN (chunk_sum != initial_n_chunks, "  initial pool sum must be initial_n_chunks\n");
 
+  ACE_DEBUG ((LM_INFO, "%C will test unsupported API ...\n", __func__));
   ptr = alloc.calloc (1, sizeof(void*));
-
-  ACE_TEST_EXCEPTION_RETURN (ptr != nullptr, "  pool must return nullptr for ccalloc(size_t n_elem, size_t elem_size, char initial_value) call\n");
-
-  ACE_TEST_EXCEPTION_RETURN(alloc.pool_depth() != initial_n_chunks, "  initial pool depth must be one\n");
-
-  size_t nbytes = chunk_size;
+  ACE_TEST_EXCEPTION_RETURN (ptr != nullptr, "  pool must return nullptr for calloc(size_t n_elem, size_t elem_size, char initial_value) call\n");
+  ACE_TEST_EXCEPTION_RETURN(alloc.pool_depth() != initial_n_chunks, "  initial pool depth must keep unchanged for call of unsupported API\n");
 
   ptr = alloc.malloc(nbytes);
   ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != 0, "  initial pool depth must be zero\n");
   alloc.free(ptr);
-  ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != initial_n_chunks, "  initial pool depth must be one after free\n");
+  ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != initial_n_chunks, "  initial pool depth must be initial_n_chunks after free\n");
 
   ACE_DEBUG ((LM_INFO, "%C will test cascaded allocator ...\n", __func__));
-  ptr = alloc.malloc (nbytes);
+  ptr  = alloc.malloc (nbytes);
+  ACE_TEST_EXCEPTION_RETURN (ptr == nullptr, "  pool must return valid ptr, cascaded pool must support to alloc more times firstly\n");
+
   ptr1 = alloc.malloc (nbytes);
-  ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != 1, "  cascaded pool depth must can support to alloc twice\n");
+  ACE_TEST_EXCEPTION_RETURN (ptr1 == nullptr, "  pool must return valid ptr, cascaded pool must support to alloc more times secondly\n");
+  ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != 1, "  cascaded pool depth must support to alloc twice\n");
+
+  old_chunk_sum = chunk_sum;
+  chunk_sum     = alloc.pool_sum ();
+  ACE_TEST_EXCEPTION_RETURN (chunk_sum < old_chunk_sum, "  cascaded pool sum must be bigger than that of initial pool\n");
+  ACE_TEST_EXCEPTION_RETURN (chunk_sum != (3*initial_n_chunks), "  cascaded pool sum must be as expected, pool has been enlarged\n");
 
   ptr2 = alloc.calloc (nbytes);
   ACE_TEST_EXCEPTION_RETURN (*static_cast<ACE_UINT64*>(ptr2) != 0, "  calloc call will clear the memory to zero\n");
@@ -61,8 +71,8 @@ run_free_lock_cascaded_allocator_test ()
   alloc.free (ptr);
   alloc.free (ptr1);
   alloc.free (ptr2);
-  ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != (initial_n_chunks + 2 * initial_n_chunks),
-                             "  cascaded pool depth must be three after freed all malloc ptrs\n");
+  ACE_TEST_EXCEPTION_RETURN (alloc.pool_depth () != (initial_n_chunks + (2 * initial_n_chunks)),
+                             "  cascaded pool depth must be three after having freed all malloc ptrs\n");
 
   return 0;
 }
