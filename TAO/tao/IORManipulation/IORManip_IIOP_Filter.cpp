3e51b282dea0957756ff611a8707be7d2ccf1df5
Johnny Willemsen <jwillemsen@remedy.nl>
2023-10-25 09:07:20 +0200

    Make use of std::nothrow
        * TAO/tao/IORManipulation/IORManip_IIOP_Filter.cpp:
        * TAO/tao/LocateRequest_Invocation.cpp:
        * TAO/tao/Object.cpp:
        * TAO/tao/Synch_Invocation.cpp:

--- TAO/tao/IORManipulation/IORManip_IIOP_Filter.cpp
+++ TAO/tao/IORManipulation/IORManip_IIOP_Filter.cpp
@@ -124,12 +124,11 @@ TAO_IORManip_IIOP_Filter::filter_and_add (TAO_Profile* profile,
               }
             else
               {
-                TAO_IIOP_Endpoint *endpoint = 0;
-                ACE_NEW_NORETURN (endpoint,
-                                   TAO_IIOP_Endpoint (endpoints[i].host,
-                                                      endpoints[i].port,
-                                                      endpoints[i].priority));
-                if (endpoint == 0)
+                TAO_IIOP_Endpoint *endpoint =
+                  new (std::nothrow) TAO_IIOP_Endpoint (endpoints[i].host,
+                                                        endpoints[i].port,
+                                                        endpoints[i].priority);
+                if (!endpoint)
                   {
                     new_profile->_decr_refcnt ();
                     return;
