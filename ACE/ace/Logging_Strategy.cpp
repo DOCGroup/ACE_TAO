5e4cba4dae83090d6df486ca04876a11d5075970
Like Ma <likemartinma@gmail.com>
2024-07-24 18:38:17 +0800

    Reset ACE_Log_Msg::msg_ostream if necessary

--- ACE/ace/Logging_Strategy.cpp
+++ ACE/ace/Logging_Strategy.cpp
@@ -443,8 +443,10 @@ ACE_Logging_Strategy::handle_timeout (const ACE_Time_Value &,
 #if defined (ACE_LACKS_IOSTREAM_TOTALLY)
       FILE *output_file = (FILE *) this->log_msg_->msg_ostream ();
       if (output_file)
-        ACE_OS::fclose (output_file);
-      this->log_msg_->msg_ostream (nullptr);
+        {
+          ACE_OS::fclose (output_file);
+          this->log_msg_->msg_ostream (nullptr);
+        }
 #else
       ofstream *output_file =
         (ofstream *) this->log_msg_->msg_ostream ();
