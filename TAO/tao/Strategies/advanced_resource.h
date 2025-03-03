b41152ee9f38cdea595524399660190de78a6917
Son Dinh <dinhs@objectcomputing.com>
2025-03-03 10:38:58 -0600

    Fix build

--- TAO/tao/Strategies/advanced_resource.h
+++ TAO/tao/Strategies/advanced_resource.h
@@ -64,7 +64,7 @@ public:
     TAO_REACTOR_WFMO      = 3,
     TAO_REACTOR_MSGWFMO   = 4,
     TAO_REACTOR_TP        = 5,
-    TAO_REACTOR_DEV_POLL  = 6
+    TAO_REACTOR_DEV_POLL  = 6,
     TAO_REACTOR_SINGLE_INPUT,
   };
 
