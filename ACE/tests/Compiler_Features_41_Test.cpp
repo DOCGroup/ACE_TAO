7bb13342183d15d93f6c3884151ef56792ae47d5
Johnny Willemsen <jwillemsen@remedy.nl>
2025-03-10 13:48:05 +0100

    New test for __try/__finally
        * ACE/tests/Compiler_Features_41_Test.cpp:
          Added.
    
        * ACE/tests/run_test.lst:
        * ACE/tests/tests.mpc:

---
+++ ACE/tests/Compiler_Features_41_Test.cpp
@@ -0,0 +1,42 @@
+/**
+ * This program checks if the compiler/RTL does have correct support
+ * for structured exception handling using __try/__finally
+ */
+
+#include "test_config.h"
+
+int
+run_main (int, ACE_TCHAR *[])
+{
+  ACE_START_TEST (ACE_TEXT("Compiler_Features_41_Test"));
+  int result = 0;
+
+#if defined (ACE_HAS_WIN32_STRUCTURED_EXCEPTIONS)
+  ACE_DEBUG ((LM_DEBUG,("Testing __try/__finally\n")));
+
+  ACE_SEH_TRY
+    {
+      ACE_DEBUG ((LM_DEBUG,("In SEH_TRY\n")));
+
+      char *cause_exception {};
+      char a = *cause_exception;
+      result = -1;
+
+    }
+  ACE_SEH_FINALLY
+    {
+      ACE_DEBUG ((LM_DEBUG,("In SEH_FINALLY\n")));
+    }
+
+  ACE_DEBUG ((LM_DEBUG,("After SEH_FINALLY\n")));
+
+#else
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("Platform lacks ACE_HAS_WIN32_STRUCTURED_EXCEPTIONS\n")));
+#endif /* ACE_HAS_WIN32_STRUCTURED_EXCEPTIONS */
+
+  ACE_END_TEST;
+
+  return result;
+}
+
