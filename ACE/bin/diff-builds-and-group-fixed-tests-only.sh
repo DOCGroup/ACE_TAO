a1e14459fc7508bd3191f342862da32811287938
Johnny Willemsen <jwillemsen@remedy.nl>
2025-05-04 14:28:21 +0200

    Make ACE 8.0.3 and TAO 4.0.3 available
        * ACE/NEWS:
        * ACE/bin/diff-builds-and-group-fixed-tests-only.sh:
        * ACE/docs/Download.html:
        * ACE/etc/index.html:
        * TAO/NEWS:

--- ACE/bin/diff-builds-and-group-fixed-tests-only.sh
+++ ACE/bin/diff-builds-and-group-fixed-tests-only.sh
@@ -2,7 +2,7 @@
 
 if test -z $1; then newdate=`date -u +%Y_%m_%d`; else newdate=$1; fi
 if test -z $2; then prefix=`date -u +%Y%m%d%a`; else prefix=$2; fi
-if test -z $3; then olddate=2024_12_06; else olddate=$3; fi
+if test -z $3; then olddate=2025_05_04; else olddate=$3; fi
 if test -z $ACE_ROOT; then ACE_ROOT=..; fi
 if test -z $TAO_ROOT; then TAO_ROOT=${ACE_ROOT}/TAO; fi
 #
