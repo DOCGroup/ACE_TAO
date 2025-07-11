160e1328e6e807e9419c280a841d4d2f5614cbaf
Johnny Willemsen <jwillemsen@remedy.nl>
2025-07-11 10:24:17 +0200

    Update ACE/apps/JAWS/server/HTTP_Server.cpp
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- ACE/apps/JAWS/server/HTTP_Server.cpp
+++ ACE/apps/JAWS/server/HTTP_Server.cpp
@@ -17,11 +17,15 @@
 class JAWS
 {
 public:
-  static constexpr size_t JAWS_POOL = 0;
-  static constexpr size_t JAWS_PER_REQUEST = 1;
+class JAWS
+{
+public:
+  static constexpr int JAWS_POOL         = 0;
+  static constexpr int JAWS_PER_REQUEST  = 1;
 
-  static constexpr size_t JAWS_SYNCH = 0;
-  static constexpr size_t JAWS_ASYNCH = 2;
+  static constexpr int JAWS_SYNCH        = 0;
+  static constexpr int JAWS_ASYNCH       = 2;
+};
 };
 
 void
