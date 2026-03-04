a84d9698a23e1fd03e1e3374521c3d891f60d135
Sihu Hwang <129564966+hwangsihu@users.noreply.github.com>
2026-03-04 18:41:36 +0900

    Add support for Visual Studio 2026

---
+++ ACE/ace/config-win32-msvc-145.h
@@ -0,0 +1,29 @@
+/* -*- C++ -*- */
+//=============================================================================
+/**
+ *  @file   config-win32-msvc-145.h
+ *
+ *  @brief  Microsoft Visual C++ 14.5 configuration file.
+ *
+ *  This file is the ACE configuration file for Microsoft Visual C++ 14.5 (as released with Visual Studio 2026).
+ *
+ *  @note Do not include this file directly, include config-win32.h instead.
+ */
+//=============================================================================
+
+#ifndef ACE_CONFIG_WIN32_MSVC_145_H
+#define ACE_CONFIG_WIN32_MSVC_145_H
+#include /**/ "ace/pre.h"
+
+#ifndef ACE_CONFIG_WIN32_H
+#error Use config-win32.h in config.h instead of this header
+#endif /* ACE_CONFIG_WIN32_H */
+
+#ifndef ACE_WIN32_VC145
+#  define ACE_WIN32_VC145
+#endif
+
+#include "ace/config-win32-msvc-143.h"
+
+#include /**/ "ace/post.h"
+#endif /* ACE_CONFIG_WIN32_MSVC_145_H */
