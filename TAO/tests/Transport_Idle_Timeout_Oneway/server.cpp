5b8884a59c7b5bc9f2982d213cab085001c27130
Johnny Willemsen <jwillemsen@remedy.nl>
2026-09-11 14:41:29 +0200

    Add oneway idle timeout test server
    (cherry picked from commit e91dbdc36f6a72561f464b2aeaff787119be7861)

---
+++ TAO/tests/Transport_Idle_Timeout_Oneway/server.cpp
@@ -0,0 +1,76 @@
+#include "OnewayIdle_i.h"
+#include "ace/Get_Opt.h"
+#include "ace/Log_Msg.h"
+#include "ace/OS_NS_stdio.h"
+
+static const ACE_TCHAR *ior_output_file = ACE_TEXT ("test.ior");
+
+static int
+parse_args (int argc, ACE_TCHAR *argv[])
+{
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT ("o:"));
+  int c;
+  while ((c = get_opts ()) != -1)
+    {
+      switch (c)
+        {
+        case 'o':
+          ior_output_file = get_opts.opt_arg ();
+          break;
+        default:
+          ACE_ERROR_RETURN ((LM_ERROR,
+                             ACE_TEXT ("Usage: server [-o <ior_file>]\n")),
+                            -1);
+        }
+    }
+  return 0;
+}
+
+int
+ACE_TMAIN (int argc, ACE_TCHAR *argv[])
+{
+  try
+    {
+      CORBA::ORB_var orb = CORBA::ORB_init (argc, argv);
+
+      if (parse_args (argc, argv) != 0)
+        return 1;
+
+      CORBA::Object_var poa_object =
+        orb->resolve_initial_references ("RootPOA");
+      PortableServer::POA_var root_poa =
+        PortableServer::POA::_narrow (poa_object.in ());
+      PortableServer::POAManager_var manager = root_poa->the_POAManager ();
+
+      OnewayIdle_i *impl = nullptr;
+      ACE_NEW_RETURN (impl, OnewayIdle_i (orb.in ()), 1);
+      PortableServer::ServantBase_var owner (impl);
+
+      PortableServer::ObjectId_var oid = root_poa->activate_object (impl);
+      CORBA::Object_var object = root_poa->id_to_reference (oid.in ());
+      Test::OnewayIdle_var test = Test::OnewayIdle::_narrow (object.in ());
+
+      CORBA::String_var ior = orb->object_to_string (test.in ());
+      FILE *file = ACE_OS::fopen (ior_output_file, ACE_TEXT ("w"));
+      if (file == nullptr)
+        ACE_ERROR_RETURN ((LM_ERROR,
+                           ACE_TEXT ("Cannot open output file '%s'\n"),
+                           ior_output_file),
+                          1);
+      ACE_OS::fprintf (file, "%s", ior.in ());
+      ACE_OS::fclose (file);
+
+      manager->activate ();
+      orb->run ();
+
+      root_poa->destroy (true, true);
+      orb->destroy ();
+    }
+  catch (const CORBA::Exception &ex)
+    {
+      ex._tao_print_exception (ACE_TEXT ("server exception"));
+      return 1;
+    }
+
+  return 0;
+}
