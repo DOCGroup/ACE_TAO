a250b8f75252d1f2b06f126cc00ea5a1f3683dcf
Johnny Willemsen <jwillemsen@remedy.nl>
2026-09-11 15:00:54 +0200

    Make oneway client exit after ping
    (cherry picked from commit ceefb87ed7d242253f1833c9a675d552aa3e2454)

--- TAO/tests/Transport_Idle_Timeout_Oneway/client.cpp
+++ TAO/tests/Transport_Idle_Timeout_Oneway/client.cpp
@@ -1,31 +1,13 @@
 #include "testC.h"
 #include "ace/Get_Opt.h"
 #include "ace/Log_Msg.h"
-#include "ace/OS_NS_sys_time.h"
 
 static const char *ior = nullptr;
-static int timeout_sec = 3;
-
-namespace
-{
-  void
-  wait_with_reactor (CORBA::ORB_ptr orb, int seconds)
-  {
-    ACE_Time_Value const deadline =
-      ACE_OS::gettimeofday () + ACE_Time_Value (seconds);
-
-    while (ACE_OS::gettimeofday () < deadline)
-      {
-        ACE_Time_Value tv (0, 50000);
-        orb->perform_work (tv);
-      }
-  }
-}
 
 static int
 parse_args (int argc, ACE_TCHAR *argv[])
 {
-  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT ("k:t:"));
+  ACE_Get_Opt get_opts (argc, argv, ACE_TEXT ("k:"));
   int c;
   while ((c = get_opts ()) != -1)
     {
@@ -34,12 +16,9 @@ parse_args (int argc, ACE_TCHAR *argv[])
         case 'k':
           ior = get_opts.opt_arg ();
           break;
-        case 't':
-          timeout_sec = ACE_OS::atoi (get_opts.opt_arg ());
-          break;
         default:
           ACE_ERROR_RETURN ((LM_ERROR,
-                             ACE_TEXT ("Usage: client -k <ior> [-t <sec>]\n")),
+                             ACE_TEXT ("Usage: client -k <ior>\n")),
                             -1);
         }
     }
@@ -68,37 +47,9 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
                            ACE_TEXT ("Narrow to Test::OnewayIdle failed\n")),
                           1);
 
-      int const wait_sec = timeout_sec + 2;
-      ACE_DEBUG ((LM_INFO,
-                  ACE_TEXT ("Sending oneway ping; server waits %d seconds ")
-                  ACE_TEXT ("with idle timeout %d seconds\n"),
-                  wait_sec,
-                  timeout_sec));
+      ACE_DEBUG ((LM_INFO, ACE_TEXT ("Sending oneway ping\n")));
+      test->ping ();
 
-      test->ping (wait_sec);
-
-      // Give the server enough time to finish the oneway operation before
-      // fetching the value it recorded while handling that operation.
-      wait_with_reactor (orb.in (), wait_sec + 1);
-
-      CORBA::Long const observed = test->observed_cache_size ();
-      if (observed != 1)
-        {
-          ACE_ERROR ((LM_ERROR,
-                      ACE_TEXT ("[FAIL] server cache size after oneway wait = %d; ")
-                      ACE_TEXT ("expected 1 (transport was closed)\n"),
-                      observed));
-          test->shutdown ();
-          orb->destroy ();
-          return 1;
-        }
-
-      ACE_DEBUG ((LM_INFO,
-                  ACE_TEXT ("[PASS] server cache size after oneway wait = %d; ")
-                  ACE_TEXT ("transport remained open\n"),
-                  observed));
-
-      test->shutdown ();
       orb->destroy ();
     }
   catch (const CORBA::Exception &ex)
