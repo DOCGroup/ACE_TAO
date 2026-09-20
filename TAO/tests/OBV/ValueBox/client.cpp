5e7be8dd8fdaefde1019e15dec6043fd687b5e63
Johnny Willemsen <jwillemsen@remedy.nl>
2026-09-20 21:07:25 +0200

    Use nullptr in valuebox test

--- TAO/tests/OBV/ValueBox/client.cpp
+++ TAO/tests/OBV/ValueBox/client.cpp
@@ -131,7 +131,7 @@ template <class BoxT, class UT>
 int default_box_test (UT expected)
 {
     int fail {};
-    BoxT *valuebox {};
+    BoxT *valuebox {nullptr};
     ACE_NEW_RETURN (valuebox,
                     BoxT (),
                     1);
