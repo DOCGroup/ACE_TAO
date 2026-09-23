fc480e4e35cdc053a8e7e6411e8842c628166a5e
Johnny Willemsen <jwillemsen@remedy.nl>
2026-09-23 11:36:06 +0200

    Compact generated union array allocation

--- TAO/TAO_IDL/be/be_visitor_union_branch/public_constructor_cs.cpp
+++ TAO/TAO_IDL/be/be_visitor_union_branch/public_constructor_cs.cpp
@@ -126,8 +126,7 @@ be_visitor_union_branch_public_constructor_cs::visit_array (be_array *node)
 
   // set the discriminant to the appropriate label
   *os << "this->u_." << ub->local_name ()
-      << "_ =" << be_idt_nl
-      << fname << "_alloc ();" << be_uidt_nl
+      << "_ = " << fname << "_alloc ();" << be_nl
       << "if (this->u_." << ub->local_name () << "_)" << be_nl
       << "{" << be_idt_nl
       << "TAO::Array_Traits<" << fname << "_forany>::zero ("
